7baf062afcdb0d48c92556f9ec61959d04d3efd5
Eric Andersen <andersen@codepoet.org>
2002-11-21 00:34:50 +0000

    Fixup findutils so it builds and installs properly

--- make/findutils.mk
+++ make/findutils.mk
@@ -22,7 +22,7 @@ $(FINDUTILS_DIR)/.unpacked: $(DL_DIR)/$(FINDUTILS_SOURCE)
 	touch $(FINDUTILS_DIR)/.unpacked
 
 $(FINDUTILS_DIR)/.configured: $(FINDUTILS_DIR)/.unpacked
-	(cd $(FINDUTILS_DIR); autoconf; rm -f config.cache; CC=$(TARGET_CC1) \
+	(cd $(FINDUTILS_DIR); rm -f config.cache; CC=$(TARGET_CC1) \
 	    CFLAGS=-D_POSIX_SOURCE ./configure --prefix=/usr --disable-nls \
 	    --mandir=/junk --infodir=/junk --localstatedir=/var/lib/locate \
 	    --libexecdir='$${prefix}/lib/locate' \
@@ -33,7 +33,12 @@ $(FINDUTILS_DIR)/$(FINDUTILS_BINARY): $(FINDUTILS_DIR)/.configured
 	$(MAKE) CC=$(TARGET_CC1) -C $(FINDUTILS_DIR)
 
 $(TARGET_DIR)/$(FINDUTILS_TARGET_BINARY): $(FINDUTILS_DIR)/$(FINDUTILS_BINARY)
-	$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC1) -C $(FINDUTILS_DIR) install
+	$(MAKE) prefix=$(TARGET_DIR)/usr exec_prefix=$(TARGET_DIR)/usr \
+	 bindir=$(TARGET_DIR)/usr/bin sbindir=$(TARGET_DIR)/usr/sbin \
+	 sysconfdir=$(TARGET_DIR)/usr/etc datadir=$(TARGET_DIR)/usr/share \
+	 includedir=$(TARGET_DIR)/usr/include libdir=$(TARGET_DIR)/usr/lib \
+	 localstatedir=$(TARGET_DIR)/var mandir=$(TARGET_DIR)/man \
+	 infodir=$(TARGET_DIR)/info CC=$(TARGET_CC1) -C $(FINDUTILS_DIR) install
 	rm -rf $(TARGET_DIR)/share/locale $(TARGET_DIR)/junk
 
 findutils: uclibc $(TARGET_DIR)/$(FINDUTILS_TARGET_BINARY)
