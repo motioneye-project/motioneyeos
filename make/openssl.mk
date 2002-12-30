1fff9412193643edf3e7d975dc6ecaae672cca6f
Eric Andersen <andersen@codepoet.org>
2002-12-30 09:47:45 +0000

    Fixup non-x86 openssl build

--- make/openssl.mk
+++ make/openssl.mk
@@ -8,28 +8,36 @@
 OPENSSL_SITE:=http://www.openssl.org/source
 OPENSSL_SOURCE:=openssl-0.9.6g.tar.gz
 OPENSSL_DIR:=$(BUILD_DIR)/openssl-0.9.6g
-
+OPENSSL_PATCH=$(SOURCE_DIR)/openssl.patch
 
 $(DL_DIR)/$(OPENSSL_SOURCE):
 	$(WGET) -P $(DL_DIR) $(OPENSSL_SITE)/$(OPENSSL_SOURCE)
 
-$(OPENSSL_DIR)/.unpacked: $(DL_DIR)/$(OPENSSL_SOURCE)
+$(OPENSSL_DIR)/.unpacked: $(DL_DIR)/$(OPENSSL_SOURCE) $(OPENSSL_PATCH)
 	gunzip -c $(DL_DIR)/$(OPENSSL_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	cat $(OPENSSL_PATCH) | patch -p1 -d $(OPENSSL_DIR)
 	touch  $(OPENSSL_DIR)/.unpacked
 
 $(OPENSSL_DIR)/Makefile: $(OPENSSL_DIR)/.unpacked
 	(cd $(OPENSSL_DIR); \
-	PATH=$(TARGET_PATH) ./Configure linux-elf --prefix=$(STAGING_DIR) \
+	PATH=$(TARGET_PATH) ./Configure linux-$(ARCH) --prefix=$(STAGING_DIR) \
 		--openssldir=$(STAGING_DIR) -L$(STAGING_DIR)/lib -ldl \
-		-I$(STAGING_DIR)/include no-threads shared no-asm)
+		-I$(STAGING_DIR)/include $(OPENSSL_OPTS) no-threads \
+		shared no-idea no-mdc2 no-rc5)
 
 $(OPENSSL_DIR)/apps/openssl: $(OPENSSL_DIR)/Makefile
-	make CC=$(TARGET_CC1) -C $(OPENSSL_DIR)
+	make CC=$(TARGET_CC1) -C $(OPENSSL_DIR) all build-shared
 
-$(STAGING_DIR)/bin/openssl: $(OPENSSL_DIR)/apps/openssl
+$(STAGING_DIR)/lib/libcrypto.so.0: $(OPENSSL_DIR)/apps/openssl
 	make CC=$(TARGET_CC1) -C $(OPENSSL_DIR) install
+	cp -fa $(OPENSSL_DIR)/libcrypto.so* $(STAGING_DIR)/lib/
+	(cd $(STAGING_DIR)/lib; ln -fs libcrypto.so.0.9.6 libcrypto.so)
+	(cd $(STAGING_DIR)/lib; ln -fs libcrypto.so.0.9.6 libcrypto.so.0)
+	cp -fa $(OPENSSL_DIR)/libssl.so* $(STAGING_DIR)/lib/
+	(cd $(STAGING_DIR)/lib; ln -fs libssl.so.0.9.6 libssl.so)
+	(cd $(STAGING_DIR)/lib; ln -fs libssl.so.0.9.6 libssl.so.0)
 
-$(TARGET_DIR)/bin/openssl: $(STAGING_DIR)/bin/openssl
+$(TARGET_DIR)/lib/libcrypto.so.0: $(STAGING_DIR)/lib/libcrypto.so.0
 	cp -fa $(STAGING_DIR)/lib/libcrypto.so* $(TARGET_DIR)/lib/
 	cp -fa $(STAGING_DIR)/lib/libssl.so* $(TARGET_DIR)/lib/
 	#cp -fa $(STAGING_DIR)/bin/openssl  $(TARGET_DIR)/bin/
@@ -43,7 +51,5 @@ openssl-clean:
 openssl-dirclean: 
 	rm -rf $(OPENSSL_DIR) 
 
-openssl: uclibc $(TARGET_DIR)/bin/openssl
-
-#EOF
+openssl: uclibc $(TARGET_DIR)/lib/libcrypto.so.0
 
