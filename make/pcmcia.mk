39eb7da8788d5f98b37fe8d730b2fbe121ce105e
Eric Andersen <andersen@codepoet.org>
2003-02-04 10:35:13 +0000

    Be extra sure that /etc/init.d/S30pcmcia is executable

--- make/pcmcia.mk
+++ make/pcmcia.mk
@@ -79,6 +79,7 @@ $(TARGET_DIR)/sbin/cardmgr: $(PCMCIA_DIR)/cardmgr/cardmgr
 	rm -f $(TARGET_DIR)/sbin/pcinitrd
 	rm -f $(TARGET_DIR)/sbin/probe
 	cp $(PCMCIA_DIR)/etc/rc.pcmcia $(TARGET_DIR)/etc/init.d/S30pcmcia
+	chmod a+x $(TARGET_DIR)/etc/init.d/S30pcmcia
 	chmod -R u+w $(TARGET_DIR)/etc/pcmcia/*
 
 pcmcia: uclibc $(TARGET_DIR)/sbin/cardmgr
