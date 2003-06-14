4cc06f8ea8ad95a5f6d0b21b88cdcd38e83646b2
Eric Andersen <andersen@codepoet.org>
2003-06-14 05:56:59 +0000

    Fixup iproute so it compiles up properly.  Use the debian source as the .ru site has availability problems

--- make/iproute2.mk
+++ make/iproute2.mk
@@ -3,43 +3,55 @@
 # iproute2
 #
 #############################################################
-IPROUTE2_SOURCE_URL=ftp://ftp.inr.ac.ru/ip-routing/iproute2-2.4.7-now-ss020116-try.tar.gz
-IPROUTE2_SOURCE=iproute2-2.4.7-now-ss020116-try.tar.gz
-IPROUTE2_BUILD_DIR=$(BUILD_DIR)/iproute2
+IPROUTE2_DIR=$(BUILD_DIR)/iproute2
+
+#IPROUTE2_SOURCE_URL=ftp://ftp.inr.ac.ru/ip-routing/
+#IPROUTE2_SOURCE=iproute2-2.4.7-now-ss020116-try.tar.gz
+
+#Use the debian source for now, as the .ru site has availability problems
+IPROUTE2_SOURCE_URL=http://ftp.debian.org/debian/pool/main/i/iproute/
+IPROUTE2_SOURCE=iproute_20010824.orig.tar.gz
+IPROUTE2_PATCH:=iproute_20010824-8.diff.gz
+
 
 $(DL_DIR)/$(IPROUTE2_SOURCE):
-	 $(WGET) -P $(DL_DIR) $(IPROUTE2_SOURCE_URL)
+	 $(WGET) -P $(DL_DIR) $(IPROUTE2_SOURCE_URL)$(IPROUTE2_SOURCE)
 
-hostap-source: $(DL_DIR)/$(IPROUTE2_SOURCE)
+$(DL_DIR)/$(IPROUTE2_PATCH):
+	$(WGET) -P $(DL_DIR) $(IPROUTE2_SOURCE_URL)/$(IPROUTE2_PATCH)
 
-$(IPROUTE2_BUILD_DIR)/.unpacked: $(DL_DIR)/$(IPROUTE2_SOURCE)
+iproute2-source: $(DL_DIR)/$(IPROUTE2_SOURCE) #$(DL_DIR)/$(IPROUTE2_PATCH)
+
+$(IPROUTE2_DIR)/.unpacked: iproute2-source
+	rm -rf $(IPROUTE2_DIR).orig $(IPROUTE2_DIR)
 	zcat $(DL_DIR)/$(IPROUTE2_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	touch $(IPROUTE2_BUILD_DIR)/.unpacked
+	#zcat $(DL_DIR)/$(IPROUTE2_PATCH) | patch -p1 -d $(IPROUTE2_DIR)
+	touch $(IPROUTE2_DIR)/.unpacked
 
-$(IPROUTE2_BUILD_DIR)/.configured: $(IPROUTE2_BUILD_DIR)/.unpacked
-	perl -pi -e "s,-I/usr/include/db3,," $(IPROUTE2_BUILD_DIR)/Makefile
+$(IPROUTE2_DIR)/.configured: $(IPROUTE2_DIR)/.unpacked
+	perl -pi -e "s,-I/usr/include/db3,," $(IPROUTE2_DIR)/Makefile
 	perl -pi -e "s,^KERNEL_INCLUDE.*,KERNEL_INCLUDE=$(LINUX_DIR)/include," \
-		$(IPROUTE2_BUILD_DIR)/Makefile
+		$(IPROUTE2_DIR)/Makefile
 	perl -pi -e "s,^LIBC_INCLUDE.*,LIBC_INCLUDE=$(STAGING_DIR)/include," \
-		$(IPROUTE2_BUILD_DIR)/Makefile
+		$(IPROUTE2_DIR)/Makefile
 	# For now disable compiling of the misc directory because it seems to fail
-	rm -rf $(IPROUTE2_BUILD_DIR)/misc 
-	perl -pi -e "s, misc,," $(IPROUTE2_BUILD_DIR)/Makefile
-	touch  $(IPROUTE2_BUILD_DIR)/.configured
+	rm -rf $(IPROUTE2_DIR)/misc 
+	perl -pi -e "s, misc,," $(IPROUTE2_DIR)/Makefile
+	touch  $(IPROUTE2_DIR)/.configured
 
-$(IPROUTE2_BUILD_DIR)/tc/tc: $(IPROUTE2_BUILD_DIR)/.configured
-	$(MAKE) -C $(IPROUTE2_BUILD_DIR) KERNEL_INCLUDE=$(LINUX_SOURCE_DIR)/include CC=$(TARGET_CC)
+$(IPROUTE2_DIR)/tc/tc: $(IPROUTE2_DIR)/.configured
+	$(MAKE) -C $(IPROUTE2_DIR) KERNEL_INCLUDE=$(LINUX_SOURCE_DIR)/include CC=$(TARGET_CC)
 
-$(TARGET_DIR)/usr/sbin/tc: $(IPROUTE2_BUILD_DIR)/tc/tc
+$(TARGET_DIR)/usr/sbin/tc: $(IPROUTE2_DIR)/tc/tc
 	# Copy The tc binary
-	cp -af $(IPROUTE2_BUILD_DIR)/tc/tc $(TARGET_DIR)/usr/sbin/
+	cp -af $(IPROUTE2_DIR)/tc/tc $(TARGET_DIR)/usr/sbin/
 
 iproute2: $(TARGET_DIR)/usr/sbin/tc 
 
 iproute2-clean:
-	$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(IPROUTE2_BUILD_DIR) uninstall
-	-$(MAKE) -C $(IPROUTE2_BUILD_DIR) clean
+	$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(IPROUTE2_DIR) uninstall
+	-$(MAKE) -C $(IPROUTE2_DIR) clean
 
 iproute2-dirclean:
-	rm -rf $(IPROUTE2_BUILD_DIR)
+	rm -rf $(IPROUTE2_DIR)
 
