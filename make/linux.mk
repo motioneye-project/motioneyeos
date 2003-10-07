976f46b798a83b7ecd546734e6324382f761c7bc
Eric Andersen <andersen@codepoet.org>
2003-10-07 10:52:23 +0000

    Update included kernel version

--- make/linux.mk
+++ make/linux.mk
@@ -22,9 +22,9 @@
 ifneq ($(filter $(TARGETS),linux),)
 
 # Version of Linux to download and then apply patches to
-DOWNLOAD_LINUX_VERSION=2.4.21
+DOWNLOAD_LINUX_VERSION=2.4.22
 # Version of Linux AFTER patches
-LINUX_VERSION=2.4.21-erik
+LINUX_VERSION=2.4.23-pre6-erik
 
 LINUX_FORMAT=bzImage
 LINUX_BINLOC=arch/$(ARCH)/boot/$(LINUX_FORMAT)
