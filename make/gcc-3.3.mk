196f87ce84b7e6140c34d930ba39b51313808e3a
Manuel Novoa III <mjn3@codepoet.org>
2003-11-24 20:50:12 +0000

    Fix the condition for applying the i386 soft float patch.

--- make/gcc-3.3.mk
+++ make/gcc-3.3.mk
@@ -67,7 +67,7 @@ $(GCC_DIR)/.unpacked: $(DL_DIR)/$(GCC_SOURCE)
 $(GCC_DIR)/.patched: $(GCC_DIR)/.unpacked
 	# Apply any files named gcc-*.patch from the source directory to gcc
 	$(SOURCE_DIR)/patch-kernel.sh $(GCC_DIR) $(SOURCE_DIR) gcc-*.patch
-ifneq ("$(strip $(TARGET_SOFT_FLOAT))","")
+ifeq ($(SOFT_FLOAT),true)
 ifeq ("$(strip $(ARCH))","i386")
 	$(SOURCE_DIR)/patch-kernel.sh $(GCC_DIR) $(SOURCE_DIR) i386-gcc-soft-float.patch
 endif
