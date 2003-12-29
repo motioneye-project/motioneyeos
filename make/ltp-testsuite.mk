df169b191c087b4ecb018795e242fad26529e2ee
Eric Andersen <andersen@codepoet.org>
2003-12-29 23:19:46 +0000

    Slightly newer ltp test

--- make/ltp-testsuite.mk
+++ make/ltp-testsuite.mk
@@ -3,10 +3,10 @@
 # ltp-testsuite
 #
 #############################################################
-LTP_TESTSUITE_SOURCE:=ltp-full-20031106.tgz
+LTP_TESTSUITE_SOURCE:=ltp-full-20031204.tgz
 LTP_TESTSUITE_SITE:=http://aleron.dl.sourceforge.net/sourceforge/ltp
 LTP_TESTSUITE_CAT:=zcat
-LTP_TESTSUITE_DIR:=$(BUILD_DIR)/ltp-full-20031106
+LTP_TESTSUITE_DIR:=$(BUILD_DIR)/ltp-full-20031204
 LTP_TESTSUITE_PATCH:=$(SOURCE_DIR)/ltp-testsuite.patch
 
 
@@ -28,12 +28,12 @@ $(TARGET_DIR)/usr/bin/ltp-testsuite: $(LTP_TESTSUITE_DIR)/ltp-testsuite
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) CROSS_COMPILER=$(TARGET_CROSS) \
 		-C $(LTP_TESTSUITE_DIR) install
 
-ltp-testsuite: uclibc $(TARGET_DIR)/usr/bin/ltp-testsuite 
+ltp-testsuite: uclibc $(TARGET_DIR)/usr/bin/ltp-testsuite
 
-ltp-testsuite-clean: 
+ltp-testsuite-clean:
 	$(MAKE) -C $(LTP_TESTSUITE_DIR) clean
 
-ltp-testsuite-dirclean: 
-	rm -rf $(LTP_TESTSUITE_DIR) 
+ltp-testsuite-dirclean:
+	rm -rf $(LTP_TESTSUITE_DIR)
 
 
