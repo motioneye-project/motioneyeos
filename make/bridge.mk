6cf315cd236b2fc4a3c0e2039624d21c247a40bb
Manuel Novoa III <mjn3@codepoet.org>
2004-03-05 18:59:41 +0000

    Clean up makefile.  Don't install (legacy?) brctld app since debian doesn't. Fix paths.  Add a patch to respect CFLAGS.  Fix buildroot kernel location.

--- make/bridge.mk
+++ make/bridge.mk
@@ -7,6 +7,7 @@
 BRIDGE_SOURCE_URL=http://bridge.sourceforge.net/bridge-utils
 BRIDGE_SOURCE=bridge-utils-0.9.6.tar.gz
 BRIDGE_BUILD_DIR=$(BUILD_DIR)/bridge-utils-0.9.6
+BRIDGE_TARGET_BINARY:=usr/sbin/brctl
 
 $(DL_DIR)/$(BRIDGE_SOURCE):
 	 $(WGET) -P $(DL_DIR) $(BRIDGE_SOURCE_URL)/$(BRIDGE_SOURCE) 
@@ -14,6 +15,7 @@ $(DL_DIR)/$(BRIDGE_SOURCE):
 $(BRIDGE_BUILD_DIR)/.unpacked: $(DL_DIR)/$(BRIDGE_SOURCE)
 	zcat $(DL_DIR)/$(BRIDGE_SOURCE) | tar -C $(BUILD_DIR) -xvf -
 	mv -f $(BUILD_DIR)/bridge-utils $(BRIDGE_BUILD_DIR)
+	patch -p1 -d $(BRIDGE_BUILD_DIR) < $(SOURCE_DIR)/bridge.patch 
 	touch $(BRIDGE_BUILD_DIR)/.unpacked
 
 $(BRIDGE_BUILD_DIR)/.configured: $(BRIDGE_BUILD_DIR)/.unpacked
@@ -35,23 +37,25 @@ $(BRIDGE_BUILD_DIR)/.configured: $(BRIDGE_BUILD_DIR)/.unpacked
 		--mandir=/usr/man \
 		--infodir=/usr/info \
 		$(DISABLE_NLS) \
-		--with-linux-headers=$(BUILD_DIR)/linux \
+		--with-linux=$(LINUX_DIR) \
 	);
 	touch  $(BRIDGE_BUILD_DIR)/.configured
 
 $(BRIDGE_BUILD_DIR)/brctl/brctl: $(BRIDGE_BUILD_DIR)/.configured
 	$(MAKE) -C $(BRIDGE_BUILD_DIR)
 
-$(TARGET_DIR)/sbin/brctl: $(BRIDGE_BUILD_DIR)/brctl/brctl
-	cp -af $(BRIDGE_BUILD_DIR)/brctl/brctl $(TARGET_DIR)/sbin/
-	cp -af $(BRIDGE_BUILD_DIR)/brctl/brctld $(TARGET_DIR)/sbin/
+$(TARGET_DIR)/$(BRIDGE_TARGET_BINARY): $(BRIDGE_BUILD_DIR)/brctl/brctl
+	cp -af $(BRIDGE_BUILD_DIR)/brctl/brctl $(TARGET_DIR)/$(BRIDGE_TARGET_BINARY)
+	$(STRIP) $(TARGET_DIR)/$(BRIDGE_TARGET_BINARY)
+	#cp -af $(BRIDGE_BUILD_DIR)/brctl/brctld $(TARGET_DIR)/usr/sbin/
+	#$(STRIP) $(TARGET_DIR)/usr/sbin/brctld
 
-bridge: $(TARGET_DIR)/sbin/brctl 
+bridge: $(TARGET_DIR)/$(BRIDGE_TARGET_BINARY)
 
 bridge-source: $(DL_DIR)/$(BRIDGE_SOURCE)
 
 bridge-clean:
-	$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(BRIDGE_BUILD_DIR) uninstall
+	#$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(BRIDGE_BUILD_DIR) uninstall
 	-$(MAKE) -C $(BRIDGE_BUILD_DIR) clean
 
 bridge-dirclean:
