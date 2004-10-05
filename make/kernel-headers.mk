46e93c076b3b49183ac9a69581e434f8fa3231a9
Eric Andersen <andersen@codepoet.org>
2004-10-05 07:16:53 +0000

    Avoid naming conflicts with 'linux' target

--- make/kernel-headers.mk
+++ make/kernel-headers.mk
@@ -9,58 +9,58 @@
 ifneq ($(filter $(TARGETS),kernel-headers),)
 DEFAULT_KERNEL_HEADERS:=$(strip $(DEFAULT_KERNEL_HEADERS))
 
-LINUX_SITE:=127.0.0.1
-LINUX_SOURCE:=unspecified-kernel-headers
+LINUX_HEADERS_SITE:=127.0.0.1
+LINUX_HEADERS_SOURCE:=unspecified-kernel-headers
 
 ifeq ("$(strip $(DEFAULT_KERNEL_HEADERS))","2.4.25")
 VERSION:=2
 PATCHLEVEL:=4
 SUBLEVEL:=25
-LINUX_SITE:=http://www.uclibc.org/downloads/toolchain
-LINUX_SOURCE:=linux-libc-headers-2.4.25.tar.bz2
-LINUX_UNPACK_DIR:=$(TOOL_BUILD_DIR)/linux-libc-headers-2.4.25
+LINUX_HEADERS_SITE:=http://www.uclibc.org/downloads/toolchain
+LINUX_HEADERS_SOURCE:=linux-libc-headers-2.4.25.tar.bz2
+LINUX_HEADERS_UNPACK_DIR:=$(TOOL_BUILD_DIR)/linux-libc-headers-2.4.25
 endif
 
 ifeq ("$(strip $(DEFAULT_KERNEL_HEADERS))","2.4.27")
 VERSION:=2
 PATCHLEVEL:=4
 SUBLEVEL:=25
-LINUX_SITE:=http://www.uclibc.org/downloads/toolchain
-LINUX_SOURCE:=linux-libc-headers-2.4.27.tar.bz2
-LINUX_UNPACK_DIR:=$(TOOL_BUILD_DIR)/linux-libc-headers-2.4.27
+LINUX_HEADERS_SITE:=http://www.uclibc.org/downloads/toolchain
+LINUX_HEADERS_SOURCE:=linux-libc-headers-2.4.27.tar.bz2
+LINUX_HEADERS_UNPACK_DIR:=$(TOOL_BUILD_DIR)/linux-libc-headers-2.4.27
 endif
 
 ifeq ("$(strip $(DEFAULT_KERNEL_HEADERS))","2.6.7")
 VERSION:=2
 PATCHLEVEL:=6
 SUBLEVEL:=7
-LINUX_SITE:=http://ep09.pld-linux.org/~mmazur/linux-libc-headers/
-LINUX_SOURCE:=linux-libc-headers-2.6.7.0.tar.bz2
-LINUX_UNPACK_DIR:=$(TOOL_BUILD_DIR)/linux-libc-headers-2.6.7.0
+LINUX_HEADERS_SITE:=http://ep09.pld-linux.org/~mmazur/linux-libc-headers/
+LINUX_HEADERS_SOURCE:=linux-libc-headers-2.6.7.0.tar.bz2
+LINUX_HEADERS_UNPACK_DIR:=$(TOOL_BUILD_DIR)/linux-libc-headers-2.6.7.0
 endif
 
 ifeq ("$(strip $(DEFAULT_KERNEL_HEADERS))","2.6.8")
 VERSION:=2
 PATCHLEVEL:=6
 SUBLEVEL:=8
-LINUX_SITE:=http://ep09.pld-linux.org/~mmazur/linux-libc-headers/
-LINUX_SOURCE:=linux-libc-headers-2.6.8.0.tar.bz2
-LINUX_UNPACK_DIR:=$(TOOL_BUILD_DIR)/linux-libc-headers-2.6.8.0
+LINUX_HEADERS_SITE:=http://ep09.pld-linux.org/~mmazur/linux-libc-headers/
+LINUX_HEADERS_SOURCE:=linux-libc-headers-2.6.8.0.tar.bz2
+LINUX_HEADERS_UNPACK_DIR:=$(TOOL_BUILD_DIR)/linux-libc-headers-2.6.8.0
 endif
 
 LINUX_VERSION:=$(VERSION).$(PATCHLEVEL).$(SUBLEVEL)
 
 LINUX_HEADERS_DIR:=$(TOOL_BUILD_DIR)/linux
 
-$(DL_DIR)/$(LINUX_SOURCE):
+$(DL_DIR)/$(LINUX_HEADERS_SOURCE):
 	mkdir -p $(DL_DIR)
-	$(WGET) -P $(DL_DIR) $(LINUX_SITE)/$(LINUX_SOURCE)
+	$(WGET) -P $(DL_DIR) $(LINUX_HEADERS_SITE)/$(LINUX_HEADERS_SOURCE)
 
-$(LINUX_HEADERS_DIR)/.unpacked: $(DL_DIR)/$(LINUX_SOURCE)
+$(LINUX_HEADERS_DIR)/.unpacked: $(DL_DIR)/$(LINUX_HEADERS_SOURCE)
 	mkdir -p $(TOOL_BUILD_DIR)
-	bzcat $(DL_DIR)/$(LINUX_SOURCE) | tar -C $(TOOL_BUILD_DIR) -xvf -
-ifneq ($(LINUX_UNPACK_DIR),$(LINUX_HEADERS_DIR))
-	mv $(LINUX_UNPACK_DIR) $(LINUX_HEADERS_DIR)
+	bzcat $(DL_DIR)/$(LINUX_HEADERS_SOURCE) | tar -C $(TOOL_BUILD_DIR) -xvf -
+ifneq ($(LINUX_HEADERS_UNPACK_DIR),$(LINUX_HEADERS_DIR))
+	mv $(LINUX_HEADERS_UNPACK_DIR) $(LINUX_HEADERS_DIR)
 endif
 	touch $(LINUX_HEADERS_DIR)/.unpacked
 
@@ -108,7 +108,7 @@ $(LINUX_KERNEL): $(LINUX_HEADERS_DIR)/.configured
 
 kernel-headers: $(LINUX_HEADERS_DIR)/.configured
 
-kernel-headers-source: $(DL_DIR)/$(LINUX_SOURCE)
+kernel-headers-source: $(DL_DIR)/$(LINUX_HEADERS_SOURCE)
 
 kernel-headers-clean: clean
 	rm -f $(LINUX_KERNEL)
