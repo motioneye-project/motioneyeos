4a2c413273ff04b9ffcbfc575ec7d0e132afe41b
Mike Frysinger <vapier@gentoo.org>
2004-10-08 18:20:15 +0000

    clean up and use a newer version that actually cross compiles :)

--- make/iproute2.mk
+++ make/iproute2.mk
@@ -3,55 +3,45 @@
 # iproute2
 #
 #############################################################
-IPROUTE2_DIR=$(BUILD_DIR)/iproute2
-
-#IPROUTE2_SOURCE_URL=ftp://ftp.inr.ac.ru/ip-routing/
-#IPROUTE2_SOURCE=iproute2-2.4.7-now-ss020116-try.tar.gz
-
-#Use the debian source for now, as the .ru site has availability problems
-IPROUTE2_SOURCE_URL=http://ftp.debian.org/debian/pool/main/i/iproute/
-IPROUTE2_SOURCE=iproute_20010824.orig.tar.gz
-IPROUTE2_PATCH:=iproute_20010824-8.diff.gz
-
+IPROUTE2_VER:=2.6.9
+IPROUTE2_STAMP:=ss040831
+IPROUTE2_SOURCE:=iproute2-$(IPROUTE2_VER)-$(IPROUTE2_STAMP).tar.gz
+IPROUTE2_SITE:=http://developer.osdl.org/dev/iproute2/download/
+IPROUTE2_DIR:=$(BUILD_DIR)/iproute2-$(IPROUTE2_VER)
+IPROUTE2_CAT:=zcat
+IPROUTE2_BINARY:=tc/tc
+IPROUTE2_TARGET_BINARY:=sbin/tc
 
 $(DL_DIR)/$(IPROUTE2_SOURCE):
-	 $(WGET) -P $(DL_DIR) $(IPROUTE2_SOURCE_URL)$(IPROUTE2_SOURCE)
+	$(WGET) -P $(DL_DIR) $(IPROUTE2_SOURCE_URL)$(IPROUTE2_SOURCE)
 
-$(DL_DIR)/$(IPROUTE2_PATCH):
-	$(WGET) -P $(DL_DIR) $(IPROUTE2_SOURCE_URL)/$(IPROUTE2_PATCH)
-
-iproute2-source: $(DL_DIR)/$(IPROUTE2_SOURCE) #$(DL_DIR)/$(IPROUTE2_PATCH)
-
-$(IPROUTE2_DIR)/.unpacked: $(DL_DIR)/$(IPROUTE2_SOURCE) #$(DL_DIR)/$(IPROUTE2_PATCH)
-	rm -rf $(IPROUTE2_DIR).orig $(IPROUTE2_DIR)
-	zcat $(DL_DIR)/$(IPROUTE2_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	#zcat $(DL_DIR)/$(IPROUTE2_PATCH) | patch -p1 -d $(IPROUTE2_DIR)
+$(IPROUTE2_DIR)/.unpacked: $(DL_DIR)/$(IPROUTE2_SOURCE)
+	$(IPROUTE2_CAT) $(DL_DIR)/$(IPROUTE2_SOURCE) | tar -C $(BUILD_DIR) -xvf -
 	touch $(IPROUTE2_DIR)/.unpacked
 
 $(IPROUTE2_DIR)/.configured: $(IPROUTE2_DIR)/.unpacked
-	$(SED) "s,-I/usr/include/db3,," $(IPROUTE2_DIR)/Makefile
-	$(SED) "s,^KERNEL_INCLUDE.*,KERNEL_INCLUDE=$(LINUX_HEADERS_DIR)/include," \
-		$(IPROUTE2_DIR)/Makefile
-	$(SED) "s,^LIBC_INCLUDE.*,LIBC_INCLUDE=$(STAGING_DIR)/include," \
-		$(IPROUTE2_DIR)/Makefile
-	# For now disable compiling of the misc directory because it seems to fail
-	rm -rf $(IPROUTE2_DIR)/misc 
-	$(SED) "s, misc,," $(IPROUTE2_DIR)/Makefile
+	(cd $(IPROUTE2_DIR); \
+		./configure; \
+		$(SED) '/TC_CONFIG_ATM/s:=.*:=n:' Config)
 	touch  $(IPROUTE2_DIR)/.configured
 
-$(IPROUTE2_DIR)/tc/tc: $(IPROUTE2_DIR)/.configured
-	$(MAKE) -C $(IPROUTE2_DIR) KERNEL_INCLUDE=$(LINUX_SOURCE_DIR)/include CC=$(TARGET_CC) AR=$(TARGET_AR)
+$(IPROUTE2_DIR)/$(IPROUTE2_BINARY): $(IPROUTE2_DIR)/.configured
+	$(MAKE) \
+		-C $(IPROUTE2_DIR) \
+		KERNEL_INCLUDE=$(LINUX_SOURCE_DIR)/include \
+		CC=$(TARGET_CC) \
+		AR=$(TARGET_CROSS)ar \
+		NETEM_DIST="" \
+		SUBDIRS="lib tc"
 
-$(TARGET_DIR)/usr/sbin/tc: $(IPROUTE2_DIR)/tc/tc
-	# Copy The tc binary
-	cp -af $(IPROUTE2_DIR)/tc/tc $(TARGET_DIR)/usr/sbin/
+$(TARGET_DIR)/$(IPROUTE2_TARGET_BINARY): $(IPROUTE2_DIR)/$(IPROUTE2_BINARY)
+	install -Dc $(IPROUTE2_DIR)/$(IPROUTE2_BINARY) $(TARGET_DIR)/$(IPROUTE2_TARGET_BINARY)
 
-iproute2: $(TARGET_DIR)/usr/sbin/tc 
+iproute2: $(TARGET_DIR)/$(IPROUTE2_TARGET_BINARY)
 
 iproute2-clean:
-	$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(IPROUTE2_DIR) uninstall
+	rm -f $(TARGET_DIR)/$(IPROUTE2_TARGET_BINARY)
 	-$(MAKE) -C $(IPROUTE2_DIR) clean
 
 iproute2-dirclean:
 	rm -rf $(IPROUTE2_DIR)
-
