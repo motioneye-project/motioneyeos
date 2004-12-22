74afafce2ba41087f1daaf298f025ef98fc97161
Mike Frysinger <vapier@gentoo.org>
2004-12-22 05:21:41 +0000

    dont output which

--- toolchain/binutils/binutils.mk
+++ toolchain/binutils/binutils.mk
@@ -58,15 +58,15 @@ $(STAGING_DIR)/$(REAL_GNU_TARGET_NAME)/bin/ld: $(BINUTILS_DIR1)/binutils/objdump
 	$(MAKE) -C $(BINUTILS_DIR1) install
 
 binutils-dependancies:
-	@if ! which bison ; then \
+	@if ! which bison > /dev/null ; then \
 		echo -e "\n\nYou must install 'bison' on your build machine\n"; \
 		exit 1; \
 	fi;
-	@if ! which flex ; then \
+	@if ! which flex > /dev/null ; then \
 		echo -e "\n\nYou must install 'flex' on your build machine\n"; \
 		exit 1; \
 	fi;
-	@if ! which msgfmt ; then \
+	@if ! which msgfmt > /dev/null ; then \
 		echo -e "\n\nYou must install 'gettext' on your build machine\n"; \
 		exit 1; \
 	fi;
