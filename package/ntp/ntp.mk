06d9abcfd2dd4f9b4a268c39e1058f6aa53ca938
Mike Frysinger <vapier@gentoo.org>
2004-12-24 06:25:25 +0000

    use target cflags and update to 4.2.0

--- package/ntp/ntp.mk
+++ package/ntp/ntp.mk
@@ -3,16 +3,16 @@
 # ntp
 #
 #############################################################
-NTP_SOURCE:=ntp-4.1.2.tar.gz
+NTP_VERSION:=4.2.0
+NTP_SOURCE:=ntp-$(NTP_VERSION).tar.gz
 NTP_SITE:=http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4
-NTP_DIR:=$(BUILD_DIR)/ntp-4.1.2
+NTP_DIR:=$(BUILD_DIR)/ntp-$(NTP_VERSION)
 NTP_CAT:=zcat
 NTP_BINARY:=ntpdate/ntpdate
 NTP_TARGET_BINARY:=usr/bin/ntpdate
 
-
 $(DL_DIR)/$(NTP_SOURCE):
-	 $(WGET) -P $(DL_DIR) $(NTP_SITE)/$(NTP_SOURCE)
+	$(WGET) -P $(DL_DIR) $(NTP_SITE)/$(NTP_SOURCE)
 
 ntp-source: $(DL_DIR)/$(NTP_SOURCE)
 
@@ -25,6 +25,7 @@ $(NTP_DIR)/.unpacked: $(DL_DIR)/$(NTP_SOURCE)
 $(NTP_DIR)/.configured: $(NTP_DIR)/.unpacked
 	(cd $(NTP_DIR); rm -rf config.cache; \
 		$(TARGET_CONFIGURE_OPTS) \
+		CFLAGS="$(TARGET_CFLAGS)" \
 		./configure \
 		--target=$(GNU_TARGET_NAME) \
 		--host=$(GNU_TARGET_NAME) \
@@ -43,18 +44,20 @@ $(NTP_DIR)/.configured: $(NTP_DIR)/.unpacked
 		--with-shared \
 		--program-transform-name=s,,, \
 	);
-	touch  $(NTP_DIR)/.configured
+	touch $(NTP_DIR)/.configured
 
 $(NTP_DIR)/$(NTP_BINARY): $(NTP_DIR)/.configured
 	$(MAKE) CC=$(TARGET_CC) -C $(NTP_DIR)
 
 $(TARGET_DIR)/$(NTP_TARGET_BINARY): $(NTP_DIR)/$(NTP_BINARY)
+	install -m 755 $(NTP_DIR)/ntpd/ntpd $(TARGET_DIR)/usr/sbin/ntpd
 	install -m 755 $(NTP_DIR)/$(NTP_BINARY) $(TARGET_DIR)/$(NTP_TARGET_BINARY)
 
 ntp: uclibc $(TARGET_DIR)/$(NTP_TARGET_BINARY)
 
 ntp-clean:
-	$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(NTP_DIR) uninstall
+	rm -f $(TARGET_DIR)/usr/sbin/ntpd
+	rm -f $(TARGET_DIR)/$(NTP_TARGET_BINARY)
 	-$(MAKE) -C $(NTP_DIR) clean
 
 ntp-dirclean:
