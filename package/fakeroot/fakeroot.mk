4f4becaf9decd99edefebd70011d430d8547985e
Eric Andersen <andersen@codepoet.org>
2005-01-23 11:03:41 +0000

    Fix from pdinoto:  http://bugs.uclibc.org/view.php?id=31
    0000031: [PATCH] fakeroot: Update to version 1.2.2
    
    Use new version. Previous is not longer available (at least in debian archive)

--- package/fakeroot/fakeroot.mk
+++ package/fakeroot/fakeroot.mk
@@ -3,7 +3,7 @@
 # fakeroot
 #
 #############################################################
-FAKEROOT_VERSION:=1.1.5
+FAKEROOT_VERSION:=1.2.2
 FAKEROOT_SOURCE:=fakeroot_$(FAKEROOT_VERSION).tar.gz
 FAKEROOT_SITE:=http://ftp.debian.org/debian/pool/main/f/fakeroot
 FAKEROOT_CAT:=zcat
