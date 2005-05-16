1de07a889630f00c45369ed481480d0bc736543c
Eric Andersen <andersen@codepoet.org>
2005-05-16 17:40:04 +0000

    This patch updates the pppd package to build version 2.4.3. The 2.4.3 version of pppd adds additional features.
    http://bugs.uclibc.org/view.php?id=259

--- package/pppd/pppd.mk
+++ package/pppd/pppd.mk
@@ -3,9 +3,9 @@
 # pppd
 #
 #############################################################
-PPPD_SOURCE:=ppp-2.4.1.tar.gz
+PPPD_SOURCE:=ppp-2.4.3.tar.gz
 PPPD_SITE:=ftp://ftp.samba.org/pub/ppp
-PPPD_DIR:=$(BUILD_DIR)/ppp-2.4.1
+PPPD_DIR:=$(BUILD_DIR)/ppp-2.4.3
 PPPD_CAT:=zcat
 PPPD_BINARY:=pppd/pppd
 PPPD_TARGET_BINARY:=usr/sbin/pppd
@@ -18,8 +18,11 @@ pppd-source: $(DL_DIR)/$(PPPD_SOURCE)
 
 $(PPPD_DIR)/.unpacked: $(DL_DIR)/$(PPPD_SOURCE)
 	$(PPPD_CAT) $(DL_DIR)/$(PPPD_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	$(SED) 's/ -DIPX_CHANGE -DHAVE_MULTILINK -DHAVE_MMAP//' $(PPPD_DIR)/pppd/Makefile.linux
-	$(SED) 's,$(INSTALL) -s,$(INSTALL),' $(PPPD_DIR)/*/Makefile.linux
+	$(SED) 's/ -DIPX_CHANGE -DHAVE_MMAP//' $(PPPD_DIR)/pppd/Makefile.linux
+	$(SED) 's/HAVE_MULTILINK=y/#HAVE_MULTILINK=y/' $(PPPD_DIR)/pppd/Makefile.linux
+	$(SED) 's/FILTER=y/#FILTER=y/' $(PPPD_DIR)/pppd/Makefile.linux
+	$(SED) 's,(INSTALL) -s,(INSTALL),' $(PPPD_DIR)/*/Makefile.linux
+	$(SED) 's,(INSTALL) -s,(INSTALL),' $(PPPD_DIR)/pppd/plugins/*/Makefile.linux
 	$(SED) 's/ -o root//' $(PPPD_DIR)/*/Makefile.linux
 	$(SED) 's/ -g daemon//' $(PPPD_DIR)/*/Makefile.linux
 	touch $(PPPD_DIR)/.unpacked
@@ -49,8 +52,8 @@ $(PPPD_DIR)/$(PPPD_BINARY): $(PPPD_DIR)/.configured
 	$(MAKE) CC=$(TARGET_CC) -C $(PPPD_DIR)
 
 $(TARGET_DIR)/$(PPPD_TARGET_BINARY): $(PPPD_DIR)/$(PPPD_BINARY)
-	$(MAKE1) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(PPPD_DIR) install
-	rm -rf $(TARGET_DIR)/share/locale $(TARGET_DIR)/usr/info \
+	$(MAKE1) DESTDIR=$(TARGET_DIR)/usr CC=$(TARGET_CC) -C $(PPPD_DIR) install
+	rm -rf $(TARGET_DIR)/usr/share/locale $(TARGET_DIR)/usr/info \
 		$(TARGET_DIR)/usr/man $(TARGET_DIR)/usr/share/doc
 
 pppd: uclibc $(TARGET_DIR)/$(PPPD_TARGET_BINARY)
@@ -59,7 +62,7 @@ pppd-clean:
 	rm -f  $(TARGET_DIR)/usr/sbin/pppd
 	rm -f  $(TARGET_DIR)/usr/sbin/chat
 	rm -rf $(TARGET_DIR)/etc/ppp
-	$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(PPPD_DIR) uninstall
+	$(MAKE) DESTDIR=$(TARGET_DIR)/usr CC=$(TARGET_CC) -C $(PPPD_DIR) uninstall
 	-$(MAKE) -C $(PPPD_DIR) clean
 
 pppd-dirclean:
