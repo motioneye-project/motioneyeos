c4f10a81ae9e3654d338c8a13d097866c27a7891
"Steven J. Hill" <sjhill@realitydiluted.com>
2005-05-31 03:17:33 +0000

    Applied patch to add TCL version 8.4.9 for buildroot submitted by Ivan Daniluk on May 30 01:28:31 MDT 2005.

---
+++ package/tcl/tcl.mk
@@ -0,0 +1,70 @@
+#############################################################
+#
+# TCL8.4
+#
+#############################################################
+TCL_VERSION:=8.4.9
+TCL_SOURCE:=tcl$(TCL_VERSION)-src.tar.gz
+TCL_SITE:=http://heanet.dl.sourceforge.net/sourceforge/tcl
+TCL_DIR:=$(BUILD_DIR)/tcl$(TCL_VERSION)
+
+$(DL_DIR)/$(TCL_SOURCE):
+	$(WGET) -P $(DL_DIR) $(TCL_SITE)/$(TCL_SOURCE)
+
+$(TCL_DIR)/.source: $(DL_DIR)/$(TCL_SOURCE)
+	zcat $(DL_DIR)/$(TCL_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	toolchain/patch-kernel.sh $(TCL_DIR) package/tcl/ tcl*.patch
+	touch $(TCL_DIR)/.source
+
+$(TCL_DIR)/.configured: $(TCL_DIR)/.source
+	(cd $(TCL_DIR)/unix; \
+		$(TARGET_CONFIGURE_OPTS) \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		./configure \
+		--target=$(GNU_TARGET_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--build=$(GNU_HOST_NAME) \
+		--prefix=/usr \
+		--sysconfdir=/etc \
+		--enable-shared \
+		--disable-symbols \
+		--disable-langinfo \
+		--disable-framework \
+	);
+	touch $(TCL_DIR)/.configured;
+
+$(TCL_DIR)/unix/libtcl8.4.so: $(TCL_DIR)/.configured
+	$(MAKE) CC=$(TARGET_CC) -C $(TCL_DIR)/unix
+
+$(TARGET_DIR)/usr/lib/libtcl8.4.so: $(TCL_DIR)/unix/libtcl8.4.so
+	$(MAKE) INSTALL_ROOT=$(TARGET_DIR) -C $(TCL_DIR)/unix install
+	-$(STRIP) --strip-unneeded $(TARGET_DIR)/usr/lib/libtcl8.4.so
+	rm -Rf $(TARGET_DIR)/usr/man
+	rm -Rf $(TARGET_DIR)/usr/include
+	-if [ "$(strip $(BR2_PACKAGE_TCL_DEL_ENCODINGS))" == "y" ]; then \
+	rm -Rf $(TARGET_DIR)/usr/lib/tcl8.4/encoding/*; \
+	fi
+	-if [ "$(strip $(BR2_PACKAGE_TCL_SHLIB_ONLY))" == "y" ]; then \
+	rm -f $(TARGET_DIR)/usr/bin/tclsh8.4; \
+	fi
+
+tcl: uclibc $(TARGET_DIR)/usr/lib/libtcl8.4.so
+
+tcl-source: $(DL_DIR)/$(TCL_SOURCE)
+
+tcl-clean:
+	$(MAKE) prefix=$(TARGET_DIR)/usr -C $(TCL_DIR)/unix uninstall
+	-$(MAKE) -C $(TCL_DIR)/unix clean
+
+less-dirclean:
+	rm -rf $(LESS_DIR)
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(strip $(BR2_PACKAGE_TCL)),y)
+TARGETS+=tcl
+endif
+
