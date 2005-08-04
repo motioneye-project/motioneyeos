1e218e33ebed5c3c434fcb3377d225d253d97128
Eric Andersen <andersen@codepoet.org>
2005-08-04 05:14:23 +0000

    Fix it up so ltp builds and installs itself properly so it is 100% ready to run on the target device.

--- package/ltp-testsuite/ltp-testsuite.mk
+++ package/ltp-testsuite/ltp-testsuite.mk
@@ -3,10 +3,11 @@
 # ltp-testsuite
 #
 #############################################################
-LTP_TESTSUITE_SOURCE:=ltp-full-20040506.tgz
+LTP_TESTSUITE_SOURCE:=ltp-full-20050707.tgz
 LTP_TESTSUITE_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/ltp
 LTP_TESTSUITE_CAT:=zcat
-LTP_TESTSUITE_DIR:=$(BUILD_DIR)/ltp-full-20040506
+LTP_TESTSUITE_ROOT:=$(TARGET_DIR)/root
+LTP_TESTSUITE_DIR:=$(LTP_TESTSUITE_ROOT)/ltp-full-20050707
 
 
 $(DL_DIR)/$(LTP_TESTSUITE_SOURCE):
@@ -15,19 +16,23 @@ $(DL_DIR)/$(LTP_TESTSUITE_SOURCE):
 ltp-testsuite-source: $(DL_DIR)/$(LTP_TESTSUITE_SOURCE)
 
 $(LTP_TESTSUITE_DIR)/.unpacked: $(DL_DIR)/$(LTP_TESTSUITE_SOURCE)
-	$(LTP_TESTSUITE_CAT) $(DL_DIR)/$(LTP_TESTSUITE_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	$(LTP_TESTSUITE_CAT) $(DL_DIR)/$(LTP_TESTSUITE_SOURCE) | tar -C $(LTP_TESTSUITE_ROOT) $(TAR_OPTIONS) -
 	toolchain/patch-kernel.sh $(LTP_TESTSUITE_DIR) package/ltp-testsuite/ ltp-testsuite\*.patch
 	touch $(LTP_TESTSUITE_DIR)/.unpacked
 
-$(LTP_TESTSUITE_DIR)/ltp-testsuite: $(LTP_TESTSUITE_DIR)/.unpacked
+$(LTP_TESTSUITE_DIR)/testcases/kernel/syscalls/write/write01: $(LTP_TESTSUITE_DIR)/.unpacked
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) CROSS_COMPILER=$(TARGET_CROSS) \
-		-C $(LTP_TESTSUITE_DIR)
+		-C $(LTP_TESTSUITE_DIR) all
+	touch -c $(LTP_TESTSUITE_DIR)/testcases/kernel/syscalls/write/write01
 
-$(TARGET_DIR)/usr/bin/ltp-testsuite: $(LTP_TESTSUITE_DIR)/ltp-testsuite
+$(LTP_TESTSUITE_DIR)/testcases/bin/1K_file: $(LTP_TESTSUITE_DIR)/testcases/kernel/syscalls/write/write01
+	# Use fakeroot to pretend to do 'make install' as root
+	$(STAGING_DIR)/usr/bin/fakeroot \
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) CROSS_COMPILER=$(TARGET_CROSS) \
 		-C $(LTP_TESTSUITE_DIR) install
+	touch -c $(LTP_TESTSUITE_DIR)/testcases/bin/1K_file
 
-ltp-testsuite: uclibc $(TARGET_DIR)/usr/bin/ltp-testsuite
+ltp-testsuite: uclibc host-fakeroot $(LTP_TESTSUITE_DIR)/testcases/bin/1K_file
 
 ltp-testsuite-clean:
 	$(MAKE) -C $(LTP_TESTSUITE_DIR) clean
