e40d0e28a3197abcd399e80c62380aa3b177cba5
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2005-12-21 13:36:47 +0000

    - use HOSTCC instead of hardcoded compiler which may or may not be available.

--- target/makedevs/makedevs.mk
+++ target/makedevs/makedevs.mk
@@ -13,7 +13,7 @@ $(MAKEDEVS_DIR)/makedevs.c: target/makedevs/makedevs.c
 	cp target/makedevs/makedevs.c $(MAKEDEVS_DIR)
 
 $(MAKEDEVS_DIR)/makedevs: $(MAKEDEVS_DIR)/makedevs.c
-	gcc -Wall -Werror -O2 $(MAKEDEVS_DIR)/makedevs.c -o $(MAKEDEVS_DIR)/makedevs
+	$(HOSTCC) -Wall -Werror -O2 $(MAKEDEVS_DIR)/makedevs.c -o $(MAKEDEVS_DIR)/makedevs
 	touch -c $(MAKEDEVS_DIR)/makedevs
 
 $(STAGING_DIR)/bin/makedevs: $(MAKEDEVS_DIR)/makedevs
