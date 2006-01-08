0e1be2d0f3e389abbd0d5aa0fedc3202268f2968
Mike Frysinger <vapier@gentoo.org>
2006-01-08 09:55:59 +0000

    force the copy in case the target files are read-only as pointed out by petev in Bug 628

--- package/gnuconfig/gnuconfig.mk
+++ package/gnuconfig/gnuconfig.mk
@@ -1,3 +1,3 @@
 # See README.buildroot
 
-CONFIG_UPDATE = cp package/gnuconfig/config.sub package/gnuconfig/config.guess 
+CONFIG_UPDATE = cp -f package/gnuconfig/config.sub package/gnuconfig/config.guess 
