f3803a9088e3c60b3fd5f8458b3a6de279593d51
David Anders <dave123@abcsinc.com>
2006-01-24 15:34:05 +0000

    disable wtmp and lastlog in config as well as the dbclient symlink generation per case 0000521

--- package/dropbear/dropbear.mk
+++ package/dropbear/dropbear.mk
@@ -42,7 +42,8 @@ $(DROPBEAR_DIR)/.configured: $(DROPBEAR_DIR)/.unpacked
 		--mandir=/usr/man \
 		--infodir=/usr/info \
 		$(DISABLE_NLS) \
-		--with-shared \
+		--disable-wtmp \
+		--disable-lastlog \
 	);
 ifeq ($(strip $(BR2_PACKAGE_DROPBEAR_URANDOM)),y)
 	$(SED) 's,^#define DROPBEAR_RANDOM_DEV.*,#define DROPBEAR_RANDOM_DEV \"/dev/urandom\",' \
@@ -70,6 +71,7 @@ $(TARGET_DIR)/$(DROPBEAR_TARGET_BINARY): $(DROPBEAR_DIR)/$(DROPBEAR_BINARY)
 	ln -snf ../sbin/dropbear $(TARGET_DIR)/usr/bin/dbclient
 	ln -snf ../sbin/dropbear $(TARGET_DIR)/usr/bin/dropbearkey
 	ln -snf ../sbin/dropbear $(TARGET_DIR)/usr/bin/dropbearconvert
+	ln -snf ../sbin/dropbear $(TARGET_DIR)/usr/bin/dbclient
 	cp $(DROPBEAR_DIR)/S50dropbear $(TARGET_DIR)/etc/init.d/
 	chmod a+x $(TARGET_DIR)/etc/init.d/S50dropbear
 
