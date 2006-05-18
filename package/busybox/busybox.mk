d503cac9fa8abfdade5e7e880dcb4b5f58b0eb57
Peter Korsgaard <jacmet@sunsite.dk>
2006-05-18 17:36:17 +0000

    Bump to upstream v1.1.3

--- package/busybox/busybox.mk
+++ package/busybox/busybox.mk
@@ -10,7 +10,7 @@ BUSYBOX_DIR:=$(BUILD_DIR)/busybox
 BUSYBOX_SOURCE:=busybox-snapshot.tar.bz2
 BUSYBOX_SITE:=http://www.busybox.net/downloads/snapshots
 else
-BUSYBOX_VER:=1.1.2
+BUSYBOX_VER:=1.1.3
 BUSYBOX_DIR:=$(BUILD_DIR)/busybox-$(BUSYBOX_VER)
 BUSYBOX_SOURCE:=busybox-$(BUSYBOX_VER).tar.bz2
 BUSYBOX_SITE:=http://www.busybox.net/downloads
