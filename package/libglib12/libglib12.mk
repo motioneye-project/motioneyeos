7909d9d1d96c46f3b2e5c65fd40f79a88a6bd809
Eric Andersen <andersen@codepoet.org>
2006-07-10 22:06:11 +0000

    fix url, fixup compile and install

--- package/libglib12/libglib12.mk
+++ package/libglib12/libglib12.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 LIBGLIB12_SOURCE:=glib-1.2.10.tar.gz
-LIBGLIB12_SITE:=ftp://ftp.gtk.org/pub/gtk/v1.2
+LIBGLIB12_SITE:=http://ftp.gnome.org/pub/gnome/sources/glib/1.2
 LIBGLIB12_CAT:=zcat
 LIBGLIB12_DIR:=$(BUILD_DIR)/glib-1.2.10
 LIBGLIB12_BINARY:=libglib.a
@@ -27,8 +27,8 @@ $(LIBGLIB12_DIR)/.configured: $(LIBGLIB12_DIR)/.unpacked
 		./configure \
 		--host=$(REAL_GNU_TARGET_NAME) \
 		--build=$(GNU_HOST_NAME) \
-		--prefix=/usr \
-		--exec-prefix=/usr \
+		--prefix=$(STAGING_DIR) \
+		--exec_prefix=$(STAGING_DIR) \
 		--bindir=/usr/bin \
 		--sbindir=/usr/sbin \
 		--libexecdir=/usr/lib \
@@ -37,8 +37,8 @@ $(LIBGLIB12_DIR)/.configured: $(LIBGLIB12_DIR)/.unpacked
 		--localstatedir=/var \
 		--mandir=/usr/man \
 		--infodir=/usr/info \
-		$(DISABLE_NLS) \
 		--enable-shared \
+		$(DISABLE_NLS) \
 	);
 	touch $(LIBGLIB12_DIR)/.configured
 
@@ -62,13 +62,17 @@ $(STAGING_DIR)/lib/$(LIBGLIB12_BINARY): $(LIBGLIB12_DIR)/.libs/$(LIBGLIB12_BINAR
 	    mandir=$(STAGING_DIR)/man \
 	    -C $(LIBGLIB12_DIR) install;
 
-$(TARGET_DIR)/lib/$(LIBGLIB12_BINARY): $(STAGING_DIR)/lib/$(LIBGLIB12_BINARY)
-	cp -a $(STAGING_DIR)/lib/$(LIBGLIB12_BINARY) $(TARGET_DIR)/lib/
+$(TARGET_DIR)/lib/libglib-1.2.so.0.0.10: $(STAGING_DIR)/lib/$(LIBGLIB12_BINARY)
 	cp -a $(STAGING_DIR)/lib/libglib.so $(TARGET_DIR)/lib/
 	cp -a $(STAGING_DIR)/lib/libglib-1.2.so.0 $(TARGET_DIR)/lib/
-	$(STRIP) --strip-unneeded $(TARGET_DIR)/lib/$(LIBGLIB12_BINARY)
+	cp -a $(STAGING_DIR)/lib/libglib-1.2.so.0.0.10 $(TARGET_DIR)/lib/
+	cp -a $(STAGING_DIR)/lib/libgmodule.so $(TARGET_DIR)/lib/
+	cp -a $(STAGING_DIR)/lib/libgmodule-1.2.so.0 $(TARGET_DIR)/lib/
+	cp -a $(STAGING_DIR)/lib/libgmodule-1.2.so.0.0.10 $(TARGET_DIR)/lib/
+	$(STRIP) --strip-unneeded $(TARGET_DIR)/lib/libglib-1.2.so.0.0.10
+	$(STRIP) --strip-unneeded $(TARGET_DIR)/lib/libgmodule-1.2.so.0.0.10
 
-libglib12: uclibc $(TARGET_DIR)/lib/$(LIBGLIB12_BINARY)
+libglib12: uclibc $(TARGET_DIR)/lib/libglib-1.2.so.0.0.10
 
 libglib12-clean:
 	rm -f $(TARGET_DIR)/lib/$(LIBGLIB12_BINARY)
