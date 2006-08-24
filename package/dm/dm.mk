5a2008d9fea10beef9eb908315d078dd498046a8
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2006-08-24 21:46:57 +0000

    - update to 1.02.09

--- package/dm/dm.mk
+++ package/dm/dm.mk
@@ -1,6 +1,6 @@
 #############################################################
 #
-# dm
+# device-mapper
 #
 #############################################################
 # Copyright (C) 2005 by Richard Downer <rdowner@gmail.com>
@@ -24,7 +24,7 @@
 # USA
 
 DM_BASEVER=1.02
-DM_PATCH=08
+DM_PATCH=09
 DM_VERSION=$(DM_BASEVER).$(DM_PATCH)
 DM_SOURCE:=device-mapper.$(DM_VERSION).tgz
 DM_SITE:=ftp://sources.redhat.com/pub/dm
