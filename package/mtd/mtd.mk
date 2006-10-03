2ce4d98fadb1f104fea85c803df30499c385559f
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2006-10-03 14:11:12 +0000

    - use the HOSTCC for building host-tools.

--- package/mtd/mtd.mk
+++ package/mtd/mtd.mk
@@ -57,7 +57,7 @@ $(MTD_HOST_DIR)/.unpacked: $(DL_DIR)/$(MTD_SOURCE)
 endif
 
 $(MTD_HOST_DIR)/util/mkfs.jffs2: $(MTD_HOST_DIR)/.unpacked
-	CFLAGS=-I$(LINUX_HEADERS_DIR)/include \
+	CC="$(HOSTCC)" CROSS= CFLAGS=-I$(LINUX_HEADERS_DIR)/include \
 		$(MAKE) LINUXDIR=$(LINUX_DIR) -C $(MTD_HOST_DIR)/util mkfs.jffs2
 
 mtd-host: $(MKFS_JFFS2)
