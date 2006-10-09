8531fbd55c091b876070ea38eec99b7a82ff3f0d
Eric Andersen <andersen@codepoet.org>
2006-10-09 21:08:56 +0000

    use http for kernel.org

--- package/modutils/modutils.mk
+++ package/modutils/modutils.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 MODUTILS_SOURCE=modutils-2.4.27.tar.bz2
-MODUTILS_SITE=ftp://ftp.kernel.org/pub/linux/utils/kernel/modutils/v2.4/
+MODUTILS_SITE=http://ftp.kernel.org/pub/linux/utils/kernel/modutils/v2.4/
 MODUTILS_DIR1=$(BUILD_DIR)/modutils-2.4.27
 MODUTILS_DIR2=$(TOOL_BUILD_DIR)/modutils-2.4.27
 MODUTILS_BINARY=depmod/depmod
