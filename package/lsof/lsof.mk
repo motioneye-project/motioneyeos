aca398bb7fa956dc44e1442969c4a2a425780997
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2006-11-29 12:59:56 +0000

    - compile against the cross toolchain, not the host

--- package/lsof/lsof.mk
+++ package/lsof/lsof.mk
@@ -10,6 +10,14 @@ LSOF_DIR:=$(BUILD_DIR)/lsof_4.77
 LSOF_BINARY:=lsof
 LSOF_TARGET_BINARY:=bin/lsof
 
+BR2_LSOF_CFLAGS:=
+ifeq ($(BR2_LARGEFILE),)
+BR2_LSOF_CFLAGS+=-U_FILE_OFFSET_BITS
+endif
+ifeq ($(UCLIBC_HAS_IPV6),)
+BR2_LSOF_CFLAGS+=-UHASIPv6
+endif
+
 $(DL_DIR)/$(LSOF_SOURCE):
 	 $(WGET) -P $(DL_DIR) $(LSOF_SITE)/$(LSOF_SOURCE)
 
@@ -24,14 +32,15 @@ $(LSOF_DIR)/.unpacked: $(DL_DIR)/$(LSOF_SOURCE)
 	touch $(LSOF_DIR)/.unpacked
 
 $(LSOF_DIR)/.configured: $(LSOF_DIR)/.unpacked
-	(cd $(LSOF_DIR)/lsof_4.77_src; echo n | CC="$(CC)" CFLAGS="$(CFLAGS)" ./Configure linux)
+	(cd $(LSOF_DIR)/lsof_4.77_src; echo n | $(TARGET_CONFIGURE_OPTS) DEBUG="$(TARGET_CFLAGS) $(BR2_LSOF_CFLAGS)" ./Configure linux)
 	touch $(LSOF_DIR)/.configured
 
 $(LSOF_DIR)/lsof_4.77_src/$(LSOF_BINARY): $(LSOF_DIR)/.configured
-	$(MAKE) CC=$(TARGET_CC) -C $(LSOF_DIR)/lsof_4.77_src
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) DEBUG="$(TARGET_CFLAGS) $(BR2_LSOF_CFLAGS)" -C $(LSOF_DIR)/lsof_4.77_src
 
 $(TARGET_DIR)/$(LSOF_TARGET_BINARY): $(LSOF_DIR)/lsof_4.77_src/$(LSOF_BINARY)
-	cp -a $(LSOF_DIR)/lsof_4.77_src/$(LSOF_BINARY) $(TARGET_DIR)/$(LSOF_TARGET_BINARY)
+	cp $(LSOF_DIR)/lsof_4.77_src/$(LSOF_BINARY) $@
+	$(STRIP) $@
 
 lsof: uclibc $(TARGET_DIR)/$(LSOF_TARGET_BINARY)
 
