ad6270091e4942e24cd111317815126fe9df86d7
Eric Andersen <andersen@codepoet.org>
2006-12-13 13:17:18 +0000

    bump version as old version has gone missing

--- package/memtester/memtester.mk
+++ package/memtester/memtester.mk
@@ -3,9 +3,9 @@
 # memtester
 #
 #############################################################
-MEMTESTER_VER:=4.0.5
+MEMTESTER_VER:=4.0.6
 MEMTESTER_SOURCE:=memtester-$(MEMTESTER_VER).tar.gz
-MEMTESTER_SITE:=http://pyropus.ca/software/memtester
+MEMTESTER_SITE:=http://pyropus.ca/software/memtester/old-versions/
 MEMTESTER_DIR:=$(BUILD_DIR)/memtester-$(MEMTESTER_VER)
 MEMTESTER_BINARY:=memtester
 MEMTESTER_TARGET_BINARY:=usr/bin/memtester
