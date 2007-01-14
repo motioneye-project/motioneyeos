ee8f2133a57111dd36a6bd667a945dc04a4e419d
Eric Andersen <andersen@codepoet.org>
2007-01-14 03:39:53 +0000

    add fontconfig package

---
+++ package/fontconfig/fontconfig.mk
@@ -0,0 +1,75 @@
+#############################################################
+#
+# fontconfig
+#
+#############################################################
+FONTCONFIG_VERSION:=2.4.2
+FONTCONFIG_SOURCE:=fontconfig-$(FONTCONFIG_VERSION).tar.gz
+FONTCONFIG_SITE:=http://fontconfig.org/release
+FONTCONFIG_CAT:=$(ZCAT)
+FONTCONFIG_DIR:=$(BUILD_DIR)/fontconfig-$(FONTCONFIG_VERSION)
+
+$(DL_DIR)/$(FONTCONFIG_SOURCE):
+	$(WGET) -P $(DL_DIR) $(FONTCONFIG_SITE)/$(FONTCONFIG_SOURCE)
+
+fontconfig-source: $(DL_DIR)/$(FONTCONFIG_SOURCE)
+
+$(FONTCONFIG_DIR)/.unpacked: $(DL_DIR)/$(FONTCONFIG_SOURCE)
+	$(FONTCONFIG_CAT) $(DL_DIR)/$(FONTCONFIG_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	touch $(FONTCONFIG_DIR)/.unpacked
+
+$(FONTCONFIG_DIR)/.configured: $(FONTCONFIG_DIR)/.unpacked
+	(cd $(FONTCONFIG_DIR); \
+	$(TARGET_CONFIGURE_OPTS) \
+	CFLAGS="$(TARGET_CFLAGS) " \
+	CCexe="$(HOSTCC)" \
+	./configure \
+		--target=$(GNU_TARGET_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--build=$(GNU_HOST_NAME) \
+		--with-arch=$(GNU_TARGET_NAME) \
+		--prefix=/usr \
+		--exec-prefix=/usr \
+		--bindir=/usr/bin \
+		--sbindir=/usr/sbin \
+		--libdir=/lib \
+		--libexecdir=/usr/lib \
+		--sysconfdir=/etc \
+		--datadir=/usr/share \
+		--localstatedir=/var \
+		--includedir=/include \
+		--mandir=/usr/man \
+		--infodir=/usr/info \
+	);
+	touch $(FONTCONFIG_DIR)/.configured
+
+$(FONTCONFIG_DIR)/.compiled: $(FONTCONFIG_DIR)/.configured
+	$(MAKE) CCexe="$(HOSTCC)" -C $(FONTCONFIG_DIR)
+	touch $(FONTCONFIG_DIR)/.compiled
+
+$(STAGING_DIR)/lib/libfontconfig.so: $(FONTCONFIG_DIR)/.compiled
+	$(MAKE) DESTDIR=$(STAGING_DIR) -C $(FONTCONFIG_DIR) install
+	$(SED) "s,^libdir=.*,libdir=\'$(STAGING_DIR)/lib\',g" $(STAGING_DIR)/lib/libfontconfig.la
+	touch -c $(STAGING_DIR)/lib/libfontconfig.so
+
+$(TARGET_DIR)/lib/libfontconfig.so: $(STAGING_DIR)/lib/libfontconfig.so
+	cp -dpf $(STAGING_DIR)/lib/libfontconfig.so* $(TARGET_DIR)/lib/
+	-$(STRIP) --strip-unneeded $(TARGET_DIR)/lib/libfontconfig.so
+
+fontconfig: uclibc freetype $(TARGET_DIR)/lib/libfontconfig.so
+
+fontconfig-clean:
+	$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(FONTCONFIG_DIR) uninstall
+	-$(MAKE) -C $(FONTCONFIG_DIR) clean
+
+fontconfig-dirclean:
+	rm -rf $(FONTCONFIG_DIR)
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(strip $(BR2_PACKAGE_FONTCONFIG)),y)
+TARGETS+=fontconfig
+endif
