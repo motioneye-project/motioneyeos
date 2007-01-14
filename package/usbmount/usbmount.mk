ea18cf815ce7a5012129d719b126bcea9cf37ddf
"Steven J. Hill" <sjhill@realitydiluted.com>
2007-01-14 03:31:43 +0000

    Add new package.

---
+++ package/usbmount/usbmount.mk
@@ -0,0 +1,43 @@
+#############################################################
+#
+# usbmount
+#
+#############################################################
+USBMOUNT_SOURCE:=usbmount_0.0.14.tar.gz
+USBMOUNT_SITE:=http://usbmount.alioth.debian.org/package/
+USBMOUNT_CAT:=$(ZCAT)
+USBMOUNT_DIR:=$(BUILD_DIR)/usbmount-0.0.14
+USBMOUNT_BINARY:=usbmount
+USBMOUNT_TARGET_BINARY:=sbin/usbmount
+
+$(DL_DIR)/$(USBMOUNT_SOURCE):
+	 $(WGET) -P $(DL_DIR) $(USBMOUNT_SITE)/$(USBMOUNT_SOURCE)
+
+$(USBMOUNT_DIR)/.unpacked: $(DL_DIR)/$(USBMOUNT_SOURCE)
+	$(USBMOUNT_CAT) $(DL_DIR)/$(USBMOUNT_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	toolchain/patch-kernel.sh $(USBMOUNT_DIR) package/usbmount \*.patch
+	touch $(USBMOUNT_DIR)/.unpacked
+
+$(TARGET_DIR)/$(USBMOUNT_TARGET_BINARY): $(USBMOUNT_DIR)/.unpacked
+	$(INSTALL) -m 0755 -D $(USBMOUNT_DIR)/usbmount $(TARGET_DIR)/$(USBMOUNT_TARGET_BINARY)
+	@if [ ! -f $(TARGET_DIR)/etc/usbmount/usbmount.conf ] ; then \
+                $(INSTALL) -m 0644 -D $(USBMOUNT_DIR)/usbmount.conf $(TARGET_DIR)/etc/usbmount/usbmount.conf; \
+        fi;
+
+usbmount: uclibc $(TARGET_DIR)/$(USBMOUNT_TARGET_BINARY)
+
+usbmount-clean:
+	rm -f $(TARGET_DIR)/$(USBMOUNT_TARGET_BINARY)
+	rm -rf $(TARGET_DIR)/etc/usbmount
+
+usbmount-dirclean:
+	rm -rf $(USBMOUNT_DIR)
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(strip $(BR2_PACKAGE_USBMOUNT)),y)
+TARGETS+=usbmount
+endif
