2c7da4d72699ac3d407dadc7432967c8ff4b6750
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2007-01-21 14:36:25 +0000

    - use makedev to get device number, else it gives wrong major/minor numbers to devices files on systems which use big device numbers.   Closes #919

--- target/makedevs/makedevs.c
+++ target/makedevs/makedevs.c
@@ -490,7 +490,7 @@ int main(int argc, char **argv)
 				full_name_inc = xmalloc(strlen(full_name) + 4);
 				for (i = start; i < count; i++) {
 					sprintf(full_name_inc, "%s%d", full_name, i);
-					rdev = (major << 8) + minor + (i * increment - start);
+					rdev = makedev(major, minor + (i * increment - start));
 					if (mknod(full_name_inc, mode, rdev) == -1) {
 						bb_perror_msg("line %d: Couldnt create node %s", linenum, full_name_inc);
 						ret = EXIT_FAILURE;
@@ -506,7 +506,7 @@ int main(int argc, char **argv)
 				}
 				free(full_name_inc);
 			} else {
-				rdev = (major << 8) + minor;
+				rdev = makedev(major, minor);
 				if (mknod(full_name, mode, rdev) == -1) {
 					bb_perror_msg("line %d: Couldnt create node %s", linenum, full_name);
 					ret = EXIT_FAILURE;
