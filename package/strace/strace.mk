0ba1bd72f32e9f335246580e3d47e5c10fd037bd
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2007-01-29 17:02:31 +0000

    - bump version and make it behave of LFS is off

--- package/strace/strace.mk
+++ package/strace/strace.mk
@@ -3,12 +3,16 @@
 # strace
 #
 #############################################################
-STRACE_VER:=4.5.14
+STRACE_VER:=4.5.15
 STRACE_SOURCE:=strace-$(STRACE_VER).tar.bz2
 STRACE_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/strace
 STRACE_CAT:=$(BZCAT)
 STRACE_DIR:=$(BUILD_DIR)/strace-$(STRACE_VER)
 
+BR2_STRACE_CFLAGS:=
+ifeq ($(BR2_LARGEFILE),)
+BR2_STRACE_CFLAGS+=-U_LARGEFILE64_SOURCE -U__USE_LARGEFILE64 -U__USE_FILE_OFFSET64
+endif
 
 $(DL_DIR)/$(STRACE_SOURCE):
 	 $(WGET) -P $(DL_DIR) $(STRACE_SITE)/$(STRACE_SOURCE)
@@ -18,12 +22,14 @@ strace-source: $(DL_DIR)/$(STRACE_SOURCE)
 $(STRACE_DIR)/.unpacked: $(DL_DIR)/$(STRACE_SOURCE)
 	$(STRACE_CAT) $(DL_DIR)/$(STRACE_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	toolchain/patch-kernel.sh $(STRACE_DIR) package/strace strace\*.patch
+	$(CONFIG_UPDATE) $(STRACE_DIR)
 	touch $(STRACE_DIR)/.unpacked
 
 $(STRACE_DIR)/.configured: $(STRACE_DIR)/.unpacked
 	(cd $(STRACE_DIR); rm -rf config.cache; \
+		$(if $(BR_LARGEFILE),ac_cv_type_stat64=yes,ac_cv_type_stat64=no) \
 		$(TARGET_CONFIGURE_OPTS) \
-		CFLAGS="$(TARGET_CFLAGS)" \
+		CFLAGS="$(TARGET_CFLAGS) $(BR2_STRACE_CFLAGS)" \
 		aaa_ac_cv_header_linux_if_packet_h=yes \
 		./configure \
 		--target=$(REAL_GNU_TARGET_NAME) \
