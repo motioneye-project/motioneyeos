f3c50a6cf2a322f94e51653784c2664348af2176
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2007-02-15 14:07:08 +0000

    - warn about missing makeinfo on the host.

--- toolchain/dependencies/dependencies.sh
+++ toolchain/dependencies/dependencies.sh
@@ -248,7 +248,12 @@ fi;
 echo "gettext installed:				Ok"
 
 
-
+if ! which makeinfo > /dev/null ; then \
+	echo "makeinfo installed:		    FALSE"
+	/bin/echo -e "\n\nMost likely some packages will fail to build their documentation"
+	/bin/echo -e "Either install 'makeinfo' on your host or fix the respective packages"
+fi
+echo "makeinfo installed:				Ok"
 
 
 #############################################################
