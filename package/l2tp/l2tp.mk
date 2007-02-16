bdf816433ca2a178354ebcda650b807fd3e64e7b
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2007-02-16 18:11:48 +0000

    - add runlevel script

--- package/l2tp/l2tp.mk
+++ package/l2tp/l2tp.mk
@@ -39,6 +39,7 @@ $(L2TP_DIR)/$(L2TP_BINARY): $(L2TP_DIR)/.unpacked
 
 $(TARGET_DIR)/$(L2TP_TARGET_BINARY): $(L2TP_DIR)/$(L2TP_BINARY)
 	cp -dpf $(L2TP_DIR)/$(L2TP_BINARY) $@
+	cp -dpf package/l2tp/l2tpd $(TARGET_DIR)/etc/init.d/
 	$(STRIP) $@
 
 l2tp: uclibc $(TARGET_DIR)/$(L2TP_TARGET_BINARY)
