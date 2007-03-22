8b855510d559d6889c8c58887eb6a0093dcd3861
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2007-03-22 16:14:42 +0000

    - fix previous incomplete commit of mine

--- package/mpfr/mpfr.mk
+++ package/mpfr/mpfr.mk
@@ -38,12 +38,16 @@ $(DL_DIR)/$(MPFR_PATCH_FILE):
 libmpfr-source: $(DL_DIR)/$(MPFR_SOURCE) $(DL_DIR)/$(MPFR_PATCH_FILE)
 endif
 
+ifeq ($(MPFR_PATCH),)
 $(MPFR_DIR)/.unpacked: $(DL_DIR)/$(MPFR_SOURCE)
+else
+$(MPFR_DIR)/.unpacked: $(DL_DIR)/$(MPFR_SOURCE) $(DL_DIR)/$(MPFR_PATCH_FILE)
+endif
 	$(MPFR_CAT) $(DL_DIR)/$(MPFR_SOURCE) | tar -C $(TOOL_BUILD_DIR) $(TAR_OPTIONS) -
 	toolchain/patch-kernel.sh $(MPFR_DIR) package/mpfr/ \*.patch
 	$(CONFIG_UPDATE) $(MPFR_DIR)
 ifneq ($(MPFR_PATCH),)
-	( cd $(MPFR_DIR) ; patch -p1 < $(MPFR_PATCH_FILE) ; )
+	( cd $(MPFR_DIR) ; patch -p1 < $(DL_DIR)/$(MPFR_PATCH_FILE) ; )
 endif
 	touch $@
 
