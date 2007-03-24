b16a581f5898b85617411c84cd042ac7827e82b7
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2007-03-24 11:47:00 +0000

    - fix downloading the setserial patch

--- package/setserial/setserial.mk
+++ package/setserial/setserial.mk
@@ -6,7 +6,7 @@
 SETSERIAL_VERSION:=2.17
 SETSERIAL_SOURCE:=setserial_$(SETSERIAL_VERSION).orig.tar.gz
 SETSERIAL_SITE:=http://ftp.debian.org/debian/pool/main/s/setserial/
-SETSERIAL_PATCH:=setserial_$(SETSERIAL_VERSION)-43.diff.gz
+SETSERIAL_PATCH_FILE:=setserial_$(SETSERIAL_VERSION)-43.diff.gz
 SETSERIAL_DIR:=$(BUILD_DIR)/setserial-$(SETSERIAL_VERSION)
 SETSERIAL_BINARY:=setserial
 SETSERIAL_TARGET_BINARY:=usr/bin/setserial
@@ -14,12 +14,20 @@ SETSERIAL_TARGET_BINARY:=usr/bin/setserial
 $(DL_DIR)/$(SETSERIAL_SOURCE):
 	$(WGET) -P $(DL_DIR) $(SETSERIAL_SITE)/$(SETSERIAL_SOURCE)
 
-$(SETSERIAL_DIR)/.unpacked: $(DL_DIR)/$(SETSERIAL_SOURCE)
+ifneq ($(SETSERIAL_PATCH_FILE),)
+SETSERIAL_PATCH:=$(DL_DIR)/$(SETSERIAL_PATCH_FILE)
+$(SETSERIAL_PATCH):
+	$(WGET) -O $@ $(SETSERIAL_SITE)/$(SETSERIAL_PATCH_FILE)
+else
+SETSERIAL_PATCH:=
+endif
+
+$(SETSERIAL_DIR)/.unpacked: $(DL_DIR)/$(SETSERIAL_SOURCE) $(SETSERIAL_PATCH)
 	$(ZCAT) $(DL_DIR)/$(SETSERIAL_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	$(CONFIG_UPDATE) $(SETSERIAL_DIR)
 	toolchain/patch-kernel.sh $(SETSERIAL_DIR) package/setserial setserial\*.patch
-ifneq ($(SETSERIAL_PATCH),)
-	toolchain/patch-kernel.sh $(SETSERIAL_DIR) $(DL_DIR) $(SETSERIAL_PATCH)
+ifneq ($(SETSERIAL_PATCH_FILE),)
+	toolchain/patch-kernel.sh $(SETSERIAL_DIR) $(DL_DIR) $(SETSERIAL_PATCH_FILE)
 	if [ -d $(SETSERIAL_DIR)/debian/patches ]; then \
 		toolchain/patch-kernel.sh $(SETSERIAL_DIR) $(SETSERIAL_DIR)/debian/patches \*.patch ; \
 	fi
@@ -27,7 +35,7 @@ endif
 	touch $@
 
 ifeq ($(BR2_PREFER_IMA),y)
-SETSERIAL_CFLAGS=--combine $(CFLAGS_WHOLE_PROGRAM)
+SETSERIAL_CFLAGS=$(CFLAGS_COMBINE) $(CFLAGS_WHOLE_PROGRAM)
 endif
 
 $(SETSERIAL_DIR)/.configured: $(SETSERIAL_DIR)/.unpacked
@@ -53,7 +61,7 @@ $(TARGET_DIR)/$(SETSERIAL_TARGET_BINARY): $(SETSERIAL_DIR)/$(SETSERIAL_BINARY)
 
 setserial: uclibc $(TARGET_DIR)/$(SETSERIAL_TARGET_BINARY)
 
-setserial-source: $(DL_DIR)/$(SETSERIAL_SOURCE) $(DL_DIR)/$(SETSERIAL_PATCH)
+setserial-source: $(DL_DIR)/$(SETSERIAL_SOURCE) $(SETSERIAL_PATCH)
 
 setserial-clean:
 	rm -f $(TARGET_DIR)/$(SETSERIAL_TARGET_BINARY)
