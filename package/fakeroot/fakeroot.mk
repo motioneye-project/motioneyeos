69765db58ea54f477bb58fa0738dfa1bae5509cc
Peter Korsgaard <jacmet@sunsite.dk>
2007-04-03 15:36:03 +0000

    Version bump. Thanks to Michael Benedict

--- package/fakeroot/fakeroot.mk
+++ package/fakeroot/fakeroot.mk
@@ -3,7 +3,7 @@
 # fakeroot
 #
 #############################################################
-FAKEROOT_VERSION:=1.6.4
+FAKEROOT_VERSION:=1.6.5
 FAKEROOT_SOURCE:=fakeroot_$(FAKEROOT_VERSION).tar.gz
 FAKEROOT_SITE:=http://ftp.debian.org/debian/pool/main/f/fakeroot
 FAKEROOT_CAT:=$(ZCAT)
