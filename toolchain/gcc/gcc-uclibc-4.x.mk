59e19c5890d22056f72520046d5ddc01be1e788e
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2007-07-07 12:58:15 +0000

    - use correct path to libstdc++

--- toolchain/gcc/gcc-uclibc-4.x.mk
+++ toolchain/gcc/gcc-uclibc-4.x.mk
@@ -303,7 +303,7 @@ ifeq ($(BR2_GCC_SHARED_LIBGCC),y)
 	-cp -dpf $(STAGING_DIR)/usr/$(REAL_GNU_TARGET_NAME)/lib/libgcc_s* $(TARGET_DIR)/lib/
 endif
 ifeq ($(BR2_INSTALL_LIBSTDCPP),y)
-	-cp -dpf $(STAGING_DIR)/lib/libstdc++.so* $(TARGET_DIR)/lib/
+	-cp -dpf $(STAGING_DIR)/usr/lib/libstdc++.so* $(TARGET_DIR)/lib/
 endif
 ifeq ($(BR2_INSTALL_LIBGCJ),y)
 	-cp -dpf $(STAGING_DIR)/lib/libgcj.so* $(TARGET_DIR)/lib/
