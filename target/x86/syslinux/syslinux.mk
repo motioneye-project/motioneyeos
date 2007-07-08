9dd07e229c674d6fb6a220cbc70859fa2e7d3736
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2007-07-08 20:51:19 +0000

    - add pxelinux

--- target/x86/syslinux/syslinux.mk
+++ target/x86/syslinux/syslinux.mk
@@ -22,7 +22,7 @@ ifeq ($(SYSLINUX_SUPPORTED_ARCH),y)
 #
 #############################################################
 
-SYSLINUX_VERSION:=3.31
+SYSLINUX_VERSION:=3.51
 SYSLINUX_DIR=$(BUILD_DIR)/syslinux-$(SYSLINUX_VERSION)
 SYSLINUX_DIR2=$(TOOL_BUILD_DIR)/syslinux-$(SYSLINUX_VERSION)
 SYSLINUX_SOURCE=syslinux-$(SYSLINUX_VERSION).tar.bz2
@@ -37,20 +37,22 @@ $(DL_DIR)/$(SYSLINUX_SOURCE):
 syslinux-source: $(DL_DIR)/$(SYSLINUX_SOURCE)
 
 $(SYSLINUX_DIR)/Makefile: $(DL_DIR)/$(SYSLINUX_SOURCE) $(SYSLINUX_PATCH)
-	$(SYSLINUX_CAT) $(DL_DIR)/$(SYSLINUX_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+	$(SYSLINUX_CAT) $(DL_DIR)/$(SYSLINUX_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	toolchain/patch-kernel.sh $(SYSLINUX_DIR) target/x86/syslinux/ \*.patch
 	touch -c $@
 
-$(SYSLINUX_DIR)/isolinux.bin: $(SYSLINUX_DIR)/Makefile
-	$(MAKE) -C $(SYSLINUX_DIR)
+$(SYSLINUX_DIR)/isolinux.bin $(SYSLINUX_DIR)/pxelinux.bin: $(SYSLINUX_DIR)/Makefile
+	$(MAKE) CC="$(HOSTCC)" AR="$(HOSTAR)" -C $(SYSLINUX_DIR)
 	touch -c $@
 
 syslinux: $(SYSLINUX_DIR)/isolinux.bin
+pxelinux: $(SYSLINUX_DIR)/pxelinux.bin
 
-syslinux-clean:
+pxelinux-clean syslinux-clean:
+	rm -f $(SYSLINUX_DIR)/isolinux.bin $(SYSLINUX_DIR)/pxelinux.bin
 	-$(MAKE) -C $(SYSLINUX_DIR) clean
 
-syslinux-dirclean:
+pxelinux-dirclean syslinux-dirclean:
 	rm -rf $(SYSLINUX_DIR)
 
 endif
@@ -62,4 +64,7 @@ endif
 #############################################################
 ifeq ($(strip $(BR2_TARGET_SYSLINUX)),y)
 TARGETS+=syslinux
+endif
+ifeq ($(strip $(BR2_TARGET_PXELINUX)),y)
+TARGETS+=pxelinux
 endif
