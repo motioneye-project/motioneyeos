06cc62f9cfbe43e9d31a07667a6af905b5f34a42
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2007-07-16 07:55:50 +0000

    Get rid of repeated copy of 'customize'

--- package/customize/customize.mk
+++ package/customize/customize.mk
@@ -5,8 +5,23 @@
 #############################################################
 CUST_DIR:=package/customize/source
 
-customize:
-	-cp -af $(CUST_DIR)/* $(TARGET_DIR)/
+$(BUILD_DIR)/.customize:
+	rm -f $(PROJECT_BUILD_DIR)/series
+	(cd $(CUST_DIR) ; \
+	 /bin/ls -d * > $(PROJECT_BUILD_DIR)/series || \
+	 touch $(PROJECT_BUILD_DIR)/series )
+	for f in `cat $(PROJECT_BUILD_DIR)/series` ; do 	\
+		cp -af $(CUST_DIR)/$$f $(TARGET_DIR)/$$f ;	\
+	done
+	rm -f $(PROJECT_BUILD_DIR)/series
+	touch $@
+
+customize: $(BUILD_DIR)/.customize
+
+customize-clean:
+	rm -f $(BUILD_DIR)/.customize
+
+.PHONY:	customize
 #############################################################
 #
 # Toplevel Makefile options
