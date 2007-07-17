bf45b770785399fb1e9d343247b4be21d6858e21
"Steven J. Hill" <sjhill@realitydiluted.com>
2007-07-17 00:27:10 +0000

    Make OpenSSL support to be configurable and get the install target footprint to be as small as possible. Also clean up the clean target. "Clean up, clean up, everybody lend a hand! Clean up, clean up, you can do it, yes you can."

--- package/lighttpd/lighttpd.mk
+++ package/lighttpd/lighttpd.mk
@@ -13,11 +13,11 @@ LIGHTTPD_BINARY:=src/lighttpd
 LIGHTTPD_TARGET_BINARY:=usr/sbin/lighttpd
 
 $(DL_DIR)/$(LIGHTTPD_SOURCE):
-	 $(WGET) -P $(DL_DIR) $(LIGHTTPD_SITE)/$(LIGHTTPD_SOURCE)
+	$(WGET) -P $(DL_DIR) $(LIGHTTPD_SITE)/$(LIGHTTPD_SOURCE)
 ifneq ($(LIGHTTPD_PATCH),)
 LIGHTTPD_PATCH_FILE:=$(DL_DIR)/$(LIGHTTPD_PATCH)
 $(LIGHTTPD_PATCH_FILE):
-	 $(WGET) -P $(DL_DIR) $(LIGHTTPD_SITE)/$(LIGHTTPD_PATCH)
+	$(WGET) -P $(DL_DIR) $(LIGHTTPD_SITE)/$(LIGHTTPD_PATCH)
 endif
 lighttpd-source: $(DL_DIR)/$(LIGHTTPD_SOURCE) $(LIGHTTPD_PATCH_FILE)
 
@@ -34,6 +34,12 @@ endif
 	$(SED) 's/-lfs/-largefile/g;s/_lfs/_largefile/g' $(LIGHTTPD_DIR)/configure
 	touch $@
 
+ifeq ($(strip $(BR2_PACKAGE_LIGHTTPD_OPENSSL)),y)
+LIGHTTPD_OPENSSL:=--with-openssl
+else
+LIGHTTPD_OPENSSL:=--without-openssl
+endif
+
 $(LIGHTTPD_DIR)/.configured: $(LIGHTTPD_DIR)/.unpacked
 	(cd $(LIGHTTPD_DIR); rm -rf config.cache; \
 		$(TARGET_CONFIGURE_OPTS) \
@@ -43,11 +49,11 @@ $(LIGHTTPD_DIR)/.configured: $(LIGHTTPD_DIR)/.unpacked
 		--host=$(GNU_TARGET_NAME) \
 		--build=$(GNU_HOST_NAME) \
 		--prefix=/usr \
-		--libdir=/lib \
+		--libdir=/usr/lib/lighttpd \
 		--libexecdir=/usr/lib \
 		--sysconfdir=/etc \
 		--localstatedir=/var \
-		--with-openssl \
+		$(LIGHTTPD_OPENSSL) \
 		--without-pcre \
 		--program-prefix="" \
 		$(DISABLE_IPV6) \
@@ -60,12 +66,27 @@ $(LIGHTTPD_DIR)/$(LIGHTTPD_BINARY): $(LIGHTTPD_DIR)/.configured
     
 $(TARGET_DIR)/$(LIGHTTPD_TARGET_BINARY): $(LIGHTTPD_DIR)/$(LIGHTTPD_BINARY)
 	$(MAKE) DESTDIR=$(TARGET_DIR) -C $(LIGHTTPD_DIR) install
+	@rm -rf $(TARGET_DIR)/usr/share/man $(TARGET_DIR)/usr/man
+	@rm -rf $(TARGET_DIR)/usr/lib/lighttpd/*.la
+	$(STRIP) --strip-unneeded $(TARGET_DIR)/usr/lib/lighttpd/*.so
+	$(STRIP) --strip-unneeded $(TARGET_DIR)/$(LIGHTTPD_TARGET_BINARY)
+	@if [ ! -f $(TARGET_DIR)/etc/lighttpd/lighttpd.conf ] ; then \
+		$(INSTALL) -m 0644 -D $(LIGHTTPD_DIR)/doc/lighttpd.conf $(TARGET_DIR)/etc/lighttpd/lighttpd.conf; \
+	fi;
 	$(INSTALL) -m 0755 -D $(LIGHTTPD_DIR)/openwrt/S51lighttpd  $(TARGET_DIR)/etc/init.d/S99lighttpd
 
+ifeq ($(strip $(BR2_PACKAGE_LIGHTTPD_OPENSSL)),y)
 lighttpd: uclibc openssl $(TARGET_DIR)/$(LIGHTTPD_TARGET_BINARY)
+else
+lighttpd: uclibc $(TARGET_DIR)/$(LIGHTTPD_TARGET_BINARY)
+endif
 
 lighttpd-clean:
 	$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(LIGHTTPD_DIR) uninstall
+	@rm -rf $(TARGET_DIR)/usr/lib/lighttpd
+	@rm -f $(TARGET_DIR)/etc/init.d/S99lighttpd
+	@rm -f $(TARGET_DIR)/etc/lighttpd/lighttpd.conf
+	@rmdir -p --ignore-fail-on-non-empty $(TARGET_DIR)/etc/lighttpd
 	-$(MAKE) -C $(LIGHTTPD_DIR) clean
 
 lighttpd-dirclean:
