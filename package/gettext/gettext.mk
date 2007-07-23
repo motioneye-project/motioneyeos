9633b737bce94c0acb934769a351182623e35ac6
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2007-07-23 08:35:06 +0000

    Update Config of gettext subdir

--- package/gettext/gettext.mk
+++ package/gettext/gettext.mk
@@ -26,6 +26,7 @@ $(GETTEXT_DIR)/.unpacked: $(DL_DIR)/$(GETTEXT_SOURCE)
 	$(GETTEXT_CAT) $(DL_DIR)/$(GETTEXT_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	toolchain/patch-kernel.sh $(GETTEXT_DIR) package/gettext/ gettext\*.patch
 	$(CONFIG_UPDATE) $(@D)
+	$(CONFIG_UPDATE) $(GETTEXT_DIR)/build-aux
 	touch $@
 
 ifeq ($(strip $(BR2_TOOLCHAIN_EXTERNAL)),y)
