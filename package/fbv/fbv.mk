71ed851f4416c5bbdd5a836a32a1660992eaf205
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2007-07-23 12:19:59 +0000

    cleanup fbv package

--- package/fbv/fbv.mk
+++ package/fbv/fbv.mk
@@ -3,10 +3,10 @@
 # fbv
 #
 #############################################################
-FBV_VER:=1.0b
-FBV_SOURCE:=fbv-$(FBV_VER).tar.gz
+FBV_VERSION:=1.0b
+FBV_SOURCE:=fbv-$(FBV_VERSION).tar.gz
 FBV_SITE:=http://s-tech.elsat.net.pl/fbv
-FBV_DIR:=$(BUILD_DIR)/fbv-$(FBV_VER)
+FBV_DIR:=$(BUILD_DIR)/fbv-$(FBV_VERSION)
 FBV_CAT:=$(ZCAT)
 FBV_BINARY:=fbv
 FBV_TARGET_BINARY:=usr/bin/$(FBV_BINARY)
@@ -16,19 +16,19 @@ $(DL_DIR)/$(FBV_SOURCE):
 
 $(FBV_DIR)/.unpacked: $(DL_DIR)/$(FBV_SOURCE)
 	$(FBV_CAT) $(DL_DIR)/$(FBV_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(FBV_DIR) package/fbv/ fbv-$(FBV_VER)\*.patch\*
-	@touch $@
+	toolchain/patch-kernel.sh $(FBV_DIR) package/fbv/ fbv-$(FBV_VERSION)\*.patch\*
+	touch $@
 
 $(FBV_DIR)/.configured: $(FBV_DIR)/.unpacked
 	(cd $(FBV_DIR); \
-		CFLAGS="$(TARGET_CFLAGS)" \
-		LDFLAGS="$(TARGET_LDFLAGS)" \
+		($(TARGET_CONFIGURE_OPTS) \
+		($(TARGET_CONFIGURE_ARGS) \
 		./configure \
 		--prefix=/usr \
 		--cc=$(TARGET_CC) \
 		--libs="-lz -lm" \
 	);
-	@touch $@
+	touch $@
 
 $(FBV_DIR)/$(FBV_BINARY): $(FBV_DIR)/.configured
 	$(MAKE) CC=$(TARGET_CC) -C $(FBV_DIR)
