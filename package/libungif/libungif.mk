fc58d10c2c3e3c4e7b9ae8554e6a2fca02566b4f
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2007-08-01 08:35:13 +0000

    Cleanup libungif, use usr/lib

--- package/libungif/libungif.mk
+++ package/libungif/libungif.mk
@@ -14,6 +14,8 @@ LIBUNGIF_TARGET_BINARY:=usr/lib/libungif.so
 $(DL_DIR)/$(LIBUNGIF_SOURCE):
 	$(WGET) -P $(DL_DIR) $(LIBUNGIF_SITE)/$(LIBUNGIF_SOURCE)
 
+libungif-source: $(DL_DIR)/$(LIBUNGIF_SOURCE)
+
 $(LIBUNGIF_DIR)/.unpacked: $(DL_DIR)/$(LIBUNGIF_SOURCE)
 	$(LIBUNGIF_CAT) $(DL_DIR)/$(LIBUNGIF_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	toolchain/patch-kernel.sh $(LIBUNGIF_DIR) package/libungif/ libungif-$(LIBUNGIF_VERSION)\*.patch\*
@@ -40,17 +42,14 @@ $(LIBUNGIF_DIR)/.configured: $(LIBUNGIF_DIR)/.unpacked
 
 $(LIBUNGIF_DIR)/lib/.libs/libungif.a: $(LIBUNGIF_DIR)/.configured
 	$(MAKE) -C $(LIBUNGIF_DIR)
-	touch -c $(LIBUNGIF_DIR)/lib/.libs/libungif.a
 
-$(STAGING_DIR)/lib/libungif.a: $(LIBUNGIF_DIR)/lib/.libs/libungif.a
+$(STAGING_DIR)/usr/lib/libungif.a: $(LIBUNGIF_DIR)/lib/.libs/libungif.a
 	$(MAKE) DESTDIR=$(STAGING_DIR) -C $(LIBUNGIF_DIR) install
-	rm $(STAGING_DIR)/lib/libungif.la
-	touch -c $(STAGING_DIR)/lib/libungif.a
+	$(SED) "s,^libdir=.*,libdir=\'$(STAGING_DIR)/usr/lib\',g" $(STAGING_DIR)/usr/lib/libungif.la
 
-$(TARGET_DIR)/$(LIBUNGIF_TARGET_BINARY): $(STAGING_DIR)/lib/libungif.a
+$(TARGET_DIR)/$(LIBUNGIF_TARGET_BINARY): $(STAGING_DIR)/usr/lib/libungif.a
 	cp -dpf $(STAGING_DIR)/$(LIBUNGIF_TARGET_BINARY)* $(TARGET_DIR)/usr/lib/
 	-$(STRIP) --strip-unneeded $(TARGET_DIR)/$(LIBUNGIF_TARGET_BINARY)*
-	touch -c $(TARGET_DIR)/$(LIBUNGIF_TARGET_BINARY)
 
 libungif: uclibc $(TARGET_DIR)/$(LIBUNGIF_TARGET_BINARY)
 
