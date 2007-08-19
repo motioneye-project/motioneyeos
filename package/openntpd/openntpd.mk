5036dd45b2171031bf2754731c352add91fd1e40
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2007-08-19 21:13:08 +0000

    openntpd cleanup

--- package/openntpd/openntpd.mk
+++ package/openntpd/openntpd.mk
@@ -29,9 +29,11 @@ $(OPENNTPD_DIR)/.configured: $(OPENNTPD_DIR)/.source
 		--build=$(GNU_HOST_NAME) \
 		--prefix=/usr \
 		--sysconfdir=/etc \
+		--datadir=/usr/share \
+		--mandir=/usr/share/man \
 		--with-builtin-arc4random \
-	);
-	touch $(OPENNTPD_DIR)/.configured;
+	)
+	touch $@
 
 $(OPENNTPD_DIR)/$(OPENNTPD_BINARY): $(OPENNTPD_DIR)/.configured
 	$(MAKE) CC=$(TARGET_CC) -C $(OPENNTPD_DIR)
@@ -45,22 +47,24 @@ $(OPENNTPD_DIR)/$(OPENNTPD_BINARY): $(OPENNTPD_DIR)/.configured
 	#	ntpd.c buffer.c log.c imsg.c ntp.c ntp_msg.c y.tab.c config.c \
 	#	server.c client.c sensors.c util.c ; \
 	#)
-	$(STRIP) -s $@
+	$(STRIP) $@
 
 $(TARGET_DIR)/$(OPENNTPD_TARGET_BINARY): $(OPENNTPD_DIR)/$(OPENNTPD_BINARY)
 	rm -f $(TARGET_DIR)/etc/ntpd.conf
 	$(MAKE) DESTDIR=$(TARGET_DIR) STRIP_OPT="" -C $(OPENNTPD_DIR) install
 	-$(STRIP) $(TARGET_DIR)/$(OPENNTPD_TARGET_BINARY)
 	cp -af $(OPENNTPD_DIR)/ntpd.conf $(TARGET_DIR)/etc/ntpd.conf
-	rm -Rf $(TARGET_DIR)/usr/man
+	# Why in earth do we wipe the whole man directory here?
+	#rm -Rf $(TARGET_DIR)/usr/man
 
 ntpd: uclibc $(TARGET_DIR)/$(OPENNTPD_TARGET_BINARY)
 
 ntpd-source: $(DL_DIR)/$(OPENNTPD_SOURCE)
 
 ntpd-clean:
-	-$(MAKE) DESTDIR=$(TARGET_DIR) -C $(OPENNTPD_DIR) uninstall
-	rm -f $(TARGET_DIR)/etc/ntpd.conf $(TARGET_DIR)/$(OPENNTPD_TARGET_BINARY)
+	rm -f $(addprefix $(TARGET_DIR)/,etc/ntpd.conf \
+					 usr/share/man/man?/ntpd.conf* \
+					 $(OPENNTPD_TARGET_BINARY))
 	-$(MAKE) -C $(OPENNTPD_DIR) clean
 
 ntpd-dirclean:
