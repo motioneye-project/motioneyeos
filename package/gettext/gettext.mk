12259e3d57e4a8328b44c8dc9e9fbc2914bb2df5
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2007-09-05 06:50:53 +0000

    Add target to unpack gettext

--- package/gettext/gettext.mk
+++ package/gettext/gettext.mk
@@ -123,6 +123,8 @@ $(STAGING_DIR)/$(GETTEXT_TARGET_BINARY): $(GETTEXT_DIR)/$(GETTEXT_BINARY)
 
 gettext: uclibc pkgconfig $(STAGING_DIR)/$(GETTEXT_TARGET_BINARY)
 
+gettext-unpacked: $(GETTEXT_DIR)/.unpacked
+
 gettext-clean:
 	$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(GETTEXT_DIR) uninstall
 	-$(MAKE) -C $(GETTEXT_DIR) clean
