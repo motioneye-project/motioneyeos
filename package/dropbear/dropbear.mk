e67d464bc4a2389655429577a5af3e1ceca11915
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2007-09-19 14:11:28 +0000

    - need to mkdir etc/init.d. Closes #1465

--- package/dropbear/dropbear.mk
+++ package/dropbear/dropbear.mk
@@ -56,6 +56,7 @@ $(TARGET_DIR)/$(DROPBEAR_TARGET_BINARY): $(DROPBEAR_DIR)/$(DROPBEAR_BINARY)
 	ln -snf ../sbin/dropbear $(TARGET_DIR)/usr/bin/dbclient
 	ln -snf ../sbin/dropbear $(TARGET_DIR)/usr/bin/dropbearkey
 	ln -snf ../sbin/dropbear $(TARGET_DIR)/usr/bin/dropbearconvert
+	mkdir -p $(TARGET_DIR)/etc/init.d
 	cp -dpf $(DROPBEAR_DIR)/S50dropbear $(TARGET_DIR)/etc/init.d/
 	chmod a+x $(TARGET_DIR)/etc/init.d/S50dropbear
 
