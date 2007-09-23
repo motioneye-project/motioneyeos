8a4110de80cc94d0a833238f79d31feb90874801
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2007-09-23 04:10:59 +0000

    Remove bad 'w' flag from rm in diffutils

--- package/diffutils/diffutils.mk
+++ package/diffutils/diffutils.mk
@@ -110,7 +110,7 @@ endif
 ifneq ($(BR2_HAVE_MANPAGES),y)
 	rm -rf $(TARGET_DIR)/usr/share/man
 endif
-	rm -rfw $(TARGET_DIR)/share/locale
+	rm -rf $(TARGET_DIR)/share/locale
 	rm -rf $(TARGET_DIR)/usr/share/doc
 
 diffutils: uclibc $(TARGET_DIR)/$(DIFFUTILS_TARGET_BINARY)
