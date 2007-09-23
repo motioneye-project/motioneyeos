f45c2c661195d546d29ec4c3ca4f83eea3856030
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2007-09-23 09:56:21 +0000

    Fix syntax error when using addprefix in ipsec-tools.mk

--- package/ipsec-tools/ipsec-tools.mk
+++ package/ipsec-tools/ipsec-tools.mk
@@ -112,14 +112,14 @@ $(TARGET_DIR)/$(IPSEC_TOOLS_TARGET_BINARY_RACOONCTL): \
 	  $(TARGET_DIR)/$(IPSEC_TOOLS_TARGET_BINARY_RACOON) \
 	  $(TARGET_DIR)/$(IPSEC_TOOLS_TARGET_BINARY_RACOONCTL)
 ifneq ($(BR2_HAVE_MANPAGES),y)
-	rm -f $(addprefix $(TARGET_DIR)/usr/man/,
+	rm -f $(addprefix $(TARGET_DIR)/usr/man/, \
 		man3/ipsec_strerror.3 man3/ipsec_set_policy.3 \
 		man5/racoon.conf.5 \
 		man8/racoonctl.8 man8/racoon.8 \
 		man8/plainrsa-gen.8 man8/setkey.8)
 endif
 ifeq ($(strip $(BR2_PACKAGE_IPSEC_TOOLS_LIBS)), y)
-	install -D $(addprefix $(IPSEC_TOOLS_DIR)/src/,
+	install -D $(addprefix $(IPSEC_TOOLS_DIR)/src/, \
 		libipsec/.libs/libipsec.a libipsec/.libs/libipsec.la \
 		racoon/.libs/libracoon.a racoon/.libs/libracoon.la) \
 		$(STAGING_DIR)/lib
