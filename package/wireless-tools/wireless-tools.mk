0864cb65d72a74748068a00a0f5ca2e3ae77bfee
Ivan Kuten <ivan.kuten@promwad.com>
2007-10-18 21:55:09 +0000

    updated wireless-tools to .29 version, thanks to sushisan

--- package/wireless-tools/wireless-tools.mk
+++ package/wireless-tools/wireless-tools.mk
@@ -8,10 +8,10 @@
 # which is available in 28-pre3 and later...
 # Jean II
 # v28.pre3 -> earliest possible
-WIRELESS_TOOLS_VERSION:=28
+WIRELESS_TOOLS_VERSION:=29
 WIRELESS_TOOLS_SUBVER:=
 
-WIRELESS_TOOLS_SOURCE_URL:=http://pcmcia-cs.sourceforge.net/ftp/contrib
+WIRELESS_TOOLS_SOURCE_URL:=http://www.hpl.hp.com/personal/Jean_Tourrilhes/Linux/
 WIRELESS_TOOLS_SOURCE:=wireless_tools.$(WIRELESS_TOOLS_VERSION)$(WIRELESS_TOOLS_SUBVER).tar.gz
 WIRELESS_TOOLS_BUILD_DIR=$(BUILD_DIR)/wireless_tools.$(WIRELESS_TOOLS_VERSION)
 
