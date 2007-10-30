b20980bd57435a3df8205c16e02ea4177ff6f3cb
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2007-10-30 20:14:12 +0000

    Fix saveconfig followed by menuconfig

--- project/project.mk
+++ project/project.mk
@@ -22,19 +22,19 @@ saveconfig: $(CONFIG)/conf
 		cp $(LINUX26_DIR)/.config \
 			$(LOCAL)/$(PROJECT)/linux-$(LINUX26_VERSION).config; \
 		$(SED) '/BR2_PACKAGE_LINUX_KCONFIG/d' $(PROJECT_FILE); \
-		echo "BR2_PACKAGE_LINUX_KCONFIG:=$(LOCAL)/$(PROJECT)/linux-$(LINUX26_VERSION).config" >> $(PROJECT_FILE); \
+		echo "BR2_PACKAGE_LINUX_KCONFIG=\"$(LOCAL)/$(PROJECT)/linux-$(LINUX26_VERSION).config\"" >> $(PROJECT_FILE); \
 	fi
 	if [ -f $(BUSYBOX_DIR)/.config ]; then \
 		cp $(BUSYBOX_DIR)/.config \
 			$(LOCAL)/$(PROJECT)/busybox-$(BUSYBOX_VERSION).config; \
 		$(SED) '/BR2_PACKAGE_BUSYBOX_CONFIG/d' $(PROJECT_FILE); \
-		echo "BR2_PACKAGE_BUSYBOX_CONFIG:=$(LOCAL)/$(PROJECT)/busybox-$(BUSYBOX_VERSION).config" >> $(PROJECT_FILE); \
+		echo "BR2_PACKAGE_BUSYBOX_CONFIG=\"$(LOCAL)/$(PROJECT)/busybox-$(BUSYBOX_VERSION).config\"" >> $(PROJECT_FILE); \
 	fi
 	if [ -f $(UCLIBC_DIR)/.config ]; then \
 		cp $(UCLIBC_DIR)/.config \
 			$(LOCAL)/$(PROJECT)/uclibc-$(UCLIBC_VER).config; \
 		$(SED) '/BR2_UCLIBC_CONFIG/d' $(PROJECT_FILE); \
-		echo "BR2_UCLIBC_CONFIG:=$(LOCAL)/$(PROJECT)/uclibc-$(UCLIBC_VER).config" >> $(PROJECT_FILE); \
+		echo "BR2_UCLIBC_CONFIG=\"$(LOCAL)/$(PROJECT)/uclibc-$(UCLIBC_VER).config\"" >> $(PROJECT_FILE); \
 	fi
 	if [ -f $(UBOOT_DIR)/include/configs/$(PROJECT).h ]; then \
 		mkdir -p $(LOCAL)/$(PROJECT)/u-boot; \
