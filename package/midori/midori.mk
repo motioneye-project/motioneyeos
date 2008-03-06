1e19e92f0081e27c7a413d9cd94db65e4ddb0f00
John Voltz <john.voltz@gmail.com>
2008-03-06 18:18:35 +0000

    added midori browser

---
+++ package/midori/midori.mk
@@ -0,0 +1,21 @@
+#############################################################
+#
+# midori
+#
+#############################################################
+
+MIDORI_VERSION = 0.0.17
+MIDORI_SOURCE = midori-$(MIDORI_VERSION).tar.gz
+MIDORI_SITE = http://software.twotoasts.de/media/midori/
+MIDORI_AUTORECONF = YES
+MIDORI_INSTALL_STAGING = NO
+MIDORI_INSTALL_TARGET = YES
+
+MIDORI_CONF_OPT = --target=$(GNU_TARGET_NAME) --host=$(GNU_TARGET_NAME) \
+		--build=$(GNU_HOST_NAME)  --prefix=/usr --sysconfdir=/etc \
+		PKG_CONFIG_PATH=$(STAGING_DIR)/usr/lib/pkgconfig 
+
+
+MIDORI_DEPENDENCIES = uclibc webkit libsexy $(XSERVER) 
+
+$(eval $(call AUTOTARGETS,package,midori))
