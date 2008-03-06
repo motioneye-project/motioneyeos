eea1d602583fae32eeee840f0277c82d5b26ada6
John Voltz <john.voltz@gmail.com>
2008-03-06 18:36:54 +0000

    added xboard

---
+++ package/xboard/xboard.mk
@@ -0,0 +1,19 @@
+#############################################################
+#
+# xboard
+#
+#############################################################
+XBOARD_VERSION = 4.2.7
+XBOARD_SOURCE = xboard-$(XBOARD_VERSION).tar.gz
+XBOARD_SITE = http://ftp.gnu.org/gnu/xboard
+XBOARD_AUTORECONF = NO
+XBOARD_INSTALL_STAGING = NO
+XBOARD_INSTALL_TARGET = YES
+XBOARD_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
+
+XBOARD_CONF_OPT --prefix=/usr
+
+XBOARD_DEPENDENCIES = uclibc gnuchess
+
+$(eval $(call AUTOTARGETS,package,xboard))
+
