e0caa0ccc387fb4a0bfdcd293e251fef278a64de
John Voltz <john.voltz@gmail.com>
2008-03-06 18:38:20 +0000

    added xstroke

---
+++ package/xstroke/xstroke.mk
@@ -0,0 +1,17 @@
+#############################################################
+#
+# xstroke
+#
+#############################################################
+XSTROKE_VERSION = 0.6
+XSTROKE_SOURCE = xstroke-$(XSTROKE_VERSION).tar.gz
+XSTROKE_SITE = http://avr32linux.org/twiki/pub/Main/XStroke
+XSTROKE_AUTORECONF = NO
+XSTROKE_INSTALL_STAGING = NO
+XSTROKE_INSTALL_TARGET = YES
+XSTROKE_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
+
+XSTROKE_DEPENDENCIES = uclibc 
+
+$(eval $(call AUTOTARGETS,package,xstroke))
+
