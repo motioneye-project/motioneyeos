dab3cd675c86d4785f6911f5a386803b92b8a0ec
John Voltz <john.voltz@gmail.com>
2008-03-06 18:10:50 +0000

    added gnuchess

---
+++ package/gnuchess/gnuchess.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# gnuchess
+#
+#############################################################
+GNUCHESS_VERSION = 5.07
+GNUCHESS_SOURCE = gnuchess-$(GNUCHESS_VERSION).tar.gz
+GNUCHESS_SITE = http://ftp.gnu.org/pub/gnu/chess
+GNUCHESS_AUTORECONF = NO
+GNUCHESS_INSTALL_STAGING = NO
+GNUCHESS_INSTALL_TARGET = YES
+
+GNUCHESS_CONF_ENV = ac_cv_func_realloc_0_nonnull=yes ac_cv_func_malloc_0_nonnull=yes
+
+GNUCHESS_DEPENDENCIES = uclibc 
+
+$(eval $(call AUTOTARGETS,package,gnuchess))
+
