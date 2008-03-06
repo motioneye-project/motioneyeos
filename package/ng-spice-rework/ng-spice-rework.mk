196aef7311a6791b1bfa1e4b680c9238f15fa724
John Voltz <john.voltz@gmail.com>
2008-03-06 18:19:42 +0000

    added ngspice-rework

---
+++ package/ng-spice-rework/ng-spice-rework.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# ng-spice-rework
+#
+#############################################################
+NG_SPICE_REWORK_VERSION = 17
+NG_SPICE_REWORK_SOURCE = ng-spice-rework-$(NG_SPICE_REWORK_VERSION).tar.gz
+NG_SPICE_REWORK_SITE = http://superb-west.dl.sourceforge.net/sourceforge/ngspice
+NG_SPICE_REWORK_AUTORECONF = NO
+NG_SPICE_REWORK_INSTALL_STAGING = NO
+NG_SPICE_REWORK_INSTALL_TARGET = YES
+
+NG-SPICE-REWORK_CONF_OPT = 
+
+NG-SPICE-REWORK_DEPENDENCIES = uclibc 
+
+$(eval $(call AUTOTARGETS,package,ng-spice-rework))
+
