9af6720ca3d48873c14038b3c753c84b1175665e
John Voltz <john.voltz@gmail.com>
2008-03-06 18:26:58 +0000

    added synergy

---
+++ package/synergy/synergy.mk
@@ -0,0 +1,19 @@
+#############################################################
+#
+# synergy
+#
+#############################################################
+
+SYNERGY_VERSION = 1.3.1
+SYNERGY_SOURCE = synergy-$(SYNERGY_VERSION).tar.gz
+SYNERGY_SITE = http://internap.dl.sourceforge.net/sourceforge/synergy2/
+SYNERGY_AUTORECONF = NO
+SYNERGY_INSTALL_STAGING = NO
+SYNERGY_INSTALL_TARGET = YES
+
+SYNERGY_CONF_OPT = --target=$(GNU_TARGET_NAME) --host=$(GNU_TARGET_NAME) \
+		--build=$(GNU_HOST_NAME) --prefix=/usr --sysconfdir=/etc 
+
+SYNERGY_DEPENDENCIES = uclibc $(XSERVER)
+
+$(eval $(call AUTOTARGETS,package,synergy))
