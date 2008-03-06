70b2460ed994b85fe8904ab8e79dda8a782e5e5b
John Voltz <john.voltz@gmail.com>
2008-03-06 18:28:26 +0000

    added vice

---
+++ package/vice/vice.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# vice
+#
+#############################################################
+VICE_VERSION = 1.22
+VICE_SOURCE = vice-$(VICE_VERSION).tar.gz
+VICE_SITE = http://www.viceteam.org/online
+VICE_AUTORECONF = NO
+VICE_INSTALL_STAGING = NO
+VICE_INSTALL_TARGET = YES
+
+VICE_CONF_OPT = --without-resid --with-alsa CFLAGS="-O0"
+
+VICE_DEPENDENCIES = uclibc 
+
+$(eval $(call AUTOTARGETS,package,vice))
+
