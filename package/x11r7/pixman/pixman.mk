f7e1c58a6cac1ad817bf1dee6f360dd206c54fc6
John Voltz <john.voltz@gmail.com>
2008-03-06 18:48:43 +0000

    fixed pixman install

--- package/x11r7/pixman/pixman.mk
+++ package/x11r7/pixman/pixman.mk
@@ -10,6 +10,8 @@ PIXMAN_SITE = http://cairographics.org/releases/
 PIXMAN_AUTORECONF = NO
 PIXMAN_INSTALL_STAGING = YES
 PIXMAN_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) install install-data
+PIXMAN_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install install-data
+
 PIXMAN_DEPENDENCIES = xlib_libX11 xlib_libXext xproto_dmxproto
 
 $(eval $(call AUTOTARGETS,package/x11r7,pixman))
