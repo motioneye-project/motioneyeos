77ec383076f19f9c2a980941f2dfa9da829de557
John Voltz <john.voltz@gmail.com>
2008-03-10 15:15:57 +0000

    changed to use sourceforge variable

--- package/icu/icu.mk
+++ package/icu/icu.mk
@@ -6,7 +6,7 @@
 
 ICU_VERSION:=4c-3_8_1
 ICU_SOURCE:=icu$(ICU_VERSION)-src.tgz
-ICU_SITE:=http://superb-west.dl.sourceforge.net/sourceforge/icu
+ICU_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/icu
 ICU_CAT:=$(ZCAT)
 ICU_DIR:=$(BUILD_DIR)/icu/source
 ICU_HOST_DIR:=$(BUILD_DIR)/icu-host/source
