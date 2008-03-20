3f488a4b0974552c45c6055c49ce5853fc388226
John Voltz <john.voltz@gmail.com>
2008-03-20 16:54:51 +0000

    some fixes for fltk

--- package/fltk/fltk.mk
+++ package/fltk/fltk.mk
@@ -10,11 +10,13 @@ FLTK_SITE = http://ftp.easysw.com/pub/fltk/1.1.7/
 FLTK_AUTORECONF = NO
 FLTK_INSTALL_STAGING = YES
 FLTK_INSTALL_TARGET = YES
-FLTK_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
+
+FLTK_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) STRIP=$(TARGET_STRIP) install
+FLTK_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) STRIP=$(TARGET_STRIP) install
 
 FLTK_CONF_OPT = --target=$(GNU_TARGET_NAME) --host=$(GNU_TARGET_NAME) \
 		--build=$(GNU_HOST_NAME) --prefix=/usr \
-		--sysconfdir=/etc 
+		--sysconfdir=/etc --enable-shared --enable-threads --with-x
 
 FLTK_DEPENDENCIES = uclibc $(XSERVER) 
 
