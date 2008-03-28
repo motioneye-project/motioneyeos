60739c33d3e146e3254639cf917b9344a0ed9edc
Peter Korsgaard <jacmet@sunsite.dk>
2008-03-28 10:39:48 +0000

    ipkg: use http url

--- package/ipkg/ipkg.mk
+++ package/ipkg/ipkg.mk
@@ -6,7 +6,7 @@
 
 IPKG_VERSION = 0.99.163
 IPKG_SOURCE = ipkg-$(IPKG_VERSION).tar.gz
-IPKG_SITE = ftp://ftp.handhelds.org/packages/ipkg
+IPKG_SITE = http://ftp.handhelds.org/packages/ipkg
 IPKG_AUTORECONF = NO
 IPKG_INSTALL_STAGING = YES
 IPKG_INSTALL_TARGET = YES
