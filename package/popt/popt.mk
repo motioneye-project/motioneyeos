9fadf8ade54a79427a8ac712e9de974c77bf02fc
Nigel Kukard <nkukard@lbsd.net>
2008-04-12 09:51:21 +0000

    * Added popt

---
+++ package/popt/popt.mk
@@ -0,0 +1,20 @@
+#############################################################
+#
+# popt
+#
+#############################################################
+POPT_VERSION = 1.14
+POPT_SITE = http://rpm5.org/files/popt
+POPT_INSTALL_STAGING = YES
+
+
+POPT_CONF_ENV = ac_cv_va_copy=yes 
+
+
+POPT_CONF_OPT = --target=$(GNU_TARGET_NAME) --host=$(REAL_GNU_TARGET_NAME) \
+		--build=$(GNU_HOST_NAME) --prefix=/usr
+
+POPT_INSTALL_TARGET_OPT=DESTDIR=$(TARGET_DIR) install
+
+
+$(eval $(call AUTOTARGETS,package,popt))
