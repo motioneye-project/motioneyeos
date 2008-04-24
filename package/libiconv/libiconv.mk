83096989f9b310faa92c3ba1cfc9fa5b1061c577
John Voltz <john.voltz@gmail.com>
2008-04-24 18:35:44 +0000

    add libiconv and framework for vlc

---
+++ package/libiconv/libiconv.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# libiconv
+#
+#############################################################
+LIBICONV_VERSION = 1.12
+LIBICONV_SOURCE = libiconv-$(LIBICONV_VERSION).tar.gz
+LIBICONV_SITE = http://ftp.gnu.org/pub/gnu/libiconv
+LIBICONV_AUTORECONF = NO
+LIBICONV_INSTALL_STAGING = YES
+LIBICONV_INSTALL_TARGET = YES
+
+LIBICONV_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
+
+LIBICONV_DEPENDENCIES = uclibc 
+
+$(eval $(call AUTOTARGETS,package,libiconv))
+
---
+++ package/vlc/vlc.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# vlc
+#
+#############################################################
+VLC_VERSION = 0.8.6e
+VLC_SOURCE = vlc-$(VLC_VERSION).tar.bz2
+VLC_SITE = http://download.videolan.org/pub/videolan/vlc/0.8.6e
+VLC_AUTORECONF = NO
+VLC_INSTALL_STAGING = NO
+VLC_INSTALL_TARGET = YES
+
+VLC_CONF_OPT = 
+
+VLC_DEPENDENCIES = uclibc 
+
+$(eval $(call AUTOTARGETS,package,vlc))
+
