509fe4a6de108464aed16e7b49aa44e779c5e11a
Nigel Kukard <nkukard@lbsd.net>
2008-04-29 05:04:14 +0000

    * Bump version to 5.1.1 * Use bzip2 compression (rpms can have gzip or bzip2 payloads) * -j1, rpm cannot build with more than 1 thread

--- package/rpm/rpm.mk
+++ package/rpm/rpm.mk
@@ -3,11 +3,11 @@
 # rpm
 #
 #############################################################
-RPM_VERSION = 5.1.0
-RPM_SITE = http://rpm5.org/files/rpm/rpm-5.0/
+RPM_VERSION = 5.1.1
+RPM_SITE = http://rpm5.org/files/rpm/rpm-5.1/
 RPM_AUTORECONF = YES
 
-RPM_DEPENDENCIES = zlib beecrypt neon popt
+RPM_DEPENDENCIES = zlib bzip2 beecrypt neon popt
 
 RPM_CONF_ENV = CFLAGS="-I$(STAGING_DIR)/usr/include/beecrypt -I$(STAGING_DIR)/usr/include/neon" \
 		ac_cv_va_copy=yes
@@ -19,7 +19,10 @@ RPM_CONF_OPT = --disable-build-versionscript --disable-rpath \
 	--with-libbeecrypt=$(STAGING_DIR) \
 	--with-popt=$(STAGING_DIR) \
 	--with-mutex=UNIX/fcntl \
+	--with-bzip2 \
 	$(DISABLE_NLS)
+
+RPM_MAKE_OPT = -j1
 
 RPM_INSTALL_TARGET_OPT=DESTDIR=$(TARGET_DIR) program_transform_name= install
 
