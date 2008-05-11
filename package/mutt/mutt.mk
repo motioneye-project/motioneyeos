29a7dd1c10d6c605dfcd0a2d2064b2bf32f5e322
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2008-05-11 06:41:33 +0000

    Bump mutt version

--- package/mutt/mutt.mk
+++ package/mutt/mutt.mk
@@ -3,9 +3,9 @@
 # mutt
 #
 #############################################################
-MUTT_VERSION:=1.5.16
+MUTT_VERSION:=1.5.17+20080114
 MUTT_SOURCE:=mutt_$(MUTT_VERSION).orig.tar.gz
-MUTT_PATCH:=mutt_$(MUTT_VERSION)-3.diff.gz
+MUTT_PATCH:=mutt_$(MUTT_VERSION)-1.diff.gz
 MUTT_SITE:=$(BR2_DEBIAN_MIRROR)/debian/pool/main/m/mutt/
 MUTT_DIR:=$(BUILD_DIR)/mutt-$(MUTT_VERSION)
 MUTT_CAT:=$(ZCAT)
@@ -18,11 +18,9 @@ $(DL_DIR)/$(MUTT_SOURCE):
 $(DL_DIR)/$(MUTT_PATCH):
 	$(WGET) -P $(DL_DIR) $(MUTT_SITE)/$(MUTT_PATCH)
 
-mutt-source: $(DL_DIR)/$(MUTT_SOURCE) $(DL_DIR)/$(MUTT_PATCH)
-
 $(MUTT_DIR)/.unpacked: $(DL_DIR)/$(MUTT_SOURCE) $(DL_DIR)/$(MUTT_PATCH)
 	$(MUTT_CAT) $(DL_DIR)/$(MUTT_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(MUTT_DIR) package/mutt/ mutt\*.patch
+	toolchain/patch-kernel.sh $(MUTT_DIR) package/mutt/ mutt-$(MUTT_VERSION)\*.patch
 ifneq ($(MUTT_PATCH),)
 	(cd $(MUTT_DIR) && $(MUTT_CAT) $(DL_DIR)/$(MUTT_PATCH) | patch -p1)
 	if [ -d $(MUTT_DIR)/debian/patches ]; then \
@@ -55,6 +53,10 @@ $(MUTT_DIR)/$(MUTT_BINARY): $(MUTT_DIR)/.configured
 $(TARGET_DIR)/$(MUTT_TARGET_BINARY): $(MUTT_DIR)/$(MUTT_BINARY)
 	cp -dpf $(MUTT_DIR)/$(MUTT_BINARY) $@
 	$(STRIPCMD) $(STRIP_STRIP_ALL) $@
+
+mutt-source: $(DL_DIR)/$(MUTT_SOURCE) $(DL_DIR)/$(MUTT_PATCH)
+
+mutt-unpacked: $(MUTT_DIR)/.unpacked
 
 mutt: uclibc ncurses $(TARGET_DIR)/$(MUTT_TARGET_BINARY)
 
