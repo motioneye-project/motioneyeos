99304e81e7be50ee5c17963e638918243bb9d7ed
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2008-05-20 21:35:55 +0000

    Update lockfile-progs version

--- package/lockfile-progs/lockfile-progs.mk
+++ package/lockfile-progs/lockfile-progs.mk
@@ -3,11 +3,11 @@
 # lockfile-progs
 #
 #############################################################
-LOCKFILE_PROGS_VERSION=0.1.11
+LOCKFILE_PROGS_VERSION=0.1.11-0.1
 LOCKFILE_PROGS_SOURCE:=lockfile-progs_$(LOCKFILE_PROGS_VERSION).tar.gz
 LOCKFILE_PROGS_SITE:=$(BR2_DEBIAN_MIRROR)/debian/pool/main/l/lockfile-progs/
 LOCKFILE_PROGS_CAT:=$(ZCAT)
-LOCKFILE_PROGS_DIR:=$(BUILD_DIR)/sid
+LOCKFILE_PROGS_DIR:=$(BUILD_DIR)/lockfile-progs-0.1.11
 LOCKFILE_PROGS_BINARY:=usr/bin/lockfile-create
 
 $(DL_DIR)/$(LOCKFILE_PROGS_SOURCE):
