d11272f159b77d11446f5450dee2b9e093e3be7a
Arnar Mar Sigurðsson <antab@valka.is>
2008-06-01 18:08:48 +0000

    Added database menu to config (maybe move the other databases under it to remove the mess from the main package menu) Added mysql 5.1 client package, can be used to provide mysql support for PHP. Note: This package has only been tested on AVR32.

---
+++ package/database/database.mk
@@ -0,0 +1,1 @@
+include package/database/*/*.mk
---
+++ package/database/mysql_client/mysql_client.mk
@@ -0,0 +1,30 @@
+#############################################################
+#
+# MySQL 5.1 Client
+#
+#############################################################
+MYSQL_CLIENT_VERSION = 5.1.23-rc
+MYSQL_CLIENT_SOURCE = mysql-$(MYSQL_CLIENT_VERSION).tar.gz
+MYSQL_CLIENT_SITE = http://mirrors.24-7-solutions.net/pub/mysql/Downloads/MySQL-5.1
+MYSQL_CLIENT_INSTALL_TARGET = YES
+MYSQL_CLIENT_INSTALL_STAGING = YES
+MYSQL_CLIENT_DEPENDENCIES = uclibc readline
+
+MYSQL_CLIENT_CONF_ENV = ac_cv_sys_restartable_syscalls=yes
+MYSQL_CLIENT_CONF_OPT = \
+	--target=$(GNU_TARGET_NAME) \
+	--host=$(GNU_TARGET_NAME) \
+	--build=$(GNU_HOST_NAME) \
+	--program-prefix="" \
+	--prefix=/usr \
+	--without-ndb-binlog \
+	--without-server \
+	--without-docs \
+	--without-man \
+	--without-readline \
+	--without-libedit \
+	--with-low-memory \
+	--enable-thread-safe-client \
+	$(ENABLE_DEBUG)
+
+$(eval $(call AUTOTARGETS,package,mysql_client))
