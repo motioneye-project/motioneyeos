8ba9a9971c7f59e14de76786445b9a9349e4b73e
Peter Korsgaard <jacmet@sunsite.dk>
2008-06-26 14:04:39 +0000

    fontconfig: bump version

--- package/fontconfig/fontconfig.mk
+++ package/fontconfig/fontconfig.mk
@@ -3,7 +3,7 @@
 # fontconfig
 #
 #############################################################
-FONTCONFIG_VERSION = 2.4.2
+FONTCONFIG_VERSION = 2.6.0
 FONTCONFIG_SOURCE = fontconfig-$(FONTCONFIG_VERSION).tar.gz
 FONTCONFIG_SITE = http://fontconfig.org/release
 FONTCONFIG_AUTORECONF = NO
@@ -14,17 +14,12 @@ FONTCONFIG_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
 FONTCONFIG_CONF_OPT = --target=$(GNU_TARGET_NAME) --host=$(GNU_TARGET_NAME) \
 		--build=$(GNU_HOST_NAME) --with-arch=$(GNU_TARGET_NAME) \
-		--prefix=/usr --exec-prefix=/usr \
-		--bindir=/usr/bin --sbindir=/usr/sbin \
-		--libdir=/usr/lib --libexecdir=/usr/lib \
-		--sysconfdir=/etc --datadir=/usr/share \
-		--localstatedir=/var --includedir=/usr/include \
-		--mandir=/usr/man --infodir=/usr/info \
+		--prefix=/usr --sysconfdir=/etc \
 		--with-freetype-config="$(STAGING_DIR)/usr/bin/freetype-config" \
 		--with-expat="$(STAGING_DIR)/usr/lib" \
 		--with-expat-lib=$(STAGING_DIR)/usr/lib \
 		--with-expat-includes=$(STAGING_DIR)/usr/include \
-		--disable-docs 
+		--disable-docs
 
 FONTCONFIG_DEPENDENCIES = uclibc freetype expat
 
