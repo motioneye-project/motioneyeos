ec771579451c54c8590800ca1803cd6bfcc9a116
Peter Korsgaard <jacmet@sunsite.dk>
2008-07-02 07:20:16 +0000

    ifplugd: only install default config if not provided in skeleton

--- package/ifplugd/ifplugd.mk
+++ package/ifplugd/ifplugd.mk
@@ -18,12 +18,13 @@ $(eval $(call AUTOTARGETS,package,ifplugd))
 
 $(IFPLUGD_HOOK_POST_INSTALL):
 	$(INSTALL) -d $(TARGET_DIR)/etc/ifplugd
-	$(INSTALL) $(IFPLUGD_DIR)/conf/ifplugd.conf $(TARGET_DIR)/etc/ifplugd/
+	@if [ ! -f $(TARGET_DIR)/etc/ifplugd/ifplugd.conf ]; then \
+		$(INSTALL) $(IFPLUGD_DIR)/conf/ifplugd.conf $(TARGET_DIR)/etc/ifplugd/; \
+		$(SED) 's^\(ARGS=.*\)w^\1^' $(TARGET_DIR)/etc/ifplugd/ifplugd.conf; \
+	fi
 	$(INSTALL) -m 0755 $(IFPLUGD_DIR)/conf/ifplugd.action \
 		$(TARGET_DIR)/etc/ifplugd/
 	$(INSTALL) -m 0755 $(IFPLUGD_DIR)/conf/ifplugd.init \
 		$(TARGET_DIR)/etc/init.d/S45ifplugd
-	# continue booting without waiting for fork (no -w option)
-	$(SED) 's^\(ARGS=.*\)w^\1^' $(TARGET_DIR)/etc/ifplugd/ifplugd.conf
 	# don't use bash for init script
 	$(SED) 's^/bin/bash^/bin/sh^g' $(TARGET_DIR)/etc/init.d/S45ifplugd
