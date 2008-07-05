e156f34a4a5bb828f2309ece2af95750a98f9d0e
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2008-07-05 08:20:34 +0000

    [0002974] add support for zlib and bzip2 in lighttpd, use alphabetical order for lighttpd config

--- package/lighttpd/lighttpd.mk
+++ package/lighttpd/lighttpd.mk
@@ -30,6 +30,20 @@ else
 LIGHTTPD_CONF_OPT += --without-openssl
 endif
 
+ifeq ($(strip $(BR2_PACKAGE_LIGHTTPD_ZLIB)),y)
+LIGHTTPD_DEPENDENCIES += zlib
+LIGHTTPD_CONF_OPT += --with-zlib
+else
+LIGHTTPD_CONF_OPT += --without-zlib
+endif
+
+ifeq ($(strip $(BR2_PACKAGE_LIGHTTPD_BZIP2)),y)
+LIGHTTPD_DEPENDENCIES += bzip2
+LIGHTTPD_CONF_OPT += --with-bzip2
+else
+LIGHTTPD_CONF_OPT += --without-bzip2
+endif
+
 ifeq ($(strip $(BR2_PACKAGE_LIGHTTPD_PCRE)),y)
 LIGHTTPD_CONF_ENV += PCRE_LIB="-lpcre"
 LIGHTTPD_DEPENDENCIES += pcre
