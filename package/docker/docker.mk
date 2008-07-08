b28921cd329786e0de21ccf98efebe97f617bbb9
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2008-07-08 05:41:44 +0000

    Add glib2.0 specific include/pkconfig paths in 'docker'

--- package/docker/docker.mk
+++ package/docker/docker.mk
@@ -11,10 +11,13 @@ DOCKER_INSTALL_STAGING = NO
 DOCKER_INSTALL_TARGET = YES
 
 DOCKER_MAKE_OPT = CC=$(TARGET_CC) CXX=$(TARGET_CXX) LD=$(TARGET_LD) \
-					CFLAGS="-I$(STAGING_DIR)/usr/include" \
+					CFLAGS="-I$(STAGING_DIR)/usr/include \
+						-I$(STAGING_DIR)/usr/include/glib-2.0 \
+						-I$(STAGING_DIR)/usr/lib/glib-2.0/include" \
 					LDFLAGS="-L$(STAGING_DIR)/usr/lib -L$(STAGING_DIR)/lib" \
 					PKGCONFIG="$(STAGING_DIR)/usr/bin/pkg-config" \
-					PKG_CONFIG_SYSROOT="$(STAGING_DIR)"
+					PKG_CONFIG_SYSROOT="$(STAGING_DIR)" \
+					PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig"
 
 DOCKER_INSTALL_TARGET_OPT = PREFIX=$(TARGET_DIR)/usr install
 
