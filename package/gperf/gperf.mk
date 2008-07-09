2685f5b13907d455881673df583c1a0084bb35e5
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2008-07-09 04:27:45 +0000

    Add gperf, perfect hash generator

---
+++ package/gperf/gperf.mk
@@ -0,0 +1,22 @@
+#############################################################
+#
+# gperf
+#
+#############################################################
+GPERF_VERSION = 3.0.3
+GPERF_SOURCE = gperf-$(GPERF_VERSION).tar.gz
+GPERF_SITE = $(BR2_GNU_MIRROR)/gperf
+GPERF_AUTORECONF = NO
+GPERF_INSTALL_STAGING = YES
+GPERF_INSTALL_TARGET = YES
+GPERF_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) install 
+GPERF_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install 
+
+GPERF_CONF_ENV = 
+
+GPERF_CONF_OPT = 
+
+GPERF_DEPENDENCIES = uclibc
+
+$(eval $(call AUTOTARGETS,package,gperf))
+
