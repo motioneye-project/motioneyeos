bea4968d34c069ab2719263286cef9f08624aa45
Peter Korsgaard <jacmet@sunsite.dk>
2008-08-04 19:08:38 +0000

    haserl: convert to Makefile.autotools.in format

--- package/haserl/haserl.mk
+++ package/haserl/haserl.mk
@@ -8,54 +8,15 @@ HASERL_VERSION:=$(strip $(subst ",,$(BR2_PACKAGE_HASERL_VERSION)))
 #"))
 HASERL_SOURCE:=haserl-$(HASERL_VERSION).tar.gz
 HASERL_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/haserl/
-HASERL_DIR:=$(BUILD_DIR)/haserl-$(HASERL_VERSION)
-HASERL_CAT:=$(ZCAT)
-HASERL_BINARY:=usr/bin/haserl
+HASERL_AUTORECONF:=no
+HASERL_INSTALL_STAGING:=NO
+HASERL_INSTALL_TARGET:=YES
+HASERL_INSTALL_TARGET_OPT:=DESTDIR=$(TARGET_DIR) STRIPPROG='$(STRIPCMD)' install-strip
+HASERL_UNINSTALL_TARGET_OPT:=DESTDIR=$(TARGET_DIR) uninstall
 
-$(DL_DIR)/$(HASERL_SOURCE):
-	$(WGET) -P $(DL_DIR) $(HASERL_SITE)/$(HASERL_SOURCE)
+# force haserl 0.8.0 to use install-sh so stripping works
+HASERL_CONF_ENV = ac_cv_path_install=./install-sh
 
-$(HASERL_DIR)/.unpacked: $(DL_DIR)/$(HASERL_SOURCE)
-	$(HASERL_CAT) $(DL_DIR)/$(HASERL_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(HASERL_DIR) package/haserl/ haserl-$(HASERL_VERSION)\*.patch
-	touch $@
+HASERL_DEPENDENCIES:=uclibc
 
-$(HASERL_DIR)/.configured: $(HASERL_DIR)/.unpacked
-	(cd $(HASERL_DIR); rm -rf config.cache; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		./configure \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--prefix=/usr \
-		--sysconfdir=/etc \
-	)
-	touch $@
-
-$(HASERL_DIR)/src/haserl: $(HASERL_DIR)/.configured
-	$(MAKE) CC=$(TARGET_CC) -C $(HASERL_DIR)
-
-$(TARGET_DIR)/$(HASERL_BINARY): $(HASERL_DIR)/src/haserl
-	cp $^ $@
-
-haserl: uclibc $(TARGET_DIR)/$(HASERL_BINARY)
-
-haserl-source: $(DL_DIR)/$(HASERL_SOURCE)
-
-haserl-unpacked: $(HASERL_DIR)/.unpacked
-
-haserl-clean:
-	-$(MAKE) -C $(HASERL_DIR) clean
-	rm -f $(TARGET_DIR)/$(HASERL_BINARY)
-
-haserl-dirclean:
-	rm -rf $(HASERL_DIR)
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(strip $(BR2_PACKAGE_HASERL)),y)
-TARGETS+=haserl
-endif
+$(eval $(call AUTOTARGETS,package,haserl))
