9b28d48012c33b37d33101991fa042f9fc9d76eb
Peter Korsgaard <jacmet@sunsite.dk>
2008-08-16 12:32:54 +0000

    pcre: fix compilation when C++ support is disabled
    pcre will use host g++ is no cross g++ is available, so explicitly disable
    c++ support in pcre if it isn't enabled in buildroot instead.

--- package/pcre/pcre.mk
+++ package/pcre/pcre.mk
@@ -9,9 +9,10 @@ PCRE_SITE = ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre
 PCRE_INSTALL_STAGING = YES
 PCRE_INSTALL_TARGET = YES
 
-PCRE_CONF_OPT = --target=$(GNU_TARGET_NAME) --host=$(GNU_TARGET_NAME) \
-        --build=$(GNU_HOST_NAME) --prefix=/usr \
-        --includedir=/usr/include
+ifneq ($(BR2_INSTALL_LIBSTDCPP),y)
+# pcre will use the host g++ if a cross version isn't available
+PCRE_CONF_OPT = --disable-cpp
+endif
 
 PCRE_DEPENDENCIES = uclibc
 
