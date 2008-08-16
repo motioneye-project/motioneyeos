c86db3448617822d163cbd3b0e6fef39df36d5fc
Peter Korsgaard <jacmet@sunsite.dk>
2008-08-16 19:36:36 +0000

    pcre: fix target install
    Use install-strip and clean up documentation files instead of
    install-exec so files get stripped and manpages installed if
    requested.

--- package/pcre/pcre.mk
+++ package/pcre/pcre.mk
@@ -8,6 +8,7 @@ PCRE_SOURCE = pcre-$(PCRE_VERSION).tar.bz2
 PCRE_SITE = ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre
 PCRE_INSTALL_STAGING = YES
 PCRE_INSTALL_TARGET = YES
+PCRE_INSTALL_TARGET_OPT:=DESTDIR=$(TARGET_DIR) install-strip
 
 ifneq ($(BR2_INSTALL_LIBSTDCPP),y)
 # pcre will use the host g++ if a cross version isn't available
@@ -17,3 +18,7 @@ endif
 PCRE_DEPENDENCIES = uclibc
 
 $(eval $(call AUTOTARGETS,package,pcre))
+
+$(PCRE_HOOK_POST_INSTALL): $(PCRE_TARGET_INSTALL_TARGET)
+	rm -rf $(TARGET_DIR)/usr/share/doc/pcre
+	touch $@
