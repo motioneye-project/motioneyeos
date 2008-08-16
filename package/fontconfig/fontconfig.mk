3c10da1528ce639d75e07404604a8f5ed262194d
Peter Korsgaard <jacmet@sunsite.dk>
2008-08-16 19:13:53 +0000

    fontconfig: remove redundant configure args
    Patch by antialize / bug #4524.

--- package/fontconfig/fontconfig.mk
+++ package/fontconfig/fontconfig.mk
@@ -12,9 +12,7 @@ FONTCONFIG_INSTALL_TARGET = YES
 
 FONTCONFIG_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
-FONTCONFIG_CONF_OPT = --target=$(GNU_TARGET_NAME) --host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) --with-arch=$(GNU_TARGET_NAME) \
-		--prefix=/usr --sysconfdir=/etc \
+FONTCONFIG_CONF_OPT = --with-arch=$(GNU_TARGET_NAME) \
 		--with-freetype-config="$(STAGING_DIR)/usr/bin/freetype-config" \
 		--with-expat="$(STAGING_DIR)/usr/lib" \
 		--with-expat-lib=$(STAGING_DIR)/usr/lib \
