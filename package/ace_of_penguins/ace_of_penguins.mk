1d91b56a78679cf65750b82509ceb9ab6948565b
Peter Korsgaard <jacmet@sunsite.dk>
2008-08-21 13:10:27 +0000

    ace_of_penguins: remove redundant configure settings
    Reported by Hartleys.

--- package/ace_of_penguins/ace_of_penguins.mk
+++ package/ace_of_penguins/ace_of_penguins.mk
@@ -11,10 +11,6 @@ ACE_OF_PENGUINS_AUTORECONF = YES
 ACE_OF_PENGUINS_STAGING = NO
 ACE_OF_PENGUINS_TARGET = YES
 
-ACE_OF_PENGUINS_CONF_OPT = --target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) --build=$(GNU_HOST_NAME) \
-		--prefix=/usr --sysconfdir=/etc
-
 ACE_OF_PENGUINS_DEPENDENCIES = uclibc libpng $(XSERVER)
 
 $(eval $(call AUTOTARGETS,package,ace_of_penguins))
