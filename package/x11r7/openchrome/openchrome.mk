0509df7e7b5b8f77d91b66a1bc136757b9ea33f7
Peter Korsgaard <jacmet@sunsite.dk>
2008-08-22 14:42:00 +0000

    openchrome: fix install
    Closes #4674.

--- package/x11r7/openchrome/openchrome.mk
+++ package/x11r7/openchrome/openchrome.mk
@@ -9,6 +9,8 @@ OPENCHROME_SITE = http://www.openchrome.org/releases
 
 OPENCHROME_DEPENDENCIES = xserver_xorg-server libdrm xlib_libX11 xlib_libXvMC xproto_fontsproto xproto_glproto xproto_randrproto xproto_renderproto xproto_xextproto xproto_xf86driproto xproto_xproto
 
+OPENCHROME_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
+
 OPENCHROME_AUTORECONF = YES
 OPENCHROME_CONF_OPT = --enable-shared --disable-static
 
