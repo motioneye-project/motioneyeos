402042a7ed48f8eafbd8c5e75c3c823071077954
Peter Korsgaard <jacmet@sunsite.dk>
2008-08-25 09:04:49 +0000

    psmisc: bump version

--- package/psmisc/psmisc.mk
+++ package/psmisc/psmisc.mk
@@ -3,7 +3,7 @@
 # psmisc
 #
 #############################################################
-PSMISC_VERSION:=22.2
+PSMISC_VERSION:=22.6
 PSMISC_SOURCE:=psmisc-$(PSMISC_VERSION).tar.gz
 PSMISC_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/psmisc
 PSMISC_AUTORECONF:=no
