1fff6d9d3d8aed70d2d77f98f2f0d0f02641bab5
Peter Korsgaard <jacmet@sunsite.dk>
2008-08-25 09:05:00 +0000

    psmic: locale support
    Closes #4274.

--- package/psmisc/psmisc.mk
+++ package/psmisc/psmisc.mk
@@ -15,4 +15,10 @@ PSMISC_CONF_ENV:=ac_cv_func_malloc_0_nonnull=yes \
 PSMISC_CONF_OPT:= $(DISABLE_NLS) $(DISABLE_IPV6)
 PSMISC_DEPENDENCIES:=uclibc ncurses
 
+ifeq ($(BR2_ENABLE_LOCALE),y)
+# psmisc gets confused and forgets to link with libintl
+PSMISC_MAKE_OPT:=LIBS=-lintl
+PSMISC_DEPENDENCIES+= gettext libintl
+endif
+
 $(eval $(call AUTOTARGETS,package,psmisc))
