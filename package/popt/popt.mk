a386f63c5796e30c15d510737343f4df2540f44f
Peter Korsgaard <jacmet@sunsite.dk>
2008-08-26 21:05:10 +0000

    popt: use := assignments

--- package/popt/popt.mk
+++ package/popt/popt.mk
@@ -3,13 +3,13 @@
 # popt
 #
 #############################################################
-POPT_VERSION = 1.14
-POPT_SITE = http://rpm5.org/files/popt
-POPT_INSTALL_STAGING = YES
+POPT_VERSION:=1.14
+POPT_SITE:=http://rpm5.org/files/popt
+POPT_INSTALL_STAGING:=YES
 POPT_INSTALL_TARGET:=YES
-POPT_CONF_ENV = ac_cv_va_copy=yes
+POPT_CONF_ENV:=ac_cv_va_copy=yes
+POPT_INSTALL_TARGET_OPT:=DESTDIR=$(TARGET_DIR) install-strip
 
-POPT_INSTALL_TARGET_OPT=DESTDIR=$(TARGET_DIR) install-strip
-
+POPT_DEPENDENCIES:=uclibc
 
 $(eval $(call AUTOTARGETS,package,popt))
