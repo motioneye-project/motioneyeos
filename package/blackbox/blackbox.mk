91fca34777a55e056d3d3905d6b9801d1ce54d81
Peter Korsgaard <jacmet@sunsite.dk>
2008-08-26 20:46:51 +0000

    blackbox: use BR2_SOURCEFORGE_MIRROR
    Don't hardcode specific sourceforge mirrors.

--- package/blackbox/blackbox.mk
+++ package/blackbox/blackbox.mk
@@ -6,7 +6,7 @@
 
 BLACKBOX_VERSION = 0.70.1
 BLACKBOX_SOURCE = blackbox-$(BLACKBOX_VERSION).tar.bz2
-BLACKBOX_SITE = http://superb-west.dl.sourceforge.net/sourceforge/blackboxwm/
+BLACKBOX_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/blackboxwm/
 BLACKBOX_AUTORECONF = NO
 BLACKBOX_INSTALL_STAGING = NO
 BLACKBOX_INSTALL_TARGET = YES
