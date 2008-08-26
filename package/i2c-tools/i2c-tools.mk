6b76f0608a3c46f66b1d223cf4fa75f009e1c41f
Daniel Laird <danieljlaird@hotmail.com>
2008-08-26 14:01:37 +0000

    reverse changes to i2c-tools.mk until I tidy it up (get clean working for example) Still feel a good change to make as some of the suggestions recently made it look like Makefile.autotools.in might add support for installing a list of binaries etc and this would benefit this package.

--- package/i2c-tools/i2c-tools.mk
+++ package/i2c-tools/i2c-tools.mk
@@ -6,22 +6,45 @@
 I2C_TOOLS_VERSION:=3.0.1
 I2C_TOOLS_SOURCE:=i2c-tools-$(I2C_TOOLS_VERSION).tar.bz2
 I2C_TOOLS_SITE:=http://dl.lm-sensors.org/i2c-tools/releases/
-I2C_TOOLS_AUTORECONF = NO
-I2C_TOOLS_INSTALL_STAGING = YES
-I2C_TOOLS_CONF_ENV =
-I2C_TOOLS_CONF_OPT =
-I2C_TOOLS_MAKE_OPT = $(TARGET_CONFIGURE_OPTS)
-I2C_TOOLS_DEPENDENCIES =
+I2C_TOOLS_DIR:=$(BUILD_DIR)/i2c-tools-$(I2C_TOOLS_VERSION)
+I2C_TOOLS_BINARY:=tools/i2cdetect
+I2C_TOOLS_TARGET_BINARY:=usr/bin/i2cdetect
 
-$(eval $(call AUTOTARGETS,package,i2c-tools))
+$(DL_DIR)/$(I2C_TOOLS_SOURCE):
+	$(WGET) -P $(DL_DIR) $(I2C_TOOLS_SITE)/$(I2C_TOOLS_SOURCE)
 
-$(I2C_TOOLS_TARGET_CONFIGURE):
+$(I2C_TOOLS_DIR)/.unpacked: $(DL_DIR)/$(I2C_TOOLS_SOURCE)
+	$(BZCAT) $(DL_DIR)/$(I2C_TOOLS_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	toolchain/patch-kernel.sh $(I2C_TOOLS_DIR) package/i2c-tools/ i2c-tools-$(I2C_TOOLS_VERSION)\*.patch
 	touch $@
 
-$(I2C_TOOLS_TARGET_INSTALL_TARGET):
+$(I2C_TOOLS_DIR)/$(I2C_TOOLS_BINARY): $(I2C_TOOLS_DIR)/.unpacked
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(I2C_TOOLS_DIR)
+
+$(TARGET_DIR)/$(I2C_TOOLS_TARGET_BINARY): $(I2C_TOOLS_DIR)/$(I2C_TOOLS_BINARY)
+	$(INSTALL) -m 755 -d $(@D)
 	for i in i2cdump i2cget i2cset i2cdetect; \
 	do \
-		$(INSTALL) -m 755 $(I2C_TOOLS_DIR)/tools/$$i $(TARGET_DIR)/usr/bin; \
-		$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/bin/$$i; \
+		$(INSTALL) -m 755 $(<D)/$$i $(@D); \
+		$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(@D)/$$i; \
 	done
-	touch $@
+
+i2c-tools: uclibc $(TARGET_DIR)/$(I2C_TOOLS_TARGET_BINARY)
+
+i2c-tools-source: $(DL_DIR)/$(I2C_TOOLS_SOURCE)
+
+i2c-tools-clean:
+	rm -f $(addprefix $(TARGET_DIR)/usr/bin/,i2cdump i2cget i2cset i2cdetect)
+	-$(MAKE) -C $(I2C_TOOLS_DIR) clean
+
+i2c-tools-dirclean:
+	rm -rf $(I2C_TOOLS_DIR)
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(strip $(BR2_PACKAGE_I2C_TOOLS)),y)
+TARGETS+=i2c-tools
+endif
