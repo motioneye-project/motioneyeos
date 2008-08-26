3e1ed2feb00d3b4901d971d5cfbf7996a47ea36c
Peter Korsgaard <jacmet@sunsite.dk>
2008-08-26 20:47:09 +0000

    neon: bump version
    Fixes CVE-2008-3746.

--- package/neon/neon.mk
+++ package/neon/neon.mk
@@ -3,7 +3,7 @@
 # neon
 #
 #############################################################
-NEON_VERSION = 0.28.2
+NEON_VERSION = 0.28.3
 NEON_SITE = http://www.webdav.org/neon/
 NEON_INSTALL_STAGING = YES
 
