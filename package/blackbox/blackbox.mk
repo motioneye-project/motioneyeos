5fb25de717e743eefb390dc7e2223204e10a8e69
Peter Korsgaard <jacmet@sunsite.dk>
2008-08-26 20:47:05 +0000

    blackbox: use := assignments

--- package/blackbox/blackbox.mk
+++ package/blackbox/blackbox.mk
@@ -4,17 +4,17 @@
 #
 #############################################################
 
-BLACKBOX_VERSION = 0.70.1
-BLACKBOX_SOURCE = blackbox-$(BLACKBOX_VERSION).tar.bz2
-BLACKBOX_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/blackboxwm/
-BLACKBOX_AUTORECONF = NO
-BLACKBOX_INSTALL_STAGING = NO
-BLACKBOX_INSTALL_TARGET = YES
-BLACKBOX_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install-strip
+BLACKBOX_VERSION:=0.70.1
+BLACKBOX_SOURCE:=blackbox-$(BLACKBOX_VERSION).tar.bz2
+BLACKBOX_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/blackboxwm/
+BLACKBOX_AUTORECONF:=NO
+BLACKBOX_INSTALL_STAGING:=NO
+BLACKBOX_INSTALL_TARGET:=YES
+BLACKBOX_INSTALL_TARGET_OPT:=DESTDIR=$(TARGET_DIR) install-strip
 
-BLACKBOX_CONF_OPT = --x-includes=$(STAGING_DIR)/usr/include/X11 \
+BLACKBOX_CONF_OPT:=--x-includes=$(STAGING_DIR)/usr/include/X11 \
 		--x-libraries=$(STAGING_DIR)/usr/lib
 
-BLACKBOX_DEPENDENCIES = uclibc $(XSERVER)
+BLACKBOX_DEPENDENCIES:=uclibc $(XSERVER)
 
 $(eval $(call AUTOTARGETS,package,blackbox))
