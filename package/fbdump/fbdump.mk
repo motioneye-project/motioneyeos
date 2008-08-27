f10aecd134cf82a20540fd31fc742add167e6167
Daniel Laird <danieljlaird@hotmail.com>
2008-08-27 11:01:16 +0000

    fbdump: Add new package fbdump.
    fbdump is a simple tool to capture snapshots from the Linux kernel framebuffer 
    device and write them out as a PPM file. Currently, most packed-pixel framebuffer
    formats and the vga16 framebuffer are supported.

---
+++ package/fbdump/fbdump.mk
@@ -0,0 +1,16 @@
+#############################################################
+#
+# fbdump
+#
+#############################################################
+FBDUMP_VERSION:=0.4.2
+FBDUMP_SOURCE:=fbdump-$(FBDUMP_VERSION).tar.gz
+FBDUMP_SITE:=http://www.rcdrummond.net/fbdump
+FBDUMP_AUTORECONF = NO
+FBDUMP_CONF_ENV = ac_cv_func_malloc_0_nonnull=yes
+FBDUMP_INSTALL_STAGING = YES
+FBDUMP_INSTALL_TARGET = YES
+FBDUMP_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install-strip
+
+$(eval $(call AUTOTARGETS,package,fbdump))
+
