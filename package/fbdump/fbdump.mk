7eb4c6a0a9977c5688d499b48b72971b4a4019c8
Daniel Laird <danieljlaird@hotmail.com>
2008-08-27 11:40:02 +0000

    fbdump: Fix help text indentation and remove staging dir installation
    Tidy up fbdump package.

--- package/fbdump/fbdump.mk
+++ package/fbdump/fbdump.mk
@@ -8,7 +8,6 @@ FBDUMP_SOURCE:=fbdump-$(FBDUMP_VERSION).tar.gz
 FBDUMP_SITE:=http://www.rcdrummond.net/fbdump
 FBDUMP_AUTORECONF = NO
 FBDUMP_CONF_ENV = ac_cv_func_malloc_0_nonnull=yes
-FBDUMP_INSTALL_STAGING = YES
 FBDUMP_INSTALL_TARGET = YES
 FBDUMP_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install-strip
 
