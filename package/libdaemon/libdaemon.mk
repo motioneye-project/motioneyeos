73ec8a1d7e81533dce71936ae922d38da404de99
Peter Korsgaard <jacmet@sunsite.dk>
2008-08-27 07:29:16 +0000

    libdaemon: convert to Makefile.autotools.in format

--- package/libdaemon/libdaemon.mk
+++ package/libdaemon/libdaemon.mk
@@ -3,98 +3,17 @@
 # libdaemon (UNIX daemon library)
 #
 #############################################################
-# Copyright 2003-2005 Lennart Poettering <mzqnrzba@0pointer.de>
-#
-# This library is free software; you can redistribute it
-# and/or modify it under the terms of the GNU Lesser General
-# Public License as published by the Free Software Foundation
-# either version 2.1 of the License, or (at your option) any
-# later version.
 
 LIBDAEMON_VERSION:=0.13
-LIBDAEMON_NAME:=libdaemon-$(LIBDAEMON_VERSION)
-LIBDAEMON_DIR:=$(BUILD_DIR)/$(LIBDAEMON_NAME)
+LIBDAEMON_SOURCE:=libdaemon-$(LIBDAEMON_VERSION).tar.gz
 LIBDAEMON_SITE:=http://0pointer.de/lennart/projects/libdaemon/
-LIBDAEMON_SOURCE:=$(LIBDAEMON_NAME).tar.gz
-LIBDAEMON_DESTDIR:=$(STAGING_DIR)/usr/lib
-LIBDAEMON_CAT:=$(ZCAT)
+LIBDAEMON_AUTORECONF:=no
+LIBDAEMON_INSTALL_STAGING:=YES
+LIBDAEMON_INSTALL_TARGET:=YES
+LIBDAEMON_INSTALL_TARGET_OPT:=DESTDIR=$(TARGET_DIR) install-strip
+LIBDAEMON_CONF_ENV:=ac_cv_func_setpgrp_void=no
+LIBDAEMON_CONF_OPT:=--disable-lynx $(DISABLE_NLS) $(DISABLE_LARGEFILE)
 
-$(DL_DIR)/$(LIBDAEMON_SOURCE):
-	 $(WGET) -P $(DL_DIR) $(LIBDAEMON_SITE)/$(LIBDAEMON_SOURCE)
+LIBDAEMON_DEPENDENCIES:=uclibc pkgconfig
 
-libdaemon-source: $(DL_DIR)/$(LIBDAEMON_SOURCE)
-
-libdaemon-unpacked: $(LIBDAEMON_DIR)/.unpacked
-$(LIBDAEMON_DIR)/.unpacked: $(DL_DIR)/$(LIBDAEMON_SOURCE)
-	$(LIBDAEMON_CAT) $(DL_DIR)/$(LIBDAEMON_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(LIBDAEMON_DIR) package/libdaemon/ \*.patch
-	touch $@
-
-libdaemon-configured: $(LIBDAEMON_DIR)/.configured
-$(LIBDAEMON_DIR)/.configured: $(LIBDAEMON_DIR)/.unpacked
-	(cd $(LIBDAEMON_DIR) && rm -rf config.cache && libtoolize --copy --force && autoreconf)
-	$(CONFIG_UPDATE) $(LIBDAEMON_DIR)
-	(cd $(LIBDAEMON_DIR) && \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		./configure \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--prefix=/usr \
-		--exec-prefix=/usr \
-		--bindir=/usr/bin \
-		--sbindir=/usr/sbin \
-		--libdir=/usr/lib \
-		--libexecdir=/usr/lib \
-		--sysconfdir=/etc \
-		--datadir=/usr/share \
-		--localstatedir=/var \
-		--includedir=/usr/include \
-		--mandir=/usr/share/man \
-		--infodir=/usr/share/info \
-		$(DISABLE_NLS) \
-		$(DISABLE_LARGEFILE) \
-		--disable-lynx \
-		--disable-shared \
-	)
-	touch $@
-
-$(LIBDAEMON_DIR)/.compiled: $(LIBDAEMON_DIR)/.configured
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) LIBTOOL=$(LIBDAEMON_DIR)/libtool -C $(LIBDAEMON_DIR)
-	touch $@
-
-$(LIBDAEMON_DESTDIR)/libdaemon.a: $(LIBDAEMON_DIR)/.compiled
-	$(MAKE) DESTDIR=$(STAGING_DIR) -C $(LIBDAEMON_DIR) install
-	$(SED) "s,^libdir=.*,libdir=\'$(STAGING_DIR)/usr/lib\',g" $(STAGING_DIR)/usr/lib/libdaemon.la
-	touch -c $@
-
-#$(TARGET_DIR)/usr/lib/libdaemon.a: $(LIBDAEMON_DESTDIR)/libdaemon.a
-# -$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libdaemon.a
-
-libdaemon: uclibc pkgconfig $(LIBDAEMON_DESTDIR)/libdaemon.a
-
-libdaemon-unpacked: $(LIBDAEMON_DIR)/.unpacked
-
-libdaemon-clean:
-	-$(MAKE) -C $(LIBDAEMON_DIR) clean
-
-libdaemon-patch-prep: libdaemon-dirclean libdaemon-unpacked
-	cp -af $(LIBDAEMON_DIR) $(LIBDAEMON_DIR)-0rig
-
-libdaemon-patch:
-	(cd $(BUILD_DIR); \
-	diff -urN $(LIBDAEMON_NAME)-0rig $(LIBDAEMON_NAME) > ../../$(LIBDAEMON_NAME)-$(DATE).patch || echo)
-
-libdaemon-dirclean:
-	rm -rf $(LIBDAEMON_DIR)
-	rm -rf $(LIBDAEMON_DIR)-0rig
-
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(strip $(BR2_PACKAGE_LIBDAEMON)),y)
-TARGETS+=libdaemon
-endif
+$(eval $(call AUTOTARGETS,package,libdaemon))
