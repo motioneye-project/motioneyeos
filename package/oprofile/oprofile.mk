16091faf1e33f7508a1b286e922689904c39b33a
Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>
2008-09-17 08:51:54 +0000

    oprofile: convert oprofile.mk to use Makefile.autotools.in and bump version
    This patch bumps the version to 0.9.4 and converts the oprofile.mk to use
    Makefile.autotools.in. Patches against 0.9.3 are removed since they are no
    longer needed and a new patch for 0.9.4 is added.
    
    Building for all architectures should now also be possible.
    
    Signed-off-by: Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>
    Signed-off-by: Fathi Boudra <fboudra@gmail.com>

--- package/oprofile/oprofile.mk
+++ package/oprofile/oprofile.mk
@@ -3,72 +3,43 @@
 # oprofile
 #
 #############################################################
-OPROFILE_VERSION	:= 0.9.3
-OPROFILE_DIR		:= $(BUILD_DIR)/oprofile-$(OPROFILE_VERSION)
-OPROFILE_SITE		:= http://prdownloads.sourceforge.net/oprofile
-OPROFILE_SOURCE		:= oprofile-$(OPROFILE_VERSION).tar.gz
-OPROFILE_CAT		:= $(ZCAT)
+OPROFILE_VERSION := 0.9.4
+OPROFILE_CONF_OPT := --localstatedir=/var \
+		     --with-extra-includes="$(BUILD_DIR)/binutils-$(BR2_BINUTILS_VERSION)-target/bfd -I$(TOOL_BUILD_DIR)/binutils-$(BR2_BINUTILS_VERSION)/include" \
+		     --with-extra-libs=$(BUILD_DIR)/binutils-$(BR2_BINUTILS_VERSION)-target/bfd \
+		     --with-kernel-support
 
-OPROFILE_BINARIES	:= utils/ophelp
-OPROFILE_BINARIES	+= pp/opannotate pp/oparchive pp/opgprof pp/opreport
-OPROFILE_BINARIES	+= daemon/oprofiled
+OPROFILE_BINARIES := utils/ophelp
+OPROFILE_BINARIES += pp/opannotate pp/oparchive pp/opgprof pp/opreport opjitconv/opjitconv
+OPROFILE_BINARIES += daemon/oprofiled
 
-$(DL_DIR)/$(OPROFILE_SOURCE):
-	$(WGET) -P $(DL_DIR) $(OPROFILE_SITE)/$(OPROFILE_SOURCE)
+ifeq ($(BR2_powerpc),y)
+OPROFILE_ARCH := ppc
+endif
+ifeq ($(BR2_x86_64),y)
+OPROFILE_ARCH := x86-64
+endif
+ifeq ($(OPROFILE_ARCH),)
+OPROFILE_ARCH := $(BR2_ARCH)
+endif
 
-oprofile-source: $(DL_DIR)/$(OPROFILE_SOURCE)
+OPROFILE_DEPENDENCIES := popt binutils_target
 
-$(OPROFILE_DIR)/.unpacked: $(DL_DIR)/$(OPROFILE_SOURCE)
-	$(OPROFILE_CAT) $(DL_DIR)/$(OPROFILE_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(OPROFILE_DIR) package/oprofile/ \*.patch*
-	$(CONFIG_UPDATE) $(OPROFILE_DIR)
-	touch $@
+$(eval $(call AUTOTARGETS,package,oprofile))
 
-$(OPROFILE_DIR)/.configured: $(OPROFILE_DIR)/.unpacked
-	(cd $(OPROFILE_DIR); rm -f config.cache;		\
-		$(TARGET_CONFIGURE_OPTS)			\
-		$(TARGET_CONFIGURE_ARGS)			\
-		./configure					\
-			--target=$(GNU_TARGET_NAME)		\
-			--host=$(GNU_TARGET_NAME)		\
-			--build=$(GNU_HOST_NAME)		\
-			--prefix=/usr				\
-			--sysconfdir=/etc			\
-			--localstatedir=/var			\
-			--includedir=/include			\
-	);
-	touch $@
-
-$(OPROFILE_DIR)/daemon/oprofiled: $(OPROFILE_DIR)/.configured
-	PATH=$(TARGET_PATH) $(MAKE) -C $(OPROFILE_DIR)
-	touch -c $@
-
-$(TARGET_DIR)/usr/bin/oprofiled: $(OPROFILE_DIR)/daemon/oprofiled
+$(OPROFILE_TARGET_INSTALL_TARGET):
 	$(INSTALL) -d -m 755 $(TARGET_DIR)/usr/bin
-	$(INSTALL) -d -m 755 $(TARGET_DIR)/usr/share/oprofile/avr32
-	$(INSTALL) -m 644 $(addprefix $(OPROFILE_DIR)/events/avr32/, events unit_masks) $(TARGET_DIR)/usr/share/oprofile/avr32
+	$(INSTALL) -d -m 755 $(TARGET_DIR)/usr/share/oprofile
+	cp -dpfr $(OPROFILE_DIR)/events/$(OPROFILE_ARCH) $(TARGET_DIR)/usr/share/oprofile
 	$(INSTALL) -m 644 $(OPROFILE_DIR)/libregex/stl.pat $(TARGET_DIR)/usr/share/oprofile
 	$(INSTALL) -m 755 $(OPROFILE_DIR)/utils/opcontrol $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 $(addprefix $(OPROFILE_DIR)/, $(OPROFILE_BINARIES)) $(TARGET_DIR)/usr/bin
-	$(STRIPCMD) --strip-unneeded $(addprefix $(TARGET_DIR)/usr/bin/, $(notdir $(OPROFILE_BINARIES)))
-	touch -c $@
+	$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(addprefix $(TARGET_DIR)/usr/bin/, $(notdir $(OPROFILE_BINARIES)))
+	touch $@
 
-oprofile: uclibc popt binutils_target $(TARGET_DIR)/usr/bin/oprofiled
-
-oprofile-clean:
+$(OPROFILE_TARGET_CLEAN):
 	rm -f $(addprefix $(TARGET_DIR)/usr/bin/, $(notdir $(OPROFILE_BINARIES)))
 	rm -f $(TARGET_DIR)/usr/bin/opcontrol
 	rm -rf $(TARGET_DIR)/usr/share/oprofile
 	-$(MAKE) -C $(OPROFILE_DIR) clean
-
-oprofile-dirclean:
-	rm -rf $(OPROFILE_DIR)
-
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(strip $(BR2_PACKAGE_OPROFILE)),y)
-TARGETS		+= oprofile
-endif
+	touch $@
