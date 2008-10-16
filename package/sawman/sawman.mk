7ba44159bdce40745bd8563951d0592cca14e749
Daniel Laird <danieljlaird@hotmail.com>
2008-10-16 12:10:23 +0000

    package/sawman: Add Package SawMan
    SaWMan is a new window manager module for use with DirectFB. 
    Its main difference to the default module is that it allows one process 
    to be an application and window manager, 
    implementing all kinds of diversity, 
    while SaWMan is only the working horse. 
    
    Daniel Laird

---
+++ package/sawman/sawman.mk
@@ -0,0 +1,16 @@
+#############################################################
+#
+# SAWMAN
+#
+#############################################################
+SAWMAN_VERSION:=1.2.0-rc1
+SAWMAN_SOURCE:=SaWMan-$(SAWMAN_VERSION).tar.gz
+SAWMAN_SITE:=http://www.directfb.org/downloads/Extras
+SAWMAN_INSTALL_STAGING = YES
+SAWMAN_INSTALL_TARGET = YES
+SAWMAN_CONF_ENV = LDFLAGS="-L$(STAGING_DIR)/usr/lib -Wl,--rpath-link -Wl,$(STAGING_DIR)/usr/lib"
+SAWMAN_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) LDFLAGS=-L$(STAGING_DIR)/usr/lib install-strip
+SAWMAN_DEPENDENCIES = directfb
+
+$(eval $(call AUTOTARGETS,package,sawman))
+
