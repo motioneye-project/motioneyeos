78fa144ee33db5e78722128cb0f883a5cb96b44a
Peter Korsgaard <jacmet@sunsite.dk>
2008-10-19 21:48:52 +0000

    x11r7/libXt: use install-strip for target install
    Saves ~1.4 MB

--- package/x11r7/xlib_libXt/xlib_libXt.mk
+++ package/x11r7/xlib_libXt/xlib_libXt.mk
@@ -9,6 +9,8 @@ XLIB_LIBXT_SOURCE = libXt-$(XLIB_LIBXT_VERSION).tar.bz2
 XLIB_LIBXT_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXT_AUTORECONF = NO
 XLIB_LIBXT_INSTALL_STAGING = YES
+XLIB_LIBXT_INSTALL_TARGET = YES
+XLIB_LIBXT_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install-strip
 XLIB_LIBXT_DEPENDENCIES = xlib_libSM xlib_libX11 xproto_kbproto xproto_xproto xcb-proto libxcb
 XLIB_LIBXT_CONF_ENV = CC_FOR_BUILD="/usr/bin/gcc -I$(STAGING_DIR)/usr/include"
 XLIB_LIBXT_CONF_OPT = --disable-malloc0returnsnull --enable-shared --disable-static
