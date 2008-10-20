2f8d8843ad6d26bde7e779c44221c10bf81d3d97
Peter Korsgaard <jacmet@sunsite.dk>
2008-10-20 09:40:41 +0000

    x11r7/libXfont: use install-strip for target install
    Saves ~800 KB

--- package/x11r7/xlib_libXfont/xlib_libXfont.mk
+++ package/x11r7/xlib_libXfont/xlib_libXfont.mk
@@ -9,6 +9,8 @@ XLIB_LIBXFONT_SOURCE = libXfont-$(XLIB_LIBXFONT_VERSION).tar.bz2
 XLIB_LIBXFONT_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXFONT_AUTORECONF = NO
 XLIB_LIBXFONT_INSTALL_STAGING = YES
+XLIB_LIBXFONT_INSTALL_TARGET = YES
+XLIB_LIBXFONT_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install-strip
 XLIB_LIBXFONT_DEPENDENCIES = freetype xlib_libfontenc xlib_xtrans xproto_fontcacheproto xproto_fontsproto xproto_xproto xfont_encodings
 XLIB_LIBXFONT_CONF_OPT = --enable-shared --disable-static
 
