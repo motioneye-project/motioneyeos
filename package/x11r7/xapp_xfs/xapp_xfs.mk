79bdd27e5d4204748a50b2dac9d85d2fb64eb947
Peter Korsgaard <jacmet@sunsite.dk>
2008-10-21 18:50:49 +0000

    x11r7/xfs: bump version - fixes CVE-2007-4568
    For details, see:
    http://lists.freedesktop.org/archives/xorg-announce/2007-October/000416.html

--- package/x11r7/xapp_xfs/xapp_xfs.mk
+++ package/x11r7/xapp_xfs/xapp_xfs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XFS_VERSION = 1.0.4
+XAPP_XFS_VERSION = 1.0.5
 XAPP_XFS_SOURCE = xfs-$(XAPP_XFS_VERSION).tar.bz2
 XAPP_XFS_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XFS_AUTORECONF = NO
