ac71691272a0ea9902a200a4e60d0e3658f5f6b9
Peter Korsgaard <jacmet@sunsite.dk>
2008-10-26 09:14:06 +0000

    docker: s/PKGCONFIG/PKG_CONFIG/ to be consistent
    Patch by Markus Heidelberg.

--- package/docker/docker.mk
+++ package/docker/docker.mk
@@ -14,6 +14,7 @@ DOCKER_MAKE_OPT = CC=$(TARGET_CC) CXX=$(TARGET_CXX) LD=$(TARGET_LD) \
 					CFLAGS="-I$(STAGING_DIR)/usr/include \
 						-I$(STAGING_DIR)/usr/include/glib-2.0 \
 						-I$(STAGING_DIR)/usr/lib/glib-2.0/include" \
+					PKG_CONFIG="$(STAGING_DIR)/usr/bin/pkg-config" \
 					LDFLAGS="-L$(STAGING_DIR)/usr/lib -L$(STAGING_DIR)/lib"
 
 DOCKER_INSTALL_TARGET_OPT = PREFIX=$(TARGET_DIR)/usr install
