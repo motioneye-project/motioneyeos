5155de9042493f91c2f37e81656ac960dfcec49d
Peter Korsgaard <jacmet@sunsite.dk>
2008-12-04 12:14:57 +0000

    ifplugd: don't overwrite target etc/init.d/ifplugd.*
    install-strip unconditionally overwrites $(TARGET_DIR)/etc/init.d/ifplugd.*,
    so the check in _HOOK_POST_INSTALL is useless.
    
    Fix it by using install-exec and manually stripping the target binaries
    instead.

--- package/ifplugd/ifplugd.mk
+++ package/ifplugd/ifplugd.mk
@@ -9,6 +9,8 @@ IFPLUGD_SITE = http://0pointer.de/lennart/projects/ifplugd/
 IFPLUGD_AUTORECONF = NO
 IFPLUGD_INSTALL_STAGING = NO
 IFPLUGD_INSTALL_TARGET = YES
+# install-strip unconditionally overwrites $(TARGET_DIR)/etc/ifplugd/ifplugd.*
+IFPLUGD_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install-exec
 
 IFPLUGD_CONF_OPT = --disable-lynx
 
@@ -17,6 +19,8 @@ IFPLUGD_DEPENDENCIES = uclibc libdaemon
 $(eval $(call AUTOTARGETS,package,ifplugd))
 
 $(IFPLUGD_HOOK_POST_INSTALL): $(IFPLUGD_TARGET_INSTALL_TARGET)
+	$(STRIPCMD) $(STRIP_STRIP_ALL) $(TARGET_DIR)/usr/sbin/ifplugd
+	$(STRIPCMD) $(STRIP_STRIP_ALL) $(TARGET_DIR)/usr/sbin/ifplugstatus
 	$(INSTALL) -d $(TARGET_DIR)/etc/ifplugd
 	@if [ ! -f $(TARGET_DIR)/etc/ifplugd/ifplugd.conf ]; then \
 		$(INSTALL) $(IFPLUGD_DIR)/conf/ifplugd.conf $(TARGET_DIR)/etc/ifplugd/; \
