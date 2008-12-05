ed63bc0b48353ba30f24c6c25cccdc0e2b725a8a
Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>
2008-12-05 09:46:17 +0000

    alsa-lib: bump version to 1.0.18

--- package/multimedia/alsa-lib/alsa-lib.mk
+++ package/multimedia/alsa-lib/alsa-lib.mk
@@ -3,7 +3,7 @@
 # alsa-lib
 #
 #############################################################
-ALSA_LIB_VERSION:=1.0.14a
+ALSA_LIB_VERSION:=1.0.18
 ALSA_LIB_SOURCE:=alsa-lib-$(ALSA_LIB_VERSION).tar.bz2
 ALSA_LIB_SITE:=ftp://ftp.alsa-project.org/pub/lib
 ALSA_LIB_DIR:=$(BUILD_DIR)/alsa-lib-$(ALSA_LIB_VERSION)
