f73e7d4611c8c6130f0026405840eb526d12c3d6
Hamish Moffatt <hamish@cloud.net.au>
2008-12-08 04:41:19 +0000

    Over-ride the target-install target rather than installing as a post-build hook. Otherwise the wrong stamps are used and the library won't be reinstalled if you clean out $(TARGET_DIR).

--- package/tiff/tiff.mk
+++ package/tiff/tiff.mk
@@ -8,7 +8,7 @@ TIFF_SITE:=ftp://ftp.remotesensing.org/pub/libtiff
 TIFF_SOURCE:=tiff-$(TIFF_VERSION).tar.gz
 TIFF_LIBTOOL_PATCH = NO
 TIFF_INSTALL_STAGING = YES
-TIFF_INSTALL_TARGET = NO
+TIFF_INSTALL_TARGET = YES
 TIFF_CONF_OPT = \
 	--enable-shared \
 	--enable-static \
@@ -19,7 +19,7 @@ TIFF_DEPENDENCIES = uclibc pkgconfig zlib jpeg
 
 $(eval $(call AUTOTARGETS,package,tiff))
 
-$(TIFF_HOOK_POST_BUILD):
+$(TIFF_TARGET_INSTALL_TARGET):
 	-cp -a $(TIFF_DIR)/libtiff/.libs/libtiff.so* $(TARGET_DIR)/usr/lib/
 	$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libtiff.so
 	touch $@
