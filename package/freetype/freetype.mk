f2824f303c0e745fd75e3a9f00e6bde51d51960c
Hamish Moffatt <hamish@cloud.net.au>
2008-12-08 04:43:33 +0000

    Override the target-install target properly rather than installing as a post-build hook

--- package/freetype/freetype.mk
+++ package/freetype/freetype.mk
@@ -8,13 +8,13 @@ FREETYPE_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/
 FREETYPE_SOURCE = freetype-$(FREETYPE_VERSION).tar.bz2
 FREETYPE_LIBTOOL_PATCH = NO
 FREETYPE_INSTALL_STAGING = YES
-FREETYPE_INSTALL_TARGET = NO
+FREETYPE_INSTALL_TARGET = YES
 FREETYPE_MAKE_OPT = CCexe="$(HOSTCC)"
 FREETYPE_DEPENDENCIES = uclibc pkgconfig zlib
 
 $(eval $(call AUTOTARGETS,package,freetype))
 
-$(FREETYPE_HOOK_POST_BUILD):
+$(FREETYPE_TARGET_INSTALL_TARGET):
 	-cp -a $(FREETYPE_DIR)/objs/.libs/libfreetype.so* $(TARGET_DIR)/usr/lib/
 	$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libfreetype.so
 	$(SED) "s,^prefix=.*,prefix=\'$(STAGING_DIR)/usr\',g" $(FREETYPE_DIR)/builds/unix/freetype-config
