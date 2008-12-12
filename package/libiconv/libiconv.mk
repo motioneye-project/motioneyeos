2bb1785adb14b50a648ab75614f78a3c7a5e5edc
Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>
2008-12-12 09:03:47 +0000

    libiconv: strip installed library and remove the preloadable library
    This patch is mainly for size reduction, removing more than half the
    needed footprint.
    
    Signed-off-by: Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>

--- package/libiconv/libiconv.mk
+++ package/libiconv/libiconv.mk
@@ -16,3 +16,11 @@ LIBICONV_DEPENDENCIES = uclibc
 
 $(eval $(call AUTOTARGETS,package,libiconv))
 
+$(LIBICONV_HOOK_POST_INSTALL):
+	# Remove not used preloadable libiconv.so
+	rm -f $(STAGING_DIR)/usr/lib/preloadable_libiconv.so
+	rm -f $(TARGET_DIR)/usr/lib/preloadable_libiconv.so
+ifneq ($(BR2_ENABLE_DEBUG),y)
+	$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libiconv.so.*
+endif
+	touch $@
