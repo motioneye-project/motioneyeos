963cbf51e7d8e89d0fbec0c244ac110e6a79606f
Peter Korsgaard <jacmet@sunsite.dk>
2008-12-26 15:36:03 +0000

    package/multimedia: add libmpd package

---
+++ package/multimedia/libmpd/libmpd.mk
@@ -0,0 +1,14 @@
+#############################################################
+#
+# libmpd
+#
+#############################################################
+LIBMPD_VERSION = 0.17.0
+LIBMPD_SOURCE = libmpd-$(LIBMPD_VERSION).tar.gz
+LIBMPD_SITE = http://download.sarine.nl/download/Programs/gmpc/$(LIBMPD_VERSION)/
+LIBMPD_AUTORECONF = YES
+LIBMPD_INSTALL_STAGING = YES
+LIBMPD_LIBTOOL_PATCH = NO
+LIBMPD_DEPENDENCIES = libglib2
+
+$(eval $(call AUTOTARGETS,package/multimedia,libmpd))
