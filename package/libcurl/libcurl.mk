84c556006d3c5349702016d75a6f123b27d4b735
Peter Korsgaard <jacmet@sunsite.dk>
2008-12-28 20:04:01 +0000

    libcurl: compile with openssl support if available

--- package/libcurl/libcurl.mk
+++ package/libcurl/libcurl.mk
@@ -10,6 +10,12 @@ LIBCURL_INSTALL_STAGING = YES
 LIBCURL_CONF_OPT = --disable-verbose --disable-manual --enable-hidden-symbols \
 		   $(DISABLE_NLS) $(DISABLE_LARGEFILE) $(DISABLE_IPV6)
 
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+LIBCURL_DEPENDENCIES += openssl
+LIBCURL_CONF_ENV += ac_cv_lib_crypto_CRYPTO_lock=yes
+LIBCURL_CONF_OPT += --with-ssl=$(STAGING_DIR)/usr --with-random=/dev/urandom
+endif
+
 $(eval $(call AUTOTARGETS,package,libcurl))
 
 $(LIBCURL_HOOK_POST_INSTALL):
