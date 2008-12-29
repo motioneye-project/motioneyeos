8dd24e0f54cd46dd687f0362e87469f77f6c7727
Peter Korsgaard <jacmet@sunsite.dk>
2008-12-29 11:05:04 +0000

    pango: fix download url
    Reported by Berns.

--- package/pango/pango.mk
+++ package/pango/pango.mk
@@ -5,7 +5,7 @@
 #############################################################
 PANGO_VERSION = 1.20.5
 PANGO_SOURCE = pango-$(PANGO_VERSION).tar.bz2
-PANGO_SITE = http://ftp.gnome.org/pub/GNOME/sources/pango/1.19
+PANGO_SITE = http://ftp.gnome.org/pub/GNOME/sources/pango/1.20
 PANGO_AUTORECONF = YES
 PANGO_INSTALL_STAGING = YES
 PANGO_INSTALL_TARGET = YES
