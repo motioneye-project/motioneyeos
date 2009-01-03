910dc8cb07abd90bc20e762fa580a8608c414d4a
Nigel Kukard <nkukard@lbsd.net>
2009-01-03 08:04:21 +0000

    rsync doesn't support install-exec, override RSYNC_INSTALL_TARGET_OPT to install non-stripped

--- package/rsync/rsync.mk
+++ package/rsync/rsync.mk
@@ -10,7 +10,10 @@ RSYNC_SITE:=http://rsync.samba.org/ftp/rsync/src
 RSYNC_AUTORECONF:=no
 RSYNC_INSTALL_STAGING:=NO
 RSYNC_INSTALL_TARGET:=YES
-ifeq ($(BR2_ENABLE_DEBUG),)
+ifeq ($(BR2_ENABLE_DEBUG),y)
+RSYNC_INSTALL_TARGET_OPT:=DESTDIR=$(TARGET_DIR) INSTALLCMD='./install-sh -c' \
+			  install
+else
 RSYNC_INSTALL_TARGET_OPT:=DESTDIR=$(TARGET_DIR) INSTALLCMD='./install-sh -c' \
 			  STRIPPROG="$(TARGET_STRIP)" install-strip
 endif
