0875bc85bbe33d542e9045e931d223812c3eca44
Daniel Laird <danieljlaird@hotmail.com>
2009-01-13 10:26:28 +0000

    package/alsa-lib/alsa-lib.mk package/gettext/gettext.mk: Both of these components look for iconv when configuring so need to be dependent.
    Both of these packages can be built with libiconv support.  If libiconv is enabled and these are built first 
    then some configure variables get stored in the system cache and then libiconv is not used properly.
    If libiconv is selected by user make sure it is built before either of these packages are built.
    
    Signed-off-by: Daniel Laird <daniel.j.laird@nxp.com>

--- package/gettext/gettext.mk
+++ package/gettext/gettext.mk
@@ -120,7 +120,7 @@ $(STAGING_DIR)/$(GETTEXT_TARGET_BINARY): $(GETTEXT_DIR)/$(GETTEXT_BINARY)
 		autopoint envsubst gettext.sh gettextize msg* ?gettext)
 	touch -c $@
 
-gettext: uclibc pkgconfig $(STAGING_DIR)/$(GETTEXT_TARGET_BINARY)
+gettext: uclibc pkgconfig $(if $(BR2_PACKAGE_LIBICONV),libiconv) $(STAGING_DIR)/$(GETTEXT_TARGET_BINARY)
 
 gettext-unpacked: $(GETTEXT_DIR)/.unpacked
 
