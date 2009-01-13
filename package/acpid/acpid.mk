4613db0009e070cb19b8cb864264d8f0d1686f4f
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2009-01-13 18:44:32 +0000

    Bump acpid version and update source filename

--- package/acpid/acpid.mk
+++ package/acpid/acpid.mk
@@ -3,9 +3,9 @@
 # acpid
 #
 #############################################################
-ACPID_VERSION:=1.0.4
+ACPID_VERSION:=1.0.8
 ACPID_DIR=$(BUILD_DIR)/acpid-$(ACPID_VERSION)
-ACPID_SOURCE=acpid_$(ACPID_VERSION)-1.tar.gz
+ACPID_SOURCE=acpid_$(ACPID_VERSION).orig.tar.gz
 ACPID_SITE=$(BR2_DEBIAN_MIRROR)/debian/pool/main/a/acpid
 
 $(DL_DIR)/$(ACPID_SOURCE):
