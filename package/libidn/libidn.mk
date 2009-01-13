2f211c877864f66c9eab3422dc60af4171394010
Daniel Laird <danieljlaird@hotmail.com>
2009-01-13 16:36:16 +0000

    package/libidn: Add a new package (required by later WebKits...)
    GNU Libidn is a fully documented implementation of the Stringprep, 
    Punycode and IDNA specifications. Libidn's purpose is to encode 
    and decode internationalized domain names. The native C, C# 
    and Java libraries are available under 
    the GNU Lesser General Public License version 2.1 or later. 
    
    Not quite sure I've put it in the correct menu but it will work.
    
    Signed-off-by: Daniel Laird <daniel.j.laird@nxp.com>

---
+++ package/libidn/libidn.mk
@@ -0,0 +1,14 @@
+#############################################################
+#
+# libidn
+#
+#############################################################
+LIBIDN_VERSION = 1.9
+LIBIDN_SOURCE = libidn-$(LIBIDN_VERSION).tar.gz
+LIBIDN_SITE = http://ftp.gnu.org/gnu/libidn/
+LIBIDN_INSTALL_STAGING = YES
+LIBIDN_INSTALL_TARGET = YES
+LIBIDN_CONF_OPT = --enable-shared
+LIBIDN_DEPENDENCIES = uclibc pkgconfig gettext $(if $(BR2_PACKAGE_LIBICONV),libiconv)
+
+$(eval $(call AUTOTARGETS,package,libidn))
