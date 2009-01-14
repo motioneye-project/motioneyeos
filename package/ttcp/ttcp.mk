76224bc019a669cbfaeafeef1619b988e26b20dd
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2009-01-14 23:53:20 +0000

    Change TTCP_BUILD_DIR to TTCP_DIR

--- package/ttcp/ttcp.mk
+++ package/ttcp/ttcp.mk
@@ -7,35 +7,35 @@
 TTCP_VERSION:=
 TTCP_SOURCE_URL=http://ftp.sunet.se/pub/network/monitoring/ttcp
 TTCP_SOURCE=ttcp$(TTCP_VERSION).c
-TTCP_BUILD_DIR=$(BUILD_DIR)/ttcp$(TTCP_VERSION)
+TTCP_DIR=$(BUILD_DIR)/ttcp$(TTCP_VERSION)
 
 $(DL_DIR)/$(TTCP_SOURCE):
 	 $(WGET) -P $(DL_DIR) $(TTCP_SOURCE_URL)/$(TTCP_SOURCE)
 
-$(TTCP_BUILD_DIR)/.unpacked: $(DL_DIR)/$(TTCP_SOURCE)
-	-mkdir $(TTCP_BUILD_DIR)
-	cp -af $(DL_DIR)/$(TTCP_SOURCE) $(TTCP_BUILD_DIR)
+$(TTCP_DIR)/.unpacked: $(DL_DIR)/$(TTCP_SOURCE)
+	-mkdir $(TTCP_DIR)
+	cp -af $(DL_DIR)/$(TTCP_SOURCE) $(TTCP_DIR)
 	toolchain/patch-kernel.sh $(TTCP_DIR) package/ttcp/ ttcp-\*.patch
-	touch $(TTCP_BUILD_DIR)/.unpacked
+	touch $(TTCP_DIR)/.unpacked
 
-$(TTCP_BUILD_DIR)/.configured: $(TTCP_BUILD_DIR)/.unpacked
-	touch $(TTCP_BUILD_DIR)/.configured
+$(TTCP_DIR)/.configured: $(TTCP_DIR)/.unpacked
+	touch $(TTCP_DIR)/.configured
 
-$(TTCP_BUILD_DIR)/ttcp: $(TTCP_BUILD_DIR)/.configured
-	$(TARGET_CC) -O2 -o $(TTCP_BUILD_DIR)/ttcp $(TTCP_BUILD_DIR)/$(TTCP_SOURCE)
+$(TTCP_DIR)/ttcp: $(TTCP_DIR)/.configured
+	$(TARGET_CC) -O2 -o $(TTCP_DIR)/ttcp $(TTCP_DIR)/$(TTCP_SOURCE)
 
-$(TARGET_DIR)/usr/bin/ttcp: $(TTCP_BUILD_DIR)/ttcp
-	cp -af $(TTCP_BUILD_DIR)/ttcp $(TARGET_DIR)/usr/bin/
+$(TARGET_DIR)/usr/bin/ttcp: $(TTCP_DIR)/ttcp
+	cp -af $(TTCP_DIR)/ttcp $(TARGET_DIR)/usr/bin/
 
 ttcp: $(TARGET_DIR)/usr/bin/ttcp
 
 ttcp-source: $(DL_DIR)/$(TTCP_SOURCE)
 
 ttcp-clean:
-	rm -f $(TTCP_BUILD_DIR)/*.o $(TTCP_BUILD_DIR)/ttcp
+	rm -f $(TTCP_DIR)/*.o $(TTCP_DIR)/ttcp
 
 ttcp-dirclean:
-	rm -rf $(TTCP_BUILD_DIR)
+	rm -rf $(TTCP_DIR)
 
 #############################################################
 #
