30ffbbe5ad4f5feb6797439d46fa2e404d188505
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2009-01-14 22:07:17 +0000

    Remove references to sys/timeb.h in libupnp configure, ftime is removed from source already

--- package/libupnp/libupnp.mk
+++ package/libupnp/libupnp.mk
@@ -7,6 +7,8 @@ LIBUPNP_VERSION:=1.6.6
 LIBUPNP_SOURCE:=libupnp-$(LIBUPNP_VERSION).tar.bz2
 LIBUPNP_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/pupnp
 
+LIBUPNP_CONF_ENV = ac_cv_lib_compat_ftime=no
+
 LIBUPNP_INSTALL_STAGING:=YES
 
 $(eval $(call AUTOTARGETS,package,libupnp))
