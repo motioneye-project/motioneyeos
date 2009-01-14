f3c2eed5d8a9e976862353ec0e01165aaccf5f91
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2009-01-14 22:52:31 +0000

    Update deprecated bmove to memmove in ttcp

--- package/ttcp/ttcp.mk
+++ package/ttcp/ttcp.mk
@@ -15,6 +15,7 @@ $(DL_DIR)/$(TTCP_SOURCE):
 $(TTCP_BUILD_DIR)/.unpacked: $(DL_DIR)/$(TTCP_SOURCE)
 	-mkdir $(TTCP_BUILD_DIR)
 	cp -af $(DL_DIR)/$(TTCP_SOURCE) $(TTCP_BUILD_DIR)
+	toolchain/patch-kernel.sh $(TTCP_DIR) package/ttcp/ ttcp-\*.patch
 	touch $(TTCP_BUILD_DIR)/.unpacked
 
 $(TTCP_BUILD_DIR)/.configured: $(TTCP_BUILD_DIR)/.unpacked
