d04321356a0c70de27788474df2e42ba8f9be3c1
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2009-01-22 22:58:29 +0000

    Use /usr/lib for slang instead of /lib

--- package/slang/slang.mk
+++ package/slang/slang.mk
@@ -18,24 +18,24 @@ $(SLANG_DIR): $(DL_DIR)/$(SLANG_SOURCE)
 $(SLANG_DIR)/libslang.so: $(SLANG_DIR)
 	$(MAKE1) CFLAGS="-Os -g -fPIC $(TARGET_CFLAGS)" CC=$(TARGET_CC) -C $(SLANG_DIR)
 
-$(STAGING_DIR)/lib/libslang.so.1: $(SLANG_DIR)/libslang.so
+$(STAGING_DIR)/usr/lib/libslang.so.1: $(SLANG_DIR)/libslang.so
 	cp -dpf $(SLANG_DIR)/libslang.a $(STAGING_DIR)/usr/lib
-	cp -dpf $(SLANG_DIR)/libslang.so $(STAGING_DIR)/lib
+	cp -dpf $(SLANG_DIR)/libslang.so $(STAGING_DIR)/usr/lib
 	cp -dpf $(SLANG_DIR)/slang.h $(STAGING_DIR)/usr/include
 	cp -dpf $(SLANG_DIR)/slcurses.h $(STAGING_DIR)/usr/include
-	(cd $(STAGING_DIR)/lib; ln -fs libslang.so libslang.so.1)
+	(cd $(STAGING_DIR)/usr/lib; ln -fs libslang.so libslang.so.1)
 	touch -c $@
 
-$(TARGET_DIR)/lib/libslang.so.1: $(STAGING_DIR)/lib/libslang.so.1
-	cp -dpf $(STAGING_DIR)/lib/libslang.so* $(TARGET_DIR)/lib/
-	-$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/lib/libslang.so*
+$(TARGET_DIR)/usr/lib/libslang.so.1: $(STAGING_DIR)/usr/lib/libslang.so.1
+	cp -dpf $(STAGING_DIR)/usr/lib/libslang.so* $(TARGET_DIR)/usr/lib/
+	-$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libslang.so*
 
-slang: uclibc $(STAGING_DIR)/lib/libslang.so.1 $(TARGET_DIR)/lib/libslang.so.1
+slang: uclibc $(STAGING_DIR)/usr/lib/libslang.so.1 $(TARGET_DIR)/usr/lib/libslang.so.1
 
 slang-source: $(DL_DIR)/$(SLANG_SOURCE)
 
 slang-clean:
-	rm -f $(TARGET_DIR)/lib/libslang.so* $(STAGING_DIR)/usr/lib/libslang.a \
+	rm -f $(TARGET_DIR)/usr/lib/libslang.so* $(STAGING_DIR)/usr/lib/libslang.a \
 		$(STAGING_DIR)/usr/include/slang.h \
 		$(STAGING_DIR)/usr/include/slcurses.h
 	-$(MAKE) -C $(SLANG_DIR) clean
