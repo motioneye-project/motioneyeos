690ba839c0a2b672a786fdf174ea14827f1c553a
Ulf Samuelsson <ulf.samuelsson@atmel.com>
2009-01-25 20:50:59 +0000

    Support libiconv option for popt

--- package/popt/popt.mk
+++ package/popt/popt.mk
@@ -5,9 +5,15 @@
 #############################################################
 POPT_VERSION:=1.14
 POPT_SITE:=http://rpm5.org/files/popt
-POPT_INSTALL_STAGING:=YES
-POPT_INSTALL_TARGET:=YES
-POPT_CONF_ENV:=ac_cv_va_copy=yes
+POPT_INSTALL_STAGING = YES
+POPT_INSTALL_TARGET = YES
+
+POPT_CONF_ENV = ac_cv_va_copy=yes
+
+ifeq ($(BR2_PACKAGE_LIBICONV),y)
+POPT_CONF_ENV += am_cv_lib_iconv=yes
+POPT_CONF_OPT += --with-libiconv-prefix=$(STAGING_DIR)/usr
+endif
 
 POPT_DEPENDENCIES:=uclibc
 
