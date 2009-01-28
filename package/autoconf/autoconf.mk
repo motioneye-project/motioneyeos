30f4e13c03d84f2d24724eb8116fbb589cf9aca5
Daniel Laird <danieljlaird@hotmail.com>
2009-01-28 15:35:39 +0000

    autoconf.mk: Bounce version to 2.63
    Have been having various issues with multiple jobs (-jX) and building
    autoconf.  Turns out that v2.63 has fixed this.  So bounce version
    to a working version.
     
    Signed-off-by: Daniel Laird <daniel.j.laird@nxp.com>

--- package/autoconf/autoconf.mk
+++ package/autoconf/autoconf.mk
@@ -3,7 +3,7 @@
 # autoconf
 #
 #############################################################
-AUTOCONF_VERSION:=2.61
+AUTOCONF_VERSION:=2.63
 AUTOCONF_SOURCE:=autoconf-$(AUTOCONF_VERSION).tar.bz2
 AUTOCONF_SITE:=$(BR2_GNU_MIRROR)/autoconf
 AUTOCONF_CAT:=$(BZCAT)
