85d756d5cc31610e54608134205ee351d3b9e180
Peter Korsgaard <jacmet@sunsite.dk>
2009-01-29 11:27:20 +0000

    busybox: remove unused handling of ancient versions no longer in BR

--- package/busybox/busybox.mk
+++ package/busybox/busybox.mk
@@ -46,14 +46,6 @@ $(BUSYBOX_DIR)/.configured: $(BUSYBOX_DIR)/.unpacked $(BUSYBOX_CONFIG_FILE)
 	cp -f $(BUSYBOX_CONFIG_FILE) $(BUSYBOX_DIR)/.config
 	$(SED) s,^CONFIG_PREFIX=.*,CONFIG_PREFIX=\"$(TARGET_DIR)\", \
 		$(BUSYBOX_DIR)/.config
-ifeq ($(BR2_BUSYBOX_VERSION_1_0_1),y)
-	$(SED) "s,^CROSS.*,CROSS=$(TARGET_CROSS)\n\PREFIX=$(TARGET_DIR),;" \
-		$(BUSYBOX_DIR)/Rules.mak
-endif
-ifeq ($(BR2_BUSYBOX_VERSION_1_1_3),y)
-	$(SED) s,^PREFIX=.*,CONFIG_PREFIX=\"$(TARGET_DIR)\", \
-		$(BUSYBOX_DIR)/.config
-endif
 ifeq ($(BR2_BUSYBOX_VERSION_1_2_2_1),y)
 	$(SED) s,^CROSS_COMPILER_PREFIX=.*,CROSS_COMPILER_PREFIX=\"$(TARGET_CROSS)\", \
 		$(BUSYBOX_DIR)/.config
