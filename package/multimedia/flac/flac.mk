24981139f6891b8249a5f43d01760c60bca508cb
Peter Korsgaard <jacmet@sunsite.dk>
2009-02-20 13:02:13 +0000

    package/multimeda: add flac package
    Patch by Sven Neumann (from #99).
    
    Removed optimization patch, cleaned up configure args and fixed download URL.

---
+++ package/multimedia/flac/flac.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# flac
+#
+################################################################################
+
+FLAC_VERSION = 1.2.1
+FLAC_SOURCE = flac-$(FLAC_VERSION).tar.gz
+FLAC_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/flac/
+FLAC_AUTORECONF = NO
+FLAC_INSTALL_TARGET = YES
+FLAC_INSTALL_STAGING = YES
+
+FLAC_CONF_OPT = \
+	--enable-shared \
+	--disable-cpplibs \
+	--disable-xmms-plugin
+
+ifeq ($(BR2_PACKAGE_LIBOGG),y)
+FLAC_CONF_OPT += --with-ogg=$(STAGING_DIR)/usr
+FLAC_DEPENDENCIES = libogg
+else
+FLAC_CONF_OPT += --disable-ogg
+endif
+
+$(eval $(call AUTOTARGETS,package/multimedia,flac))
