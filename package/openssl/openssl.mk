e14c11230a4f7682153431700ff2bee90c820371
Hamish Moffatt <hamish@cloud.net.au>
2009-02-24 00:37:06 +0000

    Bump version to 0.9.8j

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -3,7 +3,7 @@
 # openssl
 #
 #############################################################
-OPENSSL_VERSION:=0.9.8g
+OPENSSL_VERSION:=0.9.8j
 OPENSSL_SITE:=http://www.openssl.org/source
 
 OPENSSL_TARGET_ARCH=generic32
