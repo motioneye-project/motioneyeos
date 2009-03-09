49d40d663a12504af0e49c3c5eac6b3e33b4a8c1
Peter Korsgaard <jacmet@sunsite.dk>
2009-03-09 14:09:25 +0000

    sysklogd: only install init script / config file if not present in skeleton
    Allow platforms to provide a custom initscript / syslog.conf in their
    target_skeleton.

--- package/sysklogd/sysklogd.mk
+++ package/sysklogd/sysklogd.mk
@@ -32,8 +32,12 @@ $(SYSKLOGD_DIR)/$(SYSKLOGD_BINARY): $(SYSKLOGD_DIR)/.unpacked
 $(TARGET_DIR)/$(SYSKLOGD_TARGET_BINARY): $(SYSKLOGD_DIR)/$(SYSKLOGD_BINARY)
 	$(INSTALL) -m 0755 -D $(SYSKLOGD_DIR)/$(SYSKLOGD_SYSLOGD_BINARY) $(TARGET_DIR)/$(SYSKLOGD_SYSLOGD_TARGET_BINARY)
 	$(INSTALL) -m 0755 -D $(SYSKLOGD_DIR)/$(SYSKLOGD_KLOGD_BINARY) $(TARGET_DIR)/$(SYSKLOGD_KLOGD_TARGET_BINARY)
-	$(INSTALL) -m 0755 -D package/sysklogd/S25syslog $(TARGET_DIR)/etc/init.d
-	$(INSTALL) -m 0644 -D package/sysklogd/syslog.conf $(TARGET_DIR)/etc/syslog.conf
+	if [ ! -f $(TARGET_DIR)/etc/init.d/S25syslog ]; then \
+		$(INSTALL) -m 0755 -D package/sysklogd/S25syslog $(TARGET_DIR)/etc/init.d; \
+	fi
+	if [ ! -f $(TARGET_DIR)/etc/syslog.conf ]; then \
+		$(INSTALL) -m 0644 -D package/sysklogd/syslog.conf $(TARGET_DIR)/etc/syslog.conf; \
+	fi
 
 sysklogd: uclibc $(TARGET_DIR)/$(SYSKLOGD_TARGET_BINARY)
 
