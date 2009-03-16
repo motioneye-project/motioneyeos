58aaf754815926de90a90f16101999d7d2b03f8a
Peter Korsgaard <jacmet@sunsite.dk>
2009-03-16 20:58:04 +0000

    expat: convert to Makefile.autotools.in format

--- package/expat/expat.mk
+++ package/expat/expat.mk
@@ -4,77 +4,22 @@
 #
 #############################################################
 
-EXPAT_VERSION=2.0.1
-EXPAT_SOURCE=expat-$(EXPAT_VERSION).tar.gz
-EXPAT_CAT:=$(ZCAT)
-EXPAT_SITE=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/expat
-EXPAT_DIR:=$(BUILD_DIR)/expat-$(EXPAT_VERSION)
+EXPAT_VERSION = 2.0.1
+EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.gz
+EXPAT_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/expat
+EXPAT_LIBTOOL_PATCH = NO
+EXPAT_INSTALL_STAGING = YES
+EXPAT_INSTALL_TARGET = YES
+# no install-strip / install-exec
+EXPAT_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) installlib
+EXPAT_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) installlib
 
-EXPAT_BINARY:=.libs/libexpat.a
-EXPAT_TARGET_BINARY:=usr/lib/libexpat.so.1
+EXPAT_CONF_OPT = --enable-shared
 
-$(DL_DIR)/$(EXPAT_SOURCE):
-	$(call DOWNLOAD,$(EXPAT_SITE),$(EXPAT_SOURCE))
+EXPAT_DEPENDENCIES = uclibc pkgconfig
 
-expat-source: $(DL_DIR)/$(EXPAT_SOURCE)
+$(eval $(call AUTOTARGETS,package,expat))
 
-$(EXPAT_DIR)/.unpacked: $(DL_DIR)/$(EXPAT_SOURCE)
-	$(EXPAT_CAT) $(DL_DIR)/$(EXPAT_SOURCE) | \
-		tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	$(CONFIG_UPDATE) $(EXPAT_DIR)
-	$(CONFIG_UPDATE) $(EXPAT_DIR)/conftools
+$(EXPAT_HOOK_POST_INSTALL): $(EXPAT_TARGET_INSTALL_TARGET)
+	$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libexpat.so.*
 	touch $@
-
-$(EXPAT_DIR)/.configured: $(EXPAT_DIR)/.unpacked
-	(cd $(EXPAT_DIR); rm -rf config.cache; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		./configure \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--prefix=/usr \
-		--exec-prefix=/usr \
-		--sysconfdir=/etc \
-		--localstatedir=/var \
-		--enable-shared \
-	)
-	touch $@
-
-$(EXPAT_DIR)/$(EXPAT_BINARY): $(EXPAT_DIR)/.configured
-	$(MAKE) -C $(EXPAT_DIR) all
-	touch -c $@
-
-$(STAGING_DIR)/$(EXPAT_TARGET_BINARY): $(EXPAT_DIR)/$(EXPAT_BINARY)
-	$(MAKE) DESTDIR=$(STAGING_DIR) -C $(EXPAT_DIR) install
-	$(SED) "s,^libdir=.*,libdir=\'$(STAGING_DIR)/usr/lib\',g" \
-		$(STAGING_DIR)/usr/lib/libexpat.la
-	touch -c $@
-
-$(TARGET_DIR)/$(EXPAT_TARGET_BINARY): $(STAGING_DIR)/$(EXPAT_TARGET_BINARY)
-	mkdir -p $(TARGET_DIR)/usr/lib
-	cp -dpf $(STAGING_DIR)/usr/lib/libexpat.so* $(TARGET_DIR)/usr/lib/
-	#cp -dpf $(STAGING_DIR)/usr/bin/xmlwf $(TARGET_DIR)/usr/bin/xmlwf
-	-$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libexpat.so*
-	touch -c $@
-
-expat: uclibc pkgconfig $(TARGET_DIR)/$(EXPAT_TARGET_BINARY)
-
-expat-clean:
-	rm -f $(EXPAT_DIR)/.configured
-	rm -f $(STAGING_DIR)/usr/lib/libexpat.* \
-		$(TARGET_DIR)/usr/lib/libexpat.*
-	#rm -f $(STAGING_DIR)/usr/bin/xmlwf $(TARGET_DIR)/usr/bin/xmlwf
-	-$(MAKE) -C $(EXPAT_DIR) clean
-
-expat-dirclean:
-	rm -rf $(EXPAT_DIR)
-
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_EXPAT),y)
-TARGETS+=expat
-endif
