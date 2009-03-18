b4510152038bdd6fb0ef9ae9ac581daf5dcb05d8
Peter Korsgaard <jacmet@sunsite.dk>
2009-03-18 19:18:40 +0000

    pkgconfig: install into HOST_DIR

--- package/pkgconfig/pkgconfig.mk
+++ package/pkgconfig/pkgconfig.mk
@@ -9,7 +9,7 @@ PKGCONFIG_SITE:=http://pkgconfig.freedesktop.org/releases/
 PKGCONFIG_DIR:=$(BUILD_DIR)/pkg-config-$(PKGCONFIG_VERSION)
 PKGCONFIG_CAT:=$(ZCAT)
 PKGCONFIG_BINARY:=pkg-config
-PKGCONFIG_TARGET_BINARY:=usr/bin/pkg-config
+PKGCONFIG_HOST_BINARY:=$(HOST_DIR)/usr/bin/pkg-config
 
 $(DL_DIR)/$(PKGCONFIG_SOURCE):
 	 $(call DOWNLOAD,$(PKGCONFIG_SITE),$(PKGCONFIG_SOURCE))
@@ -24,28 +24,27 @@ $(PKGCONFIG_DIR)/.unpacked: $(DL_DIR)/$(PKGCONFIG_SOURCE)
 $(PKGCONFIG_DIR)/.configured: $(PKGCONFIG_DIR)/.unpacked
 	(cd $(PKGCONFIG_DIR); rm -rf config.cache; \
 		./configure \
-		--prefix=/usr \
-		--sysconfdir=/etc \
+		--prefix=$(HOST_DIR)/usr \
+		--sysconfdir=$(HOST_DIR)/etc \
 		--with-pc-path="$(STAGING_DIR)/usr/lib/pkgconfig" \
-		$(DISABLE_NLS) \
-		$(DISABLE_LARGEFILE) \
+		--disable-static \
 	)
 	touch $@
 
 $(PKGCONFIG_DIR)/$(PKGCONFIG_BINARY): $(PKGCONFIG_DIR)/.configured
 	$(MAKE) -C $(PKGCONFIG_DIR)
 
-$(STAGING_DIR)/$(PKGCONFIG_TARGET_BINARY): $(PKGCONFIG_DIR)/$(PKGCONFIG_BINARY)
-	$(MAKE) DESTDIR=$(STAGING_DIR) -C $(PKGCONFIG_DIR) install
-	rm -rf $(STAGING_DIR)/usr/share/man
+$(PKGCONFIG_HOST_BINARY): $(PKGCONFIG_DIR)/$(PKGCONFIG_BINARY)
+	$(MAKE) -C $(PKGCONFIG_DIR) install
 
-pkgconfig: uclibc $(STAGING_DIR)/$(PKGCONFIG_TARGET_BINARY)
+host-pkgconfig pkgconfig: $(PKGCONFIG_HOST_BINARY)
 
-pkgconfig-clean:
-	-$(MAKE) DESTDIR=$(STAGING_DIR) -C $(PKGCONFIG_DIR) uninstall
+host-pkgconfig-clean pkgconfig-clean:
+	rm -f $(addprefix $(PKGCONFIG_DIR)/,.unpacked .configured .compiled)
+	-$(MAKE) -C $(PKGCONFIG_DIR) uninstall
 	-$(MAKE) -C $(PKGCONFIG_DIR) clean
 
-pkgconfig-dirclean:
+host-pkgconfig-dirclean pkgconfig-dirclean:
 	rm -rf $(PKGCONFIG_DIR)
 
 #############################################################
