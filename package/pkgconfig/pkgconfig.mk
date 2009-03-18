344d59d788d697f4f6704dd920cad615318e47be
Peter Korsgaard <jacmet@sunsite.dk>
2009-03-18 19:18:45 +0000

    pkgconfig: use pkg-config-*-host build dir for host version

--- package/pkgconfig/pkgconfig.mk
+++ package/pkgconfig/pkgconfig.mk
@@ -6,9 +6,9 @@
 PKGCONFIG_VERSION:=0.23
 PKGCONFIG_SOURCE:=pkg-config-$(PKGCONFIG_VERSION).tar.gz
 PKGCONFIG_SITE:=http://pkgconfig.freedesktop.org/releases/
-PKGCONFIG_DIR:=$(BUILD_DIR)/pkg-config-$(PKGCONFIG_VERSION)
-PKGCONFIG_CAT:=$(ZCAT)
-PKGCONFIG_BINARY:=pkg-config
+
+# pkgconfig for the host
+PKGCONFIG_HOST_DIR:=$(BUILD_DIR)/pkg-config-$(PKGCONFIG_VERSION)-host
 PKGCONFIG_HOST_BINARY:=$(HOST_DIR)/usr/bin/pkg-config
 
 $(DL_DIR)/$(PKGCONFIG_SOURCE):
@@ -16,13 +16,15 @@ $(DL_DIR)/$(PKGCONFIG_SOURCE):
 
 pkgconfig-source: $(DL_DIR)/$(PKGCONFIG_SOURCE)
 
-$(PKGCONFIG_DIR)/.unpacked: $(DL_DIR)/$(PKGCONFIG_SOURCE)
-	$(PKGCONFIG_CAT) $(DL_DIR)/$(PKGCONFIG_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(PKGCONFIG_DIR) package/pkgconfig/ \*.patch
+$(PKGCONFIG_HOST_DIR)/.unpacked: $(DL_DIR)/$(PKGCONFIG_SOURCE)
+	mkdir -p $(@D)
+	$(INFLATE$(suffix $(PKGCONFIG_SOURCE))) $< | \
+		$(TAR) $(TAR_STRIP_COMPONENTS)=1 -C $(@D) $(TAR_OPTIONS) -
+	toolchain/patch-kernel.sh $(@D) package/pkgconfig/ \*.patch
 	touch $@
 
-$(PKGCONFIG_DIR)/.configured: $(PKGCONFIG_DIR)/.unpacked
-	(cd $(PKGCONFIG_DIR); rm -rf config.cache; \
+$(PKGCONFIG_HOST_DIR)/.configured: $(PKGCONFIG_HOST_DIR)/.unpacked
+	(cd $(@D); rm -rf config.cache; \
 		./configure \
 		--prefix=$(HOST_DIR)/usr \
 		--sysconfdir=$(HOST_DIR)/etc \
@@ -31,21 +33,21 @@ $(PKGCONFIG_DIR)/.configured: $(PKGCONFIG_DIR)/.unpacked
 	)
 	touch $@
 
-$(PKGCONFIG_DIR)/$(PKGCONFIG_BINARY): $(PKGCONFIG_DIR)/.configured
-	$(MAKE) -C $(PKGCONFIG_DIR)
+$(PKGCONFIG_HOST_DIR)/.compiled: $(PKGCONFIG_HOST_DIR)/.configured
+	$(MAKE) -C $(@D)
 
-$(PKGCONFIG_HOST_BINARY): $(PKGCONFIG_DIR)/$(PKGCONFIG_BINARY)
-	$(MAKE) -C $(PKGCONFIG_DIR) install
+$(PKGCONFIG_HOST_BINARY): $(PKGCONFIG_HOST_DIR)/.compiled
+	$(MAKE) -C $(<D) install
 
 host-pkgconfig pkgconfig: $(PKGCONFIG_HOST_BINARY)
 
 host-pkgconfig-clean pkgconfig-clean:
-	rm -f $(addprefix $(PKGCONFIG_DIR)/,.unpacked .configured .compiled)
-	-$(MAKE) -C $(PKGCONFIG_DIR) uninstall
-	-$(MAKE) -C $(PKGCONFIG_DIR) clean
+	rm -f $(addprefix $(PKGCONFIG_HOST_DIR)/,.unpacked .configured .compiled)
+	-$(MAKE) -C $(PKGCONFIG_HOST_DIR) uninstall
+	-$(MAKE) -C $(PKGCONFIG_HOST_DIR) clean
 
 host-pkgconfig-dirclean pkgconfig-dirclean:
-	rm -rf $(PKGCONFIG_DIR)
+	rm -rf $(PKGCONFIG_HOST_DIR)
 
 #############################################################
 #
