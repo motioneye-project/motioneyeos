eb84b8ba872aacbdccffffa49b350c26beeed40e
Peter Korsgaard <jacmet@sunsite.dk>
2009-03-18 21:21:48 +0000

    freetype: get rid of ancient 2.2.1 version

--- package/freetype/freetype.mk
+++ package/freetype/freetype.mk
@@ -3,8 +3,7 @@
 # freetype
 #
 #############################################################
-FREETYPE_VERSION = $(strip $(subst ",, $(BR2_FREETYPE_VERSION)))
-#"))
+FREETYPE_VERSION = 2.3.7
 FREETYPE_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/freetype
 FREETYPE_SOURCE = freetype-$(FREETYPE_VERSION).tar.bz2
 FREETYPE_LIBTOOL_PATCH = NO
