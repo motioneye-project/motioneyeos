396480b41d69be7ef9c84ff4ed693ce38d42f8f3
Peter Korsgaard <jacmet@sunsite.dk>
2009-03-18 11:27:49 +0000

    package: add dbus-python

---
+++ package/dbus-python/dbus-python.mk
@@ -0,0 +1,24 @@
+#############################################################
+#
+# dbus-python
+#
+#############################################################
+DBUS_PYTHON_VERSION = 0.83.0
+DBUS_PYTHON_SOURCE = dbus-python-$(DBUS_PYTHON_VERSION).tar.gz
+DBUS_PYTHON_SITE = http://dbus.freedesktop.org/releases/dbus-python/
+DBUS_PYTHON_INSTALL_STAGING = YES
+DBUS_PYTHON_INSTALL_TARGET = YES
+
+DBUS_PYTHON_CONF_ENV = am_cv_pathless_PYTHON=python \
+		ac_cv_path_PYTHON=$(PYTHON_DIR)/hostpython \
+		am_cv_python_version=$(PYTHON_VERSION) \
+		am_cv_python_platform=linux2 \
+		am_cv_python_pythondir=/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages \
+		am_cv_python_pyexecdir=/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages \
+		am_cv_python_includes=-I$(STAGING_DIR)/usr/include/python$(PYTHON_VERSION_MAJOR)
+
+DBUS_PYTHON_CONF_OPT = --disable-html-docs --disable-api-docs
+
+DBUS_PYTHON_DEPENDENCIES = uclibc dbus-glib libpython
+
+$(eval $(call AUTOTARGETS,package,dbus-python))
