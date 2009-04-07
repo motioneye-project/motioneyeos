5b8c84e717fdc8fcb4217e6f1818fc230b2bb209
Peter Korsgaard <jacmet@sunsite.dk>
2009-04-07 07:16:37 +0000

    rsync: bump version

--- package/rsync/rsync.mk
+++ package/rsync/rsync.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-RSYNC_VERSION:=3.0.3
+RSYNC_VERSION:=3.0.5
 RSYNC_SOURCE:=rsync-$(RSYNC_VERSION).tar.gz
 RSYNC_SITE:=http://rsync.samba.org/ftp/rsync/src
 RSYNC_AUTORECONF:=no
