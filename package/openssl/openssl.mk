bd14b0e70f5b5b1d1e308bab6af0821e5894de57
Peter Korsgaard <jacmet@sunsite.dk>
2009-04-07 07:01:20 +0000

    openssl: bump version
    Patch by Gustavo Zacarias <gustavo@zacarias.com.ar>, closes #217
    
    Fixes multiple security flaws - See
    http://www.openssl.org/news/secadv_20090325.txt for details.

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -3,7 +3,7 @@
 # openssl
 #
 #############################################################
-OPENSSL_VERSION:=0.9.8j
+OPENSSL_VERSION:=0.9.8k
 OPENSSL_SITE:=http://www.openssl.org/source
 
 OPENSSL_TARGET_ARCH=generic32
