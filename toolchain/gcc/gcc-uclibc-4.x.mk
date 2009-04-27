1f3eec6e8fc30b961e9b24ca1d7fc676fcdd41ae
Peter Korsgaard <jacmet@sunsite.dk>
2009-04-27 10:26:56 +0000

    toolchain/gcc: disable tls support
    Fixes libstdc++ linking with gcc 4.4.x

--- toolchain/gcc/gcc-uclibc-4.x.mk
+++ toolchain/gcc/gcc-uclibc-4.x.mk
@@ -206,6 +206,7 @@ $(GCC_BUILD_DIR1)/.configured: $(GCC_DIR)/.patched
 		--with-gnu-ld \
 		--disable-shared \
 		--disable-libssp \
+		--disable-tls \
 		$(GCC_WITH_HOST_GMP) \
 		$(GCC_WITH_HOST_MPFR) \
 		$(DISABLE_NLS) \
@@ -281,6 +282,7 @@ $(GCC_BUILD_DIR2)/.configured: $(GCC_SRC_DIR)/.patched $(GCC_STAGING_PREREQ)
 		--enable-target-optspace \
 		--with-gnu-ld \
 		--disable-libssp \
+		--disable-tls \
 		$(GCC_SHARED_LIBGCC) \
 		$(GCC_WITH_HOST_GMP) \
 		$(GCC_WITH_HOST_MPFR) \
@@ -410,6 +412,7 @@ $(GCC_BUILD_DIR3)/.configured: $(GCC_BUILD_DIR3)/.prepared
 		--disable-__cxa_atexit \
 		--with-gnu-ld \
 		--disable-libssp \
+		--disable-tls \
 		$(GCC_SHARED_LIBGCC) \
 		$(GCC_WITH_TARGET_GMP) \
 		$(GCC_WITH_TARGET_MPFR) \
