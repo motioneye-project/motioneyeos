81f9990e021011146537c106190116d728477329
Will Newton <will.newton@gmail.com>
2009-06-29 11:54:06 +0100

    webkit: Update to WebKit svn r44552.
    Closes #427
    
    - Fix generation of configure script and Makefiles.
     - Add dependencies on libsoup and enchant.
     - Allow configuration of rendering target (currently X11 or DirectFB).
    
    Signed-off-by: Will Newton <will.newton@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/webkit/webkit.mk
+++ package/webkit/webkit.mk
@@ -3,7 +3,7 @@
 # webkit
 #
 #############################################################
-WEBKIT_VERSION = r31321
+WEBKIT_VERSION = r44552
 WEBKIT_SOURCE = WebKit-$(WEBKIT_VERSION).tar.bz2
 WEBKIT_SITE = http://nightly.webkit.org/files/trunk/src/
 
@@ -11,7 +11,24 @@ WEBKIT_INSTALL_STAGING = YES
 WEBKIT_INSTALL_TARGET = YES
 WEBKIT_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) install
 
-WEBKIT_AUTORECONF = YES
-WEBKIT_DEPENDENCIES = icu curl libxml2 libxslt libgtk2 sqlite
+WEBKIT_DEPENDENCIES = icu curl libxml2 libxslt libgtk2 sqlite enchant \
+			libsoup
+
+ifeq ($(BR2_PACKAGE_WEBKIT_X11),y)
+WEBKIT_CONF_OPT = --with-target=x11
+endif
+
+ifeq ($(BR2_PACKAGE_WEBKIT_DIRECTFB),y)
+WEBKIT_CONF_OPT = --with-target=directfb
+endif
+
+WEBKIT_CONF_OPT += --disable-video
 
 $(eval $(call AUTOTARGETS,package,webkit))
+
+$(WEBKIT_HOOK_POST_EXTRACT):
+	$(SED) 's/AUTOMAKE_FLAGS=.*/AUTOMAKE_FLAGS="--foreign --add-missing --copy"/' $(WEBKIT_DIR)/autogen.sh
+	$(SED) 's/LIBTOOLIZE_FLAGS=.*/LIBTOOLIZE_FLAGS="--force --automake --copy"/' $(WEBKIT_DIR)/autogen.sh
+	# Don't run the configure step yet
+	cd $(WEBKIT_DIR); AUTOGEN_CONFIGURE_ARGS=--version ./autogen.sh
+	touch $@
