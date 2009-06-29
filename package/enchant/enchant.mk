2c3a3af86e072b021a4e4924297a6fbab42d321b
Will Newton <will.newton@gmail.com>
2009-06-29 11:26:23 +0100

    enchant: New package for enchant spell-checking library.
    Enchant is a spell checking library that provides a consistent API across
    a number of spell-checking system backends.
    
    Signed-off-by: Will Newton <will.newton@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/enchant/enchant.mk
@@ -0,0 +1,14 @@
+#############################################################
+#
+# enchant
+#
+#############################################################
+ENCHANT_VERSION = 1.5.0
+ENCHANT_SOURCE = enchant-$(ENCHANT_VERSION).tar.gz
+ENCHANT_SITE = http://www.abisource.com/downloads/enchant/$(ENCHANT_VERSION)
+
+ENCHANT_INSTALL_STAGING = YES
+ENCHANT_INSTALL_TARGET = YES
+ENCHANT_LIBTOOL_PATCH = NO
+
+$(eval $(call AUTOTARGETS,package,enchant))
