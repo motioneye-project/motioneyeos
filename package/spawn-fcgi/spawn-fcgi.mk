0468be6e07185a57a8d01c57bfefcc2487a16875
Gustavo Zacarias <gustavo@zacarias.com.ar>
2009-07-01 15:36:19 +0200

    package: add spawn-fcgi package
    Closes #417,
    
    New package spawn-fcgi, basically a FastCGI process spawner.
    This was previously included in lighttpd up to version 1.4.22 and is now
    a separate project.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/spawn-fcgi/spawn-fcgi.mk
@@ -0,0 +1,15 @@
+#############################################################
+#
+# spawn-fcgi
+#
+#############################################################
+
+SPAWN_FCGI_VERSION = 1.6.2
+SPAWN_FCGI_SITE = http://www.lighttpd.net/download
+SPAWN_FCGI_SOURCE = spawn-fcgi-$(SPAWN_FCGI_VERSION).tar.bz2
+
+ifneq ($(BR2_INET_IPV6),y)
+SPAWN_FCGI_CONF_OPT = --disable-ipv6
+endif
+
+$(eval $(call AUTOTARGETS,package,spawn-fcgi))
