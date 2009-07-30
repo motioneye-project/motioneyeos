20417bbf4e8be341c6754e5444c58ad1a25f3096
Chris Packham <judge.packham@gmail.com>
2009-07-30 13:59:49 +1200

    add tcpreplay tool
    Closes #515.
    
    Tcpreplay is a tool for replaying network traffic from files saved with
    tcpdump or other tools which write pcap(3) files.
    
    The tcpreplay suite also ships with other tools (tcprewrite,
    tcpreplay-edit) for manipulating pcap files.
    
    [Peter: minor tweaks]
    Signed-off-by: Chris Packham <judge.packham@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/tcpreplay/tcpreplay.mk
@@ -0,0 +1,16 @@
+#############################################################
+#
+# tcpreplay
+#
+#############################################################
+
+TCPREPLAY_VERSION = 3.4.3
+TCPREPLAY_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/tcpreplay
+TCPREPLAY_LIBTOOL_PATCH = NO
+TCPREPLAY_CONF_ENV = tr_cv_libpcap_version=">= 0.7.0"
+TCPREPLAY_CONF_OPT = --program-prefix="" --with-libpcap=$(STAGING_DIR)/usr
+
+TCPREPLAY_DEPENDENCIES = uclibc libpcap
+
+$(eval $(call AUTOTARGETS,package,tcpreplay))
+
