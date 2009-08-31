cc8a9175980d6f58d3c60071a6e9d64cf9485bda
Kelvin Cheung <keguang.zhang@gmail.com>
2009-08-31 07:23:59 +0200

    ctorrent: build with ssl support if available
    Closes #557
    
    Build with openssl support if that is enabled.
    
    Signed-off-by: Kelvin Cheung <keguang.zhang@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ctorrent/ctorrent.mk
+++ package/ctorrent/ctorrent.mk
@@ -6,6 +6,12 @@
 CTORRENT_VERSION:=dnh3.3.2
 CTORRENT_SOURCE:=ctorrent-$(CTORRENT_VERSION).tar.gz
 CTORRENT_SITE:=http://www.rahul.net/dholmes/ctorrent/
-CTORRENT_CONF_OPT:=--with-ssl=no
+
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+CTORRENT_CONF_OPT+=--with-ssl=yes
+CTORRENT_DEPENDENCIES+=openssl
+else
+CTORRENT_CONF_OPT+=--with-ssl=no
+endif
 
 $(eval $(call AUTOTARGETS,package,ctorrent))
