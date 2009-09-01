6b0d895f641b5309b000e7165c33daf01eefe7f0
Peter Korsgaard <jacmet@sunsite.dk>
2009-09-01 17:00:29 +0200

    xdata_xcursor-themes: -source target should only download, not extract
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xdata_xcursor-themes/xdata_xcursor-themes.mk
+++ package/x11r7/xdata_xcursor-themes/xdata_xcursor-themes.mk
@@ -68,7 +68,7 @@ xdata_xcursor-themes-dirclean:
 	rm -rf $(XDATA_XCURSOR_THEMES_DIR)
 
 xdata_xcursor-themes-depends:
-xdata_xcursor-themes-source: $(XDATA_XCURSOR_THEMES_DIR)/.extracted
+xdata_xcursor-themes-source: $(DL_DIR)/$(XDATA_XCURSOR_THEMES_SOURCE)
 xdata_xcursor-themes-patch: $(XDATA_XCURSOR_THEMES_DIR)/.patched
 xdata_xcursor-themes-configure: $(XDATA_XCURSOR_THEMES_DIR)/.configured
 xdata_xcursor-themes-build: $(XDATA_XCURSOR_THEMES_DIR)/.built
