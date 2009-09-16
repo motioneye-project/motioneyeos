27cf8114bfd8625584c6a631abead2376679e926
Olaf Rempel <razzor@kopf-tisch.de>
2009-09-16 08:39:26 +0200

    iptables: use multipurpose binaries and bump version
    Closes #587
    
    - bump version to 1.4.4
    - iptables-optional-ipv6.patch no longer needed (included in upstream)
    - use ipXtables-multi binaries with symlinks to ipXtables, ipXtables-save,
      ipXtables-restore instead of 3 distinct binaries
    
    Signed-off-by: Olaf Rempel <razzor@kopf-tisch.de>
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/iptables/iptables.mk
+++ package/iptables/iptables.mk
@@ -3,25 +3,34 @@
 # iptables
 #
 #############################################################
-IPTABLES_VERSION = 1.4.2
+IPTABLES_VERSION = 1.4.4
 IPTABLES_SOURCE = iptables-$(IPTABLES_VERSION).tar.bz2
 IPTABLES_SITE = http://ftp.netfilter.org/pub/iptables
 
 IPTABLES_CONF_OPT = --libexecdir=/usr/lib --with-kernel=$(LINUX_HEADERS_DIR)
 ifneq ($(BR2_INET_IPV6),y)
-IPTABLES_CONF_OPT += --enable-ipv6=no
+IPTABLES_CONF_OPT += --disable-ipv6
 endif
 
-IPTABLES_INSTALL_TARGET = YES
-
 IPTABLES_AUTORECONF = YES
-IPTABLES_DEPENDENCIES =
+IPTABLES_LIBTOOL_PATCH = NO
 
 $(eval $(call AUTOTARGETS,package,iptables))
+
+$(IPTABLES_HOOK_POST_INSTALL): $(IPTABLES_TARGET_INSTALL_TARGET)
+	ln -sf iptables-multi $(TARGET_DIR)/usr/sbin/iptables
+	ln -sf iptables-multi $(TARGET_DIR)/usr/sbin/iptables-save
+	ln -sf iptables-multi $(TARGET_DIR)/usr/sbin/iptables-restore
+ifeq ($(BR2_INET_IPV6),y)
+	ln -sf ip6tables-multi $(TARGET_DIR)/usr/sbin/ip6tables
+	ln -sf ip6tables-multi $(TARGET_DIR)/usr/sbin/ip6tables-save
+	ln -sf ip6tables-multi $(TARGET_DIR)/usr/sbin/ip6tables-restore
+endif
+	touch $@
 
 $(IPTABLES_TARGET_UNINSTALL):
 	$(call MESSAGE,"Uninstalling")
 	rm -f $(TARGET_DIR)/usr/bin/iptables-xml
 	rm -f $(TARGET_DIR)/usr/sbin/iptables* $(TARGET_DIR)/usr/sbin/ip6tables*
 	rm -rf $(TARGET_DIR)/usr/lib/xtables
-	rm -f $(IPTABLES_TARGET_INSTALL_TARGET)
+	rm -f $(IPTABLES_TARGET_INSTALL_TARGET) $(IPTABLES_HOOK_POST_INSTALL)
