c3bb9c1cc4e86367ced83f9f7117541172f5570b
Peter Korsgaard <jacmet@sunsite.dk>
2009-10-03 22:45:38 +0200

    shared-mime-info: create post-install-hook stamp file
    Ensures that the hook isn't run every time. Also use the real stamp
    file from the host build instead of host-share-mime-info as dependency,
    as otherwise the target is always considered out-of-date.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/shared-mime-info/shared-mime-info.mk
+++ package/shared-mime-info/shared-mime-info.mk
@@ -66,6 +66,7 @@ host-shared-mime-info-dirclean:
 	rm -rf $(SHARED_MIME_INFO_HOST_DIR)
 
 # update the shared-mime-info database in the target
-$(SHARED_MIME_INFO_HOOK_POST_INSTALL): host-shared-mime-info
+$(SHARED_MIME_INFO_HOOK_POST_INSTALL): $(STAMP_DIR)/host_shared-mime-info_installed
 	$(SHARED_MIME_INFO_HOST_BINARY) $(STAGING_DIR)/usr/share/mime
 	$(INSTALL) -D $(STAGING_DIR)/usr/share/mime/mime.cache $(TARGET_DIR)/usr/share/mime/mime.cache
+	touch $@
