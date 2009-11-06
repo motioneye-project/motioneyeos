e8929df01711b7a5fe893c58fe14989e6e2a64b1
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2009-11-06 19:18:34 +0100

    xproto_xproto: convert to autotools infrastructure for host package
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xproto_xproto/xproto_xproto.mk
+++ package/x11r7/xproto_xproto/xproto_xproto.mk
@@ -12,46 +12,4 @@ XPROTO_XPROTO_INSTALL_STAGING = YES
 XPROTO_XPROTO_INSTALL_TARGET = NO
 
 $(eval $(call AUTOTARGETS,package/x11r7,xproto_xproto))
-
-# xproto_xproto for the host
-XPROTO_XPROTO_HOST_DIR:=$(BUILD_DIR)/xproto_xproto-$(XPROTO_XPROTO_VERSION)-host
-
-$(DL_DIR)/$(XPROTO_XPROTO_SOURCE):
-	$(call DOWNLOAD,$(XPROTO_XPROTO_SITE),$(XPROTO_XPROTO_SOURCE))
-
-$(STAMP_DIR)/host_xproto_xproto_unpacked: $(DL_DIR)/$(XPROTO_XPROTO_SOURCE)
-	mkdir -p $(XPROTO_XPROTO_HOST_DIR)
-	$(INFLATE$(suffix $(XPROTO_XPROTO_SOURCE))) $< | \
-		$(TAR) $(TAR_STRIP_COMPONENTS)=1 -C $(XPROTO_XPROTO_HOST_DIR) $(TAR_OPTIONS) -
-	touch $@
-
-$(STAMP_DIR)/host_xproto_xproto_configured: $(STAMP_DIR)/host_xproto_xproto_unpacked
-	(cd $(XPROTO_XPROTO_HOST_DIR); rm -rf config.cache; \
-		$(HOST_CONFIGURE_OPTS) \
-		CFLAGS="$(HOST_CFLAGS)" \
-		LDFLAGS="$(HOST_LDFLAGS)" \
-		./configure $(QUIET) \
-		--prefix="$(HOST_DIR)/usr" \
-		--sysconfdir="$(HOST_DIR)/etc" \
-	)
-	touch $@
-
-$(STAMP_DIR)/host_xproto_xproto_compiled: $(STAMP_DIR)/host_xproto_xproto_configured
-	$(HOST_MAKE_ENV) $(MAKE) -C $(XPROTO_XPROTO_HOST_DIR)
-	touch $@
-
-$(STAMP_DIR)/host_xproto_xproto_installed: $(STAMP_DIR)/host_xproto_xproto_compiled
-	$(MAKE) -C $(XPROTO_XPROTO_HOST_DIR) install
-	touch $@
-
-host-xproto_xproto: $(STAMP_DIR)/host_xproto_xproto_installed
-
-host-xproto_xproto-source: xproto_xproto-source
-
-host-xproto_xproto-clean:
-	rm -f $(addprefix $(STAMP_DIR)/host_xproto_xproto_,unpacked configured compiled installed)
-	-$(MAKE) -C $(XPROTO_XPROTO_HOST_DIR) uninstall
-	-$(MAKE) -C $(XPROTO_XPROTO_HOST_DIR) clean
-
-host-xproto_xproto-dirclean:
-	rm -rf $(XPROTO_XPROTO_HOST_DIR)
+$(eval $(call AUTOTARGETS,package/x11r7,xproto_xproto,host))
