b9c569347d24a0ba90240ec6d74235375c320172
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2009-11-06 19:18:50 +0100

    xutil_makedepend: convert to autotools infrastructure for host package
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xutil_makedepend/xutil_makedepend.mk
+++ package/x11r7/xutil_makedepend/xutil_makedepend.mk
@@ -11,47 +11,7 @@ XUTIL_MAKEDEPEND_AUTORECONF = NO
 XUTIL_MAKEDEPEND_INSTALL_STAGING = NO
 XUTIL_MAKEDEPEND_INSTALL_TARGET = YES
 
+HOST_XUTIL_MAKEDEPEND_DEPENDENCIES = host-xproto-xproto
+
 $(eval $(call AUTOTARGETS,package/x11r7,xutil_makedepend))
-
-# makedepend for the host
-MAKEDEPEND_HOST_DIR:=$(BUILD_DIR)/makedepend-$(XUTIL_MAKEDEPEND_VERSION)-host
-
-$(DL_DIR)/$(XUTIL_MAKEDEPEND_SOURCE):
-	$(call DOWNLOAD,$(XUTIL_MAKEDEPEND_SITE),$(XUTIL_MAKEDEPEND_SOURCE))
-
-$(STAMP_DIR)/host_makedepend_unpacked: $(DL_DIR)/$(XUTIL_MAKEDEPEND_SOURCE)
-	mkdir -p $(MAKEDEPEND_HOST_DIR)
-	$(INFLATE$(suffix $(XUTIL_MAKEDEPEND_SOURCE))) $< | \
-		$(TAR) $(TAR_STRIP_COMPONENTS)=1 -C $(MAKEDEPEND_HOST_DIR) $(TAR_OPTIONS) -
-	touch $@
-
-$(STAMP_DIR)/host_makedepend_configured: $(STAMP_DIR)/host_makedepend_unpacked $(STAMP_DIR)/host_xproto_xproto_installed
-	(cd $(MAKEDEPEND_HOST_DIR); rm -rf config.cache; \
-		$(HOST_CONFIGURE_OPTS) \
-		CFLAGS="$(HOST_CFLAGS)" \
-		LDFLAGS="$(HOST_LDFLAGS)" \
-		./configure $(QUIET) \
-		--prefix="$(HOST_DIR)/usr" \
-		--sysconfdir="$(HOST_DIR)/etc" \
-	)
-	touch $@
-
-$(STAMP_DIR)/host_makedepend_compiled: $(STAMP_DIR)/host_makedepend_configured
-	$(HOST_MAKE_ENV) $(MAKE) -C $(MAKEDEPEND_HOST_DIR)
-	touch $@
-
-$(STAMP_DIR)/host_makedepend_installed: $(STAMP_DIR)/host_makedepend_compiled
-	$(MAKE) -C $(MAKEDEPEND_HOST_DIR) install
-	touch $@
-
-host-makedepend: $(STAMP_DIR)/host_makedepend_installed
-
-host-makedepend-source: makedepend-source
-
-host-makedepend-clean:
-	rm -f $(addprefix $(STAMP_DIR)/host_makedepend_,unpacked configured compiled installed)
-	-$(MAKE) -C $(MAKEDEPEND_HOST_DIR) uninstall
-	-$(MAKE) -C $(MAKEDEPEND_HOST_DIR) clean
-
-host-makedepend-dirclean:
-	rm -rf $(MAKEDEPEND_HOST_DIR)
+$(eval $(call AUTOTARGETS,package/x11r7,xutil_makedepend,host))
