ab92ca91ac3ef3862b7866bd73a47bf9aa738311
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2009-11-06 19:15:48 +0100

    lzo: convert to autotools infrastructure for host package
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lzo/lzo.mk
+++ package/lzo/lzo.mk
@@ -12,45 +12,4 @@ LZO_INSTALL_TARGET = YES
 LZO_INSTALL_STAGING_OPT = CC="$(TARGET_CC)" DESTDIR=$(STAGING_DIR) install
 
 $(eval $(call AUTOTARGETS,package,lzo))
-
-# lzo for the host
-LZO_HOST_DIR:=$(BUILD_DIR)/lzo-$(LZO_VERSION)-host
-
-$(DL_DIR)/$(LZO_SOURCE):
-	$(call DOWNLOAD,$(LZO_SITE),$(LZO_SOURCE))
-
-$(STAMP_DIR)/host_lzo_unpacked: $(DL_DIR)/$(LZO_SOURCE)
-	mkdir -p $(LZO_HOST_DIR)
-	$(INFLATE$(suffix $(LZO_SOURCE))) $< | \
-		$(TAR) $(TAR_STRIP_COMPONENTS)=1 -C $(LZO_HOST_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(LZO_HOST_DIR) package/lzo/ \*.patch
-	touch $@
-
-$(STAMP_DIR)/host_lzo_configured: $(STAMP_DIR)/host_lzo_unpacked
-	(cd $(LZO_HOST_DIR); rm -rf config.cache; \
-		$(HOST_CONFIGURE_OPTS) \
-		CFLAGS="$(HOST_CFLAGS)" \
-		LDFLAGS="$(HOST_LDFLAGS)" \
-		./configure $(QUIET) \
-		--prefix="$(HOST_DIR)/usr" \
-		--sysconfdir="$(HOST_DIR)/etc" \
-	)
-	touch $@
-
-$(STAMP_DIR)/host_lzo_compiled: $(STAMP_DIR)/host_lzo_configured
-	$(MAKE) -C $(LZO_HOST_DIR)
-	touch $@
-
-$(STAMP_DIR)/host_lzo_installed: $(STAMP_DIR)/host_lzo_compiled
-	$(MAKE) -C $(LZO_HOST_DIR) install
-	touch $@
-
-host-lzo: $(STAMP_DIR)/host_lzo_installed
-
-host-lzo-clean:
-	rm -f $(addprefix $(STAMP_DIR)/host_lzo_,unpacked configured compiled installed)
-	-$(MAKE) -C $(LZO_HOST_DIR) uninstall
-	-$(MAKE) -C $(LZO_HOST_DIR) clean
-
-host-lzo-dirclean:
-	rm -rf $(LZO_HOST_DIR)
+$(eval $(call AUTOTARGETS,package,lzo,host))
