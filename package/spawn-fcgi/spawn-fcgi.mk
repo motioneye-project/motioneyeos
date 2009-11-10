3848cd6dcafb31e7f4d5204d295dff9d3698576d
Gustavo Zacarias <gustavo@zacarias.com.ar>
2009-11-10 12:01:09 -0300

    Bump spawn-fcgi to 1.6.3
    Closes #705
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/spawn-fcgi/spawn-fcgi.mk
+++ package/spawn-fcgi/spawn-fcgi.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SPAWN_FCGI_VERSION = 1.6.2
+SPAWN_FCGI_VERSION = 1.6.3
 SPAWN_FCGI_SITE = http://www.lighttpd.net/download
 SPAWN_FCGI_SOURCE = spawn-fcgi-$(SPAWN_FCGI_VERSION).tar.bz2
 
