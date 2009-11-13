83f9881d64cd7bcd760bc935e992e95ef3136b93
Gustavo Zacarias <gustavo@zacarias.com.ar>
2009-11-13 12:07:56 -0300

    libidn: update to 1.15 and other fixes
    Closes #715
    
    * Add target uninstall.
    * Remove stray /usr/share/emacs files.
    * Remove stray mono (host, autodetected) dll installation in
      $(TARGET_DIR)/usr/lib by disabling C# altogether.
    * Disable java.
    * Add new option to install idn binary (default N, usually not required).
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libidn/libidn.mk
+++ package/libidn/libidn.mk
@@ -3,12 +3,26 @@
 # libidn
 #
 #############################################################
-LIBIDN_VERSION = 1.9
-LIBIDN_SOURCE = libidn-$(LIBIDN_VERSION).tar.gz
+
+LIBIDN_VERSION = 1.15
 LIBIDN_SITE = http://ftp.gnu.org/gnu/libidn/
 LIBIDN_INSTALL_STAGING = YES
 LIBIDN_INSTALL_TARGET = YES
-LIBIDN_CONF_OPT = --enable-shared
+LIBIDN_CONF_OPT = --enable-shared --disable-java --enable-csharp=no
+LIBIDN_LIBTOOL_PATCH = NO
 LIBIDN_DEPENDENCIES = host-pkgconfig gettext $(if $(BR2_PACKAGE_LIBICONV),libiconv)
 
 $(eval $(call AUTOTARGETS,package,libidn))
+
+$(LIBIDN_HOOK_POST_INSTALL):
+ifneq ($(BR2_PACKAGE_LIBIDN_BINARY),y)
+	rm -f $(TARGET_DIR)/usr/bin/idn
+endif
+	rm -rf $(TARGET_DIR)/usr/share/emacs
+	touch $@
+
+$(LIBIDN_TARGET_UNINSTALL):
+	$(call MESSAGE,"Uninstalling")
+	rm -f $(TARGET_DIR)/usr/lib/libidn*
+	rm -f $(TARGET_DIR)/usr/bin/idn
+	rm -f $(LIBIDN_TARGET_INSTALL_TARGET) $(LIBIDN_HOOK_POST_INSTALL)
