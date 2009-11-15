8fb159e14002d7a0ac51d5a531b1015dabb2f05a
Lionel Landwerlin <llandwerlin@gmail.com>
2009-11-15 11:12:54 +0100

    gperf: convert to newstyle autotools infrastructure
    Signed-off-by: Lionel Landwerlin <llandwerlin@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gperf/gperf.mk
+++ package/gperf/gperf.mk
@@ -9,12 +9,8 @@ GPERF_SITE = $(BR2_GNU_MIRROR)/gperf
 GPERF_AUTORECONF = NO
 GPERF_INSTALL_STAGING = NO
 GPERF_INSTALL_TARGET = YES
-GPERF_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) install
+
+# install-strip rule does not exist in gperf's makefiles
 GPERF_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
 $(eval $(call AUTOTARGETS,package,gperf))
-
-$(GPERF_HOOK_POST_INSTALL): $(GPERF_TARGET_INSTALL_TARGET)
-	$(STRIPCMD) $(STRIP_STRIP_ALL) $(TARGET_DIR)/usr/bin/gperf
-	touch $@
-
