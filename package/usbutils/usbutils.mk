d8968c6794d3cee4e2ed228745b0783c09aff59a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2009-11-30 12:58:49 -0300

    Bump usbutils package to 0.86
    Closes #749.
    
    [Peter: remove redundant ac_cv_func_malloc_0_nonnull setting]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/usbutils/usbutils.mk
+++ package/usbutils/usbutils.mk
@@ -3,59 +3,26 @@
 # usbutils
 #
 #############################################################
-USBUTILS_VERSION:=0.72
-USBUTILS_SOURCE:=usbutils-$(USBUTILS_VERSION).tar.gz
-USBUTILS_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/linux-usb/
-USBUTILS_DIR:=$(BUILD_DIR)/usbutils-$(USBUTILS_VERSION)
-USBUTILS_CAT:=$(ZCAT)
-USBUTILS_BINARY:=lsusb
-USBUTILS_TARGET_BINARY:=usr/sbin/lsusb
 
-$(DL_DIR)/$(USBUTILS_SOURCE):
-	$(call DOWNLOAD,$(USBUTILS_SITE),$(USBUTILS_SOURCE))
+USBUTILS_VERSION = 0.86
+USBUTILS_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/linux-usb/
+USBUTILS_DEPENDENCIES = host-pkgconfig
 
-usbutils-source: $(DL_DIR)/$(USBUTILS_SOURCE)
-
-usbutils-unpacked: $(USBUTILS_DIR)/.unpacked
-$(USBUTILS_DIR)/.unpacked: $(DL_DIR)/$(USBUTILS_SOURCE)
-	$(USBUTILS_CAT) $(DL_DIR)/$(USBUTILS_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	touch $(USBUTILS_DIR)/.unpacked
-
-$(USBUTILS_DIR)/.configured: $(USBUTILS_DIR)/.unpacked
-	(cd $(USBUTILS_DIR); rm -rf config.cache; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		ac_cv_func_malloc_0_nonnull=yes \
-		./configure $(QUIET) \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--prefix=/usr \
-	)
-	touch $(USBUTILS_DIR)/.configured
-
-$(USBUTILS_DIR)/$(USBUTILS_BINARY): $(USBUTILS_DIR)/.configured
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(USBUTILS_DIR)
-
-$(TARGET_DIR)/$(USBUTILS_TARGET_BINARY): $(USBUTILS_DIR)/$(USBUTILS_BINARY)
-	$(MAKE) -C $(USBUTILS_DIR) DESTDIR=$(TARGET_DIR) install
-	rm -rf $(TARGET_DIR)/usr/man
-
-usbutils: libusb $(TARGET_DIR)/$(USBUTILS_TARGET_BINARY)
-
-usbutils-clean:
-	rm -f $(TARGET_DIR)/$(USBUTILS_TARGET_BINARY)
-	rm -f $(TARGET_DIR)/usr/share/usb.ids
-	rmdir --ignore-fail-on-non-empty $(TARGET_DIR)/usr/share
-	-$(MAKE) -C $(USBUTILS_DIR) clean
-
-usbutils-dirclean:
-	rm -rf $(USBUTILS_DIR)
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_USBUTILS),y)
-TARGETS+=usbutils
+ifeq ($(BR2_PACKAGE_USBUTILS_ZLIB),y)
+	USBUTILS_DEPENDENCIES += zlib
+else
+	USBUTILS_CONF_OPT = --disable-zlib
 endif
+
+$(eval $(call AUTOTARGETS,package,usbutils))
+
+$(USBUTILS_HOOK_POST_INSTALL):
+	rm -f $(TARGET_DIR)/usr/bin/usb-devices
+	rm -f $(TARGET_DIR)/usr/sbin/update-usbids.sh
+	rm -f $(TARGET_DIR)/usr/share/pkgconfig/usbutils.pc
+ifeq ($(BR2_PACKAGE_USBUTILS_ZLIB),y)
+	rm -f $(TARGET_DIR)/usr/share/usb.ids
+else
+	rm -f $(TARGET_DIR)/usr/share/usb.ids.gz
+endif
+	touch $@
