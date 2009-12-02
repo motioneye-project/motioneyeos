9f17728fb5a2bab29e2764c2def51cd732718d9b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2009-12-02 12:39:21 -0300

    Bump pcre to 7.9
    Closes #763.
    
    [Peter: Only remove pcre-config from target if !BR2_HAVE_DEVFILES]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/pcre/pcre.mk
+++ package/pcre/pcre.mk
@@ -3,11 +3,10 @@
 # PCRE
 #
 #############################################################
-PCRE_VERSION = 7.7
-PCRE_SOURCE = pcre-$(PCRE_VERSION).tar.bz2
+
+PCRE_VERSION = 7.9
 PCRE_SITE = ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre
 PCRE_INSTALL_STAGING = YES
-PCRE_INSTALL_TARGET = YES
 
 ifneq ($(BR2_INSTALL_LIBSTDCPP),y)
 # pcre will use the host g++ if a cross version isn't available
@@ -21,4 +20,7 @@ $(PCRE_HOOK_POST_INSTALL): $(PCRE_TARGET_INSTALL_TARGET)
 		-e 's,^exec_prefix=.*,exec_prefix=$(STAGING_DIR)/usr,' \
 		$(STAGING_DIR)/usr/bin/pcre-config
 	rm -rf $(TARGET_DIR)/usr/share/doc/pcre
+ifneq ($(BR2_HAVE_DEVFILES),y)
+	rm -f $(TARGET_DIR)/usr/bin/pcre-config
+endif
 	touch $@
