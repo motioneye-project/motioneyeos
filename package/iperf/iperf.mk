6a3a58f137ecdd493cacc8981b6fb230dc022390
Richard van Paasen <rvpaasen@t3i.nl>
2009-12-14 22:54:12 +0100

    iperf: update to 2.0.4
    Closes #800
    
    Signed-off-by: Richard van Paasen <rvpaasen@t3i.nl>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/iperf/iperf.mk
+++ package/iperf/iperf.mk
@@ -3,11 +3,21 @@
 # iperf
 #
 #############################################################
+IPERF_VERSION = 2.0.4
+IPERF_SOURCE = iperf-$(IPERF_VERSION).tar.gz
+IPERF_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/iperf
 
-IPERF_VERSION:=2.0.3
-IPERF_SOURCE:=iperf-$(IPERF_VERSION).tar.gz
-IPERF_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/iperf
-IPERF_AUTORECONF:=NO
-IPERF_CONF_OPT:=--disable-dependency-tracking --disable-web100
+IPERF_AUTORECONF = NO
+FETCHMAIL_LIBTOOL_PATCH = NO
+
+IPERF_INSTALL_STAGING = NO
+IPERF_INSTALL_TARGET = YES
+
+IPERF_CONF_ENV = \
+	ac_cv_func_malloc_0_nonnull=yes
+
+IPERF_CONF_OPT = \
+	--disable-dependency-tracking \
+	--disable-web100
 
 $(eval $(call AUTOTARGETS,package,iperf))
