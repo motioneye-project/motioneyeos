35a478e8f53613cda0f1ddbdc09db8e0a02c4a3c
Francisco Gonzalez <gzmorell@gmail.com>
2009-12-16 17:40:23 +0100

    New package ser2net request
    [Peter: whitespace fixes / cleanups]
    Signed-off-by: Francisco Gonzalez <gzmorell@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/ser2net/ser2net.mk
@@ -0,0 +1,11 @@
+#############################################################
+#
+# ser2net
+#
+#############################################################
+
+SER2NET_VERSION = 2.6
+SER2NET_SOURCE = ser2net-$(SER2NET_VERSION).tar.gz
+SER2NET_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/ser2net
+
+$(eval $(call AUTOTARGETS,package,ser2net))
