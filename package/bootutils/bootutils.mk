4d190ec0bfccedbc2d84f76f4de5bd8e7ed5d612
Nigel Kukard <nkukard@lbsd.net>
2009-12-25 10:34:58 +0000

    Bump bootutils

--- package/bootutils/bootutils.mk
+++ package/bootutils/bootutils.mk
@@ -3,7 +3,7 @@
 # bootutils
 #
 #############################################################
-BOOTUTILS_VERSION = 0.0.9
+BOOTUTILS_VERSION = 1.0.0
 BOOTUTILS_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/bootutils
 
 BOOTUTILS_CONF_OPT = --prefix=/ --exec-prefix=/
