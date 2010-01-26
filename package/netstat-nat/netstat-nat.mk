60ce1b0842de5897b943017c404855c25ffe33da
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-01-26 11:47:42 -0300

    Bump netstat-nat to 1.4.10
    Closes #967.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/netstat-nat/netstat-nat.mk
+++ package/netstat-nat/netstat-nat.mk
@@ -4,8 +4,7 @@
 #
 #############################################################
 
-NETSTAT_NAT_VERSION = 1.4.9
-NETSTAT_NAT_SOURCE = netstat-nat-$(NETSTAT_NAT_VERSION).tar.gz
+NETSTAT_NAT_VERSION = 1.4.10
 NETSTAT_NAT_SITE = http://tweegy.nl/download
 
 $(eval $(call AUTOTARGETS,package,netstat-nat))
