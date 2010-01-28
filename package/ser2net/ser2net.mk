62a4149250f42539e8300b9391ce1e0e8bf114ee
Peter Korsgaard <jacmet@sunsite.dk>
2010-01-28 16:30:37 +0100

    ser2net: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ser2net/ser2net.mk
+++ package/ser2net/ser2net.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SER2NET_VERSION = 2.6
+SER2NET_VERSION = 2.7
 SER2NET_SOURCE = ser2net-$(SER2NET_VERSION).tar.gz
 SER2NET_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/ser2net
 
