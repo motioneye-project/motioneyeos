5d4aeeda7e66680ee4903ce8dafa9fc026ef8960
Peter Korsgaard <jacmet@sunsite.dk>
2010-02-08 21:05:38 +0100

    rdesktop: convert to Makefile.autotools.in format
    And also adapt X depency. Rdesktop only needs libX11/Xt, and long term
    we should be able to use the X stuff without having a X server on the
    target.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/rdesktop/rdesktop.mk
+++ package/rdesktop/rdesktop.mk
@@ -4,55 +4,12 @@
 #
 #############################################################
 
-RDESKTOP_VERSION=1.5.0
-RDESKTOP_SOURCE=rdesktop-$(RDESKTOP_VERSION).tar.gz
-RDESKTOP_SITE=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/rdesktop/
-RDESKTOP_DIR=$(BUILD_DIR)/rdesktop-$(RDESKTOP_VERSION)
-RDESKTOP_CAT:=$(ZCAT)
+RDESKTOP_VERSION = 1.5.0
+RDESKTOP_SOURCE = rdesktop-$(RDESKTOP_VERSION).tar.gz
+RDESKTOP_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/rdesktop/
+# no install-strip / install-exec
+RDESKTOP_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
+RDESKTOP_DEPENDENCIES = openssl xlib_libX11 xlib_libXt
+RDESKTOP_CONF_OPT = --with-openssl=$(STAGING_DIR)/usr
 
-$(DL_DIR)/$(RDESKTOP_SOURCE):
-	$(call DOWNLOAD,$(RDESKTOP_SITE),$(RDESKTOP_SOURCE))
-
-$(RDESKTOP_DIR)/.unpacked: $(DL_DIR)/$(RDESKTOP_SOURCE)
-	$(RDESKTOP_CAT) $(DL_DIR)/$(RDESKTOP_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(RDESKTOP_DIR) package/rdesktop/ \*.patch*
-	touch $(RDESKTOP_DIR)/.unpacked
-
-$(RDESKTOP_DIR)/.configured: $(RDESKTOP_DIR)/.unpacked
-	(cd $(RDESKTOP_DIR); rm -rf config.cache; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		./configure $(QUIET) \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--prefix=/usr \
-		--with-openssl=$(STAGING_DIR)/usr \
-		--sysconfdir=/etc \
-	)
-	touch $(RDESKTOP_DIR)/.configured
-
-$(RDESKTOP_DIR)/rdesktop: $(RDESKTOP_DIR)/.configured
-	$(MAKE) CC=$(TARGET_CC) -C $(RDESKTOP_DIR)
-
-$(TARGET_DIR)/usr/bin/rdesktop: $(RDESKTOP_DIR)/rdesktop
-	cp $^ $@
-
-rdesktop: openssl xserver_xorg-server $(TARGET_DIR)/usr/bin/rdesktop
-
-rdesktop-source: $(DL_DIR)/$(RDESKTOP_SOURCE)
-
-rdesktop-clean:
-	-$(MAKE) -C $(RDESKTOP_DIR) clean
-	rm -f $(TARGET_DIR)/usr/bin/rdesktop
-
-rdesktop-dirclean:
-	rm -rf $(RDESKTOP_DIR) $(RDESKTOP_DIR)
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_RDESKTOP),y)
-TARGETS+=rdesktop
-endif
+$(eval $(call AUTOTARGETS,package,rdesktop))
