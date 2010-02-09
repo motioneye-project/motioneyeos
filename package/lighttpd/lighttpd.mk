d5d48254dc539dfb3f50d18fa3d72a1effd74267
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-02-09 11:30:06 -0300

    Bump lighttpd to 1.4.26
    Closes #1063.
    
    lighttpd 1.4.26 fixes:
    
    * Request parser handling for splitted header data
    * FD leak in mod_cgi
    * Segfault with broken configs in mod_rewrite/mod_redirect
    * An OOM/DoS vulnerability (CVE-2010-0295)
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/lighttpd/lighttpd.mk
+++ package/lighttpd/lighttpd.mk
@@ -4,10 +4,9 @@
 #
 #############################################################
 
-LIGHTTPD_VERSION = 1.4.25
+LIGHTTPD_VERSION = 1.4.26
 LIGHTTPD_SITE = http://download.lighttpd.net/lighttpd/releases-1.4.x
 LIGHTTPD_LIBTOOL_PATCH = NO
-LIGHTTPD_DEPENDENCIES =
 
 ifneq ($(BR2_LARGEFILE),y)
 LIGHTTPD_LFS:=$(DISABLE_LARGEFILE) --disable-lfs
