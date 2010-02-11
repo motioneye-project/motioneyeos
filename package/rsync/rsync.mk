a3609f35398042ebca8231a34ca8806a018ff0fb
Julien Boibessot <julien.boibessot@armadeus.com>
2010-02-11 13:46:47 +0100

    Updates rsync source repository
    rsync source repository has moved (?).
    Bump version while we are at it.
    
    Signed-off-by: Julien Boibessot <julien.boibessot@armadeus.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/rsync/rsync.mk
+++ package/rsync/rsync.mk
@@ -4,9 +4,9 @@
 #
 #############################################################
 
-RSYNC_VERSION:=3.0.6
+RSYNC_VERSION:=3.0.7
 RSYNC_SOURCE:=rsync-$(RSYNC_VERSION).tar.gz
-RSYNC_SITE:=http://rsync.samba.org/ftp/rsync
+RSYNC_SITE:=http://rsync.samba.org/ftp/rsync/src
 RSYNC_INSTALL_STAGING:=NO
 RSYNC_INSTALL_TARGET:=YES
 RSYNC_CONF_OPT=$(if $(BR2_ENABLE_DEBUG),--enable-debug,--disable-debug)
