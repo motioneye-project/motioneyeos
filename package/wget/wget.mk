8fe6cc98d9211fbb5d7e18f16cc97d4a525a0126
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-02-18 14:51:51 -0300

    wget: bump to 1.12 and migrate to Makefile.autotools.in
    Closes #1111.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/wget/wget.mk
+++ package/wget/wget.mk
@@ -3,58 +3,14 @@
 # wget
 #
 #############################################################
-WGET_VERSION:=1.10.2
-WGET_SOURCE:=wget-$(WGET_VERSION).tar.gz
-WGET_SITE:=$(BR2_GNU_MIRROR)/wget
-WGET_DIR:=$(BUILD_DIR)/wget-$(WGET_VERSION)
-WGET_CAT:=$(ZCAT)
-WGET_BINARY:=src/wget
-WGET_TARGET_BINARY:=bin/wget
 
-$(DL_DIR)/$(WGET_SOURCE):
-	$(call DOWNLOAD,$(WGET_SITE),$(WGET_SOURCE))
+WGET_VERSION = 1.12
+WGET_SITE = $(BR2_GNU_MIRROR)/wget
+WGET_CONF_OPT = $(DISABLE_IPV6)
 
-wget-source: $(DL_DIR)/$(WGET_SOURCE)
-
-$(WGET_DIR)/.unpacked: $(DL_DIR)/$(WGET_SOURCE)
-	$(WGET_CAT) $(DL_DIR)/$(WGET_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	$(CONFIG_UPDATE) $(WGET_DIR)
-	touch $@
-
-$(WGET_DIR)/.configured: $(WGET_DIR)/.unpacked
-	(cd $(WGET_DIR); rm -rf config.cache; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		./configure $(QUIET) \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--prefix=/ \
-		--disable-ipv6 \
-		$(DISABLE_NLS) \
-		--without-ssl \
-	)
-	touch $@
-
-$(WGET_DIR)/$(WGET_BINARY): $(WGET_DIR)/.configured
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(WGET_DIR)
-
-$(TARGET_DIR)/$(WGET_TARGET_BINARY): $(WGET_DIR)/$(WGET_BINARY)
-	install -D $(WGET_DIR)/$(WGET_BINARY) $(TARGET_DIR)/$(WGET_TARGET_BINARY)
-
-wget: $(TARGET_DIR)/$(WGET_TARGET_BINARY)
-
-wget-clean:
-	rm -f $(TARGET_DIR)/$(WGET_TARGET_BINARY)
-	-$(MAKE) -C $(WGET_DIR) clean
-
-wget-dirclean:
-	rm -rf $(WGET_DIR)
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_WGET),y)
-TARGETS+=wget
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+	WGET_CONF_OPT += --with-ssl --with-libssl-prefix=$(STAGING_DIR)
+	WGET_DEPENDENCIES += openssl
 endif
+
+$(eval $(call AUTOTARGETS,package,wget))
