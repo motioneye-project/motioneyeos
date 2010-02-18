19f1506ac880135e210f3533b291d7f66feec0cd
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-02-18 16:01:01 -0300

    Bump nano to 2.2.3 and migrate to Makefile.autotools.in
    Closes #1117
    
    [Peter: get rid of redundant DISABLE_LARGEFILE, trailing spaces]
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/editors/nano/nano.mk
+++ package/editors/nano/nano.mk
@@ -3,57 +3,22 @@
 # nano
 #
 #############################################################
-NANO_VERSION:=1.3.12
-NANO_SOURCE:=nano-$(NANO_VERSION).tar.gz
-NANO_SITE:=http://www.nano-editor.org/dist/v1.3/
-NANO_DIR:=$(BUILD_DIR)/nano-$(NANO_VERSION)
-NANO_CAT:=$(ZCAT)
-NANO_BINARY:=src/nano
-NANO_TARGET_BINARY:=usr/bin/nano
 
-$(DL_DIR)/$(NANO_SOURCE):
-	$(call DOWNLOAD,$(NANO_SITE),$(NANO_SOURCE))
+NANO_VERSION = 2.2.3
+NANO_SITE = http://www.nano-editor.org/dist/v2.2
+NANO_MAKE_ENV = CURSES_LIB="-lncurses"
+NANO_CONF_OPT = --without-slang --enable-tiny
+NANO_DEPENDENCIES = ncurses
 
-nano-source: $(DL_DIR)/$(NANO_SOURCE)
+$(eval $(call AUTOTARGETS,package,nano))
 
-$(NANO_DIR)/.unpacked: $(DL_DIR)/$(NANO_SOURCE)
-	$(NANO_CAT) $(DL_DIR)/$(NANO_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	$(CONFIG_UPDATE) $(NANO_DIR)
+$(NANO_TARGET_INSTALL_TARGET):
+	$(call MESSAGE,"Installing to target")
+	$(INSTALL) -m 0755 $(NANO_DIR)/src/nano \
+		$(TARGET_DIR)/usr/bin/nano
 	touch $@
 
-$(NANO_DIR)/.configured: $(NANO_DIR)/.unpacked
-	(cd $(NANO_DIR); rm -rf config.cache; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		ac_cv_header_regex_h=no \
-		./configure $(QUIET) \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--prefix=/ \
-		--enable-tiny \
-	)
-	touch $@
-
-$(NANO_DIR)/$(NANO_BINARY): $(NANO_DIR)/.configured
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(NANO_DIR)
-
-$(TARGET_DIR)/$(NANO_TARGET_BINARY): $(NANO_DIR)/$(NANO_BINARY)
-	install -D $(NANO_DIR)/$(NANO_BINARY) $(TARGET_DIR)/$(NANO_TARGET_BINARY)
-
-nano: ncurses $(TARGET_DIR)/$(NANO_TARGET_BINARY)
-
-nano-clean:
-	rm -f $(TARGET_DIR)/$(NANO_TARGET_BINARY)
-	-$(MAKE) -C $(NANO_DIR) clean
-
-nano-dirclean:
-	rm -rf $(NANO_DIR)
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_NANO),y)
-TARGETS+=nano
-endif
+$(NANO_TARGET_UNINSTALL):
+	$(call MESSAGE,"Uninstalling")
+	rm -f $(TARGET_DIR)/usr/bin/nano
+	rm -f $(NANO_TARGET_INSTALL_TARGET) $(NANO_HOOK_POST_INSTALL)
