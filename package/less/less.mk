4f0361ab2ca4f25207c84b557e31319c9a417a76
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-02-18 16:15:58 -0300

    Bump less to 436 and migrate to Makefile.autotools.in
    Closes #1123
    
    [Peter: get rid of redundant DISABLE_LARGEFILE]
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/less/less.mk
+++ package/less/less.mk
@@ -3,58 +3,19 @@
 # less
 #
 #############################################################
-LESS_VERSION:=394
-LESS_SOURCE:=less-$(LESS_VERSION).tar.gz
-LESS_SITE:=http://www.greenwoodsoftware.com/less
-LESS_DIR:=$(BUILD_DIR)/less-$(LESS_VERSION)
-LESS_BINARY:=less
-LESS_TARGET_BINARY:=usr/bin/less
 
-$(DL_DIR)/$(LESS_SOURCE):
-	$(call DOWNLOAD,$(LESS_SITE),$(LESS_SOURCE))
+LESS_VERSION = 436
+LESS_SITE = http://www.greenwoodsoftware.com/less
 
-$(LESS_DIR)/.source: $(DL_DIR)/$(LESS_SOURCE)
-	$(ZCAT) $(DL_DIR)/$(LESS_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+$(eval $(call AUTOTARGETS,package,less))
+
+$(LESS_TARGET_INSTALL_TARGET):
+	$(call MESSAGE,"Installing to target")
+	$(INSTALL) -m 0755 $(LESS_DIR)/less \
+		$(TARGET_DIR)/usr/bin/less
 	touch $@
 
-$(LESS_DIR)/.configured: $(LESS_DIR)/.source
-	(cd $(LESS_DIR); rm -f config.cache; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		./configure $(QUIET) \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--prefix=/usr \
-		--sysconfdir=/etc \
-	)
-	touch $@
-
-$(LESS_DIR)/$(LESS_BINARY): $(LESS_DIR)/.configured
-	$(MAKE) CC=$(TARGET_CC) -C $(LESS_DIR)
-
-$(TARGET_DIR)/$(LESS_TARGET_BINARY): $(LESS_DIR)/$(LESS_BINARY)
-	$(MAKE) prefix=$(TARGET_DIR)/usr -C $(LESS_DIR) install
-ifneq ($(BR2_HAVE_MANPAGES),y)
-	rm -Rf $(TARGET_DIR)/usr/man
-endif
-
-less: ncurses $(TARGET_DIR)/$(LESS_TARGET_BINARY)
-
-less-source: $(DL_DIR)/$(LESS_SOURCE)
-
-less-clean:
-	$(MAKE) prefix=$(TARGET_DIR)/usr -C $(LESS_DIR) uninstall
-	-$(MAKE) -C $(LESS_DIR) clean
-
-less-dirclean:
-	rm -rf $(LESS_DIR)
-
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_LESS),y)
-TARGETS+=less
-endif
+$(LESS_TARGET_UNINSTALL):
+	$(call MESSAGE,"Uninstalling")
+	rm -f $(TARGET_DIR)/usr/bin/less
+	rm -f $(LESS_TARGET_INSTALL_TARGET) $(LESS_HOOK_POST_INSTALL)
