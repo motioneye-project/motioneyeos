e5d1244a747a3863b9f53c80c3ad3f0d60fbe8cd
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2010-02-20 18:13:18 +0100

    libxcb: fix build with Buildroot compiled xsltproc
    Make sure that libxcb depends on host-xsltproc, since xsltproc is
    required for the compilation of libxcb.
    
    Moreover, remove the XSLTPROC=/usr/bin/xsltproc variable definition,
    which was assuming that XSLTPROC was installed on the host. xsltproc
    should just be part of the PATH.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/libxcb/libxcb.mk
+++ package/x11r7/libxcb/libxcb.mk
@@ -10,9 +10,9 @@ LIBXCB_SITE = http://xcb.freedesktop.org/dist/
 LIBXCB_INSTALL_STAGING = YES
 
 LIBXCB_AUTORECONF = NO
-LIBXCB_DEPENDENCIES = pthread-stubs xcb-proto xlib_libXdmcp xlib_libXau
+LIBXCB_DEPENDENCIES = host-libxslt pthread-stubs xcb-proto xlib_libXdmcp xlib_libXau
 LIBXCB_CONF_ENV = STAGING_DIR="$(STAGING_DIR)"
-LIBXCB_MAKE_OPT = XCBPROTO_XCBINCLUDEDIR=$(STAGING_DIR)/usr/share/xcb XSLTPROC=/usr/bin/xsltproc
+LIBXCB_MAKE_OPT = XCBPROTO_XCBINCLUDEDIR=$(STAGING_DIR)/usr/share/xcb
 
 $(eval $(call AUTOTARGETS,package/x11r7,libxcb))
 
