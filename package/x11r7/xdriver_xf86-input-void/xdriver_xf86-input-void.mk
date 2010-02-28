a2ad36e5dee9fd084a01befcef93a1f2bc51bbdd
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:33:56 +0200

    Bump xf86-input-void to 1.3.0
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xdriver_xf86-input-void/xdriver_xf86-input-void.mk
+++ package/x11r7/xdriver_xf86-input-void/xdriver_xf86-input-void.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-XDRIVER_XF86_INPUT_VOID_VERSION = 1.1.1
+XDRIVER_XF86_INPUT_VOID_VERSION = 1.3.0
 XDRIVER_XF86_INPUT_VOID_SOURCE = xf86-input-void-$(XDRIVER_XF86_INPUT_VOID_VERSION).tar.bz2
 XDRIVER_XF86_INPUT_VOID_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_INPUT_VOID_AUTORECONF = NO
+XDRIVER_XF86_INPUT_VOID_LIBTOOL_PATCH = NO
 XDRIVER_XF86_INPUT_VOID_DEPENDENCIES = xserver_xorg-server xproto_inputproto xproto_randrproto xproto_xproto
 XDRIVER_XF86_INPUT_VOID_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
