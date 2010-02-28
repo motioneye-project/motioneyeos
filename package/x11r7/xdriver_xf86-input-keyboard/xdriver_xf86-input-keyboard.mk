accb8589e3e4be10f2cabda58de50f8afc1d7d19
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:33:45 +0200

    Bump xf86-input-keyboard to 1.4.0
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xdriver_xf86-input-keyboard/xdriver_xf86-input-keyboard.mk
+++ package/x11r7/xdriver_xf86-input-keyboard/xdriver_xf86-input-keyboard.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-XDRIVER_XF86_INPUT_KEYBOARD_VERSION = 1.3.1
+XDRIVER_XF86_INPUT_KEYBOARD_VERSION = 1.4.0
 XDRIVER_XF86_INPUT_KEYBOARD_SOURCE = xf86-input-keyboard-$(XDRIVER_XF86_INPUT_KEYBOARD_VERSION).tar.bz2
 XDRIVER_XF86_INPUT_KEYBOARD_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_INPUT_KEYBOARD_AUTORECONF = NO
+XDRIVER_XF86_INPUT_KEYBOARD_LIBTOOL_PATCH = NO
 XDRIVER_XF86_INPUT_KEYBOARD_DEPENDENCIES = xserver_xorg-server xproto_inputproto xproto_kbproto xproto_randrproto xproto_xproto
 XDRIVER_XF86_INPUT_KEYBOARD_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
