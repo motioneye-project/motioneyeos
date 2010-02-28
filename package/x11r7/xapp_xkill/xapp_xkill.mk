b0bb8aa9394eadf2cd003a5df1ca0866e4aa5314
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:34:39 +0200

    Bump xkill to 1.0.2
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xapp_xkill/xapp_xkill.mk
+++ package/x11r7/xapp_xkill/xapp_xkill.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XKILL_VERSION = 1.0.1
+XAPP_XKILL_VERSION = 1.0.2
 XAPP_XKILL_SOURCE = xkill-$(XAPP_XKILL_VERSION).tar.bz2
 XAPP_XKILL_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XKILL_AUTORECONF = NO
