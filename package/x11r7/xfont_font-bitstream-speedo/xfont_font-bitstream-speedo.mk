f1e5e9371a32d093439ac869964efaec96a7c628
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:35:17 +0200

    Bump font-bitstream-speedo to 1.0.1
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xfont_font-bitstream-speedo/xfont_font-bitstream-speedo.mk
+++ package/x11r7/xfont_font-bitstream-speedo/xfont_font-bitstream-speedo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XFONT_FONT_BITSTREAM_SPEEDO_VERSION = 1.0.0
+XFONT_FONT_BITSTREAM_SPEEDO_VERSION = 1.0.1
 XFONT_FONT_BITSTREAM_SPEEDO_SOURCE = font-bitstream-speedo-$(XFONT_FONT_BITSTREAM_SPEEDO_VERSION).tar.bz2
 XFONT_FONT_BITSTREAM_SPEEDO_SITE = http://xorg.freedesktop.org/releases/individual/font
 XFONT_FONT_BITSTREAM_SPEEDO_AUTORECONF = NO
