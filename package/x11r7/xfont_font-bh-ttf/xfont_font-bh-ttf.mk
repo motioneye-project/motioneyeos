60c09f6f552718e4dcfe9c2956a729ab2037596b
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:35:15 +0200

    Bump font-bh-ttf to 1.0.1
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xfont_font-bh-ttf/xfont_font-bh-ttf.mk
+++ package/x11r7/xfont_font-bh-ttf/xfont_font-bh-ttf.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XFONT_FONT_BH_TTF_VERSION = 1.0.0
+XFONT_FONT_BH_TTF_VERSION = 1.0.1
 XFONT_FONT_BH_TTF_SOURCE = font-bh-ttf-$(XFONT_FONT_BH_TTF_VERSION).tar.bz2
 XFONT_FONT_BH_TTF_SITE = http://xorg.freedesktop.org/releases/individual/font
 XFONT_FONT_BH_TTF_AUTORECONF = NO
