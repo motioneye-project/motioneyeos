fc1cc7e5e0b6735ef2c9da590a9cc21c03c4e583
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:34:14 +0200

    Bump xf86-video-suntcx to 1.1.1
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xdriver_xf86-video-suntcx/xdriver_xf86-video-suntcx.mk
+++ package/x11r7/xdriver_xf86-video-suntcx/xdriver_xf86-video-suntcx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_SUNTCX_VERSION = 1.1.0
+XDRIVER_XF86_VIDEO_SUNTCX_VERSION = 1.1.1
 XDRIVER_XF86_VIDEO_SUNTCX_SOURCE = xf86-video-suntcx-$(XDRIVER_XF86_VIDEO_SUNTCX_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_SUNTCX_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_SUNTCX_AUTORECONF = NO
