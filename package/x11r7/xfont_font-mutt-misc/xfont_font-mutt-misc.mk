05d390efd87f65dac8f45642d7cc5270d2feccb2
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:35:26 +0200

    Bump font-mutt-misc to 1.0.1
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xfont_font-mutt-misc/xfont_font-mutt-misc.mk
+++ package/x11r7/xfont_font-mutt-misc/xfont_font-mutt-misc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XFONT_FONT_MUTT_MISC_VERSION = 1.0.0
+XFONT_FONT_MUTT_MISC_VERSION = 1.0.1
 XFONT_FONT_MUTT_MISC_SOURCE = font-mutt-misc-$(XFONT_FONT_MUTT_MISC_VERSION).tar.bz2
 XFONT_FONT_MUTT_MISC_SITE = http://xorg.freedesktop.org/releases/individual/font
 XFONT_FONT_MUTT_MISC_AUTORECONF = NO
