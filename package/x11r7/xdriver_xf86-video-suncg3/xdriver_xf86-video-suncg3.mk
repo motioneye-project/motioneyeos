f688e1ed1157b3bf57866e01c6119febad9fe6cf
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:34:11 +0200

    Bump xf86-video-suncg3 to 1.1.1
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xdriver_xf86-video-suncg3/xdriver_xf86-video-suncg3.mk
+++ package/x11r7/xdriver_xf86-video-suncg3/xdriver_xf86-video-suncg3.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_SUNCG3_VERSION = 1.1.0
+XDRIVER_XF86_VIDEO_SUNCG3_VERSION = 1.1.1
 XDRIVER_XF86_VIDEO_SUNCG3_SOURCE = xf86-video-suncg3-$(XDRIVER_XF86_VIDEO_SUNCG3_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_SUNCG3_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_SUNCG3_AUTORECONF = NO
