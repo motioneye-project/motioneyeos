4cc856d71d979ff68757ba6fe57fa42b2faacad1
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:34:34 +0200

    Bump xev to 1.0.4
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xapp_xev/xapp_xev.mk
+++ package/x11r7/xapp_xev/xapp_xev.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XEV_VERSION = 1.0.3
+XAPP_XEV_VERSION = 1.0.4
 XAPP_XEV_SOURCE = xev-$(XAPP_XEV_VERSION).tar.bz2
 XAPP_XEV_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XEV_AUTORECONF = NO
