04e6be89f7916b318d7e5a496d62a6731f8eac62
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:35:25 +0200

    Bump font-misc-misc to 1.1.0
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xfont_font-misc-misc/xfont_font-misc-misc.mk
+++ package/x11r7/xfont_font-misc-misc/xfont_font-misc-misc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XFONT_FONT_MISC_MISC_VERSION = 1.0.0
+XFONT_FONT_MISC_MISC_VERSION = 1.1.0
 XFONT_FONT_MISC_MISC_SOURCE = font-misc-misc-$(XFONT_FONT_MISC_MISC_VERSION).tar.bz2
 XFONT_FONT_MISC_MISC_SITE = http://xorg.freedesktop.org/releases/individual/font
 XFONT_FONT_MISC_MISC_AUTORECONF = NO
