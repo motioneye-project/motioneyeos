3f3262b950bb7277bceb1d4cbc46e78ad23383c3
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:33:43 +0200

    Bump libxcb to 1.5
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/libxcb/libxcb.mk
+++ package/x11r7/libxcb/libxcb.mk
@@ -3,16 +3,17 @@
 # libxcb
 #
 #############################################################
-LIBXCB_VERSION = 1.1
+LIBXCB_VERSION = 1.5
 LIBXCB_SOURCE = libxcb-$(LIBXCB_VERSION).tar.bz2
 LIBXCB_SITE = http://xcb.freedesktop.org/dist/
 
 LIBXCB_INSTALL_STAGING = YES
 
 LIBXCB_AUTORECONF = NO
+LIBXCB_LIBTOOL_PATCH = NO
 LIBXCB_DEPENDENCIES = host-libxslt pthread-stubs xcb-proto xlib_libXdmcp xlib_libXau
 LIBXCB_CONF_ENV = STAGING_DIR="$(STAGING_DIR)"
-LIBXCB_MAKE_OPT = XCBPROTO_XCBINCLUDEDIR=$(STAGING_DIR)/usr/share/xcb
+LIBXCB_MAKE_OPT = XCBPROTO_XCBINCLUDEDIR=$(STAGING_DIR)/usr/share/xcb XCBPROTO_XCBPYTHONDIR=$(STAGING_DIR)/usr/lib/python2.6/site-packages
 
 $(eval $(call AUTOTARGETS,package/x11r7,libxcb))
 
