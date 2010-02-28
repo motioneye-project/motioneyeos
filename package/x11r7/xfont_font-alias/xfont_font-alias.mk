7c6fe8c0075c140e09b2f8a10c0a553e76bc4da8
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:35:12 +0200

    Bump font-alias to 1.0.2
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xfont_font-alias/xfont_font-alias.mk
+++ package/x11r7/xfont_font-alias/xfont_font-alias.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XFONT_FONT_ALIAS_VERSION = 1.0.1
+XFONT_FONT_ALIAS_VERSION = 1.0.2
 XFONT_FONT_ALIAS_SOURCE = font-alias-$(XFONT_FONT_ALIAS_VERSION).tar.bz2
 XFONT_FONT_ALIAS_SITE = http://xorg.freedesktop.org/releases/individual/font
 XFONT_FONT_ALIAS_AUTORECONF = NO
