9e0e0d4c454f4a8045959b91f1929edb0d075969
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:33:54 +0200

    Bump xf86-input-aiptek to 1.3.0
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xdriver_xf86-input-aiptek/xdriver_xf86-input-aiptek.mk
+++ package/x11r7/xdriver_xf86-input-aiptek/xdriver_xf86-input-aiptek.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-XDRIVER_XF86_INPUT_AIPTEK_VERSION = 1.1.1
+XDRIVER_XF86_INPUT_AIPTEK_VERSION = 1.3.0
 XDRIVER_XF86_INPUT_AIPTEK_SOURCE = xf86-input-aiptek-$(XDRIVER_XF86_INPUT_AIPTEK_VERSION).tar.bz2
 XDRIVER_XF86_INPUT_AIPTEK_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_INPUT_AIPTEK_AUTORECONF = NO
+XDRIVER_XF86_INPUT_AIPTEK_LIBTOOL_PATCH = NO
 XDRIVER_XF86_INPUT_AIPTEK_DEPENDENCIES = xserver_xorg-server xproto_inputproto xproto_randrproto xproto_xproto
 XDRIVER_XF86_INPUT_AIPTEK_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
