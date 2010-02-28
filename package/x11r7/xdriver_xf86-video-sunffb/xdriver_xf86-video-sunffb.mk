6b53c5aa38e2392cd9700e83c80c51122b076fc1
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:34:13 +0200

    Bump xf86-video-sunffb to 1.2.1
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xdriver_xf86-video-sunffb/xdriver_xf86-video-sunffb.mk
+++ package/x11r7/xdriver_xf86-video-sunffb/xdriver_xf86-video-sunffb.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_SUNFFB_VERSION = 1.2.0
+XDRIVER_XF86_VIDEO_SUNFFB_VERSION = 1.2.1
 XDRIVER_XF86_VIDEO_SUNFFB_SOURCE = xf86-video-sunffb-$(XDRIVER_XF86_VIDEO_SUNFFB_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_SUNFFB_SITE = http://xorg.freedesktop.org/releases/individual/driver
-XDRIVER_XF86_VIDEO_SUNFFB_AUTORECONF = YES
+XDRIVER_XF86_VIDEO_SUNFFB_AUTORECONF = NO
 XDRIVER_XF86_VIDEO_SUNFFB_DEPENDENCIES = xserver_xorg-server libdrm xproto_fontsproto xproto_randrproto xproto_renderproto xproto_xextproto xproto_xf86driproto xproto_xproto
 XDRIVER_XF86_VIDEO_SUNFFB_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
