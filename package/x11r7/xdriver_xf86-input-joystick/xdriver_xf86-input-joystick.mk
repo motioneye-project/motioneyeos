017bdf8177f56a01efdcc572816e543fd1acc7be
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:33:55 +0200

    Bump xf86-input-joystick to 1.4.99.2
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xdriver_xf86-input-joystick/xdriver_xf86-input-joystick.mk
+++ package/x11r7/xdriver_xf86-input-joystick/xdriver_xf86-input-joystick.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-XDRIVER_XF86_INPUT_JOYSTICK_VERSION = 1.3.2
+XDRIVER_XF86_INPUT_JOYSTICK_VERSION = 1.4.99.2
 XDRIVER_XF86_INPUT_JOYSTICK_SOURCE = xf86-input-joystick-$(XDRIVER_XF86_INPUT_JOYSTICK_VERSION).tar.bz2
 XDRIVER_XF86_INPUT_JOYSTICK_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_INPUT_JOYSTICK_AUTORECONF = NO
+XDRIVER_XF86_INPUT_JOYSTICK_LIBTOOL_PATCH = NO
 XDRIVER_XF86_INPUT_JOYSTICK_DEPENDENCIES = xserver_xorg-server xproto_inputproto xproto_randrproto xproto_xproto
 XDRIVER_XF86_INPUT_JOYSTICK_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
