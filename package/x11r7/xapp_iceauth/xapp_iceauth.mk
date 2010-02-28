f962d03471321bcac28465f003ac26fdfa869b31
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:34:25 +0200

    Bump iceauth to 1.0.3
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xapp_iceauth/xapp_iceauth.mk
+++ package/x11r7/xapp_iceauth/xapp_iceauth.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_ICEAUTH_VERSION = 1.0.2
+XAPP_ICEAUTH_VERSION = 1.0.3
 XAPP_ICEAUTH_SOURCE = iceauth-$(XAPP_ICEAUTH_VERSION).tar.bz2
 XAPP_ICEAUTH_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_ICEAUTH_AUTORECONF = NO
