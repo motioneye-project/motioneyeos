ebd78a912d7edf111af71a7fd55c7c682c6544ec
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:33:39 +0200

    Bump renderproto to 0.11
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xproto_renderproto/xproto_renderproto.mk
+++ package/x11r7/xproto_renderproto/xproto_renderproto.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XPROTO_RENDERPROTO_VERSION = 0.9.3
+XPROTO_RENDERPROTO_VERSION = 0.11
 XPROTO_RENDERPROTO_SOURCE = renderproto-$(XPROTO_RENDERPROTO_VERSION).tar.bz2
 XPROTO_RENDERPROTO_SITE = http://xorg.freedesktop.org/releases/individual/proto
 XPROTO_RENDERPROTO_AUTORECONF = NO
