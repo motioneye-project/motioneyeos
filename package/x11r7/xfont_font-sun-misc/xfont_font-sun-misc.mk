94dc634d66fd9c9d2e55ed07b5d51314f82e8250
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:35:28 +0200

    Bump font-sun-misc to 1.0.1
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xfont_font-sun-misc/xfont_font-sun-misc.mk
+++ package/x11r7/xfont_font-sun-misc/xfont_font-sun-misc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XFONT_FONT_SUN_MISC_VERSION = 1.0.0
+XFONT_FONT_SUN_MISC_VERSION = 1.0.1
 XFONT_FONT_SUN_MISC_SOURCE = font-sun-misc-$(XFONT_FONT_SUN_MISC_VERSION).tar.bz2
 XFONT_FONT_SUN_MISC_SITE = http://xorg.freedesktop.org/releases/individual/font
 XFONT_FONT_SUN_MISC_AUTORECONF = NO
