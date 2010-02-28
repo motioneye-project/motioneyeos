8cfd4c5f7a0ddd2a233438d512e1d599f77c1698
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:33:45 +0200

    Bump xf86-input-mouse to 1.5.0
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xdriver_xf86-input-mouse/xdriver_xf86-input-mouse.mk
+++ package/x11r7/xdriver_xf86-input-mouse/xdriver_xf86-input-mouse.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-XDRIVER_XF86_INPUT_MOUSE_VERSION = 1.3.0
+XDRIVER_XF86_INPUT_MOUSE_VERSION = 1.5.0
 XDRIVER_XF86_INPUT_MOUSE_SOURCE = xf86-input-mouse-$(XDRIVER_XF86_INPUT_MOUSE_VERSION).tar.bz2
 XDRIVER_XF86_INPUT_MOUSE_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_INPUT_MOUSE_AUTORECONF = NO
+XDRIVER_XF86_INPUT_MOUSE_LIBTOOL_PATCH = NO
 XDRIVER_XF86_INPUT_MOUSE_DEPENDENCIES = xserver_xorg-server xproto_inputproto xproto_randrproto xproto_xproto
 XDRIVER_XF86_INPUT_MOUSE_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
