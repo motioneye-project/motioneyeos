a65577f858d9ba4498d51d8c35d4d2b398acbf50
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-02-28 11:35:26 +0200

    Bump font-schumacher-misc to 1.1.0
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>

--- package/x11r7/xfont_font-schumacher-misc/xfont_font-schumacher-misc.mk
+++ package/x11r7/xfont_font-schumacher-misc/xfont_font-schumacher-misc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XFONT_FONT_SCHUMACHER_MISC_VERSION = 1.0.0
+XFONT_FONT_SCHUMACHER_MISC_VERSION = 1.1.0
 XFONT_FONT_SCHUMACHER_MISC_SOURCE = font-schumacher-misc-$(XFONT_FONT_SCHUMACHER_MISC_VERSION).tar.bz2
 XFONT_FONT_SCHUMACHER_MISC_SITE = http://xorg.freedesktop.org/releases/individual/font
 XFONT_FONT_SCHUMACHER_MISC_AUTORECONF = NO
