1f5c3ccdceac58b2a4af6fc22fb49041462cc04f
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2010-03-02 22:43:50 +0100

    target/squashfs: cleanup, remove support for 3.x
    Remove the code to build squashfs-tools for the host, since they can
    now be built using package/squashfs.
    
    We also remove support for Squashfs 3.x, because the Squashfs driver
    merged inside the mainline kernel is a 4.x driver. So 3.x is only
    useful for old kernels using a Squashfs patch.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- target/squashfs/squashfsroot.mk
+++ target/squashfs/squashfsroot.mk
@@ -1,52 +1,12 @@
 #############################################################
 #
-# mksquashfs to build to target squashfs filesystems
-#
-#############################################################
-SQUASHFS_VERSION:=$(call qstrip,$(BR2_TARGET_ROOTFS_SQUASHFS_VERSION))
-SQUASHFS_DIR:=$(BUILD_DIR)/squashfs$(SQUASHFS_VERSION)
-SQUASHFS_SOURCE:=squashfs$(SQUASHFS_VERSION).tar.gz
-SQUASHFS_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/squashfs
-SQUASHFS_CAT:=$(ZCAT)
-
-$(DL_DIR)/$(SQUASHFS_SOURCE):
-	 $(call DOWNLOAD,$(SQUASHFS_SITE),$(SQUASHFS_SOURCE))
-
-$(SQUASHFS_DIR)/.unpacked: $(DL_DIR)/$(SQUASHFS_SOURCE) #$(SQUASHFS_PATCH)
-	$(SQUASHFS_CAT) $(DL_DIR)/$(SQUASHFS_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	toolchain/patch-kernel.sh $(SQUASHFS_DIR) target/squashfs/ squashfs-$(SQUASHFS_VERSION)-\*.patch
-	touch $@
-
-$(SQUASHFS_DIR)/squashfs-tools/mksquashfs: $(SQUASHFS_DIR)/.unpacked
-	$(MAKE) CFLAGS="$(HOST_CFLAGS)" LDFLAGS="$(HOST_LDFLAGS)" -C $(SQUASHFS_DIR)/squashfs-tools
-
-squashfs: host-zlib $(SQUASHFS_DIR)/squashfs-tools/mksquashfs
-
-squashfs-source: $(DL_DIR)/$(SQUASHFS_SOURCE)
-
-squashfs-clean:
-	-$(MAKE) -C $(SQUASHFS_DIR)/squashfs-tools clean
-
-squashfs-dirclean:
-	rm -rf $(SQUASHFS_DIR)
-
-#############################################################
-#
 # Build the squashfs root filesystem image
 #
 #############################################################
-ifeq ($(BR2_TARGET_ROOTFS_SQUASHFS_3),y)
-# 4.x is always little endian
-ifeq ($(BR2_ENDIAN),"BIG")
-SQUASHFS_ENDIANNESS=-be
-else
-SQUASHFS_ENDIANNESS=-le
-endif
-endif
 
 SQUASHFS_TARGET:=$(IMAGE).squashfs
 
-squashfsroot: host-fakeroot makedevs squashfs
+squashfsroot: host-fakeroot host-squashfs makedevs
 	# Use fakeroot to pretend all target binaries are owned by root
 	rm -f $(BUILD_DIR)/_fakeroot.$(notdir $(SQUASHFS_TARGET))
 	touch $(BUILD_DIR)/.fakeroot.00000
@@ -58,7 +18,7 @@ ifneq ($(TARGET_DEVICE_TABLE),)
 		>> $(BUILD_DIR)/_fakeroot.$(notdir $(SQUASHFS_TARGET))
 endif
 	# Use fakeroot so mksquashfs believes the previous fakery
-	echo "$(SQUASHFS_DIR)/squashfs-tools/mksquashfs " \
+	echo "$(HOST_DIR)/usr/bin/mksquashfs " \
 		    "$(TARGET_DIR) $(SQUASHFS_TARGET) " \
 		    "-noappend $(SQUASHFS_ENDIANNESS)" \
 		>> $(BUILD_DIR)/_fakeroot.$(notdir $(SQUASHFS_TARGET))
@@ -66,14 +26,6 @@ endif
 	$(HOST_DIR)/usr/bin/fakeroot -- $(BUILD_DIR)/_fakeroot.$(notdir $(SQUASHFS_TARGET))
 	chmod 0644 $(SQUASHFS_TARGET)
 	-@rm -f $(BUILD_DIR)/_fakeroot.$(notdir $(SQUASHFS_TARGET))
-
-squashfsroot-source: squashfs-source
-
-squashfsroot-clean:
-	-$(MAKE) -C $(SQUASHFS_DIR) clean
-
-squashfsroot-dirclean:
-	rm -rf $(SQUASHFS_DIR)
 
 #############################################################
 #
