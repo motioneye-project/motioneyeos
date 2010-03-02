e01de2e7d6b5402a9dd5e9b2754035676a542a1e
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2010-03-02 22:40:45 +0100

    target/cramfs: cleanup
    Remove the cramfs host tools building process, which is now in
    package/cramfs.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- target/cramfs/cramfs.mk
+++ target/cramfs/cramfs.mk
@@ -1,40 +1,5 @@
 #############################################################
 #
-# mkcramfs to build to target cramfs filesystems
-#
-#############################################################
-CRAMFS_DIR=$(BUILD_DIR)/cramfs-1.1
-CRAMFS_SOURCE=cramfs-1.1.tar.gz
-ifeq ($(call qstrip,$(BR2_SOURCEFORGE_MIRROR)),unc)
-# UNC does not seem to have cramfs
-CRAMFS_SITE=http://internap.dl.sourceforge.net/sourceforge/cramfs
-else
-CRAMFS_SITE=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/cramfs
-endif
-
-$(DL_DIR)/$(CRAMFS_SOURCE):
-	 $(call DOWNLOAD,$(CRAMFS_SITE),$(CRAMFS_SOURCE))
-
-$(CRAMFS_DIR): $(DL_DIR)/$(CRAMFS_SOURCE)
-	$(ZCAT) $(DL_DIR)/$(CRAMFS_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	toolchain/patch-kernel.sh $(CRAMFS_DIR) target/cramfs/ cramfs\*.patch
-
-$(CRAMFS_DIR)/mkcramfs: $(CRAMFS_DIR)
-	$(MAKE) CFLAGS="$(HOST_CFLAGS) -Wall -O2 -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64" LDFLAGS="$(HOST_LDFLAGS)" -C $(CRAMFS_DIR)
-	touch -c $@
-
-cramfs: host-zlib $(CRAMFS_DIR)/mkcramfs
-
-cramfs-source: $(DL_DIR)/$(CRAMFS_SOURCE)
-
-cramfs-clean:
-	-$(MAKE) -C $(CRAMFS_DIR) clean
-
-cramfs-dirclean:
-	rm -rf $(CRAMFS_DIR)
-
-#############################################################
-#
 # Build the cramfs root filesystem image
 #
 #############################################################
@@ -50,7 +15,7 @@ endif
 
 CRAMFS_TARGET=$(IMAGE).cramfs
 
-cramfsroot: host-fakeroot makedevs cramfs
+cramfsroot: host-fakeroot host-cramfs makedevs
 	# Use fakeroot to pretend all target binaries are owned by root
 	rm -f $(BUILD_DIR)/_fakeroot.$(notdir $(CRAMFS_TARGET))
 	touch $(BUILD_DIR)/.fakeroot.00000
@@ -62,19 +27,11 @@ ifneq ($(TARGET_DEVICE_TABLE),)
 		>> $(BUILD_DIR)/_fakeroot.$(notdir $(CRAMFS_TARGET))
 endif
 	# Use fakeroot so mkcramfs believes the previous fakery
-	echo "$(CRAMFS_DIR)/mkcramfs -q $(CRAMFS_OPTS) " \
+	echo "$(HOST_DIR)/usr/bin/mkcramfs -q $(CRAMFS_OPTS) " \
 		"$(TARGET_DIR) $(CRAMFS_TARGET)" >> $(BUILD_DIR)/_fakeroot.$(notdir $(CRAMFS_TARGET))
 	chmod a+x $(BUILD_DIR)/_fakeroot.$(notdir $(CRAMFS_TARGET))
 	$(HOST_DIR)/usr/bin/fakeroot -- $(BUILD_DIR)/_fakeroot.$(notdir $(CRAMFS_TARGET))
 	-@rm -f $(BUILD_DIR)/_fakeroot.$(notdir $(CRAMFS_TARGET))
-
-cramfsroot-source: cramfs-source
-
-cramfsroot-clean:
-	-$(MAKE) -C $(CRAMFS_DIR) clean
-
-cramfsroot-dirclean:
-	rm -rf $(CRAMFS_DIR)
 
 #############################################################
 #
