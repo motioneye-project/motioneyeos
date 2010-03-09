3892493f8ddcc3219b1ce6ee8ebdc25eb999c2c4
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2010-03-10 00:14:29 +0100

    squashfs: convert to ROOTFS_TARGET infrastructure
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- target/squashfs/squashfsroot.mk
+++ target/squashfs/squashfsroot.mk
@@ -4,34 +4,10 @@
 #
 #############################################################
 
-SQUASHFS_TARGET:=$(IMAGE).squashfs
+ROOTFS_SQUASHFS_DEPENDENCIES = host-squashfs
 
-squashfsroot: host-fakeroot host-squashfs makedevs
-	# Use fakeroot to pretend all target binaries are owned by root
-	rm -f $(BUILD_DIR)/_fakeroot.$(notdir $(SQUASHFS_TARGET))
-	touch $(BUILD_DIR)/.fakeroot.00000
-	cat $(BUILD_DIR)/.fakeroot* > $(BUILD_DIR)/_fakeroot.$(notdir $(SQUASHFS_TARGET))
-	echo "chown -R 0:0 $(TARGET_DIR)" >> $(BUILD_DIR)/_fakeroot.$(notdir $(SQUASHFS_TARGET))
-ifneq ($(TARGET_DEVICE_TABLE),)
-	# Use fakeroot to pretend to create all needed device nodes
-	echo "$(HOST_DIR)/usr/bin/makedevs -d $(TARGET_DEVICE_TABLE) $(TARGET_DIR)" \
-		>> $(BUILD_DIR)/_fakeroot.$(notdir $(SQUASHFS_TARGET))
-endif
-	# Use fakeroot so mksquashfs believes the previous fakery
-	echo "$(HOST_DIR)/usr/bin/mksquashfs " \
-		    "$(TARGET_DIR) $(SQUASHFS_TARGET) " \
-		    "-noappend $(SQUASHFS_ENDIANNESS)" \
-		>> $(BUILD_DIR)/_fakeroot.$(notdir $(SQUASHFS_TARGET))
-	chmod a+x $(BUILD_DIR)/_fakeroot.$(notdir $(SQUASHFS_TARGET))
-	$(HOST_DIR)/usr/bin/fakeroot -- $(BUILD_DIR)/_fakeroot.$(notdir $(SQUASHFS_TARGET))
-	chmod 0644 $(SQUASHFS_TARGET)
-	-@rm -f $(BUILD_DIR)/_fakeroot.$(notdir $(SQUASHFS_TARGET))
+define ROOTFS_SQUASHFS_CMD
+	$(HOST_DIR)/usr/bin/mksquashfs $(TARGET_DIR) $$@ -noappend
+endef
 
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_TARGET_ROOTFS_SQUASHFS),y)
-TARGETS+=squashfsroot
-endif
+$(eval $(call ROOTFS_TARGET,squashfs))
