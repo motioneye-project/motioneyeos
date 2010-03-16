d3ea4f4cf52eea9bfe8e3837f66e199f1d6b7e04
Peter Korsgaard <jacmet@sunsite.dk>
2010-03-16 16:30:00 +0100

    libglib2: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libglib2/libglib2.mk
+++ package/libglib2/libglib2.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 LIBGLIB2_VERSION_MAJOR = 2.22
-LIBGLIB2_VERSION_MINOR = 4
+LIBGLIB2_VERSION_MINOR = 5
 LIBGLIB2_VERSION = $(LIBGLIB2_VERSION_MAJOR).$(LIBGLIB2_VERSION_MINOR)
 LIBGLIB2_SOURCE = glib-$(LIBGLIB2_VERSION).tar.bz2
 LIBGLIB2_SITE = http://ftp.gnome.org/pub/gnome/sources/glib/$(LIBGLIB2_VERSION_MAJOR)
