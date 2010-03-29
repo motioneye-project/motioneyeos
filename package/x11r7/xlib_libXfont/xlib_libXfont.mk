8a5e8e2d009b809e99990eba90085df67c9fbfe9
Peter Korsgaard <jacmet@sunsite.dk>
2010-03-29 12:41:17 +0200

    xlib_libXfont: don't build devel documentation
    Adds to the build time and brings in an xmlto dependency
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xlib_libXfont/xlib_libXfont.mk
+++ package/x11r7/xlib_libXfont/xlib_libXfont.mk
@@ -10,6 +10,6 @@ XLIB_LIBXFONT_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXFONT_AUTORECONF = YES
 XLIB_LIBXFONT_INSTALL_STAGING = YES
 XLIB_LIBXFONT_DEPENDENCIES = freetype xlib_libfontenc xlib_xtrans xproto_fontcacheproto xproto_fontsproto xproto_xproto xfont_encodings
-XLIB_LIBXFONT_CONF_OPT = --enable-shared --disable-static
+XLIB_LIBXFONT_CONF_OPT = --enable-shared --disable-static --disable-devel-docs
 
 $(eval $(call AUTOTARGETS,package/x11r7,xlib_libXfont))
