a88d0ea6252f2d499efcfb0b96615baf10bb05e6
Peter Korsgaard <jacmet@sunsite.dk>
2010-03-29 17:42:12 +0200

    libxml2: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libxml2/libxml2.mk
+++ package/libxml2/libxml2.mk
@@ -3,7 +3,7 @@
 # libxml2
 #
 #############################################################
-LIBXML2_VERSION = 2.7.6
+LIBXML2_VERSION = 2.7.7
 LIBXML2_SOURCE = libxml2-sources-$(LIBXML2_VERSION).tar.gz
 LIBXML2_SITE = ftp://xmlsoft.org/libxml2
 LIBXML2_INSTALL_STAGING = YES
