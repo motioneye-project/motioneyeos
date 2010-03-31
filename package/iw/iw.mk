259d6fc0e097685a1340ab56e666ab384884bbc2
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-03-31 15:30:30 -0300

    iw: needs host-pkg-config
    Closes #1459
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/iw/iw.mk
+++ package/iw/iw.mk
@@ -7,7 +7,7 @@
 IW_VERSION = 0.9.19
 IW_SOURCE = iw-$(IW_VERSION).tar.bz2
 IW_SITE = http://wireless.kernel.org/download/iw
-IW_DEPENDENCIES = libnl
+IW_DEPENDENCIES = host-pkg-config libnl
 IW_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 IW_CONFIG = $(IW_DIR)/.config
 IW_MAKE_ENV = PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" \
