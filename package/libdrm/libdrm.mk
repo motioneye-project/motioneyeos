faa2f01231964c74bccb3e216a543469e4d72577
Paul Jones <paul@pauljones.id.au>
2010-03-31 21:15:02 +1100

    Bump libdrm to 2.4.19
    Also adds proper dependency information to stop build errors.
    
    Signed-off-by: Paul Jones <paul@pauljones.id.au>
    Acked-By: Will Wagner<willw@carallon.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libdrm/libdrm.mk
+++ package/libdrm/libdrm.mk
@@ -3,12 +3,14 @@
 # libdrm
 #
 #############################################################
-LIBDRM_VERSION = 2.4.11
+LIBDRM_VERSION = 2.4.19
 LIBDRM_SOURCE = libdrm-$(LIBDRM_VERSION).tar.bz2
 LIBDRM_SITE = http://dri.freedesktop.org/libdrm/
 LIBDRM_AUTORECONF = NO
 LIBDRM_LIBTOOL_PATCH = NO
 LIBDRM_INSTALL_STAGING = YES
 LIBDRM_INSTALL_TARGET = YES
+
+LIBDRM_DEPENDENCIES = xproto_glproto xproto_xf86vidmodeproto xlib_libXxf86vm xlib_libXmu xproto_dri2proto pthread-stubs
 
 $(eval $(call AUTOTARGETS,package,libdrm))
