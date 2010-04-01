9b02ce9e9d98e32b6e658bf7217a43a65a51787a
Will Wagner <will_wagner@carallon.com>
2010-04-01 13:46:42 +0100

    xkeyboard-config: bump version
    Signed-off-by: Will Wagner <will_wagner@carallon.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xkeyboard-config/xkeyboard-config.mk
+++ package/x11r7/xkeyboard-config/xkeyboard-config.mk
@@ -3,7 +3,7 @@
 # xkeyboard-config
 #
 #############################################################
-XKEYBOARD_CONFIG_VERSION = 1.2
+XKEYBOARD_CONFIG_VERSION = 1.8
 XKEYBOARD_CONFIG_SOURCE = xkeyboard-config-$(XKEYBOARD_CONFIG_VERSION).tar.bz2
 XKEYBOARD_CONFIG_SITE = http://xlibs.freedesktop.org/xkbdesc
 XKEYBOARD_CONFIG_AUTORECONF = NO
