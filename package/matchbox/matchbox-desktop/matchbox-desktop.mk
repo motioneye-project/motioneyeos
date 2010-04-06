c03c626ecb5ad285dbfc126fec46d2b94bf23a16
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-04-06 23:36:53 +0300

    matchbox-desktop: fix compilation and installation
    Libtool patch applies just fine.
    mbdesktop_modules is installed by the matchbox-desktop and we
    don't have to overwrite it with our copy(contains wrong paths).
    
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/matchbox/matchbox-desktop/matchbox-desktop.mk
+++ package/matchbox/matchbox-desktop/matchbox-desktop.mk
@@ -7,15 +7,8 @@
 MATCHBOX_DESKTOP_VERSION = 0.9.1
 MATCHBOX_DESKTOP_SOURCE = matchbox-desktop-$(MATCHBOX_DESKTOP_VERSION).tar.bz2
 MATCHBOX_DESKTOP_SITE = http://matchbox-project.org/sources/matchbox-desktop/$(MATCHBOX_DESKTOP_VERSION)
-MATCHBOX_DESKTOP_LIBTOOL_PATCH = NO
 MATCHBOX_DESKTOP_DEPENDENCIES = matchbox-lib
 MATCHBOX_DESKTOP_CONF_OPT = --enable-expat
-
-define MATCHBOX_DESKTOP_POST_INSTALL_FIXES
- cp -f ./package/matchbox/matchbox-desktop/mbdesktop_modules $(TARGET_DIR)/etc/matchbox/
-endef
-
-MATCHBOX_DESKTOP_POST_INSTALL_TARGET_HOOKS += MATCHBOX_DESKTOP_POST_INSTALL_FIXES
 
 #############################################################
 
