6caec6f3b3aecf41c3a051ffa4b5d9a2e41775ca
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2010-04-06 21:50:52 +0200

    usb_modeswitch: use GENTARGETS instead of AUTOTARGETS
    usb_modeswitch build system is just a simple Makefile, so the
    GENTARGETS infrastructure is more appropriate than the AUTOTARGETS
    infrastructure. The build system is slightly fixed through a small
    patch, that replaces the previous "nostrip" patch.
    
    The latest version of usb_modeswitch is 1.1.1, but it now relies on
    udev and requires TCL to operate. Someone actually using
    usb_modeswitch would probably be at a better place to do this version
    bump.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/usb_modeswitch/usb_modeswitch.mk
+++ package/usb_modeswitch/usb_modeswitch.mk
@@ -7,22 +7,20 @@
 USB_MODESWITCH_VERSION = 1.0.7
 USB_MODESWITCH_SOURCE = usb_modeswitch-$(USB_MODESWITCH_VERSION).tar.bz2
 USB_MODESWITCH_SITE = http://www.draisberghof.de/usb_modeswitch
-USB_MODESWITCH_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 USB_MODESWITCH_DEPENDENCIES = libusb-compat
-USB_MODESWITCH_MAKE_OPT = CC="$(TARGET_CC)" OPTS="$(TARGET_CFLAGS)"
 
-$(eval $(call AUTOTARGETS,package,usb_modeswitch))
+define USB_MODESWITCH_BUILD_CMDS
+ rm $(@D)/usb_modeswitch
+ $(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
 
-$(USB_MODESWITCH_TARGET_CONFIGURE):
-	rm -f $(USB_MODESWITCH_DIR)/usb_modeswitch
-	touch $@
+define USB_MODESWITCH_INSTALL_TARGET_CMDS
+ $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
+endef
 
-$(USB_MODESWITCH_HOOK_POST_INSTALL):
-	chmod a-x $(TARGET_DIR)/etc/usb_modeswitch.conf
-	touch $@
+define USB_MODESWITCH_CLEAN_CMDS
+ rm -f $(TARGET_DIR)/usr/sbin/usb_modeswitch
+ rm -f $(TARGET_DIR)/etc/usb_modeswitch.conf
+endef
 
-$(USB_MODESWITCH_TARGET_UNINSTALL):
-	$(call MESSAGE,"Uninstalling")
-	rm -f $(TARGET_DIR)/usr/sbin/usb_modeswitch
-	rm -f $(TARGET_DIR)/etc/usb_modeswitch.conf
-	rm -f $(USB_MODESWITCH_TARGET_INSTALL_TARGET) $(USB_MODESWITCH_HOOK_POST_INSTALL)
+$(eval $(call GENTARGETS,package,usb_modeswitch))
