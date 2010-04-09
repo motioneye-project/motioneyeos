b8cee9fb4380a8d58a324d141dcab68a4c3aa29c
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2010-04-09 12:15:59 +0200

    Add version selection for the SquashFS root filesystem
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- fs/squashfs/squashfs.mk
+++ fs/squashfs/squashfs.mk
@@ -4,7 +4,11 @@
 #
 #############################################################
 
+ifeq ($(BR2_TARGET_ROOTFS_SQUASHFS4),y)
 ROOTFS_SQUASHFS_DEPENDENCIES = host-squashfs
+else
+ROOTFS_SQUASHFS_DEPENDENCIES = host-squashfs3
+endif
 
 define ROOTFS_SQUASHFS_CMD
 	$(HOST_DIR)/usr/bin/mksquashfs $(TARGET_DIR) $$@ -noappend
