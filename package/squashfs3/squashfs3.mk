5ef9fe7e53956ab0a213f64c02366fbfb996bd83
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2010-04-09 12:15:25 +0200

    squashfs3: new package
    Add support for the old squashfs 3.x tools, so that squashfs 3.x root
    filesystems can be generated.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/squashfs3/squashfs3.mk
@@ -0,0 +1,30 @@
+SQUASHFS3_VERSION=3.4
+SQUASHFS3_SOURCE=squashfs$(SQUASHFS3_VERSION).tar.gz
+SQUASHFS3_SITE=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/squashfs
+
+SQUASHFS3_DEPENDENCIES = zlib
+HOST_SQUASHFS3_DEPENDENCIES = host-zlib
+
+define SQUASHFS3_BUILD_CMDS
+ $(TARGET_MAKE_ENV) $(MAKE)    \
+   CC="$(TARGET_CC)"           \
+   CFLAGS="$(TARGET_CFLAGS)"   \
+   LDFLAGS="$(TARGET_LDFLAGS)" \
+   -C $(@D)/squashfs-tools/
+endef
+
+define SQUASHFS3_INSTALL_TARGET_CMDS
+ $(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/squashfs-tools/ INSTALL_DIR=$(TARGET_DIR)/usr/bin install
+endef
+
+define HOST_SQUASHFS3_BUILD_CMDS
+ $(HOST_MAKE_ENV) $(MAKE) -C $(@D)/squashfs-tools/
+endef
+
+define HOST_SQUASHFS3_INSTALL_CMDS
+ $(HOST_MAKE_ENV) $(MAKE) -C $(@D)/squashfs-tools/ INSTALL_DIR=$(HOST_DIR)/usr/bin install
+endef
+
+$(eval $(call GENTARGETS,package,squashfs3))
+$(eval $(call GENTARGETS,package,squashfs3,host))
+
