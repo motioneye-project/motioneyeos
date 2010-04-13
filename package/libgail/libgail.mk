4e9424088bc1a7300a5e613cba30d8e662508274
Lionel Landwerlin <llandwerlin@gmail.com>
2010-04-14 01:06:08 +0200

    package: add libgail package
    This package include a patch for directfb support. This patch has been
    proposed for upstream integration
    (https://bugzilla.gnome.org/show_bug.cgi?id=614199).
    
    Signed-off-by: Lionel Landwerlin <llandwerlin@gmail.com>

---
+++ package/libgail/libgail.mk
@@ -0,0 +1,26 @@
+#############################################################
+#
+# libgail
+#
+#############################################################
+LIBGAIL_VERSION_MAJOR = 1.22
+LIBGAIL_VERSION_MINOR = 3
+LIBGAIL_VERSION = $(LIBGAIL_VERSION_MAJOR).$(LIBGAIL_VERSION_MINOR)
+LIBGAIL_SOURCE = gail-$(LIBGAIL_VERSION).tar.bz2
+LIBGAIL_SITE = http://ftp.gnome.org/pub/gnome/sources/gail/$(LIBGAIL_VERSION_MAJOR)
+
+LIBGAIL_AUTORECONF = YES
+LIBGAIL_LIBTOOL_PATCH = NO
+LIBGAIL_INSTALL_STAGING = YES
+LIBGAIL_INSTALL_TARGET = YES
+
+LIBGAIL_CONF_OPT = --enable-shared \
+		--enable-static
+
+ifneq ($(BR2_PACKAGE_XLIB_LIBX11),y)
+LIBGAIL_CONF_OPT += --disable-x
+endif
+
+LIBGAIL_DEPENDENCIES = host-pkg-config libgtk2 pango
+
+$(eval $(call AUTOTARGETS,package,libgail))
