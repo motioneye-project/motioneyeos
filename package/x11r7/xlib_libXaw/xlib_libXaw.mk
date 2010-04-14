e5b948a54ec74ccf35f9158e9eda240ca8ec6bf1
Will Wagner <will_wagner@carallon.com>
2010-04-14 18:06:44 +0100

    libXaw: do not build docs (fails if missing host tools), also cleanup makefile
    Signed-off-by: Will Wagner <will_wagner@carallon.com>
    Acked-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xlib_libXaw/xlib_libXaw.mk
+++ package/x11r7/xlib_libXaw/xlib_libXaw.mk
@@ -7,9 +7,8 @@
 XLIB_LIBXAW_VERSION = 1.0.7
 XLIB_LIBXAW_SOURCE = libXaw-$(XLIB_LIBXAW_VERSION).tar.bz2
 XLIB_LIBXAW_SITE = http://xorg.freedesktop.org/releases/individual/lib
-XLIB_LIBXAW_AUTORECONF = NO
 XLIB_LIBXAW_INSTALL_STAGING = YES
 XLIB_LIBXAW_DEPENDENCIES = xlib_libX11 xlib_libXt xlib_libXmu xlib_libXpm xproto_xproto xlib_libXp
-XLIB_LIBXAW_CONF_OPT = --enable-shared --disable-static
+XLIB_LIBXAW_CONF_OPT = --enable-shared --disable-static --disable-docs
 
 $(eval $(call AUTOTARGETS,package/x11r7,xlib_libXaw))
