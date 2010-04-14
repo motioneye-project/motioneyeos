c8b4f397e623c6a8672667866b2c541bf4d10cd6
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-04-14 15:31:02 +0300

    libdrm: enable/disable features according to driver selection
    Also cleanup autotools symbols and add libatomic_ops dependency
    for Intel part.
    
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libdrm/libdrm.mk
+++ package/libdrm/libdrm.mk
@@ -6,11 +6,20 @@
 LIBDRM_VERSION = 2.4.19
 LIBDRM_SOURCE = libdrm-$(LIBDRM_VERSION).tar.bz2
 LIBDRM_SITE = http://dri.freedesktop.org/libdrm/
-LIBDRM_AUTORECONF = NO
 LIBDRM_LIBTOOL_PATCH = NO
 LIBDRM_INSTALL_STAGING = YES
-LIBDRM_INSTALL_TARGET = YES
 
 LIBDRM_DEPENDENCIES = xproto_glproto xproto_xf86vidmodeproto xlib_libXxf86vm xlib_libXmu xproto_dri2proto pthread-stubs
+
+ifeq ($(BR2_PACKAGE_XDRIVER_XF86_VIDEO_INTEL),y)
+LIBDRM_CONF_OPT += --enable-intel
+LIBDRM_DEPENDENCIES += libatomic_ops
+else
+LIBDRM_CONF_OPT += --disable-intel
+endif
+
+ifneq ($(BR2_PACKAGE_XDRIVER_XF86_VIDEO_ATI),y)
+LIBDRM_CONF_OPT += --disable-radeon
+endif
 
 $(eval $(call AUTOTARGETS,package,libdrm))
