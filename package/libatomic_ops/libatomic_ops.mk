71a041c852df27f7400472c7ba4bbb62615994b5
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-04-14 15:30:52 +0300

    Add libatomic_ops 1.2 needed by libdrm Intel support
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libatomic_ops/libatomic_ops.mk
@@ -0,0 +1,12 @@
+#############################################################
+#
+# Atomic_ops library
+#
+#############################################################
+
+LIBATOMIC_OPS_VERSION = 1.2
+LIBATOMIC_OPS_SOURCE = libatomic_ops-$(LIBATOMIC_OPS_VERSION).tar.gz
+LIBATOMIC_OPS_SITE = http://www.hpl.hp.com/research/linux/atomic_ops/download
+LIBATOMIC_OPS_INSTALL_STAGING = YES
+
+$(eval $(call AUTOTARGETS,package,libatomic_ops))
