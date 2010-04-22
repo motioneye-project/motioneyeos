63b866640957c0e42467914effa7cdc2451e7900
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-04-22 15:58:16 -0300

    usb_modeswitch: Bump to version 1.1.2
    And correct kconfig dependency.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/usb_modeswitch/usb_modeswitch.mk
+++ package/usb_modeswitch/usb_modeswitch.mk
@@ -4,23 +4,26 @@
 #
 #############################################################
 
-USB_MODESWITCH_VERSION = 1.0.7
-USB_MODESWITCH_SOURCE = usb_modeswitch-$(USB_MODESWITCH_VERSION).tar.bz2
+USB_MODESWITCH_VERSION = 1.1.2
+USB_MODESWITCH_SOURCE = usb-modeswitch-$(USB_MODESWITCH_VERSION).tar.bz2
 USB_MODESWITCH_SITE = http://www.draisberghof.de/usb_modeswitch
 USB_MODESWITCH_DEPENDENCIES = libusb-compat
 
 define USB_MODESWITCH_BUILD_CMDS
- rm $(@D)/usb_modeswitch
- $(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+	$(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
 endef
 
 define USB_MODESWITCH_INSTALL_TARGET_CMDS
- $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
+	$(INSTALL) -D $(@D)/usb_modeswitch.setup -m 0644 \
+		$(TARGET_DIR)/etc/usb_modeswitch.setup
 endef
 
 define USB_MODESWITCH_CLEAN_CMDS
- rm -f $(TARGET_DIR)/usr/sbin/usb_modeswitch
- rm -f $(TARGET_DIR)/etc/usb_modeswitch.conf
+	rm -f $(TARGET_DIR)/usr/sbin/usb_modeswitch
+	rm -f $(TARGET_DIR)/etc/usb_modeswitch.setup
+	rm -f $(TARGET_DIR)/usr/share/man/man1/usb_modeswitch.1
 endef
 
 $(eval $(call GENTARGETS,package,usb_modeswitch))
+
