f421f557f52818ba835b1aa55f93682a76b0ee3c
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-04-29 23:00:43 +0300

    xlib_libXtst: depends on libXi
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xlib_libXtst/xlib_libXtst.mk
+++ package/x11r7/xlib_libXtst/xlib_libXtst.mk
@@ -7,10 +7,15 @@
 XLIB_LIBXTST_VERSION = 1.1.0
 XLIB_LIBXTST_SOURCE = libXtst-$(XLIB_LIBXTST_VERSION).tar.bz2
 XLIB_LIBXTST_SITE = http://xorg.freedesktop.org/releases/individual/lib
-XLIB_LIBXTST_AUTORECONF = NO
 XLIB_LIBXTST_LIBTOOL_PATCH = NO
 XLIB_LIBXTST_INSTALL_STAGING = YES
-XLIB_LIBXTST_DEPENDENCIES = xlib_libX11 xlib_libXext xproto_recordproto
+
+XLIB_LIBXTST_DEPENDENCIES = \
+	xlib_libX11 \
+	xlib_libXext \
+	xlib_libXi \
+	xproto_recordproto
+
 XLIB_LIBXTST_CONF_OPT = --enable-shared --disable-static
 
 $(eval $(call AUTOTARGETS,package/x11r7,xlib_libXtst))
