20c59892f9560184831154ab37bf519556303183
Darius Augulis <augulis.darius@gmail.com>
2010-04-29 23:11:25 +0300

    xda_xcursor-themes: depends on libXcursor
    While at it convert spaces to TABs
    
    Signed-off-by: Darius Augulis <augulis.darius@gmail.com>
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xdata_xcursor-themes/xdata_xcursor-themes.mk
+++ package/x11r7/xdata_xcursor-themes/xdata_xcursor-themes.mk
@@ -9,5 +9,6 @@ XDATA_XCURSOR_THEMES_SITE = http://xorg.freedesktop.org/releases/individual/data
 XDATA_XCURSOR_THEMES_AUTORECONF = NO
 XDATA_XCURSOR_THEMES_INSTALL_STAGING = YES
 XDATA_XCURSOR_THEMES_INSTALL_TARGET = YES
+XDATA_XCURSOR_THEMES_DEPENDENCIES = xlib_libXcursor
 
 $(eval $(call AUTOTARGETS,package/x11r7,xdata_xcursor-themes))
