6660b7dc3fd34de25de64d1cf983d097939b47c5
Lionel Landwerlin <llandwerlin@gmail.com>
2010-04-30 02:24:08 +0200

    scripts: force copy of files to erase already present and readonly files
    Signed-off-by: Lionel Landwerlin <llandwerlin@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -5,7 +5,7 @@ TARGET_DIR=$2
 
 echo "Copying development files to target..."
 
-cp -a ${STAGING_DIR}/usr/include ${TARGET_DIR}/usr
+cp -af ${STAGING_DIR}/usr/include ${TARGET_DIR}/usr
 
 for LIBSDIR in /lib /usr/lib; do
 	for WILDCARD in *.a *.la; do
@@ -13,7 +13,7 @@ for LIBSDIR in /lib /usr/lib; do
 			STAGING_STRIPPED=${FILE_PATH##${STAGING_DIR}}
 			EXTENDED_DIR=${STAGING_STRIPPED%/${WILDCARD}}
 			mkdir -p ${TARGET_DIR}${EXTENDED_DIR}
-			cp ${FILE_PATH} ${TARGET_DIR}${STAGING_STRIPPED}
+			cp -fa ${FILE_PATH} ${TARGET_DIR}${STAGING_STRIPPED}
 			#echo ${TARGET_DIR}${STAGING_STRIPPED}
 		done
 	done
