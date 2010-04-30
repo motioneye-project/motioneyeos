79832827f3e1bb26095a7ad5237d28c89c8b0eb7
Lionel Landwerlin <llandwerlin@gmail.com>
2010-04-30 02:58:49 +0200

    linux-fusion: bump to 8.1.1
    Also convert to generic package infrastructure
    
    Signed-off-by: Lionel Landwerlin <llandwerlin@gmail.com>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/linux-fusion/linux-fusion.mk
+++ package/linux-fusion/linux-fusion.mk
@@ -3,77 +3,64 @@
 # linux-fusion
 #
 #############################################################
-LINUX_FUSION_VERSION = 8.0.2
+LINUX_FUSION_VERSION = 8.1.1
 LINUX_FUSION_SOURCE = linux-fusion-$(LINUX_FUSION_VERSION).tar.gz
-LINUX_FUSION_SITE = http://www.directfb.org/downloads/Core/
-LINUX_FUSION_AUTORECONF = NO
+LINUX_FUSION_SITE = http://directfb.org/downloads/Core/linux-fusion
 LINUX_FUSION_INSTALL_STAGING = YES
-LINUX_FUSION_INSTALL_TARGET = YES
 
 # BR2_LINUX26_VERSION is not really dependable
 # LINUX26_VERSION is not yet set.
 # Retrieve REAL kernel version from file.
-LINUX_FOR_FUSION=`cat $(BUILD_DIR)/.linux-version`
+LINUX_FOR_FUSION=$(LINUX_VERSION)
+# `cat $(BUILD_DIR)/.linux-version`
+LINUX_FUSION_ETC_DIR=$(TARGET_DIR)/etc/udev/rules.d
 
-LINUX_FUSION_DIR:=$(BUILD_DIR)/linux-fusion-$(LINUX_FUSION_VERSION)
-LINUX_FUSION_ETC_DIR:=$(TARGET_DIR)/etc/udev/rules.d
+LINUX_FUSION_CAT=$(ZCAT)
 
-LINUX_FUSION_CAT:=$(ZCAT)
-
-LINUX_FUSION_MAKE_OPTS:=  KERNEL_VERSION=$(LINUX_FOR_FUSION)
+LINUX_FUSION_MAKE_OPTS =  KERNEL_VERSION=$(LINUX_FOR_FUSION)
 LINUX_FUSION_MAKE_OPTS += KERNEL_BUILD=$(BUILD_DIR)/linux-$(LINUX_FOR_FUSION)
 LINUX_FUSION_MAKE_OPTS += KERNEL_SOURCE=$(BUILD_DIR)/linux-$(LINUX_FOR_FUSION)
 
 LINUX_FUSION_MAKE_OPTS += SYSROOT=$(STAGING_DIR)
-LINUX_FUSION_MAKE_OPTS += ARCH=$(BR2_ARCH)
+LINUX_FUSION_MAKE_OPTS += ARCH=$(KERNEL_ARCH)
 LINUX_FUSION_MAKE_OPTS += CROSS_COMPILE=$(TARGET_CROSS)
 LINUX_FUSION_MAKE_OPTS += KERNEL_MODLIB=/lib/modules/$(LINUX_FOR_FUSION)
 LINUX_FUSION_MAKE_OPTS += DESTDIR=$(BUILD_DIR)/root
 LINUX_FUSION_MAKE_OPTS += HEADERDIR=$(STAGING_DIR)
-#LINUX_FUSION_MAKE_OPTS += 
+#LINUX_FUSION_MAKE_OPTS +=
 
 #LINUX_FUSION_MAKE_OPTS += __KERNEL__=$(LINUX26_VERSION)
 
-$(DL_DIR)/$(LINUX_FUSION_SOURCE):
-	$(call DOWNLOAD,$(LINUX_FUSION_SITE),$(LINUX_FUSION_SOURCE))
 
-$(LINUX_FUSION_DIR)/.unpacked: $(DL_DIR)/$(LINUX_FUSION_SOURCE)
-	$(LINUX_FUSION_CAT) $(DL_DIR)/$(LINUX_FUSION_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(LINUX_FUSION_DIR) package/linux-fusion/ linux-fusion\*.patch
-	touch $@
-
-$(LINUX_FUSION_DIR)/.install: $(LINUX_FUSION_DIR)/.unpacked
+define LINUX_FUSION_INSTALL_STAGING_CMDS
 	mkdir -p $(STAGING_DIR)/lib/modules/$(LINUX_FOR_FUSION)/source/include/linux
-	echo "LINUX=$(LINUX26_VERSION)"
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) \
 		$(LINUX_FUSION_MAKE_OPTS) \
-		-C $(LINUX_FUSION_DIR) install
-	rm -f $(TARGET_DIR)/usr/include/linux/fusion.h
+		INSTALL_MOD_PATH=$(STAGING_DIR) \
+		-C $(@D) headers_install
+endef
+
+define LINUX_FUSION_INSTALL_TARGET_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) \
+		$(LINUX_FUSION_MAKE_OPTS) \
+		INSTALL_MOD_PATH=$(TARGET_DIR) \
+		-C $(@D) install
 	mkdir -p $(LINUX_FUSION_ETC_DIR)
 	cp -dpf package/linux-fusion/40-fusion.rules $(LINUX_FUSION_ETC_DIR)
-	touch $@
+endef
 
-
-linux-fusion-source: $(DL_DIR)/$(LINUX_FUSION_SOURCE)
-
-linux-fusion-unpacked: $(LINUX_FUSION_DIR)/.unpacked
-
-linux-fusion: linux26 $(LINUX_FUSION_DIR)/.install
-
-linux-fusion-clean:
-	-$(MAKE) -C $(LINUX_FUSION_DIR) clean
+define LINUX_FUSION_UNINSTALL_STAGING
 	rm -f $(STAGING_DIR)/usr/include/linux/fusion.h
+endef
+
+LINUX_FUSION_UNINSTALL_STAGING_CMDS += LINUX_FUSION_UNINSTALL_STAGING
+
+define LINUX_FUSION_UNINSTALL_TARGET
+	rm -f $(TARGET_DIR)/usr/include/linux/fusion.h
 	rm -rf $(TARGET_DIR)/lib/modules/$(LINUX_FOR_FUSION)/drivers/char/fusion
-	rm -f $(LINUX_FUSION_DIR)/.install
+	rm -f $(LINUX_FUSION_ETC_DIR)/40-fusion.rules
+endef
 
-linux-fusion-dirclean:
-	rm -rf $(LINUX_FUSION_DIR)
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_LINUX_FUSION),y)
-TARGETS+=linux-fusion
-endif
+LINUX_FUSION_UNINSTALL_TARGET_CMDS += LINUX_FUSION_UNINSTALL_TARGET
 
+$(eval $(call GENTARGETS,package,linux-fusion))
