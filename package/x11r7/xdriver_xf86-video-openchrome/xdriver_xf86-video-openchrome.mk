ca218debd5c54023517955babd080dc0db9204ee
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-05-03 19:51:58 +0300

    X11 OpenChrome driver depends on libxcomposite
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xdriver_xf86-video-openchrome/xdriver_xf86-video-openchrome.mk
+++ package/x11r7/xdriver_xf86-video-openchrome/xdriver_xf86-video-openchrome.mk
@@ -7,7 +7,19 @@ XDRIVER_XF86_VIDEO_OPENCHROME_VERSION = 0.2.904
 XDRIVER_XF86_VIDEO_OPENCHROME_SOURCE = xf86-video-openchrome-$(XDRIVER_XF86_VIDEO_OPENCHROME_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_OPENCHROME_SITE = http://www.openchrome.org/releases
 
-XDRIVER_XF86_VIDEO_OPENCHROME_DEPENDENCIES = xserver_xorg-server libdrm xlib_libX11 xlib_libXvMC xproto_fontsproto xproto_glproto xproto_randrproto xproto_renderproto xproto_xextproto xproto_xf86driproto xproto_xproto
+XDRIVER_XF86_VIDEO_OPENCHROME_DEPENDENCIES = \
+	xserver_xorg-server \
+	libdrm \
+	xlib_libX11 \
+	xlib_libXcomposite \
+	xlib_libXvMC \
+	xproto_fontsproto \
+	xproto_glproto \
+	xproto_randrproto \
+	xproto_renderproto \
+	xproto_xextproto \
+	xproto_xf86driproto \
+	xproto_xproto
 
 XDRIVER_XF86_VIDEO_OPENCHROME_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
