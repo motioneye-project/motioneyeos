bfce1e4cbcd995792e5fe92c4c1f2d0e77d52618
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-05-10 14:52:27 -0300

    New lua module: copas
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/copas/copas.mk
@@ -0,0 +1,20 @@
+#############################################################
+#
+# copas
+#
+#############################################################
+
+COPAS_VERSION = 1.1.6
+COPAS_SITE = http://github.com/downloads/keplerproject/copas
+COPAS_DEPENDENCIES = lua coxpcall luasocket
+
+define COPAS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0644 -D $(@D)/src/copas/copas.lua \
+		$(TARGET_DIR)/usr/share/lua/copas.lua
+endef
+
+define COPAS_UNINSTALL_TARGET_CMDS
+	rm -f "$(TARGET_DIR)/usr/share/lua/copas.lua"
+endef
+
+$(eval $(call GENTARGETS,package,copas))
