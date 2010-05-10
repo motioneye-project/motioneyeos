50d6f2dde4db8c9d042294a09d5e8e120aec9987
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-05-10 14:47:12 -0300

    New lua module: rings
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/rings/rings.mk
@@ -0,0 +1,29 @@
+#############################################################
+#
+# rings
+#
+#############################################################
+
+RINGS_VERSION = 1.2.3
+RINGS_SITE = http://github.com/downloads/keplerproject/rings
+RINGS_DEPENDENCIES = lua
+
+define RINGS_BUILD_CMDS
+	$(MAKE) -C $(@D) CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS) -fPIC"
+endef
+
+define RINGS_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) LUA_LIBDIR="$(TARGET_DIR)/usr/lib/lua" \
+		LUA_DIR="$(TARGET_DIR)/usr/share/lua" install
+endef
+
+define RINGS_UNINSTALL_TARGET_CMDS
+	rm -f "$(TARGET_DIR)/usr/lib/lua/rings.so"
+	rm -f "$(TARGET_DIR)/usr/share/lua/stable.lua"
+endef
+
+define RINGS_CLEAN_CMDS
+	$(MAKE) -C $(@D) clean
+endef
+
+$(eval $(call GENTARGETS,package,rings))
