e74bcb6f9a69e55e57d8c677567c86fa76abc5a7
Peter Korsgaard <jacmet@sunsite.dk>
2010-05-26 22:46:05 +0200

    fs/squashfs: squashfs3 needs to set big/little endian
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- fs/squashfs/squashfs.mk
+++ fs/squashfs/squashfs.mk
@@ -8,10 +8,18 @@ ifeq ($(BR2_TARGET_ROOTFS_SQUASHFS4),y)
 ROOTFS_SQUASHFS_DEPENDENCIES = host-squashfs
 else
 ROOTFS_SQUASHFS_DEPENDENCIES = host-squashfs3
+
+ifeq ($(BR2_ENDIAN),"BIG")
+ROOTFS_SQUASHF_ARGS=-be
+else
+ROOTFS_SQUASHFS_ARGS=-le
+endif
+
 endif
 
 define ROOTFS_SQUASHFS_CMD
-	$(HOST_DIR)/usr/bin/mksquashfs $(TARGET_DIR) $$@ -noappend
+	$(HOST_DIR)/usr/bin/mksquashfs $(TARGET_DIR) $$@ -noappend \
+		$(ROOTFS_SQUASHFS_ARGS)
 endef
 
 $(eval $(call ROOTFS_TARGET,squashfs))
