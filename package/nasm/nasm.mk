da8d09b716a7dea65697efcadd1067d5e9b4f7f7
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2010-05-31 21:08:14 +0200

    nasm: new package, needed for syslinux
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/nasm/nasm.mk
@@ -0,0 +1,19 @@
+############################################################
+#
+# nasm
+#
+# This is special case: nasm is used to build syslinux and
+# pxelinux. As these are for the target, we should cross-compile
+# nasm. However, as nasm is x86-only, there's no point in
+# cross-compiling it. So we just build it for the host. The target
+# variant is only provided because of a bug in the package
+# infrastructure that prevents having only a host variant.
+############################################################
+
+NASM_VERSION=2.08.01
+NASM_SOURCE=nasm-$(NASM_VERSION).tar.bz2
+NASM_SITE=http://www.nasm.us/pub/nasm/releasebuilds/2.08.01/
+NASM_LIBTOOL_PATCH=NO
+
+$(eval $(call AUTOTARGETS,package,nasm))
+$(eval $(call AUTOTARGETS,package,nasm,host))
