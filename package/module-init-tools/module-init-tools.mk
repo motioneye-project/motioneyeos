dcc08e84a5e94e7ffaf52ec4374821e56bb20a34
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2010-06-05 17:39:05 +0200

    module-init-tools: bump version + convert to autotools
    The "remove-index" patch is no longer needed, the newer version of
    module-init-tools correctly uses strchr().
    
    The "module-init-tools" patch, whose main purpose was to disable the
    generation of man pages (it requires docbook2man), is replaced by a
    simpler implementation,
    module-init-tools-3.11-add-manpages-config-option.patch.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/module-init-tools/module-init-tools.mk
+++ package/module-init-tools/module-init-tools.mk
@@ -3,77 +3,17 @@
 # module-init-tools
 #
 #############################################################
-MODULE_INIT_TOOLS_VERSION=3.2.2
+MODULE_INIT_TOOLS_VERSION=3.11
 MODULE_INIT_TOOLS_SOURCE=module-init-tools-$(MODULE_INIT_TOOLS_VERSION).tar.bz2
-MODULE_INIT_TOOLS_CAT:=$(BZCAT)
 MODULE_INIT_TOOLS_SITE=$(BR2_KERNEL_MIRROR)/linux/utils/kernel/module-init-tools/
-MODULE_INIT_TOOLS_DIR=$(BUILD_DIR)/module-init-tools-$(MODULE_INIT_TOOLS_VERSION)
-MODULE_INIT_TOOLS_DIR2=$(TOOLCHAIN_DIR)/module-init-tools-$(MODULE_INIT_TOOLS_VERSION)
-MODULE_INIT_TOOLS_BINARY=depmod
-MODULE_INIT_TOOLS_TARGET_BINARY=$(TARGET_DIR)/sbin/$(MODULE_INIT_TOOLS_BINARY)
+MODULE_INIT_TOOLS_CONF_OPT = \
+	--disable-static-utils \
+	--disable-builddir \
+	--program-transform-name=''
 
-STRIPPROG=$(STRIPCMD)
+# module-init-tools-3.11-add-manpages-config-option.patch is modifying
+# configure.ac and Makefile.am
+MODULE_INIT_TOOLS_AUTORECONF=YES
+HOST_MODULE_INIT_TOOLS_AUTORECONF=YES
 
-$(DL_DIR)/$(MODULE_INIT_TOOLS_SOURCE):
-	$(call DOWNLOAD,$(MODULE_INIT_TOOLS_SITE),$(MODULE_INIT_TOOLS_SOURCE))
-
-$(MODULE_INIT_TOOLS_DIR)/.unpacked: $(DL_DIR)/$(MODULE_INIT_TOOLS_SOURCE)
-	$(MODULE_INIT_TOOLS_CAT) $(DL_DIR)/$(MODULE_INIT_TOOLS_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	toolchain/patch-kernel.sh $(MODULE_INIT_TOOLS_DIR) package/module-init-tools \*.patch
-	$(CONFIG_UPDATE) $(MODULE_INIT_TOOLS_DIR)
-	touch $(MODULE_INIT_TOOLS_DIR)/.unpacked
-
-$(MODULE_INIT_TOOLS_DIR)/.configured: $(MODULE_INIT_TOOLS_DIR)/.unpacked
-	(cd $(MODULE_INIT_TOOLS_DIR); rm -f config.cache; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		INSTALL=$(MODULE_INIT_TOOLS_DIR)/install-sh \
-		./configure $(QUIET) \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--prefix=/ \
-		--sysconfdir=/etc \
-		--program-transform-name='' \
-	)
-	touch $(MODULE_INIT_TOOLS_DIR)/.configured
-
-$(MODULE_INIT_TOOLS_DIR)/$(MODULE_INIT_TOOLS_BINARY): $(MODULE_INIT_TOOLS_DIR)/.configured
-	$(MAKE) CC=$(TARGET_CC) -C $(MODULE_INIT_TOOLS_DIR)
-	touch -c $(MODULE_INIT_TOOLS_DIR)/$(MODULE_INIT_TOOLS_BINARY)
-
-ifeq ($(BR2_PACKAGE_MODUTILS),y)
-$(MODULE_INIT_TOOLS_TARGET_BINARY): \
-	$(MODULE_INIT_TOOLS_DIR)/$(MODULE_INIT_TOOLS_BINARY) \
-	modutils
-else
-$(MODULE_INIT_TOOLS_TARGET_BINARY): \
-	$(MODULE_INIT_TOOLS_DIR)/$(MODULE_INIT_TOOLS_BINARY)
-endif
-ifeq ($(BR2_PACKAGE_MODUTILS),y)
-	$(MAKE) prefix=$(TARGET_DIR) -C $(MODULE_INIT_TOOLS_DIR) moveold
-endif
-	STRIPPROG='$(STRIPPROG)' \
-	$(MAKE) prefix=$(TARGET_DIR) -C $(MODULE_INIT_TOOLS_DIR) install-exec
-	rm -Rf $(TARGET_DIR)/usr/man
-	rm -f $(TARGET_DIR)/sbin/generate-modprobe.conf
-	rm -f $(TARGET_DIR)/sbin/insmod.static
-	touch -c $(MODULE_INIT_TOOLS_TARGET_BINARY)
-
-module-init-tools: $(MODULE_INIT_TOOLS_TARGET_BINARY)
-
-module-init-tools-clean:
-	$(MAKE) prefix=$(TARGET_DIR)/usr -C $(MODULE_INIT_TOOLS_DIR) uninstall
-	-$(MAKE) -C $(MODULE_INIT_TOOLS_DIR) clean
-
-module-init-tools-dirclean:
-	rm -rf $(MODULE_INIT_TOOLS_DIR)
-
-#############################################################
-#
-## Toplevel Makefile options
-#
-##############################################################
-ifeq ($(BR2_PACKAGE_MODULE_INIT_TOOLS),y)
-TARGETS+=module-init-tools
-endif
+$(eval $(call AUTOTARGETS,package,module-init-tools))
