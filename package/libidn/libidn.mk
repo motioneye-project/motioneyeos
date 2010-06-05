168f3b6e1412d47738c0a24660b34c58fdc680c7
Peter Korsgaard <jacmet@sunsite.dk>
2010-06-05 08:10:13 +0200

    libidn: fix build with config.cache, autoconf and emacs
    libidn contains a few elisp files, and it's configure script checks for
    emacs to know if it should install them. This is not important for BR as
    we don't have emacs, but configure fails if it's available on the host,
    config.cache is used and autoconf has already been used as that loads
    EMACS="no" into the cache.
    
    Fix it by also setting EMACS="no" here.
    
    At the same time, fix up trailing spaces in Config.in.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libidn/libidn.mk
+++ package/libidn/libidn.mk
@@ -8,6 +8,7 @@ LIBIDN_VERSION = 1.15
 LIBIDN_SITE = http://ftp.gnu.org/gnu/libidn/
 LIBIDN_INSTALL_STAGING = YES
 LIBIDN_INSTALL_TARGET = YES
+LIBIDN_CONF_ENV = EMACS="no"
 LIBIDN_CONF_OPT = --enable-shared --disable-java --enable-csharp=no
 LIBIDN_LIBTOOL_PATCH = NO
 LIBIDN_DEPENDENCIES = host-pkg-config $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext) $(if $(BR2_PACKAGE_LIBICONV),libiconv)
