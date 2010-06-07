8cd06967b9beb025ca37f695f18beb6dc45d6a85
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-06-07 11:35:58 -0300

    gawk: bump version and migrate to autotargets
    Closes #1933
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gawk/gawk.mk
+++ package/gawk/gawk.mk
@@ -3,75 +3,22 @@
 # gawk
 #
 #############################################################
-GAWK_VERSION:=3.1.5
-GAWK_SOURCE:=gawk-$(GAWK_VERSION).tar.bz2
-GAWK_SITE:=$(BR2_GNU_MIRROR)/gawk
-GAWK_CAT:=$(BZCAT)
-GAWK_DIR:=$(BUILD_DIR)/gawk-$(GAWK_VERSION)
-GAWK_BINARY:=gawk
-GAWK_TARGET_BINARY:=usr/bin/gawk
 
-$(DL_DIR)/$(GAWK_SOURCE):
-	 $(call DOWNLOAD,$(GAWK_SITE),$(GAWK_SOURCE))
+GAWK_VERSION = 3.1.8
+GAWK_SITE = $(BR2_GNU_MIRROR)/gawk
+GAWK_TARGET_BINS = awk gawk igawk pgawk
 
-gawk-source: $(DL_DIR)/$(GAWK_SOURCE)
+$(eval $(call AUTOTARGETS,package,gawk))
 
-$(GAWK_DIR)/.unpacked: $(DL_DIR)/$(GAWK_SOURCE)
-	$(GAWK_CAT) $(DL_DIR)/$(GAWK_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(GAWK_DIR) package/gawk gawk\*.patch
-	$(CONFIG_UPDATE) $(GAWK_DIR)
+$(GAWK_HOOK_POST_INSTALL): $(GAWK_TARGET_INSTALL_TARGET)
+	ln -sf /usr/bin/gawk $(TARGET_DIR)/usr/bin/awk
 	touch $@
 
-$(GAWK_DIR)/.configured: $(GAWK_DIR)/.unpacked
-	(cd $(GAWK_DIR); rm -rf config.cache; autoconf; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		ac_cv_func_getpgrp_void=yes \
-		./configure $(QUIET) \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--prefix=/usr \
-		--exec-prefix=/usr \
-		--bindir=/usr/bin \
-		--sbindir=/usr/sbin \
-		--libdir=/lib \
-		--libexecdir=/usr/lib \
-		--sysconfdir=/etc \
-		--datadir=/usr/share \
-		--localstatedir=/var \
-		--mandir=/usr/share/man \
-		--infodir=/usr/share/info \
-		$(DISABLE_NLS) \
-		$(DISABLE_LARGEFILE) \
-	)
-	touch $@
-
-$(GAWK_DIR)/$(GAWK_BINARY): $(GAWK_DIR)/.configured
-	$(MAKE) -C $(GAWK_DIR)
-
-$(TARGET_DIR)/$(GAWK_TARGET_BINARY): $(GAWK_DIR)/$(GAWK_BINARY)
-	rm -f $(TARGET_DIR)/usr/bin/awk
-	$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(GAWK_DIR) install
-	rm -f $(TARGET_DIR)/usr/bin/gawk-*
-	(cd $(TARGET_DIR)/usr/bin; ln -snf gawk awk)
-	$(STRIPCMD) $(TARGET_DIR)/usr/lib/awk/* > /dev/null 2>&1
-	rm -rf $(TARGET_DIR)/share/locale
-
-gawk: $(TARGET_DIR)/$(GAWK_TARGET_BINARY)
-
-gawk-clean:
-	$(MAKE) DESTDIR=$(TARGET_DIR) -C $(GAWK_DIR) uninstall
-	-$(MAKE) -C $(GAWK_DIR) clean
-
-gawk-dirclean:
-	rm -rf $(GAWK_DIR)
-
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_GAWK),y)
-TARGETS+=gawk
-endif
+$(GAWK_TARGET_UNINSTALL):
+	$(call MESSAGE,"Uninstalling")
+	rm -f $(addprefix $(TARGET_DIR)/usr/bin/, $(GAWK_TARGET_BINS))
+	rm -f $(TARGET_DIR)/usr/share/info/gawk*.info
+	rm -f $(TARGET_DIR)/usr/share/man/man*/*gawk.1
+	rm -rf $(TARGET_DIR)/usr/libexec/awk
+	rm -rf $(TARGET_DIR)/usr/share/awk
+	rm -f $(GAWK_TARGET_INSTALL_TARGET) $(GAWK_HOOK_POST_INSTALL)
