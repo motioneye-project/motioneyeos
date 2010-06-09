239181f865e8b6fa286264b190c0b89796fdd19e
Peter Korsgaard <jacmet@sunsite.dk>
2010-06-09 08:53:56 +0200

    file: use new-style target override for uninstall
    As suggested by Thomas.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/file/file.mk
+++ package/file/file.mk
@@ -10,11 +10,10 @@ FILE_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE"
 FILE_DEPENDENCIES = host-file zlib
 HOST_FILE_DEPENDENCIES = host-zlib
 
-$(eval $(call AUTOTARGETS,package,file))
-$(eval $(call AUTOTARGETS,package,file,host))
-
-$(FILE_TARGET_UNINSTALL):
-	$(call MESSAGE,"Uninstalling")
+define FILE_UNINSTALL_TARGET_CMDS
 	$(MAKE) DESTDIR=$(TARGET_DIR) uninstall -C $(FILE_DIR)
 	rm -f $(TARGET_DIR)/usr/lib/libmagic.*
-	rm -f $(FILE_TARGET_INSTALL_TARGET) $(FILE_HOOK_POST_INSTALL)
+endef
+
+$(eval $(call AUTOTARGETS,package,file))
+$(eval $(call AUTOTARGETS,package,file,host))
