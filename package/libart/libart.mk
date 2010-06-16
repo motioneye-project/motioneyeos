b767b8d1abe8969597fcadbfdae3f2906d192187
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-06-16 16:17:23 -0300

    libart: fix staging install
    The libart package had a couple of typos so it never really installed to
    staging. So fix it.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libart/libart.mk
+++ package/libart/libart.mk
@@ -8,7 +8,6 @@ LIBART_VERSION = 2.3.20
 LIBART_SOURCE = libart_lgpl-$(LIBART_VERSION).tar.gz
 LIBART_SITE = http://ftp.gnome.org/pub/gnome/sources/libart_lgpl/2.3/
 LIBART_AUTORECONF = YES
-LIBART_STAGING = YES
-LIBART_TARGET = YES
+LIBART_INSTALL_STAGING = YES
 
 $(eval $(call AUTOTARGETS,package,libart))
