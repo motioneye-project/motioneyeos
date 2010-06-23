5d73b07ee731a890e13b00b529f74b093d5ca8d4
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-06-23 17:22:16 -0300

    package: Add rrdtool package
    [Peter: use hook to remove examples]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/rrdtool/rrdtool.mk
@@ -0,0 +1,28 @@
+#############################################################
+#
+# rrdtool
+#
+#############################################################
+
+RRDTOOL_VERSION = 1.2.30
+RRDTOOL_SITE = http://oss.oetiker.ch/rrdtool/pub
+RRDTOOL_DEPENDENCIES = host-pkg-config freetype libart libpng zlib
+RRDTOOL_INSTALL_STAGING = YES
+RRDTOOL_CONF_ENV = rd_cv_ieee_works=yes rd_cv_null_realloc=nope \
+			ac_cv_func_mmap_fixed_mapped=yes
+RRDTOOL_CONF_OPT = --disable-perl --disable-python --disable-ruby \
+			--disable-tcl --program-transform-name=''
+
+define RRDTOOL_REMOVE_EXAMPLES
+	rm -rf $(TARGET_DIR)/usr/share/rrdtool/examples
+endef
+
+RRDTOOL_POST_INSTALL_TARGET_HOOKS += RRDTOOL_REMOVE_EXAMPLES
+
+define RRDTOOL_UNINSTALL_TARGET_CMDS
+	$(MAKE) DESTDIR=$(TARGET_DIR) -C $(@D) uninstall
+	rm -rf $(TARGET_DIR)/usr/share/rrdtool
+	rm -f $(TARGET_DIR)/usr/lib/librrd*
+endef
+
+$(eval $(call AUTOTARGETS,package,rrdtool))
