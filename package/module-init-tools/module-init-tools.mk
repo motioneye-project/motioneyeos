503f8c041d7791d89538b214a0df1a946c4f7586
Peter Korsgaard <jacmet@sunsite.dk>
2010-06-23 11:05:51 +0200

    module-init-tools: don't build documentation
    We don't have docbook2man in buildroot, and the build breaks if you
    have it on the host, but in an incompatible version (like on my machine).
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/module-init-tools/module-init-tools.mk
+++ package/module-init-tools/module-init-tools.mk
@@ -6,6 +6,7 @@
 MODULE_INIT_TOOLS_VERSION=3.11
 MODULE_INIT_TOOLS_SOURCE=module-init-tools-$(MODULE_INIT_TOOLS_VERSION).tar.bz2
 MODULE_INIT_TOOLS_SITE=$(BR2_KERNEL_MIRROR)/linux/utils/kernel/module-init-tools/
+MODULE_INIT_TOOLS_CONF_ENV = ac_cv_prog_DOCBOOKTOMAN=''
 MODULE_INIT_TOOLS_CONF_OPT = \
 	--disable-static-utils \
 	--disable-builddir \
@@ -15,6 +16,7 @@ MODULE_INIT_TOOLS_CONF_OPT = \
 # configure.ac and Makefile.am
 MODULE_INIT_TOOLS_AUTORECONF=YES
 HOST_MODULE_INIT_TOOLS_AUTORECONF=YES
+HOST_MODULE_INIT_TOOLS_CONF_ENV = ac_cv_prog_DOCBOOKTOMAN=''
 
 $(eval $(call AUTOTARGETS,package,module-init-tools))
 $(eval $(call AUTOTARGETS,package,module-init-tools,host))
