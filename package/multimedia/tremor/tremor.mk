da32c7ca2d488ffdd2e37a1cc533338741342a3c
Peter Korsgaard <jacmet@sunsite.dk>
2010-07-01 17:33:29 +0200

    tremor: fix svn checkout
    Commit 1dc19c445 (split tremor into its own package) unfortunately
    broke the svn checkout step (but normally not noticable as we have
    a tarball on sources.buildroot.net that will get downloaded instead).
    
    Fix it by using a custom download step, and remove unused variables
    while we're at it.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/tremor/tremor.mk
+++ package/multimedia/tremor/tremor.mk
@@ -4,28 +4,20 @@
 #
 ############################################################
 
-TREMOR_TRUNK:=http://svn.xiph.org/trunk/Tremor/
+TREMOR_SITE:=http://svn.xiph.org/trunk/Tremor/
 TREMOR_VERSION:=16259
-TREMOR_NAME:=Tremor-svn-r$(TREMOR_VERSION)
-TREMOR_DIR:=$(BUILD_DIR)/$(TREMOR_NAME)
-TREMOR_SOURCE:=$(TREMOR_NAME).tar.bz2
-TREMOR_CAT=$(BZCAT)
+TREMOR_SVNDIR = Tremor-svn-r$(TREMOR_VERSION)
+TREMOR_SOURCE:= $(TREMOR_SVNDIR).tar.bz2
 TREMOR_AUTORECONF = YES
 TREMOR_INSTALL_STAGING = YES
 TREMOR_INSTALL_TARGET = YES
 
 $(DL_DIR)/$(TREMOR_SOURCE):
-	$(SVN_CO) -r $(TREMOR_VERSION) $(TREMOR_TRUNK) $(TREMOR_DIR)
-	tar -cv -C $(BUILD_DIR) $(TREMOR_NAME) | bzip2 - -c > $@
+	$(SVN_CO) -r $(TREMOR_VERSION) $(TREMOR_SITE) $(BUILD_DIR)/$(TREMOR_SVNDIR)
+	tar -cv -C $(BUILD_DIR) $(TREMOR_SVNDIR) | bzip2 - -c > $@
+	rm -rf $(BUILD_DIR)/$(TREMOR_SVNDIR)
+
+# use custom download step
+TREMOR_TARGET_SOURCE := $(DL_DIR)/$(TREMOR_SOURCE)
 
 $(eval $(call AUTOTARGETS,package/multimedia,tremor))
-
-
-############################################################
-#
-# Toplevel Makefile options
-#
-############################################################
-ifeq ($(BR2_PACKAGE_TREMOR),y)
-TARGETS+=tremor
-endif
