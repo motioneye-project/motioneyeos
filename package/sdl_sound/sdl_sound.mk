5ba54aa10bb8936f3575e3b97916d299783cf858
Peter Korsgaard <jacmet@sunsite.dk>
2010-07-04 08:56:00 +0200

    sdl_sound: add optional dependencies
    Add the optional dependencies we have in BR (flac/speex/vorbis).
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sdl_sound/sdl_sound.mk
+++ package/sdl_sound/sdl_sound.mk
@@ -15,6 +15,19 @@ ifneq ($(BR2_ENABLE_LOCALE),y)
 SDL_SOUND_DEPENDENCIES += libiconv
 endif
 
+# optional dependencies
+ifeq ($(BR2_PACKAGE_FLAC),y)
+SDL_SOUND_DEPENDENCIES += flac # is only used if ogg is also enabled
+endif
+
+ifeq ($(BR2_PACKAGE_LIBVORBIS),y)
+SDL_SOUND_DEPENDENCIES += libvorbis
+endif
+
+ifeq ($(BR2_PACKAGE_SPEEX),y)
+SDL_SOUND_DEPENDENCIES += speex
+endif
+
 SDL_SOUND_CONF_OPT:=--with-sdl-prefix=$(STAGING_DIR)/usr \
 		--with-sdl-exec-prefix=$(STAGING_DIR)/usr \
 		--disable-sdltest \
