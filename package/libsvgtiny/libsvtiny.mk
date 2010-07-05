ab10ed8c87c38c54940ee5418b996b7a6ca79642
Peter Korsgaard <jacmet@sunsite.dk>
2010-07-05 11:01:55 +0200

    package: add libsvgtiny library
    No official releases, so use svn snapshot like tremor :/
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libsvgtiny/libsvtiny.mk
@@ -0,0 +1,47 @@
+############################################################
+#
+# libsvgtiny
+#
+############################################################
+
+LIBSVGTINY_SITE = svn://svn.netsurf-browser.org/trunk/libsvgtiny
+LIBSVGTINY_VERSION = 9800
+LIBSVGTINY_SVNDIR = libsvgtiny-svn-r$(LIBSVGTINY_VERSION)
+LIBSVGTINY_SOURCE = $(LIBSVGTINY_SVNDIR).tar.bz2
+LIBSVGTINY_INSTALL_STAGING = YES
+LIBSVGTINY_INSTALL_TARGET = YES
+LIBSVGTINY_DEPENDENCIES = libxml2
+
+$(DL_DIR)/$(LIBSVGTINY_SOURCE):
+	$(SVN_CO) -r $(LIBSVGTINY_VERSION) $(LIBSVGTINY_SITE) $(BUILD_DIR)/$(LIBSVGTINY_SVNDIR)
+	tar -cv -C $(BUILD_DIR) $(LIBSVGTINY_SVNDIR) | bzip2 - -c > $@
+	rm -rf $(BUILD_DIR)/$(LIBSVGTINY_SVNDIR)
+
+# use custom download step
+LIBSVGTINY_TARGET_SOURCE := $(DL_DIR)/$(LIBSVGTINY_SOURCE)
+
+define LIBSVGTINY_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) PREFIX=/usr
+endef
+
+define LIBSVGTINY_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) PREFIX=/usr DESTDIR=$(STAGING_DIR) install
+endef
+
+define LIBSVGTINY_UNINSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) PREFIX=/usr DESTDIR=$(STAGING_DIR) uninstall
+endef
+
+define LIBSVGTINY_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) PREFIX=/usr DESTDIR=$(TARGET_DIR) install
+endef
+
+define LIBSVGTINY_UNINSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) PREFIX=/usr DESTDIR=$(TARGET_DIR) uninstall
+endef
+
+define BZIP2_CLEAN_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) clean
+endef
+
+$(eval $(call GENTARGETS,package,libsvgtiny))
