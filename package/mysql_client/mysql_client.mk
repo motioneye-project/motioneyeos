ab7d4fb421d7d765f832cd4c7fd8cb10004b8a70
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2010-07-16 11:18:09 +0200

    mysql_client: Bump to 5.1.47 and fix build
    MySQL build has been broken for quite some time due to the ./configure
    inability to find out how to use 'ps' to find the PID of a running
    program. This commit bumps MySQL to the latest version and adds a few
    patches to fix various build issues, including the 'ps' issue.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/mysql_client/mysql_client.mk
+++ package/mysql_client/mysql_client.mk
@@ -3,23 +3,31 @@
 # MySQL 5.1 Client
 #
 #############################################################
-MYSQL_CLIENT_VERSION = 5.1.23-rc
+MYSQL_CLIENT_VERSION = 5.1.47
 MYSQL_CLIENT_SOURCE = mysql-$(MYSQL_CLIENT_VERSION).tar.gz
-MYSQL_CLIENT_SITE = http://mirrors.24-7-solutions.net/pub/mysql/Downloads/MySQL-5.1
+MYSQL_CLIENT_SITE = http://downloads.mysql.com/archives/mysql-5.1/
 MYSQL_CLIENT_INSTALL_TARGET = YES
 MYSQL_CLIENT_INSTALL_STAGING = YES
 MYSQL_CLIENT_DEPENDENCIES = readline ncurses
+MYSQL_CLIENT_AUTORECONF=YES
 
-MYSQL_CLIENT_CONF_ENV = ac_cv_sys_restartable_syscalls=yes
+MYSQL_CLIENT_CONF_ENV = \
+	ac_cv_sys_restartable_syscalls=yes \
+	ac_cv_path_PS=/bin/ps \
+	ac_cv_FIND_PROC="/bin/ps p \$\$PID | grep -v grep | grep mysqld > /dev/null" \
+	ac_cv_have_decl_HAVE_IB_ATOMIC_PTHREAD_T_GCC=yes \
+	ac_cv_have_decl_HAVE_IB_ATOMIC_PTHREAD_T_SOLARIS=no \
+	ac_cv_have_decl_HAVE_IB_GCC_ATOMIC_BUILTINS=yes \
+	mysql_cv_new_rl_interface=yes
+
 MYSQL_CLIENT_CONF_OPT = \
 	--program-prefix="" \
 	--without-ndb-binlog \
 	--without-server \
 	--without-docs \
 	--without-man \
-	--without-readline \
 	--without-libedit \
-	--with-readline=$(STAGING_DIR)/usr \
+	--without-readline \
 	--with-low-memory \
 	--enable-thread-safe-client \
 	$(ENABLE_DEBUG)
