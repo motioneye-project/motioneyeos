aaa2b660a9f9f0446fb2ebd4dde7c76c9750f9c8
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2010-07-25 16:53:51 +0200

    setserial: convert to autotargets and fix manpages install
    Close #2269
    
    [Peter: fix Makefile patch to not strip wrong file, rename hayes patch]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/setserial/setserial.mk
+++ package/setserial/setserial.mk
@@ -3,16 +3,18 @@
 # Setserial
 #
 #############################################################
-SETSERIAL_VERSION:=2.17
-SETSERIAL_PATCH_VERSION:=.orig
-SETSERIAL_PATCH_FILE:=setserial_2.17-45.diff.gz
-SETSERIAL_SOURCE:=setserial_$(SETSERIAL_VERSION)$(SETSERIAL_PATCH_VERSION).tar.gz
-SETSERIAL_SITE:=$(BR2_DEBIAN_MIRROR)/debian/pool/main/s/setserial/
-SETSERIAL_INSTALL_TARGET_OPT=DESTDIR=$(TARGET_DIR) install
+SETSERIAL_VERSION            = 2.17
+SETSERIAL_PATCH              = setserial_2.17-45.2.diff.gz
+SETSERIAL_SOURCE             = setserial_$(SETSERIAL_VERSION).orig.tar.gz
+SETSERIAL_SITE               = $(BR2_DEBIAN_MIRROR)/debian/pool/main/s/setserial/
+SETSERIAL_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
 define SETSERIAL_APPLY_DEBIAN_PATCHES
+	# Touching gorhack.h is needed for the Debian patch 18 to work
 	if [ -d $(@D)/debian/patches ]; then \
-		toolchain/patch-kernel.sh $(@D) $(@D)/debian/patches \*.patch; \
+		touch $(@D)/gorhack.h; \
+		rm $(@D)/debian/patches/01_makefile.dpatch; \
+		toolchain/patch-kernel.sh $(@D) $(@D)/debian/patches *.dpatch; \
 	fi
 endef
 
