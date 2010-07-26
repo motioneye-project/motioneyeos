52beecb809d24264970a5748ba77a005159db45b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-07-26 22:54:52 +0200

    iw: bump to 0.9.20 and migrate to gentargets
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/iw/iw.mk
+++ package/iw/iw.mk
@@ -4,25 +4,32 @@
 #
 #############################################################
 
-IW_VERSION = 0.9.19
+IW_VERSION = 0.9.20
 IW_SOURCE = iw-$(IW_VERSION).tar.bz2
 IW_SITE = http://wireless.kernel.org/download/iw
 IW_DEPENDENCIES = host-pkg-config libnl
-IW_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 IW_CONFIG = $(IW_DIR)/.config
 IW_MAKE_ENV = PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" \
 	PKG_CONFIG="$(HOST_DIR)/usr/bin/pkg-config" \
 	GIT_DIR=$(IW_DIR)
 
-$(eval $(call AUTOTARGETS,package,iw))
-
-$(IW_TARGET_CONFIGURE):
+define IW_CONFIGURE_CMDS
 	echo "CC = $(TARGET_CC)" >$(IW_CONFIG)
 	echo "CFLAGS = $(TARGET_CFLAGS)" >>$(IW_CONFIG)
 	echo "LDFLAGS = $(TARGET_LDFLAGS)" >>$(IW_CONFIG)
-	touch $@
+endef
 
-$(IW_TARGET_UNINSTALL):
-	$(call MESSAGE,"Uninstalling")
-	rm -f $(TARGET_DIR)/usr/bin/iw
-	rm -f $(IW_TARGET_INSTALL_TARGET) $(IW_HOOK_POST_INSTALL)
+define IW_BUILD_CMDS
+	$(IW_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define IW_INSTALL_TARGET_CMDS
+	$(IW_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
+endef
+
+define IW_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/sbin/iw
+	rm -f $(TARGET_DIR)/usr/share/man/man8/iw.8*
+endef
+
+$(eval $(call GENTARGETS,package,iw))
