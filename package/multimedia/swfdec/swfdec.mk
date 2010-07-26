f44935e4d3719ae1d5e3dcf95d507bda1fe6b8e6
Peter Korsgaard <jacmet@sunsite.dk>
2010-07-26 11:10:03 +0200

    swfdec: fix build when no libglib2-dev package is installed on host
    Make sure it uses the libglib2-host binaries we are building.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/swfdec/swfdec.mk
+++ package/multimedia/swfdec/swfdec.mk
@@ -8,6 +8,9 @@ SWFDEC_VERSION_MINOR = 4
 SWFDEC_VERSION = $(SWFDEC_VERSION_MAJOR).$(SWFDEC_VERSION_MINOR)
 SWFDEC_SOURCE = swfdec-$(SWFDEC_VERSION).tar.gz
 SWFDEC_SITE = http://swfdec.freedesktop.org/download/swfdec/$(SWFDEC_VERSION_MAJOR)
+SWFDEC_MAKE_OPT = \
+	GLIB_MKENUMS=$(HOST_DIR)/usr/bin/glib-mkenums \
+	GLIB_GENMARSHAL=$(HOST_DIR)/usr/bin/glib-genmarshal
 
 SWFDEC_LIBTOOL_PATCH = NO
 SWFDEC_INSTALL_STAGING = YES
@@ -28,3 +31,8 @@ SWFDEC_CONF_OPT += --disable-gtk
 endif
 
 $(eval $(call AUTOTARGETS,package/multimedia,swfdec))
+
+# swfdec uses glib-* at install time
+# Notice: must come after AUTOTARGETS as that's where these variables gets set
+SWFDEC_INSTALL_TARGET_OPT += $(SWFDEC_MAKE_OPT)
+SWFDEC_INSTALL_STAGING_OPT += $(SWFDEC_MAKE_OPT)
