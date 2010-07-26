3da3c338773021406a76e17f37ba7b9c1650b8b4
Peter Korsgaard <jacmet@sunsite.dk>
2010-07-26 15:04:59 +0200

    flex: fix configure cache issue with BR2_NEEDS_GETTEXT
    Flex doesn't NEED gettext/libintl, but it's configure script checks for it,
    so make sure those a built before flex, otherwise flex will populate
    tgt-config.cache with invalid values, breaking the build of other packages
    needing it (like libglib2).
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/flex/flex.mk
+++ package/flex/flex.mk
@@ -10,6 +10,9 @@ FLEX_PATCH:=flex_$(FLEX_VERSION)-$(FLEX_PATCH_VERSION).diff.gz
 FLEX_SITE:=$(BR2_DEBIAN_MIRROR)/debian/pool/main/f/flex
 FLEX_DIR:=$(BUILD_DIR)/flex-$(FLEX_VERSION)
 FLEX_INSTALL_STAGING=YES
+FLEX_DEPENDENCIES = \
+	$(if $(BR2_PACKAGE_GETTEXT),gettext) \
+	$(if $(BR2_PACKAGE_LIBINTL),libintl)
 
 # lex -> flex
 define FLEX_INSTALL_LEX
