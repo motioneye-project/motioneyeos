a731c7d6235306a6ecb29ff7848682736fde7a7b
Peter Korsgaard <jacmet@sunsite.dk>
2010-07-27 09:25:39 +0200

    gnuchess: fix configure cache issue with readline
    Make sure readline is built before gnuchess if enabled, so it correctly
    detects it and doesn't populate tgt-config.cache with invalid values.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/games/gnuchess/gnuchess.mk
+++ package/games/gnuchess/gnuchess.mk
@@ -7,5 +7,7 @@ GNUCHESS_VERSION = 5.07
 GNUCHESS_SOURCE = gnuchess-$(GNUCHESS_VERSION).tar.gz
 GNUCHESS_SITE = $(BR2_GNU_MIRROR)/chess
 
+GNUCHESS_DEPENDENCIES = $(if $(BR2_PACKAGE_READLINE),readline)
+
 $(eval $(call AUTOTARGETS,package/games,gnuchess))
 
