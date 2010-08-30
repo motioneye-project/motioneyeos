40f8f3ff1d7582e1a2d92c707a57582e7dccbb54
Peter Korsgaard <jacmet@sunsite.dk>
2010-08-30 22:52:01 +0200

    xstroke: fix dependencies
    xstroke doesn't need docker, but it does need libXft/Xtst/Xpm.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/xstroke/xstroke.mk
+++ package/xstroke/xstroke.mk
@@ -6,12 +6,9 @@
 XSTROKE_VERSION = 0.6
 XSTROKE_SOURCE = xstroke-$(XSTROKE_VERSION).tar.gz
 XSTROKE_SITE = http://avr32linux.org/twiki/pub/Main/XStroke
-XSTROKE_AUTORECONF = NO
-XSTROKE_INSTALL_STAGING = NO
-XSTROKE_INSTALL_TARGET = YES
 XSTROKE_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
-XSTROKE_DEPENDENCIES = xserver_xorg-server
+XSTROKE_DEPENDENCIES = xlib_libXft xlib_libXtst xlib_libXpm
 
 $(eval $(call AUTOTARGETS,package,xstroke))
 
