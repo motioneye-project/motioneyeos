ee314c97f9321eda8776d17a02ce445691eda50c
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-09-01 09:26:21 -0300

    kismet: bump to  2010-07-R1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/kismet/kismet.mk
+++ package/kismet/kismet.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-KISMET_VERSION = 2010-01-R1
+KISMET_VERSION = 2010-07-R1
 KISMET_SITE = http://www.kismetwireless.net/code
 KISMET_DEPENDENCIES = libpcap ncurses
 
