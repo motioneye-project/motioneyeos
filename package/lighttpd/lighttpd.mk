489e2b803e9ef362cbc4fef675459a6a781bb16a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-09-06 16:08:47 -0300

    lighttpd: bump version
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/lighttpd/lighttpd.mk
+++ package/lighttpd/lighttpd.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIGHTTPD_VERSION = 1.4.26
+LIGHTTPD_VERSION = 1.4.28
 LIGHTTPD_SITE = http://download.lighttpd.net/lighttpd/releases-1.4.x
 LIGHTTPD_LIBTOOL_PATCH = NO
 
