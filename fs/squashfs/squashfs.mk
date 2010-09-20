ed4ca35a1a5996569b1a1d769cb080c5f1fd62c6
Peter Korsgaard <jacmet@sunsite.dk>
2010-09-20 22:05:39 +0200

    squashfs: add lzma support
    And try to select a sane default compression algorithm.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- fs/squashfs/squashfs.mk
+++ fs/squashfs/squashfs.mk
@@ -10,7 +10,11 @@ ROOTFS_SQUASHFS_DEPENDENCIES = host-squashfs
 ifeq ($(BR2_TARGET_ROOTFS_SQUASHFS4_LZO),y)
 ROOTFS_SQUASHFS_ARGS += -comp lzo
 else
+ifeq ($(BR2_TARGET_ROOTFS_SQUASHFS4_LZMA),y)
+ROOTFS_SQUASHFS_ARGS += -comp lzma
+else
 ROOTFS_SQUASHFS_ARGS += -comp gzip
+endif
 endif
 
 else
