5e6e63e53782a3814fafe8b1834aa575e7b0c81f
Peter Korsgaard <jacmet@sunsite.dk>
2010-09-25 23:48:50 +0200

    sdl_mixer: disable plugins not available in BR, add optional mp3/ogg support
    Closes #2521
    
    Disable plugins with dependencies we don't have in BR / aren't enabled, so
    the configure script doesn't get confused if they are installed on the
    host. Also enable ogg/libmad support if enabled in BR.
    
    Notice: flac support is disabled for now as the sdl_mixer configure script
    doesn't work when cross compiling.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sdl_mixer/sdl_mixer.mk
+++ package/sdl_mixer/sdl_mixer.mk
@@ -10,7 +10,27 @@ SDL_MIXER_SITE:=http://www.libsdl.org/projects/SDL_mixer/release/
 SDL_MIXER_LIBTOOL_PATCH = NO
 SDL_MIXER_INSTALL_STAGING = YES
 SDL_MIXER_DEPENDENCIES = sdl
-SDL_MIXER_CONF_OPT = --without-x --with-sdl-prefix=$(STAGING_DIR)/usr
+SDL_MIXER_CONF_OPT = \
+	--without-x \
+	--with-sdl-prefix=$(STAGING_DIR)/usr \
+	--disable-music-midi \
+	--disable-music-mod \
+	--disable-music-mp3 \
+	--disable-music-flac # configure script fails when cross compiling
+
+ifeq ($(BR2_PACKAGE_LIBMAD),y)
+SDL_MIXER_CONF_OPT += --enable-music-mp3-mad-gpl
+SDL_MIXER_DEPENDENCIES += libmad
+else
+SDL_MIXER_CONF_OPT += --disable-music-mp3-mad-gpl
+endif
+
+ifeq ($(BR2_PACKAGE_LIBVORBIS),y)
+SDL_MIXER_CONF_OPT += --enable-music-ogg
+SDL_MIXER_DEPENDENCIES += libvorbis
+else
+SDL_MIXER_CONF_OPT += --disable-music-ogg
+endif
 
 define SDL_MIXER_INSTALL_TARGET_CMDS
 	cp -dpf $(STAGING_DIR)/usr/lib/libSDL_mixer*.so* $(TARGET_DIR)/usr/lib/
