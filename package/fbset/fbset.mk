789f61379afa8d065edeb6f876e9d0025c4f4fe5
Peter Korsgaard <jacmet@sunsite.dk>
2010-09-26 20:54:34 +0200

    fbset: misc cleanup
    Use install -D so it doesn't fail if usr/sbin doesn't exist, remove
    unneeded stripping and move target cleanup to _UNINSTALL_TARGET_CMDS.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/fbset/fbset.mk
+++ package/fbset/fbset.mk
@@ -6,20 +6,20 @@
 FBSET_VERSION = 2.1
 FBSET_SOURCE = fbset-$(FBSET_VERSION).tar.gz
 FBSET_SITE = http://users.telenet.be/geertu/Linux/fbdev
-FBSET_BINARY = fbset
-FBSET_TARGET_BINARY = usr/sbin/$(FBSET_BINARY)
 
 define FBSET_BUILD_CMDS
 	$(MAKE1) $(TARGET_CONFIGURE_OPTS) -C $(@D)
 endef
 
 define FBSET_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 755 $(@D)/$(FBSET_BINARY) $(TARGET_DIR)/$(FBSET_TARGET_BINARY)
-	-$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/$(FBSET_TARGET_BINARY)
+	$(INSTALL) -D -m 755 $(@D)/fbset $(TARGET_DIR)/usr/sbin/fbset
+endef
+
+define FBSET_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/sbin/fbset
 endef
 
 define FBSET_CLEAN_CMDS
-	rm -f $(TARGET_DIR)/$(FBSET_TARGET_BINARY)
 	-$(MAKE) -C $(@D) clean
 endef
 
