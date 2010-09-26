9793ea56615616589e2eccdab65c49aa501c2ed4
Martin Banky <martin.banky@gmail.com>
2010-09-26 00:14:08 -0700

    lockfile-progs: convert to gentargets and bump to 0.1.15
    [Peter: no strip, remove unused _BINARY var]
    Signed-off-by: Martin Banky <Martin.Banky@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/lockfile-progs/lockfile-progs.mk
+++ package/lockfile-progs/lockfile-progs.mk
@@ -3,42 +3,21 @@
 # lockfile-progs
 #
 #############################################################
-LOCKFILE_PROGS_VERSION=0.1.11-0.1
-LOCKFILE_PROGS_SOURCE:=lockfile-progs_$(LOCKFILE_PROGS_VERSION).tar.gz
-LOCKFILE_PROGS_SITE:=$(BR2_DEBIAN_MIRROR)/debian/pool/main/l/lockfile-progs/
-LOCKFILE_PROGS_CAT:=$(ZCAT)
-LOCKFILE_PROGS_DIR:=$(BUILD_DIR)/lockfile-progs-0.1.11
-LOCKFILE_PROGS_BINARY:=usr/bin/lockfile-create
+LOCKFILE_PROGS_VERSION = 0.1.15
+LOCKFILE_PROGS_SOURCE = lockfile-progs_$(LOCKFILE_PROGS_VERSION).tar.gz
+LOCKFILE_PROGS_SITE = $(BR2_DEBIAN_MIRROR)/debian/pool/main/l/lockfile-progs/
 
-$(DL_DIR)/$(LOCKFILE_PROGS_SOURCE):
-	 $(call DOWNLOAD,$(LOCKFILE_PROGS_SITE),$(LOCKFILE_PROGS_SOURCE))
+define LOCKFILE_PROGS_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
 
-lockfile-progs-source: $(DL_DIR)/$(LOCKFILE_PROGS_SOURCE)
+define LOCKFILE_PROGS_INSTALL_TARGET_CMDS
+	cp -a $(@D)/bin/lockfile* $(TARGET_DIR)/usr/bin
+endef
 
-$(LOCKFILE_PROGS_DIR)/.unpacked: $(DL_DIR)/$(LOCKFILE_PROGS_SOURCE)
-	$(LOCKFILE_PROGS_CAT) $(DL_DIR)/$(LOCKFILE_PROGS_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(LOCKFILE_PROGS_DIR) package/lockfile-progs/ \*.patch
-	touch $(LOCKFILE_PROGS_DIR)/.unpacked
-
-$(TARGET_DIR)/$(LOCKFILE_PROGS_BINARY): $(LOCKFILE_PROGS_DIR)/.unpacked
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(LOCKFILE_PROGS_DIR)
-	cp -a $(LOCKFILE_PROGS_DIR)/bin/lockfile* $(TARGET_DIR)/usr/bin
-	$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/$(LOCKFILE_PROGS_BINARY)
-
-lockfile-progs: liblockfile $(TARGET_DIR)/$(LOCKFILE_PROGS_BINARY)
-
-lockfile-progs-clean:
+define LOCKFILE_PROGS_CLEAN_CMDS
 	-rm -f $(TARGET_DIR)/usr/bin/lockfile-*
-	-$(MAKE) -C $(LOCKFILE_PROGS_DIR) clean
+	-$(MAKE) -C $(@D) clean
+endef
 
-lockfile-progs-dirclean:
-	rm -rf $(LOCKFILE_PROGS_DIR)
-
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_LOCKFILE_PROGS),y)
-TARGETS+=lockfile-progs
-endif
+$(eval $(call GENTARGETS,package,lockfile-progs))
