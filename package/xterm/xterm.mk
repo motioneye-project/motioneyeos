1f4000e562040046aef1ec79e33abdeedbe33005
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-09-28 14:52:15 +0300

    xterm: should select libXaw and bump to latest version
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/xterm/xterm.mk
+++ package/xterm/xterm.mk
@@ -4,10 +4,10 @@
 #
 #############################################################
 
-XTERM_VERSION:=259
+XTERM_VERSION:=262
 XTERM_SOURCE:=xterm-$(XTERM_VERSION).tgz
 XTERM_SITE:=ftp://invisible-island.net/xterm
-XTERM_DEPENDENCIES = xserver_xorg-server
+XTERM_DEPENDENCIES = xserver_xorg-server xlib_libXaw
 XTERM_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
 $(eval $(call AUTOTARGETS,package,xterm))
