799c1328234bca5c39249ee0fea7cc614c0be680
Peter Korsgaard <jacmet@sunsite.dk>
2010-09-29 21:04:57 +0200

    libdaemon; bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libdaemon/libdaemon.mk
+++ package/libdaemon/libdaemon.mk
@@ -4,15 +4,15 @@
 #
 #############################################################
 
-LIBDAEMON_VERSION:=0.13
-LIBDAEMON_SOURCE:=libdaemon-$(LIBDAEMON_VERSION).tar.gz
-LIBDAEMON_SITE:=http://0pointer.de/lennart/projects/libdaemon/
-LIBDAEMON_AUTORECONF:=no
-LIBDAEMON_INSTALL_STAGING:=YES
-LIBDAEMON_INSTALL_TARGET:=YES
-LIBDAEMON_CONF_ENV:=ac_cv_func_setpgrp_void=no
-LIBDAEMON_CONF_OPT:=--disable-lynx 
+LIBDAEMON_VERSION = 0.14
+LIBDAEMON_SOURCE = libdaemon-$(LIBDAEMON_VERSION).tar.gz
+LIBDAEMON_SITE = http://0pointer.de/lennart/projects/libdaemon/
+LIBDAEMON_AUTORECONF = NO
+LIBDAEMON_LIBTOOL_PATCH = NO
+LIBDAEMON_INSTALL_STAGING = YES
+LIBDAEMON_CONF_ENV = ac_cv_func_setpgrp_void=no
+LIBDAEMON_CONF_OPT = --disable-lynx
 
-LIBDAEMON_DEPENDENCIES:=host-pkg-config
+LIBDAEMON_DEPENDENCIES = host-pkg-config
 
 $(eval $(call AUTOTARGETS,package,libdaemon))
