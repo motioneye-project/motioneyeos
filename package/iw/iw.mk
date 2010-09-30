e50b4dcbf9a5cf9f391ab0ad72880b5e85ae6dec
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-09-30 09:46:13 -0300

    iw: bump version
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/iw/iw.mk
+++ package/iw/iw.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-IW_VERSION = 0.9.20
+IW_VERSION = 0.9.21
 IW_SOURCE = iw-$(IW_VERSION).tar.bz2
 IW_SITE = http://wireless.kernel.org/download/iw
 IW_DEPENDENCIES = host-pkg-config libnl
