55cc1fd5510fbf70ba5433f70c15c02e1c0de1a9
Peter Korsgaard <jacmet@sunsite.dk>
2010-10-07 15:55:08 +0200

    package: add gst-ffmpeg
    ffmpeg plugin for gstreamer.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/multimedia/gst-ffmpeg/gst-ffmpeg.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# gst-ffmpeg
+#
+#############################################################
+GST_FFMPEG_VERSION = 0.10.9
+GST_FFMPEG_SOURCE = gst-ffmpeg-$(GST_FFMPEG_VERSION).tar.bz2
+GST_FFMPEG_SITE = http://gstreamer.freedesktop.org/src/gst-ffmpeg
+GST_FFMPEG_INSTALL_STAGING = YES
+GST_FFMPEG_LIBTOOL_PATCH = NO
+GST_FFMPEG_DEPENDENCIES = gstreamer gst-plugins-base ffmpeg
+GST_FFMPEG_CONF_OPT = --with-system-ffmpeg
+
+ifeq ($(BR2_PACKAGE_BZIP2),y)
+GST_FFMPEG_DEPENDENCIES += bzip2
+endif
+
+$(eval $(call AUTOTARGETS,package/multimedia,gst-ffmpeg))
