6097b191d3bfae34c27c077c369869e4b49ff76f
Yegor Yefremov <yegor_sub1@visionsystems.de>
2010-10-08 12:26:20 +0200

    New package: nuttcp
    Add a new TCP/UDP network testing tool, that provides some advanced
    features compared to ttcp.
    
    [Peter: cleanup, fix FLAGS, move to network]
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/nuttcp/nuttcp.mk
@@ -0,0 +1,25 @@
+#############################################################
+#
+# nuttcp
+#
+#############################################################
+
+NUTTCP_VERSION = 6.1.2
+NUTTCP_SITE = http://www.lcp.nrl.navy.mil/nuttcp/
+NUTTCP_SOURCE = nuttcp-$(NUTTCP_VERSION).tar.bz2
+
+define NUTTCP_BUILD_CMDS
+	$(MAKE1) CC="$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS)" \
+		-C $(@D) all
+endef
+
+define NUTTCP_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/nuttcp-$(NUTTCP_VERSION) \
+		$(TARGET_DIR)/usr/bin/nuttcp
+endef
+
+define NUTTCP_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/bin/nuttcp
+endef
+
+$(eval $(call GENTARGETS,package,nuttcp))
