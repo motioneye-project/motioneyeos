ed720e8825fb028c262bff5bdf9a85eb41df95ae
Martin Banky <martin.banky@gmail.com>
2010-10-08 01:43:02 -0700

    proftpd: convert to autotargets and bump to 1.3.3b
    Also, added web site to Config.in
    
    Signed-off-by: Martin Banky <Martin.Banky@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/proftpd/proftpd.mk
+++ package/proftpd/proftpd.mk
@@ -3,83 +3,42 @@
 # proftpd
 #
 #############################################################
-PROFTPD_VERSION:=1.3.1
-PROFTPD_SOURCE:=proftpd-$(PROFTPD_VERSION).tar.bz2
-PROFTPD_SITE:=ftp://ftp.proftpd.org/distrib/source/
-PROFTPD_DIR:=$(BUILD_DIR)/proftpd-$(PROFTPD_VERSION)
-PROFTPD_CAT:=$(BZCAT)
-PROFTPD_BINARY:=proftpd
-PROFTPD_TARGET_BINARY:=usr/sbin/proftpd
+PROFTPD_VERSION = 1.3.3b
+PROFTPD_SOURCE = proftpd-$(PROFTPD_VERSION).tar.bz2
+PROFTPD_SITE = ftp://ftp.proftpd.org/distrib/source/
 
-ifeq ($(BR2_INET_IPV6),y)
-ENABLE_IPV6:=--enable-ipv6
-endif
+PROFTPD_CONF_ENV = ac_cv_func_setpgrp_void=yes \
+		ac_cv_func_setgrent_void=yes
 
-$(DL_DIR)/$(PROFTPD_SOURCE):
-	 $(call DOWNLOAD,$(PROFTPD_SITE),$(PROFTPD_SOURCE))
-
-proftpd-source: $(DL_DIR)/$(PROFTPD_SOURCE)
-
-$(PROFTPD_DIR)/.unpacked: $(DL_DIR)/$(PROFTPD_SOURCE)
-	$(PROFTPD_CAT) $(DL_DIR)/$(PROFTPD_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	$(CONFIG_UPDATE) $(PROFTPD_DIR)
-	toolchain/patch-kernel.sh $(PROFTPD_DIR) package/proftpd/ proftpd-$(PROFTPD_VERSION)\*.patch;
-	touch $@
-
-$(PROFTPD_DIR)/.configured: $(PROFTPD_DIR)/.unpacked
-	(cd $(PROFTPD_DIR); rm -rf config.cache; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		ac_cv_func_setpgrp_void=yes \
-		ac_cv_func_setgrent_void=yes \
-		./configure $(QUIET) \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--prefix=/usr \
-		--sysconfdir=/etc \
-		--localstatedir=/var/run \
+PROFTPD_CONF_OPT = --localstatedir=/var/run \
 		--disable-static \
 		--disable-curses \
 		--disable-ncurses \
 		--disable-facl \
 		--disable-dso \
 		--enable-shadow \
-		$(DISABLE_LARGEFILE) \
-		$(ENABLE_IPV6) \
-		--with-gnu-ld \
-	)
-	touch $@
+		--with-gnu-ld
 
-$(PROFTPD_DIR)/$(PROFTPD_BINARY): $(PROFTPD_DIR)/.configured
-	$(MAKE1) CC="$(HOSTCC)" CFLAGS="" LDFLAGS="" \
-		-C $(PROFTPD_DIR)/lib/libcap _makenames
-	$(MAKE1) -C $(PROFTPD_DIR)
+define PROFTPD_MAKENAMES
+	$(MAKE1) CC="$(HOSTCC)" CFLAGS="" LDFLAGS="" -C $(@D)/lib/libcap _makenames
+endef
 
-$(TARGET_DIR)/$(PROFTPD_TARGET_BINARY): $(PROFTPD_DIR)/$(PROFTPD_BINARY)
-	cp -dpf $(PROFTPD_DIR)/$(PROFTPD_BINARY) \
-		$(TARGET_DIR)/$(PROFTPD_TARGET_BINARY)
+PROFTPD_POST_CONFIGURE_HOOKS = PROFTPD_MAKENAMES
+
+PROFTPD_MAKE=$(MAKE1)
+
+define PROFTPD_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/proftpd $(TARGET_DIR)/usr/sbin/proftpd
 	@if [ ! -f $(TARGET_DIR)/etc/proftpd.conf ]; then \
-		$(INSTALL) -m 0644 -D $(PROFTPD_DIR)/sample-configurations/basic.conf $(TARGET_DIR)/etc/proftpd.conf; \
+		$(INSTALL) -m 0644 -D $(@D)/sample-configurations/basic.conf $(TARGET_DIR)/etc/proftpd.conf; \
 	fi
 	$(INSTALL) -m 0755 package/proftpd/S50proftpd $(TARGET_DIR)/etc/init.d
+endef
 
-proftpd: $(TARGET_DIR)/$(PROFTPD_TARGET_BINARY)
-
-proftpd-clean:
+define PROFTPD_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/$(PROFTPD_TARGET_BINARY)
 	rm -f $(TARGET_DIR)/etc/init.d/S50proftpd
 	rm -f $(TARGET_DIR)/etc/proftpd.conf
-	-$(MAKE) -C $(PROFTPD_DIR) clean
+endef
 
-proftpd-dirclean:
-	rm -rf $(PROFTPD_DIR)
-
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_PROFTPD),y)
-TARGETS+=proftpd
-endif
+$(eval $(call AUTOTARGETS,package,proftpd))
