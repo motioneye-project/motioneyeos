ab6925eabfa553732a3e7723b8d3d4c2af2806e2
Peter Korsgaard <jacmet@sunsite.dk>
2010-10-14 20:46:49 +0200

    xapp_xinput: needs libXi
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xinput/xapp_xinput.mk
+++ package/x11r7/xapp_xinput/xapp_xinput.mk
@@ -8,6 +8,6 @@ XAPP_XINPUT_VERSION = 1.5.0
 XAPP_XINPUT_SOURCE = xinput-$(XAPP_XINPUT_VERSION).tar.bz2
 XAPP_XINPUT_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XINPUT_AUTORECONF = NO
-XAPP_XINPUT_DEPENDENCIES = xlib_libX11
+XAPP_XINPUT_DEPENDENCIES = xlib_libX11 xlib_libXi
 
 $(eval $(call AUTOTARGETS,package/x11r7,xapp_xinput))
