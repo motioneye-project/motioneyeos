e09f7b8623daa58cac6a89a69051effae461d609
Peter Korsgaard <jacmet@sunsite.dk>
2010-10-17 20:53:49 +0200

    torsmo: use correct variable for HELP2MAN preset
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/torsmo/torsmo.mk
+++ package/torsmo/torsmo.mk
@@ -11,7 +11,7 @@ TORSMO_INSTALL_STAGING = NO
 TORSMO_INSTALL_TARGET = YES
 
 # help2man doesn't work when cross compiling
-TORSMO_CONF_ENV = HELP2MAN=''
+TORSMO_CONF_ENV = ac_cv_path_HELP2MAN=''
 TORSMO_CONF_OPT = --x-includes="-I$(STAGING_DIR)/usr/include/X11" --x-libraries="-I$(STAGING_DIR)/usr/lib" --with-x
 
 TORSMO_DEPENDENCIES = xserver_xorg-server
