fe67fb0456f8786374b387da26bd37e5b09f71c8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-11-02 16:30:12 -0300

    ethtool: bump to version 2.6.35
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -4,11 +4,7 @@
 #
 #############################################################
 
-ETHTOOL_VERSION:=6
-ETHTOOL_SOURCE:=ethtool-$(ETHTOOL_VERSION).tar.gz
-ETHTOOL_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/gkernel/
-ETHTOOL_AUTORECONF:=no
-ETHTOOL_INSTALL_STAGING:=NO
-ETHTOOL_INSTALL_TARGET:=YES
+ETHTOOL_VERSION = 2.6.35
+ETHTOOL_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/gkernel/
 
 $(eval $(call AUTOTARGETS,package,ethtool))
