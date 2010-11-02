666bef2d20a35507827284c874e23f43c3745f34
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-11-02 16:35:11 -0300

    argp-standalone: new package
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/argp-standalone/argp-standalone.mk
@@ -0,0 +1,21 @@
+#############################################################
+#
+# argp-standalone
+#
+#############################################################
+
+ARGP_STANDALONE_VERSION = 1.3
+ARGP_STANDALONE_SITE = http://www.lysator.liu.se/~nisse/archive
+ARGP_STANDALONE_INSTALL_STAGING = YES
+
+define ARGP_STANDALONE_INSTALL_STAGING_CMDS
+	$(INSTALL) -D $(@D)/libargp.a $(STAGING_DIR)/usr/lib/libargp.a
+	$(INSTALL) -D $(@D)/argp.h $(STAGING_DIR)/usr/include/argp.h
+endef
+
+define ARGP_STANDALONE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/libargp.a $(TARGET_DIR)/usr/lib/libargp.a
+	$(INSTALL) -D $(@D)/argp.h $(TARGET_DIR)/usr/include/argp.h
+endef
+
+$(eval $(call AUTOTARGETS,package,argp-standalone))
