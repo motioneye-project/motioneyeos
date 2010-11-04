504a4ad0bbd4b1cad719a82619f814433cc29822
Lionel Landwerlin <llandwerlin@gmail.com>
2010-11-04 03:50:31 +0100

    sawman: bump to 1.4.10
    Signed-off-by: Lionel Landwerlin <llandwerlin@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sawman/sawman.mk
+++ package/sawman/sawman.mk
@@ -3,7 +3,7 @@
 # SAWMAN
 #
 #############################################################
-SAWMAN_VERSION:=1.4.3
+SAWMAN_VERSION:=1.4.10
 SAWMAN_SOURCE:=SaWMan-$(SAWMAN_VERSION).tar.gz
 SAWMAN_SITE:=http://www.directfb.org/downloads/Extras
 SAWMAN_INSTALL_STAGING = YES
