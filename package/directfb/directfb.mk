9f049e1c1f529addfeb4f4d7356e1718ffc5b322
Lionel Landwerlin <llandwerlin@gmail.com>
2010-11-04 03:50:30 +0100

    directfb: bump to 1.4.10
    Signed-off-by: Lionel Landwerlin <llandwerlin@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/directfb/directfb.mk
+++ package/directfb/directfb.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 DIRECTFB_VERSION_MAJOR:=1.4
-DIRECTFB_VERSION:=1.4.5
+DIRECTFB_VERSION:=$(DIRECTFB_VERSION_MAJOR).10
 DIRECTFB_SITE:=http://www.directfb.org/downloads/Core/DirectFB-$(DIRECTFB_VERSION_MAJOR)
 DIRECTFB_SOURCE:=DirectFB-$(DIRECTFB_VERSION).tar.gz
 DIRECTFB_AUTORECONF = NO
