38a611c269f90c3c9dc392d6738c63085123b49c
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-11-04 16:09:32 -0300

    ruby: bump to version 1.9.2-p0 to fix openssl issues, fix dependency issues too
    [Peter: autoreconf needed for host as well]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ruby/ruby.mk
+++ package/ruby/ruby.mk
@@ -3,14 +3,31 @@
 # ruby
 #
 #############################################################
-RUBY_VERSION:=1.9.1-p129
-RUBY_SOURCE:=ruby-$(RUBY_VERSION).tar.gz
-RUBY_SITE:=ftp://ftp.ruby-lang.org/pub/ruby/1.9
-RUBY_AUTORECONF=YES
-RUBY_DEPENDENCIES=host-ruby
-RUBY_MAKE_ENV=$(TARGET_MAKE_ENV)
 
+RUBY_VERSION = 1.9.2-p0
+RUBY_SITE = ftp://ftp.ruby-lang.org/pub/ruby/1.9
+RUBY_AUTORECONF = YES
+HOST_RUBY_AUTORECONF = YES
+RUBY_DEPENDENCIES = host-ruby
+RUBY_MAKE_ENV = $(TARGET_MAKE_ENV)
 RUBY_CONF_OPT = --disable-install-doc
+
+# Force optionals to build before we do
+ifeq ($(BR2_PACKAGE_BERKELEYDB),y)
+	RUBY_DEPENDENCIES += berkeleydb
+endif
+ifeq ($(BR2_PACKAGE_NCURSES),y)
+	RUBY_DEPENDENCIES += ncurses
+endif
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+	RUBY_DEPENDENCIES += openssl
+endif
+ifeq ($(BR2_PACKAGE_READLINE),y)
+	RUBY_DEPENDENCIES += readline
+endif
+ifeq ($(BR2_PACKAGE_ZLIB),y)
+	RUBY_DEPENDENCIES += zlib
+endif
 
 $(eval $(call AUTOTARGETS,package,ruby))
 $(eval $(call AUTOTARGETS,package,ruby,host))
