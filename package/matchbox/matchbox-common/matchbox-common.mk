c2d6095f9ffb40d73ef0549412092447b7e7d1cc
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2010-11-04 10:57:27 +0200

    matchbox-common was missing in Config.in
    While at it add option to install PDA style desktop folders.
    
    Signed-off-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/matchbox/matchbox-common/matchbox-common.mk
+++ package/matchbox/matchbox-common/matchbox-common.mk
@@ -9,6 +9,10 @@ MATCHBOX_COMMON_SOURCE = matchbox-common-$(MATCHBOX_COMMON_VERSION).tar.bz2
 MATCHBOX_COMMON_SITE = http://matchbox-project.org/sources/matchbox-common/$(MATCHBOX_COMMON_VERSION)
 MATCHBOX_COMMON_DEPENDENCIES = matchbox-lib
 
+ifeq ($(strip $(BR2_PACKAGE_MATCHBOX_COMMON_PDA)),y)
+	MATCHBOX_COMMON_CONF_OPT += --enable-pda-folders
+endif
+
 #############################################################
 
 $(eval $(call AUTOTARGETS,package/matchbox,matchbox-common))
