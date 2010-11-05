9cadb9aca37e3a295aee2721cafcb8c8b0b074c0
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-11-05 10:49:07 -0300

    openssh: bump to version 5.6p1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -3,12 +3,12 @@
 # openssh
 #
 #############################################################
-OPENSSH_VERSION=5.1p1
-OPENSSH_SITE=ftp://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 
+OPENSSH_VERSION = 5.6p1
+OPENSSH_SITE = ftp://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 OPENSSH_CONF_ENV = LD="$(TARGET_CC)"
 OPENSSH_CONF_OPT = --libexecdir=/usr/lib --disable-lastlog --disable-utmp \
-		--disable-utmpx --disable-wtmp --disable-wtmpx --without-x
+		--disable-utmpx --disable-wtmp --disable-wtmpx --disable-strip
 
 OPENSSH_DEPENDENCIES = zlib openssl
 
