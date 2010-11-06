94f2063773fb23431cce33ed19052c5b4eae6564
Peter Korsgaard <jacmet@sunsite.dk>
2010-11-06 13:48:39 +0100

    tcpdump: bump version
    The old version didn't build with the updated libpcap anymore.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tcpdump/tcpdump.mk
+++ package/tcpdump/tcpdump.mk
@@ -6,7 +6,7 @@
 # Copyright (C) 2001-2003 by Erik Andersen <andersen@codepoet.org>
 # Copyright (C) 2002 by Tim Riker <Tim@Rikers.org>
 
-TCPDUMP_VERSION:=4.0.0
+TCPDUMP_VERSION:=4.1.1
 TCPDUMP_SITE:=http://www.tcpdump.org/release
 TCPDUMP_SOURCE:=tcpdump-$(TCPDUMP_VERSION).tar.gz
 TCPDUMP_CONF_ENV:=ac_cv_linux_vers=$(firstword $(subst .,$(space),$(firstword $(call qstrip,$(BR2_DEFAULT_KERNEL_HEADERS)))))
