e5edc7338175f8cbbf2d90c5d6be7ccc39bd94e2
Stanislav Bogatyrev <realloc@gmail.com>
2010-11-15 16:23:30 -0300

    pciutils: compilation failed with zlib enabled because it lacks a dependency
    Closes #2797
    
    Signed-off-by: Stanislav Bogatyrev <realloc@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/pciutils/pciutils.mk
+++ package/pciutils/pciutils.mk
@@ -8,6 +8,7 @@ PCIUTILS_VERSION = 3.1.7
 PCIUTILS_SITE = ftp://atrey.karlin.mff.cuni.cz/pub/linux/pci
 ifeq ($(BR2_PACKAGE_ZLIB),y)
 	PCIUTILS_ZLIB=yes
+	PCIUTILS_DEPENDENCIES += zlib
 else
 	PCIUTILS_ZLIB=no
 endif
