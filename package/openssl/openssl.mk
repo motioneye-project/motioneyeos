9df0952493bdea3f28765800055c266ccf7db166
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-11-17 12:43:09 -0300

    openssl: security bump to version 1.0.0b
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OPENSSL_VERSION = 1.0.0a
+OPENSSL_VERSION = 1.0.0b
 OPENSSL_SITE = http://www.openssl.org/source
 OPENSSL_INSTALL_STAGING = YES
 OPENSSL_DEPENDENCIES = zlib
