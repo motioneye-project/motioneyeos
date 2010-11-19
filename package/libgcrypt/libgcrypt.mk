10af218da8c8b00164dd92db12f862f7846a8ea9
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-11-19 10:56:51 -0300

    libgcrypt: bump to version 1.4.6 to fix MIPS build issues
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libgcrypt/libgcrypt.mk
+++ package/libgcrypt/libgcrypt.mk
@@ -3,11 +3,11 @@
 # libgcrypt
 #
 #############################################################
-LIBGCRYPT_VERSION:=1.4.5
-LIBGCRYPT_SOURCE:=libgcrypt-$(LIBGCRYPT_VERSION).tar.bz2
-LIBGCRYPT_SITE:=ftp://ftp.gnupg.org/gcrypt/libgcrypt
+
+LIBGCRYPT_VERSION = 1.4.6
+LIBGCRYPT_SOURCE = libgcrypt-$(LIBGCRYPT_VERSION).tar.bz2
+LIBGCRYPT_SITE = ftp://ftp.gnupg.org/gcrypt/libgcrypt
 LIBGCRYPT_INSTALL_STAGING = YES
-LIBGCRYPT_INSTALL_TARGET = YES
 
 LIBGCRYPT_CONF_ENV = \
 	ac_cv_sys_symbol_underscore=no
