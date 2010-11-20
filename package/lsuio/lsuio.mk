95f17bf78f94a69858ebe9a7a64a5b9cce76eedb
Mike Frysinger <vapier@gentoo.org>
2010-11-20 01:52:43 -0500

    lsuio: new UIO helper package
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/lsuio/lsuio.mk
@@ -0,0 +1,11 @@
+#############################################################
+#
+# lsuio
+#
+#############################################################
+
+LSUIO_VERSION = 0.2.0
+LSUIO_SOURCE = lsuio-$(LSUIO_VERSION).tar.gz
+LSUIO_SITE = http://www.osadl.org/projects/downloads/UIO/user
+
+$(eval $(call AUTOTARGETS,package,lsuio))
