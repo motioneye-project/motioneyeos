f53c46ffb27ea2a687def2fa40be942714b4e851
Luca Ceresoli <luca@lucaceresoli.net>
2010-11-22 23:16:18 +0100

    qt: bump to 4.7.1
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/qt/qt.mk
+++ package/qt/qt.mk
@@ -12,7 +12,7 @@
 #
 ######################################################################
 
-QT_VERSION:=4.7.0
+QT_VERSION:=4.7.1
 QT_SOURCE:=qt-everywhere-opensource-src-$(QT_VERSION).tar.gz
 QT_SITE:=http://get.qt.nokia.com/qt/source
 QT_CAT:=$(ZCAT)
