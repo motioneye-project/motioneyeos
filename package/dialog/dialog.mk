b9c5809a0bd697612da9185df03a17a0f699eb73
Peter Korsgaard <jacmet@sunsite.dk>
2010-11-25 11:07:08 +0100

    dialog: fix build, ensure host includes aren't used
    configure looks for libintl, so ensure it is available. Also make sure
    the host ncurses headers aren't used.
    Fix up Config.in indentation while we're at it.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/dialog/dialog.mk
+++ package/dialog/dialog.mk
@@ -7,7 +7,12 @@ DIALOG_VERSION:=1.1-20100428
 DIALOG_SOURCE:=dialog-$(DIALOG_VERSION).tgz
 DIALOG_SITE:=ftp://invisible-island.net/dialog
 DIALOG_CONF_OPT = --with-ncurses
+DIALOG_CONF_ENV = ac_cv_path_NCURSES_CONFIG=true LIBS=-lncurses
 DIALOG_DEPENDENCIES = ncurses
+
+ifneq ($(BR2_ENABLE_LOCALE),y)
+DIALOG_DEPENDENCIES+=libiconv
+endif
 
 define DIALOG_INSTALL_TARGET_CMDS
 	install -c $(@D)/dialog $(TARGET_DIR)/usr/bin/dialog
