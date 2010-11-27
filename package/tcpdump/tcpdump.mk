3783d4a30140ae68e016677c125405cf77142e38
Mike Frysinger <vapier@gentoo.org>
2010-11-27 18:55:20 -0500

    tcpdump: punt duplicate versioned binary
    The tcpdump package likes to install two binaries into /usr/sbin --
    one named "tcpdump" and one named "tcpdump.<ver>".  But it does this
    by installing the file twice rather than using links.  Since the alt
    name is useless to us, just punt it.
    
    [Peter: Rework to use POST_INSTALL_TARGET_HOOKS instead]
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tcpdump/tcpdump.mk
+++ package/tcpdump/tcpdump.mk
@@ -14,4 +14,11 @@ TCPDUMP_CONF_OPT:=--without-crypto \
 		$(if $(BR2_PACKAGE_TCPDUMP_SMB),--enable-smb,--disable-smb)
 TCPDUMP_DEPENDENCIES:=zlib libpcap
 
+# make install installs an unneeded extra copy of the tcpdump binary
+define TCPDUMP_REMOVE_DUPLICATED_BINARY
+	rm -f $(TARGET_DIR)/usr/sbin/tcpdump.$(TCPDUMP_VERSION)
+endef
+
+TCPDUMP_POST_INSTALL_TARGET_HOOKS += TCPDUMP_REMOVE_DUPLICATED_BINARY
+
 $(eval $(call AUTOTARGETS,package,tcpdump))
