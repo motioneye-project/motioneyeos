b98e4d001cb8073fa8aefd2193fd70df0274d05e
H Hartley Sweeten <hartleys@visionengravers.com>
2010-11-29 18:22:20 -0700

    package: Add libsigc++ package
    Add support for the libsigc++ library.
    
    [Peter: Rename dir, use libtool patch, add C++ dep]
    Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libsigc/libsigc.mk
@@ -0,0 +1,11 @@
+#############################################################
+#
+# libsigc++
+#
+#############################################################
+LIBSIGC_VERSION = 2.2.8
+LIBSIGC_SOURCE = libsigc++-$(LIBSIGC_VERSION).tar.bz2
+LIBSIGC_SITE = http://ftp.gnome.org/pub/GNOME/sources/libsigc++/2.2
+LIBSIGC_INSTALL_STAGING = YES
+
+$(eval $(call AUTOTARGETS,package,libsigc))
