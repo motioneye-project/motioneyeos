56a2e82c40947bb4eab2a129bfe184df83fd2807
Will Newton <will.newton@gmail.com>
2010-12-01 14:44:26 +0000

    package/openssh: Use HTTP instead of FTP for source download
    HTTP should be more firewall friendly.
    
    Signed-off-by: Will Newton <will.newton@imgtec.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -5,7 +5,7 @@
 #############################################################
 
 OPENSSH_VERSION = 5.6p1
-OPENSSH_SITE = ftp://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
+OPENSSH_SITE = http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 OPENSSH_CONF_ENV = LD="$(TARGET_CC)"
 OPENSSH_CONF_OPT = --libexecdir=/usr/lib --disable-lastlog --disable-utmp \
 		--disable-utmpx --disable-wtmp --disable-wtmpx --disable-strip
