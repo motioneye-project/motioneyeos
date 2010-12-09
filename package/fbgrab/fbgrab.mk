f6cdaf5982097e3e2504c3e8209a2cc510a06576
Daniel Nyström <daniel.nystrom@timeterminal.se>
2010-12-09 16:28:29 +0100

    New package: fbgrab
    FBGrab is a framebuffer screenshot program, capturing the linux
    frambuffer and converting it to a png-picture.
    
    [Peter: drop zlib dep, add uninstall]
    Signed-off-by: Daniel Nyström <daniel.nystrom@timeterminal.se>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/fbgrab/fbgrab.mk
@@ -0,0 +1,18 @@
+FBGRAB_VERSION = 1.0
+FBGRAB_SOURCE = fbgrab-$(FBGRAB_VERSION).tar.gz
+FBGRAB_SITE = http://hem.bredband.net/gmogmo/fbgrab
+FBGRAB_DEPENDENCIES = libpng
+
+define FBGRAB_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
+endef
+
+define FBGRAB_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/fbgrab $(TARGET_DIR)/usr/bin/fbgrab
+endef
+
+define FBGRAB_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/bin/fbgrab
+endef
+
+$(eval $(call GENTARGETS,package,fbgrab))
