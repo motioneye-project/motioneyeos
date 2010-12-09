41a1a92095d2addc55f7050a67ce18d98781fa36
Lionel Landwerlin <llandwerlin@gmail.com>
2010-12-09 10:55:03 +0100

    i2c-tools: ensure binaries are installed in the right place
    And remove unneeded explicit stripping of target binaries
    
    [Peter: use install -D]
    Signed-off-by: Lionel Landwerlin <llandwerlin@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/i2c-tools/i2c-tools.mk
+++ package/i2c-tools/i2c-tools.mk
@@ -14,8 +14,7 @@ endef
 define I2C_TOOLS_INSTALL_TARGET_CMDS
 	for i in i2cdump i2cget i2cset i2cdetect; \
 	do \
-		$(INSTALL) -m 755 $(@D)/tools/$$i $(TARGET_DIR)/usr/bin/$$i; \
-		$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/bin/$$i; \
+		$(INSTALL) -m 755 -D $(@D)/tools/$$i $(TARGET_DIR)/usr/bin/$$i; \
 	done
 endef
 
