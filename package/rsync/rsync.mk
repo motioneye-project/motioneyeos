8c874a334edb4d14b73a3964e90b20e78729fc93
Lionel Landwerlin <llandwerlin@gmail.com>
2010-12-09 10:55:09 +0100

    rsync: avoid using non reevaluated $(TARGET_DIR) in RSYNC_INSTALL_TARGET_OPT
    Signed-off-by: Lionel Landwerlin <llandwerlin@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/rsync/rsync.mk
+++ package/rsync/rsync.mk
@@ -4,19 +4,19 @@
 #
 #############################################################
 
-RSYNC_VERSION:=3.0.7
-RSYNC_SOURCE:=rsync-$(RSYNC_VERSION).tar.gz
-RSYNC_SITE:=http://rsync.samba.org/ftp/rsync/src
-RSYNC_INSTALL_STAGING:=NO
-RSYNC_INSTALL_TARGET:=YES
-RSYNC_CONF_OPT=$(if $(BR2_ENABLE_DEBUG),--enable-debug,--disable-debug)
+RSYNC_VERSION := 3.0.7
+RSYNC_SOURCE := rsync-$(RSYNC_VERSION).tar.gz
+RSYNC_SITE := http://rsync.samba.org/ftp/rsync/src
+RSYNC_INSTALL_STAGING = NO
+RSYNC_INSTALL_TARGET = YES
+RSYNC_CONF_OPT = $(if $(BR2_ENABLE_DEBUG),--enable-debug,--disable-debug)
 ifeq ($(BR2_ENABLE_DEBUG),y)
-RSYNC_INSTALL_TARGET_OPT:=DESTDIR=$(TARGET_DIR) INSTALLCMD='./install-sh -c' \
+RSYNC_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) INSTALLCMD='./install-sh -c' \
 			  install
 else
-RSYNC_INSTALL_TARGET_OPT:=DESTDIR=$(TARGET_DIR) INSTALLCMD='./install-sh -c' \
+RSYNC_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) INSTALLCMD='./install-sh -c' \
 			  STRIPPROG="$(TARGET_STRIP)" install-strip
 endif
-RSYNC_CONF_OPT:=--with-included-popt
+RSYNC_CONF_OPT = --with-included-popt
 
 $(eval $(call AUTOTARGETS,package,rsync))
