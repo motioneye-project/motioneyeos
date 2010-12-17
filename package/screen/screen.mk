748eb04a40d0dd5ef3b65448e0e732648c1ac019
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-12-17 13:30:56 -0300

    screen: bump to version 4.0.3
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/screen/screen.mk
+++ package/screen/screen.mk
@@ -3,15 +3,12 @@
 # screen
 #
 #############################################################
-SCREEN_VERSION = 4.0.2
+
+SCREEN_VERSION = 4.0.3
 SCREEN_SITE = $(BR2_GNU_MIRROR)/screen
-
 SCREEN_DEPENDENCIES = ncurses
-
 SCREEN_CONF_ENV = CFLAGS=-DSYSV=1
-
 SCREEN_MAKE_OPT = -j1
-
 SCREEN_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) SCREEN=screen install_bin
 
 $(eval $(call AUTOTARGETS,package,screen))
