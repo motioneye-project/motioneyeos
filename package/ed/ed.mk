8960ecf5a2bef97eeac1ade21cc3987dfd22a461
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-12-17 13:41:05 -0300

    ed: bump to version 1.5
    * Bump to version 1.5
    
    * Really configure right (or wrong depending on perspective) since we
      weren't building ed for the target it was completely broken.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ed/ed.mk
+++ package/ed/ed.mk
@@ -3,8 +3,10 @@
 # ed
 #
 #############################################################
-ED_VERSION:=1.1
-ED_SOURCE:=ed-$(ED_VERSION).tar.bz2
-ED_SITE:=$(BR2_GNU_MIRROR)/ed/
+
+ED_VERSION = 1.5
+ED_SITE = $(BR2_GNU_MIRROR)/ed
+ED_CONF_OPT = CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" \
+		LDFLAGS="$(TARGET_LDFLAGS)"
 
 $(eval $(call AUTOTARGETS,package,ed))
