4860730019db598b8e4b6f2f03cef0677ab36fef
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-12-17 13:00:58 -0300

    tar: bump to version 1.25
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tar/tar.mk
+++ package/tar/tar.mk
@@ -3,8 +3,8 @@
 # tar
 #
 #############################################################
-TAR_VERSION:=1.21
-TAR_SOURCE:=tar-$(TAR_VERSION).tar.bz2
-TAR_SITE:=$(BR2_GNU_MIRROR)/tar/
+
+TAR_VERSION = 1.25
+TAR_SITE = $(BR2_GNU_MIRROR)/tar
 
 $(eval $(call AUTOTARGETS,package,tar))
