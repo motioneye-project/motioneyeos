2a92cee21a24daa0ef6e49a08c8c298fcee91c63
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-12-17 13:21:56 -0300

    bison: bump to version 2.4.3
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/bison/bison.mk
+++ package/bison/bison.mk
@@ -3,9 +3,9 @@
 # bison
 #
 #############################################################
-BISON_VERSION:=2.3
-BISON_SOURCE:=bison-$(BISON_VERSION).tar.bz2
-BISON_SITE:=$(BR2_GNU_MIRROR)/bison
+
+BISON_VERSION = 2.4.3
+BISON_SITE = $(BR2_GNU_MIRROR)/bison
 
 define BISON_DISABLE_EXAMPLES
 	echo 'all install:' > $(@D)/examples/Makefile
