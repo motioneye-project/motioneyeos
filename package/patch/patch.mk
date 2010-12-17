b236dcb618d8fd0422d79c4682837a5f9bd59d2b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-12-17 12:29:09 -0300

    patch: needs toolchain with wchar
    * Patch needs a toolchain with WCHAR support
    * Use real upstream gnu mirror
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/patch/patch.mk
+++ package/patch/patch.mk
@@ -3,8 +3,8 @@
 # patch
 #
 #############################################################
-PATCH_VERSION:=2.6
-PATCH_SOURCE:=patch_$(PATCH_VERSION).orig.tar.gz
-PATCH_SITE:=$(BR2_DEBIAN_MIRROR)/debian/pool/main/p/patch
+
+PATCH_VERSION = 2.6
+PATCH_SITE = $(BR2_GNU_MIRROR)/patch
 
 $(eval $(call AUTOTARGETS,package,patch))
