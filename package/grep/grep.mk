0b584467c193234250819ee52d806f8cec13c1f0
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-12-17 13:24:10 -0300

    grep: bump to version 2.7
    [Peter: needs wchar]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/grep/grep.mk
+++ package/grep/grep.mk
@@ -3,11 +3,10 @@
 # grep
 #
 #############################################################
-GREP_VERSION:=2.5.3
-GREP_SOURCE:=grep-$(GREP_VERSION).tar.bz2
-GREP_SITE:=$(BR2_GNU_MIRROR)/grep
-GREP_CONF_OPT = --disable-perl-regexp --without-included-regex
 
+GREP_VERSION = 2.7
+GREP_SITE = $(BR2_GNU_MIRROR)/grep
+GREP_CONF_OPT = --disable-perl-regexp --without-included-regex
 GREP_DEPENDENCIES = $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext libintl)
 
 $(eval $(call AUTOTARGETS,package,grep))
