36f33bc00f95b27702e07bacac8047f0d1882b62
Daniel Nyström <daniel.nystrom@timeterminal.se>
2010-12-21 19:06:55 +0100

    tslib: bump version to current git head
    Due to missing official releases, fetch directly from GitHub
    repository. Many bugfixes since 1.0 and all previous patches in
    Buildroot are either applied or obsoleted.
    
    Signed-off-by: Daniel Nyström <daniel.nystrom@timeterminal.se>
    Acked-by: Paulius Zaleckas <paulius.zaleckas@gmail.com>
    Acked-by: Luca Ceresoli <luca@lucaceresoli.net>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tslib/tslib.mk
+++ package/tslib/tslib.mk
@@ -3,11 +3,9 @@
 # tslib
 #
 #############################################################
-TSLIB_VERSION:=1.0
-TSLIB_SOURCE:=tslib-$(TSLIB_VERSION).tar.bz2
-TSLIB_SITE:=http://download.berlios.de/tslib
+TSLIB_VERSION:=860d69ca
+TSLIB_SITE:=git://github.com/kergoth/tslib.git
 TSLIB_AUTORECONF = YES
-TSLIB_LIBTOOL_PATCH = YES
 TSLIB_INSTALL_STAGING = YES
 TSLIB_INSTALL_TARGET = YES
 TSLIB_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) LDFLAGS=-L$(STAGING_DIR)/usr/lib install
