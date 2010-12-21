9e9213f11219e588f5759955c5731bfa0f1ed172
Gustavo Zacarias <gustavo@zacarias.com.ar>
2010-12-21 11:39:28 -0300

    libxml2: bump to version 2.7.8
    Bump libxml2 to version 2.7.8.
    Re-enable libtool patch now that we handle libtool2 in package infra.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libxml2/libxml2.mk
+++ package/libxml2/libxml2.mk
@@ -3,13 +3,10 @@
 # libxml2
 #
 #############################################################
-LIBXML2_VERSION = 2.7.7
-LIBXML2_SOURCE = libxml2-sources-$(LIBXML2_VERSION).tar.gz
+
+LIBXML2_VERSION = 2.7.8
 LIBXML2_SITE = ftp://xmlsoft.org/libxml2
 LIBXML2_INSTALL_STAGING = YES
-LIBXML2_INSTALL_TARGET = YES
-#this is needed for version 2.7.4 and higher
-LIBXML2_LIBTOOL_PATCH = NO
 
 ifneq ($(BR2_LARGEFILE),y)
 LIBXML2_CONF_ENV = CC="$(TARGET_CC) $(TARGET_CFLAGS) -DNO_LARGEFILE_SOURCE"
