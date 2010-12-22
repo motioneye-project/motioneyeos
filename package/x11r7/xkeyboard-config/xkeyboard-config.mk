6ddf4702647e46a09c15bedbe99017eaa10573e3
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2010-12-22 10:35:04 +0100

    xkeyboard-config: bump to 2.0 to fix bug 2965
    Closes #2965
    
    For some odd reason, xkeyboard-config < 1.8 was creating a symbolic
    link from /usr/share/X11/xkb/xkbcomp to the xkbcomp binary. But in
    cross-compilation mode, this is absurd as the xkbcomp binary to which
    the link is pointing is the one in $(HOST_DIR).
    
    This symbolic link thing has been removed completely starting from
    xkeyboard-config 1.9. See
    http://cgit.freedesktop.org/xkeyboard-config/commit/?id=f413dff57e77e7b01461508f74b4e92d1dc8defd.
    
    Therefore, we simply bump xkeyboard-config to the latest available
    version, 2.0.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xkeyboard-config/xkeyboard-config.mk
+++ package/x11r7/xkeyboard-config/xkeyboard-config.mk
@@ -3,9 +3,9 @@
 # xkeyboard-config
 #
 #############################################################
-XKEYBOARD_CONFIG_VERSION = 1.8
+XKEYBOARD_CONFIG_VERSION = 2.0
 XKEYBOARD_CONFIG_SOURCE = xkeyboard-config-$(XKEYBOARD_CONFIG_VERSION).tar.bz2
-XKEYBOARD_CONFIG_SITE = http://xlibs.freedesktop.org/xkbdesc
+XKEYBOARD_CONFIG_SITE = http://www.x.org/releases/individual/data/xkeyboard-config/
 XKEYBOARD_CONFIG_AUTORECONF = NO
 XKEYBOARD_CONFIG_INSTALL_STAGING = NO
 XKEYBOARD_CONFIG_INSTALL_TARGET = YES
