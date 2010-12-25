28f77123de67834dee88d2c092c0f4b0770d6af2
Peter Korsgaard <jacmet@sunsite.dk>
2010-12-25 22:33:20 +0100

    grep: fix build when libiconv is enabled
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/grep/grep.mk
+++ package/grep/grep.mk
@@ -9,4 +9,10 @@ GREP_SITE = $(BR2_GNU_MIRROR)/grep
 GREP_CONF_OPT = --disable-perl-regexp --without-included-regex
 GREP_DEPENDENCIES = $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext libintl)
 
+# link with iconv if enabled
+ifeq ($(BR2_PACKAGE_LIBICONV),y)
+GREP_CONF_ENV += LIBS=-liconv
+GREP_DEPENDENCIES += libiconv
+endif
+
 $(eval $(call AUTOTARGETS,package,grep))
