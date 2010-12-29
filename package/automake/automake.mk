38826858c03db1ac09723ef5948eba438a349e09
Peter Korsgaard <jacmet@sunsite.dk>
2010-12-29 09:14:15 +0100

    automake: ensure staging aclocal dir gets created
    Otherwise aclocal fails.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/automake/automake.mk
+++ package/automake/automake.mk
@@ -15,7 +15,13 @@ define GTK_DOC_M4_INSTALL
  $(INSTALL) -D -m 0644 package/automake/gtk-doc.m4 $(HOST_DIR)/usr/share/aclocal/gtk-doc.m4
 endef
 
+# ensure staging aclocal dir exists
+define HOST_AUTOMAKE_MAKE_ACLOCAL
+	mkdir -p $(ACLOCAL_DIR)
+endef
+
 HOST_AUTOMAKE_POST_INSTALL_HOOKS += GTK_DOC_M4_INSTALL
+HOST_AUTOMAKE_POST_INSTALL_HOOKS += HOST_AUTOMAKE_MAKE_ACLOCAL
 
 $(eval $(call AUTOTARGETS,package,automake))
 $(eval $(call AUTOTARGETS,package,automake,host))
