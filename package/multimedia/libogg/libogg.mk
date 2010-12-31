582c0a7803119691537d2e167ffca622679f0cdb
Peter Korsgaard <jacmet@sunsite.dk>
2010-12-31 11:29:34 +0100

    libogg: bump version
    Derived from patch by Martin Banky.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/libogg/libogg.mk
+++ package/multimedia/libogg/libogg.mk
@@ -3,7 +3,7 @@
 # libogg
 #
 #############################################################
-LIBOGG_VERSION = 1.1.4
+LIBOGG_VERSION = 1.2.2
 LIBOGG_SOURCE = libogg-$(LIBOGG_VERSION).tar.gz
 LIBOGG_SITE = http://downloads.xiph.org/releases/ogg
 LIBOGG_AUTORECONF = NO
