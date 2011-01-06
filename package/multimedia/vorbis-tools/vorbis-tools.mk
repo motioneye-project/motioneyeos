d0f607c47ad5a90bede8f2db314d9f4355e97877
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-01-06 15:42:01 -0300

    vorbis-tools: new package
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/multimedia/vorbis-tools/vorbis-tools.mk
@@ -0,0 +1,20 @@
+#############################################################
+#
+# vorbis-tools
+#
+#############################################################
+
+VORBIS_TOOLS_VERSION = 1.4.0
+VORBIS_TOOLS_SITE = http://downloads.xiph.org/releases/vorbis
+VORBIS_TOOLS_DEPENDENCIES = libao libogg libvorbis libcurl
+VORBIS_TOOLS_CONF_OPT = --program-transform-name=''
+
+ifeq ($(BR2_PACKAGE_FLAC),y)
+VORBIS_TOOLS_DEPENDENCIES += flac
+endif
+
+ifeq ($(BR2_PACKAGE_SPEEX),y)
+VORBIS_TOOLS_DEPENDENCIES += speex
+endif
+
+$(eval $(call AUTOTARGETS,package/multimedia,vorbis-tools))
