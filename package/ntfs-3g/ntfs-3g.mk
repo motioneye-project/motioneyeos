f6cc067d961cfd7c326bccabedca8d46ea807d5d
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-01-07 15:20:40 -0300

    ntfs-3g: bump to version 2010.10.2
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ntfs-3g/ntfs-3g.mk
+++ package/ntfs-3g/ntfs-3g.mk
@@ -3,10 +3,11 @@
 # ntfs-3g
 #
 #############################################################
-NTFS_3G_VERSION:=2009.3.8
-NTFS_3G_SOURCE:=ntfs-3g-$(NTFS_3G_VERSION).tgz
-NTFS_3G_SITE:=http://www.ntfs-3g.org/
-NTFS_3G_CONF_OPT:=--disable-ldconfig --program-prefix=""
-NTFS_3G_INSTALL_STAGING:=yes
+
+NTFS_3G_VERSION = 2010.10.2
+NTFS_3G_SOURCE = ntfs-3g-$(NTFS_3G_VERSION).tgz
+NTFS_3G_SITE = http://tuxera.com/opensource
+NTFS_3G_CONF_OPT = --disable-ldconfig --program-prefix=""
+NTFS_3G_INSTALL_STAGING = YES
 
 $(eval $(call AUTOTARGETS,package,ntfs-3g))
