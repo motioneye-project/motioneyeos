147b81996db73787f578a73fd222d297e1e4ceef
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-01-07 15:05:27 -0300

    psmisc: bump to version 22.13
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/psmisc/psmisc.mk
+++ package/psmisc/psmisc.mk
@@ -3,10 +3,14 @@
 # psmisc
 #
 #############################################################
-PSMISC_VERSION:=22.8
-PSMISC_SOURCE:=psmisc-$(PSMISC_VERSION).tar.gz
-PSMISC_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/psmisc
-PSMISC_AUTORECONF:=NO
-PSMISC_DEPENDENCIES:=ncurses $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext libintl)
+
+PSMISC_VERSION = 22.13
+PSMISC_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/psmisc
+PSMISC_DEPENDENCIES = ncurses $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext libintl)
+
+# build after busybox, we prefer fat versions while we're at it
+ifeq ($(BR2_PACKAGE_BUSYBOX),y)
+PSMISC_DEPENDENCIES += busybox
+endif
 
 $(eval $(call AUTOTARGETS,package,psmisc))
