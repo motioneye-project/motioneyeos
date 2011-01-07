ae2aa63dafca83259c4e113c9934bd5fef715881
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-01-07 15:19:03 -0300

    sshfs: select openssh instead of depending
    Select openssh in Config.in rather than depend on it.
    Otherwise the option is missing without an obvious reason.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sshfs/sshfs.mk
+++ package/sshfs/sshfs.mk
@@ -4,13 +4,9 @@
 #
 #############################################################
 
-SSHFS_VERSION:=2.2
-SSHFS_SOURCE:=sshfs-fuse-$(SSHFS_VERSION).tar.gz
-SSHFS_SITE=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/fuse/$(SSHFS_VERSION)/sshfs-fuse
-SSHFS_AUTORECONF:=NO
-SSHFS_INSTALL_STAGING:=NO
-SSHFS_INSTALL_TARGET:=YES
-
+SSHFS_VERSION = 2.2
+SSHFS_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/fuse/$(SSHFS_VERSION)/sshfs-fuse
+SSHFS_SOURCE = sshfs-fuse-$(SSHFS_VERSION).tar.gz
 SSHFS_DEPENDENCIES = libglib2 libfuse $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext libintl)
 
 $(eval $(call AUTOTARGETS,package,sshfs))
