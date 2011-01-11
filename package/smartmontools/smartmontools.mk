47f8a98c5bdd3713e0ddac66b4154004cf20c9c0
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-01-11 15:32:31 -0300

    smartmontools: bump to version 5.40
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/smartmontools/smartmontools.mk
+++ package/smartmontools/smartmontools.mk
@@ -3,9 +3,8 @@
 # smartmontools
 #
 #############################################################
-SMARTMONTOOLS_VERSION:=5.33
-SMARTMONTOOLS_SOURCE:=smartmontools-$(SMARTMONTOOLS_VERSION).tar.gz
-SMARTMONTOOLS_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/smartmontools
-SMARTMONTOOLS_DIR:=$(BUILD_DIR)/smartmontools-$(SMARTMONTOOLS_VERSION)
+
+SMARTMONTOOLS_VERSION = 5.40
+SMARTMONTOOLS_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/smartmontools
 
 $(eval $(call AUTOTARGETS,package,smartmontools))
