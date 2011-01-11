0eafafbe3dcff1362ee7f0617af6b75e22cd5014
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-01-11 21:45:55 +0100

    python-serial: new package
    python-serial is a Python library to access serial ports.
    
    This package has originally been introduced to test that third-party
    pure Python modules (that do not use C extensions) build properly
    against the Buildroot Python infrastructure.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/python-serial/python-serial.mk
@@ -0,0 +1,21 @@
+#############################################################
+#
+# python-serial
+#
+#############################################################
+
+PYTHON_SERIAL_VERSION = 2.5
+PYTHON_SERIAL_SOURCE  = pyserial-$(PYTHON_SERIAL_VERSION).tar.gz
+PYTHON_SERIAL_SITE    = http://pypi.python.org/packages/source/p/pyserial/
+
+PYTHON_SERIAL_DEPENDENCIES = python
+
+define PYTHON_SERIAL_BUILD_CMDS
+	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py build)
+endef
+
+define PYTHON_SERIAL_INSTALL_TARGET_CMDS
+	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
+endef
+
+$(eval $(call GENTARGETS,package,python-serial))
