7f8164267594c5ce8bed685192729c0fb0fd5056
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-01-11 10:45:30 -0300

    libao: new package
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/multimedia/libao/libao.mk
@@ -0,0 +1,21 @@
+#############################################################
+#
+# libao
+#
+#############################################################
+
+LIBAO_VERSION = 1.0.0
+LIBAO_SITE = http://downloads.xiph.org/releases/ao
+LIBAO_DEPENDENCIES = host-pkg-config
+LIBAO_INSTALL_STAGING = YES
+LIBAO_CONF_OPT = --disable-esd --disable-wmm --disable-arts \
+			--disable-nas --disable-pulse
+
+ifeq ($(BR2_PACKAGE_ALSA_LIB),y)
+LIBAO_DEPENDENCIES += alsa-lib
+LIBAO_CONF_OPT += --enable-alsa --enable-alsa-mmap
+else
+LIBAO_CONF_OPT += --disable-alsa
+endif
+
+$(eval $(call AUTOTARGETS,package/multimedia,libao))
