5118e1cf26641b6dd4dba0dd83be12b1ba56020d
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-01-12 15:21:05 -0300

    gperf: bump to version 3.0.4
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gperf/gperf.mk
+++ package/gperf/gperf.mk
@@ -3,12 +3,9 @@
 # gperf
 #
 #############################################################
-GPERF_VERSION = 3.0.3
-GPERF_SOURCE = gperf-$(GPERF_VERSION).tar.gz
+
+GPERF_VERSION = 3.0.4
 GPERF_SITE = $(BR2_GNU_MIRROR)/gperf
-GPERF_AUTORECONF = NO
-GPERF_INSTALL_STAGING = NO
-GPERF_INSTALL_TARGET = YES
 
 $(eval $(call AUTOTARGETS,package,gperf))
 $(eval $(call AUTOTARGETS,package,gperf,host))
