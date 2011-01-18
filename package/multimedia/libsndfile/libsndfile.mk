8e3d1242d8c6083aa35c7bf95eb099adfb7d3aab
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-01-18 13:54:32 -0300

    libsndfile: bump to version 1.0.23
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/libsndfile/libsndfile.mk
+++ package/multimedia/libsndfile/libsndfile.mk
@@ -3,10 +3,9 @@
 # libsndfile
 #
 #############################################################
-LIBSNDFILE_VERSION:=1.0.18
-LIBSNDFILE_SOURCE:=libsndfile-$(LIBSNDFILE_VERSION).tar.gz
-LIBSNDFILE_SITE:=http://www.mega-nerd.com/libsndfile
-LIBSNDFILE_LIBTOOL_PATCH:=NO
-LIBSNDFILE_INSTALL_STAGING:=YES
+
+LIBSNDFILE_VERSION = 1.0.23
+LIBSNDFILE_SITE = http://www.mega-nerd.com/libsndfile/files
+LIBSNDFILE_INSTALL_STAGING = YES
 
 $(eval $(call AUTOTARGETS,package/multimedia,libsndfile))
