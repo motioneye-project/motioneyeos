5ba84234e831cf8c1c78d5073be5cda474c5f299
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-01-18 13:54:35 -0300

    libsamplerate: new package
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/multimedia/libsamplerate/libsamplerate.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# libsamplerate
+#
+################################################################################
+
+LIBSAMPLERATE_VERSION = 0.1.7
+LIBSAMPLERATE_SITE = http://www.mega-nerd.com/SRC
+LIBSAMPLERATE_INSTALL_STAGING = YES
+LIBSAMPLERATE_DEPENDENCIES = host-pkg-config
+LIBSAMPLERATE_CONF_OPT = --disable-fftw --program-transform-name=''
+
+ifeq ($(BR2_PACKAGE_LIBSNDFILE),y)
+LIBSAMPLERATE_DEPENDENCIES += libsndfile
+endif
+
+$(eval $(call AUTOTARGETS,package/multimedia,libsamplerate))
