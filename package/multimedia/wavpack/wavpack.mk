f873d28f9276d17c8c82bb27147d0070da516dcc
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-01-20 10:32:36 -0300

    wavpack: new package
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/multimedia/wavpack/wavpack.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# wavpack
+#
+################################################################################
+
+WAVPACK_VERSION = 4.60.1
+WAVPACK_SITE = http://www.wavpack.com
+WAVPACK_SOURCE = wavpack-$(WAVPACK_VERSION).tar.bz2
+WAVPACK_INSTALL_STAGING = YES
+
+ifneq ($(BR2_ENABLE_LOCALE),y)
+WAVPACK_DEPENDENCIES += libiconv
+endif
+
+$(eval $(call AUTOTARGETS,package/multimedia,wavpack))
