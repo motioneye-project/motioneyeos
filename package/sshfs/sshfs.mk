eef3d90998441d060785366d27f0080bbfc9e9c6
Peter Korsgaard <jacmet@sunsite.dk>
2011-01-24 21:11:36 +0100

    sshfs: adjust make dependencies to match Kconfig
    Spotted by Thomas, thanks.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sshfs/sshfs.mk
+++ package/sshfs/sshfs.mk
@@ -7,6 +7,9 @@
 SSHFS_VERSION = 2.2
 SSHFS_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/fuse/$(SSHFS_VERSION)/sshfs-fuse
 SSHFS_SOURCE = sshfs-fuse-$(SSHFS_VERSION).tar.gz
-SSHFS_DEPENDENCIES = libglib2 libfuse $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext libintl)
+SSHFS_DEPENDENCIES = \
+	libglib2 libfuse openssh \
+	$(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext libintl) \
+	$(if $(BR2_ENABLE_LOCALE),,libiconv)
 
 $(eval $(call AUTOTARGETS,package,sshfs))
