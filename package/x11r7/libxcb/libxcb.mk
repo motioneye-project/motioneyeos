5f19df9224d6ef826f9d4ce9a06673ada705d743
Peter Korsgaard <jacmet@sunsite.dk>
2011-01-27 23:38:48 +0100

    libxcb: use host-python and fix dependencies
    Now that we have a proper host-python package, use that one instead of
    whatever might be available on the build host. Also don't overwrite
    the host-python package version variable and fix dependency list
    (xcb-proto is needed for the host).
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/libxcb/libxcb.mk
+++ package/x11r7/libxcb/libxcb.mk
@@ -11,12 +11,16 @@ LIBXCB_INSTALL_STAGING = YES
 
 LIBXCB_AUTORECONF = NO
 LIBXCB_LIBTOOL_PATCH = NO
-LIBXCB_DEPENDENCIES = host-libxslt pthread-stubs xcb-proto xlib_libXdmcp xlib_libXau
+LIBXCB_DEPENDENCIES = \
+	host-libxslt pthread-stubs xcb-proto xlib_libXdmcp xlib_libXau \
+	host-xcb-proto host-python
 LIBXCB_CONF_ENV = STAGING_DIR="$(STAGING_DIR)"
-HOST_PYTHON_VERSION=$(shell python --version 2>&1 | sed 's/Python \([0-9]\.[^\.]\).*/\1/')
-LIBXCB_MAKE_OPT = XCBPROTO_XCBINCLUDEDIR=$(STAGING_DIR)/usr/share/xcb XCBPROTO_XCBPYTHONDIR=$(STAGING_DIR)/usr/lib/python$(HOST_PYTHON_VERSION)/site-packages
+LIBXCB_MAKE_OPT = XCBPROTO_XCBINCLUDEDIR=$(STAGING_DIR)/usr/share/xcb \
+	XCBPROTO_XCBPYTHONDIR=$(HOST_DIR)/usr/lib/python$(HOST_PYTHON_VERSION)/site-packages
 
-HOST_LIBXCB_DEPENDENCIES = host-libxslt host-pthread-stubs host-xcb-proto host-xlib_libXdmcp host-xlib_libXau
+HOST_LIBXCB_DEPENDENCIES = \
+	host-libxslt host-pthread-stubs host-xcb-proto host-xlib_libXdmcp \
+	host-xlib_libXau host-python
 
 $(eval $(call AUTOTARGETS,package/x11r7,libxcb))
 $(eval $(call AUTOTARGETS,package/x11r7,libxcb,host))
