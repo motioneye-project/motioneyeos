b47a4d1073fa9f92692bdd2a9748f36db3e2f1fa
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-01-27 11:21:32 -0300

    libpng: bump to version 1.4.5
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libpng/libpng.mk
+++ package/libpng/libpng.mk
@@ -3,11 +3,10 @@
 # libpng (Portable Network Graphic library)
 #
 #############################################################
-LIBPNG_VERSION = 1.4.4
+LIBPNG_VERSION = 1.4.5
 LIBPNG_SERIES = 14
 LIBPNG_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/libpng
 LIBPNG_SOURCE = libpng-$(LIBPNG_VERSION).tar.bz2
-LIBPNG_LIBTOOL_PATCH = NO
 LIBPNG_INSTALL_STAGING = YES
 LIBPNG_DEPENDENCIES = host-pkg-config zlib
 
