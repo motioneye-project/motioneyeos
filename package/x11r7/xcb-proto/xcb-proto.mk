f13d97e106d115e00b1ca4e9009baaf0de43cd85
Peter Korsgaard <jacmet@sunsite.dk>
2011-01-27 23:34:39 +0100

    xcb-proto: needs python for the host
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xcb-proto/xcb-proto.mk
+++ package/x11r7/xcb-proto/xcb-proto.mk
@@ -9,6 +9,8 @@ XCB_PROTO_SITE = http://xcb.freedesktop.org/dist/
 
 XCB_PROTO_INSTALL_STAGING = YES
 
+XCB_PROTO_DEPENDENCIES = host-python
+
 $(eval $(call AUTOTARGETS,package/x11r7,xcb-proto))
 $(eval $(call AUTOTARGETS,package/x11r7,xcb-proto,host))
 
