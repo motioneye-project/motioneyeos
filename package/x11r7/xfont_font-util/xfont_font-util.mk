a52509a4ddabba26b1c927551cea209c4432b693
Peter Korsgaard <jacmet@sunsite.dk>
2011-01-30 21:07:26 +0100

    xfont_font-util: remove mapfiles workaround
    Not needed (in fact breaks the build) now pkg-config --variable= does the
    right thing.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xfont_font-util/xfont_font-util.mk
+++ package/x11r7/xfont_font-util/xfont_font-util.mk
@@ -13,13 +13,5 @@ XFONT_FONT_UTIL_INSTALL_TARGET = NO
 
 HOST_XFONT_FONT_UTIL_DEPENDENCIES = host-pkg-config
 
-define XFONT_FONT_UTIL_POST_INSTALL_FIXES
-	sed "s,^mapdir=.*,mapdir=$(STAGING_DIR)/usr/share/fonts/X11/util,g" \
-		$(STAGING_DIR)/usr/lib/pkgconfig/fontutil.pc > $(STAGING_DIR)/usr/lib/pkgconfig/fontutil.pc.new
-	mv $(STAGING_DIR)/usr/lib/pkgconfig/fontutil.pc.new $(STAGING_DIR)/usr/lib/pkgconfig/fontutil.pc
-endef
-
-XFONT_FONT_UTIL_POST_INSTALL_STAGING_HOOKS += XFONT_FONT_UTIL_POST_INSTALL_FIXES
-
 $(eval $(call AUTOTARGETS,package/x11r7,xfont_font-util))
 $(eval $(call AUTOTARGETS,package/x11r7,xfont_font-util,host))
