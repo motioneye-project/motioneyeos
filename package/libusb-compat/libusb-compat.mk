7e7a8dc85e92171e93e14eb0323bc518ecada3cc
Peter Korsgaard <jacmet@sunsite.dk>
2011-01-31 20:02:36 +0100

    libusb-compat: fixup libusb-config script similarly to other *-config scripts
    So programs using this don't end up looking for host includes / libraries.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libusb-compat/libusb-compat.mk
+++ package/libusb-compat/libusb-compat.mk
@@ -11,4 +11,12 @@ LIBUSB_COMPAT_DEPENDENCIES = host-pkg-config libusb
 LIBUSB_COMPAT_INSTALL_STAGING = YES
 LIBUSB_COMPAT_INSTALL_TARGET = YES
 
+define LIBUSB_COMPAT_FIXUP_CONFIG
+	$(SED) 's%prefix=/usr%prefix=$(STAGING_DIR)/usr%' \
+	    -e 's%exec_prefix=/usr%exec_prefix=$(STAGING_DIR)/usr%' \
+		$(STAGING_DIR)/usr/bin/libusb-config
+endef
+
+LIBUSB_COMPAT_POST_INSTALL_STAGING_HOOKS+=LIBUSB_COMPAT_FIXUP_CONFIG
+
 $(eval $(call AUTOTARGETS,package,libusb-compat))
