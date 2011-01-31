4bc799856b3ba9378b1d03dac7069d15500cbeeb
Martin Hicks <mort@bork.org>
2011-01-31 15:32:06 -0500

    package: add bonnie++
    Closes #3379
    
    A good filesystem performance benchmark program
    
    Signed-off-by: Martin Hicks <mort@bork.org>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/bonnie/bonnie.mk
@@ -0,0 +1,18 @@
+#
+# bonnie++
+#
+
+BONNIE_VERSION = 1.03e
+BONNIE_SOURCE = bonnie++-$(BONNIE_VERSION).tgz
+BONNIE_SITE = http://www.coker.com.au/bonnie++/
+
+define BONNIE_INSTALL_TARGET_CMDS
+	install -D -m 755 $(@D)/bonnie++ $(TARGET_DIR)/usr/sbin/bonnie++
+	install -D -m 755 $(@D)/zcav $(TARGET_DIR)/usr/sbin/zcav
+endef
+
+define BONNIE_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/sbin/bonnie++ $(TARGET_DIR)/usr/sbin/zcav
+endef
+
+$(eval $(call AUTOTARGETS,package,bonnie))
