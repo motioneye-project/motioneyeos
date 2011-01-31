343eb8b58389ef2335e79238a088c543f1c602ca
Peter Korsgaard <jacmet@sunsite.dk>
2011-01-31 20:23:42 +0100

    package: add libhid
    Based on patch by Johan Sagaert, but with some additional tweaks.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libhid/libhid.mk
@@ -0,0 +1,17 @@
+#############################################################
+#
+# libhid
+#
+#############################################################
+
+LIBHID_VERSION = 0.2.16
+LIBHID_SOURCE = libhid-$(LIBHID_VERSION).tar.gz
+LIBHID_SITE = http://alioth.debian.org/frs/download.php/1958
+LIBHID_DEPENDENCIES = libusb-compat libusb
+LIBHID_INSTALL_STAGING = YES
+# configure runs libusb-config for cflags/ldflags. Ensure it picks up
+# the target version
+LIBHID_CONF_ENV = PATH=$(STAGING_DIR)/usr/bin:$(TARGET_PATH)
+LIBHID_CONF_OPT = --disable-swig --without-doxygen --disable-package-config
+
+$(eval $(call AUTOTARGETS,package,libhid))
