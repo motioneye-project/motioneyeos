64d999b0226535eecde2046d05a29f0e88f0f18f
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-02-02 09:22:22 -0300

    libart: bump to version 2.3.21
    Bump libart to version 2.3.21 and update patch accordingly.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libart/libart.mk
+++ package/libart/libart.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBART_VERSION = 2.3.20
+LIBART_VERSION = 2.3.21
 LIBART_SOURCE = libart_lgpl-$(LIBART_VERSION).tar.gz
 LIBART_SITE = http://ftp.gnome.org/pub/gnome/sources/libart_lgpl/2.3/
 LIBART_AUTORECONF = YES
