694301dc5acd5b78d7baae69d622b3f07eb4ef4e
Peter Korsgaard <jacmet@sunsite.dk>
2011-02-03 22:51:43 +0100

    sawman: bump version
    Old version no longer builds after directfb bump.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sawman/sawman.mk
+++ package/sawman/sawman.mk
@@ -3,12 +3,10 @@
 # SAWMAN
 #
 #############################################################
-SAWMAN_VERSION:=1.4.10
+SAWMAN_VERSION:=1.4.11
 SAWMAN_SOURCE:=SaWMan-$(SAWMAN_VERSION).tar.gz
 SAWMAN_SITE:=http://www.directfb.org/downloads/Extras
 SAWMAN_INSTALL_STAGING = YES
-SAWMAN_INSTALL_TARGET = YES
-# SAWMAN_CONF_ENV = LDFLAGS="-L$(STAGING_DIR)/usr/lib -Wl,--rpath-link -Wl,$(STAGING_DIR)/usr/lib"
 SAWMAN_DEPENDENCIES = directfb
 
 $(eval $(call AUTOTARGETS,package,sawman))
