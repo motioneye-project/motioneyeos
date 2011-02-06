c7bffc1d32e985a0663bce9ca9e13ee191962cf9
Felipe Contreras <felipe.contreras@gmail.com>
2011-02-07 01:45:04 +0200

    Add dsp-tools package
    [Peter: add tidsp-binaries dependency, cleanup]
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/dsp-tools/dsp-tools.mk
@@ -0,0 +1,18 @@
+DSP_TOOLS_VERSION = 2.0
+DSP_TOOLS_SOURCE = dsp-tools-$(DSP_TOOLS_VERSION).tar.gz
+DSP_TOOLS_SITE = http://gst-dsp.googlecode.com/files/
+DSP_TOOLS_DEPENDENCIES = tidsp-binaries
+
+define DSP_TOOLS_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) -e
+endef
+
+define DSP_TOOLS_INSTALL_TARGET_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) -e DESTDIR=$(TARGET_DIR) install
+endef
+
+define DSP_TOOLS_UNINSTALL_TARGET_CMDS
+	rm -f $(addprefix $(TARGET_DIR)/usr/bin/dsp-, load probe test exec)
+endef
+
+$(eval $(call GENTARGETS,package,dsp-tools))
