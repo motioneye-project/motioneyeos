3b35cb07226006bf219ec07cf2074cfe5c1a3900
Felipe Contreras <felipe.contreras@gmail.com>
2011-02-07 01:45:02 +0200

    Add tidsp-binaries package
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/multimedia/tidsp-binaries/tidsp-binaries.mk
@@ -0,0 +1,13 @@
+TIDSP_BINARIES_VERSION=23.i3.8
+TIDSP_BINARIES_SOURCE=tidsp-binaries-$(TIDSP_BINARIES_VERSION).tar.gz
+TIDSP_BINARIES_SITE:=http://gst-dsp.googlecode.com/files/
+
+define TIDSP_BINARIES_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) -e DESTDIR=$(TARGET_DIR) install
+endef
+
+define TIDSP_BINARIES_UNINSTALL_TARGET_CMDS
+	$(RM) -r $(TARGET_DIR)/lib/dsp
+endef
+
+$(eval $(call GENTARGETS,package/multimedia,tidsp-binaries))
