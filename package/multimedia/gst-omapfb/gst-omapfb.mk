0c19765ba4289f0a1d53b7060095b65c42b9cda6
Felipe Contreras <felipe.contreras@gmail.com>
2011-02-07 01:45:05 +0200

    Add gst-omapfb package
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/multimedia/gst-omapfb/gst-omapfb.mk
@@ -0,0 +1,19 @@
+GST_OMAPFB_VERSION=1.0
+GST_OMAPFB_SOURCE=gst-omapfb-$(GST_OMAPFB_VERSION).tar.gz
+GST_OMAPFB_SITE=http://gst-dsp.googlecode.com/files/
+
+define GST_OMAPFB_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) -e
+endef
+
+define GST_OMAPFB_INSTALL_TARGET_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) -e DESTDIR=$(TARGET_DIR) install
+endef
+
+define GST_OMAPFB_UNINSTALL_TARGET_CMDS
+	$(RM) $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstomapfb.so
+endef
+
+GST_OMAPFB_DEPENDENCIES = gstreamer
+
+$(eval $(call GENTARGETS,package/multimedia,gst-omapfb))
