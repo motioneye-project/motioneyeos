6854cb49f8f673cb5700d52e8a133dcdde78c702
Felipe Contreras <felipe.contreras@gmail.com>
2011-02-07 01:45:03 +0200

    Add gst-dsp package
    [Peter: add tidsp-binaries to _DEPENDENCIES]
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/multimedia/gst-dsp/gst-dsp.mk
@@ -0,0 +1,19 @@
+GST_DSP_VERSION=0.8.0
+GST_DSP_SOURCE=gst-dsp-$(GST_DSP_VERSION).tar.gz
+GST_DSP_SITE=http://gst-dsp.googlecode.com/files/
+
+define GST_DSP_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) -e
+endef
+
+define GST_DSP_INSTALL_TARGET_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) -e DESTDIR=$(TARGET_DIR) install
+endef
+
+define GST_DSP_UNINSTALL_TARGET_CMDS
+	$(RM) $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstdsp.so
+endef
+
+GST_DSP_DEPENDENCIES = gstreamer tidsp-binaries
+
+$(eval $(call GENTARGETS,package/multimedia,gst-dsp))
