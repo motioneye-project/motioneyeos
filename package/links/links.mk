27680c7ad9f091b85c90ff13f4679985743f0363
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-02-09 15:22:40 -0300

    links: bump to version 2.3pre1
    * Switch to links 2.3pre1.
    * Enable graphics mode via directfb.
    
    TODO: Support X11 mode, maybe linuxfb (requires gpm).
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/links/links.mk
+++ package/links/links.mk
@@ -1,16 +1,35 @@
 #############################################################
 #
-# links (text based web browser)
+# links
 #
 #############################################################
-LINKS_VERSION:=1.01pre1-no-ssl
-LINKS_SITE:=http://artax.karlin.mff.cuni.cz/~mikulas/vyplody/links/download/no-ssl
-LINKS_SOURCE:=links-$(LINKS_VERSION).tar.gz
 
-LINKS_CONF_OPT = --localstatedir=/tmp
+LINKS_VERSION = 2.3pre1
+LINKS_SITE = http://links.twibright.com/download
+LINKS_CONF_OPT = --without-x
 
-define LINKS_INSTALL_TARGET_CMDS
-	install -c $(@D)/links $(TARGET_DIR)/usr/bin/links
-endef
+ifeq ($(BR2_PACKAGE_LINKS_GRAPHICS),y)
+LINKS_CONF_OPT += --enable-graphics
+LINKS_CONF_ENV = ac_cv_path_DIRECTFB_CONFIG=$(STAGING_DIR)/usr/bin/directfb-config
+LINKS_DEPENDENCIES += directfb libpng
+ifeq ($(BR2_PACKAGE_JPEG),y)
+LINKS_DEPENDENCIES += jpeg
+endif
+ifeq ($(BR2_PACKAGE_TIFF),y)
+LINKS_DEPENDENCIES += tiff
+endif
+endif
+
+ifeq ($(BR2_PACKAGE_BZIP2),y)
+LINKS_DEPENDENCIES += bzip2
+endif
+
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+LINKS_DEPENDENCIES += openssl
+endif
+
+ifeq ($(BR2_PACKAGE_ZLIB),y)
+LINKS_DEPENDENCIES += zlib
+endif
 
 $(eval $(call AUTOTARGETS,package,links))
