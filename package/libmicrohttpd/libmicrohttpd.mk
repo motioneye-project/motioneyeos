7ee387f43b8daa8e749472a9b1153941a44e688e
Peter Korsgaard <jacmet@sunsite.dk>
2011-02-19 23:37:40 +0100

    libmicrohttpd: make https support optional
    Libmicrohttpd works nicely without https support, so make it optional.
    Also ensure the cross-libgcrypt-config is found, rather than whatever
    is installed on the host.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libmicrohttpd/libmicrohttpd.mk
+++ package/libmicrohttpd/libmicrohttpd.mk
@@ -8,6 +8,12 @@ LIBMICROHTTPD_SOURCE:=libmicrohttpd-$(LIBMICROHTTPD_VERSION).tar.gz
 LIBMICROHTTPD_SITE:=$(BR2_GNU_MIRROR)/libmicrohttpd
 LIBMICROHTTPD_INSTALL_STAGING = YES
 
-LIBMICROHTTPD_DEPENDENCIES = libgcrypt
+ifeq ($(BR2_PACKAGE_LIBMICROHTTPD_SSL),y)
+LIBMICROHTTPD_DEPENDENCIES += libgcrypt
+LIBMICROHTTPD_CONF_OPT += --enable-https \
+			  --with-libgcrypt-prefix=$(STAGING_DIR)/usr
+else
+LIBMICROHTTPD_CONF_OPT += --disable-https
+endif
 
 $(eval $(call AUTOTARGETS,package,libmicrohttpd))
