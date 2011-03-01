0a064b48c9d1044b05aee65272f5af7f28879473
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-03-01 13:20:01 -0300

    gst-ffmpeg: bump to version 0.10.11
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/gst-ffmpeg/gst-ffmpeg.mk
+++ package/multimedia/gst-ffmpeg/gst-ffmpeg.mk
@@ -3,11 +3,12 @@
 # gst-ffmpeg
 #
 #############################################################
-GST_FFMPEG_VERSION = 0.10.9
+
+GST_FFMPEG_VERSION = 0.10.11
 GST_FFMPEG_SOURCE = gst-ffmpeg-$(GST_FFMPEG_VERSION).tar.bz2
 GST_FFMPEG_SITE = http://gstreamer.freedesktop.org/src/gst-ffmpeg
 GST_FFMPEG_INSTALL_STAGING = YES
-GST_FFMPEG_DEPENDENCIES = gstreamer gst-plugins-base ffmpeg liboil
+GST_FFMPEG_DEPENDENCIES = gstreamer gst-plugins-base ffmpeg
 GST_FFMPEG_CONF_OPT = --with-system-ffmpeg
 
 ifeq ($(BR2_PACKAGE_BZIP2),y)
