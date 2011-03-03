99df35d9a5246751a4a751f4b4b5c5cdbc78bc6a
Marek Belisko <marek.belisko@open-nandra.com>
2011-03-03 14:53:30 +0100

    buildroot: Add webdav support to lighttpd.
    [Peter: needs host-pkg-config]
    Signed-off-by: Marek Belisko <marek.belisko@open-nandra.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/lighttpd/lighttpd.mk
+++ package/lighttpd/lighttpd.mk
@@ -6,7 +6,7 @@
 
 LIGHTTPD_VERSION = 1.4.28
 LIGHTTPD_SITE = http://download.lighttpd.net/lighttpd/releases-1.4.x
-
+LIGHTTPD_DEPENDENCIES = host-pkg-config
 LIGHTTPD_CONF_OPT = \
 	--libdir=/usr/lib/lighttpd \
 	--libexecdir=/usr/lib \
@@ -41,6 +41,13 @@ LIGHTTPD_DEPENDENCIES += pcre
 LIGHTTPD_CONF_OPT += --with-pcre
 else
 LIGHTTPD_CONF_OPT += --without-pcre
+endif
+
+ifeq ($(BR2_PACKAGE_LIGHTTPD_WEBDAV),y)
+LIGHTTPD_DEPENDENCIES += libxml2 sqlite
+LIGHTTPD_CONF_OPT += --with-webdav-props --with-webdav-locks
+else
+LIGHTTPD_CONF_OPT += ---without-webdav-props --without-webdav-locks
 endif
 
 define LIGHTTPD_UNINSTALL_TARGET_CMDS
