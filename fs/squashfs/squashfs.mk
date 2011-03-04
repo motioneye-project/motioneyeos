194ac2d5496d7895980252059d9e133b218cf170
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-03-04 11:10:14 -0300

    squashfs: re-add legacy lzma support
    Readd legacy LZMA support since older patched kernels might be using it.
    Introduce an XZ target filesystem option for the new format.
    
    [Peter: add a seperate xz target for squashfs-target as well]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- fs/squashfs/squashfs.mk
+++ fs/squashfs/squashfs.mk
@@ -11,9 +11,13 @@ ifeq ($(BR2_TARGET_ROOTFS_SQUASHFS4_LZO),y)
 ROOTFS_SQUASHFS_ARGS += -comp lzo
 else
 ifeq ($(BR2_TARGET_ROOTFS_SQUASHFS4_LZMA),y)
+ROOTFS_SQUASHFS_ARGS += -comp lzma
+else
+ifeq ($(BR2_TARGET_ROOTFS_SQUASHFS4_XZ),y)
 ROOTFS_SQUASHFS_ARGS += -comp xz
 else
 ROOTFS_SQUASHFS_ARGS += -comp gzip
+endif
 endif
 endif
 
