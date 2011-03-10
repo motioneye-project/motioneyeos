f5626851ab1a1e51cdcc6153b5913ba19dac79d3
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-03-10 10:44:35 -0300

    midori: bump to version 0.3.2
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/midori/midori.mk
+++ package/midori/midori.mk
@@ -4,9 +4,9 @@
 #
 #############################################################
 
-MIDORI_VERSION = 0.2.9
+MIDORI_VERSION = 0.3.2
 MIDORI_SOURCE = midori-$(MIDORI_VERSION).tar.bz2
-MIDORI_SITE = http://archive.xfce.org/src/apps/midori/0.2/
+MIDORI_SITE = http://archive.xfce.org/src/apps/midori/0.3/
 MIDORI_INSTALL_TARGET = YES
 
 MIDORI_DEPENDENCIES = host-pkg-config host-intltool webkit libsexy libgtk2
