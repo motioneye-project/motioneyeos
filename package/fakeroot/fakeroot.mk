308bc537919e0ea4dfbeda0889de9e6c14a73640
Hector Oron <hector.oron@gmail.com>
2011-03-13 21:28:18 +0100

    fakeroot: fix download URL
    Closes #3451
    
    The Debian package snapshot service moved to an official debian.org
    address last year (http://www.debian.org/News/2010/20100412) with a
    slightly different URL structure, so adjust the download URL to match.
    
    Signed-off-by: Hector Oron <hector.oron@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/fakeroot/fakeroot.mk
+++ package/fakeroot/fakeroot.mk
@@ -5,7 +5,7 @@
 #############################################################
 FAKEROOT_VERSION:=1.9.5
 FAKEROOT_SOURCE:=fakeroot_$(FAKEROOT_VERSION).tar.gz
-FAKEROOT_SITE:=http://snapshot.debian.net/archive/2008/04/27/debian/pool/main/f/fakeroot/
+FAKEROOT_SITE:=http://snapshot.debian.org/archive/debian/20080427T000000Z/pool/main/f/fakeroot/
 
 define FAKEROOT_PATCH_FAKEROOT_IN
 	# If using busybox getopt, make it be quiet.
