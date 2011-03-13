b698051d599206a9b90e425a8fc54225410dd762
Peter Korsgaard <jacmet@sunsite.dk>
2011-03-13 23:46:54 +0100

    fakeroot: use --program-prefix rather than manually renaming binaries
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/fakeroot/fakeroot.mk
+++ package/fakeroot/fakeroot.mk
@@ -6,6 +6,7 @@
 FAKEROOT_VERSION:=1.9.5
 FAKEROOT_SOURCE:=fakeroot_$(FAKEROOT_VERSION).tar.gz
 FAKEROOT_SITE:=http://snapshot.debian.org/archive/debian/20080427T000000Z/pool/main/f/fakeroot/
+FAKEROOT_CONF_OPT = --program-prefix=''
 
 define FAKEROOT_PATCH_FAKEROOT_IN
 	# If using busybox getopt, make it be quiet.
@@ -14,15 +15,6 @@ define FAKEROOT_PATCH_FAKEROOT_IN
 endef
 
 FAKEROOT_POST_PATCH_HOOKS += FAKEROOT_PATCH_FAKEROOT_IN
-
-define FAKEROOT_RENAME_TARGET_BINARIES
-	-mv $(TARGET_DIR)/usr/bin/$(ARCH)-*-faked \
-		$(TARGET_DIR)/usr/bin/faked
-	-mv $(TARGET_DIR)/usr/bin/$(ARCH)-*-fakeroot \
-		$(TARGET_DIR)/usr/bin/fakeroot
-endef
-
-FAKEROOT_POST_INSTALL_TARGET_HOOKS += FAKEROOT_RENAME_TARGET_BINARIES
 
 $(eval $(call AUTOTARGETS,package,fakeroot))
 $(eval $(call AUTOTARGETS,package,fakeroot,host))
