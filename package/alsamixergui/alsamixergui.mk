39aafe84c1be0d267f87dc98015ae7fbb62394e2
Hector Oron <hector.oron@gmail.com>
2011-03-13 21:33:53 +0100

    alsamixergui: fix download URL
    Closes #3457
    
    The Debian package snapshot service moved to an official debian.org
    address last year (http://www.debian.org/News/2010/20100412) with a
    slightly different URL structure, so adjust the download URL to match.
    
    Signed-off-by: Hector Oron <hector.oron@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/alsamixergui/alsamixergui.mk
+++ package/alsamixergui/alsamixergui.mk
@@ -5,10 +5,8 @@
 #############################################################
 ALSAMIXERGUI_VERSION = 0.9.0rc2-1
 ALSAMIXERGUI_SOURCE = alsamixergui_$(ALSAMIXERGUI_VERSION).orig.tar.gz
-ALSAMIXERGUI_SITE = http://snapshot.debian.net/archive/2008/03/19/debian/pool/main/a/alsamixergui
+ALSAMIXERGUI_SITE = http://snapshot.debian.org/archive/debian/20080319T000000Z/pool/main/a/alsamixergui/
 ALSAMIXERGUI_AUTORECONF = YES
-ALSAMIXERGUI_INSTALL_STAGING = NO
-ALSAMIXERGUI_INSTALL_TARGET = YES
 
 ALSAMIXERGUI_CONF_ENV = ac_cv_lib_fltk_numericsort=yes \
 			ac_cv_lib_fltk_fl_numericsort=yes \
