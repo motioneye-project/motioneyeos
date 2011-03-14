b05c54daf0066f29bd2542bea637f646a7fbb4d1
Peter Korsgaard <jacmet@sunsite.dk>
2011-03-14 23:36:12 +0100

    xterm: fix dependencies
    xterm doesn't need the xserver to build.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/xterm/xterm.mk
+++ package/xterm/xterm.mk
@@ -7,7 +7,7 @@
 XTERM_VERSION = 267
 XTERM_SOURCE = xterm-$(XTERM_VERSION).tgz
 XTERM_SITE = ftp://invisible-island.net/xterm
-XTERM_DEPENDENCIES = ncurses xserver_xorg-server xlib_libXaw
+XTERM_DEPENDENCIES = ncurses xlib_libXaw
 XTERM_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
 $(eval $(call AUTOTARGETS,package,xterm))
