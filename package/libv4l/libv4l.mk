54f33b3865a88002c06abf8da1c4529d43cd07c8
Peter Korsgaard <jacmet@sunsite.dk>
2011-04-05 12:05:08 +0200

    libv4l: add options to install utilites
    These have extra deps though (C++ for some, argp.h for others which is not
    available on uClibc).
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libv4l/libv4l.mk
+++ package/libv4l/libv4l.mk
@@ -9,20 +9,33 @@ LIBV4L_SITE = http://linuxtv.org/downloads/v4l-utils/
 LIBV4L_INSTALL_STAGING = YES
 LIBV4L_MAKE_OPTS = PREFIX=/usr
 
+LIBV4L_DIRS_y += lib
+LIBV4L_DIRS_$(BR2_PACKAGE_LIBV4L_DECODE_TM6000)	+= utils/decode_tm6000
+LIBV4L_DIRS_$(BR2_PACKAGE_LIBV4L_IR_KEYTABLE)	+= utils/keytable
+LIBV4L_DIRS_$(BR2_PACKAGE_LIBV4L_V4L2_COMPLIANCE) += utils/v4l2-compliance
+LIBV4L_DIRS_$(BR2_PACKAGE_LIBV4L_V4L2_CTL)	+= utils/v4l2-ctl
+LIBV4L_DIRS_$(BR2_PACKAGE_LIBV4L_V4L2_DBG)	+= utils/v4l2-dbg
+
 ifeq ($(BR2_PREFER_STATIC_LIB),y)
 	LIBV4L_MAKE_OPTS += LINKTYPE=static
 endif
 
 define LIBV4L_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/lib $(LIBV4L_MAKE_OPTS)
+	for i in $(LIBV4L_DIRS_y); do \
+		$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/$$i \
+			$(LIBV4L_MAKE_OPTS); done
 endef
 
 define LIBV4L_INSTALL_STAGING_CMDS
-	$(MAKE) -C $(@D)/lib DESTDIR=$(STAGING_DIR) $(LIBV4L_MAKE_OPTS) install
+	for i in $(LIBV4L_DIRS_y); do \
+		$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/$$i \
+			DESTDIR=$(STAGING_DIR) $(LIBV4L_MAKE_OPTS) install; done
 endef
 
 define LIBV4L_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(@D)/lib DESTDIR=$(TARGET_DIR) $(LIBV4L_MAKE_OPTS) install
+	for i in $(LIBV4L_DIRS_y); do \
+		$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/$$i \
+			DESTDIR=$(TARGET_DIR) $(LIBV4L_MAKE_OPTS) install; done
 endef
 
 $(eval $(call GENTARGETS,package,libv4l))
