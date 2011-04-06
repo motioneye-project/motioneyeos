8cc20e69850482faa8fc6be74b1a90c2b044c1ef
Allan W. Nielsen <a@awn.dk>
2011-04-06 12:04:14 +0200

    xdriver_xf86-video-ati: version bump to 6.14.1 and removed cross-compile patch
    [Peter: drop AUTORECONF]
    Signed-off-by: Allan W. Nielsen <a@awn.dk>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xdriver_xf86-video-ati/xdriver_xf86-video-ati.mk
+++ package/x11r7/xdriver_xf86-video-ati/xdriver_xf86-video-ati.mk
@@ -4,10 +4,9 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_ATI_VERSION = 6.12.4
+XDRIVER_XF86_VIDEO_ATI_VERSION = 6.14.1
 XDRIVER_XF86_VIDEO_ATI_SOURCE = xf86-video-ati-$(XDRIVER_XF86_VIDEO_ATI_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_ATI_SITE = http://xorg.freedesktop.org/releases/individual/driver
-XDRIVER_XF86_VIDEO_ATI_AUTORECONF = YES
 XDRIVER_XF86_VIDEO_ATI_DEPENDENCIES = xserver_xorg-server libdrm xproto_fontsproto xproto_glproto xproto_randrproto xproto_videoproto xproto_xextproto xproto_xf86driproto xproto_xineramaproto xproto_xproto
 
 $(eval $(autotools-package))
