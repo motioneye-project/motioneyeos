7889705f0b745c15e73331e543e979e015c33d7a
Allan W. Nielsen <a@awn.dk>
2011-04-06 12:03:52 +0200

    xproto_xf86driproto: version bump to 2.1.1
    Signed-off-by: Allan W. Nielsen <a@awn.dk>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xproto_xf86driproto/xproto_xf86driproto.mk
+++ package/x11r7/xproto_xf86driproto/xproto_xf86driproto.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XPROTO_XF86DRIPROTO_VERSION = 2.1.0
+XPROTO_XF86DRIPROTO_VERSION = 2.1.1
 XPROTO_XF86DRIPROTO_SOURCE = xf86driproto-$(XPROTO_XF86DRIPROTO_VERSION).tar.bz2
 XPROTO_XF86DRIPROTO_SITE = http://xorg.freedesktop.org/releases/individual/proto
 XPROTO_XF86DRIPROTO_INSTALL_STAGING = YES
