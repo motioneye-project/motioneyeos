cae99aa41caa9eecc1f6d0a9d1e0c8ac10ae6638
Allan W. Nielsen <a@awn.dk>
2011-04-06 12:03:58 +0200

    xproto_renderproto: version bump to 0.11.1
    Signed-off-by: Allan W. Nielsen <a@awn.dk>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xproto_renderproto/xproto_renderproto.mk
+++ package/x11r7/xproto_renderproto/xproto_renderproto.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XPROTO_RENDERPROTO_VERSION = 0.11
+XPROTO_RENDERPROTO_VERSION = 0.11.1
 XPROTO_RENDERPROTO_SOURCE = renderproto-$(XPROTO_RENDERPROTO_VERSION).tar.bz2
 XPROTO_RENDERPROTO_SITE = http://xorg.freedesktop.org/releases/individual/proto
 XPROTO_RENDERPROTO_INSTALL_STAGING = YES
