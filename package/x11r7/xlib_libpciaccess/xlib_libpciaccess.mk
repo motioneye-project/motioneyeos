185999048cbdcf7a99062a8fb0d012a713418e0c
Allan W. Nielsen <a@awn.dk>
2011-04-06 12:03:44 +0200

    xlib_libpciaccess: version bump to 0.12.1
    Signed-off-by: Allan W. Nielsen <a@awn.dk>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xlib_libpciaccess/xlib_libpciaccess.mk
+++ package/x11r7/xlib_libpciaccess/xlib_libpciaccess.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBPCIACCESS_VERSION = 0.11.0
+XLIB_LIBPCIACCESS_VERSION = 0.12.1
 XLIB_LIBPCIACCESS_SOURCE = libpciaccess-$(XLIB_LIBPCIACCESS_VERSION).tar.bz2
 XLIB_LIBPCIACCESS_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBPCIACCESS_INSTALL_STAGING = YES
