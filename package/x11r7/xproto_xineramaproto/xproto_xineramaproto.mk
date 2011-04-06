58335d4de12f188f3f15799c68f7b7245bb4cfb5
Allan W. Nielsen <a@awn.dk>
2011-04-06 12:03:50 +0200

    xproto_xineramaproto: version bump to 1.2.1
    Signed-off-by: Allan W. Nielsen <a@awn.dk>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xproto_xineramaproto/xproto_xineramaproto.mk
+++ package/x11r7/xproto_xineramaproto/xproto_xineramaproto.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XPROTO_XINERAMAPROTO_VERSION = 1.2
+XPROTO_XINERAMAPROTO_VERSION = 1.2.1
 XPROTO_XINERAMAPROTO_SOURCE = xineramaproto-$(XPROTO_XINERAMAPROTO_VERSION).tar.bz2
 XPROTO_XINERAMAPROTO_SITE = http://xorg.freedesktop.org/releases/individual/proto
 XPROTO_XINERAMAPROTO_INSTALL_STAGING = YES
