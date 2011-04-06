5f97e6ffb99beaa176559cb09472212ec74369e0
Allan W. Nielsen <a@awn.dk>
2011-04-06 12:04:00 +0200

    xproto_printproto: version bump to 1.0.5
    Signed-off-by: Allan W. Nielsen <a@awn.dk>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xproto_printproto/xproto_printproto.mk
+++ package/x11r7/xproto_printproto/xproto_printproto.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XPROTO_PRINTPROTO_VERSION = 1.0.3
+XPROTO_PRINTPROTO_VERSION = 1.0.5
 XPROTO_PRINTPROTO_SOURCE = printproto-$(XPROTO_PRINTPROTO_VERSION).tar.bz2
 XPROTO_PRINTPROTO_SITE = http://xorg.freedesktop.org/releases/individual/proto
 XPROTO_PRINTPROTO_INSTALL_STAGING = YES
