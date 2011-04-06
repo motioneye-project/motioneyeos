6f26cc6660111607d35e84c7da4772f78b78feb7
Allan W. Nielsen <a@awn.dk>
2011-04-06 12:03:35 +0200

    xlib_libXext: version bump to 1.2.0
    Signed-off-by: Allan W. Nielsen <a@awn.dk>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xlib_libXext/xlib_libXext.mk
+++ package/x11r7/xlib_libXext/xlib_libXext.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBXEXT_VERSION = 1.1.1
+XLIB_LIBXEXT_VERSION = 1.2.0
 XLIB_LIBXEXT_SOURCE = libXext-$(XLIB_LIBXEXT_VERSION).tar.bz2
 XLIB_LIBXEXT_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXEXT_INSTALL_STAGING = YES
