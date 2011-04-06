0eb66c7d08c2abe3a6724eec1d8902ad96f02077
Allan W. Nielsen <a@awn.dk>
2011-04-06 12:04:06 +0200

    xproto_dmxproto: version bump to 2.3.1
    Signed-off-by: Allan W. Nielsen <a@awn.dk>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xproto_dmxproto/xproto_dmxproto.mk
+++ package/x11r7/xproto_dmxproto/xproto_dmxproto.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XPROTO_DMXPROTO_VERSION = 2.3
+XPROTO_DMXPROTO_VERSION = 2.3.1
 XPROTO_DMXPROTO_SOURCE = dmxproto-$(XPROTO_DMXPROTO_VERSION).tar.bz2
 XPROTO_DMXPROTO_SITE = http://xorg.freedesktop.org/releases/individual/proto
 XPROTO_DMXPROTO_INSTALL_STAGING = YES
