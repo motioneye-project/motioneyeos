2fed71e3de9ebca27afe5efa55a6dc308156983b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-04-13 13:29:24 -0300

    toolchain: bump mpc to version 0.9
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/mpc/mpc.mk
+++ package/mpc/mpc.mk
@@ -4,10 +4,12 @@
 #
 #############################################################
 
-MPC_VERSION = 0.8.2
+MPC_VERSION = 0.9
 MPC_SITE = http://www.multiprecision.org/mpc/download
 MPC_INSTALL_STAGING = YES
 MPC_DEPENDENCIES = gmp mpfr
+MPC_AUTORECONF = YES
+HOST_MPC_AUTORECONF = YES
 HOST_MPC_DEPENDENCIES = host-gmp host-mpfr
 
 $(eval $(call AUTOTARGETS,package,mpc))
