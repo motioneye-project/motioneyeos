efae605c88deb15d9c50fd67bdd80f46a78b06c1
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-04-26 11:56:48 -0300

    libcap: bump to version 2.20
    Bump libcap to version 2.20 and adjust style to match buildroot's
    defacto standard.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libcap/libcap.mk
+++ package/libcap/libcap.mk
@@ -1,27 +1,37 @@
-LIBCAP_VERSION=2.19
-LIBCAP_SOURCE=libcap-$(LIBCAP_VERSION).tar.bz2
-LIBCAP_SITE=http://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/
-LIBCAP_INSTALL_STAGING=YES
+#############################################################
+#
+# libcap
+#
+#############################################################
+
+LIBCAP_VERSION = 2.20
+LIBCAP_SITE = http://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2
+LIBCAP_DEPENDENCIES = host-libcap
+LIBCAP_INSTALL_STAGING = YES
 
 define LIBCAP_BUILD_CMDS
- $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) LIBATTR=no
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
+		LIBATTR=no BUILD_CC="$(HOSTCC)" BUILD_CFLAGS="$(HOST_CFLAGS)"
 endef
 
 define LIBCAP_INSTALL_STAGING_CMDS
- $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) LIBATTR=no DESTDIR=$(STAGING_DIR) prefix=/usr lib=lib install
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) LIBATTR=no DESTDIR=$(STAGING_DIR) \
+		prefix=/usr lib=lib install
 endef
 
 define LIBCAP_INSTALL_TARGET_CMDS
- $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) LIBATTR=no DESTDIR=$(TARGET_DIR) prefix=/usr lib=lib install
- rm -f $(addprefix $(TARGET_DIR)/usr/sbin/,capsh getpcaps)
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) LIBATTR=no DESTDIR=$(TARGET_DIR) \
+		prefix=/usr lib=lib install
+	rm -f $(addprefix $(TARGET_DIR)/usr/sbin/,capsh getpcaps)
 endef
 
 define HOST_LIBCAP_BUILD_CMDS
- $(HOST_MAKE_ENV) $(HOST_CONFIGURE_OPTS) $(MAKE) -C $(@D) LIBATTR=no
+	$(HOST_MAKE_ENV) $(HOST_CONFIGURE_OPTS) $(MAKE) -C $(@D) LIBATTR=no
 endef
 
 define HOST_LIBCAP_INSTALL_CMDS
- $(HOST_MAKE_ENV) $(MAKE) -C $(@D) LIBATTR=no DESTDIR=$(HOST_DIR) prefix=/usr lib=lib install
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) LIBATTR=no DESTDIR=$(HOST_DIR) \
+		prefix=/usr lib=lib install
 endef
 
 $(eval $(call GENTARGETS,package,libcap))
