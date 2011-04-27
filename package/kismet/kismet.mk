2c200d5b369378876f85afc222ba7f14b120bbc6
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-04-27 15:05:09 -0300

    kismet: bump to version 2011-03-R2
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/kismet/kismet.mk
+++ package/kismet/kismet.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-KISMET_VERSION = 2010-07-R1
+KISMET_VERSION = 2011-03-R2
 KISMET_SITE = http://www.kismetwireless.net/code
 KISMET_DEPENDENCIES = libpcap ncurses
 
