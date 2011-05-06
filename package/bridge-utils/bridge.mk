466cb4531d939785c96dfc1fa931b0815dd24e3c
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-05-06 11:04:45 -0300

    bridge-utils: bump to version 1.5
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/bridge-utils/bridge.mk
+++ package/bridge-utils/bridge.mk
@@ -1,20 +1,18 @@
 #############################################################
 #
-# bridge-utils - User Space Program For Controlling Bridging
+# bridge-utils
 #
 #############################################################
-#
-BRIDGE_VERSION:=1.4
-BRIDGE_SOURCE:=bridge-utils-$(BRIDGE_VERSION).tar.gz
-BRIDGE_SITE=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/bridge/
-BRIDGE_AUTORECONF:=YES
-BRIDGE_INSTALL_STAGING:=NO
-BRIDGE_INSTALL_TARGET:=YES
-BRIDGE_CONF_OPT:=--with-linux-headers=$(LINUX_HEADERS_DIR)
+
+BRIDGE_VERSION = 1.5
+BRIDGE_SOURCE = bridge-utils-$(BRIDGE_VERSION).tar.gz
+BRIDGE_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/bridge
+BRIDGE_AUTORECONF = YES
+BRIDGE_CONF_OPT = --with-linux-headers=$(LINUX_HEADERS_DIR)
 
 define BRIDGE_UNINSTALL_TARGET_CMDS
 	rm -f $(addprefix $(TARGET_DIR)/usr/,lib/libbridge.a \
-		include/libbridge.h man/man8/brctl.8 sbin/brctl)
+		include/libbridge.h share/man/man8/brctl.8 sbin/brctl)
 endef
 
 $(eval $(call AUTOTARGETS,package,bridge))
