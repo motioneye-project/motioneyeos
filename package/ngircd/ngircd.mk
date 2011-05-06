ee3f31b536965d5419d6e437cb4e241ece84587f
Fabien Marteau <fabien.marteau@armadeus.com>
2011-05-06 11:23:33 +0200

    package: Adding Next Generation IRC deamon ngircd
    [Peter: drop explicit documentation removal]
    Acked-by: Daniel Nyström <daniel.nystrom@timeterminal.se>
    Signed-off-by: Fabien Marteau <fabien.marteau@armadeus.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/ngircd/ngircd.mk
@@ -0,0 +1,11 @@
+#############################################################
+#
+# ngIRCd: Next Generation IRC Daemon
+#
+#############################################################
+
+NGIRCD_VERSION = 17.1
+NGIRCD_SITE = ftp://ftp.berlios.de/pub/ngircd/
+NGIRCD_DEPENDENCIES = zlib
+
+$(eval $(call AUTOTARGETS,package,ngircd))
