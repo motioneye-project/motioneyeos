80eff666607300578090b8eee5dcd33d947dec82
Peter Korsgaard <jacmet@sunsite.dk>
2011-05-15 00:07:28 +0200

    barebox: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- boot/barebox/barebox.mk
+++ boot/barebox/barebox.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-BAREBOX_VERSION:=2011.04.0
+BAREBOX_VERSION:=2011.05.0
 BAREBOX_SOURCE:=barebox-$(BAREBOX_VERSION).tar.bz2
 BAREBOX_SITE:=http://www.barebox.org/download/
 BAREBOX_DIR:=$(BUILD_DIR)/barebox-$(BAREBOX_VERSION)
