91b2ec27a7b06c603f7c2f1bce3e7f422fb6b776
Peter Korsgaard <jacmet@sunsite.dk>
2011-05-16 10:08:38 +0200

    haserl: fix lua support after 0.9.29 bump
    haserl no longer ships haserl_lualib.inc so our lua2c hack didn't work
    anymore.
    
    Fix is by adding a patch to re-add it and move the Makefile.in changes
    to here rather than with sed.
    
    Longer term we should probably add host-lua support instead.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/haserl/haserl.mk
+++ package/haserl/haserl.mk
@@ -10,14 +10,6 @@ HASERL_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/ha
 # force haserl 0.8.0 to use install-sh so stripping works
 HASERL_CONF_ENV = ac_cv_path_install=./install-sh
 
-# lua2c is only needed when haserl_lualib.inc is older than haserl_lualib.lua
-# So avoid having a host-lua just for this
-define HASERL_NO_LUA2C
-	$(SED) 's/haserl_lualib.lua lua2c/haserl_lualib.lua/' \
-		$(@D)/src/Makefile.in
-endef
-HASERL_POST_EXTRACT_HOOKS += HASERL_NO_LUA2C
-
 ifeq ($(BR2_PACKAGE_HASERL_WITH_LUA),y)
 	HASERL_CONF_OPT += --with-lua=$(STAGING_DIR) \
 		--with-lua-headers=$(STAGING_DIR)
