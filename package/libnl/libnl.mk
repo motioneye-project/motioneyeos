f890debb30c0ce920bc44e7d74f1d5d4280d4625
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-05-16 11:18:26 -0300

    libnl: minor package fixes
    * libnl fails to build with multiple jobs so keep make at -j1
    * Multiple libraries are installed so update uninstall target for this
    
    [Peter: remove libnl/libnl-*.* to catch subdir + any new libs in the future]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libnl/libnl.mk
+++ package/libnl/libnl.mk
@@ -9,9 +9,11 @@ LIBNL_SOURCE = libnl-$(LIBNL_VERSION).tar.gz
 LIBNL_SITE = http://www.infradead.org/~tgr/libnl/files/
 LIBNL_INSTALL_STAGING = YES
 LIBNL_DEPENDENCIES = host-bison
+LIBNL_MAKE = $(MAKE1)
 
 define LIBNL_UNINSTALL_TARGET_CMDS
-	rm -f $(TARGET_DIR)/usr/lib/libnl.so*
+	rm -r $(TARGET_DIR)/usr/lib/libnl.* $(TARGET_DIR)/usr/lib/libnl-*.*
+	rm -rf $(TARGET_DIR)/usr/lib/libnl
 endef
 
 $(eval $(call AUTOTARGETS,package,libnl))
