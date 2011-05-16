5c22f20f4b12395a34ce47c1aba608fccaf1fbe0
Peter Korsgaard <jacmet@sunsite.dk>
2011-05-16 14:43:51 +0200

    libnl: depend on host-bison
    libnl build breaks with bison <2.4 with:
    
    bison -y -d  -o route/cls/ematch_syntax.c route/cls/ematch_syntax.y
    route/cls/ematch_syntax.y:31.9-16: syntax error, unexpected
    identifier, expecting string
    
    So build our own known good version and use instead.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libnl/libnl.mk
+++ package/libnl/libnl.mk
@@ -8,6 +8,7 @@ LIBNL_VERSION = 3.0
 LIBNL_SOURCE = libnl-$(LIBNL_VERSION).tar.gz
 LIBNL_SITE = http://www.infradead.org/~tgr/libnl/files/
 LIBNL_INSTALL_STAGING = YES
+LIBNL_DEPENDENCIES = host-bison
 
 define LIBNL_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/lib/libnl.so*
