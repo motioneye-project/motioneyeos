efce5be9af5c026bb1136cb2876c36edaaaba49a
Peter Korsgaard <jacmet@sunsite.dk>
2011-05-16 10:13:19 +0200

    haserl: remove 0.8.0 install workaround
    Not needed anymore as stripping is handled on a global level.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/haserl/haserl.mk
+++ package/haserl/haserl.mk
@@ -7,9 +7,6 @@
 HASERL_VERSION = $(call qstrip,$(BR2_PACKAGE_HASERL_VERSION))
 HASERL_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/haserl/
 
-# force haserl 0.8.0 to use install-sh so stripping works
-HASERL_CONF_ENV = ac_cv_path_install=./install-sh
-
 ifeq ($(BR2_PACKAGE_HASERL_WITH_LUA),y)
 	HASERL_CONF_OPT += --with-lua=$(STAGING_DIR) \
 		--with-lua-headers=$(STAGING_DIR)
