95cf21ef8c098369a54e8bd2ec97f1b56433c928
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-05-17 20:22:29 +0200

    dhcpdump: move as a proper package, bump version
    dhcpdump 1.7 required tcpdump, but version 1.8 now uses libpcap
    instead. Also version 1.8 is no longer autotools based, but just a
    simple Makefile.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/dhcpdump/dhcpdump.mk
@@ -0,0 +1,21 @@
+#############################################################
+#
+# dhcpdump
+#
+#############################################################
+
+DHCPDUMP_VERSION = 1.8
+DHCPDUMP_SITE    = http://www.mavetju.org/download/
+DHCPDUMP_SOURCE  = dhcpdump-$(DHCPDUMP_VERSION).tar.gz
+
+DHCPDUMP_DEPENDENCIES = libpcap
+
+define DHCPDUMP_BUILD_CMDS
+	$(MAKE) -C $(@D) CC="$(TARGET_CC) $(TARGET_CFLAGS)"
+endef
+
+define DHCPDUMP_INSTALL_TARGET_CMDS
+	install -m 0755 -D $(@D)/dhcpdump $(TARGET_DIR)/usr/bin/dhcpdump
+endef
+
+$(eval $(call GENTARGETS,package,dhcpdump))
