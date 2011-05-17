e3bf4de3298ec01cf6d91bc55758b4e605b52b88
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-05-17 20:06:28 +0200

    festival: remove packages
    Festival does not build, nobody cared since a long time, and they have
    been marked deprecated since quite some time.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/multimedia/festival/festival.mk
+++
@@ -1,4 +0,0 @@
-include package/multimedia/festival/speech-tools/speech-tools.mk
-include package/multimedia/festival/festival/festival.mk
-include package/multimedia/festival/voices/festvox.mk
-include package/multimedia/festival/lexicons/festlex.mk
