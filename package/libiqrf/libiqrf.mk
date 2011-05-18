24d498dadf35db72a1a8d173c071def9b0c66361
Marek Belisko <marek.belisko@open-nandra.com>
2011-05-18 18:08:27 +0200

    package: Add libiqrf package
    Libiqrf is library written in C which implement specific iqrf
    protocol to communicate with iqrf usb devices (http://iqrf.org).
    
    [Peter: select libusb, fix Config.in indentation]
    Signed-off-by: Marek Belisko <marek.belisko@open-nandra.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libiqrf/libiqrf.mk
@@ -0,0 +1,14 @@
+#############################################################
+#
+# libiqrf library
+#
+#############################################################
+
+LIBIQRF_VERSION = v0.1.0
+LIBIQRF_SITE = git://github.com/nandra/libiqrf.git
+LIBIQRF_INSTALL_STAGING = YES
+
+LIBIQRF_DEPENDENCIES = libusb
+
+$(eval $(call AUTOTARGETS,package,libiqrf))
+
