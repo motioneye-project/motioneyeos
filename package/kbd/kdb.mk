383074a6335d310918aa11430341614dc79580a5
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-05-22 16:32:49 +0200

    kbd: make sure to link against libintl when needed
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/kbd/kdb.mk
+++ package/kbd/kdb.mk
@@ -2,4 +2,6 @@ KBD_VERSION = 1.15.2
 KBD_SOURCE = kbd-$(KBD_VERSION).tar.gz
 KBD_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kbd
 
+KBD_DEPENDENCIES = $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext libintl)
+
 $(eval $(call AUTOTARGETS,package,kbd))
