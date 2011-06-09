e446aa072a9504906dc01070847a9048410e218f
Yegor Yefremov <yegor_sub1@visionsystems.de>
2011-06-09 14:19:41 +0200

    ethtool: bump to 2.6.39
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ETHTOOL_VERSION = 2.6.38
+ETHTOOL_VERSION = 2.6.39
 ETHTOOL_SITE = $(BR2_KERNEL_MIRROR)/software/network/ethtool/
 
 $(eval $(call AUTOTARGETS,package,ethtool))
