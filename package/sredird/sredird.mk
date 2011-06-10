c324cf05be628b5506a1077a711a59a7a501b24d
Peter Korsgaard <jacmet@sunsite.dk>
2011-06-10 14:15:48 +0200

    package: add sredird
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/sredird/sredird.mk
@@ -0,0 +1,27 @@
+#############################################################
+#
+# sredird
+#
+#############################################################
+
+SREDIRD_VERSION = 2.2.1-1.1
+SREDIRD_SOURCE = sredird_$(SREDIRD_VERSION).tar.gz
+SREDIRD_SITE = $(BR2_DEBIAN_MIRROR)/debian/pool/main/s/sredird
+
+define SREDIRD_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define SREDIRD_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/sredird $(TARGET_DIR)/usr/sbin/sredird
+endef
+
+define SREDIRD_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/sbin/sredird
+endef
+
+define SREDIRD_CLEAN_CMDS
+	rm -f $(@D)/sredird
+endef
+
+$(eval $(call GENTARGETS,package,sredird))
