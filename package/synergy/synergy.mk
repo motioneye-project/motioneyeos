3790e3ceed72f13767cdc41f8c05188d487c3a5e
Peter Korsgaard <jacmet@sunsite.dk>
2011-06-14 08:49:00 +0200

    synergy: ensure cross X11 headers / libraries are used
    Otherwise configure would check for Xtst support on the host.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/synergy/synergy.mk
+++ package/synergy/synergy.mk
@@ -7,9 +7,8 @@
 SYNERGY_VERSION = 1.3.1
 SYNERGY_SOURCE = synergy-$(SYNERGY_VERSION).tar.gz
 SYNERGY_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/synergy2/
-SYNERGY_AUTORECONF = NO
-SYNERGY_INSTALL_STAGING = NO
-SYNERGY_INSTALL_TARGET = YES
+SYNERGY_CONF_OPT = --x-includes=$(STAGING_DIR)/usr/include/X11 \
+                   --x-libraries=$(STAGING_DIR)/usr/lib
 
 SYNERGY_DEPENDENCIES = xserver_xorg-server xlib_libXtst
 
