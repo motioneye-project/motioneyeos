6e53e059c82ba0ac235a5ca732a40b8999a7e65e
Maxime Ripard <maxime.ripard@free-electrons.com>
2011-06-29 15:46:21 +0200

    package: add rsyslog
    Rsyslog is a powerful and flexible syslog daemon.
    
    [Peter: whitespace, rename initscript, allow platform override of conf file]
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/rsyslog/rsyslog.mk
@@ -0,0 +1,26 @@
+#############################################################
+#
+# rsyslog
+#
+#############################################################
+RSYSLOG_VERSION = 5.8.0
+RSYSLOG_SITE = http://rsyslog.com/files/download/rsyslog/
+RSYSLOG_DEPENDENCIES = zlib
+
+# Build after Busybox
+ifeq ($(BR2_PACKAGE_BUSYBOX),y)
+	RSYSLOG_DEPENDENCIES += busybox
+endif
+
+define RSYSLOG_INSTALL_CONF_SCRIPT
+	$(INSTALL) -m 0755 -D package/rsyslog/S01rsyslog \
+		$(TARGET_DIR)/etc/init.d/S01rsyslog
+	[ -f $(TARGET_DIR)/etc/rsyslog.conf ] || \
+		$(INSTALL) -m 0644 -D $(@D)/rsyslog.conf \
+			$(TARGET_DIR)/etc/rsyslog.conf
+	mkdir -p $(TARGET_DIR)/etc/rsyslog.d
+endef
+
+RSYSLOG_POST_INSTALL_TARGET_HOOKS += RSYSLOG_INSTALL_CONF_SCRIPT
+
+$(eval $(call AUTOTARGETS,package,rsyslog))
