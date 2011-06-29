266c5cdaeb1c27fec4b72b43200162b30bf356da
Maxime Ripard <maxime.ripard@free-electrons.com>
2011-06-29 15:46:17 +0200

    package: add stunnel
    Stunnel is an SSL tunnelling proxy.
    
    [Peter: misc fixes, Config.in, allow skeleton override of stunnel.conf]
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/stunnel/stunnel.mk
@@ -0,0 +1,25 @@
+#############################################################
+#
+# stunnel
+#
+#############################################################
+
+STUNNEL_VERSION = 4.36
+STUNNEL_SITE = http://ftp.nluug.nl/pub/networking/stunnel/obsolete/4.x/
+STUNNEL_DEPENDENCIES = openssl
+
+STUNNEL_CONF_OPT += \
+	--with-ssl=$(STAGING_DIR)/usr \
+	--with-threads=fork
+
+define STUNNEL_INSTALL_CONF_SCRIPT
+	$(INSTALL) -m 0755 -D package/stunnel/S50stunnel $(TARGET_DIR)/etc/init.d/S50stunnel
+	[ -f $(TARGET_DIR)/etc/stunnel/stunnel.conf ] || \
+		$(INSTALL) -m 0644 -D $(@D)/tools/stunnel.conf \
+			$(TARGET_DIR)/etc/stunnel/stunnel.conf
+	rm -f $(TARGET_DIR)/etc/stunnel/stunnel.conf-sample
+endef
+
+STUNNEL_POST_INSTALL_TARGET_HOOKS += STUNNEL_INSTALL_CONF_SCRIPT
+
+$(eval $(call AUTOTARGETS,package,stunnel))
