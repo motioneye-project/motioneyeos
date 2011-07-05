4346902ca9a0e91e915ba40a50a66d66941d3c67
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-07-05 21:54:02 +0200

    at91dataflashboot: convert to GENTARGETS
    This commit converts the at91dataflashboot bootloader build process to
    GENTARGETS. It also does the following changes :
    
     * As the package directory name is at91dataflashboot, the
       configuration options and make variables are renamed to
       AT91DATAFLASHBOOT instead of DATAFLASHBOOT.
    
     * Since the board selection in Kconfig wasn't used anywhere, get rid
       of it. at91dataflashboot is only for AT91RM9200 apparently anyway.
    
    [Peter: add ':=' -> '=' change]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- boot/at91dataflashboot/at91dataflashboot.mk
+++ boot/at91dataflashboot/at91dataflashboot.mk
@@ -3,44 +3,29 @@
 # DataFlashBoot
 #
 #############################################################
-DATAFLASHBOOT_VERSION:=1.05
-DATAFLASHBOOT_NAME:=DataflashBoot-$(DATAFLASHBOOT_VERSION)
-DATAFLASHBOOT_SITE:=ftp://www.at91.com/pub/buildroot/
-DATAFLASHBOOT_SOURCE:=$(DATAFLASHBOOT_NAME).tar.bz2
-DATAFLASHBOOT_DIR:=$(BUILD_DIR)/at91dataflashboot-$(DATAFLASHBOOT_VERSION)
-DATAFLASHBOOT_BINARY:=$(DATAFLASHBOOT_NAME).bin
 
-$(DL_DIR)/$(DATAFLASHBOOT_SOURCE):
-	 $(call DOWNLOAD,$(DATAFLASHBOOT_SITE),$(DATAFLASHBOOT_SOURCE))
+AT91DATAFLASHBOOT_VERSION = 1.05
+AT91DATAFLASHBOOT_SOURCE = DataflashBoot-$(AT91DATAFLASHBOOT_VERSION).tar.bz2
+AT91DATAFLASHBOOT_SITE = ftp://www.at91.com/pub/buildroot/
 
-$(DATAFLASHBOOT_DIR)/.unpacked: $(DL_DIR)/$(DATAFLASHBOOT_SOURCE)
-	mkdir -p $(@D)
-	$(BZCAT) $(DL_DIR)/$(DATAFLASHBOOT_SOURCE) | tar $(TAR_STRIP_COMPONENTS)=1 -C $(@D) $(TAR_OPTIONS) -
-	touch $@
+AT91DATAFLASHBOOT_INSTALL_TARGET = NO
+AT91DATAFLASHBOOT_INSTALL_IMAGES = YES
 
-$(DATAFLASHBOOT_DIR)/.patched: $(DATAFLASHBOOT_DIR)/.unpacked
-	toolchain/patch-kernel.sh $(@D) boot/at91dataflashboot \
-		at91dataflashboot-$(DATAFLASHBOOT_VERSION)-\*.patch
-	touch $@
+define AT91DATAFLASHBOOT_BUILD_CMDS
+	make -C $(@D) CROSS_COMPILE=$(TARGET_CROSS)
+endef
 
-$(DATAFLASHBOOT_DIR)/$(DATAFLASHBOOT_BINARY): $(DATAFLASHBOOT_DIR)/.patched
-	ls $(DATAFLASHBOOT_DIR)/.unpacked
-	make -C $(DATAFLASHBOOT_DIR) CROSS_COMPILE=$(TARGET_CROSS)
+define AT91DATAFLASHBOOT_INSTALL_IMAGES_CMDS
+	cp $(@D)/DataflashBoot-$(AT91DATAFLASHBOOT_VERSION).bin $(BINARIES_DIR)
+endef
 
-dataflashboot-clean:
-	make -C $(DATAFLASHBOOT_DIR) clean
-
-dataflashboot-dirclean:
-	rm -rf $(DATAFLASHBOOT_DIR)
-
-dataflash:	 $(DATAFLASHBOOT_DIR)/$(DATAFLASHBOOT_BINARY)
-	cp $(DATAFLASHBOOT_DIR)/$(DATAFLASHBOOT_BINARY) $(BINARIES_DIR)
+$(eval $(call GENTARGETS,boot,at91dataflashboot))
 
 #############################################################
 #
 # Toplevel Makefile options
 #
 #############################################################
-ifeq ($(BR2_TARGET_DATAFLASHBOOT),y)
-TARGETS+=dataflash
+ifeq ($(BR2_TARGET_AT91DATAFLASHBOOT),y)
+TARGETS+=at91dataflashboot
 endif
