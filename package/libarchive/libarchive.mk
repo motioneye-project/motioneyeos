9eb70889bc7ec28790ac488a8edf5cd4404c2467
Peter Korsgaard <jacmet@sunsite.dk>
2011-07-10 19:52:52 +0200

    libarchive: add optional libxml2 support
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libarchive/libarchive.mk
+++ package/libarchive/libarchive.mk
@@ -14,8 +14,14 @@ LIBARCHIVE_DEPENDENCIES = zlib
 endif
 
 LIBARCHIVE_CONF_OPT = \
-	--without-xml2 \
 	$(if $(BR2_PACKAGE_LIBARCHIVE_BSDTAR),--enable-bsdtar,--disable-bsdtar) \
 	$(if $(BR2_PACKAGE_LIBARCHIVE_BSDCPIO),--enable-bsdcpio,--disable-bsdcpio)
+
+ifeq ($(BR2_PACKAGE_LIBXML2),y)
+LIBARCHIVE_DEPENDENCIES += libxml2
+LIBARCHIVE_CONF_ENV += XML2_CONFIG=$(STAGING_DIR)/usr/bin/xml2-config
+else
+LIBARCHIVE_CONF_OPT += --without-xml2
+endif
 
 $(eval $(call AUTOTARGETS,package,libarchive))
