9c6106f10494e539f00b1fc117801171348bd4bd
Daniel Mack <zonque@gmail.com>
2011-07-11 15:34:27 +0200

    package: add gnutls
    [Peter: move to crypto section]
    Signed-off-by: Daniel Mack <zonque@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/gnutls/gnutls.mk
@@ -0,0 +1,14 @@
+#############################################################
+#
+# gnutls
+#
+#############################################################
+
+GNUTLS_VERSION = 2.8.6
+GNUTLS_SOURCE = gnutls-$(GNUTLS_VERSION).tar.bz2
+GNUTLS_SITE = http://ftp.gnu.org/gnu/gnutls/
+GNUTLS_DEPENDENCIES = libgcrypt
+GNUTLS_CONF_OPT += --without-libgcrypt-prefix
+GNUTLS_INSTALL_STAGING = YES
+
+$(eval $(call AUTOTARGETS,package,gnutls))
