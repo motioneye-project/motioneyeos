aa0c4d4e48c615b1eab0b10296680b4a047c500e
Peter Korsgaard <jacmet@sunsite.dk>
2011-07-13 08:09:41 +0200

    libargtable2: fix source tarball name
    Last minute brown paper bag bug.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libargtable2/libargtable2.mk
+++ package/libargtable2/libargtable2.mk
@@ -5,6 +5,7 @@
 #############################################################
 
 LIBARGTABLE2_VERSION = 13
+LIBARGTABLE2_SOURCE = argtable2-$(LIBARGTABLE2_VERSION).tar.gz
 LIBARGTABLE2_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/argtable/
 
 LIBARGTABLE2_INSTALL_STAGING = YES
