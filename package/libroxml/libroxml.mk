b28de60c24ad4bef4ff7a6ad51cbb6e79906c868
Peter Korsgaard <jacmet@sunsite.dk>
2011-07-13 12:44:42 +0200

    package: add libroxml
    Closes #3595
    
    Based on patch by Tristan Lelong <tristan.lelong@blunderer.org>.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libroxml/libroxml.mk
@@ -0,0 +1,33 @@
+#
+# libroxml
+#
+
+LIBROXML_VERSION = 2.0.3
+LIBROXML_SITE = http://libroxml.googlecode.com/files
+LIBROXML_INSTALL_STAGING = YES
+
+define LIBROXML_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) OPTIM= -C $(@D) V=1 all
+endef
+
+define LIBROXML_INSTALL_STAGING_CMDS
+	$(MAKE) DESTDIR=$(STAGING_DIR) -C $(@D) install
+endef
+
+define LIBROXML_INSTALL_TARGET_CMDS
+	$(MAKE) DESTDIR=$(TARGET_DIR) -C $(@D) install
+endef
+
+define LIBROXML_UNINSTALL_STAGING_CMDS
+	$(MAKE) DESTDIR=$(STAGING_DIR) -C $(@D) uninstall
+endef
+
+define LIBROXML_UNINSTALL_TARGET_CMDS
+	$(MAKE) DESTDIR=$(TARGET_DIR) -C $(@D) uninstall
+endef
+
+define LIBROXML_CLEAN_CMDS
+	-$(MAKE) -C $(@D) clean
+endef
+
+$(eval $(call GENTARGETS,package,libroxml))
