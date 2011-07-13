5739c7fac6ec95fd6394e33a0f28ac663f2c8178
Sven Neumann <s.neumann@raumfeld.com>
2011-07-13 18:54:48 +0200

    gvfs: drop dbus-glib dependency
    Drop the dbus-glib dependency as gvfs uses dbus directly.
    
    Also change dependency on the XML parser for dbus to be more
    in sync with the dbus package.
    
    [Peter: drop expat|libxml2 deps as shared-mime-type already selects libxml2]
    Signed-off-by: Sven Neumann <s.neumann@raumfeld.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gvfs/gvfs.mk
+++ package/gvfs/gvfs.mk
@@ -11,7 +11,7 @@ GVFS_SITE = http://ftp.gnome.org/pub/GNOME/sources/gvfs/$(GVFS_VERSION_MAJOR)
 GVFS_INSTALL_STAGING = NO
 GVFS_INSTALL_TARGET = YES
 GVFS_AUTORECONF = NO
-GVFS_DEPENDENCIES = host-pkg-config host-libglib2 libglib2 dbus-glib shared-mime-info
+GVFS_DEPENDENCIES = host-pkg-config host-libglib2 libglib2 dbus shared-mime-info
 
 GVFS_CONF_OPT = \
 	--disable-gconf			\
