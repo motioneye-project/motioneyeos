d2b438cd1409dc2a61cd3428ec6da6a2f66633af
Sven Neumann <s.neumann@raumfeld.com>
2011-07-13 18:54:47 +0200

    gvfs: update to latest upstream version (1.8.2)
    Signed-off-by: Sven Neumann <s.neumann@raumfeld.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gvfs/gvfs.mk
+++ package/gvfs/gvfs.mk
@@ -3,8 +3,8 @@
 # gvfs
 #
 #############################################################
-GVFS_VERSION_MAJOR = 1.6
-GVFS_VERSION_MINOR = 6
+GVFS_VERSION_MAJOR = 1.8
+GVFS_VERSION_MINOR = 2
 GVFS_VERSION = $(GVFS_VERSION_MAJOR).$(GVFS_VERSION_MINOR)
 GVFS_SOURCE = gvfs-$(GVFS_VERSION).tar.gz
 GVFS_SITE = http://ftp.gnome.org/pub/GNOME/sources/gvfs/$(GVFS_VERSION_MAJOR)
