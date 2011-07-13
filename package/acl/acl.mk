41d2a1477233231ae0445d5ac07320290b686b4e
Yegor Yefremov <yegor_sub1@visionsystems.de>
2011-07-13 11:13:59 +0200

    New package: acl
    POSIX Access Control Lists, which are used to define more
    fine-grained discretionary access rights for files and
    directories.
    
    [Peter: needs largefile]
    Signed-off-by: Heiko Zuerker <smiley73@users.sourceforge.net>
    Signed-off-by: Daniel Mack <zonque@gmail.com>
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/acl/acl.mk
@@ -0,0 +1,28 @@
+#############################################################
+#
+# acl
+#
+#############################################################
+
+ACL_VERSION = 2.2.51
+ACL_SOURCE = acl-$(ACL_VERSION).src.tar.gz
+ACL_SITE = http://download.savannah.gnu.org/releases/acl
+ACL_INSTALL_STAGING = YES
+ACL_DEPENDENCIES = attr
+ACL_CONF_OPT = --enable-gettext=no
+
+# While the configuration system uses autoconf, the Makefiles are
+# hand-written and do not use automake. Therefore, we have to hack
+# around their deficiencies by passing installation paths.
+ACL_INSTALL_STAGING_OPT = 			\
+	prefix=$(STAGING_DIR)/usr 		\
+	exec_prefix=$(STAGING_DIR)/usr 		\
+	PKG_DEVLIB_DIR=$(STAGING_DIR)/usr/lib	\
+	install-dev install-lib
+
+ACL_INSTALL_TARGET_OPT = 			\
+	prefix=$(TARGET_DIR)/usr 		\
+	exec_prefix=$(TARGET_DIR)/usr 		\
+	install install-lib
+
+$(eval $(call AUTOTARGETS,package,acl))
