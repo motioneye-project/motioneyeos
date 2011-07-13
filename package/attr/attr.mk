691a5ead68deb5c77442f7a88ede479e57a9758c
Yegor Yefremov <yegor_sub1@visionsystems.de>
2011-07-13 11:12:29 +0200

    New package: attr
    Commands for Manipulating Filesystem Extended Attributes.
    
    [Peter: needs largefile]
    Signed-off-by: Heiko Zuerker <smiley73@users.sourceforge.net>
    Signed-off-by: Daniel Mack <zonque@gmail.com>
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/attr/attr.mk
@@ -0,0 +1,27 @@
+#############################################################
+#
+# attr
+#
+#############################################################
+
+ATTR_VERSION = 2.4.46
+ATTR_SOURCE = attr-$(ATTR_VERSION).src.tar.gz
+ATTR_SITE = http://download.savannah.gnu.org/releases/attr
+ATTR_INSTALL_STAGING = YES
+ATTR_CONF_OPT = --enable-gettext=no
+
+# While the configuration system uses autoconf, the Makefiles are
+# hand-written and do not use automake. Therefore, we have to hack
+# around their deficiencies by passing installation paths.
+ATTR_INSTALL_STAGING_OPT = 			\
+	prefix=$(STAGING_DIR)/usr 		\
+	exec_prefix=$(STAGING_DIR)/usr 		\
+	PKG_DEVLIB_DIR=$(STAGING_DIR)/usr/lib 	\
+	install-dev install-lib
+
+ATTR_INSTALL_TARGET_OPT = 			\
+	prefix=$(TARGET_DIR)/usr 		\
+	exec_prefix=$(TARGET_DIR)/usr 		\
+	install install-lib
+
+$(eval $(call AUTOTARGETS,package,attr))
