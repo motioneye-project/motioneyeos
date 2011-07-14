cf0114a41827ea80d4f70b3024a58f6eece4774c
Sven Neumann <s.neumann@raumfeld.com>
2011-07-14 09:32:39 +0200

    orc: new package
    Orc - the Oil Runtime Compiler - is the successor of liboil.
    There's a runtime library that is installed in staging and target
    and there's a host version that can be built if orcc, the ORC
    compiler is needed at build time.
    
    [Peter: minor tweaks]
    Signed-off-by: Sven Neumann <s.neumann@raumfeld.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/orc/orc.mk
@@ -0,0 +1,10 @@
+#############################################################
+#
+# orc
+#
+#############################################################
+ORC_VERSION = 0.4.14
+ORC_SITE = http://code.entropywave.com/download/orc/
+ORC_INSTALL_STAGING = YES
+
+$(eval $(call AUTOTARGETS,package,orc))
