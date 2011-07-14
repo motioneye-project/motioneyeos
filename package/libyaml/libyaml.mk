ca0c065cb7f50f7dfbb3f5c02361f5166d4101ee
Evgeni Dobrev <evgeni_dobrev@developer.bg>
2011-07-14 22:27:51 +0200

    package: add libyaml
    YAML 1.1 parser and emitter written in C
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libyaml/libyaml.mk
@@ -0,0 +1,12 @@
+#############################################################
+#
+# libyaml
+#
+#############################################################
+LIBYAML_VERSION = 0.1.4
+LIBYAML_SOURCE = yaml-$(LIBYAML_VERSION).tar.gz
+LIBYAML_SITE = http://pyyaml.org/download/libyaml/
+LIBYAML_INSTALL_STAGING = YES
+
+$(eval $(call AUTOTARGETS,package,libyaml))
+
