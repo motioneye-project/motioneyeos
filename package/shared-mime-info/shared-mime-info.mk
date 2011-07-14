b9187818daa207d4781a3e07659c08e66e23fd4b
Sven Neumann <s.neumann@raumfeld.com>
2011-07-14 09:16:28 +0200

    shared-mime-info: update to version 0.90
    Need to use $(MAKE1) as parallel builds appear to be broken.
    
    Signed-off-by: Sven Neumann <s.neumann@raumfeld.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/shared-mime-info/shared-mime-info.mk
+++ package/shared-mime-info/shared-mime-info.mk
@@ -3,7 +3,7 @@
 # shared-mime-info
 #
 #############################################################
-SHARED_MIME_INFO_VERSION = 0.60
+SHARED_MIME_INFO_VERSION = 0.90
 SHARED_MIME_INFO_SOURCE = shared-mime-info-$(SHARED_MIME_INFO_VERSION).tar.bz2
 SHARED_MIME_INFO_SITE = http://freedesktop.org/~hadess
 
@@ -15,11 +15,12 @@ SHARED_MIME_INFO_CONF_ENV = XMLLINT=$(HOST_DIR)/usr/bin/xmllint
 SHARED_MIME_INFO_DEPENDENCIES = host-pkg-config host-libglib2 host-libxml2 host-shared-mime-info libxml2 libglib2
 
 SHARED_MIME_INFO_CONF_OPT = --disable-update-mimedb
+SHARED_MIME_INFO_MAKE = $(MAKE1)
 
 HOST_SHARED_MIME_INFO_DEPENDENCIES = host-pkg-config host-intltool
 
-HOST_SHARED_MIME_INFO_CONF_OPT = \
-	--disable-update-mimedb
+HOST_SHARED_MIME_INFO_CONF_OPT = --disable-update-mimedb
+HOST_SHARED_MIME_INFO_MAKE = $(MAKE1)
 
 define SHARED_MIME_INFO_INSTALL_TARGET_CMDS
 	$(HOST_MAKE_ENV) $(SHARED_MIME_INFO_HOST_BINARY) $(STAGING_DIR)/usr/share/mime
