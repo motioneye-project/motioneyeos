ca99c04d2676ba66b670b0eb17ed5fe5d37db4e2
Peter Korsgaard <jacmet@sunsite.dk>
2011-07-22 13:51:45 +0200

    ipset: ensure target kernel headers are used
    ipsets configure script checks the kernel headers to ensure the kernel
    is new enough / has patch applied, and errors out if this is not so.
    
    Ensure that the target kernel headers are searched, rather than
    what is available on the build host.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ipset/ipset.mk
+++ package/ipset/ipset.mk
@@ -9,5 +9,6 @@ IPSET_SOURCE = ipset-$(IPSET_VERSION).tar.bz2
 IPSET_SITE = http://ipset.netfilter.org
 IPSET_AUTORECONF = YES
 IPSET_DEPENDENCIES = libmnl host-pkg-config
+IPSET_CONF_OPT = --with-ksource=$(LINUX_HEADERS_DIR)
 
 $(eval $(call AUTOTARGETS,package,ipset))
