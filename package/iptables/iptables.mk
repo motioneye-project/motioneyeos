332dd196cad4fa1a9b0257b08091b902efa5a7c2
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-07-22 15:02:48 -0300

    iptables: fixup multitarget binary links
    Newer vesions renamed iptables-multi to xtables-multi so fix
    the symlinks accordingly.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/iptables/iptables.mk
+++ package/iptables/iptables.mk
@@ -14,15 +14,15 @@ IPTABLES_DEPENDENCIES = host-pkg-config
 IPTABLES_CONF_OPT = --libexecdir=/usr/lib --with-kernel=$(LINUX_HEADERS_DIR)
 
 define IPTABLES_TARGET_SYMLINK_CREATE
-	ln -sf iptables-multi $(TARGET_DIR)/usr/sbin/iptables
-	ln -sf iptables-multi $(TARGET_DIR)/usr/sbin/iptables-save
-	ln -sf iptables-multi $(TARGET_DIR)/usr/sbin/iptables-restore
+	ln -sf xtables-multi $(TARGET_DIR)/usr/sbin/iptables
+	ln -sf xtables-multi $(TARGET_DIR)/usr/sbin/iptables-save
+	ln -sf xtables-multi $(TARGET_DIR)/usr/sbin/iptables-restore
 endef
 
 define IPTABLES_TARGET_IPV6_SYMLINK_CREATE
-	ln -sf ip6tables-multi $(TARGET_DIR)/usr/sbin/ip6tables
-	ln -sf ip6tables-multi $(TARGET_DIR)/usr/sbin/ip6tables-save
-	ln -sf ip6tables-multi $(TARGET_DIR)/usr/sbin/ip6tables-restore
+	ln -sf xtables-multi $(TARGET_DIR)/usr/sbin/ip6tables
+	ln -sf xtables-multi $(TARGET_DIR)/usr/sbin/ip6tables-save
+	ln -sf xtables-multi $(TARGET_DIR)/usr/sbin/ip6tables-restore
 endef
 
 define IPTABLES_TARGET_IPV6_REMOVE
@@ -40,6 +40,7 @@ endif
 define IPTABLES_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/bin/iptables-xml
 	rm -f $(TARGET_DIR)/usr/sbin/iptables* $(TARGET_DIR)/usr/sbin/ip6tables*
+	rm -f $(TARGET_DIR)/usr/sbin/xtables-multi
 	rm -rf $(TARGET_DIR)/usr/lib/xtables
 endef
 
