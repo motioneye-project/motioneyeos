4b9bd9736e97d84a86bb52844a99e74f157980c0
Frederic Bassaler <frederic.bassaler@gmail.com>
2011-08-03 19:23:01 +0200

    package: add Fluxbox 1.3.1 window manager
    [Peter: fix build without iconv, whitespace fixes]
    Signed-off-by: Frederic Bassaler <frederic.bassaler@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/fluxbox/fluxbox.mk
@@ -0,0 +1,16 @@
+#############################################################
+#
+# FLUXBOX
+#
+#############################################################
+
+FLUXBOX_VERSION = 1.3.1
+FLUXBOX_SOURCE = fluxbox-$(FLUXBOX_VERSION).tar.bz2
+FLUXBOX_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/fluxbox/
+
+FLUXBOX_CONF_OPT = --x-includes=$(STAGING_DIR)/usr/include/X11 \
+		   --x-libraries=$(STAGING_DIR)/usr/lib
+
+FLUXBOX_DEPENDENCIES = xlib_libX11 $(if $(BR2_PACKAGE_LIBICONV),libiconv)
+
+$(eval $(call AUTOTARGETS,package,fluxbox))
