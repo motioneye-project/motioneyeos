fc57b341d9e3ce65d6df4e3ec14d87eb2ff361be
Peter Korsgaard <jacmet@sunsite.dk>
2011-08-04 21:15:40 +0200

    orc: target install: cleanup unused files
    orcc is only needed when developing.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/orc/orc.mk
+++ package/orc/orc.mk
@@ -7,4 +7,18 @@ ORC_VERSION = 0.4.14
 ORC_SITE = http://code.entropywave.com/download/orc/
 ORC_INSTALL_STAGING = YES
 
+define ORC_REMOVE_BUGREPORT
+	rm -f $(TARGET_DIR)/usr/bin/orc-bugreport
+endef
+
+define ORC_REMOVE_DEVFILES
+	rm -f $(TARGET_DIR)/usr/bin/orcc
+endef
+
+ORC_POST_INSTALL_TARGET_HOOKS += ORC_REMOVE_BUGREPORT
+
+ifneq ($(BR2_HAVE_DEVFILES),y)
+ORC_POST_INSTALL_TARGET_HOOKS += ORC_REMOVE_DEVFILES
+endif
+
 $(eval $(call AUTOTARGETS,package,orc))
