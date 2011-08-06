d5368064c2556a5df9e04538c485453f53936342
Murat Demirten <mdemirten@yh.com.tr>
2011-08-07 00:55:07 +0300

    New package: libgeotiff
    Libgeotiff is an open source library normally hosted on top of
    libtiff for reading, and writing GeoTIFF information tags.
    
    Signed-off-by: Murat Demirten <mdemirten@yh.com.tr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libgeotiff/libgeotiff.mk
@@ -0,0 +1,12 @@
+#############################################################
+#
+# libgeotiff
+#
+#############################################################
+LIBGEOTIFF_VERSION = 1.3.0
+LIBGEOTIFF_SOURCE = libgeotiff-$(LIBGEOTIFF_VERSION).tar.gz
+LIBGEOTIFF_SITE = http://download.osgeo.org/geotiff/libgeotiff
+LIBGEOTIFF_DEPENDENCIES = tiff
+LIBGEOTIFF_INSTALL_STAGING = YES
+
+$(eval $(call AUTOTARGETS,package,libgeotiff))
