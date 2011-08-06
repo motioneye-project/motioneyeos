2c85cb9308ed907a8a8e487be8c7ab971a1fb126
Peter Korsgaard <jacmet@sunsite.dk>
2011-08-06 12:06:41 +0200

    xfont_font-misc-misc: remove mapfiles workaround
    Not needed anymore after the recent pkg-config mapfiles fix.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xfont_font-misc-misc/xfont_font-misc-misc.mk
+++ package/x11r7/xfont_font-misc-misc/xfont_font-misc-misc.mk
@@ -12,11 +12,5 @@ XFONT_FONT_MISC_MISC_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) MKFONTSCALE=$(
 XFONT_FONT_MISC_MISC_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) MKFONTSCALE=$(HOST_DIR)/usr/bin/mkfontscale MKFONTDIR=$(HOST_DIR)/usr/bin/mkfontdir install-data
 XFONT_FONT_MISC_MISC_DEPENDENCIES = xfont_font-util host-xfont_font-util host-xapp_mkfontscale host-xapp_mkfontdir host-xapp_bdftopcf
 
-define XFONT_FONT_MISC_MISC_MAPFILES_PATH_FIX
-	$(SED) 's|UTIL_DIR = |UTIL_DIR = $(STAGING_DIR)|' $(@D)/Makefile
-endef
-
-XFONT_FONT_MISC_MISC_POST_CONFIGURE_HOOKS += XFONT_FONT_MISC_MISC_MAPFILES_PATH_FIX
-
 $(eval $(call AUTOTARGETS,package/x11r7,xfont_font-misc-misc))
 
