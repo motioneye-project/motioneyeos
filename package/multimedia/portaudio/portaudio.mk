a991e3e143408d573aa3d0a3090cabfac65a570e
Murat Demirten <mdemirten@yh.com.tr>
2011-08-07 01:42:07 +0300

    package: add portaudio
    PortAudio is a free, cross-platform, open-source, audio I/O library.
    
    [Peter: fix whitespace, dependencies, alsa-lib name]
    Signed-off-by: Murat Demirten <mdemirten@yh.com.tr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/multimedia/portaudio/portaudio.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# portaudio
+#
+################################################################################
+
+PORTAUDIO_VERSION = V19
+PORTAUDIO_SITE = http://www.portaudio.com/archives
+PORTAUDIO_SOURCE = pa_stable_v19_20110326.tgz
+PORTAUDIO_INSTALL_STAGING = YES
+
+PORTAUDIO_DEPENDENCIES = \
+       $(if $(BR2_PACKAGE_PORTAUDIO_WITH_ALSA),alsa-lib)
+
+PORTAUDIO_CONF_OPT = \
+       $(if $(BR2_PACKAGE_PORTAUDIO_ALSA),--with-alsa,--without-alsa) \
+       $(if $(BR2_PACKAGE_PORTAUDIO_OSS),--with-oss,--without-oss) \
+       $(if $(BR2_PACKAGE_PORTAUDIO_CXX),--enable-cxx,--disable-cxx)
+
+$(eval $(call AUTOTARGETS,package/multimedia,portaudio))
