efd5ca7b262f67c5343267eb3c13b517568f8126
Yegor Yefremov <yegor_sub1@visionsystems.de>
2011-08-08 09:10:29 +0200

    ti-utils: bump version and clean up
    [Peter: fixup whitespace, simplify target install]
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ti-utils/ti-utils.mk
+++ package/ti-utils/ti-utils.mk
@@ -4,26 +4,28 @@
 #
 #############################################################
 
-TI_UTILS_VERSION = fbceab8f228cff80fd29b830bb85a188c69def08
+TI_UTILS_VERSION = 06dbdb2727354b5f3ad7c723897f40051fddee49
 TI_UTILS_SITE = git://github.com/gxk/ti-utils.git
 TI_UTILS_DEPENDENCIES = libnl
 
 define TI_UTILS_BUILD_CMDS
-	$(MAKE1) NFSROOT="$(STAGING_DIR)" CC="$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS)" \
+	$(MAKE1) NFSROOT="$(STAGING_DIR)" \
+		CC="$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS)" \
 		-C $(@D) all
 endef
 
 define TI_UTILS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/calibrator \
+	$(INSTALL) -m 0755 -D $(@D)/calibrator \
 		$(TARGET_DIR)/usr/bin/calibrator
-	$(INSTALL) -m 0755 $(@D)/scripts/go.sh \
-		$(TARGET_DIR)/usr/bin/go.sh
+	$(INSTALL) -m 0755 -D $(@D)/scripts/go.sh \
+		$(TARGET_DIR)/usr/share/ti-utils/scripts/go.sh
+
+	cp -r $(@D)/ini_files $(TARGET_DIR)/usr/share/ti-utils
 endef
 
 define TI_UTILS_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/bin/calibrator
-	rm -f $(TARGET_DIR)/usr/bin/go.sh
+	rm -fr $(TARGET_DIR)/usr/share/ti-utils
 endef
 
 $(eval $(call GENTARGETS,package,ti-utils))
-
