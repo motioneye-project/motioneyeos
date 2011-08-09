8e837d5dfe6c40ed3e3cdf0d7c9db163b0c8c305
Yann E. MORIN <yann.morin.1998@anciens.enib.fr>
2011-08-10 00:12:49 +0200

    ntfs-3g: remove --program-prefix
    --program-prefix is now set in the auto-target infrastructure.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ntfs-3g/ntfs-3g.mk
+++ package/ntfs-3g/ntfs-3g.mk
@@ -7,7 +7,7 @@
 NTFS_3G_VERSION = 2010.10.2
 NTFS_3G_SOURCE = ntfs-3g-$(NTFS_3G_VERSION).tgz
 NTFS_3G_SITE = http://tuxera.com/opensource
-NTFS_3G_CONF_OPT = --disable-ldconfig --program-prefix=""
+NTFS_3G_CONF_OPT = --disable-ldconfig
 NTFS_3G_INSTALL_STAGING = YES
 
 $(eval $(call AUTOTARGETS,package,ntfs-3g))
