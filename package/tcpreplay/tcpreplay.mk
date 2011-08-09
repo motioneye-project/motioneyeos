aa6e5aed3a702f47221e4614be38e4376e240343
Yann E. MORIN <yann.morin.1998@anciens.enib.fr>
2011-08-10 00:12:53 +0200

    tcpreplay: remove --program-prefix
    --program-prefix is now set in the auto-target infrastructure.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tcpreplay/tcpreplay.mk
+++ package/tcpreplay/tcpreplay.mk
@@ -7,7 +7,7 @@
 TCPREPLAY_VERSION = 3.4.3
 TCPREPLAY_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/tcpreplay
 TCPREPLAY_CONF_ENV = tr_cv_libpcap_version=">= 0.7.0"
-TCPREPLAY_CONF_OPT = --program-prefix="" --with-libpcap=$(STAGING_DIR)/usr
+TCPREPLAY_CONF_OPT = --with-libpcap=$(STAGING_DIR)/usr
 
 TCPREPLAY_DEPENDENCIES = uclibc libpcap
 
