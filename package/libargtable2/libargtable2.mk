e127003af528678ea4b6c9eaca9af5647c25bfc2
Yann E. MORIN <yann.morin.1998@anciens.enib.fr>
2011-08-10 00:12:50 +0200

    libargtable2: remove --program-prefix
    --program-prefix is now set in the auto-target infrastructure.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libargtable2/libargtable2.mk
+++ package/libargtable2/libargtable2.mk
@@ -9,8 +9,7 @@ LIBARGTABLE2_SOURCE = argtable2-$(LIBARGTABLE2_VERSION).tar.gz
 LIBARGTABLE2_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/argtable/
 
 LIBARGTABLE2_INSTALL_STAGING = YES
-LIBARGTABLE2_CONF_OPT= --program-prefix="" \
-		--disable-example \
+LIBARGTABLE2_CONF_OPT= --disable-example \
 		--disable-kernel-module \
 		--enable-lib \
 		--enable-util
