2caafe6be7bc61569317a35b4b36880c7c6761b0
Yann E. MORIN <yann.morin.1998@anciens.enib.fr>
2011-08-10 00:12:42 +0200

    lzop: remove --program-prefix
    --program-prefix is now set in the auto-target infrastructure.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/lzop/lzop.mk
+++ package/lzop/lzop.mk
@@ -6,7 +6,6 @@
 LZOP_VERSION:=1.02rc1
 LZOP_SOURCE:=lzop-$(LZOP_VERSION).tar.gz
 LZOP_SITE:=http://www.lzop.org/download/
-LZOP_CONF_OPT:=--program-prefix=""
 LZOP_DEPENDENCIES:=lzo
 
 $(eval $(call AUTOTARGETS,package,lzop))
