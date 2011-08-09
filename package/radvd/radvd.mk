dc1b29e9bca1218dc1bda67880dfc442e803bca1
Yann E. MORIN <yann.morin.1998@anciens.enib.fr>
2011-08-10 00:12:55 +0200

    radvd: remove --program-prefix
    --program-prefix is now set in the auto-target infrastructure.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@anciens.enib.fr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/radvd/radvd.mk
+++ package/radvd/radvd.mk
@@ -7,7 +7,6 @@ RADVD_VERSION:=1.6
 RADVD_SOURCE:=radvd-$(RADVD_VERSION).tar.gz
 RADVD_SITE:=http://www.litech.org/radvd/dist/
 RADVD_DEPENDENCIES:=flex host-flex
-RADVD_CONF_OPT:= --program-prefix=''
 
 define RADVD_INSTALL_INITSCRIPT
 	$(INSTALL) -m 0755 package/radvd/S50radvd $(TARGET_DIR)/etc/init.d
