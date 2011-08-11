d2f84e1623183aabc63f84c7105bf958c0a90914
Frederic Bassaler <frederic.bassaler@gmail.com>
2011-08-11 15:11:03 +0200

    freetype: bump to 2.4.6
    Security update.
    
    Signed-off-by: Frederic Bassaler <frederic.bassaler@gmail.com>
    Acked-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/freetype/freetype.mk
+++ package/freetype/freetype.mk
@@ -3,7 +3,7 @@
 # freetype
 #
 #############################################################
-FREETYPE_VERSION = 2.4.4
+FREETYPE_VERSION = 2.4.6
 FREETYPE_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/freetype
 FREETYPE_SOURCE = freetype-$(FREETYPE_VERSION).tar.bz2
 FREETYPE_INSTALL_STAGING = YES
