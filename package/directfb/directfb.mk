e35921b3a0d148098f08d2f0533b238a65ff3e86
Sven Neumann <s.neumann@raumfeld.com>
2011-08-11 12:44:07 +0200

    directfb: bump to version 1.4.13
    There's an even newer stable release available (1.4.14), but it
    has problems with cross-compilation that we are still resolving
    on the directfb-dev mailing-list.
    
    [Peter: rename ppc patch to match]
    Signed-off-by: Sven Neumann <s.neumann@raumfeld.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/directfb/directfb.mk
+++ package/directfb/directfb.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 DIRECTFB_VERSION_MAJOR = 1.4
-DIRECTFB_VERSION = $(DIRECTFB_VERSION_MAJOR).11
+DIRECTFB_VERSION = $(DIRECTFB_VERSION_MAJOR).13
 DIRECTFB_SITE = http://www.directfb.org/downloads/Core/DirectFB-$(DIRECTFB_VERSION_MAJOR)
 DIRECTFB_SOURCE = DirectFB-$(DIRECTFB_VERSION).tar.gz
 DIRECTFB_AUTORECONF = YES
