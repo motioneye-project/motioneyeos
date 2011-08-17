e1472e14491cea068e27b686174af2827d93cd8b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-08-17 16:27:37 -0300

    ipset: fixup kernel checks for external toolchains
    Add patch to kill kernel checks since we don't build the kernel
    modules anyway.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ipset/ipset.mk
+++ package/ipset/ipset.mk
@@ -9,6 +9,5 @@ IPSET_SOURCE = ipset-$(IPSET_VERSION).tar.bz2
 IPSET_SITE = http://ipset.netfilter.org
 IPSET_AUTORECONF = YES
 IPSET_DEPENDENCIES = libmnl host-pkg-config
-IPSET_CONF_OPT = --with-ksource=$(LINUX_HEADERS_DIR)
 
 $(eval $(call AUTOTARGETS,package,ipset))
