06c3e2159686a886e52b2522a47e60c300cfb7f7
Yegor Yefremov <yegor_sub1@visionsystems.de>
2011-08-30 15:36:11 +0200

    New package: bluez-utils
    Bluetooth utils often used in embedded system, add it to package.
    
    [Peter: fix dbus dependency]
    Signed-off-by: Bob Liu <lliubbo@gmail.com>
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/bluez_utils/bluez_utils.mk
@@ -0,0 +1,46 @@
+#############################################################
+#
+# bluez_utils
+#
+#############################################################
+BLUEZ_UTILS_VERSION = 4.96
+BLUEZ_UTILS_SOURCE = bluez-$(BLUEZ_UTILS_VERSION).tar.gz
+BLUEZ_UTILS_SITE = http://www.kernel.org/pub/linux/bluetooth/
+BLUEZ_UTILS_INSTALL_STAGING = YES
+BLUEZ_UTILS_DEPENDENCIES = dbus libglib2
+BLUEZ_UTILS_CONF_OPT = --enable-test --enable-tools
+
+# BlueZ 3.x compatibility
+ifeq ($(BR2_PACKAGE_BLUEZ_UTILS_COMPAT),y)
+BLUEZ_UTILS_CONF_OPT +=	\
+	--enable-hidd	\
+	--enable-pand	\
+	--enable-sdp	\
+	--enable-dund
+endif
+
+# audio support
+ifeq ($(BR2_PACKAGE_BLUEZ_UTILS_AUDIO),y)
+BLUEZ_UTILS_DEPENDENCIES +=	\
+	alsa-lib		\
+	libsndfile
+BLUEZ_UTILS_CONF_OPT +=	\
+	--enable-alsa	\
+	--enable-audio
+else
+BLUEZ_UTILS_CONF_OPT +=	\
+	--disable-alsa	\
+	--disable-audio
+endif
+
+# USB support
+ifeq ($(BR2_PACKAGE_BLUEZ_UTILS_USB),y)
+BLUEZ_UTILS_DEPENDENCIES += libusb
+BLUEZ_UTILS_CONF_OPT +=	\
+	--enable-usb
+else
+BLUEZ_UTILS_CONF_OPT +=	\
+	--disable-usb
+endif
+
+$(eval $(call AUTOTARGETS,package,bluez_utils))
