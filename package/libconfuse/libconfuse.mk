6f103474ad6387cb5a00dc3419efc52e78afe5a5
Dan Pattison <dan.pattison@ethertek.ca>
2011-09-01 07:47:49 -0700

    libconfuse: bump to version 2.7
    Old libconfuse version 2.6 is from year 2007 so bump to version 2.7
    Updated download URL in libconfuse.mk / Config.in
    Removed libconfuse-2.6-dont-use-werror.patch
    Changed := to just =
    
    Signed-off-by: Dan Pattison <dan.pattison@ethertek.ca>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libconfuse/libconfuse.mk
+++ package/libconfuse/libconfuse.mk
@@ -3,13 +3,14 @@
 # libconfuse
 #
 #############################################################
-LIBCONFUSE_VERSION:=2.6
-LIBCONFUSE_SOURCE:=confuse-$(LIBCONFUSE_VERSION).tar.gz
-LIBCONFUSE_SITE:=http://bzero.se/confuse/
-LIBCONFUSE_AUTORECONF:=NO
-LIBCONFUSE_INSTALL_STAGING:=YES
-LIBCONFUSE_INSTALL_TARGET:=YES
+LIBCONFUSE_VERSION = 2.7
+LIBCONFUSE_SOURCE = confuse-$(LIBCONFUSE_VERSION).tar.gz
+LIBCONFUSE_SITE = http://savannah.nongnu.org/download/confuse/
+LIBCONFUSE_AUTORECONF = NO
+LIBCONFUSE_INSTALL_STAGING = YES
 
-LIBCONFUSE_CONF_OPT:=--disable-rpath
+LIBCONFUSE_CONF_OPT = --disable-rpath
+
+LIBCONFUSE_DEPENDENCIES = uclibc
 
 $(eval $(call AUTOTARGETS,package,libconfuse))
