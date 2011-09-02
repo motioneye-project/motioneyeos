34d2239ac4d4d4b373de9c21231616883636c7f4
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-09-02 11:04:56 -0300

    sqlite: bump to version 3.7.7.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SQLITE_VERSION = 3070603
+SQLITE_VERSION = 3070701
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_VERSION).tar.gz
 SQLITE_SITE = http://www.sqlite.org
 SQLITE_INSTALL_STAGING = YES
