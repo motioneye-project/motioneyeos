2a49da16465ad2a1993b5a1736c70f6e1b1ac270
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-09-02 11:04:52 -0300

    libsndfile: bump to version 1.0.25
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/libsndfile/libsndfile.mk
+++ package/multimedia/libsndfile/libsndfile.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBSNDFILE_VERSION = 1.0.23
+LIBSNDFILE_VERSION = 1.0.25
 LIBSNDFILE_SITE = http://www.mega-nerd.com/libsndfile/files
 LIBSNDFILE_INSTALL_STAGING = YES
 
