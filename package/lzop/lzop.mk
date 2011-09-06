307c006695f24621a8e515f85d0cbbe4374afd89
Yegor Yefremov <yegor_sub1@visionsystems.de>
2011-09-06 11:46:21 +0200

    lzop : bump to 1.03
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/lzop/lzop.mk
+++ package/lzop/lzop.mk
@@ -3,9 +3,9 @@
 # lzop
 #
 #############################################################
-LZOP_VERSION:=1.02rc1
-LZOP_SOURCE:=lzop-$(LZOP_VERSION).tar.gz
-LZOP_SITE:=http://www.lzop.org/download/
-LZOP_DEPENDENCIES:=lzo
+LZOP_VERSION = 1.03
+LZOP_SOURCE = lzop-$(LZOP_VERSION).tar.gz
+LZOP_SITE = http://www.lzop.org/download/
+LZOP_DEPENDENCIES = lzo
 
 $(eval $(call AUTOTARGETS,package,lzop))
