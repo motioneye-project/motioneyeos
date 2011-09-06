3fac21ef8d794b47f731003b399e704349c54346
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2011-09-06 23:16:08 +0200

    cpio: fix boot with dynamic /dev
    Devtmpfs doesn't get automounted by the kernel when an initramfs is used.
    Since cpio archives are used almost exclusively as an initramfs, the same
    fix should be applied to it as for the initramfs.
    
    Cfr. commit 424888e47431db738f5f9b3c6392435bfce7a842 and
    10a130f91e5b947e2d2558443ff09b1686eac273.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- fs/cpio/cpio.mk
+++ fs/cpio/cpio.mk
@@ -4,12 +4,26 @@
 #
 #############################################################
 
-define ROOTFS_CPIO_INIT_SYMLINK
-	rm -f $(TARGET_DIR)/init
-	ln -s sbin/init $(TARGET_DIR)/init
+ifeq ($(BR2_ROOTFS_DEVICE_CREATION_STATIC),y)
+
+define ROOTFS_CPIO_ADD_INIT
+        if [ ! -e $(TARGET_DIR)/init ]; then \
+                ln -sf sbin/init $(TARGET_DIR)/init; \
+        fi
 endef
 
-ROOTFS_CPIO_PRE_GEN_HOOKS += ROOTFS_CPIO_INIT_SYMLINK
+else
+# devtmpfs does not get automounted when initramfs is used.
+# Add a pre-init script to mount it before running init
+define ROOTFS_CPIO_ADD_INIT
+        if [ ! -e $(TARGET_DIR)/init ]; then \
+                $(INSTALL) -m 0755 fs/cpio/init $(TARGET_DIR)/init; \
+        fi
+endef
+
+endif # BR2_ROOTFS_DEVICE_CREATION_STATIC
+
+ROOTFS_CPIO_PRE_GEN_HOOKS += ROOTFS_CPIO_ADD_INIT
 
 define ROOTFS_CPIO_CMD
 	cd $(TARGET_DIR) && find . | cpio --quiet -o -H newc > $$@
