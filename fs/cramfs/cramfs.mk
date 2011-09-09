ccd96a3559324b742f357ce6bd8969c6318fe94b
Maxime Ripard <maxime.ripard@free-electrons.com>
2011-09-09 16:05:07 +0200

    Cleanup cramfs package declaration.
    Cramfs still passes the device table to mkcramfs, which is irrelevant
    now with fakeroot, remove this behaviour and the associated patch.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- fs/cramfs/cramfs.mk
+++ fs/cramfs/cramfs.mk
@@ -9,10 +9,6 @@ else
 CRAMFS_OPTS=-l
 endif
 
-ifneq ($(ROOTFS_DEVICE_TABLE),)
-CRAMFS_OPTS += -D $(ROOTFS_DEVICE_TABLE)
-endif
-
 define ROOTFS_CRAMFS_CMD
  $(HOST_DIR)/usr/bin/mkcramfs -q $(CRAMFS_OPTS) $(TARGET_DIR) $$@
 endef
