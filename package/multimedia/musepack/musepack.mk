d5b35e2c700c7316c90a0aee9710382e8b10e2cb
Peter Korsgaard <jacmet@sunsite.dk>
2011-09-12 13:57:14 +0200

    musepack: disable parallel build
    The musepack build system contains a race condition, which gets triggered by
    high BR2_JLEVEL settings - It tries to link against libmpcdec before it is
    created:
    
    Linking C executable mpcgain
    /scratch/peko/host/usr/lib/gcc/arm-unknown-linux-uclibcgnueabi/4.4.6/../../../../arm-unknown-linux-uclibcgnueabi/bin/ld: cannot find -lmpcdec
    collect2: ld returned 1 exit status
    make[3]: *** [mpcgain/mpcgain] Error 1
    
    Fix it by disabling parallel build.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/musepack/musepack.mk
+++ package/multimedia/musepack/musepack.mk
@@ -9,5 +9,6 @@ MUSEPACK_SITE = http://files.musepack.net/source
 MUSEPACK_SOURCE = musepack_src_$(MUSEPACK_VERSION).tar.gz
 MUSEPACK_DEPENDENCIES = libcuefile libreplaygain
 MUSEPACK_INSTALL_STAGING = YES
+MUSEPACK_MAKE = $(MAKE1)
 
 $(eval $(call CMAKETARGETS,package/multimedia,musepack))
