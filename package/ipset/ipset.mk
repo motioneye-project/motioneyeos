065762c091db7b4efb9adfe7c820788c94f0d1ec
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-09-13 13:40:15 -0300

    ipset: bump to version 6.9.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ipset/ipset.mk
+++ package/ipset/ipset.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-IPSET_VERSION = 6.8
+IPSET_VERSION = 6.9.1
 IPSET_SOURCE = ipset-$(IPSET_VERSION).tar.bz2
 IPSET_SITE = http://ipset.netfilter.org
 IPSET_AUTORECONF = YES
