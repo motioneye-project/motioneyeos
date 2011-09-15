25c117fa2a6c0f8e58d5786e4ea9e1e00893d2dd
Peter Korsgaard <jacmet@sunsite.dk>
2011-09-15 15:34:19 +0200

    fuse: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/fuse/libfuse.mk
+++ package/fuse/libfuse.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBFUSE_VERSION = 2.8.5
+LIBFUSE_VERSION = 2.8.6
 LIBFUSE_SOURCE = fuse-$(LIBFUSE_VERSION).tar.gz
 LIBFUSE_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/fuse/
 
