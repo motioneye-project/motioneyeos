cbdb0a09224400c97632fa58e1a33d5901ff8f69
Peter Korsgaard <jacmet@sunsite.dk>
2011-09-17 14:03:33 +0200

    tslib: drop redundant configure options
    And cleanup while we're at it.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tslib/tslib.mk
+++ package/tslib/tslib.mk
@@ -3,15 +3,10 @@
 # tslib
 #
 #############################################################
-TSLIB_VERSION:=412d99d8b92c
-TSLIB_SITE:=git://github.com/kergoth/tslib.git
+TSLIB_VERSION = 412d99d8b92c
+TSLIB_SITE = git://github.com/kergoth/tslib.git
 TSLIB_AUTORECONF = YES
 TSLIB_INSTALL_STAGING = YES
-TSLIB_INSTALL_TARGET = YES
 TSLIB_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) LDFLAGS=-L$(STAGING_DIR)/usr/lib install
-
-TSLIB_CONF_OPT = \
-	--prefix=/usr	\
-	--sysconfdir=/etc
 
 $(eval $(call AUTOTARGETS,package,tslib))
