c20ab0fecada71f12b1e7f49a10c03b4250267a5
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-09-19 23:42:57 +0200

    cifs-utils: new package
    [Peter: fix Config.in]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/cifs-utils/cifs-utils.mk
@@ -0,0 +1,5 @@
+CIFS_UTILS_VERSION = 5.0
+CIFS_UTILS_SOURCE  = cifs-utils-$(CIFS_UTILS_VERSION).tar.bz2
+CIFS_UTILS_SITE    = ftp://ftp.samba.org/pub/linux-cifs/cifs-utils/
+
+$(eval $(call AUTOTARGETS,package,cifs-utils))
