53b26022965cdbeafe5e584d77354c68d229805b
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-09-19 22:53:07 +0200

    bridge-utils: normalize package name
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/bridge-utils/bridge-utils.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# bridge-utils
+#
+#############################################################
+
+BRIDGE_UTILS_VERSION = 1.5
+BRIDGE_UTILS_SOURCE = bridge-utils-$(BRIDGE_UTILS_VERSION).tar.gz
+BRIDGE_UTILS_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/bridge
+BRIDGE_UTILS_AUTORECONF = YES
+BRIDGE_UTILS_CONF_OPT = --with-linux-headers=$(LINUX_HEADERS_DIR)
+
+define BRIDGE_UTILS_UNINSTALL_TARGET_CMDS
+	rm -f $(addprefix $(TARGET_DIR)/usr/,lib/libbridge.a \
+		include/libbridge.h share/man/man8/brctl.8 sbin/brctl)
+endef
+
+$(eval $(call AUTOTARGETS,package,bridge-utils))
