a323d0eeabab7f86954d0b5e58d3d75acc6b6fda
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-09-19 22:53:24 +0200

    gtk2-theme-hicolor: remove useless definitions
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gtk2-themes/gtk2-theme-hicolor/gtk2-theme-hicolor.mk
+++ package/gtk2-themes/gtk2-theme-hicolor/gtk2-theme-hicolor.mk
@@ -6,12 +6,6 @@
 GTK2_THEME_HICOLOR_VERSION = 0.10
 GTK2_THEME_HICOLOR_SOURCE = hicolor-icon-theme-$(GTK2_THEME_HICOLOR_VERSION).tar.gz
 GTK2_THEME_HICOLOR_SITE = http://icon-theme.freedesktop.org/releases/
-GTK2_THEME_HICOLOR_INSTALL_STAGING = NO
-GTK2_THEME_HICOLOR_INSTALL_TARGET = YES
-
-GTK2_THEME_HICOLOR_CONF_OPT = --target=$(GNU_TARGET_NAME) --host=$(GNU_TARGET_NAME) \
-        --build=$(GNU_HOST_NAME) --prefix=/usr \
-        --includedir=/usr/include
 
 GTK2_THEME_HICOLOR_DEPENDENCIES = libgtk2
 
