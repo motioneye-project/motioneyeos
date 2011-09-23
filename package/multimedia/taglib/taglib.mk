945b5356faae24b6d474d370e16e543090ce2a44
Sven Neumann <s.neumann@raumfeld.com>
2011-09-23 10:50:09 +0200

    taglib: bump to 1.7
    Bump the version of taglib to 1.7. This version does not any longer
    ship with a configure script so convert the package from autotargets
    to cmaketargets.
    
    The 1.7 version offers optional support for MP4 and WMA files.
    Expose these options in buildroot.
    
    [Peter: drop unneded dependencies, keep target install, fix Config.in indent]
    Signed-off-by: Sven Neumann <s.neumann@raumfeld.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/taglib/taglib.mk
+++ package/multimedia/taglib/taglib.mk
@@ -3,18 +3,18 @@
 # taglib
 #
 #############################################################
-TAGLIB_VERSION = 1.5
+TAGLIB_VERSION = 1.7
 TAGLIB_SOURCE = taglib-$(TAGLIB_VERSION).tar.gz
 TAGLIB_SITE = http://developer.kde.org/~wheeler/files/src
-TAGLIB_LIBTOOL_PATCH = NO
 TAGLIB_INSTALL_STAGING = YES
 
-TAGLIB_CONF_ENV = \
-	DO_NOT_COMPILE='bindings tests examples' \
-	ac_cv_header_cppunit_extensions_HelperMacros_h=no \
-	ac_cv_header_zlib_h=$(if $(BR2_PACKAGE_ZLIB),yes,no)
+ifeq ($(BR2_PACKAGE_TAGLIB_ASF),y)
+TAGLIB_CONF_OPT += -DWITH_ASF=ON
+endif
 
-TAGLIB_CONF_OPT = --disable-libsuffix
+ifeq ($(BR2_PACKAGE_TAGLIB_MP4),y)
+TAGLIB_CONF_OPT += -DWITH_MP4=ON
+endif
 
 define TAGLIB_REMOVE_DEVFILE
 	rm -f $(TARGET_DIR)/usr/bin/taglib-config
@@ -24,4 +24,4 @@ ifneq ($(BR2_HAVE_DEVFILES),y)
 TAGLIB_POST_INSTALL_TARGET_HOOKS += TAGLIB_REMOVE_DEVFILE
 endif
 
-$(eval $(call AUTOTARGETS,package/multimedia,taglib))
+$(eval $(call CMAKETARGETS,package/multimedia,taglib))
