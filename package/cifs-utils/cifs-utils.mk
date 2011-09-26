4353fec6d7e5f6ab607af4fc897a02eeca637f85
Peter Korsgaard <jacmet@sunsite.dk>
2011-09-26 22:37:46 +0200

    cifs-utils: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/cifs-utils/cifs-utils.mk
+++ package/cifs-utils/cifs-utils.mk
@@ -1,4 +1,4 @@
-CIFS_UTILS_VERSION = 5.0
+CIFS_UTILS_VERSION = 5.1
 CIFS_UTILS_SOURCE  = cifs-utils-$(CIFS_UTILS_VERSION).tar.bz2
 CIFS_UTILS_SITE    = ftp://ftp.samba.org/pub/linux-cifs/cifs-utils/
 
