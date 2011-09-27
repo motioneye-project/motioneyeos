0a775837fb869ff6ac0996a08b8a3c9ead8fdbf0
Yegor Yefremov <yegor_sub1@visionsystems.de>
2011-09-27 12:17:43 +0200

    rsync: bump to 3.0.9
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/rsync/rsync.mk
+++ package/rsync/rsync.mk
@@ -4,10 +4,9 @@
 #
 #############################################################
 
-RSYNC_VERSION := 3.0.8
-RSYNC_SOURCE := rsync-$(RSYNC_VERSION).tar.gz
-RSYNC_SITE := http://rsync.samba.org/ftp/rsync/src
-RSYNC_INSTALL_STAGING = NO
+RSYNC_VERSION = 3.0.9
+RSYNC_SOURCE = rsync-$(RSYNC_VERSION).tar.gz
+RSYNC_SITE = http://rsync.samba.org/ftp/rsync/src
 RSYNC_CONF_OPT = $(if $(BR2_ENABLE_DEBUG),--enable-debug,--disable-debug)
 RSYNC_CONF_OPT = --with-included-popt
 
