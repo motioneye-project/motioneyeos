6fc42cda363292b05d16d8eabaceba6376edc318
Peter Korsgaard <jacmet@sunsite.dk>
2011-09-28 22:18:29 +0200

    package: add json-c library
    json-c is a JSON library written in C.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/json-c/json-c.mk
@@ -0,0 +1,11 @@
+################################################################################
+#
+# json-c
+#
+################################################################################
+
+JSON_C_VERSION = 0.9
+JSON_C_SITE = http://oss.metaparadigm.com/json-c/
+JSON_C_INSTALL_STAGING = YES
+
+$(eval $(call AUTOTARGETS,package,json-c))
