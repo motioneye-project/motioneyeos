190700ab034670084fd71f80a42a664ade2130a8
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-09-29 21:57:50 +0200

    mcookie: convert to the GENTARGETS infrastructure
    [Peter: fix missing newline at eof]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/mcookie/mcookie.mk
+++ package/x11r7/mcookie/mcookie.mk
@@ -4,17 +4,19 @@
 #
 #############################################################
 
-$(TARGET_DIR)/usr/bin/mcookie: package/x11r7/mcookie/mcookie.c
-	$(TARGET_CC) -Wall -Os -s package/x11r7/mcookie/mcookie.c -o $(TARGET_DIR)/usr/bin/mcookie
+# Source included in Buildroot
+MCOOKIE_SOURCE =
 
+define MCOOKIE_EXTRACT_CMDS
+	cp package/x11r7/mcookie/mcookie.c $(@D)/
+endef
 
-mcookie: $(TARGET_DIR)/usr/bin/mcookie
+define MCOOKIE_BUILD_CMDS
+	(cd $(@D); $(TARGET_CC) -Wall -Os -s mcookie.c -o mcookie)
+endef
 
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_MCOOKIE),y)
-TARGETS+=mcookie
-endif
+define MCOOKIE_INSTALL_TARGET_CMDS
+	install -m 0755 -D $(@D)/mcookie $(TARGET_DIR)/usr/bin/mcookie
+endef
+
+$(eval $(call GENTARGETS))
