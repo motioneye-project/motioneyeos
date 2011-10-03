08540f43b63abccfc243834c637a5624ef69952f
Maxime Ripard <maxime@free-electrons.com>
2011-10-03 12:10:31 +0200

    Add the picocom package
    [Peter: fix Config.in help text indentation]
    Signed-off-by: Maxime Ripard <maxime@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/picocom/picocom.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# picocom
+#
+#############################################################
+
+PICOCOM_VERSION = 1.6
+PICOCOM_SITE    = http://picocom.googlecode.com/files/
+
+define PICOCOM_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
+endef
+
+define PICOCOM_INSTALL_TARGET_CMDS
+	install -D -m 0755 $(@D)/picocom $(TARGET_DIR)/usr/bin/picocom
+endef
+
+$(eval $(call GENTARGETS,package,picocom))
