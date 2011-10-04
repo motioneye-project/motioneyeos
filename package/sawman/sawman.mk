9db9779a3f240b10bb4f1a3e649f1829b6f35c6b
Sven Neumann <s.neumann@raumfeld.com>
2011-10-04 13:45:48 +0200

    sawman: bump to version 1.4.15
    Signed-off-by: Sven Neumann <s.neumann@raumfeld.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sawman/sawman.mk
+++ package/sawman/sawman.mk
@@ -3,11 +3,10 @@
 # SAWMAN
 #
 #############################################################
-SAWMAN_VERSION:=1.4.11
-SAWMAN_SOURCE:=SaWMan-$(SAWMAN_VERSION).tar.gz
-SAWMAN_SITE:=http://www.directfb.org/downloads/Extras
+SAWMAN_VERSION = 1.4.15
+SAWMAN_SOURCE = SaWMan-$(SAWMAN_VERSION).tar.gz
+SAWMAN_SITE = http://www.directfb.org/downloads/Extras
 SAWMAN_INSTALL_STAGING = YES
 SAWMAN_DEPENDENCIES = directfb
 
 $(eval $(call AUTOTARGETS))
-
