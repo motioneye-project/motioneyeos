d7139cd936e0cb793dc552ddf484aacac1b8dd45
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-10-05 17:23:38 -0300

    gmp: work around ARM thumb build breakage
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gmp/gmp.mk
+++ package/gmp/gmp.mk
@@ -9,5 +9,10 @@ GMP_SITE = $(BR2_GNU_MIRROR)/gmp
 GMP_SOURCE = gmp-$(GMP_VERSION).tar.bz2
 GMP_INSTALL_STAGING = YES
 
+# Bad ARM assembly breaks on pure thumb
+ifeq ($(ARCH),arm)
+GMP_MAKE_OPT += CFLAGS="$(TARGET_CFLAGS) -marm"
+endif
+
 $(eval $(call AUTOTARGETS))
 $(eval $(call AUTOTARGETS,host))
