7f7cff5239a29e9a8e6436650ea4fad3a736d90d
Roeland Van Praet <roelvp@mind.be>
2011-10-07 09:04:26 +0200

    package: add 'pv' (Pipe-Viewer)
    Signed-off-by: Roeland Van Praet <roelvp@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/pv/pv.mk
@@ -0,0 +1,11 @@
+#######################
+#
+# pv - Pipe Viewer
+#
+#######################
+
+PV_VERSION = 1.2.0
+PV_SOURCE = pv-$(PV_VERSION).tar.bz2
+PV_SITE = http://pipeviewer.googlecode.com/files
+
+$(eval $(call AUTOTARGETS))
