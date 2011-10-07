cec7128dc1a30880523011a115ff52d7a27c403d
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-10-07 19:39:01 -0300

    noip: new package
    [Peter: use install -D / proper permissions]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/noip/noip.mk
@@ -0,0 +1,30 @@
+#############################################################
+#
+# noip
+#
+#############################################################
+
+NOIP_VERSION = 2.1.9
+NOIP_SITE = http://www.no-ip.com/client/linux
+NOIP_SOURCE = noip-duc-linux.tar.gz
+
+define NOIP_BUILD_CMDS
+	sed -i -e "s:\(#define CONFIG_FILENAME\).*:\1 \"/etc/no-ip2.conf\":" \
+		$(@D)/noip2.c
+	$(MAKE) -C $(@D) CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" \
+		PREFIX=/usr CONFDIR=/etc
+endef
+
+define NOIP_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/noip2 $(TARGET_DIR)/usr/sbin/noip2
+endef
+
+define NOIP_UNINSTALL_TARGET_CMDS
+	rm -f "$(TARGET_DIR)/usr/sbin/noip2"
+endef
+
+define NOIP_CLEAN_CMDS
+	$(MAKE) -C $(@D) clean
+endef
+
+$(eval $(call GENTARGETS))
