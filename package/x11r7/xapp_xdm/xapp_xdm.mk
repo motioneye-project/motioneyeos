bdb40d8bac210a8457140f2521a045d425ca8e29
Yegor Yefremov <yegorslists@googlemail.com>
2011-10-10 11:21:59 +0200

    xdm: bump to 1.1.11
    [Peter: Minor whitespace fix]
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xdm/xapp_xdm.mk
+++ package/x11r7/xapp_xdm/xapp_xdm.mk
@@ -4,10 +4,12 @@
 #
 ################################################################################
 
-XAPP_XDM_VERSION = 1.1.6
+XAPP_XDM_VERSION = 1.1.11
 XAPP_XDM_SOURCE = xdm-$(XAPP_XDM_VERSION).tar.bz2
 XAPP_XDM_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XDM_CONF_ENV = ac_cv_file__dev_urandom=yes
 XAPP_XDM_DEPENDENCIES = xapp_xinit xapp_sessreg xapp_xrdb xlib_libX11 xlib_libXaw xlib_libXdmcp xlib_libXinerama xlib_libXt xproto_xineramaproto xproto_xproto
+XAPP_XDM_CONF_OPT = --with-utmp-file=/var/adm/utmpx \
+		    --with-wtmp-file=/var/adm/wtmpx
 
 $(eval $(call AUTOTARGETS))
