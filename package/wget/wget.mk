b79ab02d462596566f8f2e2db518dad158bae5eb
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-10-15 00:07:33 -0300

    wget: bump to version 1.13.4 and improvements
    * Bump wget to version 1.13.4
    * Enable wget again on !wchar toolchains
    * Enable support for gnutls
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/wget/wget.mk
+++ package/wget/wget.mk
@@ -4,12 +4,30 @@
 #
 #############################################################
 
-WGET_VERSION = 1.12
+WGET_VERSION = 1.13.4
 WGET_SITE = $(BR2_GNU_MIRROR)/wget
 
+# Prefer full-blown wget over busybox
+ifeq ($(BR2_PACKAGE_BUSYBOX),y)
+	WGET_DEPENDENCIES += busybox
+endif
+
+ifeq ($(BR2_PACKAGE_GNUTLS),y)
+	WGET_CONF_OPT += --with-ssl=gnutls \
+		--with-libgnutls-prefix=$(STAGING_DIR)
+	WGET_DEPENDENCIES += gnutls
+endif
+
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
-	WGET_CONF_OPT += --with-ssl --with-libssl-prefix=$(STAGING_DIR)
+	WGET_CONF_OPT += --with-ssl=openssl --with-libssl-prefix=$(STAGING_DIR)
 	WGET_DEPENDENCIES += openssl
+endif
+
+# --with-ssl is default
+ifneq ($(BR2_PACKAGE_GNUTLS),y)
+ifneq ($(BR2_PACKAGE_OPENSSL),y)
+	WGET_CONF_OPT += --without-ssl
+endif
 endif
 
 $(eval $(call AUTOTARGETS))
