2da2281cbcdc67ffb9419e76650f3636a5313d62
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-10-15 00:07:30 -0300

    gmpc: bump to version 11.8.16
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gmpc/gmpc.mk
+++ package/gmpc/gmpc.mk
@@ -3,21 +3,14 @@
 # gmpc
 #
 #############################################################
-GMPC_VERSION = 0.17.0
-GMPC_SOURCE = gmpc-$(GMPC_VERSION).tar.gz
-GMPC_SITE = http://download.sarine.nl/download/Programs/gmpc/$(GMPC_VERSION)/
-GMPC_CONF_ENV = ac_cv_lib_curl_curl_global_init=yes \
-		ac_cv_path_GOB2=$(GOB2_HOST_BINARY)
-GMPC_CONF_OPT = --disable-mmkeys
 
-GMPC_DEPENDENCIES = libglib2 libgtk2 libglade libcurl libmpd host-gob2 host-intltool \
+GMPC_VERSION = 11.8.16
+GMPC_SITE = http://download.sarine.nl/Programs/gmpc/$(GMPC_VERSION)
+GMPC_CONF_ENV = ac_cv_path_GOB2=$(GOB2_HOST_BINARY)
+GMPC_CONF_OPT = --disable-mmkeys --disable-unique
+GMPC_DEPENDENCIES = host-gob2 host-intltool host-pkg-config host-vala \
+	libglib2 libgtk2 libmpd libsoup sqlite \
+	xlib_libICE xlib_libSM xlib_libX11 \
 	$(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext libintl)
-
-ifeq ($(BR2_PACKAGE_XLIB_LIBSM),y)
-GMPC_DEPENDENCIES += xlib_libSM
-GMPC_CONF_OPT += --enable-sm
-else
-GMPC_CONF_OPT += --disable-sm
-endif
 
 $(eval $(call AUTOTARGETS))
