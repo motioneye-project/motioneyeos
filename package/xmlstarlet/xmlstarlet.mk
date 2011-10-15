5c57141228fbc7ae6642f4f6d3309da15466cbe8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-10-15 00:07:18 -0300

    xmlstarlet: bump to version 1.3.0 and style fix
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/xmlstarlet/xmlstarlet.mk
+++ package/xmlstarlet/xmlstarlet.mk
@@ -4,11 +4,8 @@
 #
 #############################################################
 
-XMLSTARLET_VERSION:=1.0.4
-XMLSTARLET_SOURCE:=xmlstarlet-$(XMLSTARLET_VERSION).tar.gz
-XMLSTARLET_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/xmlstar/xmlstarlet/$(XMLSTARLET_VERSION)
-XMLSTARLET_INSTALL_STAGING:=NO
-XMLSTARLET_INSTALL_TARGET:=YES
+XMLSTARLET_VERSION = 1.3.0
+XMLSTARLET_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/xmlstar/xmlstarlet/$(XMLSTARLET_VERSION)
 
 XMLSTARLET_DEPENDENCIES += libxml2 libxslt \
 	$(if $(BR2_PACKAGE_LIBICONV),libiconv)
