69037a484728d1f284fe71df13af6b157c695133
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-10-15 00:07:35 -0300

    libdaemon: style cleanup
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libdaemon/libdaemon.mk
+++ package/libdaemon/libdaemon.mk
@@ -5,13 +5,10 @@
 #############################################################
 
 LIBDAEMON_VERSION = 0.14
-LIBDAEMON_SOURCE = libdaemon-$(LIBDAEMON_VERSION).tar.gz
-LIBDAEMON_SITE = http://0pointer.de/lennart/projects/libdaemon/
-LIBDAEMON_AUTORECONF = NO
+LIBDAEMON_SITE = http://0pointer.de/lennart/projects/libdaemon
 LIBDAEMON_INSTALL_STAGING = YES
 LIBDAEMON_CONF_ENV = ac_cv_func_setpgrp_void=no
 LIBDAEMON_CONF_OPT = --disable-lynx
-
 LIBDAEMON_DEPENDENCIES = host-pkg-config
 
 $(eval $(call AUTOTARGETS))
