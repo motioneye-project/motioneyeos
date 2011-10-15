5a9d386ca96960d0c7d7db4268f3877688662e10
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-10-15 00:07:27 -0300

    gob2: bump to version 2.0.18
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gob2/gob2.mk
+++ package/gob2/gob2.mk
@@ -3,13 +3,10 @@
 # gob2
 #
 #############################################################
-GOB2_VERSION = 2.0.15
-GOB2_SOURCE = gob2-$(GOB2_VERSION).tar.gz
-GOB2_SITE = http://ftp.5z.com/pub/gob/
-IPERF_CONF_ENV = ac_cv_lib_lex=-lfl
 
+GOB2_VERSION = 2.0.18
+GOB2_SITE = http://ftp.5z.com/pub/gob
 GOB2_DEPENDENCIES = libglib2 flex bison host-pkg-config host-flex
-
 HOST_GOB2_DEPENDENCIES = host-libglib2
 
 $(eval $(call AUTOTARGETS))
