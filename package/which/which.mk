b8684494837ca9dee7defe84cbc6079a593cf2ae
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-10-15 00:07:17 -0300

    which: style cleanup
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/which/which.mk
+++ package/which/which.mk
@@ -3,11 +3,8 @@
 # which
 #
 #############################################################
-WHICH_VERSION:=2.20
-WHICH_SOURCE:=which-$(WHICH_VERSION).tar.gz
-WHICH_SITE:=http://www.xs4all.nl/~carlo17/which/
-WHICH_AUTORECONF:=NO
-WHICH_INSTALL_STAGING:=NO
-WHICH_INSTALL_TARGET:=YES
+
+WHICH_VERSION = 2.20
+WHICH_SITE = http://carlo17.home.xs4all.nl/which
 
 $(eval $(call AUTOTARGETS))
