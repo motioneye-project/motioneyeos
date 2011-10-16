79f5d0e3b97edcf6f43c270a505d0462bcdab4e8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-10-16 00:04:32 -0300

    mutt: bump to version 1.5.21
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/mutt/mutt.mk
+++ package/mutt/mutt.mk
@@ -3,23 +3,11 @@
 # mutt
 #
 #############################################################
-MUTT_VERSION:=1.5.17+20080114
-MUTT_SOURCE:=mutt_$(MUTT_VERSION).orig.tar.gz
-MUTT_PATCH:=mutt_$(MUTT_VERSION)-1.diff.gz
-MUTT_SITE:=$(BR2_DEBIAN_MIRROR)/debian/pool/main/m/mutt/
-MUTT_DEPENDENCIES=ncurses
-MUTT_CONF_OPT = \
-		--disable-smtp \
-		--disable-iconv \
-		--without-wc-funcs
-MUTT_AUTORECONF=YES
 
-define MUTT_APPLY_DEBIAN_PATCHES
-        if [ -d $(@D)/debian/patches ]; then \
-                support/scripts/apply-patches.sh $(@D) $(@D)/debian/patches \*.patch; \
-        fi
-endef
-
-MUTT_POST_PATCH_HOOKS += MUTT_APPLY_DEBIAN_PATCHES
+MUTT_VERSION = 1.5.21
+MUTT_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/mutt
+MUTT_DEPENDENCIES = ncurses
+MUTT_CONF_OPT = --disable-iconv --disable-smtp
+MUTT_AUTORECONF = YES
 
 $(eval $(call AUTOTARGETS))
