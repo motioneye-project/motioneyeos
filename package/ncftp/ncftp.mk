ea729ab3e5936011e91d3ba636bf6e0a0bf51528
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-10-16 00:04:33 -0300

    ncftp: bump to version 3.2.5
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ncftp/ncftp.mk
+++ package/ncftp/ncftp.mk
@@ -3,33 +3,33 @@
 # ncftp client
 #
 #############################################################
-NCFTP_VERSION:=3.2.4
-NCFTP_SOURCE:=ncftp-$(NCFTP_VERSION)-src.tar.bz2
-NCFTP_SITE:=ftp://ftp.ncftp.com/ncftp
 
-NCFTP_TARGET_BINS:=ncftp
+NCFTP_VERSION = 3.2.5
+NCFTP_SOURCE = ncftp-$(NCFTP_VERSION)-src.tar.bz2
+NCFTP_SITE = ftp://ftp.ncftp.com/ncftp
+NCFTP_TARGET_BINS = ncftp
 
 ifeq ($(BR2_PACKAGE_NCFTP_GET),y)
-NCFTP_TARGET_BINS+=ncftpget
+	NCFTP_TARGET_BINS += ncftpget
 endif
 
 ifeq ($(BR2_PACKAGE_NCFTP_PUT),y)
-NCFTP_TARGET_BINS+=ncftpput
+	NCFTP_TARGET_BINS += ncftpput
 endif
 
 ifeq ($(BR2_PACKAGE_NCFTP_LS),y)
-NCFTP_TARGET_BINS+=ncftpls
+	NCFTP_TARGET_BINS += ncftpls
 endif
 
 ifeq ($(BR2_PACKAGE_NCFTP_BATCH),y)
-NCFTP_TARGET_BINS+=ncftpbatch
-NCFTP_INSTALL_NCFTP_BATCH = \
-	ln -s /usr/bin/ncftpbatch $(TARGET_DIR)/usr/bin/ncftpspooler
+	NCFTP_TARGET_BINS += ncftpbatch
+	NCFTP_INSTALL_NCFTP_BATCH = \
+		ln -s /usr/bin/ncftpbatch $(TARGET_DIR)/usr/bin/ncftpspooler
 endif
 
 ifeq ($(BR2_PACKAGE_NCFTP_BOOKMARKS),y)
-NCFTP_TARGET_BINS+=ncftpbookmarks
-NCFTP_DEPENDENCIES:=ncurses
+	NCFTP_TARGET_BINS += ncftpbookmarks
+	NCFTP_DEPENDENCIES += ncurses
 endif
 
 define NCFTP_INSTALL_TARGET_CMDS
