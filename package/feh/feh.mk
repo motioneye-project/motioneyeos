507e88bea8d57136a0966e3716b4698872d45d70
Yegor Yefremov <yegorslists@googlemail.com>
2011-10-17 10:41:07 +0200

    New package: feh
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Tested-By: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/feh/feh.mk
@@ -0,0 +1,21 @@
+FEH_VERSION = 2.0
+FEH_SOURCE = feh-$(FEH_VERSION).tar.bz2
+FEH_SITE = http://feh.finalrewind.org/
+FEH_DEPENDENCIES = libcurl giblib imlib2 libpng xlib_libXinerama xlib_libXt
+
+define FEH_BUILD_CMDS
+	$(MAKE1) CC="$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS)" \
+		-C $(@D) all
+endef
+
+define FEH_INSTALL_TARGET_CMDS
+	$(MAKE1) CC="$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) " \
+		DESTDIR=$(TARGET_DIR) -C $(@D) install
+endef
+
+define FEH_UNINSTALL_TARGET_CMDS
+	$(MAKE1) CC="$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) " \
+		DESTDIR=$(TARGET_DIR) -C $(@D) uninstall
+endef
+
+$(eval $(call GENTARGETS))
