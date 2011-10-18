fb97591e228b19f40c840f99893830b2dd022ab5
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-10-18 09:32:10 -0300

    ntfs-3g: bump to version 2011.4.12 and enhance
    ntfsprogs is now included into ntfs-3g so add an option to
    enable/disable it.
    It probably doesn't make sense to build ntfsprogs alone so it's not
    implemented.
    
    Add the encrypted volume support option now that we've got gnutls.
    
    Also add support for libuuid if it's present.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ntfs-3g/ntfs-3g.mk
+++ package/ntfs-3g/ntfs-3g.mk
@@ -4,10 +4,29 @@
 #
 #############################################################
 
-NTFS_3G_VERSION = 2010.10.2
-NTFS_3G_SOURCE = ntfs-3g-$(NTFS_3G_VERSION).tgz
+NTFS_3G_VERSION = 2011.4.12
+NTFS_3G_SOURCE = ntfs-3g_ntfsprogs-$(NTFS_3G_VERSION).tgz
 NTFS_3G_SITE = http://tuxera.com/opensource
 NTFS_3G_CONF_OPT = --disable-ldconfig
 NTFS_3G_INSTALL_STAGING = YES
+NTFS_3G_DEPENDENCIES = host-pkg-config
+
+ifeq ($(BR2_PACKAGE_LIBFUSE),y)
+	NTFS_3G_CONF_OPT += --with-fuse=external
+	NTFS_3G_DEPENDENCIES += libfuse
+endif
+
+ifeq ($(BR2_PACKAGE_UTIL_LINUX_LIBUUID),y)
+	NTFS_3G_DEPENDENCIES += util-linux
+endif
+
+ifeq ($(BR2_PACKAGE_NTFS_3G_ENCRYPTED),y)
+	NTFS_3G_CONF_OPT += --enable-crypto
+	NTFS_3G_DEPENDENCIES += gnutls
+endif
+
+ifneq ($(BR2_PACKAGE_NTFS_3G_NTFSPROGS),y)
+	NTFS_3G_CONF_OPT += --disable-ntfsprogs
+endif
 
 $(eval $(call AUTOTARGETS))
