6206828e3f39721fc0a567818d967236b1afc97c
Peter Korsgaard <jacmet@sunsite.dk>
2011-10-20 16:07:46 +0200

    mpg123: fix module loading with libtool / !HAVE_DEVFILES
    mpg123 enables loadable modules support if libtool is detected, but
    by default tries to load <module>.la, which we strip from the
    rootfs unless HAVE_DEVFILES is enabled.
    
    Fix it by directly loading .so files instead.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/mpg123/mpg123.mk
+++ package/multimedia/mpg123/mpg123.mk
@@ -57,4 +57,11 @@ endif
 
 MPG123_CONF_OPT += --with-audio=$(shell echo $(MPG123_AUDIO) | tr ' ' ,)
 
+ifeq ($(BR2_PACKAGE_LIBTOOL),y)
+MPG123_DEPENDENCIES += libtool
+# .la files gets stripped unless HAVE_DEVFILES is enabled, so directly
+# load .so files rather than .la
+MPG123_CONF_OPT += --with-modules --with-module-suffix=.so
+endif
+
 $(eval $(call AUTOTARGETS))
