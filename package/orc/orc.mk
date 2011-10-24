5889480ec61ecba3e6abe0d9cb70c1b7507a6ee5
Peter Korsgaard <jacmet@sunsite.dk>
2011-10-24 14:23:30 +0200

    orc: add host support
    Some packages use orc to generate C code at build time using orcc, so we
    need to build orc for the host as well.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/orc/orc.mk
+++ package/orc/orc.mk
@@ -6,6 +6,7 @@
 ORC_VERSION = 0.4.14
 ORC_SITE = http://code.entropywave.com/download/orc/
 ORC_INSTALL_STAGING = YES
+ORC_DEPENDENCIES = host-orc
 
 define ORC_REMOVE_BUGREPORT
 	rm -f $(TARGET_DIR)/usr/bin/orc-bugreport
@@ -22,3 +23,4 @@ ORC_POST_INSTALL_TARGET_HOOKS += ORC_REMOVE_DEVFILES
 endif
 
 $(eval $(call AUTOTARGETS))
+$(eval $(call AUTOTARGETS,host))
