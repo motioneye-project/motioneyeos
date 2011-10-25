9dd51dce953818f050d55a6cf6e53ad2806df5f6
Peter Korsgaard <jacmet@sunsite.dk>
2011-10-25 09:32:19 +0200

    intltool: style/white space cleanup
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/intltool/intltool.mk
+++ package/intltool/intltool.mk
@@ -3,9 +3,10 @@
 # intltool
 #
 #############################################################
-INTLTOOL_VERSION:=0.40.6
-INTLTOOL_SOURCE:=intltool-$(INTLTOOL_VERSION).tar.bz2
-INTLTOOL_SITE:=http://ftp.acc.umu.se/pub/GNOME/sources/intltool/0.40/
+
+INTLTOOL_VERSION = 0.40.6
+INTLTOOL_SOURCE = intltool-$(INTLTOOL_VERSION).tar.bz2
+INTLTOOL_SITE = http://ftp.acc.umu.se/pub/GNOME/sources/intltool/0.40/
 
 HOST_INTLTOOL_DEPENDENCIES = host-libxml-parser-perl
 HOST_INTLTOOL_CONF_OPT = \
