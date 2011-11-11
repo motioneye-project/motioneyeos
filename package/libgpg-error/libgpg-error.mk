1f52b2ae1d2bbc5784c10dda397493236fb1ed5a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-11-11 16:04:54 -0300

    libgpg-error: bump to version 1.10
    Bump to version 1.10, closes bug #3403
    Switch to gnupg.org mirror, closes bug #3409
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libgpg-error/libgpg-error.mk
+++ package/libgpg-error/libgpg-error.mk
@@ -3,10 +3,9 @@
 # libgpg-error
 #
 #############################################################
-LIBGPG_ERROR_VERSION:=1.8
-LIBGPG_ERROR_SOURCE:=libgpg-error-$(LIBGPG_ERROR_VERSION).tar.bz2
-LIBGPG_ERROR_SITE:=ftp://gd.tuwien.ac.at/privacy/gnupg/libgpg-error
 
+LIBGPG_ERROR_VERSION = 1.10
+LIBGPG_ERROR_SITE = ftp://ftp.gnupg.org/gcrypt/libgpg-error
 LIBGPG_ERROR_INSTALL_STAGING = YES
 
 $(eval $(call AUTOTARGETS))
