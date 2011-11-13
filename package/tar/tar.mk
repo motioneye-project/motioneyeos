8795caa082382f600f006c15275d20df00ce1179
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-11-13 19:27:57 -0300

    tar: build after busybox if enabled
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tar/tar.mk
+++ package/tar/tar.mk
@@ -7,4 +7,9 @@
 TAR_VERSION = 1.26
 TAR_SITE = $(BR2_GNU_MIRROR)/tar
 
+# Prefer full-blown tar over buybox's version
+ifeq ($(BR2_PACKAGE_BUSYBOX),y)
+TAR_DEPENDENCIES += busybox
+endif
+
 $(eval $(call AUTOTARGETS))
