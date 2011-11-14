a276ff90301ee82a4955fa15c8410fb2e3cf7ddd
Peter Korsgaard <jacmet@sunsite.dk>
2011-11-14 16:16:17 +0100

    file: install to staging as well
    file provides libmagic, which other packages might use - So install it
    into staging.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/file/file.mk
+++ package/file/file.mk
@@ -8,6 +8,7 @@ FILE_VERSION = 5.09
 FILE_SITE = ftp://ftp.astron.com/pub/file/
 FILE_DEPENDENCIES = host-file zlib
 HOST_FILE_DEPENDENCIES = host-zlib
+FILE_INSTALL_STAGING = YES
 
 define FILE_UNINSTALL_TARGET_CMDS
 	$(MAKE) DESTDIR=$(TARGET_DIR) uninstall -C $(FILE_DIR)
