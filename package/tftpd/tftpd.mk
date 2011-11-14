e796a1fd7cda4d93a372a2d89d0fc0e5deb6c7f0
Peter Korsgaard <jacmet@sunsite.dk>
2011-11-14 12:39:15 +0100

    tftpd: fix build without ipv6
    tftpd's configure script uses a nonstandard --without-ipv6 option to
    disable ipv6 support.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tftpd/tftpd.mk
+++ package/tftpd/tftpd.mk
@@ -8,6 +8,10 @@ TFTPD_SOURCE   = tftp-hpa-$(TFTPD_VERSION).tar.bz2
 TFTPD_SITE     = $(BR2_KERNEL_MIRROR)/software/network/tftp/
 TFTPD_CONF_OPT = --without-tcpwrappers
 
+ifneq ($(BR2_INET_IPV6),y)
+TFTPD_CONF_OPT += --without-ipv6
+endif
+
 define TFTPD_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/tftpd/tftpd $(TARGET_DIR)/usr/sbin/tftpd
 	$(INSTALL) -D package/tftpd/S80tftpd-hpa $(TARGET_DIR)/etc/init.d/
