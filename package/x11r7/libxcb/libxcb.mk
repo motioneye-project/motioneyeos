3acf323e9e92042b34f98065b65cee16f214aa54
Peter Korsgaard <jacmet@sunsite.dk>
2011-11-14 11:52:48 +0100

    libxcb: don't build doxygen documentation
    In most situations we don't care about documentation, and newer doxygen
    versions (1.7.x) seems to have issues with it, breaking the build.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/libxcb/libxcb.mk
+++ package/x11r7/libxcb/libxcb.mk
@@ -15,10 +15,13 @@ LIBXCB_DEPENDENCIES = \
 LIBXCB_CONF_ENV = STAGING_DIR="$(STAGING_DIR)"
 LIBXCB_MAKE_OPT = XCBPROTO_XCBINCLUDEDIR=$(STAGING_DIR)/usr/share/xcb \
 	XCBPROTO_XCBPYTHONDIR=$(HOST_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages
+HOST_LIBXCB_CONF_OPT = --disable-build-docs
 
 HOST_LIBXCB_DEPENDENCIES = \
 	host-libxslt host-pthread-stubs host-xcb-proto host-xlib_libXdmcp \
 	host-xlib_libXau host-python
+
+HOST_LIBXCB_CONF_OPT = --disable-build-docs
 
 $(eval $(call AUTOTARGETS))
 $(eval $(call AUTOTARGETS,host))
