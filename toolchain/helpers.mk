5931db0de902d2da434fbf682d4177e3b3c7e31b
Peter Korsgaard <jacmet@sunsite.dk>
2011-11-24 14:26:52 +0100

    gdb: fix kconfig dependency handling with !BR_TOOLCHAIN_BUILDROOT
    Newer versions of GDB need pthread debugging support if threads are
    enabled, which is always the case for glibc but is a configure option
    for uClibc.
    
    We have solved this for internal toolchains by selecting the
    BR2_PTHREAD_DEBUG option from the GDB selection if needed, but as this
    option isn't available when ctng/external toolchains are used, mconf
    prints ugly warnings and the build may fail if an external uClibc
    toolchain without pthread debugging support is used.
    
    Fix it by introducing 2 more hidden config options:
     - BR2_TOOLCHAIN_HAS_THREADS_DEBUG
     - BR2_TOOLCHAIN_HAS_THREADS_DEBUG_IF_NEEDED
    
    The first tells us if the toolchain HAS pthreads debugging support,
    and is checked by check_uclibc_feature in helper.mk for external uClibc
    based toolchains.
    
    The second tells us if the toolchain is ABLE TO provide pthreads debugging
    support if threads are enabled, either because it's an internal toolchain
    where we can force enable it or an external glibc/eglibc toolchain or
    uClibc with the option enabled.
    
    Crosstool-ng forcibly enables this support, so those will always work.
    The preconfigured uClibc-based toolchains we have also all enable it.
    
    Finally, show a comment if this isn't the case so the (external toolchain)
    user knows why. This is placed outside the choice option, as menuconfig
    has a bug where it doesn't show choice selections which only contain
    comments.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- toolchain/helpers.mk
+++ toolchain/helpers.mk
@@ -195,7 +195,8 @@ check_uclibc = \
 	$(call check_uclibc_feature,__UCLIBC_HAS_RPC__,BR2_INET_RPC,$${UCLIBC_CONFIG_FILE},RPC support) ;\
 	$(call check_uclibc_feature,__UCLIBC_HAS_LOCALE__,BR2_ENABLE_LOCALE,$${UCLIBC_CONFIG_FILE},Locale support) ;\
 	$(call check_uclibc_feature,__UCLIBC_HAS_WCHAR__,BR2_USE_WCHAR,$${UCLIBC_CONFIG_FILE},Wide char support) ;\
-	$(call check_uclibc_feature,__UCLIBC_HAS_THREADS__,BR2_TOOLCHAIN_HAS_THREADS,$${UCLIBC_CONFIG_FILE},Thread support)
+	$(call check_uclibc_feature,__UCLIBC_HAS_THREADS__,BR2_TOOLCHAIN_HAS_THREADS,$${UCLIBC_CONFIG_FILE},Thread support) ;\
+	$(call check_uclibc_feature,__PTHREADS_DEBUG_SUPPORT__,BR2_TOOLCHAIN_HAS_THREADS_DEBUG,$${UCLIBC_CONFIG_FILE},Thread debugging support)
 
 #
 # Check that the Buildroot configuration of the ABI matches the
