150124b9525c5db8f1f70e092ac6b00a7fb7e898
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-11-25 08:35:12 -0300

    ruby: fix host build breakage
    When building host-ruby with ncurses enabled on an x86_64 host we get a
    "relocation R_X86_64_32 against `_nc_globals' can not be used when making
    a shared object; recompile with -fPIC" error when building.
    Just disable ncurses and readline support for the host version - we
    don't need it.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ruby/ruby.mk
+++ package/ruby/ruby.mk
@@ -11,8 +11,7 @@ HOST_RUBY_AUTORECONF = YES
 RUBY_DEPENDENCIES = host-ruby
 RUBY_MAKE_ENV = $(TARGET_MAKE_ENV)
 RUBY_CONF_OPT = --disable-install-doc --disable-rpath
-
-HOST_RUBY_CONF_OPT = --disable-install-doc
+HOST_RUBY_CONF_OPT = --disable-install-doc --without-ncurses --without-readline
 
 # Force optionals to build before we do
 ifeq ($(BR2_PACKAGE_BERKELEYDB),y)
