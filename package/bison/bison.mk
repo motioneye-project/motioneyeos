2077f955ef64b48c3832e0be31a18aaf04d786da
Peter Korsgaard <jacmet@sunsite.dk>
2011-12-04 21:39:02 +0100

    bison: fix m4 dependency
    Bison needs m4 (at runtime). In addition, the bison 2.5 configure script
    checks for m4 4.1.6 or newer, and bails out otherwise, but ends up looking
    at whatever the build host has rather than the cross version, which might
    not be the correct version (or even available).
    
    Fix it by short circuiting the check.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/bison/bison.mk
+++ package/bison/bison.mk
@@ -7,11 +7,16 @@
 BISON_VERSION = 2.5
 BISON_SITE = $(BR2_GNU_MIRROR)/bison
 
+BISON_CONF_ENV += ac_cv_path_M4=/usr/bin/m4
+BISON_DEPENDENCIES += m4
+
 define BISON_DISABLE_EXAMPLES
 	echo 'all install:' > $(@D)/examples/Makefile
 endef
 
 BISON_POST_CONFIGURE_HOOKS += BISON_DISABLE_EXAMPLES
+
+HOST_BISON_DEPENDENCIES += host-m4
 
 $(eval $(call AUTOTARGETS))
 $(eval $(call AUTOTARGETS,host))
