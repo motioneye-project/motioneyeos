8c9a74836aef0c0a6e7e8aee614982225a89d5cf
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-12-05 10:00:42 -0300

    iproute2: bump to version 3.1.0
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/iproute2/iproute2.mk
+++ package/iproute2/iproute2.mk
@@ -4,8 +4,8 @@
 #
 #############################################################
 
-IPROUTE2_VERSION = 2.6.39
-IPROUTE2_SITE = http://devresources.linuxfoundation.org/dev/iproute2/download
+IPROUTE2_VERSION = 3.1.0
+IPROUTE2_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/net/iproute2
 IPROUTE2_TARGET_SBINS = ctstat genl ifstat ip lnstat nstat routef routel rtacct rtmon rtpr rtstat ss tc
 
 # If both iproute2 and busybox are selected, make certain we win
