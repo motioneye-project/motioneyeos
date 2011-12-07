64be796285685d0a451f79d08108bd4c0198bce2
Sven Neumann <s.neumann@raumfeld.com>
2011-12-07 20:00:07 +0100

    sqlite: bump to version 3.7.9
    Signed-off-by: Sven Neumann <s.neumann@raumfeld.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SQLITE_VERSION = 3070800
+SQLITE_VERSION = 3070900
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_VERSION).tar.gz
 SQLITE_SITE = http://www.sqlite.org
 SQLITE_INSTALL_STAGING = YES
