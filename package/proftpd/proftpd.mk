43ba7c1af8f89be7092516cada8e95e147f79e72
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-12-09 09:39:19 -0300

    proftpd: security bump to version 1.3.3g
    Security bump version to 1.3.3g, fix for CVE-2011-4130
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/proftpd/proftpd.mk
+++ package/proftpd/proftpd.mk
@@ -3,7 +3,8 @@
 # proftpd
 #
 #############################################################
-PROFTPD_VERSION = 1.3.3e
+
+PROFTPD_VERSION = 1.3.3g
 PROFTPD_SOURCE = proftpd-$(PROFTPD_VERSION).tar.bz2
 PROFTPD_SITE = ftp://ftp.proftpd.org/distrib/source/
 
