c8623910ed01805ccd7408a0582ddabb7997a1a8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-12-09 09:15:35 -0300

    ipset: bump to version 6.10
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ipset/ipset.mk
+++ package/ipset/ipset.mk
@@ -4,10 +4,11 @@
 #
 #############################################################
 
-IPSET_VERSION = 6.9.1
+IPSET_VERSION = 6.10
 IPSET_SOURCE = ipset-$(IPSET_VERSION).tar.bz2
 IPSET_SITE = http://ipset.netfilter.org
 IPSET_AUTORECONF = YES
 IPSET_DEPENDENCIES = libmnl host-pkg-config
+IPSET_CONF_OPT = --with-kmod=no
 
 $(eval $(call AUTOTARGETS))
