e1d1c379e9f9aad3956f19a2c3d67eb7701cb48f
Sagaert Johan <sagaert.johan@skynet.be>
2011-12-10 11:46:29 +0100

    libcgi: build for target, not host
    [Peter: Ensure correct CFLAGS are used, whitespace cleanup]
    Signed-off-by: Sagaert Johan <sagaert.johan@skynet.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libcgi/libcgi.mk
+++ package/libcgi/libcgi.mk
@@ -3,9 +3,11 @@
 # libcgi
 #
 #############################################################
-LIBCGI_VERSION:=1.0
-LIBCGI_SOURCE:=libcgi-$(LIBCGI_VERSION).tar.gz
-LIBCGI_SITE:=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/libcgi
-LIBCGI_INSTALL_STAGING=YES
+
+LIBCGI_VERSION = 1.0
+LIBCGI_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/libcgi
+LIBCGI_INSTALL_STAGING = YES
+# use cross CC/AR rather than host
+LIBCGI_MAKE_ENV = CC="$(TARGET_CC) $(TARGET_CFLAGS)" AR="$(TARGET_AR)"
 
 $(eval $(call AUTOTARGETS))
