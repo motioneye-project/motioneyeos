62c10600ab3a0958c9a141de7f99cfbf55308cd9
Peter Korsgaard <jacmet@sunsite.dk>
2011-12-12 21:13:34 +0100

    libosip: bump version, use upstream directly
    Instead of Debian.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libosip2/libosip2.mk
+++ package/libosip2/libosip2.mk
@@ -3,24 +3,10 @@
 # libosip2
 #
 #############################################################
-LIBOSIP2_VERSION = 3.3.0
-LIBOSIP2_SOURCE = libosip2_$(LIBOSIP2_VERSION).orig.tar.gz
-LIBOSIP2_PATCH = libosip2_$(LIBOSIP2_VERSION)-1.diff.gz
-LIBOSIP2_SITE = $(BR2_DEBIAN_MIRROR)/debian/pool/main/libo/libosip2
+
+LIBOSIP2_VERSION = 3.6.0
+LIBOSIP2_SOURCE = libosip2-$(LIBOSIP2_VERSION).tar.gz
+LIBOSIP2_SITE = $(BR2_GNU_MIRROR)/osip
 LIBOSIP2_INSTALL_STAGING = YES
-
-ifneq ($(LIBOSIP2_PATCH),)
-define LIBOSIP2_DEBIAN_PATCHES
-	if [ -d $(@D)/debian/patches ]; then \
-		(cd $(@D)/debian/patches && for i in *; \
-		 do $(SED) 's,^\+\+\+ .*cvs-$(LIBOSIP2_VERSION)/,+++ cvs-$(LIBOSIP2_VERSION)/,' $$i; \
-		 done; \
-		); \
-		support/scripts/apply-patches.sh $(@D) $(@D)/debian/patches \*; \
-	fi
-endef
-endif
-
-LIBOSIP2_POST_PATCH_HOOKS += LIBOSIP2_DEBIAN_PATCHES
 
 $(eval $(call AUTOTARGETS))
