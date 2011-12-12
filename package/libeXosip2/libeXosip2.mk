02ad75e9ee750782a8186d6aec8c478528735f1b
Peter Korsgaard <jacmet@sunsite.dk>
2011-12-12 20:35:16 +0100

    libeXosip: bump version, use upstream directly
    Instead of Debian.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libeXosip2/libeXosip2.mk
+++ package/libeXosip2/libeXosip2.mk
@@ -3,10 +3,10 @@
 # libeXosip2
 #
 #############################################################
-LIBEXOSIP2_VERSION = 3.5.0
-LIBEXOSIP2_SOURCE = libexosip2_$(LIBEXOSIP2_VERSION).orig.tar.gz
-LIBEXOSIP2_PATCH = libexosip2_$(LIBEXOSIP2_VERSION)-1.diff.gz
-LIBEXOSIP2_SITE = $(BR2_DEBIAN_MIRROR)/debian/pool/main/libe/libexosip2
+
+LIBEXOSIP2_VERSION = 3.6.0
+LIBEXOSIP2_SOURCE = libeXosip2-$(LIBEXOSIP2_VERSION).tar.gz
+LIBEXOSIP2_SITE = http://download.savannah.gnu.org/releases/exosip/
 LIBEXOSIP2_INSTALL_STAGING = YES
 
 LIBEXOSIP2_DEPENDENCIES = host-pkg-config libosip2
@@ -17,19 +17,5 @@ LIBEXOSIP2_CONF_OPT += --enable-openssl
 else
 LIBEXOSIP2_CONF_OPT += --disable-openssl
 endif
-
-ifneq ($(LIBEXOSIP2_PATCH),)
-define LIBEXOSIP2_DEBIAN_PATCHES
-	if [ -d $(@D)/debian/patches ]; then \
-		(cd $(@D)/debian/patches && for i in *; \
-		 do $(SED) 's,^\+\+\+ .*cvs-$(LIBEXOSIP2_VERSION)/,+++ cvs-$(LIBEXOSIP2_VERSION)/,' $$i; \
-		 done; \
-		); \
-		support/scripts/apply-patches.sh $(@D) $(@D)/debian/patches \*; \
-	fi
-endef
-endif
-
-LIBEXOSIP2_POST_PATCH_HOOKS += LIBEXOSIP2_DEBIAN_PATCHES
 
 $(eval $(call AUTOTARGETS))
