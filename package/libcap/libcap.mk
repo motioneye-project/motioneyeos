b2b55b459c5e61575b5ff3e10e5161f87c0da8df
Gustavo Zacarias <gustavo@zacarias.com.ar>
2011-12-14 12:15:17 -0300

    libcap: security bump to version 2.22
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libcap/libcap.mk
+++ package/libcap/libcap.mk
@@ -4,8 +4,11 @@
 #
 #############################################################
 
-LIBCAP_VERSION = 2.20
-LIBCAP_SITE = http://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2
+LIBCAP_VERSION = 2.22
+# Until kernel.org is completely back up use debian mirror
+#LIBCAP_SITE = http://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2
+LIBCAP_SITE = $(BR2_DEBIAN_MIRROR)/debian/pool/main/libc/libcap2
+LIBCAP_SOURCE = libcap2_$(LIBCAP_VERSION).orig.tar.gz
 LIBCAP_DEPENDENCIES = host-libcap
 LIBCAP_INSTALL_STAGING = YES
 
