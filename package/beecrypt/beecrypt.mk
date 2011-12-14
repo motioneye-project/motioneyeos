8145105ee348fe560222cb8a244188659d636690
Peter Korsgaard <jacmet@sunsite.dk>
2011-12-14 15:19:20 +0100

    beecrypt: bump version, build with C++ support if available
    The issue with cppglue.cxx is still present in 4.2.1, but the previous
    workaround had to be extended to allow builds with C++ support.
    Similary, the ICU check didn't handle cross compilation.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/beecrypt/beecrypt.mk
+++ package/beecrypt/beecrypt.mk
@@ -3,14 +3,27 @@
 # beecrypt
 #
 #############################################################
-BEECRYPT_VERSION = 4.1.2
+BEECRYPT_VERSION = 4.2.1
 BEECRYPT_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/beecrypt
 BEECRYPT_AUTORECONF = YES
 BEECRYPT_INSTALL_STAGING = YES
-
-BEECRYPT_CONF_OPT =  --without-cplusplus \
+BEECRYPT_CONF_OPT = \
 		--without-java \
-		--without-python  \
-		--disable-rpath
+		--without-python
+
+ifeq ($(BR2_PACKAGE_ICU),y)
+# C++ support needs icu
+BEECRYPT_DEPENDENCIES += icu
+else
+BEECRYPT_CONF_OPT += --without-cplusplus
+
+# automake/libtool uses the C++ compiler to link libbeecrypt because of
+# (the optional) cppglue.cxx. Force it to use the C compiler instead.
+define BEECRYPT_LINK_WITH_CC
+	$(SED) 's/--tag=CXX/--tag=CC/g' $(@D)/Makefile
+endef
+
+BEECRYPT_POST_CONFIGURE_HOOKS += BEECRYPT_LINK_WITH_CC
+endif
 
 $(eval $(call AUTOTARGETS))
