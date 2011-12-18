a8342a525bb2287846d1dcfd279e54b1bfc2d3db
Peter Korsgaard <jacmet@sunsite.dk>
2011-12-19 00:01:43 +0100

    package: add flot jQuery plugin
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/flot/flot.mk
@@ -0,0 +1,23 @@
+FLOT_VERSION = 0.7
+FLOT_SITE = http://flot.googlecode.com/files
+FLOT_FILES = jquery.flot $(addprefix jquery.flot.,\
+	$(if $(BR2_PACKAGE_FLOT_NAVIGATE),navigate) \
+	$(if $(BR2_PACKAGE_FLOT_PIE),pie) \
+	$(if $(BR2_PACKAGE_FLOT_RESIZE),resize) \
+	$(if $(BR2_PACKAGE_FLOT_SELECTION),selection) \
+	$(if $(BR2_PACKAGE_FLOT_STACK),stack) \
+	$(if $(BR2_PACKAGE_FLOT_SYMBOL),symbol) \
+	$(if $(BR2_PACKAGE_FLOT_THRESHOLD),threshold) \
+	)
+
+define FLOT_INSTALL_TARGET_CMDS
+	for i in $(FLOT_FILES); do \
+		$(INSTALL) -D $(@D)/$$i.min.js $(TARGET_DIR)/var/www/$$i.js; \
+	done
+endef
+
+define FLOT_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/var/www/jquery.flot*
+endef
+
+$(eval $(call GENTARGETS))
