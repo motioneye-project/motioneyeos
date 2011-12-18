645ff758fea4c640d1afc8d1a00da1a5f0b076ab
Peter Korsgaard <jacmet@sunsite.dk>
2011-12-18 23:40:11 +0100

    package: add jQuery sparkline plugin
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/jquery-sparkline/jquery-sparkline.mk
@@ -0,0 +1,18 @@
+JQUERY_SPARKLINE_VERSION = 1.6
+JQUERY_SPARKLINE_SITE = http://www.omnipotent.net/jquery.sparkline/$(JQUERY_SPARKLINE_VERSION)
+JQUERY_SPARKLINE_SOURCE = jquery.sparkline.min.js
+
+define JQUERY_SPARKLINE_EXTRACT_CMDS
+	cp $(DL_DIR)/$(JQUERY_SPARKLINE_SOURCE) $(@D)
+endef
+
+define JQUERY_SPARKLINE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/$(JQUERY_SPARKLINE_SOURCE) \
+		$(TARGET_DIR)/var/www/jquery.sparkline.js
+endef
+
+define JQUERY_SPARKLINE_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/var/www/jquery.sparkline.js
+endef
+
+$(eval $(call GENTARGETS))
