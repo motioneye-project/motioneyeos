4e20e8fe00b7125e2fe4538c69c6560f5bb75121
Peter Korsgaard <jacmet@sunsite.dk>
2011-12-18 23:11:14 +0100

    package: add Javascript menu and jQuery package
    A lot of open source Javascript libraries can be interesting to use on
    embedded systems, so add a specific Javascript menu and the probably most
    well known library of them all, jQuery.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/jquery/jquery.mk
@@ -0,0 +1,17 @@
+JQUERY_VERSION = 1.7.1
+JQUERY_SITE = http://code.jquery.com
+JQUERY_SOURCE = jquery-$(JQUERY_VERSION).min.js
+
+define JQUERY_EXTRACT_CMDS
+	cp $(DL_DIR)/$(JQUERY_SOURCE) $(@D)
+endef
+
+define JQUERY_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/$(JQUERY_SOURCE) $(TARGET_DIR)/var/www/jquery.js
+endef
+
+define JQUERY_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/var/www/jquery.js
+endef
+
+$(eval $(call GENTARGETS))
