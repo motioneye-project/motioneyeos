1886506b6b9e45f678f98e3d8e50bbcde1acad95
Peter Korsgaard <jacmet@sunsite.dk>
2011-12-19 00:07:46 +0100

    package: add explorercanvas
    Explorercanvas is a HTML5 canvas tag emulation library for IE<9.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/explorercanvas/explorercanvas.mk
@@ -0,0 +1,18 @@
+EXPLORERCANVAS_VERSION = r3
+EXPLORERCANVAS_SITE = http://explorercanvas.googlecode.com/files/
+EXPLORERCANVAS_SOURCE = excanvas_$(EXPLORERCANVAS_VERSION).zip
+
+define EXPLORERCANVAS_EXTRACT_CMDS
+	unzip -d $(@D) $(DL_DIR)/$(EXPLORERCANVAS_SOURCE)
+endef
+
+define EXPLORERCANVAS_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/excanvas.compiled.js \
+		$(TARGET_DIR)/var/www/excanvas.js
+endef
+
+define EXPLORERCANVAS_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/var/www/excanvas.js
+endef
+
+$(eval $(call GENTARGETS))
