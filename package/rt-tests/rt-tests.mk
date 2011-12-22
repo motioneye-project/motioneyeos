2d75c7927f044e9e847f7f91db9365e8b711bb2b
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2011-12-22 13:13:38 +0100

    rt-tests: new package
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/rt-tests/rt-tests.mk
@@ -0,0 +1,25 @@
+RT_TESTS_SITE    = http://snapshot.debian.org/archive/debian/20111202T222101Z/pool/main/r/rt-tests/
+RT_TESTS_VERSION = 0.83
+RT_TESTS_SOURCE  = rt-tests_$(RT_TESTS_VERSION).orig.tar.gz
+
+ifeq ($(BR2_PACKAGE_PYTHON),y)
+RT_TESTS_DEPENDENCIES = python
+endif
+
+define RT_TESTS_BUILD_CMDS
+	$(MAKE) -C $(@D) 			\
+		CC="$(TARGET_CC)" 		\
+		CFLAGS="$(TARGET_CFLAGS)"	\
+		prefix=/usr
+endef
+
+define RT_TESTS_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) 				\
+		DESTDIR="$(TARGET_DIR)" 		\
+		prefix=/usr 				\
+		$(if $(BR2_PACKAGE_PYTHON),HASPYTHON=1 PYLIB=/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages/) \
+		install
+endef
+
+$(eval $(call GENTARGETS))
+
