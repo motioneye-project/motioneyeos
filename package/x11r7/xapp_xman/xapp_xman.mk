3f83273eefb1f72194ee7a9e26d995e37cd53684
Peter Korsgaard <jacmet@sunsite.dk>
2011-12-23 12:15:04 +0100

    xapp_xman: fix dependencies
    xman needs libXaw, has optional Xprintutil support and doesn't have a
    --disable-IPv6 option.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xman/xapp_xman.mk
+++ package/x11r7/xapp_xman/xapp_xman.mk
@@ -7,11 +7,14 @@ XAPP_XMAN_VERSION:=1.0.3
 XAPP_XMAN_SOURCE:=xman-$(XAPP_XMAN_VERSION).tar.bz2
 XAPP_XMAN_SITE:=http://xorg.freedesktop.org/releases/individual/app
 XAPP_XMAN_INSTALL_TARGET = YES
+XAPP_XMAN_DEPENDENCIES = xlib_libXaw
 
 XAPP_XMAN_CONF_ENV = ac_cv_file__etc_man_conf=no \
 		ac_cv_file__etc_man_config=no \
 		ac_cv_file__etc_manpath_config=no
 
-XAPP_XMAN_CONF_OPT = --disable-IPv6
+ifeq ($(BR2_PACKAGE_XLIB_LIBXPRINTUTIL),y)
+XAPP_XMAN_CONF_OPT += --enable-xprint
+endif
 
 $(eval $(call AUTOTARGETS))
