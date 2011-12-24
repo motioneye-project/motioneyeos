3a9d3bc0c320614c8d831d77d9c28a7b90ad21da
Sergio Prado <sergiosiena@gmail.com>
2011-12-24 14:51:04 -0200

    Adding xf86-input-tslib driver
    Adding xf86-input-tslib driver so X11 can talk directly to
    touchscreen devices.
    
    [Peter: Add tslib deps and upstream URL]
    Signed-off-by: Sergio Prado <sergiosiena@gmail.com>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/x11r7/xdriver_xf86-input-tslib/xdriver_xf86-input-tslib.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# xdriver_xf86-input-tslib -- Touch screen library input driver
+#
+################################################################################
+
+XDRIVER_XF86_INPUT_TSLIB_VERSION = 0.0.6
+XDRIVER_XF86_INPUT_TSLIB_SOURCE = xf86-input-tslib-$(XDRIVER_XF86_INPUT_TSLIB_VERSION).tar.bz2
+XDRIVER_XF86_INPUT_TSLIB_SITE = http://www.ptxdist.org/software/xf86-input-tslib/download
+XDRIVER_XF86_INPUT_TSLIB_DEPENDENCIES = \
+	xproto_inputproto \
+	xserver_xorg-server \
+	xproto_randrproto \
+	xproto_xproto \
+	tslib
+
+$(eval $(call AUTOTARGETS))
