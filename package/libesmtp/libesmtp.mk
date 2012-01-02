5ec8da031b9fad064d4cc6ee70106af12cb109b2
Peter Korsgaard <jacmet@sunsite.dk>
2012-01-02 23:34:15 +0100

    libesmtp: add optional openssl support
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libesmtp/libesmtp.mk
+++ package/libesmtp/libesmtp.mk
@@ -8,4 +8,6 @@ LIBESMTP_SOURCE = libesmtp-$(LIBESMTP_VERSION).tar.bz2
 LIBESMTP_SITE = http://www.stafford.uklinux.net/libesmtp
 LIBESMTP_INSTALL_STAGING = YES
 
+LIBESMTP_DEPENDENCIES = $(if $(BR2_PACKAGE_OPENSSL),openssl)
+
 $(eval $(call AUTOTARGETS))
