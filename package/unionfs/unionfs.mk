7360f16a22af3e0a1a2e5891a275a7c86e7e5cc3
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-01-11 11:11:58 -0300

    unionfs: new package
    Add the unionfs FUSE unification filesystem.
    
    [Peter: whitespace/help text fix]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/unionfs/unionfs.mk
@@ -0,0 +1,20 @@
+#############################################################
+#
+# unionfs
+#
+#############################################################
+
+UNIONFS_VERSION = 0.24
+UNIONFS_SITE = http://podgorny.cz/unionfs-fuse/releases
+UNIONFS_SOURCE = unionfs-fuse-$(UNIONFS_VERSION).tar.bz2
+UNIONFS_DEPENDENCIES = libfuse
+
+define UNIONFS_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define UNIONFS_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) PREFIX=/usr DESTDIR=$(TARGET_DIR) install
+endef
+
+$(eval $(call GENTARGETS))
