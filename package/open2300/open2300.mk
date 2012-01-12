723cc255f74321967df80a6ff821edad5a35830b
Alexandre Belloni <alexandre.belloni@piout.net>
2012-01-12 15:03:02 +0100

    Add package for open2300
    Open2300 is a package of software tools that reads (and writes) data
    from a Lacrosse WS2300/WS2305/WS2310/WS2315 Weather Station.
    
    This package will install all the binaries on the target.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@piout.net>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/open2300/open2300.mk
@@ -0,0 +1,29 @@
+############################################################
+#
+# Open2300
+#
+############################################################
+
+OPEN2300_SITE        = http://www.lavrsen.dk/svn/open2300/trunk/
+OPEN2300_SITE_METHOD = svn
+OPEN2300_VERSION     = 12
+
+OPEN2300_BINS = open2300 dump2300 log2300 fetch2300 wu2300 cw2300 history2300 \
+		histlog2300 bin2300 xml2300 light2300 interval2300 minmax2300
+
+ifeq ($(BR2_PACKAGE_MYSQL),y)
+	OPEN2300_DEPENDENCIES += mysql_client
+	OPEN2300_BINS         += mysql2300 mysqlhistlog2300
+endif
+
+define OPEN2300_BUILD_CMDS
+	$(MAKE) CC=$(TARGET_CC) LD=$(TARGET_LD) -C $(@D) $(OPEN2300_BINS)
+endef
+
+define OPEN2300_INSTALL_TARGET_CMDS
+	for prog in $(OPEN2300_BINS); do \
+		$(INSTALL) -D -m 0755 $(@D)/$$prog $(TARGET_DIR)/usr/bin/$$prog ; \
+	done
+endef
+
+$(eval $(call GENTARGETS))
