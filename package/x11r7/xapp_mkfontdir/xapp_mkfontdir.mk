38faeafab7cf0272093ad996225b2f7dbdaa2d42
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2012-01-16 14:58:56 +0100

    xapp_mkfontdir: removed redundant HOST_FOO_DEPENDENCIES
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_mkfontdir/xapp_mkfontdir.mk
+++ package/x11r7/xapp_mkfontdir/xapp_mkfontdir.mk
@@ -8,7 +8,6 @@ XAPP_MKFONTDIR_VERSION = 1.0.5
 XAPP_MKFONTDIR_SOURCE = mkfontdir-$(XAPP_MKFONTDIR_VERSION).tar.bz2
 XAPP_MKFONTDIR_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_MKFONTDIR_DEPENDENCIES = xapp_mkfontscale
-HOST_XAPP_MKFONTDIR_DEPENDENCIES = host-xapp_mkfontscale
 
 $(eval $(call AUTOTARGETS))
 $(eval $(call AUTOTARGETS,host))
