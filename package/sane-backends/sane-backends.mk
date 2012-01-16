8c7c0ab34469ac7bfe1f5ccb9fc3c74c9ef0b4c7
Peter Korsgaard <jacmet@sunsite.dk>
2012-01-16 10:30:43 +0100

    sane-backends: make libusb optional, add optional jpeg, tiff, avahi and snmp deps
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sane-backends/sane-backends.mk
+++ package/sane-backends/sane-backends.mk
@@ -7,7 +7,30 @@ SANE_BACKENDS_VERSION = 1.0.22
 SANE_BACKENDS_SOURCE = sane-backends-$(SANE_BACKENDS_VERSION).tar.gz
 SANE_BACKENDS_SITE = ftp://ftp2.sane-project.org/pub/sane/sane-backends-$(SANE_BACKENDS_VERSION)
 
+ifeq ($(BR2_PACKAGE_LIBUSB),y)
 SANE_BACKENDS_DEPENDENCIES += libusb
 SANE_BACKENDS_CONF_OPT += --enable-libusb_1_0
+else
+SANE_BACKENDS_CONF_OPT += --disable-libusb
+endif
+
+ifeq ($(BR2_PACKAGE_JPEG),y)
+SANE_BACKENDS_DEPENDENCIES += jpeg
+endif
+
+ifeq ($(BR2_PACKAGE_TIFF),y)
+SANE_BACKENDS_DEPENDENCIES += tiff
+endif
+
+ifeq ($(BR2_PACKAGE_AVAHI)$(BR2_PACKAGE_DBUS)$(BR2_PACKAGE_LIBGLIB2),yyy)
+SANE_BACKENDS_DEPENDENCIES += avahi
+SANE_BACKENDS_CONF_OPT += --enable-avahi
+endif
+
+ifeq ($(BR2_PACKAGE_NETSNMP),y)
+SANE_BACKENDS_DEPENDENCIES += netsnmp
+else
+SANE_BACKENDS_CONF_OPT += --without-snmp
+endif
 
 $(eval $(call AUTOTARGETS))
