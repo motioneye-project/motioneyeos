f8245eb1952598d1c028ef3565def8550d6290aa
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2012-01-16 14:58:44 +0100

    xapp_mkfontscale: removed redundant HOST_FOO_DEPENDENCIES
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_mkfontscale/xapp_mkfontscale.mk
+++ package/x11r7/xapp_mkfontscale/xapp_mkfontscale.mk
@@ -8,7 +8,6 @@ XAPP_MKFONTSCALE_VERSION = 1.0.7
 XAPP_MKFONTSCALE_SOURCE = mkfontscale-$(XAPP_MKFONTSCALE_VERSION).tar.bz2
 XAPP_MKFONTSCALE_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_MKFONTSCALE_DEPENDENCIES = zlib freetype xlib_libfontenc xproto_xproto
-HOST_XAPP_MKFONTSCALE_DEPENDENCIES = host-zlib host-freetype host-xlib_libfontenc host-xproto_xproto
 
 $(eval $(call AUTOTARGETS))
 $(eval $(call AUTOTARGETS,host))
