9e42c6d04e43b562fc6683c34179426dff9d5a84
Yegor Yefremov <yegorslists@googlemail.com>
2012-01-16 16:04:35 +0100

    ethtool: bump to 3.2
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ETHTOOL_VERSION = 3.1
+ETHTOOL_VERSION = 3.2
 ETHTOOL_SITE = $(BR2_KERNEL_MIRROR)/software/network/ethtool
 
 $(eval $(call AUTOTARGETS))
