70bef8e49504de24df85ea72cdbc9212a1539785
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2012-01-16 14:58:49 +0100

    xapp_xkbcomp: removed redundant HOST_FOO_DEPENDENCIES
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xkbcomp/xapp_xkbcomp.mk
+++ package/x11r7/xapp_xkbcomp/xapp_xkbcomp.mk
@@ -8,7 +8,6 @@ XAPP_XKBCOMP_VERSION = 1.1.1
 XAPP_XKBCOMP_SOURCE = xkbcomp-$(XAPP_XKBCOMP_VERSION).tar.bz2
 XAPP_XKBCOMP_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XKBCOMP_DEPENDENCIES = xlib_libX11 xlib_libxkbfile
-HOST_XAPP_XKBCOMP_DEPENDENCIES = host-xlib_libX11 host-xlib_libxkbfile
 
 $(eval $(call AUTOTARGETS))
 $(eval $(call AUTOTARGETS,host))
