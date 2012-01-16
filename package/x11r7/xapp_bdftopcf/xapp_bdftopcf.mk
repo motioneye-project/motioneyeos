ea8805eb74fb71854f674b16a1e0d70509fd2677
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2012-01-16 14:58:52 +0100

    xapp_bdftopcf: removed redundant HOST_FOO_DEPENDENCIES
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_bdftopcf/xapp_bdftopcf.mk
+++ package/x11r7/xapp_bdftopcf/xapp_bdftopcf.mk
@@ -8,7 +8,6 @@ XAPP_BDFTOPCF_VERSION = 1.0.2
 XAPP_BDFTOPCF_SOURCE = bdftopcf-$(XAPP_BDFTOPCF_VERSION).tar.bz2
 XAPP_BDFTOPCF_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_BDFTOPCF_DEPENDENCIES = xlib_libXfont
-HOST_XAPP_BDFTOPCF_DEPENDENCIES = host-xlib_libXfont
 
 $(eval $(call AUTOTARGETS))
 $(eval $(call AUTOTARGETS,host))
