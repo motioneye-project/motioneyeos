deae09e965de114e281b954714e26931b09c7a9f
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2012-01-16 14:58:46 +0100

    xfont_encodings: removed redundant HOST_FOO_DEPENDENCIES
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xfont_encodings/xfont_encodings.mk
+++ package/x11r7/xfont_encodings/xfont_encodings.mk
@@ -9,7 +9,6 @@ XFONT_ENCODINGS_SOURCE = encodings-$(XFONT_ENCODINGS_VERSION).tar.bz2
 XFONT_ENCODINGS_SITE = http://xorg.freedesktop.org/releases/individual/font
 XFONT_ENCODINGS_MAKE_OPT =
 XFONT_ENCODINGS_DEPENDENCIES = host-xapp_mkfontscale
-HOST_XFONT_ENCODINGS_DEPENDENCIES = host-xapp_mkfontscale
 
 $(eval $(call AUTOTARGETS))
 $(eval $(call AUTOTARGETS,host))
