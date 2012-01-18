ef52202c15af9fe9e24f7722ee51974e72e0612c
Peter Korsgaard <jacmet@sunsite.dk>
2012-01-18 11:35:02 +0100

    ruby: unbreak host build after auto-host-deps change
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ruby/ruby.mk
+++ package/ruby/ruby.mk
@@ -9,6 +9,7 @@ RUBY_SITE = ftp://ftp.ruby-lang.org/pub/ruby/1.9
 RUBY_AUTORECONF = YES
 HOST_RUBY_AUTORECONF = YES
 RUBY_DEPENDENCIES = host-ruby
+HOST_RUBY_DEPENDENCIES =
 RUBY_MAKE_ENV = $(TARGET_MAKE_ENV)
 RUBY_CONF_OPT = --disable-install-doc --disable-rpath
 HOST_RUBY_CONF_OPT = --disable-install-doc --with-out-ext=curses,readline
