9e896860ae7c5ebecd2e5dc4e0ac1734526e1916
Peter Korsgaard <jacmet@sunsite.dk>
2012-01-18 11:41:57 +0100

    xapp_xcursorgen: remove redundant HOST_XAPP_XCURSORGEN_DEPENDENCIES
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xcursorgen/xapp_xcursorgen.mk
+++ package/x11r7/xapp_xcursorgen/xapp_xcursorgen.mk
@@ -9,7 +9,5 @@ XAPP_XCURSORGEN_SOURCE = xcursorgen-$(XAPP_XCURSORGEN_VERSION).tar.bz2
 XAPP_XCURSORGEN_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XCURSORGEN_DEPENDENCIES = libpng xlib_libX11 xlib_libXcursor
 
-HOST_XAPP_XCURSORGEN_DEPENDENCIES = host-libpng host-xlib_libX11 host-xlib_libXcursor
-
 $(eval $(call AUTOTARGETS))
 $(eval $(call AUTOTARGETS,host))
