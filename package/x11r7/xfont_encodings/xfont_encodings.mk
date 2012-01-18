c601f4661f217510b2fefb72c35b61871ccff1e6
Peter Korsgaard <jacmet@sunsite.dk>
2012-01-18 11:44:15 +0100

    xfont_encodings: cleanup
    MAKE_OPT defaults to the empty string, so drop it.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xfont_encodings/xfont_encodings.mk
+++ package/x11r7/xfont_encodings/xfont_encodings.mk
@@ -7,7 +7,6 @@
 XFONT_ENCODINGS_VERSION = 1.0.3
 XFONT_ENCODINGS_SOURCE = encodings-$(XFONT_ENCODINGS_VERSION).tar.bz2
 XFONT_ENCODINGS_SITE = http://xorg.freedesktop.org/releases/individual/font
-XFONT_ENCODINGS_MAKE_OPT =
 XFONT_ENCODINGS_DEPENDENCIES = host-xapp_mkfontscale
 
 $(eval $(call AUTOTARGETS))
