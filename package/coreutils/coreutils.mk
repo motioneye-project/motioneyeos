0d2a304ef39fa838824b31e4453dcef21b5302ff
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-01-24 13:29:08 -0300

    coreutils: bump to version 8.15
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/coreutils/coreutils.mk
+++ package/coreutils/coreutils.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-COREUTILS_VERSION = 8.14
+COREUTILS_VERSION = 8.15
 COREUTILS_SITE = $(BR2_GNU_MIRROR)/coreutils
 COREUTILS_SOURCE = coreutils-$(COREUTILS_VERSION).tar.xz
 
