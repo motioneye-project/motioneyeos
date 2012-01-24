ce7a7896c5cdc4ecc6a5ca16f9403c57d0a329f9
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-01-24 13:29:07 -0300

    grep: bump to version 2.10
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/grep/grep.mk
+++ package/grep/grep.mk
@@ -4,8 +4,9 @@
 #
 #############################################################
 
-GREP_VERSION = 2.9
+GREP_VERSION = 2.10
 GREP_SITE = $(BR2_GNU_MIRROR)/grep
+GREP_SOURCE = grep-$(GREP_VERSION).tar.xz
 GREP_CONF_OPT = --disable-perl-regexp --without-included-regex
 GREP_DEPENDENCIES = $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext libintl)
 
