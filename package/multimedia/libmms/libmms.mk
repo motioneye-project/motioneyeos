2f7496e938bedbeff8d1f2e943a0a4b7d4673816
Peter Korsgaard <jacmet@sunsite.dk>
2012-01-25 12:19:40 +0100

    libmms: bump version
    Patch is now upstream.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/libmms/libmms.mk
+++ package/multimedia/libmms/libmms.mk
@@ -3,13 +3,11 @@
 # libmms
 #
 #############################################################
-LIBMMS_VERSION = 0.6
+LIBMMS_VERSION = 0.6.2
 LIBMMS_SOURCE = libmms-$(LIBMMS_VERSION).tar.gz
 LIBMMS_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/libmms
 
-LIBMMS_AUTORECONF = NO
 LIBMMS_INSTALL_STAGING = YES
-LIBMMS_INSTALL_TARGET = YES
 
 LIBMMS_DEPENDENCIES = host-pkg-config libglib2
 
