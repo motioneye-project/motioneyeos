28f9df141ed47badcc1ab9d8a44513fae39f78ad
Peter Korsgaard <jacmet@sunsite.dk>
2012-01-25 14:37:03 +0100

    libogg: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/libogg/libogg.mk
+++ package/multimedia/libogg/libogg.mk
@@ -3,12 +3,10 @@
 # libogg
 #
 #############################################################
-LIBOGG_VERSION = 1.2.2
+LIBOGG_VERSION = 1.3.0
 LIBOGG_SOURCE = libogg-$(LIBOGG_VERSION).tar.gz
 LIBOGG_SITE = http://downloads.xiph.org/releases/ogg
-LIBOGG_AUTORECONF = NO
 LIBOGG_INSTALL_STAGING = YES
-LIBOGG_INSTALL_TARGET = YES
 
 LIBOGG_DEPENDENCIES = host-pkg-config
 
