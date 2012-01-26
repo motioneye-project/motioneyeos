bc83dd2706082b140203a29084ca33c293cf6469
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-01-26 13:51:25 -0300

    expat: fix style and drop stray file
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/expat/expat.mk
+++ package/expat/expat.mk
@@ -5,13 +5,10 @@
 #############################################################
 
 EXPAT_VERSION = 2.0.1
-EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.gz
 EXPAT_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/expat
 EXPAT_INSTALL_STAGING = YES
-EXPAT_INSTALL_TARGET = YES
 EXPAT_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) installlib
 EXPAT_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) installlib
-
 EXPAT_DEPENDENCIES = host-pkg-config
 
 $(eval $(call AUTOTARGETS))
