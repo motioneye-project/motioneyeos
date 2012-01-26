17bbd07af452bf76b6b2a605fc669a487a362986
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-01-26 13:51:30 -0300

    libpcap: bump to version 1.2.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libpcap/libpcap.mk
+++ package/libpcap/libpcap.mk
@@ -4,18 +4,17 @@
 #
 #############################################################
 
-LIBPCAP_VERSION:=1.1.1
-LIBPCAP_SITE:=http://www.tcpdump.org/release
-LIBPCAP_SOURCE:=libpcap-$(LIBPCAP_VERSION).tar.gz
-LIBPCAP_INSTALL_STAGING:=YES
+LIBPCAP_VERSION = 1.2.1
+LIBPCAP_SITE = http://www.tcpdump.org/release
+LIBPCAP_INSTALL_STAGING = YES
 # doesn't have an install-strip
 LIBPCAP_INSTALL_TARGET_OPT= DESTDIR="$(TARGET_DIR)" \
 	$(if $(BR2_PREFER_STATIC_LIB),install,install-shared)
 LIBPCAP_INSTALL_STAGING_OPT= DESTDIR="$(STAGING_DIR)" install \
 	$(if $(BR2_PREFER_STATIC_LIB),,install-shared)
-LIBPCAP_DEPENDENCIES:=zlib
-LIBPCAP_CONF_ENV:=ac_cv_linux_vers=2 \
-		  ac_cv_header_linux_wireless_h=yes # configure misdetects this
-LIBPCAP_CONF_OPT:=--disable-yydebug --with-pcap=linux
+LIBPCAP_DEPENDENCIES = zlib
+LIBPCAP_CONF_ENV = ac_cv_linux_vers=2 \
+		ac_cv_header_linux_wireless_h=yes # configure misdetects this
+LIBPCAP_CONF_OPT = --disable-yydebug --with-pcap=linux
 
 $(eval $(call AUTOTARGETS))
