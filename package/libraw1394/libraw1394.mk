39820e507df144c1af8b2dd90e9051fafd170966
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-01-26 13:51:22 -0300

    libraw1394: bump to version 2.0.7
    [Peter: fixup whitespace]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libraw1394/libraw1394.mk
+++ package/libraw1394/libraw1394.mk
@@ -3,9 +3,9 @@
 # libraw1394
 #
 #############################################################
-LIBRAW1394_VERSION:=1.2.1
-LIBRAW1394_SOURCE:=libraw1394-$(LIBRAW1394_VERSION).tar.gz
-LIBRAW1394_SITE:=http://www.kernel.org/pub/linux/libs/ieee1394/
-LIBRAW1394_INSTALL_STAGING=YES
+
+LIBRAW1394_VERSION = 2.0.7
+LIBRAW1394_SITE = $(BR2_KERNEL_MIRROR)/linux/libs/ieee1394
+LIBRAW1394_INSTALL_STAGING = YES
 
 $(eval $(call AUTOTARGETS))
