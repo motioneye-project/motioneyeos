cc06f041fcffecf9a62c64eeff40a00f522bcddf
Will Newton <will.newton@gmail.com>
2012-01-26 17:42:29 +0000

    libeet: Add new package
    [Peter: add URL, drop _INSTALL_TARGET]
    Signed-off-by: Will Newton <will.newton@imgtec.com>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/efl/libeet/libeet.mk
@@ -0,0 +1,15 @@
+#############################################################
+#
+# libeet
+#
+#############################################################
+
+LIBEET_VERSION = 1.5.0
+LIBEET_SOURCE = eet-$(LIBEET_VERSION).tar.bz2
+LIBEET_SITE = http://download.enlightenment.org/releases/
+LIBEET_INSTALL_STAGING = YES
+
+LIBEET_DEPENDENCIES = host-pkg-config zlib jpeg libeina
+
+$(eval $(call AUTOTARGETS))
+$(eval $(call AUTOTARGETS,host))
