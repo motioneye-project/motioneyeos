9c3e2f9789d2b1536157a19379d352840e76f85a
Will Newton <will.newton@gmail.com>
2012-01-26 17:42:56 +0000

    libelementary: Add new package
    [Peter: Add upstream URL, remove examples option/patches]
    Signed-off-by: Will Newton <will.newton@imgtec.com>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/efl/libelementary/libelementary.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# libelementary
+#
+################################################################################
+
+LIBELEMENTARY_VERSION = 0.8.0.65643
+LIBELEMENTARY_SOURCE = elementary-$(LIBELEMENTARY_VERSION).tar.bz2
+LIBELEMENTARY_SITE = http://download.enlightenment.org/snapshots/2011-11-28
+LIBELEMENTARY_INSTALL_STAGING = YES
+
+LIBELEMENTARY_DEPENDENCIES = libeina libevas libecore libedje host-libedje \
+				host-libeet
+
+LIBELEMENTARY_CONF_OPT = --with-edje-cc=$(HOST_DIR)/usr/bin/edje_cc \
+			 --with-eet-eet=$(HOST_DIR)/usr/bin/eet
+
+$(eval $(call AUTOTARGETS))
