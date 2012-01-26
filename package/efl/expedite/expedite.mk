d9632d5985845dc93657bab591c07df1e529de36
Will Newton <will.newton@gmail.com>
2012-01-26 17:42:46 +0000

    expedite: Add new package
    Signed-off-by: Will Newton <will.newton@imgtec.com>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/efl/expedite/expedite.mk
@@ -0,0 +1,11 @@
+#############################################################
+#
+# expedite
+#
+#############################################################
+
+EXPEDITE_VERSION = 1.1.0
+EXPEDITE_SITE = http://download.enlightenment.org/releases/
+EXPEDITE_DEPENDENCIES = libevas libeina libeet
+
+$(eval $(call AUTOTARGETS))
