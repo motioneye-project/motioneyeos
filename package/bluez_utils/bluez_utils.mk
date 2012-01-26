362281b466a44b37fc088632e390e39e3beaa808
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-01-26 13:51:23 -0300

    bluez_utils: bump to version 4.98
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/bluez_utils/bluez_utils.mk
+++ package/bluez_utils/bluez_utils.mk
@@ -3,9 +3,10 @@
 # bluez_utils
 #
 #############################################################
-BLUEZ_UTILS_VERSION = 4.97
+
+BLUEZ_UTILS_VERSION = 4.98
 BLUEZ_UTILS_SOURCE = bluez-$(BLUEZ_UTILS_VERSION).tar.gz
-BLUEZ_UTILS_SITE = http://www.kernel.org/pub/linux/bluetooth/
+BLUEZ_UTILS_SITE = $(BR2_KERNEL_MIRROR)/linux/bluetooth
 BLUEZ_UTILS_INSTALL_STAGING = YES
 BLUEZ_UTILS_DEPENDENCIES = dbus libglib2
 BLUEZ_UTILS_CONF_OPT = --enable-test --enable-tools
