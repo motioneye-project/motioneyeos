bbceb3c9ea5aa341e66785727155e2f1f54f3ada
Will Newton <will.newton@gmail.com>
2012-01-26 17:42:42 +0000

    libedje: Add new package
    [Peter: add upstream URL, remove redundant _INSTALL_TARGET]
    Signed-off-by: Will Newton <will.newton@imgtec.com>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/efl/libedje/libedje.mk
@@ -0,0 +1,24 @@
+#############################################################
+#
+# libedje
+#
+#############################################################
+
+LIBEDJE_VERSION = 1.1.0
+LIBEDJE_SOURCE = edje-$(LIBEDJE_VERSION).tar.bz2
+LIBEDJE_SITE =  http://download.enlightenment.org/releases/
+LIBEDJE_INSTALL_STAGING = YES
+
+LIBEDJE_DEPENDENCIES = host-pkg-config lua libeina libeet libecore libevas \
+			libembryo
+
+ifeq ($(BR2_PACKAGE_LIBEDJE_CC),y)
+LIBEDJE_CONF_OPT += --enable-edje-cc
+else
+LIBEDJE_CONF_OPT += --disable-edje-cc
+endif
+
+HOST_LIBEDJE_CONF_OPT = --enable-edje-cc
+
+$(eval $(call AUTOTARGETS))
+$(eval $(call AUTOTARGETS,host))
