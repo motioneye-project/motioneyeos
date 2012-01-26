5a05d31da25a28e8acaae116e923541be012bb0c
Will Newton <will.newton@gmail.com>
2012-01-26 17:42:22 +0000

    xcb-util: Install xcb-util libraries into staging
    Signed-off-by: Will Newton <will.newton@imgtec.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xcb-util/xcb-util.mk
+++ package/x11r7/xcb-util/xcb-util.mk
@@ -6,6 +6,7 @@
 XCB_UTIL_VERSION = 0.3.6
 XCB_UTIL_SOURCE = xcb-util-$(XCB_UTIL_VERSION).tar.bz2
 XCB_UTIL_SITE = http://xcb.freedesktop.org/dist/
+XCB_UTIL_INSTALL_STAGING = YES
 XCB_UTIL_DEPENDENCIES = host-gperf
 
 $(eval $(call AUTOTARGETS))
