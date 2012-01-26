66c1b5f4088382da8b607bed7809169fc1edad8f
Will Newton <will.newton@gmail.com>
2012-01-26 17:42:25 +0000

    libeina: Add new package along with EFL infrastructure
    [Peter: drop autoreconf, add URL]
    Signed-off-by: Will Newton <will.newton@imgtec.com>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/efl/libeina/libeina.mk
@@ -0,0 +1,15 @@
+#############################################################
+#
+# libeina
+#
+#############################################################
+
+LIBEINA_VERSION = 1.1.0
+LIBEINA_SOURCE = eina-$(LIBEINA_VERSION).tar.bz2
+LIBEINA_SITE = http://download.enlightenment.org/releases/
+LIBEINA_INSTALL_STAGING = YES
+
+LIBEINA_DEPENDENCIES = host-pkg-config
+
+$(eval $(call AUTOTARGETS))
+$(eval $(call AUTOTARGETS,host))
