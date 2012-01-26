eed21084821591a7537f00edf826e427c7123078
Will Newton <will.newton@gmail.com>
2012-01-26 17:42:49 +0000

    libefreet: Add new package
    [Peter: add upstream URL]
    Signed-off-by: Will Newton <will.newton@imgtec.com>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/efl/libefreet/libefreet.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# libefreet
+#
+################################################################################
+
+LIBEFREET_VERSION = 1.1.0
+LIBEFREET_SOURCE = efreet-$(LIBEFREET_VERSION).tar.bz2
+LIBEFREET_SITE = http://download.enlightenment.org/releases/
+LIBEFREET_INSTALL_STAGING = YES
+
+LIBEFREET_DEPENDENCIES = libeina libeet libecore
+
+$(eval $(call AUTOTARGETS))
