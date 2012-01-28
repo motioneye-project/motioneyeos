a23c0e59684628f69615e25ad1b9dbca972a3815
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-01-28 18:42:55 +0100

    sam-ba: new package with host variant only
    sam-ba is a tool needed to reprogram AT91-based systems using an USB
    connection or a serial port connection.
    
    [Peter: Add upstream URL]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Acked-by: Luca Ceresoli <luca@lucaceresoli.net>
    Acked-by: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/sam-ba/sam-ba.mk
@@ -0,0 +1,22 @@
+SAM_BA_SITE    = http://www.atmel.com/dyn/resources/prod_documents/
+SAM_BA_VERSION = 2.10
+SAM_BA_SOURCE  = sam-ba_$(SAM_BA_VERSION).zip
+
+define HOST_SAM_BA_EXTRACT_CMDS
+        unzip -d $(BUILD_DIR) $(DL_DIR)/$(SAM_BA_SOURCE)
+        mv $(BUILD_DIR)/sam-ba_cdc_linux/* $(@D)
+        rmdir $(BUILD_DIR)/sam-ba_cdc_linux/
+endef
+
+# Since it's a prebuilt application and it does not conform to the
+# usual Unix hierarchy, we install it in $(HOST_DIR)/opt/sam-ba and
+# then create a symbolic link from $(HOST_DIR)/usr/bin to the
+# application binary, for easier usage.
+
+define HOST_SAM_BA_INSTALL_CMDS
+	mkdir -p $(HOST_DIR)/opt/sam-ba/
+	cp -a $(@D)/* $(HOST_DIR)/opt/sam-ba/
+	ln -s ../../opt/sam-ba/sam-ba $(HOST_DIR)/usr/bin/sam-ba
+endef
+
+$(eval $(call GENTARGETS,host))
