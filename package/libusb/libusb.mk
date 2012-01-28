66ac654d8ebeffde1414d58bda4945fbd68e7de7
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-01-28 18:42:53 +0100

    libusb: add host variant
    [Peter: remove redundant HOST_*_DEPENDENCIES]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Acked-by: Luca Ceresoli <luca@lucaceresoli.net>
    Acked-by: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libusb/libusb.mk
+++ package/libusb/libusb.mk
@@ -8,6 +8,6 @@ LIBUSB_SOURCE = libusb-$(LIBUSB_VERSION).tar.bz2
 LIBUSB_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/libusb/libusb-1.0/libusb-$(LIBUSB_VERSION)
 LIBUSB_DEPENDENCIES = host-pkg-config
 LIBUSB_INSTALL_STAGING = YES
-LIBUSB_INSTALL_TARGET = YES
 
 $(eval $(call AUTOTARGETS))
+$(eval $(call AUTOTARGETS,host))
