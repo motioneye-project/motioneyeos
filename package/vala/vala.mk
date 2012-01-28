5d2df61ebd0ee06b5c7cec6fc9c06286dbf85eae
Peter Korsgaard <jacmet@sunsite.dk>
2012-01-28 23:01:05 +0100

    vala: bump version
    Tarball is only available as .xz
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/vala/vala.mk
+++ package/vala/vala.mk
@@ -4,9 +4,11 @@
 #
 #############################################################
 
-VALA_VERSION = 0.14.0
-VALA_SITE = http://download.gnome.org/sources/vala/0.14
-VALA_SOURCE = vala-$(VALA_VERSION).tar.bz2
+VALA_VERSION_MAJOR = 0.15
+VALA_VERSION_MINOR = 1
+VALA_VERSION = $(VALA_VERSION_MAJOR).$(VALA_VERSION_MINOR)
+VALA_SITE = http://download.gnome.org/sources/vala/$(VALA_VERSION_MAJOR)
+VALA_SOURCE = vala-$(VALA_VERSION).tar.xz
 VALA_DEPENDENCIES = host-flex libglib2 \
 		$(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext libintl)
 HOST_VALA_DEPENDENCIES = host-flex host-libglib2
