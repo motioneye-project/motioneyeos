f8a9fe6406786b46a80538aba1b58c3092636a0a
Yegor Yefremov <yegorslists@googlemail.com>
2012-01-30 15:46:24 +0100

    xapp_xinput: bump to 1.5.4
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xinput/xapp_xinput.mk
+++ package/x11r7/xapp_xinput/xapp_xinput.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XINPUT_VERSION = 1.5.0
+XAPP_XINPUT_VERSION = 1.5.4
 XAPP_XINPUT_SOURCE = xinput-$(XAPP_XINPUT_VERSION).tar.bz2
 XAPP_XINPUT_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XINPUT_DEPENDENCIES = xlib_libX11 xlib_libXi
