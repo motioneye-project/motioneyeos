ec35c076e1c20856baa48d461ebf968ef9fbd74f
Yegor Yefremov <yegorslists@googlemail.com>
2012-01-31 11:45:52 +0100

    python-netifaces: bump to 0.7
    cross-compilation changes were applied upstream, so remove the patch
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/python-netifaces/python-netifaces.mk
+++ package/python-netifaces/python-netifaces.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-PYTHON_NETIFACES_VERSION = 0.6
+PYTHON_NETIFACES_VERSION = 0.7
 PYTHON_NETIFACES_SOURCE  = netifaces-$(PYTHON_NETIFACES_VERSION).tar.gz
 PYTHON_NETIFACES_SITE    = http://alastairs-place.net/projects/netifaces
 
