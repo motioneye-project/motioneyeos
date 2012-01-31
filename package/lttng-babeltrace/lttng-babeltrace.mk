d2605833a3207c1037337c296a0bd0d1be15c7fc
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-01-31 20:40:06 +0100

    lttng-babeltrace: new package
    [Peter: drop redundant HOST_.._DEPENDENCIES setting]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/lttng-babeltrace/lttng-babeltrace.mk
@@ -0,0 +1,15 @@
+LTTNG_BABELTRACE_SITE    = http://lttng.org/files/bundles/20111214/
+LTTNG_BABELTRACE_VERSION = 0.8
+LTTNG_BABELTRACE_SOURCE  = babeltrace-$(LTTNG_BABELTRACE_VERSION).tar.bz2
+
+# Needed to fix libtool handling, otherwise the build fails when
+# building the ctf-parser-test program, which depends on libctf-ast.so
+# which itself depends on libbabeltrace_types.so.0 (and libtool gets
+# lost in the middle of this).
+LTTNG_BABELTRACE_AUTORECONF      = YES
+HOST_LTTNG_BABELTRACE_AUTORECONF = YES
+
+LTTNG_BABELTRACE_DEPENDENCIES = popt util-linux libglib2
+
+$(eval $(call AUTOTARGETS))
+$(eval $(call AUTOTARGETS,host))
