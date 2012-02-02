a2a9f6358d3c1442028a7fd5366651b8826d4547
Will Newton <will.newton@gmail.com>
2012-02-02 11:31:09 +0000

    pixman: Bump version to 0.24.2
    [Peter: depend on pkg-config, forcibly disable gtk-based demos]
    Signed-off-by: Will Newton <will.newton@imgtec.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/pixman/pixman.mk
+++ package/pixman/pixman.mk
@@ -3,11 +3,13 @@
 # pixman
 #
 ################################################################################
-PIXMAN_VERSION = 0.17.6
+PIXMAN_VERSION = 0.24.2
 PIXMAN_SOURCE = pixman-$(PIXMAN_VERSION).tar.gz
 PIXMAN_SITE = http://cairographics.org/releases/
-PIXMAN_AUTORECONF = NO
 PIXMAN_INSTALL_STAGING = YES
+PIXMAN_DEPENDENCIES = host-pkg-config
+# don't build gtk based demos
+PIXMAN_CONF_OPT = --disable-gtk
 
 $(eval $(call AUTOTARGETS))
 $(eval $(call AUTOTARGETS,host))
