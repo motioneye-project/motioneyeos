cee43cbcbd698f95025ecf3fe0697ecedf5f4aab
Will Newton <will.newton@gmail.com>
2012-02-02 11:28:11 +0000

    liboauth: Add new package
    [Peter: swap libcurl test around]
    Signed-off-by: Will Newton <will.newton@imgtec.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/liboauth/liboauth.mk
@@ -0,0 +1,19 @@
+#############################################################
+#
+# liboauth
+#
+#############################################################
+LIBOAUTH_VERSION = 0.9.6
+LIBOAUTH_SOURCE = liboauth-$(LIBOAUTH_VERSION).tar.gz
+LIBOAUTH_SITE = http://liboauth.sourceforge.net/pool
+LIBOAUTH_INSTALL_STAGING = YES
+
+LIBOAUTH_DEPENDENCIES += host-pkg-config openssl
+
+ifeq ($(BR2_PACKAGE_LIBCURL),y)
+LIBOAUTH_DEPENDENCIES += libcurl
+else
+LIBOAUTH_CONF_OPT += --disable-libcurl
+endif
+
+$(eval $(call AUTOTARGETS))
