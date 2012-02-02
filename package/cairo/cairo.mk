dfa93b47f721c4161dcb782cd48cb2fb52493d73
Will Newton <will.newton@imgtec.com>
2012-02-02 12:00:23 +0000

    cairo: Bump version to 1.10.2
    Update the DirectFB patch as it no longer applied cleanly.
    
    [Peter: drop redundant AUTORECONF = NO line]
    Signed-off-by: Will Newton <will.newton@imgtec.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/cairo/cairo.mk
+++ package/cairo/cairo.mk
@@ -3,12 +3,10 @@
 # cairo
 #
 #############################################################
-CAIRO_VERSION = 1.8.10
+CAIRO_VERSION = 1.10.2
 CAIRO_SOURCE = cairo-$(CAIRO_VERSION).tar.gz
 CAIRO_SITE = http://cairographics.org/releases
-CAIRO_AUTORECONF = NO
 CAIRO_INSTALL_STAGING = YES
-CAIRO_INSTALL_TARGET = YES
 
 CAIRO_CONF_ENV = ac_cv_func_posix_getpwuid_r=yes glib_cv_stack_grows=no \
 		glib_cv_uscore=no ac_cv_func_strtod=yes \
