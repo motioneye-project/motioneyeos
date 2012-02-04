4a6f72650681ea55370cbe21ef02aca266daf5ea
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-02-04 15:48:46 +0100

    lttng-tools: make the dependency on lttng-libust optional
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/lttng-tools/lttng-tools.mk
+++ package/lttng-tools/lttng-tools.mk
@@ -6,8 +6,15 @@ LTTNG_TOOLS_SOURCE  = lttng-tools-$(LTTNG_TOOLS_VERSION).tar.bz2
 # dependency. However, having the babeltrace utilities built for the
 # host is very useful, since those tools allow to convert the binary
 # trace format into an human readable format.
-LTTNG_TOOLS_DEPENDENCIES = liburcu popt host-lttng-babeltrace lttng-libust
+LTTNG_TOOLS_DEPENDENCIES = liburcu popt host-lttng-babeltrace
 
 LTTNG_AUTORECONF = YES
+
+ifeq ($(BR2_PACKAGE_LTTNG_LIBUST),y)
+LTTNG_TOOLS_CONF_OPT += --enable-lttng-ust
+LTTNG_TOOLS_DEPENDENCIES += lttng-libust
+else
+LTTNG_TOOLS_CONF_OPT += --disable-lttng-ust
+endif
 
 $(eval $(call AUTOTARGETS))
