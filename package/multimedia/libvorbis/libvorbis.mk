7b236b937df3507d4d7abd1fc58dae523df5a59e
Peter Korsgaard <jacmet@sunsite.dk>
2012-02-05 22:14:54 +0100

    libvorbis: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/libvorbis/libvorbis.mk
+++ package/multimedia/libvorbis/libvorbis.mk
@@ -4,12 +4,10 @@
 #
 #############################################################
 
-LIBVORBIS_VERSION = 1.3.2
+LIBVORBIS_VERSION = 1.3.3
 LIBVORBIS_SOURCE = libvorbis-$(LIBVORBIS_VERSION).tar.gz
 LIBVORBIS_SITE = http://downloads.xiph.org/releases/vorbis/$(LIBVORBIS-SOURCE)
-LIBVORBIS_AUTORECONF = NO
 LIBVORBIS_INSTALL_STAGING = YES
-LIBVORBIS_INSTALL_TARGET = YES
 
 LIBVORBIS_CONF_OPT = --disable-oggtest
 
