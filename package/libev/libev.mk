0802fc24e999ca42cbc7ac1a8596ded6d6d8c729
Yegor Yefremov <yegorslists@googlemail.com>
2012-02-06 10:55:34 +0100

    libev: bump to 4.11
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libev/libev.mk
+++ package/libev/libev.mk
@@ -3,7 +3,7 @@
 # libev
 #
 #############################################################
-LIBEV_VERSION = 4.04
+LIBEV_VERSION = 4.11
 LIBEV_SOURCE = libev-$(LIBEV_VERSION).tar.gz
 LIBEV_SITE = http://dist.schmorp.de/libev/
 LIBEV_INSTALL_STAGING = YES
