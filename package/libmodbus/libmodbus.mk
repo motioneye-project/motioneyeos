4baff8bb2266f5dd2bdd84ee9244ca4f591675a8
Yegor Yefremov <yegorslists@googlemail.com>
2012-02-07 10:18:30 +0100

    libmodbus: bump to 3.0.2
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libmodbus/libmodbus.mk
+++ package/libmodbus/libmodbus.mk
@@ -3,7 +3,7 @@
 # libmodbus
 #
 #############################################################
-LIBMODBUS_VERSION = 3.0.1
+LIBMODBUS_VERSION = 3.0.2
 LIBMODBUS_SITE = http://github.com/downloads/stephane/libmodbus
 LIBMODBUS_SOURCE = libmodbus-$(LIBMODBUS_VERSION).tar.gz
 LIBMODBUS_INSTALL_STAGING = YES
