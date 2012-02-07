d444e1d4c328d88f1f8befe6912e816876c1cbf4
Alexandre Belloni <alexandre.belloni@piout.net>
2012-02-07 12:01:25 +0100

    Add lpc3250loader to the host tools
    [Peter: use install -D for installation]
    Signed-off-by: Alexandre Belloni <alexandre.belloni@piout.net>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/lpc3250loader/lpc3250loader.mk
@@ -0,0 +1,9 @@
+HOST_LPC3250LOADER_SITE = git://gitorious.org/lpc3250loader/lpc3250loader.git
+HOST_LPC3250LOADER_VERSION = 1.0
+
+define HOST_LPC3250LOADER_INSTALL_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/LPC3250loader.py \
+		$(HOST_DIR)/usr/bin/LPC3250loader.py
+endef
+
+$(eval $(call GENTARGETS,host))
