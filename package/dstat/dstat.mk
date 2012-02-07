9b9765b8a3dfb1aa2f0f48092df19523aac78f8a
Thomas De Schampheleire <patrickdepinguin+buildroot@gmail.com>
2012-02-07 21:51:22 +0100

    Add package dstat (python script for system stats/profiling)
    [Peter: depend on WCHAR as python needs it, fix white space]
    Signed-off-by: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/dstat/dstat.mk
@@ -0,0 +1,15 @@
+#############################################################
+#
+# dstat
+#
+#############################################################
+
+DSTAT_VERSION = 0.7.2
+DSTAT_SOURCE = dstat-$(DSTAT_VERSION).tar.bz2
+DSTAT_SITE = http://dag.wieers.com/home-made/dstat
+
+define DSTAT_INSTALL_TARGET_CMDS
+       $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
+endef
+
+$(eval $(call GENTARGETS))
