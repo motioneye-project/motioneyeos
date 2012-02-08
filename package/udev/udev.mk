f94818b0fa77644b989bab5aa0866de7fe4ffbb1
Yegor Yefremov <yegorslists@googlemail.com>
2012-02-08 10:50:25 +0100

    udev: bump to 181 and other fixes
    Changes:
    - Linux kernel requirement: 2.6.34 and above (devtmpfs is mandatory)
    (this also applies to the chosen toolchain, because of Kernel headers)
    - optional dependency on libusb and usbutils removed
    - added dependency on kmod
    - added dependency on util-linux for libblkid
    - install in /lib/udev instead of /usr/libexec/udev
    - udevd moved to /lib/udev
    - fixed path to pci.ids and usb.ids
    - persistent rules generator is disabled by default, so option
    is introduced to enable this option if desired
    
    [Peter: fix build on uClibc, Config.in tweaks]
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/udev/udev.mk
+++ package/udev/udev.mk
@@ -3,28 +3,35 @@
 # udev
 #
 #############################################################
-UDEV_VERSION = 173
+UDEV_VERSION = 181
 UDEV_SOURCE = udev-$(UDEV_VERSION).tar.bz2
 UDEV_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kernel/hotplug/
 UDEV_INSTALL_STAGING = YES
 
+# mq_getattr is in librt
+UDEV_CONF_ENV += LIBS=-lrt
+
 UDEV_CONF_OPT =			\
 	--sbindir=/sbin		\
 	--with-rootlibdir=/lib	\
-	--libexecdir=/lib/udev	\
+	--libexecdir=/lib	\
+	--with-usb-ids-path=/usr/share/hwdata/usb.ids	\
+	--with-pci-ids-path=/usr/share/hwdata/pci.ids	\
+	--with-firmware-path=/lib/firmware		\
 	--disable-introspection
 
-UDEV_DEPENDENCIES = host-gperf host-pkg-config
+UDEV_DEPENDENCIES = host-gperf host-pkg-config util-linux kmod
+
+ifeq ($(BR2_PACKAGE_UDEV_RULES_GEN),y)
+UDEV_CONF_OPT += --enable-rule_generator
+endif
 
 ifeq ($(BR2_PACKAGE_UDEV_ALL_EXTRAS),y)
-UDEV_DEPENDENCIES += libusb libusb-compat acl usbutils hwdata libglib2
-UDEV_CONF_OPT +=							\
-	--with-pci-ids-path=$(TARGET_DIR)/usr/share/hwdata/pci.ids	\
-	--with-usb-ids-path=$(TARGET_DIR)/usr/share/hwdata/usb.ids	\
+UDEV_DEPENDENCIES += acl hwdata libglib2
+UDEV_CONF_OPT +=		\
 	--enable-udev_acl
 else
 UDEV_CONF_OPT +=		\
-	--disable-hwdb		\
 	--disable-gudev
 endif
 
