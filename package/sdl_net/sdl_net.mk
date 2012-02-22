bf6a6e441a8a924ba25298cbff6d20665403c0c3
Julien Boibessot <julien.boibessot@armadeus.com>
2012-02-22 14:47:57 +0100

    sdl_net: bump version, fix tarball name
    Bump version & cleanup variables setting while we are at it.
    
    Signed-off-by: Julien Boibessot <julien.boibessot@armadeus.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sdl_net/sdl_net.mk
+++ package/sdl_net/sdl_net.mk
@@ -3,8 +3,9 @@
 # SDL_net: network addon for SDL
 #
 #############################################################
-SDL_NET_VERSION:=1.2.7
-SDL_NET_SITE:=http://www.libsdl.org/projects/SDL_net/release
+SDL_NET_VERSION = 1.2.8
+SDL_NET_SITE = http://www.libsdl.org/projects/SDL_net/release
+SDL_NET_SOURCE = SDL_net-$(SDL_NET_VERSION).tar.gz
 
 SDL_NET_CONF_OPT = --localstatedir=/var \
 		--with-sdl-prefix=$(STAGING_DIR)/usr \
