b38f41736e33df75a802828dfc5f6673f6419e8b
Peter Korsgaard <jacmet@sunsite.dk>
2012-03-04 20:34:02 +0100

    glib-networking: fix build without gnutls
    configure errors out when it doesn't find gnutls unless explicitly
    disabled.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/glib-networking/glib-networking.mk
+++ package/glib-networking/glib-networking.mk
@@ -16,7 +16,9 @@ GLIB_NETWORKING_DEPENDENCIES = $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext libin
 
 ifeq ($(BR2_PACKAGE_GNUTLS),y)
 GLIB_NETWORKING_DEPENDENCIES += gnutls
-GLIB_NETWORKING_CONF_OPT = --with-libgcrypt-prefix=$(STAGING_DIR)/usr
+GLIB_NETWORKING_CONF_OPT += --with-libgcrypt-prefix=$(STAGING_DIR)/usr
+else
+GLIB_NETWORKING_CONF_OPT += --without-gnutls
 endif
 
 $(eval $(call AUTOTARGETS))
