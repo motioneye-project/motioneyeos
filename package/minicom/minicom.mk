608e6af167bd692611c9f60efe4f57dadc8b7fd7
Simon Dawson <spdawson@gmail.com>
2012-03-09 13:18:03 +0000

    New package minicom
    [Peter: fix whitespace and deps (wchar, ncurses, only iconv if !locale)]
    Signed-off-by: Simon Dawson <spdawson at gmail.com>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/minicom/minicom.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# minicom
+#
+#############################################################
+MINICOM_VERSION = 2.6
+MINICOM_SOURCE = minicom-$(MINICOM_VERSION).tar.gz
+MINICOM_SITE = http://alioth.debian.org/frs/download.php/3689/
+
+# pkg-config is only used to check for liblockdev, which we don't have
+# in BR, so instead of adding host-pkg-config as a dependency, simply
+# make sure the host version isn't used so we don't end up with problems
+# if people have liblockdev1-dev installed
+MINICOM_CONF_ENV = PKG_CONFIG=/bin/false
+
+MINICOM_DEPENDENCIES = ncurses $(if $(BR2_ENABLE_LOCALE),,libiconv)
+
+$(eval $(call AUTOTARGETS))
