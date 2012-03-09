7f65decfa7a3dcaa49d63a367a872094167b5edc
Yegor Yefremov <yegorslists@googlemail.com>
2012-03-09 15:39:46 +0100

    tiff: bump to 4.0.1
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tiff/tiff.mk
+++ package/tiff/tiff.mk
@@ -3,11 +3,10 @@
 # tiff
 #
 #############################################################
-TIFF_VERSION:=3.9.4
-TIFF_SITE:=ftp://ftp.remotesensing.org/pub/libtiff
-TIFF_SOURCE:=tiff-$(TIFF_VERSION).tar.gz
+TIFF_VERSION = 4.0.1
+TIFF_SITE = ftp://ftp.remotesensing.org/pub/libtiff
+TIFF_SOURCE = tiff-$(TIFF_VERSION).tar.gz
 TIFF_INSTALL_STAGING = YES
-TIFF_INSTALL_TARGET = YES
 TIFF_CONF_OPT = \
 	--disable-cxx \
 	--without-x \
