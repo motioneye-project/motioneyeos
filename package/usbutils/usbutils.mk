3df335ff8c5582771eceecd647323c8da528627b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-03-11 18:47:55 -0300

    usbutils: bump version to 005
    Bump usbutils version to 005 and also switch to a debian mirror since
    it's still missing upstream.
    
    [Peter: add comment about autoreconf, use snapshot.debian.org]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/usbutils/usbutils.mk
+++ package/usbutils/usbutils.mk
@@ -4,10 +4,13 @@
 #
 #############################################################
 
-USBUTILS_VERSION = 004
-USBUTILS_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/usb/usbutils
+USBUTILS_VERSION = 005
+USBUTILS_SITE = http://snapshot.debian.org/archive/debian/20111211T214105Z/pool/main/u/usbutils/
+USBUTILS_SOURCE = usbutils_$(USBUTILS_VERSION).orig.tar.gz
 USBUTILS_DEPENDENCIES = host-pkg-config libusb
 USBUTILS_INSTALL_STAGING = YES
+# no configure in tarball
+USBUTILS_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_USBUTILS_ZLIB),y)
 	USBUTILS_DEPENDENCIES += zlib
