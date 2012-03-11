4e57e9ce7704916d3409f7e5d63c8ca3776c06f0
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-03-11 18:47:56 -0300

    ntfs-3g: bump to version 2012.1.15
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ntfs-3g/ntfs-3g.mk
+++ package/ntfs-3g/ntfs-3g.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-NTFS_3G_VERSION = 2011.4.12
+NTFS_3G_VERSION = 2012.1.15
 NTFS_3G_SOURCE = ntfs-3g_ntfsprogs-$(NTFS_3G_VERSION).tgz
 NTFS_3G_SITE = http://tuxera.com/opensource
 NTFS_3G_CONF_OPT = --disable-ldconfig
