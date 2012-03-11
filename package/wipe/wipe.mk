1f8442b5c287660050b59951a8f0a1e32cb7be4d
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-03-11 18:48:07 -0300

    wipe: bump to version 2.3.1 and switch to sourceforge
    The original version is gone, so switch to sourceforge wipe and bump to
    version 2.3.1
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/wipe/wipe.mk
+++ package/wipe/wipe.mk
@@ -4,20 +4,10 @@
 #
 #############################################################
 
-WIPE_VERSION = 0.22
-WIPE_SITE = http://lambda-diode.com/resources/wipe
-WIPE_CFLAGS = $(TARGET_CFLAGS) -DHAVE_DEV_URANDOM -DHAVE_OSYNC -DHAVE_STRCASECMP -DHAVE_RANDOM -DSYNC_WAITS_FOR_SYNC -DFIND_DEVICE_SIZE_BY_BLKGETSIZE
-
-ifeq ($(BR2_LARGEFILE),y)
-WIPE_CFLAGS += -DSIXTYFOUR
-endif
-
-define WIPE_BUILD_CMDS
-	# Fix busted git version logic
-	$(SED) "s/which/!which/" $(@D)/Makefile
-	$(MAKE) -C $(@D) linux CC_LINUX="$(TARGET_CC)" \
-		CCO_LINUX="$(WIPE_CFLAGS)"
-endef
+WIPE_VERSION = 2.3.1
+WIPE_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/wipe
+WIPE_SOURCE = wipe-$(WIPE_VERSION).tar.bz2
+WIPE_AUTORECONF = YES
 
 define WIPE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/wipe $(TARGET_DIR)/usr/bin/wipe
@@ -29,8 +19,4 @@ define WIPE_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/share/man/man1/wipe.1
 endef
 
-define WIPE_CLEAN_CMDS
-	$(MAKE) -C $(@D) clean
-endef
-
-$(eval $(call GENTARGETS))
+$(eval $(call AUTOTARGETS))
