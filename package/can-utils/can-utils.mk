0ffcd60a25fa1d93809cdb5ed354a62adae72759
Yegor Yefremov <yegorslists@googlemail.com>
2012-03-12 11:47:21 +0100

    can-utils: bump version and change download location
    SocketCAN repository moved from Berlios.de to gitorious
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/can-utils/can-utils.mk
+++ package/can-utils/can-utils.mk
@@ -4,8 +4,8 @@
 #
 #############################################################
 
-CAN_UTILS_VERSION = 1235
-CAN_UTILS_SITE = svn://svn.berlios.de/socketcan/trunk/can-utils
+CAN_UTILS_VERSION = 50775159276d896d8b3102b6dbc658a91a2a1d53
+CAN_UTILS_SITE = git://gitorious.org/linux-can/can-utils.git
 CAN_UTILS_AUTORECONF = YES
 
 $(eval $(call AUTOTARGETS))
