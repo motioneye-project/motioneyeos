5dce78c22a6135d0e7367025079c216107934111
Sagaert Johan <sagaert.johan@skynet.be>
2012-03-12 13:30:33 +0100

    libffi: bump version to 3.0.10
    Signed-off-by: Sagaert Johan <sagaert.johan@skynet.be>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libffi/libffi.mk
+++ package/libffi/libffi.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBFFI_VERSION = 3.0.9
+LIBFFI_VERSION = 3.0.10
 LIBFFI_SITE    = ftp://sources.redhat.com/pub/libffi/
 
 LIBFFI_INSTALL_STAGING = YES
