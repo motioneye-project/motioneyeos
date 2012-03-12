8b51e3cfc8f386d1f59fa8f9447ce90428d2a267
Yegor Yefremov <yegorslists@googlemail.com>
2012-03-12 11:02:09 +0100

    pixman: bump version and compilation fix
    - bump to 2.5 and change download location
    - disable iwmmxt support for CPU's that don't have
      this feature
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/pixman/pixman.mk
+++ package/pixman/pixman.mk
@@ -3,13 +3,18 @@
 # pixman
 #
 ################################################################################
-PIXMAN_VERSION = 0.24.2
-PIXMAN_SOURCE = pixman-$(PIXMAN_VERSION).tar.gz
-PIXMAN_SITE = http://cairographics.org/releases/
+PIXMAN_VERSION = 0.25.2
+PIXMAN_SITE = http://xorg.freedesktop.org/releases/individual/lib
 PIXMAN_INSTALL_STAGING = YES
 PIXMAN_DEPENDENCIES = host-pkg-config
 # don't build gtk based demos
 PIXMAN_CONF_OPT = --disable-gtk
+
+# disable iwmmxt support for CPU's that don't have
+# this feature
+ifneq ($(BR2_iwmmxt),y)
+PIXMAN_CONF_OPT += --disable-arm-iwmmxt
+endif
 
 $(eval $(call AUTOTARGETS))
 $(eval $(call AUTOTARGETS,host))
