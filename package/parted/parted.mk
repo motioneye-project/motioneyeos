aa9f6a9b133104b54c1abc0db203c30b6c0ea364
Yegor Yefremov <yegorslists@googlemail.com>
2012-03-12 11:24:04 +0100

    New package: parted
    [Peter: needs largefile + wchar]
    Signed-off-by: Nicolas Dechesne <n-dechesne@ti.com>
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/parted/parted.mk
@@ -0,0 +1,13 @@
+#############################################################
+#
+# parted
+#
+#############################################################
+
+PARTED_VERSION = 3.1
+PARTED_SOURCE = parted-$(PARTED_VERSION).tar.xz
+PARTED_SITE = $(BR2_GNU_MIRROR)/parted
+PARTED_DEPENDENCIES = readline util-linux lvm2
+PARTED_INSTALL_STAGING = YES
+
+$(eval $(call AUTOTARGETS))
