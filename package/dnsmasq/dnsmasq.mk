f1e2288d157b5cd8b4b8a037dfb77eb804703a53
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-03-13 10:20:15 -0300

    dnsmasq: bump to version 2.60
    Bump to version 2.60 of dnsmasq with the new lua scripting option.
    Switch LDFLAGS from MAKE_ENV to MAKE_OPT to make it work properly.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/dnsmasq/dnsmasq.mk
+++ package/dnsmasq/dnsmasq.mk
@@ -4,11 +4,11 @@
 #
 #############################################################
 
-DNSMASQ_VERSION = 2.59
+DNSMASQ_VERSION = 2.60
 DNSMASQ_SITE = http://thekelleys.org.uk/dnsmasq
-DNSMASQ_MAKE_ENV = CC="$(TARGET_CC)" LDFLAGS="$(TARGET_LDFLAGS)"
+DNSMASQ_MAKE_ENV = CC="$(TARGET_CC)"
 DNSMASQ_MAKE_OPT = COPTS="$(DNSMASQ_COPTS)" PREFIX=/usr CFLAGS="$(TARGET_CFLAGS)"
-DNSMASQ_MAKE_OPT += DESTDIR=$(TARGET_DIR)
+DNSMASQ_MAKE_OPT += DESTDIR=$(TARGET_DIR) LDFLAGS="$(TARGET_LDFLAGS)"
 
 ifneq ($(BR2_INET_IPV6),y)
 	DNSMASQ_COPTS += -DNO_IPV6
@@ -25,7 +25,20 @@ endif
 ifeq ($(BR2_PACKAGE_DNSMASQ_IDN),y)
 	DNSMASQ_MAKE_OPT += all-i18n
 	DNSMASQ_DEPENDENCIES += libidn libintl
-	DNSMASQ_MAKE_ENV += LDFLAGS+="-lintl -lidn"
+	DNSMASQ_MAKE_OPT += LDFLAGS+="-lintl -lidn"
+endif
+
+ifeq ($(BR2_PACKAGE_DNSMASQ_LUA),y)
+	DNSMASQ_DEPENDENCIES += lua
+	DNSMASQ_MAKE_OPT += LDFLAGS+="-ldl"
+endif
+
+ifeq ($(BR2_PACKAGE_DNSMASQ_LUA),y)
+define DNSMASQ_ENABLE_LUA
+	$(SED) 's/lua5.1/lua/g' $(DNSMASQ_DIR)/Makefile
+	$(SED) 's^.*#define HAVE_LUASCRIPT.*^#define HAVE_LUASCRIPT^' \
+		$(DNSMASQ_DIR)/src/config.h
+endef
 endif
 
 ifneq ($(BR2_LARGEFILE),y)
@@ -36,12 +49,15 @@ ifeq ($(BR2_PACKAGE_DBUS),y)
 	DNSMASQ_DEPENDENCIES += host-pkg-config dbus
 endif
 
+define DNSMASQ_FIX_PKGCONFIG
+	$(SED) 's^PKG_CONFIG = pkg-config^PKG_CONFIG = $(PKG_CONFIG_HOST_BINARY)^' \
+		$(DNSMASQ_DIR)/Makefile
+endef
+
 ifeq ($(BR2_PACKAGE_DBUS),y)
 define DNSMASQ_ENABLE_DBUS
 	$(SED) 's^.*#define HAVE_DBUS.*^#define HAVE_DBUS^' \
 		$(DNSMASQ_DIR)/src/config.h
-	$(SED) 's^PKG_CONFIG = pkg-config^PKG_CONFIG = $(PKG_CONFIG_HOST_BINARY)^' \
-		$(DNSMASQ_DIR)/Makefile
 endef
 else
 define DNSMASQ_ENABLE_DBUS
@@ -51,7 +67,9 @@ endef
 endif
 
 define DNSMASQ_BUILD_CMDS
+	$(DNSMASQ_FIX_PKGCONFIG)
 	$(DNSMASQ_ENABLE_DBUS)
+	$(DNSMASQ_ENABLE_LUA)
 	$(DNSMASQ_MAKE_ENV) $(MAKE) -C $(@D) $(DNSMASQ_MAKE_OPT)
 endef
 
