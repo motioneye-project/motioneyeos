06211219a06c795d2c5d4352a1637a4d60f881ba
Thomas De Schampheleire <patrickdepinguin+buildroot@gmail.com>
2012-03-14 17:38:16 +0100

    new package: zeromq
    [Peter: fix C++/LF deps, reformat help, install into staging]
    Signed-off-by: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/zeromq/zeromq.mk
@@ -0,0 +1,12 @@
+#############################################################
+#
+# zeromq
+#
+#############################################################
+
+ZEROMQ_VERSION = 2.1.11
+ZEROMQ_SITE = http://download.zeromq.org/
+ZEROMQ_INSTALL_STAGING = YES
+ZEROMQ_DEPENDENCIES = util-linux
+
+$(eval $(call AUTOTARGETS))
