d90dd7c5e8491135c50ae872a0c459556d304785
Yann E. MORIN <yann.morin.1998@free.fr>
2012-03-15 23:54:09 +0100

    package/giblib: fix breakage
    Fix two things in the giblib package:
    
    1- fix prefix breakage
       - s/STAGING/STAGING_DIR/ (Thomas)
       - prefix is .../usr, not .../usr/lib or .../usr/bin
    
    2- fix the configure.ac/Makefile.am breakage
       - imlib2 sets IMLIB2_(LIBS|CFLAGS), not IMLIB_(LIBS|CFLAGS)
       - autoreconf the package, install missing files
         Note that both configure.ac and configure.in are present in the
         package, so both are fixed, although only configure.ac is used
         (configure.in is the legacy name).
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    CC: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/giblib/giblib.mk
+++ package/giblib/giblib.mk
@@ -8,7 +8,9 @@ GIBLIB_SOURCE = giblib-$(GIBLIB_VERSION).tar.gz
 GIBLIB_SITE = http://linuxbrit.co.uk/downloads/
 GIBLIB_INSTALL_STAGING = YES
 GIBLIB_DEPENDENCIES = imlib2
-GIBLIB_CONF_OPT = --with-imlib2-prefix=$(STAGING)/usr/lib \
-		  --with-imlib2-exec-prefix=$(STAGING)/usr/bin
+GIBLIB_AUTORECONF = YES
+GIBLIB_AUTORECONF_OPT = --install
+GIBLIB_CONF_OPT = --with-imlib2-prefix=$(STAGING_DIR)/usr \
+		  --with-imlib2-exec-prefix=$(STAGING_DIR)/usr
 
 $(eval $(call AUTOTARGETS))
