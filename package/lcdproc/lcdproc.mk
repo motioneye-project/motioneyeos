ccb44fd52f76bca3cbd6a89bc9407ada5ab47d65
Simon Dawson <spdawson@gmail.com>
2012-03-15 08:49:42 +0000

    Added lcdproc package
    [Peter: fix download URL, move to 'Hardware handling' section]
    Signed-off-by: Simon Dawson <spdawson at gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/lcdproc/lcdproc.mk
@@ -0,0 +1,14 @@
+#############################################################
+#
+# lcdproc
+#
+#############################################################
+LCDPROC_VERSION = 0.5.5
+LCDPROC_SOURCE = lcdproc-$(LCDPROC_VERSION).tar.gz
+LCDPROC_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/lcdproc/lcdproc/$(LCDPROC_VERSION)
+
+LCDPROC_CONF_OPT = --enable-drivers=all
+
+LCDPROC_DEPENDENCIES = ncurses
+
+$(eval $(call AUTOTARGETS))
