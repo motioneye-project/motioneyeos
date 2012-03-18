81302ba68569ff62566fe463a4a8ac1a1d12baca
Nicolas Dechesne <n-dechesne@ti.com>
2012-03-18 23:04:51 +0100

    uboot: add a new binary format for u-boot.img
    For some platforms like OMAP, a new binary format is now being used
    for u-boot: u-boot.img. It is basically u-boot.bin which has been
    processed with mkimage.
    
    Signed-off-by: Nicolas Dechesne <n-dechesne@ti.com>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Acked-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- boot/uboot/uboot.mk
+++ boot/uboot/uboot.mk
@@ -29,6 +29,8 @@ else ifeq ($(BR2_TARGET_UBOOT_FORMAT_LDR),y)
 UBOOT_BIN          = u-boot.ldr
 else ifeq ($(BR2_TARGET_UBOOT_FORMAT_NAND_BIN),y)
 UBOOT_BIN          = u-boot-nand.bin
+else ifeq ($(BR2_TARGET_UBOOT_FORMAT_IMG),y)
+UBOOT_BIN          = u-boot.img
 else
 UBOOT_BIN          = u-boot.bin
 endif
