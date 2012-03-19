f21f4d43b325c3cbad126b8f10fd24498c6af285
Simon Dawson <spdawson@gmail.com>
2012-03-19 10:23:55 +0000

    Added the libtpl package
    [Peter: drop _INSTALL_TARGET = NO as it installs a shared lib]
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libtpl/libtpl.mk
@@ -0,0 +1,11 @@
+#############################################################
+#
+# libtpl
+#
+#############################################################
+LIBTPL_VERSION = 1.5
+LIBTPL_SOURCE = libtpl-$(LIBTPL_VERSION).tar.bz2
+LIBTPL_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/tpl
+LIBTPL_INSTALL_STAGING = YES
+
+$(eval $(call AUTOTARGETS))
