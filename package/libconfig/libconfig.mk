5b84ec26c8981c6ab0e119b455e807152ded449a
Yegor Yefremov <yegorslists@googlemail.com>
2012-03-21 12:02:36 +0100

    libconfig: bump to 1.4.8 and cleanup
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libconfig/libconfig.mk
+++ package/libconfig/libconfig.mk
@@ -3,13 +3,12 @@
 # libconfig
 #
 #############################################################
-LIBCONFIG_VERSION:=1.4.6
-LIBCONFIG_SOURCE:=libconfig-$(LIBCONFIG_VERSION).tar.gz
-LIBCONFIG_SITE:=http://www.hyperrealm.com/libconfig/
-LIBCONFIG_INSTALL_STAGING:=YES
+LIBCONFIG_VERSION = 1.4.8
+LIBCONFIG_SITE = http://www.hyperrealm.com/libconfig/
+LIBCONFIG_INSTALL_STAGING = YES
 
 ifneq ($(BR2_INSTALL_LIBSTDCPP),y)
-LIBCONFIG_CONF_OPT:=--disable-cxx
+LIBCONFIG_CONF_OPT = --disable-cxx
 endif
 
 $(eval $(call AUTOTARGETS))
