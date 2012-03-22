bdd73ea91717390637fdcaf17606f78590543238
Yegor Yefremov <yegorslists@googlemail.com>
2012-03-22 16:00:59 +0100

    New package: socketcand
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/socketcand/socketcand.mk
@@ -0,0 +1,11 @@
+#############################################################
+#
+# socketcand
+#
+#############################################################
+SOCKETCAND_VERSION = 7d06986fa4b5fd2c210ec4e248dab41107be1ccd
+SOCKETCAND_SITE = git://github.com/dschanoeh/socketcand.git
+SOCKETCAND_AUTORECONF = YES
+SOCKETCAND_DEPENDENCIES = libconfig
+
+$(eval $(call AUTOTARGETS))
