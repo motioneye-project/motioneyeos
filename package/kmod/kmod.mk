78f5ac2908b30aa6f67583a355e058d6703755ac
Peter Korsgaard <jacmet@sunsite.dk>
2012-03-22 16:02:14 +0100

    kmod: add option to install module utilities
    And ensure optional zlib+xz dependencies gets picked up.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/kmod/kmod.mk
+++ package/kmod/kmod.mk
@@ -4,4 +4,30 @@ KMOD_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kernel/kmod/
 KMOD_INSTALL_STAGING = YES
 KMOD_DEPENDENCIES = host-pkg-config
 
+ifeq ($(BR2_PACKAGE_ZLIB),y)
+KMOD_DEPENDENCIES += zlib
+KMOD_CONF_OPT += --with-zlib
+endif
+
+ifeq ($(BR2_PACKAGE_XZ),y)
+KMOD_DEPENDENCIES += xz
+KMOD_CONF_OPT += --with-xz
+endif
+
+ifeq ($(BR2_PACKAGE_KMOD_TOOLS),y)
+# take precedence over busybox / module-init-tools implementations
+KMOD_DEPENDENCIES += \
+	$(if $(BR2_PACKAGE_BUSYBOX),busybox) \
+	$(if $(BR2_PACKAGE_MODULE_INIT_TOOLS),module-init-tools)
+
+define KMOD_INSTALL_TOOLS
+	for i in depmod insmod lsmod modinfo modprobe rmmod; \
+	do ln -sf ../usr/bin/kmod $(TARGET_DIR)/sbin/$$i; done
+endef
+
+KMOD_POST_INSTALL_TARGET_HOOKS += KMOD_INSTALL_TOOLS
+else
+KMOD_CONF_OPT += --disable-tools
+endif
+
 $(eval $(call AUTOTARGETS))
