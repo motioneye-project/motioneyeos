47aa58a4ddd3774fa78acffbf85384c558b7f2dd
Frank Hunleth <fhunleth@troodon-software.com>
2012-03-25 14:15:40 +0000

    New package: zxing
    [Peter: build using make instead of scons, fixup whitespace, target install]
    Signed-off-by: Frank Hunleth <fhunleth@troodon-software.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/zxing/zxing.mk
@@ -0,0 +1,39 @@
+#############################################################
+#
+# zxing
+#
+#############################################################
+ZXING_VERSION = 2.0
+ZXING_SITE = http://zxing.googlecode.com/files
+ZXING_SOURCE = ZXing-$(ZXING_VERSION).zip
+ZXING_INSTALL_STAGING = YES
+
+ifneq ($(BR2_ENABLE_LOCALE),y)
+ZXING_DEPENDENCIES += libiconv
+endif
+
+define ZXING_EXTRACT_CMDS
+	unzip -d $(BUILD_DIR) $(DL_DIR)/$(ZXING_SOURCE)
+endef
+
+define ZXING_BUILD_CMDS
+	$(MAKE) -C $(@D)/cpp/core/src $(TARGET_CONFIGURE_OPTS)
+endef
+
+define ZXING_INSTALL_STAGING_CMDS
+	$(MAKE) -C $(@D)/cpp/core/src DESTDIR=$(STAGING_DIR) install
+endef
+
+define ZXING_UNINSTALL_STAGING_CMDS
+	$(MAKE) -C $(@D)/cpp/core/src DESTDIR=$(STAGING_DIR) uninstall
+endef
+
+define ZXING_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D)/cpp/core/src DESTDIR=$(TARGET_DIR) install
+endef
+
+define ZXING_UNINSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D)/cpp/core/src DESTDIR=$(TARGET_DIR) uninstall
+endef
+
+$(eval $(call GENTARGETS))
