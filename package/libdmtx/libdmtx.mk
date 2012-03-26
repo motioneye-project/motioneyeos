2f41adabc5a12fcca1de5b6c296c2206b518abff
Frank Hunleth <fhunleth@troodon-software.com>
2012-03-26 10:51:52 -0400

    New package: libdmtx
    Signed-off-by: Frank Hunleth <fhunleth@troodon-software.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libdmtx/libdmtx.mk
@@ -0,0 +1,12 @@
+#############################################################
+#
+# libdmtx
+#
+#############################################################
+
+LIBDMTX_VERSION = 0.7.4
+LIBDMTX_SOURCE = libdmtx-$(LIBDMTX_VERSION).tar.gz
+LIBDMTX_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/libdmtx
+LIBDMTX_INSTALL_STAGING = YES
+
+$(eval $(call AUTOTARGETS))
