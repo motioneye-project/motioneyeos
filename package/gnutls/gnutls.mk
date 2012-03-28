11c5f6971a8d179fb02a432ae988cde80b0f5212
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-03-28 18:17:20 -0300

    gnutls: security bump to version 2.12.18
    Fixes CVE-2012-1573
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gnutls/gnutls.mk
+++ package/gnutls/gnutls.mk
@@ -4,11 +4,12 @@
 #
 #############################################################
 
-GNUTLS_VERSION = 2.10.5
+GNUTLS_VERSION = 2.12.18
 GNUTLS_SOURCE = gnutls-$(GNUTLS_VERSION).tar.bz2
-GNUTLS_SITE = http://ftp.gnu.org/gnu/gnutls/
-GNUTLS_DEPENDENCIES = libgcrypt
-GNUTLS_CONF_OPT += --without-libgcrypt-prefix
+GNUTLS_SITE = $(BR2_GNU_MIRROR)/gnutls
+GNUTLS_DEPENDENCIES = host-pkg-config libgcrypt
+GNUTLS_CONF_OPT = --with-libgcrypt --with-libgcrypt-prefix=$(STAGING_DIR)/usr \
+		--without-p11-kit
 GNUTLS_INSTALL_STAGING = YES
 
 $(eval $(call AUTOTARGETS))
