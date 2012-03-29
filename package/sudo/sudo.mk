61d3d740dba46f469ea368b9080091ce614455a8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-03-28 23:25:03 -0300

    sudo: bump to version 1.8.4p4
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sudo/sudo.mk
+++ package/sudo/sudo.mk
@@ -4,9 +4,8 @@
 #
 #############################################################
 
-SUDO_VERSION = 1.7.8p1
+SUDO_VERSION = 1.8.4p4
 SUDO_SITE    = http://www.sudo.ws/sudo/dist
-
 SUDO_CONF_OPT = \
 		--without-lecture \
 		--without-sendmail \
@@ -16,9 +15,11 @@ SUDO_CONF_OPT = \
 		--without-pam
 
 define SUDO_INSTALL_TARGET_CMDS
-	install -m 4555 -D $(@D)/sudo $(TARGET_DIR)/usr/bin/sudo
-	install -m 0555 -D $(@D)/visudo $(TARGET_DIR)/usr/sbin/visudo
-	install -m 0440 -D $(@D)/sudoers $(TARGET_DIR)/etc/sudoers
+	install -m 4555 -D $(@D)/src/sudo $(TARGET_DIR)/usr/bin/sudo
+	install -m 0555 -D $(@D)/plugins/sudoers/visudo  \
+		$(TARGET_DIR)/usr/sbin/visudo
+	install -m 0440 -D $(@D)/plugins/sudoers/sudoers \
+		$(TARGET_DIR)/etc/sudoers
 endef
 
 $(eval $(call AUTOTARGETS))
