180c7126dfe19ba7c22ae414d4d99501152f13ff
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-03-28 23:25:00 -0300

    tftp-hpa: bump to version 5.2
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tftpd/tftpd.mk
+++ package/tftpd/tftpd.mk
@@ -3,9 +3,10 @@
 # tftpd
 #
 #############################################################
-TFTPD_VERSION  = 5.0
+
+TFTPD_VERSION  = 5.2
 TFTPD_SOURCE   = tftp-hpa-$(TFTPD_VERSION).tar.bz2
-TFTPD_SITE     = $(BR2_KERNEL_MIRROR)/software/network/tftp/
+TFTPD_SITE     = $(BR2_KERNEL_MIRROR)/software/network/tftp/tftp-hpa
 TFTPD_CONF_OPT = --without-tcpwrappers
 
 ifneq ($(BR2_INET_IPV6),y)
