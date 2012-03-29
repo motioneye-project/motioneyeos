953b0f4de8ebcb5b42c8da760c025873ce82550d
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-03-28 23:25:01 -0300

    psmisc: bump to version 22.16
    [Peter: Don't disable stack protection if toolchain has ssp support]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/psmisc/psmisc.mk
+++ package/psmisc/psmisc.mk
@@ -4,9 +4,14 @@
 #
 #############################################################
 
-PSMISC_VERSION = 22.14
-PSMISC_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/psmisc
+PSMISC_VERSION = 22.16
+PSMISC_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/psmisc/psmisc
 PSMISC_DEPENDENCIES = ncurses $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext libintl)
+
+ifneq ($(BR2_TOOLCHAIN_BUILDROOT_USE_SSP),y)
+# Don't force -fstack-protector
+PSMISC_CONF_OPT = --disable-harden-flags
+endif
 
 # build after busybox, we prefer fat versions while we're at it
 ifeq ($(BR2_PACKAGE_BUSYBOX),y)
