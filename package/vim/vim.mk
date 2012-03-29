f179d3315c781f2f3cbfc26092b67dc75417efd0
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-03-28 23:24:59 -0300

    vim: bump to version 7.3.487
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/vim/vim.mk
+++ package/vim/vim.mk
@@ -4,10 +4,10 @@
 #
 #############################################################
 
-# svn r1889 == 7.2 release + patchlevel 446
-VIM_SITE = https://vim.svn.sourceforge.net/svnroot/vim/branches/vim7.2
-VIM_SITE_METHOD = svn
-VIM_VERSION = 1889
+# hg 21219ffc9790 = 7.3 release + patchlevel 487
+VIM_SITE = https://vim.googlecode.com/hg
+VIM_SITE_METHOD = hg
+VIM_VERSION = 21219ffc9790
 VIM_DEPENDENCIES = ncurses
 VIM_SUBDIR = src
 VIM_CONF_ENV = vim_cv_toupper_broken=no \
@@ -19,8 +19,8 @@ VIM_CONF_ENV = vim_cv_toupper_broken=no \
 		vim_cv_memmove_handles_overlap=yes \
 		ac_cv_sizeof_int=4 \
 		ac_cv_small_wchar_t=no
-
-VIM_CONF_OPT = --with-tlib=ncurses
+# GUI/X11 headers leak from the host so forcibly disable them
+VIM_CONF_OPT = --with-tlib=ncurses --enable-gui=no --without-x
 
 define VIM_INSTALL_TARGET_CMDS
 	cd $(@D)/src; \
