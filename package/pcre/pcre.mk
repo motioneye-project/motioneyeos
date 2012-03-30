60a395708d99b1787800bedd983450d0097ce6af
Luca Ceresoli <luca@lucaceresoli.net>
2012-03-30 14:50:26 +0200

    pcre: bump to 8.30
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Acked-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/pcre/pcre.mk
+++ package/pcre/pcre.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-PCRE_VERSION = 8.20
+PCRE_VERSION = 8.30
 PCRE_SITE = ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre
 PCRE_INSTALL_STAGING = YES
 
