acf7bb7217d7de6981909112bdbd2af1faf680b5
Marek Belisko <marek.belisko@open-nandra.com>
2012-04-01 14:18:43 +0200

    package: add fmtools
    [Peter: fix install, help text]
    Signed-off-by: Marek Belisko <marek.belisko@open-nandra.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/fmtools/fmtools.mk
@@ -0,0 +1,19 @@
+#############################################################
+#
+# fmtools
+#
+#############################################################
+
+FMTOOLS_VERSION = 1.0.2
+FMTOOLS_SITE = http://www.stanford.edu/~blp/fmtools/
+
+define FMTOOLS_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define FMTOOLS_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/fm $(TARGET_DIR)/usr/sbin/fm
+	$(INSTALL) -D -m 0755 $(@D)/fmscan $(TARGET_DIR)/usr/sbin/fmscan
+endef
+
+$(eval $(call GENTARGETS))
