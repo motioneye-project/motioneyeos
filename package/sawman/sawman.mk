bc28297ce49f7749f6eab268010d3efe473a1353
Peter Korsgaard <jacmet@sunsite.dk>
2012-04-05 14:48:35 +0200

    sawman: bump version
    Old version was not compatible with dfb 1.4.16.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sawman/sawman.mk
+++ package/sawman/sawman.mk
@@ -3,7 +3,7 @@
 # SAWMAN
 #
 #############################################################
-SAWMAN_VERSION = 1.4.15
+SAWMAN_VERSION = 1.4.16
 SAWMAN_SOURCE = SaWMan-$(SAWMAN_VERSION).tar.gz
 SAWMAN_SITE = http://www.directfb.org/downloads/Extras
 SAWMAN_INSTALL_STAGING = YES
