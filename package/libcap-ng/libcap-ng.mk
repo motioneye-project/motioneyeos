5ee4614c4e72dfab4e8f36186fc8ef4ab05fb9a9
Peter Korsgaard <jacmet@sunsite.dk>
2012-04-05 22:42:35 +0200

    libcap-ng: explicit disable python bindings
    The python bindings use swig, which we don't have in buildroot (but might be
    installed on the host), so disable it.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libcap-ng/libcap-ng.mk
+++ package/libcap-ng/libcap-ng.mk
@@ -8,5 +8,7 @@ LIBCAP_NG_VERSION = 0.6.6
 LIBCAP_NG_SITE = http://people.redhat.com/sgrubb/libcap-ng/
 LIBCAP_NG_SOURCE = libcap-ng-$(LIBCAP_NG_VERSION).tar.gz
 LIBCAP_NG_INSTALL_STAGING = YES
+LIBCAP_NG_CONF_ENV = ac_cv_prog_swig_found=no
+LIBCAP_NG_CONF_OPT = --without-python
 
 $(eval $(call AUTOTARGETS))
