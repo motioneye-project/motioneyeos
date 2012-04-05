0c8244251e730c9873776f2d8d86d23d5a12af6c
Peter Korsgaard <jacmet@sunsite.dk>
2012-04-05 20:20:05 +0200

    fakeroot: bump version, cleanup package
    LFS patch is now upstream, and we don't support building fakeroot for the
    target anyway.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/fakeroot/fakeroot.mk
+++ package/fakeroot/fakeroot.mk
@@ -3,20 +3,8 @@
 # fakeroot
 #
 #############################################################
-FAKEROOT_VERSION:=1.9.5
-FAKEROOT_SOURCE:=fakeroot_$(FAKEROOT_VERSION).tar.gz
-FAKEROOT_SITE:=http://snapshot.debian.org/archive/debian/20080427T000000Z/pool/main/f/fakeroot/
+FAKEROOT_VERSION = 1.18.2
+FAKEROOT_SOURCE = fakeroot_$(FAKEROOT_VERSION).orig.tar.bz2
+FAKEROOT_SITE = http://snapshot.debian.org/archive/debian/20111201T093630Z/pool/main/f/fakeroot/
 
-define FAKEROOT_PATCH_FAKEROOT_IN
-	# If using busybox getopt, make it be quiet.
-	$(SED) "s,getopt --version,getopt --version 2>/dev/null," \
-		$(@D)/scripts/fakeroot.in
-endef
-
-FAKEROOT_POST_PATCH_HOOKS += FAKEROOT_PATCH_FAKEROOT_IN
-
-# The package for the target cannot be selected (build problems when
-# largefile is enabled), but is needed for the host package to work
-# due to deficiencies in the package infrastructure.
-$(eval $(call AUTOTARGETS))
 $(eval $(call AUTOTARGETS,host))
