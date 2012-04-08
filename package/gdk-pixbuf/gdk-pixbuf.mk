0b9eb5713d971cb9af7c75e92a9647e0fd449a9e
Peter Korsgaard <jacmet@sunsite.dk>
2012-04-08 08:24:25 +0200

    gdk-pixbuf: bump version
    The 2.26 series needs glib >= 2.31.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gdk-pixbuf/gdk-pixbuf.mk
+++ package/gdk-pixbuf/gdk-pixbuf.mk
@@ -4,9 +4,9 @@
 #
 #############################################################
 
-GDK_PIXBUF_MAJOR_VERSION = 2.22
-GDK_PIXBUF_VERSION = $(GDK_PIXBUF_MAJOR_VERSION).0
-GDK_PIXBUF_SOURCE = gdk-pixbuf-$(GDK_PIXBUF_VERSION).tar.bz2
+GDK_PIXBUF_MAJOR_VERSION = 2.24
+GDK_PIXBUF_VERSION = $(GDK_PIXBUF_MAJOR_VERSION).1
+GDK_PIXBUF_SOURCE = gdk-pixbuf-$(GDK_PIXBUF_VERSION).tar.xz
 GDK_PIXBUF_SITE = http://ftp.gnome.org/pub/gnome/sources/gdk-pixbuf/$(GDK_PIXBUF_MAJOR_VERSION)
 GDK_PIXBUF_INSTALL_STAGING = YES
 
