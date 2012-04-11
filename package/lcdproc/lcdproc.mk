acf2af2d91838b15a645bf17cb4a50688603b2af
spdawson@gmail.com <spdawson@gmail.com>
2012-04-11 13:12:30 +0100

    Add lcdproc config options, for driver selection
    Add a configuration menu item to specify a list of lcdproc drivers to
    be built. The list defaults to 'all'.
    
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/lcdproc/lcdproc.mk
+++ package/lcdproc/lcdproc.mk
@@ -7,7 +7,7 @@ LCDPROC_VERSION = 0.5.5
 LCDPROC_SOURCE = lcdproc-$(LCDPROC_VERSION).tar.gz
 LCDPROC_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/lcdproc/lcdproc/$(LCDPROC_VERSION)
 
-LCDPROC_CONF_OPT = --enable-drivers=all
+LCDPROC_CONF_OPT = --enable-drivers=$(BR2_PACKAGE_LCDPROC_DRIVERS)
 
 LCDPROC_DEPENDENCIES = ncurses
 
