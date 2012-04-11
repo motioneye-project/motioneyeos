b6e0f2fdf2665f67e2bc6400c8479d6fca7e2fe4
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-04-11 10:40:36 -0300

    samba: security bump to version 3.5.14
    Bump samba to version 3.5.14 to fix CVE-2012-1182
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/samba/samba.mk
+++ package/samba/samba.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SAMBA_VERSION = 3.5.12
+SAMBA_VERSION = 3.5.14
 SAMBA_SITE = http://ftp.samba.org/pub/samba/stable
 SAMBA_SUBDIR = source3
 SAMBA_INSTALL_STAGING = YES
