b4a8ae3e8cc9f5a7b0b4b6f7004c51e661dcc456
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-04-13 09:44:45 -0300

    toolchain/gcc: add 4.7.x series
    Add gcc 4.7.0 to the toolchain options.
    
    [Peter: drop 0001-toolchain-gcc-add-4.7.x-series.txt]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- toolchain/gcc/gcc-uclibc-4.x.mk
+++ toolchain/gcc/gcc-uclibc-4.x.mk
@@ -120,6 +120,16 @@ endif
 GCC_HOST_PREREQ += host-mpc
 endif
 
+# GCC 4.7.x prerequisites
+ifeq ($(findstring x4.7.,x$(GCC_VERSION)),x4.7.)
+GCC_WITH_HOST_MPC = --with-mpc=$(HOST_DIR)/usr
+GCC_TARGET_PREREQ += mpc
+ifeq ($(BR2_TOOLCHAIN_BUILDROOT),y)
+HOST_SOURCE += host-mpc-source
+endif
+GCC_HOST_PREREQ += host-mpc
+endif
+
 # GCC snapshot prerequisites
 # Since we don't know and it can be quite new just ask for everything known
 ifneq ($(GCC_SNAP_DATE),)
