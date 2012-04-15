5fadbb58abb54f816f13367c9af4323136f4cf49
Peter Korsgaard <jacmet@sunsite.dk>
2012-04-15 14:02:10 +0200

    screen: fix build when libelf is enabled
    screen's configure script thinks it is running on a SVR4 system when it
    detects libelf, breaking the build.
    
    At the same time cleanup the package somewhat, and ensure the correct
    CFLAGS are used.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/screen/screen.mk
+++ package/screen/screen.mk
@@ -7,8 +7,8 @@
 SCREEN_VERSION = 4.0.3
 SCREEN_SITE = $(BR2_GNU_MIRROR)/screen
 SCREEN_DEPENDENCIES = ncurses
-SCREEN_CONF_ENV = CFLAGS=-DSYSV=1
-SCREEN_MAKE_OPT = -j1
+SCREEN_CONF_ENV = ac_cv_header_elf_h=no ac_cv_header_dwarf_h=no
+SCREEN_MAKE = $(MAKE1)
 SCREEN_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) SCREEN=screen install_bin
 
 $(eval $(call AUTOTARGETS))
