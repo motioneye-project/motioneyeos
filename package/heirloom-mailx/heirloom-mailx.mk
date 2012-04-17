7ee5f4483952ccf1625af56a97c38f5b856d5a8c
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-04-17 05:46:56 +0000

    heirloom-mailx: new package
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/heirloom-mailx/heirloom-mailx.mk
@@ -0,0 +1,28 @@
+#############################################################
+#
+# heirloom-mailx
+#
+#############################################################
+
+HEIRLOOM_MAILX_VERSION = 12.5
+HEIRLOOM_MAILX_SOURCE  = heirloom-mailx_$(HEIRLOOM_MAILX_VERSION).orig.tar.gz
+HEIRLOOM_MAILX_SITE    = http://snapshot.debian.org/archive/debian/20110427T035506Z/pool/main/h/heirloom-mailx/
+
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+HEIRLOOM_MAILX_DEPENDENCIES += openssl
+endif
+
+define HEIRLOOM_MAILX_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
+endef
+
+define HEIRLOOM_MAILX_INSTALL_TARGET_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
+		PREFIX=/usr \
+		UCBINSTALL=$(INSTALL) \
+		STRIP=/bin/true \
+		DESTDIR=$(TARGET_DIR) \
+		install
+endef
+
+$(eval $(call GENTARGETS))
