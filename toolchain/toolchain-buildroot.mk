290bbdb4aa1cb731390fabb35ad962cf15e7e7c2
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-04-17 04:45:33 +0000

    mklibs: remove support
    The support for mklibs has been marked broken since more than a year
    and nobody cared to bring it up to a working state.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- toolchain/toolchain-buildroot.mk
+++ toolchain/toolchain-buildroot.mk
@@ -4,5 +4,4 @@ include toolchain/elf2flt/elf2flt.mk
 include toolchain/gcc/gcc-uclibc-4.x.mk
 include toolchain/gdb/gdb.mk
 include toolchain/kernel-headers/kernel-headers.mk
-include toolchain/mklibs/mklibs.mk
 include toolchain/uClibc/uclibc.mk
--- toolchain/toolchain-crosstool-ng.mk
+++ toolchain/toolchain-crosstool-ng.mk
@@ -6,5 +6,4 @@ include toolchain/elf2flt/elf2flt.mk
 include toolchain/gcc/gcc-uclibc-4.x.mk
 include toolchain/gdb/gdb.mk
 include toolchain/toolchain-crosstool-ng/crosstool-ng.mk
-include toolchain/mklibs/mklibs.mk
 include toolchain/uClibc/uclibc.mk
