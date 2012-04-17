72713672cca968c37617512af0cc50f2b4114292
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-04-17 05:46:57 +0000

    msmtp: new package
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/msmtp/msmtp.mk
@@ -0,0 +1,28 @@
+#############################################################
+#
+# msmtp
+#
+#############################################################
+
+MSMTP_VERSION = 1.4.27
+MSMTP_SITE = http://downloads.sourceforge.net/project/msmtp/msmtp/$(MSMTP_VERSION)
+MSMTP_SOURCE = msmtp-$(MSMTP_VERSION).tar.bz2
+
+MSMTP_DEPENDENCIES += host-pkg-config
+
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+MSMTP_CONF_OPT += --with-ssl=openssl
+MSMTP_DEPENDENCIES += openssl
+else ifeq ($(BR2_PACKAGE_GNUTLS),y)
+MSMTP_CONF_OPT += --with-ssl=gnutls
+MSMTP_DEPENDENCIES += gnutls
+else
+MSMTP_CONF_OPT += --with-ssl=no
+endif
+
+MSMTP_CONF_OPT += \
+	--without-libidn \
+	--without-libgsasl \
+	--without-gnome-keyring
+
+$(eval $(call AUTOTARGETS))
