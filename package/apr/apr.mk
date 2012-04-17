e5ece0d8eebf2f908b952141130664d3a6430f67
Rico Bachmann <bachmann@tofwerk.com>
2012-04-17 16:35:16 +0200

    Add apr-package
    Signed-off-by: Rico Bachmann <bachmann@tofwerk.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/apr/apr.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# apr
+#
+#############################################################
+APR_VERSION = 1.4.6
+APR_SITE = http://archive.apache.org/dist/apr
+APR_INSTALL_STAGING = YES
+APR_CONF_ENV = \
+	ac_cv_file__dev_zero=yes \
+	ac_cv_func_setpgrp_void=yes \
+	apr_cv_process_shared_works=yes \
+	apr_cv_mutex_robust_shared=no \
+	apr_cv_tcp_nodelay_with_cork=yes \
+	ac_cv_sizeof_struct_iovec=8 \
+	apr_cv_mutex_recursive=yes
+
+$(eval $(call AUTOTARGETS))
