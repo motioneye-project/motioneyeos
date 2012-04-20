58e472374f79a240c38077dd2426ecae259013a5
Peter Korsgaard <jacmet@sunsite.dk>
2012-04-20 11:20:30 +0200

    libusb: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libusb/libusb.mk
+++ package/libusb/libusb.mk
@@ -3,7 +3,7 @@
 # libusb
 #
 #############################################################
-LIBUSB_VERSION = 1.0.8
+LIBUSB_VERSION = 1.0.9
 LIBUSB_SOURCE = libusb-$(LIBUSB_VERSION).tar.bz2
 LIBUSB_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/libusb/libusb-1.0/libusb-$(LIBUSB_VERSION)
 LIBUSB_DEPENDENCIES = host-pkg-config
