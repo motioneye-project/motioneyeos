4dc432071f927df1a7477fc76c9a372c92c66c62
Yegor Yefremov <yegorslists@googlemail.com>
2012-04-20 09:17:11 +0200

    mpg123: bump to 1.13.8
    Fixes build errors for ARM arch using Linaro toolchain.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/mpg123/mpg123.mk
+++ package/multimedia/mpg123/mpg123.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-MPG123_VERSION = 1.13.4
+MPG123_VERSION = 1.13.8
 MPG123_SOURCE = mpg123-$(MPG123_VERSION).tar.bz2
 MPG123_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/mpg123
 MPG123_CONF_OPT = --with-optimization=0 --disable-lfs-alias
