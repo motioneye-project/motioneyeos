da5948fe720d5a074186f973a9effc53cae00de0
Yegor Yefremov <yegorslists@googlemail.com>
2012-04-23 21:01:46 +0000

    libusb-compat: bump to 0.1.4
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>

--- package/libusb-compat/libusb-compat.mk
+++ package/libusb-compat/libusb-compat.mk
@@ -3,7 +3,7 @@
 # libusb-compat
 #
 #############################################################
-LIBUSB_COMPAT_VERSION = 0.1.3
+LIBUSB_COMPAT_VERSION = 0.1.4
 LIBUSB_COMPAT_SOURCE = libusb-compat-$(LIBUSB_COMPAT_VERSION).tar.bz2
 LIBUSB_COMPAT_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/libusb/libusb-compat-0.1/libusb-compat-$(LIBUSB_COMPAT_VERSION)
 LIBUSB_COMPAT_DEPENDENCIES = host-pkg-config libusb
