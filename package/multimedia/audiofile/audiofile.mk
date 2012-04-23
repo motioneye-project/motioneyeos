a1c9bdd64393078f38f054a6aa6f7c7a84dbc483
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-04-23 03:31:36 +0000

    audiofile: new package
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/multimedia/audiofile/audiofile.mk
@@ -0,0 +1,19 @@
+#############################################################
+#
+# audiofile
+#
+#############################################################
+
+AUDIOFILE_VERSION = 0.3.3
+AUDIOFILE_SITE = http://audiofile.68k.org
+AUDIOFILE_INSTALL_STAGING = YES
+AUDIOFILE_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
+
+# Useless and needs alsa-lib
+define AUDIOFILE_DISABLE_EXAMPLES
+	$(SED) 's/examples//' $(@D)/Makefile.in
+endef
+
+AUDIOFILE_POST_PATCH_HOOKS += AUDIOFILE_DISABLE_EXAMPLES
+
+$(eval $(call AUTOTARGETS))
