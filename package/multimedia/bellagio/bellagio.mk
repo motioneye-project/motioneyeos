7d012ac9699cf8707a17d5b3576212ab17a2bf72
Qais Yousef <Qais.Yousef@imgtec.com>
2012-04-24 06:51:21 +0000

    new package: bellagio
    [Peter: needs C++, shouldn't force -Werror]
    Signed-off-by: Qais Yousef <qais.yousef@imgtec.com>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/multimedia/bellagio/bellagio.mk
@@ -0,0 +1,12 @@
+#############################################################
+#
+# bellagio
+#
+#############################################################
+BELLAGIO_VERSION = 0.9.3
+BELLAGIO_SOURCE = libomxil-bellagio-$(BELLAGIO_VERSION).tar.gz
+BELLAGIO_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/omxil
+BELLAGIO_AUTORECONF = YES
+BELLAGIO_INSTALL_STAGING = YES
+
+$(eval $(call AUTOTARGETS))
