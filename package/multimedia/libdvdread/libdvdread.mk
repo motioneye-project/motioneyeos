01c227553c1e77e29bbc185824bae9b614b52ad8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-05-01 03:30:19 +0000

    libdvdread: change SITE
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/libdvdread/libdvdread.mk
+++ package/multimedia/libdvdread/libdvdread.mk
@@ -6,7 +6,7 @@
 
 LIBDVDREAD_VERSION = 4.1.3
 LIBDVDREAD_SOURCE = libdvdread-$(LIBDVDREAD_VERSION).tar.bz2
-LIBDVDREAD_SITE = http://www.mplayerhq.hu/MPlayer/releases/dvdnav/
+LIBDVDREAD_SITE = http://dvdnav.mplayerhq.hu/releases
 LIBDVDREAD_AUTORECONF = YES
 LIBDVDREAD_LIBTOOL_PATCH = YES
 LIBDVDREAD_INSTALL_STAGING = YES
