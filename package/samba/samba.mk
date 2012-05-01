9d51770a0c53f1ddd46fb3c10315734c72ecd070
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-05-01 05:31:52 +0000

    samba: security bump to version 3.5.15
    Fix for CVE-2012-2111
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/samba/samba.mk
+++ package/samba/samba.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SAMBA_VERSION = 3.5.14
+SAMBA_VERSION = 3.5.15
 SAMBA_SITE = http://ftp.samba.org/pub/samba/stable
 SAMBA_SUBDIR = source3
 SAMBA_INSTALL_STAGING = YES
