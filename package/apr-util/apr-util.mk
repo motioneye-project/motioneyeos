4205dbd9f665556cc56a36c0e11a5f845c73a57a
Rico Bachmann <bachmann@tofwerk.com>
2012-05-03 14:22:36 +0200

    Add new package apr-util
    [Peter: BR2_PACKAGE_NEON not LIBNEON]
    Signed-off-by: Rico Bachmann <bachmann@tofwerk.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/apr-util/apr-util.mk
@@ -0,0 +1,13 @@
+#############################################################
+#
+# apr-util
+#
+#############################################################
+APR_UTIL_VERSION = 1.4.1
+APR_UTIL_SITE = http://archive.apache.org/dist/apr
+APR_UTIL_INSTALL_STAGING = YES
+APR_UTIL_DEPENDENCIES = apr sqlite neon zlib
+APR_UTIL_CONF_OPT = \
+	--with-apr=$(BUILD_DIR)/apr-$(APR_VERSION) \
+
+$(eval $(call AUTOTARGETS))
