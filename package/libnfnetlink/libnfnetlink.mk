a5686b8acdfc81f3ab73f013f04be0a1462d3408
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-05-04 03:16:44 +0000

    libnfnetlink: new package
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libnfnetlink/libnfnetlink.mk
@@ -0,0 +1,12 @@
+#############################################################
+#
+# libnfnetlink
+#
+#############################################################
+
+LIBNFNETLINK_VERSION = 1.0.0
+LIBNFNETLINK_SOURCE = libnfnetlink-$(LIBNFNETLINK_VERSION).tar.bz2
+LIBNFNETLINK_SITE = http://www.netfilter.org/projects/libnfnetlink/files
+LIBNFNETLINK_INSTALL_STAGING = YES
+
+$(eval $(call AUTOTARGETS))
