a90617b6271c86769a03ea8f7cfc5a58451e02c5
Arnout Vandecappelle <arnout@mind.be>
2012-05-04 11:32:56 +0000

    stress: new package
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/stress/stress.mk
@@ -0,0 +1,13 @@
+#############################################################
+#
+# stress - a workload generator
+#
+# http://weather.ou.edu/~apw/projects/stress/
+#
+#############################################################
+
+STRESS_VERSION = 1.0.4
+STRESS_SITE    = http://weather.ou.edu/~apw/projects/stress
+STRESS_AUTORECONF = YES
+
+$(eval $(call AUTOTARGETS))
