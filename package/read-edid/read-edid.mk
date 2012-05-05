adebe9df0f42a929441d40e84f30e546f2852d68
Dimitry Golubovsky <golubovsky@gmail.com>
2012-05-05 16:05:18 +0000

    package: add read-edid
    [Peter: fixup whitespace]
    Signed-off-by: Dmitry <golubovsky@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/read-edid/read-edid.mk
@@ -0,0 +1,19 @@
+############################################
+#
+# read-edid
+#
+############################################
+
+READ_EDID_VERSION = 1.4.2
+READ_EDID_SITE = http://www.polypux.org/projects/read-edid/
+
+define READ_EDID_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define READ_EDID_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/get-edid $(TARGET_DIR)/sbin/get-edid
+	$(INSTALL) -D -m 0755 $(@D)/parse-edid $(TARGET_DIR)/sbin/parse-edid
+endef
+
+$(eval $(call AUTOTARGETS))
