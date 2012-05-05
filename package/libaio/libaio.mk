73aa143b66437f6796a94dfaa1240044513e337e
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-05-05 06:26:06 +0000

    libaio: use alternative upstream URL
    Since the kernel.org security problem and reinstallation, libaio is no
    longer available from this site. Use snapshot.debian.net as an
    alternative source.
    
    [Peter: Add spaces around '=' while we're at it]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libaio/libaio.mk
+++ package/libaio/libaio.mk
@@ -3,10 +3,10 @@
 # libaio
 #
 #############################################################
-LIBAIO_VERSION=0.3.109
-LIBAIO_SOURCE=libaio-$(LIBAIO_VERSION).tar.bz2
-LIBAIO_SITE=$(BR2_KERNEL_MIRROR)/linux/libs/aio/
-LIBAIO_INSTALL_STAGING=YES
+LIBAIO_VERSION = 0.3.109
+LIBAIO_SOURCE = libaio_$(LIBAIO_VERSION).orig.tar.gz
+LIBAIO_SITE = http://snapshot.debian.org/archive/debian/20110227T085214Z/pool/main/liba/libaio/
+LIBAIO_INSTALL_STAGING = YES
 
 define LIBAIO_BUILD_CMDS
   $(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
