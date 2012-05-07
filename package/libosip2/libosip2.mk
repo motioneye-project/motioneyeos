674d4db3e68578a567e2e0ddc99be371bc9ea5ba
Luca Ceresoli <luca@lucaceresoli.net>
2012-05-07 10:43:09 +0000

    libosip2: remove redundant LIBOSIP2_SOURCE definition
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libosip2/libosip2.mk
+++ package/libosip2/libosip2.mk
@@ -5,7 +5,6 @@
 #############################################################
 
 LIBOSIP2_VERSION = 3.6.0
-LIBOSIP2_SOURCE = libosip2-$(LIBOSIP2_VERSION).tar.gz
 LIBOSIP2_SITE = $(BR2_GNU_MIRROR)/osip
 LIBOSIP2_INSTALL_STAGING = YES
 
