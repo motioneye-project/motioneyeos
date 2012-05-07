98873717c2654e27ce069c44e9b5dbe3383c0f97
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-05-07 00:33:37 +0000

    screen: enable terminfo and install screenrc
    Closes #5198
    
    Enable terminfo support as reported on bug #5198
    Also install screenrc if it's not already present in the target.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/screen/screen.mk
+++ package/screen/screen.mk
@@ -7,8 +7,17 @@
 SCREEN_VERSION = 4.0.3
 SCREEN_SITE = $(BR2_GNU_MIRROR)/screen
 SCREEN_DEPENDENCIES = ncurses
-SCREEN_CONF_ENV = ac_cv_header_elf_h=no ac_cv_header_dwarf_h=no
+SCREEN_CONF_ENV = ac_cv_header_elf_h=no ac_cv_header_dwarf_h=no \
+	CFLAGS="$(TARGET_CFLAGS) -DTERMINFO"
 SCREEN_MAKE = $(MAKE1)
 SCREEN_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) SCREEN=screen install_bin
+
+define SCREEN_INSTALL_SCREENRC
+	if [ ! -f $(TARGET_DIR)/etc/screenrc ]; then \
+		$(INSTALL) -m 0755 -D $(@D)/etc/screenrc $(TARGET_DIR)/etc/screenrc; \
+	fi
+endef
+
+SCREEN_POST_INSTALL_TARGET_HOOKS += SCREEN_INSTALL_SCREENRC
 
 $(eval $(call AUTOTARGETS))
