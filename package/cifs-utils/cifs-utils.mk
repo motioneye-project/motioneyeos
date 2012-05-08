18bd541dd02b153427fb4f010a6426c8e91bef1b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-05-08 01:51:18 +0000

    cifs-utils: security bump to version 5.4
    Fixes CVE-2012-1586.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/cifs-utils/cifs-utils.mk
+++ package/cifs-utils/cifs-utils.mk
@@ -1,5 +1,11 @@
-CIFS_UTILS_VERSION = 5.1
+CIFS_UTILS_VERSION = 5.4
 CIFS_UTILS_SOURCE  = cifs-utils-$(CIFS_UTILS_VERSION).tar.bz2
 CIFS_UTILS_SITE    = ftp://ftp.samba.org/pub/linux-cifs/cifs-utils/
+
+define CIFS_UTILS_NO_WERROR
+	$(SED) 's/-Werror//' $(@D)/Makefile.in
+endef
+
+CIFS_UTILS_POST_PATCH_HOOKS += CIFS_UTILS_NO_WERROR
 
 $(eval $(call AUTOTARGETS))
