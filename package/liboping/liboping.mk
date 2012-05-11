f88b1e963ed843b04041ce6b2bb11d2da5666b9b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-05-11 03:29:30 +0000

    liboping: new package
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/liboping/liboping.mk
@@ -0,0 +1,13 @@
+#############################################################
+#
+# liboping
+#
+#############################################################
+
+LIBOPING_VERSION = 1.6.2
+LIBOPING_SITE = http://verplant.org/liboping/files
+LIBOPING_INSTALL_STAGING = YES
+LIBOPING_DEPENDENCIES = $(if $(BR2_PACKAGE_NCURSES),ncurses)
+LIBOPING_CONF_OPT = --without-perl-bindings
+
+$(eval $(call AUTOTARGETS))
