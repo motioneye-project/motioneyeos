0b99b75fb66b453746179eee8e2a9131de87e331
Dominik Faessler <faessler@was.ch>
2012-05-14 21:36:22 +0000

    logsurfer: new package
    [Peter: fix target installation, install man pages, AUTOTARGETS args]
    Signed-off-by: Dominik Faessler <faessler@was.ch>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/logsurfer/logsurfer.mk
@@ -0,0 +1,29 @@
+#############################################################
+#
+# logsurfer
+#
+#############################################################
+
+LOGSURFER_VERSION = 1.8
+LOGSURFER_SOURCE = logsurfer-$(LOGSURFER_VERSION).tar.gz
+LOGSURFER_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/logsurfer/logsurfer/logsurfer-$(LOGSURFER_VERSION)
+
+define LOGSURFER_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/src/logsurfer \
+		$(TARGET_DIR)/usr/bin/logsurfer
+endef
+
+ifeq ($(BR2_HAVE_DOCUMENTATION),y)
+
+define LOGSURFER_INSTALL_TARGET_MAN
+	$(INSTALL) -D -m 0644 $(@D)/man/logsurfer.1 \
+		$(TARGET_DIR)/usr/man/man1/logsurfer.1
+	$(INSTALL) -D -m 0644 $(@D)/man/logsurfer.conf.4 \
+		$(TARGET_DIR)/usr/man/man4/logsurfer.conf.4
+endef
+
+LOGSURFER_POST_INSTALL_TARGET_HOOKS += LOGSURFER_INSTALL_TARGET_MAN
+
+endif
+
+$(eval $(call AUTOTARGETS))
