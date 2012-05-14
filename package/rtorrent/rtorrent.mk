54366d812d860aa7de776f14b00d52f89bb52371
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-05-14 10:55:29 +0000

    rtorrent: new package
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/rtorrent/rtorrent.mk
@@ -0,0 +1,12 @@
+#############################################################
+#
+# rtorrent
+#
+#############################################################
+
+RTORRENT_VERSION = 0.9.2
+RTORRENT_SITE = http://libtorrent.rakshasa.no/downloads
+RTORRENT_DEPENDENCIES = host-pkg-config libcurl libsigc libtorrent ncurses
+RTORRENT_AUTORECONF = YES
+
+$(eval $(call AUTOTARGETS))
