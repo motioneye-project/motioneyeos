0340b45da0afdc3ac1de9ea7169ccee5941dfc3c
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-05-14 09:46:21 +0000

    wpa_supplicant: bump to version 1.0
    Bump to version 1.0
    Also convert to gentargets.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/wpa_supplicant/wpa_supplicant.mk
+++ package/wpa_supplicant/wpa_supplicant.mk
@@ -4,18 +4,19 @@
 #
 #############################################################
 
-WPA_SUPPLICANT_VERSION = 0.7.3
+WPA_SUPPLICANT_VERSION = 1.0
 WPA_SUPPLICANT_SITE = http://hostap.epitest.fi/releases
 WPA_SUPPLICANT_CONFIG = $(WPA_SUPPLICANT_DIR)/wpa_supplicant/.config
 WPA_SUPPLICANT_SUBDIR = wpa_supplicant
-WPA_SUPPLICANT_TARGET_BINS = wpa_cli wpa_supplicant wpa_passphrase
 WPA_SUPPLICANT_DBUS_SERVICE = fi.epitest.hostap.WPASupplicant
+WPA_SUPPLICANT_CFLAGS = $(TARGET_CFLAGS) -I$(STAGING_DIR)/usr/include/libnl3/
+WPA_SUPPLICANT_LDFLAGS = $(TARGET_LDFLAGS)
 
 ifeq ($(BR2_PACKAGE_LIBNL),y)
 	WPA_SUPPLICANT_DEPENDENCIES += libnl
 define WPA_SUPPLICANT_LIBNL_CONFIG
 	$(SED) "s/^#CONFIG_DRIVER_NL80211/CONFIG_DRIVER_NL80211/" $(WPA_SUPPLICANT_CONFIG)
-	echo "CONFIG_LIBNL20=y" >>$(WPA_SUPPLICANT_CONFIG)
+	echo "CONFIG_LIBNL32=y" >>$(WPA_SUPPLICANT_CONFIG)
 endef
 endif
 
@@ -47,17 +48,23 @@ define WPA_SUPPLICANT_CRYPTO_CONFIG
 	echo "CONFIG_INTERNAL_LIBTOMMATH_FAST=y" >>$(WPA_SUPPLICANT_CONFIG)
 endef
 
-# Try to use openssl for TLS if it's already available
-# gnutls is also supported for TLS
+# Try to use openssl or gnutls if it's already available
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 	WPA_SUPPLICANT_DEPENDENCIES += openssl
 define WPA_SUPPLICANT_TLS_CONFIG
 	echo "CONFIG_TLS=openssl" >>$(WPA_SUPPLICANT_CONFIG)
 endef
 else
+ifeq ($(BR2_PACKAGE_GNUTLS),y)
+	WPA_SUPPLICANT_DEPENDENCIES += gnutls
+define WPA_SUPPLICANT_TLS_CONFIG
+	echo "CONFIG_TLS=gnutls" >>$(WPA_SUPPLICANT_CONFIG)
+endef
+else
 define WPA_SUPPLICANT_TLS_CONFIG
 	echo "CONFIG_TLS=internal" >>$(WPA_SUPPLICANT_CONFIG)
 endef
+endif
 endif
 
 ifeq ($(BR2_PACKAGE_DBUS),y)
@@ -72,9 +79,6 @@ endif
 
 define WPA_SUPPLICANT_CONFIGURE_CMDS
 	cp $(@D)/wpa_supplicant/defconfig $(WPA_SUPPLICANT_CONFIG)
-	echo "CFLAGS += $(TARGET_CFLAGS) -I$(STAGING_DIR)/usr/include/libnl3/" >>$(WPA_SUPPLICANT_CONFIG)
-	echo "LDFLAGS += $(TARGET_LDFLAGS)" >>$(WPA_SUPPLICANT_CONFIG)
-	echo "CC = $(TARGET_CC)" >>$(WPA_SUPPLICANT_CONFIG)
 	$(SED) "s/^#CONFIG_IEEE80211R/CONFIG_IEEE80211R/" $(WPA_SUPPLICANT_CONFIG)
 	$(SED) "s/^#CONFIG_DELAYED_MIC/CONFIG_DELAYED_MIC/" $(WPA_SUPPLICANT_CONFIG)
 	$(SED) "s/^CONFIG_DRIVER_ATMEL/#CONFIG_DRIVER_ATMEL/" $(WPA_SUPPLICANT_CONFIG)
@@ -89,22 +93,27 @@ define WPA_SUPPLICANT_CONFIGURE_CMDS
 	$(WPA_SUPPLICANT_AP_CONFIG)
 endef
 
-define WPA_SUPPLICANT_REMOVE_CLI
-	rm -f $(TARGET_DIR)/usr/sbin/wpa_cli
+define WPA_SUPPLICANT_BUILD_CMDS
+	$(TARGET_MAKE_ENV) CFLAGS="$(WPA_SUPPLICANT_CFLAGS)" \
+		LDFLAGS="$(TARGET_LDFLAGS)" \
+		$(MAKE) CC="$(TARGET_CC)" -C $(@D)/$(WPA_SUPPLICANT_SUBDIR)
 endef
 
-ifneq ($(BR2_PACKAGE_WPA_SUPPLICANT_CLI),y)
-WPA_SUPPLICANT_POST_INSTALL_TARGET_HOOKS += WPA_SUPPLICANT_REMOVE_CLI
-endif
-
-define WPA_SUPPLICANT_REMOVE_PASSPHRASE
-	rm -f $(TARGET_DIR)/usr/sbin/wpa_passphrase
+ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT_CLI),y)
+define WPA_SUPPLICANT_INSTALL_CLI
+	$(INSTALL) -m 0755 -D $(@D)/$(WPA_SUPPLICANT_SUBDIR)/wpa_cli \
+		$(TARGET_DIR)/usr/sbin/wpa_cli
 endef
-
-ifneq ($(BR2_PACKAGE_WPA_SUPPLICANT_PASSPHRASE),y)
-WPA_SUPPLICANT_POST_INSTALL_TARGET_HOOKS += WPA_SUPPLICANT_REMOVE_PASSPHRASE
 endif
 
+ifeq ($(BR2_PACKAGE_WPA_SUPPLICANT_PASSPHRASE),y)
+define WPA_SUPPLICANT_INSTALL_PASSPHRASE
+	$(INSTALL) -m 0755 -D $(@D)/$(WPA_SUPPLICANT_SUBDIR)/wpa_passphrase \
+		$(TARGET_DIR)/usr/sbin/wpa_passphrase
+endef
+endif
+
+ifeq ($(BR2_PACKAGE_DBUS),y)
 define WPA_SUPPLICANT_INSTALL_DBUS
 	$(INSTALL) -D \
 	  $(@D)/wpa_supplicant/dbus/dbus-wpa_supplicant.conf \
@@ -113,23 +122,16 @@ define WPA_SUPPLICANT_INSTALL_DBUS
 	  $(@D)/wpa_supplicant/dbus/$(WPA_SUPPLICANT_DBUS_SERVICE).service \
 	  $(TARGET_DIR)/usr/share/dbus-1/system-services/$(WPA_SUPPLICANT_DBUS_SERVICE).service
 endef
-
-ifeq ($(BR2_PACKAGE_DBUS),y)
-WPA_SUPPLICANT_POST_INSTALL_TARGET_HOOKS += WPA_SUPPLICANT_INSTALL_DBUS
 endif
 
-define WPA_SUPPLICANT_INSTALL_CONFIG
-	$(INSTALL) -m 644 \
-	  package/wpa_supplicant/wpa_supplicant.conf  $(TARGET_DIR)/etc/wpa_supplicant.conf
+define WPA_SUPPLICANT_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/$(WPA_SUPPLICANT_SUBDIR)/wpa_supplicant \
+		$(TARGET_DIR)/usr/sbin/wpa_supplicant
+	$(INSTALL) -m 644 -D package/wpa_supplicant/wpa_supplicant.conf \
+		$(TARGET_DIR)/etc/wpa_supplicant.conf
+	$(WPA_SUPPLICANT_INSTALL_CLI)
+	$(WPA_SUPPLICANT_INSTALL_PASSPHRASE)
+	$(WPA_SUPPLICANT_INSTALL_DBUS)
 endef
 
-WPA_SUPPLICANT_POST_INSTALL_TARGET_HOOKS += WPA_SUPPLICANT_INSTALL_CONFIG
-
-define WPA_SUPPLICANT_UNINSTALL_TARGET_CMDS
-	rm -f $(addprefix $(TARGET_DIR)/usr/sbin/, $(WPA_SUPPLICANT_TARGET_BINS))
-	rm -f $(TARGET_DIR)/etc/dbus-1/system.d/wpa_supplicant.conf
-	rm -f $(TARGET_DIR)/usr/share/dbus-1/system-services/$(WPA_SUPPLICANT_DBUS_SERVICE).service
-	rm -f $(TARGET_DIR)/usr/etc/wpa_supplicant.conf
-endef
-
-$(eval $(call AUTOTARGETS))
+$(eval $(call GENTARGETS))
