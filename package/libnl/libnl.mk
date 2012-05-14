4b55a9952014b37ce565b46ee6e93018555127fa
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-05-14 02:29:58 +0000

    libnl: bump to version 3.2.9 and enhance
    Bump to version 3.2.9 and add an option to install the tools.
    Since they're not usually required we default to N to reduce size.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libnl/libnl.mk
+++ package/libnl/libnl.mk
@@ -4,15 +4,26 @@
 #
 #############################################################
 
-LIBNL_VERSION = 3.2.8
+LIBNL_VERSION = 3.2.9
 LIBNL_SITE = http://www.infradead.org/~tgr/libnl/files
 LIBNL_INSTALL_STAGING = YES
 LIBNL_DEPENDENCIES = host-bison
 LIBNL_MAKE = $(MAKE1)
+LIBNL_BINARIES = class-add class-delete class-list classid-lookup cls-add \
+	cls-delete cls-list link-list pktloc-lookup qdisc-add qdisc-delete \
+	qdisc-list
 
 define LIBNL_UNINSTALL_TARGET_CMDS
 	rm -r $(TARGET_DIR)/usr/lib/libnl.* $(TARGET_DIR)/usr/lib/libnl-*.*
 	rm -rf $(TARGET_DIR)/usr/lib/libnl
 endef
+
+define LIBNL_REMOVE_TOOLS
+	rm -rf $(addprefix $(TARGET_DIR)/usr/sbin/nl-, $(LIBNL_BINARIES))
+endef
+
+ifneq ($(BR2_PACKAGE_LIBNL_TOOLS),y)
+LIBNL_POST_INSTALL_TARGET_HOOKS += LIBNL_REMOVE_TOOLS
+endif
 
 $(eval $(call AUTOTARGETS))
