beeacca5f804841ffd1ed03532a9e5b0ca5937ca
Dimitry Golubovsky <golubovsky@gmail.com>
2012-05-15 16:36:06 +0000

    package: add fbterm, a framebuffer-based terminal emulator
    [Peter: fix dependencies / white space]
    Signed-off-by: Dmitry <golubovsky@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/fbterm/fbterm.mk
@@ -0,0 +1,12 @@
+############################################
+#
+# fbterm
+#
+############################################
+
+FBTERM_VERSION = 1.7.0
+FBTERM_SITE = http://fbterm.googlecode.com/files/
+
+FBTERM_DEPENDENCIES = fontconfig liberation
+
+$(eval $(call AUTOTARGETS))
