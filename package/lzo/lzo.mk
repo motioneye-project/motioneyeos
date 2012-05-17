1c5597f17731d0816129080c5b51e0a6f117f73c
Luca Ceresoli <luca@lucaceresoli.net>
2012-05-17 19:33:16 +0200

    lzo: define license
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lzo/lzo.mk
+++ package/lzo/lzo.mk
@@ -5,6 +5,8 @@
 #############################################################
 LZO_VERSION = 2.06
 LZO_SITE = http://www.oberhumer.com/opensource/lzo/download
+LZO_LICENSE = GPLv2+
+LZO_LICENSE_FILES = COPYING
 LZO_INSTALL_STAGING = YES
 
 $(eval $(call AUTOTARGETS))
