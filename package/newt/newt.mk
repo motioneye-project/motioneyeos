75672c122c20a2c041bb50650227b43fefa4e2ce
Luca Ceresoli <luca@lucaceresoli.net>
2012-05-17 19:33:04 +0200

    newt: warn that legal-info is not implemented
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/newt/newt.mk
+++ package/newt/newt.mk
@@ -58,6 +58,9 @@ $(TARGET_DIR)/usr/lib/libnewt.so.$(NEWT_VERSION): $(STAGING_DIR)/usr/lib/libnewt
 	-$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libnewt.so*
 	touch -c $@
 
+newt-legal-info:
+	@$(call legal-warning-pkg,newt,legal-info not yet implemented)
+
 newt: slang $(TARGET_DIR)/usr/lib/libnewt.so.$(NEWT_VERSION)
 
 newt-source: $(DL_DIR)/$(NEWT_SOURCE)
