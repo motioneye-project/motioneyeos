46f81aa05b226c0ab77bb31346ba0f83dfb3c006
Luca Ceresoli <luca@lucaceresoli.net>
2012-05-17 19:33:17 +0200

    lzop: define license
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lzop/lzop.mk
+++ package/lzop/lzop.mk
@@ -6,6 +6,8 @@
 LZOP_VERSION = 1.03
 LZOP_SOURCE = lzop-$(LZOP_VERSION).tar.gz
 LZOP_SITE = http://www.lzop.org/download/
+LZOP_LICENSE = GPLv2+
+LZOP_LICENSE_FILES = COPYING
 LZOP_DEPENDENCIES = lzo
 
 $(eval $(call AUTOTARGETS))
