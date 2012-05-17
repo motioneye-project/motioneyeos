41633ecc24aee2e1938be80945015d4f9ae98803
Luca Ceresoli <luca@lucaceresoli.net>
2012-05-17 19:33:19 +0200

    pcre: define license
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/pcre/pcre.mk
+++ package/pcre/pcre.mk
@@ -6,6 +6,8 @@
 
 PCRE_VERSION = 8.30
 PCRE_SITE = ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre
+PCRE_LICENSE = BSD-3c
+PCRE_LICENSE_FILES = LICENCE
 PCRE_INSTALL_STAGING = YES
 
 ifneq ($(BR2_INSTALL_LIBSTDCPP),y)
