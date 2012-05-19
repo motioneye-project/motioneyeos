8bde151e5c147bf84ed98f54512d50c81e1f1587
Peter Korsgaard <jacmet@sunsite.dk>
2012-05-20 00:28:07 +0200

    package: add webrtc-audio-processing library
    Optional dependency to pulseaudio >= 2.0
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/webrtc-audio-processing/webrtc-audio-processing.mk
@@ -0,0 +1,10 @@
+WEBRTC_AUDIO_PROCESSING_VERSION = 0.1
+WEBRTC_AUDIO_PROCESSING_SOURCE = webrtc-audio-processing-$(WEBRTC_AUDIO_PROCESSING_VERSION).tar.xz
+WEBRTC_AUDIO_PROCESSING_SITE = http://freedesktop.org/software/pulseaudio/webrtc-audio-processing/
+WEBRTC_AUDIO_PROCESSING_INSTALL_STAGING = YES
+
+ifeq ($(BR2_SOFT_FLOAT),y)
+WEBRTC_AUDIO_PROCESSING_CONF_OPT += --with-ns-mode=fixed
+endif
+
+$(eval $(call AUTOTARGETS))
