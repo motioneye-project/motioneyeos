e6e6101bd552f96e32cf935cf6492864795b5364
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-05-28 04:50:03 +0000

    libmnl: bump to version 1.0.3
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libmnl/libmnl.mk
+++ package/libmnl/libmnl.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBMNL_VERSION = 1.0.2
+LIBMNL_VERSION = 1.0.3
 LIBMNL_SOURCE = libmnl-$(LIBMNL_VERSION).tar.bz2
 LIBMNL_SITE = http://netfilter.org/projects/libmnl/files
 LIBMNL_INSTALL_STAGING = YES
