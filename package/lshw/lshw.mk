c75e768d7945d0b760e176ee9d2cc93796078870
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-05-28 15:11:23 +0000

    lshw: new package
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/lshw/lshw.mk
@@ -0,0 +1,23 @@
+#############################################################
+#
+# lshw
+#
+#############################################################
+
+LSHW_VERSION = B.02.16
+LSHW_SITE = http://ezix.org/software/files
+LSHW_MAKE_OPT = CC=$(TARGET_CC) CXX=$(TARGET_CXX) AR=$(TARGET_AR) \
+	RPM_OPT_FLAGS="$(TARGET_CFLAGS)" all
+LSHW_MAKE_ENV = LIBS="$(if $(BR2_NEEDS_GETTEXT),-lintl)"
+LSHW_DEPENDENCIES = $(if $(BR2_NEEDS_GETTEXT),libintl)
+
+define LSHW_BUILD_CMDS
+	$(LSHW_MAKE_ENV) $(MAKE) -C $(@D)/src $(LSHW_MAKE_OPT)
+endef
+
+define LSHW_INSTALL_TARGET_CMDS
+	$(LSHW_MAKE_ENV) $(MAKE) -C $(@D)/src DESTDIR=$(TARGET_DIR) \
+		$(LSHW_MAKE_OPT) install
+endef
+
+$(eval $(call GENTARGETS))
