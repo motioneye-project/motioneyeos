2105ecbd86d04e446effa11108f6bd755ed5f19a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-05-29 15:13:21 -0300

    nfs-utils: bump to version 1.2.6
    Bump nfs-utils to version 1.2.6
    Portmap or rpcbind (which we don't have) is required so select it.
    Remove nfsiostat if there's no target python.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/nfs-utils/nfs-utils.mk
+++ package/nfs-utils/nfs-utils.mk
@@ -3,14 +3,17 @@
 # nfs-utils
 #
 #############################################################
-NFS_UTILS_VERSION = 1.2.3
+
+NFS_UTILS_VERSION = 1.2.6
 NFS_UTILS_SOURCE = nfs-utils-$(NFS_UTILS_VERSION).tar.bz2
-NFS_UTILS_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/nfs/
+NFS_UTILS_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/nfs/nfs-utils/$(NFS_UTILS_VERSION)
+NFS_UTILS_AUTORECONF = YES
 
 NFS_UTILS_CONF_ENV = knfsd_cv_bsd_signals=no
 
 NFS_UTILS_CONF_OPT = \
 		--disable-nfsv4 \
+		--disable-nfsv41 \
 		--disable-gss \
 		--disable-tirpc \
 		--disable-uuid \
@@ -28,6 +31,13 @@ define NFS_UTILS_INSTALL_FIXUP
 	rm -f $(NFS_UTILS_TARGETS_)
 endef
 
+define NFS_UTILS_REMOVE_NFSIOSTAT
+	rm -f $(TARGET_DIR)/usr/sbin/nfsiostat
+endef
+
 NFS_UTILS_POST_INSTALL_TARGET_HOOKS += NFS_UTILS_INSTALL_FIXUP
+
+# nfsiostat is interpreted python, so remove it unless it's in the target
+NFS_UTILS_POST_INSTALL_TARGET_HOOKS += $(if $(BR2_PACKAGE_PYTHON),,NFS_UTILS_REMOVE_NFSIOSTAT)
 
 $(eval $(call AUTOTARGETS))
