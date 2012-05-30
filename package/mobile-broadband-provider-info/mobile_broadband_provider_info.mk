103b816fe19e2c007bdcf45b59a7a10cfadcc19f
Peter Korsgaard <jacmet@sunsite.dk>
2012-05-30 11:04:36 +0200

    mobile-broadband-provider-info: use release tarball instead of git
    Reported-by: Arnout Vandecappelle <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/mobile-broadband-provider-info/mobile_broadband_provider_info.mk
+++ package/mobile-broadband-provider-info/mobile_broadband_provider_info.mk
@@ -4,10 +4,8 @@
 #
 #############################################################
 MOBILE_BROADBAND_PROVIDER_INFO_VERSION = 20110511
-MOBILE_BROADBAND_PROVIDER_INFO_SITE = git://git.gnome.org/mobile-broadband-provider-info
+MOBILE_BROADBAND_PROVIDER_INFO_SITE = http://ftp.gnome.org/pub/GNOME/sources/mobile-broadband-provider-info/$(MOBILE_BROADBAND_PROVIDER_INFO_VERSION)
 MOBILE_BROADBAND_PROVIDER_INFO_INSTALL_STAGING = YES
 MOBILE_BROADBAND_PROVIDER_INFO_DEPENDENCIES = host-pkg-config
-
-MOBILE_BROADBAND_PROVIDER_INFO_AUTORECONF = YES
 
 $(eval $(call AUTOTARGETS))
