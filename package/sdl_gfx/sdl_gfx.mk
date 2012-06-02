64d4866257db72f06284a0fcef9a816d4291462a
Samuel Martin <s.martin49@gmail.com>
2012-06-02 12:08:46 +0000

    sdl_gfx: fix assignments
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sdl_gfx/sdl_gfx.mk
+++ package/sdl_gfx/sdl_gfx.mk
@@ -3,11 +3,11 @@
 # SDL_gfx addon for SDL
 #
 #############################################################
-SDL_GFX_VERSION:=2.0.19
-SDL_GFX_SOURCE:=SDL_gfx-$(SDL_GFX_VERSION).tar.gz
-SDL_GFX_SITE:=http://www.ferzkopp.net/Software/SDL_gfx-2.0/
-SDL_GFX_INSTALL_STAGING:=YES
-SDL_GFX_DEPENDENCIES:=sdl
+SDL_GFX_VERSION = 2.0.19
+SDL_GFX_SOURCE = SDL_gfx-$(SDL_GFX_VERSION).tar.gz
+SDL_GFX_SITE = http://www.ferzkopp.net/Software/SDL_gfx-2.0/
+SDL_GFX_INSTALL_STAGING = YES
+SDL_GFX_DEPENDENCIES = sdl
 SDL_GFX_CONF_OPT = \
 	--with-sdl-prefix=$(STAGING_DIR)/usr \
 	--disable-sdltest \
