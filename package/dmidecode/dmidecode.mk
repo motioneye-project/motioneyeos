9d3547529b87ce3bddfcdc962e0c9d6d2b2ed9d5
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-06-02 06:13:57 +0000

    dmidecode: new package
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/dmidecode/dmidecode.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# dmidecode
+#
+#############################################################
+
+DMIDECODE_VERSION = 2.11
+DMIDECODE_SITE    = http://download.savannah.gnu.org/releases/dmidecode/
+
+define DMIDECODE_BUILD_CMDS
+	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS)
+endef
+
+define DMIDECODE_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) prefix=/usr DESTDIR=$(TARGET_DIR) install
+endef
+
+$(eval $(call GENTARGETS))
