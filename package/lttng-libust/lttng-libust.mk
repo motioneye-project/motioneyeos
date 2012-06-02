a45c3f2d9abecbed4b7c0d14408dfa486f12888a
Samuel Martin <s.martin49@gmail.com>
2012-06-03 00:09:16 +0200

    lttng-libust: bump to version 2.0.3
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/lttng-libust/lttng-libust.mk
+++ package/lttng-libust/lttng-libust.mk
@@ -1,6 +1,11 @@
-LTTNG_LIBUST_SITE    = http://lttng.org/files/bundles/20111214/
-LTTNG_LIBUST_VERSION = 1.9.2
-LTTNG_LIBUST_SOURCE  = lttng-ust-$(LTTNG_LIBUST_VERSION).tar.gz
+#############################################################
+#
+# LTTng-UST: the userspace tracing library
+#
+#############################################################
+LTTNG_LIBUST_SITE    = http://lttng.org/files/lttng-ust/
+LTTNG_LIBUST_VERSION = 2.0.3
+LTTNG_LIBUST_SOURCE  = lttng-ust-$(LTTNG_LIBUST_VERSION).tar.bz2
 
 LTTNG_LIBUST_INSTALL_STAGING = YES
 LTTNG_LIBUST_DEPENDENCIES    = liburcu util-linux
