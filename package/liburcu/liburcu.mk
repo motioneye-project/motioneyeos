eb26e26269726434ced2850163ad8ba1e0253571
Samuel Martin <s.martin49@gmail.com>
2012-06-02 12:09:15 +0000

    liburcu: bump to version 0.7.3
    Note that liburcu-0.6.7-missing-gnu-source.patch has been merged upstream.
    
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>
    
     delete mode 100644 package/liburcu/liburcu-0.6.7-missing-gnu-source.patch
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/liburcu/liburcu.mk
+++ package/liburcu/liburcu.mk
@@ -1,5 +1,10 @@
-LIBURCU_VERSION = 0.6.7
-LIBURCU_SITE    = http://lttng.org/files/bundles/20111214/
+#############################################################
+#
+# liburc: userspace RCU (read-copy-update) library
+#
+#############################################################
+LIBURCU_VERSION = 0.7.3
+LIBURCU_SITE    = http://lttng.org/files/urcu/
 LIBURCU_SOURCE  = userspace-rcu-$(LIBURCU_VERSION).tar.bz2
 
 LIBURCU_INSTALL_STAGING = YES
