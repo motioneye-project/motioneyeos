39348d803bda0c0f33805b06d302669293e4d38a
Murat Demirten <mdemirten@yh.com.tr>
2012-06-03 06:32:24 +0000

    jpeg-turbo: new package
    [Peter: Various cleanups/fixes/simplifications]
    Signed-off-by: Murat Demirten <mdemirten@yh.com.tr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/jpeg-turbo/jpeg-turbo.mk
@@ -0,0 +1,27 @@
+#############################################################
+#
+# jpeg-turbo
+#
+#############################################################
+
+JPEG_TURBO_VERSION = 1.2.1
+JPEG_TURBO_SOURCE = libjpeg-turbo-$(JPEG_TURBO_VERSION).tar.gz
+JPEG_TURBO_SITE = http://downloads.sourceforge.net/project/libjpeg-turbo/$(JPEG_TURBO_VERSION)
+JPEG_TURBO_INSTALL_STAGING = YES
+JPEG_TURBO_CONF_OPT = --with-jpeg8
+
+ifeq ($(BR2_X86_CPU_HAS_MMX)$(BR2_ARM_CPU_HAS_NEON),y)
+JPEG_TURBO_CONF_OPT += --with-simd
+# x86 simd support needs nasm
+JPEG_TURBO_DEPENDENCIES += $(if $(BR2_X86_CPU_HAS_MMX),host-nasm)
+else
+JPEG_TURBO_CONF_OPT += --without-simd
+endif
+
+define JPEG_TURBO_REMOVE_USELESS_TOOLS
+	rm -f $(addprefix $(TARGET_DIR)/usr/bin/,cjpeg djpeg jpegtrans rdjpgcom tjbench wrjpgcom)
+endef
+
+JPEG_TURBO_POST_INSTALL_TARGET_HOOKS += JPEG_TURBO_REMOVE_USELESS_TOOLS
+
+$(eval $(autotools-package))
