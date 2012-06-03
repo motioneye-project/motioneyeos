1a3045f209d651ebbe1e5e87ccb7686e8ea595a5
Peter Korsgaard <jacmet@sunsite.dk>
2012-06-04 00:04:16 +0200

    linphone: add optional gtk support
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/linphone/linphone.mk
+++ package/linphone/linphone.mk
@@ -7,10 +7,16 @@
 LINPHONE_VERSION = 3.5.2
 LINPHONE_SITE = http://download-mirror.savannah.gnu.org/releases/linphone/3.5.x/sources/
 LINPHONE_CONF_OPT = \
-	--disable-gtk_ui \
 	--enable-external-ortp \
 	--enable-external-mediastreamer
 
 LINPHONE_DEPENDENCIES = host-pkg-config ortp mediastreamer libeXosip2 speex
+
+ifeq ($(BR2_PACKAGE_LIBGTK2)$(BR2_PACKAGE_XORG7),yy)
+LINPHONE_CONF_OPT += --enable-gtk_ui
+LINPHONE_DEPENDENCIES += libgtk2
+else
+LINPHONE_CONF_OPT += --disable-gtk_ui
+endif
 
 $(eval $(call AUTOTARGETS))
