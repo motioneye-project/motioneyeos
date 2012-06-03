fdf093b7cef81cc9cbf8bdab3a7e2d88a02d611d
Peter Korsgaard <jacmet@sunsite.dk>
2012-06-03 21:21:28 +0200

    package: add mediastreamer
    It doesn't really need C++ support on Linux, but the configure script
    checks for it (and fails if not available), and the package is not easy
    to autoreconf as there's C++ source files for Android/Windows, causing
    automake to fail if AC_PROG_CXX isn't set.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/mediastreamer/mediastreamer.mk
@@ -0,0 +1,77 @@
+MEDIASTREAMER_VERSION = 2.8.2
+MEDIASTREAMER_SITE = http://download.savannah.nongnu.org/releases/linphone/mediastreamer
+MEDIASTREAMER_INSTALL_STAGING = YES
+MEDIASTREAMER_DEPENDENCIES = host-pkg-config ortp
+
+ifeq ($(BR2_PACKAGE_ALSA_LIB),y)
+MEDIASTREAMER_CONF_OPT += --enable-alsa
+MEDIASTREAMER_DEPENDENCIES += alsa-lib
+else
+MEDIASTREAMER_CONF_OPT += --disable-alsa
+endif
+
+ifeq ($(BR2_PACKAGE_PORTAUDIO),y)
+MEDIASTREAMER_CONF_OPT += --enable-portaudio
+MEDIASTREAMER_DEPENDENCIES += portaudio
+else
+MEDIASTREAMER_CONF_OPT += --disable-portaudio
+endif
+
+ifeq ($(BR2_PACKAGE_PULSEAUDIO),y)
+MEDIASTREAMER_CONF_OPT += --enable-pulseaudio
+MEDIASTREAMER_DEPENDENCIES += pulseaudio
+else
+MEDIASTREAMER_CONF_OPT += --disable-pulseaudio
+endif
+
+ifeq ($(BR2_PACKAGE_SPEEX),y)
+MEDIASTREAMER_CONF_OPT += --enable-speex
+MEDIASTREAMER_DEPENDENCIES += speex
+else
+MEDIASTREAMER_CONF_OPT += --disable-speex
+endif
+
+ifeq ($(BR2_PACKAGE_FFMPEG_SWSCALE),y)
+MEDIASTREAMER_CONF_OPT += --enable-ffmpeg
+MEDIASTREAMER_DEPENDENCIES += ffmpeg
+else
+MEDIASTREAMER_CONF_OPT += --disable-ffmpeg
+endif
+
+ifeq ($(BR2_PACKAGE_SDL),y)
+MEDIASTREAMER_CONF_OPT += --enable-sdl
+MEDIASTREAMER_DEPENDENCIES += sdl
+else
+MEDIASTREAMER_CONF_OPT += --disable-sdl
+endif
+
+ifeq ($(BR2_PACKAGE_XLIB_LIBX11),y)
+MEDIASTREAMER_CONF_OPT += --enable-x11
+MEDIASTREAMER_DEPENDENCIES += xlib_libX11
+
+ifeq ($(BR2_PACKAGE_XLIB_LIBXV),y)
+MEDIASTREAMER_CONF_OPT += --enable-xv
+MEDIASTREAMER_DEPENDENCIES += xlib_libXv
+else
+MEDIASTREAMER_CONF_OPT += --disable-xv
+endif
+
+else
+MEDIASTREAMER_CONF_OPT += --disable-x11
+endif
+
+ifeq ($(BR2_PACKAGE_LIBTHEORA),y)
+MEDIASTREAMER_CONF_OPT += --enable-theora
+MEDIASTREAMER_DEPENDENCIES += libtheora
+else
+MEDIASTREAMER_CONF_OPT += --disable-theora
+endif
+
+ifeq ($(BR2_PACKAGE_LIBV4L),y)
+MEDIASTREAMER_CONF_OPT += --enable-libv4l1 --enable-libv4l2
+MEDIASTREAMER_DEPENDENCIES += libv4l
+else
+MEDIASTREAMER_CONF_OPT += --disable-libv4l1 --disable-libv4l2
+endif
+
+$(eval $(call AUTOTARGETS))
