922fdbcdda7a71447307f648e3c51f42232ddbbe
Murat Demirten <mdemirten@yh.com.tr>
2012-06-03 06:17:10 +0000

    portaudio: bump to version v19_20111121
    Signed-off-by: Murat Demirten <mdemirten@yh.com.tr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/portaudio/portaudio.mk
+++ package/portaudio/portaudio.mk
@@ -4,18 +4,18 @@
 #
 ################################################################################
 
-PORTAUDIO_VERSION = V19
+PORTAUDIO_VERSION = v19_20111121
 PORTAUDIO_SITE = http://www.portaudio.com/archives
-PORTAUDIO_SOURCE = pa_stable_v19_20110326.tgz
+PORTAUDIO_SOURCE = pa_stable_$(PORTAUDIO_VERSION).tgz
 PORTAUDIO_INSTALL_STAGING = YES
 PORTAUDIO_MAKE = $(MAKE1)
 
 PORTAUDIO_DEPENDENCIES = \
-       $(if $(BR2_PACKAGE_PORTAUDIO_WITH_ALSA),alsa-lib)
+	$(if $(BR2_PACKAGE_PORTAUDIO_WITH_ALSA),alsa-lib)
 
 PORTAUDIO_CONF_OPT = \
-       $(if $(BR2_PACKAGE_PORTAUDIO_ALSA),--with-alsa,--without-alsa) \
-       $(if $(BR2_PACKAGE_PORTAUDIO_OSS),--with-oss,--without-oss) \
-       $(if $(BR2_PACKAGE_PORTAUDIO_CXX),--enable-cxx,--disable-cxx)
+	$(if $(BR2_PACKAGE_PORTAUDIO_ALSA),--with-alsa,--without-alsa) \
+	$(if $(BR2_PACKAGE_PORTAUDIO_OSS),--with-oss,--without-oss) \
+	$(if $(BR2_PACKAGE_PORTAUDIO_CXX),--enable-cxx,--disable-cxx)
 
 $(eval $(call AUTOTARGETS))
