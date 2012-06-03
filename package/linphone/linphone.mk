96f41055068e54e734edc99c88ec46aa40d8ced5
Peter Korsgaard <jacmet@sunsite.dk>
2012-06-03 21:25:00 +0200

    linphone: use external ortp/mediastreamer libraries
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/linphone/linphone.mk
+++ package/linphone/linphone.mk
@@ -6,7 +6,11 @@
 
 LINPHONE_VERSION = 3.5.2
 LINPHONE_SITE = http://download-mirror.savannah.gnu.org/releases/linphone/3.5.x/sources/
-LINPHONE_CONF_OPT = --disable-video --disable-gtk_ui
-LINPHONE_DEPENDENCIES = libeXosip2 speex
+LINPHONE_CONF_OPT = \
+	--disable-gtk_ui \
+	--enable-external-ortp \
+	--enable-external-mediastreamer
+
+LINPHONE_DEPENDENCIES = host-pkg-config ortp mediastreamer libeXosip2 speex
 
 $(eval $(call AUTOTARGETS))
