5c959c117d9079432e6e03c249a6534cca4eb6eb
Simon Dawson <spdawson@gmail.com>
2012-06-04 20:32:15 +0000

    nanocom: new package
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/nanocom/nanocom.mk
@@ -0,0 +1,27 @@
+#############################################################
+#
+# nanocom
+#
+#############################################################
+NANOCOM_VERSION = 2.6.1
+NANOCOM_SOURCE = nanocom.tar.gz
+NANOCOM_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/nanocom/nanocom/v1.0
+
+# N.B. Don't strip any path components during extraction.
+define NANOCOM_EXTRACT_CMDS
+	gzip -d -c $(DL_DIR)/$(NANOCOM_SOURCE) | tar --strip-components=0 -C $(NANOCOM_DIR) -xf -
+endef
+
+define NANOCOM_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define NANOCOM_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/nanocom $(TARGET_DIR)/usr/bin/nanocom
+endef
+
+define NANOCOM_UNINSTALL_TARGET_CMDS
+	$(RM) $(TARGET_DIR)/usr/bin/nanocom
+endef
+
+$(eval $(call GENTARGETS))
