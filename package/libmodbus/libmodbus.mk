aae26bb872384559df1b211ef0a96ea50c7a1fe8
Yegor Yefremov <yegorslists@googlemail.com>
2012-06-04 21:02:52 +0000

    libmodbus: bump to 3.0.3
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libmodbus/libmodbus.mk
+++ package/libmodbus/libmodbus.mk
@@ -3,9 +3,8 @@
 # libmodbus
 #
 #############################################################
-LIBMODBUS_VERSION = 3.0.2
+LIBMODBUS_VERSION = 3.0.3
 LIBMODBUS_SITE = http://github.com/downloads/stephane/libmodbus
-LIBMODBUS_SOURCE = libmodbus-$(LIBMODBUS_VERSION).tar.gz
 LIBMODBUS_INSTALL_STAGING = YES
 
 $(eval $(call AUTOTARGETS))
