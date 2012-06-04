d6d52903bba357f3bc18d1736e97e4da2ad91c00
Peter Korsgaard <jacmet@sunsite.dk>
2012-06-04 14:00:13 +0200

    gnutls: fix libgcrypt handling with external toolchains
    The explicit gnutls prefix in combination with the libc.so linker script
    and non-default sysroot seems to confuse external toolchains, causing
    configure to error out. Workaround it by not using an explicit libgcrypt
    prefix as it is installed in the sysroot anyway.
    
    Fixes http://autobuild.buildroot.net/results/3b3901bea904bf32bccbd3e9a4274d604776cb98
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gnutls/gnutls.mk
+++ package/gnutls/gnutls.mk
@@ -8,7 +8,7 @@ GNUTLS_VERSION = 2.12.19
 GNUTLS_SOURCE = gnutls-$(GNUTLS_VERSION).tar.bz2
 GNUTLS_SITE = $(BR2_GNU_MIRROR)/gnutls
 GNUTLS_DEPENDENCIES = host-pkg-config libgcrypt
-GNUTLS_CONF_OPT = --with-libgcrypt --with-libgcrypt-prefix=$(STAGING_DIR)/usr \
+GNUTLS_CONF_OPT = --with-libgcrypt \
 		--without-p11-kit
 GNUTLS_INSTALL_STAGING = YES
 
