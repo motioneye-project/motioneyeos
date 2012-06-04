3bc0d5be844f41cf58120ffc3d306eeae8868cc9
Peter Korsgaard <jacmet@sunsite.dk>
2012-06-04 16:14:43 +0200

    gnutls: add optional zlib support
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gnutls/gnutls.mk
+++ package/gnutls/gnutls.mk
@@ -7,7 +7,7 @@
 GNUTLS_VERSION = 2.12.19
 GNUTLS_SOURCE = gnutls-$(GNUTLS_VERSION).tar.bz2
 GNUTLS_SITE = $(BR2_GNU_MIRROR)/gnutls
-GNUTLS_DEPENDENCIES = host-pkg-config libgcrypt
+GNUTLS_DEPENDENCIES = host-pkg-config libgcrypt $(if $(BR2_PACKAGE_ZLIB),zlib)
 GNUTLS_CONF_OPT = --with-libgcrypt \
 		--without-p11-kit
 GNUTLS_INSTALL_STAGING = YES
