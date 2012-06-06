27930b0c219300ce5f32ac676245306481e776cf
Peter Korsgaard <jacmet@sunsite.dk>
2012-06-06 02:23:44 +0200

    gnutls: disable libgcrypt prefix mangling
    Fixes http://autobuild.buildroot.net/results/77a0ca1d28a68daf25c2588eede86f779c772f97
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gnutls/gnutls.mk
+++ package/gnutls/gnutls.mk
@@ -8,7 +8,7 @@ GNUTLS_VERSION = 2.12.19
 GNUTLS_SOURCE = gnutls-$(GNUTLS_VERSION).tar.bz2
 GNUTLS_SITE = $(BR2_GNU_MIRROR)/gnutls
 GNUTLS_DEPENDENCIES = host-pkg-config libgcrypt $(if $(BR2_PACKAGE_ZLIB),zlib)
-GNUTLS_CONF_OPT = --with-libgcrypt \
+GNUTLS_CONF_OPT = --with-libgcrypt --without-libgcrypt-prefix \
 		--without-p11-kit
 GNUTLS_INSTALL_STAGING = YES
 
