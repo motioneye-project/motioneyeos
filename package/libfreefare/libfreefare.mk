e2b296ad7f01bbc3dd073afd3e9fbb140244bed0
Simon Dawson <spdawson@gmail.com>
2012-06-08 03:37:25 +0000

    libfreefare: new package
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libfreefare/libfreefare.mk
@@ -0,0 +1,10 @@
+#############################################################
+#
+# libfreefare
+#
+#############################################################
+LIBFREEFARE_VERSION = 0.3.2
+LIBFREEFARE_SITE = http://nfc-tools.googlecode.com/files
+LIBFREEFARE_DEPENDENCIES = libnfc
+
+$(eval $(call AUTOTARGETS))
