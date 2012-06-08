4628f823ee0f7dcea55d7111bcfdafb664843351
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-06-08 05:30:44 +0000

    bison: bump to version 2.5.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/bison/bison.mk
+++ package/bison/bison.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-BISON_VERSION = 2.5
+BISON_VERSION = 2.5.1
 BISON_SITE = $(BR2_GNU_MIRROR)/bison
 
 BISON_CONF_ENV += ac_cv_path_M4=/usr/bin/m4
