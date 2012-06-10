0a65b7d93662c9c801af777528561e488389d592
Markos Chandras <markos.chandras@imgtec.com>
2012-06-10 23:35:19 +0000

    synergy: Remove -Werror from CXXFLAGS
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/synergy/synergy.mk
+++ package/synergy/synergy.mk
@@ -7,6 +7,8 @@
 SYNERGY_VERSION = 1.3.1
 SYNERGY_SOURCE = synergy-$(SYNERGY_VERSION).tar.gz
 SYNERGY_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/synergy2/
+
+SYNERGY_AUTORECONF = YES
 SYNERGY_CONF_OPT = --x-includes=$(STAGING_DIR)/usr/include/X11 \
                    --x-libraries=$(STAGING_DIR)/usr/lib
 
