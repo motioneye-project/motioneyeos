e12d78e2bc2d0e36dbdf1ced099ae924f7a82ca2
Peter Korsgaard <jacmet@sunsite.dk>
2012-06-10 23:46:36 +0200

    synergy: fix dependencies
    Synergy can be used without a local X server, and Xinerama is an optional
    dependency.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/synergy/synergy.mk
+++ package/synergy/synergy.mk
@@ -10,6 +10,7 @@ SYNERGY_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/s
 SYNERGY_CONF_OPT = --x-includes=$(STAGING_DIR)/usr/include/X11 \
                    --x-libraries=$(STAGING_DIR)/usr/lib
 
-SYNERGY_DEPENDENCIES = xserver_xorg-server xlib_libXtst
+SYNERGY_DEPENDENCIES = xlib_libXtst \
+		$(if $(BR2_PACKAGE_XLIB_LIBXINERAMA),xlib_libXinerama)
 
 $(eval $(call AUTOTARGETS))
