c5832382a18e285376e9a519e3fbfd6436e42a73
Simon Dawson <spdawson@gmail.com>
2012-06-11 22:27:15 +0000

    monit: fix configuration error
    Configuration fails for the monit package, with the following error.
    
      checking for SSL library directory... Not found
    
      Couldn't find your SSL library files.
      Use --with-ssl-lib-dir option to fix this problem or disable the
      SSL support with --without-ssl
    
    Tweak the monit configuration command line so that openssl is found.
    
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/monit/monit.mk
+++ package/monit/monit.mk
@@ -15,7 +15,7 @@ MONIT_CONF_OPT += \
 	--without-pam
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
-MONIT_CONF_OPT += --with-ssl
+MONIT_CONF_OPT += --with-ssl=$(STAGING_DIR)/usr
 MONIT_DEPENDENCIES += openssl
 else
 MONIT_CONF_OPT += --without-ssl
