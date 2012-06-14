97e90a63ccea99bbd4119e94e7b025a5b36d9cdf
Will Wagner <will_wagner@carallon.com>
2012-06-14 21:10:34 +0100

    xlib_libXScrnSaver: bump revision to X11R7.7
    Signed-off-by: Will Wagner <will_wagner@carallon.com>

--- package/x11r7/xlib_libXScrnSaver/xlib_libXScrnSaver.mk
+++ package/x11r7/xlib_libXScrnSaver/xlib_libXScrnSaver.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBXSCRNSAVER_VERSION = 1.2.1
+XLIB_LIBXSCRNSAVER_VERSION = 1.2.2
 XLIB_LIBXSCRNSAVER_SOURCE = libXScrnSaver-$(XLIB_LIBXSCRNSAVER_VERSION).tar.bz2
 XLIB_LIBXSCRNSAVER_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXSCRNSAVER_INSTALL_STAGING = YES
