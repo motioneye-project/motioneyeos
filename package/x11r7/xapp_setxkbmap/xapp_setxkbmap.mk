1b600d9a76913b2cec678f4ebbdad8a254829f6f
Will Wagner <will_wagner@carallon.com>
2012-06-14 20:41:22 +0100

    xapp_setxkbmap: bump revision to X11R7.7
    Signed-off-by: Will Wagner <will_wagner@carallon.com>

--- package/x11r7/xapp_setxkbmap/xapp_setxkbmap.mk
+++ package/x11r7/xapp_setxkbmap/xapp_setxkbmap.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_SETXKBMAP_VERSION = 1.2.0
+XAPP_SETXKBMAP_VERSION = 1.3.0
 XAPP_SETXKBMAP_SOURCE = setxkbmap-$(XAPP_SETXKBMAP_VERSION).tar.bz2
 XAPP_SETXKBMAP_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_SETXKBMAP_DEPENDENCIES = xlib_libX11 xlib_libxkbfile
