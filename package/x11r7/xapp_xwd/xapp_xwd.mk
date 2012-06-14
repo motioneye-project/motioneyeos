59a9c8c5938e618f58f21d55798df5bd8425e650
Will Wagner <will_wagner@carallon.com>
2012-06-14 20:52:29 +0100

    xapp_xwd: bump revision to X11R7.7
    Signed-off-by: Will Wagner <will_wagner@carallon.com>

--- package/x11r7/xapp_xwd/xapp_xwd.mk
+++ package/x11r7/xapp_xwd/xapp_xwd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XWD_VERSION = 1.0.4
+XAPP_XWD_VERSION = 1.0.5
 XAPP_XWD_SOURCE = xwd-$(XAPP_XWD_VERSION).tar.bz2
 XAPP_XWD_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XWD_DEPENDENCIES = xlib_libX11 xlib_libXmu
