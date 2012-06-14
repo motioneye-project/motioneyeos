3bce5eedcd7f3c3ec1af5090496410f7081e9fae
Will Wagner <will_wagner@carallon.com>
2012-06-14 20:40:42 +0100

    xapp_makefontscale: bump revision to X11R7.7
    Signed-off-by: Will Wagner <will_wagner@carallon.com>

--- package/x11r7/xapp_mkfontscale/xapp_mkfontscale.mk
+++ package/x11r7/xapp_mkfontscale/xapp_mkfontscale.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_MKFONTSCALE_VERSION = 1.0.8
+XAPP_MKFONTSCALE_VERSION = 1.1.0
 XAPP_MKFONTSCALE_SOURCE = mkfontscale-$(XAPP_MKFONTSCALE_VERSION).tar.bz2
 XAPP_MKFONTSCALE_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_MKFONTSCALE_DEPENDENCIES = zlib freetype xlib_libfontenc xproto_xproto
