b843b9de00bcc19a721dc189cda48402cfa2bbdd
Will Wagner <will_wagner@carallon.com>
2012-06-14 21:00:24 +0100

    xdriver_xf86-video-glide: bump revision to X11R7.7
    Signed-off-by: Will Wagner <will_wagner@carallon.com>

--- package/x11r7/xdriver_xf86-video-glide/xdriver_xf86-video-glide.mk
+++ package/x11r7/xdriver_xf86-video-glide/xdriver_xf86-video-glide.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_GLIDE_VERSION = 1.1.0
+XDRIVER_XF86_VIDEO_GLIDE_VERSION = 1.2.0
 XDRIVER_XF86_VIDEO_GLIDE_SOURCE = xf86-video-glide-$(XDRIVER_XF86_VIDEO_GLIDE_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_GLIDE_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_GLIDE_DEPENDENCIES = xserver_xorg-server xproto_fontsproto xproto_randrproto xproto_renderproto xproto_videoproto xproto_xproto
