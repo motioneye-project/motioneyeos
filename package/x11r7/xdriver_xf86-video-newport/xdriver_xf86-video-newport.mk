a77da265be651591bd9f70783f3df4f96aed9337
Will Wagner <will_wagner@carallon.com>
2012-06-14 20:09:46 +0100

    xdriver_xf86-video-newport: bump revision to X11R7.7
    Signed-off-by: Will Wagner <will_wagner@carallon.com>

--- package/x11r7/xdriver_xf86-video-newport/xdriver_xf86-video-newport.mk
+++ package/x11r7/xdriver_xf86-video-newport/xdriver_xf86-video-newport.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_NEWPORT_VERSION = 0.2.3
+XDRIVER_XF86_VIDEO_NEWPORT_VERSION = 0.2.4
 XDRIVER_XF86_VIDEO_NEWPORT_SOURCE = xf86-video-newport-$(XDRIVER_XF86_VIDEO_NEWPORT_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_NEWPORT_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_NEWPORT_DEPENDENCIES = xserver_xorg-server xproto_fontsproto xproto_randrproto xproto_renderproto xproto_videoproto xproto_xproto
