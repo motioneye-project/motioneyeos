856727f4d0e5c8db9316c97cd2f4a51a643e373d
Simon Dawson <spdawson@gmail.com>
2012-06-14 10:02:11 +0000

    jquery-ui: new package
    [Peter: select jquery, update for generic-package, drop _PATH options]
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/jquery-ui/jquery-ui.mk
@@ -0,0 +1,35 @@
+#############################################################
+#
+# jquery-ui
+#
+#############################################################
+
+JQUERY_UI_VERSION = 1.8.21
+JQUERY_UI_SITE = http://jquery-ui.googlecode.com/files
+JQUERY_UI_SOURCE = jquery-ui-$(JQUERY_UI_VERSION).zip
+
+define JQUERY_UI_EXTRACT_CMDS
+	unzip -d $(@D) $(DL_DIR)/$(JQUERY_UI_SOURCE)
+	mv $(@D)/jquery-ui-$(JQUERY_UI_VERSION)/* $(@D)
+	$(RM) -r $(@D)/jquery-ui-$(JQUERY_UI_VERSION)
+endef
+
+define JQUERY_UI_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0644 -D $(@D)/ui/minified/jquery-ui.min.js \
+		$(TARGET_DIR)/var/www/jquery-ui.js
+	$(INSTALL) -m 0644 -D $(@D)/ui/minified/i18n/jquery-ui-i18n.min.js \
+		$(TARGET_DIR)/var/www/jquery-ui-i18n.js
+	$(INSTALL) -m 0644 -D $(@D)/themes/base/minified/jquery-ui.min.css \
+		$(TARGET_DIR)/var/www/jquery-ui.css
+	$(INSTALL) -d $(TARGET_DIR)/var/www/images
+	cp -a $(@D)/themes/base/minified/images/*.png \
+		$(TARGET_DIR)/var/www/images
+	chmod 0644 $(TARGET_DIR)/var/www/images/*.png
+endef
+
+define JQUERY_UI_UNINSTALL_TARGET_CMDS
+	$(RM) $(addprefix $(TARGET_DIR)/var/www/,\
+		jquery-ui.js jquery-ui-i18n.js jquery-ui.css)
+endef
+
+$(eval $(generic-package))
