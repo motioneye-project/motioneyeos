802c43ac3058c538e0d41844d7180a3f94e29570
Will Wagner <will_wagner@carallon.com>
2012-06-14 21:12:55 +0100

    xlib_libXfont: bump revision to X11R7.7
    Signed-off-by: Will Wagner <will_wagner@carallon.com>

--- package/x11r7/xlib_libXfont/xlib_libXfont.mk
+++ package/x11r7/xlib_libXfont/xlib_libXfont.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-XLIB_LIBXFONT_VERSION = 1.4.3
+XLIB_LIBXFONT_VERSION = 1.4.5
 XLIB_LIBXFONT_SOURCE = libXfont-$(XLIB_LIBXFONT_VERSION).tar.bz2
 XLIB_LIBXFONT_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXFONT_AUTORECONF = YES
 XLIB_LIBXFONT_INSTALL_STAGING = YES
-XLIB_LIBXFONT_DEPENDENCIES = freetype xlib_libfontenc xlib_xtrans xproto_fontcacheproto xproto_fontsproto xproto_xproto xfont_encodings
+XLIB_LIBXFONT_DEPENDENCIES = freetype xlib_libfontenc xlib_xtrans xproto_fontsproto xproto_xproto xfont_encodings
 XLIB_LIBXFONT_CONF_OPT = --disable-devel-docs
 
 HOST_XLIB_LIBXFONT_CONF_OPT = --disable-devel-docs
