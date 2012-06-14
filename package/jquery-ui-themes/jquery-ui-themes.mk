77572e6f9fa116562f050b3d1c255ba0010e8ff0
Simon Dawson <spdawson@gmail.com>
2012-06-14 10:02:12 +0000

    jquery-ui-themes: new package
    [Peter: drop CSS path option, depend on jquery-ui, misc cleanups]
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/jquery-ui-themes/jquery-ui-themes.mk
@@ -0,0 +1,32 @@
+#############################################################
+#
+# jquery-ui-themes
+#
+#############################################################
+
+JQUERY_UI_THEMES_VERSION = 1.8.21
+JQUERY_UI_THEMES_SITE = http://jquery-ui.googlecode.com/files
+JQUERY_UI_THEMES_SOURCE = jquery-ui-themes-$(JQUERY_UI_THEMES_VERSION).zip
+JQUERY_UI_THEMES_DEPENDENCIES = jquery-ui
+
+define JQUERY_UI_THEMES_EXTRACT_CMDS
+	unzip -d $(@D) $(DL_DIR)/$(JQUERY_UI_THEMES_SOURCE)
+	mv $(@D)/jquery-ui-themes-$(JQUERY_UI_THEMES_VERSION)/* $(@D)
+	$(RM) -r $(@D)/jquery-ui-themes-$(JQUERY_UI_THEMES_VERSION)
+endef
+
+define JQUERY_UI_THEMES_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0644 -D \
+		$(@D)/themes/$(BR2_PACKAGE_JQUERY_UI_THEMES_THEME)/jquery-ui.css \
+		$(TARGET_DIR)/var/www/jquery-ui.css
+	$(INSTALL) -d $(TARGET_DIR)/var/www/images
+	cp -a $(@D)/themes/$(BR2_PACKAGE_JQUERY_UI_THEMES_THEME)/images/*.png \
+		$(TARGET_DIR)/var/www/images
+	chmod 0644 $(TARGET_DIR)/var/www/images/*.png
+endef
+
+define JQUERY_UI_THEMES_UNINSTALL_TARGET_CMDS
+	$(RM) $(TARGET_DIR)/var/www/jquery-ui.css
+endef
+
+$(eval $(generic-package))
