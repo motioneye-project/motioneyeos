929c3e89a6715ecfdc8055ec82025cee5e8e23a1
Simon Dawson <spdawson@gmail.com>
2012-06-16 23:07:40 +0000

    libfreefare: add missing openssl dependency
    If openssl is not selected, configuration fails for the libfreefare package
    as follows.
    
      configure: error: Cannot find libcrypto.
    
    This patch adds the missing dependency on openssl.
    
    [Peter: use select instead of depends on]
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libfreefare/libfreefare.mk
+++ package/libfreefare/libfreefare.mk
@@ -5,6 +5,6 @@
 #############################################################
 LIBFREEFARE_VERSION = 0.3.2
 LIBFREEFARE_SITE = http://nfc-tools.googlecode.com/files
-LIBFREEFARE_DEPENDENCIES = libnfc
+LIBFREEFARE_DEPENDENCIES = libnfc openssl
 
 $(eval $(call AUTOTARGETS))
