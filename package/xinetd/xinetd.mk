1a4b482776db83e331ff787d01512f6d52610f2a
Peter Korsgaard <jacmet@sunsite.dk>
2012-06-16 22:58:26 +0200

    xinetd: fix build without RPC support
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/xinetd/xinetd.mk
+++ package/xinetd/xinetd.mk
@@ -7,4 +7,8 @@ XINETD_VERSION = 2.3.15
 XINETD_SOURCE  = xinetd-$(XINETD_VERSION).tar.gz
 XINETD_SITE    = http://www.xinetd.org
 
+ifneq ($(BR2_INET_RPC),y)
+XINETD_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -DNO_RPC"
+endif
+
 $(eval $(call AUTOTARGETS))
