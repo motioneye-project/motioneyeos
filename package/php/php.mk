74b442c8b41f466253b84fd4b529cd1a6bac2587
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-06-17 04:06:03 +0000

    php: security bump to version 5.3.14
    Bump php to version 5.3.14 to fix CVE-2012-2143 and other assorted bugs.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-PHP_VERSION = 5.3.13
+PHP_VERSION = 5.3.14
 PHP_SOURCE = php-$(PHP_VERSION).tar.bz2
 PHP_SITE = http://www.php.net/distributions
 PHP_INSTALL_STAGING = YES
