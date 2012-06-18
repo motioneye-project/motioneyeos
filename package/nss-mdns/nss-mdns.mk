28943cf00545e9c923364bfca4af044faaf6fc53
Marek Belisko <marek.belisko@gmail.com>
2012-06-18 11:32:51 +0000

    Add package nss-mdns.
    Signed-off-by: Marek Belisko <marek.belisko@open-nandra.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/nss-mdns/nss-mdns.mk
@@ -0,0 +1,15 @@
+#############################################################
+#
+# nss_mdns
+#
+#############################################################
+NSS_MDNS_VERSION=0.10
+NSS_MDNS_SITE=http://0pointer.de/lennart/projects/nss-mdns
+
+define NSS_MDNS_INSTALL_CONFIG
+	$(INSTALL) -D -m 0664 package/nss-mdns/nsswitch.conf $(TARGET_DIR)/etc/nsswitch.conf
+endef
+
+NSS_MDNS_POST_INSTALL_TARGET_HOOKS += NSS_MDNS_INSTALL_CONFIG
+
+$(eval $(call AUTOTARGETS))
