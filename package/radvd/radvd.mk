e73f03d579aeb194ded0f9951571553a8422dd23
Peter Korsgaard <jacmet@sunsite.dk>
2012-06-18 23:32:39 +0200

    radvd: fix bashism in configure.ac
    Fixes http://autobuild.buildroot.net/results/c3d0d04f8b3df63b3206cecf54dfdd67a4c7ccfb/build-end.log
    
    Patch sent upstream.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/radvd/radvd.mk
+++ package/radvd/radvd.mk
@@ -7,6 +7,7 @@
 RADVD_VERSION = 1.8.5
 RADVD_SITE = http://www.litech.org/radvd/dist
 RADVD_DEPENDENCIES = flex host-flex
+RADVD_AUTORECONF = YES
 
 define RADVD_INSTALL_INITSCRIPT
 	$(INSTALL) -m 0755 package/radvd/S50radvd $(TARGET_DIR)/etc/init.d
