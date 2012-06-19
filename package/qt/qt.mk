81e0952d9ece82d992070cc8215553d396897e4f
Luca Ceresoli <luca@lucaceresoli.net>
2012-06-19 06:59:03 +0000

    qt: bump to 4.8.2
    Also switch to the new upstream URL.
    
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/qt/qt.mk
+++ package/qt/qt.mk
@@ -11,9 +11,9 @@
 #
 ######################################################################
 
-QT_VERSION = 4.8.1
+QT_VERSION = 4.8.2
 QT_SOURCE  = qt-everywhere-opensource-src-$(QT_VERSION).tar.gz
-QT_SITE    = http://get.qt.nokia.com/qt/source
+QT_SITE    = http://releases.qt-project.org/qt4/source
 QT_DEPENDENCIES = host-pkg-config
 QT_INSTALL_STAGING = YES
 
