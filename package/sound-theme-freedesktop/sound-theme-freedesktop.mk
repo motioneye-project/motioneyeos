f86391fc07731c26d96acec5f1d082378f9e494d
Simon Dawson <spdawson@gmail.com>
2012-06-20 01:50:04 +0000

    sound-theme-freedesktop: new package
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/sound-theme-freedesktop/sound-theme-freedesktop.mk
@@ -0,0 +1,12 @@
+#############################################################
+#
+# sound-theme-freedesktop
+#
+#############################################################
+SOUND_THEME_FREEDESKTOP_VERSION = 0.7
+SOUND_THEME_FREEDESKTOP_SITE = \
+	http://people.freedesktop.org/~mccann/dist
+SOUND_THEME_FREEDESKTOP_SOURCE = \
+	sound-theme-freedesktop-$(SOUND_THEME_FREEDESKTOP_VERSION).tar.bz2
+
+$(eval $(call AUTOTARGETS))
