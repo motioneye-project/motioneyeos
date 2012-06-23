e065b0ecd548c7c3ea6069b50f183dd2fc85a1b9
Peter Korsgaard <jacmet@sunsite.dk>
2012-06-24 00:55:27 +0200

    libconfig: don't build examples
    Fixes (works around)
    
    http://autobuild.buildroot.net/results/2378161d70beade94547a68eeeaa69e34705baf2
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libconfig/libconfig.mk
+++ package/libconfig/libconfig.mk
@@ -6,9 +6,10 @@
 LIBCONFIG_VERSION = 1.4.8
 LIBCONFIG_SITE = http://www.hyperrealm.com/libconfig/
 LIBCONFIG_INSTALL_STAGING = YES
+LIBCONFIG_CONF_OPT = --disable-examples
 
 ifneq ($(BR2_INSTALL_LIBSTDCPP),y)
-LIBCONFIG_CONF_OPT = --disable-cxx
+LIBCONFIG_CONF_OPT += --disable-cxx
 endif
 
 $(eval $(call AUTOTARGETS))
