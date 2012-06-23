e3cae83573ab9030da718b68de2d9d61a0728143
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-06-23 01:29:59 +0000

    gzip: bump to version 1.5
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gzip/gzip.mk
+++ package/gzip/gzip.mk
@@ -3,8 +3,8 @@
 # gzip
 #
 #############################################################
-GZIP_VERSION:=1.4
-GZIP_SOURCE:=gzip-$(GZIP_VERSION).tar.gz
-GZIP_SITE:=$(BR2_GNU_MIRROR)/gzip
+
+GZIP_VERSION = 1.5
+GZIP_SITE = $(BR2_GNU_MIRROR)/gzip
 
 $(eval $(call AUTOTARGETS))
