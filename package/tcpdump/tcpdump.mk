ac2a58c5c3627d39e2e7f77119dea2da6fa0b355
Baruch Siach <baruch@tkos.co.il>
2012-06-24 18:00:29 +0000

    tcpdump: bump to version 4.3.0
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tcpdump/tcpdump.mk
+++ package/tcpdump/tcpdump.mk
@@ -6,9 +6,8 @@
 # Copyright (C) 2001-2003 by Erik Andersen <andersen@codepoet.org>
 # Copyright (C) 2002 by Tim Riker <Tim@Rikers.org>
 
-TCPDUMP_VERSION = 4.2.1
+TCPDUMP_VERSION = 4.3.0
 TCPDUMP_SITE = http://www.tcpdump.org/release
-TCPDUMP_SOURCE = tcpdump-$(TCPDUMP_VERSION).tar.gz
 TCPDUMP_CONF_ENV = ac_cv_linux_vers=2 td_cv_buggygetaddrinfo=no
 TCPDUMP_CONF_OPT = --without-crypto \
 		$(if $(BR2_PACKAGE_TCPDUMP_SMB),--enable-smb,--disable-smb)
