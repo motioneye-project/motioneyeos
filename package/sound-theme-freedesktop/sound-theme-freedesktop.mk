ea6c1b9f12b932dc373b3b58459bc676e17f0e54
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-06-24 11:16:34 +0000

    sound-theme-freedesktop: add host-intltool dependency
    Fixes
    http://autobuild.buildroot.org/results/c4e5cad3e4bf08440cea081789b8d58b238d9683/build-end.log.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sound-theme-freedesktop/sound-theme-freedesktop.mk
+++ package/sound-theme-freedesktop/sound-theme-freedesktop.mk
@@ -8,5 +8,6 @@ SOUND_THEME_FREEDESKTOP_SITE = \
 	http://people.freedesktop.org/~mccann/dist
 SOUND_THEME_FREEDESKTOP_SOURCE = \
 	sound-theme-freedesktop-$(SOUND_THEME_FREEDESKTOP_VERSION).tar.bz2
+SOUND_THEME_FREEDESKTOP_DEPENDENCIES = host-intltool
 
 $(eval $(call AUTOTARGETS))
