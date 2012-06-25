eb3bff76eceeb5ee277d19f5e357b2b35e7922de
Simon Dawson <spdawson@gmail.com>
2012-06-25 04:43:17 +0000

    json-javascript: new package
    [Peter: update for generic-package, add license info]
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/json-javascript/json-javascript.mk
@@ -0,0 +1,20 @@
+#############################################################
+#
+# json-javascript
+#
+#############################################################
+
+JSON_JAVASCRIPT_VERSION = 42c18c621a411c3f39a81bb0a387fc50dcd738d9
+JSON_JAVASCRIPT_SITE = git://github.com/douglascrockford/JSON-js.git
+JSON_JAVASCRIPT_LICENSE = Public Domain
+JSON_JAVASCRIPT_LICENSE_FILES = json2.js
+
+define JSON_JAVASCRIPT_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0644 -D $(@D)/json2.js $(TARGET_DIR)/var/www/json2.js
+endef
+
+define JSON_JAVASCRIPT_UNINSTALL_TARGET_CMDS
+	$(RM) $(TARGET_DIR)/var/www/json2.js
+endef
+
+$(eval $(generic-package))
