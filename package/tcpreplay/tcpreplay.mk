a2ddfdb11a2c28c511a2a4d5bf0060217c93d140
Peter Korsgaard <jacmet@sunsite.dk>
2012-06-26 23:25:29 +0200

    tcpreplay: cleanup
    Drop unneeded uclibc dependency and autoreconf as patch changes
    configure.ac.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tcpreplay/tcpreplay.mk
+++ package/tcpreplay/tcpreplay.mk
@@ -8,8 +8,8 @@ TCPREPLAY_VERSION = 3.4.3
 TCPREPLAY_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/tcpreplay
 TCPREPLAY_CONF_ENV = tr_cv_libpcap_version=">= 0.7.0"
 TCPREPLAY_CONF_OPT = --with-libpcap=$(STAGING_DIR)/usr
-
-TCPREPLAY_DEPENDENCIES = uclibc libpcap
+TCPREPLAY_AUTORECONF = YES
+TCPREPLAY_DEPENDENCIES = libpcap
 
 $(eval $(call AUTOTARGETS))
 
