c69be6d2abe8f8e265f92494867e2c7e1519dd08
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-06-26 04:19:50 +0000

    libpcap: canusb support needs pthreads
    canusb support in libpcap uses pthreads but the configure script doesn't
    account for -lpthread usage so it doesn't get added into LIBS which in
    turn doesn't add the appropiate entry in pcap-config, causing build
    failures for programs that link to libpcap that don't use pthreads
    themselves (for example tcpdump).
    Also add the conditional dependency on BR2_PACKAGE_LIBUSB for canusb.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libpcap/libpcap.mk
+++ package/libpcap/libpcap.mk
@@ -12,7 +12,8 @@ LIBPCAP_INSTALL_TARGET_OPT= DESTDIR="$(TARGET_DIR)" \
 	$(if $(BR2_PREFER_STATIC_LIB),install,install-shared)
 LIBPCAP_INSTALL_STAGING_OPT= DESTDIR="$(STAGING_DIR)" install \
 	$(if $(BR2_PREFER_STATIC_LIB),,install-shared)
-LIBPCAP_DEPENDENCIES = zlib
+LIBPCAP_DEPENDENCIES = zlib \
+	$(if $(BR2_PACKAGE_LIBUSB),libusb)
 LIBPCAP_CONF_ENV = ac_cv_linux_vers=2 \
 		ac_cv_header_linux_wireless_h=yes # configure misdetects this
 LIBPCAP_CONF_OPT = --disable-yydebug --with-pcap=linux
