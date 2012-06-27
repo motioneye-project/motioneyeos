56168c215595e4b7bc6222fcee3d1021e327051d
Peter Korsgaard <jacmet@sunsite.dk>
2012-06-27 09:55:04 +0200

    faad2: fix frontend/faad linking
    Fixes http://autobuild.buildroot.net/results/84db857a6fc8e73483493e484f2cc5f4bf64f3d0
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/faad2/faad2.mk
+++ package/multimedia/faad2/faad2.mk
@@ -6,6 +6,8 @@
 
 FAAD2_VERSION = 2.7
 FAAD2_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/faac
+# frontend/faad calls frexp()
+FAAD2_CONF_ENV = LIBS=-lm
 FAAD2_INSTALL_STAGING = YES
 
 $(eval $(call AUTOTARGETS))
