a795e2a491c7a99530c79fc3f23a39b6a85ebce5
Peter Korsgaard <jacmet@sunsite.dk>
2012-06-27 09:42:33 +0200

    tcpreplay: fix linking when libpcap is built with canusb support
    Fixes http://autobuild.buildroot.net/results/f3cee3df639a8ecf0f7f81ca33efee4e040c76ff
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tcpreplay/tcpreplay.mk
+++ package/tcpreplay/tcpreplay.mk
@@ -11,5 +11,9 @@ TCPREPLAY_CONF_OPT = --with-libpcap=$(STAGING_DIR)/usr
 TCPREPLAY_AUTORECONF = YES
 TCPREPLAY_DEPENDENCIES = libpcap
 
+# libpcap may depend on symbols in libusb as well
+TCPREPLAY_LIBS = -lpcap $(if $(BR2_PACKAGE_LIBUSB),-lusb-1.0)
+TCPREPLAY_CONF_ENV += ac_cv_search_pcap_close='$(TCPREPLAY_LIBS)'
+
 $(eval $(call AUTOTARGETS))
 
