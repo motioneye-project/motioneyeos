df2b9516eaa6fc59b8bd847fa0af7ada99e59ad6
Peter Korsgaard <jacmet@sunsite.dk>
2012-06-29 16:34:46 +0200

    eeprog: use provided linker flags as well
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/eeprog/eeprog.mk
+++ package/eeprog/eeprog.mk
@@ -8,7 +8,7 @@ EEPROG_VERSION = 0.7.6
 EEPROG_SITE = http://codesink.org/download
 
 define EEPROG_BUILD_CMDS
-	$(MAKE) CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" -C $(@D)
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
 endef
 
 define EEPROG_INSTALL_TARGET_CMDS
