23465b76b4325f1742a53b2e2fd3795c87aecbf0
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-07-15 10:47:40 -0300

    mpfr: bump to version 3.1.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/mpfr/mpfr.mk
+++ package/mpfr/mpfr.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-MPFR_VERSION = 3.1.0
+MPFR_VERSION = 3.1.1
 MPFR_SITE = http://www.mpfr.org/mpfr-$(MPFR_VERSION)
 MPFR_SOURCE = mpfr-$(MPFR_VERSION).tar.bz2
 MPFR_INSTALL_STAGING = YES
