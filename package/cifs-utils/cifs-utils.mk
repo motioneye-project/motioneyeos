b9c3890cdd22f168bdc3cb52926d51ba2a2b61b3
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-07-15 10:47:44 -0300

    cifs-utils: bump to version 5.5
    Bump cifs-utils to version 5.5
    Also switch to http source instead of ftp.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/cifs-utils/cifs-utils.mk
+++ package/cifs-utils/cifs-utils.mk
@@ -1,6 +1,6 @@
-CIFS_UTILS_VERSION = 5.4
+CIFS_UTILS_VERSION = 5.5
 CIFS_UTILS_SOURCE  = cifs-utils-$(CIFS_UTILS_VERSION).tar.bz2
-CIFS_UTILS_SITE    = ftp://ftp.samba.org/pub/linux-cifs/cifs-utils/
+CIFS_UTILS_SITE    = http://ftp.samba.org/pub/linux-cifs/cifs-utils
 
 define CIFS_UTILS_NO_WERROR
 	$(SED) 's/-Werror//' $(@D)/Makefile.in
