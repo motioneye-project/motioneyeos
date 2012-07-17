9f16b69c595faf0eb48aa4fa581cbbb0bacde603
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-07-17 09:54:27 -0300

    libexif: security bump to version 0.6.21
    Fixes CVE-2012-2812, CVE-2012-2813, CVE-2012-2814, CVE-2012-2836,
    CVE-2012-2837, CVE-2012-2840, CVE-2012-2841 and CVE-2012-2845.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libexif/libexif.mk
+++ package/libexif/libexif.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBEXIF_VERSION = 0.6.20
+LIBEXIF_VERSION = 0.6.21
 LIBEXIF_SOURCE = libexif-$(LIBEXIF_VERSION).tar.bz2
 LIBEXIF_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/libexif/libexif/$(LIBEXIF_VERSION)
 LIBEXIF_INSTALL_STAGING = YES
