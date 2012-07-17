d09967e0716894b91d27348877467ecba5eccad0
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-07-17 20:03:53 +0200

    nasm: bump version to 2.10.01, remove dummy target package
    The target package was only present because at the time 'nasm' was
    packaged, there was a bug in the package infrastructure that prevented
    a package from having a host only variant. This has been fixed since
    then, so we can get rid of the useless dummy target package.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/nasm/nasm.mk
+++ package/nasm/nasm.mk
@@ -2,17 +2,10 @@
 #
 # nasm
 #
-# This is special case: nasm is used to build syslinux and
-# pxelinux. As these are for the target, we should cross-compile
-# nasm. However, as nasm is x86-only, there's no point in
-# cross-compiling it. So we just build it for the host. The target
-# variant is only provided because of a bug in the package
-# infrastructure that prevents having only a host variant.
 ############################################################
 
-NASM_VERSION=2.08.01
-NASM_SOURCE=nasm-$(NASM_VERSION).tar.bz2
-NASM_SITE=http://www.nasm.us/pub/nasm/releasebuilds/2.08.01/
+NASM_VERSION = 2.10.01
+NASM_SOURCE  = nasm-$(NASM_VERSION).tar.bz2
+NASM_SITE    = http://www.nasm.us/pub/nasm/releasebuilds/$(NASM_VERSION)
 
-$(eval $(call AUTOTARGETS))
 $(eval $(call AUTOTARGETS,host))
