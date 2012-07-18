5276cb27c0e9a2a0326148ab6198c827b6a72c8a
Simon Dawson <spdawson@gmail.com>
2012-07-18 18:11:51 +0100

    libnfc-llcp: new package
    This patch adds the libnfc-llcp package.
    
    This package requires a minium libnfc version of 1.6. A separate patch has
    been submitted to bump the libnfc version; that patch should be regarded as
    a prerequisite for the present patch.
    
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/libnfc-llcp/libnfc-llcp.mk
@@ -0,0 +1,14 @@
+#############################################################
+#
+# libnfc-llcp
+#
+#############################################################
+LIBNFC_LLCP_VERSION = 1090
+LIBNFC_LLCP_SITE = http://nfc-tools.googlecode.com/svn/trunk/libnfc-llcp/
+LIBNFC_LLCP_SITE_METHOD = svn
+LIBNFC_LLCP_DEPENDENCIES = libnfc
+LIBNFC_LLCP_AUTORECONF = YES
+LIBNFC_LLCP_AUTORECONF_OPT = --install --force --verbose
+LIBNFC_LLCP_INSTALL_STAGING = YES
+
+$(eval $(autotools-package))
