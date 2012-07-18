cc160a941ee2de5aa9bd4624dd56fd6b38bb29c9
Eric Jarrige <eric.jarrige@armadeus.org>
2012-07-18 18:02:42 +0200

    Add (Freescale) elftosb host package
    Elftosb is a Freescale toolset to build binary image files (like U-Boot.sb)
    to be used with CPUs imx23, imx28, stp37xx
    
    thomas.petazzoni@free-electrons.com:
     * Add -m 0755 when installing binaries
     * the _CLEAN_CMDS is not supposed to uninstall things from the
       HOST_DIR.
     * Use 'host-generic-package' instead of 'GENTARGETS,host'
    
    Signed-off-by: Eric Jarrige <eric.jarrige@armadeus.org>
    Acked-by: Maxime Ripard <maxime.ripard@free-electrons.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/elftosb/elftosb.mk
@@ -0,0 +1,25 @@
+#############################################################
+#
+# elftosb
+#
+#############################################################
+ELFTOSB_VERSION = 10.12.01
+ELFTOSB_SOURCE = elftosb-$(ELFTOSB_VERSION).tar.gz
+ELFTOSB_SITE = http://repository.timesys.com/buildsources/e/elftosb/elftosb-$(ELFTOSB_VERSION)
+
+define HOST_ELFTOSB_BUILD_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) all
+endef
+
+define HOST_ELFTOSB_INSTALL_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/bld/linux/elftosb $(HOST_DIR)/usr/bin/elftosb
+	$(INSTALL) -D -m 0755 $(@D)/bld/linux/keygen $(HOST_DIR)/usr/bin/keygen
+	$(INSTALL) -D -m 0755 $(@D)/bld/linux/sbtool $(HOST_DIR)/usr/bin/sbtool
+endef
+
+define HOST_ELFTOSB_CLEAN_CMDS
+	rm -rf $(@D)/bld/linux
+endef
+
+$(eval $(host-generic-package))
+
