6d38cf852248764b633750d210f71cac983ec451
Simon Dawson <spdawson@gmail.com>
2012-07-18 18:10:25 +0100

    libfreefare: bump version to 0.3.4
    This patch bumps the libfreefare package from version 0.3.2 to version 0.3.4.
    
    Version 0.3.4 requires a minium libnfc version of 1.6. A separate patch has
    been submitted to bump the libnfc version; that patch should be regarded as
    a prerequisite for the present patch.
    
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libfreefare/libfreefare.mk
+++ package/libfreefare/libfreefare.mk
@@ -3,7 +3,7 @@
 # libfreefare
 #
 #############################################################
-LIBFREEFARE_VERSION = 0.3.2
+LIBFREEFARE_VERSION = 0.3.4
 LIBFREEFARE_SITE = http://nfc-tools.googlecode.com/files
 LIBFREEFARE_DEPENDENCIES = libnfc openssl
 
