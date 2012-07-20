f3c1ee9a598d470956d029a16b64a33c10aee06d
gregory hermant <gregory.hermant@calao-systems.com>
2012-07-20 18:06:12 +0200

    dfu-util: new package
    Signed-off-by: Gregory Hermant <gregory.hermant@calao-systems.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/dfu-util/dfu-util.mk
@@ -0,0 +1,13 @@
+#############################################################
+#
+# dfu-util
+#
+#############################################################
+DFU_UTIL_VERSION:=0.6
+DFU_UTIL_SOURCE = dfu-util-$(DFU_UTIL_VERSION).tar.gz
+DFU_UTIL_SITE = http://dfu-util.gnumonks.org/releases/
+
+HOST_DFU_UTIL_DEPENDENCIES = host-libusb
+
+$(eval $(host-autotools-package))
+
