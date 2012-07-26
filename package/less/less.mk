fe355d22785479a646b7875c8b0404ca5d6292e8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-07-26 16:18:37 -0300

    less: build after busybox
    Build less after busybox if it's enabled, since we prefer the full-blown
    version over the light one if it's enabled (and busybox would overwrite
    the link when built afterwards).
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/less/less.mk
+++ package/less/less.mk
@@ -6,7 +6,8 @@
 
 LESS_VERSION = 444
 LESS_SITE = http://www.greenwoodsoftware.com/less
-LESS_DEPENDENCIES = ncurses
+# Build after busybox, full-blown is better
+LESS_DEPENDENCIES = ncurses $(if $(BR2_PACKAGE_BUSYBOX),busybox)
 
 define LESS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/less $(TARGET_DIR)/usr/bin/less
