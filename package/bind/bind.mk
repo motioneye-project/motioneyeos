c53e14e30c3df40c579032f3c50e5b3db34efd98
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-07-27 20:31:28 -0300

    bind: security bump to version 9.6-ESV-R7-P2
    Fixes CVE-2012-3817
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/bind/bind.mk
+++ package/bind/bind.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-BIND_VERSION = 9.6-ESV-R7-P1
+BIND_VERSION = 9.6-ESV-R7-P2
 BIND_SITE = ftp://ftp.isc.org/isc/bind9/$(BIND_VERSION)
 BIND_MAKE = $(MAKE1)
 BIND_TARGET_SBINS = lwresd named named-checkconf named-checkzone
