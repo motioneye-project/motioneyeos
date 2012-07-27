58a62766a1869c913845a649890855515aebb016
Will Newton <will.newton@gmail.com>
2012-07-27 16:26:23 +0100

    sysprof: New package.
    [thomas.petazzoni@free-electrons.com:
     - add dependencies on the supported architectures to avoid build
       failures on ARM and other unsupported arches
     - add a patch that removes an useless warning about the kernel
       version. This patch is present upstream. However since it modifies
       configure.ac, we need to autoreconf the package.]
    
    Signed-off-by: Will Newton <will.newton@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/sysprof/sysprof.mk
@@ -0,0 +1,22 @@
+#############################################################
+#
+# sysprof
+#
+#############################################################
+
+SYSPROF_VERSION = 1.1.8
+SYSPROF_SITE = http://sysprof.com
+SYSPROF_DEPENDENCIES = libglib2
+SYSPROF_AUTORECONF = YES
+
+ifeq ($(BR2_PACKAGE_SYSPROF_GUI),y)
+SYSPROF_DEPENDENCIES += libgtk2 libglade gdk-pixbuf
+endif
+
+define SYSPROF_CREATE_M4_DIR
+	mkdir -p $(@D)/m4
+endef
+
+SYSPROF_POST_PATCH_HOOKS += SYSPROF_CREATE_M4_DIR
+
+$(eval $(autotools-package))
