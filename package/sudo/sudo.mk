d30aaf3e0c5f90306a1c931794d9096d549405cf
Simon Dawson <spdawson@gmail.com>
2012-08-03 12:48:28 +0100

    sudo: bump version to 1.8.5p2
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/sudo/sudo.mk
+++ package/sudo/sudo.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SUDO_VERSION = 1.8.5p1
+SUDO_VERSION = 1.8.5p2
 SUDO_SITE    = http://www.sudo.ws/sudo/dist
 SUDO_CONF_OPT = \
 		--without-lecture \
