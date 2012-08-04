8c529aa7e4d7bd0b9a8e76c902de3e81480255ac
Danomi Manchego <danomimanchego123@gmail.com>
2012-08-03 22:57:46 -0400

    xinetd: Add license info
    Signed-off-by: Danomi Manchego <danomimanchego123@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/xinetd/xinetd.mk
+++ package/xinetd/xinetd.mk
@@ -3,9 +3,11 @@
 # xinetd
 #
 #############################################################
-XINETD_VERSION = 2.3.15
-XINETD_SOURCE  = xinetd-$(XINETD_VERSION).tar.gz
-XINETD_SITE    = http://www.xinetd.org
+XINETD_VERSION       = 2.3.15
+XINETD_SOURCE        = xinetd-$(XINETD_VERSION).tar.gz
+XINETD_SITE          = http://www.xinetd.org
+XINETD_LICENSE       = xinetd license
+XINETD_LICENSE_FILES = COPYRIGHT
 
 ifneq ($(BR2_INET_RPC),y)
 XINETD_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -DNO_RPC"
