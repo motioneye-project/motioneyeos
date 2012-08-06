8c5707020c64d7cd0184f01f71c231eeec7da2e4
Francois Perrad <fperrad@gmail.com>
2012-08-06 07:46:40 +0200

    cgilua: add license info
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/cgilua/cgilua.mk
+++ package/cgilua/cgilua.mk
@@ -7,6 +7,7 @@
 CGILUA_VERSION = 5.1.4
 CGILUA_SITE = http://github.com/downloads/keplerproject/cgilua
 CGILUA_DEPENDENCIES = luafilesystem
+CGILUA_LICENSE = MIT
 
 define CGILUA_INSTALL_TARGET_CMDS
 	$(MAKE) -C $(@D) install LUA_DIR="$(TARGET_DIR)/usr/share/lua"
