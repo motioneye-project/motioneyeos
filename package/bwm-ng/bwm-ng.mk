ee508010f41c0037614e90c08420208a7afe93a3
Kelvin Cheung <keguang.zhang@gmail.com>
2012-08-06 10:21:21 +0800

    bwm-ng: Add license info
    Add license info for bwm-ng.
    
    Signed-off-by: Kelvin Cheung <keguang.zhang@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/bwm-ng/bwm-ng.mk
+++ package/bwm-ng/bwm-ng.mk
@@ -7,6 +7,8 @@
 BWM_NG_VERSION = 0.6
 BWM_NG_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/bwmng/bwmng/$(BWM_NG_VERSION)/
 BWM_NG_CONF_OPT = --with-procnetdev --with-diskstats
+BWM_NG_LICENSE = GPLv2
+BWM_NG_LICENSE_FILES = COPYING
 
 ifeq ($(BR2_PACKAGE_NCURSES),y)
 BWM_NG_CONF_OPT += --with-ncurses
