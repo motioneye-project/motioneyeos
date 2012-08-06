e21517689de5e1f56dc2aa788a4aa84f89c54c8b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-08-06 16:15:18 -0300

    imagemagick: security bump to version 6.7.8-8
    Fixes CVE-2012-3437, CVE-2012-3437 and CVE-2012-3438.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/imagemagick/imagemagick.mk
+++ package/imagemagick/imagemagick.mk
@@ -4,8 +4,8 @@
 #
 #############################################################
 
-IMAGEMAGICK_MAJOR = 6.7.6
-IMAGEMAGICK_VERSION = $(IMAGEMAGICK_MAJOR)-9
+IMAGEMAGICK_MAJOR = 6.7.8
+IMAGEMAGICK_VERSION = $(IMAGEMAGICK_MAJOR)-8
 IMAGEMAGICK_SOURCE = ImageMagick-$(IMAGEMAGICK_VERSION).tar.bz2
 # The official ImageMagick site only keeps the latest versions
 # available, which is annoying. Use an alternate site that keeps all
