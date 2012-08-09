f0e1710c916a081d6e02daf804a585d2a9a0fd39
Simon Dawson <spdawson@gmail.com>
2012-08-09 10:59:32 +0100

    libtpl: add license information
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libtpl/libtpl.mk
+++ package/libtpl/libtpl.mk
@@ -7,5 +7,7 @@ LIBTPL_VERSION = 1.5
 LIBTPL_SOURCE = libtpl-$(LIBTPL_VERSION).tar.bz2
 LIBTPL_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/tpl
 LIBTPL_INSTALL_STAGING = YES
+LIBTPL_LICENSE = BSD-like
+LIBTPL_LICENSE_FILES = LICENSE
 
 $(eval $(autotools-package))
