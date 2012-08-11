9de6de21c2ea7b9ac4a135771ec6f85ee254ddb1
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-08-11 19:27:29 -0300

    mpd: bump to version 0.17
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/multimedia/mpd/mpd.mk
+++ package/multimedia/mpd/mpd.mk
@@ -4,9 +4,11 @@
 #
 #############################################################
 
-MPD_VERSION = 0.16.8
+MPD_VERSION = 0.17
 MPD_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/musicpd/mpd/$(MPD_VERSION)
 MPD_DEPENDENCIES = host-pkg-config libglib2
+MPD_LICENSE = GPLv2
+MPD_LICENSE_FILES = COPYING
 
 # Some options need an explicit --disable or --enable
 
