71438633767e30b5126867aab8170776bc31119e
Danomi Manchego <danomimanchego123@gmail.com>
2012-08-11 16:10:20 -0400

    libpcap: add license info
    Signed-off-by: Danomi Manchego <danomimanchego123@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libpcap/libpcap.mk
+++ package/libpcap/libpcap.mk
@@ -6,7 +6,10 @@
 
 LIBPCAP_VERSION = 1.3.0
 LIBPCAP_SITE = http://www.tcpdump.org/release
+LIBPCAP_LICENSE = BSD-3c
+LIBPCAP_LICENSE_FILES = LICENSE
 LIBPCAP_INSTALL_STAGING = YES
+
 # doesn't have an install-strip
 LIBPCAP_INSTALL_TARGET_OPT= DESTDIR="$(TARGET_DIR)" \
 	$(if $(BR2_PREFER_STATIC_LIB),install,install-shared)
