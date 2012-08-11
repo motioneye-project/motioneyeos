a3a698de727b9747f4fd4b1495426b7fe4918af3
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-08-11 19:27:32 -0300

    ethtool: bump to version 3.5
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -4,7 +4,9 @@
 #
 #############################################################
 
-ETHTOOL_VERSION = 3.4.1
+ETHTOOL_VERSION = 3.5
 ETHTOOL_SITE = $(BR2_KERNEL_MIRROR)/software/network/ethtool
+ETHTOOL_LICENSE = GPLv2
+ETHTOOL_LICENSE_FILE = COPYING
 
 $(eval $(autotools-package))
