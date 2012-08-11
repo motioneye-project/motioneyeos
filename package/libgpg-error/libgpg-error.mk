ad9bab7ae3e84e5216ac753eefe93c1d34c7c979
Danomi Manchego <danomimanchego123@gmail.com>
2012-08-11 16:10:19 -0400

    libgpg-error: add license info
    Signed-off-by: Danomi Manchego <danomimanchego123@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libgpg-error/libgpg-error.mk
+++ package/libgpg-error/libgpg-error.mk
@@ -6,6 +6,8 @@
 
 LIBGPG_ERROR_VERSION = 1.10
 LIBGPG_ERROR_SITE = ftp://ftp.gnupg.org/gcrypt/libgpg-error
+LIBGPG_ERROR_LICENSE = LGPLv2.1+
+LIBGPG_ERROR_LICENSE_FILES = COPYING.LIB
 LIBGPG_ERROR_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
