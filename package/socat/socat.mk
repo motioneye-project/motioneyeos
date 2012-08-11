b27921a809a395ee21ba88c7e87de2133ffc0ac6
Danomi Manchego <danomimanchego123@gmail.com>
2012-08-11 16:13:24 -0400

    socat: add license info
    Signed-off-by: Danomi Manchego <danomimanchego123@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/socat/socat.mk
+++ package/socat/socat.mk
@@ -7,6 +7,8 @@
 SOCAT_VERSION = 2.0.0-b5
 SOCAT_SOURCE = socat-$(SOCAT_VERSION).tar.bz2
 SOCAT_SITE = http://www.dest-unreach.org/socat/download/
+SOCAT_LICENSE = GPLv2
+SOCAT_LICENSE_FILES = COPYING
 SOCAT_CONF_ENV = sc_cv_termios_ispeed=no \
 		 sc_cv_sys_crdly_shift=9 \
 		 sc_cv_sys_tabdly_shift=11 \
