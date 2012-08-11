f40f70d148f0897ae9d05558d91c6d10bf61f95d
Danomi Manchego <danomimanchego123@gmail.com>
2012-08-11 16:13:35 -0400

    spawn-fcgi: add license info
    Signed-off-by: Danomi Manchego <danomimanchego123@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/spawn-fcgi/spawn-fcgi.mk
+++ package/spawn-fcgi/spawn-fcgi.mk
@@ -7,6 +7,8 @@
 SPAWN_FCGI_VERSION = 1.6.3
 SPAWN_FCGI_SITE = http://www.lighttpd.net/download
 SPAWN_FCGI_SOURCE = spawn-fcgi-$(SPAWN_FCGI_VERSION).tar.bz2
+SPAWN_FCGI_LICENSE = BSD-3c
+SPAWN_FCGI_LICENSE_FILES = COPYING
 
 ifneq ($(BR2_INET_IPV6),y)
 SPAWN_FCGI_CONF_OPT = --disable-ipv6
