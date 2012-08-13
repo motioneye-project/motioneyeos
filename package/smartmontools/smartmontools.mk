47a3fa7b593adb77c2f9f1a4b1608ac2ce2af6d8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-08-13 17:02:38 -0300

    smartmontools: bump to version 5.43
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/smartmontools/smartmontools.mk
+++ package/smartmontools/smartmontools.mk
@@ -4,7 +4,9 @@
 #
 #############################################################
 
-SMARTMONTOOLS_VERSION = 5.42
+SMARTMONTOOLS_VERSION = 5.43
 SMARTMONTOOLS_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/smartmontools
+SMARTMONTOOLS_LICENSE = GPLv2+
+SMARTMONTOOLS_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
