2ecfaac73d26a3b73b64d0d0e33f7c11186f4d54
Danomi Manchego <danomimanchego123@gmail.com>
2012-08-19 16:18:25 -0400

    faad2: add license info
    Signed-off-by: Danomi Manchego <danomimanchego123@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/multimedia/faad2/faad2.mk
+++ package/multimedia/faad2/faad2.mk
@@ -6,6 +6,8 @@
 
 FAAD2_VERSION = 2.7
 FAAD2_SITE = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/faac
+FAAD2_LICENSE = GPLv2
+FAAD2_LICENSE_FILES = COPYING
 # frontend/faad calls frexp()
 FAAD2_CONF_ENV = LIBS=-lm
 FAAD2_INSTALL_STAGING = YES
