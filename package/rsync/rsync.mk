9ca544dec71a1e1f80e370c08deaf8937d250fa5
Danomi Manchego <danomimanchego123@gmail.com>
2012-08-19 16:18:29 -0400

    rsync: add license info
    Signed-off-by: Danomi Manchego <danomimanchego123@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/rsync/rsync.mk
+++ package/rsync/rsync.mk
@@ -7,6 +7,8 @@
 RSYNC_VERSION = 3.0.9
 RSYNC_SOURCE = rsync-$(RSYNC_VERSION).tar.gz
 RSYNC_SITE = http://rsync.samba.org/ftp/rsync/src
+RSYNC_LICENSE = GPLv3+
+RSYNC_LICENSE_FILES = COPYING
 RSYNC_CONF_OPT = $(if $(BR2_ENABLE_DEBUG),--enable-debug,--disable-debug)
 RSYNC_CONF_OPT = --with-included-popt
 
