2b595d6c923c197b8410470b663720e339682b70
Simon Dawson <simond@trainfx.com>
2012-08-28 02:07:43 +0000

    zeromq: add license information
    Note that the COPYING.LESSER file states that "Parts of the
    software are licensed under the MIT (X11) license".
    On the 0MQ mailing list, Pieter Hintjens has confirmed that
    the code no longer contains any parts that are under the
    MIT/X11 license. (The COPYING.LESSER file will be fixed in
    a future release.)
    
    Signed-off-by: Simon Dawson <simond@trainfx.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/zeromq/zeromq.mk
+++ package/zeromq/zeromq.mk
@@ -8,5 +8,7 @@ ZEROMQ_VERSION = 2.1.11
 ZEROMQ_SITE = http://download.zeromq.org/
 ZEROMQ_INSTALL_STAGING = YES
 ZEROMQ_DEPENDENCIES = util-linux
+ZEROMQ_LICENSE = LGPLv3+ with exceptions
+ZEROMQ_LICENSE_FILES = COPYING COPYING.LESSER
 
 $(eval $(autotools-package))
