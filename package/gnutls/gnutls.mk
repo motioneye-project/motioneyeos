b3578cb0e4b0b2bbad781a88bcb41c2ad0fb82a1
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-09-07 04:48:52 +0000

    gnutls: bump to 2.12.20
    [Peter: License is GPLv3+ LGPLv2.1+]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gnutls/gnutls.mk
+++ package/gnutls/gnutls.mk
@@ -4,9 +4,11 @@
 #
 #############################################################
 
-GNUTLS_VERSION = 2.12.19
+GNUTLS_VERSION = 2.12.20
 GNUTLS_SOURCE = gnutls-$(GNUTLS_VERSION).tar.bz2
 GNUTLS_SITE = $(BR2_GNU_MIRROR)/gnutls
+GNUTLS_LICENSE = GPLv3+ LGPLv2.1+
+GNUTLS_LICENSE_FILES = COPYING lib/COPYING
 GNUTLS_DEPENDENCIES = host-pkg-config libgcrypt $(if $(BR2_PACKAGE_ZLIB),zlib)
 GNUTLS_CONF_OPT = --with-libgcrypt --without-libgcrypt-prefix \
 		--without-p11-kit
