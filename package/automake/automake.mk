fb07eb9eee6c6084e694711c0a924dcfd3661c52
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-09-07 04:48:50 +0000

    automake: bump to version 1.11.6
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/automake/automake.mk
+++ package/automake/automake.mk
@@ -4,8 +4,10 @@
 #
 #############################################################
 
-AUTOMAKE_VERSION = 1.11.5
+AUTOMAKE_VERSION = 1.11.6
 AUTOMAKE_SITE = $(BR2_GNU_MIRROR)/automake
+AUTOMAKE_LICENSE = GPLv2+
+AUTOMAKE_LICENSE_FILES = COPYING
 
 AUTOMAKE_DEPENDENCIES = host-autoconf autoconf microperl
 
