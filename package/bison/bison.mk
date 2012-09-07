826407fa55546057612ed9709eee735a1be37924
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-09-07 04:48:54 +0000

    bison: bump to version 2.6.2
    [Peter: License is GPLv3+]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/bison/bison.mk
+++ package/bison/bison.mk
@@ -4,11 +4,12 @@
 #
 #############################################################
 
-BISON_VERSION = 2.5.1
+BISON_VERSION = 2.6.2
 BISON_SITE = $(BR2_GNU_MIRROR)/bison
-
-BISON_CONF_ENV += ac_cv_path_M4=/usr/bin/m4
-BISON_DEPENDENCIES += m4
+BISON_LICENSE = GPLv3+
+BISON_LICENSE_FILES = COPYING
+BISON_CONF_ENV = ac_cv_path_M4=/usr/bin/m4
+BISON_DEPENDENCIES = m4
 
 define BISON_DISABLE_EXAMPLES
 	echo 'all install:' > $(@D)/examples/Makefile
