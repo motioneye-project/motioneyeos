b9e89b340e27daff98faa430082cbe1a871dfb6f
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-09 23:46:23 +0200

    sudo: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sudo/sudo.mk
+++ package/sudo/sudo.mk
@@ -4,8 +4,10 @@
 #
 #############################################################
 
-SUDO_VERSION = 1.8.5p2
+SUDO_VERSION = 1.8.6
 SUDO_SITE    = http://www.sudo.ws/sudo/dist
+SUDO_LICENSE = ICS BSD-3c
+SUDO_LICENSE_FILES = doc/LICENSE
 SUDO_CONF_OPT = \
 		--without-lecture \
 		--without-sendmail \
@@ -13,6 +15,13 @@ SUDO_CONF_OPT = \
 		--with-logging=syslog \
 		--without-interfaces \
 		--without-pam
+
+# mksigname/mksiglist needs to run on build host to generate source files
+define SUDO_BUILD_MKSIGNAME_MKSIGLIST_HOST
+	$(MAKE) $(HOST_CONFIGURE_OPTS) -C $(@D)/compat mksigname mksiglist
+endef
+
+SUDO_POST_CONFIGURE_HOOKS += SUDO_BUILD_MKSIGNAME_MKSIGLIST_HOST
 
 define SUDO_PERMISSIONS
 /usr/bin/sudo			 f 4755	0 0 - - - - -
