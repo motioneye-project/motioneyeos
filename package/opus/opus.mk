c226de26ec1379a88f7fe0e278ceb5888b92a054
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-11 22:45:08 +0200

    package: add opus audio codec library
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/opus/opus.mk
@@ -0,0 +1,16 @@
+#############################################################
+#
+# opus
+#
+#############################################################
+
+OPUS_VERSION = 1.0.1
+OPUS_SITE = http://downloads.xiph.org/releases/opus
+OPUS_INSTALL_STAGING = YES
+OPUS_CONF_OPT = --disable-doc
+
+ifeq ($(BR2_SOFT_FLOAT),y)
+OPUS_CONF_OPT += --enable-fixed-point
+endif
+
+$(eval $(autotools-package))
