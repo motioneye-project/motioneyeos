3b9e61f9234165db1f70021396c8d59c2d52d3ec
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-11 09:13:11 +0200

    lcdproc: fix build with high BR2_JLEVEL setting
    Fixes http://autobuild.buildroot.net/results/646db4b6b284beb4392759a57efffc8d10ee9f49
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/lcdproc/lcdproc.mk
+++ package/lcdproc/lcdproc.mk
@@ -8,6 +8,7 @@ LCDPROC_SOURCE = lcdproc-$(LCDPROC_VERSION).tar.gz
 LCDPROC_SITE = http://downloads.sourceforge.net/project/lcdproc/lcdproc/$(LCDPROC_VERSION)
 LCDPROC_LICENSE = GPLv2+
 LCDPROC_LICENSE_FILES = COPYING
+LCDPROC_MAKE = $(MAKE1)
 
 LCDPROC_CONF_OPT = --enable-drivers=$(BR2_PACKAGE_LCDPROC_DRIVERS)
 
