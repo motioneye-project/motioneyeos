fee31757683e39dba28527215e9e7fa6150c52cd
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-09-19 10:28:10 +0000

    leafpad: needs host-intltool
    Fixes:
    
      http://autobuild.buildroot.org/results/d313a67b3deca262844d1043eafa5f6c5b6b7cad/build-end.log
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/leafpad/leafpad.mk
+++ package/leafpad/leafpad.mk
@@ -6,7 +6,7 @@
 LEAFPAD_VERSION = 0.8.18
 LEAFPAD_SOURCE = leafpad-$(LEAFPAD_VERSION).tar.gz
 LEAFPAD_SITE = http://savannah.nongnu.org/download/leafpad
-LEAFPAD_DEPENDENCIES = libgtk2
+LEAFPAD_DEPENDENCIES = libgtk2 host-intltool
 
 $(eval $(autotools-package))
 
