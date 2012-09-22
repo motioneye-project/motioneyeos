eadc7d87c191a9e7b5769ea7aa4f5e8edbd49fb3
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 21:22:20 +0200

    xfont_font-bitstream-75dpi: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xfont_font-bitstream-75dpi/xfont_font-bitstream-75dpi.mk
+++ package/x11r7/xfont_font-bitstream-75dpi/xfont_font-bitstream-75dpi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XFONT_FONT_BITSTREAM_75DPI_VERSION = 1.0.1
+XFONT_FONT_BITSTREAM_75DPI_VERSION = 1.0.3
 XFONT_FONT_BITSTREAM_75DPI_SOURCE = font-bitstream-75dpi-$(XFONT_FONT_BITSTREAM_75DPI_VERSION).tar.bz2
 XFONT_FONT_BITSTREAM_75DPI_SITE = http://xorg.freedesktop.org/releases/individual/font
 XFONT_FONT_BITSTREAM_75DPI_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) MKFONTSCALE=$(HOST_DIR)/usr/bin/mkfontscale MKFONTDIR=$(HOST_DIR)/usr/bin/mkfontdir install
