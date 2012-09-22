4fecf12ab0388cfd3a5370871b3ac5ce169b2792
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 20:58:23 +0200

    xapp_xhost: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xhost/xapp_xhost.mk
+++ package/x11r7/xapp_xhost/xapp_xhost.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XHOST_VERSION = 1.0.3
+XAPP_XHOST_VERSION = 1.0.4
 XAPP_XHOST_SOURCE = xhost-$(XAPP_XHOST_VERSION).tar.bz2
 XAPP_XHOST_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XHOST_DEPENDENCIES = xlib_libX11 xlib_libXmu
