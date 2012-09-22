dd02eb936604f28612c3844fc598dc554ade1f96
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 21:11:09 +0200

    xapp_xvinfo: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xvinfo/xapp_xvinfo.mk
+++ package/x11r7/xapp_xvinfo/xapp_xvinfo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XVINFO_VERSION = 1.1.0
+XAPP_XVINFO_VERSION = 1.1.1
 XAPP_XVINFO_SOURCE = xvinfo-$(XAPP_XVINFO_VERSION).tar.bz2
 XAPP_XVINFO_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XVINFO_DEPENDENCIES = xlib_libX11 xlib_libXv
