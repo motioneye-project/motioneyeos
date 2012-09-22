652620c54da89406b70abdb09b543e1a896d7f5f
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 15:41:32 +0200

    xdriver_x86-input-keyboard: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xdriver_xf86-input-keyboard/xdriver_xf86-input-keyboard.mk
+++ package/x11r7/xdriver_xf86-input-keyboard/xdriver_xf86-input-keyboard.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_INPUT_KEYBOARD_VERSION = 1.4.0
+XDRIVER_XF86_INPUT_KEYBOARD_VERSION = 1.5.0
 XDRIVER_XF86_INPUT_KEYBOARD_SOURCE = xf86-input-keyboard-$(XDRIVER_XF86_INPUT_KEYBOARD_VERSION).tar.bz2
 XDRIVER_XF86_INPUT_KEYBOARD_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_INPUT_KEYBOARD_DEPENDENCIES = xserver_xorg-server xproto_inputproto xproto_kbproto xproto_randrproto xproto_xproto
