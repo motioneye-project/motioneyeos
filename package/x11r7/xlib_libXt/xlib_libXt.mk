bfc29d721a890c4ee05eac73b4b2737bddfd260f
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 14:06:16 +0200

    xlib_libXt: bump version
    Based on patch by Allan W. Nielsen.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xlib_libXt/xlib_libXt.mk
+++ package/x11r7/xlib_libXt/xlib_libXt.mk
@@ -4,10 +4,9 @@
 #
 ################################################################################
 
-XLIB_LIBXT_VERSION = 1.0.7
+XLIB_LIBXT_VERSION = 1.0.9
 XLIB_LIBXT_SOURCE = libXt-$(XLIB_LIBXT_VERSION).tar.bz2
 XLIB_LIBXT_SITE = http://xorg.freedesktop.org/releases/individual/lib
-XLIB_LIBXT_AUTORECONF = YES
 XLIB_LIBXT_INSTALL_STAGING = YES
 XLIB_LIBXT_DEPENDENCIES = xlib_libSM xlib_libX11 xproto_kbproto xproto_xproto xcb-proto libxcb host-xproto_xproto
 XLIB_LIBXT_CONF_OPT = --disable-malloc0returnsnull --disable-install-makestrs
