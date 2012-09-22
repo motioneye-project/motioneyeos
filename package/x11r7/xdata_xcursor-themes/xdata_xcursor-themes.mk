d52664bc93c54e3586a287057f3654cf99c6215b
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 21:13:14 +0200

    xdata_xcursor-themes: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xdata_xcursor-themes/xdata_xcursor-themes.mk
+++ package/x11r7/xdata_xcursor-themes/xdata_xcursor-themes.mk
@@ -3,7 +3,8 @@
 # xdata_xcursor-themes - No description available
 #
 #############################################################
-XDATA_XCURSOR_THEMES_VERSION = 1.0.2
+
+XDATA_XCURSOR_THEMES_VERSION = 1.0.3
 XDATA_XCURSOR_THEMES_SOURCE = xcursor-themes-$(XDATA_XCURSOR_THEMES_VERSION).tar.bz2
 XDATA_XCURSOR_THEMES_SITE = http://xorg.freedesktop.org/releases/individual/data
 XDATA_XCURSOR_THEMES_INSTALL_STAGING = YES
