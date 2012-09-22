4db3d59b264dde31c81009d85316da9bbc34fcf1
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 21:10:45 +0200

    xapp_xsetroot: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xsetroot/xapp_xsetroot.mk
+++ package/x11r7/xapp_xsetroot/xapp_xsetroot.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XSETROOT_VERSION = 1.0.3
+XAPP_XSETROOT_VERSION = 1.1.0
 XAPP_XSETROOT_SOURCE = xsetroot-$(XAPP_XSETROOT_VERSION).tar.bz2
 XAPP_XSETROOT_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XSETROOT_DEPENDENCIES = xlib_libX11 xlib_libXmu xdata_xbitmaps
