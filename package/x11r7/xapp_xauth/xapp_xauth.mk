d97fe429f22b27200b8e16f5de85b785d1f4678e
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 20:54:33 +0200

    xapp_xauth: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xauth/xapp_xauth.mk
+++ package/x11r7/xapp_xauth/xapp_xauth.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XAUTH_VERSION = 1.0.4
+XAPP_XAUTH_VERSION = 1.0.5
 XAPP_XAUTH_SOURCE = xauth-$(XAPP_XAUTH_VERSION).tar.bz2
 XAPP_XAUTH_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XAUTH_DEPENDENCIES = xlib_libX11 xlib_libXau xlib_libXext xlib_libXmu
