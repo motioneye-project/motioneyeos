84893444bb7d20fc09c53f6ed53b296135ea043d
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 21:06:18 +0200

    xapp_xprop: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xprop/xapp_xprop.mk
+++ package/x11r7/xapp_xprop/xapp_xprop.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XPROP_VERSION = 1.1.0
+XAPP_XPROP_VERSION = 1.2.0
 XAPP_XPROP_SOURCE = xprop-$(XAPP_XPROP_VERSION).tar.bz2
 XAPP_XPROP_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XPROP_DEPENDENCIES = xlib_libX11 xlib_libXmu
