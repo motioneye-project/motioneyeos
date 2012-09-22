579f7b124726cf386c66cb2a5e630a8c651e2f60
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 21:23:29 +0200

    xfont_font-cursor-misc: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xfont_font-cursor-misc/xfont_font-cursor-misc.mk
+++ package/x11r7/xfont_font-cursor-misc/xfont_font-cursor-misc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XFONT_FONT_CURSOR_MISC_VERSION = 1.0.1
+XFONT_FONT_CURSOR_MISC_VERSION = 1.0.3
 XFONT_FONT_CURSOR_MISC_SOURCE = font-cursor-misc-$(XFONT_FONT_CURSOR_MISC_VERSION).tar.bz2
 XFONT_FONT_CURSOR_MISC_SITE = http://xorg.freedesktop.org/releases/individual/font
 XFONT_FONT_CURSOR_MISC_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) MKFONTSCALE=$(HOST_DIR)/usr/bin/mkfontscale MKFONTDIR=$(HOST_DIR)/usr/bin/mkfontdir install
