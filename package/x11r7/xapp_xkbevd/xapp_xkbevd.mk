5b58ef18998203b93bf50f3ad3100c207780d718
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 21:03:23 +0200

    xapp_xkbevd: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xkbevd/xapp_xkbevd.mk
+++ package/x11r7/xapp_xkbevd/xapp_xkbevd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XKBEVD_VERSION = 1.1.0
+XAPP_XKBEVD_VERSION = 1.1.2
 XAPP_XKBEVD_SOURCE = xkbevd-$(XAPP_XKBEVD_VERSION).tar.bz2
 XAPP_XKBEVD_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XKBEVD_DEPENDENCIES = xlib_libxkbfile
