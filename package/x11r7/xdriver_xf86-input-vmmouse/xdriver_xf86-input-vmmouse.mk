229a4de32eb5e5af21581055ef8c0f9cd7d24b97
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 15:42:47 +0200

    xdriver_x86-input-vmmouse: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xdriver_xf86-input-vmmouse/xdriver_xf86-input-vmmouse.mk
+++ package/x11r7/xdriver_xf86-input-vmmouse/xdriver_xf86-input-vmmouse.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_INPUT_VMMOUSE_VERSION = 12.6.5
+XDRIVER_XF86_INPUT_VMMOUSE_VERSION = 12.6.10
 XDRIVER_XF86_INPUT_VMMOUSE_SOURCE = xf86-input-vmmouse-$(XDRIVER_XF86_INPUT_VMMOUSE_VERSION).tar.bz2
 XDRIVER_XF86_INPUT_VMMOUSE_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_INPUT_VMMOUSE_DEPENDENCIES = xserver_xorg-server xproto_inputproto xproto_randrproto xproto_xproto
