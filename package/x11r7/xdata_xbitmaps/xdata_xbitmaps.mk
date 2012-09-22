5e4dc1a08fbda69d1b8335d7843df74e80016b61
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 21:12:40 +0200

    xdata_xbitmaps: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xdata_xbitmaps/xdata_xbitmaps.mk
+++ package/x11r7/xdata_xbitmaps/xdata_xbitmaps.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDATA_XBITMAPS_VERSION = 1.1.0
+XDATA_XBITMAPS_VERSION = 1.1.1
 XDATA_XBITMAPS_SOURCE = xbitmaps-$(XDATA_XBITMAPS_VERSION).tar.bz2
 XDATA_XBITMAPS_SITE = http://xorg.freedesktop.org/releases/individual/data
 XDATA_XBITMAPS_INSTALL_STAGING = YES
