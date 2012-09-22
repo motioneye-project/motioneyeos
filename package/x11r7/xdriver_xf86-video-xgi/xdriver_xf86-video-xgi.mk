8c647d14fd71fb526fce0382f9af69c150626851
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 16:55:50 +0200

    xdriver_x86-driver-xgi: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xdriver_xf86-video-xgi/xdriver_xf86-video-xgi.mk
+++ package/x11r7/xdriver_xf86-video-xgi/xdriver_xf86-video-xgi.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_XGI_VERSION = 1.5.1
+XDRIVER_XF86_VIDEO_XGI_VERSION = 1.6.0
 XDRIVER_XF86_VIDEO_XGI_SOURCE = xf86-video-xgi-$(XDRIVER_XF86_VIDEO_XGI_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_XGI_SITE = http://xorg.freedesktop.org/releases/individual/driver
-XDRIVER_XF86_VIDEO_XGI_AUTORECONF = YES
 XDRIVER_XF86_VIDEO_XGI_DEPENDENCIES = xserver_xorg-server xproto_fontsproto xproto_randrproto xproto_renderproto xproto_xextproto xproto_xproto
+XDRIVER_XF86_VIDEO_XGI_CONF_OPT = --enable-dri
 
 $(eval $(autotools-package))
