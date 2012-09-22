98fda5ba568d03380eba5a52374b2166f201a60f
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 21:04:55 +0200

    xapp_xlsatoms: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xlsatoms/xapp_xlsatoms.mk
+++ package/x11r7/xapp_xlsatoms/xapp_xlsatoms.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XLSATOMS_VERSION = 1.0.2
+XAPP_XLSATOMS_VERSION = 1.1.0
 XAPP_XLSATOMS_SOURCE = xlsatoms-$(XAPP_XLSATOMS_VERSION).tar.bz2
 XAPP_XLSATOMS_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XLSATOMS_DEPENDENCIES = xlib_libX11 xlib_libXmu
