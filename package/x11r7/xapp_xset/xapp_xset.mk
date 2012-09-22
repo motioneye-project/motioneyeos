7ed1bb4d59c88d3bb497a7c4998dbaca75919459
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 21:10:22 +0200

    xapp_xset: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xset/xapp_xset.mk
+++ package/x11r7/xapp_xset/xapp_xset.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XSET_VERSION = 1.1.0
+XAPP_XSET_VERSION = 1.2.1
 XAPP_XSET_SOURCE = xset-$(XAPP_XSET_VERSION).tar.bz2
 XAPP_XSET_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XSET_DEPENDENCIES = xlib_libXfontcache xlib_libXmu
