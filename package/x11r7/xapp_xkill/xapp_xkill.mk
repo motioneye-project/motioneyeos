f78da79741086b8e815ef3d9b093e1a2a08731aa
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-22 21:04:31 +0200

    xapp_xkill: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xkill/xapp_xkill.mk
+++ package/x11r7/xapp_xkill/xapp_xkill.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XKILL_VERSION = 1.0.2
+XAPP_XKILL_VERSION = 1.0.3
 XAPP_XKILL_SOURCE = xkill-$(XAPP_XKILL_VERSION).tar.bz2
 XAPP_XKILL_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XKILL_DEPENDENCIES = xlib_libX11 xlib_libXmu
