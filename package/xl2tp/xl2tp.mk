31b45a8c72b365c09100a7cb99c82c8e63d1b479
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-09-23 02:51:00 +0000

    xl2tp: bump to version 1.3.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/xl2tp/xl2tp.mk
+++ package/xl2tp/xl2tp.mk
@@ -3,31 +3,18 @@
 # xl2tp
 #
 #############################################################
-XL2TP_VERSION = 1.2.7
-XL2TP_SOURCE = xl2tpd-$(XL2TP_VERSION).tar.gz
-XL2TP_SITE = ftp://ftp.xelerance.com/xl2tpd/
 
+XL2TP_VERSION = v1.3.1
+XL2TP_SITE = http://github.com/xelerance/xl2tpd/tarball/$(XL2TP_VERSION)
 XL2TP_DEPENDENCIES = libpcap
 
 define XL2TP_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+	$(SED) 's/ -O2 //' $(@D)/Makefile
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
 endef
 
 define XL2TP_INSTALL_TARGET_CMDS
 	$(MAKE) DESTDIR=$(TARGET_DIR) PREFIX=/usr -C $(@D) install
-endef
-
-define XL2TP_UNINSTALL_TARGET_CMDS
-	rm -f $(TARGET_DIR)/usr/bin/pfc
-	rm -f $(TARGET_DIR)/usr/sbin/xl2tpd
-	rm -f $(TARGET_DIR)/usr/share/man/man1/pfc.1
-	rm -f $(TARGET_DIR)/usr/share/man/man8/xl2tpd.8
-	rm -f $(TARGET_DIR)/usr/share/man/man5/xl2tpd.conf.5
-	rm -f $(TARGET_DIR)/usr/share/man/man5/l2tp-secrets.5
-endef
-
-define XL2TP_CLEAN_CMDS
-	-$(MAKE) -C $(@D) clean
 endef
 
 $(eval $(generic-package))
