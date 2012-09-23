90f908f21e4f7b1b7dd0c4c44515db67f441dcc3
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-23 21:27:13 +0200

    xlsclient: unbreak build
    Fixes http://autobuild.buildroot.net/results/4cb357a99de9e2c2940fbdfccb314aaef5891b6b
    
    xlsclient nowadays needs libxcb and xcb-util, and update to 1.1.1, as
    1.1.0 tarball was missing strnlen.h.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xlsclients/xapp_xlsclients.mk
+++ package/x11r7/xapp_xlsclients/xapp_xlsclients.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XLSCLIENTS_VERSION = 1.1.0
+XAPP_XLSCLIENTS_VERSION = 1.1.1
 XAPP_XLSCLIENTS_SOURCE = xlsclients-$(XAPP_XLSCLIENTS_VERSION).tar.bz2
 XAPP_XLSCLIENTS_SITE = http://xorg.freedesktop.org/releases/individual/app
-XAPP_XLSCLIENTS_DEPENDENCIES = xlib_libX11 xlib_libXmu
+XAPP_XLSCLIENTS_DEPENDENCIES = xlib_libX11 xlib_libXmu libxcb xcb-util
 
 $(eval $(autotools-package))
