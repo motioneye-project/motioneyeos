f0b4b5a06867fd1ac71fd8f32ac837245fbbcd03
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-23 13:13:32 +0200

    xapp_xkbcomp: preseed xkbparse.c check
    Fixes http://autobuild.buildroot.net/results/3fd3f1c7dfd17178c33d8c7f6f4b184019709967
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xkbcomp/xapp_xkbcomp.mk
+++ package/x11r7/xapp_xkbcomp/xapp_xkbcomp.mk
@@ -8,6 +8,7 @@ XAPP_XKBCOMP_VERSION = 1.2.0
 XAPP_XKBCOMP_SOURCE = xkbcomp-$(XAPP_XKBCOMP_VERSION).tar.bz2
 XAPP_XKBCOMP_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XKBCOMP_DEPENDENCIES = xlib_libX11 xlib_libxkbfile
+XAPP_XKBCOMP_CONF_ENV = ac_cv_file___xkbparse_c=yes
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
