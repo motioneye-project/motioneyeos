198f0c911105dab7e06a5ded574e09f1b4c02c71
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-25 20:25:17 +0200

    x11vnc: bump version to fix build issue with newer libX11
    Fixes http://autobuild.buildroot.net/results/2733c2c9fa7aacb355e0e0b184a5afe41cf35a3d
    
    Old x11vnc defined a 'pointer' function, which collides with the libX11
    pointer type. Fix it by bumping the version (upstream renamed it to
    pointer_event()).
    
    Fix the dependencies and help text while we're at it.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11vnc/x11vnc.mk
+++ package/x11vnc/x11vnc.mk
@@ -3,12 +3,12 @@
 # X11VNC
 #
 #############################################################
-X11VNC_VERSION = 0.9.3
+X11VNC_VERSION = 0.9.13
 X11VNC_SOURCE = x11vnc-$(X11VNC_VERSION).tar.gz
 X11VNC_SITE = http://downloads.sourceforge.net/project/libvncserver/x11vnc/$(X11VNC_VERSION)
 X11VNC_CONF_OPT = \
 	--without-avahi
 
-X11VNC_DEPENDENCIES = xserver_xorg-server xlib_libXt
+X11VNC_DEPENDENCIES = xlib_libXt xlib_libXext
 
 $(eval $(autotools-package))
