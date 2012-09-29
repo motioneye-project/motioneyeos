b86a86cfbbf72499b62e3b498001d97366bdb6d9
Peter Korsgaard <jacmet@sunsite.dk>
2012-09-29 20:40:39 +0200

    x11vnc: add libXtst as a dependency
    x11vnc CAN be built without XTEST support, but it isn't very useful as
    noted by the warning printed by configure when it isn't found:
    
    *** A working build environment for the XTEST extension was not found ***
    (libXtst).  An x11vnc built this way will be *ONLY BARELY USABLE*.
    You will be able to move the mouse but not click or type.  There can
    also be deadlocks if an application grabs the X server.
    
    It is recommended that you install the necessary development packages
    for XTEST (perhaps it is named something like libxtst-dev) and run
    configure again.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11vnc/x11vnc.mk
+++ package/x11vnc/x11vnc.mk
@@ -9,7 +9,7 @@ X11VNC_SITE = http://downloads.sourceforge.net/project/libvncserver/x11vnc/$(X11
 X11VNC_CONF_OPT = \
 	--without-avahi
 
-X11VNC_DEPENDENCIES = xlib_libXt xlib_libXext
+X11VNC_DEPENDENCIES = xlib_libXt xlib_libXext xlib_libXtst
 
 ifneq ($(BR2_INET_IPV6),y)
 # configure option only used for libvncserver
