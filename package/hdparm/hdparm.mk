f78b189a024f62eb4865a04094dc44e9cf3bac3b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-10-02 07:40:48 +0000

    hdparm: bump to version 9.42
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/hdparm/hdparm.mk
+++ package/hdparm/hdparm.mk
@@ -4,8 +4,10 @@
 #
 #############################################################
 
-HDPARM_VERSION = 9.39
+HDPARM_VERSION = 9.42
 HDPARM_SITE = http://downloads.sourceforge.net/project/hdparm/hdparm
+HDPARM_LICENSE = BSD-Style
+HDPARM_LICENSE_FILES = LICENSE.TXT
 
 define HDPARM_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) \
