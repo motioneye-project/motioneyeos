2af923069ed36868e0f100b63692ef0b999b58ea
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-10-02 14:40:49 -0300

    ipset: bump to version 6.14
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ipset/ipset.mk
+++ package/ipset/ipset.mk
@@ -4,10 +4,12 @@
 #
 #############################################################
 
-IPSET_VERSION = 6.13
+IPSET_VERSION = 6.14
 IPSET_SOURCE = ipset-$(IPSET_VERSION).tar.bz2
 IPSET_SITE = http://ipset.netfilter.org
 IPSET_DEPENDENCIES = libmnl host-pkg-config
 IPSET_CONF_OPT = --with-kmod=no
+IPSET_LICENSE = GPLv2
+IPSET_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
