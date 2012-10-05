0c5e24d48bdd63ce55cc8fb2b00f05b501876a60
Thiago Correa <thiago.correa@gmail.com>
2012-10-05 09:36:03 +0000

    python-protobuf: Add support for Python implementation of Google's Protocol Buffers
    Signed-off-by: Thiago A. Correa <thiago.correa@gmail.com>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/python-protobuf/python-protobuf.mk
@@ -0,0 +1,28 @@
+#############################################################
+#
+# python-protobuf
+#
+#############################################################
+
+PYTHON_PROTOBUF_VERSION = $(PROTOBUF_VERSION)
+PYTHON_PROTOBUF_SOURCE  = $(PROTOBUF_SOURCE)
+PYTHON_PROTOBUF_SITE    = $(PROTOBUF_SITE)
+PYTHON_PROTOBUF_LICENSE = BSD-3c
+PYTHON_PROTOBUF_LICENSE_FILES = COPYING.txt
+
+PYTHON_PROTOBUF_DEPENDENCIES = python host-python-setuptools \
+        host-python-distutilscross host-protobuf
+
+define PYTHON_PROTOBUF_BUILD_CMDS
+	(cd $(@D)/python; \
+		PYTHONXCPREFIX="$(STAGING_DIR)/usr/" \
+		PATH=$(HOST_PATH) \
+	$(HOST_DIR)/usr/bin/python setup.py build -x)
+endef
+
+define PYTHON_PROTOBUF_INSTALL_TARGET_CMDS
+	(cd $(@D)/python; PYTHONPATH=$(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages \
+	$(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
+endef
+
+$(eval $(generic-package))
