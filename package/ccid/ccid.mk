42f0a84efd2cf511f331114db9167d00283c1425
Waldemar Rymarkiewicz <waldemar.rymarkiewicz@tieto.com>
2012-10-07 19:05:13 +0000

    ccid: new package
    PC/SC driver for USB CCID smart card readers.
    
    http://pcsclite.alioth.debian.org/ccid.html
    
    Signed-off-by: Waldemar Rymarkiewicz <waldemar.rymarkiewicz@tieto.com>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Tested-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/ccid/ccid.mk
@@ -0,0 +1,29 @@
+##########################################################
+#
+# CCID
+#
+# ########################################################
+CCID_VERSION = 1.4.7
+CCID_SOURCE = ccid-$(CCID_VERSION).tar.bz2
+CCID_SITE = https://alioth.debian.org/frs/download.php/3730
+CCID_INSTALL_STAGING = YES
+CCID_DEPENDENCIES = pcsc-lite host-pkg-config libusb
+
+ifeq ($(BR2_ROOTFS_DEVICE_CREATION_DYNAMIC_UDEV),y)
+define CCID_INSTALL_UDEV_RULES
+	if test -d $(TARGET_DIR)/etc/udev/rules.d ; then \
+		cp $(@D)/src/92_pcscd_ccid.rules $(TARGET_DIR)/etc/udev/rules.d/ ; \
+	fi;
+endef
+
+CCID_POST_INSTALL_TARGET_HOOKS += CCID_INSTALL_UDEV_RULES
+endif
+
+define CCID_REMOVE_UDEV_RULES
+	if test -d $(TARGET_DIR)/etc/udev/rules.d ; then \
+		rm -f $(TARGET_DIR)/etc/udev/rules.d/92_pcscd_ccid.rules ; \
+	fi;
+endef
+CCID_POST_UNINSTALL_TARGET_HOOKS += CCID_REMOVE_UDEV_RULES
+
+$(eval $(autotools-package))
