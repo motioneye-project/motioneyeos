7d9dfd817012090bb2753c7a3dee94ff1d2e0027
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-10-08 12:01:05 +0000

    patch: bump to version 2.7.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/patch/patch.mk
+++ package/patch/patch.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-PATCH_VERSION = 2.7
+PATCH_VERSION = 2.7.1
 PATCH_SITE = $(BR2_GNU_MIRROR)/patch
 PATCH_LICENSE = GPLv3+
 PATCH_LICENSE_FILE = COPYING
