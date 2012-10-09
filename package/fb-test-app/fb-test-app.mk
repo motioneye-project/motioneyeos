bbfa50f3ead1f19e4d0bb9dcb7364d58225be139
Ludovic Desroches <ludovic.desroches@atmel.com>
2012-10-09 05:53:27 +0000

    fb-test-app: new package
    Test suite for Linux framebuffer.
    
    [Peter: add patch to build with our TARGET_CFLAGS]
    Signed-off-by: Ludovic Desroches <ludovic.desroches@atmel.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/fb-test-app/fb-test-app.mk
@@ -0,0 +1,34 @@
+#############################################################
+#
+# fb-test-app
+#
+#############################################################
+
+FB_TEST_APP_VERSION = v1.0.0
+FB_TEST_APP_SITE = http://github.com/prpplague/fb-test-app/tarball/$(FB_TEST_APP_VERSION)
+FB_TEST_APP_LICENSE = GPLv2
+FB_TEST_APP_LICENSE_FILE = COPYING
+
+define FB_TEST_APP_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
+
+define FB_TEST_APP_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/perf $(TARGET_DIR)/usr/bin/fb-test-perf
+	$(INSTALL) -D -m 0755 $(@D)/rect $(TARGET_DIR)/usr/bin/fb-test-rect
+	$(INSTALL) -D -m 0755 $(@D)/fb-test $(TARGET_DIR)/usr/bin/fb-test
+	$(INSTALL) -D -m 0755 $(@D)/offset $(TARGET_DIR)/usr/bin/fb-test-offset
+endef
+
+define FB_TEST_APP_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/bin/fb-test-perf
+	rm -f $(TARGET_DIR)/usr/bin/fb-test-rect
+	rm -f $(TARGET_DIR)/usr/bin/fb-test
+	rm -f $(TARGET_DIR)/usr/bin/fb-test-offset
+endef
+
+define FB_TEST_APP_CLEAN_CMDS
+	$(MAKE) -C $(@D) clean
+endef
+
+$(eval $(generic-package))
