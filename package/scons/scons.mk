b08979477d65203b9b90c88d2a00246342c0b56a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-10-09 12:25:56 +0000

    scons: fix library installation directory
    On gentoo systems SCons setup.py tries to install into
    $(HOST_DIR)/usr/lib64 but SCons looks up for its stuff in
    $(HOST_DIR)/usr/lib instead hence breaking.
    Make it install into $(HOST_DIR)/usr/lib by force to avoid the issue, it
    shouldn't matter on other distributions.
    Version bumping doesn't fix it.
    
    [Peter: wrapped long line]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/scons/scons.mk
+++ package/scons/scons.mk
@@ -7,7 +7,8 @@ define HOST_SCONS_BUILD_CMDS
 endef
 
 define HOST_SCONS_INSTALL_CMDS
-	(cd $(@D); python setup.py install --prefix=$(HOST_DIR)/usr)
+	(cd $(@D); python setup.py install --prefix=$(HOST_DIR)/usr \
+		--install-lib=$(HOST_DIR)/usr/lib/scons-$(SCONS_VERSION))
 endef
 
 $(eval $(host-generic-package))
