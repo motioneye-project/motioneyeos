6d4cdb081020bafa7062c6a8cf052b3d5000635c
Alexander Varnin <fenixk19@mail.ru>
2012-10-10 23:34:26 +0000

    New package liblog4c-localtime
    [Peter: minor fixes]
    Signed-off-by: Alexander Varnin <fenixk19@mail.ru>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/liblog4c-localtime/liblog4c-localtime.mk
@@ -0,0 +1,18 @@
+ #############################################################
+ #
+ # liblog4c
+ #
+ #############################################################
+ LIBLOG4C_LOCALTIME_VERSION = 1.0
+ LIBLOG4C_LOCALTIME_SITE    = https://github.com/rcmadruga/log4c-localtime/tarball/v$(LIBLOG4C_LOCALTIME_VERSION)
+ LIBLOG4C_LOCALTIME_INSTALL_STAGING = YES
+ LIBLOG4C_LOCALTIME_CONF_OPT = --disable-expattest
+ LIBLOG4C_LOCALTIME_DEPENDENCIES = expat
+
+define LIBLOG4C_LOCALTIME_FIX_CONFIGURE_PERMS
+	chmod +x $(@D)/configure
+endef
+
+LIBLOG4C_LOCALTIME_PRE_CONFIGURE_HOOKS += LIBLOG4C_LOCALTIME_FIX_CONFIGURE_PERMS
+
+ $(eval $(autotools-package))
