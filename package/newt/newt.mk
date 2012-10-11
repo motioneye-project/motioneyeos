a34896a8d7ee175d90b8cce244d4cfa11863292a
Samuel Martin <s.martin49@gmail.com>
2012-10-11 09:46:39 +0000

    newt: convert to autotools-package infra
    [Peter: drop uneeded configure args, full install to target]
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/newt/newt.mk
+++ package/newt/newt.mk
@@ -3,80 +3,32 @@
 # newt
 #
 #############################################################
-NEWT_SOURCE=newt-0.51.0.tar.bz2
-NEWT_CAT:=$(BZCAT)
-NEWT_SITE=http://www.uclibc.org/
-NEWT_DIR=$(BUILD_DIR)/newt-0.51.0
-NEWT_VERSION=0.51.0
-NEWT_CFLAGS=-Os -g -fPIC
+NEWT_VERSION         = 0.51.0
+NEWT_SITE            = http://www.uclibc.org/
+NEWT_SOURCE          = newt-$(NEWT_VERSION).tar.bz2
+NEWT_LICENSE         = GPLv2
+NEWT_LICENSE_FILES   = COPYING
+NEWT_INSTALL_STAGING = YES
 
-$(DL_DIR)/$(NEWT_SOURCE):
-	$(call DOWNLOAD,$(NEWT_SITE)/$(NEWT_SOURCE))
+NEWT_DEPENDENCIES = slang
 
-$(NEWT_DIR)/.source: $(DL_DIR)/$(NEWT_SOURCE)
-	$(NEWT_CAT) $(DL_DIR)/$(NEWT_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	touch $(NEWT_DIR)/.source
+NEWT_MAKE_ENV += $(TARGET_CONFIGURE_OPTS)
+NEWT_MAKE = $(MAKE1)
 
-$(NEWT_DIR)/.configured: $(NEWT_DIR)/.source
-	(cd $(NEWT_DIR); rm -rf config.cache; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		CFLAGS="$(TARGET_CFLAGS) $(NEWT_CFLAGS)" \
-		./configure $(QUIET) \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--prefix=/usr \
-		--exec-prefix=/usr \
-		--bindir=/usr/bin \
-		--sbindir=/usr/sbin \
-		--libdir=/lib \
-		--libexecdir=/usr/lib \
-		--sysconfdir=/etc \
-		--datadir=/usr/share \
-		--localstatedir=/var \
-		--mandir=/usr/man \
-		--infodir=/usr/info \
-		$(DISABLE_NLS) \
-	)
-	touch $(NEWT_DIR)/.configured
+define NEWT_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m644 $(@D)/newt.h    $(STAGING_DIR)/usr/include/newt.h
+	$(INSTALL) -D -m644 $(@D)/libnewt.a $(STAGING_DIR)/usr/lib/libnewt.a
+	$(INSTALL) -m755 $(@D)/libnewt.so*  $(STAGING_DIR)/usr/lib/
+	ln -fs libnewt.so.$(NEWT_VERSION)   $(STAGING_DIR)/usr/lib/libnewt.so
+	ln -fs libnewt.so.$(NEWT_VERSION)   $(STAGING_DIR)/usr/lib/libnewt.so.0.51
+endef
 
-$(NEWT_DIR)/libnewt.so.$(NEWT_VERSION): $(NEWT_DIR)/.configured
-	$(MAKE1) CFLAGS="$(TARGET_CFLAGS) $(NEWT_CFLAGS)" CC="$(TARGET_CC)" -C $(NEWT_DIR)
-	touch -c $@
+define NEWT_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m644 $(@D)/newt.h    $(TARGET_DIR)/usr/include/newt.h
+	$(INSTALL) -D -m644 $(@D)/libnewt.a $(TARGET_DIR)/usr/lib/libnewt.a
+	$(INSTALL) -m755 $(@D)/libnewt.so*  $(TARGET_DIR)/usr/lib/
+	ln -fs libnewt.so.$(NEWT_VERSION)   $(TARGET_DIR)/usr/lib/libnewt.so
+	ln -fs libnewt.so.$(NEWT_VERSION)   $(TARGET_DIR)/usr/lib/libnewt.so.0.51
+endef
 
-$(STAGING_DIR)/usr/lib/libnewt.a: $(NEWT_DIR)/libnewt.so.$(NEWT_VERSION)
-	cp -a $(NEWT_DIR)/libnewt.a $(STAGING_DIR)/usr/lib/
-	cp -a $(NEWT_DIR)/newt.h $(STAGING_DIR)/usr/include/
-	cp -a $(NEWT_DIR)/libnewt.so* $(STAGING_DIR)/usr/lib/
-	(cd $(STAGING_DIR)/usr/lib; ln -fs libnewt.so.$(NEWT_VERSION) libnewt.so)
-	(cd $(STAGING_DIR)/usr/lib; ln -fs libnewt.so.$(NEWT_VERSION) libnewt.so.0.51)
-	touch -c $@
-
-$(TARGET_DIR)/usr/lib/libnewt.so.$(NEWT_VERSION): $(STAGING_DIR)/usr/lib/libnewt.a
-	cp -a $(STAGING_DIR)/usr/lib/libnewt.so* $(TARGET_DIR)/usr/lib/
-	-$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libnewt.so*
-	touch -c $@
-
-newt-legal-info:
-	@$(call legal-warning-pkg,newt,legal-info not yet implemented)
-
-newt: slang $(TARGET_DIR)/usr/lib/libnewt.so.$(NEWT_VERSION)
-
-newt-source: $(DL_DIR)/$(NEWT_SOURCE)
-
-newt-clean:
-	rm -f $(TARGET_DIR)/usr/lib/libnewt.so*
-	-$(MAKE) -C $(NEWT_DIR) clean
-
-newt-dirclean: slang-dirclean
-	rm -rf $(NEWT_DIR)
-
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_NEWT),y)
-TARGETS+=newt
-endif
+$(eval $(autotools-package))
