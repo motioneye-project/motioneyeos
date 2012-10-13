ee1dd812881cc7928e11addaa18e8185d2b02000
Arnout Vandecappelle <arnout@mind.be>
2012-10-13 01:46:09 +0000

    unionfs: convert to cmake-package
    Fixes e.g. http://autobuild.buildroot.net/results/c279007dd8b89a1c6fc9b58d3c2cda01ab84279e
    
    The addition of CPPFLAGS overrides the defines added in unionfs's
    Makefile.  Since unionfs now has CMake support, use that instead.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/unionfs/unionfs.mk
+++ package/unionfs/unionfs.mk
@@ -11,12 +11,4 @@ UNIONFS_DEPENDENCIES = libfuse host-pkg-config
 UNIONFS_LICENSE = BSD-3c
 UNIONFS_LICENSE_FILES = LICENSE
 
-define UNIONFS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
-endef
-
-define UNIONFS_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(@D) PREFIX=/usr DESTDIR=$(TARGET_DIR) install
-endef
-
-$(eval $(generic-package))
+$(eval $(cmake-package))
