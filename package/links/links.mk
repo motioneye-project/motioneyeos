3c592cb4dffc1a0f18d2106e15ebfb7c3d316fd5
Valentine Barshak <gvaxon@gmail.com>
2012-10-15 06:44:58 +0000

    links: Enable graphics for X11
    Graphics can be enabled for X11 as well.
    Do not select DirectFB for graphics,
    set DirectFB or X11 dependencies instead.
    
    Signed-off-by: Valentine Barshak <gvaxon@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/links/links.mk
+++ package/links/links.mk
@@ -6,13 +6,24 @@
 
 LINKS_VERSION = 2.5
 LINKS_SITE = http://links.twibright.com/download
-LINKS_CONF_OPT = --without-x
 LINKS_DEPENDENCIES = host-pkgconf
 
 ifeq ($(BR2_PACKAGE_LINKS_GRAPHICS),y)
 LINKS_CONF_OPT += --enable-graphics
+LINKS_DEPENDENCIES += libpng
+ifeq ($(BR2_PACKAGE_XLIB_LIBXT),y)
+LINKS_CONF_OPT += --with-x
+LINKS_DEPENDENCIES += xlib_libXt
+else
+LINKS_CONF_OPT += --without-x
+endif
+ifeq ($(BR2_PACKAGE_DIRECTFB),y)
 LINKS_CONF_ENV = ac_cv_path_DIRECTFB_CONFIG=$(STAGING_DIR)/usr/bin/directfb-config
-LINKS_DEPENDENCIES += directfb libpng
+LINKS_CONF_OPT += --with-directfb
+LINKS_DEPENDENCIES += directfb
+else
+LINKS_CONF_OPT += --without-directfb
+endif
 ifeq ($(BR2_PACKAGE_JPEG),y)
 LINKS_DEPENDENCIES += jpeg
 endif
