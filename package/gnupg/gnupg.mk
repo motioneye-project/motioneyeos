260a5820180760d4b00e530041d98eab4bab60fc
Peter Korsgaard <jacmet@sunsite.dk>
2012-10-15 09:19:55 +0200

    package: add gnupg
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/gnupg/gnupg.mk
@@ -0,0 +1,48 @@
+#############################################################
+#
+# gnupg
+#
+#############################################################
+
+GNUPG_VERSION = 1.4.12
+GNUPG_SOURCE = gnupg-$(GNUPG_VERSION).tar.bz2
+GNUPG_SITE = ftp://ftp.gnupg.org/gcrypt/gnupg
+GNUPG_LICENSE = GPLv3+
+GNUPG_LICENSE_FILES = COPYING
+GNUPG_DEPENDENCIES = zlib ncurses
+GNUPG_CONF_OPT = --disable-rpath --enable-minimal --disable-regex
+
+ifeq ($(BR2_PACKAGE_BZIP2),y)
+GNUPG_CONF_OPT += --enable-bzip2
+GNUPG_DEPENDENCIES += bzip2
+endif
+
+ifeq ($(BR2_PACKAGE_LIBCURL),y)
+GNUPG_CONF_ENV += ac_cv_path__libcurl_config=$(STAGING_DIR)/usr/bin/curl-config
+GNUPG_DEPENDENCIES += libcurl
+else
+GNUPG_CONF_OPT += --without-libcurl
+endif
+
+ifeq ($(BR2_PACKAGE_READLINE),y)
+GNUPG_DEPENDENCIES += readline
+else
+GNUPG_CONF_OPT += --without-readline
+endif
+
+ifneq ($(BR2_PACKAGE_GNUPG_GPGV),y)
+define GNUPG_REMOVE_GPGV
+	rm -f $(TARGET_DIR)/usr/bin/gpgv \
+		$(TARGET_DIR)/usr/share/man/man1/gpgv.1
+endef
+GNUPG_POST_INSTALL_TARGET_HOOKS += GNUPG_REMOVE_GPGV
+endif
+
+ifneq ($(BR2_PACKAGE_GNUPG_GPGSPLIT),y)
+define GNUPG_REMOVE_GPGSPLIT
+	rm -f $(TARGET_DIR)/usr/bin/gpgsplit
+endef
+GNUPG_POST_INSTALL_TARGET_HOOKS += GNUPG_REMOVE_GPGSPLIT
+endif
+
+$(eval $(autotools-package))
