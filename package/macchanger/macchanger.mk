464e88c8bd90e52fa128dae2ce20a0edd9a9f0e9
Simon Dawson <spdawson@gmail.com>
2012-10-16 09:30:14 +0000

    macchanger: new package
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Tested-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
      (build-test with a WCHAR-only internal powerpc toolchain)
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/macchanger/macchanger.mk
@@ -0,0 +1,11 @@
+#############################################################
+#
+# macchanger
+#
+#############################################################
+MACCHANGER_VERSION = 1.5.0
+MACCHANGER_SITE = $(BR2_GNU_MIRROR)/macchanger
+MACCHANGER_LICENSE = GPLv2
+MACCHANGER_LICENSE_FILES = COPYING
+
+$(eval $(autotools-package))
