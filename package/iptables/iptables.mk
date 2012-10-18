dd81b498378b60b380d75b63f58418b2465731d5
Peter Korsgaard <jacmet@sunsite.dk>
2012-10-18 14:43:13 +0200

    iptables: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/iptables/iptables.mk
+++ package/iptables/iptables.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-IPTABLES_VERSION = 1.4.15
+IPTABLES_VERSION = 1.4.16.3
 IPTABLES_SOURCE = iptables-$(IPTABLES_VERSION).tar.bz2
 IPTABLES_SITE = http://ftp.netfilter.org/pub/iptables
 IPTABLES_INSTALL_STAGING = YES
