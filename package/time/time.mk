2073c21ec041b6404b19e17e0f139ddbc639a35f
Will Newton <will.newton@gmail.com>
2012-10-19 03:05:17 +0000

    package/time: New package for GNU time.
    Signed-off-by: Will Newton <will.newton@imgtec.com>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Tested-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/time/time.mk
@@ -0,0 +1,17 @@
+#############################################################
+#
+# time
+#
+#############################################################
+
+TIME_VERSION = 1.7
+TIME_SITE = $(BR2_GNU_MIRROR)/time
+TIME_CONF_ENV = ac_cv_func_wait3=yes
+
+# time uses an old version of automake that does not support
+# installing in DESTDIR.
+define TIME_INSTALL_TARGET_CMDS
+	install -D -m 755  $(@D)/time $(TARGET_DIR)/usr/bin/time
+endef
+
+$(eval $(autotools-package))
