8b4418c364ef39da7c800735806040f7853f6b65
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-10-22 02:06:54 +0000

    xapp_xfontsel: fix build against Xmu by bumping version
    Fix a build issue similar to the one encountered on xapp_listres at
    http://autobuild.buildroot.org/results/06d0ef38165769df38047c1f64d4cfae835e9e54/.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xfontsel/xapp_xfontsel.mk
+++ package/x11r7/xapp_xfontsel/xapp_xfontsel.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XFONTSEL_VERSION = 1.0.1
+XAPP_XFONTSEL_VERSION = 1.0.4
 XAPP_XFONTSEL_SOURCE = xfontsel-$(XAPP_XFONTSEL_VERSION).tar.bz2
 XAPP_XFONTSEL_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XFONTSEL_DEPENDENCIES = xlib_libXaw
