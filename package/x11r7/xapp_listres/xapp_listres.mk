50a331ec956815843e21f20d4838051456056a6d
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-10-22 02:06:51 +0000

    xapp_listres: fix build against Xmu by bumping the version
    Fixes
    
      http://autobuild.buildroot.org/results/06d0ef38165769df38047c1f64d4cfae835e9e54
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_listres/xapp_listres.mk
+++ package/x11r7/xapp_listres/xapp_listres.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_LISTRES_VERSION = 1.0.1
+XAPP_LISTRES_VERSION = 1.0.3
 XAPP_LISTRES_SOURCE = listres-$(XAPP_LISTRES_VERSION).tar.bz2
 XAPP_LISTRES_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_LISTRES_DEPENDENCIES = xlib_libX11 xlib_libXaw xlib_libXmu xlib_libXt
