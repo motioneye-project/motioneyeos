b6d2046c1eec975944c2709c86d5d5259e7b2494
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-10-22 02:06:55 +0000

    xapp_xgc: fix build against Xmu by bumping version
    Fix a build issue similar to the one encountered on xapp_listres at
    http://autobuild.buildroot.org/results/06d0ef38165769df38047c1f64d4cfae835e9e54/.
    
    Note that this new version requires a minor cross-compilation fix in
    configure.ac, which has already been committed upstream but is not yet
    part of a release tarball.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xgc/xapp_xgc.mk
+++ package/x11r7/xapp_xgc/xapp_xgc.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-XAPP_XGC_VERSION = 1.0.1
+XAPP_XGC_VERSION = 1.0.3
 XAPP_XGC_SOURCE = xgc-$(XAPP_XGC_VERSION).tar.bz2
 XAPP_XGC_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XGC_DEPENDENCIES = xlib_libXaw
+XAPP_XGC_AUTORECONF = YES
 
 $(eval $(autotools-package))
