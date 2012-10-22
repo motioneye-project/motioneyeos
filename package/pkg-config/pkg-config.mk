703d5943f403f779a8ad44afe83d794c2a392d34
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-10-22 15:47:53 +0000

    pkg-config: remove host variant
    Remove host-pkg-config in favour of host-pkgconf.
    Also remove the sysroot support patch since it's only intended for the
    host variant.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/pkg-config/pkg-config.mk
+++ package/pkg-config/pkg-config.mk
@@ -3,23 +3,11 @@
 # pkgconfig
 #
 #############################################################
+
 PKG_CONFIG_VERSION = 0.25
 PKG_CONFIG_SOURCE = pkg-config-$(PKG_CONFIG_VERSION).tar.gz
 PKG_CONFIG_SITE = http://pkgconfig.freedesktop.org/releases/
-
 PKG_CONFIG_DEPENDENCIES = libglib2
-
 PKG_CONFIG_CONF_OPT = --with-installed-glib
 
-HOST_PKG_CONFIG_CONF_OPT = \
-		--with-pc-path="$(STAGING_DIR)/usr/lib/pkgconfig:$(STAGING_DIR)/usr/share/pkgconfig" \
-		--with-sysroot="$(STAGING_DIR)" \
-		--disable-static
-
-HOST_PKG_CONFIG_AUTORECONF = YES
-HOST_PKG_CONFIG_DEPENDENCIES =
-
 $(eval $(autotools-package))
-$(eval $(host-autotools-package))
-
-PKG_CONFIG_HOST_BINARY:=$(HOST_DIR)/usr/bin/pkg-config
