6e24dfada40bd49847814e6f6e26b528aeec7f48
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-10-30 02:29:51 +0000

    conntrack-tools: bump to version 1.4.0
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/conntrack-tools/conntrack-tools.mk
+++ package/conntrack-tools/conntrack-tools.mk
@@ -4,11 +4,12 @@
 #
 #############################################################
 
-CONNTRACK_TOOLS_VERSION = 1.2.2
+CONNTRACK_TOOLS_VERSION = 1.4.0
 CONNTRACK_TOOLS_SOURCE = conntrack-tools-$(CONNTRACK_TOOLS_VERSION).tar.bz2
 CONNTRACK_TOOLS_SITE = http://www.netfilter.org/projects/conntrack-tools/files
 CONNTRACK_TOOLS_DEPENDENCIES = host-pkgconf \
-	libnetfilter_conntrack libnetfilter_cttimeout
+	libnetfilter_conntrack libnetfilter_cthelper libnetfilter_cttimeout \
+	libnetfilter_queue
 CONNTRACK_TOOLS_LICENSE = GPLv2
 CONNTRACK_TOOLS_LICENSE_FILES = COPYING
 
