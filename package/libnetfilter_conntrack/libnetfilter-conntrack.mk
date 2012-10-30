6ee86f603bd449f81c9e262e01750d3ba4c9767f
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-10-30 02:29:42 +0000

    libnetfilter_conntrack: bump to version 1.0.2
    [Peter: adjust dnsmasq dependency]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libnetfilter_conntrack/libnetfilter-conntrack.mk
+++ package/libnetfilter_conntrack/libnetfilter-conntrack.mk
@@ -4,10 +4,12 @@
 #
 #############################################################
 
-LIBNETFILTER_CONNTRACK_VERSION = 1.0.1
+LIBNETFILTER_CONNTRACK_VERSION = 1.0.2
 LIBNETFILTER_CONNTRACK_SOURCE = libnetfilter_conntrack-$(LIBNETFILTER_CONNTRACK_VERSION).tar.bz2
 LIBNETFILTER_CONNTRACK_SITE = http://www.netfilter.org/projects/libnetfilter_conntrack/files
 LIBNETFILTER_CONNTRACK_INSTALL_STAGING = YES
-LIBNETFILTER_CONNTRACK_DEPENDENCIES = host-pkgconf libnfnetlink
+LIBNETFILTER_CONNTRACK_DEPENDENCIES = host-pkgconf libnfnetlink libmnl
+LIBNETFILTER_CONNTRACK_LICENSE = GPLv2+
+LIBNETFILTER_CONNTRACK_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
