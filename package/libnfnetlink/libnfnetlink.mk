50fbbd3f6dcbb8c1ebd2d2e97310c2ff7cf39aa6
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-10-30 02:29:44 +0000

    libnfnetlink: bump to version 1.0.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libnfnetlink/libnfnetlink.mk
+++ package/libnfnetlink/libnfnetlink.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBNFNETLINK_VERSION = 1.0.0
+LIBNFNETLINK_VERSION = 1.0.1
 LIBNFNETLINK_SOURCE = libnfnetlink-$(LIBNFNETLINK_VERSION).tar.bz2
 LIBNFNETLINK_SITE = http://www.netfilter.org/projects/libnfnetlink/files
 LIBNFNETLINK_INSTALL_STAGING = YES
