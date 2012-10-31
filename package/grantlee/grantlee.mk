062129c16eeea69c72d7d65b109bc7667449cb71
Zoltan Gyarmati <mr.zoltan.gyarmati@gmail.com>
2012-10-31 10:38:03 +0000

    add new package grantlee
    [Peter: sort alphabetically in Config.in]
    Signed-off-by: Zoltan Gyarmati <mr.zoltan.gyarmati@gmail.com>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Tested-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/grantlee/grantlee.mk
@@ -0,0 +1,13 @@
+#############################################################
+#
+# grantlee
+#
+#############################################################
+
+GRANTLEE_VERSION = 0.2.0
+GRANTLEE_SITE = http://downloads.grantlee.org
+GRANTLEE_INSTALL_STAGING = YES
+GRANTLEE_DEPENDENCIES =  qt
+
+$(eval $(cmake-package))
+
