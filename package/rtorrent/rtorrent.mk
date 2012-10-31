3ff05f80ffb82e077a332b0c0625f4696a138528
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-10-31 07:23:34 +0000

    rtorrent: bump to version 0.9.3
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/rtorrent/rtorrent.mk
+++ package/rtorrent/rtorrent.mk
@@ -4,9 +4,11 @@
 #
 #############################################################
 
-RTORRENT_VERSION = 0.9.2
+RTORRENT_VERSION = 0.9.3
 RTORRENT_SITE = http://libtorrent.rakshasa.no/downloads
 RTORRENT_DEPENDENCIES = host-pkgconf libcurl libsigc libtorrent ncurses
 RTORRENT_AUTORECONF = YES
+RTORRENT_LICENSE = GPLv2
+RTORRENT_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
