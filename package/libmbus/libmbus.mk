d727a4da444cb0001f8b5f73e73809780542be65
Sagaert Johan <sagaert.johan@skynet.be>
2012-11-05 15:20:03 +0000

    libmbus : bump version to 0.8
    This release contains many improvements bug fixes, and major new features and
    support for float point numbers and support for multi-telegram communication...
    
    [Peter: also drop autoreconf]
    Signed-off-by: Sagaert Johan <sagaert.johan@skynet.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libmbus/libmbus.mk
+++ package/libmbus/libmbus.mk
@@ -4,12 +4,8 @@
 #
 #############################################################
 
-LIBMBUS_VERSION = 0.7.0
+LIBMBUS_VERSION = 0.8.0
 LIBMBUS_SITE = http://www.freescada.com/public-dist
 LIBMBUS_INSTALL_STAGING = YES
-
-# Without this the build yields an error:
-#   cannot find input file: `test/Makefile.in'
-LIBMBUS_AUTORECONF = YES
 
 $(eval $(autotools-package))
