41a625c4b48db66ff626d65c524eaca71afcfd40
Yegor Yefremov <yegorslists@googlemail.com>
2012-11-05 22:07:53 +0000

    xapp_mkfontdir: bump to 1.0.7
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_mkfontdir/xapp_mkfontdir.mk
+++ package/x11r7/xapp_mkfontdir/xapp_mkfontdir.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_MKFONTDIR_VERSION = 1.0.6
+XAPP_MKFONTDIR_VERSION = 1.0.7
 XAPP_MKFONTDIR_SOURCE = mkfontdir-$(XAPP_MKFONTDIR_VERSION).tar.bz2
 XAPP_MKFONTDIR_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_MKFONTDIR_DEPENDENCIES = xapp_mkfontscale
