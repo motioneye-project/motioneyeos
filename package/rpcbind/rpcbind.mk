456f9de2fd6feffa465aeef44fb3d0082e3b0e0f
Peter Korsgaard <jacmet@sunsite.dk>
2012-11-07 00:07:51 +0100

    rpcbind: correct download URL
    BR2_SOURCEFORGE_MIRROR is gone.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/rpcbind/rpcbind.mk
+++ package/rpcbind/rpcbind.mk
@@ -5,7 +5,7 @@
 #############################################################
 
 RPCBIND_VERSION = 0.2.0
-RPCBIND_SITE    = http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/project/rpcbind/rpcbind/$(RPCBIND_VERSION)
+RPCBIND_SITE    = http://downloads.sourceforge.net/project/rpcbind/rpcbind/$(RPCBIND_VERSION)
 RPCBIND_SOURCE  = rpcbind-$(RPCBIND_VERSION).tar.bz2
 RPCBIND_LICENSE = BSD-3c
 RPCBIND_LICENSE_FILES = COPYING
