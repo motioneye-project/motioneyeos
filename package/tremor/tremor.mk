695c4bcebc6093f44d21117489bb72cc7c76c5af
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-11-10 12:43:01 +0000

    tremor: force ARM mode when building to avoid Thumb2 build problems
    Fixes:
    
      http://autobuild.buildroot.org/results/502b7d3f29d7982d56de2ae4a0b943315e04713b/build-end.log
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tremor/tremor.mk
+++ package/tremor/tremor.mk
@@ -12,4 +12,11 @@ TREMOR_AUTORECONF = YES
 TREMOR_INSTALL_STAGING = YES
 TREMOR_DEPENDENCIES = libogg
 
+# tremor has ARM assembly code that cannot be compiled in Thumb2 mode,
+# so we must force the traditional ARM mode.
+ifeq ($(BR2_arm),y)
+TREMOR_CONF_ENV = \
+	CFLAGS="$(TARGET_CFLAGS) -marm"
+endif
+
 $(eval $(autotools-package))
