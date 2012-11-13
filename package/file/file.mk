ba1c9131537573169f7bc1cfbb39a261118107ab
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2012-11-13 01:05:43 +0000

    file: add license info
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/file/file.mk
+++ package/file/file.mk
@@ -8,6 +8,8 @@ FILE_VERSION = 5.11
 FILE_SITE = ftp://ftp.astron.com/pub/file
 FILE_DEPENDENCIES = host-file zlib
 FILE_INSTALL_STAGING = YES
+FILE_LICENSE = BSD-2c, one file BSD-4c, one file BSD-3c
+FILE_LICENSE_FILES = COPYING src/mygetopt.h src/vasprintf.c
 
 define FILE_UNINSTALL_TARGET_CMDS
 	$(MAKE) DESTDIR=$(TARGET_DIR) uninstall -C $(FILE_DIR)
