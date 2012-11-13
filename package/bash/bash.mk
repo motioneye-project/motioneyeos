ef2d26003fbee27b7e53e49110ed3badd65c7200
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2012-11-13 01:05:37 +0000

    bash: add license info
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/bash/bash.mk
+++ package/bash/bash.mk
@@ -7,6 +7,9 @@
 BASH_VERSION = 4.2
 BASH_SITE = $(BR2_GNU_MIRROR)/bash
 BASH_DEPENDENCIES = ncurses
+BASH_LICENSE = GPLv3+
+BASH_LICENSE_FILES = COPYING
+
 BASH_CONF_ENV +=                       \
    bash_cv_job_control_missing=present \
    bash_cv_sys_named_pipes=present     \
