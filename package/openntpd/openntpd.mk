c4030d3acf64c338eb7c4f8b77508fc7762bfcab
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2012-11-13 01:05:53 +0000

    openntpd: add license info
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/openntpd/openntpd.mk
+++ package/openntpd/openntpd.mk
@@ -7,6 +7,8 @@
 OPENNTPD_VERSION = 3.9p1
 OPENNTPD_SITE = ftp://ftp.openbsd.org/pub/OpenBSD/OpenNTPD
 OPENNTPD_CONF_OPT = --with-builtin-arc4random --disable-strip
+OPENNTPD_LICENSE = MIT-like, BSD-2c, BSD-3c
+OPENNTPD_LICENSE_FILES = LICENCE
 
 define OPENNTPD_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/usr/sbin/ntpd
