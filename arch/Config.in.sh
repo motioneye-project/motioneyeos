44c04a2b4aa998e9c52c4d30f45b1ded933b966a
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-11-15 03:53:48 +0000

    arch: improve definition of gcc mtune, mcpu, etc.
    As suggested by Yann E. Morin, there is a better way than our current
    big Config.in.common to define the gcc mtune, mcpu, march,
    etc. values. We can split the setting of those values in each
    architecture file, which makes a lot more sense.
    
    Therefore, the Config.in file now creates empty kconfig variables
    BR2_ARCH, BR2_ENDIAN, BR2_GCC_TARGET_TUNE, BR2_GCC_TARGET_ARCH,
    BR2_GCC_TARGET_ABI and BR2_GCC_TARGET_CPU. The values of those
    variables are set by the individual Config.in.<arch> files. This is
    possible because such files are now only conditionally included
    depending on the top-level architecture that has been selected.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- arch/Config.in.sh
+++ arch/Config.in.sh
@@ -22,3 +22,20 @@ config BR2_sh4a
 config BR2_sh4aeb
 	bool "sh4aeb (SH4A big endian)"
 endchoice
+
+config BR2_ARCH
+	default "sh2"		if BR2_sh2
+	default "sh2a"		if BR2_sh2a
+	default "sh3"		if BR2_sh3
+	default "sh3eb"		if BR2_sh3eb
+	default "sh4"		if BR2_sh4
+	default "sh4eb"		if BR2_sh4eb
+	default "sh4a"		if BR2_sh4a
+	default "sh4aeb"	if BR2_sh4aeb
+	default "sh64"		if BR2_sh64
+
+config BR2_ENDIAN
+	default "LITTLE"	if BR2_sh3 || BR2_sh4 || BR2_sh4a || \
+				   BR2_x86_64 || BR2_sh64
+	default "BIG"		if BR2_sh2 || BR2_sh2a || BR2_sh3eb || \
+				   BR2_sh4eb || BR2_sh4aeb
