cea473d8afaf164a83b1bef92965d3d28e11c41f
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-11-15 12:28:47 +0000

    xlib_libXt: add patch to fix cross-compilation issue
    Fixes the following build failure:
    
     http://autobuild.buildroot.org/results/2815885d589d75259508bcce4f272b8dddb3be77/build-end.log
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xlib_libXt/xlib_libXt.mk
+++ package/x11r7/xlib_libXt/xlib_libXt.mk
@@ -10,5 +10,6 @@ XLIB_LIBXT_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXT_INSTALL_STAGING = YES
 XLIB_LIBXT_DEPENDENCIES = xlib_libSM xlib_libX11 xproto_kbproto xproto_xproto xcb-proto libxcb host-xproto_xproto
 XLIB_LIBXT_CONF_OPT = --disable-malloc0returnsnull --disable-install-makestrs
+XLIB_LIBXT_AUTORECONF = YES
 
 $(eval $(autotools-package))
