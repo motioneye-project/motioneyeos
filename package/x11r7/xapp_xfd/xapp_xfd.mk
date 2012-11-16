0a4f38f1d9bf1d1f73a9b411eee0524b9c199638
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-11-16 07:23:57 +0000

    xapp_xfd: bump version to 1.1.1 to fix Xmu related build failure
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xfd/xapp_xfd.mk
+++ package/x11r7/xapp_xfd/xapp_xfd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XFD_VERSION = 1.0.1
+XAPP_XFD_VERSION = 1.1.1
 XAPP_XFD_SOURCE = xfd-$(XAPP_XFD_VERSION).tar.bz2
 XAPP_XFD_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XFD_DEPENDENCIES = freetype fontconfig xlib_libXaw xlib_libXft
