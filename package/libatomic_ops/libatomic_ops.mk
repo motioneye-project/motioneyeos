b2ed6a0564946932ebfc6eaf2470d333bf188f62
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-11-17 12:24:02 +0000

    libatomic_ops: bump to version 7.2
    The 1.2 we were using doesn't build on ARM Thumb platforms:
    
      http://autobuild.buildroot.org/results/94ef6be7dcb31021462e7313724217627d4b29df/build-end.log
    
    Moreover, the 7.2 version is the one used by Debian/Ubuntu, and it has
    been tested to build fine with the two packages that depend on
    libatomic_ops: libdrm and pulseaudio.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libatomic_ops/libatomic_ops.mk
+++ package/libatomic_ops/libatomic_ops.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBATOMIC_OPS_VERSION = 1.2
+LIBATOMIC_OPS_VERSION = 7.2
 LIBATOMIC_OPS_SOURCE = libatomic_ops-$(LIBATOMIC_OPS_VERSION).tar.gz
 LIBATOMIC_OPS_SITE = http://www.hpl.hp.com/research/linux/atomic_ops/download
 LIBATOMIC_OPS_INSTALL_STAGING = YES
