d69172d7d807769f5091a612f7145bd464c7bc82
Peter Korsgaard <jacmet@sunsite.dk>
2012-11-27 16:42:43 -0800

    package: add libmatroska package
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libmatroska/libmatroska.mk
@@ -0,0 +1,27 @@
+#############################################################
+#
+# libmatroska
+#
+#############################################################
+
+LIBMATROSKA_VERSION = 1.3.0
+LIBMATROSKA_SOURCE = libmatroska-$(LIBMATROSKA_VERSION).tar.bz2
+LIBMATROSKA_SITE = http://dl.matroska.org/downloads/libmatroska/
+LIBMATROSKA_INSTALL_STAGING = YES
+LIBMATROSKA_LICENSE = LGPLv2.1+
+LIBMATROSKA_LICENSE_FILES = LICENSE.LGPL
+LIBMATROSKA_DEPENDENCIES = libebml
+
+define LIBMATROSKA_BUILD_CMDS
+	$(MAKE) -C $(@D)/make/linux CROSS="$(CCACHE) $(TARGET_CROSS)"
+endef
+
+define LIBMATROSKA_INSTALL_STAGING_CMDS
+	$(MAKE) -C $(@D)/make/linux prefix=$(STAGING_DIR)/usr install
+endef
+
+define LIBMATROSKA_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D)/make/linux prefix=$(TARGET_DIR)/usr install
+endef
+
+$(eval $(generic-package))
