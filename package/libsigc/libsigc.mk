537556b22ca6252d319a33ac797afb45b215dbd1
Charles Manning <cdhmanning@gmail.com>
2012-11-29 15:42:32 +0000

    libsigc: Don't copy devel documentation onto target
    The libsigc++ developer documentation was being copied onto the target.
    Add a clean-up to fix that.
    
    Signed-off-by: Charles Manning <cdhmanning@gmail.com>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libsigc/libsigc.mk
+++ package/libsigc/libsigc.mk
@@ -8,4 +8,10 @@ LIBSIGC_SOURCE = libsigc++-$(LIBSIGC_VERSION).tar.bz2
 LIBSIGC_SITE = http://ftp.gnome.org/pub/GNOME/sources/libsigc++/2.2
 LIBSIGC_INSTALL_STAGING = YES
 
+define LIBSIGC_INSTALL_TARGET_FIXUP
+	rm -rf $(TARGET_DIR)/usr/share/devhelp/books/libsigc++*
+endef
+
+LIBSIGC_POST_INSTALL_TARGET_HOOKS += LIBSIGC_INSTALL_TARGET_FIXUP
+
 $(eval $(autotools-package))
