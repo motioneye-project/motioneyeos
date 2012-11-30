4d4b1e4c5d98cbaafb8473c29d06b0d472cb34df
Gregory Hermant <gregory.hermant@calao-systems.com>
2012-11-30 02:39:14 +0000

    Add init script files for snowball board
    Signed-off-by: Gregory Hermant <gregory.hermant@calao-systems.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/snowball-init/snowball-init.mk
@@ -0,0 +1,15 @@
+#############################################################
+#
+# snowball-init
+#
+#############################################################
+SNOWBALL_INIT_VERSION = b064be21de25729039e5e54037bbdd2e25cfd5b7
+SNOWBALL_INIT_SITE = git://igloocommunity.org/git/bsp/snowball-init.git
+SNOWBALL_INIT_LICENSE = BSD-4c
+SNOWBALL_INIT_LICENSE_FILES = debian/copyright
+
+define SNOWBALL_INIT_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/snowball $(TARGET_DIR)/etc/init.d/S50snowball
+endef
+
+$(eval $(generic-package))
