51eb3be3da6e9ee8b32d6872db5159e594dc98e4
Samuel Martin <s.martin49@gmail.com>
2012-11-30 11:46:32 +0000

    owl-linux: add license info
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/owl-linux/owl-linux.mk
+++ package/owl-linux/owl-linux.mk
@@ -5,6 +5,9 @@
 #############################################################
 OWL_LINUX_VERSION = 1.0.7
 OWL_LINUX_SITE = http://linux.hd-wireless.se/pub/Linux/DownloadDrivers
+OWL_LINUX_LICENSE = PROPRIETARY
+OWL_LINUX_LICENSE_FILES = LICENSE
+OWL_LINUX_REDISTRIBUTE = NO
 
 OWL_LINUX_DEPENDENCIES = linux
 
