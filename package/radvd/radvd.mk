34c4091d2e3fb0a37e39b0bd740bdd1c17965899
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-12-03 04:46:01 +0000

    radvd: bump to version 1.9.2
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/radvd/radvd.mk
+++ package/radvd/radvd.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-RADVD_VERSION = 1.9.1
+RADVD_VERSION = 1.9.2
 RADVD_SITE = http://www.litech.org/radvd/dist
 RADVD_DEPENDENCIES = flex libdaemon host-flex host-pkgconf
 RADVD_AUTORECONF = YES
