ea2df5659831325605a0735e8ea6e7ec6fb5dfe2
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-12-03 04:46:05 +0000

    ed: bump to version 1.7
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ed/ed.mk
+++ package/ed/ed.mk
@@ -4,9 +4,11 @@
 #
 #############################################################
 
-ED_VERSION = 1.6
+ED_VERSION = 1.7
 ED_SITE = $(BR2_GNU_MIRROR)/ed
 ED_CONF_OPT = CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" \
 		LDFLAGS="$(TARGET_LDFLAGS)"
+ED_LICENSE = GPLv3+
+ED_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
