440750207d594c932c23196d265847fef5a3426a
Peter Korsgaard <jacmet@sunsite.dk>
2012-12-03 18:35:28 -0800

    xproto_xcmiscproto: only build specs if explicitly requested
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xproto_xcmiscproto/xproto_xcmiscproto.mk
+++ package/x11r7/xproto_xcmiscproto/xproto_xcmiscproto.mk
@@ -8,7 +8,8 @@ XPROTO_XCMISCPROTO_VERSION = 1.2.1
 XPROTO_XCMISCPROTO_SOURCE = xcmiscproto-$(XPROTO_XCMISCPROTO_VERSION).tar.bz2
 XPROTO_XCMISCPROTO_SITE = http://xorg.freedesktop.org/releases/individual/proto
 XPROTO_XCMISCPROTO_INSTALL_STAGING = YES
-XPROTO_XCMISCPROTO_INSTALL_TARGET = NO
+XPROTO_XCMISCPROTO_CONF_OPT = $(if $(BR2_HAVE_DOCUMENTATION),,--disable-specs)
+HOST_XPROTO_XCMISCPROTO_CONF_OPT = --disable-specs
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
