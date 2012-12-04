9e5a4b26a272e6a31584b5933e38b05f816a0c7b
Peter Korsgaard <jacmet@sunsite.dk>
2012-12-03 18:28:21 -0800

    xlib_xtrans: only build documentation if explicitly requested
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xlib_xtrans/xlib_xtrans.mk
+++ package/x11r7/xlib_xtrans/xlib_xtrans.mk
@@ -8,6 +8,8 @@ XLIB_XTRANS_VERSION = 1.2.6
 XLIB_XTRANS_SOURCE = xtrans-$(XLIB_XTRANS_VERSION).tar.bz2
 XLIB_XTRANS_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_XTRANS_INSTALL_STAGING = YES
+XLIB_XTRANS_CONF_OPT = $(if $(BR2_HAVE_DOCUMENTATION),,--disable-docs)
+HOST_XLIB_XTRANS_CONF_OPT = --disable-docs
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
