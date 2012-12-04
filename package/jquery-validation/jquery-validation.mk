48e0da1d745435f1c64c17556fc8758c6e37ce42
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-12-04 07:24:06 +0000

    jquery-validation: bump to version 1.10.0
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/jquery-validation/jquery-validation.mk
+++ package/jquery-validation/jquery-validation.mk
@@ -1,13 +1,13 @@
-JQUERY_VALIDATION_VERSION = 1.9.0
+JQUERY_VALIDATION_VERSION = 1.10.0
 JQUERY_VALIDATION_SITE = http://jquery.bassistance.de/validate
 JQUERY_VALIDATION_SOURCE = jquery-validation-$(JQUERY_VALIDATION_VERSION).zip
 
 define JQUERY_VALIDATION_EXTRACT_CMDS
-	unzip -d $(BUILD_DIR) $(DL_DIR)/$(JQUERY_VALIDATION_SOURCE)
+	unzip -d $(@D) $(DL_DIR)/$(JQUERY_VALIDATION_SOURCE)
 endef
 
 define JQUERY_VALIDATION_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0644 -D $(@D)/jquery.validate.min.js \
+	$(INSTALL) -m 0644 -D $(@D)/dist/jquery.validate.min.js \
 		$(TARGET_DIR)/var/www/jquery.validate.js
 endef
 
