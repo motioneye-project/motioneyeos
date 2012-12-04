73bf02320e91380e7f10c7501e35b6b669797564
Peter Korsgaard <jacmet@sunsite.dk>
2012-12-03 18:34:04 -0800

    xproto_xextproto: only build specs if explicitly requested
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xproto_xextproto/xproto_xextproto.mk
+++ package/x11r7/xproto_xextproto/xproto_xextproto.mk
@@ -8,7 +8,8 @@ XPROTO_XEXTPROTO_VERSION = 7.2.0
 XPROTO_XEXTPROTO_SOURCE = xextproto-$(XPROTO_XEXTPROTO_VERSION).tar.bz2
 XPROTO_XEXTPROTO_SITE = http://xorg.freedesktop.org/releases/individual/proto
 XPROTO_XEXTPROTO_INSTALL_STAGING = YES
-XPROTO_XEXTPROTO_INSTALL_TARGET = NO
+XPROTO_XEXTPROTO_CONF_OPT = $(if $(BR2_HAVE_DOCUMENTATION),,--disable-specs)
+HOST_XPROTO_XEXTPROTO_CONF_OPT = --disable-specs
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
