34ae9eb63db4ff48da4ed45b9b48997e7c6cd590
Simon Dawson <spdawson@gmail.com>
2012-12-05 08:39:07 +0000

    neard: new package
    [Peter: add toolchain dependencies for libglib2/libnl/dbus + host-pkgconf]
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/neard/neard.mk
@@ -0,0 +1,14 @@
+#############################################################
+#
+# neard
+#
+#############################################################
+NEARD_VERSION = 0.8
+NEARD_SITE = $(BR2_KERNEL_MIRROR)/linux/network/nfc
+NEARD_LICENSE = GPLv2
+NEARD_LICENSE_FILES = COPYING
+
+NEARD_DEPENDENCIES = host-pkgconf dbus libglib2 libnl
+NEARD_CONF_OPT = --disable-traces
+
+$(eval $(autotools-package))
