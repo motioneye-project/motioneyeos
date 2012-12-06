7ffda3e241c4d57394d9f5eeb19e41a3546c0495
Davide Viti <zinosat@tiscali.it>
2012-12-06 14:16:25 +0000

    tcllib: new package
    Signed-off-by: Davide Viti <zinosat@tiscali.it>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Tested-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/tcllib/tcllib.mk
@@ -0,0 +1,14 @@
+#############################################################
+#
+# tcllib
+#
+#############################################################
+TCLLIB_VERSION       = 1.13
+TCLLIB_SOURCE        = tcllib-$(TCLLIB_VERSION).tar.bz2
+TCLLIB_SITE          = http://downloads.sourceforge.net/project/tcllib/tcllib/$(TCLLIB_VERSION)
+TCLLIB_LICENSE       = tcl license
+TCLLIB_LICENSE_FILES = license.terms
+TCLLIB_DEPENDENCIES  = host-tcl
+TCLLIB_CONF_ENV      = ac_cv_path_tclsh="$(HOST_DIR)/usr/bin/tclsh8.4"
+
+$(eval $(autotools-package))
