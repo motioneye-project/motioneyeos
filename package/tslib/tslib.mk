5611509259e513486dfc35941e80549b081b28fd
Stephan Hoffmann <sho@relinux.de>
2012-12-07 00:29:23 +0000

    tslib: add license info
    [Peter: Change to LGPL as pointed out by Arnout]
    Signed-off-by: Stephan Hoffmann <sho@relinux.de>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tslib/tslib.mk
+++ package/tslib/tslib.mk
@@ -5,6 +5,9 @@
 #############################################################
 TSLIB_VERSION = 412d99d
 TSLIB_SITE = http://github.com/kergoth/tslib/tarball/master
+TSLIB_LICENSE = LGPL
+TSLIB_LICENSE_FILES = COPYING
+
 TSLIB_AUTORECONF = YES
 TSLIB_INSTALL_STAGING = YES
 TSLIB_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) LDFLAGS=-L$(STAGING_DIR)/usr/lib install
