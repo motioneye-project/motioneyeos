36f96dbb7816ddeaa7f0a588f0a54ad8fd7a0ee5
Gary Coulbourne <bear@bears.org>
2012-12-07 07:04:37 +0000

    bump connman version to 1.9
    A quick patch to bump connman's version to 1.9
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/connman/connman.mk
+++ package/connman/connman.mk
@@ -4,7 +4,7 @@
 #
 #######################################################
 
-CONNMAN_VERSION = 1.8
+CONNMAN_VERSION = 1.9
 CONNMAN_SITE = $(BR2_KERNEL_MIRROR)/linux/network/connman/
 CONNMAN_DEPENDENCIES = libglib2 dbus iptables gnutls
 CONNMAN_INSTALL_STAGING = YES
