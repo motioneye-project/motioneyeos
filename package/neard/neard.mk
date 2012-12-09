53f722dff7a9931f67b66a68e9b8e0b11b6b1a85
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-12-09 08:14:02 +0000

    neard: fix build failure caused by missing <linux/nfc.h>
    Fixes:
    
      http://autobuild.buildroot.org/results/bd1cb8795415b7369a9f4268b9f058cb78d4000d/build-end.log
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/neard/neard.mk
+++ package/neard/neard.mk
@@ -8,6 +8,7 @@ NEARD_SITE = $(BR2_KERNEL_MIRROR)/linux/network/nfc
 NEARD_LICENSE = GPLv2
 NEARD_LICENSE_FILES = COPYING
 
+NEARD_AUTORECONF = YES
 NEARD_DEPENDENCIES = host-pkgconf dbus libglib2 libnl
 NEARD_CONF_OPT = --disable-traces
 
