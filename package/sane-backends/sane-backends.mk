fc3a89d1cde8da2df17fdcc42289f330c2711d9b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-12-09 04:35:02 +0000

    sane-backends: fix documentation build failure
    Disable documentation build, it seems to break with older
    texlive/kpathsea versions, fixes:
    http://autobuild.buildroot.net/results/44327b543d8918929bf252c5284e8691fda87fd0/
    Also switch to alioth mirror since the original tarball is gone from the
    regular site when 1.0.23 was released.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sane-backends/sane-backends.mk
+++ package/sane-backends/sane-backends.mk
@@ -5,7 +5,7 @@
 #############################################################
 SANE_BACKENDS_VERSION = 1.0.22
 SANE_BACKENDS_SOURCE = sane-backends-$(SANE_BACKENDS_VERSION).tar.gz
-SANE_BACKENDS_SITE = ftp://ftp2.sane-project.org/pub/sane/sane-backends-$(SANE_BACKENDS_VERSION)
+SANE_BACKENDS_SITE = https://alioth.debian.org/frs/download.php/3503
 
 ifeq ($(BR2_PACKAGE_LIBUSB),y)
 SANE_BACKENDS_DEPENDENCIES += libusb
@@ -36,5 +36,11 @@ SANE_BACKENDS_DEPENDENCIES += netsnmp
 else
 SANE_BACKENDS_CONF_OPT += --without-snmp
 endif
+
+define SANE_BACKENDS_DISABLE_DOCS
+	$(SED) 's/ doc//' $(@D)/Makefile
+endef
+
+SANE_BACKENDS_POST_CONFIGURE_HOOKS += SANE_BACKENDS_DISABLE_DOCS
 
 $(eval $(autotools-package))
