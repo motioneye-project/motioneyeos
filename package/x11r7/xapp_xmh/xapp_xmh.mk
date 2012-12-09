7e099a6da82d59ce737e48f279495e84d77bd5d3
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-12-09 08:14:01 +0000

    xapp_xmh: add dependency on xdata_xbitmap
    Fixes:
    
      http://autobuild.buildroot.org/results/fa059a89a305914700f63d7f0b0d4a3f63beb039/build-end.log
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xmh/xapp_xmh.mk
+++ package/x11r7/xapp_xmh/xapp_xmh.mk
@@ -7,6 +7,6 @@
 XAPP_XMH_VERSION = 1.0.2
 XAPP_XMH_SOURCE = xmh-$(XAPP_XMH_VERSION).tar.bz2
 XAPP_XMH_SITE = http://xorg.freedesktop.org/releases/individual/app
-XAPP_XMH_DEPENDENCIES = xlib_libXaw
+XAPP_XMH_DEPENDENCIES = xlib_libXaw xdata_xbitmaps
 
 $(eval $(autotools-package))
