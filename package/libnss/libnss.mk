8b827859f994767052f31d4ee6ca6524b5065d01
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-12-11 03:31:00 +0000

    libnss: fix build failures
    Such as
    http://autobuild.buildroot.net/results/2ca1d135269a67201e6b4d995ce8fbe94db1ed3f
    
    The mozilla build system passes all of the different ways of CFLAGS
    (OPTIMIZER, XCFLAGS, OS_CFLAGS) even when building host tools
    (nsintall).
    The best way around this without patching libnss is just building the
    host tools first without any CFLAGS and then going on with the rest.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libnss/libnss.mk
+++ package/libnss/libnss.mk
@@ -17,7 +17,6 @@ LIBNSS_BUILD_VARS = MOZILLA_CLIENT=1 \
 		NSPR_LIB_DIR=$(STAGING_DIR)/usr/lib \
 		BUILD_OPT=1 \
 		NS_USE_GCC=1 \
-		OPTIMIZER="$(TARGET_CFLAGS)" \
 		NSS_USE_SYSTEM_SQLITE=1 \
 		NSS_ENABLE_ECC=1 \
 		NATIVE_CC="$(HOSTCC)" \
@@ -34,11 +33,16 @@ endif
 
 
 define LIBNSS_BUILD_CMDS
-	$(MAKE1) -C $(@D)/$(LIBNSS_SUBDIR)/nss build_coreconf build_dbm all \
+	$(MAKE1) -C $(@D)/$(LIBNSS_SUBDIR)/nss build_coreconf \
 			SOURCE_MD_DIR=$(@D)/$(LIBNSS_DISTDIR) \
 			DIST=$(@D)/$(LIBNSS_DISTDIR) \
 			CHECKLOC= \
 			$(LIBNSS_BUILD_VARS)
+	$(MAKE1) -C $(@D)/$(LIBNSS_SUBDIR)/nss build_dbm all \
+			SOURCE_MD_DIR=$(@D)/$(LIBNSS_DISTDIR) \
+			DIST=$(@D)/$(LIBNSS_DISTDIR) \
+			CHECKLOC= \
+			$(LIBNSS_BUILD_VARS) OPTIMIZER="$(TARGET_CFLAGS)"
 endef
 
 define LIBNSS_INSTALL_STAGING_CMDS
