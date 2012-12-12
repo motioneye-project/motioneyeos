53f2c75754ad7cd5958d9156e01156fe9c8fce53
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-12-12 08:21:24 +0000

    samba: bump to version 3.6.10
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/samba/samba.mk
+++ package/samba/samba.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SAMBA_VERSION = 3.6.9
+SAMBA_VERSION = 3.6.10
 SAMBA_SITE = http://ftp.samba.org/pub/samba/stable
 SAMBA_SUBDIR = source3
 SAMBA_INSTALL_STAGING = YES
