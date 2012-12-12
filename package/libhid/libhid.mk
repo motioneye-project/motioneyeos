31341e8d40ec6c926419ebce8a71daba98a6ff9e
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-12-12 09:38:42 +0000

    libhid: replace patch on configure by patch on configure.ac
    In order to fix some other build problem of libhid, we'll need to do
    modifications to the configure.ac. Therefore, let's first convert the
    current patch on configure to a patch on configure.ac, and mark the
    package as AUTORECONF=YES.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libhid/libhid.mk
+++ package/libhid/libhid.mk
@@ -9,6 +9,7 @@ LIBHID_SOURCE = libhid-$(LIBHID_VERSION).tar.gz
 LIBHID_SITE = http://alioth.debian.org/frs/download.php/1958
 LIBHID_DEPENDENCIES = libusb-compat libusb
 LIBHID_INSTALL_STAGING = YES
+LIBHID_AUTORECONF = YES
 # configure runs libusb-config for cflags/ldflags. Ensure it picks up
 # the target version
 LIBHID_CONF_ENV = PATH=$(STAGING_DIR)/usr/bin:$(TARGET_PATH)
