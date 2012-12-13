21dd3e777b5b9eb40c6e0fe9fadcf0c7fcc63901
Peter Korsgaard <jacmet@sunsite.dk>
2012-12-13 23:58:11 +0100

    package: add selection between libjpeg and jpeg-turbo
    Introduce a virtual 'jpeg' package, which pulls in either libjpeg or
    jpeg-turbo depending on a choice selection.
    
    Rename jpeg package to libjpeg so we can reuse 'jpeg' for the virtual
    package, making the change transparent to existing users and all the
    packages using libjpeg.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/jpeg/jpeg.mk
+++ package/jpeg/jpeg.mk
@@ -1,18 +1,9 @@
 #############################################################
 #
-# jpeg (libraries needed by some apps)
+# jpeg
 #
 #############################################################
-JPEG_VERSION = 8d
-JPEG_SITE = http://www.ijg.org/files/
-JPEG_SOURCE = jpegsrc.v$(JPEG_VERSION).tar.gz
-JPEG_INSTALL_STAGING = YES
 
-define JPEG_REMOVE_USELESS_TOOLS
-	rm -f $(addprefix $(TARGET_DIR)/usr/bin/,cjpeg djpeg jpegtrans rdjpgcom wrjpgcom)
-endef
+jpeg: $(if $(BR2_PACKAGE_JPEG_TURBO),jpeg-turbo,libjpeg)
 
-JPEG_POST_INSTALL_TARGET_HOOKS += JPEG_REMOVE_USELESS_TOOLS
-
-$(eval $(autotools-package))
-$(eval $(host-autotools-package))
+host-jpeg: host-libjpeg
---
+++ package/libjpeg/libjpeg.mk
@@ -0,0 +1,19 @@
+#############################################################
+#
+# libjpeg (libraries needed by some apps)
+#
+#############################################################
+
+LIBJPEG_VERSION = 8d
+LIBJPEG_SITE = http://www.ijg.org/files/
+LIBJPEG_SOURCE = jpegsrc.v$(LIBJPEG_VERSION).tar.gz
+LIBJPEG_INSTALL_STAGING = YES
+
+define LIBJPEG_REMOVE_USELESS_TOOLS
+	rm -f $(addprefix $(TARGET_DIR)/usr/bin/,cjpeg djpeg jpegtrans rdjpgcom wrjpgcom)
+endef
+
+LIBJPEG_POST_INSTALL_TARGET_HOOKS += LIBJPEG_REMOVE_USELESS_TOOLS
+
+$(eval $(autotools-package))
+$(eval $(host-autotools-package))
