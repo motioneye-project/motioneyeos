018a49a507fb9354b10ff7afd39e9fe2603cab4e
Peter Korsgaard <jacmet@sunsite.dk>
2012-12-15 00:58:02 +0100

    package: add flashbench
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/flashbench/flashbench.mk
@@ -0,0 +1,22 @@
+#############################################################
+#
+# flashbench
+#
+#############################################################
+
+FLASHBENCH_VERSION = 2e30b1968a66147412f21002ea844122a0d5e2f0
+FLASHBENCH_SITE = git://git.linaro.org/people/arnd/flashbench.git
+FLASHBENCH_LICENSE = GPLv2
+FLASHBENCH_LICENSE_FILES = COPYING
+
+define FLASHBENCH_BUILD_CMDS
+	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS) \
+		LDFLAGS="$(TARGET_LDFLAGS) -lrt"
+endef
+
+define FLASHBENCH_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -D $(@D)/flashbench $(TARGET_DIR)/usr/bin/flashbench
+	$(INSTALL) -m 755 -D $(@D)/erase $(TARGET_DIR)/usr/bin/erase
+endef
+
+$(eval $(generic-package))
