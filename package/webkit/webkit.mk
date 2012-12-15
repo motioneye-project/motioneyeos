d2d81f4b3420ae004bdcd3b3edf742ac7f10ff7a
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-12-15 07:29:41 +0000

    webkit: silence gazillions of warnings
    Building webkit with a recent gcc versions gives gazillions of
    warnings such as 'warning: cast from 'WTF::AlignedBufferChar* {aka
    char*}' to 'JSC::Identifier*' increases required alignment of target
    type [-Wcast-align]'.
    
    Those make the webkit build very noise, and hard to debug, so let's
    silence those warnings.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/webkit/webkit.mk
+++ package/webkit/webkit.mk
@@ -9,7 +9,13 @@ WEBKIT_SITE = http://www.webkitgtk.org
 WEBKIT_INSTALL_STAGING = YES
 WEBKIT_DEPENDENCIES = host-flex host-gperf icu libcurl libxml2 libxslt \
 			libgtk2 sqlite enchant libsoup jpeg libgail
-WEBKIT_CONF_ENV = ac_cv_path_icu_config=$(STAGING_DIR)/usr/bin/icu-config
+
+# Give explicit path to icu-config, and silence gazillions of warnings
+# with recent gcc versions.
+WEBKIT_CONF_ENV = ac_cv_path_icu_config=$(STAGING_DIR)/usr/bin/icu-config \
+	CFLAGS="$(TARGET_CFLAGS) -Wno-cast-align" \
+	CXXFLAGS="$(TARGET_CXXFLAGS) -Wno-cast-align"
+
 
 ifeq ($(BR2_PACKAGE_XORG7),y)
 	WEBKIT_CONF_OPT += --with-target=x11
