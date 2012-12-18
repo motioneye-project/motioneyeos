0ef90a63e845883aa45cedfdb26ed388d400f044
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-12-18 10:20:42 +0000

    xstroke: fix link issue
    The allyespackageconfig builds have trigerred a link issue on
    xstroke. In fact, xstroke must be linked against Xrender, Xext and dl
    in addition to the other libraries it was already be linked
    against. We fix that by adding a patch that modifies configure.ac and
    Makefile.am, and enabling autoreconf for this package.
    
    In addition, since xstroke uses directly Xrender and Xext, we add
    those dependencies to the package. In practice, it doesn't change
    anything, since some of the xstroke dependencies were already pulling
    those libraries, but as xstroke uses directly Xrender and Xext, it
    makes sense to have them as dependencies as well.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/xstroke/xstroke.mk
+++ package/xstroke/xstroke.mk
@@ -7,7 +7,8 @@ XSTROKE_VERSION = 0.6
 XSTROKE_SOURCE = xstroke-$(XSTROKE_VERSION).tar.gz
 XSTROKE_SITE = http://mirror.egtvedt.no/avr32linux.org/twiki/pub/Main/XStroke
 
-XSTROKE_DEPENDENCIES = xlib_libXft xlib_libXtst xlib_libXpm
+XSTROKE_DEPENDENCIES = xlib_libXft xlib_libXtst xlib_libXpm xlib_libXrender xlib_libXext
+XSTROKE_AUTORECONF = YES
 
 $(eval $(autotools-package))
 
