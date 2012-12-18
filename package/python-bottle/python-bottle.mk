0a3b19cdc92429ce4552be108283441ef4334a05
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2012-12-18 16:14:44 +0100

    python-bottle: new package
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/python-bottle/python-bottle.mk
@@ -0,0 +1,22 @@
+#############################################################
+#
+# python-bottle
+#
+#############################################################
+
+PYTHON_BOTTLE_VERSION = 0.11.4
+PYTHON_BOTTLE_SOURCE = bottle-$(PYTHON_BOTTLE_VERSION).tar.gz
+PYTHON_BOTTLE_SITE = http://pypi.python.org/packages/source/b/bottle
+PYTHON_BOTTLE_DEPENDENCIES = python
+PYTHON_BOTTLE_LICENSE = MIT
+# README.rst refers to the file "LICENSE" but it's not included
+
+define PYTHON_BOTTLE_BUILD_CMDS
+	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py build)
+endef
+
+define PYTHON_BOTTLE_INSTALL_TARGET_CMDS
+	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
+endef
+
+$(eval $(generic-package))
