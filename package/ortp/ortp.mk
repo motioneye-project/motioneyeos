356c2d7d843f7faa78c9ace79f17b0d947c9435b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-12-19 03:07:27 +0000

    ortp: disable Werror
    Disable -Werror for the ortp build, fixes:
    http://autobuild.buildroot.net/results/b869fcfdfdb9d1ec4864b86c6953b0242200c502
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ortp/ortp.mk
+++ package/ortp/ortp.mk
@@ -1,5 +1,6 @@
 ORTP_VERSION = 0.20.0
 ORTP_SITE = http://download.savannah.nongnu.org/releases/linphone/ortp/sources
+ORTP_CONF_OPT = --disable-strict
 ORTP_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
