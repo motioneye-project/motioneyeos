a5d3241f3bb620f7479e30c12bcc632734663792
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-12-24 12:20:43 +0100

    efl/libeet: bump to version 1.7.4
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/efl/libeet/libeet.mk
+++ package/efl/libeet/libeet.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBEET_VERSION = 1.5.0
+LIBEET_VERSION = 1.7.4
 LIBEET_SOURCE = eet-$(LIBEET_VERSION).tar.bz2
 LIBEET_SITE = http://download.enlightenment.org/releases/
 LIBEET_LICENSE = BSD-2c
