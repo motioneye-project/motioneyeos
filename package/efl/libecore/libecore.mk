5def1c1ab747e1650786e1cfe4d9d45042bd6ec1
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2012-12-24 12:19:05 +0100

    efl/libecore: bump version to 1.7.4
    The libecore-ecore_exe-fix-build-with-glibc-2-16 patch is no longer
    needed, since it has been merged upstream.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/efl/libecore/libecore.mk
+++ package/efl/libecore/libecore.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBECORE_VERSION = 1.1.0
+LIBECORE_VERSION = 1.7.4
 LIBECORE_SOURCE = ecore-$(LIBECORE_VERSION).tar.bz2
 LIBECORE_SITE = http://download.enlightenment.org/releases/
 LIBECORE_LICENSE = BSD-2c
