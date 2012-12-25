cf6ec44578a06907b9d86dc57b20ddf226ea0c74
Mikhail Boiko <mikhailboiko85@gmail.com>
2012-12-25 21:43:58 +0000

    new package: libfribidi
    [Peter: moved under 'Text and terminal handling']
    Signed-off-by: Mikhail Boiko <mikhailboiko85@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libfribidi/libfribidi.mk
@@ -0,0 +1,13 @@
+#############################################################
+#
+# libfribidi
+#
+#############################################################
+
+LIBFRIBIDI_VERSION = 0.19.5
+LIBFRIBIDI_SOURCE = fribidi-$(LIBFRIBIDI_VERSION).tar.bz2
+LIBFRIBIDI_SITE = http://www.fribidi.org/download/
+LIBFRIBIDI_LICENSE = LGPLv2.1+
+LIBFRIBIDI_LICENSE_FILES = COPYING
+
+$(eval $(autotools-package))
