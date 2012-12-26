f1b86cef9884894df26ed25efaabe254f8f0754b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-12-26 03:55:48 +0000

    gnutls: make it non-wchar friendly
    Make gnutls work for non-wchar toolchains.
    It's just a matter of throwing a helping hand to configure.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gnutls/gnutls.mk
+++ package/gnutls/gnutls.mk
@@ -10,7 +10,10 @@ GNUTLS_SITE = $(BR2_GNU_MIRROR)/gnutls
 GNUTLS_LICENSE = GPLv3+ LGPLv2.1+
 GNUTLS_LICENSE_FILES = COPYING lib/COPYING
 GNUTLS_DEPENDENCIES = host-pkgconf libgcrypt $(if $(BR2_PACKAGE_ZLIB),zlib)
-GNUTLS_CONF_ENV = acl_cv_rpath=no
+GNUTLS_CONF_ENV = acl_cv_rpath=no \
+	ac_cv_header_wchar_h=$(if $(BR2_USE_WCHAR),yes,no) \
+	gt_cv_c_wchar_t=$(if $(BR2_USE_WCHAR),yes,no) \
+	gt_cv_c_wint_t=$(if $(BR2_USE_WCHAR),yes,no)
 GNUTLS_CONF_OPT = --with-libgcrypt --without-libgcrypt-prefix \
 		--without-p11-kit --disable-rpath
 GNUTLS_INSTALL_STAGING = YES
