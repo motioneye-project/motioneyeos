562d1ad82e33cd2234af9abac7847e990af037ce
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-12-26 03:55:49 +0000

    libmicrohttpd: bump to version 0.9.24
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libmicrohttpd/libmicrohttpd.mk
+++ package/libmicrohttpd/libmicrohttpd.mk
@@ -3,14 +3,14 @@
 # libmicrohttpd
 #
 #############################################################
-LIBMICROHTTPD_VERSION = 0.4.6
-LIBMICROHTTPD_SOURCE = libmicrohttpd-$(LIBMICROHTTPD_VERSION).tar.gz
+
+LIBMICROHTTPD_VERSION = 0.9.24
 LIBMICROHTTPD_SITE = $(BR2_GNU_MIRROR)/libmicrohttpd
 LIBMICROHTTPD_INSTALL_STAGING = YES
 
 ifeq ($(BR2_PACKAGE_LIBMICROHTTPD_SSL),y)
-LIBMICROHTTPD_DEPENDENCIES += libgcrypt
-LIBMICROHTTPD_CONF_OPT += --enable-https \
+LIBMICROHTTPD_DEPENDENCIES += gnutls libgcrypt
+LIBMICROHTTPD_CONF_OPT += --enable-https --with-gnutls=$(STAGING_DIR)/usr \
 			  --with-libgcrypt-prefix=$(STAGING_DIR)/usr
 else
 LIBMICROHTTPD_CONF_OPT += --disable-https
