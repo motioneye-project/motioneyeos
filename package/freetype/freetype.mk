fc0d1be16b6ee1a24eeb3498b98034aa37cf4cca
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-12-27 05:39:10 +0000

    freetype: security bump to version 2.4.11
    Fixes a security vulnerability in the BDF implementation.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/freetype/freetype.mk
+++ package/freetype/freetype.mk
@@ -4,11 +4,13 @@
 #
 #############################################################
 
-FREETYPE_VERSION = 2.4.10
+FREETYPE_VERSION = 2.4.11
 FREETYPE_SOURCE = freetype-$(FREETYPE_VERSION).tar.bz2
 FREETYPE_SITE = http://downloads.sourceforge.net/project/freetype/freetype2/$(FREETYPE_VERSION)
 FREETYPE_INSTALL_STAGING = YES
 FREETYPE_MAKE_OPT = CCexe="$(HOSTCC)"
+FREETYPE_LICENSE = Dual FTL/GPLv2+
+FREETYPE_LICENSE_FILES = docs/FTL.txt docs/GPLv2.txt
 FREETYPE_DEPENDENCIES = host-pkgconf \
 	$(if $(BR2_PACKAGE_ZLIB),zlib) \
 	$(if $(BR2_PACKAGE_BZIP2),bzip2)
