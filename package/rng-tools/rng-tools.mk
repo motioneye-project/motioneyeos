a2529f7f4d18b18dac1ee00ac7655116cb805818
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-12-27 05:39:09 +0000

    rng-tools: bump to version 4
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/rng-tools/rng-tools.mk
+++ package/rng-tools/rng-tools.mk
@@ -4,9 +4,11 @@
 #
 #############################################################
 
-RNG_TOOLS_VERSION = 3
+RNG_TOOLS_VERSION = 4
 RNG_TOOLS_SITE = http://downloads.sourceforge.net/project/gkernel/rng-tools/$(RNG_TOOLS_VERSION)
 RNG_TOOLS_CONF_ENV = LIBS="-largp"
 RNG_TOOLS_DEPENDENCIES = argp-standalone
+RNG_TOOLS_LICENSE = GPLv2
+RNG_TOOLS_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
