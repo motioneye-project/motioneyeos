ec10d81e46cbcbb644dcfe0f91961fb09706be77
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-12-28 03:05:55 +0000

    ulogd: add support for mysql output
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ulogd/ulogd.mk
+++ package/ulogd/ulogd.mk
@@ -7,12 +7,19 @@
 ULOGD_VERSION = 2.0.1
 ULOGD_SOURCE = ulogd-$(ULOGD_VERSION).tar.bz2
 ULOGD_SITE = http://www.netfilter.org/projects/ulogd/files
-ULOGD_CONF_OPT = --with-dbi=no --with-mysql=no --with-pgsql=no
+ULOGD_CONF_OPT = --with-dbi=no --with-pgsql=no
 ULOGD_AUTORECONF = YES
 ULOGD_DEPENDENCIES = host-pkgconf \
 	libmnl libnetfilter_acct libnetfilter_conntrack libnetfilter_log \
 	libnfnetlink $(if $(BR2_PACKAGE_SQLITE),sqlite)
 ULOGD_LICENSE = GPLv2
 ULOGD_LICENSE_FILES = COPYING
+
+ifeq ($(BR2_PACKAGE_MYSQL_CLIENT),y)
+ULOGD_CONF_OPT += --with-mysql=$(STAGING_DIR)/usr
+ULOGD_DEPENDENCIES += mysql_client
+else
+ULOGD_CONF_OPT += --with-mysql=no
+endif
 
 $(eval $(autotools-package))
