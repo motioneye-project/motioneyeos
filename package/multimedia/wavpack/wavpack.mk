2c5b4ddc1932e7dedec1e412a5c5465f4fbe6f8b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2012-12-29 03:06:11 +0000

    wavpack: fix broken link on newer binutils
    Newer versions of binutils (2.22+) default to --no-copy-dt-needed hence
    all the used libraries must be explicitly named.
    This is accounted for in the source configure script but not in the
    compiled form so we just need to autoreconf it.
    
    [Peter: add comment why autoconf is needed]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/wavpack/wavpack.mk
+++ package/multimedia/wavpack/wavpack.mk
@@ -8,9 +8,10 @@ WAVPACK_VERSION = 4.60.1
 WAVPACK_SITE = http://www.wavpack.com
 WAVPACK_SOURCE = wavpack-$(WAVPACK_VERSION).tar.bz2
 WAVPACK_INSTALL_STAGING = YES
-
-ifneq ($(BR2_ENABLE_LOCALE),y)
-WAVPACK_DEPENDENCIES += libiconv
-endif
+# configure not up to date
+WAVPACK_AUTORECONF = YES
+WAVPACK_DEPENDENCIES = $(if $(BR2_ENABLE_LOCALE),libiconv)
+WAVPACK_LICENSE = BSD-3c
+WAVPACK_LICENSE_FILES = license.txt
 
 $(eval $(autotools-package))
