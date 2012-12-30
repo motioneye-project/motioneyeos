a025fd9b5a43f61f4b7ed051f6dcb422ec18cc83
Samuel Martin <s.martin49@gmail.com>
2012-12-30 11:33:26 +0000

    fluxbox: fix build against imlib2
    * fix configure.in to take care of the given imlib2-prefix
    * disable imlib2 support if imlib2 is not part of the selection
    
    [Peter: imlib2 needs to be built with X support]
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/fluxbox/fluxbox.mk
+++ package/fluxbox/fluxbox.mk
@@ -10,10 +10,18 @@ FLUXBOX_SITE = http://downloads.sourceforge.net/project/fluxbox/fluxbox/$(FLUXBO
 FLUXBOX_LICENSE = MIT
 FLUXBOX_LICENSE_FILES = COPYING
 
+FLUXBOX_AUTORECONF = YES
+
 FLUXBOX_CONF_OPT = --x-includes=$(STAGING_DIR)/usr/include/X11 \
 		   --x-libraries=$(STAGING_DIR)/usr/lib
-
 FLUXBOX_DEPENDENCIES = xlib_libX11 $(if $(BR2_PACKAGE_LIBICONV),libiconv)
+
+ifeq ($(BR2_PACKAGE_IMLIB2_X),y)
+FLUXBOX_CONF_OPT += --enable-imlib2 --with-imlib2-prefix=$(STAGING_DIR)/usr
+FLUXBOX_DEPENDENCIES += imlib2
+else
+FLUXBOX_CONF_OPT += --disable-imlib2
+endif
 
 define FLUXBOX_INSTALL_XSESSION_FILE
 	[ -f $(TARGET_DIR)/root/.xsession ] || $(INSTALL) -m 0755 -D \
