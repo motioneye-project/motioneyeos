9fb388c677e33d3031e62a13335e3bacfcc28f1d
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-01-02 01:26:37 +0000

    xapp_xman: replace := by =
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xman/xapp_xman.mk
+++ package/x11r7/xapp_xman/xapp_xman.mk
@@ -3,9 +3,9 @@
 # xapp_xman - Manual page display program for the X Window System
 #
 #############################################################
-XAPP_XMAN_VERSION:=1.0.3
-XAPP_XMAN_SOURCE:=xman-$(XAPP_XMAN_VERSION).tar.bz2
-XAPP_XMAN_SITE:=http://xorg.freedesktop.org/releases/individual/app
+XAPP_XMAN_VERSION = 1.0.3
+XAPP_XMAN_SOURCE = xman-$(XAPP_XMAN_VERSION).tar.bz2
+XAPP_XMAN_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XMAN_DEPENDENCIES = xlib_libXaw
 
 XAPP_XMAN_CONF_ENV = ac_cv_file__etc_man_conf=no \
