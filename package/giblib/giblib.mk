5a6f270344f7c6e857702bbd5591a62f59f12b2a
Stefan Fröberg <stefan.froberg@petroprogram.com>
2013-01-03 12:09:01 +0000

    giblib: fix giblib-config
    [Peter: use a single sed invocation]
    Signed-off-by: Stefan Fröberg <stefan.froberg@petroprogram.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/giblib/giblib.mk
+++ package/giblib/giblib.mk
@@ -13,4 +13,12 @@ GIBLIB_AUTORECONF_OPT = --install
 GIBLIB_CONF_OPT = --with-imlib2-prefix=$(STAGING_DIR)/usr \
 		  --with-imlib2-exec-prefix=$(STAGING_DIR)/usr
 
+define GIBLIB_STAGING_GIBLIB_CONFIG_FIXUP
+	$(SED) "s,^prefix=.*,prefix=\'$(STAGING_DIR)/usr\',g" \
+		-e "s,^exec_prefix=.*,exec_prefix=\'$(STAGING_DIR)/usr\',g" \
+		$(STAGING_DIR)/usr/bin/giblib-config
+endef
+
+GIBLIB_POST_INSTALL_STAGING_HOOKS += GIBLIB_STAGING_GIBLIB_CONFIG_FIXUP
+
 $(eval $(autotools-package))
