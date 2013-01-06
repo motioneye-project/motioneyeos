bfee0ba5dde011f44bcca9e08d35e244e35d0fe4
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-01-06 11:29:34 +0000

    metacity: fix X.org dependencies
    Metacity is a X client application, so there is no reason to depend on
    the X.org server. Instead, depend on the appropriate X libraries.
    
    In addition to the X.org related dependencies, we also add the
    host-libxml-parser-perl dependency, otherwise:
    
      checking for XML::Parser... configure: error: XML::Parser perl module is required for intltool
      make[1]: *** [/home/test/outputs/xorg/build/metacity-2.25.1/.stamp_configured] Error 1
      make: *** [all] Error 2
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/metacity/metacity.mk
+++ package/metacity/metacity.mk
@@ -17,7 +17,17 @@ METACITY_CONF_OPT = --x-includes=$(STAGING_DIR)/usr/include/X11 \
 		--disable-dependency-tracking \
 		--disable-sm --disable-startup-notification
 
-METACITY_DEPENDENCIES = libgtk2 xserver_xorg-server
+METACITY_DEPENDENCIES = libgtk2 \
+	xlib_libX11 \
+	host-libxml-parser-perl \
+	xlib_libXcomposite \
+	xlib_libXfixes \
+	xlib_libXrender \
+	xlib_libXdamage
+
+ifeq ($(BR2_PACKAGE_XLIB_LIBXCURSOR),y)
+METACITY_DEPENDENCIES += xlib_libXcursor
+endif
 
 define METACITY_INSTALL_XSESSION
 	install -D package/metacity/Xsession $(TARGET_DIR)/etc/X11/Xsession
