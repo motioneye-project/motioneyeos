ab0e905aaf440374b5059962ca8591197b2cbc54
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-01-06 11:29:37 +0000

    xvkbd: fix X.org dependencies
    xvkbd is a X.org client application, so there is no reason to depend
    on the X.org server. Instead, depend on the X.org client libraries
    that xvkbd actually links against.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/xvkbd/xvkbd.mk
+++ package/xvkbd/xvkbd.mk
@@ -10,7 +10,16 @@ XVKBD_SITE = http://homepage3.nifty.com/tsato/xvkbd
 XVKBD_MAKE_OPT = CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" LD="$(TARGET_CC)" \
 				CFLAGS="-O2 -I$(STAGING_DIR)/usr/include" USRLIBDIR="$(STAGING_DIR)/usr/lib"
 
-XVKBD_DEPENDENCIES = xserver_xorg-server xlib_libXaw xlib_libXtst
+XVKBD_DEPENDENCIES = \
+	xlib_libICE \
+	xlib_libSM \
+	xlib_libX11 \
+	xlib_libXaw \
+	xlib_libXext \
+	xlib_libXmu \
+	xlib_libXpm \
+	xlib_libXt \
+	xlib_libXtst
 
 $(eval $(autotools-package))
 
