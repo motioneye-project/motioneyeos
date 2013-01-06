4f83232e8d307f6184900c6e4a99a5f76dc66fa4
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-01-06 11:29:23 +0000

    libsha1: new package
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libsha1/libsha1.mk
@@ -0,0 +1,15 @@
+#############################################################
+#
+# libsha1
+#
+#############################################################
+
+LIBSHA1_VERSION = 0.3
+LIBSHA1_SITE = http://github.com/dottedmag/libsha1/tarball/$(LIBSHA1_VERSION)
+LIBSHA1_INSTALL_STAGING = YES
+
+# We're getting the source code from Github, so there is no generated
+# configure script in the tarball.
+LIBSHA1_AUTORECONF = YES
+
+$(eval $(autotools-package))
