e4d89e7f2de5f2586223466a48d8eefba3b0f216
Jan Pedersen <jp@jp-embedded.com>
2013-01-14 03:30:00 +0000

    infozip: new package
    This commit adds a package for infozip, compression and file
    packaging/archive utility.
    
    [Peter: fix trailing spaces in help, long line]
    Signed-off-by: Jan Pedersen <jp@jp-embedded.com>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/infozip/infozip.mk
@@ -0,0 +1,21 @@
+#############################################################
+#
+# infozip
+#
+#############################################################
+INFOZIP_VERSION = 30
+INFOZIP_SOURCE = zip$(INFOZIP_VERSION).tgz
+INFOZIP_SITE = ftp://ftp.info-zip.org/pub/infozip/src
+INFOZIP_LICENSE = Info-ZIP
+INFOZIP_LICENSE_FILES = LICENSE
+
+define INFOZIP_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) -f unix/Makefile generic
+endef
+
+define INFOZIP_INSTALL_TARGET_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) -f unix/Makefile install \
+		prefix=$(TARGET_DIR)/usr
+endef
+
+$(eval $(generic-package))
