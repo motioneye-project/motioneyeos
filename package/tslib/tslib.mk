4cf256ec4943fe35f488238dc90fd45ff509a783
Luca Ceresoli <luca@lucaceresoli.net>
2013-01-15 06:34:44 +0000

    tslib: fix license info
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tslib/tslib.mk
+++ package/tslib/tslib.mk
@@ -5,7 +5,7 @@
 #############################################################
 TSLIB_VERSION = 412d99d
 TSLIB_SITE = http://github.com/kergoth/tslib/tarball/master
-TSLIB_LICENSE = LGPL
+TSLIB_LICENSE = GPL, LGPL
 TSLIB_LICENSE_FILES = COPYING
 
 TSLIB_AUTORECONF = YES
