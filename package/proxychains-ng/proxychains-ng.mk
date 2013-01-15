10e058b0ee7fd8135fef35bcf24ae985707078c2
Stefan Fröberg <stefan.froberg@petroprogram.com>
2013-01-15 18:10:15 +0000

    proxychains-ng: copy proxychains.conf to target dir
    proxychains installation does not copy proxychains.conf to
    $(TARGET_DIR)/etc anymore so we have to do it manually now.
    
    Signed-off-by: Stefan Fröberg <stefan.froberg@petroprogram.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/proxychains-ng/proxychains-ng.mk
+++ package/proxychains-ng/proxychains-ng.mk
@@ -7,4 +7,11 @@ PROXYCHAINS_NG_VERSION = 4.4
 PROXYCHAINS_NG_SOURCE = proxychains-$(PROXYCHAINS_NG_VERSION).tar.bz2
 PROXYCHAINS_NG_SITE = http://downloads.sourceforge.net/project/proxychains-ng
 
+define PROXYCHAINS_NG_POST_INSTALL_TARGET
+	$(INSTALL) -m 644 -D $(@D)/src/proxychains.conf \
+		$(TARGET_DIR)/etc/proxychains.conf
+endef
+
+PROXYCHAINS_NG_POST_INSTALL_TARGET_HOOKS += PROXYCHAINS_NG_POST_INSTALL_TARGET
+
 $(eval $(autotools-package))
