e59117fe938990f82944f5ec0464330389b0ff0d
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-01-17 02:25:43 +0000

    vim: bump to version 7.3-762
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/vim/vim.mk
+++ package/vim/vim.mk
@@ -4,10 +4,10 @@
 #
 #############################################################
 
-# hg 21219ffc9790 = 7.3 release + patchlevel 487
 VIM_SITE = https://vim.googlecode.com/hg
 VIM_SITE_METHOD = hg
-VIM_VERSION = 21219ffc9790
+# 7.3 release patchlevel 762
+VIM_VERSION = 699f8d8f096d
 VIM_DEPENDENCIES = ncurses
 VIM_SUBDIR = src
 VIM_CONF_ENV = vim_cv_toupper_broken=no \
@@ -21,6 +21,8 @@ VIM_CONF_ENV = vim_cv_toupper_broken=no \
 		ac_cv_small_wchar_t=no
 # GUI/X11 headers leak from the host so forcibly disable them
 VIM_CONF_OPT = --with-tlib=ncurses --enable-gui=no --without-x
+VIM_LICENSE = Charityware
+VIM_LICENSE_FILES = README.txt
 
 define VIM_INSTALL_TARGET_CMDS
 	cd $(@D)/src; \
