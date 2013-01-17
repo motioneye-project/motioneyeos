7f2d0c82c287d6d6e4a5c76f2a32a1cc87585022
gilles.talis@gmail.com <gilles.talis@gmail.com>
2013-01-17 10:08:38 +0000

    sdl_gfx: bump version
    [Peter: use 2.0.23 instead of .24 because of build issues with .24]
    Signed-off-by: Gilles Talis <gilles.talis@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sdl_gfx/sdl_gfx.mk
+++ package/sdl_gfx/sdl_gfx.mk
@@ -3,9 +3,12 @@
 # SDL_gfx addon for SDL
 #
 #############################################################
-SDL_GFX_VERSION = 2.0.19
+
+SDL_GFX_VERSION = 2.0.23
 SDL_GFX_SOURCE = SDL_gfx-$(SDL_GFX_VERSION).tar.gz
 SDL_GFX_SITE = http://www.ferzkopp.net/Software/SDL_gfx-2.0/
+SDL_GFX_LICENSE = zlib
+SDL_GFX_LICENSE_FILES = COPYING LICENSE
 SDL_GFX_INSTALL_STAGING = YES
 SDL_GFX_DEPENDENCIES = sdl
 SDL_GFX_CONF_OPT = \
