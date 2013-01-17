13a81fa2caf32b4fa1a5452f308eed14e127a8f5
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-01-17 02:25:42 +0000

    libsigc: bump to version 2.2.11
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libsigc/libsigc.mk
+++ package/libsigc/libsigc.mk
@@ -3,10 +3,13 @@
 # libsigc++
 #
 #############################################################
-LIBSIGC_VERSION = 2.2.10
-LIBSIGC_SOURCE = libsigc++-$(LIBSIGC_VERSION).tar.bz2
+
+LIBSIGC_VERSION = 2.2.11
+LIBSIGC_SOURCE = libsigc++-$(LIBSIGC_VERSION).tar.xz
 LIBSIGC_SITE = http://ftp.gnome.org/pub/GNOME/sources/libsigc++/2.2
 LIBSIGC_INSTALL_STAGING = YES
+LIBSIGC_LICENSE = LGPLv2.1+
+LIBSIGC_LICENSE_FILES = COPYING
 
 define LIBSIGC_INSTALL_TARGET_FIXUP
 	rm -rf $(TARGET_DIR)/usr/share/devhelp/books/libsigc++*
