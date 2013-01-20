b4be060773ce3a7411509c034808df76ae98c104
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-01-20 09:22:35 +0000

    libogg: add license informations
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libogg/libogg.mk
+++ package/libogg/libogg.mk
@@ -5,8 +5,10 @@
 #############################################################
 LIBOGG_VERSION = 1.3.0
 LIBOGG_SITE = http://downloads.xiph.org/releases/ogg
-LIBOGG_INSTALL_STAGING = YES
+LIBOGG_LICENSE = BSD-3c
+LIBOGG_LICENSE_FILES = COPYING
 
+LIBOGG_INSTALL_STAGING = YES
 LIBOGG_DEPENDENCIES = host-pkgconf
 
 $(eval $(autotools-package))
