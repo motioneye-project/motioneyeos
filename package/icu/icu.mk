13ac891065e4c7295bd4ee12e56a08511e4c8446
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-01-20 09:22:33 +0000

    icu: add license informations
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/icu/icu.mk
+++ package/icu/icu.mk
@@ -7,6 +7,9 @@
 ICU_VERSION = 4.8.1.1
 ICU_SOURCE = icu4c-$(subst .,_,$(ICU_VERSION))-src.tgz
 ICU_SITE = http://download.icu-project.org/files/icu4c/$(ICU_VERSION)
+ICU_LICENSE = ICU License
+ICU_LICENSE_FILES = license.html
+
 ICU_DEPENDENCIES = host-icu
 ICU_INSTALL_STAGING = YES
 ICU_CONF_OPT = --with-cross-build=$(HOST_ICU_DIR)/source --disable-samples \
