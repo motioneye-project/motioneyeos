bcd02088b300c99b262be26a53f493ad0c82903b
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-01-20 09:22:36 +0000

    pkgconf: add license informations
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/pkgconf/pkgconf.mk
+++ package/pkgconf/pkgconf.mk
@@ -7,6 +7,8 @@
 PKGCONF_VERSION = 0.8.9
 PKGCONF_SITE = http://tortois.es/~nenolod/distfiles
 PKGCONF_SOURCE = pkgconf-$(PKGCONF_VERSION).tar.bz2
+PKGCONF_LICENSE = pkgconf license
+PKGCONF_LICENSE_FILES = COPYING
 
 PKG_CONFIG_HOST_BINARY = $(HOST_DIR)/usr/bin/pkg-config
 
