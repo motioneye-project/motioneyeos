ca005cdd214546c53d09233750b412896cc64d37
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-01-21 11:29:46 +0000

    sshfs: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sshfs/sshfs.mk
+++ package/sshfs/sshfs.mk
@@ -7,6 +7,8 @@
 SSHFS_VERSION = 2.4
 SSHFS_SITE = http://downloads.sourceforge.net/project/fuse/sshfs-fuse/$(SSHFS_VERSION)
 SSHFS_SOURCE = sshfs-fuse-$(SSHFS_VERSION).tar.gz
+SSHFS_LICENSE = GPLv2
+SSHFS_LICENSE_FILES = COPYING
 SSHFS_DEPENDENCIES = \
 	libglib2 libfuse openssh \
 	$(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext) \
