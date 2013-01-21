30a198196a40ce469cc53f1e261a27acd35b8968
Bogdan Radulescu <bogdan@nimblex.net>
2013-01-21 10:40:41 +0000

    iftop: new package
    Added the iftop package which is really useful to see quickly
    what uses the bandwidth on your machine.
    
    [Peter: move to 'Networking applications', fix license]
    Signed-off-by: Bogdan Radulescu <bogdan@nimblex.net>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/iftop/iftop.mk
@@ -0,0 +1,15 @@
+#############################################################
+#
+# iftop
+#
+#############################################################
+
+IFTOP_VERSION = 1.0pre2
+IFTOP_SITE = http://www.ex-parrot.com/pdw/iftop/download/
+
+IFTOP_DEPENDENCIES = ncurses libpcap
+
+IFTOP_LICENSE = GPLv2+
+IFTOP_LICENSE_FILES = COPYING
+
+$(eval $(autotools-package))
