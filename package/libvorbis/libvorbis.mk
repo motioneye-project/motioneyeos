5a016d2148816961b27bbe05bd869024fccf02f7
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-01-21 11:29:49 +0000

    libvorbis: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libvorbis/libvorbis.mk
+++ package/libvorbis/libvorbis.mk
@@ -8,9 +8,9 @@ LIBVORBIS_VERSION = 1.3.3
 LIBVORBIS_SOURCE = libvorbis-$(LIBVORBIS_VERSION).tar.gz
 LIBVORBIS_SITE = http://downloads.xiph.org/releases/vorbis/$(LIBVORBIS-SOURCE)
 LIBVORBIS_INSTALL_STAGING = YES
-
 LIBVORBIS_CONF_OPT = --disable-oggtest
-
 LIBVORBIS_DEPENDENCIES = host-pkgconf libogg
+LIBVORBIS_LICENSE = BSD-3c
+LIBVORBIS_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
