7a52cda9bd494f6a2530752b2d97199cd7891b69
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-01-22 03:35:50 +0000

    sdparm: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sdparm/sdparm.mk
+++ package/sdparm/sdparm.mk
@@ -1,5 +1,7 @@
 SDPARM_VERSION = 1.07
 SDPARM_SOURCE = sdparm-$(SDPARM_VERSION).tgz
 SDPARM_SITE = http://sg.danny.cz/sg/p/
+SDPARM_LICENSE = BSD-3c
+SDPARM_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
