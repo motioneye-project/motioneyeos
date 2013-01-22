db7a831da184e2fa2ed1f43ef528b81d2023d986
Peter Korsgaard <jacmet@sunsite.dk>
2013-01-22 15:55:55 +0100

    package: add bustle dbus monitor package
    Only builds the capture tool, not the (host) graphical viewer.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/bustle/bustle.mk
@@ -0,0 +1,23 @@
+#############################################################
+#
+# bustle
+#
+#############################################################
+
+BUSTLE_VERSION = 0.4.2
+BUSTLE_SITE = http://www.willthompson.co.uk/bustle/releases/$(BUSTLE_VERSION)/
+BUSTLE_LICENSE = LGPLv2.1+
+BUSTLE_LICENSE_FILES = LICENSE
+BUSTLE_DEPENDENCIES = libglib2 libpcap host-pkgconf
+
+define BUSTLE_BUILD_CMDS
+	PATH=$(TARGET_PATH) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
+		PCAP_FLAGS='-lpcap' -C $(@D) dist/build/bustle-pcap
+endef
+
+define BUSTLE_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/dist/build/bustle-pcap \
+		$(TARGET_DIR)/usr/bin/bustle-pcap
+endef
+
+$(eval $(generic-package))
