a58506aa0863a888863f2e9c77f6cd729cb0bd11
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-01-25 02:50:39 +0000

    flex: bump to version 2.5.37
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/flex/flex.mk
+++ package/flex/flex.mk
@@ -4,12 +4,8 @@
 #
 #############################################################
 
-FLEX_VERSION = 2.5.35
-FLEX_PATCH_VERSION = 10
-FLEX_SOURCE = flex_$(FLEX_VERSION).orig.tar.gz
-FLEX_PATCH = flex_$(FLEX_VERSION)-$(FLEX_PATCH_VERSION).diff.gz
-FLEX_SITE = $(BR2_DEBIAN_MIRROR)/debian/pool/main/f/flex
-FLEX_DIR = $(BUILD_DIR)/flex-$(FLEX_VERSION)
+FLEX_VERSION = 2.5.37
+FLEX_SITE = http://download.sourceforge.net/project/flex
 FLEX_INSTALL_STAGING = YES
 FLEX_LICENSE = FLEX
 FLEX_LICENSE_FILES = COPYING
