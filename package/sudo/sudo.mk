41a221332682db9902ca1572d548e56cda87f260
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-01-25 02:50:40 +0000

    sudo: bump to version 1.8.6p4
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sudo/sudo.mk
+++ package/sudo/sudo.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SUDO_VERSION = 1.8.6
+SUDO_VERSION = 1.8.6p4
 SUDO_SITE    = http://www.sudo.ws/sudo/dist
 SUDO_LICENSE = ICS BSD-3c
 SUDO_LICENSE_FILES = doc/LICENSE
