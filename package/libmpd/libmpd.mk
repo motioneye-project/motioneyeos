563ffadac1d1d86d81f607b7420cad637b515b32
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-01-29 08:23:54 +0000

    libmpd: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libmpd/libmpd.mk
+++ package/libmpd/libmpd.mk
@@ -8,5 +8,7 @@ LIBMPD_VERSION = 11.8.17
 LIBMPD_SITE = http://download.sarine.nl/Programs/gmpc/11.8.16
 LIBMPD_INSTALL_STAGING = YES
 LIBMPD_DEPENDENCIES = libglib2
+LIBMPD_LICENSE = GPLv2+
+LIBMPD_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
