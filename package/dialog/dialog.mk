4ec86f27b2d5790ee3e4775a239b1b72906c4dce
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-01-29 08:23:51 +0000

    dialog: add license information
    [Peter: License is LGPLv2.1]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/dialog/dialog.mk
+++ package/dialog/dialog.mk
@@ -10,6 +10,8 @@ DIALOG_SITE = ftp://invisible-island.net/dialog
 DIALOG_CONF_OPT = --with-ncurses
 DIALOG_CONF_ENV = ac_cv_path_NCURSES_CONFIG=true LIBS=-lncurses
 DIALOG_DEPENDENCIES = ncurses
+DIALOG_LICENSE = LGPLv2.1
+DIALOG_LICENSE_FILES = COPYING
 
 ifneq ($(BR2_ENABLE_LOCALE),y)
 DIALOG_DEPENDENCIES+=libiconv
