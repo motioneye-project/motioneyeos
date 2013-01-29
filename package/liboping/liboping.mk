d780f83a5a882af066e865119e2697b5754ad69a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-01-29 08:23:57 +0000

    liboping: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/liboping/liboping.mk
+++ package/liboping/liboping.mk
@@ -9,5 +9,7 @@ LIBOPING_SITE = http://verplant.org/liboping/files
 LIBOPING_INSTALL_STAGING = YES
 LIBOPING_DEPENDENCIES = $(if $(BR2_PACKAGE_NCURSES),ncurses)
 LIBOPING_CONF_OPT = --without-perl-bindings
+LIBOPING_LICENSE = LGPLv2.1+ GPLv2
+LIBOPING_LICENSE_FILES = COPYING.LIB
 
 $(eval $(autotools-package))
