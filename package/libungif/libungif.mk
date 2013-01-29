706a016b29f6fe11c996cd61ab5a8b66cbc6b5e1
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-01-29 08:23:56 +0000

    libungif: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libungif/libungif.mk
+++ package/libungif/libungif.mk
@@ -9,6 +9,8 @@ LIBUNGIF_SOURCE = libungif-$(LIBUNGIF_VERSION).tar.bz2
 LIBUNGIF_SITE = http://downloads.sourceforge.net/project/giflib/libungif-4.x/libungif-$(LIBUNGIF_VERSION)
 LIBUNGIF_INSTALL_STAGING = YES
 LIBUNGIF_CONF_OPT = --without-x
+LIBUNGIF_LICENSE = MIT
+LIBUNGIF_LICENSE_FILES = COPYING
 
 LIBUNGIF_BINS = gif2epsn gif2ps gif2rgb gif2x11 gifasm gifbg gifburst gifclip \
 		gifclrmp gifcolor gifcomb gifcompose giffiltr giffix gifflip  \
