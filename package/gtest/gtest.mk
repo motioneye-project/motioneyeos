a42e10c61b19a6054d10fc809460c7e23c46fde7
Stephan Hoffmann <sho@relinux.de>
2013-02-05 08:16:08 +0000

    New package: gtest
    Google's framework for writing C++ tests on a variety of platforms (Linux,
    Mac OS X, Windows, Cygwin, Windows CE, and Symbian). Based on the xUnit
    architecture. Supports automatic test discovery, a rich set of assertions,
    user-defined assertions, death tests, fatal and non-fatal failures, value-
    and type-parameterized tests, various options for running the tests, and XML
    test report generation.
    
    Gtest also allows to easily build testsuites for C programs.
    
    This package allows running testsuites on the target which might be
    advantgeous in certain cases.
    
    http://code.google.com/p/googletest/
    
    [Peter: Tweak Config.in, use GTEST_VERSION in _SOURCE]
    Signed-off-by: Stephan Hoffmann <sho@relinux.de>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/gtest/gtest.mk
@@ -0,0 +1,23 @@
+#############################################################
+#
+# googletest
+#
+#############################################################
+
+GTEST_VERSION = 1.6.0
+GTEST_SOURCE = gtest-$(GTEST_VERSION).zip
+GTEST_SITE = http://googletest.googlecode.com/files/
+GTEST_INSTALL_STAGING = YES
+GTEST_INSTALL_TARGET = NO
+
+define GTEST_EXTRACT_CMDS
+	unzip $(DL_DIR)/$(GTEST_SOURCE) -d $(BUILD_DIR)
+endef
+
+define GTEST_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/libgtest.a $(STAGING_DIR)/usr/lib/libgtest.a
+	$(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/include/gtest/
+	cp -rp $(@D)/include/gtest/* $(STAGING_DIR)/usr/include/gtest/
+endef
+
+$(eval $(cmake-package))
