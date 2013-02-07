5ff6989445eac9ce59a5041c656bcb0319dce0eb
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-02-07 12:35:07 +0000

    libgpg-error: use <pkg>_CONFIG_SCRIPTS
    libgpg-error installs a gpg-error-config script, but it wasn't fixed
    up properly. Use the <pkg>_CONFIG_SCRIPTS to handle this.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Acked-by: "Samuel Martin" <s.martin49@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libgpg-error/libgpg-error.mk
+++ package/libgpg-error/libgpg-error.mk
@@ -9,5 +9,6 @@ LIBGPG_ERROR_SITE = ftp://ftp.gnupg.org/gcrypt/libgpg-error
 LIBGPG_ERROR_LICENSE = LGPLv2.1+
 LIBGPG_ERROR_LICENSE_FILES = COPYING.LIB
 LIBGPG_ERROR_INSTALL_STAGING = YES
+LIBGPG_ERROR_CONFIG_SCRIPTS = gpg-error-config
 
 $(eval $(autotools-package))
