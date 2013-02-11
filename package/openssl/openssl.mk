0ff2d0f74b311c5b235a1294521e0e80bb82af2b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-02-11 15:37:54 +0000

    openssl: bump to version 1.0.1e
    Fixes some regressions introduced by 1.0.1d
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-OPENSSL_VERSION = 1.0.1d
+OPENSSL_VERSION = 1.0.1e
 OPENSSL_SITE = http://www.openssl.org/source
 OPENSSL_LICENSE = OpenSSL or SSLeay
 OPENSSL_LICENSE_FILES = LICENSE
