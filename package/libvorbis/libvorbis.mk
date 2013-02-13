933e0ffe2ca1613e7b98deac61d8f3a94485b712
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-02-13 11:55:56 +0000

    libvorbis: fix LIBVORBIS_SITE
    The LIBVORBIS_SITE included a reference to $(LIBVORBIS-SOURCE), which
    doesn't exist. And anyway was useless because the <pkg>_SITE variable
    should not contain the name of the tarball.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libvorbis/libvorbis.mk
+++ package/libvorbis/libvorbis.mk
@@ -6,7 +6,7 @@
 
 LIBVORBIS_VERSION = 1.3.3
 LIBVORBIS_SOURCE = libvorbis-$(LIBVORBIS_VERSION).tar.gz
-LIBVORBIS_SITE = http://downloads.xiph.org/releases/vorbis/$(LIBVORBIS-SOURCE)
+LIBVORBIS_SITE = http://downloads.xiph.org/releases/vorbis/
 LIBVORBIS_INSTALL_STAGING = YES
 LIBVORBIS_CONF_OPT = --disable-oggtest
 LIBVORBIS_DEPENDENCIES = host-pkgconf libogg
