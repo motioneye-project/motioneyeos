e3b3be6bb63e9cf5d2465ff55c01c24299f0caed
Baruch Siach <baruch@tkos.co.il>
2013-02-17 20:18:11 +0000

    wireshark: new package
    This is only the bare minimum needed to build the textual tshark utility.
    Support for more options, including the wireshark GUI, can be added later.
    
    [Peter: point includedir at staging to workaround host/target headers issue]
    Cc: Stefan Fröberg <stefan.froberg@petroprogram.com>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Acked-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Tested-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/wireshark/wireshark.mk
@@ -0,0 +1,23 @@
+#############################################################
+#
+# wireshark
+#
+#############################################################
+
+WIRESHARK_VERSION = 1.8.5
+WIRESHARK_SOURCE = wireshark-$(WIRESHARK_VERSION).tar.bz2
+WIRESHARK_SITE = http://www.wireshark.org/download/src/all-versions
+WIRESHARK_LICENSE = wireshark license
+WIRESHARK_LICENSE_FILES = COPYING
+WIRESHARK_DEPENDENCIES = libpcap libgcrypt libglib2
+WIRESHARK_AUTORECONF = YES
+WIRESHARK_CONF_ENV = LIBGCRYPT_CONFIG=$(STAGING_DIR)/usr/bin/libgcrypt-config \
+			ac_cv_path_PCAP_CONFIG=$(STAGING_DIR)/usr/bin/pcap-config
+
+# wireshark adds -I$includedir to CFLAGS, causing host/target headers mixup.
+# Work around it by pointing includedir at staging
+WIRESHARK_CONF_OPT = --disable-wireshark --without-krb5 --disable-usr-local \
+		     --enable-static=no --with-gnutls=no \
+		     --includedir=$(STAGING_DIR)/usr/include
+
+$(eval $(call autotools-package))
