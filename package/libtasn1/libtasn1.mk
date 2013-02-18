e4d32a7a465e98770cb1da7afddf4b2d6b154790
Stefan Fröberg <stefan.froberg@petroprogram.com>
2013-02-18 10:28:58 +0000

    new package: libtasn1
    Libtasn1 is the ASN.1 library used by GnuTLS, GNU Shishi and some
    other packages. It was written by Fabio Fiorina, and has been
    shipped as part of GnuTLS for some time but is now a proper
    GNU package.
    
    [Peter: Both licenses are '+']
    Signed-off-by: Stefan Fröberg <stefan.froberg@petroprogram.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libtasn1/libtasn1.mk
@@ -0,0 +1,14 @@
+#############################################################
+#
+# libtasn1
+#
+#############################################################
+
+LIBTASN1_VERSION = 3.2
+LIBTASN1_SITE = http://ftp.gnu.org/gnu/libtasn1
+LIBTASN1_DEPENDENCIES = host-bison
+LIBTASN1_LICENSE = GPLv3+ LGPLv2.1+
+LIBTASN1_LICENSE_FILES = COPYING COPYING.LIB
+LIBTASN1_INSTALL_STAGING = YES
+
+$(eval $(autotools-package))
