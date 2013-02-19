b7988b0fda1ceaacd7b23b2b98065a537c3cd87c
Andreas Wetzel <andreas.wetzel@nanotronic.ch>
2013-02-19 02:07:33 +0000

    bump json-c to 0.10
    this patch bumps json-c to version 0.10 and adapts the no-reentrant patch
    to the new version.
    
    Signed-off-by: Andreas Wetzel <andreas.wetzel@nanotronic.ch>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/json-c/json-c.mk
+++ package/json-c/json-c.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-JSON_C_VERSION = 0.9
-JSON_C_SITE = http://oss.metaparadigm.com/json-c/
+JSON_C_VERSION = 0.10
+JSON_C_SITE = https://github.com/downloads/json-c/json-c
 JSON_C_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
