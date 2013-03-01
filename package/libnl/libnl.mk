fca82fceec87ddf5e2267471d67ea0d906acefa3
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-03-01 00:23:25 +0000

    libnl: bump to version 3.2.21
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libnl/libnl.mk
+++ package/libnl/libnl.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBNL_VERSION = 3.2.18
+LIBNL_VERSION = 3.2.21
 LIBNL_SITE = http://www.infradead.org/~tgr/libnl/files
 LIBNL_LICENSE = LGPLv2.1+
 LIBNL_LICENSE_FILES = COPYING
