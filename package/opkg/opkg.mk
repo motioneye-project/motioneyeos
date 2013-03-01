d31db334c3ad219c5a55aece8602880c7eb732ef
Philipp Claves <claves@budelmann-elektronik.com>
2013-03-01 14:38:29 +0100

    opkg: Add gnupg signature checking support.
    [Thomas: add ac_cv_path_GPGME_CONFIG in the configure environment to
    help opkg find libgpgme.]
    
    Signed-off-by: Philipp Claves <claves@budelmann-elektronik.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/opkg/opkg.mk
+++ package/opkg/opkg.mk
@@ -9,7 +9,7 @@ OPKG_SOURCE = opkg-$(OPKG_VERSION).tar.gz
 OPKG_SITE = http://opkg.googlecode.com/svn/trunk/
 OPKG_SITE_METHOD = svn
 OPKG_INSTALL_STAGING = YES
-OPKG_CONF_OPT = --disable-curl --disable-gpg
+OPKG_CONF_OPT = --disable-curl
 OPKG_AUTORECONF = YES
 # Uses PKG_CHECK_MODULES() in configure.ac
 OPKG_DEPENDENCIES = host-pkgconf
@@ -18,6 +18,14 @@ OPKG_DEPENDENCIES = host-pkgconf
 define OPKG_CREATE_LOCKDIR
 	mkdir -p $(TARGET_DIR)/usr/lib/opkg
 endef
+
+ifeq ($(BR2_PACKAGE_OPKG_GPG_SIGN),y)
+OPKG_CONF_OPT += --enable-gpg
+OPKG_CONF_ENV = ac_cv_path_GPGME_CONFIG=$(STAGING_DIR)/usr/bin/gpgme-config
+OPKG_DEPENDENCIES += libgpgme
+else
+OPKG_CONF_OPT += --disable-gpg
+endif
 
 OPKG_POST_INSTALL_TARGET_HOOKS += OPKG_CREATE_LOCKDIR
 
