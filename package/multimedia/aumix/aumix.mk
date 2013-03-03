4e7d5d0a93114ed49095079f66124dd163e23f9c
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2013-03-03 08:37:52 +0000

    aumix.mk: fix whitespace
    No functional change.
    
    [Peter: adjust commit message]
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/aumix/aumix.mk
+++ package/multimedia/aumix/aumix.mk
@@ -4,10 +4,10 @@
 #
 #############################################################
 
-AUMIX_VERSION=2.8
-AUMIX_SOURCE=aumix-$(AUMIX_VERSION).tar.bz2
-AUMIX_SITE=http://jpj.net/~trevor/aumix/releases
-AUMIX_AUTORECONF=YES
+AUMIX_VERSION = 2.8
+AUMIX_SOURCE = aumix-$(AUMIX_VERSION).tar.bz2
+AUMIX_SITE = http://jpj.net/~trevor/aumix/releases
+AUMIX_AUTORECONF = YES
 
 AUMIX_CONF_OPT = \
 	--without-gtk \
