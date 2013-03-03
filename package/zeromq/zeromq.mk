eaf746eb9e8b10d5ff5011a989014b3df4b31ddc
Alexander Lukichev <alexander.lukichev@gmail.com>
2013-03-03 23:48:12 +0000

    zeromq: add PGM/EPGM support
    This adds support for Pragmatic General Multicast protocol on raw
    IP (PGM, RFC 3208) or UDP frames (EPGM) for use as zeromq reliable
    multicast transport. The library relies on openpgm package to
    implement the protocol itself.
    
    Signed-off-by: Alexander Lukichev <alexander.lukichev@gmail.com>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/zeromq/zeromq.mk
+++ package/zeromq/zeromq.mk
@@ -11,4 +11,9 @@ ZEROMQ_DEPENDENCIES = util-linux
 ZEROMQ_LICENSE = LGPLv3+ with exceptions
 ZEROMQ_LICENSE_FILES = COPYING COPYING.LESSER
 
+ifeq ($(BR2_PACKAGE_ZEROMQ_PGM),y)
+	ZEROMQ_DEPENDENCIES += host-pkgconf openpgm
+	ZEROMQ_CONF_OPT = --with-system-pgm
+endif
+
 $(eval $(autotools-package))
