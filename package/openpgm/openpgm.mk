e019b10ac25e0876b4706a584842be4595b8fd2b
Alexander Lukichev <alexander.lukichev@espotel.com>
2013-03-03 23:48:11 +0000

    openpgm: new package
    OpenPGM is an open source implementation of the Pragmatic General
    Multicast (PGM) specification in RFC 3208 available at www.ietf.org.
    It is required for PGM/EPGM support in ZeroMQ library.
    
    Signed-off-by: Alexander Lukichev <alexander.lukichev@gmail.com>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/openpgm/openpgm.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# openpgm
+#
+#############################################################
+
+OPENPGM_VERSION = 5.1.118~dfsg
+OPENPGM_SOURCE = libpgm-$(OPENPGM_VERSION).tar.gz
+OPENPGM_SITE = http://openpgm.googlecode.com/files/
+OPENPGM_LICENSE = LGPLv2.1+
+OPENPGM_LICENSE_FILES = openpgm/pgm/LICENSE
+OPENPGM_INSTALL_STAGING = YES
+OPENPGM_AUTORECONF = YES
+OPENPGM_SUBDIR = openpgm/pgm/
+OPENPGM_CONF_ENV = ac_cv_file__proc_cpuinfo=yes ac_cv_file__dev_rtc=no \
+                   ac_cv_file__dev_hpet=no
+
+$(eval $(autotools-package))
