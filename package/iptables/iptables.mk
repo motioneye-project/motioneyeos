d4f49fc1fef5cc0d5f42d035b48c96ad941c9001
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-03-04 06:50:40 +0000

    iptables: bump to version 1.4.18
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/iptables/iptables.mk
+++ package/iptables/iptables.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-IPTABLES_VERSION = 1.4.17
+IPTABLES_VERSION = 1.4.18
 IPTABLES_SOURCE = iptables-$(IPTABLES_VERSION).tar.bz2
 IPTABLES_SITE = http://ftp.netfilter.org/pub/iptables
 IPTABLES_INSTALL_STAGING = YES
