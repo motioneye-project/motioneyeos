3ceb91e8b14233f4b40c3acb14384f5898461f95
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-03-04 06:00:00 +0000

    ethtool: bump to version 3.8
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ETHTOOL_VERSION = 3.7
+ETHTOOL_VERSION = 3.8
 ETHTOOL_SITE = $(BR2_KERNEL_MIRROR)/software/network/ethtool
 ETHTOOL_LICENSE = GPLv2
 ETHTOOL_LICENSE_FILES = COPYING
