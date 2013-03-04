6c1df894a972bb60a0a1863710f71807e7307343
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-03-04 06:50:36 +0000

    libnetfilter_acct: bump to version 1.0.2
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libnetfilter_acct/libnetfilter-acct.mk
+++ package/libnetfilter_acct/libnetfilter-acct.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-LIBNETFILTER_ACCT_VERSION = 1.0.1
+LIBNETFILTER_ACCT_VERSION = 1.0.2
 LIBNETFILTER_ACCT_SOURCE = libnetfilter_acct-$(LIBNETFILTER_ACCT_VERSION).tar.bz2
 LIBNETFILTER_ACCT_SITE = http://www.netfilter.org/projects/libnetfilter_acct/files
 LIBNETFILTER_ACCT_INSTALL_STAGING = YES
