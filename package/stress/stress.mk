1b9a4f1e91ac98d836775c293acb9a947b846de1
roylee17 <roylee17@gmail.com>
2013-03-04 07:47:20 +0000

    stress: honor the BR2_PREFER_STATIC_LIB preference
    Stress is linked statically if the --enable-static is specified.
    However, this option is always specified in the global
    SHARED_STATIC_LIBS_OPTS to tell packages to build static libraries,
    if supported.
    
    If the BR2_PREFER_STATIC_LIB is not defined, we have to specify
    --disable-static explicitly to get stress linked dynamically.
    
    Signed-off-by: Tzu-Jung Lee <tjlee@ambarella.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/stress/stress.mk
+++ package/stress/stress.mk
@@ -10,4 +10,14 @@ STRESS_VERSION = 1.0.4
 STRESS_SITE    = http://weather.ou.edu/~apw/projects/stress
 STRESS_AUTORECONF = YES
 
+# Stress is linked statically if the --enable-static is specified.
+# However, this option is always specified in the global
+# SHARED_STATIC_LIBS_OPTS to tell packages to build static libraries,
+# if supported.
+#
+# If the BR2_PREFER_STATIC_LIB is not defined, we have to specify
+# --disable-static explicitly to get stress linked dynamically.
+STRESS_CONF_OPT = \
+	$(if $(BR2_PREFER_STATIC_LIB),,--disable-static)
+
 $(eval $(autotools-package))
