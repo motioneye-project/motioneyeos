a9b0023c5b2c41002aae8826a63e1127cb6d846d
Luca Ceresoli <luca@lucaceresoli.net>
2013-03-06 07:14:22 +0000

    cmake: convert spaces to tabs in make rules
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/cmake/cmake.mk
+++ package/cmake/cmake.mk
@@ -10,19 +10,19 @@ CMAKE_LICENSE = BSD-3c
 CMAKE_LICENSE_FILES = Copyright.txt
 
 define HOST_CMAKE_CONFIGURE_CMDS
- (cd $(@D); \
-	LDFLAGS="$(HOST_LDFLAGS)" \
-	CFLAGS="$(HOST_CFLAGS)" \
-	./bootstrap --prefix=$(HOST_DIR)/usr --parallel=$(PARALLEL_JOBS) \
- )
+	(cd $(@D); \
+		LDFLAGS="$(HOST_LDFLAGS)" \
+		CFLAGS="$(HOST_CFLAGS)" \
+		./bootstrap --prefix=$(HOST_DIR)/usr --parallel=$(PARALLEL_JOBS) \
+	)
 endef
 
 define HOST_CMAKE_BUILD_CMDS
- $(HOST_MAKE_ENV) $(MAKE) -C $(@D)
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D)
 endef
 
 define HOST_CMAKE_INSTALL_CMDS
- $(HOST_MAKE_ENV) $(MAKE) -C $(@D) install
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) install
 endef
 
 $(eval $(generic-package))
