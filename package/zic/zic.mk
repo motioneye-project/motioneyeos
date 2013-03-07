8d13f6718742553b144946d56205ab23fe1f2972
Richard Braun <rbraun@sceen.net>
2013-03-07 11:53:40 +0000

    package/zic: new host package
    This is the zone information compiler, used to compile the time zone
    database.
    
    Signed-off-by: Richard Braun <rbraun@sceen.net>
    [yann.morin.1998@free.fr: export ZIC for all to use]
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/zic/zic.mk
@@ -0,0 +1,29 @@
+#############################################################
+#
+# zic
+#
+#############################################################
+
+ZIC_VERSION = 2012j
+ZIC_SOURCE = tzcode$(ZIC_VERSION).tar.gz
+ZIC_SITE = http://www.iana.org/time-zones/repository/releases
+ZIC_LICENSE = Public domain
+
+# Don't strip any path components during extraction.
+define HOST_ZIC_EXTRACT_CMDS
+	gzip -d -c $(DL_DIR)/$(ZIC_SOURCE) \
+		| $(TAR) --strip-components=0 -C $(@D) -xf -
+endef
+
+define HOST_ZIC_BUILD_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) zic
+endef
+
+define HOST_ZIC_INSTALL_CMDS
+	mkdir -p $(HOST_DIR)/usr/sbin
+	install -D -m 755 $(@D)/zic $(HOST_DIR)/usr/sbin/zic
+endef
+
+$(eval $(host-generic-package))
+
+ZIC = $(HOST_DIR)/usr/sbin/zic
