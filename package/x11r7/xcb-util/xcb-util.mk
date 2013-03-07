70f99d80ef87b1de02fe0f4353128dc9f5101409
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-03-07 10:18:29 +0000

    xcb-util: add dependency on libxcb
    The xcb-util lacks a dependency on libxcb. Without this dependency, it
    fails to build with:
    
    checking for XCB... no
    configure: error: in `/home/thomas/projets/buildroot/output/build/xcb-util-0.3.9':
    configure: error: The pkg-config script could not be found or is too old.  Make sure it
    is in your PATH or set the PKG_CONFIG environment variable to the full
    path to pkg-config.
    
    Alternatively, you may set the environment variables XCB_CFLAGS
    and XCB_LIBS to avoid the need to call pkg-config.
    See the pkg-config man page for more details.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xcb-util/xcb-util.mk
+++ package/x11r7/xcb-util/xcb-util.mk
@@ -7,7 +7,7 @@ XCB_UTIL_VERSION = 0.3.9
 XCB_UTIL_SOURCE = xcb-util-$(XCB_UTIL_VERSION).tar.bz2
 XCB_UTIL_SITE = http://xcb.freedesktop.org/dist/
 XCB_UTIL_INSTALL_STAGING = YES
-XCB_UTIL_DEPENDENCIES = host-gperf
+XCB_UTIL_DEPENDENCIES = host-gperf libxcb
 
 $(eval $(autotools-package))
 
