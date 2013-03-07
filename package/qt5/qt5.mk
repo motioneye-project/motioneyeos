1737b5648c9f778149eb7da5897b66a9577d22c4
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-03-07 10:18:37 +0000

    qt5: add macro to fixup Qt5 .la and .prl files
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/qt5/qt5.mk
+++ package/qt5/qt5.mk
@@ -1,1 +1,11 @@
 include package/qt5/*/*.mk
+
+define QT5_LA_PRL_FILES_FIXUP
+	for i in $$(find $(STAGING_DIR)/usr/lib* -name "libQt5*.la"); do \
+		$(SED) "s:\(['= ]\)/usr:\\1$(STAGING_DIR)/usr:g" $$i; \
+		$(SED) "/^dependency_libs=/s%-L/usr/lib %%g" $$i ; \
+	done
+	for i in $$(find $(STAGING_DIR)/usr/lib* -name "libQt5*.prl"); do \
+		$(SED) "s%-L/usr/lib%%" $$i; \
+	done
+endef
