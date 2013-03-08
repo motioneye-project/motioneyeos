ef123e427878ee02fdaf8b22c9f9ba419eab758a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-03-08 08:22:32 +0000

    ser2net: bump to version 2.8
    Bump ser2net to version 2.8.
    
    Exclude for nommu (uses fork).
    Version 2.7 also did, guess the autobuilders never caught it.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ser2net/ser2net.mk
+++ package/ser2net/ser2net.mk
@@ -4,9 +4,8 @@
 #
 #############################################################
 
-SER2NET_VERSION = 2.7
-SER2NET_SOURCE = ser2net-$(SER2NET_VERSION).tar.gz
-SER2NET_SITE = http://downloads.sourceforge.net/project/ser2net/ser2net/$(SER2NET_VERSION)
+SER2NET_VERSION = 2.8
+SER2NET_SITE = http://downloads.sourceforge.net/project/ser2net/ser2net
 SER2NET_LICENSE = GPLv2+
 SER2NET_LICENSE_FILES = COPYING
 
