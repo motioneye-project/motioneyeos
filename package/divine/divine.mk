3fe23510961fe44db2bf3747163b1a8125ea5be4
Carsten Schoenert <c.schoenert@gmail.com>
2013-03-10 08:32:43 +0000

    divine: bumping version to 1.6.3
    To be identical to the version of directfb switching also to version
    1.6.3. The divine package needs at minimum directfb 1.6.0 as
    dependencie.
    Adding the license information to the package divine.
    Also switch to Git for getting the package source because the tar.gz
    for version 1.6.3 on directfb.org is broken because of a missed header
    file inside.
    
    [Peter: tweak comments]
    Signed-off-by: Carsten Schoenert <c.schoenert@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/divine/divine.mk
+++ package/divine/divine.mk
@@ -4,11 +4,16 @@
 #
 #############################################################
 
-DIVINE_VERSION = 0.4.0
-DIVINE_SITE = http://www.directfb.org/downloads/Extras
-DIVINE_SOURCE = DiVine-$(DIVINE_VERSION).tar.gz
+# tagged version 1.6.3, tarball is missing files
+DIVINE_VERSION = 83cafc257a42b9465cd9d6185bf66b8c1b7ed704
+DIVINE_SITE = git://git.directfb.org/git/directfb/extras/DiVine.git
+DIVINE_LICENSE = LGPLv2.1+
+DIVINE_LICENSE_FILE = COPYING
 DIVINE_INSTALL_STAGING = YES
 DIVINE_DEPENDENCIES = directfb
 DIVINE_CONFIG_SCRIPTS = divine-config
+
+# package has no configure script so we have to generate it
+DIVINE_AUTORECONF = YES
 
 $(eval $(autotools-package))
