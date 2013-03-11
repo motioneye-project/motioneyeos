976e3b2dc4db12a6ac20dbc6d5a523c2929ebdde
Samuel Martin <s.martin49@gmail.com>
2013-03-11 18:58:41 +0000

    urg: new package
    [Peter: drop 'library' from name]
    Cc: Eric Jarrige <eric.jarrige@armadeus.org>
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/urg/urg.mk
@@ -0,0 +1,27 @@
+#############################################################
+#
+# urg library
+#
+#############################################################
+
+URG_VERSION = 0.8.18
+URG_SITE = http://www.hokuyo-aut.jp/02sensor/07scanner/download/urg_programs_en/
+URG_SOURCE = urg-$(URG_VERSION).zip
+URG_LICENSE = LGPLv3+
+URG_LICENSE_FILES = COPYING
+
+URG_INSTALL_STAGING = YES
+
+URG_DEPENDENCIES = sdl sdl_net
+
+URG_CONF_OPT = --with-sdl-prefix=$(STAGING_DIR)/usr \
+		--with-sdl-exec-prefix=$(STAGING_DIR)/usr
+
+define URG_EXTRACT_CMDS
+	$(RM) -rf $(URG_DIR)
+	unzip -q -d $(BUILD_DIR)/ $(DL_DIR)/$(URG_SOURCE)
+	test -d $(URG_DIR) || \
+		mv $(BUILD_DIR)/$(subst .zip,,$(URG_SOURCE)) $(URG_DIR)
+endef
+
+$(eval $(autotools-package))
