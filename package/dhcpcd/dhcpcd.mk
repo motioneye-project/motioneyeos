69a0a1e1d369fc2312b741f009bcecaf1ee33798
John Stile <johns@msli.com>
2013-03-10 18:23:14 -0700

    dhcpcd: new package
    Dhcpcd is an RFC2131 compliant DHCP client.
    
    [Peter: needs ipv6, fix whitespace/file header]
    Signed-off-by: John Stile <john@stilen.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/dhcpcd/dhcpcd.mk
@@ -0,0 +1,42 @@
+################################################################################
+#
+# dhcpcd
+#
+################################################################################
+
+DHCPCD_VERSION = 5.6.7
+DHCPCD_SOURCE = dhcpcd-$(DHCPCD_VERSION).tar.bz2
+DHCPCD_SITE = http://roy.marples.name/downloads/dhcpcd/
+DHCPCD_LICENSE = BSD-2c
+
+ifeq ($(BR2_USE_MMU),)
+	DHCPCD_CONFIG_OPT += --disable-fork
+endif
+
+define DHCPCD_CONFIGURE_CMDS
+	(cd $(@D); \
+	./configure \
+		--target=$(BR2_GCC_TARGET_ARCH) \
+		--os=linux \
+		$(DHCPCD_CONFIG_OPT) )
+endef
+
+define DHCPCD_BUILD_CMDS
+	$(MAKE) \
+		$(TARGET_CONFIGURE_OPTS) \
+		-C $(@D) all
+endef
+
+define DHCPCD_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/dhcpcd \
+		$(TARGET_DIR)/usr/bin/dhcpcd
+	$(INSTALL) -D -m 0644 $(@D)/dhcpcd.conf \
+		$(TARGET_DIR)/etc/dhcpcd.conf
+	$(INSTALL) -D -m 0755 $(@D)/dhcpcd-run-hooks \
+		$(TARGET_DIR)/libexec/dhcpcd-run-hooks
+endef
+
+# NOTE: Even though this package has a configure script, it is not generated
+# using the autotools, so we have to use the generic package infrastructure.
+
+$(eval $(call generic-package))
