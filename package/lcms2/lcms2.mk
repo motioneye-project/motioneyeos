901282aa6e330c5e61ab25a83ba68e443cb3aec2
Olivier Schonken <olivier.schonken@gmail.com>
2013-03-16 02:42:22 +0000

    Added lcms2 package
    Little CMS intends to be an OPEN SOURCE small-footprint color management
    engine, with special focus on accuracy and performance.
    
    [Peter: tweak help text, fix white space]
    Signed-off-by: Olivier Schonken <olivier.schonken@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/lcms2/lcms2.mk
@@ -0,0 +1,35 @@
+#############################################################
+#
+# lcms2
+#
+#############################################################
+
+LCMS2_VERSION = 2.4
+LCMS2_SITE = http://downloads.sourceforge.net/lcms/lcms
+LCMS2_LICENSE = MIT
+LCMS2_LICENSE_FILES = COPYING
+
+LCMS2_CONF_OPT = \
+
+ifeq ($(BR2_PACKAGE_JPEG),y)
+	LCMS2_CONF_OPT += --with-jpeg
+	LCMS2_DEPENDENCIES += jpeg
+else
+	LCMS2_CONF_OPT += --without-jpeg
+endif
+
+ifeq ($(BR2_PACKAGE_TIFF),y)
+	LCMS2_CONF_OPT += --with-tiff
+	LCMS2_DEPENDENCIES += tiff
+else
+	LCMS2_CONF_OPT += --without-tiff
+endif
+
+ifeq ($(BR2_PACKAGE_ZLIB),y)
+	LCMS2_CONF_OPT += --with-zlib
+	LCMS2_DEPENDENCIES += zlib
+else
+	LCMS2_CONF_OPT += --without-zlib
+endif
+
+$(eval $(autotools-package))
