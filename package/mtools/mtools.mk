ae2cd54a2706082c6056017790da01ab5139419d
Yann E. MORIN <yann.morin.1998@free.fr>
2013-03-17 08:19:45 +0000

    package/mtools: new host-package
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Tested-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/mtools/mtools.mk
@@ -0,0 +1,17 @@
+#############################################################
+#
+# mtools
+#
+#############################################################
+
+MTOOLS_VERSION       = 4.0.18
+MTOOLS_SOURCE        = mtools-$(MTOOLS_VERSION).tar.bz2
+MTOOLS_SITE          = $(BR2_GNU_MIRROR)/mtools/
+MTOOLS_LICENSE       = GPLv3+
+MTOOLS_LICENSE_FILES = COPYING
+
+MTOOLS_CONF_OPT = --enable-xdf        \
+                  --enable-vold       \
+                  --enable-new-vold
+
+$(eval $(host-autotools-package))
