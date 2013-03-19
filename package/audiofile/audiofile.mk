61aa05cc89d31de083c624185aa8ba1a910a6c7d
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-03-19 03:05:11 +0000

    audiofile: bump to version 0.3.6
    Now with FLAC support.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/audiofile/audiofile.mk
+++ package/audiofile/audiofile.mk
@@ -4,18 +4,20 @@
 #
 #############################################################
 
-AUDIOFILE_VERSION = 0.3.4
+AUDIOFILE_VERSION = 0.3.6
 AUDIOFILE_SITE = http://audiofile.68k.org
 AUDIOFILE_INSTALL_STAGING = YES
 AUDIOFILE_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
-AUDIOFILE_LICENSE = GPLv2+ LGPLv2+
+AUDIOFILE_CONF_OPT = --disable-examples --disable-docs
+AUDIOFILE_DEPENDENCIES = host-pkgconf
+AUDIOFILE_LICENSE = GPLv2+ LGPLv2.1+
 AUDIOFILE_LICENSE_FILES = COPYING COPYING.GPL
 
-# Useless and needs alsa-lib
-define AUDIOFILE_DISABLE_EXAMPLES
-	$(SED) 's/examples//' $(@D)/Makefile.in
-endef
-
-AUDIOFILE_POST_PATCH_HOOKS += AUDIOFILE_DISABLE_EXAMPLES
+ifeq ($(BR2_PACKAGE_FLAC),y)
+AUDIOFILE_DEPENDENCIES += flac
+AUDIOFILE_CONF_OPT += --enable-flac
+else
+AUDIOFILE_CONF_OPT += --disable-flac
+endif
 
 $(eval $(autotools-package))
