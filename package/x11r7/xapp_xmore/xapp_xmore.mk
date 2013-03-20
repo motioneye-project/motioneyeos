3c7eb6ca23b6b17357059688b47c150a06310dac
Jesper Baekdahl <jbb@gamblify.com>
2013-03-20 11:56:25 +0100

    xlib_libXprintUtil: removed in X11R7.7

--- package/x11r7/xapp_xmore/xapp_xmore.mk
+++ package/x11r7/xapp_xmore/xapp_xmore.mk
@@ -7,6 +7,6 @@
 XAPP_XMORE_VERSION = 1.0.1
 XAPP_XMORE_SOURCE = xmore-$(XAPP_XMORE_VERSION).tar.bz2
 XAPP_XMORE_SITE = http://xorg.freedesktop.org/releases/individual/app
-XAPP_XMORE_DEPENDENCIES = xlib_libXprintUtil xlib_libXaw
+XAPP_XMORE_DEPENDENCIES = xlib_libXaw
 
 $(eval $(autotools-package))
