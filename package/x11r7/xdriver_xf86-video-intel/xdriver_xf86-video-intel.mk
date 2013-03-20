d7f87f81dea74ae1cd5b144b2060535bdbdd2246
Jesper Baekdahl <jbb@gamblify.com>
2013-03-20 14:33:15 +0100

    xdriver_xf86-video-intel: bump revision to X11R7.7

--- package/x11r7/xdriver_xf86-video-intel/xdriver_xf86-video-intel.mk
+++ package/x11r7/xdriver_xf86-video-intel/xdriver_xf86-video-intel.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_INTEL_VERSION = 2.13.0
+XDRIVER_XF86_VIDEO_INTEL_VERSION = 2.19.0
 XDRIVER_XF86_VIDEO_INTEL_SOURCE = xf86-video-intel-$(XDRIVER_XF86_VIDEO_INTEL_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_INTEL_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_INTEL_AUTORECONF = YES
