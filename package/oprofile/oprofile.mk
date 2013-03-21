99fc6299dbceaf6d58be2fb516a346451968a704
Thomas De Schampheleire <patrickdepinguin+buildroot@gmail.com>
2013-03-21 09:03:17 +0000

    oprofile 0.9.8: fix compilation on powerpc
    The oprofile build was broken on powerpc since version 0.9.8.
    This was detected in several autobuilds, like
    http://autobuild.buildroot.net/results/6f6c02d18495907d50fcdfc6003ac20d493c55fe/
    
    Thomas Petazzoni had some fixes pending in his own tree, and this patch is
    partially based on this work (credits to him). Here is an overview:
    - I took over (and fixed) the oprofile.mk changes, except for the powerpc-
      specific part. For powerpc, there is a new dependency to libpfm4.
    - I reimported those Yocto patches that were specific to the ppc build
      issues, but left out the other ones. Those can be added in separate
      commits.
    
    [Peter: simplify libpfm4 check]
    Signed-off-by: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    Tested-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/oprofile/oprofile.mk
+++ package/oprofile/oprofile.mk
@@ -8,7 +8,12 @@ OPROFILE_VERSION = 0.9.8
 OPROFILE_SITE = http://downloads.sourceforge.net/project/oprofile/oprofile/oprofile-$(OPROFILE_VERSION)
 OPROFILE_LICENSE = GPLv2+
 OPROFILE_LICENSE_FILES = COPYING
-OPROFILE_CONF_OPT = --localstatedir=/var --with-kernel-support
+OPROFILE_CONF_OPT = \
+	--localstatedir=/var \
+	--disable-account-check \
+	--enable-gui=no \
+	--with-kernel=$(STAGING_DIR)/usr
+OPROFILE_AUTORECONF = YES
 OPROFILE_BINARIES = utils/ophelp pp/opannotate pp/oparchive pp/opgprof
 OPROFILE_BINARIES += pp/opreport opjitconv/opjitconv daemon/oprofiled
 OPROFILE_BINARIES += utils/op-check-perfevents pe_profiling/operf libabi/opimport
@@ -29,7 +34,17 @@ ifeq ($(OPROFILE_ARCH),)
 OPROFILE_ARCH = $(BR2_ARCH)
 endif
 
-OPROFILE_DEPENDENCIES = popt binutils
+OPROFILE_DEPENDENCIES = popt binutils host-pkgconf
+
+ifeq ($(BR2_PACKAGE_LIBPFM4),y)
+OPROFILE_DEPENDENCIES += libpfm4
+endif
+
+define OPROFILE_CREATE_FILES
+	touch $(@D)/NEWS $(@D)/AUTHORS $(@D)/ChangeLog
+endef
+
+OPROFILE_POST_PATCH_HOOKS += OPROFILE_CREATE_FILES
 
 define OPROFILE_INSTALL_TARGET_CMDS
 	$(INSTALL) -d -m 755 $(TARGET_DIR)/usr/bin
