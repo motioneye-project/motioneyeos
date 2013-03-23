e30c5bc58e35b1043f0fe619b6aca122558344d0
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-03-23 22:26:28 +0000

    gcc: fix location of snapshots
    The gcc snapshots are now located at
    ftp://gcc.gnu.org/pub/gcc/snapshots/. This has been tested with a
    recent 4.8.0-RC snapshot.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- toolchain/gcc/gcc-uclibc-4.x.mk
+++ toolchain/gcc/gcc-uclibc-4.x.mk
@@ -26,7 +26,7 @@ GCC_SNAP_DATE:=
 endif
 
 ifneq ($(GCC_SNAP_DATE),)
- GCC_SITE:=ftp://sources.redhat.com/pub/gcc/snapshots/$(GCC_VERSION)
+ GCC_SITE:=ftp://gcc.gnu.org/pub/gcc/snapshots/$(GCC_SNAP_DATE)/
 else ifeq ($(findstring avr32,$(GCC_VERSION)),avr32)
  GCC_SITE:=ftp://www.at91.com/pub/buildroot/
 else
