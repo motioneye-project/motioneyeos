3f251aa5b93be64ba61edeea93e5822962cebf39
Yann E. MORIN <yann.morin.1998@free.fr>
2013-03-23 22:26:25 +0000

    package/connman: version bump to 1.12, and fix build with newer kernels headers
    ... while keeping compatibility with older ones.
    
    Fixes autobuild failure:
    http://autobuild.buildroot.org/results/f99ae7af46c70f7c356b7771321511a42eb3694d/
    
    Reported-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/connman/connman.mk
+++ package/connman/connman.mk
@@ -4,7 +4,7 @@
 #
 #######################################################
 
-CONNMAN_VERSION = 1.11
+CONNMAN_VERSION = 1.12
 CONNMAN_SITE = $(BR2_KERNEL_MIRROR)/linux/network/connman/
 CONNMAN_DEPENDENCIES = libglib2 dbus iptables gnutls
 CONNMAN_INSTALL_STAGING = YES
