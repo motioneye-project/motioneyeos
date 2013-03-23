2f55f8e440ea6040ec7fc9ba0692dd0980a36bd9
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-03-23 22:26:35 +0000

    libpcap: add missing flex/bison dependencies
    In preparation for the removal of the flex/bison mandatory
    dependencies, make sure libpcap builds host-flex/host-bison.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libpcap/libpcap.mk
+++ package/libpcap/libpcap.mk
@@ -20,7 +20,7 @@ LIBPCAP_INSTALL_STAGING = YES
 # configure script correctly handles --enable-shared/--disable-shared.
 LIBPCAP_AUTORECONF = YES
 
-LIBPCAP_DEPENDENCIES = zlib
+LIBPCAP_DEPENDENCIES = zlib host-flex host-bison
 LIBPCAP_CONF_ENV = ac_cv_linux_vers=2 \
 		ac_cv_header_linux_wireless_h=yes # configure misdetects this
 LIBPCAP_CONF_OPT = --disable-yydebug --with-pcap=linux
