94dad5d0351c1577237ecc77421cfb74a976c068
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-03-24 21:34:59 +0000

    mrouted: add missing dependency on host-bison
    Fixes
    http://autobuild.buildroot.org/results/10d875724388034369e7673a6c08ecad3863ba06/build-end.log.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/mrouted/mrouted.mk
+++ package/mrouted/mrouted.mk
@@ -7,6 +7,7 @@
 MROUTED_VERSION = 3.9.6
 MROUTED_SOURCE = mrouted-$(MROUTED_VERSION).tar.bz2
 MROUTED_SITE = http://cloud.github.com/downloads/troglobit/mrouted
+MROUTED_DEPENDENCIES = host-bison
 
 define MROUTED_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
