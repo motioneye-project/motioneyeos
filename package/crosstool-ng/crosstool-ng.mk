8dfe032f7e2db454453ba868d3356d5f3afdbab5
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-03-24 12:29:06 +0000

    crosstool-ng: add missing dependencies
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/crosstool-ng/crosstool-ng.mk
+++ package/crosstool-ng/crosstool-ng.mk
@@ -3,6 +3,10 @@ CROSSTOOL_NG_SOURCE            = crosstool-ng-$(CROSSTOOL_NG_VERSION).tar.bz2
 CROSSTOOL_NG_SITE              = http://crosstool-ng.org/download/crosstool-ng/
 CROSSTOOL_NG_INSTALL_TARGET    = NO
 CROSSTOOL_NG_MAKE              = $(MAKE1)
-HOST_CROSSTOOL_NG_DEPENDENCIES = $(if $(BR2_CCACHE),host-ccache) host-gawk host-automake host-gperf
+
+HOST_CROSSTOOL_NG_DEPENDENCIES = \
+	$(if $(BR2_CCACHE),host-ccache) host-gawk \
+	host-automake host-gperf host-bison host-flex \
+	host-texinfo
 
 $(eval $(host-autotools-package))
