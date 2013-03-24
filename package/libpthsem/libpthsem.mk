468e9595be307b06ffd8bdcd6b7cac624e6549cc
Gregory Hermant <gregory.hermant@calao-systems.com>
2013-03-24 11:34:05 +0000

    libpthsem: new package
    [Peter: remove extra newlines]
    [Thomas: various fixes/improvements]
    Signed-off-by: Gregory Hermant <gregory.hermant@calao-systems.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libpthsem/libpthsem.mk
@@ -0,0 +1,17 @@
+#############################################################
+#
+# libpthsem
+#
+#############################################################
+
+LIBPTHSEM_VERSION = 2.0.8
+LIBPTHSEM_SOURCE = pthsem_$(LIBPTHSEM_VERSION).tar.gz
+LIBPTHSEM_SITE = http://www.auto.tuwien.ac.at/~mkoegler/pth/
+LIBPTHSEM_LICENSE = LGPLv2.1+
+LIBPTHSEM_LICENSE_FILES = COPYING
+LIBPTHSEM_AUTORECONF = YES
+LIBPTHSEM_INSTALL_STAGING = YES
+LIBPTHSEM_DEPENDENCIES = argp-standalone
+LIBPTHSEM_CONFIG_SCRIPTS = pthsem-config
+
+$(eval $(autotools-package))
