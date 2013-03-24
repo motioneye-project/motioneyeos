48465f0796b0ffc53300161b746ff20ccce1e8df
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-03-24 21:42:30 +0000

    gnuchess: add missing dependencies on flex and host-flex
    And also add a missing newline and licensing informations while we're
    at it.
    
    Fixes:
    http://autobuild.buildroot.org/results/2851f23990c9f6642b1e706a94b1afc15a24b6f7/build-end.log.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gnuchess/gnuchess.mk
+++ package/gnuchess/gnuchess.mk
@@ -3,11 +3,15 @@
 # gnuchess
 #
 #############################################################
+
 GNUCHESS_VERSION = 5.07
 GNUCHESS_SOURCE = gnuchess-$(GNUCHESS_VERSION).tar.gz
 GNUCHESS_SITE = $(BR2_GNU_MIRROR)/chess
+GNUCHESS_LICENSE = GPLv2+
+GNUCHESS_LICENSE_FILES = COPYING
 
-GNUCHESS_DEPENDENCIES = $(if $(BR2_PACKAGE_READLINE),readline)
+GNUCHESS_DEPENDENCIES = host-flex flex
+GNUCHESS_DEPENDENCIES += $(if $(BR2_PACKAGE_READLINE),readline)
 
 $(eval $(autotools-package))
 
