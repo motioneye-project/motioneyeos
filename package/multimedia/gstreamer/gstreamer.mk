e5ce4e4164e30189e02f04d37708e7d4703851cf
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-03-24 13:42:59 +0000

    gstreamer: add missing dependencies on host-flex and host-bison
    Fixes:
    http://autobuild.buildroot.org/results/f9f32bfd44a3f74ec30fc61594bf6eb04f80f872/build-end.log.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/multimedia/gstreamer/gstreamer.mk
+++ package/multimedia/gstreamer/gstreamer.mk
@@ -27,6 +27,6 @@ GSTREAMER_CONF_OPT = \
 		$(if $(BR2_PACKAGE_GSTREAMER_GST_DEBUG),,--disable-gst-debug) \
 		$(if $(BR2_PACKAGE_GSTREAMER_PLUGIN_REGISTRY),,--disable-registry)
 
-GSTREAMER_DEPENDENCIES = libglib2 host-pkgconf
+GSTREAMER_DEPENDENCIES = libglib2 host-pkgconf host-bison host-flex
 
 $(eval $(autotools-package))
