69aeb066a68108e0f78af34d5baf770c490954d0
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-03-24 12:29:05 +0000

    texinfo: new host package
    The makeinfo program from texinfo is needed to get Crosstool-NG to
    build properly a toolchain. Long term, it would be great if
    Crosstool-NG supported building without texinfo, simply by disabling
    the build of documentation.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/texinfo/texinfo.mk
@@ -0,0 +1,14 @@
+#############################################################
+#
+# texinfo
+#
+#############################################################
+
+# We are intentionally not using the latest version 5.x, because it
+# causes issues with the documentation building process when creating
+# a toolchain with the Crosstool-NG backend.
+
+TEXINFO_VERSION = 4.13a
+TEXINFO_SITE = $(BR2_GNU_MIRROR)/texinfo
+
+$(eval $(host-autotools-package))
