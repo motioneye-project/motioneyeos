83d871809c52c3f79989164a07c6b9021bfa1851
Anthony Viallard <viallard@syscom-instruments.com>
2013-03-24 08:19:25 +0000

    gnuplot: new package
    [Thomas: Improved by adding support to use readline functionality, gd
    functionality, and various fixes.]
    
    Signed-off-by: Anthony Viallard <viallard@syscom-instruments.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/gnuplot/gnuplot.mk
@@ -0,0 +1,50 @@
+#############################################################
+#
+# gnuplot
+#
+#############################################################
+
+GNUPLOT_VERSION = 4.6.2
+GNUPLOT_SITE = http://downloads.sourceforge.net/project/gnuplot/gnuplot/$(GNUPLOT_VERSION)
+GNUPLOT_LICENSE = gnuplot license (open source)
+GNUPLOT_LICENSE_FILES = Copyright
+
+GNUPLOT_AUTORECONF = YES
+
+GNUPLOT_CONF_OPT = --without-x \
+		--disable-raise-console \
+		--disable-mouse \
+	        --without-tutorial \
+		--disable-demo \
+	        --without-row-help \
+		--disable-history-file \
+	        --without-lisp-files \
+	        --disable-wxwidgets \
+	        --without-lua \
+		--without-latex \
+	        --without-cairo
+
+ifeq ($(BR2_PACKAGE_GD),y)
+GNUPLOT_CONF_OPT += --with-gd
+GNUPLOT_DEPENDENCIES += gd
+GNUPLOT_CONF_ENV += \
+	ac_cv_path_GDLIB_CONFIG=$(STAGING_DIR)/usr/bin/gdlib-config
+else
+GNUPLOT_CONF_OPT += --without-gd
+endif
+
+ifeq ($(BR2_PACKAGE_READLINE),y)
+GNUPLOT_CONF_OPT += --with-readline=gnu
+GNUPLOT_DEPENDENCIES += readline
+else
+GNUPLOT_CONF_OPT += --without-readline
+endif
+
+# Remove Javascript scripts, lua scripts, PostScript files
+define GNUPLOT_REMOVE_UNNEEDED_FILES
+	$(RM) -rf $(TARGET_DIR)/usr/share/gnuplot
+endef
+
+GNUPLOT_POST_INSTALL_TARGET_HOOKS += GNUPLOT_REMOVE_UNNEEDED_FILES
+
+$(eval $(autotools-package))
