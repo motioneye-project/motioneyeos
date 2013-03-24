c415c33959d15b5632708c051acb4fcdacaa90a4
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-03-24 08:19:30 +0000

    xcursor-transparent-theme: new package
    Based on work originally done by Aleksandar Zivkovic at
    http://patchwork.ozlabs.org/patch/171109/.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/x11r7/xcursor-transparent-theme/xcursor-transparent-theme.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# xcursor-transparent-theme - No description available
+#
+#############################################################
+
+XCURSOR_TRANSPARENT_THEME_VERSION = 0.1.1
+XCURSOR_TRANSPARENT_THEME_SITE = http://downloads.yoctoproject.org/releases/matchbox/utils/
+XCURSOR_TRANSPARENT_THEME_DEPENDENCIES = xlib_libXcursor host-xapp_xcursorgen
+
+define ICONS_DEFAULT_CONFIG_INSTALL
+        $(INSTALL) -m 0755 -D package/x11r7/xcursor-transparent-theme/index.theme \
+		$(TARGET_DIR)/usr/share/icons/default/index.theme
+endef
+
+XCURSOR_TRANSPARENT_THEME_POST_INSTALL_TARGET_HOOKS += ICONS_DEFAULT_CONFIG_INSTALL
+
+$(eval $(autotools-package))
