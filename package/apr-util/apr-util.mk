a00fc22779983440d0bec2b996b55f4064079e55
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-03-24 08:19:28 +0000

    apr-util: various improvements
    This commit:
     * Adds licensing informations.
     * Uses the apr-1-config script instead of pointing apr-util to the
       apr build directory.
     * Ensures apu-1-config gets fixed properly using the
       <pkg>_CONFIG_SCRIPTS mechanism.
     * Ensures that libiconv gets built before apr-util if available, so
       that character set conversion features of apr-util can be enabled.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/apr-util/apr-util.mk
+++ package/apr-util/apr-util.mk
@@ -5,9 +5,16 @@
 #############################################################
 APR_UTIL_VERSION = 1.4.1
 APR_UTIL_SITE = http://archive.apache.org/dist/apr
+APR_UTIL_LICENSE = Apache-2.0
+APR_UTIL_LICENSE_FILES = LICENSE
 APR_UTIL_INSTALL_STAGING = YES
 APR_UTIL_DEPENDENCIES = apr sqlite neon zlib
 APR_UTIL_CONF_OPT = \
-	--with-apr=$(BUILD_DIR)/apr-$(APR_VERSION) \
+	--with-apr=$(STAGING_DIR)/usr/bin/apr-1-config
+APR_UTIL_CONFIG_SCRIPTS = apu-1-config
+
+# When iconv is available, then use it to provide charset conversion
+# features.
+APR_UTIL_DEPENDENCIES = $(if $(BR2_PACKAGE_LIBICONV),libiconv)
 
 $(eval $(autotools-package))
