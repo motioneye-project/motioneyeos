7441228fb62b3c93efc03a173f57d6fec5486068
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-03-25 11:42:35 +0000

    vala: add missing host-bison dependency
    Fixes:
    http://autobuild.buildroot.org/results/a59cceb695298c212c91d2e32b381a096f875274/build-end.log.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/vala/vala.mk
+++ package/vala/vala.mk
@@ -10,7 +10,7 @@ VALA_VERSION = $(VALA_VERSION_MAJOR).$(VALA_VERSION_MINOR)
 VALA_SITE = http://download.gnome.org/sources/vala/$(VALA_VERSION_MAJOR)
 VALA_SOURCE = vala-$(VALA_VERSION).tar.xz
 
-VALA_DEPENDENCIES = host-flex libglib2 \
+VALA_DEPENDENCIES = host-flex host-bison libglib2 \
 		$(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext)
 
 # If we want the documentation, then xsltproc is needed. If we don't
