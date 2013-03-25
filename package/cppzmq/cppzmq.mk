5514b7a520dea993e05d6fcddc40e4d55f467b7c
Simon Dawson <spdawson@gmail.com>
2013-03-25 13:28:02 +0000

    cppzmq: new package
    Now that zeromq has been bumped to version 3.2.2, we need to provide the
    C++ binding using a separate package. The cppzmq package provides a simple
    single-header C++ binding.
    
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    [yann.morin.1998@free.fr: add comment about no license file]
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/cppzmq/cppzmq.mk
@@ -0,0 +1,23 @@
+#############################################################
+#
+# cppzmq
+#
+#############################################################
+
+CPPZMQ_VERSION = b232978
+CPPZMQ_SITE = git://github.com/zeromq/cppzmq.git
+CPPZMQ_INSTALL_STAGING = YES
+CPPZMQ_DEPENDENCIES = zeromq
+CPPZMQ_LICENSE = MIT
+# No license file, the license is in the installed header
+CPPZMQ_LICENSE_FILES = zmq.hpp
+
+define CPPZMQ_INSTALL_STAGING_CMDS
+	$(INSTALL) -m 0644 -D $(@D)/zmq.hpp $(STAGING_DIR)/usr/include/zmq.hpp
+endef
+
+define CPPZMQ_UNINSTALL_STAGING_CMDS
+	$(RM) $(STAGING_DIR)/usr/include/zmq.hpp
+endef
+
+$(eval $(generic-package))
