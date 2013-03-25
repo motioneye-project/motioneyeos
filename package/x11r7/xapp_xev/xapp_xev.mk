e68c544ef89144f73b7d814e439476dac3c4d1bc
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-03-25 10:27:12 +0000

    xapp_xev: add missing dependencies on xlib_libXrandr
    Fixes:
    http://autobuild.buildroot.org/results/ec1855d3d96cf01409999492c77a2709be8c4d01/build-end.log.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xapp_xev/xapp_xev.mk
+++ package/x11r7/xapp_xev/xapp_xev.mk
@@ -7,6 +7,6 @@
 XAPP_XEV_VERSION = 1.2.0
 XAPP_XEV_SOURCE = xev-$(XAPP_XEV_VERSION).tar.bz2
 XAPP_XEV_SITE = http://xorg.freedesktop.org/releases/individual/app
-XAPP_XEV_DEPENDENCIES = xlib_libX11
+XAPP_XEV_DEPENDENCIES = xlib_libX11 xlib_libXrandr
 
 $(eval $(autotools-package))
