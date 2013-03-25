fe2bcc9ea903365d9c8f86ed5785cbd9f6228899
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-03-25 08:17:07 +0000

    conntrack-tools: add missing dependencies on host-flex/host-bison
    Fixes
    http://autobuild.buildroot.org/results/fd739aa0028fac1418ed33bc0ea831b6fc17a853/build-end.log.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/conntrack-tools/conntrack-tools.mk
+++ package/conntrack-tools/conntrack-tools.mk
@@ -9,7 +9,7 @@ CONNTRACK_TOOLS_SOURCE = conntrack-tools-$(CONNTRACK_TOOLS_VERSION).tar.bz2
 CONNTRACK_TOOLS_SITE = http://www.netfilter.org/projects/conntrack-tools/files
 CONNTRACK_TOOLS_DEPENDENCIES = host-pkgconf \
 	libnetfilter_conntrack libnetfilter_cthelper libnetfilter_cttimeout \
-	libnetfilter_queue
+	libnetfilter_queue host-bison host-flex
 CONNTRACK_TOOLS_LICENSE = GPLv2
 CONNTRACK_TOOLS_LICENSE_FILES = COPYING
 
