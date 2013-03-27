93581de0ec4cfc77ea6006b6283d034f82b45b93
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-03-27 11:38:01 +0000

    gmp: bump to version 5.1.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gmp/gmp.mk
+++ package/gmp/gmp.mk
@@ -4,19 +4,13 @@
 #
 #############################################################
 
-GMP_VERSION = 5.1.0
+GMP_VERSION = 5.1.1
 GMP_SITE = ftp://ftp.gmplib.org/pub/gmp-$(GMP_VERSION)
 GMP_SOURCE = gmp-$(GMP_VERSION).tar.bz2
 GMP_INSTALL_STAGING = YES
 GMP_LICENSE = LGPLv3+
 GMP_LICENSE_FILES = COPYING.LIB
-
 GMP_DEPENDENCIES = host-m4
-
-# Bad ARM assembly breaks on pure thumb
-ifeq ($(ARCH),arm)
-GMP_MAKE_OPT += CFLAGS="$(TARGET_CFLAGS) -marm"
-endif
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
