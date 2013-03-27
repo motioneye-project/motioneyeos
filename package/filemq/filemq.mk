ba9c34787b5c089eab29302df2c6adb3a2de24ae
Simon Dawson <spdawson@gmail.com>
2013-03-27 03:58:48 +0000

    filemq: new package
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/filemq/filemq.mk
@@ -0,0 +1,22 @@
+#############################################################
+#
+# filemq
+#
+#############################################################
+
+FILEMQ_VERSION = 8fac5140dd
+FILEMQ_SITE = git://github.com/zeromq/filemq.git
+
+FILEMQ_AUTORECONF = YES
+FILEMQ_INSTALL_STAGING = YES
+FILEMQ_DEPENDENCIES = czmq openssl zeromq
+FILEMQ_LICENSE = LGPLv3+ with exceptions
+FILEMQ_LICENSE_FILES = COPYING COPYING.LESSER
+
+define FILEMQ_CREATE_CONFIG_DIR
+	mkdir -p $(@D)/config
+endef
+
+FILEMQ_POST_PATCH_HOOKS += FILEMQ_CREATE_CONFIG_DIR
+
+$(eval $(autotools-package))
