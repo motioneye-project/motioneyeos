5cae3e8af0fe62e51e4fc675c566501f2bfd1cc2
Simon Dawson <spdawson@gmail.com>
2013-03-27 00:33:57 +0000

    jansson: new package
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/jansson/jansson.mk
@@ -0,0 +1,12 @@
+#############################################################
+#
+# jansson
+#
+#############################################################
+JANSSON_VERSION = 2.4
+JANSSON_SITE = http://www.digip.org/jansson/releases
+JANSSON_LICENSE = MIT
+JANSSON_LICENSE_FILES = LICENSE
+JANSSON_INSTALL_STAGING = YES
+
+$(eval $(autotools-package))
