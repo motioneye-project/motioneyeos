a590595c700370eef5c024d7e264bf26f29db2fe
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-03-27 06:32:32 +0000

    xkeyboard-config: needs libX11 and xproto_xproto
    Fixes a build failure i've found while debugging another one :)
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xkeyboard-config/xkeyboard-config.mk
+++ package/x11r7/xkeyboard-config/xkeyboard-config.mk
@@ -6,7 +6,7 @@
 XKEYBOARD_CONFIG_VERSION = 2.6
 XKEYBOARD_CONFIG_SOURCE = xkeyboard-config-$(XKEYBOARD_CONFIG_VERSION).tar.bz2
 XKEYBOARD_CONFIG_SITE = http://www.x.org/releases/individual/data/xkeyboard-config/
-XKEYBOARD_CONFIG_DEPENDENCIES = host-intltool host-xapp_xkbcomp
+XKEYBOARD_CONFIG_DEPENDENCIES = host-intltool host-xapp_xkbcomp xlib_libX11 xproto_xproto
 XKEYBOARD_CONFIG_CONF_OPT = GMSGFMT=/usr/bin/msgfmt
 
 $(eval $(autotools-package))
