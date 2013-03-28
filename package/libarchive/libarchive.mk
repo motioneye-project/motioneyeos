db5db3486f0f6e3113ffc890b2076e4534d4ee69
Tilman Keskinöz <arved@arved.at>
2013-03-28 05:29:24 +0000

    libarchive: update to 3.0.4
    Signed-off-by: Tilman Keskinöz <arved@arved.at>
    Acked-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Tested-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libarchive/libarchive.mk
+++ package/libarchive/libarchive.mk
@@ -3,8 +3,8 @@
 # libarchive (reusable C library for archive formats)
 #
 #############################################################
-LIBARCHIVE_VERSION = 2.8.5
-LIBARCHIVE_SITE = http://libarchive.googlecode.com/files/
+LIBARCHIVE_VERSION = 3.0.4
+LIBARCHIVE_SITE = http://github.com/downloads/libarchive/libarchive
 LIBARCHIVE_SOURCE = libarchive-$(LIBARCHIVE_VERSION).tar.gz
 LIBARCHIVE_INSTALL_STAGING = YES
 
