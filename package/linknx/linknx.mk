4deba5c3d91d97de80b45241bcdc67e25f6e92e8
Gregory Hermant <gregory.hermant@calao-systems.com>
2013-03-28 03:36:34 +0000

    linknx: new package
    [Peter: add patch to fix build issue with gcc 4.7+]
    [Thomas: various fixes/improvements]
    Signed-off-by: Gregory Hermant <gregory.hermant@calao-systems.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/linknx/linknx.mk
@@ -0,0 +1,30 @@
+#############################################################
+#
+# linknx
+#
+#############################################################
+
+LINKNX_VERSION = 0.0.1.30
+LINKNX_SITE = http://downloads.sourceforge.net/project/linknx/linknx/linknx-$(LINKNX_VERSION)
+LINKNX_LICENSE = GPLv2+
+LINKNX_INSTALL_STAGING = YES
+LINKNX_CONF_OPT = --without-lua --without-log4cpp --without-pth-test \
+		  --with-pth=$(STAGING_DIR)/usr
+
+LINKNX_DEPENDENCIES = libpthsem
+
+ifeq ($(BR2_PACKAGE_MYSQL_CLIENT),y)
+LINKNX_CONF_OPT += --with-mysql=$(STAGING_DIR)/usr/bin/mysql_config
+LINKNX_DEPENDENCIES = mysql_client
+else
+LINKNX_CONF_OPT += --without-mysql
+endif
+
+ifeq ($(BR2_PACKAGE_LIBCURL),y)
+LINKNX_CONF_OPT += --with-libcurl=$(STAGING_DIR)/usr/bin/curl-config
+LINKNX_DEPENDENCIES += libcurl
+else
+LINKNX_CONF_OPT += --without-libcurl
+endif
+
+$(eval $(autotools-package))
