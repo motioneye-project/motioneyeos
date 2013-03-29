9f2b3f9605c92f6a9fa2247b12d11985c1688f17
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-03-29 01:19:53 +0000

    genimage: needs host-pkgconf
    Fixes:
    http://autobuild.buildroot.net/results/1561c26e45d0604f058eb0c7a8bebf08d4abe756/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/genimage/genimage.mk
+++ package/genimage/genimage.mk
@@ -7,7 +7,7 @@
 GENIMAGE_VERSION        = 4
 GENIMAGE_SOURCE         = genimage-$(GENIMAGE_VERSION).tar.xz
 GENIMAGE_SITE           = http://www.pengutronix.de/software/genimage/download/
-GENIMAGE_DEPENDENCIES   = libconfuse
+GENIMAGE_DEPENDENCIES   = host-pkgconf libconfuse
 GENIMAGE_LICENSE        = GPLv2
 
 $(eval $(host-autotools-package))
