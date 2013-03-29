421d9d636271e5872d10a521922b06683ab967b5
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-03-29 01:19:52 +0000

    intltool: host variant needs host-gettext
    Fixes issues like:
    http://autobuild.buildroot.net/results/1c7d2e50b622179aff469fddedaa4dc2015fb496/
    http://autobuild.buildroot.net/results/7d85ff51b339f1da720bb079eb05e7fba179f101/
    and probably a lot more.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/intltool/intltool.mk
+++ package/intltool/intltool.mk
@@ -8,7 +8,7 @@ INTLTOOL_VERSION = 0.40.6
 INTLTOOL_SOURCE = intltool-$(INTLTOOL_VERSION).tar.bz2
 INTLTOOL_SITE = http://ftp.acc.umu.se/pub/GNOME/sources/intltool/0.40/
 
-HOST_INTLTOOL_DEPENDENCIES = host-libxml-parser-perl
+HOST_INTLTOOL_DEPENDENCIES = host-gettext host-libxml-parser-perl
 HOST_INTLTOOL_CONF_OPT = \
   PERLLIB=$(HOST_DIR)/usr/lib/perl
 
