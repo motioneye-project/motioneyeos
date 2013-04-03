aae616e5550e0a466a956bbf5b8d8e8d1016c6b7
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-04-03 00:37:57 +0000

    gob2: host variant needs host-bison and host-flex
    Fixes:
    http://autobuild.buildroot.net/results/837ab8dc79f732c2cdf52b9ac6c53d64e841b35f/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gob2/gob2.mk
+++ package/gob2/gob2.mk
@@ -7,7 +7,7 @@
 GOB2_VERSION = 2.0.18
 GOB2_SITE = http://ftp.5z.com/pub/gob
 GOB2_DEPENDENCIES = libglib2 flex bison host-pkgconf host-flex
-HOST_GOB2_DEPENDENCIES = host-libglib2
+HOST_GOB2_DEPENDENCIES = host-bison host-flex host-libglib2
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
