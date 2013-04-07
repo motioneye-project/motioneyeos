1eb15de182484261538f815dcc8f59e84ba3b2fd
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-04-07 09:57:25 +0000

    qt5/qt5multimedia: new package
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/qt5/qt5multimedia/qt5multimedia.mk
@@ -0,0 +1,49 @@
+#############################################################
+#
+# qt5multimedia
+#
+#############################################################
+
+QT5MULTIMEDIA_VERSION = $(QT5_VERSION)
+QT5MULTIMEDIA_SITE = http://releases.qt-project.org/qt5/$(QT5MULTIMEDIA_VERSION)/submodules_tar/
+QT5MULTIMEDIA_SOURCE = qtmultimedia-opensource-src-$(QT5MULTIMEDIA_VERSION).tar.xz
+QT5MULTIMEDIA_DEPENDENCIES = qt5base qt5declarative
+QT5MULTIMEDIA_INSTALL_STAGING = YES
+
+ifeq ($(BR2_PACKAGE_QT5BASE_LICENSE_APPROVED),y)
+QT5MULTIMEDIA_CONFIGURE_OPTS += -opensource -confirm-license
+QT5MULTIMEDIA_LICENSE = LGPLv2.1 or GPLv3.0
+# Here we would like to get license files from qt5base, but qt5base
+# may not be extracted at the time we get the legal-info for
+# qt5script.
+else
+QT5MULTIMEDIA_LICENSE = Commercial license
+QT5MULTIMEDIA_REDISTRIBUTE = NO
+endif
+
+define QT5MULTIMEDIA_CONFIGURE_CMDS
+	(cd $(@D); $(HOST_DIR)/usr/bin/qmake)
+endef
+
+define QT5MULTIMEDIA_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define QT5MULTIMEDIA_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) install
+	$(QT5_LA_PRL_FILES_FIXUP)
+endef
+
+ifeq ($(BR2_PREFER_STATIC_LIB),)
+define QT5MULTIMEDIA_INSTALL_TARGET_LIBS
+	cp -dpf $(STAGING_DIR)/usr/lib/libQt5Multimedia*.so.* $(TARGET_DIR)/usr/lib
+	cp -dpfr $(STAGING_DIR)/usr/lib/qt/plugins/* $(TARGET_DIR)/usr/lib/qt/plugins
+endef
+endif
+
+define QT5MULTIMEDIA_INSTALL_TARGET_CMDS
+	cp -dpfr $(STAGING_DIR)/usr/qml/* $(TARGET_DIR)/usr/qml
+	$(QT5MULTIMEDIA_INSTALL_TARGET_LIBS)
+endef
+
+$(eval $(generic-package))
