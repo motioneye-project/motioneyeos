f1a251e7223c9732ca47d46c836851d3dbbfaa0f
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-04-07 01:50:52 +0000

    radvd: needs host-bison
    Fixes:
    http://autobuild.buildroot.net/results/849c48029ba77789f33ff726c5a33cd959200e38/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/radvd/radvd.mk
+++ package/radvd/radvd.mk
@@ -6,7 +6,7 @@
 
 RADVD_VERSION = 1.9.2
 RADVD_SITE = http://www.litech.org/radvd/dist
-RADVD_DEPENDENCIES = flex libdaemon host-flex host-pkgconf
+RADVD_DEPENDENCIES = host-bison flex libdaemon host-flex host-pkgconf
 RADVD_AUTORECONF = YES
 
 define RADVD_INSTALL_INITSCRIPT
