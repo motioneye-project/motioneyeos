6b65be6b19c2189f3631e8ac2c71bca0de740d0d
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-04-07 09:57:24 +0000

    qt5/qt5graphicaleffects: new package
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/qt5/qt5graphicaleffects/qt5graphicaleffects.mk
@@ -0,0 +1,40 @@
+#############################################################
+#
+# qt5graphicaleffects
+#
+#############################################################
+
+QT5GRAPHICALEFFECTS_VERSION = $(QT5_VERSION)
+QT5GRAPHICALEFFECTS_SITE = http://releases.qt-project.org/qt5/$(QT5GRAPHICALEFFECTS_VERSION)/submodules_tar/
+QT5GRAPHICALEFFECTS_SOURCE = qtgraphicaleffects-opensource-src-$(QT5GRAPHICALEFFECTS_VERSION).tar.xz
+QT5GRAPHICALEFFECTS_DEPENDENCIES = qt5base qt5declarative
+QT5GRAPHICALEFFECTS_INSTALL_STAGING = YES
+
+ifeq ($(BR2_PACKAGE_QT5BASE_LICENSE_APPROVED),y)
+QT5GRAPHICALEFFECTS_CONFIGURE_OPTS += -opensource -confirm-license
+QT5GRAPHICALEFFECTS_LICENSE = LGPLv2.1 or GPLv3.0
+# Here we would like to get license files from qt5base, but qt5base
+# may not be extracted at the time we get the legal-info for
+# qt5script.
+else
+QT5GRAPHICALEFFECTS_LICENSE = Commercial license
+QT5GRAPHICALEFFECTS_REDISTRIBUTE = NO
+endif
+
+define QT5GRAPHICALEFFECTS_CONFIGURE_CMDS
+	(cd $(@D); $(HOST_DIR)/usr/bin/qmake)
+endef
+
+define QT5GRAPHICALEFFECTS_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define QT5GRAPHICALEFFECTS_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) install
+endef
+
+define QT5GRAPHICALEFFECTS_INSTALL_TARGET_CMDS
+	cp -dpfr $(STAGING_DIR)/usr/qml/QtGraphicalEffects $(TARGET_DIR)/usr/qml
+endef
+
+$(eval $(generic-package))
