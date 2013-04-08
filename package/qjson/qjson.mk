ecfee1073251147f3cbdcbaa531eb5e2b61f4824
Zoltan Gyarmati <mr.zoltan.gyarmati@gmail.com>
2013-04-08 10:35:48 +0000

    qjson: new package
    [Peter: misc white space fixes]
    Signed-off-by: Zoltan Gyarmati <mr.zoltan.gyarmati@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/qjson/qjson.mk
@@ -0,0 +1,15 @@
+#############################################################
+#
+# qjson
+#
+#############################################################
+
+QJSON_VERSION = 0.8.1
+
+QJSON_SITE = http://github.com/flavio/qjson/tarball/$(QJSON_VERSION)
+QJSON_INSTALL_STAGING = YES
+QJSON_DEPENDENCIES =  qt
+QJSON_LICENSE = LGPLv2.1
+QJSON_LICENSE_FILES = COPYING.lib
+
+$(eval $(cmake-package))
