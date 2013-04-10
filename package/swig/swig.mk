65dc5b7ad2fb690774fa30af0a39048dc9cab1f0
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-04-10 10:29:25 +0000

    swig: new package
    Add the swig package, host-variant only since toolchain on the target is
    deprecated and this package wouldn't serve any purpose.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/swig/swig.mk
@@ -0,0 +1,14 @@
+#############################################################
+#
+# swig
+#
+#############################################################
+
+SWIG_VERSION = 2.0.9
+SWIG_SITE = http://downloads.sourceforge.net/project/swig/swig/swig-$(SWIG_VERSION)
+SWIG_DEPENDENCIES = host-bison
+HOST_SWIG_CONF_OPT = --without-pcre --disable-ccache
+SWIG_LICENSE = GPLv3+ BSD-2c BSD-3c
+SWIG_LICENSE_FILES = LICENSE LICENSE-GPL LICENSE-UNIVERSITIES
+
+$(eval $(host-autotools-package))
