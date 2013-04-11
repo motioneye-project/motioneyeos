23e5b60fdef5548fa7e5a4285ad22d08a32e8d3a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-04-11 04:41:35 +0000

    protobuf-c: new package
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/protobuf-c/protobuf-c.mk
@@ -0,0 +1,16 @@
+#############################################################
+#
+# protobuf-c
+#
+#############################################################
+
+PROTOBUF_C_VERSION = 0.15
+PROTOBUF_C_SITE = http://protobuf-c.googlecode.com/files
+PROTOBUF_C_DEPENDENCIES = protobuf host-protobuf-c
+HOST_PROTOBUF_C_DEPENDENCIES = host-protobuf
+PROTOBUF_C_CONF_OPT = --disable-protoc
+PROTOBUF_C_INSTALL_STAGING = YES
+PROTOBUF_C_LICENSE = BSD-3c
+
+$(eval $(autotools-package))
+$(eval $(host-autotools-package))
