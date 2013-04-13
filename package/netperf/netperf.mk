eecdea2aeaccdc028cf88e1575eefccfa0cb8c85
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-04-13 14:23:40 +0000

    netperf: bump to version 2.6.0
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/netperf/netperf.mk
+++ package/netperf/netperf.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-NETPERF_VERSION = 2.5.0
+NETPERF_VERSION = 2.6.0
 NETPERF_SITE = ftp://ftp.netperf.org/netperf
 NETPERF_CONF_ENV = ac_cv_func_setpgrp_void=set
 
