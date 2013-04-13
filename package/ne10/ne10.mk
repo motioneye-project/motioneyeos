318f3db0dcc052aa770ca57c143b5f7bc40cdaf3
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-04-13 08:09:45 +0000

    ne10: new package
    The Ne10 project has been set up to provide a set of common, useful
    functions which have been heavily optimized for the ARM Architecture
    and provide consistent well tested behavior that can be easily
    incorporated into applications. C interfaces to the functions are
    provided for both assembler and NEON implementations.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/ne10/ne10.mk
@@ -0,0 +1,39 @@
+#############################################################
+#
+# ne10
+#
+#############################################################
+
+# We use a Git commit ID because the last tagged version is more than
+# one year old.
+NE10_VERSION = 88c18f02199947b2c8b57796f5a3ca53160aff96
+NE10_SITE = git://github.com/projectNe10/Ne10.git
+NE10_LICENSE = BSD-3c or Apache 2.0
+NE10_LICENSE_FILES = doc/LICENSE
+
+ifeq ($(BR2_PREFER_STATIC_LIB),)
+NE10_CONF_OPT = \
+	-DNE10_BUILD_SHARED=ON
+endif
+
+# The package does not have any install target, so have to provide
+# INSTALL_STAGING_CMDS and INSTALL_TARGET_CMDS.
+
+ifeq ($(BR2_PREFER_STATIC_LIB),)
+define NE10_INSTALL_STAGING_SHARED_LIB
+	cp -dpf $(@D)/modules/libNE10*.so* $(STAGING_DIR)/usr/lib/
+endef
+endif
+
+define NE10_INSTALL_STAGING_CMDS
+	cp -dpf $(@D)/inc/NE10*h $(STAGING_DIR)/usr/include/
+	cp -dpf $(@D)/modules/libNE10.a $(STAGING_DIR)/usr/lib/
+	$(NE10_INSTALL_STAGING_SHARED_LIB)
+endef
+
+
+define NE10_INSTALL_TARGET_CMDS
+	cp -dpf $(@D)/modules/libNE10*.so* $(TARGET_DIR)/usr/lib/
+endef
+
+$(eval $(cmake-package))
