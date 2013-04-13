378ceef8c9ad36a93cc835e617ce3583affba06b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-04-13 06:56:49 +0000

    python-bottle: bump version and fix shebang
    Bump to version 0.11.6
    Add the --executable shebang fixup option to the build phase so that the
    script ends with a proper #!/usr/bin/python interpeter line set.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/python-bottle/python-bottle.mk
+++ package/python-bottle/python-bottle.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-PYTHON_BOTTLE_VERSION = 0.11.4
+PYTHON_BOTTLE_VERSION = 0.11.6
 PYTHON_BOTTLE_SOURCE = bottle-$(PYTHON_BOTTLE_VERSION).tar.gz
 PYTHON_BOTTLE_SITE = http://pypi.python.org/packages/source/b/bottle
 PYTHON_BOTTLE_DEPENDENCIES = python
@@ -12,7 +12,7 @@ PYTHON_BOTTLE_LICENSE = MIT
 # README.rst refers to the file "LICENSE" but it's not included
 
 define PYTHON_BOTTLE_BUILD_CMDS
-	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py build)
+	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py build --executable=/usr/bin/python)
 endef
 
 define PYTHON_BOTTLE_INSTALL_TARGET_CMDS
