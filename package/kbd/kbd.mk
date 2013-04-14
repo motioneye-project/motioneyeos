46ecd9117d0d74fd10c06ff322e87f25cf275d74
Simon Dawson <spdawson@gmail.com>
2013-04-14 21:48:56 +0000

    kbd: add license information
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/kbd/kbd.mk
+++ package/kbd/kbd.mk
@@ -1,6 +1,14 @@
+#############################################################
+#
+# kbd
+#
+#############################################################
+
 KBD_VERSION = 1.15.2
 KBD_SOURCE = kbd-$(KBD_VERSION).tar.gz
 KBD_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kbd
+KBD_LICENSE = GPLv2+
+KBD_LICENSE_FILES = COPYING
 
 KBD_DEPENDENCIES = $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext)
 
