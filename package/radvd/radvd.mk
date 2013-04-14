cbf8cfdbb22eb1490ddf78415541e9e710ef44ba
Peter Korsgaard <jacmet@sunsite.dk>
2013-04-14 20:40:53 +0200

    radvd: drop autoreconf
    We're no longer patching Makefile.am since the 1.9.2 bump, so we don't
    need to autoreconf anymore.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/radvd/radvd.mk
+++ package/radvd/radvd.mk
@@ -7,7 +7,6 @@
 RADVD_VERSION = 1.9.3
 RADVD_SITE = http://www.litech.org/radvd/dist
 RADVD_DEPENDENCIES = host-bison flex libdaemon host-flex host-pkgconf
-RADVD_AUTORECONF = YES
 
 define RADVD_INSTALL_INITSCRIPT
 	$(INSTALL) -m 0755 package/radvd/S50radvd $(TARGET_DIR)/etc/init.d
