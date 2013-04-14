e2a547fc53fe6c345d873ba19d9975d96b3ef484
Simon Dawson <spdawson@gmail.com>
2013-04-14 10:33:01 +0000

    zyre: new package
    [Peter: add comment about toolchain dependencies, fix license]
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/zyre/zyre.mk
@@ -0,0 +1,22 @@
+#############################################################
+#
+# zyre
+#
+#############################################################
+
+ZYRE_VERSION = 44a57a449a
+ZYRE_SITE = git://github.com/zeromq/zyre.git
+ZYRE_LICENSE = LGPLv3+
+ZYRE_LICENSE_FILES = COPYING COPYING.LESSER
+ZYRE_INSTALL_STAGING = YES
+ZYRE_DEPENDENCIES = filemq
+ZYRE_AUTORECONF = YES
+ZYRE_AUTORECONF_OPT = --install --force --verbose
+
+define ZYRE_CREATE_CONFIG_DIR
+	mkdir -p $(@D)/config
+endef
+
+ZYRE_POST_PATCH_HOOKS += ZYRE_CREATE_CONFIG_DIR
+
+$(eval $(autotools-package))
