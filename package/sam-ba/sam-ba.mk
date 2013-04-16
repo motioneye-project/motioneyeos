f39b706b087865112e351503ee51c60e5c2453f5
Simon Dawson <spdawson@gmail.com>
2013-04-16 22:04:15 +0000

    sam-ba: add license information
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sam-ba/sam-ba.mk
+++ package/sam-ba/sam-ba.mk
@@ -1,6 +1,14 @@
+#############################################################
+#
+# sam-ba
+#
+#############################################################
+
 SAM_BA_SITE    = http://www.atmel.com/dyn/resources/prod_documents/
 SAM_BA_VERSION = 2.12
 SAM_BA_SOURCE  = sam-ba_$(SAM_BA_VERSION).zip
+SAM_BA_LICENSE = BSD-like (partly binary-only)
+SAM_BA_LICENSE_FILES = doc/readme.txt
 
 define HOST_SAM_BA_EXTRACT_CMDS
         unzip -d $(BUILD_DIR) $(DL_DIR)/$(SAM_BA_SOURCE)
