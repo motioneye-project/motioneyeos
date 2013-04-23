85ffab574935ae07c090e05b5d720e1fb76569c7
Simon Dawson <spdawson@gmail.com>
2013-04-23 04:52:39 +0000

    libserial: new package
    Note that the Python binding is disabled; we can't build this in Buildroot
    without the sipconfig Python module for the host.
    
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libserial/libserial.mk
@@ -0,0 +1,13 @@
+#############################################################
+#
+# libserial
+#
+#############################################################
+
+LIBSERIAL_VERSION = 0.6.0rc1
+LIBSERIAL_SITE = http://downloads.sourceforge.net/libserial
+LIBSERIAL_INSTALL_STAGING = YES
+LIBSERIAL_LICENSE = GPLv2+
+LIBSERIAL_LICENSE_FILES = COPYING
+
+$(eval $(autotools-package))
