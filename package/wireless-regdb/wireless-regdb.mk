28431c2c0ee33dd192933954ca2e667624882503
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-04-23 08:59:02 +0000

    wireless-regdb: bump to version 2013.02.13 and style fixes
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/wireless-regdb/wireless-regdb.mk
+++ package/wireless-regdb/wireless-regdb.mk
@@ -4,15 +4,17 @@
 #
 #############################################################
 
-WIRELESS_REGDB_VERSION = 2013.01.11
+WIRELESS_REGDB_VERSION = 2013.02.13
 WIRELESS_REGDB_SOURCE = wireless-regdb-$(WIRELESS_REGDB_VERSION).tar.bz2
 WIRELESS_REGDB_SITE = http://kernel.org/pub/software/network/wireless-regdb
 WIRELESS_REGDB_LICENSE = ISC
 WIRELESS_REGDB_LICENSE_FILES = LICENSE
 
 define WIRELESS_REGDB_INSTALL_TARGET_CMDS
-    install -m 644 -D -T $(@D)/regulatory.bin $(TARGET_DIR)/usr/lib/crda/regulatory.bin
-    install -m 644 -D -T $(@D)/linville.key.pub.pem $(TARGET_DIR)/etc/wireless-regdb/pubkeys/linville.key.pub.pem
+	$(INSTALL) -m 644 -D -T $(@D)/regulatory.bin \
+		$(TARGET_DIR)/usr/lib/crda/regulatory.bin
+	$(INSTALL) -m 644 -D -T $(@D)/linville.key.pub.pem \
+		$(TARGET_DIR)/etc/wireless-regdb/pubkeys/linville.key.pub.pem
 endef
 
 $(eval $(generic-package))
