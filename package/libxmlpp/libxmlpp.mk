536ac0f7aaaef63e4c749219125fd1eeed5e76ab
nmenegale <nicolas.menegale@openwide.fr>
2013-04-24 05:11:23 +0000

    libxml++: new package
    libxml++ is a C++ wrapper for libxml2.
    
    [Peter: misc Config.in fixes]
    Signed-off-by: Nicolas Ménégale <nicolas.menegale@openwide.fr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libxmlpp/libxmlpp.mk
@@ -0,0 +1,17 @@
+#############################################################
+#
+# LibXML++
+#
+#############################################################
+
+LIBXMLPP_VERSION_MAJOR = 2.34
+LIBXMLPP_VERSION_MINOR = 2
+LIBXMLPP_VERSION = $(LIBXMLPP_VERSION_MAJOR).$(LIBXMLPP_VERSION_MINOR)
+LIBXMLPP_LICENSE = LGPLv2.1 (library), LGPLv2+ (examples)
+LIBXMLPP_LICENSE_FILE = COPYING
+LIBXMLPP_SOURCE = libxml++-$(LIBXMLPP_VERSION).tar.xz
+LIBXMLPP_SITE = http://ftp.gnome.org/pub/GNOME/sources/libxml++/$(LIBXMLPP_VERSION_MAJOR)
+LIBXMLPP_INSTALL_STAGING = YES
+LIBXMLPP_DEPENDENCIES = libxml2 glibmm host-pkgconf
+
+$(eval $(autotools-package))
