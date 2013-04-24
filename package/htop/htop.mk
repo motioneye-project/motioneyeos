9a4b2642aa0534d21b9377e763e8431e6eade115
gilles.talis@gmail.com <gilles.talis@gmail.com>
2013-04-24 12:57:04 +0000

    htop: bump to version 1.0.2
    Also add license information
    
    Signed-off-by: Gilles Talis <gilles.talis@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/htop/htop.mk
+++ package/htop/htop.mk
@@ -4,11 +4,13 @@
 #
 #############################################################
 
-HTOP_VERSION = 1.0.1
+HTOP_VERSION = 1.0.2
 HTOP_SITE = http://downloads.sourceforge.net/project/htop/htop/$(HTOP_VERSION)
 HTOP_DEPENDENCIES = ncurses
 HTOP_AUTORECONF = YES
 HTOP_CONF_OPT = --disable-unicode
 HTOP_CONF_ENV = ac_cv_file__proc_stat=yes ac_cv_file__proc_meminfo=yes
+HTOP_LICENSE = GPLv2
+HTOP_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
