1d8c988bc94f94a0a4e296f1520396735d928fc1
nmenegale <nicolas.menegale@openwide.fr>
2013-04-24 05:11:22 +0000

    glibmm: new package
    [Peter: fixup white space]
    Signed-off-by: Nicolas Ménégale  <nicolas.menegale@openwide.fr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/glibmm/glibmm.mk
@@ -0,0 +1,17 @@
+#############################################################
+#
+# Glibmm
+#
+#############################################################
+
+GLIBMM_VERSION_MAJOR = 2.30
+GLIBMM_VERSION_MINOR = 1
+GLIBMM_VERSION = $(GLIBMM_VERSION_MAJOR).$(GLIBMM_VERSION_MINOR)
+GLIBMM_LICENSE = LGPLv2.1+ (library), GPLv2+ (tools)
+GLIBMM_LICENSE_FILES = COPYING COPYING.tools
+GLIBMM_SOURCE = glibmm-$(GLIBMM_VERSION).tar.xz
+GLIBMM_SITE = http://ftp.gnome.org/pub/gnome/sources/glibmm/$(GLIBMM_VERSION_MAJOR)
+GLIBMM_INSTALL_STAGING = YES
+GLIBMM_DEPENDENCIES = libglib2 libsigc host-pkgconf
+
+$(eval $(autotools-package))
