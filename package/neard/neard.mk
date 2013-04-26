3f14d9baedaca2f55299b7b616b9939336196963
Simon Dawson <spdawson@gmail.com>
2013-04-26 01:25:48 +0000

    neard: add support for tools
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/neard/neard.mk
+++ package/neard/neard.mk
@@ -13,4 +13,8 @@ NEARD_AUTORECONF = YES
 NEARD_DEPENDENCIES = host-pkgconf dbus libglib2 libnl
 NEARD_CONF_OPT = --disable-traces
 
+ifeq ($(BR2_PACKAGE_NEARD_TOOLS),y)
+	NEARD_CONF_OPT += --enable-tools
+endif
+
 $(eval $(autotools-package))
