6235f6822274fa1d7989de258587074b101cdafe
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-04-27 10:09:46 +0000

    cryptodev: add new virtual
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/cryptodev/cryptodev.mk
@@ -0,0 +1,24 @@
+#############################################################
+#
+# Virtual cryptodev package
+#
+#############################################################
+
+CRYPTODEV_SOURCE =
+
+ifeq ($(BR2_PACKAGE_CRYPTODEV_LINUX),y)
+CRYPTODEV_DEPENDENCIES += cryptodev-linux
+endif
+
+ifeq ($(BR2_PACKAGE_OCF_LINUX),y)
+CRYPTODEV_DEPENDENCIES += ocf-linux
+endif
+
+ifeq ($(CRYPTODEV_DEPENDENCIES),)
+define CRYPTODEV_CONFIGURE_CMDS
+	echo "No CRYPTODEV implementation defined. Configuration error"
+	exit 1
+endef
+endif
+
+$(eval $(generic-package))
