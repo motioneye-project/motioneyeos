20e1e84272701413574633dafe207b40c2a5d6dd
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-04-27 10:09:44 +0000

    ocf-linux: remove extension and build out of tree
    Remove the OCF linux kernel extension instead opting to build ocf-linux
    modules out of tree.
    This is easier for users since no kernel config tweaking is required.
    On the downside the OCF drivers can't be used, but then all of the
    kernel crypto drivers are available to users via cryptosoft which is
    preferred.
    Also remove it from the menu to utilize a virtual.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ocf-linux/ocf-linux.mk
+++ package/ocf-linux/ocf-linux.mk
@@ -6,16 +6,22 @@
 
 OCF_LINUX_VERSION = 20120127
 OCF_LINUX_SITE = http://downloads.sourceforge.net/project/ocf-linux/ocf-linux/$(OCF_LINUX_VERSION)
+OCF_LINUX_DEPENDENCIES = linux
 OCF_LINUX_INSTALL_STAGING = YES
+
+define OCF_LINUX_BUILD_CMDS
+	$(MAKE) -C $(@D)/ocf $(LINUX_MAKE_FLAGS) KDIR=$(LINUX_DIR) \
+		ocf_modules
+endef
+
+define OCF_LINUX_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D)/ocf $(LINUX_MAKE_FLAGS) KDIR=$(LINUX_DIR) \
+		ocf_install
+endef
 
 define OCF_LINUX_INSTALL_STAGING_CMDS
 	$(INSTALL) -D -m 644 $(@D)/ocf/cryptodev.h \
 		$(STAGING_DIR)/usr/include/crypto/cryptodev.h
-endef
-
-define OCF_LINUX_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 644 $(@D)/ocf/cryptodev.h \
-		$(TARGET_DIR)/usr/include/crypto/cryptodev.h
 endef
 
 $(eval $(generic-package))
