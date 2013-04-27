1c2d3183e2e75bd21bc4b7377e76be74ce6e1432
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-04-27 10:09:45 +0000

    cryptodev-linux: new package
    cryptodev-linux is an alternative implementation to ocf-linux.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/cryptodev-linux/cryptodev-linux.mk
@@ -0,0 +1,28 @@
+#############################################################
+#
+# cryptodev-linux
+#
+#############################################################
+
+CRYPTODEV_LINUX_VERSION = 1.6
+CRYPTODEV_LINUX_SITE = http://download.gna.org/cryptodev-linux
+CRYPTODEV_LINUX_DEPENDENCIES = linux
+CRYPTODEV_LINUX_INSTALL_STAGING = YES
+CRYPTODEV_LICENSE = GPLv2+
+CRYPTODEV_LICENSE_FILES = COPYING
+
+define CRYPTODEV_LINUX_BUILD_CMDS
+	$(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) KERNEL_DIR=$(LINUX_DIR)
+endef
+
+define CRYPTODEV_LINUX_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) KERNEL_DIR=$(LINUX_DIR) \
+		PREFIX=$(TARGET_DIR) modules_install
+endef
+
+define CRYPTODEV_LINUX_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 644 $(@D)/crypto/cryptodev.h \
+		$(STAGING_DIR)/usr/include/crypto/cryptodev.h
+endef
+
+$(eval $(generic-package))
