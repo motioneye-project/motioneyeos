b1c79ec15e724939d7ef2b2357d1f25de3a940f0
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-04-28 02:31:24 +0000

    nettle: bump to version 2.7
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/nettle/nettle.mk
+++ package/nettle/nettle.mk
@@ -1,9 +1,21 @@
-NETTLE_VERSION = 2.6
+NETTLE_VERSION = 2.7
 NETTLE_SITE = http://www.lysator.liu.se/~nisse/archive
 NETTLE_DEPENDENCIES = gmp
 NETTLE_INSTALL_STAGING = YES
 NETTLE_LICENSE = LGPLv2.1+
 NETTLE_LICENSE_FILES = COPYING.LIB
+
+# ARM assembly requires v6+ ISA
+ifeq ($(BR2_arm7tdmi)$(BR2_arm720t)$(BR2_arm920t)$(BR2_arm922t)$(BR2_arm926t)$(BR2_arm10t)$(BR2_fa526)$(BR2_strongarm)$(BR2_xscale)$(BR2_iwmmxt),y)
+NETTLE_CONF_OPT += --disable-assembler
+endif
+
+# ARM NEON
+ifeq ($(BR2_ARM_CPU_HAS_NEON),y)
+NETTLE_CONF_OPT += --enable-arm-neon
+else
+NETTLE_CONF_OPT += --disable-arm-neon
+endif
 
 define NETTLE_DITCH_DEBUGGING_CFLAGS
 	$(SED) '/CFLAGS/ s/ -ggdb3//' $(@D)/configure
