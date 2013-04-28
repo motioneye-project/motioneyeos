1655f3c5b735b3dad75df1665b47d309fdf3afcb
Peter Korsgaard <jacmet@sunsite.dk>
2013-04-28 22:32:39 +0200

    sdl: fix directfb configure check
    The configure script nicely supports a preconfigured directfb-config
    (using ac_cv_path_DIRECTFBCONFIG), except for when it checks the directfb
    version where it simply uses whatever directfb-config is in the path,
    breaking the build when this isn't available / recent enough.
    
    Fix is to use the correct one instead. As SDL doesn't autoreconf cleanly,
    also patch the generated configure script.
    
    Finally move the ac_cv_path_DIRECTFBCONFIG setting under the directfb
    conditional as it is only needed there.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sdl/sdl.mk
+++ package/sdl/sdl.mk
@@ -8,7 +8,6 @@ SDL_VERSION = 1.2.15
 SDL_SOURCE = SDL-$(SDL_VERSION).tar.gz
 SDL_SITE = http://www.libsdl.org/release
 SDL_INSTALL_STAGING = YES
-SDL_CONF_ENV = ac_cv_path_DIRECTFBCONFIG=$(STAGING_DIR)/usr/bin/directfb-config
 
 ifeq ($(BR2_PACKAGE_SDL_FBCON),y)
 SDL_CONF_OPT+=--enable-video-fbcon=yes
@@ -19,6 +18,7 @@ endif
 ifeq ($(BR2_PACKAGE_SDL_DIRECTFB),y)
 SDL_DEPENDENCIES += directfb
 SDL_CONF_OPT+=--enable-video-directfb=yes
+SDL_CONF_ENV = ac_cv_path_DIRECTFBCONFIG=$(STAGING_DIR)/usr/bin/directfb-config
 else
 SDL_CONF_OPT=--enable-video-directfb=no
 endif
