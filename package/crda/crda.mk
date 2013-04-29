9a5820465e79497a1304dcbce9a835dfb1197e6c
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-04-29 08:57:49 +0000

    crda: new package
    Based on the one from Dimitrios Siganos <dimitris@siganos.org> but with
    a proper host-python-m2crypto instead of patching.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/crda/crda.mk
@@ -0,0 +1,23 @@
+#############################################################
+#
+# crda
+#
+#############################################################
+
+CRDA_VERSION = 1.1.3
+CRDA_SOURCE = crda-$(CRDA_VERSION).tar.bz2
+CRDA_SITE = http://wireless.kernel.org/download/crda
+CRDA_DEPENDENCIES = host-pkgconf host-python-m2crypto \
+	libnl libgcrypt
+CRDA_LICENSE = ISC
+CRDA_LICENSE_FILES = LICENSE
+
+define CRDA_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) all_noverify -C $(@D)
+endef
+
+define CRDA_INSTALL_TARGET_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) install -C $(@D) DESTDIR=$(TARGET_DIR)
+endef
+
+$(eval $(generic-package))
