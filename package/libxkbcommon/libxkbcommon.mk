67a96673830383028018f254611a9968b9efeffc
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-04-29 10:26:03 +0000

    libxkbcommon: new package
    [Peter: set gnu99 C mode]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libxkbcommon/libxkbcommon.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# libxkbcommon
+#
+#############################################################
+
+LIBXKBCOMMON_VERSION = 0.3.0
+LIBXKBCOMMON_SITE = http://xkbcommon.org/download/
+LIBXKBCOMMON_SOURCE = libxkbcommon-$(LIBXKBCOMMON_VERSION).tar.xz
+LIBXKBCOMMON_LICENSE = MIT/X11
+LIBXKBCOMMON_LICENSE_FILES = COPYING
+
+LIBXKBCOMMON_INSTALL_STAGING = YES
+LIBXKBCOMMON_DEPENDENCIES = host-bison host-flex
+# uses C99 features
+LIBXKBCOMMON_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -std=gnu99"
+
+$(eval $(autotools-package))
