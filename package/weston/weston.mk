be33cb9ecae2b4427a23b213f44b96c3dbd1d540
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-04-29 10:26:05 +0000

    weston: new package
    [Peter: fix udev comment, add upstream URL]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/weston/weston.mk
@@ -0,0 +1,32 @@
+#############################################################
+#
+# weston
+#
+#############################################################
+
+WESTON_VERSION = 1.1.0
+WESTON_SITE = http://wayland.freedesktop.org/releases/
+WESTON_SOURCE = weston-$(WAYLAND_VERSION).tar.xz
+WESTON_LICENSE = MIT
+WESTON_LICENSE_FILES = COPYING
+
+WESTON_DEPENDENCIES = wayland libxkbcommon pixman libpng jpeg mtdev udev
+WESTON_CONF_OPT = \
+	--disable-egl \
+	--disable-xwayland \
+	--disable-x11-compositor \
+	--disable-drm-compositor \
+	--disable-wayland-compositor \
+	--disable-headless-compositor \
+	--disable-rpi-compositor \
+	--disable-weston-launch \
+	--disable-libunwind
+
+ifeq ($(BR2_PACKAGE_WESTON_FBDEV),y)
+WESTON_CONF_OPT += --enable-fbdev-compositor
+else
+WESTON_CONF_OPT += --disable-fbdev-compositor
+endif
+
+$(eval $(autotools-package))
+
