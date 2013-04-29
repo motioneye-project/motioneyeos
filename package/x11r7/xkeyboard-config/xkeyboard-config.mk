dde83bc4639a7bf80ddd0d66b9ea3a8cc0157d65
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-04-29 10:26:02 +0000

    xkeyboard-config: make available outside of X.org, reduce dependencies
    xkeyboard-connfig was selectable only if X.org was enabled. However,
    weston, the reference implementation of the Wayland protocol, also
    needs xkeyboard-config, so we have to make this package available
    outside of the if BR2_PACKAGE_XORG7 ... endif conditional.
    
    In addition to this, the xkeyboard-config currently pulls in
    xapp_xkbcomp as a runtime dependency, but this dependency is only
    needed with X.org. And it also pulls in xlib_libX11 and xproto_proto
    as build-time dependencies. But in fact those ones are runtime
    dependencies, and they are only needed under X.org. This helps
    reducing the number of dependencies of xkeyboard-config in a
    weston/wayland configuration.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xkeyboard-config/xkeyboard-config.mk
+++ package/x11r7/xkeyboard-config/xkeyboard-config.mk
@@ -3,10 +3,15 @@
 # xkeyboard-config
 #
 #############################################################
+
 XKEYBOARD_CONFIG_VERSION = 2.6
 XKEYBOARD_CONFIG_SOURCE = xkeyboard-config-$(XKEYBOARD_CONFIG_VERSION).tar.bz2
 XKEYBOARD_CONFIG_SITE = http://www.x.org/releases/individual/data/xkeyboard-config/
-XKEYBOARD_CONFIG_DEPENDENCIES = host-gettext host-intltool host-xapp_xkbcomp xlib_libX11 xproto_xproto
+XKEYBOARD_CONFIG_LICENSE = MIT
+XKEYBAORD_CONFIG_LICENSE_FILES = COPYING
+
+XKEYBOARD_CONFIG_DEPENDENCIES = host-gettext host-intltool host-xapp_xkbcomp
+XKEYBOARD_CONFIG_CONF_OPT += --disable-runtime-deps
 
 $(eval $(autotools-package))
 
