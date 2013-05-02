3cb0f841e7fb72acbe56844b8bdd42696cf678bc
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-05-02 06:15:55 +0000

    iw: bump to version 3.10
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/iw/iw.mk
+++ package/iw/iw.mk
@@ -4,27 +4,21 @@
 #
 #############################################################
 
-IW_VERSION = 3.8
+IW_VERSION = 3.10
 IW_SOURCE = iw-$(IW_VERSION).tar.bz2
 IW_SITE = $(BR2_KERNEL_MIRROR)/software/network/iw
 IW_LICENSE = iw license
 IW_LICENSE_FILES = COPYING
 IW_DEPENDENCIES = host-pkgconf libnl
-IW_CONFIG = $(IW_DIR)/.config
+IW_MAKE_OPT = CC="$(TARGET_CC)" LD="$(TARGET_LD)" LDFLAGS="$(TARGET_LDFLAGS)"
 IW_MAKE_ENV = PKG_CONFIG="$(HOST_DIR)/usr/bin/pkg-config" \
 	GIT_DIR=$(IW_DIR)
 
 ifeq ($(BR2_PREFER_STATIC_LIB),y)
 # libnl needs pthread/m, so we need to explicitly with them when static
 # these need to added AFTER libnl, so we have to override LIBS completely
-IW_MAKE_OPT = LIBS='-lnl-genl-3 -lnl-3 -lpthread -lm'
+IW_MAKE_OPT += LIBS='-lnl-genl-3 -lnl-3 -lpthread -lm'
 endif
-
-define IW_CONFIGURE_CMDS
-	echo "CC = $(TARGET_CC)" >$(IW_CONFIG)
-	echo "CFLAGS = $(TARGET_CFLAGS)" >>$(IW_CONFIG)
-	echo "LDFLAGS = $(TARGET_LDFLAGS)" >>$(IW_CONFIG)
-endef
 
 define IW_BUILD_CMDS
 	$(IW_MAKE_ENV) $(MAKE) $(IW_MAKE_OPT) -C $(@D)
