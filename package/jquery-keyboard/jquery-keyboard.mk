58a73c6ab55d9eadaecfda3ea0ed23d07bb7fca7
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-05-03 10:11:05 +0000

    jquery-keyboard: new package
    Add the jquery-keyboard package based on Simon Dawson's patch with a
    newer version and improvements (license, select jquery, install
    layouts).
    
    [Peter: don't use install -D when destination is a directory]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/jquery-keyboard/jquery-keyboard.mk
@@ -0,0 +1,25 @@
+#############################################################
+#
+# jquery-keyboard
+#
+#############################################################
+
+JQUERY_KEYBOARD_VERSION = 1.17.4
+JQUERY_KEYBOARD_SITE = \
+	http://github.com/Mottie/Keyboard/tarball/v$(JQUERY_KEYBOARD_VERSION)
+JQUERY_KEYBOARD_LICENSE = MIT WTFPL
+JQUERY_KEYBOARD_LICENSE_FILES = README.markdown
+
+define JQUERY_KEYBOARD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0644 -D $(@D)/css/keyboard.css \
+		$(TARGET_DIR)/var/www/css/keyboard.css
+	$(INSTALL) -m 0644 $(@D)/js/jquery.keyboard*.js \
+		$(TARGET_DIR)/var/www
+	$(INSTALL) -m 0644 -D $(@D)/js/jquery.mousewheel.js \
+		$(TARGET_DIR)/var/www/jquery.mousewheel.js
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/var/www/layouts
+	$(INSTALL) -m 0644 $(@D)/layouts/*.js \
+		$(TARGET_DIR)/var/www/layouts
+endef
+
+$(eval $(call generic-package))
