ae47deffa16d5a78e790696a189bfb023c3754cb
Simon Dawson <spdawson@gmail.com>
2013-05-03 07:49:02 +0000

    inotify-tools: install to staging
    inotify-tools includes a shared library, which should be installed in the
    staging directory
    
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/inotify-tools/inotify-tools.mk
+++ package/inotify-tools/inotify-tools.mk
@@ -3,9 +3,11 @@
 # inotify-utils
 #
 #############################################################
+
 INOTIFY_TOOLS_VERSION = 3.14
 INOTIFY_TOOLS_SITE = http://github.com/downloads/rvoicilas/inotify-tools/
 INOTIFY_TOOLS_LICENSE = GPL
 INOTIFY_TOOLS_LICENSE_FILES = COPYING
+INOTIFY_TOOLS_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
