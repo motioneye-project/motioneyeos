b51099dc91b49e4a26cc616620974d0cd0fdd97d
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-05-05 03:14:35 +0000

    ltp-testsuite: fix build breakage with libcap
    ltp-testsuite needs libcap with file attributes which in turn needs
    attr.
    Only libcap is checked for so if it lacks file attr support it breaks.
    Disable libcap support unless it's got attr support, fixes:
    http://autobuild.buildroot.net/results/b16b20963c7cb17d8e220a5baa1356372ebf85a6/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ltp-testsuite/ltp-testsuite.mk
+++ package/ltp-testsuite/ltp-testsuite.mk
@@ -3,10 +3,18 @@
 # ltp-testsuite
 #
 #############################################################
+
 LTP_TESTSUITE_VERSION = 20130109
 LTP_TESTSUITE_SOURCE  = ltp-full-$(LTP_TESTSUITE_VERSION).bz2
 LTP_TESTSUITE_SITE    = http://downloads.sourceforge.net/project/ltp/LTP%20Source/ltp-$(LTP_TESTSUITE_VERSION)
 LTP_TESTSUITE_LICENSE = GPLv2 GPLv2+
 LTP_TESTSUITE_LICENSE_FILES = COPYING
+
+# Needs libcap with file attrs which needs attr, so both required
+ifeq ($(BR2_PACKAGE_LIBCAP)$(BR2_PACKAGE_ATTR),yy)
+LTP_TESTSUITE_DEPENDENCIES += libcap
+else
+LTP_TESTSUITE_CONF_ENV += ac_cv_lib_cap_cap_compare=no
+endif
 
 $(eval $(autotools-package))
