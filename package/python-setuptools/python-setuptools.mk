2ffad2a1f2f6abd60769bd7f2753ebe3d9ca0396
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-05-06 03:15:22 +0000

    python-setuptools: forkbump and add workaround for shebangs
    Switch to distribute fork since it's maintained and fixes a few issues
    while bumping to version 0.6.36.
    There's work upstream in re-merging with setuptools which is now
    supposedly maintained, see
    http://mail.python.org/pipermail/distutils-sig/2013-March/020126.html
    
    Buildroot python packages should use --single-version-externally-managed
    and --root=/ in the install phase even if they're not setuptools-based.
    This is because the default is to build egg files (zip files) for
    packages to support multiple target versions, and since the packages
    should be handled by buildroot infrastructure that's not needed.
    Also potentially doing double-compression is time-wasting (i.e. egg
    file and target filesystem) when running on slow targets.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/python-setuptools/python-setuptools.mk
+++ package/python-setuptools/python-setuptools.mk
@@ -4,9 +4,14 @@
 #
 #############################################################
 
-PYTHON_SETUPTOOLS_VERSION = 0.6c11
-PYTHON_SETUPTOOLS_SOURCE  = setuptools-$(PYTHON_SETUPTOOLS_VERSION).tar.gz
-PYTHON_SETUPTOOLS_SITE    = http://pypi.python.org/packages/source/s/setuptools
+# "distribute" is a fork of the unmaintained setuptools package. There
+# are plans to re-merge it into setuptools; if this happens, we can
+# switch back to it.
+# See http://pypi.python.org/packages/source/s/setuptools
+
+PYTHON_SETUPTOOLS_VERSION = 0.6.36
+PYTHON_SETUPTOOLS_SOURCE  = distribute-$(PYTHON_SETUPTOOLS_VERSION).tar.gz
+PYTHON_SETUPTOOLS_SITE    = http://pypi.python.org/packages/source/d/distribute
 PYTHON_SETUPTOOLS_DEPENDENCIES = python
 
 define HOST_PYTHON_SETUPTOOLS_BUILD_CMDS
@@ -14,19 +19,23 @@ define HOST_PYTHON_SETUPTOOLS_BUILD_CMDS
 endef
 
 define PYTHON_SETUPTOOLS_BUILD_CMDS
-	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py build)
+	(cd $(@D); \
+	PYTHONPATH="/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages" \
+	$(HOST_DIR)/usr/bin/python setup.py build)
 endef
 
 define HOST_PYTHON_SETUPTOOLS_INSTALL_CMDS
-	(cd $(@D); PYTHONPATH="$(HOST_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages"\
+	(cd $(@D); \
+	PYTHONPATH="$(HOST_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages" \
 	$(HOST_DIR)/usr/bin/python setup.py install --prefix=$(HOST_DIR)/usr)
 endef
 
 define PYTHON_SETUPTOOLS_INSTALL_TARGET_CMDS
-	(cd $(@D); PYTHONPATH="$(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages"\
-	$(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
+	(cd $(@D); \
+	PYTHONPATH="/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages" \
+	$(HOST_DIR)/usr/bin/python setup.py install --executable=/usr/bin/python \
+	--single-version-externally-managed --root=/ --prefix=$(TARGET_DIR)/usr)
 endef
 
 $(eval $(generic-package))
 $(eval $(host-generic-package))
-
