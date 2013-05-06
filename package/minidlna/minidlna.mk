7bf659ab111b91384af30cd219b539e2f5080ae1
Simon Dawson <spdawson@gmail.com>
2013-05-06 08:13:10 +0100

    minidlna: new package
    [Peter: license is GPLv2, fix license file, drop unneeded standard include]
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/minidlna/minidlna.mk
@@ -0,0 +1,43 @@
+################################################################################
+#
+# minidlna
+#
+################################################################################
+
+MINIDLNA_VERSION = 1.0.25
+MINIDLNA_SITE = http://downloads.sourceforge.net/project/minidlna/minidlna/$(MINIDLNA_VERSION)
+MINIDLNA_SOURCE = minidlna_$(MINIDLNA_VERSION)_src.tar.gz
+MINIDLNA_LICENSE = GPLv2 BSD-3c
+MINIDLNA_LICENSE_FILES = LICENCE LICENCE.miniupnpd
+
+MINIDLNA_DEPENDENCIES = \
+	ffmpeg flac libvorbis libogg libid3tag libexif libjpeg sqlite \
+	host-xutil_makedepend
+
+MINIDLNA_CFLAGS=$(TARGET_CFLAGS) \
+	-I"$(STAGING_DIR)/usr/include/libavutil" \
+	-I"$(STAGING_DIR)/usr/include/libavcodec" \
+	-I"$(STAGING_DIR)/usr/include/libavformat"
+
+define MINIDLNA_BUILD_CMDS
+	PREFIX=$(STAGING_DIR)/usr \
+		$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
+		CFLAGS="$(MINIDLNA_CFLAGS)" -C $(@D) depend
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
+		CFLAGS="$(MINIDLNA_CFLAGS)" -C $(@D) all
+endef
+
+define MINIDLNA_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
+		-C $(@D) DESTDIR=$(TARGET_DIR) install
+endef
+
+define MINIDLNA_UNINSTALL_TARGET_CMDS
+	$(RM) $(TARGET_DIR)/usr/sbin/minidlna
+endef
+
+define MINIDLNA_CLEAN_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) clean
+endef
+
+$(eval $(generic-package))
