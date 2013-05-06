8ee4b6946ef87533ad29d1c2438b1d4ffbf8ca5c
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-05-06 07:02:57 +0000

    ebtables: fix static build
    ebtables has a funky way of building the static version.
    Fixes:
    http://autobuild.buildroot.net/results/f5916d84194b6aa02d8eca7cbbb4e7867bb6afd4/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ebtables/ebtables.mk
+++ package/ebtables/ebtables.mk
@@ -9,11 +9,19 @@ EBTABLES_SOURCE = ebtables-v$(EBTABLES_VERSION).tar.gz
 EBTABLES_SITE = http://downloads.sourceforge.net/project/ebtables/ebtables/ebtables-$(EBTABLES_VERSION)
 EBTABLES_LICENSE = GPLv2
 EBTABLES_LICENSE_FILES = COPYING
+EBTABLES_STATIC = $(if $(BR2_PREFER_STATIC_LIB),static)
 
 define EBTABLES_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) LIBDIR=/lib/ebtables -C $(@D)
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) LIBDIR=/lib/ebtables $(EBTABLES_STATIC) \
+		-C $(@D)
 endef
 
+ifeq ($(BR2_PREFER_STATIC_LIB),y)
+define EBTABLES_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/$(EBTABLES_SUBDIR)/static \
+		$(TARGET_DIR)/sbin/ebtables
+endef
+else
 define EBTABLES_INSTALL_TARGET_CMDS
 	for so in $(@D)/$(EBTABLES_SUBDIR)/*.so \
 		$(@D)/$(EBTABLES_SUBDIR)/extensions/*.so; \
@@ -24,6 +32,7 @@ define EBTABLES_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/$(EBTABLES_SUBDIR)/ebtables \
 		$(TARGET_DIR)/sbin/ebtables
 endef
+endif
 
 define EBTABLES_UNINSTALL_TARGET_CMDS
 	rm -rf $(TARGET_DIR)/lib/ebtables
