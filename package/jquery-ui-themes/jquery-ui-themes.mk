ea1ece13e219a592e0ea3a4f9d7f1c6d1ef86fb0
Peter Korsgaard <jacmet@sunsite.dk>
2013-05-06 23:52:41 +0200

    jquery-ui-themes: bump version
    The base theme is no longer available in the themes download (it is the
    standard theme installed by jquery-ui), so default to ui-lightness instead.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/jquery-ui-themes/jquery-ui-themes.mk
+++ package/jquery-ui-themes/jquery-ui-themes.mk
@@ -4,8 +4,8 @@
 #
 #############################################################
 
-JQUERY_UI_THEMES_VERSION = 1.8.21
-JQUERY_UI_THEMES_SITE = http://jquery-ui.googlecode.com/files
+JQUERY_UI_THEMES_VERSION = 1.10.3
+JQUERY_UI_THEMES_SITE = http://jqueryui.com/resources/download
 JQUERY_UI_THEMES_SOURCE = jquery-ui-themes-$(JQUERY_UI_THEMES_VERSION).zip
 JQUERY_UI_THEMES_LICENSE = MIT
 JQUERY_UI_THEMES_LICENSE_FILES = MIT-LICENSE.txt
