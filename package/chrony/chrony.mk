f183c7362cba836b1dbde37f77fbb6f3e0d7406b
Nathan Lynch <ntl@pobox.com>
2013-05-07 07:16:19 +0000

    chrony: new package
    Signed-off-by: Nathan Lynch <ntl@pobox.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/chrony/chrony.mk
@@ -0,0 +1,46 @@
+#############################################################
+#
+# chrony
+#
+#############################################################
+
+CHRONY_VERSION = 1.27
+CHRONY_SITE = http://download.tuxfamily.org/chrony/
+CHRONY_LICENSE = GPLv2
+CHRONY_LICENSE_FILES = COPYING
+
+CHRONY_CONF_OPT = --host-system=Linux --host-machine=$(BR2_ARCH) --prefix=/usr
+
+ifeq ($(BR2_PACKAGE_LIBNSS),y)
+CHRONY_DEPENDENCIES += host-pkgconf libnss
+else
+CHRONY_CONF_OPT += --without-nss
+endif
+
+ifeq ($(BR2_PACKAGE_READLINE),y)
+CHRONY_DEPENDENCIES += readline
+else
+CHRONY_CONF_OPT += --disable-readline
+endif
+
+ifneq ($(BR2_INET_IPV6),y)
+CHRONY_CONF_OPT += --disable-ipv6
+endif
+
+define CHRONY_CONFIGURE_CMDS
+	cd $(@D) && $(TARGET_CONFIGURE_OPTS) ./configure $(CHRONY_CONF_OPT)
+endef
+
+define CHRONY_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define CHRONY_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR="$(TARGET_DIR)" install
+endef
+
+define CHRONY_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/bin/chronyc $(TARGET_DIR)/usr/sbin/chronyd
+endef
+
+$(eval $(generic-package))
