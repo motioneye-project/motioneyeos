f2a007480cab92c359432b9fa307eb48c5510086
Spenser Gilliland <spenser@gillilanding.com>
2013-05-08 13:55:26 -0500

    lockdev: add lockdev
    Add lockdev a package for locking devices.
    
    [Thomas: change location in menuconfig, reformat header in .mk file.]
    
    Signed-off-by: Spenser Gilliland <spenser@gillilanding.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/lockdev/lockdev.mk
@@ -0,0 +1,41 @@
+################################################################################
+#
+# lockdev
+#
+################################################################################
+
+LOCKDEV_VERSION = 1.0.3
+LOCKDEV_SOURCE = lockdev_$(LOCKDEV_VERSION).orig.tar.gz
+LOCKDEV_SITE = http://snapshot.debian.org/archive/debian/20130508T154517Z/pool/main/l/lockdev/
+LOCKDEV_LICENSE = LGPLv2.1
+LOCKDEV_LICENSE_FILES = LICENSE
+LOCKDEV_INSTALL_STAGING = YES
+
+ifeq ($(BR2_PREFER_STATIC_LIB),y)
+define LOCKDEV_BUILD_CMDS
+	$(MAKE1) $(TARGET_CONFIGURE_OPTS) -C $(@D) static
+endef
+
+define LOCKDEV_INSTALL_STAGING_CMDS
+	$(MAKE1) basedir=$(STAGING_DIR)/usr -C $(@D) install_dev
+endef
+
+else # BR2_PREFER_STATIC_LIB
+
+define LOCKDEV_BUILD_CMDS
+	$(MAKE1) $(TARGET_CONFIGURE_OPTS) -C $(@D) static shared
+endef
+
+define LOCKDEV_INSTALL_STAGING_CMDS
+	$(MAKE1) basedir=$(STAGING_DIR)/usr -C $(@D) install_dev install_run
+	ln -sf liblockdev.$(LOCKDEV_VERSION).so $(STAGING_DIR)/usr/lib/liblockdev.so
+	ln -sf liblockdev.$(LOCKDEV_VERSION).so $(STAGING_DIR)/usr/lib/liblockdev.so.1
+endef
+
+define LOCKDEV_INSTALL_TARGET_CMDS
+	$(MAKE1) basedir=$(TARGET_DIR)/usr -C $(@D) install_run
+	ln -sf liblockdev.$(LOCKDEV_VERSION).so $(STAGING_DIR)/usr/lib/liblockdev.so.1
+endef
+endif # BR2_PREFER_STATIC_LIB
+
+$(eval $(generic-package))
