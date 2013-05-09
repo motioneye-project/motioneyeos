a5d5b097a42afeda02ca93b7e83d17d4dbde02c3
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-05-09 05:51:04 +0000

    libatomic_ops: add license informations
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libatomic_ops/libatomic_ops.mk
+++ package/libatomic_ops/libatomic_ops.mk
@@ -7,6 +7,16 @@
 LIBATOMIC_OPS_VERSION = 7.2
 LIBATOMIC_OPS_SOURCE = libatomic_ops-$(LIBATOMIC_OPS_VERSION).tar.gz
 LIBATOMIC_OPS_SITE = http://www.hpl.hp.com/research/linux/atomic_ops/download
+
+# From doc/LICENSING.txt: "Our intent is to make it easy to use
+# libatomic_ops, in both free and proprietary software.  Hence most
+# code that we expect to be linked into a client application is
+# covered by an MIT-style license. A few library routines are covered
+# by the GNU General Public License. These are put into a separate
+# library, libatomic_ops_gpl.a."
+LIBATOMIC_OPS_LICENSE = MIT (main library) / GPLv2+ (gpl extension)
+LIBATOMIC_OPS_LICENSE_FILES = doc/LICENSING.txt doc/COPYING
+
 LIBATOMIC_OPS_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
