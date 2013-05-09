48ac24985c0dcf37c1951da9f7f4d4ae18ee1266
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-05-09 05:51:06 +0000

    libsha1: add license informations
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libsha1/libsha1.mk
+++ package/libsha1/libsha1.mk
@@ -6,6 +6,9 @@
 
 LIBSHA1_VERSION = 0.3
 LIBSHA1_SITE = http://github.com/dottedmag/libsha1/tarball/$(LIBSHA1_VERSION)
+LIBSHA1_LICENSE = BSD-3c or GPL
+LIBSHA1_LICENSE_FILES = COPYING
+
 LIBSHA1_INSTALL_STAGING = YES
 
 # We're getting the source code from Github, so there is no generated
