971d3f1fbc620277eb8a5108cd825b497bcd3870
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-05-10 06:29:38 +0000

    sane-backends: handle sane-config script
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sane-backends/sane-backends.mk
+++ package/sane-backends/sane-backends.mk
@@ -6,6 +6,7 @@
 SANE_BACKENDS_VERSION = 1.0.22
 SANE_BACKENDS_SOURCE = sane-backends-$(SANE_BACKENDS_VERSION).tar.gz
 SANE_BACKENDS_SITE = https://alioth.debian.org/frs/download.php/3503
+SANE_BACKENDS_CONFIG_SCRIPTS = sane-config
 
 ifeq ($(BR2_PACKAGE_LIBUSB),y)
 SANE_BACKENDS_DEPENDENCIES += libusb
