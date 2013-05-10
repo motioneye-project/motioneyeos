e62d01535ab78e8ff80b730e9b2df903f0747d0c
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-05-10 06:29:27 +0000

    libart: handle libart2-config script
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libart/libart.mk
+++ package/libart/libart.mk
@@ -9,5 +9,6 @@ LIBART_SOURCE = libart_lgpl-$(LIBART_VERSION).tar.gz
 LIBART_SITE = http://ftp.gnome.org/pub/gnome/sources/libart_lgpl/2.3/
 LIBART_AUTORECONF = YES
 LIBART_INSTALL_STAGING = YES
+LIBART_CONFIG_SCRIPTS = libart2-config
 
 $(eval $(autotools-package))
