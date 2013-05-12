9b273ea860504666cf204eb475421e21cbcac639
Fabio Porcedda <fabio.porcedda@gmail.com>
2013-05-12 21:58:40 +0000

    linux: CONFIG_KERNEL_LZO option requires host-lzop
    When the LZO compression mode is chosen the linux kernel requires
    lzop host utility at build time.
    
    Signed-off-by: Fabio Porcedda <fabio.porcedda@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- linux/linux.mk
+++ linux/linux.mk
@@ -34,7 +34,7 @@ endif
 LINUX_PATCHES = $(call qstrip,$(BR2_LINUX_KERNEL_PATCH))
 
 LINUX_INSTALL_IMAGES = YES
-LINUX_DEPENDENCIES  += host-module-init-tools
+LINUX_DEPENDENCIES  += host-module-init-tools host-lzop
 
 ifeq ($(BR2_LINUX_KERNEL_UBOOT_IMAGE),y)
 	LINUX_DEPENDENCIES += host-uboot-tools
