fb1ba5dff1748226c13cb92320f5171c026a47ca
gilles.talis@gmail.com <gilles.talis@gmail.com>
2013-05-15 11:47:32 +0000

    libeXosip2: disable multi-thread support for toolchains without threads
    Also added license information
    
    Signed-off-by: Gilles Talis <gilles.talis@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libeXosip2/libeXosip2.mk
+++ package/libeXosip2/libeXosip2.mk
@@ -7,6 +7,8 @@
 LIBEXOSIP2_VERSION = 3.6.0
 LIBEXOSIP2_SITE = http://download.savannah.gnu.org/releases/exosip/
 LIBEXOSIP2_INSTALL_STAGING = YES
+LIBEXOSIP2_LICENSE = GPLv2+
+LIBEXOSIP2_LICENSE_FILES = COPYING
 
 LIBEXOSIP2_DEPENDENCIES = host-pkgconf libosip2
 
@@ -16,5 +18,8 @@ LIBEXOSIP2_CONF_OPT += --enable-openssl
 else
 LIBEXOSIP2_CONF_OPT += --disable-openssl
 endif
+
+LIBEXOSIP2_CONF_OPT += \
+	--enable-mt=$(if $(BR2_TOOLCHAIN_HAS_THREADS),yes,no)
 
 $(eval $(autotools-package))
