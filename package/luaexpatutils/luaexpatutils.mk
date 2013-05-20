0b20c1aa33638bdf6cbcddd7a01ce6a78e25a382
Assaf Inbal <shmuelzon@gmail.com>
2013-05-20 15:56:40 +0300

    luaexpatutils: New package
    lxp.doc is a module that provides useful features for working with XML
    formats in LOM format as used by the LuaExpat project from Kepler.
    
    [Peter: use fixed git revision, github tarball, install -D]
    Signed-off-by: Assaf Inbal <shmuelzon@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/luaexpatutils/luaexpatutils.mk
@@ -0,0 +1,21 @@
+#############################################################
+#
+# luaexpatutils
+#
+#############################################################
+
+LUAEXPATUTILS_VERSION = 88c228365b
+LUAEXPATUTILS_SITE = http://github.com/stevedonovan/LuaExpatUtils/tarball/$(LUAEXPATUTILS_VERSION)
+LUAEXPATUTILS_LICENSE = Public Domain
+LUAEXPATUTILS_DEPENDENCIES = luaexpat
+
+define LUAEXPATUTILS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0644 -D $(@D)/lua/doc.lua \
+		$(TARGET_DIR)/usr/share/lua/lxp/doc.lua
+endef
+
+define LUAEXPATUTILS_UNINSTALL_TARGET_CMDS
+	rm -f $(TARGET_DIR)/usr/share/lua/lxp/doc.lua
+endef
+
+$(eval $(generic-package))
