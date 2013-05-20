02e4326fe7f1912a6f12bdef54c7295f9188dd38
Assaf Inbal <shmuelzon@gmail.com>
2013-05-20 15:56:44 +0300

    orbit: New package
    An MVC web framework for Lua
    
    [Thomas P: add patch to fix installation procedure, and use the
    provided Makefile for installation. Remove build-time dependency on
    wsapi since there's nothing to build in 'orbit'. Remove run-time
    dependency on 'rings', since it's not clear why Orbit would depend on
    that.]
    
    Signed-off-by: Assaf Inbal <shmuelzon@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/orbit/orbit.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# orbit
+#
+################################################################################
+
+ORBIT_VERSION = 2.2.0
+ORBIT_SITE = http://github.com/downloads/keplerproject/orbit
+ORBIT_LICENSE = MIT
+ORBIT_LICENSE_FILES = COPYRIGHT
+
+define ORBIT_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) \
+		LUA_DIR=$(TARGET_DIR)/usr/share/lua/ \
+		BIN_DIR=$(TARGET_DIR)/usr/bin \
+		install
+endef
+
+$(eval $(generic-package))
