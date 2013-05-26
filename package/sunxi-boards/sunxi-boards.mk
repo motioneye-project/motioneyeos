fc40219173997b87d46ef53f497f0496bbf60cac
Carlo Caione <carlo.caione@gmail.com>
2013-05-26 12:08:22 +0000

    sunxi-boards: new package
    [Peter: depend on arm, wrap help, change to _FEX_FILE option, error on empty]
    Signed-off-by: Carlo Caione <carlo.caione@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/sunxi-boards/sunxi-boards.mk
@@ -0,0 +1,28 @@
+#############################################################
+#
+# sunxi-boards
+#
+#############################################################
+
+SUNXI_BOARDS_VERSION = 88d663db44f65b73ef65c4148a28c6fa3665d2b6
+SUNXI_BOARDS_SITE = https://github.com/linux-sunxi/sunxi-boards/tarball/master
+SUNXI_BOARDS_DEPENDENCIES = host-sunxi-tools
+SUNXI_BOARDS_INSTALL_IMAGES = YES
+SUNXI_BOARDS_INSTALL_TARGET = NO
+SUNXI_BOARDS_FEX_FILE = $(call qstrip,$(BR2_PACKAGE_SUNXI_BOARDS_FEX_FILE))
+
+define SUNXI_BOARDS_INSTALL_IMAGES_CMDS
+	$(FEX2BIN) $(@D)/sys_config/$(SUNXI_BOARDS_FEX_FILE) \
+		$(BINARIES_DIR)/script.bin
+endef
+
+ifeq ($(BR2_PACKAGE_SUNXI_BOARDS),y)
+# we NEED a board name
+ifeq ($(filter source,$(MAKECMDGOALS)),)
+ifeq ($(SUNXI_BOARDS_FEX_FILE),)
+$(error No sunxi .fex file specified. Check your BR2_PACKAGE_SUNXI_BOARDS_FEX_FILE settings)
+endif
+endif
+endif
+
+$(eval $(generic-package))
