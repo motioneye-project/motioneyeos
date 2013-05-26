e2e33e2cb56d7cdb04259366ba076cd168576cbe
Henk Fijnvandraat <h.fijnvandraat@inter.nl.net>
2013-05-26 12:08:24 +0000

    freescale-imx: new package directory
    The freescale-imx directory is intended to contain all freescale
    i.MXyy related packages, together with their download site and version
    info
    
    Signed-off-by: Henk Fijnvandraat <h.fijnvandraat@inter.nl.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/freescale-imx/freescale-imx.mk
@@ -0,0 +1,13 @@
+#############################################################
+#
+# freescale-imx
+#
+#############################################################
+
+FREESCALE_IMX_VERSION = 12.09.01
+
+# No official download site from freescale, just this mirror
+FREESCALE_IMX_MIRROR_SITE   = http://download.ossystems.com.br/bsp/freescale/source
+
+include package/freescale-imx/*/*.mk
+
