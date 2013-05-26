a94650c184f78620eb63d2d25065c4ea3ceec8d8
Simon Dawson <spdawson@gmail.com>
2013-05-26 23:16:55 +0000

    neard: add init script
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/neard/neard.mk
+++ package/neard/neard.mk
@@ -17,4 +17,13 @@ ifeq ($(BR2_PACKAGE_NEARD_TOOLS),y)
 	NEARD_CONF_OPT += --enable-tools
 endif
 
+define NEARD_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 package/neard/S53neard \
+		$(TARGET_DIR)/etc/init.d/S53neard
+endef
+
+define NEARD_UNINSTALL_INIT_SYSV
+	$(RM) $(TARGET_DIR)/etc/init.d/S53neard
+endef
+
 $(eval $(autotools-package))
