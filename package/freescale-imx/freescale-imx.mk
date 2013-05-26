41a65bf5be0724e8d36832e330a18a01e3845658
Henk Fijnvandraat <h.fijnvandraat@inter.nl.net>
2013-05-26 12:08:27 +0000

    freescale-imx: bump to 1.1.0
    This commit bumps both imx-lib and firmware-imx to version 1.1.0.
    
    Signed-off-by: Henk Fijnvandraat <h.fijnvandraat@inter.nl.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/freescale-imx/freescale-imx.mk
+++ package/freescale-imx/freescale-imx.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-FREESCALE_IMX_VERSION = 12.09.01
+FREESCALE_IMX_VERSION = 1.1.0
 
 # No official download site from freescale, just this mirror
 FREESCALE_IMX_MIRROR_SITE   = http://download.ossystems.com.br/bsp/freescale/source
