c95ba8a487ba751134f63e2bbf45c5a09ee870a3
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-05-28 08:21:29 +0000

    lttng-babeltrace: fix build failure on gcc14
    The gcc14 machine has a moderately old host gcc, which was causing a
    recurrent build failure of host-lttng-babeltrace:
    
     http://autobuild.buildroot.org/results/492/49216052c161874f41738e41e6e0c89a6dd04000/build-end.log
    
    This commit adds a patch to fix this build failure, related to the
    access of the ULLONG_MAX define on compilers that did not default to
    the C99 variant of the C language.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/lttng-babeltrace/lttng-babeltrace.mk
+++ package/lttng-babeltrace/lttng-babeltrace.mk
@@ -4,5 +4,9 @@ LTTNG_BABELTRACE_SOURCE  = babeltrace-$(LTTNG_BABELTRACE_VERSION).tar.bz2
 
 LTTNG_BABELTRACE_DEPENDENCIES = popt util-linux libglib2
 
+# One patch touches configure.ac
+LTTNG_BABELTRACE_AUTORECONF = YES
+HOST_LTTNG_BABELTRACE_AUTORECONF = YES
+
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
