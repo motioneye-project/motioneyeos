e87a062e19d65d52af7f5b547e0068f09d7d33b5
Peter Korsgaard <jacmet@sunsite.dk>
2013-06-02 23:16:32 +0200

    intltool: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/intltool/intltool.mk
+++ package/intltool/intltool.mk
@@ -4,9 +4,11 @@
 #
 #############################################################
 
-INTLTOOL_VERSION = 0.40.6
-INTLTOOL_SOURCE = intltool-$(INTLTOOL_VERSION).tar.bz2
-INTLTOOL_SITE = http://ftp.acc.umu.se/pub/GNOME/sources/intltool/0.40/
+INTLTOOL_VERSION = 0.50.2
+INTLTOOL_SOURCE = intltool-$(INTLTOOL_VERSION).tar.gz
+INTLTOOL_SITE = https://launchpad.net/intltool/trunk/$(INTLTOOL_VERSION)/+download/
+INTLTOOL_LICENSE = GPLv2+
+INTLTOOL_LICENSE_FILES = COPYING
 
 HOST_INTLTOOL_DEPENDENCIES = host-gettext host-libxml-parser-perl
 HOST_INTLTOOL_CONF_OPT = \
