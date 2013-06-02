0f0f72c8792ff416621cdd354aca084f1c0c4e02
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-06-02 14:50:52 +0000

    p11-kit: new package
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/p11-kit/p11-kit.mk
@@ -0,0 +1,15 @@
+#############################################################
+#
+# p11-kit
+#
+#############################################################
+
+P11_KIT_VERSION = 0.18.1
+P11_KIT_SITE = http://p11-glue.freedesktop.org/releases
+P11_KIT_DEPENDENCIES = host-pkgconf libtasn1
+P11_KIT_INSTALL_STAGING = YES
+P11_KIT_CONF_OPT = --disable-static
+P11_KIT_LICENSE = BSD-3c
+P11_KIT_LICENSE_FILES = COPYING
+
+$(eval $(autotools-package))
