9c778689c4d443705f69ab0477747adc54aa5dbc
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-06-06 04:42:18 +0000

    p11-kit: fix __progname build failure
    Even though we've got UCLIBC_HAS___PROGNAME=y in our internal toolchains
    the build seems to fail under some circumstances.
    Since program_invocation_short_name is equally as useful just disable
    __progname usage. Fixes:
    http://autobuild.buildroot.net/results/cc8/cc861f5a034a0922a90fb6f22835a1fe8205d497/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/p11-kit/p11-kit.mk
+++ package/p11-kit/p11-kit.mk
@@ -10,7 +10,7 @@ P11_KIT_DEPENDENCIES = host-pkgconf libtasn1
 P11_KIT_INSTALL_STAGING = YES
 P11_KIT_CONF_OPT = --disable-static
 P11_KIT_CONF_ENV = ac_cv_have_decl_program_invocation_short_name=yes \
-	ac_cv_have_decl___progname=yes
+	ac_cv_have_decl___progname=no
 P11_KIT_LICENSE = BSD-3c
 P11_KIT_LICENSE_FILES = COPYING
 
