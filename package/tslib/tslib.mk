013365238f0b00f01dcc173edf2f3b6e37f7beb1
Spenser Gilliland <spenser@gillilanding.com>
2013-06-06 06:47:29 +0000

    tslib: fix github tarball to use commit id
    Signed-off-by: Spenser Gilliland <spenser@gillilanding.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tslib/tslib.mk
+++ package/tslib/tslib.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 TSLIB_VERSION = 158ee49b32f83cb7b02d5315f41c2e4cff38942d
-TSLIB_SITE = http://github.com/kergoth/tslib/tarball/master
+TSLIB_SITE = http://github.com/kergoth/tslib/tarball/$(TSLIB_VERSION)
 TSLIB_LICENSE = GPL, LGPL
 TSLIB_LICENSE_FILES = COPYING
 
