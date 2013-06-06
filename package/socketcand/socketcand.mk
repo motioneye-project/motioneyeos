fe64434df6c8a5f3516f92caf9c04d6dedda444b
Spenser Gilliland <spenser@gillilanding.com>
2013-06-06 06:47:25 +0000

    socketcand: fix github tarball to use commit id
    Signed-off-by: Spenser Gilliland <spenser@gillilanding.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/socketcand/socketcand.mk
+++ package/socketcand/socketcand.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 SOCKETCAND_VERSION = 7d06986
-SOCKETCAND_SITE = http://github.com/dschanoeh/socketcand/tarball/master
+SOCKETCAND_SITE = http://github.com/dschanoeh/socketcand/tarball/$(SOCKETCAND_VERSION)
 SOCKETCAND_AUTORECONF = YES
 SOCKETCAND_DEPENDENCIES = libconfig
 
