f7c5d92314061ff68c8f12d0788f65b1d979cf11
Peter Korsgaard <jacmet@sunsite.dk>
2013-06-06 16:38:04 +0200

    package: add tree
    Tree is a recursive directory listing command that produces a depth
    indented listing of files.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/tree/tree.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# tree
+#
+################################################################################
+
+TREE_VERSION = 1.6.0
+TREE_SOURCE = tree-$(TREE_VERSION).tgz
+TREE_SITE = http://mama.indstate.edu/users/ice/tree/src/
+TREE_LICENSE = GPLv2+
+TREE_LICENSE_FILES = LICENSE
+
+define TREE_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define TREE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/tree $(TARGET_DIR)/usr/bin/tree
+	$(INSTALL) -D -m 0644 $(@D)/doc/tree.1 \
+		$(TARGET_DIR)/usr/share/man/man1/tree.1
+endef
+
+define TREE_CLEAN_CMDS
+	-$(MAKE) -C $(@D) clean
+endef
+
+$(eval $(generic-package))
