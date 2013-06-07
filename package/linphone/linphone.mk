258bb8e1610e13d2d5dfaeb39fe8291fcabb3558
Markos Chandras <markos.chandras@imgtec.com>
2013-06-07 05:04:30 +0000

    linphone: Disable building with -Werror
    Pass --disable-strict to avoid having -Werror in CFLAGS.
    Having -Werror in CFLAGS can lead to build problems depending
    on the gcc version that is being used and it should not be used
    in released code.
    
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/linphone/linphone.mk
+++ package/linphone/linphone.mk
@@ -8,7 +8,8 @@ LINPHONE_VERSION = 3.5.2
 LINPHONE_SITE = http://download-mirror.savannah.gnu.org/releases/linphone/3.5.x/sources/
 LINPHONE_CONF_OPT = \
 	--enable-external-ortp \
-	--enable-external-mediastreamer
+	--enable-external-mediastreamer \
+	--disable-strict
 
 LINPHONE_DEPENDENCIES = host-pkgconf ortp mediastreamer libeXosip2 speex
 
