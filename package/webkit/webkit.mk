69e3d032ed0da9f246d39c5e04b63102004810f2
Spenser Gilliland <spenser@gillilanding.com>
2013-06-07 13:18:50 +0000

    webkit: bump to version 1.9.6
    Remove bison-2.6-compat patch as it has been incorporated upstream.
    Remove fix build issue with old gtk as gtk has been bumped to a newer
    version.
    Remove fix-build-with-gcc-4-6 as it has incorporated upstream.
    Remove directfb patches due to depreciation
    Remove pool patch as it is no longer required
    Remove pthread_getattr_np as webkit no longer uses this function.
    
    Signed-off-by: Spenser Gilliland <spenser@gillilanding.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/webkit/webkit.mk
+++ package/webkit/webkit.mk
@@ -4,27 +4,34 @@
 #
 ################################################################################
 
-WEBKIT_VERSION = 1.2.7
+WEBKIT_VERSION = 1.9.6
 WEBKIT_SITE = http://www.webkitgtk.org/releases
+WEBKIT_SOURCE = webkit-$(WEBKIT_VERSION).tar.xz
 WEBKIT_INSTALL_STAGING = YES
 WEBKIT_DEPENDENCIES = host-flex host-bison host-gperf icu libcurl libxml2 \
 	libxslt libgtk2 sqlite enchant libsoup jpeg libgail
 
+# webkit-disable-tests.patch changes configure.ac therefore autoreconf required
+WEBKIT_AUTORECONF = YES
+WEBKIT_AUTORECONF_OPT = -I $(@D)/Source/autotools
+
 # Give explicit path to icu-config, and silence gazillions of warnings
 # with recent gcc versions.
 WEBKIT_CONF_ENV = ac_cv_path_icu_config=$(STAGING_DIR)/usr/bin/icu-config \
-	CFLAGS="$(TARGET_CFLAGS) -Wno-cast-align" \
-	CXXFLAGS="$(TARGET_CXXFLAGS) -Wno-cast-align"
+	CFLAGS="$(TARGET_CFLAGS) -Wno-cast-align -Wno-sign-compare" \
+	CXXFLAGS="$(TARGET_CXXFLAGS) -Wno-cast-align -Wno-sign-compare"
 
+WEBKIT_CONF_OPT += \
+	--with-gtk=2.0 \
+	--disable-geolocation \
+	--disable-webkit2 \
+	--disable-glibtest \
+	--disable-video \
+	--disable-gtk-doc-html \
+	--disable-tests
 
-ifeq ($(BR2_PACKAGE_XORG7),y)
-	WEBKIT_CONF_OPT += --with-target=x11
-	WEBKIT_DEPENDENCIES += xlib_libXt
-else
-	WEBKIT_CONF_OPT += --with-target=directfb
-	WEBKIT_DEPENDENCIES += directfb
-endif
-
-WEBKIT_CONF_OPT += --disable-video
+# Xorg Dependencies
+WEBKIT_CONF_OPT += --with-target=x11
+WEBKIT_DEPENDENCIES += xlib_libXt
 
 $(eval $(autotools-package))
