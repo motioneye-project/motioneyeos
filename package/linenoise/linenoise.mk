41190ef0f1ffb68675bfe77205e9207363546377
Peter Korsgaard <jacmet@sunsite.dk>
2013-06-08 12:12:21 +0200

    linenoise: fix git commit id
    Somehow a 'g' had sneaked in before the commit id.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/linenoise/linenoise.mk
+++ package/linenoise/linenoise.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LINENOISE_VERSION = g27a3b4d
+LINENOISE_VERSION = 27a3b4d5
 LINENOISE_SITE = http://github.com/antirez/linenoise/tarball/$(LINENOISE_VERSION)
 LINENOISE_LICENSE = BSD-2c
 LINENOISE_INSTALL_STAGING = YES
