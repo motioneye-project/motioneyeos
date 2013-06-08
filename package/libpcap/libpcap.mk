8d4c3a7eefa26e804ec93f1a16e27cc7696c7213
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-06-08 03:52:59 +0000

    libpcap: bump to version 1.4.0
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libpcap/libpcap.mk
+++ package/libpcap/libpcap.mk
@@ -4,23 +4,13 @@
 #
 ################################################################################
 
-LIBPCAP_VERSION = 1.3.0
+LIBPCAP_VERSION = 1.4.0
 LIBPCAP_SITE = http://www.tcpdump.org/release
 LIBPCAP_LICENSE = BSD-3c
 LIBPCAP_LICENSE_FILES = LICENSE
 LIBPCAP_INSTALL_STAGING = YES
-
-# The configure.in file contains the necessary code to handle
-# --enable-shared/--disable-shared properly, but the configure script
-# bundled with the libpcap tarball is not up-to-date with the
-# configure.in file. In order to get proper handling of
-# --enable-shared/--disable-shared, which is needed when doing a
-# static library build, we autoreconfigure this package. This can be
-# removed when bumping libpcap, after checking that the bundled
-# configure script correctly handles --enable-shared/--disable-shared.
-LIBPCAP_AUTORECONF = YES
-
-LIBPCAP_DEPENDENCIES = zlib host-flex host-bison
+LIBPCAP_DEPENDENCIES = zlib host-flex host-bison \
+	$(if $(BR2_PACKAGE_LIBNL),libnl)
 LIBPCAP_CONF_ENV = ac_cv_linux_vers=2 \
 		ac_cv_header_linux_wireless_h=yes # configure misdetects this
 LIBPCAP_CONF_OPT = --disable-yydebug --with-pcap=linux
