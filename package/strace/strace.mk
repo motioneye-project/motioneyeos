45ef34d5cf28b4a96fcaea7aacae309ac3b4c51b
Chris Zankel <chris@zankel.net>
2013-06-09 14:35:44 -0700

    strace: upgrade to 4.8, supporting now aarch64, and xtensa
    Remove 'strace-fix-arm-bad-syscall.patch'. This patch had been integrated
    in v4.6 (commit: 9bc6340d2) and was later replaced with a generic solution
    in v.7 (commit: 2ce12ed31c2).
    
    Strace still cannot handle non-LFS environments, so a modified version of
    strace-fix-disabled-largefile-syscalls.patch remains. The 64-bit syscalls
    (sys_truncate64, etc.) are references in the sysent structure but the
    functinon definitions are commented out becuase of the missing LFS support.
    
    The workaround for the 'forced lfs mode' doesn't seem to be necessary anymore.
    
    Build tested on arm w and w/o LFS support.
    
    [Peter: arc still not supported]
    Signed-off-by: Chris Zankel <chris@zankel.net>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/strace/strace.mk
+++ package/strace/strace.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-STRACE_VERSION = 4.7
+STRACE_VERSION = 4.8
 STRACE_SOURCE = strace-$(STRACE_VERSION).tar.xz
 STRACE_SITE = http://downloads.sourceforge.net/project/strace/strace/$(STRACE_VERSION)
 STRACE_LICENSE = BSD-3c
@@ -12,13 +12,6 @@ STRACE_LICENSE_FILES = COPYRIGHT
 
 STRACE_CONF_ENV = ac_cv_header_linux_if_packet_h=yes \
 		  ac_cv_header_linux_netlink_h=yes
-
-ifeq ($(BR2_LARGEFILE),y)
-# strace gets confused when lfs mode is forced, so don't
-STRACE_CONF_ENV += \
-	CFLAGS="$(filter-out -D_FILE_OFFSET_BITS=64,$(TARGET_CFLAGS))" \
-	CPPFLAGS="$(filter-out -D_FILE_OFFSET_BITS=64,$(TARGET_CPPFLAGS))"
-endif
 
 define STRACE_REMOVE_STRACE_GRAPH
 	rm -f $(TARGET_DIR)/usr/bin/strace-graph
