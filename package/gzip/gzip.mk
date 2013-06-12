0396ef14403ca5ef25de42da6ac99470cbdefc27
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-06-12 11:14:27 -0300

    gzip: bump to version 1.6
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gzip/gzip.mk
+++ package/gzip/gzip.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GZIP_VERSION = 1.5
+GZIP_VERSION = 1.6
 GZIP_SITE = $(BR2_GNU_MIRROR)/gzip
 GZIP_LICENSE = GPLv3+
 GZIP_LICENSE_FILES = COPYING
