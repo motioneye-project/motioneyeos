df13ceb90c59f6bbe13c9f5fda5725a627dde941
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-06-12 11:14:28 -0300

    sdparm: bump to version 1.08
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/sdparm/sdparm.mk
+++ package/sdparm/sdparm.mk
@@ -1,6 +1,12 @@
-SDPARM_VERSION = 1.07
+################################################################################
+#
+# sdparm
+#
+################################################################################
+
+SDPARM_VERSION = 1.08
 SDPARM_SOURCE = sdparm-$(SDPARM_VERSION).tgz
-SDPARM_SITE = http://sg.danny.cz/sg/p/
+SDPARM_SITE = http://sg.danny.cz/sg/p
 SDPARM_LICENSE = BSD-3c
 SDPARM_LICENSE_FILES = COPYING
 
