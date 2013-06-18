53e498da2f2ebfc87c3de0fe18bc9132e94b4fcb
gilles.talis@gmail.com <gilles.talis@gmail.com>
2013-06-18 14:22:34 -0700

    socketcand: bump to version 0.3.1
    Since version 0.3.0, libconfig has been made optional
    
    Signed-off-by: Gilles Talis <gilles.talis@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/socketcand/socketcand.mk
+++ package/socketcand/socketcand.mk
@@ -4,9 +4,14 @@
 #
 ################################################################################
 
-SOCKETCAND_VERSION = 7d06986
+SOCKETCAND_VERSION = dc3437ab
 SOCKETCAND_SITE = http://github.com/dschanoeh/socketcand/tarball/$(SOCKETCAND_VERSION)
 SOCKETCAND_AUTORECONF = YES
+
+ifeq ($(BR2_PACKAGE_LIBCONFIG),y)
 SOCKETCAND_DEPENDENCIES = libconfig
+else
+SOCKETCAND_CONF_OPT = --without-config
+endif
 
 $(eval $(autotools-package))
