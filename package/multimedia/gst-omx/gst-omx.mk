a82f18ad8e7e167ecaffe9493a0bd823eabd1782
Spenser Gilliland <spenser@gillilanding.com>
2013-06-20 17:07:43 -0500

    gst-omx: add gst-omx package
    gst-omx enables OpenMAX based accelerated video decode.
    
    Signed-off-by: Spenser Gilliland <spenser@gillilanding.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/multimedia/gst-omx/gst-omx.mk
@@ -0,0 +1,37 @@
+#############################################################
+#
+# gst-omx
+#
+#############################################################
+
+GST_OMX_VERSION = 1.0.0
+GST_OMX_SOURCE = gst-omx-$(GST_OMX_VERSION).tar.xz
+GST_OMX_SITE = http://gstreamer.freedesktop.org/src/gst-omx/
+
+GST_OMX_LICENSE = LGPLv2.1
+GST_OMX_LICENSE_FILE = COPYING
+
+ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
+GST_OMX_CONF_OPT = \
+	--with-omx-target=rpi
+GST_OMX_CONF_ENV = \
+	CFLAGS="$(TARGET_CFLAGS) \
+		-I$(STAGING_DIR)/usr/include/IL \
+		-I$(STAGING_DIR)/usr/include/interface/vcos/pthreads \
+		-I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux"
+endif
+
+ifeq ($(BR2_PACKAGE_BELLAGIO),y)
+GST_OMX_CONF_OPT = \
+	--with-omx-target=bellagio
+GST_OMX_CONF_ENV = \
+	CFLAGS="$(TARGET_CFLAGS) \
+		-DOMX_VERSION_MAJOR=1 \
+		-DOMX_VERSION_MINOR=1 \
+		-DOMX_VERSION_REVISION=2 \
+		-DOMX_VERSION_STEP=0"
+endif
+
+GST_OMX_DEPENDENCIES = gstreamer1 gst1-plugins-base libopenmax
+
+$(eval $(autotools-package))
