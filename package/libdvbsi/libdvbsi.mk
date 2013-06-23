84f2dece13cbadd6f87c6223c1f452b5446633a4
Carsten Schoenert <c.schoenert@gmail.com>
2013-06-23 17:30:33 +0200

    package/libdvbsi: new package
    libdvbsi++ is a C++ library for parsing DVB SI sections and providing
    the output in C++ objects. It's usefull in any case you need prepared
    and preselected DVB SI data from the raw stream.
    
    [Peter: fix _LICENSE_FILES tag]
    Signed-off-by: Carsten Schoenert <c.schoenert@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libdvbsi/libdvbsi.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# libdvbsi++
+#
+################################################################################
+
+LIBDVBSI_VERSION = 0.3.6
+LIBDVBSI_SOURCE = libdvbsi++-$(LIBDVBSI_VERSION).tar.bz2
+LIBDVBSI_SITE = http://www.saftware.de/libdvbsi++
+LIBDVBSI_INSTALL_STAGING = YES
+LIBDVBSI_LICENSE = LGPLv2.1
+LIBDVBSI_LICENSE_FILES = COPYING
+
+# sometimes no Makefile is in the archive, just (re)generate
+LIBDVBSI_AUTORECONF=YES
+
+$(eval $(autotools-package))
