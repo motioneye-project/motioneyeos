0fcef3c9fe343ac2b54c07b0711210a96d993762
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-06-23 15:58:08 +0200

    lttng: bump the various LTTng components
    This commit bumps the various LTTng components to their latest stable
    version: 0.7.7 for liburcu, 1.1.1 for babeltrace, 2.1.3 for libust and
    2.1.2 for lttng-modules.
    
    For babeltrace, the patch
    lttng-babeltrace-02-fix-build-old-compilers.patch has been removed
    because it has been merged upstream as of version 1.1.1. This is also
    the reason why the package no longer needs to be autoreconfigured.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/liburcu/liburcu.mk
+++ package/liburcu/liburcu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBURCU_VERSION = 0.7.6
+LIBURCU_VERSION = 0.7.7
 LIBURCU_SITE    = http://lttng.org/files/urcu/
 LIBURCU_SOURCE  = userspace-rcu-$(LIBURCU_VERSION).tar.bz2
 
--- package/lttng-babeltrace/lttng-babeltrace.mk
+++ package/lttng-babeltrace/lttng-babeltrace.mk
@@ -5,14 +5,10 @@
 ################################################################################
 
 LTTNG_BABELTRACE_SITE    = http://lttng.org/files/babeltrace/
-LTTNG_BABELTRACE_VERSION = 1.0.2
+LTTNG_BABELTRACE_VERSION = 1.1.1
 LTTNG_BABELTRACE_SOURCE  = babeltrace-$(LTTNG_BABELTRACE_VERSION).tar.bz2
 
 LTTNG_BABELTRACE_DEPENDENCIES = popt util-linux libglib2
-
-# One patch touches configure.ac
-LTTNG_BABELTRACE_AUTORECONF = YES
-HOST_LTTNG_BABELTRACE_AUTORECONF = YES
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
