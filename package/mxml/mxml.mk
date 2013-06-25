dc6d9070b2141afc057ef92253cfbd8199876527
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2013-06-25 07:15:46 +0200

    mxml: fix site url
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/mxml/mxml.mk
+++ package/mxml/mxml.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 MXML_VERSION = 2.7
-MXML_SITE = http://ftp.easysw.com/pub/mxml/$(MXML_VERSION)
+MXML_SITE = http://www.msweet.org/files/project3
 MXML_LICENSE = LGPLv2+ with exceptions
 MXML_LICENSE_FILES = COPYING
 MXML_INSTALL_STAGING = YES
