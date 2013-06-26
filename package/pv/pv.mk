6b7e53d3616bcceb6c298d954b811e82d30481e6
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2013-06-26 21:50:28 +0200

    pv: bump to version 1.4.6
    [Peter: drop patch and instead pass LD= on the make cmdline]
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/pv/pv.mk
+++ package/pv/pv.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-PV_VERSION = 1.2.0
+PV_VERSION = 1.4.6
 PV_SOURCE = pv-$(PV_VERSION).tar.bz2
-PV_SITE = http://pipeviewer.googlecode.com/files
+PV_SITE = http://www.ivarch.com/programs/sources
+PV_MAKE_OPT = LD=$(TARGET_LD) # otherwise 'ld' is used
 
 $(eval $(autotools-package))
