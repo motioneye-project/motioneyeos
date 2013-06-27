79c55aadd54d098696644d77f9e60cd200e08753
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-06-27 11:49:14 -0300

    xlib_libXpm: needs host-gettext
    Fixes:
    http://autobuild.buildroot.net/results/00b/00b5c7745df5359908f6c77f90c1598b71638081/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/x11r7/xlib_libXpm/xlib_libXpm.mk
+++ package/x11r7/xlib_libXpm/xlib_libXpm.mk
@@ -10,6 +10,7 @@ XLIB_LIBXPM_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXPM_LICENSE = MIT
 XLIB_LIBXPM_LICENSE_FILES = COPYING
 XLIB_LIBXPM_INSTALL_STAGING = YES
-XLIB_LIBXPM_DEPENDENCIES = xlib_libX11 xlib_libXext xlib_libXt xproto_xproto
+XLIB_LIBXPM_DEPENDENCIES = xlib_libX11 xlib_libXext xlib_libXt xproto_xproto \
+	host-gettext
 
 $(eval $(autotools-package))
