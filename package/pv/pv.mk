3ba9f8231d3dd71303b0218679d6c384be76fc6e
Peter Korsgaard <jacmet@sunsite.dk>
2013-06-27 11:29:06 +0200

    pv: add license info
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/pv/pv.mk
+++ package/pv/pv.mk
@@ -7,6 +7,8 @@
 PV_VERSION = 1.4.6
 PV_SOURCE = pv-$(PV_VERSION).tar.bz2
 PV_SITE = http://www.ivarch.com/programs/sources
+PV_LICENSE = Artistic-2.0
+PV_LICENSE_FILES = doc/COPYING
 PV_MAKE_OPT = LD=$(TARGET_LD) # otherwise 'ld' is used
 
 $(eval $(autotools-package))
