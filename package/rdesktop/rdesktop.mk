9c3b56fb8de761abced19de129ba971dc1378965
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-06-28 10:41:12 -0300

    rdesktop: define license
    [Peter: license has an openssl exception]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/rdesktop/rdesktop.mk
+++ package/rdesktop/rdesktop.mk
@@ -9,5 +9,7 @@ RDESKTOP_SOURCE = rdesktop-$(RDESKTOP_VERSION).tar.gz
 RDESKTOP_SITE = http://downloads.sourceforge.net/project/rdesktop/rdesktop/$(RDESKTOP_VERSION)
 RDESKTOP_DEPENDENCIES = openssl xlib_libX11 xlib_libXt
 RDESKTOP_CONF_OPT = --with-openssl=$(STAGING_DIR)/usr
+RDESKTOP_LICENSE = GPLv2+ with exceptions
+RDESKTOP_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
