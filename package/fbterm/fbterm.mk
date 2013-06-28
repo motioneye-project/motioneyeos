38f96a6ce2786d2429ffe0ec23c1e6be46024311
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-06-28 10:41:07 -0300

    fbterm: define license
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/fbterm/fbterm.mk
+++ package/fbterm/fbterm.mk
@@ -5,8 +5,9 @@
 ################################################################################
 
 FBTERM_VERSION = 1.7.0
-FBTERM_SITE = http://fbterm.googlecode.com/files/
-
+FBTERM_SITE = http://fbterm.googlecode.com/files
+FBTERM_LICENSE = GPLv2+
+FBTERM_LICENSE_FILES = COPYING
 FBTERM_DEPENDENCIES = fontconfig liberation
 
 $(eval $(autotools-package))
