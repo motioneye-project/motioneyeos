c211a53d3fc4b19a527c632091534ef822696228
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-06-28 10:41:10 -0300

    libcap-ng: define license
    [Peter: license is LGPLv2.1+]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libcap-ng/libcap-ng.mk
+++ package/libcap-ng/libcap-ng.mk
@@ -10,5 +10,7 @@ LIBCAP_NG_SOURCE = libcap-ng-$(LIBCAP_NG_VERSION).tar.gz
 LIBCAP_NG_INSTALL_STAGING = YES
 LIBCAP_NG_CONF_ENV = ac_cv_prog_swig_found=no
 LIBCAP_NG_CONF_OPT = --without-python
+LIBCAP_NG_LICENSE = GPLv2+ LGPLv2.1+
+LIBCAP_NG_LICENSE_FILES = COPYING COPYING.LIB
 
 $(eval $(autotools-package))
