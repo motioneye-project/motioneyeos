e1b1e5f5b36ed9b39f46a80c333a42386088a57c
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-06-28 10:41:14 -0300

    parted: define license
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/parted/parted.mk
+++ package/parted/parted.mk
@@ -9,5 +9,7 @@ PARTED_SOURCE = parted-$(PARTED_VERSION).tar.xz
 PARTED_SITE = $(BR2_GNU_MIRROR)/parted
 PARTED_DEPENDENCIES = readline util-linux lvm2
 PARTED_INSTALL_STAGING = YES
+PARTED_LICENSE = GPLv3+
+PARTED_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
