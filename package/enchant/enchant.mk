303f4707d2d413176069d59a72d401b73094feb0
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-06-28 10:41:05 -0300

    enchant: define license
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/enchant/enchant.mk
+++ package/enchant/enchant.mk
@@ -9,5 +9,7 @@ ENCHANT_SOURCE = enchant-$(ENCHANT_VERSION).tar.gz
 ENCHANT_SITE = http://www.abisource.com/downloads/enchant/$(ENCHANT_VERSION)
 ENCHANT_INSTALL_STAGING = YES
 ENCHANT_DEPENDENCIES = libglib2 host-pkgconf
+ENCHANT_LICENSE = LGPLv2.1+
+ENCHANT_LICENSE_FILES = COPYING.LIB
 
 $(eval $(autotools-package))
