75cfdb1fd7db85f96dae9886c92ab60d30bf3f66
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-06-28 10:41:15 -0300

    ortp: define license
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ortp/ortp.mk
+++ package/ortp/ortp.mk
@@ -8,5 +8,7 @@ ORTP_VERSION = 0.20.0
 ORTP_SITE = http://download.savannah.nongnu.org/releases/linphone/ortp/sources
 ORTP_CONF_OPT = --disable-strict
 ORTP_INSTALL_STAGING = YES
+ORTP_LICENSE = LGPLv2.1+
+ORTP_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
