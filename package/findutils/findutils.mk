781771cf1e0f1599f648cc1cc2fb7d3f778535c7
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-06-28 10:41:08 -0300

    findutils: define license
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/findutils/findutils.mk
+++ package/findutils/findutils.mk
@@ -6,6 +6,8 @@
 
 FINDUTILS_VERSION = 4.4.2
 FINDUTILS_SITE = $(BR2_GNU_MIRROR)/findutils
+FINDUTILS_LICENSE = GPLv3+
+FINDUTILS_LICENSE_FILES = COPYING
 FINDUTILS_CONF_ENV = gl_cv_func_stdin=yes \
 			gl_cv_func_wcwidth_works=yes
 
