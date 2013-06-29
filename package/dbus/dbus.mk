0cf8595f2996b46143c1a95d3bd6f4e131a94b94
Danomi Manchego <danomimanchego123@gmail.com>
2013-06-29 09:27:04 -0400

    dbus: ensure directory exists before installing symlink
    If you use a custom target skeleton with no /var/lib directory,
    then the symlink installation in dbus.mk will fail.  This patch
    fixes this issue by creating /var/lib before creating a symlink
    there.
    
    Signed-off-by: Danomi Manchego <danomimanchego123@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/dbus/dbus.mk
+++ package/dbus/dbus.mk
@@ -66,6 +66,7 @@ DBUS_POST_INSTALL_TARGET_HOOKS += DBUS_REMOVE_DEVFILES
 endif
 
 define DBUS_INSTALL_TARGET_FIXUP
+	mkdir -p $(TARGET_DIR)/var/lib
 	rm -rf $(TARGET_DIR)/var/lib/dbus
 	ln -sf /tmp/dbus $(TARGET_DIR)/var/lib/dbus
 	$(INSTALL) -m 0755 -D package/dbus/S30dbus $(TARGET_DIR)/etc/init.d/S30dbus
