8d29893893fe25461abef645bfd38bf2f49c59a4
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-06-30 21:29:12 +0200

    eglibc: enable support in the Buildroot toolchain backend
    Using the newly introduced 'eglibc' package, this commit enables the
    option of building a toolchain using the eglibc C library in the
    Buildroot toolchain backend.
    
    In details, this commit:
    
     * Creates a choice to select uClibc or eglibc in the Buildroot
       toolchain backend (in toolchain/toolchain-buildroot/Config.in), and
       removes the fact that the Buildroot toolchain backend forcefully
       enables uClibc (toolchain/Config.in).
    
     * Creates a BUILDROOT_LIBC variables, which points to the package
       implementing the C library (i.e either 'uclibc' or 'eglibc').
    
     * Modifies the gcc-final and gcc-intermediate makefiles to use the
       BUILDROOT_LIBC variable instead of hardcoding the use of uclibc.
    
     * Ensures that TLS support is always enabled when building eglibc.
    
    [Peter: fix commit text to refer to BUILDROOT_LIBC]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- toolchain/toolchain-buildroot.mk
+++ toolchain/toolchain-buildroot.mk
@@ -2,4 +2,8 @@
 # build of binutils, uClibc, kernel headers and all the intermediate
 # gcc steps.
 
+include toolchain/helpers.mk
+
+BUILDROOT_LIBC = $(call qstrip,$(BR2_TOOLCHAIN_BUILDROOT_LIBC))
+
 toolchain-buildroot: host-gcc-final
