327748e9e83fc8a5f4101624c96faaf5221f50db
Samuel Martin <s.martin49@gmail.com>
2013-06-30 17:01:05 +0200

    liburcu: define license
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/liburcu/liburcu.mk
+++ package/liburcu/liburcu.mk
@@ -7,6 +7,8 @@
 LIBURCU_VERSION = 0.7.7
 LIBURCU_SITE    = http://lttng.org/files/urcu/
 LIBURCU_SOURCE  = userspace-rcu-$(LIBURCU_VERSION).tar.bz2
+LIBURCU_LICENSE = LGPLv2.1+ for the library; MIT-like license for few source files listed in LICENSE
+LIBURCU_LICENSE_FILES = lgpl-2.1.txt lgpl-relicensing.txt LICENSE
 
 LIBURCU_INSTALL_STAGING = YES
 
