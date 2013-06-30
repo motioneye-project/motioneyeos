d6e251d07c57c07f6bae0b7f40b9eedf49bb4f4a
Samuel Martin <s.martin49@gmail.com>
2013-06-30 17:01:09 +0200

    lttng-tools: define license
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/lttng-tools/lttng-tools.mk
+++ package/lttng-tools/lttng-tools.mk
@@ -7,6 +7,9 @@
 LTTNG_TOOLS_VERSION = 2.2.0
 LTTNG_TOOLS_SITE    = http://lttng.org/files/lttng-tools/
 LTTNG_TOOLS_SOURCE  = lttng-tools-$(LTTNG_TOOLS_VERSION).tar.bz2
+LTTNG_TOOLS_LICENSE = GPLv2; LGPLv2.1 for include/lttng/* and src/lib/lttng-ctl/*
+# gpl-2.0.txt and lgpl-2.1.txt files are missing from the sources
+LTTNG_TOOLS_LICENSE_FILES = LICENSE
 
 # The host-lttng-babeltrace technically isn't a required build
 # dependency. However, having the babeltrace utilities built for the
