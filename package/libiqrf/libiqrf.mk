aef9be3eb38ece0169ab1e70c45ea07ff0588288
Marek Belisko <marek.belisko@open-nandra.com>
2013-07-03 22:51:20 +0200

    libiqrf: Bump to v0.1.2 + update build system.
    v0.1.2 update build system from autotools to cmake.
    
    [Thomas: add patch to ensure the CMake build system only checks for a
    C compiler and not a C++ compiler]
    
    Signed-off-by: Marek Belisko <marek.belisko@open-nandra.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libiqrf/libiqrf.mk
+++ package/libiqrf/libiqrf.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-LIBIQRF_VERSION = v0.1.0
+LIBIQRF_VERSION = v0.1.2
 LIBIQRF_SITE = http://github.com/nandra/libiqrf/tarball/$(LIBIQRF_VERSION)
 LIBIQRF_INSTALL_STAGING = YES
 
 LIBIQRF_DEPENDENCIES = libusb
 
-$(eval $(autotools-package))
+$(eval $(cmake-package))
 
