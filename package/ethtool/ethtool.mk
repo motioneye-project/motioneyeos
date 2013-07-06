fbca9aa24ede06dd1bcdff630d1a7cb61266ef22
gilles.talis@gmail.com <gilles.talis@gmail.com>
2013-07-05 17:31:50 -0700

    ethtool: bump to version 3.10
    Signed-off-by: Gilles Talis <gilles.talis@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ETHTOOL_VERSION = 3.9
+ETHTOOL_VERSION = 3.10
 ETHTOOL_SITE = $(BR2_KERNEL_MIRROR)/software/network/ethtool
 ETHTOOL_LICENSE = GPLv2
 ETHTOOL_LICENSE_FILES = COPYING
