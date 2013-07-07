7ca310b0a0a38f193f573aac65628adac57ebf49
Yann E. MORIN <yann.morin.1998@free.fr>
2013-07-07 15:13:24 +0200

    package/rpi-firmware: bump version
    Fixes for:
      - sound volume
      - GL deadlock
      - ressources leak
      - compressed audio streams
      - audio sampling up to 192kHz
      - display scaling
      - cache flush for mailbox
    
    Switch to using a 7-char cset, instead of the long one.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = a0c98fa6bc0f93bad8cbfcf749f7be7dc2276192
+RPI_FIRMWARE_VERSION = 320084a
 RPI_FIRMWARE_SITE = http://github.com/raspberrypi/firmware/tarball/$(RPI_FIRMWARE_VERSION)
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
