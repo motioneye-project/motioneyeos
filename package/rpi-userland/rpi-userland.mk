14c9ef95a4db03d89f9683812173878807fcb0c1
Yann E. MORIN <yann.morin.1998@free.fr>
2013-07-07 15:13:26 +0200

    package/rpi-userland: bump version
    Misc fixes for audio and raspi-cam.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_USERLAND_VERSION = 986774edf7c91a8a91d59abadbdb0b34b5a387dc
+RPI_USERLAND_VERSION = 8b271ac
 RPI_USERLAND_SITE = http://github.com/raspberrypi/userland/tarball/$(RPI_USERLAND_VERSION)
 RPI_USERLAND_LICENSE = BSD-3c
 RPI_USERLAND_LICENSE_FILES = LICENCE
