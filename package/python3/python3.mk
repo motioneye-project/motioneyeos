c492a9a24af66e641d8e718620b00318fac89590
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-07-10 15:39:18 -0300

    python3: remove smtpd.py.3 sample
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/python3/python3.mk
+++ package/python3/python3.mk
@@ -141,6 +141,7 @@ define PYTHON3_REMOVE_USELESS_FILES
 	rm -f $(TARGET_DIR)/usr/bin/python$(PYTHON3_VERSION_MAJOR)-config
 	rm -f $(TARGET_DIR)/usr/bin/python$(PYTHON3_VERSION_MAJOR)m-config
 	rm -f $(TARGET_DIR)/usr/bin/python3-config
+	rm -f $(TARGET_DIR)/usr/bin/smtpd.py.3
 	for i in `find $(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/config-3.3m/ \
 		-type f -not -name pyconfig.h -a -not -name Makefile` ; do \
 		rm -f $$i ; \
