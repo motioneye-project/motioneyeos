0b7e6fdbc706b946b32fb91e146344e1c10f5483
gilles.talis@gmail.com <gilles.talis@gmail.com>
2013-07-10 10:25:41 -0700

    json-c: define license
    Signed-off-by: Gilles Talis <gilles.talis@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/json-c/json-c.mk
+++ package/json-c/json-c.mk
@@ -9,5 +9,7 @@ JSON_C_SITE = https://s3.amazonaws.com/json-c_releases/releases/
 JSON_C_INSTALL_STAGING = YES
 JSON_C_MAKE = $(MAKE1)
 JSON_C_CONF_OPT = --disable-oldname-compat
+JSON_C_LICENSE = MIT
+JSON_C_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
