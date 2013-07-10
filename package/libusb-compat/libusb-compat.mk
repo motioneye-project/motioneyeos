326915e43dc3a9f26d47fdb24200899d1822cd61
gilles.talis@gmail.com <gilles.talis@gmail.com>
2013-07-10 10:25:45 -0700

    libusb-compat: define license
    Signed-off-by: Gilles Talis <gilles.talis@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libusb-compat/libusb-compat.mk
+++ package/libusb-compat/libusb-compat.mk
@@ -10,6 +10,8 @@ LIBUSB_COMPAT_SITE = http://downloads.sourceforge.net/project/libusb/libusb-comp
 LIBUSB_COMPAT_DEPENDENCIES = host-pkgconf libusb
 LIBUSB_COMPAT_INSTALL_STAGING = YES
 LIBUSB_COMPAT_CONFIG_SCRIPTS = libusb-config
+LIBUSB_COMPAT_LICENSE = LGPLv2.1+
+LIBUSB_COMPAT_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
