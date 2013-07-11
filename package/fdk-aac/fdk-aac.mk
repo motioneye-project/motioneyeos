4ddff0099fa0619cfd1bcf5afa96f56a703de385
gilles.talis@gmail.com <gilles.talis@gmail.com>
2013-07-11 16:46:10 -0700

    fdk-aac: new package
    Signed-off-by: Gilles Talis <gilles.talis@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/fdk-aac/fdk-aac.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# fdk-aac
+#
+################################################################################
+
+FDK_AAC_VERSION = 0.1.1
+FDK_AAC_SITE = http://downloads.sourceforge.net/project/opencore-amr/fdk-aac
+FDK_AAC_LICENSE = fdk-aac license
+FDK_AAC_LICENSE_FILES = NOTICE
+
+FDK_AAC_INSTALL_STAGING = YES
+
+$(eval $(autotools-package))
