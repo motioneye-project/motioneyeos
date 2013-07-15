61900d9245806ce1387fac730a5fd926bf032443
Spenser Gilliland <spenser@gillilanding.com>
2013-07-15 16:44:18 -0500

    powervr: add powervr virtual package
    Signed-off-by: Spenser Gilliland <spenser@gillilanding.com>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Sundareson, Prabindh <prabu@ti.com>
    Cc: Peter Korsgaard <jacmet@uclibc.org>
    Cc: Sinan Akpolat <sinan@linkas.com.tr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/powervr/powervr.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# Virtual package for PowerVR driver
+#
+################################################################################
+
+POWERVR_SOURCE =
+
+ifeq ($(BR2_PACKAGE_TI_GFX),y)
+POWERVR_DEPENDENCIES += ti-gfx
+endif
+
+ifeq ($(POWERVR_DEPENDENCIES),)
+define POWERVR_CONFIGURE_CMDS
+	echo "No PowerVR implementation selected. Configuration error."
+	exit 1
+endef
+endif
+
+$(eval $(generic-package))
