f2e610da5a8dfd37ec4a963f0c99496a304d0b26
Yann E. MORIN <yann.morin.1998@free.fr>
2013-07-18 23:24:14 +0200

    package/nss-mdns: do not overwrite user's nsswitch.conf
    Instead of over-writing the nsswitch.conf file (which may already contain
    user's specific configuration if it comes from a custom skeleton), just
    replace the 'hosts' line.
    
    Remove now-useless custom nsswitch.conf file.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/nss-mdns/nss-mdns.mk
+++ package/nss-mdns/nss-mdns.mk
@@ -10,7 +10,8 @@ NSS_MDNS_SITE=http://0pointer.de/lennart/projects/nss-mdns
 NSS_MDNS_CONF_OPT += --localstatedir=/var
 
 define NSS_MDNS_INSTALL_CONFIG
-	$(INSTALL) -D -m 0664 package/nss-mdns/nsswitch.conf $(TARGET_DIR)/etc/nsswitch.conf
+	sed -r -i -e 's/^(hosts:[[:space:]]+).*/\1files mdns4_minimal [NOTFOUND=return] dns mdns4/' \
+	    $(TARGET_DIR)/etc/nsswitch.conf
 endef
 
 NSS_MDNS_POST_INSTALL_TARGET_HOOKS += NSS_MDNS_INSTALL_CONFIG
