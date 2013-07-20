396e92ed2e2aad55fbb2c0592c7e3f0063e0e065
Tzu-Jung Lee <roylee17@gmail.com>
2013-07-20 20:52:19 +0800

    tstools: new package
    [Thomas: properly indent help text, add largefile dependency, remove
    unneeded libpcap dependency, remove unneeded CLEAN_CMDS, reorder build
    and install commands, remove uneeded <pkg>_INSTALL_STAGING,
    <pkg>_INSTALL_TARGET and <pkg>_INSTALL_IMAGES variables, move from
    package/multimedia/ to package/, add license details, rename patch to
    remove version number in the patch filename.]
    
    Signed-off-by: Tzu-Jung Lee <tjlee@ambarella.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/tstools/tstools.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# tstools
+#
+################################################################################
+
+TSTOOLS_VERSION = 1_11
+TSTOOLS_SITE    = https://tstools.googlecode.com/files/
+TSTOOLS_SOURCE  = tstools-$(TSTOOLS_VERSION).tgz
+TSTOOLS_LICENSE = MPL v1.1
+
+define TSTOOLS_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define TSTOOLS_INSTALL_TARGET_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
+		DESTDIR=$(TARGET_DIR) install
+endef
+
+$(eval $(generic-package))
