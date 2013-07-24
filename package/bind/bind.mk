d8966bab7a49426d06029de9d12299bd32aff990
Thomas De Schampheleire <patrickdepinguin+buildroot@gmail.com>
2013-07-24 12:34:31 +0200

    bind: install to staging
    Install bind to staging so other applications can use its include files
    and libraries.
    
    Signed-off-by: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/bind/bind.mk
+++ package/bind/bind.mk
@@ -7,6 +7,7 @@
 BIND_VERSION = 9.6-ESV-R9-P1
 BIND_SITE = ftp://ftp.isc.org/isc/bind9/$(BIND_VERSION)
 BIND_MAKE = $(MAKE1)
+BIND_INSTALL_STAGING = YES
 BIND_LICENSE = ISC
 BIND_LICENSE_FILES = COPYRIGHT
 BIND_TARGET_SBINS = lwresd named named-checkconf named-checkzone
