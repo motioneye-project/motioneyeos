d0c0635fe2b50d8baff95f62b857e12a2cbb4167
Yegor Yefremov <yegorslists@googlemail.com>
2013-07-28 14:13:08 +0200

    ser2net: bump to 2.9
    SER2NET_AUTORECONF is needed because of libtool version mismatch.
    See error message bellow:
    
    /bin/bash ./libtool --tag=CC   --mode=link gcc -Wall -g -O2   -o
    ser2net controller.o dataxfer.o devcfg.o readconfig.o selector.o
    ser2net.o utils.o telnet.o buffer.o  -lnsl
    libtool: Version mismatch error.  This is libtool 2.4.2
    Debian-2.4.2-1ubuntu1, but the
    libtool: definition of this LT_INIT comes from libtool 2.2.6b.
    libtool: You should recreate aclocal.m4 with macros from libtool 2.4.2
    Debian-2.4.2-1ubuntu1
    libtool: and run autoconf again.
    make: *** [ser2net] Error 63
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ser2net/ser2net.mk
+++ package/ser2net/ser2net.mk
@@ -4,8 +4,10 @@
 #
 ################################################################################
 
-SER2NET_VERSION = 2.8
+SER2NET_VERSION = 2.9
 SER2NET_SITE = http://downloads.sourceforge.net/project/ser2net/ser2net
+# shipped configure script is broken (libtool version error)
+SER2NET_AUTORECONF = YES
 SER2NET_LICENSE = GPLv2+
 SER2NET_LICENSE_FILES = COPYING
 
