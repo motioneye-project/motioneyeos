6cabf13b2677bb760cbece8c914a6db8d0815637
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2013-08-02 10:57:31 +0200

    cppcms, libcec: fix license typo
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libcec/libcec.mk
+++ package/libcec/libcec.mk
@@ -7,7 +7,7 @@
 LIBCEC_VERSION = libcec-2.1.1
 LIBCEC_SITE = http://github.com/Pulse-Eight/libcec/tarball/$(LIBCEC_VERSION)
 LIBCEC_LICENSE = GPLv2+
-LIBCEC_LICENSE_FILE = COPYING
+LIBCEC_LICENSE_FILES = COPYING
 
 # Autoreconf required due to being a dev tarball and not a release tarball.
 LIBCEC_AUTORECONF = YES
