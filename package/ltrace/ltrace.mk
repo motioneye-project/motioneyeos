8a14dab8bdad1d819924d6039b16061fccef3df2
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-08-12 15:02:42 -0300

    ltrace: disable libunwind for uClibc toolchains
    ltrace requires that libunwind is built with backtrace() support.
    For the internal uClibc toolchain we don't enable it, and for external
    uClibc toolchains we can't know.
    It's also unavailable for static uClibc toolchains.
    So just disable libunwind support for uClibc toolchains in general.
    Fixes:
    http://autobuild.buildroot.net/results/ee0/ee037a19590fb85c64f97f78f74bcfd4d7766706/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ltrace/ltrace.mk
+++ package/ltrace/ltrace.mk
@@ -13,4 +13,15 @@ LTRACE_CONF_OPT = --disable-werror
 LTRACE_LICENSE = GPLv2
 LTRACE_LICENSE_FILES = COPYING
 
+# ltrace can use libunwind only if libc has backtrace() support
+# We don't normally do so for uClibc and we can't know if it's external
+ifeq ($(BR2_PACKAGE_LIBUNWIND),y)
+ifeq ($(BR2_TOOLCHAIN_USES_UCLIBC),)
+LTRACE_CONF_OPT += --with-libunwind=yes
+LTRACE_DEPENDENCIES += libunwind
+else
+LTRACE_CONF_OPT += --with-libunwind=no
+endif
+endif
+
 $(eval $(autotools-package))
