c1a6bcece5e5832cc8897b5b600dfb958cab88f0
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-08-12 10:37:46 -0300

    readline: use normal/default install target
    Since the custom INSTALL_TARGET doesn't consider static builds it breaks
    horribly and there's no need with the current version since it works
    just fine with a little varnish to remove samples. Fixes:
    http://autobuild.buildroot.net/results/5a6/5a62c38ec621d49230d76981db6024035e88804a/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/readline/readline.mk
+++ package/readline/readline.mk
@@ -13,14 +13,10 @@ READLINE_CONF_ENV = bash_cv_func_sigsetjmp=yes
 READLINE_LICENSE = GPLv3+
 READLINE_LICENSE_FILES = COPYING
 
-define READLINE_INSTALL_TARGET_CMDS
-	$(MAKE1) DESTDIR=$(TARGET_DIR) -C $(@D) uninstall
-	$(MAKE1) DESTDIR=$(TARGET_DIR) -C $(@D) install-shared uninstall-doc
-	chmod 775 $(TARGET_DIR)/usr/lib/libreadline.so.$(READLINE_VERSION) \
-		$(TARGET_DIR)/usr/lib/libhistory.so.$(READLINE_VERSION)
-	$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) \
-		$(TARGET_DIR)/usr/lib/libreadline.so.$(READLINE_VERSION) \
-		$(TARGET_DIR)/usr/lib/libhistory.so.$(READLINE_VERSION)
+define READLINE_PURGE_EXAMPLES
+	rm -rf $(TARGET_DIR)/usr/share/readline
 endef
+
+READLINE_POST_INSTALL_TARGET_HOOKS += READLINE_PURGE_EXAMPLES
 
 $(eval $(autotools-package))
