4ed05f0ff666f5be9b485ccc91b29825326fcd37
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-08-18 19:49:43 +0200

    qt5: change upstream URL
    The Qt5 upstream URL has changed, which leads to build failures in the
    autobuilders.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/qt5/qt5.mk
+++ package/qt5/qt5.mk
@@ -1,5 +1,5 @@
 QT5_VERSION = 5.0.2
-QT5_SITE = http://releases.qt-project.org/qt5/$(QT5_VERSION)/submodules/
+QT5_SITE = http://download.qt-project.org/archive/qt/5.0/$(QT5_VERSION)/submodules/
 include package/qt5/*/*.mk
 
 define QT5_LA_PRL_FILES_FIXUP
