58f6d0a285a085ce0299bf27ef50b6c51a6e3d1e
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-08-28 17:13:08 +0200

    kmod: bump to version 15
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/kmod/kmod.mk
+++ package/kmod/kmod.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KMOD_VERSION = 14
+KMOD_VERSION = 15
 KMOD_SOURCE = kmod-$(KMOD_VERSION).tar.xz
 KMOD_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kernel/kmod/
 KMOD_INSTALL_STAGING = YES
