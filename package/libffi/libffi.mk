7b5bd3a6ddd04cf32dce82485116f87c614a5ce4
nmenegale <nicolas.menegale@openwide.fr>
2013-08-28 11:48:59 +0200

    libffi: minor corrections in post install hooks
    In order to be sure that we just deal with the includes related with
    the actual package described into libffi.mk it is better to explicitly
    specify the version of the libffi we deal with into the post install
    hook.
    As dev files are deprecated in target rootfs target post install
    hook was also deleted.
    
    Signed-off-by: Nicolas Ménégale <nicolas.menegale@openwide.fr>
    Reviewed-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libffi/libffi.mk
+++ package/libffi/libffi.mk
@@ -14,22 +14,13 @@ LIBFFI_AUTORECONF = YES
 # Move the headers to the usual location, and adjust the .pc file
 # accordingly.
 define LIBFFI_MOVE_STAGING_HEADERS
-	mv $(STAGING_DIR)/usr/lib/libffi-*/include/*.h $(STAGING_DIR)/usr/include/
+	mv $(STAGING_DIR)/usr/lib/libffi-$(LIBFFI_VERSION)/include/*.h $(STAGING_DIR)/usr/include/
 	$(SED) '/^includedir.*/d' -e '/^Cflags:.*/d' \
 		$(STAGING_DIR)/usr/lib/pkgconfig/libffi.pc
 	rm -rf $(TARGET_DIR)/usr/lib/libffi-*
 endef
 
 LIBFFI_POST_INSTALL_STAGING_HOOKS += LIBFFI_MOVE_STAGING_HEADERS
-
-# Similar for target headers
-define LIBFFI_MOVE_TARGET_HEADERS
-	install -d $(TARGET_DIR)/usr/include/
-	mv $(TARGET_DIR)/usr/lib/libffi-*/include/*.h $(TARGET_DIR)/usr/include/
-	rm -rf $(TARGET_DIR)/usr/lib/libffi-*
-endef
-
-LIBFFI_POST_INSTALL_TARGET_HOOKS += LIBFFI_MOVE_TARGET_HEADERS
 
 HOST_LIBFFI_AUTORECONF = YES
 
