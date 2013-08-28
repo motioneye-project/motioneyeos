6492fb02423c091c1c2227a13dfa7e6ee4ba3e1e
Markos Chandras <markos.chandras@imgtec.com>
2013-08-28 11:23:37 +0100

    libsecret: New package
    libsecret is a library for storing and retrieving passwords
    and other secrets. It communicates with the "Secret Service"
    using DBus. gnome-keyring and ksecretservice are both
    implementations of a Secret Service.
    
    libsecret replaces libgnome-keyring.
    
    https://wiki.gnome.org/Libsecret
    
    [Peter: also needs host-intltool]
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/libsecret/libsecret.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# libsecret
+#
+################################################################################
+
+LIBSECRET_VERSION = 0.15
+LIBSECRET_SITE = http://ftp.gnome.org/pub/GNOME/sources/libsecret/$(LIBSECRET_VERSION)
+LIBSECRET_SOURCE = libsecret-$(LIBSECRET_VERSION).tar.xz
+LIBSECRET_LICENSE = LGPLv2.1+
+LIBSECRET_LICENSE_FILES = COPYING
+LIBSECRET_INSTALL_STAGING = YES
+
+LIBSECRET_DEPENDENCIES = libglib2 host-intltool
+LIBSECRET_CONF_OPT = --disable-manpages --disable-strict --disable-coverage --enable-vala=no
+
+ifeq ($(BR2_PACKAGE_LIBGCRYPT),y)
+	LIBSECRET_DEPENDENCIES += libgcrypt
+	LIBSECRET_CONF_OPT += --enable-gcrypt
+else
+	LIBSECRET_CONF_OPT += --disable-gcrypt
+endif
+
+$(eval $(autotools-package))
