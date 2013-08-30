6f6c05b80d1cbe797da3b2e9b3b310ba15197335
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-08-30 19:00:47 -0300

    gettext: bump to version 0.18.3.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/gettext/gettext.mk
+++ package/gettext/gettext.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GETTEXT_VERSION = 0.18.3
+GETTEXT_VERSION = 0.18.3.1
 GETTEXT_SITE = $(BR2_GNU_MIRROR)/gettext
 GETTEXT_INSTALL_STAGING = YES
 GETTEXT_LICENSE = GPLv2+
