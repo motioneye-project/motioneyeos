2e20d072ae2ceef8d5b54022616c8f6bcd194e7f
Peter Sanford <psanford@nearbuysystems.com>
2013-09-03 10:22:54 -0700

    netplug: properly install init.d script.
    Looks like this was lost in 20d4792e.
    
    [Peter: use _INSTALL_INIT_SYSV, explicitly delete script on uninstall]
    Signed-off-by: Peter Sanford <psanford@nearbuysystems.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/netplug/netplug.mk
+++ package/netplug/netplug.mk
@@ -16,10 +16,15 @@ define NETPLUG_INSTALL_TARGET_CMDS
 	$(MAKE) DESTDIR=$(TARGET_DIR) -C $(@D) install
 endef
 
+define NETPLUG_INSTALL_INIT_SYSV
+	$(INSTALL) -m 0755 -D package/netplug/S29netplug \
+		$(TARGET_DIR)/etc/init.d/S29netplug
+endef
+
 define NETPLUG_UNINSTALL_TARGET_CMDS
 	rm -f $(TARGET_DIR)/sbin/netplugd
 	rm -rf $(TARGET_DIR)/etc/netplug*
-	rm -f $(TARGET_DIR)/etc/init.d/S*netplug
+	rm -f $(TARGET_DIR)/etc/init.d/S29netplug
 endef
 
 define NETPLUG_CLEAN_CMDS
