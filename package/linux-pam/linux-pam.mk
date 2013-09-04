10e10bbf54825b444b95a1a6f45435e32c994180
Ryan Barnett <rjbarnet@rockwellcollins.com>
2013-09-04 18:15:31 -0500

    linux-pam: bump to version 1.1.6
    Signed-off-by: Ryan Barnett <rjbarnet@rockwellcollins.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/linux-pam/linux-pam.mk
+++ package/linux-pam/linux-pam.mk
@@ -4,7 +4,7 @@
 # 
 ################################################################################
 
-LINUX_PAM_VERSION = 1.1.4
+LINUX_PAM_VERSION = 1.1.6
 LINUX_PAM_SOURCE = Linux-PAM-$(LINUX_PAM_VERSION).tar.bz2
 LINUX_PAM_SITE = http://linux-pam.org/library/
 LINUX_PAM_INSTALL_STAGING = YES
@@ -16,13 +16,13 @@ LINUX_PAM_CONF_OPT = \
 	--disable-regenerate-docu \
 	--enable-securedir=/lib/security \
 	--libdir=/lib
-LINUX_PAM_DEPENDENCIES = $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext) \
-	flex host-flex
+LINUX_PAM_DEPENDENCIES = flex host-flex host-pkgconf
 LINUX_PAM_AUTORECONF = YES
 LINUX_PAM_LICENSE = BSD-3c
 LINUX_PAM_LICENSE_FILES = Copyright
 
 ifeq ($(BR2_NEEDS_GETTEXT_IF_LOCALE),y)
+	LINUX_PAM_DEPENDENCIES += gettext
 	LINUX_PAM_MAKE_OPT += LIBS=-lintl
 endif
 
