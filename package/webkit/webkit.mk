52fcc6a256bcfd541486257bfd05ff12d47aef75
Markos Chandras <markos.chandras@imgtec.com>
2013-09-05 12:37:02 +0100

    webkit: Version bump to 1.11.5
    Also apply the following patches:
    - upstream fix for 32-bit autotools
    - upstream fix for conditional gstreamer build of gtklauncher
    - upstream fix for detecting the latest harfbuzz
    - Update existing patch for disabling docs
    - Update existing patch for execinfo_h
    - upstream patch for the MIPS DFG support. This fixes
    compilation problems on MIPS.
    
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/webkit/webkit.mk
+++ package/webkit/webkit.mk
@@ -4,22 +4,32 @@
 #
 ################################################################################
 
-WEBKIT_VERSION = 1.9.6
+WEBKIT_VERSION = 1.11.5
 WEBKIT_SITE = http://www.webkitgtk.org/releases
-WEBKIT_SOURCE = webkit-$(WEBKIT_VERSION).tar.xz
+WEBKIT_SOURCE = webkitgtk-$(WEBKIT_VERSION).tar.xz
 WEBKIT_INSTALL_STAGING = YES
-WEBKIT_DEPENDENCIES = host-ruby host-flex host-bison host-gperf icu libcurl \
-	libxml2 libxslt libgtk2 sqlite enchant libsoup jpeg libgail
+WEBKIT_DEPENDENCIES = host-ruby host-flex host-bison host-gperf enchant harfbuzz \
+	icu jpeg libcurl libgail libsecret libsoup libxml2 libxslt libgtk2 sqlite webp
 
 # webkit-disable-tests.patch changes configure.ac therefore autoreconf required
 WEBKIT_AUTORECONF = YES
 WEBKIT_AUTORECONF_OPT = -I $(@D)/Source/autotools
 
+# Does not build and it's disabled by default
+# in newer releases
+define DISABLE_INDEXED_DATABASE
+	$(SED) '/ENABLE_INDEXED_DATABASE/s:1:0:' \
+			$(@D)/Source/WebCore/GNUmakefile.features.am
+endef
+
+WEBKIT_PRE_CONFIGURE_HOOKS += DISABLE_INDEXED_DATABASE
+
 # Give explicit path to icu-config, and silence gazillions of warnings
 # with recent gcc versions.
 WEBKIT_CONF_ENV = ac_cv_path_icu_config=$(STAGING_DIR)/usr/bin/icu-config \
 	CFLAGS="$(TARGET_CFLAGS) -Wno-cast-align -Wno-sign-compare" \
-	CXXFLAGS="$(TARGET_CXXFLAGS) -Wno-cast-align -Wno-sign-compare"
+	CXXFLAGS="$(TARGET_CXXFLAGS) -Wno-cast-align -Wno-sign-compare" \
+	AR_FLAGS="cru"
 
 WEBKIT_CONF_OPT += \
 	--with-gtk=2.0 \
