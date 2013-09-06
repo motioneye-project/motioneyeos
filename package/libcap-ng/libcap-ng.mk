e51b06e95c2355b96893823165985e23f0ecec99
Ryan Barnett <rjbarnet@rockwellcollins.com>
2013-09-06 11:06:00 -0500

    libcap-ng: bump revision to 0.7.3
    Also cleaned the package makefile up by removing explicitly naming
    the source along with moving the configure of the package down below
    the standard package declarations.
    
    Signed-off-by: Ryan Barnett <rjbarnet@rockwellcollins.com>
    Reviewed-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libcap-ng/libcap-ng.mk
+++ package/libcap-ng/libcap-ng.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-LIBCAP_NG_VERSION = 0.6.6
+LIBCAP_NG_VERSION = 0.7.3
 LIBCAP_NG_SITE = http://people.redhat.com/sgrubb/libcap-ng/
-LIBCAP_NG_SOURCE = libcap-ng-$(LIBCAP_NG_VERSION).tar.gz
-LIBCAP_NG_INSTALL_STAGING = YES
-LIBCAP_NG_CONF_ENV = ac_cv_prog_swig_found=no
-LIBCAP_NG_CONF_OPT = --without-python
 LIBCAP_NG_LICENSE = GPLv2+ LGPLv2.1+
 LIBCAP_NG_LICENSE_FILES = COPYING COPYING.LIB
+LIBCAP_NG_INSTALL_STAGING = YES
+
+LIBCAP_NG_CONF_ENV = ac_cv_prog_swig_found=no
+LIBCAP_NG_CONF_OPT = --without-python
 
 $(eval $(autotools-package))
