0175dce8bce9528bf7600e43158cfcf7e9b18331
Peter Korsgaard <jacmet@sunsite.dk>
2013-09-08 23:58:05 +0200

    opus-tools: bump version
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/opus-tools/opus-tools.mk
+++ package/opus-tools/opus-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPUS_TOOLS_VERSION = 0.1.6
+OPUS_TOOLS_VERSION = 0.1.7
 OPUS_TOOLS_SITE = http://downloads.xiph.org/releases/opus
 OPUS_TOOLS_CONF_OPT = --disable-oggtest --disable-opustest
 OPUS_TOOLS_DEPENDENCIES = opus libogg host-pkgconf
