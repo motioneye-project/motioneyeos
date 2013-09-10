4c04f21b8238ea79db580b042a849132234f0d8a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-09-10 16:26:11 -0300

    ipset: add patch for uclinux tuple
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ipset/ipset.mk
+++ package/ipset/ipset.mk
@@ -9,6 +9,7 @@ IPSET_SOURCE = ipset-$(IPSET_VERSION).tar.bz2
 IPSET_SITE = http://ipset.netfilter.org
 IPSET_DEPENDENCIES = libmnl host-pkgconf
 IPSET_CONF_OPT = --with-kmod=no
+IPSET_AUTORECONF = YES
 IPSET_LICENSE = GPLv2
 IPSET_LICENSE_FILES = COPYING
 
