cc34519201278a4737fb12b551fce98740383ff5
Peter Korsgaard <jacmet@sunsite.dk>
2013-09-10 21:54:59 +0200

    linux: fix depmod location after move to host-kmod
    The host-kmod version of depmod gets installed into HOST_DIR/sbin, so
    adjust the path we use to refer to it in linux.mk
    
    Reported-by: Ryan Coe <bluemrp9@gmail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- linux/linux.mk
+++ linux/linux.mk
@@ -47,7 +47,7 @@ LINUX_MAKE_FLAGS = \
 	ARCH=$(KERNEL_ARCH) \
 	INSTALL_MOD_PATH=$(TARGET_DIR) \
 	CROSS_COMPILE="$(CCACHE) $(TARGET_CROSS)" \
-	DEPMOD=$(HOST_DIR)/usr/sbin/depmod
+	DEPMOD=$(HOST_DIR)/sbin/depmod
 
 # Get the real Linux version, which tells us where kernel modules are
 # going to be installed in the target filesystem.
