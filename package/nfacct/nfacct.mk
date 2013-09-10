177549dcbe9971306f48bf3fe0c26fa665ecc639
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-09-10 16:26:20 -0300

    nfacct: add patch for uclinux tuple
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/nfacct/nfacct.mk
+++ package/nfacct/nfacct.mk
@@ -9,6 +9,7 @@ NFACCT_SOURCE = nfacct-$(NFACCT_VERSION).tar.bz2
 NFACCT_SITE = http://www.netfilter.org/projects/nfacct/files
 NFACCT_DEPENDENCIES = host-pkgconf \
 	libnetfilter_acct
+NFACCT_AUTORECONF = YES
 NFACCT_LICENSE = GPLv2
 NFACCT_LICENSE_FILES = COPYING
 
