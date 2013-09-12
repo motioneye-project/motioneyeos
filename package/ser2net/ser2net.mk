7dbc737d604d35c6d8155896320ecf736b6f8870
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-09-11 21:56:13 -0300

    ser2net: bump to version 2.9.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ser2net/ser2net.mk
+++ package/ser2net/ser2net.mk
@@ -4,10 +4,8 @@
 #
 ################################################################################
 
-SER2NET_VERSION = 2.9
+SER2NET_VERSION = 2.9.1
 SER2NET_SITE = http://downloads.sourceforge.net/project/ser2net/ser2net
-# shipped configure script is broken (libtool version error)
-SER2NET_AUTORECONF = YES
 SER2NET_LICENSE = GPLv2+
 SER2NET_LICENSE_FILES = COPYING
 
