1e9b983fe24fd9345df609e1f84a570208cdb45e
Michael Rommel <rommel@layer-7.net>
2013-09-13 22:57:04 +0200

    aiccu: new package
    Automatic IPv6 Connectivity Configuration Utility for users of a
    IPv6 tunnel broker, developed by sixxs.net
    
    [Peter: drop strip patch, pass TARGET_CONFIGURE_OPTS]
    Signed-off-by: Michael Rommel <rommel@layer-7.net>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/aiccu/aiccu.mk
@@ -0,0 +1,34 @@
+################################################################################
+#
+# aiccu
+#
+################################################################################
+
+AICCU_VERSION = 20070115
+AICCU_SOURCE = aiccu_$(AICCU_VERSION).tar.gz
+AICCU_SITE = http://www.sixxs.net/archive/sixxs/aiccu/unix/
+AICCU_LICENSE = SixXS License, concise redistribution license
+AICCU_LICENSE_FILES = doc/LICENSE
+AICCU_DEPENDENCIES = gnutls
+
+# dummy RPM_OPT_FLAGS to disable stripping
+define AICCU_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CC="$(TARGET_CC)" \
+		RPM_OPT_FLAGS=1 -C $(@D)/unix-console all
+endef
+
+define AICCU_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/unix-console/aiccu \
+		$(TARGET_DIR)/usr/sbin/aiccu
+	[ -f $(TARGET_DIR)/etc/aiccu.conf ] || \
+		$(INSTALL) -D -m 0644 $(@D)/doc/aiccu.conf \
+		$(TARGET_DIR)/etc/aiccu.conf
+endef
+
+define AICCU_INSTALL_INIT_SYSV
+	[ -f $(TARGET_DIR)/etc/init.d/S50aiccu ] || \
+		$(INSTALL) -D -m 0755 $(@D)/doc/aiccu.init \
+		$(TARGET_DIR)/etc/init.d/S50aiccu
+endef
+
+$(eval $(generic-package))
