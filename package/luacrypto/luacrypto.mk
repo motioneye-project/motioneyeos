549c09b6270c1e940d23c3d7feed86a84b9f6048
Francois Perrad <fperrad@gmail.com>
2013-09-15 17:40:37 +0200

    luacrypto: refactor with autotools
    some CMake stuff comes from LuaDist, but CMake is not the primary build infrastructure of LuaCrypto.
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/luacrypto/luacrypto.mk
+++ package/luacrypto/luacrypto.mk
@@ -8,7 +8,7 @@ LUACRYPTO_VERSION = 0.3.2
 LUACRYPTO_SITE = http://github.com/mkottman/luacrypto/tarball/$(LUACRYPTO_VERSION)
 LUACRYPTO_LICENSE = MIT
 LUACRYPTO_LICENSE_FILES = COPYING
-LUACRYPTO_DEPENDENCIES = lua openssl
-LUACRYPTO_CONF_OPT = "-DLUA_LIBRARIES=\"$(STAGING_DIR)/usr/lib/liblua.so\""
+LUACRYPTO_DEPENDENCIES = lua openssl host-pkgconf
+LUACRYPTO_AUTORECONF = YES
 
-$(eval $(cmake-package))
+$(eval $(autotools-package))
