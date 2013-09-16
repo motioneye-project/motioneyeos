55b081505528e63566ed29d3e61becc40c8a920a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-09-16 13:52:55 -0300

    luaposix: bump to version 31
    Releases are no longer named 5.1.X, just the X portion remains now.
    
    Fixes autoreconf issues with the automake bump, however since the patch
    is no longer required that's not an issue now.
    
    [Peter: now needs IPv6]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/luaposix/luaposix.mk
+++ package/luaposix/luaposix.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-LUAPOSIX_VERSION = 5.1.20
-LUAPOSIX_SITE = https://github.com/downloads/luaposix/luaposix
+LUAPOSIX_VERSION = 31
+LUAPOSIX_SITE = https://github.com/luaposix/luaposix/archive
+LUAPOSIX_SOURCE = release-v$(LUAPOSIX_VERSION).tar.gz
 LUAPOSIX_LICENSE = MIT
 LUAPOSIX_LICENSE_FILES = COPYING
 LUAPOSIX_DEPENDENCIES = lua host-lua
 LUAPOSIX_CONF_OPT = --libdir="/usr/lib/lua" --datarootdir="/usr/share/lua"
-LUAPOSIX_AUTORECONF = YES
 
 $(eval $(autotools-package))
