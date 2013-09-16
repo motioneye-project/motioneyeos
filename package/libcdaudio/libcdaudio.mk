32b869c32407ede709ab9657b42f32ac3da5b4e3
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-09-16 13:52:51 -0300

    libcdaudio: disable autoreconf
    There's no stated purpose and it breaks with automake 1.12+
    If it breaks in some scenario ping me, i've got a patch to fix the
    autoreconf issue and re-enable it.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libcdaudio/libcdaudio.mk
+++ package/libcdaudio/libcdaudio.mk
@@ -5,10 +5,7 @@
 ################################################################################
 
 LIBCDAUDIO_VERSION = 0.99.12p2
-LIBCDAUDIO_SOURCE = libcdaudio-$(LIBCDAUDIO_VERSION).tar.gz
 LIBCDAUDIO_SITE = http://downloads.sourceforge.net/project/libcdaudio/libcdaudio/$(LIBCDAUDIO_VERSION)/
-LIBCDAUDIO_AUTORECONF = YES
-LIBCDAUDIO_LIBTOOL_PATCH = YES
 LIBCDAUDIO_INSTALL_STAGING = YES
 LIBCDAUDIO_CONFIG_SCRIPTS = libcdaudio-config
 
