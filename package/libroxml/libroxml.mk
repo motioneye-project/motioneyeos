c1f03f652f02c4f3374d4c671698748dcaba98ab
Tristan Lelong <tristan.lelong@blunderer.org>
2013-09-16 14:47:49 -0700

    libroxml: bump version to 2.2.3
    Signed-off-by: Tristan Lelong <tristan.lelong@blunderer.org>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/libroxml/libroxml.mk
+++ package/libroxml/libroxml.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LIBROXML_VERSION = 2.2.2
-LIBROXML_SITE = http://libroxml.googlecode.com/files
+LIBROXML_VERSION = 2.2.3
+LIBROXML_SITE = http://download.libroxml.net/pool/v2.x/
 LIBROXML_INSTALL_STAGING = YES
 
 define LIBROXML_BUILD_CMDS
@@ -17,7 +17,7 @@ define LIBROXML_INSTALL_STAGING_CMDS
 endef
 
 define LIBROXML_INSTALL_TARGET_CMDS
-	$(MAKE) DESTDIR=$(TARGET_DIR) -C $(@D) install
+	$(MAKE) DESTDIR=$(TARGET_DIR)/usr/ -C $(@D) install
 endef
 
 define LIBROXML_UNINSTALL_STAGING_CMDS
