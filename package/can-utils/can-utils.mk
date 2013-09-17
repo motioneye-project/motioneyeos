2a3e794ef76fc4edae0ce4551e06ea5ba15fe23d
Yegor Yefremov <yegorslists@googlemail.com>
2013-09-17 09:10:52 +0200

    can-utils: bump version
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/can-utils/can-utils.mk
+++ package/can-utils/can-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAN_UTILS_VERSION = 836d3cc0122ce31a1b732d369cbd27b690c3110f
+CAN_UTILS_VERSION = 16c970d40e6c19dde705bad4751bab1a3a4f3a0d
 CAN_UTILS_SITE = git://gitorious.org/linux-can/can-utils.git
 CAN_UTILS_AUTORECONF = YES
 
