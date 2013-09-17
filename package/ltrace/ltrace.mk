238d13ab47dfaabad7296c13a2903ad422ed2fda
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-09-17 14:39:34 -0300

    ltrace: bump to version 0.7.3
    Now with proper mips support!
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/ltrace/ltrace.mk
+++ package/ltrace/ltrace.mk
@@ -4,21 +4,13 @@
 #
 ################################################################################
 
-LTRACE_VERSION = 0.7.2
-LTRACE_SITE = http://alioth.debian.org/frs/download.php/file/3848
+LTRACE_VERSION = 0.7.3
+LTRACE_SITE = http://alioth.debian.org/frs/download.php/file/3947
 LTRACE_SOURCE = ltrace-$(LTRACE_VERSION).tar.bz2
 LTRACE_DEPENDENCIES = libelf
-LTRACE_AUTORECONF = YES
 LTRACE_CONF_OPT = --disable-werror
 LTRACE_LICENSE = GPLv2
 LTRACE_LICENSE_FILES = COPYING
-
-# symlink missing from tarball
-define LTRACE_MIPS_SYMLINK
-	cd $(@D)/sysdeps/linux-gnu; ln -sf mipsel mips
-endef
-
-LTRACE_POST_EXTRACT_HOOKS += LTRACE_MIPS_SYMLINK
 
 # ltrace can use libunwind only if libc has backtrace() support
 # We don't normally do so for uClibc and we can't know if it's external
