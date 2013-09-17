35f6c37e2d0551d6568bd42fa5e4321680782b92
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-09-17 09:12:30 -0300

    time: needs mmu
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/time/time.mk
+++ package/time/time.mk
@@ -7,11 +7,13 @@
 TIME_VERSION = 1.7
 TIME_SITE = $(BR2_GNU_MIRROR)/time
 TIME_CONF_ENV = ac_cv_func_wait3=yes
+TIME_LICENSE = GPLv2+
+TIME_LICENSE_FILES = COPYING
 
 # time uses an old version of automake that does not support
 # installing in DESTDIR.
 define TIME_INSTALL_TARGET_CMDS
-	install -D -m 755  $(@D)/time $(TARGET_DIR)/usr/bin/time
+	$(INSTALL) -D -m 755  $(@D)/time $(TARGET_DIR)/usr/bin/time
 endef
 
 $(eval $(autotools-package))
