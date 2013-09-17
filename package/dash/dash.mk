ab782560690a960200951bb0131536ce853731fd
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-09-17 10:30:04 -0300

    dash: bump to version 0.5.7-3
    Bump to patchlevel 3 since 2 is gone upstream, fixes when
    sources.buildroot.net isn't available:
    http://autobuild.buildroot.net/results/5ee/5ee6544a9d08025060cb5806870ae1c3f252cd93/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/dash/dash.mk
+++ package/dash/dash.mk
@@ -7,7 +7,7 @@
 DASH_VERSION = 0.5.7
 DASH_SOURCE = dash_$(DASH_VERSION).orig.tar.gz
 DASH_SITE = $(BR2_DEBIAN_MIRROR)/debian/pool/main/d/dash
-DASH_PATCH = dash_$(DASH_VERSION)-2.diff.gz
+DASH_PATCH = dash_$(DASH_VERSION)-3.diff.gz
 DASH_LICENSE = BSD-3c, GPLv2+ (mksignames.c)
 DASH_LICENSE_FILES = COPYING
 
