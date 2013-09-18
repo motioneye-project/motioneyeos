c95dcd464512ede5ffdef5567558611f05e2e52b
Matt Weber <mlweber1@rockwellcollins.com>
2013-09-17 23:26:41 -0500

    bc: new package
    Signed-off-by: Matt Weber <mlweber1@rockwellcollins.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

---
+++ package/bc/bc.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# bc
+#
+################################################################################
+
+BC_VERSION = 1.06
+BC_SITE = $(BR2_GNU_MIRROR)/bc/
+BC_LICENSE = GPLv2+ LGPLv2.1+
+BC_LICENSE_FILES = COPYING COPYING.LIB
+
+# Build after busybox so target ends up with bc's "dc" version
+ifeq ($(BR2_PACKAGE_BUSYBOX),y)
+BC_DEPENDENCIES += busybox
+endif
+
+$(eval $(autotools-package))
