e0dabf9ea84bb4b312c521d95d9b259b5e6ac39f
Yegor Yefremov <yegorslists@googlemail.com>
2013-09-19 09:12:57 +0200

    tslib: bump version to 1.1
    [Peter: use 1.1 tag]
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/tslib/tslib.mk
+++ package/tslib/tslib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TSLIB_VERSION = 158ee49b32f83cb7b02d5315f41c2e4cff38942d
+TSLIB_VERSION = 1.1
 TSLIB_SITE = http://github.com/kergoth/tslib/tarball/$(TSLIB_VERSION)
 TSLIB_LICENSE = GPL, LGPL
 TSLIB_LICENSE_FILES = COPYING
