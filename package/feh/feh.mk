8ccbca218ed17945a07e585abcc8e28f7eb5b9dc
Yegor Yefremov <yegorslists@googlemail.com>
2013-09-20 14:29:03 +0200

    feh: bump to 2.9.3
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/feh/feh.mk
+++ package/feh/feh.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FEH_VERSION = 2.9.1
+FEH_VERSION = 2.9.3
 FEH_SOURCE = feh-$(FEH_VERSION).tar.bz2
 FEH_SITE = http://feh.finalrewind.org/
 FEH_DEPENDENCIES = libcurl giblib imlib2 libpng xlib_libXinerama xlib_libXt
