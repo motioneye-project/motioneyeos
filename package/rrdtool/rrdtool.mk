f16e9476f42d13021a352ca633c39e25fe3a547a
Peter Korsgaard <jacmet@sunsite.dk>
2013-09-24 13:35:49 +0200

    rrdtool: fix build without threads support
    Fixes http://autobuild.buildroot.net/results/f7a/f7ab00ddae121ccfc6d54f7eda8ea8ac6df359c2/
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/rrdtool/rrdtool.mk
+++ package/rrdtool/rrdtool.mk
@@ -15,7 +15,8 @@ RRDTOOL_INSTALL_STAGING = YES
 RRDTOOL_CONF_ENV = rd_cv_ieee_works=yes rd_cv_null_realloc=nope \
 			ac_cv_func_mmap_fixed_mapped=yes
 RRDTOOL_CONF_OPT = --disable-perl --disable-python --disable-ruby \
-			--disable-tcl --program-transform-name=''
+			--disable-tcl --program-transform-name='' \
+			$(if $(BR2_TOOLCHAIN_HAS_THREADS),,--disable-pthread)
 RRDTOOL_MAKE = $(MAKE1)
 
 define RRDTOOL_REMOVE_EXAMPLES
