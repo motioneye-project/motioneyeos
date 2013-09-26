b720a014bba13818c33810a9673b36d2114055a2
Peter Korsgaard <jacmet@sunsite.dk>
2013-09-26 21:15:59 +0200

    nitrogen6x: post-build.sh: use the exported variables for {TARGET,BINARIES}_DIR
    No functional change, but lets show the preferred way to do this.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- board/boundarydevices/nitrogen6x/post-build.sh
+++ board/boundarydevices/nitrogen6x/post-build.sh
@@ -5,16 +5,14 @@
 #  http://boundarydevices.com/u-boot-on-i-mx6/
 #
 
-TARGET_DIR=$1
-IMAGES_DIR=$1/../images
 BOARD_DIR="$(dirname $0)"
 
 # bd u-boot looks for bootscript here
 cp $BOARD_DIR/6x_bootscript $TARGET_DIR
 
 # u-boot / update script for bd upgradeu command
-if [ -e $IMAGES_DIR/u-boot.imx ];
+if [ -e $BINARIES_DIR/u-boot.imx ];
 then
-    cp $IMAGES_DIR/u-boot.imx $TARGET_DIR
+    cp $BINARIES_DIR/u-boot.imx $TARGET_DIR
     cp $BOARD_DIR/6x_upgrade $TARGET_DIR
 fi
