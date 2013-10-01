b7c5625e0acfd62f34dd45f91ea0ea3150c2e8e3
Matt Weber <mlweber1@rockwellcollins.com>
2013-10-01 15:01:08 -0500

    python-ipy: new package
    Signed-off-by: Matt Weber <mlweber1@rockwellcollins.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/python-ipy/python-ipy.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# python-ipy
+#
+################################################################################
+
+PYTHON_IPY_VERSION = 0.75-0-g58006fb
+PYTHON_IPY_SOURCE  = haypo-python-ipy-IPy-$(PYTHON_IPY_VERSION).tar.gz
+PYTHON_IPY_SITE    = https://github.com/haypo/python-ipy/tarball/IPy-0.75
+PYTHON_IPY_DEPENDENCIES = host-python python
+PYTHON_IPY_LICENSE = BSD-3c
+PYTHON_IPY_LICENSE_FILES = COPYING
+
+define PYTHON_IPY_BUILD_CMDS
+	(cd $(@D); \
+		$(TARGET_CONFIGURE_OPTS) \
+		$(HOST_DIR)/usr/bin/python setup.py build_ext \
+		--include-dirs=$(STAGING_DIR)/usr/include/python$(PYTHON_VERSION_MAJOR) \
+	)
+	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py build)
+endef
+
+define PYTHON_IPY_INSTALL_TARGET_CMDS
+	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
+endef
+
+$(eval $(generic-package))
