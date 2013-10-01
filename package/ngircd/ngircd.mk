ce5b2aed7e6b24b53766f215fafb01b7390ea79f
Peter Korsgaard <peter@korsgaard.com>
2013-10-01 21:25:12 +0200

    ngircd: add license info
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ngircd/ngircd.mk
+++ package/ngircd/ngircd.mk
@@ -7,5 +7,7 @@
 NGIRCD_VERSION = 20.3
 NGIRCD_SITE = ftp://ftp.berlios.de/pub/ngircd/
 NGIRCD_DEPENDENCIES = zlib
+NGIRCD_LICENSE = GPLv2+
+NGIRCD_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
