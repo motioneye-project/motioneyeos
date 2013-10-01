22ea98e727fd6ac2430c4a8f904511322743f43c
Peter Korsgaard <peter@korsgaard.com>
2013-10-01 21:22:11 +0200

    picocom: add license info
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/picocom/picocom.mk
+++ package/picocom/picocom.mk
@@ -6,6 +6,8 @@
 
 PICOCOM_VERSION = 1.7
 PICOCOM_SITE    = http://picocom.googlecode.com/files/
+PICOCOM_LICENSE = GPLv2+
+PICOCOM_LICENSE_FILES = LICENSE.txt
 
 define PICOCOM_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
