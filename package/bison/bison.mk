d268bcc4d6e571a0be0ecd38f41ab674c39ac226
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-10-02 09:28:45 -0300

    bison: bump to version 3.0
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/bison/bison.mk
+++ package/bison/bison.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BISON_VERSION = 2.7.1
+BISON_VERSION = 3.0
 BISON_SOURCE = bison-$(BISON_VERSION).tar.xz
 BISON_SITE = $(BR2_GNU_MIRROR)/bison
 BISON_LICENSE = GPLv3+
