bdabe4cbcc1ed58a5db30e3aa01c0ff697d68b8d
Axel Lin <axel.lin@ingics.com>
2013-10-03 15:43:09 +0800

    sqlite: bump to version 3.8.0.2
    Also drop sqlite-dont-force-posix-fallocate.patch because it is fixed upstream.
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SQLITE_VERSION = 3071700
+SQLITE_VERSION = 3080002
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_VERSION).tar.gz
 SQLITE_SITE = http://www.sqlite.org/2013
 SQLITE_LICENSE = Public domain
