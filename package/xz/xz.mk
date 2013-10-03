e9eb2a7e20e3c5f0a236d6776f82a5659bac4074
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2013-10-03 18:29:29 +0200

    xz: bump to version 5.0.5
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/xz/xz.mk
+++ package/xz/xz.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XZ_VERSION = 5.0.4
+XZ_VERSION = 5.0.5
 XZ_SOURCE = xz-$(XZ_VERSION).tar.bz2
 XZ_SITE = http://tukaani.org/xz/
 XZ_INSTALL_STAGING = YES
