b8b69b2da9fa8140fa6dde766170a0815c87a9dd
Yann E. MORIN <yann.morin.1998@free.fr>
2013-10-05 00:00:08 +0200

    package/rpi-userland: bump version
    Misc enhancements and fixes for USB, camera, audio, codecs, dispman,
    and openMAX.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_USERLAND_VERSION = 8b271ac
+RPI_USERLAND_VERSION = 77d32cd
 RPI_USERLAND_SITE = http://github.com/raspberrypi/userland/tarball/$(RPI_USERLAND_VERSION)
 RPI_USERLAND_LICENSE = BSD-3c
 RPI_USERLAND_LICENSE_FILES = LICENCE
