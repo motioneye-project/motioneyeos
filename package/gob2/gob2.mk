053f8154b03e860ec55b7a62ce5bbd32a0be8574
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2013-10-06 22:04:36 +0200

    gob2: bump to version 2.0.19
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gob2/gob2.mk
+++ package/gob2/gob2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GOB2_VERSION = 2.0.18
+GOB2_VERSION = 2.0.19
 GOB2_SITE = http://ftp.5z.com/pub/gob
 GOB2_DEPENDENCIES = libglib2 flex bison host-pkgconf host-flex
 HOST_GOB2_DEPENDENCIES = host-bison host-flex host-libglib2
