c5d270004031338455890a19e9519a670dae71a2
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2013-10-06 20:36:12 +0200

    lockfile-progs: bump to version 0.1.17
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lockfile-progs/lockfile-progs.mk
+++ package/lockfile-progs/lockfile-progs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LOCKFILE_PROGS_VERSION = 0.1.15
+LOCKFILE_PROGS_VERSION = 0.1.17
 LOCKFILE_PROGS_SOURCE = lockfile-progs_$(LOCKFILE_PROGS_VERSION).tar.gz
 LOCKFILE_PROGS_SITE = $(BR2_DEBIAN_MIRROR)/debian/pool/main/l/lockfile-progs/
 LOCKFILE_PROGS_DEPENDENCIES = liblockfile
