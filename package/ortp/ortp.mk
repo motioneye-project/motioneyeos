280a387737ed72a2dd844253eeb64cc373ca7656
Axel Lin <axel.lin@ingics.com>
2013-10-07 13:51:25 +0800

    ortp: bump to version 0.22.0
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ortp/ortp.mk
+++ package/ortp/ortp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ORTP_VERSION = 0.20.0
+ORTP_VERSION = 0.22.0
 ORTP_SITE = http://download.savannah.nongnu.org/releases/linphone/ortp/sources
 ORTP_CONF_OPT = --disable-strict
 ORTP_INSTALL_STAGING = YES
