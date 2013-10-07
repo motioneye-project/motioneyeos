4f92c64fd60d8d58db67eb06c3e39b790b230385
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-10-07 16:46:55 -0300

    ffmpeg: security bump to version 0.8.15
    Fixes several vulnerabilities.
    TODO: update to a more modern branch.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ffmpeg/ffmpeg.mk
+++ package/ffmpeg/ffmpeg.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FFMPEG_VERSION = 0.8.12
+FFMPEG_VERSION = 0.8.15
 FFMPEG_SOURCE = ffmpeg-$(FFMPEG_VERSION).tar.bz2
 FFMPEG_SITE = http://ffmpeg.org/releases
 FFMPEG_INSTALL_STAGING = YES
