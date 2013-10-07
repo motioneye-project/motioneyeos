b953d79b62ae75950349ea6575fe8ee9b895f0c0
Axel Lin <axel.lin@ingics.com>
2013-10-07 13:35:18 +0800

    smartmontools: bump to version 6.2
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/smartmontools/smartmontools.mk
+++ package/smartmontools/smartmontools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SMARTMONTOOLS_VERSION = 6.1
+SMARTMONTOOLS_VERSION = 6.2
 SMARTMONTOOLS_SITE = http://downloads.sourceforge.net/project/smartmontools/smartmontools/$(SMARTMONTOOLS_VERSION)
 SMARTMONTOOLS_LICENSE = GPLv2+
 SMARTMONTOOLS_LICENSE_FILES = COPYING
