f9d5a367c0517c546a85d79b49baf39f077f1750
Axel Lin <axel.lin@ingics.com>
2013-10-07 13:26:28 +0800

    gtest: bump to version 1.7.0
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gtest/gtest.mk
+++ package/gtest/gtest.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GTEST_VERSION = 1.6.0
+GTEST_VERSION = 1.7.0
 GTEST_SOURCE = gtest-$(GTEST_VERSION).zip
 GTEST_SITE = http://googletest.googlecode.com/files/
 GTEST_INSTALL_STAGING = YES
