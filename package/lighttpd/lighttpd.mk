38cde8594dcacb8d1e7460e7eab4e70ef9ecb464
Markos Chandras <markos.chandras@imgtec.com>
2013-10-07 13:49:56 +0100

    lighttpd: Version bump to 1.4.33
    Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lighttpd/lighttpd.mk
+++ package/lighttpd/lighttpd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIGHTTPD_VERSION = 1.4.32
+LIGHTTPD_VERSION = 1.4.33
 LIGHTTPD_SOURCE = lighttpd-$(LIGHTTPD_VERSION).tar.xz
 LIGHTTPD_SITE = http://download.lighttpd.net/lighttpd/releases-1.4.x
 LIGHTTPD_LICENSE = BSD-3c
