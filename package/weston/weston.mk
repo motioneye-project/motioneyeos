516246ddf2d7c2569ee6c0ffa4cfc80ff4ecc203
Yann E. MORIN <yann.morin.1998@free.fr>
2013-10-09 00:09:48 +0200

    package/weston: fix variable name
    The weston package uses the $(WAYLAND_VERSION) variable instead of
    $(WESTON_VERSION).
    
    This went unnoticed for now, as we were using the same version for
    wayland and weston. But that's not always the case, since we have,
    for example: wayland-1.2.1 and weston-1.2.2, and no wayland-1.2.2.
    
    Fix this by using the correct variable.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/weston/weston.mk
+++ package/weston/weston.mk
@@ -6,7 +6,7 @@
 
 WESTON_VERSION = 1.1.0
 WESTON_SITE = http://wayland.freedesktop.org/releases/
-WESTON_SOURCE = weston-$(WAYLAND_VERSION).tar.xz
+WESTON_SOURCE = weston-$(WESTON_VERSION).tar.xz
 WESTON_LICENSE = MIT
 WESTON_LICENSE_FILES = COPYING
 
