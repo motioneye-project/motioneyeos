09e59d73f588ff26a62787dc91e0a0bc810b39e0
Yegor Yefremov <yegorslists@googlemail.com>
2013-10-08 09:11:55 +0200

    libsoc: bump to 0.4
    "Non-blocking GPIO Interrupts with callback mechanism" feature
    requires pthreads.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libsoc/libsoc.mk
+++ package/libsoc/libsoc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSOC_VERSION = 356760dcb93b22d6c67c5232cde2ade8c968c932
+LIBSOC_VERSION = 0.4
 LIBSOC_SITE = http://github.com/jackmitch/libsoc/tarball/$(LIBSOC_VERSION)
 LIBSOC_LICENSE = LGPLv2.1
 LIBSOC_LICENSE_FILES = COPYING
