f65e8e31a5cbd27ae3aced271de7cffc4713ac4c
Axel Lin <axel.lin@ingics.com>
2013-10-08 15:59:29 +0800

    stress: add license information
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/stress/stress.mk
+++ package/stress/stress.mk
@@ -6,6 +6,8 @@
 
 STRESS_VERSION = 1.0.4
 STRESS_SITE = http://people.seas.harvard.edu/~apw/stress
+STRESS_LICENSE = GPLv2+
+STRESS_LICENSE_FILES = COPYING
 STRESS_AUTORECONF = YES
 
 # Stress is linked statically if the --enable-static is specified.
