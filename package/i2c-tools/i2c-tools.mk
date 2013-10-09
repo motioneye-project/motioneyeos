e8d807e3a6a65b1a26fd2110b4de826ec1a3f2a3
Thomas De Schampheleire <patrickdepinguin@gmail.com>
2013-10-09 09:10:51 +0200

    i2c-tools: add license information
    Signed-off-by: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/i2c-tools/i2c-tools.mk
+++ package/i2c-tools/i2c-tools.mk
@@ -7,6 +7,8 @@
 I2C_TOOLS_VERSION = 3.1.0
 I2C_TOOLS_SOURCE = i2c-tools-$(I2C_TOOLS_VERSION).tar.bz2
 I2C_TOOLS_SITE = http://dl.lm-sensors.org/i2c-tools/releases
+I2C_TOOLS_LICENSE = GPLv2+, GPLv2 (py-smbus)
+I2C_TOOLS_LICENSE_FILES = COPYING
 
 define I2C_TOOLS_BUILD_CMDS
  $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
