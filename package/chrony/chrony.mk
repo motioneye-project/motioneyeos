cbcbed4a24a8fd9b8a1f9c546cb43e56ae961bc3
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2013-10-10 17:58:27 +0200

    chrony: bump to version 1.29
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/chrony/chrony.mk
+++ package/chrony/chrony.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CHRONY_VERSION = 1.27
+CHRONY_VERSION = 1.29
 CHRONY_SITE = http://download.tuxfamily.org/chrony/
 CHRONY_LICENSE = GPLv2
 CHRONY_LICENSE_FILES = COPYING
