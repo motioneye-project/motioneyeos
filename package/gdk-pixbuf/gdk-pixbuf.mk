c551ac4cff997c26cc8ee26ab2fe1437cbbfa3d9
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2013-10-10 18:01:17 +0200

    gdk-pixbuf: bump to version 2.28.2
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gdk-pixbuf/gdk-pixbuf.mk
+++ package/gdk-pixbuf/gdk-pixbuf.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 GDK_PIXBUF_MAJOR_VERSION = 2.28
-GDK_PIXBUF_VERSION = $(GDK_PIXBUF_MAJOR_VERSION).1
+GDK_PIXBUF_VERSION = $(GDK_PIXBUF_MAJOR_VERSION).2
 GDK_PIXBUF_SOURCE = gdk-pixbuf-$(GDK_PIXBUF_VERSION).tar.xz
 GDK_PIXBUF_SITE = http://ftp.gnome.org/pub/gnome/sources/gdk-pixbuf/$(GDK_PIXBUF_MAJOR_VERSION)
 GDK_PIXBUF_LICENSE = LGPLv2+
