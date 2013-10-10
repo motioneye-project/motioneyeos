883c4dc43aed75d1dc0875b0ee529d9e72c7f01e
Yann E. MORIN <yann.morin.1998@free.fr>
2013-10-10 23:10:33 +0200

    package/rpi-firmware: bump version
    Fixes for de-interlacing of /unusual/ MPEG streams.
    Yes, some people seem to enjoy generating MPEG streams in
    which interlacing is not constant. That's apparently 100%
    valid, but yet very unusual, and at the very least, weird.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = e6b21d6
+RPI_FIRMWARE_VERSION = 58036da
 RPI_FIRMWARE_SITE = http://github.com/raspberrypi/firmware/tarball/$(RPI_FIRMWARE_VERSION)
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
