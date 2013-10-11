9876471082c017132b53ec0a420a298db9aa78a0
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2013-10-11 18:31:37 +0200

    zeromq: bump to version 4.0.1
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/zeromq/zeromq.mk
+++ package/zeromq/zeromq.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ZEROMQ_VERSION = 3.2.4
+ZEROMQ_VERSION = 4.0.1
 ZEROMQ_SITE = http://download.zeromq.org/
 ZEROMQ_INSTALL_STAGING = YES
 ZEROMQ_DEPENDENCIES = util-linux
