2e80b7072a5394ca13272a9ed36c4de96472ff91
Yann E. MORIN <yann.morin.1998@free.fr>
2013-10-14 23:26:35 +0200

    package/wayland: bump to 1.3.0
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/wayland/wayland.mk
+++ package/wayland/wayland.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WAYLAND_VERSION = 1.2.1
+WAYLAND_VERSION = 1.3.0
 WAYLAND_SITE = http://wayland.freedesktop.org/releases/
 WAYLAND_SOURCE = wayland-$(WAYLAND_VERSION).tar.xz
 WAYLAND_LICENSE = MIT
