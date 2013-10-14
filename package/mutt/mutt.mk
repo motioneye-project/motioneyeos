9f355f89c92492c4275373a9bb9ab1e6d0a1dd9c
Phil Eichinger <phil.eichinger@gmail.com>
2013-10-14 16:05:38 +0200

    mutt: add license information
    Signed-off-by: Phil Eichinger <phil@zankapfel.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mutt/mutt.mk
+++ package/mutt/mutt.mk
@@ -6,6 +6,8 @@
 
 MUTT_VERSION = 1.5.21
 MUTT_SITE = http://downloads.sourceforge.net/project/mutt/mutt-dev
+MUTT_LICENSE = GPLv2+
+MUTT_LICENSE_FILES = GPL
 MUTT_DEPENDENCIES = ncurses
 MUTT_CONF_OPT = --disable-iconv --disable-smtp
 MUTT_AUTORECONF = YES
