54e0493ad7399faae23757270be4db29efa063ac
Phil Eichinger <phil.eichinger@gmail.com>
2013-10-14 16:05:39 +0200

    netcat: add license information
    Signed-off-by: Phil Eichinger <phil@zankapfel.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/netcat/netcat.mk
+++ package/netcat/netcat.mk
@@ -6,5 +6,7 @@
 
 NETCAT_VERSION = 0.7.1
 NETCAT_SITE = http://downloads.sourceforge.net/project/netcat/netcat/$(NETCAT_VERSION)
+NETCAT_LICENSE = GPLv2+
+NETCAT_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
