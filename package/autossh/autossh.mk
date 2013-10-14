05d59aa2689acb86e72052418fe85b186b0a39e7
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-10-14 10:46:11 -0300

    autossh: new package
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/autossh/autossh.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# autossh
+#
+################################################################################
+
+AUTOSSH_VERSION = 1.4c
+AUTOSSH_SITE = http://www.harding.motd.ca/autossh
+AUTOSSH_SOURCE = autossh-$(AUTOSSH_VERSION).tgz
+AUTOSSH_LICENSE = Modified BSD
+AUTOSSH_LICENSE_FILES = autossh.c
+
+define AUTOSSH_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -D $(@D)/autossh $(TARGET_DIR)/usr/bin/autossh
+endef
+
+$(eval $(autotools-package))
