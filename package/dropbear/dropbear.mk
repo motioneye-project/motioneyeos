a95b9d77d1dc3ce79be680b3aec70627fda2c92b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-10-16 19:31:51 -0300

    dropbear: bump to version 2013.60
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/dropbear/dropbear.mk
+++ package/dropbear/dropbear.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DROPBEAR_VERSION = 2013.59
+DROPBEAR_VERSION = 2013.60
 DROPBEAR_SITE = http://matt.ucc.asn.au/dropbear/releases
 DROPBEAR_SOURCE = dropbear-$(DROPBEAR_VERSION).tar.bz2
 DROPBEAR_TARGET_BINS = dbclient dropbearkey dropbearconvert scp ssh
