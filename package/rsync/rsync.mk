6eac08192d8dceaba992d11d8874ea3af93011b5
Denis Mingulov <denis@mingulov.com>
2013-10-16 16:13:37 +0300

    rsync: use system zlib and popt, also do not lose debug options
    rsync by default compiles statically its own zlib and popt
    libraries, it is better to use system wide libraries instead -
    both for disk and memory consumption. Change rsync package
    accordingly.
    
    Also previously a debug configure option has been lost, as
    overwritten by '--with-included-popt' option - fixed.
    
    For example, on ARMv5 rsync binary size is reduced by about 25%.
    
    Signed-off-by: Denis Mingulov <denis@mingulov.com>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/rsync/rsync.mk
+++ package/rsync/rsync.mk
@@ -8,7 +8,10 @@ RSYNC_VERSION = 3.1.0
 RSYNC_SITE = http://rsync.samba.org/ftp/rsync/src
 RSYNC_LICENSE = GPLv3+
 RSYNC_LICENSE_FILES = COPYING
-RSYNC_CONF_OPT = $(if $(BR2_ENABLE_DEBUG),--enable-debug,--disable-debug)
-RSYNC_CONF_OPT = --with-included-popt
+RSYNC_DEPENDENCIES = zlib popt
+RSYNC_CONF_OPT = \
+	$(if $(BR2_ENABLE_DEBUG),--enable-debug,--disable-debug) \
+	--with-included-zlib=no \
+	--with-included-popt=no
 
 $(eval $(autotools-package))
