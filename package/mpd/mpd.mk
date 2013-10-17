00cb32b56a8194cc16a33f0183f7ee4ca6c81db7
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-10-17 20:05:53 -0300

    mpd: bump to version 0.17.6
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mpd/mpd.mk
+++ package/mpd/mpd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MPD_VERSION = 0.17.5
+MPD_VERSION = 0.17.6
 MPD_SOURCE = mpd-$(MPD_VERSION).tar.xz
 MPD_SITE = http://www.musicpd.org/download/mpd/stable
 MPD_DEPENDENCIES = host-pkgconf libglib2
