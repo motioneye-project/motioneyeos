46b3f2d83e2af4461dd5b66fca7838310d5d3b01
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-10-22 12:18:42 -0300

    libmicrohttpd: bump to version 0.9.31
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libmicrohttpd/libmicrohttpd.mk
+++ package/libmicrohttpd/libmicrohttpd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBMICROHTTPD_VERSION = 0.9.30
+LIBMICROHTTPD_VERSION = 0.9.31
 LIBMICROHTTPD_SITE = $(BR2_GNU_MIRROR)/libmicrohttpd
 LIBMICROHTTPD_LICENSE_FILES = COPYING
 LIBMICROHTTPD_INSTALL_STAGING = YES
