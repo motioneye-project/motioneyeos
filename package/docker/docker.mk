eb742c19566e77086967d20b0df789b3ccafe3e6
Arnout Vandecappelle <arnout@mind.be>
2013-10-24 00:24:18 +0200

    docker: change into generic-package
    docker is using autotools-package while it is in fact just a plain make
    package.
    
    Remove docker-1.5-skip-configure.patch which was just a hack to avoid
    calling the non-existent configure script.
    
    Remove docker-1.5-pkgconfig.patch which is not needed when PATH is set
    appropriately.
    
    Add a XLIBPATH option to the command line to avoid linking with
    /usr/X11R6/lib.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/docker/docker.mk
+++ package/docker/docker.mk
@@ -6,16 +6,16 @@
 
 DOCKER_VERSION = 1.5
 DOCKER_SITE = http://icculus.org/openbox/2/docker
-
-DOCKER_MAKE_OPT = CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" LD="$(TARGET_LD)" \
-					CFLAGS="-I$(STAGING_DIR)/usr/include \
-						-I$(STAGING_DIR)/usr/include/glib-2.0 \
-						-I$(STAGING_DIR)/usr/lib/glib-2.0/include" \
-					PKG_CONFIG="$(PKG_CONFIG_HOST_BINARY)" \
-					LDFLAGS="-L$(STAGING_DIR)/usr/lib -L$(STAGING_DIR)/lib"
-
-DOCKER_INSTALL_TARGET_OPT = PREFIX=$(TARGET_DIR)/usr install
-
 DOCKER_DEPENDENCIES = host-pkgconf libglib2 xlib_libX11
 
-$(eval $(autotools-package))
+define DOCKER_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
+		XLIBPATH=$(STAGING_DIR)/usr/lib
+endef
+
+define DOCKER_INSTALL_TARGET_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
+		XLIBPATH=$(STAGING_DIR)/usr/lib PREFIX=$(TARGET_DIR)/usr install
+endef
+
+$(eval $(generic-package))
