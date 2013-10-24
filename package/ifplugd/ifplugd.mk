cfea3556422e92969bedbde95d32af4c3dc25c83
Danomi Manchego <danomimanchego123@gmail.com>
2013-10-23 21:05:49 -0400

    ifplugd: ensure directories exist before installing to them
    Add the -D to install commands to ensure that all the necessary
    installation directories exist.
    
    Signed-off-by: Danomi Manchego <danomimanchego123@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ifplugd/ifplugd.mk
+++ package/ifplugd/ifplugd.mk
@@ -21,14 +21,13 @@ ifeq ($(BR2_PACKAGE_BUSYBOX),y)
 endif
 
 define IFPLUGD_INSTALL_FIXUP
-	$(INSTALL) -d $(TARGET_DIR)/etc/ifplugd
 	@if [ ! -f $(TARGET_DIR)/etc/ifplugd/ifplugd.conf ]; then \
-		$(INSTALL) $(@D)/conf/ifplugd.conf $(TARGET_DIR)/etc/ifplugd/; \
+		$(INSTALL) -D $(@D)/conf/ifplugd.conf $(TARGET_DIR)/etc/ifplugd/ifplugd.conf; \
 		$(SED) 's^\(ARGS=.*\)w^\1^' $(TARGET_DIR)/etc/ifplugd/ifplugd.conf; \
 	fi
-	$(INSTALL) -m 0755 $(@D)/conf/ifplugd.action \
-		$(TARGET_DIR)/etc/ifplugd/
-	$(INSTALL) -m 0755 $(@D)/conf/ifplugd.init \
+	$(INSTALL) -D -m 0755 $(@D)/conf/ifplugd.action \
+		$(TARGET_DIR)/etc/ifplugd/ifplugd.action
+	$(INSTALL) -D -m 0755 $(@D)/conf/ifplugd.init \
 		$(TARGET_DIR)/etc/init.d/S45ifplugd
 	# don't use bash for init script
 	$(SED) 's^/bin/bash^/bin/sh^g' $(TARGET_DIR)/etc/init.d/S45ifplugd
