d4f839248d2d57406db85803ab041d03cbaaffda
Arnaud Aujon <arnaud.aujon@gmail.com>
2013-10-24 11:00:05 +0200

    libfreefare: new repository
    [Peter: also adjust upstream URL]
    Signed-off-by: Arnaud Aujon <arnaud.aujon@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libfreefare/libfreefare.mk
+++ package/libfreefare/libfreefare.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBFREEFARE_VERSION = 0.3.4
-LIBFREEFARE_SITE = http://nfc-tools.googlecode.com/files
+LIBFREEFARE_SITE = http://libfreefare.googlecode.com/files
 LIBFREEFARE_DEPENDENCIES = libnfc openssl
 
 $(eval $(autotools-package))
