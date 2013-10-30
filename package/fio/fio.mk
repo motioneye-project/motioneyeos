43e9128600c209b669cb0eca93717d5bc4fa08bd
Ezequiel Garcia <ezequiel.garcia@free-electrons.com>
2013-10-30 12:22:25 -0300

    fio: new package
    [Thomas: add largefile and thread dependencies, bump to version 2.1.4,
     use upstream Git instead of Debian tarballs, adjust license
     information]
    
    Signed-off-by: Ezequiel Garcia <ezequiel.garcia@free-electrons.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/fio/fio.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# fio
+#
+################################################################################
+
+FIO_VERSION = fio-2.1.4
+FIO_SITE = git://git.kernel.dk/fio.git
+FIO_LICENSE = GPLv2 + special obligations
+FIO_LICENSE_FILES = LICENSE
+
+define FIO_CONFIGURE_CMDS
+	(cd $(@D); ./configure --cc="$(TARGET_CC)" --extra-cflags="$(TARGET_CFLAGS)")
+endef
+
+define FIO_BUILD_CMDS
+	$(MAKE) -C $(@D)
+endef
+
+define FIO_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/fio $(TARGET_DIR)/usr/bin/fio
+endef
+
+$(eval $(generic-package))
