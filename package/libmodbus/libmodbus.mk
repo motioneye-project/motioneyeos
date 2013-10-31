8639afc6f097337241d1ebb88bd7ae0b97606372
Yegor Yefremov <yegorslists@googlemail.com>
2013-10-31 14:29:05 +0100

    libmodbus: bump version and change download location
    The license of libmodbus is LGPL v2.1 or later and the
    licence of programs in tests directory is GPL v3. So
    specify only LGPL v2.1.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libmodbus/libmodbus.mk
+++ package/libmodbus/libmodbus.mk
@@ -4,8 +4,10 @@
 #
 ################################################################################
 
-LIBMODBUS_VERSION = 3.0.3
-LIBMODBUS_SITE = http://github.com/downloads/stephane/libmodbus
+LIBMODBUS_VERSION = 3.0.5
+LIBMODBUS_SITE = http://libmodbus.org/site_media/build/
+LIBMODBUS_LICENSE = LGPLv2.1+
+LIBMODBUS_LICENSE_FILES = COPYING.LESSER
 LIBMODBUS_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
