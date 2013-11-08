69635dbe2ff6d300a31f99b34dc9e6ff9221f583
Danomi Manchego <danomimanchego123@gmail.com>
2013-11-07 22:10:39 -0500

    libqrencode: select libpng only when tools are selected
    As stated on the libqrencode web site, PNG (or SDL) is only
    needed for the test and utility programs; the library itself
    has no dependencies.
    
    While we are here, remove spaces in the ifeq clause, to match
    the examples in the buildroot manual.
    
    Signed-off-by: Danomi Manchego <danomimanchego123@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libqrencode/libqrencode.mk
+++ package/libqrencode/libqrencode.mk
@@ -7,15 +7,15 @@
 LIBQRENCODE_VERSION = 3.4.2
 LIBQRENCODE_SOURCE = qrencode-$(LIBQRENCODE_VERSION).tar.gz
 LIBQRENCODE_SITE = http://fukuchi.org/works/qrencode
-LIBQRENCODE_DEPENDENCIES = libpng
 LIBQRENCODE_INSTALL_STAGING = YES
 LIBQRENCODE_LICENSE = LGPLv2.1+
 LIBQRENCODE_LICENSE_FILES = COPYING
 
 ifeq ($(BR2_PACKAGE_LIBQRENCODE_TOOLS),y)
-        LIBQRENCODE_CONF_OPT += --with-tools=yes
+LIBQRENCODE_CONF_OPT += --with-tools=yes
+LIBQRENCODE_DEPENDENCIES += libpng
 else
-        LIBQRENCODE_CONF_OPT += --with-tools=no
+LIBQRENCODE_CONF_OPT += --with-tools=no
 endif
 
 $(eval $(autotools-package))
