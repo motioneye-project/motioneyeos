5a0e1258f369d9fcf75e977c3a76727a9a3096a3
Peter Korsgaard <peter@korsgaard.com>
2013-11-10 22:17:29 +0100

    openssh: bump version
    From the announcement:
    
    This release fixes a security bug:
    
     * sshd(8): fix a memory corruption problem triggered during rekeying
       when an AES-GCM cipher is selected. Full details of the vulnerability
       are available at: http://www.openssh.com/txt/gcmrekey.adv
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENSSH_VERSION = 6.3p1
+OPENSSH_VERSION = 6.4p1
 OPENSSH_SITE = http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 OPENSSH_CONF_ENV = LD="$(TARGET_CC)" LDFLAGS="$(TARGET_CFLAGS)"
 OPENSSH_CONF_OPT = --disable-lastlog --disable-utmp \
