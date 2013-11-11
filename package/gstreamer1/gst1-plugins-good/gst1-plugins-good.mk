14ee08f3e3c98a074ccd8cbc971d633c61110ab7
Peter Korsgaard <peter@korsgaard.com>
2013-11-11 11:29:49 +0100

    gst1-plugins-good: bump version
    Bugfix release. Patch is now upstream.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gstreamer1/gst1-plugins-good/gst1-plugins-good.mk
+++ package/gstreamer1/gst1-plugins-good/gst1-plugins-good.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_PLUGINS_GOOD_VERSION = 1.2.0
+GST1_PLUGINS_GOOD_VERSION = 1.2.1
 GST1_PLUGINS_GOOD_SOURCE = gst-plugins-good-$(GST1_PLUGINS_GOOD_VERSION).tar.xz
 GST1_PLUGINS_GOOD_SITE = http://gstreamer.freedesktop.org/src/gst-plugins-good
 GST1_PLUGINS_GOOD_LICENSE_FILES = COPYING
