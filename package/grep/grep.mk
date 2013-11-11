2299e2f2d73ba6dc56ebf5bc53e6e2aa599a5faf
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-11-11 11:25:22 -0300

    grep: bump to version 2.15
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/grep/grep.mk
+++ package/grep/grep.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GREP_VERSION = 2.14
+GREP_VERSION = 2.15
 GREP_SITE = $(BR2_GNU_MIRROR)/grep
 GREP_SOURCE = grep-$(GREP_VERSION).tar.xz
 GREP_LICENSE = GPLv3+
