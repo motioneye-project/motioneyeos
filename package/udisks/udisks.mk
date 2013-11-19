5e827993e737a3330e18fe97d99ccfbd46be2239
Peter Korsgaard <peter@korsgaard.com>
2013-11-19 23:49:09 +0100

    udisks: only build documentation if requested
    Fixes http://autobuild.buildroot.org/results/b23/b234e63f92e6e5162dd5340624e32caa31b82a1a/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/udisks/udisks.mk
+++ package/udisks/udisks.mk
@@ -20,7 +20,8 @@ UDISKS_DEPENDENCIES = 	\
 	lvm2		\
 	libatasmart
 
-UDISKS_CONF_OPT = --disable-remote-access
+UDISKS_CONF_OPT = --disable-remote-access \
+	$(if $(BR2_HAVE_DOCUMENTATION),,--disable-man-pages)
 
 ifeq ($(BR2_PACKAGE_UDISKS_LVM2),y)
 UDISKS_CONF_OPT += --enable-lvm2
