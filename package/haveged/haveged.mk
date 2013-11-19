9f07355f3dae75d557db2c2fe32929154ef8a812
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-11-19 09:10:32 -0300

    haveged: new package
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/haveged/haveged.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# haveged
+#
+################################################################################
+
+HAVEGED_VERSION = 1.7c
+HAVEGED_SITE = http://www.issihosts.com/haveged
+HAVEGED_LICENSE = GPLv3+
+HAVEGED_LICENSE_FILES = COPYING
+
+define HAVEGED_INSTALL_INIT_SYSV
+	$(INSTALL) -m 755 -D package/haveged/S21haveged \
+		$(TARGET_DIR)/etc/init.d/S21haveged
+endef
+
+$(eval $(autotools-package))
