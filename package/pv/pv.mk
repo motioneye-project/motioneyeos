8b24c37e3a332509d91ac2a4edc983a5e0b57ab0
Arnout Vandecappelle <arnout@mind.be>
2013-11-20 00:50:48 +0100

    pv: needs gettext if locale is enabled
    Fixes http://autobuild.buildroot.net/results/c4f/c4f5ed916d85740ec00f5f34ded5de0195e5cd45/
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/pv/pv.mk
+++ package/pv/pv.mk
@@ -9,6 +9,7 @@ PV_SOURCE = pv-$(PV_VERSION).tar.bz2
 PV_SITE = http://www.ivarch.com/programs/sources
 PV_LICENSE = Artistic-2.0
 PV_LICENSE_FILES = doc/COPYING
+PV_DEPENDENCIES = $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext)
 
 # While 'pv' uses autoconf, it does not use automake for its
 # makefiles. It uses $(LD) $(LDFLAGS) to achieve partial linking, but
