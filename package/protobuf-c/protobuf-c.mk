720008338acd7012b0a8c69525f2479f0794dffe
Ryan Barnett <rjbarnet@rockwellcollins.com>
2013-11-26 03:09:41 -0600

    protobuf-c: disable parallel build
    Fixes http://autobuild.buildroot.net/results/f47c4f0883019ba3a8e61a9ae51b7bd7f347b38c/
    
    Signed-off-by: Ryan Barnett <rjbarnet@rockwellcollins.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/protobuf-c/protobuf-c.mk
+++ package/protobuf-c/protobuf-c.mk
@@ -8,6 +8,7 @@ PROTOBUF_C_VERSION = 0.15
 PROTOBUF_C_SITE = http://protobuf-c.googlecode.com/files
 PROTOBUF_C_DEPENDENCIES = protobuf host-protobuf-c
 HOST_PROTOBUF_C_DEPENDENCIES = host-protobuf
+PROTOBUF_C_MAKE = $(MAKE1)
 PROTOBUF_C_CONF_OPT = --disable-protoc
 PROTOBUF_C_INSTALL_STAGING = YES
 PROTOBUF_C_LICENSE = BSD-3c
