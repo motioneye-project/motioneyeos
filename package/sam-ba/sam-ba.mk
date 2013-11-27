874f5f92ea5b210ab54c624eb4e6fc076e67124a
Simon Dawson <spdawson@gmail.com>
2013-11-27 22:23:57 +0000

    sam-ba: patch to latest version
    Apply the latest Atmel patch for sam-ba version 2.12
    
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sam-ba/sam-ba.mk
+++ package/sam-ba/sam-ba.mk
@@ -7,6 +7,7 @@
 SAM_BA_SITE    = http://www.atmel.com/dyn/resources/prod_documents/
 SAM_BA_VERSION = 2.12
 SAM_BA_SOURCE  = sam-ba_$(SAM_BA_VERSION).zip
+SAM_BA_PATCH = sam-ba_$(SAM_BA_VERSION)_patch5.gz
 SAM_BA_LICENSE = BSD-like (partly binary-only)
 SAM_BA_LICENSE_FILES = doc/readme.txt
 
