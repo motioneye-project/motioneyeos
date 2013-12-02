a34cad761baa82e50c855fe96de68de816399204
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-12-02 07:49:28 -0300

    sysklogd: install to /sbin
    Install syslogd/klogd to /sbin instead of /usr/sbin to really win over
    busybox and avoid the ugly noise when booting without busybox installed.
    Also drop uninstall hook and manpage installation.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sysklogd/sysklogd.mk
+++ package/sysklogd/sysklogd.mk
@@ -27,26 +27,12 @@ define SYSKLOGD_BUILD_CMDS
 endef
 
 define SYSKLOGD_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0500 $(@D)/syslogd $(TARGET_DIR)/usr/sbin/syslogd
-	$(INSTALL) -D -m 0500 $(@D)/klogd $(TARGET_DIR)/usr/sbin/klogd
-	$(INSTALL) -D -m 0644 $(@D)/sysklogd.8 $(TARGET_DIR)/usr/share/man/man8/sysklogd.8
-	$(INSTALL) -D -m 0644 $(@D)/syslogd.8 $(TARGET_DIR)/usr/share/man/man8/syslogd.8
-	$(INSTALL) -D -m 0644 $(@D)/syslog.conf.5 $(TARGET_DIR)/usr/share/man/man5/syslog.conf.5
-	$(INSTALL) -D -m 0644 $(@D)/klogd.8 $(TARGET_DIR)/usr/share/man/man8/klogd.8
+	$(INSTALL) -D -m 0500 $(@D)/syslogd $(TARGET_DIR)/sbin/syslogd
+	$(INSTALL) -D -m 0500 $(@D)/klogd $(TARGET_DIR)/sbin/klogd
 	if [ ! -f $(TARGET_DIR)/etc/syslog.conf ]; then \
 		$(INSTALL) -D -m 0644 package/sysklogd/syslog.conf \
 			$(TARGET_DIR)/etc/syslog.conf; \
 	fi
-endef
-
-define SYSKLOGD_UNINSTALL_TARGET_CMDS
-	rm -f $(TARGET_DIR)/usr/sbin/syslogd
-	rm -f $(TARGET_DIR)/usr/sbin/klogd
-	rm -f $(TARGET_DIR)/usr/share/man/man8/sysklogd.8
-	rm -f $(TARGET_DIR)/usr/share/man/man8/syslogd.8
-	rm -f $(TARGET_DIR)/usr/share/man/man5/syslog.conf.5
-	rm -f $(TARGET_DIR)/usr/share/man/man8/klogd.8
-	rm -f $(TARGET_DIR)/etc/syslog.conf
 endef
 
 define SYSKLOGD_CLEAN_CMDS
