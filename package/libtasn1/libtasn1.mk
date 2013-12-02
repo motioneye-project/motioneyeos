edce303b8da95a9b19939ab7a4199a39c0483c6f
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-12-02 09:22:57 -0300

    libtasn1: fix botched makeinfo/missing logic
    The makeinfo/missing logic is botched leading to failures such as:
    http://autobuild.buildroot.net/results/5f0/5f0e8f62be86b56813f5fc0510e83289ed2b0410/
    Just disable documentation build the hard way.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libtasn1/libtasn1.mk
+++ package/libtasn1/libtasn1.mk
@@ -10,5 +10,7 @@ LIBTASN1_DEPENDENCIES = host-bison
 LIBTASN1_LICENSE = GPLv3+ LGPLv2.1+
 LIBTASN1_LICENSE_FILES = COPYING COPYING.LIB
 LIBTASN1_INSTALL_STAGING = YES
+# 'missing' fallback logic botched so disable it completely
+LIBTASN1_CONF_ENV = MAKEINFO="true"
 
 $(eval $(autotools-package))
