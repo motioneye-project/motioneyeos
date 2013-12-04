b341b6c1a7330066e243e8288af0966a576dc46a
Yegor Yefremov <yegorslists@googlemail.com>
2013-12-04 10:55:04 +0100

    ModemManager: add new package
    [Peter: fixup license info as pointed out by Thomas]
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/modem-manager/modem-manager.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# modem-manager
+#
+################################################################################
+
+MODEM_MANAGER_VERSION = 1.0.0
+MODEM_MANAGER_SOURCE = ModemManager-$(MODEM_MANAGER_VERSION).tar.xz
+MODEM_MANAGER_SITE = http://www.freedesktop.org/software/ModemManager/
+MODEM_MANAGER_LICENSE = GPLv2+ (programs, plugins), LGPLv2+ (libmm-glib)
+MODEM_MANAGER_LICENSE_FILES = COPYING
+MODEM_MANAGER_DEPENDENCIES = host-pkgconf udev dbus-glib host-intltool
+
+ifeq ($(BR2_PACKAGE_MODEM_MANAGER_LIBQMI),y)
+	MODEM_MANAGER_DEPENDENCIES += libqmi
+	MODEM_MANAGER_CONF_OPT += --with-qmi
+else
+	MODEM_MANAGER_CONF_OPT += --without-qmi
+endif
+
+ifeq ($(BR2_PACKAGE_MODEM_MANAGER_LIBMBIM),y)
+	MODEM_MANAGER_DEPENDENCIES += libmbim
+	MODEM_MANAGER_CONF_OPT += --with-mbim
+else
+	MODEM_MANAGER_CONF_OPT += --without-mbim
+endif
+
+$(eval $(autotools-package))
