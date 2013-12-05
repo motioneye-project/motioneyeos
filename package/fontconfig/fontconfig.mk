7b8df9269904b1b92213ae95163bc7af95b4d11a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-12-05 11:04:48 -0300

    fontconfig: bump to version 2.11.0
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/fontconfig/fontconfig.mk
+++ package/fontconfig/fontconfig.mk
@@ -4,24 +4,16 @@
 #
 ################################################################################
 
-FONTCONFIG_VERSION = 2.6.0
+FONTCONFIG_VERSION = 2.11.0
 FONTCONFIG_SITE = http://fontconfig.org/release
+FONTCONFIG_INSTALL_STAGING = YES
+FONTCONFIG_DEPENDENCIES = freetype expat host-pkgconf
 FONTCONFIG_LICENSE = fontconfig license
 FONTCONFIG_LICENSE_FILES = COPYING
-FONTCONFIG_AUTORECONF = YES
-FONTCONFIG_INSTALL_STAGING = YES
-# This package does not like using the target cflags for some reason.
-FONTCONFIG_CONF_ENV = CFLAGS="-I$(STAGING_DIR)/usr/include/freetype2"
 
 FONTCONFIG_CONF_OPT = --with-arch=$(GNU_TARGET_NAME) \
-		--with-freetype-config="$(STAGING_DIR)/usr/bin/freetype-config" \
 		--with-cache-dir=/var/cache/fontconfig \
-		--with-expat="$(STAGING_DIR)/usr/lib" \
-		--with-expat-lib=$(STAGING_DIR)/usr/lib \
-		--with-expat-includes=$(STAGING_DIR)/usr/include \
 		--disable-docs
-
-FONTCONFIG_DEPENDENCIES = freetype expat
 
 HOST_FONTCONFIG_CONF_OPT = \
 		--disable-static
