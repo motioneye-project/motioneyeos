c703ef5a2c6f61d28cf0001a4cfd9f5be27bc71c
Axel Lin <axel.lin@ingics.com>
2013-12-09 10:05:00 +0800

    snappy: bump to version 1.1.1
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/snappy/snappy.mk
+++ package/snappy/snappy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SNAPPY_VERSION		= 1.1.0
+SNAPPY_VERSION		= 1.1.1
 SNAPPY_SITE		= http://snappy.googlecode.com/files/
 SNAPPY_LICENSE		= BSD-3c
 SNAPPY_LICENSE_FILES	= COPYING
