dd6a8c728e4161d245ba91196123a90de7461342
Peter Korsgaard <peter@korsgaard.com>
2013-12-10 10:08:26 +0100

    imagemagick: drop IMAGEMAGICK_MAJOR variable
    It is only expanded once just below, so we might as well drop it.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/imagemagick/imagemagick.mk
+++ package/imagemagick/imagemagick.mk
@@ -4,8 +4,7 @@
 #
 ################################################################################
 
-IMAGEMAGICK_MAJOR = 6.8.7
-IMAGEMAGICK_VERSION = $(IMAGEMAGICK_MAJOR)-9
+IMAGEMAGICK_VERSION = 6.8.7-9
 IMAGEMAGICK_SOURCE = ImageMagick-$(IMAGEMAGICK_VERSION).tar.xz
 # The official ImageMagick site only keeps the latest versions
 # available, which is annoying. Use an alternate site that keeps all
