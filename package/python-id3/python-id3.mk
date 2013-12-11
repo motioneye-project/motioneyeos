d26c5bc2e9945f4b2b0b8d130ada6dfa1a7957b9
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-12-11 21:26:40 +0100

    python-id3: convert to the Python package infrastructure
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-id3/python-id3.mk
+++ package/python-id3/python-id3.mk
@@ -7,15 +7,6 @@
 PYTHON_ID3_VERSION = 1.2
 PYTHON_ID3_SOURCE = id3-py_$(PYTHON_ID3_VERSION).tar.gz
 PYTHON_ID3_SITE = http://downloads.sourceforge.net/project/id3-py/id3-py/$(PYTHON_ID3_VERSION)
+PYTHON_ID3_SETUP_TYPE = distutils
 
-PYTHON_ID3_DEPENDENCIES = python
-
-define PYTHON_ID3_BUILD_CMDS
-    (cd $(@D); $(HOST_DIR)/usr/bin/python setup.py build)
-endef
-
-define PYTHON_ID3_INSTALL_TARGET_CMDS
-    (cd $(@D); $(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
-endef
-
-$(eval $(generic-package))
+$(eval $(python-package))
