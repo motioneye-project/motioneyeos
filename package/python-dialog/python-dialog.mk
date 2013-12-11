d3e65118132968b6575a98431337a5300e38879d
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-12-11 21:26:58 +0100

    python-dialog: convert to the Python package infrastructure
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-dialog/python-dialog.mk
+++ package/python-dialog/python-dialog.mk
@@ -9,15 +9,7 @@ PYTHON_DIALOG_SOURCE  = python2-pythondialog-$(PYTHON_DIALOG_VERSION).tar.bz2
 PYTHON_DIALOG_SITE    = http://downloads.sourceforge.net/project/pythondialog/pythondialog/$(PYTHON_DIALOG_VERSION)
 PYTHON_DIALOG_LICENSE = LGPLv2.1+
 PYTHON_DIALOG_LICENSE_FILES = COPYING
+PYTHON_DIALOG_SETUP_TYPE = distutils
+PYTHON_DIALOG_DEPENDENCIES = dialog
 
-PYTHON_DIALOG_DEPENDENCIES = python dialog
-
-define PYTHON_DIALOG_BUILD_CMDS
-	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py build)
-endef
-
-define PYTHON_DIALOG_INSTALL_TARGET_CMDS
-	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
-endef
-
-$(eval $(generic-package))
+$(eval $(python-package))
