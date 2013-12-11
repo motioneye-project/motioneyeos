033e218c34398afc1c52c7d30216e13bf5242582
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-12-11 21:26:46 +0100

    python-protobuf: convert to the Python package infrastructure
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-protobuf/python-protobuf.mk
+++ package/python-protobuf/python-protobuf.mk
@@ -9,20 +9,8 @@ PYTHON_PROTOBUF_SOURCE  = $(PROTOBUF_SOURCE)
 PYTHON_PROTOBUF_SITE    = $(PROTOBUF_SITE)
 PYTHON_PROTOBUF_LICENSE = BSD-3c
 PYTHON_PROTOBUF_LICENSE_FILES = COPYING.txt
+PYTHON_PROTOBUF_DEPENDENCIES = host-protobuf
+PYTHON_PROTOBUF_SETUP_TYPE = setuptools
+PYTHON_PROTOBUF_SUBDIR = python
 
-PYTHON_PROTOBUF_DEPENDENCIES = python host-python-setuptools \
-        host-python-distutilscross host-protobuf
-
-define PYTHON_PROTOBUF_BUILD_CMDS
-	(cd $(@D)/python; \
-		PYTHONXCPREFIX="$(STAGING_DIR)/usr/" \
-		PATH=$(HOST_PATH) \
-	$(HOST_DIR)/usr/bin/python setup.py build -x)
-endef
-
-define PYTHON_PROTOBUF_INSTALL_TARGET_CMDS
-	(cd $(@D)/python; PYTHONPATH=$(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages \
-	$(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
-endef
-
-$(eval $(generic-package))
+$(eval $(python-package))
