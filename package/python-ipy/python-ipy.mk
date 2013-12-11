1825edece08e4bc5e5e7c52268ff39cb7cdc174c
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-12-11 21:26:41 +0100

    python-ipy: convert to the Python package infrastructure
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-ipy/python-ipy.mk
+++ package/python-ipy/python-ipy.mk
@@ -6,21 +6,8 @@
 
 PYTHON_IPY_VERSION = IPy-0.75
 PYTHON_IPY_SITE    = $(call github,haypo,python-ipy,$(PYTHON_IPY_VERSION))
-PYTHON_IPY_DEPENDENCIES = host-python python
 PYTHON_IPY_LICENSE = BSD-3c
 PYTHON_IPY_LICENSE_FILES = COPYING
+PYTHON_IPY_SETUP_TYPE = distutils
 
-define PYTHON_IPY_BUILD_CMDS
-	(cd $(@D); \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(HOST_DIR)/usr/bin/python setup.py build_ext \
-		--include-dirs=$(STAGING_DIR)/usr/include/python$(PYTHON_VERSION_MAJOR) \
-	)
-	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py build)
-endef
-
-define PYTHON_IPY_INSTALL_TARGET_CMDS
-	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
-endef
-
-$(eval $(generic-package))
+$(eval $(python-package))
