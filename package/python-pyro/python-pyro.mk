fac776c8ccc41dc030345d5c5286485b47b17c9f
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-12-11 21:26:49 +0100

    python-pyro: convert to the Python package infrastructure
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-pyro/python-pyro.mk
+++ package/python-pyro/python-pyro.mk
@@ -9,10 +9,6 @@ PYTHON_PYRO_SOURCE  = Pyro-$(PYTHON_PYRO_VERSION).tar.gz
 PYTHON_PYRO_SITE    = https://pypi.python.org/packages/source/P/Pyro/
 PYTHON_PYRO_LICENSE = MIT
 PYTHON_PYRO_LICENSE_FILES = LICENSE
-PYTHON_PYRO_DEPENDENCIES = python
+PYTHON_PYRO_SETUP_TYPE = distutils
 
-define PYTHON_PYRO_INSTALL_TARGET_CMDS
-	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
-endef
-
-$(eval $(generic-package))
+$(eval $(python-package))
