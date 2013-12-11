f36827703a3b2ed6eb18643fbd151e74ff885114
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-12-11 10:23:44 -0300

    libcdaudio: add licensing info
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libcdaudio/libcdaudio.mk
+++ package/libcdaudio/libcdaudio.mk
@@ -8,5 +8,7 @@ LIBCDAUDIO_VERSION = 0.99.12p2
 LIBCDAUDIO_SITE = http://downloads.sourceforge.net/project/libcdaudio/libcdaudio/$(LIBCDAUDIO_VERSION)/
 LIBCDAUDIO_INSTALL_STAGING = YES
 LIBCDAUDIO_CONFIG_SCRIPTS = libcdaudio-config
+LIBCDAUDIO_LICENSE = GPLv2+
+LIBCDAUDIO_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
