5302d7ebbae5c6ca9ce8b016cb4876bbef27802e
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-12-11 10:23:36 -0300

    libsamplerate: add licensing info
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libsamplerate/libsamplerate.mk
+++ package/libsamplerate/libsamplerate.mk
@@ -9,6 +9,8 @@ LIBSAMPLERATE_SITE = http://www.mega-nerd.com/SRC
 LIBSAMPLERATE_INSTALL_STAGING = YES
 LIBSAMPLERATE_DEPENDENCIES = host-pkgconf
 LIBSAMPLERATE_CONF_OPT = --disable-fftw --program-transform-name=''
+LIBSAMPLERATE_LICENSE = Dual GPLv2+ / libsamplerate commercial use license
+LIBSAMPLERATE_LICENSE_FILES = COPYING
 
 ifeq ($(BR2_PACKAGE_LIBSNDFILE),y)
 LIBSAMPLERATE_DEPENDENCIES += libsndfile
