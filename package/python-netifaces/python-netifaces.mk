8fd5a94562b771b56e1b06d6b3ea9b9a19e1a52a
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-12-11 21:26:54 +0100

    python-netifaces: convert to the Python package infrastructure
    Also, remove the "select BR2_PACKAGE_PYTHON_SETUPTOOLS", since
    setuptools is a host dependency (needed to built the package), not a
    target dependency.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-netifaces/python-netifaces.mk
+++ package/python-netifaces/python-netifaces.mk
@@ -7,21 +7,6 @@
 PYTHON_NETIFACES_VERSION = 0.7
 PYTHON_NETIFACES_SOURCE  = netifaces-$(PYTHON_NETIFACES_VERSION).tar.gz
 PYTHON_NETIFACES_SITE    = http://alastairs-place.net/projects/netifaces
+PYTHON_NETIFACES_SETUP_TYPE = setuptools
 
-PYTHON_NETIFACES_DEPENDENCIES = python host-python-setuptools host-python-distutilscross
-
-define PYTHON_NETIFACES_BUILD_CMDS
-	(cd $(@D); \
-		PYTHONXCPREFIX="$(STAGING_DIR)/usr/" \
-		LDFLAGS="-L$(STAGING_DIR)/lib -L$(STAGING_DIR)/usr/lib" \
-	$(HOST_DIR)/usr/bin/python setup.py build -x)
-endef
-
-define PYTHON_NETIFACES_INSTALL_TARGET_CMDS
-	(cd $(@D); \
-	PYTHONPATH=$(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages \
-	$(HOST_DIR)/usr/bin/python setup.py install \
-	--single-version-externally-managed --root=/ --prefix=$(TARGET_DIR)/usr)
-endef
-
-$(eval $(generic-package))
+$(eval $(python-package))
