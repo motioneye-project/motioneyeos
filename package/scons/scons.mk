a69b2d3926826d72374800ac8eda6646aa0aa078
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-12-11 21:26:53 +0100

    scons: convert to the Python package infrastructure
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/scons/scons.mk
+++ package/scons/scons.mk
@@ -8,17 +8,12 @@ SCONS_VERSION = 2.3.0
 SCONS_SITE = http://downloads.sourceforge.net/project/scons/scons/$(SCONS_VERSION)
 SCONS_LICENSE = MIT
 SCONS_LICENSE_FILES = LICENSE.txt
+SCONS_SETUP_TYPE = distutils
 
-define HOST_SCONS_BUILD_CMDS
-	(cd $(@D); python setup.py build)
-endef
+HOST_SCONS_INSTALL_OPT = \
+	--install-lib=$(HOST_DIR)/usr/lib/scons-$(SCONS_VERSION)
 
-define HOST_SCONS_INSTALL_CMDS
-	(cd $(@D); python setup.py install --prefix=$(HOST_DIR)/usr \
-		--install-lib=$(HOST_DIR)/usr/lib/scons-$(SCONS_VERSION))
-endef
-
-$(eval $(host-generic-package))
+$(eval $(host-python-package))
 
 # variables used by other packages
 SCONS = $(HOST_DIR)/usr/bin/scons
