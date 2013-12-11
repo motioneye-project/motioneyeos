d6c20a3524513c39dfb2d47251c2cccd384b5b8f
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-12-11 21:26:39 +0100

    python-dpkt: convert to the Python package infrastructure
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-dpkt/python-dpkt.mk
+++ package/python-dpkt/python-dpkt.mk
@@ -7,15 +7,6 @@
 PYTHON_DPKT_VERSION = 1.7
 PYTHON_DPKT_SOURCE  = dpkt-$(PYTHON_DPKT_VERSION).tar.gz
 PYTHON_DPKT_SITE    = http://dpkt.googlecode.com/files
+PYTHON_DPKT_SETUP_TYPE = distutils
 
-PYTHON_DPKT_DEPENDENCIES = python
-
-define PYTHON_DPKT_BUILD_CMDS
-	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py build)
-endef
-
-define PYTHON_DPKT_INSTALL_TARGET_CMDS
-	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
-endef
-
-$(eval $(generic-package))
+$(eval $(python-package))
