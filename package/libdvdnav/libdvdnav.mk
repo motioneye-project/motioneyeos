d7f9e045dc062b3ef962df7954de93f7ff831c80
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-12-11 10:23:41 -0300

    libdvdnav: add licensing info
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libdvdnav/libdvdnav.mk
+++ package/libdvdnav/libdvdnav.mk
@@ -11,6 +11,8 @@ LIBDVDNAV_AUTORECONF = YES
 LIBDVDNAV_INSTALL_STAGING = YES
 LIBDVDNAV_CONFIG_SCRIPTS = dvdnav-config
 LIBDVDNAV_DEPENDENCIES = libdvdread host-pkgconf
+LIBDVDNAV_LICENSE = GPLv2+
+LIBDVDNAV_LICENSE_FILES = COPYING
 
 # By default libdvdnav tries to find dvdread-config in $PATH. Because
 # of cross compilation, we prefer using pkg-config.
