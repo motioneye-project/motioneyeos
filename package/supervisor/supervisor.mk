0935498a76d4a86d8d998ebb8d42c74af5a26075
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-12-11 21:26:55 +0100

    supervisor: convert to the Python package infrastructure
    Remove the select of BR2_PACKAGE_PYTHON_SETUPTOOLS, since what is
    really needed is host-setuptools (as can be seen from the original .mk
    file), and not setuptools for the target.
    
    Also, remove the mangling of the Python shebang, since it's now done
    by passing the --executable= option at install time (done in the
    Python package infrastructure).
    
    Finally, convert the package to use <pkg>_INSTALL_INIT_SYSV.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/supervisor/supervisor.mk
+++ package/supervisor/supervisor.mk
@@ -7,19 +7,19 @@
 SUPERVISOR_VERSION = 3.0a12
 SUPERVISOR_SITE = http://pypi.python.org/packages/source/s/supervisor/
 SUPERVISOR_LICENSE_FILES = LICENSES.txt
+SUPERVISOR_SETUP_TYPE = setuptools
 
-SUPERVISOR_DEPENDENCIES = python host-python-setuptools
-
-define SUPERVISOR_BUILD_CMDS
-	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py build)
-endef
-
-define SUPERVISOR_INSTALL_TARGET_CMDS
-	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py install --prefix=/usr --root=$(TARGET_DIR))
-	sed -i '1s|#!.*python.*|#!/usr/bin/env python|' $(TARGET_DIR)/usr/bin/{echo_supervisord_conf,pidproxy,supervisorctl,supervisord}
+define SUPERVISOR_INSTALL_CONF_FILES
 	$(INSTALL) -d -m 755 $(TARGET_DIR)/etc/supervisor.d
-	$(INSTALL) -D -m 644 package/supervisor/supervisord.conf $(TARGET_DIR)/etc/supervisord.conf
-	$(INSTALL) -m 755 package/supervisor/S99supervisord $(TARGET_DIR)/etc/init.d/S99supervisord
+	$(INSTALL) -D -m 644 package/supervisor/supervisord.conf \
+		$(TARGET_DIR)/etc/supervisord.conf
 endef
 
-$(eval $(generic-package))
+SUPERVISOR_POST_INSTALL_TARGET_HOOKS += SUPERVISOR_INSTALL_CONF_FILES
+
+define SUPERVISOR_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 package/supervisor/S99supervisord \
+		$(TARGET_DIR)/etc/init.d/S99supervisord
+endef
+
+$(eval $(python-package))
