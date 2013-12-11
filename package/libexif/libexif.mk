b3da05b899aa758a66f7eecc422c3ff676a2465b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-12-11 10:23:39 -0300

    libexif: add licensing info
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libexif/libexif.mk
+++ package/libexif/libexif.mk
@@ -10,5 +10,7 @@ LIBEXIF_SITE = http://downloads.sourceforge.net/project/libexif/libexif/$(LIBEXI
 LIBEXIF_INSTALL_STAGING = YES
 LIBEXIF_CONF_OPT = --disable-docs
 LIBEXIF_DEPENDENCIES = host-pkgconf
+LIBEXIF_LICENSE = LGPLv2.1+
+LIBEXIF_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
