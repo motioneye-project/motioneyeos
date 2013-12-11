b8b9ef8934c510dfee7e27951dedcc755e52578c
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-12-11 21:26:56 +0100

    python-distutilscross: convert to the Python package infrastructure
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-distutilscross/python-distutilscross.mk
+++ package/python-distutilscross/python-distutilscross.mk
@@ -7,15 +7,6 @@
 PYTHON_DISTUTILSCROSS_VERSION = 0.1
 PYTHON_DISTUTILSCROSS_SOURCE  = distutilscross-$(PYTHON_DISTUTILSCROSS_VERSION).tar.gz
 PYTHON_DISTUTILSCROSS_SITE    = http://pypi.python.org/packages/source/d/distutilscross
+PYTHON_DISTUTILSCROSS_SETUP_TYPE = setuptools
 
-HOST_PYTHON_DISTUTILSCROSS_DEPENDENCIES = host-python host-python-setuptools
-
-define HOST_PYTHON_DISTUTILSCROSS_BUILD_CMDS
-	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py build)
-endef
-
-define HOST_PYTHON_DISTUTILSCROSS_INSTALL_CMDS
-	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py install --prefix=$(HOST_DIR)/usr)
-endef
-
-$(eval $(host-generic-package))
+$(eval $(host-python-package))
