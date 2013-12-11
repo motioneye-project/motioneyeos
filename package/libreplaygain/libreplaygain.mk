f29e5768ca3ba67346ee27d879722226b6fb3aba
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-12-11 10:23:37 -0300

    libreplaygain: add licensing info
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libreplaygain/libreplaygain.mk
+++ package/libreplaygain/libreplaygain.mk
@@ -10,5 +10,7 @@ LIBREPLAYGAIN_SOURCE = libreplaygain_$(LIBREPLAYGAIN_VERSION).tar.gz
 # upstream doesn't ship configure
 LIBREPLAYGAIN_AUTORECONF = YES
 LIBREPLAYGAIN_INSTALL_STAGING = YES
+LIBREPLAYGAIN_LICENSE = LGPLv2.1+
+LIBREPLAYGAIN_LICENSE_FILES = src/gain_analysis.c
 
 $(eval $(autotools-package))
