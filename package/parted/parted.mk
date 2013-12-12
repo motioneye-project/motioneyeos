b1f2cbb40950a73741dfa578f43f2a5e88cdd953
Yann E. MORIN <yann.morin.1998@free.fr>
2013-12-12 19:18:40 +0100

    package/parted: relax dependency on lvm2
    parted can be configured without support for LVM volumes, so
    we can relax the dependency on lvm2, and make it an optional
    feature.
    
    Remove now no-longer needed dependency on MMU because of lvm2.
    
    (Based on a code-snippet from Thomas.)
    
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/parted/parted.mk
+++ package/parted/parted.mk
@@ -7,9 +7,16 @@
 PARTED_VERSION = 3.1
 PARTED_SOURCE = parted-$(PARTED_VERSION).tar.xz
 PARTED_SITE = $(BR2_GNU_MIRROR)/parted
-PARTED_DEPENDENCIES = readline util-linux lvm2
+PARTED_DEPENDENCIES = readline util-linux
 PARTED_INSTALL_STAGING = YES
 PARTED_LICENSE = GPLv3+
 PARTED_LICENSE_FILES = COPYING
+
+ifeq ($(BR2_PACKAGE_LVM2),y)
+PARTED_DEPENDENCIES += lvm2
+PARTED_CONF_OPT += --enable-device-mapper
+else
+PARTED_CONF_OPT += --disable-device-mapper
+endif
 
 $(eval $(autotools-package))
