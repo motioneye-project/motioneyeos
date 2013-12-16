906ab08f946936a58dc36efd878ea45ea453df69
Peter Korsgaard <peter@korsgaard.com>
2013-12-16 16:53:59 +0100

    knock: really fix github macro arguments
    Somehow I ended up dropping the package name when I fixed the version,
    breaking download.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/knock/knock.mk
+++ package/knock/knock.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 KNOCK_VERSION = 7666f2e86e18d482eaad5fe1fea46d87d80b0555
-KNOCK_SITE = $(call github,jvinet,$(KNOCK_VERSION))
+KNOCK_SITE = $(call github,jvinet,knock,$(KNOCK_VERSION))
 KNOCK_LICENSE = GPLv2+
 KNOCK_LICENSE_FILES = COPYING
 KNOCK_AUTORECONF = YES
