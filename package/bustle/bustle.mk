a0469f5bac47ec1434fa2e54a01c9f287a729e81
Peter Korsgaard <peter@korsgaard.com>
2013-12-17 10:36:32 +0100

    bustle: bump version
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/bustle/bustle.mk
+++ package/bustle/bustle.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BUSTLE_VERSION = 0.4.2
+BUSTLE_VERSION = 0.4.3
 BUSTLE_SITE = http://www.willthompson.co.uk/bustle/releases/$(BUSTLE_VERSION)/
 BUSTLE_LICENSE = LGPLv2.1+
 BUSTLE_LICENSE_FILES = LICENSE
