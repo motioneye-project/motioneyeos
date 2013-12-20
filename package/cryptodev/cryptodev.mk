569b842985713d02f34367621ed7a6b6e5efdf73
Yann E. MORIN <yann.morin.1998@free.fr>
2013-12-20 23:27:59 +0100

    package/cryptodev: switch to package-defined providers
    For the underlying reasons, see previous patch, titled:
        package/opengl/libegl: switch to package-defined providers
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Arnout Vandecappelle <arnout@mind.be>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Peter Korsgaard <jacmet@uclibc.org>
    Cc: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/cryptodev/cryptodev.mk
+++ package/cryptodev/cryptodev.mk
@@ -5,14 +5,7 @@
 ################################################################################
 
 CRYPTODEV_SOURCE =
-
-ifeq ($(BR2_PACKAGE_CRYPTODEV_LINUX),y)
-CRYPTODEV_DEPENDENCIES += cryptodev-linux
-endif
-
-ifeq ($(BR2_PACKAGE_OCF_LINUX),y)
-CRYPTODEV_DEPENDENCIES += ocf-linux
-endif
+CRYPTODEV_DEPENDENCIES = $(call qstrip,$(BR2_PACKAGE_PROVIDES_CRYPTODEV))
 
 ifeq ($(CRYPTODEV_DEPENDENCIES),)
 define CRYPTODEV_CONFIGURE_CMDS
