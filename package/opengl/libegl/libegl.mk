2c33417c9e777ab5eb5d21bc5354208d9b79ba11
Yann E. MORIN <yann.morin.1998@free.fr>
2013-12-20 23:27:53 +0100

    package/opengl/libegl: switch to package-defined providers
    It seems weird that the libegl package has to know all of the packages
    that may provide the EGL implementation.
    
    This also breaks implementation provided by packages in BR2_EXTERNAL,
    since we can't easily depend on the order of packages before we can
    derive the dependencies of libegl.
    
    So, Arnout and I separately came up to the same conclusion: move the
    dependency selection from out of libegl, and into each of the packages
    that provides an EGL implementation. Here it is.
    
    Since we no longer depend on the order packages are included, but on
    values set at Kconfig-time, we can now have EGL providers in BR2_EXTERNAL.
    
    Reported-by: David Corvoysier <david.corvoysier@orange.com>
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Arnout Vandecappelle <arnout@mind.be>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Peter Korsgaard <jacmet@uclibc.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/opengl/libegl/libegl.mk
+++ package/opengl/libegl/libegl.mk
@@ -5,22 +5,7 @@
 ################################################################################
 
 LIBEGL_SOURCE =
-
-ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
-LIBEGL_DEPENDENCIES += rpi-userland
-endif
-
-ifeq ($(BR2_PACKAGE_TI_GFX),y)
-LIBEGL_DEPENDENCIES += ti-gfx
-endif
-
-ifeq ($(BR2_PACKAGE_SUNXI_MALI),y)
-LIBEGL_DEPENDENCIES += sunxi-mali
-endif
-
-ifeq ($(BR2_PACKAGE_GPU_VIV_BIN_MX6Q),y)
-LIBEGL_DEPENDENCIES += gpu-viv-bin-mx6q
-endif
+LIBEGL_DEPENDENCIES = $(call qstrip,$(BR2_PACKAGE_PROVIDES_OPENGL_EGL))
 
 ifeq ($(LIBEGL_DEPENDENCIES),)
 define LIBEGL_CONFIGURE_CMDS
