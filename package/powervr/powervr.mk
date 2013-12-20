fe93cc8b5006db835ab1e417c2df6c9a0364697b
Yann E. MORIN <yann.morin.1998@free.fr>
2013-12-20 23:27:57 +0100

    package/powervr: switch to package-defined providers
    For the underlying reasons, see previous patch, titled:
        package/opengl/libegl: switch to package-defined providers
    
    Reported-by: David Corvoysier <david.corvoysier@orange.com>
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Arnout Vandecappelle <arnout@mind.be>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Peter Korsgaard <jacmet@uclibc.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/powervr/powervr.mk
+++ package/powervr/powervr.mk
@@ -5,10 +5,7 @@
 ################################################################################
 
 POWERVR_SOURCE =
-
-ifeq ($(BR2_PACKAGE_TI_GFX),y)
-POWERVR_DEPENDENCIES += ti-gfx
-endif
+POWERVR_DEPENDENCIES = $(call qstrip,$(BR2_PACKAGE_PROVIDES_POWERVR))
 
 ifeq ($(POWERVR_DEPENDENCIES),)
 define POWERVR_CONFIGURE_CMDS
