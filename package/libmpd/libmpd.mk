e9b979dc342f4fa12dcc43eab8bf3bd2789cc528
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2013-12-23 09:36:47 +0100

    libmpd: change download URL
    The old and new URLs are equivalent, but the new URL is not specific
    to an older version of libmpd, which seems more logical.
    
    [Thomas: update commit log.]
    
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libmpd/libmpd.mk
+++ package/libmpd/libmpd.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBMPD_VERSION = 11.8.17
-LIBMPD_SITE = http://download.sarine.nl/Programs/gmpc/11.8.16
+LIBMPD_SITE = http://download.sarine.nl/Programs/gmpc/11.8
 LIBMPD_INSTALL_STAGING = YES
 LIBMPD_DEPENDENCIES = libglib2
 LIBMPD_LICENSE = GPLv2+
