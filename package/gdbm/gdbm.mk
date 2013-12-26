15fe6f6181493a5f16f3a991037c972523ab5fc1
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-12-26 14:02:09 -0300

    gdbm: bump to version 1.11 and fix license
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gdbm/gdbm.mk
+++ package/gdbm/gdbm.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-GDBM_VERSION = 1.10
+GDBM_VERSION = 1.11
 GDBM_SITE = $(BR2_GNU_MIRROR)/gdbm
-GDBM_LICENSE = GPLv3
+GDBM_LICENSE = GPLv3+
 GDBM_LICENSE_FILES = COPYING
 GDBM_INSTALL_STAGING = YES
 
