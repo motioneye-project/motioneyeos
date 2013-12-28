aa83bc88ba4c738daaf944c1b2a7d00001d05a67
Axel Lin <axel.lin@ingics.com>
2013-12-28 10:22:28 +0800

    logrotate: bump to version 3.8.7
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/logrotate/logrotate.mk
+++ package/logrotate/logrotate.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LOGROTATE_VERSION = 3.8.6
+LOGROTATE_VERSION = 3.8.7
 LOGROTATE_SITE = https://www.fedorahosted.org/releases/l/o/logrotate/
 LOGROTATE_LICENSE = GPLv2+
 LOGROTATE_LICENSE_FILES = COPYING
