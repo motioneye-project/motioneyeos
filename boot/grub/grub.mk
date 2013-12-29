5cab919bb3aec8270786f6bd460d677a8546c70f
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2013-12-29 18:33:47 +0100

    grub: align splashscreen handling with iso9660 code
    This commit aligns the Grub handling of the splash screen with what is
    done in the ISO9660 code.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>

--- boot/grub/grub.mk
+++ boot/grub/grub.mk
@@ -82,7 +82,10 @@ GRUB_CONF_OPT = \
 ifeq ($(BR2_TARGET_GRUB_SPLASH),y)
 define GRUB_INSTALL_SPLASH
 	cp boot/grub/splash.xpm.gz $(TARGET_DIR)/boot/grub/
-	$(SED) 's/^#splashimage/splashimage/' $(TARGET_DIR)/boot/grub/menu.lst
+endef
+else
+define GRUB_INSTALL_SPLASH
+	$(SED) '/^splashimage/d' $(TARGET_DIR)/boot/grub/menu.lst
 endef
 endif
 
