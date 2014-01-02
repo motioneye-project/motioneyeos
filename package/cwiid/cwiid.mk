9c47cd5b6471e62874fa88e58c1bf9b44742a9c3
Samuel Martin <s.martin49@gmail.com>
2014-01-02 20:00:57 +0100

    cwiid: new package
    This patch only add the native cwiid library and a couple of tools.
    
    The 2 first cwiid patches are retrieved from upstream and fix build
    issues, so does the 3rd one.
    The 4th patch allows to disable wmgui build, so allows to reduce to
    number of dependencies.
    
    Cc: Eric Jarrige <eric.jarrige@armadeus.org>
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>

---
+++ package/cwiid/cwiid.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# cwiid
+#
+################################################################################
+
+CWIID_VERSION = 0.6.00
+CWIID_SOURCE = cwiid-$(CWIID_VERSION).tgz
+CWIID_SITE = http://abstrakraft.org/cwiid/downloads
+CWIID_LICENSE = GPLv2+
+CWIID_LICENSE_FILES = COPYING
+
+CWIID_AUTORECONF = YES
+CWIID_INSTALL_STAGING = YES
+
+CWIID_DEPENDENCIES = host-pkgconf host-bison host-flex bluez_utils
+
+# Disable python support. This disables the 2 following things:
+#   - wminput Python plugin support
+#   - cwiid Python module
+CWIID_CONF_OPT = --without-python --disable-ldconfig
+
+ifeq ($(BR2_PACKAGE_CWIID_WMGUI),y)
+CWIID_DEPENDENCIES += libgtk2 libglib2
+CWIID_CONF_OPT += --enable-wmgui
+else
+CWIID_CONF_OPT += --disable-wmgui
+endif
+
+$(eval $(autotools-package))
