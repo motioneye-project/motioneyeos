e8ac7537186e3b5318a3872d39a5a6e4e4b3657b
Bernd Kuhls <berndkuhls@hotmail.com>
2014-01-04 14:39:02 +0100

    xorg: Bump xproto_xproto version to 7.0.25
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xproto_xproto/xproto_xproto.mk
+++ package/x11r7/xproto_xproto/xproto_xproto.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XPROTO_XPROTO_VERSION = 7.0.23
+XPROTO_XPROTO_VERSION = 7.0.25
 XPROTO_XPROTO_SOURCE = xproto-$(XPROTO_XPROTO_VERSION).tar.bz2
 XPROTO_XPROTO_SITE = http://xorg.freedesktop.org/releases/individual/proto
 XPROTO_XPROTO_LICENSE = MIT
