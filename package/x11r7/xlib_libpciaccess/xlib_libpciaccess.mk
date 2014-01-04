a1a0144039daf9db3f52553c7bfdb6b28e1c1549
Bernd Kuhls <berndkuhls@hotmail.com>
2014-01-04 14:38:37 +0100

    xorg: Bump xlib_libpciaccess version to 0.13.2
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xlib_libpciaccess/xlib_libpciaccess.mk
+++ package/x11r7/xlib_libpciaccess/xlib_libpciaccess.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBPCIACCESS_VERSION = 0.13.1
+XLIB_LIBPCIACCESS_VERSION = 0.13.2
 XLIB_LIBPCIACCESS_SOURCE = libpciaccess-$(XLIB_LIBPCIACCESS_VERSION).tar.bz2
 XLIB_LIBPCIACCESS_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBPCIACCESS_LICENSE = MIT
