ed7621c536a69f692d7564bbcc299d6b189b5c55
Bernd Kuhls <berndkuhls@hotmail.com>
2014-01-04 14:38:35 +0100

    xorg: Bump xkeyboard-config version to 2.10.1
    [Peter: drop uneeded extra dependencies]
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xkeyboard-config/xkeyboard-config.mk
+++ package/x11r7/xkeyboard-config/xkeyboard-config.mk
@@ -4,13 +4,12 @@
 #
 ################################################################################
 
-XKEYBOARD_CONFIG_VERSION = 2.6
+XKEYBOARD_CONFIG_VERSION = 2.10.1
 XKEYBOARD_CONFIG_SOURCE = xkeyboard-config-$(XKEYBOARD_CONFIG_VERSION).tar.bz2
 XKEYBOARD_CONFIG_SITE = http://www.x.org/releases/individual/data/xkeyboard-config/
 XKEYBOARD_CONFIG_LICENSE = MIT
 XKEYBOARD_CONFIG_LICENSE_FILES = COPYING
 
 XKEYBOARD_CONFIG_DEPENDENCIES = host-gettext host-intltool host-xapp_xkbcomp
-XKEYBOARD_CONFIG_CONF_OPT += --disable-runtime-deps
 
 $(eval $(autotools-package))
