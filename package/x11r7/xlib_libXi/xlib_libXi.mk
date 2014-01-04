a6ff1dbfaacaf90d8c2f45bddea2fe6c300945e1
Bernd Kuhls <berndkuhls@hotmail.com>
2014-01-04 14:38:46 +0100

    xorg: Bump xlib_libXi version to 1.7.2
    [Peter: needs libXfixes, cleanup dependency list (white space)]
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xlib_libXi/xlib_libXi.mk
+++ package/x11r7/xlib_libXi/xlib_libXi.mk
@@ -4,13 +4,20 @@
 #
 ################################################################################
 
-XLIB_LIBXI_VERSION = 1.6.1
+XLIB_LIBXI_VERSION = 1.7.2
 XLIB_LIBXI_SOURCE = libXi-$(XLIB_LIBXI_VERSION).tar.bz2
 XLIB_LIBXI_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXI_LICENSE = MIT
 XLIB_LIBXI_LICENSE_FILES = COPYING
 XLIB_LIBXI_INSTALL_STAGING = YES
-XLIB_LIBXI_DEPENDENCIES = xproto_inputproto xlib_libX11 xlib_libXext xproto_xproto
+XLIB_LIBXI_DEPENDENCIES = \
+	host-pkgconf \
+	xproto_inputproto \
+	xproto_xproto \
+	xlib_libX11 \
+	xlib_libXext \
+	xlib_libXfixes
+
 XLIB_LIBXI_CONF_OPT = --disable-malloc0returnsnull
 
 $(eval $(autotools-package))
