6796e2ee4c7ac1e310f674eedf19198553fab56a
Bernd Kuhls <berndkuhls@hotmail.com>
2014-01-04 14:38:38 +0100

    xorg: Bump xlib_libSM version to 1.2.2
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xlib_libSM/xlib_libSM.mk
+++ package/x11r7/xlib_libSM/xlib_libSM.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBSM_VERSION = 1.2.1
+XLIB_LIBSM_VERSION = 1.2.2
 XLIB_LIBSM_SOURCE = libSM-$(XLIB_LIBSM_VERSION).tar.bz2
 XLIB_LIBSM_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBSM_LICENSE = MIT
