28b3775428455c5fe6a753cb6add250928021106
Bernd Kuhls <berndkuhls@hotmail.com>
2014-01-04 14:38:59 +0100

    xorg: Bump xproto_randrproto version to 1.4.0
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xproto_randrproto/xproto_randrproto.mk
+++ package/x11r7/xproto_randrproto/xproto_randrproto.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XPROTO_RANDRPROTO_VERSION = 1.3.2
+XPROTO_RANDRPROTO_VERSION = 1.4.0
 XPROTO_RANDRPROTO_SOURCE = randrproto-$(XPROTO_RANDRPROTO_VERSION).tar.bz2
 XPROTO_RANDRPROTO_SITE = http://xorg.freedesktop.org/releases/individual/proto
 XPROTO_RANDRPROTO_LICENSE = MIT
