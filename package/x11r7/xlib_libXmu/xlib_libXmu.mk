743a23458d40008dcf86cd98c1d094b3cb1c3017
Bernd Kuhls <berndkuhls@hotmail.com>
2014-01-04 14:38:48 +0100

    xorg: Bump xlib_libXmu version to 1.1.2
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xlib_libXmu/xlib_libXmu.mk
+++ package/x11r7/xlib_libXmu/xlib_libXmu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBXMU_VERSION = 1.1.1
+XLIB_LIBXMU_VERSION = 1.1.2
 XLIB_LIBXMU_SOURCE = libXmu-$(XLIB_LIBXMU_VERSION).tar.bz2
 XLIB_LIBXMU_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXMU_LICENSE = MIT
