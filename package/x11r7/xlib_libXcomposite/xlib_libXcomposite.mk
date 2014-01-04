11851a59a1feb375e6f558cafe49f5664e4d53ec
Bernd Kuhls <berndkuhls@hotmail.com>
2014-01-04 14:38:41 +0100

    xorg: Bump xlib_libXcomposite version to 0.4.4
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xlib_libXcomposite/xlib_libXcomposite.mk
+++ package/x11r7/xlib_libXcomposite/xlib_libXcomposite.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBXCOMPOSITE_VERSION = 0.4.3
+XLIB_LIBXCOMPOSITE_VERSION = 0.4.4
 XLIB_LIBXCOMPOSITE_SOURCE = libXcomposite-$(XLIB_LIBXCOMPOSITE_VERSION).tar.bz2
 XLIB_LIBXCOMPOSITE_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXCOMPOSITE_LICENSE = MIT
