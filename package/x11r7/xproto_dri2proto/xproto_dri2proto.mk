ec007ab751126796cf6ddd0ddcf25a070f697d5a
Bernd Kuhls <berndkuhls@hotmail.com>
2014-01-04 14:38:56 +0100

    xorg: Bump xproto_dri2proto version to 2.8
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xproto_dri2proto/xproto_dri2proto.mk
+++ package/x11r7/xproto_dri2proto/xproto_dri2proto.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XPROTO_DRI2PROTO_VERSION = 2.6
+XPROTO_DRI2PROTO_VERSION = 2.8
 XPROTO_DRI2PROTO_SOURCE = dri2proto-$(XPROTO_DRI2PROTO_VERSION).tar.bz2
 XPROTO_DRI2PROTO_SITE = http://xorg.freedesktop.org/releases/individual/proto
 XPROTO_DRI2PROTO_LICENSE = MIT
