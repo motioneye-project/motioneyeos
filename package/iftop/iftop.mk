bdef700af433c4591576369274e185298c4cb0b0
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-01-04 09:11:44 -0300

    iftop: bump to version 1.0pre3
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/iftop/iftop.mk
+++ package/iftop/iftop.mk
@@ -4,11 +4,9 @@
 #
 ################################################################################
 
-IFTOP_VERSION = 1.0pre2
+IFTOP_VERSION = 1.0pre3
 IFTOP_SITE = http://www.ex-parrot.com/pdw/iftop/download/
-
 IFTOP_DEPENDENCIES = ncurses libpcap
-
 IFTOP_LICENSE = GPLv2+
 IFTOP_LICENSE_FILES = COPYING
 
