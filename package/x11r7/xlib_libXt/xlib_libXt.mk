9da193de740405b18af8ac61757cefb37d304a18
Bernd Kuhls <berndkuhls@hotmail.com>
2014-01-04 14:38:52 +0100

    xorg: Bump xlib_libXt version to 1.1.4
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xlib_libXt/xlib_libXt.mk
+++ package/x11r7/xlib_libXt/xlib_libXt.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBXT_VERSION = 1.1.3
+XLIB_LIBXT_VERSION = 1.1.4
 XLIB_LIBXT_SOURCE = libXt-$(XLIB_LIBXT_VERSION).tar.bz2
 XLIB_LIBXT_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXT_LICENSE = MIT
