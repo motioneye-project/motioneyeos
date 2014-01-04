132efcd2c80b8ac8b8cc4e6ddd5b31967553b10c
Bernd Kuhls <berndkuhls@hotmail.com>
2014-01-04 14:38:42 +0100

    xorg: Bump xlib_libXdamage version to 1.1.4
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xlib_libXdamage/xlib_libXdamage.mk
+++ package/x11r7/xlib_libXdamage/xlib_libXdamage.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBXDAMAGE_VERSION = 1.1.3
+XLIB_LIBXDAMAGE_VERSION = 1.1.4
 XLIB_LIBXDAMAGE_SOURCE = libXdamage-$(XLIB_LIBXDAMAGE_VERSION).tar.bz2
 XLIB_LIBXDAMAGE_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXDAMAGE_LICENSE = MIT
