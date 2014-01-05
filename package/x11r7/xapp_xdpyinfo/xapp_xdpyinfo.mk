a97b5add2e10c019b23fb331360dcac71f7074d4
Peter Korsgaard <peter@korsgaard.com>
2014-01-05 21:50:02 +0100

    xapp_xdpyinfo: fix dependencies
    Most of the dependencies listed were optional or only indirectly needed
    (E.G. the xproto ones), so don't select them.
    
    Further more, the optional xcomposite and xinerama dependencies were missing.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xapp_xdpyinfo/xapp_xdpyinfo.mk
+++ package/x11r7/xapp_xdpyinfo/xapp_xdpyinfo.mk
@@ -9,6 +9,29 @@ XAPP_XDPYINFO_SOURCE = xdpyinfo-$(XAPP_XDPYINFO_VERSION).tar.bz2
 XAPP_XDPYINFO_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XDPYINFO_LICENSE = MIT
 XAPP_XDPYINFO_LICENSE_FILES = COPYING
-XAPP_XDPYINFO_DEPENDENCIES = xlib_libX11 xlib_libXext xlib_libXi xlib_libXrender xlib_libXtst xlib_libXxf86dga xlib_libXxf86vm xproto_inputproto xproto_kbproto xproto_renderproto xproto_xf86dgaproto xproto_xf86vidmodeproto xlib_libdmx
+XAPP_XDPYINFO_CONF_OPT = --without-xf86misc # not in BR
+XAPP_XDPYINFO_DEPENDENCIES = xlib_libX11 xlib_libXext xlib_libXtst \
+	$(if $(BR2_PACKAGE_XLIB_LIBXI),xlib_libXi) \
+	$(if $(BR2_PACKAGE_XLIB_LIBXRENDER),xlib_libXrender) \
+	$(if $(BR2_PACKAGE_XLIB_LIBXCOMPOSITE),xlib_libXcomposite) \
+	$(if $(BR2_PACKAGE_XLIB_LIBXXF86VM),xlib_libXxf86vm)
+
+ifeq ($(BR2_PACKAGE_XLIB_LIBXXF86DGA),y)
+XAPP_XDPYINFO_DEPENDENCIES += xlib_libXxf86dga
+else
+XAPP_XDPYINFO_CONF_OPT += --without-dga
+endif
+
+ifeq ($(BR2_PACKAGE_XLIB_LIBDMX),y)
+XAPP_XDPYINFO_DEPENDENCIES += xlib_libdmx
+else
+XAPP_XDPYINFO_CONF_OPT += --without-dmx
+endif
+
+ifeq ($(BR2_PACKAGE_XLIB_LIBXINERAMA),y)
+XAPP_XDPYINFO_DEPENDENCIES += xlib_libXinerama
+else
+XAPP_XDPYINFO_CONF_OPT += --without-xinerama
+endif
 
 $(eval $(autotools-package))
