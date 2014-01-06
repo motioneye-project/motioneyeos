88adfadccc7b1ad3b009e3d927e1acbb937ee801
Bernd Kuhls <berndkuhls@hotmail.com>
2014-01-06 21:40:06 +0100

    xorg: Bump xlib_libXcursor to version 1.1.14
    thanks to Jerzy Grzegorek for the hint
    
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xlib_libXcursor/xlib_libXcursor.mk
+++ package/x11r7/xlib_libXcursor/xlib_libXcursor.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBXCURSOR_VERSION = 1.1.13
+XLIB_LIBXCURSOR_VERSION = 1.1.14
 XLIB_LIBXCURSOR_SOURCE = libXcursor-$(XLIB_LIBXCURSOR_VERSION).tar.bz2
 XLIB_LIBXCURSOR_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXCURSOR_LICENSE = MIT
