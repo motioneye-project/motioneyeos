3ed7b4921e8f349e6b12d6e8f74c971f2d001f85
Julien Boibessot <julien.boibessot@armadeus.com>
2014-01-08 14:07:36 +0100

    Add LTris package (SDL game)
    Signed-off-by: Julien Boibessot <julien.boibessot@armadeus.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/ltris/ltris.mk
@@ -0,0 +1,24 @@
+#############################################################
+#
+# ltris
+#
+#############################################################
+
+LTRIS_SITE = http://downloads.sourceforge.net/lgames/ltris/
+LTRIS_VERSION = 1.0.19
+LTRIS_LICENSE = GPLv2+
+LTRIS_LICENSE_FILES = COPYING
+
+LTRIS_DEPENDENCIES = sdl
+
+LTRIS_CONF_ENV = \
+	SDL_CONFIG="$(STAGING_DIR)/usr/bin/sdl-config"
+
+ifeq ($(BR2_PACKAGE_LTRIS_AUDIO),y)
+LTRIS_DEPENDENCIES += sdl_mixer
+LTRIS_CONF_OPT += --enable-audio=yes
+else
+LTRIS_CONF_OPT += --disable-audio
+endif
+
+$(eval $(autotools-package))
