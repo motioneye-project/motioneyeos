db1e4fb89113c00867fae968b09bf7552cace6c8
Naumann Andreas <ANaumann@ultratronik.de>
2014-01-08 17:05:08 +0100

    evemu: new package
    [Peter: don't try to generate manpages even if host has asciidoc]
    Signed-off-by: Andreas Naumann <anaumann@ultratronik.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/evemu/evemu.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# evemu
+#
+################################################################################
+
+EVEMU_VERSION = 1.0.10
+EVEMU_SITE = http://cgit.freedesktop.org/evemu/snapshot
+EVEMU_LICENSE = LGPLv3 (library), GPLv3 (tools)
+EVEMU_LICENSE_FILES = COPYING COPYING.GPL3
+
+# asciidoc used to generate manpages, which we don't need, and if it's
+# present on the build host, it ends getting called with our host-python
+# which doesn't have all the needed modules enabled, breaking the build
+EVEMU_CONF_ENV = ac_cv_path_ASCIIDOC=""
+
+# Uses PKG_CHECK_MODULES() in configure.ac
+EVEMU_DEPENDENCIES = host-pkgconf libevdev
+
+# Needs Python to generate a wrapper
+EVEMU_DEPENDENCIES += host-python
+
+# package source code coming from git, so it doesn't have generated
+# configure and Makefile.in
+EVEMU_AUTORECONF = YES
+
+$(eval $(autotools-package))
