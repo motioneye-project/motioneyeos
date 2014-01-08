bf93f6a2e2f22f10e7257c0e987a2c319df1519a
Simon Dawson <spdawson@gmail.com>
2014-01-08 08:47:54 +0000

    python-nfc: bump version
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-nfc/python-nfc.mk
+++ package/python-nfc/python-nfc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_NFC_VERSION = 142
+PYTHON_NFC_VERSION = 212
 PYTHON_NFC_SITE = https://launchpad.net/nfcpy
 PYTHON_NFC_SITE_METHOD = bzr
 PYTHON_NFC_DEPENDENCIES = libusb libusb-compat
