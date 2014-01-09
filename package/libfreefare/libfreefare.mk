712ebe85799287c3964aeab62982013b15076f92
Peter Korsgaard <peter@korsgaard.com>
2014-01-09 11:23:56 +0100

    libfreefare: examples need to link with zlib for openssl when static linking
    Fixes http://autobuild.buildroot.net/results/f09/f09d515f0e7b6d78010f1ce624336ceaef624989/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libfreefare/libfreefare.mk
+++ package/libfreefare/libfreefare.mk
@@ -8,4 +8,9 @@ LIBFREEFARE_VERSION = 0.3.4
 LIBFREEFARE_SITE = http://libfreefare.googlecode.com/files
 LIBFREEFARE_DEPENDENCIES = libnfc openssl
 
+ifeq ($(BR2_PREFER_STATIC_LIB),y)
+# openssl needs zlib even if the libfreefare example itself doesn't
+LIBFREEFARE_CONF_ENV += LIBS='-lz'
+endif
+
 $(eval $(autotools-package))
