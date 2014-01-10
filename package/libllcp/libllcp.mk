8b2e235743260872bbd7753a41f8cf56a628260e
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2014-01-10 10:06:19 +0000

    libnfc-llcp: rename to libllcp and bump version
    This package has been renamed upstream to libllcp. Also, the current
    package we have in Buildroot fails to compile due to a version bump of
    it's main dependence, libnfc. A version bump is required because this
    package has been adapted upstream to work with libnfc-1.7.0-rc6.
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/libllcp/libllcp.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# libllcp
+#
+################################################################################
+
+LIBLLCP_VERSION = cf0c4b3c9df98851c6092c130192130c3f5a46bd
+LIBLLCP_SITE = https://libllcp.googlecode.com/git/
+LIBLLCP_SITE_METHOD = git
+LIBLLCP_DEPENDENCIES = libnfc
+# There's no ./configure in the repository, so we need to autoreconf
+LIBLLCP_AUTORECONF = YES
+LIBLLCP_INSTALL_STAGING = YES
+LIBLLCP_LICENSE = GPLv3+
+LIBLLCP_LICENSE_FILES = COPYING
+
+$(eval $(autotools-package))
