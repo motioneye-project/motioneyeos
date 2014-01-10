755bed66356d81974fdbd25ff15e80d270a45920
Clayton Shotwell <clshotwe@rockwellcollins.com>
2014-01-10 07:38:43 -0600

    sdl: Fix missing autoconf
    Fixing an issue seen on the autobuilders with sdl failing to run the
    autogen.sh script. Several of the autobuild issues are listed below.
    
    http://autobuild.buildroot.net/results/7326631048bc9e15ffadc6f541f2235592b5259b/
    http://autobuild.buildroot.net/results/0adf5ea64075323eba1183f93c16d2954673f3e1/
    
    Signed-off-by: Clayton Shotwell <clshotwe@rockwellcollins.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sdl/sdl.mk
+++ package/sdl/sdl.mk
@@ -14,7 +14,7 @@ SDL_INSTALL_STAGING = YES
 # we're patching configure.in, but package cannot autoreconf with our version of
 # autotools, so we have to do it manually instead of setting SD_AUTORECONF = YES
 define SDL_RUN_AUTOGEN
-	cd $(@D) && ./autogen.sh
+	cd $(@D) && PATH=$(HOST_PATH) ./autogen.sh
 endef
 
 SDL_POST_PATCH_HOOKS += SDL_RUN_AUTOGEN
