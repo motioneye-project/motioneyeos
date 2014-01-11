a6c5347844a45a5f61260cf4e1d108c39fc6bf44
Francois Perrad <fperrad@gmail.com>
2014-01-11 16:42:08 +0100

    lua-cjson: restore official name
    (need by LuaRocks)
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    Reviewed-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lua-cjson/lua-cjson.mk
+++ package/lua-cjson/lua-cjson.mk
@@ -1,17 +1,16 @@
 ################################################################################
 #
-# luacjson
+# lua-cjson
 #
 ################################################################################
 
-LUACJSON_VERSION       = 2.1.0
-LUACJSON_SOURCE        = lua-cjson-$(LUACJSON_VERSION).tar.gz
-LUACJSON_SITE          = http://www.kyne.com.au/~mark/software/download
-LUACJSON_DEPENDENCIES  = luainterpreter
-LUACJSON_LICENSE       = MIT
-LUACJSON_LICENSE_FILES = LICENSE
+LUA_CJSON_VERSION       = 2.1.0
+LUA_CJSON_SITE          = http://www.kyne.com.au/~mark/software/download
+LUA_CJSON_DEPENDENCIES  = luainterpreter
+LUA_CJSON_LICENSE       = MIT
+LUA_CJSON_LICENSE_FILES = LICENSE
 
-define LUACJSON_BUILD_CMDS
+define LUA_CJSON_BUILD_CMDS
 	$(MAKE) -C $(@D) \
 		CFLAGS="$(TARGET_CFLAGS)"   \
 		LDFLAGS="$(TARGET_LDFLAGS)" \
@@ -20,7 +19,7 @@ define LUACJSON_BUILD_CMDS
 		PREFIX=$(STAGING_DIR)/usr
 endef
 
-define LUACJSON_INSTALL_TARGET_CMDS
+define LUA_CJSON_INSTALL_TARGET_CMDS
 	$(MAKE) DESTDIR="$(TARGET_DIR)" PREFIX="/usr" -C $(@D) install
 endef
 
