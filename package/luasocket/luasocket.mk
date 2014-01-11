91d8aa63060a3769a1f31e0b7fadfd5439ff5dff
Francois Perrad <fperrad@gmail.com>
2014-01-11 16:42:12 +0100

    luasocket: bump to version 3.0-rc1
    and refactor with luarocks infra
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/luasocket/luasocket.mk
+++ package/luasocket/luasocket.mk
@@ -4,22 +4,9 @@
 #
 ################################################################################
 
-LUASOCKET_VERSION = 2.0.2
-LUASOCKET_SITE = http://luaforge.net/frs/download.php/2664
-LUASOCKET_DEPENDENCIES = luainterpreter
+LUASOCKET_VERSION = 3.0rc1-1
+LUASOCKET_SUBDIR  = luasocket-3.0-rc1
 LUASOCKET_LICENSE = MIT
-LUASOCKET_LICENSE_FILES = LICENSE
+LUASOCKET_LICENSE_FILES = $(LUASOCKET_SUBDIR)/LICENSE
 
-define LUASOCKET_BUILD_CMDS
-	$(MAKE) -C $(@D) -f makefile \
-		CC="$(TARGET_CC)" LD="$(TARGET_CC)" \
-		CFLAGS="$(TARGET_CFLAGS) -fPIC"
-endef
-
-define LUASOCKET_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(@D) -f makefile \
-		INSTALL_TOP_SHARE="$(TARGET_DIR)/usr/share/lua/5.1" \
-		INSTALL_TOP_LIB="$(TARGET_DIR)/usr/lib/lua/5.1" install
-endef
-
-$(eval $(generic-package))
+$(eval $(luarocks-package))
