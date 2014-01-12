57fc5df9e9accde40e034ae788ebf5d03556cbbe
Axel Lin <axel.lin@ingics.com>
2014-01-12 21:34:34 +0800

    zsh: bump to version 5.0.5
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/zsh/zsh.mk
+++ package/zsh/zsh.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ZSH_VERSION=5.0.2
+ZSH_VERSION=5.0.5
 ZSH_SITE=http://downloads.sourceforge.net/project/zsh/zsh/$(ZSH_VERSION)
 ZSH_LICENSE = MIT-like
 ZSH_LICENSE_FILES = LICENCE
