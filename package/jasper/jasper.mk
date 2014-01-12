9f596dbdf5fcabed82b6078446da686def7fde2f
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-01-12 18:53:15 +0100

    jasper: new package
    JPEG-2000 decoder.
    This package was originally found at : https://github.com/huceke/buildroot-rbp
    By gimli <ebsi4711@gmail.com>
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/jasper/jasper.mk
@@ -0,0 +1,15 @@
+#############################################################
+#
+# jasper
+#
+#############################################################
+
+JASPER_VERSION = 1.900.1
+JASPER_SITE = http://sources.openelec.tv/devel
+JASPER_SOURCE = jasper-$(JASPER_VERSION).tar.bz2
+JASPER_INSTALL_STAGING = YES
+JASPER_DEPENDENCIES = jpeg
+JASPER_LICENSE = MIT
+JASPER_LICENSE_FILE = LICENSE
+
+$(eval $(autotools-package))
