6ea49a23df3fa950e253bf704188e87a583a8ba4
Bernd Kuhls <berndkuhls@hotmail.com>
2014-01-16 20:48:04 +0100

    gst-ffmpeg: Use internal libav instead of external ffmpeg
    External ffmpeg 1.2.4 is incompatible, gst-ffmpeg suggests
    to always use internal libav.
    
    Adding one patch:
    Fixes compiling with gcc-4.7: gst-ffmpeg-0.10.13_gcc47.patch (taken from Slackware)
    
    Fixes
    http://autobuild.buildroot.net/results/ab8/ab8ec4904020ceb848a1e50f1865f594f46b8295/
    http://autobuild.buildroot.net/results/f0a/f0a420015a92ebb8868c41c0dbf82cf27b2bdd0f/
    
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gstreamer/gst-ffmpeg/gst-ffmpeg.mk
+++ package/gstreamer/gst-ffmpeg/gst-ffmpeg.mk
@@ -8,11 +8,74 @@ GST_FFMPEG_VERSION = 0.10.13
 GST_FFMPEG_SOURCE = gst-ffmpeg-$(GST_FFMPEG_VERSION).tar.bz2
 GST_FFMPEG_SITE = http://gstreamer.freedesktop.org/src/gst-ffmpeg
 GST_FFMPEG_INSTALL_STAGING = YES
-GST_FFMPEG_DEPENDENCIES = host-pkgconf gstreamer gst-plugins-base ffmpeg
-GST_FFMPEG_CONF_OPT = --with-system-ffmpeg
+GST_FFMPEG_DEPENDENCIES = host-pkgconf gstreamer gst-plugins-base
+
+GST_FFMPEG_CONF_EXTRA_OPT = \
+		--cross-prefix=$(TARGET_CROSS) \
+		--target-os=linux
+
+ifeq ($(BR2_PACKAGE_ZLIB),y)
+GST_FFMPEG_CONF_EXTRA_OPT += --enable-zlib
+GST_FFMPEG_DEPENDENCIES += zlib
+else
+GST_FFMPEG_CONF_EXTRA_OPT += --disable-zlib
+endif
 
 ifeq ($(BR2_PACKAGE_BZIP2),y)
+GST_FFMPEG_CONF_EXTRA_OPT += --enable-bzlib
 GST_FFMPEG_DEPENDENCIES += bzip2
+else
+GST_FFMPEG_CONF_EXTRA_OPT += --disable-bzlib
 endif
+
+ifeq ($(BR2_X86_CPU_HAS_MMX),y)
+GST_FFMPEG_CONF_EXTRA_OPT += --enable-yasm
+GST_FFMPEG_DEPENDENCIES += host-yasm
+else
+GST_FFMPEG_CONF_EXTRA_OPT += --disable-yasm
+GST_FFMPEG_CONF_EXTRA_OPT += --disable-mmx
+endif
+
+ifeq ($(BR2_X86_CPU_HAS_SSE),y)
+GST_FFMPEG_CONF_EXTRA_OPT += --enable-sse
+else
+GST_FFMPEG_CONF_EXTRA_OPT += --disable-sse
+endif
+
+ifeq ($(BR2_X86_CPU_HAS_SSSE3),y)
+GST_FFMPEG_CONF_EXTRA_OPT += --enable-ssse3
+else
+GST_FFMPEG_CONF_EXTRA_OPT += --disable-ssse3
+endif
+
+# Explicitly disable everything that doesn't match for ARM
+# FFMPEG "autodetects" by compiling an extended instruction via AS
+# This works on compilers that aren't built for generic by default
+ifeq ($(BR2_arm7tdmi)$(BR2_arm720t)$(BR2_arm920t)$(BR2_arm922t)$(BR2_strongarm)$(BR2_fa526),y)
+GST_FFMPEG_CONF_EXTRA_OPT += --disable-armv5te
+endif
+ifeq ($(BR2_arm1136jf_s)$(BR2_arm1176jz_s)$(BR2_arm1176jzf_s),y)
+GST_FFMPEG_CONF_EXTRA_OPT += --enable-armv6
+else
+GST_FFMPEG_CONF_EXTRA_OPT += --disable-armv6 --disable-armv6t2
+endif
+ifeq ($(BR2_ARM_CPU_HAS_NEON),y)
+GST_FFMPEG_CONF_EXTRA_OPT += --enable-neon
+endif
+
+# Set powerpc altivec appropriately
+ifeq ($(BR2_powerpc),y)
+ifeq ($(BR2_powerpc_7400)$(BR2_powerpc_7450)$(BR2_powerpc_970),y)
+GST_FFMPEG_CONF_EXTRA_OPT += --enable-altivec
+else
+GST_FFMPEG_CONF_EXTRA_OPT += --disable-altivec
+endif
+endif
+
+ifeq ($(BR2_PREFER_STATIC_LIB),)
+GST_FFMPEG_CONF_EXTRA_OPT += --enable-pic
+endif
+
+GST_FFMPEG_CONF_OPT = --with-ffmpeg-extra-configure="$(GST_FFMPEG_CONF_EXTRA_OPT)"
 
 $(eval $(autotools-package))
