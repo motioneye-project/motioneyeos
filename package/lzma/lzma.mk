0a07731933bded233b840e71770c01118b3a709c
Thomas De Schampheleire <patrickdepinguin@gmail.com>
2014-01-21 08:54:12 +0100

    lzma: remove deprecated target package
    The on-target lzma package has been deprecated for a long time, so remove
    it. This does not remove the support for generating lzma-compressed rootfs
    images.
    
    Signed-off-by: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lzma/lzma.mk
+++ package/lzma/lzma.mk
@@ -7,10 +7,8 @@
 LZMA_VERSION = 4.32.7
 LZMA_SOURCE = lzma-$(LZMA_VERSION).tar.xz
 LZMA_SITE = http://tukaani.org/lzma/
-LZMA_INSTALL_STAGING = YES
 LZMA_CONF_OPT = $(if $(BR2_ENABLE_DEBUG),--enable-debug,--disable-debug)
 
-$(eval $(autotools-package))
 $(eval $(host-autotools-package))
 
 LZMA = $(HOST_DIR)/usr/bin/lzma
