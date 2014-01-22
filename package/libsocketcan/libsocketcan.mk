f8ba83fd21e4bda9cab34777bda6eb0a312b8f3c
Yegor Yefremov <yegorslists@googlemail.com>
2014-01-22 08:32:37 +0100

    libsocketcan: install to staging
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libsocketcan/libsocketcan.mk
+++ package/libsocketcan/libsocketcan.mk
@@ -6,5 +6,6 @@
 LIBSOCKETCAN_VERSION = 0.0.9
 LIBSOCKETCAN_SITE = http://www.pengutronix.de/software/libsocketcan/download/
 LIBSOCKETCAN_SOURCE = libsocketcan-$(LIBSOCKETCAN_VERSION).tar.bz2
+LIBSOCKETCAN_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
