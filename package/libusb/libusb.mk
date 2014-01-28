f92aa38db31f3d741e22136f929c3135f034037e
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-01-28 17:11:35 -0300

    libusb: bump to version 1.0.18
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libusb/libusb.mk
+++ package/libusb/libusb.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBUSB_VERSION_MAJOR = 1.0
-LIBUSB_VERSION = $(LIBUSB_VERSION_MAJOR).9
+LIBUSB_VERSION = $(LIBUSB_VERSION_MAJOR).18
 LIBUSB_SOURCE = libusb-$(LIBUSB_VERSION).tar.bz2
 LIBUSB_SITE = http://downloads.sourceforge.net/project/libusb/libusb-$(LIBUSB_VERSION_MAJOR)/libusb-$(LIBUSB_VERSION)
 LIBUSB_LICENSE = LGPLv2.1+
@@ -15,6 +15,12 @@ LIBUSB_INSTALL_STAGING = YES
 
 ifeq ($(BR2_avr32),y)
 LIBUSB_CONF_OPT += --disable-timerfd
+endif
+
+ifeq ($(BR2_ROOTFS_DEVICE_CREATION_DYNAMIC_UDEV),y)
+LIBUSB_DEPENDENCIES += udev
+else
+LIBUSB_CONF_OPT += --disable-udev
 endif
 
 $(eval $(autotools-package))
