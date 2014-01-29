f6305d135ea422342a5eedce2e268508a6df0968
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-01-29 15:51:57 -0300

    rng-tools: argp-standalone required for uclibc toolchains only
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rng-tools/rng-tools.mk
+++ package/rng-tools/rng-tools.mk
@@ -6,8 +6,10 @@
 
 RNG_TOOLS_VERSION = 4
 RNG_TOOLS_SITE = http://downloads.sourceforge.net/project/gkernel/rng-tools/$(RNG_TOOLS_VERSION)
+ifeq ($(BR2_TOOLCHAIN_USES_UCLIBC),y)
 RNG_TOOLS_CONF_ENV = LIBS="-largp"
 RNG_TOOLS_DEPENDENCIES = argp-standalone
+endif
 RNG_TOOLS_LICENSE = GPLv2
 RNG_TOOLS_LICENSE_FILES = COPYING
 
