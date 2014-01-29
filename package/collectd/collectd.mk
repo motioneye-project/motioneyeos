0586b24a594a044e20905001df5ccf1af5d819d6
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-01-29 10:01:59 -0300

    collectd: bump to version 5.4.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/collectd/collectd.mk
+++ package/collectd/collectd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-COLLECTD_VERSION = 5.4.0
+COLLECTD_VERSION = 5.4.1
 COLLECTD_SITE = http://collectd.org/files
 COLLECTD_MAKE_OPT = LDFLAGS="$(TARGET_LDFLAGS) -lm"
 COLLECTD_CONF_ENV = ac_cv_lib_yajl_yajl_alloc=yes
