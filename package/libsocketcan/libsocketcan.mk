3bc4c064d22298f540830d94aaa87ef99bd3f9a2
Yegor Yefremov <yegorslists@googlemail.com>
2014-01-29 09:36:26 +0100

    libsocketcan: add license information and fix formatting
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libsocketcan/libsocketcan.mk
+++ package/libsocketcan/libsocketcan.mk
@@ -3,9 +3,11 @@
 # libsocketcan
 #
 ################################################################################
+
 LIBSOCKETCAN_VERSION = 0.0.9
 LIBSOCKETCAN_SITE = http://www.pengutronix.de/software/libsocketcan/download/
 LIBSOCKETCAN_SOURCE = libsocketcan-$(LIBSOCKETCAN_VERSION).tar.bz2
 LIBSOCKETCAN_INSTALL_STAGING = YES
+LIBSOCKETCAN_LICENSE = LGPLv2.1+
 
 $(eval $(autotools-package))
