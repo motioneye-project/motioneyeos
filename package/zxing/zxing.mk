95b623963a8c90e6776ba025869ee19bb98480b1
Martin Bark <martin@barkynet.com>
2014-02-01 21:03:01 +0000

    zxing: don't select libiconv
    The zxing package currently selects libiconv when locale support is not
    available in the toolchain.  This patch stops the zxing package selecting
    libiconv.  The zxing package now uses uses libiconv if it's available
    otherwise iconv support is disabled in zxing.
    
    Signed-off-by: Martin Bark <martin@barkynet.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/zxing/zxing.mk
+++ package/zxing/zxing.mk
@@ -12,7 +12,13 @@ ZXING_LICENSE_FILES = COPYING
 ZXING_INSTALL_STAGING = YES
 
 ifneq ($(BR2_ENABLE_LOCALE),y)
+ifeq ($(BR2_PACKAGE_LIBICONV),y)
 ZXING_DEPENDENCIES += libiconv
+else
+# There is no locale support in the toolchain and libiconv
+# is not available so disable iconv support in zxing
+ZING_MAKE_OPT=CXXFLAGS+="-DNO_ICONV"
+endif
 endif
 
 define ZXING_EXTRACT_CMDS
@@ -20,7 +26,7 @@ define ZXING_EXTRACT_CMDS
 endef
 
 define ZXING_BUILD_CMDS
-	$(MAKE) -C $(@D)/cpp/core/src $(TARGET_CONFIGURE_OPTS)
+	$(MAKE) -C $(@D)/cpp/core/src $(TARGET_CONFIGURE_OPTS) $(ZING_MAKE_OPT)
 endef
 
 define ZXING_INSTALL_STAGING_CMDS
