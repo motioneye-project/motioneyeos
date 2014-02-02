f7f39ccc9f17e23b98c68850ae2ff7b8f5de1af0
Yegor Yefremov <yegorslists@googlemail.com>
2014-02-02 22:54:38 +0100

    libsoc: bump to 0.6
    Add --disable-debug to unset default DEBUG option.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libsoc/libsoc.mk
+++ package/libsoc/libsoc.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-LIBSOC_VERSION = 0.5
+LIBSOC_VERSION = 0.6
 LIBSOC_SITE = $(call github,jackmitch,libsoc,$(LIBSOC_VERSION))
 LIBSOC_LICENSE = LGPLv2.1
 LIBSOC_LICENSE_FILES = COPYING
 LIBSOC_AUTORECONF = YES
 LIBSOC_INSTALL_STAGING = YES
+LIBSOC_CONF_OPT = --disable-debug
 
 $(eval $(autotools-package))
