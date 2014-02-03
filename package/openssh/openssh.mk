a93c98bb95d9b2fecfaec7c6347c57bf90e51c31
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-02-02 23:01:28 -0300

    openssh: bump to version 6.5p1
    Convert the ever growing drop-SUSv3-legacy patch to a sed expression.
    Modify the initscript to create ed25519 server key.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENSSH_VERSION = 6.4p1
+OPENSSH_VERSION = 6.5p1
 OPENSSH_SITE = http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 OPENSSH_CONF_ENV = LD="$(TARGET_CC)" LDFLAGS="$(TARGET_CFLAGS)"
 OPENSSH_CONF_OPT = --disable-lastlog --disable-utmp \
@@ -29,5 +29,15 @@ define OPENSSH_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 755 package/openssh/S50sshd \
 		$(TARGET_DIR)/etc/init.d/S50sshd
 endef
+
+# Replace deprecated bcopy/bzero with memset/memcpy
+define OPENSSH_REPLACE_SUSV3_DEPRECATED
+	for src in `find $(@D) -name \*.c`; do \
+		$(SED) "s/bzero(\(.*,\)/memset(\1 0, /" $${src} ;\
+		$(SED) "s/bcopy(\(.*,\) \(.*,\)/memcpy(\2 \1/" $${src} ;\
+	done
+endef
+
+OPENSSH_POST_PATCH_HOOKS += OPENSSH_REPLACE_SUSV3_DEPRECATED
 
 $(eval $(autotools-package))
