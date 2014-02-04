ee8f0ecfbcd0deb2f02a7f2dbd9e94af9ad4d8fe
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-02-04 10:25:57 +0100

    intel-microcode: new package
    The intel microcode is a proprietary package which provides a data file
    used to correct processors errors.
    It was originally sent by Richard Braun <rbraun@sceen.net>
    
    [Peter: set _LICENSE_FILES]
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Cc: Richard Braun <rbraun@sceen.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/intel-microcode/intel-microcode.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# intel-microcode
+#
+################################################################################
+
+INTEL_MICROCODE_VERSION = 20140122
+INTEL_MICROCODE_SOURCE = microcode-$(INTEL_MICROCODE_VERSION).tgz
+INTEL_MICROCODE_SITE = http://downloadmirror.intel.com/23574/eng
+INTEL_MICROCODE_LICENSE = PROPRIETARY
+INTEL_MICROCODE_LICENSE_FILES = license.txt
+INTEL_MICROCODE_REDISTRIBUTE = NO
+
+# N.B. Don't strip any path components during extraction.
+define INTEL_MICROCODE_EXTRACT_CMDS
+	gzip -d -c $(DL_DIR)/$(INTEL_MICROCODE_SOURCE) | \
+	tar --strip-components=0 -C $(@D) -xf -
+	head -n 33 $(@D)/microcode.dat > $(@D)/license.txt
+endef
+
+define INTEL_MICROCODE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/microcode.dat \
+		$(TARGET_DIR)/usr/share/misc/intel-microcode.dat
+endef
+
+$(eval $(generic-package))
