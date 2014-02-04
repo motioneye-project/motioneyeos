de13e31a165268e030018bb6d168637303a65802
Peter Korsgaard <peter@korsgaard.com>
2014-02-04 15:03:06 +0100

    Rename BUILDROOT_LIBC to BR_LIBC
    No functional change, but internal variables should be name BR_foo, not
    BUILDROOT_foo (I think ..).
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- toolchain/toolchain-buildroot/toolchain-buildroot.mk
+++ toolchain/toolchain-buildroot/toolchain-buildroot.mk
@@ -6,7 +6,7 @@
 
 TOOLCHAIN_BUILDROOT_SOURCE =
 
-BUILDROOT_LIBC = $(call qstrip,$(BR2_TOOLCHAIN_BUILDROOT_LIBC))
+BR_LIBC = $(call qstrip,$(BR2_TOOLCHAIN_BUILDROOT_LIBC))
 
 # Triggering the build of the host-gcc-final will automatically do the
 # build of binutils, uClibc, kernel headers and all the intermediate
