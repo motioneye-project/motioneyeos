3e693854b361f8bf18613c2b54fea5a530883bbd
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-02-05 19:12:17 +0100

    libatomic_ops: Update repository and bump version
    Change the download source to a Github repository which is more recent.
    This patch also bump libatomic_ops version and remove a patch that is
    now upstream.
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libatomic_ops/libatomic_ops.mk
+++ package/libatomic_ops/libatomic_ops.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-LIBATOMIC_OPS_VERSION = 7.2
-LIBATOMIC_OPS_SITE = http://www.hpl.hp.com/research/linux/atomic_ops/download
+LIBATOMIC_OPS_VERSION = libatomic_ops-7_4_0
+LIBATOMIC_OPS_SITE = $(call github,ivmai,libatomic_ops,$(LIBATOMIC_OPS_VERSION))
+LIBATOMIC_OPS_AUTORECONF = YES
 
 # From doc/LICENSING.txt: "Our intent is to make it easy to use
 # libatomic_ops, in both free and proprietary software.  Hence most
@@ -14,7 +15,7 @@ LIBATOMIC_OPS_SITE = http://www.hpl.hp.com/research/linux/atomic_ops/download
 # by the GNU General Public License. These are put into a separate
 # library, libatomic_ops_gpl.a."
 LIBATOMIC_OPS_LICENSE = MIT (main library) / GPLv2+ (gpl extension)
-LIBATOMIC_OPS_LICENSE_FILES = doc/LICENSING.txt doc/COPYING
+LIBATOMIC_OPS_LICENSE_FILES = doc/LICENSING.txt COPYING
 
 LIBATOMIC_OPS_INSTALL_STAGING = YES
 
