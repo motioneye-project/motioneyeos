2c66e4429d23c6e293f9789cdef01b760399fccb
eric.le.bihan.dev@free.fr <eric.le.bihan.dev@free.fr>
2014-02-07 14:21:34 +0100

    systemd: bump to v207
    This patch bumps systemd to v207 but also declares it as a provider for the
    udev virtual package.
    
    Starting with systemd 183, udev has been merged into
    systemd. The udev daemon is now installed as /lib/systemd/systemd-udevd.
    This means that /dev management using udev is only available if systemd
    is chosen as init system.
    
    When configuring systemd, the following options are available:
    
    - activation of systemd-journal-gatewayd, to access the journal via
      HTTP.
    - activation of extra features like journal compression and sealing.
    
    Support for uClibc has also been removed because:
    
    - upstream has no interest in supporting uClibc.
    - using a shrinked libc brings no advantage, given the size of all the
      programs included in Systemd. So using glibc does not matter.
    
    Signed-off-by: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    Reviewed-by: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/udev/udev.mk
+++ package/udev/udev.mk
@@ -10,6 +10,10 @@ ifeq ($(BR2_PACKAGE_EUDEV),y)
 UDEV_DEPENDENCIES += eudev
 endif
 
+ifeq ($(BR2_PACKAGE_SYSTEMD),y)
+UDEV_DEPENDENCIES += systemd
+endif
+
 ifeq ($(UDEV_DEPENDENCIES),)
 define UDEV_CONFIGURE_CMDS
 	echo "No Udev implementation selected. Configuration error."
