35cb43b9d82d9bc1e90a2ed23d21fab357acd873
Julien Boibessot <julien.boibessot@armadeus.com>
2014-02-10 11:17:48 +0100

    madplay: fixes build when toolchain has locale support
    Otherwise build fails in intl/relocatable.c with:
    ./relocatable.c: In function 'libintl_relocate':
    ./relocatable.c:402:40: error: 'INSTALLPREFIX' undeclared (first use in this function)
    ./relocatable.c:402:40: note: each undeclared identifier is reported only once for each function it appears in
    
    [Peter: drop redundant 'else' part]
    Signed-off-by: Julien Boibessot <julien.boibessot@armadeus.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/madplay/madplay.mk
+++ package/madplay/madplay.mk
@@ -9,7 +9,7 @@ MADPLAY_SITE = http://downloads.sourceforge.net/project/mad/madplay/$(MADPLAY_VE
 MADPLAY_LICENSE = GPLv2+
 MADPLAY_LICENSE_FILES = COPYING COPYRIGHT
 MADPLAY_LIBTOOL_PATCH = NO
-MADPLAY_DEPENDENCIES = libmad libid3tag
+MADPLAY_DEPENDENCIES = libmad libid3tag $(if $(BR2_PACKAGE_GETTEXT),gettext)
 
 # Check if ALSA is built, then we should configure after alsa-lib so
 # ./configure can find alsa-lib.
