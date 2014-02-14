afebed35a19b587729631f5cf922456a34bfb33c
Peter Kümmel <syntheticpp@gmx.net>
2014-02-14 16:22:06 +0100

    Qt5: update to 5.2.1
    Removed patches are now part of the 5.2.1 release.
    
    Signed-off-by: Peter Kümmel <syntheticpp@gmx.net>
    Acked-by: Fatih Aşıcı <fatih.asici@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/qt5/qt5.mk
+++ package/qt5/qt5.mk
@@ -1,5 +1,5 @@
 QT5_VERSION_MAJOR = 5.2
-QT5_VERSION = $(QT5_VERSION_MAJOR).0
+QT5_VERSION = $(QT5_VERSION_MAJOR).1
 QT5_SITE = http://download.qt-project.org/official_releases/qt/$(QT5_VERSION_MAJOR)/$(QT5_VERSION)/submodules/
 include $(sort $(wildcard package/qt5/*/*.mk))
 
