5ddfe76889ea61195c34c2719decbc69943ae848
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-02-22 22:12:00 +0100

    libcgi: fix build failure on pure static library builds
    libcgi always builds both the shared and static library, which doesn't
    work on architecture that don't support shared library at all, such as
    Blackfin with the FLAT format. libcgi uses autoconf, but not automake,
    and the Makefile is not of the highest possible quality, so this
    commit fixes the problem by introducing a "STATIC" variable that can
    be set from the environment. When set to a non-empty value, the
    Makefile assumes it should only build the static version of the
    library.
    
    Note that this package is in desperate need of some care: there is one
    single patch that mixes several changes together, it doesn't have a
    description or a Signed-off-by line, and there is now a github project
    for libcgi at https://github.com/rafaelsteil/libcgi/ that has the same
    fixes.
    
    However, for the purpose of the master branch, we're doing the most
    minimal fix possible, by just adding this STATIC variable.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/625/625105bcaf26345f422b225787fc19611b65cd57/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libcgi/libcgi.mk
+++ package/libcgi/libcgi.mk
@@ -8,7 +8,8 @@ LIBCGI_VERSION = 1.0
 LIBCGI_SITE = http://downloads.sourceforge.net/project/libcgi/libcgi/$(LIBCGI_VERSION)
 LIBCGI_INSTALL_STAGING = YES
 # use cross CC/AR rather than host
-LIBCGI_MAKE_ENV = CC="$(TARGET_CC) $(TARGET_CFLAGS)" AR="$(TARGET_AR)"
+LIBCGI_MAKE_ENV = CC="$(TARGET_CC) $(TARGET_CFLAGS)" AR="$(TARGET_AR)" \
+	$(if $(BR2_PREFER_STATIC_LIB),STATIC=1)
 LIBCGI_LICENSE = LGPLv2.1+
 
 $(eval $(autotools-package))
