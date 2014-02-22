0a2850104bd1b3e9e7011e8dd7767eff158b835e
Ryan Barnett <ryanbarnett3@gmail.com>
2014-02-22 22:29:40 +0100

    sawman: add autoreconf step
    sawman needs the autoreconf step added in order for Vicente Olivert
    Riera's patch - "sawman: Link using gcc instead of ld"
    (8fc5ea004386431eaecfea51376fcc11c23c77c1) - to take effect.
    
    Fixes:
      http://autobuild.buildroot.net/results/19b/19bfe0d39e457fd260b73928331721f57555fc84
    
    Signed-off-by: Ryan Barnett <rjbarnet@rockwellcollins.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sawman/sawman.mk
+++ package/sawman/sawman.mk
@@ -8,6 +8,7 @@ SAWMAN_VERSION = 1.6.3
 SAWMAN_SOURCE = SaWMan-$(SAWMAN_VERSION).tar.gz
 SAWMAN_SITE = http://www.directfb.org/downloads/Extras
 SAWMAN_INSTALL_STAGING = YES
+SAWMAN_AUTORECONF = YES
 SAWMAN_DEPENDENCIES = directfb
 
 $(eval $(autotools-package))
