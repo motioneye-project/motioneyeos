8dbab113a07ff8b3223d0c0ea24f85effbc7cc61
Francois Perrad <fperrad@gmail.com>
2014-02-23 15:17:17 +0100

    host-perl-xml-parser: rename and refactor with perl infrastructure
    Even though libxml-parser-perl had a Config.in file with an option to
    enable it on the target, this option was hidden by a dependency on
    BR2_HOST_ONLY. So in practice, it was not possible to enable
    libxml-parser-perl on the target. This allows us to rename
    libxml-parser-perl to perl-xml-parser to follow the new naming
    convention of Perl packages, without having to introduce
    Config.in.legacy material.
    
    In addition to this rename, the package is converted to use the newly
    introduced Perl package infrastructure.
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Tested-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Reviewed-by: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/intltool/intltool.mk
+++ package/intltool/intltool.mk
@@ -9,7 +9,7 @@ INTLTOOL_SITE = https://launchpad.net/intltool/trunk/$(INTLTOOL_VERSION)/+downlo
 INTLTOOL_LICENSE = GPLv2+
 INTLTOOL_LICENSE_FILES = COPYING
 
-HOST_INTLTOOL_DEPENDENCIES = host-gettext host-libxml-parser-perl
+HOST_INTLTOOL_DEPENDENCIES = host-gettext host-perl-xml-parser
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
