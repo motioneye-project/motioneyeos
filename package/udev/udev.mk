5e35c1bedd55c044ab68f3b37738686a951eb9ce
eric.le.bihan.dev@free.fr <eric.le.bihan.dev@free.fr>
2014-02-24 10:25:41 +0100

    udev: update udev to new virtual package infra.
    The udev virtual package and its current providers (eudev and systemd)
    have been updated to use the latest version of virtual package
    infrastructure.
    
    A provider should now select BR2_PACKAGE_HAS_UDEV and well as set a
    value for BR2_PACKAGE_PROVIDES_UDEV.
    
    Signed-off-by: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/udev/udev.mk
+++ package/udev/udev.mk
@@ -5,20 +5,12 @@
 ################################################################################
 
 UDEV_SOURCE =
+UDEV_DEPENDENCIES = $(call qstrip,$(BR2_PACKAGE_PROVIDES_UDEV))
 
-ifeq ($(BR2_PACKAGE_EUDEV),y)
-UDEV_DEPENDENCIES += eudev
-endif
-
-ifeq ($(BR2_PACKAGE_SYSTEMD),y)
-UDEV_DEPENDENCIES += systemd
-endif
-
+ifeq ($(BR2_PACKAGE_HAS_UDEV),y)
 ifeq ($(UDEV_DEPENDENCIES),)
-define UDEV_CONFIGURE_CMDS
-	echo "No Udev implementation selected. Configuration error."
-	exit 1
-endef
+$(error No Udev implementation selected. Configuration error)
+endif
 endif
 
 $(eval $(generic-package))
