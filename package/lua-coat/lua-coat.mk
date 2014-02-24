a07d3d7a6413bebf6e70a04efe3e8162473e927a
Francois Perrad <fperrad@gmail.com>
2014-02-24 08:34:53 +0100

    lua-coat: new package
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/lua-coat/lua-coat.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# lua-coat
+#
+################################################################################
+
+LUA_COAT_VERSION_UPSTREAM = 0.9.0
+LUA_COAT_VERSION = $(LUA_COAT_VERSION_UPSTREAM)-1
+LUA_COAT_SUBDIR  = lua-Coat-$(LUA_COAT_VERSION_UPSTREAM)
+LUA_COAT_LICENSE = MIT
+LUA_COAT_LICENSE_FILES = $(LUA_COAT_SUBDIR)/COPYRIGHT
+
+$(eval $(luarocks-package))
