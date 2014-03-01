46416950caac37a16f2c34c77be3ac2b046f266c
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-03-01 16:04:15 -0300

    lzip: new package
    The host variant is in preparation for possibly some gnu packages
    shipping tar.lz versions only.
    At least ed 1.10 did it (still needed to add extraction support for it).
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/lzip/lzip.mk
@@ -0,0 +1,40 @@
+################################################################################
+#
+# lzip
+#
+################################################################################
+
+LZIP_VERSION = 1.15
+LZIP_SITE = http://download.savannah.gnu.org/releases/lzip/
+LZIP_LICENSE = GPLv3+
+LZIP_LICENSE_FILES = COPYING
+
+define LZIP_CONFIGURE_CMDS
+	(cd $(@D); ./configure --prefix=/usr \
+		$(TARGET_CONFIGURE_OPTS) )
+endef
+
+define HOST_LZIP_CONFIGURE_CMDS
+	(cd $(@D); ./configure --prefix=/usr \
+		$(HOST_CONFIGURE_OPTS) )
+endef
+
+define LZIP_BUILD_CMDS
+	$(MAKE) -C $(@D)
+endef
+
+define HOST_LZIP_BUILD_CMDS
+	$(MAKE) -C $(@D)
+endef
+
+define LZIP_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
+endef
+
+define HOST_LZIP_INSTALL_CMDS
+	$(MAKE) -C $(@D) DESTDIR=$(HOST_DIR) install
+endef
+
+# It's not autotools-based
+$(eval $(generic-package))
+$(eval $(host-generic-package))
