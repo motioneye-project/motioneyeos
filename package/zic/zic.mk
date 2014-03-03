5c053979d4e22ec080aecf1911a354616124dcf4
Yann E. MORIN <yann.morin.1998@free.fr>
2014-03-03 23:10:23 +0100

    package/zic: install header
    The incoming host-tzdump package requires the tzfile header,
    so we isntall it.
    
    [Thomas: remove mkdir -p, unneeded because of the use of install -D,
    and change install to $(INSTALL), like we do in all other packages.]
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    [yann.morin.1998@free.fr: split zic header-install to its own cset]
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/zic/zic.mk
+++ package/zic/zic.mk
@@ -20,8 +20,8 @@ define HOST_ZIC_BUILD_CMDS
 endef
 
 define HOST_ZIC_INSTALL_CMDS
-	mkdir -p $(HOST_DIR)/usr/sbin
-	install -D -m 755 $(@D)/zic $(HOST_DIR)/usr/sbin/zic
+	$(INSTALL) -D -m 755 $(@D)/zic $(HOST_DIR)/usr/sbin/zic
+	$(INSTALL) -D -m 644 $(@D)/tzfile.h $(HOST_DIR)/usr/include/tzfile.h
 endef
 
 $(eval $(host-generic-package))
