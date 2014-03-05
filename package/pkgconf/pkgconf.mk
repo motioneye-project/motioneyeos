958c9438ccb5e8c8864edf242b6b4ce3b1f058a4
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2014-03-05 12:19:37 +0000

    host-pkgconf: use --static option for static builds
    Detect when BR2_PREFER_STATIC_LIBS is selected and modify the host
    pkg-config wrapper to append the --static option in that case.
    
    Fixes:
       http://autobuild.buildroot.net/results/161/161446dde7e8e774773eb2b34fd555f5ac22dd02/
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Tested-by: Baruch Siach <baruch@tkos.co.il>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Tested-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/pkgconf/pkgconf.mk
+++ package/pkgconf/pkgconf.mk
@@ -24,8 +24,22 @@ define HOST_PKGCONF_INSTALL_WRAPPER
 		$(HOST_DIR)/usr/bin/pkg-config
 endef
 
+define HOST_PKGCONF_STATIC
+	$(SED) 's,@STATIC@,--static,' $(HOST_DIR)/usr/bin/pkg-config
+endef
+
+define HOST_PKGCONF_SHARED
+	$(SED) 's,@STATIC@,,' $(HOST_DIR)/usr/bin/pkg-config
+endef
+
 PKGCONF_POST_INSTALL_TARGET_HOOKS += PKGCONF_LINK_PKGCONFIG
 HOST_PKGCONF_POST_INSTALL_HOOKS += HOST_PKGCONF_INSTALL_WRAPPER
+
+ifeq ($(BR2_PREFER_STATIC_LIB),y)
+	HOST_PKGCONF_POST_INSTALL_HOOKS += HOST_PKGCONF_STATIC
+else
+	HOST_PKGCONF_POST_INSTALL_HOOKS += HOST_PKGCONF_SHARED
+endif
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
