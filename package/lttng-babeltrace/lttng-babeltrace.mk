7e634a4836c5a2754cf94924baee7082efa9dd49
Samuel Martin <s.martin49@gmail.com>
2014-03-08 15:38:30 +0100

    lttng-babeltrace: bump version to 1.2.1
    Also adjust the license details
    
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lttng-babeltrace/lttng-babeltrace.mk
+++ package/lttng-babeltrace/lttng-babeltrace.mk
@@ -5,10 +5,10 @@
 ################################################################################
 
 LTTNG_BABELTRACE_SITE    = http://lttng.org/files/babeltrace/
-LTTNG_BABELTRACE_VERSION = 1.1.1
+LTTNG_BABELTRACE_VERSION = 1.2.1
 LTTNG_BABELTRACE_SOURCE  = babeltrace-$(LTTNG_BABELTRACE_VERSION).tar.bz2
-LTTNG_BABELTRACE_LICENSE = MIT; LGPLv2 for include/babeltrace/list.h; GPLv3+ for formats/ctf/metadata/ctf-parser.h
-LTTNG_BABELTRACE_LICENSE_FILES = mit-license.txt LICENSE
+LTTNG_BABELTRACE_LICENSE = MIT; LGPL2.1 for include/babeltrace/list.h; GPLv2+ for test code
+LTTNG_BABELTRACE_LICENSE_FILES = mit-license.txt glp-2.0.txt LICENSE
 
 LTTNG_BABELTRACE_DEPENDENCIES = popt util-linux libglib2
 
