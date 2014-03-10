2310c598b094665f854c6847d200b7b5ba398968
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-03-10 17:26:31 -0300

    macchanger: bump to version 1.6.0
    Also switch the homepage, it's not a proper one but at least it's no
    longer a 404.
    The license is now GPLv2+ but there's no COPYING file.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/macchanger/macchanger.mk
+++ package/macchanger/macchanger.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-MACCHANGER_VERSION = 1.5.0
+MACCHANGER_VERSION = 1.6.0
 MACCHANGER_SITE = $(BR2_GNU_MIRROR)/macchanger
-MACCHANGER_LICENSE = GPLv2
-MACCHANGER_LICENSE_FILES = COPYING
+MACCHANGER_LICENSE = GPLv2+
 
 $(eval $(autotools-package))
