cdf2b6f5d9bddf22c7f4609964a1549bfb219019
Yann E. MORIN <yann.morin.1998@free.fr>
2014-03-10 22:24:14 +0100

    package/linux-firmware: bump for new firmwares
    A bunch of new WiFi firmwares has been added, some have been updated.
    Not limited to WiFi, but other firmwares are of less interest to
    Buildroot for now (eg. radeon, myri10ge...).
    
    Fixups in WHENCE file (the all-licenses file).
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/linux-firmware/linux-firmware.mk
+++ package/linux-firmware/linux-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LINUX_FIRMWARE_VERSION = 52d77db9f133afe4d2b26aeccfd603745f45fda1
+LINUX_FIRMWARE_VERSION = f8c22c692bdee57a20b092e647464ff6176df3ed
 LINUX_FIRMWARE_SITE = http://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
 LINUX_FIRMWARE_SITE_METHOD = git
 
