fb3ef246c695b625e1b78f03b28778b237e7a72d
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-03-10 17:26:22 -0300

    fping: bump to version 3.9
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/fping/fping.mk
+++ package/fping/fping.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FPING_VERSION = 3.8
+FPING_VERSION = 3.9
 FPING_SITE = http://fping.org/dist
 FPING_LICENSE = BSD-like
 FPING_LICENSE_FILES = COPYING
