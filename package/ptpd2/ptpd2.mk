7ae48d3b0b92c1fc6039ba06ff858a201c8570cc
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-03-11 21:30:42 -0300

    ptpd2: needs libpcap
    That's what happens when testing a bunch of stuff that uses libpcap.
    Fixes:
    http://autobuild.buildroot.net/results/eaa/eaa615830a6b4cf3da88e71315364ad3e380f681/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ptpd2/ptpd2.mk
+++ package/ptpd2/ptpd2.mk
@@ -7,6 +7,7 @@
 PTPD2_VERSION = 2.3.0
 PTPD2_SITE = http://downloads.sourceforge.net/project/ptpd/ptpd/$(PTPD2_VERSION)
 PTPD2_SOURCE = ptpd-$(PTPD2_VERSION).tar.gz
+PTPD2_DEPENDENCIES = libpcap
 # configure not shipped
 PTPD2_AUTORECONF = YES
 PTPD2_LICENSE = BSD-2c
