5e3ded0d75b00bb212b373a42851db98b79e28c1
Jeremy Kerr <jk@ozlabs.org>
2014-03-12 11:45:57 +0800

    package/iprutils: Add IBM Power RAID utilities
    [Thomas: add patch to allow passing CFLAGS from the environment, use
    TARGET_CONFIGURE_OPTS instead of manually passing CC/LD, add upstream
    URL in Config.in, use the Makefile install target instead of
    open-coding the installation in iprutils.mk.]
    
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/iprutils/iprutils.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# iprutils
+#
+################################################################################
+
+IPRUTILS_VERSION = 2.3.15
+IPRUTILS_SITE = http://downloads.sourceforge.net/project/iprdd/iprutils%20for%202.6%20kernels/$(IPRUTILS_VERSION)
+IPRUTILS_SOURCE = iprutils-$(IPRUTILS_VERSION)-src.tgz
+IPRUTILS_DEPENDENCIES = ncurses libsysfs pciutils
+IPRUTILS_LICENSE = Common Public License Version 1.0
+IPRUTILS_LICENSE_FILES = LICENSE
+
+define IPRUTILS_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
+
+define IPRUTILS_INSTALL_TARGET_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) INSTALL_MOD_PATH=$(TARGET_DIR) -C $(@D) install
+endef
+
+$(eval $(generic-package))
