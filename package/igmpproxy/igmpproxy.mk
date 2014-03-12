c5cb5c3605cc3ce6c54601080aa421ec0c93d06f
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-03-12 18:50:40 -0300

    igmpproxy: uclinux is also linux
    Fixes:
    http://autobuild.buildroot.net/results/3dc/3dcb26947d7eee7ae4f17a082a144912c44f0526/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/igmpproxy/igmpproxy.mk
+++ package/igmpproxy/igmpproxy.mk
@@ -6,6 +6,8 @@
 
 IGMPPROXY_VERSION = 0.1
 IGMPPROXY_SITE = http://downloads.sourceforge.net/project/igmpproxy/igmpproxy/$(IGMPPROXY_VERSION)
+# igmpproxy-01-uclinux.patch
+IGMPPROXY_AUTORECONF = YES
 IGMPPROXY_LICENSE = GPLv2+
 IGMPPROXY_LICENSE_FILES = COPYING
 
