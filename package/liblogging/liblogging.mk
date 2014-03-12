3bb6a3667b1b3a04a5e91073071d5cf60a01d63f
Clayton Shotwell <clshotwe@rockwellcollins.com>
2014-03-12 12:51:08 -0500

    liblogging: new package
    Signed-off-by: Clayton Shotwell <clshotwe@rockwellcollins.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/liblogging/liblogging.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# liblogging
+#
+################################################################################
+
+LIBLOGGING_VERSION = 1.0.2
+LIBLOGGING_SITE = http://download.rsyslog.com/liblogging
+LIBLOGGING_LICENSE = BSD-2c
+LIBLOGGING_LICENSE_FILES = COPYING
+LIBLOGGING_INSTALL_STAGING = YES
+LIBLOGGING_CONF_OPT = --enable-cached-man-pages
+
+ifeq ($(BR2_INIT_SYSTEMD),y)
+LIBLOGGING_CONF_OPT += --enable-journal
+else
+LIBLOGGING_CONF_OPT += --disable-journal
+endif
+
+$(eval $(autotools-package))
