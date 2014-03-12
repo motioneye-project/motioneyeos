9b4696a49f35d64dfae5bcdc11a5a6fb2d0a76cf
Clayton Shotwell <clshotwe@rockwellcollins.com>
2014-03-12 12:51:09 -0500

    rsyslog: bump to version 7.6.0
    Bumping version of rsyslog and adding new dependencies of liblogging
    and libestr.  The first patch has be submitted to the upstream mailing
    list and the second patch is a backport from the development branch of
    the upstream.
    
    [Peter: needs libuuid / host-pkgconf, fix comment]
    Signed-off-by: Clayton Shotwell <clshotwe@rockwellcollins.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rsyslog/rsyslog.mk
+++ package/rsyslog/rsyslog.mk
@@ -4,21 +4,41 @@
 #
 ################################################################################
 
-RSYSLOG_VERSION = 5.8.0
+RSYSLOG_VERSION = 7.6.0
 RSYSLOG_SITE = http://rsyslog.com/files/download/rsyslog/
-RSYSLOG_DEPENDENCIES = zlib
+RSYSLOG_LICENSE = GPLv3 LGPLv3 Apache-2.0
+RSYSLOG_LICENSE_FILES = COPYING COPYING.LESSER COPYING.ASL20
+RSYSLOG_DEPENDENCIES = zlib libestr liblogging json-c util-linux host-pkgconf
+RSYSLOG_AUTORECONF = YES
+
+RSYSLOG_CONF_OPT = --disable-testbench \
+		   --enable-cached-man-pages
 
 # Build after Busybox
 ifeq ($(BR2_PACKAGE_BUSYBOX),y)
 	RSYSLOG_DEPENDENCIES += busybox
 endif
 
-define RSYSLOG_INSTALL_CONF_SCRIPT
+ifeq ($(BR2_PACKAGE_LIBEE),y)
+	RSYSLOG_DEPENDENCIES += libee
+endif
+
+ifeq ($(BR2_PACKAGE_LIBGCRYPT),y)
+	RSYSLOG_CONF_ENV += LIBGCRYPT_CONFIG=$(STAGING_DIR)/usr/bin/libgcrypt-config
+	RSYSLOG_CONF_OPT += --enable-libgcrypt=yes
+else
+	RSYSLOG_CONF_OPT += --enable-libgcrypt=no
+endif
+
+define RSYSLOG_INSTALL_INIT_SYSV
 	[ -f $(TARGET_DIR)/etc/init.d/S01rsyslog ] || \
 		$(INSTALL) -m 0755 -D package/rsyslog/S01rsyslog \
 			$(TARGET_DIR)/etc/init.d/S01rsyslog
+endef
+
+define RSYSLOG_INSTALL_CONF_SCRIPT
 	[ -f $(TARGET_DIR)/etc/rsyslog.conf ] || \
-		$(INSTALL) -m 0644 -D $(@D)/rsyslog.conf \
+		$(INSTALL) -m 0644 -D $(@D)/platform/redhat/rsyslog.conf \
 			$(TARGET_DIR)/etc/rsyslog.conf
 	mkdir -p $(TARGET_DIR)/etc/rsyslog.d
 endef
