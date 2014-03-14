c8528884479a210618b1cd2b99b79358a474904d
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-14 23:21:33 +0100

    fetchmail: New package
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/fetchmail/fetchmail.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# fetchmail
+#
+################################################################################
+
+FETCHMAIL_VERSION_MAJOR = 6.3
+FETCHMAIL_VERSION_MINOR = 26
+FETCHMAIL_VERSION = $(FETCHMAIL_VERSION_MAJOR).$(FETCHMAIL_VERSION_MINOR)
+FETCHMAIL_SOURCE = fetchmail-$(FETCHMAIL_VERSION).tar.xz
+FETCHMAIL_SITE = http://downloads.sourceforge.net/project/fetchmail/branch_$(FETCHMAIL_VERSION_MAJOR)/
+FETCHMAIL_LICENSE = GPLv2; some exceptions are mentioned in COPYING
+FETCHMAIL_LICENSE_FILES = COPYING
+
+FETCHMAIL_CONF_OPT = \
+	--with-ssl=$(STAGING_DIR)/usr
+
+FETCHMAIL_DEPENDENCIES = \
+	ca-certificates \
+	openssl \
+	$(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext)
+
+$(eval $(autotools-package))
