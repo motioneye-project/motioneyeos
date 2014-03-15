6f4ba73eb64f6ea2d871c479c389ff6c4e6125e8
Yann E. MORIN <yann.morin.1998@free.fr>
2014-03-15 18:19:19 +0100

    package/tzdata: bump to latest version 2014a
    Adds new definition for Turkish DST.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tzdata/tzdata.mk
+++ package/tzdata/tzdata.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TZDATA_VERSION = 2013i
+TZDATA_VERSION = 2014a
 TZDATA_SOURCE = tzdata$(TZDATA_VERSION).tar.gz
 TZDATA_SITE = ftp://ftp.iana.org/tz/releases
 TZDATA_DEPENDENCIES = host-zic
