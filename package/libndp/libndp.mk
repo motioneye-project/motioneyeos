2d07ad8026eaedc3fb01c94ac56791aeef3202f5
Yegor Yefremov <yegorslists@googlemail.com>
2014-03-17 11:57:07 +0100

    libndp: new package
    [Thomas: add patch to add stdarg.h include needed on uClibc, add
    missing upstream URL in Config.in, fix license to be LGPLv2.1+ instead
    of LGPLv2.1]
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/libndp/libndp.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# libndp
+#
+################################################################################
+
+LIBNDP_VERSION = v1.2
+LIBNDP_SITE = $(call github,jpirko,libndp,$(LIBNDP_VERSION))
+LIBNDP_LICENSE = LGPLv2.1+
+LIBNDP_LICENSE_FILES = COPYING
+LIBNDP_AUTORECONF = YES
+LIBNDP_INSTALL_STAGING = YES
+LIBNDP_CONF_OPT = --disable-debug
+
+$(eval $(autotools-package))
