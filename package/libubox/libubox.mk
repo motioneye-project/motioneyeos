2f2becd52210dd4376b7468708c8d63b446b1c23
Yegor Yefremov <yegorslists@googlemail.com>
2014-03-20 23:17:10 +0100

    libubox: new package
    [Peter: needs !static, use += for _DEPENDENCIES/_CONF_OPT]
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/libubox/libubox.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# libubox
+#
+################################################################################
+
+LIBUBOX_VERSION = bbd846ec2d72b2629758b69dc122ac0b0f2c3e4b
+LIBUBOX_SITE = git://nbd.name/luci2/libubox.git
+LIBUBOX_LICENSE = LGPLv2.1, GPLv2, BSD-3c, MIT
+LIBUBOX_INSTALL_STAGING = YES
+
+ifeq ($(BR2_PACKAGE_LUA),y)
+	LIBUBOX_DEPENDENCIES += lua
+else
+	LIBUBOX_CONF_OPT += -DBUILD_LUA:BOOL=OFF
+endif
+
+$(eval $(cmake-package))
