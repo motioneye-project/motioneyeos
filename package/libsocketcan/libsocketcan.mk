2ece1a6a2cd8ff8c37123bb6bf4af9a6e08e2edd
Yegor Yefremov <yegorslists@googlemail.com>
2014-03-24 11:32:34 +0100

    libsocketcan: bump to 0.0.10
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libsocketcan/libsocketcan.mk
+++ package/libsocketcan/libsocketcan.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSOCKETCAN_VERSION = 0.0.9
+LIBSOCKETCAN_VERSION = 0.0.10
 LIBSOCKETCAN_SITE = http://www.pengutronix.de/software/libsocketcan/download/
 LIBSOCKETCAN_SOURCE = libsocketcan-$(LIBSOCKETCAN_VERSION).tar.bz2
 LIBSOCKETCAN_INSTALL_STAGING = YES
