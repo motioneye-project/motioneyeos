200e9595c2e47c35c042bb330415dd95382b9182
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-03-28 20:29:19 -0300

    newt: fix weird indentation
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/newt/newt.mk
+++ package/newt/newt.mk
@@ -4,16 +4,14 @@
 #
 ################################################################################
 
-NEWT_VERSION         = 0.52.17
-NEWT_SITE            = https://fedorahosted.org/releases/n/e/newt/
-NEWT_LICENSE         = GPLv2
-NEWT_LICENSE_FILES   = COPYING
+NEWT_VERSION = 0.52.17
+NEWT_SITE = https://fedorahosted.org/releases/n/e/newt/
 NEWT_INSTALL_STAGING = YES
-
-NEWT_DEPENDENCIES = $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext) slang popt
-
+NEWT_DEPENDENCIES = popt slang \
+	$(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext)
 NEWT_CONF_OPT = --without-python --without-tcl
-
 NEWT_MAKE = $(MAKE1)
+NEWT_LICENSE = GPLv2
+NEWT_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
