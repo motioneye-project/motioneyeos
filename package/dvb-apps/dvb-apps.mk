c110099191a335d496e70a910e215a61254a9ad3
Yann E. MORIN <yann.morin.1998@free.fr>
2014-03-28 01:00:21 +0100

    package/dvb-apps: rely on dtv-scan-tables to provide transponder data
    This is a transitioning solution before switching packages that need the
    transponders data, to use dtv-scan-tables instead of dvb-apps.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/dvb-apps/dvb-apps.mk
+++ package/dvb-apps/dvb-apps.mk
@@ -8,21 +8,13 @@ DVB_APPS_VERSION        = be76da69f250
 DVB_APPS_SITE           = http://linuxtv.org/hg/dvb-apps
 DVB_APPS_SITE_METHOD    = hg
 
-# We just install the transponders data. As this is not a 'work' as per
-# traditional copyright, but just a collection of 'facts', there's probably
-# no license to apply to these data files.
-# To be noted however, is that the dvb-apps package bundles a copy of the
-# GPLv2 and a copy of the LGPLv2.1, and that some of the source files refer
-# to either the GPLv2+ or the LGPLv2.1+.
-# But since we do not use any of those source files, their license do not
-# apply to us.
-DVB_APPS_LICENSE        = unknown (probably public domain)
+DVB_APPS_DEPENDENCIES   = dtv-scan-tables
 
 ifeq ($(BR2_PACKAGE_DVB_APPS_UTILS),y)
 # Utilitiess are selected, build and install everything
 
-DVB_APPS_LICENSE       += GPLv2 GPLv2+ LGPLv2.1+
-DVB_APPS_LICENSE_FILES += COPYING COPYING.LGPL
+DVB_APPS_LICENSE        = GPLv2 GPLv2+ LGPLv2.1+
+DVB_APPS_LICENSE_FILES  = COPYING COPYING.LGPL
 
 DVB_APPS_LDFLAGS = $(TARGET_LDFLAGS)
 
@@ -47,13 +39,8 @@ define DVB_APPS_INSTALL_TARGET_CMDS
 endef
 
 else
-# Utilities are not selected, just install the scan files
-define DVB_APPS_INSTALL_TARGET_CMDS
-	for i in atsc dvb-c dvb-s dvb-t; do \
-		mkdir -p $(TARGET_DIR)/usr/share/dvb/$$i; \
-		$(INSTALL) $(@D)/util/scan/$$i/* $(TARGET_DIR)/usr/share/dvb/$$i; \
-	done
-endef
+# We do not really install anything, so there's no license
+DVB_APPS_LICENSE        = none
 endif
 
 $(eval $(generic-package))
