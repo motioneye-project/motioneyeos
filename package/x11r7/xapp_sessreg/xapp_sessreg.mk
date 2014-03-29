36bcfb849080669f4d9dd46e5368da9d2b8eb0cf
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 22:43:37 +0100

    xapp_sessreg: Bump version to 1.0.8
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_sessreg/xapp_sessreg.mk
+++ package/x11r7/xapp_sessreg/xapp_sessreg.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_SESSREG_VERSION = 1.0.7
+XAPP_SESSREG_VERSION = 1.0.8
 XAPP_SESSREG_SOURCE = sessreg-$(XAPP_SESSREG_VERSION).tar.bz2
 XAPP_SESSREG_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_SESSREG_LICENSE = MIT
