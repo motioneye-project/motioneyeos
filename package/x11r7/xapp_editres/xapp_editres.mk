efcb4567a40990e0aff090a78f8e345143aefdb4
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 22:43:59 +0100

    xapp_editres: Bump version to 1.0.6
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_editres/xapp_editres.mk
+++ package/x11r7/xapp_editres/xapp_editres.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_EDITRES_VERSION = 1.0.5
+XAPP_EDITRES_VERSION = 1.0.6
 XAPP_EDITRES_SOURCE = editres-$(XAPP_EDITRES_VERSION).tar.bz2
 XAPP_EDITRES_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_EDITRES_LICENSE = MIT
