6097f291b6903399eca33f04860da7b11c9e9b32
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 22:43:27 +0100

    xapp_xhost: Bump version to 1.0.6
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xhost/xapp_xhost.mk
+++ package/x11r7/xapp_xhost/xapp_xhost.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XHOST_VERSION = 1.0.5
+XAPP_XHOST_VERSION = 1.0.6
 XAPP_XHOST_SOURCE = xhost-$(XAPP_XHOST_VERSION).tar.bz2
 XAPP_XHOST_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XHOST_LICENSE = MIT
