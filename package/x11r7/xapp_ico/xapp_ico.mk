0de525e713b65f6ce1dc8b3e659919129e77be15
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 22:43:48 +0100

    xapp_ico: Bump version to 1.0.4
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_ico/xapp_ico.mk
+++ package/x11r7/xapp_ico/xapp_ico.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_ICO_VERSION = 1.0.2
+XAPP_ICO_VERSION = 1.0.4
 XAPP_ICO_SOURCE = ico-$(XAPP_ICO_VERSION).tar.bz2
 XAPP_ICO_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_ICO_LICENSE = MIT
