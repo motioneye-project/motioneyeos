1716776eec025a3ee439f7727ba2663786affaa8
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 21:56:43 +0100

    xdriver_xf86-video-ark: Bump version to 0.7.5
    xdriver_xf86-video-ark-0001-mibstore.patch
    - added upstream patch to fix compile error:
    http://cgit.freedesktop.org/xorg/driver/xf86-video-ark/commit/?id=3a3eb72baec46f48e6cf15a834271c3d3386deac
    
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xdriver_xf86-video-ark/xdriver_xf86-video-ark.mk
+++ package/x11r7/xdriver_xf86-video-ark/xdriver_xf86-video-ark.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_ARK_VERSION = 0.7.4
+XDRIVER_XF86_VIDEO_ARK_VERSION = 0.7.5
 XDRIVER_XF86_VIDEO_ARK_SOURCE = xf86-video-ark-$(XDRIVER_XF86_VIDEO_ARK_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_ARK_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_ARK_LICENSE = MIT
