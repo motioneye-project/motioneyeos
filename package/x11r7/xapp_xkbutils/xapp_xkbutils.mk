d8ac87d265829469624c6e80a4f9c9093c31ac1b
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 22:43:43 +0100

    xapp_xkbutils: Bump version to 1.0.4
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xkbutils/xapp_xkbutils.mk
+++ package/x11r7/xapp_xkbutils/xapp_xkbutils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XKBUTILS_VERSION = 1.0.3
+XAPP_XKBUTILS_VERSION = 1.0.4
 XAPP_XKBUTILS_SOURCE = xkbutils-$(XAPP_XKBUTILS_VERSION).tar.bz2
 XAPP_XKBUTILS_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XKBUTILS_LICENSE = MIT
