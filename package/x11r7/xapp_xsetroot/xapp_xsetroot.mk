63d2ff5c9df611bc9ac587e4433b913d206e5bf6
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 22:43:46 +0100

    xapp_xsetroot: Bump version to 1.1.1
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xsetroot/xapp_xsetroot.mk
+++ package/x11r7/xapp_xsetroot/xapp_xsetroot.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XSETROOT_VERSION = 1.1.0
+XAPP_XSETROOT_VERSION = 1.1.1
 XAPP_XSETROOT_SOURCE = xsetroot-$(XAPP_XSETROOT_VERSION).tar.bz2
 XAPP_XSETROOT_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XSETROOT_LICENSE = MIT
