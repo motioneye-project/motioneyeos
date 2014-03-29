aca528b411cfaadce36bdb98795f196c88637f2c
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 22:43:44 +0100

    xapp_xfsinfo: Bump version to 1.0.4
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xfsinfo/xapp_xfsinfo.mk
+++ package/x11r7/xapp_xfsinfo/xapp_xfsinfo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XFSINFO_VERSION = 1.0.1
+XAPP_XFSINFO_VERSION = 1.0.4
 XAPP_XFSINFO_SOURCE = xfsinfo-$(XAPP_XFSINFO_VERSION).tar.bz2
 XAPP_XFSINFO_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XFSINFO_LICENSE = MIT
