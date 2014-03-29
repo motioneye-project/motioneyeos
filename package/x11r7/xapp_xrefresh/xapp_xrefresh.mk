fd0640097a67584b00abf261373e21f122e11145
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 22:43:33 +0100

    xapp_xrefresh: Bump version to 1.0.5
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xrefresh/xapp_xrefresh.mk
+++ package/x11r7/xapp_xrefresh/xapp_xrefresh.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XREFRESH_VERSION = 1.0.4
+XAPP_XREFRESH_VERSION = 1.0.5
 XAPP_XREFRESH_SOURCE = xrefresh-$(XAPP_XREFRESH_VERSION).tar.bz2
 XAPP_XREFRESH_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XREFRESH_LICENSE = MIT
