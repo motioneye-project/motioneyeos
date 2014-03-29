33b031c0f27e7553d12d6687e456bde2bd60ae04
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 21:56:29 +0100

    xdriver_xf86-video-dummy: Bump version to 0.3.7
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xdriver_xf86-video-dummy/xdriver_xf86-video-dummy.mk
+++ package/x11r7/xdriver_xf86-video-dummy/xdriver_xf86-video-dummy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_DUMMY_VERSION = 0.3.5
+XDRIVER_XF86_VIDEO_DUMMY_VERSION = 0.3.7
 XDRIVER_XF86_VIDEO_DUMMY_SOURCE = xf86-video-dummy-$(XDRIVER_XF86_VIDEO_DUMMY_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_DUMMY_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_DUMMY_LICENSE = MIT
