1496b7e31c438371d1f2f03b0e7c7d5402eeeb74
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 22:43:38 +0100

    xapp_twm: Bump version to 1.0.8
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_twm/xapp_twm.mk
+++ package/x11r7/xapp_twm/xapp_twm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_TWM_VERSION = 1.0.6
+XAPP_TWM_VERSION = 1.0.8
 XAPP_TWM_SOURCE = twm-$(XAPP_TWM_VERSION).tar.bz2
 XAPP_TWM_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_TWM_LICENSE = MIT
