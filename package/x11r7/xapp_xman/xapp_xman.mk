e8e71ebe192edc1c2ed3ee284f20430b01924f93
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 22:43:21 +0100

    xapp_xman: Bump version to 1.1.3
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xman/xapp_xman.mk
+++ package/x11r7/xapp_xman/xapp_xman.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XMAN_VERSION = 1.0.3
+XAPP_XMAN_VERSION = 1.1.3
 XAPP_XMAN_SOURCE = xman-$(XAPP_XMAN_VERSION).tar.bz2
 XAPP_XMAN_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XMAN_LICENSE = MIT
@@ -13,6 +13,7 @@ XAPP_XMAN_DEPENDENCIES = xlib_libXaw
 
 XAPP_XMAN_CONF_ENV = ac_cv_file__etc_man_conf=no \
 		ac_cv_file__etc_man_config=no \
-		ac_cv_file__etc_manpath_config=no
+		ac_cv_file__etc_manpath_config=no \
+		ac_cv_file__usr_share_misc_man_conf=no
 
 $(eval $(autotools-package))
