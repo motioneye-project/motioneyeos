849beadf67a232e92f8c81185c2b5da4d445a2be
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 21:56:47 +0100

    xapp_xbacklight: Bump version to 1.2.1
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xbacklight/xapp_xbacklight.mk
+++ package/x11r7/xapp_xbacklight/xapp_xbacklight.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-XAPP_XBACKLIGHT_VERSION = 1.1.2
+XAPP_XBACKLIGHT_VERSION = 1.2.1
 XAPP_XBACKLIGHT_SOURCE = xbacklight-$(XAPP_XBACKLIGHT_VERSION).tar.bz2
 XAPP_XBACKLIGHT_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XBACKLIGHT_LICENSE = MIT
 XAPP_XBACKLIGHT_LICENSE_FILES = COPYING
-XAPP_XBACKLIGHT_DEPENDENCIES = xlib_libX11 xlib_libXrandr xlib_libXrender
+XAPP_XBACKLIGHT_DEPENDENCIES = xlib_libX11 xlib_libXrandr xlib_libXrender xcb-util
 
 $(eval $(autotools-package))
