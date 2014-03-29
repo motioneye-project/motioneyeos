cc527214854fb864263c26ebd7ac81ff153c392f
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 21:56:13 +0100

    xcb-util-wm: Bump version to 0.4.1
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xcb-util-wm/xcb-util-wm.mk
+++ package/x11r7/xcb-util-wm/xcb-util-wm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XCB_UTIL_WM_VERSION = 0.3.9
+XCB_UTIL_WM_VERSION = 0.4.1
 XCB_UTIL_WM_SITE = http://xcb.freedesktop.org/dist/
 XCB_UTIL_WM_SOURCE = xcb-util-wm-$(XCB_UTIL_WM_VERSION).tar.bz2
 XCB_UTIL_WM_INSTALL_STAGING = YES
