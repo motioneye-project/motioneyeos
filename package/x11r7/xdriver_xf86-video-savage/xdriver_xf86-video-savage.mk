e81ac419d9f6b670ef9b23babd3f3007b2875908
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 21:56:24 +0100

    xdriver_xf86-video-savage: Bump version to 2.3.7
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xdriver_xf86-video-savage/xdriver_xf86-video-savage.mk
+++ package/x11r7/xdriver_xf86-video-savage/xdriver_xf86-video-savage.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_SAVAGE_VERSION = 2.3.4
+XDRIVER_XF86_VIDEO_SAVAGE_VERSION = 2.3.7
 XDRIVER_XF86_VIDEO_SAVAGE_SOURCE = xf86-video-savage-$(XDRIVER_XF86_VIDEO_SAVAGE_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_SAVAGE_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_SAVAGE_LICENSE = MIT
