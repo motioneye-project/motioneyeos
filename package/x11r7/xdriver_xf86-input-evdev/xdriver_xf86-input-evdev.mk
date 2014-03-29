a7535b0b265d74263eef5aca54a9e751717a0cac
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 21:56:14 +0100

    xdriver_xf86-input-evdev: Bump version to 2.8.2
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xdriver_xf86-input-evdev/xdriver_xf86-input-evdev.mk
+++ package/x11r7/xdriver_xf86-input-evdev/xdriver_xf86-input-evdev.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_INPUT_EVDEV_VERSION = 2.7.0
+XDRIVER_XF86_INPUT_EVDEV_VERSION = 2.8.2
 XDRIVER_XF86_INPUT_EVDEV_SOURCE = xf86-input-evdev-$(XDRIVER_XF86_INPUT_EVDEV_VERSION).tar.bz2
 XDRIVER_XF86_INPUT_EVDEV_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_INPUT_EVDEV_LICENSE = MIT
