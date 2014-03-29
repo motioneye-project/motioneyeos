999151f5bc3dd6185f82e42aa2b21ecced5a97a7
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 21:56:11 +0100

    xlib_libFS: Bump version to 1.0.6
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xlib_libFS/xlib_libFS.mk
+++ package/x11r7/xlib_libFS/xlib_libFS.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBFS_VERSION = 1.0.5
+XLIB_LIBFS_VERSION = 1.0.6
 XLIB_LIBFS_SOURCE = libFS-$(XLIB_LIBFS_VERSION).tar.bz2
 XLIB_LIBFS_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBFS_LICENSE = MIT
