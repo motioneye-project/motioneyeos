dbbdf3469100cdd1ad17bc0274a13e1b332d3d83
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 22:44:00 +0100

    xapp_fstobdf: Bump version to 1.0.5
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_fstobdf/xapp_fstobdf.mk
+++ package/x11r7/xapp_fstobdf/xapp_fstobdf.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_FSTOBDF_VERSION = 1.0.2
+XAPP_FSTOBDF_VERSION = 1.0.5
 XAPP_FSTOBDF_SOURCE = fstobdf-$(XAPP_FSTOBDF_VERSION).tar.bz2
 XAPP_FSTOBDF_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_FSTOBDF_LICENSE = MIT
