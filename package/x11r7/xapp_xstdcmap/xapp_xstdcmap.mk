39937c6687dfbfd4db54f53529845e2b5bd999cf
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 22:43:41 +0100

    xapp_xstdcmap: Bump version to 1.0.3
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xstdcmap/xapp_xstdcmap.mk
+++ package/x11r7/xapp_xstdcmap/xapp_xstdcmap.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XSTDCMAP_VERSION = 1.0.1
+XAPP_XSTDCMAP_VERSION = 1.0.3
 XAPP_XSTDCMAP_SOURCE = xstdcmap-$(XAPP_XSTDCMAP_VERSION).tar.bz2
 XAPP_XSTDCMAP_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XSTDCMAP_LICENSE = MIT
