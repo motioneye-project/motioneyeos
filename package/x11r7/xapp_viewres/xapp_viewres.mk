bbd9575c23ab41f5e7bc4a168a69dfc60cf155b3
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 22:43:57 +0100

    xapp_viewres: Bump version to 1.0.4
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_viewres/xapp_viewres.mk
+++ package/x11r7/xapp_viewres/xapp_viewres.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_VIEWRES_VERSION = 1.0.3
+XAPP_VIEWRES_VERSION = 1.0.4
 XAPP_VIEWRES_SOURCE = viewres-$(XAPP_VIEWRES_VERSION).tar.bz2
 XAPP_VIEWRES_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_VIEWRES_LICENSE = MIT
