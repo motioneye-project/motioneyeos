fe65ce3787d76f6f3c6fd07983ed3d20ed4a7fe2
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 21:56:41 +0100

    xdriver_xf86-video-i128: Bump version to 1.3.6
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xdriver_xf86-video-i128/xdriver_xf86-video-i128.mk
+++ package/x11r7/xdriver_xf86-video-i128/xdriver_xf86-video-i128.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_I128_VERSION = 1.3.5
+XDRIVER_XF86_VIDEO_I128_VERSION = 1.3.6
 XDRIVER_XF86_VIDEO_I128_SOURCE = xf86-video-i128-$(XDRIVER_XF86_VIDEO_I128_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_I128_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_I128_LICENSE = MIT
