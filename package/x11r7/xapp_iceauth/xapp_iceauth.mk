bac5f2f69259d20b5a21996cb70ad7c958a4eaca
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 22:43:26 +0100

    xapp_iceauth: Bump version to 1.0.6
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_iceauth/xapp_iceauth.mk
+++ package/x11r7/xapp_iceauth/xapp_iceauth.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_ICEAUTH_VERSION = 1.0.5
+XAPP_ICEAUTH_VERSION = 1.0.6
 XAPP_ICEAUTH_SOURCE = iceauth-$(XAPP_ICEAUTH_VERSION).tar.bz2
 XAPP_ICEAUTH_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_ICEAUTH_LICENSE = MIT
