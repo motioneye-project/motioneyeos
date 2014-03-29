22e1d32847c6a768b27ffe1f2d9c203f2bd67e9e
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 22:43:25 +0100

    xapp_xload: Bump version to 1.1.2
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xload/xapp_xload.mk
+++ package/x11r7/xapp_xload/xapp_xload.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XLOAD_VERSION = 1.1.1
+XAPP_XLOAD_VERSION = 1.1.2
 XAPP_XLOAD_SOURCE = xload-$(XAPP_XLOAD_VERSION).tar.bz2
 XAPP_XLOAD_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XLOAD_LICENSE = MIT
