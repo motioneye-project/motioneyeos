66b6baf4167a00c6dc8a9afdf7e75701889d4ec4
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 21:56:39 +0100

    xdriver_xf86-video-sis: Bump version to 0.10.7
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xdriver_xf86-video-sis/xdriver_xf86-video-sis.mk
+++ package/x11r7/xdriver_xf86-video-sis/xdriver_xf86-video-sis.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_SIS_VERSION = 0.10.4
+XDRIVER_XF86_VIDEO_SIS_VERSION = 0.10.7
 XDRIVER_XF86_VIDEO_SIS_SOURCE = xf86-video-sis-$(XDRIVER_XF86_VIDEO_SIS_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_SIS_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_SIS_LICENSE = MIT
