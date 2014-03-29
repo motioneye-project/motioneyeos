597dd87ed2df80509223bd473db080e7aeda1226
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 21:56:21 +0100

    xdriver_xf86-video-ast: Bump version to 0.99.9
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xdriver_xf86-video-ast/xdriver_xf86-video-ast.mk
+++ package/x11r7/xdriver_xf86-video-ast/xdriver_xf86-video-ast.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_AST_VERSION = 0.93.10
+XDRIVER_XF86_VIDEO_AST_VERSION = 0.99.9
 XDRIVER_XF86_VIDEO_AST_SOURCE = xf86-video-ast-$(XDRIVER_XF86_VIDEO_AST_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_AST_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_AST_LICENSE = MIT
