c46daf0ee40c0ac7acf6556e1e5b877491e58cd4
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 22:43:34 +0100

    xapp_xfontsel: Bump version to 1.0.5
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xfontsel/xapp_xfontsel.mk
+++ package/x11r7/xapp_xfontsel/xapp_xfontsel.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XFONTSEL_VERSION = 1.0.4
+XAPP_XFONTSEL_VERSION = 1.0.5
 XAPP_XFONTSEL_SOURCE = xfontsel-$(XAPP_XFONTSEL_VERSION).tar.bz2
 XAPP_XFONTSEL_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XFONTSEL_LICENSE = MIT
