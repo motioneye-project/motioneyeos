79f537636a44f0351fed424c9bd197999862c87c
Bernd Kuhls <berndkuhls@hotmail.com>
2014-03-29 21:56:27 +0100

    xdriver_xf86-video-glide: Bump version to 1.2.2
    Signed-off-by: Bernd Kuhls <berndkuhls@hotmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xdriver_xf86-video-glide/xdriver_xf86-video-glide.mk
+++ package/x11r7/xdriver_xf86-video-glide/xdriver_xf86-video-glide.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_GLIDE_VERSION = 1.2.0
+XDRIVER_XF86_VIDEO_GLIDE_VERSION = 1.2.2
 XDRIVER_XF86_VIDEO_GLIDE_SOURCE = xf86-video-glide-$(XDRIVER_XF86_VIDEO_GLIDE_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_GLIDE_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_GLIDE_LICENSE = MIT
