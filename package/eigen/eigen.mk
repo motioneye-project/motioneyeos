feefe6b8843740562067625aaf3ca3b639502afc
Davide Viti <zinosat@tiscali.it>
2014-03-31 00:30:30 +0200

    eigen: add an option to install unsupported modules
    Signed-off-by: Davide Viti <d.viti@infosolution.it>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/eigen/eigen.mk
+++ package/eigen/eigen.mk
@@ -14,12 +14,20 @@ EIGEN_INSTALL_STAGING = YES
 EIGEN_INSTALL_TARGET = NO
 EIGEN_DEST_DIR = $(STAGING_DIR)/usr/include/eigen3
 
+ifeq ($(BR2_PACKAGE_EIGEN_UNSUPPORTED_MODULES),y)
+define EIGEN_INSTALL_UNSUPPORTED_MODULES_CMDS
+	mkdir -p $(EIGEN_DEST_DIR)/unsupported
+	cp -a $(@D)/unsupported/Eigen $(EIGEN_DEST_DIR)/unsupported
+endef
+endif
+
 # This package only consists of headers that need to be
 # copied over to the sysroot for compile time use
 define EIGEN_INSTALL_STAGING_CMDS
 	$(RM) -r $(EIGEN_DEST_DIR)
 	mkdir -p $(EIGEN_DEST_DIR)
 	cp -a $(@D)/Eigen $(EIGEN_DEST_DIR)
+	$(EIGEN_INSTALL_UNSUPPORTED_MODULES_CMDS)
 endef
 
 $(eval $(generic-package))
