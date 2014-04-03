58442b5eefab37eb6b55b6f63f7a3995c042b7ca
Mischa Jonker <mjonker@synopsys.com>
2014-04-03 18:40:52 +0400

    util-linux: bump version to 2.24.1
    Removed "support older machines" patch as it has been fixed upstream,
    fixed up other patches.
    
    This removes some arch-dependencies in fdiskbsdlabel.h, fixing the
    build for the ARC-architecture.
    
    This fixes:
    http://autobuild.buildroot.net/results/8863799d856ccab51ee8ffd499378574ef166aee/
    
    Signed-off-by: Mischa Jonker <mjonker@synopsys.com>
    Signed-off-by: Anton Kolesov <Anton.Kolesov@synopsys.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/util-linux/util-linux.mk
+++ package/util-linux/util-linux.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-UTIL_LINUX_VERSION = $(UTIL_LINUX_VERSION_MAJOR).2
-UTIL_LINUX_VERSION_MAJOR = 2.23
+UTIL_LINUX_VERSION = $(UTIL_LINUX_VERSION_MAJOR).1
+UTIL_LINUX_VERSION_MAJOR = 2.24
 UTIL_LINUX_SOURCE = util-linux-$(UTIL_LINUX_VERSION).tar.xz
 UTIL_LINUX_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/util-linux/v$(UTIL_LINUX_VERSION_MAJOR)
 
