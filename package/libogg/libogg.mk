98cc6e8fcd8e714baaacac62c583c71e28fd2985
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-04-03 21:01:04 +0200

    libogg: version bump 1.3.1
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libogg/libogg.mk
+++ package/libogg/libogg.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBOGG_VERSION = 1.3.0
+LIBOGG_VERSION = 1.3.1
 LIBOGG_SOURCE = libogg-$(LIBOGG_VERSION).tar.xz
 LIBOGG_SITE = http://downloads.xiph.org/releases/ogg
 LIBOGG_LICENSE = BSD-3c
