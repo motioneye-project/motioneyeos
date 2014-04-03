f7acb2bdcd7615cccbe8204fae58e2f4fe6c016b
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-04-03 21:01:03 +0200

    libmodplug: version bump 0.8.8.5
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libmodplug/libmodplug.mk
+++ package/libmodplug/libmodplug.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBMODPLUG_VERSION = 0.8.8.4
+LIBMODPLUG_VERSION = 0.8.8.5
 LIBMODPLUG_SITE = http://downloads.sourceforge.net/project/modplug-xmms/libmodplug/$(LIBMODPLUG_VERSION)
 LIBMODPLUG_INSTALL_STAGING = YES
 LIBMODPLUG_LICENSE = Public Domain
