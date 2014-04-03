053459a0f32b415a0aba25da3e3fc214898cea51
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-04-03 11:14:56 -0300

    gmp: bump to version 6.0.0a
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gmp/gmp.mk
+++ package/gmp/gmp.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-GMP_VERSION = 5.1.3
+GMP_VERSION = 6.0.0a
 GMP_SITE = $(BR2_GNU_MIRROR)/gmp
 GMP_SOURCE = gmp-$(GMP_VERSION).tar.xz
 GMP_INSTALL_STAGING = YES
 GMP_LICENSE = LGPLv3+
-GMP_LICENSE_FILES = COPYING.LIB
+GMP_LICENSE_FILES = COPYING.LESSERv3
 GMP_DEPENDENCIES = host-m4
 
 $(eval $(autotools-package))
