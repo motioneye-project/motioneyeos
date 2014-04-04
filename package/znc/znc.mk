d66f773aa7f13992b4381f64e308b7feb8c99727
Cédric Chépied <cedric.chepied@gmail.com>
2014-04-04 20:22:53 +0200

    znc: new package
    ported patch from Cédric Chépied:
    http://patchwork.ozlabs.org/patch/306896/
    
    [Thomas: added comment explaining why we don't use <pkg>_AUTORECONF,
    and use a more conventional syntax for <pkg>_DEPENDENCIES and
    <pkg>_CONF_OPT.]
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/znc/znc.mk
@@ -0,0 +1,49 @@
+################################################################################
+#
+# znc
+#
+################################################################################
+
+ZNC_VERSION = b396cafdb249544164ed02942a5babba59e519a3
+ZNC_SITE = git://github.com/znc/znc.git
+ZNC_LICENSE = Apache-2.0
+ZNC_LICENSE_FILES = LICENSE
+ZNC_DEPENDENCIES = host-pkgconf host-autoconf host-automake
+ZNC_CONF_OPT = --disable-perl
+
+# The standard <pkg>_AUTORECONF = YES invocation doesn't work for this
+# package, because it does not use automake in a normal way.
+define ZNC_RUN_AUTOGEN
+	cd $(@D) && PATH=$(HOST_PATH) ./autogen.sh
+endef
+ZNC_PRE_CONFIGURE_HOOKS += ZNC_RUN_AUTOGEN
+
+ifeq ($(BR2_PACKAGE_ICU),y)
+ZNC_DEPENDENCIES += icu
+ZNC_CONF_OPT += --enable-icu
+else
+ZNC_CONF_OPT += --disable-icu
+endif
+
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+ZNC_DEPENDENCIES += openssl
+ZNC_CONF_OPT += --enable-openssl
+else
+ZNC_CONF_OPT += --disable-openssl
+endif
+
+ifeq ($(BR2_PACKAGE_ZLIB),y)
+ZNC_DEPENDENCIES += zlib
+ZNC_CONF_OPT += --enable-zlib
+else
+ZNC_CONF_OPT += --disable-zlib
+endif
+
+ifeq ($(BR2_PACKAGE_PYTHON3),y)
+ZNC_DEPENDENCIES += python3 host-swig
+ZNC_CONF_OPT += --enable-python=python3
+else
+ZNC_CONF_OPT += --disable-python
+endif
+
+$(eval $(autotools-package))
