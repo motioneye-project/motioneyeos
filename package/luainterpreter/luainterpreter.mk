50dfaef6ec2d1bcfb6db232a3802882d3054707c
Yann E. MORIN <yann.morin.1998@free.fr>
2014-04-05 17:21:42 +0200

    package/luainterpreter: rename the _HAS and _PROVIDES variables
    The basic rule for a package is to have its options named
    after the package name. There is no reason this should not
    also be the case for virtual packages.
    
    Besides, this will allow us to switch luainterpreter to use the
    soon-to-be-introduced virtual-package infrastructure.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Thomas De Schampheleire <patrickdepinguin@gmail.com>
    Cc: Mike Zick <minimod@morethan.org>
    Cc: Francois Perrad <fperrad@gmail.com>
    Reviewed-by: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/luainterpreter/luainterpreter.mk
+++ package/luainterpreter/luainterpreter.mk
@@ -5,11 +5,11 @@
 ################################################################################
 
 LUAINTERPRETER_SOURCE =
-LUAINTERPRETER_DEPENDENCIES = $(call qstrip,$(BR2_PACKAGE_PROVIDES_LUA_INTERPRETER))
+LUAINTERPRETER_DEPENDENCIES = $(call qstrip,$(BR2_PACKAGE_PROVIDES_LUAINTERPRETER))
 
 LUAINTERPRETER_ABIVER = $(call qstrip,$(BR2_PACKAGE_LUAINTERPRETER_ABI_VERSION))
 
-ifeq ($(BR2_PACKAGE_HAS_LUA_INTERPRETER),y)
+ifeq ($(BR2_PACKAGE_HAS_LUAINTERPRETER),y)
 ifeq ($(LUAINTERPRETER_DEPENDENCIES),)
 $(error No lua interpreter implementation selected. Configuration error.)
 endif
