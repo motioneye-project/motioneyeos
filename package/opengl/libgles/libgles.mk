c5d546450fc166ff862b0e39f206fdb0caed5a1a
Yann E. MORIN <yann.morin.1998@free.fr>
2014-04-05 17:21:49 +0200

    package/libgles: convert to the virtual-package infrastructure
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Thomas De Schampheleire <patrickdepinguin@gmail.com>
    Cc: Mike Zick <minimod@morethan.org>
    Reviewed-by: Samuel Martin <s.martin49@gmail.com>
    [eric.le.bihan.dev@free.fr: tested on RPi, with Qt5]
    Tested-by: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/opengl/libgles/libgles.mk
+++ package/opengl/libgles/libgles.mk
@@ -4,13 +4,4 @@
 #
 ################################################################################
 
-LIBGLES_SOURCE =
-LIBGLES_DEPENDENCIES = $(call qstrip,$(BR2_PACKAGE_PROVIDES_LIBGLES))
-
-ifeq ($(BR2_PACKAGE_HAS_LIBGLES),y)
-ifeq ($(LIBGLES_DEPENDENCIES),)
-$(error No libGLES implementation selected. Configuration error.)
-endif
-endif
-
-$(eval $(generic-package))
+$(eval $(virtual-package))
