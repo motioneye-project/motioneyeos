30eafe597ff0641654ecb1e1eeef0a652eac455e
Yann E. MORIN <yann.morin.1998@free.fr>
2014-04-05 17:21:50 +0200

    package/libegl: convert to the virtual-package infrastructure
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Thomas De Schampheleire <patrickdepinguin@gmail.com>
    Cc: Mike Zick <minimod@morethan.org>
    Reviewed-by: Samuel Martin <s.martin49@gmail.com>
    [eric.le.bihan.dev@free.fr: tested on RPi, with Qt5]
    Tested-by: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/opengl/libegl/libegl.mk
+++ package/opengl/libegl/libegl.mk
@@ -4,13 +4,4 @@
 #
 ################################################################################
 
-LIBEGL_SOURCE =
-LIBEGL_DEPENDENCIES = $(call qstrip,$(BR2_PACKAGE_PROVIDES_LIBEGL))
-
-ifeq ($(BR2_PACKAGE_HAS_LIBEGL),y)
-ifeq ($(LIBEGL_DEPENDENCIES),)
-$(error No libEGL implementation selected. Configuration error.)
-endif
-endif
-
-$(eval $(generic-package))
+$(eval $(virtual-package))
