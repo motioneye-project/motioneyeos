7b624de63a460194b0957bfd42b6bf51d0f4b98b
Yann E. MORIN <yann.morin.1998@free.fr>
2014-04-05 17:21:48 +0200

    package/powervr: convert to the virtual-package infrastructure
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Thomas De Schampheleire <patrickdepinguin@gmail.com>
    Cc: Mike Zick <minimod@morethan.org>
    Reviewed-by: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/powervr/powervr.mk
+++ package/powervr/powervr.mk
@@ -4,13 +4,4 @@
 #
 ################################################################################
 
-POWERVR_SOURCE =
-POWERVR_DEPENDENCIES = $(call qstrip,$(BR2_PACKAGE_PROVIDES_POWERVR))
-
-ifeq ($(BR2_PACKAGE_HAS_POWERVR),y)
-ifeq ($(POWERVR_DEPENDENCIES),)
-$(error No PowerVR implementation selected. Configuration error.)
-endif
-endif
-
-$(eval $(generic-package))
+$(eval $(virtual-package))
