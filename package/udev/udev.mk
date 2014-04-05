7397f45782abd3249bbb7786076c4ee58b29e9b6
Yann E. MORIN <yann.morin.1998@free.fr>
2014-04-05 17:21:56 +0200

    package/udev: convert to the virtual-package infrastructure
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Thomas De Schampheleire <patrickdepinguin@gmail.com>
    Cc: Mike Zick <minimod@morethan.org>
    [eric.le.bihan.dev@free.fr: tested with both systemd and eudev, on QEMU x86]
    Tested-by: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/udev/udev.mk
+++ package/udev/udev.mk
@@ -4,13 +4,4 @@
 #
 ################################################################################
 
-UDEV_SOURCE =
-UDEV_DEPENDENCIES = $(call qstrip,$(BR2_PACKAGE_PROVIDES_UDEV))
-
-ifeq ($(BR2_PACKAGE_HAS_UDEV),y)
-ifeq ($(UDEV_DEPENDENCIES),)
-$(error No Udev implementation selected. Configuration error)
-endif
-endif
-
-$(eval $(generic-package))
+$(eval $(virtual-package))
