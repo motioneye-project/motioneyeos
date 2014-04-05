76c07120e7380509b63b3ab4fb5e98eee9d6ad04
Yann E. MORIN <yann.morin.1998@free.fr>
2014-04-05 17:21:55 +0200

    package/cryptodev: convert to the virtual-package infrastructure
    Since this package is implemented via a choice rather than the usual
    separate-package providers, we only need BR2_PACKAGE_HAS_CRYPTODEV
    to be always defined when the 'cryptodev' package is selected.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Thomas De Schampheleire <patrickdepinguin@gmail.com>
    Cc: Mike Zick <minimod@morethan.org>
    Reviewed-by: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/cryptodev/cryptodev.mk
+++ package/cryptodev/cryptodev.mk
@@ -4,14 +4,4 @@
 #
 ################################################################################
 
-CRYPTODEV_SOURCE =
-CRYPTODEV_DEPENDENCIES = $(call qstrip,$(BR2_PACKAGE_PROVIDES_CRYPTODEV))
-
-ifeq ($(CRYPTODEV_DEPENDENCIES),)
-define CRYPTODEV_CONFIGURE_CMDS
-	echo "No CRYPTODEV implementation defined. Configuration error"
-	exit 1
-endef
-endif
-
-$(eval $(generic-package))
+$(eval $(virtual-package))
