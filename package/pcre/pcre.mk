66103837416f6fb649053cba07d51c36246ef570
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-04-04 21:12:36 -0300

    pcre: bump to version 8.35
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/pcre/pcre.mk
+++ package/pcre/pcre.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PCRE_VERSION = 8.34
+PCRE_VERSION = 8.35
 PCRE_SITE = ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre
 PCRE_LICENSE = BSD-3c
 PCRE_LICENSE_FILES = LICENCE
