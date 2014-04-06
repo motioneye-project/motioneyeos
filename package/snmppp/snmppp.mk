4474809aa4b81fc234159e2555d5c0045e18692c
Luca Ceresoli <luca@lucaceresoli.net>
2014-04-07 00:20:05 +0200

    snmppp: add option to disable SNMPv3 support
    Disabling SNMPv3 support also removes the dependency on OpenSSL, which is
    pretty large (over 2 MB of uncompressed filesystem size on an ARM926 platform).
    
    BR2_PACKAGE_SNMPPP_SNMPV3 defaults to yes for backward compatibility with
    previous Buildroot releases, where SNMPv3 was always enabled.
    
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/snmppp/snmppp.mk
+++ package/snmppp/snmppp.mk
@@ -7,12 +7,19 @@
 SNMPPP_VERSION = 3.3.4
 SNMPPP_SOURCE = snmp++-$(SNMPPP_VERSION).tar.gz
 SNMPPP_SITE = http://www.agentpp.com
-SNMPPP_DEPENDENCIES = openssl host-pkgconf
+SNMPPP_DEPENDENCIES = host-pkgconf
 SNMPPP_INSTALL_STAGING = YES
 # no configure script in tarball
 SNMPPP_AUTORECONF = YES
 SNMPPP_CONF_OPT = $(if $(BR2_PACKAGE_SNMPPP_LOGGING),--enable-logging,--disable-logging)
 SNMPPP_LICENSE = SNMP++
 SNMPPP_LICENSE_FILES = src/v3.cpp
+
+ifeq ($(BR2_PACKAGE_SNMPPP_SNMPV3),y)
+	SNMPPP_CONF_OPT += --enable-snmpv3
+	SNMPPP_DEPENDENCIES += openssl
+else
+	SNMPPP_CONF_OPT += --disable-snmpv3
+endif
 
 $(eval $(autotools-package))
