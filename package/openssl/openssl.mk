ab39c1b47cf8555b82105ecc9b98adb7cb7d225f
Baruch Siach <baruch@tkos.co.il>
2014-04-08 07:31:35 +0300

    openssl: bump to version 1.0.1g
    Fixes highly critical CVE-2014-0160. See http://heartbleed.com .
    
    Drop patch applied upstream.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENSSL_VERSION = 1.0.1f
+OPENSSL_VERSION = 1.0.1g
 OPENSSL_SITE = http://www.openssl.org/source
 OPENSSL_LICENSE = OpenSSL or SSLeay
 OPENSSL_LICENSE_FILES = LICENSE
