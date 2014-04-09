e36344728dba458977ee0bc22844c943425d5de2
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-04-09 15:11:46 -0300

    libyaml: security bump to version 0.1.6
    Fixes CVE-2014-2525.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libyaml/libyaml.mk
+++ package/libyaml/libyaml.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBYAML_VERSION = 0.1.4
+LIBYAML_VERSION = 0.1.6
 LIBYAML_SOURCE = yaml-$(LIBYAML_VERSION).tar.gz
 LIBYAML_SITE = http://pyyaml.org/download/libyaml/
 LIBYAML_INSTALL_STAGING = YES
