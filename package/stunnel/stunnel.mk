22fe313dcc11233fb91e46b4dbcde36c7bb36d5f
Baruch Siach <baruch@tkos.co.il>
2014-04-10 11:52:36 +0300

    stunnel: bump to version 5.01
    Fixes CVE-2014-0016.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/stunnel/stunnel.mk
+++ package/stunnel/stunnel.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-STUNNEL_VERSION = 4.56
-STUNNEL_SITE = http://ftp.nluug.nl/pub/networking/stunnel/archive/4.x/
+STUNNEL_VERSION = 5.01
+STUNNEL_SITE = http://ftp.nluug.nl/pub/networking/stunnel/archive/5.x/
 STUNNEL_DEPENDENCIES = openssl
 STUNNEL_CONF_OPT = --with-ssl=$(STAGING_DIR)/usr --with-threads=fork
 STUNNEL_LICENSE = GPLv2+
