00856e2ba2e9a30dba00db5503231e5a35157b24
Frank Hunleth <fhunleth@troodon-software.com>
2014-04-10 10:35:25 -0400

    erlang: bump version to 17.0
    Erlang/OTP 17.0 is a new major release with new features, characteristics
    improvements, as well as some minor incompatibilities.
    
    Some highlights of the release are:
      - Erlang/OTP has been ported to the realtime operating system OSE.
      - Maps, a new dictionary data type (experimental)
      - A more natural mapping from ASN.1 OCTET STRING and BIT STRING to Erlang
        types, and other ASN.1 improvements and optimizations
      - The {active, N} socket option for TCP, UDP, and SCTP
      - A new (optional) scheduler utilization balancing mechanism
      - Migration of memory carriers has been enabled by default on all ERTS
        internal memory allocators
      - Increased garbage collection tenure rate
      - Experimental "dirty schedulers" functionality
      - Funs can now be given names
      - Miscellaneous unicode support enhancements
      - A new version scheme for OTP its applications has been introduced
    
    Signed-off-by: Frank Hunleth <fhunleth@troodon-software.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/erlang/erlang.mk
+++ package/erlang/erlang.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ERLANG_VERSION = R16B03-1
+ERLANG_VERSION = 17.0
 ERLANG_SITE = http://www.erlang.org/download
 ERLANG_SOURCE = otp_src_$(ERLANG_VERSION).tar.gz
 ERLANG_DEPENDENCIES = host-erlang
