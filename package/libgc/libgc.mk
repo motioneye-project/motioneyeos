8986a221172cac4f84e74212476c12c288995abf
alex.bennee@linaro.org <alex.bennee@linaro.org>
2014-04-11 15:05:55 +0100

    package: add Boehm-Demers-Weiser gc library
    This is needed for applications like Zile
    
    [Thomas:
     - use $(...) instead of ${...} to reference LIBGC_VERSION
     - add missing dependency on libatomic_ops dependencies
     - fix DEPENDANCIES -> DEPENDENCIES
     - replace += by = in dependencies definition
     - add host-pkgconf to the list of dependencies
     - fix license, it's a X11-style permissive license
     - fix license file, COPYING didn't exist, use README.md instead]
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/libgc/libgc.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# libgc
+#
+################################################################################
+
+LIBGC_VERSION = 7.4.0
+LIBGC_SOURCE = gc-$(LIBGC_VERSION).tar.gz
+LIBGC_SITE = http://www.hboehm.info/gc/gc_source/
+LIBGC_DEPENDENCIES = libatomic_ops host-pkgconf
+LIBGC_LICENSE = Permissive X11-style
+LIBGC_LICENSE_FILES = README.md
+LIBGC_INSTALL_STAGING = YES
+
+$(eval $(autotools-package))
