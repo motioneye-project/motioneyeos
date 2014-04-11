230d1af54ecc2ac8a22f26d5d24836416e698bc0
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-04-11 07:55:05 -0300

    flac: bump to version 1.3.0
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/flac/flac.mk
+++ package/flac/flac.mk
@@ -4,19 +4,31 @@
 #
 ################################################################################
 
-FLAC_VERSION = 1.2.1
-FLAC_SITE = http://downloads.sourceforge.net/project/flac/flac-src/flac-$(FLAC_VERSION)-src
+FLAC_VERSION = 1.3.0
+FLAC_SITE = http://downloads.xiph.org/releases/flac
+FLAC_SOURCE = flac-$(FLAC_VERSION).tar.xz
 FLAC_INSTALL_STAGING = YES
-
+FLAC_AUTORECONF = YES
+FLAC_DEPENDENCIES = $(if $(BR2_PACKAGE_LIBICONV),libiconv)
+FLAC_LICENSE = Xiph BSD-like (libFLAC), GPLv2+ (tools), LGPLv2.1+ (other libraries)
+FLAC_LICENSE_FILES = COPYING.Xiph COPYING.GPL COPYING.LGPL
 FLAC_CONF_OPT = \
 	--disable-cpplibs \
-	--disable-xmms-plugin
+	--disable-xmms-plugin \
+	--disable-altivec
 
 ifeq ($(BR2_PACKAGE_LIBOGG),y)
 FLAC_CONF_OPT += --with-ogg=$(STAGING_DIR)/usr
-FLAC_DEPENDENCIES = libogg
+FLAC_DEPENDENCIES += libogg
 else
 FLAC_CONF_OPT += --disable-ogg
+endif
+
+ifeq ($(BR2_X86_CPU_HAS_SSE),y)
+FLAC_DEPENDENCIES += host-nasm
+FLAC_CONF_OPT += --enable-sse
+else
+FLAC_CONF_OPT += --disable-sse
 endif
 
 $(eval $(autotools-package))
