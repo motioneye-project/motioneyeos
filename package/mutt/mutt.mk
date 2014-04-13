0fcd010a2db771c259224ad1d025fb4c5a9baf3b
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-04-13 14:14:17 +0200

    mutt: Add options for imap/pop3 support, enable optional iconv/OpenSSL support
    [Thomas: keep a normal 'config' instead of 'menuconfig' for mutt.]
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/mutt/mutt.mk
+++ package/mutt/mutt.mk
@@ -9,7 +9,36 @@ MUTT_SITE = http://downloads.sourceforge.net/project/mutt/mutt
 MUTT_LICENSE = GPLv2+
 MUTT_LICENSE_FILES = GPL
 MUTT_DEPENDENCIES = ncurses
-MUTT_CONF_OPT = --disable-iconv --disable-smtp
+MUTT_CONF_OPT = --disable-smtp
 MUTT_AUTORECONF = YES
+
+ifeq ($(BR2_PACKAGE_LIBICONV),y)
+MUTT_DEPENDENCIES += libiconv
+MUTT_CONF_OPT += --enable-iconv
+endif
+
+ifeq ($(BR2_PACKAGE_MUTT_IMAP),y)
+MUTT_CONF_OPT += --enable-imap
+else
+MUTT_CONF_OPT += --disable-imap
+endif
+
+ifeq ($(BR2_PACKAGE_MUTT_POP3),y)
+MUTT_CONF_OPT += --enable-pop
+else
+MUTT_CONF_OPT += --disable-pop
+endif
+
+# SSL support is only used by imap or pop3 module
+ifneq ($(BR2_PACKAGET_MUTT_IMAP)$(BR2_PACKAGE_MUTT_POP3),)
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+MUTT_DEPENDENCIES += openssl
+MUTT_CONF_OPT += --with-ssl=$(STAGING_DIR)/usr
+else
+MUTT_CONF_OPT += --without-ssl
+endif
+else
+MUTT_CONF_OPT += --without-ssl
+endif
 
 $(eval $(autotools-package))
