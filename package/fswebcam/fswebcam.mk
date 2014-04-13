b6e3ada833b65fd79dcc5e10bd1e12f0774a6510
Baruch Siach <baruch@tkos.co.il>
2014-04-13 09:44:17 +0300

    fswebcam: bump version to 20140113
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/fswebcam/fswebcam.mk
+++ package/fswebcam/fswebcam.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FSWEBCAM_VERSION = 20110717
+FSWEBCAM_VERSION = 20140113
 FSWEBCAM_SITE = http://www.firestorm.cx/fswebcam/files/
 FSWEBCAM_LICENSE = GPLv2
 FSWEBCAM_LICENSE_FILES = LICENSE
