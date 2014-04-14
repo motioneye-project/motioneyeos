c50e7439d912cbc0fd48396cf7559cf477216670
Samuel Martin <s.martin49@gmail.com>
2014-04-14 23:05:52 +0200

    openpgm: make its build system python2/python3 friendly
    This patch fixes build issue when python3 is enabled, and host python2
    interpreter is not available.
    
    Fixes:
      http://autobuild.buildroot.net/results/01f/01f886a073439c0639ed93e596b68bcadf1a5824/
    
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>
    Cc: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/openpgm/openpgm.mk
+++ package/openpgm/openpgm.mk
@@ -12,6 +12,7 @@ OPENPGM_LICENSE_FILES = openpgm/pgm/LICENSE
 OPENPGM_INSTALL_STAGING = YES
 OPENPGM_AUTORECONF = YES
 OPENPGM_SUBDIR = openpgm/pgm/
+OPENPGM_DEPENDENCIES = $(if $(BR2_PACKAGE_PYTHON3),host-python3,host-python)
 OPENPGM_CONF_ENV = ac_cv_file__proc_cpuinfo=yes ac_cv_file__dev_rtc=no \
                    ac_cv_file__dev_hpet=no
 
