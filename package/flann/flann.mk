5fefbceeb68b2a3ee03dadc429fd5039ba9bc96f
Davide Viti <d.viti@infosolution.it>
2014-04-15 22:50:05 +0200

    flann: new package
    [Thomas: changed license from BSD to BSD-3c, to be more specific.]
    
    Signed-off-by: Davide Viti <zinosat@tiscali.it>
    CC: minimod@morethan.org
    CC: patrickdepinguin@gmail.com
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/flann/flann.mk
@@ -0,0 +1,23 @@
+###############################################################################
+#
+# flann
+#
+###############################################################################
+
+FLANN_VERSION = d0c04f4d290ebc3aa9411a3322992d298e51f5aa
+FLANN_SITE = $(call github,mariusmuja,flann,$(FLANN_VERSION))
+FLANN_INSTALL_STAGING = YES
+FLANN_LICENSE = BSD-3c
+FLANN_LICENSE_FILES = COPYING
+FLANN_CONF_OPT = \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DBUILD_C_BINDINGS=ON \
+	-DBUILD_PYTHON_BINDINGS=OFF \
+	-DBUILD_MATLAB_BINDINGS=OFF \
+	-DBUILD_EXAMPLES=$(if $(BR2_PACKAGE_FLANN_EXAMPLES),ON,OFF) \
+	-DBUILD_TESTS=OFF \
+	-DBUILD_DOC=OFF \
+	-DUSE_OPENMP=$(if $(BR2_GCC_ENABLE_OPENMP),ON,OFF) \
+	-DPYTHON_EXECUTABLE=OFF
+
+$(eval $(cmake-package))
