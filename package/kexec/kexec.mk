5416acb98cb6e1d7f9b6dd796fe8ae095851f91b
Sven Neumann <neumann@teufel.de>
2014-04-17 09:15:18 +0200

    kexec: bump to version 2.0.6
    Remove two patches which have been included upstream.
    
    Signed-off-by: Sven Neumann <neumann@teufel.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/kexec/kexec.mk
+++ package/kexec/kexec.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KEXEC_VERSION = 2.0.5
+KEXEC_VERSION = 2.0.6
 KEXEC_SOURCE = kexec-tools-$(KEXEC_VERSION).tar.xz
 KEXEC_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kernel/kexec
 
