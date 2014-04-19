afef09eab3e9ad5ed29738e9b7340bd8777b6858
Francois Perrad <fperrad@gmail.com>
2014-04-19 22:57:57 +0200

    wsapi: bump to version 1.6.1
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/wsapi/wsapi.mk
+++ package/wsapi/wsapi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WSAPI_VERSION = 1.6-1
+WSAPI_VERSION = 1.6.1-1
 WSAPI_LICENSE = MIT
 
 $(eval $(luarocks-package))
