90c72d8e817227fe95cf62edec8450124e8b6bb0
Francois Perrad <fperrad@gmail.com>
2014-04-19 22:57:56 +0200

    orbit: bump to version 2.2.1
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/orbit/orbit.mk
+++ package/orbit/orbit.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-ORBIT_VERSION_UPSTREAM = 2.2.0
-ORBIT_VERSION = $(ORBIT_VERSION_UPSTREAM)-2
-ORBIT_SUBDIR  = orbit-$(ORBIT_VERSION_UPSTREAM)
+ORBIT_VERSION_UPSTREAM = 2.2.1
+ORBIT_VERSION = $(ORBIT_VERSION_UPSTREAM)-1
+ORBIT_SUBDIR  = orbit
 ORBIT_LICENSE = MIT
 ORBIT_LICENSE_FILES = $(ORBIT_SUBDIR)/doc/us/license.md
 
