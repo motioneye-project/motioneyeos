54b6cd11341be1cf7cbb025e7be624487619976f
Francois Perrad <fperrad@gmail.com>
2014-04-19 22:57:59 +0200

    luafilesystem: bump rockspec revision
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/luafilesystem/luafilesystem.mk
+++ package/luafilesystem/luafilesystem.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUAFILESYSTEM_VERSION = 1.6.2-1
+LUAFILESYSTEM_VERSION = 1.6.2-2
 LUAFILESYSTEM_LICENSE = MIT
 
 $(eval $(luarocks-package))
