773ee9797ad4c74e967ad959f6ce0677b6c66677
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2014-04-20 20:54:03 +0200

    smack: new package.
    SMACK stands for Simplified Mandatory Access Control Kernel. It is a Linux
    Security Module which provides a Mandatory Access Control mechanism,
    like SELinux, but aiming towards simplicity.
    
    This package provides the tools to load/unload the policy from the
    kernel as well as a library allowing applications to interact with
    SMACK. The proper kernel options are also set.
    
    [Thomas:
    - fixed license to be LGPLv2.1 instead of LGPLv2.1+. Even though the
      debian/copyright file has the "or later" indication, none of the .c
      source files carry it, so I suppose LGPLv2.1 is more correct.
    - added !BR2_PREFER_STATIC_LIB dependency.
    - added dependency on host-pkgconf, since Smack configure.ac uses
      PKG_CHECK_MODULES.]
    
    Signed-off-by: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/smack/smack.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# smack
+#
+################################################################################
+
+SMACK_VERSION = 1.0.4
+SMACK_SITE = $(call github,smack-team,smack,v$(SMACK_VERSION))
+SMACK_LICENSE = LGPLv2.1
+SMACK_LICENSE_FILES = COPYING
+SMACK_INSTALL_STAGING = YES
+SMACK_DEPENDENCIES = host-pkgconf
+
+# Sources from github, no configure script included.
+SMACK_AUTORECONF = YES
+
+$(eval $(autotools-package))
