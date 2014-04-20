cbe4b0a71fdac86e6eaf09ee6ed5b8fa313287ac
Baruch Siach <baruch@tkos.co.il>
2014-04-20 10:22:39 +0300

    jansson: needs math library
    libjansson.so doesn't list libm in its NEEDED header. Fixes the following
    nftables config time failure:
    
    .../usr/lib/libjansson.so.4: undefined reference to `__isnan'
    .../usr/lib/libjansson.so.4: undefined reference to `__isinf'
    
    Fixes:
    http://autobuild.buildroot.net/results/152/1525d7a3e1d1fcf35858962251c0b69a5e1b64db/
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/jansson/jansson.mk
+++ package/jansson/jansson.mk
@@ -9,5 +9,6 @@ JANSSON_SITE = http://www.digip.org/jansson/releases
 JANSSON_LICENSE = MIT
 JANSSON_LICENSE_FILES = LICENSE
 JANSSON_INSTALL_STAGING = YES
+JANSSON_CONF_ENV = LIBS="-lm"
 
 $(eval $(autotools-package))
