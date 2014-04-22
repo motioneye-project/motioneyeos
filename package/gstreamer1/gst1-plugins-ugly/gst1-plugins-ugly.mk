af56ff42ab41b88d5d2a496495bc885dd4af02ae
Peter Korsgaard <peter@korsgaard.com>
2014-04-23 00:07:51 +0200

    gst1-plugins-ugly: bump version
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gstreamer1/gst1-plugins-ugly/gst1-plugins-ugly.mk
+++ package/gstreamer1/gst1-plugins-ugly/gst1-plugins-ugly.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_PLUGINS_UGLY_VERSION = 1.2.3
+GST1_PLUGINS_UGLY_VERSION = 1.2.4
 GST1_PLUGINS_UGLY_SOURCE = gst-plugins-ugly-$(GST1_PLUGINS_UGLY_VERSION).tar.xz
 GST1_PLUGINS_UGLY_SITE = http://gstreamer.freedesktop.org/src/gst-plugins-ugly
 GST1_PLUGINS_UGLY_LICENSE_FILES = COPYING
