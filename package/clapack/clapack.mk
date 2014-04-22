dd5dbe627f4d66603e03ae2fdb09d4fe34d34dc7
Samuel Martin <s.martin49@gmail.com>
2014-04-23 01:15:36 +0200

    clapack: new package
    This package provides BLAS and LAPACK libraries.
    
    Though it is common to find implementation of these two libraries in
    Fortran, this package provides a C-implementation for both, because:
    - Fortran support has been deprecated in Buildroot since the 2013.11
      release;
    - most of the external toolchains do not provide a Fortran compiler.
    
    Often BLAS build-systems build some test programs and run them to
    generate some source files or adjust some build optimizations, naively
    assuming they are building the library for the build-machine. This does
    not play well when cross-compiling.
    
    This implementation has this defect too, by building and running a tool
    generating a header.
    However, the build-system allows to pass an empty header.
    So, we have to patch the CMake to build the generator (but never
    install it) and correctly support building with and without this header
    provided by the user.
    
    Also, some CMake patches are needed to fix the build and install rules.
    
    [Peter: needs largefile, fix _LICENSE_FILES, tweak patch desc]
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/clapack/clapack.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# clapack
+#
+################################################################################
+
+CLAPACK_VERSION = 3.2.1
+CLAPACK_SOURCE = clapack-$(CLAPACK_VERSION)-CMAKE.tgz
+# This package provides 3 libraries:
+# - libf2c.a (not installed)
+# - libblas (statically linked with libf2c.a)
+# - liblapack (statically linked with libf2c.a)
+CLAPACK_LICENSE = HPND (libf2c), BSD-3c (libblas and liblapack)
+CLAPACK_LICENSE_FILES = F2CLIBS/libf2c/Notice COPYING
+CLAPACK_SITE = http://www.netlib.org/clapack
+CLAPACK_INSTALL_STAGING = YES
+CLAPACK_CONF_OPT = -DBUILDTESTING=OFF
+
+ifneq ($(BR2_PACKAGE_CLAPACK_ARITH_H),)
+CLAPACK_CONF_OPT += -DARITH_H=$(BR2_PACKAGE_CLAPACK_ARITH_H)
+endif
+
+$(eval $(cmake-package))
