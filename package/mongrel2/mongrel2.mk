2bddad55dbe7e29ef168b7b4ec2b4e7e8804419b
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2014-04-22 20:40:40 +0200

    mongrel2: bump to version v1.9.1
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/mongrel2/mongrel2.mk
+++ package/mongrel2/mongrel2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MONGREL2_VERSION = v1.9.0
+MONGREL2_VERSION = v1.9.1
 MONGREL2_SITE = https://github.com/zedshaw/mongrel2/releases/download/$(MONGREL2_VERSION)/
 MONGREL2_LICENSE = BSD-3c
 MONGREL2_LICENSE_FILES = LICENSE
