d20fdb8b305c0ac80a103ad55b994a2ede077d78
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-04-22 18:25:33 -0300

    acpid: bump to version 2.0.22
    Switch to upstream sourceforge page/download.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/acpid/acpid.mk
+++ package/acpid/acpid.mk
@@ -4,24 +4,23 @@
 #
 ################################################################################
 
-ACPID_VERSION = 2.0.11
-ACPID_SOURCE = acpid_$(ACPID_VERSION).orig.tar.gz
-ACPID_SITE = $(BR2_DEBIAN_MIRROR)/debian/pool/main/a/acpid
+ACPID_VERSION = 2.0.22
+ACPID_SOURCE = acpid-$(ACPID_VERSION).tar.xz
+ACPID_SITE = http://downloads.sourceforge.net/project/acpid2
 ACPID_LICENSE = GPLv2+
 ACPID_LICENSE_FILES = COPYING
 
-define ACPID_BUILD_CMDS
-	$(MAKE) CC="$(TARGET_CC)" -C $(@D)
+define ACPID_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 package/acpid/S02acpid \
+		$(TARGET_DIR)/etc/init.d/S02acpid
 endef
 
-define ACPID_INSTALL_TARGET_CMDS
-	install -D -m 755 $(@D)/acpid $(TARGET_DIR)/usr/sbin/acpid
-	install -D -m 755 $(@D)/acpi_listen $(TARGET_DIR)/usr/bin/acpi_listen
-	install -D -m 644 $(@D)/acpid.8 $(TARGET_DIR)/usr/share/man/man8/acpid.8
-	install -D -m 644 $(@D)/acpi_listen.8 $(TARGET_DIR)/usr/share/man/man8/acpi_listen.8
+define ACPID_SET_EVENTS
 	mkdir -p $(TARGET_DIR)/etc/acpi/events
-	/bin/echo -e "event=button[ /]power\naction=/sbin/poweroff" > $(TARGET_DIR)/etc/acpi/events/powerbtn
-	$(INSTALL) -D -m 0755 package/acpid/S02acpid $(TARGET_DIR)/etc/init.d/S02acpid
+	/bin/echo -e "event=button[ /]power\naction=/sbin/poweroff" \
+		>$(TARGET_DIR)/etc/acpi/events/powerbtn
 endef
 
-$(eval $(generic-package))
+ACPID_POST_INSTALL_TARGET_HOOKS += ACPID_SET_EVENTS
+
+$(eval $(autotools-package))
