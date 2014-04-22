4990a381e304a93f7ac125b1aed43df2f39d5bc6
Peter Seiderer <ps.report@gmx.net>
2014-04-23 00:12:23 +0200

    evtest: bump version to 1.31
    - update version to 1.31
    - correct license to GPLv2+
    - change download url to official repository
    - remove evtest-capture support (dropped since 1.31)
    - change broken project URL to official repository
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/evtest/evtest.mk
+++ package/evtest/evtest.mk
@@ -4,25 +4,13 @@
 #
 ################################################################################
 
-EVTEST_VERSION = 1.30
-EVTEST_SOURCE = evtest-$(EVTEST_VERSION).tar.bz2
-# no official upstream tarball
-EVTEST_SITE = http://pkgs.fedoraproject.org/repo/pkgs/evtest/evtest-1.30.tar.bz2/27c0902839babfd07136f232c63c895c/
-EVTEST_LICENSE = GPLv2
+EVTEST_VERSION = 1.31
+EVTEST_SOURCE = evtest-$(EVTEST_VERSION).tar.gz
+EVTEST_SITE = http://cgit.freedesktop.org/evtest/snapshot
+EVTEST_LICENSE = GPLv2+
 EVTEST_LICENSE_FILES = COPYING
 EVTEST_DEPENDENCIES = host-pkgconf
-
-ifeq ($(BR2_PACKAGE_EVTEST_CAPTURE),y)
-EVTEST_DEPENDENCIES += libxml2
-else
-# evtest-capture will unconditionally be built/installed if libxml2 is present
-define EVTEST_REMOVE_EVTEST_CAPTURE
-	rm -rf $(TARGET_DIR)/usr/bin/evtest-capture \
-	       $(TARGET_DIR)/usr/share/evtest \
-	       $(TARGET_DIR)/usr/share/man/man1/evtest-capture.1
-endef
-
-EVTEST_POST_INSTALL_TARGET_HOOKS += EVTEST_REMOVE_EVTEST_CAPTURE
-endif
+# needed because source package contains no generated files
+EVTEST_AUTORECONF = YES
 
 $(eval $(autotools-package))
