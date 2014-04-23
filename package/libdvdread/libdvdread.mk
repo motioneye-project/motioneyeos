3d31e98f8166e1c33c599d56342c442b47d5bfad
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-04-23 09:39:32 -0300

    libdvdread: bump to version 4.2.1
    And drop redundant LIBTOOL_PATCH = YES
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libdvdread/libdvdread.mk
+++ package/libdvdread/libdvdread.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-LIBDVDREAD_VERSION = 4.1.3
-LIBDVDREAD_SOURCE = libdvdread-$(LIBDVDREAD_VERSION).tar.bz2
+LIBDVDREAD_VERSION = 4.2.1
+LIBDVDREAD_SOURCE = libdvdread-$(LIBDVDREAD_VERSION).tar.xz
 LIBDVDREAD_SITE = http://dvdnav.mplayerhq.hu/releases
+# configure not shipped
 LIBDVDREAD_AUTORECONF = YES
-LIBDVDREAD_LIBTOOL_PATCH = YES
 LIBDVDREAD_INSTALL_STAGING = YES
 LIBDVDREAD_CONFIG_SCRIPTS = dvdread-config
 LIBDVDREAD_LICENSE = GPLv2+
