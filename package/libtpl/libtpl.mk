cc7e2babbefd2be47a2027a250603b1105f724a5
Simon Dawson <spdawson@gmail.com>
2014-04-26 09:08:20 +0100

    libtpl: bump version to v1.6.1
    Signed-off-by: Simon Dawson <spdawson@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libtpl/libtpl.mk
+++ package/libtpl/libtpl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBTPL_VERSION = 6df007e450cbee7d7a4896d3538128c5370b1c61
+LIBTPL_VERSION = v1.6.1
 LIBTPL_SITE = $(call github,troydhanson,tpl,$(LIBTPL_VERSION))
 LIBTPL_INSTALL_STAGING = YES
 LIBTPL_LICENSE = BSD-like
