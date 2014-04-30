947ca9e1207a906960efdeb1b377be57c0f305e2
Thomas De Schampheleire <patrickdepinguin@gmail.com>
2014-04-30 20:18:23 +0200

    vala: remove deprecated target package
    The vala target package has been deprecated since 2013.05 and thus can be
    removed in 2014.05. The host vala support is left untouched.
    
    Signed-off-by: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/vala/vala.mk
+++ package/vala/vala.mk
@@ -11,17 +11,9 @@ VALA_SOURCE = vala-$(VALA_VERSION).tar.xz
 VALA_LICENSE = LGPLv2.1+
 VALA_LICENSE_FILES = COPYING
 
-VALA_DEPENDENCIES = host-flex host-bison libglib2 \
-		$(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext)
-
-# Force Vala to not use the host xsltproc even if available, because it may or
-# may not work with Vala documentation (some versions of xsltproc segfault)
-VALA_CONF_ENV = ac_cv_path_XSLTPROC=:
-
 HOST_VALA_DEPENDENCIES = host-flex host-libglib2
 # Yes, the autoconf script understands ':' as "xsltproc is not
 # available".
 HOST_VALA_CONF_ENV = ac_cv_path_XSLTPROC=:
 
-$(eval $(autotools-package))
 $(eval $(host-autotools-package))
