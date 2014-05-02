5be988c560a84b46235d2724ad11458353384cac
Frank Hunleth <fhunleth@troodon-software.com>
2014-05-02 19:10:32 +0200

    package/ucl: new package
    UCL implements a number of compression algorithms that achieve an
    excellent compression ratio while allowing *very* fast decompression.
    Decompression requires no additional memory.
    
    This is needed for UPX, which is needed for syslinux 6.x.
    
    Signed-off-by: Frank Hunleth <fhunleth@troodon-software.com>
    [yann.morin.1998@free.fr: don't add a menuconfig entry, since it
     currently is for internal use only]
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Tested-by: Romain Naour <romain.naour@openwide.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/ucl/ucl.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# ucl
+#
+################################################################################
+
+UCL_VERSION = 1.03
+UCL_SITE = http://www.oberhumer.com/opensource/ucl/download/
+UCL_LICENSE = GPLv2+
+UCL_LICENSE_FILES = COPYING
+
+$(eval $(host-autotools-package))
