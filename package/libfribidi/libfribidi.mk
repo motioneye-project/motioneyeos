5693f236a2a0e3ec0a5166b403eec1c350a2d7c4
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-05-04 17:03:14 +0200

    libfribidi: bump version
    This patch also call autoreconf in order to use buildroot's libtool as
    buildroot's libtool patch doesn't apply to the upstream ltmain.sh.
    
    [Peter: clarified the need for AUTORECONF]
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libfribidi/libfribidi.mk
+++ package/libfribidi/libfribidi.mk
@@ -4,11 +4,13 @@
 #
 ################################################################################
 
-LIBFRIBIDI_VERSION = 0.19.5
+LIBFRIBIDI_VERSION = 0.19.6
 LIBFRIBIDI_SOURCE = fribidi-$(LIBFRIBIDI_VERSION).tar.bz2
 LIBFRIBIDI_SITE = http://www.fribidi.org/download/
 LIBFRIBIDI_LICENSE = LGPLv2.1+
 LIBFRIBIDI_LICENSE_FILES = COPYING
 LIBFRIBIDI_INSTALL_STAGING = YES
+# BR's libtool patch doesn't apply
+LIBFRIBIDI_AUTORECONF = YES
 
 $(eval $(autotools-package))
