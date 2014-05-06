d629983d892fdb7365de25ff6d8abc2fb7d748a2
Ryan Barnett <rjbarnet@rockwellcollins.com>
2014-05-06 10:42:38 -0500

    openssh: add license information
    Signed-off-by: Ryan Barnett <rjbarnet@rockwellcollins.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -6,6 +6,8 @@
 
 OPENSSH_VERSION = 6.6p1
 OPENSSH_SITE = http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
+OPENSSH_LICENSE = BSD-3c BSD-2c Public Domain
+OPENSSH_LICENSE_FILES = LICENCE
 OPENSSH_CONF_ENV = LD="$(TARGET_CC)" LDFLAGS="$(TARGET_CFLAGS)"
 OPENSSH_CONF_OPT = --disable-lastlog --disable-utmp \
 		--disable-utmpx --disable-wtmp --disable-wtmpx --disable-strip
