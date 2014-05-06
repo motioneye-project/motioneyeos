374e299f7dd22b4aa39da252c2e4edaaea52fa69
Phil Eichinger <phil.eichinger@gmail.com>
2014-05-06 22:24:59 +0200

    protobuf-c: project is on github now
    old release tag got lost in translation
    
    Signed-off-by: Phil Eichinger <phil@zankapfel.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/protobuf-c/protobuf-c.mk
+++ package/protobuf-c/protobuf-c.mk
@@ -4,14 +4,16 @@
 #
 ################################################################################
 
-PROTOBUF_C_VERSION = 0.15
-PROTOBUF_C_SITE = http://protobuf-c.googlecode.com/files
+PROTOBUF_C_VERSION = 1d1ad931dfda273bb12b9925d4b8cc413131af03
+PROTOBUF_C_SITE = $(call github,protobuf-c,protobuf-c,$(PROTOBUF_C_VERSION))
 PROTOBUF_C_DEPENDENCIES = host-protobuf-c
 HOST_PROTOBUF_C_DEPENDENCIES = host-protobuf
 PROTOBUF_C_MAKE = $(MAKE1)
 PROTOBUF_C_CONF_OPT = --disable-protoc
 PROTOBUF_C_INSTALL_STAGING = YES
 PROTOBUF_C_LICENSE = BSD-3c
+PROTOBUF_C_AUTORECONF = YES
+HOST_PROTOBUF_C_AUTORECONF = YES
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
