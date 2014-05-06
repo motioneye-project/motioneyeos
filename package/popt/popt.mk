769eb599579f0cee32997a2d242e91b0540620e5
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-05-06 20:59:50 +0200

    popt: fix build issue due to glob functions on Blackfin
    While the Blackfin external uClibc toolchains have <glob.h>, they
    don't implement all the functions needed by popt, causing build
    failures. One solution would have been to disable popt with such
    toolchains, but this would have meant propagating this additional
    dependency to the numerous reverse dependencies of popt. Instead, this
    commit chooses to make popt build on Blackfin external toolchains by
    disabling the usage of <glob.h> functions altogether.
    
    Fixes:
    
      http://autobuild.buildroot.org/results/bfc/bfcb2ed27debafc584e133f5ae11ad2061ad2b16/build-end.log
      http://autobuild.buildroot.org/results/410/410c8f3a0c7ca2c7098a47c30088038411635ae4/build-end.log
    
    and gazillion of similar failures.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/popt/popt.mk
+++ package/popt/popt.mk
@@ -13,6 +13,13 @@ POPT_LICENSE_FILES = COPYING
 
 POPT_CONF_ENV = ac_cv_va_copy=yes
 
+# The external Blackfin toolchains have <glob.h>, but not all the
+# necessary uClibc options for glob functions, so we force disable the
+# usage of the glob functions for these toolchains.
+ifeq ($(BR2_TOOLCHAIN_EXTERNAL_BLACKFIN_UCLINUX_2012R1)$(BR2_TOOLCHAIN_EXTERNAL_BLACKFIN_UCLINUX_2012R2)$(BR2_TOOLCHAIN_EXTERNAL_BLACKFIN_UCLINUX_2013R1),y)
+POPT_CONF_ENV += ac_cv_header_glob_h=no
+endif
+
 ifeq ($(BR2_PACKAGE_LIBICONV),y)
 POPT_CONF_ENV += am_cv_lib_iconv=yes
 POPT_CONF_OPT += --with-libiconv-prefix=$(STAGING_DIR)/usr
