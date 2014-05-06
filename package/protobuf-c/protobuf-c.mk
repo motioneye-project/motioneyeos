fcb5b619f8ad6a58f4afa88c46941848a58e40e0
Phil Eichinger <phil.eichinger@gmail.com>
2014-05-06 22:25:01 +0200

    protobuf-c: bump to version 1.0.0-rc1
    Signed-off-by: Phil Eichinger <phil@zankapfel.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/protobuf-c/protobuf-c.mk
+++ package/protobuf-c/protobuf-c.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PROTOBUF_C_VERSION = 1d1ad931dfda273bb12b9925d4b8cc413131af03
+PROTOBUF_C_VERSION = v1.0.0-rc1
 PROTOBUF_C_SITE = $(call github,protobuf-c,protobuf-c,$(PROTOBUF_C_VERSION))
 PROTOBUF_C_DEPENDENCIES = host-protobuf-c
-HOST_PROTOBUF_C_DEPENDENCIES = host-protobuf
+HOST_PROTOBUF_C_DEPENDENCIES = host-protobuf host-pkgconf
 PROTOBUF_C_MAKE = $(MAKE1)
 PROTOBUF_C_CONF_OPT = --disable-protoc
 PROTOBUF_C_INSTALL_STAGING = YES
