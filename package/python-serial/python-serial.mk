05b95a148a8928a83a88d4e6f4071fdc088281b2
Ryan Barnett <rjbarnet@rockwellcollins.com>
2014-05-06 10:42:42 -0500

    python-serial: add license information
    Signed-off-by: Ryan Barnett <rjbarnet@rockwellcollins.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-serial/python-serial.mk
+++ package/python-serial/python-serial.mk
@@ -7,6 +7,8 @@
 PYTHON_SERIAL_VERSION = 2.6
 PYTHON_SERIAL_SOURCE  = pyserial-$(PYTHON_SERIAL_VERSION).tar.gz
 PYTHON_SERIAL_SITE    = http://pypi.python.org/packages/source/p/pyserial
+PYTHON_SERIAL_LICENSE = Python Software Foundation License
+PYTHON_SERIAL_LICENSE_FILES = LICENSE.txt
 PYTHON_SERIAL_SETUP_TYPE = distutils
 
 $(eval $(python-package))
