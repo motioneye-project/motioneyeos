d7b03fc417d42d2fe6377673d0f07378e61a1d7d
Peter Korsgaard <peter@korsgaard.com>
2014-05-07 14:28:03 +0200

    jpeg-turbo: add license information
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/jpeg-turbo/jpeg-turbo.mk
+++ package/jpeg-turbo/jpeg-turbo.mk
@@ -7,6 +7,8 @@
 JPEG_TURBO_VERSION = 1.3.1
 JPEG_TURBO_SOURCE = libjpeg-turbo-$(JPEG_TURBO_VERSION).tar.gz
 JPEG_TURBO_SITE = http://downloads.sourceforge.net/project/libjpeg-turbo/$(JPEG_TURBO_VERSION)
+JPEG_TURBO_LICENSE = jpeg-license (BSD-3c-like)
+JPEG_TURBO_LICENSE_FILES = README README-turbo.txt
 JPEG_TURBO_INSTALL_STAGING = YES
 JPEG_TURBO_CONF_OPT = --with-jpeg8
 
