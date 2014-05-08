37b2613dfe95e0aad50b97e3d9d7ff9a1d9ef137
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-05-08 10:59:42 +0200

    libcec: Bump version
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libcec/libcec.mk
+++ package/libcec/libcec.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBCEC_VERSION = libcec-2.1.1
+LIBCEC_VERSION = libcec-2.1.4-repack
 LIBCEC_SITE = $(call github,Pulse-Eight,libcec,$(LIBCEC_VERSION))
 LIBCEC_LICENSE = GPLv2+
 LIBCEC_LICENSE_FILES = COPYING
