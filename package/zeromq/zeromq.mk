ebb5692f358833e4fb6f3ed5f3075241d80c6667
Arnout Vandecappelle <arnout@mind.be>
2014-05-09 01:24:27 +0200

    zeromq: explicitly add -lstdc++ for static build
    Fixes http://autobuild.buildroot.net/results/7c552e514f8c13ba8cdd420dc5bbf5edac915a83
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/zeromq/zeromq.mk
+++ package/zeromq/zeromq.mk
@@ -12,9 +12,15 @@ ZEROMQ_LICENSE = LGPLv3+ with exceptions
 ZEROMQ_LICENSE_FILES = COPYING COPYING.LESSER
 ZEROMQ_AUTORECONF = YES
 
+# Only tools/curve_keygen.c needs this, but it doesn't hurt to pass it
+# for the rest of the build as well (which automatically includes stdc++).
+ifeq ($(BR2_PREFER_STATIC_LIB),y)
+	ZEROMQ_CONF_OPT += LIBS=-lstdc++
+endif
+
 ifeq ($(BR2_PACKAGE_ZEROMQ_PGM),y)
 	ZEROMQ_DEPENDENCIES += host-pkgconf openpgm
-	ZEROMQ_CONF_OPT = --with-system-pgm
+	ZEROMQ_CONF_OPT += --with-system-pgm
 endif
 
 $(eval $(autotools-package))
