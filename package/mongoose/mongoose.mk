926affb40dba28696ef4a62aced946269c1f1a62
Davide Viti <d.viti@infosolution.it>
2014-05-12 21:31:26 +0200

    mongoose: bump to version 5.3
    Mongoose has changed quite drastically in the last months.
    Here is a list of changes which had to be applied:
    
     - Bump version from 3.7 to 5.3
     - use tarball download via github helper iso git clone
     - need largefile support
     - compilation takes place into "examples"
     - weberver source is now "server.c"
     - adapt to new command line options
     - SSL support now controlled via NS_ENABLE_SSL
    
    [Peter: extend commit text, use CFLAGS_EXTRA, only build server]
    Signed-off-by: Davide Viti <zinosat@tiscali.it>
    Cc: Will Wagner <will_wagner@carallon.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mongoose/mongoose.mk
+++ package/mongoose/mongoose.mk
@@ -4,10 +4,9 @@
 #
 ################################################################################
 
-MONGOOSE_VERSION = 3.7
-MONGOOSE_SOURCE = mongoose-$(MONGOOSE_VERSION).tgz
-MONGOOSE_SITE = https://mongoose.googlecode.com/files
-MONGOOSE_LICENSE = MIT
+MONGOOSE_VERSION = 5.3
+MONGOOSE_SITE = $(call github,cesanta,mongoose,$(MONGOOSE_VERSION))
+MONGOOSE_LICENSE = GPLv2
 MONGOOSE_LICENSE_FILES = LICENSE
 
 MONGOOSE_CFLAGS = $(TARGET_CFLAGS) $(TARGET_LDFLAGS)
@@ -15,18 +14,16 @@ MONGOOSE_CFLAGS = $(TARGET_CFLAGS) $(TARGET_LDFLAGS)
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 MONGOOSE_DEPENDENCIES += openssl
 # directly linked
-MONGOOSE_CFLAGS += -DNO_SSL_DL -lssl -lcrypto -lz
-else
-MONGOOSE_CFLAGS += -DNO_SSL
+MONGOOSE_CFLAGS += -DNS_ENABLE_SSL -lssl -lcrypto -lz
 endif
 
 define MONGOOSE_BUILD_CMDS
-	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) \
-		linux COPT="$(MONGOOSE_CFLAGS)"
+	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)/examples \
+		CFLAGS_EXTRA="$(MONGOOSE_CFLAGS)" server
 endef
 
 define MONGOOSE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/mongoose $(TARGET_DIR)/usr/sbin/mongoose
+	$(INSTALL) -D -m 755 $(@D)/examples/server $(TARGET_DIR)/usr/sbin/mongoose
 	$(INSTALL) -D -m 755 package/mongoose/S85mongoose \
 		$(TARGET_DIR)/etc/init.d/S85mongoose
 endef
