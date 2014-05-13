5035b7819ccda95f2b1bbc5fb99175f5dbe26b37
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-05-13 23:47:37 +0200

    libunwind: add patch to fix behavior for ARM < v6
    Since libatomic_ops does not implement real atomic operations for
    ARMv4 and ARMv5, libunwind must define AO_REQUIRE_CAS do indicate it
    requires compare-and-swap operations, even if not available as real
    atomic operations for the current architecture. In this case,
    libatomic_ops will rely on emulated atomic operations, which also
    require linking against libatomic_ops, which was until now not done by
    libunwind.
    
    This fixes the mysterious ltrace build issue:
    
      http://autobuild.buildroot.org/results/e1b/e1b330abfa2d80f3f30bc3359428ea429c690eb8/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libunwind/libunwind.mk
+++ package/libunwind/libunwind.mk
@@ -9,5 +9,6 @@ LIBUNWIND_SITE = http://download.savannah.gnu.org/releases/libunwind
 LIBUNWIND_INSTALL_STAGING = YES
 LIBUNWIND_LICENSE_FILES = COPYING
 LIBUNWIND_LICENSE = MIT
+LIBUNWIND_AUTORECONF = YES
 
 $(eval $(autotools-package))
