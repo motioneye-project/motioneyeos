5ff8a7156b9591da9a5c9a2ebc7b690f7ccd87ce
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-05-17 18:53:30 +0200

    xlib_libXpm: fix build on noMMU architectures
    Add a patch to libXpm to fix build on noMMU architectures.
    
    Fixes:
    
      http://autobuild.buildroot.org/results/c57/c571b0a9d9f36a445ea36a4bd609154f9a9bddd5/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xlib_libXpm/xlib_libXpm.mk
+++ package/x11r7/xlib_libXpm/xlib_libXpm.mk
@@ -10,6 +10,8 @@ XLIB_LIBXPM_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXPM_LICENSE = MIT
 XLIB_LIBXPM_LICENSE_FILES = COPYING
 XLIB_LIBXPM_INSTALL_STAGING = YES
+# we patch configure.ac
+XLIB_LIBXPM_AUTORECONF = YES
 XLIB_LIBXPM_DEPENDENCIES = xlib_libX11 xlib_libXext xlib_libXt xproto_xproto \
 	host-gettext
 
