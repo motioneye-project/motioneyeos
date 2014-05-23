b19399cc1de0d27543ed97efad5dd9adfd04f7c0
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-05-23 09:14:04 -0300

    rdesktop: security bump to version 1.8.2
    Long overdue, fixes CVE-2008-1801, CVE-2008-1802, CVE-2008-1803 and
    CVE-2011-1595.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rdesktop/rdesktop.mk
+++ package/rdesktop/rdesktop.mk
@@ -4,11 +4,20 @@
 #
 ################################################################################
 
-RDESKTOP_VERSION = 1.5.0
+RDESKTOP_VERSION = 1.8.2
 RDESKTOP_SITE = http://downloads.sourceforge.net/project/rdesktop/rdesktop/$(RDESKTOP_VERSION)
-RDESKTOP_DEPENDENCIES = openssl xlib_libX11 xlib_libXt
-RDESKTOP_CONF_OPT = --with-openssl=$(STAGING_DIR)/usr
-RDESKTOP_LICENSE = GPLv2+ with exceptions
+RDESKTOP_DEPENDENCIES = host-pkgconf openssl xlib_libX11 xlib_libXt \
+	$(if $(BR2_PACKAGE_ALSA_LIB_PCM),alsa-lib) \
+	$(if $(BR2_PACKAGE_LIBAO),libao) \
+	$(if $(BR2_PACKAGE_LIBSAMPLERATE),libsamplerate)
+RDESKTOP_CONF_OPT = --with-openssl=$(STAGING_DIR)/usr --disable-credssp
+RDESKTOP_LICENSE = GPLv3+
 RDESKTOP_LICENSE_FILES = COPYING
+
+ifeq ($(BR2_PACKAGE_PCSC_LITE),y)
+RDESKTOP_DEPENDENCIES += pcsc-lite
+else
+RDESKTOP_CONF_OPT += --disable-smartcard
+endif
 
 $(eval $(autotools-package))
