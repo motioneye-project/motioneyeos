b9824b3631a183c3594fda8c021e334b51b7c0c6
Heiko Abraham <abrahamh@web.de>
2014-05-24 15:49:43 +0200

    hwdata: add license information
    Add missing license info and file for package 'hwdata'
    
    [Peter: GPLv2 should be '+' variant, clarify that it is dual licensed]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/hwdata/hwdata.mk
+++ package/hwdata/hwdata.mk
@@ -8,6 +8,8 @@ HWDATA_VERSION = 0.230
 HWDATA_SOURCE = hwdata_$(HWDATA_VERSION).orig.tar.gz
 HWDATA_PATCH = hwdata_$(HWDATA_VERSION)-1.diff.gz
 HWDATA_SITE = $(BR2_DEBIAN_MIRROR)/debian/pool/main/h/hwdata/
+HWDATA_LICENSE = GPLv2+ or XFree86 1.0 license
+HWDATA_LICENSE_FILES = COPYING LICENSE
 
 define HWDATA_INSTALL_TARGET_CMDS
 	install -D -m 644 $(@D)/pci.ids $(TARGET_DIR)/usr/share/hwdata/pci.ids
