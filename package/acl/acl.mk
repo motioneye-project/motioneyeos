6783c64d0fee12ff107ef9fbdf14f07b8901256b
Heiko Abraham <abrahamh@web.de>
2014-05-24 15:16:52 +0200

    acl: add license information
    Add missing license info and file for package 'acl'
    
    [Peter: change to '+' version of licenses]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/acl/acl.mk
+++ package/acl/acl.mk
@@ -10,6 +10,8 @@ ACL_SITE = http://download.savannah.gnu.org/releases/acl
 ACL_INSTALL_STAGING = YES
 ACL_DEPENDENCIES = attr
 ACL_CONF_OPT = --enable-gettext=no
+ACL_LICENSE = GPLv2+, LGPLv2.1+
+ACL_LICENSE_FILES = doc/COPYING doc/COPYING.LGPL
 
 # While the configuration system uses autoconf, the Makefiles are
 # hand-written and do not use automake. Therefore, we have to hack
