f1a3e0cb1be043bb8efe91a415615440f7ba94a4
Peter Korsgaard <peter@korsgaard.com>
2014-05-26 11:46:06 +0200

    acl: clarify license info
    As pointed out by Thomas Petazzoni in his review.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/acl/acl.mk
+++ package/acl/acl.mk
@@ -10,7 +10,7 @@ ACL_SITE = http://download.savannah.gnu.org/releases/acl
 ACL_INSTALL_STAGING = YES
 ACL_DEPENDENCIES = attr
 ACL_CONF_OPT = --enable-gettext=no
-ACL_LICENSE = GPLv2+, LGPLv2.1+
+ACL_LICENSE = GPLv2+ (programs), LGPLv2.1+ (libraries)
 ACL_LICENSE_FILES = doc/COPYING doc/COPYING.LGPL
 
 # While the configuration system uses autoconf, the Makefiles are
