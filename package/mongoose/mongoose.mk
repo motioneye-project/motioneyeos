66d364f4f21924af7f5273fea7b96293a1a463a9
Davide Viti <zinosat@tiscali.it>
2014-05-27 11:31:43 +0200

    mongoose: provide libmongoose.a static library
    The package provides a webserver and, eventually, a library: this is
    what Centos, among other distros, is doing.
    
    [Peter: correct install -D invocation]
    Signed-off-by: Davide Viti <d.viti@infosolution.it>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mongoose/mongoose.mk
+++ package/mongoose/mongoose.mk
@@ -8,6 +8,7 @@ MONGOOSE_VERSION = 5.3
 MONGOOSE_SITE = $(call github,cesanta,mongoose,$(MONGOOSE_VERSION))
 MONGOOSE_LICENSE = GPLv2
 MONGOOSE_LICENSE_FILES = LICENSE
+MONGOOSE_INSTALL_STAGING = YES
 
 MONGOOSE_CFLAGS = $(TARGET_CFLAGS) $(TARGET_LDFLAGS)
 
@@ -20,12 +21,22 @@ endif
 define MONGOOSE_BUILD_CMDS
 	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)/examples \
 		CFLAGS_EXTRA="$(MONGOOSE_CFLAGS)" server
+	$(TARGET_CC) -c $(@D)/mongoose.c $(MONGOOSE_CFLAGS) -o $(@D)/mongoose.o
+	$(TARGET_AR) rcs $(@D)/libmongoose.a $(@D)/mongoose.o
 endef
 
 define MONGOOSE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/examples/server $(TARGET_DIR)/usr/sbin/mongoose
+	$(INSTALL) -D -m 755 $(@D)/examples/server \
+		$(TARGET_DIR)/usr/sbin/mongoose
 	$(INSTALL) -D -m 755 package/mongoose/S85mongoose \
 		$(TARGET_DIR)/etc/init.d/S85mongoose
+endef
+
+define MONGOOSE_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 644 $(@D)/libmongoose.a \
+		$(STAGING_DIR)/usr/lib/libmongoose.a
+	$(INSTALL) -D -m 644 $(@D)/mongoose.h \
+		$(STAGING_DIR)/usr/include/mongoose.h
 endef
 
 $(eval $(generic-package))
