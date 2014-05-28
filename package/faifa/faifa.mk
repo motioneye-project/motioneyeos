0bd7eb8effd898e796d77986c53e9520807d34ba
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-05-28 15:39:06 +0200

    faifa: new package
    faifa is a library and a cli used to manage PLC hardware that use
    Intellon chipset.  Almost everybody use theses chips nowadays.
    
    [Thomas: license is GPLv2+, not GPLv2. Fix indentation. Add dependency
    on host-autoconf and a comment to explain what's going on. Fix
    indentation of target/staging installation commands. Mark the package
    as not available for static library builds as it always build a shared
    library. Rewrap Config.in help text.]
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/faifa/faifa.mk
@@ -0,0 +1,39 @@
+################################################################################
+#
+# faifa
+#
+################################################################################
+
+FAIFA_VERSION = aa0b5f90b89a9fd1533137b68ebf85f5437aba53
+FAIFA_SITE = $(call github,ffainelli,faifa,$(FAIFA_VERSION))
+FAIFA_INSTALL_STAGING = YES
+FAIFA_DEPENDENCIES = libpcap host-autoconf
+FAIFA_LICENSE = GPLv2+
+FAIFA_LICENSE_FILES = COPYING
+
+# This package uses autoconf, but not automake, so we need to call
+# their special autogen.sh script, and have custom target and staging
+# installation commands.
+
+define FAIFA_RUN_AUTOGEN
+	cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
+endef
+FAIFA_PRE_CONFIGURE_HOOKS += FAIFA_RUN_AUTOGEN
+
+define FAIFA_INSTALL_TARGET_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
+		PREFIX=/usr \
+		STRIP=/bin/true \
+		DESTDIR=$(TARGET_DIR) \
+		install
+endef
+
+define FAIFA_INSTALL_STAGING_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
+		PREFIX=/usr \
+		STRIP=/bin/true \
+		DESTDIR=$(STAGING_DIR) \
+		install
+endef
+
+$(eval $(autotools-package))
