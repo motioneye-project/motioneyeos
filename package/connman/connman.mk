8b8174e714118a559d7cb196d89a8303c0122bf3
Heiko Abraham <abrahamh@web.de>
2014-05-30 17:26:21 +0200

    package/connman : enable build without gnutls dependency
    - add new configuration option '--enable-wispr' (disabled by default)
    - move gnutls dependency to "wispr" config option
    
    [Peter: drop gnutls ref for wchar, tweak help text, simplify .mk]
    Signed-off-by: Heiko Abraham <abrahamh@web.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/connman/connman.mk
+++ package/connman/connman.mk
@@ -7,7 +7,7 @@
 CONNMAN_VERSION = 1.23
 CONNMAN_SOURCE = connman-$(CONNMAN_VERSION).tar.xz
 CONNMAN_SITE = $(BR2_KERNEL_MIRROR)/linux/network/connman/
-CONNMAN_DEPENDENCIES = libglib2 dbus iptables gnutls
+CONNMAN_DEPENDENCIES = libglib2 dbus iptables
 CONNMAN_INSTALL_STAGING = YES
 CONNMAN_LICENSE = GPLv2
 CONNMAN_LICENSE_FILES = COPYING
@@ -15,6 +15,7 @@ CONNMAN_CONF_OPT += --localstatedir=/var \
 	$(if $(BR2_PACKAGE_CONNMAN_DEBUG),--enable-debug,--disable-debug)		\
 	$(if $(BR2_PACKAGE_CONNMAN_ETHERNET),--enable-ethernet,--disable-ethernet)	\
 	$(if $(BR2_PACKAGE_CONNMAN_WIFI),--enable-wifi,--disable-wifi)			\
+	$(if $(BR2_PACKAGE_CONNMAN_WISPR),--enable-wispr,--disable-wispr) \
 	$(if $(BR2_PACKAGE_CONNMAN_BLUETOOTH),--enable-bluetooth,--disable-bluetooth)	\
 	$(if $(BR2_PACKAGE_CONNMAN_LOOPBACK),--enable-loopback,--disable-loopback)	\
 	$(if $(BR2_PACKAGE_CONNMAN_NEARD),--enable-neard,--disable-neard) \
@@ -23,7 +24,8 @@ CONNMAN_CONF_OPT += --localstatedir=/var \
 
 CONNMAN_DEPENDENCIES += \
 	$(if $(BR2_PACKAGE_CONNMAN_NEARD),neard) \
-	$(if $(BR2_PACKAGE_CONNMAN_OFONO),ofono)
+	$(if $(BR2_PACKAGE_CONNMAN_OFONO),ofono) \
+	$(if $(BR2_PACKAGE_CONNMAN_WISPR),gnutls)
 
 define CONNMAN_INSTALL_INIT_SYSV
 	$(INSTALL) -m 0755 -D package/connman/S45connman $(TARGET_DIR)/etc/init.d/S45connman
