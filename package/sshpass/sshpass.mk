ffe60b1bfa062ae73f0db65c44ebd1781722cced
Gary Bisson <bisson.gary@gmail.com>
2014-05-31 15:32:16 -0700

    sshpass: new package
    [Thomas: add MMU dependency, fix license to GPLv2+.]
    
    Signed-off-by: Gary Bisson <bisson.gary@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/sshpass/sshpass.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# sshpass
+#
+################################################################################
+
+SSHPASS_VERSION = 1.05
+SSHPASS_SITE = http://downloads.sourceforge.net/project/sshpass/sshpass/$(SSHPASS_VERSION)
+SSHPASS_LICENSE = GPLv2+
+SSHPASS_LICENSE_FILES = COPYING
+
+$(eval $(autotools-package))
