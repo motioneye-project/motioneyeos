e9f28b7c8caa4091836866f3323cb7905257539c
Yann E. MORIN <yann.morin.1998@free.fr>
2014-06-01 23:43:41 +0200

    package/snappy: bump version
    This is the un-tagged 1.1.2 release.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/snappy/snappy.mk
+++ package/snappy/snappy.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-SNAPPY_VERSION = 1.1.1
-SNAPPY_SITE = http://snappy.googlecode.com/files/
+SNAPPY_VERSION = 1ff9be9b8fafc8528ca9e055646f5932aa5db9c4
+SNAPPY_SITE = $(call github,google,snappy,$(SNAPPY_VERSION))
 SNAPPY_LICENSE = BSD-3c
 SNAPPY_LICENSE_FILES = COPYING
 SNAPPY_INSTALL_STAGING = YES
