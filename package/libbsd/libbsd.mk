8862585c278888cec01037080f770561b669a0f0
Yann E. MORIN <yann.morin.1998@free.fr>
2014-06-01 23:43:38 +0200

    package/libbsd: do not line-up variables
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libbsd/libbsd.mk
+++ package/libbsd/libbsd.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-LIBBSD_VERSION         = 0.4.2
-LIBBSD_SITE            = http://libbsd.freedesktop.org/releases
-LIBBSD_LICENSE         = BSD-3c MIT
-LIBBSD_LICENSE_FILES   = COPYING
+LIBBSD_VERSION = 0.4.2
+LIBBSD_SITE = http://libbsd.freedesktop.org/releases
+LIBBSD_LICENSE = BSD-3c MIT
+LIBBSD_LICENSE_FILES = COPYING
 
 LIBBSD_INSTALL_STAGING = YES
 
