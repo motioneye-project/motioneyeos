40ca4044bdeea7d5501e025d30e65f35e30442e8
Yann E. MORIN <yann.morin.1998@free.fr>
2014-06-01 23:43:40 +0200

    package/snappy: do not line-up variables
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/snappy/snappy.mk
+++ package/snappy/snappy.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-SNAPPY_VERSION		= 1.1.1
-SNAPPY_SITE		= http://snappy.googlecode.com/files/
-SNAPPY_LICENSE		= BSD-3c
-SNAPPY_LICENSE_FILES	= COPYING
-SNAPPY_INSTALL_STAGING	= YES
+SNAPPY_VERSION = 1.1.1
+SNAPPY_SITE = http://snappy.googlecode.com/files/
+SNAPPY_LICENSE = BSD-3c
+SNAPPY_LICENSE_FILES = COPYING
+SNAPPY_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
