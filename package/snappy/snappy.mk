88107de5522725a3d5d5b492a696eb76e14f5e15
Peter Korsgaard <peter@korsgaard.com>
2014-06-02 21:17:10 +0200

    snappy: needs autoreconf
    Fixes http://autobuild.buildroot.net/results/0ec/0ec62efed0e0aec56b4aa6b5ef1a3bcbd1474949/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/snappy/snappy.mk
+++ package/snappy/snappy.mk
@@ -8,6 +8,8 @@ SNAPPY_VERSION = 1ff9be9b8fafc8528ca9e055646f5932aa5db9c4
 SNAPPY_SITE = $(call github,google,snappy,$(SNAPPY_VERSION))
 SNAPPY_LICENSE = BSD-3c
 SNAPPY_LICENSE_FILES = COPYING
+# from git
+SNAPPY_AUTORECONF = YES
 SNAPPY_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
