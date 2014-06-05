70bd2d9ab5ca4261cade36aa671675dea52688b6
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-05 07:51:35 -0300

    python-bottle: security bump to version 0.12.7
    Fixes CVE-2014-3137.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-bottle/python-bottle.mk
+++ package/python-bottle/python-bottle.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_BOTTLE_VERSION = 0.11.6
+PYTHON_BOTTLE_VERSION = 0.12.7
 PYTHON_BOTTLE_SOURCE = bottle-$(PYTHON_BOTTLE_VERSION).tar.gz
 PYTHON_BOTTLE_SITE = http://pypi.python.org/packages/source/b/bottle
 PYTHON_BOTTLE_LICENSE = MIT
