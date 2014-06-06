beb49d882e03d4596d40dafad21e50460a6f1ef0
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2014-06-06 14:44:27 +0100

    gnupg2: new package
    [Thomas: use libpthsem instead of pth, remove !uclibc dependency,
    minor formatting fixes in the .mk file.]
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Reviewed-by: Markos Chandras <Markos.Chandras@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/gnupg2/gnupg2.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# gnupg2
+#
+################################################################################
+
+GNUPG2_VERSION = 2.0.23
+GNUPG2_SOURCE = gnupg-$(GNUPG2_VERSION).tar.bz2
+GNUPG2_SITE = ftp://ftp.gnupg.org/gcrypt/gnupg
+GNUPG2_LICENSE = GPLv3+
+GNUPG2_LICENSE_FILES = COPYING
+GNUPG2_DEPENDENCIES = zlib libgpg-error libgcrypt libassuan libksba libpthsem
+GNUPG2_CONF_OPT = \
+	--disable-rpath --disable-regex --disable-doc \
+	--with-libgpg-error-prefix=$(STAGING_DIR)/usr \
+	--with-libgcrypt-prefix=$(STAGING_DIR)/usr \
+	--with-libassuan-prefix=$(STAGING_DIR)/usr \
+	--with-ksba-prefix=$(STAGING_DIR)/usr \
+	--with-pth-prefix=$(STAGING_DIR)/usr
+
+ifneq ($(BR2_PACKAGE_GNUPG2_GPGV2),y)
+define GNUPG2_REMOVE_GPGV2
+	rm -f $(TARGET_DIR)/usr/bin/gpgv2
+endef
+GNUPG2_POST_INSTALL_TARGET_HOOKS += GNUPG2_REMOVE_GPGV2
+endif
+
+$(eval $(autotools-package))
