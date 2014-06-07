7d48b6aeae11e5313ca41aaf2d0fbccb5575be60
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-07 15:20:20 -0300

    spawn-fcgi: bump to version 1.6.4
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/spawn-fcgi/spawn-fcgi.mk
+++ package/spawn-fcgi/spawn-fcgi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SPAWN_FCGI_VERSION = 1.6.3
+SPAWN_FCGI_VERSION = 1.6.4
 SPAWN_FCGI_SITE = http://www.lighttpd.net/download
 SPAWN_FCGI_SOURCE = spawn-fcgi-$(SPAWN_FCGI_VERSION).tar.bz2
 SPAWN_FCGI_LICENSE = BSD-3c
