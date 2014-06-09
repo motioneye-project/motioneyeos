c4c8e74f8508f738dfb520758c7198fd9f69c1fc
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-06-09 11:54:35 +0200

    cloog: remove INSTALL_STAGING = YES
    The package is host-only, so there's no point in specifying
    INSTALL_STAGING = YES.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/cloog/cloog.mk
+++ package/cloog/cloog.mk
@@ -6,7 +6,6 @@
 
 CLOOG_VERSION = 0.18.2
 CLOOG_SITE = http://www.bastoul.net/cloog/pages/download/
-CLOOG_INSTALL_STAGING = YES
 CLOOG_LICENSE = LGPLv2.1+
 CLOOG_DEPENDENCIES = gmp isl
 # Our libtool patch doesn't apply, and since this package is only
