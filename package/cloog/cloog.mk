1ed9a57180a12dd13f207b104c5c928b221efd70
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-06-09 11:51:44 +0200

    cloog: fix license details
    License is in fact LGPLv2.1+, and there is no LICENSE file around.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/cloog/cloog.mk
+++ package/cloog/cloog.mk
@@ -7,8 +7,7 @@
 CLOOG_VERSION = 0.18.2
 CLOOG_SITE = http://www.bastoul.net/cloog/pages/download/
 CLOOG_INSTALL_STAGING = YES
-CLOOG_LICENSE = LGPLv2.1
-CLOOG_LICENSE_FILES = LICENSE
+CLOOG_LICENSE = LGPLv2.1+
 CLOOG_DEPENDENCIES = gmp isl
 # Our libtool patch doesn't apply, and since this package is only
 # built for the host, we don't really care about it.
