f20f48e5e476d279d50d74a4e25bb86e929c1e11
Yegor Yefremov <yegorslists@googlemail.com>
2014-06-10 08:30:05 +0200

    kexec: bump to 2.0.7
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/kexec/kexec.mk
+++ package/kexec/kexec.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KEXEC_VERSION = 2.0.6
+KEXEC_VERSION = 2.0.7
 KEXEC_SOURCE = kexec-tools-$(KEXEC_VERSION).tar.xz
 KEXEC_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kernel/kexec
 
