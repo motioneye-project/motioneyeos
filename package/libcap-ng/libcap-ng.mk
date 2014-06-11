d0da839ff84aea159ec557958a72b208a5e94651
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-11 09:55:56 -0300

    libcap-ng: security bump to version 0.7.4
    Fixes CVE-2014-3215.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libcap-ng/libcap-ng.mk
+++ package/libcap-ng/libcap-ng.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LIBCAP_NG_VERSION = 0.7.3
-LIBCAP_NG_SITE = http://people.redhat.com/sgrubb/libcap-ng/
+LIBCAP_NG_VERSION = 0.7.4
+LIBCAP_NG_SITE = http://people.redhat.com/sgrubb/libcap-ng
 LIBCAP_NG_LICENSE = GPLv2+ LGPLv2.1+
 LIBCAP_NG_LICENSE_FILES = COPYING COPYING.LIB
 LIBCAP_NG_INSTALL_STAGING = YES
