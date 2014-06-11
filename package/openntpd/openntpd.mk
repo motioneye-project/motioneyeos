fb6c286697c5f579759fd279ea421667755700ef
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-11 08:33:15 -0300

    openntpd: add SysV initscript and user
    Add the _ntp user that the daemon requires to work in any way.
    Add a SysV-style initscript as well, but not with '-s' (set time
    immediately after startup) because it can make the boot process stall
    for a few seconds if there's no proper network connectivity/dns setup.
    Make ntp and openntpd mutually exclusive since they overstep each other.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/openntpd/openntpd.mk
+++ package/openntpd/openntpd.mk
@@ -10,4 +10,13 @@ OPENNTPD_CONF_OPT = --with-builtin-arc4random --disable-strip
 OPENNTPD_LICENSE = MIT-like, BSD-2c, BSD-3c
 OPENNTPD_LICENSE_FILES = LICENCE
 
+define OPENNTPD_INSTALL_INIT_SYSV
+	$(INSTALL) -m 0755 -D package/openntpd/S49ntp \
+		$(TARGET_DIR)/etc/init.d/S49ntp
+endef
+
+define OPENNTPD_USERS
+_ntp -1 _ntp -1 * - - - Network Time Protocol daemon
+endef
+
 $(eval $(autotools-package))
