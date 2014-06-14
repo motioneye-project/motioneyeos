9c080f34076cdf48bcf72e1a1fbe2024b47a2694
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-14 07:56:48 -0300

    dialog: fix build failure
    The old ncurses trick is no longer needed for this version, in fact it's
    harmful. Switch to proper configure options.
    Also disable rpath hackery since it's not required and could be
    problematic. Fixes:
    http://autobuild.buildroot.net/results/411/411f6171e972eab4486143dedbfd078136886ab0/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/dialog/dialog.mk
+++ package/dialog/dialog.mk
@@ -7,9 +7,9 @@
 DIALOG_VERSION = 1.2-20140219
 DIALOG_SOURCE = dialog-$(DIALOG_VERSION).tgz
 DIALOG_SITE = ftp://invisible-island.net/dialog
-DIALOG_CONF_OPT = --with-ncurses
-DIALOG_CONF_ENV = ac_cv_path_NCURSES_CONFIG=true LIBS=-lncurses
-DIALOG_DEPENDENCIES = ncurses
+DIALOG_CONF_OPT = --with-ncurses --with-curses-dir=$(STAGING_DIR)/usr \
+	--disable-rpath-hack
+DIALOG_DEPENDENCIES = host-pkgconf ncurses
 DIALOG_LICENSE = LGPLv2.1
 DIALOG_LICENSE_FILES = COPYING
 
