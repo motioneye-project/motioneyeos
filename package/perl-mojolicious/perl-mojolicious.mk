970b061e6701ee2939d0a9c4543048392e9814ac
Francois Perrad <fperrad@gmail.com>
2014-06-16 21:35:57 +0200

    perl-mojolicious: new package
    [Thomas: as noted by Arnout, remove trailing whitespace, and fix the
    license to Artistic-2.0. Also, adjust the indentation in
    package/Config.in to the new standard.]
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/perl-mojolicious/perl-mojolicious.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-mojolicious
+#
+################################################################################
+
+PERL_MOJOLICIOUS_VERSION = 5.07
+PERL_MOJOLICIOUS_SOURCE = Mojolicious-$(PERL_MOJOLICIOUS_VERSION).tar.gz
+PERL_MOJOLICIOUS_SITE = $(BR2_CPAN_MIRROR)/authors/id/S/SR/SRI/
+PERL_MOJOLICIOUS_DEPENDENCIES = perl
+PERL_MOJOLICIOUS_LICENSE = Artistic-2.0
+
+$(eval $(perl-package))
