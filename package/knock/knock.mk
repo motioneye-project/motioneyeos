116f050273d3b4dac3c3831c5a8ace097a2b04c4
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-16 14:13:53 -0300

    knock: bump to version 0.7
    Bump knock to version 0.7 and switch away from a git snapshot, it's
    nicer and avoids the need to autoreconf.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/knock/knock.mk
+++ package/knock/knock.mk
@@ -4,11 +4,10 @@
 #
 ################################################################################
 
-KNOCK_VERSION = 7ce33cd553800f48f16b3bb70b9cd0f1310d2c18
-KNOCK_SITE = $(call github,jvinet,knock,$(KNOCK_VERSION))
+KNOCK_VERSION = 0.7
+KNOCK_SITE = http://www.zeroflux.org/proj/knock/files
 KNOCK_LICENSE = GPLv2+
 KNOCK_LICENSE_FILES = COPYING
-KNOCK_AUTORECONF = YES
 KNOCK_DEPENDENCIES = libpcap
 
 $(eval $(autotools-package))
