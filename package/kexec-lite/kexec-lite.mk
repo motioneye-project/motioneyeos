e36e7db93300338842afd0264faa88343e8a2d0d
Jeremy Kerr <jk@ozlabs.org>
2014-06-17 13:21:47 +0800

    package/kexec-lite: Add a package for the kexec-lite tools
    Kexec-lite is a tiny impementation of kexec for devicetree-based
    platforms.
    
    [Thomas:
    - Add !BR2_PREFER_STATIC_LIB dependency, inherited from the dtc
      package
    - Fix license to be GPLv2+, and not just GPL.
    - Use $(TARGET_CONFIGURE_OPTS) instead of manually passing CC, LD,
      CFLAGS.
    - Use a full path as the target of $(INSTALL)]
    
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/kexec-lite/kexec-lite.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# kexec-lite
+#
+################################################################################
+
+KEXEC_LITE_VERSION = fb8543fea3beb0522b5a63a74ea1a845dbd7b954
+KEXEC_LITE_SITE = $(call github,antonblanchard,kexec-lite,$(KEXEC_LITE_VERSION))
+KEXEC_LITE_LICENSE = GPLv2+
+KEXEC_LITE_DEPENDENCIES = libelf dtc
+
+define KEXEC_LITE_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
+
+define KEXEC_LITE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/kexec $(TARGET_DIR)/usr/sbin/kexec
+endef
+
+$(eval $(generic-package))
