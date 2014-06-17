4ff0323105d1f978c75a1adb73f744a6324369d8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-17 19:23:19 -0300

    blackfin: 2012R1 toolchain is gone, remove kludges
    Remove ADI toolchain 2012R1 package kludges since that version is gone
    since the last bump.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/popt/popt.mk
+++ package/popt/popt.mk
@@ -16,7 +16,7 @@ POPT_CONF_ENV = ac_cv_va_copy=yes
 # The external Blackfin toolchains have <glob.h>, but not all the
 # necessary uClibc options for glob functions, so we force disable the
 # usage of the glob functions for these toolchains.
-ifeq ($(BR2_TOOLCHAIN_EXTERNAL_BLACKFIN_UCLINUX_2012R1)$(BR2_TOOLCHAIN_EXTERNAL_BLACKFIN_UCLINUX_2012R2)$(BR2_TOOLCHAIN_EXTERNAL_BLACKFIN_UCLINUX_2013R1),y)
+ifeq ($(BR2_TOOLCHAIN_EXTERNAL_BLACKFIN_UCLINUX_2012R2)$(BR2_TOOLCHAIN_EXTERNAL_BLACKFIN_UCLINUX_2013R1),y)
 POPT_CONF_ENV += ac_cv_header_glob_h=no
 endif
 
