b94777220a17c269cd6e82d681a4b29351773e1a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-17 16:44:09 -0300

    opus-tools: bump to version 0.1.9
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/opus-tools/opus-tools.mk
+++ package/opus-tools/opus-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPUS_TOOLS_VERSION = 0.1.8
+OPUS_TOOLS_VERSION = 0.1.9
 OPUS_TOOLS_SITE = http://downloads.xiph.org/releases/opus
 OPUS_TOOLS_LICENSE = BSD-2c
 OPUS_TOOLS_LICENSE_FILES = COPYING
