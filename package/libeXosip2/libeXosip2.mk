fb59306c38a4e69db780777262f26a9fd50c77e3
Yann E. MORIN <yann.morin.1998@free.fr>
2014-06-23 00:41:28 +0200

    package/libeXosip2: fix build for static linking
    Add a new patch to use pkg-config to detect openssl.
    
    [Peter: fix minor typos in description]
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libeXosip2/libeXosip2.mk
+++ package/libeXosip2/libeXosip2.mk
@@ -12,6 +12,9 @@ LIBEXOSIP2_LICENSE_FILES = COPYING
 
 LIBEXOSIP2_DEPENDENCIES = host-pkgconf libosip2
 
+# We are touching configure.in and Makefile.am with one of our patches
+LIBEXOSIP2_AUTORECONF = YES
+
 ifeq ($(BR2_arc),y)
 # toolchain __arc__ define conflicts with libeXosip2 source
 LIBEXOSIP2_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -U__arc__"
