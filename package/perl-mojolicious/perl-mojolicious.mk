dd490c1fb22148a10b20c27cfd02b5f6d29f792b
Francois Perrad <fperrad@gmail.com>
2014-06-23 20:43:17 +0200

    perl-mojolicious: bump to version 5.08
    - switch to BackPan in order to prevent build breakage
    (like http://autobuild.buildroot.net/results/358/358f531f2db90b9bc3b1e4e2158c68d2bf6587fc/)
    - add license file
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Tested-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/perl-mojolicious/perl-mojolicious.mk
+++ package/perl-mojolicious/perl-mojolicious.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-PERL_MOJOLICIOUS_VERSION = 5.07
+PERL_MOJOLICIOUS_VERSION = 5.08
 PERL_MOJOLICIOUS_SOURCE = Mojolicious-$(PERL_MOJOLICIOUS_VERSION).tar.gz
-PERL_MOJOLICIOUS_SITE = $(BR2_CPAN_MIRROR)/authors/id/S/SR/SRI/
+PERL_MOJOLICIOUS_SITE = http://backpan.perl.org/authors/id/S/SR/SRI/
 PERL_MOJOLICIOUS_DEPENDENCIES = perl
 PERL_MOJOLICIOUS_LICENSE = Artistic-2.0
+PERL_MOJOLICIOUS_LICENSE_FILES = LICENSE
 
 $(eval $(perl-package))
