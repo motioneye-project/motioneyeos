0b4aa0894ebcf693b91f29dc01cedc0fc5f79c24
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-24 15:55:19 -0300

    libical: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libical/libical.mk
+++ package/libical/libical.mk
@@ -7,5 +7,7 @@
 LIBICAL_VERSION = 0.48
 LIBICAL_SITE = http://downloads.sourceforge.net/project/freeassociation/libical/libical-$(LIBICAL_VERSION)
 LIBICAL_INSTALL_STAGING = YES
+LIBICAL_LICENSE = MPLv1.0 or LGPLv2.1
+LIBICAL_LICENSE_FILES = LICENSE
 
 $(eval $(autotools-package))
