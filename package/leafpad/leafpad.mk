73992f1680c4ad395e545738fe7c3d01aff494d8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-24 15:55:10 -0300

    leafpad: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Acked-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/leafpad/leafpad.mk
+++ package/leafpad/leafpad.mk
@@ -7,5 +7,7 @@
 LEAFPAD_VERSION = 0.8.18
 LEAFPAD_SITE = http://savannah.nongnu.org/download/leafpad
 LEAFPAD_DEPENDENCIES = libgtk2 host-intltool
+LEAFPAD_LICENSE = GPLv2+
+LEAFPAD_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
