c3ffd01e68c87dbd41601592e418d058c340e1bb
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-24 15:55:16 -0300

    libglade: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libglade/libglade.mk
+++ package/libglade/libglade.mk
@@ -10,6 +10,8 @@ LIBGLADE_SOURCE = libglade-$(LIBGLADE_VERSION).tar.bz2
 LIBGLADE_SITE = http://ftp.gnome.org/pub/GNOME/sources/libglade/$(LIBGLADE_VERSION_MAJOR)/
 LIBGLADE_INSTALL_STAGING = YES
 LIBGLADE_DEPENDENCIES = host-pkgconf libglib2 libgtk2 atk libxml2
+LIBGLADE_LICENSE = LGPLV2+
+LIBGLADE_LICENSE_FILES = COPYING
 
 define LIBGLADE_INSTALL_FIX
 	rm -rf $(TARGET_DIR)/usr/share/xml/libglade \
