890b362bc400e34d3b67ad8a56106f1d2d946fe2
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-24 15:55:50 -0300

    xapp_xinit: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xapp_xinit/xapp_xinit.mk
+++ package/x11r7/xapp_xinit/xapp_xinit.mk
@@ -8,5 +8,7 @@ XAPP_XINIT_VERSION = 1.3.3
 XAPP_XINIT_SOURCE = xinit-$(XAPP_XINIT_VERSION).tar.bz2
 XAPP_XINIT_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XINIT_DEPENDENCIES = xapp_xauth xlib_libX11
+XAPP_XINIT_LICENSE = MIT
+XAPP_XINIT_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
