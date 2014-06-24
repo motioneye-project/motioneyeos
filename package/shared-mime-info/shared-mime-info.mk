e22599f6f5c306683622d7cca6b87f088aa155ed
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-24 15:55:41 -0300

    shared-mime-info: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/shared-mime-info/shared-mime-info.mk
+++ package/shared-mime-info/shared-mime-info.mk
@@ -7,14 +7,13 @@
 SHARED_MIME_INFO_VERSION = 0.90
 SHARED_MIME_INFO_SOURCE = shared-mime-info-$(SHARED_MIME_INFO_VERSION).tar.bz2
 SHARED_MIME_INFO_SITE = http://freedesktop.org/~hadess
-
 SHARED_MIME_INFO_INSTALL_STAGING = YES
-
 SHARED_MIME_INFO_CONF_ENV = XMLLINT=$(HOST_DIR)/usr/bin/xmllint
 SHARED_MIME_INFO_DEPENDENCIES = host-pkgconf host-libglib2 host-libxml2 host-shared-mime-info libxml2 libglib2
-
 SHARED_MIME_INFO_CONF_OPT = --disable-update-mimedb
 SHARED_MIME_INFO_MAKE = $(MAKE1)
+SHARED_MIME_INFO_LICENSE = GPLv2
+SHARED_MIME_INFO_LICENSE_FILES = COPYING
 
 HOST_SHARED_MIME_INFO_DEPENDENCIES = host-pkgconf host-intltool
 
