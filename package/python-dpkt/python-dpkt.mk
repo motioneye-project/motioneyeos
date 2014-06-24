ddfd12c8ec2d26b57844a0f869f35aa6ab8bbfac
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-24 15:55:36 -0300

    python-dpkt: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-dpkt/python-dpkt.mk
+++ package/python-dpkt/python-dpkt.mk
@@ -5,8 +5,10 @@
 ################################################################################
 
 PYTHON_DPKT_VERSION = 1.7
-PYTHON_DPKT_SOURCE  = dpkt-$(PYTHON_DPKT_VERSION).tar.gz
-PYTHON_DPKT_SITE    = http://dpkt.googlecode.com/files
+PYTHON_DPKT_SOURCE = dpkt-$(PYTHON_DPKT_VERSION).tar.gz
+PYTHON_DPKT_SITE = http://dpkt.googlecode.com/files
 PYTHON_DPKT_SETUP_TYPE = distutils
+PYTHON_DPKT_LICENSE = BSD-3c
+PYTHON_DPKT_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
