385adb81a3fdd388da684969ad95929b8c00dc36
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-24 18:59:06 -0300

    libfreefare: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libfreefare/libfreefare.mk
+++ package/libfreefare/libfreefare.mk
@@ -7,6 +7,8 @@
 LIBFREEFARE_VERSION = 0.3.4
 LIBFREEFARE_SITE = http://libfreefare.googlecode.com/files
 LIBFREEFARE_DEPENDENCIES = libnfc openssl
+LIBFREEFARE_LICENSE = LGPLv3+ with exception
+LIBFREEFARE_LICENSE_FILES = COPYING
 
 ifeq ($(BR2_PREFER_STATIC_LIB),y)
 # openssl needs zlib even if the libfreefare example itself doesn't
