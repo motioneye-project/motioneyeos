baa330e227689f36fd62b8606f3a0df299d86032
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-24 15:55:55 -0300

    gqview: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gqview/gqview.mk
+++ package/gqview/gqview.mk
@@ -8,5 +8,7 @@ GQVIEW_VERSION = 2.1.5
 GQVIEW_SITE = http://prdownloads.sourceforge.net/gqview
 GQVIEW_DEPENDENCIES = host-pkgconf libgtk2
 GQVIEW_CONF_ENV = LIBS="-lm"
+GQVIEW_LICENSE = GPLv2
+GQVIEW_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
