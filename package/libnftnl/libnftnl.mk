74924f719dd7bcb4f7c9522bc67bbf5f8c236c35
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-25 16:01:26 -0300

    libnftnl: bump to version 1.0.2
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libnftnl/libnftnl.mk
+++ package/libnftnl/libnftnl.mk
@@ -4,14 +4,12 @@
 #
 ################################################################################
 
-LIBNFTNL_VERSION = 1.0.1
+LIBNFTNL_VERSION = 1.0.2
 LIBNFTNL_SITE = http://netfilter.org/projects/libnftnl/files
 LIBNFTNL_SOURCE = libnftnl-$(LIBNFTNL_VERSION).tar.bz2
 LIBNFTNL_LICENSE = GPLv2+
 LIBNFTNL_LICENSE_FILES = COPYING
 LIBNFTNL_INSTALL_STAGING = YES
-# For uclinux support patch
-LIBNFTNL_AUTORECONF = YES
 LIBNFTNL_DEPENDENCIES = host-pkgconf libmnl
 
 ifeq ($(BR2_PACKAGE_LIBNFTNL_JSON),y)
