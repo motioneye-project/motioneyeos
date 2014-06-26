076375eb2df2073a33a3e98bf37789e57e99d48c
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-26 16:21:13 -0300

    rt-test: add license information
    [Peter; fix variable name typo]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rt-tests/rt-tests.mk
+++ package/rt-tests/rt-tests.mk
@@ -4,9 +4,11 @@
 #
 ################################################################################
 
-RT_TESTS_SITE    = http://snapshot.debian.org/archive/debian/20111202T222101Z/pool/main/r/rt-tests/
+RT_TESTS_SITE = http://snapshot.debian.org/archive/debian/20111202T222101Z/pool/main/r/rt-tests/
 RT_TESTS_VERSION = 0.83
-RT_TESTS_SOURCE  = rt-tests_$(RT_TESTS_VERSION).orig.tar.gz
+RT_TESTS_SOURCE = rt-tests_$(RT_TESTS_VERSION).orig.tar.gz
+RT_TESTS_LICENSE = GPLv2+
+RT_TESTS_LICENSE_FILES = COPYING
 
 ifeq ($(BR2_PACKAGE_PYTHON),y)
 RT_TESTS_DEPENDENCIES = python
