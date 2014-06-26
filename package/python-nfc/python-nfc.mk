1bafbcca553c52f2c81cf6dbebf14d16514c0859
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-26 16:21:09 -0300

    python-nfc: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-nfc/python-nfc.mk
+++ package/python-nfc/python-nfc.mk
@@ -9,5 +9,6 @@ PYTHON_NFC_SITE = https://launchpad.net/nfcpy
 PYTHON_NFC_SITE_METHOD = bzr
 PYTHON_NFC_DEPENDENCIES = libusb libusb-compat
 PYTHON_NFC_SETUP_TYPE = distutils
+PYTHON_NFC_LICENSE = EUPLv1.1+
 
 $(eval $(python-package))
