5d9215351f3b529bfdf625742eec530e2739b11b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-26 16:20:29 -0300

    blackbox: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/blackbox/blackbox.mk
+++ package/blackbox/blackbox.mk
@@ -7,11 +7,11 @@
 BLACKBOX_VERSION = 0.70.1
 BLACKBOX_SOURCE = blackbox-$(BLACKBOX_VERSION).tar.bz2
 BLACKBOX_SITE = http://downloads.sourceforge.net/project/blackboxwm/blackboxwm/Blackbox%20$(BLACKBOX_VERSION)
-
 BLACKBOX_CONF_OPT = --x-includes=$(STAGING_DIR)/usr/include/X11 \
 		--x-libraries=$(STAGING_DIR)/usr/lib
-
 BLACKBOX_DEPENDENCIES = xlib_libX11
+BLACKBOX_LICENSE = MIT
+BLACKBOX_LICENSE_FILES = LICENSE
 
 ifneq ($(BR2_ENABLE_LOCALE),y)
 BLACKBOX_DEPENDENCIES += libiconv
