b33f4e98183e09e4b724aa2d64df0fbcfa002b37
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-26 16:20:54 -0300

    libconfuse: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libconfuse/libconfuse.mk
+++ package/libconfuse/libconfuse.mk
@@ -9,6 +9,8 @@ LIBCONFUSE_SOURCE = confuse-$(LIBCONFUSE_VERSION).tar.gz
 LIBCONFUSE_SITE = http://savannah.nongnu.org/download/confuse/
 LIBCONFUSE_INSTALL_STAGING = YES
 LIBCONFUSE_CONF_OPT = --disable-rpath
+LIBCONFUSE_LICENSE = ISC
+LIBCONFUSE_LICENSE_FILES = src/confuse.c
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
