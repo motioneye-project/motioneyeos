0a69f22d6008b4863ad9f9600efef29288daa048
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-26 16:20:34 -0300

    dfu-util: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/dfu-util/dfu-util.mk
+++ package/dfu-util/dfu-util.mk
@@ -6,6 +6,8 @@
 
 DFU_UTIL_VERSION = 0.6
 DFU_UTIL_SITE = http://dfu-util.gnumonks.org/releases/
+DFU_UTIL_LICENSE = GPLv2+
+DFU_UTIL_LICENSE_FILES = COPYING
 
 HOST_DFU_UTIL_DEPENDENCIES = host-libusb
 
