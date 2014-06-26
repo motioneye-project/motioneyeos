a216d1a43e5ee2a3e82683976babc0852b9ccda9
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-26 16:20:49 -0300

    ipkg: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ipkg/ipkg.mk
+++ package/ipkg/ipkg.mk
@@ -7,5 +7,7 @@
 IPKG_VERSION = 0.99.163
 IPKG_SITE = http://www.handhelds.org/download/packages/ipkg
 IPKG_INSTALL_STAGING = YES
+IPKG_LICENSE = GPLv2+
+IPKG_LICENSE_FILES = COPYING
 
 $(eval $(autotools-package))
