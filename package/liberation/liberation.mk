6a50f3206456e2fa2b7bbb016ce4e256350a2953
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-26 16:20:56 -0300

    liberation: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/liberation/liberation.mk
+++ package/liberation/liberation.mk
@@ -7,8 +7,10 @@
 LIBERATION_VERSION = 1.06.0.20100721
 LIBERATION_SITE = http://www.fedorahosted.org/releases/l/i/liberation-fonts
 LIBERATION_SOURCE = liberation-fonts-ttf-$(LIBERATION_VERSION).tar.gz
-
 LIBERATION_TARGET_DIR = $(TARGET_DIR)/usr/share/fonts/liberation
+# Bump to 2.x changes license!!
+LIBERATION_LICENSE = GPLv2 with exceptions
+LIBERATION_LICENSE_FILES = License.txt
 
 ifeq ($(BR2_PACKAGE_LIBERATION_MONO),y)
 define LIBERATION_INSTALL_MONO
