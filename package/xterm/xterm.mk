0b0285213d156f20b94e906b568c77f2c9982a20
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-26 16:21:20 -0300

    xterm: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/xterm/xterm.mk
+++ package/xterm/xterm.mk
@@ -9,5 +9,7 @@ XTERM_SOURCE = xterm-$(XTERM_VERSION).tgz
 XTERM_SITE = ftp://invisible-island.net/xterm
 XTERM_DEPENDENCIES = ncurses xlib_libXaw
 XTERM_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
+XTERM_LICENSE = MIT
+XTERM_LICENSE_FILES = version.c
 
 $(eval $(autotools-package))
