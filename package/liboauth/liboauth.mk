367dcacca54f89fa3348b317003cacb185fe9778
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-27 17:09:26 -0300

    liboauth: bump to version 1.0.3
    Add license information as well.
    It could be "or GPL" as well but since we link to openssl that's a murky path
    as stated in the README so just leave it at MIT.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/liboauth/liboauth.mk
+++ package/liboauth/liboauth.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-LIBOAUTH_VERSION = 0.9.6
+LIBOAUTH_VERSION = 1.0.3
 LIBOAUTH_SITE = http://liboauth.sourceforge.net/pool
 LIBOAUTH_INSTALL_STAGING = YES
-
 LIBOAUTH_DEPENDENCIES += host-pkgconf openssl
+LIBOAUTH_LICENSE = MIT
+LIBOAUTH_LICENSE_FILES = COPYING.MIT
 
 ifeq ($(BR2_PACKAGE_LIBCURL),y)
 LIBOAUTH_DEPENDENCIES += libcurl
