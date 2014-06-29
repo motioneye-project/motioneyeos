7001f391eef299634615e8a3b7db02d03c94ee27
Baruch Siach <baruch@tkos.co.il>
2014-06-29 07:47:51 +0300

    lzo: security bump to version 2.07
    Fixes CVE-2014-4607.
    
    [Thomas: fix small typo in comment.]
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lzo/lzo.mk
+++ package/lzo/lzo.mk
@@ -4,11 +4,14 @@
 #
 ################################################################################
 
-LZO_VERSION = 2.06
+LZO_VERSION = 2.07
 LZO_SITE = http://www.oberhumer.com/opensource/lzo/download
 LZO_LICENSE = GPLv2+
 LZO_LICENSE_FILES = COPYING
 LZO_INSTALL_STAGING = YES
+# Our libtool patch does not apply to bundled ltmain.sh since it's too new.
+# Run autoreconf to regenerate ltmain.sh.
+LZO_AUTORECONF = YES
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
