206b7abd98645cf73154147986388667ff7f8dd9
Gary Bisson <bisson.gary@gmail.com>
2014-06-30 23:52:06 +0200

    libfslvpuwrap: bump to version 1.0.46
    The libfslvpuwrapp versionning no longer follows the same version scheme
    as the other Freescale components.
    
    As VPU code is no longer in imx-lib, this package now depends on imx-vpu
    package.
    
    Signed-off-by: Gary Bisson <bisson.gary@gmail.com>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    [yann.morin.1998@free.fr: expand commit log]
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Hadrien Boutteville <hadrien.boutteville@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libfslvpuwrap/libfslvpuwrap.mk
+++ package/libfslvpuwrap/libfslvpuwrap.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBFSLVPUWRAP_VERSION = $(FREESCALE_IMX_VERSION)
+LIBFSLVPUWRAP_VERSION = 1.0.46
 LIBFSLVPUWRAP_SITE = $(FREESCALE_IMX_SITE)
 LIBFSLVPUWRAP_SOURCE = libfslvpuwrap-$(LIBFSLVPUWRAP_VERSION).bin
 LIBFSLVPUWRAP_LICENSE = Freescale Semiconductor Software License Agreement
@@ -14,7 +14,7 @@ LIBFSLVPUWRAP_REDISTRIBUTE = NO
 
 LIBFSLVPUWRAP_INSTALL_STAGING = YES
 
-LIBFSLVPUWRAP_DEPENDENCIES += imx-lib
+LIBFSLVPUWRAP_DEPENDENCIES += imx-vpu
 
 # The archive is a shell-self-extractor of a bzipped tar. It happens
 # to extract in the correct directory (libfslvpuwrap-x.y.z)
