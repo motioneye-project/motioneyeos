f2ac23454f2e6724ab252447452101c420b7cf90
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-06-30 13:56:08 +0200

    libtirpc: bump to 0.2.4
    This commit bumps the version of libtirpc to the latest upstream
    version 0.2.4, with the following changes on the patches:
    
     Updated to take into account minor upstream changes:
    
     - libtirpc-0001-Disable-parts-of-TIRPC-requiring-NIS-support.patch
     - libtirpc-0002-uClibc-without-RPC-support-does-not-install-rpcent.h.patch
     - libtirpc-0004-Make-IPv6-support-optional.patch
     - libtirpc-0008-Add-rpcgen-program-from-nfs-utils-sources.patch
     - libtirpc-0009-Automatically-generate-XDR-header-files-from-.x-sour.patch
     - libtirpc-0010-Add-more-XDR-files-needed-to-build-rpcbind-on-top-of.patch
    
     Removed since they were merged upstream:
    
     - libtirpc-0003-Add-missing-INET6-conditional.patch
     - libtirpc-0005-rpcent-mark-getrpcbyname-name-argument-as-const-char.patch
     - libtirpc-0006-rpcent-remove-prototypes-of-reentrant-variants.patch
     - libtirpc-0007-doc-Makefile.am-fix-out-of-tree-installation.patch
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libtirpc/libtirpc.mk
+++ package/libtirpc/libtirpc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBTIRPC_VERSION = 0.2.2
+LIBTIRPC_VERSION = 0.2.4
 LIBTIRPC_SOURCE = libtirpc-$(LIBTIRPC_VERSION).tar.bz2
 LIBTIRPC_SITE = http://downloads.sourceforge.net/project/libtirpc/libtirpc/$(LIBTIRPC_VERSION)
 LIBTIRPC_LICENSE = BSD-3c
@@ -15,6 +15,8 @@ LIBTIRPC_AUTORECONF = YES
 
 # getrpcby{number,name} are only provided if 'GQ' is defined
 LIBTIRPC_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -DGQ"
+
+LIBTIRPC_CONF_OPT = --disable-gssapi
 
 # We need host-pkgconf because the configure.ac contains a
 # PKG_CHECK_MODULES macro call, and since we're autoreconfiguring this
