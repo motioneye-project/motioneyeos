a660373f2c3d8dd7f4b0fbc61c8e40143e14f5cd
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-30 09:56:31 -0300

    python-simplejson: security bump to version 3.5.3
    No CVE assigned, see https://github.com/simplejson/simplejson/issues/98
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-simplejson/python-simplejson.mk
+++ package/python-simplejson/python-simplejson.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_SIMPLEJSON_VERSION = 3.3.1
+PYTHON_SIMPLEJSON_VERSION = 3.5.3
 PYTHON_SIMPLEJSON_SOURCE = simplejson-$(PYTHON_SIMPLEJSON_VERSION).tar.gz
 PYTHON_SIMPLEJSON_SITE = http://pypi.python.org/packages/source/s/simplejson/
 PYTHON_SIMPLEJSON_LICENSE = MIT
