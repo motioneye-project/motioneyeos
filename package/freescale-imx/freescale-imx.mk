e783d8a51988a089d2ee799ebfe59695c27ee328
Gary Bisson <bisson.gary@gmail.com>
2014-06-30 23:52:01 +0200

    freescale-imx: bump to version 3.10.17-1.0.0
    Bumping the version requires some heavy changes in the different
    i.MX related packages. Instead of having a single huge patch, those
    changes are split in smaller patches, one per affected package.
    
    This has the inconvenient side-effect of breaking the build right
    now with the bump; but it is better since the patches are easier to
    understand.
    
    Signed-off-by: Gary Bisson <bisson.gary@gmail.com>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    [yann.morin.1998@free.fr: expand commit log]
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Hadrien Boutteville <hadrien.boutteville@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/freescale-imx/freescale-imx.mk
+++ package/freescale-imx/freescale-imx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FREESCALE_IMX_VERSION = 3.5.7-1.0.0
+FREESCALE_IMX_VERSION = 3.10.17-1.0.0
 FREESCALE_IMX_SITE    = http://www.freescale.com/lgfiles/NMG/MAD/YOCTO/
 
 include $(sort $(wildcard package/freescale-imx/*/*.mk))
