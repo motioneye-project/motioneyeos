ea7061f40e04f6a475705a579529978de1b9cc91
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-06-30 10:16:18 -0300

    parted: fix nommu build
    Add patch to accept uclinux in the tuple, and also disable dynamic libs
    for static scenarios. Fixes:
    http://autobuild.buildroot.net/results/d9a/d9a9baf0c1bfdb7f7d03ebed084a80ef3001c86d/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/parted/parted.mk
+++ package/parted/parted.mk
@@ -9,6 +9,8 @@ PARTED_SOURCE = parted-$(PARTED_VERSION).tar.xz
 PARTED_SITE = $(BR2_GNU_MIRROR)/parted
 PARTED_DEPENDENCIES = util-linux
 PARTED_INSTALL_STAGING = YES
+# For uclinux patch
+PARTED_AUTORECONF = YES
 PARTED_LICENSE = GPLv3+
 PARTED_LICENSE_FILES = COPYING
 
@@ -24,6 +26,10 @@ PARTED_DEPENDENCIES += lvm2
 PARTED_CONF_OPT += --enable-device-mapper
 else
 PARTED_CONF_OPT += --disable-device-mapper
+endif
+
+ifeq ($(BR2_PREFER_STATIC_LIB),y)
+PARTED_CONF_OPT += --disable-dynamic-loading
 endif
 
 HOST_PARTED_DEPENDENCIES = host-util-linux
