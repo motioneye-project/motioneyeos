4414f7f165a32ba5934261d23a882b1152024cd1
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-06-30 13:56:09 +0200

    rpcbind: bump to version 0.2.1
    Patches that were updated to take into account minor upstream
     changes:
    
     - rpcbind-0001-Remove-yellow-pages-support.patch
     - rpcbind-0003-Make-IPv6-configurable.patch
    
     Patches that were removed as they were no longer needed thanks to
     upstream changes:
    
     - rpcbind-0002-Do-not-try-to-use-NSS-support-when-not-available-in-.patch
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rpcbind/rpcbind.mk
+++ package/rpcbind/rpcbind.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPCBIND_VERSION = 0.2.0
+RPCBIND_VERSION = 0.2.1
 RPCBIND_SITE    = http://downloads.sourceforge.net/project/rpcbind/rpcbind/$(RPCBIND_VERSION)
 RPCBIND_SOURCE  = rpcbind-$(RPCBIND_VERSION).tar.bz2
 RPCBIND_LICENSE = BSD-3c
@@ -14,5 +14,6 @@ RPCBIND_AUTORECONF = YES
 RPCBIND_CONF_ENV += \
 	CFLAGS="$(TARGET_CFLAGS) -I$(STAGING_DIR)/usr/include/tirpc/"
 RPCBIND_DEPENDENCIES += libtirpc
+RPCBIND_CONF_OPT += --with-rpcuser=root
 
 $(eval $(autotools-package))
