71e06e10d7c7455717ddfcd52f30aa4a284d4240
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-07-03 16:50:41 -0300

    dbus-glib: add license information
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/dbus-glib/dbus-glib.mk
+++ package/dbus-glib/dbus-glib.mk
@@ -7,6 +7,8 @@
 DBUS_GLIB_VERSION = 0.98
 DBUS_GLIB_SITE = http://dbus.freedesktop.org/releases/dbus-glib/
 DBUS_GLIB_INSTALL_STAGING = YES
+DBUS_GLIB_LICENSE = AFLv2.1 GPLv2+
+DBUS_GLIB_LICENSE_FILES = COPYING
 
 DBUS_GLIB_CONF_ENV = ac_cv_have_abstract_sockets=yes \
 		ac_cv_func_posix_getpwnam_r=yes \
