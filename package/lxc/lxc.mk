065c7fcd7a16b4e4c908c975112dc3fb1265c415
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-07-04 10:35:31 -0300

    lxc: bump to version 1.0.4
    Also switch to new homepage and github download, and specify distro
    variant since host distro != target distro and sample configs for each
    case aren't useful anyway.
    It sometimes failed to autodetect because some of the tests can't be
    executed when cross compiling (happened on gentoo host).
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lxc/lxc.mk
+++ package/lxc/lxc.mk
@@ -4,11 +4,13 @@
 #
 ################################################################################
 
-LXC_VERSION = 0.9.0
-LXC_SITE = http://downloads.sourceforge.net/project/lxc/lxc/lxc-$(LXC_VERSION)
+LXC_VERSION = 1.0.4
+LXC_SITE = $(call github,lxc,lxc,lxc-$(LXC_VERSION))
 LXC_LICENSE = LGPLv2.1+
 LXC_LICENSE_FILES = COPYING
-LXC_DEPENDENCIES = libcap
-LXC_CONF_OPT = --disable-apparmor
+LXC_DEPENDENCIES = libcap host-pkgconf
+# configure not shipped
+LXC_AUTORECONF = YES
+LXC_CONF_OPT = --disable-apparmor --with-distro=buildroot
 
 $(eval $(autotools-package))
