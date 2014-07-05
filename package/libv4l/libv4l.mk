07ac045655d08255fbc2d17b16a42d0f0017632e
Ezequiel García <ezequiel@vanguardiasur.com.ar>
2014-07-05 18:07:40 -0300

    libv4l: Bump version to 1.0.1
    Quite some work has been done from 0.8.3 to 1.0.1, and as a result
    this commit is very intrusive. The biggest change is the move to an
    autotools package.
    
    Then, the options that enable utilities individually have been deprecated
    and moved to Config.in.legacy. Instead, we introduce new option to select
    either all the utilities. This change loses granularity in favor of
    maintainability.
    
    [Peter: needs host-pkgconf. Select and use argp-standalone on uClibc]
    Signed-off-by: Ezequiel Garcia <ezequiel@vanguardiasur.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libv4l/libv4l.mk
+++ package/libv4l/libv4l.mk
@@ -4,42 +4,32 @@
 #
 ################################################################################
 
-LIBV4L_VERSION = 0.8.9
+LIBV4L_VERSION = 1.0.1
 LIBV4L_SOURCE = v4l-utils-$(LIBV4L_VERSION).tar.bz2
 LIBV4L_SITE = http://linuxtv.org/downloads/v4l-utils/
 LIBV4L_INSTALL_STAGING = YES
-LIBV4L_MAKE_OPTS = PREFIX=/usr
-LIBV4L_DEPENDENCIES = jpeg
+LIBV4L_DEPENDENCIES = host-pkgconf
+
+# v4l-utils components have different licences, see v4l-utils.spec for details
 LIBV4L_LICENSE = GPLv2+ (utilities), LGPLv2.1+ (libraries)
-LIBV4L_LICENSE_FILES = COPYING COPYING.LIB lib/libv4l1/libv4l1-kernelcode-license.txt
+LIBV4L_LICENSE_FILES = COPYING COPYING.libv4l lib/libv4l1/libv4l1-kernelcode-license.txt
 
-LIBV4L_DIRS_y += lib
-LIBV4L_DIRS_$(BR2_PACKAGE_LIBV4L_DECODE_TM6000)	+= utils/libv4l2util utils/decode_tm6000
-LIBV4L_DIRS_$(BR2_PACKAGE_LIBV4L_IR_KEYTABLE)	+= utils/keytable
-LIBV4L_DIRS_$(BR2_PACKAGE_LIBV4L_V4L2_COMPLIANCE) += utils/v4l2-compliance
-LIBV4L_DIRS_$(BR2_PACKAGE_LIBV4L_V4L2_CTL)	+= utils/v4l2-ctl
-LIBV4L_DIRS_$(BR2_PACKAGE_LIBV4L_V4L2_DBG)	+= utils/v4l2-dbg
-
-ifeq ($(BR2_PREFER_STATIC_LIB),y)
-	LIBV4L_MAKE_OPTS += LINKTYPE=static
+ifeq ($(BR2_PACKAGE_ARGP_STANDALONE),y)
+LIBV4L_DEPENDENCIES += argp-standalone
+LIBV4L_CONF_ENV += LIBS="-largp"
 endif
 
-define LIBV4L_BUILD_CMDS
-	for i in $(LIBV4L_DIRS_y); do \
-		$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/$$i \
-			$(LIBV4L_MAKE_OPTS) || exit 1 ; done
-endef
+ifeq ($(BR2_PACKAGE_JPEG),y)
+LIBV4L_DEPENDENCIES += jpeg
+LIBV4L_CONF_OPT += --with-jpeg
+else
+LIBV4L_CONF_OPT += --without-jpeg
+endif
 
-define LIBV4L_INSTALL_STAGING_CMDS
-	for i in $(LIBV4L_DIRS_y); do \
-		$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/$$i \
-			DESTDIR=$(STAGING_DIR) $(LIBV4L_MAKE_OPTS) install || exit 1; done
-endef
+ifeq ($(BR2_PACKAGE_LIBV4L_UTILS),y)
+LIBV4L_CONF_OPT += --enable-v4l-utils
+else
+LIBV4L_CONF_OPT += --disable-v4l-utils
+endif
 
-define LIBV4L_INSTALL_TARGET_CMDS
-	for i in $(LIBV4L_DIRS_y); do \
-		$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/$$i \
-			DESTDIR=$(TARGET_DIR) $(LIBV4L_MAKE_OPTS) install || exit 1; done
-endef
-
-$(eval $(generic-package))
+$(eval $(autotools-package))
