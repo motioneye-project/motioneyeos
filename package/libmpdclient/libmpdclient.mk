d330aadac23bdb4229fe52221007d681c4c42acb
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-07-07 16:53:03 -0300

    libmpdclient: new package
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/libmpdclient/libmpdclient.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# libmpdclient
+#
+################################################################################
+
+LIBMPDCLIENT_VERSION_MAJOR = 2
+LIBMPDCLIENT_VERSION = $(LIBMPDCLIENT_VERSION_MAJOR).9
+LIBMPDCLIENT_SOURCE = libmpdclient-$(LIBMPDCLIENT_VERSION).tar.xz
+LIBMPDCLIENT_SITE = http://www.musicpd.org/download/libmpdclient/$(LIBMPDCLIENT_VERSION_MAJOR)
+LIBMPDCLIENT_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
+LIBMPDCLIENT_INSTALL_STAGING = YES
+LIBMPDCLIENT_LICENSE = BSD-3c
+LIBMPDCLIENT_LICENSE_FILES = COPYING
+
+$(eval $(autotools-package))
