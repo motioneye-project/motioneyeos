7d1047e915c14879fe273488c7ce7c3a37971100
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-07-09 16:03:42 -0300

    rsync: bump to version 3.1.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rsync/rsync.mk
+++ package/rsync/rsync.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RSYNC_VERSION = 3.1.0
+RSYNC_VERSION = 3.1.1
 RSYNC_SITE = http://rsync.samba.org/ftp/rsync/src
 RSYNC_LICENSE = GPLv3+
 RSYNC_LICENSE_FILES = COPYING
