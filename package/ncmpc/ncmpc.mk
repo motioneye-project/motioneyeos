8957080d6d9626e2a27a9950d209d2cad9a2f1f8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-07-10 17:10:32 -0300

    ncmpc: bump to version 0.23
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ncmpc/ncmpc.mk
+++ package/ncmpc/ncmpc.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 NCMPC_VERSION_MAJOR = 0
-NCMPC_VERSION = $(NCMPC_VERSION_MAJOR).21
+NCMPC_VERSION = $(NCMPC_VERSION_MAJOR).23
 NCMPC_SOURCE = ncmpc-$(NCMPC_VERSION).tar.gz
 NCMPC_SITE = http://www.musicpd.org/download/ncmpc/$(NCMPC_VERSION_MAJOR)
 NCMPC_DEPENDENCIES = host-pkgconf libglib2 libmpdclient ncurses
