7c84418e414b1c1645cd7c677e98649827daf9b1
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-07-10 10:59:23 -0300

    atftp: fix static linking
    Readline uses ncurses hence pull it in, also needs to explicitly link
    against libpthread. Fixes:
    http://autobuild.buildroot.net/results/259/2592612be38a2c1dde21b44be2ecf25d1cc5211a/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/atftp/atftp.mk
+++ package/atftp/atftp.mk
@@ -9,10 +9,15 @@ ATFTP_SITE = http://sourceforge.net/projects/atftp/files/
 ATFTP_LICENSE = GPLv2+
 ATFTP_LICENSE_FILES = LICENSE
 ATFTP_CONF_OPT = --disable-libwrap --disable-mtftp
+# For static we need to explicitly link against libpthread
+ATFTP_LIBS = -lpthread
+ATFTP_CONF_ENV = LIBS="$(ATFTP_LIBS)"
 
 ifeq ($(BR2_PACKAGE_READLINE),y)
 ATFTP_DEPENDENCIES += readline
 ATFTP_CONF_OPT += --enable-libreadline
+# For static, readline links with ncurses
+ATFTP_LIBS += -lncurses
 else
 ATFTP_CONF_OPT += --disable-libreadline
 endif
