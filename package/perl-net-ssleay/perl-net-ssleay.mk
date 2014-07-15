1f624d6b841570b4e53d98eea0f12009fbe794bc
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-07-15 19:35:01 +0200

    perl-net-ssleay: add comment to new hook
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/perl-net-ssleay/perl-net-ssleay.mk
+++ package/perl-net-ssleay/perl-net-ssleay.mk
@@ -15,6 +15,7 @@ PERL_NET_SSLEAY_LICENSE_FILES = LICENSE
 # suffers from: don't search for openssl, they pick the host-system one.
 PERL_NET_SSLEAY_CONF_ENV = OPENSSL_PREFIX=$(STAGING_DIR)/usr
 
+# Remove problematic single quotes in LDDLFLAGS definition
 define PERL_NET_SSLEAY_FIX_MAKEFILE
 	$(SED) "s/^LDDLFLAGS = '\(.*\)'/LDDLFLAGS = \1/" $(@D)/Makefile
 endef
