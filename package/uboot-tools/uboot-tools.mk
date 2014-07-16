57defd2bc29851b8a1e8f0ac31554008fbbf6ef8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-07-16 15:07:03 -0300

    uboot-tools: bump to version 2014.07
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/uboot-tools/uboot-tools.mk
+++ package/uboot-tools/uboot-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UBOOT_TOOLS_VERSION = 2014.04
+UBOOT_TOOLS_VERSION = 2014.07
 UBOOT_TOOLS_SOURCE  = u-boot-$(UBOOT_TOOLS_VERSION).tar.bz2
 UBOOT_TOOLS_SITE    = ftp://ftp.denx.de/pub/u-boot
 UBOOT_TOOLS_LICENSE = GPLv2+
