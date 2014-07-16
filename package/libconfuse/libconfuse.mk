351680c1a094beae47d27437fabca1eb1cf12929
Alvaro Gamez <alvaro.gamez@hazent.com>
2014-07-17 00:32:45 +0200

    package/libconfuse: switch to the new upstream
    libconfuse is being migrated to github (see
    http://www.nongnu.org/confuse/) So, just follow suit.
    
    Needs a bit of a hack so gettextize + autoreconf does not step on each
    other's toes. See comment in the patch.
    
    [Yann: fix gettextizing, write commit log]
    Signed-off-by: Alvaro G. M <alvaro.gamez@hazent.com>
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Arnout Vandecappelle <arnout@mind.be>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libconfuse/libconfuse.mk
+++ package/libconfuse/libconfuse.mk
@@ -4,13 +4,16 @@
 #
 ################################################################################
 
-LIBCONFUSE_VERSION = 2.7
-LIBCONFUSE_SOURCE = confuse-$(LIBCONFUSE_VERSION).tar.gz
-LIBCONFUSE_SITE = http://savannah.nongnu.org/download/confuse/
+LIBCONFUSE_VERSION = V2_7
+LIBCONFUSE_SITE = $(call github,martinh,libconfuse,$(LIBCONFUSE_VERSION))
 LIBCONFUSE_INSTALL_STAGING = YES
 LIBCONFUSE_CONF_OPT = --disable-rpath
 LIBCONFUSE_LICENSE = ISC
 LIBCONFUSE_LICENSE_FILES = src/confuse.c
+
+# Fresh from the repository, no configure et al.
+LIBCONFUSE_AUTORECONF = YES
+LIBCONFUSE_GETTEXTIZE = YES
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
