a97d7e8f1ae6e05df15aa0baa9f5fe70c23ca0d6
Francois Perrad <fperrad@gmail.com>
2014-07-17 18:32:59 +0200

    perl-io-socket-ssl: new package
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/perl-io-socket-ssl/perl-io-socket-ssl.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-io-socket-ssl
+#
+################################################################################
+
+PERL_IO_SOCKET_SSL_VERSION = 1.997
+PERL_IO_SOCKET_SSL_SOURCE = IO-Socket-SSL-$(PERL_IO_SOCKET_SSL_VERSION).tar.gz
+PERL_IO_SOCKET_SSL_SITE = $(BR2_CPAN_MIRROR)/authors/id/S/SU/SULLR/
+PERL_IO_SOCKET_SSL_DEPENDENCIES = perl perl-net-ssleay
+PERL_IO_SOCKET_SSL_LICENSE = Artistic or GPLv1+
+
+$(eval $(perl-package))
