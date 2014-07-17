35c386682020880fc5306439afa508a57e57a321
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-07-17 18:20:14 -0300

    libmemcached: new package
    For varying reasons it doesn't play well with uClibc or musl based
    toolchains so they're excluded for now.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/libmemcached/libmemcached.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# libmemcached
+#
+################################################################################
+
+LIBMEMCACHED_MAJOR_VERSION = 1.0
+LIBMEMCACHED_VERSION = $(LIBMEMCACHED_MAJOR_VERSION).18
+LIBMEMCACHED_SITE = http://launchpad.net/libmemcached/$(LIBMEMCACHED_MAJOR_VERSION)/$(LIBMEMCACHED_VERSION)/+download
+LIBMEMCACHED_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
+LIBMEMCACHED_CONF_OPT = --disable-dtrace
+LIBMEMCACHED_INSTALL_STAGING = YES
+LIBMEMCACHED_DEPENDENCIES = $(if $(BR2_PACKAGE_LIBEVENT),libevent)
+LIBMEMCACHED_LICENSE = BSD-3c
+LIBMEMCACHED_LICENSE_FILES = COPYING
+
+$(eval $(autotools-package))
