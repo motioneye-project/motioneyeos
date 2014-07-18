39d1f0645ea21a36eeeabd6406429ccbdc7d48b7
Francois Perrad <fperrad@gmail.com>
2014-07-18 15:43:37 +0200

    perl-gd: new package
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/perl-gd/perl-gd.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# perl-gd
+#
+################################################################################
+
+PERL_GD_VERSION = 2.53
+PERL_GD_SOURCE = GD-$(PERL_GD_VERSION).tar.gz
+PERL_GD_SITE = $(BR2_CPAN_MIRROR)/authors/id/L/LD/LDS/
+PERL_GD_DEPENDENCIES = perl zlib libpng freetype gd
+PERL_GD_LICENSE = Artistic-2.0 or GPLv1+
+PERL_GD_LICENSE_FILES = LICENSE
+
+PERL_GD_CONF_OPT = \
+	-options=FT,PNG \
+	-lib_gd_path=$(STAGING_DIR)/usr \
+	-lib_ft_path=$(STAGING_DIR)/usr \
+	-lib_png_path=$(STAGING_DIR)/usr \
+	-lib_zlib_path=$(STAGING_DIR)/usr \
+	-ignore_missing_gd
+
+$(eval $(perl-package))
