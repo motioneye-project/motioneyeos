1aa42ad4aed013c03d1e17f4cfe2ace2194f423d
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-07-19 14:45:55 +0200

    make: bump to version 4.0
    This commit bumps make to version 4.0. This version now uses fork(),
    so it is no longer available on non-MMU platforms, which consequently
    works around:
    
      http://autobuild.buildroot.org/results/2e1/2e1b11ccb501f1ab431c04435be52a6538d035cb/
    
    The glob special options seem to no longer be needed, at least make
    4.0 builds fine with both an ARM uClibc toolchain and an ARM glibc
    toolchain.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/make/make.mk
+++ package/make/make.mk
@@ -4,16 +4,12 @@
 #
 ################################################################################
 
-MAKE_VERSION = 3.82
+MAKE_VERSION = 4.0
 MAKE_SOURCE = make-$(MAKE_VERSION).tar.bz2
 MAKE_SITE = $(BR2_GNU_MIRROR)/make
 MAKE_LICENSE = GPLv3+
 MAKE_LICENSE_FILES = COPYING
 
 MAKE_DEPENDENCIES = $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext)
-
-MAKE_CONF_ENV = make_cv_sys_gnu_glob=no \
-		GLOBINC='-I$(@D)/glob' \
-		GLOBLIB=glob/libglob.a
 
 $(eval $(autotools-package))
