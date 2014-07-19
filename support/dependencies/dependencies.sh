14c1641c1a83b7292783a3e0862403afb4d66c84
Vincent <vincent.stehle@laposte.net>
2014-07-19 17:15:16 +0200

    dependencies.sh: fix typo
    Signed-off-by: Vincent Stehlé <vincent.stehle@laposte.net>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- support/dependencies/dependencies.sh
+++ support/dependencies/dependencies.sh
@@ -206,7 +206,7 @@ if grep -q ^BR2_HOSTARCH_NEEDS_IA32_LIBS=y $BR2_CONFIG ; then
 		echo "but your build machine uses the x86-64 architecture without the 32 bits compatibility"
 		echo "library."
 		echo "If you're running a Debian/Ubuntu distribution, install the libc6-386,"
-		echo "lib32stcd++6, and lib32z1 packages (or alternatively libc6:i386,"
+		echo "lib32stdc++6, and lib32z1 packages (or alternatively libc6:i386,"
 		echo "libstdc++6:i386, and zlib1g:i386)."
 		echo "For other distributions, refer to the documentation on how to install the 32 bits"
 		echo "compatibility libraries."
