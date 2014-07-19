46fb5b82ee82d90e9ed6ac8de67062912bed50f0
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-07-19 15:53:20 +0200

    xlib_libXft: Bump version to 2.3.2
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xlib_libXft/xlib_libXft.mk
+++ package/x11r7/xlib_libXft/xlib_libXft.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBXFT_VERSION = 2.3.1
+XLIB_LIBXFT_VERSION = 2.3.2
 XLIB_LIBXFT_SOURCE = libXft-$(XLIB_LIBXFT_VERSION).tar.bz2
 XLIB_LIBXFT_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXFT_LICENSE = MIT
