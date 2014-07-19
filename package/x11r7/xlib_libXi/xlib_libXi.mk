f4d6b2aea3de09fad07260aa3a0d55a1571e2b75
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-07-19 15:53:17 +0200

    xlib_libXi: Bump version to 1.7.4
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xlib_libXi/xlib_libXi.mk
+++ package/x11r7/xlib_libXi/xlib_libXi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBXI_VERSION = 1.7.2
+XLIB_LIBXI_VERSION = 1.7.4
 XLIB_LIBXI_SOURCE = libXi-$(XLIB_LIBXI_VERSION).tar.bz2
 XLIB_LIBXI_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXI_LICENSE = MIT
