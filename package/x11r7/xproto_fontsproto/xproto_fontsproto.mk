e8f52c2ab5edf2e5edd370f4b25a9962c53d5a91
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-07-19 15:53:23 +0200

    xserver_xorg-server: Bump version to 1.16.0
    This version bump includes bumping version
    
    xlib_libXfont to 1.4.99.901 & xproto_fontsproto to 2.1.3.
    
    xserver_xorg-server 1.16.0 depends on them, but they are incompatible
    with older versions of xserver_xorg-server, for details and libXfont-related
    CVE´s see http://lists.x.org/archives/xorg-announce/2014-July/002453.html
    
    Also remove patches applied upstream.
    
    Since a different patch than xserver_xorg-server-100-mips.patch
    was committed upstream:
    http://cgit.freedesktop.org/xorg/xserver/commit/?id=19e5a13970ad9d440c0789395e94931429ef4fbb
    I also compile tested using this defconfig:
    
    BR2_mips64=y
    BR2_TOOLCHAIN_BUILDROOT_CXX=y
    BR2_ROOTFS_DEVICE_CREATION_DYNAMIC_EUDEV=y
    BR2_PACKAGE_MESA3D=y
    BR2_PACKAGE_MESA3D_DRI_DRIVER_SWRAST=y
    BR2_PACKAGE_MESA3D_OPENGL_EGL=y
    BR2_PACKAGE_MESA3D_OPENGL_ES=y
    BR2_PACKAGE_XORG7=y
    BR2_PACKAGE_XSERVER_XORG_SERVER=y
    BR2_PACKAGE_XSERVER_XORG_SERVER_MODULAR=y
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xproto_fontsproto/xproto_fontsproto.mk
+++ package/x11r7/xproto_fontsproto/xproto_fontsproto.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XPROTO_FONTSPROTO_VERSION = 2.1.2
+XPROTO_FONTSPROTO_VERSION = 2.1.3
 XPROTO_FONTSPROTO_SOURCE = fontsproto-$(XPROTO_FONTSPROTO_VERSION).tar.bz2
 XPROTO_FONTSPROTO_SITE = http://xorg.freedesktop.org/releases/individual/proto
 XPROTO_FONTSPROTO_LICENSE = MIT
