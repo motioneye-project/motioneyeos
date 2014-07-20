ca7e3aebf4f4daf796f9c309f2e55d075517df60
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-07-20 09:52:13 +0200

    xapp_xfs: Fix compile errors after libXfont 1.5.0 update
    Fixes
    http://autobuild.buildroot.net/results/385/385cffbab4261337afa449998aa7457a8142db92/
    
    [Thomas: add comment about the new xfont_font-util dependency being
    needed only for autoreconf.]
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xfs/xapp_xfs.mk
+++ package/x11r7/xapp_xfs/xapp_xfs.mk
@@ -4,11 +4,13 @@
 #
 ################################################################################
 
-XAPP_XFS_VERSION = 1.1.3
-XAPP_XFS_SOURCE = xfs-$(XAPP_XFS_VERSION).tar.bz2
-XAPP_XFS_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XFS_VERSION = 1c8459eafc04997751ae3d200d0ec91e43e19b5b
+XAPP_XFS_SITE = git://anongit.freedesktop.org/xorg/app/xfs
 XAPP_XFS_LICENSE = MIT
 XAPP_XFS_LICENSE_FILES = COPYING
-XAPP_XFS_DEPENDENCIES = xlib_libFS xlib_libXfont xproto_fontsproto
+XAPP_XFS_AUTORECONF = YES
+# xfont_font-util is only needed due to autoreconf. When switching
+# back to a tarball release, it can be removed.
+XAPP_XFS_DEPENDENCIES = xfont_font-util xlib_libFS xlib_libXfont xproto_fontsproto
 
 $(eval $(autotools-package))
