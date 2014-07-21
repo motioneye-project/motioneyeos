f682803a6d1afe15fbd3e023c3dd6b227effbfbd
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-07-21 08:39:33 -0300

    php-memcached: make zlib directory explicit
    Otherwise it fails to find in some cases. Fixes:
    http://autobuild.buildroot.net/results/993/99342e74d0866a7acddbf9da66aafbfa4f3c12f1/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/php-memcached/php-memcached.mk
+++ package/php-memcached/php-memcached.mk
@@ -9,7 +9,8 @@ PHP_MEMCACHED_SOURCE = memcached-$(PHP_MEMCACHED_VERSION).tgz
 PHP_MEMCACHED_SITE = http://pecl.php.net/get
 PHP_MEMCACHED_CONF_OPT = --with-php-config=$(STAGING_DIR)/usr/bin/php-config \
 	--disable-memcached-sasl \
-	--with-libmemcached-dir=$(STAGING_DIR)/usr
+	--with-libmemcached-dir=$(STAGING_DIR)/usr \
+	--with-zlib-dir=$(STAGING_DIR)/usr
 # phpize does the autoconf magic
 PHP_MEMCACHED_DEPENDENCIES = libmemcached php zlib host-autoconf host-pkgconf
 PHP_MEMCACHED_LICENSE = MIT
