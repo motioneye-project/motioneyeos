efee851c614926dd9ed0f49c4808a6d9b4f64eb0
Alexandre Belloni <alexandre.belloni@free-electrons.com>
2014-07-24 23:59:21 +0200

    ltrace: use current master
    ltrace 0.7.3 is the latest release but it is actually broken on ARM
    since PTRACE_SINGLESTEP emulation has been removed, see:
    
    http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=425fc47adb5bb69f76285be77a09a3341a30799e
    
    It fails with:
    PTRACE_SINGLESTEP: Input/output error
    
    Using master solves that until a new release is made.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ltrace/ltrace.mk
+++ package/ltrace/ltrace.mk
@@ -4,13 +4,18 @@
 #
 ################################################################################
 
-LTRACE_VERSION = 0.7.3
-LTRACE_SITE = http://sources.buildroot.net/
-LTRACE_SOURCE = ltrace-$(LTRACE_VERSION).tar.bz2
-LTRACE_DEPENDENCIES = libelf
+LTRACE_VERSION = 0896ce554f80afdcba81d9754f6104f863dea803
+LTRACE_SITE = git://anonscm.debian.org/collab-maint/ltrace.git
+LTRACE_DEPENDENCIES = libelf elfutils
 LTRACE_CONF_OPT = --disable-werror
 LTRACE_LICENSE = GPLv2
 LTRACE_LICENSE_FILES = COPYING
+LTRACE_AUTORECONF = YES
+
+define LTRACE_CREATE_CONFIG_M4
+	mkdir -p $(@D)/config/m4
+endef
+LTRACE_POST_PATCH_HOOKS += LTRACE_CREATE_CONFIG_M4
 
 # ltrace can use libunwind only if libc has backtrace() support
 # We don't normally do so for uClibc and we can't know if it's external
