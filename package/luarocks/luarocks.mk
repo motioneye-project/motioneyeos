0b8411afac3864a2a25a502ede791ccc8bd57aef
Francois Perrad <fperrad@gmail.com>
2014-07-25 20:21:24 +0200

    luarocks: bump to version 2.2.0beta1
    this version includes a new default repository for rocks
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/luarocks/luarocks.mk
+++ package/luarocks/luarocks.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUAROCKS_VERSION = 2.1.2
+LUAROCKS_VERSION = 2.2.0beta1
 LUAROCKS_SITE = http://luarocks.org/releases/
 LUAROCKS_LICENSE = MIT
 LUAROCKS_LICENSE_FILES = COPYING
