1d001e322460fa47786f5790fd15ab367d47ab0e
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-07-29 09:17:05 +0200

    python-flup: re-add MIT license
    The MIT license was in the original submission, but I had removed it
    since I couldn't find where it was used. It is indeed used in
    flup/server/paste_factory.py as noted by the original
    submitter. Therefore, this commit adds the MIT license as one of the
    licenses of python-flup.
    
    Reported-by: Oli Vogt <oli.vogt.pub01@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-flup/python-flup.mk
+++ package/python-flup/python-flup.mk
@@ -8,7 +8,7 @@ PYTHON_FLUP_VERSION = 1.0.3.dev-20110405
 PYTHON_FLUP_SOURCE = flup-$(PYTHON_FLUP_VERSION).tar.gz
 PYTHON_FLUP_SITE = http://pypi.python.org/packages/source/f/flup
 
-PYTHON_FLUP_LICENSE = BSD-2c
+PYTHON_FLUP_LICENSE = BSD-2c, MIT
 
 PYTHON_FLUP_SETUP_TYPE = setuptools
 
