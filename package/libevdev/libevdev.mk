79f742f7871e502e6fa0565b8eb60f9db9241e06
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-07-31 22:49:28 +0200

    libevdev: use tarball instead of git repository
    Tarballs are now provided for libevdev, so use that instead of the Git
    repository, as it allows to avoid autoreconf for the package.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>

--- package/libevdev/libevdev.mk
+++ package/libevdev/libevdev.mk
@@ -5,16 +5,14 @@
 ################################################################################
 
 LIBEVDEV_VERSION = 1.2.2
-LIBEVDEV_SITE = http://cgit.freedesktop.org/libevdev/snapshot
+LIBEVDEV_SITE = http://www.freedesktop.org/software/libevdev/
+LIBEVDEV_SOURCE = libevdev-$(LIBEVDEV_VERSION).tar.xz
 LIBEVDEV_LICENSE = X11
 LIBEVDEV_LICENSE_FILES = COPYING
 
 # Uses PKG_CHECK_MODULES() in configure.ac
 LIBEVDEV_DEPENDENCIES = host-pkgconf
 
-# package source code coming from git, so it doesn't have generated
-# configure and Makefile.in
-LIBEVDEV_AUTORECONF = YES
 LIBEVDEV_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
