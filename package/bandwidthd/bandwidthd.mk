c9f5a86eaeb16c76f8b1f9cb940d2207759dd8d3
Nathaniel Roach <nroach44@gmail.com>
2014-07-31 13:26:59 +0800

    bandwidthd: new package
    [Thomas: minor reformatting like removing trailing whitespaces,
    wrapping to a shorter length Config.in help texts, etc.]
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/bandwidthd/bandwidthd.mk
@@ -0,0 +1,32 @@
+################################################################################
+#
+# bandwidthd
+#
+################################################################################
+
+BANDWIDTHD_VERSION = v2.0.1-auto-r07
+BANDWIDTHD_SITE = $(call github,nroach44,bandwidthd,$(BANDWIDTHD_VERSION))
+
+# Specified as "any version of the GPL that is current as of your
+# download" by upstream.
+BANDWIDTHD_LICENSE = GPL
+
+BANDWIDTHD_DEPENDENCIES = gd libpng libpcap
+
+BANDWIDTHD_AUTORECONF = YES
+
+ifeq ($(BR2_PACKAGE_BANDWIDTHD_POSTGRESQL),y)
+BANDWIDTHD_DEPENDENCIES += postgresql
+BANDWIDTHD_CONF_OPT += --with-postgresql-logging=true
+else
+BANDWIDTHD_CONF_OPT += --with-postgresql-logging=false
+endif
+
+ifeq ($(BR2_PACKAGE_BANDWIDTHD_SQLITE3),y)
+BANDWIDTHD_DEPENDENCIES += sqlite
+BANDWIDTHD_CONF_OPT += --with-sqlite-storage=true
+else
+BANDWIDTHD_CONF_OPT += --with-sqlite-storage=false
+endif
+
+$(eval $(autotools-package))
