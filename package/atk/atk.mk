b83aa532c1f1d0b0a548a29d7c3b460b42114a0e
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2014-08-01 11:27:51 +0200

    atk: bump to version 2.12.0
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Tested-by: Fabio Porcedda <fabio.porcedda@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/atk/atk.mk
+++ package/atk/atk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ATK_VERSION_MAJOR = 2.10
+ATK_VERSION_MAJOR = 2.12
 ATK_VERSION = $(ATK_VERSION_MAJOR).0
 ATK_SOURCE = atk-$(ATK_VERSION).tar.xz
 ATK_SITE = http://ftp.gnome.org/pub/gnome/sources/atk/$(ATK_VERSION_MAJOR)
