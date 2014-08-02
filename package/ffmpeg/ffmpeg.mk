bf76f4319131a107b2dd4ee5ee1eb703825ddbed
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-08-03 00:11:17 +0200

    package/ffmpeg: Bump to version 2.3.1
    - remove obsolete option --disable-vis
    - add patch to fix compile error with uClibc
    - add patch to fix compile error in freerdp
    
    Compile-tested against all packages depending on ffmpeg using this defconfig:
    
    BR2_TOOLCHAIN_EXTERNAL=y
    BR2_PACKAGE_GSTREAMER=y
    BR2_PACKAGE_GST_FFMPEG=y
    BR2_PACKAGE_MPD=y
    BR2_PACKAGE_MPD_FFMPEG=y
    BR2_PACKAGE_XORG7=y
    BR2_PACKAGE_FREERDP=y
    BR2_PACKAGE_OPENCV=y
    BR2_PACKAGE_OPENCV_WITH_FFMPEG=y
    BR2_PACKAGE_MEDIASTREAMER=y
    BR2_PACKAGE_MINIDLNA=y
    
    VLC also compiled when the build of vlc-static is disabled in Makefiles,
    otherwise this known error occurs:
    http://autobuild.buildroot.net/results/022/022252176c051ff3365b2cb820f32aafc802803a/
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ffmpeg/ffmpeg.mk
+++ package/ffmpeg/ffmpeg.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FFMPEG_VERSION = 1.2.7
+FFMPEG_VERSION = 2.3.1
 FFMPEG_SOURCE = ffmpeg-$(FFMPEG_VERSION).tar.bz2
 FFMPEG_SITE = http://ffmpeg.org/releases
 FFMPEG_INSTALL_STAGING = YES
@@ -65,7 +65,6 @@ FFMPEG_CONF_OPT = \
 	--disable-libtheora \
 	--disable-libvo-aacenc \
 	--disable-libvo-amrwbenc \
-	--disable-vis \
 	--disable-sram \
 	--disable-symver \
 	--disable-doc
