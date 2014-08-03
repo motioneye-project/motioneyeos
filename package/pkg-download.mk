a92290aa20a446b3f01822f0637a2ef7a4846c0b
Yann E. MORIN <yann.morin.1998@free.fr>
2014-08-03 19:53:41 +0200

    support/download: convert svn to use the wrapper
    This drastically simplifies the svn helper, as it no longer has to deal
    with atomically saving the downloaded archive.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Tested-by: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    (Tested by running 'make open2300-source')
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/pkg-download.mk
+++ package/pkg-download.mk
@@ -151,8 +151,11 @@ endef
 
 define DOWNLOAD_SVN
 	test -e $(DL_DIR)/$($(PKG)_SOURCE) || \
-	$(EXTRA_ENV) support/download/svn $($(PKG)_SITE) $($(PKG)_DL_VERSION) \
-					  $($(PKG)_BASE_NAME) $(DL_DIR)/$($(PKG)_SOURCE)
+	$(EXTRA_ENV) support/download/wrapper svn \
+		$(DL_DIR)/$($(PKG)_SOURCE) \
+		$($(PKG)_SITE) \
+		$($(PKG)_DL_VERSION) \
+		$($(PKG)_BASE_NAME)
 endef
 
 define SOURCE_CHECK_SVN
