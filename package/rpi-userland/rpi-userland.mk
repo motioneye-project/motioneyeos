997c17a84c04cc4a63b15ff062d1ce8e5106cab4
Benoît Thébaudeau <benoit.thebaudeau@advansee.com>
2014-08-07 17:24:28 +0200

    rpi-userland: Fix vcfiled startup
    The VideoCore file server daemon startup script installed from this package is
    not compatible with BuildRoot (because of its naming and other Debian
    dependencies), which prevented vcfiled from starting. Hence, prevent this
    package from installing its vcfiled startup script, and add a configuration
    option to install a vcfiled SysV init script suitable for BuildRoot.
    
    [Peter: tweak help text as suggested by Yann]
    Signed-off-by: Benoît Thébaudeau <benoit.thebaudeau@advansee.com>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Reviewed-by: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -9,11 +9,22 @@ RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
 RPI_USERLAND_LICENSE = BSD-3c
 RPI_USERLAND_LICENSE_FILES = LICENCE
 RPI_USERLAND_INSTALL_STAGING = YES
-RPI_USERLAND_CONF_OPT = -DVMCS_INSTALL_PREFIX=/usr
+RPI_USERLAND_CONF_OPT = -DVMCS_INSTALL_PREFIX=/usr \
+	-DCMAKE_C_FLAGS="-DVCFILED_LOCKFILE=\\\"/var/run/vcfiled.pid\\\""
 
 RPI_USERLAND_PROVIDES = libegl libgles libopenmax libopenvg
 
+ifeq ($(BR2_PACKAGE_RPI_USERLAND_START_VCFILED),y)
+define RPI_USERLAND_INSTALL_INIT_SYSV
+	$(INSTALL) -m 0755 -D package/rpi-userland/S94vcfiled \
+			$(TARGET_DIR)/etc/init.d/S94vcfiled
+endef
+endif
+
 define RPI_USERLAND_POST_TARGET_CLEANUP
+	rm -f $(TARGET_DIR)/etc/init.d/vcfiled
+	rm -f $(TARGET_DIR)/usr/share/install/vcfiled
+	rmdir --ignore-fail-on-non-empty $(TARGET_DIR)/usr/share/install
 	rm -Rf $(TARGET_DIR)/usr/src
 endef
 RPI_USERLAND_POST_INSTALL_TARGET_HOOKS += RPI_USERLAND_POST_TARGET_CLEANUP
