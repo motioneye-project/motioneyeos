d016a6a30dd86f5d6a02663fa4729a75e48ae379
Peter Korsgaard <peter@korsgaard.com>
2014-08-12 18:02:46 +0200

    chrony: add sysv initscript
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/chrony/chrony.mk
+++ package/chrony/chrony.mk
@@ -39,4 +39,8 @@ define CHRONY_INSTALL_TARGET_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR="$(TARGET_DIR)" install
 endef
 
+define CHRONY_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 package/chrony/S49chrony $(TARGET_DIR)/etc/init.d/S49chrony
+endef
+
 $(eval $(generic-package))
