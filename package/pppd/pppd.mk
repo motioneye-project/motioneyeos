86b830d70a079f3b935b6afc8f429961fb738027
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-08-13 11:41:56 -0300

    pppd: security bump to version 2.4.7
    Fixes CVE-2014-3158 - potential security issue in parsing option files.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/pppd/pppd.mk
+++ package/pppd/pppd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PPPD_VERSION = 2.4.6
+PPPD_VERSION = 2.4.7
 PPPD_SOURCE = ppp-$(PPPD_VERSION).tar.gz
 PPPD_SITE = ftp://ftp.samba.org/pub/ppp
 PPPD_LICENSE = LGPLv2+ LGPL BSD-4c BSD-3c GPLv2+
