5d22b6e06c65a65de1c39a98b6a84a9eeb751936
Yann E. MORIN <yann.morin.1998@free.fr>
2014-08-17 21:29:59 +0200

    arch/sh: always has atomic ops
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Anton Kolesov <Anton.Kolesov@synopsys.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- arch/Config.in.sh
+++ arch/Config.in.sh
@@ -28,3 +28,6 @@ config BR2_ARCH
 config BR2_ENDIAN
 	default "LITTLE"	if BR2_sh4 || BR2_sh4a || BR2_sh64
 	default "BIG"		if BR2_sh2a || BR2_sh4eb || BR2_sh4aeb
+
+config BR2_ARCH_HAS_ATOMICS
+	default y
