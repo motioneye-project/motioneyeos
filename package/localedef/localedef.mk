b4b5ac28b609087e2cdac83dea05caf752bbfe07
Romain Naour <romain.naour@openwide.fr>
2014-08-22 11:12:54 +0200

    Revert "package/localedef: don't use config.site when cross-compiling"
    CONFIG_SITE is now a part of pkg-autotools infrastructure.
    
    This reverts commit 85448febb3c9f21e6e4082fc344b3ef46261c782.
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>
    Acked-by: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/localedef/localedef.mk
+++ package/localedef/localedef.mk
@@ -8,10 +8,6 @@ LOCALEDEF_VERSION = 2.14.1-r17443-ptx1
 LOCALEDEF_SOURCE  = localedef-eglibc-$(LOCALEDEF_VERSION).tar.bz2
 LOCALEDEF_SITE    = http://www.pengutronix.de/software/ptxdist/temporary-src
 
-# Avoid loading /usr/share/config.site that can redefine libdir when
-# the host arch is a 64bit system.
-HOST_LOCALEDEF_CONF_ENV = CONFIG_SITE="no"
-
 HOST_LOCALEDEF_CONF_OPT += \
 	--prefix=/usr \
 	--with-glibc=./eglibc
