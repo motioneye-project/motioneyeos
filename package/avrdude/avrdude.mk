635555cbd9d135eedbfc9ac23ee02f37e7d3de00
Gregory CLEMENT <gregory.clement@free-electrons.com>
2014-08-28 14:21:32 +0200

    avrdude: Depend on elfutils instead of libelf to get the libelf library
    The elfutils package provides a more recent version of the libelf, so
    let's use it. It will allow to remove the libelf package and to avoid
    conflicts with two packages providing the same library.
    
    [Peter: adjust toolchain options comment to match]
    Signed-off-by: Gregory CLEMENT <gregory.clement@free-electrons.com>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/avrdude/avrdude.mk
+++ package/avrdude/avrdude.mk
@@ -12,7 +12,7 @@ AVRDUDE_SUBDIR = avrdude
 # Sources coming from git, without generated configure and Makefile.in
 # files.
 AVRDUDE_AUTORECONF = YES
-AVRDUDE_DEPENDENCIES = libelf libusb libusb-compat ncurses \
+AVRDUDE_DEPENDENCIES = elfutils libusb libusb-compat ncurses \
 	host-flex host-bison
 AVRDUDE_LICENSE = GPLv2+
 AVRDUDE_LICENSE_FILES = avrdude/COPYING
