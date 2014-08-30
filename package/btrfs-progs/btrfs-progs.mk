a2d7725e4d3e2b415b2708bf80a78db71f801aec
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-08-30 14:51:44 +0200

    btrfs-progs: bump to version 3.16, fix static linking
    The original aim of this patch was to fix static linking of
    btrfs-progs. In order to implement this in the form of patches that
    can potentially be upstreamed, this patch first bumps the btrfs-progs
    package to the latest upstream stable version. It then:
    
     - Reworks the existing documentation disabling patch in a form that
       can potentially be upstreamed.
    
     - Adds a patch to support static building of the btrfs-progs
       binaries.
    
    Both patches have been submitted upstream, see
    http://article.gmane.org/gmane.comp.file-systems.btrfs/38145.
    
    Fixes:
    
      http://autobuild.buildroot.org/results/ddc/ddcc70143ce2c2882894184e4ce195a11407e027/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/btrfs-progs/btrfs-progs.mk
+++ package/btrfs-progs/btrfs-progs.mk
@@ -4,19 +4,31 @@
 #
 ################################################################################
 
-BTRFS_PROGS_VERSION = 3.14.2
+BTRFS_PROGS_VERSION = 3.16
 BTRFS_PROGS_SITE = https://www.kernel.org/pub/linux/kernel/people/mason/btrfs-progs
 BTRFS_PROGS_SOURCE = btrfs-progs-v$(BTRFS_PROGS_VERSION).tar.xz
 BTRFS_PROGS_DEPENDENCIES = acl attr e2fsprogs lzo util-linux zlib
 BTRFS_PROGS_LICENSE = GPLv2
 BTRFS_PROGS_LICENSE_FILES = COPYING
 
+BTRFS_PROGS_MAKE_FLAGS = DISABLE_DOCUMENTATION=1
+
+ifeq ($(BR2_PREFER_STATIC_LIB),y)
+BTRFS_PROGS_MAKE_TARGET = static
+BTRFS_PROGS_MAKE_INSTALL_TARGET = install-static
+else
+BTRFS_PROGS_MAKE_TARGET = all
+BTRFS_PROGS_MAKE_INSTALL_TARGET = install
+endif
+
 define BTRFS_PROGS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) \
+		$(BTRFS_PROGS_MAKE_FLAGS) $(BTRFS_PROGS_MAKE_TARGET)
 endef
 
 define BTRFS_PROGS_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(@D) prefix=/usr DESTDIR=$(TARGET_DIR) install
+	$(MAKE) -C $(@D) prefix=/usr DESTDIR=$(TARGET_DIR) \
+		$(BTRFS_PROGS_MAKE_FLAGS) $(BTRFS_PROGS_MAKE_INSTALL_TARGET)
 endef
 
 $(eval $(generic-package))
