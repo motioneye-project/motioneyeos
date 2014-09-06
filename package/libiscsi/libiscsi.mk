c05c751a257f4f11ea84a007ea570416dd842fc7
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-09-06 16:00:40 +0200

    libiscsi: bump version to 1.12.0
    This commit bumps libiscsi to version 1.12.0, which requires a few
    changes:
    
     - A number of patches to get libiscsi to build properly.
     - The removal of the popt dependency, which is no longer needed.
     - The removal of unnecessary spaces for alignment of variable
       definitions in libiscsi.mk.
    
    Fixes:
    
      http://autobuild.buildroot.org/results/72f/72f389edf9c5a7e35d01dc4f76fd6dd0d8110f20/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libiscsi/libiscsi.mk
+++ package/libiscsi/libiscsi.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-LIBISCSI_VERSION         = 1.6.0
-LIBISCSI_SITE            = https://github.com/downloads/sahlberg/libiscsi
-LIBISCSI_LICENSE         = GPLv2+ LGPLv2.1+
-LIBISCSI_LICENSE_FILES   = COPYING LICENCE-GPL-2.txt LICENCE-LGPL-2.1.txt
+LIBISCSI_VERSION = 1.12.0
+LIBISCSI_SITE = https://sites.google.com/site/libiscsitarballs/libiscsitarballs/
+LIBISCSI_LICENSE = GPLv2+ LGPLv2.1+
+LIBISCSI_LICENSE_FILES = COPYING LICENCE-GPL-2.txt LICENCE-LGPL-2.1.txt
 LIBISCSI_INSTALL_STAGING = YES
-LIBISCSI_DEPENDENCIES    = popt
-# fixes MIPS64 link failure
-LIBISCSI_AUTORECONF      = YES
+
+# We patch configure.ac and Makefile.am
+LIBISCSI_AUTORECONF = YES
 
 $(eval $(autotools-package))
