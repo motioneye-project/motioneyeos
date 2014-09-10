324ccec90d9c98d6aa535c96861eeb84d2e3ff33
Peter Korsgaard <peter@korsgaard.com>
2014-09-10 10:49:46 +0200

    jasper: autoreconf to fix rpath issue
    The old version of autotools used gets confused and ends up looking in
    /usr/lib for libjpeg when host == target..
    
    Fixes http://autobuild.buildroot.net/results/307/307cac65287420252a5bb64715d9a1edd90e72fa/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/jasper/jasper.mk
+++ package/jasper/jasper.mk
@@ -11,5 +11,7 @@ JASPER_INSTALL_STAGING = YES
 JASPER_DEPENDENCIES = jpeg
 JASPER_LICENSE = MIT
 JASPER_LICENSE_FILES = LICENSE
+# needed to fix rpath issue (http://autobuild.buildroot.net/results/307/307cac65287420252a5bb64715d9a1edd90e72fa/)
+JASPER_AUTORECONF = YES
 
 $(eval $(autotools-package))
