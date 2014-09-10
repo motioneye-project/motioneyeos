6b20237391971ceb94251952c1501c9197b90203
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-09-10 16:07:29 -0300

    exfat: bump to version 1.1.0
    Use gentoo distfiles since the download infra can't handle google docs
    URLs.
    
    [Peter: also pass TARGET_CFLAGS in CCFLAGS]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/exfat/exfat.mk
+++ package/exfat/exfat.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-EXFAT_VERSION = 1.0.1
-EXFAT_SITE = http://exfat.googlecode.com/files
+EXFAT_VERSION = 1.1.0
+EXFAT_SITE = http://distfiles.gentoo.org/distfiles
 EXFAT_SOURCE = fuse-exfat-$(EXFAT_VERSION).tar.gz
 EXFAT_DEPENDENCIES = host-scons libfuse
 EXFAT_LICENSE = GPLv3+
@@ -13,12 +13,12 @@ EXFAT_LICENSE_FILES = COPYING
 
 define EXFAT_BUILD_CMDS
 	(cd $(@D); \
-		$(TARGET_CONFIGURE_OPTS) $(SCONS))
+		$(TARGET_CONFIGURE_OPTS) CCFLAGS="$(TARGET_CFLAGS) -std=c99" $(SCONS))
 endef
 
 define EXFAT_INSTALL_TARGET_CMDS
 	(cd $(@D); \
-		$(TARGET_CONFIGURE_OPTS) $(SCONS) \
+		$(TARGET_CONFIGURE_OPTS) CCFLAGS="$(TARGET_CFLAGS) -std=c99" $(SCONS) \
 		DESTDIR=$(TARGET_DIR)/usr/sbin install)
 endef
 
