c37ad316975dac3bf4efd58121d4c178fd3c381f
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-09-10 16:07:30 -0300

    exfat-utils: bump to version 1.1.0
    Use gentoo distfiles since the download infra can't handle google docs
    URLs.
    
    [Peter: also pass TARGET_CFLAGS in CCFLAGS]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/exfat-utils/exfat-utils.mk
+++ package/exfat-utils/exfat-utils.mk
@@ -4,20 +4,20 @@
 #
 ################################################################################
 
-EXFAT_UTILS_VERSION = 1.0.1
-EXFAT_UTILS_SITE = http://exfat.googlecode.com/files
+EXFAT_UTILS_VERSION = 1.1.0
+EXFAT_UTILS_SITE = http://distfiles.gentoo.org/distfiles
 EXFAT_UTILS_DEPENDENCIES = host-scons
 EXFAT_UTILS_LICENSE = GPLv3+
 EXFAT_UTILS_LICENSE_FILES = COPYING
 
 define EXFAT_UTILS_BUILD_CMDS
 	(cd $(@D); \
-		$(TARGET_CONFIGURE_OPTS) $(SCONS))
+		$(TARGET_CONFIGURE_OPTS) CCFLAGS="$(TARGET_CFLAGS) -std=c99" $(SCONS))
 endef
 
 define EXFAT_UTILS_INSTALL_TARGET_CMDS
 	(cd $(@D); \
-		$(TARGET_CONFIGURE_OPTS) $(SCONS) \
+		$(TARGET_CONFIGURE_OPTS) CCFLAGS="$(TARGET_CFLAGS) -std=c99" $(SCONS) \
 		DESTDIR=$(TARGET_DIR)/usr/bin install)
 endef
 
