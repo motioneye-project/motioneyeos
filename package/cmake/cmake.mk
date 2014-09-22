4aef37b683a637c8755a8b197a46aa30674c2074
Jeremy Rosen <jeremy.rosen@openwide.fr>
2014-09-22 13:31:07 +0200

    cmake : bump to version 3.0.2
    The
    cmake-0001-FindQt4-do-not-prepend-CMake-root-path-when-searchin.patch
    patch has been merged upstream as part of commit
    e8b8b37ef6fef094940d3384df5a1d421b9fa568.
    
    [Thomas: tweak commit log by adding details about the patch being
    removed.]
    
    Signed-off-by: Jérémy Rosen <jeremy.rosen@openwide.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/cmake/cmake.mk
+++ package/cmake/cmake.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-CMAKE_VERSION_MAJOR = 2.8
-CMAKE_VERSION = $(CMAKE_VERSION_MAJOR).12.2
+CMAKE_VERSION_MAJOR = 3.0
+CMAKE_VERSION = $(CMAKE_VERSION_MAJOR).2
 CMAKE_SITE = http://www.cmake.org/files/v$(CMAKE_VERSION_MAJOR)
 CMAKE_LICENSE = BSD-3c
 CMAKE_LICENSE_FILES = Copyright.txt
