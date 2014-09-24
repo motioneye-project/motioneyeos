e25da0c74537b5262a622ba98bce46b07ed0a70d
Jérôme Pouiller <jezz@sysmic.org>
2014-09-24 08:43:04 +0200

    lttng-libust: bump to version 2.5.0
    lttng-libust-0001-Disable-liblttng-ust-dl-if-dlinfo-is-not-available.patch
    was upstreamed
    
    [Peter: also remove _AUTORECONF = YES]
    Signed-off-by: Jérôme Pouiller <jezz@sysmic.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lttng-libust/lttng-libust.mk
+++ package/lttng-libust/lttng-libust.mk
@@ -5,15 +5,13 @@
 ################################################################################
 
 LTTNG_LIBUST_SITE    = http://lttng.org/files/lttng-ust
-LTTNG_LIBUST_VERSION = 2.4.1
+LTTNG_LIBUST_VERSION = 2.5.0
 LTTNG_LIBUST_SOURCE  = lttng-ust-$(LTTNG_LIBUST_VERSION).tar.bz2
 LTTNG_LIBUST_LICENSE = LGPLv2.1; MIT for system headers; GPLv2 for liblttng-ust-ctl/ustctl.c (used by lttng-sessiond)
 LTTNG_LIBUST_LICENSE_FILES = COPYING
 
 LTTNG_LIBUST_INSTALL_STAGING = YES
 LTTNG_LIBUST_DEPENDENCIES    = liburcu util-linux
-
-LTTNG_LIBUST_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_PYTHON),y)
 LTTNG_LIBUST_DEPENDENCIES += python
