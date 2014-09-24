204fd5e01263376cc6541badc122d4a0e4c383a7
Jérôme Pouiller <jezz@sysmic.org>
2014-09-24 08:43:03 +0200

    lttng-tools: bump to version 2.5.0
    libxml2 is now necessary
    
    Signed-off-by: Jérôme Pouiller <jezz@sysmic.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lttng-tools/lttng-tools.mk
+++ package/lttng-tools/lttng-tools.mk
@@ -4,17 +4,18 @@
 #
 ################################################################################
 
-LTTNG_TOOLS_VERSION = 2.4.1
+LTTNG_TOOLS_VERSION = 2.5.0
 LTTNG_TOOLS_SITE    = http://lttng.org/files/lttng-tools
 LTTNG_TOOLS_SOURCE  = lttng-tools-$(LTTNG_TOOLS_VERSION).tar.bz2
 LTTNG_TOOLS_LICENSE = GPLv2+; LGPLv2.1+ for include/lttng/* and src/lib/lttng-ctl/*
 LTTNG_TOOLS_LICENSE_FILES = gpl-2.0.txt lgpl-2.1.txt LICENSE
+LTTNG_TOOLS_CONF_OPT += --with-xml-prefix=$(STAGING_DIR)/usr
 
 # The host-lttng-babeltrace technically isn't a required build
 # dependency. However, having the babeltrace utilities built for the
 # host is very useful, since those tools allow to convert the binary
 # trace format into an human readable format.
-LTTNG_TOOLS_DEPENDENCIES = liburcu popt host-lttng-babeltrace util-linux
+LTTNG_TOOLS_DEPENDENCIES = liburcu libxml2 popt host-lttng-babeltrace util-linux
 
 LTTNG_AUTORECONF = YES
 
