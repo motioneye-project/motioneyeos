425a668dfdb7a861457a6615e0d18ce394bfc52c
Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
2014-09-26 21:36:43 +0200

    jamvm: bump to 2.0.0 (including mips big-endian support)
    This commit bumps jamvm to version 2.0.0. From the release notes:
    
        I'm pleased to announce a new release of JamVM.  JamVM 2.0.0 is the
        first release of JamVM with support for OpenJDK (in addition to GNU
        Classpath). Although IcedTea already includes JamVM with OpenJDK
        support, this has been based on periodic snapshots of the development
        tree.
    
        JamVM 2.0.0 supports OpenJDK 6, 7 and 8 (the latest). With OpenJDK 7 and
        8 this includes full support for JSR 292 (invokedynamic). JamVM 2.0.0
        with OpenJDK 8 also includes full support for Lambda expressions (JSR
        335), type annotations (JSR 308) and method parameter reflection.
    
        In addition to OpenJDK support, JamVM 2.0.0 also includes many
        bug-fixes, performance improvements and improved compatibility (from
        running the OpenJDK jtreg tests).
    
    As this release now also supports MIPS big-endian targets, this commit
    fixes bug #7010 (https://bugs.busybox.net/show_bug.cgi?id=7010)
    
    Signed-off-by: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/jamvm/jamvm.mk
+++ package/jamvm/jamvm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-JAMVM_VERSION = 1.5.4
+JAMVM_VERSION = 2.0.0
 JAMVM_SITE = http://downloads.sourceforge.net/project/jamvm/jamvm/JamVM%20$(JAMVM_VERSION)
 JAMVM_LICENSE = GPLv2+
 JAMVM_LICENSE_FILES = COPYING
