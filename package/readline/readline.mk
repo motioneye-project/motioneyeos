063ff25f6aeb626da03c49d5216bdd3c6b4f0482
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-09-27 18:39:15 +0200

    package/readline: add host variant
    Needed by SoftEther VPN.
    
    host-readline will also be needed by host-pcre, which in turn will
    be needed by the yet-to-be-released leafnode2 package.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/readline/readline.mk
+++ package/readline/readline.mk
@@ -8,6 +8,7 @@ READLINE_VERSION = 6.3
 READLINE_SITE = $(BR2_GNU_MIRROR)/readline
 READLINE_INSTALL_STAGING = YES
 READLINE_DEPENDENCIES = ncurses
+HOST_READLINE_DEPENDENCIES = host-ncurses
 READLINE_CONF_ENV = bash_cv_func_sigsetjmp=yes \
 	bash_cv_wcwidth_broken=no
 READLINE_LICENSE = GPLv3+
@@ -20,3 +21,4 @@ endef
 READLINE_POST_INSTALL_TARGET_HOOKS += READLINE_PURGE_EXAMPLES
 
 $(eval $(autotools-package))
+$(eval $(host-autotools-package))
