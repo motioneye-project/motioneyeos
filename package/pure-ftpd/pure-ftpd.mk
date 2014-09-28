212820b65fd02bc1ef13498a9becdbd8e59eb35a
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-09-28 21:12:50 +0200

    package/pure-ftpd: New package
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/pure-ftpd/pure-ftpd.mk
@@ -0,0 +1,35 @@
+################################################################################
+#
+# pure-ftpd
+#
+################################################################################
+
+PURE_FTPD_VERSION = 1.0.36
+PURE_FTPD_SITE = http://download.pureftpd.org/pub/pure-ftpd/releases
+PURE_FTPD_LICENSE = ISC
+PURE_FTPD_LICENSE_FILES = COPYING
+PURE_FTPD_DEPENDENCIES = $(if $(BR2_PACKAGE_LIBICONV),libiconv)
+
+PURE_FTPD_CONF_OPT = \
+	--with-altlog \
+	--with-puredb \
+	--with-rfc2640
+
+ifeq ($(BR2_PACKAGE_LIBCAP),y)
+PURE_FTPD_CONF_OPT += --with-capabilities
+PURE_FTPD_DEPENDENCIES += libcap
+else
+PURE_FTPD_CONF_OPT += --without-capabilities
+endif
+
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+PURE_FTPD_CONF_OPT += --with-tls
+PURE_FTPD_DEPENDENCIES += openssl
+ifeq ($(BR2_PREFER_STATIC_LIB),y)
+PURE_FTPD_CONF_ENV += LIBS='-lssl -lcrypto -lz'
+endif
+else
+PURE_FTPD_CONF_OPT += --without-tls
+endif
+
+$(eval $(autotools-package))
