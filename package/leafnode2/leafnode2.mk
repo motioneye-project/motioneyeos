2f6b9b2b71db3e9ff3a6d57c92a5c9bd52cba206
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-09-28 12:02:12 +0200

    package/leafnode2: New package
    [Thomas:
      - Rewrap help text in Config.in
      - Improve cross-compilation patch to not call the 'amiroot' program
        on the host, while it is compiled for the target.
      - Use CONF_OPTS instead of CONF_OPT.
      - Use a pre-build hook rather than re-implementing the BUILD_CMDS
        entirely.
      - Also include COPYING file in license files.
      - License is LGPLv2.1, not LGPLv2.1+: only one file is LGPLv2.1+,
        the rest doesn't have any "or later" specification, so we assume
        it's only LGPLv2.1.]
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/leafnode2/leafnode2.mk
@@ -0,0 +1,35 @@
+################################################################################
+#
+# leafnode2
+#
+################################################################################
+
+LEAFNODE2_VERSION = 2.0.0.alpha20140727b
+LEAFNODE2_SOURCE = leafnode-$(LEAFNODE2_VERSION).tar.bz2
+LEAFNODE2_SITE = http://home.pages.de/~mandree/leafnode/beta
+LEAFNODE2_LICENSE = LGPLv2.1
+LEAFNODE2_LICENSE_FILES = COPYING COPYING.LGPL
+LEAFNODE2_DEPENDENCIES = host-pcre pcre
+
+LEAFNODE2_CONF_ENV = \
+	PCRECONFIG="$(STAGING_DIR)/usr/bin/pcre-config"
+
+LEAFNODE2_CONF_OPTS = \
+	--sysconfdir=/etc/leafnode2 \
+	--enable-spooldir=/var/spool/news
+
+# Leafnode2 needs the host version of b_sortnl during
+# compilation. Instead of creating a seperate host package and
+# installing b_sortnl to $(HOST_DIR) this binary is compiled
+# on-the-fly, host-pcre is needed for this
+define LEAFNODE2_BUILD_SORTNL_TOOL
+	cd $(@D); \
+	$(HOSTCC) $(HOST_CFLAGS) -o b_sortnl_host \
+		arc4random.c mergesort.c b_sortnl.c critmem_malloc.c \
+		critmem_realloc.c -DHAVE_CONFIG_H -I$(HOST_DIR)/usr/include \
+		-L $(HOST_DIR)/usr/lib -Wl,-rpath,$(HOST_DIR)/usr/lib -lpcre
+endef
+
+LEAFNODE2_PRE_BUILD_HOOKS += LEAFNODE2_BUILD_SORTNL_TOOL
+
+$(eval $(autotools-package))
