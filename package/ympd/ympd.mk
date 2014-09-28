40aa523af26963321443a2d96c64ce128577ca77
Eric Limpens <limpens@gmail.com>
2014-09-28 14:04:02 +0200

    ympd: new package
    [Thomas:
     - Add the package to package/Config.in
     - Add dependency on largefile
     - Add new patch to CMakeLists.txt to remove the unused C++ dependency
     - Change the existing CMakeLists.txt patch to simply remove the
       problematic code, and reword the explanation.]
    
    Signed-off-by: Eric Limpens <limpens@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/ympd/ympd.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# ympd
+#
+################################################################################
+
+YMPD_VERSION = a98d760dddff4b0dd595d977c9077f34beff05c5
+YMPD_SITE = $(call github,notandy,ympd,$(YMPD_VERSION))
+YMPD_LICENSE = GPLv2
+YMPD_LICENSE_FILE = LICENSE
+YMPD_DEPENDENCIES = libmpdclient
+
+define YMPD_MAKE_HOST_TOOL
+        $(HOSTCC) $(HOST_CFLAGS) $(@D)/htdocs/mkdata.c -o $(@D)/mkdata
+endef
+
+YMPD_PRE_BUILD_HOOKS += YMPD_MAKE_HOST_TOOL
+
+YMPD_CONF_OPT += -DMKDATA_EXE=$(@D)/mkdata
+
+$(eval $(cmake-package))
