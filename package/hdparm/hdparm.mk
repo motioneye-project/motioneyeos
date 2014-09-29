bf672f9e18850c8b6592bb72b3b602c105cbd41b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-09-29 09:44:41 -0300

    hdparm: bump to version 9.45
    Also add hash file.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Tested-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/hdparm/hdparm.mk
+++ package/hdparm/hdparm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HDPARM_VERSION = 9.43
+HDPARM_VERSION = 9.45
 HDPARM_SITE = http://downloads.sourceforge.net/project/hdparm/hdparm
 HDPARM_LICENSE = BSD-Style
 HDPARM_LICENSE_FILES = LICENSE.TXT
