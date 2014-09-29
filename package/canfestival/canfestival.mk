23c52855e212aa34d67fd8d791f82aae635a9306
Samuel Martin <s.martin49@gmail.com>
2014-09-29 23:43:20 +0200

    package/canfestival: new package
    Note: the patch adding the pkgconfig module has been sent upstream [1].
    
    [1] http://sourceforge.net/p/canfestival/mailman/message/32876320/
    
    [Thomas:
     - license seems to be only LGPLv2.1+. At least, the specific files
       pointed to be GPLv2 carry a LGPLv2.1+ header.
     - added thread dependency
     - minor rewording here and there.]
    
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>
    Cc: Eric Jarrige <eric.jarrige@armadeus.org>
    Cc: Julien Boibessot <julien.boibessot@armadeus.com>
    Cc: Davide Viti <zinosat@tiscali.it>
    Cc: Claudio Laurita <claudio.laurita@integrazionetotale.it>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/canfestival/canfestival.mk
@@ -0,0 +1,53 @@
+################################################################################
+#
+# canfestival
+#
+################################################################################
+
+# Revision 791:
+CANFESTIVAL_VERSION = 7740ac6fdedc
+CANFESTIVAL_SOURCE = $(CANFESTIVAL_VERSION).tar.bz2
+CANFESTIVAL_SITE = http://dev.automforge.net/CanFestival-3/archive
+CANFESTIVAL_LICENSE = LGPLv2.1+
+CANFESTIVAL_LICENSE_FILES = COPYING LICENCE
+CANFESTIVAL_INSTALL_STAGING = YES
+CANFESTIVAL_INSTALLED-y = src drivers
+CANFESTIVAL_INSTALLED-$(BR2_PACKAGE_CANFESTIVAL_INSTALL_EXAMPLES) += examples
+
+# canfestival uses its own hand-written build-system. Though there is
+# a configure script, it does not use the autotools, so, we use the
+# generic-package infrastructure.
+define CANFESTIVAL_CONFIGURE_CMDS
+	cd $(@D) && \
+		$(TARGET_CONFIGURE_OPTS) ./configure \
+		--target=unix \
+		--arch=$(BR2_ARCH) \
+		--timers=unix \
+		--binutils=$(TARGET_CROSS) \
+		--cc="$(TARGET_CC)" \
+		--cxx="$(TARGET_CC)" \
+		--ld="$(TARGET_CC)" \
+		--prefix=/usr \
+		--can=$(BR2_PACKAGE_CANFESTIVAL_DRIVER) \
+		$(call qstrip,$(BR2_PACKAGE_CANFESTIVAL_ADDITIONAL_OPTIONS))
+endef
+
+define CANFESTIVAL_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) all
+endef
+
+define CANFESTIVAL_INSTALL_TARGET_CMDS
+	for d in $(CANFESTIVAL_INSTALLED-y) ; do \
+		$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/$$d install \
+			 DESTDIR=$(TARGET_DIR) || exit 1 ; \
+	done
+endef
+
+define CANFESTIVAL_INSTALL_STAGING_CMDS
+	for d in $(CANFESTIVAL_INSTALLED-y) ; do \
+		$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/$$d install \
+			DESTDIR=$(STAGING_DIR) || exit 1 ; \
+	done
+endef
+
+$(eval $(generic-package))
