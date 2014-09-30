fad1e2322fe0556ebf7e8887334a9d02712980a7
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-09-30 23:38:58 +0200

    python-pyparsing: bump to version 2.0.2, enable on Python 3
    This commit bumps the pyparsing package to version 2.0.2, and
    therefore enables it usage with Python 3.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Tested-by: Christophe Vu-Brugier <cvubrugier@fastmail.fm>

--- package/python-pyparsing/python-pyparsing.mk
+++ package/python-pyparsing/python-pyparsing.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PYPARSING_VERSION         = 1.5.6
+PYTHON_PYPARSING_VERSION         = 2.0.2
 PYTHON_PYPARSING_SOURCE          = pyparsing-$(PYTHON_PYPARSING_VERSION).tar.gz
 PYTHON_PYPARSING_SITE            = http://downloads.sourceforge.net/project/pyparsing/pyparsing/pyparsing-$(PYTHON_PYPARSING_VERSION)
 PYTHON_PYPARSING_LICENSE         = MIT
