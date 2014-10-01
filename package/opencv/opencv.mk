14a97e945012b080ac97db45596fb122322c43b9
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2014-10-01 13:18:00 +0100

    opencv: Bump version to 2.4.10
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Reviewed-by: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/opencv/opencv.mk
+++ package/opencv/opencv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENCV_VERSION = 2.4.9.1
+OPENCV_VERSION = 2.4.10
 OPENCV_SITE = $(call github,itseez,opencv,$(OPENCV_VERSION))
 OPENCV_INSTALL_STAGING = YES
 OPENCV_LICENSE = BSD-3c
