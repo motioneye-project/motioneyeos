3251b7793f607b162ada8d86aab283e4aa0445e1
Peter Korsgaard <peter@korsgaard.com>
2014-10-01 13:57:34 +0200

    rsyslog: bump version
    To the latest release in the 7.x series. Also remove the two patches now
    upstream and add tarball hash.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rsyslog/rsyslog.mk
+++ package/rsyslog/rsyslog.mk
@@ -4,12 +4,11 @@
 #
 ################################################################################
 
-RSYSLOG_VERSION = 7.6.0
+RSYSLOG_VERSION = 7.6.5
 RSYSLOG_SITE = http://rsyslog.com/files/download/rsyslog
 RSYSLOG_LICENSE = GPLv3 LGPLv3 Apache-2.0
 RSYSLOG_LICENSE_FILES = COPYING COPYING.LESSER COPYING.ASL20
 RSYSLOG_DEPENDENCIES = zlib libestr liblogging json-c util-linux host-pkgconf
-RSYSLOG_AUTORECONF = YES
 
 RSYSLOG_CONF_OPT = --disable-testbench \
 		   --enable-cached-man-pages
