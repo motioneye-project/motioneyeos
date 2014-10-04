2210e58c8fcfa09818c02868e7e5656ec7319e7c
Yegor Yefremov <yegorslists@googlemail.com>
2014-10-04 21:20:29 +0200

    ser2net: bump to 2.10.0
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ser2net/ser2net.mk
+++ package/ser2net/ser2net.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SER2NET_VERSION = 2.9.1
+SER2NET_VERSION = 2.10.0
 SER2NET_SITE = http://downloads.sourceforge.net/project/ser2net/ser2net
 SER2NET_LICENSE = GPLv2+
 SER2NET_LICENSE_FILES = COPYING
