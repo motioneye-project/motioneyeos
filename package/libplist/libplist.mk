fe70cb0187d3e1e75e009b924392fee9cc4dc680
Yann E. MORIN <yann.morin.1998@free.fr>
2014-10-05 15:23:22 +0200

    package/libplist: bump version
    libplist has switched to using autotools, so remove our cmake-related
    patches.
    
    Completely disable Python bindings:
      - it requires cython, which we do not have packaged (so far)
      - the only user of libplist is XBMC, which does not need the Python
        bindings.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Cc: Samuel Martin <s.martin49@gmail.com>
    Tested-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libplist/libplist.mk
+++ package/libplist/libplist.mk
@@ -4,25 +4,21 @@
 #
 ################################################################################
 
-LIBPLIST_VERSION = 1.6
+LIBPLIST_VERSION = 1.11
 LIBPLIST_SITE = http://cgit.sukimashita.com/libplist.git/snapshot
-LIBPLIST_DEPENDENCIES = libxml2
+LIBPLIST_DEPENDENCIES = libxml2 host-pkgconf
 LIBPLIST_INSTALL_STAGING = YES
-LIBPLIST_MAKE = $(MAKE1)
 LIBPLIST_LICENSE = LGPLv2.1+
 LIBPLIST_LICENSE_FILES = COPYING
 
-ifeq ($(BR2_PACKAGE_PYTHON)$(BR2_PACKAGE_PYTHON3),y)
-LIBPLIST_DEPENDENCIES += \
-	python$(if $(BR2_PACKAGE_PYTHON3),3) \
-	host-swig
-LIBPLIST_CONF_OPTS += \
-	-DENABLE_PYTHON=ON \
-	-DSWIG_EXECUTABLE=$(SWIG)
-else
-LIBPLIST_CONF_OPTS += \
-	-DENABLE_PYTHON=OFF \
-	-DSWIG_EXECUTABLE=SWIG_EXECUTABLE-NOTFOUND
-endif
+# Straight out of the git tree:
+LIBPLIST_AUTORECONF = YES
 
-$(eval $(cmake-package))
+# Does not build in parallel
+LIBPLIST_MAKE = $(MAKE1)
+
+# Disable building Python bindings, because it requires host-cython, which
+# is not packaged in Buildroot at all.
+LIBPLIST_CONF_OPTS = --without-cython
+
+$(eval $(autotools-package))
