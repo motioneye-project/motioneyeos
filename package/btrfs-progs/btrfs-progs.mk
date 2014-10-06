eab4e5605bf72d316eeded9fccdda26f05d75bba
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-10-06 17:23:36 -0300

    btrfs-progs: bump to version 3.16.2
    Patches now upstream.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/btrfs-progs/btrfs-progs.mk
+++ package/btrfs-progs/btrfs-progs.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-BTRFS_PROGS_VERSION = 3.16
-BTRFS_PROGS_SITE = https://www.kernel.org/pub/linux/kernel/people/mason/btrfs-progs
+BTRFS_PROGS_VERSION = 3.16.2
+BTRFS_PROGS_SITE = https://www.kernel.org/pub/linux/kernel/people/kdave/btrfs-progs
 BTRFS_PROGS_SOURCE = btrfs-progs-v$(BTRFS_PROGS_VERSION).tar.xz
 BTRFS_PROGS_DEPENDENCIES = acl attr e2fsprogs lzo util-linux zlib
 BTRFS_PROGS_LICENSE = GPLv2
