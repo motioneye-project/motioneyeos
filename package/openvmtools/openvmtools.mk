7349af1bc4ed941d59c0c9971007479881379d3f
Karoly Kasza <kaszak@gmail.com>
2014-10-06 13:17:18 +0200

    openvmtools: remove unnecessary C flag
    Since commit 8bec0a1af57165081dc275f813c8c5c74ff12c14
    the -Wno-unused-local-typedefs CFLAG in not needed.
    
    Signed-off-by: Karoly Kasza <kaszak@gmail.com>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/openvmtools/openvmtools.mk
+++ package/openvmtools/openvmtools.mk
@@ -41,7 +41,6 @@ endif
 
 ifeq ($(BR2_PACKAGE_OPENVMTOOLS_PAM),y)
 OPENVMTOOLS_CONF_OPTS += --with-pam
-OPENVMTOOLS_MAKE_OPTS += CFLAGS+="-Wno-unused-local-typedefs"
 OPENVMTOOLS_DEPENDENCIES += linux-pam
 else
 OPENVMTOOLS_CONF_OPTS += --without-pam
