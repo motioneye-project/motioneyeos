9a0990c4088635ab4869849b79870c8b781fc5b8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-10-08 10:19:45 -0300

    dash: bump to version 0.5.8-1
    Add hash file and use $(INSTALL) instead of cp for proper mode handling.
    
    [Peter: drop /bin/sh handling as we're going to handle it globally]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/dash/dash.mk
+++ package/dash/dash.mk
@@ -4,15 +4,15 @@
 #
 ################################################################################
 
-DASH_VERSION = 0.5.7
+DASH_VERSION = 0.5.8
 DASH_SOURCE = dash_$(DASH_VERSION).orig.tar.gz
 DASH_SITE = $(BR2_DEBIAN_MIRROR)/debian/pool/main/d/dash
-DASH_PATCH = dash_$(DASH_VERSION)-3.diff.gz
+DASH_PATCH = dash_$(DASH_VERSION)-1.diff.gz
 DASH_LICENSE = BSD-3c, GPLv2+ (mksignames.c)
 DASH_LICENSE_FILES = COPYING
 
 define DASH_INSTALL_TARGET_CMDS
-	cp -a $(@D)/src/dash $(TARGET_DIR)/bin/dash
+	$(INSTALL) -m 0755 $(@D)/src/dash $(TARGET_DIR)/bin/dash
 endef
 
 $(eval $(autotools-package))
