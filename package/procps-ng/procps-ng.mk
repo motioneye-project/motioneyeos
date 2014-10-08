d1058c2d2d9204bea3a903cc92c1d21f4c5aae54
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-10-08 10:19:48 -0300

    procps-ng: bump to version 3.3.10
    Also add hash file.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/procps-ng/procps-ng.mk
+++ package/procps-ng/procps-ng.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PROCPS_NG_VERSION = 3.3.9
+PROCPS_NG_VERSION = 3.3.10
 PROCPS_NG_SOURCE = procps-ng-$(PROCPS_NG_VERSION).tar.xz
 PROCPS_NG_SITE = http://downloads.sourceforge.net/project/procps-ng/Production
 PROCPS_NG_LICENSE = GPLv2+, libproc and libps LGPLv2+
