c3bf16f884b8c5518a47dd95922bb4fc0bfc9d4b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-10-08 10:19:46 -0300

    zsh: security bump to version 5.0.7
    Fixes shellshock-alike exploits.
    
    Install binary to /bin as all shells should be and add hash file.
    
    [Peter: drop /bin/sh handling as we're going to handle it globally]
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/zsh/zsh.mk
+++ package/zsh/zsh.mk
@@ -4,10 +4,17 @@
 #
 ################################################################################
 
-ZSH_VERSION = 5.0.6
-ZSH_SITE = http://downloads.sourceforge.net/project/zsh/zsh/$(ZSH_VERSION)
+ZSH_VERSION = 5.0.7
+ZSH_SITE = http://www.zsh.org/pub
+ZSH_DEPENDENCIES = ncurses
+ZSH_CONF_OPTS = --bindir=/bin
 ZSH_LICENSE = MIT-like
 ZSH_LICENSE_FILES = LICENCE
-ZSH_DEPENDENCIES = ncurses
+
+# Remove versioned zsh-x.y.z binary taking up space
+define ZSH_TARGET_INSTALL_FIXUPS
+	rm -f $(TARGET_DIR)/bin/zsh-$(ZSH_VERSION)
+endef
+ZSH_POST_INSTALL_TARGET_HOOKS += ZSH_TARGET_INSTALL_FIXUPS
 
 $(eval $(autotools-package))
