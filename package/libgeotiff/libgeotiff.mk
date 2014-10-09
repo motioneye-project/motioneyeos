ca3465f6673201a28af076755cc01c57bf1090de
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-10-09 18:10:45 +0200

    libgeotiff: fix static linking issue
    This commit adds a patch to improve the libgeotiff configure script to
    use pkg-config to detect libtiff, which allows to properly take into
    account dependant libraries such as libz and libjpeg.
    
    As a consequence, we now autoreconf the libgeotiff package, and add
    host-pkgconf as a dependency.
    
    Fixes:
    
      http://autobuild.buildroot.org/results/649/6498d6516a412b12d68fa9f6a66172021abadc34/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libgeotiff/libgeotiff.mk
+++ package/libgeotiff/libgeotiff.mk
@@ -6,7 +6,8 @@
 
 LIBGEOTIFF_VERSION = 1.4.0
 LIBGEOTIFF_SITE = http://download.osgeo.org/geotiff/libgeotiff
-LIBGEOTIFF_DEPENDENCIES = tiff
+LIBGEOTIFF_DEPENDENCIES = tiff host-pkgconf
 LIBGEOTIFF_INSTALL_STAGING = YES
+LIBGEOTIFF_AUTORECONF = YES
 
 $(eval $(autotools-package))
