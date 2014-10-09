b2ec261b3a9a123e6f384ab30f89689f2cf90003
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-10-09 18:10:46 +0200

    libgeotiff: explicit optional dependencies on zlib and jpeg
    In order to provide consistent behavior, this commit explicits the
    optional dependencies of libgeotiff on zlib and jpeg.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libgeotiff/libgeotiff.mk
+++ package/libgeotiff/libgeotiff.mk
@@ -10,4 +10,18 @@ LIBGEOTIFF_DEPENDENCIES = tiff host-pkgconf
 LIBGEOTIFF_INSTALL_STAGING = YES
 LIBGEOTIFF_AUTORECONF = YES
 
+ifeq ($(BR2_PACKAGE_ZLIB),y)
+LIBGEOTIFF_DEPENDENCIES += zlib
+LIBGEOTIFF_CONF_OPTS += --with-zlib
+else
+LIBGEOTIFF_CONF_OPTS += --without-zlib
+endif
+
+ifeq ($(BR2_PACKAGE_JPEG),y)
+LIBGEOTIFF_DEPENDENCIES += jpeg
+LIBGEOTIFF_CONF_OPTS += --with-jpeg
+else
+LIBGEOTIFF_CONF_OPTS += --without-jpeg
+endif
+
 $(eval $(autotools-package))
