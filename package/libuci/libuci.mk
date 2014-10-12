ab73c6c7b9a2c4cba0a36347898757240ec03479
Alexey Mednyy <swexru@gmail.com>
2014-10-12 12:20:32 +0400

    libuci: bump version
    Signed-off-by: Alexey Mednyy <swexru@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libuci/libuci.mk
+++ package/libuci/libuci.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUCI_VERSION = ed938cc8e423d4a33b8f31e6e6d1eb0805ae3d10
+LIBUCI_VERSION = e339407372ffc70b1451e4eda218c01aa95a6a7f
 LIBUCI_SITE = git://nbd.name/uci.git
 LIBUCI_LICENSE = LGPLv2.1
 LIBUCI_INSTALL_STAGING = YES
