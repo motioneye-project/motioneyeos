1dbdf1e2a297f269c2820399705ed16b63796a2d
Jörg Krause <jkrause@posteo.de>
2014-10-12 20:55:10 +0200

    package/upmpdcli: fix missing dependency of libcurl
    Signed-off-by: Jörg Krause <jkrause@posteo.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/upmpdcli/upmpdcli.mk
+++ package/upmpdcli/upmpdcli.mk
@@ -8,7 +8,7 @@ UPMPDCLI_VERSION = 0.8.3
 UPMPDCLI_SITE = http://www.lesbonscomptes.com/upmpdcli/downloads
 UPMPDCLI_LICENSE = GPLv2+
 UPMPDCLI_LICENSE_FILES = COPYING
-UPMPDCLI_DEPENDENCIES = expat libupnp libmpdclient
+UPMPDCLI_DEPENDENCIES = expat libcurl libupnp libmpdclient
 
 # Upmpdcli only runs if user upmpdcli exists
 define UPMPDCLI_USERS
