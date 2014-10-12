cc27551feaf50fc8c66cb0a1cb0321354550d1cb
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-10-12 16:58:24 +0200

    inotify-tools: Bump version
    The release is a few years old but the repository is still going, so
    uses the SHA1 and uses the github function to download it.
    
    Also, it now needs AUTORECONF.
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/inotify-tools/inotify-tools.mk
+++ package/inotify-tools/inotify-tools.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-INOTIFY_TOOLS_VERSION = 3.14
-INOTIFY_TOOLS_SITE = http://github.com/downloads/rvoicilas/inotify-tools
+INOTIFY_TOOLS_VERSION = 06007d350faa8731c67e186923ab417486104719
+INOTIFY_TOOLS_SITE = $(call github,rvoicilas,inotify-tools,$(INOTIFY_TOOLS_VERSION))
 INOTIFY_TOOLS_LICENSE = GPL
 INOTIFY_TOOLS_LICENSE_FILES = COPYING
 INOTIFY_TOOLS_INSTALL_STAGING = YES
+INOTIFY_TOOLS_AUTORECONF = YES
 
 $(eval $(autotools-package))
