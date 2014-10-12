b05963181a2402e446e4a917fc4e7ad655ba6978
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-10-12 20:00:27 +0200

    snowball-hdmiservice: Use github call
    Also drop trailing spaces.
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/snowball-hdmiservice/snowball-hdmiservice.mk
+++ package/snowball-hdmiservice/snowball-hdmiservice.mk
@@ -5,13 +5,13 @@
 ################################################################################
 
 SNOWBALL_HDMISERVICE_VERSION = f75c99d1c52707240a78b4ba78e41d20d3aa3b08
-SNOWBALL_HDMISERVICE_SITE = https://github.com/igloocommunity/hdmiservice
+SNOWBALL_HDMISERVICE_SITE = $(call github,igloocommunity,hdmiservice,$(SNOWBALL_HDMISERVICE_VERSION))
 SNOWBALL_HDMISERVICE_LICENSE = MIT
 SNOWBALL_HDMISERVICE_LICENSE_FILES = debian/copyright
 SNOWBALL_HDMISERVICE_INSTALL_STAGING = YES
 
 define SNOWBALL_HDMISERVICE_BUILD_CMDS
-	$(MAKE) -C $(@D) CC="$(TARGET_CC) $(TARGET_CFLAGS)" 	
+	$(MAKE) -C $(@D) CC="$(TARGET_CC) $(TARGET_CFLAGS)"
 endef
 
 define SNOWBALL_HDMISERVICE_INSTALL_STAGING_CMDS
