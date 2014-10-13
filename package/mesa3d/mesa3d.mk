d2bb10e322bc0629e39b7aec3b3d7858c74ea32f
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-10-13 20:05:26 +0200

    package/mesa3d: Bump version to 10.3.1
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-MESA3D_VERSION = 10.3.0
+MESA3D_VERSION = 10.3.1
 MESA3D_SOURCE = MesaLib-$(MESA3D_VERSION).tar.bz2
-MESA3D_SITE = ftp://ftp.freedesktop.org/pub/mesa/10.3
+MESA3D_SITE = ftp://ftp.freedesktop.org/pub/mesa/$(MESA3D_VERSION)
 MESA3D_LICENSE = MIT, SGI, Khronos
 MESA3D_LICENSE_FILES = docs/license.html
 MESA3D_AUTORECONF = YES
