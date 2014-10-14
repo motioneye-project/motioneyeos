1f3481268c97e231632547a85c02a02df2d584de
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-10-14 20:28:23 +0200

    package/libdvbcsa: New package
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/libdvbcsa/libdvbcsa.mk
@@ -0,0 +1,31 @@
+################################################################################
+#
+# libdvbcsa
+#
+################################################################################
+
+LIBDVBCSA_VERSION = 1.1.0
+LIBDVBCSA_SITE = http://download.videolan.org/pub/videolan/libdvbcsa/$(LIBDVBCSA_VERSION)
+LIBDVBCSA_LICENSE = GPLv2+
+LIBDVBCSA_LICENSE_FILES = COPYING
+LIBDVBCSA_INSTALL_STAGING = YES
+
+ifeq ($(BR2_X86_CPU_HAS_MMX),y)
+LIBDVBCSA_CONF_OPTS += --enable-mmx
+else
+LIBDVBCSA_CONF_OPTS += --disable-mmx
+endif
+
+ifeq ($(BR2_X86_CPU_HAS_SSE2),y)
+LIBDVBCSA_CONF_OPTS += --enable-sse2
+else
+LIBDVBCSA_CONF_OPTS += --disable-sse2
+endif
+
+ifeq ($(BR2_POWERPC_CPU_HAS_ALTIVEC),y)
+LIBDVBCSA_CONF_OPTS += --enable-altivec
+else
+LIBDVBCSA_CONF_OPTS += --disable-altivec
+endif
+
+$(eval $(autotools-package))
