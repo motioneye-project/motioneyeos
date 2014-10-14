10772176f10aed43811ac2ae410fe08a88042c3e
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-10-14 07:54:01 -0300

    make: bump to version 4.1
    Also add hash file.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/make/make.mk
+++ package/make/make.mk
@@ -4,12 +4,11 @@
 #
 ################################################################################
 
-MAKE_VERSION = 4.0
+MAKE_VERSION = 4.1
 MAKE_SOURCE = make-$(MAKE_VERSION).tar.bz2
 MAKE_SITE = $(BR2_GNU_MIRROR)/make
+MAKE_DEPENDENCIES = $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext)
 MAKE_LICENSE = GPLv3+
 MAKE_LICENSE_FILES = COPYING
-
-MAKE_DEPENDENCIES = $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext)
 
 $(eval $(autotools-package))
