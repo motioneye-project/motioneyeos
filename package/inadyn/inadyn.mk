fe0261aac1aba903716321244dab013a90918acc
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-10-16 14:06:05 -0300

    inadyn: bump to version 1.99.11
    Update initscript for the /usr/sbin -> /usr/bin change which is the
    default installation directory now that it's using autotools infra.
    Also add hash file.
    Make the initscript use tabs instead of spaces.
    Make the config file installation conditional.
    Now with optional gnutls support for HTTPS.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/inadyn/inadyn.mk
+++ package/inadyn/inadyn.mk
@@ -4,33 +4,31 @@
 #
 ################################################################################
 
-INADYN_VERSION = 1.99.9
+INADYN_VERSION = 1.99.11
 INADYN_SITE = $(call github,troglobit,inadyn,$(INADYN_VERSION))
 INADYN_LICENSE = GPLv2+
 INADYN_LICENSE_FILES = COPYING LICENSE
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
+INADYN_CONF_OPTS += --enable-openssl
 INADYN_DEPENDENCIES += openssl
+else ifeq ($(BR2_PACKAGE_GNUTLS),y)
+INADYN_DEPENDENCIES += gnutls
 else
-define INADYN_DISABLE_OPENSSL
-	$(SED) '/ssl/Id' $(@D)/config.mk
-endef
+INADYN_CONF_OPTS += --disable-ssl
 endif
-INADYN_POST_PATCH_HOOKS += INADYN_DISABLE_OPENSSL
 
-define INADYN_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
+define INADYN_INSTALL_SAMPLE_CONFIG
+	@if [ ! -f $(TARGET_DIR)/etc/inadyn.conf ]; then \
+		$(INSTALL) -D -m 0600 package/inadyn/inadyn.conf \
+			$(TARGET_DIR)/etc/inadyn.conf; \
+	fi
 endef
-
-define INADYN_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/src/inadyn $(TARGET_DIR)/usr/sbin/inadyn
-	$(INSTALL) -D -m 0600 package/inadyn/inadyn.conf \
-		$(TARGET_DIR)/etc/inadyn.conf
-endef
+INADYN_POST_INSTALL_TARGET_HOOKS += INADYN_INSTALL_SAMPLE_CONFIG
 
 define INADYN_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 0755 package/inadyn/S70inadyn \
 		$(TARGET_DIR)/etc/init.d/S70inadyn
 endef
 
-$(eval $(generic-package))
+$(eval $(autotools-package))
