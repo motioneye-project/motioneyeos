4f5bba9854c07a6d564a5659bf7b66114383b715
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-10-18 21:05:38 +0200

    package/libplist: Bump version to 1.12
    Quoting http://cgit.sukimashita.com/libplist.git/tree/NEWS
    "  * Fix parallel build for autotools"
    
    Therefore removed MAKE1, tested with -j90
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libplist/libplist.mk
+++ package/libplist/libplist.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBPLIST_VERSION = 1.11
+LIBPLIST_VERSION = 1.12
 LIBPLIST_SITE = http://cgit.sukimashita.com/libplist.git/snapshot
 LIBPLIST_DEPENDENCIES = libxml2 host-pkgconf
 LIBPLIST_INSTALL_STAGING = YES
@@ -13,9 +13,6 @@ LIBPLIST_LICENSE_FILES = COPYING
 
 # Straight out of the git tree:
 LIBPLIST_AUTORECONF = YES
-
-# Does not build in parallel
-LIBPLIST_MAKE = $(MAKE1)
 
 # Disable building Python bindings, because it requires host-cython, which
 # is not packaged in Buildroot at all.
