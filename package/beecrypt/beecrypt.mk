2717199e1359d287cc47fa255928c7737f09a9a9
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-10-19 19:07:57 +0200

    package/beecrypt: Replace work-around to fix gcc-4.7 compile error
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/beecrypt/beecrypt.mk
+++ package/beecrypt/beecrypt.mk
@@ -11,12 +11,6 @@ BEECRYPT_INSTALL_STAGING = YES
 BEECRYPT_LICENSE = LGPLv2.1+
 BEECRYPT_LICENSE_FILES = COPYING.LIB
 
-# beecrypt contains C++ code that g++ 4.7 doesn't really
-# like. Upstream does not seem to be really active, so workaround this
-# by passing -fpermissive.
-BEECRYPT_CONF_ENV = \
-	CXXFLAGS="$(TARGET_CXXFLAGS) -fpermissive"
-
 BEECRYPT_CONF_OPTS = \
 		--disable-expert-mode \
 		--without-java \
