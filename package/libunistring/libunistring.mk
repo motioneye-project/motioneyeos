c2a5b777a5ba923f440a21497a97d32b64f09164
Pedro Aguilar <paguilar@paguilar.org>
2014-10-20 00:36:53 +0200

    libunistring: new package
    [Thomas:
      - adjust the title of the commit
      - remove the thread dependency, by using the --disable-threads
        option when needed
      - remove LIBUNISTRING_SOURCE, since its value was the default
      - use BR2_GNU_MIRROR in LIBUNISTRING_SITE
      - adjust the license, which really is LGPLv3+, not LGPLv2. The
        license file is COPYING.LIB.]
    
    Signed-off-by: Pedro Aguilar <paguilar@paguilar.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/libunistring/libunistring.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# libunistring
+#
+################################################################################
+
+LIBUNISTRING_VERSION = 0.9.4
+LIBUNISTRING_SITE = $(BR2_GNU_MIRROR)/libunistring
+LIBUNISTRING_INSTALL_STAGING = YES
+LIBUNISTRING_LICENSE = LGPLv3+
+LIBUNISTRING_LICENSE_FILES = COPYING.LIB
+
+ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
+LIBUNISTRING_CONF_OPTS += --enable-threads=posix
+else
+LIBUNISTRING_CONF_OPTS += --disable-threads
+endif
+
+$(eval $(autotools-package))
