2cb7b9fe1e258fc2f7fe810dcb69390e4f49e7d0
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-10-21 22:26:53 +0200

    gst1-libav: use the new BR2_ARM_CPU_ARM* options
    Instead of using the Config.in options for each individual ARM core,
    use the newly introduced BR2_ARM_CPU_ARM* options, which exist per ARM
    architecture.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gstreamer1/gst1-libav/gst1-libav.mk
+++ package/gstreamer1/gst1-libav/gst1-libav.mk
@@ -49,10 +49,10 @@ endif
 # Explicitly disable everything that doesn't match for ARM
 # FFMPEG "autodetects" by compiling an extended instruction via AS
 # This works on compilers that aren't built for generic by default
-ifeq ($(BR2_arm920t)$(BR2_arm922t)$(BR2_strongarm)$(BR2_fa526),y)
+ifeq ($(BR2_ARM_CPU_ARMV4),y)
 GST1_LIBAV_CONF_EXTRA_OPTS += --disable-armv5te
 endif
-ifeq ($(BR2_arm1136jf_s)$(BR2_arm1176jz_s)$(BR2_arm1176jzf_s),y)
+ifeq ($(BR2_ARM_CPU_ARMV6),y)
 GST1_LIBAV_CONF_EXTRA_OPTS += --enable-armv6
 else
 GST1_LIBAV_CONF_EXTRA_OPTS += --disable-armv6 --disable-armv6t2
