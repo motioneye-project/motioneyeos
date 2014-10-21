98913307bc7a784469e9868993f7ac0506d33e07
Jörg Krause <jkrause@posteo.de>
2014-10-21 22:11:59 +0200

    libupnpp: new package
    [Thomas: adjust dependencies on the comment.]
    
    Signed-off-by: Jörg Krause <jkrause@posteo.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Tested-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/libupnpp/libupnpp.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# libupnpp
+#
+################################################################################
+
+LIBUPNPP_VERSION = 0.8.5
+LIBUPNPP_SITE = http://www.lesbonscomptes.com/upmpdcli/downloads
+LIBUPNPP_LICENSE = GPLv2+
+LIBUPNPP_LICENSE_FILES = COPYING
+LIBUPNPP_INSTALL_STAGING = YES
+LIBUPNPP_DEPENDENCIES = expat libcurl libupnp
+
+$(eval $(autotools-package))
