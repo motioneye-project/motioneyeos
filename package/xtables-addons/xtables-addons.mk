490917387a0ff6969f8130be38993eed5f06f73e
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-10-21 18:10:50 -0300

    xtables-addons: new package
    The geoip "helpers" are basically scripts that download and reformat
    the geoip database in a form usable by xt_geoip.
    
    The netfilter (kernel & userland) sides of it are built and installed.
    Since there are many considerations to geoip databases (free,
    commercial and variants for each) it's left to the user to deal with
    that if they plan to use the extension which is only one among many.
    
    [Thomas:
     - Take into account the rename of BR2_PREFER_STATIC_LIB to
       BR2_STATIC_LIBS
     - Remove "depends on BR2_LINUX_KERNEL" as suggested by Arnout.
     - Move XTABLES_ADDONS_CONF_OPTS a bit further down, with newlines
       around it, and adjust the indentation of the first line. Just to
       make it slightly more readable.]
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/xtables-addons/xtables-addons.mk
@@ -0,0 +1,33 @@
+################################################################################
+#
+# xtables-addons
+#
+################################################################################
+
+XTABLES_ADDONS_VERSION = 2.6
+XTABLES_ADDONS_SOURCE = xtables-addons-$(XTABLES_ADDONS_VERSION).tar.xz
+XTABLES_ADDONS_SITE = http://downloads.sourceforge.net/project/xtables-addons/Xtables-addons/$(XTABLES_ADDONS_VERSION)
+XTABLES_ADDONS_DEPENDENCIES = iptables linux host-pkgconf
+XTABLES_ADDONS_LICENSE = GPLv2+
+XTABLES_ADDONS_LICENSE_FILES = LICENSE
+
+XTABLES_ADDONS_CONF_OPTS = \
+	--with-kbuild="$(LINUX_DIR)" \
+	--with-xtables="$(STAGING_DIR)/usr" \
+	--with-xtlibdir="/usr/lib/xtables"
+
+# geoip helpers need perl with modules and unzip so disable
+define XTABLES_DISABLE_GEOIP_HELPERS
+	$(SED) 's/ geoip//' $(@D)/Makefile.in
+endef
+XTABLES_ADDONS_POST_PATCH_HOOKS += XTABLES_DISABLE_GEOIP_HELPERS
+
+define XTABLES_ADDONS_BUILD_CMDS
+	$(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS)
+endef
+
+define XTABLES_ADDONS_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) DESTDIR="$(TARGET_DIR)" install
+endef
+
+$(eval $(autotools-package))
