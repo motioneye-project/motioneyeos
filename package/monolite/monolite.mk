9cd62c96e9d4db3e688e5a28abbb2a61d4ccc90d
Angelo Compagnucci <angelo.compagnucci@gmail.com>
2014-10-22 22:22:09 +0200

    package/monolite: new package
    This patch adds Monolite package. This package is a dependency of Mono
    package.
    
    [Thomas: adjust license information.]
    
    Signed-off-by: Angelo Compagnucci <angelo.compagnucci@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/monolite/monolite.mk
@@ -0,0 +1,17 @@
+#############################################################
+#
+# monolite
+#
+#############################################################
+
+MONOLITE_VERSION = 111
+MONOLITE_SITE = http://storage.bos.xamarin.com/mono-dist-master/latest/
+MONOLITE_SOURCE = monolite-$(MONOLITE_VERSION)-latest.tar.gz
+MONOLITE_LICENSE = LGPLv2 or commercial
+
+define HOST_MONOLITE_INSTALL_CMDS
+	mkdir -p $(HOST_DIR)/usr/lib/monolite
+	cp $(@D)/* $(HOST_DIR)/usr/lib/monolite
+endef
+
+$(eval $(host-generic-package))
