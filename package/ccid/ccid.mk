aa940483f29e1ec07e6436d91688eef51aade886
Karoly Kasza <kaszak@gmail.com>
2014-10-23 16:48:32 +0200

    package/ccid: bump to 1.4.18, move download URL to snapshot.debian.org
    Signed-off-by: Karoly Kasza <kaszak@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ccid/ccid.mk
+++ package/ccid/ccid.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-CCID_VERSION = 1.4.14
-CCID_SOURCE = ccid-$(CCID_VERSION).tar.bz2
-CCID_SITE = http://alioth.debian.org/frs/download.php/file/3971
+CCID_VERSION = 1.4.18
+CCID_SOURCE = ccid_$(CCID_VERSION).orig.tar.bz2
+CCID_SITE = http://snapshot.debian.org/archive/debian/20141023T043132Z/pool/main/c/ccid
 CCID_LICENSE = LGPLv2.1+
 CCID_LICENSE_FILES = COPYING
 CCID_INSTALL_STAGING = YES
