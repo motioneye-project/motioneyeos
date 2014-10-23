0659ca799fe0b8945880fb768364a6e7721b132e
Kelvin Cheung <keguang.zhang@gmail.com>
2014-10-23 19:13:24 +0800

    tftpd: install TFTP client as well
    This patch installs TFTP client as well and overrides BusyBox
    implementations if BusyBox is enabled.
    
    [Thomas: fix typo SYSKLOGD_DEPENDENCIES -> TFTPD_DEPENDENCIES noticed
    by Baruch.]
    
    Signed-off-by: Kelvin Cheung <keguang.zhang@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/tftpd/tftpd.mk
+++ package/tftpd/tftpd.mk
@@ -13,7 +13,13 @@ ifneq ($(BR2_INET_IPV6),y)
 TFTPD_CONF_OPTS += --without-ipv6
 endif
 
+# Override BusyBox implementations if BusyBox is enabled.
+ifeq ($(BR2_PACKAGE_BUSYBOX),y)
+TFTPD_DEPENDENCIES += busybox
+endif
+
 define TFTPD_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/tftp/tftp $(TARGET_DIR)/usr/bin/tftp
 	$(INSTALL) -D $(@D)/tftpd/tftpd $(TARGET_DIR)/usr/sbin/tftpd
 	$(INSTALL) -D package/tftpd/S80tftpd-hpa $(TARGET_DIR)/etc/init.d/
 endef
