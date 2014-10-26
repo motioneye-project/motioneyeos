56a902f40ed10c757b91d073ea9092ba33dc29b1
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-10-26 19:45:19 +0100

    libconfuse: bump version
    The tag v2.7 is from 2010, no release has been since then and a few
    changes have been made, notably a LICENSE file was added.
    
    [Thomas: fix minor typos in commit log.]
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libconfuse/libconfuse.mk
+++ package/libconfuse/libconfuse.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-LIBCONFUSE_VERSION = V2_7
+LIBCONFUSE_VERSION = 9413a82f9cb56a94f71fa420f146e841c5372ed8
 LIBCONFUSE_SITE = $(call github,martinh,libconfuse,$(LIBCONFUSE_VERSION))
 LIBCONFUSE_INSTALL_STAGING = YES
 LIBCONFUSE_CONF_OPTS = --disable-rpath
 LIBCONFUSE_DEPENDENCIES = host-flex
 LIBCONFUSE_LICENSE = ISC
-LIBCONFUSE_LICENSE_FILES = src/confuse.c
+LIBCONFUSE_LICENSE_FILES = LICENSE
 
 # Fresh from the repository, no configure et al.
 LIBCONFUSE_AUTORECONF = YES
