fb03ea6a769255530fa84c3eab68d84890c25105
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-10-26 19:45:12 +0100

    filemq: bump version
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/filemq/filemq.mk
+++ package/filemq/filemq.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FILEMQ_VERSION = 9a24134d4c0a84abc5eebf1dfb2eb184adee72df
+FILEMQ_VERSION = 482797b8aa30fcc9ea1377aabdf2b0769f9f698e
 FILEMQ_SITE = $(call github,zeromq,filemq,$(FILEMQ_VERSION))
 
 FILEMQ_AUTORECONF = YES
