17a74d47dbe27b608c7ac099f09df8d6ae1b2e2b
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-10-26 19:45:31 +0100

    python-rtslib-fb: bump version
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-rtslib-fb/python-rtslib-fb.mk
+++ package/python-rtslib-fb/python-rtslib-fb.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_RTSLIB_FB_VERSION = v2.1.fb49
+PYTHON_RTSLIB_FB_VERSION = v2.1.fb50
 PYTHON_RTSLIB_FB_SITE = $(call github,agrover,rtslib-fb,$(PYTHON_RTSLIB_FB_VERSION))
 PYTHON_RTSLIB_FB_LICENSE = Apache-2.0
 PYTHON_RTSLIB_FB_LICENSE_FILES = COPYING
