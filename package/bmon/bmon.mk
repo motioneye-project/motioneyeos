148ea737c084f6cee7d46cca7a3548959fc43965
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-10-26 19:45:07 +0100

    bmon: Bump version
    The release contains the configure scripts, as such we don't need the
    autoreconf anymore.
    
    Also add a hash file since there is a release tarball uploaded by the
    maintainer.
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/bmon/bmon.mk
+++ package/bmon/bmon.mk
@@ -4,10 +4,8 @@
 #
 ################################################################################
 
-BMON_VERSION = 3.2
-BMON_SITE = $(call github,tgraf,bmon,v$(BMON_VERSION))
-# configure not shipped
-BMON_AUTORECONF = YES
+BMON_VERSION = 3.5
+BMON_SITE = https://github.com/tgraf/bmon/releases/download/v$(BMON_VERSION)/
 BMON_DEPENDENCIES = host-pkgconf libconfuse libnl ncurses
 BMON_LICENSE = BSD-2c
 BMON_LICENSE_FILES = LICENSE
