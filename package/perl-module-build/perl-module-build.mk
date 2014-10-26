b4bff77d59a314dfe3d74e988ecb9aea7684d420
Francois Perrad <fperrad@gmail.com>
2014-10-26 09:12:49 +0100

    perl-module-build: bump to version 0.4210
    [Thomas: drop empty HOST_PERL_MODULE_BUILD_DEPENDENCIES.]
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/perl-module-build/perl-module-build.mk
+++ package/perl-module-build/perl-module-build.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-PERL_MODULE_BUILD_VERSION = 0.4206
+PERL_MODULE_BUILD_VERSION = 0.4210
 PERL_MODULE_BUILD_SOURCE = Module-Build-$(PERL_MODULE_BUILD_VERSION).tar.gz
 PERL_MODULE_BUILD_SITE = $(BR2_CPAN_MIRROR)/authors/id/L/LE/LEONT
 PERL_MODULE_BUILD_LICENSE = Artistic or GPLv1+
+PERL_MODULE_BUILD_LICENSE_FILES = LICENSE
 
 $(eval $(host-perl-package))
