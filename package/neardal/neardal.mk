7cd6782ad79edce02ff6ab6d8885c92ec128ad85
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-10-26 19:45:52 +0100

    neardal: bump version
    [Thomas: add dependency on either readline or libedit, which is needed
    for new versions of neardal.]
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/neardal/neardal.mk
+++ package/neardal/neardal.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NEARDAL_VERSION = da8e06d3884d214bb19b894d2b8fdc9c7ca8575f
+NEARDAL_VERSION = 33b54a55032b047fd885a5eb3592c169c0056c49
 NEARDAL_SITE = $(call github,connectivity,neardal,$(NEARDAL_VERSION))
 NEARDAL_INSTALL_STAGING = YES
 NEARDAL_LICENSE = GPLv2
@@ -12,6 +12,14 @@ NEARDAL_LICENSE_FILES = COPYING
 
 NEARDAL_DEPENDENCIES = host-pkgconf dbus dbus-glib
 NEARDAL_AUTORECONF = YES
+
+# Either readline or libedit are needed, and the Config.in file
+# guarantees at least one of them is enabled
+ifeq ($(BR2_PACKAGE_READLINE),y)
+NEARDAL_DEPENDENCIES += readline
+else ifeq ($(BR2_PACKAGE_LIBEDIT),y)
+NEARDAL_DEPENDENCIES += libedit
+endif
 
 define NEARDAL_INSTALL_NCL
 	$(INSTALL) -m 0755 -D $(@D)/ncl/ncl $(TARGET_DIR)/usr/bin/ncl
