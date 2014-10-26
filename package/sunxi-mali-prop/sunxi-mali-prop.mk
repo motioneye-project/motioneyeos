bc91aaa6fe3ee94cd6306580532c997371bc8426
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-10-26 19:45:38 +0100

    sunxi-mali-prop: bump version
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/sunxi-mali-prop/sunxi-mali-prop.mk
+++ package/sunxi-mali-prop/sunxi-mali-prop.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SUNXI_MALI_PROP_VERSION = e4ced471d576708ac9aa093e41a0f91cf429862b
+SUNXI_MALI_PROP_VERSION = 1c5063f43cdc9de341c0d63b2e3921cab86c7742
 SUNXI_MALI_PROP_SITE = $(call github,linux-sunxi,sunxi-mali-proprietary,$(SUNXI_MALI_PROP_VERSION))
 
 $(eval $(generic-package))
