40c341da78f3315fa31e4ea56b470753bf915a31
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-10-29 13:01:35 -0300

    cryptsetup: bump to version 1.6.6
    Drop upstream patch, add hash file, switch to kernel.org mirror.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/cryptsetup/cryptsetup.mk
+++ package/cryptsetup/cryptsetup.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-CRYPTSETUP_VERSION = 1.6.3
-CRYPTSETUP_SOURCE = cryptsetup-$(CRYPTSETUP_VERSION).tar.bz2
-CRYPTSETUP_SITE = http://cryptsetup.googlecode.com/files
+CRYPTSETUP_VERSION_MAJOR = 1.6
+CRYPTSETUP_VERSION = $(CRYPTSETUP_VERSION_MAJOR).6
+CRYPTSETUP_SOURCE = cryptsetup-$(CRYPTSETUP_VERSION).tar.xz
+CRYPTSETUP_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/cryptsetup/v$(CRYPTSETUP_VERSION_MAJOR)
 CRYPTSETUP_CONF_ENV += LIBGCRYPT_CONFIG=$(STAGING_DIR)/usr/bin/libgcrypt-config
 CRYPTSETUP_DEPENDENCIES = lvm2 popt e2fsprogs libgcrypt host-pkgconf \
 	$(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext)
