3174f736d766d5abf3c035581b3da38dc90fab0f
Jörg Krause <jkrause@posteo.de>
2014-10-30 10:07:05 +0100

    package/nodejs: security bump to version 0.10.33
    This release handles the recent POODLE vulnerability [CVE-2014-3566] by
    disabling SSLv2/SSLv3 by default.
    
    Signed-off-by: Jörg Krause <jkrause@posteo.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/nodejs/nodejs.mk
+++ package/nodejs/nodejs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NODEJS_VERSION = 0.10.32
+NODEJS_VERSION = 0.10.33
 NODEJS_SOURCE = node-v$(NODEJS_VERSION).tar.gz
 NODEJS_SITE = http://nodejs.org/dist/v$(NODEJS_VERSION)
 NODEJS_DEPENDENCIES = host-python host-nodejs zlib \
