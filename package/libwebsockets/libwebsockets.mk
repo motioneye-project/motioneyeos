a1b4effcbbcf1edebe39c34ca497bb34897471d8
Sagaert Johan <sagaert.johan@skynet.be>
2014-10-31 14:49:02 +0100

    libwebsockets : bump version to v1.3-chrome37-firefox30
    Bumped the version and did some trivial reworking of the patch file :
    libwebsockets-0002-cmake-disable-shared-library-build-when-BUILD_SHARED.patch
    
    [Thomas:
     - Pass LWS_IPV6={ON,OFF} depending on the value of BR2_INET_IPV6,
       otherwise the build failed in a non-IPv6 configuration.
     - The name of the CMake option for OpenSSL support has changed, it's
       now LWS_WITH_SSL={ON,OFF}.
     - Adjust the description of patch 0002.]
    
    Signed-off-by: Sagaert Johan <sagaert.johan@skynet.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libwebsockets/libwebsockets.mk
+++ package/libwebsockets/libwebsockets.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBWEBSOCKETS_VERSION = v1.23-chrome32-firefox24
+LIBWEBSOCKETS_VERSION = v1.3-chrome37-firefox30
 LIBWEBSOCKETS_SOURCE = libwebsockets-$(LIBWEBSOCKETS_VERSION).tar.xz
 LIBWEBSOCKETS_SITE = http://git.libwebsockets.org/cgi-bin/cgit/libwebsockets/snapshot
 LIBWEBSOCKETS_LICENSE = LGPLv2.1 with exceptions
@@ -14,10 +14,17 @@ LIBWEBSOCKETS_INSTALL_STAGING = YES
 
 LIBWEBSOCKETS_CONF_OPTS += -DWITHOUT_TESTAPPS=ON
 
+ifeq ($(BR2_INET_IPV6),y)
+LIBWEBSOCKETS_CONF_OPTS += -DLWS_IPV6=ON
+else
+LIBWEBSOCKETS_CONF_OPTS += -DLWS_IPV6=OFF
+endif
+
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 LIBWEBSOCKETS_DEPENDENCIES += openssl
+LIBWEBSOCKETS_CONF_OPTS += -DLWS_WITH_SSL=ON
 else
-LIBWEBSOCKETS_CONF_OPTS += -DWITH_SSL=OFF
+LIBWEBSOCKETS_CONF_OPTS += -DLWS_WITH_SSL=OFF
 endif
 
 $(eval $(cmake-package))
