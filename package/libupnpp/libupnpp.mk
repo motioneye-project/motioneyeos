d9dfa28d0335bf74480fcf942c7bd2efbc9f0be3
Jörg Krause <jkrause@posteo.de>
2014-11-03 11:01:16 +0100

    package/libupnpp: bump to version 0.8.6
    Signed-off-by: Jörg Krause <jkrause@posteo.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libupnpp/libupnpp.mk
+++ package/libupnpp/libupnpp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUPNPP_VERSION = 0.8.5
+LIBUPNPP_VERSION = 0.8.6
 LIBUPNPP_SITE = http://www.lesbonscomptes.com/upmpdcli/downloads
 LIBUPNPP_LICENSE = GPLv2+
 LIBUPNPP_LICENSE_FILES = COPYING
