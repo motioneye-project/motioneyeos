104f2ed2d22306fe1273c5a6d24e432690bf0681
Yegor Yefremov <yegorslists@googlemail.com>
2014-11-03 20:37:18 +0100

    python-netifaces: bump to 0.10.4
    Change download location and remove obsolete patch.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-netifaces/python-netifaces.mk
+++ package/python-netifaces/python-netifaces.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_NETIFACES_VERSION = 0.7
+PYTHON_NETIFACES_VERSION = 0.10.4
 PYTHON_NETIFACES_SOURCE = netifaces-$(PYTHON_NETIFACES_VERSION).tar.gz
-PYTHON_NETIFACES_SITE = http://alastairs-place.net/projects/netifaces
+PYTHON_NETIFACES_SITE = https://pypi.python.org/packages/source/n/netifaces/
 PYTHON_NETIFACES_LICENSE = MIT
 PYTHON_NETIFACES_LICENSE_FILES = README
 PYTHON_NETIFACES_SETUP_TYPE = setuptools
