1127b3a2ca9a88236f25dae4d83ecb3990735c94
Jörg Krause <jkrause@posteo.de>
2014-11-04 14:04:29 +0100

    package/shairport-sync: fix avahi dependency
    shairport-sync uses mDNS to pubish its service. This task is implemented
    (among others) in avahi and tinysvcmdns.
    
    To use avahi as the mDNS backend, shairport-sync requires libavahi-client or
    libdns_sd. Both will work, but libavahi-client is sufficient.
    
    To get libavahi-client support from avahi BR2_PACKAGE_AVAHI_DAEMON and
    BR2_PACKAGE_DBUS needs to be selected. Unfortunatly this is not immediately
    obvious if you've not checked avahis configure file. A
    BR2_PACKAGE_LIBAVAHI_CLIENT config symbol may help here for clarification,
    but is not present yet.
    
    Fixes autobuild errors:
      - http://autobuild.buildroot.net/results/a6a/a6ab7641a3cafcdaeffe11f08d5fcacbbb882d8d/
      - http://autobuild.buildroot.net/results/cb4/cb41980f2fb416d627bea0950e1dae727eae0ca6/
      - http://autobuild.buildroot.net/results/009/00929df01474813be576936e80e86374efdbc327/
      - http://autobuild.buildroot.net/results/a40/a40b8cf9aaa4ade4d96524219276cfad2920104c/
      - http://autobuild.buildroot.net/results/03f/03fff2805b3348f2557bbef73a716eadea3add03/
    
    and some more.
    
    Signed-off-by: Jörg Krause <jkrause@posteo.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/shairport-sync/shairport-sync.mk
+++ package/shairport-sync/shairport-sync.mk
@@ -13,8 +13,12 @@ SHAIRPORT_SYNC_DEPENDENCIES = alsa-lib libdaemon popt
 SHAIRPORT_SYNC_AUTORECONF = YES
 SHAIRPORT_SYNC_CONF_OPTS = --with-alsa # required
 
-# Avahi or tinysvcmdns (shaiport-sync bundles its own version of tinysvcmdns)
-ifeq ($(BR2_PACKAGE_AVAHI),y)
+# Avahi or tinysvcmdns (shaiport-sync bundles its own version of tinysvcmdns).
+# Avahi support needs libavahi-client, which is built by avahi if avahi-daemon
+# and dbus is selected. Since there is no BR2_PACKAGE_LIBAVAHI_CLIENT config
+# option yet, use the avahi-daemon and dbus congig symbols to check for
+# libavahi-client.
+ifeq ($(BR2_PACKAGE_AVAHI_DAEMON)$(BR2_PACKAGE_DBUS),yy)
 SHAIRPORT_SYNC_DEPENDENCIES += avahi
 SHAIRPORT_SYNC_CONF_OPTS += --with-avahi
 else
