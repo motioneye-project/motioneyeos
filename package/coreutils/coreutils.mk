ef5c3f4e89c91597f1748b8877c1e3dd5c77c28b
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-11-07 09:45:46 -0300

    coreutils: fix build failure for musl
    musl doesn't implement re_match and friends and coreutils fails to
    detect this properly, so force internal regex routines for musl.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/coreutils/coreutils.mk
+++ package/coreutils/coreutils.mk
@@ -14,7 +14,8 @@ COREUTILS_LICENSE_FILES = COPYING
 COREUTILS_AUTORECONF = YES
 COREUTILS_GETTEXTIZE = YES
 
-COREUTILS_CONF_OPTS = --disable-rpath --enable-single-binary=shebangs
+COREUTILS_CONF_OPTS = --disable-rpath --enable-single-binary=shebangs \
+	$(if $(BR2_TOOLCHAIN_USES_MUSL),--with-included-regex)
 COREUTILS_CONF_ENV = ac_cv_c_restrict=no \
 	ac_cv_func_chown_works=yes \
 	ac_cv_func_euidaccess=no \
