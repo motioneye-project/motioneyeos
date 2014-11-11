7d925ffcbfd5d78707c5fc87bc4dc52df0b57e58
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-11-11 11:17:10 -0300

    usbutils: bump to version 008
    Now uses hwdb from libudev for usb information.
    Drop all redundant/obsolete dependencies and cleanup hooks.
    Don't install to staging, it's pointless, it provides no libraries at
    all and it doesn't bundle usb.ids any more.
    
    [Thomas: change 'comment' in Config.in to match what's recommended in
    the Buildroot manual.]
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/usbutils/usbutils.mk
+++ package/usbutils/usbutils.mk
@@ -4,19 +4,12 @@
 #
 ################################################################################
 
-USBUTILS_VERSION = 007
+USBUTILS_VERSION = 008
 USBUTILS_SOURCE = usbutils-$(USBUTILS_VERSION).tar.xz
 USBUTILS_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/usb/usbutils
-USBUTILS_DEPENDENCIES = host-pkgconf libusb
-USBUTILS_INSTALL_STAGING = YES
+USBUTILS_DEPENDENCIES = host-pkgconf libusb udev
 USBUTILS_LICENSE = GPLv2+
 USBUTILS_LICENSE_FILES = COPYING
-
-ifeq ($(BR2_PACKAGE_USBUTILS_ZLIB),y)
-	USBUTILS_DEPENDENCIES += zlib
-else
-	USBUTILS_CONF_OPTS = --disable-zlib
-endif
 
 # Build after busybox since it's got a lightweight lsusb
 ifeq ($(BR2_PACKAGE_BUSYBOX),y)
@@ -34,30 +27,8 @@ endif
 
 define USBUTILS_TARGET_CLEANUP
 	rm -f $(TARGET_DIR)/usr/bin/usb-devices
-	rm -f $(TARGET_DIR)/usr/sbin/update-usbids.sh
-	rm -f $(TARGET_DIR)/usr/share/pkgconfig/usbutils.pc
 endef
 
 USBUTILS_POST_INSTALL_TARGET_HOOKS += USBUTILS_TARGET_CLEANUP
-
-define USBUTILS_REMOVE_UNCOMPRESSED_IDS
-	rm -f $(TARGET_DIR)/usr/share/usb.ids
-endef
-
-define USBUTILS_REMOVE_COMPRESSED_IDS
-	rm -f $(TARGET_DIR)/usr/share/usb.ids.gz
-endef
-
-ifeq ($(BR2_PACKAGE_USBUTILS_ZLIB),y)
-USBUTILS_POST_INSTALL_TARGET_HOOKS += USBUTILS_REMOVE_UNCOMPRESSED_IDS
-else
-USBUTILS_POST_INSTALL_TARGET_HOOKS += USBUTILS_REMOVE_COMPRESSED_IDS
-endif
-
-define USBUTILS_REMOVE_DEVFILES
-	rm -f $(TARGET_DIR)/usr/bin/libusb-config
-endef
-
-USBUTILS_POST_INSTALL_TARGET_HOOKS += USBUTILS_REMOVE_DEVFILES
 
 $(eval $(autotools-package))
