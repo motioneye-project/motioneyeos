143fe5b443da448c2652527cce299dab0c4fab96
Alexey Brodkin <Alexey.Brodkin@synopsys.com>
2014-11-11 16:56:48 +0300

    rt-tests: allow building subset of tests with non-NPTL toolchains
    Some architectures are still stuck with non-NPTL toolchains.
    These are for example ARC, Blackfin, Xtensa etc.
    
    Still rt-tests are very good benchmarks and it would be good to enable use of
    at least selected (those that will be built) tests on those architectures.
    
    This change makes it possible to only build subset of tests that don't require
    NPTL calls.
    
    Following tests will be built with non-NPTL toolchain:
     * signaltest
     * ptsematest
     * sigwaittest
     * svsematest
     * sendme
     * hackbench
    
    Still it's required to have a toolchain with threads support because most of
    mentioned tests use threads.
    
    03-fix-non-nptl-buil.patch was submitted upstream:
    https://www.mail-archive.com/linux-kernel@vger.kernel.org/msg762958.html
    so as soon as it is accepted with the next version bump this patch should be
    removed.
    
    [Thomas: fix the rt-tests.mk test on NPTL to use positive logic.]
    
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Peter Korsgaard <peter@korsgaard.com>
    Cc: Arnout Vandecappelle <arnout@mind.be>
    Cc: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/rt-tests/rt-tests.mk
+++ package/rt-tests/rt-tests.mk
@@ -14,15 +14,21 @@ ifeq ($(BR2_PACKAGE_PYTHON),y)
 RT_TESTS_DEPENDENCIES = python
 endif
 
+ifeq ($(BR2_TOOLCHAIN_HAS_THREADS_NPTL),)
+RT_TESTS_HAVE_NPTL=no
+endif
+
 define RT_TESTS_BUILD_CMDS
 	$(MAKE) -C $(@D) 			\
 		CC="$(TARGET_CC)" 		\
+		HAVE_NPTL=$(RT_TESTS_HAVE_NPTL)	\
 		CFLAGS="$(TARGET_CFLAGS)"	\
 		prefix=/usr
 endef
 
 define RT_TESTS_INSTALL_TARGET_CMDS
 	$(MAKE) -C $(@D) 				\
+		HAVE_NPTL=$(RT_TESTS_HAVE_NPTL)		\
 		DESTDIR="$(TARGET_DIR)" 		\
 		prefix=/usr 				\
 		$(if $(BR2_PACKAGE_PYTHON),HASPYTHON=1 PYLIB=/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages/) \
