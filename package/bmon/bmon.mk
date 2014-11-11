70374616dc58fca1233e3b381e13ad3dabaf61a4
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-11-11 20:42:45 -0300

    bmon: uclinux is also linux
    Fixes:
    http://autobuild.buildroot.net/results/4d0/4d0c3c8b68b37f532378f19041379cb5c5798950/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/bmon/bmon.mk
+++ package/bmon/bmon.mk
@@ -9,6 +9,8 @@ BMON_SITE = https://github.com/tgraf/bmon/releases/download/v$(BMON_VERSION)/
 BMON_DEPENDENCIES = host-pkgconf libconfuse libnl ncurses
 BMON_LICENSE = BSD-2c
 BMON_LICENSE_FILES = LICENSE
+# For 0001-build-uclinux-is-also-linux.patch
+BMON_AUTORECONF = YES
 
 # link dynamically unless explicitly requested otherwise
 ifeq ($(BR2_PREFER_STATIC_LIB),)
