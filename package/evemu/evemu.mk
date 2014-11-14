4b8bab325381277af07f66792fb9ccbfd8e96de4
Peter Seiderer <ps.report@gmx.net>
2014-11-14 19:31:14 +0100

    evemu: bump version to 2.1.0
    See [1] for details.
    
    Change download url and source package format to enable usage of
    upstream provided hash values.
    
    Patches
    - evemu-0001-SYN_MAX.patch
    - evemu-0003-make-event-names-use-input.h-from-sysroot-instead-of.patch
    are obsolete as there is no more host side python script for event name generation.
    
    Patches
    - evemu-0002-configure.ac-add-disable-tests-option.patch
    - evemu-0005-configure.ac-add-disable-python-bindings-option.patch
    are committed upstream.
    
    [1] http://lists.freedesktop.org/archives/input-tools/2014-November/001122.html
    
    [Thomas: drop EVEMU_AUTORECONF = YES, since we're now using a tarball
    that has the configure script already generated, and we no longer have
    patches touching the configure.ac/Makefile.am]
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/evemu/evemu.mk
+++ package/evemu/evemu.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-EVEMU_VERSION = 2.0.0
-EVEMU_SITE = http://cgit.freedesktop.org/evemu/snapshot
+EVEMU_VERSION = 2.1.0
+EVEMU_SITE = http://www.freedesktop.org/software/evemu
+EVEMU_SOURCE = evemu-$(EVEMU_VERSION).tar.xz
 EVEMU_LICENSE = LGPLv3 (library), GPLv3 (tools)
 EVEMU_LICENSE_FILES = COPYING COPYING.GPL3
 
@@ -14,18 +15,11 @@ EVEMU_LICENSE_FILES = COPYING COPYING.GPL3
 # which doesn't have all the needed modules enabled, breaking the build
 EVEMU_CONF_ENV = ac_cv_path_ASCIIDOC=""
 
-# needed for make-event-names.py to find sysroot provided input.h (instead
-# of host system version)
-EVEMU_MAKE_ENV = SYSROOT=$(STAGING_DIR)
-
 # disable tests generation because of C++ dependency
 EVEMU_CONF_OPTS += --disable-tests
 
 # Uses PKG_CHECK_MODULES() in configure.ac
 EVEMU_DEPENDENCIES = host-pkgconf libevdev
-
-# Needs Python for header file generation
-EVEMU_DEPENDENCIES += $(if $(BR2_PACKAGE_PYTHON3),host-python3,host-python)
 
 # Check for target python
 ifeq ($(BR2_PACKAGE_PYTHON)$(BR2_PACKAGE_PYTHON3),y)
@@ -34,9 +28,5 @@ EVEMU_DEPENDENCIES += $(if $(BR2_PACKAGE_PYTHON3),python3,python)
 else
 EVEMU_CONF_OPTS += --disable-python-bindings
 endif
-
-# package source code coming from git, so it doesn't have generated
-# configure and Makefile.in
-EVEMU_AUTORECONF = YES
 
 $(eval $(autotools-package))
