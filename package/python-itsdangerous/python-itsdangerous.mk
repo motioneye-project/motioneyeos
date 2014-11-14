5c3d11909048a42c702594e1354ee1d4603419b4
Peter Korsgaard <peter@korsgaard.com>
2014-11-14 18:25:04 +0100

    package: add python-itsdangerous
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/python-itsdangerous/python-itsdangerous.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# python-itsdangerous
+#
+################################################################################
+
+PYTHON_ITSDANGEROUS_VERSION = 0.24
+PYTHON_ITSDANGEROUS_SITE = $(call github,mitsuhiko,itsdangerous,$(PYTHON_ITSDANGEROUS_VERSION))
+PYTHON_ITSDANGEROUS_SETUP_TYPE = setuptools
+PYTHON_ITSDANGEROUS_LICENSE = BSD-3c
+PYTHON_ITSDANGEROUS_LICENSE_FILES = LICENSE
+
+$(eval $(python-package))
