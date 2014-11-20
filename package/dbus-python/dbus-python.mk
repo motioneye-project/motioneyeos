a8998ae9c545f2fc262f9925f85835a92a86ee5c
Peter Korsgaard <peter@korsgaard.com>
2014-11-20 14:53:29 +0100

    dbus-python: cleanup package
    The patch is not needed anymore as the proper includes can be specified
    using PYTHON_INCLUDES, and version/pythondir/pyexecdir is correctly detected
    using host-python.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/dbus-python/dbus-python.mk
+++ package/dbus-python/dbus-python.mk
@@ -10,13 +10,10 @@ DBUS_PYTHON_INSTALL_STAGING = YES
 DBUS_PYTHON_LICENSE = MIT
 DBUS_PYTHON_LICENSE_FILES = COPYING
 
-DBUS_PYTHON_CONF_ENV = am_cv_pathless_PYTHON=python \
-		ac_cv_path_PYTHON=$(HOST_DIR)/usr/bin/python \
-		am_cv_python_version=$(PYTHON_VERSION) \
-		am_cv_python_platform=linux2 \
-		am_cv_python_pythondir=/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages \
-		am_cv_python_pyexecdir=/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages \
-		am_cv_python_includes=-I$(STAGING_DIR)/usr/include/python$(PYTHON_VERSION_MAJOR)
+DBUS_PYTHON_CONF_ENV = \
+	PYTHON=$(HOST_DIR)/usr/bin/python2 \
+	PYTHON_INCLUDES="$(shell $(STAGING_DIR)/usr/bin/python2-config --includes)" \
+	PYTHON_LIBS="$(shell $(STAGING_DIR)/usr/bin/python2-config --ldflags)"
 
 DBUS_PYTHON_CONF_OPTS = --disable-html-docs --disable-api-docs
 
