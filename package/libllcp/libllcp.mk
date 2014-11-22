e6dcfa5693eaccc99c2a7becba7eb5cbef846719
Peter Korsgaard <peter@korsgaard.com>
2014-11-22 14:37:42 +0100

    libllcp: needs host-pkgconf
    configure.ac uses PKG_CHECK_MODULES, so we need host-pkgconf.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libllcp/libllcp.mk
+++ package/libllcp/libllcp.mk
@@ -7,7 +7,7 @@
 LIBLLCP_VERSION = cf0c4b3c9df98851c6092c130192130c3f5a46bd
 LIBLLCP_SITE = https://libllcp.googlecode.com/git
 LIBLLCP_SITE_METHOD = git
-LIBLLCP_DEPENDENCIES = libnfc
+LIBLLCP_DEPENDENCIES = host-pkgconf libnfc
 # There's no ./configure in the repository, so we need to autoreconf
 LIBLLCP_AUTORECONF = YES
 LIBLLCP_INSTALL_STAGING = YES
