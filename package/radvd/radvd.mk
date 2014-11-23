870b94c944d5f1ccf7ffd96ac513801eb37339f7
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-11-23 18:36:02 +0100

    radvd: switch to new patch naming convention, use Git formatted patches
    This commit renames the radvd patches to follow the new naming
    convention, and reformats them to be Git patches.
    
    Since the patches are renamed, we adjust the comment in the .mk file
    explaining why we autoreconf the package.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>

--- package/radvd/radvd.mk
+++ package/radvd/radvd.mk
@@ -9,7 +9,7 @@ RADVD_SOURCE = radvd-$(RADVD_VERSION).tar.xz
 RADVD_SITE = http://www.litech.org/radvd/dist
 RADVD_DEPENDENCIES = host-bison flex host-flex host-pkgconf
 RADVD_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
-# For radvd-01-drop-check.patch & radvd-02-drop-stack-protector.patch
+# We're patching configure.ac/Makefile.am.
 RADVD_AUTORECONF = YES
 
 define RADVD_INSTALL_INITSCRIPT
