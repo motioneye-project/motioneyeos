067570a39f65fd44782cce887da567b521651ec3
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-11-26 11:19:01 -0300

    valgrind: bump to version 3.10.1
    Add hash file and switch to a non-dead homepage.
    Enable for supported architectures: powerpc64 & powerpc64le.
    Even though aarch64 is supported it's still a bit rought resulting in
    build failures so don't enable it for now.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/valgrind/valgrind.mk
+++ package/valgrind/valgrind.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VALGRIND_VERSION = 3.10.0
+VALGRIND_VERSION = 3.10.1
 VALGRIND_SITE = http://valgrind.org/downloads
 VALGRIND_SOURCE = valgrind-$(VALGRIND_VERSION).tar.bz2
 VALGRIND_LICENSE = GPLv2 GFDLv1.2
