a0cefa916b2131054fe3f5d0916dbbe2bcc54f4a
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-11-30 15:18:30 +0100

    googlefontdirectory: add error handling to for loop
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/googlefontdirectory/googlefontdirectory.mk
+++ package/googlefontdirectory/googlefontdirectory.mk
@@ -16,7 +16,7 @@ define GOOGLEFONTDIRECTORY_INSTALL_TARGET_CMDS
 	for i in $(GOOGLEFONTDIRECTORY_FONTS); \
 	do \
 		$(INSTALL) -d $(TARGET_DIR)/usr/share/fonts/$$i && \
-		$(INSTALL) -m 0644 -t $(TARGET_DIR)/usr/share/fonts/$$i $(@D)/$$i/*.ttf; \
+		$(INSTALL) -m 0644 -t $(TARGET_DIR)/usr/share/fonts/$$i $(@D)/$$i/*.ttf || exit 1; \
 	done
 endef
 
