51d973a622c9befc8caa6445e7e98aca2e1ded94
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-11-30 15:18:18 +0100

    ndisc6: remove man page related code
    The target-finalize target takes care of removing
    $(TARGET_DIR)/usr/share/man, so there's no point in having package
    specific code doing this.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ndisc6/ndisc6.mk
+++ package/ndisc6/ndisc6.mk
@@ -27,14 +27,9 @@ NDISC6_SBIN_$(BR2_PACKAGE_NDISC6_RDNSSD) += rdnssd
 NDISC6_SBIN_$(BR2_PACKAGE_NDISC6_RLTRACEROUTE6) += rltraceroute6 tracert6
 NDISC6_SBIN_$(BR2_PACKAGE_NDISC6_TCPTRACEROUTE6) += tcptraceroute6
 
-NDISC6_MAN1_ = $(addsuffix .1,$(NDISC6_BIN_))
-NDISC6_MAN8_ = $(addsuffix .8,$(NDISC6_SBIN_))
-
 define NDISC6_REMOVE_UNNEEDED
 	rm -rf $(addprefix $(TARGET_DIR)/usr/bin/,$(NDISC6_BIN_)) \
-		$(addprefix $(TARGET_DIR)/usr/sbin/,$(NDISC6_SBIN_)) \
-		$(addprefix $(TARGET_DIR)/usr/share/man/man1/,$(NDISC6_MAN1_)) \
-		$(addprefix $(TARGET_DIR)/usr/share/man/man8/,$(NDISC6_MAN8_))
+		$(addprefix $(TARGET_DIR)/usr/sbin/,$(NDISC6_SBIN_))
 	$(if $(BR2_PACKAGE_NDISC6_RDNSSD),,\
 		rm -rf $(TARGET_DIR)/etc/rdnssd $(TARGET_DIR)/var/run/rdnssd)
 endef
