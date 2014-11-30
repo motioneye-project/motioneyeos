e28cb103ab6944d51547266ddb58997897988cc8
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-11-30 15:18:04 +0100

    nfs-utils: use <pkg>_INSTALL_INIT_SYSV mechanism
    Also move back the NFS_UTILS_INSTALL_FIXUP hook registration right
    after the hook definition.
    
    [Peter: use install -D as noted by Yann]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/nfs-utils/nfs-utils.mk
+++ package/nfs-utils/nfs-utils.mk
@@ -35,16 +35,19 @@ NFS_UTILS_CONF_OPTS += --disable-tirpc
 endif
 
 define NFS_UTILS_INSTALL_FIXUP
-	$(INSTALL) -m 0755 package/nfs-utils/S60nfs \
-		$(TARGET_DIR)/etc/init.d/S60nfs
 	rm -f $(NFS_UTILS_TARGETS_)
+endef
+
+NFS_UTILS_POST_INSTALL_TARGET_HOOKS += NFS_UTILS_INSTALL_FIXUP
+
+define NFS_UTILS_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 package/nfs-utils/S60nfs \
+		$(TARGET_DIR)/etc/init.d/S60nfs
 endef
 
 define NFS_UTILS_REMOVE_NFSIOSTAT
 	rm -f $(TARGET_DIR)/usr/sbin/nfsiostat
 endef
-
-NFS_UTILS_POST_INSTALL_TARGET_HOOKS += NFS_UTILS_INSTALL_FIXUP
 
 # nfsiostat is interpreted python, so remove it unless it's in the target
 NFS_UTILS_POST_INSTALL_TARGET_HOOKS += $(if $(BR2_PACKAGE_PYTHON),,NFS_UTILS_REMOVE_NFSIOSTAT)
