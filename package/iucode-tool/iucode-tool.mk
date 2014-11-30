febe876dcc18938b43fe1012b0acc689176188ac
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-11-30 15:18:43 +0100

    iucode-tool: install init script unconditionally
    As discussed, users should use a rootfs overlay or a post-build script
    instead of a custom skeleton to override files installed by Buildroot,
    so there is no point in having conditions when installing init scripts
    or configuration files.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/iucode-tool/iucode-tool.mk
+++ package/iucode-tool/iucode-tool.mk
@@ -15,9 +15,8 @@ IUCODE_TOOL_LICENSE = GPLv2+
 IUCODE_TOOL_LICENSE_FILES = COPYING
 
 define IUCODE_TOOL_INSTALL_INIT_SYSV
-	[ -f $(TARGET_DIR)/etc/init.d/S00iucode-tool ] || \
-		$(INSTALL) -D -m 0755 package/iucode-tool/S00iucode-tool \
-			$(TARGET_DIR)/etc/init.d/S00iucode-tool
+	$(INSTALL) -D -m 0755 package/iucode-tool/S00iucode-tool \
+		$(TARGET_DIR)/etc/init.d/S00iucode-tool
 endef
 
 $(eval $(autotools-package))
