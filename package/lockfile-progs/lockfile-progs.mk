ef1c109e6fbc998d347e87247cff010c27a45de1
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-11-30 15:18:35 +0100

    lockfile-progs: add error handling to for loop
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lockfile-progs/lockfile-progs.mk
+++ package/lockfile-progs/lockfile-progs.mk
@@ -21,7 +21,7 @@ endef
 
 define LOCKFILE_PROGS_INSTALL_TARGET_CMDS
 	for i in $(LOCKFILE_BINS); do \
-		$(INSTALL) -D -m 755 $(@D)/bin/$$i $(TARGET_DIR)/usr/bin/$$i; \
+		$(INSTALL) -D -m 755 $(@D)/bin/$$i $(TARGET_DIR)/usr/bin/$$i || exit 1; \
 	done
 endef
 
