bc92e491f1bfc2a20e5a533e61b1d2de79855544
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-12-01 10:24:35 -0300

    elfutils: download portability patch
    Download the portability patch into the buildroot tree.
    
    This isn't nice but there really isn't another good solution for it.
    The elfutils-portability.patch is version-dependent hence we already
    have an older version for an older version of elfutils in the mirrors.
    This causes hash check failures when fetching from sources.buildroot.net
    But we can't remove (hence replace) the old version with the new one
    since this would causee issues with older versions of
    buildroot/elfutils for the same scenario.
    
    Fixes:
    http://autobuild.buildroot.net/results/26a/26afb92dc14fd0f85c0800dd4e034ea86af19fe2/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/elfutils/elfutils.mk
+++ package/elfutils/elfutils.mk
@@ -7,15 +7,13 @@
 ELFUTILS_VERSION = 0.160
 ELFUTILS_SOURCE = elfutils-$(ELFUTILS_VERSION).tar.bz2
 ELFUTILS_SITE = https://fedorahosted.org/releases/e/l/elfutils/$(ELFUTILS_VERSION)
+ELFUTILS_INSTALL_STAGING = YES
 ELFUTILS_LICENSE = GPLv3 GPLv2 LGPLv3
 ELFUTILS_LICENSE_FILES = COPYING COPYING-GPLV2 COPYING-LGPLV3
 
 # The tarball does not have a generated configure script
 ELFUTILS_AUTORECONF = YES
 ELFUTILS_CONF_OPTS += --disable-werror
-ELFUTILS_PATCH = elfutils-portability.patch
-
-ELFUTILS_INSTALL_STAGING = YES
 
 ifeq ($(BR2_LARGEFILE),y)
 # elfutils gets confused when lfs mode is forced, so don't
