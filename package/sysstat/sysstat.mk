d855e9e52e062c600f748e6b356fd17ed79d7d14
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-12-02 07:10:58 -0300

    sysstat: bump to version 11.0.2
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sysstat/sysstat.mk
+++ package/sysstat/sysstat.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SYSSTAT_VERSION = 11.0.1
+SYSSTAT_VERSION = 11.0.2
 SYSSTAT_SOURCE = sysstat-$(SYSSTAT_VERSION).tar.xz
 SYSSTAT_SITE = http://pagesperso-orange.fr/sebastien.godard
 SYSSTAT_CONF_OPTS = --disable-man-group --disable-sensors
