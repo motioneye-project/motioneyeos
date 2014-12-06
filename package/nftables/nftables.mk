9d1aa46c9e42670513d02c428f452fef7baa481a
Alexander Clouter <alex+buildroot@digriz.org.uk>
2014-12-06 23:21:53 +0000

    nftables: making interactive console optional
    This lets you (by default enabled) compile out its readline
    dependency.
    
    [Thomas:
      - remove the patch, which is now unneeded, since we've bumped to
        nftables 0.4, which as the patch to make readline optional.
      - remove the new Config.in option, just enable the interactive
        console when the readline package is enabled.]
    
    Signed-off-by: Alexander Clouter <alex+buildroot@digriz.org.uk>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/nftables/nftables.mk
+++ package/nftables/nftables.mk
@@ -7,12 +7,18 @@
 NFTABLES_VERSION = 0.4
 NFTABLES_SOURCE = nftables-$(NFTABLES_VERSION).tar.bz2
 NFTABLES_SITE = http://www.netfilter.org/projects/nftables/files
-NFTABLES_DEPENDENCIES = gmp libmnl libnftnl readline host-bison host-flex \
+NFTABLES_DEPENDENCIES = gmp libmnl libnftnl host-bison host-flex \
 	host-pkgconf $(if $(BR2_NEEDS_GETTEXT),gettext)
 NFTABLES_LICENSE = GPLv2
 NFTABLES_LICENSE_FILES = COPYING
 
-NFTABLES_LIBS = -lncurses
+ifeq ($(BR2_PACKAGE_READLINE),y)
+NFTABLES_DEPENDENCIES += readline
+NFTABLES_LIBS += -lncurses
+else
+NFTABLES_CONF_OPTS = --without-cli
+endif
+
 ifeq ($(BR2_STATIC_LIBS)$(BR2_PACKAGE_LIBNFTNL_JSON),yy)
 NFTABLES_LIBS += -ljansson -lm
 endif
