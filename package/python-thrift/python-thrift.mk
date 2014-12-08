00d53748a896520f20baec6ce77294dddde77439
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-12-08 23:03:28 +0100

    python-thrift: bump to version 0.9.2
    Following the bump of the thrift package, it makes sense to bump the
    python-thrift package as well. Python 3 is still not supported
    apparently, so we keep this package a Python 2 only one.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-thrift/python-thrift.mk
+++ package/python-thrift/python-thrift.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_THRIFT_VERSION = 0.9.1
+PYTHON_THRIFT_VERSION = 0.9.2
 PYTHON_THRIFT_SOURCE = thrift-$(PYTHON_THRIFT_VERSION).tar.gz
 PYTHON_THRIFT_SITE = http://www.us.apache.org/dist/thrift/$(PYTHON_THRIFT_VERSION)
 PYTHON_THRIFT_LICENSE = Apache-2.0
