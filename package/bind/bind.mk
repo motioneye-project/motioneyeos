9289dc562d89aeca722d4b4c3b0083183c2eeaa7
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-12-09 08:34:51 -0300

    bind: security bump to version 9.9.6-P1
    Fixes CVE-2014-8500 - A flaw in delegation handling could be exploited
    to put named into an infinite loop, in which each lookup of a name
    server triggered additional lookups of more name servers.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/bind/bind.mk
+++ package/bind/bind.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BIND_VERSION = 9.9.6
+BIND_VERSION = 9.9.6-P1
 BIND_SITE = ftp://ftp.isc.org/isc/bind9/$(BIND_VERSION)
 BIND_INSTALL_STAGING = YES
 BIND_CONFIG_SCRIPTS = bind9-config isc-config.sh
