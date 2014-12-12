41d987dcc186bc3ff46763329247219ca97ddedf
Frank Hunleth <fhunleth@troodon-software.com>
2014-12-12 14:11:23 -0500

    erlang: bump to version 17.4
    Erlang/OTP 17.4 is a service release on the 17 track with mostly bug
    fixes, but it does contain a number of new features and characteristics
    improvements as well.
    
    Some highlights of the release are:
    
      - eldap: Nearly all TCP options are possible to give in the
        eldap:open/2 call.
      - ssh: Added API functions ptty_alloc/3 and ptty_alloc/4, to allocate a
        pseudo tty.
      - ssl: Handle servers that may send an empty SNI extension to the client.
    
    Signed-off-by: Frank Hunleth <fhunleth@troodon-software.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/erlang/erlang.mk
+++ package/erlang/erlang.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ERLANG_VERSION = 17.3
+ERLANG_VERSION = 17.4
 ERLANG_SITE = http://www.erlang.org/download
 ERLANG_SOURCE = otp_src_$(ERLANG_VERSION).tar.gz
 ERLANG_DEPENDENCIES = host-erlang
