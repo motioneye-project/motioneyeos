89502b9d56ad4f06954fae55684ef21525ad2515
Gregory Dymarek <gregd72002@gmail.com>
2014-12-12 15:42:43 +0000

    libnice: new package
    [Thomas:
     - Rename patch to the proper naming convention
     - Reduce patch to only take care of the IPv6 case
     - Add description in the patch
     - Propagate libglib2 dependencies in Config.in + added the
       corresponding comment.
     - Added a real help text, and fixed the help text indentation.
     - Fixed the comment header in the .mk file to have the right number
       of # signs.
     - Added the license informations
     - Added a dependency on host-pkgconf, used by the configure script
     - Added --without-gstreamer and --without-gstreamer-0.10 to be
       explicit about the fact that we don't support GStreamer in libnice
       for now.
     - Fixed the hash file to have the proper format.]
    
    Signed-off-by: Gregory Dymarek <gregd72002@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/libnice/libnice.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# libnice
+#
+################################################################################
+
+LIBNICE_VERSION = 0.1.8
+LIBNICE_SITE = http://nice.freedesktop.org/releases/
+LIBNICE_LICENSE = MPLv1.1 or LGPLv2.1
+LIBNICE_LICENSE_FILES = COPYING COPYING.MPL COPYING.LGPL
+LIBNICE_DEPENDENCIES = libglib2 host-pkgconf
+LIBNICE_INSTALL_STAGING = YES
+LIBNICE_CONF_OPTS = \
+	--without-gstreamer \
+	--without-gstreamer-0.10 \
+
+$(eval $(autotools-package))
