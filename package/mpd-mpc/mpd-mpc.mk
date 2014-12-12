4f2367f416baa3e146f0ac8355bd7dacdcdec74d
Thierry Bultel <tbultel@free.fr>
2014-12-12 22:24:39 +0100

    mpd-mpc: new package
    [Thomas:
     - Changed hash file to use SHA256
     - Remove libiconv handling, since the iconv detection in mpc is
       broken: if it's not available in the C library, it expects to find
       iconv() in libintl (from gettext). But it's actually libiconv that
       provides iconv() for non-locale capable uClibc toolchains. But
       since anyway the package builds fine without iconv() support and
       properly detects when it's available, don't bother with this.]
    
    Signed-off-by: Thierry Bultel <tbultel@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/mpd-mpc/mpd-mpc.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# mpd-mpc
+#
+################################################################################
+
+MPD_MPC_VERSION_MAJOR = 0
+MPD_MPC_VERSION = $(MPD_MPC_VERSION_MAJOR).26
+MPD_MPC_SITE = http://www.musicpd.org/download/mpc/$(MPD_MPC_VERSION_MAJOR)
+MPD_MPC_SOURCE = mpc-$(MPD_MPC_VERSION).tar.xz
+MPD_MPC_LICENSE = GPLv2+
+MPD_MPC_LICENSE_FILES = COPYING
+MPD_MPC_DEPENDENCIES = host-pkgconf libmpdclient
+MPD_MPC_CONF_ENV = ac_cv_prog_cc_c99='-std=c99'
+
+$(eval $(autotools-package))
