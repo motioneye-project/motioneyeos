dadd8dd0b913f41faf4412dab8100965bb3fc621
Gregory Dymarek <gregd72002@gmail.com>
2014-12-12 15:42:42 +0000

    gengetopt: new package
    Add a host only package, which will be needed for janus-gateway.
    
    [Thomas:
      - Improve commit log.
      - Remove target variant of the package, not needed as we don't do
        development on the target.
      - Remove Config.in.host option for the package, there is not really
        a need to select it from menuconfig, it's merely a build
        dependency for another package.
      - Fix format of hash file.
      - Fix format of comment header in .mk file.
      - Use BR2_GNU_MIRROR.
      - License is GPLv3+, not GPLv3.
      - Add 'LICENSE' to the license files, as it indicates that the
        license is GPLv3+.
      - Add an explanation about $(MAKE1) being used.]
    
    Signed-off-by: Gregory Dymarek <gregd72002@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/gengetopt/gengetopt.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# gengetopt
+#
+################################################################################
+
+GENGETOPT_VERSION = 2.22.6
+GENGETOPT_SITE = $(BR2_GNU_MIRROR)/gengetopt/
+GENGETOPT_LICENSE = GPLv3+
+GENGETOPT_LICENSE_FILES = COPYING LICENSE
+
+# Parallel build broken
+GENGETOPT_MAKE = $(MAKE1)
+
+$(eval $(host-autotools-package))
