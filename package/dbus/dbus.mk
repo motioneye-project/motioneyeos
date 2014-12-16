f66ea18d98f141baae7de2970ca29286b1761749
Peter Korsgaard <peter@korsgaard.com>
2014-12-16 21:24:53 +0100

    dbus: bump version
    Regression fix for CVE-2014-3636, see release announcement:
    http://lists.freedesktop.org/archives/dbus/2014-November/016430.html
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/dbus/dbus.mk
+++ package/dbus/dbus.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DBUS_VERSION = 1.8.10
+DBUS_VERSION = 1.8.12
 DBUS_SITE = http://dbus.freedesktop.org/releases/dbus
 DBUS_LICENSE = AFLv2.1 GPLv2+
 DBUS_LICENSE_FILES = COPYING
