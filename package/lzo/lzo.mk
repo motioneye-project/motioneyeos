056e558fef591d4f4e56186a285c6abd00186256
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-12-16 11:08:18 -0300

    lzo: adjust libtool comment
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lzo/lzo.mk
+++ package/lzo/lzo.mk
@@ -9,7 +9,7 @@ LZO_SITE = http://www.oberhumer.com/opensource/lzo/download
 LZO_LICENSE = GPLv2+
 LZO_LICENSE_FILES = COPYING
 LZO_INSTALL_STAGING = YES
-# Our libtool patch does not apply to bundled ltmain.sh since it's too new.
+# Ships a beta libtool version hence our patch doesn't apply.
 # Run autoreconf to regenerate ltmain.sh.
 LZO_AUTORECONF = YES
 
