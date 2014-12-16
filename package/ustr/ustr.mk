5cefca4cc3da4fc5e6649aa3ae3a828df3d09a44
Clayton Shotwell <clshotwe@rockwellcollins.com>
2014-12-15 21:53:54 -0600

    ustr: new package
    [Thomas:
     - Fix typo in patch description.
     - Remove trailing whitespace in Config.in.
     - Fix Config.in comment to match what's specified in the Buildroot
       manual.
     - Adjust .mk logic to use BR2_STATIC_LIBS vs. BR2_SHARED_LIBS instead
       of BR2_PREFER_STATIC_LIB, and therefore support the installation of
       only the shared library.
     - Remove the libustr-debug library from the target and staging
       directories, it's the same as libustr, except that it's compiled
       with debugging symbols.
     - Always install only the shared variant of the library for
       host-ustr.]
    
    Signed-off-by: Clayton Shotwell <clshotwe@rockwellcollins.com>
    Signed-off-by: Matt Weber <matthew.weber@rockwellcollins.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/ustr/ustr.mk
@@ -0,0 +1,54 @@
+################################################################################
+#
+# ustr
+#
+################################################################################
+
+USTR_VERSION = 1.0.4
+USTR_SOURCE = ustr-$(USTR_VERSION).tar.bz2
+USTR_SITE = http://www.and.org/ustr/$(USTR_VERSION)/
+USTR_LICENSE = BSD-2c MIT LGPLv2+
+USTR_LICENSE_FILES = LICENCE LICENSE_BSD LICENSE_LGPL LICENSE_MIT
+
+USTR_INSTALL_STAGING = YES
+
+ifeq ($(BR2_STATIC_LIBS),y)
+USTR_BUILD_TARGETS = all
+USTR_INSTALL_TARGETS = install
+else ifeq ($(BR2_SHARED_LIBS),y)
+USTR_BUILD_TARGETS = all-shared
+USTR_INSTALL_TARGETS = install-shared
+else
+USTR_BUILD_TARGETS = all all-shared
+USTR_INSTALL_TARGETS = all install-shared
+endif
+
+define USTR_BUILD_CMDS
+	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS) $(USTR_BUILD_TARGETS)
+endef
+
+define USTR_INSTALL_STAGING_CMDS
+	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS) DESTDIR=$(STAGING_DIR) \
+		$(USTR_INSTALL_TARGETS)
+	$(RM) -f $(STAGING_DIR)/usr/lib/libustr-debug*
+	$(RM) -f $(STAGING_DIR)/usr/lib/pkgconfig/libustr-debug.pc
+endef
+
+define USTR_INSTALL_TARGET_CMDS
+	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS) DESTDIR=$(TARGET_DIR) \
+		$(USTR_INSTALL_TARGETS)
+	$(RM) -f $(TARGET_DIR)/usr/lib/libustr-debug*
+	$(RM) -rf $(TARGET_DIR)/usr/share/ustr-$(USTR_VERSION)
+endef
+
+define HOST_USTR_BUILD_CMDS
+	$(MAKE) -C $(@D) $(HOST_CONFIGURE_OPTS) all-shared
+endef
+
+define HOST_USTR_INSTALL_CMDS
+	$(MAKE) -C $(@D) $(HOST_CONFIGURE_OPTS) DESTDIR=$(HOST_DIR) \
+		install-shared
+endef
+
+$(eval $(generic-package))
+$(eval $(host-generic-package))
