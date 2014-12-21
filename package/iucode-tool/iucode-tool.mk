ac5052da0d02d2ea06c6c57b306d2dc2ee4988f9
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-12-21 11:09:31 +0100

    package/iucode-tool: bump version
    - updated upstream location
      http://metadata.ftp-master.debian.org/changelogs/contrib/i/iucode-tool/unstable_changelog
      "iucode-tool (1.0.2-1) unstable; urgency=low
       [...]
       * debian/control: update for new upstream location at Gitorious"
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/iucode-tool/iucode-tool.mk
+++ package/iucode-tool/iucode-tool.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-IUCODE_TOOL_VERSION = v1.0.1
-IUCODE_TOOL_SITE = git://git.debian.org/users/hmh/iucode-tool.git
+IUCODE_TOOL_VERSION = 1cbd73013cd4c6b2b287f07f6189eef35cb83ca0
+IUCODE_TOOL_SITE = git://gitorious.org/iucode-tool/iucode-tool.git
 ifeq ($(BR2_TOOLCHAIN_USES_UCLIBC),y)
 IUCODE_TOOL_CONF_ENV = LIBS="-largp"
 IUCODE_TOOL_DEPENDENCIES = argp-standalone
