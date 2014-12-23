e243a5102aa0e589262f14c9b49a61e8c8028ab9
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-12-23 18:46:25 +0100

    package/libcec: Bump to version 2.2.0
    Kodi Helix depends on libcec >= 2.2.0
    
    Removed libcec-0001-Fix-unregistering-a-callback-that-wasn-t-register.patch
    applied upstream: https://github.com/Pulse-Eight/libcec/commit/485660f
    
    Renamed remaining patch according to new naming convention.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libcec/libcec.mk
+++ package/libcec/libcec.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBCEC_VERSION = libcec-2.1.4-repack
+LIBCEC_VERSION = libcec-2.2.0-repack
 LIBCEC_SITE = $(call github,Pulse-Eight,libcec,$(LIBCEC_VERSION))
 LIBCEC_LICENSE = GPLv2+
 LIBCEC_LICENSE_FILES = COPYING
