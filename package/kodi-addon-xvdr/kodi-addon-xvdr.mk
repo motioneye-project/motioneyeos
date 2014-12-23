db78eb071546c74b7d2e06c9a567e5a3887880c3
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-12-23 18:46:33 +0100

    package/xbmc-addon-xvdr: rename to kodi-addon-xvdr
    Following the rename of the XBMC project to Kodi, this commit renames
    the xbmc-addon-xvdr package to kodi-addon-xvdr, and adds the
    appropriate Config.in.legacy handling.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/kodi-addon-xvdr/kodi-addon-xvdr.mk
+++ package/kodi-addon-xvdr/kodi-addon-xvdr.mk
@@ -1,26 +1,26 @@
 ################################################################################
 #
-# xbmc-addon-xvdr
+# kodi-addon-xvdr
 #
 ################################################################################
 
 # This cset is on master. When a Gotham branch is made, we should
 # follow it, as incompatible changes in the plugins API can happen
 # on the master branch.
-XBMC_ADDON_XVDR_VERSION = 82b5093ad33847f97a978cb620c9a87d2dbcf7eb
-XBMC_ADDON_XVDR_SITE = $(call github,pipelka,xbmc-addon-xvdr,$(XBMC_ADDON_XVDR_VERSION))
-XBMC_ADDON_XVDR_LICENSE = GPLv2+
-XBMC_ADDON_XVDR_LICENSE_FILES = COPYING
+KODI_ADDON_XVDR_VERSION = 82b5093ad33847f97a978cb620c9a87d2dbcf7eb
+KODI_ADDON_XVDR_SITE = $(call github,pipelka,xbmc-addon-xvdr,$(KODI_ADDON_XVDR_VERSION))
+KODI_ADDON_XVDR_LICENSE = GPLv2+
+KODI_ADDON_XVDR_LICENSE_FILES = COPYING
 
 # There's no ./configure in the git tree, we need to generate it
-# xbmc-addon-xvdr uses a weird autogen.sh script, which
+# kodi-addon-xvdr uses a weird autogen.sh script, which
 # is even incorrect (it's missing the #! ) Sigh... :-(
 # Fortunately, with our little patch, it autoreconfs nicely! :-)
-XBMC_ADDON_XVDR_AUTORECONF = YES
+KODI_ADDON_XVDR_AUTORECONF = YES
 
-# This really is a runtime dependency, but we need XBMC to be installed
-# first, since we'll install files in XBMC's directories _after_ XBMC has
+# This really is a runtime dependency, but we need KODI to be installed
+# first, since we'll install files in KODI's directories _after_ KODI has
 # installed his own files
-XBMC_ADDON_XVDR_DEPENDENCIES = kodi
+KODI_ADDON_XVDR_DEPENDENCIES = kodi
 
 $(eval $(autotools-package))
