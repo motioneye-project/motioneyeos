f84f60c652b1aa29b7bb02dcf1fd76b4498fe1e8
Bernd Kuhls <bernd.kuhls@t-online.de>
2014-12-23 18:46:32 +0100

    package/kodi-pvr-addons: Bump version for Helix-compatibility
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/kodi-pvr-addons/kodi-pvr-addons.mk
+++ package/kodi-pvr-addons/kodi-pvr-addons.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-# This cset is on the branch 'gotham'
-# When Kodi is updated, then this should be updated to the corresponding branch
-KODI_PVR_ADDONS_VERSION = be12a8da2072e9c3ddad54892df2f85b759d4e9a
+# This cset is on the branch 'master'
+# When Kodi is released this should be updated to the corresponding branch
+KODI_PVR_ADDONS_VERSION = 89ba5bb6917a820b278688b9f43b23834002e9b5
 KODI_PVR_ADDONS_SITE = $(call github,opdenkamp,xbmc-pvr-addons,$(KODI_PVR_ADDONS_VERSION))
 KODI_PVR_ADDONS_LICENSE = GPLv3+
 KODI_PVR_ADDONS_LICENSE_FILES = COPYING
@@ -14,7 +14,7 @@ KODI_PVR_ADDONS_LICENSE_FILES = COPYING
 # There's no ./configure in the git tree, we need to generate it
 KODI_PVR_ADDONS_AUTORECONF = YES
 
-KODI_PVR_ADDONS_DEPENDENCIES = boost zlib
+KODI_PVR_ADDONS_DEPENDENCIES = zlib
 # This really is a runtime dependency, but we need KODI to be installed
 # first, since we'll install files in KODI's directories _after_ KODI has
 # installed its own files
@@ -23,12 +23,5 @@ KODI_PVR_ADDONS_DEPENDENCIES += kodi
 KODI_PVR_ADDONS_CONF_OPTS = \
 	--enable-release \
 	--enable-addons-with-dependencies
-
-ifeq ($(BR2_PACKAGE_MYSQL),y)
-KODI_PVR_ADDONS_CONF_OPTS += --enable-mysql
-KODI_PVR_ADDONS_DEPENDENCIES += mysql
-else
-KODI_PVR_ADDONS_CONF_OPTS += --disable-mysql
-endif
 
 $(eval $(autotools-package))
