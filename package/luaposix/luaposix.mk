fdca0dda1d7f91d2af0467f04c812af944a86cb7
Francois Perrad <fperrad@gmail.com>
2014-12-24 10:10:40 +0100

    luaposix: bump to version 33.0.0
    ncurses becomes a mandatory dependency
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/luaposix/luaposix.mk
+++ package/luaposix/luaposix.mk
@@ -4,15 +4,11 @@
 #
 ################################################################################
 
-LUAPOSIX_VERSION = 32
+LUAPOSIX_VERSION = 33.0.0
 LUAPOSIX_SITE = $(call github,luaposix,luaposix,release-v$(LUAPOSIX_VERSION))
 LUAPOSIX_LICENSE = MIT
 LUAPOSIX_LICENSE_FILES = COPYING
-LUAPOSIX_DEPENDENCIES = luainterpreter host-lua
+LUAPOSIX_DEPENDENCIES = luainterpreter host-lua ncurses
 LUAPOSIX_CONF_OPTS = --libdir="/usr/lib/lua/$(LUAINTERPRETER_ABIVER)" --datarootdir="/usr/share/lua/$(LUAINTERPRETER_ABIVER)"
-
-ifeq ($(BR2_PACKAGE_NCURSES),y)
-LUAPOSIX_DEPENDENCIES += ncurses
-endif
 
 $(eval $(autotools-package))
