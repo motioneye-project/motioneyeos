1696f254aa2c68a274956902cfac7b7e9a607b11
Floris Bos <bos@je-eigen-domein.nl>
2014-12-26 01:46:14 +0100

    libvncserver: bump version to 0.9.10
    [Thomas:
     - add comment about AUTORECONF = YES, noticed by Baruch Siach, and
       suggested by Yann E. Morin.
     - add dependency on host-pkgconf, since the configure script uses
       PKG_CHECK_MODULES.]
    
    Signed-off-by: Floris Bos <bos@je-eigen-domein.nl>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libvncserver/libvncserver.mk
+++ package/libvncserver/libvncserver.mk
@@ -4,13 +4,18 @@
 #
 ################################################################################
 
-LIBVNCSERVER_VERSION = 0.9.9
+LIBVNCSERVER_VERSION = 0.9.10
 LIBVNCSERVER_SOURCE = LibVNCServer-$(LIBVNCSERVER_VERSION).tar.gz
-LIBVNCSERVER_SITE = http://downloads.sourceforge.net/project/libvncserver/libvncserver/$(LIBVNCSERVER_VERSION)
+LIBVNCSERVER_SITE = https://github.com/LibVNC/libvncserver/archive
 LIBVNCSERVER_LICENSE = GPLv2+
 LIBVNCSERVER_LICENSE_FILES = COPYING
 LIBVNCSERVER_INSTALL_STAGING = YES
 LIBVNCSERVER_CONFIG_SCRIPTS = libvncserver-config
+LIBVNCSERVER_DEPENDENCIES = host-pkgconf
+
+# Upstream decided to remove generated autotools files from the
+# tarball, so we need to generate them.
+LIBVNCSERVER_AUTORECONF = YES
 
 # only used for examples
 LIBVNCSERVER_CONF_OPTS += --with-sdl-config=/bin/false
