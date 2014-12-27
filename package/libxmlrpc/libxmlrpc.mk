7b5de43a03d432c28536cbb58fc50e5c393494b6
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-12-27 17:25:54 +0100

    libxmlrpc: bump to 1.25.30 and add hash file
    This commit bumps libxmlrpc to the latest available stable version,
    and adds a hash file.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>

--- package/libxmlrpc/libxmlrpc.mk
+++ package/libxmlrpc/libxmlrpc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBXMLRPC_VERSION = 1.25.26
+LIBXMLRPC_VERSION = 1.25.30
 LIBXMLRPC_SOURCE = xmlrpc-c-$(LIBXMLRPC_VERSION).tgz
 LIBXMLRPC_SITE = http://downloads.sourceforge.net/project/xmlrpc-c/Xmlrpc-c%20Super%20Stable/$(LIBXMLRPC_VERSION)
 LIBXMLRPC_LICENSE = BSD-3c (xml-rpc main code and abyss web server), BSD like (lib/expat), Python 1.5.2 license (parts of xmlrpc_base64.c)
