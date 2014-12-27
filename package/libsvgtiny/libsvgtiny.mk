c24f7cdd11a2045e4d018103fa36e61e556bbde6
Romain Naour <romain.naour@openwide.fr>
2014-12-27 22:58:21 +0100

    package/libsvgtiny: enable shared library build
    [Thomas: clarify the comment explaining why we only build/install the
    shared library when BR2_STATIC_SHARED_LIBS=y.]
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libsvgtiny/libsvgtiny.mk
+++ package/libsvgtiny/libsvgtiny.mk
@@ -11,18 +11,30 @@ LIBSVGTINY_DEPENDENCIES = libxml2 host-gperf host-pkgconf
 LIBSVGTINY_LICENSE = MIT
 LIBSVGTINY_LICENSE_FILES = README
 
+# The libsvgtiny build system cannot build both the shared and static
+# libraries. So when the Buildroot configuration requests to build
+# both the shared and static variants, we build only the shared one.
+ifeq ($(BR2_SHARED_LIBS)$(BR2_SHARED_STATIC_LIBS),y)
+LIBSVGTINY_COMPONENT_TYPE = lib-shared
+else
+LIBSVGTINY_COMPONENT_TYPE = lib-static
+endif
+
 define LIBSVGTINY_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) PREFIX=/usr
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) PREFIX=/usr \
+		COMPONENT_TYPE=$(LIBSVGTINY_COMPONENT_TYPE)
 endef
 
 define LIBSVGTINY_INSTALL_STAGING_CMDS
 	$(TARGET_CONFIGURE_OPTS) \
-		$(MAKE) -C $(@D) PREFIX=/usr DESTDIR=$(STAGING_DIR) install
+		$(MAKE) -C $(@D) PREFIX=/usr DESTDIR=$(STAGING_DIR) \
+		COMPONENT_TYPE=$(LIBSVGTINY_COMPONENT_TYPE) install
 endef
 
 define LIBSVGTINY_INSTALL_TARGET_CMDS
 	$(TARGET_CONFIGURE_OPTS) \
-		$(MAKE) -C $(@D) PREFIX=/usr DESTDIR=$(TARGET_DIR) install
+		$(MAKE) -C $(@D) PREFIX=/usr DESTDIR=$(TARGET_DIR) \
+		COMPONENT_TYPE=$(LIBSVGTINY_COMPONENT_TYPE) install
 endef
 
 $(eval $(generic-package))
