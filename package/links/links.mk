d856c328243930aa7c83aa79942f5978cd41be8f
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-12-28 09:31:17 -0300

    links: bump to version 2.8
    Also add hash file.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/links/links.mk
+++ package/links/links.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LINKS_VERSION = 2.7
+LINKS_VERSION = 2.8
 LINKS_SITE = http://links.twibright.com/download
 LINKS_DEPENDENCIES = host-pkgconf
 LINKS_LICENSE = GPLv2+
