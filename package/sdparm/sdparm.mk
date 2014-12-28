09a3e7545f12d588cfb4ff1428d6bf4943e34072
Gustavo Zacarias <gustavo@zacarias.com.ar>
2014-12-28 12:24:38 -0300

    sdparm: bump to version 1.09
    Also add hash file.
    And make the libsgutils2 support (from sg3_utils) deterministic.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/sdparm/sdparm.mk
+++ package/sdparm/sdparm.mk
@@ -4,10 +4,16 @@
 #
 ################################################################################
 
-SDPARM_VERSION = 1.08
-SDPARM_SOURCE = sdparm-$(SDPARM_VERSION).tgz
+SDPARM_VERSION = 1.09
+SDPARM_SOURCE = sdparm-$(SDPARM_VERSION).tar.xz
 SDPARM_SITE = http://sg.danny.cz/sg/p
 SDPARM_LICENSE = BSD-3c
 SDPARM_LICENSE_FILES = COPYING
+
+ifeq ($(BR2_PACKAGE_SG3_UTILS),y)
+SDPARM_DEPENDENCIES += sg3_utils
+else
+SDPARM_CONF_OPTS += --disable-libsgutils
+endif
 
 $(eval $(autotools-package))
