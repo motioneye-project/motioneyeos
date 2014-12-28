144e21f203cf88c1768488d08e201e9579db857e
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-12-28 21:54:52 +0100

    python3: bump to 3.4.2
    This commit bumps python3 to Python 3.4.2. Two patches had to be
    changed slightly to fix some minor conflicts.
    
    PYTHON3_LIBTOOL_PATH = NO was added to prevent Buildroot from trying
    to patch a version of libtool for which we don't have matching
    patches, which isn't a problem since we're anyway not using the part
    of the Python sources that uses libtool (it's the built-in copy of
    libffi, and we use the external libffi).
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Tested-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>

--- package/python3/python3.mk
+++ package/python3/python3.mk
@@ -5,11 +5,18 @@
 ################################################################################
 
 PYTHON3_VERSION_MAJOR = 3.4
-PYTHON3_VERSION = $(PYTHON3_VERSION_MAJOR).1
+PYTHON3_VERSION = $(PYTHON3_VERSION_MAJOR).2
 PYTHON3_SOURCE = Python-$(PYTHON3_VERSION).tar.xz
 PYTHON3_SITE = http://python.org/ftp/python/$(PYTHON3_VERSION)
 PYTHON3_LICENSE = Python software foundation license v2, others
 PYTHON3_LICENSE_FILES = LICENSE
+
+# Python itself doesn't use libtool, but it includes the source code
+# of libffi, which uses libtool. Unfortunately, it uses a beta version
+# of libtool for which we don't have a matching patch. However, this
+# is not a problem, because we don't use the libffi copy included in
+# the Python sources, but instead use an external libffi library.
+PYTHON3_LIBTOOL_PATCH = NO
 
 # Python needs itself and a "pgen" program to build itself, both being
 # provided in the Python sources. So in order to cross-compile Python,
