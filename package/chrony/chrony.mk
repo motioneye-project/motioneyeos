08b1c665171727cae439c440f4502a2424891bdf
Peter Korsgaard <peter@korsgaard.com>
2014-12-29 15:55:17 +0100

    chrony: bump version
    And add hash file.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/chrony/chrony.mk
+++ package/chrony/chrony.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CHRONY_VERSION = 1.30
+CHRONY_VERSION = 1.31
 CHRONY_SITE = http://download.tuxfamily.org/chrony
 CHRONY_LICENSE = GPLv2
 CHRONY_LICENSE_FILES = COPYING
