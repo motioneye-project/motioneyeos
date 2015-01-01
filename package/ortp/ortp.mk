823e90cb531e2a9e479aa39f923156cb55f7ef12
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2015-01-01 17:02:20 +0100

    ortp: do not use libsrtp
    ortp is not happy with the newly packaged libsrtp implementation, so
    let's not use it.
    
    Fixes:
    
      http://autobuild.buildroot.org/results/ba1/ba1e29c6b5cc7ff7ff4582ec2319598ff9b28bae/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ortp/ortp.mk
+++ package/ortp/ortp.mk
@@ -6,7 +6,10 @@
 
 ORTP_VERSION = 0.22.0
 ORTP_SITE = http://download.savannah.nongnu.org/releases/linphone/ortp/sources
-ORTP_CONF_OPTS = --disable-strict
+
+# ortp is not happy with the libsrtp implementation packaged in
+# Buildroot
+ORTP_CONF_OPTS = --disable-strict --with-srtp=none
 ORTP_INSTALL_STAGING = YES
 ORTP_LICENSE = LGPLv2.1+
 ORTP_LICENSE_FILES = COPYING
