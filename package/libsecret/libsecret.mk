cf2c54b25b8f6311fce81604a07d3d43ffbdfd3c
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-02 19:45:06 -0300

    libsecret: bump to version 0.18
    Also add hash file.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libsecret/libsecret.mk
+++ package/libsecret/libsecret.mk
@@ -4,22 +4,21 @@
 #
 ################################################################################
 
-LIBSECRET_VERSION = 0.15
+LIBSECRET_VERSION = 0.18
 LIBSECRET_SITE = http://ftp.gnome.org/pub/GNOME/sources/libsecret/$(LIBSECRET_VERSION)
 LIBSECRET_SOURCE = libsecret-$(LIBSECRET_VERSION).tar.xz
-LIBSECRET_LICENSE = LGPLv2.1+
-LIBSECRET_LICENSE_FILES = COPYING
 LIBSECRET_INSTALL_STAGING = YES
-
 LIBSECRET_DEPENDENCIES = libglib2 host-intltool
 LIBSECRET_CONF_OPTS = --disable-manpages --disable-strict --disable-coverage --enable-vala=no
+LIBSECRET_LICENSE = LGPLv2.1+
+LIBSECRET_LICENSE_FILES = COPYING
 
 ifeq ($(BR2_PACKAGE_LIBGCRYPT),y)
-	LIBSECRET_DEPENDENCIES += libgcrypt
-	LIBSECRET_CONF_OPTS += --enable-gcrypt \
-		--with-libgcrypt-prefix=$(STAGING_DIR)/usr
+LIBSECRET_DEPENDENCIES += libgcrypt
+LIBSECRET_CONF_OPTS += --enable-gcrypt \
+	--with-libgcrypt-prefix=$(STAGING_DIR)/usr
 else
-	LIBSECRET_CONF_OPTS += --disable-gcrypt
+LIBSECRET_CONF_OPTS += --disable-gcrypt
 endif
 
 $(eval $(autotools-package))
