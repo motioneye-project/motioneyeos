e2db1daacff21133e2c03967e4b65a67df746ce8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-02 09:39:40 -0300

    mrouted: bump to version 3.9.7
    bcopy patch removed since code is upstream.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mrouted/mrouted.mk
+++ package/mrouted/mrouted.mk
@@ -4,9 +4,17 @@
 #
 ################################################################################
 
-MROUTED_VERSION = 3.9.6
+MROUTED_VERSION = 3.9.7
 MROUTED_SITE = $(call github,troglobit,mrouted,$(MROUTED_VERSION))
 MROUTED_DEPENDENCIES = host-bison
+MROUTED_LICENSE = BSD-3c
+MROUTED_LICENSE_FILES = LICENSE
+
+define MROUTED_CONFIGURE_CMDS
+	(cd $(@D); \
+		$(TARGET_MAKE_ENV) $(@D)/configure --enable-rsrr \
+	)
+endef
 
 define MROUTED_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
