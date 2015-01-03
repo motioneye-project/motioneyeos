616c878ce5ecfef1fc69fe6d87e135b326d63a37
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-03 08:06:37 -0300

    stunnel: bump to version 5.09
    Change to usenix.org.uk mirror since nluug.nl doesn't seem to mirror
    files as quickly and is down at the moment.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/stunnel/stunnel.mk
+++ package/stunnel/stunnel.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-STUNNEL_VERSION = 5.08
-STUNNEL_SITE = http://ftp.nluug.nl/pub/networking/stunnel/archive/5.x
+STUNNEL_VERSION = 5.09
+STUNNEL_SITE = http://www.usenix.org.uk/mirrors/stunnel/archive/5.x
 STUNNEL_DEPENDENCIES = openssl
 STUNNEL_CONF_OPTS = --with-ssl=$(STAGING_DIR)/usr --with-threads=fork \
 	--disable-libwrap
