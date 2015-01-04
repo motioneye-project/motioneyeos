514d713edc02598402aa0ebe7c724aa25b3abcfe
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2015-01-04 22:08:44 +0100

    python-django: be more specific about the license
    The license is really a 3 clauses BSD license, so let's specify this
    in python-django.mk.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -8,7 +8,7 @@ PYTHON_DJANGO_VERSION = 1.7.2
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
 PYTHON_DJANGO_SITE = https://pypi.python.org/packages/source/D/Django/
-PYTHON_DJANGO_LICENSE = BSD
+PYTHON_DJANGO_LICENSE = BSD-3c
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_SETUP_TYPE = setuptools
 
