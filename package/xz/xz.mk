95806a5fe1ac0f43f4e20cc0ac7036029e8d94a0
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2015-01-05 14:57:50 +0000

    xz: bump version to 5.2.0
    -Bump version to 5.2.0
    -Update hash file
    -Use xz tarball instead of bz2 to save space and bandwidth
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/xz/xz.mk
+++ package/xz/xz.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-XZ_VERSION = 5.0.5
-XZ_SOURCE = xz-$(XZ_VERSION).tar.bz2
+XZ_VERSION = 5.2.0
+XZ_SOURCE = xz-$(XZ_VERSION).tar.xz
 XZ_SITE = http://tukaani.org/xz
 XZ_INSTALL_STAGING = YES
 XZ_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
