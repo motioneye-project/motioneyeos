b7f5b421008fbe68583148af1b7475a7b2911b39
Jörg Krause <jkrause@posteo.de>
2015-01-05 17:35:36 +0100

    package/nodejs: bump to version 0.10.35
    Signed-off-by: Jörg Krause <jkrause@posteo.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/nodejs/nodejs.mk
+++ package/nodejs/nodejs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NODEJS_VERSION = 0.10.33
+NODEJS_VERSION = 0.10.35
 NODEJS_SOURCE = node-v$(NODEJS_VERSION).tar.gz
 NODEJS_SITE = http://nodejs.org/dist/v$(NODEJS_VERSION)
 NODEJS_DEPENDENCIES = host-python host-nodejs zlib \
