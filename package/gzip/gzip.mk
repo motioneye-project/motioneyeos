09d6597cb5ab326097533cd4be7b9764b960c930
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-06 11:42:29 -0300

    gzip: fix installation path
    Make gzip install binaries to / rather than /usr to fix bug #7766, it's
    the FHS mandated target.
    
    This also avoids duplicating binaries with busybox when both are
    installed.
    
    Also make gzip install after busybox if both are enabled to make the
    proper gzip package override any busybox version since it's usually more
    lightweight in functionality and slower.
    
    And add a hash file while at it.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gzip/gzip.mk
+++ package/gzip/gzip.mk
@@ -7,6 +7,10 @@
 GZIP_VERSION = 1.6
 GZIP_SOURCE = gzip-$(GZIP_VERSION).tar.xz
 GZIP_SITE = $(BR2_GNU_MIRROR)/gzip
+# Some other tools expect it to be in /bin
+GZIP_CONF_OPTS = --exec-prefix=/
+# Prefer full gzip over potentially lightweight/slower from busybox
+GZIP_DEPENDENCIES = $(if $(BR2_PACKAGE_BUSYBOX),busybox)
 GZIP_LICENSE = GPLv3+
 GZIP_LICENSE_FILES = COPYING
 
