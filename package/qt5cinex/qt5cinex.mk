2b266815cb90a9bb8f5e4bc85463f51accf91060
Pierre Le Magourou <pierre.lemagourou@openwide.fr>
2015-01-07 15:22:21 +0100

    qt5cinex: Add new Qt5CinematicExperience package.
    [Thomas:
     - Change the dependency logic in the Config.in file. We don't want to
       have a 'depends on BR2_PACKAGE_QT5GRAPHICALEFFECTS': it should be
       selected automatically. Instead, let's have a dependency on Qt5 and
       OpenGL, and select everything else automatically. A comment is
       added, shown only when Qt5 is available, on the right platforms
       (which have JSCore support), to explain that we need an OpenGL
       backend.
     - Change the prompt of the package to be qt5cinex, to match the
       package name.
     - Replace "High-definition support" by "High-definition version".
     - Fix a typo in the Config.in help text: definifition -> definition.
     - Add a comment in the .mk file explaining why we install a wrapper
       shell script (explanation taken from Pierre's e-mail).
     - Fix indentation in the install target commands.
     - Keep only sha256 hashes, those are sufficient. Replace the comment
       in the hash file by the more traditional "Locally computed".]
    
    Signed-off-by: Pierre Le Magourou <pierre.lemagourou@openwide.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/qt5cinex/qt5cinex.mk
@@ -0,0 +1,42 @@
+################################################################################
+#
+# qt5cinex
+#
+################################################################################
+
+QT5CINEX_VERSION = 1.0
+QT5CINEX_SITE = http://quitcoding.com/download/
+
+ifeq ($(BR2_PACKAGE_QT5CINEX_HD),y)
+QT5CINEX_HD = "rpi_"
+endif
+
+QT5CINEX_SOURCE = Qt5_CinematicExperience_$(QT5CINEX_HD)$(QT5CINEX_VERSION).tgz
+QT5CINEX_DEPENDENCIES = qt5base qt5declarative
+
+QT5CINEX_LICENSE = CC-BY-3.0
+QT5CINEX_LICENSE_FILE = README
+
+define QT5CINEX_CONFIGURE_CMDS
+	cd $(@D); $(TARGET_MAKE_ENV) $(HOST_DIR)/usr/bin/qmake
+endef
+
+define QT5CINEX_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+# In addition to the Cinematic Experience itself, we also install a
+# wrapper shell script to /usr/bin: the Cinematic Experience binary
+# wants its resource files to be available directly under a contents/
+# sub-directory, which isn't very practical to install in /usr/bin/.
+define QT5CINEX_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/Qt5_CinematicExperience \
+		$(TARGET_DIR)/usr/share/Qt5/CinematicExperience/Qt5_CinematicExperience
+	$(INSTALL) -D -m 0664 $(@D)/Qt5_CinematicExperience.qml \
+		$(TARGET_DIR)/usr/share/Qt5/CinematicExperience/Qt5_CinematicExperience.qml
+	cp -dpfr $(@D)/content $(TARGET_DIR)/usr/share/Qt5/CinematicExperience/content
+	$(INSTALL) -m 0755 -D package/qt5cinex/CinematicExperience-demo \
+		$(TARGET_DIR)/usr/bin/CinematicExperience-demo
+endef
+
+$(eval $(generic-package))
