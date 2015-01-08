0ae1ab3ed51ff2368767b4d8b163a5da7011a8b4
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-08 20:06:37 -0300

    openntpd: bump to version 5.7p1
    Also:
    
    * Add hash file
    * Tweak the initscript to use a pidfile to avoid nasty warnings
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/openntpd/openntpd.mk
+++ package/openntpd/openntpd.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-OPENNTPD_VERSION = 3.9p1
-OPENNTPD_SITE = ftp://ftp.openbsd.org/pub/OpenBSD/OpenNTPD
-OPENNTPD_CONF_OPTS = --with-builtin-arc4random --disable-strip
+OPENNTPD_VERSION = 5.7p1
+OPENNTPD_SITE = http://ftp.openbsd.org/pub/OpenBSD/OpenNTPD
 OPENNTPD_LICENSE = MIT-like, BSD-2c, BSD-3c
 OPENNTPD_LICENSE_FILES = LICENCE
 
