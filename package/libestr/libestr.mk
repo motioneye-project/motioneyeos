1e3dc8f9de561e81a2804149cd9bf3a32788e6e0
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-13 20:16:56 -0300

    libestr: bump to version 0.1.10
    Also add hash file.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libestr/libestr.mk
+++ package/libestr/libestr.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBESTR_VERSION = 0.1.9
+LIBESTR_VERSION = 0.1.10
 LIBESTR_SITE = http://libestr.adiscon.com/files/download
 LIBESTR_LICENSE = LGPLv2.1+
 LIBESTR_LICENSE_FILES = COPYING
