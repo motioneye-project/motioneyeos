072ba39cee77bb2487a84627056900ad6be52c9b
Steve Kenton <skenton@ou.edu>
2015-01-12 19:55:56 -0600

    dvdauthor : new package
    DVDAuthor is a set of tools to help you author the file and directory
    structure of a DVD-Video disc, including programmatic commands for
    implementing interactive behaviour. It is driven by command lines and
    XML control files, though there are other programs that provide
    GUI-based front ends if you prefer
    
    [Thomas:
     - Make the libdvdread dependency optional: it is only needed for the
       dvdunauthor program, which can be disabled using a configure
       option. So a separate Config.in option was added for dvdunauthor.
     - Adjust license to GPLv2+, as can be seen in the source code itself.
     - Add optional dependency on fontconfig and imagemagick. Make sure we
       don't pick up a host installed GraphicsMagick (which is different
       from ImageMagick!)
     - Add a hash file.]
    
    Signed-off-by Stephen M. Kenton <skenton@ou.edu>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/dvdauthor/dvdauthor.mk
@@ -0,0 +1,39 @@
+###############################################################################
+#
+# dvdauthor
+#
+###############################################################################
+
+DVDAUTHOR_VERSION = 0.7.1
+DVDAUTHOR_SITE = http://sourceforge.net/projects/dvdauthor/files/dvdauthor/$(DVDAUTHOR_VERSION)
+DVDAUTHOR_DEPENDENCIES = host-pkgconf libxml2 freetype libpng
+DVDAUTHOR_LICENSE = GPLv2+
+DVDAUTHOR_LICENSE_FILES = COPYING
+DVDAUTHOR_CONF_ENV = \
+	ac_cv_prog_FREETYPECONFIG=$(STAGING_DIR)/usr/bin/freetype-config \
+	ac_cv_path_XML2_CONFIG=$(STAGING_DIR)/usr/bin/xml2-config \
+	ac_cv_prog_GMAGICKCONFIG=
+
+ifeq ($(BR2_PACKAGE_IMAGEMAGICK),y)
+DVDAUTHOR_DEPENDENCIES += imagemagick
+DVDAUTHOR_CONF_ENV += \
+	ac_cv_prog_MAGICKCONFIG=$(STAGING_DIR)/usr/bin/Magick-config
+else
+DVDAUTHOR_CONF_ENV += \
+	ac_cv_prog_MAGICKCONFIG=
+endif
+
+# Automatically detected by dvdauthor configure script, no way to
+# disable.
+ifeq ($(BR2_PACKAGE_FONTCONFIG),y)
+DVDAUTHOR_DEPENDENCIES += fontconfig
+endif
+
+ifeq ($(BR2_PACKAGE_DVDAUTHOR_DVDUNAUTHOR),y)
+DVDAUTHOR_DEPENDENCIES += libdvdread
+DVDAUTHOR_CONF_OPTS += --enable-dvdunauthor
+else
+DVDAUTHOR_CONF_OPTS += --disable-dvdunauthor
+endif
+
+$(eval $(autotools-package))
