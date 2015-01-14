52620d1eac198145c1a1abc97ddac3d7513e0673
Yegor Yefremov <yegorslists@googlemail.com>
2015-01-14 21:11:06 +0100

    python-pyinotify: new package
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/python-pyinotify/python-pyinotify.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# python-pyinotify
+#
+################################################################################
+
+PYTHON_PYINOTIFY_VERSION = 0.9.5
+PYTHON_PYINOTIFY_SITE = $(call github,seb-m,pyinotify,$(PYTHON_PYINOTIFY_VERSION))
+PYTHON_PYINOTIFY_SETUP_TYPE = setuptools
+PYTHON_PYINOTIFY_LICENSE = MIT
+PYTHON_PYINOTIFY_LICENSE_FILES = COPYING
+
+$(eval $(python-package))
