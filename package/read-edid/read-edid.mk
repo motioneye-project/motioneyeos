71539b69ae9bf521aaf8c2adb48545a9355dce5b
Peter Korsgaard <peter@korsgaard.com>
2015-01-14 16:30:58 +0100

    read-edid: make available for !x86
    It is only the get-edid tool that is x86 specific, parse-edid builds and
    works fine on other architectures so make it available everywhere.
    
    Also drop the custom install step as 'make install' does the right thing.
    This does cause us to install into /usr/sbin instead of /sbin, but as that
    is what upstream wants we can consider that a bugfix.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/read-edid/read-edid.mk
+++ package/read-edid/read-edid.mk
@@ -13,9 +13,4 @@ define READ_EDID_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
 endef
 
-define READ_EDID_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/get-edid $(TARGET_DIR)/sbin/get-edid
-	$(INSTALL) -D -m 0755 $(@D)/parse-edid $(TARGET_DIR)/sbin/parse-edid
-endef
-
 $(eval $(autotools-package))
