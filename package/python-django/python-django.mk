23ed2cf2dc85f98412862c66766f9aaeee23621e
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-14 15:21:44 -0300

    python-django: security bump to version 1.7.3
    Fixes:
    
    CVE-2015-0219 - incorrectly handled underscores in WSGI headers. A
    remote attacker could possibly use this issue to spoof headers in
    certain environments.
    
    CVE-2015-0220 - incorrectly handled user-supplied redirect URLs. A
    remote attacker could possibly use this issue to perform a cross-site
    scripting attack.
    
    CVE-2015-0221 - incorrectly handled reading files in
    django.views.static.serve(). A remote attacker could possibly use this
    issue to cause Django to consume resources, resulting in a denial of
    service.
    
    CVE-2015-0222 - incorrectly handled forms with ModelMultipleChoiceField.
    A remote attacker could possibly use this issue to cause a large number
    of SQL queries, resulting in a database denial of service.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 1.7.2
+PYTHON_DJANGO_VERSION = 1.7.3
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
 PYTHON_DJANGO_SITE = https://pypi.python.org/packages/source/D/Django/
