38dabc6e9b4b67e33b3b1ec3def7d8366096136a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-15 11:30:19 -0300

    bison: remove deprecated target package
    It's been deprecated for a year now so remove the target variant.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/bison/bison.mk
+++ package/bison/bison.mk
@@ -9,15 +9,6 @@ BISON_SOURCE = bison-$(BISON_VERSION).tar.xz
 BISON_SITE = $(BR2_GNU_MIRROR)/bison
 BISON_LICENSE = GPLv3+
 BISON_LICENSE_FILES = COPYING
-BISON_CONF_ENV = ac_cv_path_M4=/usr/bin/m4
-BISON_DEPENDENCIES = m4
 BISON_AUTORECONF = YES
 
-define BISON_DISABLE_EXAMPLES
-	echo 'all install:' > $(@D)/examples/Makefile
-endef
-
-BISON_POST_CONFIGURE_HOOKS += BISON_DISABLE_EXAMPLES
-
-$(eval $(autotools-package))
 $(eval $(host-autotools-package))
