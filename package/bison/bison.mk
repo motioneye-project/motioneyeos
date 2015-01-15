3dafe024764a9e7b4a1c89a0ecc8047f6d4c2f2c
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-15 11:30:20 -0300

    bison: bump to version 3.0.3
    Also add hash file and remove patch since it's no longer necessary.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/bison/bison.mk
+++ package/bison/bison.mk
@@ -4,11 +4,10 @@
 #
 ################################################################################
 
-BISON_VERSION = 3.0.2
+BISON_VERSION = 3.0.3
 BISON_SOURCE = bison-$(BISON_VERSION).tar.xz
 BISON_SITE = $(BR2_GNU_MIRROR)/bison
 BISON_LICENSE = GPLv3+
 BISON_LICENSE_FILES = COPYING
-BISON_AUTORECONF = YES
 
 $(eval $(host-autotools-package))
