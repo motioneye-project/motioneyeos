6927bc9150a8304ce6c059ae904d9fa6a8fea4ca
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-15 11:30:22 -0300

    m4: remove deprecated target package
    It's been deprecated for a year now so remove the target variant.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/m4/m4.mk
+++ package/m4/m4.mk
@@ -9,13 +9,6 @@ M4_SOURCE = m4-$(M4_VERSION).tar.xz
 M4_SITE = $(BR2_GNU_MIRROR)/m4
 M4_LICENSE = GPLv3+
 M4_LICENSE_FILES = COPYING
-M4_CONF_ENV = gl_cv_func_gettimeofday_clobber=no
-
-ifneq ($(BR2_USE_WCHAR),y)
-M4_CONF_ENV += gt_cv_c_wchar_t=no gl_cv_absolute_wchar_h=__fpending.h
-endif
-
 HOST_M4_CONF_OPTS = --disable-static
 
-$(eval $(autotools-package))
 $(eval $(host-autotools-package))
