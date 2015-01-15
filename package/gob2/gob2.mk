7f9d4441159836b1a3ba9b214f318943bbda4b44
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-15 11:30:18 -0300

    gob2: remove deprecated target package
    It's been deprecated for a year now so remove the target variant.
    
    Also rename patch to new naming convention.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gob2/gob2.mk
+++ package/gob2/gob2.mk
@@ -7,10 +7,8 @@
 GOB2_VERSION = 2.0.20
 GOB2_SOURCE = gob2-$(GOB2_VERSION).tar.xz
 GOB2_SITE = http://ftp.5z.com/pub/gob
-GOB2_DEPENDENCIES = libglib2 flex bison host-pkgconf host-flex
 HOST_GOB2_DEPENDENCIES = host-bison host-flex host-libglib2
 
-$(eval $(autotools-package))
 $(eval $(host-autotools-package))
 
 # gob2 for the host
