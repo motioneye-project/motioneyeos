ef31b974ad9aeb4e9bfd58d54c7a5d7ad471ce9c
Francois Perrad <fperrad@gmail.com>
2015-01-16 22:09:53 +0100

    lsqlite3: bump to version 0.9.3
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lsqlite3/lsqlite3.mk
+++ package/lsqlite3/lsqlite3.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LSQLITE3_VERSION = 0.9.1-2
-LSQLITE3_SUBDIR = lsqlite3_fsl09t
+LSQLITE3_VERSION = 0.9.3-0
+LSQLITE3_SUBDIR = lsqlite3_fsl09w
 LSQLITE3_DEPENDENCIES = sqlite
 LSQLITE3_LICENSE = MIT
 
