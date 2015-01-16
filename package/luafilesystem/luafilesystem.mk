5487b2630716ea146ada738ac875340f7f3ade29
Francois Perrad <fperrad@gmail.com>
2015-01-16 22:09:55 +0100

    luafilesystem: bump to version 1.6.3
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/luafilesystem/luafilesystem.mk
+++ package/luafilesystem/luafilesystem.mk
@@ -4,7 +4,11 @@
 #
 ################################################################################
 
-LUAFILESYSTEM_VERSION = 1.6.2-2
+LUAFILESYSTEM_VERSION = 1.6.3-1
 LUAFILESYSTEM_LICENSE = MIT
+LUAFILESYSTEM_SUBDIR = luafilesystem
+ifneq ($(BR2_LARGEFILE),y)
+LUAFILESYSTEM_BUILD_OPTS += CFLAGS="$(LUAROCKS_CFLAGS) -DLFS_DO_NOT_USE_LARGE_FILE"
+endif
 
 $(eval $(luarocks-package))
