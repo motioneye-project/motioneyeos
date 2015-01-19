22cb51e1a2cdff07f220d7519d05934f6ce1bf2c
Maxim Mikityanskiy <maxtram95@gmail.com>
2015-01-19 18:14:16 +0200

    package/rng-tools: add systemd support for rngd
    Add rngd.service for systemd. Also patch rngd with patch from Fedora to
    ignore failure if no hwrng present:
    https://bugzilla.redhat.com/show_bug.cgi?id=892178
    
    [Thomas:
     - rename patch to the proper naming convention
     - add description and SoB to patch
     - install service file to /usr/lib/systemd and not /lib/systemd
     - use a relative symbolic link instead of an absolute one]
    
    Signed-off-by: Maxim Mikityanskiy <maxtram95@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/rng-tools/rng-tools.mk
+++ package/rng-tools/rng-tools.mk
@@ -21,4 +21,12 @@ else
 RNG_TOOLS_CONF_OPTS += --without-libgcrypt
 endif
 
+define RNG_TOOLS_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 package/rng-tools/rngd.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/rngd.service
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+	ln -fs ../../../../usr/lib/systemd/system/rngd.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/rngd.service
+endef
+
 $(eval $(autotools-package))
