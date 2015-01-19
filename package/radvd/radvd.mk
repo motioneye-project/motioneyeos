4ab5cbb4bda30d6eb7e4f73ee1197401aeb419e9
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-19 10:22:50 -0300

    radvd: add license info
    It's BSD-4c with a few extras, so BSD-4c-like.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/radvd/radvd.mk
+++ package/radvd/radvd.mk
@@ -11,6 +11,8 @@ RADVD_DEPENDENCIES = host-bison flex host-flex host-pkgconf
 RADVD_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
 # We're patching configure.ac/Makefile.am.
 RADVD_AUTORECONF = YES
+RADVD_LICENSE = BSD-4c-like
+RADVD_LICENSE_FILES = COPYRIGHT
 
 define RADVD_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 0755 package/radvd/S50radvd $(TARGET_DIR)/etc/init.d/S50radvd
