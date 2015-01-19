66d3cce7056c8996329c1aab6a71602bd8b37159
Romain Naour <romain.naour@openwide.fr>
2015-01-20 00:29:55 +0100

    package/gnu-efi: bump version to 3.0.1
    remove upstream patch gnu-efi-03-parallel-make.patch
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gnu-efi/gnu-efi.mk
+++ package/gnu-efi/gnu-efi.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-GNU_EFI_VERSION = 3.0u
-GNU_EFI_SOURCE = gnu-efi_$(GNU_EFI_VERSION).orig.tar.gz
+GNU_EFI_VERSION = 3.0.1
+GNU_EFI_SOURCE = gnu-efi-$(GNU_EFI_VERSION).tar.bz2
 GNU_EFI_SITE = http://downloads.sourceforge.net/project/gnu-efi
 GNU_EFI_INSTALL_STAGING = YES
 GNU_EFI_LICENSE = GPLv2+ (gnuefi), BSD (efilib)
