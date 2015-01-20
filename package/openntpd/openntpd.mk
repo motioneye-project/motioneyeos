35f37d762648bb921aafd43fb34476c76e4eec91
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-20 20:31:26 -0300

    openntpd: add systemd unit file
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/openntpd/openntpd.mk
+++ package/openntpd/openntpd.mk
@@ -9,6 +9,14 @@ OPENNTPD_SITE = http://ftp.openbsd.org/pub/OpenBSD/OpenNTPD
 OPENNTPD_LICENSE = MIT-like, BSD-2c, BSD-3c
 OPENNTPD_LICENSE_FILES = COPYING
 
+define OPENNTPD_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 0644 package/openntpd/ntpd.service \
+		$(TARGET_DIR)/etc/systemd/system/ntpd.service
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+	ln -fs ../ntpd.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/ntpd.service
+endef
+
 define OPENNTPD_INSTALL_INIT_SYSV
 	$(INSTALL) -m 0755 -D package/openntpd/S49ntp \
 		$(TARGET_DIR)/etc/init.d/S49ntp
