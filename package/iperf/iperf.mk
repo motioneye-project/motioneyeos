417302c75b6b1879d838c2eb7596e172baafe049
Richard Genoud <richard.genoud@gmail.com>
2015-01-20 12:23:36 +0100

    iperf: add license information
    Signed-off-by: Richard Genoud <richard.genoud@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/iperf/iperf.mk
+++ package/iperf/iperf.mk
@@ -6,6 +6,8 @@
 
 IPERF_VERSION = 2.0.5
 IPERF_SITE = http://downloads.sourceforge.net/project/iperf
+IPERF_LICENSE = MIT-like
+IPERF_LICENSE_FILES = COPYING
 
 IPERF_CONF_ENV = \
 	ac_cv_func_malloc_0_nonnull=yes \
