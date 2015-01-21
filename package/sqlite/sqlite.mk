69bed62e968ad054059c2c7e39ba1d4c5a47bdf5
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-21 08:15:53 -0300

    sqlite: bump to version 3.8.8.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SQLITE_VERSION = 3080800
+SQLITE_VERSION = 3080801
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_VERSION).tar.gz
 SQLITE_SITE = http://www.sqlite.org/2015
 SQLITE_LICENSE = Public domain
