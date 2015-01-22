626030fb1b9de47430254fce4dcf5ac5c526242f
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-22 14:17:45 -0300

    php: security bump to version 5.5.21
    Fixes:
    CVE-2015-0231 - Use After Free Vulnerability in PHP's unserialize()
    CVE-2014-9427 - Out of bounds read crashes php-cgi
    CVE-2015-0232 - Free called on unitialized pointer
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PHP_VERSION = 5.5.20
+PHP_VERSION = 5.5.21
 PHP_SITE = http://www.php.net/distributions
 PHP_SOURCE = php-$(PHP_VERSION).tar.xz
 PHP_INSTALL_STAGING = YES
