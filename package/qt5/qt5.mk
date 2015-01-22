1ca2171a645a1ffe579b55501b410ef12570ffee
Fatih Aşıcı <fatih.asici@gmail.com>
2015-01-22 14:37:58 +0200

    qt5: update URLs to use Qt's new domain
    Signed-off-by: Fatih Aşıcı <fatih.asici@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/qt5/qt5.mk
+++ package/qt5/qt5.mk
@@ -1,6 +1,6 @@
 QT5_VERSION_MAJOR = 5.4
 QT5_VERSION = $(QT5_VERSION_MAJOR).0
-QT5_SITE = http://download.qt-project.org/official_releases/qt/$(QT5_VERSION_MAJOR)/$(QT5_VERSION)/submodules
+QT5_SITE = http://download.qt.io/official_releases/qt/$(QT5_VERSION_MAJOR)/$(QT5_VERSION)/submodules
 include $(sort $(wildcard package/qt5/*/*.mk))
 
 define QT5_LA_PRL_FILES_FIXUP
