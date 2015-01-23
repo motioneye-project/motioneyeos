78cb8c81fa81f2a4dd63bcc2621ac5436edf26f6
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-23 07:47:56 -0300

    patch: security bump to version 2.7.3
    Fixes CVE-2015-1196 - allows remote attackers to write to arbitrary
    files via a symlink attack in a patch file.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/patch/patch.mk
+++ package/patch/patch.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PATCH_VERSION = 2.7.1
+PATCH_VERSION = 2.7.3
 PATCH_SOURCE = patch-$(PATCH_VERSION).tar.xz
 PATCH_SITE = $(BR2_GNU_MIRROR)/patch
 PATCH_LICENSE = GPLv3+
