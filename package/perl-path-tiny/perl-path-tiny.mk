f902be52293ec35a2ce168b24b702a662b3de550
Francois Perrad <fperrad@gmail.com>
2015-01-23 17:37:04 +0100

    perl-path-tiny: bump to version 0.061
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/perl-path-tiny/perl-path-tiny.mk
+++ package/perl-path-tiny/perl-path-tiny.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_PATH_TINY_VERSION = 0.059
+PERL_PATH_TINY_VERSION = 0.061
 PERL_PATH_TINY_SOURCE = Path-Tiny-$(PERL_PATH_TINY_VERSION).tar.gz
 PERL_PATH_TINY_SITE = $(BR2_CPAN_MIRROR)/authors/id/D/DA/DAGOLDEN
 PERL_PATH_TINY_DEPENDENCIES = perl
