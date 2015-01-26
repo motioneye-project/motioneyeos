7b6f4d49a99f6c8edb5286e52f2cc54f7094d5d2
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-01-25 21:14:47 -0300

    bison: add host-m4 as dependency
    Pull in host-m4 as dependency since it requires a new-enough m4 to build
    properly and some distributions don't update that often. Fixes:
    http://autobuild.buildroot.net/results/ee8/ee88f2c2a80dc19699d176ff2acde62fbc8a251f/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/bison/bison.mk
+++ package/bison/bison.mk
@@ -9,5 +9,6 @@ BISON_SOURCE = bison-$(BISON_VERSION).tar.xz
 BISON_SITE = $(BR2_GNU_MIRROR)/bison
 BISON_LICENSE = GPLv3+
 BISON_LICENSE_FILES = COPYING
+HOST_BISON_DEPENDENCIES = host-m4
 
 $(eval $(host-autotools-package))
