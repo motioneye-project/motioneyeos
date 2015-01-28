b8e13fbeb97826d0dd158115852a1445250372a4
erico.nunes <erico.nunes@datacom.ind.br>
2015-01-28 13:14:23 -0200

    python-jinja2: allow build as host-package
    Jinja2 is a general purpose templating language for python and can be
    required during build time for python scripts that generate code.
    
    While currently there are no in-tree Buildroot packages which depend on
    host-python-jinja2, I'm currently having to deal with a proprietary
    software build system that requires it.
    
    I have tested that it builds and works correctly as a host python
    package for a Buildroot host-python and someone else might require it,
    so I'm proposing its inclusion.
    
    python-jinja2 for target builds fine without python-markupsafe as a
    build dependency, but when building host-python-jinja2 without
    host-python-markupsafe installed, the python package manager tries to
    download and install it.
    To avoid that and install host-python-markupsafe properly through
    Buildroot, the host build dependency is needed. Host support for
    python-markupsafe was submitted in a previous patch from this patchset.
    
    Signed-off-by: Erico Nunes <erico.nunes@datacom.ind.br>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-jinja2/python-jinja2.mk
+++ package/python-jinja2/python-jinja2.mk
@@ -10,5 +10,8 @@ PYTHON_JINJA2_SITE = http://pypi.python.org/packages/source/J/Jinja2
 PYTHON_JINJA2_SETUP_TYPE = setuptools
 PYTHON_JINJA2_LICENSE = BSD-3c
 PYTHON_JINJA2_LICENSE_FILES = LICENSE
+# In host build, setup.py tries to download markupsafe if it is not installed
+HOST_PYTHON_JINJA2_DEPENDENCIES = host-python-markupsafe
 
 $(eval $(python-package))
+$(eval $(host-python-package))
