793486e687c5c56b0dfda99ad6ecd603058dba38
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2015-01-29 12:35:39 +0100

    sigrok-cli: new package
    [Peter: needs host-pkgconf, simplify pre configure hook]
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/sigrok-cli/sigrok-cli.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# sigrok-cli
+#
+################################################################################
+
+SIGROK_CLI_VERSION = 198487f611d8a7be4fa15017c22fa01a56551ca6
+# No https access on upstream git
+SIGROK_CLI_SITE = git://sigrok.org/sigrok-cli
+SIGROK_CLI_LICENSE = GPLv3+
+SIGROK_CLI_LICENSE_FILES = COPYING
+# Git checkout has no configure script
+SIGROK_CLI_AUTORECONF = YES
+SIGROK_CLI_DEPENDENCIES = host-pkgconf libsigrok
+SIGROK_CLI_CONF_OPTS = --with-libsigrokdecode=NO
+
+define SIGROK_CLI_ADD_MISSING
+	mkdir -p $(@D)/autostuff
+endef
+
+SIGROK_CLI_PRE_CONFIGURE_HOOKS += SIGROK_CLI_ADD_MISSING
+
+$(eval $(autotools-package))
