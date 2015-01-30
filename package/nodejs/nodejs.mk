3bd712bb8284b966055645c529e7a92be218985a
Jörg Krause <joerg.krause@embedded.rocks>
2015-01-30 01:53:47 +0100

    package/nodejs: bump to version 0.10.36
    Bump to new version and update hash.
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/nodejs/nodejs.mk
+++ package/nodejs/nodejs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NODEJS_VERSION = 0.10.35
+NODEJS_VERSION = 0.10.36
 NODEJS_SOURCE = node-v$(NODEJS_VERSION).tar.gz
 NODEJS_SITE = http://nodejs.org/dist/v$(NODEJS_VERSION)
 NODEJS_DEPENDENCIES = host-python host-nodejs zlib \
