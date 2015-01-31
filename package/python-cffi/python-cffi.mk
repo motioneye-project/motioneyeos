7010c18443f7fec968d0f8c7388dc447fb59635b
Yegor Yefremov <yegorslists@googlemail.com>
2015-01-31 19:59:35 +0100

    python-cffi: bump to 0.8.6
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-cffi/python-cffi.mk
+++ package/python-cffi/python-cffi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_CFFI_VERSION = 0.8.2
+PYTHON_CFFI_VERSION = 0.8.6
 PYTHON_CFFI_SOURCE = cffi-$(PYTHON_CFFI_VERSION).tar.gz
 PYTHON_CFFI_SITE = https://pypi.python.org/packages/source/c/cffi
 PYTHON_CFFI_SETUP_TYPE = setuptools
