242f6720931d4f86fb6f8fc9742a29a297f878c4
Ryan Coe <bluemrp9@gmail.com>
2015-02-01 16:58:08 -0700

    pps-tools: also pass TARGET_CONFIGURE_OPTS to make install
    Without these environment variables, pps-tools rebuilds for the host machine
    architecture and does not run on the target machine.
    
    Signed-off-by: Ryan Coe <bluemrp9@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/pps-tools/pps-tools.mk
+++ package/pps-tools/pps-tools.mk
@@ -16,12 +16,14 @@ endef
 
 define PPS_TOOLS_INSTALL_STAGING_CMDS
 	mkdir -p $(STAGING_DIR)/usr/include/sys $(STAGING_DIR)/usr/bin
-	$(TARGET_MAKE_ENV) $(MAKE) DESTDIR=$(STAGING_DIR) -C $(@D) install
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
+		DESTDIR=$(STAGING_DIR) -C $(@D) install
 endef
 
 define PPS_TOOLS_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/include/sys $(TARGET_DIR)/usr/bin
-	$(TARGET_MAKE_ENV) $(MAKE) DESTDIR=$(TARGET_DIR) -C $(@D) install
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
+		DESTDIR=$(TARGET_DIR) -C $(@D) install
 endef
 
 $(eval $(generic-package))
