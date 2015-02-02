0df309a3e805ec0f6ccd963ce0dbb0a2934acf44
Steve Kenton <skenton@ou.edu>
2015-02-02 22:35:06 +0000

    mjpegtools: new package
    The mjpegtools programs are a set of tools that can do recording of videos
    and playback, simple cut-and-paste editing and the MPEG compression of audio
    and video under Linux.
    
    [Peter: license is GPLv2+]
    Signed-off-by: Steve Kenton <skenton@ou.edu>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/mjpegtools/mjpegtools.mk
@@ -0,0 +1,13 @@
+###############################################################################
+#
+# mjpegtools
+#
+###############################################################################
+
+MJPEGTOOLS_VERSION = 2.1.0
+MJPEGTOOLS_SITE = http://sourceforge.net/projects/mjpeg/files/mjpegtools/$(MJPEGTOOLS_VERSION)
+MJPEGTOOLS_DEPENDENCIES = jpeg
+MJPEGTOOLS_LICENSE = GPLv2+
+MJPEGTOOLS_LICENSE_FILES = COPYING
+
+$(eval $(autotools-package))
