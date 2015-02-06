6240f33bec63546d419019be52105c408b0557dd
Baruch Siach <baruch@tkos.co.il>
2015-02-06 09:45:14 +0200

    sdparm: remove include of host headers
    Fixes:
    http://autobuild.buildroot.net/results/458/458b3da912b977335ac292fa7adddfb85961557a/
    http://autobuild.buildroot.net/results/ead/ead9c1d354e9beebdcb892ded2d63b8d3bc3eb51/
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sdparm/sdparm.mk
+++ package/sdparm/sdparm.mk
@@ -10,6 +10,9 @@ SDPARM_SITE = http://sg.danny.cz/sg/p
 SDPARM_LICENSE = BSD-3c
 SDPARM_LICENSE_FILES = COPYING
 
+# Patching src/Makefile.am
+SDPARM_AUTORECONF = YES
+
 ifeq ($(BR2_PACKAGE_SG3_UTILS),y)
 SDPARM_DEPENDENCIES += sg3_utils
 else
