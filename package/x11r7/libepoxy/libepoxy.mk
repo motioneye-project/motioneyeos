2ac0250cc6f1522f7b8de65b538e3882b11c0a6f
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-02-07 21:11:16 +0100

    package/x11r7/libepoxy: New package
    xdriver_xf86-video-ati needs glamor support from xserver_xorg-server,
    which in turn depends on libepoxy.
    
    [Thomas: add license information.]
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/x11r7/libepoxy/libepoxy.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# libepoxy
+#
+#################################################################################
+
+LIBEPOXY_VERSION = v1.2
+LIBEPOXY_SITE = $(call github,anholt,libepoxy,$(LIBEPOXY_VERSION))
+LIBEPOXY_INSTALL_STAGING = YES
+LIBEPOXY_AUTORECONF = YES
+LIBEPOXY_DEPENDENCIES = mesa3d
+LIBEPOXY_LICENSE = MIT
+LIBEPOXY_LICENSE_FILES = COPYING
+
+$(eval $(autotools-package))
