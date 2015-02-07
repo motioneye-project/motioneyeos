29803f9da8c2493beba7e9dfd44b4868eeb49e56
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-02-07 21:11:26 +0100

    package/x11r7/xapp_showfont: bump version to 1.0.5
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_showfont/xapp_showfont.mk
+++ package/x11r7/xapp_showfont/xapp_showfont.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_SHOWFONT_VERSION = 1.0.4
+XAPP_SHOWFONT_VERSION = 1.0.5
 XAPP_SHOWFONT_SOURCE = showfont-$(XAPP_SHOWFONT_VERSION).tar.bz2
 XAPP_SHOWFONT_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_SHOWFONT_LICENSE = MIT
