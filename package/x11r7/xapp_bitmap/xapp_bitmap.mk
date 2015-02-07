a879031a434d671bfda64af0fe3a34e1b7134e23
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-02-07 21:11:19 +0100

    package/x11r7/xapp_bitmap: bump version to 1.0.8
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_bitmap/xapp_bitmap.mk
+++ package/x11r7/xapp_bitmap/xapp_bitmap.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_BITMAP_VERSION = 1.0.7
+XAPP_BITMAP_VERSION = 1.0.8
 XAPP_BITMAP_SOURCE = bitmap-$(XAPP_BITMAP_VERSION).tar.bz2
 XAPP_BITMAP_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_BITMAP_LICENSE = MIT
