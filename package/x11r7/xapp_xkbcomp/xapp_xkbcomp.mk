0667a294e828dc5e8b85fff8a36ee58052baf655
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-02-07 21:11:29 +0100

    package/x11r7/xapp_xkbcomp: bump version to 1.3.0
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xkbcomp/xapp_xkbcomp.mk
+++ package/x11r7/xapp_xkbcomp/xapp_xkbcomp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XKBCOMP_VERSION = 1.2.4
+XAPP_XKBCOMP_VERSION = 1.3.0
 XAPP_XKBCOMP_SOURCE = xkbcomp-$(XAPP_XKBCOMP_VERSION).tar.bz2
 XAPP_XKBCOMP_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XKBCOMP_LICENSE = MIT
