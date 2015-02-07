6bc6c3aabe9d3c9592754e0c49a2d476473b5754
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-02-07 21:11:30 +0100

    package/x11r7/xdriver_xf86-input-evdev: bump version to 2.9.1
    Package now needs libevdev:
    http://cgit.freedesktop.org/xorg/driver/xf86-input-evdev/commit/?id=164c62a97591d4c9b649460e7442977ea82ac02b
    
    [Thomas: adapt to current master, update hash file.]
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xdriver_xf86-input-evdev/xdriver_xf86-input-evdev.mk
+++ package/x11r7/xdriver_xf86-input-evdev/xdriver_xf86-input-evdev.mk
@@ -4,13 +4,14 @@
 #
 ################################################################################
 
-XDRIVER_XF86_INPUT_EVDEV_VERSION = 2.8.4
+XDRIVER_XF86_INPUT_EVDEV_VERSION = 2.9.1
 XDRIVER_XF86_INPUT_EVDEV_SOURCE = xf86-input-evdev-$(XDRIVER_XF86_INPUT_EVDEV_VERSION).tar.bz2
 XDRIVER_XF86_INPUT_EVDEV_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_INPUT_EVDEV_LICENSE = MIT
 XDRIVER_XF86_INPUT_EVDEV_LICENSE_FILES = COPYING
 XDRIVER_XF86_INPUT_EVDEV_DEPENDENCIES = \
 	host-pkgconf \
+	libevdev \
 	xproto_inputproto \
 	xserver_xorg-server \
 	xproto_randrproto \
