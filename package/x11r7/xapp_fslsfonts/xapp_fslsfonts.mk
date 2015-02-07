e732d8ff8da592207b3a3a66fe7b85b5ecbe1825
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-02-07 21:11:20 +0100

    package/x11r7/xapp_fslsfonts: bump version to 1.0.5
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_fslsfonts/xapp_fslsfonts.mk
+++ package/x11r7/xapp_fslsfonts/xapp_fslsfonts.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_FSLSFONTS_VERSION = 1.0.4
+XAPP_FSLSFONTS_VERSION = 1.0.5
 XAPP_FSLSFONTS_SOURCE = fslsfonts-$(XAPP_FSLSFONTS_VERSION).tar.bz2
 XAPP_FSLSFONTS_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_FSLSFONTS_LICENSE = MIT
