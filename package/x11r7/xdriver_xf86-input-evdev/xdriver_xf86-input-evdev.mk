69cf5f5ff70873d700ad7332c37e0a5c8e0ed509
Peter Korsgaard <peter@korsgaard.com>
2015-02-12 21:50:49 +0100

    xdriver_xf86-input-evdev: needs host-pkgconf
    The configure script uses PKG_CHECK_MODULES, so explictly add it to
    dependencies instead of relying on the fact that the xserver pulls it in.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xdriver_xf86-input-evdev/xdriver_xf86-input-evdev.mk
+++ package/x11r7/xdriver_xf86-input-evdev/xdriver_xf86-input-evdev.mk
@@ -9,6 +9,12 @@ XDRIVER_XF86_INPUT_EVDEV_SOURCE = xf86-input-evdev-$(XDRIVER_XF86_INPUT_EVDEV_VE
 XDRIVER_XF86_INPUT_EVDEV_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_INPUT_EVDEV_LICENSE = MIT
 XDRIVER_XF86_INPUT_EVDEV_LICENSE_FILES = COPYING
-XDRIVER_XF86_INPUT_EVDEV_DEPENDENCIES = xproto_inputproto xserver_xorg-server xproto_randrproto xproto_xproto udev
+XDRIVER_XF86_INPUT_EVDEV_DEPENDENCIES = \
+	host-pkgconf \
+	xproto_inputproto \
+	xserver_xorg-server \
+	xproto_randrproto \
+	xproto_xproto \
+	udev
 
 $(eval $(autotools-package))
