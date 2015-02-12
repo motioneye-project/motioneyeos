f1927896c2825fcbd45218a9796f0d9a3a3235fa
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2015-02-12 13:48:37 +0100

    imagemagick: bump to version 6.9.0-5
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/imagemagick/imagemagick.mk
+++ package/imagemagick/imagemagick.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IMAGEMAGICK_VERSION = 6.9.0-4
+IMAGEMAGICK_VERSION = 6.9.0-5
 IMAGEMAGICK_SOURCE = ImageMagick-$(IMAGEMAGICK_VERSION).tar.xz
 # The official ImageMagick site only keeps the latest versions
 # available, which is annoying. Use an alternate site that keeps all
