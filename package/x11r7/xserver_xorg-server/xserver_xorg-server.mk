d01389c9d9cacd860c569f229ff9ec47f9dffb2c
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-02-15 14:19:32 +0100

    package/x11r7/xserver_xorg-server: Relax dependency on libdrm
    xserver_xorg-server needs libdrm only when DRI is enabled, see configure.ac,
    line 1280. But since version 1.16.99.901 configure searches for optional libdrm
    support in configure.ac, line 2041,
    
    http://cgit.freedesktop.org/xorg/xserver/commit/configure.ac?id=2feda3b6b58f46cef91ea41312aac9021a703777
    
    as well due to libdrm support being enabled by default, but the macro
    PKG_CHECK_EXISTS used in line 2041 does not add libdrm-specific CFLAGS to
    Makefile causing the build to fail:
    
    In file included from dumb_bo.c:36:0:
    /home/br/br7/output/host/usr/i586-buildroot-linux-uclibc/sysroot/usr/include/xf86drm.h:40:17: fatal error: drm.h: No such file or directory
     #include <drm.h>
    
    This patch enables libdrm support only when dri support is enabled, which in
    turn depends on mesa3d. Since mesa3d selects libdrm already remove that line
    from Config.in as well.
    
    Tested using this defconfig
    
    BR2_TOOLCHAIN_BUILDROOT_LARGEFILE=y
    BR2_TOOLCHAIN_BUILDROOT_WCHAR=y
    BR2_TOOLCHAIN_BUILDROOT_CXX=y
    BR2_PACKAGE_XORG7=y
    BR2_PACKAGE_XSERVER_XORG_SERVER=y
    BR2_PACKAGE_XSERVER_XORG_SERVER_MODULAR=y
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xserver_xorg-server/xserver_xorg-server.mk
+++ package/x11r7/xserver_xorg-server/xserver_xorg-server.mk
@@ -66,7 +66,7 @@ XSERVER_XORG_SERVER_CONF_OPTS = \
 
 ifeq ($(BR2_PACKAGE_XSERVER_XORG_SERVER_MODULAR),y)
 XSERVER_XORG_SERVER_CONF_OPTS += --enable-xorg
-XSERVER_XORG_SERVER_DEPENDENCIES += libpciaccess libdrm
+XSERVER_XORG_SERVER_DEPENDENCIES += libpciaccess
 else
 XSERVER_XORG_SERVER_CONF_OPTS += --disable-xorg
 endif
@@ -106,10 +106,10 @@ XSERVER_XORG_SERVER_CONF_OPTS += --disable-kdrive --disable-xfbdev
 endif
 
 ifeq ($(BR2_PACKAGE_MESA3D_DRI_DRIVER),y)
-XSERVER_XORG_SERVER_CONF_OPTS += --enable-dri --enable-glx
-XSERVER_XORG_SERVER_DEPENDENCIES += mesa3d xproto_xf86driproto
+XSERVER_XORG_SERVER_CONF_OPTS += --enable-dri --enable-libdrm --enable-glx
+XSERVER_XORG_SERVER_DEPENDENCIES += libdrm mesa3d xproto_xf86driproto
 else
-XSERVER_XORG_SERVER_CONF_OPTS += --disable-dri --disable-glx
+XSERVER_XORG_SERVER_CONF_OPTS += --disable-dri --disable-libdrm --disable-glx
 endif
 
 ifeq ($(BR2_PACKAGE_XSERVER_XORG_SERVER_AIGLX),y)
