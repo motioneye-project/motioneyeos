fec618d1172388b6b67288dd090184461725a8d2
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-02-17 10:43:54 -0300

    xterm: remove superfluous INSTALL_TARGET_OPTS
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/xterm/xterm.mk
+++ package/xterm/xterm.mk
@@ -8,7 +8,6 @@ XTERM_VERSION = 306
 XTERM_SOURCE = xterm-$(XTERM_VERSION).tgz
 XTERM_SITE = ftp://invisible-island.net/xterm
 XTERM_DEPENDENCIES = ncurses xlib_libXaw
-XTERM_INSTALL_TARGET_OPTS = DESTDIR=$(TARGET_DIR) install
 XTERM_LICENSE = MIT
 XTERM_LICENSE_FILES = version.c
 XTERM_CONF_OPTS = --enable-256-color
