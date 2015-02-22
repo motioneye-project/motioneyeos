fdfbd27f3484923d9e1a3d6307a15e32b790068a
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2015-02-22 16:14:57 +0000

    evtest: disable documentation
    evtest uses asciidoc to generate documentation, but /usr/bin/asciidoc
    will try to use buildroot's host-python which doesn't have the required
    unicodedata. To avoid that, define ASCIIDOC as an empty string so the
    Makefile can't find the executable and skips the documentation
    generation.  This is the same solution used in the evemu package.
    Otherwise it will fail with this message:
    
    make[1]: Entering directory `/br/output/build/evtest-1.32'
    /usr/bin/make  all-am
    make[2]: Entering directory `/br/output/build/evtest-1.32'
      File "/usr/bin/asciidoc", line 95
        except KeyError, k: return None
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/evtest/evtest.mk
+++ package/evtest/evtest.mk
@@ -13,4 +13,9 @@ EVTEST_DEPENDENCIES = host-pkgconf
 # needed because source package contains no generated files
 EVTEST_AUTORECONF = YES
 
+# asciidoc used to generate manpages, which we don't need, and if it's
+# present on the build host, it ends getting called with our host-python
+# which doesn't have all the needed modules enabled, breaking the build
+EVTEST_CONF_ENV = ac_cv_path_ASCIIDOC=""
+
 $(eval $(autotools-package))
