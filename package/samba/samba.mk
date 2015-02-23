340f0a802d7f8451ea52e5ee69637c4784541b99
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-02-23 09:06:27 -0300

    samba: security bump to version 3.6.25
    Fixes:
    CVE-2014-0178 - Malformed FSCTL_SRV_ENUMERATE_SNAPSHOTS response.
    CVE-2015-0240 - Unexpected code execution in smbd.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/samba/samba.mk
+++ package/samba/samba.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SAMBA_VERSION = 3.6.24
+SAMBA_VERSION = 3.6.25
 SAMBA_SITE = http://ftp.samba.org/pub/samba/stable
 SAMBA_SUBDIR = source3
 SAMBA_INSTALL_STAGING = YES
