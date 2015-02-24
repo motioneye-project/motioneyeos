03ff39c1eba8eab13d1484a3db13c22cfa3691d8
Matthew Bastian <mbastian@zaphod.ds.nrc.ca>
2015-02-24 10:13:14 -0500

    rtai: bump to version 4.0.1
    Signed-off-by: Matthew Bastian <Matthew.Bastian@nrc-cnrc.gc.ca>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/rtai/rtai.mk
+++ package/rtai/rtai.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTAI_VERSION = 3.8.1
+RTAI_VERSION = 4.0.1
 RTAI_SOURCE = rtai-$(RTAI_VERSION).tar.bz2
 RTAI_SITE = https://www.rtai.org/userfiles/downloads/RTAI
 RTAI_INSTALL_STAGING = YES
@@ -22,6 +22,8 @@ RTAI_CONF_OPTS = \
 	--includedir=/usr/include/rtai \
 	--with-linux-dir=$(LINUX_DIR) 	\
 	--disable-leds		      	\
+	--enable-usi			\
+	--enable-align-priority		\
 	--disable-rtailab		\
 	--with-module-dir=/lib/modules/$(LINUX_VERSION_PROBED)/rtai
 
