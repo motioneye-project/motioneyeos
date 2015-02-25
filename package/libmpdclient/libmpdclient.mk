1a680da2834efbb15b706b7f69a7bf568e16596a
Peter Kümmel <syntheticpp@gmx.net>
2015-02-25 09:10:03 +0100

    package/libmpdclient: update to 2.10
    Removed patch is upstream.
    
    Signed-off-by: Peter Kümmel <syntheticpp@gmx.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libmpdclient/libmpdclient.mk
+++ package/libmpdclient/libmpdclient.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBMPDCLIENT_VERSION_MAJOR = 2
-LIBMPDCLIENT_VERSION = $(LIBMPDCLIENT_VERSION_MAJOR).9
+LIBMPDCLIENT_VERSION = $(LIBMPDCLIENT_VERSION_MAJOR).10
 LIBMPDCLIENT_SOURCE = libmpdclient-$(LIBMPDCLIENT_VERSION).tar.xz
 LIBMPDCLIENT_SITE = http://www.musicpd.org/download/libmpdclient/$(LIBMPDCLIENT_VERSION_MAJOR)
 LIBMPDCLIENT_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
