fb56d371639746f0d90f2e443e1b032883d57cef
Peter Korsgaard <peter@korsgaard.com>
2015-02-27 16:43:46 +0100

    libupnpp: fix libupnp detection with static linking
    Fixes http://autobuild.buildroot.net/results/566/566e1510f94670d4ff642c7bfa9918c1d9ec583f/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libupnpp/libupnpp.mk
+++ package/libupnpp/libupnpp.mk
@@ -11,4 +11,10 @@ LIBUPNPP_LICENSE_FILES = COPYING
 LIBUPNPP_INSTALL_STAGING = YES
 LIBUPNPP_DEPENDENCIES = expat libcurl libupnp
 
+# configure script fails to link against the dependencies of libupnp
+# causing upnp detection to fail when statically linking
+ifeq ($(BR2_STATIC_LIBS),y)
+LIBUPNPP_CONF_ENV += LIBS='-lthreadutil -lixml -pthread'
+endif
+
 $(eval $(autotools-package))
