160d24fd704f18acbff12ccc27e745e9ad7725cd
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-03-02 19:40:47 -0300

    alsa-utils: bump to version 1.0.29
    Also add hash file.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/alsa-utils/alsa-utils.mk
+++ package/alsa-utils/alsa-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ALSA_UTILS_VERSION = 1.0.28
+ALSA_UTILS_VERSION = 1.0.29
 ALSA_UTILS_SOURCE = alsa-utils-$(ALSA_UTILS_VERSION).tar.bz2
 ALSA_UTILS_SITE = ftp://ftp.alsa-project.org/pub/utils
 ALSA_UTILS_LICENSE = GPLv2
