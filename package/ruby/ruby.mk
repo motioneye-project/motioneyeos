ada937a94b7e43bc4d46973946e24590064a7923
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-03-03 11:18:54 -0300

    ruby: bump to version 2.2.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ruby/ruby.mk
+++ package/ruby/ruby.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-RUBY_VERSION_MAJOR = 2.1
-RUBY_VERSION = $(RUBY_VERSION_MAJOR).5
-RUBY_VERSION_EXT = 2.1.0
+RUBY_VERSION_MAJOR = 2.2
+RUBY_VERSION = $(RUBY_VERSION_MAJOR).1
+RUBY_VERSION_EXT = 2.2.0
 RUBY_SITE = http://cache.ruby-lang.org/pub/ruby/$(RUBY_VERSION_MAJOR)
+RUBY_SOURCE = ruby-$(RUBY_VERSION).tar.xz
 RUBY_DEPENDENCIES = host-pkgconf host-ruby
 HOST_RUBY_DEPENDENCIES = host-pkgconf
 RUBY_MAKE_ENV = $(TARGET_MAKE_ENV)
