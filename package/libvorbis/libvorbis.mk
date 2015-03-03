9142661f402a22b5c14672e8ca8c254ba8a051f0
Peter Korsgaard <peter@korsgaard.com>
2015-03-03 08:51:40 +0100

    libvorbis: bump version
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libvorbis/libvorbis.mk
+++ package/libvorbis/libvorbis.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBVORBIS_VERSION = 1.3.4
+LIBVORBIS_VERSION = 1.3.5
 LIBVORBIS_SOURCE = libvorbis-$(LIBVORBIS_VERSION).tar.xz
 LIBVORBIS_SITE = http://downloads.xiph.org/releases/vorbis
 LIBVORBIS_INSTALL_STAGING = YES
