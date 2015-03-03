dd9e3caaa06777cc83db6cd72cef5afb1d1eac26
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2015-03-03 09:41:14 +0100

    ncmpc: change tarball compression to xz
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ncmpc/ncmpc.mk
+++ package/ncmpc/ncmpc.mk
@@ -6,7 +6,7 @@
 
 NCMPC_VERSION_MAJOR = 0
 NCMPC_VERSION = $(NCMPC_VERSION_MAJOR).24
-NCMPC_SOURCE = ncmpc-$(NCMPC_VERSION).tar.gz
+NCMPC_SOURCE = ncmpc-$(NCMPC_VERSION).tar.xz
 NCMPC_SITE = http://www.musicpd.org/download/ncmpc/$(NCMPC_VERSION_MAJOR)
 NCMPC_DEPENDENCIES = host-pkgconf libglib2 libmpdclient ncurses
 NCMPC_LICENSE = GPLv2+
