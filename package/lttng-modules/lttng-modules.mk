9728dfabf0f81e469cc14a2e7d0b1eb64b34d134
Philippe Proulx <eeppeliteloop@gmail.com>
2015-03-06 12:35:54 -0500

    lttng-modules: bump to version 2.6.0
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lttng-modules/lttng-modules.mk
+++ package/lttng-modules/lttng-modules.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LTTNG_MODULES_VERSION = 2.5.1
+LTTNG_MODULES_VERSION = 2.6.0
 LTTNG_MODULES_SITE = http://lttng.org/files/lttng-modules
 LTTNG_MODULES_SOURCE = lttng-modules-$(LTTNG_MODULES_VERSION).tar.bz2
 LTTNG_MODULES_LICENSE = LGPLv2.1/GPLv2 for kernel modules; MIT for lib/bitfield.h and lib/prio_heap/*
