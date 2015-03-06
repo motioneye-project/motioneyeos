38a6552635e98cca09b75abc79c9f0e71cfdea20
Philippe Proulx <eeppeliteloop@gmail.com>
2015-03-06 12:35:51 -0500

    liburcu: bump to version 0.8.6
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/liburcu/liburcu.mk
+++ package/liburcu/liburcu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBURCU_VERSION = 0.8.4
+LIBURCU_VERSION = 0.8.6
 LIBURCU_SITE = http://lttng.org/files/urcu
 LIBURCU_SOURCE = userspace-rcu-$(LIBURCU_VERSION).tar.bz2
 LIBURCU_LICENSE = LGPLv2.1+ for the library; MIT-like license for few source files listed in LICENSE; GPLv2+ for test; GPLv3 for few *.m4 files
