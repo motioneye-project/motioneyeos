3890d1d10ef5c3ecd87778d77cb8849a77112ebe
Mike Williams <mike@mikebwilliams.com>
2015-03-06 12:20:22 -0500

    libqmi: bump to 1.12.6
    ModemManager 1.4.4 requires libqmi >= 1.12.4.
    
    Signed-off-by: Mike Williams <mike@mikebwilliams.com>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libqmi/libqmi.mk
+++ package/libqmi/libqmi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBQMI_VERSION = 1.12.2
+LIBQMI_VERSION = 1.12.6
 LIBQMI_SITE = http://www.freedesktop.org/software/libqmi
 LIBQMI_SOURCE = libqmi-$(LIBQMI_VERSION).tar.xz
 LIBQMI_LICENSE = LGPLv2+ (library), GPLv2+ (programs)
