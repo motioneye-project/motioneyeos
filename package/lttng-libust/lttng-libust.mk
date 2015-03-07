d8b4ba71d11a1e8c6898ac8ad46bf0a42e0e445d
Philippe Proulx <eeppeliteloop@gmail.com>
2015-03-06 19:48:13 -0500

    lttng-libust: bump to version 2.6.0
    [Thomas: revert the change of <pkg>_LICENSE_FILES, which was
    incorrect.]
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lttng-libust/lttng-libust.mk
+++ package/lttng-libust/lttng-libust.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LTTNG_LIBUST_SITE = http://lttng.org/files/lttng-ust
-LTTNG_LIBUST_VERSION = 2.5.1
+LTTNG_LIBUST_VERSION = 2.6.0
 LTTNG_LIBUST_SOURCE = lttng-ust-$(LTTNG_LIBUST_VERSION).tar.bz2
 LTTNG_LIBUST_LICENSE = LGPLv2.1; MIT for system headers; GPLv2 for liblttng-ust-ctl/ustctl.c (used by lttng-sessiond)
 LTTNG_LIBUST_LICENSE_FILES = COPYING
