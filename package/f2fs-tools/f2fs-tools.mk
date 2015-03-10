ae38de2d0ef4024f49af62bcd4746faae3e1ec68
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-03-10 15:56:34 -0300

    f2fs-tools: bump to version 1.4.1
    And drop the old patches, they're upstream.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/f2fs-tools/f2fs-tools.mk
+++ package/f2fs-tools/f2fs-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-F2FS_TOOLS_VERSION = baac4b4e6f41ceb02511da49dd3707674f3fea21
+F2FS_TOOLS_VERSION = fd32210229d4ad7d1c916c74a0dfc150aae4c89e
 F2FS_TOOLS_SITE = http://git.kernel.org/pub/scm/linux/kernel/git/jaegeuk/f2fs-tools.git
 F2FS_TOOLS_SITE_METHOD = git
 F2FS_TOOLS_CONF_ENV = ac_cv_file__git=no
