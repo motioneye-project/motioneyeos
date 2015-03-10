34cefa79d04c2b588d7f3e2ee98fd7f5ac7dcd99
Alexandre Belloni <alexandre.belloni@free-electrons.com>
2015-03-10 19:21:20 +0100

    lpc3250loader: switch to github
    Gitorious is closing end of may. Switch LPC3250Loader to the github
    repository.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lpc3250loader/lpc3250loader.mk
+++ package/lpc3250loader/lpc3250loader.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-HOST_LPC3250LOADER_SITE = git://gitorious.org/lpc3250loader/lpc3250loader.git
 HOST_LPC3250LOADER_VERSION = 1.0
+HOST_LPC3250LOADER_SITE = $(call github,alexandrebelloni,lpc3250loader,$(HOST_LPC3250LOADER_VERSION))
 LPC3250LOADER_LICENSE = GPLv2+
 LPC3250LOADER_LICENSE_FILES = LPC3250loader.py
 
