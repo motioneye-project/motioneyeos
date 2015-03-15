6866c35838ed29ccdd8442c2161580c7a9eaffd4
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-03-15 12:19:18 +0100

    package/x11r7/xfont_font-util: bump version to 1.3.1
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xfont_font-util/xfont_font-util.mk
+++ package/x11r7/xfont_font-util/xfont_font-util.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XFONT_FONT_UTIL_VERSION = 1.3.0
+XFONT_FONT_UTIL_VERSION = 1.3.1
 XFONT_FONT_UTIL_SOURCE = font-util-$(XFONT_FONT_UTIL_VERSION).tar.bz2
 XFONT_FONT_UTIL_SITE = http://xorg.freedesktop.org/releases/individual/font
 XFONT_FONT_UTIL_LICENSE = MIT/BSD-2c
