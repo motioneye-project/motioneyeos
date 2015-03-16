20b741da4cf90a321ce51fa0c48c07d7b5ae574e
Yann E. MORIN <yann.morin.1998@free.fr>
2015-03-16 17:31:22 +0100

    package/fftw: add x86 optimisations
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
    Cc: guillaume william brs <guillaume.bressaix@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/fftw/fftw.mk
+++ package/fftw/fftw.mk
@@ -19,6 +19,10 @@ ifeq ($(BR2_PACKAGE_FFTW_FAST),y)
 FFTW_CFLAGS += -Ofast
 endif
 
+# x86 optimisations
+FFTW_CONF_OPTS += $(if $(BR2_PACKAGE_FFTW_USE_SSE),--enable,--disable)-sse
+FFTW_CONF_OPTS += $(if $(BR2_PACKAGE_FFTW_USE_SSE2),--enable,--disable)-sse2
+
 FFTW_CONF_OPTS += CFLAGS="$(FFTW_CFLAGS)"
 
 $(eval $(autotools-package))
