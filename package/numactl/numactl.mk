4237a1f786def8b03ffd054aa0f151bf73a6b4cb
Steven Noonan <steven@uplinklabs.net>
2015-03-19 00:49:56 -0700

    package/numactl: upgrade to 2.0.10
    The package now uses autotools, although it's missing a pregenerated configure
    script.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/numactl/numactl.mk
+++ package/numactl/numactl.mk
@@ -4,17 +4,12 @@
 #
 ################################################################################
 
-NUMACTL_VERSION = 2.0.9
+NUMACTL_VERSION = 2.0.10
 NUMACTL_SITE = ftp://oss.sgi.com/www/projects/libnuma/download
 NUMACTL_LICENSE = LGPLv2.1 (libnuma), GPLv2 (programs)
 NUMACTL_LICENSE_FILES = README
 
-define NUMACTL_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
-endef
+# numactl tarball doesn't contain a pregenerated configure script.
+NUMACTL_AUTORECONF = YES
 
-define NUMACTL_INSTALL_TARGET_CMDS
-	$(MAKE) prefix=$(TARGET_DIR) libdir=$(TARGET_DIR)/lib -C $(@D) install
-endef
-
-$(eval $(generic-package))
+$(eval $(autotools-package))
