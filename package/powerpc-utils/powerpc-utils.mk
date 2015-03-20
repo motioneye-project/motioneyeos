0a12a5a1b94465fd0aef6185e81715880be26ab0
Baruch Siach <baruch@tkos.co.il>
2015-03-20 07:55:47 +0200

    powerpc-utils: security bump to 1.2.24
    Fixes CVE-2014-4040: A local attacker could obtain sensitive information from
    the generated archive such as plain text passwords.
    
    Yes, version 1.2.24 seems to be newer than 1.4, which is equivalent to 1.2.20.
    
    Also, switch from git clone to tarball download , and add a .hash file.
    
    The configure script seems to misdetect stack smashing protection support in
    the toolchain. gcc accepts -fstack_protector_all, but the linker complains:
    "ld: cannot find -lssp".
    
    Cc: Jeremy Kerr <jk@ozlabs.org>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/powerpc-utils/powerpc-utils.mk
+++ package/powerpc-utils/powerpc-utils.mk
@@ -4,13 +4,14 @@
 #
 ################################################################################
 
-POWERPC_UTILS_VERSION = v1.4
-POWERPC_UTILS_SITE = git://git.code.sf.net/p/powerpc-utils/powerpc-utils
-POWERPC_UTILS_AUTORECONF = YES
+POWERPC_UTILS_VERSION = 1.2.24
+POWERPC_UTILS_SITE = http://downloads.sourceforge.net/project/powerpc-utils/powerpc-utils
 POWERPC_UTILS_DEPENDENCIES = zlib
 POWERPC_UTILS_LICENSE = Common Public License Version 1.0
 POWERPC_UTILS_LICENSE_FILES = COPYRIGHT
 
 POWERPC_UTILS_CONF_OPTS = --without-librtas
+POWERPC_UTILS_CONF_ENV = \
+	ax_cv_check_cflags___fstack_protector_all=$(if $(BR2_TOOLCHAIN_HAS_SSP),yes,no)
 
 $(eval $(autotools-package))
