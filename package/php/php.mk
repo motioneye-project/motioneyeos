2c88be8ef755c8714ad05821c66555f6544f4794
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-03-20 09:49:51 -0300

    php: security bump to version 5.5.23
    Fixes:
    CVE-2015-0231 - Use After Free Vulnerability in unserialize()
    CVE-2015-2305 - heap overflow vulnerability in regcomp.c
    CVE-2015-2331 - ZIP Integer Overflow leads to writing past heap boundary
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PHP_VERSION = 5.5.22
+PHP_VERSION = 5.5.23
 PHP_SITE = http://www.php.net/distributions
 PHP_SOURCE = php-$(PHP_VERSION).tar.xz
 PHP_INSTALL_STAGING = YES
