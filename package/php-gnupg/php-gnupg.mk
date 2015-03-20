6259a985a28a74f4c36dfebbca5fad8c3595e56d
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-03-20 09:50:22 -0300

    php-gnupg: bump to version 1.3.6
    Also add hash file.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/php-gnupg/php-gnupg.mk
+++ package/php-gnupg/php-gnupg.mk
@@ -4,10 +4,9 @@
 #
 ################################################################################
 
-PHP_GNUPG_VERSION = 1.3.3
+PHP_GNUPG_VERSION = 1.3.6
 PHP_GNUPG_SOURCE = gnupg-$(PHP_GNUPG_VERSION).tgz
-# pecl.php.net returns html with db connect failed
-PHP_GNUPG_SITE = http://sources.buildroot.net
+PHP_GNUPG_SITE = http://pecl.php.net/get
 # phpize does the autoconf magic
 PHP_GNUPG_DEPENDENCIES = php libgpgme host-autoconf host-pkgconf
 PHP_GNUPG_CONF_OPTS = --with-php-config=$(STAGING_DIR)/usr/bin/php-config \
