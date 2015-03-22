02ab0bdc5815a3910760d9c95007e55b1c62f8fd
Olivier Schonken <olivier.schonken@gmail.com>
2015-03-22 23:12:50 +0200

    package/qpdf: New package qpdf 5.1.2
    [Thomas:
      - adjust length of comment header in .mk file.
      - fix comment in Config.in to match what's specific in the Buildroot
        manual.
      - fix <pkg>_LICENSE to 'Artistic-2.0'.]
    
    Signed-off-by: Olivier Schonken <olivier.schonken@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/qpdf/qpdf.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# qpdf
+#
+################################################################################
+
+QPDF_VERSION = 5.1.2
+QPDF_SITE = http://downloads.sourceforge.net/project/qpdf/qpdf/$(QPDF_VERSION)
+QPDF_INSTALL_STAGING = YES
+QPDF_LICENSE = Artistic-2.0
+QPDF_LICENSE_FILES = Artistic-2.0
+QPDF_DEPENDENCIES = pcre zlib
+
+QPDF_CONF_OPTS = --without-random
+
+$(eval $(autotools-package))
