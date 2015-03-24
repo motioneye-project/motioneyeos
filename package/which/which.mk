00516418a4b1e25941001dd42f37c0f0ce1d9d9d
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-03-24 09:31:29 -0300

    which: bump to version 2.21
    Add hash file and switch to gnu mirror.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/which/which.mk
+++ package/which/which.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-WHICH_VERSION = 2.20
-WHICH_SITE = http://carlo17.home.xs4all.nl/which
+WHICH_VERSION = 2.21
+WHICH_SITE = $(BR2_GNU_MIRROR)/which
 WHICH_LICENSE = GPLv3+
 WHICH_LICENSE_FILES = COPYING
 
