e759a07939cbd4204212d787520b60b66c6e8011
Benoît Thébaudeau <benoit.thebaudeau.dev@gmail.com>
2015-03-30 02:32:36 +0200

    logrotate: bump version to 3.8.9
    This version comes with a new build system based on autoconf/automake,
    so we switch from generic-package to autotools-package. However, since
    the tarball does not contain a pre-generated configure script, we have
    to set LOGROTATE_AUTORECONF = YES.
    
    [Thomas:
      - add comment explaining why AUTORECONF = YES is needed
      - expand commit log
      - use --without/--with instead of --with=yes/--with=no
      - fix indentation in the ACL condition]
    
    Signed-off-by: Benoît Thébaudeau <benoit.thebaudeau.dev@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/logrotate/logrotate.mk
+++ package/logrotate/logrotate.mk
@@ -4,23 +4,27 @@
 #
 ################################################################################
 
-LOGROTATE_VERSION = 3.8.7
+LOGROTATE_VERSION = 3.8.9
 LOGROTATE_SITE = https://www.fedorahosted.org/releases/l/o/logrotate
 LOGROTATE_LICENSE = GPLv2+
 LOGROTATE_LICENSE_FILES = COPYING
-
 LOGROTATE_DEPENDENCIES = popt host-pkgconf
+# tarball does not have a generated configure script
+LOGROTATE_AUTORECONF = YES
+LOGROTATE_CONF_ENV = LIBS="$(shell $(PKG_CONFIG_HOST_BINARY) --libs popt)"
+LOGROTATE_CONF_OPTS = --without-selinux
 
-define LOGROTATE_BUILD_CMDS
-	$(MAKE) CC="$(TARGET_CC) $(TARGET_CFLAGS)" LDFLAGS="$(LDFLAGS)" \
-		LOADLIBES="$(shell $(PKG_CONFIG_HOST_BINARY) --libs popt)" \
-		-C $(@D)
-endef
+ifeq ($(BR2_PACKAGE_ACL),y)
+LOGROTATE_DEPENDENCIES += acl
+LOGROTATE_CONF_OPTS += --with-acl
+else
+LOGROTATE_CONF_OPTS += --without-acl
+endif
 
-define LOGROTATE_INSTALL_TARGET_CMDS
-	$(MAKE) PREFIX=$(TARGET_DIR) -C $(@D) install
+define LOGROTATE_INSTALL_TARGET_CONF
 	$(INSTALL) -m 0644 package/logrotate/logrotate.conf $(TARGET_DIR)/etc/logrotate.conf
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/logrotate.d
 endef
+LOGROTATE_POST_INSTALL_TARGET_HOOKS += LOGROTATE_INSTALL_TARGET_CONF
 
-$(eval $(generic-package))
+$(eval $(autotools-package))
