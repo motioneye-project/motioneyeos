14cb63461ebb27a9628e58aebaea7474fdd27db3
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-03-31 08:41:52 -0300

    harfbuzz: bump to version 0.9.40
    Also add hash file.
    And do an autoreconf run since otherwise the libtool patch fails.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/harfbuzz/harfbuzz.mk
+++ package/harfbuzz/harfbuzz.mk
@@ -4,14 +4,15 @@
 #
 ################################################################################
 
-HARFBUZZ_VERSION = 0.9.22
+HARFBUZZ_VERSION = 0.9.40
 HARFBUZZ_SITE = http://www.freedesktop.org/software/harfbuzz/release
 HARFBUZZ_SOURCE = harfbuzz-$(HARFBUZZ_VERSION).tar.bz2
 HARFBUZZ_LICENSE = MIT, ISC (ucdn library)
 HARFBUZZ_LICENSE_FILES = COPYING src/hb-ucdn/COPYING
 HARFBUZZ_INSTALL_STAGING = YES
-
 HARFBUZZ_CONF_OPTS = --without-coretext --without-uniscribe --without-graphite2
+# beta libtool version
+HARFBUZZ_AUTORECONF = YES
 
 ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
 # forgets to link test programs with -pthread breaking static link
