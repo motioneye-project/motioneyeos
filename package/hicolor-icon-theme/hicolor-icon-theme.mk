704bab724164faefc33c61b54fe0e4c9d9ed269e
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-03-31 08:39:27 -0300

    hicolor-icon-theme: bump to version 0.15
    Also add hash file.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/hicolor-icon-theme/hicolor-icon-theme.mk
+++ package/hicolor-icon-theme/hicolor-icon-theme.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-HICOLOR_ICON_THEME_VERSION = 0.13
+HICOLOR_ICON_THEME_VERSION = 0.15
 HICOLOR_ICON_THEME_SITE = http://icon-theme.freedesktop.org/releases
+HICOLOR_ICON_THEME_SOURCE = hicolor-icon-theme-$(HICOLOR_ICON_THEME_VERSION).tar.xz
 HICOLOR_ICON_THEME_LICENSE = GPLv2
 HICOLOR_ICON_THEME_LICENSE_FILES = COPYING
 
