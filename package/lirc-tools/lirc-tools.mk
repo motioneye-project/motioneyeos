bcfa8e1ff31a5f74be2c780dc845fbb71b393f4d
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2015-04-05 17:24:50 +0200

    lirc-tools: add host-pkgconf dependency
    Otherwise, the configure script fails with "Required program
    pkg-config is missing".
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lirc-tools/lirc-tools.mk
+++ package/lirc-tools/lirc-tools.mk
@@ -9,6 +9,7 @@ LIRC_TOOLS_SOURCE = lirc-$(LIRC_TOOLS_VERSION).tar.bz2
 LIRC_TOOLS_SITE = http://downloads.sourceforge.net/project/lirc/LIRC/$(LIRC_TOOLS_VERSION)/
 LIRC_TOOLS_LICENSE = GPLv2+
 LIRC_TOOLS_LICENSE_FILES = COPYING
+LIRC_TOOLS_DEPENDENCIES = host-pkgconf
 
 LIRC_TOOLS_CONF_OPTS = \
 	--without-x \
