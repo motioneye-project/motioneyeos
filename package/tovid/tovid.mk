f6ee0fff287ad0efd8c246379796a4b8629751a4
Steve Kenton <skenton@ou.edu>
2015-04-08 18:15:58 -0500

    tovid: change from SVN to github
    New download information for tovid plus hash file
    
    Signed-off-by: Steve Kenton <skenton@ou.edu>
    Reviewed-by: Ryan Barnett <ryanbarnett3@gmail.com>
    Tested-by: Ryan Barnett <ryanbarnett3@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/tovid/tovid.mk
+++ package/tovid/tovid.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-TOVID_SITE = http://tovid.googlecode.com/svn/trunk/tovid
-TOVID_SITE_METHOD = svn
-TOVID_VERSION = 3534
+TOVID_VERSION = 0.35.0
+TOVID_SITE = https://github.com/tovid-suite/tovid/releases/download/v$(TOVID_VERSION)
 TOVID_LICENSE = GPLv2+
 TOVID_LICENSE_FILES = COPYING
 TOVID_SETUP_TYPE = distutils
