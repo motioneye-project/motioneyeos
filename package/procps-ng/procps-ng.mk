502565f90abf6fab4d84f22ed99b3d446cdd446d
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-04-10 15:26:49 -0300

    procps-ng: needs gettextize
    Fixes:
    http://autobuild.buildroot.org/results/904/90401cdb471f9e9582e2ca2ad07fb3db148420d7/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/procps-ng/procps-ng.mk
+++ package/procps-ng/procps-ng.mk
@@ -13,6 +13,7 @@ PROCPS_NG_INSTALL_STAGING = YES
 PROCPS_NG_DEPENDENCIES = ncurses host-pkgconf
 # For 0002-use-pkgconfig-for-ncursesw-cflags.patch
 PROCPS_NG_AUTORECONF = YES
+PROCPS_NG_GETTEXTIZE = YES
 
 # If both procps-ng and busybox are selected, make certain procps-ng
 # wins the fight over who gets to have their utils actually installed.
