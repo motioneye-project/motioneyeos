df88859646c0a34a1e99ad79a115217393073efb
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 14:40:28 +0200

    package/x11r7/xapp_xditview: bump version to 1.0.4, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xditview/xapp_xditview.mk
+++ package/x11r7/xapp_xditview/xapp_xditview.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XDITVIEW_VERSION = 1.0.3
+XAPP_XDITVIEW_VERSION = 1.0.4
 XAPP_XDITVIEW_SOURCE = xditview-$(XAPP_XDITVIEW_VERSION).tar.bz2
 XAPP_XDITVIEW_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XDITVIEW_LICENSE = MIT
