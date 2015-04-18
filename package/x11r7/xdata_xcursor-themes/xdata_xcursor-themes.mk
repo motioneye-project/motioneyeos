007ac7efecfcaf10d1acf6fc5e2be5c9bbe2bfac
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 21:53:48 +0200

    package/x11r7/xdata_xcursor-themes: bump version to 1.0.4, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xdata_xcursor-themes/xdata_xcursor-themes.mk
+++ package/x11r7/xdata_xcursor-themes/xdata_xcursor-themes.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDATA_XCURSOR_THEMES_VERSION = 1.0.3
+XDATA_XCURSOR_THEMES_VERSION = 1.0.4
 XDATA_XCURSOR_THEMES_SOURCE = xcursor-themes-$(XDATA_XCURSOR_THEMES_VERSION).tar.bz2
 XDATA_XCURSOR_THEMES_SITE = http://xorg.freedesktop.org/releases/individual/data
 XDATA_XCURSOR_THEMES_LICENSE = MIT
