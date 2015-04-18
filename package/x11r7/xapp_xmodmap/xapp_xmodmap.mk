0b9d9f958d6d208dfa4ff9c05116ff61a28dfe9e
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 14:40:39 +0200

    package/x11r7/xapp_xmodmap: bump version to 1.0.9, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xmodmap/xapp_xmodmap.mk
+++ package/x11r7/xapp_xmodmap/xapp_xmodmap.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XMODMAP_VERSION = 1.0.8
+XAPP_XMODMAP_VERSION = 1.0.9
 XAPP_XMODMAP_SOURCE = xmodmap-$(XAPP_XMODMAP_VERSION).tar.bz2
 XAPP_XMODMAP_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XMODMAP_LICENSE = MIT
