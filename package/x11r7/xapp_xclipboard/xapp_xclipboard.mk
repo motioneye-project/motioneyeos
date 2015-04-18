075e936e60a201faa2ecee72051e0c665fdbd975
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 16:29:29 +0200

    package/x11r7/xapp_xclipboard: bump version to 1.1.3, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xclipboard/xapp_xclipboard.mk
+++ package/x11r7/xapp_xclipboard/xapp_xclipboard.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XCLIPBOARD_VERSION = 1.1.2
+XAPP_XCLIPBOARD_VERSION = 1.1.3
 XAPP_XCLIPBOARD_SOURCE = xclipboard-$(XAPP_XCLIPBOARD_VERSION).tar.bz2
 XAPP_XCLIPBOARD_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XCLIPBOARD_LICENSE = MIT
