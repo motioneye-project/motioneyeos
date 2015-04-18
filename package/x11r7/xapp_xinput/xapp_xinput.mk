fbe9bf3c3904d839fa0622279a051c7e0f4005e6
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 16:29:34 +0200

    package/x11r7/xapp_xinput: bump version to 1.6.1, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xinput/xapp_xinput.mk
+++ package/x11r7/xapp_xinput/xapp_xinput.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XINPUT_VERSION = 1.6.0
+XAPP_XINPUT_VERSION = 1.6.1
 XAPP_XINPUT_SOURCE = xinput-$(XAPP_XINPUT_VERSION).tar.bz2
 XAPP_XINPUT_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XINPUT_LICENSE = MIT
