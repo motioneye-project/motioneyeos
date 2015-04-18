f484ceb59a815db3905bd166e9d66746b7383f97
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 14:40:31 +0200

    package/x11r7/xapp_xgc: bump version to 1.0.5, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xgc/xapp_xgc.mk
+++ package/x11r7/xapp_xgc/xapp_xgc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XGC_VERSION = 1.0.4
+XAPP_XGC_VERSION = 1.0.5
 XAPP_XGC_SOURCE = xgc-$(XAPP_XGC_VERSION).tar.bz2
 XAPP_XGC_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XGC_LICENSE = MIT
