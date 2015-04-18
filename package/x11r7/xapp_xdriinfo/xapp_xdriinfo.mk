f604f310c3120acb4bf343239fd3c707297cd3b5
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 14:40:27 +0200

    package/x11r7/xapp_xdriinfo: bump version to 1.0.5, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xdriinfo/xapp_xdriinfo.mk
+++ package/x11r7/xapp_xdriinfo/xapp_xdriinfo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XDRIINFO_VERSION = 1.0.4
+XAPP_XDRIINFO_VERSION = 1.0.5
 XAPP_XDRIINFO_SOURCE = xdriinfo-$(XAPP_XDRIINFO_VERSION).tar.bz2
 XAPP_XDRIINFO_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XDRIINFO_LICENSE = MIT
