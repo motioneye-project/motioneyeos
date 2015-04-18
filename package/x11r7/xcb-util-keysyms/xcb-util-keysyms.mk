804c507e2cd8a5a5f245e98c0d21f50b0e31d6bb
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 21:53:47 +0200

    package/x11r7/xcb-util-keysyms: bump version to 0.4.0, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xcb-util-keysyms/xcb-util-keysyms.mk
+++ package/x11r7/xcb-util-keysyms/xcb-util-keysyms.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XCB_UTIL_KEYSYMS_VERSION = 0.3.9
+XCB_UTIL_KEYSYMS_VERSION = 0.4.0
 XCB_UTIL_KEYSYMS_SOURCE = xcb-util-keysyms-$(XCB_UTIL_KEYSYMS_VERSION).tar.bz2
 XCB_UTIL_KEYSYMS_SITE = http://xcb.freedesktop.org/dist
 
