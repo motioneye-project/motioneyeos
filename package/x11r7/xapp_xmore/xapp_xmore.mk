b7fdb52826816a0e1d7c70751e6e869d0841641c
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 16:29:35 +0200

    package/x11r7/xapp_xmore: bump version to 1.0.2, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xmore/xapp_xmore.mk
+++ package/x11r7/xapp_xmore/xapp_xmore.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XMORE_VERSION = 1.0.1
+XAPP_XMORE_VERSION = 1.0.2
 XAPP_XMORE_SOURCE = xmore-$(XAPP_XMORE_VERSION).tar.bz2
 XAPP_XMORE_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XMORE_LICENSE = MIT
