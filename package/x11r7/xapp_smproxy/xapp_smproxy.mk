a390ba3fa87ada1d2e8be2ae36823483b56b720f
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 14:40:22 +0200

    package/x11r7/xapp_smproxy: bump version to 1.0.6, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_smproxy/xapp_smproxy.mk
+++ package/x11r7/xapp_smproxy/xapp_smproxy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_SMPROXY_VERSION = 1.0.5
+XAPP_SMPROXY_VERSION = 1.0.6
 XAPP_SMPROXY_SOURCE = smproxy-$(XAPP_SMPROXY_VERSION).tar.bz2
 XAPP_SMPROXY_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_SMPROXY_LICENSE = MIT
