c0d8c54fb3636b3652f0362911f5a31de9eb55b8
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 14:40:35 +0200

    package/x11r7/xapp_xlsatoms: bump version to 1.1.2, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xlsatoms/xapp_xlsatoms.mk
+++ package/x11r7/xapp_xlsatoms/xapp_xlsatoms.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XLSATOMS_VERSION = 1.1.1
+XAPP_XLSATOMS_VERSION = 1.1.2
 XAPP_XLSATOMS_SOURCE = xlsatoms-$(XAPP_XLSATOMS_VERSION).tar.bz2
 XAPP_XLSATOMS_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XLSATOMS_LICENSE = MIT
