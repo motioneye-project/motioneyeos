5456ed370579e4758122d0d1acbcfb29a2bb2754
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 14:40:34 +0200

    package/x11r7/xapp_xkbprint: bump version to 1.0.4, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xkbprint/xapp_xkbprint.mk
+++ package/x11r7/xapp_xkbprint/xapp_xkbprint.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XKBPRINT_VERSION = 1.0.1
+XAPP_XKBPRINT_VERSION = 1.0.4
 XAPP_XKBPRINT_SOURCE = xkbprint-$(XAPP_XKBPRINT_VERSION).tar.bz2
 XAPP_XKBPRINT_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XKBPRINT_LICENSE = MIT
