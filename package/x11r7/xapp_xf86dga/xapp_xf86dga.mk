0687ccac6c0264c75b1a3b02c68847f9f3e59284
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 16:29:32 +0200

    package/x11r7/xapp_xf86dga: bump version to 1.0.3, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xf86dga/xapp_xf86dga.mk
+++ package/x11r7/xapp_xf86dga/xapp_xf86dga.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XF86DGA_VERSION = 1.0.2
+XAPP_XF86DGA_VERSION = 1.0.3
 XAPP_XF86DGA_SOURCE = xf86dga-$(XAPP_XF86DGA_VERSION).tar.bz2
 XAPP_XF86DGA_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XF86DGA_LICENSE = MIT
