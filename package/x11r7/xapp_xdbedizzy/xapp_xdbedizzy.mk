c975b46409fb3d11703be675ad11b6b85107df4e
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 16:29:30 +0200

    package/x11r7/xapp_xdbedizzy: bump version to 1.1.0, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xdbedizzy/xapp_xdbedizzy.mk
+++ package/x11r7/xapp_xdbedizzy/xapp_xdbedizzy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XDBEDIZZY_VERSION = 1.0.2
+XAPP_XDBEDIZZY_VERSION = 1.1.0
 XAPP_XDBEDIZZY_SOURCE = xdbedizzy-$(XAPP_XDBEDIZZY_VERSION).tar.bz2
 XAPP_XDBEDIZZY_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XDBEDIZZY_LICENSE = MIT
