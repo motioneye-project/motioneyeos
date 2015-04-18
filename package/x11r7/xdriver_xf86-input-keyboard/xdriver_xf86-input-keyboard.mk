3f866eb24b807a2a9055fda7561e835452dca8ed
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 18:39:29 +0200

    package/x11r7/xdriver_xf86-input-keyboard: Fix compile error
    lnx_kbd.c: In function 'OpenKeyboard':
    lnx_kbd.c:194:8: error: implicit declaration of function 'getpgid' [-Werror=implicit-function-declaration]
            rc = tcsetpgrp(pInfo->fd, getpgid(0));
            ^
    lnx_kbd.c:194:8: warning: nested extern declaration of 'getpgid' [-Wnested-externs]
    cc1: some warnings being treated as errors
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xdriver_xf86-input-keyboard/xdriver_xf86-input-keyboard.mk
+++ package/x11r7/xdriver_xf86-input-keyboard/xdriver_xf86-input-keyboard.mk
@@ -10,5 +10,6 @@ XDRIVER_XF86_INPUT_KEYBOARD_SITE = http://xorg.freedesktop.org/releases/individu
 XDRIVER_XF86_INPUT_KEYBOARD_LICENSE = MIT
 XDRIVER_XF86_INPUT_KEYBOARD_LICENSE_FILES = COPYING
 XDRIVER_XF86_INPUT_KEYBOARD_DEPENDENCIES = xserver_xorg-server xproto_inputproto xproto_kbproto xproto_randrproto xproto_xproto
+XDRIVER_XF86_INPUT_KEYBOARD_CONF_OPTS = --disable-selective-werror
 
 $(eval $(autotools-package))
