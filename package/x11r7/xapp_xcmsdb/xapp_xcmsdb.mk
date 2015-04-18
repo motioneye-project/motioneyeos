7f55cfb4a2ac869eed6c49fa8d4f63c2881f142e
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 14:40:25 +0200

    package/x11r7/xapp_xcmsdb: bump version to 1.0.5, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xcmsdb/xapp_xcmsdb.mk
+++ package/x11r7/xapp_xcmsdb/xapp_xcmsdb.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XCMSDB_VERSION = 1.0.4
+XAPP_XCMSDB_VERSION = 1.0.5
 XAPP_XCMSDB_SOURCE = xcmsdb-$(XAPP_XCMSDB_VERSION).tar.bz2
 XAPP_XCMSDB_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XCMSDB_LICENSE = MIT
