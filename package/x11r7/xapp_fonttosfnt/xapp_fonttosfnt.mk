754fa759c6d8fcb8d2979b3e858a13c0c9ec56dd
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 16:29:41 +0200

    package/x11r7/xapp_fonttosfnt: bump version to 1.0.4, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_fonttosfnt/xapp_fonttosfnt.mk
+++ package/x11r7/xapp_fonttosfnt/xapp_fonttosfnt.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_FONTTOSFNT_VERSION = 1.0.3
+XAPP_FONTTOSFNT_VERSION = 1.0.4
 XAPP_FONTTOSFNT_SOURCE = fonttosfnt-$(XAPP_FONTTOSFNT_VERSION).tar.bz2
 XAPP_FONTTOSFNT_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_FONTTOSFNT_LICENSE = MIT
