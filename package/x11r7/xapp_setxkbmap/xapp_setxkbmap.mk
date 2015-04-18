7bdc16f64f0ec04adab04eabe62c40f13f462dd6
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 14:40:21 +0200

    package/x11r7/xapp_setxkbmap: bump version to 1.3.1, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_setxkbmap/xapp_setxkbmap.mk
+++ package/x11r7/xapp_setxkbmap/xapp_setxkbmap.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_SETXKBMAP_VERSION = 1.3.0
+XAPP_SETXKBMAP_VERSION = 1.3.1
 XAPP_SETXKBMAP_SOURCE = setxkbmap-$(XAPP_SETXKBMAP_VERSION).tar.bz2
 XAPP_SETXKBMAP_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_SETXKBMAP_LICENSE = MIT
