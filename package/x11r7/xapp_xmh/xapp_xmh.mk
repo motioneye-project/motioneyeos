681b444c4e20a7f8001aefc5e4dccdbbf44191ac
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 14:40:38 +0200

    package/x11r7/xapp_xmh: bump version to 1.0.3, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xmh/xapp_xmh.mk
+++ package/x11r7/xapp_xmh/xapp_xmh.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XMH_VERSION = 1.0.2
+XAPP_XMH_VERSION = 1.0.3
 XAPP_XMH_SOURCE = xmh-$(XAPP_XMH_VERSION).tar.bz2
 XAPP_XMH_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XMH_LICENSE = MIT
