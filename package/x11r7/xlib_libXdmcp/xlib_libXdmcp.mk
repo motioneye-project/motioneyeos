d68dde1724d898f2bebc418bce0614ccee777b7a
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 14:40:15 +0200

    package/x11r7/xlib_libXdmcp: bump to version 1.1.2
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xlib_libXdmcp/xlib_libXdmcp.mk
+++ package/x11r7/xlib_libXdmcp/xlib_libXdmcp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBXDMCP_VERSION = 1.1.1
+XLIB_LIBXDMCP_VERSION = 1.1.2
 XLIB_LIBXDMCP_SOURCE = libXdmcp-$(XLIB_LIBXDMCP_VERSION).tar.bz2
 XLIB_LIBXDMCP_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXDMCP_LICENSE = MIT
