9c288fb178fcae8c058efbf195500e7cc28e19cc
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 14:40:29 +0200

    package/x11r7/xapp_xev: bump version to 1.2.2, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xev/xapp_xev.mk
+++ package/x11r7/xapp_xev/xapp_xev.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XEV_VERSION = 1.2.1
+XAPP_XEV_VERSION = 1.2.2
 XAPP_XEV_SOURCE = xev-$(XAPP_XEV_VERSION).tar.bz2
 XAPP_XEV_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XEV_LICENSE = MIT
