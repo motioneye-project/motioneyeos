eec59e8cbc9568a4676541911123c6fd56167069
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 16:29:33 +0200

    package/x11r7/xapp_xinit: bump version to 1.3.4, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xinit/xapp_xinit.mk
+++ package/x11r7/xapp_xinit/xapp_xinit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XINIT_VERSION = 1.3.3
+XAPP_XINIT_VERSION = 1.3.4
 XAPP_XINIT_SOURCE = xinit-$(XAPP_XINIT_VERSION).tar.bz2
 XAPP_XINIT_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XINIT_DEPENDENCIES = xapp_xauth xlib_libX11
