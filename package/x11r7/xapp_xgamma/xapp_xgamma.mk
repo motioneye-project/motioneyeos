c03a590e9b5a1d3adda0138f4eddb17719520428
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 14:40:30 +0200

    package/x11r7/xapp_xgamma: bump version to 1.0.6, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xgamma/xapp_xgamma.mk
+++ package/x11r7/xapp_xgamma/xapp_xgamma.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XGAMMA_VERSION = 1.0.5
+XAPP_XGAMMA_VERSION = 1.0.6
 XAPP_XGAMMA_SOURCE = xgamma-$(XAPP_XGAMMA_VERSION).tar.bz2
 XAPP_XGAMMA_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XGAMMA_LICENSE = MIT
