462118beab9bb83f52855fff3c24576ce1b3f263
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-18 14:40:36 +0200

    package/x11r7/xapp_xlsfonts: bump version to 1.0.5, add hash
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xlsfonts/xapp_xlsfonts.mk
+++ package/x11r7/xapp_xlsfonts/xapp_xlsfonts.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XLSFONTS_VERSION = 1.0.4
+XAPP_XLSFONTS_VERSION = 1.0.5
 XAPP_XLSFONTS_SOURCE = xlsfonts-$(XAPP_XLSFONTS_VERSION).tar.bz2
 XAPP_XLSFONTS_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XLSFONTS_LICENSE = MIT
