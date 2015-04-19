c0229917ffd969b08a7fcd268db678d424a7f676
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-19 12:17:01 +0200

    package/perl-encode-locale: new package
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/perl-encode-locale/perl-encode-locale.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# perl-encode-locale
+#
+################################################################################
+
+PERL_ENCODE_LOCALE_VERSION = 1.04
+PERL_ENCODE_LOCALE_SOURCE = Encode-Locale-$(PERL_ENCODE_LOCALE_VERSION).tar.gz
+PERL_ENCODE_LOCALE_SITE = $(BR2_CPAN_MIRROR)/authors/id/G/GA/GAAS
+PERL_ENCODE_LOCALE_DEPENDENCIES = perl
+PERL_ENCODE_LOCALE_LICENSE = Artistic or GPLv1+
+PERL_ENCODE_LOCALE_LICENSE_FILES = README
+
+$(eval $(perl-package))
