de934378f0d8d0840b0aebdcb8d971f822284f52
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-19 19:53:04 +0200

    package/libev: bump version to 4.19
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libev/libev.mk
+++ package/libev/libev.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBEV_VERSION = 4.15
+LIBEV_VERSION = 4.19
 LIBEV_SITE = http://dist.schmorp.de/libev
 LIBEV_INSTALL_STAGING = YES
 LIBEV_LICENSE = BSD-2c or GPLv2+
