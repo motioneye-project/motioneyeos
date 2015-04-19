c015d2f2d8269f38a7378f7ac8e1385f6a7d39bc
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-19 16:29:31 +0200

    package/iozone: bump to version 3_430
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/iozone/iozone.mk
+++ package/iozone/iozone.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IOZONE_VERSION = 3_414
+IOZONE_VERSION = 3_430
 IOZONE_SOURCE = iozone$(IOZONE_VERSION).tar
 IOZONE_SITE = http://www.iozone.org/src/current
 IOZONE_LICENSE = IOzone license (NO DERIVED WORKS ALLOWED)
