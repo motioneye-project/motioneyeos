55bab4f619833b4edf933a7309503c9425cf623d
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-04-19 09:39:59 -0300

    packages: remove non-IPv6 enabler patches
    These are no longer required so remove them.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/rpcbind/rpcbind.mk
+++ package/rpcbind/rpcbind.mk
@@ -9,7 +9,6 @@ RPCBIND_SITE = http://downloads.sourceforge.net/project/rpcbind/rpcbind/$(RPCBIN
 RPCBIND_SOURCE = rpcbind-$(RPCBIND_VERSION).tar.bz2
 RPCBIND_LICENSE = BSD-3c
 RPCBIND_LICENSE_FILES = COPYING
-RPCBIND_AUTORECONF = YES
 
 RPCBIND_CONF_ENV += \
 	CFLAGS="$(TARGET_CFLAGS) -I$(STAGING_DIR)/usr/include/tirpc/"
