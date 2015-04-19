15f46e3354d999ff86461f75f1d123ac58961432
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-19 12:17:13 +0200

    package/perl-libwww: New package
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/perl-libwww/perl-libwww.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# perl-libwww
+#
+################################################################################
+
+PERL_LIBWWW_VERSION = 6.13
+PERL_LIBWWW_SITE = $(call github,libwww-perl,libwww-perl,$(PERL_LIBWWW_VERSION))
+PERL_LIBWWW_LICENSE = Artistic or GPLv1+
+PERL_LIBWWW_LICENSE_FILES = LICENSE
+PERL_LIBWWW_DEPENDENCIES = \
+	perl-encode-locale \
+	perl-file-listing \
+	perl-html-parser \
+	perl-http-cookies \
+	perl-http-daemon \
+	perl-http-date \
+	perl-http-negotiate \
+	perl-net-http \
+	perl-uri \
+	perl-www-robotrules
+
+$(eval $(perl-package))
