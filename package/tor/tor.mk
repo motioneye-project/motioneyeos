17d2c6d5db1058efde7b65c8d068ec5fdfabe960
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-20 19:51:37 +0200

    package/tor: new package
    [Thomas:
      - remove reference to BR2_LARGEFILE, this option has been
        removed. Largefile support is now guaranteed to be present.
      - rewrap Config.in help text to the appropriate length.]
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/tor/tor.mk
@@ -0,0 +1,35 @@
+################################################################################
+#
+# tor
+#
+################################################################################
+
+TOR_VERSION = 0.2.5.12
+TOR_SITE = https://dist.torproject.org
+TOR_LICENSE = BSD-3c
+TOR_LICENSE_FILES = LICENSE
+TOR_DEPENDENCIES = libevent openssl zlib
+TOR_AUTORECONF = YES
+
+TOR_CONF_OPTS = \
+	--disable-gcc-hardening \
+	--with-libevent-dir=$(STAGING_DIR)/usr \
+	--with-openssl-dir=$(STAGING_DIR)/usr \
+	--with-zlib-dir=$(STAGING_DIR)/usr
+
+ifeq ($(BR2_STATIC_LIBS),y)
+TOR_CONF_OPTS += \
+	--enable-static-libevent \
+	--enable-static-openssl \
+	--enable-static-tor \
+	--enable-static-zlib
+endif
+
+ifeq ($(BR2_PACKAGE_LIBSECCOMP),y)
+TOR_CONF_OPTS += --enable-seccomp
+TOR_DEPENDENCIES += libseccomp
+else
+TOR_CONF_OPTS += --disable-seccomp
+endif
+
+$(eval $(autotools-package))
