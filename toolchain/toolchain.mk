fb5cbf3198c9af20f7440b66c070873bb2207797
Yann E. MORIN <yann.morin.1998@free.fr>
2015-04-21 18:55:52 +0200

    toolchain: fix installing gconv libs with multi-arch toolchain
    For a multi-arch toolchain, gconv modules are in a sub-directory named
    after the machine gcc targets. This is the case, for example, for the
    Linaro ARM 2014.09 toolchain, which has the gconv modules in (relative
    to the sysroot):
        /usr/lib/arm-linux-gnueabihf/gconv
    
    while the Sourcery CodeBench ARM 2014.05 (non-multi-arch) has them in:
        /usr/lib/gconv
    
    So, to catter for both cases, search both paths. We want to favour the
    machine-specific gconv modules over potentially existing "generic" ones,
    so we first search that (if it exists) and fallback to looking in the
    generic location.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- toolchain/toolchain.mk
+++ toolchain/toolchain.mk
@@ -17,28 +17,38 @@ endif
 ifeq ($(BR2_TOOLCHAIN_GLIBC_GCONV_LIBS_COPY),y)
 GCONV_LIBS = $(call qstrip,$(BR2_TOOLCHAIN_GLIBC_GCONV_LIBS_LIST))
 define COPY_GCONV_LIBS
-	$(Q)if [ -z "$(GCONV_LIBS)" ]; then \
-		$(INSTALL) -m 0644 -D $(STAGING_DIR)/usr/lib/gconv/gconv-modules \
-				      $(TARGET_DIR)/usr/lib/gconv/gconv-modules; \
-		$(INSTALL) -m 0644 $(STAGING_DIR)/usr/lib/gconv/*.so \
+	$(Q)found_gconv=no; \
+	for d in $(TOOLCHAIN_EXTERNAL_PREFIX) ''; do \
+		[ -d "$(STAGING_DIR)/usr/lib/$${d}/gconv" ] || continue; \
+		found_gconv=yes; \
+		break; \
+	done; \
+	if [ "$${found_gconv}" = "no" ]; then \
+		printf "Unable to find gconv modules\n" >&2; \
+		exit 1; \
+	fi; \
+	if [ -z "$(GCONV_LIBS)" ]; then \
+		$(INSTALL) -m 0644 -D $(STAGING_DIR)/usr/lib/$${d}/gconv/gconv-modules \
+				      $(TARGET_DIR)/usr/lib/gconv/gconv-modules && \
+		$(INSTALL) -m 0644 $(STAGING_DIR)/usr/lib/$${d}/gconv/*.so \
 				   $(TARGET_DIR)/usr/lib/gconv \
 		|| exit 1; \
 	else \
 		for l in $(GCONV_LIBS); do \
-			$(INSTALL) -m 0644 -D $(STAGING_DIR)/usr/lib/gconv/$${l}.so \
+			$(INSTALL) -m 0644 -D $(STAGING_DIR)/usr/lib/$${d}/gconv/$${l}.so \
 					      $(TARGET_DIR)/usr/lib/gconv/$${l}.so \
 			|| exit 1; \
-			$(TARGET_READELF) -d $(STAGING_DIR)/usr/lib/gconv/$${l}.so |\
+			$(TARGET_READELF) -d $(STAGING_DIR)/usr/lib/$${d}/gconv/$${l}.so |\
 			sort -u |\
 			sed -e '/.*(NEEDED).*\[\(.*\.so\)\]$$/!d; s//\1/;' |\
 			while read lib; do \
-				 $(INSTALL) -m 0644 -D $(STAGING_DIR)/usr/lib/gconv/$${lib} \
+				 $(INSTALL) -m 0644 -D $(STAGING_DIR)/usr/lib/$${d}/gconv/$${lib} \
 						       $(TARGET_DIR)/usr/lib/gconv/$${lib} \
 				 || exit 1; \
 			done; \
 		done; \
 		./support/scripts/expunge-gconv-modules "$(GCONV_LIBS)" \
-			<$(STAGING_DIR)/usr/lib/gconv/gconv-modules \
+			<$(STAGING_DIR)/usr/lib/$${d}/gconv/gconv-modules \
 			>$(TARGET_DIR)/usr/lib/gconv/gconv-modules; \
 	fi
 endef
