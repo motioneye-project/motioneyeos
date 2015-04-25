2ab02505d5c88b3a54f234fb5bceb4e3618347b6
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-25 15:14:02 +0200

    package/perl-libwww-perl: Move package/perl-libwww to new package
    This move is necessary to sync the package with scancpan output
    as suggested by Francois.
    http://lists.busybox.net/pipermail/buildroot/2015-April/126406.html
    
    Since perl-libwww was never part of any official buildroot release
    I did not bother the include legavy handling of its options ;)
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/perl-libwww-perl/perl-libwww-perl.mk
+++ package/perl-libwww-perl/perl-libwww-perl.mk
@@ -1,21 +1,25 @@
 ################################################################################
 #
-# perl-libwww
+# perl-libwww-perl
 #
 ################################################################################
 
-PERL_LIBWWW_VERSION = 6.13
-PERL_LIBWWW_SITE = $(call github,libwww-perl,libwww-perl,$(PERL_LIBWWW_VERSION))
-PERL_LIBWWW_LICENSE = Artistic or GPLv1+
+PERL_LIBWWW_PERL_VERSION = 6.13
+PERL_LIBWWW_PERL_SOURCE = libwww-perl-$(PERL_LIBWWW_PERL_VERSION).tar.gz
+PERL_LIBWWW_PERL_SITE = $(BR2_CPAN_MIRROR)/authors/id/E/ET/ETHER
+PERL_LIBWWW_PERL_LICENSE = Artistic or GPLv1+
 PERL_LIBWWW_LICENSE_FILES = README
-PERL_LIBWWW_DEPENDENCIES = \
+PERL_LIBWWW_PERL_DEPENDENCIES = \
+	perl \
 	perl-encode-locale \
 	perl-file-listing \
 	perl-html-parser \
 	perl-http-cookies \
 	perl-http-daemon \
 	perl-http-date \
+	perl-http-message \
 	perl-http-negotiate \
+	perl-lwp-mediatypes \
 	perl-net-http \
 	perl-uri \
 	perl-www-robotrules
