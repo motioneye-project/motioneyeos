1284bad1c476caf8c9cc65b72078d99c91659170
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-26 11:37:14 +0200

    package/perl-db-file: new package
    [Thomas: rename package to perl-db-file.]
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/perl-db-file/perl-db-file.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-db_file
+#
+################################################################################
+
+PERL_DB_FILE_VERSION = 1.835
+PERL_DB_FILE_SOURCE = DB_File-$(PERL_DB_FILE_VERSION).tar.gz
+PERL_DB_FILE_SITE = $(BR2_CPAN_MIRROR)/authors/id/P/PM/PMQS
+PERL_DB_FILE_DEPENDENCIES = berkeleydb perl
+PERL_DB_FILE_LICENSE = Artistic or GPLv1+
+
+$(eval $(perl-package))
