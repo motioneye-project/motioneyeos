c80f0490d994688e57799c02ad1e6c7dcaf5e9e3
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-04-26 14:36:07 +0200

    package/libedit: bump version to 20150325-3.1
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libedit/libedit.mk
+++ package/libedit/libedit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBEDIT_VERSION = 20130712-3.1
+LIBEDIT_VERSION = 20150325-3.1
 LIBEDIT_SITE = http://www.thrysoee.dk/editline
 LIBEDIT_INSTALL_STAGING = YES
 LIBEDIT_DEPENDENCIES = ncurses
@@ -13,6 +13,12 @@ LIBEDIT_LICENSE_FILES = COPYING
 
 # We're patching configure.ac
 LIBEDIT_AUTORECONF = YES
+
+# Needed for autoreconf to work properly
+define LIBEDIT_FIXUP_M4_DIR
+	mkdir $(@D)/m4
+endef
+LIBEDIT_POST_EXTRACT_HOOKS += LIBEDIT_FIXUP_M4_DIR
 
 # Doesn't really support !wchar, but support is disabled otherwise
 LIBEDIT_CONF_OPTS += --enable-widec
