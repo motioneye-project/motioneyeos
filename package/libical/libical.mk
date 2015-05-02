3ac4bf634e16cc8b5f1f8d732da296b4e32dd734
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-05-02 11:15:04 +0200

    package/libical: bump version to v1.0.1 and switch to cmake
    Project moved to github according to http://sourceforge.net/projects/freeassociation
    
    "As of 2014-06-14, this project may now be found at http://github.com/libical."
    
    [Thomas: propagate the C++ dependency to bluez5_utils OBEX support,
    which selects libical.]
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libical/libical.mk
+++ package/libical/libical.mk
@@ -4,10 +4,14 @@
 #
 ################################################################################
 
-LIBICAL_VERSION = 0.48
-LIBICAL_SITE = http://downloads.sourceforge.net/project/freeassociation/libical/libical-$(LIBICAL_VERSION)
+LIBICAL_VERSION = 1.0.1
+LIBICAL_SITE = https://github.com/libical/libical/releases/download/v$(LIBICAL_VERSION)
 LIBICAL_INSTALL_STAGING = YES
 LIBICAL_LICENSE = MPLv1.0 or LGPLv2.1
 LIBICAL_LICENSE_FILES = LICENSE
 
-$(eval $(autotools-package))
+# building without this option is broken, it is used by
+# Gentoo/alpinelinux as well
+LIBICAL_CONF_OPTS = -DSHARED_ONLY=true
+
+$(eval $(cmake-package))
