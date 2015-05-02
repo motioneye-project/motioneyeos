7e47ac2d8dd4323d11615e8bb50e66e4b182b2f7
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-05-02 10:17:36 +0200

    package/x11r7/xlib_libxkbfile: bump version to 1.0.9
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xlib_libxkbfile/xlib_libxkbfile.mk
+++ package/x11r7/xlib_libxkbfile/xlib_libxkbfile.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBXKBFILE_VERSION = 1.0.8
+XLIB_LIBXKBFILE_VERSION = 1.0.9
 XLIB_LIBXKBFILE_SOURCE = libxkbfile-$(XLIB_LIBXKBFILE_VERSION).tar.bz2
 XLIB_LIBXKBFILE_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXKBFILE_LICENSE = MIT
