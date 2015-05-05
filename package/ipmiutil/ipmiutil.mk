ec45eb1619da40ea97fa39dfe60cee2a9b8e78c6
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-05-05 10:43:54 -0300

    ipmiutil: force md2 off
    The configure test runs against LIB_DIR (which is --libdir in configure
    parlay) hence it's against the distro openssl version.
    On newish openssl versions md2 is disabled by default, and buildroot
    doesn't try to enable it (it's obsolete), but if the distro
    libcrypto/openssl has it enabled then ipmiutil tries to use it.
    
    Force it off to avoid build breakage. Fixes:
    http://autobuild.buildroot.net/results/cd2/cd2e617f8e2b00581ab5936029f85e62ed3259ba/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ipmiutil/ipmiutil.mk
+++ package/ipmiutil/ipmiutil.mk
@@ -10,7 +10,9 @@ IPMIUTIL_LICENSE = BSD-3c
 IPMIUTIL_LICENSE_FILES = COPYING
 # We're patching configure.ac
 IPMIUTIL_AUTORECONF = YES
-
+# tests against distro libcrypto so it might get a false positive when
+# the openssl version is old, so force it off
+IPMIUTIL_CONF_OPTS = CPPFLAGS="$(TARGET_CPPFLAGS) -DSKIP_MD2"
 IPMIUTIL_DEPENDENCIES = openssl
 
 $(eval $(autotools-package))
