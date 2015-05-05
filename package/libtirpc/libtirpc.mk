aee57627b09d0955270a562b8adaa0ff88ef523b
Jörg Krause <joerg.krause@embedded.rocks>
2015-05-05 18:24:33 +0200

    package/libtirpc: remove host-pkgconf dependency
    configure.ac does not contain PKG_CHECK_MODULES anymore.
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libtirpc/libtirpc.mk
+++ package/libtirpc/libtirpc.mk
@@ -18,12 +18,4 @@ LIBTIRPC_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -DGQ"
 
 LIBTIRPC_CONF_OPTS = --disable-gssapi
 
-# We need host-pkgconf because the configure.ac contains a
-# PKG_CHECK_MODULES macro call, and since we're autoreconfiguring this
-# package, we need the source for this PKG_CHECK_MODULES macro, which
-# comes from host-pkgconf. Other than that, pkgconf is only used to
-# find if there is a gss implementation somewhere, which we don't
-# support in Buildroot yet.
-LIBTIRPC_DEPENDENCIES = host-pkgconf
-
 $(eval $(autotools-package))
