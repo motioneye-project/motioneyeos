c69bafe0aef71b9846866270da9462c3492d9508
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2015-05-16 15:36:50 +0200

    libmatroska: add host-pkgconf dependency
    libmatroska configure.ac uses PKG_CHECK_MODULES(), so this package
    needs to depend on host-pkgconf.
    
    Fixes:
    
      http://autobuild.buildroot.org/results/80a/80a9f0f5594ab833600ea8514dc184e50d2bf08c/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libmatroska/libmatroska.mk
+++ package/libmatroska/libmatroska.mk
@@ -10,6 +10,6 @@ LIBMATROSKA_SITE = http://dl.matroska.org/downloads/libmatroska
 LIBMATROSKA_INSTALL_STAGING = YES
 LIBMATROSKA_LICENSE = LGPLv2.1+
 LIBMATROSKA_LICENSE_FILES = LICENSE.LGPL
-LIBMATROSKA_DEPENDENCIES = libebml
+LIBMATROSKA_DEPENDENCIES = libebml host-pkgconf
 
 $(eval $(autotools-package))
