3a07f5e4fe2543f8fbee4cdc680318bc426bd83d
Peter Korsgaard <peter@korsgaard.com>
2015-05-17 13:42:34 +0200

    ipmiutil: openssl is an optional dependency
    So handle it as such (it is only needed for the lanplus functionality).
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ipmiutil/ipmiutil.mk
+++ package/ipmiutil/ipmiutil.mk
@@ -10,10 +10,15 @@ IPMIUTIL_LICENSE = BSD-3c
 IPMIUTIL_LICENSE_FILES = COPYING
 # We're patching configure.ac
 IPMIUTIL_AUTORECONF = YES
+
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
 # tests against distro libcrypto so it might get a false positive when
 # the openssl version is old, so force it off
 # SKIP_MD2 can be used only if ALLOW_GNU is defined.
-IPMIUTIL_CONF_OPTS = CPPFLAGS="$(TARGET_CPPFLAGS) -DALLOW_GNU -DSKIP_MD2"
-IPMIUTIL_DEPENDENCIES = openssl
+IPMIUTIL_CONF_OPTS += CPPFLAGS="$(TARGET_CPPFLAGS) -DALLOW_GNU -DSKIP_MD2"
+IPMIUTIL_DEPENDENCIES += openssl
+else
+IPMIUTIL_CONF_OPTS += --disable-lanplus
+endif
 
 $(eval $(autotools-package))
