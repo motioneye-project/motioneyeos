754f0015c89e9d17e09f073259ecbc1bd64a0ef6
Samuel Martin <s.martin49@gmail.com>
2015-05-22 06:30:20 +0200

    package/protobuf: autoreconf is now needed
    Since its update, the *_SITE points to the git repository, so configure
    script is not generated in this tarball.
    
    Fixes:
      http://autobuild.buildroot.org/results/ccb/ccba6d8c73b5e826b86065b77b3fe5077db7b99c/
      http://autobuild.buildroot.org/results/57e/57ea34e9b48293075e10f0dd2331de2785730827/
      and many more...
    
    [Peter: drop redundant HOST_*_AUTORECONF]
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/protobuf/protobuf.mk
+++ package/protobuf/protobuf.mk
@@ -8,6 +8,8 @@ PROTOBUF_VERSION = v2.5.0
 PROTOBUF_SITE = $(call github,google,protobuf,$(PROTOBUF_VERSION))
 PROTOBUF_LICENSE = BSD-3c
 PROTOBUF_LICENSE_FILES = COPYING.txt
+# no configure script
+PROTOBUF_AUTORECONF = YES
 
 # N.B. Need to use host protoc during cross compilation.
 PROTOBUF_DEPENDENCIES = host-protobuf
