c6946f24cc1e4cbb8aaff1a2192c1a95d539ca5d
Alex Suykov <alex.suykov@gmail.com>
2015-05-23 13:02:24 +0300

    chrony: systemd support
    systemd has its own NTP daemon, which must be disabled before
    starting chrony. Possible (but unlikely) conflict with openntpd
    is not marked in either package.
    
    Signed-off-by: Alex Suykov <alex.suykov@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/chrony/chrony.mk
+++ package/chrony/chrony.mk
@@ -45,4 +45,12 @@ define CHRONY_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 755 package/chrony/S49chrony $(TARGET_DIR)/etc/init.d/S49chrony
 endef
 
+define CHRONY_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 package/chrony/chrony.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/chrony.service
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+	ln -sf /usr/lib/systemd/system/chrony.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/chrony.service
+endef
+
 $(eval $(generic-package))
