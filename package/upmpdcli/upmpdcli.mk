2154063a38cffbcf189af3bb7fec2288cdc6fe82
Alex Suykov <alex.suykov@gmail.com>
2015-05-23 13:13:53 +0300

    upmpdcli: systemd support
    Usable .service file is provided in the package.
    
    Signed-off-by: Alex Suykov <alex.suykov@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/upmpdcli/upmpdcli.mk
+++ package/upmpdcli/upmpdcli.mk
@@ -31,6 +31,14 @@ define UPMPDCLI_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 0755 package/upmpdcli/S99upmpdcli $(TARGET_DIR)/etc/init.d/S99upmpdcli
 endef
 
+define UPMPDCLI_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 $(@D)/systemd/upmpdcli.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/upmpdcli.service
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+	ln -sf /usr/lib/systemd/system/upmpdcli.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/upmpdcli.service
+endef
+
 define UPMPDCLI_INSTALL_CONF_FILE
 	$(INSTALL) -D -m 0755 $(@D)/src/upmpdcli.conf $(TARGET_DIR)/etc/upmpdcli.conf
 endef
