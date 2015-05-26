4c0613e7de01c9e38d0f9750347937f2dbcf66cc
Gary Bisson <gary.bisson@boundarydevices.com>
2015-05-26 10:19:37 +0200

    gpu-viv-bin-mx6q: rename package to imx-gpu-viv
    In order to be in line with new Freescale naming convention:
    
    https://github.com/Freescale/meta-fsl-arm/commit/74c86aa9
    
    "Graphics recipe is renamed from gpu-viv-bin-mx6q to imx-gpu-viv"
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/freescale-imx/imx-gpu-viv/imx-gpu-viv.mk
@@ -0,0 +1,98 @@
+################################################################################
+#
+# imx-gpu-viv
+#
+################################################################################
+
+IMX_GPU_VIV_BASE_VERSION = 5.0.11.p4.1
+ifeq ($(BR2_ARM_EABIHF),y)
+IMX_GPU_VIV_VERSION = $(IMX_GPU_VIV_BASE_VERSION)-hfp
+else
+IMX_GPU_VIV_VERSION = $(IMX_GPU_VIV_BASE_VERSION)-sfp
+endif
+IMX_GPU_VIV_SITE = $(FREESCALE_IMX_SITE)
+IMX_GPU_VIV_SOURCE = imx-gpu-viv-$(IMX_GPU_VIV_VERSION).bin
+
+IMX_GPU_VIV_INSTALL_STAGING = YES
+
+IMX_GPU_VIV_LICENSE = Freescale Semiconductor Software License Agreement
+IMX_GPU_VIV_LICENSE_FILES = EULA
+IMX_GPU_VIV_REDISTRIBUTE = NO
+
+IMX_GPU_VIV_PROVIDES = libegl libgles libopenvg
+IMX_GPU_VIV_LIB_TARGET = $(call qstrip,$(BR2_PACKAGE_IMX_GPU_VIV_OUTPUT))
+
+define IMX_GPU_VIV_EXTRACT_CMDS
+	$(call FREESCALE_IMX_EXTRACT_HELPER,$(DL_DIR)/$(IMX_GPU_VIV_SOURCE))
+endef
+
+# For some reason libGAL_egl for x11 is called libGAL_egl.dri.so
+ifeq ($(IMX_GPU_VIV_LIB_TARGET),x11)
+define IMX_GPU_VIV_FIXUP_SYMLINKS
+	ln -sf libGAL_egl.dri.so $(@D)/gpu-core/usr/lib/libGAL_egl.so
+endef
+endif
+
+# Instead of building, we fix up the inconsistencies that exist
+# in the upstream archive here.
+# Make sure these commands are idempotent.
+define IMX_GPU_VIV_BUILD_CMDS
+	$(SED) 's/defined(LINUX)/defined(__linux__)/g' $(@D)/gpu-core/usr/include/*/*.h
+	ln -sf libGL.so.1.2 $(@D)/gpu-core/usr/lib/libGL.so
+	ln -sf libGL.so.1.2 $(@D)/gpu-core/usr/lib/libGL.so.1
+	ln -sf libGL.so.1.2 $(@D)/gpu-core/usr/lib/libGL.so.1.2.0
+	ln -sf libEGL-$(IMX_GPU_VIV_LIB_TARGET).so $(@D)/gpu-core/usr/lib/libEGL.so
+	ln -sf libEGL-$(IMX_GPU_VIV_LIB_TARGET).so $(@D)/gpu-core/usr/lib/libEGL.so.1
+	ln -sf libEGL-$(IMX_GPU_VIV_LIB_TARGET).so $(@D)/gpu-core/usr/lib/libEGL.so.1.0
+	ln -sf libGLESv2-$(IMX_GPU_VIV_LIB_TARGET).so $(@D)/gpu-core/usr/lib/libGLESv2.so
+	ln -sf libGLESv2-$(IMX_GPU_VIV_LIB_TARGET).so $(@D)/gpu-core/usr/lib/libGLESv2.so.2
+	ln -sf libGLESv2-$(IMX_GPU_VIV_LIB_TARGET).so $(@D)/gpu-core/usr/lib/libGLESv2.so.2.0.0
+	ln -sf libVIVANTE-$(IMX_GPU_VIV_LIB_TARGET).so $(@D)/gpu-core/usr/lib/libVIVANTE.so
+	ln -sf libGAL-$(IMX_GPU_VIV_LIB_TARGET).so $(@D)/gpu-core/usr/lib/libGAL.so
+	ln -sf libGAL_egl.$(IMX_GPU_VIV_LIB_TARGET).so $(@D)/gpu-core/usr/lib/libGAL_egl.so
+	$(IMX_GPU_VIV_FIXUP_SYMLINKS)
+endef
+
+ifeq ($(IMX_GPU_VIV_LIB_TARGET),fb)
+define IMX_GPU_VIV_FIXUP_FB_HEADERS
+	$(SED) '39i\
+		#if !defined(EGL_API_X11) && !defined(EGL_API_DFB) && !defined(EGL_API_FB) \n\
+		#define EGL_API_FB \n\
+		#endif' $(STAGING_DIR)/usr/include/EGL/eglvivante.h
+endef
+endif
+
+define IMX_GPU_VIV_INSTALL_STAGING_CMDS
+	cp -r $(@D)/gpu-core/usr/* $(STAGING_DIR)/usr
+	$(IMX_GPU_VIV_FIXUP_FB_HEADERS)
+	for lib in egl glesv2 vg; do \
+		$(INSTALL) -m 0644 -D \
+			$(@D)/gpu-core/usr/lib/pkgconfig/$${lib}.pc \
+			$(STAGING_DIR)/usr/lib/pkgconfig/$${lib}.pc; \
+	done
+endef
+
+ifeq ($(BR2_PACKAGE_IMX_GPU_VIV_EXAMPLES),y)
+define IMX_GPU_VIV_INSTALL_EXAMPLES
+	mkdir -p $(TARGET_DIR)/usr/share/examples/
+	cp -r $(@D)/gpu-demos/opt/* $(TARGET_DIR)/usr/share/examples/
+endef
+endif
+
+# On the target, remove the unused libraries.
+# Note that this is _required_, else ldconfig may create symlinks
+# to the wrong library
+define IMX_GPU_VIV_INSTALL_TARGET_CMDS
+	$(IMX_GPU_VIV_INSTALL_EXAMPLES)
+	cp -a $(@D)/gpu-core/usr/lib $(TARGET_DIR)/usr
+	for lib in EGL GAL VIVANTE GLESv2; do \
+		for f in $(TARGET_DIR)/usr/lib/lib$${lib}-*.so; do \
+			case $$f in \
+				*-$(IMX_GPU_VIV_LIB_TARGET).so) : ;; \
+				*) $(RM) $$f ;; \
+			esac; \
+		done; \
+	done
+endef
+
+$(eval $(generic-package))
--- package/gstreamer1/gst1-imx/gst1-imx.mk
+++ package/gstreamer1/gst1-imx/gst1-imx.mk
@@ -13,7 +13,7 @@ GST1_IMX_LICENSE_FILES = LICENSE
 GST1_IMX_INSTALL_STAGING = YES
 
 GST1_IMX_DEPENDENCIES += host-pkgconf host-python \
-	gpu-viv-bin-mx6q gstreamer1 gst1-plugins-base libfslvpuwrap
+	imx-gpu-viv gstreamer1 gst1-plugins-base libfslvpuwrap
 
 # needs access to imx-specific kernel headers
 GST1_IMX_DEPENDENCIES += linux
