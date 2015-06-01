67524ba4e8591c1dc02e8e52408adcba34cb8c43
Karoly Kasza <kaszak@gmail.com>
2015-06-01 15:17:50 +0200

    package/ccache: bump to version 3.2.2
    Bump ccache to minor version 3.2.2.
    Also changed download URL to https.
    
    Signed-off-by: Karoly Kasza <kaszak@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ccache/ccache.mk
+++ package/ccache/ccache.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-CCACHE_VERSION = 3.2.1
-CCACHE_SITE = http://samba.org/ftp/ccache
+CCACHE_VERSION = 3.2.2
+CCACHE_SITE = https://samba.org/ftp/ccache
 CCACHE_SOURCE = ccache-$(CCACHE_VERSION).tar.xz
 CCACHE_LICENSE = GPLv3+, others
 CCACHE_LICENSE_FILES = LICENSE.txt GPL-3.0.txt
