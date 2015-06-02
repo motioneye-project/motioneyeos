aaba5c5a7ea8eafd7157c78e0fd4b393d3bec336
Francois Perrad <fperrad@gmail.com>
2015-06-02 18:32:58 +0200

    lua-messagepack: bump to version 0.3.3
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lua-messagepack/lua-messagepack.mk
+++ package/lua-messagepack/lua-messagepack.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUA_MESSAGEPACK_VERSION_UPSTREAM = 0.3.2
+LUA_MESSAGEPACK_VERSION_UPSTREAM = 0.3.3
 ifeq ($(BR2_PACKAGE_LUA_5_3),y)
 LUA_MESSAGEPACK_VERSION = lua53-$(LUA_MESSAGEPACK_VERSION_UPSTREAM)-1
 else
