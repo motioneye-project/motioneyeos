390ef4ebb441c263f16572e458c71bde35d01c58
Yann E. MORIN <yann.morin.1998@free.fr>
2015-06-03 22:41:29 +0200

    package/matchbox: get rid of intermediate non-package matchbox
    Currently, the matchbox entry in the menu is a non-package entry, that
    forcibly selects the matchbox-wm package, which is the real matchbox WM.
    
    So, get rid of the current matchbox option, rename the existing
    matchbox-wm to simply matchbox, so we have a real package from the
    onset.
    
    Since we're re-using the previous option for the WM package, there is no
    need for an entry in the legacy menu.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/matchbox/matchbox.mk
+++ package/matchbox/matchbox.mk
@@ -1,1 +1,41 @@
+################################################################################
+#
+# matchbox
+#
+################################################################################
+
+MATCHBOX_VERSION = 1.2
+MATCHBOX_SOURCE = matchbox-window-manager-$(MATCHBOX_VERSION).tar.bz2
+MATCHBOX_SITE = http://downloads.yoctoproject.org/releases/matchbox/matchbox-window-manager/$(MATCHBOX_VERSION)
+MATCHBOX_LICENSE = GPLv2+
+MATCHBOX_LICENSE_FILES = COPYING
+
+MATCHBOX_DEPENDENCIES = matchbox-lib
+MATCHBOX_CONF_OPTS = --enable-expat
+
+# Workaround bug in configure script
+MATCHBOX_CONF_ENV = expat=yes
+
+ifeq ($(BR2_PACKAGE_X11R7_LIBXCOMPOSITE),y)
+ifeq ($(BR2_PACKAGE_X11R7_LIBXPM),y)
+MATCHBOX_CONF_OPTS += --enable-composite
+MATCHBOX_DEPENDENCIES += xlib_libXcomposite
+MATCHBOX_DEPENDENCIES += xlib_libXpm
+endif
+endif
+
+ifeq ($(BR2_PACKAGE_XLIB_LIBXFT),y)
+MATCHBOX_DEPENDENCIES += xlib_libXft
+endif
+
+ifeq ($(BR2_PACKAGE_STARTUP_NOTIFICATION),y)
+MATCHBOX_CONF_OPTS += --enable-startup-notification
+MATCHBOX_DEPENDENCIES += startup-notification
+else
+MATCHBOX_CONF_OPTS += --disable-startup-notification
+endif
+
+$(eval $(autotools-package))
+
+# After we called a package infra, we can include more files
 include $(sort $(wildcard package/matchbox/*/*.mk))
