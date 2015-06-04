583edd3f865e1ba298d7e8fe765e27a0a0eb7069
Baruch Siach <baruch@tkos.co.il>
2015-06-04 18:05:49 +0300

    gpsd: bump to version 3.15
    Drop upstream patches #1 and #3. Drop the no longer needed patch #2.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gpsd/gpsd.mk
+++ package/gpsd/gpsd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GPSD_VERSION = 3.11
+GPSD_VERSION = 3.15
 GPSD_SITE = http://download-mirror.savannah.gnu.org/releases/gpsd
 GPSD_LICENSE = BSD-3c
 GPSD_LICENSE_FILES = COPYING
