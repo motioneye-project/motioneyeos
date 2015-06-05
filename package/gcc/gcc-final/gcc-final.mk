e1cf34a39538e58ecf50d091482b2304c2f7e6d8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-06-04 21:28:47 -0300

    gcc/gcc-final: install libatomic via HOST_GCC_FINAL_GCC_LIB_DIR
    Otherwise it's a no-op for sh4.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gcc/gcc-final/gcc-final.mk
+++ package/gcc/gcc-final/gcc-final.mk
@@ -131,9 +131,9 @@ endef
 HOST_GCC_FINAL_POST_INSTALL_HOOKS += HOST_GCC_FINAL_INSTALL_LIBGCC
 
 define HOST_GCC_FINAL_INSTALL_LIBATOMIC
-	-cp -dpf $(HOST_DIR)/usr/$(GNU_TARGET_NAME)/lib*/libatomic* \
+	-cp -dpf $(HOST_GCC_FINAL_GCC_LIB_DIR)/libatomic* \
 		$(STAGING_DIR)/lib/
-	-cp -dpf $(HOST_DIR)/usr/$(GNU_TARGET_NAME)/lib*/libatomic* \
+	-cp -dpf $(HOST_GCC_FINAL_GCC_LIB_DIR)/libatomic* \
 		$(TARGET_DIR)/lib/
 endef
 
