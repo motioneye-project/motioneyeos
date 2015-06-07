6bad48c2e1e75b71b391d13a4b731bd7ac699259
Peter Korsgaard <peter@korsgaard.com>
2015-06-07 23:57:02 +0200

    xscreensaver: bump version
    And drop no longer needed patches.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/xscreensaver/xscreensaver.mk
+++ package/xscreensaver/xscreensaver.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XSCREENSAVER_VERSION = 5.22
+XSCREENSAVER_VERSION = 5.32
 XSCREENSAVER_SITE = http://www.jwz.org/xscreensaver
 
 # N.B. GPLv2+ code (in the hacks/glx subdirectory) is not currently built.
@@ -12,7 +12,7 @@ XSCREENSAVER_LICENSE = MIT-like GPLv2+
 XSCREENSAVER_LICENSE_FILES = hacks/screenhack.h hacks/glx/chessmodels.h
 
 XSCREENSAVER_DEPENDENCIES = jpeg libglade libgtk2 xlib_libX11 xlib_libXt \
-	$(if $(BR2_PACKAGE_GETTEXT),gettext)
+	$(if $(BR2_PACKAGE_GETTEXT),gettext) host-intltool
 
 # otherwise we end up with host include/library dirs passed to the
 # compiler/linker
