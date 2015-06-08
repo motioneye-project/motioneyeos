fb67b749cacacb5aaf8cf594dfb80d975982dfe2
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-06-08 16:20:31 -0300

    openvpn: bump to version 2.3.7
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/openvpn/openvpn.mk
+++ package/openvpn/openvpn.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENVPN_VERSION = 2.3.6
+OPENVPN_VERSION = 2.3.7
 OPENVPN_SOURCE = openvpn-$(OPENVPN_VERSION).tar.xz
 OPENVPN_SITE = http://swupdate.openvpn.net/community/releases
 OPENVPN_DEPENDENCIES = host-pkgconf
