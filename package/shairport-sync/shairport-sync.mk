678f386f927226bb7604c968e812ca77b1bc16a5
Jörg Krause <joerg.krause@embedded.rocks>
2015-06-14 14:14:55 +0200

    package/shairport-sync: bump to version 2.2.4
    Amongst other things, the new release fixes the build with musl.
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/shairport-sync/shairport-sync.mk
+++ package/shairport-sync/shairport-sync.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SHAIRPORT_SYNC_VERSION = 2.2.3
+SHAIRPORT_SYNC_VERSION = 2.2.4
 SHAIRPORT_SYNC_SITE = $(call github,mikebrady,shairport-sync,$(SHAIRPORT_SYNC_VERSION))
 
 SHAIRPORT_SYNC_LICENSE = MIT, BSD-3c
