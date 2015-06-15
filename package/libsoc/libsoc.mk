9badabae854bf7560f737478e92e0d5873e4d31f
Yegor Yefremov <yegorslists@googlemail.com>
2015-06-15 21:00:45 +0200

    libsoc: bump to 0.6.4
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libsoc/libsoc.mk
+++ package/libsoc/libsoc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSOC_VERSION = 0.6.3
+LIBSOC_VERSION = 0.6.4
 LIBSOC_SITE = $(call github,jackmitch,libsoc,$(LIBSOC_VERSION))
 LIBSOC_LICENSE = LGPLv2.1
 LIBSOC_LICENSE_FILES = COPYING
