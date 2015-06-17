f6f3f3e6dd0887b8315e84795f7d2b2516c9d766
Yegor Yefremov <yegorslists@googlemail.com>
2015-06-17 21:28:23 +0200

    python-pyinotify: bump to 0.9.6
    Remove hash file as the project doesn't provide manual tarballs
    on GitHub.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-pyinotify/python-pyinotify.mk
+++ package/python-pyinotify/python-pyinotify.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PYINOTIFY_VERSION = 0.9.5
+PYTHON_PYINOTIFY_VERSION = 0.9.6
 PYTHON_PYINOTIFY_SITE = $(call github,seb-m,pyinotify,$(PYTHON_PYINOTIFY_VERSION))
 PYTHON_PYINOTIFY_SETUP_TYPE = setuptools
 PYTHON_PYINOTIFY_LICENSE = MIT
