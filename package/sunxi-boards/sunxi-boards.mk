d5764292470e93a8b6b8aa47eeaa9bee2f0039e3
Francois Perrad <fperrad@gmail.com>
2015-06-19 21:13:21 +0200

    sunxi-boards: bump to version a6a4e94066f4ec6b26fc7060dc453a6bcf26a504
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/sunxi-boards/sunxi-boards.mk
+++ package/sunxi-boards/sunxi-boards.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SUNXI_BOARDS_VERSION = 68acb3b1610a44b9402560623f7e35b7882585e9
+SUNXI_BOARDS_VERSION = a6a4e94066f4ec6b26fc7060dc453a6bcf26a504
 SUNXI_BOARDS_SITE = $(call github,linux-sunxi,sunxi-boards,$(SUNXI_BOARDS_VERSION))
 SUNXI_BOARDS_DEPENDENCIES = host-sunxi-tools
 SUNXI_BOARDS_INSTALL_IMAGES = YES
