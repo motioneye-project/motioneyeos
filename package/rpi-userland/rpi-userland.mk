41282cbdd62dd1299be50428e436e96c45f916eb
Yann E. MORIN <yann.morin.1998@free.fr>
2015-06-29 00:25:14 +0200

    package/rpi-userland: bump version
    Misc fixes and improvements all over the place...
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Floris Bos <bos@je-eigen-domein.nl>
    Cc: Pascal de Bruijn <pmjdebruijn@pcode.nl>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_USERLAND_VERSION = 8f542a1647e6f88f254eadd9ad6929301c81913b
+RPI_USERLAND_VERSION = b834074d0c0d9d7e64c133ab14ed691999cee990
 RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
 RPI_USERLAND_LICENSE = BSD-3c
 RPI_USERLAND_LICENSE_FILES = LICENCE
