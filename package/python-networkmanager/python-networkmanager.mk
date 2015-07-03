558caf4e05b06dcbdd209da9a69192c41785ea28
Petr Vorel <petr.vorel@gmail.com>
2015-07-03 06:22:12 +0200

    python-networkmanager: bump to version 0.9.13
    [Peter: fix upstream hash link]
    Signed-off-by: Petr Vorel <petr.vorel@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-networkmanager/python-networkmanager.mk
+++ package/python-networkmanager/python-networkmanager.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_NETWORKMANAGER_VERSION = 0.9.12
+PYTHON_NETWORKMANAGER_VERSION = 0.9.13
 PYTHON_NETWORKMANAGER_SITE = http://pypi.python.org/packages/source/p/python-networkmanager
 PYTHON_NETWORKMANAGER_SETUP_TYPE = distutils
 PYTHON_NETWORKMANAGER_LICENSE = GPLv3+
