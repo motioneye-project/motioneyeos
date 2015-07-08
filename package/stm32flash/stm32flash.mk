d503a13a7c4ec8e61f17610cc0768090fef079f7
Luca Ceresoli <luca@lucaceresoli.net>
2015-07-08 10:15:02 +0200

    stm32flash: new package
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Tested-by: Frank Hunleth <fhunleth@troodon-software.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/stm32flash/stm32flash.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# stm32flash
+#
+################################################################################
+
+STM32FLASH_VERSION = 4ac0566f188f39eace0223d966d349cd2f2c5895
+STM32FLASH_SITE = git://git.code.sf.net/p/stm32flash/code
+STM32FLASH_LICENSE = GPLv2+
+STM32FLASH_LICENSE_FILES = gpl-2.0.txt
+
+define STM32FLASH_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define STM32FLASH_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) DESTDIR="$(TARGET_DIR)" PREFIX="/usr" \
+		-C $(@D) install
+endef
+
+$(eval $(generic-package))
