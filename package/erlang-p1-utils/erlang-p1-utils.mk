226f7794943634fa3b1163d9fec6f942479dffd7
Johan Oudinet <johan.oudinet@gmail.com>
2015-07-08 11:54:17 +0200

    erlang-p1-utils: bump to version 1bf99f9
    Signed-off-by: Johan Oudinet <johan.oudinet@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/erlang-p1-utils/erlang-p1-utils.mk
+++ package/erlang-p1-utils/erlang-p1-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ERLANG_P1_UTILS_VERSION = 940f42d
+ERLANG_P1_UTILS_VERSION = 1bf99f9
 ERLANG_P1_UTILS_SITE = $(call github,processone,p1_utils,$(ERLANG_P1_UTILS_VERSION))
 ERLANG_P1_UTILS_LICENSE = GPLv2+
 ERLANG_P1_UTILS_LICENSE_FILES = COPYING
