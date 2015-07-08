3bb40c7d8608e1e55c19d00ab142c4fce14bd73e
Luca Ceresoli <luca@lucaceresoli.net>
2015-07-08 12:36:13 +0200

    snmppp: autoreconf is not needed anymore
    Tarballs of recent versions ship a configure script.
    
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/snmppp/snmppp.mk
+++ package/snmppp/snmppp.mk
@@ -9,8 +9,6 @@ SNMPPP_SOURCE = snmp++-$(SNMPPP_VERSION).tar.gz
 SNMPPP_SITE = http://www.agentpp.com/download
 SNMPPP_DEPENDENCIES = host-pkgconf
 SNMPPP_INSTALL_STAGING = YES
-# no configure script in tarball
-SNMPPP_AUTORECONF = YES
 SNMPPP_CONF_OPTS = $(if $(BR2_PACKAGE_SNMPPP_LOGGING),--enable-logging,--disable-logging)
 SNMPPP_LICENSE = SNMP++
 SNMPPP_LICENSE_FILES = src/v3.cpp
