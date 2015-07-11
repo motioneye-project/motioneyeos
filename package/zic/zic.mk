10a2b798a45ca13099091760a78408172e1164b2
Luca Ceresoli <luca@lucaceresoli.net>
2015-07-11 16:15:09 +0200

    zic: use <PKG>_STRIP_COMPONENTS
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Tested-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/zic/zic.mk
+++ package/zic/zic.mk
@@ -7,13 +7,8 @@
 ZIC_VERSION = 2015e
 ZIC_SOURCE = tzcode$(ZIC_VERSION).tar.gz
 ZIC_SITE = ftp://ftp.iana.org/tz/releases
+ZIC_STRIP_COMPONENTS = 0
 ZIC_LICENSE = Public domain
-
-# Don't strip any path components during extraction.
-define HOST_ZIC_EXTRACT_CMDS
-	gzip -d -c $(DL_DIR)/$(ZIC_SOURCE) \
-		| $(TAR) --strip-components=0 -C $(@D) -xf -
-endef
 
 define HOST_ZIC_BUILD_CMDS
 	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) zic
