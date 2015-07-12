5ec154fa413c991290a5661cc9bfc679ef997440
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2015-07-12 12:43:29 +0200

    c-icap: disable Perl support
    The Perl support in c-icap does not build properly, because it queries
    the system-installed Perl for cflags/ldflags, which returns things
    like -I/usr/local/include causing build failures since those are
    invalid paths when cross-compiling.
    
    Since there is no easy way to fix that without changing c-icap
    configure.in, let's disable the Perl support for now, until someone is
    interested enough to fix this up.
    
    Fixes:
    
      http://autobuild.buildroot.org/results/f60/f6011ab8211589fe8e1e8932db9f8be81768d3e6/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/c-icap/c-icap.mk
+++ package/c-icap/c-icap.mk
@@ -14,6 +14,7 @@ C_ICAP_CONFIG_SCRIPTS = c-icap-config c-icap-libicapapi-config
 C_ICAP_CONF_OPTS = \
 	--with-process-mutexes=posix \
 	--without-ldap \
+	--without-perl \
 	--enable-large-files \
 	--enable-ipv6
 
@@ -29,13 +30,6 @@ C_ICAP_CONF_OPTS += --with-bzip2
 C_ICAP_DEPENDENCIES += bzip2
 else
 C_ICAP_CONF_OPTS += --without-bzip2
-endif
-
-ifeq ($(BR2_PACKAGE_PERL),y)
-C_ICAP_CONF_OPTS += --with-perl
-C_ICAP_DEPENDENCIES += perl
-else
-C_ICAP_CONF_OPTS += --without-perl
 endif
 
 ifeq ($(BR2_PACKAGE_ZLIB),y)
