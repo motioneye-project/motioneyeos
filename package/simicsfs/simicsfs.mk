e5f4122c56debc43bba1d0e8085a2cd25541e546
Yann E. MORIN <yann.morin.1998@free.fr>
2015-07-12 02:21:40 +0200

    package/simicsfs: use kernel-module helper
    We were already using the Linux buildsystem to build simicsfs' modules,
    so switch to using the kernel-module infra instead.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/simicsfs/simicsfs.mk
+++ package/simicsfs/simicsfs.mk
@@ -8,14 +8,6 @@ SIMICSFS_VERSION = 1.18
 SIMICSFS_SITE = http://download.simics.net/pub
 SIMICSFS_LICENSE = GPLv2+
 SIMICSFS_LICENSE_FILES = hostfs.h
-SIMICSFS_DEPENDENCIES = linux
 
-define SIMICSFS_BUILD_CMDS
-	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules
-endef
-
-define SIMICSFS_INSTALL_TARGET_CMDS
-	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) INSTALL_MOD_PATH=$(TARGET_DIR) modules_install
-endef
-
+$(eval $(kernel-module))
 $(eval $(generic-package))
