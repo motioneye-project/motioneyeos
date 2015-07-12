54a42de966706b42aa6b9f9cabac64ebb69d17c0
Yann E. MORIN <yann.morin.1998@free.fr>
2015-07-12 02:21:37 +0200

    package/ocf-linux: use kernel-module helper
    We have a patch that makes ocf-linux' Makefile basically only call to
    the Linux buildsystem, with commands basically like:
        make -C $(LINUX_DIR) M=$(pwd) modules
    
    which is basically what our new kernel-module infra basically does.
    
    Remove our patch since it is basically no longer needed.
    
    Basically-Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ocf-linux/ocf-linux.mk
+++ package/ocf-linux/ocf-linux.mk
@@ -6,22 +6,19 @@
 
 OCF_LINUX_VERSION = 20120127
 OCF_LINUX_SITE = http://downloads.sourceforge.net/project/ocf-linux/ocf-linux/$(OCF_LINUX_VERSION)
-OCF_LINUX_DEPENDENCIES = linux
 OCF_LINUX_INSTALL_STAGING = YES
 
-define OCF_LINUX_BUILD_CMDS
-	$(MAKE) -C $(@D)/ocf $(LINUX_MAKE_FLAGS) KDIR=$(LINUX_DIR) \
-		ocf_modules
-endef
-
-define OCF_LINUX_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(@D)/ocf $(LINUX_MAKE_FLAGS) KDIR=$(LINUX_DIR) \
-		ocf_install
-endef
+OCF_LINUX_MODULE_SUBDIRS = ocf
+OCF_LINUX_MODULE_MAKE_OPTS = \
+	CONFIG_OCF_OCF=m \
+	CONFIG_OCF_CRYPTOSOFT=m \
+	CONFIG_OCF_BENCH=m \
+	CONFIG_OCF_OCFNULL=m
 
 define OCF_LINUX_INSTALL_STAGING_CMDS
 	$(INSTALL) -D -m 644 $(@D)/ocf/cryptodev.h \
 		$(STAGING_DIR)/usr/include/crypto/cryptodev.h
 endef
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
