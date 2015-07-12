7b5c2e658f3c35a6378a02736d1057ed1bef464b
Yann E. MORIN <yann.morin.1998@free.fr>
2015-07-12 02:21:39 +0200

    package/owl-linux: use kernel-module helper
    The owl-linux' Makefile is only calling the Linux buildsystem, so switch
    do using the new kernel-module infra.
    
    Get rid of first patch, no longer needed; rename remaining patch.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/owl-linux/owl-linux.mk
+++ package/owl-linux/owl-linux.mk
@@ -10,14 +10,5 @@ OWL_LINUX_LICENSE = PROPRIETARY
 OWL_LINUX_LICENSE_FILES = LICENSE
 OWL_LINUX_REDISTRIBUTE = NO
 
-OWL_LINUX_DEPENDENCIES = linux
-
-define OWL_LINUX_BUILD_CMDS
-	$(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR)
-endef
-
-define OWL_LINUX_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M="$(@D)" modules_install
-endef
-
+$(eval $(kernel-module))
 $(eval $(generic-package))
