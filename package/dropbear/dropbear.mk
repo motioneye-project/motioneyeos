e7d04dd2df8bb935c61f7c814ee88eba7e75b5e4
Yann E. MORIN <yann.morin.1998@free.fr>
2015-07-14 00:44:01 +0200

    package/dropbear: fix generating keys on RO file systems
    dropbear generates its keys at the first connection, and wants to save
    them in /etc/dropbear (not configurable).
    
    Currently, our /etc/dropbear is a directory.
    
    When the filesystem is read-only, dropbear can't save its keys, so
    refuses all connections.
    
    Fix that with:
    
      - at build time, create /etc/dropbear as a symlink to
        /var/run/dropbear
    
      - at runtime, if the filesystem is RW (we can rm /etc/dropbear),
        we replace the symlink with an actual directory; otherwise,
        when the filesystem is RO (we can't rm /etc/dropbear), we create
        /var/run/dropbear so the symlink points to an existing directory
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Arnout Vandecappelle <arnout@mind.be>
    Cc: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Acked-by: "Maxime Hadjinlian" <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/dropbear/dropbear.mk
+++ package/dropbear/dropbear.mk
@@ -93,7 +93,7 @@ define DROPBEAR_INSTALL_TARGET_CMDS
 	for f in $(DROPBEAR_TARGET_BINS); do \
 		ln -snf ../sbin/dropbear $(TARGET_DIR)/usr/bin/$$f ; \
 	done
-	mkdir -p $(TARGET_DIR)/etc/dropbear
+	ln -snf ../var/run/dropbear $(TARGET_DIR)/etc/dropbear
 endef
 
 $(eval $(autotools-package))
