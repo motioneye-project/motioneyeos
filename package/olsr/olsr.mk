ec9e34c9bb7c60e6fe71529553a87215501ea63a
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2015-07-14 12:15:55 +0200

    olsr: bump version
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/olsr/olsr.mk
+++ package/olsr/olsr.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-OLSR_VERSION_MAJOR = 0.6
-OLSR_VERSION = $(OLSR_VERSION_MAJOR).8
+OLSR_VERSION_MAJOR = 0.9
+OLSR_VERSION = $(OLSR_VERSION_MAJOR).0.2
 OLSR_SOURCE = olsrd-$(OLSR_VERSION).tar.bz2
 OLSR_SITE = http://www.olsr.org/releases/$(OLSR_VERSION_MAJOR)
 OLSR_PLUGINS = arprefresh bmf dot_draw dyn_gw dyn_gw_plain httpinfo jsoninfo \
