7697638c09e462b5ac3b006a464958547d6d0a57
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-07-15 20:11:43 +0200

    package/libinput: bump version to 0.19.0, add locale dependency
    Since 2015-04-22 libinput needs locale support:
    http://cgit.freedesktop.org/wayland/libinput/commit/src/libinput-util.c?id=5c671e0375cc03032fcac26efe4a7835a74c8bfc
    
    This patch adds the needed dependency also to
    weston and xdriver_xf86-input-libinput.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libinput/libinput.mk
+++ package/libinput/libinput.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBINPUT_VERSION = 0.14.1
+LIBINPUT_VERSION = 0.19.0
 LIBINPUT_SOURCE = libinput-$(LIBINPUT_VERSION).tar.xz
 LIBINPUT_SITE = http://www.freedesktop.org/software/libinput
 LIBINPUT_LICENSE = MIT
