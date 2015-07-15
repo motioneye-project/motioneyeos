81a2b4d67275ace8ee2894b289a76d0cd7777c7d
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-07-15 20:11:52 +0200

    package/x11r7/xdriver_xf86-video-ati: Re-wrap dependencies, no code change
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xdriver_xf86-video-ati/xdriver_xf86-video-ati.mk
+++ package/x11r7/xdriver_xf86-video-ati/xdriver_xf86-video-ati.mk
@@ -9,6 +9,16 @@ XDRIVER_XF86_VIDEO_ATI_SOURCE = xf86-video-ati-$(XDRIVER_XF86_VIDEO_ATI_VERSION)
 XDRIVER_XF86_VIDEO_ATI_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_ATI_LICENSE = MIT
 XDRIVER_XF86_VIDEO_ATI_LICENSE_FILES = COPYING
-XDRIVER_XF86_VIDEO_ATI_DEPENDENCIES = xserver_xorg-server libdrm xproto_fontsproto xproto_glproto xproto_randrproto xproto_videoproto xproto_xextproto xproto_xf86driproto xproto_xineramaproto xproto_xproto
+XDRIVER_XF86_VIDEO_ATI_DEPENDENCIES = \
+	libdrm \
+	xproto_fontsproto \
+	xproto_glproto \
+	xproto_randrproto \
+	xproto_videoproto \
+	xproto_xextproto \
+	xproto_xf86driproto \
+	xproto_xineramaproto \
+	xproto_xproto \
+	xserver_xorg-server
 
 $(eval $(autotools-package))
