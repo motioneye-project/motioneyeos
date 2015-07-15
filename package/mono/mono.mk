7b4c260ef3fb8798105e0e3549448fb4a1ea2f17
Angelo Compagnucci <angelo.compagnucci@gmail.com>
2015-07-15 12:51:26 +0200

    package/mono: bump to version 4.0.2.5
    This patch bumps mono to version 4.0.2.5
    
    Signed-off-by: Angelo Compagnucci <angelo.compagnucci@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/mono/mono.mk
+++ package/mono/mono.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MONO_VERSION = 4.0.0
+MONO_VERSION = 4.0.2.5
 MONO_SITE = http://download.mono-project.com/sources/mono
 MONO_SOURCE = mono-$(MONO_VERSION).tar.bz2
 MONO_LICENSE = GPLv2 or MIT (compiler, tools), LGPLv2 (runtime libs), MIT (class libs) or commercial
