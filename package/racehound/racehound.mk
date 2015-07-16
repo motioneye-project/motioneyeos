0ad6ee7f36904c756a8c6792581daf83ff91f8c3
Peter Seiderer <ps.report@gmx.net>
2015-07-16 23:24:12 +0200

    racehound: fix kernel module compile
    - check for linux kernel >= 3.14
    - add cross compile enviornment for linux kernel compile
    - add additional kernel config hint (CONFIG_KALLSYMS_ALL)
    - fix kernel module install path
    
    [Thomas:
     - fix Config.in dependency logic for the comment on the kernel
       version, as noticed by Baruch.
     - minor improvements in the .mk file.]
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/racehound/racehound.mk
+++ package/racehound/racehound.mk
@@ -12,10 +12,16 @@ RACEHOUND_SUPPORTS_IN_SOURCE_BUILD = NO
 
 RACEHOUND_DEPENDENCIES = elfutils linux
 
+# override auto detection (uses host parameters, not cross compile
+# ready)
 RACEHOUND_CONF_OPTS += \
 	-DKERNEL_VERSION_OK=YES \
 	-DMODULE_BUILD_SUPPORTED=YES \
 	-DKERNEL_CONFIG_OK=YES \
-	-DKBUILD_BUILD_DIR=$(LINUX_DIR)
+	-DKBUILD_BUILD_DIR=$(LINUX_DIR) \
+	-DKBUILD_VERSION_STRING=$(LINUX_VERSION_PROBED)
+
+# cross compile environment for linux kernel module
+RACEHOUND_MAKE_ENV = $(LINUX_MAKE_FLAGS)
 
 $(eval $(cmake-package))
