abf422ae0a523ac24bcd07aec6748bc9d2f706bc
Aurélien Chabot <aurelien@chabot.fr>
2015-07-18 00:30:19 +0200

    gptfdisk: bump version 1.0.0
    Signed-off-by: Aurélien Chabot <aurelien@chabot.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gptfdisk/gptfdisk.mk
+++ package/gptfdisk/gptfdisk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GPTFDISK_VERSION = 0.8.10
+GPTFDISK_VERSION = 1.0.0
 GPTFDISK_SITE = http://downloads.sourceforge.net/sourceforge/gptfdisk
 GPTFDISK_LICENSE = GPLv2+
 GPTFDISK_LICENSE_FILES = COPYING
