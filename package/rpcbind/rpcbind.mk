48dad0689e200c53352b94d380e7c5993b89688e
Aurélien Chabot <aurelien@chabot.fr>
2015-07-19 23:02:38 +0200

    rpcbind: bump version 0.2.3
    Refreshed patch:
    	0001-Remove-yellow-pages-support.patch
    
    Signed-off-by: Aurélien Chabot <aurelien@chabot.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/rpcbind/rpcbind.mk
+++ package/rpcbind/rpcbind.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPCBIND_VERSION = 0.2.1
+RPCBIND_VERSION = 0.2.3
 RPCBIND_SITE = http://downloads.sourceforge.net/project/rpcbind/rpcbind/$(RPCBIND_VERSION)
 RPCBIND_SOURCE = rpcbind-$(RPCBIND_VERSION).tar.bz2
 RPCBIND_LICENSE = BSD-3c
@@ -14,6 +14,7 @@ RPCBIND_CONF_ENV += \
 	CFLAGS="$(TARGET_CFLAGS) -I$(STAGING_DIR)/usr/include/tirpc/"
 RPCBIND_DEPENDENCIES += libtirpc host-pkgconf
 RPCBIND_CONF_OPTS += --with-rpcuser=root
+RPCBIND_CONF_OPTS += --with-systemdsystemunitdir=no
 
 define RPCBIND_INSTALL_INIT_SYSV
 	$(INSTALL) -m 0755 -D package/rpcbind/S30rpcbind \
