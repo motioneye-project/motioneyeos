0db9c4d197b19bb14f67deeeac495a85a4c28767
Aurélien Chabot <aurelien@chabot.fr>
2015-07-19 23:02:39 +0200

    rpcbind: add systemd support
    systemd files taken from Arch Linux.
    
    [Thomas:
     - fix indentation
     - indicate source of systemd files.]
    
    Signed-off-by: Aurélien Chabot <aurelien@chabot.fr>
    Acked-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/rpcbind/rpcbind.mk
+++ package/rpcbind/rpcbind.mk
@@ -14,11 +14,27 @@ RPCBIND_CONF_ENV += \
 	CFLAGS="$(TARGET_CFLAGS) `$(PKG_CONFIG_HOST_BINARY) --cflags libtirpc`"
 RPCBIND_DEPENDENCIES += libtirpc host-pkgconf
 RPCBIND_CONF_OPTS += --with-rpcuser=root
+
+ifeq ($(BR2_INIT_SYSTEMD),y)
+RPCBIND_CONF_OPTS += --with-systemdsystemunitdir=/usr/lib/systemd/system
+else
 RPCBIND_CONF_OPTS += --with-systemdsystemunitdir=no
+endif
+
+define RPCBIND_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -m 0755 -D package/rpcbind/rpcbind.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/rpcbind.service
+	$(INSTALL) -m 0755 -D package/rpcbind/rpcbind.socket \
+		$(TARGET_DIR)/usr/lib/systemd/system/rpcbind.socket
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/sockets.target.wants
+	ln -fs ../../../../usr/lib/systemd/system/rpcbind.socket \
+		$(TARGET_DIR)/etc/systemd/system/sockets.target.wants/rpcbind.socket
+endef
 
 define RPCBIND_INSTALL_INIT_SYSV
 	$(INSTALL) -m 0755 -D package/rpcbind/S30rpcbind \
 		$(TARGET_DIR)/etc/init.d/S30rpcbind
 endef
+
 
 $(eval $(autotools-package))
