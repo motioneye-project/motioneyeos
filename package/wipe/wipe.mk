1768b5f97426395d8cc2c2cc1de6c914e9fffd7b
Romain Naour <romain.naour@openwide.fr>
2015-07-20 19:47:51 +0200

    package/wipe: remove autoreconf
    A configure script is already bundled in the archive.
    There is no comment why autoreconf was added.
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/wipe/wipe.mk
+++ package/wipe/wipe.mk
@@ -7,7 +7,6 @@
 WIPE_VERSION = 2.3.1
 WIPE_SITE = http://downloads.sourceforge.net/project/wipe/wipe/$(WIPE_VERSION)
 WIPE_SOURCE = wipe-$(WIPE_VERSION).tar.bz2
-WIPE_AUTORECONF = YES
 WIPE_LICENSE = GPLv2+
 WIPE_LICENSE_FILES = LICENSE
 
