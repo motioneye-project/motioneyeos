ca607498a6bc96f55765c985bad46095143e3691
Romain Naour <romain.naour@openwide.fr>
2015-07-20 19:47:55 +0200

    package/libdvbsi: remove autoreconf
    A configure script is bundled with the archive
    of the current version.
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libdvbsi/libdvbsi.mk
+++ package/libdvbsi/libdvbsi.mk
@@ -11,7 +11,4 @@ LIBDVBSI_INSTALL_STAGING = YES
 LIBDVBSI_LICENSE = LGPLv2.1
 LIBDVBSI_LICENSE_FILES = COPYING
 
-# sometimes no Makefile is in the archive, just (re)generate
-LIBDVBSI_AUTORECONF = YES
-
 $(eval $(autotools-package))
