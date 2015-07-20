06ba02a936459cb023a3be893d5ae2cb2914ae66
Romain Naour <romain.naour@openwide.fr>
2015-07-20 19:47:50 +0200

    package/stress: remove autoreconf
    autoreconf is not needed anymore since non-lfs enabler patch
    has been removed.
    
    commit: 415765b5a8861f3b7e86e9786bf903a5a4d09e73
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/stress/stress.mk
+++ package/stress/stress.mk
@@ -8,7 +8,6 @@ STRESS_VERSION = 1.0.4
 STRESS_SITE = http://people.seas.harvard.edu/~apw/stress
 STRESS_LICENSE = GPLv2+
 STRESS_LICENSE_FILES = COPYING
-STRESS_AUTORECONF = YES
 
 # Stress is linked statically if the --enable-static is specified.
 # However, this option is always specified in the global
