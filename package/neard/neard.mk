a91e289630e6f1cee8b8b722151a7f6d8a50bb2d
Romain Naour <romain.naour@openwide.fr>
2015-07-20 19:47:52 +0200

    package/neard: remove autoreconf
    neard have been bumped several time since autoreconf
    was added in neard.mk.
    autoreconf seems not needed anymore.
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/neard/neard.mk
+++ package/neard/neard.mk
@@ -10,7 +10,6 @@ NEARD_SITE = $(BR2_KERNEL_MIRROR)/linux/network/nfc
 NEARD_LICENSE = GPLv2
 NEARD_LICENSE_FILES = COPYING
 
-NEARD_AUTORECONF = YES
 NEARD_DEPENDENCIES = host-pkgconf dbus libglib2 libnl
 NEARD_CONF_OPTS = --disable-traces
 
