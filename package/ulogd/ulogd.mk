65be5bc30d91f3418b7a1b66ba304be762f24574
Romain Naour <romain.naour@openwide.fr>
2015-07-20 19:47:49 +0200

    package/ulogd: remove autoreconf
    Autoreconf is not needed anymore since ulogd 2.0.4
    where logd-01-pthread-conditional.patch was removed.
    
    commit: 010745a955b6cf4c979588fafdb7506460038e0f
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ulogd/ulogd.mk
+++ package/ulogd/ulogd.mk
@@ -8,7 +8,6 @@ ULOGD_VERSION = 2.0.5
 ULOGD_SOURCE = ulogd-$(ULOGD_VERSION).tar.bz2
 ULOGD_SITE = http://www.netfilter.org/projects/ulogd/files
 ULOGD_CONF_OPTS = --with-dbi=no --with-pgsql=no
-ULOGD_AUTORECONF = YES
 ULOGD_DEPENDENCIES = host-pkgconf \
 	libmnl libnetfilter_acct libnetfilter_conntrack libnetfilter_log \
 	libnfnetlink
