2aa1f9c715c1aab3b7747f45fd62c1c97ba303cb
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-07-22 10:59:50 -0300

    netperf: bump to version 2.7.0
    Also add hash file.
    And work around gcc 5+ defaulting to gnu99, fixes:
    
    http://autobuild.buildroot.net/results/077/077714a0ee67057abcff680b244228a409243bbf/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/netperf/netperf.mk
+++ package/netperf/netperf.mk
@@ -4,9 +4,13 @@
 #
 ################################################################################
 
-NETPERF_VERSION = 2.6.0
+NETPERF_VERSION = 2.7.0
 NETPERF_SITE = ftp://ftp.netperf.org/netperf
-NETPERF_CONF_ENV = ac_cv_func_setpgrp_void=set
+NETPERF_SOURCE = netperf-$(NETPERF_VERSION).tar.bz2
+# gcc 5+ defaults to gnu99 which breaks netperf
+NETPERF_CONF_ENV = \
+	ac_cv_func_setpgrp_void=set \
+	CFLAGS="$(TARGET_CFLAGS) -std=gnu89"
 NETPERF_CONF_OPTS = --enable-demo=yes
 NETPERF_LICENSE = netperf license
 NETPERF_LICENSE_FILES = COPYING
