e5ce686d3012d8f6603b1b4b49325e283021e19b
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-07-22 22:30:30 +0200

    package/kodi-audioencoder-lame: bump version
    [Thomas:
      - also remove -DCMAKE_MODULE_PATH, no longer needed.]
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/kodi-audioencoder-lame/kodi-audioencoder-lame.mk
+++ package/kodi-audioencoder-lame/kodi-audioencoder-lame.mk
@@ -4,13 +4,12 @@
 #
 ################################################################################
 
-KODI_AUDIOENCODER_LAME_VERSION = 3eb59de951996659c143faaabacc5f34ee9a0b81
+KODI_AUDIOENCODER_LAME_VERSION = b283cd50cd2f89b7fd7b903c957a6a993e3756d2
 KODI_AUDIOENCODER_LAME_SITE = $(call github,xbmc,audioencoder.lame,$(KODI_AUDIOENCODER_LAME_VERSION))
 KODI_AUDIOENCODER_LAME_LICENSE = GPLv2+
 KODI_AUDIOENCODER_LAME_LICENSE_FILES = src/EncoderLame.cpp
 KODI_AUDIOENCODER_LAME_DEPENDENCIES = kodi lame
 KODI_AUDIOENCODER_LAME_CONF_OPTS += \
-	-DCMAKE_MODULE_PATH=$(STAGING_DIR)/usr/lib/kodi \
 	-DLAME_INCLUDE_DIRS=$(STAGING_DIR)/usr/include
 
 $(eval $(cmake-package))
