cfefcd859f2b51a6ebe23c5be149ff258755e249
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-07-22 22:30:25 +0200

    package/libcec: bump to version 3.0.1
    - reflect upstream changes of the build system by switching from
      an autotools-package to cmake-package
    
    [Thomas:
      - remove the gcc 4.5 dependency. While we do need a dependency on
        the gcc version, the proposed implementation was not correct as it
        wasn't handling external toolchains.
      - add a patch to fix a problem caused by libcec build system trying
        to use 'git' to get a version number, which does not work when the
        version control system information is not available, as is the
        case in Buildroot.]
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libcec/libcec.mk
+++ package/libcec/libcec.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBCEC_VERSION = libcec-2.2.0-repack
+LIBCEC_VERSION = libcec-3.0.1
 LIBCEC_SITE = $(call github,Pulse-Eight,libcec,$(LIBCEC_VERSION))
 LIBCEC_LICENSE = GPLv2+
 LIBCEC_LICENSE_FILES = COPYING
@@ -12,7 +12,7 @@ LIBCEC_LICENSE_FILES = COPYING
 # Autoreconf required due to being a dev tarball and not a release tarball.
 LIBCEC_AUTORECONF = YES
 LIBCEC_INSTALL_STAGING = YES
-LIBCEC_DEPENDENCIES = host-pkgconf
+LIBCEC_DEPENDENCIES = host-pkgconf libplatform
 
 ifeq ($(BR2_PACKAGE_LOCKDEV),y)
 LIBCEC_DEPENDENCIES += lockdev
@@ -22,13 +22,20 @@ ifeq ($(BR2_PACKAGE_HAS_UDEV),y)
 LIBCEC_DEPENDENCIES += udev
 endif
 
-ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
-LIBCEC_CONF_OPTS = --enable-rpi \
-	--with-rpi-include-path=$(STAGING_DIR)/usr/include
-LIBCEC_DEPENDENCIES += rpi-userland
-LIBCEC_CONF_ENV += LIBS="-lvcos -lvchostif"
-else
-LIBCEC_CONF_OPTS = --disable-rpi
+ifeq ($(BR2_PACKAGE_PYTHON)$(BR2_PACKAGE_PYTHON3),y)
+LIBCEC_DEPENDENCIES += host-swig $(if $(BR2_PACKAGE_PYTHON3),python3,python)
 endif
 
-$(eval $(autotools-package))
+ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
+LIBCEC_DEPENDENCIES += rpi-userland
+LIBCEC_CONF_OPTS += \
+	-DCMAKE_C_FLAGS="-lvcos -lvchiq_arm" \
+	-DCMAKE_CXX_FLAGS="-I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux \
+		-I$(STAGING_DIR)/usr/include/interface/vcos/pthreads"
+endif
+
+ifeq ($(BR2_PACKAGE_XLIB_LIBXRANDR),y)
+LIBCEC_DEPENDENCIES += xlib_libXrandr
+endif
+
+$(eval $(cmake-package))
