2cc1d73ef5ee8be1b24477ace16542a1d6106db0
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-07-22 22:30:32 +0200

    package/kodi-audioencoder-wav: bump version
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/kodi-audioencoder-wav/kodi-audioencoder-wav.mk
+++ package/kodi-audioencoder-wav/kodi-audioencoder-wav.mk
@@ -4,12 +4,10 @@
 #
 ################################################################################
 
-KODI_AUDIOENCODER_WAV_VERSION = 40aaedfa1cd9c75749c82f6e1bd7c42ef61fdb38
+KODI_AUDIOENCODER_WAV_VERSION = 797c990eea7851889cd44a597c0392deba9c51c0
 KODI_AUDIOENCODER_WAV_SITE = $(call github,xbmc,audioencoder.wav,$(KODI_AUDIOENCODER_WAV_VERSION))
 KODI_AUDIOENCODER_WAV_LICENSE = GPLv2+
 KODI_AUDIOENCODER_WAV_LICENSE_FILES = src/EncoderWav.cpp
 KODI_AUDIOENCODER_WAV_DEPENDENCIES = kodi
-KODI_AUDIOENCODER_WAV_CONF_OPTS += \
-	-DCMAKE_MODULE_PATH=$(STAGING_DIR)/usr/lib/kodi
 
 $(eval $(cmake-package))
