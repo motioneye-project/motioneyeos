eeb210d8f31c5c9e368378010c1a829e931183da
Francois Perrad <fperrad@gmail.com>
2015-07-24 16:38:47 +0200

    perl-mojolicious: bump to version 6.14
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/perl-mojolicious/perl-mojolicious.mk
+++ package/perl-mojolicious/perl-mojolicious.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_MOJOLICIOUS_VERSION = 6.08
+PERL_MOJOLICIOUS_VERSION = 6.14
 PERL_MOJOLICIOUS_SOURCE = Mojolicious-$(PERL_MOJOLICIOUS_VERSION).tar.gz
 PERL_MOJOLICIOUS_SITE = $(BR2_CPAN_MIRROR)/authors/id/S/SR/SRI
 PERL_MOJOLICIOUS_DEPENDENCIES = perl
