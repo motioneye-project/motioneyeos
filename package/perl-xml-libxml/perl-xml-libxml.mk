70b0640a2b72f6160244bdc1ae2b060367cd2a41
Francois Perrad <fperrad@gmail.com>
2015-07-24 16:38:51 +0200

    perl-xml-libxml: bump to version 2.0121
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/perl-xml-libxml/perl-xml-libxml.mk
+++ package/perl-xml-libxml/perl-xml-libxml.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_XML_LIBXML_VERSION = 2.0118
+PERL_XML_LIBXML_VERSION = 2.0121
 PERL_XML_LIBXML_SOURCE = XML-LibXML-$(PERL_XML_LIBXML_VERSION).tar.gz
 PERL_XML_LIBXML_SITE = $(BR2_CPAN_MIRROR)/authors/id/S/SH/SHLOMIF
 PERL_XML_LIBXML_DEPENDENCIES = perl zlib libxml2 perl-xml-sax perl-xml-namespacesupport
