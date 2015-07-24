889a4a8d4f609f9e3c33afc8953d045255ef2b54
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-07-24 20:38:27 +0200

    package/minidlna: Fix static compile
    Fixes
    http://autobuild.buildroot.net/results/aeb/aeb5481526497a01ada8ee9403f282a2374b0383/
    http://autobuild.buildroot.net/results/255/25536df514477f3210caf4af27f2f107683f7fb2/
    http://autobuild.buildroot.net/results/fa1/fa11cb360190b02d08e74997b0abc73079760dfa/
    and many others
    
    This is a follow-up for these patches:
    http://patchwork.ozlabs.org/patch/493360/
    http://patchwork.ozlabs.org/patch/416007/
    
    [Thomas:
      - don't pass --static to pkg-config, since our pkg-config wrapper
        already passes it automatically when BR2_STATIC_LIBS=y.
      - make only one call to pkg-config for all three libraries
      - put the libintl logic separately from the definition of
        MINIDLNA_CONF_ENV.]
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/minidlna/minidlna.mk
+++ package/minidlna/minidlna.mk
@@ -17,8 +17,9 @@ MINIDLNA_DEPENDENCIES = \
 ifeq ($(BR2_STATIC_LIBS),y)
 # the configure script / Makefile forgets to link with some of the dependent
 # libraries breaking static linking, so help it along
-MINIDLNA_CONF_ENV = \
-	LIBS='-lavformat -lavcodec -lavutil -logg -lz -lpthread -lm'
+MINIDLNA_STATIC_LIBS += `$(PKG_CONFIG_HOST_BINARY) --libs libavcodec libexif libvorbis`
+MINIDLNA_STATIC_LIBS += $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),-lintl)
+MINIDLNA_CONF_ENV += LIBS="$(MINIDLNA_STATIC_LIBS)"
 else
 MINIDLNA_CONF_OPTS = \
 	--disable-static
