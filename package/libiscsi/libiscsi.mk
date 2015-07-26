64295d38712ef2aa7cda7a94837d085f63a550f1
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2015-07-26 22:38:32 +0200

    libiscsi: Bump version
    Patches were applied upstream so they are removed. Also remove the
    autoreconf which has no purposes anymore
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libiscsi/libiscsi.mk
+++ package/libiscsi/libiscsi.mk
@@ -4,13 +4,10 @@
 #
 ################################################################################
 
-LIBISCSI_VERSION = 1.12.0
+LIBISCSI_VERSION = 1.15.0
 LIBISCSI_SITE = https://sites.google.com/site/libiscsitarballs/libiscsitarballs
 LIBISCSI_LICENSE = GPLv2+ LGPLv2.1+
 LIBISCSI_LICENSE_FILES = COPYING LICENCE-GPL-2.txt LICENCE-LGPL-2.1.txt
 LIBISCSI_INSTALL_STAGING = YES
-
-# We patch configure.ac and Makefile.am
-LIBISCSI_AUTORECONF = YES
 
 $(eval $(autotools-package))
