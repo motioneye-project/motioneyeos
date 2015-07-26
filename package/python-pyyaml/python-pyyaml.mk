f098adb66cf744b8e3700990f5ee9857483e010f
Geoffrey Ragot <geoffreyragot@gmail.com>
2015-07-26 14:12:17 +0200

    python-pyyaml: new package
    [Thomas:
      - use 'select' rather than 'depends on' for the yaml library
        dependency
      - add hash file.
      - add upstream URL in Config.in help text
      - rework commit title.]
    
    Signed-off-by: Geoffrey Ragot <geoffrey.ragot@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/python-pyyaml/python-pyyaml.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-pyyaml
+#
+################################################################################
+
+PYTHON_PYYAML_VERSION = 3.11
+PYTHON_PYYAML_SOURCE = PyYAML-$(PYTHON_PYYAML_VERSION).tar.gz
+PYTHON_PYYAML_SITE = https://pypi.python.org/packages/source/P/PyYAML/
+PYTHON_PYYAML_SETUP_TYPE = distutils
+PYTHON_PYYAML_LICENSE = Python software foundation license v2
+PYTHON_PYYAML_LICENSE_FILES = LICENSE
+PYTHON_PYYAML_DEPENDENCIES = libyaml
+
+$(eval $(python-package))
