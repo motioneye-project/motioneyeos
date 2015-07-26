2b7edd7756cac51a04cf90799bd31e08e1b4b01f
Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
2015-07-26 20:45:14 +0200

    conntrack-tools: use pkg-config for libtirpc flags
    Instead of hardcoding the flags needed for libtirpc, use pkg-config.
    
    Signed-off-by: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/conntrack-tools/conntrack-tools.mk
+++ package/conntrack-tools/conntrack-tools.mk
@@ -15,8 +15,8 @@ CONNTRACK_TOOLS_LICENSE_FILES = COPYING
 
 ifeq ($(BR2_PACKAGE_LIBTIRPC),y)
 CONNTRACK_TOOLS_CONF_ENV += \
-	CFLAGS="$(TARGET_CFLAGS) -I$(STAGING_DIR)/usr/include/tirpc/"
-CONNTRACK_TOOLS_DEPENDENCIES += libtirpc
+	CFLAGS="$(TARGET_CFLAGS) `$(PKG_CONFIG_HOST_BINARY) --cflags libtirpc`"
+CONNTRACK_TOOLS_DEPENDENCIES += libtirpc host-pkgconf
 endif
 
 $(eval $(autotools-package))
