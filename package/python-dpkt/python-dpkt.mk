2f38b397c886472169fdb1ad8594024eb7cc538c
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2015-07-26 22:38:36 +0200

    python-dpkt: Bump version and change site
    Google Code is a closing forge, python-dpkt has already made the move to
    Github.
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-dpkt/python-dpkt.mk
+++ package/python-dpkt/python-dpkt.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-PYTHON_DPKT_VERSION = 1.7
-PYTHON_DPKT_SOURCE = dpkt-$(PYTHON_DPKT_VERSION).tar.gz
-PYTHON_DPKT_SITE = http://dpkt.googlecode.com/files
+PYTHON_DPKT_VERSION = 1.8.r98
+PYTHON_DPKT_SITE = $(call github,kbandla,dpkt,$(PYTHON_DPKT_VERSION))
 PYTHON_DPKT_SETUP_TYPE = distutils
 PYTHON_DPKT_LICENSE = BSD-3c
 PYTHON_DPKT_LICENSE_FILES = LICENSE
