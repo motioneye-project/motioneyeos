cb582b4614705c32c4025dce4b6cf0090691d73b
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2015-07-26 22:38:30 +0200

    explorercanvas: Bump version and change site
    Google Code is a closing forge, libass has already made the move to
    Github.
    
    This package has no release yet, so we are using a commit.
    Since the tarball is generated by Github, a hash is useless here.
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/explorercanvas/explorercanvas.mk
+++ package/explorercanvas/explorercanvas.mk
@@ -4,19 +4,13 @@
 #
 ################################################################################
 
-EXPLORERCANVAS_VERSION = r3
-EXPLORERCANVAS_SITE = http://explorercanvas.googlecode.com/files
-EXPLORERCANVAS_SOURCE = excanvas_$(EXPLORERCANVAS_VERSION).zip
+EXPLORERCANVAS_VERSION = aa989ea9d9bac748638f7c66b0fc88e619715da6
+EXPLORERCANVAS_SITE = $(call github,arv,ExplorerCanvas,$(EXPLORERCANVAS_VERSION))
 EXPLORERCANVAS_LICENSE = Apache-2.0
 EXPLORERCANVAS_LICENSE_FILES = COPYING
 
-define EXPLORERCANVAS_EXTRACT_CMDS
-	$(UNZIP) -d $(@D) $(DL_DIR)/$(EXPLORERCANVAS_SOURCE)
-endef
-
 define EXPLORERCANVAS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/excanvas.compiled.js \
-		$(TARGET_DIR)/var/www/excanvas.js
+	$(INSTALL) -D $(@D)/excanvas.js $(TARGET_DIR)/var/www/excanvas.js
 endef
 
 $(eval $(generic-package))
