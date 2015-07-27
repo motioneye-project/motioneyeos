f24bd7273dcec0876779a6c3949a3e5e64d44712
kyak <bas@bmail.ru>
2015-07-27 17:46:49 +0300

    xterm: x-includes and x-libraries must be set for cross-compiling
    These flags need to be set so that the configure script would
    correctly use libICE from cross-toolchain rather than from host.
    
    This fix is similar to "package/efl/libevas: x-includes and x-libraries
    must be set for cross-compiling" done by Romain Naour on libecore.
    
    Signed-off-by: Mikhail Peselnik <bas@bmail.ru>
    Reviewed-by: Romain Naour <romain.naour@openwide.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/xterm/xterm.mk
+++ package/xterm/xterm.mk
@@ -10,6 +10,8 @@ XTERM_SITE = ftp://invisible-island.net/xterm
 XTERM_DEPENDENCIES = ncurses xlib_libXaw
 XTERM_LICENSE = MIT
 XTERM_LICENSE_FILES = version.c
-XTERM_CONF_OPTS = --enable-256-color
+XTERM_CONF_OPTS = --enable-256-color \
+	--x-includes=$(STAGING_DIR)/usr/include \
+	--x-libraries=$(STAGING_DIR)/usr/lib
 
 $(eval $(autotools-package))
