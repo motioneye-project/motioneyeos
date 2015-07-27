a815e1cb464eb475b7f52b42007adbd32cf43673
kyak <bas@bmail.ru>
2015-07-27 17:46:50 +0300

    package/xterm: compile with libXft support when it selected
    When libXft (FreeType) library is selected, xterm will use it.
    host-pkgcong is used to search for libXft during configure, so it
    is in the dependencies.
    
    Signed-off-by: Mikhail Peselnik <bas@bmail.ru>
    Reviewed-by: Romain Naour <romain.naour@openwide.fr>
    Tested-by: Romain Naour <romain.naour@openwide.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/xterm/xterm.mk
+++ package/xterm/xterm.mk
@@ -7,11 +7,19 @@
 XTERM_VERSION = 314
 XTERM_SOURCE = xterm-$(XTERM_VERSION).tgz
 XTERM_SITE = ftp://invisible-island.net/xterm
-XTERM_DEPENDENCIES = ncurses xlib_libXaw
+XTERM_DEPENDENCIES = ncurses xlib_libXaw host-pkgconf
 XTERM_LICENSE = MIT
 XTERM_LICENSE_FILES = version.c
 XTERM_CONF_OPTS = --enable-256-color \
 	--x-includes=$(STAGING_DIR)/usr/include \
 	--x-libraries=$(STAGING_DIR)/usr/lib
+
+ifeq ($(BR2_PACKAGE_XLIB_LIBXFT),y)
+XTERM_DEPENDENCIES += xlib_libXft
+XTERM_CONF_OPTS += --enable-freetype \
+	--with-freetype-config=auto
+else
+XTERM_CONF_OPTS += --disable-freetype
+endif
 
 $(eval $(autotools-package))
