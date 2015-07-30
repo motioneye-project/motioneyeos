00e7d4089a72bfe1eb5294edb5c7f43a772902d5
Scott Fan <fancp2007@gmail.com>
2015-07-31 00:59:07 +0200

    xdriver_xf86-video-fbturbo: new package
    Add the fbturbo video driver, which is based on xf86-video-fbdev (with
    none of the original features stripped), primarily optimized for the
    devices powered by the Allwinner SoC (A10, A13, A20).
    
    https://github.com/ssvb/xf86-video-fbturbo/
    
    Signed-off-by: Scott Fan <fancp2007@gmail.com>
    [yann.morin.1998@free.fr: strip help text to the basics only]
    [Peter: needs libdrm/pixman, add patch to drop libdri2 dependency
    	reformat and add optional libpciaccess support]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/x11r7/xdriver_xf86-video-fbturbo/xdriver_xf86-video-fbturbo.mk
@@ -0,0 +1,35 @@
+################################################################################
+#
+# xdriver_xf86-video-fbturbo
+#
+################################################################################
+
+XDRIVER_XF86_VIDEO_FBTURBO_VERSION = 0.4.0
+XDRIVER_XF86_VIDEO_FBTURBO_SITE = $(call github,ssvb,xf86-video-fbturbo,$(XDRIVER_XF86_VIDEO_FBTURBO_VERSION))
+XDRIVER_XF86_VIDEO_FBTURBO_LICENSE = MIT
+XDRIVER_XF86_VIDEO_FBTURBO_LICENSE_FILES = COPYING
+XDRIVER_XF86_VIDEO_FBTURBO_DEPENDENCIES = \
+	xserver_xorg-server \
+	libdrm \
+	pixman \
+	xproto_fontsproto \
+	xproto_randrproto \
+	xproto_renderproto \
+	xproto_videoproto \
+	xproto_xproto \
+	xproto_xf86driproto
+
+ifeq ($(BR2_PACKAGE_LIBPCIACCESS),y)
+XDRIVER_XF86_VIDEO_FBTURBO_DEPENDENCIES += libpciaccess
+XDRIVER_XF86_VIDEO_FBTURBO_CONF_OPTS += --enable-pciaccess
+else
+XDRIVER_XF86_VIDEO_FBTURBO_CONF_OPTS += --disable-pciaccess
+endif
+
+define XDRIVER_XF86_VIDEO_FBTURBO_INSTALL_CONF_FILE
+        $(INSTALL) -m 0644 -D $(@D)/xorg.conf $(TARGET_DIR)/etc/X11/xorg.conf
+endef
+
+XDRIVER_XF86_VIDEO_FBTURBO_POST_INSTALL_TARGET_HOOKS += XDRIVER_XF86_VIDEO_FBTURBO_INSTALL_CONF_FILE
+
+$(eval $(autotools-package))
