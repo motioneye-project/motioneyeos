855bb15aa1d646e70b5bdb200017021d39ea5334
Alexey Brodkin <Alexey.Brodkin@synopsys.com>
2015-08-10 14:24:50 +0300

    libsodium: disable PIE on ARC
    Even though ARC gcc understands "-pie" option and attempts to generate
    PIE binaries as of today PIE is not really supported for user-space
    applications.
    
    So we disable PIE detection if building for ARC.
    That fixes http://autobuild.buildroot.net/results/be4/be46b621ce5443788b0a1bc9fab614c4ca5d0859
    and also prevents execution of non-supported PIE binary in runtime.
    
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libsodium/libsodium.mk
+++ package/libsodium/libsodium.mk
@@ -10,4 +10,8 @@ LIBSODIUM_LICENSE = ISC
 LIBSODIUM_LICENSE_FILES = LICENSE
 LIBSODIUM_INSTALL_STAGING = YES
 
+ifeq ($(BR2_arc),y)
+LIBSODIUM_CONF_OPTS += --disable-pie
+endif
+
 $(eval $(autotools-package))
