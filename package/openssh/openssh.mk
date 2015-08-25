0916daeff1056f930736bd4b644aff986b8eb88a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-08-25 15:53:59 -0300

    openssh: security bump to version 7.1p1
    Fixes:
    CVE-2015-6563 - Fixed a privilege separation weakness related to PAM
    support.
    CVE-2015-6564 - Fixed a use-after-free bug related to PAM support that
    was reachable by attackers who could compromise the pre-authentication
    process for remote code exectuion.
    CVE-2015-6565 - incorrectly set TTYs to be world-writable.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENSSH_VERSION = 6.9p1
+OPENSSH_VERSION = 7.1p1
 OPENSSH_SITE = http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 OPENSSH_LICENSE = BSD-3c BSD-2c Public Domain
 OPENSSH_LICENSE_FILES = LICENCE
