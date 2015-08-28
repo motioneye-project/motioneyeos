0d83de383224c65ec250a005130b1d3cc6fd6d76
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2015-08-28 14:59:51 +0200

    scrypt: commit packaging improvements
    The changes brought by this commit should have been part of the
    previous commit, but were not due to a mistake. This commit implements
    the following changes to the scrypt package:
    
       - move from "Libraries -> Crypto" to "System tools", since it only
         installs one binary, scrypt, and no library.
       - bump from 1.1.6 to 1.2.0
       - add comment in the .hash file explaining where the hash is coming
         from.
       - add missing dependency on OpenSSL
       - use BSD-2c as the license code, instead of BSD2
       - use 'main.c' as the license file.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/scrypt/scrypt.mk
+++ package/scrypt/scrypt.mk
@@ -4,10 +4,12 @@
 #
 ################################################################################
 
-SCRYPT_VERSION = 1.1.6
+SCRYPT_VERSION = 1.2.0
 SCRYPT_SOURCE = scrypt-$(SCRYPT_VERSION).tgz
 SCRYPT_SITE = http://www.tarsnap.com/scrypt
-SCRYPT_LICENSE = BSD2
+SCRYPT_LICENSE = BSD-2c
+SCRYPT_LICENSE_FILES = main.c
+SCRYPT_DEPENDENCIES = openssl
 SCRYPT_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
