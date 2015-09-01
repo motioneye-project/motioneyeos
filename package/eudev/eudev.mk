1037475b9b9e1ec8092e4f90c3e33f9c485867ff
Romain Naour <romain.naour@openwide.fr>
2015-09-01 13:39:16 +0200

    package/eudev: fix install command
    The directory $(TARGET_DIR)/etc/init.d/ must exist before installing
    S10udev init script.
    Add the missing "-D" option to create the "init.d" directory.
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/eudev/eudev.mk
+++ package/eudev/eudev.mk
@@ -57,7 +57,7 @@ EUDEV_CONF_OPTS += --disable-selinux
 endif
 
 define EUDEV_INSTALL_INIT_SYSV
-	$(INSTALL) -m 0755 package/eudev/S10udev $(TARGET_DIR)/etc/init.d/S10udev
+	$(INSTALL) -D -m 0755 package/eudev/S10udev $(TARGET_DIR)/etc/init.d/S10udev
 endef
 
 # Required by default rules for input devices
