46a615bb6439a4fe69f1bfc6a347852b993d46a2
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2015-09-02 15:21:19 +0100

    udisks: bump to version 1.0.5
    This version depends on libgudev when using systemd, otherwise it fails
    with an error like this one:
    
    checking for GUDEV... no
    configure: error: Package requirements (gudev-1.0 >= 147) were not met:
    
    [Peter: add to _DEPENDENCIES, not _CONF_OPTS]
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/udisks/udisks.mk
+++ package/udisks/udisks.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UDISKS_VERSION = 1.0.4
+UDISKS_VERSION = 1.0.5
 UDISKS_SITE = http://hal.freedesktop.org/releases
 UDISKS_LICENSE = GPLv2+
 UDISKS_LICENSE_FILES = COPYING
@@ -21,6 +21,15 @@ UDISKS_DEPENDENCIES = 	\
 	libatasmart
 
 UDISKS_CONF_OPTS = --disable-remote-access --disable-man-pages
+
+# When eudev is used as the udev provider, libgudev is automatically
+# provided as it is part of eudev. However, when systemd is used as the
+# udev provider, libgudev is not provided, and needs to be built
+# separately. This is why we select the libgudev package only if systemd
+# is used.
+ifeq ($(BR2_PACKAGE_SYSTEMD),y)
+UDISKS_DEPENDENCIES += libgudev
+endif
 
 ifeq ($(BR2_PACKAGE_UDISKS_LVM2),y)
 UDISKS_CONF_OPTS += --enable-lvm2
