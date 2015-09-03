0ac29e66148b64ff462e48ab85ffa5cebc7b732e
Anders Roxell <anders.roxell@linaro.org>
2015-09-03 11:01:56 +0200

    package/ltp-testsuite: enable realtime tests
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>
    Reviewed-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Tested-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ltp-testsuite/ltp-testsuite.mk
+++ package/ltp-testsuite/ltp-testsuite.mk
@@ -9,6 +9,8 @@ LTP_TESTSUITE_SOURCE = ltp-full-$(LTP_TESTSUITE_VERSION).tar.xz
 LTP_TESTSUITE_SITE = http://downloads.sourceforge.net/project/ltp/LTP%20Source/ltp-$(LTP_TESTSUITE_VERSION)
 LTP_TESTSUITE_LICENSE = GPLv2 GPLv2+
 LTP_TESTSUITE_LICENSE_FILES = COPYING
+LTP_TESTSUITE_CONF_OPTS += \
+			   --with-realtime-testsuite
 
 # Needs libcap with file attrs which needs attr, so both required
 ifeq ($(BR2_PACKAGE_LIBCAP)$(BR2_PACKAGE_ATTR),yy)
