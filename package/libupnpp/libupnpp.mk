637b209268ad398275c7d673911f2b37fafee4eb
Jörg Krause <joerg.krause@embedded.rocks>
2015-09-07 17:35:16 +0200

    package/libupnpp: bump to version 0.12.1
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libupnpp/libupnpp.mk
+++ package/libupnpp/libupnpp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUPNPP_VERSION = 0.11.0
+LIBUPNPP_VERSION = 0.12.1
 LIBUPNPP_SITE = http://www.lesbonscomptes.com/upmpdcli/downloads
 LIBUPNPP_LICENSE = GPLv2+
 LIBUPNPP_LICENSE_FILES = COPYING
