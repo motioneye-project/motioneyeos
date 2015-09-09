92122338c3d11a52e4ee7e0a92204d189ece9eb8
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2015-09-09 15:13:21 +0100

    ffmpeg: bump to version 2.8
    0016-ranlib.patch is not needed anymore since the problem that it fixes
    is already handled upstream:
    
      https://github.com/FFmpeg/FFmpeg/commit/2c77ca459e9c9aceee5ed93f8dd8de09e9fbf8df
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ffmpeg/ffmpeg.mk
+++ package/ffmpeg/ffmpeg.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FFMPEG_VERSION = 2.7.2
+FFMPEG_VERSION = 2.8
 FFMPEG_SOURCE = ffmpeg-$(FFMPEG_VERSION).tar.bz2
 FFMPEG_SITE = http://ffmpeg.org/releases
 FFMPEG_INSTALL_STAGING = YES
