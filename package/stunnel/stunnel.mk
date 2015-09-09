7244dea0a5ca788c9382a226651145cb240c30ae
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-09-09 16:19:34 -0300

    stunnel: bump to version 5.23
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/stunnel/stunnel.mk
+++ package/stunnel/stunnel.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 STUNNEL_VERSION_MAJOR = 5
-STUNNEL_VERSION = $(STUNNEL_VERSION_MAJOR).22
+STUNNEL_VERSION = $(STUNNEL_VERSION_MAJOR).23
 STUNNEL_SITE = http://www.usenix.org.uk/mirrors/stunnel/archive/$(STUNNEL_VERSION_MAJOR).x
 STUNNEL_DEPENDENCIES = openssl
 STUNNEL_CONF_OPTS = --with-ssl=$(STAGING_DIR)/usr --with-threads=fork \
