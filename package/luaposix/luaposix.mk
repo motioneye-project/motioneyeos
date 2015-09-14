d54751e2d451cf2737971bd901e88501349d0dd6
Jörg Krause <joerg.krause@embedded.rocks>
2015-09-15 01:22:39 +0200

    package/luaposix: add patch to fix musl compatibility
    Backport patch from upstream to fix musl build error:
    
    ```
    In file included from ext/posix/posix.c:25:0:
    ext/posix/sched.c: In function 'Psched_setscheduler':
    ext/posix/sched.c:74:9: error: variable 'sched_param' has initializer but incomplete type
      struct sched_param sched_param = {0};
    ```
    
    Fixed upstream by commit 11cc8a2973569ec7fb1e7c2466dca0282944b124.
    
    [Thomas: add missing <pkg>_AUTORECONF=YES.]
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/luaposix/luaposix.mk
+++ package/luaposix/luaposix.mk
@@ -9,6 +9,8 @@ LUAPOSIX_SITE = $(call github,luaposix,luaposix,release-v$(LUAPOSIX_VERSION))
 LUAPOSIX_LICENSE = MIT
 LUAPOSIX_LICENSE_FILES = COPYING
 LUAPOSIX_DEPENDENCIES = luainterpreter host-lua ncurses
+# 0001-sched-workaround-glibc-_POSIX_PRIORITY_SCHEDULING-bu.patch
+LUAPOSIX_AUTORECONF = YES
 LUAPOSIX_CONF_OPTS = --libdir="/usr/lib/lua/$(LUAINTERPRETER_ABIVER)" --datarootdir="/usr/share/lua/$(LUAINTERPRETER_ABIVER)"
 
 $(eval $(autotools-package))
