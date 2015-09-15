0ae594247be21ce2836b774123eac68419e8e8e8
Ben Shelton <benjamin.h.shelton@intel.com>
2015-09-15 09:35:18 -0500

    liburcu: Add support for aarch64
    Bring over a patch from OpenEmbedded to treat aarch64 like arm in the build
    process, which allows the build to succeed.
    
    Signed-off-by: Ben Shelton <benjamin.h.shelton@intel.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/liburcu/liburcu.mk
+++ package/liburcu/liburcu.mk
@@ -10,6 +10,7 @@ LIBURCU_SOURCE = userspace-rcu-$(LIBURCU_VERSION).tar.bz2
 LIBURCU_LICENSE = LGPLv2.1+ for the library; MIT-like license for few source files listed in LICENSE; GPLv2+ for test; GPLv3 for few *.m4 files
 LIBURCU_LICENSE_FILES = lgpl-2.1.txt lgpl-relicensing.txt gpl-2.0.txt LICENSE
 
+LIBURCU_AUTORECONF = YES
 LIBURCU_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
