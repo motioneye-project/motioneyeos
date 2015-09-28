1bed212e46b4638ef54eb5d26fcce4c0ac0278ca
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-09-28 21:27:56 +0200

    package/libbluray: bump version
    Kodi 16.x-Jarvis depends on libbluray > 0.7.0
    https://github.com/xbmc/xbmc/commit/329190a0323e332dd9fa9ad14fcbf3b2d92afe77
    
    Also enable UDF support like in OpenElec:
    https://github.com/OpenELEC/OpenELEC.tv/commit/50d086b1b21645b0baec881a72fc3c4476f7ade5
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libbluray/libbluray.mk
+++ package/libbluray/libbluray.mk
@@ -4,13 +4,15 @@
 #
 ################################################################################
 
-LIBBLURAY_VERSION = 0.6.2
+LIBBLURAY_VERSION = 0.8.1
 LIBBLURAY_SITE = http://get.videolan.org/libbluray/$(LIBBLURAY_VERSION)
 LIBBLURAY_SOURCE = libbluray-$(LIBBLURAY_VERSION).tar.bz2
 LIBBLURAY_INSTALL_STAGING = YES
 LIBBLURAY_LICENSE = LGPLv2.1+
 LIBBLURAY_LICENSE_FILES = COPYING
 LIBBLURAY_DEPENDENCIES = host-pkgconf
+
+LIBBLURAY_CONF_OPTS = --enable-udf
 
 ifeq ($(BR2_PACKAGE_LIBICONV),y)
 LIBBLURAY_DEPENDENCIES += libiconv
