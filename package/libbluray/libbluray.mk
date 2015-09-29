7d9b4b6431609f86f56cea6396ecf459ef94d12a
Baruch Siach <baruch@tkos.co.il>
2015-09-29 21:48:47 +0300

    libbluray: explicitly enable optional enabled features
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libbluray/libbluray.mk
+++ package/libbluray/libbluray.mk
@@ -20,18 +20,21 @@ endif
 
 ifeq ($(BR2_PACKAGE_FREETYPE),y)
 LIBBLURAY_DEPENDENCIES += freetype
+LIBBLURAY_CONF_OPTS += --with-freetype
 else
 LIBBLURAY_CONF_OPTS += --without-freetype
 endif
 
 ifeq ($(BR2_PACKAGE_FONTCONFIG),y)
 LIBBLURAY_DEPENDENCIES += fontconfig
+LIBBLURAY_CONF_OPTS += --with-fontconfig
 else
 LIBBLURAY_CONF_OPTS += --without-fontconfig
 endif
 
 ifeq ($(BR2_PACKAGE_LIBXML2),y)
 LIBBLURAY_DEPENDENCIES += libxml2
+LIBBLURAY_CONF_OPTS += --with-libxml2
 else
 LIBBLURAY_CONF_OPTS += --without-libxml2
 endif
