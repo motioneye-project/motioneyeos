8f1c4c49a2ab0fc9e6cf73370f4483e253e5ac2d
Baruch Siach <baruch@tkos.co.il>
2015-09-30 14:38:49 +0300

    ltp-testsuite: don't link with libtirpc
    Buildroot patches libtirpc to remove auth_des support. This breaks tirpc test
    build. Remove support for libtirpc for now.
    
    Fixes:
    http://autobuild.buildroot.net/results/301/3015eee7b1b6b240e7948b08954d273d28f44c32/
    
    Cc: Anders Roxell <anders.roxell@linaro.org>
    Cc: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ltp-testsuite/ltp-testsuite.mk
+++ package/ltp-testsuite/ltp-testsuite.mk
@@ -24,18 +24,11 @@ endif
 # support.
 LTP_TESTSUITE_CFLAGS = $(filter-out -D_FILE_OFFSET_BITS=64,$(TARGET_CFLAGS))
 LTP_TESTSUITE_CPPFLAGS = $(filter-out -D_FILE_OFFSET_BITS=64,$(TARGET_CPPFLAGS))
-LTP_TESTSUITE_LIBS =
-
-ifeq ($(BR2_PACKAGE_LIBTIRPC),y)
-LTP_TESTSUITE_DEPENDENCIES += libtirpc host-pkgconf
-LTP_TESTSUITE_CFLAGS += "`$(PKG_CONFIG_HOST_BINARY) --cflags libtirpc`"
-LTP_TESTSUITE_LIBS += "`$(PKG_CONFIG_HOST_BINARY) --libs libtirpc`"
-endif
 
 LTP_TESTSUITE_CONF_ENV += \
+	ac_cv_header_tirpc_netconfig_h=no \
 	CFLAGS="$(LTP_TESTSUITE_CFLAGS)" \
 	CPPFLAGS="$(LTP_TESTSUITE_CPPFLAGS)" \
-	LIBS="$(LTP_TESTSUITE_LIBS)" \
 	SYSROOT="$(STAGING_DIR)"
 
 $(eval $(autotools-package))
