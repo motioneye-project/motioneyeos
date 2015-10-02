845d71c65f7e06c73a4375ac5748f470fa3de0e9
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-10-02 10:43:48 -0300

    cpio: bump to version 2.12
    All patches upstream so drop them.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/cpio/cpio.mk
+++ package/cpio/cpio.mk
@@ -4,16 +4,10 @@
 #
 ################################################################################
 
-CPIO_VERSION = 2.11
+CPIO_VERSION = 2.12
 CPIO_SITE = http://ftp.gnu.org/gnu/cpio
 CPIO_LICENSE = GPLv3+
 CPIO_LICENSE_FILES = COPYING
-CPIO_PATCH = \
-	https://projects.archlinux.org/svntogit/packages.git/plain/cpio/trunk/cpio-2.11-stdio.in.patch \
-	https://projects.archlinux.org/svntogit/packages.git/plain/cpio/trunk/cpio-2.11-CVE-2014-9112.patch \
-	https://projects.archlinux.org/svntogit/packages.git/plain/cpio/trunk/cpio-2.11-testsuite-CVE-2014-9112.patch \
-	https://projects.archlinux.org/svntogit/packages.git/plain/cpio/trunk/cpio-2.11-check_for_symlinks-CVE-2015-1197.patch \
-	https://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/app-arch/cpio/files/cpio-2.11-stat.patch
 
 # cpio uses argp.h which is not provided by uclibc or musl by default.
 # Use the argp-standalone package to provide this.
