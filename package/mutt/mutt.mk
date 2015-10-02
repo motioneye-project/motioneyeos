33cfba38521f1096530dc3c3aa76e8dd6da9c37d
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-10-02 14:11:13 -0300

    mutt: bump to version 1.5.24
    Switch to bitbucket since it's not hosted at sourceforge any more (and
    the official mutt source is ftp).
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mutt/mutt.mk
+++ package/mutt/mutt.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-MUTT_VERSION = 1.5.23
-MUTT_SITE = http://downloads.sourceforge.net/project/mutt/mutt
+MUTT_VERSION = 1.5.24
+MUTT_SITE = https://bitbucket.org/mutt/mutt/downloads
 MUTT_LICENSE = GPLv2+
 MUTT_LICENSE_FILES = GPL
 MUTT_DEPENDENCIES = ncurses
