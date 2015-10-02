7e4b26fbc8219d3f0fe799e5f0c97b2f70eeed8f
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-10-02 10:41:40 -0300

    squid: bump to version 3.5.10
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/squid/squid.mk
+++ package/squid/squid.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 SQUID_VERSION_MAJOR = 3.5
-SQUID_VERSION = $(SQUID_VERSION_MAJOR).9
+SQUID_VERSION = $(SQUID_VERSION_MAJOR).10
 SQUID_SOURCE = squid-$(SQUID_VERSION).tar.xz
 SQUID_SITE = http://www.squid-cache.org/Versions/v3/$(SQUID_VERSION_MAJOR)
 SQUID_LICENSE = GPLv2+
