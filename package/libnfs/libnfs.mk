c1a20960c4a291c977bddb0ca6eabe193d9a18e6
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2015-10-03 21:50:27 +0200

    libnfs: Without native RPC use libtirpc
    Signed-off-by Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libnfs/libnfs.mk
+++ package/libnfs/libnfs.mk
@@ -13,4 +13,8 @@ LIBNFS_LICENSE = LGPLv2.1+
 LIBNFS_LICENSE_FILES = LICENCE-LGPL-2.1.txt
 LIBNFS_DEPENDENCIES = host-pkgconf
 
+ifeq ($(BR2_PACKAGE_LIBTIRPC),y)
+LIBNFS_DEPENDENCIES += libtirpc
+endif
+
 $(eval $(autotools-package))
