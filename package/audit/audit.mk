2bdd9724ee66eb1dad514afad1f0e1cefbfdb8d0
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2015-10-04 19:29:34 +0200

    audit: Add systemd support
    The main .service file is installed by audit.
    We are using tmpfiles mechanisms to create the log directory, which
    would not exists otherwise since /var/log points to /tmp
    
    [Thomas: add explicit --disable-systemd when systemd is not used.]
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/audit/audit.mk
+++ package/audit/audit.mk
@@ -31,8 +31,24 @@ ifeq ($(BR2_aarch64),y)
 AUDIT_CONF_OPTS += --with-aarch64
 endif
 
+ifeq ($(BR2_INIT_SYSTEMD),y)
+AUDIT_CONF_OPTS += --enable-systemd
+else
+AUDIT_CONF_OPTS += --disable-systemd
+endif
+
 define AUDIT_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 755 package/audit/S01auditd $(TARGET_DIR)/etc/init.d/S01auditd
+endef
+
+define AUDIT_INSTALL_INIT_SYSTEMD
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+	ln -fs ../../../../usr/lib/systemd/system/auditd.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/auditd.service
+
+	mkdir -p $(TARGET_DIR)/usr/lib/tmpfiles.d
+	$(INSTALL) -D -m 644 package/audit/audit_tmpfiles.conf \
+		$(TARGET_DIR)/usr/lib/tmpfiles.d/audit.conf
 endef
 
 define AUDIT_INSTALL_CLEANUP
