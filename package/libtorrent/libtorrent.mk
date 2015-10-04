137620371928a819cf752622b6201c5cbc5352ef
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2015-10-05 00:56:15 +0100

    libtorrent: bump to version 0.13.6
    - Bump to version 0.13.6.
    
    - Add a hash file.
    
    - Change the website URL to the new official one.
    
    - Help the build system to find the zlib headers, otherwise the
      configure script will fail like this:
    
        checking if zlib is wanted... yes
        checking for inflateEnd in -lz... no
        checking zlib.h usability... no
        checking zlib.h presence... no
        checking for zlib.h... no
        configure: error: either specify a valid zlib installation with
        --with-zlib=DIR or disable zlib usage with --without-zlib
    
    - Disable instrumentation support by default since it avoids runtime
      failures on rtorrent for some architectures like MIPS, PowerPC or ARM:
    
        https://github.com/rakshasa/rtorrent/issues/156
    
    - Tweak the 0001-cross_compile.patch for this new version.
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libtorrent/libtorrent.mk
+++ package/libtorrent/libtorrent.mk
@@ -4,12 +4,15 @@
 #
 ################################################################################
 
-LIBTORRENT_VERSION = 0.13.3
-LIBTORRENT_SITE = http://libtorrent.rakshasa.no/downloads
+LIBTORRENT_VERSION = 0.13.6
+LIBTORRENT_SITE = http://rtorrent.net/downloads
 LIBTORRENT_DEPENDENCIES = host-pkgconf libsigc \
-	$(if $(BR2_PACKAGE_OPENSSL),openssl)
+	$(if $(BR2_PACKAGE_OPENSSL),openssl) \
+	$(if $(BR2_PACKAGE_ZLIB),zlib)
 LIBTORRENT_CONF_OPTS = --enable-aligned \
-	$(if $(BR2_PACKAGE_OPENSSL),--enable-openssl,--disable-openssl)
+	--disable-instrumentation \
+	$(if $(BR2_PACKAGE_OPENSSL),--enable-openssl,--disable-openssl) \
+	$(if $(BR2_PACKAGE_ZLIB),--with-zlib=$(STAGING_DIR)/usr,--without-zlib)
 LIBTORRENT_INSTALL_STAGING = YES
 LIBTORRENT_AUTORECONF = YES
 LIBTORRENT_LICENSE = GPLv2
