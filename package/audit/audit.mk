70589245564a4142aa40e16d203f604fbee742f2
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2015-10-04 19:01:14 +0200

    audit: Bump version
    Remove patch as it was merged in this release:
    https://fedorahosted.org/audit/changeset/1117
    
    [Thomas: remove autoreconf, no longer needed.]
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/audit/audit.mk
+++ package/audit/audit.mk
@@ -4,15 +4,12 @@
 #
 ################################################################################
 
-AUDIT_VERSION = 2.4.3
+AUDIT_VERSION = 2.4.4
 AUDIT_SITE = http://people.redhat.com/sgrubb/audit
 AUDIT_LICENSE = GPLv2
 AUDIT_LICENSE_FILES = COPYING
 
 AUDIT_INSTALL_STAGING = YES
-
-# Patching Makefile.am
-AUDIT_AUTORECONF = YES
 
 AUDIT_CONF_OPTS = --without-python --without-python3 --disable-zos-remote
 
