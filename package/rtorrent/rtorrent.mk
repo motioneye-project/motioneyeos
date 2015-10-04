0162131675e22512edbb8a2030de9f1f30165579
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2015-10-05 00:56:16 +0100

    rtorrent: bump to version 0.9.6
    - Bump to version 0.9.6
    
    - Add a hash file.
    
    - Change the website URL to the new official one.
    
    - Tweak the 0001-cross_compile.patch for the new version.
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rtorrent/rtorrent.mk
+++ package/rtorrent/rtorrent.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-RTORRENT_VERSION = 0.9.3
-RTORRENT_SITE = http://libtorrent.rakshasa.no/downloads
+RTORRENT_VERSION = 0.9.6
+RTORRENT_SITE = http://rtorrent.net/downloads
 RTORRENT_DEPENDENCIES = host-pkgconf libcurl libsigc libtorrent ncurses
 RTORRENT_AUTORECONF = YES
 RTORRENT_LICENSE = GPLv2
