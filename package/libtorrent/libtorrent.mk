f7facd5b091e1110cdb584130e4378a9eebcf652
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-10-04 22:04:37 -0300

    libtorrent: zlib is mandatory
    zlib isn't optional even though it seems like it, fixes build failure
    such as:
    
    log.cc:51:18: fatal error: zlib.h: No such file or directory
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libtorrent/libtorrent.mk
+++ package/libtorrent/libtorrent.mk
@@ -6,13 +6,12 @@
 
 LIBTORRENT_VERSION = 0.13.6
 LIBTORRENT_SITE = http://rtorrent.net/downloads
-LIBTORRENT_DEPENDENCIES = host-pkgconf \
-	$(if $(BR2_PACKAGE_OPENSSL),openssl) \
-	$(if $(BR2_PACKAGE_ZLIB),zlib)
+LIBTORRENT_DEPENDENCIES = host-pkgconf zlib \
+	$(if $(BR2_PACKAGE_OPENSSL),openssl)
 LIBTORRENT_CONF_OPTS = --enable-aligned \
 	--disable-instrumentation \
-	$(if $(BR2_PACKAGE_OPENSSL),--enable-openssl,--disable-openssl) \
-	$(if $(BR2_PACKAGE_ZLIB),--with-zlib=$(STAGING_DIR)/usr,--without-zlib)
+	--with-zlib=$(STAGING_DIR)/usr \
+	$(if $(BR2_PACKAGE_OPENSSL),--enable-openssl,--disable-openssl)
 LIBTORRENT_INSTALL_STAGING = YES
 LIBTORRENT_AUTORECONF = YES
 LIBTORRENT_LICENSE = GPLv2
