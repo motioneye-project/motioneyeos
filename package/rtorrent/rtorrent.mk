f904fd9f1d97d63504610cb75b3b041e12f24b5f
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-10-04 22:04:36 -0300

    rtorrent: drop sigc dep & switch homepage
    libsigc isn't used/needed any longer so drop it.
    And switch to the proper homepage rather than the tarball repository.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rtorrent/rtorrent.mk
+++ package/rtorrent/rtorrent.mk
@@ -6,7 +6,7 @@
 
 RTORRENT_VERSION = 0.9.6
 RTORRENT_SITE = http://rtorrent.net/downloads
-RTORRENT_DEPENDENCIES = host-pkgconf libcurl libsigc libtorrent ncurses
+RTORRENT_DEPENDENCIES = host-pkgconf libcurl libtorrent ncurses
 RTORRENT_AUTORECONF = YES
 RTORRENT_LICENSE = GPLv2
 RTORRENT_LICENSE_FILES = COPYING
