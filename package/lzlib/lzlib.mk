0682970b3d69c65291ee872db20ca4264720ed48
Jörg Krause <joerg.krause@embedded.rocks>
2015-10-08 04:12:54 +0200

    package/lzlib: bump to version 0.4.3
    Switch to more recent github repository.
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    [Thomas: add hash file.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lzlib/lzlib.mk
+++ package/lzlib/lzlib.mk
@@ -4,9 +4,11 @@
 #
 ################################################################################
 
-LZLIB_VERSION = 0.4.work3-1
-LZLIB_SUBDIR = lzlib-0.4-work3
+LZLIB_VERSION = 0.4.3
+LZLIB_SITE = $(call github,LuaDist,lzlib,$(LZLIB_VERSION))
 LZLIB_DEPENDENCIES = zlib
 LZLIB_LICENSE = MIT
+LZLIB_CONF_OPTS = -DINSTALL_CMOD="/usr/lib/lua/$(LUAINTERPRETER_ABIVER)" \
+	-DINSTALL_LMOD="/usr/share/lua/$(LUAINTERPRETER_ABIVER)"
 
-$(eval $(luarocks-package))
+$(eval $(cmake-package))
