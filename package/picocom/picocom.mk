fbc4463d10d3875eb344a497db097205ce63eced
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2015-10-08 10:47:20 +0100

    picocom: bump to version 2.0
    - Bump to version 2.0
    - Switch the download site to GitHub
    - Apply an upstream patch to fix a missing define
    - Add a hash file
    - Rewrap the help text to 72 characters length
    - Change the official website
    
    [Thomas: fix s/MAKE_ENV/TARGET_MAKE_ENV/.]
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/picocom/picocom.mk
+++ package/picocom/picocom.mk
@@ -4,13 +4,14 @@
 #
 ################################################################################
 
-PICOCOM_VERSION = 1.7
-PICOCOM_SITE = http://picocom.googlecode.com/files
+PICOCOM_VERSION = 2.0
+PICOCOM_SITE = $(call github,npat-efault,picocom,$(PICOCOM_VERSION))
 PICOCOM_LICENSE = GPLv2+
 PICOCOM_LICENSE_FILES = LICENSE.txt
+PICOCOM_PATCH = https://github.com/npat-efault/picocom/commit/2c4c2317592daac97aac6669fd7b68e07a3dbec6.patch
 
 define PICOCOM_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
 endef
 
 define PICOCOM_INSTALL_TARGET_CMDS
