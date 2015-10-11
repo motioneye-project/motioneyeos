4dbeb3740b7e8a843f5a0d611dce883e9526aa35
Calin Crisan <ccrisan@gmail.com>
2015-10-11 20:38:52 +0300

    raspberrypi2: fixed RPi.GPIO wrong base address

--- package/python-rpi-gpio/python-rpi-gpio.mk
+++ package/python-rpi-gpio/python-rpi-gpio.mk
@@ -9,5 +9,16 @@ PYTHON_RPI_GPIO_SOURCE = RPi.GPIO-$(PYTHON_RPI_GPIO_VERSION).tar.gz
 PYTHON_RPI_GPIO_SITE = http://pypi.python.org/packages/source/R/RPi.GPIO
 PYTHON_RPI_GPIO_SETUP_TYPE = distutils
 
+BOARD = $(shell basename $(BASE_DIR))
+PYTHON_RPI_GPIO_PKG_DIR = $(TOPDIR)/package/python-rpi-gpio
+
+define RPI2_BASE_ADDRESS_PATCH
+    cd $(@D) && patch -p1 < $(PYTHON_RPI_GPIO_PKG_DIR)/bcm2836-base-address._patch
+endef
+
+ifeq ($(BOARD), raspberrypi2)
+    PYTHON_RPI_GPIO_POST_PATCH_HOOKS += RPI2_BASE_ADDRESS_PATCH
+endif
+
 $(eval $(python-package))
 
