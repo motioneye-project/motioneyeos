3ec38bab0f886bcba05b8b6bbdae8e0f9b549478
Peter Seiderer <ps.report@gmx.net>
2015-10-11 23:56:33 +0200

    libevdev: disable runtime tests
    Add new configure option '--disable-runtime-tests' and use it do
    disable runtime tests which fail to compile on some uclibc
    configurations.
    
    Fixes [1], [2]:
      test-libevdev-init.c: In function 'test_set_clock_id':
      test-libevdev-init.c:493: error: 'CLOCK_MONOTONIC_RAW' undeclared (first use in this function)
    
    and compile failure for !BR2_USE_MMU configuration:
      test-main.c:(.text+0x8): undefined reference to `_fork'
    
    [1] http://autobuild.buildroot.net/results/0d0/0d0a987471ce8e9f76fc96e0ae8f0bfeadb45028/
    [2] http://autobuild.buildroot.net/results/939/9392033f1b8d2c8e4e8791c90918dabbe28a7067/
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libevdev/libevdev.mk
+++ package/libevdev/libevdev.mk
@@ -10,9 +10,14 @@ LIBEVDEV_SOURCE = libevdev-$(LIBEVDEV_VERSION).tar.xz
 LIBEVDEV_LICENSE = X11
 LIBEVDEV_LICENSE_FILES = COPYING
 
+# patch touches configure.ac
+LIBEVDEV_AUTORECONF = YES
+
 # Uses PKG_CHECK_MODULES() in configure.ac
 LIBEVDEV_DEPENDENCIES = host-pkgconf
 
 LIBEVDEV_INSTALL_STAGING = YES
+
+LIBEVDEV_CONF_OPTS += --disable-runtime-tests
 
 $(eval $(autotools-package))
