a173a5e3d848c2d52b936289ce1b1ef764761b70
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-10-14 10:44:34 -0300

    xtables-addons: bump to version 2.9
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/xtables-addons/xtables-addons.mk
+++ package/xtables-addons/xtables-addons.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XTABLES_ADDONS_VERSION = 2.8
+XTABLES_ADDONS_VERSION = 2.9
 XTABLES_ADDONS_SOURCE = xtables-addons-$(XTABLES_ADDONS_VERSION).tar.xz
 XTABLES_ADDONS_SITE = http://downloads.sourceforge.net/project/xtables-addons/Xtables-addons
 XTABLES_ADDONS_DEPENDENCIES = iptables linux host-pkgconf
