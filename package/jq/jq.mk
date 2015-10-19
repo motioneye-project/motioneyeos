db6fb49e7fcd703f85edddd25dee3d0709a499dd
Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
2015-10-19 12:42:09 +0200

    jq: enable host package
    Allow building jq as host utility for use in post-build scripts.
    This can be useful to created, edit, merge or even perform syntax checking
    on JSON files.
    
    Signed-off-by: Thomas De Schampheleire <thomas.de.schampheleire@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/jq/jq.mk
+++ package/jq/jq.mk
@@ -11,8 +11,11 @@ JQ_LICENSE_FILES = COPYING
 
 # uses c99 specific features
 JQ_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -std=c99"
+HOST_JQ_CONF_ENV += CFLAGS="$(HOST_CFLAGS) -std=c99"
 
 # jq explicitly enables maintainer mode, which we don't need/want
 JQ_CONF_OPTS += --disable-maintainer-mode
+HOST_JQ_CONF_OPTS += --disable-maintainer-mode
 
 $(eval $(autotools-package))
+$(eval $(host-autotools-package))
