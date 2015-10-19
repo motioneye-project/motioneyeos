98538bb0a246519912e4014630ea92ccd90ab07a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-10-19 18:38:33 -0300

    exfat: bump to version 1.2.1
    Switch to github homepage and site.
    Package is now autotools-based.
    musl tweak no longer required.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/exfat/exfat.mk
+++ package/exfat/exfat.mk
@@ -4,31 +4,12 @@
 #
 ################################################################################
 
-EXFAT_VERSION = 1.1.0
-EXFAT_SITE = http://distfiles.gentoo.org/distfiles
+EXFAT_VERSION = 1.2.1
+EXFAT_SITE = https://github.com/relan/exfat/releases/download/v$(EXFAT_VERSION)
 EXFAT_SOURCE = fuse-exfat-$(EXFAT_VERSION).tar.gz
-EXFAT_DEPENDENCIES = host-scons libfuse
+EXFAT_DEPENDENCIES = libfuse host-pkgconf
 EXFAT_LICENSE = GPLv3+
 EXFAT_LICENSE_FILES = COPYING
 EXFAT_CFLAGS = $(TARGET_CFLAGS) -std=c99
 
-# The endianness handling functions in platform.h are protected behind
-# ifdef __GLIBC__ which musl doesn't define even though it does
-# provide the endianness handling interface. Work around it by
-# ensuring __GLIBC__ is defined.
-ifeq ($(BR2_TOOLCHAIN_USES_MUSL),y)
-EXFAT_CFLAGS += -D__GLIBC__
-endif
-
-define EXFAT_BUILD_CMDS
-	(cd $(@D); \
-		$(TARGET_CONFIGURE_OPTS) CCFLAGS="$(EXFAT_CFLAGS)" $(SCONS))
-endef
-
-define EXFAT_INSTALL_TARGET_CMDS
-	(cd $(@D); \
-		$(TARGET_CONFIGURE_OPTS) CCFLAGS="$(EXFAT_CFLAGS)" $(SCONS) \
-		DESTDIR=$(TARGET_DIR)/usr/sbin install)
-endef
-
-$(eval $(generic-package))
+$(eval $(autotools-package))
