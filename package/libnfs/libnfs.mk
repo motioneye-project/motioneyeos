39d1f0156b1ea18db084220dd3a6518a2d0d8b5c
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-10-19 10:33:57 -0300

    libnfs: bump to version 1.9.8
    Prerequisite for the gvfs 1.26.1.1 bump.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libnfs/libnfs.mk
+++ package/libnfs/libnfs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBNFS_VERSION = libnfs-1.9.5
+LIBNFS_VERSION = libnfs-1.9.8
 LIBNFS_SITE = $(call github,sahlberg,libnfs,$(LIBNFS_VERSION))
 LIBNFS_INSTALL_STAGING = YES
 LIBNFS_AUTORECONF = YES
@@ -16,5 +16,11 @@ LIBNFS_DEPENDENCIES = host-pkgconf
 ifeq ($(BR2_PACKAGE_LIBTIRPC),y)
 LIBNFS_DEPENDENCIES += libtirpc
 endif
+
+# Needed for autoreconf
+define LIBNFS_MAKE_M4_DIR
+	mkdir $(@D)/m4
+endef
+LIBNFS_POST_EXTRACT_HOOKS += LIBNFS_MAKE_M4_DIR
 
 $(eval $(autotools-package))
