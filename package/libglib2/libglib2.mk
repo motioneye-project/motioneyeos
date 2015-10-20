756284a1e45300963e995c465dfbd03414861637
Gustavo Zacarias <gustavo.zacarias@free-electrons.com>
2015-10-20 19:40:55 -0300

    libglib2: disable tests and update configure options
    Sometimes if host-python is around it doesn't work too well with the
    distro python (Gentoo, FC20 at least) leading to build failures.
    
    This failure is very hard to trigger, it's normally easier to do so by
    enabling top-level parallel builds.
    
    Fixes:
    http://autobuild.buildroot.net/results/0f8/0f881e322b7f9d244b8dd2440ba0afd3a8418027/
    
    While at it update the configure options to avoid any host
    libraries/tools from leaking in, new naming conventions and options.
    And remove dead code dealing with the nothreaded variant.
    
    Signed-off-by: Gustavo Zacarias <gustavozacarias@free-electrons.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libglib2/libglib2.mk
+++ package/libglib2/libglib2.mk
@@ -86,15 +86,13 @@ LIBGLIB2_CONF_ENV += glib_cv_have_qsort_r=yes
 endif
 
 HOST_LIBGLIB2_CONF_OPTS = \
+	--disable-coverage \
 	--disable-dtrace \
+	--disable-fam \
+	--disable-libelf \
+	--disable-selinux \
 	--disable-systemtap \
-	--disable-gcov \
-	--disable-modular-tests
-
-LIBGLIB2_CONF_OPTS += --disable-modular-tests
-ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),)
-LIBGLIB2_CONF_OPTS += --with-threads=none --disable-threads
-endif
+	--disable-xattr
 
 LIBGLIB2_DEPENDENCIES = host-pkgconf host-libglib2 libffi zlib $(if $(BR2_NEEDS_GETTEXT),gettext) host-gettext
 
