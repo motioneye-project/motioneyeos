73b193f840a9790f2438c02853f9be8738b7fb50
James Knight <james.knight@rockwellcollins.com>
2015-10-21 20:08:11 -0400

    ntp: bump to version 4.2.8p4
    Signed-off-by: James Knight <james.knight@rockwellcollins.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ntp/ntp.mk
+++ package/ntp/ntp.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 NTP_VERSION_MAJOR = 4.2
-NTP_VERSION = $(NTP_VERSION_MAJOR).8p3
+NTP_VERSION = $(NTP_VERSION_MAJOR).8p4
 NTP_SITE = http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-$(NTP_VERSION_MAJOR)
 NTP_DEPENDENCIES = host-pkgconf libevent $(if $(BR2_PACKAGE_BUSYBOX),busybox)
 NTP_LICENSE = ntp license
