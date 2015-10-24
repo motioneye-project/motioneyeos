fff978be292b859525247e87eab17297cb74f316
Francois Perrad <fperrad@gmail.com>
2015-10-24 18:26:53 +0200

    perl-netaddr-ip: bump to version 4.078
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/perl-netaddr-ip/perl-netaddr-ip.mk
+++ package/perl-netaddr-ip/perl-netaddr-ip.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_NETADDR_IP_VERSION = 4.075
+PERL_NETADDR_IP_VERSION = 4.078
 PERL_NETADDR_IP_SOURCE = NetAddr-IP-$(PERL_NETADDR_IP_VERSION).tar.gz
 PERL_NETADDR_IP_SITE = $(BR2_CPAN_MIRROR)/authors/id/M/MI/MIKER
 PERL_NETADDR_IP_LICENSE = Artistic or GPLv1+
