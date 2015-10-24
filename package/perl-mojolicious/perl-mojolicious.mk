a3b6336c9ac0e8a62c273e3b0bdf92d2bda369f3
Francois Perrad <fperrad@gmail.com>
2015-10-24 18:26:52 +0200

    perl-mojolicious: bump to version 6.25
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/perl-mojolicious/perl-mojolicious.mk
+++ package/perl-mojolicious/perl-mojolicious.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_MOJOLICIOUS_VERSION = 6.14
+PERL_MOJOLICIOUS_VERSION = 6.25
 PERL_MOJOLICIOUS_SOURCE = Mojolicious-$(PERL_MOJOLICIOUS_VERSION).tar.gz
 PERL_MOJOLICIOUS_SITE = $(BR2_CPAN_MIRROR)/authors/id/S/SR/SRI
 PERL_MOJOLICIOUS_LICENSE = Artistic-2.0
