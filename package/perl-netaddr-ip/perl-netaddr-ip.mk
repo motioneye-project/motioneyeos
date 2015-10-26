0a2d217826d64ac0f13dcb26499a077b864ba2f3
Francois Perrad <fperrad@gmail.com>
2015-10-26 18:45:00 +0100

    perl-netaddr-ip: remove !BR2_STATIC_LIBS constraint
    with the option -noxs, the Pure Perl version is always built.
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/perl-netaddr-ip/perl-netaddr-ip.mk
+++ package/perl-netaddr-ip/perl-netaddr-ip.mk
@@ -10,6 +10,8 @@ PERL_NETADDR_IP_SITE = $(BR2_CPAN_MIRROR)/authors/id/M/MI/MIKER
 PERL_NETADDR_IP_LICENSE = Artistic or GPLv1+
 PERL_NETADDR_IP_LICENSE_FILES = Artistic Copying
 
+# we always build the Pure Perl version.
+# the build of the native part of NetAddr::IP::Util is buggy.
 PERL_NETADDR_IP_CONF_OPTS = -noxs
 
 $(eval $(perl-package))
