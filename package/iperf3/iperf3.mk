ec658ff15a0f1098360e9c9efecec31c6b731e95
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-10-29 10:17:57 -0300

    iperf3: bump to version 3.1
    Switch to proper homepage, download from homepage and add a hash file.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/iperf3/iperf3.mk
+++ package/iperf3/iperf3.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-IPERF3_VERSION = 3.0.10
-IPERF3_SITE = $(call github,esnet,iperf,$(IPERF3_VERSION))
+IPERF3_VERSION = 3.1
+IPERF3_SITE = https://iperf.fr/download/iperf_$(IPERF3_VERSION)
+IPERF3_SOURCE = iperf-$(IPERF3_VERSION)-source.tar.gz
 IPERF3_LICENSE = BSD-3c, BSD-2c, MIT
 IPERF3_LICENSE_FILES = LICENSE
 
