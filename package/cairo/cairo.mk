d5f36eb63d7b3dd0104b8ce3458b8d0b54dd883f
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-10-29 21:05:58 -0300

    cairo: bump to version 1.14.4
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/cairo/cairo.mk
+++ package/cairo/cairo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAIRO_VERSION = 1.14.2
+CAIRO_VERSION = 1.14.4
 CAIRO_SOURCE = cairo-$(CAIRO_VERSION).tar.xz
 CAIRO_LICENSE = LGPLv2.1+
 CAIRO_LICENSE_FILES = COPYING
