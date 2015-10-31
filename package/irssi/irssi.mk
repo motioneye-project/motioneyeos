26856b81e9053bdc31bba9b5c0f7223b97f1d0a3
Rodrigo Rebello <rprebello@gmail.com>
2015-10-30 23:56:21 -0200

    irssi: new package
    Irssi is a terminal based IRC client for UNIX systems.
    
    http://irssi.org/
    
    [Thomas: use alternate site since the official site is currently
    down.]
    
    Signed-off-by: Rodrigo Rebello <rprebello@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/irssi/irssi.mk
@@ -0,0 +1,54 @@
+################################################################################
+#
+# irssi
+#
+################################################################################
+
+IRSSI_VERSION = 0.8.17
+IRSSI_SOURCE = irssi-$(IRSSI_VERSION).tar.bz2
+# Official web site is down
+# IRSSI_SITE = http://irssi.org/files
+IRSSI_SITE = http://pkgs.fedoraproject.org/repo/pkgs/irssi/$(IRSSI_SOURCE)/ecf64be47978d89a742b435a81cb47db
+IRSSI_LICENSE = GPLv2+
+IRSSI_LICENSE_FILES = COPYING
+IRSSI_DEPENDENCIES = libglib2 ncurses
+IRSSI_AUTORECONF = YES
+
+IRSSI_CONF_OPTS = \
+	--disable-glibtest \
+	--with-ncurses=$(STAGING_DIR)/usr \
+	--without-perl
+
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+IRSSI_CONF_OPTS += --enable-ssl
+IRSSI_DEPENDENCIES += openssl
+else
+IRSSI_CONF_OPTS += --disable-ssl
+endif
+
+ifeq ($(BR2_PACKAGE_IRSSI_PROXY),y)
+IRSSI_CONF_OPTS += --with-proxy
+# If shared libs are disabled, 'proxy' has to go in the list of built-in
+# modules.
+ifeq ($(BR2_STATIC_LIBS),y)
+IRSSI_CONF_OPTS += --with-modules=proxy
+endif
+else
+IRSSI_CONF_OPTS += --without-proxy
+endif # proxy
+
+ifeq ($(BR2_PACKAGE_IRSSI_TRUE_COLOR),y)
+IRSSI_CONF_OPTS += --enable-true-color
+else
+IRSSI_CONF_OPTS += --disable-true-color
+endif
+
+# Cross-compiling irssi with the perl interpreter enabled doesn't work
+# yet. So, remove scripts as they are useless in that case.
+define IRSSI_REMOVE_SCRIPTS
+	rm -rf $(TARGET_DIR)/usr/share/irssi/scripts/
+endef
+
+IRSSI_POST_INSTALL_TARGET_HOOKS += IRSSI_REMOVE_SCRIPTS
+
+$(eval $(autotools-package))
