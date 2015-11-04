4b8b1af2f2fbbe5a19e40ad8d35d2d6fbb6cfbae
Steven Noonan <steven@uplinklabs.net>
2015-11-04 00:37:21 -0800

    protobuf: bump to 2.6.1
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/protobuf/protobuf.mk
+++ package/protobuf/protobuf.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PROTOBUF_VERSION = v2.5.0
+PROTOBUF_VERSION = v2.6.1
 PROTOBUF_SITE = $(call github,google,protobuf,$(PROTOBUF_VERSION))
 PROTOBUF_LICENSE = BSD-3c
 PROTOBUF_LICENSE_FILES = COPYING.txt
