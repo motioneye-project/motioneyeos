1072b38788b03f39bb5f302a5b71837f25af2d9f
kei-k@ca2.so-net.ne.jp <kei-k@ca2.so-net.ne.jp>
2015-11-04 22:40:27 +0900

    gauche: new package
    Gauche is an R7RS Scheme implementation developed to be a handy script
    interpreter, which allows programmers and systemadministrators to write
    small to large scripts for their daily chores. Quick startup, built-in
    system interface, native multilingual support are some of its goals.
    
    Signed-off-by: Hiroshi Kawashima <kei-k@ca2.so-net.ne.jp>
    Reviewed-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Tested-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/gauche/gauche.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# gauche
+#
+################################################################################
+
+GAUCHE_VERSION = 0.9.4
+GAUCHE_SOURCE = Gauche-$(GAUCHE_VERSION).tgz
+GAUCHE_SITE = http://downloads.sourceforge.net/project/gauche/Gauche
+GAUCHE_LICENSE = BSD-3c, Boehm-gc, SRFI (srfi-11.scm), reload (reload.scm)
+GAUCHE_LICENSE_FILES = COPYING
+GAUCHE_DEPENDENCIES = host-gauche
+
+# Detection of c99 support in configure fails without WCHAR. To enable
+# automatic detection of c99 support by configure, we need to enable
+# WCHAR in toolchain. But actually we do not need WCHAR at gauche
+# runtime. So reuesting WCHAR in toolchain just for automatic detection
+# will be overkill. To solve this, explicitly -std=gnu99 is specified
+# here.
+GAUCHE_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -std=gnu99"
+
+$(eval $(host-autotools-package))
+$(eval $(autotools-package))
