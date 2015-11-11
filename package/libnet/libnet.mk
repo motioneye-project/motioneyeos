6d33e77f742cc5f9706a6eb4eaeeaabe27315939
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2015-11-11 15:02:24 +0100

    libnet: adjust license information
    As noticed by Arnout, some files are under BSD-3c.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libnet/libnet.mk
+++ package/libnet/libnet.mk
@@ -7,7 +7,7 @@
 LIBNET_VERSION = 1.1.6
 LIBNET_SITE = http://sourceforge.net/projects/libnet-dev/files
 LIBNET_INSTALL_STAGING = YES
-LIBNET_LICENSE = BSD-2c
+LIBNET_LICENSE = BSD-2c, BSD-3c
 LIBNET_LICENSE_FILES = doc/COPYING
 
 $(eval $(autotools-package))
