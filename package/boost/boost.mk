4b13641be6a83aca80a7a9bdfe03f71465355b3c
Waldemar Brodkorb <wbx@openadk.org>
2015-11-15 13:24:55 +0100

    boost: specify architecture variant for sparc
    Instead of using a patch to specify the architecture variant for
    sparc, use some Boost configure variables.
    
    [Thomas: reword commit log.]
    
    Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/boost/boost.mk
+++ package/boost/boost.mk
@@ -89,6 +89,10 @@ BOOST_OPTS += toolset=gcc \
 	     abi=$(BOOST_ABI) \
 	     variant=$(if $(BR2_ENABLE_DEBUG),debug,release)
 
+ifeq ($(BR2_sparc),y)
+BOOST_OPTS += architecture=sparc instruction-set=v8
+endif
+
 # By default, Boost build and installs both the shared and static
 # variants. Override that if we want static only or shared only.
 ifeq ($(BR2_STATIC_LIBS),y)
