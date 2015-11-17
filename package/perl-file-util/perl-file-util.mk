5b7cb94c967549cd59cc692ec2fc843f66974478
Carlos Santos <casantos@datacom.ind.br>
2015-11-17 10:24:06 -0200

    perl-file-util: restore host variant and fix dependencies
    The host variant was added by us (DATACOM) because it is useful to
    build some third-party packages that we use in our firmware. We humbly
    request you to keep it alive.
    
    Declare that it requires host-perl-module-build.
    
    Signed-off-by: Carlos Santos <casantos@datacom.ind.br>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/perl-file-util/perl-file-util.mk
+++ package/perl-file-util/perl-file-util.mk
@@ -10,5 +10,7 @@ PERL_FILE_UTIL_SITE = $(BR2_CPAN_MIRROR)/authors/id/T/TO/TOMMY
 PERL_FILE_UTIL_DEPENDENCIES = host-perl-module-build
 PERL_FILE_UTIL_LICENSE = Artistic or GPLv1+
 PERL_FILE_UTIL_LICENSE_FILES = COPYING LICENSE
+HOST_PERL_FILE_UTIL_DEPENDENCIES = host-perl-module-build
 
 $(eval $(perl-package))
+$(eval $(host-perl-package))
