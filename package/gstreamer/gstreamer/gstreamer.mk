bb5e3ffb11477d5a21f01e3ae8b9b6100fef859a
Gustavo Zacarias <gustavo.zacarias@free-electrons.com>
2015-11-20 10:06:51 -0300

    gstreamer: can optionally use libxml2
    gstreamer can optionally build against libxml2, so account for it in
    the dependencies.
    Functionality seems somewhat reduced without it, an example being
    misdetecting subtitle (.srt) files as subrip mime type instead of
    subtitle itself.
    
    A failed scenario would be:
    
    $ gst-typefind-0.10 some.srt
    some.srt - application/x-subrip
    
    When built with libxml2:
    
    $ gst-typefind-0.10 some.srt
    some.srt - application/x-subtitle
    
    This doesn't affect gstreamer 1.x since it dropped any use of libxml2.
    
    Signed-off-by: Gustavo Zacarias <gustavo.zacarias@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gstreamer/gstreamer/gstreamer.mk
+++ package/gstreamer/gstreamer/gstreamer.mk
@@ -27,6 +27,7 @@ GSTREAMER_CONF_OPTS = \
 	$(if $(BR2_PACKAGE_GSTREAMER_GST_DEBUG),,--disable-gst-debug) \
 	$(if $(BR2_PACKAGE_GSTREAMER_PLUGIN_REGISTRY),,--disable-registry)
 
-GSTREAMER_DEPENDENCIES = libglib2 host-pkgconf host-bison host-flex
+GSTREAMER_DEPENDENCIES = libglib2 host-pkgconf host-bison host-flex \
+	$(if $(BR2_PACKAGE_LIBXML2),libxml2)
 
 $(eval $(autotools-package))
