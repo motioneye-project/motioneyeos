23ba818b630df8f58f7d0e731b689750a0fba025
Romain Naour <romain.naour@openwide.fr>
2015-11-21 17:36:14 +0100

    toolchain-external: CodeSourcery AMD64: support only one version
    See the conclusion about external toolchains during the Buildroot
    meeting [1]:
    "In the future, we stick to a single external toolchain version. The
    Kconfig symbol should not encode the version (avoid legacy handling)"
    
    [1] http://elinux.org/index.php?title=Buildroot:DeveloperDaysELCE2015#Report
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>
    Reviewed-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Tested-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- toolchain/toolchain-external/toolchain-external.mk
+++ toolchain/toolchain-external/toolchain-external.mk
@@ -392,7 +392,7 @@ TOOLCHAIN_EXTERNAL_SOURCE = ia32-2012.03-27-i686-pc-linux-gnu-i386-linux.tar.bz2
 else ifeq ($(BR2_TOOLCHAIN_EXTERNAL_CODESOURCERY_X86_201209),y)
 TOOLCHAIN_EXTERNAL_SITE = https://sourcery.mentor.com/public/gnu_toolchain/i686-pc-linux-gnu
 TOOLCHAIN_EXTERNAL_SOURCE = ia32-2012.09-62-i686-pc-linux-gnu-i386-linux.tar.bz2
-else ifeq ($(BR2_TOOLCHAIN_EXTERNAL_CODESOURCERY_AMD64_201405),y)
+else ifeq ($(BR2_TOOLCHAIN_EXTERNAL_CODESOURCERY_AMD64),y)
 TOOLCHAIN_EXTERNAL_SITE = https://sourcery.mentor.com/public/gnu_toolchain/x86_64-amd-linux-gnu
 TOOLCHAIN_EXTERNAL_SOURCE = amd-2014.05-25-x86_64-amd-linux-gnu-i686-pc-linux-gnu.tar.bz2
 else ifeq ($(BR2_TOOLCHAIN_EXTERNAL_BLACKFIN_UCLINUX_2013R1),y)
