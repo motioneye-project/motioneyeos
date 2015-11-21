b18e4b58499a90694b2447db4e4ceef3af8d897c
Jörg Krause <joerg.krause@embedded.rocks>
2015-11-21 20:22:06 +0100

    package/yajl: add patch to fix static build issue
    mpd's plugin soundcloud requires yajl. In static build context yajl library is
    named yajl_s so linking against yajl fails:
      /bin/ld: cannot find -lyajl
    
    Use a pull request patch [1] to let the shared and the static library
    have the same name. They will be differentiated by the extension (.so or .a).
    
    Fixes:
    http://autobuild.buildroot.net/results/5ac/5acba9b0ed852512fc88e83973a3b5389e6d54be/
    http://autobuild.buildroot.net/results/739/73934ad67efa121fedeaa03fdad290aa63f1485e/
    
    [1] https://github.com/lloyd/yajl/pull/174
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/yajl/yajl.mk
+++ package/yajl/yajl.mk
@@ -9,5 +9,6 @@ YAJL_SITE = $(call github,lloyd,yajl,$(YAJL_VERSION))
 YAJL_INSTALL_STAGING = YES
 YAJL_LICENSE = ISC
 YAJL_LICENSE_FILES = COPYING
+YAJL_PATCH = https://github.com/vriera/yajl/commit/6d09f11b8fd358cab0e31b965327e64a599f9ce9.patch
 
 $(eval $(cmake-package))
