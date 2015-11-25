3d644777683abdc2a6fad8c662ed86f719cf1b21
Peter Rosin <peda@axentia.se>
2015-11-25 13:29:11 +0100

    sqlite: add license file
    Signed-off-by: Peter Rosin <peda@axentia.se>
    Acked-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -8,6 +8,7 @@ SQLITE_VERSION = 3090200
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_VERSION).tar.gz
 SQLITE_SITE = http://www.sqlite.org/2015
 SQLITE_LICENSE = Public domain
+SQLITE_LICENSE_FILES = tea/license.terms
 SQLITE_INSTALL_STAGING = YES
 
 # Patching Makefile.am:
