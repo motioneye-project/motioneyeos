cf44dfedc6cec76c245d9de6545a4349b7747bd1
Gustavo Zacarias <gustavo.zacarias@free-electrons.com>
2015-11-25 09:44:13 -0300

    c-ares: define random file
    When cross-compiling it assumes there's none, however that's not true.
    Specify the usual /dev/urandom for this usage, it's used for random key
    generation on startup.
    
    Signed-off-by: Gustavo Zacarias <gustavo.zacarias@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/c-ares/c-ares.mk
+++ package/c-ares/c-ares.mk
@@ -7,6 +7,7 @@
 C_ARES_VERSION = 1.10.0
 C_ARES_SITE = http://c-ares.haxx.se/download
 C_ARES_INSTALL_STAGING = YES
+C_ARES_CONF_OPTS = --with-random=/dev/urandom
 # Rebuild configure to avoid XC_CHECK_USER_CFLAGS
 C_ARES_AUTORECONF = YES
 C_ARES_LICENSE = MIT
