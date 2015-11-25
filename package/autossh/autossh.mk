870e13d916bab55e0e517853463aef520f361472
Yann E. MORIN <yann.morin.1998@free.fr>
2015-11-25 22:44:58 +0100

    package/autossh: do not look for the host's ssh command
    autossh looks for 'ssh' in the PATH, which may not exist on a build
    machine.
    
    Fixes a private autobuild failure (soon to go public).
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Peter Korsgaard <jacmet@uclibc.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/autossh/autossh.mk
+++ package/autossh/autossh.mk
@@ -10,6 +10,8 @@ AUTOSSH_SOURCE = autossh-$(AUTOSSH_VERSION).tgz
 AUTOSSH_LICENSE = Modified BSD
 AUTOSSH_LICENSE_FILES = autossh.c
 
+AUTOSSH_CONF_OPTS = --with-ssh=/usr/bin/ssh
+
 define AUTOSSH_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -D $(@D)/autossh $(TARGET_DIR)/usr/bin/autossh
 endef
