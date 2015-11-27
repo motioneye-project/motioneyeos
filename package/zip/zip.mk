f0c9470451b23989fe7b298d30352760b1513c37
Luca Ceresoli <luca@lucaceresoli.net>
2015-11-27 18:38:00 +0100

    infozip: rename to zip
    We currently have a package named "infozip" for Info-Zip's Zip.
    
    With the upcoming addition of a package the Info-Zip's UnZip the name
    is becoming ambiguous. Rename it to the more specific name "zip".
    
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Cc: Romain Naour <romain.naour@openwide.fr>
    Cc: Jan Pedersen <jp@jp-embedded.com>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Reviewed-by: Romain Naour <romain.naour@openwide.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/zip/zip.mk
+++ package/zip/zip.mk
@@ -1,51 +1,51 @@
 ################################################################################
 #
-# infozip
+# zip
 #
 ################################################################################
 
-INFOZIP_VERSION = 30
-INFOZIP_SOURCE = zip$(INFOZIP_VERSION).tgz
-INFOZIP_SITE = ftp://ftp.info-zip.org/pub/infozip/src
-INFOZIP_LICENSE = Info-ZIP
-INFOZIP_LICENSE_FILES = LICENSE
+ZIP_VERSION = 30
+ZIP_SOURCE = zip$(ZIP_VERSION).tgz
+ZIP_SITE = ftp://ftp.info-zip.org/pub/infozip/src
+ZIP_LICENSE = Info-ZIP
+ZIP_LICENSE_FILES = LICENSE
 
 ifeq ($(BR2_PACKAGE_BZIP2),y)
-INFOZIP_DEPENDENCIES += bzip2
+ZIP_DEPENDENCIES += bzip2
 endif
 
 # Infozip's default CFLAGS.
-INFOZIP_CFLAGS = -I. -DUNIX
+ZIP_CFLAGS = -I. -DUNIX
 
 # Disable the support of 16-bit UIDs/GIDs, the test in unix/configure was
 # removed since it can't work for cross-compilation.
-INFOZIP_CFLAGS += -DUIDGID_NOT_16BIT
+ZIP_CFLAGS += -DUIDGID_NOT_16BIT
 
 # infozip already defines _LARGEFILE_SOURCE and _LARGEFILE64_SOURCE when
 # necessary, redefining it on the command line causes some warnings.
-INFOZIP_TARGET_CFLAGS = \
+ZIP_TARGET_CFLAGS = \
 	$(filter-out -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE,$(TARGET_CFLAGS))
 
-define INFOZIP_BUILD_CMDS
+define ZIP_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) \
-		CFLAGS="$(INFOZIP_TARGET_CFLAGS) $(INFOZIP_CFLAGS)" \
+		CFLAGS="$(ZIP_TARGET_CFLAGS) $(ZIP_CFLAGS)" \
 		AS="$(TARGET_CC) -c" \
 		-f unix/Makefile generic
 endef
 
-define INFOZIP_INSTALL_TARGET_CMDS
+define ZIP_INSTALL_TARGET_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) -f unix/Makefile install \
 		prefix=$(TARGET_DIR)/usr
 endef
 
-define HOST_INFOZIP_BUILD_CMDS
+define HOST_ZIP_BUILD_CMDS
 	$(MAKE) $(HOST_CONFIGURE_OPTS) -C $(@D) \
-		CFLAGS="$(HOST_CFLAGS) $(INFOZIP_CFLAGS)" \
+		CFLAGS="$(HOST_CFLAGS) $(ZIP_CFLAGS)" \
 		AS="$(HOSTCC) -c" \
 		-f unix/Makefile generic
 endef
 
-define HOST_INFOZIP_INSTALL_CMDS
+define HOST_ZIP_INSTALL_CMDS
 	$(MAKE) $(HOST_CONFIGURE_OPTS) -C $(@D) -f unix/Makefile install \
 		prefix=$(HOST_DIR)/usr
 endef
