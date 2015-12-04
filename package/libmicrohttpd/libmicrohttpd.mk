64d859ee02f15dc60799fd7069d35e241f8752d6
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-12-04 11:02:44 -0300

    libmicrohttpd: bump to version 0.9.47
    Experimental SPDY support was dropped so drop the configure option.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libmicrohttpd/libmicrohttpd.mk
+++ package/libmicrohttpd/libmicrohttpd.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-LIBMICROHTTPD_VERSION = 0.9.46
+LIBMICROHTTPD_VERSION = 0.9.47
 LIBMICROHTTPD_SITE = $(BR2_GNU_MIRROR)/libmicrohttpd
 LIBMICROHTTPD_LICENSE_FILES = COPYING
 LIBMICROHTTPD_INSTALL_STAGING = YES
-LIBMICROHTTPD_CONF_OPTS = --disable-curl --disable-spdy --disable-examples
+LIBMICROHTTPD_CONF_OPTS = --disable-curl --disable-examples
 
 ifeq ($(BR2_PACKAGE_LIBMICROHTTPD_SSL),y)
 LIBMICROHTTPD_LICENSE = LGPLv2.1+
