1682aeefa312f8cc759cbf576a9b8d2e54a97695
Yann E. MORIN <yann.morin.1998@free.fr>
2015-12-06 17:35:29 +0100

    package/mpython-m2crypto: bump version
    Fixes:
    
    http://autobuild.buildroot.org/results/ee1/ee1bb80214a51c212712de8193b89a32e9901408/
    http://autobuild.buildroot.org/results/ce8/ce8d5d68eef312ca168f351b845920eb62ca4eb1/
    http://autobuild.buildroot.org/results/500/5004a2a9cabe75669bbe90f3ee35ab3869617fc9/
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-m2crypto/python-m2crypto.mk
+++ package/python-m2crypto/python-m2crypto.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_M2CRYPTO_VERSION = 0.21.1
+PYTHON_M2CRYPTO_VERSION = 0.22.5
 PYTHON_M2CRYPTO_SITE = http://pypi.python.org/packages/source/M/M2Crypto
 PYTHON_M2CRYPTO_SOURCE = M2Crypto-$(PYTHON_M2CRYPTO_VERSION).tar.gz
 PYTHON_M2CRYPTO_SETUP_TYPE = setuptools
@@ -13,10 +13,16 @@ HOST_PYTHON_M2CRYPTO_DEPENDENCIES = host-openssl host-swig
 # We need to use python2 because m2crypto is not python3 compliant.
 HOST_PYTHON_M2CRYPTO_NEEDS_HOST_PYTHON = python2
 
+HOST_PYTHON_M2CRYPTO_BUILD_DIR = $(@D)/build/lib.linux-$(HOSTARCH)-$(PYTHON_VERSION_MAJOR)
+
 # * We need to override the build commands to be able to use build_ext,
 #   which accepts the --openssl option.
 # * Use python2 interpreter to avoid trying building some python3 objects.
+# * because build_ext is flawed, is forgets to copy the files prior to
+#   the build, so we do it manually.
 define HOST_PYTHON_M2CRYPTO_BUILD_CMDS
+	mkdir -p $(HOST_PYTHON_M2CRYPTO_BUILD_DIR)
+	cp -av $(@D)/M2Crypto $(HOST_PYTHON_M2CRYPTO_BUILD_DIR)/M2Crypto
 	(cd $(@D); \
 		$(HOST_PKG_PYTHON_SETUPTOOLS_ENV) \
 		$(HOST_DIR)/usr/bin/python2 setup.py build_ext \
