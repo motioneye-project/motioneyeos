90e5cea9cf44dde7eb8131de37273a4111e6d279
André Hentschel <nerv@dawncrow.de>
2015-12-13 23:22:46 +0100

    openal: Bump to 1.17.1
    Fixes
    http://autobuild.buildroot.org/results/3b8/3b8dadb1fcb71dae3daf500df95e270c2f86ec3b/
    by fixing the jack without pulseaudio bug
    
    Signed-off-by: André Hentschel <nerv@dawncrow.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/openal/openal.mk
+++ package/openal/openal.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENAL_VERSION = 1.17.0
+OPENAL_VERSION = 1.17.1
 OPENAL_SOURCE = openal-soft-$(OPENAL_VERSION).tar.bz2
 OPENAL_SITE = http://kcat.strangesoft.net/openal-releases
 OPENAL_LICENSE = LGPLv2+
