57c953d1e45fc9acb488e4fd994e9f0e3e820a45
Yegor Yefremov <yegorslists@googlemail.com>
2015-12-14 09:25:56 +0100

    python-pyasn: bump to 0.1.9
    Add a hash file.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-pyasn/python-pyasn.mk
+++ package/python-pyasn/python-pyasn.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PYASN_VERSION = 0.1.7
+PYTHON_PYASN_VERSION = 0.1.9
 PYTHON_PYASN_SOURCE = pyasn1-$(PYTHON_PYASN_VERSION).tar.gz
 PYTHON_PYASN_SITE = https://pypi.python.org/packages/source/p/pyasn1
 PYTHON_PYASN_LICENSE = BSD-2c
