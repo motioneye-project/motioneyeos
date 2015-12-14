3490e53eb9404750f338a1382e857eb52c34324d
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2015-12-14 14:17:00 +0000

    expedite: use a tar.bz2 tarball to save bandwitdh and disk space
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/expedite/expedite.mk
+++ package/expedite/expedite.mk
@@ -5,6 +5,7 @@
 ################################################################################
 
 EXPEDITE_VERSION = 1.7.10
+EXPEDITE_SOURCE = expedite-$(EXPEDITE_VERSION).tar.bz2
 EXPEDITE_SITE = http://download.enlightenment.org/releases
 EXPEDITE_LICENSE = BSD-2c
 EXPEDITE_LICENSE_FILES = COPYING
