98d58b7c282bd3283f57b840554a81d2df93e53c
Romain Naour <romain.naour@openwide.fr>
2015-12-15 23:40:22 +0100

    package/libevas-generic-loaders: bump to version 1.15.0
    Use the newly added libefl package wich provide a new version
    of libeina.
    
    Also, update the upstream url and add a hash file.
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libevas-generic-loaders/libevas-generic-loaders.mk
+++ package/libevas-generic-loaders/libevas-generic-loaders.mk
@@ -4,17 +4,19 @@
 #
 ################################################################################
 
-LIBEVAS_GENERIC_LOADERS_VERSION = 1.7.10
-LIBEVAS_GENERIC_LOADERS_SOURCE = evas_generic_loaders-$(LIBEVAS_GENERIC_LOADERS_VERSION).tar.bz2
-LIBEVAS_GENERIC_LOADERS_SITE = http://download.enlightenment.org/releases
+LIBEVAS_GENERIC_LOADERS_VERSION = 1.15.0
+LIBEVAS_GENERIC_LOADERS_SOURCE = evas_generic_loaders-$(LIBEVAS_GENERIC_LOADERS_VERSION).tar.xz
+LIBEVAS_GENERIC_LOADERS_SITE = http://download.enlightenment.org/rel/libs/evas_generic_loaders
 LIBEVAS_GENERIC_LOADERS_LICENSE = GPLv2
 LIBEVAS_GENERIC_LOADERS_LICENSE_FILES = COPYING
 
 LIBEVAS_GENERIC_LOADERS_INSTALL_STAGING = YES
 
-LIBEVAS_GENERIC_LOADERS_DEPENDENCIES = libeina zlib
+LIBEVAS_GENERIC_LOADERS_DEPENDENCIES = host-pkgconf libefl zlib
 
 # For now, we only support the SVG loader
+# poppler >= 0.32 is not supported by the current version of
+# libevas-generic-loaders.
 LIBEVAS_GENERIC_LOADERS_CONF_OPTS += \
 	--disable-poppler \
 	--disable-spectre \
