5a5e7132647c22ac3c93d53cb3b6a41a9fef5590
Yegor Yefremov <yegorslists@googlemail.com>
2015-12-15 08:16:50 +0100

    python-pyroute2: bump to 0.3.15
    Add a hash file.
    
    Change download location to pypi and remove a pre-configure
    hook as the package now provides a stand-alone setup.py.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-pyroute2/python-pyroute2.mk
+++ package/python-pyroute2/python-pyroute2.mk
@@ -4,18 +4,11 @@
 #
 #############################################################
 
-PYTHON_PYROUTE2_VERSION = 16a8aced82c65b94eb9e15a7d773a3256763d155
-PYTHON_PYROUTE2_SITE = $(call github,svinota,pyroute2,$(PYTHON_PYROUTE2_VERSION))
+PYTHON_PYROUTE2_VERSION = 0.3.15
+PYTHON_PYROUTE2_SOURCE = pyroute2-$(PYTHON_PYROUTE2_VERSION).tar.gz
+PYTHON_PYROUTE2_SITE = https://pypi.python.org/packages/source/p/pyroute2
 PYTHON_PYROUTE2_LICENSE = Apache-2.0 or GPLv2+
 PYTHON_PYROUTE2_LICENSE_FILES = LICENSE.Apache.v2 LICENSE.GPL.v2 README.license.md
 PYTHON_PYROUTE2_SETUP_TYPE = distutils
-PYTHON_PYROUTE2_DEPENDENCIES = host-gawk
-
-# this hook is needed to create setup.py from setup.py.in
-define PYTHON_PYROUTE2_CREATE_SETUP_PY
-	$(TARGET_MAKE_ENV) $(MAKE1) -C $(@D) update-version
-endef
-
-PYTHON_PYROUTE2_PRE_CONFIGURE_HOOKS += PYTHON_PYROUTE2_CREATE_SETUP_PY
 
 $(eval $(python-package))
