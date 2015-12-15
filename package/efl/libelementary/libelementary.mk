efee00a832671355064c66b94c7884bc15ef5b0a
Romain Naour <romain.naour@openwide.fr>
2015-12-15 23:40:24 +0100

    package/efl/libelementary: bump to version 1.15.2
    Use the newly added libefl package wich provide a new version
    of libeina, libevas, libecore and libedje.
    
    Update the upstream url and add a hash file.
    
    We need to add a host package to provide elm_prefs_cc the
    host machine to cross-compile correctly libelementary
    for the target. Otherwise, elm_prefs_cc for the
    target is used on the host machine.
    
    Since eet, eolian_gen and eldbus_codegen are installed in
    HOST_DIR by host-efl package, help configure script to find
    them.
    
    Explicitly disable doxygen and elementary-test.
    
    [Thomas: add explicit select BR2_PACKAGE_LIBEFL.]
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/efl/libelementary/libelementary.mk
+++ package/efl/libelementary/libelementary.mk
@@ -4,29 +4,34 @@
 #
 ################################################################################
 
-LIBELEMENTARY_VERSION = 1.7.10
-LIBELEMENTARY_SOURCE = elementary-$(LIBELEMENTARY_VERSION).tar.bz2
-LIBELEMENTARY_SITE = http://download.enlightenment.org/releases
+LIBELEMENTARY_VERSION = 1.15.2
+LIBELEMENTARY_SOURCE = elementary-$(LIBELEMENTARY_VERSION).tar.xz
+LIBELEMENTARY_SITE = http://download.enlightenment.org/rel/libs/elementary
 LIBELEMENTARY_LICENSE = LGPLv2.1
 LIBELEMENTARY_LICENSE_FILES = COPYING
 
 LIBELEMENTARY_INSTALL_STAGING = YES
 
-LIBELEMENTARY_DEPENDENCIES = \
-	libeina libevas libecore libedje host-libedje \
-	host-libeet
+LIBELEMENTARY_DEPENDENCIES = host-pkgconf host-libefl host-libelementary libefl
 
 LIBELEMENTARY_CONF_OPTS = \
 	--with-edje-cc=$(HOST_DIR)/usr/bin/edje_cc \
-	--with-eet-eet=$(HOST_DIR)/usr/bin/eet
+	--with-eet-eet=$(HOST_DIR)/usr/bin/eet \
+	--with-eolian-gen=$(HOST_DIR)/usr/bin/eolian_gen \
+	--with-eldbus_codegen=$(HOST_DIR)/usr/bin/eldbus-codegen \
+	--with-elm-prefs-cc=$(HOST_DIR)/usr/bin/elm_prefs_cc \
+	--with-doxygen=no \
+	--disable-elementary-test
 
-# libethumb_client is only built when ethumbd is built.
-# ethumbd is only built if edbus is built.
-ifeq ($(BR2_PACKAGE_LIBETHUMB)$(BR2_PACKAGE_LIBEDBUS),yy)
-LIBELEMENTARY_DEPENDENCIES += libethumb
-LIBELEMENTARY_CONF_OPTS += --enable-ethumb
-else
-LIBELEMENTARY_CONF_OPTS += --disable-ethumb
-endif
+# We need a host package in order to provide elm_prefs_cc and
+# elementary_codegen.
+HOST_LIBELEMENTARY_DEPENDENCIES = host-pkgconf host-libefl
+HOST_LIBELEMENTARY_CONF_OPTS = \
+	--with-edje-cc=$(HOST_DIR)/usr/bin/edje_cc \
+	--with-eet-eet=$(HOST_DIR)/usr/bin/eet \
+	--with-eolian-gen=$(HOST_DIR)/usr/bin/eolian_gen \
+	--with-doxygen=no \
+	--disable-elementary-test
 
 $(eval $(autotools-package))
+$(eval $(host-autotools-package))
