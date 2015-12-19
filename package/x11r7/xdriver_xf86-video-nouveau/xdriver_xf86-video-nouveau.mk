4d2357e1839e728f4b2f808fd9b8a4315cbb577d
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-12-19 15:22:05 +0100

    package/x11r7/xdriver_xf86-video-nouveau: new package
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/x11r7/xdriver_xf86-video-nouveau/xdriver_xf86-video-nouveau.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# xdriver_xf86-video-nouveau
+#
+################################################################################
+
+XDRIVER_XF86_VIDEO_NOUVEAU_VERSION = 1.0.12
+XDRIVER_XF86_VIDEO_NOUVEAU_SOURCE = xf86-video-nouveau-$(XDRIVER_XF86_VIDEO_NOUVEAU_VERSION).tar.bz2
+XDRIVER_XF86_VIDEO_NOUVEAU_SITE = http://xorg.freedesktop.org/releases/individual/driver
+XDRIVER_XF86_VIDEO_NOUVEAU_LICENSE = MIT
+XDRIVER_XF86_VIDEO_NOUVEAU_LICENSE_FILES = COPYING
+XDRIVER_XF86_VIDEO_NOUVEAU_DEPENDENCIES = \
+	host-pkgconf \
+	libdrm \
+	xproto_dri2proto \
+	xproto_randrproto \
+	xproto_renderproto \
+	xproto_videoproto \
+	xproto_xextproto \
+	xserver_xorg-server
+
+# xdriver_xf86-video-nouveau requires O_CLOEXEC
+XDRIVER_XF86_VIDEO_NOUVEAU_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE"
+
+$(eval $(autotools-package))
