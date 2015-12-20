7d09da02e25fcacbac3cfa3760ed4c0c55f2baf8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-12-20 17:30:09 -0300

    libmicrohttpd: bump to version 0.9.48
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libmicrohttpd/libmicrohttpd.mk
+++ package/libmicrohttpd/libmicrohttpd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBMICROHTTPD_VERSION = 0.9.47
+LIBMICROHTTPD_VERSION = 0.9.48
 LIBMICROHTTPD_SITE = $(BR2_GNU_MIRROR)/libmicrohttpd
 LIBMICROHTTPD_LICENSE_FILES = COPYING
 LIBMICROHTTPD_INSTALL_STAGING = YES
