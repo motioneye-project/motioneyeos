aee96dcc61ee72f0983f283b6599d95be03269d8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-12-20 17:23:31 -0300

    gnupg: bump to version 1.4.20
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gnupg/gnupg.mk
+++ package/gnupg/gnupg.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GNUPG_VERSION = 1.4.19
+GNUPG_VERSION = 1.4.20
 GNUPG_SOURCE = gnupg-$(GNUPG_VERSION).tar.bz2
 GNUPG_SITE = ftp://ftp.gnupg.org/gcrypt/gnupg
 GNUPG_LICENSE = GPLv3+
