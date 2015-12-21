9f44d4fd6ff2510d1f2eafb9efdf5739018e61a3
Rodrigo Rebello <rprebello@gmail.com>
2015-12-21 15:06:28 -0200

    picocom: bump to version 2.1
    Also remove no longer needed patch (merged upstream before the release
    of v2.1).
    
    Signed-off-by: Rodrigo Rebello <rprebello@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/picocom/picocom.mk
+++ package/picocom/picocom.mk
@@ -4,11 +4,10 @@
 #
 ################################################################################
 
-PICOCOM_VERSION = 2.0
+PICOCOM_VERSION = 2.1
 PICOCOM_SITE = $(call github,npat-efault,picocom,$(PICOCOM_VERSION))
 PICOCOM_LICENSE = GPLv2+
 PICOCOM_LICENSE_FILES = LICENSE.txt
-PICOCOM_PATCH = https://github.com/npat-efault/picocom/commit/2c4c2317592daac97aac6669fd7b68e07a3dbec6.patch
 
 define PICOCOM_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
