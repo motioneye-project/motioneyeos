44859e68cc52e31eb569ed0a108db156d678f198
Gustavo Zacarias <gustavo@zacarias.com.ar>
2015-12-22 08:02:34 -0300

    cantarell: bump to version 0.0.19
    Also use CANTARELL_VERSION_MAJOR in SITE as reported by Baruch.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/cantarell/cantarell.mk
+++ package/cantarell/cantarell.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 CANTARELL_VERSION_MAJOR = 0.0
-CANTARELL_VERSION = $(CANTARELL_VERSION_MAJOR).18.1
-CANTARELL_SITE = http://ftp.gnome.org/pub/gnome/sources/cantarell-fonts/0.0
+CANTARELL_VERSION = $(CANTARELL_VERSION_MAJOR).19
+CANTARELL_SITE = http://ftp.gnome.org/pub/gnome/sources/cantarell-fonts/$(CANTARELL_VERSION_MAJOR)
 CANTARELL_SOURCE = cantarell-fonts-$(CANTARELL_VERSION).tar.xz
 CANTARELL_DEPENDENCIES = host-pkgconf
 CANTARELL_LICENSE = OFLv1.1
