748131a4c56f092bf302a6fea6901220d50174cd
Jonathan Ben Avraham <yba@tkos.co.il>
2015-12-22 18:51:10 +0200

    autofs: new package
    Tested on i.MX6 using NFS mounts only. Not tested with DAS mounts as
    of this commit.
    
    [Thomas:
     - remove the "# rpcbind" comments on the thread and mmu dependencies,
       since we don't select rpcbind. The thread and mmu dependencies are
       still needed though, since autofs uses pthread_*() functions and
       fork().
     - Remove "Requires a toolchain with native RPC." in the Config.in
       help text and instead add a proper Config.in comment about the
       thread and RPC dependency.
     - Add patch to fix building with uClibc, taken from OpenEmbedded.]
    
    Signed-off-by: Jonathan Ben Avraham <yba@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/autofs/autofs.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# autofs
+#
+################################################################################
+
+AUTOFS_VERSION = 5.1.1
+AUTOFS_SOURCE = autofs-$(AUTOFS_VERSION).tar.xz
+AUTOFS_SITE = $(BR2_KERNEL_MIRROR)/linux/daemons/autofs/v5
+AUTOFS_LICENSE = GPLv2+
+AUTOFS_LICENSE_FILES = COPYING COPYRIGHT
+
+AUTOFS_CONF_OPTS = \
+	--disable-mount-locking \
+	--enable-ignore-busy \
+	--without-openldap \
+	--without-sasl
+
+AUTOFS_MAKE_ENV = DONTSTRIP=1
+
+$(eval $(autotools-package))
