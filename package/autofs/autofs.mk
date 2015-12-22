2badd96d9e933afc052f78c1a91cdc5dd98e1a1f
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2015-12-22 21:34:18 +0100

    autofs: add missing dependencies on flex and bison
    Fixes:
    
      http://autobuild.buildroot.org/results/999/999d2d5fe07994642ab39b26d936c230d79947a2/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/autofs/autofs.mk
+++ package/autofs/autofs.mk
@@ -9,6 +9,7 @@ AUTOFS_SOURCE = autofs-$(AUTOFS_VERSION).tar.xz
 AUTOFS_SITE = $(BR2_KERNEL_MIRROR)/linux/daemons/autofs/v5
 AUTOFS_LICENSE = GPLv2+
 AUTOFS_LICENSE_FILES = COPYING COPYRIGHT
+AUTOFS_DEPENDENCIES = host-flex host-bison
 
 AUTOFS_CONF_OPTS = \
 	--disable-mount-locking \
