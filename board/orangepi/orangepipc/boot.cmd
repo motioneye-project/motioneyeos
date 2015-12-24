d7327c69c8459adf0b878205e0a2ab63c60d02bd
Eelco Chaudron <echaudron@xiot.nl>
2015-12-24 14:08:04 +0100

    configs: new orangepipc_defconfig
    This commit adds a configuration for the Orange Pi PC board,
    <http://www.orangepi.org/orangepipc/>.
    
    Signed-off-by: Eelco Chaudron <echaudron@xiot.nl>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ board/orangepi/orangepipc/boot.cmd
@@ -0,0 +1,8 @@
+setenv fdt_high ffffffff
+
+setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait
+
+fatload mmc 0 $kernel_addr_r zImage
+fatload mmc 0 $fdt_addr_r sun8i-h3-orangepi-pc.dtb
+
+bootz $kernel_addr_r - $fdt_addr_r
