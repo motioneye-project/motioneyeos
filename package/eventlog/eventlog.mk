ebafb18877da782377cd337171e3a6b487cb6069
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2015-12-29 09:13:35 +0100

    eventlog: add EVENTLOG_VERSION_MAJOR variable
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/eventlog/eventlog.mk
+++ package/eventlog/eventlog.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-EVENTLOG_VERSION = 0.2.12
+EVENTLOG_VERSION_MAJOR = 0.2
+EVENTLOG_VERSION = $(EVENTLOG_VERSION_MAJOR).12
 EVENTLOG_SOURCE = eventlog_$(EVENTLOG_VERSION).tar.gz
-EVENTLOG_SITE = https://my.balabit.com/downloads/eventlog/0.2
+EVENTLOG_SITE = https://my.balabit.com/downloads/eventlog/$(EVENTLOG_VERSION_MAJOR)
 EVENTLOG_LICENSE = BSD-3c
 EVENTLOG_LICENSE_FILES = COPYING
 EVENTLOG_INSTALL_STAGING = YES
