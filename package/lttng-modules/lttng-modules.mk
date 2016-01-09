bbb2f8bf32b93377daae8d020848b6ab8b77a014
Samuel Martin <s.martin49@gmail.com>
2016-01-09 21:59:24 +0100

    package/lttng-modules: bump to version 2.7.1
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lttng-modules/lttng-modules.mk
+++ package/lttng-modules/lttng-modules.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LTTNG_MODULES_VERSION = 2.7.0
+LTTNG_MODULES_VERSION = 2.7.1
 LTTNG_MODULES_SITE = http://lttng.org/files/lttng-modules
 LTTNG_MODULES_SOURCE = lttng-modules-$(LTTNG_MODULES_VERSION).tar.bz2
 LTTNG_MODULES_LICENSE = LGPLv2.1/GPLv2 (kernel modules), MIT (lib/bitfield.h, lib/prio_heap/*)
