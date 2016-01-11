85590e245c92b07f0ef7be3e628cd6349cb5a2d9
Yegor Yefremov <yegorslists@googlemail.com>
2016-01-11 12:04:36 +0100

    python-httplib2: bump to 0.9.2
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-httplib2/python-httplib2.mk
+++ package/python-httplib2/python-httplib2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_HTTPLIB2_VERSION = 0.9
+PYTHON_HTTPLIB2_VERSION = 0.9.2
 PYTHON_HTTPLIB2_SOURCE = httplib2-$(PYTHON_HTTPLIB2_VERSION).tar.gz
 PYTHON_HTTPLIB2_SITE = http://pypi.python.org/packages/source/h/httplib2
 PYTHON_HTTPLIB2_SETUP_TYPE = setuptools
