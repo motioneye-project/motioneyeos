8d49d853f5c882c7d9c3c30e61c82265ee26e9e1
Yegor Yefremov <yegorslists@googlemail.com>
2016-01-13 15:24:18 +0100

    python-can: bump to 1.4.2
    Add hash file and change download location to PyPi.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-can/python-can.mk
+++ package/python-can/python-can.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-PYTHON_CAN_VERSION = ae5b6cf
-PYTHON_CAN_SITE = https://bitbucket.org/hardbyte/python-can/get
-PYTHON_CAN_SOURCE = $(PYTHON_CAN_VERSION).tar.bz2
+PYTHON_CAN_VERSION = 1.4.2
+PYTHON_CAN_SITE = https://pypi.python.org/packages/source/p/python-can
 PYTHON_CAN_LICENSE = LGPLv3
 PYTHON_CAN_LICENSE_FILES = LICENSE.txt
 PYTHON_CAN_SETUP_TYPE = setuptools
