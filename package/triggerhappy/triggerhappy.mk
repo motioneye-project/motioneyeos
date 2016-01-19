cc9304aa7220d9b3657572940b1e61504f920e24
Jörg Krause <joerg.krause@embedded.rocks>
2016-01-19 10:51:53 +0100

    package/triggerhappy: bump to latest commit to fix runtime issue
    Bump to latest commit to fix runtime issue with the Linux Kernel headers 4.4:
      Starting thd: Unable to parse trigger line: [..]
    
    With version 4.4 of the Linux Kernel the input event codes are not defined in
    'linux/input.h' anymore, but in a seperate header file
    'linux/input-event-codes.h' leaving triggerhappys evtable_*.h header files
    empty with no event codes defined.
    
    This issue is fixed upstream by commit 7e5abc69f215678e93a6b999524981c8b40bdcd9
    which also makes overwriting of the 'LINUX_INPUT_H' definition obsolet.
    
    Tested with an internal Buildroot toolchain with Linux Kernel headers 4.4 and
    an external Linaro ARM 2015.08 toolchain with Linux Kernel headers 4.0.
    
    [Peter: add hash file]
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/triggerhappy/triggerhappy.mk
+++ package/triggerhappy/triggerhappy.mk
@@ -4,15 +4,13 @@
 #
 ################################################################################
 
-TRIGGERHAPPY_VERSION = aac9f353a28c0f414b27ac54bbbb2292c152eedc
+TRIGGERHAPPY_VERSION = 7e5abc69f215678e93a6b999524981c8b40bdcd9
 TRIGGERHAPPY_SITE = $(call github,wertarbyte,triggerhappy,$(TRIGGERHAPPY_VERSION))
 TRIGGERHAPPY_LICENSE = GPLv3+
 TRIGGERHAPPY_LICENSE_FILES = COPYING
 
 define TRIGGERHAPPY_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) \
-		LINUX_INPUT_H=$(STAGING_DIR)/usr/include/linux/input.h \
-		-C $(@D) thd th-cmd
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) thd th-cmd
 endef
 
 ifeq ($(BR2_PACKAGE_HAS_UDEV),y)
