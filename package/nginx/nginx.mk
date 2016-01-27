77dd34bc795cad4163476426db0e3fda51d1c67d
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-01-27 09:03:30 -0300

    nginx: security bump to version 1.8.1
    Fixes:
    
    CVE-2016-0742 - invalid pointer dereference might occur during DNS
    server response processing if the "resolver" directive was used,
    allowing anattacker who is able to forge UDP packets from the DNS server
    to cause segmentation fault in a worker process.
    
    CVE-2016-0746 - use-after-free condition might occur during CNAME
    response processing if the "resolver" directive was used, allowing an
    attacker who is able to trigger name resolution to cause segmentation
    fault in a worker process, or might have potential other impact.
    
    CVE-2016-0747 - CNAME resolution was insufficiently limited if the
    "resolver" directive was used, allowing an attacker who is able to
    trigger arbitrary name resolution to cause excessive resource
    consumption in worker processes.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/nginx/nginx.mk
+++ package/nginx/nginx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NGINX_VERSION = 1.8.0
+NGINX_VERSION = 1.8.1
 NGINX_SITE = http://nginx.org/download
 NGINX_LICENSE = BSD-2c
 NGINX_LICENSE_FILES = LICENSE
