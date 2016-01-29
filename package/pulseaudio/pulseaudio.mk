1cffb454320aeba80a56985ecfc44c3c1d293802
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-01-29 22:05:49 +0100

    package/pulseaudio: fix musl build
    Fixes build errors in previous version which are still present
    http://autobuild.buildroot.net/results/879/879260f0efc80c2c9061d89d3e99ad107083f72a/
    http://autobuild.buildroot.net/results/1e5/1e5f6a5f349800226d8b07601dab68ffe7794461/
    http://autobuild.buildroot.net/results/0f8/0f8d68b11c6807a9d1d4c40a66430ac096902720/
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Reviewed-by: Romain Naour <romain.naour@gmail.com>
    
    [Thomas:
     - Rework patch 0001-padsp-Make-it-compile-on-musl.patch to put
       Bernd's comment and SoB *after* the original commit log.
     - Rework patch 0002-musl-fixes.patch for the same reason, and also to
       remove the xlocale.h part, which has already been fixed upstream.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/pulseaudio/pulseaudio.mk
+++ package/pulseaudio/pulseaudio.mk
@@ -7,6 +7,8 @@
 PULSEAUDIO_VERSION = 8.0
 PULSEAUDIO_SOURCE = pulseaudio-$(PULSEAUDIO_VERSION).tar.xz
 PULSEAUDIO_SITE = http://freedesktop.org/software/pulseaudio/releases
+# 0002-musl-fixes.patch patches configure.ac
+PULSEAUDIO_AUTORECONF = YES
 PULSEAUDIO_INSTALL_STAGING = YES
 PULSEAUDIO_LICENSE = LGPLv2.1+ (specific license for modules, see LICENSE file)
 PULSEAUDIO_LICENSE_FILES = LICENSE GPL LGPL
