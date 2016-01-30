5ea881e711543bae8f03d7c8f049141c5bcce345
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-01-30 14:17:03 +0100

    package/leafpad: bump version to 0.8.18.1
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/leafpad/leafpad.mk
+++ package/leafpad/leafpad.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LEAFPAD_VERSION = 0.8.18
+LEAFPAD_VERSION = 0.8.18.1
 LEAFPAD_SITE = http://savannah.nongnu.org/download/leafpad
 LEAFPAD_DEPENDENCIES = libgtk2 host-intltool
 LEAFPAD_LICENSE = GPLv2+
