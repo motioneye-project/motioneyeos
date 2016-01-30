e2daaf9179b43a524c944b15a42b23ef920f4627
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-01-30 11:30:54 +0100

    package/fio: fix license file
    Sorry, I missed the changed filename.
    
    Fixes
    http://autobuild.buildroot.net/results/a35/a3596283632433831ea24c1d586f2067b034eb32/
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/fio/fio.mk
+++ package/fio/fio.mk
@@ -7,7 +7,7 @@
 FIO_VERSION = fio-2.6
 FIO_SITE = git://git.kernel.dk/fio.git
 FIO_LICENSE = GPLv2 + special obligations
-FIO_LICENSE_FILES = LICENSE
+FIO_LICENSE_FILES = COPYING
 
 define FIO_CONFIGURE_CMDS
 	(cd $(@D); ./configure --cc="$(TARGET_CC)" --extra-cflags="$(TARGET_CFLAGS)")
