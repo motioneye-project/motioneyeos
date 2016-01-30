e22f85b1802ac7d602887a2918c92a51ca59b458
Gary Bisson <gary.bisson@boundarydevices.com>
2016-01-30 12:45:53 +0100

    libfslparser: bump to version 4.0.7
    Changelog:
    - Add h264 parser version.
    - Support wide and 'ms 0x0 0x11' adpcm tag.
    - Support hev1 tag for hevc video codec.
    - TS segment fault.
    - Improve mkv parser's loading time when create parser with large size
    file.
    
    This patch is based on the Yocto equivalent:
    https://github.com/Freescale/meta-fsl-arm/commit/6406ad4
    https://github.com/Freescale/meta-fsl-arm/commit/09a532c
    https://github.com/Freescale/meta-fsl-arm/commit/371df94
    
    This package has been implicitely tested through gstreamer as the 0.10
    plugin relies on it:
     # gst-launch-0.10 playbin uri=file:///root/tears_of_steel_1080p.webm
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libfslparser/libfslparser.mk
+++ package/libfslparser/libfslparser.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBFSLPARSER_VERSION = 4.0.3
+LIBFSLPARSER_VERSION = 4.0.7
 LIBFSLPARSER_SITE = $(FREESCALE_IMX_SITE)
 LIBFSLPARSER_SOURCE = libfslparser-$(LIBFSLPARSER_VERSION).bin
 LIBFSLPARSER_INSTALL_STAGING = YES
