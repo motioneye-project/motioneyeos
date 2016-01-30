7d1feadb19fb78dc816b1e1ece46f02c53a0afbe
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-01-30 17:17:12 +0100

    package/libraw: bump version to 0.17.1
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libraw/libraw.mk
+++ package/libraw/libraw.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRAW_VERSION = 0.13.4
+LIBRAW_VERSION = 0.17.1
 LIBRAW_SOURCE = LibRaw-$(LIBRAW_VERSION).tar.gz
 LIBRAW_SITE = http://www.libraw.org/data
 LIBRAW_INSTALL_STAGING = YES
