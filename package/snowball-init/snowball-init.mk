7aecb34cbf4c9eeda5a1012b1a588de510506f60
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-01-31 17:32:13 +0100

    package/snowball-init: use github helper
    Otherwise download is broken:
    
    >>> snowball-init b064be21de25729039e5e54037bbdd2e25cfd5b7 Downloading
    --2016-01-31 17:31:27--  https://github.com/igloocommunity/snowball-init/snowball-init-b064be21de25729039e5e54037bbdd2e25cfd5b7.tar.gz
    Resolving github.com (github.com)... 192.30.252.128
    Connecting to github.com (github.com)|192.30.252.128|:443... connected.
    HTTP request sent, awaiting response... 404 Not Found
    2016-01-31 17:31:27 ERROR 404: Not Found.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/snowball-init/snowball-init.mk
+++ package/snowball-init/snowball-init.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 SNOWBALL_INIT_VERSION = b064be21de25729039e5e54037bbdd2e25cfd5b7
-SNOWBALL_INIT_SITE = https://github.com/igloocommunity/snowball-init
+SNOWBALL_INIT_SITE = $(call github,igloocommunity,snowball-init,$(SNOWBALL_INIT_VERSION))
 SNOWBALL_INIT_LICENSE = BSD-4c
 SNOWBALL_INIT_LICENSE_FILES = debian/copyright
 
