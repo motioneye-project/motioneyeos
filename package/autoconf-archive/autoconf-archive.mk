196eb53ac886b1badc64513061d71efc3b21b066
Thomas Claveirole <thomas.claveirole@green-communications.fr>
2016-02-01 15:34:55 +0100

    package/autoconf-archive: bump to version 2015.09.25
    Signed-off-by: Thomas Claveirole <thomas.claveirole@green-communications.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/autoconf-archive/autoconf-archive.mk
+++ package/autoconf-archive/autoconf-archive.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AUTOCONF_ARCHIVE_VERSION = 2015.02.24
+AUTOCONF_ARCHIVE_VERSION = 2015.09.25
 AUTOCONF_ARCHIVE_SOURCE = autoconf-archive-$(AUTOCONF_ARCHIVE_VERSION).tar.xz
 AUTOCONF_ARCHIVE_SITE = $(BR2_GNU_MIRROR)/autoconf-archive
 AUTOCONF_ARCHIVE_LICENSE = GPLv3+ with exception
