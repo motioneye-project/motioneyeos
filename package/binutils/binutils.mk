1d42d0acca6fbae5fca01024314600369d99995e
Peter Korsgaard <peter@korsgaard.com>
2016-02-01 23:42:35 +0100

    binutils: ensure TARGET_CONFIGURE_ARGS is taken into consideration for subdirs
    Binutils runs the configure scripts of subdirs at make time, so we need a
    custom build command to ensure TARGET_CONFIGURE_ARGS are taken into
    consideration for those (E.G. for musl gettext handling).
    
    Reported-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/binutils/binutils.mk
+++ package/binutils/binutils.mk
@@ -74,6 +74,12 @@ HOST_BINUTILS_CONF_OPTS = \
 	$(BINUTILS_DISABLE_GDB_CONF_OPTS) \
 	$(BINUTILS_EXTRA_CONFIG_OPTIONS)
 
+# binutils run configure script of subdirs at make time, so ensure
+# our TARGET_CONFIGURE_ARGS are taken into consideration for those
+define BINUTILS_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_ARGS) $(MAKE) -C $(@D)
+endef
+
 # We just want libbfd, libiberty and libopcodes,
 # not the full-blown binutils in staging
 define BINUTILS_INSTALL_STAGING_CMDS
