2e2eff303095366092e11278bc0689e707cf9ef5
Mauro Condarelli <mc5686@mclink.it>
2016-02-02 01:44:41 +0100

    python-jinja2: bump to 2.8
    Signed-off-by: Mauro Condarelli <mc5686@mclink.it>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-jinja2/python-jinja2.mk
+++ package/python-jinja2/python-jinja2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_JINJA2_VERSION = 2.7.3
+PYTHON_JINJA2_VERSION = 2.8
 PYTHON_JINJA2_SOURCE = Jinja2-$(PYTHON_JINJA2_VERSION).tar.gz
 PYTHON_JINJA2_SITE = http://pypi.python.org/packages/source/J/Jinja2
 PYTHON_JINJA2_SETUP_TYPE = setuptools
