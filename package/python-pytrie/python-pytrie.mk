3116fd4797d5dc63c5529a4cd2f85e51adfbfc66
Mauro Condarelli <mc5686@mclink.it>
2016-02-06 23:59:52 +0100

    python-pytrie: new package
    Signed-off-by: Mauro Condarelli <mc5686@mclink.it>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/python-pytrie/python-pytrie.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-pytrie
+#
+################################################################################
+
+PYTHON_PYTRIE_VERSION = 0.2
+PYTHON_PYTRIE_SITE = http://pypi.python.org/packages/source/P/PyTrie
+PYTHON_PYTRIE_SOURCE = PyTrie-$(PYTHON_PYTRIE_VERSION).tar.gz
+PYTHON_PYTRIE_LICENSE = BSD-3c
+PYTHON_PYTRIE_LICENSE_FILES = LICENSE
+PYTHON_PYTRIE_SETUP_TYPE = distutils
+
+$(eval $(python-package))
