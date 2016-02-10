a279cf74bcea4f57f5bd2887b8e8a43026a2197f
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-02-10 14:04:36 -0300

    gupnp: bump to version 0.20.16
    Which also means moving from unstable->stable (odd numbers in gnome
    project packages are development).
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gupnp/gupnp.mk
+++ package/gupnp/gupnp.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-GUPNP_VERSION_MAJOR = 0.19
-GUPNP_VERSION = $(GUPNP_VERSION_MAJOR).4
+GUPNP_VERSION_MAJOR = 0.20
+GUPNP_VERSION = $(GUPNP_VERSION_MAJOR).16
 GUPNP_SOURCE = gupnp-$(GUPNP_VERSION).tar.xz
 GUPNP_SITE = http://ftp.gnome.org/pub/gnome/sources/gupnp/$(GUPNP_VERSION_MAJOR)
 GUPNP_LICENSE = LGPLv2+
