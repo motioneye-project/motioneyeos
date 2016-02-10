b989436447d7e518d52517ebb7792af3736e1a79
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2016-02-10 23:40:23 +0100

    vpnc: bump to newer upstream version
    While the official upstream has not published any new tarball, they
    have continued to update their Subversion repository with additional
    changes. This commit therefore updates the vpnc package to a newer
    version, using the Github repository that replicates the original
    Subversion repository.
    
    Since the update required some updates to the patches, we took this
    opportunity to split them in more fine-grained changes, and document
    them more properly. Consequently:
    
     - The patch 0001 that fixes the Makefile gets split into 3 patches,
       0001, 0002 and 0003, each for one specific issue.
    
     - Patch 0002 to disable building the manpages gets changed into a
       solution that is potentially upstreamable, and is now patch 0004.
    
     - A new patch 0005 is added to specify a custom libgcrypt-config
       path, since this tool is now used to know how to link with
       libgcrypt.
    
     - The SUSv3 function removal patch is now patch 0006, and gains a
       better commit log.
    
    Also, this vpnc version now depends on gnutls, which basically is a
    better replacement for OpenSSL, whose support was not enabled by
    default due to licensing issues (vpnc is under the GPL, which isn't
    compatible with the OpenSSL license).
    
    Patches have been submitted upstream:
    http://lists.unix-ag.uni-kl.de/pipermail/vpnc-devel/2016-June/004186.html
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/vpnc/vpnc.mk
+++ package/vpnc/vpnc.mk
@@ -4,24 +4,23 @@
 #
 ################################################################################
 
-VPNC_VERSION = 70f1211f6f26e87e374d66f9a986e855215b8e3e
+VPNC_VERSION = 024aa17a0a86716cac0db185b44bf07ba4f8c135
 VPNC_SITE = $(call github,ndpgroup,vpnc,$(VPNC_VERSION))
 VPNC_LICENSE = GPLv2+
 VPNC_LICENSE_FILES = COPYING
 
-VPNC_DEPENDENCIES = libgcrypt libgpg-error
-
-VPNC_LDFLAGS = $(TARGET_LDFLAGS) -lgcrypt -lgpg-error
-VPNC_CPPFLAGS = -DVERSION=\\\"$(VPNC_VERSION)\\\"
+VPNC_DEPENDENCIES = libgcrypt libgpg-error gnutls host-pkgconf
 
 define VPNC_BUILD_CMDS
-	$(MAKE)	-C $(@D) $(TARGET_CONFIGURE_OPTS) \
-		CPPFLAGS="$(VPNC_CPPFLAGS)" LDFLAGS="$(VPNC_LDFLAGS)"
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
+		$(TARGET_CONFIGURE_OPTS) \
+		LIBGCRYPT_CONFIG=$(STAGING_DIR)/usr/bin/libgcrypt-config \
+		VERSION=$(VPNC_VERSION) MANS=
 endef
 
 define VPNC_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(@D) install $(TARGET_CONFIGURE_OPTS) \
-		CPPFLAGS="$(VPNC_CPPFLAGS)" LDFLAGS="$(VPNC_LDFLAGS)" \
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) install $(TARGET_CONFIGURE_OPTS) \
+		VERSION=$(VPNC_VERSION) MANS= \
 		DESTDIR="$(TARGET_DIR)" PREFIX=/usr
 endef
 
