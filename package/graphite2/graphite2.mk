36bdaa2e5d94aa7d7f6d49edde17d5b737048ad4
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-02-15 13:45:49 -0300

    graphite2: security bump to version 1.3.5
    Fixes:
    CVE-2016-1521 - An exploitable out-of-bounds read vulnerability exists
    in the opcode handling functionality of Libgraphite. A specially crafted
    font can cause an out-of-bounds read resulting in arbitrary code
    execution. An attacker can provide a malicious font to trigger this
    vulnerability.
    CVE-2016-1522 - An exploitable NULL pointer dereference exists in the
    bidirectional font handling functionality of Libgraphite. A specially
    crafted font can cause a NULL pointer dereference resulting in a crash.
    An attacker can provide a malicious font to trigger this vulnerability.
    CVE-2016-1523 - An exploitable heap-based buffer overflow exists in the
    context item handling functionality of Libgraphite. A specially crafted
    font can cause a buffer overflow resulting in potential code execution.
    An attacker can provide a malicious font to trigger this vulnerability.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/graphite2/graphite2.mk
+++ package/graphite2/graphite2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GRAPHITE2_VERSION = 1.3.3
+GRAPHITE2_VERSION = 1.3.5
 GRAPHITE2_SOURCE = graphite2-$(GRAPHITE2_VERSION).tgz
 GRAPHITE2_SITE = http://downloads.sourceforge.net/project/silgraphite/graphite2
 GRAPHITE2_INSTALL_STAGING = YES
