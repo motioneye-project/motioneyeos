806ca1e76784c2796d68eb5d3756d7c44becf5fd
Peter Seiderer <ps.report@gmx.net>
2016-02-18 22:46:58 +0100

    fbterm: fix musl compile (missing string.h include)
    Fixes [1]:
    
      mouse.cpp: In function 's32 open_gpm(Gpm_Connect*)':
      mouse.cpp:58:37: error: 'memset' was not declared in this scope
        memset((s8 *)&addr, 0, sizeof(addr));
                                         ^
      mouse.cpp:60:64: error: 'strncpy' was not declared in this scope
        strncpy(addr.sun_path, GPM_NODE_CTL, sizeof(addr.sun_path) - 1);
    
    in case BR2_PACKAGE_GPM is defined.
    
    Add proper gpm dependency and configure option too.
    
    [1] http://autobuild.buildroot.net/results/a60/a602d61e68f662217ad868de79711a8ea892a3a9
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/fbterm/fbterm.mk
+++ package/fbterm/fbterm.mk
@@ -16,4 +16,11 @@ ifeq ($(BR2_STATIC_LIBS)$(BR2_TOOLCHAIN_HAS_THREADS),yy)
 FBTERM_CONF_ENV += LIBS='-lpthread'
 endif
 
+ifeq ($(BR2_PACKAGE_GPM),y)
+FBTERM_DEPENDENCIES += gpm
+FBTERM_CONF_OPTS += --enable-gpm
+else
+FBTERM_CONF_OPTS += --disable-gpm
+endif
+
 $(eval $(autotools-package))
