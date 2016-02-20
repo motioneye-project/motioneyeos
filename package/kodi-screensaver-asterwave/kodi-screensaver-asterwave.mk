ce9e7f73cb6ee0ddffb366b514629d1dff77cf6d
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-02-20 23:09:24 +0100

    package/kodi-screensaver-asterwave: new package
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/kodi-screensaver-asterwave/kodi-screensaver-asterwave.mk
@@ -0,0 +1,11 @@
+################################################################################
+#
+# kodi-screensaver-asterwave
+#
+################################################################################
+
+KODI_SCREENSAVER_ASTERWAVE_VERSION = 0dc2c48dadb100954eef823e7e3a5f502ce65b1e
+KODI_SCREENSAVER_ASTERWAVE_SITE = $(call github,notspiff,screensaver.asterwave,$(KODI_SCREENSAVER_ASTERWAVE_VERSION))
+KODI_SCREENSAVER_ASTERWAVE_DEPENDENCIES = kodi libsoil
+
+$(eval $(cmake-package))
