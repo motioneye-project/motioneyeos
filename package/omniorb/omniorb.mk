9d519412d65e13e4a7ffbb83edd8d1e4b2dfe4b7
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-02-21 16:18:52 +0100

    package/omniorb: add optional support for openssl
    When openssl was compiled before, omniorb will use it as optional
    dependency:
    
    $ output/host/usr/bin/x86_64-linux-readelf -a output/target/usr/lib/libomnisslTP4.so.2.0 | grep NEEDED
     0x0000000000000001 (NEEDED)             Shared library: [libomniORB4.so.2]
     0x0000000000000001 (NEEDED)             Shared library: [libomnithread.so.4]
     0x0000000000000001 (NEEDED)             Shared library: [libpthread.so.1]
     0x0000000000000001 (NEEDED)             Shared library: [libssl.so.1.0.0]
     0x0000000000000001 (NEEDED)             Shared library: [libcrypto.so.1.0.0]
    [...]
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/omniorb/omniorb.mk
+++ package/omniorb/omniorb.mk
@@ -30,6 +30,13 @@ HOST_OMNIORB_CONF_OPTS = ac_cv_path_PYTHON=$(HOST_DIR)/usr/bin/python2
 OMNIORB_CONF_OPTS += --disable-longdouble
 HOST_OMNIORB_CONF_OPTS += --disable-longdouble
 
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+OMNIORB_CONF_OPTS += --with-openssl
+OMNIORB_DEPENDENCIES += openssl
+else
+OMNIORB_CONF_OPTS += --without-openssl
+endif
+
 ifeq ($(BR2_PACKAGE_ZLIB),y)
 OMNIORB_DEPENDENCIES += zlib
 endif
