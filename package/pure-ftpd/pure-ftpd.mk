a40c74d5389722ebf46e422da486f52ba199056e
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-02-21 18:34:56 +0100

    package/pure-ftpd: add optional support for libsodium
    When libsodium was compiled before, pure-ftpd will use it as optional
    dependency:
    
    $ output/host/usr/bin/x86_64-linux-readelf -a output/target/usr/bin/pure-statsdecode | grep NEEDED
     0x0000000000000001 (NEEDED)             Shared library: [libsodium.so.17]
     0x0000000000000001 (NEEDED)             Shared library: [libcrypt.so.1]
     0x0000000000000001 (NEEDED)             Shared library: [libc.so.1]
    
    The build system offers no option to en-/disable libsodium support,
    check configure.ac, line 953.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/pure-ftpd/pure-ftpd.mk
+++ package/pure-ftpd/pure-ftpd.mk
@@ -27,6 +27,10 @@ else
 PURE_FTPD_CONF_OPTS += --without-capabilities
 endif
 
+ifeq ($(BR2_PACKAGE_LIBSODIUM),y)
+PURE_FTPD_DEPENDENCIES += libsodium
+endif
+
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 PURE_FTPD_CONF_OPTS += --with-tls
 PURE_FTPD_DEPENDENCIES += openssl
