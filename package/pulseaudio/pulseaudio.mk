2160eb681c3e62b74483a17daede8ae4a71e9e91
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-02-21 10:50:16 +0100

    package/pulseaudio: be explicit about udev usage
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    [Thomas: udev was already handled as an optional dependency, Bernd's
    patch only made it explicit by using
    --enable-libudev/--disable-libudev.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/pulseaudio/pulseaudio.mk
+++ package/pulseaudio/pulseaudio.mk
@@ -29,7 +29,6 @@ PULSEAUDIO_DEPENDENCIES = \
 	$(if $(BR2_PACKAGE_AVAHI_DAEMON),avahi) \
 	$(if $(BR2_PACKAGE_DBUS),dbus) \
 	$(if $(BR2_PACKAGE_BLUEZ_UTILS),bluez_utils) \
-	$(if $(BR2_PACKAGE_HAS_UDEV),udev) \
 	$(if $(BR2_PACKAGE_OPENSSL),openssl) \
 	$(if $(BR2_PACKAGE_FFTW),fftw) \
 	$(if $(BR2_PACKAGE_WEBRTC_AUDIO_PROCESSING),webrtc-audio-processing) \
@@ -77,6 +76,13 @@ PULSEAUDIO_CONF_OPTS += --with-soxr
 PULSEAUDIO_DEPENDENCIES += libsoxr
 else
 PULSEAUDIO_CONF_OPTS += --without-soxr
+endif
+
+ifeq ($(BR2_PACKAGE_HAS_UDEV),y)
+PULSEAUDIO_CONF_OPTS += --enable-libudev
+PULSEAUDIO_DEPENDENCIES += udev
+else
+PULSEAUDIO_CONF_OPTS += --disable-libudev
 endif
 
 ifneq ($(BR2_INSTALL_LIBSTDCPP),y)
