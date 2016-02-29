bfebfaec098bb92c255e55914292b7db5bcbccd9
Stanislav Vasic <svlasic@gmail.com>
2016-02-29 11:19:23 +0100

    libasplib: bump version and change source location
    Signed-off-by: Stanislav Vlasic <svlasic@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libasplib/libasplib.mk
+++ package/libasplib/libasplib.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LIBASPLIB_VERSION = 2fac1bf34efd210b95949fddcbd4a12d287d3e82
-LIBASPLIB_SITE = $(call github,kodi-adsp,asplib,$(LIBASPLIB_VERSION))
+LIBASPLIB_VERSION = f7219142e790a329b002a94f3db943abcb183739
+LIBASPLIB_SITE = $(call github,AchimTuran,asplib,$(LIBASPLIB_VERSION))
 LIBASPLIB_LICENSE = GPLv3+
 LIBASPLIB_LICENSE_FILES = LICENSE
 LIBASPLIB_INSTALL_STAGING = YES
