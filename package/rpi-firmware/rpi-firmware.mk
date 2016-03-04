b36a40994d94a4ca85ceda90a53703f36cbea3df
Calin Crisan <ccrisan@gmail.com>
2016-03-04 21:24:20 +0200

    raspberrypi(2): updated kernel and firmware

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 3442862c10fab68c2e88d660d2e69c143bb1f00c
+RPI_FIRMWARE_VERSION = b48c36f7431c45f3c1ce7c84036f2202a8b611cf
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
