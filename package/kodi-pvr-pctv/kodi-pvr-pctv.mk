c7e13ec36b4b3a2bff54219ea780104092a5e2be
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-03-05 19:58:52 +0100

    package/kodi-pvr-pctv: bump version
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/kodi-pvr-pctv/kodi-pvr-pctv.mk
+++ package/kodi-pvr-pctv/kodi-pvr-pctv.mk
@@ -6,7 +6,7 @@
 
 # This cset is on the branch 'Jarvis'
 # When Kodi is updated, then this should be updated to the corresponding branch
-KODI_PVR_PCTV_VERSION = 8b3ad52f63cd0abff4f4e39c1a5b96b868a8d817
+KODI_PVR_PCTV_VERSION = 0096770e96f84c46f444e159b9b737ac8b4238dc
 KODI_PVR_PCTV_SITE = $(call github,kodi-pvr,pvr.pctv,$(KODI_PVR_PCTV_VERSION))
 KODI_PVR_PCTV_LICENSE = GPLv2+
 KODI_PVR_PCTV_LICENSE_FILES = src/client.h
