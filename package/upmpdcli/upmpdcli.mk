affb1557ca5fc007438ee9019fe4aef107eafa32
Jörg Krause <joerg.krause@embedded.rocks>
2016-03-05 17:03:51 +0100

    package/upmpdcli: bump to version 1.1.2
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/upmpdcli/upmpdcli.mk
+++ package/upmpdcli/upmpdcli.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UPMPDCLI_VERSION = 1.1.0
+UPMPDCLI_VERSION = 1.1.2
 UPMPDCLI_SITE = http://www.lesbonscomptes.com/upmpdcli/downloads
 UPMPDCLI_LICENSE = GPLv2+
 UPMPDCLI_LICENSE_FILES = COPYING
