b88c34033ac85baeb141a6b4c6043fc0ca05067a
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-03-05 19:58:46 +0100

    package/kodi-pvr-hdhomerun: bump version
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/kodi-pvr-hdhomerun/kodi-pvr-hdhomerun.mk
+++ package/kodi-pvr-hdhomerun/kodi-pvr-hdhomerun.mk
@@ -6,7 +6,7 @@
 
 # This cset is on the branch 'Jarvis'
 # When Kodi is updated, then this should be updated to the corresponding branch
-KODI_PVR_HDHOMERUN_VERSION = 8d7b8af10633ad995193fb045cbaac6094a8f98d
+KODI_PVR_HDHOMERUN_VERSION = aa4324eaa5f738394f21d3f54667e346eb65038e
 KODI_PVR_HDHOMERUN_SITE = $(call github,kodi-pvr,pvr.hdhomerun,$(KODI_PVR_HDHOMERUN_VERSION))
 KODI_PVR_HDHOMERUN_LICENSE = GPLv2+
 KODI_PVR_HDHOMERUN_LICENSE_FILES = src/client.h
