fb7aec9fddfbcbc1d1a52d436fe1ff05ce2c633b
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-03-05 19:58:54 +0100

    package/kodi-pvr-vbox: bump version
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/kodi-pvr-vbox/kodi-pvr-vbox.mk
+++ package/kodi-pvr-vbox/kodi-pvr-vbox.mk
@@ -6,7 +6,7 @@
 
 # This cset is on the branch 'Jarvis'
 # When Kodi is updated, then this should be updated to the corresponding branch
-KODI_PVR_VBOX_VERSION = a846f6e85d894123986c39226564f9cc11c6f924
+KODI_PVR_VBOX_VERSION = 15e864d160da5a051e18aef06f3a53e49808be02
 KODI_PVR_VBOX_SITE = $(call github,kodi-pvr,pvr.vbox,$(KODI_PVR_VBOX_VERSION))
 KODI_PVR_VBOX_LICENSE = GPLv2+
 KODI_PVR_VBOX_LICENSE_FILES = src/client.h
