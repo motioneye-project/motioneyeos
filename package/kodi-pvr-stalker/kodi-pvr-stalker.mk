b55a5f10a48a62ec583268c46595bee8dd67d00b
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-03-05 19:58:53 +0100

    package/kodi-pvr-stalker: bump version
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/kodi-pvr-stalker/kodi-pvr-stalker.mk
+++ package/kodi-pvr-stalker/kodi-pvr-stalker.mk
@@ -6,7 +6,7 @@
 
 # This cset is on the branch 'Jarvis'
 # When Kodi is updated, then this should be updated to the corresponding branch
-KODI_PVR_STALKER_VERSION = e31351527b712c85b669eb876eb12f5809e400d7
+KODI_PVR_STALKER_VERSION = 5e588330c453141ae0a2f4fd9c02d909ac9d199e
 KODI_PVR_STALKER_SITE = $(call github,kodi-pvr,pvr.stalker,$(KODI_PVR_STALKER_VERSION))
 KODI_PVR_STALKER_LICENSE = GPLv2+
 KODI_PVR_STALKER_LICENSE_FILES = src/client.h
