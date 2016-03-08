52be26e90c08caf78617deb4f1af6746af0f5b95
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-03-08 18:31:20 -0300

    samba4: security bump to version 4.3.6
    Fixes:
    CVE-2015-7560 - Authenticated client could cause Samba to overwrite ACLs
    with incorrect owner/group.
    CVE-2016-0771 - Malicious request can cause the Samba internal DNS
    server to crash or unintentionally return uninitialized memory.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/samba4/samba4.mk
+++ package/samba4/samba4.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SAMBA4_VERSION = 4.3.5
+SAMBA4_VERSION = 4.3.6
 SAMBA4_SITE = http://ftp.samba.org/pub/samba/stable
 SAMBA4_SOURCE = samba-$(SAMBA4_VERSION).tar.gz
 SAMBA4_INSTALL_STAGING = YES
