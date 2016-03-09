0911f32d1c34b239cbb59e1b2d89d1b5cadee7e0
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-03-09 06:58:39 -0300

    libepoxy: move outside x11r7 scope
    Move it outside x11r7 scope since it can be built without it with a few
    patches (sent upstream via github pull request).
    
    Motivation is simple, it's a dependency for newer gtk3 versions which
    can work with a wayland or broadway backend, and having a full x11 stack
    is pointless for that scenario.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    [Thomas: split the first patch into four separate patches, since
    that's how they were submitted upstream.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/libepoxy/libepoxy.mk
@@ -0,0 +1,32 @@
+################################################################################
+#
+# libepoxy
+#
+################################################################################
+
+LIBEPOXY_VERSION = v1.3.1
+LIBEPOXY_SITE = $(call github,anholt,libepoxy,$(LIBEPOXY_VERSION))
+LIBEPOXY_INSTALL_STAGING = YES
+# For patches:
+# 0001-make-egl-and-glx-conditional.patch
+# 0002-Forward-egl-cflags-into-epoxy.pc.patch
+LIBEPOXY_AUTORECONF = YES
+LIBEPOXY_DEPENDENCIES = xutil_util-macros
+LIBEPOXY_LICENSE = MIT
+LIBEPOXY_LICENSE_FILES = COPYING
+
+ifeq ($(BR2_PACKAGE_HAS_LIBEGL),y)
+LIBEPOXY_CONF_OPTS += --enable-egl
+LIBEPOXY_DEPENDENCIES += libegl
+else
+LIBEPOXY_CONF_OPTS += --disable-egl
+endif
+
+ifeq ($(BR2_PACKAGE_HAS_LIBGL)$(BR2_PACKAGE_XLIB_LIBX11),yy)
+LIBEPOXY_CONF_OPTS += --enable-egl
+LIBEPOXY_DEPENDENCIES += libgl xlib_libX11
+else
+LIBEPOXY_CONF_OPTS += --disable-glx
+endif
+
+$(eval $(autotools-package))
