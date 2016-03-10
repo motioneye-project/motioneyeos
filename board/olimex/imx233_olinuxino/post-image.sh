0b7b84310c3781c346eea4c114c8cb4e97bbd8ea
Phil Eichinger <phil.eichinger@gmail.com>
2016-03-10 22:34:11 +0100

    olimex_imx233_olinuxino_defconfig: genimage support
    * add a post-image.sh script
    * update defconfig for genimage
    * update readme.txt
    
    [Peter: also add host-dosfstools dependency for vfat partition]
    Signed-off-by: Phil Eichinger <phil@zankapfel.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ board/olimex/imx233_olinuxino/post-image.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+BOARD_DIR="$(dirname $0)"
+BOARD_NAME="$(basename ${BOARD_DIR})"
+GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+# Create symlink to "rename" kernel image
+ln -sf uImage.imx23-olinuxino ${BINARIES_DIR}/uImage
+
+rm -rf "${GENIMAGE_TMP}"
+
+genimage                           \
+	--rootpath "${TARGET_DIR}"     \
+	--tmppath "${GENIMAGE_TMP}"    \
+	--inputpath "${BINARIES_DIR}"  \
+	--outputpath "${BINARIES_DIR}" \
+	--config "${GENIMAGE_CFG}"
+
+exit $?
