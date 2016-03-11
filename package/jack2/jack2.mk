ce4795e22e0b42b8d61711a358e00ffbc9190985
Antoine Ténart <antoine.tenart@free-electrons.com>
2016-03-11 19:00:34 +0100

    jack2: allow to choose the mode to operate jackd
    jack2 allows to use three modes:
    - A legacy jackd service alone.
    - A DBUS jackd service alone.
    - A mixture between the standalone jackd and the DBUS jackd.
    
    This patch adds the possibility to choose between these three modes,
    while the legacy jackd was the only option before.
    
    In addition, the jack_control tool is removed when the DBUS jackd is not
    available, as this tool is used to control it.
    
    Signed-off-by: Antoine Tenart <antoine.tenart@free-electrons.com>
    [yann.morin.1998@free.fr:
      - rebase
      - only use two booleans, not a choice
      - python is a runtime-only dependency
      - use python3 if enabled, fallback to python
      - simplify post-install condition
    ]
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    [Thomas: fix minor typo.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/jack2/jack2.mk
+++ package/jack2/jack2.mk
@@ -18,12 +18,27 @@ ifeq ($(BR2_PACKAGE_READLINE),y)
 JACK2_DEPENDENCIES += readline
 endif
 
+ifeq ($(BR2_PACKAGE_JACK2_LEGACY),y)
+JACK2_CONF_OPTS += --classic
+else
+define JACK2_REMOVE_JACK_CONTROL
+	$(RM) -f $(TARGET_DIR)/usr/bin/jack_control
+endef
+JACK2_POST_INSTALL_TARGET_HOOKS += JACK2_REMOVE_JACK_CONTROL
+endif
+
+ifeq ($(BR2_PACKAGE_JACK2_DBUS),y)
+JACK2_DEPENDENCIES += dbus
+JACK2_CONF_OPTS += --dbus
+endif
+
 define JACK2_CONFIGURE_CMDS
 	(cd $(@D); \
 		$(TARGET_CONFIGURE_OPTS)	\
 		$(HOST_DIR)/usr/bin/python2 ./waf configure \
 		--prefix=/usr			\
 		--alsa				\
+		$(JACK2_CONF_OPTS)		\
 	)
 endef
 
