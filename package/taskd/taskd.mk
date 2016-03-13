9b931ab7aafdb261086f0ff62dbdfff79914d14a
Arnout Vandecappelle <arnout@mind.be>
2016-03-14 00:00:08 +0100

    taskd: include only one license file
    COPYING and LICENSE are identical, so no point including them both.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/taskd/taskd.mk
+++ package/taskd/taskd.mk
@@ -7,7 +7,7 @@
 TASKD_VERSION = 1.1.0
 TASKD_SITE = http://taskwarrior.org/download
 TASKD_LICENSE = MIT
-TASKD_LICENSE_FILES = COPYING LICENSE
+TASKD_LICENSE_FILES = COPYING
 TASKD_DEPENDENCIES = gnutls util-linux
 
 $(eval $(cmake-package))
