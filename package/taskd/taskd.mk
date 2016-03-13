1031bf6a36e3c41328ebb9f9216a34b158fdb1e8
Arnout Vandecappelle <arnout@mind.be>
2016-03-14 00:00:07 +0100

    taskd: remove redundant _SOURCE definition
    .tar.gz is the default.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/taskd/taskd.mk
+++ package/taskd/taskd.mk
@@ -5,7 +5,6 @@
 ################################################################################
 
 TASKD_VERSION = 1.1.0
-TASKD_SOURCE = taskd-$(TASKD_VERSION).tar.gz
 TASKD_SITE = http://taskwarrior.org/download
 TASKD_LICENSE = MIT
 TASKD_LICENSE_FILES = COPYING LICENSE
