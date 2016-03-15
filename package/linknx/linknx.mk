f35f54585f9e63400bdbd9c9ba2b9fd67be538ea
Baruch Siach <baruch@tkos.co.il>
2016-03-15 15:03:48 +0200

    linknx: fix static build with mysql client
    Add a patch correcting the libraries order in the link command to fix static
    mysql libraries link.
    
    Fixes:
    http://autobuild.buildroot.net/results/2df/2dfcbd30079d13997de37e2d1c4643653e200be6/
    http://autobuild.buildroot.net/results/4cc/4cc9c8bd223d1c9d604322ba04060dd35fc8aa45/
    http://autobuild.buildroot.net/results/4bd/4bd04b7a3ca0092b50ad5c5212830d8935f54d48/
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/linknx/linknx.mk
+++ package/linknx/linknx.mk
@@ -8,6 +8,8 @@ LINKNX_VERSION = 0.0.1.32
 LINKNX_SITE = http://downloads.sourceforge.net/project/linknx/linknx/linknx-$(LINKNX_VERSION)
 LINKNX_LICENSE = GPLv2+
 LINKNX_INSTALL_STAGING = YES
+# Patching acinclude.m4
+LINKNX_AUTORECONF = YES
 LINKNX_CONF_OPTS = \
 	--without-lua \
 	--without-log4cpp \
@@ -16,6 +18,12 @@ LINKNX_CONF_OPTS = \
 	--disable-smtp
 
 LINKNX_DEPENDENCIES = libpthsem $(if $(BR2_PACKAGE_ARGP_STANDALONE),argp-standalone)
+
+# This is needed to make autoreconf happy
+define LINKNX_CREATE_MISSING_FILES
+	touch $(@D)/NEWS $(@D)/AUTHORS $(@D)/README
+endef
+LINKNX_POST_EXTRACT_HOOKS += LINKNX_CREATE_MISSING_FILES
 
 ifeq ($(BR2_PACKAGE_MYSQL),y)
 LINKNX_CONF_OPTS += --with-mysql=$(STAGING_DIR)/usr/bin/mysql_config
