8aafe0be1ece97b71d28bd7840031e54915a348a
Yegor Yefremov <yegorslists@googlemail.com>
2016-03-16 16:41:47 +0100

    python-pyroute2: bump to version 0.3.16
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-pyroute2/python-pyroute2.mk
+++ package/python-pyroute2/python-pyroute2.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-PYTHON_PYROUTE2_VERSION = 0.3.15
+PYTHON_PYROUTE2_VERSION = 0.3.16
 PYTHON_PYROUTE2_SOURCE = pyroute2-$(PYTHON_PYROUTE2_VERSION).tar.gz
 PYTHON_PYROUTE2_SITE = https://pypi.python.org/packages/source/p/pyroute2
 PYTHON_PYROUTE2_LICENSE = Apache-2.0 or GPLv2+
