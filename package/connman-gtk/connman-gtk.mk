4d12d53e23076ea48cfd50e4e911155dc04a0bfc
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-03-22 18:37:33 -0300

    connman-gtk: bump to version 1.1.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/connman-gtk/connman-gtk.mk
+++ package/connman-gtk/connman-gtk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CONNMAN_GTK_VERSION = 1.1
+CONNMAN_GTK_VERSION = 1.1.1
 CONNMAN_GTK_SITE = https://github.com/jgke/connman-gtk/releases/download/v$(CONNMAN_GTK_VERSION)
 CONNMAN_GTK_SOURCE = connman-gtk-$(CONNMAN_GTK_VERSION).tar.bz2
 CONNMAN_GTK_INSTALL_STAGING = YES
