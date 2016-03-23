3caa0d0e31607f7fe94462b8dcfc344f88998d92
Yegor Yefremov <yegorslists@googlemail.com>
2016-03-23 16:59:23 +0100

    python-idna: bump to version 2.1
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-idna/python-idna.mk
+++ package/python-idna/python-idna.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_IDNA_VERSION = 2.0
+PYTHON_IDNA_VERSION = 2.1
 PYTHON_IDNA_SOURCE = idna-$(PYTHON_IDNA_VERSION).tar.gz
 PYTHON_IDNA_SITE = https://pypi.python.org/packages/source/i/idna
 PYTHON_IDNA_LICENSE =  BSD-3c
