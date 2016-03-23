fbe3270ac3f0aba50cc31d2f346c21c40a2c3417
Yegor Yefremov <yegorslists@googlemail.com>
2016-03-23 16:59:24 +0100

    python-cryptography: bump to version 1.3.1
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-cryptography/python-cryptography.mk
+++ package/python-cryptography/python-cryptography.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_CRYPTOGRAPHY_VERSION = 1.2.3
+PYTHON_CRYPTOGRAPHY_VERSION = 1.3.1
 PYTHON_CRYPTOGRAPHY_SOURCE = cryptography-$(PYTHON_CRYPTOGRAPHY_VERSION).tar.gz
 PYTHON_CRYPTOGRAPHY_SITE = https://pypi.python.org/packages/source/c/cryptography
 PYTHON_CRYPTOGRAPHY_SETUP_TYPE = setuptools
