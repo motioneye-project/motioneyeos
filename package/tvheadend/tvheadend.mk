c70ad5880d304d184e6466d5f60a3438fd4062f7
Yann E. MORIN <yann.morin.1998@free.fr>
2016-03-28 17:15:47 +0200

    package/tvheadend: use wizard mode on first run
    The format of the ACL database in tvheadend has changed, and generating
    a default user is a little bit more involved than just dumping a file in
    the correct locations: filenames are now md5sum (of something?) and the
    usernames and passwords now have their own DB.
    
    However, tvheadend has a wizard mode, where it is possible to configure
    the basic features, of which creating an admin user.
    
    We remove our canned ACL database, and change the startup script to
    start in wizard mode on first run. We also switch to using our infra to
    set the permissions.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/tvheadend/tvheadend.mk
+++ package/tvheadend/tvheadend.mk
@@ -107,14 +107,8 @@ TVHEADEND_POST_INSTALL_TARGET_HOOKS += TVHEADEND_CLEAN_SHARE
 #----------------------------------------------------------------------------
 # To run tvheadend, we need:
 #  - a startup script, and its config file
-#  - a default DB with a tvheadend admin
-#  - a non-root user to run as
-define TVHEADEND_INSTALL_DB
-	$(INSTALL) -D -m 0600 package/tvheadend/accesscontrol.1     \
-		$(TARGET_DIR)/home/tvheadend/.hts/tvheadend/accesscontrol/1
-	chmod -R go-rwx $(TARGET_DIR)/home/tvheadend
-endef
-TVHEADEND_POST_INSTALL_TARGET_HOOKS += TVHEADEND_INSTALL_DB
+#  - a non-root user to run as, and a home for it that is not accessible
+#    to the other users (because there will be crendentials in there)
 
 define TVHEADEND_INSTALL_INIT_SYSV
 	$(INSTALL) -D package/tvheadend/etc.default.tvheadend $(TARGET_DIR)/etc/default/tvheadend
@@ -123,6 +117,9 @@ endef
 
 define TVHEADEND_USERS
 	tvheadend -1 tvheadend -1 * /home/tvheadend - video TVHeadend daemon
+endef
+define TVHEADEND_PERMISSIONS
+	/home/tvheadend r 0700 tvheadend tvheadend - - - - -
 endef
 
 $(eval $(generic-package))
