f457bc139f6827e7f8281469322659340ab45bd9
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-03-28 08:15:55 -0300

    pangomm: bump to version 2.40.0
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/pangomm/pangomm.mk
+++ package/pangomm/pangomm.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PANGOMM_VERSION_MAJOR = 2.38
-PANGOMM_VERSION = $(PANGOMM_VERSION_MAJOR).1
+PANGOMM_VERSION_MAJOR = 2.40
+PANGOMM_VERSION = $(PANGOMM_VERSION_MAJOR).0
 PANGOMM_SOURCE = pangomm-$(PANGOMM_VERSION).tar.xz
 PANGOMM_SITE = http://ftp.gnome.org/pub/gnome/sources/pangomm/$(PANGOMM_VERSION_MAJOR)
 PANGOMM_LICENSE = LGPLv2.1+ (library), GPLv2+ (tools)
