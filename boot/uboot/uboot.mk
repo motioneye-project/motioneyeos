1a807b4a893db66862b00c4ea3d9992af02a76e1
Pauli Sundberg <susundberg@gmail.com>
2016-03-29 20:22:57 +0300

    boot/uboot: Add custom Subversion repository support for the package
    Signed-off-by: Pauli Sundberg <susundberg@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- boot/uboot/uboot.mk
+++ boot/uboot/uboot.mk
@@ -24,6 +24,9 @@ UBOOT_SITE_METHOD = git
 else ifeq ($(BR2_TARGET_UBOOT_CUSTOM_HG),y)
 UBOOT_SITE = $(call qstrip,$(BR2_TARGET_UBOOT_CUSTOM_REPO_URL))
 UBOOT_SITE_METHOD = hg
+else ifeq ($(BR2_TARGET_UBOOT_CUSTOM_SVN),y)
+UBOOT_SITE = $(call qstrip,$(BR2_TARGET_UBOOT_CUSTOM_REPO_URL))
+UBOOT_SITE_METHOD = svn
 else
 # Handle stable official U-Boot versions
 UBOOT_SITE = ftp://ftp.denx.de/pub/u-boot
