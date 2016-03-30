ab61031a34df8875cec0d9c605f7f69840c11cfe
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-03-30 16:51:07 -0300

    nss: security bump to version 3.23
    Fixes:
    CVE-2016-1950 - Fixed a heap-based buffer overflow related to the
    parsing of certain ASN.1 structures. An attacker could create a
    specially-crafted certificate which, when parsed by NSS, would cause a
    crash or execution of arbitrary code with the permissions of the user.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libnss/libnss.mk
+++ package/libnss/libnss.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBNSS_VERSION = 3.22.2
+LIBNSS_VERSION = 3.23
 LIBNSS_SOURCE = nss-$(LIBNSS_VERSION).tar.gz
 LIBNSS_SITE = https://ftp.mozilla.org/pub/mozilla.org/security/nss/releases/NSS_$(subst .,_,$(LIBNSS_VERSION))_RTM/src
 LIBNSS_DISTDIR = dist
