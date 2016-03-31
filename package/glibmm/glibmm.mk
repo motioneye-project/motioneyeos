66348de5fbced51f7436c95da5f61e239de9c67e
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-03-31 16:12:36 -0300

    glibmm: bump to version 2.48.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/glibmm/glibmm.mk
+++ package/glibmm/glibmm.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 GLIBMM_VERSION_MAJOR = 2.48
-GLIBMM_VERSION = $(GLIBMM_VERSION_MAJOR).0
+GLIBMM_VERSION = $(GLIBMM_VERSION_MAJOR).1
 GLIBMM_LICENSE = LGPLv2.1+ (library), GPLv2+ (tools)
 GLIBMM_LICENSE_FILES = COPYING COPYING.tools
 GLIBMM_SOURCE = glibmm-$(GLIBMM_VERSION).tar.xz
