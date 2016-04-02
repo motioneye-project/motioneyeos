0f48c5a7e65323a36943182c9ccbcdafab9af4ec
Alexander Dahl <post@lespocky.de>
2016-04-02 12:38:27 +0200

    libuecc: bump to v7
    >From v6 to v7 just few changes were made for performance improvement and
    compatibility.
    
    Switched from git download to source tarballs and hashes.
    
    Signed-off-by: Alexander Dahl <post@lespocky.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libuecc/libuecc.mk
+++ package/libuecc/libuecc.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-LIBUECC_VERSION = v6
-LIBUECC_SITE = git://git.universe-factory.net/libuecc
+LIBUECC_VERSION = 7
+LIBUECC_SITE = https://projects.universe-factory.net/attachments/download/85
+LIBUECC_SOURCE = libuecc-$(LIBUECC_VERSION).tar.xz
 LIBUECC_LICENSE = BSD-2c
 LIBUECC_LICENSE_FILES = COPYRIGHT
 LIBUECC_INSTALL_STAGING = YES
