b0b28b776f4e8b74c43a60e86e279941ce84c4da
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-04-10 19:15:01 +0200

    package/x11r7/xdriver_xf86-video-ast: bump version to 1.1.5
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xdriver_xf86-video-ast/xdriver_xf86-video-ast.mk
+++ package/x11r7/xdriver_xf86-video-ast/xdriver_xf86-video-ast.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_AST_VERSION = 1.0.1
+XDRIVER_XF86_VIDEO_AST_VERSION = 1.1.5
 XDRIVER_XF86_VIDEO_AST_SOURCE = xf86-video-ast-$(XDRIVER_XF86_VIDEO_AST_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_AST_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_AST_LICENSE = MIT
