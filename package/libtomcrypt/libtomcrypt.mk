cbed1c3b4a89e4c4d4b5cc715b1fd7202ad36881
Francois Perrad <fperrad@gmail.com>
2016-04-10 16:55:25 +0200

    libtomcrypt: new package
    [Peter: fix building without wchar]
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/libtomcrypt/libtomcrypt.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# libtomcrypt
+#
+################################################################################
+
+LIBTOMCRYPT_VERSION = 1.17
+LIBTOMCRYPT_SITE = https://github.com/libtom/libtomcrypt/releases/download/$(LIBTOMCRYPT_VERSION)
+LIBTOMCRYPT_SOURCE = crypt-$(LIBTOMCRYPT_VERSION).tar.bz2
+LIBTOMCRYPT_LICENSE = WTFPL
+LIBTOMCRYPT_LICENSE_FILES = LICENSE
+LIBTOMCRYPT_INSTALL_STAGING = YES
+LIBTOMCRYPT_INSTALL_TARGET = NO # only static library
+LIBTOMCRYPT_DEPENDENCIES = libtommath
+
+LIBTOMCRYPT_CFLAGS = -I./src/headers $(TARGET_CFLAGS) -DLTC_SOURCE -DLTM_DESC \
+	$(if $(BR2_USE_WCHAR),,-DLTC_NO_WCHAR)
+
+define LIBTOMCRYPT_BUILD_CMDS
+	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS) CFLAGS="$(LIBTOMCRYPT_CFLAGS)"
+endef
+
+define LIBTOMCRYPT_INSTALL_STAGING_CMDS
+	$(MAKE) -C $(@D) DESTDIR="$(STAGING_DIR)" NODOCS=1 INSTALL_USER=$(shell id -u) INSTALL_GROUP=$(shell id -g) install
+endef
+
+$(eval $(generic-package))
