6a8b94819c7a747e84b4fd02a86f437f4321b208
Gary Bisson <gary.bisson@boundarydevices.com>
2016-04-13 12:40:52 +0200

    imx-uuc: new package
    This package provides the Universal Adapter user-space utility that is
    used to receive commands from the Manufacturing Tool using the Freescale
    UTP Protocol.
    
    It requires a Freescale/NXP kernels whose configuration contains the
    CONFIG_FSL_UTP option.
    
    The /fat file is provided as a bootargs to the g_mass_storage driver
    from U-Boot, see:
    http://git.freescale.com/git/cgit.cgi/imx/uboot-imx.git/tree/include/
    configs/mx6sabre_common.h?h=imx_v2015.04_3.14.52_1.1.0_ga#n116
    
    Init scripts are provided so that the tool starts automatically at
    bootup.
    
    Tested on Nitrogen6_MAX + MFGTools.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
    [Thomas:
     - test return value from start-stop⁻daemon in init script, and
       reindent the init script
     - fix dependency of the comment
     - rewrap Config.in help text.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/freescale-imx/imx-uuc/imx-uuc.mk
@@ -0,0 +1,40 @@
+################################################################################
+#
+# imx-uuc
+#
+################################################################################
+
+IMX_UUC_VERSION = 0.5.1
+IMX_UUC_SITE = $(FREESCALE_IMX_SITE)
+IMX_UUC_LICENSE = GPLv2+
+IMX_UUC_LICENSE_FILES = COPYING
+
+# mkfs.vfat is needed to create a FAT partition used by g_mass_storage
+# so Windows do not offer to format the device when connected to the PC.
+IMX_UUC_DEPENDENCIES = host-dosfstools
+
+define IMX_UUC_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) CC=$(TARGET_CC)
+endef
+
+define IMX_UUC_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/uuc $(TARGET_DIR)/usr/bin/uuc
+	$(INSTALL) -D -m 755 $(@D)/sdimage $(TARGET_DIR)/usr/bin/sdimage
+	dd if=/dev/zero of=$(TARGET_DIR)/fat bs=1M count=1
+	$(HOST_DIR)/usr/sbin/mkfs.vfat $(TARGET_DIR)/fat
+endef
+
+define IMX_UUC_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 package/freescale-imx/imx-uuc/S80imx-uuc \
+		$(TARGET_DIR)/etc/init.d/S80imx-uuc
+endef
+
+define IMX_UUC_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 0644 package/freescale-imx/imx-uuc/imx-uuc.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/imx-uuc.service
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/
+	ln -fs ../../../../usr/lib/systemd/system/imx-uuc.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/imx-uuc.service
+endef
+
+$(eval $(generic-package))
