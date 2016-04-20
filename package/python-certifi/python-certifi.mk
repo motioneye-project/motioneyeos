dfb53d30bf824f5bac36082c118f1af83639b3e2
Yegor Yefremov <yegorslists@googlemail.com>
2016-04-20 10:41:56 +0200

    python-certifi: bump to version 2016.2.28
    Use upstream MD5 checksum.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-certifi/python-certifi.mk
+++ package/python-certifi/python-certifi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_CERTIFI_VERSION = 14.05.14
+PYTHON_CERTIFI_VERSION = 2016.2.28
 PYTHON_CERTIFI_SOURCE = certifi-$(PYTHON_CERTIFI_VERSION).tar.gz
 PYTHON_CERTIFI_SITE = https://pypi.python.org/packages/source/c/certifi
 PYTHON_CERTIFI_SETUP_TYPE = setuptools
