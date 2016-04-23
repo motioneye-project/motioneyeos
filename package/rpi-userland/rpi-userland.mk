2ce7c6a9a1e1a6e6afe44122ee4f1bd43063c083
Peter Seiderer <ps.report@gmx.net>
2016-04-23 18:46:22 +0200

    rpi-userland: bump version
    - remove upstream commited patch:
      0002-makefiles-cmake-vmcs.cmake-allow-to-override-VMCS_IN.patch
    
    - fix compile errror (missing include for va_list with uclibc-1.0.14):
      0006-dtmerge-add-missing-include-for-va_list.patch
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_USERLAND_VERSION = 8369e390999f4a7c3bc57e577247e0dd502c51f7
+RPI_USERLAND_VERSION = 52eca998aa43f278314579dba4798a3fe1da46c3
 RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
 RPI_USERLAND_LICENSE = BSD-3c
 RPI_USERLAND_LICENSE_FILES = LICENCE
