6f8851d49cb1a28fb2110a788da8d856e9a8d7b6
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-04-28 16:26:23 +0200

    package/libdrm: bump version to 2.4.68
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libdrm/libdrm.mk
+++ package/libdrm/libdrm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBDRM_VERSION = 2.4.67
+LIBDRM_VERSION = 2.4.68
 LIBDRM_SOURCE = libdrm-$(LIBDRM_VERSION).tar.bz2
 LIBDRM_SITE = http://dri.freedesktop.org/libdrm
 LIBDRM_LICENSE = MIT
