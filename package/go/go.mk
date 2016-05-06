abfaf47e4c4054cac9e8869f43ddf77c28fbb588
Christian Stewart <christian@paral.in>
2016-05-06 12:23:32 -0400

    package/go: bump version to 1.6.2
    Golang has significant improvements to support for multiarch in later
    versions. This bump is required to make many go programs functional
    under arm64, for example.
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    Acked-by: Geoff Levand <geoff@infradead.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.5.3
+GO_VERSION = 1.6.2
 GO_SITE = https://storage.googleapis.com/golang
 GO_SOURCE = go$(GO_VERSION).src.tar.gz
 
