7103990107073f486ef8cde15ed3888da3d3927e
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-05-09 18:24:48 -0300

    smartmontools: bump to version 6.5
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/smartmontools/smartmontools.mk
+++ package/smartmontools/smartmontools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SMARTMONTOOLS_VERSION = 6.4
+SMARTMONTOOLS_VERSION = 6.5
 SMARTMONTOOLS_SITE = http://downloads.sourceforge.net/project/smartmontools/smartmontools/$(SMARTMONTOOLS_VERSION)
 SMARTMONTOOLS_LICENSE = GPLv2+
 SMARTMONTOOLS_LICENSE_FILES = COPYING
