0401ccc325632ff47b5475ab39b463ae7348f1f6
Yegor Yefremov <yegorslists@googlemail.com>
2016-05-10 10:40:06 +0200

    lksctp-tools: bump to version 1.0.17
    Remove upstreamed patch and autoreconf related hook. Add
    --disable-tests config option to reduce build time.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lksctp-tools/lksctp-tools.mk
+++ package/lksctp-tools/lksctp-tools.mk
@@ -4,19 +4,14 @@
 #
 ################################################################################
 
-LKSCTP_TOOLS_VERSION = 1.0.16
+LKSCTP_TOOLS_VERSION = 1.0.17
 LKSCTP_TOOLS_SITE = http://downloads.sourceforge.net/project/lksctp/lksctp-tools
 LKSCTP_TOOLS_INSTALL_STAGING = YES
 # configure not shipped
 LKSCTP_TOOLS_AUTORECONF = YES
 LKSCTP_TOOLS_LICENSE = LGPLv2.1 (library), GPLv2+ (programs)
 LKSCTP_TOOLS_LICENSE_FILES = COPYING.lib COPYING
-
-# Needed by autoreconf
-define LKSCTP_TOOLS_MAKE_M4
-	mkdir -p $(@D)/m4
-endef
-LKSCTP_TOOLS_POST_PATCH_HOOKS += LKSCTP_TOOLS_MAKE_M4
+LKSCTP_TOOLS_CONF_OPTS = --disable-tests
 
 # Cleanup installed target source code
 define LKSCTP_TOOLS_CLEANUP_TARGET
