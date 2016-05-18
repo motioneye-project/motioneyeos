279f4a0eaa54a003b7f245145de952b1d5188e2a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-05-18 16:23:17 -0300

    gdbm: bump to version 1.12
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gdbm/gdbm.mk
+++ package/gdbm/gdbm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GDBM_VERSION = 1.11
+GDBM_VERSION = 1.12
 GDBM_SITE = $(BR2_GNU_MIRROR)/gdbm
 GDBM_LICENSE = GPLv3+
 GDBM_LICENSE_FILES = COPYING
