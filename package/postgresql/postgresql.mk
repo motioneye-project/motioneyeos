02c43157ab09f6fe4078fd1145fc9d403d66c089
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-05-21 11:06:46 +0200

    package/postgresql: fix snprintf() detection
    https://git.busybox.net/buildroot/commit/package/postgresql/postgresql.mk?id=3d3f70b567718ae308b5704ddd7d107c4849309e
    forced postgresql not to use its own snprintf() implementation which
    needs libm. This fix got broken after this upstream commit
    
    http://git.postgresql.org/gitweb/?p=postgresql.git;a=commitdiff;h=ce486056ecd28050f367894a2b5aad3656d37511#patch1
    
    changed the variable name from
      pgac_cv_snprintf_long_long_int_format
    to
      pgac_cv_snprintf_long_long_int_modifier
    
    leading to problems in the rsyslog package while detecting postgresql
    support.
    
    Fixes
    http://autobuild.buildroot.net/results/1cd/1cda32983fdf9522e4a9d48cc1304cc1451771a9/
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/postgresql/postgresql.mk
+++ package/postgresql/postgresql.mk
@@ -12,7 +12,7 @@ POSTGRESQL_LICENSE_FILES = COPYRIGHT
 POSTGRESQL_INSTALL_STAGING = YES
 POSTGRESQL_CONFIG_SCRIPTS = pg_config
 POSTGRESQL_CONF_ENV = ac_cv_type_struct_sockaddr_in6=yes \
-		      pgac_cv_snprintf_long_long_int_format="%lld" \
+		      pgac_cv_snprintf_long_long_int_modifier="%lld" \
 		      pgac_cv_snprintf_size_t_support=yes
 POSTGRESQL_CONF_OPTS = --disable-rpath
 
