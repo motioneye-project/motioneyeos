b9a0903cfeec867b80bd646903af3f149eeefc4e
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2016-06-01 11:21:36 +0100

    php: security bump version to 7.0.7
    Fixes CVE-2013-7456 https://bugs.php.net/bug.php?id=72227
    Fixes CVE-2016-5093 https://bugs.php.net/bug.php?id=72241
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PHP_VERSION = 7.0.6
+PHP_VERSION = 7.0.7
 PHP_SITE = http://www.php.net/distributions
 PHP_SOURCE = php-$(PHP_VERSION).tar.xz
 PHP_INSTALL_STAGING = YES
