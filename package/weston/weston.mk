fb6e2101cd9faaf28040be958ce6ab19f7458db5
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-06-01 08:37:16 -0300

    weston: bump to version 1.11.0
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/weston/weston.mk
+++ package/weston/weston.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WESTON_VERSION = 1.10.0
+WESTON_VERSION = 1.11.0
 WESTON_SITE = http://wayland.freedesktop.org/releases
 WESTON_SOURCE = weston-$(WESTON_VERSION).tar.xz
 WESTON_LICENSE = MIT
