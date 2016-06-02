5e230f4b89fecbad9cda8dcd12b21769008de8e2
Yegor Yefremov <yegorslists@googlemail.com>
2016-06-02 16:05:30 +0200

    python-flask: bump to version 0.11
    Change download location and rework dependencies, i.e.
    select Python's zlib support and python-click package
    and remove unneeded build-time dependencies.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-flask/python-flask.mk
+++ package/python-flask/python-flask.mk
@@ -4,12 +4,11 @@
 #
 ################################################################################
 
-PYTHON_FLASK_VERSION = 0.10.1
+PYTHON_FLASK_VERSION = 0.11
 PYTHON_FLASK_SOURCE = Flask-$(PYTHON_FLASK_VERSION).tar.gz
-PYTHON_FLASK_SITE = http://pypi.python.org/packages/source/F/Flask
+PYTHON_FLASK_SITE = https://pypi.python.org/packages/dc/ca/c0ed9cc90c079085c698e284b672edbc1ffd6866b1830574095cbc5b7752
 PYTHON_FLASK_SETUP_TYPE = setuptools
 PYTHON_FLASK_LICENSE = BSD-3c
 PYTHON_FLASK_LICENSE_FILES = LICENSE
-PYTHON_FLASK_DEPENDENCIES = python-werkzeug python-jinja2 python-itsdangerous
 
 $(eval $(python-package))
