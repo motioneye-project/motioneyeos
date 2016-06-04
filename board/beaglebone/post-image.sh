7d0b7d3c27a6795667e1aeb18e9978e1a44361e5
Ezequiel Garcia <ezequiel@vanguardiasur.com.ar>
2016-06-04 18:29:09 -0300

    configs/beaglebone: Use genimage to produce the SD card image
    Signed-off-by: Ezequiel Garcia <ezequiel@vanguardiasur.com.ar>
    [Thomas: added host dosfstools to the defconfig, needed to build a SD
    card image with genimage when a FAT partition is listed in the
    genimage configuration.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- board/beaglebone/post-image.sh
+++ board/beaglebone/post-image.sh
@@ -2,5 +2,19 @@
 # post-image.sh for BeagleBone
 # 2014, Marcin Jabrzyk <marcin.jabrzyk@gmail.com>
 
+BOARD_DIR="$(dirname $0)"
+
 # copy the uEnv.txt to the output/images directory
 cp board/beaglebone/uEnv.txt $BINARIES_DIR/uEnv.txt
+
+GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+rm -rf "${GENIMAGE_TMP}"
+
+genimage \
+    --rootpath "${TARGET_DIR}" \
+    --tmppath "${GENIMAGE_TMP}" \
+    --inputpath "${BINARIES_DIR}" \
+    --outputpath "${BINARIES_DIR}" \
+    --config "${GENIMAGE_CFG}"
