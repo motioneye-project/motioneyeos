c8d56fb62173a6f6ffb97c005975d89a51f4f78b
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-06-05 23:26:00 +0200

    package/emlog: bump version
    Removed patch applied upstream:
    https://github.com/nicupavel/emlog/commit/576a6c0fcf76dfeff48124b2f5fc0b4207d88ca1
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/emlog/emlog.mk
+++ package/emlog/emlog.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EMLOG_VERSION = bd32494ad757c3d37469877aaf99ced3ee6ca3f8
+EMLOG_VERSION = 576a6c0fcf76dfeff48124b2f5fc0b4207d88ca1
 EMLOG_SITE = $(call github,nicupavel,emlog,$(EMLOG_VERSION))
 EMLOG_LICENSE = GPLv2
 EMLOG_LICENSE_FILES = COPYING
