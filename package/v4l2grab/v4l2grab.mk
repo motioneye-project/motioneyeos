2da1a7ad753df5663697355deeb1dafa51d7a988
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-06-05 19:52:12 +0200

    package/v4l2grab: bump version
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/v4l2grab/v4l2grab.mk
+++ package/v4l2grab/v4l2grab.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-V4L2GRAB_VERSION = 6a52a234e227a30e16591d1a0e7afc52a2c5d964
+V4L2GRAB_VERSION = f8d8844d52387b3db7b8736f5e86156d9374f781
 V4L2GRAB_SITE = $(call github,twam,v4l2grab,$(V4L2GRAB_VERSION))
 V4L2GRAB_LICENSE = GPLv2+
 V4L2GRAB_LICENSE_FILES = LICENSE.md
