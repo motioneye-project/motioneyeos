9cfc842ae62e32abab00aedb310e61e7b8060a9a
Peter Korsgaard <peter@korsgaard.com>
2016-06-15 15:17:47 +0200

    rsyslog: bump version to 8.19.0
    And drop patch now upstream. Rsyslog now uses libfastjson instead of json-c,
    so adjust dependencies.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/rsyslog/rsyslog.mk
+++ package/rsyslog/rsyslog.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-RSYSLOG_VERSION = 8.9.0
+RSYSLOG_VERSION = 8.19.0
 RSYSLOG_SITE = http://rsyslog.com/files/download/rsyslog
 RSYSLOG_LICENSE = GPLv3, LGPLv3, Apache-2.0
 RSYSLOG_LICENSE_FILES = COPYING COPYING.LESSER COPYING.ASL20
-RSYSLOG_DEPENDENCIES = zlib libestr liblogging json-c host-pkgconf
+RSYSLOG_DEPENDENCIES = zlib libestr liblogging libfastjson host-pkgconf
 RSYSLOG_CONF_ENV = ac_cv_prog_cc_c99='-std=c99'
 RSYSLOG_PLUGINS = imdiag imfile impstats imptcp \
 	mmanon mmaudit mmfields mmjsonparse mmpstrucdata mmsequence mmutf8fix \
