1474b369638e84090d3f6c2b51a5269076d00be5
Martin Bark <martin@barkynet.com>
2016-06-16 17:00:49 +0100

    package/ucl: fix build with host gcc 6.x
    ucl fails with the following error when using gcc 6.x on the host:
    
     checking whether your compiler passes the ACC conformance test... FAILED
     configure:
     configure: Your compiler failed the ACC conformance test - for details see
     configure: `config.log'. Please check that log file and consider sending
     configure: a patch or bug-report to <markus@oberhumer.com>.
     configure: Thanks for your support.
     configure:
     configure: error: ACC conformance test failed. Stop.
    
    Fix the issue by using the ISO C90 standard.
    
    Signed-off-by: Martin Bark <martin@barkynet.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ucl/ucl.mk
+++ package/ucl/ucl.mk
@@ -9,4 +9,7 @@ UCL_SITE = http://www.oberhumer.com/opensource/ucl/download
 UCL_LICENSE = GPLv2+
 UCL_LICENSE_FILES = COPYING
 
+# Fix ACC conformance test failure for host gcc 6.x
+HOST_UCL_CONF_ENV += CPPFLAGS="$(HOST_CPPFLAGS) -std=c90"
+
 $(eval $(host-autotools-package))
