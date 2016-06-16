76c1961145a95282b5af326c572d7f4364bae4d9
Yegor Yefremov <yegorslists@googlemail.com>
2016-06-16 11:34:50 +0200

    python-sdnotify: bump to version 0.3.1
    LICENSE.txt is now part of the PyPi's archive.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-sdnotify/python-sdnotify.mk
+++ package/python-sdnotify/python-sdnotify.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-PYTHON_SDNOTIFY_VERSION = 0.3.0
+PYTHON_SDNOTIFY_VERSION = 0.3.1
 PYTHON_SDNOTIFY_SOURCE = sdnotify-$(PYTHON_SDNOTIFY_VERSION).tar.gz
-PYTHON_SDNOTIFY_SITE = https://pypi.python.org/packages/6f/ef/d57f930ba403b2e6440bd7ab14dbdd7ce1a1bfb5617bba62a950d04bfa34
+PYTHON_SDNOTIFY_SITE = https://pypi.python.org/packages/57/f9/ae03e3ebc83be0d501cde1f5d6d23dee74f5c2105f2cdb98bff4fa9ada9c
 PYTHON_SDNOTIFY_SETUP_TYPE = distutils
 PYTHON_SDNOTIFY_LICENSE = MIT
+PYTHON_SDNOTIFY_LICENSE_FILES = LICENSE.txt
 
 $(eval $(python-package))
