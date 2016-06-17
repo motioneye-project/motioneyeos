a97dc46d7e7fcdf83020371d4ae3fe8d886d8aee
Peter Seiderer <ps.report@gmx.net>
2016-06-18 00:13:24 +0200

    libevdev: bump version to 1.5.2
    For details see [1].
    
    [1] https://lists.freedesktop.org/archives/input-tools/2016-June/001307.html
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libevdev/libevdev.mk
+++ package/libevdev/libevdev.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBEVDEV_VERSION = 1.5.1
+LIBEVDEV_VERSION = 1.5.2
 LIBEVDEV_SITE = http://www.freedesktop.org/software/libevdev
 LIBEVDEV_SOURCE = libevdev-$(LIBEVDEV_VERSION).tar.xz
 LIBEVDEV_LICENSE = X11
