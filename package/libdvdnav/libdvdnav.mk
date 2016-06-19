80d3e50c9e326ea361f10aa92b34e03fbe9a91b6
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-06-19 18:20:24 +0200

    package/libdvdnav: bump version to 5.0.3
    Removed 0001-fix-os2-support.patch, not needed anymore after the
    version bump according to Thomas' note in the patch file.
    
    Removed autoreconf, a configure script is now part of the upstream
    tarball.
    
    The script dvdnav-config was removed:
    https://code.videolan.org/videolan/libdvdnav/commit/1de99510f507e4d84a658411605dd6424466d7b5
    
    libdvdnav uses pkg-config to find libdvdread, therefore remove
    --with-dvdread-config:
    https://code.videolan.org/videolan/libdvdnav/commit/5254b5d7ad9c10b48c97ce72a1b8bf7aef54b12c
    
    Changed LIBDVDNAV_SITE according to the news post dating 13 Feb 2014
    from http://dvdnav.mplayerhq.hu/
    
    Enabled static build, tested using this defconfig:
    http://autobuild.buildroot.net/toolchains/configs/br-arm-full-static.config
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libdvdnav/libdvdnav.mk
+++ package/libdvdnav/libdvdnav.mk
@@ -4,19 +4,12 @@
 #
 ################################################################################
 
-LIBDVDNAV_VERSION = 4.2.1
-LIBDVDNAV_SOURCE = libdvdnav-$(LIBDVDNAV_VERSION).tar.xz
-LIBDVDNAV_SITE = http://dvdnav.mplayerhq.hu/releases
-# configure not shipped
-LIBDVDNAV_AUTORECONF = YES
+LIBDVDNAV_VERSION = 5.0.3
+LIBDVDNAV_SOURCE = libdvdnav-$(LIBDVDNAV_VERSION).tar.bz2
+LIBDVDNAV_SITE = http://www.videolan.org/pub/videolan/libdvdnav/$(LIBDVDNAV_VERSION)
 LIBDVDNAV_INSTALL_STAGING = YES
-LIBDVDNAV_CONFIG_SCRIPTS = dvdnav-config
 LIBDVDNAV_DEPENDENCIES = libdvdread host-pkgconf
 LIBDVDNAV_LICENSE = GPLv2+
 LIBDVDNAV_LICENSE_FILES = COPYING
-
-# By default libdvdnav tries to find dvdread-config in $PATH. Because
-# of cross compilation, we prefer using pkg-config.
-LIBDVDNAV_CONF_OPTS = --with-dvdread-config="$(PKG_CONFIG_HOST_BINARY) dvdread"
 
 $(eval $(autotools-package))
