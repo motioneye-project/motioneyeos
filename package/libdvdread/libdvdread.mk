0306b5926493e3b153ed32f20f131f0407552bdb
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-06-19 18:20:26 +0200

    package/libdvdread: add optional support for libdvdcss
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libdvdread/libdvdread.mk
+++ package/libdvdread/libdvdread.mk
@@ -11,4 +11,11 @@ LIBDVDREAD_INSTALL_STAGING = YES
 LIBDVDREAD_LICENSE = GPLv2+
 LIBDVDREAD_LICENSE_FILES = COPYING
 
+ifeq ($(BR2_PACKAGE_LIBDVDCSS),y)
+LIBDVDREAD_CONF_OPTS += --with-libdvdcss
+LIBDVDREAD_DEPENDENCIES += libdvdcss
+else
+LIBDVDREAD_CONF_OPTS += --without-libdvdcss
+endif
+
 $(eval $(autotools-package))
