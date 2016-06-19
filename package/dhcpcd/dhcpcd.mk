5e3de4bb466d15d0b9d5ae3a2159238839e016ca
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-06-18 23:58:22 -0300

    dhcpcd: bump to version 6.11.1
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/dhcpcd/dhcpcd.mk
+++ package/dhcpcd/dhcpcd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DHCPCD_VERSION = 6.11.0
+DHCPCD_VERSION = 6.11.1
 DHCPCD_SOURCE = dhcpcd-$(DHCPCD_VERSION).tar.xz
 DHCPCD_SITE = http://roy.marples.name/downloads/dhcpcd
 DHCPCD_DEPENDENCIES = host-pkgconf
