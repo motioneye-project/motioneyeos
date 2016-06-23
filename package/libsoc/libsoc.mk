5c285a79e0fca1096bb64411a47eac4d8b5b71ef
Yegor Yefremov <yegorslists@googlemail.com>
2016-06-23 07:41:56 +0200

    libsoc: bump to version 0.8.2
    License file changed from COPYING to LICENCE.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libsoc/libsoc.mk
+++ package/libsoc/libsoc.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-LIBSOC_VERSION = 0.7.1
+LIBSOC_VERSION = 0.8.2
 LIBSOC_SITE = $(call github,jackmitch,libsoc,$(LIBSOC_VERSION))
 LIBSOC_LICENSE = LGPLv2.1
-LIBSOC_LICENSE_FILES = COPYING
+LIBSOC_LICENSE_FILES = LICENCE
 LIBSOC_AUTORECONF = YES
 LIBSOC_INSTALL_STAGING = YES
 
