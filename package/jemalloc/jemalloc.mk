e6cdb4dd3e32a6de61118cec6237aa75f2a83754
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2016-06-24 21:46:30 +0200

    jemalloc: new package
    This new package provides jemalloc, a malloc(3) implementation that
    emphasizes fragmentation avoidance and scalable concurrency support.
    
    A host variant is added as it will be used by rust.
    
    Signed-off-by: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    [Thomas:
     - remove trailing slash (noticed by Romain Naour)
     - add !BR2_STATIC_LIBS dependency (noticed by Romain Naour).]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/jemalloc/jemalloc.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# jemalloc
+#
+################################################################################
+
+JEMALLOC_VERSION = 4.2.0
+JEMALLOC_SOURCE = jemalloc-$(JEMALLOC_VERSION).tar.bz2
+JEMALLOC_SITE = http://www.canonware.com/download/jemalloc
+JEMALLOC_LICENSE = BSD-2c
+JEMALLOC_LICENSE_FILES = COPYING
+JEMALLOC_INSTALL_STAGING = YES
+
+$(eval $(autotools-package))
+$(eval $(host-autotools-package))
