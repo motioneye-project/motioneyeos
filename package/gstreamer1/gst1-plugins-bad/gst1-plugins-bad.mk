5baa92b0925fbd784c5be96eaed94d70d2286714
Marcin Nowakowski <marcin.nowakowski@imgtec.com>
2016-06-24 08:12:21 +0200

    gst1-plugins-bad: cleanup of build options
    Remove non-existing options:
     * --disable-wsap
     * --disable-direct3d
     * --disable-gsettings
    
    Remove options that are already handled later in the .mk file, using
    optional dependencies:
     * --disable-rtmp
     * --disable-hls
     * --disable-dash
    
    Rename disable->strp to disable-srtp, which essentially fixes a typo.
    
    Remove liveadder plugin - no longer a separate built option, it's been
    merged into audiomixer. Config.in.legacy handling is added for the
    removed option.
    
    Signed-off-by: Marcin Nowakowski <marcin.nowakowski@imgtec.com>
    [Thomas: add Config.in.legacy handling for the liveaddr plugin option,
    tweaks to the commit log.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gstreamer1/gst1-plugins-bad/gst1-plugins-bad.mk
+++ package/gstreamer1/gst1-plugins-bad/gst1-plugins-bad.mk
@@ -17,9 +17,7 @@ GST1_PLUGINS_BAD_CONF_OPTS = \
 	--disable-examples \
 	--disable-valgrind \
 	--disable-directsound \
-	--disable-wsapi \
 	--disable-direct3d \
-	--disable-direct3d9 \
 	--disable-winks \
 	--disable-android_media \
 	--disable-apple_media \
@@ -35,7 +33,6 @@ GST1_PLUGINS_BAD_CONF_OPTS += \
 	--disable-voamrwbenc \
 	--disable-bs2b \
 	--disable-chromaprint \
-	--disable-dash \
 	--disable-dc1394 \
 	--disable-dts \
 	--disable-resindvd \
@@ -47,7 +44,7 @@ GST1_PLUGINS_BAD_CONF_OPTS += \
 	--disable-ladspa \
 	--disable-lv2 \
 	--disable-libde265 \
-	--disable-strp \
+	--disable-srtp \
 	--disable-linsys \
 	--disable-modplug \
 	--disable-mimic \
@@ -69,11 +66,8 @@ GST1_PLUGINS_BAD_CONF_OPTS += \
 	--disable-vdpau \
 	--disable-schro \
 	--disable-zbar \
-	--disable-rtmp \
 	--disable-spandsp \
-	--disable-gsettings \
 	--disable-sndio \
-	--disable-hls \
 	--disable-gtk3 \
 	--disable-qt
 
@@ -374,12 +368,6 @@ ifeq ($(BR2_PACKAGE_GST1_PLUGINS_BAD_PLUGIN_LIBRFB),y)
 GST1_PLUGINS_BAD_CONF_OPTS += --enable-librfb
 else
 GST1_PLUGINS_BAD_CONF_OPTS += --disable-librfb
-endif
-
-ifeq ($(BR2_PACKAGE_GST1_PLUGINS_BAD_PLUGIN_LIVEADDER),y)
-GST1_PLUGINS_BAD_CONF_OPTS += --enable-liveadder
-else
-GST1_PLUGINS_BAD_CONF_OPTS += --disable-liveadder
 endif
 
 ifeq ($(BR2_PACKAGE_GST1_PLUGINS_BAD_PLUGIN_MIDI),y)
