07006d4408eb2ca3dc0fa32ad92987bdff86dc33
Akihiko Odaki <akihiko.odaki.4i@stu.hosei.ac.jp>
2016-06-26 14:54:46 +0900

    python-sip: bump to version 4.18
    Signed-off-by: Akihiko Odaki <akihiko.odaki.4i@stu.hosei.ac.jp>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-sip/python-sip.mk
+++ package/python-sip/python-sip.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_SIP_VERSION = 4.16.7
+PYTHON_SIP_VERSION = 4.18
 PYTHON_SIP_SOURCE = sip-$(PYTHON_SIP_VERSION).tar.gz
 PYTHON_SIP_SITE = http://downloads.sourceforge.net/project/pyqt/sip/sip-$(PYTHON_SIP_VERSION)
 PYTHON_SIP_LICENSE = SIP license or GPLv2 or GPLv3
