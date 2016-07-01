7c8c6e1b87fd127c72f79d001fe45fd43b1c9a4c
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2016-07-02 00:06:11 +0200

    babeld: New package
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    [Thomas: use $(INSTALL) instead of install.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/babeld/babeld.mk
@@ -0,0 +1,36 @@
+################################################################################
+#
+# babeld
+#
+################################################################################
+
+BABELD_VERSION = 1.7.1
+BABELD_SITE = http://www.pps.univ-paris-diderot.fr/~jch/software/files
+BALELD_LICENSE = MIT
+BALELD_LICENSE_FILES = LICENSE
+
+define BABELD_BUILD_CMDS
+	$(MAKE) -C $(@D) CC="$(TARGET_CC)" \
+		PLATFORM_DEFINES="$(TARGET_CFLAGS) $(TARGET_LDFLAGS)"
+endef
+
+define BABELD_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/babeld $(TARGET_DIR)/usr/sbin/babeld
+endef
+
+define BABELD_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 package/babeld/S50babeld \
+		$(TARGET_DIR)/etc/init.d/S50babeld
+endef
+
+define BABELD_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 package/babeld/babeld.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/babeld.service
+
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+
+	ln -fs ../../../../usr/lib/systemd/system/babeld.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/babeld.service
+endef
+
+$(eval $(generic-package))
