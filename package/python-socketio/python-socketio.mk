b0eb6bf447379ad14a826b0c7aee87c636367c6b
Yegor Yefremov <yegorslists@googlemail.com>
2016-07-01 12:06:19 +0200

    python-socketio: new package
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/python-socketio/python-socketio.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-socketio
+#
+################################################################################
+
+PYTHON_SOCKETIO_VERSION = 1.4.1
+PYTHON_SOCKETIO_SOURCE = python-socketio-$(PYTHON_SOCKETIO_VERSION).tar.gz
+PYTHON_SOCKETIO_SITE = https://pypi.python.org/packages/da/00/66103df6a116a2ce3ea662a9c8c80f337d43d7f138caf04e9af6dec24ba7
+PYTHON_SOCKETIO_SETUP_TYPE = setuptools
+PYTHON_SOCKETIO_LICENSE = MIT
+PYTHON_SOCKETIO_LICENSE_FILES = LICENSE
+
+$(eval $(python-package))
