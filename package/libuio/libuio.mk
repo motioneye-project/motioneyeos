9df195bec7ddd949f827d17e3b291e3ba19e7adf
Joris Lijssens <joris.lijssens@gmail.com>
2016-07-01 18:32:22 +0200

    package/libuio: new package
    Signed-off-by: Joris Lijssens <joris.lijssens@gmail.com>
    [Romain:
     - fix autoreconf issue by using GETTEXTIZE and AUTORECONF
     - fix issue with missing makeinfo
     - move version number comment in .mk]
    Signed-off-by: Romain Naour <romain.naour@smile.fr>
    [Thomas: add GPLv2 license, used for the programs.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/libuio/libuio.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# libuio
+#
+################################################################################
+
+# v0.2.7
+LIBUIO_VERSION = 940861de278cb794bf9d775b76a4d1d4f9108607
+LIBUIO_SITE = $(call github,Linutronix,libuio,$(LIBUIO_VERSION))
+LIBUIO_LICENSE = LGPLv2.1 (library), GPLv2 (programs)
+LIBUIO_LICENSE_FILES = COPYING
+LIBUIO_CONF_OPTS = --with-glib=no
+LIBUIO_INSTALL_STAGING = YES
+
+# Fetched from github, no pre-generated configure script provided
+LIBUIO_GETTEXTIZE = YES
+LIBUIO_AUTORECONF = YES
+
+# Avoid build issue when makeinfo is missing
+LIBUIO_CONF_ENV += MAKEINFO=true
+
+ifeq ($(BR2_PACKAGE_ARGP_STANDALONE),y)
+LIBUIO_DEPENDENCIES += argp-standalone
+LIBUIO_CONF_ENV += LIBS="-largp"
+endif
+
+$(eval $(autotools-package))
