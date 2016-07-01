722a24e175b754767d7547f7042ff06d603d3176
Gabe Evans <gabe@hashrabbit.co>
2016-07-01 23:51:05 +0200

    package/systemd: add optional support for backlight
    Signed-off-by: Gabe Evans <gabe@hashrabbit.co>
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/systemd/systemd.mk
+++ package/systemd/systemd.mk
@@ -183,6 +183,12 @@ else
 SYSTEMD_CONF_OPTS += --disable-randomseed
 endif
 
+ifeq ($(BR2_PACKAGE_SYSTEMD_BACKLIGHT),y)
+SYSTEMD_CONF_OPTS += --enable-backlight
+else
+SYSTEMD_CONF_OPTS += --disable-backlight
+endif
+
 ifeq ($(BR2_PACKAGE_SYSTEMD_NETWORKD),y)
 SYSTEMD_CONF_OPTS += --enable-networkd
 define SYSTEMD_INSTALL_RESOLVCONF_HOOK
