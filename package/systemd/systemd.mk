bab978a09bd48e587c110390618def91cf9507be
Gabe Evans <gabe@hashrabbit.co>
2016-07-01 23:51:04 +0200

    package/systemd: add optional support for random-seed
    Signed-off-by: Gabe Evans <gabe@hashrabbit.co>
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/systemd/systemd.mk
+++ package/systemd/systemd.mk
@@ -177,6 +177,12 @@ else
 SYSTEMD_CONF_OPTS += --disable-firstboot
 endif
 
+ifeq ($(BR2_PACKAGE_SYSTEMD_RANDOMSEED),y)
+SYSTEMD_CONF_OPTS += --enable-randomseed
+else
+SYSTEMD_CONF_OPTS += --disable-randomseed
+endif
+
 ifeq ($(BR2_PACKAGE_SYSTEMD_NETWORKD),y)
 SYSTEMD_CONF_OPTS += --enable-networkd
 define SYSTEMD_INSTALL_RESOLVCONF_HOOK
