5a136aba9ccf3b75d29fe2f7babdc46d8389906a
Julien Floret <julien.floret@6wind.com>
2016-07-03 00:20:41 +0200

    file: add explicit dependencies for host variant
    Signed-off-by: Julien Floret <julien.floret@6wind.com>
    [Thomas: remove host-file dependency.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/file/file.mk
+++ package/file/file.mk
@@ -7,6 +7,7 @@
 FILE_VERSION = 5.28
 FILE_SITE = ftp://ftp.astron.com/pub/file
 FILE_DEPENDENCIES = host-file zlib
+HOST_FILE_DEPENDENCIES = host-zlib
 FILE_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
 FILE_INSTALL_STAGING = YES
 FILE_LICENSE = BSD-2c, one file BSD-4c, one file BSD-3c
