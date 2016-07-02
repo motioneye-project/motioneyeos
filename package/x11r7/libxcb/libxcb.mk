8bfafa8cf34d1017ff92d18373f50293dd672b2b
Julien Floret <julien.floret@6wind.com>
2016-07-03 00:21:05 +0200

    libxcb: add explicit dependencies for host variant
    Signed-off-by: Julien Floret <julien.floret@6wind.com>
    [Thomas: add missing host-xcb-proto dependency.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/libxcb/libxcb.mk
+++ package/x11r7/libxcb/libxcb.mk
@@ -15,6 +15,9 @@ LIBXCB_INSTALL_STAGING = YES
 LIBXCB_DEPENDENCIES = \
 	host-libxslt libpthread-stubs xcb-proto xlib_libXdmcp xlib_libXau \
 	host-xcb-proto host-python host-pkgconf
+HOST_LIBXCB_DEPENDENCIES = \
+	host-libxslt host-libpthread-stubs host-xcb-proto host-xlib_libXdmcp \
+	host-xlib_libXau host-xcb-proto host-python host-pkgconf
 
 LIBXCB_CONF_OPTS = --with-doxygen=no
 HOST_LIBXCB_CONF_OPTS = --with-doxygen=no
