fb12ad357832c9aedc3f3aa41ee0ddebcd8e64fd
Julien Floret <julien.floret@6wind.com>
2016-07-03 00:20:57 +0200

    lttng-babeltrace: add explicit dependencies for host variant
    Signed-off-by: Julien Floret <julien.floret@6wind.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lttng-babeltrace/lttng-babeltrace.mk
+++ package/lttng-babeltrace/lttng-babeltrace.mk
@@ -11,6 +11,9 @@ LTTNG_BABELTRACE_LICENSE = MIT, LGPLv2.1 (include/babeltrace/list.h), GPLv2 (tes
 LTTNG_BABELTRACE_LICENSE_FILES = mit-license.txt gpl-2.0.txt LICENSE
 
 LTTNG_BABELTRACE_DEPENDENCIES = popt util-linux libglib2 host-pkgconf
+HOST_LTTNG_BABELTRACE_DEPENDENCIES = \
+	host-popt host-util-linux host-libglib2 host-pkgconf
+
 # for 0002-configure-fix-uuid-support-detection-on-static-build.patch
 LTTNG_BABELTRACE_AUTORECONF = YES
 
