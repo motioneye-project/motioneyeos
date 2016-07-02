bd849a10318b0379c59b052889c8cb6e1157c27f
Julien Floret <julien.floret@6wind.com>
2016-07-03 00:20:43 +0200

    fwup: add explicit dependencies for host variant
    Signed-off-by: Julien Floret <julien.floret@6wind.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/fwup/fwup.mk
+++ package/fwup/fwup.mk
@@ -9,6 +9,7 @@ FWUP_SITE = $(call github,fhunleth,fwup,$(FWUP_VERSION))
 FWUP_LICENSE = Apache-2.0
 FWUP_LICENSE_FILES = LICENSE
 FWUP_DEPENDENCIES = libconfuse libarchive libsodium
+HOST_FWUP_DEPENDENCIES = host-libconfuse host-libarchive host-libsodium
 FWUP_AUTORECONF = YES
 FWUP_CONF_ENV = ac_cv_path_HELP2MAN=""
 
