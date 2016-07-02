9e9dc4fc6884e352340b7229736652a29be1ee04
Julien Floret <julien.floret@6wind.com>
2016-07-03 00:20:44 +0200

    genimage: add explicit dependencies for host variant
    Signed-off-by: Julien Floret <julien.floret@6wind.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/genimage/genimage.mk
+++ package/genimage/genimage.mk
@@ -7,7 +7,7 @@
 GENIMAGE_VERSION = 8
 GENIMAGE_SOURCE = genimage-$(GENIMAGE_VERSION).tar.xz
 GENIMAGE_SITE = http://www.pengutronix.de/software/genimage/download
-GENIMAGE_DEPENDENCIES = host-pkgconf libconfuse
+HOST_GENIMAGE_DEPENDENCIES = host-pkgconf host-libconfuse
 GENIMAGE_LICENSE = GPLv2
 
 $(eval $(host-autotools-package))
