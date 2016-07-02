81cb325c9d60a985ff0f9f116bec8d40a2698e3f
Julien Floret <julien.floret@6wind.com>
2016-07-03 00:20:31 +0200

    axfsutils: add explicit dependencies for host variant
    Signed-off-by: Julien Floret <julien.floret@6wind.com>
    Reviewed-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/axfsutils/axfsutils.mk
+++ package/axfsutils/axfsutils.mk
@@ -8,7 +8,7 @@ AXFSUTILS_VERSION = 64886703fdff6cfc64eb20fbe7d7295bbeec867b
 AXFSUTILS_SITE = $(call github,jaredeh,axfs,$(AXFSUTILS_VERSION))
 AXFSUTILS_LICENSE = GPLv2
 AXFSUTILS_LICENSE_FILES = mkfs.axfs-legacy/COPYING
-AXFSUTILS_DEPENDENCIES = host-zlib
+HOST_AXFSUTILS_DEPENDENCIES = host-zlib
 
 # The 'new' mkfs.axfs version requires GNUstep which is not a buildroot
 # prerequisite. The 'legacy' one works just as well without that requirement.
