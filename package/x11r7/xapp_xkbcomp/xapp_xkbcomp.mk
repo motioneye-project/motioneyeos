f8f1ecc6259d31b7018204bc9d14656dc49c75bd
Julien Floret <julien.floret@6wind.com>
2016-07-03 00:21:10 +0200

    xapp_xkbcomp: add explicit dependencies for host variant
    Signed-off-by: Julien Floret <julien.floret@6wind.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xapp_xkbcomp/xapp_xkbcomp.mk
+++ package/x11r7/xapp_xkbcomp/xapp_xkbcomp.mk
@@ -10,6 +10,7 @@ XAPP_XKBCOMP_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XKBCOMP_LICENSE = MIT
 XAPP_XKBCOMP_LICENSE_FILES = COPYING
 XAPP_XKBCOMP_DEPENDENCIES = xlib_libX11 xlib_libxkbfile
+HOST_XAPP_XKBCOMP_DEPENDENCIES = host-xlib_libX11 host-xlib_libxkbfile
 XAPP_XKBCOMP_CONF_ENV = ac_cv_file___xkbparse_c=yes
 
 $(eval $(autotools-package))
