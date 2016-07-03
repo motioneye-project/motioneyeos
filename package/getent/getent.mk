5c772a3942f14d4b3e58d4f36bed0139b70804be
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2016-07-03 11:49:52 +0200

    getent: remove VERSION/SOURCE
    Now that the package infrastructure doesn't attempt to download a package
    that has an empty version string, there's no need to define the VERSION and
    SOURCE variables in the getent package.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/getent/getent.mk
+++ package/getent/getent.mk
@@ -4,10 +4,6 @@
 #
 ################################################################################
 
-# source included in Buildroot
-GETENT_SOURCE =
-
-GETENT_VERSION = buildroot-$(BR2_VERSION)
 GETENT_LICENSE = LGPLv2.1+
 
 # For glibc toolchains, we use the getent program built/installed by
