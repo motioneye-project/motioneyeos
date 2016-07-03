2f89476ad98b82ea9f914337b0050c4808082c82
Jörg Krause <joerg.krause@embedded.rocks>
2016-07-04 01:48:34 +0200

    package/libgpg-error: bump to version 1.23
    This patch is based on a patch sent by Vicente Olivert Riera and commented by
    Arnout Vandecappelle [1].
    
    - Bump version to 1.23
    - Add a hook to fix cross-compilation
    - Fix license and license files
    - Remove patch applied upstream
    - Add a BR2_PACKAGE_LIBGPG_ERROR_ARCH_SUPPORTS variable
    - Propagate the dependencies using that variable:
        * package/cppcms
        * package/crda
        * package/gnupg2
            - package/gcr
                - package/midori
        * package/kodi
        * package/libaacs
        * package/libassuan
        * package/libgcrypt
        * package/libgpgme
        * package/libksba
        * package/libmicrohttpd
            - package/janus-gateway
            - package/kodi
            - package/ola
            - package/systemd
        * package/libssh
        * package/libssh2
            - package/php-ssh2
        * package/netatalk
        * package/network-manager
        * package/ntfs-3g
        * package/opkg
        * package/php-gnupg
        * package/rng-tools
        * package/strongswan
        * package/vpnc
    
    [1] http://patchwork.ozlabs.org/patch/416427/
    
    Cc: Arnout Vandecappelle <arnout@mind.be>
    Cc: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    [Thomas:
     - rebase on master
     - changing systemd no longer needed, as it no longer selects
       libgcrypt.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    [Maxime:
     - rebase on master
     - bump to new version
     - propagate dependencies to missing packages]
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Reviewed-by: Romain Naour <romain.naour@gmail.com>
    [Thomas:
     - fix hash file.
     - change the way to handle the various arch so that it works properly
       for uClibc.
     - add nios2 arch support.
     - Maxime Hadjinlian learned some basic Emacs-fu to do the final fixups
       of this commit.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libgpg-error/libgpg-error.mk
+++ package/libgpg-error/libgpg-error.mk
@@ -4,14 +4,18 @@
 #
 ################################################################################
 
-LIBGPG_ERROR_VERSION = 1.12
+LIBGPG_ERROR_VERSION = 1.23
 LIBGPG_ERROR_SITE = ftp://ftp.gnupg.org/gcrypt/libgpg-error
-LIBGPG_ERROR_LICENSE = LGPLv2.1+
-LIBGPG_ERROR_LICENSE_FILES = COPYING.LIB
+LIBGPG_ERROR_LICENSE = GPLv2+, LGPLv2.1+
+LIBGPG_ERROR_LICENSE_FILES = COPYING COPYING.LIB
 LIBGPG_ERROR_INSTALL_STAGING = YES
 LIBGPG_ERROR_CONFIG_SCRIPTS = gpg-error-config
-# we patch src/Makefile.am
-LIBGPG_ERROR_AUTORECONF = YES
-LIBGPG_ERROR_GETTEXTIZE = YES
+
+define LIBGPG_ERROR_FIX_CROSS_COMPILATION
+	cd $(@D)/src/syscfg && \
+	ln -s lock-obj-pub.$(call qstrip, $(BR2_PACKAGE_LIBGPG_ERROR_SYSCFG)).h \
+		lock-obj-pub.$(GNU_TARGET_NAME).h
+endef
+LIBGPG_ERROR_PRE_CONFIGURE_HOOKS += LIBGPG_ERROR_FIX_CROSS_COMPILATION
 
 $(eval $(autotools-package))
