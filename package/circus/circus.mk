739e64e6ea98db2c6a05c04a631c75745f225895
Yegor Yefremov <yegorslists@googlemail.com>
2016-07-05 16:41:33 +0200

    circus: new package
    Reviewed-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    [Thomas: minor tweaks to Config.in.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/circus/circus.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# circus
+#
+################################################################################
+
+CIRCUS_VERSION = 0.13.0
+CIRCUS_SOURCE = circus-$(CIRCUS_VERSION).tar.gz
+CIRCUS_SITE = https://pypi.python.org/packages/source/c/circus
+CIRCUS_SETUP_TYPE = setuptools
+CIRCUS_LICENSE = Apache-2.0
+CIRCUS_LICENSE_FILES = LICENSE
+
+$(eval $(python-package))
