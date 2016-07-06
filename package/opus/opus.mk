d55c6b132e2f4835b6af6e5055c985ad5f715936
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2016-07-06 21:41:47 +0200

    opus: don't use assembly when on ARM, but without ARM instructions
    The ARM assembly code in opus uses full ARM instructions, which won't
    work on Thumb-2 only platforms such as ARMv7-M, so we disable the
    assembly code in such situations.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/ffa12ab7abadd76901228fc82da24e81bb6da625/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/opus/opus.mk
+++ package/opus/opus.mk
@@ -14,4 +14,10 @@ ifeq ($(BR2_PACKAGE_OPUS_FIXED_POINT),y)
 OPUS_CONF_OPTS += --enable-fixed-point
 endif
 
+# When we're on ARM, but we don't have ARM instructions (only
+# Thumb-2), disable the usage of assembly as it is not Thumb-ready.
+ifeq ($(BR2_arm)$(BR2_armeb):$(BR2_ARM_CPU_HAS_ARM),y:)
+OPUS_CONF_OPTS += --disable-asm
+endif
+
 $(eval $(autotools-package))
