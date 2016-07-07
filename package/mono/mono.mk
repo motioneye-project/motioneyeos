94db0e0874d6863e5dc0caec0b268b9156311895
Angelo Compagnucci <angelo.compagnucci@gmail.com>
2016-07-07 22:52:15 +0200

    package/mono: bump version to 4.4.1.0
    This patch bumps mono version to the latest released version 4.4.1.0.
    
    Signed-off-by: Angelo Compagnucci <angelo.compagnucci@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/mono/mono.mk
+++ package/mono/mono.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MONO_VERSION = 4.4.0.182
+MONO_VERSION = 4.4.1.0
 MONO_SITE = http://download.mono-project.com/sources/mono
 MONO_SOURCE = mono-$(MONO_VERSION).tar.bz2
 MONO_LICENSE = GPLv2 or MIT (compiler, tools), LGPLv2 (runtime libs), MIT (class libs) or commercial
