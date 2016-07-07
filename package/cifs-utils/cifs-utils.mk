7446bf29584c78f7b448b99ad90c760b8bad9da1
Waldemar Brodkorb <wbx@openadk.org>
2016-07-07 19:46:53 +0200

    cifs-utils: disable PIE for m68k
    PIE is not supported for uClibc-ng/m68k.
    
    Fixes following autobuild failure:
    http://autobuild.buildroot.net/results/4246de0280d4e7f8765adefd379c9dd414c316f1/
    
    Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/cifs-utils/cifs-utils.mk
+++ package/cifs-utils/cifs-utils.mk
@@ -10,7 +10,7 @@ CIFS_UTILS_SITE = http://ftp.samba.org/pub/linux-cifs/cifs-utils
 CIFS_UTILS_LICENSE = GPLv3+
 CIFS_UTILS_LICENSE_FILES = COPYING
 
-ifeq ($(BR2_STATIC_LIBS),y)
+ifneq ($(BR2_STATIC_LIBS)$(BR2_m68k),)
 CIFS_UTILS_CONF_OPTS += --disable-pie
 endif
 
