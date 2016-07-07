e7a82e9653c79dfe202881ec0304016e7582a6bf
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-07-07 07:10:09 +0200

    package/libsndfile: bump version to 1.0.27
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libsndfile/libsndfile.mk
+++ package/libsndfile/libsndfile.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSNDFILE_VERSION = 1.0.26
+LIBSNDFILE_VERSION = 1.0.27
 LIBSNDFILE_SITE = http://www.mega-nerd.com/libsndfile/files
 LIBSNDFILE_INSTALL_STAGING = YES
 LIBSNDFILE_LICENSE = LGPLv2.1+
