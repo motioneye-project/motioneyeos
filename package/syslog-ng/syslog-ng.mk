6868a342b4cf6a0b0184a0c3040cc300a68bb86b
Adam Duskett <Aduskett@gmail.com>
2016-07-12 13:09:20 -0400

    package/syslog-ng: fix localstatdir directory
    syslog-ng is built with the default buildroot flag localstatdir=/var
    
    This causes three issues:
    
    1) syslog-ng creates two files (syslog-ng.ctl and syslog-ng.persist in /var)
    which shouldn't have random files generated in it.
    
    2) SELinux expects it in it's default directory /var/run.
    
    3) This breakes read only file systems because /var/ usually isn't mounted as
    a seperate filing system.
    
    Setting the buildflag to the default /var/run
    fixes all three of thee of these issues.
    
    Signed-off-by: Adam Duskett <aduskett@codeblue.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/syslog-ng/syslog-ng.mk
+++ package/syslog-ng/syslog-ng.mk
@@ -13,7 +13,7 @@ SYSLOG_NG_DEPENDENCIES = host-bison host-flex host-pkgconf \
 	eventlog libglib2 openssl pcre
 # rabbit-mq needs -lrt
 SYSLOG_NG_CONF_ENV = LIBS=-lrt
-SYSLOG_NG_CONF_OPTS = --disable-manpages
+SYSLOG_NG_CONF_OPTS = --disable-manpages --localstatedir=/var/run
 
 # We override busybox's S01logging init script
 ifeq ($(BR2_PACKAGE_BUSYBOX),y)
