e42dccd7b5cd54f329ae561217f4d35386f288bb
Francois Perrad <fperrad@gmail.com>
2016-07-21 21:08:50 +0200

    perl-mojolicious: bump to version 7.0
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/perl-mojolicious/perl-mojolicious.mk
+++ package/perl-mojolicious/perl-mojolicious.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_MOJOLICIOUS_VERSION = 6.25
+PERL_MOJOLICIOUS_VERSION = 7.0
 PERL_MOJOLICIOUS_SOURCE = Mojolicious-$(PERL_MOJOLICIOUS_VERSION).tar.gz
 PERL_MOJOLICIOUS_SITE = $(BR2_CPAN_MIRROR)/authors/id/S/SR/SRI
 PERL_MOJOLICIOUS_LICENSE = Artistic-2.0
