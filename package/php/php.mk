ac43e455fe37a872398fc6552d39aed3cd84009e
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2016-07-21 12:23:07 +0100

    php: bump version to 7.0.9
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PHP_VERSION = 7.0.8
+PHP_VERSION = 7.0.9
 PHP_SITE = http://www.php.net/distributions
 PHP_SOURCE = php-$(PHP_VERSION).tar.xz
 PHP_INSTALL_STAGING = YES
