4cf4080087e7d7defa5fc50f42a3c6f94e3979be
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2016-07-22 16:08:58 +0100

    ser2net: add sysv init script
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    [Thomas:
     - remove check for the ser2net binary
     - add check for the configuration file
     - use 'printf' instead of 'echo -n'
     - fix indentation where needed]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ser2net/ser2net.mk
+++ package/ser2net/ser2net.mk
@@ -9,4 +9,9 @@ SER2NET_SITE = http://downloads.sourceforge.net/project/ser2net/ser2net
 SER2NET_LICENSE = GPLv2+
 SER2NET_LICENSE_FILES = COPYING
 
+define SER2NET_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 package/ser2net/S50ser2net \
+		$(TARGET_DIR)/etc/init.d/S50ser2net
+endef
+
 $(eval $(autotools-package))
