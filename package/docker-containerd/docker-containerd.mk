317e3b3469347071a1dcd5160b0c0e662894be61
Christian Stewart <christian@paral.in>
2016-07-22 13:52:58 -0700

    package/docker-containerd: bump to latest
    Bump the pinned docker-containerd version to the latest in advance
    of the introduction of docker-engine v1.12.0-rc4.
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/docker-containerd/docker-containerd.mk
+++ package/docker-containerd/docker-containerd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOCKER_CONTAINERD_VERSION = 1b3a81545ca79456086dc2aa424357be98b962ee
+DOCKER_CONTAINERD_VERSION = 0ac3cd1be170d180b2baed755e8f0da547ceb267
 DOCKER_CONTAINERD_SITE = $(call github,docker,containerd,$(DOCKER_CONTAINERD_VERSION))
 DOCKER_CONTAINERD_LICENSE = Apache-2.0
 DOCKER_CONTAINERD_LICENSE_FILES = LICENSE.code
