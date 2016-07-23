ad225d8c2a6cde309bd220e298770962f78618b8
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-07-23 17:17:17 +0200

    package/libdrm: bump version to 2.4.70
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libdrm/libdrm.mk
+++ package/libdrm/libdrm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBDRM_VERSION = 2.4.69
+LIBDRM_VERSION = 2.4.70
 LIBDRM_SOURCE = libdrm-$(LIBDRM_VERSION).tar.bz2
 LIBDRM_SITE = http://dri.freedesktop.org/libdrm
 LIBDRM_LICENSE = MIT
