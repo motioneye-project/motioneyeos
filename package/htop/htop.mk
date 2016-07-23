3a7da4c66ad2abd9f9659c3c3619fb3d5901f1e8
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-07-22 22:00:16 -0300

    htop: bump to version 2.0.2
    And drop upstream patch.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/htop/htop.mk
+++ package/htop/htop.mk
@@ -4,14 +4,12 @@
 #
 ################################################################################
 
-HTOP_VERSION = 2.0.1
+HTOP_VERSION = 2.0.2
 HTOP_SITE = http://hisham.hm/htop/releases/$(HTOP_VERSION)
 HTOP_DEPENDENCIES = ncurses
-# For 0001-Allow-to-override-ncurses-config-path.patch
-HTOP_AUTORECONF = YES
 HTOP_CONF_OPTS = --disable-unicode
 # Prevent htop build system from searching the host paths
-HTOP_CONF_ENV = htop_ncurses_config_script=$(STAGING_DIR)/usr/bin/ncurses5-config
+HTOP_CONF_ENV = HTOP_NCURSES_CONFIG_SCRIPT=$(STAGING_DIR)/usr/bin/ncurses5-config
 HTOP_LICENSE = GPLv2
 HTOP_LICENSE_FILES = COPYING
 
