6b372804be2d60f8f4cef17ae835de8a3aae00c5
Christian Stewart <christian@paral.in>
2016-07-24 14:10:27 -0700

    package/btrfs-progs: install to staging directory
    Other packages when building against btrfs-progs require headers in the
    staging directory under /usr/include/btrfs. This patch enables
    installing btrfs-progs to the staging directory to enable other packages
    to build against the btrfs-progs headers.
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    [Thomas: take into account the BR2_STATIC_LIBS case, by adding the
    relevant BTRFS_PROGS_INSTALL_STAGING_OPTS variable, as well as a patch
    to make sure "install-static" installs the static library and the
    headers.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/btrfs-progs/btrfs-progs.mk
+++ package/btrfs-progs/btrfs-progs.mk
@@ -11,11 +11,13 @@ BTRFS_PROGS_DEPENDENCIES = host-pkgconf e2fsprogs lzo util-linux zlib
 BTRFS_PROGS_CONF_OPTS = --disable-backtrace
 BTRFS_PROGS_LICENSE = GPLv2
 BTRFS_PROGS_LICENSE_FILES = COPYING
+BTRFS_PROGS_INSTALL_STAGING = YES
 
 # Doesn't autodetect static-only and tries to build both
 ifeq ($(BR2_STATIC_LIBS),y)
 BTRFS_PROGS_MAKE_OPTS = static
 BTRFS_PROGS_INSTALL_TARGET_OPTS = DESTDIR=$(TARGET_DIR) install-static
+BTRFS_PROGS_INSTALL_STAGING_OPTS = DESTDIR=$(STAGING_DIR) install-static
 endif
 
 $(eval $(autotools-package))
