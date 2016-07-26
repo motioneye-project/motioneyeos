0ee00d53d7b88a4ee2c68ebfb4d7449b89d62251
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2016-07-26 17:34:59 +0200

    gssdp: bump to version 0.14.16
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gssdp/gssdp.mk
+++ package/gssdp/gssdp.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 GSSDP_VERSION_MAJOR = 0.14
-GSSDP_VERSION = $(GSSDP_VERSION_MAJOR).14
+GSSDP_VERSION = $(GSSDP_VERSION_MAJOR).16
 GSSDP_SOURCE = gssdp-$(GSSDP_VERSION).tar.xz
 GSSDP_SITE = http://ftp.gnome.org/pub/gnome/sources/gssdp/$(GSSDP_VERSION_MAJOR)
 GSSDP_LICENSE = LGPLv2+
