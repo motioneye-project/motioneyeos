3a1f5d1ebaf39c757f64ec083034f50c16288901
Gary Bisson <gary.bisson@boundarydevices.com>
2016-07-27 15:53:30 +0200

    imx-codec: add install hooks to fix libraries path
    By default, all the libraries are installed under /usr/lib/imx-mm
    which causes problems at runtime.
    
    The hooks are inspired from the mechanism used in the Yocto recipe:
    https://github.com/Freescale/meta-fsl-arm/blob/krogoth/recipes-multimedia/imx-codec/imx-codec.inc
    
    Tested with the following commands:
     # gst-launch-0.10 audiotestsrc ! mfw_mp3encoder ! fakesink
     # gst-launch-1.0 audiotestsrc ! imxmp3audioenc ! fakesink
    
    [Peter: drop -mindepth/-maxdepth, add comment explaining why this is done]
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/freescale-imx/imx-codec/imx-codec.mk
+++ package/freescale-imx/imx-codec/imx-codec.mk
@@ -20,4 +20,13 @@ endef
 # FIXME The Makefile installs both the arm9 and arm11 versions of the
 # libraries, but we only need one of them.
 
+# Upstream installs libraries into usr/lib/imx-mm, but the dynamic
+# loader only looks in usr/lib, so move the libraries there
+define IMX_CODEC_FIXUP_TARGET_PATH
+	find $(TARGET_DIR)/usr/lib/imx-mm -not -type d \
+		-exec mv {} $(TARGET_DIR)/usr/lib \;
+	rm -rf $(TARGET_DIR)/usr/lib/imx-mm
+endef
+IMX_CODEC_POST_INSTALL_TARGET_HOOKS += IMX_CODEC_FIXUP_TARGET_PATH
+
 $(eval $(autotools-package))
