6008f02fc82c3cfcb4bb6e8e6e99e5dd7396b500
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2016-07-29 09:51:29 +0200

    gupnp-tools: new package
    GUPnP Tools are free replacements of Intel UPnP tools that use
    GUPnP. They provides client and server side tools which enable
    one to easily test and debug one's UPnP devices and control
    points.
    
    http://www.gupnp.org/
    
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>
    [Thomas:
     - use "depends on BR2_PACKAGE_LIBGTK3" instead of a select, and
       simplify the Config.in comments consequently.
     - move from "Development tools" to "Networking applications"
     - license is GPLv2+, not LGPLv2+.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/gupnp-tools/gupnp-tools.mk
@@ -0,0 +1,35 @@
+################################################################################
+#
+# gupnp-tools
+#
+################################################################################
+
+GUPNP_TOOLS_VERSION_MAJOR = 0.8
+GUPNP_TOOLS_VERSION = $(GUPNP_TOOLS_VERSION_MAJOR).12
+GUPNP_TOOLS_SOURCE = gupnp-tools-$(GUPNP_TOOLS_VERSION).tar.xz
+GUPNP_TOOLS_SITE = \
+	http://ftp.gnome.org/pub/gnome/sources/gupnp-tools/$(GUPNP_TOOLS_VERSION_MAJOR)
+GUPNP_TOOLS_LICENSE = GPLv2+
+GUPNP_TOOLS_LICENSE_FILES = COPYING
+GUPNP_TOOLS_INSTALL_STAGING = YES
+GUPNP_TOOLS_DEPENDENCIES = \
+	host-pkgconf \
+	libglib2 \
+	libxml2 \
+	gssdp \
+	gupnp \
+	libsoup \
+	libgtk3
+
+ifeq ($(BR2_PACKAGE_GUPNP_AV),y)
+GUPNP_TOOLS_CONF_OPTS += --with-av
+GUPNP_TOOLS_DEPENDENCIES += gupnp-av
+else
+GUPNP_TOOLS_CONF_OPTS += --without-av
+endif
+
+ifeq ($(BR2_PACKAGE_GTKSOURCEVIEW),y)
+GUPNP_TOOLS_DEPENDENCIES += gtksourceview
+endif
+
+$(eval $(autotools-package))
