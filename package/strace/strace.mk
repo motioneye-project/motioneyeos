59899f40f1777c3120881947cbd078b42efb05cb
Baruch Siach <baruch@tkos.co.il>
2016-07-31 21:44:31 +0300

    strace: bump to version 4.13
    Drop upstream patch.
    
    Remove preseed of netlink.h and if_packet.h tests since these tests were
    removed from configure.ac.
    
    Add stronger sha256 hash.
    
    Version 4.13 fixes a typo in configure.ac (commit 7c0e8875a432) that causes
    build failure with kernel v4.7 headers, thus fixing:
    http://autobuild.buildroot.net/results/aa6/aa6badae8b3d58d6d48161f261988103271bc2ef/
    http://autobuild.buildroot.net/results/4ad/4ad1b0c42206243bd76e94b34454d4108fb31070/
    http://autobuild.buildroot.net/results/1c6/1c6c88a7163529a666d1661969736f66e9aba7f7/
    
    and more.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/strace/strace.mk
+++ package/strace/strace.mk
@@ -4,15 +4,11 @@
 #
 ################################################################################
 
-STRACE_VERSION = 4.12
+STRACE_VERSION = 4.13
 STRACE_SOURCE = strace-$(STRACE_VERSION).tar.xz
 STRACE_SITE = http://downloads.sourceforge.net/project/strace/strace/$(STRACE_VERSION)
 STRACE_LICENSE = BSD-3c
 STRACE_LICENSE_FILES = COPYING
-
-STRACE_CONF_ENV = \
-	ac_cv_header_linux_if_packet_h=yes \
-	ac_cv_header_linux_netlink_h=yes
 
 define STRACE_REMOVE_STRACE_GRAPH
 	rm -f $(TARGET_DIR)/usr/bin/strace-graph
