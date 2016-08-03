ba865a4c92454e75340908e2e0dec38180fba130
Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
2016-08-03 23:23:27 +0200

    package/imagemagick: bump to version 7.0.2-6
    Signed-off-by: Jerzy Grzegorek <jerzy.grzegorek@trzebnica.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/imagemagick/imagemagick.mk
+++ package/imagemagick/imagemagick.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IMAGEMAGICK_VERSION = 7.0.2-5
+IMAGEMAGICK_VERSION = 7.0.2-6
 IMAGEMAGICK_SOURCE = ImageMagick-$(IMAGEMAGICK_VERSION).tar.xz
 IMAGEMAGICK_SITE = http://www.imagemagick.org/download/releases
 IMAGEMAGICK_LICENSE = Apache-2.0
