d1dbd48ddc6e01e3a38e97d95af8bd96b5528fb1
Yann E. MORIN <yann.morin.1998@free.fr>
2016-08-06 14:44:18 +0200

    package/lttng-libust: fix build with musl
    Backport two patches from upstream to fix build with musl.
    
    Fixes:
        http://autobuild.buildroot.org/results/dcb/dcb6c3aadf6f955016cf3c3f9b917cbec6d29342/
        http://autobuild.buildroot.org/results/deb/deb80eea5ceae3e7f8e0549596dce2fb6a1e7596/
        http://autobuild.buildroot.org/results/91e/91e64477864825c39911e3f3d0a2459f2987c209/
        [...and a lot more...]
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Philippe Proulx <eeppeliteloop@gmail.com>
    Cc: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lttng-libust/lttng-libust.mk
+++ package/lttng-libust/lttng-libust.mk
@@ -10,6 +10,10 @@ LTTNG_LIBUST_SOURCE = lttng-ust-$(LTTNG_LIBUST_VERSION).tar.bz2
 LTTNG_LIBUST_LICENSE = LGPLv2.1, MIT (system headers), GPLv2 (liblttng-ust-ctl/ustctl.c used by lttng-sessiond)
 LTTNG_LIBUST_LICENSE_FILES = COPYING
 
+LTTNG_LIBUST_PATCH = \
+	https://github.com/lttng/lttng-ust/commit/787364e8bac9a146f9d65c763633c2b75eb76463.patch \
+	https://github.com/lttng/lttng-ust/commit/30307a67b48641875b122d8f832bbfb454e95d92.patch
+
 LTTNG_LIBUST_INSTALL_STAGING = YES
 LTTNG_LIBUST_DEPENDENCIES = liburcu util-linux
 
