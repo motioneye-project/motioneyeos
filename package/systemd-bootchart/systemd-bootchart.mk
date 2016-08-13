a2e56724a3348ecf1dc57fb683c1f22ac5334a8d
Yann E. MORIN <yann.morin.1998@free.fr>
2016-08-13 19:06:59 +0200

    package/systemd-bootchart: don't build manpages
    Building the man pages requires xsltproc, which is not always available
    on the build machine, so the build fails at configure:
        configure: error: *** xsltproc is required for man pages
    
    But then, even when xsltproc is present, it can't actually build the man
    pages, because it can't "download external entities":
        I/O error : Attempt to load network entity http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl
        warning: failed to load external entity "http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl"
        compilation error: file ./man/custom-man.xsl line 27 element import
    
    Add a patch (sent upstream) to allow not building the man pages.
    
    Fixes:
        http://autobuild.buildroot.org/results/e2a/e2ae38775b6acff3c3a8ea233ec70dd68b017319/
        http://autobuild.buildroot.org/results/66d/66d85622db9f7ff5a4e8d6eed3fd3b32959685cf/
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/systemd-bootchart/systemd-bootchart.mk
+++ package/systemd-bootchart/systemd-bootchart.mk
@@ -11,6 +11,15 @@ SYSTEMD_BOOTCHART_SOURCE = systemd-bootchart-$(SYSTEMD_BOOTCHART_VERSION).tar.xz
 SYSTEMD_BOOTCHART_SITE = https://github.com/systemd/systemd-bootchart/releases/download/v$(SYSTEMD_BOOTCHART_VERSION)
 SYSTEMD_BOOTCHART_LICENSE = LGPLv2.1+
 SYSTEMD_BOOTCHART_LICENSE_FILES = LICENSE.LGPL2.1
-SYSTEMD_BOOTCHART_DEPENDENCIES = systemd
+SYSTEMD_BOOTCHART_DEPENDENCIES = host-intltool systemd
+
+# 0001-configure-add-option-to-not-build-manpages.patch
+SYSTEMD_BOOTCHART_AUTORECONF = YES
+SYSTEMD_BOOTCHART_CONF_OPTS = --disable-man
+
+define SYSTEMD_BOOTCHART_INTLTOOLIZE
+        cd $(@D) && $(HOST_DIR)/usr/bin/intltoolize --force --automake
+endef
+SYSTEMD_BOOTCHART_PRE_CONFIGURE_HOOKS = SYSTEMD_BOOTCHART_INTLTOOLIZE
 
 $(eval $(autotools-package))
