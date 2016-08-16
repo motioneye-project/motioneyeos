d3a1d96f8903b3d8ba288a932f3427109854c674
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-08-16 09:37:45 -0300

    dbus: bump to version 1.10.10
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/dbus/dbus.mk
+++ package/dbus/dbus.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DBUS_VERSION = 1.10.8
+DBUS_VERSION = 1.10.10
 DBUS_SITE = http://dbus.freedesktop.org/releases/dbus
 DBUS_LICENSE = AFLv2.1 or GPLv2+ (library, tools), GPLv2+ (tools)
 DBUS_LICENSE_FILES = COPYING
