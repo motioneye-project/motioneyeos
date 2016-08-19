112ec4a8f78b558513b292ab5a2c48814e47510f
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2016-08-19 15:50:29 +0200

    lirc-tools: fix build issue related to clock_gettime()
    In some older versions of glibc, clock_gettime() is in librt, so we need
    to link with librt when clock_gettime() is used. This commit adds a
    lirc-tools patch to make sure we link with librt when needed.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/2770360880c9b265c8e019141925e56c35ba22dc/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lirc-tools/lirc-tools.mk
+++ package/lirc-tools/lirc-tools.mk
@@ -13,6 +13,7 @@ LIRC_TOOLS_DEPENDENCIES = host-libxslt host-pkgconf host-python3
 LIRC_TOOLS_INSTALL_STAGING = YES
 # 0001-lib-use-proper-linking-method-to-avoid-parallel-buil.patch
 # 0003-Don-t-build-commandir-unless-we-have-usb.h-191.patch
+# 0004-configure-check-for-clock_gettime-in-librt.patch
 LIRC_TOOLS_AUTORECONF = YES
 
 LIRC_TOOLS_CONF_ENV = XSLTPROC=yes
