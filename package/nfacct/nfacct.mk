7f02e6b1e81e826a88968780243925ec895926ca
Baruch Siach <baruch@tkos.co.il>
2016-08-24 16:59:00 +0300

    nfacct: remove autoreconf
    Since commit ae0e8950540f (nfacct: bump to version 1.0.2, 2016-08-23) we no
    longer patch configure.ac.
    
    Cc: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/nfacct/nfacct.mk
+++ package/nfacct/nfacct.mk
@@ -9,7 +9,6 @@ NFACCT_SOURCE = nfacct-$(NFACCT_VERSION).tar.bz2
 NFACCT_SITE = http://www.netfilter.org/projects/nfacct/files
 NFACCT_DEPENDENCIES = host-pkgconf \
 	libnetfilter_acct
-NFACCT_AUTORECONF = YES
 NFACCT_LICENSE = GPLv2
 NFACCT_LICENSE_FILES = COPYING
 
