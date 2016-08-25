70730ff49805e4d5eb99d1cc117c607b7efbafbc
Calin Crisan <ccrisan@gmail.com>
2016-08-25 23:08:38 +0300

    updated motioneye

--- package/motioneye/motioneye.mk
+++ package/motioneye/motioneye.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-MOTIONEYE_VERSION = c09764c55e20519f38c64332703ddaf55e3e10f2
+MOTIONEYE_VERSION = 093c61ea811c92b5168797e4f4fd7398eb31bb5d
 MOTIONEYE_SITE = $(call github,ccrisan,motioneye,$(MOTIONEYE_VERSION))
 MOTIONEYE_SOURCE = $(MOTIONEYE_VERSION).tar.gz
 MOTIONEYE_LICENSE = GPLv3
