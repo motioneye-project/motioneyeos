707465da72c617982eb5cf2b8c04cbde4bd3768a
Peter Korsgaard <peter@korsgaard.com>
2016-08-26 11:52:19 +0200

    board/minnowboard/post-build.sh: only tweak /etc/inittab if available
    As pointed out in bug #9161, we don't always have an inittab file (if
    systemd or no init is used), so the post build script should only try to
    tweak it if present.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- board/minnowboard/post-build.sh
+++ board/minnowboard/post-build.sh
@@ -5,6 +5,8 @@
 cp -v board/minnowboard/grub-${2}.cfg ${BINARIES_DIR}/efi-part/EFI/BOOT/grub.cfg
 
 # Add a console on tty1
-grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
-sed -i '/GENERIC_SERIAL/a\
+if [ -e ${TARGET_DIR}/etc/inittab ]; then
+    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
+	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
+fi
