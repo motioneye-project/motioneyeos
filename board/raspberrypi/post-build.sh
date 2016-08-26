720b9095a6a9fb23c7a2d7d3e4dabbe6b5299b12
Peter Korsgaard <peter@korsgaard.com>
2016-08-26 11:52:20 +0200

    board/raspberrypi/post-build.sh: only tweak /etc/inittab if available
    As pointed out in bug #9161, we don't always have an inittab file (if
    systemd or no init is used), so the post build script should only try to
    tweak it if present.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -4,6 +4,8 @@ set -u
 set -e
 
 # Add a console on tty1
-grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
-sed -i '/GENERIC_SERIAL/a\
+if [ -e ${TARGET_DIR}/etc/inittab ]; then
+    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
+	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
+fi
