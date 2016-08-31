0cd962b3bcb842985369cb276e6cba0f8b15289c
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2016-08-31 18:29:54 +0200

    gtest: retrieve package from github
    https://googletest.googlecode.com/files is no more available, so update
    gtest.mk and gtest.hash to retrieve googletest source code as a tar.gz
    (and not a zip) from github as https://googletest.googlecode.com is now
    a redirection to https://github.com/google/googletest.
    
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gtest/gtest.mk
+++ package/gtest/gtest.mk
@@ -5,9 +5,8 @@
 ################################################################################
 
 # Make sure this remains the same version as the gmock one
-GTEST_VERSION = 1.7.0
-GTEST_SOURCE = gtest-$(GTEST_VERSION).zip
-GTEST_SITE = http://googletest.googlecode.com/files
+GTEST_VERSION = release-1.7.0
+GTEST_SITE = $(call github,google,googletest,$(GTEST_VERSION))
 GTEST_INSTALL_STAGING = YES
 GTEST_INSTALL_TARGET = NO
 GTEST_LICENSE = BSD-3c
@@ -21,10 +20,6 @@ GTEST_LICENSE_FILES = LICENSE
 # For further details, refer to the explaination given in the README file from
 # the gtest sources.
 GTEST_CONF_OPTS = -DBUILD_SHARED_LIBS=OFF
-
-define GTEST_EXTRACT_CMDS
-	$(UNZIP) $(DL_DIR)/$(GTEST_SOURCE) -d $(BUILD_DIR)
-endef
 
 define GTEST_INSTALL_STAGING_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/libgtest.a $(STAGING_DIR)/usr/lib/libgtest.a
