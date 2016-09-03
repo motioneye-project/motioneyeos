9b18f4fbe8c810a62ed991a87309c28dfda4bac2
Jérôme Pouiller <jezz@sysmic.org>
2016-09-03 17:24:49 +0200

    lxc: revive "drop-werror" patch
    commit 5050a94ae9f448641e05ce29c931671ffdc62784 dropped patch that
    remove '-Werror' from CFLAGS. However, it seems this patch is still
    necessary:
    
      http://autobuild.buildroot.net/results/b1cbca6d0396863654b1d09ccc3163c5f6124ab8
      http://autobuild.buildroot.net/results/fcf2834ad74b95548d25dad2274704ea401f9665
      http://autobuild.buildroot.net/results/048be2fc702c9dba9ca4439ff687d71b30c10551
      http://autobuild.buildroot.net/results/939246c9a4f433dfd0dc414988f5957441b8e9b4
    
    Signed-off-by: Jérôme Pouiller <jezz@sysmic.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lxc/lxc.mk
+++ package/lxc/lxc.mk
@@ -9,6 +9,8 @@ LXC_SITE = https://linuxcontainers.org/downloads/lxc
 LXC_LICENSE = LGPLv2.1+
 LXC_LICENSE_FILES = COPYING
 LXC_DEPENDENCIES = libcap host-pkgconf
+# we're patching configure.ac
+LXC_AUTORECONF = YES
 LXC_CONF_OPTS = --disable-apparmor --with-distro=buildroot \
 	--disable-lua --disable-python \
 	$(if $(BR2_PACKAGE_BASH),,--disable-bash)
