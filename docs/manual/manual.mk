d4db4277ffbd1fae478ba748c626eddc7c220e67
Yann E. MORIN <yann.morin.1998@free.fr>
2016-09-04 16:17:46 +0200

    manual: get rid of the lists of packages
    We currently have four lists of packages in the manual:
      - the non-virtual target packages,
      - the virtual target packages,
      - the host packages,
      - the deprecated features.
    
    Those list take more than half of the manual. They do not serve much
    purpose except to show off.
    
    After the recent discussion on the list [0], remove them all.
    
    We can now get rid of our biggish and complex generating script (and its
    companion library kconfiglib).
    
    [0] http://lists.busybox.net/pipermail/buildroot/2016-September/171199.html
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Peter Korsgaard <peter@korsgaard.com>
    Cc: Arnout Vandecappelle <arnout@mind.be>
    Cc: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- docs/manual/manual.mk
+++ docs/manual/manual.mk
@@ -7,28 +7,4 @@
 MANUAL_SOURCES = $(sort $(wildcard docs/manual/*.txt) $(wildcard docs/images/*))
 MANUAL_RESOURCES = $(TOPDIR)/docs/images
 
-# Ensure the kconfig snippet is generated (for MANUAL_GEN_LISTS, below):
-MANUAL_DEPENDENCIES += prepare-kconfig
-
-# Our manual needs to generate lists
-define MANUAL_GEN_LISTS
-	$(Q)$(call MESSAGE,"Updating the manual lists...")
-	$(Q)$(COMMON_CONFIG_ENV) \
-		BR2_DEFCONFIG="" \
-		TOPDIR=$(TOPDIR) \
-		O=$(@D) \
-		python -B $(TOPDIR)/support/scripts/gen-manual-lists.py
-endef
-MANUAL_POST_RSYNC_HOOKS += MANUAL_GEN_LISTS
-
-# Our list-generating script requires argparse
-define MANUAL_CHECK_LISTS_DEPS
-	$(Q)if ! python -c "import argparse" >/dev/null 2>&1 ; then \
-		echo "You need python with argparse on your host to generate" \
-			"the list of packages in the manual"; \
-		exit 1; \
-	fi
-endef
-MANUAL_CHECK_DEPENDENCIES_HOOKS += MANUAL_CHECK_LISTS_DEPS
-
 $(eval $(call asciidoc-document))
