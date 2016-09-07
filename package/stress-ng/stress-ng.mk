5609fe65aa7a3b85f1226f57439abd07a2869fae
Romain Naour <romain.naour@gmail.com>
2016-09-07 22:21:23 +0200

    package/stress-ng: fix install commande
    Don't use make install otherwise stress-ng will be rebuild without
    required link libraries if any. Furthermore, using INSTALL allow to
    set the file permission correcly on the target.
    
    Fixes:
    http://autobuild.buildroot.net/results/bba/bba4d0b635ab6849f969b8a0363ebe9f5fafd82e
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/stress-ng/stress-ng.mk
+++ package/stress-ng/stress-ng.mk
@@ -15,8 +15,11 @@ define STRESS_NG_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
 endef
 
+# Don't use make install otherwise stress-ng will be rebuild without
+# required link libraries if any. Furthermore, using INSTALL allow to
+# set the file permission correcly on the target.
 define STRESS_NG_INSTALL_TARGET_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) DESTDIR=$(TARGET_DIR) -C $(@D) install
+	$(INSTALL) -m 0755 -D $(@D)/stress-ng $(TARGET_DIR)/usr/bin/stress-ng
 endef
 
 $(eval $(generic-package))
