aeaf66fc7a2be26bbc8bcb8a0c0243677bbcd3fb
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2016-09-08 11:22:34 +0100

    wireshark: bump version to 2.2.0
    - Adapt current patch for 2.2.0.
    
    - Adapt gtk configure options for 2.2.0:
      - Instead of --enable-gtk3 or --enable-gtk2, now it uses
        --enable-gtk=[no/2/3/yes].
    
    - Remove non-existing configure options:
      - --enable-usr-local: removed by
        https://code.wireshark.org/review/gitweb?p=wireshark.git;a=commit;h=125989780ba8e658ad0d1f1c71ec01e6d36a2266
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/wireshark/wireshark.mk
+++ package/wireshark/wireshark.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIRESHARK_VERSION = 2.0.5
+WIRESHARK_VERSION = 2.2.0
 WIRESHARK_SOURCE = wireshark-$(WIRESHARK_VERSION).tar.bz2
 WIRESHARK_SITE = http://www.wireshark.org/download/src/all-versions
 WIRESHARK_LICENSE = wireshark license
@@ -20,7 +20,6 @@ WIRESHARK_AUTORECONF = YES
 # Work around it by pointing includedir at staging
 WIRESHARK_CONF_OPTS = \
 	--without-krb5 \
-	--disable-usr-local \
 	--enable-static=no \
 	--with-libsmi=no \
 	--with-lua=no \
@@ -29,13 +28,13 @@ WIRESHARK_CONF_OPTS = \
 
 # wireshark GUI options
 ifeq ($(BR2_PACKAGE_LIBGTK3),y)
-WIRESHARK_CONF_OPTS += --with-gtk3=yes
+WIRESHARK_CONF_OPTS += --with-gtk=3
 WIRESHARK_DEPENDENCIES += libgtk3
 else ifeq ($(BR2_PACKAGE_LIBGTK2),y)
-WIRESHARK_CONF_OPTS += --with-gtk2=yes
+WIRESHARK_CONF_OPTS += --with-gtk=2
 WIRESHARK_DEPENDECIES += libgtk2
 else
-WIRESHARK_CONF_OPTS += --with-gtk3=no --with-gtk2=no
+WIRESHARK_CONF_OPTS += --with-gtk=no
 endif
 
 # Qt4 needs accessibility, we don't support it
