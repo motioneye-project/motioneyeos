327c985ce1b0e2809751abe612a03a1760b249aa
Peter Korsgaard <peter@korsgaard.com>
2016-09-13 09:20:38 +0200

    configs/orangepipc: Use Linux 4.7.3
    There's now an sun8i-h3-orangepi-pc dts, so we can use that and drop the
    hack in the post-image script.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- board/orangepi/orangepipc/post-image.sh
+++ board/orangepi/orangepipc/post-image.sh
@@ -6,17 +6,9 @@ GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
 rm -rf "${GENIMAGE_TMP}"
 
-# As we now use the Linux v4.5-RC1 kernel no orangepi-pc dtb exists yet.
-# However the orangepi-plus dtb has not much content, only mmc0 and uart
-# which are equal to the pc version of the board, so we use it here.
-mv ${BINARIES_DIR}/sun8i-h3-orangepi-plus.dtb ${BINARIES_DIR}/sun8i-h3-orangepi-pc.dtb
-
-
 genimage                               \
 	--rootpath "${TARGET_DIR}"     \
 	--tmppath "${GENIMAGE_TMP}"    \
 	--inputpath "${BINARIES_DIR}"  \
 	--outputpath "${BINARIES_DIR}" \
 	--config "${GENIMAGE_CFG}"
-
-exit $?
