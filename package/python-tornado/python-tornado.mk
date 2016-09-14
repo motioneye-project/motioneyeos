5757aaf36da8a34d7ff2e31a8738a45ec72734af
Yegor Yefremov <yegorslists@googlemail.com>
2016-09-14 12:36:28 +0200

    python-tornado: bump to version 4.4.1
    Remove upstream patch.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-tornado/python-tornado.mk
+++ package/python-tornado/python-tornado.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TORNADO_VERSION = 4.3
+PYTHON_TORNADO_VERSION = 4.4.1
 PYTHON_TORNADO_SOURCE = tornado-$(PYTHON_TORNADO_VERSION).tar.gz
-PYTHON_TORNADO_SITE = https://pypi.python.org/packages/source/t/tornado
+PYTHON_TORNADO_SITE = https://pypi.python.org/packages/96/5d/ff472313e8f337d5acda5d56e6ea79a43583cc8771b34c85a1f458e197c3
 PYTHON_TORNADO_LICENSE = Apache-2.0
 PYTHON_TORNADO_SETUP_TYPE = setuptools
 
