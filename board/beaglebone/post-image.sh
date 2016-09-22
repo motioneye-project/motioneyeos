87d265f13fd0dbab82055c22fb54aa7c7051d9da
Lothar Felten <lothar.felten@gmail.com>
2016-09-23 00:20:48 +0200

    configs/beaglebone: update kernel/uboot, add AM335x EVM support
    This patch updates the target config for the beaglebone based on the
    AM335x CPU. It also supports the beagleboneblack, the TI evm and evmsk.
    Device tree blobs for am335x-evm, am335x-evmsk, am335x-bone,
    am335x-boneblack will be built and a sd card image is created with a
    post-image script.  The kernel and driver versions match the
    ti-processor-sdk 02.00.00.00.  Patches allow the use of old AM335x evm
    revisions (1 bit sd card) and JTAG debugging.
    
    Kernel JTAG patch: By default the kernel will disable the JTAG clock,
    access via the JTAG port will not work after the kernel booted. The
    AM335x-evm has a JTAG header fitted, this patch keeps the JTAG clock
    alive.
    
    Signed-off-by: Lothar Felten <lothar.felten@gmail.com>
    [Thomas: remove 1-bit MMC patch, it's causing a significant performance
    regression for users of BeagleBone, which are 99.99% of the users of
    this defconfig.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- board/beaglebone/post-image.sh
+++ board/beaglebone/post-image.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# post-image.sh for BeagleBone
+# post-image.sh for CircuitCo BeagleBone and TI am335x-evm
 # 2014, Marcin Jabrzyk <marcin.jabrzyk@gmail.com>
+# 2016, Lothar Felten <lothar.felten@gmail.com>
 
 BOARD_DIR="$(dirname $0)"
 
