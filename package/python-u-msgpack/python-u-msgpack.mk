dd9c4871d5506641a08d0c8690ea6175015a4a2c
Yegor Yefremov <yegorslists@googlemail.com>
2016-09-26 12:07:26 +0200

    python-u-msgpack: bump to version 2.2
    Package archive includes a proper licence file now, so use it instead of
    a source file.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-u-msgpack/python-u-msgpack.mk
+++ package/python-u-msgpack/python-u-msgpack.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_U_MSGPACK_VERSION = 2.1
+PYTHON_U_MSGPACK_VERSION = 2.2
 PYTHON_U_MSGPACK_SOURCE = u-msgpack-python-$(PYTHON_U_MSGPACK_VERSION).tar.gz
-PYTHON_U_MSGPACK_SITE = https://pypi.python.org/packages/b7/8d/791f037a0954f98a8eceb09c7c14babb97db0a81c985108bb21939b801eb
+PYTHON_U_MSGPACK_SITE = https://pypi.python.org/packages/66/5b/36fe0fcf290bd39f6ef6c1f5924cf0a9a76b0dc94575975ad7d318619cf9
 PYTHON_U_MSGPACK_SETUP_TYPE = distutils
 PYTHON_U_MSGPACK_LICENSE = MIT
-PYTHON_U_MSGPACK_LICENSE_FILES = umsgpack.py
+PYTHON_U_MSGPACK_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
