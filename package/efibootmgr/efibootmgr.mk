3413c4a0a8cd10e84e797d2ed00f022fab986117
Erico Nunes <nunes.erico@gmail.com>
2016-09-30 16:46:44 +0200

    efibootmgr: bump to version 14
    Upstream has changed version numbering scheme since version 13, which
    came after version 0.12.
    
    There is now a make install target, so we can drop the manual file
    installing. The make install target also installs the new efibootdump
    tool.
    
    Signed-off-by: Erico Nunes <nunes.erico@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/efibootmgr/efibootmgr.mk
+++ package/efibootmgr/efibootmgr.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EFIBOOTMGR_VERSION = efibootmgr-0.12
+EFIBOOTMGR_VERSION = 14
 EFIBOOTMGR_SITE = $(call github,rhinstaller,efibootmgr,$(EFIBOOTMGR_VERSION))
 EFIBOOTMGR_LICENSE = GPLv2+
 EFIBOOTMGR_LICENSE_FILES = COPYING
@@ -21,8 +21,8 @@ define EFIBOOTMGR_BUILD_CMDS
 endef
 
 define EFIBOOTMGR_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/src/efibootmgr/efibootmgr \
-		$(TARGET_DIR)/usr/bin/efibootmgr
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
+		DESTDIR=$(TARGET_DIR) install
 endef
 
 $(eval $(generic-package))
