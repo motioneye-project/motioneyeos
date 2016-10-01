bb3013204337f3cb192c507e051d8878d6809512
Calin Crisan <ccrisan@gmail.com>
2016-10-01 16:23:11 +0300

    raspberrypi(1,2,3): updated picamera to 1.12; added more resolutions to fast network camera

--- package/python-picamera/python-picamera.mk
+++ package/python-picamera/python-picamera.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PICAMERA_VERSION = 1.10
+PYTHON_PICAMERA_VERSION = 1.12
 PYTHON_PICAMERA_SOURCE = picamera-$(PYTHON_PICAMERA_VERSION).tar.gz
-PYTHON_PICAMERA_SITE = http://pypi.python.org/packages/source/p/picamera
+PYTHON_PICAMERA_SITE = https://pypi.python.org/packages/ab/53/54a20f53e61df5c329480207fb0b6e6e25c64d16e82e899de335e08df7d9
 PYTHON_PICAMERA_SETUP_TYPE = distutils
 
 $(eval $(python-package))
