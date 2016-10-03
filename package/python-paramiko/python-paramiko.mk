8816ba2e6c064ff673e082ef5c2289552e77b372
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-10-03 23:11:39 +0200

    package/python-paramiko: bump version to 2.0.2
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-paramiko/python-paramiko.mk
+++ package/python-paramiko/python-paramiko.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PARAMIKO_VERSION = 1.16.0
+PYTHON_PARAMIKO_VERSION = 2.0.2
 PYTHON_PARAMIKO_SOURCE = paramiko-$(PYTHON_PARAMIKO_VERSION).tar.gz
-PYTHON_PARAMIKO_SITE = https://pypi.python.org/packages/source/p/paramiko
+PYTHON_PARAMIKO_SITE = https://pypi.python.org/packages/6b/4e/971b8c917456a2ec9666311f7e695493002a64022397cd668150b5efd2a8
 PYTHON_PARAMIKO_SETUP_TYPE = setuptools
 PYTHON_PARAMIKO_LICENSE = LGPLv2.1+
 PYTHON_PARAMIKO_LICENSE_FILES = LICENSE
