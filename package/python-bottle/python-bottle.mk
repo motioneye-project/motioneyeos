5f48be594e962cd7a07e36607c29162605e6a1a6
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-10-03 23:11:24 +0200

    package/python-bottle: bump version to 0.12.9
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-bottle/python-bottle.mk
+++ package/python-bottle/python-bottle.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_BOTTLE_VERSION = 0.12.7
+PYTHON_BOTTLE_VERSION = 0.12.9
 PYTHON_BOTTLE_SOURCE = bottle-$(PYTHON_BOTTLE_VERSION).tar.gz
 PYTHON_BOTTLE_SITE = http://pypi.python.org/packages/source/b/bottle
 PYTHON_BOTTLE_LICENSE = MIT
