76cd20c98069704ef1bb7c3493ec6fdbf97d9994
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-10-03 23:11:21 +0200

    package/python-argh: bump version to 0.26.2
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-argh/python-argh.mk
+++ package/python-argh/python-argh.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ARGH_VERSION = 0.26.1
+PYTHON_ARGH_VERSION = 0.26.2
 PYTHON_ARGH_SOURCE = argh-$(PYTHON_ARGH_VERSION).tar.gz
-PYTHON_ARGH_SITE = https://pypi.python.org/packages/14/7f/794a7f4a48cba505a4b4c714f81fed844a3a5f7340b171f448711439b09e
+PYTHON_ARGH_SITE = https://pypi.python.org/packages/e3/75/1183b5d1663a66aebb2c184e0398724b624cecd4f4b679cb6e25de97ed15
 PYTHON_ARGH_SETUP_TYPE = setuptools
 PYTHON_ARGH_LICENSE = LGPLv3+
 PYTHON_ARGH_LICENSE_FILES = README.rst
