a0f399399c8af6ac931a4246d6e91caf8f646209
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2016-10-07 23:30:26 +0200

    Revert "package/python-rtslib-fb: bump version to 2.1.58"
    This reverts commit e57ea511ee8f4e944bc6343b467386fff20acd37. As noted
    by the original author of the package, Christophe Vu-Brugier, this
    package should be updated together with python-configshell-fb and
    python-targetcli-fb.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-rtslib-fb/python-rtslib-fb.mk
+++ package/python-rtslib-fb/python-rtslib-fb.mk
@@ -4,11 +4,10 @@
 #
 ################################################################################
 
-PYTHON_RTSLIB_FB_VERSION = 2.1.58
-PYTHON_RTSLIB_FB_SOURCE = rtslib-fb-$(PYTHON_RTSLIB_FB_VERSION).tar.gz
-PYTHON_RTSLIB_FB_SITE = https://pypi.python.org/packages/c8/26/ae0ff9a721d046bef78c457d8fc19287f7dbe36f98f5e190b017f0d9e9b7
+PYTHON_RTSLIB_FB_VERSION = v2.1.fb57
+PYTHON_RTSLIB_FB_SITE = $(call github,agrover,rtslib-fb,$(PYTHON_RTSLIB_FB_VERSION))
 PYTHON_RTSLIB_FB_LICENSE = Apache-2.0
-PYTHON_RTSLIB_FB_LICENSE_FILES = setup.py
+PYTHON_RTSLIB_FB_LICENSE_FILES = COPYING
 PYTHON_RTSLIB_FB_SETUP_TYPE = setuptools
 PYTHON_RTSLIB_FB_DEPENDENCIES = python-six
 
