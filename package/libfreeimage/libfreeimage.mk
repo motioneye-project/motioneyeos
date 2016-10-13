0c5e57f973a8b1946736f21daba724a4cc32fb9f
Gustavo Zacarias <gustavo.zacarias@free-electrons.com>
2016-10-13 19:13:46 -0300

    libfreeimage: use $(TARGET_MAKE_ENV) when calling $(MAKE)
    Signed-off-by: Gustavo Zacarias <gustavo.zacarias@free-electrons.com>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libfreeimage/libfreeimage.mk
+++ package/libfreeimage/libfreeimage.mk
@@ -22,11 +22,11 @@ define LIBFREEIMAGE_BUILD_CMDS
 endef
 
 define LIBFREEIMAGE_INSTALL_STAGING_CMDS
-	$(MAKE) -C $(@D) DESTDIR=$(STAGING_DIR) install
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(STAGING_DIR) install
 endef
 
 define LIBFREEIMAGE_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
 endef
 
 $(eval $(generic-package))
