72be5e10879ab545010e95fc06f69fe9924519e8
Gustavo Zacarias <gustavo.zacarias@free-electrons.com>
2016-10-13 19:13:12 -0300

    zlog: use $(TARGET_MAKE_ENV) when calling $(MAKE)
    Signed-off-by: Gustavo Zacarias <gustavo.zacarias@free-electrons.com>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/zlog/zlog.mk
+++ package/zlog/zlog.mk
@@ -11,16 +11,16 @@ ZLOG_LICENSE_FILES = COPYING
 ZLOG_INSTALL_STAGING = YES
 
 define ZLOG_BUILD_CMDS
-	$(MAKE1) CC="$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS)" \
+	$(TARGET_MAKE_ENV) $(MAKE1) CC="$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS)" \
 		-C $(@D) all
 endef
 
 define ZLOG_INSTALL_STAGING_CMDS
-	$(MAKE) PREFIX=$(STAGING_DIR)/usr -C $(@D) install
+	$(TARGET_MAKE_ENV) $(MAKE) PREFIX=$(STAGING_DIR)/usr -C $(@D) install
 endef
 
 define ZLOG_INSTALL_TARGET_CMDS
-	$(MAKE) PREFIX=$(TARGET_DIR)/usr -C $(@D) install
+	$(TARGET_MAKE_ENV) $(MAKE) PREFIX=$(TARGET_DIR)/usr -C $(@D) install
 endef
 
 $(eval $(generic-package))
