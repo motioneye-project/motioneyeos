2206a0c0a7381cbfb53f6997e6c2405e96b391f8
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2016-10-16 11:25:26 +0100

    libiscsi: bump version to 1.18.0
    Also...
    
    - Switch to GitHub.
    - Remove LD=CC logic in libscsi.mk. This is now handled by the configure
      script.
    - Add patch to fix unsafe include paths issues. This patch has been sent
      upstream as a pull request.
    - Use a hook to create the m4 directory so autoreconf doesn't fail.
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libiscsi/libiscsi.mk
+++ package/libiscsi/libiscsi.mk
@@ -4,14 +4,17 @@
 #
 ################################################################################
 
-LIBISCSI_VERSION = 1.15.0
-LIBISCSI_SITE = https://sites.google.com/site/libiscsitarballs/libiscsitarballs
+LIBISCSI_VERSION = 1.18.0
+LIBISCSI_SITE = $(call github,sahlberg,libiscsi,$(LIBISCSI_VERSION))
 LIBISCSI_LICENSE = GPLv2+, LGPLv2.1+
 LIBISCSI_LICENSE_FILES = COPYING LICENCE-GPL-2.txt LICENCE-LGPL-2.1.txt
 LIBISCSI_INSTALL_STAGING = YES
+LIBISCSI_AUTORECONF = YES
 
-# Force libiscsi to use gcc as the linker, otherwise it uses directly
-# ld, which doesn't work for certain architectures.
-LIBISCSI_CONF_ENV = LD="$(TARGET_CC)"
+# We need to create the m4 directory to make autoreconf work properly.
+define LIBISCSI_CREATE_M4_DIR
+	mkdir -p $(@D)/m4
+endef
+LIBISCSI_POST_PATCH_HOOKS += LIBISCSI_CREATE_M4_DIR
 
 $(eval $(autotools-package))
