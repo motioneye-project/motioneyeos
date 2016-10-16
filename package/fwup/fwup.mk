15c950731d6cd950bd74748ed121a994d65305f0
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2016-10-16 17:49:50 +0200

    fwup: add dependency on host-pkgconf
    The fwup configure.ac uses PKG_CHECK_MODULES(), and we're
    autoreconfiguring this package, so we should depend on host-pkgconf.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/fwup/fwup.mk
+++ package/fwup/fwup.mk
@@ -8,8 +8,8 @@ FWUP_VERSION = v0.8.2
 FWUP_SITE = $(call github,fhunleth,fwup,$(FWUP_VERSION))
 FWUP_LICENSE = Apache-2.0
 FWUP_LICENSE_FILES = LICENSE
-FWUP_DEPENDENCIES = libconfuse libarchive libsodium
-HOST_FWUP_DEPENDENCIES = host-libconfuse host-libarchive host-libsodium
+FWUP_DEPENDENCIES = host-pkgconf libconfuse libarchive libsodium
+HOST_FWUP_DEPENDENCIES = host-pkgconf host-libconfuse host-libarchive host-libsodium
 FWUP_AUTORECONF = YES
 FWUP_CONF_ENV = ac_cv_path_HELP2MAN=""
 
