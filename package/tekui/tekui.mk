c842b72e10602e31a14dbfbeb1f23fd6db5239ca
Gustavo Zacarias <gustavo.zacarias@free-electrons.com>
2016-10-17 13:06:09 -0300

    tekui: use $(TARGET_MAKE_ENV) when calling $(MAKE)
    Signed-off-by: Gustavo Zacarias <gustavo.zacarias@free-electrons.com>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/tekui/tekui.mk
+++ package/tekui/tekui.mk
@@ -56,11 +56,11 @@ endif
 endif
 
 define TEKUI_BUILD_CMDS
-	$(MAKE) $(TEKUI_MAKE_OPTS) PREFIX="/usr" -C $(@D) all
+	$(TARGET_MAKE_ENV) $(MAKE) $(TEKUI_MAKE_OPTS) PREFIX="/usr" -C $(@D) all
 endef
 
 define TEKUI_INSTALL_TARGET_CMDS
-	$(MAKE) $(TEKUI_MAKE_OPTS) PREFIX="$(TARGET_DIR)/usr" -C $(@D) install
+	$(TARGET_MAKE_ENV) $(MAKE) $(TEKUI_MAKE_OPTS) PREFIX="$(TARGET_DIR)/usr" -C $(@D) install
 endef
 
 $(eval $(generic-package))
