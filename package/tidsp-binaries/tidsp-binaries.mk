beb1c8cb5e7b5c2fcb7263d108d30f6eeaf30eb7
Gustavo Zacarias <gustavo.zacarias@free-electrons.com>
2016-10-17 13:06:19 -0300

    tidsp-binaries: use $(TARGET_MAKE_ENV) when calling $(MAKE)
    Signed-off-by: Gustavo Zacarias <gustavo.zacarias@free-electrons.com>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/tidsp-binaries/tidsp-binaries.mk
+++ package/tidsp-binaries/tidsp-binaries.mk
@@ -10,7 +10,7 @@ TIDSP_BINARIES_LICENSE = TI Proprietary License
 TIDSP_BINARIES_LICENSE_FILES = LICENSE
 
 define TIDSP_BINARIES_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(@D) -e DESTDIR=$(TARGET_DIR) install
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) -e DESTDIR=$(TARGET_DIR) install
 endef
 
 $(eval $(generic-package))
