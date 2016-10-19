26a37a26e16a2fa6d3d0d7ceb097d42e2342841c
Marcin Nowakowski <marcin.nowakowski@imgtec.com>
2016-10-19 09:45:54 +0200

    kexec: bump to 2.0.13
    Signed-off-by: Marcin Nowakowski <marcin.nowakowski@imgtec.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/kexec/kexec.mk
+++ package/kexec/kexec.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KEXEC_VERSION = 2.0.11
+KEXEC_VERSION = 2.0.13
 KEXEC_SOURCE = kexec-tools-$(KEXEC_VERSION).tar.xz
 KEXEC_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kernel/kexec
 KEXEC_LICENSE = GPLv2
