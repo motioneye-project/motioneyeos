cd6448d501c770e48607c27ffe402409a84c555c
Rahul Bedarkar <rahul.bedarkar@imgtec.com>
2016-10-22 00:41:13 +0530

    libfastjson: bump version to 0.99.4
    Drop upstream patch and rename remaining one.
    
    Also fixes:
      http://autobuild.buildroot.net/results/bfe/bfe8ebe3966324b16ca572bfba0a1a8ddc275db9
    
    Cc: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Rahul Bedarkar <rahul.bedarkar@imgtec.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libfastjson/libfastjson.mk
+++ package/libfastjson/libfastjson.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBFASTJSON_VERSION = v0.99.2
+LIBFASTJSON_VERSION = v0.99.4
 LIBFASTJSON_SITE = $(call github,rsyslog,libfastjson,$(LIBFASTJSON_VERSION))
 LIBFASTJSON_INSTALL_STAGING = YES
 # From git
