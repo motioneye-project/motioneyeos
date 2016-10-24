b8bd2bea3b1ce2cdb0c510e2aee889ae9b3f2c92
Yann E. MORIN <yann.morin.1998@free.fr>
2016-10-24 18:38:49 +0200

    package/xdriver_xf86-input-libinput: fix install paths
    Get rid of the borked sdkdir variable.
    
    Fixes (partially):
        https://bugs.busybox.net/show_bug.cgi?id=8696
    
    Reported-by: buildroot@netsolux.ch
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: buildroot@netsolux.ch
    Cc: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xdriver_xf86-input-libinput/xdriver_xf86-input-libinput.mk
+++ package/x11r7/xdriver_xf86-input-libinput/xdriver_xf86-input-libinput.mk
@@ -10,5 +10,6 @@ XDRIVER_XF86_INPUT_LIBINPUT_SITE = http://xorg.freedesktop.org/releases/individu
 XDRIVER_XF86_INPUT_LIBINPUT_LICENSE = MIT
 XDRIVER_XF86_INPUT_LIBINPUT_LICENSE_FILES = COPYING
 XDRIVER_XF86_INPUT_LIBINPUT_DEPENDENCIES = libinput xserver_xorg-server xproto_inputproto xproto_xproto
+XDRIVER_XF86_INPUT_LIBINPUT_AUTORECONF = YES
 
 $(eval $(autotools-package))
