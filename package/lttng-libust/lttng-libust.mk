006c7ab5c45e3fc42729dbe242a0418fb8133c82
Philippe Proulx <eeppeliteloop@gmail.com>
2016-10-31 03:46:16 -0400

    lttng-libust: bump to version 2.8.1
    The two current patches are removed because they were merged upstream.
    
    The new patch is needed to build with uClibc. It was also submitted
    upstream, but not merged as is yet. Defining NT_GNU_BUILD_ID to 3 when
    it's not defined seems like it's something done in other projects. This
    definition's value should never change (number of the build ID note
    section in ELF objects).
    
    LTTng-UST now has its man pages written in AsciiDoc and they get
    converted to troff at build time. This is not needed in a Buildroot
    image anyway.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lttng-libust/lttng-libust.mk
+++ package/lttng-libust/lttng-libust.mk
@@ -5,17 +5,13 @@
 ################################################################################
 
 LTTNG_LIBUST_SITE = http://lttng.org/files/lttng-ust
-LTTNG_LIBUST_VERSION = 2.7.1
+LTTNG_LIBUST_VERSION = 2.8.1
 LTTNG_LIBUST_SOURCE = lttng-ust-$(LTTNG_LIBUST_VERSION).tar.bz2
 LTTNG_LIBUST_LICENSE = LGPLv2.1, MIT (system headers), GPLv2 (liblttng-ust-ctl/ustctl.c used by lttng-sessiond)
 LTTNG_LIBUST_LICENSE_FILES = COPYING
-
-LTTNG_LIBUST_PATCH = \
-	https://github.com/lttng/lttng-ust/commit/787364e8bac9a146f9d65c763633c2b75eb76463.patch \
-	https://github.com/lttng/lttng-ust/commit/30307a67b48641875b122d8f832bbfb454e95d92.patch
-
 LTTNG_LIBUST_INSTALL_STAGING = YES
 LTTNG_LIBUST_DEPENDENCIES = liburcu util-linux
+LTTNG_LIBUST_CONF_OPTS += --disable-man-pages
 
 ifeq ($(BR2_PACKAGE_PYTHON),y)
 LTTNG_LIBUST_DEPENDENCIES += python
