47b16341e10c512164156f2d0e9f342238d0254f
Gary Bisson <gary.bisson@boundarydevices.com>
2016-11-01 17:10:02 +0100

    kernel-module-imx-gpu-viv: bump version to 5.0.11.p8.6
    This patch is based on the Yocto equivalent:
    https://github.com/Freescale/meta-freescale/commit/51148d70
    
    This package has been tested with the following commands:
     # modprobe galcore
     # cd /usr/share/examples/viv_samples/vdk/
     # ./tutorial7
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/freescale-imx/kernel-module-imx-gpu-viv/kernel-module-imx-gpu-viv.mk
+++ package/freescale-imx/kernel-module-imx-gpu-viv/kernel-module-imx-gpu-viv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KERNEL_MODULE_IMX_GPU_VIV_VERSION = aa83874b916cf5655cb90498b1ea5691537fdbc8
+KERNEL_MODULE_IMX_GPU_VIV_VERSION = bfa78b095a892705a09afe9514fc5d236b3124d2
 KERNEL_MODULE_IMX_GPU_VIV_SITE = \
 	$(call github,Freescale,kernel-module-imx-gpu-viv,$(KERNEL_MODULE_IMX_GPU_VIV_VERSION))
 KERNEL_MODULE_IMX_GPU_VIV_LICENSE = GPLv2
