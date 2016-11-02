ab70598952323a01dcec8d1a337f6c748399d00b
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2016-11-02 18:46:24 +0100

    tinyalsa: bump version to 1.0.2
    tinyalsa has started doing release, so we will use it.
    
    With this release, both our patches were applied mainline:
        - 0001-tinypcminfo-make-function-pcm_get_format_name-static.patch [1]
        - 0002-asound.h-include-time.h-to-get-struct-timespec-proto.patch [2]
    
    The Makefile has changed a lot, so we take advantage of that to simplify
    our package.
    
    1. https://github.com/tinyalsa/tinyalsa/commit/7d204edfe87f4e22105bcaeb750d69ac769afdd6
    2. https://github.com/tinyalsa/tinyalsa/commit/c8333f8c7a4e4b9549abeef7530b2cd20a18e537
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    [Thomas: add patch to remove doxygen usage.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/tinyalsa/tinyalsa.mk
+++ package/tinyalsa/tinyalsa.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TINYALSA_VERSION = f2a7b6d3d81bd337a540d56704b4aaa7bdb046fe
+TINYALSA_VERSION = v1.0.2
 TINYALSA_SITE = $(call github,tinyalsa,tinyalsa,$(TINYALSA_VERSION))
 TINYALSA_LICENSE = BSD-3c
 TINYALSA_INSTALL_STAGING = YES
@@ -14,19 +14,17 @@ define TINYALSA_BUILD_CMDS
 endef
 
 define TINYALSA_INSTALL_STAGING_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/libtinyalsa.so \
-		$(STAGING_DIR)/usr/lib/libtinyalsa.so
-	$(INSTALL) -D -m 0644 $(@D)/include/tinyalsa/asoundlib.h \
-		$(STAGING_DIR)/usr/include/tinyalsa/asoundlib.h
+	$(TARGET_MAKE_ENV) $(MAKE) \
+		-C $(@D) \
+		CROSS_COMPILE="$(TARGET_CROSS)" \
+		DESTDIR="$(STAGING_DIR)" install
 endef
 
 define TINYALSA_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/libtinyalsa.so \
-		$(TARGET_DIR)/usr/lib/libtinyalsa.so
-	$(INSTALL) -D -m 0755 $(@D)/tinyplay $(TARGET_DIR)/usr/bin/tinyplay
-	$(INSTALL) -D -m 0755 $(@D)/tinycap $(TARGET_DIR)/usr/bin/tinycap
-	$(INSTALL) -D -m 0755 $(@D)/tinymix $(TARGET_DIR)/usr/bin/tinymix
-	$(INSTALL) -D -m 0755 $(@D)/tinypcminfo $(TARGET_DIR)/usr/bin/tinypcminfo
+	$(TARGET_MAKE_ENV) $(MAKE) \
+		-C $(@D) \
+		CROSS_COMPILE="$(TARGET_CROSS)" \
+		DESTDIR="$(TARGET_DIR)" install
 endef
 
 $(eval $(generic-package))
