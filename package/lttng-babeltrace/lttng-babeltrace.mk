be641e4e3847883552730b4f0511b7ab80e3f8aa
Philippe Proulx <eeppeliteloop@gmail.com>
2016-11-02 02:55:06 -0400

    lttng-babeltrace: bump to version 1.4.0
    Current patches are dropped because they were merged upstream.
    
    Official upstream site of Babeltrace for the distribution of tarballs
    changed from lttng.org to efficios.com (see
    <http://diamon.org/babeltrace>).
    
    Official Babeltrace's website changed from
    <http://www.efficios.com/babeltrace> to <http://diamon.org/babeltrace>.
    
    I explicitly disable the support for debug informations in this patch to
    add it in a separate patch.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lttng-babeltrace/lttng-babeltrace.mk
+++ package/lttng-babeltrace/lttng-babeltrace.mk
@@ -4,18 +4,15 @@
 #
 ################################################################################
 
-LTTNG_BABELTRACE_SITE = http://lttng.org/files/babeltrace
-LTTNG_BABELTRACE_VERSION = 1.2.4
+LTTNG_BABELTRACE_SITE = http://www.efficios.com/files/babeltrace
+LTTNG_BABELTRACE_VERSION = 1.4.0
 LTTNG_BABELTRACE_SOURCE = babeltrace-$(LTTNG_BABELTRACE_VERSION).tar.bz2
 LTTNG_BABELTRACE_LICENSE = MIT, LGPLv2.1 (include/babeltrace/list.h), GPLv2 (test code)
 LTTNG_BABELTRACE_LICENSE_FILES = mit-license.txt gpl-2.0.txt LICENSE
-
 LTTNG_BABELTRACE_DEPENDENCIES = popt util-linux libglib2 host-pkgconf
+LTTNG_BABELTRACE_CONF_OPTS += --disable-debug-info
 HOST_LTTNG_BABELTRACE_DEPENDENCIES = \
 	host-popt host-util-linux host-libglib2 host-pkgconf
-
-# for 0002-configure-fix-uuid-support-detection-on-static-build.patch
-LTTNG_BABELTRACE_AUTORECONF = YES
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
