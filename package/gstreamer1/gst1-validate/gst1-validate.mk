32e0d57a0fed932fcfc4745afda4d65aeb71c39f
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-11-03 06:58:38 -0300

    gst1-validate: bump to version 1.10.0
    It now requires json-glib as dependency so adjust.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gstreamer1/gst1-validate/gst1-validate.mk
+++ package/gstreamer1/gst1-validate/gst1-validate.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_VALIDATE_VERSION = 1.8.3
+GST1_VALIDATE_VERSION = 1.10.0
 GST1_VALIDATE_SOURCE = gst-validate-$(GST1_VALIDATE_VERSION).tar.xz
 GST1_VALIDATE_SITE = https://gstreamer.freedesktop.org/src/gst-validate
 GST1_VALIDATE_LICENSE = LGPLv2.1+
@@ -15,6 +15,7 @@ GST1_VALIDATE_CONF_OPTS = --disable-sphinx-doc
 GST1_VALIDATE_DEPENDENCIES = \
 	gstreamer1 \
 	gst1-plugins-base \
+	json-glib \
 	host-python \
 	python \
 	$(if $(BR2_PACKAGE_CAIRO),cairo)
