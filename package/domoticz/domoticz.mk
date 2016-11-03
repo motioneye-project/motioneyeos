3e45b0314b64dff4882a5e2d3cf77a164e7c4ef4
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2016-11-03 16:32:44 +0100

    domoticz: bump to version 3.5837
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/domoticz/domoticz.mk
+++ package/domoticz/domoticz.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOMOTICZ_VERSION = 3.5834
+DOMOTICZ_VERSION = 3.5837
 DOMOTICZ_SITE = $(call github,domoticz,domoticz,$(DOMOTICZ_VERSION))
 DOMOTICZ_LICENSE = GPLv3
 DOMOTICZ_LICENSE_FILES = License.txt
