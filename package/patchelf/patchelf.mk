b2163fb8d4cc6ca925464b60b517e8953038559d
Jérôme Pouiller <jezz@sysmic.org>
2016-11-09 10:43:46 +0100

    patchelf: bump version to 0.9
    Signed-off-by: Jérôme Pouiller <jezz@sysmic.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/patchelf/patchelf.mk
+++ package/patchelf/patchelf.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PATCHELF_VERSION = 0.8
-PATCHELF_SITE = http://releases.nixos.org/patchelf/patchelf-0.8
+PATCHELF_VERSION = 0.9
+PATCHELF_SITE = http://releases.nixos.org/patchelf/patchelf-0.9
 PATCHELF_LICENSE = GPLv3+
 PATCHELF_LICENSE_FILES = COPYING
 
