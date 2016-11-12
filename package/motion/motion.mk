b0309d3cd37c71341d21bfaf4c5c0012f51f2fb6
Calin Crisan <ccrisan@gmail.com>
2016-11-12 22:59:44 +0200

    removed motion-mrdave; the new 4.0 motion version is now the default

--- package/motion/motion.mk
+++ package/motion/motion.mk
@@ -4,14 +4,15 @@
 #
 ################################################################################
 
-MOTION_SITE = http://www.lavrsen.dk/svn/motion/trunk
-MOTION_SITE_METHOD = svn
-MOTION_VERSION = r561
+MOTION_VERSION = 37b3595
+MOTION_SITE = $(call github,motion-project,motion,$(MOTION_VERSION))
+MOTION_AUTORECONF = YES
 MOTION_CONF_OPTS = --without-pgsql --without-sdl --without-sqlite3 --without-mysql --with-ffmpeg=$(STAGING_DIR)/usr/lib \
         --with-ffmpeg-headers=$(STAGING_DIR)/usr/include
 
 define MOTION_INSTALL_TARGET_CMDS
-    cp $(@D)/motion $(TARGET_DIR)/usr/bin/motion-svn
+    cp $(@D)/motion $(TARGET_DIR)/usr/bin/motion
 endef
 
 $(eval $(autotools-package))
+
