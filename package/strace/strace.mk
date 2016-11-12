7892a778b8bed72202db9641c2649825852148b0
Romain Naour <romain.naour@gmail.com>
2016-11-12 16:30:12 +0100

    package/strace: disable libmpers with musl toolchains
    strace bundle some kernel headers to build libmpers, this mixes userspace
    headers and kernel headers which break the build with musl.
    The stddef.h from gcc is used instead of the one from musl.
    
    Disable libmpers for musl toolchains.
    
    Fixes:
    http://autobuild.buildroot.net/results/6f6/6f63fa2537ff316ba259a20966faadcf5ab9deff
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/strace/strace.mk
+++ package/strace/strace.mk
@@ -10,6 +10,14 @@ STRACE_SITE = http://downloads.sourceforge.net/project/strace/strace/$(STRACE_VE
 STRACE_LICENSE = BSD-3c
 STRACE_LICENSE_FILES = COPYING
 
+# strace bundle some kernel headers to build libmpers, this mixes userspace
+# headers and kernel headers which break the build with musl.
+# The stddef.h from gcc is used instead of the one from musl.
+ifeq ($(BR2_TOOLCHAIN_USES_MUSL),y)
+STRACE_CONF_OPTS += st_cv_m32_mpers=no \
+	st_cv_mx32_mpers=no
+endif
+
 define STRACE_REMOVE_STRACE_GRAPH
 	rm -f $(TARGET_DIR)/usr/bin/strace-graph
 endef
