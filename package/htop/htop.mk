a75a1d36d235b8552e3f6989681d0a4f17acef64
Jérôme Pouiller <jezz@sysmic.org>
2016-11-14 12:04:48 +0100

    htop: enable unicode if possible
    Signed-off-by: Jérôme Pouiller <jezz@sysmic.org>
    [Thomas:
     - use positive logic
     - use += instead of = in conditions.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/htop/htop.mk
+++ package/htop/htop.mk
@@ -7,10 +7,15 @@
 HTOP_VERSION = 2.0.2
 HTOP_SITE = http://hisham.hm/htop/releases/$(HTOP_VERSION)
 HTOP_DEPENDENCIES = ncurses
-HTOP_CONF_OPTS = --disable-unicode
 # Prevent htop build system from searching the host paths
 HTOP_CONF_ENV = HTOP_NCURSES_CONFIG_SCRIPT=$(STAGING_DIR)/usr/bin/ncurses5-config
 HTOP_LICENSE = GPLv2
 HTOP_LICENSE_FILES = COPYING
+
+ifeq ($(BR2_PACKAGE_NCURSES_WCHAR),y)
+HTOP_CONF_OPTS += --enable-unicode
+else
+HTOP_CONF_OPTS += --disable-unicode
+endif
 
 $(eval $(autotools-package))
