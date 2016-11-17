4e8937b7ca484426daab3864a7e5489f3a3a77be
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2016-11-17 17:09:04 +0000

    gstreamer1/gst1-validate: bump version to 1.10.1
    Release notes:
      https://gstreamer.freedesktop.org/releases/gst-validate/1.10.1.html
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gstreamer1/gst1-validate/gst1-validate.mk
+++ package/gstreamer1/gst1-validate/gst1-validate.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_VALIDATE_VERSION = 1.10.0
+GST1_VALIDATE_VERSION = 1.10.1
 GST1_VALIDATE_SOURCE = gst-validate-$(GST1_VALIDATE_VERSION).tar.xz
 GST1_VALIDATE_SITE = https://gstreamer.freedesktop.org/src/gst-validate
 GST1_VALIDATE_LICENSE = LGPLv2.1+
