97f8aa4005be8456af52cacd5cfb4b077e55b5fa
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2016-11-29 11:23:03 +0000

    jasper: bump version to 1.900.31 (security)
    Fixed CVEs:
     - CVE-2016-9387
     - CVE-2016-9388
     - CVE-2016-9389
     - CVE-2016-9390
     - CVE-2016-9391
     - CVE-2016-9392
     - CVE-2016-9393
     - CVE-2016-9394
     - CVE-2016-9395
     - CVE-2016-9396
     - CVE-2016-9397
     - CVE-2016-9398
     - CVE-2016-9399
     - CVE-2016-9557
     - CVE-2016-9560
    
    Changes to jasper.mk:
     - Switched site method to GitHub. 1.900.31 is not released as a tarball
       in the official website.
     - Autoreconf necessary since there isn't any configure script. We need
       to generate it.
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/jasper/jasper.mk
+++ package/jasper/jasper.mk
@@ -4,12 +4,16 @@
 #
 ################################################################################
 
-JASPER_VERSION = 1.900.22
-JASPER_SITE = http://www.ece.uvic.ca/~frodo/jasper/software
+JASPER_VERSION = version-1.900.31
+JASPER_SITE = $(call github,mdadams,jasper,$(JASPER_VERSION))
 JASPER_INSTALL_STAGING = YES
 JASPER_DEPENDENCIES = jpeg
 JASPER_LICENSE = JasPer License Version 2.0
 JASPER_LICENSE_FILES = LICENSE
+
+# No configure script included. We need to generate it.
+JASPER_AUTORECONF = YES
+
 JASPER_CONF_OPTS = --disable-strict
 
 # Xtensa gcc is unable to generate correct code with -O0 enabled by
