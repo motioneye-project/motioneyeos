8af8c9d15fc4b8861e14b118b14339d0bb62f03d
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2016-11-29 11:29:19 +0000

    sqlite: bump version to 3150200 (3.15.2)
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SQLITE_VERSION = 3150100
+SQLITE_VERSION = 3150200
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_VERSION).tar.gz
 SQLITE_SITE = http://www.sqlite.org/2016
 SQLITE_LICENSE = Public domain
