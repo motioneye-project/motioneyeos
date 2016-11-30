cf865e061e38f1a32ae5c2d47e10c00441f33c4e
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2016-11-30 17:04:45 +0000

    lttng-libust: bump version to 2.9.0
    Drop sha256 hash. The combination of md5 and sha1 hashes is enough.
    
    Add a patch that fixes a build failure due to using the compiler of the
    host machine. The patch has been submitted as a pull request:
      https://github.com/lttng/lttng-ust/pull/41
    
    Release notes:
      https://lists.lttng.org/pipermail/lttng-dev/2016-November/026761.html
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lttng-libust/lttng-libust.mk
+++ package/lttng-libust/lttng-libust.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LTTNG_LIBUST_SITE = http://lttng.org/files/lttng-ust
-LTTNG_LIBUST_VERSION = 2.8.1
+LTTNG_LIBUST_VERSION = 2.9.0
 LTTNG_LIBUST_SOURCE = lttng-ust-$(LTTNG_LIBUST_VERSION).tar.bz2
 LTTNG_LIBUST_LICENSE = LGPLv2.1, MIT (system headers), GPLv2 (liblttng-ust-ctl/ustctl.c used by lttng-sessiond)
 LTTNG_LIBUST_LICENSE_FILES = COPYING
