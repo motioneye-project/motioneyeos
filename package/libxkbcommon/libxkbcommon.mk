ed9924e4f434b01d20e0f4985671d47f73d6c92f
Gustavo Zacarias <gustavo@zacarias.com.ar>
2016-12-02 14:42:04 -0300

    libxkbcommon: bump to version 0.7.0
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libxkbcommon/libxkbcommon.mk
+++ package/libxkbcommon/libxkbcommon.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBXKBCOMMON_VERSION = 0.6.1
+LIBXKBCOMMON_VERSION = 0.7.0
 LIBXKBCOMMON_SITE = http://xkbcommon.org/download
 LIBXKBCOMMON_SOURCE = libxkbcommon-$(LIBXKBCOMMON_VERSION).tar.xz
 LIBXKBCOMMON_LICENSE = MIT/X11
