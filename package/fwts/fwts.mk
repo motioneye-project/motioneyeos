b04a707e7e36c9784d5a53a4087e8ad3f609c937
Erico Nunes <nunes.erico@gmail.com>
2016-12-02 23:40:17 +0100

    fwts: bump to version V16.11.00
    Build- and run-tested on x86_64 and aarch64.
    
    Signed-off-by: Erico Nunes <nunes.erico@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/fwts/fwts.mk
+++ package/fwts/fwts.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FWTS_VERSION = V16.09.00
+FWTS_VERSION = V16.11.00
 FWTS_SITE = http://fwts.ubuntu.com/release
 FWTS_STRIP_COMPONENTS = 0
 FWTS_LICENSE = GPLv2, LGPLv2.1, Custom
