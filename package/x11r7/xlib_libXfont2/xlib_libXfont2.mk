a0e86e51e16ceeb222368df16876b4a232e8453f
Bernd Kuhls <bernd.kuhls@t-online.de>
2016-12-03 12:58:06 +0100

    package/x11r7/xlib_libXfont2: new package
    Needed for xserver_xorg-server 1.19+
    https://cgit.freedesktop.org/xorg/xserver/commit/?id=05a793f5b3c40747d5a92a076def7f4fb673c7e7
    
    We need to keep the xlib_libXfont package because xapp_bdftopcf and
    xapp_xfs were not ported yet to xlib_libXfont2:
    https://lists.x.org/archives/xorg-announce/2016-August/002702.html
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/x11r7/xlib_libXfont2/xlib_libXfont2.mk
@@ -0,0 +1,33 @@
+################################################################################
+#
+# xlib_libXfont2
+#
+################################################################################
+
+XLIB_LIBXFONT2_VERSION = 2.0.1
+XLIB_LIBXFONT2_SOURCE = libXfont2-$(XLIB_LIBXFONT2_VERSION).tar.bz2
+XLIB_LIBXFONT2_SITE = http://xorg.freedesktop.org/releases/individual/lib
+XLIB_LIBXFONT2_LICENSE = MIT
+XLIB_LIBXFONT2_LICENSE_FILES = COPYING
+XLIB_LIBXFONT2_INSTALL_STAGING = YES
+XLIB_LIBXFONT2_DEPENDENCIES = \
+	freetype \
+	xlib_libfontenc \
+	xlib_xtrans \
+	xproto_fontsproto \
+	xproto_xproto \
+	xfont_encodings
+
+HOST_XLIB_LIBXFONT2_DEPENDENCIES = \
+	host-freetype \
+	host-xlib_libfontenc \
+	host-xlib_xtrans \
+	host-xproto_fontsproto \
+	host-xproto_xproto \
+	host-xfont_encodings
+
+XLIB_LIBXFONT2_CONF_OPTS = --disable-devel-docs
+HOST_XLIB_LIBXFONT2_CONF_OPTS = --disable-devel-docs
+
+$(eval $(autotools-package))
+$(eval $(host-autotools-package))
