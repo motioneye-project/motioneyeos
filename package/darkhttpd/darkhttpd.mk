9c9de1e2b5d29e171ec98d2fab778f1806ea3ebb
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2016-12-10 20:37:59 +0100

    darkhttpd: new package
    This new package provides darkhttpd, a simple, fast HTTP 1.1 web server
    for static content.
    
    Signed-off-by: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/darkhttpd/darkhttpd.mk
@@ -0,0 +1,34 @@
+################################################################################
+#
+# darkhttpd
+#
+################################################################################
+
+DARKHTTPD_VERSION = 1.12
+DARKHTTPD_SITE = https://unix4lyfe.org/darkhttpd
+DARKHTTPD_SOURCE = darkhttpd-$(DARKHTTPD_VERSION).tar.bz2
+DARKHTTPD_LICENSE = MIT
+
+define DARKHTTPD_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
+endef
+
+define DARKHTTPD_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/darkhttpd \
+		$(TARGET_DIR)/usr/sbin/darkhttpd
+endef
+
+define DARKHTTPD_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 0644 package/darkhttpd/darkhttpd.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/darkhttpd.service
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+	ln -fs ../../../../usr/lib/systemd/system/darkhttpd.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/darkhttpd.service
+endef
+
+define DARKHTTPD_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 package/darkhttpd/S50darkhttpd \
+		$(TARGET_DIR)/etc/init.d/S50darkhttpd
+endef
+
+$(eval $(generic-package))
