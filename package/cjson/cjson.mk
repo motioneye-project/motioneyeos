4f11de1a60064c91245411c06771141c90ca67c6
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2016-12-15 19:08:59 +0100

    cjson: fix error with sh4a and powerpc
    Fixes:
     - http://autobuild.buildroot.net/results/3d899790acdc5c21733ff6f7f5a1b500e862ea0a
    
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/cjson/cjson.mk
+++ package/cjson/cjson.mk
@@ -10,4 +10,10 @@ CJSON_INSTALL_STAGING = YES
 CJSON_LICENSE = MIT
 CJSON_LICENSE_FILES = LICENSE
 
+# This patch fixes -Werror=strict-overflow compile errors on sh4a and powerpc
+# toolchains:
+# http://autobuild.buildroot.net/results/3d899790acdc5c21733ff6f7f5a1b500e862ea0a
+CJSON_PATCH = \
+	https://github.com/DaveGamble/cjson/commit/fcc89c4bb264d665929b00eeebc479a643a90896.patch
+
 $(eval $(cmake-package))
