c02278a426dff5076dcf420c4d699b31a88a30b0
Yegor Yefremov <yegorslists@googlemail.com>
2016-12-16 12:19:16 +0100

    can-utils: bump version
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/can-utils/can-utils.mk
+++ package/can-utils/can-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAN_UTILS_VERSION = b70a76670dbe8925c2a7c75f90d36a28a8878d7a
+CAN_UTILS_VERSION = 64edc021d22441002028bc451548b9e7f1874b91
 CAN_UTILS_SITE = $(call github,linux-can,can-utils,$(CAN_UTILS_VERSION))
 CAN_UTILS_AUTORECONF = YES
 
