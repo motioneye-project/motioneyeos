be2adc1ab28a9b255068c3786b63406b482ed286
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2016-12-17 22:20:47 +0100

    lxc: bump to version 2.0.6
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lxc/lxc.mk
+++ package/lxc/lxc.mk
@@ -4,13 +4,11 @@
 #
 ################################################################################
 
-LXC_VERSION = 2.0.3
+LXC_VERSION = 2.0.6
 LXC_SITE = https://linuxcontainers.org/downloads/lxc
 LXC_LICENSE = LGPLv2.1+
 LXC_LICENSE_FILES = COPYING
 LXC_DEPENDENCIES = libcap host-pkgconf
-# we're patching configure.ac
-LXC_AUTORECONF = YES
 LXC_CONF_OPTS = --disable-apparmor --with-distro=buildroot \
 	--disable-lua --disable-python --disable-werror \
 	$(if $(BR2_PACKAGE_BASH),,--disable-bash)
