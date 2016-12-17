57b628a932b9b4a3c4bf80f4c82a81da5adcb173
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2016-12-17 15:56:21 +0100

    Revert "package/aespipe: fix host compile"
    This reverts commit cb33a7d6bd89306635749b497cd004e986bcd7e5, as it
    causes build failures on platforms where gcc does not understand the
    "-no-pie" option.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/51be9526300b3d03e2e304dec9cae953f2f59de4/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/aespipe/aespipe.mk
+++ package/aespipe/aespipe.mk
@@ -9,12 +9,5 @@ AESPIPE_SOURCE = aespipe-v$(AESPIPE_VERSION).tar.bz2
 AESPIPE_SITE = http://loop-aes.sourceforge.net/aespipe
 AESPIPE_LICENSE = GPL
 
-# Debian enables -fPIE by default, breaking the build:
-# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=837393
-# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=835148
-HOST_AESPIPE_CONF_ENV = \
-	CFLAGS="$(HOST_CFLAGS) -no-pie" \
-	LDFLAGS="$(HOST_LDFLAGS) -no-pie"
-
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
