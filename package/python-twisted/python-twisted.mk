c07ad416b41eab898fc8899f46aed35d5ad923ef
Yegor Yefremov <yegorslists@googlemail.com>
2016-12-19 09:22:36 +0100

    python-twisted: bump to version 16.6.0
    Add two new dependencies:
    
    python-contstantly: runtime only
    python-incremental: both runtime and build-time dependency
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-twisted/python-twisted.mk
+++ package/python-twisted/python-twisted.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_TWISTED_VERSION = 16.4.1
+PYTHON_TWISTED_VERSION = 16.6.0
 PYTHON_TWISTED_SOURCE = Twisted-$(PYTHON_TWISTED_VERSION).tar.bz2
-PYTHON_TWISTED_SITE = https://pypi.python.org/packages/6b/23/8dbe86fc83215015e221fbd861a545c6ec5c9e9cd7514af114d1f64084ab
+PYTHON_TWISTED_SITE = https://pypi.python.org/packages/dc/c0/a0114a6d7fa211c0904b0de931e8cafb5210ad824996cc6a9d67f3bae22c
 PYTHON_TWISTED_SETUP_TYPE = setuptools
 PYTHON_TWISTED_LICENSE = MIT
 PYTHON_TWISTED_LICENSE_FILES = LICENSE
+PYTHON_TWISTED_DEPENDENCIES = python-incremental
 
 $(eval $(python-package))
