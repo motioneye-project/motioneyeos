cc6091caea67e8a9144f0f2602ddb2798eb725cb
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2016-12-19 16:26:18 +0100

    lxc: add optional dependency on gnutls
    lxc can use gnutls for various checksumming so add a dependency on it if
    package is selected
    
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lxc/lxc.mk
+++ package/lxc/lxc.mk
@@ -9,9 +9,23 @@ LXC_SITE = https://linuxcontainers.org/downloads/lxc
 LXC_LICENSE = LGPLv2.1+
 LXC_LICENSE_FILES = COPYING
 LXC_DEPENDENCIES = libcap host-pkgconf
+# we're patching configure.ac
+LXC_AUTORECONF = YES
+
+# This patch adds --enable-gnutls option
+LXC_PATCH = \
+	https://github.com/lxc/lxc/commit/64fa248372c90c9d98fc9d67f80327d865c11a48.patch
+
 LXC_CONF_OPTS = --disable-apparmor --with-distro=buildroot \
 	--disable-python --disable-werror \
 	$(if $(BR2_PACKAGE_BASH),,--disable-bash)
+
+ifeq ($(BR2_PACKAGE_GNUTLS),y)
+LXC_CONF_OPTS += --enable-gnutls
+LXC_DEPENDENCIES += gnutls
+else
+LXC_CONF_OPTS += --disable-gnutls
+endif
 
 ifeq ($(BR2_PACKAGE_LIBSECCOMP),y)
 LXC_CONF_OPTS += --enable-seccomp
