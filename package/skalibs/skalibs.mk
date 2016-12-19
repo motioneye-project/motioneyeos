332360e34289cfaea66cbf04ddc648ba837a904f
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2016-12-19 22:29:07 +0100

    skalibs: new package
    This new package provides skalibs, a collection of free software / open
    source C development files used for building all softwares from
    skarnet.org.
    
    Note that, though skalibs (and all skarnet softwares) follows the
    "./configure; make; make install" convention, it does not behave like a
    traditional autotools project:
    
     - static libraries are installed in $prefix/usr/lib/skalibs.
     - pkg-config and libtool are not used: instead a custom system called
       "sysdeps" is used and locations to libraries and headers are to be
       passed explicitly via options of the './configure' script.
    
    The host variant is provided to allow building the host variants of the
    other skarnet softwares.
    
    Signed-off-by: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    [Thomas: remove post install target hook, do it directly in the target
    installation commands.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/skalibs/skalibs.mk
@@ -0,0 +1,54 @@
+################################################################################
+#
+# skalibs
+#
+################################################################################
+
+SKALIBS_VERSION = 2.4.0.2
+SKALIBS_SITE = http://skarnet.org/software/skalibs
+SKALIBS_LICENSE = ISC
+SKALIBS_LICENSE_FILES = COPYING
+SKALIBS_INSTALL_STAGING = YES
+
+SKALIBS_CONF_OPTS = \
+	--prefix=/usr \
+	--with-default-path=/sbin:/usr/sbin:/bin:/usr/bin \
+	$(SHARED_STATIC_LIBS_OPTS)
+
+define SKALIBS_CONFIGURE_CMDS
+	(cd $(@D); $(TARGET_CONFIGURE_OPTS) ./configure $(SKALIBS_CONF_OPTS))
+endef
+
+define SKALIBS_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define SKALIBS_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
+	rm -rf $(TARGET_DIR)/usr/lib/skalibs
+endef
+
+define SKALIBS_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(STAGING_DIR) install
+endef
+
+HOST_SKALIBS_CONF_OPTS = \
+	--prefix=$(HOST_DIR)/usr \
+	--disable-static \
+	--enable-shared \
+	--disable-allstatic
+
+define HOST_SKALIBS_CONFIGURE_CMDS
+	(cd $(@D); $(HOST_CONFIGURE_OPTS) ./configure $(HOST_SKALIBS_CONF_OPTS))
+endef
+
+define HOST_SKALIBS_BUILD_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define HOST_SKALIBS_INSTALL_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) install
+endef
+
+$(eval $(generic-package))
+$(eval $(host-generic-package))
