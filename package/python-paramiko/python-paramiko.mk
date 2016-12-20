2d7b73cf75daedd4a198a75546308fedae7c9467
Yegor Yefremov <yegorslists@googlemail.com>
2016-12-20 15:28:52 +0100

    python-paramiko: bump to version 2.1.1
    Replace wrong runtime dependencies with python-pyasn and
    python-cryptography.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-paramiko/python-paramiko.mk
+++ package/python-paramiko/python-paramiko.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PARAMIKO_VERSION = 2.0.2
+PYTHON_PARAMIKO_VERSION = 2.1.1
 PYTHON_PARAMIKO_SOURCE = paramiko-$(PYTHON_PARAMIKO_VERSION).tar.gz
-PYTHON_PARAMIKO_SITE = https://pypi.python.org/packages/6b/4e/971b8c917456a2ec9666311f7e695493002a64022397cd668150b5efd2a8
+PYTHON_PARAMIKO_SITE = https://pypi.python.org/packages/d1/5a/ebd00d884f30baf208359a027eb7b38372d81d0c004724bb1aa71ae43b37
 PYTHON_PARAMIKO_SETUP_TYPE = setuptools
 PYTHON_PARAMIKO_LICENSE = LGPLv2.1+
 PYTHON_PARAMIKO_LICENSE_FILES = LICENSE
