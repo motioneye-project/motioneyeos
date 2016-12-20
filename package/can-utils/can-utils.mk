8812cffe996c8d0cbd48bf7e565921ba68d5d293
Rahul Bedarkar <rahul.bedarkar@imgtec.com>
2016-12-20 22:40:48 +0530

    can-utils: bump version
    This version bump also fixes:
    http://autobuild.buildroot.net/results/0e5/0e5242376ff6aa82e89ed1172350e05009d48156
    
    Signed-off-by: Rahul Bedarkar <rahul.bedarkar@imgtec.com>
    Cc: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/can-utils/can-utils.mk
+++ package/can-utils/can-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAN_UTILS_VERSION = 64edc021d22441002028bc451548b9e7f1874b91
+CAN_UTILS_VERSION = c3305fdd515464153d20199db232b6124bc962c0
 CAN_UTILS_SITE = $(call github,linux-can,can-utils,$(CAN_UTILS_VERSION))
 CAN_UTILS_AUTORECONF = YES
 
