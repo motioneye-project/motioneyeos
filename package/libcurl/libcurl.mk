0c5beb6501707dd5cb80484562bf2b0cbe2b4423
Peter Korsgaard <peter@korsgaard.com>
2016-12-21 08:48:49 +0100

    libcurl: security bump to 7.52.0
    Fixes CVE-2016-9586 - printf floating point buffer overflow
    
    For details, see:
    https://curl.haxx.se/docs/adv_20161221A.html
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libcurl/libcurl.mk
+++ package/libcurl/libcurl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBCURL_VERSION = 7.51.0
+LIBCURL_VERSION = 7.52.0
 LIBCURL_SOURCE = curl-$(LIBCURL_VERSION).tar.bz2
 LIBCURL_SITE = https://curl.haxx.se/download
 LIBCURL_DEPENDENCIES = host-pkgconf \
