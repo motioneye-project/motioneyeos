427c432821d2edc349328633af8df359b2a5e075
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2016-12-22 09:02:32 +0100

    libupnp: bump to version 1.6.21
    Signed-off-by: Fabrice Fontaine <fabrice.fontaine@orange.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libupnp/libupnp.mk
+++ package/libupnp/libupnp.mk
@@ -4,14 +4,12 @@
 #
 ################################################################################
 
-LIBUPNP_VERSION = 1.6.20
+LIBUPNP_VERSION = 1.6.21
 LIBUPNP_SOURCE = libupnp-$(LIBUPNP_VERSION).tar.bz2
 LIBUPNP_SITE = http://downloads.sourceforge.net/project/pupnp/pupnp/libUPnP%20$(LIBUPNP_VERSION)
 LIBUPNP_CONF_ENV = ac_cv_lib_compat_ftime=no
 LIBUPNP_INSTALL_STAGING = YES
 LIBUPNP_LICENSE = BSD-3c
 LIBUPNP_LICENSE_FILES = LICENSE
-# configure.ac patched by 0001-Don-t-allow-unhandled-POSTs-to-write-to-the-filesyst.patch
-LIBUPNP_AUTORECONF = YES
 
 $(eval $(autotools-package))
