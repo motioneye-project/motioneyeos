80f6660460538f4f318719583619a2be059ed3fe
Christian Stewart <christian@paral.in>
2016-12-22 23:37:01 -0800

    docker-containerd: bump version to v0.2.5
    Version bump as a dependency of Docker-Engine v1.12.5.
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/docker-containerd/docker-containerd.mk
+++ package/docker-containerd/docker-containerd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOCKER_CONTAINERD_VERSION = v0.2.4
+DOCKER_CONTAINERD_VERSION = v0.2.5
 DOCKER_CONTAINERD_SITE = $(call github,docker,containerd,$(DOCKER_CONTAINERD_VERSION))
 DOCKER_CONTAINERD_LICENSE = Apache-2.0
 DOCKER_CONTAINERD_LICENSE_FILES = LICENSE.code
