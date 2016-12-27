d91ce1aa08016fa3fb186be9e6ef425dad6f050f
Baruch Siach <baruch@tkos.co.il>
2016-12-27 19:42:03 +0200

    fs/tar: make --no-recursion effective
    The tar --no-recursion option is position sensitive. It only affects following
    file listing options. Move --no-recursion before the -T option to make it
    effective. This fixes duplication of entries in the generated rootfs.tar
    archive.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- fs/tar/tar.mk
+++ fs/tar/tar.mk
@@ -8,7 +8,7 @@ TAR_OPTS := $(call qstrip,$(BR2_TARGET_ROOTFS_TAR_OPTIONS))
 
 define ROOTFS_TAR_CMD
 	(cd $(TARGET_DIR); find -print0 | LC_ALL=C sort -z | \
-		tar $(TAR_OPTS) -cf $@ --null -T - --no-recursion --numeric-owner)
+		tar $(TAR_OPTS) -cf $@ --null --no-recursion -T - --numeric-owner)
 endef
 
 $(eval $(call ROOTFS_TARGET,tar))
