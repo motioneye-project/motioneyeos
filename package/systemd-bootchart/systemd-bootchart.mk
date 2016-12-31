0d7b6a470f5ed006dfd48c7c8ee504f02677c812
Yann E. MORIN <yann.morin.1998@free.fr>
2016-12-31 17:54:39 +0100

    package/systemd-bootchart: bump version
    Get rid of our patch, applied upstream. Which means we no longer need to
    run intltoolize. So drop the dependency on host-intltool
    
    Fixes:
        http://autobuild.buildroot.net/results/696/696254009f830134ef9398369ca2cbb257b33f52/
        http://autobuild.buildroot.org/results/aca/aca210de7d3f2eda54e5630206e9ff80d72d85c5/
        http://autobuild.buildroot.org/results/e5d/e5df8d11bfce4ba7a4c5c760b4784c31c506d8d4/
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Reviewed-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/systemd-bootchart/systemd-bootchart.mk
+++ package/systemd-bootchart/systemd-bootchart.mk
@@ -4,22 +4,16 @@
 #
 ################################################################################
 
-SYSTEMD_BOOTCHART_VERSION = 230
+SYSTEMD_BOOTCHART_VERSION = 231
 SYSTEMD_BOOTCHART_SOURCE = systemd-bootchart-$(SYSTEMD_BOOTCHART_VERSION).tar.xz
-# Do not use the github helper here, the generated tarball is *NOT* the same
-# as the one uploaded by upstream for the release.
+# Do not use the github helper here: the uploaded release tarball already
+# contains the generated autotools scripts. It also slightly differs with
+# two missing source files... :-/
 SYSTEMD_BOOTCHART_SITE = https://github.com/systemd/systemd-bootchart/releases/download/v$(SYSTEMD_BOOTCHART_VERSION)
 SYSTEMD_BOOTCHART_LICENSE = LGPLv2.1+
 SYSTEMD_BOOTCHART_LICENSE_FILES = LICENSE.LGPL2.1
-SYSTEMD_BOOTCHART_DEPENDENCIES = host-intltool systemd
+SYSTEMD_BOOTCHART_DEPENDENCIES = systemd
 
-# 0001-configure-add-option-to-not-build-manpages.patch
-SYSTEMD_BOOTCHART_AUTORECONF = YES
 SYSTEMD_BOOTCHART_CONF_OPTS = --disable-man
-
-define SYSTEMD_BOOTCHART_INTLTOOLIZE
-	cd $(@D) && $(HOST_DIR)/usr/bin/intltoolize --force --automake
-endef
-SYSTEMD_BOOTCHART_PRE_CONFIGURE_HOOKS = SYSTEMD_BOOTCHART_INTLTOOLIZE
 
 $(eval $(autotools-package))
