03a3a201a35bc23acaa65104bdb2d523aa053068
David Bachelart <david.bachelart@bbright.com>
2017-01-03 09:26:46 +0100

    python-chardet: new package
    Signed-off-by: David Bachelart <david.bachelart@bbright.com>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/python-chardet/python-chardet.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-chardet
+#
+################################################################################
+
+PYTHON_CHARDET_VERSION = 2.3.0
+PYTHON_CHARDET_SOURCE = chardet-$(PYTHON_CHARDET_VERSION).tar.gz
+PYTHON_CHARDET_SITE = https://pypi.python.org/packages/7d/87/4e3a3f38b2f5c578ce44f8dc2aa053217de9f0b6d737739b0ddac38ed237
+PYTHON_CHARDET_SETUP_TYPE = setuptools
+PYTHON_CHARDET_LICENSE = LGPLv2.1+
+PYTHON_CHARDET_LICENSE_FILES = LICENSE
+
+$(eval $(python-package))
