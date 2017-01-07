a21d2c94091596ecf9a66726265eb5641c35d653
Gilles Talis <gilles.talis@gmail.com>
2017-01-07 15:06:10 +0100

    ocrad: bump to version 0.25
    Signed-off-by: Gilles Talis <gilles.talis@gmail.com>
    [Thomas:
     - fix indentation.]
     - use $(TAR_OPTIONS) instead of hardcoding -xf.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ocrad/ocrad.mk
+++ package/ocrad/ocrad.mk
@@ -4,11 +4,18 @@
 #
 ################################################################################
 
-OCRAD_VERSION = 0.21
+OCRAD_VERSION = 0.25
+OCRAD_SOURCE = ocrad-$(OCRAD_VERSION).tar.lz
 OCRAD_SITE = $(BR2_GNU_MIRROR)/ocrad
 OCRAD_LICENSE = GPLv3+
 OCRAD_LICENSE_FILES = COPYING
 OCRAD_INSTALL_STAGING = YES
+OCRAD_DEPENDENCIES = host-lzip
+
+define OCRAD_EXTRACT_CMDS
+	$(HOST_DIR)/usr/bin/lzip -d -c $(DL_DIR)/$(OCRAD_SOURCE) | \
+		tar --strip-components=1 -C $(@D) $(TAR_OPTIONS) -
+endef
 
 # This is not a true autotools package.
 define OCRAD_CONFIGURE_CMDS
