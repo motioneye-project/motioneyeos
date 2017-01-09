f5c2d063278d876b0b5555f06e1e2e8e1b57bbcf
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-01-09 15:22:08 +0000

    lttng-babeltrace: bump version to 1.5.1
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lttng-babeltrace/lttng-babeltrace.mk
+++ package/lttng-babeltrace/lttng-babeltrace.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LTTNG_BABELTRACE_SITE = http://www.efficios.com/files/babeltrace
-LTTNG_BABELTRACE_VERSION = 1.5.0
+LTTNG_BABELTRACE_VERSION = 1.5.1
 LTTNG_BABELTRACE_SOURCE = babeltrace-$(LTTNG_BABELTRACE_VERSION).tar.bz2
 LTTNG_BABELTRACE_LICENSE = MIT, LGPLv2.1 (include/babeltrace/list.h), GPLv2 (test code)
 LTTNG_BABELTRACE_LICENSE_FILES = mit-license.txt gpl-2.0.txt LICENSE
