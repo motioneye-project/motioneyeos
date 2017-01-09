0c61c4cd317bed43478415c3f3cd74ba1ec31f8e
Adam Duskett <Aduskett@gmail.com>
2017-01-09 11:15:21 -0500

    libsemanage: bump version to 2.6
    Signed-off-by: Adam Duskett <aduskett@codeblue.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libsemanage/libsemanage.mk
+++ package/libsemanage/libsemanage.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LIBSEMANAGE_VERSION = 2.5
-LIBSEMANAGE_SITE = https://raw.githubusercontent.com/wiki/SELinuxProject/selinux/files/releases/20160223
+LIBSEMANAGE_VERSION = 2.6
+LIBSEMANAGE_SITE = https://raw.githubusercontent.com/wiki/SELinuxProject/selinux/files/releases/20161014
 LIBSEMANAGE_LICENSE = LGPLv2.1+
 LIBSEMANAGE_LICENSE_FILES = COPYING
 LIBSEMANAGE_DEPENDENCIES = host-bison host-flex audit libselinux ustr bzip2
