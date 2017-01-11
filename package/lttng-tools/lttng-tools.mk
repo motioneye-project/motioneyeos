2248fa6a5a9127b44150c2bcbfea5a368952a233
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-01-11 14:24:23 +0000

    lttng-tools: bump version to 2.9.3
    0001-Fix-Add-missing-pthread.h-include.patch already included in this
    release:
    
      https://github.com/lttng/lttng-tools/commit/5f702b607116a8303cb09fb091d8b9740c06baca
    
    --with-xml-prefix configure option doesn't exist, so drop it:
    
      configure:22815: WARNING: unrecognized options: [snip], --with-xml-prefix
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lttng-tools/lttng-tools.mk
+++ package/lttng-tools/lttng-tools.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-LTTNG_TOOLS_VERSION = 2.9.0
+LTTNG_TOOLS_VERSION = 2.9.3
 LTTNG_TOOLS_SITE = http://lttng.org/files/lttng-tools
 LTTNG_TOOLS_SOURCE = lttng-tools-$(LTTNG_TOOLS_VERSION).tar.bz2
 LTTNG_TOOLS_LICENSE = GPLv2+, LGPLv2.1+ (include/lttng/*, src/lib/lttng-ctl/*)
 LTTNG_TOOLS_LICENSE_FILES = gpl-2.0.txt lgpl-2.1.txt LICENSE
-LTTNG_TOOLS_CONF_OPTS += --with-xml-prefix=$(STAGING_DIR)/usr --disable-man-pages
+LTTNG_TOOLS_CONF_OPTS += --disable-man-pages
 
 LTTNG_TOOLS_DEPENDENCIES = liburcu libxml2 popt util-linux
 
