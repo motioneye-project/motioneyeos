44d2fdf62f115cd667d996896ffcad3923f975c8
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-01-12 21:38:21 +0100

    package/freeswitch: bump version to 1.6.14
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/freeswitch/freeswitch.mk
+++ package/freeswitch/freeswitch.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FREESWITCH_VERSION = 1.6.13
+FREESWITCH_VERSION = 1.6.14
 FREESWITCH_SOURCE = freeswitch-$(FREESWITCH_VERSION).tar.xz
 FREESWITCH_SITE = http://files.freeswitch.org/freeswitch-releases
 FREESWITCH_LICENSE = MPLv1.1, \
