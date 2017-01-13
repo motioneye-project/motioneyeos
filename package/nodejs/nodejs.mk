0064132ba032da39cefa4fffe59c31a71d1f1ddb
Daniel Mack <daniel@zonque.org>
2017-01-13 11:55:25 +0100

    package/nodejs: add support for building on aarch64 architectures
    Signed-off-by: Daniel Mack <daniel@zonque.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/nodejs/nodejs.mk
+++ package/nodejs/nodejs.mk
@@ -90,6 +90,8 @@ else ifeq ($(BR2_mipsel),y)
 NODEJS_CPU = mipsel
 else ifeq ($(BR2_arm),y)
 NODEJS_CPU = arm
+else ifeq ($(BR2_aarch64),y)
+NODEJS_CPU = arm64
 # V8 needs to know what floating point ABI the target is using.
 NODEJS_ARM_FP = $(call qstrip,$(BR2_GCC_TARGET_FLOAT_ABI))
 endif
