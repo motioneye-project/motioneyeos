24fa507acf53ad26144d7f57629407723e4a75c6
Gustavo Zacarias <gustavo@zacarias.com.ar>
2017-01-17 20:23:12 -0300

    Revert "gperf: bump to version 3.1"
    Revert gperf bump, it's breaking everything left and right. Fixes:
    http://autobuild.buildroot.net/results/9d5/9d5dad172fe5c5a6247cd5ce6928307236184a64/
    http://autobuild.buildroot.net/results/872/8728860024c7d114fb5b8df3f55588ac09ecf323/
    
    And probably other packages as well.
    
    This reverts commit 570c8d2ae6a10e10b45adc75145726994219f946.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gperf/gperf.mk
+++ package/gperf/gperf.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GPERF_VERSION = 3.1
+GPERF_VERSION = 3.0.4
 GPERF_SITE = $(BR2_GNU_MIRROR)/gperf
 GPERF_LICENSE = GPLv3+
 GPERF_LICENSE_FILES = COPYING
