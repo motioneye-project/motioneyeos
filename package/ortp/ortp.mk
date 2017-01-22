2564c3b321313b52122513c63e62353485ac7128
Jörg Krause <joerg.krause@embedded.rocks>
2017-01-22 22:00:21 +0100

    package/ortp: bump version to 0.27.0
    ortp now depends on bctoolbox, which means we have to add the C++
    dependency of bctoolbox to ortp and its reverse dependencies.
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ortp/ortp.mk
+++ package/ortp/ortp.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-ORTP_VERSION = 0.25.0
+ORTP_VERSION = 0.27.0
 ORTP_SITE = http://download.savannah.nongnu.org/releases/linphone/ortp/sources
 
 ORTP_CONF_OPTS = --disable-strict
 ORTP_INSTALL_STAGING = YES
 ORTP_LICENSE = LGPLv2.1+
 ORTP_LICENSE_FILES = COPYING
+ORTP_DEPENDENCIES = bctoolbox
 
 $(eval $(autotools-package))
