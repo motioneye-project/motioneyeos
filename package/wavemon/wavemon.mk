5fa89d2ca82bd6228b3983951648772203028504
Jörg Krause <joerg.krause@embedded.rocks>
2017-01-22 22:12:00 +0100

    package/wavemon: bump to version 0.8.1
    No need to fetch the musl patch from upstream anymore.
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/wavemon/wavemon.mk
+++ package/wavemon/wavemon.mk
@@ -4,14 +4,11 @@
 #
 ################################################################################
 
-WAVEMON_VERSION = v0.8.0
+WAVEMON_VERSION = v0.8.1
 WAVEMON_SITE = $(call github,uoaerg,wavemon,$(WAVEMON_VERSION))
 WAVEMON_LICENSE = GPLv3+
 WAVEMON_LICENSE_FILES = COPYING
 WAVEMON_DEPENDENCIES = host-pkgconf libnl ncurses
-
-# Fix musl build issue
-WAVEMON_PATCH = https://github.com/uoaerg/wavemon/commit/01e987a032b81af7bbeba1c439759d750dc1f398.patch
 
 # Handwritten Makefile.in, automake isn't used
 WAVEMON_MAKE_OPTS = CC="$(TARGET_CC)"
