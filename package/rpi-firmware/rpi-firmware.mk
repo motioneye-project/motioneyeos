14b8ad175da731113c285ca83855341a765c6364
Peter Seiderer <ps.report@gmx.net>
2017-01-23 21:23:17 +0100

    rpi-firmware: bump version
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 2d5dc978b5a1a25f1c68666747e7585fabbcde2f
+RPI_FIRMWARE_VERSION = 7f8ac8dac0b80291cbf5e56580139034a0a42070
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
