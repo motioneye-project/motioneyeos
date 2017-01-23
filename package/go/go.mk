5c9db62171cefb125193a6f814a0046536fc76a1
Peter Korsgaard <peter@korsgaard.com>
2017-01-23 16:17:46 +0100

    go: security bump to version 1.7.4
    On Darwin, user's trust preferences for root certificates were not honored.
    If the user had a root certificate loaded in their Keychain that was
    explicitly not trusted, a Go program would still verify a connection using
    that root certificate.  This is addressed by https://golang.org/cl/33721,
    tracked in https://golang.org/issue/18141.  Thanks to Xy Ziemba for
    identifying and reporting this issue.
    
    The net/http package's Request.ParseMultipartForm method starts writing to
    temporary files once the request body size surpasses the given "maxMemory"
    limit.  It was possible for an attacker to generate a multipart request
    crafted such that the server ran out of file descriptors.  This is addressed
    by https://golang.org/cl/30410, tracked in https://golang.org/issue/17965.
    Thanks to Simon Rawet for the report.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.7.2
+GO_VERSION = 1.7.4
 GO_SITE = https://storage.googleapis.com/golang
 GO_SOURCE = go$(GO_VERSION).src.tar.gz
 
