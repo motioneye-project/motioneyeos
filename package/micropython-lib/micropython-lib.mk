2b4d4db2a296859fd509978003c4fa41e4073644
Chris Packham <judge.packham@gmail.com>
2017-01-24 23:14:22 +1300

    micropython-lib: needs pcre
    The micropython libs load libpcre dynamically using the foreign function
    interface (libffi). Without pcre the build will succeed but at run time
    anything that uses the 're' module will have issues.
    
    Signed-off-by: Chris Packham <judge.packham@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/micropython-lib/micropython-lib.mk
+++ package/micropython-lib/micropython-lib.mk
@@ -8,6 +8,7 @@ MICROPYTHON_LIB_VERSION = v1.8.2
 MICROPYTHON_LIB_SITE = $(call github,micropython,micropython-lib,$(MICROPYTHON_LIB_VERSION))
 MICROPYTHON_LIB_LICENSE = Python software foundation license v2 (some modules), MIT (everything else)
 MICROPYTHON_LIB_LICENSE_FILES = LICENSE
+MICROPYTHON_LIB_DEPENDENCIES = pcre
 
 define MICROPYTHON_LIB_INSTALL_TARGET_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
