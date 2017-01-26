f9a6a2df56012b2ee6d171ca9371910c668bfa78
Gustavo Zacarias <gustavo@zacarias.com.ar>
2017-01-26 17:07:56 -0300

    openssl: security bump to version 1.0.2k
    Fixes:
    CVE-2017-3731 - Truncated packet could crash via OOB read.
    CVE-2017-3732 - BN_mod_exp may produce incorrect results on x86_64
    CVE-2016-7055 - Montgomery multiplication may produce incorrect results
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENSSL_VERSION = 1.0.2j
+OPENSSL_VERSION = 1.0.2k
 OPENSSL_SITE = http://www.openssl.org/source
 OPENSSL_LICENSE = OpenSSL or SSLeay
 OPENSSL_LICENSE_FILES = LICENSE
