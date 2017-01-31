c06aa42f7c1a0d22a3c816adacc25bb0b12d0179
Lionel Flandrin <lionel@svkt.org>
2017-01-31 12:22:45 +0100

    python-pyzmq: bump to version 16.0.2
    Updated the zmq version detection patch to apply cleanly on 16.0.2.
    
    Added patch #2 to replace compile_and_run with compile_and_forget in the setup
    code: this function is only used to check for the availability of "sys/un.h" and
    we only need to compile the test code to make sure of that. This fixes the
    cross-compilation.
    
    Signed-off-by: Lionel Flandrin <lionel@svkt.org>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-pyzmq/python-pyzmq.mk
+++ package/python-pyzmq/python-pyzmq.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYZMQ_VERSION = 14.3.1
+PYTHON_PYZMQ_VERSION = 16.0.2
 PYTHON_PYZMQ_SOURCE = pyzmq-$(PYTHON_PYZMQ_VERSION).tar.gz
-PYTHON_PYZMQ_SITE = http://pypi.python.org/packages/source/p/pyzmq
+PYTHON_PYZMQ_SITE = https://pypi.python.org/packages/af/37/8e0bf3800823bc247c36715a52e924e8f8fd5d1432f04b44b8cd7a5d7e55
 PYTHON_PYZMQ_LICENSE = LGPLv3+, BSD-3c, Apache-2.0
 # Apache license only online: http://www.apache.org/licenses/LICENSE-2.0
 PYTHON_PYZMQ_LICENSE_FILES = COPYING.LESSER COPYING.BSD
