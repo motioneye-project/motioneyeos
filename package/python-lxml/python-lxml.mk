4f147ae0a0f940dfc7ec93af8e70b6625821d91d
Carlos Santos <casantos@datacom.ind.br>
2017-02-01 15:51:08 -0200

    python-lxml: allow build as host package
    While currently there is no in-tree Buildroot package which depends on
    host-python-lxml, we (DATACOM) have some proprietary modules that use it
    in their test scripts.
    
    We tested python-lxml as host package and confirmed that it builds and
    works correctly. Someone else might require it, so we are proposing its
    inclusion.
    
    Signed-off-by: Carlos Santos <casantos@datacom.ind.br>
    [Thomas: add Config.in.host entry.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-lxml/python-lxml.mk
+++ package/python-lxml/python-lxml.mk
@@ -21,11 +21,16 @@ PYTHON_LXML_LICENSE_FILES = \
 PYTHON_LXML_SETUP_TYPE = setuptools
 
 PYTHON_LXML_DEPENDENCIES = libxml2 libxslt zlib
+HOST_PYTHON_LXML_DEPENDENCIES = host-libxml2 host-libxslt host-zlib
 
 # python-lxml needs these scripts in order to properly detect libxml2 and
 # libxslt compiler and linker flags
 PYTHON_LXML_BUILD_OPTS = \
 	--with-xslt-config=$(STAGING_DIR)/usr/bin/xslt-config \
 	--with-xml2-config=$(STAGING_DIR)/usr/bin/xml2-config
+HOST_PYTHON_LXML_BUILD_OPTS = \
+	--with-xslt-config=$(HOST_DIR)/usr/bin/xslt-config \
+	--with-xml2-config=$(HOST_DIR)/usr/bin/xml2-config
 
 $(eval $(python-package))
+$(eval $(host-python-package))
