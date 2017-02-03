971a13472d9c52c1498f603bd1bc9f70f51ef925
Alexey Brodkin <Alexey.Brodkin@synopsys.com>
2017-02-03 10:26:41 +0300

    toolchain-external: Update Synopsys prebuilt toolchain for ARC cores
    Sync external prebuilt toolchain with the one we now build in Buildroot,
    i.e. arc-2016.09. Since that prebuilt toolchain finally has IPv6 enabled
    it works pretty fine for building packages in Buildroot.
    
    Still note:
     1) There might be subtle differences between uClibc configuration
        compared to Buildroot's one.
     2) A couple of patches we apply on top of Builroot-built toolchain
        are obviously missing in the prebuilt version - they will be
        available in the next release.
    
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>
    Cc: Vlad Zakharov <vzakhar@synopsys.com>
    Cc: Peter Korsgaard <peter@korsgaard.com>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- toolchain/toolchain-external/toolchain-external-synopsys-arc/toolchain-external-synopsys-arc.mk
+++ toolchain/toolchain-external/toolchain-external-synopsys-arc/toolchain-external-synopsys-arc.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-TOOLCHAIN_EXTERNAL_SYNOPSYS_ARC_VERSION = 2014.12
-TOOLCHAIN_EXTERNAL_SYNOPSYS_ARC_SITE = https://github.com/foss-for-synopsys-dwc-arc-processors/toolchain/releases/download/arc-$(TOOLCHAIN_EXTERNAL_SYNOPSYS_ARC_VERSION)
+TOOLCHAIN_EXTERNAL_SYNOPSYS_ARC_VERSION = 2016.09
+TOOLCHAIN_EXTERNAL_SYNOPSYS_ARC_SITE = https://github.com/foss-for-synopsys-dwc-arc-processors/toolchain/releases/download/arc-$(TOOLCHAIN_EXTERNAL_SYNOPSYS_ARC_VERSION)-release
 
 ifeq ($(BR2_arc750d)$(BR2_arc770d),y)
 TOOLCHAIN_EXTERNAL_SYNOPSYS_ARC_CORE = arc700
@@ -19,6 +19,6 @@ else
 TOOLCHAIN_EXTERNAL_SYNOPSYS_ARC_ENDIANESS = be
 endif
 
-TOOLCHAIN_EXTERNAL_SYNOPSYS_ARC_SOURCE = arc_gnu_$(TOOLCHAIN_EXTERNAL_SYNOPSYS_ARC_VERSION)_prebuilt_uclibc_$(TOOLCHAIN_EXTERNAL_SYNOPSYS_ENDIANESS)_$(TOOLCHAIN_EXTERNAL_SYNOPSYS_CORE)_linux_install.tar.gz
+TOOLCHAIN_EXTERNAL_SYNOPSYS_ARC_SOURCE = arc_gnu_$(TOOLCHAIN_EXTERNAL_SYNOPSYS_ARC_VERSION)_prebuilt_uclibc_$(TOOLCHAIN_EXTERNAL_SYNOPSYS_ARC_ENDIANESS)_$(TOOLCHAIN_EXTERNAL_SYNOPSYS_ARC_CORE)_linux_install.tar.gz
 
 $(eval $(toolchain-external-package))
