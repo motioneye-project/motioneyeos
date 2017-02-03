648fef3edf2b4cd243a463a41f324c1db99eb56b
Adam Duskett <Aduskett@gmail.com>
2017-02-03 15:29:57 -0500

    nginx-naxsi: bump version to 0.55.1
    Signed-off-by: Adam Duskett <aduskett@codeblue.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/nginx-naxsi/nginx-naxsi.mk
+++ package/nginx-naxsi/nginx-naxsi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NGINX_NAXSI_VERSION = 0.54
+NGINX_NAXSI_VERSION = 0.55.1
 NGINX_NAXSI_SITE = $(call github,nbs-system,naxsi,$(NGINX_NAXSI_VERSION))
 NGINX_NAXSI_LICENSE = GPLv2+ with OpenSSL exception
 NGINX_NAXSI_LICENSE_FILES = naxsi_src/naxsi_json.c
