c225bd9762903b4c39524446c77f8bedcaa66bf5
Adam Duskett <Aduskett@gmail.com>
2017-02-03 15:29:58 -0500

    openvmtools: bump version to 5a9033ddfa95786d867e4d02bbb9a29bac8fb64f
    Technically this is 10.1.0, however the initial release left out a config
    directory causing autoconfig to fail.  See this post:
    https://github.com/vmware/open-vm-tools/issues/115#issuecomment-269963907
    
    As such, I specified the hash to fix the release:
    https://github.com/vmware/open-vm-tools/commit/5a9033ddfa95786d867e4d02bbb9a29bac8fb64f
    
    Signed-off-by: Adam Duskett <aduskett@codeblue.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/openvmtools/openvmtools.mk
+++ package/openvmtools/openvmtools.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-OPENVMTOOLS_VERSION = stable-9.10.2
+OPENVMTOOLS_VERSION = 5a9033ddfa95786d867e4d02bbb9a29bac8fb64f
 OPENVMTOOLS_SITE = $(call github,vmware,open-vm-tools,$(OPENVMTOOLS_VERSION))
 OPENVMTOOLS_SUBDIR = open-vm-tools
 OPENVMTOOLS_LICENSE = LGPLv2.1
 OPENVMTOOLS_LICENSE_FILES = $(OPENVMTOOLS_SUBDIR)/COPYING
+
 # Autoreconf needed or config/missing will run configure again at buildtime
 OPENVMTOOLS_AUTORECONF = YES
 OPENVMTOOLS_CONF_OPTS = --with-dnet \
@@ -44,13 +45,6 @@ OPENVMTOOLS_DEPENDENCIES += linux-pam
 else
 OPENVMTOOLS_CONF_OPTS += --without-pam
 endif
-
-# configure needs execution permission
-define OPENVMTOOLS_PRE_CONFIGURE_CHMOD
-	chmod 0755 $(@D)/$(OPENVMTOOLS_SUBDIR)/configure
-endef
-
-OPENVMTOOLS_PRE_CONFIGURE_HOOKS += OPENVMTOOLS_PRE_CONFIGURE_CHMOD
 
 # symlink needed by lib/system/systemLinux.c (or will cry in /var/log/messages)
 # defined in lib/misc/hostinfoPosix.c
