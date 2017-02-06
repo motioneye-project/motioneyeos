0f3627a91fd97ee9442755403e2864cd32994f6d
Brian Redbeard <redbeard@coreos.com>
2017-02-06 08:49:25 -0800

    package/cpio: Fix GNU Mirror Path
    Replacing ftpmirror.gnu.org with BR2_GNU_MIRROR variable
    
    Signed-off-by: Brian 'redbeard' Harrington <redbeard@coreos.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/cpio/cpio.mk
+++ package/cpio/cpio.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 CPIO_VERSION = 2.12
-CPIO_SITE = http://ftp.gnu.org/gnu/cpio
+CPIO_SITE = $(BR2_GNU_MIRROR)/cpio
 CPIO_CONF_OPTS = --bindir=/bin
 CPIO_DEPENDENCIES = $(if $(BR2_PACKAGE_BUSYBOX),busybox)
 CPIO_LICENSE = GPLv3+
