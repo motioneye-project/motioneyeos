9d8cf78f7240560a9262d8c2b31fd9db158fd29f
Peter Korsgaard <peter@korsgaard.com>
2017-02-07 09:55:27 +0100

    libftdi: fix typo in variable name
    As pointed out during the check-package discussion, there is a typo in a
    variable name:
    
    http://lists.busybox.net/pipermail/buildroot/2016-December/180765.html
    
    Reported-by: Ricardo Martincoski <ricardo.martincoski@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libftdi/libftdi.mk
+++ package/libftdi/libftdi.mk
@@ -12,14 +12,14 @@ LIBFTDI_INSTALL_STAGING = YES
 LIBFTDI_CONFIG_SCRIPTS = libftdi-config
 LIBFTDI_AUTORECONF = YES
 
-LIBFDTI_CONF_OPTS = --without-examples
+LIBFTDI_CONF_OPTS = --without-examples
 
 # configure detect it automaticaly so we need to force it
 ifeq ($(BR2_PACKAGE_LIBTFDI_CPP),y)
 LIBFTDI_DEPENDENCIES += boost
-LIBFDTI_CONF_OPTS += --enable-libftdipp
+LIBFTDI_CONF_OPTS += --enable-libftdipp
 else
-LIBFDTI_CONF_OPTS += --disable-libftdipp
+LIBFTDI_CONF_OPTS += --disable-libftdipp
 endif
 
 $(eval $(autotools-package))
