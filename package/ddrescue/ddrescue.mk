cc8c12345cf2ff6e154845f89d1ba65ea2bd3f49
Baruch Siach <baruch@tkos.co.il>
2017-02-09 21:50:18 +0200

    ddrescue: fix target build
    ddrescue is not an autotools package. Convert to generic package to make
    it build correctly for target architecture.
    
    Cc: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ddrescue/ddrescue.mk
+++ package/ddrescue/ddrescue.mk
@@ -16,4 +16,20 @@ define DDRESCUE_EXTRACT_CMDS
 		tar --strip-components=1 -C $(@D) $(TAR_OPTIONS) -
 endef
 
-$(eval $(autotools-package))
+define DDRESCUE_CONFIGURE_CMDS
+	(cd $(@D); \
+		$(TARGET_MAKE_ENV) ./configure \
+		--prefix=/usr \
+		$(TARGET_CONFIGURE_OPTS) \
+	)
+endef
+
+define DDRESCUE_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define DDRESCUE_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR="$(TARGET_DIR)" install
+endef
+
+$(eval $(generic-package))
