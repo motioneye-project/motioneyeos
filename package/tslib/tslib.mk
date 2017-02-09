7794d5f94946d94c2a19913fb88cacfd2b7d3531
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2017-02-09 07:55:04 +0200

    Revert "tslib: needs kernel headers 3.12"
    This reverts commit 7c60211ce38273580f1f8bf5886771406f9899ca.
    
    tslib-1.4 removes this dependency.
    
    Add upstream patch that fixes input macros declaration for older kernels.
    
    The ts_uinput tool requires 3.6 headers; disable for older kernels.
    
    Signed-off-by: Martin Kepplinger <martin.kepplinger@ginzinger.com>
    [baruch: add tslib patch; disable ts_uinput]
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tslib/tslib.mk
+++ package/tslib/tslib.mk
@@ -10,8 +10,13 @@ TSLIB_SOURCE = tslib-$(TSLIB_VERSION).tar.xz
 TSLIB_LICENSE = GPL, LGPL
 TSLIB_LICENSE_FILES = COPYING
 
+TSLIB_PATCH = https://github.com/kergoth/tslib/commit/02795c4fa8b842c6fc017e86b5e036466bd4d3ef.patch
 TSLIB_AUTORECONF = YES
 TSLIB_INSTALL_STAGING = YES
 TSLIB_INSTALL_STAGING_OPTS = DESTDIR=$(STAGING_DIR) LDFLAGS=-L$(STAGING_DIR)/usr/lib install
+
+ifeq ($(BR2_TOOLCHAIN_HEADERS_AT_LEAST_3_6),)
+TSLIB_CONF_OPTS += --disable-tools
+endif
 
 $(eval $(autotools-package))
