42eabd755130d3a8cabc5f8fe81f5d46b55d37b8
Baruch Siach <baruch@tkos.co.il>
2017-02-09 15:32:30 +0200

    ed: security bump to version 1.14.1
    Fixes CVE-2017-5357: crash with some malformed commands.
    
    Upstream now provides .tar.lz archive. Add the necessary extract command.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ed/ed.mk
+++ package/ed/ed.mk
@@ -4,13 +4,20 @@
 #
 ################################################################################
 
-ED_VERSION = 1.9
+ED_VERSION = 1.14.1
 ED_SITE = $(BR2_GNU_MIRROR)/ed
+ED_SOURCE = ed-$(ED_VERSION).tar.lz
 ED_CONF_OPTS = \
 	CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" \
 	LDFLAGS="$(TARGET_LDFLAGS)"
+ED_DEPENDENCIES = host-lzip
 ED_LICENSE = GPLv3+
 ED_LICENSE_FILES = COPYING
+
+define ED_EXTRACT_CMDS
+	$(HOST_DIR)/usr/bin/lzip -d -c $(DL_DIR)/$(ED_SOURCE) | \
+		tar --strip-components=1 -C $(@D) $(TAR_OPTIONS) -
+endef
 
 define ED_CONFIGURE_CMDS
 	(cd $(@D); \
