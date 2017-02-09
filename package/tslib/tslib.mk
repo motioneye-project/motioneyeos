211d8a9dce207d832459644e227d8ac99399d394
Martin Kepplinger <martin.kepplinger@ginzinger.com>
2017-02-09 14:37:24 +0100

    tslib: bump to 1.5
    Signed-off-by: Martin Kepplinger <martin.kepplinger@ginzinger.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tslib/tslib.mk
+++ package/tslib/tslib.mk
@@ -4,13 +4,12 @@
 #
 ################################################################################
 
-TSLIB_VERSION = 1.4
+TSLIB_VERSION = 1.5
 TSLIB_SITE = https://github.com/kergoth/tslib/releases/download/$(TSLIB_VERSION)
 TSLIB_SOURCE = tslib-$(TSLIB_VERSION).tar.xz
 TSLIB_LICENSE = GPL, LGPL
 TSLIB_LICENSE_FILES = COPYING
 
-TSLIB_PATCH = https://github.com/kergoth/tslib/commit/02795c4fa8b842c6fc017e86b5e036466bd4d3ef.patch
 TSLIB_AUTORECONF = YES
 TSLIB_INSTALL_STAGING = YES
 TSLIB_INSTALL_STAGING_OPTS = DESTDIR=$(STAGING_DIR) LDFLAGS=-L$(STAGING_DIR)/usr/lib install
