ed0234ad786215b530bbce345458e36b5ba89237
Adam Duskett <Aduskett@gmail.com>
2017-02-19 14:12:55 -0500

    python-xlwt: bump to version 1.2.0
    Signed-off-by: Adam Duskett <Adamduskett@outlook.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-xlwt/python-xlwt.mk
+++ package/python-xlwt/python-xlwt.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_XLWT_VERSION = 1.1.2
+PYTHON_XLWT_VERSION = 1.2.0
 PYTHON_XLWT_SOURCE = xlwt-$(PYTHON_XLWT_VERSION).tar.gz
-PYTHON_XLWT_SITE = https://pypi.python.org/packages/0b/69/644313df86e6375ec2c6b34ec8ac544b9cc7803b7d943223d32811860f3d
+PYTHON_XLWT_SITE = https://pypi.python.org/packages/5b/8d/22b9ec552a1d7865de39f54bd15f9db09c72a6bf8ab77b11dcce4ae336bb
 PYTHON_XLWT_SETUP_TYPE = setuptools
 PYTHON_XLWT_LICENSE = BSD-3c, BSD-4c
 PYTHON_XLWT_LICENSE_FILES = docs/licenses.rst
