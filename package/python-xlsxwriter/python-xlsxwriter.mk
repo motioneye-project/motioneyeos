94d5b89456156bb3f5c1f1fdd24b6a33fcb1956c
Adam Duskett <Aduskett@gmail.com>
2017-02-19 14:12:54 -0500

    python-xlsxwriter: bump to version 0.9.6
    Signed-off-by: Adam Duskett <Adamduskett@outlook.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-xlsxwriter/python-xlsxwriter.mk
+++ package/python-xlsxwriter/python-xlsxwriter.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_XLSXWRITER_VERSION = 0.9.4
+PYTHON_XLSXWRITER_VERSION = 0.9.6
 PYTHON_XLSXWRITER_SOURCE = XlsxWriter-$(PYTHON_XLSXWRITER_VERSION).tar.gz
-PYTHON_XLSXWRITER_SITE = https://pypi.python.org/packages/52/66/670322b9999b8e47659c8b172a11aa3c0e629a5fc86b5a8fd497b85941c0
+PYTHON_XLSXWRITER_SITE = https://pypi.python.org/packages/c9/e0/c2b2f57bf53c84af3d2a41a82af7025828cb2bc8a312848a6ab7d018de56
 PYTHON_XLSXWRITER_SETUP_TYPE = setuptools
 PYTHON_XLSXWRITER_LICENSE = BSD-2c
 PYTHON_XLSXWRITER_LICENSE_FILES = LICENSE.txt
