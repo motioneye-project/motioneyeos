e7b8484eef2a2d069f906636307866964ac4976d
Adam Duskett <Aduskett@gmail.com>
2017-02-19 14:12:40 -0500

    python-pysocks: bump to version 1.6.6
    Signed-off-by: Adam Duskett <Adamduskett@outlook.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-pysocks/python-pysocks.mk
+++ package/python-pysocks/python-pysocks.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYSOCKS_VERSION = 1.5.7
+PYTHON_PYSOCKS_VERSION = 1.6.6
 PYTHON_PYSOCKS_SOURCE = PySocks-$(PYTHON_PYSOCKS_VERSION).tar.gz
-PYTHON_PYSOCKS_SITE = https://pypi.python.org/packages/16/56/9b3513078f837fa8cb88ee01ec1cd805ed8104a37bc02ca8c2588ae8fe5a
+PYTHON_PYSOCKS_SITE = https://pypi.python.org/packages/fd/70/ba9982cedc9b3ed3c06934f1f46a609e0f23c7bfdf567c52a09f1296b8cb
 PYTHON_PYSOCKS_LICENSE = BSD-3c
 PYTHON_PYSOCKS_LICENSE_FILES = socks.py
 PYTHON_PYSOCKS_SETUP_TYPE = distutils
