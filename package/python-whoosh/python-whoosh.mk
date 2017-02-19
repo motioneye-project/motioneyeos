95cc2b1d2a4ab3faf925ff063fa98a7116344cda
Adam Duskett <Aduskett@gmail.com>
2017-02-19 14:12:53 -0500

    python-whoosh: bump to version 2.7.4
    Signed-off-by: Adam Duskett <Adamduskett@outlook.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-whoosh/python-whoosh.mk
+++ package/python-whoosh/python-whoosh.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WHOOSH_VERSION = 2.7.0
+PYTHON_WHOOSH_VERSION = 2.7.4
 PYTHON_WHOOSH_SOURCE = Whoosh-$(PYTHON_WHOOSH_VERSION).tar.gz
-PYTHON_WHOOSH_SITE = http://pypi.python.org/packages/source/W/Whoosh
+PYTHON_WHOOSH_SITE = https://pypi.python.org/packages/25/2b/6beed2107b148edc1321da0d489afc4617b9ed317ef7b72d4993cad9b684
 PYTHON_WHOOSH_SETUP_TYPE = setuptools
 PYTHON_WHOOSH_LICENSE = BSD-3c
 PYTHON_WHOOSH_LICENSE_FILES = LICENSE.txt
