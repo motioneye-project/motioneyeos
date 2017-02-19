b0c7bd34286b39192dc8958dcf2140b240032b19
Adam Duskett <Aduskett@gmail.com>
2017-02-19 14:12:41 -0500

    python-pytrie: bump to version 0.3
    Signed-off-by: Adam Duskett <Adamduskett@outlook.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-pytrie/python-pytrie.mk
+++ package/python-pytrie/python-pytrie.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_PYTRIE_VERSION = 0.2
-PYTHON_PYTRIE_SITE = http://pypi.python.org/packages/source/P/PyTrie
+PYTHON_PYTRIE_VERSION = 0.3
+PYTHON_PYTRIE_SITE = https://pypi.python.org/packages/aa/4a/3a3e7e95cb6202192c268043bed586f299e5b72b2650ece4614f60aecf2b
 PYTHON_PYTRIE_SOURCE = PyTrie-$(PYTHON_PYTRIE_VERSION).tar.gz
 PYTHON_PYTRIE_LICENSE = BSD-3c
 PYTHON_PYTRIE_LICENSE_FILES = LICENSE
