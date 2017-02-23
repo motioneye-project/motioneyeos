97b723685aae570d897d81b22187e28a464839b2
Arnout Vandecappelle <arnout@mind.be>
2017-02-23 18:00:27 +0100

    lpty: remove LPTY_VERSION_UPSTREAM
    It is not used for anything.
    
    Note that the SUBDIR is NOT the usual lpty-$(LPTY_VERSION_UPSTREAM);
    it *does* have the rockspec version in its name.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lpty/lpty.mk
+++ package/lpty/lpty.mk
@@ -4,8 +4,7 @@
 #
 ################################################################################
 
-LPTY_VERSION_UPSTREAM = 1.0.1
-LPTY_VERSION = $(LPTY_VERSION_UPSTREAM)-1
+LPTY_VERSION = 1.0.1-1
 LPTY_SUBDIR = lpty-$(LPTY_VERSION)
 LPTY_LICENSE = MIT
 LPTY_LICENSE_FILES = $(LPTY_SUBDIR)/doc/LICENSE
