0d87679a5c33aa7f0970f5c290b537dab526d95f
Arnout Vandecappelle <arnout@mind.be>
2017-02-23 18:00:41 +0100

    lua-bit32: use PKG_NAME_UPSTREAM
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lua-bit32/lua-bit32.mk
+++ package/lua-bit32/lua-bit32.mk
@@ -5,9 +5,8 @@
 ################################################################################
 
 LUA_BIT32_VERSION = 5.3.0-1
+LUA_BIT32_NAME_UPSTREAM = bit32
 LUA_BIT32_SUBDIR = lua-compat-5.2
-LUA_BIT32_ROCKSPEC = bit32-$(LUA_BIT32_VERSION).rockspec
-LUA_BIT32_SOURCE = bit32-$(LUA_BIT32_VERSION).src.rock
 LUA_BIT32_LICENSE = MIT
 LUA_BIT32_LICENSE_FILES = $(LUA_BIT32_SUBDIR)/LICENSE
 
