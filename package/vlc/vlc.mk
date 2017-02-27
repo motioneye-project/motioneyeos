ba1dd18a4963cb0e63fbc93bb5e108c4d1f137b5
Waldemar Brodkorb <wbx@openadk.org>
2017-02-27 07:07:25 +0100

    vlc: workaround microblaze gcc internal compiler error
    Override gcc optimization flags with -O0. The workaround is not required
    for gcc6 anymore, so we take this into account into our condition.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/a318f0838a6a602046e719103ac81965c0084d52
    
    Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>
    [Thomas: add gcc 6.x condition.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/vlc/vlc.mk
+++ package/vlc/vlc.mk
@@ -15,6 +15,13 @@ VLC_AUTORECONF = YES
 # Install vlc libraries in staging.
 VLC_INSTALL_STAGING = YES
 
+# gcc bug internal compiler error: in merge_overlapping_regs, at
+# regrename.c:304. This bug is fixed since gcc 6.
+ifeq ($(BR2_microblaze):$(BR2_TOOLCHAIN_GCC_AT_LEAST_6),y:)
+VLC_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -O0"
+VLC_CONF_OPTS += --disable-optimizations
+endif
+
 # VLC defines two autoconf functions which are also defined by our own pkg.m4
 # from pkgconf. Unfortunately, they are defined in a different way: VLC adds
 # --enable- options, but pkg.m4 adds --with- options. To make sure we use
