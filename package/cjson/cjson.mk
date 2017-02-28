e01248c4cfd34a66517dc2c57d4478e3ce31ba43
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2017-02-28 23:26:07 +0100

    cjson: fix build for toolchains without ssp
    Set ENABLE_CUSTOM_COMPILER_FLAGS to OFF to disable custom flags, in
    particular -fstack-protector-strong which depends on
    BR2_TOOLCHAIN_HAS_SSP
    
    Fixes:
    
      http://autobuild.buildroot.net/results/cfcf3bc8066159dfddd1786954d78e8527858f2f/
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/cjson/cjson.mk
+++ package/cjson/cjson.mk
@@ -9,7 +9,10 @@ CJSON_SITE = $(call github,DaveGamble,cjson,$(CJSON_VERSION))
 CJSON_INSTALL_STAGING = YES
 CJSON_LICENSE = MIT
 CJSON_LICENSE_FILES = LICENSE
+# Set ENABLE_CUSTOM_COMPILER_FLAGS to OFF in particular to disable
+# -fstack-protector-strong which depends on BR2_TOOLCHAIN_HAS_SSP
 CJSON_CONF_OPTS += \
-	-DENABLE_CJSON_TEST=OFF
+	-DENABLE_CJSON_TEST=OFF \
+	-DENABLE_CUSTOM_COMPILER_FLAGS=OFF
 
 $(eval $(cmake-package))
