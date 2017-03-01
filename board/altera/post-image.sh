41a69cc2dcaba4e3796a00194256353895a12382
Arnout Vandecappelle <arnout@mind.be>
2017-03-01 09:30:46 +0100

    altera_*_defconfig: remove broken defconfigs
    These defconfigs have been broken due to gcc5 for more than half a year,
    and nobody stepped up to fix it. In addition, the rocketboards git
    repository used by these defconfigs hasn't been reachable for more than
    half a year. Finally, these defconfigs don't even use the
    BR2_TARGET_UBOOT_ALTERA_SOCFPGA_IMAGE_CRC option so they are not a
    great example for this family of targets.
    
    Remove these defconfigs, as well as the board directory. Both are
    removed in a single commit to avoid having an inconsistent readme.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Cc: Jan Viktorin <viktorin@rehivetech.com>
    Cc: Charles Manning <cdhmanning@gmail.com>
    Cc: Sebastien Bourdelin <sebastien.bourdelin@savoirfairelinux.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- board/altera/post-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# post-image.sh for SoCkit/SoCDK
-# 2014, "Roman Diouskine" <roman.diouskine@savoirfairelinux.com>
-# 2014, "Sebastien Bourdelin" <sebastien.bourdelin@savoirfairelinux.com>
-
-# create a DTB file copy with the name expected by the u-boot config
-# Name of the DTB is passed as the second argument to the script.
-cp -af $BINARIES_DIR/${2}.dtb  $BINARIES_DIR/socfpga.dtb
