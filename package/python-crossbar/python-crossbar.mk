ff51f57731724cb34faa5bff94a21c90031d6a31
Yegor Yefremov <yegorslists@googlemail.com>
2017-03-02 14:58:53 +0100

    python-crossbar: add a warning comment concerning version bump
    The latest crossbar needs a rather recent setuptools package, that
    has circular dependencies. Until this issue is not resolved, it is
    not safe to bump crossbar version.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-crossbar/python-crossbar.mk
+++ package/python-crossbar/python-crossbar.mk
@@ -4,6 +4,8 @@
 #
 ################################################################################
 
+# don't bump version as newer setuptools package is needed,
+# that still didn't make its way into Buildroot
 PYTHON_CROSSBAR_VERSION = 0.14.0
 PYTHON_CROSSBAR_SOURCE = crossbar-$(PYTHON_CROSSBAR_VERSION).tar.gz
 PYTHON_CROSSBAR_SITE = https://pypi.python.org/packages/f0/9a/e0b77e15698c47b6293655bc0e1996dd8e87bd8af7bc7434a5c8281a024e
