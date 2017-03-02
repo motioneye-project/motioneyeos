68793184f557ccf2989ad6c1c89b1d943bb9b866
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2017-03-02 19:58:26 +0100

    docker-containerd: bump to version 977c511eda0925a723debdc94d09459af49d082a
    This is a runtime dependency of docker-engine in version 17.03.0-ce
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/docker-containerd/docker-containerd.mk
+++ package/docker-containerd/docker-containerd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOCKER_CONTAINERD_VERSION = aa8187dbd3b7ad67d8e5e3a15115d3eef43a7ed1
+DOCKER_CONTAINERD_VERSION = 977c511eda0925a723debdc94d09459af49d082a
 DOCKER_CONTAINERD_SITE = $(call github,docker,containerd,$(DOCKER_CONTAINERD_VERSION))
 DOCKER_CONTAINERD_LICENSE = Apache-2.0
 DOCKER_CONTAINERD_LICENSE_FILES = LICENSE.code
