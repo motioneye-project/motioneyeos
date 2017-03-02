3eb1ed921c308de2bbe5d98762ef17525a9618d5
Gustavo Zacarias <gustavo@zacarias.com.ar>
2017-03-02 12:26:30 -0300

    weston: bump to version 2.0.0
    Drop upstream patches.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/weston/weston.mk
+++ package/weston/weston.mk
@@ -4,13 +4,11 @@
 #
 ################################################################################
 
-WESTON_VERSION = 1.12.0
+WESTON_VERSION = 2.0.0
 WESTON_SITE = http://wayland.freedesktop.org/releases
 WESTON_SOURCE = weston-$(WESTON_VERSION).tar.xz
 WESTON_LICENSE = MIT
 WESTON_LICENSE_FILES = COPYING
-# configure.ac patched by 0003-configure-search-for-lib-with-clock_getres.patch
-WESTON_AUTORECONF = YES
 
 WESTON_DEPENDENCIES = host-pkgconf wayland wayland-protocols \
 	libxkbcommon pixman libpng jpeg mtdev udev cairo libinput \
