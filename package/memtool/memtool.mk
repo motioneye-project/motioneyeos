949e97bca088b5a59ac87439330555eeab3fe437
Yann E. MORIN <yann.morin.1998@free.fr>
2017-03-03 19:13:54 +0100

    package/memtool: fix trailing /
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/memtool/memtool.mk
+++ package/memtool/memtool.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 MEMTOOL_VERSION = 2016.10.0
-MEMTOOL_SITE = http://public.pengutronix.de/software/memtool/
+MEMTOOL_SITE = http://public.pengutronix.de/software/memtool
 MEMTOOL_SOURCE = memtool-$(MEMTOOL_VERSION).tar.xz
 MEMTOOL_LICENSE = GPLv2
 MEMTOOL_LICENSE_FILES = COPYING
