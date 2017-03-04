17fc9162648e0d76e007e557fc3225d138ae85ec
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-03-04 11:38:49 +0100

    Revert "python-cherrypy: bump to version 10.1.1"
    This reverts commit 98c0bb2324ab8a2eb2bacaa497f11f773af771cd, which
    causes some build failures.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/b66/b66f8fbce3a5127b69a09bdc91a460a3c107e29d/build-end.log
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-cherrypy/python-cherrypy.mk
+++ package/python-cherrypy/python-cherrypy.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CHERRYPY_VERSION = 10.1.1
+PYTHON_CHERRYPY_VERSION = 4.0.0
 PYTHON_CHERRYPY_SOURCE = CherryPy-$(PYTHON_CHERRYPY_VERSION).tar.gz
-PYTHON_CHERRYPY_SITE = https://pypi.python.org/packages/da/e2/6ed242ebfe96912e0866cc01139027d342d52c35ad33ee182bfb671c177d
+PYTHON_CHERRYPY_SITE = http://pypi.python.org/packages/source/C/CherryPy
 PYTHON_CHERRYPY_LICENSE = BSD-3c
 PYTHON_CHERRYPY_LICENSE_FILES = cherrypy/LICENSE.txt
 PYTHON_CHERRYPY_SETUP_TYPE = distutils
