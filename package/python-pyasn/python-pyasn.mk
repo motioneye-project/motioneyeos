9c2ef1eafd398faf7223828165e4757ff2991383
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-03-04 11:40:44 +0100

    Revert "python-pyasn: bump to version 1.6.0b1"
    This reverts commit 98c9b1bec6fd511889f38c72bc4aa5aba89e264d, which
    causes build failures.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/27e1c6cdf330bd2009bf8895d8c72a7c4f5186af/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-pyasn/python-pyasn.mk
+++ package/python-pyasn/python-pyasn.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYASN_VERSION = 1.6.0b1
-PYTHON_PYASN_SOURCE = pyasn-$(PYTHON_PYASN_VERSION).tar.gz
-PYTHON_PYASN_SITE = https://pypi.python.org/packages/31/da/c8338545be0ee7a727c977113e75888e4f1f2b2e10f9284fdfa31dab29bc
+PYTHON_PYASN_VERSION = 0.1.9
+PYTHON_PYASN_SOURCE = pyasn1-$(PYTHON_PYASN_VERSION).tar.gz
+PYTHON_PYASN_SITE = https://pypi.python.org/packages/source/p/pyasn1
 PYTHON_PYASN_LICENSE = BSD-2c
 PYTHON_PYASN_LICENSE_FILES = LICENSE.txt
 PYTHON_PYASN_SETUP_TYPE = distutils
