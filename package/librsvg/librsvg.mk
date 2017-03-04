7372c80cc5b07b25d5ee5bc9909ed807a9e69fd7
Romain Naour <romain.naour@gmail.com>
2017-03-04 11:35:03 +0100

    package/librsvg: disable gdk-pixbuf-loader
    gdk-pixbuf-loader support is enabled by default but it silently fail to
    generate gdk-pixbuf.loaders file when host != target.
    
    For exemple on ARM target:
    output/host/usr/bin/gdk-pixbuf-query-loaders ./libpixbufloader-svg.la
    g_module_open() failed for output/build/librsvg-2.40.16/gdk-pixbuf-loader/./libpixbufloader-svg.la: output/build/librsvg-2.40.16/gdk-pixbuf-loader/./.libs/libpixbufloader-svg.so: wrong ELF class: ELFCLASS32
    
    But it doesn't break the build.
    
    When host = target using the Sourcery CodeBench AMD64 2016.11 toolchain
    optimized for x86_68 AMD Puma/Jaguar or AMD Steamroller, it break the
    build due to "Illegal instruction".
    
    output/host/usr/bin/gdk-pixbuf-query-loaders libpixbufloader-svg.la
    Illegal instruction (core dumped)
    
    Since this option is broken for cross-compilation, disable it.
    
    Fixes:
    http://autobuild.buildroot.net/results/393/393145bc9bcb93d6df55ec8c63725c3d9a299957
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/librsvg/librsvg.mk
+++ package/librsvg/librsvg.mk
@@ -9,7 +9,7 @@ LIBRSVG_VERSION = $(LIBRSVG_VERSION_MAJOR).16
 LIBRSVG_SITE = http://ftp.gnome.org/pub/gnome/sources/librsvg/$(LIBRSVG_VERSION_MAJOR)
 LIBRSVG_SOURCE = librsvg-$(LIBRSVG_VERSION).tar.xz
 LIBRSVG_INSTALL_STAGING = YES
-LIBRSVG_CONF_OPTS = --disable-tools --enable-introspection=no
+LIBRSVG_CONF_OPTS = --disable-pixbuf-loader --disable-tools --enable-introspection=no
 HOST_LIBRSVG_CONF_OPTS = --enable-introspection=no
 LIBRSVG_DEPENDENCIES = cairo host-gdk-pixbuf gdk-pixbuf libcroco libglib2 libxml2 pango \
 	$(if $(BR2_PACKAGE_LIBGTK3),libgtk3)
