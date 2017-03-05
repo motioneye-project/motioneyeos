2ea735f5a0ec3bbb51e77375f4cecf66eca9c539
Baruch Siach <baruch@tkos.co.il>
2017-03-05 21:33:36 +0200

    ipset: drop obsolete patch
    Since commit 0552a367dbd ("Remove --{enable, disable}-debug configure flags.")
    we do not pass --enable-debug to configure script. The patch to remove -Werror
    in this case is thus no longer needed.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ipset/ipset.mk
+++ package/ipset/ipset.mk
@@ -9,8 +9,6 @@ IPSET_SOURCE = ipset-$(IPSET_VERSION).tar.bz2
 IPSET_SITE = http://ipset.netfilter.org
 IPSET_DEPENDENCIES = libmnl host-pkgconf
 IPSET_CONF_OPTS = --with-kmod=no
-# For 0001-Do-not-pass-Werror-when-enable-debug-is-used.patch
-IPSET_AUTORECONF = YES
 IPSET_LICENSE = GPLv2
 IPSET_LICENSE_FILES = COPYING
 IPSET_INSTALL_STAGING = YES
