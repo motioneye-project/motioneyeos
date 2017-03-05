bd73c9966dad344bc3958f78877bbb9994407794
Baruch Siach <baruch@tkos.co.il>
2017-03-05 21:33:37 +0200

    ipset: bump to version 6.31
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ipset/ipset.mk
+++ package/ipset/ipset.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IPSET_VERSION = 6.30
+IPSET_VERSION = 6.31
 IPSET_SOURCE = ipset-$(IPSET_VERSION).tar.bz2
 IPSET_SITE = http://ipset.netfilter.org
 IPSET_DEPENDENCIES = libmnl host-pkgconf
