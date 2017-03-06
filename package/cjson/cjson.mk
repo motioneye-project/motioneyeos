81f55e43094eed1000fa836db7a36151e89356cf
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2017-03-06 19:06:47 +0100

    cjson: bump to version 1.4.0
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/cjson/cjson.mk
+++ package/cjson/cjson.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CJSON_VERSION = v1.3.2
+CJSON_VERSION = v1.4.0
 CJSON_SITE = $(call github,DaveGamble,cjson,$(CJSON_VERSION))
 CJSON_INSTALL_STAGING = YES
 CJSON_LICENSE = MIT
