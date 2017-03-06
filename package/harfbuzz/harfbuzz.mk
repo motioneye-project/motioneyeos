7bbdd9afa4fa1014fdb050c1437d6cd90ca3a44a
Gustavo Zacarias <gustavo@zacarias.com.ar>
2017-03-06 11:54:49 -0300

    harfbuzz: security bump to version 1.4.4
    Fixes a buffer-overrun in Bengali.
    Switch to https URL to avoid a small delay in protocol redirection.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/harfbuzz/harfbuzz.mk
+++ package/harfbuzz/harfbuzz.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-HARFBUZZ_VERSION = 1.4.2
-HARFBUZZ_SITE = http://www.freedesktop.org/software/harfbuzz/release
+HARFBUZZ_VERSION = 1.4.4
+HARFBUZZ_SITE = https://www.freedesktop.org/software/harfbuzz/release
 HARFBUZZ_SOURCE = harfbuzz-$(HARFBUZZ_VERSION).tar.bz2
 HARFBUZZ_LICENSE = MIT, ISC (ucdn library)
 HARFBUZZ_LICENSE_FILES = COPYING src/hb-ucdn/COPYING
