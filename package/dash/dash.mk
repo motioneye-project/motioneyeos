0935781891e64c555664d20d6c3fabf83d27424c
Yann E. MORIN <yann.morin.1998@free.fr>
2017-03-07 18:37:16 +0100

    package/dash: fix parallel build
    Fixes:
        http://autobuild.buildroot.org/results/14e/14e35f399bb169d00a6e5d76fc981f87980e970d/
        http://autobuild.buildroot.org/results/b04/b043f94c445eefd5781e3ddf699e39cf88fcaa08/
        http://autobuild.buildroot.org/results/2f8/2f81182eb5733c9f4e23b380df5093978ea465ac/
        ...
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/dash/dash.mk
+++ package/dash/dash.mk
@@ -11,6 +11,9 @@ DASH_PATCH = dash_$(DASH_VERSION)-1.diff.gz
 DASH_LICENSE = BSD-3c, GPLv2+ (mksignames.c)
 DASH_LICENSE_FILES = COPYING
 
+# 0002-fix-parallel-build.patch
+DASH_AUTORECONF = YES
+
 define DASH_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/src/dash $(TARGET_DIR)/bin/dash
 endef
