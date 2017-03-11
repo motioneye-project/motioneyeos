660651491ea2ff8fc82d503a8e28bbd54e8d9131
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-03-11 17:32:59 +0100

    package/tor: bump version to 0.2.9.10
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/tor/tor.mk
+++ package/tor/tor.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TOR_VERSION = 0.2.9.9
+TOR_VERSION = 0.2.9.10
 TOR_SITE = https://dist.torproject.org
 TOR_LICENSE = BSD-3c
 TOR_LICENSE_FILES = LICENSE
