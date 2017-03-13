71c71f5f38b3141cde36fb381440d1d2a41febf1
Fabio Estevam <festevam@gmail.com>
2017-03-13 13:26:21 -0300

    kmscube: bump version to fix build failures
    Bump kmscube version to fix build failures such as this one seen on sparc64:
    http://autobuild.buildroot.net/results/d7e/d7e82c67e0b04b0aea990bfb19dd6e4fd914bebe/build-end.log
    
    ,and also the one reported by Gary Bisson with Linaro 2017.02 ARM
    gnueabihf toolchain.
    
    Signed-off-by: Fabio Estevam <festevam@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/kmscube/kmscube.mk
+++ package/kmscube/kmscube.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KMSCUBE_VERSION = 87e3ff5683ee54228b3e6e75f7d4de83901fadb0
+KMSCUBE_VERSION = db8c6fc0f625e3a3d36731363276c459661d4149
 KMSCUBE_SITE = https://cgit.freedesktop.org/mesa/kmscube/snapshot
 KMSCUBE_LICENSE = MIT
 KMSCUBE_DEPENDENCIES = host-pkgconf mesa3d libdrm
