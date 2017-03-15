467b38892aa23927ad1140119f0f17c23e896e7e
Peter Korsgaard <peter@korsgaard.com>
2017-03-15 11:48:32 +0100

    mariadb: security bump to version 10.1.22
    Fixes:
    
    CVE-2017-3302 - C client library for MySQL (libmysqlclient.so) has
    use-after-free defect which can cause crash of applications using that MySQL
    client.
    
    CVE-2017-3313 - Difficult to exploit vulnerability allows low privileged
    attacker with logon to the infrastructure where MySQL Server executes to
    compromise MySQL Server.  Successful attacks of this vulnerability can
    result in unauthorized access to critical data or complete access to all
    MySQL Server accessible data.
    
    And a number of important, but non-security related fixes:
    
    MDEV-11842: Fix a 10.1.21 regression with failed INSERT, BEFORE INSERT
    triggers, and columns with no default value
    
    MDEV-12075: Fix a 10.1.21 regression in the InnoDB data file extension code
    
    For details, see the release notes:
    https://mariadb.com/kb/en/mariadb/mariadb-10122-release-notes/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mariadb/mariadb.mk
+++ package/mariadb/mariadb.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MARIADB_VERSION = 10.1.21
+MARIADB_VERSION = 10.1.22
 MARIADB_SITE = https://downloads.mariadb.org/interstitial/mariadb-$(MARIADB_VERSION)/source
 MARIADB_LICENSE = GPLv2 (server), GPLv2 with FLOSS exception (GPL client library), LGPLv2 (LGPL client library)
 MARIADB_LICENSE_FILES = README COPYING COPYING.LESSER
