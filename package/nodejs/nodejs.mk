cae760c5e46e229690691f08c2358bea8c82216c
Martin Bark <martin@barkynet.com>
2017-03-16 10:28:30 +0000

    package/nodejs: bump version to 7.7.3
    See https://nodejs.org/en/blog/release/v7.7.3/
    
    Signed-off-by: Martin Bark <martin@barkynet.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/nodejs/nodejs.mk
+++ package/nodejs/nodejs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NODEJS_VERSION = 7.7.2
+NODEJS_VERSION = 7.7.3
 NODEJS_SOURCE = node-v$(NODEJS_VERSION).tar.xz
 NODEJS_SITE = http://nodejs.org/dist/v$(NODEJS_VERSION)
 NODEJS_DEPENDENCIES = host-python host-nodejs zlib \
