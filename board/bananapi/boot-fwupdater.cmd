4beac8e8ceaa2810c70f7c4d3c29bdecf63f1901
Calin Crisan <ccrisan@gmail.com>
2017-03-16 22:32:20 +0200

    bananapi: initial work on fwupdate support

--- board/bananapi/boot-fwupdater.cmd
+++ board/bananapi/boot-fwupdater.cmd
@@ -3,5 +3,6 @@ setenv fdt_high ffffffff
 setenv video-mode sunxi:1024x768-24@60,monitor=hdmi,hpd=1,edid=1
 fatload mmc 0 0x43000000 sun7i-a20-bananapi.dtb
 fatload mmc 0 0x41000000 uImage
-bootm 0x43000000 - 0x45000000
+fatload mmc 0 0x45000000 uInitrd
+bootm 0x43000000 0x41000000 0x45000000
 
