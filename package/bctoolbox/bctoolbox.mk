d10b493e489da81e7867400f03c5111f7ecca172
Gustavo Zacarias <gustavo@zacarias.com.ar>
2017-03-16 10:04:34 -0300

    polarssl: remove unmaintained package
    The 1.2.x branch is no longer maintained, so remove it since it's
    likely security-vulnerable.
    
    mbedtls is the modern replacement which was renamed from polarssl when
    ARM bought them up.
    However major releases broke API so polarssl 1.2.x isn't always
    interchangeable with polarssl/mbedtls 1.3.x (interim mixed naming
    because of new ownership) or newer 2.x series.
    
    Fortunately we don't have any package in the tree that uses polarssl
    exclusively.
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    [Thomas:
     - Remove entry in DEVELOPERS file for this package. Noticed by Arnout.
     - Remove comment in bctoolbox.mk that no longer makes sense after
       polarssl removal. Noticed by Arnout.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/bctoolbox/bctoolbox.mk
+++ package/bctoolbox/bctoolbox.mk
@@ -13,26 +13,18 @@ BCTOOLBOX_INSTALL_STAGING = YES
 # Set CMAKE_SKIP_RPATH to prevent bctoolbox from adding the rpath to
 # shared library.
 BCTOOLBOX_CONF_OPTS = \
+	-DENABLE_POLARSSL=OFF \
 	-DENABLE_STRICT=OFF \
 	-DENABLE_TESTS_COMPONENT=OFF \
 	-DENABLE_TESTS=OFF \
 	-DGIT_EXECUTABLE=OFF \
 	-DCMAKE_SKIP_RPATH=ON
 
-# bctoolbox can be build with mbedTLS or PolarSSL support. If both
-# libraries are present, mbedTLS is preferred over PolarSSL.
 ifeq ($(BR2_PACKAGE_MBEDTLS),y)
 BCTOOLBOX_DEPENDENCIES += mbedtls
 BCTOOLBOX_CONF_OPTS += -DENABLE_MBEDTLS=ON
 else
 BCTOOLBOX_CONF_OPTS += -DENABLE_MBEDTLS=OFF
-endif
-
-ifeq ($(BR2_PACKAGE_POLARSSL),y)
-BCTOOLBOX_DEPENDENCIES += polarssl
-BCTOOLBOX_CONF_OPTS += -DENABLE_POLARSSL=ON
-else
-BCTOOLBOX_CONF_OPTS += -DENABLE_POLARSSL=OFF
 endif
 
 ifeq ($(BR2_STATIC_LIBS),y)
