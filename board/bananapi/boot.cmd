2e7c4ff0745a22ee0a81209ccfb521c09975b27a
Calin Crisan <ccrisan@gmail.com>
2017-03-17 20:27:32 +0200

    bananapi: add initramfs fwupdate support

--- board/bananapi/boot.cmd
+++ board/bananapi/boot.cmd
@@ -1,7 +1,7 @@
 setenv bootargs console=tty1 root=/dev/mmcblk0p2 rootwait panic=10 earlyprintk quiet loglevel=1 ipv6.disable=1 ${extra}
 setenv fdt_high ffffffff
 setenv video-mode sunxi:1024x768-24@60,monitor=hdmi,hpd=1,edid=1
-fatload mmc 0 0x43000000 sun7i-a20-bananapi.dtb
 fatload mmc 0 0x41000000 uImage
-bootm 0x43000000 - 0x45000000
+fatload mmc 0 0x43000000 sun7i-a20-bananapi.dtb
+bootm 0x41000000 - 0x43000000
 
