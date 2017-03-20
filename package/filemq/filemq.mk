c72bc729fb01442233f733ef40605fec8634e530
Rahul Bedarkar <rahul.bedarkar@imgtec.com>
2017-03-20 20:41:19 +0530

    filemq: bump version to 3dc89932903b0320853d87833aabc6f7e7781c49
    This version bump removes (L)GPLv3 license text which were leftover
    from previous implementation.
    
    Signed-off-by: Rahul Bedarkar <rahul.bedarkar@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/filemq/filemq.mk
+++ package/filemq/filemq.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FILEMQ_VERSION = e59951489045825d6fc5bdc6a5a5ecf1abf51943
+FILEMQ_VERSION = 3dc89932903b0320853d87833aabc6f7e7781c49
 FILEMQ_SITE = $(call github,zeromq,filemq,$(FILEMQ_VERSION))
 
 FILEMQ_AUTORECONF = YES
