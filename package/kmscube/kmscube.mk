1a602f3c2ee7acc95bea082e2fdcddb926b032e8
Fabio Estevam <fabio.estevam@nxp.com>
2017-03-21 13:48:22 -0300

    kmscube: Bump to latest version to fix mips64el errors
    Building kmscube for mips64el leads to the following build error:
    
    cube-tex.c: In function 'get_fd_rgba':
    cube-tex.c:230:2: error: 'for' loop initial declarations are only allowed in C99 mode
      for (int i = 0; i < texh; i++) {
      ^
    cube-tex.c:230:2: note: use option -std=c99 or -std=gnu99 to compile your code
    cube-tex.c: In function 'get_fd_y':
    cube-tex.c:261:2: error: 'for' loop initial declarations are only allowed in C99 mode
      for (int i = 0; i < texh; i++) {
      ^
    cube-tex.c: In function 'get_fd_uv':
    cube-tex.c:292:2: error: 'for' loop initial declarations are only allowed in C99 mode
      for (int i = 0; i < texh/2; i++) {
    
    I have sent a fix to mainline kmscube to address such issue, so bump to
    the latest version.
    
    Fixes: http://autobuild.buildroot.net/results/1f2/1f2ad0536254834582cf7fbce6a38dbb46a5e249/build-end.log
    
    Signed-off-by: Fabio Estevam <fabio.estevam@nxp.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/kmscube/kmscube.mk
+++ package/kmscube/kmscube.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KMSCUBE_VERSION = db8c6fc0f625e3a3d36731363276c459661d4149
+KMSCUBE_VERSION = 6cbd03ab94066dddbba7bedfde87c7c4319c18d5
 KMSCUBE_SITE = https://cgit.freedesktop.org/mesa/kmscube/snapshot
 KMSCUBE_LICENSE = MIT
 KMSCUBE_DEPENDENCIES = host-pkgconf mesa3d libdrm
