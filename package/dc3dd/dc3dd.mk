bf2f6f49ff106a80dce8c973f4869a0c7947d298
Benoît Allard <benoit.allard@greenbone.net>
2017-03-27 10:32:10 +0200

    dc3dd: new package
    We actually prefer the version 7.2.641 (over 7.2.646) as the
    content of the release is identical and the 641 has the advantage
    of providing a proper .tar.xz file.
    
    Signed-off-by: Benoît Allard <benoit.allard@greenbone.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/dc3dd/dc3dd.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# dc3dd
+#
+################################################################################
+
+DC3DD_VERSION_MAJOR = 7.2
+DC3DD_VERSION = $(DC3DD_VERSION_MAJOR).641
+DC3DD_SOURCE = dc3dd-$(DC3DD_VERSION).tar.xz
+DC3DD_SITE = https://downloads.sourceforge.net/project/dc3dd/dc3dd/$(DC3DD_VERSION_MAJOR)
+DC3DD_LICENSE = GPLv3+
+DC3DD_LICENSE_FILES = COPYING
+# We are patching the Makefile.am
+DC3DD_AUTORECONF = yes
+
+$(eval $(autotools-package))
