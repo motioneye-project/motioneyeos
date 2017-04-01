b06d055c113b2aca30163ea6961631c9817d090a
Rahul Bedarkar <rahulbedarkar89@gmail.com>
2017-04-02 00:36:23 +0530

    package: use SPDX short identifier for OFL license
    We want to use SPDX identifier for license string as much as possible.
    SPDX short identifier for OFLv1.0/OFLv1.1 is OFL-1.0/OFL-1.1.
    
    This change is done using following command.
    find . -name "*.mk" | xargs sed -ri '/LICENSE( )?[\+:]?=/s/OFLv([1]\.[0-1])/OFL-\1/g'
    
    Signed-off-by: Rahul Bedarkar <rahulbedarkar89@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/font-awesome/font-awesome.mk
+++ package/font-awesome/font-awesome.mk
@@ -6,7 +6,7 @@
 
 FONT_AWESOME_VERSION = v4.5.0
 FONT_AWESOME_SITE = $(call github,FortAwesome,Font-Awesome,$(FONT_AWESOME_VERSION))
-FONT_AWESOME_LICENSE = OFLv1.1 (font), MIT (CSS, LESS and Sass files)
+FONT_AWESOME_LICENSE = OFL-1.1 (font), MIT (CSS, LESS and Sass files)
 FONT_AWESOME_DIRECTORIES_LIST = css fonts less scss
 
 define FONT_AWESOME_INSTALL_TARGET_CMDS
