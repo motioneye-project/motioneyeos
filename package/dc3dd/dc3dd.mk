09c76342487c3d73abc771f71cbb216cc7252d9e
Rodrigo Rebello <rprebello@gmail.com>
2017-04-02 22:42:22 -0300

    dc3dd: gettextize to match newer gettext version
    Because dc3dd is being autoreconfigured and comes with an old gettext
    infra, gettextize needs to be called so that the infra is updated to
    match the newer version used in Buildroot.
    
    Commit b36d57fab included a patch to add the definition of MKDIR_P to
    po/Makefile.in.in in order to fix autobuild failures that ocurred when
    host-gettext was built before dc3dd. This patch is no longer necessary
    as gettextize adds a new Makefile template which contains the needed
    definition, so drop it.
    
    Signed-off-by: Rodrigo Rebello <rprebello@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/dc3dd/dc3dd.mk
+++ package/dc3dd/dc3dd.mk
@@ -10,7 +10,9 @@ DC3DD_SOURCE = dc3dd-$(DC3DD_VERSION).tar.xz
 DC3DD_SITE = https://downloads.sourceforge.net/project/dc3dd/dc3dd/$(DC3DD_VERSION_MAJOR)
 DC3DD_LICENSE = GPL-3.0+
 DC3DD_LICENSE_FILES = COPYING
-# We are patching the Makefile.am
+# We are patching Makefile.am, so we need to autoreconf. We also need to
+# enable gettextize as dc3dd comes with an old gettext infra.
 DC3DD_AUTORECONF = YES
+DC3DD_GETTEXTIZE = YES
 
 $(eval $(autotools-package))
