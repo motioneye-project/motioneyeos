d2ea2479e5061c9d48b7ee544271828563e83da8
Peter Korsgaard <peter@korsgaard.com>
2017-04-07 16:28:59 +0200

    icu: legal-info: refer to the real license file
    License.html now just contains the string:
    
    The ICU license is now in plain text format, see <a href="./LICENSE">LICENSE</a>.
    Update links and software appropriately.
    
    So refer directly to that file instead.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/icu/icu.mk
+++ package/icu/icu.mk
@@ -8,7 +8,7 @@ ICU_VERSION = 58.2
 ICU_SOURCE = icu4c-$(subst .,_,$(ICU_VERSION))-src.tgz
 ICU_SITE = http://download.icu-project.org/files/icu4c/$(ICU_VERSION)
 ICU_LICENSE = ICU License
-ICU_LICENSE_FILES = license.html
+ICU_LICENSE_FILES = LICENSE
 
 ICU_DEPENDENCIES = host-icu
 ICU_INSTALL_STAGING = YES
