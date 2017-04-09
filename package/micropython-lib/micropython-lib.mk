665f17150420b037ea34b819a3a09f4cd15d5721
Rahul Bedarkar <rahulbedarkar89@gmail.com>
2017-04-09 14:04:12 +0530

    micropython-lib: use SPDX short identifier for Python license
    Signed-off-by: Rahul Bedarkar <rahulbedarkar89@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/micropython-lib/micropython-lib.mk
+++ package/micropython-lib/micropython-lib.mk
@@ -6,7 +6,7 @@
 
 MICROPYTHON_LIB_VERSION = v1.8.6
 MICROPYTHON_LIB_SITE = $(call github,micropython,micropython-lib,$(MICROPYTHON_LIB_VERSION))
-MICROPYTHON_LIB_LICENSE = Python software foundation license v2 (some modules), MIT (everything else)
+MICROPYTHON_LIB_LICENSE = Python-2.0 (some modules), MIT (everything else)
 MICROPYTHON_LIB_LICENSE_FILES = LICENSE
 
 define MICROPYTHON_LIB_INSTALL_TARGET_CMDS
