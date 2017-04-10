35db2ad48e7d49461ce401d838afb69ecb921307
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-04-10 12:08:52 +0100

    tcpreplay: bump version to 4.2.2
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/tcpreplay/tcpreplay.mk
+++ package/tcpreplay/tcpreplay.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TCPREPLAY_VERSION = 4.2.1
+TCPREPLAY_VERSION = 4.2.2
 TCPREPLAY_SITE = https://github.com/appneta/tcpreplay/releases/download/v$(TCPREPLAY_VERSION)
 TCPREPLAY_LICENSE = GPL-3.0
 TCPREPLAY_LICENSE_FILES = docs/LICENSE
