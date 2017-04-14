e9b95f0cd02bd7f2dafc26d658c137554249bf75
Ricardo Martincoski <ricardo.martincoski@gmail.com>
2017-04-13 21:22:25 -0300

    atop: bump version to 2.3.0
    Remove patch applied upstream [1].
    
    [1] https://github.com/Atoptool/atop/commit/a2306c0e1c83f5a123ce521a34d9efaa2297c26d
    
    Signed-off-by: Ricardo Martincoski <ricardo.martincoski@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/atop/atop.mk
+++ package/atop/atop.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ATOP_VERSION = 2.2-3
+ATOP_VERSION = 2.3.0
 ATOP_SITE = http://www.atoptool.nl/download
 ATOP_LICENSE = GPL-2.0+
 ATOP_LICENSE_FILES = COPYING
