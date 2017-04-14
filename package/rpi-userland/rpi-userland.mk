f135942f2c0424aaa6eee00b8f75efb776153746
Martin Bark <martin@barkynet.com>
2017-04-14 16:32:25 +0100

    package/rpi-userland: bump version
    Signed-off-by: Martin Bark <martin@barkynet.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_USERLAND_VERSION = f410ec051a3724934e3671315474d4b81424b0ef
+RPI_USERLAND_VERSION = f0642e3b58d8a140a3f7621630c15fbfa794b19d
 RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
 RPI_USERLAND_LICENSE = BSD-3-Clause
 RPI_USERLAND_LICENSE_FILES = LICENCE
