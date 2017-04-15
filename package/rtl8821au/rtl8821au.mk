b207e010d87e3c9612b18392d54b680185143ae8
Christian Stewart <christian@paral.in>
2017-04-15 13:33:20 -0400

    rtl8821au: bump to latest
    We add -Wno-error to not error out on warnings, which would otherwise
    cause build failures.
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    [Thomas: as suggested by Baruch, use -Wno-error instead of disabling
    specific warnings.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/rtl8821au/rtl8821au.mk
+++ package/rtl8821au/rtl8821au.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8821AU_VERSION = c33ddb05a77741d2a9c9b974ad0cf0fa26d17b6e
+RTL8821AU_VERSION = b018b446fb0729fa659e7eead475d6c13f192642
 RTL8821AU_SITE = $(call github,abperiasamy,rtl8812AU_8821AU_linux,$(RTL8821AU_VERSION))
 RTL8821AU_LICENSE = GPL-2.0
 RTL8821AU_LICENSE_FILES = COPYING
@@ -12,7 +12,8 @@ RTL8821AU_LICENSE_FILES = COPYING
 RTL8821AU_MODULE_MAKE_OPTS = \
 	CONFIG_RTL8812AU_8821AU=m \
 	KVER=$(LINUX_VERSION_PROBED) \
-	USER_EXTRA_CFLAGS=-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN
+	USER_EXTRA_CFLAGS="-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN \
+		-Wno-error"
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
