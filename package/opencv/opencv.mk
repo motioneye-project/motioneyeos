b4da02a8192e72180cf9ceedc39fc1df19e3ab46
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-04-19 12:11:41 +0200

    package/opencv: bump version to 2.4.13.2
    Removed patch applied upstream:
    https://github.com/opencv/opencv/commit/c7045184ce220a04796d8e9072ac42d2d362dc71
    
    Renumbered remaining patches.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/opencv/opencv.mk
+++ package/opencv/opencv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENCV_VERSION = 2.4.13
+OPENCV_VERSION = 2.4.13.2
 OPENCV_SITE = $(call github,itseez,opencv,$(OPENCV_VERSION))
 OPENCV_INSTALL_STAGING = YES
 OPENCV_LICENSE = BSD-3-Clause
