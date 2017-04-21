1872903d868ccce3a29a906807fa17570f238bb0
Mario J. Rugiero <mrugiero@gmail.com>
2017-04-21 13:06:37 -0300

    package{protobuf,python-protobuf}: bump to v3.2.0
    Both packages are coupled, so both are bumped and build-tested.
    
    The atomics' support patch is no longer needed, and neither is the
    autoreconf option, and SPARC64 is no longer broken.
    
    To make sure of this, one config of each of the following archs was
    tested (base defconfig in parens):
    
     - PowerPC (qemu_ppc_g3beige_defconfig)
     - SPARC (qemu_sparc_ss10_defconfig)
     - SPARC64 (qemu_sparc64_sun4u_defconfig)
    
    Signed-off-by: Mario J. Rugiero <mrugiero@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/protobuf/protobuf.mk
+++ package/protobuf/protobuf.mk
@@ -7,12 +7,11 @@
 # When bumping this package, make sure to also verify if the
 # python-protobuf package still works, as they share the same
 # version/site variables.
-PROTOBUF_VERSION = v3.0.0
-PROTOBUF_SITE = $(call github,google,protobuf,$(PROTOBUF_VERSION))
+PROTOBUF_VERSION = 3.2.0
+PROTOBUF_SOURCE = protobuf-cpp-$(PROTOBUF_VERSION).tar.gz
+PROTOBUF_SITE = https://github.com/google/protobuf/releases/download/v$(PROTOBUF_VERSION)
 PROTOBUF_LICENSE = BSD-3-Clause
 PROTOBUF_LICENSE_FILES = LICENSE
-# no configure script
-PROTOBUF_AUTORECONF = YES
 
 # N.B. Need to use host protoc during cross compilation.
 PROTOBUF_DEPENDENCIES = host-protobuf
