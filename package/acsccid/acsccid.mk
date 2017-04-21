ff75225568eea04a92e8cf5173be59635dc3e3dd
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-04-21 15:51:42 +0200

    package/acsccid: bump version to 1.1.4
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/acsccid/acsccid.mk
+++ package/acsccid/acsccid.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ACSCCID_VERSION = 1.1.1
+ACSCCID_VERSION = 1.1.4
 ACSCCID_SOURCE = acsccid-$(ACSCCID_VERSION).tar.bz2
 ACSCCID_SITE = http://downloads.sourceforge.net/acsccid
 ACSCCID_LICENSE = LGPL-2.1+
