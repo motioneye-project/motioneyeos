d9c7723da45557abd0375d9d9edbdd0e9257b29e
Francois Perrad <fperrad@gmail.com>
2017-04-22 08:34:06 +0200

    perl-gd: bump to version 2.59
    remove patches merged upstream
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    [Thomas: renumber patches.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/perl-gd/perl-gd.mk
+++ package/perl-gd/perl-gd.mk
@@ -4,18 +4,12 @@
 #
 ################################################################################
 
-PERL_GD_VERSION = 2.56
+PERL_GD_VERSION = 2.59
 PERL_GD_SOURCE = GD-$(PERL_GD_VERSION).tar.gz
-PERL_GD_SITE = $(BR2_CPAN_MIRROR)/authors/id/L/LD/LDS
+PERL_GD_SITE = $(BR2_CPAN_MIRROR)/authors/id/R/RU/RURBAN
 PERL_GD_DEPENDENCIES = zlib libpng freetype gd
 PERL_GD_LICENSE = Artistic or GPL-1.0+
 PERL_GD_LICENSE_FILES = LICENSE
-PERL_GD_PREFER_INSTALLER = EUMM
-
-define PERL_GD_MOVE_XS
-	mv $(@D)/lib/GD.xs $(@D)/GD.xs
-endef
-PERL_GD_POST_PATCH_HOOKS += PERL_GD_MOVE_XS
 
 PERL_GD_CONF_OPTS = \
 	-options=FT,PNG \
