8569e78de682affea612daf628c661e6a48c0716
Yann E. MORIN <yann.morin.1998@free.fr>
2017-04-23 22:30:52 +0200

    package/qt5base: always use the opensource license
    We never ask the user to confirm their assent about the use of a free
    license; there is no reason we do so for Qt.
    
    The output of legal-info is there to gather all the licenses used in a
    build; the license for qt5base will be there to be consulted as well.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Julien Corjon <corjon.j@ecagroup.com>
    Cc: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/qt5/qt5base/qt5base.mk
+++ package/qt5/qt5base/qt5base.mk
@@ -47,7 +47,6 @@ ifeq ($(BR2_PACKAGE_QT5_VERSION_5_6),y)
 QT5BASE_CONFIGURE_OPTS += -largefile
 endif
 
-ifeq ($(BR2_PACKAGE_QT5BASE_LICENSE_APPROVED),y)
 QT5BASE_CONFIGURE_OPTS += -opensource -confirm-license
 ifeq ($(BR2_PACKAGE_QT5_VERSION_LATEST),y)
 QT5BASE_LICENSE = GPL-2.0+ or LGPL-3.0, GPL-3.0 with exception(tools), GFDL-1.3 (docs)
@@ -59,10 +58,6 @@ endif
 ifeq ($(BR2_PACKAGE_QT5BASE_EXAMPLES),y)
 QT5BASE_LICENSE := $(QT5BASE_LICENSE), BSD-3-Clause (examples)
 QT5BASE_LICENSE_FILES += header.BSD
-endif
-else
-QT5BASE_LICENSE = Commercial license
-QT5BASE_REDISTRIBUTE = NO
 endif
 
 QT5BASE_CONFIG_FILE = $(call qstrip,$(BR2_PACKAGE_QT5BASE_CONFIG_FILE))
