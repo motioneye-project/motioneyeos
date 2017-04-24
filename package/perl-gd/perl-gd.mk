bea0cfc5962f60ecae90f092c1f31f106d06697b
Francois Perrad <fperrad@gmail.com>
2017-04-24 09:15:43 +0200

    perl-gd: bump to version 2.66
    remove last patch
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/perl-gd/perl-gd.mk
+++ package/perl-gd/perl-gd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_GD_VERSION = 2.59
+PERL_GD_VERSION = 2.66
 PERL_GD_SOURCE = GD-$(PERL_GD_VERSION).tar.gz
 PERL_GD_SITE = $(BR2_CPAN_MIRROR)/authors/id/R/RU/RURBAN
 PERL_GD_DEPENDENCIES = zlib libpng freetype gd
@@ -12,12 +12,10 @@ PERL_GD_LICENSE = Artistic or GPL-1.0+
 PERL_GD_LICENSE_FILES = LICENSE
 
 PERL_GD_CONF_OPTS = \
-	-options=FT,PNG \
 	-lib_gd_path=$(STAGING_DIR)/usr \
 	-lib_ft_path=$(STAGING_DIR)/usr \
 	-lib_png_path=$(STAGING_DIR)/usr \
 	-lib_zlib_path=$(STAGING_DIR)/usr \
-	-ignore_missing_gd \
-	-gdlib=$(STAGING_DIR)/usr/bin/gdlib-config
+	-gdlib_config_path=$(STAGING_DIR)/usr/bin
 
 $(eval $(perl-package))
