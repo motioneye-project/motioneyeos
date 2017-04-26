c363e070d8ee036052fbcadd153d8c39ce0db55b
Peter Korsgaard <peter@korsgaard.com>
2017-04-26 13:52:14 +0200

    libsndfile: security bump to version 1.0.28
    Fixes:
    
    CVE-2017-7585 - In libsndfile before 1.0.28, an error in the
    "flac_buffer_copy()" function (flac.c) can be exploited to cause a
    stack-based buffer overflow via a specially crafted FLAC file.
    
    CVE-2017-7586 - In libsndfile before 1.0.28, an error in the "header_read()"
    function (common.c) when handling ID3 tags can be exploited to cause a
    stack-based buffer overflow via a specially crafted FLAC file.
    
    CVE-2017-7741 - In libsndfile before 1.0.28, an error in the
    "flac_buffer_copy()" function (flac.c) can be exploited to cause a
    segmentation violation (with write memory access) via a specially crafted
    FLAC file during a resample attempt, a similar issue to CVE-2017-7585.
    
    CVE-2017-7742 - In libsndfile before 1.0.28, an error in the
    "flac_buffer_copy()" function (flac.c) can be exploited to cause a
    segmentation violation (with read memory access) via a specially crafted
    FLAC file during a resample attempt, a similar issue to CVE-2017-7585.
    
    Dop undocumented patch adjusting SUBDIRS in Makefile.in as it no longer
    applies.  Instead pass --disable-full-suite to disable man pages,
    documentation and programs, as that was presumably the reason for the patch.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libsndfile/libsndfile.mk
+++ package/libsndfile/libsndfile.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSNDFILE_VERSION = 1.0.27
+LIBSNDFILE_VERSION = 1.0.28
 LIBSNDFILE_SITE = http://www.mega-nerd.com/libsndfile/files
 LIBSNDFILE_INSTALL_STAGING = YES
 LIBSNDFILE_LICENSE = LGPL-2.1+
@@ -13,6 +13,7 @@ LIBSNDFILE_LICENSE_FILES = COPYING
 LIBSNDFILE_CONF_OPTS = \
 	--disable-sqlite \
 	--disable-alsa \
-	--disable-external-libs
+	--disable-external-libs \
+	--disable-full-suite
 
 $(eval $(autotools-package))
