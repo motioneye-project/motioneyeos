3046f64c6a341165a3108900fc978579af6959f3
Steve Kenton <skenton@ou.edu>
2017-04-27 05:29:21 +0000

    tovid: bump version to 0.35.2
    Signed-off-by: Steve Kenton <skenton@ou.edu>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tovid/tovid.mk
+++ package/tovid/tovid.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-TOVID_VERSION = 0.35.0
-TOVID_SITE = https://github.com/tovid-suite/tovid/releases/download/v$(TOVID_VERSION)
+TOVID_VERSION = 0.35.2
+TOVID_SITE = https://github.com/tovid-suite/tovid/releases/download/$(TOVID_VERSION)
 TOVID_LICENSE = GPL-2.0+
 TOVID_LICENSE_FILES = COPYING
 TOVID_SETUP_TYPE = distutils
