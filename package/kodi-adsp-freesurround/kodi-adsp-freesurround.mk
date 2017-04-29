2a5cf81c05c8cc37641092b6ac58623f3dabbb9a
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-04-29 10:37:33 +0200

    package/kodi-adsp-freesurround: bump version
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/kodi-adsp-freesurround/kodi-adsp-freesurround.mk
+++ package/kodi-adsp-freesurround/kodi-adsp-freesurround.mk
@@ -4,7 +4,9 @@
 #
 ################################################################################
 
-KODI_ADSP_FREESURROUND_VERSION = 08b691d3d9a0382d2f6f789a31614fb02512036c
+# This cset is on the branch 'Krypton'
+# When Kodi is updated, then this should be updated to the corresponding branch
+KODI_ADSP_FREESURROUND_VERSION = 34f50406bcba22e364711562e5b9205e57ae844b
 KODI_ADSP_FREESURROUND_SITE = $(call github,kodi-adsp,adsp.freesurround,$(KODI_ADSP_FREESURROUND_VERSION))
 KODI_ADSP_FREESURROUND_LICENSE = GPL-3.0+
 KODI_ADSP_FREESURROUND_LICENSE_FILES = LICENSE.md
