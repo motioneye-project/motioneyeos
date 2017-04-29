b3d8ac2aa770e14da5a48901459e8ce38ec7fbc2
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-04-29 11:04:49 +0200

    package/libsodium: bump version to 1.0.12
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libsodium/libsodium.mk
+++ package/libsodium/libsodium.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSODIUM_VERSION = 1.0.11
+LIBSODIUM_VERSION = 1.0.12
 LIBSODIUM_SITE = https://download.libsodium.org/libsodium/releases
 LIBSODIUM_LICENSE = ISC
 LIBSODIUM_LICENSE_FILES = LICENSE
