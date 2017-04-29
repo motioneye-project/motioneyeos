1dd318805e500dde7c556dea0d1009d58cde1de1
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-04-29 10:37:26 +0200

    package/kodi-jsonschemabuilder: new host package
    Needed for upcoming kodi version bump to 17.1-Krypton which will also
    switch the kodi build system to CMake.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    [Thomas: do not add texturepacker patch in this commit, use SPDX license
    code.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/kodi-jsonschemabuilder/kodi-jsonschemabuilder.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# kodi-jsonschemabuilder
+#
+################################################################################
+
+# Not possible to directly refer to kodi variables, because of
+# first/second expansion trickery...
+KODI_JSONSCHEMABUILDER_VERSION = 17.1-Krypton
+KODI_JSONSCHEMABUILDER_SITE = $(call github,xbmc,xbmc,$(KODI_JSONSCHEMABUILDER_VERSION))
+KODI_JSONSCHEMABUILDER_SOURCE = kodi-$(KODI_JSONSCHEMABUILDER_VERSION).tar.gz
+KODI_JSONSCHEMABUILDER_LICENSE = GPL-2.0
+KODI_JSONSCHEMABUILDER_LICENSE_FILES = LICENSE.GPL
+HOST_KODI_JSONSCHEMABUILDER_SUBDIR = tools/depends/native/JsonSchemaBuilder
+
+HOST_KODI_JSONSCHEMABUILDER_CONF_OPTS = \
+	-DCMAKE_MODULE_PATH=$(@D)/project/cmake/modules
+
+define HOST_KODI_JSONSCHEMABUILDER_INSTALL_CMDS
+	$(INSTALL) -m 755 -D \
+		$(@D)/tools/depends/native/JsonSchemaBuilder/JsonSchemaBuilder \
+		$(HOST_DIR)/usr/bin/JsonSchemaBuilder
+endef
+
+$(eval $(host-cmake-package))
