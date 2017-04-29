12d4cc4cccf37ffb3564a695c64a2dfac5ca0f25
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-04-29 10:37:31 +0200

    package/kodi-pvr-nextpvr: bump version to 2.4.11
    Removed patch applied upstream:
    https://github.com/kodi-pvr/pvr.nextpvr/commit/9e042807f1ce18df6937312cb0abded24d09bfb7
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/kodi-pvr-nextpvr/kodi-pvr-nextpvr.mk
+++ package/kodi-pvr-nextpvr/kodi-pvr-nextpvr.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-# This cset is on the branch 'Jarvis'
+# This cset is on the branch 'Krypton'
 # When Kodi is updated, then this should be updated to the corresponding branch
-KODI_PVR_NEXTPVR_VERSION = 2055105c93e18e04e73c87578bece3edf7525e14
+KODI_PVR_NEXTPVR_VERSION = 2.4.11-Krypton
 KODI_PVR_NEXTPVR_SITE = $(call github,kodi-pvr,pvr.nextpvr,$(KODI_PVR_NEXTPVR_VERSION))
 KODI_PVR_NEXTPVR_LICENSE = GPL-2.0+
 KODI_PVR_NEXTPVR_LICENSE_FILES = src/client.h
