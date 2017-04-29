97bebe2d00141bdd6613cbfa7990767468fb55b2
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-04-29 22:36:07 +0200

    package/kodi-skin-confluence: new package
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Tested-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    [Thomas:
     - use SPDX license code
     - license is GPL-2.0, not GPL-2.0+
     - remove unused _CONF_OPTS variable]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/kodi-skin-confluence/kodi-skin-confluence.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# kodi-skin-confluence
+#
+################################################################################
+
+KODI_SKIN_CONFLUENCE_VERSION = 7044dabb3a818d7ff47b7a34bfb3480f2a9441d9
+KODI_SKIN_CONFLUENCE_SITE = $(call github,xbmc,skin.confluence,$(KODI_SKIN_CONFLUENCE_VERSION))
+KODI_SKIN_CONFLUENCE_LICENSE = GPL-2.0
+KODI_SKIN_CONFLUENCE_LICENSE_FILES = LICENSE.txt
+KODI_SKIN_CONFLUENCE_DEPENDENCIES = kodi
+
+define KODI_SKIN_CONFLUENCE_BUILD_CMDS
+	$(HOST_DIR)/usr/bin/TexturePacker -input $(@D)/media/ -output $(@D)/media/Textures.xbt -dupecheck -use_none
+endef
+
+define KODI_SKIN_CONFLUENCE_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/share/kodi/addons/skin.confluence
+	cp -dpfr $(@D)/* $(TARGET_DIR)/usr/share/kodi/addons/skin.confluence
+	find $(TARGET_DIR)/usr/share/kodi/addons/skin.confluence/media -name *.jpg -delete
+	find $(TARGET_DIR)/usr/share/kodi/addons/skin.confluence/media -name *.png -delete
+endef
+
+$(eval $(generic-package))
