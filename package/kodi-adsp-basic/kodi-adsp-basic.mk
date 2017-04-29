f4594a277c12e2cee732aaccf36e477143bc5336
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-04-29 10:37:32 +0200

    package/kodi-adsp-basic: bump version
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/kodi-adsp-basic/kodi-adsp-basic.mk
+++ package/kodi-adsp-basic/kodi-adsp-basic.mk
@@ -4,7 +4,9 @@
 #
 ################################################################################
 
-KODI_ADSP_BASIC_VERSION = cb56e0eb6530fd50a286d47ef0be529001e9d556
+# This cset is on the branch 'Krypton'
+# When Kodi is updated, then this should be updated to the corresponding branch
+KODI_ADSP_BASIC_VERSION = 2ae604e591346741324663260696cfa231931870
 KODI_ADSP_BASIC_SITE = $(call github,kodi-adsp,adsp.basic,$(KODI_ADSP_BASIC_VERSION))
 KODI_ADSP_BASIC_LICENSE = GPL-3.0+
 KODI_ADSP_BASIC_LICENSE_FILES = LICENSE.md
