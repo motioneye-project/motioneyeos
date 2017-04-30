c975631dd9c965a60b679a3b637c36f776da21b9
Romain Naour <romain.naour@gmail.com>
2017-04-30 13:14:38 +0200

    package/xenomai: update udev rules search path
    The udev rules has been moved to kernel/cobalt/udev.
    
    Fixes:
    http://autobuild.buildroot.net/results/8eb/8eb4643faed523ffccd421f20c2955adfde634a6
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/xenomai/xenomai.mk
+++ package/xenomai/xenomai.mk
@@ -146,7 +146,7 @@ XENOMAI_DEPENDENCIES += udev
 
 define XENOMAI_INSTALL_UDEV_RULES
 	if test -d $(TARGET_DIR)/etc/udev/rules.d ; then \
-		for f in $(@D)/ksrc/nucleus/udev/*.rules ; do \
+		for f in $(@D)/kernel/cobalt/udev/*.rules ; do \
 			cp $$f $(TARGET_DIR)/etc/udev/rules.d/ || exit 1 ; \
 		done ; \
 	fi;
