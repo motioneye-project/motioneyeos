920c7f470678dcc550dd0c8de20e92d9da7bb0d1
Baruch Siach <baruch@tkos.co.il>
2017-05-03 15:01:03 +0300

    empty: bump to version 0.6.20b
    Drop upstream patch.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/empty/empty.mk
+++ package/empty/empty.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EMPTY_VERSION = 0.6.19b
+EMPTY_VERSION = 0.6.20b
 EMPTY_SOURCE = empty-$(EMPTY_VERSION).tgz
 EMPTY_SITE = http://downloads.sourceforge.net/project/empty/empty/empty-$(EMPTY_VERSION)
 EMPTY_LICENSE = BSD-3-Clause
