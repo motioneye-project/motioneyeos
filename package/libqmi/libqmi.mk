4d11848a21d4424a04e8428c4cd3bd5e972aacdf
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-05-04 21:33:49 +0200

    libqmi: fix patch for canonicalize_file_name() issue
    The patch added in commit c78b65c4f161ec101ff02880788ef22eb7c87d76 was
    bogus: it adds some code conditionally on HAVE_CANONICALIZE_FILE_NAME
    being undefined, but there is nothing that defines
    HAVE_CANONICALIZE_FILE_NAME if canonicalize_file_name() is already
    provided by the C library.
    
    This commit fixes that by adding the appropriate autoconf check.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/4f0eb2af09c92ff4cd7448211ffda432754d4e60/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libqmi/libqmi.mk
+++ package/libqmi/libqmi.mk
@@ -10,6 +10,8 @@ LIBQMI_SOURCE = libqmi-$(LIBQMI_VERSION).tar.xz
 LIBQMI_LICENSE = LGPL-2.0+ (library), GPL-2.0+ (programs)
 LIBQMI_LICENSE_FILES = COPYING
 LIBQMI_INSTALL_STAGING = YES
+# 0001-musl-compat-canonicalize_file_name.patch
+LIBQMI_AUTORECONF = YES
 
 LIBQMI_DEPENDENCIES = libglib2
 
