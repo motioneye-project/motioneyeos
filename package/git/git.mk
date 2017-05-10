312d71c7a9139e3a23f0c9c284d4b36414f48623
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-05-10 12:09:08 +0100

    git: bump version to 2.13.0
    Patches 0001 and 0002 already included in this release:
      https://github.com/git/git/commit/379642bcd8d89db52feba88a651e4e56d6ac5767
      https://github.com/git/git/commit/2225e1ea20481a7c0da526891470abf9ece623e7
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/git/git.mk
+++ package/git/git.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GIT_VERSION = 2.12.2
+GIT_VERSION = 2.13.0
 GIT_SOURCE = git-$(GIT_VERSION).tar.xz
 GIT_SITE = https://www.kernel.org/pub/software/scm/git
 GIT_LICENSE = GPL-2.0, LGPL-2.1+
