6a72aea6161b8deb1779af090f36e536c4e6478a
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-05-10 12:09:09 +0100

    cjson: bump version to v1.5.2
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/cjson/cjson.mk
+++ package/cjson/cjson.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CJSON_VERSION = v1.5.1
+CJSON_VERSION = v1.5.2
 CJSON_SITE = $(call github,DaveGamble,cjson,$(CJSON_VERSION))
 CJSON_INSTALL_STAGING = YES
 CJSON_LICENSE = MIT
