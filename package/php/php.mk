f9aee682f9db9ee367afb4e3876d20c6a7ca4dc3
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-05-12 13:45:14 +0100

    php: bump version to 7.1.5
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PHP_VERSION = 7.1.4
+PHP_VERSION = 7.1.5
 PHP_SITE = http://www.php.net/distributions
 PHP_SOURCE = php-$(PHP_VERSION).tar.xz
 PHP_INSTALL_STAGING = YES
