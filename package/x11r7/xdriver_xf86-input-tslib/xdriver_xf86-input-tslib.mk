0af5029453453286904431ef412d72c0bb877fce
Martin Kepplinger <martink@posteo.de>
2017-05-14 09:50:26 +0200

    x11r7: xf86-input-tslib: bump to version 1.0.0
    As the update from 0.0.X to 1.0.0 suggests, this is a major update. The driver
    uses X11's touch API now, instead of motion with button emulation. Furthermore
    it supports multitouch when a recent version of tslib is installed.
    
    It is not backwards compatible for very old versions of the X server. See
    https://github.com/merge/xf86-input-tslib/releases for some details.
    
    Signed-off-by: Martin Kepplinger <martink@posteo.de>
    Tested-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xdriver_xf86-input-tslib/xdriver_xf86-input-tslib.mk
+++ package/x11r7/xdriver_xf86-input-tslib/xdriver_xf86-input-tslib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_INPUT_TSLIB_VERSION = 0.0.7
+XDRIVER_XF86_INPUT_TSLIB_VERSION = 1.0.0
 XDRIVER_XF86_INPUT_TSLIB_SOURCE = xf86-input-tslib-$(XDRIVER_XF86_INPUT_TSLIB_VERSION).tar.bz2
 XDRIVER_XF86_INPUT_TSLIB_SITE = https://github.com/merge/xf86-input-tslib/releases/download/$(XDRIVER_XF86_INPUT_TSLIB_VERSION)
 XDRIVER_XF86_INPUT_TSLIB_LICENSE = MIT
