7dbd396d81ea39bf158353b67991462f922542f1
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-05-21 19:47:15 +0200

    package/biosdevname: bump to version 0.7.2
    Removed self-computed sha1 hash, we already provide a sha256 hash.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/biosdevname/biosdevname.mk
+++ package/biosdevname/biosdevname.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BIOSDEVNAME_VERSION = 0.6.0
+BIOSDEVNAME_VERSION = 0.7.2
 BIOSDEVNAME_SITE = http://linux.dell.com/biosdevname/biosdevname-$(BIOSDEVNAME_VERSION)
 BIOSDEVNAME_LICENSE = GPL
 BIOSDEVNAME_LICENSE_FILES = COPYING
