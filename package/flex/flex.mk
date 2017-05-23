41ee484bafe402cfadcf1d480b7c8b44bf206af1
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-05-23 11:22:01 +0100

    flex: only prevent the flex binary to be built for target, not for host
    We do want to build the flex binary when building host-flex since it's
    needed as a build dependency for other packages.
    
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/flex/flex.mk
+++ package/flex/flex.mk
@@ -15,6 +15,11 @@ FLEX_CONF_ENV = ac_cv_path_M4=/usr/bin/m4
 # we don't have a host-gettext/libintl
 HOST_FLEX_DEPENDENCIES = host-m4
 
+define FLEX_DISABLE_PROGRAM
+	$(SED) 's/^bin_PROGRAMS.*//' $(@D)/src/Makefile.in
+endef
+FLEX_POST_PATCH_HOOKS += FLEX_DISABLE_PROGRAM
+
 # flex++ symlink is broken when flex binary is not installed
 define FLEX_REMOVE_BROKEN_SYMLINK
 	rm -f $(TARGET_DIR)/usr/bin/flex++
