f66952197b41452b77dfa065adc7172137e4c243
Romain Naour <romain.naour@gmail.com>
2017-05-23 22:24:40 +0200

    package/gcc: remove gcc 4.8
    We're already using 5.x as default, and have 4.9.x on the lower side
    together with 7.x (7.1) on the higher side.
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    CC: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/uclibc/uclibc.mk
+++ package/uclibc/uclibc.mk
@@ -119,12 +119,6 @@ define UCLIBC_ARM_ABI_CONFIG
 	$(call KCONFIG_ENABLE_OPT,CONFIG_ARM_EABI,$(@D)/.config)
 endef
 
-# Thumb1 build is broken with threads with old gcc versions (< 4.8). Since
-# all cores supporting Thumb1 also support ARM, we use ARM code in this case.
-ifeq ($(BR2_GCC_VERSION_4_8_X)$(BR2_ARM_INSTRUCTIONS_THUMB)$(BR2_TOOLCHAIN_HAS_THREADS),yyy)
-UCLIBC_EXTRA_CFLAGS += -marm
-endif
-
 ifeq ($(BR2_BINFMT_FLAT),y)
 define UCLIBC_ARM_BINFMT_FLAT
 	$(call KCONFIG_DISABLE_OPT,DOPIC,$(@D)/.config)
