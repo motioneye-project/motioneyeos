4a7b70f2e4a0afc6f6ee1e6c445cffa9ca41543b
Frank Hunleth <fhunleth@troodon-software.com>
2017-05-24 10:01:00 -0400

    libconfuse: bump version to 3.1
    Release notes are at:
    
    https://github.com/martinh/libconfuse/releases/tag/v3.1
    
    Also, this release now ships with lexer.c so flex is no longer required
    for building libconfuse.
    
    Signed-off-by: Frank Hunleth <fhunleth@troodon-software.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libconfuse/libconfuse.mk
+++ package/libconfuse/libconfuse.mk
@@ -4,13 +4,11 @@
 #
 ################################################################################
 
-LIBCONFUSE_VERSION = 3.0
+LIBCONFUSE_VERSION = 3.1
 LIBCONFUSE_SOURCE = confuse-$(LIBCONFUSE_VERSION).tar.xz
 LIBCONFUSE_SITE = https://github.com/martinh/libconfuse/releases/download/v$(LIBCONFUSE_VERSION)
 LIBCONFUSE_INSTALL_STAGING = YES
 LIBCONFUSE_CONF_OPTS = --disable-rpath
-LIBCONFUSE_DEPENDENCIES = host-flex
-HOST_LIBCONFUSE_DEPENDENCIES = host-flex
 LIBCONFUSE_LICENSE = ISC
 LIBCONFUSE_LICENSE_FILES = LICENSE
 
