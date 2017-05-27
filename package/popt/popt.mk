da5816561b4a660e1562a7cea5b079d776e206a3
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-05-27 10:19:52 +0200

    package/popt: fix libiconv dependency
    Make sure that libiconv is built before popt when needed.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/popt/popt.mk
+++ package/popt/popt.mk
@@ -21,6 +21,7 @@ POPT_CONF_ENV = ac_cv_va_copy=yes
 ifeq ($(BR2_PACKAGE_LIBICONV),y)
 POPT_CONF_ENV += am_cv_lib_iconv=yes
 POPT_CONF_OPTS += --with-libiconv-prefix=$(STAGING_DIR)/usr
+POPT_DEPENDENCIES += libiconv
 endif
 
 $(eval $(autotools-package))
