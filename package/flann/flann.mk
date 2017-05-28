f6da4ce5fa88ed46d52d65b6b22ac818b2c5e2af
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-05-28 15:42:30 +0200

    package/flann: bump version to 1.9.1
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/flann/flann.mk
+++ package/flann/flann.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FLANN_VERSION = 3645f0c30a47267e56e5acdecfc7bac2b76bc3d5
+FLANN_VERSION = 1.9.1
 FLANN_SITE = $(call github,mariusmuja,flann,$(FLANN_VERSION))
 FLANN_INSTALL_STAGING = YES
 FLANN_LICENSE = BSD-3-Clause
