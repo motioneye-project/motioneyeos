eca61c93d5e6061a0749c94f7a4b5da73e332777
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-05-28 23:37:38 +0200

    package/mxml: fix download URL
    The project moved to github, the current download URL is broken:
    
    $ wget -q  http://www.msweet.org/files/project3/mxml-2.10.tar.gz
    $ file mxml-2.10.tar.gz
    mxml-2.10.tar.gz: HTML document, UTF-8 Unicode text, with very long lines
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/mxml/mxml.mk
+++ package/mxml/mxml.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 MXML_VERSION = 2.10
-MXML_SITE = http://www.msweet.org/files/project3
+MXML_SITE = https://github.com/michaelrsweet/mxml/releases/download/release-$(MXML_VERSION)
 MXML_LICENSE = LGPL-2.0+ with exceptions
 MXML_LICENSE_FILES = COPYING
 MXML_INSTALL_STAGING = YES
