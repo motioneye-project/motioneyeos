d35b447f09b5bd766a7627b596869b8f8b3f91ba
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-05-28 16:31:35 +0200

    package/ftop: Fix broken download URL
    Also use bz2 tarball and provide md5 & sha256 hashes.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ftop/ftop.mk
+++ package/ftop/ftop.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 FTOP_VERSION = 1.0
-FTOP_SITE = http://ftop.googlecode.com/files
+FTOP_SOURCE = ftop-$(FTOP_VERSION).tar.bz2
+FTOP_SITE = https://sourceforge.net/projects/ftop/files/ftop/$(FTOP_VERSION)
 FTOP_DEPENDENCIES = ncurses
 FTOP_LICENSE = GPL-3.0+
 FTOP_LICENSE_FILES = COPYING
