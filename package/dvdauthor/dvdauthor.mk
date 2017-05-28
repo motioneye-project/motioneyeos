49c44daa6d31c31da0560dfefb193e89ec37d325
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-05-28 12:33:34 +0200

    package/dvdauthor: bump version to 0.7.2
    Changed _SITE to https, added upstream hashes.
    
    Removed patch applied upstream:
    https://github.com/ldo/dvdauthor/commit/176172d5dcba4d6e2704a1a2cc48c9e687761996
    
    Also remove autoreconf-related code, basically reverting the original
    commit:
    https://git.buildroot.net/buildroot/commit/package/dvdauthor?id=1cac78fbb044279f1854b1c7c59e94f03fb061f1
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/dvdauthor/dvdauthor.mk
+++ package/dvdauthor/dvdauthor.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-DVDAUTHOR_VERSION = 0.7.1
-DVDAUTHOR_SITE = http://sourceforge.net/projects/dvdauthor/files/dvdauthor/$(DVDAUTHOR_VERSION)
+DVDAUTHOR_VERSION = 0.7.2
+DVDAUTHOR_SITE = https://sourceforge.net/projects/dvdauthor/files
 DVDAUTHOR_DEPENDENCIES = host-pkgconf libxml2 freetype libpng
 DVDAUTHOR_LICENSE = GPL-2.0+
 DVDAUTHOR_LICENSE_FILES = COPYING
@@ -13,11 +13,6 @@ DVDAUTHOR_CONF_ENV = \
 	ac_cv_prog_FREETYPECONFIG=$(STAGING_DIR)/usr/bin/freetype-config \
 	ac_cv_path_XML2_CONFIG=$(STAGING_DIR)/usr/bin/xml2-config \
 	ac_cv_prog_GMAGICKCONFIG=
-
-# configure.ac patched by 0001-configure.ac-fix-static-build.patch
-DVDAUTHOR_AUTORECONF = YES
-# add host-gettext for AM_ICONV macro
-DVDAUTHOR_DEPENDENCIES += host-gettext
 
 ifeq ($(BR2_PACKAGE_IMAGEMAGICK),y)
 DVDAUTHOR_DEPENDENCIES += imagemagick
