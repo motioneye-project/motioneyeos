2fb7cbeb743e343fcc4aa37d6015b0a523c8b16f
Peter Korsgaard <peter@korsgaard.com>
2017-05-29 23:54:48 +0200

    libtasn1: security bump to version 4.12
    Fixes CVE-2017-7650: Two errors in the "asn1_find_node()" function
    (lib/parser_aux.c) within GnuTLS libtasn1 version 4.10 can be exploited to
    cause a stacked-based buffer overflow by tricking a user into processing a
    specially crafted assignments file via the e.g.  asn1Coding utility.
    
    For more details, see:
    
    https://secuniaresearch.flexerasoftware.com/secunia_research/2017-11/
    
    Or the 1.4.11 release mail (no mail about 1.4.12, but identical to 1.4.11 +
    a soname fix):
    
    https://lists.gnu.org/archive/html/help-libtasn1/2017-05/msg00003.html
    
    Remove 0001-configure-don-t-add-Werror-to-build-flags.patch and autoreconf
    as that patch is now upstream.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libtasn1/libtasn1.mk
+++ package/libtasn1/libtasn1.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBTASN1_VERSION = 4.9
+LIBTASN1_VERSION = 4.12
 LIBTASN1_SITE = $(BR2_GNU_MIRROR)/libtasn1
 LIBTASN1_DEPENDENCIES = host-bison
 LIBTASN1_LICENSE = GPL-3.0+ (tests, tools), LGPL-2.1+ (library)
@@ -12,7 +12,5 @@ LIBTASN1_LICENSE_FILES = COPYING COPYING.LIB
 LIBTASN1_INSTALL_STAGING = YES
 # 'missing' fallback logic botched so disable it completely
 LIBTASN1_CONF_ENV = MAKEINFO="true"
-# For 0001-configure-don-t-add-Werror-to-build-flags.patch
-LIBTASN1_AUTORECONF = YES
 
 $(eval $(autotools-package))
