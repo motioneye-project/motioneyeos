26903e429e7b8b9214189f9f12134a5ae91a9361
Marcin Niestroj <m.niestroj@grinn-global.com>
2017-05-29 16:32:05 +0200

    package/luaossl: new package
    Signed-off-by: Marcin Niestroj <m.niestroj@grinn-global.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/luaossl/luaossl.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# luaossl
+#
+################################################################################
+
+LUAOSSL_VERSION_UPSTREAM = 20161214
+LUAOSSL_VERSION = $(LUAOSSL_VERSION_UPSTREAM)-0
+LUAOSSL_SUBDIR = luaossl-rel-$(LUAOSSL_VERSION_UPSTREAM)
+LUAOSSL_LICENSE = MIT
+LUAOSSL_LICENSE_FILES = $(LUAOSSL_SUBDIR)/LICENSE
+LUAOSSL_DEPENDENCIES = openssl
+
+$(eval $(luarocks-package))
