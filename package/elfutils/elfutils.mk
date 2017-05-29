27e0626e99e79655b949e13a16ebdda28c0faca2
Baruch Siach <baruch@tkos.co.il>
2017-05-29 23:53:23 +0300

    elfutils: security bump to version 0.169
    Fixes a number of security issues: CVE-2017-7607, CVE-2017-7608,
    CVE-2017-7609, CVE-2017-7610, CVE-2017-7611, CVE-2017-7612, CVE-2017-7613.
    
    Rebase patches, and convert to git format.
    
    Remove --disable-werror; unrecognized configure option.
    
    Use upstream provided hash.
    
    Cc: Stefan Fröberg <stefan.froberg@petroprogram.com>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/elfutils/elfutils.mk
+++ package/elfutils/elfutils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ELFUTILS_VERSION = 0.166
+ELFUTILS_VERSION = 0.169
 ELFUTILS_SOURCE = elfutils-$(ELFUTILS_VERSION).tar.bz2
 ELFUTILS_SITE = https://sourceware.org/elfutils/ftp/$(ELFUTILS_VERSION)
 ELFUTILS_INSTALL_STAGING = YES
@@ -20,11 +20,9 @@ HOST_ELFUTILS_AUTORECONF = YES
 # Pass a custom program prefix to avoid a naming conflict between
 # elfutils binaries and binutils binaries.
 ELFUTILS_CONF_OPTS += \
-	--disable-werror \
 	--program-prefix="eu-"
 
 HOST_ELFUTILS_CONF_OPTS = \
-	--disable-werror \
 	--with-bzlib \
 	--with-lzma \
 	--disable-progs
