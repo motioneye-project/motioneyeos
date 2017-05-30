83a626b7812498b568d0e51d5d1978b2316909c6
Johan Oudinet <johan.oudinet@gmail.com>
2017-05-30 16:51:59 +0200

    erlang-p1-zlib: bump to version 1.0.2
    License changed to Apache-2.0.
    
    Signed-off-by: Johan Oudinet <johan.oudinet@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/erlang-p1-zlib/erlang-p1-zlib.mk
+++ package/erlang-p1-zlib/erlang-p1-zlib.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-ERLANG_P1_ZLIB_VERSION = 1.0.0
-ERLANG_P1_ZLIB_SITE = $(call github,processone,zlib,$(ERLANG_P1_ZLIB_VERSION))
-ERLANG_P1_ZLIB_LICENSE = GPL-2.0+
-ERLANG_P1_ZLIB_LICENSE_FILES = COPYING
+ERLANG_P1_ZLIB_VERSION = 1.0.2
+ERLANG_P1_ZLIB_SITE = $(call github,processone,ezlib,$(ERLANG_P1_ZLIB_VERSION))
+ERLANG_P1_ZLIB_LICENSE = Apache-2.0
+ERLANG_P1_ZLIB_LICENSE_FILES = LICENSE.txt
 ERLANG_P1_ZLIB_DEPENDENCIES = zlib
 
 $(eval $(rebar-package))
