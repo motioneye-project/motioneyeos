399a10bd1c5b10cf799f8336a22d4983c83099aa
Johan Oudinet <johan.oudinet@gmail.com>
2017-05-30 16:52:01 +0200

    erlang-p1-iconv: bump to version 1.0.4
    License changed to Apache-2.0.
    
    Signed-off-by: Johan Oudinet <johan.oudinet@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/erlang-p1-iconv/erlang-p1-iconv.mk
+++ package/erlang-p1-iconv/erlang-p1-iconv.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-ERLANG_P1_ICONV_VERSION = 0.9.0
-ERLANG_P1_ICONV_SITE = $(call github,processone,eiconv,$(ERLANG_P1_ICONV_VERSION))
-ERLANG_P1_ICONV_LICENSE = GPL-2.0+
-ERLANG_P1_ICONV_LICENSE_FILES = COPYING
+ERLANG_P1_ICONV_VERSION = 1.0.4
+ERLANG_P1_ICONV_SITE = $(call github,processone,iconv,$(ERLANG_P1_ICONV_VERSION))
+ERLANG_P1_ICONV_LICENSE = Apache-2.0
+ERLANG_P1_ICONV_LICENSE_FILES = LICENSE.txt
+ERLANG_P1_ICONV_DEPENDENCIES = erlang-p1-utils
 
 ifeq ($(BR2_PACKAGE_LIBICONV),y)
 ERLANG_P1_ICONV_DEPENDENCIES += libiconv
