238f98437bac7073b117e9857aa392d7d9a10d56
Johan Oudinet <johan.oudinet@gmail.com>
2017-05-30 16:52:02 +0200

    erlang-p1-stringprep: bump to version 1.0.8
    Remove a patch that has been applied upstream.
    
    Signed-off-by: Johan Oudinet <johan.oudinet@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/erlang-p1-stringprep/erlang-p1-stringprep.mk
+++ package/erlang-p1-stringprep/erlang-p1-stringprep.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ERLANG_P1_STRINGPREP_VERSION = 1.0.0
+ERLANG_P1_STRINGPREP_VERSION = 1.0.8
 ERLANG_P1_STRINGPREP_SITE = $(call github,processone,stringprep,$(ERLANG_P1_STRINGPREP_VERSION))
 ERLANG_P1_STRINGPREP_LICENSE = TCL (tools/*.tcl), Apache-2.0 (rest)
 ERLANG_P1_STRINGPREP_LICENSE_FILES = LICENSE.ALL LICENSE.TCL LICENSE.txt
