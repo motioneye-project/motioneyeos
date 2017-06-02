8d69d8033469228968043087fd31300af1e1453a
Andrey Smirnov <andrew.smirnov@gmail.com>
2017-06-02 16:56:45 -0700

    package/python-simplegeneric: Switch setup type to 'setuptools'
    Specifying setup type to be 'distutils' results in this package being
    installed as .egg, a zipped file, which inadvertantly makes
    'simplegeneric' to be dependent on BR2_PACKAGE_PYTHON*_ZLIB. In order
    to avoid that specify setup type to be 'setuptools' which makes use of
    --single-version-externally-managed and therefore installs the package
    as .pyc and a .egg-info folder.
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-simplegeneric/python-simplegeneric.mk
+++ package/python-simplegeneric/python-simplegeneric.mk
@@ -8,7 +8,12 @@ PYTHON_SIMPLEGENERIC_VERSION = 0.8.1
 PYTHON_SIMPLEGENERIC_SOURCE = simplegeneric-$(PYTHON_SIMPLEGENERIC_VERSION).zip
 PYTHON_SIMPLEGENERIC_SITE = https://pypi.python.org/packages/3d/57/4d9c9e3ae9a255cd4e1106bb57e24056d3d0709fc01b2e3e345898e49d5b
 PYTHON_SIMPLEGENERIC_LICENSE = ZPL-2.1
-PYTHON_SIMPLEGENERIC_SETUP_TYPE = distutils
+
+# Force setup-type to be 'setuptools' in order to force the package to
+# be installed with --single-version-externally-managed. Otherwise we
+# end up with a .egg file, which is a zip archive, and as such forces
+# us to have 'zlib' as a runtime dependency
+PYTHON_SIMPLEGENERIC_SETUP_TYPE = setuptools
 
 define PYTHON_SIMPLEGENERIC_EXTRACT_CMDS
 	unzip $(DL_DIR)/$(PYTHON_SIMPLEGENERIC_SOURCE) -d $(@D)
