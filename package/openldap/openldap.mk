6f44918d49c656ca1db33ca2d0fca1d086d737be
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-06-02 16:20:45 +0100

    openldap: bump version to 2.4.45
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/openldap/openldap.mk
+++ package/openldap/openldap.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENLDAP_VERSION = 2.4.44
+OPENLDAP_VERSION = 2.4.45
 OPENLDAP_SOURCE = openldap-$(OPENLDAP_VERSION).tgz
 OPENLDAP_SITE = http://www.openldap.org/software/download/OpenLDAP/openldap-release
 OPENLDAP_LICENSE = OpenLDAP Public License
