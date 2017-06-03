53530cb994c780a606d1e0d5de58e77c5aa2b96f
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-06-03 19:01:24 +0200

    package/armadillo: bump version to 7.900.1
    Changed upstream URL to https, use .xz instead of .gz tarball.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/armadillo/armadillo.mk
+++ package/armadillo/armadillo.mk
@@ -4,10 +4,9 @@
 #
 ################################################################################
 
-ARMADILLO_VERSION = 6.500.4
-# upstream removed tarball from
-# http://downloads.sourceforge.net/project/arma
-ARMADILLO_SITE = https://ftp.fau.de/macports/distfiles/armadillo
+ARMADILLO_VERSION = 7.900.1
+ARMADILLO_SOURCE = armadillo-$(ARMADILLO_VERSION).tar.xz
+ARMADILLO_SITE = https://downloads.sourceforge.net/project/arma
 ARMADILLO_DEPENDENCIES = clapack
 ARMADILLO_INSTALL_STAGING = YES
 ARMADILLO_LICENSE = MPL-2.0
