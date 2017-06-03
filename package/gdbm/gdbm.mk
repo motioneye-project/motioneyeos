9a91a43a7e84e95afb777f831513b0774ff2cc17
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-06-03 19:34:58 +0200

    package/gdbm: bump version to 1.13
    Added sha256 hash.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gdbm/gdbm.mk
+++ package/gdbm/gdbm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GDBM_VERSION = 1.12
+GDBM_VERSION = 1.13
 GDBM_SITE = $(BR2_GNU_MIRROR)/gdbm
 GDBM_LICENSE = GPL-3.0+
 GDBM_LICENSE_FILES = COPYING
