7d2c135b9406b9ce3b83597ec0f3329413da5521
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-06-03 15:12:53 +0200

    package/libldns: bump version to 1.7.0
    Commit https://git.nlnetlabs.nl/ldns/commit/host2str.c?id=f7b3a9700e91f8e46477f335c5aaac7c6cdd8acb
    added compatability with openssl 1.1.0 but, at the same time, breaks
    build without openssl:
    
    ./host2str.c:1934:74: error: unknown type name ‘BIGNUM’
     ldns_print_bignum_b64_line(ldns_buffer* output, const char* label, const BIGNUM* num)
    
    This bump makes openssl a mandatory dependency to circumvent the
    problem, we do not gain much by leaving out crypto support in a
    sensitive area like dns. The only package using libldns, freeswitch,
    selects openssl as well, so we have no use case for having libldns
    without openssl support.
    
    --disable-dane-verify is needed until we bump our openssl to 1.1.x.,
    this fixes the following configure error:
    
    checking for SSL_get0_dane... no
    configure: error: OpenSSL does not support offline DANE verification
    (Needed for the DANE-TA usage type).  Please upgrade OpenSSL to version
    >= 1.1.0 or rerun with --disable-dane-verify or --disable-dane-ta-usage
    
    Removed patches applied upstream:
    
    0001-bugfix-548-Double-free-for-answers-4096.patch
    https://git.nlnetlabs.nl/ldns/commit/?h=release-1.7.x&id=2853eb352b4461e98f2926ace4ea3810cedf4167
    
    0002-whitespace-bug-in-ldns-read-zone.patch
    https://git.nlnetlabs.nl/ldns/commit/?h=release-1.7.x&id=d6037a22fbedb8ef3a22de4107e4eaa36840865b
    
    0003-let-doxyparse-output-manpage-generation-issues.patch
    https://git.nlnetlabs.nl/ldns/commit/?h=release-1.7.x&id=168ee09a4bf184947798c3ee3ac99b6651470d64
    
    0004-Comment-out-debugging-remnants.patch
    https://git.nlnetlabs.nl/ldns/commit/?h=release-1.7.x&id=a0a943d504debb05ce4d62ea81c2d218a6cc8727
    
    0005-Remove-manpage-content-nondeterminicity-warnings.patch
    https://git.nlnetlabs.nl/ldns/commit/?h=release-1.7.x&id=a56e48856d519d12f1ef600848f335615467f147
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libldns/libldns.mk
+++ package/libldns/libldns.mk
@@ -4,40 +4,30 @@
 #
 ################################################################################
 
-LIBLDNS_VERSION = 1.6.17
+LIBLDNS_VERSION = 1.7.0
 LIBLDNS_SOURCE = ldns-$(LIBLDNS_VERSION).tar.gz
 LIBLDNS_SITE = http://www.nlnetlabs.nl/downloads/ldns
 LIBLDNS_LICENSE = BSD-3-Clause
 LIBLDNS_LICENSE_FILES = LICENSE
 LIBLDNS_INSTALL_STAGING = YES
+LIBLDNS_DEPENDENCIES = openssl
+# --disable-dane-verify can be removed after openssl bump to 1.1.x
 LIBLDNS_CONF_OPTS = \
+	--with-ssl=$(STAGING_DIR)/usr \
+	--enable-dane \
+	--disable-dane-verify \
+	--enable-ecdsa \
+	--enable-gost \
+	--enable-sha2 \
 	--without-examples \
 	--without-p5-dns-ldns \
 	--without-pyldns \
 	--without-pyldnsx
 
-ifeq ($(BR2_PACKAGE_OPENSSL),y)
-LIBLDNS_DEPENDENCIES += openssl
-LIBLDNS_CONF_OPTS += \
-	--with-ssl=$(STAGING_DIR)/usr \
-	--enable-dane \
-	--enable-ecdsa \
-	--enable-gost \
-	--enable-sha2
-
 ifeq ($(BR2_STATIC_LIBS),y)
 LIBLDNS_DEPENDENCIES += host-pkgconf
 # missing -lz breaks configure, add it using pkgconf
 LIBLDNS_CONF_ENV += LIBS="`$(PKG_CONFIG_HOST_BINARY) --libs openssl`"
-endif
-
-else
-LIBLDNS_CONF_OPTS += \
-	--without-ssl \
-	--disable-dane \
-	--disable-ecdsa \
-	--disable-gost \
-	--disable-sha2
 endif
 
 # the linktest make target fails with static linking, and we are only
