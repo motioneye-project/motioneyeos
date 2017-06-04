4d7a56587487b42fd0ce056e1ef07b301d39f513
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-06-04 15:04:22 +0200

    package/gqrx: bump version to 2.6.1
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gqrx/gqrx.mk
+++ package/gqrx/gqrx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GQRX_VERSION = v2.6
+GQRX_VERSION = v2.6.1
 GQRX_SITE = $(call github,csete,gqrx,$(GQRX_VERSION))
 GQRX_LICENSE = GPL-3.0+, Apache-2.0
 GQRX_LICENSE_FILES = COPYING LICENSE-CTK
