f17738fde23e2c89006b770eb1270a7e8e19a8d6
Martin Bark <martin@barkynet.com>
2017-06-04 23:16:01 +0100

    package/nodejs: bump version to 8.0.0
    See https://nodejs.org/en/blog/release/v8.0.0/
    
    Signed-off-by: Martin Bark <martin@barkynet.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/nodejs/nodejs.mk
+++ package/nodejs/nodejs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NODEJS_VERSION = 7.10.0
+NODEJS_VERSION = 8.0.0
 NODEJS_SOURCE = node-v$(NODEJS_VERSION).tar.xz
 NODEJS_SITE = http://nodejs.org/dist/v$(NODEJS_VERSION)
 NODEJS_DEPENDENCIES = host-python host-nodejs zlib \
