c755b160a240f84b315c8182459f94b1fb1d37bb
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-06-04 15:08:53 +0200

    package/gssdp: bump version to 1.0.2
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gssdp/gssdp.mk
+++ package/gssdp/gssdp.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 GSSDP_VERSION_MAJOR = 1.0
-GSSDP_VERSION = $(GSSDP_VERSION_MAJOR).1
+GSSDP_VERSION = $(GSSDP_VERSION_MAJOR).2
 GSSDP_SOURCE = gssdp-$(GSSDP_VERSION).tar.xz
 GSSDP_SITE = http://ftp.gnome.org/pub/gnome/sources/gssdp/$(GSSDP_VERSION_MAJOR)
 GSSDP_LICENSE = LGPL-2.0+
