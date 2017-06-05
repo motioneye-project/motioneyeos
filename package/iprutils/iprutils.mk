a34b77e44aa926ff1aba48d8dda7db28ccbad948
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-06-05 13:06:20 +0200

    package/iprutils: bump version to 2.4.14.1
    Removed patches applied upstream:
    
    0001-iprutils-Don-t-use-gettext.patch
    https://sourceforge.net/p/iprdd/iprutils/ci/4512cfb915b492e9eed65f7b9f72fc1c1bd097c2/
    
    0002-configure.ac-use-pow-instead-of-matherr-for-libm-che.patch
    https://sourceforge.net/p/iprdd/iprutils/ci/d7cfe35b9706da357814d4bdd21563396733753c/
    
    0003-iprlib-fixes-for-compatibility-with-musl.patch
    https://sourceforge.net/p/iprdd/iprutils/ci/ed9da50d114ea5e97f3cf599fb7b305e0cec18d6/
    
    Autoreconf is not needed anymore, added upstream hashes, switched _SITE
    to https.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/iprutils/iprutils.mk
+++ package/iprutils/iprutils.mk
@@ -4,13 +4,11 @@
 #
 ################################################################################
 
-IPRUTILS_VERSION_MAJOR = 2.4.10
+IPRUTILS_VERSION_MAJOR = 2.4.14
 IPRUTILS_VERSION = $(IPRUTILS_VERSION_MAJOR).1
-IPRUTILS_SITE = http://downloads.sourceforge.net/project/iprdd/iprutils%20for%202.6%20kernels/$(IPRUTILS_VERSION_MAJOR)
+IPRUTILS_SITE = https://downloads.sourceforge.net/project/iprdd/iprutils%20for%202.6%20kernels/$(IPRUTILS_VERSION_MAJOR)
 IPRUTILS_DEPENDENCIES = ncurses libsysfs pciutils
 IPRUTILS_LICENSE = Common Public License Version 1.0
 IPRUTILS_LICENSE_FILES = LICENSE
-# Patches touching configure.ac
-IPRUTILS_AUTORECONF = YES
 
 $(eval $(autotools-package))
