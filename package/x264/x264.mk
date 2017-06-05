cf57c6aa76729bb0e22d94839db0c69f2402c979
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-06-05 18:22:38 +0200

    package/x264: bump version
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    [Thomas: update hash file.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x264/x264.mk
+++ package/x264/x264.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-X264_VERSION = 97eaef2ab82a46d13ea5e00270712d6475fbe42b
+X264_VERSION = df79067c0cf33da712d344b5f8869be7eaf326f3
 X264_SITE = git://git.videolan.org/x264.git
 X264_LICENSE = GPL-2.0+
 X264_DEPENDENCIES = host-pkgconf
