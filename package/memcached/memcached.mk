1b862a4fa5ad68467b7a9773e53e8b8c99d1aae7
Vicente Olivert Riera <Vincent.Riera@imgtec.com>
2017-06-05 11:06:19 +0100

    memcached: bump version to 1.4.37
    Signed-off-by: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/memcached/memcached.mk
+++ package/memcached/memcached.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MEMCACHED_VERSION = 1.4.36
+MEMCACHED_VERSION = 1.4.37
 MEMCACHED_SITE = http://www.memcached.org/files
 MEMCACHED_DEPENDENCIES = libevent
 MEMCACHED_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
