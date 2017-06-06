a52896f755a55d0fa30f6ba5092c69edaa73724d
Andrey Smirnov <andrew.smirnov@gmail.com>
2017-06-06 15:04:03 -0700

    package/socat: Fix TABDLY, CSIZE and CRDLY shifts for PowerPC
    As can be seen in linux/arch/powerpc/include/uapi/asm/termbits.h are
    defined as (note that those are octal numbers):
    
    \#define TABDLY	00006000
    \#define CSIZE	00001400
    \#define CRDLY	00030000
    
    which gives shifts of 10, 8 and 12. Adjust socat.mk accordingly to
    reflect that difference.
    
    Signed-off-by: Mark Hinds <zoronic@gmail.com>
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/socat/socat.mk
+++ package/socat/socat.mk
@@ -9,11 +9,20 @@ SOCAT_SOURCE = socat-$(SOCAT_VERSION).tar.bz2
 SOCAT_SITE = http://www.dest-unreach.org/socat/download
 SOCAT_LICENSE = GPL-2.0
 SOCAT_LICENSE_FILES = COPYING
-SOCAT_CONF_ENV = \
-	sc_cv_termios_ispeed=no \
+
+SOCAT_CONF_ENV = sc_cv_termios_ispeed=no
+
+ifeq ($(BR2_powerpc)$(BR2_powerpc64)$(BR2_powerpc64le),y)
+SOCAT_CONF_ENV += \
+	sc_cv_sys_crdly_shift=12 \
+	sc_cv_sys_tabdly_shift=10 \
+	sc_cv_sys_csize_shift=8
+else
+SOCAT_CONF_ENV += \
 	sc_cv_sys_crdly_shift=9 \
 	sc_cv_sys_tabdly_shift=11 \
 	sc_cv_sys_csize_shift=4
+endif
 
 # We need to run autoconf to regenerate the configure script, in order
 # to ensure that the test checking linux/ext2_fs.h works
