67117adc891e06154d23a01b8c8ee348c63e78eb
Adam Duskett <Aduskett@gmail.com>
2017-06-08 23:05:08 +0200

    mtd: bump to version 2.0.0
    This revision includes:
      - Moving from a handwritten makefile to autotools.
      - Restructuring and cleaning up the source tree.
      - Fixing the problems that the patches in the package/mtd directory fixed.
    
    Changes:
      - Move from generic-package to autotools-package in mtd.mk.
      - Remove no longer necessary patches.
      - Update binary locations in mtd.mk
      - Update library/header locations in mtd.mk
      - Remove MTD_ADD_MISSING_LINTL definition from mtd.mk, as it's no longer
        needed.
    
    Tested with toolchains compiled with musl, uclibc, and glibc.
    
    Signed-off-by: Adam Duskett <aduskett@codeblue.com>
    [Thomas: additional improvements
     - introduce hidden options BR2_PACKAGE_MTD_JFFS_UTILS,
       BR2_PACKAGE_MTD_UBIFS_UTILS and BR2_PACKAGE_MTD_TESTS that match the
       ./configure options of mtd. Those hidden options select the
       appropriate dependencies checked by the configure script, and are
       selected by the existing per-tool Config.in options.
     - .mk file is changed to handle properly the new hidden options
       BR2_PACKAGE_MTD_JFFS_UTILS, BR2_PACKAGE_MTD_UBIFS_UTILS and
       BR2_PACKAGE_MTD_TESTS.
     - .mk file is changed to properly handle BR2_PACKAGE_ACL, by passing
       --with-xattr/--without-xattr.
     - remove HOST_MTD_BUILD_CMDS and HOST_MTD_INSTALL_CMDS, those are no
       longer needed since we have an autotools-package now.
     - MTD_STAGING_y and MTD_INSTALL_STAGING_CMDS are removed, we use the
       default staging installation commands, that install everything that
       is needed.
     - the MTD_TARGETS_UBI_y variable is merged into MTD_TARGETS_y, as we no
       longer need to distinguish both.
     - integck installation logic is moved into MTD_TARGETS_y.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/mtd/mtd.mk
+++ package/mtd/mtd.mk
@@ -4,25 +4,31 @@
 #
 ################################################################################
 
-MTD_VERSION = 1.5.2
+MTD_VERSION = 2.0.0
 MTD_SOURCE = mtd-utils-$(MTD_VERSION).tar.bz2
 MTD_SITE = ftp://ftp.infradead.org/pub/mtd-utils
 MTD_LICENSE = GPL-2.0
 MTD_LICENSE_FILES = COPYING
-
 MTD_INSTALL_STAGING = YES
 
-ifeq ($(BR2_PACKAGE_MTD_MKFSJFFS2),y)
-MTD_DEPENDENCIES = zlib lzo
+ifeq ($(BR2_PACKAGE_MTD_JFFS_UTILS),y)
+MTD_DEPENDENCIES += zlib lzo host-pkgconf
+MTD_CONF_OPTS += --with-jffs
+else
+MTD_CONF_OPTS += --without-jffs
 endif
 
-ifeq ($(BR2_PACKAGE_MTD_MKFSUBIFS),y)
+ifeq ($(BR2_PACKAGE_MTD_UBIFS_UTILS),y)
 MTD_DEPENDENCIES += util-linux zlib lzo host-pkgconf
-define MTD_ADD_MISSING_LINTL
-	$(SED) "/^LDLIBS_mkfs\.ubifs/ s%$$% `$(PKG_CONFIG_HOST_BINARY) --libs uuid`%" \
-		$(@D)/Makefile
-endef
-MTD_POST_PATCH_HOOKS += MTD_ADD_MISSING_LINTL
+MTD_CONF_OPTS += --with-ubifs
+else
+MTD_CONF_OPTS += --without-ubifs
+endif
+
+ifeq ($(BR2_PACKAGE_MTD_TESTS),y)
+MTD_CONF_OPTS += --enable-tests
+else
+MTD_CONF_OPTS += --disable-tests
 endif
 
 ifeq ($(BR2_PACKAGE_BUSYBOX),y)
@@ -33,26 +39,16 @@ endif
 # also be enabled which will also include the attr package.
 ifeq ($(BR2_PACKAGE_ACL),y)
 MTD_DEPENDENCIES += acl
-MTD_MAKE_OPTS += WITHOUT_XATTR=0
+MTD_CONF_OPTS += --with-xattr
 else
-MTD_MAKE_OPTS += WITHOUT_XATTR=1
+MTD_CONF_OPTS += --without-xattr
 endif
 
 HOST_MTD_DEPENDENCIES = host-zlib host-lzo host-e2fsprogs
 
-define HOST_MTD_BUILD_CMDS
-	$(HOST_CONFIGURE_OPTS) $(MAKE1) \
-		CROSS= BUILDDIR=$(@D) WITHOUT_XATTR=1 -C $(@D)
-endef
-
-define HOST_MTD_INSTALL_CMDS
-	$(MAKE1) BUILDDIR=$(@D) DESTDIR=$(HOST_DIR) -C $(@D) install
-endef
-
 MKFS_JFFS2 = $(HOST_DIR)/usr/sbin/mkfs.jffs2
 SUMTOOL = $(HOST_DIR)/usr/sbin/sumtool
 
-MTD_STAGING_y = lib/libmtd.a ubi-utils/libubi.a
 MTD_TARGETS_$(BR2_PACKAGE_MTD_DOCFDISK)		+= docfdisk
 MTD_TARGETS_$(BR2_PACKAGE_MTD_DOC_LOADBIOS)	+= doc_loadbios
 MTD_TARGETS_$(BR2_PACKAGE_MTD_FLASHCP)		+= flashcp
@@ -79,56 +75,27 @@ MTD_TARGETS_$(BR2_PACKAGE_MTD_RFDDUMP)		+= rfddump
 MTD_TARGETS_$(BR2_PACKAGE_MTD_RFDFORMAT)	+= rfdformat
 MTD_TARGETS_$(BR2_PACKAGE_MTD_SERVE_IMAGE)	+= serve_image
 MTD_TARGETS_$(BR2_PACKAGE_MTD_SUMTOOL)		+= sumtool
-
-MTD_TARGETS_UBI_$(BR2_PACKAGE_MTD_MTDINFO)	+= mtdinfo
-MTD_TARGETS_UBI_$(BR2_PACKAGE_MTD_UBIATTACH)	+= ubiattach
-MTD_TARGETS_UBI_$(BR2_PACKAGE_MTD_UBICRC32)	+= ubicrc32
-MTD_TARGETS_UBI_$(BR2_PACKAGE_MTD_UBIDETACH)	+= ubidetach
-MTD_TARGETS_UBI_$(BR2_PACKAGE_MTD_UBIFORMAT)	+= ubiformat
-MTD_TARGETS_UBI_$(BR2_PACKAGE_MTD_UBIMKVOL)	+= ubimkvol
-MTD_TARGETS_UBI_$(BR2_PACKAGE_MTD_UBINFO)	+= ubinfo
-MTD_TARGETS_UBI_$(BR2_PACKAGE_MTD_UBINIZE)	+= ubinize
-MTD_TARGETS_UBI_$(BR2_PACKAGE_MTD_UBIRENAME)	+= ubirename
-MTD_TARGETS_UBI_$(BR2_PACKAGE_MTD_UBIRMVOL)	+= ubirmvol
-MTD_TARGETS_UBI_$(BR2_PACKAGE_MTD_UBIRSVOL)	+= ubirsvol
-MTD_TARGETS_UBI_$(BR2_PACKAGE_MTD_UBIUPDATEVOL)	+= ubiupdatevol
-MTD_TARGETS_UBI_$(BR2_PACKAGE_MTD_UBIBLOCK)	+= ubiblock
-
-MTD_TARGETS_y += $(addprefix ubi-utils/,$(MTD_TARGETS_UBI_y))
-MTD_TARGETS_$(BR2_PACKAGE_MTD_MKFSUBIFS) += mkfs.ubifs/mkfs.ubifs
-
-ifeq ($(BR2_PACKAGE_MTD_INTEGCK),y)
-define MTD_BUILD_INTEGCK
-	$(TARGET_CONFIGURE_OPTS) $(MAKE1) CROSS=$(TARGET_CROSS) \
-		BUILDDIR=$(@D) $(MTD_MAKE_OPTS) -C $(@D)/tests/fs-tests all
-endef
-define MTD_INSTALL_INTEGCK
-	$(INSTALL) -D -m 755 $(@D)/tests/fs-tests/integrity/integck $(TARGET_DIR)/usr/sbin/integck
-endef
-endif
-
-define MTD_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE1) CROSS=$(TARGET_CROSS) \
-		BUILDDIR=$(@D) $(MTD_MAKE_OPTS) -C $(@D) \
-		$(addprefix $(@D)/,$(MTD_TARGETS_y)) \
-		$(addprefix $(@D)/,$(MTD_STAGING_y))
-	$(MTD_BUILD_INTEGCK)
-endef
-
-define MTD_INSTALL_STAGING_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/lib/libmtd.a $(STAGING_DIR)/usr/lib/libmtd.a
-	$(INSTALL) -D -m 0755 $(@D)/ubi-utils/libubi.a $(STAGING_DIR)/usr/lib/libubi.a
-	$(INSTALL) -D -m 0644 $(@D)/include/libmtd.h $(STAGING_DIR)/usr/include/mtd/libmtd.h
-	$(INSTALL) -D -m 0644 $(@D)/ubi-utils/include/libubi.h $(STAGING_DIR)/usr/include/mtd/libubi.h
-	$(INSTALL) -D -m 0644 $(@D)/include/mtd/ubi-media.h $(STAGING_DIR)/usr/include/mtd/ubi-media.h
-endef
+MTD_TARGETS_$(BR2_PACKAGE_MTD_MTDINFO)		+= mtdinfo
+MTD_TARGETS_$(BR2_PACKAGE_MTD_UBIATTACH)	+= ubiattach
+MTD_TARGETS_$(BR2_PACKAGE_MTD_UBICRC32)		+= ubicrc32
+MTD_TARGETS_$(BR2_PACKAGE_MTD_UBIDETACH)	+= ubidetach
+MTD_TARGETS_$(BR2_PACKAGE_MTD_UBIFORMAT)	+= ubiformat
+MTD_TARGETS_$(BR2_PACKAGE_MTD_UBIMKVOL)		+= ubimkvol
+MTD_TARGETS_$(BR2_PACKAGE_MTD_UBINFO)		+= ubinfo
+MTD_TARGETS_$(BR2_PACKAGE_MTD_UBINIZE)		+= ubinize
+MTD_TARGETS_$(BR2_PACKAGE_MTD_UBIRENAME)	+= ubirename
+MTD_TARGETS_$(BR2_PACKAGE_MTD_UBIRMVOL)		+= ubirmvol
+MTD_TARGETS_$(BR2_PACKAGE_MTD_UBIRSVOL)		+= ubirsvol
+MTD_TARGETS_$(BR2_PACKAGE_MTD_UBIUPDATEVOL)	+= ubiupdatevol
+MTD_TARGETS_$(BR2_PACKAGE_MTD_UBIBLOCK)		+= ubiblock
+MTD_TARGETS_$(BR2_PACKAGE_MTD_MKFSUBIFS)	+= mkfs.ubifs
+MTD_TARGETS_$(BR2_PACKAGE_MTD_INTEGCK)		+= integck
 
 define MTD_INSTALL_TARGET_CMDS
 	for f in $(MTD_TARGETS_y) ; do \
 		$(INSTALL) -D -m 0755 $(@D)/$$f $(TARGET_DIR)/usr/sbin/$${f##*/} ; \
 	done
-	$(MTD_INSTALL_INTEGCK)
 endef
 
-$(eval $(generic-package))
-$(eval $(host-generic-package))
+$(eval $(autotools-package))
+$(eval $(host-autotools-package))
