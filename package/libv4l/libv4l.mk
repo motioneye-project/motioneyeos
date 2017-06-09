f837251785e9389f53d695ddb8a094707865171b
Hugues Fruchet <hugues.fruchet@st.com>
2017-06-09 14:18:03 +0200

    package/libv4l: allow build of v4l2 utilities on noMMU platforms
    This commit removes the dependencies on MMU and dynamic libraries.
    
    In order to build v4l2 utilities such as compliancy tools like
    v4l2-compliance or cec-compliance, v4-utils can now be built without
    dynamic libraries support.
    
    In that case the v4l-utils parts which depends on dynamic library
    support are not built:
     - libv4l & libv4lconvert libraries
     - libv4l plugins
     - rds-ctl utility
     - contrib test utilities
    
    The rest of the utilities are built.
    
    Signed-off-by: Hugues Fruchet <hugues.fruchet@st.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libv4l/libv4l.mk
+++ package/libv4l/libv4l.mk
@@ -13,6 +13,13 @@ LIBV4L_CONF_OPTS = --disable-doxygen-doc
 
 # 0003-utils-qv4l2-Makefile.am-link-with-librt.patch, host-gettext
 # needed for autoreconf to work
+# below patches requires autoreconf:
+# 0004-configure.ac-clarify-configure-summary.patch
+# 0005-configure.ac-revisit-v4l2-ctl-compliance-using-libv4.patch
+# 0006-configure.ac-revisit-disable-libv4l-to-disable-dyn-l.patch
+# 0007-configure.ac-add-disable-libv4l-option.patch
+# 0008-configure.ac-fix-build-of-v4l-utils-on-uclinux.patch
+# 0009-configure.ac-add-USE_LIBV4L-to-summary.patch
 LIBV4L_AUTORECONF = YES
 LIBV4L_DEPENDENCIES += host-gettext
 
