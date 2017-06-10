405e1b6499f473db86afa34a1e26cec8c388806c
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-06-10 16:01:06 +0200

    package/iprutils: needs zlib
    Dependency was introduced by
    https://sourceforge.net/p/iprdd/iprutils/ci/1f24b814e56939fd09a2319323e604594784fef3/
    
    Fixes
    http://autobuild.buildroot.net/results/48e/48e1c7fb920cbc81eee1cf2f43d1551a0a9ce31e/
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/iprutils/iprutils.mk
+++ package/iprutils/iprutils.mk
@@ -7,7 +7,7 @@
 IPRUTILS_VERSION_MAJOR = 2.4.14
 IPRUTILS_VERSION = $(IPRUTILS_VERSION_MAJOR).1
 IPRUTILS_SITE = https://downloads.sourceforge.net/project/iprdd/iprutils%20for%202.6%20kernels/$(IPRUTILS_VERSION_MAJOR)
-IPRUTILS_DEPENDENCIES = ncurses libsysfs pciutils
+IPRUTILS_DEPENDENCIES = ncurses libsysfs pciutils zlib
 IPRUTILS_LICENSE = Common Public License Version 1.0
 IPRUTILS_LICENSE_FILES = LICENSE
 
