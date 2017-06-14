bf613197b65193bb4476613b7df741dd5b4683fb
Baruch Siach <baruch@tkos.co.il>
2017-06-14 21:35:40 +0300

    libnpth: bump to version 1.5
    Switch to HTTPS for better corporate firewall compliance.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libnpth/libnpth.mk
+++ package/libnpth/libnpth.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-LIBNPTH_VERSION = 1.3
+LIBNPTH_VERSION = 1.5
 LIBNPTH_SOURCE = npth-$(LIBNPTH_VERSION).tar.bz2
-LIBNPTH_SITE = ftp://ftp.gnupg.org/gcrypt/npth
+LIBNPTH_SITE = https://www.gnupg.org/ftp/gcrypt/npth
 LIBNPTH_LICENSE = LGPL-3.0+ or GPL-2.0+
 LIBNPTH_LICENSE_FILES = COPYING COPYING.LESSER
 LIBNPTH_INSTALL_STAGING = YES
