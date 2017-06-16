cdd213170c2a61bde07ac07f1f288f8d75216478
Yegor Yefremov <yegorslists@googlemail.com>
2017-06-16 09:27:58 +0200

    python-cherrypy: bump to version 10.2.2
    Fix license info, add new dependencies and change setup type.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-cherrypy/python-cherrypy.mk
+++ package/python-cherrypy/python-cherrypy.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_CHERRYPY_VERSION = 4.0.0
+PYTHON_CHERRYPY_VERSION = 10.2.2
 PYTHON_CHERRYPY_SOURCE = CherryPy-$(PYTHON_CHERRYPY_VERSION).tar.gz
-PYTHON_CHERRYPY_SITE = http://pypi.python.org/packages/source/C/CherryPy
+PYTHON_CHERRYPY_SITE = https://pypi.python.org/packages/a2/de/dddea0c4d4be436724bfc2d1486c99368541d93c269bfa2a7fa3d132021e
 PYTHON_CHERRYPY_LICENSE = BSD-3-Clause
-PYTHON_CHERRYPY_LICENSE_FILES = cherrypy/LICENSE.txt
-PYTHON_CHERRYPY_SETUP_TYPE = distutils
+PYTHON_CHERRYPY_LICENSE_FILES = LICENSE.md
+PYTHON_CHERRYPY_SETUP_TYPE = setuptools
+PYTHON_CHERRYPY_DEPENDENCIES = host-python-setuptools-scm
 
 $(eval $(python-package))
