a435247b015a794d1c302c7b19ff68a616a1078b
Marcin Niestroj <m.niestroj@grinn-global.com>
2017-06-20 17:16:31 +0200

    package/lua-flu: new package
    Signed-off-by: Marcin Niestroj <m.niestroj@grinn-global.com>
    [Thomas: "depends on" before "select" in Config.in]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/lua-flu/lua-flu.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# lua-flu
+#
+################################################################################
+
+LUA_FLU_VERSION = 20150331-1
+LUA_FLU_NAME_UPSTREAM = flu
+LUA_FLU_SUBDIR = doub-flu-a7daae986339
+LUA_FLU_LICENSE = MIT
+LUA_FLU_LICENSE_FILES = $(LUA_FLU_SUBDIR)/doc/LICENSE.txt
+LUA_FLU_DEPENDENCIES = attr libfuse
+
+$(eval $(luarocks-package))
