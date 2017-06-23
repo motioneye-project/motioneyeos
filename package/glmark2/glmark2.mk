e4768669a753876a9ae7b6ea8952421db19e7f12
Fabio Estevam <festevam@gmail.com>
2017-06-23 10:28:10 -0300

    glmark2: Bump to the latest version
    The two patches are no longer needed with the latest upstream version,
    so bump to the latest one.
    
    Tested on imx6.
    
    Signed-off-by: Fabio Estevam <festevam@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/glmark2/glmark2.mk
+++ package/glmark2/glmark2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GLMARK2_VERSION = 7215c0f337dae0b232535549c37fca441747a891
+GLMARK2_VERSION = 9b1070fe9c5cf908f323909d3c8cbed08022abe8
 GLMARK2_SITE = $(call github,glmark2,glmark2,$(GLMARK2_VERSION))
 GLMARK2_LICENSE = GPL-3.0+, SGIv1
 GLMARK2_LICENSE_FILES = COPYING COPYING.SGI
