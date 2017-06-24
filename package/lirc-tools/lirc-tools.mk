e42a827ea1511ff11b91f9957603ab7a01f24c61
Baruch Siach <baruch@tkos.co.il>
2017-06-24 23:56:01 +0300

    lirc-tools: bump to version 0.9.4d
    Drop 0001-tools-make_rel_symlink.py-can-also-use-python2.patch. Build requires
    host-python3 since the previous version bump.
    
    Drop upstream 0002-lircd-Remove-use-of-functions-killed-in-kernel-4.8.0.patch.
    
    Add a patch fixing header guard macro collision with musl libc.
    
    Set HAVE_WORKING_POLL to skip poll(2) run test that is not compatible with
    cross compilation.
    
    Add optional dependency on libftdi1.
    
    Cc: Rhys Williams <github@wilberforce.co.nz>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lirc-tools/lirc-tools.mk
+++ package/lirc-tools/lirc-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIRC_TOOLS_VERSION = 0.9.4b
+LIRC_TOOLS_VERSION = 0.9.4d
 LIRC_TOOLS_SOURCE = lirc-$(LIRC_TOOLS_VERSION).tar.bz2
 LIRC_TOOLS_SITE = http://downloads.sourceforge.net/project/lirc/LIRC/$(LIRC_TOOLS_VERSION)
 LIRC_TOOLS_LICENSE = GPL-2.0+
@@ -12,7 +12,7 @@ LIRC_TOOLS_LICENSE_FILES = COPYING
 LIRC_TOOLS_DEPENDENCIES = host-libxslt host-pkgconf host-python3
 LIRC_TOOLS_INSTALL_STAGING = YES
 
-LIRC_TOOLS_CONF_ENV = XSLTPROC=yes
+LIRC_TOOLS_CONF_ENV = XSLTPROC=yes HAVE_WORKING_POLL=yes
 LIRC_TOOLS_CONF_OPTS = --without-x
 
 ifeq ($(BR2_PACKAGE_HAS_UDEV),y)
@@ -29,6 +29,10 @@ endif
 
 ifeq ($(BR2_PACKAGE_PORTAUDIO),y)
 LIRC_TOOLS_DEPENDENCIES += portaudio
+endif
+
+ifeq ($(BR2_PACKAGE_LIBFTDI1),y)
+LIRC_TOOLS_DEPENDENCIES += libftdi1
 endif
 
 define LIRC_TOOLS_INSTALL_INIT_SYSV
