0111ced7705f4038eb91a66e7f055a4994246751
Marcin Niestroj <m.niestroj@grinn-global.com>
2017-06-26 17:19:41 +0200

    package/lua-stdlib: new package
    Signed-off-by: Marcin Niestroj <m.niestroj@grinn-global.com>
    Reviewed-by: Romain Naour <romain.naour@smile.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/lua-stdlib/lua-stdlib.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# lua-stdlib
+#
+################################################################################
+
+LUA_STDLIB_VERSION_UPSTREAM = 41.2.0
+LUA_STDLIB_VERSION = $(LUA_STDLIB_VERSION_UPSTREAM)-1
+LUA_STDLIB_NAME_UPSTREAM = stdlib
+LUA_STDLIB_SUBDIR = lua-stdlib-release-v$(LUA_STDLIB_VERSION_UPSTREAM)
+LUA_STDLIB_LICENSE = MIT
+LUA_STDLIB_LICENSE_FILES = $(LUA_STDLIB_SUBDIR)/COPYING
+
+$(eval $(luarocks-package))
