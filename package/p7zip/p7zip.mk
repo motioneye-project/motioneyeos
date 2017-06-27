d233eec73fcc9d927d2a118cddb48afe9d24e9dc
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-06-27 19:15:18 +0200

    package/p7zip: bump version to 16.02
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/p7zip/p7zip.mk
+++ package/p7zip/p7zip.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-P7ZIP_VERSION = 15.14.1
+P7ZIP_VERSION = 16.02
 P7ZIP_SOURCE = p7zip_$(P7ZIP_VERSION)_src_all.tar.bz2
 P7ZIP_SITE = http://downloads.sourceforge.net/project/p7zip/p7zip/$(P7ZIP_VERSION)
 P7ZIP_LICENSE = LGPL-2.1+ with unRAR restriction
