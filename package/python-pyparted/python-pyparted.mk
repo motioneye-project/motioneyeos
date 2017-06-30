05ce14894e6e43a8c88755032e0d428e8c2a4a3f
Yegor Yefremov <yegorslists@googlemail.com>
2017-06-30 14:45:24 +0200

    python-pyparted: bump to version 3.11.0
    Remove erroneously added CFFI dependency.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-pyparted/python-pyparted.mk
+++ package/python-pyparted/python-pyparted.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_PYPARTED_VERSION = v3.10.7
+PYTHON_PYPARTED_VERSION = v3.11.0
 PYTHON_PYPARTED_SITE = $(call github,rhinstaller,pyparted,$(PYTHON_PYPARTED_VERSION))
 PYTHON_PYPARTED_SETUP_TYPE = distutils
 PYTHON_PYPARTED_LICENSE = GPL-2.0+
 PYTHON_PYPARTED_LICENSE_FILES = COPYING
-PYTHON_PYPARTED_DEPENDENCIES = host-python-cffi parted
+PYTHON_PYPARTED_DEPENDENCIES = parted
 
 $(eval $(python-package))
