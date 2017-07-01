7a794e1143f1ba65e94fb85873a2cc74f0103257
Francois Perrad <fperrad@gmail.com>
2017-07-01 21:37:03 +0200

    olimex_a20: refactor with U-Boot boot script
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- board/olimex/a20_olinuxino/post-build.sh
+++ board/olimex/a20_olinuxino/post-build.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-# args from BR2_ROOTFS_POST_SCRIPT_ARGS
-# $2    path of boot.cmd
-# $3    output directory for boot.scr
 
-MKIMAGE=$HOST_DIR/usr/bin/mkimage
-
-$MKIMAGE -A arm -O linux -T script -C none -d $2 $3/boot.scr
+cp $BINARIES_DIR/boot.scr $TARGET_DIR/boot/boot.scr
 
 if [ -e $BINARIES_DIR/script.bin ]; then
-	cp $BINARIES_DIR/script.bin $3/script.bin
+	# mali requires a legacy kernel
+	cp $BINARIES_DIR/script.bin $TARGET_DIR/boot/script.bin
 fi
