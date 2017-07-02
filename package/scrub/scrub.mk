4eb7f76ed187cdb79cafb8efa52f00b085274a44
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-07-02 18:53:36 +0200

    scrub: don't download patch from Github
    Patches downloaded from Github are not stable, so bring them in the
    tree.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/scrub/scrub.mk
+++ package/scrub/scrub.mk
@@ -8,7 +8,6 @@ SCRUB_VERSION = 2.6.1
 SCRUB_SITE = $(call github,chaos,scrub,$(SCRUB_VERSION))
 SCRUB_LICENSE = GPL-2.0+
 SCRUB_LICENSE_FILES = COPYING DISCLAIMER
-SCRUB_PATCH = https://github.com/chaos/scrub/commit/11d30916dd9c11a26c7c8a0f6db9e6ebca301594.patch
 
 # Fetching from the git repo, no configure/Makefile generated, and patching
 # configure.ac
