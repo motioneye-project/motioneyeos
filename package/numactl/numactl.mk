9ef0e9afbabc78adb7f6df282ea4b0e31d7fb504
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-07-02 18:53:29 +0200

    numactl: don't download patches from Github
    Patches downloaded from Github are not stable, so bring them in the
    tree.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/numactl/numactl.mk
+++ package/numactl/numactl.mk
@@ -6,9 +6,6 @@
 
 NUMACTL_VERSION = 2.0.11
 NUMACTL_SITE = ftp://oss.sgi.com/www/projects/libnuma/download
-NUMACTL_PATCH = \
-	https://github.com/numactl/numactl/commit/3770bdc4fa7b9059db5cd2aa8bb09b50fa15e456.patch \
-	https://github.com/numactl/numactl/commit/31dc2951c758698bff060aeae8ffd8854616183b.patch
 NUMACTL_LICENSE = LGPL-2.1 (libnuma), GPL-2.0 (programs)
 NUMACTL_LICENSE_FILES = README
 NUMACTL_INSTALL_STAGING = YES
