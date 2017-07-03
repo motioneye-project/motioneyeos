8833495a3659ee0686d6413133742a0b42580ecb
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-07-03 23:14:54 +0200

    procps-ng: use the new gettext logic
    This commit switches to use the new gettext logic, which involves:
    
     - using TARGET_NLS_DEPENDENCIES instead of hand-encoded dependencies
       on gettext/host-gettext
    
     - using TARGET_NLS_LIBS to force linking against libintl
    
     - dropping BR2_PACKAGE_GETTEXT selection
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/procps-ng/procps-ng.mk
+++ package/procps-ng/procps-ng.mk
@@ -10,7 +10,8 @@ PROCPS_NG_SITE = http://downloads.sourceforge.net/project/procps-ng/Production
 PROCPS_NG_LICENSE = GPL-2.0+, LGPL-2.0+ (libproc and libps)
 PROCPS_NG_LICENSE_FILES = COPYING COPYING.LIB
 PROCPS_NG_INSTALL_STAGING = YES
-PROCPS_NG_DEPENDENCIES = ncurses host-pkgconf
+PROCPS_NG_DEPENDENCIES = ncurses host-pkgconf $(TARGET_NLS_DEPENDENCIES)
+PROCPS_NG_CONF_OPTS = LIBS=$(TARGET_NLS_LIBS)
 # For 0002-use-pkgconfig-for-ncursesw-cflags.patch
 PROCPS_NG_AUTORECONF = YES
 PROCPS_NG_GETTEXTIZE = YES
@@ -19,11 +20,6 @@ PROCPS_NG_GETTEXTIZE = YES
 # wins the fight over who gets to have their utils actually installed.
 ifeq ($(BR2_PACKAGE_BUSYBOX),y)
 PROCPS_NG_DEPENDENCIES += busybox
-endif
-
-ifeq ($(BR2_NEEDS_GETTEXT_IF_LOCALE),y)
-PROCPS_NG_DEPENDENCIES += gettext
-PROCPS_NG_CONF_OPTS += LIBS=-lintl
 endif
 
 ifeq ($(BR2_PACKAGE_SYSTEMD),y)
