b198cedc5007e6777efcedd2a6d9e2ff9952f65d
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-07-03 22:45:08 +0200

    e2fsprogs: remove libintl static linking handling
    We no longer support building the full-blown libintl in static linking
    scenarios, as it causes too many problems. Therefore, remove the
    special code in e2fsprogs that was handling this.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Reviewed-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/e2fsprogs/e2fsprogs.mk
+++ package/e2fsprogs/e2fsprogs.mk
@@ -77,12 +77,6 @@ HOST_E2FSPROGS_CONF_ENV += \
 	ac_cv_header_magic_h=no \
 	ac_cv_lib_magic_magic_file=no
 
-ifeq ($(BR2_NEEDS_GETTEXT_IF_LOCALE)$(BR2_STATIC_LIBS),yy)
-# util-linux libuuid pulls in libintl if needed, so ensure we also
-# link against it, otherwise static linking fails
-E2FSPROGS_CONF_ENV += LIBS=-lintl
-endif
-
 E2FSPROGS_MAKE_OPTS = LDCONFIG=true
 
 E2FSPROGS_INSTALL_STAGING_OPTS = \
