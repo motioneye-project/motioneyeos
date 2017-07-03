17875ff36910b2c8d780955bacbb9d92668a98c8
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-07-03 22:58:12 +0200

    madplay: use the new gettext logic
    This commit switches to use the new gettext logic, which involves:
    
     - using TARGET_NLS_DEPENDENCIES instead of hand-encoded dependencies
       on gettext/host-gettext
    
     - dropping BR2_PACKAGE_GETTEXT selection
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/madplay/madplay.mk
+++ package/madplay/madplay.mk
@@ -9,7 +9,7 @@ MADPLAY_SITE = http://downloads.sourceforge.net/project/mad/madplay/$(MADPLAY_VE
 MADPLAY_LICENSE = GPL-2.0+
 MADPLAY_LICENSE_FILES = COPYING COPYRIGHT
 MADPLAY_LIBTOOL_PATCH = NO
-MADPLAY_DEPENDENCIES = libmad libid3tag $(if $(BR2_PACKAGE_GETTEXT),gettext)
+MADPLAY_DEPENDENCIES = libmad libid3tag $(TARGET_NLS_DEPENDENCIES)
 
 # Check if ALSA is built, then we should configure after alsa-lib so
 # ./configure can find alsa-lib.
