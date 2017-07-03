b08bf566e3619e409542ed189a4c5cbea52ec5f1
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-07-03 23:06:55 +0200

    newt: use the new gettext logic
    This commit switches to use the new gettext logic, which involves:
    
     - using TARGET_NLS_DEPENDENCIES instead of hand-encoded dependencies
       on gettext/host-gettext
    
     - using TARGET_NLS_LIBS to force linking against libintl
    
     - dropping BR2_PACKAGE_GETTEXT selection
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/newt/newt.mk
+++ package/newt/newt.mk
@@ -7,8 +7,10 @@
 NEWT_VERSION = 0.52.19
 NEWT_SITE = https://pagure.io/releases/newt
 NEWT_INSTALL_STAGING = YES
-NEWT_DEPENDENCIES = popt slang \
-	$(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext)
+NEWT_DEPENDENCIES = popt slang $(TARGET_NLS_DEPENDENCIES)
+# Force to use libintl, otherwise it finds gettext functions in the C
+# library, and does not link against libintl.
+NEWT_CONF_ENV = LIBS=$(TARGET_NLS_LIBS)
 NEWT_CONF_OPTS = --without-python --without-tcl
 NEWT_MAKE = $(MAKE1)
 NEWT_LICENSE = GPL-2.0
