9835c1921c08f6eb7723d5df64371ac3446a7539
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-07-03 23:19:34 +0200

    sysstat: use the new gettext logic
    This commit switches to use the new gettext logic, which involves:
    
     - using TARGET_NLS_DEPENDENCIES instead of hand-encoded dependencies
       on gettext/host-gettext
    
     - using TARGET_NLS_LIBS to force linking against libintl
    
     - dropping BR2_PACKAGE_GETTEXT selection
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/sysstat/sysstat.mk
+++ package/sysstat/sysstat.mk
@@ -8,14 +8,10 @@ SYSSTAT_VERSION = 11.4.4
 SYSSTAT_SOURCE = sysstat-$(SYSSTAT_VERSION).tar.xz
 SYSSTAT_SITE = http://pagesperso-orange.fr/sebastien.godard
 SYSSTAT_CONF_OPTS = --disable-file-attr --disable-sensors
-SYSSTAT_DEPENDENCIES = host-gettext
+SYSSTAT_DEPENDENCIES = host-gettext $(TARGET_NLS_DEPENDENCIES)
 SYSSTAT_LICENSE = GPL-2.0+
 SYSSTAT_LICENSE_FILES = COPYING
-
-ifeq ($(BR2_NEEDS_GETTEXT_IF_LOCALE),y)
-SYSSTAT_DEPENDENCIES += gettext
-SYSSTAT_MAKE_OPTS += LFLAGS="$(TARGET_LDFLAGS) -lintl"
-endif
+SYSSTAT_MAKE_OPTS += LFLAGS="$(TARGET_LDFLAGS) $(TARGET_NLS_LIBS)"
 
 # The isag tool is a post processing script that depends on tcl/tk
 # among other things. So we don't install it.
