cb3de26fe459a1452af173b07d98430cd90a4130
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-07-03 22:57:49 +0200

    ltris: use the new gettext logic
    This commit switches to use the new gettext logic, which involves:
    
     - using TARGET_NLS_DEPENDENCIES instead of hand-encoded dependencies
       on gettext/host-gettext
    
     - using TARGET_NLS_LIBS to force linking against libintl
    
     - dropping BR2_PACKAGE_GETTEXT selection
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ltris/ltris.mk
+++ package/ltris/ltris.mk
@@ -9,7 +9,8 @@ LTRIS_VERSION = 1.0.19
 LTRIS_LICENSE = GPL-2.0+
 LTRIS_LICENSE_FILES = COPYING
 
-LTRIS_DEPENDENCIES = sdl
+LTRIS_DEPENDENCIES = sdl $(TARGET_NLS_DEPENDENCIES)
+LTRIS_LIBS = $(TARGET_NLS_LIBS)
 
 LTRIS_CONF_ENV = \
 	SDL_CONFIG="$(STAGING_DIR)/usr/bin/sdl-config" \
@@ -23,11 +24,6 @@ LTRIS_CONF_OPTS += --enable-sound
 LTRIS_LIBS += `$(PKG_CONFIG_HOST_BINARY) --libs SDL_mixer`
 else
 LTRIS_CONF_OPTS += --disable-sound
-endif
-
-ifeq ($(BR2_NEEDS_GETTEXT_IF_LOCALE),y)
-LTRIS_DEPENDENCIES += gettext
-LTRIS_LIBS += -lintl
 endif
 
 $(eval $(autotools-package))
