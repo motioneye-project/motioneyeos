9494e46a07d82ff781a92af0b63676f15a5ecb85
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-07-03 22:41:25 +0200

    ding-libs: use the new gettext logic
    This commit switches to use the new gettext logic, which involves:
    
     - using TARGET_NLS_DEPENDENCIES instead of hand-encoded dependencies
       on gettext/host-gettext
    
     - dropping BR2_PACKAGE_GETTEXT selection
    
    The BR2_USE_WCHAR dependency was only needed for gettext. But now, we
    no longer need gettext unconditionally, so this dependency on
    BR2_USE_WCHAR is removed.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/ding-libs/ding-libs.mk
+++ package/ding-libs/ding-libs.mk
@@ -8,7 +8,7 @@ DING_LIBS_VERSION = 0_4_0
 DING_LIBS_SOURCE = ding_libs-$(DING_LIBS_VERSION).tar.xz
 DING_LIBS_SITE = https://git.fedorahosted.org/cgit/ding-libs.git/snapshot
 DING_LIBS_DEPENDENCIES = host-pkgconf \
-	$(if $(BR2_PACKAGE_GETTEXT),gettext) \
+	$(TARGET_NLS_DEPENDENCIES) \
 	$(if $(BR2_PACKAGE_LIBICONV),libiconv)
 DING_LIBS_INSTALL_STAGING = YES
 DING_LIBS_LICENSE = LGPL-3.0+ (library),GPL-3.0+ (test programs)
