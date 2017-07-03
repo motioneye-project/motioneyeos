24c6a63367a52eb43322a930f20f0d1087bdd136
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-07-03 22:53:04 +0200

    libuio: use the new gettext logic
    This commit switches to use the new gettext logic, which involves:
    
     - using TARGET_NLS_DEPENDENCIES instead of hand-encoded dependencies
       on gettext/host-gettext
    
     - using TARGET_NLS_LIBS to force linking against libintl
    
     - dropping BR2_PACKAGE_GETTEXT selection
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libuio/libuio.mk
+++ package/libuio/libuio.mk
@@ -10,6 +10,8 @@ LIBUIO_SITE = $(call github,Linutronix,libuio,$(LIBUIO_VERSION))
 LIBUIO_LICENSE = LGPL-2.1 (library), GPL-2.0 (programs)
 LIBUIO_LICENSE_FILES = COPYING
 LIBUIO_CONF_OPTS = --with-glib=no --without-werror
+LIBUIO_DEPENDENCIES = $(TARGET_NLS_DEPENDENCIES)
+LIBUIO_LIBS = $(TARGET_NLS_LIBS)
 LIBUIO_INSTALL_STAGING = YES
 
 # Fetched from github, no pre-generated configure script provided
@@ -22,13 +24,6 @@ LIBUIO_CONF_ENV += MAKEINFO=true
 ifeq ($(BR2_PACKAGE_ARGP_STANDALONE),y)
 LIBUIO_DEPENDENCIES += argp-standalone
 LIBUIO_LIBS += -largp
-endif
-
-# libuio pulls in libintl if needed, so ensure we also
-# link against it, otherwise static linking fails
-ifeq ($(BR2_NEEDS_GETTEXT_IF_LOCALE),y)
-LIBUIO_DEPENDENCIES += gettext
-LIBUIO_LIBS += -lintl
 endif
 
 LIBUIO_CONF_ENV += LIBS="$(LIBUIO_LIBS)"
