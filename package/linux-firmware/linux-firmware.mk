caaff41ec168d4abe083eccebb534c555b816e66
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2017-07-04 18:39:35 +0200

    linux-firmware: add qca6174{bt} firmware
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
    [Thomas: adjust alphabetic ordering, rename bluetooth option to
    include vendor name.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/linux-firmware/linux-firmware.mk
+++ package/linux-firmware/linux-firmware.mk
@@ -30,6 +30,12 @@ LINUX_FIRMWARE_FILES += intel/ibt-*
 LINUX_FIRMWARE_ALL_LICENSE_FILES += LICENCE.ibt_firmware
 endif
 
+# Qualcomm Atheros Rome 6174A Bluetooth
+ifeq ($(BR2_PACKAGE_LINUX_FIRMWARE_QUALCOMM_6174A_BT),y)
+LINUX_FIRMWARE_FILES += qca/rampatch_usb_00000302.bin qca/nvm_usb_00000302.bin
+LINUX_FIRMWARE_ALL_LICENSE_FILES += LICENCE.qcom
+endif
+
 # Freescale i.MX SDMA
 ifeq ($(BR2_PACKAGE_LINUX_FIRMWARE_IMX_SDMA),y)
 LINUX_FIRMWARE_FILES += imx/sdma/sdma-imx6q.bin imx/sdma/sdma-imx7d.bin
@@ -192,6 +198,12 @@ endif
 ifeq ($(BR2_PACKAGE_LINUX_FIRMWARE_MEDIATEK_MT7601U),y)
 LINUX_FIRMWARE_FILES += mt7601u.bin
 LINUX_FIRMWARE_ALL_LICENSE_FILES += LICENCE.ralink_a_mediatek_company_firmware
+endif
+
+# qca6174
+ifeq ($(BR2_PACKAGE_LINUX_FIRMWARE_QUALCOMM_6174),y)
+LINUX_FIRMWARE_FILES += ath10k/QCA6174
+LINUX_FIRMWARE_ALL_LICENSE_FILES += LICENSE.QualcommAtheros_ath10k
 endif
 
 # wl127x
