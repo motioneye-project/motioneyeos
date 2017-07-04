39b4a27ec294ce3bc0d0e3c5731aff7d5ba69dee
Adam Duskett <Aduskett@gmail.com>
2017-07-04 13:20:08 -0400

    nginx: bump to version 1.12.0
    Also update the patch line numbers to work with 1.12.0
    
    Signed-off-by: Adam Duskett <Adamduskett@outlook.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/nginx/nginx.mk
+++ package/nginx/nginx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NGINX_VERSION = 1.10.3
+NGINX_VERSION = 1.12.0
 NGINX_SITE = http://nginx.org/download
 NGINX_LICENSE = BSD-2-Clause
 NGINX_LICENSE_FILES = LICENSE
