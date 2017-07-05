1d54c8415816a8116185b495822bfc6ec44f4071
Keguang Zhang <keguang.zhang@spreadtrum.com>
2017-07-05 18:33:44 +0800

    bwm-ng: update to latest upstream version to fix build issue
    The latest upstream version has fixed the build issue.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/1d9f0b62e4cc387f508b4fe0086621ab9996a085/
    
    Signed-off-by: Keguang Zhang <keguang.zhang@spreadtrum.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/bwm-ng/bwm-ng.mk
+++ package/bwm-ng/bwm-ng.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-BWM_NG_VERSION = 0.6.1
-BWM_NG_SITE = http://www.gropp.org/bwm-ng
+BWM_NG_VERSION = f54b3fad2c80bfe63f920c9b5e7c1d80389c57ef
+BWM_NG_SITE = $(call github,vgropp,bwm-ng,$(BWM_NG_VERSION))
 BWM_NG_CONF_OPTS = --with-procnetdev --with-diskstats
 BWM_NG_LICENSE = GPL-2.0
 BWM_NG_LICENSE_FILES = COPYING
+BWM_NG_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_NCURSES),y)
 BWM_NG_CONF_OPTS += --with-ncurses
