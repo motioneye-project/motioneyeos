6424c0637698f537a4b2d3a41791b3b7c63322b7
Olivier Schonken <olivier.schonken@gmail.com>
2017-07-05 13:42:36 +0200

    libcodec2: bump to 0.7, switch to CMake
    Since version 0.6, libcodec2 switched buildsystem from autotools to
    cmake.
    
    The patches in the libcodec2 directory were removed because they were
    only relevant to the autotools build, but another patch was added to
    fix the codec2.pc file being installed.
    
    For the cmake package unittests are disabled by default. The download
    site is changed to http://files.freedv.org/codec2. An option is added
    to be able to install examples to target.
    
    Signed-off-by: Olivier Schonken <olivier.schonken@gmail.com>
    [Thomas: switch to 0.7, add patch to fix .pc file.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libcodec2/libcodec2.mk
+++ package/libcodec2/libcodec2.mk
@@ -4,13 +4,16 @@
 #
 ################################################################################
 
-LIBCODEC2_VERSION = 392a55b4f3f8ad30d845ac6ae35e8b27343bb944
-LIBCODEC2_SITE = https://freeswitch.org/stash/scm/sd/libcodec2.git
-LIBCODEC2_SITE_METHOD = git
+LIBCODEC2_VERSION = 0.7
+LIBCODEC2_SITE = http://files.freedv.org/codec2
+LIBCODEC2_SOURCE = codec2-$(LIBCODEC2_VERSION).tar.xz
 LIBCODEC2_LICENSE = LGPL-2.1
 LIBCODEC2_LICENSE_FILES = COPYING
-LIBCODEC2_AUTORECONF = YES
 LIBCODEC2_INSTALL_STAGING = YES
-LIBCODEC2_CONF_OPTS = --disable-unittests
+LIBCODEC2_SUPPORTS_IN_SOURCE_BUILD = NO
 
-$(eval $(autotools-package))
+ifeq ($(BR2_PACKAGE_LIBCODEC2_EXAMPLES),y)
+LIBCODEC2_CONF_OPTS += -DINSTALL_EXAMPLES=ON
+endif
+
+$(eval $(cmake-package))
