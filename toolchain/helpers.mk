9971e936be6eb2ee5f531953bc35259d016dc60f
Arnout Vandecappelle <arnout@mind.be>
2017-07-05 16:54:18 +0200

    toolchain/helpers.mk: fix creation of DESTDIR in copy_toolchain_lib_root
    In commit b3cc7e65ee, the definition of the DESTDIR variable was moved
    down into the loop that follows symlinks in the libraries that are
    copied to target. However, the corresponding mkdir was not moved down,
    so that no directories are ever created.
    
    In practice, this mkdir is normally redundant since the directories
    should already have been created as part of creating STAGING_DIR.
    Still, the current situation is clearly wrong, so fix it by moving the
    mkdir down to after the assignment to DESTDIR.
    
    While we're at it, also remove a redundant empty line. It's a leftover
    from when a lot of variables were declared above.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- toolchain/helpers.mk
+++ toolchain/helpers.mk
@@ -10,13 +10,12 @@
 #
 copy_toolchain_lib_root = \
 	LIBPATTERN="$(strip $1)"; \
-\
 	LIBPATHS=`find $(STAGING_DIR)/ -name "$${LIBPATTERN}" 2>/dev/null` ; \
 	for LIBPATH in $${LIBPATHS} ; do \
-		mkdir -p $(TARGET_DIR)/$${DESTDIR}; \
 		while true ; do \
 			LIBNAME=`basename $${LIBPATH}`; \
 			DESTDIR=`echo $${LIBPATH} | sed "s,^$(STAGING_DIR)/,," | xargs dirname` ; \
+			mkdir -p $(TARGET_DIR)/$${DESTDIR}; \
 			rm -fr $(TARGET_DIR)/$${DESTDIR}/$${LIBNAME}; \
 			if test -h $${LIBPATH} ; then \
 				cp -d $${LIBPATH} $(TARGET_DIR)/$${DESTDIR}/$${LIBNAME}; \
