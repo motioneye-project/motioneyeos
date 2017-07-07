49dfb3306f4985907672a3734957b2198c068b15
Naoki Matsumoto <n-matsumoto@melcoinc.co.jp>
2017-07-07 03:45:16 +0000

    pcre2: new package
    Signed-off-by: Naoki Matsumoto <n-matsumoto@melcoinc.co.jp>
    Signed-off-by: Brent Sink <brents_3@hotmail.com>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    [Thomas:
     - reassign authorship to Naoki Matsumoto
     - add entry to DEVELOPERS file
     - fix broken patch
     - add comment about where the PGP signature was coming from]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/pcre2/pcre2.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# pcre2
+#
+################################################################################
+
+PCRE2_VERSION = 10.23
+PCRE2_SITE = https://ftp.pcre.org/pub/pcre
+PCRE2_SOURCE = pcre2-$(PCRE2_VERSION).tar.bz2
+PCRE2_LICENSE = BSD-3-Clause
+PCRE2_LICENSE_FILES = LICENCE
+PCRE2_INSTALL_STAGING = YES
+PCRE2_CONFIG_SCRIPTS = pcre2-config
+
+PCRE2_CONF_OPTS += --enable-pcre2-8
+PCRE2_CONF_OPTS += $(if $(BR2_PACKAGE_PCRE2_16),--enable-pcre2-16,--disable-pcre2-16)
+PCRE2_CONF_OPTS += $(if $(BR2_PACKAGE_PCRE2_32),--enable-pcre2-32,--disable-pcre2-32)
+
+$(eval $(autotools-package))
+$(eval $(host-autotools-package))
