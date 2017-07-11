474daa20f8da2a677250146e8ee1652206923ee8
Peter Korsgaard <peter@korsgaard.com>
2017-07-11 12:28:57 +0200

    mpg123: security bump to version 1.25.2
    >From the release notes:
    
     - Extend pow tables for layer III to properly handle files with i-stereo and
       5-bit scalefactors. Never observed them for real, just as fuzzed input to
       trigger the read overflow. Note: This one goes on record as CVE-2017-11126,
       calling remote denial of service. While the accesses are out of bounds for
       the pow tables, they still are safely within libmpg123's memory (other
       static tables). Just wrong values are used for computation, no actual crash
       unless you use something like GCC's AddressSanitizer, nor any information
       disclosure.
     - Avoid left-shifts of negative integers in layer I decoding.
    
    While we're at it, add a hash for the license file.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mpg123/mpg123.mk
+++ package/mpg123/mpg123.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MPG123_VERSION = 1.25.1
+MPG123_VERSION = 1.25.2
 MPG123_SOURCE = mpg123-$(MPG123_VERSION).tar.bz2
 MPG123_SITE = http://downloads.sourceforge.net/project/mpg123/mpg123/$(MPG123_VERSION)
 MPG123_CONF_OPTS = --disable-lfs-alias
