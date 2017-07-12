5beef05d1a5d399ef9b2599b78c186ed0e070144
Yegor Yefremov <yegorslists@googlemail.com>
2017-07-12 16:41:30 +0200

    python-pysocks: bump to version 1.6.7
    Change setup type to setuptools and use proper license file.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-pysocks/python-pysocks.mk
+++ package/python-pysocks/python-pysocks.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_PYSOCKS_VERSION = 1.6.6
+PYTHON_PYSOCKS_VERSION = 1.6.7
 PYTHON_PYSOCKS_SOURCE = PySocks-$(PYTHON_PYSOCKS_VERSION).tar.gz
-PYTHON_PYSOCKS_SITE = https://pypi.python.org/packages/fd/70/ba9982cedc9b3ed3c06934f1f46a609e0f23c7bfdf567c52a09f1296b8cb
+PYTHON_PYSOCKS_SITE = https://pypi.python.org/packages/7d/38/edca891ce16827a1de45cc347e4b6c22311eba25838b9825a5e6c48cf560
 PYTHON_PYSOCKS_LICENSE = BSD-3-Clause
-PYTHON_PYSOCKS_LICENSE_FILES = socks.py
-PYTHON_PYSOCKS_SETUP_TYPE = distutils
+PYTHON_PYSOCKS_LICENSE_FILES = LICENSE
+PYTHON_PYSOCKS_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
