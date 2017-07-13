476ccdb3e49be2349248385dbacf4a72efebe343
Martin Bark <martin@barkynet.com>
2017-07-13 13:24:31 +0100

    package/nodejs: security bump to version 8.1.4
    Fixes CVE-2017-1000381 - The c-ares function ares_parse_naptr_reply(), which
    is used for parsing NAPTR responses, could be triggered to read memory
    outside of the given input buffer if the passed in DNS response packet was
    crafted in a particular way.  This patch checks that there is enough data
    for the required elements of an NAPTR record (2 int16, 3 bytes for string
    lengths) before processing a record.
    
    See https://nodejs.org/en/blog/release/v8.1.4/
    
    [Peter: add CVE info]
    Signed-off-by: Martin Bark <martin@barkynet.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/nodejs/nodejs.mk
+++ package/nodejs/nodejs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NODEJS_VERSION = 8.1.2
+NODEJS_VERSION = 8.1.4
 NODEJS_SOURCE = node-v$(NODEJS_VERSION).tar.xz
 NODEJS_SITE = http://nodejs.org/dist/v$(NODEJS_VERSION)
 NODEJS_DEPENDENCIES = host-python host-nodejs zlib \
