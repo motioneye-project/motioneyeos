f5f3eb0bc229893de883fdd8863851e68a1f1be6
Peter Korsgaard <peter@korsgaard.com>
2017-07-14 11:20:32 +0200

    nginx-upload: bump version to fix build against nginx-1.12.x
    Fixes:
    http://autobuild.buildroot.net/results/9ec/9ecf714667736bf61f643ad55588d3d6ebdde603/
    http://autobuild.buildroot.net/results/cea/cea64e30e98543e10c8819f6337babfec6d7ac0f/
    
    Bump to the latest commit on the 2.255 branch, which contains pull request
    88 (work with latest nginx versions):
    
    https://github.com/vkholodkov/nginx-upload-module/pull/88
    
    git shortlog aba1e3f34c754551f4f49e572bc86863d535609d..
    Anton (1):
          Add files via upload
    
    Valery Kholodkov (5):
          Added tag for version 2.0.8
          Recreated tag for version 2.0.8
          Backported to nginx 0.5.37 by Anthony Kholodkov
          Updated Changelog
          Merge pull request #88 from antonbarinov/2.255
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/nginx-upload/nginx-upload.mk
+++ package/nginx-upload/nginx-upload.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NGINX_UPLOAD_VERSION = aba1e3f34c754551f4f49e572bc86863d535609d
+NGINX_UPLOAD_VERSION = 70bee48f1811eecd255ed094ce9f0fb560c390c3
 NGINX_UPLOAD_SITE = $(call github,vkholodkov,nginx-upload-module,$(NGINX_UPLOAD_VERSION))
 NGINX_UPLOAD_LICENSE = BSD-3-Clause
 NGINX_UPLOAD_LICENSE_FILES = LICENCE
