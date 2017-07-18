11f1ec7bc0c89d6526e52aac776eb1a5c97094bb
Gary Bisson <gary.bisson@boundarydevices.com>
2017-07-18 14:58:00 +0200

    kernel-module-imx-gpu-viv: bump revision to a05d9b23
    Changelog since previous revision:
    6a01dfb fix for memory leak in gc_hal_kernel_os.c
    f73c35b Add support for PREEMPT_RT kernels
    
    Also updating the help text to make sure users know this module won't
    build with Mainline kernel but only with NXP forks.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/freescale-imx/kernel-module-imx-gpu-viv/kernel-module-imx-gpu-viv.mk
+++ package/freescale-imx/kernel-module-imx-gpu-viv/kernel-module-imx-gpu-viv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KERNEL_MODULE_IMX_GPU_VIV_VERSION = bfa78b095a892705a09afe9514fc5d236b3124d2
+KERNEL_MODULE_IMX_GPU_VIV_VERSION = a05d9b23b9902f6ce87d23772de2fdb2ecfb37a7
 KERNEL_MODULE_IMX_GPU_VIV_SITE = \
 	$(call github,Freescale,kernel-module-imx-gpu-viv,$(KERNEL_MODULE_IMX_GPU_VIV_VERSION))
 KERNEL_MODULE_IMX_GPU_VIV_LICENSE = GPL-2.0
