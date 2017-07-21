e1708fa00f071168627eed0cc7c791528ecb0435
Yegor Yefremov <yegorslists@googlemail.com>
2017-07-21 10:12:26 +0200

    python-hyperlink: bump to version 17.3.0
    Add license file.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-hyperlink/python-hyperlink.mk
+++ package/python-hyperlink/python-hyperlink.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-PYTHON_HYPERLINK_VERSION = 17.2.1
+PYTHON_HYPERLINK_VERSION = 17.3.0
 PYTHON_HYPERLINK_SOURCE = hyperlink-$(PYTHON_HYPERLINK_VERSION).tar.gz
-PYTHON_HYPERLINK_SITE = https://pypi.python.org/packages/a2/d9/56b6a007a643d6511e616a2be74f67c3703e2aea4e9eaa44bdf48bc78c82
+PYTHON_HYPERLINK_SITE = https://pypi.python.org/packages/61/9c/69aa5d6942271961ad1fff910db77706623423d054ecb647da963efdf49a
 PYTHON_HYPERLINK_SETUP_TYPE = setuptools
 PYTHON_HYPERLINK_LICENSE = MIT
+PYTHON_HYPERLINK_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
