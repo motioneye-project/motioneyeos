a92b4489316805f1ed916dad4c162debfe595be8
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-07-24 20:36:21 +0200

    package/kbd: bump version to 2.0.4
    Removed patches applied upstream
    
    0001-add-configure-flag-to-disable-tests.patch
    https://git.kernel.org/pub/scm/linux/kernel/git/legion/kbd.git/commit/?id=baeb5aa827d956bd06492775dc5bd9f89d394149
    
    0002-Link-against-libintl-when-needed.patch
    https://git.kernel.org/pub/scm/linux/kernel/git/legion/kbd.git/commit/?id=b08caa2e3db53ea4263d0236c83a0df11a40a569
    
    Autoreconf is not necessary anymore.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/kbd/kbd.mk
+++ package/kbd/kbd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KBD_VERSION = 2.0.3
+KBD_VERSION = 2.0.4
 KBD_SOURCE = kbd-$(KBD_VERSION).tar.xz
 KBD_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kbd
 KBD_CONF_OPTS = \
@@ -15,7 +15,6 @@ KBD_DEPENDENCIES = \
 	host-pkgconf
 KBD_LICENSE = GPL-2.0+
 KBD_LICENSE_FILES = COPYING
-KBD_AUTORECONF = YES
 
 KBD_INSTALL_TARGET_OPTS = MKINSTALLDIRS=$(@D)/config/mkinstalldirs DESTDIR=$(TARGET_DIR) install
 
