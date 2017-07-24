44f5f1d6a1b87e808ecced5ad73dc7f06e484ee2
Peter Seiderer <ps.report@gmx.net>
2017-07-24 21:46:25 +0200

    gstreamer1: bump version to 1.12.2
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gstreamer1/gstreamer1/gstreamer1.mk
+++ package/gstreamer1/gstreamer1/gstreamer1.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GSTREAMER1_VERSION = 1.12.1
+GSTREAMER1_VERSION = 1.12.2
 GSTREAMER1_SOURCE = gstreamer-$(GSTREAMER1_VERSION).tar.xz
 GSTREAMER1_SITE = https://gstreamer.freedesktop.org/src/gstreamer
 GSTREAMER1_INSTALL_STAGING = YES
