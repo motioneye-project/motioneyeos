7c31aafcd4fe28f244410fc6e64f7d3f73b4c400
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-07-24 20:30:47 +0200

    package/jsoncpp: bump version to 1.8.1
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/jsoncpp/jsoncpp.mk
+++ package/jsoncpp/jsoncpp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-JSONCPP_VERSION = 1.8.0
+JSONCPP_VERSION = 1.8.1
 JSONCPP_SITE = $(call github,open-source-parsers,jsoncpp,$(JSONCPP_VERSION))
 JSONCPP_LICENSE = Public Domain or MIT
 JSONCPP_LICENSE_FILES = LICENSE
