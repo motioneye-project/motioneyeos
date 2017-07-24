f97e0b1a929aae11aa72889223570742244764c8
Peter Seiderer <ps.report@gmx.net>
2017-07-24 22:14:41 +0200

    orc: bump version to 0.4.27
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/orc/orc.mk
+++ package/orc/orc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ORC_VERSION = 0.4.26
+ORC_VERSION = 0.4.27
 ORC_SOURCE = orc-$(ORC_VERSION).tar.xz
 ORC_SITE = http://gstreamer.freedesktop.org/data/src/orc
 ORC_LICENSE = BSD-2-Clause, BSD-3-Clause
