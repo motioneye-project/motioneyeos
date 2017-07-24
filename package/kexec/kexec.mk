7c37d2631987d7b05dc040aee6406f893fa9d30e
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-07-24 20:40:16 +0200

    package/kexec: bump version to 2.0.15
    Removed patch applied upstream:
    
    0001-vmcore-dmesg-Define-_GNU_SOURCE.patch
    https://git.kernel.org/pub/scm/utils/kernel/kexec/kexec-tools.git/commit/?id=2f6f6d6fef7872647cacc6741ac35ac2b4df7ed5
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/kexec/kexec.mk
+++ package/kexec/kexec.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KEXEC_VERSION = 2.0.14
+KEXEC_VERSION = 2.0.15
 KEXEC_SOURCE = kexec-tools-$(KEXEC_VERSION).tar.xz
 KEXEC_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kernel/kexec
 KEXEC_LICENSE = GPL-2.0
