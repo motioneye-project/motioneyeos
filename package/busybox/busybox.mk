8cea29361770bd740b9799ac9b0b09ec131d7117
Adam Duskett <Aduskett@gmail.com>
2017-07-28 12:18:08 -0400

    busybox: bump version to 1.27.1
    In addition, update busybox-minimal.config and busybox.config by loading the
    config files and saving them back.
    
    Signed-off-by: Adam Duskett <aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/busybox/busybox.mk
+++ package/busybox/busybox.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BUSYBOX_VERSION = 1.26.2
+BUSYBOX_VERSION = 1.27.1
 BUSYBOX_SITE = http://www.busybox.net/downloads
 BUSYBOX_SOURCE = busybox-$(BUSYBOX_VERSION).tar.bz2
 BUSYBOX_LICENSE = GPL-2.0
