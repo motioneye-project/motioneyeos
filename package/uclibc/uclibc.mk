69895cfc0a7b28042ae78e21abdf435947e68f3f
Waldemar Brodkorb <wbx@openadk.org>
2017-07-29 04:56:20 +0200

    uclibc: update to 1.0.26
    Both patches applied upstream.
    The release contains mostly bugfixes and a basic sparc64
    port. As the sparc64 port only contains support for static
    binaries, it will be not enabled in buildroot yet.
    
    Signed-off-by: Waldemar Brodkorb <wbx@openadk.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/uclibc/uclibc.mk
+++ package/uclibc/uclibc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UCLIBC_VERSION = 1.0.25
+UCLIBC_VERSION = 1.0.26
 UCLIBC_SOURCE = uClibc-ng-$(UCLIBC_VERSION).tar.xz
 UCLIBC_SITE = http://downloads.uclibc-ng.org/releases/$(UCLIBC_VERSION)
 UCLIBC_LICENSE = LGPL-2.1+
