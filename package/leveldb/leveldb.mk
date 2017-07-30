61830bb0d4b10821e59fb4ea93f55a98280d3f82
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-07-30 18:09:10 +0200

    package/leveldb: bump version to 1.20
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/leveldb/leveldb.mk
+++ package/leveldb/leveldb.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LEVELDB_VERSION = v1.19
+LEVELDB_VERSION = v1.20
 LEVELDB_SITE = $(call github,google,leveldb,$(LEVELDB_VERSION))
 LEVELDB_LICENSE = BSD-3-Clause
 LEVELDB_LICENSE_FILES = LICENSE
