b56083099f113817edc6811e2cdd820df1c80450
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-07-30 18:05:08 +0200

    package/linknx: bump version to 0.0.1.33
    Switch _SITE to github:
    https://github.com/linknx/linknx/commit/80264a26318820e5864196aefe3af093071b90b8
    
    Mysql support was fixed with
    https://github.com/linknx/linknx/commit/83d1d88dc4cec422e13e0c2baa34a828a3f67270
    there 0001-fix-static-build-with-mysql.patch can be removed.
    
    Build-tested using this defconfig
    http://autobuild.buildroot.net/results/2df/2dfcbd30079d13997de37e2d1c4643653e200be6
    mentioned in
    https://git.buildroot.net/buildroot/commit/package/linknx?id=f35f54585f9e63400bdbd9c9ba2b9fd67be538ea
    
    After removing the mysql patch autoreconf is not needed anymore,
    therefore we can remove the libcurl dependency which was only added to
    fix autoreconf:
    https://git.buildroot.net/buildroot/commit/package/linknx?id=e1c9a2349006e657e76dff35706a774376921fb7
    
    LINKNX_CREATE_MISSING_FILES is also not needed anymore after commit
    https://github.com/linknx/linknx/commit/ef04c6d12846800fa1554d2fe31813dd09c5f29b
    
    Renumbered remaining patch.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/linknx/linknx.mk
+++ package/linknx/linknx.mk
@@ -4,31 +4,22 @@
 #
 ################################################################################
 
-LINKNX_VERSION = 0.0.1.32
-LINKNX_SITE = http://downloads.sourceforge.net/project/linknx/linknx/linknx-$(LINKNX_VERSION)
+LINKNX_VERSION = 0.0.1.33
+LINKNX_SITE = $(call github,linknx,linknx,$(LINKNX_VERSION))
 LINKNX_LICENSE = GPL-2.0+
 LINKNX_INSTALL_STAGING = YES
-# Patching acinclude.m4
-LINKNX_AUTORECONF = YES
 LINKNX_CONF_OPTS = \
 	--without-lua \
 	--without-log4cpp \
 	--without-pth-test \
 	--with-pth=$(STAGING_DIR)/usr \
-	--disable-smtp \
-	--with-libcurl=$(STAGING_DIR)/usr/bin/curl-config
+	--disable-smtp
 
-LINKNX_DEPENDENCIES = libpthsem libcurl \
+LINKNX_DEPENDENCIES = libpthsem \
 	$(if $(BR2_PACKAGE_ARGP_STANDALONE),argp-standalone)
 
-# This is needed to make autoreconf happy
-define LINKNX_CREATE_MISSING_FILES
-	touch $(@D)/NEWS $(@D)/AUTHORS $(@D)/README
-endef
-LINKNX_POST_EXTRACT_HOOKS += LINKNX_CREATE_MISSING_FILES
-
 ifeq ($(BR2_PACKAGE_MYSQL),y)
-LINKNX_CONF_OPTS += --with-mysql=$(STAGING_DIR)/usr/bin/mysql_config
+LINKNX_CONF_OPTS += --with-mysql=$(STAGING_DIR)/usr
 LINKNX_DEPENDENCIES += mysql
 else
 LINKNX_CONF_OPTS += --without-mysql
