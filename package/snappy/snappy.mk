662b830dd702f085bc0473a469e893ec9e59149e
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-07-30 16:47:30 +0200

    package/snappy: fix typo
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/snappy/snappy.mk
+++ package/snappy/snappy.mk
@@ -17,7 +17,7 @@ SNAPPY_INSTALL_STAGING = YES
 SNAPPY_CONF_OPTS = --disable-gtest
 
 # libsnappy links with libstdc++. Some libstdc++/arch variants use
-# pthread symbols for internal locking if built with thead
+# pthread symbols for internal locking if built with thread
 # support. libstdc++ does not have a .pc file, and its .la file does
 # not mention -pthread.  So, static linkig to libstdc++ will fail if
 # -pthread is not explicity linked to. Only do that for static builds.
