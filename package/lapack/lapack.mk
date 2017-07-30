b63c29ad209b3e7b47f6e85a7a8e5b0154bbb990
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-07-30 17:08:01 +0200

    package/lapack: bump version to 3.7.1
    lapack does not support in-source builds anymore:
    https://github.com/Reference-LAPACK/lapack/commit/ea081254b4a0538f81c74bdb8338b29a068303aa
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lapack/lapack.mk
+++ package/lapack/lapack.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-LAPACK_VERSION = 3.6.1
+LAPACK_VERSION = 3.7.1
 LAPACK_SOURCE = lapack-$(LAPACK_VERSION).tgz
 LAPACK_LICENSE = BSD-3-Clause
 LAPACK_LICENSE_FILES = LICENSE
 LAPACK_SITE = http://www.netlib.org/lapack
 LAPACK_INSTALL_STAGING = YES
+LAPACK_SUPPORTS_IN_SOURCE_BUILD = NO
 LAPACK_CONF_OPTS = -DLAPACKE=ON -DCBLAS=ON
 
 ifeq ($(BR2_PACKAGE_LAPACK_COMPLEX),y)
