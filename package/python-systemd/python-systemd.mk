cdb1117be0b28cfd1060a0ffb1fba3c6aa8890a1
Adam Duskett <Aduskett@gmail.com>
2017-07-31 10:22:10 -0400

    python-systemd: bump to version 234
    Signed-off-by: Adam Duskett <aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-systemd/python-systemd.mk
+++ package/python-systemd/python-systemd.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SYSTEMD_VERSION = 233 # Should be kept in sync with $(SYSTEMD_VERSION)
+PYTHON_SYSTEMD_VERSION = 234 # Should be kept in sync with $(SYSTEMD_VERSION)
 PYTHON_SYSTEMD_SOURCE = systemd-python-$(PYTHON_SYSTEMD_VERSION).tar.gz
-PYTHON_SYSTEMD_SITE = https://pypi.python.org/packages/10/89/d66ae73bdbc2614e4f2e90ddf9ef80d22c28f3cd70071345c3640465c14c
+PYTHON_SYSTEMD_SITE = https://pypi.python.org/packages/e8/a8/00ba0f605837a8f69523e6c3a4fb14675a6430c163f836540129c50b3aef
 PYTHON_SYSTEMD_SETUP_TYPE = distutils
 PYTHON_SYSTEMD_LICENSE = LGPL-2.1
 PYTHON_SYSTEMD_LICENSE_FILES = LICENSE.txt
