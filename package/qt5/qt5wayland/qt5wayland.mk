4073fd0377c0df1941550a24191000afcf4ee63b
Joshua Henderson <joshua.henderson@microchip.com>
2017-07-31 14:06:48 -0700

    qt5wayland: new package
    QtWayland is a Qt 5 module that wraps the functionality of Wayland.
    
    Cc: Julien Corjon <corjon.j@ecagroup.com>
    Cc: Brent Sink <brents_3@hotmail.com>
    Cc: Naoki Matsumoto <n-matsumoto@melcoinc.co.jp>
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>
    [Arnout:
     - select wayland instead of depending on it;
     - propagate jscore dependency from qt5declarative
     - re-order select/depends statements
     - rewrite comment to only refer to GL backend (jscore is an arch
       dependency)
     - rewrap help text of BR2_PACKAGE_QT5WAYLAND_COMPOSITOR]
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

---
+++ package/qt5/qt5wayland/qt5wayland.mk
@@ -0,0 +1,61 @@
+################################################################################
+#
+# qt5wayland
+#
+################################################################################
+
+QT5WAYLAND_VERSION = $(QT5_VERSION)
+QT5WAYLAND_SITE = $(QT5_SITE)
+QT5WAYLAND_SOURCE = qtwayland-opensource-src-$(QT5WAYLAND_VERSION).tar.xz
+QT5WAYLAND_DEPENDENCIES = qt5base qt5declarative wayland
+QT5WAYLAND_INSTALL_STAGING = YES
+
+ifeq ($(BR2_PACKAGE_LIBXKBCOMMON),y)
+QT5WAYLAND_DEPENDENCIES += libxkbcommon
+endif
+
+ifeq ($(BR2_PACKAGE_QT5_VERSION_LATEST),y)
+QT5WAYLAND_LICENSE = GPL-2.0+ or LGPL-3.0, GPL-3.0 with exception(tools), GFDL-1.3 (docs)
+QT5WAYLAND_LICENSE_FILES = LICENSE.GPL2 LICENSE.GPL3 LICENSE.GPL3-EXCEPT LICENSE.LGPL3 LICENSE.FDL
+else
+QT5WAYLAND_LICENSE = GPL-3.0 or LGPL-2.1 with exception or LGPL-3.0, GFDL-1.3 (docs)
+QT5WAYLAND_LICENSE_FILES = LICENSE.GPLv3 LICENSE.LGPLv21 LGPL_EXCEPTION.txt LICENSE.LGPLv3 LICENSE.FDL
+endif
+
+ifeq ($(BR2_PACKAGE_QT5WAYLAND_COMPOSITOR),y)
+QT5WAYLAND_QMAKEFLAGS += CONFIG+=wayland-compositor
+endif
+
+define QT5WAYLAND_CONFIGURE_CMDS
+	(cd $(@D); $(TARGET_MAKE_ENV) $(HOST_DIR)/bin/qmake $(QT5WAYLAND_QMAKEFLAGS))
+endef
+
+define QT5WAYLAND_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define QT5WAYLAND_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) install
+	$(QT5_LA_PRL_FILES_FIXUP)
+endef
+
+ifeq ($(BR2_PACKAGE_QT5WAYLAND_COMPOSITOR),y)
+ifeq ($(BR2_PACKAGE_QT5_VERSION_LATEST),y)
+define QT5WAYLAND_INSTALL_COMPOSITOR
+	cp -dpf $(STAGING_DIR)/usr/lib/libQt5WaylandCompositor.so* $(TARGET_DIR)/usr/lib
+endef
+else
+define QT5WAYLAND_INSTALL_COMPOSITOR
+	cp -dpf $(STAGING_DIR)/usr/lib/libQt5Compositor.so* $(TARGET_DIR)/usr/lib
+endef
+endif
+endif
+
+define QT5WAYLAND_INSTALL_TARGET_CMDS
+	cp -dpf $(STAGING_DIR)/usr/lib/libQt5WaylandClient.so* $(TARGET_DIR)/usr/lib
+	cp -dpfr $(STAGING_DIR)/usr/lib/qt/plugins/wayland* $(TARGET_DIR)/usr/lib/qt/plugins
+	cp -dpfr $(STAGING_DIR)/usr/lib/qt/plugins/platforms/libqwayland* $(TARGET_DIR)/usr/lib/qt/plugins/platforms
+	$(QT5WAYLAND_INSTALL_COMPOSITOR)
+endef
+
+$(eval $(generic-package))
