2a3f6e7b1eaebb274f044657c3c9b122301c487c
Joo Aun Saw <jasaw@dius.com.au>
2017-07-31 12:12:01 +1000

    motion: upgrade to latest master

--- package/motion/motion.mk
+++ package/motion/motion.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MOTION_VERSION = 9fa9d22473899f654922174db0c474e14738a833
+MOTION_VERSION = e9e0a2d29205956055dd5ba7ca0cb52b585f182a
 MOTION_SITE = $(call github,motion-project,motion,$(MOTION_VERSION))
 MOTION_AUTORECONF = YES
 MOTION_CONF_OPTS = --without-pgsql --without-sdl --without-sqlite3 --without-mysql --with-ffmpeg=$(STAGING_DIR)/usr/lib \
