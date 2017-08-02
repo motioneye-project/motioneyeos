e40f8cf77cacdfd5027199ebe8584c33c16b9d3f
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-08-02 19:08:50 +0200

    package/tor: bump version to 0.3.0.10
    Release notes: https://blog.torproject.org/blog/tor-03010-released
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/tor/tor.mk
+++ package/tor/tor.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TOR_VERSION = 0.3.0.9
+TOR_VERSION = 0.3.0.10
 TOR_SITE = https://dist.torproject.org
 TOR_LICENSE = BSD-3-Clause
 TOR_LICENSE_FILES = LICENSE
