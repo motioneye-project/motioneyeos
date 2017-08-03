dcfdf82e536cab7cd94ad057b25bc392e985de9f
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-08-03 23:58:12 +0200

    gdb: drop support for Microblaze
    Microblaze support was never upstreamed in gdb. We currently use a gdb
    from the Xilinx Github repository, but this gdb is based on 7.6, and
    has never been updated in the last 4 years. There are no other active
    branches at https://github.com/Xilinx/gdb/branches.
    
    Xilinx has a slightly newer gdb, based on 7.7, available at
    https://github.com/Xilinx/meta-xilinx/tree/master/recipes-microblaze/gdb. However,
    it's apparently only available in the form of stack of big patches
    (https://github.com/Xilinx/meta-xilinx/tree/master/recipes-microblaze/gdb/files).
    
    Currently, gdb on Microblaze fails to build in various conditions:
    
     * Against glibc, with "error: conflicting types for 'ps_lgetfpregs'"
    
     * Against musl, with "error: unknown type name 'elf_gregset_t'"
    
    While those issues can probably be fixed, the fact that there is no
    active upstream significantly reduces the incentive to fix those
    problems.
    
    Therefore, let's drop support for gdb on Microblaze entirely.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Acked-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Acked-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/gdb/gdb.mk
+++ package/gdb/gdb.mk
@@ -14,12 +14,6 @@ GDB_SOURCE = gdb-$(GDB_VERSION).tar.gz
 GDB_FROM_GIT = y
 endif
 
-ifeq ($(BR2_microblaze),y)
-GDB_SITE = $(call github,Xilinx,gdb,$(GDB_VERSION))
-GDB_SOURCE = gdb-$(GDB_VERSION).tar.gz
-GDB_FROM_GIT = y
-endif
-
 GDB_LICENSE = GPL-2.0+, LGPL-2.0+, GPL-3.0+, LGPL-3.0+
 GDB_LICENSE_FILES = COPYING COPYING.LIB COPYING3 COPYING3.LIB
 
