1e2c245bf4f21e9da41a60856cfa50eff5671e19
Baruch Siach <baruch@tkos.co.il>
2017-08-09 08:02:02 +0300

    faad2: security bump to version 2.8.1
    Fixes: CVE-2017-9218, CVE-2017-9219, CVE-2017-9220, CVE-2017-9221,
    CVE-2017-9222, CVE-2017-9223, CVE-2017-9253, CVE-2017-9254,
    CVE-2017-9255, CVE-2017-9256, CVE-2017-9257
    
    http://seclists.org/fulldisclosure/2017/Jun/32
    
    Switch to .tar.bz2 to save some bandwidth.
    
    Add autoreconf since unfortunately upstream tarball does not ship the
    configure script.
    
    Cc: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/faad2/faad2.mk
+++ package/faad2/faad2.mk
@@ -4,10 +4,14 @@
 #
 ################################################################################
 
-FAAD2_VERSION = 2.7
-FAAD2_SITE = http://downloads.sourceforge.net/project/faac/faad2-src/faad2-$(FAAD2_VERSION)
+FAAD2_VERSION_MAJOR = 2.8
+FAAD2_VERSION = $(FAAD2_VERSION_MAJOR).1
+FAAD2_SITE = http://downloads.sourceforge.net/project/faac/faad2-src/faad2-$(FAAD2_VERSION_MAJOR).0
+FAAD2_SOURCE = faad2-$(FAAD2_VERSION).tar.bz2
 FAAD2_LICENSE = GPL-2.0
 FAAD2_LICENSE_FILES = COPYING
+# No configure script in upstream tarball
+FAAD2_AUTORECONF = YES
 # frontend/faad calls frexp()
 FAAD2_CONF_ENV = LIBS=-lm
 FAAD2_INSTALL_STAGING = YES
