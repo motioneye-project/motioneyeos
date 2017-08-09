b0a7ac88739214d5d83c101c24b700d9534e831a
Baruch Siach <baruch@tkos.co.il>
2017-08-09 08:39:49 +0300

    expat: bump to version 2.2.3
    Drop the XML_POOR_ENTROPY workaround. Upstream commit fd9581a34e5665
    (Never require XML_POOR_ENTROPY for "./configure && make") fixes this
    issue.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/expat/expat.mk
+++ package/expat/expat.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EXPAT_VERSION = 2.2.2
+EXPAT_VERSION = 2.2.3
 EXPAT_SITE = http://downloads.sourceforge.net/project/expat/expat/$(EXPAT_VERSION)
 EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.bz2
 EXPAT_INSTALL_STAGING = YES
@@ -14,15 +14,6 @@ EXPAT_DEPENDENCIES = host-pkgconf
 HOST_EXPAT_DEPENDENCIES = host-pkgconf
 EXPAT_LICENSE = MIT
 EXPAT_LICENSE_FILES = COPYING
-
-# Kernel versions older than 3.17 do not support getrandom()
-ifeq ($(BR2_TOOLCHAIN_HEADERS_AT_LEAST_3_17),)
-EXPAT_CONF_ENV += CPPFLAGS="$(TARGET_CPPFLAGS) -DXML_POOR_ENTROPY"
-endif
-
-# Make build succeed on host kernel older than 3.17. getrandom() will still
-# be used on newer kernels.
-HOST_EXPAT_CONF_ENV += CPPFLAGS="$(HOST_CPPFLAGS) -DXML_POOR_ENTROPY"
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
