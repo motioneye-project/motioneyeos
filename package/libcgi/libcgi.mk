14b0118216e5b3e67556703fa214c33944c75f9b
Joe Lin <xlcwu.taiwan@gmail.com>
2017-08-10 07:45:27 +0000

    libcgi: bump to version 1.1
    Cleanup the libcgi package,
    by using https://github.com/rafaelsteil/libcgi as an upstream.
    Remove old patch and use cmake to build new version.
    
    Signed-off-by: Joe Lin <xlcwu.taiwan@gmail.com>
    [Mario:
    - Use github URL in Config.in
    - Use upstream sha256 hash]
    Signed-off-by: Mario J. Rugiero <mrugiero@gmail.com>
    [Arnout:
    - Add reference to upstream hash file
    - Remove unneeded LIBCGI_SUPPORTS_IN_SOURCE_BUILD = NO]
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/libcgi/libcgi.mk
+++ package/libcgi/libcgi.mk
@@ -4,12 +4,10 @@
 #
 ################################################################################
 
-LIBCGI_VERSION = 1.0
-LIBCGI_SITE = http://downloads.sourceforge.net/project/libcgi/libcgi/$(LIBCGI_VERSION)
+LIBCGI_VERSION = 1.1
+LIBCGI_SITE = https://github.com/rafaelsteil/libcgi/releases/download/v$(LIBCGI_VERSION)
+
 LIBCGI_INSTALL_STAGING = YES
-# use cross CC/AR rather than host
-LIBCGI_MAKE_ENV = CC="$(TARGET_CC) $(TARGET_CFLAGS)" AR="$(TARGET_AR)" \
-	$(if $(BR2_STATIC_LIBS),STATIC=1)
 LIBCGI_LICENSE = LGPL-2.1+
 
-$(eval $(autotools-package))
+$(eval $(cmake-package))
