968fd3d8f95e61f02bb3e1a2216e2190cffa5357
Angelo Compagnucci <angelo.compagnucci@gmail.com>
2017-08-11 18:00:48 +0200

    package/jq: install to staging
    Install the libraries to staging to be usable by other packages.
    
    Signed-off-by: Angelo Compagnucci <angelo.compagnucci@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/jq/jq.mk
+++ package/jq/jq.mk
@@ -8,6 +8,7 @@ JQ_VERSION = 1.5
 JQ_SITE = https://github.com/stedolan/jq/releases/download/jq-$(JQ_VERSION)
 JQ_LICENSE = MIT (code), CC-BY-3.0 (documentation)
 JQ_LICENSE_FILES = COPYING
+JQ_INSTALL_STAGING = YES
 
 # uses c99 specific features
 JQ_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -std=c99"
