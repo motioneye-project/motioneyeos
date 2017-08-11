1a04b0b519e89a2fa9f82ff81376509b8d4e6a32
Angelo Compagnucci <angelo.compagnucci@gmail.com>
2017-08-11 18:00:49 +0200

    package/sysdig: bump to version 0.17.0
    This patch bumps sysdig to version 0.17.0 and updates the
    dependencies accordingly.
    
    According to [1], the minimum GCC version is 4.8 so encode that as
    well.
    
    [1] https://www.sysdig.org/wiki/how-to-install-sysdig-from-the-source-code/
    
    Signed-off-by: Angelo Compagnucci <angelo.compagnucci@gmail.com>
    [Arnout: also bump gcc dependency to 4.8]
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/sysdig/sysdig.mk
+++ package/sysdig/sysdig.mk
@@ -4,13 +4,15 @@
 #
 ################################################################################
 
-SYSDIG_VERSION = 0.1.100
+SYSDIG_VERSION = 0.17.0
 SYSDIG_SITE = $(call github,draios,sysdig,$(SYSDIG_VERSION))
 SYSDIG_LICENSE = GPL-2.0
 SYSDIG_LICENSE_FILES = COPYING
 SYSDIG_CONF_OPTS = -DUSE_BUNDLED_LUAJIT=OFF -DUSE_BUNDLED_ZLIB=OFF \
-	-DUSE_BUNDLED_JSONCPP=OFF -DENABLE_DKMS=OFF
-SYSDIG_DEPENDENCIES = zlib luajit jsoncpp
+	-DUSE_BUNDLED_JSONCPP=OFF -DENABLE_DKMS=OFF -DUSE_BUNDLED_CURL=OFF \
+	-DUSE_BUNDLED_NCURSES=OFF -DUSE_BUNDLED_OPENSSL=OFF \
+	-DUSE_BUNDLED_JQ=OFF -DUSE_BUNDLED_B64=OFF
+SYSDIG_DEPENDENCIES = zlib luajit jsoncpp libcurl ncurses openssl jq libb64
 SYSDIG_SUPPORTS_IN_SOURCE_BUILD = NO
 
 # sysdig creates the module Makefile from a template, which contains a
