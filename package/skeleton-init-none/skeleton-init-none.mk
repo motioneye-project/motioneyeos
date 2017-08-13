f4a8ba8c8be2a471a2bb9d42961f90438b1056b3
Cam Hutchison <camh@xdna.net>
2017-08-14 07:21:09 +1000

    skeleton: Rename skeleton-none to skeleton-init-none
    The skeletons are based on the selection of BR2_INIT_*, so add init- to
    the package name to make this clearer. The name skeleton-none implies no
    skeleton at all, not a base skeleton with no init-specific files.
    
    Signed-off-by: Cam Hutchison <camh@xdna.net>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/skeleton-init-none/skeleton-init-none.mk
+++ package/skeleton-init-none/skeleton-init-none.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #
-# skeleton-none
+# skeleton-init-none
 #
 ################################################################################
 
@@ -8,11 +8,11 @@
 # skeleton and the toolchain is a target package, as is skeleton.
 # Hence, skeleton would depends on the toolchain and the toolchain would depend
 # on skeleton.
-SKELETON_NONE_ADD_TOOLCHAIN_DEPENDENCY = NO
-SKELETON_NONE_ADD_SKELETON_DEPENDENCY = NO
+SKELETON_INIT_NONE_ADD_TOOLCHAIN_DEPENDENCY = NO
+SKELETON_INIT_NONE_ADD_SKELETON_DEPENDENCY = NO
 
-SKELETON_NONE_DEPENDENCIES = skeleton-common
+SKELETON_INIT_NONE_DEPENDENCIES = skeleton-common
 
-SKELETON_NONE_PROVIDES = skeleton
+SKELETON_INIT_NONE_PROVIDES = skeleton
 
 $(eval $(generic-package))
