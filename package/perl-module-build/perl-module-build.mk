68fb9044be4f380504ff8fa40720f852060a336c
Francois Perrad <fperrad@gmail.com>
2017-08-20 10:03:20 +0200

    perl-module-build: bump to version 0.4224
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/perl-module-build/perl-module-build.mk
+++ package/perl-module-build/perl-module-build.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_MODULE_BUILD_VERSION = 0.4220
+PERL_MODULE_BUILD_VERSION = 0.4224
 PERL_MODULE_BUILD_SOURCE = Module-Build-$(PERL_MODULE_BUILD_VERSION).tar.gz
 PERL_MODULE_BUILD_SITE = $(BR2_CPAN_MIRROR)/authors/id/L/LE/LEONT
 PERL_MODULE_BUILD_LICENSE = Artistic or GPL-1.0+
