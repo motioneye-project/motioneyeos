61c4c63c79e37f86adf8af0ef896624af7ed4b77
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-08-20 10:07:44 +0200

    package/lzo: switch to cmake
    autoreconf for the host package is broken after bumping to 2.10:
    http://lists.busybox.net/pipermail/buildroot/2017-August/200634.html
    
    To solve the problem we switch to cmake.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Tested-by: Ricardo Martincoski <ricardo.martincoski@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lzo/lzo.mk
+++ package/lzo/lzo.mk
@@ -9,9 +9,7 @@ LZO_SITE = http://www.oberhumer.com/opensource/lzo/download
 LZO_LICENSE = GPL-2.0+
 LZO_LICENSE_FILES = COPYING
 LZO_INSTALL_STAGING = YES
-# Ships a beta libtool version hence our patch doesn't apply.
-# Run autoreconf to regenerate ltmain.sh.
-LZO_AUTORECONF = YES
+LZO_SUPPORTS_IN_SOURCE_BUILD = NO
 
-$(eval $(autotools-package))
-$(eval $(host-autotools-package))
+$(eval $(cmake-package))
+$(eval $(host-cmake-package))
