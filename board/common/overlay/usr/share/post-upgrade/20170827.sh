b676e2ba9d5020795ac8f6c00d281ad9d032dfd7
Calin Crisan <ccrisan@gmail.com>
2017-08-27 15:32:13 +0300

    20170827 post upgrade script: create /data/varlib directory

--- board/common/overlay/usr/share/post-upgrade/20170827.sh
+++ board/common/overlay/usr/share/post-upgrade/20170827.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# create the persisted /var/lib directory (/data/varlib)
+mkdir -p /data/varlib
+
 # add password_hook motioneye setting
 grep password_hook /data/etc/motioneye.conf &>/dev/null || echo -e 'password_hook /usr/libexec/meyepasswd' >> /data/etc/motioneye.conf
 
