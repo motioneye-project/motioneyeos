1b191d64d381a08d4bff4e004b273b4b0e89bc06
Baruch Siach <baruch@tkos.co.il>
2017-08-28 08:34:16 +0300

    libftdi1: bump to version 1.4
    Cc: Daniel Sangue <daniel.sangue@sangue.ch>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libftdi1/libftdi1.mk
+++ package/libftdi1/libftdi1.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBFTDI1_VERSION = 1.3
+LIBFTDI1_VERSION = 1.4
 LIBFTDI1_SOURCE = libftdi1-$(LIBFTDI1_VERSION).tar.bz2
 LIBFTDI1_SITE = http://www.intra2net.com/en/developer/libftdi/download
 LIBFTDI1_INSTALL_STAGING = YES
