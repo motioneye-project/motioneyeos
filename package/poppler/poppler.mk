1c5385541a2147241535979ee4ba3f452ceff150
Olivier Schonken <olivier.schonken@gmail.com>
2017-08-28 15:53:24 +0200

    poppler: bump version to 0.57.0
    Signed-off-by: Olivier Schonken <olivier.schonken@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/poppler/poppler.mk
+++ package/poppler/poppler.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-POPPLER_VERSION = 0.55.0
+POPPLER_VERSION = 0.57.0
 POPPLER_SOURCE = poppler-$(POPPLER_VERSION).tar.xz
 POPPLER_SITE = http://poppler.freedesktop.org
 POPPLER_DEPENDENCIES = fontconfig host-pkgconf
