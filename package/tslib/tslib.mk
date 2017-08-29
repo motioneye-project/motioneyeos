5ba888a29784c3edd54594da5f72cac60722eaa2
Martin Kepplinger <martink@posteo.de>
2017-08-29 19:02:27 +0200

    tslib: update to version 1.12
    The release page has a short changelog overview:
    
        https://github.com/kergoth/tslib/releases/tag/1.12
    
    Signed-off-by: Martin Kepplinger <martink@posteo.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/tslib/tslib.mk
+++ package/tslib/tslib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TSLIB_VERSION = 1.11
+TSLIB_VERSION = 1.12
 TSLIB_SITE = https://github.com/kergoth/tslib/releases/download/$(TSLIB_VERSION)
 TSLIB_SOURCE = tslib-$(TSLIB_VERSION).tar.xz
 TSLIB_LICENSE = GPL-2.0+ (programs), LGPL-2.1+ (libraries)
