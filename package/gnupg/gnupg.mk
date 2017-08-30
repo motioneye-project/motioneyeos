453ca1d6ad6aa3d55f44734ed8479ac5fa909d8a
Baruch Siach <baruch@tkos.co.il>
2017-08-30 15:01:04 +0300

    gnupg: security bump to version 1.4.22
    Mitigate a flush+reload side-channel attack on RSA secret keys
    dubbed "Sliding right into disaster".  For details see
    <https://eprint.iacr.org/2017/627>.  [CVE-2017-7526]
    
    Switch to https site for better firewall compatibility and security.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gnupg/gnupg.mk
+++ package/gnupg/gnupg.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-GNUPG_VERSION = 1.4.21
+GNUPG_VERSION = 1.4.22
 GNUPG_SOURCE = gnupg-$(GNUPG_VERSION).tar.bz2
-GNUPG_SITE = ftp://ftp.gnupg.org/gcrypt/gnupg
+GNUPG_SITE = https://gnupg.org/ftp/gcrypt/gnupg
 GNUPG_LICENSE = GPL-3.0+
 GNUPG_LICENSE_FILES = COPYING
 GNUPG_DEPENDENCIES = zlib ncurses $(if $(BR2_PACKAGE_LIBICONV),libiconv)
