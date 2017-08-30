a1e9fa24e12e2cb529717b24e1a6b7dbd23d7a72
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-08-30 21:28:53 +0200

    package/opus-tools: bump version to 0.1.10
    Switch upstream URL to https.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/opus-tools/opus-tools.mk
+++ package/opus-tools/opus-tools.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-OPUS_TOOLS_VERSION = 0.1.9
-OPUS_TOOLS_SITE = http://downloads.xiph.org/releases/opus
+OPUS_TOOLS_VERSION = 0.1.10
+OPUS_TOOLS_SITE = https://downloads.xiph.org/releases/opus
 OPUS_TOOLS_LICENSE = BSD-2-Clause
 OPUS_TOOLS_LICENSE_FILES = COPYING
 OPUS_TOOLS_CONF_OPTS = --disable-oggtest --disable-opustest
