72007d80d43408e195906920bcf6842dcdc60201
Baruch Siach <baruch@tkos.co.il>
2017-09-01 11:34:33 +0300

    gnupg2: drop non-wchar toolchain workaround
    gnupg2 removed gnulib in version 2.1.1. The workaround for non-wchar
    toolchains introduced in commit 8a878870958 (gnupg2: fix build on
    non-wchar toolchains), is no longer needed.
    
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Romain Naour <romain.naour@openwide.fr>
    Cc: Vicente Olivert Riera <Vincent.Riera@imgtec.com>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gnupg2/gnupg2.mk
+++ package/gnupg2/gnupg2.mk
@@ -19,7 +19,6 @@ GNUPG2_CONF_OPTS = \
 	--with-libassuan-prefix=$(STAGING_DIR)/usr \
 	--with-ksba-prefix=$(STAGING_DIR)/usr \
 	--with-npth-prefix=$(STAGING_DIR)/usr
-GNUPG2_CONF_ENV = gl_cv_header_working_stdint_h=yes
 
 ifneq ($(BR2_PACKAGE_GNUPG2_GPGV2),y)
 define GNUPG2_REMOVE_GPGV2
