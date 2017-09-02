39d98970c437decd8621b5a8dd345f5ae612d40a
Francois Perrad <fperrad@gmail.com>
2017-09-02 18:56:38 +0200

    lua-stdlib: bump to version 41.2.1
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lua-stdlib/lua-stdlib.mk
+++ package/lua-stdlib/lua-stdlib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUA_STDLIB_VERSION_UPSTREAM = 41.2.0
+LUA_STDLIB_VERSION_UPSTREAM = 41.2.1
 LUA_STDLIB_VERSION = $(LUA_STDLIB_VERSION_UPSTREAM)-1
 LUA_STDLIB_NAME_UPSTREAM = stdlib
 LUA_STDLIB_SUBDIR = lua-stdlib-release-v$(LUA_STDLIB_VERSION_UPSTREAM)
