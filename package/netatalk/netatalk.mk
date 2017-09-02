38745672c1b214ceee69bdbb24057c5c0f4936e9
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-02 19:03:13 +0200

    package/netatalk: bump version to 3.1.11
    Building 3.1.9 still fails after removing patch 0002:
    
    volume.c:383:26: error: 'ldap_config_valid' undeclared (first use in this function)
                         if (!ldap_config_valid || vol->v_flags & AFPVOL_NONETIDS)
    
    Bumping to 3.1.11 solves the problem.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/netatalk/netatalk.mk
+++ package/netatalk/netatalk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NETATALK_VERSION = 3.1.9
+NETATALK_VERSION = 3.1.11
 NETATALK_SITE = http://downloads.sourceforge.net/project/netatalk/netatalk/$(NETATALK_VERSION)
 NETATALK_SOURCE = netatalk-$(NETATALK_VERSION).tar.bz2
 # For 0001-Fix-setting-of-LD_LIBRARY_FLAGS-shlibpath_var.patch
