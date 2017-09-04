ad8d1e444cbbec276b33a1a67726b2a01c6e8516
Francois Perrad <fperrad@gmail.com>
2017-09-04 19:50:42 +0200

    rings: add LICENSE_FILES
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/rings/rings.mk
+++ package/rings/rings.mk
@@ -8,5 +8,6 @@ RINGS_VERSION_MAJOR = 1.3.0
 RINGS_VERSION = $(RINGS_VERSION_MAJOR)-1
 RINGS_SUBDIR = rings-v_$(subst .,_,$(RINGS_VERSION_MAJOR))
 RINGS_LICENSE = MIT
+RINGS_LICENSE_FILES = $(RINGS_SUBDIR)/doc/us/license.html
 
 $(eval $(luarocks-package))
