fe6afbfa03b21a45fba549ab1d8bb387a17ae21b
Francois Perrad <fperrad@gmail.com>
2017-09-05 20:52:19 +0200

    libtommath: set PREFIX when installing
    The default value is /usr/local and causes breakage in libtomcrypt & moarvm
    
    Fixes:
    
      http://autobuild.buildroot.net/results/a760063f05ae23f79920bc89ffc37e7dcf77aeab/
      http://autobuild.buildroot.net/results/78c9bc9a21cfce48615b70d80b25dd4847bd803b/
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libtommath/libtommath.mk
+++ package/libtommath/libtommath.mk
@@ -17,7 +17,7 @@ define LIBTOMMATH_BUILD_CMDS
 endef
 
 define LIBTOMMATH_INSTALL_STAGING_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR="$(STAGING_DIR)" install
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR="$(STAGING_DIR)" PREFIX=/usr install
 endef
 
 $(eval $(generic-package))
