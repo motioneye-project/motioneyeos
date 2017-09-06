028cf5897c4760de0c25bf5dc8eef58946d50d62
Sven Haardiek <sven.haardiek@greenbone.net>
2017-09-06 16:48:55 +0200

    lcdproc: bump to version 0.5.9
    This commit bumps lcdproc to version 0.5.9, and switches to the new
    upstream on github.
    
    The new version also compiles with musl without any patches.
    
    Signed-off-by: Sven Haardiek <sven.haardiek@greenbone.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/lcdproc/lcdproc.mk
+++ package/lcdproc/lcdproc.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LCDPROC_VERSION = 0.5.7
-LCDPROC_SITE = http://downloads.sourceforge.net/project/lcdproc/lcdproc/$(LCDPROC_VERSION)
+LCDPROC_VERSION = 0.5.9
+LCDPROC_SITE = https://github.com/lcdproc/lcdproc/releases/download/v$(LCDPROC_VERSION)
 LCDPROC_LICENSE = GPL-2.0+
 LCDPROC_LICENSE_FILES = COPYING
 LCDPROC_MAKE = $(MAKE1)
