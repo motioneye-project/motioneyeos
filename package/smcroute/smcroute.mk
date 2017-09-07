5b5ef4bbeeaf8ec92eb9758d3a8106f30bbb3cbb
Thomas Faivre <thomas.faivre@6wind.com>
2017-09-07 15:09:08 +0200

    smcroute: bump to version 2.3.1
    Add optional dependency on libcap, which exists since this version
    bump.
    
    Signed-off-by: Thomas Faivre <thomas.faivre@6wind.com>
    [Thomas: add explicit --with-libcap.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/smcroute/smcroute.mk
+++ package/smcroute/smcroute.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SMCROUTE_VERSION = 2.1.1
+SMCROUTE_VERSION = 2.3.1
 SMCROUTE_SOURCE = smcroute-$(SMCROUTE_VERSION).tar.xz
 SMCROUTE_SITE = https://github.com/troglobit/smcroute/releases/download/$(SMCROUTE_VERSION)
 SMCROUTE_LICENSE = GPL-2.0+
@@ -13,5 +13,12 @@ SMCROUTE_LICENSE_FILES = COPYING
 SMCROUTE_CONF_OPTS = ac_cv_func_setpgrp_void=yes
 #BUG:The package Makefile uses CC?= even though the package is autotools based
 SMCROUTE_MAKE_ENV = $(TARGET_CONFIGURE_OPTS)
+
+ifeq ($(BR2_PACKAGE_LIBCAP),y)
+SMCROUTE_DEPENDENCIES = libcap
+SMCROUTE_CONF_OPTS += --with-libcap
+else
+SMCROUTE_CONF_OPTS += --without-libcap
+endif
 
 $(eval $(autotools-package))
