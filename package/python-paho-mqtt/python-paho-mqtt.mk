30f667b7047ec46911a48d3745d4ae9c7b361909
Yegor Yefremov <yegorslists@googlemail.com>
2017-09-08 10:38:16 +0200

    python-paho-mqtt: bump to version 1.3.0
    Change setup type to setuptools.
    
    Add a patch removing the pytest-runner dependency. The patch was sent
    upstream.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-paho-mqtt/python-paho-mqtt.mk
+++ package/python-paho-mqtt/python-paho-mqtt.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_PAHO_MQTT_VERSION = 1.2
+PYTHON_PAHO_MQTT_VERSION = 1.3.0
 PYTHON_PAHO_MQTT_SOURCE = paho-mqtt-$(PYTHON_PAHO_MQTT_VERSION).tar.gz
-PYTHON_PAHO_MQTT_SITE = https://pypi.python.org/packages/82/d9/7064d3a0a1d62756a1a809c85b99f864c641b66de84c15458f72193b7708
+PYTHON_PAHO_MQTT_SITE = https://pypi.python.org/packages/33/7f/3ce1ffebaa0343d509aac003800b305d821e89dac3c11666f92e12feca14
 PYTHON_PAHO_MQTT_LICENSE = EPL-1.0 or EDLv1.0
 PYTHON_PAHO_MQTT_LICENSE_FILES = LICENSE.txt edl-v10 epl-v10
-PYTHON_PAHO_MQTT_SETUP_TYPE = distutils
+PYTHON_PAHO_MQTT_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
