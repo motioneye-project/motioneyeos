26493c0f1b0dd16b0a1e37deaead7b4a489c3af0
Yegor Yefremov <yegorslists@googlemail.com>
2017-09-08 15:23:49 +0200

    libmodbus: bump to version 3.1.4
    Disable tests compilation and documentation generation.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libmodbus/libmodbus.mk
+++ package/libmodbus/libmodbus.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-LIBMODBUS_VERSION = 3.0.6
+LIBMODBUS_VERSION = 3.1.4
 LIBMODBUS_SITE = http://libmodbus.org/releases
 LIBMODBUS_LICENSE = LGPL-2.1+
 LIBMODBUS_LICENSE_FILES = COPYING.LESSER
 LIBMODBUS_INSTALL_STAGING = YES
+LIBMODBUS_CONF_OPTS = --without-documentation --disable-tests
 
 $(eval $(autotools-package))
