ed13c6ecc78d4873a722dfbe40b6acb389265de7
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2017-09-09 12:34:16 +0200

    s6-dns: bump version to 2.2.0.1
    Signed-off-by: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/s6-dns/s6-dns.mk
+++ package/s6-dns/s6-dns.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-S6_DNS_VERSION = 2.2.0.0
+S6_DNS_VERSION = 2.2.0.1
 S6_DNS_SITE = http://skarnet.org/software/s6-dns
 S6_DNS_LICENSE = ISC
 S6_DNS_LICENSE_FILES = COPYING
