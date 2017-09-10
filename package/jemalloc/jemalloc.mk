2543544e8d7e319c2353957d710a2ec792777db1
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2017-09-10 05:37:30 -0500

    jemalloc: bump version to 5.0.1
    Signed-off-by: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/jemalloc/jemalloc.mk
+++ package/jemalloc/jemalloc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-JEMALLOC_VERSION = 4.5.0
+JEMALLOC_VERSION = 5.0.1
 JEMALLOC_SOURCE = jemalloc-$(JEMALLOC_VERSION).tar.bz2
 JEMALLOC_SITE = https://github.com/jemalloc/jemalloc/releases/download/$(JEMALLOC_VERSION)
 JEMALLOC_LICENSE = BSD-2-Clause
