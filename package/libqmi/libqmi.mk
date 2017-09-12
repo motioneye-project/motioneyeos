277d6354e096e4d613a690c2144a40fec9efb26a
Aleksander Morgado <aleksander@aleksander.es>
2017-09-12 10:39:02 +0200

    libqmi: add COPYING.LIB to LICENSE_FILES
    qmicli, qmi-network and qmi-firmware-update are GPLv2+ (COPYING file
    applies) and libqmi-glib is LGPLv2.1+ (COPYING.LIB file applies).
    
    Also, setup hashes of the license files, for a correct
    "make legal-info".
    
    Signed-off-by: Aleksander Morgado <aleksander@aleksander.es>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libqmi/libqmi.mk
+++ package/libqmi/libqmi.mk
@@ -8,7 +8,7 @@ LIBQMI_VERSION = 1.18.0
 LIBQMI_SITE = http://www.freedesktop.org/software/libqmi
 LIBQMI_SOURCE = libqmi-$(LIBQMI_VERSION).tar.xz
 LIBQMI_LICENSE = LGPL-2.0+ (library), GPL-2.0+ (programs)
-LIBQMI_LICENSE_FILES = COPYING
+LIBQMI_LICENSE_FILES = COPYING COPYING.LIB
 LIBQMI_INSTALL_STAGING = YES
 # 0001-musl-compat-canonicalize_file_name.patch
 LIBQMI_AUTORECONF = YES
