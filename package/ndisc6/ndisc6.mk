1bf62576c12fff7141a4262cdf3fe541e86b82e7
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-13 11:38:23 +0200

    package/ndisc6: bump version to 1.0.3
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ndisc6/ndisc6.mk
+++ package/ndisc6/ndisc6.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NDISC6_VERSION = 1.0.2
+NDISC6_VERSION = 1.0.3
 NDISC6_SOURCE = ndisc6-$(NDISC6_VERSION).tar.bz2
 NDISC6_SITE = http://www.remlab.net/files/ndisc6
 NDISC6_CONF_ENV = CC="$(TARGET_CC) -std=gnu99" LIBS=$(TARGET_NLS_LIBS)
