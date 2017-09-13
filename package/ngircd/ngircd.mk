1e4f43e439a71c3905a640fe282dc167a341b0e2
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-13 14:29:54 +0200

    package/ngircd: bump version to 24
    Switched _SITE to https, added patch to fix compile error.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/ngircd/ngircd.mk
+++ package/ngircd/ngircd.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-NGIRCD_VERSION = 20.3
+NGIRCD_VERSION = 24
 NGIRCD_SOURCE = ngircd-$(NGIRCD_VERSION).tar.xz
-NGIRCD_SITE = http://arthur.barton.de/pub/ngircd
+NGIRCD_SITE = https://arthur.barton.de/pub/ngircd
 NGIRCD_DEPENDENCIES = zlib
 NGIRCD_LICENSE = GPL-2.0+
 NGIRCD_LICENSE_FILES = COPYING
