c4faf1d4d11764da769abdd252dd3925d83378cb
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-13 15:13:15 +0200

    package/nmap: bump version to 7.60
    Version 7.60 added optional support for libssh2:
    http://seclists.org/nmap-announce/2017/4
    
    Switched _SITE to https
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/nmap/nmap.mk
+++ package/nmap/nmap.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-NMAP_VERSION = 7.40
-NMAP_SITE = http://nmap.org/dist
+NMAP_VERSION = 7.60
+NMAP_SITE = https://nmap.org/dist
 NMAP_SOURCE = nmap-$(NMAP_VERSION).tar.bz2
 NMAP_DEPENDENCIES = libpcap pcre
 NMAP_CONF_OPTS = --without-liblua --without-zenmap \
@@ -26,6 +26,14 @@ define NMAP_WRAPPER_EXEC
 	chmod +x $(@D)/libdnet-stripped/configure.gnu
 endef
 NMAP_POST_PATCH_HOOKS += NMAP_WRAPPER_EXEC
+
+ifeq ($(BR2_PACKAGE_LIBSSH2),y)
+NMAP_CONF_OPTS += --with-libssh2="$(STAGING_DIR)/usr"
+NMAP_DEPENDENCIES += libssh2
+NMAP_LIBS_FOR_STATIC_LINK += `$(PKG_CONFIG_HOST_BINARY) --libs libssh2`
+else
+NMAP_CONF_OPTS += --without-libssh2
+endif
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 NMAP_CONF_OPTS += --with-openssl="$(STAGING_DIR)/usr"
