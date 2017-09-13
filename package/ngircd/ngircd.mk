6ee9dbcc7546f07d34498fac69934d3095e17d8d
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-13 14:29:57 +0200

    package/ngircd: make zlib an optional dependency
    zlib is not a hard dependency:
    https://github.com/ngircd/ngircd/blob/master/configure.ng#L256
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/ngircd/ngircd.mk
+++ package/ngircd/ngircd.mk
@@ -7,7 +7,6 @@
 NGIRCD_VERSION = 24
 NGIRCD_SOURCE = ngircd-$(NGIRCD_VERSION).tar.xz
 NGIRCD_SITE = https://arthur.barton.de/pub/ngircd
-NGIRCD_DEPENDENCIES = zlib
 NGIRCD_LICENSE = GPL-2.0+
 NGIRCD_LICENSE_FILES = COPYING
 
@@ -22,6 +21,13 @@ NGIRCD_DEPENDENCIES += gnutls
 else
 NGIRCD_CONF_OPTS += --without-gnutls
 endif
+endif
+
+ifeq ($(BR2_PACKAGE_ZLIB),y)
+NGIRCD_CONF_OPTS += --with-zlib=$(STAGING_DIR)/usr
+NGIRCD_DEPENDENCIES += zlib
+else
+NGIRCD_CONF_OPTS += --without-zlib
 endif
 
 $(eval $(autotools-package))
