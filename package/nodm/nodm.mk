c6a4513577a7c0590352df800aeeac7f20316d59
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-13 16:21:53 +0200

    package/nodm: bump version to 0.13
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/nodm/nodm.mk
+++ package/nodm/nodm.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-NODM_VERSION = 0.12-1
-NODM_SITE = $(call github,spanezz,nodm,debian/$(NODM_VERSION))
+NODM_VERSION = 0.13
+NODM_SITE = $(call github,spanezz,nodm,$(NODM_VERSION))
 NODM_LICENSE = GPL-2.0+
 NODM_LICENSE_FILES = COPYING
 NODM_AUTORECONF = YES
