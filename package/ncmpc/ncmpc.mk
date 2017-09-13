850389d486cf967f14ce0374a39457e15b919402
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-13 11:33:54 +0200

    package/ncmpc: bump version to 0.27
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ncmpc/ncmpc.mk
+++ package/ncmpc/ncmpc.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 NCMPC_VERSION_MAJOR = 0
-NCMPC_VERSION = $(NCMPC_VERSION_MAJOR).25
+NCMPC_VERSION = $(NCMPC_VERSION_MAJOR).27
 NCMPC_SOURCE = ncmpc-$(NCMPC_VERSION).tar.xz
 NCMPC_SITE = http://www.musicpd.org/download/ncmpc/$(NCMPC_VERSION_MAJOR)
 NCMPC_DEPENDENCIES = host-pkgconf libglib2 libmpdclient ncurses
