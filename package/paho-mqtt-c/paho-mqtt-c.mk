d3a0be55382b18abb22a5592fb67a25f7f8a8d32
Yegor Yefremov <yegorslists@googlemail.com>
2017-09-13 08:07:18 +0200

    paho-mqtt-c: bump to version 1.2.0
    Add a patch fixing compilation on the systems without C++ compiler.
    
    Also add licence hashes.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/paho-mqtt-c/paho-mqtt-c.mk
+++ package/paho-mqtt-c/paho-mqtt-c.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PAHO_MQTT_C_VERSION = v1.1.0
+PAHO_MQTT_C_VERSION = v1.2.0
 PAHO_MQTT_C_SITE = $(call github,eclipse,paho.mqtt.c,$(PAHO_MQTT_C_VERSION))
 PAHO_MQTT_C_LICENSE = EPL-1.0 or BSD-3-Clause
 PAHO_MQTT_C_LICENSE_FILES = epl-v10 edl-v10
