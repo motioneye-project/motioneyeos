aa84f776a9d68c199bf404fb5f4bf6b1eb49727a
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-13 16:17:32 +0200

    package/libuv: bump version to 1.14.1
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/libuv/libuv.mk
+++ package/libuv/libuv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUV_VERSION = v1.14.0
+LIBUV_VERSION = v1.14.1
 LIBUV_SITE = $(call github,libuv,libuv,$(LIBUV_VERSION))
 LIBUV_DEPENDENCIES = host-pkgconf
 LIBUV_INSTALL_STAGING = YES
