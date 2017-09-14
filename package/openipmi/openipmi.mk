74c5081467cc22232aa81ebe15571399eb06b0d7
Joel Stanley <joel@jms.id.au>
2017-09-14 13:47:48 +0930

    openipmi: bump to version 2.0.24
    Some changes in addition to the version bump:
    
     - The uint32_t patch is now upstream
     - Drop --no-glib12 flag as support was removed in 2.0.23
     - The host library path patch was refreshed
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>
    Reviewed-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    [Arnout: remove uint32_t patch from hash file]
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/openipmi/openipmi.mk
+++ package/openipmi/openipmi.mk
@@ -4,11 +4,9 @@
 #
 ################################################################################
 
-OPENIPMI_VERSION = 2.0.22
+OPENIPMI_VERSION = 2.0.24
 OPENIPMI_SITE = http://sourceforge.net/projects/openipmi/files/OpenIPMI%202.0%20Library
 OPENIPMI_SOURCE = OpenIPMI-$(OPENIPMI_VERSION).tar.gz
-OPENIPMI_PATCH = \
-	http://sourceforge.net/p/openipmi/patches/26/attachment/0001-ui-use-standard-uint32_t.patch
 OPENIPMI_LICENSE = LGPL-2.0+, GPL-2.0+, BSD-3-Clause
 OPENIPMI_LICENSE_FILES = COPYING.LIB COPYING COPYING.BSD
 OPENIPMI_DEPENDENCIES = popt ncurses host-pkgconf
@@ -17,7 +15,6 @@ OPENIPMI_AUTORECONF = YES
 OPENIPMI_CONF_ENV = ac_cv_path_pkgprog="$(PKG_CONFIG_HOST_BINARY)"
 OPENIPMI_CONF_OPTS = \
 	--with-glib=no \
-	--with-glib12=no \
 	--with-tcl=no \
 	--with-perl=no \
 	--with-python=no \
