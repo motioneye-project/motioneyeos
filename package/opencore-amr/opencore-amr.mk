548af0870c991b3e91a58ef5401fd4554cd12768
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-16 15:22:03 +0200

    package/opencore-amr: bump version to 0.1.5
    Added md5 & sha1 hashes provided by upstream.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/opencore-amr/opencore-amr.mk
+++ package/opencore-amr/opencore-amr.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENCORE_AMR_VERSION = 0.1.3
+OPENCORE_AMR_VERSION = 0.1.5
 OPENCORE_AMR_SITE = http://downloads.sourceforge.net/project/opencore-amr/opencore-amr
 OPENCORE_AMR_INSTALL_STAGING = YES
 OPENCORE_AMR_LICENSE = Apache-2.0
