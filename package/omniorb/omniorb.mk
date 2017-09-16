f1e0d85d3e67de4f15a0f160579e0fd4c2f2a1e4
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-16 18:53:27 +0200

    package/omniorb: bump version to 4.2.2
    Removed patch after upstream committed a different solution:
    https://sourceforge.net/p/omniorb/svn/6330/
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/omniorb/omniorb.mk
+++ package/omniorb/omniorb.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OMNIORB_VERSION = 4.2.1
+OMNIORB_VERSION = 4.2.2
 OMNIORB_SITE = http://downloads.sourceforge.net/project/omniorb/omniORB/omniORB-$(OMNIORB_VERSION)
 OMNIORB_SOURCE = omniORB-$(OMNIORB_VERSION).tar.bz2
 OMNIORB_INSTALL_STAGING = YES
