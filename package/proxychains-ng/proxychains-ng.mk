9d71b8978aa24dd80c51af239633b17437140c2b
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-17 20:52:16 +0200

    package/proxychains-ng: security bump to version 4.11
    Version 4.9 fixes CVE-2015-3887:
    https://github.com/rofl0r/proxychains-ng/issues/60
    
    Added md5 & sha1 hashes provided by upstream.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/proxychains-ng/proxychains-ng.mk
+++ package/proxychains-ng/proxychains-ng.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PROXYCHAINS_NG_VERSION = 4.6
-PROXYCHAINS_NG_SOURCE = proxychains-$(PROXYCHAINS_NG_VERSION).tar.bz2
+PROXYCHAINS_NG_VERSION = 4.11
+PROXYCHAINS_NG_SOURCE = proxychains-ng-$(PROXYCHAINS_NG_VERSION).tar.bz2
 PROXYCHAINS_NG_SITE = http://downloads.sourceforge.net/project/proxychains-ng
 PROXYCHAINS_NG_LICENSE = GPL-2.0+
 PROXYCHAINS_NG_LICENSE_FILES = COPYING
