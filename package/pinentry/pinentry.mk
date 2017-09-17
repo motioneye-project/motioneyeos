6759caf679c5a479639ece9e2486eb9333e02708
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-17 19:09:58 +0200

    package/pinentry: added optional support for qt5
    Upstream added support for qt5:
    https://git.gnupg.org/cgi-bin/gitweb.cgi?p=pinentry.git;a=commit;h=abb59f50abf698ff1e56490fb39bcc98c26ab44b
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    [Arnout: completely separate qt4 and qt5 in the .mk file instead of
     making a complicated mix.]
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/pinentry/pinentry.mk
+++ package/pinentry/pinentry.mk
@@ -48,13 +48,15 @@ else
 PINENTRY_CONF_OPTS += --disable-pinentry-gtk2
 endif
 
-# pinentry-qt4 backend
+# pinentry-qt4/5 backend
+ifeq ($(BR2_PACKAGE_PINENTRY_QT4)$(BR2_PACKAGE_PINENTRY_QT5),y)
 ifeq ($(BR2_PACKAGE_PINENTRY_QT4),y)
 # -pthread needs to be passed for certain toolchains
 # http://autobuild.buildroot.net/results/6be/6be109ccedec603a67cebdb31b55865dcce0e128/
 PINENTRY_CONF_OPTS += LIBS=-pthread MOC=$(HOST_DIR)/bin/moc
+endif
 PINENTRY_CONF_OPTS += --enable-pinentry-qt
-PINENTRY_DEPENDENCIES += qt
+PINENTRY_DEPENDENCIES += $(if $(BR2_PACKAGE_PINENTRY_QT4),qt,qt5base)
 else
 PINENTRY_CONF_OPTS += --disable-pinentry-qt
 endif
