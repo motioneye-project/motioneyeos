4f526687d6214dbe7dd48ddab34182f4a5fc2fbf
bradford barr <bradford@density.io>
2017-09-18 11:58:24 -0400

    msgpack: bump version to 2.1.5
    Modern versions of msgpack are hosted on Github. Msgpack switched
    their build systems from autotools to cmake a while back. The licence
    was also updated from Apache to Boost.
    
    Signed-off-by: bradford barr <bradford@density.io>
    [Thomas: drop custom install commands, add LICENSE_1_0.txt to license
    files.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/msgpack/msgpack.mk
+++ package/msgpack/msgpack.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-MSGPACK_VERSION = 0.5.4
-MSGPACK_SITE = http://downloads.sourceforge.net/project/msgpack/msgpack/cpp
-MSGPACK_LICENSE = Apache-2.0
-MSGPACK_LICENSE_FILES = COPYING
+MSGPACK_VERSION = 2.1.5
+MSGPACK_SITE = $(call github,msgpack,msgpack-c,cpp-$(MSGPACK_VERSION))
+MSGPACK_LICENSE = BSL-1.0
+MSGPACK_LICENSE_FILES = COPYING LICENSE_1_0.txt
+MSGPACK_INSTALL_STAGING = YES
 
-$(eval $(autotools-package))
+$(eval $(cmake-package))
