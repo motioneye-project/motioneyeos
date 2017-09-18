dab996309e6f3147be343f8ee6a228a8130394f9
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-18 20:35:37 +0200

    package/libinput: bump version to 1.8.2
    Added all hashes applied upstream.
    
    Renamed configure option -event-gui to -debug-gui after upstream commit
    https://cgit.freedesktop.org/wayland/libinput/commit/configure.ac?h=1.8-branch&id=a69294251df92e818f695cd2456c8eb860e02522
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libinput/libinput.mk
+++ package/libinput/libinput.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBINPUT_VERSION = 1.7.2
+LIBINPUT_VERSION = 1.8.2
 LIBINPUT_SOURCE = libinput-$(LIBINPUT_VERSION).tar.xz
 LIBINPUT_SITE = http://www.freedesktop.org/software/libinput
 LIBINPUT_DEPENDENCIES = host-pkgconf libevdev mtdev udev
@@ -15,10 +15,10 @@ LIBINPUT_LICENSE_FILES = COPYING
 LIBINPUT_CONF_OPTS = --disable-tests --disable-libwacom
 
 ifeq ($(BR2_PACKAGE_LIBGTK3),y)
-LIBINPUT_CONF_OPTS += --enable-event-gui
+LIBINPUT_CONF_OPTS += --enable-debug-gui
 LIBINPUT_DEPENDENCIES += libgtk3
 else
-LIBINPUT_CONF_OPTS += --disable-event-gui
+LIBINPUT_CONF_OPTS += --disable-debug-gui
 endif
 
 $(eval $(autotools-package))
