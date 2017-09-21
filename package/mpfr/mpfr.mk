6d9bf93bc81e052a70fcc18c574d2cc6e3669cb3
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-21 18:45:36 +0200

    package/mpfr: bump to version 3.1.6
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/mpfr/mpfr.mk
+++ package/mpfr/mpfr.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MPFR_VERSION = 3.1.5
+MPFR_VERSION = 3.1.6
 MPFR_SITE = http://www.mpfr.org/mpfr-$(MPFR_VERSION)
 MPFR_SOURCE = mpfr-$(MPFR_VERSION).tar.xz
 MPFR_LICENSE = LGPL-3.0+
