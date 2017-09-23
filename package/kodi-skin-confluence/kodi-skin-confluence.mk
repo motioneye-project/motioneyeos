6e8ce1eccc2477f2046e2aeaf93bf4f4986c5ad2
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-23 14:02:43 +0200

    package/kodi-skin-confluence: bump version to 3.1.4
    This version was not tagged upstream so use a git hash which has that
    version.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/kodi-skin-confluence/kodi-skin-confluence.mk
+++ package/kodi-skin-confluence/kodi-skin-confluence.mk
@@ -4,7 +4,9 @@
 #
 ################################################################################
 
-KODI_SKIN_CONFLUENCE_VERSION = 7044dabb3a818d7ff47b7a34bfb3480f2a9441d9
+# This cset is on the branch 'krypton'
+# When Kodi is updated, then this should be updated to the corresponding branch
+KODI_SKIN_CONFLUENCE_VERSION = 96bed03570a90d4fdfb96ab71de9a51ceedd03fb
 KODI_SKIN_CONFLUENCE_SITE = $(call github,xbmc,skin.confluence,$(KODI_SKIN_CONFLUENCE_VERSION))
 KODI_SKIN_CONFLUENCE_LICENSE = GPL-2.0
 KODI_SKIN_CONFLUENCE_LICENSE_FILES = LICENSE.txt
