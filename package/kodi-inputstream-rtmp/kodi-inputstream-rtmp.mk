eec04a7b7dc32749f9c1f5e0ac1087230c542f28
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-23 14:02:41 +0200

    package/kodi-inputstream-rtmp: bump version to 1.0.6
    This version was not tagged upstream so use a git hash which has that
    version.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/kodi-inputstream-rtmp/kodi-inputstream-rtmp.mk
+++ package/kodi-inputstream-rtmp/kodi-inputstream-rtmp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_INPUTSTREAM_RTMP_VERSION = v1.0.4
+KODI_INPUTSTREAM_RTMP_VERSION = d93d32d45dfa61ad4b27f166f08736bd416bc329
 KODI_INPUTSTREAM_RTMP_SITE = $(call github,notspiff,inputstream.rtmp,$(KODI_INPUTSTREAM_RTMP_VERSION))
 KODI_INPUTSTREAM_RTMP_LICENSE = GPL-2.0+
 KODI_INPUTSTREAM_RTMP_LICENSE_FILES = src/RTMPStream.cpp
