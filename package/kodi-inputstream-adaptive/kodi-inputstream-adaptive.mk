f11469fcd650f0ff401fa4f4ff1ed301c605855b
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-09-23 14:02:40 +0200

    package/kodi-inputstream-adaptive: bump version to 2.0.18
    Fixes https://bugs.busybox.net/show_bug.cgi?id=10256
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/kodi-inputstream-adaptive/kodi-inputstream-adaptive.mk
+++ package/kodi-inputstream-adaptive/kodi-inputstream-adaptive.mk
@@ -4,7 +4,9 @@
 #
 ################################################################################
 
-KODI_INPUTSTREAM_ADAPTIVE_VERSION = 161f319bb2cd946f4659d841a54f19b1fb6143cc
+# This cset is on the branch 'Krypton'
+# When Kodi is updated, then this should be updated to the corresponding branch
+KODI_INPUTSTREAM_ADAPTIVE_VERSION = f2904b547e940c724dce7412a26744c2698cab66
 KODI_INPUTSTREAM_ADAPTIVE_SITE = $(call github,peak3d,inputstream.adaptive,$(KODI_INPUTSTREAM_ADAPTIVE_VERSION))
 KODI_INPUTSTREAM_ADAPTIVE_LICENSE = GPL-2.0+
 KODI_INPUTSTREAM_ADAPTIVE_LICENSE_FILES = src/main.cpp
