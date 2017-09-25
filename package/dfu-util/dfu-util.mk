24407cb4dcb63e1836b0545ede8dbb8b68ccd812
Wolfgang Grandegger <wg@grandegger.com>
2017-09-25 08:12:48 +0200

    package/dfu-util: allow to build it for the target as well
    Signed-off-by: Wolfgang Grandegger <wg@grandegger.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/dfu-util/dfu-util.mk
+++ package/dfu-util/dfu-util.mk
@@ -9,6 +9,8 @@ DFU_UTIL_SITE = http://dfu-util.sourceforge.net/releases
 DFU_UTIL_LICENSE = GPL-2.0+
 DFU_UTIL_LICENSE_FILES = COPYING
 
+DFU_UTIL_DEPENDENCIES = libusb
 HOST_DFU_UTIL_DEPENDENCIES = host-libusb
 
+$(eval $(autotools-package))
 $(eval $(host-autotools-package))
