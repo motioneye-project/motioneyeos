651af57cf011e6d8764d11932ce87fc064cd9a9b
Carlos Santos <casantos@datacom.ind.br>
2017-09-28 15:07:56 -0300

    dash: enable line editting if libedit is selected
    Otherwise, explicitly disable it, so that the configure script does not
    misdetect a system-installed libedit for example.
    
    Also, add a profile snippet enabling line editing, Emacs style.
    
    [Peter: move DASH_INSTALL_PROFILE inside conditional,
    	ensure /etc/profile.d exists]
    Change-Id: I68c6dbbafa95e266860329cb9c7ff5519fda5bf8
    Signed-off-by: Carlos Santos <casantos@datacom.ind.br>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/dash/dash.mk
+++ package/dash/dash.mk
@@ -9,6 +9,20 @@ DASH_SITE = http://gondor.apana.org.au/~herbert/dash/files
 DASH_LICENSE = BSD-3-Clause, GPL-2.0+ (mksignames.c)
 DASH_LICENSE_FILES = COPYING
 
+ifeq ($(BR2_PACKAGE_LIBEDIT),y)
+DASH_DEPENDENCIES += libedit
+DASH_CONF_OPTS += --with-libedit
+
+# Enable line editing, Emacs style
+define DASH_INSTALL_PROFILE
+	mkdir -p $(TARGET_DIR)/etc/profile.d
+	echo 'set -E' > $(TARGET_DIR)/etc/profile.d/dash.sh
+endef
+DASH_POST_INSTALL_TARGET_HOOKS += DASH_INSTALL_PROFILE
+else
+DASH_CONF_OPTS += --without-libedit
+endif
+
 define DASH_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/src/dash $(TARGET_DIR)/bin/dash
 endef
