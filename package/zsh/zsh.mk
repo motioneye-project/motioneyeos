4a4f99a428f3c49aa80440325db33485777e8f69
Baruch Siach <baruch@tkos.co.il>
2017-09-28 23:43:52 +0300

    zsh: disable NIS support
    The configure test for NIS and NIS+ looks for the ypcat and nisls host
    utilities, respectively. This is not compatible with cross compilation.
    Disable both unconditionally.
    
    Fixes:
    http://autobuild.buildroot.net/results/54c/54c9c5dff3df0a43573b03896cb0629fcae08478/
    http://autobuild.buildroot.net/results/c8e/c8e30a1eb6e77ae76292b6a01bede5abb2b1b7b7/
    
    Cc: Phil Eichinger <phil.eichinger@gmail.com>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/zsh/zsh.mk
+++ package/zsh/zsh.mk
@@ -9,6 +9,7 @@ ZSH_SITE = http://www.zsh.org/pub
 ZSH_SOURCE = zsh-$(ZSH_VERSION).tar.xz
 ZSH_DEPENDENCIES = ncurses
 ZSH_CONF_OPTS = --bindir=/bin
+ZSH_CONF_ENV = zsh_cv_sys_nis=no zsh_cv_sys_nis_plus=no
 ZSH_LICENSE = MIT-like
 ZSH_LICENSE_FILES = LICENCE
 
