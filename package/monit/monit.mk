fe945e6c23d1140dddf2de51ec5c53764c1b127d
Jörg Krause <joerg.krause@embedded.rocks>
2017-10-02 20:51:09 +0200

    monit: bump to version 5.24.0
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/monit/monit.mk
+++ package/monit/monit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MONIT_VERSION = 5.23.0
+MONIT_VERSION = 5.24.0
 MONIT_SITE = http://mmonit.com/monit/dist
 MONIT_LICENSE = AGPL-3.0 with OpenSSL exception
 MONIT_LICENSE_FILES = COPYING
