fe7285cd4e985a82d846302ef6ed8053c4e9a26d
Joel Stanley <joel@jms.id.au>
2017-10-03 22:44:25 +1030

    pdbg: bump version to latest
    I got this error when attempting to build:
    
     aclocal: error: couldn't open directory 'm4': No such file or directory
    
    So we now have the post patch hook to ensure the m4 directory is
    present.
    
    In addition, the package now sets a variable with the Git SHA1 so that
    the binary contains the expected output for --version.
    
    Specific configurations of uClibc cause a build failure in pdbg. This commit
    adds a patch to remove the definition of the offending macro (it is unused in
    the source).
    
     In file included from usr/include/stdio.h:71:0,
                      from libpdbg/fake.c:17:
     include/bits/uClibc_stdio.h:149:16: error: expected identifier or ‘(’ before ‘;’ token
       void *__unused;    /* Placeholder for codeset binding. */
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/pdbg/pdbg.mk
+++ package/pdbg/pdbg.mk
@@ -4,10 +4,17 @@
 #
 ################################################################################
 
-PDBG_VERSION = 90a7370a11e727f1482dea6ff2bd6aec20c64805
+PDBG_VERSION = 77158819158d1d7053a737ac090d04fdfbfe9265
 PDBG_SITE = $(call github,open-power,pdbg,$(PDBG_VERSION))
 PDBG_LICENSE = Apache-2.0
 PDBG_LICENSE_FILES = COPYING
 PDBG_AUTORECONF = YES
+
+PDBG_MAKE_OPTS = "GIT_SHA1=\"$(PDBG_VERSION)\""
+
+define PDBG_PATCH_M4
+	mkdir -p $(@D)/m4
+endef
+PDBG_POST_PATCH_HOOKS += PDBG_PATCH_M4
 
 $(eval $(autotools-package))
