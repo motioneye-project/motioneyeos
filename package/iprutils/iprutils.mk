2c3fcb1e448acff9a029bf7117ed5191bc5f723f
Samuel Mendoza-Jonas <sam@mendozajonas.com>
2017-10-04 13:12:54 +1100

    package/iprutils: bump version to 2.4.15.1
    Signed-off-by: Samuel Mendoza-Jonas <sam@mendozajonas.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/iprutils/iprutils.mk
+++ package/iprutils/iprutils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IPRUTILS_VERSION_MAJOR = 2.4.14
+IPRUTILS_VERSION_MAJOR = 2.4.15
 IPRUTILS_VERSION = $(IPRUTILS_VERSION_MAJOR).1
 IPRUTILS_SITE = https://downloads.sourceforge.net/project/iprdd/iprutils%20for%202.6%20kernels/$(IPRUTILS_VERSION_MAJOR)
 IPRUTILS_DEPENDENCIES = ncurses libsysfs pciutils zlib
