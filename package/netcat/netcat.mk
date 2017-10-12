2da613a7d94a4bd34e9f70533cf0d8b2803be5bb
Carlos Santos <casantos@datacom.ind.br>
2017-10-12 12:14:18 -0300

    netcat: add forced dependence on BusyBox
    It may be necessary if packages become built in parallel, leading to a
    race condition on the creation of the "nc" link.
    
    Of course this still leaves a race conditon if other netcat competitors
    but we must assume thet the user is a grown-up person who knows what is
    doing.
    
    Signed-off-by: Carlos Santos <casantos@datacom.ind.br>
    [Arnout: Don't force-remove the existing nc, not needed according to
             Romain.]
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/netcat/netcat.mk
+++ package/netcat/netcat.mk
@@ -9,4 +9,10 @@ NETCAT_SITE = http://downloads.sourceforge.net/project/netcat/netcat/$(NETCAT_VE
 NETCAT_LICENSE = GPL-2.0+
 NETCAT_LICENSE_FILES = COPYING
 
+# Ensure Busybox gets built/installed before, so that this package overrides
+# Busybox nc.
+ifeq ($(BR2_PACKAGE_BUSYBOX),y)
+NETCAT_DEPENDENCIES += busybox
+endif
+
 $(eval $(autotools-package))
