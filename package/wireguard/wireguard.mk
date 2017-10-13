b5781dd96a08c3d377f8161bf46690f1d6fd8ad0
Peter Korsgaard <peter@korsgaard.com>
2017-10-13 22:39:52 +0200

    wireguard: bump to version 0.0.20171011
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/wireguard/wireguard.mk
+++ package/wireguard/wireguard.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIREGUARD_VERSION = 0.0.20171005
+WIREGUARD_VERSION = 0.0.20171011
 WIREGUARD_SITE = https://git.zx2c4.com/WireGuard/snapshot
 WIREGUARD_SOURCE = WireGuard-$(WIREGUARD_VERSION).tar.xz
 WIREGUARD_LICENSE = GPL-2.0
