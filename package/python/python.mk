1842244c6d6e09154708e29a1cf527d2b01409d0
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-10-14 14:40:00 +0200

    package/python: bump version to 2.7.14
    Rebased patch 0016, changed _SITE to https.
    
    Release notes:
    https://raw.githubusercontent.com/python/cpython/84471935ed2f62b8c5758fd544c7d37076fe0fa5/Misc/NEWS
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python/python.mk
+++ package/python/python.mk
@@ -5,9 +5,9 @@
 ################################################################################
 
 PYTHON_VERSION_MAJOR = 2.7
-PYTHON_VERSION = $(PYTHON_VERSION_MAJOR).13
+PYTHON_VERSION = $(PYTHON_VERSION_MAJOR).14
 PYTHON_SOURCE = Python-$(PYTHON_VERSION).tar.xz
-PYTHON_SITE = http://python.org/ftp/python/$(PYTHON_VERSION)
+PYTHON_SITE = https://python.org/ftp/python/$(PYTHON_VERSION)
 PYTHON_LICENSE = Python-2.0, others
 PYTHON_LICENSE_FILES = LICENSE
 PYTHON_LIBTOOL_PATCH = NO
