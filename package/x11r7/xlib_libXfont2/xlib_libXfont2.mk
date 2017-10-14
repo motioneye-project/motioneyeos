eb8222ab0bd47387d14d44bbd809988fef8b69f4
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-10-14 13:37:25 +0200

    package/x11r7/xlib_libXfont2: bump version to 2.0.2
    Removed patches applied upstream, added all upstream hashes.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xlib_libXfont2/xlib_libXfont2.mk
+++ package/x11r7/xlib_libXfont2/xlib_libXfont2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBXFONT2_VERSION = 2.0.1
+XLIB_LIBXFONT2_VERSION = 2.0.2
 XLIB_LIBXFONT2_SOURCE = libXfont2-$(XLIB_LIBXFONT2_VERSION).tar.bz2
 XLIB_LIBXFONT2_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXFONT2_LICENSE = MIT
