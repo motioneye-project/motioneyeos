2d020aeb60470c2ebf77cf10688e3d02cf0d3d4a
Cam Hutchison <camh@xdna.net>
2017-10-16 12:13:51 +1100

    package/ncurses: add option to install extra terminfo files
    Add an option to ncurses to install extra user-configured terminfo
    files. By default, only a small number of vital terminfo files are
    installed on the target. This allows a build to specify the terminfo
    files it needs.
    
    Signed-off-by: Cam Hutchison <camh@xdna.net>
    [Thomas: qstrip the variable before using it.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/ncurses/ncurses.mk
+++ package/ncurses/ncurses.mk
@@ -63,7 +63,8 @@ NCURSES_TERMINFO_FILES = \
 	x/xterm+256color \
 	x/xterm-256color \
 	x/xterm-color \
-	x/xterm-xfree86
+	x/xterm-xfree86 \
+	$(call qstrip,$(BR2_PACKAGE_NCURSES_ADDITIONAL_TERMINFO))
 
 ifeq ($(BR2_PACKAGE_NCURSES_WCHAR),y)
 NCURSES_CONF_OPTS += --enable-widec
