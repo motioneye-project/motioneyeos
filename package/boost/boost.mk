4ef4a3405f43a284689b6451b1cfad4718339439
Adam Duskett <aduskett@gmail.com>
2017-10-17 16:44:07 -0400

    boost: bump to 1.65.1, coroutine2 is now a header only library
    The coroutine2 functionality is now provided only through headers, the
    compiled library has disappeared. Due to that passing "coroutine2" as
    argument to --without-libraries. Hence, the
    BR2_PACKAGE_BOOST_COROUTINE2 option is removed by this commit.
    
    We don't need Config.in.legacy handling, because coroutine2 support is
    now unconditionally available in boost.
    
    While at it, add LICENSE_1_0.txt sha256sum to boost.hash.
    
    Signed-off-by: Adam Duskett <Adamduskett@outlook.com>
    [Thomas: drop Config.in.legacy handling, reword commit log.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/boost/boost.mk
+++ package/boost/boost.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BOOST_VERSION = 1.64.0
+BOOST_VERSION = 1.65.1
 BOOST_SOURCE = boost_$(subst .,_,$(BOOST_VERSION)).tar.bz2
 BOOST_SITE = http://downloads.sourceforge.net/project/boost/boost/$(BOOST_VERSION)
 BOOST_INSTALL_STAGING = YES
@@ -14,9 +14,9 @@ BOOST_LICENSE_FILES = LICENSE_1_0.txt
 # keep host variant as minimal as possible
 HOST_BOOST_FLAGS = --without-icu \
 	--without-libraries=$(subst $(space),$(comma),atomic chrono context \
-	coroutine coroutine2 date_time exception filesystem graph \
-	graph_parallel iostreams locale log math mpi program_options python \
-	random regex serialization signals system test thread timer type_erasure \
+	coroutine date_time exception filesystem graph graph_parallel iostreams \
+	locale log math mpi program_options python random regex serialization \
+	signals system test thread timer type_erasure \
 	wave)
 
 BOOST_WITHOUT_FLAGS += $(if $(BR2_PACKAGE_BOOST_ATOMIC),,atomic)
@@ -24,7 +24,6 @@ BOOST_WITHOUT_FLAGS += $(if $(BR2_PACKAGE_BOOST_CHRONO),,chrono)
 BOOST_WITHOUT_FLAGS += $(if $(BR2_PACKAGE_BOOST_CONTAINER),,container)
 BOOST_WITHOUT_FLAGS += $(if $(BR2_PACKAGE_BOOST_CONTEXT),,context)
 BOOST_WITHOUT_FLAGS += $(if $(BR2_PACKAGE_BOOST_COROUTINE),,coroutine)
-BOOST_WITHOUT_FLAGS += $(if $(BR2_PACKAGE_BOOST_COROUTINE2),,coroutine2)
 BOOST_WITHOUT_FLAGS += $(if $(BR2_PACKAGE_BOOST_DATE_TIME),,date_time)
 BOOST_WITHOUT_FLAGS += $(if $(BR2_PACKAGE_BOOST_EXCEPTION),,exception)
 BOOST_WITHOUT_FLAGS += $(if $(BR2_PACKAGE_BOOST_FILESYSTEM),,filesystem)
