0b44c23089308cd6394858c924c34c411fb361e9
Olivier Schonken <olivier.schonken@gmail.com>
2017-10-19 10:56:35 +0200

    qpdf: add missing dependency on jpeg
    Fixes:
    
      configure: WARNING: unable to find required header jpeglib.h
      configure: WARNING: unable to find required library jpeg
      configure: error: some required prerequisites were not found
    
    Signed-off-by: Olivier Schonken <olivier.schonken@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/qpdf/qpdf.mk
+++ package/qpdf/qpdf.mk
@@ -9,7 +9,7 @@ QPDF_SITE = http://downloads.sourceforge.net/project/qpdf/qpdf/$(QPDF_VERSION)
 QPDF_INSTALL_STAGING = YES
 QPDF_LICENSE = Artistic-2.0
 QPDF_LICENSE_FILES = Artistic-2.0
-QPDF_DEPENDENCIES = pcre zlib
+QPDF_DEPENDENCIES = pcre zlib jpeg
 
 QPDF_CONF_OPTS = --without-random
 
