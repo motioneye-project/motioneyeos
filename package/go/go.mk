b3c2acba9b29aa2f9c2e36cae368ee98697002d0
Christian Stewart <christian@paral.in>
2017-10-18 20:22:52 -0400

    go: bump version to 1.9
    Go 1.9 is required for docker-engine and other Go packages in Buildroot.
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.8.3
+GO_VERSION = 1.9
 GO_SITE = https://storage.googleapis.com/golang
 GO_SOURCE = go$(GO_VERSION).src.tar.gz
 
