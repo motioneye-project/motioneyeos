06a4975d4bd030ab3cf291d18fa36a39cb58cc89
Peter Seiderer <ps.report@gmx.net>
2017-10-20 20:41:10 +0200

    qt5: bump LTS version to 5.6.3
    qt5base:
      - move hash file to 5.6.3
      - move 0001-eglfs-rasp-pi-header-inclusion.patch to 5.6.3
      - remove 0002-eglfs-fix-eglfs_mali-compile-for-odroid-mali.patch (upstream committed [1])
    
    qt5declarative:
      - move patches to 5.6.3
    
    qt5quickcontrols2:
      - move hash file to 5.6.3
    
    qt5webkit:
      - move patches to 5.6.3
    
    [1] http://code.qt.io/cgit/qt/qtbase.git/commit/?h=5.6&id=f1b4bd4790860e1ff5afcec111a359bc3a91cfda
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/qt5/qt5.mk
+++ package/qt5/qt5.mk
@@ -10,7 +10,7 @@ QT5_VERSION = $(QT5_VERSION_MAJOR).2
 QT5_SITE = http://download.qt.io/official_releases/qt/$(QT5_VERSION_MAJOR)/$(QT5_VERSION)/submodules
 else
 QT5_VERSION_MAJOR = 5.6
-QT5_VERSION = $(QT5_VERSION_MAJOR).2
+QT5_VERSION = $(QT5_VERSION_MAJOR).3
 QT5_SITE = http://download.qt.io/official_releases/qt/$(QT5_VERSION_MAJOR)/$(QT5_VERSION)/submodules
 QT5_SNAPSHOTS_SITE = http://download.qt.io/snapshots/qt/$(QT5_VERSION_MAJOR)/$(QT5_VERSION)/latest_src/submodules
 endif
