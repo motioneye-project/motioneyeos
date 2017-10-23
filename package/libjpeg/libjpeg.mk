c93f56d46593155a244bf83fd36c78c7968ce6b0
Olivier Schonken <olivier.schonken@gmail.com>
2017-10-23 15:17:43 +0200

    libjpeg: add pkg-config file for libjpeg
    Signed-off-by: Olivier Schonken <olivier.schonken@gmail.com>
    [Thomas: change to automatically set the version in the generated .pc file.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libjpeg/libjpeg.mk
+++ package/libjpeg/libjpeg.mk
@@ -18,5 +18,14 @@ endef
 
 LIBJPEG_POST_INSTALL_TARGET_HOOKS += LIBJPEG_REMOVE_USELESS_TOOLS
 
+define LIBJPEG_INSTALL_STAGING_PC
+	$(INSTALL) -D -m 0644 package/libjpeg/libjpeg.pc.in \
+		$(STAGING_DIR)/usr/lib/pkgconfig/libjpeg.pc
+	version=`sed -e '/^PACKAGE_VERSION/!d;s/PACKAGE_VERSION = \(.*\)/\1/' $(@D)/Makefile` ; \
+		$(SED) "s/@PACKAGE_VERSION@/$${version}/" $(STAGING_DIR)/usr/lib/pkgconfig/libjpeg.pc
+endef
+
+LIBJPEG_POST_INSTALL_STAGING_HOOKS += LIBJPEG_INSTALL_STAGING_PC
+
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
