62d4dd2999a0446c2b868a7c6fbcc764a470493d
Peter Korsgaard <peter@korsgaard.com>
2017-10-24 01:13:17 +0200

    libcurl: security bump to version 7.56.1
    Fixes CVE-2017-1000257 - IMAP FETCH response out of bounds read
    
    https://curl.haxx.se/docs/adv_20171023.html
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libcurl/libcurl.mk
+++ package/libcurl/libcurl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBCURL_VERSION = 7.56.0
+LIBCURL_VERSION = 7.56.1
 LIBCURL_SOURCE = curl-$(LIBCURL_VERSION).tar.xz
 LIBCURL_SITE = https://curl.haxx.se/download
 LIBCURL_DEPENDENCIES = host-pkgconf \
