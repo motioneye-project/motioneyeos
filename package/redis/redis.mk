751cd4cfab88ff15143d13eccf87f982a274e4e3
Peter Korsgaard <peter@korsgaard.com>
2017-10-26 14:44:15 +0200

    redis: bump to version 3.2.11
    3.2.11 fixes important issues. From the release notes:
    
    ================================================================================
    Redis 3.2.11     Released Thu Sep 21 15:47:53 CEST 2017
    ================================================================================
    
    Upgrade urgency HIGH: Potentially critical bugs fixed.
    
    AOF flush on SHUTDOWN did not cared to really write the AOF buffers
    (not in the kernel but in the Redis process memory) to disk before exiting.
    Calling SHUTDOWN during traffic resulted into not every operation to be
    persisted on disk.
    
    Also add a hash for the license file while we're at it.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/redis/redis.mk
+++ package/redis/redis.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-REDIS_VERSION = 3.2.9
+REDIS_VERSION = 3.2.11
 REDIS_SITE = http://download.redis.io/releases
 REDIS_LICENSE = BSD-3-Clause (core); MIT and BSD family licenses (Bundled components)
 REDIS_LICENSE_FILES = COPYING
