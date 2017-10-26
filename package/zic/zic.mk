ec176933c282f68b30f3a21f58372ea3e199ae53
Martin Bark <martin@barkynet.com>
2017-10-26 11:18:47 +0100

    package/zic: bump version to 2017c
    Signed-off-by: Martin Bark <martin@barkynet.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/zic/zic.mk
+++ package/zic/zic.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ZIC_VERSION = 2017b
+ZIC_VERSION = 2017c
 ZIC_SOURCE = tzcode$(ZIC_VERSION).tar.gz
 ZIC_SITE = http://www.iana.org/time-zones/repository/releases
 ZIC_STRIP_COMPONENTS = 0
