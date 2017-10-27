86eb94636e0dcfaf332ba808eeee04a61b13af60
Peter Korsgaard <peter@korsgaard.com>
2017-10-27 14:02:08 +0200

    wget: security bump to version 1.19.2
    Fixes the following security issues:
    
    CVE-2017-13089: The http.c:skip_short_body() function is called in some
    circumstances, such as when processing redirects.  When the response is sent
    chunked, the chunk parser uses strtol() to read each chunk's length, but
    doesn't check that the chunk length is a non-negative number.  The code then
    tries to skip the chunk in pieces of 512 bytes by using the MIN() macro, but
    ends up passing the negative chunk length to connect.c:fd_read().  As
    fd_read() takes an int argument, the high 32 bits of the chunk length are
    discarded, leaving fd_read() with a completely attacker controlled length
    argument.
    
    CVE-2017-13090: The retr.c:fd_read_body() function is called when processing
    OK responses.  When the response is sent chunked, the chunk parser uses
    strtol() to read each chunk's length, but doesn't check that the chunk
    length is a non-negative number.  The code then tries to read the chunk in
    pieces of 8192 bytes by using the MIN() macro, but ends up passing the
    negative chunk length to retr.c:fd_read().  As fd_read() takes an int
    argument, the high 32 bits of the chunk length are discarded, leaving
    fd_read() with a completely attacker controlled length argument.  The
    attacker can corrupt malloc metadata after the allocated buffer.
    
    Drop now upstreamed patch and change to .tar.lz as .tar.xz is no longer
    available.
    
    Also add a hash for the license file while we're at it.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/wget/wget.mk
+++ package/wget/wget.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-WGET_VERSION = 1.19.1
-WGET_SOURCE = wget-$(WGET_VERSION).tar.xz
+WGET_VERSION = 1.19.2
+WGET_SOURCE = wget-$(WGET_VERSION).tar.lz
 WGET_SITE = $(BR2_GNU_MIRROR)/wget
 WGET_DEPENDENCIES = host-pkgconf
 WGET_LICENSE = GPL-3.0+
