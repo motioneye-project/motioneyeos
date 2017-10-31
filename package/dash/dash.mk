e41f4d239b1efc6e4b595196152ae05fc18d3662
Jerzy Grzegorek <jerzy.m.grzegorek@gmail.com>
2017-10-31 09:16:12 +0100

    package/dash: drop the default value of the DASH_SOURCE variable
    Signed-off-by: Jerzy Grzegorek <jerzy.m.grzegorek@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/dash/dash.mk
+++ package/dash/dash.mk
@@ -5,7 +5,6 @@
 ################################################################################
 
 DASH_VERSION = 0.5.9.1
-DASH_SOURCE = dash-$(DASH_VERSION).tar.gz
 DASH_SITE = http://gondor.apana.org.au/~herbert/dash/files
 DASH_LICENSE = BSD-3-Clause, GPL-2.0+ (mksignames.c)
 DASH_LICENSE_FILES = COPYING
