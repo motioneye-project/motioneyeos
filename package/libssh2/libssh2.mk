a660d1fc7447601ed153685c58f2db5e7b99422d
Sam Voss <sam.voss@rockwellcollins.com>
2017-10-31 12:56:30 -0500

    package/libssh2: update to newest version
    Update libssh2 to use the newest version from git. This caused a
    transition from released version number to hash as it has not been
    version rev'd in over a year (see issue
    https://github.com/libssh2/libssh2/issues/220 for bump request).
    
    This brings in changes to the autoconf to correctly pick the crypto
    library.
    
    Signed-off-by: Sam Voss <sam.voss@rockwellcollins.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libssh2/libssh2.mk
+++ package/libssh2/libssh2.mk
@@ -4,32 +4,33 @@
 #
 ################################################################################
 
-LIBSSH2_VERSION = 1.8.0
-LIBSSH2_SITE = http://www.libssh2.org/download
+LIBSSH2_VERSION = 616fd4d1b3e4a55de67c48819fefca83132126b5
+LIBSSH2_SITE = $(call github,libssh2,libssh2,$(LIBSSH2_VERSION))
 LIBSSH2_LICENSE = BSD
 LIBSSH2_LICENSE_FILES = COPYING
 LIBSSH2_INSTALL_STAGING = YES
 LIBSSH2_CONF_OPTS = --disable-examples-build
 
+# building from a git clone
+LIBSSH2_AUTORECONF = YES
+
 # Dependency is one of mbedtls, libgcrypt or openssl, guaranteed in
 # Config.in. Favour mbedtls.
 ifeq ($(BR2_PACKAGE_MBEDTLS),y)
 LIBSSH2_DEPENDENCIES += mbedtls
-LIBSSH2_CONF_OPTS += --with-mbedtls=$(STAGING_DIR)/usr \
-	--without-openssl --without-libgcrypt
-LIBSSH2_CONF_ENV += ac_cv_libgcrypt=no
+LIBSSH2_CONF_OPTS += --with-libmbedcrypto-prefix=$(STAGING_DIR)/usr \
+	--with-crypto=mbedtls
 else ifeq ($(BR2_PACKAGE_LIBGCRYPT),y)
 LIBSSH2_DEPENDENCIES += libgcrypt
-LIBSSH2_CONF_OPTS += --with-libgcrypt=$(STAGING_DIR)/usr \
-	--without-openssl --without-mbedtls
+LIBSSH2_CONF_OPTS += --with-libgcrypt-prefix=$(STAGING_DIR)/usr \
+	--with-crypto=libgcrypt
 # configure.ac forgets to link to dependent libraries of gcrypt breaking static
 # linking
 LIBSSH2_CONF_ENV += LIBS="`$(STAGING_DIR)/usr/bin/libgcrypt-config --libs`"
 else
 LIBSSH2_DEPENDENCIES += openssl
-LIBSSH2_CONF_OPTS += --with-openssl \
-	--with-libssl-prefix=$(STAGING_DIR)/usr \
-	--without-libgcrypt --without-mbedtls
+LIBSSH2_CONF_OPTS += --with-libssl-prefix=$(STAGING_DIR)/usr \
+	--with-crypto=openssl
 endif
 
 # Add zlib support if enabled
