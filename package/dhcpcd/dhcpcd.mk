e04c5d3c34961aebf254b01a2bcb8508979446aa
Markus Mayer <mmayer@broadcom.com>
2017-10-31 15:36:59 -0700

    dhcpcd: add systemd start-up service
    Add systemd start-up configuration for dhcpcd that is executed after
    the network has been brought up.
    
    Signed-off-by: Markus Mayer <mmayer@broadcom.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/dhcpcd/dhcpcd.mk
+++ package/dhcpcd/dhcpcd.mk
@@ -41,6 +41,14 @@ define DHCPCD_INSTALL_INIT_SYSV
 		$(TARGET_DIR)/etc/init.d/S41dhcpcd
 endef
 
+define DHCPCD_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 0644 package/dhcpcd/dhcpcd.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/dhcpcd.service
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+	ln -sf ../../../../usr/lib/systemd/system/dhcpcd.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/dhcpcd.service
+endef
+
 # NOTE: Even though this package has a configure script, it is not generated
 # using the autotools, so we have to use the generic package infrastructure.
 
