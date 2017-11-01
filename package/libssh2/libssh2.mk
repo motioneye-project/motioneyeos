04a1031d3429ff8e5a2ae4d820702c50519243a8
Sam Voss <sam.voss@rockwellcollins.com>
2017-11-01 11:22:56 -0500

    package/libssh2: Add selectable crypto libraries
    Currently, the selection of the backend is based on a priority order,
    which is not always desirable: not all features are available for all
    backends, as reported upstream:
        https://github.com/libssh2/libssh2/issues/213
    In that particular case, the problem is that libgcrypt is unable to
    read encrypted certificates created with openssl, but it is likely
    that other incompatibilities exist as well.
    
    As such, allow a user to select the backend most appropriate to their
    use-case.
    
    Note that this changes the defaults: previously, if openssl was already
    selected and we additionally select libssh2, openssl would be used as
    a backend. Now, mbedtls is the default so if the user doesn't change
    it, mbedtls will be used.
    
    Signed-off-by: Sam Voss <sam.voss@rockwellcollins.com>
    [Arnout: remove now-unneeded comment in .mk file]
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/libssh2/libssh2.mk
+++ package/libssh2/libssh2.mk
@@ -14,20 +14,18 @@ LIBSSH2_CONF_OPTS = --disable-examples-build
 # building from a git clone
 LIBSSH2_AUTORECONF = YES
 
-# Dependency is one of mbedtls, libgcrypt or openssl, guaranteed in
-# Config.in. Favour mbedtls.
-ifeq ($(BR2_PACKAGE_MBEDTLS),y)
+ifeq ($(BR2_PACKAGE_LIBSSH2_MBEDTLS),y)
 LIBSSH2_DEPENDENCIES += mbedtls
 LIBSSH2_CONF_OPTS += --with-libmbedcrypto-prefix=$(STAGING_DIR)/usr \
 	--with-crypto=mbedtls
-else ifeq ($(BR2_PACKAGE_LIBGCRYPT),y)
+else ifeq ($(BR2_PACKAGE_LIBSSH2_LIBGCRYPT),y)
 LIBSSH2_DEPENDENCIES += libgcrypt
 LIBSSH2_CONF_OPTS += --with-libgcrypt-prefix=$(STAGING_DIR)/usr \
 	--with-crypto=libgcrypt
 # configure.ac forgets to link to dependent libraries of gcrypt breaking static
 # linking
 LIBSSH2_CONF_ENV += LIBS="`$(STAGING_DIR)/usr/bin/libgcrypt-config --libs`"
-else
+else ifeq ($(BR2_PACKAGE_LIBSSH2_OPENSSL),y)
 LIBSSH2_DEPENDENCIES += openssl
 LIBSSH2_CONF_OPTS += --with-libssl-prefix=$(STAGING_DIR)/usr \
 	--with-crypto=openssl
