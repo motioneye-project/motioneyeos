233242baed3ea1d980f23e53d8317b86b8114e2d
Petr Vorel <petr.vorel@gmail.com>
2017-11-02 21:11:55 +0100

    package/feh: Bump to version 2.21
    Signed-off-by: Petr Vorel <petr.vorel@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/feh/feh.mk
+++ package/feh/feh.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FEH_VERSION = 2.18.3
+FEH_VERSION = 2.21
 FEH_SOURCE = feh-$(FEH_VERSION).tar.bz2
 FEH_SITE = http://feh.finalrewind.org
 FEH_DEPENDENCIES = libcurl imlib2 libpng xlib_libXinerama xlib_libXt
