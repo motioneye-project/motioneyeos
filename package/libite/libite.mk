d69db786f1cccf18f382bb4e051fc2aa5a6c5488
Ryan Coe <bluemrp9@gmail.com>
2017-11-03 06:30:03 -0700

    libite: bump version to 1.9.2
    Also add hashes for license files.
    
    Signed-off-by: Ryan Coe <bluemrp9@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libite/libite.mk
+++ package/libite/libite.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBITE_VERSION = v1.8.2
+LIBITE_VERSION = v1.9.2
 LIBITE_SITE = $(call github,troglobit,libite,$(LIBITE_VERSION))
 LIBITE_LICENSE = MIT, X11, ISC, BSD-2-Clause
 LIBITE_LICENSE_FILES = LICENSE chomp.c pidfile.c
