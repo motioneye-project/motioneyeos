4d80fc8a37ace4230ebccaa79d0281358bd2e514
Sergey Matyukevich <geomatsi@gmail.com>
2017-11-04 22:40:48 +0300

    package/xr819-xradio: update to the latest version
    Bump kernel module git revision to make it work with Linux v4.13.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/xr819-xradio/xr819-xradio.mk
+++ package/xr819-xradio/xr819-xradio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XR819_XRADIO_VERSION = 014dfdd203102c5fd2370a73ec4ae3e6dd4e9ded
+XR819_XRADIO_VERSION = c03a053a9e731d56c89c0c09627e886db2a0e04e
 XR819_XRADIO_SITE = $(call github,fifteenhex,xradio,$(XR819_XRADIO_VERSION))
 XR819_XRADIO_LICENSE = GPL-2.0
 XR819_XRADIO_LICENSE_FILES = LICENSE
