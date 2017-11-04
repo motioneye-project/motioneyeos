89d525186161d4651366379ac21e80eead6a754e
Florian Fainelli <f.fainelli@gmail.com>
2017-11-04 15:02:04 -0700

    ethtool: Bump to version 4.13
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ETHTOOL_VERSION = 4.11
+ETHTOOL_VERSION = 4.13
 ETHTOOL_SOURCE = ethtool-$(ETHTOOL_VERSION).tar.xz
 ETHTOOL_SITE = $(BR2_KERNEL_MIRROR)/software/network/ethtool
 ETHTOOL_LICENSE = GPL-2.0
