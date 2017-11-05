4c3820248761c279e3c8f9839e5b24c8b39891fd
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-11-05 15:58:52 +0100

    package/libplist: security bump to version 2.0.0
    Release notes:
    https://github.com/libimobiledevice/libplist/blob/master/NEWS
    
    This version bump fixes
      * CVE-2017-6440
      * CVE-2017-6439
      * CVE-2017-6438
      * CVE-2017-6437
      * CVE-2017-6436
      * CVE-2017-6435
      * CVE-2017-5836
      * CVE-2017-5835
      * CVE-2017-5834
      * CVE-2017-5545
      * CVE-2017-5209
    ... and several others that didn't receive any CVE (yet).
    
    The dependency to libxml2 was removed.
    Autoreconf is not needed anymore, the upstream tarball includes a
    configure script.
    
    [Peter: also drop host-pkgconf dependency, only used for cython]
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libplist/libplist.mk
+++ package/libplist/libplist.mk
@@ -4,16 +4,12 @@
 #
 ################################################################################
 
-LIBPLIST_VERSION = 1.12
+LIBPLIST_VERSION = 2.0.0
 LIBPLIST_SOURCE = libplist-$(LIBPLIST_VERSION).tar.bz2
 LIBPLIST_SITE = http://www.libimobiledevice.org/downloads
-LIBPLIST_DEPENDENCIES = libxml2 host-pkgconf
 LIBPLIST_INSTALL_STAGING = YES
 LIBPLIST_LICENSE = LGPL-2.1+
 LIBPLIST_LICENSE_FILES = COPYING
-
-# Straight out of the git tree:
-LIBPLIST_AUTORECONF = YES
 
 # Disable building Python bindings, because it requires host-cython, which
 # is not packaged in Buildroot at all.
