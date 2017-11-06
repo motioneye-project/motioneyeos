b17351661b241a377f8f696a850d88243eb76dc9
Florian Fainelli <f.fainelli@gmail.com>
2017-11-05 17:29:06 -0800

    ethtool: Allow enabling/disabling pretty printing
    Ethtool has a configure script option allowing the enabling or disabling of
    pretty printing for drivers, SFP modules etc which can save a significant
    amount of bytes on the root filesystem. Allow selecting that, and turn it on by
    default (like ethtool's configure script).
    
    [Peter: fix Config.in indentation/style]
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/ethtool/ethtool.mk
+++ package/ethtool/ethtool.mk
@@ -9,5 +9,7 @@ ETHTOOL_SOURCE = ethtool-$(ETHTOOL_VERSION).tar.xz
 ETHTOOL_SITE = $(BR2_KERNEL_MIRROR)/software/network/ethtool
 ETHTOOL_LICENSE = GPL-2.0
 ETHTOOL_LICENSE_FILES = COPYING
+ETHTOOL_CONF_OPTS = \
+	$(if $(BR2_PACKAGE_ETHTOOL_PRETTY_PRINT),--enable-pretty-dump,--disable-pretty-dump)
 
 $(eval $(autotools-package))
