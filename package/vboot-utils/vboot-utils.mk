bbb25c3ad7b66e6882508e49028d1739732bca34
Alex Suykov <alex.suykov@gmail.com>
2017-11-07 12:17:15 +0200

    vboot-utils: fix ARCH detection
    The package includes some target-specific code that is irrelevant
    in a host package but gets built anyway. The target for this code
    must be one of the supported ChromeOS targets.
    
    Supplied Makefile apparently relies on the environment to provide
    a valid target, with a simple fallback to host arch. This breaks
    the build if no value is provided and the host arch is not among
    the supported ones.
    
    Should fix
    http://autobuild.buildroot.net/results/d118a83b6c4f7f910d0d44c279f36251d7ba29e8/
    and similar failures.
    
    Signed-off-by: Alex Suykov <alex.suykov@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/vboot-utils/vboot-utils.mk
+++ package/vboot-utils/vboot-utils.mk
@@ -21,12 +21,17 @@ HOST_VBOOT_UTILS_DEPENDENCIES = host-openssl host-util-linux host-pkgconf
 # media partitioning.
 #
 # make target for futility is "futil".
+#
+# The value of ARCH is only relevant for crossystem (a target tool) and
+# does not affect futil or cgpt in any way as long as it is one of the
+# supported targets.
 
 define HOST_VBOOT_UTILS_BUILD_CMDS
 	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) \
 		CC="$(HOSTCC)" \
 		CFLAGS="$(HOST_CFLAGS) -D_LARGEFILE64_SOURCE -D_GNU_SOURCE" \
 		LDFLAGS="$(HOST_LDFLAGS)" \
+		ARCH=arm \
 		futil cgpt
 endef
 
