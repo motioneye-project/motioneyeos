90e829e0fdf386ff379debe1417f31c6947929b2
Sergey Matyukevich <geomatsi@gmail.com>
2017-11-08 22:43:33 +0300

    rtl8189fs: new package
    This package adds the rtl8189fs WiFi driver. The driver is sourced
    from jwrdegoede's and icenowy's work on GitHub:
       repo: https://github.com/jwrdegoede/rtl8189ES_linux.git
       branch: rtl8189fs
    
    Driver is known to support Realtek RTL8189FTV chip.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/rtl8189fs/rtl8189fs.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# rtl8189fs
+#
+################################################################################
+
+RTL8189FS_VERSION = 04b44d437b20f28ba83fcaecc0bdab7f7647fd11
+RTL8189FS_SITE = $(call github,jwrdegoede,rtl8189ES_linux,$(RTL8189FS_VERSION))
+RTL8189FS_LICENSE = GPL-2.0
+
+RTL8189FS_MODULE_MAKE_OPTS = \
+	CONFIG_RTL8189FS=m \
+	KVER=$(LINUX_VERSION_PROBED) \
+	KSRC=$(LINUX_DIR)
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
