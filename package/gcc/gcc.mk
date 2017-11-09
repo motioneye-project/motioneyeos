b09586c4c3560cb3b0607d91a4a50df6c9d9d775
Alexey Brodkin <Alexey.Brodkin@synopsys.com>
2017-11-10 00:56:48 +0300

    gcc: allow the selection of upstream GCC 7.x for ARC
    Upstream gcc now has support for ARC, as of 7.x, with 7.2 being
    usable, so let's allow its selection.
    
    GCC from Synopsys GitHub still has a lot of fixes and improvements
    most of which will be a part of the next upstream release but let's
    see how it goes and if we see vanilla GCC doesn't require way too many
    back-ported patches we'll be able to get rid of Synopsys GitHub here
    at all.
    
    As of today the only really required back-port is the one for non-uClibc
    libc.
    
    Back-ported from upstream master with help of:
    -------------------->8--------------------
    git format-patch 927c0132251f -1 -- . ':!gcc/ChangeLog' ':!libgcc/ChangeLog'
    -------------------->8--------------------
    
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>
    Cc: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Cc: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/gcc/gcc.mk
+++ package/gcc/gcc.mk
@@ -10,7 +10,7 @@
 
 GCC_VERSION = $(call qstrip,$(BR2_GCC_VERSION))
 
-ifeq ($(BR2_arc),y)
+ifeq ($(BR2_GCC_VERSION_ARC),y)
 GCC_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,gcc,$(GCC_VERSION))
 GCC_SOURCE = gcc-$(GCC_VERSION).tar.gz
 else ifeq ($(BR2_or1k),y)
