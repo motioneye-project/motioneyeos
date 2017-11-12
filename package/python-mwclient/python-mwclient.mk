7d55b9351d68f7dc47c52b725b7ab9a1d5f0ec0a
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-11-12 11:45:23 +0100

    package/python-mwclient: bump version to 0.8.6
    Version 0.8.2 added OAuth support so we need python-requests-oauthlib
    as runtime dependency from now on. This package also has a runtime
    dependency on python-requests so all we need is to update the select
    command in Config.in.
    
    Removed patch applied upstream.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-mwclient/python-mwclient.mk
+++ package/python-mwclient/python-mwclient.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MWCLIENT_VERSION = 0.8.1
+PYTHON_MWCLIENT_VERSION = 0.8.6
 PYTHON_MWCLIENT_SOURCE = mwclient-$(PYTHON_MWCLIENT_VERSION).tar.gz
-PYTHON_MWCLIENT_SITE = https://pypi.python.org/packages/19/79/481b288a497f625ee8f76141ff3472d81428b1f14b7155a28a63a3247197
+PYTHON_MWCLIENT_SITE = https://pypi.python.org/packages/cd/38/beaf985032b42a0b0c8f9028b469c4dcb0bd7bfab62707ec27af7e890e84
 PYTHON_MWCLIENT_LICENSE = MIT
 PYTHON_MWCLIENT_LICENSE_FILES = mwclient/__init__.py
 PYTHON_MWCLIENT_SETUP_TYPE = setuptools
