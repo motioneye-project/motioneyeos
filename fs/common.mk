787a31fed42f98a8e2e6a0bd2079376c861436f2
Yann E. MORIN <yann.morin.1998@free.fr>
2017-11-12 18:45:49 +0100

    fs: don't remove intermediate files
    Now that they are in their own directory and no longer pollute the build
    dir, there is no point in removing them.
    
    Furthermore, a follow-up patch will require that those files survive
    when more than one filesystem image is generated.
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- fs/common.mk
+++ fs/common.mk
@@ -103,7 +103,6 @@ endif
 	rm -f $$(TARGET_DIR_WARNING_FILE)
 	PATH=$$(BR_PATH) $$(HOST_DIR)/bin/fakeroot -- $$(FAKEROOT_SCRIPT)
 	$$(INSTALL) -m 0644 support/misc/target-dir-warning.txt $$(TARGET_DIR_WARNING_FILE)
-	-@rm -f $$(FAKEROOT_SCRIPT) $$(FULL_DEVICE_TABLE)
 ifneq ($$(ROOTFS_$(2)_COMPRESS_CMD),)
 	PATH=$$(BR_PATH) $$(ROOTFS_$(2)_COMPRESS_CMD) $$@ > $$@$$(ROOTFS_$(2)_COMPRESS_EXT)
 endif
