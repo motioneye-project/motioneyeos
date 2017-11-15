325e60767da45efe507ea13fc304866257421334
Vivien Didelot <vivien.didelot@savoirfairelinux.com>
2017-11-15 13:14:35 -0500

    genimage: change upstream URL
    Pengutronix is now hosting the official genimage repository and release
    tarballs on Github. This patch updates the official URL from the package
    description and its download website described in GENIMAGE_SITE.
    
    Signed-off-by: Vivien Didelot <vivien.didelot@savoirfairelinux.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/genimage/genimage.mk
+++ package/genimage/genimage.mk
@@ -6,7 +6,7 @@
 
 GENIMAGE_VERSION = 9
 GENIMAGE_SOURCE = genimage-$(GENIMAGE_VERSION).tar.xz
-GENIMAGE_SITE = http://www.pengutronix.de/software/genimage/download
+GENIMAGE_SITE = https://github.com/pengutronix/genimage/releases/download/v$(GENIMAGE_VERSION)
 HOST_GENIMAGE_DEPENDENCIES = host-pkgconf host-libconfuse
 GENIMAGE_LICENSE = GPL-2.0
 GENIMAGE_LICENSE_FILES = COPYING
