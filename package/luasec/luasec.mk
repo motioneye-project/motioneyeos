fa1f589c411df04d6dff0cb27d5abf4465605c73
Francois Perrad <fperrad@gmail.com>
2017-11-16 18:41:15 +0100

    luasec: bump to version 0.6
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/luasec/luasec.mk
+++ package/luasec/luasec.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUASEC_VERSION = 0.5-2
+LUASEC_VERSION = 0.6-1
 LUASEC_SUBDIR = luasec
 LUASEC_LICENSE = MIT
 LUASEC_LICENSE_FILES = $(LUASEC_SUBDIR)/LICENSE
