b4245aef53020ae48c6b2bdeb239b144ce8316f2
Jagan Teki <jagannadh.teki@gmail.com>
2017-11-18 19:26:40 +0100

    board: bananapi-m1: Fix to use dtb instead of dts
    boot.cmd using dts file instead of dtb which was
    added in below commit, so fix the same.
    
    "board: Add Bananapi M1 support"
    (sha1: 5f50fb8d1df51b622537da015f8c3b7b6dcbbc35)
    
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- board/bananapi/bananapi-m1/boot.cmd
+++ board/bananapi/bananapi-m1/boot.cmd
@@ -2,6 +2,6 @@ setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait
 
 mmc dev 0
 fatload mmc 0 $kernel_addr_r zImage
-fatload mmc 0 $fdt_addr_r sun7i-a20-bananapi.dts
+fatload mmc 0 $fdt_addr_r sun7i-a20-bananapi.dtb
 
 bootz $kernel_addr_r - $fdt_addr_r
