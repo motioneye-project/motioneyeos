47df95a73d24d0bfe4216b6fc53da17abbafa4fa
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-11-20 23:22:53 +0100

    jose: fix build with old compilers
    This commit adds two patches to the jose package that fix the build
    with old compilers such as gcc 4.7.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/00d5a4b809922f8fa42c0de9d461c9754d2f8098
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/jose/jose.mk
+++ package/jose/jose.mk
@@ -9,6 +9,8 @@ JOSE_SOURCE = jose-$(JOSE_VERSION).tar.bz2
 JOSE_SITE = https://github.com/latchset/jose/releases/download/v$(JOSE_VERSION)
 JOSE_LICENSE = Apache-2.0
 JOSE_LICENSE_FILES = COPYING
+# 0002-configure-fix-build-with-old-compilers.patch
+JOSE_AUTORECONF = YES
 JOSE_INSTALL_STAGING = YES
 JOSE_DEPENDENCIES = host-pkgconf zlib jansson openssl
 
