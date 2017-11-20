5eb9f431fa29b3572971f28e687d96c15ecd8519
Adam Duskett <aduskett@gmail.com>
2017-11-20 09:57:58 -0500

    pango: bump to 1.140.14
    In addition, add a hash for the license file.
    
    Signed-off-by: Adam Duskett <aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/pango/pango.mk
+++ package/pango/pango.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 PANGO_VERSION_MAJOR = 1.40
-PANGO_VERSION = $(PANGO_VERSION_MAJOR).12
+PANGO_VERSION = $(PANGO_VERSION_MAJOR).14
 PANGO_SOURCE = pango-$(PANGO_VERSION).tar.xz
 PANGO_SITE = http://ftp.gnome.org/pub/GNOME/sources/pango/$(PANGO_VERSION_MAJOR)
 PANGO_AUTORECONF = YES
