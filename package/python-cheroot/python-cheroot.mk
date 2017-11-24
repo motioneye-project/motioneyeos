43f76947ac2a05f24c3129db1bb690aad65e9474
Yegor Yefremov <yegorslists@googlemail.com>
2017-11-24 15:44:59 +0100

    python-cheroot: bump version to 5.10.0
    Fix licence file name and add checksum.
    
    Fixes:
    http://autobuild.buildroot.net/results/94d/94dee5b01e0f7693d4e6e05074e7d93e92236e2c
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-cheroot/python-cheroot.mk
+++ package/python-cheroot/python-cheroot.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_CHEROOT_VERSION = 5.9.0
+PYTHON_CHEROOT_VERSION = 5.10.0
 PYTHON_CHEROOT_SOURCE = cheroot-$(PYTHON_CHEROOT_VERSION).tar.gz
-PYTHON_CHEROOT_SITE = https://pypi.python.org/packages/df/63/5c5265feb7e02e43d2f3a3c19dbb2d78742b0a2ff237c9e473c77bc23500
+PYTHON_CHEROOT_SITE = https://pypi.python.org/packages/14/dc/afc41e7f7e797973808d8520e6aef21c4efd00550aa32f5b726327d36be0
 PYTHON_CHEROOT_LICENSE = BSD-3-Clause
-PYTHON_CHEROOT_LICENSE_FILES = LICENSE.md
+PYTHON_CHEROOT_LICENSE_FILES = LICENSE
 PYTHON_CHEROOT_SETUP_TYPE = setuptools
 PYTHON_CHEROOT_DEPENDENCIES = host-python-setuptools-scm
 
