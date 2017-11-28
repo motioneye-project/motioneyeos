b865c38ea627ae59e46a67efe805b4bce101fecb
Gaël PORTAY <gael.portay@savoirfairelinux.com>
2017-11-28 06:59:30 -0500

    package/qt5: simplify and cleanup _SITE variables
    The variable QT5_SNAPSHOTS_SITE was introduced in commit 9d5ac9698 to
    fetch sources of obsolete modules. It was used to download qt5webkit
    tarballs.
    
    The variable is not referenced anymore since the qt5webkit tarballs are
    downloaded from the Qt community release site.
    
    Since the introduction of qt-5.9 in aa1c40ba3, the QT5_SITE variable
    was duplicated for no good reason.
    
    Signed-off-by: Gaël PORTAY <gael.portay@savoirfairelinux.com>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/qt5/qt5.mk
+++ package/qt5/qt5.mk
@@ -7,13 +7,11 @@
 ifeq ($(BR2_PACKAGE_QT5_VERSION_LATEST),y)
 QT5_VERSION_MAJOR = 5.9
 QT5_VERSION = $(QT5_VERSION_MAJOR).2
-QT5_SITE = http://download.qt.io/official_releases/qt/$(QT5_VERSION_MAJOR)/$(QT5_VERSION)/submodules
 else
 QT5_VERSION_MAJOR = 5.6
 QT5_VERSION = $(QT5_VERSION_MAJOR).3
-QT5_SITE = http://download.qt.io/official_releases/qt/$(QT5_VERSION_MAJOR)/$(QT5_VERSION)/submodules
-QT5_SNAPSHOTS_SITE = http://download.qt.io/snapshots/qt/$(QT5_VERSION_MAJOR)/$(QT5_VERSION)/latest_src/submodules
 endif
+QT5_SITE = http://download.qt.io/official_releases/qt/$(QT5_VERSION_MAJOR)/$(QT5_VERSION)/submodules
 
 include $(sort $(wildcard package/qt5/*/*.mk))
 
