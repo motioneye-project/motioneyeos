35f6288a54bca343b180a4d210ffd6e1b5655ab1
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-11-29 21:45:57 +0100

    package/x11r7/xlib_libXfont2: security bump to version 2.0.3
    Fixes CVE-2017-16611:
    https://lists.x.org/archives/xorg-announce/2017-November/002824.html
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/x11r7/xlib_libXfont2/xlib_libXfont2.mk
+++ package/x11r7/xlib_libXfont2/xlib_libXfont2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBXFONT2_VERSION = 2.0.2
+XLIB_LIBXFONT2_VERSION = 2.0.3
 XLIB_LIBXFONT2_SOURCE = libXfont2-$(XLIB_LIBXFONT2_VERSION).tar.bz2
 XLIB_LIBXFONT2_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXFONT2_LICENSE = MIT
