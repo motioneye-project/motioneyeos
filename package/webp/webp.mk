8833ab4d6ef0b68fdab43aa76b797529f38f0765
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2017-12-02 13:31:57 +0100

    webp: add giflib optional dependency
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/webp/webp.mk
+++ package/webp/webp.mk
@@ -29,6 +29,13 @@ else
 WEBP_CONF_OPTS += --disable-libwebpmux
 endif
 
+ifeq ($(BR2_PACKAGE_GIFLIB),y)
+WEBP_DEPENDENCIES += giflib
+WEBP_CONF_OPTS += --enable-gif
+else
+WEBP_CONF_OPTS += --disable-gif
+endif
+
 ifeq ($(BR2_PACKAGE_LIBPNG),y)
 WEBP_DEPENDENCIES += libpng
 WEBP_CONF_ENV += ac_cv_path_LIBPNG_CONFIG=$(STAGING_DIR)/usr/bin/libpng-config
