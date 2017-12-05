e716e1f3fbb5c7a90d36779b4426ebee25725ad8
Ismael Luceno <ismael@iodev.co.uk>
2017-12-05 02:42:37 -0300

    axel: bump to version 2.16.1
    Fixes building against static libssl:
    
      http://autobuild.buildroot.net/results/7edb3756a07bbac27f852332b81677feb6156a48/
    
    The major change between 2.16 and 2.16.1 that fixes static linking
    with libssl is that pkg-config is now used to detect
    libssl. Therefore, we add a dependency on host-pkgconf.
    
    Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/axel/axel.mk
+++ package/axel/axel.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-AXEL_VERSION = 2.16
+AXEL_VERSION = 2.16.1
 AXEL_SITE = https://github.com/axel-download-accelerator/axel/releases/download/v$(AXEL_VERSION)
 AXEL_SOURCE = axel-$(AXEL_VERSION).tar.xz
 AXEL_LICENSE = GPL-2.0+
 AXEL_LICENSE_FILES = COPYING
-AXEL_DEPENDENCIES = $(TARGET_NLS_DEPENDENCIES)
+AXEL_DEPENDENCIES = host-pkgconf $(TARGET_NLS_DEPENDENCIES)
 
 # ac_cv_prog_cc_c99 is required for BR2_USE_WCHAR=n because the C99 test
 # provided by autoconf relies on wchar_t.
