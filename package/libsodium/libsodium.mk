20b0535ea4fbd2560d8a9b98c1abd8f3955ab2cd
Adrien Gallouët <adrien@gallouet.fr>
2017-12-06 10:54:46 +0000

    libsodium: bump to version 1.0.15
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libsodium/libsodium.mk
+++ package/libsodium/libsodium.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LIBSODIUM_VERSION = 1.0.13
-LIBSODIUM_SITE = https://download.libsodium.org/libsodium/releases
+LIBSODIUM_VERSION = 1.0.15
+LIBSODIUM_SITE = https://github.com/jedisct1/libsodium/releases/download/$(LIBSODIUM_VERSION)
 LIBSODIUM_LICENSE = ISC
 LIBSODIUM_LICENSE_FILES = LICENSE
 LIBSODIUM_INSTALL_STAGING = YES
