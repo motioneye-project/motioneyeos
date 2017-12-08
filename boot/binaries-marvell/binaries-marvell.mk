a82882ae10e9b80635d0d5e884b6f291df2b4949
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2017-12-08 17:22:30 +0100

    binaries-marvell: bump version, add license file and hash
    Following our feedback, Marvell has added a README.md file into the
    branch that contains the firmware. Thereore, this commit bumps the
    version to the commit that includes the README.md file (it's the only
    change, the firmware files are unmodified), updates
    <pkg>_LICENSE_FILES to point to README.md, and adds the hash for this
    license files.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- boot/binaries-marvell/binaries-marvell.mk
+++ boot/binaries-marvell/binaries-marvell.mk
@@ -4,13 +4,11 @@
 #
 ################################################################################
 
-BINARIES_MARVELL_VERSION = 0dabe23b956420b0928d337d635f0cd5646c33d0
+BINARIES_MARVELL_VERSION = a8ac27b7884ed2c1efcf9f3326de3e0ed7b94e91
 BINARIES_MARVELL_SITE = $(call github,MarvellEmbeddedProcessors,binaries-marvell,$(BINARIES_MARVELL_VERSION))
 
-# The license text is only available in the master branch, which is
-# not used in this package. See
-# https://raw.githubusercontent.com/MarvellEmbeddedProcessors/binaries-marvell/master/README.md.
 BINARIES_MARVELL_LICENSE = GPL-2.0 with freertos-exception-2.0
+BINARIES_MARVELL_LICENSE_FILES = README.md
 
 BINARIES_MARVELL_IMAGE = $(call qstrip,$(BR2_TARGET_BINARIES_MARVELL_IMAGE))
 BINARIES_MARVELL_INSTALL_IMAGES  = YES
