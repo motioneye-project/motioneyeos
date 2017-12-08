7918f1b042d8f0f6f7fdbf5315752a266f102550
Jan Kraval <jan.kraval@gmail.com>
2017-12-08 18:44:56 +0100

    rtl8189fs: bump to newer revision supporting Linux 4.14
    Signed-off-by: Jan Kraval <jan.kraval@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rtl8189fs/rtl8189fs.mk
+++ package/rtl8189fs/rtl8189fs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTL8189FS_VERSION = 04b44d437b20f28ba83fcaecc0bdab7f7647fd11
+RTL8189FS_VERSION = e0e80a9805472df943ad159b12633881aa69374a
 RTL8189FS_SITE = $(call github,jwrdegoede,rtl8189ES_linux,$(RTL8189FS_VERSION))
 RTL8189FS_LICENSE = GPL-2.0
 
