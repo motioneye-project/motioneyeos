6465e4d34a4c1d94e73ec849297fc77b47b498a6
Luca Ceresoli <luca@lucaceresoli.net>
2017-12-09 22:30:06 +0100

    boards/zynq: rename the DTB to be loaded by U-Boot
    We are about to introduce genimage support for the Zynq boards. For
    all of them U-Boot loads the DTB from a file named "devicetree.dtb" so
    we need to rename it on the SD card image.
    
    We could use genimage itself to rename files while creating the
    image. But this would require a different genimage.cfg for each board,
    leading to duplicate code.
    
    Instead let's create a "devicetree.dtb" symlink pointing to the dtb
    listed in the configuration. Currently all the Zynq-based boards have
    only one DTS configured, but if one had two or more of them, the
    symlink would point to the first one.
    
    [Peter: only create symlink if dtb option is available]
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ board/zynq/post-image.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# By default U-Boot loads DTB from a file named "devicetree.dtb", so
+# let's use a symlink with that name that points to the *first*
+# devicetree listed in the config.
+
+FIRST_DT=$(sed -n \
+           's/^BR2_LINUX_KERNEL_INTREE_DTS_NAME="\([a-z0-9\-]*\).*"$/\1/p' \
+           ${BR2_CONFIG})
+
+[ -z "${FIRST_DT}" ] || ln -fs ${FIRST_DT}.dtb ${BINARIES_DIR}/devicetree.dtb
