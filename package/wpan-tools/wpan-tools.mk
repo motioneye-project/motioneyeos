5f8570cb76c97b3726fe2e5d378364fb30d35519
Mario Lang <mlang@blind.guru>
2017-12-10 22:52:16 +0100

    package/wpan-tools: change tarball compression to xz
    Signed-off-by: Mario Lang <mlang@blind.guru>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/wpan-tools/wpan-tools.mk
+++ package/wpan-tools/wpan-tools.mk
@@ -5,6 +5,7 @@
 ################################################################################
 
 WPAN_TOOLS_VERSION = 0.7
+WPAN_TOOLS_SOURCE = wpan-tools-$(WPAN_TOOLS_VERSION).tar.xz
 WPAN_TOOLS_SITE = http://wpan.cakelab.org/releases
 WPAN_TOOLS_DEPENDENCIES = host-pkgconf libnl
 WPAN_TOOLS_LICENSE = ISC
