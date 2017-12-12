85e08bfdb5f39caf71846628764ee57e2d0b62d5
Mario Lang <mlang@blind.guru>
2017-12-12 19:41:28 +0100

    lynx: new package
    Signed-off-by: Mario Lang <mlang@blind.guru>
    Reviewed-by: Adrian Perez de Castro <aperez@igalia.com>
    [Thomas: invert ncurses/slang select to avoid a circular dependency,
    add BR2_USE_MMU dependency inherited from slang.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/lynx/lynx.mk
@@ -0,0 +1,38 @@
+################################################################################
+#
+# lynx
+#
+################################################################################
+
+LYNX_VERSION = 2.8.9dev.16
+LYNX_SOURCE = lynx$(LYNX_VERSION).tar.bz2
+LYNX_SITE = ftp://ftp.invisible-island.net/lynx/tarballs
+LYNX_LICENSE = GPL-2.0
+LYNX_LICENSE_FILES = COPYING
+
+LYNX_DEPENDENCIES = $(TARGET_NLS_DEPENDENCIES)
+
+ifeq ($(BR2_PACKAGE_NCURSES),y)
+LYNX_DEPENDENCIES += ncurses
+LYNX_CONF_OPTS += --with-screen=ncurses$(if $(BR2_PACKAGE_NCURSES_WCHAR),w)
+else ifeq ($(BR2_PACKAGE_SLANG),y)
+LYNX_DEPENDENCIES += slang
+LYNX_CONF_OPTS += --with-screen=slang
+endif
+
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+LYNX_DEPENDENCIES += openssl
+LYNX_CONF_OPTS += --with-ssl
+else ifeq ($(BR2_PACKAGE_GNUTLS),y)
+LYNX_DEPENDENCIES += gnutls
+LYNX_CONF_OPTS += --with-gnutls
+endif
+
+ifeq ($(BR2_PACKAGE_ZLIB),y)
+LYNX_DEPENDENCIES += zlib
+LYNX_CONF_OPTS += --with-zlib
+else
+LYNX_CONF_OPTS += --without-zlib
+endif
+
+$(eval $(autotools-package))
