6f1d0abe183ea381e3ece9ee4fe0f75e3fe7c4ea
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-12-13 06:33:02 +0100

    package/python-cheetah: switch SITE to https
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-cheetah/python-cheetah.mk
+++ package/python-cheetah/python-cheetah.mk
@@ -6,7 +6,7 @@
 
 PYTHON_CHEETAH_VERSION = 2.4.4
 PYTHON_CHEETAH_SOURCE = Cheetah-$(PYTHON_CHEETAH_VERSION).tar.gz
-PYTHON_CHEETAH_SITE = http://pypi.python.org/packages/source/C/Cheetah
+PYTHON_CHEETAH_SITE = https://pypi.python.org/packages/source/C/Cheetah
 PYTHON_CHEETAH_LICENSE = MIT
 PYTHON_CHEETAH_SETUP_TYPE = setuptools
 
