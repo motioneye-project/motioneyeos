a4629269575d3e3103f61a41ecd4a968a7fe1923
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-12-13 06:33:05 +0100

    package/python-pynacl: switch SITE to https
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-pynacl/python-pynacl.mk
+++ package/python-pynacl/python-pynacl.mk
@@ -6,7 +6,7 @@
 
 PYTHON_PYNACL_VERSION = 1.0.1
 PYTHON_PYNACL_SOURCE = PyNaCl-$(PYTHON_PYNACL_VERSION).tar.gz
-PYTHON_PYNACL_SITE = http://pypi.python.org/packages/source/P/PyNaCl
+PYTHON_PYNACL_SITE = https://pypi.python.org/packages/source/P/PyNaCl
 PYTHON_PYNACL_LICENSE = Apache-2.0
 PYTHON_PYNACL_LICENSE_FILES = LICENSE
 PYTHON_PYNACL_SETUP_TYPE = setuptools
