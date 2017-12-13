3677776c488a2462b0b6dc33d4a31b4bc98de6e6
Bernd Kuhls <bernd.kuhls@t-online.de>
2017-12-13 06:33:03 +0100

    package/python-ipaddr: switch SITE to https
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-ipaddr/python-ipaddr.mk
+++ package/python-ipaddr/python-ipaddr.mk
@@ -6,7 +6,7 @@
 
 PYTHON_IPADDR_VERSION = 2.1.11
 PYTHON_IPADDR_SOURCE = ipaddr-$(PYTHON_IPADDR_VERSION).tar.gz
-PYTHON_IPADDR_SITE = http://pypi.python.org/packages/source/i/ipaddr
+PYTHON_IPADDR_SITE = https://pypi.python.org/packages/source/i/ipaddr
 PYTHON_IPADDR_SETUP_TYPE = distutils
 PYTHON_IPADDR_LICENSE = Apache-2.0
 PYTHON_IPADDR_LICENSE_FILES = COPYING
