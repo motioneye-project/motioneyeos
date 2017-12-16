cb8ff6ffc9ab0eb10bd58533bfb37a778c58905b
Zoltan Gyarmati <mr.zoltan.gyarmati@gmail.com>
2017-12-17 00:52:59 +0100

    proj: new package
    Signed-off-by: Zoltan Gyarmati <mr.zoltan.gyarmati@gmail.com>
    [Thomas:
     - switch to the autotools build system, which supports static linking
       properly
     - add <pkg>_LICENSE_FILES
     - add sha256 hash for the tarball, in addition to the md5 one
     - add license file hash
     - squash DEVELOPERS file addition]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/proj/proj.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# proj
+#
+################################################################################
+
+PROJ_VERSION = 4.9.3
+PROJ_SOURCE = proj-$(PROJ_VERSION).tar.gz
+PROJ_SITE = http://download.osgeo.org/proj
+PROJ_LICENSE = MIT
+PROJ_LICENSE_FILES = COPYING
+PROJ_INSTALL_STAGING = YES
+
+$(eval $(autotools-package))
