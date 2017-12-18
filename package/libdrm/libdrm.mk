fa6c7d165971e1f70b9ac94bae9cd1fd9180c072
Peter Seiderer <ps.report@gmx.net>
2017-12-18 19:55:31 +0100

    libdrm: fix libatomic_ops linking
    Add patch 0003-configure-Makefile.am-use-pkg-config-to-discover-lib.patch
    provided by Thomas Petazzoni handling libatomic_ops linking when needed.
    
    Fixes [1]:
    
        CCLD     etnaviv_cmd_stream_test
      ../../etnaviv/.libs/libdrm_etnaviv.so: undefined reference to `AO_store_full_emulation'
      ../../etnaviv/.libs/libdrm_etnaviv.so: undefined reference to `AO_fetch_compare_and_swap_emulation'
      collect2: error: ld returned 1 exit status
    
    [1] http://autobuild.buildroot.net/results/92e074c49417261843eef95872ff935a56077c98
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libdrm/libdrm.mk
+++ package/libdrm/libdrm.mk
@@ -9,9 +9,15 @@ LIBDRM_SOURCE = libdrm-$(LIBDRM_VERSION).tar.bz2
 LIBDRM_SITE = https://dri.freedesktop.org/libdrm
 LIBDRM_LICENSE = MIT
 LIBDRM_INSTALL_STAGING = YES
+
+# patch 0003-configure-Makefile.am-use-pkg-config-to-discover-lib.patch
+# touching configure.ac/Makefile.am (and host-xutil_util-macros dependency)
+LIBDRM_AUTORECONF = YES
+
 LIBDRM_DEPENDENCIES = \
 	libpthread-stubs \
-	host-pkgconf
+	host-pkgconf \
+	host-xutil_util-macros
 
 LIBDRM_CONF_OPTS = \
 	--disable-cairo-tests \
