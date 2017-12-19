954fef2281d61de9171f089f9c036093126ec6d5
Yair Ben Avraham <yairba@tkos.co.il>
2017-12-19 18:36:57 +0200

    tpm-tools: bump to version 1.3.9.1
    This patch contains the following changes:
     - Remove all three patches, they are included in upstream version
     - Add locally calculated sha256 hash
     - Remove <pkg>_STRIP_COMPONENTS = 2, there is no leading directory
     - Remove <pkg>_AUTORECONF and <pkg>_GETTEXTIZE since all the patches are
       being removed.
    
    Signed-off-by: Yair Ben Avraham <yairba@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/tpm-tools/tpm-tools.mk
+++ package/tpm-tools/tpm-tools.mk
@@ -4,15 +4,11 @@
 #
 ################################################################################
 
-TPM_TOOLS_VERSION = 1.3.8
+TPM_TOOLS_VERSION = 1.3.9.1
 TPM_TOOLS_SITE = http://downloads.sourceforge.net/project/trousers/tpm-tools/$(TPM_TOOLS_VERSION)
-TPM_TOOLS_STRIP_COMPONENTS = 2
 TPM_TOOLS_LICENSE = Common Public License Version 1.0
 TPM_TOOLS_LICENSE_FILES = LICENSE
 TPM_TOOLS_DEPENDENCIES = trousers openssl $(TARGET_NLS_DEPENDENCIES)
-# configure.in and lib/Makefile.am is patched
-TPM_TOOLS_AUTORECONF = YES
-TPM_TOOLS_GETTEXTIZE = YES
 
 TPM_TOOLS_CONF_OPTS = --disable-pkcs11-support
 
