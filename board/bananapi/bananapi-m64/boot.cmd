5b9ac80fef2581632d2e6a42c479822929e9ac10
Jagan Teki <jagannadh.teki@gmail.com>
2017-12-20 10:53:30 +0100

    board: Add Bananapi M64 support
    Add initial support for bananapi M64 board
    with below features:
    - U-Boot 2017.11
    - Linux 4.14
    - Default packages from buildroot
    
    Cc: Jason <manager@sinovoip.com.cn>
    Cc: hailymei@banana-pi.com <hailymei@banana-pi.com>
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ board/bananapi/bananapi-m64/boot.cmd
@@ -0,0 +1,6 @@
+setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait
+
+fatload mmc 0 $kernel_addr_r Image
+fatload mmc 0 $fdt_addr_r sun50i-a64-bananapi-m64.dtb
+
+booti $kernel_addr_r - $fdt_addr_r
