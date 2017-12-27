ef385d04277baa3842a7468c7675f3d8fa9986d2
Adam Duskett <aduskett@gmail.com>
2017-12-27 13:08:10 -0500

    libpqxx: bump to 6.0.0
    Other changes:
    - Add a hash for the license file.
    
    - Change the URL from pqxx.org to the official GitHub repository, as releases
      are no longer being posted to the old download mirror.
    
    - Remove the patch, this has been fixed in the upstream code for some time.
    
    - Remove AUTORECONF, as it is no longer necessary because the patch has been
      removed.
    
    - Require gcc >= 4.7, as C++11 is now mandatory:
      http://pqxx.org/development/libpqxx/
    
    Signed-off-by: Adam Duskett <Adamduskett@outlook.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/libpqxx/libpqxx.mk
+++ package/libpqxx/libpqxx.mk
@@ -4,15 +4,12 @@
 #
 ################################################################################
 
-LIBPQXX_VERSION = 4.0.1
-LIBPQXX_SITE = http://pqxx.org/download/software/libpqxx
+LIBPQXX_VERSION = 6.0.0
+LIBPQXX_SITE = $(call github,jtv,libpqxx,$(LIBPQXX_VERSION))
 LIBPQXX_INSTALL_STAGING = YES
 LIBPQXX_DEPENDENCIES = postgresql
 LIBPQXX_LICENSE = BSD-3-Clause
 LIBPQXX_LICENSE_FILES = COPYING
-
-# 0001-Fix-broken-sed-call-in-configure.ac.in.patch
-LIBPQXX_AUTORECONF = YES
 
 LIBPQXX_CONF_ENV += ac_cv_path_PG_CONFIG=$(STAGING_DIR)/usr/bin/pg_config
 
