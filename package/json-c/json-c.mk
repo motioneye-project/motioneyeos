ba3769c39aa32ef844e53750c658f023473c9391
Baruch Siach <baruch@tkos.co.il>
2017-12-31 00:31:29 +0200

    json-c: fix build without threads support
    An uClibc toolchain that does not support threads emits a warning when
    _REENTRANT is defined. This breaks build with -Werror. Remove -Werror as
    a workaround to fix the build.
    
    This issue as been reported upstream
    (https://github.com/json-c/json-c/issues/396).
    
    Fixes:
    http://autobuild.buildroot.net/results/606/60683141f2ef320a28820c4cd65d2b951123b147/
    http://autobuild.buildroot.net/results/969/969faba96d94c1bda7e06b005d8f3a3f7175a0a2/
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/json-c/json-c.mk
+++ package/json-c/json-c.mk
@@ -10,5 +10,8 @@ JSON_C_INSTALL_STAGING = YES
 JSON_C_LICENSE = MIT
 JSON_C_LICENSE_FILES = COPYING
 
+# Patching configure.ac
+JSON_C_AUTORECONF = YES
+
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
