8b0a90d4f13e14495d7e9670ed661d430273dff7
Adam Duskett <aduskett@gmail.com>
2017-12-30 14:24:37 -0500

    json-for-modern-cpp: new package
    A json library that aims to make json feel like a first class data
    type using C++11, json for modern c++ is a easy to use, flexible,
    modern json library.
    
    Design goals include:
    
    - Intuitive syntax. In languages such as Python, JSON feels like a
      first class data type. All the operator magic of modern C++ is used
      to achieve the same feeling in c++.
    
    - Trivial integration. The whole code consists of a single header file
      json.hpp. That's it. No library, no subproject, no dependencies, no
      complex build system. The class is written in vanilla C++11.  All in
      all, everything should require no adjustment of compiler flags or
      project settings.
    
    - Serious testing. The class is heavily unit-tested and covers 100% of
      the code, including all exceptional behavior. Furthermore, it is
      checked with Valgrind that there are no memory leaks. To maintain
      high quality, the project is following the Core Infrastructure
      Initiative (CII) best practices.
    
    Signed-off-by: Adam Duskett <Adamduskett@outlook.com>
    [Thomas:
     - use "json-for-modern-cpp" everywhere as the name of the package
     - add <pkg>_INSTALL_TARGET = NO since it's a header only library]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

---
+++ package/json-for-modern-cpp/json-for-modern-cpp.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# json-for-modern-cpp
+#
+################################################################################
+
+JSON_FOR_MODERN_CPP_VERSION = v3.0.1
+JSON_FOR_MODERN_CPP_SOURCE = json-${JSON_FOR_MODERN_CPP_VERSION}.tar.gz
+JSON_FOR_MODERN_CPP_SITE = $(call github,nlohmann,json,$(JSON_FOR_MODERN_CPP_VERSION))
+JSON_FOR_MODERN_CPP_LICENSE = MIT
+JSON_FOR_MODERN_CPP_LICENSE_FILES = LICENSE.MIT
+JSON_FOR_MODERN_CPP_INSTALL_STAGING = YES
+# header only library
+JSON_FOR_MODERN_CPP_INSTALL_TARGET = NO
+JSON_FOR_MODERN_CPP_CONF_OPTS = -DJSON_BuildTests=OFF
+
+$(eval $(cmake-package))
