7007822f75dfcd402d4df91aff21bee260423183
Calin Crisan <ccrisan@gmail.com>
2017-12-31 20:15:54 +0200

    nanopineo2: add firmware update support

--- board/nanopineo2/postscript.sh
+++ board/nanopineo2/postscript.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 cp $IMG_DIR/Image $BOOT_DIR
-cp $IMG_DIR/boot.scr $BOOT_DIR
+cp $BOARD_DIR/boot.scr $BOOT_DIR
+cp $BOARD_DIR/boot-fwupdater.scr $BOOT_DIR
 cp $BOARD_DIR/sun50i-h5-nanopi-neo2.dtb $BOOT_DIR
-#cp $BOARD_DIR/fwupdater.img $BOOT_DIR
+cp $BOARD_DIR/rootfs.cpio.uboot $BOOT_DIR
 
