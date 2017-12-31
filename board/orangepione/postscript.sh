8d3fc2ad82e953d4f11256e859b917455775c5e5
Calin Crisan <ccrisan@gmail.com>
2017-12-31 23:23:25 +0200

    orangepione: add firmware update support

--- board/orangepione/postscript.sh
+++ board/orangepione/postscript.sh
@@ -4,4 +4,4 @@ cp $IMG_DIR/zImage $BOOT_DIR
 cp $IMG_DIR/sun8i-h3-orangepi-one.dtb $BOOT_DIR
 cp $BOARD_DIR/boot.scr $BOOT_DIR
 cp $BOARD_DIR/boot-fwupdater.scr $BOOT_DIR
-
+cp $BOARD_DIR/rootfs.cpio.uboot $BOOT_DIR
