f7489cfcfdd3c3ce71ca31675808308c5e74d109
Baruch Siach <baruch@tkos.co.il>
2018-01-01 21:24:42 +0200

    sysstat: bump to version 11.6.1
    The isag tool has been removed in version 11.5.5; no need to avoid its
    installation anymore.
    
    Add license file hash.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/sysstat/sysstat.mk
+++ package/sysstat/sysstat.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SYSSTAT_VERSION = 11.4.4
+SYSSTAT_VERSION = 11.6.1
 SYSSTAT_SOURCE = sysstat-$(SYSSTAT_VERSION).tar.xz
 SYSSTAT_SITE = http://pagesperso-orange.fr/sebastien.godard
 SYSSTAT_CONF_OPTS = --disable-file-attr --disable-sensors
@@ -12,9 +12,5 @@ SYSSTAT_DEPENDENCIES = host-gettext $(TARGET_NLS_DEPENDENCIES)
 SYSSTAT_LICENSE = GPL-2.0+
 SYSSTAT_LICENSE_FILES = COPYING
 SYSSTAT_MAKE_OPTS += LFLAGS="$(TARGET_LDFLAGS) $(TARGET_NLS_LIBS)"
-
-# The isag tool is a post processing script that depends on tcl/tk
-# among other things. So we don't install it.
-SYSSTAT_INSTALL_TARGET_OPTS = DESTDIR=$(TARGET_DIR) INSTALL_ISAG=n install
 
 $(eval $(autotools-package))
