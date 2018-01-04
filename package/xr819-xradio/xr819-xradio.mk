120a015d1cf7683236cb8bbedcbd4f7110f4c09b
Sergey Matyukevich <geomatsi@gmail.com>
2018-01-04 21:36:28 +0300

    xr819-xradio: bump version to 33f4b1c2
    Out-of-tree driver has been updated according to mac80211
    and timer API changes in the upcoming Linux v4.15.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/xr819-xradio/xr819-xradio.mk
+++ package/xr819-xradio/xr819-xradio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XR819_XRADIO_VERSION = c03a053a9e731d56c89c0c09627e886db2a0e04e
+XR819_XRADIO_VERSION = 33f4b1c25eff0d9db7cbac19f36b130da9857f37
 XR819_XRADIO_SITE = $(call github,fifteenhex,xradio,$(XR819_XRADIO_VERSION))
 XR819_XRADIO_LICENSE = GPL-2.0
 XR819_XRADIO_LICENSE_FILES = LICENSE
