60f492040aa6f0c5d50acb0cb315c111065ac307
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-01-04 20:17:39 +0100

    daq: libpcap is optional not mandatory
    daq can be compiled without libpcap thanks to --enable-pcap-module and
     --disable-pcap-module options.
    libpcap is just a mandadory dependency of snort.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/daq/daq.mk
+++ package/daq/daq.mk
@@ -10,16 +10,22 @@ DAQ_SOURCE = daq-$(DAQ_VERSION).tar.gz
 DAQ_LICENSE = GPL-2.0
 DAQ_LICENSE_FILES = COPYING
 DAQ_INSTALL_STAGING = YES
-DAQ_DEPENDENCIES = libpcap libdnet
+DAQ_DEPENDENCIES = libdnet
 
 # package does not build in parallel due to improper make rules
 # related to the generation of the tokdefs.h header file
 DAQ_MAKE = $(MAKE1)
 
+ifeq ($(BR2_PACKAGE_LIBPCAP),y)
+DAQ_DEPENDENCIES += libpcap
 # assume these flags are available to prevent configure from running
 # test programs while cross compiling
-DAQ_CONF_ENV = \
+DAQ_CONF_ENV += \
 	ac_cv_lib_pcap_pcap_lib_version=yes \
 	daq_cv_libpcap_version_1x=yes
+DAQ_CONF_OPTS += --enable-pcap-module
+else
+DAQ_CONF_OPTS += --disable-pcap-module
+endif
 
 $(eval $(autotools-package))
