650e1639e2014ba126b017d2ef1f1998b7513f9f
Gary Bisson <gary.bisson@boundarydevices.com>
2018-01-05 15:39:43 +0100

    imx-kobs: bump revision to a0e9adce
    Changelog:
    - Fix compiler error with kernel headers >= 4.4
    - Add i.MX8 platforms support
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/freescale-imx/imx-kobs/imx-kobs.mk
+++ package/freescale-imx/imx-kobs/imx-kobs.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-IMX_KOBS_VERSION = b402243a04e5a6760a860445e5ff6a931d86f794
-IMX_KOBS_SITE = $(call github,NXPmicro,imx-kobs,$(IMX_KOBS_VERSION))
+IMX_KOBS_VERSION = a0e9adce2fb7fcd57e794d7f9a5deba0f94f521b
+IMX_KOBS_SITE = $(call github,codeauroraforum,imx-kobs,$(IMX_KOBS_VERSION))
 IMX_KOBS_LICENSE = GPL-2.0+
 IMX_KOBS_LICENSE_FILES = COPYING
 
