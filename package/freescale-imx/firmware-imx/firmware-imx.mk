b0c934cfc2981b12b338fad4ba28eb0faf19c332
Gary Bisson <gary.bisson@boundarydevices.com>
2018-01-05 15:39:39 +0100

    firmware-imx: bump to version 6.0
    Cc: Arnout Vandecappelle <arnout@mind.be>
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/freescale-imx/firmware-imx/firmware-imx.mk
+++ package/freescale-imx/firmware-imx/firmware-imx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FIRMWARE_IMX_VERSION = 5.4
+FIRMWARE_IMX_VERSION = 6.0
 FIRMWARE_IMX_SITE = $(FREESCALE_IMX_SITE)
 FIRMWARE_IMX_SOURCE = firmware-imx-$(FIRMWARE_IMX_VERSION).bin
 
