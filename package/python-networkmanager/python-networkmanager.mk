4f4af171d8cb3d87432610c7c87d4ca3ca34b073
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2018-01-09 09:47:43 +0100

    python-networkmanager: remove default <pkg>_SOURCE variable
    <pkg>-<pkg-version>.tar.gz is the default value, so there's no need to
    explicitly specify it.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-networkmanager/python-networkmanager.mk
+++ package/python-networkmanager/python-networkmanager.mk
@@ -5,7 +5,6 @@
 ################################################################################
 
 PYTHON_NETWORKMANAGER_VERSION = 2.0.1
-PYTHON_NETWORKMANAGER_SOURCE = python-networkmanager-$(PYTHON_NETWORKMANAGER_VERSION).tar.gz
 PYTHON_NETWORKMANAGER_SITE = https://pypi.python.org/packages/d7/f9/5cbd99fd24a072875ce048e48d1754285f137aab447de8fee63b6cba990a
 PYTHON_NETWORKMANAGER_SETUP_TYPE = setuptools
 PYTHON_NETWORKMANAGER_LICENSE = Zlib
