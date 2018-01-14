0f75ed80a21c83098a59400087d37f64fe93b565
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-01-14 19:18:27 +0100

    package/python-mwclient: bump version to 0.8.7
    Removed patch applied upstream:
    https://github.com/mwclient/mwclient/commit/60d4b933184172b98e4b31b79b4420c35571bbea
    
    Added license hash.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/python-mwclient/python-mwclient.mk
+++ package/python-mwclient/python-mwclient.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MWCLIENT_VERSION = 0.8.6
+PYTHON_MWCLIENT_VERSION = 0.8.7
 PYTHON_MWCLIENT_SOURCE = mwclient-$(PYTHON_MWCLIENT_VERSION).tar.gz
-PYTHON_MWCLIENT_SITE = https://pypi.python.org/packages/cd/38/beaf985032b42a0b0c8f9028b469c4dcb0bd7bfab62707ec27af7e890e84
+PYTHON_MWCLIENT_SITE = https://pypi.python.org/packages/63/05/ddf7d1b0d3a1dc9ee650dcaef7ddfbb980b4d2f0c41128c5f9e6fed5e8e2
 PYTHON_MWCLIENT_LICENSE = MIT
 PYTHON_MWCLIENT_LICENSE_FILES = mwclient/__init__.py
 PYTHON_MWCLIENT_SETUP_TYPE = setuptools
