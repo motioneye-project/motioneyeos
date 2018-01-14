22126be85c2824d6379bae4481577962d172e207
Peter Korsgaard <peter@korsgaard.com>
2018-01-14 23:47:11 +0100

    xen: bump to version 4.9.1
    Drop 0003-tools-libxc-xc_dom_arm-add-missing-variable-initiali.patch as that
    is now upstream:
    
    https://xenbits.xenproject.org/gitweb/?p=xen.git;a=commit;h=88bfbf90e35f1213f9967a97dee0b2039f9998a4
    
    Drop security patches as 4.9.1 includes up to xsa-245.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/xen/xen.mk
+++ package/xen/xen.mk
@@ -4,21 +4,8 @@
 #
 ################################################################################
 
-XEN_VERSION = 4.9.0
+XEN_VERSION = 4.9.1
 XEN_SITE = https://downloads.xenproject.org/release/xen/$(XEN_VERSION)
-XEN_PATCH = \
-	https://xenbits.xenproject.org/xsa/xsa226.patch \
-	https://xenbits.xenproject.org/xsa/xsa227.patch \
-	https://xenbits.xenproject.org/xsa/xsa228.patch \
-	https://xenbits.xenproject.org/xsa/xsa230.patch \
-	https://xenbits.xenproject.org/xsa/xsa231-4.9.patch \
-	https://xenbits.xenproject.org/xsa/xsa232.patch \
-	https://xenbits.xenproject.org/xsa/xsa233.patch \
-	https://xenbits.xenproject.org/xsa/xsa234-4.9.patch \
-	https://xenbits.xenproject.org/xsa/xsa235-4.9.patch \
-	https://xenbits.xenproject.org/xsa/xsa245/0001-xen-page_alloc-Cover-memory-unreserved-after-boot-in.patch \
-	https://xenbits.xenproject.org/xsa/xsa245/0002-xen-arm-Correctly-report-the-memory-region-in-the-du.patch
-
 XEN_LICENSE = GPL-2.0
 XEN_LICENSE_FILES = COPYING
 XEN_DEPENDENCIES = host-acpica host-python
