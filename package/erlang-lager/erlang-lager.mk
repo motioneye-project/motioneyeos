375051f40212f3d2cab23909e8d6df807cda8ff9
Johan Oudinet <johan.oudinet@gmail.com>
2018-01-16 15:06:36 +0100

    erlang-lager: Bump to version 3.4.2
    Change repository from basho to erlang-lager.
    
    Signed-off-by: Johan Oudinet <johan.oudinet@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/erlang-lager/erlang-lager.mk
+++ package/erlang-lager/erlang-lager.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-ERLANG_LAGER_VERSION = 3.2.1
-ERLANG_LAGER_SITE = $(call github,basho,lager,$(ERLANG_LAGER_VERSION))
+ERLANG_LAGER_VERSION = 3.4.2
+ERLANG_LAGER_SITE = $(call github,erlang-lager,lager,$(ERLANG_LAGER_VERSION))
 ERLANG_LAGER_LICENSE = Apache-2.0
 ERLANG_LAGER_LICENSE_FILES = LICENSE
 ERLANG_LAGER_DEPENDENCIES = erlang-goldrush
