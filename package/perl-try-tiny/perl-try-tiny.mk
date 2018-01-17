3d15b368ad26739738a05f6c0c4b625da03db269
Francois Perrad <fperrad@gmail.com>
2018-01-17 18:45:09 +0100

    perl-try-tiny: bump to version 0.30
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/perl-try-tiny/perl-try-tiny.mk
+++ package/perl-try-tiny/perl-try-tiny.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_TRY_TINY_VERSION = 0.28
+PERL_TRY_TINY_VERSION = 0.30
 PERL_TRY_TINY_SOURCE = Try-Tiny-$(PERL_TRY_TINY_VERSION).tar.gz
 PERL_TRY_TINY_SITE = $(BR2_CPAN_MIRROR)/authors/id/E/ET/ETHER
 PERL_TRY_TINY_LICENSE = MIT
