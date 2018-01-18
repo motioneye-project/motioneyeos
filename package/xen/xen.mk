7d5796062f19db89880dac899030ac4cbc0372f2
Alistair Francis <alistair.francis@xilinx.com>
2018-01-18 10:55:48 -0800

    package/xen: Bump to latest 4.10.0 release
    Signed-off-by: Alistair Francis <alistair.francis@xilinx.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/xen/xen.mk
+++ package/xen/xen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XEN_VERSION = 4.9.1
+XEN_VERSION = 4.10.0
 XEN_SITE = https://downloads.xenproject.org/release/xen/$(XEN_VERSION)
 XEN_LICENSE = GPL-2.0
 XEN_LICENSE_FILES = COPYING
