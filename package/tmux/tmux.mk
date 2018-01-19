8491c62f44e0ad7a0b7bed6f922f814004d4a6bd
Romain Naour <romain.naour@smile.fr>
2018-01-19 11:15:05 +0100

    package/tmux: add /usr/bin/tmux to /etc/shells
    When tmux is selected, /usr/bin/tmux is not added to /etc/shells
    (see man shells). So, login tools like dropbear reject the ssh
    connections for users using tmux as shell in /etc/passwd.
    
    buildroot authpriv.warn dropbear[853]: User 'kubu' has invalid shell, rejected
    
    Signed-off-by: Romain Naour <romain.naour@smile.fr>
    Cc: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/tmux/tmux.mk
+++ package/tmux/tmux.mk
@@ -10,4 +10,12 @@ TMUX_LICENSE = ISC
 TMUX_LICENSE_FILES = README
 TMUX_DEPENDENCIES = libevent ncurses host-pkgconf
 
+# Add /usr/bin/tmux to /etc/shells otherwise some login tools like dropbear
+# can reject the user connection. See man shells.
+define TMUX_ADD_TMUX_TO_SHELLS
+	grep -qsE '^/usr/bin/tmux$$' $(TARGET_DIR)/etc/shells \
+		|| echo "/usr/bin/tmux" >> $(TARGET_DIR)/etc/shells
+endef
+TMUX_TARGET_FINALIZE_HOOKS += TMUX_ADD_TMUX_TO_SHELLS
+
 $(eval $(autotools-package))
