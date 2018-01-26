4f6db0d50449f26a0280f9ea9316a041729f5b22
Zoltan Gyarmati <mr.zoltan.gyarmati@gmail.com>
2018-01-26 19:54:44 +0100

    package/tinc: upgrade to 1.0.33, install upstream's systemd files
    Signed-off-by: Zoltan Gyarmati <mr.zoltan.gyarmati@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tinc/tinc.mk
+++ package/tinc/tinc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TINC_VERSION = 1.0.30
+TINC_VERSION = 1.0.33
 TINC_SITE = http://www.tinc-vpn.org/packages
 TINC_DEPENDENCIES = lzo openssl zlib
 TINC_LICENSE = GPL-2.0+ with OpenSSL exception
@@ -15,6 +15,11 @@ ifeq ($(BR2_TOOLCHAIN_SUPPORTS_PIE),)
 TINC_CONF_ENV += \
 	ax_cv_check_cflags___fPIE=no \
 	ax_cv_check_ldflags___pie=no
+endif
+
+ifeq ($(BR2_INIT_SYSTEMD),y)
+TINC_DEPENDENCIES += systemd
+TINC_CONF_OPTS += --with-systemdsystemunitdir=/usr/lib/systemd/system
 endif
 
 $(eval $(autotools-package))
