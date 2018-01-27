9a893f56ce833a59aad0cb61a2f932aa622e4c8a
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-01-27 14:42:29 +0100

    package/freeswitch: bump version to 1.6.20
    Added license hashes.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/freeswitch/freeswitch.mk
+++ package/freeswitch/freeswitch.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FREESWITCH_VERSION = 1.6.19
+FREESWITCH_VERSION = 1.6.20
 FREESWITCH_SOURCE = freeswitch-$(FREESWITCH_VERSION).tar.xz
 FREESWITCH_SITE = http://files.freeswitch.org/freeswitch-releases
 # External modules need headers/libs from staging
