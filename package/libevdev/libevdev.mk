e4e4f7da179110a1fcd58c60056bc2ce41869af7
Peter Seiderer <ps.report@gmx.net>
2018-01-30 21:01:21 +0100

    libevdev: bump version to 1.5.8
    For details see [1].
    
    [1] https://lists.freedesktop.org/archives/input-tools/2018-January/001447.html
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libevdev/libevdev.mk
+++ package/libevdev/libevdev.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBEVDEV_VERSION = 1.5.7
+LIBEVDEV_VERSION = 1.5.8
 LIBEVDEV_SITE = http://www.freedesktop.org/software/libevdev
 LIBEVDEV_SOURCE = libevdev-$(LIBEVDEV_VERSION).tar.xz
 LIBEVDEV_LICENSE = X11
