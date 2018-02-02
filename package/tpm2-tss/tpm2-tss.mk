7b3bb51809517ea045dedc8544db51fc0515d5b4
Carlos Santos <casantos@datacom.ind.br>
2018-02-02 13:56:11 -0200

    tpm2-tss: new package
    OSS implementation of the TCG TPM2 Software Stack (TSS2). This stack
    consists of the following layers from top to bottom:
    
    * System API (SAPI) as described in the system level API and TPM command
      transmission interface specification. This API is a 1-to-1 mapping of
      the TPM2 commands documented in Part 3 of the TPM2 specification.
      Additionally there are asynchronous versions of each command. These
      asynchronous variants may be useful for integration into event-driven
      programming environments. Both the synchronous and asynchronous API
      are exposed through a single library: libsapi.
    
    * TPM Command Transmission Interface (TCTI) that is described in the
      same specification. This API provides a standard interface to transmit
      / receive TPM command / response buffers. It is expected that any
      number of libraries implementing the TCTI API will be implemented as a
      way to abstract various platform specific IPC mechanisms. Currently
      this repository provides two TCTI implementations: libtcti-device and
      libtcti-socket. The prior should be used for direct access to the TPM
      through the Linux kernel driver. The later implements the protocol
      exposed by the Microsoft software TPM2 simulator.
    
    Signed-off-by: Carlos Santos <casantos@datacom.ind.br>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/tpm2-tss/tpm2-tss.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# tpm2-tss
+#
+################################################################################
+
+TPM2_TSS_VERSION = 1.3.0
+TPM2_TSS_SITE = https://github.com/tpm2-software/tpm2-tss/releases/download/$(TPM2_TSS_VERSION)
+TPM2_TSS_LICENSE = BSD-2-Clause
+TPM2_TSS_LICENSE_FILES = LICENSE
+TPM2_TSS_INSTALL_STAGING = YES
+TPM2_TSS_DEPENDENCIES = liburiparser host-pkgconf
+
+$(eval $(autotools-package))
