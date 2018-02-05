c032e6825ad96e6d4b69cecde2402c02a2a356b5
Romain Naour <romain.naour@gmail.com>
2018-02-05 21:57:16 +0100

    package/glibc: security bump to 2.27
    See: https://sourceware.org/ml/libc-announce/2018/msg00000.html
    https://sourceware.org/glibc/wiki/Release/2.27
    
    Fixes the following CVEs:
     CVE-2017-1000408
     CVE-2017-1000409
     CVE-2017-16997
     CVE-2018-1000001
     CVE-2018-6485
    
    While at it, add license file hashes.
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -10,7 +10,7 @@ GLIBC_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,glibc,$(GLIBC_VE
 else
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master
-GLIBC_VERSION = glibc-2.26-107-g73a92363619e52c458146e903dfb9b1ba823aa40
+GLIBC_VERSION = glibc-2.27
 # Upstream doesn't officially provide an https download link.
 # There is one (https://sourceware.org/git/glibc.git) but it's not reliable,
 # sometimes the connection times out. So use an unofficial github mirror.
