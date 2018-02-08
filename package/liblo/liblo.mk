e0e460df83865bb45567d5283e0f3528629a7050
Alex Baldwin <alexbaldwinmusic@gmail.com>
2018-02-08 20:37:48 +0100

    liblo : bump version to 0.29
    Starting from version 0.28, C++ wrappers are provided as well. The
    configure script appropriately detects the availability of a C++
    compiler so no Buildroot changes are needed for this.
    
    Building fails with --enable-ipv6 so --disable-ipv6 is needed in
    CONF_OPTS.  This is known by liblo developers and building without
    ipv6 support is recomennded.
    
    Included hash for license files.
    
    Signed-off-by: Alex Baldwin <alexbaldwinmusic@gmail.com>
    [Thomas: use single sha256 hash for license file.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/liblo/liblo.mk
+++ package/liblo/liblo.mk
@@ -4,11 +4,14 @@
 #
 ################################################################################
 
-LIBLO_VERSION = 0.26
+LIBLO_VERSION = 0.29
 LIBLO_SITE = http://downloads.sourceforge.net/project/liblo/liblo/$(LIBLO_VERSION)
 
 LIBLO_LICENSE = LGPL-2.1+
 LIBLO_LICENSE_FILES = COPYING
 LIBLO_INSTALL_STAGING = YES
+
+# IPv6 support broken, issue known upstream
+LIBLO_CONF_OPTS = --disable-ipv6
 
 $(eval $(autotools-package))
