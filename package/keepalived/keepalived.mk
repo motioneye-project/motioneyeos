6320325eba253935f53e75e8ed365ad704d2b443
Baruch Siach <baruch@tkos.co.il>
2018-02-09 12:28:47 +0200

    keepalived: bump to version 1.4.1
    Add license file hash.
    
    Cc: Ilias Apalodimas <apalos@gmail.com>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/keepalived/keepalived.mk
+++ package/keepalived/keepalived.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KEEPALIVED_VERSION = 1.3.5
+KEEPALIVED_VERSION = 1.4.1
 KEEPALIVED_SITE = http://www.keepalived.org/software
 KEEPALIVED_DEPENDENCIES = host-pkgconf openssl popt
 KEEPALIVED_LICENSE = GPL-2.0+
