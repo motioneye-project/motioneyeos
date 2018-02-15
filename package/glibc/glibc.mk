f55ab4a08ff8e02575759d58a7972824e792e657
Peter Korsgaard <peter@korsgaard.com>
2018-02-16 00:01:00 +0100

    glibc: security bump to the latest commit on 2.26 branch
    Fixes the following security issues according to NEWS:
    
    CVE-2018-6485: An integer overflow in the implementation of the
    posix_memalign in memalign functions in the GNU C Library (aka
    
    CVE-2018-6551: The malloc implementation in the GNU C Library (aka glibc or
    libc6), from version 2.24 to 2.26 on powerpc, and only in version 2.26 on
    i386, did not properly handle malloc calls with arguments close to SIZE_MAX
    and could return a pointer to a heap region that is smaller than requested,
    eventually leading to heap corruption.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -10,7 +10,7 @@ GLIBC_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,glibc,$(GLIBC_VE
 else
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master
-GLIBC_VERSION = glibc-2.26-144-gbbabb868cd248763373d0db763bacd84ce27ede8
+GLIBC_VERSION = glibc-2.26-146-gd300041c533a3d837c9f37a099bcc95466860e98
 # Upstream doesn't officially provide an https download link.
 # There is one (https://sourceware.org/git/glibc.git) but it's not reliable,
 # sometimes the connection times out. So use an unofficial github mirror.
