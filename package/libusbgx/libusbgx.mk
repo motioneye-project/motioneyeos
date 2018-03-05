3ced4d018c1f533240977420b43898a1cef9e7be
John Keeping <john@metanate.com>
2018-03-05 14:26:38 +0000

    libusbgx: bump to v0.2.0
    The GitHub tag is really "libusbgx-0.2.0" so we end up with a slightly
    strange looking version and archive name, but this is correct given the
    tag.
    
    Signed-off-by: John Keeping <john@metanate.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libusbgx/libusbgx.mk
+++ package/libusbgx/libusbgx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUSBGX_VERSION = 2e3d43ee098ed928d1baa61ce791ce9ff4788c5a
+LIBUSBGX_VERSION = libusbgx-v0.2.0
 LIBUSBGX_SITE = $(call github,libusbgx,libusbgx,$(LIBUSBGX_VERSION))
 LIBUSBGX_LICENSE = GPL-2.0+ (examples), LGPL-2.1+ (library)
 LIBUSBGX_LICENSE_FILES = COPYING COPYING.LGPL
