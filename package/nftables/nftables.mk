43500d5c85df7f73ccb55ce866cbe9293b8b4b10
Baruch Siach <baruch@tkos.co.il>
2018-03-06 08:36:53 +0200

    nftables: bump to version 0.8.3
    Disable man pages and pdf build to avoid dependency on docbook and
    dblatex.
    
    Drop upstream patch.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/nftables/nftables.mk
+++ package/nftables/nftables.mk
@@ -4,13 +4,14 @@
 #
 ################################################################################
 
-NFTABLES_VERSION = 0.8.1
+NFTABLES_VERSION = 0.8.3
 NFTABLES_SOURCE = nftables-$(NFTABLES_VERSION).tar.bz2
 NFTABLES_SITE = http://www.netfilter.org/projects/nftables/files
 NFTABLES_DEPENDENCIES = gmp libmnl libnftnl host-bison host-flex \
 	host-pkgconf $(TARGET_NLS_DEPENDENCIES)
 NFTABLES_LICENSE = GPL-2.0
 NFTABLES_LICENSE_FILES = COPYING
+NFTABLES_CONF_OPTS = --disable-man-doc --disable-pdf-doc
 
 ifeq ($(BR2_PACKAGE_READLINE),y)
 NFTABLES_DEPENDENCIES += readline
@@ -23,12 +24,6 @@ ifeq ($(BR2_STATIC_LIBS)$(BR2_PACKAGE_LIBNFTNL_JSON),yy)
 NFTABLES_LIBS += -ljansson -lm
 endif
 
-NFTABLES_CONF_ENV = \
-	ac_cv_prog_CONFIG_PDF=no \
-	LIBS="$(NFTABLES_LIBS)" \
-	DBLATEX=no \
-	DOCBOOK2X_MAN=no \
-	DOCBOOK2MAN=no \
-	DB2X_DOCBOOK2MAN=no
+NFTABLES_CONF_ENV = LIBS="$(NFTABLES_LIBS)"
 
 $(eval $(autotools-package))
