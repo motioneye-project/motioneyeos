1314459a863daf92bd952f42e0180bc8fbb666fa
James Knight <james.d.knight@live.com>
2018-03-06 02:22:15 +0000

    inotify-tools: bump to version 3.20.1
    Bumping this package to the most recent stable version. Drops the
    need for the GCC 6.x patch as a related fix has been merged in [1].
    
    [1]: https://github.com/rvoicilas/inotify-tools/pull/65
    
    Signed-off-by: James Knight <james.d.knight@live.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/inotify-tools/inotify-tools.mk
+++ package/inotify-tools/inotify-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-INOTIFY_TOOLS_VERSION = 1df9af4d6cd0f4af4b1b19254bcf056aed4ae395
+INOTIFY_TOOLS_VERSION = 3.20.1
 INOTIFY_TOOLS_SITE = $(call github,rvoicilas,inotify-tools,$(INOTIFY_TOOLS_VERSION))
 INOTIFY_TOOLS_LICENSE = GPL
 INOTIFY_TOOLS_LICENSE_FILES = COPYING
