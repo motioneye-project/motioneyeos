9cd9c2ddf533f480d5fc502493c04dd7d82ccc9e
Jörg Krause <joerg.krause@embedded.rocks>
2018-03-07 23:52:57 +0100

    libmpdclient: bump to version 2.14
    Since version 2.13 libmpdclient only supports the meson build.
    Therefore, adapt to package infrastructure according to the user manual.
    
    Also add a hash for the license file.
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libmpdclient/libmpdclient.mk
+++ package/libmpdclient/libmpdclient.mk
@@ -5,12 +5,38 @@
 ################################################################################
 
 LIBMPDCLIENT_VERSION_MAJOR = 2
-LIBMPDCLIENT_VERSION = $(LIBMPDCLIENT_VERSION_MAJOR).10
+LIBMPDCLIENT_VERSION = $(LIBMPDCLIENT_VERSION_MAJOR).14
 LIBMPDCLIENT_SOURCE = libmpdclient-$(LIBMPDCLIENT_VERSION).tar.xz
 LIBMPDCLIENT_SITE = http://www.musicpd.org/download/libmpdclient/$(LIBMPDCLIENT_VERSION_MAJOR)
-LIBMPDCLIENT_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
 LIBMPDCLIENT_INSTALL_STAGING = YES
 LIBMPDCLIENT_LICENSE = BSD-3-Clause
 LIBMPDCLIENT_LICENSE_FILES = COPYING
 
-$(eval $(autotools-package))
+LIBMPDCLIENT_CONF_OPTS += \
+	--prefix=/usr \
+	--buildtype $(if $(BR2_ENABLE_DEBUG),debug,release) \
+	--cross-file $(HOST_DIR)/etc/meson/cross-compilation.conf
+
+LIBMPDCLIENT_NINJA_OPTS = $(if $(VERBOSE),-v)
+
+define LIBMPDCLIENT_CONFIGURE_CMDS
+	rm -rf $(@D)/build
+	mkdir -p $(@D)/build
+	$(TARGET_MAKE_ENV) meson $(LIBMPDCLIENT_CONF_OPTS) $(@D) $(@D)/build
+endef
+
+define LIBMPDCLIENT_BUILD_CMDS
+	$(TARGET_MAKE_ENV) ninja $(LIBMPDCLIENT_NINJA_OPTS) -C $(@D)/build
+endef
+
+define LIBMPDCLIENT_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) DESTDIR=$(TARGET_DIR) \
+		ninja $(LIBMPDCLIENT_NINJA_OPTS) -C $(@D)/build install
+endef
+
+define LIBMPDCLIENT_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) DESTDIR=$(STAGING_DIR) \
+		ninja $(LIBMPDCLIENT_NINJA_OPTS) -C $(@D)/build install
+endef
+
+$(eval $(generic-package))
