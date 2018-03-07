4af097fb38ff8ee8baacfd44e4d7adba03cd1684
Jörg Krause <joerg.krause@embedded.rocks>
2018-03-07 23:54:21 +0100

    mpd-mpc: bump to version 0.29
    Since version 0.29 MPDs mpc only supports the meson build. Therefore,
    adapt the package infrastructure according to the user manual.
    
    Also add a hash for the license file.
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mpd-mpc/mpd-mpc.mk
+++ package/mpd-mpc/mpd-mpc.mk
@@ -5,12 +5,33 @@
 ################################################################################
 
 MPD_MPC_VERSION_MAJOR = 0
-MPD_MPC_VERSION = $(MPD_MPC_VERSION_MAJOR).28
+MPD_MPC_VERSION = $(MPD_MPC_VERSION_MAJOR).29
 MPD_MPC_SITE = http://www.musicpd.org/download/mpc/$(MPD_MPC_VERSION_MAJOR)
 MPD_MPC_SOURCE = mpc-$(MPD_MPC_VERSION).tar.xz
 MPD_MPC_LICENSE = GPL-2.0+
 MPD_MPC_LICENSE_FILES = COPYING
-MPD_MPC_DEPENDENCIES = host-pkgconf libmpdclient
-MPD_MPC_CONF_ENV = ac_cv_prog_cc_c99='-std=c99'
+MPD_MPC_DEPENDENCIES = host-meson host-pkgconf libmpdclient
 
-$(eval $(autotools-package))
+MPD_MPC_CONF_OPTS += \
+	--prefix=/usr \
+	--buildtype $(if $(BR2_ENABLE_DEBUG),debug,release) \
+	--cross-file $(HOST_DIR)/etc/meson/cross-compilation.conf
+
+MPD_MPC_NINJA_OPTS = $(if $(VERBOSE),-v)
+
+define MPD_MPC_CONFIGURE_CMDS
+	rm -rf $(@D)/build
+	mkdir -p $(@D)/build
+	$(TARGET_MAKE_ENV) meson $(MPD_MPC_CONF_OPTS) $(@D) $(@D)/build
+endef
+
+define MPD_MPC_BUILD_CMDS
+	$(TARGET_MAKE_ENV) ninja $(MPD_MPC_NINJA_OPTS) -C $(@D)/build
+endef
+
+define MPD_MPC_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) DESTDIR=$(TARGET_DIR) ninja $(MPD_MPC_NINJA_OPTS) \
+		-C $(@D)/build install
+endef
+
+$(eval $(generic-package))
