9b224e6c209c2eb6188731ebad65fa97515580e0
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-03-08 17:42:52 +0100

    ncmpc: bump to version 0.29
    Since version 0.28 ncmpc only supports the meson build.
    Therefore, adapt to package infrastructure according to the user manual.
    
    Add a hash for the license file.
    
    Add a nmpc entry for myself in DEVELOPERS
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/ncmpc/ncmpc.mk
+++ package/ncmpc/ncmpc.mk
@@ -5,11 +5,33 @@
 ################################################################################
 
 NCMPC_VERSION_MAJOR = 0
-NCMPC_VERSION = $(NCMPC_VERSION_MAJOR).27
+NCMPC_VERSION = $(NCMPC_VERSION_MAJOR).29
 NCMPC_SOURCE = ncmpc-$(NCMPC_VERSION).tar.xz
 NCMPC_SITE = http://www.musicpd.org/download/ncmpc/$(NCMPC_VERSION_MAJOR)
-NCMPC_DEPENDENCIES = host-pkgconf libglib2 libmpdclient ncurses
+NCMPC_DEPENDENCIES = host-meson host-pkgconf libglib2 libmpdclient ncurses
 NCMPC_LICENSE = GPL-2.0+
 NCMPC_LICENSE_FILES = COPYING
 
-$(eval $(autotools-package))
+NCMPC_CONF_OPTS += \
+	--prefix=/usr \
+	--buildtype $(if $(BR2_ENABLE_DEBUG),debug,release) \
+	--cross-file $(HOST_DIR)/etc/meson/cross-compilation.conf
+
+NCMPC_NINJA_OPTS = $(if $(VERBOSE),-v)
+
+define NCMPC_CONFIGURE_CMDS
+	rm -rf $(@D)/build
+	mkdir -p $(@D)/build
+	$(TARGET_MAKE_ENV) meson $(NCMPC_CONF_OPTS) $(@D) $(@D)/build
+endef
+
+define NCMPC_BUILD_CMDS
+	$(TARGET_MAKE_ENV) ninja $(NCMPC_NINJA_OPTS) -C $(@D)/build
+endef
+
+define NCMPC_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) DESTDIR=$(TARGET_DIR) \
+		ninja $(NCMPC_NINJA_OPTS) -C $(@D)/build install
+endef
+
+$(eval $(generic-package))
