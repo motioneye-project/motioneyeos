d44816f611a552716c878642f397a58cb034088b
Ricardo Martincoski <ricardo.martincoski@gmail.com>
2018-03-13 00:09:41 -0300

    test_python_cryptography: fix code style
    Fix these warnings:
    E122 continuation line missing indentation or outdented
    E301 expected 1 blank line, found 0
    E302 expected 2 blank lines, found 1
    F401 'os' imported but unused
    
    Signed-off-by: Ricardo Martincoski <ricardo.martincoski@gmail.com>
    Cc: Yegor Yefremov <yegorslists@googlemail.com>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- support/testing/tests/package/test_python_cryptography.py
+++ support/testing/tests/package/test_python_cryptography.py
@@ -1,6 +1,5 @@
-import os
-
 from tests.package.test_python import TestPythonBase
+
 
 class TestPythonCryptography(TestPythonBase):
     def fernet_test(self, timeout=-1):
@@ -10,22 +9,26 @@ class TestPythonCryptography(TestPythonBase):
         _, exit_code = self.emulator.run(cmd, timeout)
         self.assertEqual(exit_code, 0)
 
+
 class TestPythonPy2Cryptography(TestPythonCryptography):
     config = TestPythonBase.config + \
-"""
-BR2_PACKAGE_PYTHON=y
-BR2_PACKAGE_PYTHON_CRYPTOGRAPHY=y
-"""
+        """
+        BR2_PACKAGE_PYTHON=y
+        BR2_PACKAGE_PYTHON_CRYPTOGRAPHY=y
+        """
+
     def test_run(self):
         self.login()
         self.fernet_test(40)
 
+
 class TestPythonPy3Cryptography(TestPythonCryptography):
     config = TestPythonBase.config + \
-"""
-BR2_PACKAGE_PYTHON3=y
-BR2_PACKAGE_PYTHON_CRYPTOGRAPHY=y
-"""
+        """
+        BR2_PACKAGE_PYTHON3=y
+        BR2_PACKAGE_PYTHON_CRYPTOGRAPHY=y
+        """
+
     def test_run(self):
         self.login()
         self.fernet_test(40)
