6c3c8ffac100f2618972173510542d69e8511dee
Peter Korsgaard <peter@korsgaard.com>
2018-03-13 14:49:41 +0100

    docker-compose: bump version to 1.20-rc2
    Fixes a race condition in ParallelStreamWriter.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/docker-compose/docker-compose.mk
+++ package/docker-compose/docker-compose.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-DOCKER_COMPOSE_VERSION = 1.20.0rc1
-DOCKER_COMPOSE_SITE = https://pypi.python.org/packages/ca/d9/21266285a3c34e8e023f2504c13ffb48e6acd1e43ccdd0c55188d7039505
+DOCKER_COMPOSE_VERSION = 1.20.0rc2
+DOCKER_COMPOSE_SITE = https://pypi.python.org/packages/b9/fe/44a2d249d86ef04ccf792c841fc73ffcf96e6138ca3a926de8ba032cf912
 DOCKER_COMPOSE_SETUP_TYPE = setuptools
 DOCKER_COMPOSE_LICENSE = Apache-2.0
 DOCKER_COMPOSE_LICENSE_FILES = LICENSE
