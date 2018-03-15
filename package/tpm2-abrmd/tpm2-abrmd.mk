dab335d901be0b26a1e8ea4e0c3343352d442e35
Carlos Santos <casantos@datacom.ind.br>
2018-03-15 09:56:00 -0300

    tpm2-abrmd: new package
    This is a system daemon implementing the TPM2 access broker (TAB) &
    Resource Manager (RM) spec from the TCG. The daemon (tpm2-abrmd) is
    implemented using Glib and the GObject system.
    
    Communication between the daemon and clients using the TPM is done with
    a combination of DBus and Unix pipes. DBus is used for discovery,
    session management and the 'cancel', 'setLocality', and 'getPollHandles'
    API calls (mostly these aren't yet implemented). Pipes are used to send
    and receive TPM commands and responses (respectively) between client and
    server.
    
    The daemon owns the com.intel.tss2.Tabrmd name on dbus. It can be
    configured to connect to either the system or the session bus.
    
    The package also provides a client library for interacting with the
    daemon via TPM Command Transmission Interface (TCTI). It is intended for
    use with the SAPI library (libsapi) like any other TCTI.
    
    [Peter: drop add default DAEMON_ARGS to init script, drop /etc/default file,
    	drop S30devtpmperms and fix permissions in S80tpm2-abrmd]
    Signed-off-by: Carlos Santos <casantos@datacom.ind.br>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/tpm2-abrmd/tpm2-abrmd.mk
@@ -0,0 +1,32 @@
+################################################################################
+#
+# tpm2-abrmd
+#
+################################################################################
+
+TPM2_ABRMD_VERSION = 1.3.0
+TPM2_ABRMD_SITE = https://github.com/tpm2-software/tpm2-abrmd/releases/download/$(TPM2_ABRMD_VERSION)
+TPM2_ABRMD_LICENSE = BSD-2-Clause
+TPM2_ABRMD_LICENSE_FILES = LICENSE
+TPM2_ABRMD_INSTALL_STAGING = YES
+TPM2_ABRMD_DEPENDENCIES = dbus libglib2 tpm2-tss host-pkgconf
+
+TPM2_ABRMD_CONF_OPTS += \
+	--with-systemdsystemunitdir=$(if $(BR2_INIT_SYSTEMD),/usr/lib/systemd/system,no) \
+	--with-udevrulesdir=$(if $(BR2_PACKAGE_HAS_UDEV),/usr/lib/udev/rules.d,no)
+
+define TPM2_ABRMD_INSTALL_INIT_SYSTEMD
+	$(TARGET_MAKE_ENV) $(MAKE1) -C $(@D) DESTDIR=$(TARGET_DIR) \
+		install-systemdpresetDATA install-systemdsystemunitDATA
+endef
+
+define TPM2_ABRMD_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 $(TPM2_ABRMD_PKGDIR)/S80tpm2-abrmd \
+		$(TARGET_DIR)/etc/init.d/S80tpm2-abrmd
+endef
+
+define TPM2_ABRMD_USERS
+	tss -1 tss -1 * - - - TPM2 Access Broker & Resource Management daemon
+endef
+
+$(eval $(autotools-package))
