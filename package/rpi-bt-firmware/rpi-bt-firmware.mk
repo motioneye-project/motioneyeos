355b87a1ac4626d1df79e5cb7fd4f8d10ac3294d
Ryan Coe <bluemrp9@gmail.com>
2018-03-17 09:11:17 -0700

    rpi-bt-firmware: remove compatibility symlink
    Commit 361d1b969ecdba1a8a62613626f90fc025b94660 defines FIRMWARE_DIR for
    bluez5_utils. The compatibility symlink for the firmware is no longer
    needed.
    
    Signed-off-by: Ryan Coe <bluemrp9@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/rpi-bt-firmware/rpi-bt-firmware.mk
+++ package/rpi-bt-firmware/rpi-bt-firmware.mk
@@ -10,10 +10,7 @@ RPI_BT_FIRMWARE_SITE_METHOD = git
 RPI_BT_FIRMWARE_LICENSE = PROPRIETARY
 RPI_BT_FIRMWARE_LICENSE_FILES = LICENCE.broadcom_bcm43xx
 
-# The BlueZ hciattach utility looks for firmware in /etc/firmware. Add a
-# compatibility symlink.
 define RPI_BT_FIRMWARE_INSTALL_TARGET_CMDS
-	ln -sf ../lib/firmware $(TARGET_DIR)/etc/firmware
 	$(INSTALL) -D -m 0644 $(@D)/BCM43430A1.hcd \
 		$(TARGET_DIR)/lib/firmware/BCM43430A1.hcd
 endef
