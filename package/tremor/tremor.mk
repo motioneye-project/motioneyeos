80266c95052024381898cada4c51d44207fddd80
Peter Korsgaard <peter@korsgaard.com>
2018-03-19 22:40:05 +0100

    tremor: security bump to fix CVE-2018-5146
    Prevent out-of-bounds write in codebook decoding.
    
    Codebooks that are not an exact divisor of the partition size are now
    truncated to fit within the partition.
    
    Upstream has migrated from subversion to git, so change to git and bump the
    version to include the fix for CVE-2018-5146.
    
    While we're at it, also add a hash file.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tremor/tremor.mk
+++ package/tremor/tremor.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-TREMOR_SITE = http://svn.xiph.org/trunk/Tremor
-TREMOR_SITE_METHOD = svn
-TREMOR_VERSION = 19427
+TREMOR_VERSION = 7c30a66346199f3f09017a09567c6c8a3a0eedc8
+TREMOR_SITE = https://git.xiph.org/tremor.git
+TREMOR_SITE_METHOD = git
 TREMOR_LICENSE = BSD-3-Clause
 TREMOR_LICENSE_FILES = COPYING
 
