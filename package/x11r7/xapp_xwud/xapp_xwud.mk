aa4c7cbefc2765d27ba95374b184795c7079acf0
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-03-20 18:17:58 +0100

    package/x11r7/xapp_xwud: bump version to 1.0.5
    Added all upstream hashes, added license hash.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xapp_xwud/xapp_xwud.mk
+++ package/x11r7/xapp_xwud/xapp_xwud.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XWUD_VERSION = 1.0.4
+XAPP_XWUD_VERSION = 1.0.5
 XAPP_XWUD_SOURCE = xwud-$(XAPP_XWUD_VERSION).tar.bz2
 XAPP_XWUD_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XWUD_LICENSE = MIT
