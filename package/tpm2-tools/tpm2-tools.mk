428dfe71f337a5de4540d57a244442d64b2d8e75
Carlos Santos <casantos@datacom.ind.br>
2018-03-22 17:27:52 -0300

    tpm2-tools: allow building without stack smashing protection (SSP)
    Disable SSP if the toolchain does not support it. This must be done
    explicitly because configure enables hardening by default but doesn't
    contain a link test, so it doesn't detect when libssp is missing.
    
    Signed-off-by: Carlos Santos <casantos@datacom.ind.br>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tpm2-tools/tpm2-tools.mk
+++ package/tpm2-tools/tpm2-tools.mk
@@ -10,4 +10,8 @@ TPM2_TOOLS_LICENSE = BSD-2-Clause
 TPM2_TOOLS_LICENSE_FILES = LICENSE
 TPM2_TOOLS_DEPENDENCIES = dbus libcurl libglib2 openssl tpm2-tss host-pkgconf
 
+# configure.ac doesn't contain a link test, so it doesn't detect when
+# libssp is missing.
+TPM2_TOOLS_CONF_OPTS = $(if $(BR2_TOOLCHAIN_HAS_SSP),--enable-hardening,--disable-hardening)
+
 $(eval $(autotools-package))
