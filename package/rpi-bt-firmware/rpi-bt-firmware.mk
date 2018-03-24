0f49bd05ea91d89fd511fd8e721743f8319e9e6f
Martin Bark <martin@barkynet.com>
2018-03-24 14:10:15 +0000

    package/rpi-bt-firmware: change to LibreELEC github repo
    Include BCM4345C0.hcd for the rpi3 b+
    
    Signed-off-by: Martin Bark <martin@barkynet.com>
    [Thomas: rebase on master, use make foreach loop instead of shell for
    loop.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/rpi-bt-firmware/rpi-bt-firmware.mk
+++ package/rpi-bt-firmware/rpi-bt-firmware.mk
@@ -4,15 +4,17 @@
 #
 ################################################################################
 
-RPI_BT_FIRMWARE_VERSION = a439f892bf549ddfefa9ba7ad1999cc515f233bf
-RPI_BT_FIRMWARE_SITE = https://aur.archlinux.org/pi-bluetooth.git
-RPI_BT_FIRMWARE_SITE_METHOD = git
+RPI_BT_FIRMWARE_VERSION = 18d7c931aff0a8a78360b9b9eaeb15d1224fb907
+RPI_BT_FIRMWARE_SITE = $(call github,LibreELEC,brcmfmac_sdio-firmware-rpi,$(RPI_BT_FIRMWARE_VERSION))
 RPI_BT_FIRMWARE_LICENSE = PROPRIETARY
 RPI_BT_FIRMWARE_LICENSE_FILES = LICENCE.broadcom_bcm43xx
 
+RPI_BT_FIRMWARE_FILES = brcm/BCM43430A1.hcd BCM4345C0.hcd
+
 define RPI_BT_FIRMWARE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/BCM43430A1.hcd \
-		$(TARGET_DIR)/lib/firmware/BCM43430A1.hcd
+	$(foreach file,$(RPI_BT_FIRMWARE_FILES),\
+		$(INSTALL) -D -m 0644 $(@D)/firmware/$(file) $(TARGET_DIR)/lib/firmware/$(notdir $(file))
+	)
 endef
 
 $(eval $(generic-package))
