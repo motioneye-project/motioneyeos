108837294191db0075aa0d05ce11f7c51a3dca14
Baruch Siach <baruch@tkos.co.il>
2018-03-25 21:40:39 +0300

    strace: bump to version 4.21
    Download site moved to strace.io. No upstream provided hashes.
    
    Drop the musl specific autoconf seed values. These values are now
    detected correctly.
    
    Add --enable-mpers=check to fix configure on aarch64 that does not
    support binaries for the compat personality.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/strace/strace.mk
+++ package/strace/strace.mk
@@ -4,19 +4,12 @@
 #
 ################################################################################
 
-STRACE_VERSION = 4.20
+STRACE_VERSION = 4.21
 STRACE_SOURCE = strace-$(STRACE_VERSION).tar.xz
-STRACE_SITE = http://downloads.sourceforge.net/project/strace/strace/$(STRACE_VERSION)
+STRACE_SITE = https://strace.io/files/$(STRACE_VERSION)
 STRACE_LICENSE = BSD-3-Clause
 STRACE_LICENSE_FILES = COPYING
-
-# strace bundle some kernel headers to build libmpers, this mixes userspace
-# headers and kernel headers which break the build with musl.
-# The stddef.h from gcc is used instead of the one from musl.
-ifeq ($(BR2_TOOLCHAIN_USES_MUSL),y)
-STRACE_CONF_OPTS += st_cv_m32_mpers=no \
-	st_cv_mx32_mpers=no
-endif
+STRACE_CONF_OPTS = --enable-mpers=check
 
 define STRACE_REMOVE_STRACE_GRAPH
 	rm -f $(TARGET_DIR)/usr/bin/strace-graph
