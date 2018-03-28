ec848f899dce5aae2197089d49131f84831eea94
Baruch Siach <baruch@tkos.co.il>
2018-03-29 00:14:01 +0300

    time: bump to version 1.9
    Fix seed variable name for the wait3 system call AC_TRY_RUN test.
    
    Remove manual installation; not needed anymore.
    
    Cc: Will Newton <will.newton@gmail.com>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/time/time.mk
+++ package/time/time.mk
@@ -4,16 +4,10 @@
 #
 ################################################################################
 
-TIME_VERSION = 1.8
+TIME_VERSION = 1.9
 TIME_SITE = $(BR2_GNU_MIRROR)/time
-TIME_CONF_ENV = ac_cv_func_wait3=yes
+TIME_CONF_ENV = ac_cv_func_wait3_rusage=yes
 TIME_LICENSE = GPL-3.0+
 TIME_LICENSE_FILES = COPYING
-
-# time uses an old version of automake that does not support
-# installing in DESTDIR.
-define TIME_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755  $(@D)/time $(TARGET_DIR)/usr/bin/time
-endef
 
 $(eval $(autotools-package))
