8c8f9fda90fd59c3120e1076f8507b16871ae4ed
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-03-29 17:27:05 +0200

    package/php: security bump to version 7.2.4
    Fixes https://bugs.php.net/bug.php?id=75605, no CVE-ID yet.
    
    Removed patch 0008, applied upstream. Re-numbered patch 0009.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 19e983a9540aa948d64423e63167aba2aff9fe41)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PHP_VERSION = 7.2.3
+PHP_VERSION = 7.2.4
 PHP_SITE = http://www.php.net/distributions
 PHP_SOURCE = php-$(PHP_VERSION).tar.xz
 PHP_INSTALL_STAGING = YES
