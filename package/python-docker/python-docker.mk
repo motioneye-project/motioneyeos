d8151145f29e80e686d87675e7da7cae696196c5
Peter Korsgaard <peter@korsgaard.com>
2018-03-30 14:03:43 +0200

    python-docker: bump version to 3.1.4
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-docker/python-docker.mk
+++ package/python-docker/python-docker.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_DOCKER_VERSION = 3.1.1
+PYTHON_DOCKER_VERSION = 3.1.4
 PYTHON_DOCKER_SOURCE = docker-$(PYTHON_DOCKER_VERSION).tar.gz
-PYTHON_DOCKER_SITE = https://pypi.python.org/packages/0d/17/ad98e025e5528337c4dc5835a5874898eb226da17e4ffed732c894cb1938
+PYTHON_DOCKER_SITE = https://pypi.python.org/packages/7f/22/fd6e97c99a512f74d46dab2b450fe370eb2f83404ef790298e3fd012cd5c
 PYTHON_DOCKER_SETUP_TYPE = setuptools
 PYTHON_DOCKER_LICENSE = Apache-2.0
 PYTHON_DOCKER_LICENSE_FILES = LICENSE
