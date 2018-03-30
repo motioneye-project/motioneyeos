205b786376f1631a877144effb4899818ef7fbe2
Peter Korsgaard <peter@korsgaard.com>
2018-03-30 14:03:44 +0200

    docker-compose: bump version to 1.20.1
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/docker-compose/docker-compose.mk
+++ package/docker-compose/docker-compose.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-DOCKER_COMPOSE_VERSION = 1.20.0rc2
-DOCKER_COMPOSE_SITE = https://pypi.python.org/packages/b9/fe/44a2d249d86ef04ccf792c841fc73ffcf96e6138ca3a926de8ba032cf912
+DOCKER_COMPOSE_VERSION = 1.20.1
+DOCKER_COMPOSE_SITE = https://pypi.python.org/packages/25/4f/4e2b8ff942c9b3d96a81082590617c5c5fa006b066a4181b8d985ea3ac79
 DOCKER_COMPOSE_SETUP_TYPE = setuptools
 DOCKER_COMPOSE_LICENSE = Apache-2.0
 DOCKER_COMPOSE_LICENSE_FILES = LICENSE
