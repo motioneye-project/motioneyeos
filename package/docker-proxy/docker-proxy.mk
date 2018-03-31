53e1b222533fd110ab9472480ded126a4762db5e
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2018-03-31 15:27:36 +0200

    package/docker-proxy: convert to golang infrastructure
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    [Arnout: remove automatically added host-go dependency]
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/docker-proxy/docker-proxy.mk
+++ package/docker-proxy/docker-proxy.mk
@@ -10,37 +10,14 @@ DOCKER_PROXY_SITE = $(call github,docker,libnetwork,$(DOCKER_PROXY_VERSION))
 DOCKER_PROXY_LICENSE = Apache-2.0
 DOCKER_PROXY_LICENSE_FILES = LICENSE
 
-DOCKER_PROXY_DEPENDENCIES = host-go host-pkgconf
+DOCKER_PROXY_DEPENDENCIES = host-pkgconf
 
-DOCKER_PROXY_GOPATH = "$(@D)/gopath"
-DOCKER_PROXY_MAKE_ENV = $(HOST_GO_TARGET_ENV) \
-	CGO_ENABLED=1 \
-	CGO_NO_EMULATION=1 \
-	GOBIN="$(@D)/bin" \
-	GOPATH="$(DOCKER_PROXY_GOPATH)" \
-	PKG_CONFIG="$(PKG_CONFIG_HOST_BINARY)" \
-	$(TARGET_MAKE_ENV)
+DOCKER_PROXY_WORKSPACE = gopath
 
-ifeq ($(BR2_STATIC_LIBS),y)
-DOCKER_PROXY_GLDFLAGS += -extldflags '-static'
-endif
-
-define DOCKER_PROXY_CONFIGURE_CMDS
-	mkdir -p $(DOCKER_PROXY_GOPATH)/src/github.com/docker
-	ln -fs $(@D) $(DOCKER_PROXY_GOPATH)/src/github.com/docker/libnetwork
-endef
-
-define DOCKER_PROXY_BUILD_CMDS
-	cd $(@D)/gopath/src/github.com/docker/libnetwork; \
-	$(DOCKER_PROXY_MAKE_ENV) \
-	$(HOST_DIR)/usr/bin/go build -v \
-		-o $(@D)/bin/docker-proxy \
-		-ldflags "$(DOCKER_PROXY_GLDFLAGS)" \
-		github.com/docker/libnetwork/cmd/proxy
-endef
+DOCKER_PROXY_BUILD_TARGETS = cmd/proxy
 
 define DOCKER_PROXY_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/bin/docker-proxy $(TARGET_DIR)/usr/bin/docker-proxy
+	$(INSTALL) -D -m 0755 $(@D)/$(DOCKER_PROXY_BINDIR)/proxy $(TARGET_DIR)/usr/bin/docker-proxy
 endef
 
-$(eval $(generic-package))
+$(eval $(golang-package))
