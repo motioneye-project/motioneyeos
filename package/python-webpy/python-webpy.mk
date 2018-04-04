dec4e2d0e8416533343284366e816fa418b33d94
Peter Korsgaard <peter@korsgaard.com>
2018-04-04 17:51:32 +0200

    python-webpy: security bump to version 0.39
    >From the changelog:
    
    2018-02-28 0.39
    * Fixed a security issue with the form module (tx Orange Tsai)
    * Fixed a security issue with the db module (tx Adrián Brav and Orange Tsai)
    
    2016-07-08 0.38
    ..
    * Fixed a potential remote exeution risk in `reparam` (tx Adrián Brav)
    
    License files are still not included on pypi, so continue to use the git
    repo. Upstream has unfortunately not tagged 0.39, so use the latest commit
    on the 0.39 branch.  A request to fix this has been submitted:
    
    https://github.com/webpy/webpy/issues/449
    
    0.39 now uses setuptools, so change the _SETUP_TYPE.
    
    Add hashes for the license files.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit ce559162fca39c273583bea0dbed643229769d8c)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-webpy/python-webpy.mk
+++ package/python-webpy/python-webpy.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-PYTHON_WEBPY_VERSION = webpy-0.37
+# corresponds to 0.39
+PYTHON_WEBPY_VERSION = 6df75fe581e0e838d28334d5c53f52421560d38b
 PYTHON_WEBPY_SITE = $(call github,webpy,webpy,$(PYTHON_WEBPY_VERSION))
-PYTHON_WEBPY_SETUP_TYPE = distutils
+PYTHON_WEBPY_SETUP_TYPE = setuptools
 PYTHON_WEBPY_LICENSE = Public Domain, CherryPy License
 PYTHON_WEBPY_LICENSE_FILES = LICENSE.txt web/wsgiserver/LICENSE.txt
 
