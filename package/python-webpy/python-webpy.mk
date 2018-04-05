9a095026c17a10e3e474d94674f6501afd54c5eb
Peter Korsgaard <peter@korsgaard.com>
2018-04-05 08:42:15 +0200

    python-webpy: use webpy-0.39 tag
    No functional change, but upstream has now tagged the release, so use the
    tag instead of the sha1.
    
    https://github.com/webpy/webpy/issues/449
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 01320bb9ff297bac38a4c9bc32ae505ac79d600f)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-webpy/python-webpy.mk
+++ package/python-webpy/python-webpy.mk
@@ -4,8 +4,7 @@
 #
 ################################################################################
 
-# corresponds to 0.39
-PYTHON_WEBPY_VERSION = 6df75fe581e0e838d28334d5c53f52421560d38b
+PYTHON_WEBPY_VERSION = webpy-0.39
 PYTHON_WEBPY_SITE = $(call github,webpy,webpy,$(PYTHON_WEBPY_VERSION))
 PYTHON_WEBPY_SETUP_TYPE = setuptools
 PYTHON_WEBPY_LICENSE = Public Domain, CherryPy License
