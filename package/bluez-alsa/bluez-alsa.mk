36f2d55eff9118c9a13dca7c77efdd4b781bd77c
Jörg Krause <joerg.krause@embedded.rocks>
2018-04-06 10:19:24 +0200

    bluez-alsa: new package
    bluez-alsa is a Bluetooth Audio ALSA Backend.
    
    Note that we are using the latest commit from git master branch
    instead of the latest version tag v1.2.0 which is almost a year old
    and is 64 commits behind master.
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    [Thomas:
     - simplify the comments in the "depends on"
     - move the Config.in comment below the hcitop option to let
       menuconfig properly indent the hcitop option under bluez-alsa]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/bluez-alsa/bluez-alsa.mk
@@ -0,0 +1,43 @@
+################################################################################
+#
+# bluez-alsa
+#
+################################################################################
+
+BLUEZ_ALSA_VERSION = 9045edb436ea755f395a2e09e4525b5defad286a
+BLUEZ_ALSA_SITE = $(call github,Arkq,bluez-alsa,$(BLUEZ_ALSA_VERSION))
+BLUEZ_ALSA_LICENSE = MIT
+BLUEZ_ALSA_LICENSE_FILES = LICENSE.txt
+BLUEZ_ALSA_DEPENDENCIES = alsa-lib bluez5_utils libglib2 sbc host-pkgconf
+
+# git repo, no configure
+BLUEZ_ALSA_AUTORECONF = YES
+
+# Autoreconf requires an existing m4 directory
+define BLUEZ_ALSA_MKDIR_M4
+	mkdir -p $(@D)/m4
+endef
+BLUEZ_ALSA_POST_PATCH_HOOKS += BLUEZ_ALSA_MKDIR_M4
+
+BLUEZ_ALSA_CONF_OPTS = \
+	--enable-aplay \
+	--disable-debug-time \
+	--disable-pcm-test \
+	--with-alsaplugindir=/usr/lib/alsa-lib \
+	--with-alsadatadir=/usr/share/alsa
+
+ifeq ($(BR2_PACKAGE_FDK_AAC),y)
+BLUEZ_ALSA_DEPENDENCIES += fdk-aac
+BLUEZ_ALSA_CONF_OPTS += --enable-aac
+else
+BLUEZ_ALSA_CONF_OPTS += --disable-aac
+endif
+
+ifeq ($(BR2_PACKAGE_BLUEZ_ALSA_HCITOP),y)
+BLUEZ_ALSA_DEPENDENCIES += libbsd ncurses
+BLUEZ_ALSA_CONF_OPTS += --enable-hcitop
+else
+BLUEZ_ALSA_CONF_OPTS += --disable-hcitop
+endif
+
+$(eval $(autotools-package))
