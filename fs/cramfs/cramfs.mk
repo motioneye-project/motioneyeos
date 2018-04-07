265aee8c51718fe7370a3dbc91048ff60eb5909c
Chris Brandt <chris.brandt@renesas.com>
2018-04-07 08:35:43 -0500

    fs/cramfs: add xip option
    Cramfs now has the ability to XIP all ELF files.
    
    Cc: Nicolas Pitre <nico@linaro.org>
    Signed-off-by: Chris Brandt <chris.brandt@renesas.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- fs/cramfs/cramfs.mk
+++ fs/cramfs/cramfs.mk
@@ -10,6 +10,14 @@ else
 CRAMFS_OPTS = -L
 endif
 
+ifeq ($(BR2_TARGET_ROOTFS_CRAMFS_XIP),y)
+ifeq ($(BR2_USE_MMU),y)
+CRAMFS_OPTS += -X -X
+else
+CRAMFS_OPTS += -X
+endif
+endif
+
 define ROOTFS_CRAMFS_CMD
 	$(HOST_DIR)/bin/mkcramfs $(CRAMFS_OPTS) $(TARGET_DIR) $@
 endef
