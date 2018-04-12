f593067b5cd807a8899ba401825b8d0180155cb8
Erik Larsson <karl.erik.larsson@gmail.com>
2018-04-12 21:32:09 +0200

    package/imx-mkimage: add dependency to host-zlib
    Fixes:
    
      http://autobuild.buildroot.net/results/00d284b4841c7ec89475d509d5b8a8df8437ee3a/
    
    Signed-off-by: Erik Larsson <karl.erik.larsson@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/imx-mkimage/imx-mkimage.mk
+++ package/imx-mkimage/imx-mkimage.mk
@@ -9,6 +9,7 @@ IMX_MKIMAGE_SITE = https://source.codeaurora.org/external/imx/imx-mkimage
 IMX_MKIMAGE_SITE_METHOD = git
 IMX_MKIMAGE_LICENSE = GPL-2.0+
 IMX_MKIMAGE_LICENSE_FILES = iMX8dv/COPYING
+HOST_IMX_MKIMAGE_DEPENDENCIES = host-zlib
 
 define HOST_IMX_MKIMAGE_BUILD_CMDS
 	# Currently this only supports iMX8M. When more hardware is available
