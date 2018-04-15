131cf9bf4b599e71d8621189b2b49fbb11c15649
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-04-15 10:18:49 +0200

    libcap-ng: fix compilation without pthread.h
    Fixes:
     - http://autobuild.buildroot.net/results/6132f33fb282fda3c39deb292784b9006c9e7872
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    [Thomas: add missing AUTORECONF = YES, since we're now patching
    configure.ac]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libcap-ng/libcap-ng.mk
+++ package/libcap-ng/libcap-ng.mk
@@ -9,6 +9,8 @@ LIBCAP_NG_SITE = http://people.redhat.com/sgrubb/libcap-ng
 LIBCAP_NG_LICENSE = GPL-2.0+ (programs), LGPL-2.1+ (library)
 LIBCAP_NG_LICENSE_FILES = COPYING COPYING.LIB
 LIBCAP_NG_INSTALL_STAGING = YES
+# 0001-Fix-compilation-without-pthread.h.patch
+LIBCAP_NG_AUTORECONF = YES
 
 LIBCAP_NG_CONF_ENV = ac_cv_prog_swig_found=no
 LIBCAP_NG_CONF_OPTS = --without-python
