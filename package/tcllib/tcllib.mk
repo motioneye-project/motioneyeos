8d4f5dbafae8b99897bd3ee5389296b09ca35208
Romain Naour <romain.naour@smile.fr>
2018-04-20 10:29:00 +0200

    package/tcllib: update license to SPDX format
    While at it add the license file hash.
    
    Signed-off-by: Romain Naour <romain.naour@smile.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 2c82d7c605e53d3687ff63f31e135ec7ce92ea05)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tcllib/tcllib.mk
+++ package/tcllib/tcllib.mk
@@ -7,7 +7,7 @@
 TCLLIB_VERSION = 1.13
 TCLLIB_SOURCE = tcllib-$(TCLLIB_VERSION).tar.bz2
 TCLLIB_SITE = http://downloads.sourceforge.net/project/tcllib/tcllib/$(TCLLIB_VERSION)
-TCLLIB_LICENSE = tcl license
+TCLLIB_LICENSE = TCL
 TCLLIB_LICENSE_FILES = license.terms
 TCLLIB_DEPENDENCIES = host-tcl
 TCLLIB_CONF_ENV = ac_cv_path_tclsh="$(HOST_DIR)/bin/tclsh$(TCL_VERSION_MAJOR)"
