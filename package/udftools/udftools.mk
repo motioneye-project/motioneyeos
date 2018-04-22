eef425146223f220587c32ad880463b1c749660c
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2018-04-22 23:20:03 +0200

    udftool: add missing dependency on readline
    udftool/Config.in selects BR2_PACKAGE_READLINE, but udftool does not
    depend on it. Readline is not a runtime dependency: one of the udftool
    program links against it.
    
    This commit fixes this inconsistency.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/udftools/udftools.mk
+++ package/udftools/udftools.mk
@@ -8,5 +8,6 @@ UDFTOOLS_VERSION = 2.0
 UDFTOOLS_SITE = https://github.com/pali/udftools/releases/download/$(UDFTOOLS_VERSION)
 UDFTOOLS_LICENSE = GPL-2.0+
 UDFTOOLS_LICENSE_FILES = COPYING
+UDFTOOLS_DEPENDENCIES = readline
 
 $(eval $(autotools-package))
