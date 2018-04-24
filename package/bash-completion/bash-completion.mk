989470cf14a34f9f7a071a9be1e55425fd242915
Jeremy Rosen <jeremy.rosen@smile.fr>
2018-04-24 11:28:22 +0200

    package/bash-completion: new package
    Signed-off-by: Jeremy Rosen <jeremy.rosen@smile.fr>
    Signed-off-by: Romain Naour <romain.naour@smile.fr>
    [Thomas: transfer the explanation from the commit log into the
    Config.in help text, after rewording it.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/bash-completion/bash-completion.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# bash-completion
+#
+################################################################################
+
+BASH_COMPLETION_VERSION = 2.8
+BASH_COMPLETION_SITE = https://github.com/scop/bash-completion/releases/download/$(BASH_COMPLETION_VERSION)
+BASH_COMPLETION_SOURCE = bash-completion-$(BASH_COMPLETION_VERSION).tar.xz
+BASH_COMPLETION_LICENSE = GPL-2.0
+BASH_COMPLETION_LICENSE_FILES = COPYING
+
+# Install bash-completion.pc file
+BASH_COMPLETION_INSTALL_STAGING = YES
+
+$(eval $(autotools-package))
