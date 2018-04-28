dededf0ff2177e5aeb051db25e178bb951debce5
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-04-28 22:00:43 +0200

    libcrossguid: bump to version 0.2.2
    - Add hash for license file
    
    - guid.cpp and guid.h have been renamed Guid.cpp and Guid.hpp (they
      will be renamed back to guid.cpp and guid.hpp in next release)
    
    - Continue to use package-generic infrastructure as cmake doesn't work
      properly with this version. Commit such as
      https://github.com/graeme-hill/crossguid/commit/974def4e0ad62b8fea17d6dee7c4f9b0d65eaa2c
      is needed. Migration on cmake should be done in next package
      release.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libcrossguid/libcrossguid.mk
+++ package/libcrossguid/libcrossguid.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBCROSSGUID_VERSION = 8f399e8bd4252be9952f3dfa8199924cc8487ca4
+LIBCROSSGUID_VERSION = v0.2.2
 LIBCROSSGUID_SITE = $(call github,graeme-hill,crossguid,$(LIBCROSSGUID_VERSION))
 LIBCROSSGUID_LICENSE = MIT
 LIBCROSSGUID_LICENSE_FILES = LICENSE
@@ -15,14 +15,14 @@ LIBCROSSGUID_DEPENDENCIES = util-linux
 
 define LIBCROSSGUID_BUILD_CMDS
 	(cd $(@D); $(TARGET_CXX) $(TARGET_CXXFLAGS) -std=c++11 -DGUID_LIBUUID \
-		-c guid.cpp -o guid.o)
+		-c Guid.cpp -o guid.o)
 	(cd $(@D); $(TARGET_AR) rvs libcrossguid.a guid.o)
 endef
 
 define LIBCROSSGUID_INSTALL_STAGING_CMDS
 	$(INSTALL) -D -m 644 $(@D)/libcrossguid.a \
 		$(STAGING_DIR)/usr/lib/libcrossguid.a
-	$(INSTALL) -D -m 644 $(@D)/guid.h $(STAGING_DIR)/usr/include/guid.h
+	$(INSTALL) -D -m 644 $(@D)/Guid.hpp $(STAGING_DIR)/usr/include/guid.h
 endef
 
 $(eval $(generic-package))
