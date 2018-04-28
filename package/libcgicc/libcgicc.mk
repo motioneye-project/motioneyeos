f1e930b5844550dc1a49f3b7ee836dce9567bea5
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-04-28 14:43:15 +0200

    libcgicc: bump to version 3.2.19
    Use tar.gz archive (tar.bz2 is not available since 3.2.17)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libcgicc/libcgicc.mk
+++ package/libcgicc/libcgicc.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-LIBCGICC_VERSION = 3.2.16
+LIBCGICC_VERSION = 3.2.19
 LIBCGICC_SITE = $(BR2_GNU_MIRROR)/cgicc
-LIBCGICC_SOURCE = cgicc-$(LIBCGICC_VERSION).tar.bz2
+LIBCGICC_SOURCE = cgicc-$(LIBCGICC_VERSION).tar.gz
 LIBCGICC_LICENSE = LGPL-3.0+ (library), GFDL-1.2+ (docs)
 LIBCGICC_LICENSE_FILES = COPYING.LIB COPYING.DOC
 LIBCGICC_INSTALL_STAGING = YES
