9b203aadcce027fd9cf5795bc3e5330c2457e69a
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-04-29 15:21:15 +0200

    libdvdcss: bump to version 1.4.2
    Add hash for license file
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libdvdcss/libdvdcss.mk
+++ package/libdvdcss/libdvdcss.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBDVDCSS_VERSION = 1.4.0
+LIBDVDCSS_VERSION = 1.4.2
 LIBDVDCSS_SOURCE = libdvdcss-$(LIBDVDCSS_VERSION).tar.bz2
 LIBDVDCSS_SITE = http://www.videolan.org/pub/videolan/libdvdcss/$(LIBDVDCSS_VERSION)
 LIBDVDCSS_INSTALL_STAGING = YES
