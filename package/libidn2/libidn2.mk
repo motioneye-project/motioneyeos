ffb85a4a16df94e4980057a194e5fdc3c8332e32
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-05-01 22:40:32 +0200

    libidn2: new package
    Libidn2 is an implementation of the IDNA2008 + TR46
    specifications (RFC 5890, RFC 5891, RFC 5892, RFC 5893,
    TR 46).
    
    http://www.gnu.org/software/libidn/
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    [Thomas:
     - Use positive logic to test the BR2_PACKAGE_LIBIDN2_BINARY
       condition.
     - Put the definition of LIBIDN2_REMOVE_BINARY inside the
       BR2_PACKAGE_LIBIDN2_BINARY condition, as suggested by Arnout.
     - Adjust license details: library is under GPL-2.0+ or LGPL-3.0+,
       while the command line tool is under GPL-3.0+.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/libidn2/libidn2.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# libidn2
+#
+################################################################################
+
+LIBIDN2_VERSION = 2.0.4
+LIBIDN2_SITE = $(BR2_GNU_MIRROR)/libidn
+LIBIDN2_LICENSE := GPL-2.0+ or LGPL-3.0+ (library)
+LIBIDN2_LICENSE_FILES = COPYING COPYINGv2 COPYING.LESSERv3 COPYING.unicode
+LIBIDN2_DEPENDENCIES = \
+	host-pkgconf \
+	$(TARGET_NLS_DEPENDENCIES) \
+	$(if $(BR2_PACKAGE_LIBICONV),libiconv)
+LIBIDN2_INSTALL_STAGING = YES
+
+ifeq ($(BR2_PACKAGE_LIBUNISTRING),y)
+LIBIDN2_DEPENDENCIES += libunistring
+endif
+
+ifeq ($(BR2_PACKAGE_LIBIDN2_BINARY),)
+define LIBIDN2_REMOVE_BINARY
+	rm -f $(TARGET_DIR)/usr/bin/idn2
+endef
+LIBIDN2_POST_INSTALL_TARGET_HOOKS += LIBIDN2_REMOVE_BINARY
+else
+LIBIDN2_LICENSE := $(LIBIDN2_LICENSE), GPL-3.0+ (program)
+endif
+
+$(eval $(autotools-package))
