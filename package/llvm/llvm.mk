cf1f6cd1e7741f90845f34c762bd8c63765d3307
Valentin Korenblit <valentin.korenblit@smile.fr>
2018-05-02 11:05:35 +0200

    package/llvm: delete LLVMHello.so from /usr/lib
    LLVMHello.so contains the Hello World example of an
    LLVM pass. It is not needed on the target.
    
    Signed-off-by: Valentin Korenblit <valentin.korenblit@smile.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/llvm/llvm.mk
+++ package/llvm/llvm.mk
@@ -267,9 +267,9 @@ endef
 HOST_LLVM_POST_INSTALL_HOOKS = HOST_LLVM_COPY_LLVM_CONFIG_TO_STAGING_DIR
 
 # By default llvm-tblgen is built and installed on the target but it is
-# not necessary.
+# not necessary. Also erase LLVMHello.so from /usr/lib
 define LLVM_DELETE_LLVM_TBLGEN_TARGET
-	rm -f $(TARGET_DIR)/usr/bin/llvm-tblgen
+	rm -f $(TARGET_DIR)/usr/bin/llvm-tblgen $(TARGET_DIR)/usr/lib/LLVMHello.so
 endef
 LLVM_POST_INSTALL_TARGET_HOOKS = LLVM_DELETE_LLVM_TBLGEN_TARGET
 
