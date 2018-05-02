ec2006b7ea6984eec41db0efba85f2ee6806ea30
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-05-02 08:35:28 +0200

    libcue: bump to version 2.2.1
    - Remove patch (already in version)
    - Include buildroot patch http://patchwork.ozlabs.org/patch/907206
      currently in new state
    
    Fixes:
    - http://autobuild.buildroot.net/results/02712e85db8e538045497e9242b476c040198f1d
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libcue/libcue.mk
+++ package/libcue/libcue.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBCUE_VERSION = v2.2.0
+LIBCUE_VERSION = v2.2.1
 LIBCUE_SITE = $(call github,lipnitsk,libcue,$(LIBCUE_VERSION))
 LIBCUE_LICENSE = GPL-2.0, BSD-2-Clause (rem.c)
 LIBCUE_LICENSE_FILES = LICENSE
