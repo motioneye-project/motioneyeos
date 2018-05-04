16012f56cade49e7ca5d33ecdd6226c1b19fc4ac
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-05-04 23:18:12 +0200

    libnice: bump to version 0.1.14
    - Remove patch (already in version)
    - Add gnutls mandatory dependency
    - Add hash for license files
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libnice/libnice.mk
+++ package/libnice/libnice.mk
@@ -4,14 +4,12 @@
 #
 ################################################################################
 
-LIBNICE_VERSION = 0.1.13
+LIBNICE_VERSION = 0.1.14
 LIBNICE_SITE = http://nice.freedesktop.org/releases
 LIBNICE_LICENSE = MPL-1.1 or LGPL-2.1
 LIBNICE_LICENSE_FILES = COPYING COPYING.MPL COPYING.LGPL
-LIBNICE_DEPENDENCIES = libglib2 host-pkgconf
+LIBNICE_DEPENDENCIES = gnutls libglib2 host-pkgconf
 LIBNICE_INSTALL_STAGING = YES
-# For 0001-configure-Fix-configure-failure-when-building-without-.patch
-LIBNICE_AUTORECONF = YES
 LIBNICE_CONF_OPTS = \
 	--without-gstreamer \
 	--without-gstreamer-0.10
