71e1de6d5d5e613b550ec48bd05ecb150f161b5b
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-05-04 22:19:03 +0200

    libndp: bump to version 1.6
    Add hash for license file
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libndp/libndp.mk
+++ package/libndp/libndp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBNDP_VERSION = v1.5
+LIBNDP_VERSION = v1.6
 LIBNDP_SITE = $(call github,jpirko,libndp,$(LIBNDP_VERSION))
 LIBNDP_LICENSE = LGPL-2.1+
 LIBNDP_LICENSE_FILES = COPYING
