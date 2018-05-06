689c083f571e0c88463a451cb4e64c1470b67afb
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-05-06 11:42:29 +0200

    libnice: add gstreamer support
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libnice/libnice.mk
+++ package/libnice/libnice.mk
@@ -10,8 +10,19 @@ LIBNICE_LICENSE = MPL-1.1 or LGPL-2.1
 LIBNICE_LICENSE_FILES = COPYING COPYING.MPL COPYING.LGPL
 LIBNICE_DEPENDENCIES = gnutls libglib2 host-pkgconf
 LIBNICE_INSTALL_STAGING = YES
-LIBNICE_CONF_OPTS = \
-	--without-gstreamer \
-	--without-gstreamer-0.10
+
+ifeq ($(BR2_PACKAGE_GST_PLUGINS_BASE),y)
+LIBNICE_CONF_OPTS += --with-gstreamer-0.10
+LIBNICE_DEPENDENCIES += gst-plugins-base
+else
+LIBNICE_CONF_OPTS += --without-gstreamer-0.10
+endif
+
+ifeq ($(BR2_PACKAGE_GST1_PLUGINS_BASE),y)
+LIBNICE_CONF_OPTS += --with-gstreamer
+LIBNICE_DEPENDENCIES += gst1-plugins-base
+else
+LIBNICE_CONF_OPTS += --without-gstreamer
+endif
 
 $(eval $(autotools-package))
