8e5837cae71a2676d9c74017e6b1ef23d99ab51c
Martin Bark <martin@barkynet.com>
2018-05-06 21:07:29 +0100

    package/rpi-bt-firmware: bump version
    Signed-off-by: Martin Bark <martin@barkynet.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/rpi-bt-firmware/rpi-bt-firmware.mk
+++ package/rpi-bt-firmware/rpi-bt-firmware.mk
@@ -4,17 +4,14 @@
 #
 ################################################################################
 
-RPI_BT_FIRMWARE_VERSION = 18d7c931aff0a8a78360b9b9eaeb15d1224fb907
+RPI_BT_FIRMWARE_VERSION = b5307312a172c8d87d44f6df32f5e24f1c19770e
 RPI_BT_FIRMWARE_SITE = $(call github,LibreELEC,brcmfmac_sdio-firmware-rpi,$(RPI_BT_FIRMWARE_VERSION))
 RPI_BT_FIRMWARE_LICENSE = PROPRIETARY
 RPI_BT_FIRMWARE_LICENSE_FILES = LICENCE.broadcom_bcm43xx
 
-RPI_BT_FIRMWARE_FILES = brcm/BCM43430A1.hcd BCM4345C0.hcd
-
 define RPI_BT_FIRMWARE_INSTALL_TARGET_CMDS
-	$(foreach file,$(RPI_BT_FIRMWARE_FILES),\
-		$(INSTALL) -D -m 0644 $(@D)/firmware/$(file) $(TARGET_DIR)/lib/firmware/$(notdir $(file))
-	)
+	$(INSTALL) -d $(TARGET_DIR)/lib/firmware/brcm
+	$(INSTALL) -m 0644 $(@D)/firmware/brcm/*.hcd $(TARGET_DIR)/lib/firmware/brcm
 endef
 
 $(eval $(generic-package))
