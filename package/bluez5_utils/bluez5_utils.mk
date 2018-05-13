b2b874fe678653d348a1d00bd11aad50ca383c0a
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2018-05-13 21:07:30 +0200

    bluez5_utils: rename options to have the proper suffix
    The bluez5_utils Config.in options had a bogus prefix:
    BR2_PACKAGE_BLUEZ5 instead of the expected
    BR2_PACKAGE_BLUEZ5_UTILS. This commit fixes that, and adds the
    appropriate Config.in.legacy handling.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Acked-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/bluez5_utils/bluez5_utils.mk
+++ package/bluez5_utils/bluez5_utils.mk
@@ -41,14 +41,14 @@ BLUEZ5_UTILS_CONF_OPTS += --disable-experimental
 endif
 
 # enable health plugin
-ifeq ($(BR2_PACKAGE_BLUEZ5_PLUGINS_HEALTH),y)
+ifeq ($(BR2_PACKAGE_BLUEZ5_UTILS_PLUGINS_HEALTH),y)
 BLUEZ5_UTILS_CONF_OPTS += --enable-health
 else
 BLUEZ5_UTILS_CONF_OPTS += --disable-health
 endif
 
 # enable midi profile
-ifeq ($(BR2_PACKAGE_BLUEZ5_PLUGINS_MIDI),y)
+ifeq ($(BR2_PACKAGE_BLUEZ5_UTILS_PLUGINS_MIDI),y)
 BLUEZ5_UTILS_CONF_OPTS += --enable-midi
 BLUEZ5_UTILS_DEPENDENCIES += alsa-lib
 else
@@ -56,21 +56,21 @@ BLUEZ5_UTILS_CONF_OPTS += --disable-midi
 endif
 
 # enable nfc plugin
-ifeq ($(BR2_PACKAGE_BLUEZ5_PLUGINS_NFC),y)
+ifeq ($(BR2_PACKAGE_BLUEZ5_UTILS_PLUGINS_NFC),y)
 BLUEZ5_UTILS_CONF_OPTS += --enable-nfc
 else
 BLUEZ5_UTILS_CONF_OPTS += --disable-nfc
 endif
 
 # enable sap plugin
-ifeq ($(BR2_PACKAGE_BLUEZ5_PLUGINS_SAP),y)
+ifeq ($(BR2_PACKAGE_BLUEZ5_UTILS_PLUGINS_SAP),y)
 BLUEZ5_UTILS_CONF_OPTS += --enable-sap
 else
 BLUEZ5_UTILS_CONF_OPTS += --disable-sap
 endif
 
 # enable sixaxis plugin
-ifeq ($(BR2_PACKAGE_BLUEZ5_PLUGINS_SIXAXIS),y)
+ifeq ($(BR2_PACKAGE_BLUEZ5_UTILS_PLUGINS_SIXAXIS),y)
 BLUEZ5_UTILS_CONF_OPTS += --enable-sixaxis
 else
 BLUEZ5_UTILS_CONF_OPTS += --disable-sixaxis
