83b97af9b52ded0be6430b0337c61b4ab1ae6032
Martin Bark <martin@barkynet.com>
2018-05-17 10:38:09 +0100

    package/zic: bump version to 2018e
    Signed-off-by: Martin Bark <martin@barkynet.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/zic/zic.mk
+++ package/zic/zic.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ZIC_VERSION = 2018c
+ZIC_VERSION = 2018e
 ZIC_SOURCE = tzcode$(ZIC_VERSION).tar.gz
 ZIC_SITE = http://www.iana.org/time-zones/repository/releases
 ZIC_STRIP_COMPONENTS = 0
