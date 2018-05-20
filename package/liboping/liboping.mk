e2d21433372b7b0f073edebe7122e7b48b484251
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-05-20 12:38:20 +0200

    liboping: bump to version 1.10.0
    - Rework ncurses dependency to use new --with[out]-ncurses option
    - Add hash for license file
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/liboping/liboping.mk
+++ package/liboping/liboping.mk
@@ -4,13 +4,19 @@
 #
 ################################################################################
 
-LIBOPING_VERSION = 1.9.0
+LIBOPING_VERSION = 1.10.0
 LIBOPING_SITE = http://noping.cc/files
 LIBOPING_SOURCE = liboping-$(LIBOPING_VERSION).tar.bz2
 LIBOPING_INSTALL_STAGING = YES
-LIBOPING_DEPENDENCIES = $(if $(BR2_PACKAGE_NCURSES),ncurses)
 LIBOPING_CONF_OPTS = --without-perl-bindings
 LIBOPING_LICENSE = LGPL-2.1+, GPL-2.0
 LIBOPING_LICENSE_FILES = COPYING
+
+ifeq ($(BR2_PACKAGE_NCURSES),y)
+LIBOPING_DEPENDENCIES += ncurses
+LIBOPING_CONF_OPTS += --with-ncurses
+else
+LIBOPING_CONF_OPTS += --without-ncurses
+endif
 
 $(eval $(autotools-package))
