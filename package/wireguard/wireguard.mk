f6b9d59f61b9084b1ae09e26e4bac5d6a1729cf6
Peter Korsgaard <peter@korsgaard.com>
2018-05-27 10:49:16 +0200

    wireguard: bump version to 0.0.20180524
    Contains a number of fixes, including fixes for building with GCC 8.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/wireguard/wireguard.mk
+++ package/wireguard/wireguard.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIREGUARD_VERSION = 0.0.20180420
+WIREGUARD_VERSION = 0.0.20180524
 WIREGUARD_SITE = https://git.zx2c4.com/WireGuard/snapshot
 WIREGUARD_SOURCE = WireGuard-$(WIREGUARD_VERSION).tar.xz
 WIREGUARD_LICENSE = GPL-2.0
