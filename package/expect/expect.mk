05ea70bf27be722b32f9c7ca96db43bff3b68520
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2018-05-31 09:05:20 +0200

    package/expect: download tarball instead of cvs
    More than 3 years after the 5.45.3 release, a tarball for that release
    was uploaded to sourceforge.net. The differences between this tarball
    and the CVS checkout are minimal:
    
    - There are no CVS directories, of course, but we don't need them.
    - File timestamps are different.
    - expect.tests is missing, but we don't execute tests anyway.
    - configure script is different, but we AUTORECONF anyway.
    - 'fixcat' script is missing, but it is not called anywhere.
    
    Since sourceforge.net has broken CVS downloads, now is a good time to
    switch away from it.
    
    While we're at it, add a hash file including license.
    
    Fixes:
    http://autobuild.buildroot.net/results/db3/db33d4fa507fb3b4132423cd0a7e25a1fe6e4105
    http://autobuild.buildroot.net/results/b6d/b6d927dcc73ac8d754422577dacefff4ff918a5c
    http://autobuild.buildroot.net/results/23d/23d1034b33d0354de15de2ec4a8ccd0603e8db78
    http://autobuild.buildroot.net/results/127/1272a3aa3077e434c9805ec3034f35e6fcc330d4
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Cc: Fabio Porcedda <fabio.porcedda@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 23ab6cb162b7456925e2c3df4917cf9b45868d55)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/expect/expect.mk
+++ package/expect/expect.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-# Version 5.45.3
-EXPECT_VERSION = 2014-05-02
-EXPECT_SITE = cvs://expect.cvs.sourceforge.net:/cvsroot/expect
+EXPECT_VERSION = 5.45.3
+EXPECT_SITE = https://sourceforge.net/projects/expect/files/Expect/$(EXPECT_VERSION)
+EXPECT_SOURCE = expect$(EXPECT_VERSION).tar.gz
 EXPECT_LICENSE = Public domain
 EXPECT_LICENSE_FILES = README
 
+# 0001-enable-cross-compilation.patch
 EXPECT_AUTORECONF = YES
 EXPECT_DEPENDENCIES = tcl
 EXPECT_CONF_OPTS = --with-tcl=$(BUILD_DIR)/tcl-$(TCL_VERSION)/unix
