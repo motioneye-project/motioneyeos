413fb2af727100b5c0842b856405a93f5528ccc3
Yegor Yefremov <yegorslists@googlemail.com>
2018-06-05 10:15:22 +0200

    python-websockets: bump to version 5.0.1
    Remove upstream patch.
    
    Fixes:
    http://autobuild.buildroot.net/results/0b59939faaefc12cd7da4f26ab86659ac031b008/
    http://autobuild.buildroot.net/results/c0196693cfdbf10b429c5c03684d5308a68c8509/
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-websockets/python-websockets.mk
+++ package/python-websockets/python-websockets.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WEBSOCKETS_VERSION = 5.0
+PYTHON_WEBSOCKETS_VERSION = 5.0.1
 PYTHON_WEBSOCKETS_SOURCE = websockets-$(PYTHON_WEBSOCKETS_VERSION).tar.gz
-PYTHON_WEBSOCKETS_SITE = https://files.pythonhosted.org/packages/0d/ea/0cc41f9ab63b4af08840a667b047d6fab6406aebb076932ef877a4511f78
+PYTHON_WEBSOCKETS_SITE = https://files.pythonhosted.org/packages/4b/c6/026da2eeed75a49dd0b72a0c7ed6ee5cb2943e396ca9753eabff7359b27b
 PYTHON_WEBSOCKETS_SETUP_TYPE = setuptools
 PYTHON_WEBSOCKETS_LICENSE = BSD-3-Clause
 PYTHON_WEBSOCKETS_LICENSE_FILES = LICENSE
