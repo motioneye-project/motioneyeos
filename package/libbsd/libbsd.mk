64aec332bf4eada6df9453d60c17460e6c17dd39
Baruch Siach <baruch@tkos.co.il>
2018-06-05 21:19:35 +0300

    libbsd: bump to version 0.9.1
    Drop upstream patches.
    
    Add a patch fixing build with musl libc.
    
    Update the list of licenses based on the content of COPYING. The
    BSD-5-Clause license is not listed by SPDX, but that is its name in the
    COPYING file.
    
    Update the license file hash: new licenses, dates update, reformat.
    
    Cc: Yann E. MORIN <yann.morin.1998@free.fr>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libbsd/libbsd.mk
+++ package/libbsd/libbsd.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-LIBBSD_VERSION = 0.8.7
+LIBBSD_VERSION = 0.9.1
 LIBBSD_SOURCE = libbsd-$(LIBBSD_VERSION).tar.xz
 LIBBSD_SITE = https://archive.hadrons.org/software/libbsd
-LIBBSD_LICENSE = BSD-3-Clause, MIT
+LIBBSD_LICENSE = BSD-2-Clause, BSD-3-Clause, BSD-4-Clause, BSD-5-Clause, \
+		MIT, ISC, Beerware
 LIBBSD_LICENSE_FILES = COPYING
 LIBBSD_INSTALL_STAGING = YES
 
