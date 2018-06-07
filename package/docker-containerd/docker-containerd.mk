ded50f69c691e4b4a391ee23ec466abaad4bed99
Sam Lancia <sam@gpsm.co.uk>
2018-06-07 08:51:07 +0100

    docker-containerd: add missing docker-containerd symlink
    Prior to commit 2dccb4f2fefd8a0dbda65dd0f7537f811e920b13
    ("package/docker-containerd: convert to golang infrastructure"), the
    containerd binary was installed as docker-containerd. Following the
    conversion to the golang package infrastructure, the binary is now
    installed as "containerd", which breaks some use cases.
    
    Let's add a symlink link docker-containerd -> containerd to fix such
    use cases.
    
    Fixes: https://bugs.busybox.net/show_bug.cgi?id=11076
    
    Signed-off-by: Sam Lancia <sam@gpsm.co.uk>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/docker-containerd/docker-containerd.mk
+++ package/docker-containerd/docker-containerd.mk
@@ -21,6 +21,7 @@ DOCKER_CONTAINERD_INSTALL_BINS = containerd containerd-shim
 define DOCKER_CONTAINERD_INSTALL_SYMLINKS
 	ln -fs runc $(TARGET_DIR)/usr/bin/docker-runc
 	ln -fs containerd-shim $(TARGET_DIR)/usr/bin/docker-containerd-shim
+	ln -fs containerd $(TARGET_DIR)/usr/bin/docker-containerd
 endef
 
 DOCKER_CONTAINERD_POST_INSTALL_TARGET_HOOKS += DOCKER_CONTAINERD_INSTALL_SYMLINKS
