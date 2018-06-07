a1f4421cc7d6c4c1a7678bcca477ac311d0b4116
Mikhail Karpenko <karpenko@fastmail.com>
2018-06-07 22:51:32 +0300

    numactl: change source code provider to GitHub
    The original ftp with source code is not reachable any more and this
    commit changes the location of the package to corresponding GitHub
    project.
    
    Signed-off-by: Mikhail Karpenko <karpenko@fastmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/numactl/numactl.mk
+++ package/numactl/numactl.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-NUMACTL_VERSION = 2.0.11
-NUMACTL_SITE = ftp://oss.sgi.com/www/projects/libnuma/download
+NUMACTL_VERSION = v2.0.11
+NUMACTL_SITE = $(call github,numactl,numactl,$(NUMACTL_VERSION))
 NUMACTL_LICENSE = LGPL-2.1 (libnuma), GPL-2.0 (programs)
 NUMACTL_LICENSE_FILES = README
 NUMACTL_INSTALL_STAGING = YES
+NUMACTL_AUTORECONF = YES
 
 $(eval $(autotools-package))
