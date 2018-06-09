a1567b603704b17e84648e2d55e2f65cf793a157
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-06-09 12:13:17 +0200

    pugixml: bump to version 1.9
    - Remove patch (already in version)
    - Add hash for license file
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/pugixml/pugixml.mk
+++ package/pugixml/pugixml.mk
@@ -4,12 +4,10 @@
 #
 ################################################################################
 
-PUGIXML_VERSION = 1.7
+PUGIXML_VERSION = 1.9
 PUGIXML_SITE = http://github.com/zeux/pugixml/releases/download/v$(PUGIXML_VERSION)
 PUGIXML_LICENSE = MIT
 PUGIXML_LICENSE_FILES = readme.txt
 PUGIXML_INSTALL_STAGING = YES
-
-PUGIXML_SUBDIR = scripts
 
 $(eval $(cmake-package))
