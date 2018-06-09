ba819ad3b91b94a03de4c509d4f0d182585af845
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-06-09 10:41:39 +0200

    json-c: bump to version 0.13.1
    Drop patch, issue has been properly fixed by:
    https://github.com/json-c/json-c/commit/0f814e52dd22ee959a29ffcce36923aff62147b3
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/json-c/json-c.mk
+++ package/json-c/json-c.mk
@@ -4,14 +4,11 @@
 #
 ################################################################################
 
-JSON_C_VERSION = 0.13
+JSON_C_VERSION = 0.13.1
 JSON_C_SITE = https://s3.amazonaws.com/json-c_releases/releases
 JSON_C_INSTALL_STAGING = YES
 JSON_C_LICENSE = MIT
 JSON_C_LICENSE_FILES = COPYING
-
-# Patching configure.ac
-JSON_C_AUTORECONF = YES
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
