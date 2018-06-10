76d1654dfc2052239f0245a3b9d215fa8dba57cd
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-06-10 12:10:23 +0200

    package/speex: use pkgconf to detect libogg
    Upstream removed with-ogg-* configure options and switched to pkgconf
    to detect libogg back in 2014:
    https://git.xiph.org/?p=speex.git;a=commitdiff;h=e1b1eeabce815283c5bbc42016a9d6a11eda2866
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/speex/speex.mk
+++ package/speex/speex.mk
@@ -8,12 +8,9 @@ SPEEX_VERSION = 1.2rc1
 SPEEX_SITE = http://downloads.us.xiph.org/releases/speex
 SPEEX_LICENSE = BSD-3-Clause
 SPEEX_LICENSE_FILES = COPYING
-
 SPEEX_INSTALL_STAGING = YES
-SPEEX_DEPENDENCIES = libogg
+SPEEX_DEPENDENCIES = host-pkgconf libogg
 SPEEX_CONF_OPTS = \
-	--with-ogg-libraries=$(STAGING_DIR)/usr/lib \
-	--with-ogg-includes=$(STAGING_DIR)/usr/include \
 	--enable-fixed-point
 
 ifeq ($(BR2_PACKAGE_SPEEX_ARM4),y)
