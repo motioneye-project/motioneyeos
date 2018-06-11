d06d70d28db37e77a74493e2181fc5b0f6dedd17
Jared Bents <jared.bents@rockwellcollins.com>
2018-06-11 09:51:53 -0500

    modem-manager: update to version 1.8.0
    Update to modem manager 1.8.0 which does not require udev.
    Added option to build without udev but if udev exists in
    the build, it build with libgudev.
    
    Signed-off-by: Jared Bents <jared.bents@rockwellcollins.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/modem-manager/modem-manager.mk
+++ package/modem-manager/modem-manager.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-MODEM_MANAGER_VERSION = 1.6.12
+MODEM_MANAGER_VERSION = 1.8.0
 MODEM_MANAGER_SOURCE = ModemManager-$(MODEM_MANAGER_VERSION).tar.xz
 MODEM_MANAGER_SITE = http://www.freedesktop.org/software/ModemManager
 MODEM_MANAGER_LICENSE = GPL-2.0+ (programs, plugins), LGPL-2.0+ (libmm-glib)
 MODEM_MANAGER_LICENSE_FILES = COPYING COPYING.LIB
-MODEM_MANAGER_DEPENDENCIES = host-pkgconf host-intltool libglib2 libgudev
+MODEM_MANAGER_DEPENDENCIES = host-pkgconf host-intltool libglib2
 MODEM_MANAGER_INSTALL_STAGING = YES
 MODEM_MANAGER_CONF_OPTS = --disable-more-warnings
 
@@ -18,6 +18,13 @@ MODEM_MANAGER_DEPENDENCIES += libqmi
 MODEM_MANAGER_CONF_OPTS += --with-qmi
 else
 MODEM_MANAGER_CONF_OPTS += --without-qmi
+endif
+
+ifeq ($(BR2_PACKAGE_LIBGUDEV),y)
+MODEM_MANAGER_DEPENDENCIES += libgudev
+MODEM_MANAGER_CONF_OPTS += --with-udev
+else
+MODEM_MANAGER_CONF_OPTS += --without-udev
 endif
 
 ifeq ($(BR2_PACKAGE_MODEM_MANAGER_LIBMBIM),y)
