69d2b693c605082f2459c282947c99df517984ab
Gary Bisson <gary.bisson@boundarydevices.com>
2018-06-12 17:06:34 +0200

    imx-usb-loader: bump revision to e539461
    This latest revision supports starting i.MX8MQ via USB recovery mode.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/imx-usb-loader/imx-usb-loader.mk
+++ package/imx-usb-loader/imx-usb-loader.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IMX_USB_LOADER_VERSION = 7e77787c83288a421080a9bf2f67063931f561be
+IMX_USB_LOADER_VERSION = e5394615dd413c3823d5bd1de340933e16a8c07c
 IMX_USB_LOADER_SITE = $(call github,boundarydevices,imx_usb_loader,$(IMX_USB_LOADER_VERSION))
 IMX_USB_LOADER_LICENSE = LGPL-2.1+
 IMX_USB_LOADER_LICENSE_FILES = COPYING
