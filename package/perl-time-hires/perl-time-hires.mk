8c2003b22f6a5e030485f8c8c4ceac2f0aa7e940
Francois Perrad <fperrad@gmail.com>
2018-06-16 10:16:26 +0200

    perl-time-hires: bump to version 1.9758
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/perl-time-hires/perl-time-hires.mk
+++ package/perl-time-hires/perl-time-hires.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_TIME_HIRES_VERSION = 1.9753
+PERL_TIME_HIRES_VERSION = 1.9758
 PERL_TIME_HIRES_SOURCE = Time-HiRes-$(PERL_TIME_HIRES_VERSION).tar.gz
 PERL_TIME_HIRES_SITE = $(BR2_CPAN_MIRROR)/authors/id/J/JH/JHI
 PERL_TIME_HIRES_LICENSE = Artistic or GPL-1.0+
