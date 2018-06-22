902f537b14ffba28f65df299b32678aecd41d686
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-06-22 23:25:26 +0200

    libnss: bump to version 3.38
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libnss/libnss.mk
+++ package/libnss/libnss.mk
@@ -4,8 +4,7 @@
 #
 ################################################################################
 
-LIBNSS_VERSION_MAJOR = 3.37
-LIBNSS_VERSION = $(LIBNSS_VERSION_MAJOR).3
+LIBNSS_VERSION = 3.38
 LIBNSS_SOURCE = nss-$(LIBNSS_VERSION).tar.gz
 LIBNSS_SITE = https://ftp.mozilla.org/pub/mozilla.org/security/nss/releases/NSS_$(subst .,_,$(LIBNSS_VERSION))_RTM/src
 LIBNSS_DISTDIR = dist
