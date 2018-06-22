6076a40cd59d7b1f26162d46b670349693532854
Baruch Siach <baruch@tkos.co.il>
2018-06-22 06:57:53 +0300

    strace: bump to version 4.23
    Add reference to tarball signature verification key.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/strace/strace.mk
+++ package/strace/strace.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-STRACE_VERSION = 4.22
+STRACE_VERSION = 4.23
 STRACE_SOURCE = strace-$(STRACE_VERSION).tar.xz
 STRACE_SITE = https://strace.io/files/$(STRACE_VERSION)
 STRACE_LICENSE = BSD-3-Clause
