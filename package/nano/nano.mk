efe67ac14a7fffbc64633bd70f2e72fd0cc92500
Leon Anavi <leon.anavi@konsulko.com>
2018-07-01 07:26:07 +0300

    package/nano: bump version to 2.9.8
    Upgrade nano to 2.9.8, the latest version as of 2 June 2018:
    https://www.nano-editor.org/dist/v2.9/ChangeLog
    
    Signed-off-by: Leon Anavi <leon.anavi@konsulko.com>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/nano/nano.mk
+++ package/nano/nano.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 NANO_VERSION_MAJOR = 2.9
-NANO_VERSION = $(NANO_VERSION_MAJOR).0
+NANO_VERSION = $(NANO_VERSION_MAJOR).8
 NANO_SITE = https://www.nano-editor.org/dist/v$(NANO_VERSION_MAJOR)
 NANO_SOURCE = nano-$(NANO_VERSION).tar.xz
 NANO_LICENSE = GPL-3.0+
