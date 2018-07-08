ede1bc996aac8744a0d9bbd50cf7071323e638d6
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-07-08 21:44:48 +0200

    snappy: bump to version 1.1.7
    Add hash for license file
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/snappy/snappy.mk
+++ package/snappy/snappy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SNAPPY_VERSION = be6dc3db83c4701e3e79694dcbfd1c3da03b91dd
+SNAPPY_VERSION = 1.1.7
 SNAPPY_SITE = $(call github,google,snappy,$(SNAPPY_VERSION))
 SNAPPY_LICENSE = BSD-3-Clause
 SNAPPY_LICENSE_FILES = COPYING
