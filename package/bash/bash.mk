a40719477f4ddda9206333648662464f37bdda23
Yann E. MORIN <yann.morin.1998@free.fr>
2018-07-08 11:46:13 +0200

    package/busybox: invert dependency with bash
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/bash/bash.mk
+++ package/bash/bash.mk
@@ -6,9 +6,7 @@
 
 BASH_VERSION = 4.4.18
 BASH_SITE = $(BR2_GNU_MIRROR)/bash
-# Build after since bash is better than busybox shells
-BASH_DEPENDENCIES = ncurses readline host-bison \
-	$(if $(BR2_PACKAGE_BUSYBOX),busybox)
+BASH_DEPENDENCIES = ncurses readline host-bison
 BASH_CONF_OPTS = --with-installed-readline --without-bash-malloc
 BASH_LICENSE = GPL-3.0+
 BASH_LICENSE_FILES = COPYING
