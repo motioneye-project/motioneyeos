0fbc50d46a52555b7fc199ba319d011c0c7fd1c8
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-07-10 20:27:40 +0200

    wampcc: needs atomic
    Fixes:
     - http://autobuild.buildroot.net/results/2f994762d4e01a904c50cf2728a4103e6e79da2a
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/wampcc/wampcc.mk
+++ package/wampcc/wampcc.mk
@@ -11,4 +11,9 @@ WAMPCC_INSTALL_STAGING = YES
 WAMPCC_LICENSE = MIT
 WAMPCC_LICENSE_FILES = LICENSE
 
+# Uses __atomic_fetch_add_8
+ifeq ($(BR2_TOOLCHAIN_HAS_LIBATOMIC),y)
+WAMPCC_CONF_OPTS += -DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) -latomic"
+endif
+
 $(eval $(cmake-package))
