5b88e60abaca1b21f6c67890c1e632d73e9b419b
Marcus Folkesson <marcus.folkesson@gmail.com>
2018-07-12 07:55:15 +0200

    libostree: add upstream patch to fix linking errors
    Support for Avahi in libostree depends on some support functions from
    libsoup.
    Use its own support functions in case of libsoup is not provided.
    
    Fixes:
      http://autobuild.buildroot.net/results/5e5441822e2a94f8de51bfa0470aced84720ce31
    
    [Peter: enable AUTORECONF as .am file is touched]
    Signed-off-by: Marcus Folkesson <marcus.folkesson@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libostree/libostree.mk
+++ package/libostree/libostree.mk
@@ -12,6 +12,8 @@ LIBOSTREE_LICENSE = LGPL-2.0+
 LIBOSTREE_LICENSE_FILES = COPYING
 LIBOSTREE_DEPENDENCIES = e2fsprogs host-bison host-pkgconf libfuse libglib2 libgpgme xz
 LIBOSTREE_INSTALL_STAGING = YES
+# For 0002-build-add-ostree-soup-to-build-process-when-configur.patch
+LIBOSTREE_AUTORECONF = YES
 
 LIBOSTREE_CONF_OPTS += \
 	--with-gpgme-prefix=$(STAGING_DIR)/usr \
