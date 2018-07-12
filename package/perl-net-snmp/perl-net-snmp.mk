0b78743d1513858489daaa72e169869675208351
Thomas De Schampheleire <thomas.de_schampheleire@nokia.com>
2018-07-12 23:00:52 +0200

    perl-net-snmp: new package
    Signed-off-by: Thomas De Schampheleire <thomas.de_schampheleire@nokia.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/perl-net-snmp/perl-net-snmp.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# perl-net-snmp
+#
+################################################################################
+
+PERL_NET_SNMP_VERSION = v6.0.1
+PERL_NET_SNMP_SOURCE = Net-SNMP-$(PERL_NET_SNMP_VERSION).tar.gz
+PERL_NET_SNMP_SITE = $(BR2_CPAN_MIRROR)/authors/id/D/DT/DTOWN
+PERL_NET_SNMP_DEPENDENCIES = host-perl-module-build
+PERL_NET_SNMP_LICENSE = Artistic or GPL-1.0+
+PERL_NET_SNMP_LICENSE_FILES = LICENSE
+
+$(eval $(perl-package))
