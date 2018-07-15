80db2f4205da12ce31169c24e8f730c36cc29149
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-07-15 10:25:37 +0200

    package/pulseaudio: bump version to 12.1
    Release notes:
    https://lists.freedesktop.org/archives/pulseaudio-discuss/2018-July/030259.html
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/pulseaudio/pulseaudio.mk
+++ package/pulseaudio/pulseaudio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PULSEAUDIO_VERSION = 12.0
+PULSEAUDIO_VERSION = 12.1
 PULSEAUDIO_SOURCE = pulseaudio-$(PULSEAUDIO_VERSION).tar.xz
 PULSEAUDIO_SITE = http://freedesktop.org/software/pulseaudio/releases
 PULSEAUDIO_INSTALL_STAGING = YES
