cfe63dcd632fed002543f51cb0ff44b6a1eaa63b
Michaël Burtin <michael.burtin@netgem.com>
2018-07-19 15:54:08 +0200

    nghttp2: new package
    nghttp2 is an implementation of HTTP/2 and its header
    compression algorithm HPACK in C.
    
    Signed-off-by: Michaël Burtin <michael.burtin@netgem.com>
    Signed-off-by: Anisse Astier <anisse.astier.ext@netgem.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/nghttp2/nghttp2.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# nghttp2
+#
+################################################################################
+
+NGHTTP2_VERSION = 1.32.0
+NGHTTP2_SITE = https://github.com/nghttp2/nghttp2/releases/download/v$(NGHTTP2_VERSION)
+NGHTTP2_LICENSE = MIT
+NGHTTP2_LICENSE_FILES = COPYING
+NGHTTP2_INSTALL_STAGING = YES
+NGHTTP2_DEPENDENCIES = host-pkgconf
+NGHTTP2_CONF_OPTS = --enable-lib-only
+
+define NGHTTP2_INSTALL_CLEAN_HOOK
+	# Remove fetch-ocsp-response script unused by library
+	$(Q)$(RM) -rf $(TARGET_DIR)/usr/share/nghttp2
+endef
+
+NGHTTP2_POST_INSTALL_TARGET_HOOKS += NGHTTP2_INSTALL_CLEAN_HOOK
+
+$(eval $(autotools-package))
