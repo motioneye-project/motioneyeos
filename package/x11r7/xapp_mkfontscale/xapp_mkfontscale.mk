9eec30fd170b39b63ae02391126decef3232e678
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-07-21 16:16:37 +0200

    package/x11r7/xapp_*: mass switch to xorgproto
    The new package xorgproto will combine and replace all current xproto_*
    packages. For details read
    https://lists.x.org/archives/xorg-announce/2018-February/002835.html
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/x11r7/xapp_mkfontscale/xapp_mkfontscale.mk
+++ package/x11r7/xapp_mkfontscale/xapp_mkfontscale.mk
@@ -9,9 +9,9 @@ XAPP_MKFONTSCALE_SOURCE = mkfontscale-$(XAPP_MKFONTSCALE_VERSION).tar.bz2
 XAPP_MKFONTSCALE_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_MKFONTSCALE_LICENSE = MIT
 XAPP_MKFONTSCALE_LICENSE_FILES = COPYING
-XAPP_MKFONTSCALE_DEPENDENCIES = zlib freetype xlib_libfontenc xproto_xproto
+XAPP_MKFONTSCALE_DEPENDENCIES = zlib freetype xlib_libfontenc xorgproto
 HOST_XAPP_MKFONTSCALE_DEPENDENCIES = \
-	host-zlib host-freetype host-xlib_libfontenc host-xproto_xproto
+	host-zlib host-freetype host-xlib_libfontenc host-xorgproto
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
