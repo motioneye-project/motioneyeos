454d246ff01e315eb4e69ad0efb29e6d6371c918
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-07-21 16:16:42 +0200

    package/x11r7/xutil_makedepend: switch to xorgproto
    The new package xorgproto will combine and replace all current xproto_*
    packages. For details read
    https://lists.x.org/archives/xorg-announce/2018-February/002835.html
    
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/x11r7/xutil_makedepend/xutil_makedepend.mk
+++ package/x11r7/xutil_makedepend/xutil_makedepend.mk
@@ -10,8 +10,8 @@ XUTIL_MAKEDEPEND_SITE = http://xorg.freedesktop.org/releases/individual/util
 XUTIL_MAKEDEPEND_LICENSE = MIT
 XUTIL_MAKEDEPEND_LICENSE_FILES = COPYING
 
-XUTIL_MAKEDEPEND_DEPENDENCIES = xproto_xproto
-HOST_XUTIL_MAKEDEPEND_DEPENDENCIES = host-xproto_xproto
+XUTIL_MAKEDEPEND_DEPENDENCIES = xorgproto
+HOST_XUTIL_MAKEDEPEND_DEPENDENCIES = host-xorgproto
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
