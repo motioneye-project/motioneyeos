9361360bfd7e069228f783fe5cfc2aa1f820304d
Trent Piepho <tpiepho@impinj.com>
2018-07-26 16:33:20 -0700

    lttng-tools: bump to version 2.10.4
    Remove patches now present upstream and drop no longer needed
    autoreconf.
    
    Signed-off-by: Trent Piepho <tpiepho@impinj.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 7417daf0eb0c056b5750c8192c5de0b35a7a70e6)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lttng-tools/lttng-tools.mk
+++ package/lttng-tools/lttng-tools.mk
@@ -4,15 +4,13 @@
 #
 ################################################################################
 
-LTTNG_TOOLS_VERSION = 2.10.1
+LTTNG_TOOLS_VERSION = 2.10.4
 LTTNG_TOOLS_SITE = http://lttng.org/files/lttng-tools
 LTTNG_TOOLS_SOURCE = lttng-tools-$(LTTNG_TOOLS_VERSION).tar.bz2
 LTTNG_TOOLS_INSTALL_STAGING = YES
 LTTNG_TOOLS_LICENSE = GPL-2.0+, LGPL-2.1+ (include/lttng/*, src/lib/lttng-ctl/*)
 LTTNG_TOOLS_LICENSE_FILES = gpl-2.0.txt lgpl-2.1.txt LICENSE
 LTTNG_TOOLS_CONF_OPTS += --disable-man-pages
-# 0001-Fix-detect-dlmopen-and-disable-corresponding-tests-i.patch
-LTTNG_TOOLS_AUTORECONF = YES
 LTTNG_TOOLS_DEPENDENCIES = liburcu libxml2 popt util-linux
 
 ifeq ($(BR2_PACKAGE_LTTNG_LIBUST),y)
