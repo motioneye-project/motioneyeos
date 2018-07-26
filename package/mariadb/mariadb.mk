acd9e32dfe05b8bcf948196e57bf876cc1f907a8
Ryan Coe <bluemrp9@gmail.com>
2018-07-26 09:37:01 -0700

    mariadb: move comments outside of define block
    There are two comments inside the post install define block that show up in
    the build. Fix this by moving the comments outside the block.
    
    Signed-off-by: Ryan Coe <bluemrp9@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit d0a0d49c20b250a71e649268df1328fca139a879)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mariadb/mariadb.mk
+++ package/mariadb/mariadb.mk
@@ -104,13 +104,13 @@ define MARIADB_INSTALL_INIT_SYSTEMD
 endef
 endif
 
+# We don't need mysql_config on the target as it's only useful in staging
+# We also don't need the test suite on the target
 define MARIADB_POST_INSTALL
 	mkdir -p $(TARGET_DIR)/var/lib/mysql
 	$(INSTALL) -D -m 644 $(TARGET_DIR)/usr/share/mysql/my-small.cnf \
 		$(TARGET_DIR)/etc/mysql/my.cnf
-	# We don't need this on the target as it's only useful in staging
 	$(RM) $(TARGET_DIR)/usr/bin/mysql_config
-	# Remove test suite
 	$(RM) -r $(TARGET_DIR)/usr/share/mysql/test
 endef
 
