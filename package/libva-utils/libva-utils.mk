1f414d6489665106b2010b815fc5c9ca2c0cbe67
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-07-29 21:25:28 +0200

    package/libva-utils: bump version to 2.2.0
    Upstream moved its github repo to github.com/intel/, updated URLs, for
    reference see upstream PR 110. Also updated project URL.
    
    Added patch to fix build without stack-protector support.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libva-utils/libva-utils.mk
+++ package/libva-utils/libva-utils.mk
@@ -4,11 +4,13 @@
 #
 ################################################################################
 
-LIBVA_UTILS_VERSION = 2.1.0
+LIBVA_UTILS_VERSION = 2.2.0
 LIBVA_UTILS_SOURCE = libva-utils-$(LIBVA_UTILS_VERSION).tar.bz2
-LIBVA_UTILS_SITE = https://github.com/01org/libva-utils/releases/download/$(LIBVA_UTILS_VERSION)
+LIBVA_UTILS_SITE = https://github.com/intel/libva-utils/releases/download/$(LIBVA_UTILS_VERSION)
 LIBVA_UTILS_LICENSE = MIT
 LIBVA_UTILS_LICENSE_FILES = COPYING
+# 0001-check-ssp.patch
+LIBVA_UTILS_AUTORECONF = YES
 LIBVA_UTILS_DEPENDENCIES = host-pkgconf libva
 
 $(eval $(autotools-package))
