motioneye-project/motioneyeos

commit a17402e42d8c996af239cfdb536e74188d6c6245

opencv3: conditionally enable NEON and VFPv3 optimizations
Signed-off-by: Mathieu Maret <mathieu.maret@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/opencv3/opencv3.mk
@@ -99,7 +99,9 @@ OPENCV3_CONF_OPTS += \
 # * PowerPC support is turned off since its only effect is altering CFLAGS,
 #   adding '-mcpu=G3 -mtune=G5' to them, which is already handled by Buildroot.
 OPENCV3_CONF_OPTS += \
-	-DENABLE_POWERPC=OFF
+	-DENABLE_POWERPC=OFF \
+	-DENABLE_NEON=$(if $(BR2_ARM_CPU_HAS_NEON),ON,OFF) \
+	-DENABLE_VFPV3=$(if $(BR2_ARM_CPU_HAS_VFPV3),ON,OFF)
 
 # Cuda stuff
 OPENCV3_CONF_OPTS += \

commit 141cf77903764efad4238abd4cd1391260c0210d

package/opencv3: fix aarch64 build
Commit a17402e42d8c996af239cfdb536e74188d6c6245 ("opencv3:
conditionally enable NEON and VFPv3 optimizations") broke the build on
AArch64, because the VFPv3 optimizations are not supported by OpenCV
on this architecture:

  https://github.com/opencv/opencv/blob/3.4/cmake/OpenCVCompilerOptimizations.cmake#L275

However BR2_ARM_CPU_HAS_VFPV3 is always true on AArch64, causing this
build failure for every AArch64 build of OpenCV 3. This commit fixes
that by enabling VFPV3 only on ARM, and not on AArch64.

Fixes:

  http://autobuild.buildroot.net/results/d38fb092cffe086f93b469d1181da77fba0994bd/

Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/opencv3/opencv3.mk
@@ -100,8 +100,13 @@ OPENCV3_CONF_OPTS += \
 #   adding '-mcpu=G3 -mtune=G5' to them, which is already handled by Buildroot.
 OPENCV3_CONF_OPTS += \
 	-DENABLE_POWERPC=OFF \
-	-DENABLE_NEON=$(if $(BR2_ARM_CPU_HAS_NEON),ON,OFF) \
-	-DENABLE_VFPV3=$(if $(BR2_ARM_CPU_HAS_VFPV3),ON,OFF)
+	-DENABLE_NEON=$(if $(BR2_ARM_CPU_HAS_NEON),ON,OFF)
+
+ifeq ($(BR2_ARCH_IS_64):$(BR2_ARM_CPU_HAS_VFPV3),:y)
+OPENCV3_CONF_OPTS += -DENABLE_VFPV3=ON
+else
+OPENCV3_CONF_OPTS += -DENABLE_VFPV3=OFF
+endif
 
 # Cuda stuff
 OPENCV3_CONF_OPTS += \