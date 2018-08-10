09721e4a54257d3709569a482c8768fb41435b1a
Christopher McCrory <chrismcc@gmail.com>
2018-08-10 09:14:59 -0700

    perl-sub-install: new package
    Signed-off-by: Christopher McCrory <chrismcc@gmail.com>
    Acked-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/perl-sub-install/perl-sub-install.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-sub-install
+#
+################################################################################
+
+PERL_SUB_INSTALL_VERSION = 0.928
+PERL_SUB_INSTALL_SOURCE = Sub-Install-$(PERL_SUB_INSTALL_VERSION).tar.gz
+PERL_SUB_INSTALL_SITE = $(BR2_CPAN_MIRROR)/authors/id/R/RJ/RJBS
+PERL_SUB_INSTALL_LICENSE = Artistic or GPL-1.0+
+PERL_SUB_INSTALL_LICENSE_FILES = LICENSE
+
+$(eval $(perl-package))
