befb60b547a3339274b5b2583b5808c2e852d070
Christopher McCrory <chrismcc@gmail.com>
2018-08-10 09:15:00 -0700

    perl-sys-meminfo: new package
    Signed-off-by: Christopher McCrory <chrismcc@gmail.com>
    Acked-by: Francois Perrad <francois.perrad@gadz.org>
    [Thomas: add LICENSE variable.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/perl-sys-meminfo/perl-sys-meminfo.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-sys-meminfo
+#
+################################################################################
+
+PERL_SYS_MEMINFO_VERSION = 0.99
+PERL_SYS_MEMINFO_SOURCE = Sys-MemInfo-$(PERL_SYS_MEMINFO_VERSION).tar.gz
+PERL_SYS_MEMINFO_SITE = $(BR2_CPAN_MIRROR)/authors/id/S/SC/SCRESTO
+PERL_SYS_MEMINFO_LICENSE = Artistic or GPL-1.0+
+PERL_SYS_MEMINFO_LICENSE_FILES = LICENSE
+
+$(eval $(perl-package))
