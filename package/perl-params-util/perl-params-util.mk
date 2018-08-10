276c59c7c526906b1a902b919402b8d74a53d6bc
Christopher McCrory <chrismcc@gmail.com>
2018-08-10 09:14:58 -0700

    perl-params-util: new package
    Signed-off-by: Christopher McCrory <chrismcc@gmail.com>
    Acked-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/perl-params-util/perl-params-util.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-params-util
+#
+################################################################################
+
+PERL_PARAMS_UTIL_VERSION = 1.07
+PERL_PARAMS_UTIL_SOURCE = Params-Util-$(PERL_PARAMS_UTIL_VERSION).tar.gz
+PERL_PARAMS_UTIL_SITE = $(BR2_CPAN_MIRROR)/authors/id/A/AD/ADAMK
+PERL_PARAMS_UTIL_LICENSE = Artistic or GPL-1.0+
+PERL_PARAMS_UTIL_LICENSE_FILES = LICENSE
+
+$(eval $(perl-package))
