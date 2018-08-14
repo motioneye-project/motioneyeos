5b354383ce7dc369edf212fec6776438b24a4b66
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-08-14 22:40:56 +0200

    jpeg-turbo: bump to version 2.0.0
    - Move to cmake-infrastructure
    - Update hash of license files: minor updates and clarifications
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/jpeg-turbo/jpeg-turbo.mk
+++ package/jpeg-turbo/jpeg-turbo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-JPEG_TURBO_VERSION = 1.5.3
+JPEG_TURBO_VERSION = 2.0.0
 JPEG_TURBO_SOURCE = libjpeg-turbo-$(JPEG_TURBO_VERSION).tar.gz
 JPEG_TURBO_SITE = https://downloads.sourceforge.net/project/libjpeg-turbo/$(JPEG_TURBO_VERSION)
 JPEG_TURBO_LICENSE = IJG (libjpeg), BSD-3-Clause (TurboJPEG), Zlib (SIMD)
@@ -13,14 +13,14 @@ JPEG_TURBO_INSTALL_STAGING = YES
 JPEG_TURBO_PROVIDES = jpeg
 JPEG_TURBO_DEPENDENCIES = host-pkgconf
 
-JPEG_TURBO_CONF_OPTS = --with-jpeg8
+JPEG_TURBO_CONF_OPTS = -DWITH_JPEG8=ON
 
 ifeq ($(BR2_PACKAGE_JPEG_SIMD_SUPPORT),y)
-JPEG_TURBO_CONF_OPTS += --with-simd
+JPEG_TURBO_CONF_OPTS += -DWITH_SIMD=ON
 # x86 simd support needs nasm
 JPEG_TURBO_DEPENDENCIES += $(if $(BR2_X86_CPU_HAS_MMX),host-nasm)
 else
-JPEG_TURBO_CONF_OPTS += --without-simd
+JPEG_TURBO_CONF_OPTS += -DWITH_SIMD=OFF
 endif
 
 define JPEG_TURBO_REMOVE_USELESS_TOOLS
@@ -29,4 +29,4 @@ endef
 
 JPEG_TURBO_POST_INSTALL_TARGET_HOOKS += JPEG_TURBO_REMOVE_USELESS_TOOLS
 
-$(eval $(autotools-package))
+$(eval $(cmake-package))
