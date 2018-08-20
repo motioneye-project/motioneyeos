11d72bdaef65e8959dc0382d22a63e0b790b8928
Christopher McCrory <chrismcc@gmail.com>
2018-08-20 04:55:04 -0700

    perl-data-dump: new package
    Signed-off-by: Christopher McCrory <chrismcc@gmail.com>
    Acked-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/perl-data-dump/perl-data-dump.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-data-dump
+#
+################################################################################
+
+PERL_DATA_DUMP_VERSION = 1.23
+PERL_DATA_DUMP_SOURCE = Data-Dump-$(PERL_DATA_DUMP_VERSION).tar.gz
+PERL_DATA_DUMP_SITE = $(BR2_CPAN_MIRROR)/authors/id/G/GA/GAAS
+PERL_DATA_DUMP_LICENSE = Artistic or GPL-1.0+
+PERL_DATA_DUMP_LICENSE_FILES = README
+
+$(eval $(perl-package))
