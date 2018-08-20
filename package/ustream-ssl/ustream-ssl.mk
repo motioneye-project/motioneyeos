25eb81d6ef5e1c2029ff1be4ac4ec894c4958177
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-08-20 07:54:39 +0200

    package/ustream-ssl: bump version
    This release includes support for OpenSSL 1.1.x
    
    Added hashes for tarball and license file.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/ustream-ssl/ustream-ssl.mk
+++ package/ustream-ssl/ustream-ssl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-USTREAM_SSL_VERSION = 45ac93088bc6f2d8ef3b0512d8e1ddfd9c4ee9e5
+USTREAM_SSL_VERSION = 23a3f2830341acd1db149175baf7315a33bd0edb
 USTREAM_SSL_SITE = git://git.openwrt.org/project/ustream-ssl.git
 USTREAM_SSL_LICENSE = ISC
 USTREAM_SSL_LICENSE_FILES = ustream-ssl.h
