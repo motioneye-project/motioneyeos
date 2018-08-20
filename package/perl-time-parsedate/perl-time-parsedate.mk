1e90fc5f2ac9070fab3f8ea07b996b68421f2577
Christopher McCrory <chrismcc@gmail.com>
2018-08-20 04:55:05 -0700

    perl-time-parsedate: new package
    Signed-off-by: Christopher McCrory <chrismcc@gmail.com>
    [Thomas: adjust license information.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/perl-time-parsedate/perl-time-parsedate.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-time-parsedate
+#
+################################################################################
+
+PERL_TIME_PARSEDATE_VERSION = 2015.103
+PERL_TIME_PARSEDATE_SOURCE = Time-ParseDate-$(PERL_TIME_PARSEDATE_VERSION).tar.gz
+PERL_TIME_PARSEDATE_SITE = $(BR2_CPAN_MIRROR)/authors/id/M/MU/MUIR/modules
+PERL_TIME_PARSEDATE_LICENSE = Time Parse Date License, Public Domain
+PERL_TIME_PARSEDATE_LICENSE_FILES = lib/Time/JulianDay.pm lib/Time/Timezone.pm
+
+$(eval $(perl-package))
