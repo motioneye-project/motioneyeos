e0bddd57fdde0e0e002087057ee6a6f68d40f640
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-08-21 22:41:34 +0200

    package/perl-crypt-openssl-rsa: bump version to 0.30
    Changelog:
    https://metacpan.org/changes/distribution/Crypt-OpenSSL-RSA
    
    Added new build dependency to host-perl-crypt-openssl-guess and force
    it to search for openssl in STAGING_DIR. Added license hash. Updated
    _SITE according to scancpan.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/perl-crypt-openssl-rsa/perl-crypt-openssl-rsa.mk
+++ package/perl-crypt-openssl-rsa/perl-crypt-openssl-rsa.mk
@@ -4,11 +4,17 @@
 #
 ################################################################################
 
-PERL_CRYPT_OPENSSL_RSA_VERSION = 0.28
+PERL_CRYPT_OPENSSL_RSA_VERSION = 0.30
 PERL_CRYPT_OPENSSL_RSA_SOURCE = Crypt-OpenSSL-RSA-$(PERL_CRYPT_OPENSSL_RSA_VERSION).tar.gz
-PERL_CRYPT_OPENSSL_RSA_SITE = $(BR2_CPAN_MIRROR)/authors/id/P/PE/PERLER
-PERL_CRYPT_OPENSSL_RSA_DEPENDENCIES = perl-crypt-openssl-random
+PERL_CRYPT_OPENSSL_RSA_SITE = $(BR2_CPAN_MIRROR)/authors/id/T/TO/TODDR
+PERL_CRYPT_OPENSSL_RSA_DEPENDENCIES = \
+	host-perl-crypt-openssl-guess \
+	perl-crypt-openssl-random
 PERL_CRYPT_OPENSSL_RSA_LICENSE = Artistic or GPL-1.0+
 PERL_CRYPT_OPENSSL_RSA_LICENSE_FILES = LICENSE
+
+# Try as hard as possible to remedy to the brain-damage their build-system
+# suffers from: don't search for openssl, they pick the host-system one.
+PERL_CRYPT_OPENSSL_RSA_CONF_ENV = OPENSSL_PREFIX=$(STAGING_DIR)/usr
 
 $(eval $(perl-package))
