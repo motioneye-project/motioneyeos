e07c200cdb65b10ba67f0a542e7e833a6ba799db
Giulio Benetti <giulio.benetti@micronovasrl.com>
2018-08-22 02:06:47 +0200

    sunxi-mali-mainline: add support for arm64 and r8p1 blob version
    Blobs for arm64(aarch64) and r8p1 version are now available at Bootlin
    Github.
    
    So:
    - Bump version to latest commit:
    For arm64 architecture and r8p1 version.
    
    git shortlog --no-merges cb3e8ece9b2c3a70cbeb3204cd6f30eceaa32023..
    Giulio Benetti (1):
          Reorder folders splitting includes and libraries.
    
    Maxime Ripard (6):
          Move binaries to an arch subfolder
          Make x11 binaries path consistent
          Add r6p2 arm wayland blobs
          Add r6p2 arm64 blobs
          Add r8p1 fbdev blobs
          Add r8p1 arm64 fbdev blobs
    
    - Add support for them also under arm64(aarch64) architecture copying the
    right blobs according to architecture(arm or arm64) checking if BR2_arm
    or BR2_aarch64 is enabled.
    Only BR2_arm needs to provide BR2_ARM_EABIHF, so check must be done only
    in that case.
    - Mali-blobs repository folder layout has been reordered, so modify path
    when copying headers and libraries.
    - When copying libraries copy only *.so* files to avoid useless files to
    end into target folder.
    
    Signed-off-by: Giulio Benetti <giulio.benetti@micronovasrl.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/sunxi-mali-mainline/sunxi-mali-mainline.mk
+++ package/sunxi-mali-mainline/sunxi-mali-mainline.mk
@@ -4,20 +4,25 @@
 #
 ################################################################################
 
-SUNXI_MALI_MAINLINE_VERSION = cb3e8ece9b2c3a70cbeb3204cd6f30eceaa32023
+SUNXI_MALI_MAINLINE_VERSION = d691cb93884ca8ac67860502117bbec283dc19aa
 SUNXI_MALI_MAINLINE_SITE = $(call github,free-electrons,mali-blobs,$(SUNXI_MALI_MAINLINE_VERSION))
 SUNXI_MALI_MAINLINE_INSTALL_STAGING = YES
 SUNXI_MALI_MAINLINE_PROVIDES = libegl libgles
 
 SUNXI_MALI_MAINLINE_REV = $(call qstrip,$(BR2_PACKAGE_SUNXI_MALI_MAINLINE_REVISION))
 
+ifeq ($(BR2_arm),y)
+SUNXI_MALI_MAINLINE_ARCH=arm
+else ifeq ($(BR2_aarch64),y)
+SUNXI_MALI_MAINLINE_ARCH=arm64
+endif
+
 define SUNXI_MALI_MAINLINE_INSTALL_STAGING_CMDS
 	mkdir -p $(STAGING_DIR)/usr/lib $(STAGING_DIR)/usr/include
 
-	cp -rf $(@D)/$(SUNXI_MALI_MAINLINE_REV)/fbdev/lib/lib_fb_dev/* \
+	cp -rf $(@D)/$(SUNXI_MALI_MAINLINE_REV)/$(SUNXI_MALI_MAINLINE_ARCH)/fbdev/*.so* \
 		$(STAGING_DIR)/usr/lib/
-	cp -rf $(@D)/$(SUNXI_MALI_MAINLINE_REV)/fbdev/include/* \
-		$(STAGING_DIR)/usr/include/
+	cp -rf $(@D)/include/fbdev/* $(STAGING_DIR)/usr/include/
 
 	$(INSTALL) -D -m 0644 package/sunxi-mali-mainline/egl.pc \
 		$(STAGING_DIR)/usr/lib/pkgconfig/egl.pc
@@ -27,7 +32,7 @@ endef
 
 define SUNXI_MALI_MAINLINE_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib
-	cp -rf $(@D)/$(SUNXI_MALI_MAINLINE_REV)/fbdev/lib/lib_fb_dev/* \
+	cp -rf $(@D)/$(SUNXI_MALI_MAINLINE_REV)/$(SUNXI_MALI_MAINLINE_ARCH)/fbdev/*.so* \
 		$(TARGET_DIR)/usr/lib/
 endef
 
