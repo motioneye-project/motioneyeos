5c29b905f332778dc2172f3462f63d6f5d0d9b04
Angelo Compagnucci <angelo.compagnucci@gmail.com>
2018-08-24 15:44:57 +0200

    package/imagemagick: add host package
    This patch adds the host package for imagemagick.
    It comes with a minimal selection of configure options to
    compile fast cause it is used only to convert
    the image files for the custom linux logo.
    
    Signed-off-by: Angelo Compagnucci <angelo@amarulasolutions.com>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    [Peter: explicitly disable other configure options like it is done for the
    	target variant, add host-pkgconf]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/imagemagick/imagemagick.mk
+++ package/imagemagick/imagemagick.mk
@@ -143,4 +143,41 @@ else
 IMAGEMAGICK_CONF_OPTS += --without-bzlib
 endif
 
+HOST_IMAGEMAGICK_CONF_OPTS = \
+	--disable-openmp \
+	--without-djvu \
+	--without-dps \
+	--without-flif \
+	--without-fpx \
+	--without-gslib \
+	--without-gvc \
+	--without-jbig \
+	--without-lqr \
+	--without-openexr \
+	--without-perl \
+	--without-raqm \
+	--without-wmf \
+	--without-x \
+	--without-bzlib \
+	--without-fftw \
+	--without-fontconfig \
+	--without-freetype \
+	--without-lcms \
+	--without-lzma \
+	--without-pango \
+	--without-rsvg \
+	--without-tiff \
+	--without-webp \
+	--without-xml \
+	--with-jpeg \
+	--with-png \
+	--with-zlib
+
+HOST_IMAGEMAGICK_DEPENDENCIES = \
+	host-libjpeg \
+	host-libpng \
+	host-pkgconf \
+	host-zlib
+
 $(eval $(autotools-package))
+$(eval $(host-autotools-package))
