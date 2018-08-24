db756327f5c765457514b005f70624c6f81688cf
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2018-08-24 14:35:17 +0200

    rauc: add patch to make it build with Linux < 3.0
    This commit adds a patch to RAUC that makes the eMMC boot partition
    support optional. This allows RAUC to build successfully on systems
    using Linux < 3.0.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/7e1cbeb458cb6536a36eae0d24cefb36edb22f55/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Reviewed-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Tested-by: "Yann E. MORIN" <yann.morin.1998@free.fr>

--- package/rauc/rauc.mk
+++ package/rauc/rauc.mk
@@ -9,6 +9,8 @@ RAUC_SITE = https://github.com/rauc/rauc/releases/download/v$(RAUC_VERSION)
 RAUC_SOURCE = rauc-$(RAUC_VERSION).tar.xz
 RAUC_LICENSE = LGPL-2.1
 RAUC_DEPENDENCIES = host-pkgconf openssl libglib2
+# 0002-build-make-eMMC-boot-partition-support-optional.patch
+RAUC_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_RAUC_NETWORK),y)
 RAUC_CONF_OPTS += --enable-network
