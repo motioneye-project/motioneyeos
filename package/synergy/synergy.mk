b20ac5f7261a63dc78be3d59d1af9059fe758792
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2018-09-08 22:38:41 +0200

    synergy: change upstream location to fix download
    The Github repository at https://github.com/symless/synergy no longer
    exists, it has apparently been moved to
    https://github.com/symless/synergy-core.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/77f43b5937f102822a5b4644551222fda62fa129/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/synergy/synergy.mk
+++ package/synergy/synergy.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 SYNERGY_VERSION = v2.0.12-beta
-SYNERGY_SITE = $(call github,symless,synergy,$(SYNERGY_VERSION))
+SYNERGY_SITE = $(call github,symless,synergy-core,$(SYNERGY_VERSION))
 SYNERGY_LICENSE = GPL-2.0
 SYNERGY_LICENSE_FILES = LICENSE
 SYNERGY_DEPENDENCIES = xlib_libX11 xlib_libXtst
