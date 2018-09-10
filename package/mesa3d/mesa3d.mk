3d3235f9c191db170c731418880fb8eb8a0413ee
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-09-10 18:30:00 +0200

    package/{mesa3d, mesa3d-headers}: bump version to 18.2.0
    Upstream unconditionally enabled floating-point textures:
    https://cgit.freedesktop.org/mesa/mesa/commit/?h=18.2&id=66673bef941af344314fe9c91cad8cd330b245eb
    
    x11 support depends on xlib_libXrandr now:
    https://cgit.freedesktop.org/mesa/mesa/commit/configure.ac?h=18.2&id=3f960c1338713d317ce6463be68cee162c968d8d
    
    Removed 0003-musl-time-h.patch, patch was applied upstream.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # When updating the version, please also update mesa3d-headers
-MESA3D_VERSION = 18.1.8
+MESA3D_VERSION = 18.2.0
 MESA3D_SOURCE = mesa-$(MESA3D_VERSION).tar.xz
 MESA3D_SITE = https://mesa.freedesktop.org/archive
 MESA3D_LICENSE = MIT, SGI, Khronos
@@ -61,6 +61,7 @@ MESA3D_DEPENDENCIES += \
 	xlib_libXext \
 	xlib_libXdamage \
 	xlib_libXfixes \
+	xlib_libXrandr \
 	xorgproto \
 	libxcb
 MESA3D_CONF_OPTS += --enable-glx --disable-mangling
@@ -200,13 +201,6 @@ MESA3D_PROVIDES += libgles
 MESA3D_CONF_OPTS += --enable-gles1 --enable-gles2
 else
 MESA3D_CONF_OPTS += --disable-gles1 --disable-gles2
-endif
-
-ifeq ($(BR2_PACKAGE_MESA3D_OPENGL_TEXTURE_FLOAT),y)
-MESA3D_CONF_OPTS += --enable-texture-float
-MESA3D_LICENSE_FILES += docs/patents.txt
-else
-MESA3D_CONF_OPTS += --disable-texture-float
 endif
 
 ifeq ($(BR2_PACKAGE_XLIB_LIBXVMC),y)
