5809d456ef798d7effd7e82dd5320027767549d5
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-09-10 23:54:51 +0200

    vlc: fix build on or1k
    vlc 3.0.4 is also affected by gcc bug internal compiler error on or1k
    
    hw/vaapi/vlc_vaapi.c: In function 'pool_pic_destroy_cb':
    hw/vaapi/vlc_vaapi.c:568:1: internal compiler error: in merge_overlapping_regs, at regrename.c:304
     }
    
    Fixes:
     - http://autobuild.buildroot.org/results/771e88e7fb06714dd4b7246f82b95dffcedde343
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/vlc/vlc.mk
+++ package/vlc/vlc.mk
@@ -17,7 +17,7 @@ VLC_INSTALL_STAGING = YES
 
 # gcc bug internal compiler error: in merge_overlapping_regs, at
 # regrename.c:304. This bug is fixed since gcc 6.
-ifeq ($(BR2_microblaze):$(BR2_TOOLCHAIN_GCC_AT_LEAST_6),y:)
+ifeq ($(BR2_microblaze)$(BR2_or1k):$(BR2_TOOLCHAIN_GCC_AT_LEAST_6),y:)
 VLC_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -O0"
 VLC_CONF_OPTS += --disable-optimizations
 endif
