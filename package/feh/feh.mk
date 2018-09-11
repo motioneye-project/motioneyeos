c29e68f808c2a8fd88722f82c3b8dd6901bfe80b
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-09-11 22:34:47 +0200

    feh: libcurl is optional not mandatory
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Acked-by: Petr Vorel <petr.vorel@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/feh/feh.mk
+++ package/feh/feh.mk
@@ -7,17 +7,25 @@
 FEH_VERSION = 2.27.1
 FEH_SOURCE = feh-$(FEH_VERSION).tar.bz2
 FEH_SITE = http://feh.finalrewind.org
-FEH_DEPENDENCIES = libcurl imlib2 libpng xlib_libXinerama xlib_libXt
+FEH_DEPENDENCIES = imlib2 libpng xlib_libXinerama xlib_libXt
 FEH_LICENSE = MIT
 FEH_LICENSE_FILES = COPYING
 
+ifeq ($(BR2_PACKAGE_LIBCURL),y)
+FEH_DEPENDENCIES += libcurl
+FEH_MAKE_OPTS += curl=1
+else
+FEH_MAKE_OPTS += curl=0
+endif
+
 define FEH_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) CFLAGS="$(TARGET_CFLAGS) -std=gnu99" \
-		$(MAKE) -C $(@D) all
+		$(MAKE) $(FEH_MAKE_OPTS) -C $(@D) all
 endef
 
 define FEH_INSTALL_TARGET_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) PREFIX=/usr DESTDIR=$(TARGET_DIR) -C $(@D) install
+	$(TARGET_MAKE_ENV) $(MAKE) $(FEH_MAKE_OPTS) PREFIX=/usr \
+		DESTDIR=$(TARGET_DIR) -C $(@D) install
 endef
 
 $(eval $(generic-package))
