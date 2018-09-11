026a1a852a487f88c095868de9ac1b75b68507dc
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-09-11 22:14:08 +0200

    package/python-cryptography: bump version to 2.3.1
    Changelog: https://cryptography.io/en/latest/changelog/#v2-3-1
    
    Please note that CVE-2018-10903, fixed in version 2.3, was introduced
    in version 1.9.0, so it was not present in buildroot:
    https://nvd.nist.gov/vuln/detail/CVE-2018-10903
    
    Added license hashes and switched runtime dependency from pyasn1 to
    asn1crypto: https://cryptography.io/en/latest/changelog/#v1-8
    
    Fixed pthread problem by adding -pthread to CFLAGS, suggested by
    https://patchwork.openembedded.org/patch/146240/
    https://bugs.gentoo.org/630578#c6
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-cryptography/python-cryptography.mk
+++ package/python-cryptography/python-cryptography.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-PYTHON_CRYPTOGRAPHY_VERSION = 1.7.2
+PYTHON_CRYPTOGRAPHY_VERSION = 2.3.1
 PYTHON_CRYPTOGRAPHY_SOURCE = cryptography-$(PYTHON_CRYPTOGRAPHY_VERSION).tar.gz
-PYTHON_CRYPTOGRAPHY_SITE = https://pypi.python.org/packages/99/df/71c7260003f5c469cec3db4c547115df39e9ce6c719a99e067ba0e78fd8a
+PYTHON_CRYPTOGRAPHY_SITE = https://files.pythonhosted.org/packages/22/21/233e38f74188db94e8451ef6385754a98f3cad9b59bedf3a8e8b14988be4
 PYTHON_CRYPTOGRAPHY_SETUP_TYPE = setuptools
 PYTHON_CRYPTOGRAPHY_LICENSE = Apache-2.0 or BSD-3-Clause
 PYTHON_CRYPTOGRAPHY_LICENSE_FILES = LICENSE LICENSE.APACHE LICENSE.BSD
 PYTHON_CRYPTOGRAPHY_DEPENDENCIES = host-python-cffi openssl
+PYTHON_CRYPTOGRAPHY_ENV = CFLAGS="$(TARGET_CFLAGS) -pthread"
 
 $(eval $(python-package))
