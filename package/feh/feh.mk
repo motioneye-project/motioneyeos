motioneye-project/motioneyeos

commit de2fe74e2ae9ed0ac0bfa6a32879c02ec8e1cb92

feh: xinemara is optional not mandatory
Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/feh/feh.mk
@@ -7,7 +7,7 @@
 FEH_VERSION = 2.27.1
 FEH_SOURCE = feh-$(FEH_VERSION).tar.bz2
 FEH_SITE = http://feh.finalrewind.org
-FEH_DEPENDENCIES = imlib2 libpng xlib_libXinerama xlib_libXt
+FEH_DEPENDENCIES = imlib2 libpng xlib_libXt
 FEH_LICENSE = MIT
 FEH_LICENSE_FILES = COPYING
 
@@ -16,6 +16,13 @@ FEH_DEPENDENCIES += libcurl
 FEH_MAKE_OPTS += curl=1
 else
 FEH_MAKE_OPTS += curl=0
+endif
+
+ifeq ($(BR2_PACKAGE_XLIB_LIBXINERAMA),y)
+FEH_DEPENDENCIES += xlib_libXinemara
+FEH_MAKE_OPTS += xinerama=1
+else
+FEH_MAKE_OPTS += xinerama=0
 endif
 
 define FEH_BUILD_CMDS

package/feh/Config.in
@@ -2,7 +2,6 @@ config BR2_PACKAGE_FEH
 	bool "feh"
 	depends on BR2_PACKAGE_XORG7
 	depends on BR2_USE_MMU # fork()
-	select BR2_PACKAGE_XLIB_LIBXINERAMA
 	select BR2_PACKAGE_XLIB_LIBXT
 	select BR2_PACKAGE_IMLIB2
 	select BR2_PACKAGE_IMLIB2_PNG

commit 04d7572a8533afa684a9ba8fbabcff7aaf85aa1e

feh: fix typo in xlib_libXinerama dependency
This typo was introduced by commit
de2fe74e2ae9ed0ac0bfa6a32879c02ec8e1cb92, and was noticed by Petr
Vorel.

Reported-by: Petr Vorel <petr.vorel@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/feh/feh.mk
@@ -19,7 +19,7 @@ FEH_MAKE_OPTS += curl=0
 endif
 
 ifeq ($(BR2_PACKAGE_XLIB_LIBXINERAMA),y)
-FEH_DEPENDENCIES += xlib_libXinemara
+FEH_DEPENDENCIES += xlib_libXinerama
 FEH_MAKE_OPTS += xinerama=1
 else
 FEH_MAKE_OPTS += xinerama=0