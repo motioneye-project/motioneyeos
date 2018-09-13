f99efd731c2e4f8f64ce312d535b55f1025eb593
Christian Stewart <christian@paral.in>
2018-09-12 21:23:36 -0700

    go: bump to v1.11
    Signed-off-by: Christian Stewart <christian@paral.in>
    Reviewed-by: Anisse Astier <anisse@astier.eu>
    Tested-by: Anisse Astier <anisse@astier.eu>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.10.2
+GO_VERSION = 1.11
 GO_SITE = https://storage.googleapis.com/golang
 GO_SOURCE = go$(GO_VERSION).src.tar.gz
 
