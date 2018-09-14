97c0b57bc1938a096aef80214ef377de55c2e2d9
Asaf Kahlon <asafka7@gmail.com>
2018-09-14 09:52:38 +0300

    python-paramiko: bump to version 2.4.1
    Add license hash.
    Update dependencies: pyasn1 replaced pyasn.
    pyasn1 is pure python, but C++ is still needed for python-cryptography.
    
    Fixes:
    http://autobuild.buildroot.org/results/25a/25abe547831f40bd3c041345bc7d41779fe137e6/
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-paramiko/python-paramiko.mk
+++ package/python-paramiko/python-paramiko.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PARAMIKO_VERSION = 2.2.0
+PYTHON_PARAMIKO_VERSION = 2.4.1
 PYTHON_PARAMIKO_SOURCE = paramiko-$(PYTHON_PARAMIKO_VERSION).tar.gz
-PYTHON_PARAMIKO_SITE = https://pypi.python.org/packages/ea/a1/75393511fa6b645d28bd614ba061af99e94a6916c6b4f619f8b6b58bc35e
+PYTHON_PARAMIKO_SITE = https://files.pythonhosted.org/packages/29/65/83181630befb17cd1370a6abb9a87957947a43c2332216e5975353f61d64
 PYTHON_PARAMIKO_SETUP_TYPE = setuptools
 PYTHON_PARAMIKO_LICENSE = LGPL-2.1+
 PYTHON_PARAMIKO_LICENSE_FILES = LICENSE
