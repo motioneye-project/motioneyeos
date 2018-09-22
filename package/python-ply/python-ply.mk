e6115701196cdf44abfc6296706c61e882ead6f6
Asaf Kahlon <asafka7@gmail.com>
2018-09-22 22:36:29 +0300

    python-ply: new package
    No license file was found in the repo and README.md seems
    to be the closest (license type was mentioned in setup.py).
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    [Thomas: use the right SPDX license tag.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/python-ply/python-ply.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-ply
+#
+################################################################################
+
+PYTHON_PLY_VERSION = 3.11
+PYTHON_PLY_SOURCE = ply-$(PYTHON_PLY_VERSION).tar.gz
+PYTHON_PLY_SITE = https://files.pythonhosted.org/packages/e5/69/882ee5c9d017149285cab114ebeab373308ef0f874fcdac9beb90e0ac4da
+PYTHON_PLY_SETUP_TYPE = setuptools
+PYTHON_PLY_LICENSE = BSD-3-Clause
+PYTHON_PLY_LICENSE_FILES = README.md
+
+$(eval $(python-package))
