567fee95e08942e4705bf925bc94ef5d42f36a26
Asaf Kahlon <asafka7@gmail.com>
2018-09-22 22:36:28 +0300

    python-pycryptodomex: new package
    Cryptographic library for Python
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    [Thomas: adjust license information, it's not under Apache-2.0, but a
    combination of BSD-2-Clause, Public Domain and OCB License.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/python-pycryptodomex/python-pycryptodomex.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# python-pycryptodomex
+#
+################################################################################
+
+PYTHON_PYCRYPTODOMEX_VERSION = 3.6.6
+PYTHON_PYCRYPTODOMEX_SOURCE = pycryptodomex-$(PYTHON_PYCRYPTODOMEX_VERSION).tar.gz
+PYTHON_PYCRYPTODOMEX_SITE = https://files.pythonhosted.org/packages/95/10/f3bd758050da1c08ea30c8b4daf9ff895f306b780f07430fcc9b38722e1b
+PYTHON_PYCRYPTODOMEX_SETUP_TYPE = setuptools
+PYTHON_PYCRYPTODOMEX_LICENSE = \
+	BSD-2-Clause, \
+	Public Domain (pycrypto original code), \
+	OCB license (OCB cypher)
+PYTHON_PYCRYPTODOMEX_LICENSE_FILES = LICENSE.rst
+
+$(eval $(python-package))
