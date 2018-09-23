fd03e8192f1b4d5eae9a9458c5a06bebfa95fb7c
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-09-23 14:11:42 +0200

    minizip: bump to version 2.5.3
    - Move to cmake infrastructure
    - Do not enforce zlib dependency
    - Add optional bzip2 dependency
    - Add mandatory host-pkgconf and libbsd dependency
    - Remove miniunzip from Config.in, miniunzip (miniunz) has been merged
      with minizip:
      https://github.com/nmoinvaz/minizip/commit/a66cc31fac61e47a34bced08569ddec38b81bbef
    - Add hash for license file
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/minizip/minizip.mk
+++ package/minizip/minizip.mk
@@ -4,13 +4,26 @@
 #
 ################################################################################
 
-MINIZIP_VERSION = 1.1
+MINIZIP_VERSION = 2.5.3
 MINIZIP_SITE = $(call github,nmoinvaz,minizip,$(MINIZIP_VERSION))
-MINIZIP_DEPENDENCIES = zlib
-MINIZIP_AUTORECONF = YES
+MINIZIP_DEPENDENCIES = host-pkgconf libbsd
 MINIZIP_INSTALL_STAGING = YES
-MINIZIP_CONF_OPTS = $(if $(BR2_PACKAGE_MINIZIP_DEMOS),--enable-demos)
+MINIZIP_CONF_OPTS = $(if $(BR2_PACKAGE_MINIZIP_DEMOS),-DBUILD_TEST=ON)
 MINIZIP_LICENSE = Zlib
 MINIZIP_LICENSE_FILES = LICENSE
 
-$(eval $(autotools-package))
+ifeq ($(BR2_PACKAGE_BZIP2),y)
+MINIZIP_DEPENDENCIES += bzip2
+MINIZIP_CONF_OPTS += -DUSE_BZIP2=ON
+else
+MINIZIP_CONF_OPTS += -DUSE_BZIP2=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_ZLIB),y)
+MINIZIP_DEPENDENCIES += zlib
+MINIZIP_CONF_OPTS += -DUSE_ZLIB=ON
+else
+MINIZIP_CONF_OPTS += -DUSE_ZLIB=OFF
+endif
+
+$(eval $(cmake-package))
