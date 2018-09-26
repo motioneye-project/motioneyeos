46568cd8101a36c296dad58a7ec91ba34c9f752f
Asaf Kahlon <asafka7@gmail.com>
2018-09-26 18:34:57 +0300

    python-txtorcon: bump to version 18.1.0
    Also add license hash.
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    [Thomas: bump to 18.1.0, to fix Python 2.x issue, reported by Ricardo.]
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-txtorcon/python-txtorcon.mk
+++ package/python-txtorcon/python-txtorcon.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TXTORCON_VERSION = 0.19.3
+PYTHON_TXTORCON_VERSION = 18.1.0
 PYTHON_TXTORCON_SOURCE = txtorcon-$(PYTHON_TXTORCON_VERSION).tar.gz
-PYTHON_TXTORCON_SITE = https://pypi.python.org/packages/05/4b/d2fbfdc08ab83c299f2ad22ba38ea35f71f1c966684f5754e079108a1f64
+PYTHON_TXTORCON_SITE = https://files.pythonhosted.org/packages/d5/e1/577e28231013fffdc97c3078bbd37d9001e6cbea757b0b10da34d765dc32
 PYTHON_TXTORCON_SETUP_TYPE = setuptools
 PYTHON_TXTORCON_LICENSE = MIT
 PYTHON_TXTORCON_LICENSE_FILES = LICENSE
