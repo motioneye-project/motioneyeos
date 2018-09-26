9adc5dd9e3605e176922e2cc2fb3edbc1305f72d
Asaf Kahlon <asafka7@gmail.com>
2018-09-26 18:35:05 +0300

    python-crossbar: bump to version 18.9.2
    Update dependencies (and fix alphabetical order).
    Add patch to remove idna direct dependency (was sent to upstream too:
    https://github.com/crossbario/crossbar/pull/1406).
    
    Fixes:
    http://autobuild.buildroot.org/results/31d/31db5fd90514906a8c77f921c9650a386ec80101/
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-crossbar/python-crossbar.mk
+++ package/python-crossbar/python-crossbar.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CROSSBAR_VERSION = 17.6.1.post3
+PYTHON_CROSSBAR_VERSION = 18.9.2
 PYTHON_CROSSBAR_SOURCE = crossbar-$(PYTHON_CROSSBAR_VERSION).tar.gz
-PYTHON_CROSSBAR_SITE = https://pypi.python.org/packages/6a/25/9bc78947a4552c5fe27671ca2abe3be0559b8c76bc93d52157a6333ea971
+PYTHON_CROSSBAR_SITE = https://files.pythonhosted.org/packages/1c/71/27e6b5ba1e557d28d155badee90022f73f4216b752d063221edbe2b188f0
 PYTHON_CROSSBAR_LICENSE = AGPL-3.0
 PYTHON_CROSSBAR_LICENSE_FILES = LICENSE
 PYTHON_CROSSBAR_SETUP_TYPE = setuptools
