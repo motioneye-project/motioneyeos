2c5832305b96203f4f9c549e010f0e06b256f605
Erico Nunes <nunes.erico@gmail.com>
2018-09-27 00:39:14 +0200

    fwts: bump to version V18.09.00
    Build- and run-tested on x86_64 and aarch64.
    Rebased -Werror removal patch, it didn't apply anymore because one of
    the files added a license header at the top.
    Added libbsd dependency which is required since V18.08.00 for the
    "bsd/string.h" header.
    Reordered list of Config.in dependencies to match the toolchain
    requirements comment order.
    
    Signed-off-by: Erico Nunes <nunes.erico@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/fwts/fwts.mk
+++ package/fwts/fwts.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-FWTS_VERSION = V16.11.00
+FWTS_VERSION = V18.09.00
 FWTS_SITE = http://fwts.ubuntu.com/release
 FWTS_STRIP_COMPONENTS = 0
 FWTS_LICENSE = GPL-2.0, LGPL-2.1, Custom
 FWTS_LICENSE_FILES = debian/copyright
 FWTS_AUTORECONF = YES
-FWTS_DEPENDENCIES = host-bison host-flex host-pkgconf json-c libglib2 \
+FWTS_DEPENDENCIES = host-bison host-flex host-pkgconf json-c libglib2 libbsd \
 	$(if $(BR2_PACKAGE_DTC),dtc)
 
 $(eval $(autotools-package))
