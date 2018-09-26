27beaf4b6d7fbfb63ec8c4bf24dfb4bf82f83686
Asaf Kahlon <asafka7@gmail.com>
2018-09-26 18:35:04 +0300

    python-twisted: bump to version 18.7.0
    Add license hash.
    
    Add a patch to fit python3.7 syntax ("async" is now a keyword).
    The patch was already applied on upstream, but only after the last tag
    was created.
    
    Fixes:
    http://autobuild.buildroot.org/results/704/7043ccfac86439406480649b3ead4882112d36e2/
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-twisted/python-twisted.mk
+++ package/python-twisted/python-twisted.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TWISTED_VERSION = 17.5.0
+PYTHON_TWISTED_VERSION = 18.7.0
 PYTHON_TWISTED_SOURCE = Twisted-$(PYTHON_TWISTED_VERSION).tar.bz2
-PYTHON_TWISTED_SITE = https://pypi.python.org/packages/31/bf/7f86a8f8b9778e90d8b2921e9f442a8c8aa33fd2489fc10f236bc8af1749
+PYTHON_TWISTED_SITE = https://files.pythonhosted.org/packages/90/50/4c315ce5d119f67189d1819629cae7908ca0b0a6c572980df5cc6942bc22
 PYTHON_TWISTED_SETUP_TYPE = setuptools
 PYTHON_TWISTED_LICENSE = MIT
 PYTHON_TWISTED_LICENSE_FILES = LICENSE
