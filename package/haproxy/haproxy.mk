8e4f6b2fc5a6b3d52b86f614ba83de065b72b1b1
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-09-27 22:21:56 +0200

    haproxy: security bump to 1.8.14
    Fix CVE-2018-14645 (see
    https://www.mail-archive.com/haproxy@formilux.org/msg31253.html)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/haproxy/haproxy.mk
+++ package/haproxy/haproxy.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 HAPROXY_VERSION_MAJOR = 1.8
-HAPROXY_VERSION = $(HAPROXY_VERSION_MAJOR).13
+HAPROXY_VERSION = $(HAPROXY_VERSION_MAJOR).14
 HAPROXY_SITE = http://www.haproxy.org/download/$(HAPROXY_VERSION_MAJOR)/src
 HAPROXY_LICENSE = GPL-2.0+ and LGPL-2.1+ with exceptions
 HAPROXY_LICENSE_FILES = LICENSE doc/lgpl.txt doc/gpl.txt
