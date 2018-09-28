5a5102c428d98d79e2b4eeba5226a7c824a1fb66
Ferdinand van Aartsen <ferdinand@ombud.nl>
2018-09-29 00:04:31 +0200

    sqlite: bump to version 3.25.2
    Signed-off-by: Ferdinand van Aartsen <ferdinand@ombud.nl>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sqlite/sqlite.mk
+++ package/sqlite/sqlite.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SQLITE_VERSION = 3250100
+SQLITE_VERSION = 3250200
 SQLITE_SOURCE = sqlite-autoconf-$(SQLITE_VERSION).tar.gz
 SQLITE_SITE = https://www.sqlite.org/2018
 SQLITE_LICENSE = Public domain
