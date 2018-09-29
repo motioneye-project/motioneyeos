803bc88a72151996b8dc73fcce2f65cd6fd6e359
Gilles Talis <gilles.talis@gmail.com>
2018-09-29 17:09:01 +0200

    tesseract-ocr: bump to version 3.05.02
    Also added license hash
    
    Signed-off-by: Gilles Talis <gilles.talis@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tesseract-ocr/tesseract-ocr.mk
+++ package/tesseract-ocr/tesseract-ocr.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TESSERACT_OCR_VERSION = 3.05.00
+TESSERACT_OCR_VERSION = 3.05.02
 TESSERACT_OCR_DATA_VERSION = 3.04.00
 TESSERACT_OCR_SITE = $(call github,tesseract-ocr,tesseract,$(TESSERACT_OCR_VERSION))
 TESSERACT_OCR_LICENSE = Apache-2.0
