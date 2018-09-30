9c9e144640139eda3863b0d22e100d04fb9f6118
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-09-30 21:03:20 +0200

    package/mjpg-streamer: bump version
    The currently used github repo from oliv3r does not exist anymore.
    https://sourceforge.net/projects/mjpg-streamer/ shows that the status of
    the original project site is "Inactive" and points to
    https://github.com/jacksonliam/mjpg-streamer as the new site.
    
    The new maintainer is active so we bump the package to HEAD of its
    master branch which fixes a glibc-related build error:
    http://autobuild.buildroot.net/results/4c8/4c8aac898d4580ed0b8b52cef7779d0f6f6a631f/
    
    Further changes due to this bump:
    - removed patch 0001, fix was applied upstream:
      https://github.com/jacksonliam/mjpg-streamer/commit/eb4dafd802b366d9b0310e91e7bb93a1082e7657#diff-511ff6908e8db4543e5a59d0ea4a2f1f
    - switched to CMake
    
    Added license hash.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mjpg-streamer/mjpg-streamer.mk
+++ package/mjpg-streamer/mjpg-streamer.mk
@@ -4,27 +4,15 @@
 #
 ################################################################################
 
-# Original source is located at
-# http://sourceforge.net/p/mjpg-streamer/code/commit_browser
-# oliv3r forked the repo to add support for 3.16 and 3.17 kernels:
-# http://sourceforge.net/p/mjpg-streamer/patches/14/
-MJPG_STREAMER_VERSION = bbf32fddfd02a9e072e89e83a5b33e6ca0a7bd4b
-MJPG_STREAMER_SITE = $(call github,oliv3r,mjpg-streamer,$(MJPG_STREAMER_VERSION))
+MJPG_STREAMER_VERSION = f387bb44e6c087271b763b27da998bf2e06c4f5d
+MJPG_STREAMER_SITE = $(call github,jacksonliam,mjpg-streamer,$(MJPG_STREAMER_VERSION))
+MJPG_STREAMER_SUBDIR = mjpg-streamer-experimental
 MJPG_STREAMER_LICENSE = GPL-2.0+
-MJPG_STREAMER_LICENSE_FILES = LICENSE
+MJPG_STREAMER_LICENSE_FILES = $(MJPG_STREAMER_SUBDIR)/LICENSE
 MJPG_STREAMER_DEPENDENCIES = jpeg
 
 ifeq ($(BR2_PACKAGE_LIBV4L),y)
 MJPG_STREAMER_DEPENDENCIES += libv4l
-MJPG_STREAMER_USE_LIBV4L += USE_LIBV4L2=true
 endif
 
-define MJPG_STREAMER_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" -C $(@D) $(MJPG_STREAMER_USE_LIBV4L)
-endef
-
-define MJPG_STREAMER_INSTALL_TARGET_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR)/usr install
-endef
-
-$(eval $(generic-package))
+$(eval $(cmake-package))
