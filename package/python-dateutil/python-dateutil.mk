296b8bc64a69ae635d4b88b12d7c776c9b3b0df1
Asaf Kahlon <asafka7@gmail.com>
2018-09-30 21:50:24 +0300

    python-dateutil: bump to version 2.7.3
    Update site address in Config.in
    Add license hash.
    host-python-setuptools-scm is now needed for setup.py
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-dateutil/python-dateutil.mk
+++ package/python-dateutil/python-dateutil.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-PYTHON_DATEUTIL_VERSION = 2.6.0
-PYTHON_DATEUTIL_SITE = https://pypi.python.org/packages/51/fc/39a3fbde6864942e8bb24c93663734b74e281b984d1b8c4f95d64b0c21f6
+PYTHON_DATEUTIL_VERSION = 2.7.3
+PYTHON_DATEUTIL_SITE = https://files.pythonhosted.org/packages/a0/b0/a4e3241d2dee665fea11baec21389aec6886655cd4db7647ddf96c3fad15
 PYTHON_DATEUTIL_SETUP_TYPE = setuptools
 PYTHON_DATEUTIL_LICENSE = BSD-3-Clause
 PYTHON_DATEUTIL_LICENSE_FILES = LICENSE
+PYTHON_DATEUTIL_DEPENDENCIES = host-python-setuptools-scm
 
 $(eval $(python-package))
