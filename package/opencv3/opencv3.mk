76f9c0bcaa00e2d7e42f8cf9ff2633832917d9bc
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-09-30 13:55:49 +0200

    package/opencv3: bump version to 3.4.3
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/opencv3/opencv3.mk
+++ package/opencv3/opencv3.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENCV3_VERSION = 3.4.2
+OPENCV3_VERSION = 3.4.3
 OPENCV3_SITE = $(call github,opencv,opencv,$(OPENCV3_VERSION))
 OPENCV3_INSTALL_STAGING = YES
 OPENCV3_LICENSE = BSD-3-Clause
