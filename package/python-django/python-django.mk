a2ec66d59d052c1b7f30795393bb172deff6971e
Peter Korsgaard <peter@korsgaard.com>
2018-10-01 13:32:55 +0200

    python-django: bump version to 1.11.16
    Fixes a race condition in QuerySet.update_or_create() that could result in
    data loss:
    
    https://code.djangoproject.com/ticket/29499
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit feb811f56756bdbbbdd5a2230545462d46b2ea61)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 1.11.15
+PYTHON_DJANGO_VERSION = 1.11.16
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/43/b5/b44286e56a5211d37b4058dcd5e62835afa5ce5aa6a38b56bd04c0d01cbc
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/35/1d/59836bce4c9cfded261e21c0abd6a4629de6d289522d0fd928117d8eb985
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_SETUP_TYPE = setuptools
