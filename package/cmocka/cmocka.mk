40d4df2156211e3f91c5340971f62a20cdf222c2
Joel Carlson <joelsoncarl@gmail.com>
2018-10-03 10:58:53 -0600

    cmocka: bump to version 1.1.3
    Removes patch for upstream commit
    d677b564c2193a5cd381925c2eeaad6b23c6af2f
    
    Adds hash for license file.
    
    Adds patch to fix a build error encountered by br-mips64-n64-full
    toolchain about uintptr_t being declared twice.
    
    Signed-off-by: Joel Carlson <JoelsonCarl@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/cmocka/cmocka.mk
+++ package/cmocka/cmocka.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CMOCKA_VERSION = 1.1.1
+CMOCKA_VERSION = 1.1.3
 CMOCKA_SOURCE = cmocka-$(CMOCKA_VERSION).tar.xz
 CMOCKA_SITE = https://cmocka.org/files/1.1
 CMOCKA_LICENSE = Apache-2.0
