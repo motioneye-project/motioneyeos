25f7a819e030378f0ccffb7784b47f1e5e525317
Asaf Kahlon <asafka7@gmail.com>
2018-10-05 08:27:25 +0300

    python-txtorcon: bump to version 18.2.0
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-txtorcon/python-txtorcon.mk
+++ package/python-txtorcon/python-txtorcon.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TXTORCON_VERSION = 18.1.0
+PYTHON_TXTORCON_VERSION = 18.2.0
 PYTHON_TXTORCON_SOURCE = txtorcon-$(PYTHON_TXTORCON_VERSION).tar.gz
-PYTHON_TXTORCON_SITE = https://files.pythonhosted.org/packages/d5/e1/577e28231013fffdc97c3078bbd37d9001e6cbea757b0b10da34d765dc32
+PYTHON_TXTORCON_SITE = https://files.pythonhosted.org/packages/08/02/5a38afe0c96bd8da466716f51c72b3b52d3a76399e62b37494b97489f74a
 PYTHON_TXTORCON_SETUP_TYPE = setuptools
 PYTHON_TXTORCON_LICENSE = MIT
 PYTHON_TXTORCON_LICENSE_FILES = LICENSE
