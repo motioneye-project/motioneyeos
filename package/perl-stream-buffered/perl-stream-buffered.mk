3836c7416351936ad267801b2ad1d7a7fe59c994
Francois Perrad <fperrad@gmail.com>
2018-10-06 18:55:12 +0200

    perl-stream-buffered: new package
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/perl-stream-buffered/perl-stream-buffered.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-stream-buffered
+#
+################################################################################
+
+PERL_STREAM_BUFFERED_VERSION = 0.03
+PERL_STREAM_BUFFERED_SOURCE = Stream-Buffered-$(PERL_STREAM_BUFFERED_VERSION).tar.gz
+PERL_STREAM_BUFFERED_SITE = $(BR2_CPAN_MIRROR)/authors/id/D/DO/DOY
+PERL_STREAM_BUFFERED_LICENSE = Artistic or GPL-1.0+
+PERL_STREAM_BUFFERED_LICENSE_FILES = LICENSE
+
+$(eval $(perl-package))
