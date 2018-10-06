c8136d8fb24835b00096a3200e30ccc70a1fd13f
Francois Perrad <fperrad@gmail.com>
2018-10-06 18:55:16 +0200

    perl-extutils-installpaths: new package
    Only the host variant is added, which is needed as a dependency of
    perl-module-build-tiny (host), itself needed for
    perl-apache-logformat-compiler (target).
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/perl-extutils-installpaths/perl-extutils-installpaths.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# perl-extutils-installpaths
+#
+################################################################################
+
+PERL_EXTUTILS_INSTALLPATHS_VERSION = 0.012
+PERL_EXTUTILS_INSTALLPATHS_SOURCE = ExtUtils-InstallPaths-$(PERL_EXTUTILS_INSTALLPATHS_VERSION).tar.gz
+PERL_EXTUTILS_INSTALLPATHS_SITE = $(BR2_CPAN_MIRROR)/authors/id/L/LE/LEONT
+HOST_PERL_EXTUTILS_INSTALLPATHS_DEPENDENCIES = host-perl-extutils-config
+PERL_EXTUTILS_INSTALLPATHS_LICENSE = Artistic or GPL-1.0+
+PERL_EXTUTILS_INSTALLPATHS_LICENSE_FILES = LICENSE
+
+$(eval $(host-perl-package))
