32fa57d1eebb5c84b65095f7a1a77b6e769fabf3
Francois Perrad <fperrad@gmail.com>
2018-10-06 18:55:10 +0200

    perl-http-multipartparser: new package
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/perl-http-multipartparser/perl-http-multipartparser.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-http-multipartparser
+#
+################################################################################
+
+PERL_HTTP_MULTIPARTPARSER_VERSION = 0.02
+PERL_HTTP_MULTIPARTPARSER_SOURCE = HTTP-MultiPartParser-$(PERL_HTTP_MULTIPARTPARSER_VERSION).tar.gz
+PERL_HTTP_MULTIPARTPARSER_SITE = $(BR2_CPAN_MIRROR)/authors/id/C/CH/CHANSEN
+PERL_HTTP_MULTIPARTPARSER_LICENSE = Artistic or GPL-1.0+
+PERL_HTTP_MULTIPARTPARSER_LICENSE_FILES = README
+
+$(eval $(perl-package))
