e9a116473fa2f7d409cb01e4950ae3072a4da3e9
Francois Perrad <fperrad@gmail.com>
2018-10-06 18:55:21 +0200

    perl-http-headers-fast: new package
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/perl-http-headers-fast/perl-http-headers-fast.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# perl-http-headers-fast
+#
+################################################################################
+
+PERL_HTTP_HEADERS_FAST_VERSION = 0.21
+PERL_HTTP_HEADERS_FAST_SOURCE = HTTP-Headers-Fast-$(PERL_HTTP_HEADERS_FAST_VERSION).tar.gz
+PERL_HTTP_HEADERS_FAST_SITE = $(BR2_CPAN_MIRROR)/authors/id/T/TO/TOKUHIROM
+PERL_HTTP_HEADERS_FAST_DEPENDENCIES = host-perl-module-build-tiny perl-http-date
+PERL_HTTP_HEADERS_FAST_LICENSE = Artistic or GPL-1.0+
+PERL_HTTP_HEADERS_FAST_LICENSE_FILES = LICENSE
+
+$(eval $(perl-package))
