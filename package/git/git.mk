74953365a635aa5147a908523e4d0a3a78f6eec0
Peter Korsgaard <peter@korsgaard.com>
2018-10-07 12:47:21 +0200

    git: security bump to version 2.16.5
    Fixes CVE-2018-17456: RCE issue in handling of git submodules
    
    For more details, see the announcement:
    https://marc.info/?l=git&m=153875888916397&w=2
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 063eff9bc622b223530e7a2a83a45f7adb5c7d6d)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/git/git.mk
+++ package/git/git.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GIT_VERSION = 2.16.4
+GIT_VERSION = 2.16.5
 GIT_SOURCE = git-$(GIT_VERSION).tar.xz
 GIT_SITE = $(BR2_KERNEL_MIRROR)/software/scm/git
 GIT_LICENSE = GPL-2.0, LGPL-2.1+
