67e4206188c30021856c55f650831f6be0780f6a
Asaf Kahlon <asafka7@gmail.com>
2018-10-08 21:31:38 +0300

    libuv: bump to version 1.23.2
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libuv/libuv.mk
+++ package/libuv/libuv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUV_VERSION = v1.23.1
+LIBUV_VERSION = v1.23.2
 LIBUV_SITE = $(call github,libuv,libuv,$(LIBUV_VERSION))
 LIBUV_DEPENDENCIES = host-pkgconf
 LIBUV_INSTALL_STAGING = YES
