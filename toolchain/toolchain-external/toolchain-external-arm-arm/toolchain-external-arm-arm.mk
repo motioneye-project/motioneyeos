a98f7236c42b365b69907cb65d91353117aa0434
Romain Naour <romain.naour@gmail.com>
2018-10-09 22:07:00 +0200

    toolchain-external: add Arm ARM toolchain 8.2-2018.08
    This is the same toolchain that was previously distributed by Linaro. [1]
    
    Switch default toolchain as this toolchain supersed the Linaro ARM toolchain.
    Only x86_64 host are supported, so keep Linaro toolchain for x86 host.
    
    Tested with qemu_arm_vexpress_defconfig.
    
    [1] https://developer.arm.com/open-source/gnu-toolchain/gnu-a/downloads
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ toolchain/toolchain-external/toolchain-external-arm-arm/toolchain-external-arm-arm.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# toolchain-external-arm-arm
+#
+################################################################################
+
+TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION = 2018.08
+TOOLCHAIN_EXTERNAL_ARM_ARM_SITE = https://developer.arm.com/-/media/Files/downloads/gnu-a/8.2-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)
+
+TOOLCHAIN_EXTERNAL_ARM_ARM_SOURCE = gcc-arm-8.2-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)-x86_64-arm-linux-gnueabihf.tar.xz
+
+$(eval $(toolchain-external-package))
