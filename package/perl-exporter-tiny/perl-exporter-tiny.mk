dc2b892dfe8312511c2a49904b1a9f6588c5385a
Francois Perrad <fperrad@gmail.com>
2018-10-10 10:18:30 +0200

    perl-exporter-tiny: new package
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/perl-exporter-tiny/perl-exporter-tiny.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-exporter-tiny
+#
+################################################################################
+
+PERL_EXPORTER_TINY_VERSION = 1.002001
+PERL_EXPORTER_TINY_SOURCE = Exporter-Tiny-$(PERL_EXPORTER_TINY_VERSION).tar.gz
+PERL_EXPORTER_TINY_SITE = $(BR2_CPAN_MIRROR)/authors/id/T/TO/TOBYINK
+PERL_EXPORTER_TINY_LICENSE = Artistic or GPL-1.0+
+PERL_EXPORTER_TINY_LICENSE_FILES = COPYRIGHT LICENSE
+
+$(eval $(perl-package))
