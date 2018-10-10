b776591ce84aa0ff06ef1baf4bd8df8af1d8dff1
Francois Perrad <fperrad@gmail.com>
2018-10-10 10:18:26 +0200

    perl-sub-exporter-progressive: new package
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/perl-sub-exporter-progressive/perl-sub-exporter-progressive.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-sub-exporter-progressive
+#
+################################################################################
+
+PERL_SUB_EXPORTER_PROGRESSIVE_VERSION = 0.001013
+PERL_SUB_EXPORTER_PROGRESSIVE_SOURCE = Sub-Exporter-Progressive-$(PERL_SUB_EXPORTER_PROGRESSIVE_VERSION).tar.gz
+PERL_SUB_EXPORTER_PROGRESSIVE_SITE = $(BR2_CPAN_MIRROR)/authors/id/F/FR/FREW
+PERL_SUB_EXPORTER_PROGRESSIVE_LICENSE = Artistic or GPL-1.0+
+PERL_SUB_EXPORTER_PROGRESSIVE_LICENSE_FILES = LICENSE
+
+$(eval $(perl-package))
