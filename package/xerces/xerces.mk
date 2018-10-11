b449ba80077d94e503521a53bc7e2613901dd1c7
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-10-11 21:04:29 +0200

    xerces: fix buid with older cmake
    Old version of cmake break cross-compilation when FindThreads is used
    because of a try_run call. Add a workaround that avoids try_run.
    
    cmake bug report: https://gitlab.kitware.com/cmake/cmake/issues/16920
    
    Fixes:
     - http://autobuild.buildroot.org/results/784918c58310474e8b4a0b42850a55ba35a1a19e
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/xerces/xerces.mk
+++ package/xerces/xerces.mk
@@ -17,6 +17,11 @@ endef
 
 XERCES_POST_PATCH_HOOKS += XERCES_DISABLE_SAMPLES
 
+# Before CMake 3.10, passing THREADS_PTHREAD_ARG=OFF was needed to
+# disable a try_run() call in the FindThreads tests, which caused a
+# build failure when cross-compiling.
+XERCES_CONF_OPTS += -DTHREADS_PTHREAD_ARG=OFF
+
 ifeq ($(BR2_PACKAGE_ICU),y)
 XERCES_DEPENDENCIES += icu
 endif
