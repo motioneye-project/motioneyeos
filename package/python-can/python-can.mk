5b4042073fdea5592a9426993896353d3cfd40ca
Yegor Yefremov <yegorslists@googlemail.com>
2018-10-12 10:06:50 +0200

    python-can: bump to version 3.0.0
    Add new dependencies and change download location.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Reviewed-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-can/python-can.mk
+++ package/python-can/python-can.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_CAN_VERSION = 2.2.1
-PYTHON_CAN_SITE = https://files.pythonhosted.org/packages/e6/49/7a25ca6f38421009621b9958a725a0bd57dc0caa656ee508324f26ea5363
+PYTHON_CAN_VERSION = 3.0.0
+PYTHON_CAN_SITE = https://files.pythonhosted.org/packages/29/47/59d07bb02d6b244fb631487ae5424a10658c316defeeb90c923b48043792
+PYTHON_CAN_SETUP_TYPE = setuptools
 PYTHON_CAN_LICENSE = LGPL-3.0
 PYTHON_CAN_LICENSE_FILES = LICENSE.txt
-PYTHON_CAN_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
