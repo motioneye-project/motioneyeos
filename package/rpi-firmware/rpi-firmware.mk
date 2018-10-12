d34ff534a13ed7233a0b2ee104ed417d0e2217f2
Peter Seiderer <ps.report@gmx.net>
2018-10-12 23:09:38 +0200

    rpi-firmware: bump version to fbad6408c4
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 287af2a2be0787a5d45281d1d6183a2161c798d4
+RPI_FIRMWARE_VERSION = fbad6408c4596d3d671736ee0571aae444f24e68
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
