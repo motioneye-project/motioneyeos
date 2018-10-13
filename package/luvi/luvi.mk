4025a65604c0be322cf02e7b3e3c76c3b19a3ea2
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-10-13 10:01:02 +0200

    Revert "package/luvi: bump version to 2.8.0"
    This reverts commit ac6fedda6b43996545ff498cd19a781698a96050.
    
    Quoting Jörg Krause:
    http://lists.busybox.net/pipermail/buildroot/2018-August/228534.html
    
    "The version bump as version 2.8.0 does only updates the projects
     submodules, which we are not using at all. Instead, we always build
     luvi with dependencies provided as packages in Buildroot."
    
    [Peter: drop autobuilder reference, issue is in luv, not luvi]
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/luvi/luvi.mk
+++ package/luvi/luvi.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-LUVI_VERSION = v2.8.0
-LUVI_SITE = https://github.com/luvit/luvi.git
-LUVI_SITE_METHOD = git
-LUVI_GIT_SUBMODULES = YES
+# we keep version 2.7.6 because 2.8.0 causes build errors and only
+# contains updates to submodules we do not need
+LUVI_VERSION = v2.7.6
+LUVI_SOURCE = luvi-src-$(LUVI_VERSION).tar.gz
+LUVI_SITE = https://github.com/luvit/luvi/releases/download/$(LUVI_VERSION)
 LUVI_LICENSE = Apache-2.0
 LUVI_LICENSE_FILES = LICENSE.txt
 LUVI_DEPENDENCIES = libuv luajit luv host-luajit
