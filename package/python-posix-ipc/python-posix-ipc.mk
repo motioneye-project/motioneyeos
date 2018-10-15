f099ef68166ad84eb204df8a04e97c00aae99b4c
Asaf Kahlon <asafka7@gmail.com>
2018-10-15 21:19:59 +0300

    python-posix-ipc: bump to version 1.0.4
    Also add license hash.
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-posix-ipc/python-posix-ipc.mk
+++ package/python-posix-ipc/python-posix-ipc.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_POSIX_IPC_VERSION = 1.0.0
+PYTHON_POSIX_IPC_VERSION = 1.0.4
 PYTHON_POSIX_IPC_SOURCE = posix_ipc-$(PYTHON_POSIX_IPC_VERSION).tar.gz
-PYTHON_POSIX_IPC_SITE = https://pypi.python.org/packages/f0/e6/bff62b62b2e75f695b737695951b7a4c1c6595369268a37868f5c34e1c12
+PYTHON_POSIX_IPC_SITE = https://files.pythonhosted.org/packages/c9/3e/54217da71aa26b488295d878df4d3132093253b4ae5798ac66fcb6921ef0
 PYTHON_POSIX_IPC_LICENSE = BSD-3-Clause
 PYTHON_POSIX_IPC_LICENSE_FILES = LICENSE
-PYTHON_POSIX_IPC_SETUP_TYPE = distutils
+PYTHON_POSIX_IPC_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
