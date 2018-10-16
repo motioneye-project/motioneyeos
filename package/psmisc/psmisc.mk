d6f2fb191828b49814b78605ee193a21750380eb
Baruch Siach <baruch@tkos.co.il>
2018-10-16 19:07:14 +0300

    psmisc: bump to version 23.2
    Drop patch #1; applied upstream.
    
    Drop patch #2; not needed since we don't autoreconf, and the issue is
    fixed upstream anyway.
    
    Add license hash.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/psmisc/psmisc.mk
+++ package/psmisc/psmisc.mk
@@ -4,19 +4,12 @@
 #
 ################################################################################
 
-PSMISC_VERSION = 23.1
+PSMISC_VERSION = 23.2
 PSMISC_SITE = http://downloads.sourceforge.net/project/psmisc/psmisc
 PSMISC_SOURCE = psmisc-$(PSMISC_VERSION).tar.xz
 PSMISC_LICENSE = GPL-2.0+
 PSMISC_LICENSE_FILES = COPYING
 PSMISC_DEPENDENCIES = ncurses $(TARGET_NLS_DEPENDENCIES)
-# Patching Makefile.am
-PSMISC_AUTORECONF = YES
-
-define PSMISC_GIT_VERSION_EXEC
-	chmod +x $(@D)/misc/git-version-gen
-endef
-PSMISC_POST_PATCH_HOOKS += PSMISC_GIT_VERSION_EXEC
 
 ifeq ($(BR2_TOOLCHAIN_HAS_SSP),)
 # Don't force -fstack-protector when SSP is not available in toolchain
