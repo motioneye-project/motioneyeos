73ea45a608f560d261da66b6e9ab9cba7add3e22
Francois Gerin <francois.gerin@essensium.com>
2018-10-16 14:31:08 +0200

    qt download site update
    The download link was broken, former qt versions are stored into a
    distinct location.
    
    Signed-off-by: Francois Gerin <francois.gerin@essensium.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 2e6cd5c2d622eb1d092c1639b129a24e79c84367)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/qt/qt.mk
+++ package/qt/qt.mk
@@ -7,7 +7,7 @@
 QT_VERSION_MAJOR = 4.8
 QT_VERSION = $(QT_VERSION_MAJOR).7
 QT_SOURCE = qt-everywhere-opensource-src-$(QT_VERSION).tar.gz
-QT_SITE = http://download.qt-project.org/official_releases/qt/$(QT_VERSION_MAJOR)/$(QT_VERSION)
+QT_SITE = http://download.qt.io/archive/qt/$(QT_VERSION_MAJOR)/$(QT_VERSION)
 QT_DEPENDENCIES = host-pkgconf
 QT_INSTALL_STAGING = YES
 
