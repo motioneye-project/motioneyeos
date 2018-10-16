a1eaa3c52c6a3b60cd6535dec6b2ef6fdb94e7a2
Asaf Kahlon <asafka7@gmail.com>
2018-10-16 21:05:14 +0300

    python-markdown2: bump to version 2.3.6
    Also add license hash.
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-markdown2/python-markdown2.mk
+++ package/python-markdown2/python-markdown2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MARKDOWN2_VERSION = 2.3.2
+PYTHON_MARKDOWN2_VERSION = 2.3.6
 PYTHON_MARKDOWN2_SITE = $(call github,trentm,python-markdown2,$(PYTHON_MARKDOWN2_VERSION))
 PYTHON_MARKDOWN2_SETUP_TYPE = distutils
 PYTHON_MARKDOWN2_LICENSE = MIT
