cfa3447a7829aed59ce673b22212f5c82f83fc61
Baruch Siach <baruch@tkos.co.il>
2018-10-16 19:07:13 +0300

    psmisc: correct license
    The license heading in source files includes the "or any later"
    language.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/psmisc/psmisc.mk
+++ package/psmisc/psmisc.mk
@@ -7,7 +7,7 @@
 PSMISC_VERSION = 23.1
 PSMISC_SITE = http://downloads.sourceforge.net/project/psmisc/psmisc
 PSMISC_SOURCE = psmisc-$(PSMISC_VERSION).tar.xz
-PSMISC_LICENSE = GPL-2.0
+PSMISC_LICENSE = GPL-2.0+
 PSMISC_LICENSE_FILES = COPYING
 PSMISC_DEPENDENCIES = ncurses $(TARGET_NLS_DEPENDENCIES)
 # Patching Makefile.am
