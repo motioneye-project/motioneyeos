48f495854d0a85d1241061a2a279aa262e8783ef
James Hilliard <james.hilliard1@gmail.com>
2018-10-22 17:31:36 +0800

    python-yarl: new package
    Yet another URL library
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

---
+++ package/python-yarl/python-yarl.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# python-yarl
+#
+################################################################################
+PYTHON_YARL_VERSION = 1.2.6
+PYTHON_YARL_SOURCE = yarl-$(PYTHON_YARL_VERSION).tar.gz
+PYTHON_YARL_SITE = https://files.pythonhosted.org/packages/43/b8/057c3e5b546ff4b24263164ecda13f6962d85c9dc477fcc0bcdcb3adb658
+PYTHON_YARL_LICENSE = Apache-2.0
+PYTHON_YARL_LICENSE_FILES = LICENSE
+PYTHON_YARL_SETUP_TYPE = setuptools
+
+$(eval $(python-package))
