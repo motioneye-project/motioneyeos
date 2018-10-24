a4e3de63e43af74c021307567e5cc8570ce4654c
James Hilliard <james.hilliard1@gmail.com>
2018-10-24 21:56:33 +0800

    python-multidict: new package
    Multidict is dict-like collection of key-value pairs where a key may
    occur more than once in the container.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Reviewed-by: Yegor Yefremov <yegorslists@googlemail.com>
    [Arnout: extend help text based on commit message.]
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

---
+++ package/python-multidict/python-multidict.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-multidict
+#
+################################################################################
+
+PYTHON_MULTIDICT_VERSION = 4.4.2
+PYTHON_MULTIDICT_SOURCE = multidict-$(PYTHON_MULTIDICT_VERSION).tar.gz
+PYTHON_MULTIDICT_SITE = https://files.pythonhosted.org/packages/b3/5f/5c29cde8511c95fad045b9ecaf2e76f0da18761e8363a82594f5a58c2ced
+PYTHON_MULTIDICT_SETUP_TYPE = setuptools
+PYTHON_MULTIDICT_LICENSE = Apache-2.0
+PYTHON_MULTIDICT_LICENSE_FILES = LICENSE
+
+$(eval $(python-package))
