4f56655b2b7dfe753a61a48b23ee3fe62f9d12fb
Francois Beerten <fbeerten.git@colabti.org>
2018-10-25 12:05:28 +0200

    board/zynqmp/post-image.sh: support dots in DTS file names
    Some ZynqMP device trees in the kernel have a dot in their name,
    for example "zynqmp-zcu102-rev1.0". The post image script
    left out the part after the dot and created an invalid symlink.
    
    Signed-off-by: Francois Beerten <fbeerten.git@colabti.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- board/zynqmp/post-image.sh
+++ board/zynqmp/post-image.sh
@@ -5,7 +5,7 @@
 # devicetree listed in the config.
 
 FIRST_DT=$(sed -nr \
-               -e 's|^BR2_LINUX_KERNEL_INTREE_DTS_NAME="xilinx/([-_/[:alnum:]]*).*"$|\1|p' \
+               -e 's|^BR2_LINUX_KERNEL_INTREE_DTS_NAME="xilinx/([-_/[:alnum:]\\.]*).*"$|\1|p' \
                ${BR2_CONFIG})
 
 [ -z "${FIRST_DT}" ] || ln -fs ${FIRST_DT}.dtb ${BINARIES_DIR}/system.dtb
