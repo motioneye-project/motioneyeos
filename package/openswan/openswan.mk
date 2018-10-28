1de17e341b05be0e94a40c87d61de845cfbcf399
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-10-28 11:22:35 +0100

    openswan: security bump to version 2.6.50.1
    - Fixes CVE-2018-15836 (a Bleichenbacher-style signature forgery which
      involves RSA padding attack)
    - Add hash for license files
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/openswan/openswan.mk
+++ package/openswan/openswan.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPENSWAN_VERSION = 2.6.50
+OPENSWAN_VERSION = 2.6.50.1
 OPENSWAN_SITE = https://download.openswan.org/openswan
 OPENSWAN_LICENSE = GPL-2.0+, BSD-3-Clause
 OPENSWAN_LICENSE_FILES = COPYING LICENSE
