a3366b270562f42723a3b8032f73bb03b541d113
Petr Vorel <petr.vorel@gmail.com>
2018-10-29 00:09:51 +0100

    kconfig: Restore missing custom changes + add patches
    Commit 6eacea5ae0 accidentally removed these changes in merge_config.sh:
    0f56304521 ("merge_config.sh: create temporary files in /tmp")
    28fac3973b ("merge_config.sh: add br2-external support")
    
    Changes were lost because commits just changed files, but didn't add patches.
    Therefore not only restore our changes, but also add (updated) patches.
    
    Missing 0f56304521 caused breaking merge_config.sh when used in out of
    tree build:
    $ make -C buildroot O=$PWD/output defconfig
    ...
    $ cd output
    $ echo 'BR2_TARGET_GENERIC_HOSTNAME="test"' > test.frag
    $ ../buildroot/support/kconfig/merge_config.sh .config test.frag
    Using .config as base
    Merging test.frag
    umask 0022 && make -C /home/test/buildroot O=/home/test/output/. alldefconfig
      GEN     /home/test/output/Makefile
    *** Can't read seed configuration "./.tmp.config.qIcpASpUyh"!
    make[1]: *** [Makefile:925: alldefconfig] Error 1
    make: *** [Makefile:16: _all] Error 2
    
    Fixes: 6eacea5ae0 support/kconfig: bump to kconfig from Linux 4.17-rc2
    
    Signed-off-by: Petr Vorel <petr.vorel@gmail.com>
    Reported-by: Erico Nunes <nunes.erico@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- support/kconfig/merge_config.sh
+++ support/kconfig/merge_config.sh
@@ -33,6 +33,7 @@ usage() {
 	echo "  -n    use allnoconfig instead of alldefconfig"
 	echo "  -r    list redundant entries when merging fragments"
 	echo "  -O    dir to put generated output files.  Consider setting \$KCONFIG_CONFIG instead."
+	echo "  -e    colon-separated list of br2-external trees to use (optional)"
 }
 
 RUNMAKE=true
@@ -71,6 +72,11 @@ while true; do
 		shift 2
 		continue
 		;;
+	"-e")
+		EXTERNAL_ARG="BR2_EXTERNAL=$2"
+		shift 2
+		continue
+		;;
 	*)
 		break
 		;;
@@ -100,7 +106,7 @@ fi
 
 MERGE_LIST=$*
 SED_CONFIG_EXP="s/^\(# \)\{0,1\}\(CONFIG_[a-zA-Z0-9_]*\)[= ].*/\2/p"
-TMP_FILE=$(mktemp ./.tmp.config.XXXXXXXXXX)
+TMP_FILE=$(mktemp -t .tmp.config.XXXXXXXXXX)
 
 echo "Using $INITFILE as base"
 cat $INITFILE > $TMP_FILE
@@ -151,7 +157,7 @@ fi
 # Use the merged file as the starting point for:
 # alldefconfig: Fills in any missing symbols with Kconfig default
 # allnoconfig: Fills in any missing symbols with # CONFIG_* is not set
-make KCONFIG_ALLCONFIG=$TMP_FILE $OUTPUT_ARG $ALLTARGET
+make KCONFIG_ALLCONFIG=$TMP_FILE $EXTERNAL_ARG $OUTPUT_ARG $ALLTARGET
 
 
 # Check all specified config values took (might have missed-dependency issues)
