67190f763560612a7e8393d2e2af9f8c5695197c
Christian Stewart <christian@paral.in>
2018-10-29 09:54:22 -0700

    go: bump to v1.11.1
    Bumps Golang host-go compiler to 1.11.1 release.
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.11
+GO_VERSION = 1.11.1
 GO_SITE = https://storage.googleapis.com/golang
 GO_SOURCE = go$(GO_VERSION).src.tar.gz
 
