614982a9111422236346244418fcc3e2369b16bb
Baruch Siach <baruch@tkos.co.il>
2018-10-30 13:40:22 +0200

    libnpth: bump to version 1.6
    Cc: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libnpth/libnpth.mk
+++ package/libnpth/libnpth.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBNPTH_VERSION = 1.5
+LIBNPTH_VERSION = 1.6
 LIBNPTH_SOURCE = npth-$(LIBNPTH_VERSION).tar.bz2
 LIBNPTH_SITE = https://www.gnupg.org/ftp/gcrypt/npth
 LIBNPTH_LICENSE = LGPL-2.0+
