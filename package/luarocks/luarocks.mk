864f85262f3fdeb3ea76a777e62f31ccc12c75ed
Francois Perrad <fperrad@gmail.com>
2018-10-31 07:50:14 +0100

    luarocks: bump to version 3.0.4
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/luarocks/luarocks.mk
+++ package/luarocks/luarocks.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUAROCKS_VERSION = 3.0.3
+LUAROCKS_VERSION = 3.0.4
 LUAROCKS_SITE = http://luarocks.org/releases
 LUAROCKS_LICENSE = MIT
 LUAROCKS_LICENSE_FILES = COPYING
