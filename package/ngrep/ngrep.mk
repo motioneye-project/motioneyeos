100ba8351c088fd764effdcaba848cfd92de79dd
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-11-04 13:14:27 +0100

    package/ngrep: add optional dependency on libdnet
    - Enable tcpkill based on libnet availability
    - Add BSD-3-Clause for tcpkill, added in version 1.46 and
      https://github.com/jpr5/ngrep/commit/f2a1f8b09194a8ab87787c1fdb60b968f5957c05
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/ngrep/ngrep.mk
+++ package/ngrep/ngrep.mk
@@ -6,7 +6,7 @@
 
 NGREP_VERSION = 1_47
 NGREP_SITE = $(call github,jpr5,ngrep,V$(NGREP_VERSION))
-NGREP_LICENSE = BSD-4-Clause-like
+NGREP_LICENSE = BSD-4-Clause-like, BSD-3-Clause (tcpkill)
 NGREP_LICENSE_FILES = LICENSE
 NGREP_INSTALL_STAGING = YES
 # We're patching configure.in
@@ -20,9 +20,15 @@ NGREP_CONF_OPTS = \
 	--with-pcap-includes=$(STAGING_DIR)/usr/include/pcap \
 	--enable-pcre \
 	--disable-dropprivs \
-	--disable-pcap-restart \
-	--disable-tcpkill
+	--disable-pcap-restart
 
 NGREP_DEPENDENCIES = libpcap pcre
+
+ifeq ($(BR2_PACKAGE_LIBNET),y)
+NGREP_DEPENDENCIES += libnet
+NGREP_CONF_OPTS += --enable-tcpkill
+else
+NGREP_CONF_OPTS += --disable-tcpkill
+endif
 
 $(eval $(autotools-package))
