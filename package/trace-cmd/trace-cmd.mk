6d1fad170eefafee056292a7f8bd56655c024378
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-11-10 18:48:47 +0100

    trace-cmd: bump to version 2.7
    Add hash for license files
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/trace-cmd/trace-cmd.mk
+++ package/trace-cmd/trace-cmd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TRACE_CMD_VERSION = trace-cmd-v2.6.1
+TRACE_CMD_VERSION = trace-cmd-v2.7
 TRACE_CMD_SITE = https://git.kernel.org/pub/scm/linux/kernel/git/rostedt/trace-cmd.git
 TRACE_CMD_SITE_METHOD = git
 TRACE_CMD_INSTALL_STAGING = YES
