08cd849bee8fb5c77db9d12de9f78e91d2002646
Carlos Santos <casantos@datacom.com.br>
2018-11-16 00:52:16 -0200

    tpm2-abrmd: bump to version 2.0.3
    - Drop C++ requirement, which came from tpm2-tss.
    - Drom musl restriction.
    - Update daemon command-line options in init script.
    
    Signed-off-by: Carlos Santos <casantos@datacom.com.br>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tpm2-abrmd/tpm2-abrmd.mk
+++ package/tpm2-abrmd/tpm2-abrmd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TPM2_ABRMD_VERSION = 1.3.0
+TPM2_ABRMD_VERSION = 2.0.3
 TPM2_ABRMD_SITE = https://github.com/tpm2-software/tpm2-abrmd/releases/download/$(TPM2_ABRMD_VERSION)
 TPM2_ABRMD_LICENSE = BSD-2-Clause
 TPM2_ABRMD_LICENSE_FILES = LICENSE
