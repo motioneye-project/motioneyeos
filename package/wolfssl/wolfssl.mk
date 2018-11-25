d9adaa15f96862cc1dd8beddf4a777cecedef2df
Sergio Prado <sergio.prado@e-labworks.com>
2018-11-25 06:36:19 -0200

    package/wolfssl: bump to version 3.15.5
    Signed-off-by: Sergio Prado <sergio.prado@e-labworks.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/wolfssl/wolfssl.mk
+++ package/wolfssl/wolfssl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WOLFSSL_VERSION = 3.14.0
+WOLFSSL_VERSION = 3.15.5
 WOLFSSL_SITE = https://github.com/wolfSSL/wolfssl/archive
 WOLFSSL_SOURCE = v$(WOLFSSL_VERSION)-stable.tar.gz
 WOLFSSL_INSTALL_STAGING = YES
