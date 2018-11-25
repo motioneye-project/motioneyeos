83d19028126ffcad91913dd62d862809f743fd64
Yann E. MORIN <yann.morin.1998@free.fr>
2018-11-25 12:26:12 +0100

    package/dante: needs autoreconf
    We have a patch that touches a .m4 file, so we need to regenerate the
    configure script. Otherwise, this is done during the build step, and
    some environment variables are thus missing and the build may fail when
    the host machine does not have the expected autostuff tools.
    
    Fixes:
        http://autobuild.buildroot.org/results/e37/e37e61bae1d81a7956e2843be70fea84b0bbb64b/
        http://autobuild.buildroot.org/results/f96/f969718402cae71446d6280ec1f66d357a155293/
        ...
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/dante/dante.mk
+++ package/dante/dante.mk
@@ -9,8 +9,8 @@ DANTE_SITE = http://www.inet.no/dante/files
 DANTE_LICENSE = BSD-3-Clause
 DANTE_LICENSE_FILES = LICENSE
 
-# Dante uses a *VERY* old configure.ac
-DANTE_LIBTOOL_PATCH = NO
+# 0002-compiler.m4-do-not-remove-g-flag.patch touches a m4 file
+DANTE_AUTORECONF = YES
 
 DANTE_CONF_OPTS += --disable-client --disable-preload --without-pam
 
