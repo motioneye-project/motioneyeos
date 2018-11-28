164aa3d90916fae95e81dd54df2eb62f8d20a696
Joseph Kogut <joseph.kogut@gmail.com>
2018-11-28 10:08:32 -0800

    python-networkmanager: bump to version 2.1
    Signed-off-by: Joseph Kogut <joseph.kogut@gmail.com>
    Acked-by: Petr Vorel <petr.vorel@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-networkmanager/python-networkmanager.mk
+++ package/python-networkmanager/python-networkmanager.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_NETWORKMANAGER_VERSION = 2.0.1
-PYTHON_NETWORKMANAGER_SITE = https://pypi.python.org/packages/d7/f9/5cbd99fd24a072875ce048e48d1754285f137aab447de8fee63b6cba990a
+PYTHON_NETWORKMANAGER_VERSION = 2.1
+PYTHON_NETWORKMANAGER_SITE = https://files.pythonhosted.org/packages/73/ec/2cc46290cedc99f59df3633d428dfa91d95946ca49af96fdb53514ee195f
 PYTHON_NETWORKMANAGER_SETUP_TYPE = setuptools
 PYTHON_NETWORKMANAGER_LICENSE = Zlib
 PYTHON_NETWORKMANAGER_LICENSE_FILES = COPYING
