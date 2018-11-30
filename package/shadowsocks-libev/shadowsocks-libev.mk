8ab22b7d5c79c9bc84728db7c08556e5f83cdd5d
DUPONCHEEL Sébastien <sebastien.duponcheel@corp.ovh.com>
2018-11-30 17:39:43 +0100

    package/shadowsocks-libev: bump to version 3.2.3
    Signed-off-by: DUPONCHEEL Sébastien <sebastien.duponcheel@corp.ovh.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/shadowsocks-libev/shadowsocks-libev.mk
+++ package/shadowsocks-libev/shadowsocks-libev.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SHADOWSOCKS_LIBEV_VERSION = 3.2.1
+SHADOWSOCKS_LIBEV_VERSION = 3.2.3
 SHADOWSOCKS_LIBEV_SITE = https://github.com/shadowsocks/shadowsocks-libev/releases/download/v$(SHADOWSOCKS_LIBEV_VERSION)
 SHADOWSOCKS_LIBEV_LICENSE = GPL-3.0+, BSD-2-Clause (libbloom), BSD-3-Clause (libcork, libipset)
 SHADOWSOCKS_LIBEV_LICENSE_FILES = COPYING libbloom/LICENSE libcork/COPYING
@@ -14,10 +14,11 @@ SHADOWSOCKS_LIBEV_CONF_OPTS = \
 	--with-pcre=$(STAGING_DIR)/usr \
 	--disable-ssp
 
-# --disable option was broken in 3.2.1, it will be fixed in 3.2.2
 ifeq ($(BR2_PACKAGE_SHADOWSOCKS_LIBEV_CONNMARKTOS),y)
 SHADOWSOCKS_LIBEV_DEPENDENCIES += libnetfilter_conntrack
 SHADOWSOCKS_LIBEV_CONF_OPTS += --enable-connmarktos
+else
+SHADOWSOCKS_LIBEV_CONF_OPTS += --disable-connmarktos
 endif
 
 $(eval $(autotools-package))
