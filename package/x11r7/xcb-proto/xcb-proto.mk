598cd3dc55dfd805de0fa7416062ee3c4a063402
Bernd Kuhls <bernd.kuhls@t-online.de>
2018-12-09 21:38:51 +0100

    package/x11r7/xcb-proto: remove pkgconf fix
    Not needed anymore after pkgconf bump.
    
    Fixes
    http://autobuild.buildroot.net/results/175/1752915119b795c916d75594e51a7c79126afd5f/
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/x11r7/xcb-proto/xcb-proto.mk
+++ package/x11r7/xcb-proto/xcb-proto.mk
@@ -15,15 +15,5 @@ XCB_PROTO_INSTALL_STAGING = YES
 XCB_PROTO_DEPENDENCIES = host-python
 HOST_XCB_PROTO_DEPENDENCIES = host-python
 
-# xcbincludedir/pythondir is used by E.G. libxcb at build time to find the
-# xml / python files, so ensure these expand to their full (host) paths
-define XCB_PROTO_FIXUP_PC_FILE
-	$(SED) 's|^\(xcbincludedir=\)|\1$(STAGING_DIR)|' \
-		-e 's|^\(pythondir=\)|\1$(STAGING_DIR)|' \
-		$(STAGING_DIR)/usr/lib/pkgconfig/xcb-proto.pc
-endef
-
-XCB_PROTO_POST_INSTALL_STAGING_HOOKS += XCB_PROTO_FIXUP_PC_FILE
-
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
