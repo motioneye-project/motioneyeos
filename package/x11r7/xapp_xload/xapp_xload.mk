ef313b0bf7ccd560e034591ea00444db6f09eab2
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-12-13 22:04:50 +0100

    xapp_xload: fix build with pkgconf 1.5.3
    Since the bump to 1.5.3, pkgconf prepends the sysroot to all absolute
    paths found in the .pc file. This is correct when the paths refer to
    something in STAGING_DIR (e.g. libdir, includedir), but not when it
    refers to something used for the target.
    
    xapp_xload uses the appdefaultdir variable from xt.pc to decide where to
    install things. Since DESTDIR is prepended to the install destination,
    this will end up in the wrong location.
    
    Until a better solution is found in pkgconf, pass the appdefaultdir to
    use explicitly instead of relying on xt.pc.
    
    Fixes:
     - http://autobuild.buildroot.org/results/722c1bdfb3f38a13be7a40793a8df4f0324885f4
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/x11r7/xapp_xload/xapp_xload.mk
+++ package/x11r7/xapp_xload/xapp_xload.mk
@@ -10,6 +10,7 @@ XAPP_XLOAD_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XLOAD_LICENSE = MIT
 XAPP_XLOAD_LICENSE_FILES = COPYING
 XAPP_XLOAD_DEPENDENCIES = xlib_libXaw
+XAPP_XLOAD_CONF_OPTS = --with-appdefaultdir=/usr/share/X11/app-defaults
 
 ifeq ($(BR2_TOOLCHAIN_USES_MUSL),y)
 # musl doesn't have rwhod.h, but xload can replace it with stubs
