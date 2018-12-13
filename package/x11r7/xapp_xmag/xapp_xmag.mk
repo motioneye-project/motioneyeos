153c5e8e92a64dac36b1d8da7e102301cbd0fed7
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-12-13 22:07:02 +0100

    xapp_xmag: fix build with pkgconf 1.5.3
    Since the bump to 1.5.3, pkgconf prepends the sysroot to all absolute
    paths found in the .pc file. This is correct when the paths refer to
    something in STAGING_DIR (e.g. libdir, includedir), but not when it
    refers to something used for the target.
    
    xapp_xmag uses the appdefaultdir variable from xt.pc to decide where to
    install things. Since DESTDIR is prepended to the install destination,
    this will end up in the wrong location.
    
    Until a better solution is found in pkgconf, pass the appdefaultdir to
    use explicitly instead of relying on xt.pc.
    
    Fixes:
     - http://autobuild.buildroot.org/results/37dd630639a0d76e8121b3cca9e0e1f305ad620b
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/x11r7/xapp_xmag/xapp_xmag.mk
+++ package/x11r7/xapp_xmag/xapp_xmag.mk
@@ -10,5 +10,6 @@ XAPP_XMAG_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XMAG_LICENSE = MIT
 XAPP_XMAG_LICENSE_FILES = COPYING
 XAPP_XMAG_DEPENDENCIES = xlib_libXaw
+XAPP_XMAG_CONF_OPTS = --with-appdefaultdir=/usr/share/X11/app-defaults
 
 $(eval $(autotools-package))
