712c2ef0176c9cc25bb8f236d1705cd4a2c35db2
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2018-12-14 16:28:42 +0100

    xapp_xfd: fix build with pkgconf 1.5.3
    Since the bump to 1.5.3, pkgconf prepends the sysroot to all absolute
    paths found in the .pc file. This is correct when the paths refer to
    something in STAGING_DIR (e.g. libdir, includedir), but not when it
    refers to something used for the target.
    
    xapp_xfd uses the appdefaultdir variable from xt.pc to decide where to
    install things. Since DESTDIR is prepended to the install destination,
    this will end up in the wrong location.
    
    Until a better solution is found in pkgconf, pass the appdefaultdir to
    use explicitly instead of relying on xt.pc.
    
    Fixes:
     - http://autobuild.buildroot.org/results/ea9884f4d676849d643d53275ebbc8668074a418
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xapp_xfd/xapp_xfd.mk
+++ package/x11r7/xapp_xfd/xapp_xfd.mk
@@ -10,5 +10,6 @@ XAPP_XFD_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XFD_LICENSE = MIT
 XAPP_XFD_LICENSE_FILES = COPYING
 XAPP_XFD_DEPENDENCIES = freetype fontconfig xlib_libXaw xlib_libXft
+XAPP_XFD_CONF_OPTS = --with-appdefaultdir=/usr/share/X11/app-defaults
 
 $(eval $(autotools-package))
