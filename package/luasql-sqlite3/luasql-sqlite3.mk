a628cbda50113139dd4f76db48d0ac5193d20d0f
Francois Perrad <fperrad@gmail.com>
2018-12-16 09:56:56 +0100

    luasql-sqlite3: bump to version 2.4.0
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/luasql-sqlite3/luasql-sqlite3.mk
+++ package/luasql-sqlite3/luasql-sqlite3.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUASQL_SQLITE3_VERSION = 2.3.0-1
+LUASQL_SQLITE3_VERSION = 2.4.0-1
 LUASQL_SQLITE3_SUBDIR = luasql
 LUASQL_SQLITE3_LICENSE = MIT
 LUASQL_SQLITE3_LICENSE_FILES = $(LUASQL_SQLITE3_SUBDIR)/doc/us/license.html
