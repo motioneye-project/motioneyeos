40f0f3d0d25ad589f4eb2ba3881c3f8243620746
Francois Perrad <fperrad@gmail.com>
2018-12-16 09:56:47 +0100

    coxpcall: bump to version 1.17.0
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/coxpcall/coxpcall.mk
+++ package/coxpcall/coxpcall.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-COXPCALL_VERSION = 1.16.0-1
+COXPCALL_VERSION = 1.17.0-1
 COXPCALL_SUBDIR = coxpcall
 COXPCALL_LICENSE = MIT
 COXPCALL_LICENSE_FILES = $(COXPCALL_SUBDIR)/doc/license.html
