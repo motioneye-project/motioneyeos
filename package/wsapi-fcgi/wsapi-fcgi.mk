9c51827822a5f0352df7fae9e762e1121a184dd9
Francois Perrad <fperrad@gmail.com>
2018-12-16 21:19:05 +0100

    package/wsapi-fcgi: bump to version 1.7
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/wsapi-fcgi/wsapi-fcgi.mk
+++ package/wsapi-fcgi/wsapi-fcgi.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-WSAPI_FCGI_VERSION_MAJOR = 1.6.1
-WSAPI_FCGI_VERSION = $(WSAPI_FCGI_VERSION_MAJOR)-1
-WSAPI_FCGI_SUBDIR = wsapi-$(WSAPI_FCGI_VERSION_MAJOR)
+WSAPI_FCGI_VERSION = 1.7-1
+WSAPI_FCGI_SUBDIR = wsapi
 WSAPI_FCGI_DEPENDENCIES = libfcgi
 WSAPI_FCGI_LICENSE = MIT
 WSAPI_FCGI_LICENSE_FILES = $(WSAPI_FCGI_SUBDIR)/doc/us/license.html
