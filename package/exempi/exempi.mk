133f85f87dc8337586a2106e32106278fb2498fe
Ilya Averyanov <averyanovin@gmail.com>
2018-12-18 21:11:02 +0300

    package/exempi: new package
    Signed-off-by: Ilya Averyanov <averyanovin@gmail.com>
    [Thomas:
     - add missing dependencies on expat and zlib
     - add missing depends on C++ and dynamic library support, and the
       corresponding Config.in comment
     - add the missing entry to the DEVELOPERS file
     - add the missing hash for the license file
     - adjust indentation in the Config.in file
     - use --disable-unittest instead of --enable-unittest=no.]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/exempi/exempi.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# exempi
+#
+################################################################################
+
+EXEMPI_VERSION = 2.4.5
+EXEMPI_SOURCE = exempi-$(EXEMPI_VERSION).tar.bz2
+EXEMPI_SITE = https://libopenraw.freedesktop.org/download
+EXEMPI_INSTALL_STAGING = YES
+EXEMPI_CONF_OPTS = --enable-unittest=no
+EXEMPI_DEPENDENCIES = host-pkgconf expat zlib
+EXEMPI_LICENSE = BSD-3-Clause
+EXEMPI_LICENSE_FILES = COPYING
+
+$(eval $(autotools-package))
