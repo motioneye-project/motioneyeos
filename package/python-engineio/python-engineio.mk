416d46d72ba2f80398942b5cd29e412f28c43ee7
Asaf Kahlon <asafka7@gmail.com>
2018-12-21 14:29:35 +0200

    python-engineio: bump to version 3.1.1
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-engineio/python-engineio.mk
+++ package/python-engineio/python-engineio.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_ENGINEIO_VERSION = 3.1.0
-PYTHON_ENGINEIO_SITE = https://files.pythonhosted.org/packages/25/37/57dc050723f3e64a0a652711b3831263d99f5ade2e05e5d6038354be7eaa
+PYTHON_ENGINEIO_VERSION = 3.1.1
+PYTHON_ENGINEIO_SITE = https://files.pythonhosted.org/packages/c7/fd/9dad6c1794be838c05610c04e869bca9a3e0d94145ac71e57a9345a784b9
 PYTHON_ENGINEIO_SETUP_TYPE = setuptools
 PYTHON_ENGINEIO_LICENSE = MIT
 PYTHON_ENGINEIO_LICENSE_FILES = LICENSE
