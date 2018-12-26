2f0f8bf66d3b247e4f442acfef67a80fc6e1fef3
Adam Duskett <Aduskett@gmail.com>
2018-12-26 16:26:32 -0500

    package/libcpprestsdk: bump version to v2.10.8
    In adition:
      - Add -DCPPREST_EXCLUDE_WEBSOCKETS=ON as a default CONF_OPT as websocketspp
        is not checked as a dependency.
    
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libcpprestsdk/libcpprestsdk.mk
+++ package/libcpprestsdk/libcpprestsdk.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-LIBCPPRESTSDK_VERSION = v2.10.5
+LIBCPPRESTSDK_VERSION = v2.10.8
 LIBCPPRESTSDK_SITE = $(call github,Microsoft,cpprestsdk,$(LIBCPPRESTSDK_VERSION))
 LIBCPPRESTSDK_LICENSE = MIT
 LIBCPPRESTSDK_LICENSE_FILES = license.txt
 LIBCPPRESTSDK_SUBDIR = Release
 LIBCPPRESTSDK_DEPENDENCIES += host-pkgconf boost openssl zlib
-LIBCPPRESTSDK_CONF_OPTS = -DWERROR=OFF
+LIBCPPRESTSDK_CONF_OPTS = -DWERROR=OFF -DCPPREST_EXCLUDE_WEBSOCKETS=ON
 
 $(eval $(cmake-package))
