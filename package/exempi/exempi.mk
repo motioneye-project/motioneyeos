ac58fc650c49d52539a221f1f31d6f2e19d00203
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2018-12-30 22:34:06 +0100

    package/exempi: use --disable-foo instead of --enable-foo=no
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/exempi/exempi.mk
+++ package/exempi/exempi.mk
@@ -8,7 +8,7 @@ EXEMPI_VERSION = 2.4.5
 EXEMPI_SOURCE = exempi-$(EXEMPI_VERSION).tar.bz2
 EXEMPI_SITE = https://libopenraw.freedesktop.org/download
 EXEMPI_INSTALL_STAGING = YES
-EXEMPI_CONF_OPTS = --enable-unittest=no
+EXEMPI_CONF_OPTS = --disable-unittest
 EXEMPI_DEPENDENCIES = host-pkgconf expat zlib
 EXEMPI_LICENSE = BSD-3-Clause
 EXEMPI_LICENSE_FILES = COPYING
