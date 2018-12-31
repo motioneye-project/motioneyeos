2ec38321d6643202c122297878bba88785795a96
Peter Korsgaard <peter@korsgaard.com>
2018-12-31 09:30:21 +0100

    package/wireshark: security bump to version 2.6.5
    The following vulnerabilities have been fixed:
    
    - wnpa-sec-2018-51 The Wireshark dissection engine could crash. Bug 14466. CVE-2018-19625.
    - wnpa-sec-2018-52 The DCOM dissector could crash. Bug 15130. CVE-2018-19626.
    - wnpa-sec-2018-53 The LBMPDM dissector could crash. Bug 15132. CVE-2018-19623.
    - wnpa-sec-2018-54 The MMSE dissector could go into an infinite loop. Bug 15250. CVE-2018-19622.
    - wnpa-sec-2018-55 The IxVeriWave file parser could crash. Bug 15279. CVE-2018-19627.
    - wnpa-sec-2018-56 The PVFS dissector could crash. Bug 15280. CVE-2018-19624.
    - wnpa-sec-2018-57 The ZigBee ZCL dissector could crash. Bug 15281. CVE-2018-19628.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/wireshark/wireshark.mk
+++ package/wireshark/wireshark.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIRESHARK_VERSION = 2.6.4
+WIRESHARK_VERSION = 2.6.5
 WIRESHARK_SOURCE = wireshark-$(WIRESHARK_VERSION).tar.xz
 WIRESHARK_SITE = https://www.wireshark.org/download/src/all-versions
 WIRESHARK_LICENSE = wireshark license
