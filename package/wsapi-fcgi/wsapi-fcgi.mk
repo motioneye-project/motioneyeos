42fa5bcbfb48f188c2c180854c0bbdefd98c32c8
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2018-12-31 17:59:52 +0100

    package/lua*: fix indentation of license file lists
    The Luarocks addon does not properly indent with a tab, let's fix that
    manually for the time being, to avoid check-package complaints.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/wsapi-fcgi/wsapi-fcgi.mk
+++ package/wsapi-fcgi/wsapi-fcgi.mk
@@ -8,8 +8,8 @@ WSAPI_FCGI_VERSION = 1.7-1
 WSAPI_FCGI_SUBDIR = wsapi
 WSAPI_FCGI_LICENSE = MIT
 WSAPI_FCGI_LICENSE_FILES = \
-    $(WSAPI_FCGI_SUBDIR)/doc/us/license.html \
-    $(WSAPI_FCGI_SUBDIR)/doc/us/license.md
+	$(WSAPI_FCGI_SUBDIR)/doc/us/license.html \
+	$(WSAPI_FCGI_SUBDIR)/doc/us/license.md
 WSAPI_FCGI_DEPENDENCIES = libfcgi
 
 $(eval $(luarocks-package))
--- package/wsapi-xavante/wsapi-xavante.mk
+++ package/wsapi-xavante/wsapi-xavante.mk
@@ -8,7 +8,7 @@ WSAPI_XAVANTE_VERSION = 1.7-1
 WSAPI_XAVANTE_SUBDIR = wsapi
 WSAPI_XAVANTE_LICENSE = MIT
 WSAPI_XAVANTE_LICENSE_FILES = \
-    $(WSAPI_XAVANTE_SUBDIR)/doc/us/license.html \
-    $(WSAPI_XAVANTE_SUBDIR)/doc/us/license.md
+	$(WSAPI_XAVANTE_SUBDIR)/doc/us/license.html \
+	$(WSAPI_XAVANTE_SUBDIR)/doc/us/license.md
 
 $(eval $(luarocks-package))
