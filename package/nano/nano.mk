c1cbd352ee66141635aefd619fa980f3cd2a8ac5
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2019-01-01 15:51:39 +0100

    package/nano: bump to version 3.2
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/nano/nano.mk
+++ package/nano/nano.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 NANO_VERSION_MAJOR = 3
-NANO_VERSION = $(NANO_VERSION_MAJOR).0
+NANO_VERSION = $(NANO_VERSION_MAJOR).2
 NANO_SITE = https://www.nano-editor.org/dist/v$(NANO_VERSION_MAJOR)
 NANO_SOURCE = nano-$(NANO_VERSION).tar.xz
 NANO_LICENSE = GPL-3.0+
