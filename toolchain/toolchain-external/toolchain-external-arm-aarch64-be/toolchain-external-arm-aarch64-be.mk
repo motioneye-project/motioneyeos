216aefa20562e13995274b1a7283976c22a6bbd0
Romain Naour <romain.naour@gmail.com>
2019-01-02 23:14:06 +0100

    toolchain-external: update Arm AArch64-BE toolchain 8.2-2018.11
    >From [1]:
    * All GCC 8.2 features. For details on GCC 8 release series.
    * Linaro specific pre-processor macros to ensure that this is a
      continuation from the Linaro releases.
    * Spectre v1 mitigation backport from upstream FSF trunk include the
      revisions. This is an initial backport of those mitigations in
      the GNU toolchain and should be regarded as support for prototyping
      and early access only. Moreover, while the backports include support
      for the other architectures, they are included for completeness and
      all issues regarding these patches must be taken up upstream in the
      https://gcc.gnu.org/bugzilla by reproducing the same with upstream
      FSF trunk.
      Arm is interested in feedback regarding these workarounds for
      Spectre v1.
      A description of the mitigation has been published on LWN.net.
    
    See "Release Note":
    [1] https://developer.arm.com/open-source/gnu-toolchain/gnu-a/downloads#
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- toolchain/toolchain-external/toolchain-external-arm-aarch64-be/toolchain-external-arm-aarch64-be.mk
+++ toolchain/toolchain-external/toolchain-external-arm-aarch64-be/toolchain-external-arm-aarch64-be.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TOOLCHAIN_EXTERNAL_ARM_AARCH64_BE_VERSION = 2018.08
+TOOLCHAIN_EXTERNAL_ARM_AARCH64_BE_VERSION = 2018.11
 TOOLCHAIN_EXTERNAL_ARM_AARCH64_BE_SITE = https://developer.arm.com/-/media/Files/downloads/gnu-a/8.2-$(TOOLCHAIN_EXTERNAL_ARM_AARCH64_BE_VERSION)
 
 TOOLCHAIN_EXTERNAL_ARM_AARCH64_BE_SOURCE = gcc-arm-8.2-$(TOOLCHAIN_EXTERNAL_ARM_AARCH64_BE_VERSION)-x86_64-aarch64_be-linux-gnu.tar.xz
