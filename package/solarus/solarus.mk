3acbb51e6b7835d524274fdb791ba3f3f0219500
Romain Naour <romain.naour@gmail.com>
2019-01-03 00:10:25 +0100

    package/solarus: update license.txt file hash and remove license_gpl.txt hash.
    The license.txt file has been updated with Solarus Free Resource Pack URL.
    
    Before version 1.6.0, the archive was downloaded from github and license_gpl.txt
    file which is present in the git repository.
    Since version 1.6.0, the archive is downloaded from [1] but the license_gpl.txt
    file is missing.
    
    This has been reported upstream [2].
    
    [1] http://www.solarus-games.org
    [2] https://gitlab.com/solarus-games/solarus/issues/1328
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/solarus/solarus.mk
+++ package/solarus/solarus.mk
@@ -10,7 +10,7 @@ SOLARUS_SOURCE = solarus-$(SOLARUS_VERSION)-src.tar.gz
 
 SOLARUS_LICENSE = GPL-3.0 (code), CC-BY-SA-4.0 (Solarus logos and icons), \
 	CC-BY-SA-3.0 (GUI icons)
-SOLARUS_LICENSE_FILES = license.txt license_gpl.txt
+SOLARUS_LICENSE_FILES = license.txt
 
 # Install libsolarus.so
 SOLARUS_INSTALL_STAGING = YES
