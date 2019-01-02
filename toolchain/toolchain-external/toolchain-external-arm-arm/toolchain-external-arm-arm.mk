e0d02a4679e65f1614c97f5ba9c7b393e8f6afb9
Romain Naour <romain.naour@gmail.com>
2019-01-02 23:14:05 +0100

    toolchain-external: update Arm ARM toolchain 8.2-2018.11
    >From [1]:
    * All GCC 8.2 features. For details on GCC 8 release series.
    * Linaro specific pre-processor macros to ensure that this is a
      continuation from the Linaro releases.
    * Spectre v1 mitigation backport from upstream FSF trunk include the
      revisions. This is an initial backport of those mitigations in
      the GNU toolchain and should be regarded as support for prototyping
      and early access only. Moreover, while the backports include support
      for the other architectures, they are included for completeness and
      all issues regarding these patches must be taken up upstream in the
      https://gcc.gnu.org/bugzilla by reproducing the same with upstream
      FSF trunk.
      Arm is interested in feedback regarding these workarounds for
      Spectre v1.
      A description of the mitigation has been published on LWN.net.
    
    See "Release Note":
    [1] https://developer.arm.com/open-source/gnu-toolchain/gnu-a/downloads#
    
    Tested with qemu_arm_vexpress_defconfig.
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- toolchain/toolchain-external/toolchain-external-arm-arm/toolchain-external-arm-arm.mk
+++ toolchain/toolchain-external/toolchain-external-arm-arm/toolchain-external-arm-arm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION = 2018.08
+TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION = 2018.11
 TOOLCHAIN_EXTERNAL_ARM_ARM_SITE = https://developer.arm.com/-/media/Files/downloads/gnu-a/8.2-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)
 
 TOOLCHAIN_EXTERNAL_ARM_ARM_SOURCE = gcc-arm-8.2-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)-x86_64-arm-linux-gnueabihf.tar.xz
