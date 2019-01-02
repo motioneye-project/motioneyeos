78bee8917cf10c21114f46d27cdb7d9efbb4e221
Joo Aun Saw <jasaw@dius.com.au>
2019-01-02 17:14:33 +1100

    gst1: install RTSP test launch program

--- package/gstreamer1/gst1-rtsp-server/gst1-rtsp-server.mk
+++ package/gstreamer1/gst1-rtsp-server/gst1-rtsp-server.mk
@@ -24,4 +24,10 @@ ifeq ($(BR2_PACKAGE_GST1_PLUGINS_BAD),y)
 GST1_RTSP_SERVER_DEPENDENCIES += gst1-plugins-bad
 endif
 
+define GST1_RTSP_SERVER_INSTALL_EXAMPLES
+	$(INSTALL) -D -m 0755 $(@D)/examples/test-launch $(TARGET_DIR)/usr/sbin
+endef
+
+GST1_RTSP_SERVER_POST_INSTALL_TARGET_HOOKS += GST1_RTSP_SERVER_INSTALL_EXAMPLES
+
 $(eval $(autotools-package))
