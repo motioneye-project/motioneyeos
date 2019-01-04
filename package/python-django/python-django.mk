86d0ecf07615ff47a11ca6aa4d6aba908d6f359a
Asaf Kahlon <asafka7@gmail.com>
2019-01-04 16:11:08 +0200

    package/python-django: security bump to version 2.1.5
    Fixes CVE-2019-3498: Content spoofing possibility in the default 404 page
    
    For more details, see the announcement:
    https://www.djangoproject.com/weblog/2019/jan/04/security-releases/
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    [Peter: mention that bump fixes security issues]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 2.1.4
+PYTHON_DJANGO_VERSION = 2.1.5
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/83/f7/4939b60c4127d5f49ccb570e34f4c59ecc222949220234a88e4f363f1456
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/5c/7f/4c750e09b246621e5e90fa08f93dec1b991f5c203b0ff615d62a891c8f41
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_SETUP_TYPE = setuptools
