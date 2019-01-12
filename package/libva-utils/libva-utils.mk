b0907949261ef5e8179c027c086903f30417dd66
James Hilliard <james.hilliard1@gmail.com>
2019-01-12 21:22:17 +0800

    package/libva-utils: bump to version 2.3.0
    Have to add a workaround since upstream didn't package this release
    properly.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libva-utils/libva-utils.mk
+++ package/libva-utils/libva-utils.mk
@@ -4,13 +4,19 @@
 #
 ################################################################################
 
-LIBVA_UTILS_VERSION = 2.2.0
-LIBVA_UTILS_SOURCE = libva-utils-$(LIBVA_UTILS_VERSION).tar.bz2
-LIBVA_UTILS_SITE = https://github.com/intel/libva-utils/releases/download/$(LIBVA_UTILS_VERSION)
+LIBVA_UTILS_VERSION = 2.3.0
+LIBVA_UTILS_SOURCE = libva-utils-$(LIBVA_UTILS_VERSION).tar.gz
+LIBVA_UTILS_SITE = https://github.com/intel/libva-utils/archive/$(LIBVA_UTILS_VERSION)
 LIBVA_UTILS_LICENSE = MIT
 LIBVA_UTILS_LICENSE_FILES = COPYING
 # 0001-check-ssp.patch
 LIBVA_UTILS_AUTORECONF = YES
 LIBVA_UTILS_DEPENDENCIES = host-pkgconf libva
+
+define LIBVA_UTILS_CREATE_M4_DIR
+	@mkdir -p $(@D)/m4
+endef
+
+LIBVA_UTILS_POST_EXTRACT_HOOKS += LIBVA_UTILS_CREATE_M4_DIR
 
 $(eval $(autotools-package))
