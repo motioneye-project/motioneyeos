582ceee3f1ed550125b51b8d2ebb242caa09ac68
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2019-01-15 10:32:29 +0100

    package/rtc-tools: new package
    This package contains a set of tools to manipulate RTC devices. They
    are written and maintained by the RTC subsystem Linux kernel
    maintainer, Alexandre Belloni.
    
    Cc: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Reviewed-by: Matthew Weber <matthew.weber@rockwellcollins.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ package/rtc-tools/rtc-tools.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# rtc-tools
+#
+################################################################################
+
+RTC_TOOLS_VERSION = 33ef4aa1c92b0c92a351284d93d1ac5570de9cc7
+RTC_TOOLS_SITE = git://git.kernel.org/pub/scm/linux/kernel/git/abelloni/rtc-tools.git
+RTC_TOOLS_LICENSE = GPL-2.0
+RTC_TOOLS_LICENSE_FILES = COPYING
+
+RTC_TOOLS_BINARIES = rtc rtc-sync rtc-range
+
+define RTC_TOOLS_BUILD_CMDS
+	$(foreach bin,$(RTC_TOOLS_BINARIES),\
+		$(TARGET_CC) $(TARGET_CFLAGS) -o $(@D)/$(bin) $(@D)/$(bin).c
+	)
+endef
+
+define RTC_TOOLS_INSTALL_TARGET_CMDS
+	$(foreach bin,$(RTC_TOOLS_BINARIES),\
+		$(INSTALL) -D -m 0755 $(@D)/$(bin) $(TARGET_DIR)/usr/bin/$(bin)
+	)
+endef
+
+$(eval $(generic-package))
