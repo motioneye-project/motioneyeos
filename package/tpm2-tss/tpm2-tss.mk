d38bcb9de572ec285bfb67f5a806a55e98a44e13
Peter Korsgaard <peter@korsgaard.com>
2019-01-15 11:15:16 +0100

    tpm2-tss: do not enforce -fstack-protector-all
    Stack protection is now controlled buildroot wide with the BR2_SSP_*
    options, so disable the explicit -fstack-protector-all so the SSP logic in
    the toolchain wrapper is used instead.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tpm2-tss/tpm2-tss.mk
+++ package/tpm2-tss/tpm2-tss.mk
@@ -12,8 +12,9 @@ TPM2_TSS_INSTALL_STAGING = YES
 TPM2_TSS_DEPENDENCIES = liburiparser openssl host-pkgconf
 TPM2_TSS_CONF_OPTS = --with-crypto=ossl --disable-doxygen-doc
 
-# configure.ac doesn't contain a link test, so it doesn't detect when
-# libssp is missing.
-TPM2_TSS_CONF_ENV = ax_cv_check_cflags___________Wall__Werror_______fstack_protector_all=$(if $(BR2_TOOLCHAIN_HAS_SSP),yes,no)
+# -fstack-protector-all is used by default. Disable that so the
+# BR2_SSP_* options in the toolchain wrapper are used instead
+TPM2_TSS_CONF_ENV = \
+	ax_cv_check_cflags___________Wall__Werror_______fstack_protector_all=no
 
 $(eval $(autotools-package))
