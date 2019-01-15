223c4fb704aa187991eb082141536e52499a5428
Peter Korsgaard <peter@korsgaard.com>
2019-01-15 11:15:17 +0100

    tpm2-tss: fix build with BR2_FORTIFY_SOURCE_1
    The configure script passes -U FORTIFY_SOURCE -D FORTIFY_SOURCE=2 by
    default, which conflicts with BR2_FORTIFY_SOURCE_1 as -Werror is used:
    
    <cross>-gcc ..  -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2 .. -D_FORTIFY_SOURCE=1
    <command-line>:0:0: error: "_FORTIFY_SOURCE" redefined [-Werror]
    
    Disable this so the FORTIFY_SOURCE flags in TARGET_CFLAGS (if any) is used
    instead.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tpm2-tss/tpm2-tss.mk
+++ package/tpm2-tss/tpm2-tss.mk
@@ -12,9 +12,12 @@ TPM2_TSS_INSTALL_STAGING = YES
 TPM2_TSS_DEPENDENCIES = liburiparser openssl host-pkgconf
 TPM2_TSS_CONF_OPTS = --with-crypto=ossl --disable-doxygen-doc
 
-# -fstack-protector-all is used by default. Disable that so the
-# BR2_SSP_* options in the toolchain wrapper are used instead
+# -fstack-protector-all and FORTIFY_SOURCE=2 is used by
+# default. Disable that so the BR2_SSP_* / BR2_FORTIFY_SOURCE_* options
+# in the toolchain wrapper and CFLAGS are used instead
 TPM2_TSS_CONF_ENV = \
-	ax_cv_check_cflags___________Wall__Werror_______fstack_protector_all=no
+	ax_cv_check_cflags___________Wall__Werror_______fstack_protector_all=no \
+	ax_cv_check_ccppflags___________Wall__Werror_______U_FORTIFY_SOURCE=no \
+	ax_cv_check_ccppflags___________Wall__Werror_______D_FORTIFY_SOURCE_2=no
 
 $(eval $(autotools-package))
