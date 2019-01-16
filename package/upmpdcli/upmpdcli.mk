8b4faf86c3691be90cc1f1a79005d855d1071624
Jörg Krause <joerg.krause@embedded.rocks>
2019-01-16 22:02:29 +0100

    package/upmpdcli: bump to version 1.4.0
    upmpdcli switched license from GPL-2.0+ to LGPL-2.1+, therefore update
    the hash file for the license file "COPYING".
    
    Note, that upmpdcli depends on libupnpp 0.17.0.
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/upmpdcli/upmpdcli.mk
+++ package/upmpdcli/upmpdcli.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-UPMPDCLI_VERSION = 1.2.16
+UPMPDCLI_VERSION = 1.4.0
 UPMPDCLI_SITE = http://www.lesbonscomptes.com/upmpdcli/downloads
-UPMPDCLI_LICENSE = GPL-2.0+
+UPMPDCLI_LICENSE = LGPL-2.1+
 UPMPDCLI_LICENSE_FILES = COPYING
 UPMPDCLI_DEPENDENCIES = host-pkgconf libmpdclient libupnpp libmicrohttpd jsoncpp
 
