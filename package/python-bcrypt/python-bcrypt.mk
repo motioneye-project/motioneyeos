f897d368c256b0ff43321b65021004ccb513542b
Asaf Kahlon <asafka7@gmail.com>
2019-01-19 10:44:52 +0200

    python-bcrypt: bump to version 3.1.6
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-bcrypt/python-bcrypt.mk
+++ package/python-bcrypt/python-bcrypt.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_BCRYPT_VERSION = 3.1.5
+PYTHON_BCRYPT_VERSION = 3.1.6
 PYTHON_BCRYPT_SOURCE = bcrypt-$(PYTHON_BCRYPT_VERSION).tar.gz
-PYTHON_BCRYPT_SITE = https://files.pythonhosted.org/packages/91/a5/fd19eac0252e56b4ce65ced937ae40024782c21108da7d830003b7f76cdb
+PYTHON_BCRYPT_SITE = https://files.pythonhosted.org/packages/ce/3a/3d540b9f5ee8d92ce757eebacf167b9deedb8e30aedec69a2a072b2399bb
 PYTHON_BCRYPT_SETUP_TYPE = setuptools
 PYTHON_BCRYPT_LICENSE = Apache-2.0
 PYTHON_BCRYPT_LICENSE_FILES = LICENSE
