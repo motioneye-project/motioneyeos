2a3e77a7026b5a354268ac490fc5ad5a05cd1406
Calin Crisan <ccrisan@gmail.com>
2019-01-20 23:01:23 +0200

    Add python-colibris package

---
+++ package/python-colibris/python-colibris.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-colibris
+#
+################################################################################
+
+PYTHON_COLIBRIS_VERSION = 0.4.2
+PYTHON_COLIBRIS_SOURCE = aiohttp-$(PYTHON_COLIBRIS_VERSION).tar.gz
+PYTHON_COLIBRIS_SITE = https://files.pythonhosted.org/packages/05/88/2fcf6ddd6c9591147b14f691b9bf4007cc909a0f2f2e9fa8af5150a8e59d
+PYTHON_COLIBRIS_SETUP_TYPE = setuptools
+PYTHON_COLIBRIS_LICENSE = Apache-2.0
+PYTHON_COLIBRIS_LICENSE_FILES = LICENSE.txt
+
+$(eval $(python-package))
