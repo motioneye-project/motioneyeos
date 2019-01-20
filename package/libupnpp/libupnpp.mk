6bf54343cdeee1d452d3bf94f7ff18430feddb40
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-01-20 10:47:50 +0100

    libupnpp: fix libupnp dependency
    Commit 9b551dacf74445d58aaefef349e2817e009c9d5d added support for
    libupnp18 but without updating LIBUPNPP_DEPENDENCIES
    
    Fixes:
     - http://autobuild.buildroot.org/results/aa734318b9ad318d25e772585c8794429cc0f489
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libupnpp/libupnpp.mk
+++ package/libupnpp/libupnpp.mk
@@ -9,7 +9,8 @@ LIBUPNPP_SITE = http://www.lesbonscomptes.com/upmpdcli/downloads
 LIBUPNPP_LICENSE = LGPL-2.1+
 LIBUPNPP_LICENSE_FILES = COPYING
 LIBUPNPP_INSTALL_STAGING = YES
-LIBUPNPP_DEPENDENCIES = expat libcurl libupnp
+LIBUPNPP_DEPENDENCIES = expat libcurl \
+	$(if $(BR2_PACKAGE_LIBUPNP),libupnp,libupnp18)
 # We're patching configure.ac
 LIBUPNPP_AUTORECONF = YES
 
