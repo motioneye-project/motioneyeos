424b1511c80cb513a36b6a096c6c66bc7c1c17a0
Calin Crisan <ccrisan@gmail.com>
2019-01-21 23:21:28 +0200

    Add python-mock package

---
+++ package/python-mock/python-mock.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-mock
+#
+################################################################################
+
+PYTHON_MOCK_VERSION = 2.0.0
+PYTHON_MOCK_SOURCE = mock-$(PYTHON_MOCK_VERSION).tar.gz
+PYTHON_MOCK_SITE = https://files.pythonhosted.org/packages/0c/53/014354fc93c591ccc4abff12c473ad565a2eb24dcd82490fae33dbf2539f
+PYTHON_MOCK_SETUP_TYPE = setuptools
+PYTHON_MOCK_LICENSE = Apache-2.0
+PYTHON_MOCK_LICENSE_FILES = LICENSE.txt
+
+$(eval $(python-package))
