9d2ac7f68a224d77f739afcd9f649070024fc190
Calin Crisan <ccrisan@gmail.com>
2019-01-21 16:29:24 +0200

    python-pyjwt: fix version

--- package/python-pyjwt/python-pyjwt.mk
+++ package/python-pyjwt/python-pyjwt.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PYJWT_VERSION = 1.7.2
+PYTHON_PYJWT_VERSION = 1.7.1
 PYTHON_PYJWT_SOURCE = PyJWT-$(PYTHON_PYJWT_VERSION).tar.gz
 PYTHON_PYJWT_SITE = https://files.pythonhosted.org/packages/2f/38/ff37a24c0243c5f45f5798bd120c0f873eeed073994133c084e1cf13b95c
 PYTHON_PYJWT_SETUP_TYPE = setuptools
