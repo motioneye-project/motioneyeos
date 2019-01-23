73dbb6e1287b36474623219de9b83b2a5fdcee3d
Calin Crisan <ccrisan@gmail.com>
2019-01-23 22:04:53 +0200

    python-idna-ssl: fix package version

--- package/python-idna-ssl/python-idna-ssl.mk
+++ package/python-idna-ssl/python-idna-ssl.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 PYTHON_IDNA_SSL_VERSION = 1.1.0
-PYTHON_IDNA_SSL_SOURCE = idna-ssl-$(PYTHON_IDNA_VERSION).tar.gz
+PYTHON_IDNA_SSL_SOURCE = idna-ssl-$(PYTHON_IDNA_SSL_VERSION).tar.gz
 PYTHON_IDNA_SSL_SITE = https://files.pythonhosted.org/packages/46/03/07c4894aae38b0de52b52586b24bf189bb83e4ddabfe2e2c8f2419eec6f4
 PYTHON_IDNA_SSL_LICENSE =  BSD-3-Clause
 PYTHON_IDNA_SSL_LICENSE_FILES = LICENSE.rst
