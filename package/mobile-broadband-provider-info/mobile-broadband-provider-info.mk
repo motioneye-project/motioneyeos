17ae27c7404c1361c9f979e8acd6dea31fddd4df
Nicolas Serafini <nicolas.serafini@sensefly.com>
2019-01-24 10:52:46 +0000

    package/mobile-broadband-provider-info: bump to version 20190116
    Signed-off-by: Nicolas Serafini <nicolas.serafini@sensefly.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mobile-broadband-provider-info/mobile-broadband-provider-info.mk
+++ package/mobile-broadband-provider-info/mobile-broadband-provider-info.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MOBILE_BROADBAND_PROVIDER_INFO_VERSION = 20151214
+MOBILE_BROADBAND_PROVIDER_INFO_VERSION = 20190116
 MOBILE_BROADBAND_PROVIDER_INFO_SITE = http://ftp.gnome.org/pub/GNOME/sources/mobile-broadband-provider-info/$(MOBILE_BROADBAND_PROVIDER_INFO_VERSION)
 MOBILE_BROADBAND_PROVIDER_INFO_SOURCE = mobile-broadband-provider-info-$(MOBILE_BROADBAND_PROVIDER_INFO_VERSION).tar.xz
 MOBILE_BROADBAND_PROVIDER_INFO_LICENSE = Public domain
