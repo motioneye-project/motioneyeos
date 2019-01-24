bcc66605f09b551c9f101997ab7ed5285cd29f32
Christian Stewart <christian@paral.in>
2019-01-24 00:22:05 -0800

    rpi-firmware: bump version to 81cca1a93
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = e1bd9b0b8cda901ee9b23cbb8b3334cde71320a7
+RPI_FIRMWARE_VERSION = 81cca1a9380c828299e884dba5efd0d4acb39e8d
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
