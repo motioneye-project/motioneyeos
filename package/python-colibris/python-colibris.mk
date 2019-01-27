abd5ee8eb32131fd811e5c78e786ae9338443f2f
Calin Crisan <ccrisan@gmail.com>
2019-01-27 22:52:04 +0200

    python-colibris: update to 0.4.3

--- package/python-colibris/python-colibris.mk
+++ package/python-colibris/python-colibris.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_COLIBRIS_VERSION = 0.4.2
+PYTHON_COLIBRIS_VERSION = 0.4.3
 PYTHON_COLIBRIS_SOURCE = colibris-$(PYTHON_COLIBRIS_VERSION).tar.gz
-PYTHON_COLIBRIS_SITE = https://files.pythonhosted.org/packages/05/88/2fcf6ddd6c9591147b14f691b9bf4007cc909a0f2f2e9fa8af5150a8e59d
+PYTHON_COLIBRIS_SITE = https://files.pythonhosted.org/packages/b7/ff/df9dd2a27f33f1d6003037f0597c4652dca50525f67f44f182ecc49c2baf
 PYTHON_COLIBRIS_SETUP_TYPE = setuptools
 PYTHON_COLIBRIS_LICENSE = Apache-2.0
 PYTHON_COLIBRIS_LICENSE_FILES = LICENSE.txt
