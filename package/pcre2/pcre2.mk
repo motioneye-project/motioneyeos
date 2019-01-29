3449c00d2b129d2509418ba9b89a234948e7985a
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-01-29 21:02:11 +0100

    pcre2: bump to version 10.32
    - Remove patch (already in version)
    - Update hash of license file:
      https://vcs.pcre.org/pcre2/code/trunk/LICENCE?r1=1001&r2=910
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/pcre2/pcre2.mk
+++ package/pcre2/pcre2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PCRE2_VERSION = 10.31
+PCRE2_VERSION = 10.32
 PCRE2_SITE = https://ftp.pcre.org/pub/pcre
 PCRE2_SOURCE = pcre2-$(PCRE2_VERSION).tar.bz2
 PCRE2_LICENSE = BSD-3-Clause
