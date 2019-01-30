59cc32513383f0c9af4e9cb634762cd56e985725
Etienne Carriere <etienne.carriere@linaro.org>
2019-01-30 11:47:24 +0100

    package/optee-client: new package
    OP-TEE client API library and supplicant daemon from the
    OP-TEE project are packaged in package/optee-client. An init script
    launches the tee-supplicant deamon. Package is added to the
    Security menu of BR configuration.
    
    This change references in Buildroot the today's latest OP-TEE
    revision release tagged 3.4.0.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    [Thomas:
     - remove version selection
     - add dependency on !BR2_STATIC_LIBS, as it unconditionally builds a
       shared library]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/optee-client/optee-client.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# optee-client
+#
+################################################################################
+
+OPTEE_CLIENT_VERSION = 3.4.0
+OPTEE_CLIENT_SITE = $(call github,OP-TEE,optee_client,$(OPTEE_CLIENT_VERSION))
+OPTEE_CLIENT_LICENSE = BSD-2-Clause
+OPTEE_CLIENT_LICENSE_FILES = LICENSE
+OPTEE_CLIENT_INSTALL_STAGING = YES
+
+define OPTEE_CLIENT_INSTALL_INIT_SYSV
+	$(INSTALL) -m 0755 -D $(OPTEE_CLIENT_PKGDIR)/S30optee \
+		$(TARGET_DIR)/etc/init.d/S30optee
+endef
+
+$(eval $(cmake-package))
