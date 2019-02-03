68c95c6ea5c6366bc977c382fadf45c8a2f433a2
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-02-03 17:17:40 +0100

    package/libpcap: bump to version 1.9.0
    - Disable Endace DAG card support as libdag is not available in
      buildroot
    - Add upstream patch to fix build on musl
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Reviewed-by: Titouan Christophe <titouan.christophe@railnova.eu>
    Tested-by: Yann E . MORIN <yann.morin.1998@free.fr>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libpcap/libpcap.mk
+++ package/libpcap/libpcap.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBPCAP_VERSION = 1.8.1
+LIBPCAP_VERSION = 1.9.0
 LIBPCAP_SITE = http://www.tcpdump.org/release
 LIBPCAP_LICENSE = BSD-3-Clause
 LIBPCAP_LICENSE_FILES = LICENSE
@@ -15,7 +15,7 @@ LIBPCAP_CONF_ENV = \
 	ac_cv_header_linux_wireless_h=yes \
 	CFLAGS="$(LIBPCAP_CFLAGS)"
 LIBPCAP_CFLAGS = $(TARGET_CFLAGS)
-LIBPCAP_CONF_OPTS = --disable-yydebug --with-pcap=linux
+LIBPCAP_CONF_OPTS = --disable-yydebug --with-pcap=linux --without-dag
 LIBPCAP_CONFIG_SCRIPTS = pcap-config
 
 # Omit -rpath from pcap-config output
