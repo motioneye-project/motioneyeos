73f0c6c899361caf489adf796697f2534bcbc93b
Adrien Gallouët <adrien@gallouet.fr>
2019-02-04 15:22:51 +0000

    package/bird: bump version to 2.0.3
    Patch 0001 is no longer needed as it has been ported to this version.
    Also, the README changes but the licence is still GPL-2.0+
    
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/bird/bird.mk
+++ package/bird/bird.mk
@@ -4,12 +4,10 @@
 #
 ################################################################################
 
-BIRD_VERSION = v2.0.2
-BIRD_SITE = $(call github,BIRD,bird,$(BIRD_VERSION))
+BIRD_VERSION = 2.0.3
+BIRD_SITE = ftp://bird.network.cz/pub/bird
 BIRD_LICENSE = GPL-2.0+
 BIRD_LICENSE_FILES = README
-# autoreconf is needed since the package is fetched from github
-BIRD_AUTORECONF = YES
 BIRD_DEPENDENCIES = host-flex host-bison
 
 ifeq ($(BR2_PACKAGE_BIRD_CLIENT),y)
