d091265ee7cc68156c383d03b252926117360952
Louis-Paul Cordier <lpdev@cordier.org>
2019-02-05 19:19:43 +0100

    package/intel-mediadriver: new package
    Signed-off-by: Louis-Paul Cordier <lpdev@cordier.org>
    [Thomas:
     - add missing Config.in comments
     - fix minor nit in the _LICENSE variable
     - add patch to drop hardening options, especially -fstack-protector,
       which was causing the build to fail on toolchains without SSP
       support]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/intel-mediadriver/intel-mediadriver.mk
@@ -0,0 +1,31 @@
+################################################################################
+#
+# intel-mediadriver
+#
+################################################################################
+
+# based on https://software.intel.com/en-us/articles/build-and-debug-open-source-media-stack
+
+INTEL_MEDIADRIVER_VERSION = 18.4.0
+INTEL_MEDIADRIVER_SITE = http://github.com/intel/media-driver/archive
+INTEL_MEDIADRIVER_SOURCE= intel-media-$(INTEL_MEDIADRIVER_VERSION).tar.gz
+INTEL_MEDIADRIVER_LICENSE = MIT, BSD-3-Clause
+INTEL_MEDIADRIVER_LICENSE_FILES = LICENSE.md
+
+INTEL_MEDIADRIVER_DEPENDENCIES = \
+	intel-gmmlib \
+	libpciaccess \
+	libva \
+	mesa3d \
+	xlib_libX11
+
+INTEL_MEDIADRIVER_SUPPORTS_IN_SOURCE_BUILD = NO
+
+INTEL_MEDIADRIVER_CONF_OPTS = \
+	-DMEDIA_VERSION="2.0.0" \
+	-DBUILD_ALONG_WITH_CMRTLIB=1 \
+	-DINSTALL_DRIVERS_SYSCONF=OFF \
+	-DMEDIA_RUN_TEST_SUITE=OFF \
+	-DRUN_TEST_SUITE=OFF
+
+$(eval $(cmake-package))
