33f1bd1f025e0d8f03017da75b04e57f8de3dc6e
Angelo Compagnucci <angelo@amarulasolutions.com>
2019-02-06 14:23:59 +0100

    package/mender-artifact: new package
    mender-artifact is a host tool to generate update images
    in the Mender artifact file format.
    This package uses the binary archive from github because it bundles the
    external dependencies.
    
    Example usage:
    
        $ mender-artifact write rootfs-image \
             --update rootfs.ext4 \
             --output-path rootfs.mender \
             --artifact-name "release-v1.0.0" \
             --device-type "beaglebone"
    
    Above will generate a Mender artifact called "rootfs.mender"
    containing the "rootfs.ext4" image along with meta-data.
    
    One can read-out the meta-data with the following command:
    
        $ mender-artifact read rootfs.mender
          Mender artifact:
          Name: release-v1.0.0
          Format: mender
          Version: 2
          Signature: no signature
          Compatible devices: '[beaglebone]'
    
        Updates:
          0000:
            Type:   rootfs-image
            Files:
              name:     rootfs.ext4
              size:     52428800
              modified: 2018-08-27 09:10:55 +0200 CEST
              checksum: e70b113fb0964a810a3043586eb4fc1c48e684ba78b02ba65fead4aa3e540d87
    
    Signed-off-by: Angelo Compagnucci <angelo@amarulasolutions.com>
    Tested-by: Adam Duskett <aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/mender-artifact/mender-artifact.mk
@@ -0,0 +1,32 @@
+################################################################################
+#
+# host-mender-artifact
+#
+################################################################################
+
+HOST_MENDER_ARTIFACT_VERSION = 2.4.0
+HOST_MENDER_ARTIFACT_SITE = https://github.com/mendersoftware/mender-artifact/archive
+HOST_MENDER_ARTIFACT_SOURCE = $(HOST_MENDER_ARTIFACT_VERSION).tar.gz
+HOST_MENDER_ARTIFACT_LICENSE = Apache2.0, BSD-2-Clause, BSD-3-Clause, ISC, MIT
+HOST_MENDER_ARTIFACT_LICENSE_FILES = \
+	LICENSE \
+	LIC_FILES_CHKSUM.sha256 \
+	vendor/github.com/mendersoftware/mendertesting/LICENSE \
+	vendor/github.com/pkg/errors/LICENSE \
+	vendor/github.com/pmezard/go-difflib/LICENSE \
+	vendor/golang.org/x/sys/LICENSE \
+	vendor/golang.org/x/crypto/LICENSE \
+	vendor/github.com/davecgh/go-spew/LICENSE \
+	vendor/github.com/stretchr/testify/LICENSE \
+	vendor/github.com/stretchr/testify/LICENSE \
+	vendor/github.com/urfave/cli/LICENSE \
+	vendor/github.com/sirupsen/logrus/LICENSE
+
+HOST_MENDER_ARTIFACT_LDFLAGS = -X main.Version=$(HOST_MENDER_ARTIFACT_VERSION)
+
+HOST_MENDER_ARTIFACT_BUILD_TARGETS = cli/mender-artifact
+
+HOST_MENDER_ARTIFACT_BIN_NAME = mender-artifact
+HOST_MENDER_ARTIFACT_INSTALL_BINS = $(HOST_MENDER_ARTIFACT_BIN_NAME)
+
+$(eval $(host-golang-package))
