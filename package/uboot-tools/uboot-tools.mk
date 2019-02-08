6ff3da375bf951ed458b502058cb8beb5b9b994e
Jörg Krause <joerg.krause@embedded.rocks>
2019-02-08 22:31:19 +0100

    package/uboot-tools: bump to version 2019.01
    Adapt patch 0002 and 0003 for version 2019.01.
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/uboot-tools/uboot-tools.mk
+++ package/uboot-tools/uboot-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UBOOT_TOOLS_VERSION = 2018.07
+UBOOT_TOOLS_VERSION = 2019.01
 UBOOT_TOOLS_SOURCE = u-boot-$(UBOOT_TOOLS_VERSION).tar.bz2
 UBOOT_TOOLS_SITE = ftp://ftp.denx.de/pub/u-boot
 UBOOT_TOOLS_LICENSE = GPL-2.0+
