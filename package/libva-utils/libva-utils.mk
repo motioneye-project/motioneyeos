22cb6ccc2dd7bdda3d96bcacf16abe52b4aae32f
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-02-10 11:03:13 +0100

    package/libva-utils: bump version to 2.4.0
    Removed patch 0002, applied upstream.
    Follow upstream switch of release tarball to bz2 and new location.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libva-utils/libva-utils.mk
+++ package/libva-utils/libva-utils.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-LIBVA_UTILS_VERSION = 2.3.0
-LIBVA_UTILS_SITE = https://github.com/intel/libva-utils/archive/$(LIBVA_UTILS_VERSION)
+LIBVA_UTILS_VERSION = 2.4.0
+LIBVA_UTILS_SOURCE = libva-utils-$(LIBVA_UTILS_VERSION).tar.bz2
+LIBVA_UTILS_SITE = https://github.com/intel/libva-utils/releases/download/$(LIBVA_UTILS_VERSION)
 LIBVA_UTILS_LICENSE = MIT
 LIBVA_UTILS_LICENSE_FILES = COPYING
 # 0001-check-ssp.patch
