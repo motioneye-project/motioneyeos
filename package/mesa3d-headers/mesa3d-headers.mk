ac0b7333beb38e1adc5d166641d50b69ecdf2ac1
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-02-10 11:04:13 +0100

    package/{mesa3d, mesa3d-headers}: bump version to 18.3.3
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mesa3d-headers/mesa3d-headers.mk
+++ package/mesa3d-headers/mesa3d-headers.mk
@@ -12,7 +12,7 @@ endif
 
 # Not possible to directly refer to mesa3d variables, because of
 # first/second expansion trickery...
-MESA3D_HEADERS_VERSION = 18.3.2
+MESA3D_HEADERS_VERSION = 18.3.3
 MESA3D_HEADERS_SOURCE = mesa-$(MESA3D_HEADERS_VERSION).tar.xz
 MESA3D_HEADERS_SITE = https://mesa.freedesktop.org/archive
 MESA3D_HEADERS_DL_SUBDIR = mesa3d
