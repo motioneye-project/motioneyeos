9f1256e1aae0a1e91e033d51b968415adec117bf
Grégoire Delattre <gregoire.delattre@gmail.com>
2019-02-12 18:05:15 +0100

    board/pc: fix typo in board/pc/post-build.sh
    Signed-off-by: Grégoire Delattre <gregoire.delattre@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- board/pc/post-build.sh
+++ board/pc/post-build.sh
@@ -7,4 +7,4 @@ BOARD_DIR=$(dirname "$0")
 cp -f "$BOARD_DIR/grub-bios.cfg" "$TARGET_DIR/boot/grub/grub.cfg"
 
 # Copy grub 1st stage to binaries, required for genimage
-cp -f "$HOST_DIR/lib/grub/i387-pc/boot.img" "$BINARIES_DIR"
+cp -f "$HOST_DIR/lib/grub/i386-pc/boot.img" "$BINARIES_DIR"
