5546dfd095df4f874ff3bc46e3a2cffda3693340
Adrian Perez de Castro <aperez@igalia.com>
2019-02-12 15:09:07 +0200

    package/wpewebkit: security bump to version 2.22.4
    This is a maintenance release of the current stable WPE WebKit version,
    which contains security fixes for CVE identifiers: CVE-2019-6212,
    CVE-2019-6215, CVE-2019-6216, CVE-2019-6217, CVE-2019-6226,
    CVE-2019-6227, CVE-2019-6229, CVE-2019-6233, and CVE-2019-6234.
    Additionally, it contains a few minor fixes.
    
    Release notes can be found in the announcement:
    
      https://wpewebkit.org/release/wpe-2.22.4.html
    
    More details on the issues covered by securit fixes can be found
    in the corresponding security advisory:
    
      https://wpewebkit.org/security/WSA-2019-0001.html
    
    Signed-off-by: Adrian Perez de Castro <aperez@igalia.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/wpewebkit/wpewebkit.mk
+++ package/wpewebkit/wpewebkit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEWEBKIT_VERSION = 2.22.3
+WPEWEBKIT_VERSION = 2.22.4
 WPEWEBKIT_SITE = http://www.wpewebkit.org/releases
 WPEWEBKIT_SOURCE = wpewebkit-$(WPEWEBKIT_VERSION).tar.xz
 WPEWEBKIT_INSTALL_STAGING = YES
