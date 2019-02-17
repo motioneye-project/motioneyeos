d5f4b3621d46b4e8a9e1f6ab6e639dd98da560db
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-02-17 18:55:37 +0100

    package/swupdate: update license files
    COPYING contains only the license for GPL-2.0 so use the new license
    files that have been added in the Licenses directory since version
    2018.03 and
    https://github.com/sbabic/swupdate/commit/32c1f98eaca69e362be074197f84a59d994c0876
    
    Also update GPL-2.0+ to "GPL-2.0+ with OpenSSL exception" and add
    Exceptions file, see:
    https://github.com/sbabic/swupdate/commit/66d0dbe80f49eb49f8999c9d738579651fc38134
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/swupdate/swupdate.mk
+++ package/swupdate/swupdate.mk
@@ -6,8 +6,9 @@
 
 SWUPDATE_VERSION = 2018.11
 SWUPDATE_SITE = $(call github,sbabic,swupdate,$(SWUPDATE_VERSION))
-SWUPDATE_LICENSE = GPL-2.0+, LGPL-2.1+, MIT
-SWUPDATE_LICENSE_FILES = COPYING
+SWUPDATE_LICENSE = GPL-2.0+ with OpenSSL exception, LGPL-2.1+, MIT
+SWUPDATE_LICENSE_FILES = Licenses/Exceptions Licenses/gpl-2.0.txt \
+	Licenses/lgpl-2.1.txt Licenses/mit.txt
 
 # swupdate uses $CROSS-cc instead of $CROSS-gcc, which is not
 # available in all external toolchains, and use CC for linking. Ensure
