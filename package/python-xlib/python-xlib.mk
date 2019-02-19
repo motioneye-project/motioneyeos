0cf6c82fc6a3319720ae0ae7e38f7d67a72fac39
Joseph Kogut <joseph.kogut@gmail.com>
2019-02-19 11:46:37 -0800

    package/python-xlib: bump to version 0.25
    LICENSE file changed due to line ending difference, updated checksum.
    
    Signed-off-by: Joseph Kogut <joseph.kogut@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-xlib/python-xlib.mk
+++ package/python-xlib/python-xlib.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_XLIB_VERSION = 0.21
+PYTHON_XLIB_VERSION = 0.25
 PYTHON_XLIB_SOURCE = python-xlib-$(PYTHON_XLIB_VERSION).tar.bz2
-PYTHON_XLIB_SITE = https://pypi.python.org/packages/eb/de/b0eaaea7b8512dc41504db071824eef30293ff55c58d83081ebaebe85a38
+PYTHON_XLIB_SITE = https://files.pythonhosted.org/packages/3c/d9/51fc07ae57f6a44e62e2ee04bd501d763ac169ff05c838403ec7ae556992
 PYTHON_XLIB_SETUP_TYPE = setuptools
 PYTHON_XLIB_LICENSE = LGPL-2.1+
 PYTHON_XLIB_LICENSE_FILES = LICENSE
