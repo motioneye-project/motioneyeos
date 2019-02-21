58a2000be892bb4eb854e9800339fe2a0c43dbec
Sergio Prado <sergio.prado@e-labworks.com>
2019-02-21 17:27:25 -0300

    package/snort: bump to version 2.9.12
    Signed-off-by: Sergio Prado <sergio.prado@e-labworks.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/snort/snort.mk
+++ package/snort/snort.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SNORT_VERSION = 2.9.11.1
+SNORT_VERSION = 2.9.12
 SNORT_SITE = https://www.snort.org/downloads/snort
 SNORT_LICENSE = GPL-2.0
 SNORT_LICENSE_FILES = LICENSE COPYING
