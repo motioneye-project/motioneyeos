da6bcf9e86cf73ec4e405f7f6e122e6510deb026
Baruch Siach <baruch@tkos.co.il>
2019-02-25 21:54:48 +0200

    package/libssh: bump to version 0.8.7
    Drop upstream patch.
    
    Fixes strict-overflow build error with older toolchains.
    
    Fixes:
    http://autobuild.buildroot.net/results/b95b1939a55111fc6b90ef253a32402b87f9c4ab/
    http://autobuild.buildroot.net/results/281cb7b940178e4fe98940ddeed07ad546b23931/
    http://autobuild.buildroot.net/results/e95d5957c19ff01120903a66d1167279a34da2ce/
    
    Cc: Scott Fan <fancp2007@gmail.com>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libssh/libssh.mk
+++ package/libssh/libssh.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBSSH_VERSION_MAJOR = 0.8
-LIBSSH_VERSION = $(LIBSSH_VERSION_MAJOR).6
+LIBSSH_VERSION = $(LIBSSH_VERSION_MAJOR).7
 LIBSSH_SOURCE = libssh-$(LIBSSH_VERSION).tar.xz
 LIBSSH_SITE = https://www.libssh.org/files/$(LIBSSH_VERSION_MAJOR)
 LIBSSH_LICENSE = LGPL-2.1
