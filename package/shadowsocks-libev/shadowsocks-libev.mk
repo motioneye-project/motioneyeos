e675e9a0473f39b60ab66d94a61ac9c077ed438f
DUPONCHEEL Sébastien <sebastien.duponcheel@corp.ovh.com>
2019-03-01 12:30:23 +0100

    package/shadowsocks-libev: bump to version 3.2.4
    The patch can be removed, as it has been applied upstream:
    
      https://github.com/shadowsocks/shadowsocks-libev/commit/9b4aebc840f444e5aa83633838e3890cec523017
    
    And because the patch is removed, we can drop AUTORECONF = YES.
    
    Signed-off-by: DUPONCHEEL Sébastien <sebastien.duponcheel@corp.ovh.com>
    [Thomas: extend commit log]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/shadowsocks-libev/shadowsocks-libev.mk
+++ package/shadowsocks-libev/shadowsocks-libev.mk
@@ -4,14 +4,12 @@
 #
 ################################################################################
 
-SHADOWSOCKS_LIBEV_VERSION = 3.2.3
+SHADOWSOCKS_LIBEV_VERSION = 3.2.4
 SHADOWSOCKS_LIBEV_SITE = https://github.com/shadowsocks/shadowsocks-libev/releases/download/v$(SHADOWSOCKS_LIBEV_VERSION)
 SHADOWSOCKS_LIBEV_LICENSE = GPL-3.0+, BSD-2-Clause (libbloom), BSD-3-Clause (libcork, libipset)
 SHADOWSOCKS_LIBEV_LICENSE_FILES = COPYING libbloom/LICENSE libcork/COPYING
 SHADOWSOCKS_LIBEV_DEPENDENCIES = host-pkgconf c-ares libev libsodium mbedtls pcre
 SHADOWSOCKS_LIBEV_INSTALL_STAGING = YES
-# We're patching configure.ac
-SHADOWSOCKS_LIBEV_AUTORECONF = YES
 SHADOWSOCKS_LIBEV_CONF_OPTS = \
 	--with-pcre=$(STAGING_DIR)/usr \
 	--disable-ssp
