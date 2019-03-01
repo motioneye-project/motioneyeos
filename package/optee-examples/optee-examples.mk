24ac0fef4a7379475025616bc98dea6063a93dc2
Etienne Carriere <etienne.carriere@linaro.org>
2019-03-01 17:47:07 +0100

    package/optee-examples: use the OPTEE_OS_SDK variable
    The optee-os package was recently changed to provide the OPTEE_OS_SDK
    to allow other packages to locate the OP-TEE Trusted Application SDK,
    so let's use this new variable in the optee-examples package.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    [Thomas: use the new variable name, rework commit log]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/optee-examples/optee-examples.mk
+++ package/optee-examples/optee-examples.mk
@@ -11,19 +11,13 @@ OPTEE_EXAMPLES_LICENSE_FILES = LICENSE
 
 OPTEE_EXAMPLES_DEPENDENCIES = optee-client optee-os
 
-ifeq ($(BR2_aarch64),y)
-OPTEE_EXAMPLES_SDK = $(STAGING_DIR)/lib/optee/export-ta_arm64
-else ifeq ($(BR2_arm),y)
-OPTEE_EXAMPLES_SDK = $(STAGING_DIR)/lib/optee/export-ta_arm32
-endif
-
 # Trusted Application are not built from CMake due to ta_dev_kit dependencies.
 # We must build and install them on target.
 define OPTEE_EXAMPLES_BUILD_TAS
 	$(foreach f,$(wildcard $(@D)/*/ta/Makefile), \
 		$(TARGET_CONFIGURE_OPTS) \
 		$(MAKE) CROSS_COMPILE=$(TARGET_CROSS) \
-			TA_DEV_KIT_DIR=$(OPTEE_EXAMPLES_SDK) \
+			TA_DEV_KIT_DIR=$(OPTEE_OS_SDK) \
 			O=out -C $(dir $f) all
 	)
 endef
