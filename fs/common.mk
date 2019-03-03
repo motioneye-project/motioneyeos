870f37fe044b98fdd393227c746bb00ecfa7184a
Yann E. MORIN <yann.morin.1998@free.fr>
2019-03-03 11:16:35 +0100

    core: add make-based full-dependency list
    Currently, when we need to build the full dependency graph, we call make
    to show the list of packages (make show-targets), and then call it again
    and again iteratively while it returns new packages.
    
    Since calling make will parse the whole set of our Makefiles, this takes
    quite a bit of time (~4s each here), and the total can get pretty long.
    
    However, make being make, already builds the whole dependency tree
    information, so we can just ask for it.
    
    Add a new top-level rule 'show-dependency-tree' that displays the whole
    set of dependencies for all packages. For each package, its name, type
    and version is displayed, then all the direct, first-level dependencies
    are dumped. We choose a format that is not unlike the dot-graph format,
    because it is both easy to read as a human, and easy to parse as a
    machine:
    
        foo: target 1.2.3
        foo -> bar host-meh
        bar: target virtual
        bar -> buz
        buz: target 2.3.4
        buz ->
        host-meh: host virtual
        host-meh -> host-bleark
        host-bleark: host 3.4.5
        host-bleark ->
        rootfs-meh: host
        rootfs-meh -> host-bleark
    
    To be noted: rootfs are currently reported as if they were 'host'
    packages, to stay aligned with how graph-depends currently treats them.
    Ideally, graph-depends could be enhanced to recognise them separately,
    but that is another story.
    
    For just plain defconfig, which is about the smallest config we can have
    with an internal toolchain, we already have a seven-fold improvement
    (with the graph-depends rule modified to not run the pdf generation, to
    be able to just compare the tree generation):
    
        $ time make graph-depends
        real    0m27.344s
        $ time make show-dependency-tree
        real    0m3.848s
    
    >From defconfig, C++, wchar, locales, ssp, and allyespackageconfig,
    tweaked for even more packages (qt5 not qt4, luajit to avoid multi
    providers, etc...), the timings are (graph-depends still modified to
    not generate the pdf):
    
        $ time make graph-depends
        real    1m56.459s
        $ time make show-dependency-tree
        real    0m5.748s
    
    There. I don't think those numbers need any explanation whatsoever;
    they do speak on their own. OK, for maths sake, the ratio is about
    twenty-fold. So, "yeah", I guess... ;-)
    
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Cc: Thomas De Schampheleire <patrickdepinguin@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- fs/common.mk
+++ fs/common.mk
@@ -47,6 +47,10 @@ ROOTFS_COMMON_DEPENDENCIES = \
 	$(BR2_TAR_HOST_DEPENDENCY) \
 	$(if $(PACKAGES_USERS)$(ROOTFS_USERS_TABLES),host-mkpasswd)
 
+rootfs-common-show-dependency-tree: $(patsubst %,%-show-dependency-tree,$(ROOTFS_COMMON_DEPENDENCIES))
+	$(info rootfs-common: host)
+	$(info rootfs-common -> $(foreach d,$(ROOTFS_COMMON_DEPENDENCIES),$(d)))
+
 .PHONY: rootfs-common
 rootfs-common: $(ROOTFS_COMMON_DEPENDENCIES) target-finalize
 	@$(call MESSAGE,"Generating root filesystems common tables")
@@ -79,6 +83,10 @@ ROOTFS_$(2)_DIR = $$(FS_DIR)/$(1)
 ROOTFS_$(2)_TARGET_DIR = $$(ROOTFS_$(2)_DIR)/target
 
 ROOTFS_$(2)_DEPENDENCIES += rootfs-common
+
+rootfs-$(1)-show-dependency-tree: $$(patsubst %,%-show-dependency-tree,$$(ROOTFS_$(2)_DEPENDENCIES))
+	$$(info rootfs-$(1): host)
+	$$(info rootfs-$(1) -> $$(foreach d,$$(ROOTFS_$(2)_DEPENDENCIES),$$(d)))
 
 ifeq ($$(BR2_TARGET_ROOTFS_$(2)_GZIP),y)
 ROOTFS_$(2)_COMPRESS_EXT = .gz
--- package/pkg-generic.mk
+++ package/pkg-generic.mk
@@ -850,6 +850,10 @@ $(1)-show-build-order: $$(patsubst %,%-show-build-order,$$($(2)_FINAL_ALL_DEPEND
 	@:
 	$$(info $(1))
 
+$(1)-show-dependency-tree: $$(patsubst %,%-show-dependency-tree,$$($(2)_FINAL_ALL_DEPENDENCIES))
+	$$(info $(1): $(4) $$(if $$($(2)_IS_VIRTUAL),virtual,$$($(2)_DL_VERSION)))
+	$$(info $(1) -> $$(foreach d,$$($(2)_FINAL_ALL_DEPENDENCIES),$$(d)))
+
 $(1)-graph-depends: graph-depends-requirements
 	$(call pkg-graph-depends,$(1),--direct)
 
