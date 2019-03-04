b0f98d5800b288500752ef51b969d9a3cb92e897
Vadim Kochan <vadim4j@gmail.com>
2019-03-04 10:57:41 +0200

    package/netsniff-ng: allow to build with uclibc but w/o mausezahn
    mausezahn can't be build with uclibc because uclibc does not provide
    ceill() function, so it does not allow to build all netsniff-ng's
    tools with uclibc.
    
    Fix it by moving mausezahn under separate config option, which has own
    dependencies and packages selection (libnet & libcli).
    
    Signed-off-by: Vadim Kochan <vadim4j@gmail.com>
    [Thomas:
     - use the allbutmausezahn and install_allbutmausezahn targets inside
       of enumerating all the tools in the .mk file
     - stick to a "config" instead of introducing a "menuconfig"]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/netsniff-ng/netsniff-ng.mk
+++ package/netsniff-ng/netsniff-ng.mk
@@ -13,9 +13,16 @@ NETSNIFF_NG_LICENSE_FILES = README COPYING
 NETSNIFF_NG_CONF_ENV = \
 	NACL_INC_DIR=/dev/null \
 	NACL_LIB_DIR=/dev/null
-NETSNIFF_NG_DEPENDENCIES = \
-	libnl libpcap libcli libnetfilter_conntrack \
-	liburcu libnet
+NETSNIFF_NG_DEPENDENCIES = libnl libpcap libnetfilter_conntrack liburcu
+
+ifeq ($(BR2_PACKAGE_NETSNIFF_NG_MAUSEZAHN),y)
+NETSNIFF_NG_DEPENDENCIES += libcli libnet
+NETSNIFF_NG_BUILD_MAKE_TARGET = all
+NETSNIFF_NG_INSTALL_MAKE_TARGET = install
+else
+NETSNIFF_NG_BUILD_MAKE_TARGET = allbutmausezahn
+NETSNIFF_NG_INSTALL_MAKE_TARGET = install_allbutmausezahn
+endif
 
 ifeq ($(BR2_PACKAGE_GEOIP),y)
 NETSNIFF_NG_DEPENDENCIES += geoip
@@ -41,12 +48,14 @@ define NETSNIFF_NG_CONFIGURE_CMDS
 endef
 
 define NETSNIFF_NG_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) \
+		$(NETSNIFF_NG_BUILD_MAKE_TARGET)
 endef
 
 define NETSNIFF_NG_INSTALL_TARGET_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
-		PREFIX=$(TARGET_DIR)/usr ETCDIR=$(TARGET_DIR)/etc install -C $(@D)
+		PREFIX=$(TARGET_DIR)/usr ETCDIR=$(TARGET_DIR)/etc install \
+			-C $(@D) $(NETSNIFF_NG_INSTALL_MAKE_TARGET)
 endef
 
 $(eval $(generic-package))
