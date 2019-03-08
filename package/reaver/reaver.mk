c8c2954649fc55fbc41b2ed5bb74ab7c0cdcb044
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-03-08 20:59:52 +0100

    package/reaver: bump to version 1.6.5
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/reaver/reaver.mk
+++ package/reaver/reaver.mk
@@ -6,7 +6,7 @@
 
 # Older repos for this project will not cross-compile easily
 # while this one works right away
-REAVER_VERSION = 9bae55bd30b6d46b42da3a09dc23c8b0f9341996
+REAVER_VERSION = v1.6.5
 REAVER_SITE = $(call github,t6x,reaver-wps-fork-t6x,$(REAVER_VERSION))
 REAVER_LICENSE = GPL-2.0+
 REAVER_LICENSE_FILES = docs/LICENSE
