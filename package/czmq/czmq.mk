1427ec7f4ef907a1bfe5ac9346964f28e434cdde
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-03-11 18:32:57 +0100

    package/czmq: remove uneeded werror patch
    Remove patch and use --disable-Werror to disable -Werror
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/czmq/czmq.mk
+++ package/czmq/czmq.mk
@@ -7,8 +7,6 @@
 CZMQ_VERSION = 4.2.0
 CZMQ_SITE = https://github.com/zeromq/czmq/releases/download/v$(CZMQ_VERSION)
 
-# 0001-configure.ac-remove-Werror.patch touches configure.ac
-CZMQ_AUTORECONF = YES
 CZMQ_INSTALL_STAGING = YES
 CZMQ_DEPENDENCIES = zeromq host-pkgconf
 CZMQ_LICENSE = MPL-2.0
@@ -17,5 +15,7 @@ CZMQ_LICENSE_FILES = LICENSE
 # asciidoc is a python script that imports unicodedata, which is not in
 # host-python, so disable asciidoc entirely.
 CZMQ_CONF_ENV = ac_cv_prog_czmq_have_asciidoc=no
+
+CZMQ_CONF_OPTS = --disable-Werror
 
 $(eval $(autotools-package))
