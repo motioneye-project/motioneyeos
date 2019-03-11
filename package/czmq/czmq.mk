82adf608cd19787c9c57fbb8ac15b07ff5164067
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-03-11 18:32:58 +0100

    package/czmq: use --without-docs
    Use --without-docs option instead of setting
    ac_cv_prog_czmq_have_asciidoc=no
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/czmq/czmq.mk
+++ package/czmq/czmq.mk
@@ -12,10 +12,6 @@ CZMQ_DEPENDENCIES = zeromq host-pkgconf
 CZMQ_LICENSE = MPL-2.0
 CZMQ_LICENSE_FILES = LICENSE
 
-# asciidoc is a python script that imports unicodedata, which is not in
-# host-python, so disable asciidoc entirely.
-CZMQ_CONF_ENV = ac_cv_prog_czmq_have_asciidoc=no
-
-CZMQ_CONF_OPTS = --disable-Werror
+CZMQ_CONF_OPTS = --disable-Werror --without-docs
 
 $(eval $(autotools-package))
