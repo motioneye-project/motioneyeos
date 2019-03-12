a22fc3a0ebf1f0f9424b4507e0572cecf102199c
Peter Korsgaard <peter@korsgaard.com>
2019-03-12 16:32:14 +0100

    package/mosquitto: bump version to 1.5.8
    Bugfix release, fixing a number of issues discovered post-1.5.7
    
    https://mosquitto.org/blog/2019/02/version-1-5-8-released/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 24cc2eaa335a34633b71a7db7c972ab64b5e7739)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mosquitto/mosquitto.mk
+++ package/mosquitto/mosquitto.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MOSQUITTO_VERSION = 1.5.7
+MOSQUITTO_VERSION = 1.5.8
 MOSQUITTO_SITE = https://mosquitto.org/files/source
 MOSQUITTO_LICENSE = EPL-1.0 or EDLv1.0
 MOSQUITTO_LICENSE_FILES = LICENSE.txt epl-v10 edl-v10
