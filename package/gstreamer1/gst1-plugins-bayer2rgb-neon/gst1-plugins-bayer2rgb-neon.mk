a9b95112f1978f8fc628577c3f6c30496b36b354
Eloi Bail <eloi.bail@savoirfairelinux.com>
2019-03-14 16:34:35 +0100

    package/gst1-plugins-bayer2rgb-neon: new package
    gst1-plugins-bayer2rgb-neon[1] is a gstreamer1 plugin which uses
    bayer2rgb-neon library to decode raw camera bayer to RGB using NEON
    hardware acceleration.
    
    [1] https://git.phytec.de/gst-bayer2rgb-neon
    
    Signed-off-by: Eloi Bail <eloi.bail@savoirfairelinux.com>
    [Thomas:
     - add entry to DEVELOPERS file
     - add .hash file
     - adjust Config.in to match bayer2rgb-neon
     - add Config.in comment
     - drop --prefix=/usr from CONF_OPTS]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/gstreamer1/gst1-plugins-bayer2rgb-neon/gst1-plugins-bayer2rgb-neon.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# gst1-plugins-bayer2rgb-neon
+#
+################################################################################
+
+GST1_PLUGINS_BAYER2RGB_NEON_VERSION = v0.3
+GST1_PLUGINS_BAYER2RGB_NEON_SOURCE = gst-bayer2rgb-neon-$(GST1_PLUGINS_BAYER2RGB_NEON_VERSION).tar.bz2
+GST1_PLUGINS_BAYER2RGB_NEON_SITE = https://git.phytec.de/gst-bayer2rgb-neon/snapshot
+GST1_PLUGINS_BAYER2RGB_NEON_LICENSE = GPL-3.0
+GST1_PLUGINS_BAYER2RGB_NEON_LICENSE_FILES = COPYING
+
+GST1_PLUGINS_BAYER2RGB_NEON_INSTALL_STAGING = YES
+
+GST1_PLUGINS_BAYER2RGB_NEON_DEPENDENCIES = \
+	host-pkgconf \
+	gstreamer1 \
+	gst1-plugins-base \
+	bayer2rgb-neon
+
+GST1_PLUGINS_BAYER2RGB_NEON_AUTORECONF = YES
+
+define GST1_PLUGINS_BAYER2RGB_NEON_PRE_CONFIGURE_FIXUP
+	mkdir -p $(@D)/m4
+endef
+GST1_PLUGINS_BAYER2RGB_NEON_PRE_CONFIGURE_HOOKS += GST1_PLUGINS_BAYER2RGB_NEON_PRE_CONFIGURE_FIXUP
+
+$(eval $(autotools-package))
