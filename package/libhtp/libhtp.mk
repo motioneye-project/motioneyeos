380375d380f68738b6032818422ca85237828699
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-03-14 22:25:59 +0100

    package/libhtp: new package
    LibHTP is a security-aware parser for the HTTP protocol and the related
    bits and pieces.
    
    https://github.com/OISF/libhtp
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    [Thomas: always disable SSP, let our gcc/wrapper handle that]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/libhtp/libhtp.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# libhtp
+#
+################################################################################
+
+LIBHTP_VERSION = 0.5.30
+LIBHTP_SITE = $(call github,OISF,libhtp,$(LIBHTP_VERSION))
+LIBHTP_LICENSE = BSD-3-Clause
+LIBHTP_LICENSE_FILES = LICENSE
+LIBHTP_DEPENDENCIES = \
+	$(if $(BR2_PACKAGE_LIBICONV),libiconv) \
+	zlib
+LIBHTP_INSTALL_STAGING = YES
+# From git
+LIBHTP_AUTORECONF = YES
+
+# Let our gcc/wrapper handle SSP
+LIBHTP_CONF_ENV = NO_STACK_PROTECTOR=true
+
+$(eval $(autotools-package))
