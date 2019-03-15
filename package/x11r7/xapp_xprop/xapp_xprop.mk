b922c5071ea50620280a3ae1e5d3754a45bfc1f2
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-03-15 19:02:41 +0100

    package/x11r7/xapp_xprop: bump version to 1.2.4
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/x11r7/xapp_xprop/xapp_xprop.mk
+++ package/x11r7/xapp_xprop/xapp_xprop.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XPROP_VERSION = 1.2.3
+XAPP_XPROP_VERSION = 1.2.4
 XAPP_XPROP_SOURCE = xprop-$(XAPP_XPROP_VERSION).tar.bz2
 XAPP_XPROP_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XPROP_LICENSE = MIT
