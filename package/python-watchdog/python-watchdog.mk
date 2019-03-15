c5a2b7c241441566edeeaecc6659947c8a5ad760
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-03-15 21:56:58 +0100

    package/python-watchdog: bump to version 0.9.0
    - Remove patch (already in version:
      https://github.com/gorakhargosh/watchdog/commit/268a46dabc10ff8dcc5a123ca8c8ceb2e9189f2a)
    - Add hash for license files
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-watchdog/python-watchdog.mk
+++ package/python-watchdog/python-watchdog.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WATCHDOG_VERSION = 0.8.3
+PYTHON_WATCHDOG_VERSION = 0.9.0
 PYTHON_WATCHDOG_SOURCE = watchdog-$(PYTHON_WATCHDOG_VERSION).tar.gz
-PYTHON_WATCHDOG_SITE = https://pypi.python.org/packages/54/7d/c7c0ad1e32b9f132075967fc353a244eb2b375a3d2f5b0ce612fd96e107e
+PYTHON_WATCHDOG_SITE = https://pypi.python.org/packages/bb/e3/5a55d48a29300160779f0a0d2776d17c1b762a2039b36de528b093b87d5b
 PYTHON_WATCHDOG_SETUP_TYPE = setuptools
 PYTHON_WATCHDOG_LICENSE = Apache-2.0
 PYTHON_WATCHDOG_LICENSE_FILES = LICENSE COPYING
