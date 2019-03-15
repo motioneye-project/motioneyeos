048b66016e03ebfb43dc6cdc6805bad451bbe51c
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-03-15 22:07:09 +0100

    package/python-tornado: bump to version 6.0.1
    Remove patch (not needed since:
    https://github.com/tornadoweb/tornado/commit/fa6a7ff6056fc8b139e51294095050f68076f850)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-tornado/python-tornado.mk
+++ package/python-tornado/python-tornado.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TORNADO_VERSION = 5.1.1
+PYTHON_TORNADO_VERSION = 6.0.1
 PYTHON_TORNADO_SOURCE = tornado-$(PYTHON_TORNADO_VERSION).tar.gz
-PYTHON_TORNADO_SITE = https://files.pythonhosted.org/packages/e6/78/6e7b5af12c12bdf38ca9bfe863fcaf53dc10430a312d0324e76c1e5ca426
+PYTHON_TORNADO_SITE = https://pypi.python.org/packages/b1/7f/8109821ff9df1bf3519169e34646705c32ac13be6a4d51a79ed57f47686e
 PYTHON_TORNADO_LICENSE = Apache-2.0
 PYTHON_TORNADO_LICENSE_FILES = LICENSE
 PYTHON_TORNADO_SETUP_TYPE = setuptools
