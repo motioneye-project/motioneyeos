ea62ff85b59aa1ff7757787061eb451b4b4780df
Norbert Lange <nolange79@gmail.com>
2019-03-15 14:30:30 +0100

    package/libfuse: Install udev rules and set permissions
    This fixes some omissions from the installation.
    
    Install the udev rules.
    
    Tell buildroot about the fuse device.
    
    Apply setuid permissions on the fusermount tool.
    
    Signed-off-by: Norbert Lange <norbert.lange@andritz.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/libfuse/libfuse.mk
+++ package/libfuse/libfuse.mk
@@ -14,11 +14,22 @@ LIBFUSE_DEPENDENCIES = $(if $(BR2_PACKAGE_LIBICONV),libiconv)
 LIBFUSE_CONF_OPTS = \
 	--disable-example \
 	--enable-lib \
-	--enable-util
+	--enable-util \
+	UDEV_RULES_PATH=/lib/udev/rules.d
 
 define LIBFUSE_INSTALL_TARGET_CMDS
 	cp -dpf $(STAGING_DIR)/usr/bin/fusermount $(TARGET_DIR)/usr/bin/
 	cp -dpf $(STAGING_DIR)/usr/lib/libfuse.so* $(TARGET_DIR)/usr/lib/
+	mkdir -p $(TARGET_DIR)/lib/udev/rules.d
+	cp $(STAGING_DIR)/lib/udev/rules.d/*-fuse.rules $(TARGET_DIR)/lib/udev/rules.d
+endef
+
+define LIBFUSE_DEVICES
+	/dev/fuse  c  666  0  0  10  229  0  0  -
+endef
+
+define LIBFUSE_PERMISSIONS
+	/usr/bin/fusermount f 4755 0 0 - - - - -
 endef
 
 $(eval $(autotools-package))
