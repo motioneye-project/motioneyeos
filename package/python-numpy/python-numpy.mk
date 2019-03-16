c82da04c7ef2eb44b320f2b4863fa3622896d038
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-03-16 12:49:43 +0100

    package/python-numpy: bump to version 1.16.2
    - Update patches and send them upstream
    - Update license hash (typos fixed and year updated)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-numpy/python-numpy.mk
+++ package/python-numpy/python-numpy.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_NUMPY_VERSION = 1.15.1
+PYTHON_NUMPY_VERSION = 1.16.2
 PYTHON_NUMPY_SOURCE = numpy-$(PYTHON_NUMPY_VERSION).tar.gz
 PYTHON_NUMPY_SITE = https://github.com/numpy/numpy/releases/download/v$(PYTHON_NUMPY_VERSION)
 PYTHON_NUMPY_LICENSE = BSD-3-Clause
