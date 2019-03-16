75e67c3decef1076e794b727d16a73c03cd83358
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-03-16 12:55:03 +0100

    package/arp-scan: bump to version 1.9.5
    Add hash for license file
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/arp-scan/arp-scan.mk
+++ package/arp-scan/arp-scan.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-ARP_SCAN_VERSION = 1.9
-ARP_SCAN_SITE = https://github.com/royhills/arp-scan/releases/download/$(ARP_SCAN_VERSION)
+ARP_SCAN_VERSION = 1.9.5
+ARP_SCAN_SITE = $(call github,royhills,arp-scan,$(ARP_SCAN_VERSION))
 ARP_SCAN_LICENSE = GPL-3.0+
 ARP_SCAN_LICENSE_FILES = COPYING
 ARP_SCAN_DEPENDENCIES = libpcap
