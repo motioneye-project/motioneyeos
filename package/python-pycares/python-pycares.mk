34c046d528d03dd5b8784716dccabb40189f81de
James Hilliard <james.hilliard1@gmail.com>
2019-03-16 11:12:39 +0800

    package/python-pycares: bump to version 3.0.0
    This version drops python 2 support and requires cffi.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pycares/python-pycares.mk
+++ package/python-pycares/python-pycares.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_PYCARES_VERSION = 2.3.0
+PYTHON_PYCARES_VERSION = 3.0.0
 PYTHON_PYCARES_SOURCE = pycares-$(PYTHON_PYCARES_VERSION).tar.gz
-PYTHON_PYCARES_SITE = https://files.pythonhosted.org/packages/89/5c/3a7e1a52d6abb52b9ca1a56d2df699936e89d8b98f75cfd60d03363e7c10
+PYTHON_PYCARES_SITE = https://files.pythonhosted.org/packages/85/de/cd46a73e43e206a6ad1e9cf9cc893c3ed1b21caf57f1e0a8d9a119d290eb
 PYTHON_PYCARES_SETUP_TYPE = setuptools
 PYTHON_PYCARES_LICENSE = MIT
 PYTHON_PYCARES_LICENSE_FILES = LICENSE
+PYTHON_PYCARES_DEPENDENCIES = host-python-cffi
 
 $(eval $(python-package))
