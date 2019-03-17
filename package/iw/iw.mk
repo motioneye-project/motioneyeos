7df834b4f0e9ac7f80df5692cd2a8e6cf9c4f2b2
Baruch Siach <baruch@tkos.co.il>
2019-03-17 05:54:26 +0200

    package/iw: bump to version 5.0.1
    Drop unused GIT_DIR environment.
    
    Cc: Matt Weber <matthew.weber@rockwellcollins.com>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/iw/iw.mk
+++ package/iw/iw.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-IW_VERSION = 4.14
+IW_VERSION = 5.0.1
 IW_SOURCE = iw-$(IW_VERSION).tar.xz
 IW_SITE = $(BR2_KERNEL_MIRROR)/software/network/iw
 IW_LICENSE = ISC
@@ -12,8 +12,7 @@ IW_LICENSE_FILES = COPYING
 IW_DEPENDENCIES = host-pkgconf libnl
 IW_MAKE_ENV = \
 	$(TARGET_MAKE_ENV) \
-	PKG_CONFIG="$(HOST_DIR)/bin/pkg-config" \
-	GIT_DIR=$(IW_DIR)
+	PKG_CONFIG="$(HOST_DIR)/bin/pkg-config"
 
 define IW_BUILD_CMDS
 	$(IW_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
