617749e9eb2b5ff95ed82980f2a626ebadc45fcd
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-03-17 17:05:19 +0100

    package/wpan-tools: bump to version 0.9
    - Switch site to github
    - Remove patch (already in version)
    - Add hash for license file
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/wpan-tools/wpan-tools.mk
+++ package/wpan-tools/wpan-tools.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-WPAN_TOOLS_VERSION = 0.7
-WPAN_TOOLS_SOURCE = wpan-tools-$(WPAN_TOOLS_VERSION).tar.xz
-WPAN_TOOLS_SITE = http://wpan.cakelab.org/releases
+WPAN_TOOLS_VERSION = 0.9
+WPAN_TOOLS_SITE = $(call github,linux-wpan,wpan-tools,wpan-tools-$(WPAN_TOOLS_VERSION))
 WPAN_TOOLS_DEPENDENCIES = host-pkgconf libnl
 WPAN_TOOLS_LICENSE = ISC
 WPAN_TOOLS_LICENSE_FILES = COPYING
+# From git
+WPAN_TOOLS_AUTORECONF = YES
 
 $(eval $(autotools-package))
