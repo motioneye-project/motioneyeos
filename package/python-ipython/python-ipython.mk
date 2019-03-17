399d83f780d7cf93a6168185f88b643e7776eb8c
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-03-17 21:40:55 +0100

    Revert "package/python-ipython: bump to version 7.3.0"
    This reverts commit bc516052593947a067c98956931df9df31ee3195.
    which adds a dependency on python3 as well as adddtional python runtime
    modules. So revert this patch to polish a better bump.
    
    Fixes:
     - http://autobuild.buildroot.org/results/7d765d2b15bfe72b05de62e86a01d57f1696c837
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-ipython/python-ipython.mk
+++ package/python-ipython/python-ipython.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_IPYTHON_VERSION = 7.3.0
+PYTHON_IPYTHON_VERSION = 5.8.0
 PYTHON_IPYTHON_SOURCE = ipython-$(PYTHON_IPYTHON_VERSION).tar.gz
-PYTHON_IPYTHON_SITE = https://pypi.python.org/packages/82/0a/146b0f0f1ddf13ab8682aed5c5a42250cddf1082e193ec95b50b03b9f791
+PYTHON_IPYTHON_SITE = https://files.pythonhosted.org/packages/41/a6/2d25314b1f9375639d8f8e0f8052e8cec5df511d3449f22c4f1c2d8cb3c6
 PYTHON_IPYTHON_LICENSE = BSD-3-Clause
-PYTHON_IPYTHON_LICENSE_FILES = COPYING.rst LICENSE
+PYTHON_IPYTHON_LICENSE_FILES = COPYING.rst
 PYTHON_IPYTHON_SETUP_TYPE = distutils
 
 $(eval $(python-package))
