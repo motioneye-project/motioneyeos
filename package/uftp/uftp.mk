340f89403efc50c3a6096d13282de0761ecf683e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-03-19 23:17:30 +0100

    package/uftp: openssl is optional, not mandatory
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/uftp/uftp.mk
+++ package/uftp/uftp.mk
@@ -6,11 +6,15 @@
 
 UFTP_VERSION = 4.9.9
 UFTP_SITE = http://sourceforge.net/projects/uftp-multicast/files/source-tar
-UFTP_DEPENDENCIES = host-pkgconf openssl
 UFTP_LICENSE = GPL-3.0+
 UFTP_LICENSE_FILES = LICENSE.txt
 
-UFTP_MAKE_OPTS = CRYPT_LIB="`$(PKG_CONFIG_HOST_BINARY) --libs libssl`"
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+UFTP_DEPENDENCIES += host-pkgconf openssl
+UFTP_MAKE_OPTS += CRYPT_LIB="`$(PKG_CONFIG_HOST_BINARY) --libs libssl`"
+else
+UFTP_MAKE_OPTS += NO_ENCRYPTION=1
+endif
 
 define UFTP_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) $(UFTP_MAKE_OPTS)
