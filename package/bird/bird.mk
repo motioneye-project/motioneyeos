529fef57f6f46201ba16349b49c0a65ceccc4c25
Adrien Gallouët <adrien@gallouet.fr>
2019-03-20 21:09:44 +0000

    package/bird: add optional protocols
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>
    [Thomas:
     - use proper variable names in .mk file
     - add comment in Config.in]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/bird/bird.mk
+++ package/bird/bird.mk
@@ -17,4 +17,20 @@ else
 BIRD_CONF_OPTS += --disable-client
 endif
 
+# BUG: RIP is mandatory for now
+BIRD_PROTOCOLS = \
+	rip \
+	$(if $(BR2_PACKAGE_BIRD_BFD),bfd) \
+	$(if $(BR2_PACKAGE_BIRD_BABEL),babel) \
+	$(if $(BR2_PACKAGE_BIRD_BGP),bgp) \
+	$(if $(BR2_PACKAGE_BIRD_MRT),mrt) \
+	$(if $(BR2_PACKAGE_BIRD_OSPF),ospf) \
+	$(if $(BR2_PACKAGE_BIRD_PERF),perf) \
+	$(if $(BR2_PACKAGE_BIRD_PIPE),pipe) \
+	$(if $(BR2_PACKAGE_BIRD_RADV),radv) \
+	$(if $(BR2_PACKAGE_BIRD_RIP),rip) \
+	$(if $(BR2_PACKAGE_BIRD_STATIC),static)
+
+BIRD_CONF_OPTS += --with-protocols=$(subst $(space),$(comma),$(strip $(BIRD_PROTOCOLS)))
+
 $(eval $(autotools-package))
