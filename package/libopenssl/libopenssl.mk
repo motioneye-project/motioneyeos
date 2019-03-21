c2c57aa30f4a77c1b7b835ed2f9002ecd35b7aac
Peter Korsgaard <peter@korsgaard.com>
2019-03-21 21:47:02 +0100

    package/libopenssl: bump to version 1.1.1b
    Drop patches 4..6 as they are now upstream.
    
    Update the hash of the license file as the copyright dates changed.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libopenssl/libopenssl.mk
+++ package/libopenssl/libopenssl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBOPENSSL_VERSION = 1.1.1a
+LIBOPENSSL_VERSION = 1.1.1b
 LIBOPENSSL_SITE = https://www.openssl.org/source
 LIBOPENSSL_SOURCE = openssl-$(LIBOPENSSL_VERSION).tar.gz
 LIBOPENSSL_LICENSE = OpenSSL or SSLeay
