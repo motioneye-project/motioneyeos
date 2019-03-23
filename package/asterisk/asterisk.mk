54c54e16558d1018cffce7717c5c2f0a6d259cae
Peter Korsgaard <peter@korsgaard.com>
2019-03-23 17:54:27 +0100

    package/asterisk: security bump to version 16.2.1
    Fixes the following security issue:
    
    AST-2019-001: Remote crash vulnerability with SDP protocol violation
    When Asterisk makes an outgoing call, a very specific SDP protocol violation
    by the remote party can cause Asterisk to crash (CVE-2019-7251)
    
    https://downloads.asterisk.org/pub/security/AST-2019-001.html
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 391a1e5df7a57abfcebd1ca260b56c76c941e5ec)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/asterisk/asterisk.mk
+++ package/asterisk/asterisk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ASTERISK_VERSION = 16.1.1
+ASTERISK_VERSION = 16.2.1
 # Use the github mirror: it's an official mirror maintained by Digium, and
 # provides tarballs, which the main Asterisk git tree (behind Gerrit) does not.
 ASTERISK_SITE = $(call github,asterisk,asterisk,$(ASTERISK_VERSION))
