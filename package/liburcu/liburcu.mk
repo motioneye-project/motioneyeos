f63dd851297ceb4f7065a9bd48f019819de042c3
Norbert Lange <norbert.lange@andritz.com>
2019-03-25 22:51:02 +0100

    package/liburcu: bump version to 0.10.2
    This includes a fix that might delay application startup
    considerably (11486a15008b9).
    
    Adapted the patch to point to the correct file.
    
    Signed-off-by: Norbert Lange <nolange79@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/liburcu/liburcu.mk
+++ package/liburcu/liburcu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBURCU_VERSION = 0.9.2
+LIBURCU_VERSION = 0.10.2
 LIBURCU_SITE = http://lttng.org/files/urcu
 LIBURCU_SOURCE = userspace-rcu-$(LIBURCU_VERSION).tar.bz2
 LIBURCU_LICENSE = LGPL-2.1+ (library), MIT-like (few source files listed in LICENSE), GPL-2.0+ (test), GPL-3.0 (few *.m4 files)
