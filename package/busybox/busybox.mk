1faf7dcac2ae79d6f2524dc7c2c72e2c1a01cd05
Jared Bents <jared.bents@rockwellcollins.com>
2019-03-25 11:28:19 -0500

    package/busybox: version bump to 1.30.1
    Update from version 1.29.3 to 1.30.1. The new version
    includes the removed patches.
    
    Signed-off-by: Jared Bents <jared.bents@rockwellcollins.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/busybox/busybox.mk
+++ package/busybox/busybox.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BUSYBOX_VERSION = 1.29.3
+BUSYBOX_VERSION = 1.30.1
 BUSYBOX_SITE = http://www.busybox.net/downloads
 BUSYBOX_SOURCE = busybox-$(BUSYBOX_VERSION).tar.bz2
 BUSYBOX_LICENSE = GPL-2.0
