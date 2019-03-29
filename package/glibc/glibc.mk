1cf12ba63164e2d495680f63978c80e59ad824e9
Peter Korsgaard <peter@korsgaard.com>
2019-03-29 10:47:14 +0100

    package/glibc: bump version for additional post-2.28 security fixes
    Fixes the following security vulnerabilities:
    
      CVE-2019-6488: On x32, the size_t parameter may be passed in the lower
      32 bits of a 64-bit register with with non-zero upper 32 bit.  When it
      happened, accessing the 32-bit size_t value as the full 64-bit register
      in the assembly string/memory functions would cause a buffer overflow.
      Reported by H.J. Lu.
    
      CVE-2019-7309: x86-64 memcmp used signed Jcc instructions to check
      size.  For x86-64, memcmp on an object size larger than SSIZE_MAX
      has undefined behavior.  On x32, the size_t argument may be passed
      in the lower 32 bits of the 64-bit RDX register with non-zero upper
      32 bits.  When it happened with the sign bit of RDX register set,
      memcmp gave the wrong result since it treated the size argument as
      zero.  Reported by H.J. Lu.
    
      CVE-2016-10739: The getaddrinfo function could successfully parse IPv4
      addresses with arbitrary trailing characters, potentially leading to data
      or command injection issues in applications.
    
      CVE-2019-9169: Attempted case-insensitive regular-expression match
      via proceed_next_node in posix/regexec.c leads to heap-based buffer
      over-read.  Reported by Hongxu Chen.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -13,7 +13,7 @@ GLIBC_SITE = $(call github,riscv,riscv-glibc,$(GLIBC_VERSION))
 else
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master
-GLIBC_VERSION = glibc-2.28-69-g1e5c5303a522764d7e9d2302a60e4a32cdb902f1
+GLIBC_VERSION = glibc-2.28-94-g4aeff335ca19286ee2382d8eba794ae5fd49281a
 # Upstream doesn't officially provide an https download link.
 # There is one (https://sourceware.org/git/glibc.git) but it's not reliable,
 # sometimes the connection times out. So use an unofficial github mirror.
