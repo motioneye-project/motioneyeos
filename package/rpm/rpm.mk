aa2703ab0dfea58a4fd2e71b25f278be62de10f1
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-03-30 15:49:42 +0100

    package/rpm: add optional xz dependency
    There is not --{disable,enable}-lzma option
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/rpm/rpm.mk
+++ package/rpm/rpm.mk
@@ -14,6 +14,7 @@ RPM_DEPENDENCIES = \
 	$(if $(BR2_PACKAGE_BZIP2),bzip2) \
 	file \
 	popt \
+	$(if $(BR2_PACKAGE_XZ),xz) \
 	zlib \
 	$(TARGET_NLS_DEPENDENCIES)
 RPM_LICENSE = GPL-2.0 or LGPL-2.0 (library only)
