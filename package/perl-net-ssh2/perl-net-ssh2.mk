42d1599acec5ff9b63628cc0911e6c4a917e9c86
Francois Perrad <fperrad@gmail.com>
2019-03-31 19:44:47 +0200

    package/perl-net-ssh2: bump to version 0.70
    The license file hash changed due to a copyright year update:
    
    -Copyright (C) 2011-2018 by Salvador Fandino (salva@cpan.org).
    +Copyright (C) 2011-2019 by Salvador Fandino (salva@cpan.org).
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    [Thomas: explain why the license file hash is changed]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/perl-net-ssh2/perl-net-ssh2.mk
+++ package/perl-net-ssh2/perl-net-ssh2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_NET_SSH2_VERSION = 0.69
+PERL_NET_SSH2_VERSION = 0.70
 PERL_NET_SSH2_SOURCE = Net-SSH2-$(PERL_NET_SSH2_VERSION).tar.gz
 PERL_NET_SSH2_SITE = $(BR2_CPAN_MIRROR)/authors/id/S/SA/SALVA
 PERL_NET_SSH2_LICENSE = Artistic or GPL-1.0+
