fd24e6ffecbce808eadd10cbc33b48d9b548a95c
Francois Perrad <fperrad@gmail.com>
2019-03-31 19:44:46 +0200

    package/perl-net-ping: bump to version 2.72
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/perl-net-ping/perl-net-ping.mk
+++ package/perl-net-ping/perl-net-ping.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_NET_PING_VERSION = 2.71
+PERL_NET_PING_VERSION = 2.72
 PERL_NET_PING_SOURCE = Net-Ping-$(PERL_NET_PING_VERSION).tar.gz
 PERL_NET_PING_SITE = $(BR2_CPAN_MIRROR)/authors/id/R/RU/RURBAN
 PERL_NET_PING_LICENSE = Artistic or GPL-1.0+
