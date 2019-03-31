3d2ceb7448a0fe18113b86134b988060a5034ddd
Francois Perrad <fperrad@gmail.com>
2019-03-31 19:44:48 +0200

    package/perl-sub-quote: bump to version 2.006003
    The hash of the README changed for reasons unrelated to licensing: a
    new "ENVIRONMENT" section was added to the README.
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    [Thomas: re-add license file, explain in the commit log why its hash
    changed]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/perl-sub-quote/perl-sub-quote.mk
+++ package/perl-sub-quote/perl-sub-quote.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_SUB_QUOTE_VERSION = 2.005001
+PERL_SUB_QUOTE_VERSION = 2.006003
 PERL_SUB_QUOTE_SOURCE = Sub-Quote-$(PERL_SUB_QUOTE_VERSION).tar.gz
 PERL_SUB_QUOTE_SITE = $(BR2_CPAN_MIRROR)/authors/id/H/HA/HAARG
 PERL_SUB_QUOTE_LICENSE = Artistic or GPL-1.0+
