d916b78a83a5da04f69bdf609736539147c4cb45
Francois Perrad <fperrad@gmail.com>
2019-03-31 19:44:36 +0200

    package/perl-class-inspector: bump to version 1.34
    The hash of the license file changes only due to copyright year
    changes:
    
    -This software is copyright (c) 2016 by Adam Kennedy.
    +This software is copyright (c) 2002-2019 by Adam Kennedy.
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    [Thomas: add details as to why the license file hash changed]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/perl-class-inspector/perl-class-inspector.mk
+++ package/perl-class-inspector/perl-class-inspector.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_CLASS_INSPECTOR_VERSION = 1.32
+PERL_CLASS_INSPECTOR_VERSION = 1.34
 PERL_CLASS_INSPECTOR_SOURCE = Class-Inspector-$(PERL_CLASS_INSPECTOR_VERSION).tar.gz
 PERL_CLASS_INSPECTOR_SITE = $(BR2_CPAN_MIRROR)/authors/id/P/PL/PLICEASE
 PERL_CLASS_INSPECTOR_LICENSE = Artistic or GPL-1.0+
