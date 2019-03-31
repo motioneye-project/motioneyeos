4854a1cf4c7101b18207cab497cd62deb19e6ff2
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-03-31 17:12:05 +0200

    package/oniguruma: new package
    Oniguruma is a modern and flexible regular expressions
    library. It encompasses features from different regular
    expression implementations that traditionally exist in
    different languages.
    
    https://github.com/kkos/oniguruma
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/oniguruma/oniguruma.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# oniguruma
+#
+################################################################################
+
+ONIGURUMA_VERSION = 6.9.1
+ONIGURUMA_SITE = $(call github,kkos,oniguruma,v$(ONIGURUMA_VERSION))
+ONIGURUMA_LICENSE = BSD-2-Clause
+ONIGURUMA_LICENSE_FILES = COPYING
+# From git
+ONIGURUMA_AUTORECONF = YES
+ONIGURUMA_INSTALL_STAGING = YES
+
+$(eval $(autotools-package))
