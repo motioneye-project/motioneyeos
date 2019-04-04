f63a58c35018d55bb56900922bd81369e214e4a6
Peter Korsgaard <peter@korsgaard.com>
2019-04-04 21:00:28 +0200

    package/tpm2-tools: drop unused dbus / libglib2 dependencies
    tpm2-tools does not need dbus or libglib2, so remove them and the
    corresponding toolchain dependencies.
    
    The confusion may have come from the upstream travis configuration, which
    also builds tpm2-abrmd (which uses dbus+libglib2).
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/tpm2-tools/tpm2-tools.mk
+++ package/tpm2-tools/tpm2-tools.mk
@@ -8,7 +8,7 @@ TPM2_TOOLS_VERSION = 3.1.4
 TPM2_TOOLS_SITE = https://github.com/tpm2-software/tpm2-tools/releases/download/$(TPM2_TOOLS_VERSION)
 TPM2_TOOLS_LICENSE = BSD-2-Clause
 TPM2_TOOLS_LICENSE_FILES = LICENSE
-TPM2_TOOLS_DEPENDENCIES = dbus libcurl libglib2 openssl tpm2-tss host-pkgconf
+TPM2_TOOLS_DEPENDENCIES = libcurl openssl tpm2-tss host-pkgconf
 
 # -fstack-protector-all and FORTIFY_SOURCE=2 is used by
 # default. Disable that so the BR2_SSP_* / BR2_FORTIFY_SOURCE_* options
