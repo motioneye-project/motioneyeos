87ec6e778701854ce58e0d907fcdf8eaaae0382a
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-04-05 19:10:22 +0200

    package/qdecoder: bump to version 12.0.7
    - Remove all patches (already in version)
    - Drop AUTORECONF as configure.ac is not patched anymore
    - Add hash for license file
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/qdecoder/qdecoder.mk
+++ package/qdecoder/qdecoder.mk
@@ -4,12 +4,10 @@
 #
 ################################################################################
 
-QDECODER_VERSION = r12.0.5
+QDECODER_VERSION = v12.0.7
 QDECODER_SITE = $(call github,wolkykim,qdecoder,$(QDECODER_VERSION))
 QDECODER_LICENSE = BSD-2
 QDECODER_LICENSE_FILES = COPYING
-# we patch configure.ac
-QDECODER_AUTORECONF = YES
 QDECODER_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
 
 QDECODER_INSTALL_STAGING = YES
