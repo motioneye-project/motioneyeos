260056ea220533ea7224d79fcdff12b18fc3a4ab
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-04-07 21:58:32 +0200

    package/lldpd: bump to version 1.0.3
    - Remove patch (already in version)
    - Drop autoreconf as patch has been removed
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/lldpd/lldpd.mk
+++ package/lldpd/lldpd.mk
@@ -4,13 +4,11 @@
 #
 ################################################################################
 
-LLDPD_VERSION = 1.0.1
+LLDPD_VERSION = 1.0.3
 LLDPD_SITE = http://media.luffy.cx/files/lldpd
 LLDPD_DEPENDENCIES = host-pkgconf libevent
 LLDPD_LICENSE = ISC
 LLDPD_LICENSE_FILES = LICENSE
-# 0002-configure-do-not-check-for-libbsd.patch / 0003-configure-remove-check-on-CXX-compiler.patch
-LLDPD_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_CHECK),y)
 LLDPD_DEPENDENCIES += check
