92c7310d5be2956d2f609013289cc85073deac24
Peter Korsgaard <peter@korsgaard.com>
2019-04-08 21:22:57 +0200

    package/tpm2-tools: license is 3c BSD, not 2c
    The license contains the "no endorsement" clause, so it should be listed as
    BSD-3-Clause:
    
      * Neither the name of Intel Corporation nor the names of its contributors
        may be used to endorse or promote products derived from this software
        without specific prior written permission.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/tpm2-tools/tpm2-tools.mk
+++ package/tpm2-tools/tpm2-tools.mk
@@ -6,7 +6,7 @@
 
 TPM2_TOOLS_VERSION = 3.1.4
 TPM2_TOOLS_SITE = https://github.com/tpm2-software/tpm2-tools/releases/download/$(TPM2_TOOLS_VERSION)
-TPM2_TOOLS_LICENSE = BSD-2-Clause
+TPM2_TOOLS_LICENSE = BSD-3-Clause
 TPM2_TOOLS_LICENSE_FILES = LICENSE
 TPM2_TOOLS_DEPENDENCIES = libcurl openssl tpm2-tss host-pkgconf
 
