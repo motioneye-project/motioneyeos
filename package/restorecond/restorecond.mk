9e40ea2f5b8cb0de1ee5e4b2b88a4233c9e5d78a
Adam Duskett <Aduskett@gmail.com>
2019-04-08 14:50:11 -0400

    package/restorecond: bump to version 2.9
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/restorecond/restorecond.mk
+++ package/restorecond/restorecond.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-RESTORECOND_VERSION = 2.8
-RESTORECOND_SITE = https://raw.githubusercontent.com/wiki/SELinuxProject/selinux/files/releases/20180524
+RESTORECOND_VERSION = 2.9
+RESTORECOND_SITE = https://github.com/SELinuxProject/selinux/releases/download/20190315
 RESTORECOND_LICENSE = GPL-2.0
 RESTORECOND_LICENSE_FILES = COPYING
 
