95854d76c55055d0dfe0b313338058c825af309d
Giulio Benetti <giulio.benetti@micronovasrl.com>
2019-04-09 22:46:30 +0200

    package/python-numpy: add missing sublibraries licenses
    Release 1.16.2 tarball doesn't contain all sublibraries license files
    listed in the base LICENSE.txt file, one is missing, even though it is
    present in the project Git repository.
    
    This makes python-numpy-legal-info incomplete.
    
    - Add patch to add missing sublibrary license file:
      numpy/linalg/lapack_lite/LICENSE.txt
    - its sha256 into hash file.
    - update PYTHON_NUMPY_LICENSE adding:
      BSD-2-Clause, PSF, Apache-2.0, MIT, Zlib
    
    Signed-off-by: Giulio Benetti <giulio.benetti@micronovasrl.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-numpy/python-numpy.mk
+++ package/python-numpy/python-numpy.mk
@@ -7,8 +7,12 @@
 PYTHON_NUMPY_VERSION = 1.16.2
 PYTHON_NUMPY_SOURCE = numpy-$(PYTHON_NUMPY_VERSION).tar.gz
 PYTHON_NUMPY_SITE = https://github.com/numpy/numpy/releases/download/v$(PYTHON_NUMPY_VERSION)
-PYTHON_NUMPY_LICENSE = BSD-3-Clause
-PYTHON_NUMPY_LICENSE_FILES = LICENSE.txt
+PYTHON_NUMPY_LICENSE = BSD-3-Clause, BSD-2-Clause, PSF, Apache-2.0, MIT, Zlib
+PYTHON_NUMPY_LICENSE_FILES = LICENSE.txt doc/sphinxext/LICENSE.txt \
+			doc/scipy-sphinx-theme/LICENSE.txt \
+			numpy/linalg/lapack_lite/LICENSE.txt \
+			tools/npy_tempita/license.txt \
+			numpy/core/src/multiarray/dragon4.c
 PYTHON_NUMPY_SETUP_TYPE = setuptools
 
 ifeq ($(BR2_PACKAGE_CLAPACK),y)
