027a8b29f1e62d5ff5bbb15b79376614f902a680
Giulio Benetti <giulio.benetti@micronovasrl.com>
2019-04-09 23:10:32 +0200

    package/civetweb: fix link failure due to missing OpenSSL dependency
    During linking one OpenSSL dependecy is missing(-latomic) on linking
    library list.
    
    - Substitute explicit library list with `pkg-config libssl` when
      BR2_PACKAGE_OPENSSL is enabled. In such way all needed libraries
      will be included in linking list.
    
    - Add also `host-pkgconf` to CIVETWEB_DEPENDENCIES if
      BR2_PACKAGE_OPENSSL is enabled to make it available for previous
      point.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/b2e210bdefe84f4ec9cfda79a33d81788fb7e66c/
    
    Signed-off-by: Giulio Benetti <giulio.benetti@micronovasrl.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/civetweb/civetweb.mk
+++ package/civetweb/civetweb.mk
@@ -32,8 +32,8 @@ endif
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 CIVETWEB_COPT += -DNO_SSL_DL
-CIVETWEB_LIBS += -lssl -lcrypto -lz
-CIVETWEB_DEPENDENCIES += openssl
+CIVETWEB_LIBS += `$(PKG_CONFIG_HOST_BINARY) --libs libssl`
+CIVETWEB_DEPENDENCIES += openssl host-pkgconf
 else
 CIVETWEB_COPT += -DNO_SSL
 endif
