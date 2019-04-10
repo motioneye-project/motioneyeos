9a47266fc03bf4c8d04478f0226dc950eac85103
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-04-10 20:59:01 +0200

    package/pixman: bump version to 0.38.4
    Removed patch 0002, applied upstream
    https://cgit.freedesktop.org/pixman/commit/?id=7c6066b700c7cdd4aeb8be426b14b3a5f0de4b6c
    
    Added md5 hash provided by upstream and sha256 hashes for tarball and
    license file.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/pixman/pixman.mk
+++ package/pixman/pixman.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PIXMAN_VERSION = 0.34.0
+PIXMAN_VERSION = 0.38.4
 PIXMAN_SOURCE = pixman-$(PIXMAN_VERSION).tar.bz2
 PIXMAN_SITE = http://xorg.freedesktop.org/releases/individual/lib
 PIXMAN_LICENSE = MIT
