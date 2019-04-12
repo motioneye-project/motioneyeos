c66a248de2906e401a530008dc2bbd699756a521
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-04-12 22:37:34 +0200

    package/libsodium: bump to version 1.0.17
    Update hash of license file (update in year)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/libsodium/libsodium.mk
+++ package/libsodium/libsodium.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSODIUM_VERSION = 1.0.16
+LIBSODIUM_VERSION = 1.0.17
 LIBSODIUM_SITE = https://github.com/jedisct1/libsodium/releases/download/$(LIBSODIUM_VERSION)
 LIBSODIUM_LICENSE = ISC
 LIBSODIUM_LICENSE_FILES = LICENSE
