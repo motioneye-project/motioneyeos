29781312584dc2cc529fede9ae8610598df735dc
Francois Perrad <fperrad@gmail.com>
2019-04-13 15:33:42 +0200

    package/lrandom: bump to version 20180729
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/lrandom/lrandom.mk
+++ package/lrandom/lrandom.mk
@@ -4,12 +4,8 @@
 #
 ################################################################################
 
-ifeq ($(BR2_PACKAGE_LUA_5_3),y)
-LRANDOM_VERSION = 20120430.52-1
-else
-LRANDOM_VERSION = 20120430.51-1
-endif
-LRANDOM_SUBDIR = random
-LRANDOM_LICENSE = Public domain
+LRANDOM_VERSION = 20180729-1
+LRANDOM_SUBDIR = lrandom-100
+LRANDOM_LICENSE = MIT
 
 $(eval $(luarocks-package))
