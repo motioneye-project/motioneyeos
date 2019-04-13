f40ceb857e7f4e5b01bf0867e729240df3c977da
Sven Oliver Moll <buildroot@svol.li>
2019-04-13 22:40:57 +0200

    package/most: new package
    --with-slang has to be given explicitly, otherwise the configure script
    looks for slang.h in host directories.
    
    Signed-off-by: Sven Oliver Moll <svolli@svolli.de>
    [Arnout: add --with-slang config option]
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

---
+++ package/most/most.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# most
+#
+################################################################################
+
+MOST_SITE = http://www.jedsoft.org/releases/most
+MOST_VERSION = 5.1.0
+MOST_LICENSE = GPL-2.0+
+MOST_LICENSE_FILES = COPYING COPYRIGHT
+MOST_DEPENDENCIES = slang
+
+MOST_CONF_OPTS = --with-slang=$(STAGING_DIR)/usr
+
+define MOST_REMOVE_LOCAL_SLANG_CHECK
+	$(SED) 's/ slangversion / /g' $(@D)/src/Makefile.in
+endef
+MOST_POST_PATCH_HOOKS += MOST_REMOVE_LOCAL_SLANG_CHECK
+
+define MOST_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/src/objs/most $(TARGET_DIR)/usr/bin/most
+endef
+
+$(eval $(autotools-package))
