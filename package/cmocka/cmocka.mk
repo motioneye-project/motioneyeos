6235e4507523ea537d1a5e6ca7fa0d3ef3f5dfe1
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-04-13 21:39:49 +0200

    package/cmocka: bump to version 1.1.5
    - Remove patch (already in version)
    - Use WITH_EXAMPLES=OFF (option added with
      https://git.cryptomilk.org/projects/cmocka.git/commit?id=ee264da1aa222b354cee36184ab48c2072f62243)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/cmocka/cmocka.mk
+++ package/cmocka/cmocka.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-CMOCKA_VERSION = 1.1.3
+CMOCKA_VERSION = 1.1.5
 CMOCKA_SOURCE = cmocka-$(CMOCKA_VERSION).tar.xz
 CMOCKA_SITE = https://cmocka.org/files/1.1
 CMOCKA_LICENSE = Apache-2.0
 CMOCKA_LICENSE_FILES = COPYING
 CMOCKA_INSTALL_STAGING = YES
+CMOCKA_CONF_OPTS = -DWITH_EXAMPLES=OFF
 
 # cmocka only supports out of source builds
 CMOCKA_SUPPORTS_IN_SOURCE_BUILD = NO
