16de141ddbfbf41b4336565449f5719ca5956a5e
Francois Perrad <fperrad@gmail.com>
2019-04-13 15:33:41 +0200

    package/luadbi-sqlite3: bump to version 0.7.2
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/luadbi-sqlite3/luadbi-sqlite3.mk
+++ package/luadbi-sqlite3/luadbi-sqlite3.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUADBI_SQLITE3_VERSION = 0.6-2
+LUADBI_SQLITE3_VERSION = 0.7.2-1
 LUADBI_SQLITE3_SUBDIR = luadbi
 LUADBI_SQLITE3_LICENSE = MIT
 LUADBI_SQLITE3_LICENSE_FILES = $(LUADBI_SQLITE3_SUBDIR)/COPYING
