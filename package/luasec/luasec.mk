ff2ad87ad10e79a08ab8b41bc0c2f1630210f90a
Francois Perrad <fperrad@gmail.com>
2019-04-17 08:16:33 +0200

    package/luasec: bump to version 0.8
    the license file hash changed due to a copyright year update
    
    -LuaSec 0.7 license
    -Copyright (C) 2006-2018 Bruno Silvestre, UFG
    +LuaSec 0.8 license
    +Copyright (C) 2006-2019 Bruno Silvestre, UFG
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/luasec/luasec.mk
+++ package/luasec/luasec.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUASEC_VERSION_UPSTREAM = 0.7
+LUASEC_VERSION_UPSTREAM = 0.8
 LUASEC_VERSION = $(LUASEC_VERSION_UPSTREAM)-1
 LUASEC_SUBDIR = luasec-luasec-$(LUASEC_VERSION_UPSTREAM)
 LUASEC_LICENSE = MIT
