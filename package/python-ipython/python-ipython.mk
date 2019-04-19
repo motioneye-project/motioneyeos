6ebaef3818d6e3dc821616a33391eb6d594a053e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-04-19 11:19:32 +0200

    package/python-ipython: bump to version 7.4.0
    - Update hash for COPYING.rst, it has been updated to remove
      BSD-3-Clause text:
      https://github.com/ipython/ipython/commit/5a6ef3e35db9ac04bf47a17d96484e03370f03dd
    - Add LICENSE to license files as this is now the file that contain
      BSD-3-Clause text
    - Needs python3 since version 6.x and above
    - Needs new runtime dependencies: backcall and jedi
    - Drop obsolete python2 dependencies as well as simplegeneric:
      https://github.com/ipython/ipython/commit/dc0ceb16f92e0e943435106e863a5cfccd724a2d
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-ipython/python-ipython.mk
+++ package/python-ipython/python-ipython.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_IPYTHON_VERSION = 5.8.0
+PYTHON_IPYTHON_VERSION = 7.4.0
 PYTHON_IPYTHON_SOURCE = ipython-$(PYTHON_IPYTHON_VERSION).tar.gz
-PYTHON_IPYTHON_SITE = https://files.pythonhosted.org/packages/41/a6/2d25314b1f9375639d8f8e0f8052e8cec5df511d3449f22c4f1c2d8cb3c6
+PYTHON_IPYTHON_SITE = https://files.pythonhosted.org/packages/e3/88/39c8b7701b2f7d5c8f3a1796b0c174f21071232bc5b242feb670e913acc6
 PYTHON_IPYTHON_LICENSE = BSD-3-Clause
-PYTHON_IPYTHON_LICENSE_FILES = COPYING.rst
+PYTHON_IPYTHON_LICENSE_FILES = COPYING.rst LICENSE
 PYTHON_IPYTHON_SETUP_TYPE = distutils
 
 $(eval $(python-package))
