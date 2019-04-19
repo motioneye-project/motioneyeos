a721018e0de59d79ad8e6802d10d39e63a0c9e3f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-04-19 14:40:08 +0200

    package/jemalloc: drop obsolete valgrind dependency
    valgrind support has been removed since version 5.0.0 and
    https://github.com/jemalloc/jemalloc/commit/9a8add1510456464bc496320990ec234798bd381
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/jemalloc/jemalloc.mk
+++ package/jemalloc/jemalloc.mk
@@ -11,14 +11,5 @@ JEMALLOC_LICENSE = BSD-2-Clause
 JEMALLOC_LICENSE_FILES = COPYING
 JEMALLOC_INSTALL_STAGING = YES
 
-ifeq ($(BR2_PACKAGE_VALGRIND),y)
-JEMALLOC_DEPENDENCIES += valgrind
-JEMALLOC_CONF_OPTS += --enable-valgrind
-else
-JEMALLOC_CONF_OPTS += --disable-valgrind
-endif
-
-HOST_JEMALLOC_CONF_OPTS += --disable-valgrind
-
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
