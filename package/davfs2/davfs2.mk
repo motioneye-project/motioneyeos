e4b39cf4f44a213c38dd01fb9495691f76e66a49
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-04-20 12:25:28 +0200

    package/davfs2: add optional dependency on iconv and NLS
    Fixes:
     - http://autobuild.buildroot.org/results/ac2c39a7300fbdd069ac5504148ccb2b065fbf8e
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/davfs2/davfs2.mk
+++ package/davfs2/davfs2.mk
@@ -9,9 +9,13 @@ DAVFS2_SITE = http://download.savannah.nongnu.org/releases/davfs2
 DAVFS2_LICENSE = GPL-3.0+
 DAVFS2_LICENSE_FILES = COPYING
 
-DAVFS2_DEPENDENCIES = neon
+DAVFS2_DEPENDENCIES = \
+	neon \
+	$(if $(BR2_PACKAGE_LIBICONV),libiconv) \
+	$(TARGET_NLS_DEPENDENCIES)
 
 DAVFS2_CONF_ENV += \
-	ac_cv_path_NEON_CONFIG=$(STAGING_DIR)/usr/bin/neon-config
+	ac_cv_path_NEON_CONFIG=$(STAGING_DIR)/usr/bin/neon-config \
+	LIBS=$(TARGET_NLS_LIBS)
 
 $(eval $(autotools-package))
