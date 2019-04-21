f243f57abcdb415b57b8b887e9cc35bf69163224
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-04-21 19:08:51 +0200

    package/jemalloc: fix build on or1k with gcc < 6
    Fixes:
     - http://autobuild.buildroot.org/results/8cb80bef5c60ac1e5f6507fbc68c21c32f4af84a
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/jemalloc/jemalloc.mk
+++ package/jemalloc/jemalloc.mk
@@ -11,5 +11,11 @@ JEMALLOC_LICENSE = BSD-2-Clause
 JEMALLOC_LICENSE_FILES = COPYING
 JEMALLOC_INSTALL_STAGING = YES
 
+# gcc bug internal compiler error: in merge_overlapping_regs, at
+# regrename.c:304. This bug is fixed since gcc 6.
+ifeq ($(BR2_or1k):$(BR2_TOOLCHAIN_GCC_AT_LEAST_6),y:)
+JEMALLOC_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -O0"
+endif
+
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
