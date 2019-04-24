b685b92642ba9435409a122073af4746c0bd4f6f
Angelo Compagnucci <angelo@amarulasolutions.com>
2019-04-24 11:37:48 +0200

    package/corkscrew: bump to version v2.0
    This patch bumps corkscrew package to version v2.0.
    
    Signed-off-by: Angelo Compagnucci <angelo@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/corkscrew/corkscrew.mk
+++ package/corkscrew/corkscrew.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CORKSCREW_VERSION = a94f745b40077172b8fe7d77e2d583b9cf900281
+CORKSCREW_VERSION = v2.0
 CORKSCREW_SITE = $(call github,bryanpkc,corkscrew,$(CORKSCREW_VERSION))
 CORKSCREW_LICENSE = GPL-2.0
 CORKSCREW_LICENSE_FILES = COPYING
