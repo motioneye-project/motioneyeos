c5b613febc09ebfd6c03dda360e1832a7c403b93
Angelo Compagnucci <angelo@amarulasolutions.com>
2019-04-24 11:37:32 +0200

    package/python-can: bump to version 3.1.1
    This patch bumps python-can to version 3.1.1, it removes md5 sum from
    hash file cause it's not present anymore on the pypi website.
    
    Signed-off-by: Angelo Compagnucci <angelo@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-can/python-can.mk
+++ package/python-can/python-can.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_CAN_VERSION = 3.0.0
-PYTHON_CAN_SITE = https://files.pythonhosted.org/packages/29/47/59d07bb02d6b244fb631487ae5424a10658c316defeeb90c923b48043792
+PYTHON_CAN_VERSION = 3.1.1
+PYTHON_CAN_SITE = https://files.pythonhosted.org/packages/c1/b0/72d7ed7840692ace5b55dd1155409cd86fd83cee4ded75e3d1102c08750a
 PYTHON_CAN_SETUP_TYPE = setuptools
 PYTHON_CAN_LICENSE = LGPL-3.0
 PYTHON_CAN_LICENSE_FILES = LICENSE.txt
