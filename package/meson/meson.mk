70fb5e610761dd6468a058c580acc3d4f81fe547
Adam Duskett <Aduskett@gmail.com>
2019-04-24 14:58:57 -0400

    package/meson: bump to version 0.50.1
    Tested against systemd and glib-networking.
    
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    [Thomas: use the upstream-uploaded tarball and not the github macro,
    which allows to have the tarball that really matches upstream's GPG
    signature]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/meson/meson.mk
+++ package/meson/meson.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MESON_VERSION = 0.49.2
+MESON_VERSION = 0.50.1
 MESON_SITE = https://github.com/mesonbuild/meson/releases/download/$(MESON_VERSION)
 MESON_LICENSE = Apache-2.0
 MESON_LICENSE_FILES = COPYING
