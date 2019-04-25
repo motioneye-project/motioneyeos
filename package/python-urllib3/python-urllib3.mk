39dc2c601f713fad3e2c8d0d2d59c13f6a53fac4
Peter Korsgaard <peter@korsgaard.com>
2019-04-25 09:07:22 +0200

    package/python-urllib3: security bump to version 1.24.2
    Fixes the following security issue:
    
    - CVE-2019-11324: The urllib3 library before 1.24.2 for Python mishandles
      certain cases where the desired set of CA certificates is different from
      the OS store of CA certificates, which results in SSL connections
      succeeding in situations where a verification failure is the correct
      outcome.  This is related to use of the ssl_context, ca_certs, or
      ca_certs_dir argument.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 5bc45c5e77505cc07902e6fbd99a15c9d352ded7)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-urllib3/python-urllib3.mk
+++ package/python-urllib3/python-urllib3.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_URLLIB3_VERSION = 1.24.1
+PYTHON_URLLIB3_VERSION = 1.24.2
 PYTHON_URLLIB3_SOURCE = urllib3-$(PYTHON_URLLIB3_VERSION).tar.gz
-PYTHON_URLLIB3_SITE = https://files.pythonhosted.org/packages/b1/53/37d82ab391393565f2f831b8eedbffd57db5a718216f82f1a8b4d381a1c1
+PYTHON_URLLIB3_SITE = https://files.pythonhosted.org/packages/fd/fa/b21f4f03176463a6cccdb612a5ff71b927e5224e83483012747c12fc5d62
 PYTHON_URLLIB3_LICENSE = MIT
 PYTHON_URLLIB3_LICENSE_FILES = LICENSE.txt
 PYTHON_URLLIB3_SETUP_TYPE = setuptools
