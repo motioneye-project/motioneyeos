b7650ae940ff8f79c78366793b8286ed0e580b6e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-04-25 20:14:37 +0200

    package/openssh: fix build with atomic
    Use pkg-config to retrieve openssl dependencies such as atomic
    
    Fixes:
     - http://autobuild.buildroot.org/results/33d0e56368ab0e74d523be4837824654a4684746
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -8,7 +8,10 @@ OPENSSH_VERSION = 7.9p1
 OPENSSH_SITE = http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 OPENSSH_LICENSE = BSD-3-Clause, BSD-2-Clause, Public Domain
 OPENSSH_LICENSE_FILES = LICENCE
-OPENSSH_CONF_ENV = LD="$(TARGET_CC)" LDFLAGS="$(TARGET_CFLAGS)"
+OPENSSH_CONF_ENV = \
+	LD="$(TARGET_CC)" \
+	LDFLAGS="$(TARGET_CFLAGS)" \
+	LIBS=`$(PKG_CONFIG_HOST_BINARY) --libs openssl`
 OPENSSH_CONF_OPTS = \
 	--sysconfdir=/etc/ssh \
 	--with-default-path=$(BR2_SYSTEM_DEFAULT_PATH) \
@@ -27,7 +30,7 @@ ifeq ($(BR2_TOOLCHAIN_SUPPORTS_PIE),)
 OPENSSH_CONF_OPTS += --without-pie
 endif
 
-OPENSSH_DEPENDENCIES = zlib openssl
+OPENSSH_DEPENDENCIES = host-pkgconf zlib openssl
 
 ifeq ($(BR2_PACKAGE_CRYPTODEV_LINUX),y)
 OPENSSH_DEPENDENCIES += cryptodev-linux
