022d85cce8d83ae380e58b04de00214a06b767ff
Peter Seiderer <ps.report@gmx.net>
2019-04-26 19:32:47 +0200

    package/{mesa3d, mesa3d-headers}: bump version to 19.0.3
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mesa3d-headers/mesa3d-headers.mk
+++ package/mesa3d-headers/mesa3d-headers.mk
@@ -12,7 +12,7 @@ endif
 
 # Not possible to directly refer to mesa3d variables, because of
 # first/second expansion trickery...
-MESA3D_HEADERS_VERSION = 19.0.2
+MESA3D_HEADERS_VERSION = 19.0.3
 MESA3D_HEADERS_SOURCE = mesa-$(MESA3D_HEADERS_VERSION).tar.xz
 MESA3D_HEADERS_SITE = https://mesa.freedesktop.org/archive
 MESA3D_HEADERS_DL_SUBDIR = mesa3d
