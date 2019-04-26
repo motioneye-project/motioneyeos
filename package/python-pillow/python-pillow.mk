2db5052bd54685fb8a130c253129368e2d619593
Angelo Compagnucci <angelo@amarulasolutions.com>
2019-04-26 22:44:48 +0200

    package/python-pillow: bump to version 6.0.0
    This patch bumps python-pillow to version 6.0.0.
    Hash for LICENSE file is changed because of a copyright year update.
    
    Signed-off-by: Angelo Compagnucci <angelo@amarulasolutions.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-pillow/python-pillow.mk
+++ package/python-pillow/python-pillow.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PILLOW_VERSION = 5.2.0
+PYTHON_PILLOW_VERSION = 6.0.0
 PYTHON_PILLOW_SOURCE = Pillow-$(PYTHON_PILLOW_VERSION).tar.gz
-PYTHON_PILLOW_SITE = https://files.pythonhosted.org/packages/d3/c4/b45b9c0d549f482dd072055e2d3ced88f3b977f7b87c7a990228b20e7da1
+PYTHON_PILLOW_SITE = https://files.pythonhosted.org/packages/81/1a/6b2971adc1bca55b9a53ed1efa372acff7e8b9913982a396f3fa046efaf8
 PYTHON_PILLOW_LICENSE = PIL Software License
 PYTHON_PILLOW_LICENSE_FILES = LICENSE
 PYTHON_PILLOW_SETUP_TYPE = setuptools
