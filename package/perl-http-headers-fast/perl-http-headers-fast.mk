1ff12ee6632f041d1f98b80e5bbbd1688a581f37
Francois Perrad <fperrad@gmail.com>
2019-04-27 09:05:18 +0200

    package/perl-http-headers-fast: bump to version 0.22
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/perl-http-headers-fast/perl-http-headers-fast.mk
+++ package/perl-http-headers-fast/perl-http-headers-fast.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_HTTP_HEADERS_FAST_VERSION = 0.21
+PERL_HTTP_HEADERS_FAST_VERSION = 0.22
 PERL_HTTP_HEADERS_FAST_SOURCE = HTTP-Headers-Fast-$(PERL_HTTP_HEADERS_FAST_VERSION).tar.gz
 PERL_HTTP_HEADERS_FAST_SITE = $(BR2_CPAN_MIRROR)/authors/id/T/TO/TOKUHIROM
 PERL_HTTP_HEADERS_FAST_DEPENDENCIES = host-perl-module-build-tiny
