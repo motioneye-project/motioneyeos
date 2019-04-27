f8887825a8b067def73155a25e65e2a401ee52d5
Francois Perrad <fperrad@gmail.com>
2019-04-27 09:05:19 +0200

    package/perl-mail-dkim: bump to version 0.55
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/perl-mail-dkim/perl-mail-dkim.mk
+++ package/perl-mail-dkim/perl-mail-dkim.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_MAIL_DKIM_VERSION = 0.54
+PERL_MAIL_DKIM_VERSION = 0.55
 PERL_MAIL_DKIM_SOURCE = Mail-DKIM-$(PERL_MAIL_DKIM_VERSION).tar.gz
 PERL_MAIL_DKIM_SITE = $(BR2_CPAN_MIRROR)/authors/id/M/MB/MBRADSHAW
 PERL_MAIL_DKIM_DISTNAME = Mail-DKIM
