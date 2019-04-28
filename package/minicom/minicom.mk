04a11ca1b826f55a1e0311d76fd7e2c5003d43c7
Giulio Benetti <giulio.benetti@micronovasrl.com>
2019-04-28 12:09:31 +0200

    package/minicom: bump to version 1e84585e8e7710677b6ff9a70dce5f3ad3e23540
    Remove both patches since they've been upstreamed.
    
    Add host-pkgconf to MINICOM_DEPENDENCIES and remove:
    `MINICOM_CONF_ENV = PKG_CONFIG=/bin/false` since now pkg-config is
    needed to build.
    
    Shortlog follows:
    git shortlog --no-merges  19ab49422f3431102c31fea01549121385113f80..
    Adam Lackorzynski (9):
          Update project URL
          Updated translations
          Updated translations: pl and sv
          Translations: regen update
          Update danish translation
          Update of danish translation
          Update README
          Formatting fix
          More friendly mbslength test
    
    Fabrice Fontaine (1):
          src/Makefile.am: fix ascii-xfr link with lintl
    
    Giulio Benetti (2):
          TODO: remove Utf8 bug.
          sysdep1: check if RS485 is support before setting its parameters
    
    Signed-off-by: Giulio Benetti <giulio.benetti@micronovasrl.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/minicom/minicom.mk
+++ package/minicom/minicom.mk
@@ -4,21 +4,15 @@
 #
 ################################################################################
 
-MINICOM_VERSION = 19ab49422f3431102c31fea01549121385113f80
+MINICOM_VERSION = 1e84585e8e7710677b6ff9a70dce5f3ad3e23540
 MINICOM_SITE = https://salsa.debian.org/minicom-team/minicom.git
 MINICOM_SITE_METHOD = git
 MINICOM_LICENSE = GPL-2.0+
 MINICOM_LICENSE_FILES = COPYING
 MINICOM_AUTORECONF = YES
 
-# pkg-config is only used to check for liblockdev, which we don't have
-# in BR, so instead of adding host-pkgconf as a dependency, simply make
-# sure the host version isn't used so we don't end up with problems if
-# people have liblockdev1-dev installed
-MINICOM_CONF_ENV = PKG_CONFIG=/bin/false
-
 MINICOM_DEPENDENCIES = ncurses $(if $(BR2_ENABLE_LOCALE),,libiconv) \
-	$(TARGET_NLS_DEPENDENCIES)
+	$(TARGET_NLS_DEPENDENCIES) host-pkgconf
 # add host-gettext for AM_ICONV macro
 MINICOM_DEPENDENCIES += host-gettext
 
