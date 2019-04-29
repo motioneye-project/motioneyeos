4f5584af06dddb08acd7a0f290f304d1272805e3
Peter Korsgaard <peter@korsgaard.com>
2019-04-29 23:44:17 +0200

    package/go: bump version to 1.12.4
    Fixes a number of issues discovered since 1.12.1.  From the release notes:
    
    go1.12.2 (released 2019/04/05) includes fixes to the compiler, the go
    command, the runtime, and the doc, net, net/http/httputil, and os packages.
    See the Go 1.12.2 milestone on our issue tracker for details.
    
    go1.12.3 (released 2019/04/08) was accidentally released without its
    intended fix.  It is identical to go1.12.2, except for its version number.
    The intended fix is in go1.12.4.
    
    go1.12.4 (released 2019/04/11) fixes an issue where using the prebuilt
    binary releases on older versions of GNU/Linux led to failures when linking
    programs that used cgo.  Only Linux users who hit this issue need to update.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.12.1
+GO_VERSION = 1.12.4
 GO_SITE = https://storage.googleapis.com/golang
 GO_SOURCE = go$(GO_VERSION).src.tar.gz
 
