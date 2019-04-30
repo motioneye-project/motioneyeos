287819d361a3799dde390112d05e39d6f863ef0a
Adam Duskett <Aduskett@gmail.com>
2019-04-30 09:19:12 -0400

    package/python-werkzeug: bump to version 0.15.2
    Also add hash for the license file, and change the license file to LICENSE.rst
    as LICENSE no longer exists.
    
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-werkzeug/python-werkzeug.mk
+++ package/python-werkzeug/python-werkzeug.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_WERKZEUG_VERSION = 0.12.2
+PYTHON_WERKZEUG_VERSION = 0.15.2
 PYTHON_WERKZEUG_SOURCE = Werkzeug-$(PYTHON_WERKZEUG_VERSION).tar.gz
-PYTHON_WERKZEUG_SITE = https://pypi.python.org/packages/56/41/c095a77eb2dd69bf278dd664a97d3416af04e9ba1a00b8c138f772741d31
+PYTHON_WERKZEUG_SITE = https://files.pythonhosted.org/packages/f3/c8/fa7e1a0283267bee8efa10c665d8dca27e591face7e333c789c85671b3ab
 PYTHON_WERKZEUG_SETUP_TYPE = setuptools
 PYTHON_WERKZEUG_LICENSE = BSD-3-Clause
-PYTHON_WERKZEUG_LICENSE_FILES = LICENSE
+PYTHON_WERKZEUG_LICENSE_FILES = LICENSE.rst
 
 $(eval $(python-package))
