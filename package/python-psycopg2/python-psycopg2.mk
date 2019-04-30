77949d5df6bb496c3330ce4c70347ebbad28dfae
Adam Duskett <Aduskett@gmail.com>
2019-04-30 09:19:14 -0400

    package/python-psycopg2: bump to version 2.8.2
    Also remove upstream patch and change the hash for the license file due to
    url changes.
    
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-psycopg2/python-psycopg2.mk
+++ package/python-psycopg2/python-psycopg2.mk
@@ -4,14 +4,13 @@
 #
 ################################################################################
 
-PYTHON_PSYCOPG2_VERSION = 2.7.7
+PYTHON_PSYCOPG2_VERSION = 2.8.2
 PYTHON_PSYCOPG2_SOURCE = psycopg2-$(PYTHON_PSYCOPG2_VERSION).tar.gz
-PYTHON_PSYCOPG2_SITE = https://files.pythonhosted.org/packages/63/54/c039eb0f46f9a9406b59a638415c2012ad7be9b4b97bfddb1f48c280df3a
+PYTHON_PSYCOPG2_SITE = https://files.pythonhosted.org/packages/23/7e/93c325482c328619870b6cd09370f6dbe1148283daca65115cd63642e60f
 PYTHON_PSYCOPG2_SETUP_TYPE = setuptools
 PYTHON_PSYCOPG2_LICENSE = LGPL-3.0+
 PYTHON_PSYCOPG2_LICENSE_FILES = LICENSE
 PYTHON_PSYCOPG2_DEPENDENCIES = postgresql
-
 # Force psycopg2 to use the Buildroot provided postgresql version
 # instead of the one from the host machine
 PYTHON_PSYCOPG2_BUILD_OPTS = build_ext --pg-config=$(STAGING_DIR)/usr/bin/pg_config
