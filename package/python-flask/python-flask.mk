fb0bb5ed449775a40a4911f75c14c6306a67f261
Adam Duskett <Aduskett@gmail.com>
2019-04-30 09:19:17 -0400

    package/python-flask: bump to version 1.0.2
    Also add hash for the license file.
    
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-flask/python-flask.mk
+++ package/python-flask/python-flask.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FLASK_VERSION = 0.12.2
+PYTHON_FLASK_VERSION = 1.0.2
 PYTHON_FLASK_SOURCE = Flask-$(PYTHON_FLASK_VERSION).tar.gz
-PYTHON_FLASK_SITE = https://pypi.python.org/packages/eb/12/1c7bd06fcbd08ba544f25bf2c6612e305a70ea51ca0eda8007344ec3f123
+PYTHON_FLASK_SITE = https://files.pythonhosted.org/packages/4b/12/c1fbf4971fda0e4de05565694c9f0c92646223cff53f15b6eb248a310a62
 PYTHON_FLASK_SETUP_TYPE = setuptools
 PYTHON_FLASK_LICENSE = BSD-3-Clause
 PYTHON_FLASK_LICENSE_FILES = LICENSE
