4900370d2f1204b3989a22408c01365f9a483d51
Nicola Di Lieto <nicola.dilieto@gmail.com>
2019-05-01 16:12:34 +0200

    package/uacme: new package
    Signed-off-by: Nicola Di Lieto <nicola.dilieto@gmail.com>
    [Thomas:
     - bump to version 1.0.19
     - drop --disable-maintainer-mode and --disable-debug
     - add missing dependency on libcurl
     - drop = in --with-gnutls=]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/uacme/uacme.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# uacme
+#
+################################################################################
+
+UACME_VERSION = 1.0.19
+# Released versions are on branch upstream/latest, tagged as
+# upstream/X.Y.Z Do not use vX.Y.Z tags from master, as they do not
+# include .tarball-version
+UACME_SITE = $(call github,ndilieto,uacme,upstream/$(UACME_VERSION))
+UACME_LICENSE = GPL-3.0+
+UACME_LICENSE_FILES = COPYING
+UACME_DEPENDENCIES = libcurl
+
+ifeq ($(BR2_PACKAGE_MBEDTLS),y)
+UACME_CONF_OPTS += --with-mbedtls
+UACME_DEPENDENCIES += mbedtls
+else
+UACME_CONF_OPTS += --without-mbedtls
+endif
+
+ifeq ($(BR2_PACKAGE_GNUTLS),y)
+UACME_CONF_OPTS += --with-gnutls
+UACME_DEPENDENCIES += gnutls
+else
+UACME_CONF_OPTS += --without-gnutls
+endif
+
+$(eval $(autotools-package))
