8e352c32b0beded97a8a5c1e9edc9d618514ee7b
Peter Seiderer <ps.report@gmx.net>
2019-05-08 19:34:27 +0200

    utils/check-package: warn about utf-8 characters in .mk files
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Tested-by: Titouan Christophe <titouan.christophe@railnova.eu>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- utils/checkpackagelib/lib_mk.py
+++ utils/checkpackagelib/lib_mk.py
@@ -11,6 +11,7 @@ from checkpackagelib.lib import ConsecutiveEmptyLines  # noqa: F401
 from checkpackagelib.lib import EmptyLastLine          # noqa: F401
 from checkpackagelib.lib import NewlineAtEof           # noqa: F401
 from checkpackagelib.lib import TrailingSpace          # noqa: F401
+from checkpackagelib.lib import Utf8Characters         # noqa: F401
 
 # used in more than one check
 start_conditional = ["ifdef", "ifeq", "ifndef", "ifneq"]
