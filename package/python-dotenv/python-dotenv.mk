db9720374f351994313ac06d3b6e1fb2fc336587
Calin Crisan <ccrisan@gmail.com>
2019-05-14 22:42:42 +0300

    python-dotenv: update to 0.10.2

--- package/python-dotenv/python-dotenv.mk
+++ package/python-dotenv/python-dotenv.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_DOTENV_VERSION = 0.10.1
+PYTHON_DOTENV_VERSION = 0.10.2
 PYTHON_DOTENV_SOURCE = python-dotenv-$(PYTHON_DOTENV_VERSION).tar.gz
-PYTHON_DOTENV_SITE = https://files.pythonhosted.org/packages/0f/fe/b0e23db9c6b7dc8c2b21b62990890c85441c95557be1f3f3d5a126ec3009
+PYTHON_DOTENV_SITE = https://files.pythonhosted.org/packages/2d/f0/f89093378e5a7c028d7d618c9ce60a7853a1f36d46ff9e497ba03df102d5
 PYTHON_DOTENV_SETUP_TYPE = setuptools
 PYTHON_DOTENV_LICENSE = Apache-2.0
 PYTHON_DOTENV_LICENSE_FILES = LICENSE.txt
