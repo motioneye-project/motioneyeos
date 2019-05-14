749857d132b8f51cb1deac0531c549ec364da0f1
Calin Crisan <ccrisan@gmail.com>
2019-05-14 22:58:15 +0300

    python-redis: update to 3.2.1

--- package/python-redis/python-redis.mk
+++ package/python-redis/python-redis.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_REDIS_VERSION = 3.0.1
+PYTHON_REDIS_VERSION = 3.2.1
 PYTHON_REDIS_SOURCE = redis-$(PYTHON_REDIS_VERSION).tar.gz
-PYTHON_REDIS_SITE = https://files.pythonhosted.org/packages/4a/1b/9b40393630954b54a4182ca65a9cf80b41803108fcae435ffd6af57af5ae
+PYTHON_REDIS_SITE = https://files.pythonhosted.org/packages/24/d4/06486dee0f66ef8c5080dc576fdfb33131fd2e0be3747f2be4e5634088a2
 PYTHON_REDIS_SETUP_TYPE = setuptools
 PYTHON_REDIS_LICENSE = Apache-2.0
 PYTHON_REDIS_LICENSE_FILES = LICENSE.txt
