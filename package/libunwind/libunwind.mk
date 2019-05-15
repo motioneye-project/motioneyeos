d5bcb30cf910f13f08f4543cfb0d92a5ec103613
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-05-15 22:47:20 +0200

    package/libunwind: add cxx exceptions support
    If C++ is enabled, enable cxx exceptions, so libunwind will implement
    _Unwind_GetIP which is used by mono (which already depends on C++)
    
    Fixes:
     - http://autobuild.buildroot.net/results/dbd64c89815d393a4e28b312d74fd80ee6de92da
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/libunwind/libunwind.mk
+++ package/libunwind/libunwind.mk
@@ -11,7 +11,9 @@ LIBUNWIND_LICENSE_FILES = COPYING
 LIBUNWIND_LICENSE = MIT
 LIBUNWIND_AUTORECONF = YES
 
-LIBUNWIND_CONF_OPTS = --disable-tests
+LIBUNWIND_CONF_OPTS = \
+	--disable-tests \
+	$(if $(BR2_INSTALL_LIBSTDCPP),--enable-cxx-exceptions,--disable-cxx-exceptions)
 
 ifeq ($(BR2_PACKAGE_LIBATOMIC_OPS),y)
 LIBUNWIND_DEPENDENCIES = libatomic_ops
