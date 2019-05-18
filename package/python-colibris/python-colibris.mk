44bd9481858a1624f2fc579a65b11d2a62a42fbd
Calin Crisan <ccrisan@gmail.com>
2019-05-18 16:14:30 +0300

    python-colibris: update to 0.6.2

--- package/python-colibris/python-colibris.mk
+++ package/python-colibris/python-colibris.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_COLIBRIS_VERSION = 0.6.1
+PYTHON_COLIBRIS_VERSION = 0.6.2
 PYTHON_COLIBRIS_SOURCE = colibris-$(PYTHON_COLIBRIS_VERSION).tar.gz
-PYTHON_COLIBRIS_SITE = https://files.pythonhosted.org/packages/61/de/6f76814c12202cceda63d0db0cdc0ada2aede33526023a00ce14f6e5ea6e
+PYTHON_COLIBRIS_SITE = https://files.pythonhosted.org/packages/f6/39/9257111da84ba61f451addc84a03b07b967c398c5eef85b739849ebaccb0
 PYTHON_COLIBRIS_SETUP_TYPE = setuptools
 PYTHON_COLIBRIS_LICENSE = Apache-2.0
 PYTHON_COLIBRIS_LICENSE_FILES = LICENSE.txt
