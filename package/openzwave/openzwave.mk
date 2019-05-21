4d20e251fbc21acc786afc3f82dbb5caf4bd3eac
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-05-21 21:25:15 +0200

    package/openzwave: bump to version 1.6
    - Remove all patches (already in version)
    - udev is an optional dependency (disabled by default) since
      https://github.com/OpenZWave/open-zwave/commit/89d5b3989fc2cfcf4db85ff9ec71e5c930d6fd33
    - Add an upstream patch to fix build
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/openzwave/openzwave.mk
+++ package/openzwave/openzwave.mk
@@ -4,13 +4,12 @@
 #
 ################################################################################
 
-OPENZWAVE_VERSION = V1.5
+OPENZWAVE_VERSION = v1.6
 OPENZWAVE_SITE = $(call github,OpenZWave,open-zwave,$(OPENZWAVE_VERSION))
 OPENZWAVE_LICENSE = LGPL-3.0+, GPL-3.0 (examples), Apache-2.0 (sh2ju.sh)
 OPENZWAVE_LICENSE_FILES = license/license.txt license/lgpl.txt \
 	license/gpl.txt license/Apache-License-2.0.txt
 
-OPENZWAVE_DEPENDENCIES = host-pkgconf udev
 OPENZWAVE_INSTALL_STAGING = YES
 
 # Set instlibdir to install libopenzwave.so* in the correct directory
@@ -30,6 +29,13 @@ OPENZWAVE_MAKE_OPTS = \
 	pkgconfigdir=/usr/lib/pkgconfig \
 	sysconfdir=/etc/openzwave \
 	DOXYGEN=
+
+ifeq ($(BR2_PACKAGE_HAS_UDEV),y)
+OPENZWAVE_DEPENDENCIES += host-pkgconf udev
+OPENZWAVE_MAKE_OPTS += USE_HID=1
+else
+OPENZWAVE_MAKE_OPTS += USE_HID=0
+endif
 
 define OPENZWAVE_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) $(OPENZWAVE_MAKE_OPTS) -C $(@D)
