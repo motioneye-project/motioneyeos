108e9638b67b7e6e219bf52f53cb434dfe870cb8
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-05-24 16:14:10 +0200

    package/openzwave: fix strncat build failure
    Retrieve upstream patch that fixes a build failure caused by
    strncat():
    
    error: 'char* strncat(char*, const char*, size_t)' output may be truncated copying between 0 and 253 bytes from a string of length 253
    
    This patch also allow to disable the use of the bundled tinyxml
    library so use this new option and add a dependency of openzwave on
    tinyxml to use it as a regular external library.
    
    Fixes:
    
      http://autobuild.buildroot.org/results/cbe0671831d874a5b96a944b891fe1eea823667b
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/openzwave/openzwave.mk
+++ package/openzwave/openzwave.mk
@@ -9,6 +9,7 @@ OPENZWAVE_SITE = $(call github,OpenZWave,open-zwave,$(OPENZWAVE_VERSION))
 OPENZWAVE_LICENSE = LGPL-3.0+, GPL-3.0 (examples), Apache-2.0 (sh2ju.sh)
 OPENZWAVE_LICENSE_FILES = license/license.txt license/lgpl.txt \
 	license/gpl.txt license/Apache-License-2.0.txt
+OPENZWAVE_DEPENDENCIES = tinyxml
 
 OPENZWAVE_INSTALL_STAGING = YES
 
@@ -28,7 +29,8 @@ OPENZWAVE_MAKE_OPTS = \
 	instlibdir=/usr/lib \
 	pkgconfigdir=/usr/lib/pkgconfig \
 	sysconfdir=/etc/openzwave \
-	DOXYGEN=
+	DOXYGEN= \
+	USE_BI_TXML=0
 
 ifeq ($(BR2_PACKAGE_HAS_UDEV),y)
 OPENZWAVE_DEPENDENCIES += host-pkgconf udev
