9386d72e742c410b7a7e155dee6542dca2c3f045
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-05-24 15:57:45 +0200

    package/gpsd: bump to version 3.18.1
    - Remove second patch (already in version)
    - Add a patch to fix build without man pages
    - Add a patch to remove C99-style loop
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Reviewed-by: Benoît Thébaudeau <benoit.thebaudeau.dev@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/gpsd/gpsd.mk
+++ package/gpsd/gpsd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GPSD_VERSION = 3.18
+GPSD_VERSION = 3.18.1
 GPSD_SITE = http://download-mirror.savannah.gnu.org/releases/gpsd
 GPSD_LICENSE = BSD-3-Clause
 GPSD_LICENSE_FILES = COPYING
