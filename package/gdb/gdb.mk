540354532c8263c4a953fce383a8413cc53c50eb
Guo Ren <ren_guo@c-sky.com>
2019-05-31 14:39:05 +0800

    package/gdb: add C-SKY specific version
    Add gdb build for C-SKY Architecture:
    
     - Support host-gdb
     - Support gdbserver for target
    
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/gdb/gdb.mk
+++ package/gdb/gdb.mk
@@ -14,6 +14,12 @@ GDB_SOURCE = gdb-$(GDB_VERSION).tar.gz
 GDB_FROM_GIT = y
 endif
 
+ifeq ($(BR2_csky),y)
+GDB_SITE = $(call github,c-sky,binutils-gdb,$(GDB_VERSION))
+GDB_SOURCE = gdb-$(GDB_VERSION).tar.gz
+GDB_FROM_GIT = y
+endif
+
 GDB_LICENSE = GPL-2.0+, LGPL-2.0+, GPL-3.0+, LGPL-3.0+
 GDB_LICENSE_FILES = COPYING COPYING.LIB COPYING3 COPYING3.LIB
 
