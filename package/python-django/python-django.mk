426084e25f1ab5c337adceb4bea6352314ff34c7
Peter Korsgaard <peter@korsgaard.com>
2019-06-05 19:23:07 +0200

    package/python-django: security bump to version 2.1.9
    Fixes the following security issues:
    
    CVE-2019-12308: AdminURLFieldWidget XSS¶
    
    The clickable "Current URL" link generated by AdminURLFieldWidget displayed
    the provided value without validating it as a safe URL.  Thus, an
    unvalidated value stored in the database, or a value provided as a URL query
    parameter payload, could result in an clickable JavaScript link.
    
    AdminURLFieldWidget now validates the provided value using URLValidator
    before displaying the clickable link.  You may customize the validator by
    passing a validator_class kwarg to AdminURLFieldWidget.__init__(), e.g.
    when using formfield_overrides.
    
    Patched bundled jQuery for CVE-2019-11358: Prototype pollution¶
    
    jQuery before 3.4.0, mishandles jQuery.extend(true, {}, ...) because of
    Object.prototype pollution.  If an unsanitized source object contained an
    enumerable __proto__ property, it could extend the native Object.prototype.
    
    The bundled version of jQuery used by the Django admin has been patched to
    allow for the select2 library’s use of jQuery.extend().
    
    For more details, see the release notes:
    https://docs.djangoproject.com/en/dev/releases/2.1.9/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 2.1.7
+PYTHON_DJANGO_VERSION = 2.1.9
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/7e/ae/29c28f6afddae0e305326078f31372f03d7f2e6d6210c9963843196ce67e
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/c1/b3/3cdc60dc2e3c11236539f9470e42c5075a2e9c9f4885f5b4b912e9f19992
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_SETUP_TYPE = setuptools
