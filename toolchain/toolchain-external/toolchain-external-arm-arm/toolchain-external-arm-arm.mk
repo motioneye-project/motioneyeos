12258ec11a8493ea9b74d066f36caf1112632b0b
Romain Naour <romain.naour@gmail.com>
2019-06-05 13:55:30 +0200

    toolchain-external: update Arm ARM toolchain 8.3-2019.03
    Update to gcc 8.3, gdb 8.2, binutils 2.32. Revert to linux kernel
    headers 4.19 instead of 5.1-rc1 [1].
    
    See "Release Note":
    https://developer.arm.com/open-source/gnu-toolchain/gnu-a/downloads#
    
    Tested with qemu_arm_vexpress_defconfig.
    
    [1] https://bugs.linaro.org/show_bug.cgi?id=4297
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- toolchain/toolchain-external/toolchain-external-arm-arm/toolchain-external-arm-arm.mk
+++ toolchain/toolchain-external/toolchain-external-arm-arm/toolchain-external-arm-arm.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION = 2018.11
-TOOLCHAIN_EXTERNAL_ARM_ARM_SITE = https://developer.arm.com/-/media/Files/downloads/gnu-a/8.2-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)
+TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION = 2019.03
+TOOLCHAIN_EXTERNAL_ARM_ARM_SITE = https://developer.arm.com/-/media/Files/downloads/gnu-a/8.3-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)/binrel
 
-TOOLCHAIN_EXTERNAL_ARM_ARM_SOURCE = gcc-arm-8.2-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)-x86_64-arm-linux-gnueabihf.tar.xz
+TOOLCHAIN_EXTERNAL_ARM_ARM_SOURCE = gcc-arm-8.3-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)-x86_64-arm-linux-gnueabihf.tar.xz
 
 $(eval $(toolchain-external-package))
