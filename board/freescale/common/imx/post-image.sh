cc1761790fdef8802a70f83c889f47193a3e0ed0
Joris Offouga <offougajoris@gmail.com>
2019-06-10 21:41:42 +0200

    boards/freescale/common: Introduce genimage.cfg.template_no_boot_part_spl
    Modern U-Boot assumes a layout where the first partition is an ext2 and
    the kernel and dtb can be found in /boot. In other words, a layout
    without boot partition.
    
    Add a genimage.cfg template for the freescale defconfigs with such a
    layout.
    
    For now, only the layout with SPL+u-boot.img is added. It's unlikely that
    the u-boot.imx approach will be used.
    
    Signed-off-by: Joris Offouga <offougajoris@gmail.com>
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Tested-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- board/freescale/common/imx/post-image.sh
+++ board/freescale/common/imx/post-image.sh
@@ -38,6 +38,10 @@ genimage_type()
 		echo "genimage.cfg.template_imx8"
 	elif grep -Eq "^BR2_PACKAGE_FREESCALE_IMX_PLATFORM_IMX8X=y$" ${BR2_CONFIG}; then
 		echo "genimage.cfg.template_imx8"
+	elif grep -Eq "^BR2_LINUX_KERNEL_INSTALL_TARGET=y$" ${BR2_CONFIG}; then
+		if grep -Eq "^BR2_TARGET_UBOOT_SPL=y$" ${BR2_CONFIG}; then
+		    echo "genimage.cfg.template_no_boot_part_spl"
+		fi
 	elif grep -Eq "^BR2_TARGET_UBOOT_SPL=y$" ${BR2_CONFIG}; then
 		echo "genimage.cfg.template_spl"
 	else
