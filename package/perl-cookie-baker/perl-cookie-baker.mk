ccdb0e88dd4ddca12f225c52e818b5ad7c506ee4
Francois Perrad <fperrad@gmail.com>
2019-06-10 10:33:20 +0200

    package/perl-cookie-baker: bump to version 0.11
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/perl-cookie-baker/perl-cookie-baker.mk
+++ package/perl-cookie-baker/perl-cookie-baker.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_COOKIE_BAKER_VERSION = 0.10
+PERL_COOKIE_BAKER_VERSION = 0.11
 PERL_COOKIE_BAKER_SOURCE = Cookie-Baker-$(PERL_COOKIE_BAKER_VERSION).tar.gz
 PERL_COOKIE_BAKER_SITE = $(BR2_CPAN_MIRROR)/authors/id/K/KA/KAZEBURO
 PERL_COOKIE_BAKER_DEPENDENCIES = host-perl-module-build-tiny
