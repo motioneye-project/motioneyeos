e2d299485ee6997e32cbc1f11989925e22b8a9e1
Pierre-Jean Texier <pjtexier@koncepto.io>
2019-06-10 21:55:36 +0200

    package/swupdate: bump to version 2019.04
    New features in this release:
     - Improved documentation
     - delta updates based on rdiff library
     - support for libubootenv
     - dry-run option
     - CA certificates for signed images
     - Fix security leak in parser
    
    This commit also:
     - introduce BR2_PACKAGE_LIBRSYNC for 'rdiff' Handler. No HAVE_* is
       needed, it just declares the functions locally and links with
       -lrsync.
     - introduce BR2_PACKAGE_LIBUBOOTENV
     - drop upstreamed patch
     - backport upstream patches (important fix)
     - regenerate the default swupdate.config. Now CONFIG_GUNZIP is always
       enabled because gunzip is provided by the default busybox config.
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    [Arnout: don't mention ZLIB in help text for libubootenv, since it is
     select'ed by libubootenv.]
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/swupdate/swupdate.mk
+++ package/swupdate/swupdate.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SWUPDATE_VERSION = 2018.11
+SWUPDATE_VERSION = 2019.04
 SWUPDATE_SITE = $(call github,sbabic,swupdate,$(SWUPDATE_VERSION))
 SWUPDATE_LICENSE = GPL-2.0+ with OpenSSL exception, LGPL-2.1+, MIT
 SWUPDATE_LICENSE_FILES = Licenses/Exceptions Licenses/gpl-2.0.txt \
@@ -77,6 +77,9 @@ endif
 ifeq ($(BR2_PACKAGE_UBOOT_TOOLS),y)
 SWUPDATE_DEPENDENCIES += uboot-tools
 SWUPDATE_MAKE_ENV += HAVE_LIBUBOOTENV=y
+else ifeq ($(BR2_PACKAGE_LIBUBOOTENV),y)
+SWUPDATE_DEPENDENCIES += libubootenv
+SWUPDATE_MAKE_ENV += HAVE_LIBUBOOTENV=y
 else
 SWUPDATE_MAKE_ENV += HAVE_LIBUBOOTENV=n
 endif
@@ -93,6 +96,10 @@ SWUPDATE_DEPENDENCIES += zlib
 SWUPDATE_MAKE_ENV += HAVE_ZLIB=y
 else
 SWUPDATE_MAKE_ENV += HAVE_ZLIB=n
+endif
+
+ifeq ($(BR2_PACKAGE_LIBRSYNC),y)
+SWUPDATE_DEPENDENCIES += librsync
 endif
 
 SWUPDATE_BUILD_CONFIG = $(@D)/.config
