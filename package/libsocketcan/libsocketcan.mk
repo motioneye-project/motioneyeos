315580e8e734335a6d3bb8592bce7eaa8175abcf
Pierre-Jean Texier <pjtexier@koncepto.io>
2019-06-16 16:05:40 +0200

    package/libsocketcan: add license file details
    Add LICENSE file
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libsocketcan/libsocketcan.mk
+++ package/libsocketcan/libsocketcan.mk
@@ -9,5 +9,6 @@ LIBSOCKETCAN_SITE = http://www.pengutronix.de/software/libsocketcan/download
 LIBSOCKETCAN_SOURCE = libsocketcan-$(LIBSOCKETCAN_VERSION).tar.bz2
 LIBSOCKETCAN_INSTALL_STAGING = YES
 LIBSOCKETCAN_LICENSE = LGPL-2.1+
+LIBSOCKETCAN_LICENSE_FILES = LICENSE
 
 $(eval $(autotools-package))
