46106db4562465844995df4d9d0c96f29e9d536b
James Hilliard <james.hilliard1@gmail.com>
2019-06-17 09:17:20 -0600

    package/meson: bump to version 0.51.0
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/meson/meson.mk
+++ package/meson/meson.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MESON_VERSION = 0.50.1
+MESON_VERSION = 0.51.0
 MESON_SITE = https://github.com/mesonbuild/meson/releases/download/$(MESON_VERSION)
 MESON_LICENSE = Apache-2.0
 MESON_LICENSE_FILES = COPYING
