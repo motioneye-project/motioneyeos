d4478438a0d93104d51a039b4ea0cbb4f1c950c7
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-06-22 20:21:12 +0200

    package/x11r7/xapp_xcalc: bump version to 1.1.0
    Switched _SITE to https.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/x11r7/xapp_xcalc/xapp_xcalc.mk
+++ package/x11r7/xapp_xcalc/xapp_xcalc.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XCALC_VERSION = 1.0.7
+XAPP_XCALC_VERSION = 1.1.0
 XAPP_XCALC_SOURCE = xcalc-$(XAPP_XCALC_VERSION).tar.bz2
-XAPP_XCALC_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XCALC_SITE = https://xorg.freedesktop.org/releases/individual/app
 XAPP_XCALC_LICENSE = MIT
 XAPP_XCALC_LICENSE_FILES = COPYING
 XAPP_XCALC_INSTALL_TARGET_OPTS = DESTDIR=$(TARGET_DIR) install-exec install-data
