85d91c343bb4455f284d7e9a8b71a8e324e98f3e
Pierre-Jean Texier <pjtexier@koncepto.io>
2019-06-22 13:55:58 +0200

    package/alsa-utils: bump to version 1.1.9
    Also remove the patch that have been upstreamed
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/alsa-utils/alsa-utils.mk
+++ package/alsa-utils/alsa-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ALSA_UTILS_VERSION = 1.1.7
+ALSA_UTILS_VERSION = 1.1.9
 ALSA_UTILS_SOURCE = alsa-utils-$(ALSA_UTILS_VERSION).tar.bz2
 ALSA_UTILS_SITE = ftp://ftp.alsa-project.org/pub/utils
 ALSA_UTILS_LICENSE = GPL-2.0
