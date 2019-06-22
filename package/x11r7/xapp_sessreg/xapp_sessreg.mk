433db5d36a175e5c4ad0a520e32c3e60fb57289c
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-06-22 20:25:36 +0200

    package/x11r7/xapp_sessreg: bump version to 1.1.2
    Removed patch which was applied upstream:
    https://gitlab.freedesktop.org/xorg/app/sessreg/commit/5b403d124c32c20fddc73bc5ae3c7e1febdf1bc4
    
    Added all hashes provided by upstream, added license hash.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/x11r7/xapp_sessreg/xapp_sessreg.mk
+++ package/x11r7/xapp_sessreg/xapp_sessreg.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_SESSREG_VERSION = 1.1.1
+XAPP_SESSREG_VERSION = 1.1.2
 XAPP_SESSREG_SOURCE = sessreg-$(XAPP_SESSREG_VERSION).tar.bz2
 XAPP_SESSREG_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_SESSREG_LICENSE = MIT
