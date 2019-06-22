ee59023794f5540e5c70bca944ad25bc79eabb0c
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-06-22 15:33:10 +0200

    package/libvncserver: bump to version 0.9.12
    - Remove patch (already in version)
    - autotools is no more available, switch to cmake
    - Disable ffmpeg (used for example)
    - Add LZO dependency (to avoid using internal LZO) through the new
      WITH_LZO option added by
      https://github.com/LibVNC/libvncserver/commit/139da17b6ed0ccd1acd824a87972182834671f92
    - Add hash for license file
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libvncserver/libvncserver.mk
+++ package/libvncserver/libvncserver.mk
@@ -4,66 +4,67 @@
 #
 ################################################################################
 
-LIBVNCSERVER_VERSION = 0.9.11
+LIBVNCSERVER_VERSION = 0.9.12
 LIBVNCSERVER_SOURCE = LibVNCServer-$(LIBVNCSERVER_VERSION).tar.gz
 LIBVNCSERVER_SITE = https://github.com/LibVNC/libvncserver/archive
 LIBVNCSERVER_LICENSE = GPL-2.0+
 LIBVNCSERVER_LICENSE_FILES = COPYING
 LIBVNCSERVER_INSTALL_STAGING = YES
-LIBVNCSERVER_CONFIG_SCRIPTS = libvncserver-config
-LIBVNCSERVER_DEPENDENCIES = host-pkgconf
-
-# Upstream decided to remove generated autotools files from the
-# tarball, so we need to generate them.
-LIBVNCSERVER_AUTORECONF = YES
-
-# libvncserver does not get along with newer libva versions
-# https://github.com/LibVNC/libvncserver/issues/11
-LIBVNCSERVER_CONF_OPTS += --without-libva
+LIBVNCSERVER_DEPENDENCIES = host-pkgconf lzo
+LIBVNCSERVER_CONF_OPTS = -DWITH_LZO=ON
 
 # only used for examples
-LIBVNCSERVER_CONF_OPTS += --with-sdl-config=/bin/false
+LIBVNCSERVER_CONF_OPTS += \
+	-DWITH_FFMPEG=OFF \
+	-DWITH_SDL=OFF
 
 ifneq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
-LIBVNCSERVER_CONF_OPTS += --without-pthread
+LIBVNCSERVER_CONF_OPTS += -DWITH_THREADS=ON
+else
+LIBVNCSERVER_CONF_OPTS += -DWITH_THREADS=OFF
 endif
 
 # openssl supports needs NPTL thread support
 ifeq ($(BR2_PACKAGE_OPENSSL)$(BR2_TOOLCHAIN_HAS_THREADS_NPTL),yy)
+LIBVNCSERVER_CONF_OPTS += -DWITH_OPENSSL=ON
 LIBVNCSERVER_DEPENDENCIES += openssl
 else
-LIBVNCSERVER_CONF_OPTS += --without-crypto --without-ssl
+LIBVNCSERVER_CONF_OPTS += -DWITH_OPENSSL=OFF
 endif
 
 ifeq ($(BR2_PACKAGE_LIBGCRYPT),y)
-LIBVNCSERVER_CONF_ENV += LIBGCRYPT_CONFIG=$(STAGING_DIR)/usr/bin/libgcrypt-config
+LIBVNCSERVER_CONF_OPTS += -DWITH_GCRYPT=ON
 LIBVNCSERVER_DEPENDENCIES += libgcrypt
 else
-LIBVNCSERVER_CONF_OPTS += --without-gcrypt
+LIBVNCSERVER_CONF_OPTS += -DWITH_GCRYPT=OFF
 endif
 
 ifeq ($(BR2_PACKAGE_GNUTLS)$(BR2_PACKAGE_LIBGCRYPT),yy)
-LIBVNCSERVER_DEPENDENCIES += gnutls host-pkgconf
+LIBVNCSERVER_CONF_OPTS += -DWITH_GNUTLS=ON
+LIBVNCSERVER_DEPENDENCIES += gnutls
 else
-LIBVNCSERVER_CONF_OPTS += --without-gnutls
+LIBVNCSERVER_CONF_OPTS += -DWITH_GNUTLS=OFF
 endif
 
 ifeq ($(BR2_PACKAGE_JPEG),y)
+LIBVNCSERVER_CONF_OPTS += -DWITH_JPEG=ON
 LIBVNCSERVER_DEPENDENCIES += jpeg
 else
-LIBVNCSERVER_CONF_OPTS += --without-jpeg
+LIBVNCSERVER_CONF_OPTS += -DWITH_JPEG=OFF
 endif
 
 ifeq ($(BR2_PACKAGE_LIBPNG),y)
+LIBVNCSERVER_CONF_OPTS += -DWITH_PNG=ON
 LIBVNCSERVER_DEPENDENCIES += libpng
 else
-LIBVNCSERVER_CONF_OPTS += --without-png
+LIBVNCSERVER_CONF_OPTS += -DWITH_PNG=OFF
 endif
 
 ifeq ($(BR2_PACKAGE_ZLIB),y)
+LIBVNCSERVER_CONF_OPTS += -DWITH_ZLIB=ON
 LIBVNCSERVER_DEPENDENCIES += zlib
 else
-LIBVNCSERVER_CONF_OPTS += --without-zlib
+LIBVNCSERVER_CONF_OPTS += -DWITH_ZLIB=OFF
 endif
 
-$(eval $(autotools-package))
+$(eval $(cmake-package))
