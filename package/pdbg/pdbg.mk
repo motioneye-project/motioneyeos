aa4474e999295c55744243379009621fcb06ae75
Joel Stanley <joel@jms.id.au>
2019-06-25 11:23:40 +0930

    pdbg: Bump version to v2.2
    Upstream changes:
    
     - libpdbg API clean-up
     - Path based targetting
     - POWER8 sreset fixes
     - gdbserver
    
    This also adds the v back to the version string that pdbg's build system
    uses, following the recent buildroot changes to remove "v" from package
    versions.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/pdbg/pdbg.mk
+++ package/pdbg/pdbg.mk
@@ -4,14 +4,14 @@
 #
 ################################################################################
 
-PDBG_VERSION = 2.0
+PDBG_VERSION = 2.2
 PDBG_SITE = $(call github,open-power,pdbg,v$(PDBG_VERSION))
 PDBG_LICENSE = Apache-2.0
 PDBG_LICENSE_FILES = COPYING
 PDBG_AUTORECONF = YES
 PDBG_DEPENDENCIES = host-dtc
 
-PDBG_MAKE_OPTS = "GIT_SHA1=\"$(PDBG_VERSION)\""
+PDBG_MAKE_OPTS = "GIT_SHA1=\"v$(PDBG_VERSION)\""
 
 define PDBG_PATCH_M4
 	mkdir -p $(@D)/m4
