2716244b340a87186a8fd6e89cd7f18197ba3cdb
Giulio Benetti <giulio.benetti@micronovasrl.com>
2019-06-26 14:58:55 +0200

    package/libnss: re-enable package on microblaze
    With Microblaze Gcc version < 8.x the build hangs due to bug 85862:
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=85862
    To avoid this, the libnss package has a !BR2_TOOLCHAIN_HAS_GCC_BUG_85862
    dependency. However, gcc bug 85862 only triggers when optimization is
    enabled, so we can work around the issue by passing -O0, which is what
    we do in other Buildroot packages to work around this bug.
    
    Signed-off-by: Giulio Benetti <giulio.benetti@micronovasrl.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libnss/libnss.mk
+++ package/libnss/libnss.mk
@@ -13,9 +13,15 @@ LIBNSS_DEPENDENCIES = libnspr sqlite zlib
 LIBNSS_LICENSE = MPL-2.0
 LIBNSS_LICENSE_FILES = nss/COPYING
 
+LIBNSS_CFLAGS = $(TARGET_CFLAGS)
+
+ifeq ($(BR2_TOOLCHAIN_HAS_GCC_BUG_85862),y)
+LIBNSS_CFLAGS += -O0
+endif
+
 # Need to pass down TARGET_CFLAGS and TARGET_LDFLAGS
 define LIBNSS_FIXUP_LINUX_MK
-	echo 'OS_CFLAGS += $(TARGET_CFLAGS)' >> $(@D)/nss/coreconf/Linux.mk
+	echo 'OS_CFLAGS += $(LIBNSS_CFLAGS)' >> $(@D)/nss/coreconf/Linux.mk
 	echo 'LDFLAGS += $(TARGET_LDFLAGS)' >> $(@D)/nss/coreconf/Linux.mk
 endef
 
