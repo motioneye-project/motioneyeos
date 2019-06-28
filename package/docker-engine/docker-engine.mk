13cf6f0c0bf0df313712aca8e4197ce96e29de89
Peter Korsgaard <peter@korsgaard.com>
2019-06-28 08:32:45 +0200

    package/docker-engine: security bump to version 18.09.7
    Fixes CVE-2018-15664: API endpoints behind the 'docker cp' command are
    vulnerable to a symlink-exchange attack with Directory Traversal, giving
    attackers arbitrary read-write access to the host filesystem with root
    privileges, because daemon/archive.go does not do archive operations on a
    frozen filesystem (or from within a chroot).
    
    And includes additional post-18.09.6 fixes:
    
    Builder
    - Fixed a panic error when building dockerfiles that contain only comments.
      moby/moby#38487
    - Added a workaround for GCR authentication issue. moby/moby#38246
    - Builder-next: Fixed a bug in the GCR token cache implementation
      workaround.  moby/moby#39183
    
    Runtime
    - Added performance optimizations in aufs and layer store that helps in
      massively parallel container creation and removal.  moby/moby#39107,
      moby/moby#39135
    - daemon: fixed a mirrors validation issue. moby/moby#38991
    - Docker no longer supports sorting UID and GID ranges in ID maps.
      moby/moby#39288
    
    Logging
    - Added a fix that now allows large log lines for logger plugins.
      moby/moby#39038
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/docker-engine/docker-engine.mk
+++ package/docker-engine/docker-engine.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOCKER_ENGINE_VERSION = 18.09.6
+DOCKER_ENGINE_VERSION = 18.09.7
 DOCKER_ENGINE_SITE = $(call github,docker,engine,v$(DOCKER_ENGINE_VERSION))
 
 DOCKER_ENGINE_LICENSE = Apache-2.0
