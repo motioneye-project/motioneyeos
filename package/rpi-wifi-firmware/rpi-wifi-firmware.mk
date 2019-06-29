645f434169e22723c67ed3fc20da7099adefc9ec
Michael Cullen <michael@michaelcullen.name>
2019-06-29 19:32:40 +0100

    package/rpi-wifi-firmware: bump to the latest version
    Includes the newest firmware as shipped by Raspbian Buster,
    the Raspbian release designed for the Pi4
    
    Signed-off-by: Michael Cullen <michael@michaelcullen.name>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rpi-wifi-firmware/rpi-wifi-firmware.mk
+++ package/rpi-wifi-firmware/rpi-wifi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_WIFI_FIRMWARE_VERSION = 8c1e2bff1da9850f68efcfff3da5d939ec27a2ee
+RPI_WIFI_FIRMWARE_VERSION = 688531da4bcf802a814d9cb0c8b6d62e3b8a3327
 RPI_WIFI_FIRMWARE_SITE = $(call github,LibreELEC,brcmfmac_sdio-firmware-rpi,$(RPI_WIFI_FIRMWARE_VERSION))
 RPI_WIFI_FIRMWARE_LICENSE = PROPRIETARY
 RPI_WIFI_FIRMWARE_LICENSE_FILES = LICENCE.broadcom_bcm43xx
