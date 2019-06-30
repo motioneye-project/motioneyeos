5d504dad2fa500b69e373a7cc20426d82cfeedc8
Pierre-Jean Texier <pjtexier@koncepto.io>
2019-06-30 19:58:45 +0200

    package/python-scandir: bump to version 1.10.0
    See https://github.com/benhoyt/scandir/releases
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-scandir/python-scandir.mk
+++ package/python-scandir/python-scandir.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SCANDIR_VERSION = 1.9.0
+PYTHON_SCANDIR_VERSION = 1.10.0
 PYTHON_SCANDIR_SOURCE = scandir-$(PYTHON_SCANDIR_VERSION).tar.gz
-PYTHON_SCANDIR_SITE = https://files.pythonhosted.org/packages/16/2a/557af1181e6b4e30254d5a6163b18f5053791ca66e251e77ab08887e8fe3
+PYTHON_SCANDIR_SITE = https://files.pythonhosted.org/packages/df/f5/9c052db7bd54d0cbf1bc0bb6554362bba1012d03e5888950a4f5c5dadc4e
 PYTHON_SCANDIR_LICENSE = BSD-3-Clause
 PYTHON_SCANDIR_LICENSE_FILES = LICENSE.txt
 PYTHON_SCANDIR_SETUP_TYPE = setuptools
