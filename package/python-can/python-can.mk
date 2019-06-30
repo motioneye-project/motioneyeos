b1b489fe74f2e5bd7cecc886d6dfde131296578f
Pierre-Jean Texier <pjtexier@koncepto.io>
2019-06-30 19:56:31 +0200

    package/python-can: bump to version 3.3.0
    See https://github.com/hardbyte/python-can/releases
    
    Also add a patch removing the pytest-runner dependency.
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-can/python-can.mk
+++ package/python-can/python-can.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_CAN_VERSION = 3.1.1
-PYTHON_CAN_SITE = https://files.pythonhosted.org/packages/c1/b0/72d7ed7840692ace5b55dd1155409cd86fd83cee4ded75e3d1102c08750a
+PYTHON_CAN_VERSION = 3.3.0
+PYTHON_CAN_SITE = https://files.pythonhosted.org/packages/9c/1e/a10b6f038a4a4bdd89d13ecc5fba36b71ab716fbd9eae9feda1d975d9504
 PYTHON_CAN_SETUP_TYPE = setuptools
 PYTHON_CAN_LICENSE = LGPL-3.0
 PYTHON_CAN_LICENSE_FILES = LICENSE.txt
