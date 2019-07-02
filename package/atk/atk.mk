c0cd5b51f22da22b077be1efbece56a4d339de40
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-07-02 21:59:14 +0200

    package/atk: bump to version 2.33.3
    - Disable introspection as otherwise build will fail since
      https://gitlab.gnome.org/GNOME/atk/commit/db2d7a6d3c45fe57420d5e5aa8d8509c52943e6e
    - Remove patch (already in version)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/atk/atk.mk
+++ package/atk/atk.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-ATK_VERSION_MAJOR = 2.30
-ATK_VERSION = $(ATK_VERSION_MAJOR).0
+ATK_VERSION_MAJOR = 2.33
+ATK_VERSION = $(ATK_VERSION_MAJOR).3
 ATK_SOURCE = atk-$(ATK_VERSION).tar.xz
 ATK_SITE = http://ftp.gnome.org/pub/gnome/sources/atk/$(ATK_VERSION_MAJOR)
 ATK_LICENSE = LGPL-2.0+
@@ -13,6 +13,7 @@ ATK_LICENSE_FILES = COPYING
 ATK_INSTALL_STAGING = YES
 ATK_DEPENDENCIES = libglib2 $(TARGET_NLS_DEPENDENCIES)
 
+ATK_CONF_OPTS = -Dintrospection=false
 ATK_LDFLAGS = $(TARGET_LDFLAGS) $(TARGET_NLS_LIBS)
 
 $(eval $(meson-package))
