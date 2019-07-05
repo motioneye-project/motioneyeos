ed3eef6bb49270de619e5a86a249fdd8724c40cd
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-07-05 20:35:40 +0200

    package/glib-networking: bump to version 2.61.1
    - gnutls can be made optional again since
      https://gitlab.gnome.org/GNOME/glib-networking/commit/14a3138a6d242238f61ce83018a806e4358961de
    - add openssl optional dependency
    - pkcs option has been removed since
      https://gitlab.gnome.org/GNOME/glib-networking/commit/4d6caa033051f55a157d8b7fb9debdc2fa7e561e
    - use new options, see
      https://gitlab.gnome.org/GNOME/glib-networking/commit/8629f3d5a58bd12d2b6818b6ca8ff1231375a2fb
    - fix build failure with gnutls
    
    Fixes:
     - http://autobuild.buildroot.org/results/e233f9a5693e18bee90669910007e122857c7f77
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/glib-networking/glib-networking.mk
+++ package/glib-networking/glib-networking.mk
@@ -4,25 +4,37 @@
 #
 ################################################################################
 
-GLIB_NETWORKING_VERSION_MAJOR = 2.58
-GLIB_NETWORKING_VERSION = $(GLIB_NETWORKING_VERSION_MAJOR).0
+GLIB_NETWORKING_VERSION_MAJOR = 2.61
+GLIB_NETWORKING_VERSION = $(GLIB_NETWORKING_VERSION_MAJOR).1
 GLIB_NETWORKING_SITE = http://ftp.gnome.org/pub/gnome/sources/glib-networking/$(GLIB_NETWORKING_VERSION_MAJOR)
 GLIB_NETWORKING_SOURCE = glib-networking-$(GLIB_NETWORKING_VERSION).tar.xz
 GLIB_NETWORKING_INSTALL_STAGING = YES
 GLIB_NETWORKING_DEPENDENCIES = \
 	$(TARGET_NLS_DEPENDENCIES) \
 	host-pkgconf \
-	libglib2 \
-	gnutls
+	libglib2
 
 GLIB_NETWORKING_CONF_OPTS = \
-	-Dlibproxy_support=false \
-	-Dgnome_proxy_support=false \
-	-Dpkcs11_support=false
+	-Dlibproxy=disabled \
+	-Dgnome_proxy=disabled
 
 GLIB_NETWORKING_LICENSE = LGPL-2.0+
 GLIB_NETWORKING_LICENSE_FILES = COPYING
 GLIB_NETWORKING_INSTALL_TARGET_OPTS = DESTDIR=$(TARGET_DIR) \
 	GIO_MODULE_DIR=/usr/lib/gio/modules install
+
+ifeq ($(BR2_PACKAGE_GNUTLS),y)
+GLIB_NETWORKING_DEPENDENCIES += gnutls
+GLIB_NETWORKING_CONF_OPTS += -Dgnutls=enabled
+else
+GLIB_NETWORKING_CONF_OPTS += -Dgnutls=disabled
+endif
+
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+GLIB_NETWORKING_DEPENDENCIES += openssl
+GLIB_NETWORKING_CONF_OPTS += -Dopenssl=enabled
+else
+GLIB_NETWORKING_CONF_OPTS += -Dopenssl=disabled
+endif
 
 $(eval $(meson-package))
