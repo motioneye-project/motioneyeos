cc2a566f71c409886aa8cad4115061821045825c
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-07-07 13:59:07 +0200

    package/libcdio: bump version to 2.1.0
    Upstream does not provide the .gz tarball anymore, switch to bz2.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libcdio/libcdio.mk
+++ package/libcdio/libcdio.mk
@@ -4,7 +4,8 @@
 #
 ################################################################################
 
-LIBCDIO_VERSION = 2.0.0
+LIBCDIO_VERSION = 2.1.0
+LIBCDIO_SOURCE = libcdio-$(LIBCDIO_VERSION).tar.bz2
 LIBCDIO_SITE = $(BR2_GNU_MIRROR)/libcdio
 LIBCDIO_INSTALL_STAGING = YES
 LIBCDIO_LICENSE = GPL-3.0+
