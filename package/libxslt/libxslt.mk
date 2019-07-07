655e9f0699e1a3dd04746704cf5e0a32a18b150b
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-07-07 13:51:51 +0200

    package/libxslt: bump version to 1.1.33
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libxslt/libxslt.mk
+++ package/libxslt/libxslt.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBXSLT_VERSION = 1.1.32
+LIBXSLT_VERSION = 1.1.33
 LIBXSLT_SITE = http://xmlsoft.org/sources
 LIBXSLT_INSTALL_STAGING = YES
 LIBXSLT_LICENSE = MIT
