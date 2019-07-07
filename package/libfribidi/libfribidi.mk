60c902afbac0414aea636c1a580fbc48f7e1a39f
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-07-07 14:12:32 +0200

    package/libfribidi: bump version to 1.0.5
    Added license hash, switched _SITE to github.
    
    Removed --with-glib=no, glib support was removed upstream:
    https://github.com/fribidi/fribidi/commit/03ba9fc5358bb3dd78c4e8708abefaad106475d0
    
    Autoreconf is not needed anymore after upstream fixed
    https://github.com/fribidi/fribidi/issues/35
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libfribidi/libfribidi.mk
+++ package/libfribidi/libfribidi.mk
@@ -4,19 +4,12 @@
 #
 ################################################################################
 
-LIBFRIBIDI_VERSION = 0.19.7
+LIBFRIBIDI_VERSION = 1.0.5
 LIBFRIBIDI_SOURCE = fribidi-$(LIBFRIBIDI_VERSION).tar.bz2
-LIBFRIBIDI_SITE = http://www.fribidi.org/download
+LIBFRIBIDI_SITE = https://github.com/fribidi/fribidi/releases/download/v$(LIBFRIBIDI_VERSION)
 LIBFRIBIDI_LICENSE = LGPL-2.1+
 LIBFRIBIDI_LICENSE_FILES = COPYING
 LIBFRIBIDI_INSTALL_STAGING = YES
-# Ships a beta libtool version hence our patch doesn't apply.
-# Run autoreconf to regenerate ltmain.sh.
-LIBFRIBIDI_AUTORECONF = YES
 LIBFRIBIDI_DEPENDENCIES = host-pkgconf
-# libglib2 dependency causes a build failure, and this optional
-# dependency is going to be removed upstream, see
-# https://github.com/behdad/fribidi/pull/14
-LIBFRIBIDI_CONF_OPTS = --with-glib=no
 
 $(eval $(autotools-package))
