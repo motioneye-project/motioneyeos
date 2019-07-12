af252509d35b02db06055f671d03a3dd89070de7
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-07-12 21:37:15 +0200

    package/x11r7/xapp_xauth: bump version to 1.1
    Added all hashes provided by upstream, added license hash.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xapp_xauth/xapp_xauth.mk
+++ package/x11r7/xapp_xauth/xapp_xauth.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XAUTH_VERSION = 1.0.10
+XAPP_XAUTH_VERSION = 1.1
 XAPP_XAUTH_SOURCE = xauth-$(XAPP_XAUTH_VERSION).tar.bz2
 XAPP_XAUTH_SITE = http://xorg.freedesktop.org/releases/individual/app
 XAPP_XAUTH_LICENSE = MIT
