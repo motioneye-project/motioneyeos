82fc6379f05668394cc8f9b573ed176ab762aa2e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-07-13 10:32:55 +0200

    package/vte: needs host-intltool
    Fixes:
     - http://autobuild.buildroot.org/results/30a8032ebba5a30bacd321c407a1d0734fadf757
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/vte/vte.mk
+++ package/vte/vte.mk
@@ -7,7 +7,7 @@
 VTE_VERSION = 0.48.3
 VTE_SOURCE = vte-$(VTE_VERSION).tar.xz
 VTE_SITE = http://ftp.gnome.org/pub/gnome/sources/vte/0.48
-VTE_DEPENDENCIES = host-pkgconf libgtk3 libxml2 pcre2
+VTE_DEPENDENCIES = host-intltool host-pkgconf libgtk3 libxml2 pcre2
 VTE_LICENSE = LGPL-2.1+
 VTE_LICENSE_FILES = COPYING
 VTE_CONF_OPTS += --disable-introspection --disable-vala
