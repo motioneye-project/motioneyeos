3a8b7f75acdda9e7155da80e7cf63970a06f34b0
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-07-14 10:41:24 +0200

    package/libiscsi: bump to version 1.19.0
    - Remove patches (already in version)
    - Use new configure options to disable examples, test-tool and tests
    - Drop cunit optional dependency now that test-tool is always disabled
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libiscsi/libiscsi.mk
+++ package/libiscsi/libiscsi.mk
@@ -4,18 +4,15 @@
 #
 ################################################################################
 
-LIBISCSI_VERSION = 1.18.0
+LIBISCSI_VERSION = 1.19.0
 LIBISCSI_SITE = $(call github,sahlberg,libiscsi,$(LIBISCSI_VERSION))
 LIBISCSI_LICENSE = GPL-2.0+, LGPL-2.1+
 LIBISCSI_LICENSE_FILES = COPYING LICENCE-GPL-2.txt LICENCE-LGPL-2.1.txt
 LIBISCSI_INSTALL_STAGING = YES
 LIBISCSI_AUTORECONF = YES
 
-ifeq ($(BR2_PACKAGE_CUNIT),y)
-LIBISCSI_DEPENDENCIES += cunit
-endif
-
-LIBISCSI_CONF_OPTS = --disable-werror --disable-manpages
+LIBISCSI_CONF_OPTS = --disable-examples --disable-werror --disable-manpages \
+	--disable-test-tool --disable-tests
 
 # We need to create the m4 directory to make autoreconf work properly.
 define LIBISCSI_CREATE_M4_DIR
