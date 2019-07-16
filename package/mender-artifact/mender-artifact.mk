c16ba2822fba0c6e01839c9cc569d651d2964b5b
Adam Duskett <Aduskett@gmail.com>
2019-07-16 11:12:57 -0400

    package/mender-artifact: bump to version 3.0.1
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mender-artifact/mender-artifact.mk
+++ package/mender-artifact/mender-artifact.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HOST_MENDER_ARTIFACT_VERSION = 3.0.0
+HOST_MENDER_ARTIFACT_VERSION = 3.0.1
 HOST_MENDER_ARTIFACT_SITE = https://github.com/mendersoftware/mender-artifact/archive
 HOST_MENDER_ARTIFACT_SOURCE = $(HOST_MENDER_ARTIFACT_VERSION).tar.gz
 HOST_MENDER_ARTIFACT_LICENSE = Apache2.0, BSD-2-Clause, BSD-3-Clause, ISC, MIT
