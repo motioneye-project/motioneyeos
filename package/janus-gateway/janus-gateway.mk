2a8e32f68baf52bd41a1308ea69058bf0624c5c0
Adam Duskett <Aduskett@gmail.com>
2019-07-16 11:37:08 -0400

    package/janus-gateway: bump to version 0.7.3
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/janus-gateway/janus-gateway.mk
+++ package/janus-gateway/janus-gateway.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-JANUS_GATEWAY_VERSION = 0.6.3
+JANUS_GATEWAY_VERSION = 0.7.3
 JANUS_GATEWAY_SITE = $(call github,meetecho,janus-gateway,v$(JANUS_GATEWAY_VERSION))
 JANUS_GATEWAY_LICENSE = GPL-3.0
 JANUS_GATEWAY_LICENSE_FILES = COPYING
