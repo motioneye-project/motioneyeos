c56a918a52158ca213d57a2e9d24513f48cabd0d
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-07-16 22:09:52 +0200

    package/x11r7/xapp_xwininfo: bump version to 1.1.5
    Updated _SITE according to release notes.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/x11r7/xapp_xwininfo/xapp_xwininfo.mk
+++ package/x11r7/xapp_xwininfo/xapp_xwininfo.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XWININFO_VERSION = 1.1.4
+XAPP_XWININFO_VERSION = 1.1.5
 XAPP_XWININFO_SOURCE = xwininfo-$(XAPP_XWININFO_VERSION).tar.bz2
-XAPP_XWININFO_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XWININFO_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XWININFO_LICENSE = MIT
 XAPP_XWININFO_LICENSE_FILES = COPYING
 XAPP_XWININFO_DEPENDENCIES = xlib_libX11 xlib_libXmu
