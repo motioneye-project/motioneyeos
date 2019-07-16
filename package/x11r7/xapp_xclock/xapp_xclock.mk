e3cc4a4cd41b8efb71cbe171e2af6cef494292af
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-07-16 22:09:50 +0200

    package/x11r7/xapp_xclock: bump version to 1.0.9
    Updated _SITE according to release notes.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/x11r7/xapp_xclock/xapp_xclock.mk
+++ package/x11r7/xapp_xclock/xapp_xclock.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XCLOCK_VERSION = 1.0.8
+XAPP_XCLOCK_VERSION = 1.0.9
 XAPP_XCLOCK_SOURCE = xclock-$(XAPP_XCLOCK_VERSION).tar.bz2
-XAPP_XCLOCK_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XCLOCK_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XCLOCK_LICENSE = MIT
 XAPP_XCLOCK_LICENSE_FILES = COPYING
 XAPP_XCLOCK_DEPENDENCIES = xlib_libX11 xlib_libXaw xlib_libXft xlib_libXrender xlib_libxkbfile
