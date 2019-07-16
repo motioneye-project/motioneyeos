ef13210b11e7df3419cc35591f15de04bdfdf71d
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-07-16 22:03:14 +0200

    package/x11r7/xapp_xbacklight: bump version to 1.2.3
    Updated _SITE according to release notes.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/x11r7/xapp_xbacklight/xapp_xbacklight.mk
+++ package/x11r7/xapp_xbacklight/xapp_xbacklight.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XBACKLIGHT_VERSION = 1.2.2
+XAPP_XBACKLIGHT_VERSION = 1.2.3
 XAPP_XBACKLIGHT_SOURCE = xbacklight-$(XAPP_XBACKLIGHT_VERSION).tar.bz2
-XAPP_XBACKLIGHT_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XBACKLIGHT_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XBACKLIGHT_LICENSE = MIT
 XAPP_XBACKLIGHT_LICENSE_FILES = COPYING
 XAPP_XBACKLIGHT_DEPENDENCIES = xlib_libX11 xlib_libXrandr xlib_libXrender xcb-util
