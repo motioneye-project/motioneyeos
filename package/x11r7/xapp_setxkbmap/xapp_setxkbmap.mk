5966187a788aed418b427d91cca079939ba2078b
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-07-16 22:03:13 +0200

    package/x11r7/xapp_setxkbmap: bump version to 1.3.2
    Added all hashes provided by upstream, added license hash.
    Updated _SITE according to release notes.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/x11r7/xapp_setxkbmap/xapp_setxkbmap.mk
+++ package/x11r7/xapp_setxkbmap/xapp_setxkbmap.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_SETXKBMAP_VERSION = 1.3.1
+XAPP_SETXKBMAP_VERSION = 1.3.2
 XAPP_SETXKBMAP_SOURCE = setxkbmap-$(XAPP_SETXKBMAP_VERSION).tar.bz2
-XAPP_SETXKBMAP_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_SETXKBMAP_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_SETXKBMAP_LICENSE = MIT
 XAPP_SETXKBMAP_LICENSE_FILES = COPYING
 XAPP_SETXKBMAP_DEPENDENCIES = xlib_libX11 xlib_libxkbfile
