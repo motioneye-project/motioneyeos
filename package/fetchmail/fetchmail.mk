bd2011671ea1ebe7f6a61db9b27e00d98c230e2e
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-07-20 11:31:03 +0200

    package/fetchmail: bump version to 6.4.0.beta5
    This bump brings buildroot in-line with Debian stable which also
    provides this version after version 6.3 was removed from testing
    in 2017: https://tracker.debian.org/pkg/fetchmail
    
    Removed patches applied upstream:
    https://gitlab.com/fetchmail/fetchmail/commit/bcab21a977732e2fe24f37cb394f913af0d8b166
    https://gitlab.com/fetchmail/fetchmail/commit/a2ae6f8d15d7caf815d7bdd13df833fd1b2af5cc
    
    fetchmailconf.py received various python3-related patches:
    https://gitlab.com/fetchmail/fetchmail/commits/legacy_64/fetchmailconf.py
    
    Therefore FETCHMAIL_REMOVE_FETCHMAILCONF_PY is not needed anymore.
    
    Added license hash.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/fetchmail/fetchmail.mk
+++ package/fetchmail/fetchmail.mk
@@ -4,14 +4,12 @@
 #
 ################################################################################
 
-FETCHMAIL_VERSION_MAJOR = 6.3
-FETCHMAIL_VERSION = $(FETCHMAIL_VERSION_MAJOR).26
+FETCHMAIL_VERSION_MAJOR = 6.4
+FETCHMAIL_VERSION = $(FETCHMAIL_VERSION_MAJOR).0.beta5
 FETCHMAIL_SOURCE = fetchmail-$(FETCHMAIL_VERSION).tar.xz
 FETCHMAIL_SITE = http://downloads.sourceforge.net/project/fetchmail/branch_$(FETCHMAIL_VERSION_MAJOR)
 FETCHMAIL_LICENSE = GPL-2.0; some exceptions are mentioned in COPYING
 FETCHMAIL_LICENSE_FILES = COPYING
-FETCHMAIL_AUTORECONF = YES
-FETCHMAIL_GETTEXTIZE = YES
 
 FETCHMAIL_CONF_OPTS = \
 	--with-ssl=$(STAGING_DIR)/usr
@@ -21,15 +19,5 @@ FETCHMAIL_DEPENDENCIES = \
 	host-pkgconf \
 	openssl \
 	$(TARGET_NLS_DEPENDENCIES)
-
-# fetchmailconf.py script is not (yet) python3-compliant.
-# Prevent the pyc-compilation with python-3 from failing by removing this
-# non-critical script.
-ifeq ($(BR2_PACKAGE_PYTHON3),y)
-define FETCHMAIL_REMOVE_FETCHMAILCONF_PY
-	$(RM) -f $(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/fetchmailconf.py
-endef
-FETCHMAIL_POST_INSTALL_TARGET_HOOKS += FETCHMAIL_REMOVE_FETCHMAILCONF_PY
-endif
 
 $(eval $(autotools-package))
