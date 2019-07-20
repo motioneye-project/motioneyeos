2f2d3bf3da69d0745ef904b2abcb4b63bc875193
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-07-20 10:19:36 +0200

    package/x11r7/xapp_xinput: bump version to 1.6.3
    Added all hashes provided by upstream, added license hash.
    Updated _SITE according to release notes.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xapp_xinput/xapp_xinput.mk
+++ package/x11r7/xapp_xinput/xapp_xinput.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XAPP_XINPUT_VERSION = 1.6.2
+XAPP_XINPUT_VERSION = 1.6.3
 XAPP_XINPUT_SOURCE = xinput-$(XAPP_XINPUT_VERSION).tar.bz2
-XAPP_XINPUT_SITE = http://xorg.freedesktop.org/releases/individual/app
+XAPP_XINPUT_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XINPUT_LICENSE = MIT
 XAPP_XINPUT_LICENSE_FILES = COPYING
 XAPP_XINPUT_DEPENDENCIES = xlib_libX11 xlib_libXi xlib_libXrandr xlib_libXinerama
