9b5b7165deb91bad3cdb1ea0dd8f940eff75f3a9
Alistair Francis <alistair.francis@wdc.com>
2019-07-22 13:44:46 -0700

    boot/opensbi: add support for including Linux payload
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>
    [Thomas:
     - Config.in: move the BR2_TARGET_OPENSBI_PLAT dependency to a
       "depends on", there's no need to have an if...endif block just for
       that.
     - Config.in: drop the BR2_TARGET_OPENSBI_LIBRARY_ONLY dependency,
       since this option doesn't exist
     - opensbi.mk: don't use qstrip on BR2_TARGET_OPENSBI_LINUX_PAYLOAD,
       since it's a boolean option
     - opensbi.mk: use += when adding linux to DEPENDENCIES
     - opensbki.mk: refactor the firmware file installation with a loop]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- boot/opensbi/opensbi.mk
+++ boot/opensbi/opensbi.mk
@@ -19,19 +19,31 @@ ifneq ($(OPENSBI_PLAT),)
 OPENSBI_MAKE_ENV += PLATFORM=$(OPENSBI_PLAT)
 endif
 
+ifeq ($(BR2_TARGET_OPENSBI_LINUX_PAYLOAD),y)
+OPENSBI_DEPENDENCIES += linux
+OPENSBI_MAKE_ENV += FW_PAYLOAD_PATH="$(BINARIES_DIR)/Image"
+endif
+
 define OPENSBI_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(OPENSBI_MAKE_ENV) $(MAKE) -C $(@D)
 endef
 
 ifneq ($(OPENSBI_PLAT),)
 OPENSBI_INSTALL_IMAGES = YES
-define OPENSBI_INSTALL_IMAGES_CMDS
-	$(INSTALL) -m 0644 -D $(@D)/build/platform/$(OPENSBI_PLAT)/firmware/fw_jump.bin $(BINARIES_DIR)/fw_jump.bin
-	$(INSTALL) -m 0644 -D $(@D)/build/platform/$(OPENSBI_PLAT)/firmware/fw_jump.elf $(BINARIES_DIR)/fw_jump.elf
-	$(INSTALL) -m 0644 -D $(@D)/build/platform/$(OPENSBI_PLAT)/firmware/fw_dynamic.bin $(BINARIES_DIR)/fw_dynamic.bin
-	$(INSTALL) -m 0644 -D $(@D)/build/platform/$(OPENSBI_PLAT)/firmware/fw_dynamic.elf $(BINARIES_DIR)/fw_dynamic.elf
-endef
+OPENSBI_FW_IMAGES += jump dynamic
+ifeq ($(BR2_TARGET_OPENSBI_LINUX_PAYLOAD),y)
+OPENSBI_FW_IMAGES += payload
 endif
+endif
+
+define OPENSBI_INSTALL_IMAGES_CMDS
+	$(foreach f,$(OPENSBI_FW_IMAGES),\
+		$(INSTALL) -m 0644 -D $(@D)/build/platform/$(OPENSBI_PLAT)/firmware/fw_$(f).bin \
+			$(BINARIES_DIR)/fw_$(f).bin
+		$(INSTALL) -m 0644 -D $(@D)/build/platform/$(OPENSBI_PLAT)/firmware/fw_$(f).elf \
+			$(BINARIES_DIR)/fw_$(f).elf
+	)
+endef
 
 # libsbi.a is not a library meant to be linked in user-space code, but
 # with bare metal code, which is why we don't install it in
