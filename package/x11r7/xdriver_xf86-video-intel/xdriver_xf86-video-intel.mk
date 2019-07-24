b13dd6c6b41a95df345e15fe6d8a8e110bf0225d
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-07-24 20:26:53 +0200

    package/x11r7/xdriver_xf86-video-intel: bump version
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/x11r7/xdriver_xf86-video-intel/xdriver_xf86-video-intel.mk
+++ package/x11r7/xdriver_xf86-video-intel/xdriver_xf86-video-intel.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_INTEL_VERSION = 6afed33b2d673d88674f0c76efe500ae414e8e1b
+XDRIVER_XF86_VIDEO_INTEL_VERSION = 6f4972d5c368c30e971a23c1dc370d3e43761282
 XDRIVER_XF86_VIDEO_INTEL_SITE = git://anongit.freedesktop.org/xorg/driver/xf86-video-intel
 XDRIVER_XF86_VIDEO_INTEL_LICENSE = MIT
 XDRIVER_XF86_VIDEO_INTEL_LICENSE_FILES = COPYING
