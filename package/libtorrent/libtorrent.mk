26545af9a165a22b63b61dbdb7b0855c1a620c64
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-08-01 13:16:53 +0200

    package/libtorrent: bump to version 0.13.8
    Remove both patches (already in version) and drop AUTORECONF as it is
    not needed anymore
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libtorrent/libtorrent.mk
+++ package/libtorrent/libtorrent.mk
@@ -4,14 +4,13 @@
 #
 ################################################################################
 
-LIBTORRENT_VERSION = 0.13.7
+LIBTORRENT_VERSION = 0.13.8
 LIBTORRENT_SITE = http://rtorrent.net/downloads
 LIBTORRENT_DEPENDENCIES = host-pkgconf zlib
 LIBTORRENT_CONF_OPTS = --enable-aligned \
 	--disable-instrumentation \
 	--with-zlib=$(STAGING_DIR)/usr
 LIBTORRENT_INSTALL_STAGING = YES
-LIBTORRENT_AUTORECONF = YES
 LIBTORRENT_LICENSE = GPL-2.0
 LIBTORRENT_LICENSE_FILES = COPYING
 
