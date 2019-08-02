f923cccb64291cb17bffaa639edcc5b8d0321094
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-08-02 09:24:55 +0200

    package/gssdp: bump to version 1.0.3
    - Switch to meson-package
    - Disable examples (enabled by default)
    - Disable introspection (enabled by default)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gssdp/gssdp.mk
+++ package/gssdp/gssdp.mk
@@ -5,19 +5,22 @@
 ################################################################################
 
 GSSDP_VERSION_MAJOR = 1.0
-GSSDP_VERSION = $(GSSDP_VERSION_MAJOR).2
+GSSDP_VERSION = $(GSSDP_VERSION_MAJOR).3
 GSSDP_SOURCE = gssdp-$(GSSDP_VERSION).tar.xz
 GSSDP_SITE = http://ftp.gnome.org/pub/gnome/sources/gssdp/$(GSSDP_VERSION_MAJOR)
 GSSDP_LICENSE = LGPL-2.0+
 GSSDP_LICENSE_FILES = COPYING
 GSSDP_INSTALL_STAGING = YES
 GSSDP_DEPENDENCIES = host-pkgconf libglib2 libsoup
+GSSDP_CONF_OPTS = \
+	-Dexamples=false \
+	-Dintrospection=false
 
 ifeq ($(BR2_PACKAGE_LIBGTK3),y)
 GSSDP_DEPENDENCIES += libgtk3
-GSSDP_CONF_OPTS += --with-gtk
+GSSDP_CONF_OPTS += -Dsniffer=true
 else
-GSSDP_CONF_OPTS += --without-gtk
+GSSDP_CONF_OPTS += -Dsniffer=false
 endif
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
