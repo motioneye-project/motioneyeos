b9c2894298a11e414c1fa276ad28de8d4960a1f9
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2019-08-02 09:04:39 +0200

    package/bison: fix parallel build issue
    Following the bump of Bison to 3.4.1 in commit
    d3c2f33543b91670f8ead627ec7975142b9e6b5a, the build started failing on
    autobuilders using a fairly old version of make (3.81). The failure is
    caused by a parallel build problem in examples/c/reccalc/.
    
    Fixing it is not trivial, and would require autoreconf, which then
    fails due to help2man being missing. So for the time being, simply
    disable parallel build on bison.
    
    The issue has been reported to the bug-bison@ mailing list, but the
    e-mail doesn't appear yet in the mailing list archives.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/856/8568bcef944096f2c21470d36f02b520989af5eb/
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/bison/bison.mk
+++ package/bison/bison.mk
@@ -9,6 +9,8 @@ BISON_SOURCE = bison-$(BISON_VERSION).tar.xz
 BISON_SITE = $(BR2_GNU_MIRROR)/bison
 BISON_LICENSE = GPL-3.0+
 BISON_LICENSE_FILES = COPYING
+# parallel build issue in examples/c/reccalc/
+BISON_MAKE = $(MAKE1)
 HOST_BISON_DEPENDENCIES = host-m4
 
 $(eval $(host-autotools-package))
