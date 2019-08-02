66a3fbb9f6c79e48eaf9e561a4143063e7e9d670
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-08-02 09:28:48 +0200

    package/gupnp: bump to version 1.0.4
    - Switch to meson-package
    - Disable introspection (enabled by default)
    - Disable examples (enabled by default)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gupnp/gupnp.mk
+++ package/gupnp/gupnp.mk
@@ -5,12 +5,15 @@
 ################################################################################
 
 GUPNP_VERSION_MAJOR = 1.0
-GUPNP_VERSION = $(GUPNP_VERSION_MAJOR).3
+GUPNP_VERSION = $(GUPNP_VERSION_MAJOR).4
 GUPNP_SOURCE = gupnp-$(GUPNP_VERSION).tar.xz
 GUPNP_SITE = http://ftp.gnome.org/pub/gnome/sources/gupnp/$(GUPNP_VERSION_MAJOR)
 GUPNP_LICENSE = LGPL-2.0+
 GUPNP_LICENSE_FILES = COPYING
 GUPNP_INSTALL_STAGING = YES
 GUPNP_DEPENDENCIES = host-pkgconf libglib2 libxml2 gssdp util-linux
+GUPNP_CONF_OPTS = \
+	-Dexamples=false \
+	-Dintrospection=false
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
