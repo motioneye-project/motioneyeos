650e9903a9cbfda660fea24239d5f8cbf4607ac8
Ricardo Martincoski <ricardo.martincoski@gmail.com>
2019-08-03 02:22:15 -0300

    support/testing: drop haveged for python tests
    The new builtin armv5 kernel 4.19 uses VirtIORNG to provide entropy, so we
    no longer need haveged.
    
    Fixes:
    https://gitlab.com/buildroot.org/buildroot/-/jobs/264508536
    https://gitlab.com/buildroot.org/buildroot/-/jobs/264508537
    https://gitlab.com/buildroot.org/buildroot/-/jobs/264508542
    https://gitlab.com/buildroot.org/buildroot/-/jobs/264508543
    
    Signed-off-by: Ricardo Martincoski <ricardo.martincoski@gmail.com>
    Cc: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    [Peter: reword]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- support/testing/tests/package/test_python_pynacl.py
+++ support/testing/tests/package/test_python_pynacl.py
@@ -3,12 +3,10 @@ from tests.package.test_python import TestPythonPackageBase
 
 class TestPythonPy2Pynacl(TestPythonPackageBase):
     __test__ = True
-    # use haveged to generate enough entropy so pynacl -> libsodium don't hang waiting for /dev/random
     config = TestPythonPackageBase.config + \
         """
         BR2_PACKAGE_PYTHON=y
         BR2_PACKAGE_PYTHON_PYNACL=y
-        BR2_PACKAGE_HAVEGED=y
         """
     sample_scripts = ["tests/package/sample_python_pynacl.py"]
     timeout = 10
@@ -16,12 +14,10 @@ class TestPythonPy2Pynacl(TestPythonPackageBase):
 
 class TestPythonPy3Pynacl(TestPythonPackageBase):
     __test__ = True
-    # use haveged to generate enough entropy so pynacl -> libsodium don't hang waiting for /dev/random
     config = TestPythonPackageBase.config + \
         """
         BR2_PACKAGE_PYTHON3=y
         BR2_PACKAGE_PYTHON_PYNACL=y
-        BR2_PACKAGE_HAVEGED=y
         """
     sample_scripts = ["tests/package/sample_python_pynacl.py"]
     timeout = 10
