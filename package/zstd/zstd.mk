1a85f43d92bf526b3fd14098fc20877fd02e2440
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-08-03 21:45:28 +0200

    package/zstd: bump version to 1.4.2
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/zstd/zstd.mk
+++ package/zstd/zstd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ZSTD_VERSION = 1.3.5
+ZSTD_VERSION = 1.4.2
 ZSTD_SITE = $(call github,facebook,zstd,v$(ZSTD_VERSION))
 ZSTD_INSTALL_STAGING = YES
 ZSTD_LICENSE = BSD-3-Clause or GPL-2.0
