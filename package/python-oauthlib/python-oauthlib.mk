6916bf987a19698231e32468677e06175bf713f8
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-08-03 22:40:37 +0200

    package/python-oauthlib: bump version to 3.0.2
    Updated license hash after upstream bump to 2019
    https://github.com/oauthlib/oauthlib/commit/f4273e75250dd36c88d63dc075ae45650a5172e9
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-oauthlib/python-oauthlib.mk
+++ package/python-oauthlib/python-oauthlib.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_OAUTHLIB_VERSION = 2.1.0
+PYTHON_OAUTHLIB_VERSION = 3.0.2
 PYTHON_OAUTHLIB_SOURCE = oauthlib-$(PYTHON_OAUTHLIB_VERSION).tar.gz
-PYTHON_OAUTHLIB_SITE = https://files.pythonhosted.org/packages/df/5f/3f4aae7b28db87ddef18afed3b71921e531ca288dc604eb981e9ec9f8853
+PYTHON_OAUTHLIB_SITE = https://files.pythonhosted.org/packages/6a/cf/f1a44998f36c62a7a9e51145101428299be27b1e3a411adfd6fd87589889
 PYTHON_OAUTHLIB_SETUP_TYPE = setuptools
 PYTHON_OAUTHLIB_LICENSE = BSD-3-Clause
 PYTHON_OAUTHLIB_LICENSE_FILES = LICENSE
