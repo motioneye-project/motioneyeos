64d14df95c97d2c2dd5e8999010e99d3cf62b47b
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-08-04 09:39:20 +0200

    package/gdbm: bump version to 1.18.1
    Added license hash.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gdbm/gdbm.mk
+++ package/gdbm/gdbm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GDBM_VERSION = 1.13
+GDBM_VERSION = 1.18.1
 GDBM_SITE = $(BR2_GNU_MIRROR)/gdbm
 GDBM_LICENSE = GPL-3.0+
 GDBM_LICENSE_FILES = COPYING
