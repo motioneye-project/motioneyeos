af8c7b41f515e76b4f00cd333e938d09fd1503d4
Michał Łyszczek <michal.lyszczek@bofc.pl>
2019-08-04 14:14:16 +0200

    package/skeleton-init-openrc: add support to remount rootfs as rw
    Add a pseudo entry for /dev/root in fstab, to tell openrc to properly
    remount or not remount / read-write.
    
    For consistency with systemd (which is the only other init system to
    tweak an fstab basd on / being rw or not), we do this change in the
    openrc skeleton rather than in the openrc package.
    
    Signed-off-by: Michał Łyszczek <michal.lyszczek@bofc.pl>
    [yann.morin.1998@free.fr:
      - tweak the sed expressions
      - reword commit log
    ]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/skeleton-init-openrc/skeleton-init-openrc.mk
+++ package/skeleton-init-openrc/skeleton-init-openrc.mk
@@ -15,8 +15,23 @@ SKELETON_INIT_OPENRC_DEPENDENCIES = skeleton-init-common
 
 SKELETON_INIT_OPENRC_PROVIDES = skeleton
 
+ifeq ($(BR2_TARGET_GENERIC_REMOUNT_ROOTFS_RW),y)
+# Comment /dev/root entry in fstab. When openrc does not find fstab entry for
+# "/", it will try to remount "/" as "rw".
+define SKELETON_INIT_OPENRC_ROOT_RO_OR_RW
+	$(SED) '\:^/dev/root :s/^/# /' $(TARGET_DIR)/etc/fstab
+endef
+else
+# Uncomment /dev/root entry in fstab which has "ro" option so openrc notices
+# it and doesn't remount root to rw.
+define SKELETON_INIT_OPENRC_ROOT_RO_OR_RW
+	$(SED) '\:^# /dev/root:s/^# //' $(TARGET_DIR)/etc/fstab
+endef
+endif # BR2_TARGET_GENERIC_REMOUNT_ROOTFS_RW
+
 define SKELETON_INIT_OPENRC_INSTALL_TARGET_CMDS
 	$(call SYSTEM_RSYNC,$(SKELETON_INIT_OPENRC_PKGDIR)/skeleton,$(TARGET_DIR))
+	$(SKELETON_INIT_OPENRC_ROOT_RO_OR_RW)
 endef
 
 $(eval $(generic-package))
