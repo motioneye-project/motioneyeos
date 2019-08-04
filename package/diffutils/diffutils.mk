efbda08b1b135817c61b9c3a522344224633d56f
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-08-04 08:57:43 +0200

    package/diffutils: bump version to 3.7
    Added license hash.
    
    Release notes:
    https://lists.gnu.org/archive/html/info-gnu/2019-01/msg00000.html
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/diffutils/diffutils.mk
+++ package/diffutils/diffutils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DIFFUTILS_VERSION = 3.6
+DIFFUTILS_VERSION = 3.7
 DIFFUTILS_SOURCE = diffutils-$(DIFFUTILS_VERSION).tar.xz
 DIFFUTILS_SITE = $(BR2_GNU_MIRROR)/diffutils
 DIFFUTILS_DEPENDENCIES = $(TARGET_NLS_DEPENDENCIES)
