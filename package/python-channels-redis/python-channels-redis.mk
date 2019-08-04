e4b8a4786ec396244e37a27dce1173e23ca17cee
Adam Duskett <Aduskett@gmail.com>
2019-08-03 21:16:31 -0400

    package/python-channels-redis: new package
    A Django Channels channel layer that uses Redis as its backing store,
    and supports both a single-server and sharded configurations, as well
    as group support.
    
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    [Thomas:
     - alphabetic ordering of selects in Config.in
     - drop redis dependency
     - add missing python3 dependency]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/python-channels-redis/python-channels-redis.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-channels-redis
+#
+################################################################################
+
+PYTHON_CHANNELS_REDIS_VERSION = 2.4.0
+PYTHON_CHANNELS_REDIS_SOURCE = channels_redis-$(PYTHON_CHANNELS_REDIS_VERSION).tar.gz
+PYTHON_CHANNELS_REDIS_SITE = https://files.pythonhosted.org/packages/79/20/896a5246ef703a74dd0af6e46038eb7838d801e532cf25474d2580f09d3e
+PYTHON_CHANNELS_REDIS_SETUP_TYPE = setuptools
+PYTHON_CHANNELS_REDIS_LICENSE = BSD-3-Clause
+PYTHON_CHANNELS_REDIS_LICENSE_FILES = LICENSE
+PYTHON_CHANNELS_REDIS_DEPENDENCIES = redis
+
+$(eval $(python-package))
