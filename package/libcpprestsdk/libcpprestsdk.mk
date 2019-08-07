d4263617b9bb60fcd497eb8a16b4847aa0d3a476
Adam Duskett <Aduskett@gmail.com>
2019-08-07 12:14:07 -0400

    package/libcpprestsdk: bump to version 2.10.14
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libcpprestsdk/libcpprestsdk.mk
+++ package/libcpprestsdk/libcpprestsdk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBCPPRESTSDK_VERSION = 2.10.12
+LIBCPPRESTSDK_VERSION = 2.10.14
 LIBCPPRESTSDK_SITE = $(call github,Microsoft,cpprestsdk,v$(LIBCPPRESTSDK_VERSION))
 LIBCPPRESTSDK_LICENSE = MIT
 LIBCPPRESTSDK_LICENSE_FILES = license.txt
