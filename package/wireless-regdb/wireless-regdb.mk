4de0b10d573736e31b2a766f62e039d44ae2c8ae
Jörg Krause <joerg.krause@embedded.rocks>
2019-08-08 23:27:53 +0200

    package/wireless-regdb: bump to version 2019.06.03
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/wireless-regdb/wireless-regdb.mk
+++ package/wireless-regdb/wireless-regdb.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIRELESS_REGDB_VERSION = 2018.09.07
+WIRELESS_REGDB_VERSION = 2019.06.03
 WIRELESS_REGDB_SOURCE = wireless-regdb-$(WIRELESS_REGDB_VERSION).tar.xz
 WIRELESS_REGDB_SITE = $(BR2_KERNEL_MIRROR)/software/network/wireless-regdb
 WIRELESS_REGDB_LICENSE = ISC
