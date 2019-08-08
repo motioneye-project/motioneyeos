29e689d41a9f73b8542643d75f550d2e118ad3a8
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-08-08 10:17:04 +0200

    package/yad: fix build with NLS
    Fixes:
     - http://autobuild.buildroot.org/results/40ccab40d7c82b908a622d45998d057a31d9cac6
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/yad/yad.mk
+++ package/yad/yad.mk
@@ -9,7 +9,8 @@ YAD_SOURCE = yad-$(YAD_VERSION).tar.xz
 YAD_SITE = http://sourceforge.net/projects/yad-dialog/files
 YAD_LICENSE = GPL-3.0
 YAD_LICENSE_FILES = COPYING
-YAD_DEPENDENCIES = host-intltool host-pkgconf
+YAD_DEPENDENCIES = host-intltool host-pkgconf $(TARGET_NLS_DEPENDENCIES)
+YAD_CONF_ENV = LIBS=$(TARGET_NLS_LIBS)
 YAD_CONF_OPTS = --enable-html=no
 
 ifeq ($(BR2_PACKAGE_LIBGTK3_X11),y)
