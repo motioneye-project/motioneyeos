7ea64484d4dcd5e2e95d903eddf166cdbbcad485
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-08-09 17:59:41 +0200

    package/postgresql: security bump version to 11.5
    Release notes: https://www.postgresql.org/about/news/1960/
    
    Switch POSTGRESQL_SITE to https.
    
    Fixes CVE-2019-10208, CVE-2019-10209, CVE-2019-10210 & CVE-2019-10211.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/postgresql/postgresql.mk
+++ package/postgresql/postgresql.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-POSTGRESQL_VERSION = 11.4
+POSTGRESQL_VERSION = 11.5
 POSTGRESQL_SOURCE = postgresql-$(POSTGRESQL_VERSION).tar.bz2
-POSTGRESQL_SITE = http://ftp.postgresql.org/pub/source/v$(POSTGRESQL_VERSION)
+POSTGRESQL_SITE = https://ftp.postgresql.org/pub/source/v$(POSTGRESQL_VERSION)
 POSTGRESQL_LICENSE = PostgreSQL
 POSTGRESQL_LICENSE_FILES = COPYRIGHT
 POSTGRESQL_INSTALL_STAGING = YES
