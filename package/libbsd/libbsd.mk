1f6c7d6e0f1e6e531100b09ec766c54e6dbafb3d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-08-12 18:37:44 +0200

    package/libbsd: security bump to version 0.10.0
    - Remove patch (already in version)
    - Update site to get the latest version
    - Update hash of license file (update in year, new file and author)
    - Remove !(BR2_TOOLCHAIN_USES_UCLIBC && !BR2_USE_MMU) dependency,
      __register_at_fork availability is correclty checked since
      https://gitlab.freedesktop.org/libbsd/libbsd/commit/b0ebb0d4c26b281facbab7a774510b541637b13b
    - Includes Several security related fixes for nlist() reported by Daniel
      Hodson and one by Coverity Scan, see
      https://lists.freedesktop.org/archives/libbsd/2019-August/000229.html
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Acked-by: Yann E. MORIN <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libbsd/libbsd.mk
+++ package/libbsd/libbsd.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-LIBBSD_VERSION = 0.9.1
+LIBBSD_VERSION = 0.10.0
 LIBBSD_SOURCE = libbsd-$(LIBBSD_VERSION).tar.xz
-LIBBSD_SITE = https://archive.hadrons.org/software/libbsd
+LIBBSD_SITE = https://libbsd.freedesktop.org/releases
 LIBBSD_LICENSE = BSD-2-Clause, BSD-3-Clause, BSD-4-Clause, BSD-5-Clause, \
 		MIT, ISC, Beerware
 LIBBSD_LICENSE_FILES = COPYING
