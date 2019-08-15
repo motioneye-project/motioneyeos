589b8cb7e2d81067fde9ee4f11acecf8fd677dde
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2019-08-15 15:43:21 +0200

    package/pigpio: install to staging
    The pigpio package installs programs and libraries to target, but does
    not install the libraries and its headers to staging, while they may
    be used by other packages. Let's install them, as was requested in bug
    
    Fixes:
    
      https://bugs.busybox.net/show_bug.cgi?id=11741
    
    Cc: vishalbhalani89@gmail.com
    Cc: ivan.nazarenko@gmail.com
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/pigpio/pigpio.mk
+++ package/pigpio/pigpio.mk
@@ -8,6 +8,7 @@ PIGPIO_VERSION = 68
 PIGPIO_SITE = $(call github,joan2937,pigpio,V$(PIGPIO_VERSION))
 PIGPIO_LICENSE = Unlicense
 PIGPIO_LICENSE_FILES = UNLICENCE
+PIGPIO_INSTALL_STAGING = YES
 
 define PIGPIO_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
@@ -20,6 +21,15 @@ define PIGPIO_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/libpigpio.so $(TARGET_DIR)/usr/lib/libpigpio.so
 	$(INSTALL) -D -m 0755 $(@D)/libpigpiod_if.so $(TARGET_DIR)/usr/lib/libpigpiod_if.so
 	$(INSTALL) -D -m 0755 $(@D)/libpigpiod_if2.so $(TARGET_DIR)/usr/lib/libpigpiod_if2.so
+endef
+
+define PIGPIO_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/libpigpio.so $(STAGING_DIR)/usr/lib/libpigpio.so
+	$(INSTALL) -D -m 0755 $(@D)/libpigpiod_if.so $(STAGING_DIR)/usr/lib/libpigpiod_if.so
+	$(INSTALL) -D -m 0755 $(@D)/libpigpiod_if2.so $(STAGING_DIR)/usr/lib/libpigpiod_if2.so
+	$(INSTALL) -D -m 0644 $(@D)/pigpio.h $(STAGING_DIR)/usr/include/pigpio.h
+	$(INSTALL) -D -m 0644 $(@D)/pigpiod_if.h $(STAGING_DIR)/usr/include/pigpiod_if.h
+	$(INSTALL) -D -m 0644 $(@D)/pigpiod_if2.h $(STAGING_DIR)/usr/include/pigpiod_if2.h
 endef
 
 $(eval $(generic-package))
