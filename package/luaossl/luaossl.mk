b0d2bee4f4e5caed12c2845246902b20220675b6
Francois Perrad <fperrad@gmail.com>
2019-08-16 09:05:10 +0200

    package/luaossl: bump to version 20190731
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/luaossl/luaossl.mk
+++ package/luaossl/luaossl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUAOSSL_VERSION_UPSTREAM = 20190612
+LUAOSSL_VERSION_UPSTREAM = 20190731
 LUAOSSL_VERSION = $(LUAOSSL_VERSION_UPSTREAM)-0
 LUAOSSL_SUBDIR = luaossl-rel-$(LUAOSSL_VERSION_UPSTREAM)
 LUAOSSL_LICENSE = MIT
