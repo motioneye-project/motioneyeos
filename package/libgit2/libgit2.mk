bee5ab6c9d68c7ddb04d64e2ebe714eca882b2f3
Nicolas Cavallari <nicolas.cavallari@green-communications.fr>
2019-08-16 13:16:42 +0200

    package/libgit2: security bump to version 0.27.9
    Signed-off-by: Nicolas Cavallari <nicolas.cavallari@green-communications.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libgit2/libgit2.mk
+++ package/libgit2/libgit2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBGIT2_VERSION = 0.27.8
+LIBGIT2_VERSION = 0.27.9
 LIBGIT2_SITE = $(call github,libgit2,libgit2,v$(LIBGIT2_VERSION))
 LIBGIT2_LICENSE = GPL-2.0 with linking exception
 LIBGIT2_LICENSE_FILES = COPYING
