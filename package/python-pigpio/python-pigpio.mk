4d81680ade9e6ad4d51b8fabe9b101d86e5455ae
Grzegorz Blach <grzegorz@blach.pl>
2019-08-20 13:03:52 +0000

    package/python-pigpio: bump to version 1.44
    Signed-off-by: Grzegorz Blach <grzegorz@blach.pl>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pigpio/python-pigpio.mk
+++ package/python-pigpio/python-pigpio.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PIGPIO_VERSION = 1.42
+PYTHON_PIGPIO_VERSION = 1.44
 PYTHON_PIGPIO_SOURCE = pigpio-$(PYTHON_PIGPIO_VERSION).tar.gz
-PYTHON_PIGPIO_SITE = https://files.pythonhosted.org/packages/f6/c1/73d7b4287e2edc7e06ac34a4ec0c73d4758ba755a8d2ecc2aaf57bf7d54c
+PYTHON_PIGPIO_SITE = https://files.pythonhosted.org/packages/8a/30/16b92e0fede5c543f56875c11f8658457af24991277aee693e0e66a6c967
 PYTHON_PIGPIO_SETUP_TYPE = setuptools
 PYTHON_PIGPIO_LICENSE = Unlicense
 
