3153d12ae3ce68c97955f58611e60c6397f18613
Asaf Kahlon <asafka7@gmail.com>
2019-08-27 20:28:02 +0300

    package/python-daemonize: bump to version 2.5.0
    Also add hash for license file.
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-daemonize/python-daemonize.mk
+++ package/python-daemonize/python-daemonize.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_DAEMONIZE_VERSION = 2.4.7
+PYTHON_DAEMONIZE_VERSION = 2.5.0
 PYTHON_DAEMONIZE_SOURCE = daemonize-$(PYTHON_DAEMONIZE_VERSION).tar.gz
-PYTHON_DAEMONIZE_SITE = https://pypi.python.org/packages/84/15/923e3fe48239adf5d697c29e04a3f868d3e4ce8539aab29d6abe784db5be
+PYTHON_DAEMONIZE_SITE = https://files.pythonhosted.org/packages/8c/20/96f7dbc23812cfe4cf479c87af3e4305d0d115fd1fffec32ddeee7b9c82b
 PYTHON_DAEMONIZE_SETUP_TYPE = setuptools
 PYTHON_DAEMONIZE_LICENSE = MIT
 PYTHON_DAEMONIZE_LICENSE_FILES = LICENSE
