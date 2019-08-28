f0c1c6c9e2dfb7ae30d8e60844144e3e710f8172
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-08-28 16:23:56 +0200

    package/libebur128: bump version to 1.2.4
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libebur128/libebur128.mk
+++ package/libebur128/libebur128.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBEBUR128_VERSION = 1.2.2
+LIBEBUR128_VERSION = 1.2.4
 LIBEBUR128_SITE = $(call github,jiixyj,libebur128,v$(LIBEBUR128_VERSION))
 LIBEBUR128_LICENSE = MIT
 LIBEBUR128_LICENSE_FILES = COPYING
