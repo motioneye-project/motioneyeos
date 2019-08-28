1edd9fa36b0ca2245cde54c42516e51f3d90d872
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-08-28 18:13:26 +0200

    package/libglfw: bump version to 3.3
    License file was renamed and whitespace was removed:
    https://github.com/glfw/glfw/commits/master/LICENSE.md
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libglfw/libglfw.mk
+++ package/libglfw/libglfw.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-LIBGLFW_VERSION = 3.2.1
+LIBGLFW_VERSION = 3.3
 LIBGLFW_SITE = $(call github,glfw,glfw,$(LIBGLFW_VERSION))
 LIBGLFW_INSTALL_STAGING = YES
 LIBGLFW_DEPENDENCIES = libgl xlib_libXcursor xlib_libXext \
 	xlib_libXinerama xlib_libXrandr
 LIBGLFW_LICENSE = Zlib
-LIBGLFW_LICENSE_FILES = COPYING.txt
+LIBGLFW_LICENSE_FILES = LICENSE.md
 
 LIBGLFW_CONF_OPTS += \
 	-DGLFW_BUILD_EXAMPLES=OFF \
