151da9661ea2b64d205da5ede126752c410be885
Neil Armstrong <narmstrong@baylibre.com>
2019-08-28 14:40:11 +0200

    package/glmark2: bump to the latest version
    Bump to the latest git version, containing multiple fixes and support
    for render-only GPUs (lima, panfrost, ...) and missing DRM driver
    names to run like meson, rockchip, sun4i-drm.
    
    Tested on Khadas VIM2 (aarch64) and Panfrost.
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/glmark2/glmark2.mk
+++ package/glmark2/glmark2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GLMARK2_VERSION = 9b1070fe9c5cf908f323909d3c8cbed08022abe8
+GLMARK2_VERSION = 9e01aef1a786b28aca73135a5b00f85c357e8f5e
 GLMARK2_SITE = $(call github,glmark2,glmark2,$(GLMARK2_VERSION))
 GLMARK2_LICENSE = GPL-3.0+, SGIv1
 GLMARK2_LICENSE_FILES = COPYING COPYING.SGI
