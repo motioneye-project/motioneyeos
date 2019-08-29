8efd4463f78fd68c85b45cc0da347dfb805abb7e
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2019-08-29 14:00:47 +0200

    configs/nanopc_t4: remove defconfig
    This defconfig tries to build an ARM Trusted Firmware version that
    needs an ARM32 toolchain, which is not available as the platform is an
    ARM64 one. The correct solution for this is to have a package in
    Buildroot for an ARM32 bare-metal toolchain, but this wasn't done in
    time for the 2019.08 release.
    
    In order to not release 2019.08 with a broken defconfig, let's remove
    it. It can be re-added later once the ARM32 bare-metal toolchain
    problem has been resolved.
    
    Fixes:
    
      https://gitlab.com/buildroot.org/buildroot/-/jobs/278489325
    
    Cc: Shyam Saini <shyam.saini@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- board/friendlyarm/nanopc-t4/post-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-MKIMAGE=$HOST_DIR/bin/mkimage
-BOARD_DIR="$(dirname $0)"
-
-$MKIMAGE -n rk3399 -T rksd -d $BINARIES_DIR/u-boot-spl-dtb.bin $BINARIES_DIR/u-boot-spl-dtb.img
-
-install -m 0644 -D $BOARD_DIR/extlinux.conf $TARGET_DIR/boot/extlinux/extlinux.conf
