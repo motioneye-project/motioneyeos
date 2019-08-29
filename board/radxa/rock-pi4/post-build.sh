5f8ed8fa5749d4ebd5e1d138f74ca40484310828
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2019-08-29 14:00:51 +0200

    configs/radxa_rock_pi4: remove defconfig
    This defconfig tries to build an ARM Trusted Firmware version that
    needs an ARM32 toolchain, which is not available as the platform is an
    ARM64 one. The correct solution for this is to have a package in
    Buildroot for an ARM32 bare-metal toolchain, but this wasn't done in
    time for the 2019.08 release.
    
    In order to not release 2019.08 with a broken defconfig, let's remove
    it. It can be re-added later once the ARM32 bare-metal toolchain
    problem has been resolved.
    
    Fixes:
    
      https://gitlab.com/buildroot.org/buildroot/-/jobs/278489410
    
    Cc: Shyam Saini <shyam.saini@amarulasolutions.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- board/radxa/rock-pi4/post-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-MKIMAGE=$HOST_DIR/bin/mkimage
-BOARD_DIR="$(dirname $0)"
-
-$MKIMAGE -n rk3399 -T rksd -d $BINARIES_DIR/u-boot-tpl-dtb.bin $BINARIES_DIR/u-boot-tpl-dtb.img
-cat $BINARIES_DIR/u-boot-tpl-dtb.img $BINARIES_DIR/u-boot-spl-dtb.bin > $BINARIES_DIR/u-boot-tpl-spl-dtb.img
-
-install -m 0644 -D $BOARD_DIR/extlinux.conf $TARGET_DIR/boot/extlinux/extlinux.conf
