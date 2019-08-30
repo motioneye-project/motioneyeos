c38766d6a64d114264b7a8262557d36cef8cfb33
Adrian Perez de Castro <aperez@igalia.com>
2019-08-30 17:15:28 +0300

    package/wpewebkit: security bump to version 2.24.3
    This is a minor release which includes fixes for CVE-2019-8644,
    CVE-2019-8649, CVE-2019-8658, CVE-2019-8666, CVE-2019-8669,
    CVE-2019-8673, CVE-2019-8676, CVE-2019-8678, CVE-2019-8680,
    CVE-2019-8681, CVE-2019-8683, CVE-2019-8684, CVE-2019-8687,
    CVE-2019-8688, CVE-2019-8689, and CVE-2019-8690.
    
    This release also contains many build fixes, a few media playback
    improvements, and a Web compatibility fix. For a complete list,
    the full release notes are available at:
    
      https://wpewebkit.org/release/wpewebkit-2.24.3.html
    
    The detailed security advisory can be found at:
    
      https://wpewebkit.org/security/WSA-2019-0004.html
    
    Patch "0001-Build-failure-after-r243644-in-GTK-Li.patch" is now unneeded
    because it is one of the build fixes included in this release.
    
    Signed-off-by: Adrian Perez de Castro <aperez@igalia.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/wpewebkit/wpewebkit.mk
+++ package/wpewebkit/wpewebkit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEWEBKIT_VERSION = 2.24.2
+WPEWEBKIT_VERSION = 2.24.3
 WPEWEBKIT_SITE = http://www.wpewebkit.org/releases
 WPEWEBKIT_SOURCE = wpewebkit-$(WPEWEBKIT_VERSION).tar.xz
 WPEWEBKIT_INSTALL_STAGING = YES
