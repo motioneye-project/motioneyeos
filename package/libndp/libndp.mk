6274f4191335cf8c9b88796f59da8af04e3abf7a
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-08-31 13:53:07 +0200

    package/libndp: bump version to 1.7
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libndp/libndp.mk
+++ package/libndp/libndp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBNDP_VERSION = 1.6
+LIBNDP_VERSION = 1.7
 LIBNDP_SITE = $(call github,jpirko,libndp,v$(LIBNDP_VERSION))
 LIBNDP_LICENSE = LGPL-2.1+
 LIBNDP_LICENSE_FILES = COPYING
