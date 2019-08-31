eb06ed189624ce84803203579ed799b3db15e976
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-08-31 12:49:30 +0200

    package/libdmtx: bump version to 0.7.5
    According to https://sourceforge.net/projects/libdmtx the project was
    moved to https://github.com/dmtx so update project URL in Config.in.
    
    Updated license hash due to upstream commit
    https://github.com/dmtx/libdmtx/commit/b65ff367ad90ab29568a5719b2ec07a2c2a6d8e8
    
    Added AUTORECONF because the github tarball does not include a
    configure script.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libdmtx/libdmtx.mk
+++ package/libdmtx/libdmtx.mk
@@ -4,10 +4,19 @@
 #
 ################################################################################
 
-LIBDMTX_VERSION = 0.7.4
-LIBDMTX_SITE = http://downloads.sourceforge.net/project/libdmtx/libdmtx/$(LIBDMTX_VERSION)
+LIBDMTX_VERSION = 0.7.5
+LIBDMTX_SITE = $(call github,dmtx,libdmtx,v$(LIBDMTX_VERSION))
 LIBDMTX_LICENSE = BSD-2-Clause or Special Permission
 LIBDMTX_LICENSE_FILES = LICENSE
 LIBDMTX_INSTALL_STAGING = YES
+# github tarball does not include configure
+LIBDMTX_AUTORECONF = YES
+
+# Create the m4 directory (missing from the archive).
+# This is required for autoconf.
+define LIBDMTX_CREATE_M4
+	mkdir -p $(@D)/m4
+endef
+LIBDMTX_PRE_CONFIGURE_HOOKS += LIBDMTX_CREATE_M4
 
 $(eval $(autotools-package))
