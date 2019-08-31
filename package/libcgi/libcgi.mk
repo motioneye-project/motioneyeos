00df4cdabf75938556bb1b17b1c104ff0f6a1de1
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-08-31 12:23:00 +0200

    package/libcgi: bump version to 1.3.0
    Removed patches applied upstream:
    https://github.com/rafaelsteil/libcgi/commit/d7cf836905ab8a46034295023a1a5677dd4c7ce5
    https://github.com/rafaelsteil/libcgi/commit/58edd5089011f7d9846e9336670f90d135d1c580
    
    Added upstream-provided md5 hash.
    
    Upstream moved and renamed license file COPYING and added MIT license
    file:
    https://github.com/rafaelsteil/libcgi/commit/86e88b4b050fa5db1a5062e74429170e49dbde5a
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Acked-by: Alexander Dahl <post@lespocky.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libcgi/libcgi.mk
+++ package/libcgi/libcgi.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-LIBCGI_VERSION = 1.1
+LIBCGI_VERSION = 1.3.0
 LIBCGI_SITE = https://github.com/rafaelsteil/libcgi/releases/download/v$(LIBCGI_VERSION)
 
 LIBCGI_INSTALL_STAGING = YES
-LIBCGI_LICENSE = LGPL-2.1+
-LIBCGI_LICENSE_FILES = COPYING
+LIBCGI_LICENSE = LGPL-2.1+, MIT (base64.c)
+LIBCGI_LICENSE_FILES = LICENSES/LGPL-2.1.txt LICENSES/MIT.txt
 
 $(eval $(cmake-package))
