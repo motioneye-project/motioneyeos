3c9265dd79bd328e50d8ec11984ba7232b3ec1f3
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-09-04 20:50:22 +0200

    package/vnstat: bump version to 2.4
    Added license hash.
    
    vnstat as of version 2.0 requires sqlite:
    https://github.com/vergoh/vnstat/commit/bb24d7bd0f5e9051872f88a98f8b7906b4959aa7
    
    Added --disable-extra-paths to _CONF_OPTS to prevent build error:
    https://github.com/vergoh/vnstat/commit/fffc15d877edf37ff42ab057e1f30c70e6e99314
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/vnstat/vnstat.mk
+++ package/vnstat/vnstat.mk
@@ -4,17 +4,18 @@
 #
 ################################################################################
 
-VNSTAT_VERSION = 1.17
+VNSTAT_VERSION = 2.4
 VNSTAT_SITE = http://humdi.net/vnstat
 VNSTAT_LICENSE = GPL-2.0
 VNSTAT_LICENSE_FILES = COPYING
-VNSTAT_DEPENDENCIES = host-pkgconf
+VNSTAT_DEPENDENCIES = host-pkgconf sqlite
+VNSTAT_CONF_OPTS = --disable-extra-paths
 
 ifeq ($(BR2_PACKAGE_GD)$(BR2_PACKAGE_LIBPNG),yy)
 VNSTAT_DEPENDENCIES += gd
-VNSTAT_CONF_OPTS = --enable-image-output
+VNSTAT_CONF_OPTS += --enable-image-output
 else
-VNSTAT_CONF_OPTS = --disable-image-output
+VNSTAT_CONF_OPTS += --disable-image-output
 endif
 
 $(eval $(autotools-package))
