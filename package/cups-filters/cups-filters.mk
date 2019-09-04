fe519ffe0c060ab5a64180407d9fe05a0cf4e58d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-09-04 19:47:47 +0200

    package/cups-filters: bump to version 1.25.4
    Update hash of license file (year, authors, files have been updated)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/cups-filters/cups-filters.mk
+++ package/cups-filters/cups-filters.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CUPS_FILTERS_VERSION = 1.23.0
+CUPS_FILTERS_VERSION = 1.25.4
 CUPS_FILTERS_SITE = http://openprinting.org/download/cups-filters
 CUPS_FILTERS_LICENSE = GPL-2.0, GPL-2.0+, GPL-3.0, GPL-3.0+, LGPL-2, LGPL-2.1+, MIT, BSD-4-Clause
 CUPS_FILTERS_LICENSE_FILES = COPYING
