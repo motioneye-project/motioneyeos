0ebdc82ca61150644d15eee74f920c4e8dfd6b30
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-09-04 23:32:56 +0200

    package/ltp-testsuite: fix build with glibc 2.30
    Fixes:
     - http://autobuild.buildroot.org/results/e8b72f5d93d3565d41364a52c0024a0292a06b41
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/ltp-testsuite/ltp-testsuite.mk
+++ package/ltp-testsuite/ltp-testsuite.mk
@@ -9,6 +9,8 @@ LTP_TESTSUITE_SOURCE = ltp-full-$(LTP_TESTSUITE_VERSION).tar.xz
 LTP_TESTSUITE_SITE = https://github.com/linux-test-project/ltp/releases/download/$(LTP_TESTSUITE_VERSION)
 LTP_TESTSUITE_LICENSE = GPL-2.0, GPL-2.0+
 LTP_TESTSUITE_LICENSE_FILES = COPYING
+# We're patching configure.ac
+LTP_TESTSUITE_AUTORECONF = YES
 
 LTP_TESTSUITE_CONF_OPTS += \
 	--with-realtime-testsuite --with-open-posix-testsuite
