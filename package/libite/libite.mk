00135b03946ab850dbeadc2a06e2ed96568cb17c
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-09-04 20:03:20 +0200

    package/libite: bump version to 2.1.0
    Added md5 hash provided by upstream.
    Switched _SOURCE to .xz tarball provided by upstream.
    This tarball contains a configure script so we do not need to
    autoreconf anymore.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libite/libite.mk
+++ package/libite/libite.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-LIBITE_VERSION = 2.0.2
-LIBITE_SITE = $(call github,troglobit,libite,v$(LIBITE_VERSION))
+LIBITE_VERSION = 2.1.0
+LIBITE_SOURCE = libite-$(LIBITE_VERSION).tar.xz
+LIBITE_SITE = https://github.com/troglobit/libite/releases/download/v$(LIBITE_VERSION)
 LIBITE_LICENSE = MIT, X11, ISC, BSD-2-Clause
 LIBITE_LICENSE_FILES = LICENSE src/chomp.c src/pidfile.c
 LIBITE_INSTALL_STAGING = YES
-LIBITE_AUTORECONF = YES
 
 $(eval $(autotools-package))
