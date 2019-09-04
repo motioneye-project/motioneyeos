d2355d88e7343197d71e3d40a5d1edca025838b4
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-09-04 23:21:17 +0200

    package/ytree: new package
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/ytree/ytree.mk
@@ -0,0 +1,31 @@
+################################################################################
+#
+# ytree
+#
+################################################################################
+
+YTREE_VERSION = 1.99pl1
+YTREE_SITE = https://www.han.de/~werner
+YTREE_LICENSE = GPL-2.0
+YTREE_LICENSE_FILES = COPYING
+YTREE_DEPENDENCIES = ncurses
+
+YTREE_CFLAGS = -DCOLOR_SUPPORT $(TARGET_CFLAGS)
+YTREE_LDFLAGS = -lncurses $(TARGET_LDFLAGS)
+
+ifeq ($(BR2_PACKAGE_READLINE),y)
+YTREE_DEPENDENCIES += readline
+YTREE_CFLAGS += -DREADLINE_SUPPORT
+YTREE_LDFLAGS += -lreadline
+endif
+
+define YTREE_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS) \
+		CFLAGS="$(YTREE_CFLAGS)" LDFLAGS="$(YTREE_LDFLAGS)"
+endef
+
+define YTREE_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR="$(TARGET_DIR)/usr" install
+endef
+
+$(eval $(generic-package))
