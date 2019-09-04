fc4ee4063b45afd1351f9594ccc08d81d4374607
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-09-04 20:12:21 +0200

    package/dav1d: disable tests and tools
    Some tools need SDL2 which will break the build if SDL2 is found on host
    
    Fixes:
     - http://autobuild.buildroot.org/results/4cea773805e62911019d0627dcdf5d17b71032b7
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/dav1d/dav1d.mk
+++ package/dav1d/dav1d.mk
@@ -10,6 +10,9 @@ DAV1D_SITE = https://code.videolan.org/videolan/dav1d/-/archive/$(DAV1D_VERSION)
 DAV1D_LICENSE = BSD-2-Clause
 DAV1D_LICENSE_FILES = COPYING
 DAV1D_INSTALL_STAGING = YES
+DAV1D_CONF_OPTS = \
+	-Denable_tests=false \
+	-Denable_tools=false
 
 ifeq ($(BR2_i386)$(BR2_x86_64),y)
 DAV1D_DEPENDENCIES += host-nasm
@@ -17,7 +20,7 @@ endif
 
 # ARM assembly requires v6+ ISA
 ifeq ($(BR2_ARM_CPU_ARMV4)$(BR2_ARM_CPU_ARMV5)$(BR2_ARM_CPU_ARMV7M),y)
-DAV1D_CONF_OPTS = -Denable_asm=false
+DAV1D_CONF_OPTS += -Denable_asm=false
 endif
 
 # Uses __atomic_fetch_add_4
