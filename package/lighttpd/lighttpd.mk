7af499631cc5036e71217d39052b3f89c3359b3e
Pierre-Jean Texier <pjtexier@koncepto.io>
2019-09-05 21:11:40 +0200

    package/lighttpd: bump to version 1.4.54
    See http://www.lighttpd.net/2019/5/27/1.4.54/
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/lighttpd/lighttpd.mk
+++ package/lighttpd/lighttpd.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIGHTTPD_VERSION_MAJOR = 1.4
-LIGHTTPD_VERSION = $(LIGHTTPD_VERSION_MAJOR).53
+LIGHTTPD_VERSION = $(LIGHTTPD_VERSION_MAJOR).54
 LIGHTTPD_SOURCE = lighttpd-$(LIGHTTPD_VERSION).tar.xz
 LIGHTTPD_SITE = http://download.lighttpd.net/lighttpd/releases-$(LIGHTTPD_VERSION_MAJOR).x
 LIGHTTPD_LICENSE = BSD-3-Clause
