965e26fd999edb8f14b44be54ffd872293da93c6
Peter Korsgaard <peter@korsgaard.com>
2019-09-06 17:46:55 +0200

    package/asterisk: security bump to version 16.5.1
    Fixes the following security issues:
    
    AST-2019-004: Crash when negotiating for T.38 with a declined stream
    When Asterisk sends a re-invite initiating T.38 faxing, and the endpoint
    responds with a declined media stream a crash will then occur in Asterisk.
    https://downloads.asterisk.org/pub/security/AST-2019-004.pdf
    
    AST-2019-005: Remote Crash Vulnerability in audio transcoding
    When audio frames are given to the audio transcoding support in Asterisk the
    number of samples are examined and as part of this a message is output to
    indicate that no samples are present. A change was done to suppress this
    message for a particular scenario in which the message was not relevant. This
    change assumed that information about the origin of a frame will always exist
    when in reality it may not.
    https://downloads.asterisk.org/pub/security/AST-2019-005.pdf
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/asterisk/asterisk.mk
+++ package/asterisk/asterisk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ASTERISK_VERSION = 16.5.0
+ASTERISK_VERSION = 16.5.1
 # Use the github mirror: it's an official mirror maintained by Digium, and
 # provides tarballs, which the main Asterisk git tree (behind Gerrit) does not.
 ASTERISK_SITE = $(call github,asterisk,asterisk,$(ASTERISK_VERSION))
