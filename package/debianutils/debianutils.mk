1f6cf3c1810cdbc530e4ae241661ecc72d07621d
Christopher McCrory <chrismcc@gmail.com>
2019-09-08 09:09:53 -0700

    package/debianutils: bump to version 4.8.6.3
    updated _SITE
    
    Signed-off-by: Christopher McCrory <chrismcc@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/debianutils/debianutils.mk
+++ package/debianutils/debianutils.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-DEBIANUTILS_VERSION = 4.8.1
+DEBIANUTILS_VERSION = 4.8.6.3
 DEBIANUTILS_SOURCE = debianutils_$(DEBIANUTILS_VERSION).tar.xz
-DEBIANUTILS_SITE = http://snapshot.debian.org/archive/debian/20161118T033019Z/pool/main/d/debianutils
+DEBIANUTILS_SITE = http://snapshot.debian.org/archive/debian/20190828T161440Z/pool/main/d/debianutils
 DEBIANUTILS_CONF_OPTS = --exec-prefix=/
 DEBIANUTILS_LICENSE = GPL-2.0+, SMAIL (savelog)
 DEBIANUTILS_LICENSE_FILES = debian/copyright
