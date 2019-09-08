8136a0a083e6d46525198a07a554edc1ba198e58
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-09-08 10:01:39 +0200

    package/aespipe: bump to version 2.4e
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/aespipe/aespipe.mk
+++ package/aespipe/aespipe.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AESPIPE_VERSION = 2.4d
+AESPIPE_VERSION = 2.4e
 AESPIPE_SOURCE = aespipe-v$(AESPIPE_VERSION).tar.bz2
 AESPIPE_SITE = http://loop-aes.sourceforge.net/aespipe
 AESPIPE_LICENSE = GPL
