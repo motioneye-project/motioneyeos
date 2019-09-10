41278d88e317ecd0adfc33f882d34f0b3142cba8
Francois Perrad <fperrad@gmail.com>
2019-09-10 05:30:07 +0200

    package/luarocks: bump to version 3.2.1
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/luarocks/luarocks.mk
+++ package/luarocks/luarocks.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUAROCKS_VERSION = 3.1.3
+LUAROCKS_VERSION = 3.2.1
 LUAROCKS_SITE = http://luarocks.org/releases
 LUAROCKS_LICENSE = MIT
 LUAROCKS_LICENSE_FILES = COPYING
