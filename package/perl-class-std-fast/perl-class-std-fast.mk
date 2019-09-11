e0ada56a2acd1eae7b7684de6cca12b0c204442f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-09-11 19:01:50 +0200

    package/perl-class-std-fast: fixup the 'v' prefix in the version
    In order for the perl-class-std-fast version to match what is given by
    release-monitoring.org, the 'v' prefix should be encoded in
    PERL_CLASS_STD_FAST_SOURCE and not PERL_CLASS_STD_FAST_VERSION.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/perl-class-std-fast/perl-class-std-fast.mk
+++ package/perl-class-std-fast/perl-class-std-fast.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PERL_CLASS_STD_FAST_VERSION = v0.0.8
-PERL_CLASS_STD_FAST_SOURCE = Class-Std-Fast-$(PERL_CLASS_STD_FAST_VERSION).tar.gz
+PERL_CLASS_STD_FAST_VERSION = 0.0.8
+PERL_CLASS_STD_FAST_SOURCE = Class-Std-Fast-v$(PERL_CLASS_STD_FAST_VERSION).tar.gz
 PERL_CLASS_STD_FAST_SITE = $(BR2_CPAN_MIRROR)/authors/id/A/AC/ACID
 PERL_CLASS_STD_FAST_DEPENDENCIES = host-perl-module-build
 PERL_CLASS_STD_FAST_LICENSE = Artistic or GPL-1.0+
