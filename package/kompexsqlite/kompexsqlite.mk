047f4bf128558ad64bfa65173bf2c24c565a86ce
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-09-11 19:22:56 +0200

    package/kompexsqlite: fixup the 'v' prefix in the version
    In order for the kompexsqlite version to match what is given by
    release-monitoring.org, the 'v' prefix should be encoded in
    KOMPEXSQLITE_SOURCE and not KOMPEXSQLITE_VERSION.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kompexsqlite/kompexsqlite.mk
+++ package/kompexsqlite/kompexsqlite.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-KOMPEXSQLITE_VERSION = v1.10.12-1
-KOMPEXSQLITE_SOURCE = $(KOMPEXSQLITE_VERSION).tar.gz
+KOMPEXSQLITE_VERSION = 1.10.12-1
+KOMPEXSQLITE_SOURCE = v$(KOMPEXSQLITE_VERSION).tar.gz
 KOMPEXSQLITE_SITE = https://github.com/Aethelflaed/kompex-sqlite-wrapper/archive
 KOMPEXSQLITE_INSTALL_STAGING = YES
 KOMPEXSQLITE_LICENSE = LGPL-3.0+ (wrapper), Public Domain (bundled sqlite)
