ca3232de359b20495c1f242c6a67791a35d6d21d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-09-14 16:52:42 +0200

    package/kompexsqlite: use github macro
    Use github macro and drop SOURCE variable to keep the default SOURCE
    value which gives a much more sensible tarball name
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kompexsqlite/kompexsqlite.mk
+++ package/kompexsqlite/kompexsqlite.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 KOMPEXSQLITE_VERSION = 1.10.12-1
-KOMPEXSQLITE_SOURCE = v$(KOMPEXSQLITE_VERSION).tar.gz
-KOMPEXSQLITE_SITE = https://github.com/Aethelflaed/kompex-sqlite-wrapper/archive
+KOMPEXSQLITE_SITE = \
+	$(call github,Aethelflaed,kompex-sqlite-wrapper,v$(KOMPEXSQLITE_VERSION))
 KOMPEXSQLITE_INSTALL_STAGING = YES
 KOMPEXSQLITE_LICENSE = LGPL-3.0+ (wrapper), Public Domain (bundled sqlite)
 KOMPEXSQLITE_LICENSE_FILES = LICENSE.txt
