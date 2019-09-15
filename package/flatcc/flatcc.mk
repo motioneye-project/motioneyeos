af926937421b0a3eab17e7b7c49d893e0b559915
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-09-15 13:43:58 +0200

    package/flatcc: bump to version 0.6.0
    Remove patch (already in version)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/flatcc/flatcc.mk
+++ package/flatcc/flatcc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FLATCC_VERSION = 0.5.2
+FLATCC_VERSION = 0.6.0
 FLATCC_SITE = $(call github,dvidelabs,flatcc,v$(FLATCC_VERSION))
 FLATCC_LICENSE = Apache-2.0
 FLATCC_LICENSE_FILES = LICENSE
