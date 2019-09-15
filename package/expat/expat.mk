386794d02ef030e14adfabc5056b54a338bd717f
Peter Korsgaard <peter@korsgaard.com>
2019-09-15 22:21:42 +0200

    package/expat: security bump to version 2.2.8
    Fixes the following security vulnerability:
    
    CVE-2019-15903: In libexpat before 2.2.8, crafted XML input could fool the
    parser into changing from DTD parsing to document parsing too early; a
    consecutive call to XML_GetCurrentLineNumber (or XML_GetCurrentColumnNumber)
    then resulted in a heap-based buffer over-read.
    
    While we're at it, also change to use .tar.xz rather than the bigger
    .tar.bz2.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/expat/expat.mk
+++ package/expat/expat.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-EXPAT_VERSION = 2.2.7
+EXPAT_VERSION = 2.2.8
 EXPAT_SITE = http://downloads.sourceforge.net/project/expat/expat/$(EXPAT_VERSION)
-EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.bz2
+EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.xz
 EXPAT_INSTALL_STAGING = YES
 EXPAT_DEPENDENCIES = host-pkgconf
 HOST_EXPAT_DEPENDENCIES = host-pkgconf
