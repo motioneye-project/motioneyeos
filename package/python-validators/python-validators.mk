06a07a8da9883292477d742c1e0af6fdf4fdd1b7
Peter Korsgaard <peter@korsgaard.com>
2019-09-16 16:34:25 +0200

    package/python-validators: bump version to 0.12.6
    Includes a number of post-0.12.2 fixes, including a python 3.7 compatibility
    fix:
    
    https://github.com/kvesteri/validators/blob/master/CHANGES.rst
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-validators/python-validators.mk
+++ package/python-validators/python-validators.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_VALIDATORS_VERSION = 0.12.2
+PYTHON_VALIDATORS_VERSION = 0.12.6
 PYTHON_VALIDATORS_SOURCE = validators-$(PYTHON_VALIDATORS_VERSION).tar.gz
-PYTHON_VALIDATORS_SITE = https://files.pythonhosted.org/packages/45/7b/5b7b74208a3e0744d1a0efbfb1935fa46fa4cfe58d3d63f17c49c58c429c
+PYTHON_VALIDATORS_SITE = https://files.pythonhosted.org/packages/8f/cd/1571ece4bf93e02143449417d244daa89dfc46288110b096b81e84aa6ddd
 PYTHON_VALIDATORS_SETUP_TYPE = setuptools
 PYTHON_VALIDATORS_LICENSE = BSD
 PYTHON_VALIDATORS_LICENSE_FILES = LICENSE
