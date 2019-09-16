44eeec471df8321308de75514d5f8254dc36027a
Pierre-Jean Texier <pjtexier@koncepto.io>
2019-09-16 17:39:12 +0200

    package/python-periphery: bump to version 1.1.2
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-periphery/python-periphery.mk
+++ package/python-periphery/python-periphery.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PERIPHERY_VERSION = 1.1.1
+PYTHON_PERIPHERY_VERSION = 1.1.2
 PYTHON_PERIPHERY_SITE = $(call github,vsergeev,python-periphery,v$(PYTHON_PERIPHERY_VERSION))
 PYTHON_PERIPHERY_LICENSE = MIT
 PYTHON_PERIPHERY_LICENSE_FILES = LICENSE
