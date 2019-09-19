82dba486928df660bb8e6e576ed21a7c65568804
Peter Seiderer <ps.report@gmx.net>
2019-09-19 22:15:16 +0200

    package/libevdev: bump version to 1.8.0
    For details see [1].
    
    [1] https://lists.freedesktop.org/archives/input-tools/2019-August/001521.html
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libevdev/libevdev.mk
+++ package/libevdev/libevdev.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBEVDEV_VERSION = 1.7.0
+LIBEVDEV_VERSION = 1.8.0
 LIBEVDEV_SITE = http://www.freedesktop.org/software/libevdev
 LIBEVDEV_SOURCE = libevdev-$(LIBEVDEV_VERSION).tar.xz
 LIBEVDEV_LICENSE = X11
