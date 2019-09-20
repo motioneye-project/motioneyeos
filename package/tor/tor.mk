36079df92f19f61a7cdb20f5350ecf785fc8ec69
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-09-20 18:54:26 +0200

    package/tor: bump version to 0.4.1.6
    Release notes: https://blog.torproject.org/new-release-tor-0416
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tor/tor.mk
+++ package/tor/tor.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TOR_VERSION = 0.4.0.5
+TOR_VERSION = 0.4.1.6
 TOR_SITE = https://dist.torproject.org
 TOR_LICENSE = BSD-3-Clause
 TOR_LICENSE_FILES = LICENSE
