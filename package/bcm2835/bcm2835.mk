414fb481429bbf22227130482a7e4c9171c15fa9
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-09-20 19:10:45 +0200

    package/bcm2835: bump to version 1.60
    Add hash for license file
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/bcm2835/bcm2835.mk
+++ package/bcm2835/bcm2835.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BCM2835_VERSION = 1.48
+BCM2835_VERSION = 1.60
 BCM2835_SITE = http://www.airspayce.com/mikem/bcm2835
 BCM2835_LICENSE = GPL-2.0
 BCM2835_LICENSE_FILES = COPYING
