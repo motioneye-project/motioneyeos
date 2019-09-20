f58ea370da70a3bbda174f77d3094975bb142161
Peter Korsgaard <peter@korsgaard.com>
2019-09-20 09:23:43 +0200

    package/bwm-ng: license is GPL-2.0+
    The source files contain the "(at your option) any later version" text, so
    change the licese to GPL-2.0+.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/bwm-ng/bwm-ng.mk
+++ package/bwm-ng/bwm-ng.mk
@@ -7,7 +7,7 @@
 BWM_NG_VERSION = 0.6.2
 BWM_NG_SITE = $(call github,vgropp,bwm-ng,v$(BWM_NG_VERSION))
 BWM_NG_CONF_OPTS = --with-procnetdev --with-diskstats
-BWM_NG_LICENSE = GPL-2.0
+BWM_NG_LICENSE = GPL-2.0+
 BWM_NG_LICENSE_FILES = COPYING
 BWM_NG_AUTORECONF = YES
 
