c5568f998520ff26d446b9c94850f7e03e9e3863
Peter Korsgaard <peter@korsgaard.com>
2019-09-20 08:09:57 +0200

    package/docker-cli: bump version to 18.09.9
    Includes a number of post-18.09.7 bugfixes and to keep in sync with the
    docker-engine version.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/docker-cli/docker-cli.mk
+++ package/docker-cli/docker-cli.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOCKER_CLI_VERSION = 18.09.7
+DOCKER_CLI_VERSION = 18.09.9
 DOCKER_CLI_SITE = $(call github,docker,cli,v$(DOCKER_CLI_VERSION))
 DOCKER_CLI_WORKSPACE = gopath
 
