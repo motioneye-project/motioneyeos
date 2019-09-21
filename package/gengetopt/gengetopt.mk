dbfee0ed986125561ca3f3ceebbb5a3212e7deb2
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-09-21 18:54:52 +0200

    package/gengetopt: bump to version 2.23
    - Remove patch (not needed since
      https://git.savannah.gnu.org/cgit/gengetopt.git/commit/?id=9769f643d2e21379b8f7a3fd52ef77184fa753f6)
    - Enable parallel build (fixed since
      https://git.savannah.gnu.org/cgit/gengetopt.git/commit/?id=8e6fa4d9d63c680b84ba872e8e581eb01ae3bc09)
    - Add hash for license files
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/gengetopt/gengetopt.mk
+++ package/gengetopt/gengetopt.mk
@@ -4,12 +4,10 @@
 #
 ################################################################################
 
-GENGETOPT_VERSION = 2.22.6
+GENGETOPT_VERSION = 2.23
+GENGETOPT_SOURCE = gengetopt-$(GENGETOPT_VERSION).tar.xz
 GENGETOPT_SITE = $(BR2_GNU_MIRROR)/gengetopt
 GENGETOPT_LICENSE = GPL-3.0+
 GENGETOPT_LICENSE_FILES = COPYING LICENSE
-
-# Parallel build broken
-GENGETOPT_MAKE = $(MAKE1)
 
 $(eval $(host-autotools-package))
