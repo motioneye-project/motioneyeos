219586ec5ae58f115f5a5e868399b2e52df707d7
Sergey Matyukevich <geomatsi@gmail.com>
2019-09-21 18:00:06 +0300

    boot/mv-ddr-marvell: bump version to armada-18.12
    Bump version of Marvell DDR training software. This update accompanies
    update of the ARM Trusted Firmware used on Marvell A8K platforms.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- boot/mv-ddr-marvell/mv-ddr-marvell.mk
+++ boot/mv-ddr-marvell/mv-ddr-marvell.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-# This is the commit for mv_ddr-armada-18.09.2
-MV_DDR_MARVELL_VERSION = 99d772547314f84921268d57e53d8769197d3e21
+# This is the commit for mv_ddr-armada-18.12.0
+MV_DDR_MARVELL_VERSION = 618dadd1491eb2f7b2fd74313c04f7accddae475
 MV_DDR_MARVELL_SITE = $(call github,MarvellEmbeddedProcessors,mv-ddr-marvell,$(MV_DDR_MARVELL_VERSION))
 MV_DDR_MARVELL_LICENSE = GPL-2.0+ or LGPL-2.1 with freertos-exception-2.0, BSD-3-Clause, Marvell Commercial
 MV_DDR_MARVELL_LICENSE_FILES = ddr3_init.c
