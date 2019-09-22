0d8c6ed1416e513efb0713f4eec9e463741be24b
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-09-22 09:40:02 +0200

    package/gengetopt: disable documentation
    Add a patch to disable documentation through a --disable-doc option
    which is already passed by autotools infrastructure
    
    Fixes:
     - http://autobuild.buildroot.org/results/63554a4168a69a0e68a222bee2269b5aaeee2021
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/gengetopt/gengetopt.mk
+++ package/gengetopt/gengetopt.mk
@@ -9,5 +9,7 @@ GENGETOPT_SOURCE = gengetopt-$(GENGETOPT_VERSION).tar.xz
 GENGETOPT_SITE = $(BR2_GNU_MIRROR)/gengetopt
 GENGETOPT_LICENSE = GPL-3.0+
 GENGETOPT_LICENSE_FILES = COPYING LICENSE
+# We're patching configure.ac
+HOST_GENGETOPT_AUTORECONF = YES
 
 $(eval $(host-autotools-package))
