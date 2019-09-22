b4e0068a74ecc26ec19838cb9df3a48ecca38875
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-09-22 13:57:56 +0200

    package/sispmctl: bump to version 4.1
    - Remove patch (already in version)
    - Change license file to COPYING and Add its hash
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sispmctl/sispmctl.mk
+++ package/sispmctl/sispmctl.mk
@@ -4,15 +4,12 @@
 #
 ################################################################################
 
-SISPMCTL_VERSION = 3.1
+SISPMCTL_VERSION = 4.1
 SISPMCTL_SITE = http://downloads.sourceforge.net/project/sispmctl/sispmctl/sispmctl-$(SISPMCTL_VERSION)
 SISPMCTL_LICENSE = GPL-2.0+
-SISPMCTL_LICENSE_FILES = LICENCE
+SISPMCTL_LICENSE_FILES = COPYING
 SISPMCTL_DEPENDENCIES = libusb-compat
 
 SISPMCTL_CONF_OPTS = --enable-webless
-
-# We're patching configure.in
-SISPMCTL_AUTORECONF = YES
 
 $(eval $(autotools-package))
