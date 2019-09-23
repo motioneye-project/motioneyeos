e1c43f1f2ba2f7d12b20b00feee2f9aba5bdd3ad
Peter Macleod Thompson <peter.macleod.thompson@gmail.com>
2019-09-23 17:15:43 -0600

    package/sdl2_image: bump to version 2.0.5
    The hash of the license is updated because the copyright year was
    updated in COPYING.txt:
    
    -  Copyright (C) 1997-2018 Sam Lantinga <slouken@libsdl.org>
    +  Copyright (C) 1997-2019 Sam Lantinga <slouken@libsdl.org>
    
    Signed-off-by: Peter Macleod Thompson <peter.macleod.thompson@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/sdl2_image/sdl2_image.mk
+++ package/sdl2_image/sdl2_image.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SDL2_IMAGE_VERSION = 2.0.3
+SDL2_IMAGE_VERSION = 2.0.5
 SDL2_IMAGE_SOURCE = SDL2_image-$(SDL2_IMAGE_VERSION).tar.gz
 SDL2_IMAGE_SITE = http://www.libsdl.org/projects/SDL_image/release
 SDL2_IMAGE_INSTALL_STAGING = YES
