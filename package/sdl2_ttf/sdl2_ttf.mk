5bef0879162de8a0cbd6dcfccebeefefb87b8a9e
Peter Macleod Thompson <peter.macleod.thompson@gmail.com>
2019-09-23 10:45:53 -0600

    package/sdl2_ttf: bump to version 2.0.15
    The patch 0001-fix-opengl-detection.patch is removed because it is now
    upstream. Thanks to this, autoreconf is no longer needed, which allows
    us to drop AUTORECONF = YES and the 0002-fix-autoreconfig.patch which
    was only needed to fix autoreconf.
    
    Signed-off-by: Peter Macleod Thompson <peter.macleod.thompson@gmail.com>
    [Thomas: improve commit log]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/sdl2_ttf/sdl2_ttf.mk
+++ package/sdl2_ttf/sdl2_ttf.mk
@@ -4,16 +4,13 @@
 #
 ################################################################################
 
-SDL2_TTF_VERSION = 2.0.14
+SDL2_TTF_VERSION = 2.0.15
 SDL2_TTF_SOURCE = SDL2_ttf-$(SDL2_TTF_VERSION).tar.gz
 SDL2_TTF_SITE = http://www.libsdl.org/projects/SDL_ttf/release
 SDL2_TTF_LICENSE = Zlib
 SDL2_TTF_LICENSE_FILES = COPYING.txt
 SDL2_TTF_INSTALL_STAGING = YES
 SDL2_TTF_DEPENDENCIES = sdl2 freetype host-pkgconf
-
-# Two patches touching configure.in
-SDL2_TTF_AUTORECONF = YES
 
 SDL2_TTF_CONF_ENV = \
 	FREETYPE_CONFIG=$(STAGING_DIR)/usr/bin/freetype-config
