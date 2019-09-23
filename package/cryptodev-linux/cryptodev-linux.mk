c0f12fa1af64962d6e6033e5a73cb7e1d4c17101
Pierre-Jean Texier <pjtexier@koncepto.io>
2019-09-23 21:01:09 +0200

    package/cryptodev-linux: bump to version a1e738a
    This includes the following changes:
    
    a1e738a tests: cipher-aead-srtp: Fix cao->dst alignment
    f971e0c Fix module loading with Linux v5.0-rc5
    fd8b15e Release version 1.10
    6b0a81c add CIOCCPHASH to copy hash state between sessions
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/cryptodev-linux/cryptodev-linux.mk
+++ package/cryptodev-linux/cryptodev-linux.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CRYPTODEV_LINUX_VERSION = f60aa08c63fc02780554a0a12180a478ca27d49f
+CRYPTODEV_LINUX_VERSION = a1e738aef6249ee3bddf997e91e59b1210553209
 CRYPTODEV_LINUX_SITE = $(call github,cryptodev-linux,cryptodev-linux,$(CRYPTODEV_LINUX_VERSION))
 CRYPTODEV_LINUX_INSTALL_STAGING = YES
 CRYPTODEV_LINUX_LICENSE = GPL-2.0+
