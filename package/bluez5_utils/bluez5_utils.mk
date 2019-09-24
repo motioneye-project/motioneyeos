74f6a8f8e3c770639537ce51fef8e91e17cc59b8
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-09-24 07:09:38 +0200

    package/{bluez5_utils, bluez5_utils-headers}: bump version to 5.51
    Release notes: http://www.bluez.org/release-of-bluez-5-51/
    
    Removed patch applied upstream.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/bluez5_utils/bluez5_utils.mk
+++ package/bluez5_utils/bluez5_utils.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # Keep the version and patches in sync with bluez5_utils-headers
-BLUEZ5_UTILS_VERSION = 5.50
+BLUEZ5_UTILS_VERSION = 5.51
 BLUEZ5_UTILS_SOURCE = bluez-$(BLUEZ5_UTILS_VERSION).tar.xz
 BLUEZ5_UTILS_SITE = $(BR2_KERNEL_MIRROR)/linux/bluetooth
 BLUEZ5_UTILS_INSTALL_STAGING = YES
