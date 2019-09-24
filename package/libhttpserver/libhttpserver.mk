a2901ac82cf0f9715c49eb9dedd75942ac726139
Stephan Hoffmann <stephan.hoffmann@ext.grandcentrix.net>
2019-09-24 14:27:54 +0200

    package/libhttpserver: new package
    libhttpserver is a C++ library for building high performance RESTfuls
    web servers. libhttpserver is built upon libmicrohttpd to provide a
    simple API for developers to create HTTP services in C++.
    
    The installed pkg-config file contains an error, thus we include the
    patch 458d16b922304006fe418897044e14b0544a127a from the upstream repo
    that is not yet released.
    
    Signed-off-by: Stephan Hoffmann <stephan.hoffmann@ext.grandcentrix.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/libhttpserver/libhttpserver.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# libhttpserver
+#
+################################################################################
+
+LIBHTTPSERVER_VERSION = 0.17.5
+LIBHTTPSERVER_SITE = $(call github,etr,libhttpserver,$(LIBHTTPSERVER_VERSION))
+LIBHTTPSERVER_LICENSE = LGPL-2.1+
+LIBHTTPSERVER_LICENSE_FILES = COPYING.LESSER
+LIBHTTPSERVER_INSTALL_STAGING = YES
+LIBHTTPSERVER_CONF_OPTS = --enable-same-directory-build
+LIBHTTPSERVER_AUTORECONF = YES
+LIBHTTPSERVER_DEPENDENCIES = libmicrohttpd
+
+$(eval $(autotools-package))
