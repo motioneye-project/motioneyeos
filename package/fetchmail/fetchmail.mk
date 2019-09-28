69343d2614bab87ea2e332f30df1c7cf95a89479
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-09-28 14:38:18 +0200

    package/fetchmail: bump version to 6.4.1
    Added hashes provided by upstream.
    
    Updated license hash after upstream commit:
    https://sourceforge.net/p/fetchmail/git/ci/51ea9ddcef4b0e513a493f62a4bd93e41ff201ea/
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/fetchmail/fetchmail.mk
+++ package/fetchmail/fetchmail.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 FETCHMAIL_VERSION_MAJOR = 6.4
-FETCHMAIL_VERSION = $(FETCHMAIL_VERSION_MAJOR).0
+FETCHMAIL_VERSION = $(FETCHMAIL_VERSION_MAJOR).1
 FETCHMAIL_SOURCE = fetchmail-$(FETCHMAIL_VERSION).tar.xz
 FETCHMAIL_SITE = http://downloads.sourceforge.net/project/fetchmail/branch_$(FETCHMAIL_VERSION_MAJOR)
 FETCHMAIL_LICENSE = GPL-2.0; some exceptions are mentioned in COPYING
