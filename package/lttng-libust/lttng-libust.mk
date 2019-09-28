91423befd5a05758bd2a02b1403aa46346913958
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-09-28 09:27:55 +0200

    package/lttng-libust: bump to version 2.10.5
    - Remove second patch (already in version)
    - Add hash for license file
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/lttng-libust/lttng-libust.mk
+++ package/lttng-libust/lttng-libust.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LTTNG_LIBUST_SITE = http://lttng.org/files/lttng-ust
-LTTNG_LIBUST_VERSION = 2.10.1
+LTTNG_LIBUST_VERSION = 2.10.5
 LTTNG_LIBUST_SOURCE = lttng-ust-$(LTTNG_LIBUST_VERSION).tar.bz2
 LTTNG_LIBUST_LICENSE = LGPL-2.1, MIT (system headers), GPL-2.0 (liblttng-ust-ctl/ustctl.c used by lttng-sessiond)
 LTTNG_LIBUST_LICENSE_FILES = COPYING
