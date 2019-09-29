135267f81da2fe7f77353b9fd10a7ae22f13e490
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-09-29 17:36:03 +0200

    package/utf8proc: new package
    Needed for subversion 1.12.x:
    https://github.com/apache/subversion/commit/472355d1597d7c1c64f20f72ed391fd34c0a8fb0
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/utf8proc/utf8proc.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# utf8proc
+#
+################################################################################
+
+UTF8PROC_VERSION = 2.4.0
+UTF8PROC_SITE = $(call github,JuliaStrings,utf8proc,v$(UTF8PROC_VERSION))
+UTF8PROC_LICENSE = MIT
+UTF8PROC_LICENSE_FILES = LICENSE.md
+UTF8PROC_INSTALL_STAGING = YES
+UTF8PROC_SUPPORTS_IN_SOURCE_BUILD = NO
+
+$(eval $(cmake-package))
