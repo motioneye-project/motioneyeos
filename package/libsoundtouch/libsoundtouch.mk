2203d4df9aadf952a62962d5dae1856918b52c97
Gilles Talis <gilles.talis@gmail.com>
2019-09-29 11:42:46 +0200

    package/libsoundtouch: bump to version 2.1.2
    - Updated download site and download method
    - The hash of the license file is updated due to a newline change at
      the end of the file
    
    Signed-off-by: Gilles Talis <gilles.talis@gmail.com>
    Reviewed-by: Giulio Benetti <giulio.benetti@benettiengineering.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libsoundtouch/libsoundtouch.mk
+++ package/libsoundtouch/libsoundtouch.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-LIBSOUNDTOUCH_VERSION = 010a91a59071c7fefd316fca62c0d980ec85b4b1
-LIBSOUNDTOUCH_SITE = https://freeswitch.org/stash/scm/sd/libsoundtouch.git
-LIBSOUNDTOUCH_SITE_METHOD = git
+LIBSOUNDTOUCH_VERSION = 2.1.2
+LIBSOUNDTOUCH_SITE = https://gitlab.com/soundtouch/soundtouch/-/archive/$(LIBSOUNDTOUCH_VERSION)
 LIBSOUNDTOUCH_LICENSE = LGPL-2.1+
 LIBSOUNDTOUCH_LICENSE_FILES = COPYING.TXT
 LIBSOUNDTOUCH_AUTORECONF = YES
