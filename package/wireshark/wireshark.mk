1218a2be41c96f28148d8e121b4c3c22d701172c
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-09-29 19:12:17 +0200

    package/wireshark: security bump to version 3.0.5
    Fixes the following security vulnerabilities (3.0.4):
    - CVE-2019-16319: Gryphon dissector infinite loop
      https://www.wireshark.org/security/wnpa-sec-2019-21
    
    Added upstream sha1 hash.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    [Peter: mention security fixes]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/wireshark/wireshark.mk
+++ package/wireshark/wireshark.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIRESHARK_VERSION = 3.0.3
+WIRESHARK_VERSION = 3.0.5
 WIRESHARK_SOURCE = wireshark-$(WIRESHARK_VERSION).tar.xz
 WIRESHARK_SITE = https://www.wireshark.org/download/src/all-versions
 WIRESHARK_LICENSE = wireshark license
