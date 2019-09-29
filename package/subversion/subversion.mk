f1e48e343a04a7bff4abd5c9c27555cddb71f7e7
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-09-29 20:34:12 +0200

    package/subversion: bump version to 1.12.2
    Added dependencies to lz4 and utf8proc.
    
    Replaced the 0002-disable-macos-specific-features.patch by a simpler
    patch/workaround that still works after the version bump.
    
    Updated license hash after various upstream commits:
    https://github.com/apache/subversion/commits/trunk/LICENSE
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/subversion/subversion.mk
+++ package/subversion/subversion.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SUBVERSION_VERSION = 1.9.12
+SUBVERSION_VERSION = 1.12.2
 SUBVERSION_SOURCE = subversion-$(SUBVERSION_VERSION).tar.bz2
 SUBVERSION_SITE = http://mirror.catn.com/pub/apache/subversion
 SUBVERSION_LICENSE = Apache-2.0
@@ -14,6 +14,8 @@ SUBVERSION_DEPENDENCIES = \
 	apr \
 	apr-util \
 	expat \
+	lz4 \
+	utf8proc \
 	zlib \
 	sqlite \
 	$(TARGET_NLS_DEPENDENCIES)
@@ -22,6 +24,8 @@ SUBVERSION_CONF_OPTS = \
 	--with-expat=$(STAGING_DIR)/usr/include:$(STAGING_DIR)/usr/lib: \
 	--with-apr=$(STAGING_DIR)/usr \
 	--with-apr-util=$(STAGING_DIR)/usr \
+	--with-lz4=$(STAGING_DIR)/usr \
+	--with-utf8proc=$(STAGING_DIR)/usr \
 	--with-zlib=$(STAGING_DIR)/usr \
 	--without-serf \
 	--without-apxs \
