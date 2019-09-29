87f76483b91c1cef5f50dd5b5aace42220656e5c
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2019-09-29 19:10:17 +0200

    package/mdevd: new package
    This new package provides mdevd, a small daemon managing kernel hotplug
    events similar to udevd.
    
    Signed-off-by: Eric Le Bihan <eric.le.bihan.dev@free.fr>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

---
+++ package/mdevd/mdevd.mk
@@ -0,0 +1,39 @@
+################################################################################
+#
+# mdevd
+#
+################################################################################
+
+MDEVD_VERSION = 0.1.1.0
+MDEVD_SITE = http://skarnet.org/software/mdevd
+MDEVD_LICENSE = ISC
+MDEVD_LICENSE_FILES = COPYING
+MDEVD_INSTALL_STAGING = YES
+MDEVD_DEPENDENCIES = skalibs
+
+MDEVD_CONF_OPTS = \
+	--prefix=/usr \
+	--with-sysdeps=$(STAGING_DIR)/usr/lib/skalibs/sysdeps \
+	--with-include=$(STAGING_DIR)/usr/include \
+	--with-dynlib=$(STAGING_DIR)/usr/lib \
+	--with-lib=$(STAGING_DIR)/usr/lib/skalibs \
+	$(if $(BR2_STATIC_LIBS),,--disable-allstatic) \
+	$(SHARED_STATIC_LIBS_OPTS)
+
+define MDEVD_CONFIGURE_CMDS
+	(cd $(@D); $(TARGET_CONFIGURE_OPTS) ./configure $(MDEVD_CONF_OPTS))
+endef
+
+define MDEVD_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define MDEVD_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
+endef
+
+define MDEVD_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(STAGING_DIR) install
+endef
+
+$(eval $(generic-package))
