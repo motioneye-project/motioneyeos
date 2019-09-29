2c273a4dbb69f3aaf18aeebec6c811bf7b705aa5
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-09-29 19:55:50 +0200

    package/x11r7/xlib_libXt: bump version to 1.2.0
    Removed patch applied upstream:
    https://cgit.freedesktop.org/xorg/lib/libXt/commit/?id=0b80bc9904c8d553c760651daca0c964df052c8b
    Also removed _AUTORECONF.
    
    Added all hashes provided by upstream.
    
    Updated license hash after upstream commit:
    https://cgit.freedesktop.org/xorg/lib/libXt/commit/?id=a8dcb749458a723dd76e342162c5c00b629374fe
    
    Removed configure option install-makestrs after upstream commit:
    https://cgit.freedesktop.org/xorg/lib/libXt/commit/?id=345633dc9c79b32a1b391dfb4dfedf87ae9e291b
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/x11r7/xlib_libXt/xlib_libXt.mk
+++ package/x11r7/xlib_libXt/xlib_libXt.mk
@@ -4,14 +4,13 @@
 #
 ################################################################################
 
-XLIB_LIBXT_VERSION = 1.1.5
+XLIB_LIBXT_VERSION = 1.2.0
 XLIB_LIBXT_SOURCE = libXt-$(XLIB_LIBXT_VERSION).tar.bz2
 XLIB_LIBXT_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXT_LICENSE = MIT
 XLIB_LIBXT_LICENSE_FILES = COPYING
 XLIB_LIBXT_INSTALL_STAGING = YES
 XLIB_LIBXT_DEPENDENCIES = xlib_libSM xlib_libX11 xorgproto xcb-proto libxcb host-xorgproto
-XLIB_LIBXT_CONF_OPTS = --disable-malloc0returnsnull --disable-install-makestrs
-XLIB_LIBXT_AUTORECONF = YES
+XLIB_LIBXT_CONF_OPTS = --disable-malloc0returnsnull
 
 $(eval $(autotools-package))
