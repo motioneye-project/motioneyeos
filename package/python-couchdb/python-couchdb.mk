741ad8124f49edf4f6f60358eb97a0e88c944b53
Asaf Kahlon <asafka7@gmail.com>
2019-09-30 15:32:37 +0300

    package/python-couchdb: bump to versoin 1.2
    Also add hash for license file.
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-couchdb/python-couchdb.mk
+++ package/python-couchdb/python-couchdb.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_COUCHDB_VERSION = 1.1
+PYTHON_COUCHDB_VERSION = 1.2
 PYTHON_COUCHDB_SOURCE = CouchDB-$(PYTHON_COUCHDB_VERSION).tar.gz
-PYTHON_COUCHDB_SITE = https://pypi.python.org/packages/9a/e8/c3c8da6d00145aaca07f2b784794917613dad26532068da4e8392dc48d7f
+PYTHON_COUCHDB_SITE = https://files.pythonhosted.org/packages/7c/c8/f94a107eca0c178e5d74c705dad1a5205c0f580840bd1b155cd8a258cb7c
 PYTHON_COUCHDB_SETUP_TYPE = setuptools
 PYTHON_COUCHDB_LICENSE = BSD-3-Clause
 PYTHON_COUCHDB_LICENSE_FILES = COPYING
