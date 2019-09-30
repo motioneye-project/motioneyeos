d7824cefb9de5fe33d532a9cf01489ae70a96d9d
Asaf Kahlon <asafka7@gmail.com>
2019-09-30 15:32:49 +0300

    package/python-pyelftools: bump to version 0.25
    Also add hash for license file and take into account the switch to
    setuptools in setup.py.
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pyelftools/python-pyelftools.mk
+++ package/python-pyelftools/python-pyelftools.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_PYELFTOOLS_VERSION = 0.24
+PYTHON_PYELFTOOLS_VERSION = 0.25
 PYTHON_PYELFTOOLS_SOURCE = pyelftools-$(PYTHON_PYELFTOOLS_VERSION).tar.gz
-PYTHON_PYELFTOOLS_SITE = https://pypi.python.org/packages/ba/78/d4a186a2e38731286c99dc3e3ca8123b6f55cf2e28608e8daf2d84b65494
+PYTHON_PYELFTOOLS_SITE = https://files.pythonhosted.org/packages/fa/9a/0674cb1725196568bdbca98304f2efb17368b57af1a4bb3fc772c026f474
 PYTHON_PYELFTOOLS_LICENSE = Public domain
 PYTHON_PYELFTOOLS_LICENSE_FILES = LICENSE
-PYTHON_PYELFTOOLS_SETUP_TYPE = distutils
+PYTHON_PYELFTOOLS_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
 $(eval $(host-python-package))
