a291943a69da92bbe99c40690a1b39ac3995cb7f
Asaf Kahlon <asafka7@gmail.com>
2019-09-30 15:33:01 +0300

    package/python-yieldfrom: bump to version 1.0.4
    LICENSE was added to the package, so we can use it instead
    of PKG-INFO.
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-yieldfrom/python-yieldfrom.mk
+++ package/python-yieldfrom/python-yieldfrom.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_YIELDFROM_VERSION = 1.0.3
+PYTHON_YIELDFROM_VERSION = 1.0.4
 PYTHON_YIELDFROM_SOURCE = yieldfrom-$(PYTHON_YIELDFROM_VERSION).tar.gz
-PYTHON_YIELDFROM_SITE = https://pypi.python.org/packages/de/2d/05524f368e691846824d962b64f983e3fde9b8c10839e7efbc1b51d42de3
+PYTHON_YIELDFROM_SITE = https://files.pythonhosted.org/packages/6e/d2/dc48e4cc6bbe48afd022da745f981cf0437ab7e658de9078bf56747b1d73
 PYTHON_YIELDFROM_SETUP_TYPE = setuptools
 PYTHON_YIELDFROM_LICENSE = MIT
-PYTHON_YIELDFROM_LICENSE_FILES = PKG-INFO
+PYTHON_YIELDFROM_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
 $(eval $(host-python-package))
