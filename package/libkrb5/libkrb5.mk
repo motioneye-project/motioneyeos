678a4b1489bc1129f4a6728075d438de86f2cc2d
André Hentschel <nerv@dawncrow.de>
2019-09-30 13:39:31 +0200

    package/libkrb5: bump to 1.17
    The hash of the NOTICE file has changed due to:
    
     - Update in copyright year
    
     - Rewrapping/indentation changes
    
     - Addition of the license for other parts of the software, but that
       are under MIT, so no change from that perspective
    
    Signed-off-by: André Hentschel <nerv@dawncrow.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libkrb5/libkrb5.mk
+++ package/libkrb5/libkrb5.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LIBKRB5_VERSION_MAJOR = 1.16
-LIBKRB5_VERSION = $(LIBKRB5_VERSION_MAJOR).2
+LIBKRB5_VERSION_MAJOR = 1.17
+LIBKRB5_VERSION = $(LIBKRB5_VERSION_MAJOR)
 LIBKRB5_SITE = https://web.mit.edu/kerberos/dist/krb5/$(LIBKRB5_VERSION_MAJOR)
 LIBKRB5_SOURCE = krb5-$(LIBKRB5_VERSION).tar.gz
 LIBKRB5_SUBDIR = src
