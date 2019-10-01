71d2911e26a8e1df3b01ba3838adf0604c893d3e
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-10-01 19:21:08 +0200

    package/putty: security bump version to 0.73
    Added upstream-provided sha1 hash.
    
    Changelog:
    https://www.chiark.greenend.org.uk/~sgtatham/putty/changes.html
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/putty/putty.mk
+++ package/putty/putty.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PUTTY_VERSION = 0.72
+PUTTY_VERSION = 0.73
 PUTTY_SITE = http://the.earth.li/~sgtatham/putty/$(PUTTY_VERSION)
 PUTTY_LICENSE = MIT
 PUTTY_LICENSE_FILES = LICENCE
