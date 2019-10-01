bd574c445c598fc14ae5cc37ec1fd7fcddd08862
Peter Korsgaard <peter@korsgaard.com>
2019-10-01 20:19:26 +0200

    package/go: security bump to version 1.12.10
    Fixes the following security vulnerabilities:
    
    - CVE-2019-16276: Go before 1.12.10 and 1.13.x before 1.13.1 allow HTTP
      Request Smuggling.
      https://github.com/golang/go/issues/34540
    
    >From the release notes:
    
    go1.12.10 (released 2019/09/25) includes security fixes to the net/http and
    net/textproto packages
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.12.9
+GO_VERSION = 1.12.10
 GO_SITE = https://storage.googleapis.com/golang
 GO_SOURCE = go$(GO_VERSION).src.tar.gz
 
