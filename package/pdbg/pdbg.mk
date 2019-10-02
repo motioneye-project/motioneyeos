a00908315ec9112a9c0a374658229ef0bba381b4
Joel Stanley <joel@jms.id.au>
2019-10-02 12:26:52 +0930

    package/pdbg: bump version to v2.3
    Drop the endian.h patch has this has been fixed upstream.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/pdbg/pdbg.mk
+++ package/pdbg/pdbg.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PDBG_VERSION = 2.2
+PDBG_VERSION = 2.3
 PDBG_SITE = $(call github,open-power,pdbg,v$(PDBG_VERSION))
 PDBG_LICENSE = Apache-2.0
 PDBG_LICENSE_FILES = COPYING
