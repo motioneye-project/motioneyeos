48b0fc4ba42216529efa7cc21fdfea68dff56754
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-10-04 15:18:06 +0200

    package/postgresql: bump version to 12.0
    Release notes: https://www.postgresql.org/about/news/1976/
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/postgresql/postgresql.mk
+++ package/postgresql/postgresql.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-POSTGRESQL_VERSION = 11.5
+POSTGRESQL_VERSION = 12.0
 POSTGRESQL_SOURCE = postgresql-$(POSTGRESQL_VERSION).tar.bz2
 POSTGRESQL_SITE = https://ftp.postgresql.org/pub/source/v$(POSTGRESQL_VERSION)
 POSTGRESQL_LICENSE = PostgreSQL
