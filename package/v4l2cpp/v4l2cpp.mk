1901a0f8fa2eefb2d01524abd130b93c0a549254
Joo Aun Saw <jasaw@dius.com.au>
2019-10-04 17:09:02 +1000

    v4l2cppwrapper v4l2tools v4l2rtspserver: fix cross-compilation

--- package/v4l2cpp/v4l2cpp.mk
+++ package/v4l2cpp/v4l2cpp.mk
@@ -4,12 +4,11 @@
 #
 #############################################################
 
-V4L2CPP_VERSION=78a8d17631f4f7e21577979ba9412757ec681850
-V4L2CPP_SITE = https://github.com/mpromonet/libv4l2cpp.git
+V4L2CPP_VERSION=021d887dc531bcf74c766cb413f87e8fc92579e3
+V4L2CPP_SITE = https://github.com/jasaw/libv4l2cpp.git
 V4L2CPP_SITE_METHOD = git
 V4L2CPP_LICENSE = UNLICENSE
 V4L2CPP_LICENSE_FILES = LICENSE
-V4L2CPP_DEPENDENCIES = live555 log4cpp
 V4L2CPP_CFLAGS = $(TARGET_CFLAGS)
 
 ifeq ($(BR2_STATIC_LIBS),y)
@@ -26,7 +25,12 @@ V4L2CPP_CFLAGS += -DLOCALE_NOT_USED
 endif
 
 define V4L2CPP_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" -C $(@D) all
+	$(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" AR="$(TARGET_AR)" EXTRA_CXXFLAGS="$(V4L2CPP_CFLAGS)" -C $(@D) all
+endef
+
+define V4L2CPP_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) PREFIX="$(STAGING_DIR)/usr" -C $(@D) install
+	$(INSTALL) -D -m 0755 $(@D)/libv4l2wrapper.so $(TARGET_DIR)/usr/lib
 endef
 
 $(eval $(generic-package))
