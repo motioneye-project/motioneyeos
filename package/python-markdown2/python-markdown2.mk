7c1f45c194ec922a641a58e5495cdebe6f71cefc
Asaf Kahlon <asafka7@gmail.com>
2019-10-04 21:31:10 +0300

    package/python-markdown2: bump to version 2.3.8
    Update setup type to setuptools.
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/python-markdown2/python-markdown2.mk
+++ package/python-markdown2/python-markdown2.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-PYTHON_MARKDOWN2_VERSION = 2.3.6
-PYTHON_MARKDOWN2_SITE = $(call github,trentm,python-markdown2,$(PYTHON_MARKDOWN2_VERSION))
-PYTHON_MARKDOWN2_SETUP_TYPE = distutils
+PYTHON_MARKDOWN2_VERSION = 2.3.8
+PYTHON_MARKDOWN2_SOURCE = markdown2-$(PYTHON_MARKDOWN2_VERSION).tar.gz
+PYTHON_MARKDOWN2_SITE = https://files.pythonhosted.org/packages/e3/93/d37055743009d1a492b2670cc215831a388b3d6e4a28b7672fdf0f7854f5
+PYTHON_MARKDOWN2_SETUP_TYPE = setuptools
 PYTHON_MARKDOWN2_LICENSE = MIT
 PYTHON_MARKDOWN2_LICENSE_FILES = LICENSE.txt
 
