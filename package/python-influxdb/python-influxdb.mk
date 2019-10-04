ba63ac8456bd601a7ca2e62ecb484060186ccd8b
Asaf Kahlon <asafka7@gmail.com>
2019-10-04 21:31:08 +0300

    package/python-influxdb: bump to version 5.2.3
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/python-influxdb/python-influxdb.mk
+++ package/python-influxdb/python-influxdb.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_INFLUXDB_VERSION = 5.0.0
+PYTHON_INFLUXDB_VERSION = 5.2.3
 PYTHON_INFLUXDB_SOURCE = influxdb-$(PYTHON_INFLUXDB_VERSION).tar.gz
-PYTHON_INFLUXDB_SITE = https://files.pythonhosted.org/packages/95/26/33e7b85b72a0df2dc00af4c1b9f5df3e7d0aea29ae4f8f65a83f7024c4e2
+PYTHON_INFLUXDB_SITE = https://files.pythonhosted.org/packages/d2/0d/351a346886ecbe61211cbfcad8ac73f99f5a9bf526916631c5668dbad601
 PYTHON_INFLUXDB_SETUP_TYPE = setuptools
 PYTHON_INFLUXDB_LICENSE = MIT
 PYTHON_INFLUXDB_LICENSE_FILES = LICENSE
