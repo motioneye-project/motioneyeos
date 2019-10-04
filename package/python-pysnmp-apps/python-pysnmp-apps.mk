28ee86f01dd46b52525ab4e3e20fefd19a58598a
Asaf Kahlon <asafka7@gmail.com>
2019-10-04 21:31:22 +0300

    package/python-pysnmp-apps: bump to version 0.5.3
    Also add hash for license file.
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pysnmp-apps/python-pysnmp-apps.mk
+++ package/python-pysnmp-apps/python-pysnmp-apps.mk
@@ -4,13 +4,12 @@
 #
 ################################################################################
 
-PYTHON_PYSNMP_APPS_VERSION = 0.4.1
+PYTHON_PYSNMP_APPS_VERSION = 0.5.3
 PYTHON_PYSNMP_APPS_SOURCE = pysnmp-apps-$(PYTHON_PYSNMP_APPS_VERSION).tar.gz
-PYTHON_PYSNMP_APPS_SITE = https://pypi.python.org/packages/source/p/pysnmp-apps
+PYTHON_PYSNMP_APPS_SITE = https://files.pythonhosted.org/packages/c2/1a/26f5f3732df2c7d34663a7f2fa6534b5b4ffae73a4a440b75864ec826ba6
 PYTHON_PYSNMP_APPS_SETUP_TYPE = setuptools
 PYTHON_PYSNMP_APPS_LICENSE = BSD-3-Clause
 PYTHON_PYSNMP_APPS_LICENSE_FILES = LICENSE.txt
-
 PYTHON_PYSNMP_APPS_DEPENDENCIES = python-pysnmp
 
 $(eval $(python-package))
