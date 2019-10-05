7a2224273d883eca2b55e73851e0a14ba2b475e4
Asaf Kahlon <asafka7@gmail.com>
2019-10-05 18:31:47 +0300

    package/python-websocket-client: bump to version 0.56.0
    The license file has changed to BSD-3-Clause, but the source files
    still specify the LGPL-2.1+ license, so we'll have to specify both
    licenses for now.
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-websocket-client/python-websocket-client.mk
+++ package/python-websocket-client/python-websocket-client.mk
@@ -4,11 +4,14 @@
 #
 ################################################################################
 
-PYTHON_WEBSOCKET_CLIENT_VERSION = 0.47.0
+PYTHON_WEBSOCKET_CLIENT_VERSION = 0.56.0
 PYTHON_WEBSOCKET_CLIENT_SOURCE = websocket_client-$(PYTHON_WEBSOCKET_CLIENT_VERSION).tar.gz
-PYTHON_WEBSOCKET_CLIENT_SITE = https://pypi.python.org/packages/c9/bb/8d3dd9063cfe0cd5d03fe6a1f74ddd948f384e9c1eff0eb978f3976a7d27
+PYTHON_WEBSOCKET_CLIENT_SITE = https://files.pythonhosted.org/packages/c5/01/8c9c7de6c46f88e70b5a3276c791a2be82ae83d8e0d0cc030525ee2866fd
 PYTHON_WEBSOCKET_CLIENT_SETUP_TYPE = setuptools
-PYTHON_WEBSOCKET_CLIENT_LICENSE = LGPL-2.1+
+# Project was under LGPL-2.1+, but was relicensed under
+# BSD-3-Clause. The LICENSE file contains the BSD-3-Clause text, but
+# the source files still contain a LGPL-2.1+ header.
+PYTHON_WEBSOCKET_CLIENT_LICENSE = LGPL-2.1+, BSD-3-Clause
 PYTHON_WEBSOCKET_CLIENT_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
