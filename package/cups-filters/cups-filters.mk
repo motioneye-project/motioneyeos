0ee699823b37f7be85121ccf207a6fb0dbd2dac7
Carlos Santos <unixmania@gmail.com>
2019-10-04 23:42:29 -0300

    package/cups-filters: drop pre-configure hook
    Version 1.25.6 contains the config.rpath and ABOUT-NLS, so it wouldn't
    be necessary to fake them even if we still used autoreconf.
    
    Signed-off-by: Carlos Santos <unixmania@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/cups-filters/cups-filters.mk
+++ package/cups-filters/cups-filters.mk
@@ -20,14 +20,6 @@ CUPS_FILTERS_CONF_OPTS = --disable-imagefilters \
 	--with-pdftops=pdftops \
 	--with-jpeg
 
-# After 0002-filter-texttotext.c-link-with-libiconv-if-needed.patch autoreconf
-# needs config.rpath and ABOUT-NLS, which are not in v1.25.4 yet. Fake them.
-define CUPS_FILTERS_ADD_MISSING_FILE
-	touch $(@D)/config.rpath $(@D)/ABOUT-NLS
-endef
-
-CUPS_FILTERS_PRE_CONFIGURE_HOOKS = CUPS_FILTERS_ADD_MISSING_FILE
-
 ifeq ($(BR2_PACKAGE_LIBPNG),y)
 CUPS_FILTERS_CONF_OPTS += --with-png
 CUPS_FILTERS_DEPENDENCIES += libpng
