8953cb5a4c92cfe0166abe1a3d4eb4232316e232
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-10-06 16:19:04 +0200

    package/libopenh264: bump version to 2.0.0
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libopenh264/libopenh264.mk
+++ package/libopenh264/libopenh264.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBOPENH264_VERSION = 1.8.0
+LIBOPENH264_VERSION = 2.0.0
 LIBOPENH264_SITE = $(call github,cisco,openh264,v$(LIBOPENH264_VERSION))
 LIBOPENH264_LICENSE = BSD-2-Clause
 LIBOPENH264_LICENSE_FILES = LICENSE
