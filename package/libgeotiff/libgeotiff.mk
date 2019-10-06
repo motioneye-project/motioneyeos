1edf0383a6a9d17a27d831bb02833870655d43d9
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-10-06 15:47:35 +0200

    package/libgeotiff: bump version to 1.5.1
    Removed patches applied upstream:
    https://github.com/OSGeo/libgeotiff/commit/2fbae8298d7cc976d7c0f329fc26dafe57fb0fc4
    https://github.com/OSGeo/libgeotiff/commit/b5895ac935914f5b5c2af66f78b11d059867d71a
    https://github.com/OSGeo/libgeotiff/commit/edc9ec69b43c27955ee4f24db2e6808bb1a8974d
    
    Updated upstream URL in Config.in due to the upstream switch to github:
    https://trac.osgeo.org/geotiff/
    
    Updated license hash after upstream commit:
    https://github.com/OSGeo/libgeotiff/commit/e3dd2e1bf1ce58aa98d25cc778cb491794a13197
    
    proj 6 is now a mandatory dependency:
    https://github.com/OSGeo/libgeotiff/commit/a0526817886614c41ba5fdee4a5998bafcdff658
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libgeotiff/libgeotiff.mk
+++ package/libgeotiff/libgeotiff.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-LIBGEOTIFF_VERSION = 1.4.3
+LIBGEOTIFF_VERSION = 1.5.1
 LIBGEOTIFF_SITE = http://download.osgeo.org/geotiff/libgeotiff
 LIBGEOTIFF_LICENSE = X11-style, public domain
 LIBGEOTIFF_LICENSE_FILES = LICENSE
-LIBGEOTIFF_DEPENDENCIES = tiff host-pkgconf
+LIBGEOTIFF_DEPENDENCIES = proj tiff host-pkgconf
 LIBGEOTIFF_INSTALL_STAGING = YES
-LIBGEOTIFF_AUTORECONF = YES
+LIBGEOTIFF_CONF_OPTS = --with-proj=$(STAGING_DIR)/usr
 
 ifeq ($(BR2_PACKAGE_ZLIB),y)
 LIBGEOTIFF_DEPENDENCIES += zlib
@@ -24,13 +24,6 @@ LIBGEOTIFF_DEPENDENCIES += jpeg
 LIBGEOTIFF_CONF_OPTS += --with-jpeg
 else
 LIBGEOTIFF_CONF_OPTS += --without-jpeg
-endif
-
-ifeq ($(BR2_PACKAGE_PROJ),y)
-LIBGEOTIFF_DEPENDENCIES += proj
-LIBGEOTIFF_CONF_OPTS += --with-proj=$(STAGING_DIR)/usr
-else
-LIBGEOTIFF_CONF_OPTS += --without-proj
 endif
 
 $(eval $(autotools-package))
