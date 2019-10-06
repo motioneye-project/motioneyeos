c9779f53d0fc0e39f08dc3204b9224f059e6d029
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-10-06 16:36:01 +0200

    package/libtasn1: bump version to 4.14
    Release notes:
    https://lists.gnu.org/archive/html/help-libtasn1/2019-07/msg00000.html
    
    Upstream moved license files COPYING/COPYING.LIB to doc/ and added
    LICENSE:
    https://gitlab.com/gnutls/libtasn1/commit/142a25a1dfe3034fc468f093ea887f12af0da5f4
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libtasn1/libtasn1.mk
+++ package/libtasn1/libtasn1.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-LIBTASN1_VERSION = 4.13
+LIBTASN1_VERSION = 4.14
 LIBTASN1_SITE = $(BR2_GNU_MIRROR)/libtasn1
 LIBTASN1_DEPENDENCIES = host-bison
 LIBTASN1_LICENSE = GPL-3.0+ (tests, tools), LGPL-2.1+ (library)
-LIBTASN1_LICENSE_FILES = COPYING COPYING.LIB
+LIBTASN1_LICENSE_FILES = LICENSE doc/COPYING doc/COPYING.LESSER
 LIBTASN1_INSTALL_STAGING = YES
 # 'missing' fallback logic botched so disable it completely
 LIBTASN1_CONF_ENV = MAKEINFO="true"
