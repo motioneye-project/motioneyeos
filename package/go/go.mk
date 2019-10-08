f9d2bd24cab1e2627b2cf00fa90f82b3b749d453
Christian Stewart <christian@paral.in>
2019-10-08 11:33:51 -0700

    package/go: bump to 1.13.1
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.12.10
+GO_VERSION = 1.13.1
 GO_SITE = https://storage.googleapis.com/golang
 GO_SOURCE = go$(GO_VERSION).src.tar.gz
 
