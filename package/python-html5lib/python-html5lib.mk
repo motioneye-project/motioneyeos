800d3ee0de84fa82f832b18335e31d9df975a249
Asaf Kahlon <asafka7@gmail.com>
2019-10-09 22:31:37 +0300

    package/python-html5lib: bump to version 1.0.1
    * Update runtime requirements.
    * Add hash for license file.
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-html5lib/python-html5lib.mk
+++ package/python-html5lib/python-html5lib.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_HTML5LIB_VERSION = 1.0b10
+PYTHON_HTML5LIB_VERSION = 1.0.1
 PYTHON_HTML5LIB_SOURCE = html5lib-$(PYTHON_HTML5LIB_VERSION).tar.gz
-PYTHON_HTML5LIB_SITE = https://pypi.python.org/packages/97/16/982214624095c1420c75f3bd295d9e658794aafb95fc075823de107e0ae4
+PYTHON_HTML5LIB_SITE = https://files.pythonhosted.org/packages/85/3e/cf449cf1b5004e87510b9368e7a5f1acd8831c2d6691edd3c62a0823f98f
 PYTHON_HTML5LIB_LICENSE = MIT
 PYTHON_HTML5LIB_LICENSE_FILES = LICENSE
 PYTHON_HTML5LIB_SETUP_TYPE = setuptools
