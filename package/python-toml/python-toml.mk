4aaaa1fc5ce86a080b53a4d897cb78ed4db2f095
Asaf Kahlon <asafka7@gmail.com>
2019-10-10 22:51:25 +0300

    package/python-toml: bump to version 0.10.0
    Also add hash for license file.
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-toml/python-toml.mk
+++ package/python-toml/python-toml.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TOML_VERSION = 0.9.3
-PYTHON_TOML_SITE = $(call github,uiri,toml,$(PYTHON_TOML_VERSION))
+PYTHON_TOML_VERSION = 0.10.0
+PYTHON_TOML_SOURCE = toml-$(PYTHON_TOML_VERSION).tar.gz
+PYTHON_TOML_SITE = https://files.pythonhosted.org/packages/b9/19/5cbd78eac8b1783671c40e34bb0fa83133a06d340a38b55c645076d40094
 PYTHON_TOML_SETUP_TYPE = setuptools
 PYTHON_TOML_LICENSE = MIT
 PYTHON_TOML_LICENSE_FILES = LICENSE
