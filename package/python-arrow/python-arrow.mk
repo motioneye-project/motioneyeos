bc356882395306a6769fb8eb2712c8656cdfecef
Asaf Kahlon <asafka7@gmail.com>
2019-10-11 11:35:27 +0300

    package/python-arrow: bump to version 0.15.2
    * Update dependency list.
    * Add hash for license file.
    * Remove docs/_themes/COPYING.txt from _LICENSE_FILES (the whole
      _themes directory was removed)
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-arrow/python-arrow.mk
+++ package/python-arrow/python-arrow.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_ARROW_VERSION = 0.10.0
+PYTHON_ARROW_VERSION = 0.15.2
 PYTHON_ARROW_SOURCE = arrow-$(PYTHON_ARROW_VERSION).tar.gz
-PYTHON_ARROW_SITE = https://pypi.python.org/packages/54/db/76459c4dd3561bbe682619a5c576ff30c42e37c2e01900ed30a501957150
+PYTHON_ARROW_SITE = https://files.pythonhosted.org/packages/43/0e/47416c54ad7742981bf77fdfc405987551ab14b181a6140c8cd2a5823872
 PYTHON_ARROW_SETUP_TYPE = setuptools
 PYTHON_ARROW_LICENSE = Apache-2.0
-PYTHON_ARROW_LICENSE_FILES = LICENSE docs/_themes/COPYING.txt
+PYTHON_ARROW_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
