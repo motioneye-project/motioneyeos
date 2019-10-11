2cf04c1ae6a3d5650e3d7e1666fe0ec318482e4a
Asaf Kahlon <asafka7@gmail.com>
2019-10-11 11:35:25 +0300

    package/python-subprocess32: bump to version 3.5.4
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-subprocess32/python-subprocess32.mk
+++ package/python-subprocess32/python-subprocess32.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SUBPROCESS32_VERSION = 3.5.3
+PYTHON_SUBPROCESS32_VERSION = 3.5.4
 PYTHON_SUBPROCESS32_SOURCE = subprocess32-$(PYTHON_SUBPROCESS32_VERSION).tar.gz
-PYTHON_SUBPROCESS32_SITE = https://files.pythonhosted.org/packages/be/2b/beeba583e9877e64db10b52a96915afc0feabf7144dcbf2a0d0ea68bf73d
+PYTHON_SUBPROCESS32_SITE = https://files.pythonhosted.org/packages/32/c8/564be4d12629b912ea431f1a50eb8b3b9d00f1a0b1ceff17f266be190007
 PYTHON_SUBPROCESS32_SETUP_TYPE = setuptools
 PYTHON_SUBPROCESS32_LICENSE = Python-2.0
 PYTHON_SUBPROCESS32_LICENSE_FILES = LICENSE
