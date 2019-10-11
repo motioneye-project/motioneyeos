d3be2087d0369066f443d0f002fe94276200b0c1
Ricardo Martincoski <ricardo.martincoski@gmail.com>
2019-10-11 00:08:39 -0300

    support/testing: add tmux test
    Signed-off-by: Ricardo Martincoski <ricardo.martincoski@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ support/testing/tests/package/test_tmux.py
@@ -0,0 +1,42 @@
+import os
+
+import infra.basetest
+
+
+class TestTmux(infra.basetest.BRTest):
+    config = infra.basetest.BASIC_TOOLCHAIN_CONFIG + \
+        """
+        BR2_GENERATE_LOCALE="en_US.UTF-8"
+        BR2_PACKAGE_TMUX=y
+        BR2_TARGET_ROOTFS_CPIO=y
+        # BR2_TARGET_ROOTFS_TAR is not set
+        """
+
+    def test_run(self):
+        cpio_file = os.path.join(self.builddir, "images", "rootfs.cpio")
+        self.emulator.boot(arch="armv5",
+                           kernel="builtin",
+                           options=["-initrd", cpio_file])
+        self.emulator.login()
+
+        cmd = "tmux -V"
+        _, exit_code = self.emulator.run(cmd)
+        self.assertEqual(exit_code, 0)
+
+        cmd = "tmux -C </dev/null"
+        _, exit_code = self.emulator.run(cmd)
+        self.assertEqual(exit_code, 0)
+
+        cmd = "tmux split"
+        _, exit_code = self.emulator.run(cmd)
+        self.assertEqual(exit_code, 0)
+
+        cmd = "tmux new-window"
+        _, exit_code = self.emulator.run(cmd)
+        self.assertEqual(exit_code, 0)
+
+        cmd = "tmux list-windows"
+        output, exit_code = self.emulator.run(cmd)
+        self.assertEqual(exit_code, 0)
+        self.assertIn("(2 panes)", output[0])
+        self.assertIn("(1 panes)", output[1])
