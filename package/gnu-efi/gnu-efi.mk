fc8c8f300ebc67c35f0e039178db722d541c78eb
James Hilliard <james.hilliard1@gmail.com>
2019-10-14 10:52:27 -0600

    package/gnu-efi: bump to version 3.0.10
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/gnu-efi/gnu-efi.mk
+++ package/gnu-efi/gnu-efi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GNU_EFI_VERSION = 3.0.9
+GNU_EFI_VERSION = 3.0.10
 GNU_EFI_SOURCE = gnu-efi-$(GNU_EFI_VERSION).tar.bz2
 GNU_EFI_SITE = http://downloads.sourceforge.net/project/gnu-efi
 GNU_EFI_INSTALL_STAGING = YES
