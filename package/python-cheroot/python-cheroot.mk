5eaf2346838bf817054a6e985fbe310538fcd3df
Asaf Kahlon <asafka7@gmail.com>
2019-10-14 22:12:28 +0300

    package/python-cheroot: bump to version 8.1.0
    * Add host-python-setuptools-scm-git-archive as build-time dependecy.
    * Update runtime dependecies list.
    * Update license file name (and hash): LICENSE was renamed to LICENSE.md
      with no changes. After that, the copyright year was bumped.
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-cheroot/python-cheroot.mk
+++ package/python-cheroot/python-cheroot.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_CHEROOT_VERSION = 5.10.0
+PYTHON_CHEROOT_VERSION = 8.1.0
 PYTHON_CHEROOT_SOURCE = cheroot-$(PYTHON_CHEROOT_VERSION).tar.gz
-PYTHON_CHEROOT_SITE = https://pypi.python.org/packages/14/dc/afc41e7f7e797973808d8520e6aef21c4efd00550aa32f5b726327d36be0
+PYTHON_CHEROOT_SITE = https://files.pythonhosted.org/packages/a1/6c/899c7de9323e440f7eda28fead4a84bd0ae5a557e6ebf8f12270cc502043
 PYTHON_CHEROOT_LICENSE = BSD-3-Clause
-PYTHON_CHEROOT_LICENSE_FILES = LICENSE
+PYTHON_CHEROOT_LICENSE_FILES = LICENSE.md
 PYTHON_CHEROOT_SETUP_TYPE = setuptools
-PYTHON_CHEROOT_DEPENDENCIES = host-python-setuptools-scm
+PYTHON_CHEROOT_DEPENDENCIES = host-python-setuptools-scm host-python-setuptools-scm-git-archive
 
 $(eval $(python-package))
