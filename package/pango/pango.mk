5cce413eb11ac75f5c48ed351896c5d1c33961d5
James Hilliard <james.hilliard1@gmail.com>
2019-10-14 11:47:59 -0600

    package/pango: bump to version 1.44.6
    Switch build system from autotools to meson.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/pango/pango.mk
+++ package/pango/pango.mk
@@ -4,17 +4,16 @@
 #
 ################################################################################
 
-PANGO_VERSION_MAJOR = 1.40
-PANGO_VERSION = $(PANGO_VERSION_MAJOR).14
+PANGO_VERSION_MAJOR = 1.44
+PANGO_VERSION = $(PANGO_VERSION_MAJOR).6
 PANGO_SOURCE = pango-$(PANGO_VERSION).tar.xz
 PANGO_SITE = http://ftp.gnome.org/pub/GNOME/sources/pango/$(PANGO_VERSION_MAJOR)
-PANGO_AUTORECONF = YES
 PANGO_INSTALL_STAGING = YES
 PANGO_LICENSE = LGPL-2.0+
 PANGO_LICENSE_FILES = COPYING
 
-PANGO_CONF_OPTS = --enable-explicit-deps=no
-HOST_PANGO_CONF_OPTS = --enable-explicit-deps=no
+PANGO_CONF_OPTS = -Duse_fontconfig=true -Dintrospection=false
+HOST_PANGO_CONF_OPTS = -Duse_fontconfig=true -Dintrospection=false
 
 PANGO_DEPENDENCIES = \
 	$(TARGET_NLS_DEPENDENCIES) \
@@ -33,16 +32,12 @@ HOST_PANGO_DEPENDENCIES = \
 	host-freetype
 
 ifeq ($(BR2_PACKAGE_XORG7),y)
-PANGO_CONF_OPTS += \
-	--x-includes=$(STAGING_DIR)/usr/include/X11 \
-	--x-libraries=$(STAGING_DIR)/usr/lib
 PANGO_DEPENDENCIES += xlib_libX11
 endif
 
 ifeq ($(BR2_PACKAGE_XLIB_LIBXFT)$(BR2_PACKAGE_XLIB_LIBXRENDER),yy)
 PANGO_DEPENDENCIES += xlib_libXft xlib_libXrender
-PANGO_CONF_OPTS += --with-xft
 endif
 
-$(eval $(autotools-package))
-$(eval $(host-autotools-package))
+$(eval $(meson-package))
+$(eval $(host-meson-package))
