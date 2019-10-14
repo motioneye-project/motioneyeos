aeefad4516165abd19f6478176a5f43bc8a067fd
Pierre-Jean Texier <pjtexier@koncepto.io>
2019-10-14 22:11:28 +0200

    package/libubootenv: bump to version 3286c57
    Also
     - add hash for license file
     - remove patches (already in version):
    	- 0001-fw_printenv-remove-declaration-in-for-loop.patch
    	- 0003-uboot_env-fix-build-with-musl-libc.patch
    	- 0004-uboot_env-fix-compilation-for-glibc-version-2.28.patch
    	- 0005-uboot_env-remove-pthread.h-header-file.patch
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libubootenv/libubootenv.mk
+++ package/libubootenv/libubootenv.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-LIBUBOOTENV_VERSION = 8a7d4030bcb106de11632e85b6a0e7b7d4cb47af
+LIBUBOOTENV_VERSION = 3286c571c7e15be4b341e46986f1c0dc74730207
 LIBUBOOTENV_SITE = $(call github,sbabic,libubootenv,$(LIBUBOOTENV_VERSION))
 LIBUBOOTENV_LICENSE = LGPL-2.1
+LIBUBOOTENV_LICENSE_FILES = Licenses/lgpl-2.1.txt
 LIBUBOOTENV_INSTALL_STAGING = YES
 LIBUBOOTENV_DEPENDENCIES = zlib
 
