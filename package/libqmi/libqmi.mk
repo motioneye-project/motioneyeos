c289119aa568e563f322912842e2f8e74b5378c0
Aleksander Morgado <aleksander@aleksander.es>
2019-10-14 10:40:52 +0200

    package/libqmi: bump to version 1.24.0
    Also drop the musl compat patch as it was already included upstream.
    
    Signed-off-by: Aleksander Morgado <aleksander@aleksander.es>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libqmi/libqmi.mk
+++ package/libqmi/libqmi.mk
@@ -4,14 +4,12 @@
 #
 ################################################################################
 
-LIBQMI_VERSION = 1.22.6
+LIBQMI_VERSION = 1.24.0
 LIBQMI_SITE = http://www.freedesktop.org/software/libqmi
 LIBQMI_SOURCE = libqmi-$(LIBQMI_VERSION).tar.xz
 LIBQMI_LICENSE = LGPL-2.0+ (library), GPL-2.0+ (programs)
 LIBQMI_LICENSE_FILES = COPYING COPYING.LIB
 LIBQMI_INSTALL_STAGING = YES
-# 0001-musl-compat-canonicalize_file_name.patch
-LIBQMI_AUTORECONF = YES
 
 LIBQMI_DEPENDENCIES = libglib2
 
