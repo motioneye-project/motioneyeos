cda7313a0d4bcebd96a81889f90589807bb35c01
James Hilliard <james.hilliard1@gmail.com>
2019-10-14 11:15:12 -0600

    package/nginx: bump to version 1.17.4
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/nginx/nginx.mk
+++ package/nginx/nginx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NGINX_VERSION = 1.16.1
+NGINX_VERSION = 1.17.4
 NGINX_SITE = http://nginx.org/download
 NGINX_LICENSE = BSD-2-Clause
 NGINX_LICENSE_FILES = LICENSE
