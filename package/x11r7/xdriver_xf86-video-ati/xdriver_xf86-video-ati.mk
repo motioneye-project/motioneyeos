38189de1c19e9240cc614c056946686b5bab4906
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-10-17 08:05:41 +0200

    package/x11r7/xdriver_xf86-video-ati: bump version to 19.1.0
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/x11r7/xdriver_xf86-video-ati/xdriver_xf86-video-ati.mk
+++ package/x11r7/xdriver_xf86-video-ati/xdriver_xf86-video-ati.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_ATI_VERSION = 19.0.1
+XDRIVER_XF86_VIDEO_ATI_VERSION = 19.1.0
 XDRIVER_XF86_VIDEO_ATI_SOURCE = xf86-video-ati-$(XDRIVER_XF86_VIDEO_ATI_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_ATI_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_ATI_LICENSE = MIT
