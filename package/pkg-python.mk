fd989575e186d0367b3ee4f5c1a78eed0d7a8f50
James Hilliard <james.hilliard1@gmail.com>
2019-10-17 10:21:27 -0600

    package/pkg-python: fix python 3.8.0 package builds
    We need to update the sysconfigdata path for python 3.8.0 per upstream
    commit 7efc526e5cfb929a79c192ac2dcf7eb78d3a4401 which indicates the "m"
    flag has been removed from sys.abiflags.
    
    Fixes this build error when building target packages:
    ValueError: Empty module name
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/pkg-python.mk
+++ package/pkg-python.mk
@@ -21,7 +21,7 @@
 ################################################################################
 
 define PKG_PYTHON_SYSCONFIGDATA_NAME
-$(basename $(notdir $(wildcard $(STAGING_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/_sysconfigdata_m_linux_*.py)))
+$(basename $(notdir $(wildcard $(STAGING_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/_sysconfigdata__linux_*.py)))
 endef
 
 # Target distutils-based packages
