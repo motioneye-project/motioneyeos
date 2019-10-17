d5787d1ab149a3092a6cb580eeabc96d12f6a8bb
Carlos Santos <unixmania@gmail.com>
2019-10-17 18:03:28 -0300

    package/libseccomp: bump to version 2.4.1
    Fixes a BPF generation bug where the optimizer mistakenly identified
    duplicate BPF code blocks.
    
    Signed-off-by: Carlos Santos <unixmania@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libseccomp/libseccomp.mk
+++ package/libseccomp/libseccomp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSECCOMP_VERSION = 2.4.0
+LIBSECCOMP_VERSION = 2.4.1
 LIBSECCOMP_SITE = $(call github,seccomp,libseccomp,v$(LIBSECCOMP_VERSION))
 LIBSECCOMP_LICENSE = LGPL-2.1
 LIBSECCOMP_LICENSE_FILES = LICENSE
