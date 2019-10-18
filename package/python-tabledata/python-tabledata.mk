c75805e3c45c843441b259cf019299b29d2875a7
Asaf Kahlon <asafka7@gmail.com>
2019-10-18 21:26:17 +0300

    package/python-tabledata: bump to version 0.9.1
    * Update dependency list.
    * Remove post-extract hook, since setup.cfg doesn't create wheel anymore.
    * Update license hash: same content, line-endings are now UNIX.
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-tabledata/python-tabledata.mk
+++ package/python-tabledata/python-tabledata.mk
@@ -4,18 +4,11 @@
 #
 ################################################################################
 
-PYTHON_TABLEDATA_VERSION = 0.0.5
+PYTHON_TABLEDATA_VERSION = 0.9.1
 PYTHON_TABLEDATA_SOURCE = tabledata-$(PYTHON_TABLEDATA_VERSION).tar.gz
-PYTHON_TABLEDATA_SITE = https://pypi.python.org/packages/9d/03/2e96d18f2bd4b76611fc6aa3881c7e15c857eea99debea9b80ec689354ba
+PYTHON_TABLEDATA_SITE = https://files.pythonhosted.org/packages/7d/20/7178ce0e3e34d5aff07e2280522b7e1bc23d4f2fbb916b765bbb41b22174
 PYTHON_TABLEDATA_SETUP_TYPE = setuptools
 PYTHON_TABLEDATA_LICENSE = MIT
 PYTHON_TABLEDATA_LICENSE_FILES = LICENSE
-
-# remove setup.cfg as it tries to create a wheel file and hence
-# breaks the build process
-define PYTHON_TABLEDATA_REMOVE_SETUP_CFG
-	rm $(@D)/setup.cfg
-endef
-PYTHON_TABLEDATA_POST_EXTRACT_HOOKS = PYTHON_TABLEDATA_REMOVE_SETUP_CFG
 
 $(eval $(python-package))
