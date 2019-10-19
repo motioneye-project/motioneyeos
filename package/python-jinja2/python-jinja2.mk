e46b564d4600090e2d840f7a981995af3267a305
Asaf Kahlon <asafka7@gmail.com>
2019-10-19 21:21:24 +0300

    package/python-jinja2: bump to version 2.10.3
    * Fix indentation in hash file.
    * LICENSE was renamed to LICENSE.rst.
      In addition, some phrasing and formatting were slightly changed
      (commit 65da441ee72ffd270e7ce4e4ab60a8826567078b on upstream).
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/python-jinja2/python-jinja2.mk
+++ package/python-jinja2/python-jinja2.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_JINJA2_VERSION = 2.10.1
+PYTHON_JINJA2_VERSION = 2.10.3
 PYTHON_JINJA2_SOURCE = Jinja2-$(PYTHON_JINJA2_VERSION).tar.gz
-PYTHON_JINJA2_SITE = https://files.pythonhosted.org/packages/93/ea/d884a06f8c7f9b7afbc8138b762e80479fb17aedbbe2b06515a12de9378d
+PYTHON_JINJA2_SITE = https://files.pythonhosted.org/packages/7b/db/1d037ccd626d05a7a47a1b81ea73775614af83c2b3e53d86a0bb41d8d799
 PYTHON_JINJA2_SETUP_TYPE = setuptools
 PYTHON_JINJA2_LICENSE = BSD-3-Clause
-PYTHON_JINJA2_LICENSE_FILES = LICENSE
+PYTHON_JINJA2_LICENSE_FILES = LICENSE.rst
 
 # In host build, setup.py tries to download markupsafe if it is not installed
 HOST_PYTHON_JINJA2_DEPENDENCIES = host-python-markupsafe
