8e13f45586530fb2c039449047c957ce9e93456e
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-10-20 12:16:00 +0200

    package/expat: bump version to 2.2.9
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/expat/expat.mk
+++ package/expat/expat.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EXPAT_VERSION = 2.2.8
+EXPAT_VERSION = 2.2.9
 EXPAT_SITE = http://downloads.sourceforge.net/project/expat/expat/$(EXPAT_VERSION)
 EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.xz
 EXPAT_INSTALL_STAGING = YES
