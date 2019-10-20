393f4c379e51b245c6b92ee1e5332902035b5703
Asaf Kahlon <asafka7@gmail.com>
2019-10-20 22:01:41 +0300

    package/python-flask: bump version to 1.1.1
    LICENSE was renamed to LICENSE.rst with little rephrasing.
    In addition, statements about the documentation licensing were
    moved from LICENSE to docs/license.rst so this file was added
    to _LICENSE_FILES (plus hash).
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-flask/python-flask.mk
+++ package/python-flask/python-flask.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_FLASK_VERSION = 1.0.2
+PYTHON_FLASK_VERSION = 1.1.1
 PYTHON_FLASK_SOURCE = Flask-$(PYTHON_FLASK_VERSION).tar.gz
-PYTHON_FLASK_SITE = https://files.pythonhosted.org/packages/4b/12/c1fbf4971fda0e4de05565694c9f0c92646223cff53f15b6eb248a310a62
+PYTHON_FLASK_SITE = https://files.pythonhosted.org/packages/2e/80/3726a729de758513fd3dbc64e93098eb009c49305a97c6751de55b20b694
 PYTHON_FLASK_SETUP_TYPE = setuptools
 PYTHON_FLASK_LICENSE = BSD-3-Clause
-PYTHON_FLASK_LICENSE_FILES = LICENSE
+PYTHON_FLASK_LICENSE_FILES = LICENSE.rst docs/license.rst
 
 $(eval $(python-package))
