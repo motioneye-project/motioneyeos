0cc009f29ec0bbe12bf4a63a6db2696db0d78012
Carlos Santos <unixmania@gmail.com>
2019-10-21 14:03:59 -0300

    package/sysvinit: bump to version 2.96
    Signed-off-by: Carlos Santos <unixmania@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/sysvinit/sysvinit.mk
+++ package/sysvinit/sysvinit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SYSVINIT_VERSION = 2.90
+SYSVINIT_VERSION = 2.96
 SYSVINIT_SOURCE = sysvinit-$(SYSVINIT_VERSION).tar.xz
 SYSVINIT_SITE = http://download.savannah.nongnu.org/releases/sysvinit
 SYSVINIT_LICENSE = GPL-2.0+
