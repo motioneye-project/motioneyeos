d0f3c9de49bfe3f8ac6e75a706540eefcc1afc07
Peter Seiderer <ps.report@gmx.net>
2019-10-21 20:45:10 +0200

    package/libv4l: bump version to 1.18.0
    - remove 0001-fixup-lfs-mismatch-in-preload-libraries.patch
      (Upstream applied [1])
    - remove 0002-keytable-fix-EVIOCSCLOCKID-related-compile-failure.patch
      (Upstream applied [2])
    - remove 0003-Build-sdlcam-only-if-jpeg-is-enabled.patch
      (Upstream applied [3])
    - remove 0004-v4l2-compliance-needs-fork.patch
      (Upstream applied [4])
    
    Despite all patches (specially the ones touching
    configure.ac/Makefile.am) keep LIBV4L_AUTORECONF = YES to get correct
    utils/qv4l linker flags (seems the original debian based libtool sets
    a wrong link_all_deplibs parameter), see [5] for details.
    
    [1] https://git.linuxtv.org/v4l-utils.git/commit/?id=9f0354c3320f3cc62983f726bfed66e1d0c21f83
    [2] https://git.linuxtv.org/v4l-utils.git/commit/?id=a3367e92f4b06667a36fb9485f22f7df52891a2f
    [3] https://git.linuxtv.org/v4l-utils.git/commit/?id=692c2f0f35dd663e84d363867f2841d387726154
    [4] https://git.linuxtv.org/v4l-utils.git/commit/?id=577ab225929c4e8dce7d2b911eeed38469a559bb
    [5] http://lists.busybox.net/pipermail/buildroot/2019-October/261558.html
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libv4l/libv4l.mk
+++ package/libv4l/libv4l.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-LIBV4L_VERSION = 1.16.6
+LIBV4L_VERSION = 1.18.0
 LIBV4L_SOURCE = v4l-utils-$(LIBV4L_VERSION).tar.bz2
 LIBV4L_SITE = https://linuxtv.org/downloads/v4l-utils
 LIBV4L_INSTALL_STAGING = YES
 LIBV4L_DEPENDENCIES = host-pkgconf
 LIBV4L_CONF_OPTS = --disable-doxygen-doc --disable-qvidcap
-# We're patching contrib/test/Makefile.am
+# needed to get utils/qv4l link flags right
 LIBV4L_AUTORECONF = YES
 # add host-gettext for AM_ICONV macro
 LIBV4L_DEPENDENCIES += host-gettext
