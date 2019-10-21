abbe6c68e0af87b0d24e9a744b7f9dee386377e5
Angelo Compagnucci <angelo@amarulasolutions.com>
2019-10-21 10:48:14 +0200

    package/python-spidev: bump to version 3.4
    Package has been relicensed under the MIT license, and LICENSE.md has been
    removed.  The git repo has a LICENSE file, but it isn't available in the
    tarball, so use the readme file instead.
    
    https://github.com/doceme/py-spidev/commit/611b74341f44d444670fd373b1fb755ea2589718
    
    Signed-off-by: Angelo Compagnucci <angelo@amarulasolutions.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-spidev/python-spidev.mk
+++ package/python-spidev/python-spidev.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_SPIDEV_VERSION = 3.2
+PYTHON_SPIDEV_VERSION = 3.4
 PYTHON_SPIDEV_SOURCE = spidev-$(PYTHON_SPIDEV_VERSION).tar.gz
-PYTHON_SPIDEV_SITE = https://pypi.python.org/packages/36/83/73748b6e1819b57d8e1df8090200195cdae33aaa22a49a91ded16785eedd
+PYTHON_SPIDEV_SITE = https://files.pythonhosted.org/packages/fb/14/4c2e1640f0cb04862c76d9d76ed7c945b0f67876e503ac02f7f675fe86a0
 PYTHON_SPIDEV_SETUP_TYPE = distutils
-PYTHON_SPIDEV_LICENSE = GPL-2.0
-PYTHON_SPIDEV_LICENSE_FILES = LICENSE.md
+PYTHON_SPIDEV_LICENSE = MIT
+PYTHON_SPIDEV_LICENSE_FILES = README.md
 
 $(eval $(python-package))
