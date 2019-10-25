ecfff0b9a205e0c4f60f992e7f586cba476826d9
Asaf Kahlon <asafka7@gmail.com>
2019-10-25 21:54:45 +0300

    package/python-uvloop: force re-cythonize on build
    The generated loop.c provided with the tarball was generated with
    Cython 0.29.10 and it doesn't compile against the new Python 3.8
    headers. Upstream is now working on a new uvloop release which will
    use the newest Cython (0.29.13).  Until then, we will remove the
    current loop.c and force regenration of it.
    
    Fixes:
     - http://autobuild.buildroot.net/results/bffe1b2ee7b674f870e15398680ce5b5b22fd296
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-uvloop/python-uvloop.mk
+++ package/python-uvloop/python-uvloop.mk
@@ -10,8 +10,15 @@ PYTHON_UVLOOP_SITE = https://files.pythonhosted.org/packages/e3/15/dc3276384f436
 PYTHON_UVLOOP_SETUP_TYPE = setuptools
 PYTHON_UVLOOP_LICENSE = Apache-2.0, MIT
 PYTHON_UVLOOP_LICENSE_FILES = LICENSE-APACHE LICENSE-MIT
-PYTHON_UVLOOP_BUILD_OPTS = build_ext --inplace --use-system-libuv
-PYTHON_UVLOOP_INSTALL_TARGET_OPTS = build_ext --inplace --use-system-libuv
-PYTHON_UVLOOP_DEPENDENCIES = libuv
+PYTHON_UVLOOP_BUILD_OPTS = build_ext --inplace --cython-always --use-system-libuv
+PYTHON_UVLOOP_INSTALL_TARGET_OPTS = build_ext --inplace --cython-always --use-system-libuv
+PYTHON_UVLOOP_DEPENDENCIES = libuv host-python-cython
+
+# force regenerating loop.c with cython. can be removed with the next
+# uvloop version bump
+define PYTHON_UVLOOP_FORCE_REGEN_LOOPC
+	$(RM) $(@D)/uvloop/loop.c
+endef
+PYTHON_UVLOOP_PRE_BUILD_HOOKS += PYTHON_UVLOOP_FORCE_REGEN_LOOPC
 
 $(eval $(python-package))
