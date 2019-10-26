245beea242e6bf770f0e3b8092b63b4b0502832c
Adrien Ricciardi <adrien.ricciardi@hotmail.fr>
2019-10-26 11:58:21 +0000

    package/linux-firmware: embed all RS9113 firmware files to target image
    Only rs9113_wlan_qspi.rps firmware was present on generated image.
    
    Signed-off-by: Adrien Ricciardi <adrien.ricciardi@hotmail.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/linux-firmware/linux-firmware.mk
+++ package/linux-firmware/linux-firmware.mk
@@ -519,7 +519,10 @@ LINUX_FIRMWARE_ALL_LICENSE_FILES += LICENCE.qla2xxx
 endif
 
 ifeq ($(BR2_PACKAGE_LINUX_FIRMWARE_REDPINE_RS9113),y)
-LINUX_FIRMWARE_FILES += rsi/rs9113_wlan_qspi.rps
+LINUX_FIRMWARE_FILES += \
+	rsi/rs9113_ap_bt_dual_mode.rps \
+	rsi/rs9113_wlan_bt_dual_mode.rps \
+	rsi/rs9113_wlan_qspi.rps
 # No license file; the license is in the file WHENCE
 # which is installed unconditionally
 endif
