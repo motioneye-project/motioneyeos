503fd718c768a9d96155ecaafb2c53a586671520
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-10-26 21:32:43 +0200

    package/ccache: bump version to 3.7.5
    Release notes:
    https://ccache.dev/releasenotes.html#_ccache_3_7_5
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/ccache/ccache.mk
+++ package/ccache/ccache.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CCACHE_VERSION = 3.7.4
+CCACHE_VERSION = 3.7.5
 CCACHE_SITE = https://github.com/ccache/ccache/releases/download/v$(CCACHE_VERSION)
 CCACHE_SOURCE = ccache-$(CCACHE_VERSION).tar.xz
 CCACHE_LICENSE = GPL-3.0+, others
