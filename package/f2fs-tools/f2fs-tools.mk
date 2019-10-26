f3614df9c448a2d11eb19057e4bed0436a39df3f
Grzegorz Blach <grzegorz@blach.pl>
2019-10-26 11:21:10 +0000

    package/f2fs-tools: bump to 1.13.0
    This release includes:
     - enable VERITY by default on Android
     - introduce some preen mode in fsck.f2fs
     - add f2fs_io tool
     - add casefolding support
    
    And, there are lots of bug fixes.
    
    Signed-off-by: Grzegorz Blach <grzegorz@blach.pl>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/f2fs-tools/f2fs-tools.mk
+++ package/f2fs-tools/f2fs-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-F2FS_TOOLS_VERSION = 1.12.0
+F2FS_TOOLS_VERSION = 1.13.0
 F2FS_TOOLS_SITE = https://git.kernel.org/pub/scm/linux/kernel/git/jaegeuk/f2fs-tools.git/snapshot
 F2FS_TOOLS_CONF_ENV = ac_cv_file__git=no
 F2FS_TOOLS_DEPENDENCIES = host-pkgconf util-linux
