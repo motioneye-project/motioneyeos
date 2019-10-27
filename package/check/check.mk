459fe7355cb57ad3206c61ab6d241f9e5f3f060d
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-10-27 10:02:20 +0100

    package/check: bump version to 0.13.0
    Release notes: https://github.com/libcheck/check/releases
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/check/check.mk
+++ package/check/check.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CHECK_VERSION = 0.12.0
+CHECK_VERSION = 0.13.0
 CHECK_SITE = https://github.com/libcheck/check/releases/download/$(CHECK_VERSION)
 CHECK_INSTALL_STAGING = YES
 CHECK_DEPENDENCIES = host-pkgconf
