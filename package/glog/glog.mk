3c4ff9cea9af08acfe3a246cdc7cdf6c05c9664e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-10-28 18:33:04 +0100

    package/glog: switch to cmake
    Switch to cmake to fix a build failure on m68k with uclibc because
    glog's configure tests wrongly thinks that -fPIE works
    
    By switching to cmake, we can remove the patch as the unittests won't be
    built anymore
    
    We can also finely tune options such as gflags, threads and dynamic
    library support. This last option is handled through the standard
    BUILD_SHARED_LIBS option
    
    Fixes:
     - http://autobuild.buildroot.net/results/5a659d960c24256a2905f129dfa70e3dc9e92aec
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/glog/glog.mk
+++ package/glog/glog.mk
@@ -9,23 +9,14 @@ GLOG_SITE = $(call github,google,glog,v$(GLOG_VERSION))
 GLOG_INSTALL_STAGING = YES
 GLOG_LICENSE = BSD-3-Clause
 GLOG_LICENSE_FILES = COPYING
-
-# No configure script provided, so we need autoreconf
-GLOG_AUTORECONF = YES
+GLOG_CONF_OPTS = \
+	$(if $(BR2_TOOLCHAIN_HAS_THREADS),-DWITH_THREADS=ON, -DWITH_THREADS=OFF)
 
 ifeq ($(BR2_PACKAGE_GFLAGS),y)
 GLOG_DEPENDENCIES = gflags
+GLOG_CONF_OPTS += -DWITH_GFLAGS=ON
+else
+GLOG_CONF_OPTS += -DWITH_GFLAGS=OFF
 endif
 
-# glog can optionally use atomic __sync built-ins. However, its
-# configure script only checks for the availability of the 4 bytes
-# version, but the code also uses the 1 byte version. While this works
-# on most architectures, it does not on architectures that implement
-# only the 4 bytes version, such as Microblaze. So if the architecture
-# does not implement the 1 byte version, we hint the configure script
-# that atomic built-ins should not be used.
-ifeq ($(BR2_TOOLCHAIN_HAS_SYNC_1),)
-GLOG_CONF_ENV += ac_cv___sync_val_compare_and_swap=no
-endif
-
-$(eval $(autotools-package))
+$(eval $(cmake-package))
