4576a846a805a3cd059f326351209e5e417eccf5
Francois Perrad <fperrad@gmail.com>
2019-10-28 20:25:18 +0100

    package/perl-moo: bump to version 2.003006
    license lives now in its own file
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/perl-moo/perl-moo.mk
+++ package/perl-moo/perl-moo.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PERL_MOO_VERSION = 2.003004
+PERL_MOO_VERSION = 2.003006
 PERL_MOO_SOURCE = Moo-$(PERL_MOO_VERSION).tar.gz
 PERL_MOO_SITE = $(BR2_CPAN_MIRROR)/authors/id/H/HA/HAARG
 PERL_MOO_LICENSE = Artistic or GPL-1.0+
-PERL_MOO_LICENSE_FILES = README
+PERL_MOO_LICENSE_FILES = LICENSE
 PERL_MOO_DISTNAME = Moo
 
 $(eval $(perl-package))
