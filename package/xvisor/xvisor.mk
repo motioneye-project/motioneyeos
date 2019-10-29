6abfa7018e692605fe5d82a81fc05f7f68b2009c
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-10-29 13:42:40 +0100

    package/xvisor: bump to version 0.2.11
    Add hash for license file
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/xvisor/xvisor.mk
+++ package/xvisor/xvisor.mk
@@ -4,7 +4,8 @@
 #
 ################################################################################
 
-XVISOR_VERSION = 0.2.9
+XVISOR_VERSION = 0.2.11
+XVISOR_SOURCE = xvisor-$(XVISOR_VERSION).tar.xz
 XVISOR_SITE = http://www.xhypervisor.org/tarball
 XVISOR_LICENSE = GPL-2.0+
 XVISOR_LICENSE_FILES = COPYING
