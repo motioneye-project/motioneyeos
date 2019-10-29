5017edfc10396f092424c023b5f9ca3e5ab421ec
Etienne Carriere <etienne.carriere@linaro.org>
2019-10-29 15:41:31 +0100

    package/optee-benchmark: bump version to 3.7.0
    Bump OP-TEE Benchmark package version to release 3.7.0.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/optee-benchmark/optee-benchmark.mk
+++ package/optee-benchmark/optee-benchmark.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-OPTEE_BENCHMARK_VERSION = 3.5.0
+OPTEE_BENCHMARK_VERSION = 3.7.0
 OPTEE_BENCHMARK_SITE = $(call github,linaro-swg,optee_benchmark,$(OPTEE_BENCHMARK_VERSION))
 OPTEE_BENCHMARK_LICENSE = BSD-2-Clause
 OPTEE_BENCHMARK_LICENSE_FILES = LICENSE
