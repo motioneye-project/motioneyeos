7f5ea32aede9007a7beb711d7b501984602f289d
Matt Weber <matthew.weber@rockwellcollins.com>
2019-10-30 10:00:39 -0500

    package/proftpd: bump to 1.3.6b
    This bump incorporates the changes found while preparing the 1.3.7
    release and drops the CVE 2019-12815 patch which was incorporated in
    https://github.com/proftpd/proftpd/commit/a73dbfe3b61459e7c2806d5162b12f0957990cb3.
    
    Adjusted the site location to point at github.
    
    Signed-off-by: Matthew Weber <matthew.weber@rockwellcollins.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/proftpd/proftpd.mk
+++ package/proftpd/proftpd.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PROFTPD_VERSION = 1.3.6
-PROFTPD_SITE = ftp://ftp.proftpd.org/distrib/source
+PROFTPD_VERSION = 1.3.6b
+PROFTPD_SITE = $(call github,proftpd,proftpd,v$(PROFTPD_VERSION))
 PROFTPD_LICENSE = GPL-2.0+
 PROFTPD_LICENSE_FILES = COPYING
 
