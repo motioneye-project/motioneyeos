7fa856caea2f116de52c3c28dc2012d2c86de34e
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-10-31 21:28:59 +0100

    package/kodi-vfs-sftp: bump version to 1.0.3
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-vfs-sftp/kodi-vfs-sftp.mk
+++ package/kodi-vfs-sftp/kodi-vfs-sftp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_VFS_SFTP_VERSION = 4e385ae0f82c062add44bce4a2d6a9058a3e0f48
+KODI_VFS_SFTP_VERSION = 1.0.3-Leia
 KODI_VFS_SFTP_SITE = $(call github,xbmc,vfs.sftp,$(KODI_VFS_SFTP_VERSION))
 KODI_VFS_SFTP_LICENSE = GPL-2.0+
 KODI_VFS_SFTP_LICENSE_FILES = src/SFTPFile.cpp
