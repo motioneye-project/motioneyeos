1185b0a56e21d10d2541d0c4da752f8cec53c4c6
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-11-03 11:15:06 +0100

    Revert "package/grantlee: fix build with Qt 5.x"
    This reverts commit c975106f60d909b244f473e381b1ebffd943364a which does
    not seem to really work. The issue will be fixed by a patch retrieved
    from upstream.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/grantlee/grantlee.mk
+++ package/grantlee/grantlee.mk
@@ -11,9 +11,4 @@ GRANTLEE_LICENSE = LGPL-2.1+
 GRANTLEE_LICENSE_FILES = COPYING.LIB
 GRANTLEE_DEPENDENCIES = qt5base qt5script
 
-# qt5 needs c++11 (since qt-5.7)
-ifeq ($(BR2_PACKAGE_QT5_VERSION_LATEST),y)
-GRANTLEE_CONF_OPTS += -DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) -std=c++11"
-endif
-
 $(eval $(cmake-package))
