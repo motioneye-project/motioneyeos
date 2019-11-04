9f9a50a38e457f5d8aaf79d36ce7331bb06fb6e6
Jörg Krause <joerg.krause@embedded.rocks>
2019-11-04 15:01:50 +0100

    package/libupnp18: bump to version 1.8.5
    Drop patches included in new version. No need for autoreconf anymore.
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libupnp18/libupnp18.mk
+++ package/libupnp18/libupnp18.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUPNP18_VERSION = 1.8.4
+LIBUPNP18_VERSION = 1.8.5
 LIBUPNP18_SOURCE = libupnp-$(LIBUPNP18_VERSION).tar.bz2
 LIBUPNP18_SITE = http://downloads.sourceforge.net/project/pupnp/pupnp/libupnp-$(LIBUPNP18_VERSION)
 LIBUPNP18_CONF_ENV = ac_cv_lib_compat_ftime=no
@@ -12,8 +12,6 @@ LIBUPNP18_INSTALL_STAGING = YES
 LIBUPNP18_LICENSE = BSD-3-Clause
 LIBUPNP18_LICENSE_FILES = COPYING
 LIBUPNP18_DEPENDENCIES = host-pkgconf
-# We're patching configure.ac
-LIBUPNP18_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
 LIBUPNP18_CONF_OPTS += --enable-open-ssl
