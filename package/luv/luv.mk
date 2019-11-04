40eecd15e81ca2e6f056a4805a07739aba58b1f0
Jörg Krause <joerg.krause@embedded.rocks>
2019-11-04 15:20:02 +0100

    package/luv: bump to version 1.32.0-0
    Drop hook to add pc-file. Correct installation of the pc-file is
    included in the new version.
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/luv/luv.mk
+++ package/luv/luv.mk
@@ -4,18 +4,12 @@
 #
 ################################################################################
 
-LUV_VERSION = 1.30.1-0
+LUV_VERSION = 1.32.0-0
 LUV_SITE = https://github.com/luvit/luv/releases/download/$(LUV_VERSION)
 LUV_LICENSE = Apache-2.0
 LUV_LICENSE_FILES = LICENSE.txt
 LUV_DEPENDENCIES = libuv
 LUV_INSTALL_STAGING = YES
-
-# Archive 1.30.1-0 doesn't contain libluv.pc.in so install it in this hook
-define LUV_INSTALL_PC_IN
-	cp package/luv/libluv.pc.in $(@D)/
-endef
-LUV_POST_EXTRACT_HOOKS += LUV_INSTALL_PC_IN
 
 LUV_CONF_OPTS += \
 	-DBUILD_MODULE=OFF \
