d908329a2fa2922157ef29ad04dcc8ab26ec2c95
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-11-04 08:12:39 +0100

    package/mpd-mpc: bump version to 0.33
    Changelog: https://github.com/MusicPlayerDaemon/mpc/blob/master/NEWS
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mpd-mpc/mpd-mpc.mk
+++ package/mpd-mpc/mpd-mpc.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 MPD_MPC_VERSION_MAJOR = 0
-MPD_MPC_VERSION = $(MPD_MPC_VERSION_MAJOR).32
+MPD_MPC_VERSION = $(MPD_MPC_VERSION_MAJOR).33
 MPD_MPC_SITE = http://www.musicpd.org/download/mpc/$(MPD_MPC_VERSION_MAJOR)
 MPD_MPC_SOURCE = mpc-$(MPD_MPC_VERSION).tar.xz
 MPD_MPC_LICENSE = GPL-2.0+
