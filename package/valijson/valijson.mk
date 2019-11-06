d8e2876104be3f9f01613b17d3e60120883be1a1
Santosh Multhalli <santosh.multhalli@rockwellcollins.com>
2019-11-06 20:18:02 +0530

    package/valijson: bump version to 0.2
    - Added hash of LICENSE file in valijson.hash
    - Updated hash of valijson tar file in valijson.hash file
    - Added option to set tests off which drops the curlpp dependency
    
    Signed-off-by: Santosh Multhalli <santosh.multhalli@rockwellcollins.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/valijson/valijson.mk
+++ package/valijson/valijson.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-VALIJSON_VERSION = 0.1
+VALIJSON_VERSION = 0.2
 VALIJSON_SITE = $(call github,tristanpenman,valijson,v$(VALIJSON_VERSION))
 VALIJSON_LICENSE = BSD-2-Clause
 VALIJSON_LICENSE_FILES = LICENSE
 VALIJSON_INSTALL_STAGING = YES
 VALIJSON_INSTALL_TARGET = NO
 VALIJSON_DEPENDENCIES = boost
-VALIJSON_CONF_OPTS = -DINSTALL_HEADERS=TRUE
+VALIJSON_CONF_OPTS = -Dvalijson_INSTALL_HEADERS=TRUE -Dvalijson_BUILD_TESTS=FALSE
 
 $(eval $(cmake-package))
