267131f98374154f338b62f60d01d4d3c6686d99
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-11-08 18:07:42 +0100

    package/safeclib: bump to version 08112019
    - Drop patch (already in version)
    - Include a fix for musl on x86_64:
      https://github.com/rurban/safeclib/commit/19e7154e9c5189b0dfb5594b94cb7b883f9b6dcf
    
    Fixes:
     - http://autobuild.buildroot.org/results/7923fe7e609a6b2638492350996cc96582cc9193
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/safeclib/safeclib.mk
+++ package/safeclib/safeclib.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-SAFECLIB_VERSION = 17102019
+SAFECLIB_VERSION = 08112019
 SAFECLIB_SITE = \
 	https://github.com/rurban/safeclib/releases/download/v$(SAFECLIB_VERSION)
-SAFECLIB_SOURCE = libsafec-$(SAFECLIB_VERSION).0-g5d92be.tar.bz2
+SAFECLIB_SOURCE = libsafec-$(SAFECLIB_VERSION).0-gad76c7.tar.bz2
 SAFECLIB_LICENSE = MIT
 SAFECLIB_LICENSE_FILES = COPYING
 SAFECLIB_INSTALL_STAGING = YES
