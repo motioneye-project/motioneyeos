c074fade2c5cfde60c26003c25c05471d1e00c0f
Peter Korsgaard <peter@korsgaard.com>
2019-11-13 16:17:16 +0100

    package/dropwatch: license is GPL-2.0+
    Dropwatch links with readline, which is GPL-3.0+, so not compatible with
    GPL-2.0.  When asked about this, upstream has clarified that the license
    really is GPL-2.0+:
    
    https://github.com/nhorman/dropwatch/issues/14
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    [yann.morin.1998@free.fr: add a coomet as suggested by Baruc]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    Cc: Baruch Siach <baruch@tkos.co.il>

--- package/dropwatch/dropwatch.mk
+++ package/dropwatch/dropwatch.mk
@@ -7,7 +7,9 @@
 DROPWATCH_VERSION = 1.5.1
 DROPWATCH_SITE = $(call github,nhorman,dropwatch,v$(DROPWATCH_VERSION))
 DROPWATCH_DEPENDENCIES = libnl readline host-pkgconf $(TARGET_NLS_DEPENDENCIES)
-DROPWATCH_LICENSE = GPL-2.0
+# Until upstream updates their tree with a proper license
+# blurb: https://github.com/nhorman/dropwatch/issues/14
+DROPWATCH_LICENSE = GPL-2.0+
 DROPWATCH_LICENSE_FILES = COPYING
 # From git
 DROPWATCH_AUTORECONF = YES
