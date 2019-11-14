93e9b5378d8ed6c67caf0aa9aca18b65d0ce0b45
Peter Korsgaard <peter@korsgaard.com>
2019-11-14 22:20:12 +0100

    package/neardal: unconditionally use libedit
    neardal is licensed under LGPL-2.0, which is (most likely) not license
    compatible with readline (GPL-3.0+), so always use the libedit backend.
    
    The choice is done by ./configure, which first checks for libedit, and
    uses readline only as a fallback. Since we do build libedit before
    neardal, that's what is going to be picked up.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    [yann.morin.1998@free.fr:
      - add blurb about preference in ./configure
    ]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/neardal/neardal.mk
+++ package/neardal/neardal.mk
@@ -10,16 +10,8 @@ NEARDAL_INSTALL_STAGING = YES
 NEARDAL_LICENSE = LGPL-2.0
 NEARDAL_LICENSE_FILES = COPYING
 
-NEARDAL_DEPENDENCIES = host-pkgconf dbus dbus-glib
+NEARDAL_DEPENDENCIES = host-pkgconf dbus dbus-glib libedit
 NEARDAL_AUTORECONF = YES
-
-# Either readline or libedit are needed, and the Config.in file
-# guarantees at least one of them is enabled
-ifeq ($(BR2_PACKAGE_READLINE),y)
-NEARDAL_DEPENDENCIES += readline
-else ifeq ($(BR2_PACKAGE_LIBEDIT),y)
-NEARDAL_DEPENDENCIES += libedit
-endif
 
 define NEARDAL_INSTALL_NCL
 	$(INSTALL) -m 0755 -D $(@D)/ncl/ncl $(TARGET_DIR)/usr/bin/ncl
