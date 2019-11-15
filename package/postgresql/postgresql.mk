c870ce547884ba519c5c69d652f48b33f8fa202c
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-11-15 19:08:31 +0100

    package/postgresql: bump version to 12.1
    Release notes of the bugfix release:
    https://www.postgresql.org/about/news/1994/
    https://www.postgresql.org/docs/current/release-12-1.html
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 7a709f77c8099af97ed532ff4bd8c0cfdc26df09)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/postgresql/postgresql.mk
+++ package/postgresql/postgresql.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-POSTGRESQL_VERSION = 12.0
+POSTGRESQL_VERSION = 12.1
 POSTGRESQL_SOURCE = postgresql-$(POSTGRESQL_VERSION).tar.bz2
 POSTGRESQL_SITE = https://ftp.postgresql.org/pub/source/v$(POSTGRESQL_VERSION)
 POSTGRESQL_LICENSE = PostgreSQL
