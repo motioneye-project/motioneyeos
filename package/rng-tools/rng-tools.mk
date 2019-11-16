b6606937d861cc4de60e40baec1cb6cdb61b2aab
Matt Weber <matthew.weber@rockwellcollins.com>
2019-11-16 06:42:47 -0600

    package/rng-tools: bump to 6.8
    Dropped autoreconf and non-standard pthread yield patches as they merged
    upstream.
    
    [Peter: really drop autoreconf]
    Signed-off-by: Matthew Weber <matthew.weber@rockwellcollins.com
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rng-tools/rng-tools.mk
+++ package/rng-tools/rng-tools.mk
@@ -4,13 +4,11 @@
 #
 ################################################################################
 
-RNG_TOOLS_VERSION = 6.7
+RNG_TOOLS_VERSION = 6.8
 RNG_TOOLS_SITE = $(call github,nhorman,$(RNG_TOOLS_NAME),v$(RNG_TOOLS_VERSION))
 RNG_TOOLS_LICENSE = GPL-2.0
 RNG_TOOLS_LICENSE_FILES = COPYING
 RNG_TOOLS_DEPENDENCIES = libsysfs jitterentropy-library host-pkgconf
-
-RNG_TOOLS_AUTORECONF = YES
 
 RNG_TOOLS_CONF_OPTS = \
 	--without-nistbeacon \
