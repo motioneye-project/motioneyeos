cf053cf414d9290b68f4737dcf457b62fbb43842
Matt Weber <matthew.weber@rockwellcollins.com>
2019-11-16 07:29:30 -0600

    package/python-txdbus: bump to 1.1.1
    Signed-off-by: Matthew Weber <matthew.weber@rockwellcollins.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-txdbus/python-txdbus.mk
+++ package/python-txdbus/python-txdbus.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TXDBUS_VERSION = 1.1.0
+PYTHON_TXDBUS_VERSION = 1.1.1
 PYTHON_TXDBUS_SOURCE = txdbus-$(PYTHON_TXDBUS_VERSION).tar.gz
-PYTHON_TXDBUS_SITE = https://files.pythonhosted.org/packages/8e/7c/0b8726b82943ae99dc71b8fe20e2e0beb7feb4ef61105865021a64f08b16
+PYTHON_TXDBUS_SITE = https://files.pythonhosted.org/packages/4a/68/dfd06f3f349999cbbb31eade239fe76fbff2d6a905eb7d20449666d1b2ce
 PYTHON_TXDBUS_SETUP_TYPE = setuptools
 PYTHON_TXDBUS_LICENSE = MIT
 
