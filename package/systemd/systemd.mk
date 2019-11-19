d3af5d7040a3e3234ac2b80549d5213c9f64b61f
Jérémy Rosen <jeremy.rosen@smile.fr>
2019-11-19 21:23:35 +0100

    package/systemd: bump to v243.4
    Upstream systemd-stable has started tagging point releses.
    
    The commit we currently used has now been tagged as v243.3, and this
    brings us to v243.4.
    
    Signed-off-by: Jérémy Rosen <jeremy.rosen@smile.fr>
    [yann.morin.1998@free.fr:
      - expand commit log to explain previous version
    ]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/systemd/systemd.mk
+++ package/systemd/systemd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SYSTEMD_VERSION = 243-78-gef677436aa
+SYSTEMD_VERSION = 243.4
 SYSTEMD_SITE = $(call github,systemd,systemd-stable,v$(SYSTEMD_VERSION))
 SYSTEMD_LICENSE = LGPL-2.1+, GPL-2.0+ (udev), Public Domain (few source files, see README)
 SYSTEMD_LICENSE_FILES = LICENSE.GPL2 LICENSE.LGPL2.1 README
