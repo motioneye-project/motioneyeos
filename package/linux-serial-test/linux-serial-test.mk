01d8b65dc733b399a0335ebdf22642b3a15a9883
Yegor Yefremov <yegorslists@googlemail.com>
2019-11-20 11:36:55 +0100

    package/linux-serial-test: bump version
    This update fixes both the below mentioned build error when handling
    undefined baud rates, and makes the patch for MIPS obsolete.
    
    No other changes will be introduced with this update.
    
    Fixes:
    http://autobuild.buildroot.net/results/ef77cbe220619050eb9d46c78ae79a94eea8aa8b
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/linux-serial-test/linux-serial-test.mk
+++ package/linux-serial-test/linux-serial-test.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LINUX_SERIAL_TEST_VERSION = 5d11b31d9c9807cd58e53a8628bc6402b6fe7fac
+LINUX_SERIAL_TEST_VERSION = 13bea838f2a7573d2a68a6d95fc9d1f153b580be
 LINUX_SERIAL_TEST_SITE = $(call github,cbrake,linux-serial-test,$(LINUX_SERIAL_TEST_VERSION))
 LINUX_SERIAL_TEST_LICENSE = MIT
 LINUX_SERIAL_TEST_LICENSE_FILES = LICENSES/MIT
