ccca4f09547e6b59c65a975ff155f5b18cb4ced2
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-11-20 20:36:16 +0100

    package/clamav: security bump version to 0.102.1
    Release notes:
    https://lists.clamav.net/pipermail/clamav-announce/2019/000043.html
    
    Fixes CVE-2019-15961.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/clamav/clamav.mk
+++ package/clamav/clamav.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CLAMAV_VERSION = 0.102.0
+CLAMAV_VERSION = 0.102.1
 CLAMAV_SITE = https://www.clamav.net/downloads/production
 CLAMAV_LICENSE = GPL-2.0
 CLAMAV_LICENSE_FILES = COPYING COPYING.bzip2 COPYING.file COPYING.getopt \
