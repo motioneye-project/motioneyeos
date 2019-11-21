dbfd6d8194a2bcdba68b34898d3af21ee26cc23f
Jörg Krause <joerg.krause@embedded.rocks>
2019-11-21 08:52:10 +0100

    package/libupnp18: bump to version 1.8.6
    This version fixes a runtime issue which crashes gerbera:
    https://github.com/gerbera/gerbera/issues/522
    
    The issue has been reported upstream:
    https://github.com/mrjimenez/pupnp/pull/122
    
    The fix for this issue is the only "useful" commit between 1.8.5 and
    1.8.6:
    
    $ git --no-pager log --format=oneline release-1.8.5..release-1.8.6
    71a47673795e9228775959ea23a984ff6c4d0a43 (tag: release-1.8.6) Adjust the library numbers for release
    436aae7b617a4cd7bc1e1411d6882780699eb2ee Put the 1.8.6 release on README.md
    90069231d83d2f365b76e2b15d918dfb06209970 Update README.md
    7d6158d2c88245f2da4354a8bd0bc359eb15fac6 Update Changelog and THANKS
    463f1cc025b27af35b0b73a05ba379d0051bcedf Fix format string for ExtraHeaders
    8516da470bf32fa1f5c6f59aac3508378d5a85be Homekeeping for the next release
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libupnp18/libupnp18.mk
+++ package/libupnp18/libupnp18.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUPNP18_VERSION = 1.8.5
+LIBUPNP18_VERSION = 1.8.6
 LIBUPNP18_SOURCE = libupnp-$(LIBUPNP18_VERSION).tar.bz2
 LIBUPNP18_SITE = http://downloads.sourceforge.net/project/pupnp/pupnp/libupnp-$(LIBUPNP18_VERSION)
 LIBUPNP18_CONF_ENV = ac_cv_lib_compat_ftime=no
