a7ba9c54b4f5cbb36386b366bfb5e5fd4ad2fe7a
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-11-21 22:37:33 +0100

    package/php: bump version to 7.3.12
    Release notes of this bugfix release:
    https://www.php.net/ChangeLog-7.php#7.3.12
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/php/php.mk
+++ package/php/php.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PHP_VERSION = 7.3.11
+PHP_VERSION = 7.3.12
 PHP_SITE = http://www.php.net/distributions
 PHP_SOURCE = php-$(PHP_VERSION).tar.xz
 PHP_INSTALL_STAGING = YES
