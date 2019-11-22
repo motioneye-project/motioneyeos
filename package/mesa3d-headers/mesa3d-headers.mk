2ee02eaaee90a7f1f00b44985624f466dc386a83
Bernd Kuhls <bernd.kuhls@t-online.de>
2019-11-22 06:25:36 +0100

    package/{mesa3d, mesa3d-headers}: bump version to 19.2.6
    Release notes of this bugfix release:
    https://lists.freedesktop.org/archives/mesa-announce/2019-November/000559.html
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/mesa3d-headers/mesa3d-headers.mk
+++ package/mesa3d-headers/mesa3d-headers.mk
@@ -12,7 +12,7 @@ endif
 
 # Not possible to directly refer to mesa3d variables, because of
 # first/second expansion trickery...
-MESA3D_HEADERS_VERSION = 19.2.5
+MESA3D_HEADERS_VERSION = 19.2.6
 MESA3D_HEADERS_SOURCE = mesa-$(MESA3D_HEADERS_VERSION).tar.xz
 MESA3D_HEADERS_SITE = https://mesa.freedesktop.org/archive
 MESA3D_HEADERS_DL_SUBDIR = mesa3d
