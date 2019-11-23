a25e67fe87644dfc6d25030dca152b7eb0f9c546
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-11-23 18:06:19 +0100

    package/libidn2: bump to version 2.3.0
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libidn2/libidn2.mk
+++ package/libidn2/libidn2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBIDN2_VERSION = 2.2.0
+LIBIDN2_VERSION = 2.3.0
 LIBIDN2_SITE = $(BR2_GNU_MIRROR)/libidn
 LIBIDN2_LICENSE = GPL-2.0+ or LGPL-3.0+ (library)
 LIBIDN2_LICENSE_FILES = COPYING COPYINGv2 COPYING.LESSERv3 COPYING.unicode
