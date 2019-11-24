9d1b48a1e56b54fce6d0b2b451d5f4dd829daf1b
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-11-24 10:10:51 +0100

    package/gob2: add license
    gob2 itself is GPL-2.0+, but it is a code generator. The code generated
    by gob2 id not covered by gob2's license, and this is made explicit in
    an accompanying license file.
    
    So we include both license files.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    [yann.morin.1998@free.fr:
      - add COPYING.generated-code
      - expand commit log accordingly
    ]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/gob2/gob2.mk
+++ package/gob2/gob2.mk
@@ -7,6 +7,8 @@
 GOB2_VERSION = 2.0.20
 GOB2_SOURCE = gob2-$(GOB2_VERSION).tar.xz
 GOB2_SITE = http://ftp.5z.com/pub/gob
+GOB2_LICENSE = GPL-2.0+
+GOB2_LICENSE_FILES = COPYING COPYING.generated-code
 HOST_GOB2_DEPENDENCIES = host-bison host-flex host-libglib2
 
 $(eval $(host-autotools-package))
