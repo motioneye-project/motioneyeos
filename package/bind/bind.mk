0432e5713a63142bdb4344b6fdfaa7adc1cd1d81
Peter Korsgaard <peter@korsgaard.com>
2019-11-24 21:59:17 +0100

    package/bind: security bump to version 9.11.13
    Fixes the following security vulnerabilities:
    
    - CVE-2019-6477: TCP-pipelined queries can bypass tcp-clients limit
    
    For details, see the release notes:
    https://downloads.isc.org/isc/bind9/9.11.13/RELEASE-NOTES-bind-9.11.13.html
    
    (9.11.11..12 were not released)
    
    Upstream moved to a 2019-2020 signing key, so update comment in hash file.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/bind/bind.mk
+++ package/bind/bind.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BIND_VERSION = 9.11.10
+BIND_VERSION = 9.11.13
 BIND_SITE = https://ftp.isc.org/isc/bind9/$(BIND_VERSION)
 # bind does not support parallel builds.
 BIND_MAKE = $(MAKE1)
