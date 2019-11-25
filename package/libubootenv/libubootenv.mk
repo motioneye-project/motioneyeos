3e472fe8f9dffed0cd57c3bd1cc5d339ad6e0ca4
Pierre-Jean Texier <pjtexier@koncepto.io>
2019-11-25 23:01:05 +0100

    package/libubootenv: bump to version 9294981
    This includes the following changes:
    
    9294981 handle protected mmcblk_boot_ devices
    ac6d18a add missing includes
    8106b65 add .editorconfig file
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libubootenv/libubootenv.mk
+++ package/libubootenv/libubootenv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUBOOTENV_VERSION = 3286c571c7e15be4b341e46986f1c0dc74730207
+LIBUBOOTENV_VERSION = 92949816720d7af2ac722016e7a5b9a85ff141bc
 LIBUBOOTENV_SITE = $(call github,sbabic,libubootenv,$(LIBUBOOTENV_VERSION))
 LIBUBOOTENV_LICENSE = LGPL-2.1
 LIBUBOOTENV_LICENSE_FILES = Licenses/lgpl-2.1.txt
