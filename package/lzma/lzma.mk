ea107efc97fa7202d8af59e4040937b09e5e2b05
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-11-28 21:17:26 +0100

    package/lzma: add license
    - lzma program is licensed under GPL-2.0+
    - lzmadec program has no license information in source file
    - lzmainfo program is licensed under LGPL-2.1+
    - LzmaDecode.h is licensed under LGPL-2.1+ (or CPL) as stated in
      src/sdk/7zip/Compress/LZMA_C/LzmaDecode.{c,h}, other sdk files have no
      license information
    - lzmore and lzgrep scripts are licensed under GPL-2.0+
    - lzdiff script has no license information
    - basic and perms tests are licensed under GPL-3.0+
    - mkdtemp and test-lib.sh tests have no license information
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/lzma/lzma.mk
+++ package/lzma/lzma.mk
@@ -7,6 +7,8 @@
 LZMA_VERSION = 4.32.7
 LZMA_SOURCE = lzma-$(LZMA_VERSION).tar.xz
 LZMA_SITE = http://tukaani.org/lzma
+LZMA_LICENSE = LGPL-2.1+ (lzmadec library, lzmainfo, LzmaDecode), GPL-2.0+ (lzma program, lzgrep and lzmore scripts), GPL-3.0+ (tests)
+LZMA_LICENSE_FILES = COPYING.GPLv2 COPYING.GPLv3 COPYING.LGPLv2.1
 
 $(eval $(host-autotools-package))
 
