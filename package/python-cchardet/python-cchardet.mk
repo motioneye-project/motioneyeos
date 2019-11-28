2da1130670447e38d94609c076e99f55352c5da1
James Hilliard <james.hilliard1@gmail.com>
2019-11-28 15:21:18 -0700

    package/python-cchardet: bump to version 2.1.5
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-cchardet/python-cchardet.mk
+++ package/python-cchardet/python-cchardet.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CCHARDET_VERSION = 2.1.4
+PYTHON_CCHARDET_VERSION = 2.1.5
 PYTHON_CCHARDET_SOURCE = cchardet-$(PYTHON_CCHARDET_VERSION).tar.gz
-PYTHON_CCHARDET_SITE = https://files.pythonhosted.org/packages/74/64/3988d388315c1af3e24f447689dadf30edead43366fb2041cb103380b57f
+PYTHON_CCHARDET_SITE = https://files.pythonhosted.org/packages/73/25/73649708a30aa97124631bd088a22f1bf721d05d4e72fb9cc9bedb97de51
 PYTHON_CCHARDET_SETUP_TYPE = setuptools
 PYTHON_CCHARDET_LICENSE = MPL-1.1
 PYTHON_CCHARDET_LICENSE_FILES = COPYING
