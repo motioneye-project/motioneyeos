8b93ddb642cd4314306f7b464370d115322e0d1c
Michael Vetter <jubalh@iodoru.org>
2019-11-29 11:12:38 +0100

    package/tiff: security bump to 4.1.0
    Fixes the following security vulnerabilities:
      * CVE-2018-12900
      * CVE-2018-17000
      * CVE-2019-6128
      * CVE-2019-7663
      * CVE-2019-14973
      * CVE-2018-19210
    
    Remove because contained upstream:
    0001-Fix-for-simple-memory-leak-that-was-assigned-CVE-2019-6128.patch
    
    Signed-off-by: Michael Vetter <jubalh@iodoru.org>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tiff/tiff.mk
+++ package/tiff/tiff.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TIFF_VERSION = 4.0.10
+TIFF_VERSION = 4.1.0
 TIFF_SITE = http://download.osgeo.org/libtiff
 TIFF_LICENSE = tiff license
 TIFF_LICENSE_FILES = COPYRIGHT
