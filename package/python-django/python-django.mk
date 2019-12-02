6340272e88db87a3917b69228997fcba1a9e37dd
Peter Korsgaard <peter@korsgaard.com>
2019-12-02 13:23:54 +0100

    package/python-django: security bump to version 2.2.8
    Fixes the following security vulnerabilities:
    
    - CVE-2019-19118: Privilege escalation in the Django admin
    
    Additionally, 2.2.8 (and 2.2.7) fixes a number of bugs and adds python 3.8
    support.
    
    For more details, see the release notes:
    https://docs.djangoproject.com/en/dev/releases/2.2.8/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 2.2.6
+PYTHON_DJANGO_VERSION = 2.2.8
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/c7/2c/bbd0fddf6a08456c3100b8e8b230f3288d4511985aa4e2368b0d115b5aae
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/1c/aa/f618f346b895123be44739b276099a2b418b45b2b7afb5e1071403e8d2e9
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_SETUP_TYPE = setuptools
