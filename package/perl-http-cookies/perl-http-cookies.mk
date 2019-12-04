de2cb081bb1e9f1dc86e59626e1d72e1a82e03c5
Francois Perrad <fperrad@gmail.com>
2019-12-04 09:33:49 +0100

    package/perl-http-cookies: bump to version 6.08
    years updated in LICENSE:
    < This software is copyright (c) 2002-2017 by Gisle Aas.
    > This software is copyright (c) 2002-2019 by Gisle Aas.
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/perl-http-cookies/perl-http-cookies.mk
+++ package/perl-http-cookies/perl-http-cookies.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_HTTP_COOKIES_VERSION = 6.05
+PERL_HTTP_COOKIES_VERSION = 6.08
 PERL_HTTP_COOKIES_SOURCE = HTTP-Cookies-$(PERL_HTTP_COOKIES_VERSION).tar.gz
 PERL_HTTP_COOKIES_SITE = $(BR2_CPAN_MIRROR)/authors/id/O/OA/OALDERS
 PERL_HTTP_COOKIES_LICENSE = Artistic or GPL-1.0+
