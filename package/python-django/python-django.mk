f58ffc49ce5d0988549575ef18033ddda17bc809
Adam Duskett <aduskett@greenlots.com>
2019-12-04 12:06:28 -0800

    package/python-django: bump to version 3.0
    Other changes:
      - Select BR2_PACKAGE_PYTHON_ASGIREF as a new runtime dependency.
      - Select BR2_PACKAGE_PYTHON_SQLPARSE as a new runtime dependency.
    
    Signed-off-by: Adam Duskett <aduskett@greenlots.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 2.2.8
+PYTHON_DJANGO_VERSION = 3.0
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/1c/aa/f618f346b895123be44739b276099a2b418b45b2b7afb5e1071403e8d2e9
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/f8/46/b3b8c61f867827fff2305db40659495dcd64fb35c399e75c53f23c113871
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_SETUP_TYPE = setuptools
