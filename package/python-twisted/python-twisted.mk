c301c190f9c8364801949e30941461b9bfd6d6e9
Adam Duskett <Aduskett@gmail.com>
2019-12-05 14:15:12 -0800

    package/python-twisted: bump to version 19.10.0
    Tested with
    run-tests tests.package.test_python_twisted.TestPythonPy3Twisted -d ./dl -o ./o
    
    Ran 1 test in 388.506s
    OK
    
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-twisted/python-twisted.mk
+++ package/python-twisted/python-twisted.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TWISTED_VERSION = 19.7.0
+PYTHON_TWISTED_VERSION = 19.10.0
 PYTHON_TWISTED_SOURCE = Twisted-$(PYTHON_TWISTED_VERSION).tar.bz2
-PYTHON_TWISTED_SITE = https://files.pythonhosted.org/packages/61/31/3855dcacd1d3b2e60c0b4ccc8e727b8cd497bd7087d327d81a9f0cbb580c
+PYTHON_TWISTED_SITE = https://files.pythonhosted.org/packages/0b/95/5fff90cd4093c79759d736e5f7c921c8eb7e5057a70d753cdb4e8e5895d7
 PYTHON_TWISTED_SETUP_TYPE = setuptools
 PYTHON_TWISTED_LICENSE = MIT
 PYTHON_TWISTED_LICENSE_FILES = LICENSE
