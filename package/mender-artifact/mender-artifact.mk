c20a107c152a5dd2a5117899b9ccf34d0de85613
Adam Duskett <Aduskett@gmail.com>
2019-12-05 12:06:36 -0800

    package/mender-artifact: bump to version 3.2.1
    Other changes:
      - Add a sha256sum for LIC_FILES_CHKSUM.sha256
      - Remove duplicated license file vendor/github.com/stretchr/testify/LICENSE
    
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mender-artifact/mender-artifact.mk
+++ package/mender-artifact/mender-artifact.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HOST_MENDER_ARTIFACT_VERSION = 3.2.0
+HOST_MENDER_ARTIFACT_VERSION = 3.2.1
 HOST_MENDER_ARTIFACT_SITE = https://github.com/mendersoftware/mender-artifact/archive
 HOST_MENDER_ARTIFACT_SOURCE = $(HOST_MENDER_ARTIFACT_VERSION).tar.gz
 HOST_MENDER_ARTIFACT_LICENSE = Apache2.0, BSD-2-Clause, BSD-3-Clause, ISC, MIT
@@ -18,7 +18,6 @@ HOST_MENDER_ARTIFACT_LICENSE_FILES = \
 	vendor/golang.org/x/crypto/LICENSE \
 	vendor/github.com/remyoudompheng/go-liblzma/LICENSE \
 	vendor/github.com/davecgh/go-spew/LICENSE \
-	vendor/github.com/stretchr/testify/LICENSE \
 	vendor/github.com/stretchr/testify/LICENSE \
 	vendor/github.com/urfave/cli/LICENSE \
 	vendor/github.com/sirupsen/logrus/LICENSE
