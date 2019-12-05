ee62fad783ca680ea617222bc9bc26d7c058afb8
Adam Duskett <Aduskett@gmail.com>
2019-12-05 14:15:15 -0800

    package/python-daphne: bump to version 2.4.0
    Other changes:
      - Update hash for PKG-INFO due to a version bump in the file.
      - select BR2_PACKAGE_PYTHON_TWISTED_TLS as a runtime dependency.
      - Update 0001-remove-pytest-runner-requirement.patch to work with the new
        version.
    
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-daphne/python-daphne.mk
+++ package/python-daphne/python-daphne.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_DAPHNE_VERSION = 2.3.0
+PYTHON_DAPHNE_VERSION = 2.4.0
 PYTHON_DAPHNE_SOURCE = daphne-$(PYTHON_DAPHNE_VERSION).tar.gz
-PYTHON_DAPHNE_SITE = https://files.pythonhosted.org/packages/7b/f1/994f55248e27059e160f9d07bf5d325a9891b99daf1eb775a14110e91e5b
+PYTHON_DAPHNE_SITE = https://files.pythonhosted.org/packages/c6/66/05ace288000d323831a9fab1e68a5625b117b7c8aca408404d33330f3906
 PYTHON_DAPHNE_SETUP_TYPE = setuptools
 # https://github.com/django/daphne/blob/master/LICENSE
 PYTHON_DAPHNE_LICENSE = BSD-3-Clause
