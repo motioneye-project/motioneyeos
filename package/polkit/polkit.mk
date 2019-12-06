ce2a5eff7890815afeb26105c7760b6f8d2986e0
Adam Duskett <Aduskett@gmail.com>
2019-12-06 15:46:44 -0800

    package/polkit: bump to version 0.116
    Other changes:
      - Add spidermonkey as a dependency.
      - Add 0001-make-netgroup-support-optional.patch to allow building on musl.
      - Add a runtime dependency on dbus.
      - Add --disable-libelongind.
      - Add --disable-libsystemd-login.
      - Update dependencies for systemd pam support.
      - Update dependencies for udisks.
    
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/polkit/polkit.mk
+++ package/polkit/polkit.mk
@@ -4,20 +4,22 @@
 #
 ################################################################################
 
-POLKIT_VERSION = 0.105
+POLKIT_VERSION = 0.116
 POLKIT_SITE = http://www.freedesktop.org/software/polkit/releases
 POLKIT_LICENSE = GPL-2.0
 POLKIT_LICENSE_FILES = COPYING
 
 POLKIT_INSTALL_STAGING = YES
 
-POLKIT_DEPENDENCIES = libglib2 host-intltool expat
+POLKIT_DEPENDENCIES = libglib2 host-intltool expat spidermonkey
 
 # We could also support --with-authfw=pam
 POLKIT_CONF_OPTS = \
 	--with-authfw=shadow \
 	--with-os-type=unknown \
 	--disable-man-pages \
-	--disable-examples
+	--disable-examples \
+	--disable-libelogind \
+	--disable-libsystemd-login
 
 $(eval $(autotools-package))
