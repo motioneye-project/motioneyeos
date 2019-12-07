45b0ed05ab135268caeec33e136843134f2cb19f
Francois Perrad <fperrad@gmail.com>
2019-12-07 08:45:40 +0100

    package/lua-resty-http: bump to version 0.15
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/lua-resty-http/lua-resty-http.mk
+++ package/lua-resty-http/lua-resty-http.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUA_RESTY_HTTP_VERSION = 0.13-0
+LUA_RESTY_HTTP_VERSION = 0.15-0
 LUA_RESTY_HTTP_SUBDIR = lua-resty-http
 LUA_RESTY_HTTP_LICENSE = BSD-2-Clause
 LUA_RESTY_HTTP_LICENSE_FILES = $(LUA_RESTY_HTTP_SUBDIR)/LICENSE
