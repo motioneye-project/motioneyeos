980476fdae654e79670a44b58d3e79418bd9a6f5
Paul Cercueil <paul@crapouillou.net>
2019-12-07 01:06:05 +0100

    package/apitrace: bump to version 9.0
    Bump to upstream version 9.0, released 2019-11-26.
    
    This may have been the case already in 8.0, but it is possible to
    compile without X11 by just specifying ENABLE_X11=OFF, so the package
    was updated to remove the X11 dependency.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/apitrace/apitrace.mk
+++ package/apitrace/apitrace.mk
@@ -4,12 +4,19 @@
 #
 ################################################################################
 
-APITRACE_VERSION = 8.0
+APITRACE_VERSION = 9.0
 APITRACE_SITE = $(call github,apitrace,apitrace,$(APITRACE_VERSION))
 APITRACE_LICENSE = MIT
 APITRACE_LICENSE_FILES = LICENSE
 
-APITRACE_DEPENDENCIES = xlib_libX11 host-python libpng
+APITRACE_DEPENDENCIES = host-python libpng
+
+ifeq ($(BR2_PACKAGE_XORG7),y)
+APITRACE_DEPENDENCIES += xlib_libX11
+APITRACE_CONF_OPTS += -DENABLE_X11=ON
+else
+APITRACE_CONF_OPTS += -DENABLE_X11=OFF
+endif
 
 # Gui was never tested, so we prefer to explicitly disable it
 APITRACE_CONF_OPTS += -DENABLE_GUI=false
