bdc395db0d769ce1fd23dbddadd5c9e383c591ac
Christian Stewart <christian@paral.in>
2019-12-07 11:49:38 -0800

    package/go: bump to 1.13.5
    go1.13.5 (released 2019/12/04) includes fixes to the go command, the runtime,
    the linker, and the net/http package.
    
    https://github.com/golang/go/issues?q=milestone%3AGo1.13.5
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.13.4
+GO_VERSION = 1.13.5
 GO_SITE = https://storage.googleapis.com/golang
 GO_SOURCE = go$(GO_VERSION).src.tar.gz
 
