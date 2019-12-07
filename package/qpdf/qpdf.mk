984bdfb0276e313c770061c7793fa8b30f34f04a
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-12-07 23:29:14 +0100

    package/qpdf: fix license
    qpdf is also licensed under Apache-2.0 since version 7.0.0 and
    https://github.com/qpdf/qpdf/commit/07c8bb2843f1a975a27d396da33d813a1537ee3a
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/qpdf/qpdf.mk
+++ package/qpdf/qpdf.mk
@@ -7,8 +7,8 @@
 QPDF_VERSION = 8.4.0
 QPDF_SITE = http://downloads.sourceforge.net/project/qpdf/qpdf/$(QPDF_VERSION)
 QPDF_INSTALL_STAGING = YES
-QPDF_LICENSE = Artistic-2.0
-QPDF_LICENSE_FILES = Artistic-2.0
+QPDF_LICENSE = Apache-2.0 or Artistic-2.0
+QPDF_LICENSE_FILES = LICENSE.txt Artistic-2.0
 QPDF_DEPENDENCIES = pcre zlib jpeg
 
 QPDF_CONF_OPTS = --without-random
