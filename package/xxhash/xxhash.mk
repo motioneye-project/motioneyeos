38ceccb5796cd34b430eb978a6cec56427aad0a2
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-12-07 20:39:30 +0100

    package/xxhash: bump to version 0.7.2
    - Remove patch (already in version)
    - Update hash of LICENSE file and remove xxhsum.c from
      XXHASH_LICENSE_FILES as LICENSE contains both licenses
      (BSD-2-Clause and GPL-2.0+) since
      https://github.com/Cyan4973/xxHash/commit/330444389b5091db1fae1cc58c890b9f99b8b35d
    - Update XXHASH_LICENSE to specify that BSD-2-Clause is for the library
      and GPL-2.0+ for xxhsum CLI
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/xxhash/xxhash.mk
+++ package/xxhash/xxhash.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-XXHASH_VERSION = 0.7.1
+XXHASH_VERSION = 0.7.2
 XXHASH_SITE = $(call github,Cyan4973,xxHash,v$(XXHASH_VERSION))
-XXHASH_LICENSE = BSD-2-Clause, GPL-2.0+
-XXHASH_LICENSE_FILES = LICENSE xxhsum.c
+XXHASH_LICENSE = BSD-2-Clause (library), GPL-2.0+ (xxhsum)
+XXHASH_LICENSE_FILES = LICENSE
 
 define XXHASH_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) xxhsum
