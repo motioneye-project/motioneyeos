b1bef9e173f334fda01c5cfa1f2dfabe1480af3b
Gilles Talis <gilles.talis@gmail.com>
2019-12-08 09:05:52 +0100

    package/xapian: bump to version 1.4.14
    Signed-off-by: Gilles Talis <gilles.talis@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/xapian/xapian.mk
+++ package/xapian/xapian.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPIAN_VERSION = 1.4.13
+XAPIAN_VERSION = 1.4.14
 XAPIAN_SOURCE = xapian-core-$(XAPIAN_VERSION).tar.xz
 XAPIAN_SITE = https://oligarchy.co.uk/xapian/$(XAPIAN_VERSION)
 XAPIAN_LICENSE = GPL-2.0+
