84a7e647aecad74a00af75637bc11181bce467bf
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-12-08 09:51:43 +0100

    package/qpdf: remove pcre dependency
    pcre is not needed since version 7.0.0 and
    https://github.com/qpdf/qpdf/commit/9a96e233b051b31289c84f90a321583887b1400a
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/qpdf/qpdf.mk
+++ package/qpdf/qpdf.mk
@@ -9,7 +9,7 @@ QPDF_SITE = http://downloads.sourceforge.net/project/qpdf/qpdf/$(QPDF_VERSION)
 QPDF_INSTALL_STAGING = YES
 QPDF_LICENSE = Apache-2.0 or Artistic-2.0
 QPDF_LICENSE_FILES = LICENSE.txt Artistic-2.0
-QPDF_DEPENDENCIES = pcre zlib jpeg
+QPDF_DEPENDENCIES = zlib jpeg
 
 QPDF_CONF_OPTS = --without-random
 
