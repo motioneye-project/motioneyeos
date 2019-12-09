53f7f7e24d8ce810c7e9051415a1abce139fa142
Peter Seiderer <ps.report@gmx.net>
2019-12-09 21:04:52 +0100

    gstreamer1-editing-services: bump version to 1.16.2
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gstreamer1/gstreamer1-editing-services/gstreamer1-editing-services.mk
+++ package/gstreamer1/gstreamer1-editing-services/gstreamer1-editing-services.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GSTREAMER1_EDITING_SERVICES_VERSION = 1.16.1
+GSTREAMER1_EDITING_SERVICES_VERSION = 1.16.2
 GSTREAMER1_EDITING_SERVICES_SOURCE = gstreamer-editing-services-$(GSTREAMER1_EDITING_SERVICES_VERSION).tar.xz
 GSTREAMER1_EDITING_SERVICES_SITE = http://gstreamer.freedesktop.org/src/gstreamer-editing-services
 GSTREAMER1_EDITING_SERVICES_LICENSE = LGPL-2.0+
