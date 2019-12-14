57d3eca52b1fc63e9c247496754f1d19311880f6
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-12-15 00:26:01 +0100

    package/tpm2-abrmd: bump to version 2.3.0
    Drop dbus dependency as it is not needed since version 2.2.0 and
    https://github.com/tpm2-software/tpm2-abrmd/commit/c1d42c9ebefbfe36255603aca065944836c14610
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    [Peter: use --disable-defaultflags and explicitly pass -std=c99]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tpm2-abrmd/tpm2-abrmd.mk
+++ package/tpm2-abrmd/tpm2-abrmd.mk
@@ -4,24 +4,19 @@
 #
 ################################################################################
 
-TPM2_ABRMD_VERSION = 2.0.3
+TPM2_ABRMD_VERSION = 2.3.0
 TPM2_ABRMD_SITE = https://github.com/tpm2-software/tpm2-abrmd/releases/download/$(TPM2_ABRMD_VERSION)
 TPM2_ABRMD_LICENSE = BSD-2-Clause
 TPM2_ABRMD_LICENSE_FILES = LICENSE
 TPM2_ABRMD_INSTALL_STAGING = YES
-TPM2_ABRMD_DEPENDENCIES = dbus libglib2 tpm2-tss host-pkgconf
-
-# -fstack-protector-all and FORTIFY_SOURCE=2 is used by
-# default. Disable that so the BR2_SSP_* / BR2_FORTIFY_SOURCE_* options
-# in the toolchain wrapper and CFLAGS are used instead
-TPM2_ABRMD_CONF_ENV = \
-	ax_cv_check_cflags___________Werror_______fstack_protector_all=no \
-	ax_cv_check_ccppflags___________Werror_______U_FORTIFY_SOURCE=no \
-	ax_cv_check_ccppflags___________Werror_______D_FORTIFY_SOURCE_2=no
-
-TPM2_ABRMD_CONF_OPTS += \
+TPM2_ABRMD_DEPENDENCIES = libglib2 tpm2-tss host-pkgconf
+TPM2_ABRMD_CONF_OPTS = \
+	--disable-defaultflags \
 	--with-systemdsystemunitdir=$(if $(BR2_INIT_SYSTEMD),/usr/lib/systemd/system,no) \
 	--with-udevrulesdir=$(if $(BR2_PACKAGE_HAS_UDEV),/usr/lib/udev/rules.d,no)
+
+# uses C99 code but forgets to pass -std=c99 when --disable-defaultflags is used
+TPM2_ABRMD_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -std=c99"
 
 define TPM2_ABRMD_INSTALL_INIT_SYSTEMD
 	$(TARGET_MAKE_ENV) $(MAKE1) -C $(@D) DESTDIR=$(TARGET_DIR) \
