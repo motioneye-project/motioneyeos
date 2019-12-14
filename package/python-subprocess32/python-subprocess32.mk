55e9290603b21e3b88085cba2349eb8af6b7535b
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-12-14 18:10:23 +0100

    package/python-subprocess32: add PYTHON_SUBPROCESS32_CONFIGURE_CMDS
    Since commit 1745fcde740057951dcc5429f3bfabd103b764a1,
    python-subprocess32 fails to build because it runs configure with
    incorrect arguments so add a PYTHON_SUBPROCESS32_CONFIGURE_CMDS
    
    Fixes:
     - http://autobuild.buildroot.org/results/dcf944129392ee6cacc106e096d8d3adfa4447bb
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-subprocess32/python-subprocess32.mk
+++ package/python-subprocess32/python-subprocess32.mk
@@ -11,4 +11,21 @@ PYTHON_SUBPROCESS32_SETUP_TYPE = setuptools
 PYTHON_SUBPROCESS32_LICENSE = Python-2.0
 PYTHON_SUBPROCESS32_LICENSE_FILES = LICENSE
 
+# The configure step needs to be run outside of the setup.py since it isn't
+# run correctly for cross-compiling
+define PYTHON_SUBPROCESS32_CONFIGURE_CMDS
+	(cd $(@D) && \
+	$(TARGET_CONFIGURE_OPTS) \
+	$(TARGET_CONFIGURE_ARGS) \
+	./configure \
+		--target=$(GNU_TARGET_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--build=$(GNU_HOST_NAME) \
+		--prefix=/usr \
+		--exec-prefix=/usr \
+		--sysconfdir=/etc \
+		--program-prefix="" \
+	)
+endef
+
 $(eval $(python-package))
