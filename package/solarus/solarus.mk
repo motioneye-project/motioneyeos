f83c9d97ddb8299c05c1e812cf14233dfe3151df
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-12-15 14:13:57 +0100

    package/solarus: allow building with lua 5.1
    Build with lua 5.1 has been fixed since version 1.6.1 and
    https://gitlab.com/solarus-games/solarus/commit/611f81a90d42692e986c78c67090541ffd1c5973
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    [Thomas: keep a select BR2_PACKAGE_LUAJIT, but make it "if
    !BR2_PACKAGE_LUA_5_1"]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/solarus/solarus.mk
+++ package/solarus/solarus.mk
@@ -16,10 +16,18 @@ SOLARUS_LICENSE_FILES = license.txt
 # Install libsolarus.so
 SOLARUS_INSTALL_STAGING = YES
 
-SOLARUS_DEPENDENCIES = libgl libmodplug libogg libvorbis luajit openal physfs sdl2 \
+SOLARUS_DEPENDENCIES = libgl libmodplug libogg libvorbis openal physfs sdl2 \
 	sdl2_image sdl2_ttf
 
 # Disable launcher GUI (requires Qt5)
 SOLARUS_CONF_OPTS = -DSOLARUS_GUI=OFF
+
+ifeq ($(BR2_PACKAGE_LUAJIT),y)
+SOLARUS_CONF_OPTS += -DSOLARUS_USE_LUAJIT=ON
+SOLARUS_DEPENDENCIES += luajit
+else
+SOLARUS_CONF_OPTS += -DSOLARUS_USE_LUAJIT=OFF
+SOLARUS_DEPENDENCIES += lua
+endif
 
 $(eval $(cmake-package))
