eb075916f411915eefa056385862b7d6a793005c
Jérémy Rosen <jeremy.rosen@smile.fr>
2019-12-16 11:30:47 +0100

    package/network-manager: adapt to systemctl preset-all
    NM has three services
    * NetworkManger.service : the main daemon
    * NetworkManger-dispatcher.service : a daemon handling network callbacks
    * NetworkManager-wait-online.service : sync of other services with network-online
    
    Only the first two were activated. We now also enable
    wait-online. Not enabling it was probably a bug.
    
    Note that buildroot adds an alias dbus-org.freedesktop.NetworkManager
    Adding an alias that is not known upstream is not clean,
    but I left it for backward compatibility
    
    Signed-off-by: Jérémy Rosen <jeremy.rosen@smile.fr>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/network-manager/network-manager.mk
+++ package/network-manager/network-manager.mk
@@ -88,16 +88,9 @@ define NETWORK_MANAGER_INSTALL_INIT_SYSV
 endef
 
 define NETWORK_MANAGER_INSTALL_INIT_SYSTEMD
-	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
-
 	ln -sf /usr/lib/systemd/system/NetworkManager.service \
 		$(TARGET_DIR)/etc/systemd/system/dbus-org.freedesktop.NetworkManager.service
 
-	ln -sf /usr/lib/systemd/system/NetworkManager.service \
-		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/NetworkManager.service
-
-	ln -sf /usr/lib/systemd/system/NetworkManager-dispatcher.service \
-		$(TARGET_DIR)/etc/systemd/system/dbus-org.freedesktop.nm-dispatcher.service
 endef
 
 $(eval $(autotools-package))
