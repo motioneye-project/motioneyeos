c561bcbf5f752d258dc08ec8e3d8160f894d592a
Jérémy Rosen <jeremy.rosen@smile.fr>
2019-12-16 11:30:46 +0100

    package/linuxptp adapt to systemctl preset-all
    linuxptp has two services
    * linuxptp.service : the actual daemon
    * linuxptp-system-clock.service : sync the linux clock to the phc clock
    
    The first was enabled, the second was not, we now enable both
    
    The second has an incorrect (though harmless) WantedBy : there is an
    explicit Wants= in linuxptp.service
    
    Signed-off-by: Jérémy Rosen <jeremy.rosen@smile.fr>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/linuxptp/linuxptp.mk
+++ package/linuxptp/linuxptp.mk
@@ -42,9 +42,6 @@ define LINUXPTP_INSTALL_INIT_SYSTEMD
 		$(TARGET_DIR)/usr/lib/systemd/system/linuxptp.service
 	$(INSTALL) -D -m 644 $(LINUXPTP_PKGDIR)/linuxptp-system-clock.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/linuxptp-system-clock.service
-	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
-	ln -sf ../../../../usr/lib/systemd/system/linuxptp.service \
-		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/linuxptp.service
 endef
 
 $(eval $(generic-package))
