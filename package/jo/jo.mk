59eb1ce7189dfb27f35d1fc803d75f5a7297fd60
Peter Korsgaard <peter@korsgaard.com>
2019-12-16 09:11:05 +0100

    package/jo: bump version to 1.3
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/jo/jo.mk
+++ package/jo/jo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-JO_VERSION = 1.2
+JO_VERSION = 1.3
 JO_SITE = https://github.com/jpmens/jo/releases/download/$(JO_VERSION)
 JO_LICENSE = MIT (json.[ch]), GPL-2.0+ (rest)
 JO_LICENSE_FILES = COPYING
