ac92146ebb7627378c0a8c92f6490e55333eb6bb
James Hilliard <james.hilliard1@gmail.com>
2019-12-18 06:04:59 -0700

    package/pipewire: add missing dependencies/plugins
    Added missing required and optional dependencies.
    Provide option to enable gstreamer plugin.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/9671ad63b1ae147a6f660337cb4b3dc38209067c/
      http://autobuild.buildroot.net/results/0e80841fe9937d7a5142035c2a41158166bcf8df/
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    [Thomas:
     - enclose new option in if ... endif block
     - add missing Config.in comment
     - add dependency on gstreamer1 in .mk file]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/pipewire/pipewire.mk
+++ package/pipewire/pipewire.mk
@@ -9,8 +9,26 @@ PIPEWIRE_SITE = $(call github,PipeWire,pipewire,$(PIPEWIRE_VERSION))
 PIPEWIRE_LICENSE = LGPL-2.1+
 PIPEWIRE_LICENSE_FILES = LICENSE LGPL
 PIPEWIRE_INSTALL_STAGING = YES
-PIPEWIRE_DEPENDENCIES = host-pkgconf dbus
-PIPEWIRE_CONF_OPTS = -Dgstreamer=disabled
+PIPEWIRE_DEPENDENCIES = host-pkgconf alsa-lib dbus udev
+
+ifeq ($(BR2_PACKAGE_FFMPEG),y)
+PIPEWIRE_DEPENDENCIES += ffmpeg
+endif
+
+ifeq ($(BR2_PACKAGE_SBC),y)
+PIPEWIRE_DEPENDENCIES += sbc
+endif
+
+ifeq ($(BR2_PACKAGE_SDL2),y)
+PIPEWIRE_DEPENDENCIES += sdl2
+endif
+
+ifeq ($(BR2_PACKAGE_PIPEWIRE_GSTREAMER),y)
+PIPEWIRE_CONF_OPTS += -Dgstreamer=enabled
+PIPEWIRE_DEPENDENCIES += libglib2 gstreamer1 gst1-plugins-base
+else
+PIPEWIRE_CONF_OPTS += -Dgstreamer=disabled
+endif
 
 ifeq ($(BR2_PACKAGE_SYSTEMD),y)
 PIPEWIRE_CONF_OPTS += -Dsystemd=true
