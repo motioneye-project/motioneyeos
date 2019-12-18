0db459fe3dd3628a54e9aa6253c8e2f61e7f554b
Asaf Kahlon <asafka7@gmail.com>
2019-12-18 19:42:59 +0200

    package/python-yieldfrom: bump to version 1.0.5
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-yieldfrom/python-yieldfrom.mk
+++ package/python-yieldfrom/python-yieldfrom.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_YIELDFROM_VERSION = 1.0.4
+PYTHON_YIELDFROM_VERSION = 1.0.5
 PYTHON_YIELDFROM_SOURCE = yieldfrom-$(PYTHON_YIELDFROM_VERSION).tar.gz
-PYTHON_YIELDFROM_SITE = https://files.pythonhosted.org/packages/6e/d2/dc48e4cc6bbe48afd022da745f981cf0437ab7e658de9078bf56747b1d73
+PYTHON_YIELDFROM_SITE = https://files.pythonhosted.org/packages/4d/f9/395917f574ace618eb234bcbae8df3fabaa9624532d96d1fbd3a20678b1e
 PYTHON_YIELDFROM_SETUP_TYPE = setuptools
 PYTHON_YIELDFROM_LICENSE = MIT
 PYTHON_YIELDFROM_LICENSE_FILES = LICENSE
