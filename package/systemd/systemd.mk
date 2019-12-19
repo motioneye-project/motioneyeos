9bdeb0b3444fbe521da9d4283714e281584f4fb8
Jérémy Rosen <jeremy.rosen@enst-bretagne.fr>
2019-12-19 16:21:34 +0100

    package/systemd: bump to 244.1
    Signed-off-by: Jérémy Rosen <jeremy.rosen@smile.fr>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/systemd/systemd.mk
+++ package/systemd/systemd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SYSTEMD_VERSION = 244
+SYSTEMD_VERSION = 244.1
 SYSTEMD_SITE = $(call github,systemd,systemd-stable,v$(SYSTEMD_VERSION))
 SYSTEMD_LICENSE = LGPL-2.1+, GPL-2.0+ (udev), Public Domain (few source files, see README), BSD-3-Clause (tools/chromiumos)
 SYSTEMD_LICENSE_FILES = LICENSE.GPL2 LICENSE.LGPL2.1 README tools/chromiumos/LICENSE
