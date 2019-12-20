9f4f5a5434899f8c56545ff8b6ef70c2bbaebafd
Heiko Thiery <heiko.thiery@gmail.com>
2019-12-20 14:53:44 +0100

    package/libnetconf2: bump to version 1.1.3
    Other changes:
    
     - Remove patch (already in upstream version)
    
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libnetconf2/libnetconf2.mk
+++ package/libnetconf2/libnetconf2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBNETCONF2_VERSION = 0.12-r2
+LIBNETCONF2_VERSION = 1.1.3
 LIBNETCONF2_SITE = $(call github,CESNET,libnetconf2,v$(LIBNETCONF2_VERSION))
 LIBNETCONF2_INSTALL_STAGING = YES
 LIBNETCONF2_LICENSE = BSD-3-Clause
