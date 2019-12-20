0ea753f8d3fbbb88629b82c1befbd1fc224b59bf
Peter Korsgaard <peter@korsgaard.com>
2019-12-20 09:19:17 +0100

    package/python-django: security bump to version 3.0.1
    Fixes the following security vulnerability:
    
    - CVE-2019-19844: Potential account hijack via password reset form
      By submitting a suitably crafted email address making use of Unicode
      characters, that compared equal to an existing user email when lower-cased
      for comparison, an attacker could be sent a password reset token for the
      matched account
    
    In addition, a number of bugs have been fixed.  For details, see the release
    notes:
    https://docs.djangoproject.com/en/dev/releases/3.0.1/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 3.0
+PYTHON_DJANGO_VERSION = 3.0.1
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/f8/46/b3b8c61f867827fff2305db40659495dcd64fb35c399e75c53f23c113871
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/44/e8/4ae9ef3d455f4ce5aa22259cb6e40c69b29ef6b02d49c5cdfa265f7fc821
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_SETUP_TYPE = setuptools
