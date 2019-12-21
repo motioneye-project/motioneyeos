ffc208144775c2bd022d14a3278ef91a3bd1ba1d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-12-21 17:17:18 +0100

    package/libvdpau: bump to version 1.3
    - Switch to meson-package
    - Drop second patch (not needed anymore)
    - Drop third patch (after feedback from upstream:
      https://gitlab.freedesktop.org/vdpau/libvdpau/merge_requests/5)
    - Disable documentation
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libvdpau/libvdpau.mk
+++ package/libvdpau/libvdpau.mk
@@ -4,25 +4,25 @@
 #
 ################################################################################
 
-LIBVDPAU_VERSION = 1.2
+LIBVDPAU_VERSION = 1.3
 LIBVDPAU_SOURCE = libvdpau-$(LIBVDPAU_VERSION).tar.bz2
-LIBVDPAU_SITE = http://people.freedesktop.org/~aplattner/vdpau
+LIBVDPAU_SITE = \
+	https://gitlab.freedesktop.org/vdpau/libvdpau/-/archive/$(LIBVDPAU_VERSION)
 LIBVDPAU_LICENSE = MIT
 LIBVDPAU_LICENSE_FILES = COPYING
 LIBVDPAU_INSTALL_STAGING = YES
 
-# autoreconf for patch 0002-link-with-libx11.patch
-LIBVDPAU_AUTORECONF = YES
-
 LIBVDPAU_DEPENDENCIES = host-pkgconf xlib_libX11 xlib_libXext
 
-LIBVDPAU_CONF_OPTS = --with-module-dir=/usr/lib/vdpau
+LIBVDPAU_CONF_OPTS = \
+	-Ddocumentation=false \
+	-Dmoduledir=/usr/lib/vdpau
 
 ifeq ($(BR2_PACKAGE_XORGPROTO),y)
 LIBVDPAU_DEPENDENCIES += xorgproto
-LIBVDPAU_CONF_OPTS += --enable-dri2
+LIBVDPAU_CONF_OPTS += -Ddri2=true
 else
-LIBVDPAU_CONF_OPTS += --disable-dri2
+LIBVDPAU_CONF_OPTS += -Ddri2=false
 endif
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
