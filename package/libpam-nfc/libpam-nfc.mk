c3077766252e0f54ae63fef3b22a57588e6053cb
Louis Aussedat <aussedat.louis@gmail.com>
2019-12-22 13:05:00 +0100

    package/libpam-nfc: new package
    Signed-off-by: Louis Aussedat <aussedat.louis@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

---
+++ package/libpam-nfc/libpam-nfc.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# libpam-nfc
+#
+################################################################################
+
+LIBPAM_NFC_VERSION = bb762e0e649195110e015ffb605c4375e927c437
+LIBPAM_NFC_SITE = $(call github,nfc-tools,pam_nfc,$(LIBPAM_NFC_VERSION))
+LIBPAM_NFC_LICENSE = GPL-2.0
+LIBPAM_NFC_LICENSE_FILES = COPYING
+LIBPAM_NFC_DEPENDENCIES = linux-pam libnfc
+# Fetching from github, we need to generate the configure script
+LIBPAM_NFC_AUTORECONF = YES
+LIBPAM_NFC_INSTALL_STAGING = YES
+
+LIBPAM_NFC_CONF_OPTS = \
+	--with-pam-dir=/lib/security
+
+# libpam breaks with parallel build, but is very fast to build.
+LIBPAM_NFC_MAKE = $(MAKE1)
+
+# Autoreconf requires an existing m4 directory
+define LIBPAM_NFC_MKDIR_M4
+	mkdir -p $(@D)/m4
+endef
+LIBPAM_NFC_PRE_CONFIGURE_HOOKS += LIBPAM_NFC_MKDIR_M4
+
+$(eval $(autotools-package))
