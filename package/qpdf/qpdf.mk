004848dae7ea914d906d40a6cd3fd6895902a487
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-12-22 11:49:28 +0100

    package/qpdf: bump to version 9.1.0
    - Add host-pkgconf dependency as it is used since version 9.0.0 and
      https://github.com/qpdf/qpdf/commit/12400475283f5081ea55f52a764e43f14032f6ba
    - C++11 is mandatory since version 9.1.0 and
      https://github.com/qpdf/qpdf/commit/653ce3550d18a07194a4c3bb9b4a3d260ed842cd
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/qpdf/qpdf.mk
+++ package/qpdf/qpdf.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-QPDF_VERSION = 8.4.0
+QPDF_VERSION = 9.1.0
 QPDF_SITE = http://downloads.sourceforge.net/project/qpdf/qpdf/$(QPDF_VERSION)
 QPDF_INSTALL_STAGING = YES
 QPDF_LICENSE = Apache-2.0 or Artistic-2.0
 QPDF_LICENSE_FILES = LICENSE.txt Artistic-2.0
-QPDF_DEPENDENCIES = zlib jpeg
+QPDF_DEPENDENCIES = host-pkgconf zlib jpeg
 
 QPDF_CONF_OPTS = --without-random
 
