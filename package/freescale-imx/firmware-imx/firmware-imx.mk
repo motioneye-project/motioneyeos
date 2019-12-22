motioneye-project/motioneyeos

commit 3ec818769d5140f337684d8c35497ca940bffa34

package/freescale-imx/firmware-imx: bump version to 8.1.1
This version is aligned with i.MX NXP BSP components version
rel_imx_4.14.98_2.0.0_ga

Signed-off-by: Julien Olivain <juju@cotds.org>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/freescale-imx/firmware-imx/firmware-imx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FIRMWARE_IMX_VERSION = 7.5
+FIRMWARE_IMX_VERSION = 8.1.1
 FIRMWARE_IMX_SITE = $(FREESCALE_IMX_SITE)
 FIRMWARE_IMX_SOURCE = firmware-imx-$(FIRMWARE_IMX_VERSION).bin
 

package/freescale-imx/firmware-imx/firmware-imx.hash
@@ -1,4 +1,4 @@
 # Locally calculated
-sha256 a8f099bdf786b2da1e8b43094950c033ccdbf93f1b8a93caffb912e1500cd735  firmware-imx-7.5.bin
+sha256 c115d5d0d115fc14754671bd2428f672f9f99677988588accef2fbca2c3f749c  firmware-imx-8.1.1.bin
 sha256 fc93f26e4197b9240c327ce26ae6e02123f022423d4704c7b0448a37372cf2a2  EULA
 sha256 78696664f7afe7f2b081c99af60082d6403fab935b1b10056ed41aec123ff7fa  COPYING

commit 571b8ca09563ffa97a1d0df2fa076530426ab8da

package/freescale-imx/firmware-imx: fix the ahab-container name
After the firmware-imx bump to version 8.1.1 the following
error is observed when building freescale_imx8qxpmek_defconfig:

>> firmware-imx 8.1.1 Installing to images directory
cp /home/fabio/buildroot/output/build/firmware-imx-8.1.1/firmware/seco/ahab-container.img
/home/fabio/buildroot/output/images/ahab-container.img
cp: cannot stat
'/home/fabio/buildroot/output/build/firmware-imx-8.1.1/firmware/seco/ahab-container.img':
No such file or directory

In the firmware-imx-8.1.1 (and newer) the i.MX8QXP container image was
renamed to mx8qx-ahab-container.img, so fix it accordingly.

Fixes: 3ec818769d51 ("package/freescale-imx/firmware-imx: bump version to 8.1.1")
Signed-off-by: Fabio Estevam <festevam@gmail.com>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

package/freescale-imx/firmware-imx/firmware-imx.mk
@@ -50,7 +50,7 @@ FIRMWARE_IMX_INSTALL_IMAGES = YES
 define FIRMWARE_IMX_INSTALL_IMAGES_CMDS
 	# SECO firmware is needed when generating imx8-boot-sd.bin which
 	# is done in post-image script.
-	cp $(@D)/firmware/seco/ahab-container.img \
+	cp $(@D)/firmware/seco/mx8qx-ahab-container.img \
 		$(BINARIES_DIR)/ahab-container.img
 endef
 else