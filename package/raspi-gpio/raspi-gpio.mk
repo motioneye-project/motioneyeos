d04d9a3ee36da8b86782c343db016054b873cc82
Pierre-Jean Texier <pjtexier@koncepto.io>
2019-12-22 22:13:12 +0100

    package/raspi-gpio: bump to revision 4edfde1
    This includes the following changes:
    
    4edfde1 Update raw output to show pull registers on 2711
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/raspi-gpio/raspi-gpio.mk
+++ package/raspi-gpio/raspi-gpio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RASPI_GPIO_VERSION = 2eaa8b8755a550e34d07c898b90b0d9b3d66fd81
+RASPI_GPIO_VERSION = 4edfde183ff3ac9ed66cdc015ae25e45f3a5502d
 RASPI_GPIO_SITE = $(call github,RPi-Distro,raspi-gpio,$(RASPI_GPIO_VERSION))
 RASPI_GPIO_LICENSE = BSD-3-Clause
 RASPI_GPIO_LICENSE_FILES = LICENSE
