e103e47b8a2041ec5cc31dc3a5fdc7d25ea93db0
Carlos Santos <unixmania@gmail.com>
2019-12-23 08:13:21 -0300

    package/tpm2-tss: fix build without C++
    C++ is required only for the fuzzing tests but AC_PROG_CXX is included
    by configure.ac even when fuzzing is not enabled (which we don't do on
    Buildroot).
    
    The patch applied upstream had issues and was reverted[1]. Use a local
    patch to solve the problem temporaryly.
    
    Fixes:
        http://autobuild.buildroot.net/results/13f5e37b47b255da4158bec34e5459136f7e60d4
        http://autobuild.buildroot.net/results/1c26db2509c79e00c0de1165945277eaa57b149f
        http://autobuild.buildroot.net/results/b7b6b7b7aca79e847b442cbd2305427d91fe5d70
        http://autobuild.buildroot.net/results/1cd5a82a0e799aa5027e2e2c03b246332cc3a15d
        http://autobuild.buildroot.net/results/d7ec878907f714377c83e9a496e97cbf9382d787
        http://autobuild.buildroot.net/results/1c7f0c1b3ce4871cd87bd6059b1f0a6dc4e74a9c
        http://autobuild.buildroot.net/results/196b81d580325607c8da90beeb79e1f6b8ab8b47
        http://autobuild.buildroot.net/results/f90f7b4ac710b56686635f8ae27059c11b963e47
    
    1. https://github.com/tpm2-software/tpm2-tss/commit/60c26e4c4faba6ba12469485653e17092b510840
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/tpm2-tss/tpm2-tss.mk
+++ package/tpm2-tss/tpm2-tss.mk
@@ -11,6 +11,8 @@ TPM2_TSS_LICENSE_FILES = LICENSE
 TPM2_TSS_INSTALL_STAGING = YES
 TPM2_TSS_DEPENDENCIES = liburiparser openssl host-pkgconf
 TPM2_TSS_CONF_OPTS = --with-crypto=ossl --disable-doxygen-doc --disable-defaultflags
+# 0001-configure-Only-use-CXX-when-fuzzing.patch
+TPM2_TSS_AUTORECONF = YES
 
 # uses C99 code but forgets to pass -std=c99 when --disable-defaultflags is used
 TPM2_TSS_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -std=c99"
