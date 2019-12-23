9a6448a859d9b6b55ee250dfcd84903443633f33
Titouan Christophe <titouan.christophe@railnova.eu>
2019-12-23 18:23:57 +0100

    package/python-avro: new package
    Signed-off-by: Titouan Christophe <titouan.christophe@railnova.eu>
    Reviewed-by: Gilles Talis <gilles.talis@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

---
+++ package/python-avro/python-avro.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-avro
+#
+################################################################################
+
+PYTHON_AVRO_VERSION = $(AVRO_C_VERSION)
+PYTHON_AVRO_SITE = https://www-eu.apache.org/dist/avro/avro-1.9.1/py3
+PYTHON_AVRO_SOURCE = avro-python3-$(PYTHON_AVRO_VERSION).tar.gz
+PYTHON_AVRO_LICENSE = Apache-2.0
+PYTHON_AVRO_LICENSE_FILES = avro/LICENSE
+PYTHON_AVRO_SETUP_TYPE = setuptools
+
+$(eval $(python-package))
