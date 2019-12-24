7d6ecbd81692dd42c4109c92eb92baa0be153683
Benjamin Kamath <kamath.ben@gmail.com>
2019-12-24 00:55:31 -0800

    package/lapack: bump to version 3.9.0
    Most recent release hosted on Github instead of previous location
    
    Signed-off-by: Benjamin Kamath <kamath.ben@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/lapack/lapack.mk
+++ package/lapack/lapack.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-LAPACK_VERSION = 3.8.0
+LAPACK_VERSION = 3.9.0
 LAPACK_LICENSE = BSD-3-Clause
 LAPACK_LICENSE_FILES = LICENSE
-LAPACK_SITE = http://www.netlib.org/lapack
+LAPACK_SITE = $(call github,Reference-LAPACK,lapack,v$(LAPACK_VERSION))
 LAPACK_INSTALL_STAGING = YES
 LAPACK_SUPPORTS_IN_SOURCE_BUILD = NO
 LAPACK_CONF_OPTS = -DLAPACKE=ON -DCBLAS=ON
