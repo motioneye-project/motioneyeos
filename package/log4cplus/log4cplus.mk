335df9d983b401b8579781ab7d519d6e2f577b51
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-12-24 10:25:14 +0100

    package/log4cplus: bump to version 2.0.5
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/log4cplus/log4cplus.mk
+++ package/log4cplus/log4cplus.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LOG4CPLUS_VERSION = 2.0.4
+LOG4CPLUS_VERSION = 2.0.5
 LOG4CPLUS_SOURCE = log4cplus-$(LOG4CPLUS_VERSION).tar.xz
 LOG4CPLUS_SITE = http://downloads.sourceforge.net/project/log4cplus/log4cplus-stable/$(LOG4CPLUS_VERSION)
 LOG4CPLUS_LICENSE = Apache-2.0, BSD-2-Clause, BSD-like (threadpool)
