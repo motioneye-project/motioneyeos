90125a6fcb394c55cca6732efed1a30e0e23de9b
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-12-26 10:00:54 +0100

    package/gmrender-resurrect: bump to version 0.0.8
    libupnp18 is supported since
    https://github.com/hzeller/gmrender-resurrect/commit/cc96edeb70fa2938a4d963524261142132ece540
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/gmrender-resurrect/gmrender-resurrect.mk
+++ package/gmrender-resurrect/gmrender-resurrect.mk
@@ -4,13 +4,15 @@
 #
 ################################################################################
 
-GMRENDER_RESURRECT_VERSION = a7b0b1b9ca482d2d34ac62c2f2dc0cf0dfbb702b
-GMRENDER_RESURRECT_SITE = $(call github,hzeller,gmrender-resurrect,$(GMRENDER_RESURRECT_VERSION))
+GMRENDER_RESURRECT_VERSION = 0.0.8
+GMRENDER_RESURRECT_SITE = $(call github,hzeller,gmrender-resurrect,v$(GMRENDER_RESURRECT_VERSION))
 # Original distribution does not have default configure,
 # so we need to autoreconf:
 GMRENDER_RESURRECT_AUTORECONF = YES
 GMRENDER_RESURRECT_LICENSE = GPL-2.0+
 GMRENDER_RESURRECT_LICENSE_FILES = COPYING
-GMRENDER_RESURRECT_DEPENDENCIES = gstreamer1 libupnp
+GMRENDER_RESURRECT_DEPENDENCIES = \
+	gstreamer1 \
+	$(if $(BR2_PACKAGE_LIBUPNP),libupnp,libupnp18)
 
 $(eval $(autotools-package))
