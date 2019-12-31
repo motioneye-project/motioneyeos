826a601b44ba60402aacd378cb480305f705faa1
Ricardo Martincoski <ricardo.martincoski@gmail.com>
2019-12-31 00:11:50 -0300

    package/atop: bump to version 2.5.0
    ./utils/test-pkg --package atop --all
    44 builds, 5 skipped, 0 build failed, 0 legal-info failed
    
    Signed-off-by: Ricardo Martincoski <ricardo.martincoski@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/atop/atop.mk
+++ package/atop/atop.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ATOP_VERSION = 2.4.0
+ATOP_VERSION = 2.5.0
 ATOP_SITE = http://www.atoptool.nl/download
 ATOP_LICENSE = GPL-2.0+
 ATOP_LICENSE_FILES = COPYING
