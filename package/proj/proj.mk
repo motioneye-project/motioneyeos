f91c16d79f5ae4e3c5290dad9d047d628e4837eb
Zoltan Gyarmati <zgyarmati@zgyarmati.de>
2020-01-01 19:46:11 +0100

    package/proj: update to 6.2.1
    Signed-off-by: Zoltan Gyarmati <zgyarmati@zgyarmati.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/proj/proj.mk
+++ package/proj/proj.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PROJ_VERSION = 6.2.0
+PROJ_VERSION = 6.2.1
 PROJ_SITE = http://download.osgeo.org/proj
 PROJ_LICENSE = MIT
 PROJ_LICENSE_FILES = COPYING
