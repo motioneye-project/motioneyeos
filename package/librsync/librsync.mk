ca7d5675676cbc4d5e795db61181925cac37a972
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-01-01 18:53:16 +0100

    package/librsync: fix static link with popt
    Use pkg-config to retrieve popt dependencies such as iconv
    
    Fixes:
     - http://autobuild.buildroot.org/results/896e8e3efbedad90d66ae8c4e1e50f16206cab49
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/librsync/librsync.mk
+++ package/librsync/librsync.mk
@@ -9,6 +9,6 @@ LIBRSYNC_SITE = $(call github,librsync,librsync,v$(LIBRSYNC_VERSION))
 LIBRSYNC_LICENSE = LGPL-2.1+
 LIBRSYNC_LICENSE_FILES = COPYING
 LIBRSYNC_INSTALL_STAGING = YES
-LIBRSYNC_DEPENDENCIES = zlib bzip2 popt
+LIBRSYNC_DEPENDENCIES = host-pkgconf zlib bzip2 popt
 
 $(eval $(cmake-package))
