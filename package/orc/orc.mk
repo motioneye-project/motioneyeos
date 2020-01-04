d4d59af898c1fd017833497de93c1f7521cc3b77
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-01-04 19:15:32 +0100

    package/orc: bump to version 0.4.31
    Switch to meson buildsystem and so drop:
     - !BR2_BINFMT_FLAT dependency
     - hooks (not needed thanks to orc-test and tools options)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/orc/orc.mk
+++ package/orc/orc.mk
@@ -4,24 +4,20 @@
 #
 ################################################################################
 
-ORC_VERSION = 0.4.29
+ORC_VERSION = 0.4.31
 ORC_SOURCE = orc-$(ORC_VERSION).tar.xz
 ORC_SITE = http://gstreamer.freedesktop.org/data/src/orc
 ORC_LICENSE = BSD-2-Clause, BSD-3-Clause
 ORC_LICENSE_FILES = COPYING
 ORC_INSTALL_STAGING = YES
 ORC_DEPENDENCIES = host-orc
+ORC_CONF_OPTS = \
+	-Dbenchmarks=disabled \
+	-Dexamples=disabled \
+	-Dgtk_doc=disabled \
+	-Dorc-test=disabled \
+	-Dtests=disabled \
+	-Dtools=disabled
 
-define ORC_REMOVE_BUGREPORT
-	rm -f $(TARGET_DIR)/usr/bin/orc-bugreport
-endef
-
-define ORC_REMOVE_DEVFILES
-	rm -f $(TARGET_DIR)/usr/bin/orcc
-endef
-
-ORC_POST_INSTALL_TARGET_HOOKS += ORC_REMOVE_BUGREPORT
-ORC_POST_INSTALL_TARGET_HOOKS += ORC_REMOVE_DEVFILES
-
-$(eval $(autotools-package))
-$(eval $(host-autotools-package))
+$(eval $(meson-package))
+$(eval $(host-meson-package))
