5cf68c8f792078cb3bd079a28995008e1a7a395c
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-01-04 14:31:41 +0100

    package/freeswitch: bump version to 1.10.2
    Rebased patch 0001.
    
    Release notes:
    https://github.com/signalwire/freeswitch/releases/tag/v1.10.2
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/freeswitch/freeswitch.mk
+++ package/freeswitch/freeswitch.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FREESWITCH_VERSION = 1.10.1
+FREESWITCH_VERSION = 1.10.2
 FREESWITCH_SOURCE = freeswitch-$(FREESWITCH_VERSION).-release.tar.xz
 FREESWITCH_SITE = https://files.freeswitch.org/freeswitch-releases
 # External modules need headers/libs from staging
