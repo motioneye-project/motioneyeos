e1a6deef259413e8771334a6255ecc6d0caf2b56
Romain Naour <romain.naour@gmail.com>
2020-01-04 12:43:29 +0100

    toolchain/toolchain-external: update Arm ARM toolchain 9.2-2019.12
    Update to gcc 9.2.1, gdb 8.3.0, binutils 2.33.1.
    
    See "Release Note":
    https://developer.arm.com/open-source/gnu-toolchain/gnu-a/downloads#
    
    Tested with qemu_arm_vexpress_defconfig.
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- toolchain/toolchain-external/toolchain-external-arm-arm/toolchain-external-arm-arm.mk
+++ toolchain/toolchain-external/toolchain-external-arm-arm/toolchain-external-arm-arm.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION = 2019.03
-TOOLCHAIN_EXTERNAL_ARM_ARM_SITE = https://developer.arm.com/-/media/Files/downloads/gnu-a/8.3-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)/binrel
+TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION = 2019.12
+TOOLCHAIN_EXTERNAL_ARM_ARM_SITE = https://developer.arm.com/-/media/Files/downloads/gnu-a/9.2-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)/binrel
 
-TOOLCHAIN_EXTERNAL_ARM_ARM_SOURCE = gcc-arm-8.3-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)-x86_64-arm-linux-gnueabihf.tar.xz
+TOOLCHAIN_EXTERNAL_ARM_ARM_SOURCE = gcc-arm-9.2-$(TOOLCHAIN_EXTERNAL_ARM_ARM_VERSION)-x86_64-arm-none-linux-gnueabihf.tar.xz
 
 $(eval $(toolchain-external-package))
