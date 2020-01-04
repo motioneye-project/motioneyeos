f9a88bdc7589439f5a92f6a64945193f9f0e2d13
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-01-04 22:00:16 +0100

    package/x11r7/xlib_libXpm: bump version to 3.5.13
    Removed patch applied upstream:
    https://cgit.freedesktop.org/xorg/lib/libXpm/commit/?id=7af7c5e275b69daedee3696bee1e880586f30373
    
    Removed autoreconf.
    
    Added all hashes provided by upstream.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xlib_libXpm/xlib_libXpm.mk
+++ package/x11r7/xlib_libXpm/xlib_libXpm.mk
@@ -4,14 +4,12 @@
 #
 ################################################################################
 
-XLIB_LIBXPM_VERSION = 3.5.12
+XLIB_LIBXPM_VERSION = 3.5.13
 XLIB_LIBXPM_SOURCE = libXpm-$(XLIB_LIBXPM_VERSION).tar.bz2
 XLIB_LIBXPM_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXPM_LICENSE = MIT
 XLIB_LIBXPM_LICENSE_FILES = COPYING COPYRIGHT
 XLIB_LIBXPM_INSTALL_STAGING = YES
-# we patch configure.ac
-XLIB_LIBXPM_AUTORECONF = YES
 XLIB_LIBXPM_DEPENDENCIES = xlib_libX11 xlib_libXext xlib_libXt xorgproto \
 	$(if $(BR2_PACKAGE_LIBICONV),libiconv) \
 	$(TARGET_NLS_DEPENDENCIES)
