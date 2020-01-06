c9b7c85623211195fa719dbdbce64993ab953539
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2020-01-06 11:01:08 +0100

    package/libsigrokdecode: bump to v0.5.3
    Remove the patch that's now upstream.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    [Peter: drop _AUTORECONF]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libsigrokdecode/libsigrokdecode.mk
+++ package/libsigrokdecode/libsigrokdecode.mk
@@ -4,13 +4,11 @@
 #
 ################################################################################
 
-LIBSIGROKDECODE_VERSION = 0.5.2
+LIBSIGROKDECODE_VERSION = 0.5.3
 LIBSIGROKDECODE_SITE = http://sigrok.org/download/source/libsigrokdecode
 LIBSIGROKDECODE_LICENSE = GPL-3.0+
 LIBSIGROKDECODE_LICENSE_FILES = COPYING
 LIBSIGROKDECODE_INSTALL_STAGING = YES
 LIBSIGROKDECODE_DEPENDENCIES = host-pkgconf libglib2 python3
-# We're patching configure.ac
-LIBSIGROKDECODE_AUTORECONF = YES
 
 $(eval $(autotools-package))
