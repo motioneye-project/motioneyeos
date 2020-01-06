e82d927da6cfb3c7acf110dcf27ea9c6ccc2cd03
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2020-01-06 11:00:53 +0100

    package/doxygen: bump to v1.8.17
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/doxygen/doxygen.mk
+++ package/doxygen/doxygen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DOXYGEN_VERSION = 1.8.16
+DOXYGEN_VERSION = 1.8.17
 DOXYGEN_SOURCE = doxygen-$(DOXYGEN_VERSION).src.tar.gz
 DOXYGEN_SITE = http://doxygen.nl/files
 DOXYGEN_LICENSE = GPL-2.0
