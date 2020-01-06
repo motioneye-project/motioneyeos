6b58773299bbdef876d93a4aaaa163eccbdb3715
Pierre-Jean Texier <pjtexier@koncepto.io>
2020-01-06 20:51:40 +0100

    package/libubootenv: bump to version cc628ee
    This includes the following changes:
    
    cc628ee libuboot: wrap libuboot in extern "C" for C++
    bf6ff63 Prepare 0.2
    3393485 Fix compiler warning
    8f7c00a uboot_env: fix the resarch of ubi volume
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libubootenv/libubootenv.mk
+++ package/libubootenv/libubootenv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUBOOTENV_VERSION = 92949816720d7af2ac722016e7a5b9a85ff141bc
+LIBUBOOTENV_VERSION = cc628ee0cd750be26275b6eaeaba4d324df11dec
 LIBUBOOTENV_SITE = $(call github,sbabic,libubootenv,$(LIBUBOOTENV_VERSION))
 LIBUBOOTENV_LICENSE = LGPL-2.1
 LIBUBOOTENV_LICENSE_FILES = Licenses/lgpl-2.1.txt
