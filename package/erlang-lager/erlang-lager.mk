01c6c72f7e1cbe5d3aa374c81c6c70494ee0f269
Johan Oudinet <johan.oudinet@gmail.com>
2020-01-06 17:10:55 +0100

    package/erlang-lager: bump to version 3.6.10
    Signed-off-by: Johan Oudinet <johan.oudinet@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/erlang-lager/erlang-lager.mk
+++ package/erlang-lager/erlang-lager.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ERLANG_LAGER_VERSION = 3.4.2
+ERLANG_LAGER_VERSION = 3.6.10
 ERLANG_LAGER_SITE = $(call github,erlang-lager,lager,$(ERLANG_LAGER_VERSION))
 ERLANG_LAGER_LICENSE = Apache-2.0
 ERLANG_LAGER_LICENSE_FILES = LICENSE
