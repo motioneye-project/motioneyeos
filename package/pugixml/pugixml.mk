ee1f731a90d43ee9464374ee3e9a3da22da2b533
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-01-08 22:50:39 +0100

    package/pugixml: bump to version 1.10
    Update hash of license file (update in year and version)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/pugixml/pugixml.mk
+++ package/pugixml/pugixml.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PUGIXML_VERSION = 1.9
+PUGIXML_VERSION = 1.10
 PUGIXML_SITE = http://github.com/zeux/pugixml/releases/download/v$(PUGIXML_VERSION)
 PUGIXML_LICENSE = MIT
 PUGIXML_LICENSE_FILES = readme.txt
