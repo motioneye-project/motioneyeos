8b957c369b8a895866b48fff8c84bbb7be8418f7
Pierre-Jean Texier <pjtexier@koncepto.io>
2020-01-08 18:58:52 +0100

    package/python-periphery: bump to version 2.0.1
    See changelog https://github.com/vsergeev/python-periphery/blob/master/CHANGELOG.md
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-periphery/python-periphery.mk
+++ package/python-periphery/python-periphery.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PERIPHERY_VERSION = 2.0.0
+PYTHON_PERIPHERY_VERSION = 2.0.1
 PYTHON_PERIPHERY_SITE = $(call github,vsergeev,python-periphery,v$(PYTHON_PERIPHERY_VERSION))
 PYTHON_PERIPHERY_LICENSE = MIT
 PYTHON_PERIPHERY_LICENSE_FILES = LICENSE
