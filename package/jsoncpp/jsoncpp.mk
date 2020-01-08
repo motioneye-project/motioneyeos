4b03dd54612f1f52cca3319ff73c4c272a4eefaf
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-01-08 07:53:24 +0100

    package/jsoncpp: disable tests
    Disable tests to avoid a build failure on gcc 4.8 because of
    std::hexfloat added in version 1.9.2 with
    https://github.com/open-source-parsers/jsoncpp/commit/638ad269e75f28f9830f7bfc01278aeaf5b00135
    
    Fixes:
     - http://autobuild.buildroot.org/results/f816229b835f61fa3e4c84aa659d30f81191f369
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/jsoncpp/jsoncpp.mk
+++ package/jsoncpp/jsoncpp.mk
@@ -9,5 +9,6 @@ JSONCPP_SITE = $(call github,open-source-parsers,jsoncpp,$(JSONCPP_VERSION))
 JSONCPP_LICENSE = Public Domain or MIT
 JSONCPP_LICENSE_FILES = LICENSE
 JSONCPP_INSTALL_STAGING = YES
+JSONCPP_CONF_OPTS = -Dtests=false
 
 $(eval $(meson-package))
