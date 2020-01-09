cfeb591865f6047f6545cc081f4712049b8bdf95
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-01-09 20:06:04 +0100

    package/pinentry: add pinentry-fltk
    pinentry-fltk has been added in version 1.1.0 with
    https://github.com/gpg/pinentry/commit/6c45eed62214b44fcc11e642b19df7b6ca0da0bd
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/pinentry/pinentry.mk
+++ package/pinentry/pinentry.mk
@@ -36,6 +36,15 @@ else
 PINENTRY_CONF_OPTS += --disable-libsecret
 endif
 
+# pinentry-fltk backend
+ifeq ($(BR2_PACKAGE_PINENTRY_FLTK),y)
+PINENTRY_CONF_ENV += ac_cv_path_FLTK_CONFIG=$(STAGING_DIR)/usr/bin/fltk-config
+PINENTRY_CONF_OPTS += --enable-pinentry-fltk
+PINENTRY_DEPENDENCIES += fltk
+else
+PINENTRY_CONF_OPTS += --disable-pinentry-fltk
+endif
+
 # pinentry-ncurses backend
 ifeq ($(BR2_PACKAGE_PINENTRY_NCURSES),y)
 PINENTRY_CONF_OPTS += --enable-ncurses --with-ncurses-include-dir=none
