de591c5c3a93a40d049a32bd79c30cc66daf5fb2
Peter Korsgaard <peter@korsgaard.com>
2020-01-09 10:16:19 +0100

    package/wireguard-linux-compat: new package
    With the kernel support for WireGuard getting mainlined, the upstream repo
    has been split in a wireguard-tools repo for the userspace tooling and
    wireguard-linux-compat for the kernel side (for 3.10+ legacy kernels).
    
    Add a wireguard-linux-compat for the compatibility out-of-tree kernel
    module, and update the Config.in.legacy handling to use it.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

---
+++ package/wireguard-linux-compat/wireguard-linux-compat.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# wireguard
+#
+################################################################################
+
+WIREGUARD_LINUX_COMPAT_VERSION = 0.0.20200105
+WIREGUARD_LINUX_COMPAT_SITE = https://git.zx2c4.com/wireguard-linux-compat/snapshot
+WIREGUARD_LINUX_COMPAT_SOURCE = wireguard-linux-compat-$(WIREGUARD_LINUX_COMPAT_VERSION).tar.xz
+WIREGUARD_LINUX_COMPAT_LICENSE = GPL-2.0
+WIREGUARD_LINUX_COMPAT_LICENSE_FILES = COPYING
+WIREGUARD_LINUX_COMPAT_MODULE_SUBDIRS = src
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
