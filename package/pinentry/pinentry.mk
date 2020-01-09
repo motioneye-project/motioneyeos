9dbf60c7b5ae3eee28d3a36260294bef2fe21940
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-01-09 20:06:03 +0100

    package/pinentry: bump to version 1.1.0
    Add hash for license file
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/pinentry/pinentry.mk
+++ package/pinentry/pinentry.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PINENTRY_VERSION = 1.0.0
+PINENTRY_VERSION = 1.1.0
 PINENTRY_SOURCE = pinentry-$(PINENTRY_VERSION).tar.bz2
 PINENTRY_SITE = https://www.gnupg.org/ftp/gcrypt/pinentry
 PINENTRY_LICENSE = GPL-2.0+
