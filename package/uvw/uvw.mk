1def43d2b2b8c4ad13947d41b06f4a307af49790
Asaf Kahlon <asafka7@gmail.com>
2020-01-09 19:32:06 +0200

    libuv, uvw: bump to versions 1.34.0, 2.3.0_libuv-v1.34
    Remove uvw patch (now on upstream).
    
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/uvw/uvw.mk
+++ package/uvw/uvw.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UVW_VERSION = 2.2.0_libuv-v1.33
+UVW_VERSION = 2.3.0_libuv-v1.34
 UVW_SITE = $(call github,skypjack,uvw,v$(UVW_VERSION))
 UVW_INSTALL_STAGING = YES
 UVW_INSTALL_TARGET = NO
