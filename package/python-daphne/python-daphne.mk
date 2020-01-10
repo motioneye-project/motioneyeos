0ed357e8c2da2403ba9a30714e73db86a2d029f7
Adam Duskett <Aduskett@gmail.com>
2020-01-10 08:16:35 -0800

    package/python-daphne: bump version to 2.4.1
    Other changes:
      - Use the LICENSE file for the license. This prevents having to change the
        sha256sum on every version bump due to PKG-INFO containing the version
        number.
    
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-daphne/python-daphne.mk
+++ package/python-daphne/python-daphne.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_DAPHNE_VERSION = 2.4.0
+PYTHON_DAPHNE_VERSION = 2.4.1
 PYTHON_DAPHNE_SOURCE = daphne-$(PYTHON_DAPHNE_VERSION).tar.gz
-PYTHON_DAPHNE_SITE = https://files.pythonhosted.org/packages/c6/66/05ace288000d323831a9fab1e68a5625b117b7c8aca408404d33330f3906
+PYTHON_DAPHNE_SITE = https://files.pythonhosted.org/packages/03/6c/847d1c962f8c45aa2ab0791583c4a41669d158e28fd6369ce940b8ea8417
 PYTHON_DAPHNE_SETUP_TYPE = setuptools
 # https://github.com/django/daphne/blob/master/LICENSE
 PYTHON_DAPHNE_LICENSE = BSD-3-Clause
-PYTHON_DAPHNE_LICENSE_FILES = PKG-INFO
+PYTHON_DAPHNE_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
