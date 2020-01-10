24e7ae452130cfbd3cbe63adccdc54fd18f3c965
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-01-10 23:02:39 +0100

    package/gstreamer1/gst1-plugins-bad: add fluidsynth option
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/gstreamer1/gst1-plugins-bad/gst1-plugins-bad.mk
+++ package/gstreamer1/gst1-plugins-bad/gst1-plugins-bad.mk
@@ -42,7 +42,6 @@ GST1_PLUGINS_BAD_CONF_OPTS += \
 	-Dfaac=disabled \
 	-Dflite=disabled \
 	-Dgsm=disabled \
-	-Dfluidsynth=disabled \
 	-Dkate=disabled \
 	-Dladspa=disabled \
 	-Dlv2=disabled \
@@ -524,6 +523,13 @@ GST1_PLUGINS_BAD_CONF_OPTS += -Dfdkaac=enabled
 GST1_PLUGINS_BAD_DEPENDENCIES += fdk-aac
 else
 GST1_PLUGINS_BAD_CONF_OPTS += -Dfdkaac=disabled
+endif
+
+ifeq ($(BR2_PACKAGE_GST1_PLUGINS_BAD_PLUGIN_FLUIDSYNTH),y)
+GST1_PLUGINS_BAD_CONF_OPTS += -Dfluidsynth=enabled
+GST1_PLUGINS_BAD_DEPENDENCIES += fluidsynth
+else
+GST1_PLUGINS_BAD_CONF_OPTS += -Dfluidsynth=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_GST1_PLUGINS_BAD_PLUGIN_GL),y)
