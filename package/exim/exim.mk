c4a993b93bad415444bd6aab90725ff99eafa917
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-01-11 09:42:06 +0100

    package/exim: bump version to 4.93.0.4
    Release notes:
    https://lists.exim.org/lurker/message/20200107.002043.053ca484.en.html
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/exim/exim.mk
+++ package/exim/exim.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EXIM_VERSION = 4.93.0.3
+EXIM_VERSION = 4.93.0.4
 EXIM_SOURCE = exim-$(EXIM_VERSION).tar.xz
 EXIM_SITE = https://ftp.exim.org/pub/exim/exim4/fixes
 EXIM_LICENSE = GPL-2.0+
