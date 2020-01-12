39476fbd82b0dac43fec5e671171e4e6ad9a4860
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-01-12 23:27:54 +0100

    package/olsr: add netjson plugin
    Add netjson plugin, available since version 0.9.5 and
    https://github.com/OLSR/olsrd/commit/64c00288f8e87d314a723e20c2dd69f1165f1645
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/olsr/olsr.mk
+++ package/olsr/olsr.mk
@@ -9,7 +9,7 @@ OLSR_VERSION = $(OLSR_VERSION_MAJOR).6.1
 OLSR_SOURCE = olsrd-$(OLSR_VERSION).tar.bz2
 OLSR_SITE = http://www.olsr.org/releases/$(OLSR_VERSION_MAJOR)
 OLSR_PLUGINS = arprefresh bmf dot_draw dyn_gw dyn_gw_plain httpinfo jsoninfo \
-	mdns nameservice p2pd pgraph secure txtinfo watchdog
+	mdns nameservice netjson p2pd pgraph secure txtinfo watchdog
 # Doesn't really need quagga but not very useful without it
 OLSR_PLUGINS += $(if $(BR2_PACKAGE_QUAGGA),quagga)
 OLSR_LICENSE = BSD-3-Clause, LGPL-2.1+
