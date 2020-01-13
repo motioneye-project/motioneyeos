b7a024372d7c5ccb48b8e078b08eddb5eaef1d15
Angelo Compagnucci <angelo.compagnucci@gmail.com>
2020-01-13 22:40:15 +0100

    package/fail2ban: bump to version 0.11.1
    This patch bump fail2ban to verison 0.11.1.
    
    Signed-off-by: Angelo Compagnucci <angelo@amarulasolutions.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/fail2ban/fail2ban.mk
+++ package/fail2ban/fail2ban.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FAIL2BAN_VERSION = 0.10.5
+FAIL2BAN_VERSION = 0.11.1
 FAIL2BAN_SITE = $(call github,fail2ban,fail2ban,$(FAIL2BAN_VERSION))
 FAIL2BAN_LICENSE = GPL-2.0+
 FAIL2BAN_LICENSE_FILES = COPYING
