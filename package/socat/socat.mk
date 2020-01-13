75f4520ac1fd53ff4c74ffe3843d8b77a1023ffe
Baruch Siach <baruch@tkos.co.il>
2020-01-13 14:35:42 +0200

    package/socat: bump to version 1.7.3.4
    Drop patch #2. Upstream commit d8a5d49c2d8359 includes stddef.h in a
    higher level header.
    
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/socat/socat.mk
+++ package/socat/socat.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SOCAT_VERSION = 1.7.3.3
+SOCAT_VERSION = 1.7.3.4
 SOCAT_SOURCE = socat-$(SOCAT_VERSION).tar.bz2
 SOCAT_SITE = http://www.dest-unreach.org/socat/download
 SOCAT_LICENSE = GPL-2.0 with OpenSSL exception
