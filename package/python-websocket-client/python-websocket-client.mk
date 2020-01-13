1cf2f512535cbf9dbfd5fc5f61097a00b5e1b505
Peter Korsgaard <peter@korsgaard.com>
2020-01-13 09:19:05 +0100

    package/python-websocket-client: bump to version 0.57.0
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-websocket-client/python-websocket-client.mk
+++ package/python-websocket-client/python-websocket-client.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WEBSOCKET_CLIENT_VERSION = 0.56.0
+PYTHON_WEBSOCKET_CLIENT_VERSION = 0.57.0
 PYTHON_WEBSOCKET_CLIENT_SOURCE = websocket_client-$(PYTHON_WEBSOCKET_CLIENT_VERSION).tar.gz
-PYTHON_WEBSOCKET_CLIENT_SITE = https://files.pythonhosted.org/packages/c5/01/8c9c7de6c46f88e70b5a3276c791a2be82ae83d8e0d0cc030525ee2866fd
+PYTHON_WEBSOCKET_CLIENT_SITE = https://files.pythonhosted.org/packages/8b/0f/52de51b9b450ed52694208ab952d5af6ebbcbce7f166a48784095d930d8c
 PYTHON_WEBSOCKET_CLIENT_SETUP_TYPE = setuptools
 # Project was under LGPL-2.1+, but was relicensed under
 # BSD-3-Clause. The LICENSE file contains the BSD-3-Clause text, but
