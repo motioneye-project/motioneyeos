e64ba1806002c118157a5d8d9f92d85baac56447
Zoltan Gyarmati <zgyarmati@zgyarmati.de>
2020-01-13 12:55:12 +0100

    package/grantlee: bump to 5.2.0, use github upstream, remove obsolete patch
    Signed-off-by: Zoltan Gyarmati <zgyarmati@zgyarmati.de>
    [yann.morin.1998@free.fr: two spaces in hash file]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/grantlee/grantlee.mk
+++ package/grantlee/grantlee.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-GRANTLEE_VERSION = 5.1.0
-GRANTLEE_SITE = http://downloads.grantlee.org
+GRANTLEE_VERSION = 5.2.0
+GRANTLEE_SITE = $(call github,steveire,grantlee,v$(GRANTLEE_VERSION))
 GRANTLEE_INSTALL_STAGING = YES
 GRANTLEE_LICENSE = LGPL-2.1+
 GRANTLEE_LICENSE_FILES = COPYING.LIB
