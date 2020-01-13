61b378e8a0ddf2e04d1e72d530cf5e524f8e28d7
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-01-13 21:40:16 +0100

    package/udftools: bump to version 2.2
    Drop all patches (already in version) and so autoreconf
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/udftools/udftools.mk
+++ package/udftools/udftools.mk
@@ -4,11 +4,10 @@
 #
 ################################################################################
 
-UDFTOOLS_VERSION = 2.1
+UDFTOOLS_VERSION = 2.2
 UDFTOOLS_SITE = https://github.com/pali/udftools/releases/download/$(UDFTOOLS_VERSION)
 UDFTOOLS_LICENSE = GPL-2.0+
 UDFTOOLS_LICENSE_FILES = COPYING
-UDFTOOLS_AUTORECONF = YES
 UDFTOOLS_DEPENDENCIES = readline host-pkgconf
 
 $(eval $(autotools-package))
