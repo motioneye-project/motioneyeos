f692541dff3f89f10d16103b0b6e0c31bb9da4b3
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-01-13 21:43:29 +0100

    package/smartmontools: add libcap-ng optional dependency
    libcap-ng is an optional dependency since a very long time (2010) and
    https://github.com/mirror/smartmontools/commit/24882d3672b759c1125169420749e186a3816fe1
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/smartmontools/smartmontools.mk
+++ package/smartmontools/smartmontools.mk
@@ -9,4 +9,11 @@ SMARTMONTOOLS_SITE = http://downloads.sourceforge.net/project/smartmontools/smar
 SMARTMONTOOLS_LICENSE = GPL-2.0+
 SMARTMONTOOLS_LICENSE_FILES = COPYING
 
+ifeq ($(BR2_PACKAGE_LIBCAP_NG),y)
+SMARTMONTOOLS_CONF_OPTS += --with-libcap-ng
+SMARTMONTOOLS_DEPENDENCIES += libcap-ng
+else
+SMARTMONTOOLS_CONF_OPTS += --without-libcap-ng
+endif
+
 $(eval $(autotools-package))
