746b143d5523d33a8e79803511414941ec470073
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-01-17 22:57:54 +0100

    package/cppzmq: bump to version 4.6.0
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/cppzmq/cppzmq.mk
+++ package/cppzmq/cppzmq.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CPPZMQ_VERSION = 4.5.0
+CPPZMQ_VERSION = 4.6.0
 CPPZMQ_SITE = $(call github,zeromq,cppzmq,v$(CPPZMQ_VERSION))
 CPPZMQ_INSTALL_STAGING = YES
 CPPZMQ_DEPENDENCIES = host-pkgconf zeromq
