27ea062ad0680626abc5822f2bfe7793be7cbeef
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-01-19 12:07:35 +0100

    package/kodi-vfs-sftp: bump version
    License hash has changed because it is a source file, but also because
    the name changed from XBMC to Kodi, and the year bumped to 2019.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/kodi-vfs-sftp/kodi-vfs-sftp.mk
+++ package/kodi-vfs-sftp/kodi-vfs-sftp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_VFS_SFTP_VERSION = 1.0.3-Leia
+KODI_VFS_SFTP_VERSION = 1.0.5-Leia
 KODI_VFS_SFTP_SITE = $(call github,xbmc,vfs.sftp,$(KODI_VFS_SFTP_VERSION))
 KODI_VFS_SFTP_LICENSE = GPL-2.0+
 KODI_VFS_SFTP_LICENSE_FILES = src/SFTPFile.cpp
