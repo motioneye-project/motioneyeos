074e1c92e481931372ad9703df1cc4f1cd4f2e1d
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-01-19 12:07:30 +0100

    package/kodi-visualisation-goom: bump version to 2.1.0-Leia
    Removed 0001-cross-compile.patch, not needed anymore:
    https://github.com/xbmc/visualization.goom/pull/4#issuecomment-537987594
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/kodi-visualisation-goom/kodi-visualisation-goom.mk
+++ package/kodi-visualisation-goom/kodi-visualisation-goom.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-KODI_VISUALISATION_GOOM_VERSION = 54da35870930acd1a3a36195dd2c1498ac336b90
-KODI_VISUALISATION_GOOM_SITE = $(call github,notspiff,visualization.goom,$(KODI_VISUALISATION_GOOM_VERSION))
+KODI_VISUALISATION_GOOM_VERSION = 2.1.0-Leia
+KODI_VISUALISATION_GOOM_SITE = $(call github,xbmc,visualization.goom,$(KODI_VISUALISATION_GOOM_VERSION))
 KODI_VISUALISATION_GOOM_LICENSE = GPL-2.0+
 KODI_VISUALISATION_GOOM_LICENSE_FILES = src/Main.cpp
 
