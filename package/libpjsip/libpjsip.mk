aed4e7d160fd5ad87fa285bc5c1dff80881625d2
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-01-19 10:52:50 +0100

    package/libpjsip: add opus support
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libpjsip/libpjsip.mk
+++ package/libpjsip/libpjsip.mk
@@ -32,7 +32,6 @@ LIBPJSIP_CONF_OPTS = \
 	--disable-g7221-codec \
 	--disable-ilbc-codec \
 	--disable-libwebrtc \
-	--disable-opus \
 	--disable-ext-sound \
 	--disable-g711-codec \
 	--disable-l16-codec \
@@ -70,6 +69,13 @@ LIBPJSIP_DEPENDENCIES += gnutls
 LIBPJSIP_CONF_OPTS += --with-gnutls=$(STAGING_DIR)/usr
 else
 LIBPJSIP_CONF_OPTS += --disable-ssl
+endif
+
+ifeq ($(BR2_PACKAGE_OPUS),y)
+LIBPJSIP_DEPENDENCIES += opus
+LIBPJSIP_CONF_OPTS += --with-opus=$(STAGING_DIR)/usr
+else
+LIBPJSIP_CONF_OPTS += --disable-opus
 endif
 
 ifeq ($(BR2_PACKAGE_SPEEX)$(BR2_PACKAGE_SPEEXDSP),yy)
