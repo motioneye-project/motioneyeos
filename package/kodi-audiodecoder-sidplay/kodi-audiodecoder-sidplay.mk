006d9367392002f7bcb9200e9ca55a8a9cfd128c
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-01-19 12:07:15 +0100

    package/kodi-audiodecoder-*: mass version bump
    Changed _SITE after upstream relocated all audio decoders to xbmc repos
    https://github.com/xbmc/repo-binary-addons/commit/4c900f6835889ce45ba4daaf0387bb5eb13db065
    
    Changed dependency from kodi-platform to kodi itself, the dependency to
    kodi-platform was removed upstream.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/kodi-audiodecoder-sidplay/kodi-audiodecoder-sidplay.mk
+++ package/kodi-audiodecoder-sidplay/kodi-audiodecoder-sidplay.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-KODI_AUDIODECODER_SIDPLAY_VERSION = 1.1.0
-KODI_AUDIODECODER_SIDPLAY_SITE = $(call github,notspiff,audiodecoder.sidplay,v$(KODI_AUDIODECODER_SIDPLAY_VERSION))
+KODI_AUDIODECODER_SIDPLAY_VERSION = 1.2.1-Leia
+KODI_AUDIODECODER_SIDPLAY_SITE = $(call github,xbmc,audiodecoder.sidplay,$(KODI_AUDIODECODER_SIDPLAY_VERSION))
 KODI_AUDIODECODER_SIDPLAY_LICENSE = GPL-2.0+
 KODI_AUDIODECODER_SIDPLAY_LICENSE_FILES = src/SIDCodec.cpp
-KODI_AUDIODECODER_SIDPLAY_DEPENDENCIES = host-pkgconf kodi-platform libsidplay2
+KODI_AUDIODECODER_SIDPLAY_DEPENDENCIES = host-pkgconf kodi libsidplay2
 
 $(eval $(cmake-package))
--- package/kodi-audiodecoder-snesapu/kodi-audiodecoder-snesapu.mk
+++ package/kodi-audiodecoder-snesapu/kodi-audiodecoder-snesapu.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-KODI_AUDIODECODER_SNESAPU_VERSION = 1.1.0
-KODI_AUDIODECODER_SNESAPU_SITE = $(call github,notspiff,audiodecoder.snesapu,v$(KODI_AUDIODECODER_SNESAPU_VERSION))
+KODI_AUDIODECODER_SNESAPU_VERSION = 2.0.1-Leia
+KODI_AUDIODECODER_SNESAPU_SITE = $(call github,xbmc,audiodecoder.snesapu,$(KODI_AUDIODECODER_SNESAPU_VERSION))
 KODI_AUDIODECODER_SNESAPU_LICENSE = GPL-2.0+
 KODI_AUDIODECODER_SNESAPU_LICENSE_FILES = src/SPCCodec.cpp
-KODI_AUDIODECODER_SNESAPU_DEPENDENCIES = kodi-platform
+KODI_AUDIODECODER_SNESAPU_DEPENDENCIES = kodi
 
 $(eval $(cmake-package))
