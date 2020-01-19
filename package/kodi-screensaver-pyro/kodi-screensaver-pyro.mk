f03ea1d7d4cf3dd682b11eecdf53d94f4c507b03
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-01-19 12:07:28 +0100

    package/kodi-screensaver-*: mass version bump
    Most screensaver repos were moved to the xbmc project:
    https://github.com/xbmc/repo-binary-addons/commit/57b8ed5fb9b5f0368738260b762cc0bab1147172
    https://github.com/xbmc/repo-binary-addons/commit/29c23f8e0cbfab6c39eb910ebc4f23f7afdec87e
    https://github.com/xbmc/repo-binary-addons/commit/98e71315026fd163de7cc15dad8e2c0398851499
    https://github.com/xbmc/repo-binary-addons/commit/994b0d5b73118e6dc5cece534daffb6a176fa426
    https://github.com/xbmc/repo-binary-addons/commit/21b0c96d7fbff7853aadf63868b79384cdb40394
    
    Add patches to fix missing cstddef include.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/kodi-screensaver-pyro/kodi-screensaver-pyro.mk
+++ package/kodi-screensaver-pyro/kodi-screensaver-pyro.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-KODI_SCREENSAVER_PYRO_VERSION = 1.1.0
-KODI_SCREENSAVER_PYRO_SITE = $(call github,notspiff,screensaver.pyro,v$(KODI_SCREENSAVER_PYRO_VERSION))
+KODI_SCREENSAVER_PYRO_VERSION = 3.0.0-Leia
+KODI_SCREENSAVER_PYRO_SITE = $(call github,xbmc,screensaver.pyro,$(KODI_SCREENSAVER_PYRO_VERSION))
 KODI_SCREENSAVER_PYRO_LICENSE = GPL-2.0+
 KODI_SCREENSAVER_PYRO_LICENSE_FILES = src/Pyro.cpp
 KODI_SCREENSAVER_PYRO_DEPENDENCIES = kodi
--- package/kodi-screensaver-stars/kodi-screensaver-stars.mk
+++ package/kodi-screensaver-stars/kodi-screensaver-stars.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-KODI_SCREENSAVER_STARS_VERSION = 1.1.0
-KODI_SCREENSAVER_STARS_SITE = $(call github,notspiff,screensaver.stars,v$(KODI_SCREENSAVER_STARS_VERSION))
+KODI_SCREENSAVER_STARS_VERSION = 2.1.3-Leia
+KODI_SCREENSAVER_STARS_SITE = $(call github,xbmc,screensaver.stars,$(KODI_SCREENSAVER_STARS_VERSION))
 KODI_SCREENSAVER_STARS_LICENSE = GPL-2.0+
 KODI_SCREENSAVER_STARS_LICENSE_FILES = src/StarField.cpp
 KODI_SCREENSAVER_STARS_DEPENDENCIES = kodi
