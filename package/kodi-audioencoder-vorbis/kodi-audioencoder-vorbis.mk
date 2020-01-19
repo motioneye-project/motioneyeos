97921206587ca65ac98627f25d0c54e72962d0e2
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-01-19 12:07:16 +0100

    package/kodi-audioencoder-*: mass version bump
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- package/kodi-audioencoder-vorbis/kodi-audioencoder-vorbis.mk
+++ package/kodi-audioencoder-vorbis/kodi-audioencoder-vorbis.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_AUDIOENCODER_VORBIS_VERSION = 1.1.0
+KODI_AUDIOENCODER_VORBIS_VERSION = 2.0.2-Leia
 KODI_AUDIOENCODER_VORBIS_SITE = $(call github,xbmc,audioencoder.vorbis,v$(KODI_AUDIOENCODER_VORBIS_VERSION))
 KODI_AUDIOENCODER_VORBIS_LICENSE = GPL-2.0+
 KODI_AUDIOENCODER_VORBIS_LICENSE_FILES = src/EncoderVorbis.cpp
