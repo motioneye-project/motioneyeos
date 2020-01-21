193d6a0e070510d35d726f175b768ff61944abbd
Peter Korsgaard <peter@korsgaard.com>
2020-01-21 17:35:19 +0100

    package/wireguard-linux-compat: bump to version 1.0-20200121
    For details, see the announcement:
    https://lists.zx2c4.com/pipermail/wireguard/2020-January/004870.html
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/wireguard-linux-compat/wireguard-linux-compat.mk
+++ package/wireguard-linux-compat/wireguard-linux-compat.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIREGUARD_LINUX_COMPAT_VERSION = 0.0.20200105
+WIREGUARD_LINUX_COMPAT_VERSION = 0.0.20200121
 WIREGUARD_LINUX_COMPAT_SITE = https://git.zx2c4.com/wireguard-linux-compat/snapshot
 WIREGUARD_LINUX_COMPAT_SOURCE = wireguard-linux-compat-$(WIREGUARD_LINUX_COMPAT_VERSION).tar.xz
 WIREGUARD_LINUX_COMPAT_LICENSE = GPL-2.0
