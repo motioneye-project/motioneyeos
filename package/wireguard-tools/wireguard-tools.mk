c309348349ea34d89473df3fea46102dcdbd6303
Peter Korsgaard <peter@korsgaard.com>
2020-01-21 17:35:18 +0100

    package/wireguard-tools: bump to version 1.0-20200121
    For details, see the announcement:
    https://lists.zx2c4.com/pipermail/wireguard/2020-January/004869.html
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/wireguard-tools/wireguard-tools.mk
+++ package/wireguard-tools/wireguard-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIREGUARD_TOOLS_VERSION = 1.0.20200102
+WIREGUARD_TOOLS_VERSION = 1.0.20200121
 WIREGUARD_TOOLS_SITE = https://git.zx2c4.com/wireguard-tools/snapshot
 WIREGUARD_TOOLS_SOURCE = wireguard-tools-$(WIREGUARD_TOOLS_VERSION).tar.xz
 WIREGUARD_TOOLS_LICENSE = GPL-2.0
