307fd6f517f1a4e64846f3a76c624aac8e6eacd9
Johan Oudinet <johan.oudinet@gmail.com>
2020-01-22 11:34:01 +0100

    package/erlang-p1-xmpp: fix host build
    Fixes:
    http://autobuild.buildroot.net/results/3e594350131994df53b20d5113fefbba2e747836
    
    Signed-off-by: Johan Oudinet <johan.oudinet@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/erlang-p1-xmpp/erlang-p1-xmpp.mk
+++ package/erlang-p1-xmpp/erlang-p1-xmpp.mk
@@ -11,6 +11,7 @@ ERLANG_P1_XMPP_LICENSE_FILES = LICENSE.txt
 ERLANG_P1_XMPP_INSTALL_STAGING = YES
 ERLANG_P1_XMPP_DEPENDENCIES = erlang-p1-xml erlang-p1-stringprep \
 	erlang-p1-tls erlang-p1-utils erlang-p1-zlib host-erlang-p1-xml
+HOST_ERLANG_P1_XMPP_DEPENDENCIES = host-erlang-p1-xml
 
 $(eval $(rebar-package))
 $(eval $(host-rebar-package))
