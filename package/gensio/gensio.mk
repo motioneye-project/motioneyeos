4ec6085f09bc753db7e09b1684bcf8ffdbdf2580
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-01-28 23:20:45 +0100

    package/gensio: fix detection of openssl with -latomic
    Add an upstream patch to fix --with-openssl argument and allow gensio to use
    pkg-config to retrieve openssl dependencies otherwise the detection of
    openssl will fail on architecture that needs to link with -latomic such as
    sparc v8 32 bits:
    
    configure:9379: checking for openssl/ssl.h in /home/fabrice/buildroot/output/host/sparc-buildroot-linux-uclibc/sysroot/usr
    configure:9386: result: yes
    configure:9402: checking whether compiling and linking against OpenSSL works
    Trying link with OPENSSL_LDFLAGS=-L/home/fabrice/buildroot/output/host/sparc-buildroot-linux-uclibc/sysroot/usr/lib; OPENSSL_LIBS=-lssl -lcrypto; OPENSSL_INCLUDES=-I/home/fabrice/buildroot/output/host/sparc-buildroot-linux-uclibc/sysroot/usr/include
    configure:9424: /home/fabrice/buildroot/output/host/bin/sparc-linux-gcc -o conftest -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64  -Os   -static -I/home/fabrice/buildroot/output/host/sparc-buildroot-linux-uclibc/sysroot/usr/include -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64  -static -L/home/fabrice/buildroot/output/host/sparc-buildroot-linux-uclibc/sysroot/usr/lib conftest.c -lssl -lcrypto  -lpthread >&5
    /home/fabrice/buildroot/output/host/opt/ext-toolchain/bin/../lib/gcc/sparc-buildroot-linux-uclibc/8.3.0/../../../../sparc-buildroot-linux-uclibc/bin/ld: /home/fabrice/buildroot/output/host/sparc-buildroot-linux-uclibc/sysroot/usr/lib/libssl.a(ssl_lib.o): in function `CRYPTO_UP_REF.isra.6':
    ssl_lib.c:(.text+0x3c8): undefined reference to `__atomic_fetch_add_4'
    
    Fixes:
     - No autobuilder failures (silent error)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gensio/gensio.mk
+++ package/gensio/gensio.mk
@@ -9,19 +9,18 @@ GENSIO_SITE = http://downloads.sourceforge.net/project/ser2net/ser2net
 GENSIO_LICENSE = LGPL-2.1+ (library), GPL-2.0+ (tools)
 GENSIO_LICENSE_FILES = COPYING.LIB COPYING
 GENSIO_INSTALL_STAGING = YES
+# We're ptching configure.ac
+GENSIO_AUTORECONF = YES
 GENSIO_CONF_OPTS = \
 	--without-openipmi \
 	--without-swig \
 	--without-python
 
-# configure script by default searches host paths for openssl,
-# breaking cross compilation. Disable this by explicitly pointing it
-# at STAGING_DIR no matter if we have openssl enabled or not as it
-# will correctly disable openssl support if not found there
-GENSIO_CONF_OPTS += --with-openssl=$(STAGING_DIR)/usr
-
 ifeq ($(BR2_PACKAGE_OPENSSL),y)
-GENSIO_DEPENDENCIES += openssl
+GENSIO_DEPENDENCIES += host-pkgconf openssl
+GENSIO_CONF_OPTS += --with-openssl
+else
+GENSIO_CONF_OPTS += --without-openssl
 endif
 
 ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
