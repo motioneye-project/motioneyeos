d22bd3749eed504e5e14ffcf6f1afbaac8331f7c
Pierre-Jean Texier <pjtexier@koncepto.io>
2020-01-29 20:57:16 +0100

    package/libubootenv: bump to version 879c073
    This includes the following changes:
    
    879c073 Do not hardcode path for install
    d9c639b libubootenv: add pkg-config support
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libubootenv/libubootenv.mk
+++ package/libubootenv/libubootenv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUBOOTENV_VERSION = cc628ee0cd750be26275b6eaeaba4d324df11dec
+LIBUBOOTENV_VERSION = 879c0731fa0725785b9fa4499bbf6aacd04ee4c2
 LIBUBOOTENV_SITE = $(call github,sbabic,libubootenv,$(LIBUBOOTENV_VERSION))
 LIBUBOOTENV_LICENSE = LGPL-2.1
 LIBUBOOTENV_LICENSE_FILES = Licenses/lgpl-2.1.txt
