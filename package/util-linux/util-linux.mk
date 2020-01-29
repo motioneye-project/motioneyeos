4f3af906fb66d540e6cf60712896175f8854a862
Carlos Santos <unixmania@gmail.com>
2020-01-29 14:34:25 -0300

    package/util-linux: bump to version 2.35
    This version brings bug fixes, enhancements and a new script utility,
    scriptlive. For detailed information see the release notes:
    
      http://www.kernel.org/pub/linux/utils/util-linux/v2.35/v2.35-ReleaseNotes
    
    Pull some fixed applied after the release.
    
    Disable the use of code under GPLv3 included in hwclock since v2.30. The
    subject was discussed upstream[1] and it was decided that hwclock will
    be made GPLv2-only again in v2.36, so do it in advance in Buildroot.
    
    Meanwhile, be warned that all OS images selecting hwclock built with
    Buildroot since commit 74235a6854 (util-linux: bump to version 2.30)
    contain code under GPLv3, which imposes some technical difficulties to
    include in embedded systems. For more information see GPLv3, Section 6,
    "Conveying Non-Source Forms", and the definitions of User Product and
    Installation Information[2].
    
    1. https://lore.kernel.org/util-linux/20200127202152.4jh2w4chch37wgee@ws.net.home/T/#t
    2. https://www.gnu.org/licenses/gpl-3.0.html
    
    Signed-off-by: Carlos Santos <unixmania@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/util-linux/util-linux.mk
+++ package/util-linux/util-linux.mk
@@ -4,13 +4,17 @@
 #
 ################################################################################
 
-UTIL_LINUX_VERSION_MAJOR = 2.34
+UTIL_LINUX_VERSION_MAJOR = 2.35
 UTIL_LINUX_VERSION = $(UTIL_LINUX_VERSION_MAJOR)
 UTIL_LINUX_SOURCE = util-linux-$(UTIL_LINUX_VERSION).tar.xz
 UTIL_LINUX_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/util-linux/v$(UTIL_LINUX_VERSION_MAJOR)
 
-# README.licensing claims that some files are GPL-2.0 only, but this is not true.
-# Some files are GPL-3.0+ but only in tests. rfkill uses an ISC-style license.
+# 0006-build-sys-add-disable-hwclock-gplv3.patch
+UTIL_LINUX_AUTORECONF = YES
+
+# README.licensing claims that some files are GPL-2.0 only, but this is not
+# true. Some files are GPL-3.0+ but only in tests and optionally in hwclock
+# (but we disable that option). rfkill uses an ISC-style license.
 UTIL_LINUX_LICENSE = GPL-2.0+, BSD-4-Clause, LGPL-2.1+ (libblkid, libfdisk, libmount), BSD-3-Clause (libuuid), ISC (rfkill)
 UTIL_LINUX_LICENSE_FILES = README.licensing \
 	Documentation/licenses/COPYING.BSD-3-Clause \
@@ -18,12 +22,13 @@ UTIL_LINUX_LICENSE_FILES = README.licensing \
 	Documentation/licenses/COPYING.GPL-2.0-or-later \
 	Documentation/licenses/COPYING.ISC \
 	Documentation/licenses/COPYING.LGPL-2.1-or-later
+
 UTIL_LINUX_INSTALL_STAGING = YES
 UTIL_LINUX_DEPENDENCIES = host-pkgconf $(TARGET_NLS_DEPENDENCIES)
 UTIL_LINUX_CONF_OPTS += \
 	--disable-rpath \
 	--disable-makeinstall-chown
-UTIL_LINUX_AUTORECONF = YES
+
 UTIL_LINUX_LIBS = $(TARGET_NLS_LIBS)
 
 # system depends on util-linux so we enable systemd support
@@ -109,7 +114,7 @@ UTIL_LINUX_CONF_OPTS += \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_FDFORMAT),--enable-fdformat,--disable-fdformat) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_FSCK),--enable-fsck,--disable-fsck) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_HARDLINK),--enable-hardlink,--disable-hardlink) \
-	$(if $(BR2_PACKAGE_UTIL_LINUX_HWCLOCK),--enable-hwclock,--disable-hwclock) \
+	$(if $(BR2_PACKAGE_UTIL_LINUX_HWCLOCK),--enable-hwclock --disable-hwclock-gplv3,--disable-hwclock) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_IPCRM),--enable-ipcrm,--disable-ipcrm) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_IPCS),--enable-ipcs,--disable-ipcs) \
 	$(if $(BR2_PACKAGE_UTIL_LINUX_KILL),--enable-kill,--disable-kill) \
