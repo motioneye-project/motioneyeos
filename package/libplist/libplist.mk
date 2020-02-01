91a1acc8054189b5e2c9a37e545d67f80b9ef2fc
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-02-01 09:55:53 +0100

    package/libplist: bump version to 2.1.0
    Switched _SITE to github, added license hash.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libplist/libplist.mk
+++ package/libplist/libplist.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-LIBPLIST_VERSION = 2.0.0
-LIBPLIST_SOURCE = libplist-$(LIBPLIST_VERSION).tar.bz2
-LIBPLIST_SITE = http://www.libimobiledevice.org/downloads
+LIBPLIST_VERSION = 2.1.0
+LIBPLIST_SITE = $(call github,libimobiledevice,libplist,$(LIBPLIST_VERSION))
 LIBPLIST_INSTALL_STAGING = YES
 LIBPLIST_LICENSE = LGPL-2.1+
 LIBPLIST_LICENSE_FILES = COPYING
+# github tarball does not include configure
+LIBPLIST_AUTORECONF = YES
 
 # Disable building Python bindings, because it requires host-cython, which
 # is not packaged in Buildroot at all.
