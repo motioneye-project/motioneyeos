2da33e963994faae5f206a3556b9bd80323740f0
Yann E. MORIN <yann.morin.1998@free.fr>
2020-02-02 11:11:03 +0100

    Revert "package/weston: bump to version 8.0.0"
    This reverts commit 4f217669de10f61d77e9ff834d9efdcee4efdf04 which
    should not have been pushed.
    
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/weston/weston.mk
+++ package/weston/weston.mk
@@ -4,33 +4,37 @@
 #
 ################################################################################
 
-WESTON_VERSION = 8.0.0
+WESTON_VERSION = 6.0.1
 WESTON_SITE = http://wayland.freedesktop.org/releases
 WESTON_SOURCE = weston-$(WESTON_VERSION).tar.xz
 WESTON_LICENSE = MIT
 WESTON_LICENSE_FILES = COPYING
 
 WESTON_DEPENDENCIES = host-pkgconf wayland wayland-protocols \
-	libxkbcommon pixman libpng jpeg udev cairo libinput libdrm
+	libxkbcommon pixman libpng jpeg udev cairo libinput libdrm \
+	$(if $(BR2_PACKAGE_WEBP),webp)
 
 WESTON_CONF_OPTS = \
-	-Dbuild.pkg_config_path=$(HOST_DIR)/lib/pkgconfig \
-	-Dremoting=false \
-	-Dbackend-headless=false \
-	-Dcolor-management-colord=false
+	--with-dtddir=$(STAGING_DIR)/usr/share/wayland \
+	--disable-headless-compositor \
+	--disable-colord \
+	--disable-devdocs \
+	--disable-setuid-install \
+	--enable-autotools
 
-ifeq ($(BR2_PACKAGE_DBUS)$(BR2_PACKAGE_SYSTEMD),yy)
-WESTON_CONF_OPTS += -Dlauncher-logind=true
-WESTON_DEPENDENCIES += dbus systemd
-else
-WESTON_CONF_OPTS += -Dlauncher-logind=false
+WESTON_MAKE_OPTS = \
+	WAYLAND_PROTOCOLS_DATADIR=$(STAGING_DIR)/usr/share/wayland-protocols
+
+# Uses VIDIOC_EXPBUF, only available from 3.8+
+ifeq ($(BR2_TOOLCHAIN_HEADERS_AT_LEAST_3_8),)
+WESTON_CONF_OPTS += --disable-simple-dmabuf-v4l-client
 endif
 
-ifeq ($(BR2_PACKAGE_WEBP),y)
-WESTON_CONF_OPTS += -Dimage-webp=true
-WESTON_DEPENDENCIES += webp
+ifeq ($(BR2_PACKAGE_DBUS),y)
+WESTON_CONF_OPTS += --enable-dbus
+WESTON_DEPENDENCIES += dbus
 else
-WESTON_CONF_OPTS += -Dimage-webp=false
+WESTON_CONF_OPTS += --disable-dbus
 endif
 
 # weston-launch must be u+s root in order to work properly
@@ -41,109 +45,93 @@ endef
 define WESTON_USERS
 	- - weston-launch -1 - - - - Weston launcher group
 endef
-WESTON_CONF_OPTS += -Dweston-launch=true
+WESTON_CONF_OPTS += --enable-weston-launch
 WESTON_DEPENDENCIES += linux-pam
 else
-WESTON_CONF_OPTS += -Dweston-launch=false
-endif
-
-# Uses VIDIOC_EXPBUF, only available from 3.8+
-ifeq ($(BR2_TOOLCHAIN_HEADERS_AT_LEAST_3_8),y)
-WESTON_CONF_OPTS +=	-Dsimple-clients=dmabuf-v4l
-else
-WESTON_CONF_OPTS +=	-Dsimple-clients=
+WESTON_CONF_OPTS += --disable-weston-launch
 endif
 
 ifeq ($(BR2_PACKAGE_HAS_LIBEGL_WAYLAND)$(BR2_PACKAGE_HAS_LIBGLES),yy)
-WESTON_CONF_OPTS += -Drenderer-gl=true
+WESTON_CONF_OPTS += --enable-egl
 WESTON_DEPENDENCIES += libegl libgles
 else
 WESTON_CONF_OPTS += \
-	-Drenderer-gl=false
+	--disable-egl \
+	--disable-simple-dmabuf-drm-client \
+	--disable-simple-egl-clients
 endif
 
 ifeq ($(BR2_PACKAGE_WESTON_RDP),y)
 WESTON_DEPENDENCIES += freerdp
-WESTON_CONF_OPTS += -Dbackend-rdp=true
+WESTON_CONF_OPTS += --enable-rdp-compositor
 else
-WESTON_CONF_OPTS += -Dbackend-rdp=false
+WESTON_CONF_OPTS += --disable-rdp-compositor
 endif
 
 ifeq ($(BR2_PACKAGE_WESTON_FBDEV),y)
-WESTON_CONF_OPTS += -Dbackend-fbdev=true
-ifeq ($(BR2_PACKAGE_WESTON_DEFAULT_BACKEND_FBDEV),y)
-WESTON_CONF_OPTS += -Dbackend-default=fbdev
-endif
+WESTON_CONF_OPTS += \
+	--enable-fbdev-compositor \
+	WESTON_NATIVE_BACKEND=fbdev-backend.so
 else
-WESTON_CONF_OPTS += -Dbackend-fbdev=false
+WESTON_CONF_OPTS += --disable-fbdev-compositor
 endif
 
 ifeq ($(BR2_PACKAGE_WESTON_DRM),y)
-WESTON_CONF_OPTS += -Dbackend-drm=true
-ifeq ($(BR2_PACKAGE_WESTON_DEFAULT_BACKEND_DRM),y)
-WESTON_CONF_OPTS += -Dbackend-default=drm
-endif
+WESTON_CONF_OPTS += \
+	--enable-drm-compositor \
+	WESTON_NATIVE_BACKEND=drm-backend.so
 else
-WESTON_CONF_OPTS += -Dbackend-drm=false
+WESTON_CONF_OPTS += --disable-drm-compositor
 endif
 
 ifeq ($(BR2_PACKAGE_WESTON_X11),y)
-WESTON_CONF_OPTS += -Dbackend-x11=true
-ifeq ($(BR2_PACKAGE_WESTON_DEFAULT_BACKEND_X11),y)
-WESTON_CONF_OPTS += -Dbackend-default=x11
-endif
+WESTON_CONF_OPTS += \
+	--enable-x11-compositor \
+	WESTON_NATIVE_BACKEND=x11-backend.so
 WESTON_DEPENDENCIES += libxcb xlib_libX11
 else
-WESTON_CONF_OPTS += -Dbackend-x11=false
+WESTON_CONF_OPTS += --disable-x11-compositor
 endif
 
 ifeq ($(BR2_PACKAGE_WESTON_XWAYLAND),y)
-WESTON_CONF_OPTS += -Dxwayland=true
+WESTON_CONF_OPTS += --enable-xwayland
 WESTON_DEPENDENCIES += cairo libepoxy libxcb xlib_libX11 xlib_libXcursor
 else
-WESTON_CONF_OPTS += -Dxwayland=false
+WESTON_CONF_OPTS += --disable-xwayland
 endif
 
 ifeq ($(BR2_PACKAGE_LIBVA),y)
-WESTON_CONF_OPTS += -Dbackend-drm-screencast-vaapi=true
+WESTON_CONF_OPTS += --enable-vaapi-recorder
 WESTON_DEPENDENCIES += libva
 else
-WESTON_CONF_OPTS += -Dbackend-drm-screencast-vaapi=false
+WESTON_CONF_OPTS += --disable-vaapi-recorder
 endif
 
 ifeq ($(BR2_PACKAGE_LCMS2),y)
-WESTON_CONF_OPTS += -Dcolor-management-lcms=true
+WESTON_CONF_OPTS += --enable-lcms
 WESTON_DEPENDENCIES += lcms2
 else
-WESTON_CONF_OPTS += -Dcolor-management-lcms=false
+WESTON_CONF_OPTS += --disable-lcms
 endif
 
 ifeq ($(BR2_PACKAGE_SYSTEMD),y)
-WESTON_CONF_OPTS += -Dsystemd=true
+WESTON_CONF_OPTS += --enable-systemd-login --enable-systemd-notify
 WESTON_DEPENDENCIES += systemd
 else
-WESTON_CONF_OPTS += -Dsystemd=false
+WESTON_CONF_OPTS += --disable-systemd-login --disable-systemd-notify
 endif
 
 ifeq ($(BR2_PACKAGE_LIBXML2),y)
-WESTON_CONF_OPTS += -Dtest-junit-xml=true
+WESTON_CONF_OPTS += --enable-junit-xml
 WESTON_DEPENDENCIES += libxml2
 else
-WESTON_CONF_OPTS += -Dtest-junit-xml=false
-endif
-
-ifeq ($(BR2_PACKAGE_PIPEWIRE)$(BR2_PACKAGE_WESTON_DRM),yy)
-WESTON_CONF_OPTS += -Dpipewire=true
-WESTON_DEPENDENCIES += pipewire
-else
-WESTON_CONF_OPTS += -Dpipewire=false
+WESTON_CONF_OPTS += --disable-junit-xml
 endif
 
 ifeq ($(BR2_PACKAGE_WESTON_DEMO_CLIENTS),y)
-WESTON_CONF_OPTS += -Ddemo-clients=true
-WESTON_DEPENDENCIES += pango
+WESTON_CONF_OPTS += --enable-demo-clients-install
 else
-WESTON_CONF_OPTS += -Ddemo-clients=false
+WESTON_CONF_OPTS += --disable-demo-clients-install
 endif
 
-$(eval $(meson-package))
+$(eval $(autotools-package))
