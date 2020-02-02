e1215b7bc0fc28603b363bec2aeaf60eaafd1f6c
Adam Duskett <aduskett@gmail.com>
2020-02-02 12:39:31 -0800

    package/{openjdk,openjdk-bin}: bump version to 13.0.2+8
    Other changes:
      - Add --with-stdc++lib=dynamic to openjdk.mk or else openjdk will fail to
        build because it defaults to looking for a static libstdc++ library.
    
    Signed-off-by: Adam Duskett <aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/openjdk/openjdk.mk
+++ package/openjdk/openjdk.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-OPENJDK_VERSION_MAJOR = 12.0.2
-OPENJDK_VERSION_MINOR = 10
+OPENJDK_VERSION_MAJOR = 13.0.2
+OPENJDK_VERSION_MINOR = 8
 OPENJDK_VERSION = $(OPENJDK_VERSION_MAJOR)+$(OPENJDK_VERSION_MINOR)
 OPENJDK_SOURCE = jdk-$(OPENJDK_VERSION).tar.gz
-OPENJDK_SITE = https://hg.openjdk.java.net/jdk-updates/jdk12u/archive
+OPENJDK_SITE = https://hg.openjdk.java.net/jdk-updates/jdk13u/archive
 OPENJDK_LICENSE = GPL-2.0+ with exception
 OPENJDK_LICENSE_FILES = LICENSE
 
@@ -70,6 +70,7 @@ OPENJDK_CONF_OPTS = \
 	--enable-unlimited-crypto \
 	--openjdk-target=$(GNU_TARGET_NAME) \
 	--with-boot-jdk=$(HOST_DIR) \
+	--with-stdc++lib=dynamic \
 	--with-debug-level=release \
 	--with-devkit=$(HOST_DIR) \
 	--with-extra-cflags="$(TARGET_CFLAGS)" \
