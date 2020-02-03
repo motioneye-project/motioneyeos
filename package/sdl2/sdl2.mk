a08abc2e3dbeb125db792f9e3540c8901b0dc50e
Michael Fischer <mf@go-sys.de>
2020-02-03 14:48:22 +0000

    package/sdl2: bump version to 2.0.10
    Adjust license hash as the copyright year was changed:
    -Copyright (C) 1997-2018 Sam Lantinga <slouken@libsdl.org>
    +Copyright (C) 1997-2019 Sam Lantinga <slouken@libsdl.org>
    
    Signed-off-by: Michael Fischer <mf@go-sys.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/sdl2/sdl2.mk
+++ package/sdl2/sdl2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SDL2_VERSION = 2.0.9
+SDL2_VERSION = 2.0.10
 SDL2_SOURCE = SDL2-$(SDL2_VERSION).tar.gz
 SDL2_SITE = http://www.libsdl.org/release
 SDL2_LICENSE = Zlib
