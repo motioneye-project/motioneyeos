d7512ee0fc927343ba360f773ed7f917a2d778e7
Grzegorz Blach <grzegorz@blach.pl>
2020-02-03 23:12:19 +0100

    package/python-redis: Bump to version 3.4.1
    Signed-off-by: Grzegorz Blach <grzegorz@blach.pl>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-redis/python-redis.mk
+++ package/python-redis/python-redis.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_REDIS_VERSION = 3.3.11
+PYTHON_REDIS_VERSION = 3.4.1
 PYTHON_REDIS_SOURCE = redis-$(PYTHON_REDIS_VERSION).tar.gz
-PYTHON_REDIS_SITE = https://files.pythonhosted.org/packages/06/ca/00557c74279d2f256d3c42cabf237631355f3a132e4c74c2000e6647ad98
+PYTHON_REDIS_SITE = https://files.pythonhosted.org/packages/ef/2e/2c0f59891db7db087a7eeaa79bc7c7f2c039e71a2b5b0a41391e9d462926
 PYTHON_REDIS_SETUP_TYPE = setuptools
 PYTHON_REDIS_LICENSE = MIT
 PYTHON_REDIS_LICENSE_FILES = LICENSE
