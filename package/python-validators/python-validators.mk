776b8d44ed7642884b582d4c19379b04ab144588
Peter Korsgaard <peter@korsgaard.com>
2020-02-03 10:27:05 +0100

    package/python-validators: bump version to 0.14.2
    Fixes an issue with domain name validation.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-validators/python-validators.mk
+++ package/python-validators/python-validators.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_VALIDATORS_VERSION = 0.14.1
+PYTHON_VALIDATORS_VERSION = 0.14.2
 PYTHON_VALIDATORS_SOURCE = validators-$(PYTHON_VALIDATORS_VERSION).tar.gz
-PYTHON_VALIDATORS_SITE = https://files.pythonhosted.org/packages/72/8a/f99287daae8cfef938e6eec785f6e259bf6bad93269d5398bb546d5b1563
+PYTHON_VALIDATORS_SITE = https://files.pythonhosted.org/packages/4d/56/9b48c918ef118ea12b90f227c4498ed4703b418bdd8fb49479dfcbeae4ef
 PYTHON_VALIDATORS_SETUP_TYPE = setuptools
 PYTHON_VALIDATORS_LICENSE = BSD
 PYTHON_VALIDATORS_LICENSE_FILES = LICENSE
