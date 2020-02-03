332d96091bff45bca1ab1e2c25780a5bef2e57f7
Adam Duskett <aduskett@gmail.com>
2020-02-03 02:21:10 -0800

    package/ninja: change package type to host-cmake
    Ninja 1.10.0 now supports cmake as a build system. This change makes the make
    file much more straightforward.
    
    The host-python2 dependency is no longer needed as it is no longer
    used during the build. We however need to keep install commands as
    there is no installation step planned in CMakeLists.txt.
    
    Signed-off-by: Adam Duskett <aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/ninja/ninja.mk
+++ package/ninja/ninja.mk
@@ -9,19 +9,8 @@ NINJA_SITE = $(call github,ninja-build,ninja,v$(NINJA_VERSION))
 NINJA_LICENSE = Apache-2.0
 NINJA_LICENSE_FILES = COPYING
 
-# Although Ninja supports both Python2 and Python3, we enforce Python3
-# on the host for the following reason: Meson is the only package
-# using Ninja so far and Meson requires Python3. In this way, we
-# prevent both Python2 and Python3 from being created on the host,
-# which is time consuming and without benefit.
-HOST_NINJA_DEPENDENCIES = host-python3
-
-define HOST_NINJA_BUILD_CMDS
-	(cd $(@D); $(HOST_DIR)/bin/python3 configure.py --bootstrap)
-endef
-
 define HOST_NINJA_INSTALL_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/ninja $(HOST_DIR)/bin/ninja
 endef
 
-$(eval $(host-generic-package))
+$(eval $(host-cmake-package))
