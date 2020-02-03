44e53c6b4843ca3f78eac5216cd737b8e27f60df
Peter Korsgaard <peter@korsgaard.com>
2020-02-03 12:28:21 +0100

    package/python-django: security bump to version 3.0.3
    Fixes the following security issues:
    
    - CVE-2020-7471: Potential SQL injection via StringAgg(delimiter)
      django.contrib.postgres.aggregates.StringAgg aggregation function was
      subject to SQL injection, using a suitably crafted delimiter.
    
    For more details, see the advisory:
    https://www.djangoproject.com/weblog/2020/feb/03/security-releases/
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-django/python-django.mk
+++ package/python-django/python-django.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_DJANGO_VERSION = 3.0.2
+PYTHON_DJANGO_VERSION = 3.0.3
 PYTHON_DJANGO_SOURCE = Django-$(PYTHON_DJANGO_VERSION).tar.gz
 # The official Django site has an unpractical URL
-PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/c5/c1/5b901e21114b5dd9233726c2975c0aa7e9f48f63e41ec95d8777721d8aff
+PYTHON_DJANGO_SITE = https://files.pythonhosted.org/packages/3d/21/316d435bf8bd6f355be6b5765da91394fb38f405e5bea6680e411e4d470c
 PYTHON_DJANGO_LICENSE = BSD-3-Clause
 PYTHON_DJANGO_LICENSE_FILES = LICENSE
 PYTHON_DJANGO_SETUP_TYPE = setuptools
