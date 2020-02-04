49c112e16cabc8eacaa74c2593e7a94834ace198
Titouan Christophe <titouan.christophe@railnova.eu>
2020-02-04 14:31:56 +0100

    package/norm: switch to Github hosted archive
    The original download server (downloads.pf.itd.nrl.navy.mil)
    is quite unreliable, which lead to the download being very slow
    or even failing. Since the project is now hosted on Github, we
    switch to that site, which makes the download tractable.
    
    Update the hash, and use the opportunity to realign fields to the new
    spacing convention.
    
    Signed-off-by: Titouan Christophe <titouan.christophe@railnova.eu>
    [yann.morin.1998@free.fr: spaces in hash file]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/norm/norm.mk
+++ package/norm/norm.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 NORM_VERSION = 1.5.8
-NORM_SITE = http://downloads.pf.itd.nrl.navy.mil/norm
-NORM_SOURCE = src-norm-$(NORM_VERSION).tgz
+NORM_SITE = https://github.com/USNavalResearchLaboratory/norm/releases/download/v$(NORM_VERSION)
+NORM_SOURCE = src-norm-with-protolib-$(NORM_VERSION).tgz
 NORM_INSTALL_STAGING = YES
 NORM_LICENSE = NRL License
 NORM_LICENSE_FILES = LICENSE.TXT
