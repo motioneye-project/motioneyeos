8f9f48acd2db27863ca8fc0a4f1377aa2b07c886
Angelo Compagnucci <angelo@amarulasolutions.com>
2020-02-04 15:24:47 +0100

    package/grep: fix egrep/fgrep shebang
    egrep/fgrep are wrapper scripts, calling the grep binary with the correct
    arguments.
    
    The shell wrappers use the value of SHELL at build time as the shebang value
    in these wrapper scripts, which in Buildroot points to /bin/bash.
    
    The target may not have bash available, causing runtime errors.
    
    As a fix, add a post-install hook to change this to /bin/sh.
    
    If the target does not have /bin/sh, simply remove the aliases.
    
    Signed-off-by: Angelo Compagnucci <angelo@amarulasolutions.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/grep/grep.mk
+++ package/grep/grep.mk
@@ -13,6 +13,23 @@ GREP_DEPENDENCIES = $(TARGET_NLS_DEPENDENCIES)
 # install into /bin like busybox grep
 GREP_CONF_OPTS = --exec-prefix=/
 
+ifeq ($(BR2_SYSTEM_BIN_SH_NONE),y)
+
+define GREP_REMOVE_ALIAS
+	$(RM) $(TARGET_DIR)/bin/[fe]grep
+endef
+GREP_POST_INSTALL_TARGET_HOOKS += GREP_REMOVE_ALIAS
+
+else
+
+# ensure egrep/fgrep shell wrappers use #!/bin/sh
+define GREP_FIXUP_SHEBANG
+	$(SED) 's/bash$$/sh/' $(TARGET_DIR)/bin/[fe]grep
+endef
+GREP_POST_INSTALL_TARGET_HOOKS += GREP_FIXUP_SHEBANG
+
+endif
+
 # link with iconv if enabled
 ifeq ($(BR2_PACKAGE_LIBICONV),y)
 GREP_CONF_ENV += LIBS=-liconv
