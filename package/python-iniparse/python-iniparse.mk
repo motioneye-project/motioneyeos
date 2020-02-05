ca328f80da2da66c0c6940cd4908ab868025f670
Titouan Christophe <titouan.christophe@railnova.eu>
2020-02-05 15:11:47 +0100

    package/python-iniparse: bump to version 0.5
    This version brings Python 3.x support, so we make the package
    available with Python 3.x. Also, iniparse now requires six, which has
    been detected thanks to the runtime tests.
    
    Signed-off-by: Titouan Christophe <titouan.christophe@railnova.eu>
    [Thomas: drop depends on python || python3]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-iniparse/python-iniparse.mk
+++ package/python-iniparse/python-iniparse.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_INIPARSE_VERSION = 0.4
+PYTHON_INIPARSE_VERSION = 0.5
 PYTHON_INIPARSE_SOURCE = iniparse-$(PYTHON_INIPARSE_VERSION).tar.gz
 PYTHON_INIPARSE_SITE = https://pypi.python.org/packages/source/i/iniparse
 PYTHON_INIPARSE_LICENSE = Python-2.0, MIT
 PYTHON_INIPARSE_LICENSE_FILES = LICENSE-PSF LICENSE
-PYTHON_INIPARSE_SETUP_TYPE = distutils
+PYTHON_INIPARSE_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
