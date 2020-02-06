f933aebc74ac8fdb103b29feb6a14078e13defc2
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-02-06 19:19:38 +0100

    package/kodi-peripheral-joystick: bump version to 1.4.9-Leia
    Switched _LICENSE_FILES to debian/copyright.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/kodi-peripheral-joystick/kodi-peripheral-joystick.mk
+++ package/kodi-peripheral-joystick/kodi-peripheral-joystick.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-KODI_PERIPHERAL_JOYSTICK_VERSION = 1.4.8-Leia
+KODI_PERIPHERAL_JOYSTICK_VERSION = 1.4.9-Leia
 KODI_PERIPHERAL_JOYSTICK_SITE = $(call github,xbmc,peripheral.joystick,$(KODI_PERIPHERAL_JOYSTICK_VERSION))
 KODI_PERIPHERAL_JOYSTICK_LICENSE = GPL-2.0+
-KODI_PERIPHERAL_JOYSTICK_LICENSE_FILES = src/addon.cpp
+KODI_PERIPHERAL_JOYSTICK_LICENSE_FILES = debian/copyright
 KODI_PERIPHERAL_JOYSTICK_DEPENDENCIES = kodi tinyxml udev
 
 $(eval $(cmake-package))
