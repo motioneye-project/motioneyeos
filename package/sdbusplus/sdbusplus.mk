6bf74ce3dbfec8979e379bc1b919f29d09f0d87b
Michael Walle <michael@walle.cc>
2020-02-10 20:11:30 +0100

    package/sdbusplus: create m4 directory before autoreconf
    Commit d255b67972b4b7f27572581fe0c8c8aa03d850c8 fixed the handling of
    the a package local m4/ directory which might be missing. But this
    only works if it is the very first argument. But for this package this
    is not possible because we already occupy this with the extra include
    directory for autoconf-archive. Bring back the hook to create the m4/
    directory to fix this.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/dc907421a343b8523b14fc9a846e0caf7abe630c/
    
    Signed-off-by: Michael Walle <michael@walle.cc>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/sdbusplus/sdbusplus.mk
+++ package/sdbusplus/sdbusplus.mk
@@ -22,5 +22,14 @@ SDBUSPLUS_INSTALL_STAGING = YES
 SDBUSPLUS_LICENSE = Apache-2.0
 SDBUSPLUS_LICENSE_FILES = LICENSE
 
+# Autoreconf is missing the m4/ directory, which might actually be missing
+# iff it was the first argument, but unfortunately we are overriding the
+# first include directory above. Thus we need that hook here.
+define SDBUSPLUS_CREATE_M4
+       mkdir -p $(@D)/m4
+endef
+SDBUSPLUS_POST_PATCH_HOOKS += SDBUSPLUS_CREATE_M4
+HOST_SDBUSPLUS_POST_PATCH_HOOKS += SDBUSPLUS_CREATE_M4
+
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
