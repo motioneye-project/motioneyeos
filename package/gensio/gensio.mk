5349c37e726263a3bd3cf8cb24fce0bba144578f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-02-12 19:06:53 +0100

    package/gensio: bump to version 1.5.1
    - Update indentation of hash file (2 spaces)
    - This will fix a build failure without threads thanks to
      https://github.com/cminyard/gensio/commit/8918de5b30f90b826c48064e9ee92304b63ffe85
      and associated upstream patch
    
    Fixes:
     - http://autobuild.buildroot.org/results/e94d0e0b46afc1223a74bcc471909f4adef0d6f3
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gensio/gensio.mk
+++ package/gensio/gensio.mk
@@ -4,11 +4,13 @@
 #
 ################################################################################
 
-GENSIO_VERSION = 1.4.1
-GENSIO_SITE = http://downloads.sourceforge.net/project/ser2net
+GENSIO_VERSION = 1.5.1
+GENSIO_SITE = http://downloads.sourceforge.net/project/ser2net/ser2net
 GENSIO_LICENSE = LGPL-2.1+ (library), GPL-2.0+ (tools)
 GENSIO_LICENSE_FILES = COPYING.LIB COPYING
 GENSIO_INSTALL_STAGING = YES
+# We're patching configure.ac
+GENSIO_AUTORECONF = YES
 GENSIO_CONF_OPTS = \
 	--without-openipmi \
 	--without-swig \
