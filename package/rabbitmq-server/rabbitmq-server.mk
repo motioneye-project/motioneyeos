27df8b7b0d7c6c331d2e4fe7b6ea2bd1596acd75
Frank Vanbever <frank.vanbever@essensium.com>
2020-02-13 16:14:14 +0100

    package/rabbitmq-server: bump version to 3.8.2
    RabbitMQ now depends on Elixir for the build system.
    The log directory is required for the server to start with the default
    configuration. This behaviour was changed in v3.7 with the move to erlang-lager
    as the logging library.
    
    License changes:
    - LICENSE: Added clarification about the licensing of a number of included
      dependencies and a URL changed to HTTPS. License remains MPL-1.1
    - LICENSE-BSD-base64js: URL changed to https (license text is actually MIT)
    - LICENSE-MIT-Flot: year update
    - LICENSE-MIT-JQuery164: URL changed to https
    - LICENSE-MPL-RabbitMQ: URL changed to https, year update
    - LICENSE-MPL2: URL changed to https
    
    Removed licenses:
    - LICENSE-APL2-Rebar: Removed dependency
    - LICENSE-BSD-gl-Matrix: Removed dependency
    - LICENSE-ERL-OTP: Removed license
    - LICENSE-MIT-Mochiweb: Removed dependency
    - LICENSE-MIT-SockJS: Removed dependency
    
    New licenses:
    - LICENSE-APACHE2-excanvas: new bundled dependency (Apache 2)
    - LICENSE-BSD-recon: new bundled dependency (BSD 3-clause)
    - LICENSE-erlcloud: new bundled dependency (BSD 2-clause)
    - LICENSE-httpc_aws: new bundled dependency (BSD 3-clause)
    - LICENSE-ISC-cowboy: new bundled dependency (ISC)
    - LICENSE-MIT-EJS: new bundled dependency (MIT)
    - LICENSE-MIT-Erlware-commons: license was present in 3.6.6 but not included in
      the list
    - LICENSE-MIT-jQuery: new bundled dependency (MIT)
    - LICENSE-MIT-Sammy: new bundled dependency (MIT)
    - LICENSE-rabbitmq_aws: new bundled dependency (BSD 3-clause)
    
    Signed-off-by: Frank Vanbever <frank.vanbever@essensium.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/rabbitmq-server/rabbitmq-server.mk
+++ package/rabbitmq-server/rabbitmq-server.mk
@@ -4,29 +4,36 @@
 #
 ################################################################################
 
-RABBITMQ_SERVER_VERSION = 3.6.6
-RABBITMQ_SERVER_SITE = http://www.rabbitmq.com/releases/rabbitmq-server/v$(RABBITMQ_SERVER_VERSION)
+RABBITMQ_SERVER_VERSION = 3.8.2
+RABBITMQ_SERVER_SITE = https://github.com/rabbitmq/rabbitmq-server/releases/download/v$(RABBITMQ_SERVER_VERSION)
 RABBITMQ_SERVER_SOURCE = rabbitmq-server-$(RABBITMQ_SERVER_VERSION).tar.xz
-RABBITMQ_SERVER_LICENSE = MPL-1.1, Apache-2.0, BSD-2-Clause, EPL, MIT, MPL-2.0
+RABBITMQ_SERVER_LICENSE = MPL-1.1, Apache-2.0, BSD-3-Clause, BSD-2-Clause, MIT, MPL-2.0, ISC
 RABBITMQ_SERVER_LICENSE_FILES = \
 	LICENSE \
+	LICENSE-APACHE2 \
+	LICENSE-APACHE2-excanvas \
 	LICENSE-APACHE2-ExplorerCanvas \
-	LICENSE-APL2-Rebar \
 	LICENSE-APL2-Stomp-Websocket \
 	LICENSE-BSD-base64js \
-	LICENSE-BSD-glMatrix \
-	LICENSE-EPL-OTP
+	LICENSE-BSD-recon \
+	LICENSE-erlcloud \
+	LICENSE-httpc_aws \
+	LICENSE-ISC-cowboy \
+	LICENSE-MIT-EJS \
 	LICENSE-MIT-EJS10 \
+	LICENSE-MIT-Erlware-Commons \
 	LICENSE-MIT-Flot \
+	LICENSE-MIT-jQuery \
 	LICENSE-MIT-jQuery164 \
 	LICENSE-MIT-Mochi \
-	LICENSE-MIT-Mochiweb \
+	LICENSE-MIT-Sammy \
 	LICENSE-MIT-Sammy060 \
-	LICENSE-MIT-SockJS \
+	LICENSE-MPL \
 	LICENSE-MPL-RabbitMQ \
-	LICENSE-MPL2
+	LICENSE-MPL2 \
+	LICENSE-rabbitmq_aws
 
-RABBITMQ_SERVER_DEPENDENCIES = host-libxslt host-zip erlang libxslt
+RABBITMQ_SERVER_DEPENDENCIES = host-elixir host-libxslt host-zip erlang libxslt
 RABBITMQ_SERVER_TARGET_BINS = rabbitmq-plugins rabbitmq-server rabbitmqctl rabbitmq-env rabbitmq-defaults
 
 define RABBITMQ_SERVER_BUILD_CMDS
