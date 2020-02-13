8371f096225d12ac66fd46edacb1e90d875af423
James Hilliard <james.hilliard1@gmail.com>
2020-02-12 23:26:14 -0700

    package/python-cbor2: new package
    Pure Python CBOR (de)serializer with extensive tag support.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/python-cbor2/python-cbor2.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-cbor2
+#
+################################################################################
+
+PYTHON_CBOR2_VERSION = 5.0.1
+PYTHON_CBOR2_SOURCE = cbor2-$(PYTHON_CBOR2_VERSION).tar.gz
+PYTHON_CBOR2_SITE = https://files.pythonhosted.org/packages/3e/2c/188d040bd4cf99012d0e847a3d95bf1ef1f783859b09eb9f736950af84f0
+PYTHON_CBOR2_SETUP_TYPE = setuptools
+PYTHON_CBOR2_LICENSE = MIT
+PYTHON_CBOR2_LICENSE_FILES = LICENSE.txt
+
+$(eval $(python-package))
