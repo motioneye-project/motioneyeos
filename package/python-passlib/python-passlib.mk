94c48ead6700f8fb5855f177d21857fc868fa860
James Hilliard <james.hilliard1@gmail.com>
2020-02-12 22:23:47 -0700

    package/python-passlib: bump to version 1.7.2
    License hash change is due to date update.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-passlib/python-passlib.mk
+++ package/python-passlib/python-passlib.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PASSLIB_VERSION = 1.7.1
+PYTHON_PASSLIB_VERSION = 1.7.2
 PYTHON_PASSLIB_SOURCE = passlib-$(PYTHON_PASSLIB_VERSION).tar.gz
-PYTHON_PASSLIB_SITE = https://files.pythonhosted.org/packages/25/4b/6fbfc66aabb3017cd8c3bd97b37f769d7503ead2899bf76e570eb91270de
+PYTHON_PASSLIB_SITE = https://files.pythonhosted.org/packages/6d/6b/4bfca0c13506535289b58f9c9761d20f56ed89439bfe6b8e07416ce58ee1
 PYTHON_PASSLIB_SETUP_TYPE = setuptools
 PYTHON_PASSLIB_LICENSE = BSD-3-Clause
 PYTHON_PASSLIB_LICENSE_FILES = LICENSE
