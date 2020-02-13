44a9f390efc282f41052b39716eab04cd76b1680
James Hilliard <james.hilliard1@gmail.com>
2020-02-12 21:45:52 -0700

    package/python-crossbar: bump to version 20.1.2
    Rebased avoid intentional syntax error patch.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-crossbar/python-crossbar.mk
+++ package/python-crossbar/python-crossbar.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_CROSSBAR_VERSION = 19.1.2
+PYTHON_CROSSBAR_VERSION = 20.1.2
 PYTHON_CROSSBAR_SOURCE = crossbar-$(PYTHON_CROSSBAR_VERSION).tar.gz
-PYTHON_CROSSBAR_SITE = https://files.pythonhosted.org/packages/86/19/54c7bd9da1ac70643b9ae7d3d4592a73391f62fcb376fcd30f8328c1c5ce
+PYTHON_CROSSBAR_SITE = https://files.pythonhosted.org/packages/2a/cd/e8be30a9ba4db52dc04e530f19a24d225d743aa36f5074ec0b3ef316637a
 PYTHON_CROSSBAR_LICENSE = AGPL-3.0
-PYTHON_CROSSBAR_LICENSE_FILES = LICENSE
+PYTHON_CROSSBAR_LICENSE_FILES = crossbar/LICENSE
 PYTHON_CROSSBAR_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
