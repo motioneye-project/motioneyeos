9b15ef3505f7db2ecc5c50cbca189796aca7899d
Peter Korsgaard <peter@korsgaard.com>
2020-02-13 21:35:27 +0100

    package/go: bump version to 1.13.8
    Includes fixes to the runtime, the crypto/x509, and net/http
    packages.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.13.7
+GO_VERSION = 1.13.8
 GO_SITE = https://storage.googleapis.com/golang
 GO_SOURCE = go$(GO_VERSION).src.tar.gz
 
