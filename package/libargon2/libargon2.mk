019f9a8bb440543b1f4c5c1d0afe04b48a72bdbd
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-02-15 11:02:47 +0100

    package/libargon2: bump to version 20190702
    - There is no incompatibility of this version with php, the issue was
      due to libargon2 itself that installs library to lib/x86_64-linux-gnu
      on Linux x86_64 host since
      https://github.com/P-H-C/phc-winner-argon2/commit/d3639142fa71f8095ee36e64068f71b3c97baf6d
    - Update indentation of hash file (two spaces)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/libargon2/libargon2.mk
+++ package/libargon2/libargon2.mk
@@ -4,18 +4,19 @@
 #
 ################################################################################
 
-# 20190702 is not recognized by php7.3
-LIBARGON2_VERSION = 20171227
+LIBARGON2_VERSION = 20190702
 LIBARGON2_SITE = $(call github,P-H-C,phc-winner-argon2,$(LIBARGON2_VERSION))
 LIBARGON2_LICENSE = CC0-1.0 or Apache-2.0
 LIBARGON2_LICENSE_FILES = LICENSE
 LIBARGON2_INSTALL_STAGING = YES
 
+LIBARGON2_OPTS = LIBRARY_REL=lib
+
 # GCC_TARGET_ARCH is not defined for all architectures, but libargon2
 # only uses it to detect if some x86 optimizations can be used, and
 # GCC_TARGET_ARCH is defined on x86.
 ifneq ($(GCC_TARGET_ARCH),)
-LIBARGON2_OPTS = OPTTARGET=$(GCC_TARGET_ARCH)
+LIBARGON2_OPTS += OPTTARGET=$(GCC_TARGET_ARCH)
 endif
 
 ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),)
@@ -29,12 +30,12 @@ endef
 
 define LIBARGON2_INSTALL_STAGING_CMDS
 	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
-		DESTDIR=$(STAGING_DIR) install
+		$(LIBARGON2_OPTS) DESTDIR=$(STAGING_DIR) install
 endef
 
 define LIBARGON2_INSTALL_TARGET_CMDS
 	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
-		DESTDIR=$(TARGET_DIR) install
+		$(LIBARGON2_OPTS) DESTDIR=$(TARGET_DIR) install
 endef
 
 $(eval $(generic-package))
