c40501b9ce272c79c94fa9c7a5126b8fd4dc1df3
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-02-15 12:27:34 +0100

    package/libgpg-error: bump to version 1.37
    - Remove patch (already in version)
    - Update indentation of hash file (two spaces)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libgpg-error/libgpg-error.mk
+++ package/libgpg-error/libgpg-error.mk
@@ -4,14 +4,12 @@
 #
 ################################################################################
 
-LIBGPG_ERROR_VERSION = 1.36
+LIBGPG_ERROR_VERSION = 1.37
 LIBGPG_ERROR_SITE = https://www.gnupg.org/ftp/gcrypt/libgpg-error
 LIBGPG_ERROR_SOURCE = libgpg-error-$(LIBGPG_ERROR_VERSION).tar.bz2
 LIBGPG_ERROR_LICENSE = GPL-2.0+, LGPL-2.1+
 LIBGPG_ERROR_LICENSE_FILES = COPYING COPYING.LIB
 LIBGPG_ERROR_INSTALL_STAGING = YES
-# 0001-gawk5-support.patch
-LIBGPG_ERROR_AUTORECONF = YES
 LIBGPG_ERROR_CONFIG_SCRIPTS = gpg-error-config
 LIBGPG_ERROR_DEPENDENCIES = $(TARGET_NLS_DEPENDENCIES)
 LIBGPG_ERROR_CONF_OPTS = --disable-tests \
