d3c552f662217c6a408f3f534f01c5819695cb3c
Titouan Christophe <titouan.christophe@railnova.eu>
2020-02-17 15:23:07 +0100

    package/{avro-c, python-avro}: bump to version 1.9.2
    - Drop patch for avro-c that was applied upstream
     - Fix inconsistency in PYTHON_AVRO_SITE
     - Adopt new spacing convention in .hash files
     - Add upstream patch for python-avro
     - Add patch to remove installation-time linting in python-avro
    
    Signed-off-by: Titouan Christophe <titouan.christophe@railnova.eu>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/avro-c/avro-c.mk
+++ package/avro-c/avro-c.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AVRO_C_VERSION = 1.9.1
+AVRO_C_VERSION = 1.9.2
 AVRO_C_SITE = https://www-eu.apache.org/dist/avro/avro-$(AVRO_C_VERSION)/c
 AVRO_C_LICENSE = Apache-2.0
 AVRO_C_LICENSE_FILES = LICENSE
--- package/python-avro/python-avro.mk
+++ package/python-avro/python-avro.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 PYTHON_AVRO_VERSION = $(AVRO_C_VERSION)
-PYTHON_AVRO_SITE = https://www-eu.apache.org/dist/avro/avro-1.9.1/py3
+PYTHON_AVRO_SITE = https://www-eu.apache.org/dist/avro/avro-$(PYTHON_AVRO_VERSION)/py3
 PYTHON_AVRO_SOURCE = avro-python3-$(PYTHON_AVRO_VERSION).tar.gz
 PYTHON_AVRO_LICENSE = Apache-2.0
 PYTHON_AVRO_LICENSE_FILES = avro/LICENSE
