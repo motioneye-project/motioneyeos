066d552499fac9bab41971530b114776b9ca27c8
Nicola Di Lieto <nicola.dilieto@gmail.com>
2020-02-18 21:12:17 +0100

    package/uacme: bump version to 1.0.22
    Signed-off-by: Nicola Di Lieto <nicola.dilieto@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/uacme/uacme.mk
+++ package/uacme/uacme.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UACME_VERSION = 1.0.21
+UACME_VERSION = 1.0.22
 # Released versions are on branch upstream/latest, tagged as
 # upstream/X.Y.Z Do not use vX.Y.Z tags from master, as they do not
 # include .tarball-version
