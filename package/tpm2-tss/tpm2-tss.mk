950e81fd173c67c25399aa1d88112f3c19809edd
Peter Korsgaard <peter@korsgaard.com>
2020-02-20 22:45:30 +0100

    package/tpm2-tss: bump to version 2.3.3
    Bugfix release, fixing a number of issues:
    
    - Fixed mixing salted and unsalted sessions in the same ESAPI context
    - Removed use of VLAs from TPML marshal code
    - Added check for object node before calling compute_session_value function
    - Fixed auth calculation in Esys_StartAuthSession called with optional parameters
    - Fixed compute_encrypted_salt error handling in Esys_StartAuthSession
    - Fixed exported symbols map for libtss2-mu
    
    The 2.3.3 tarball accidently contains a Makefile-fuzz-generated.am with
    content from a fuzz testing run (rather than an empty file as in earlier
    releases), confusing autoreconf together with our
    0001-configure-Only-use-CXX-when-fuzzing.patch.
    
    Work around that by adding a post-patch hook to truncate the file.  The
    issue has been reported upstream and the release logic has been changed to
    ensure this does not happen again for future releases:
    
    https://github.com/tstruk/tpm2-tss/commit/d163041e3bafa23c0be89c82a99b8501634ebdb4
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/tpm2-tss/tpm2-tss.mk
+++ package/tpm2-tss/tpm2-tss.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TPM2_TSS_VERSION = 2.3.2
+TPM2_TSS_VERSION = 2.3.3
 TPM2_TSS_SITE = https://github.com/tpm2-software/tpm2-tss/releases/download/$(TPM2_TSS_VERSION)
 TPM2_TSS_LICENSE = BSD-2-Clause
 TPM2_TSS_LICENSE_FILES = LICENSE
@@ -16,5 +16,14 @@ TPM2_TSS_AUTORECONF = YES
 
 # uses C99 code but forgets to pass -std=c99 when --disable-defaultflags is used
 TPM2_TSS_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -std=c99"
+
+# The 2.3.3 tarball accidently contains a Makefile-fuzz-generated.am
+# with content from a fuzz testing run rather than an empty file,
+# confusing autoreconf with
+# 0001-configure-Only-use-CXX-when-fuzzing.patch
+define TPM2_TSS_TRUNCATE_MAKEFILE_FUZZ_GENERATED_AM
+	truncate -s 0 $(@D)/Makefile-fuzz-generated.am
+endef
+TPM2_TSS_POST_PATCH_HOOKS += TPM2_TSS_TRUNCATE_MAKEFILE_FUZZ_GENERATED_AM
 
 $(eval $(autotools-package))
