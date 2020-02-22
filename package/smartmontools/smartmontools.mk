0910230231e66c1339b18e7b8326745760ef3c0e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-02-22 23:08:55 +0100

    package/smartmontools: bump to version 7.1
    - systemd optional dependency has been added in version 7.0
    - Update indentation of hash file (two spaces)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/smartmontools/smartmontools.mk
+++ package/smartmontools/smartmontools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SMARTMONTOOLS_VERSION = 6.5
+SMARTMONTOOLS_VERSION = 7.1
 SMARTMONTOOLS_SITE = http://downloads.sourceforge.net/project/smartmontools/smartmontools/$(SMARTMONTOOLS_VERSION)
 SMARTMONTOOLS_LICENSE = GPL-2.0+
 SMARTMONTOOLS_LICENSE_FILES = COPYING
@@ -21,6 +21,13 @@ SMARTMONTOOLS_CONF_OPTS += --with-selinux
 SMARTMONTOOLS_DEPENDENCIES += libselinux
 else
 SMARTMONTOOLS_CONF_OPTS += --without-selinux
+endif
+
+ifeq ($(BR2_PACKAGE_SYSTEMD),y)
+SMARTMONTOOLS_CONF_OPTS += --with-libsystemd
+SMARTMONTOOLS_DEPENDENCIES += systemd
+else
+SMARTMONTOOLS_CONF_OPTS += --without-libsystemd
 endif
 
 $(eval $(autotools-package))
