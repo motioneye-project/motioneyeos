dc171f123c6b320c4930ba00debeb34fede82d99
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-02-23 16:36:38 +0100

    package/smartmontools: fix build without stack-protector
    Fixes:
     - http://autobuild.buildroot.org/results/0de9f2a69fa2a39164211299f8a429d2fec6935a
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/smartmontools/smartmontools.mk
+++ package/smartmontools/smartmontools.mk
@@ -8,6 +8,8 @@ SMARTMONTOOLS_VERSION = 6.5
 SMARTMONTOOLS_SITE = http://downloads.sourceforge.net/project/smartmontools/smartmontools/$(SMARTMONTOOLS_VERSION)
 SMARTMONTOOLS_LICENSE = GPL-2.0+
 SMARTMONTOOLS_LICENSE_FILES = COPYING
+# 0001-configure.ac-fix-stack-protector-detection.patch
+SMARTMONTOOLS_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_LIBCAP_NG),y)
 SMARTMONTOOLS_CONF_OPTS += --with-libcap-ng
