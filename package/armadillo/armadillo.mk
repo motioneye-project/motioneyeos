9918596544323bc0bceb1c40363b208962462f36
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-02-25 23:13:56 +0100

    package/armadillo: fix license
    License is Apache-2.0 since version 7.800:
    http://arma.sourceforge.net/license.html
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/armadillo/armadillo.mk
+++ package/armadillo/armadillo.mk
@@ -9,7 +9,7 @@ ARMADILLO_SOURCE = armadillo-$(ARMADILLO_VERSION).tar.xz
 ARMADILLO_SITE = https://downloads.sourceforge.net/project/arma
 ARMADILLO_DEPENDENCIES = clapack
 ARMADILLO_INSTALL_STAGING = YES
-ARMADILLO_LICENSE = MPL-2.0
+ARMADILLO_LICENSE = Apache-2.0
 ARMADILLO_LICENSE_FILES = LICENSE.txt
 
 $(eval $(cmake-package))
