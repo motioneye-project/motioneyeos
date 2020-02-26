051a2e7b1b696707161507ddd3f7aff437ee2b81
Peter Korsgaard <peter@korsgaard.com>
2020-02-26 15:45:46 +0100

    package/qt5script: drop qt 5.6 support
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/qt5/qt5script/qt5script.mk
+++ package/qt5/qt5script/qt5script.mk
@@ -14,14 +14,9 @@ QT5SCRIPT_INSTALL_STAGING = YES
 # This is linked into libQt5Script, which also contains Qt sources under
 # LGPL-2.1 (only). Therefore, the library is  LGPL-2.1 and BSD-3-Clause.
 # libQt5ScriptTools is under the normal Qt opensource license.
-ifeq ($(BR2_PACKAGE_QT5_VERSION_LATEST),y)
 QT5SCRIPT_LICENSE = LGPL-2.1, BSD-3-Clause, LGPL-3.0 or GPL-2.0+ (libQt5ScriptTools), GFDL-1.3 (docs)
 # LGPL-2.1 license file is missing
 QT5SCRIPT_LICENSE_FILES = LICENSE.GPL2 LICENSE.GPL3 LICENSE.LGPL3 LICENSE.FDL
-else
-QT5SCRIPT_LICENSE = LGPL-2.1, BSD-3-Clause, LGPL-2.1 with exception or LGPL-3.0 with exception (libQt5ScriptTools), GFDL-1.3 (docs)
-QT5SCRIPT_LICENSE_FILES = LICENSE.LGPLv21 LICENSE.LGPLv3 LGPL_EXCEPTION.txt LICENSE.FDL
-endif
 # License files from JavaScriptCore
 QT5SCRIPT_LICENSE_FILES += \
 	src/3rdparty/javascriptcore/JavaScriptCore/COPYING.LIB \
