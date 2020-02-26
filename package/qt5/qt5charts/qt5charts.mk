4ccf0f8360a74905c3e6e7f69f451c1915dc92e8
Peter Korsgaard <peter@korsgaard.com>
2020-02-26 15:45:36 +0100

    package/qt5charts: drop qt 5.6 support
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/qt5/qt5charts/qt5charts.mk
+++ package/qt5/qt5charts/qt5charts.mk
@@ -4,21 +4,14 @@
 #
 ################################################################################
 
-# Qt5Charts does not follow Qt versionning for 5.6
-ifeq ($(BR2_PACKAGE_QT5_VERSION_LATEST),y)
 QT5CHARTS_VERSION = $(QT5_VERSION)
-else
-QT5CHARTS_VERSION = 2.1.3
-endif
 QT5CHARTS_SITE = $(QT5_SITE)
 QT5CHARTS_SOURCE = qtcharts-$(QT5_SOURCE_TARBALL_PREFIX)-$(QT5CHARTS_VERSION).tar.xz
 QT5CHARTS_DEPENDENCIES = qt5base
 QT5CHARTS_INSTALL_STAGING = YES
 
 QT5CHARTS_LICENSE = GPL-3.0
-ifeq ($(BR2_PACKAGE_QT5_VERSION_LATEST),y)
 QT5CHARTS_LICENSE_FILES = LICENSE.GPL3
-endif
 
 ifeq ($(BR2_PACKAGE_QT5DECLARATIVE),y)
 QT5CHARTS_DEPENDENCIES += qt5declarative
