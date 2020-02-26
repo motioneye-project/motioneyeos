f2744e48adc3da7f8dd4efaeeae4480dbc6d7b10
Peter Korsgaard <peter@korsgaard.com>
2020-02-26 15:45:32 +0100

    package/qt5: drop 5.6 support
    As discussed during the FOSDEM2019 develop days, Qt 5.6 is very old (5.6.3
    was released in September 2017, and 5.6.x became EOL in March 2019), so drop
    it before the new Buildroot LTS release:
    
    https://elinux.org/Buildroot:DeveloperDaysFOSDEM2019#Qt5_versions_to_support:_keep_5.6_or_a_newer_LTS.3F
    
    And add legacy handling for it.
    
    There are a number of places where code checks for
    BR2_PACKAGE_QT5_VERSION_LATEST, so leave that as a blind option for now to
    not break the build.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/qt5/qt5.mk
+++ package/qt5/qt5.mk
@@ -4,15 +4,9 @@
 #
 ################################################################################
 
-ifeq ($(BR2_PACKAGE_QT5_VERSION_LATEST),y)
 QT5_VERSION_MAJOR = 5.12
 QT5_VERSION = $(QT5_VERSION_MAJOR).7
 QT5_SOURCE_TARBALL_PREFIX = everywhere-src
-else
-QT5_VERSION_MAJOR = 5.6
-QT5_VERSION = $(QT5_VERSION_MAJOR).3
-QT5_SOURCE_TARBALL_PREFIX = opensource-src
-endif
 QT5_SITE = https://download.qt.io/archive/qt/$(QT5_VERSION_MAJOR)/$(QT5_VERSION)/submodules
 
 include $(sort $(wildcard package/qt5/*/*.mk))
