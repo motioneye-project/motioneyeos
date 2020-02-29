32d9a95d9460f78e9f33349937f426c3ae11662f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-02-29 21:45:48 +0100

    package/emlog: annotate CVE-2019-16868 and CVE-2019-17073
    CVE-2019-16868 and CVE-2019-17073 are misclassified (by our CVE tracker)
    as affecting emlog, while in fact it affects http://www.emlog.net.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/emlog/emlog.mk
+++ package/emlog/emlog.mk
@@ -9,6 +9,10 @@ EMLOG_SITE = $(call github,nicupavel,emlog,emlog-$(EMLOG_VERSION))
 EMLOG_LICENSE = GPL-2.0
 EMLOG_LICENSE_FILES = COPYING
 
+# CVE-2019-16868 and CVE-2019-17073 are misclassified (by our CVE tracker) as
+# affecting emlog, while in fact it affects http://www.emlog.net.
+EMLOG_IGNORE_CVES += CVE-2019-16868 CVE-2019-17073
+
 define EMLOG_BUILD_CMDS
 	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS) nbcat
 endef
