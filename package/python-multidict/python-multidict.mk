62355ebd4f94b70c4ecc677133a6d79a7466c124
James Hilliard <james.hilliard1@gmail.com>
2020-03-02 02:43:33 -0700

    package/python-multidict: bump to version 4.7.5
    Bugfix release, fixing a number of issues. From the CHANGES file:
    
    - Fixed creating and updating of MultiDict from a sequence of pairs and
      keyword arguments.  Previously passing a list argument modified it
      inplace, and other sequences caused an error.
      https://github.com/aio-libs/multidict/issues/457
    
    - Fixed comparing with mapping: an exception raised in the __len__ method caused raising a SyntaxError.
      https://github.com/aio-libs/multidict/issues/459
    
    - Fixed comparing with mapping: all exceptions raised in the __getitem__
      method were silenced.
      https://github.com/aio-libs/multidict/issues/460>
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    [Peter: extend commit message]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-multidict/python-multidict.mk
+++ package/python-multidict/python-multidict.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MULTIDICT_VERSION = 4.7.4
+PYTHON_MULTIDICT_VERSION = 4.7.5
 PYTHON_MULTIDICT_SOURCE = multidict-$(PYTHON_MULTIDICT_VERSION).tar.gz
-PYTHON_MULTIDICT_SITE = https://files.pythonhosted.org/packages/b6/22/ae21cedaa0e6d35e84e8ab57700dcf3d4609421ebe113e1aaafc468eec42
+PYTHON_MULTIDICT_SITE = https://files.pythonhosted.org/packages/61/b4/475114b3f1671da634f89239e61038f8742d9ac13aa34b32a05bf8022d22
 PYTHON_MULTIDICT_SETUP_TYPE = setuptools
 PYTHON_MULTIDICT_LICENSE = Apache-2.0
 PYTHON_MULTIDICT_LICENSE_FILES = LICENSE
