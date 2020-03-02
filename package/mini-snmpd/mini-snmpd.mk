fccdc6bd0b587b1b60565d957fe963fa4396f120
Alexander Sverdlin <alexander.sverdlin@gmail.com>
2020-03-02 19:50:54 +0100

    package/mini-snmpd: bump to version 1.6
    Drop both patches:
    
     - 0001-Prepend-zero-byte-before-unsigned-integers.patch is upstream
       as of 949ae648bf7c654b8fae607a0988bfa672607156
    
     - 0002-mib.c-allow-unsigned-integers-to-have-an-extra-byte.patch is
       upstream as of
    
    Use the systemd unit file provided by the upstream project instead of
    our own, just add an /etc/default/ file to add the -a option to
    preserve the same behavior.
    
    This new version now needs pkg-config.
    
    v1.6 changelog:
    
    Bug fix release.
    
    - Fix #16: regression in ifTable for point-to-point interfaces
    - Fix #17: major memory leak in Linux backend
    - Fix #18: consistent timeout handling in .conf file and command line
    
    v1.5 changelog:
    
    Major feature release.  Support for TCP-MIB, UDP-MIB, IP-MIB,
    ifXTable with 64-bit counters.
    
    - Majority of new features from [NDM Systems][]
    - CVE fixes from [Cisco Talos Intelligence Group][talos]
    
    - Add support for ifXTable (64-bit counters), from NDM Systems
    - Add support for TCP-MIB, from NDM Systems
    - Add support for UDP-MIB, from NDM Systems
    - Add support for IP-MIB, from NDM Systems
    - Add support for ifType
    - Add support for ifMtu
    - Binary and man page renamed: `mini_snmpd` --> `mini-snmpd`
    - New command line option `-l LEVEL` replaces `--verbose`
    - New command line option `-v` to show program version
    - Create PID file when daemon is ready to receive signals
    - Add support for systemd unit file on Linux
    - Add support for /etc/mini-snmpd.conf, disabled by default
    
    - CVE-2020-6060: Fix stack overflow in client connection handler
    - CVE-2020-6059: Fix out-of-bounds read in parsing of SNMP packet
    - CVE-2020-6058: Fix out-of-bounds read in parsing of SNMP packet
    - Let `-s` flag control use of syslog, when running in foreground
    - Removed all (known) GNU:isms; i.e., `__progname` and `%m`
    
    Signed-off-by: Alexander Sverdlin <alexander.sverdlin@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mini-snmpd/mini-snmpd.mk
+++ package/mini-snmpd/mini-snmpd.mk
@@ -4,15 +4,18 @@
 #
 ################################################################################
 
-MINI_SNMPD_VERSION = 1.4
+MINI_SNMPD_VERSION = 1.6
 MINI_SNMPD_SITE = $(call github,troglobit,mini-snmpd,v$(MINI_SNMPD_VERSION))
 MINI_SNMPD_LICENSE = GPL-2.0
 MINI_SNMPD_LICENSE_FILES = COPYING
 MINI_SNMPD_AUTORECONF = YES
+MINI_SNMPD_DEPENDENCIES = host-pkgconf
 
-define MINI_SNMPD_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 644 package/mini-snmpd/mini-snmpd.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/mini-snmpd.service
+define MINI_SNMPD_INSTALL_ETC_DEFAULT
+	$(INSTALL) -D -m 644 package/mini-snmpd/mini-snmpd \
+		$(TARGET_DIR)/etc/default/mini-snmpd
 endef
+
+MINI_SNMPD_POST_INSTALL_TARGET_HOOKS += MINI_SNMPD_INSTALL_ETC_DEFAULT
 
 $(eval $(autotools-package))
