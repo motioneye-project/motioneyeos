62b478fae8ba499c25d01b193954d743cb107073
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-03-09 19:39:32 +0100

    package/kodi-screensaver-biogenesis: bump version to 2.2.3-Leia
    Drop patch 0001-Add-missing-stddef-include.patch which was applied
    upstream.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-screensaver-biogenesis/kodi-screensaver-biogenesis.mk
+++ package/kodi-screensaver-biogenesis/kodi-screensaver-biogenesis.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_SCREENSAVER_BIOGENESIS_VERSION = 2.2.2-Leia
+KODI_SCREENSAVER_BIOGENESIS_VERSION = 2.2.3-Leia
 KODI_SCREENSAVER_BIOGENESIS_SITE = $(call github,xbmc,screensaver.biogenesis,$(KODI_SCREENSAVER_BIOGENESIS_VERSION))
 KODI_SCREENSAVER_BIOGENESIS_LICENSE = GPL-2.0+
 KODI_SCREENSAVER_BIOGENESIS_LICENSE_FILES = src/Life.cpp
