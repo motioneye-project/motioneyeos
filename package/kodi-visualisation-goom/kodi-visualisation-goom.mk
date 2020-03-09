6420dc3c54e999c3aa8f0d1cbfc12ed209e5473b
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-03-09 19:39:37 +0100

    package/kodi-visualisation-goom: bump version to 2.2.1-Leia
    Change license file to debian/copyright, since src/Main.cpp changed
    between 2.1.0 and 2.2.1.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-visualisation-goom/kodi-visualisation-goom.mk
+++ package/kodi-visualisation-goom/kodi-visualisation-goom.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-KODI_VISUALISATION_GOOM_VERSION = 2.1.0-Leia
+KODI_VISUALISATION_GOOM_VERSION = 2.2.1-Leia
 KODI_VISUALISATION_GOOM_SITE = $(call github,xbmc,visualization.goom,$(KODI_VISUALISATION_GOOM_VERSION))
 KODI_VISUALISATION_GOOM_LICENSE = GPL-2.0+
-KODI_VISUALISATION_GOOM_LICENSE_FILES = src/Main.cpp
+KODI_VISUALISATION_GOOM_LICENSE_FILES = debian/copyright
 
 KODI_VISUALISATION_GOOM_DEPENDENCIES = glm kodi
 
