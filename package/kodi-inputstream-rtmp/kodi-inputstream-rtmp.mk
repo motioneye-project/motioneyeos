4998ae53b81a8d75d6ece831ea1ec654ae510b76
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-03-09 19:39:25 +0100

    package/kodi-inputstream-rtmp: bump version to 2.0.9
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-inputstream-rtmp/kodi-inputstream-rtmp.mk
+++ package/kodi-inputstream-rtmp/kodi-inputstream-rtmp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_INPUTSTREAM_RTMP_VERSION = 2.0.8-Leia
+KODI_INPUTSTREAM_RTMP_VERSION = 2.0.9-Leia
 KODI_INPUTSTREAM_RTMP_SITE = $(call github,xbmc,inputstream.rtmp,$(KODI_INPUTSTREAM_RTMP_VERSION))
 KODI_INPUTSTREAM_RTMP_LICENSE = GPL-2.0+
 KODI_INPUTSTREAM_RTMP_LICENSE_FILES = debian/copyright
