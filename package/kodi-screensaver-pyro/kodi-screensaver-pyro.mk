7fc97af090e456b16c918b4d3010bd6a9b9f49de
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-03-09 19:39:36 +0100

    package/kodi-screensaver-pyro: bump version to 3.0.1-Leia
    Drop 0001-Add-missing-stddef-include.patch as it is now upstream.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-screensaver-pyro/kodi-screensaver-pyro.mk
+++ package/kodi-screensaver-pyro/kodi-screensaver-pyro.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_SCREENSAVER_PYRO_VERSION = 3.0.0-Leia
+KODI_SCREENSAVER_PYRO_VERSION = 3.0.1-Leia
 KODI_SCREENSAVER_PYRO_SITE = $(call github,xbmc,screensaver.pyro,$(KODI_SCREENSAVER_PYRO_VERSION))
 KODI_SCREENSAVER_PYRO_LICENSE = GPL-2.0+
 KODI_SCREENSAVER_PYRO_LICENSE_FILES = src/Pyro.cpp
