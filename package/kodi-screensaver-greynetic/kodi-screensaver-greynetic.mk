1f97d9cd24486aff3f94d66daaef6eb47e55a770
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-03-09 19:39:34 +0100

    package/kodi-screensaver-greynetic: bump version to 2.2.2-Leia
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-screensaver-greynetic/kodi-screensaver-greynetic.mk
+++ package/kodi-screensaver-greynetic/kodi-screensaver-greynetic.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_SCREENSAVER_GREYNETIC_VERSION = 2.2.1-Leia
+KODI_SCREENSAVER_GREYNETIC_VERSION = 2.2.2-Leia
 KODI_SCREENSAVER_GREYNETIC_SITE = $(call github,xbmc,screensaver.greynetic,$(KODI_SCREENSAVER_GREYNETIC_VERSION))
 KODI_SCREENSAVER_GREYNETIC_LICENSE = GPL-2.0+
 KODI_SCREENSAVER_GREYNETIC_LICENSE_FILES = src/GreyNetic.cpp
