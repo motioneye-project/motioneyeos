8c7653be34914673a77e1bcf85a50ff203b7c63c
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-03-09 19:39:31 +0100

    package/kodi-screensaver-asterwave: bump version to 3.0.4-Leia
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-screensaver-asterwave/kodi-screensaver-asterwave.mk
+++ package/kodi-screensaver-asterwave/kodi-screensaver-asterwave.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_SCREENSAVER_ASTERWAVE_VERSION = 3.0.3-Leia
+KODI_SCREENSAVER_ASTERWAVE_VERSION = 3.0.4-Leia
 KODI_SCREENSAVER_ASTERWAVE_SITE = $(call github,xbmc,screensaver.asterwave,$(KODI_SCREENSAVER_ASTERWAVE_VERSION))
 KODI_SCREENSAVER_ASTERWAVE_DEPENDENCIES = kodi libglu libsoil
 
