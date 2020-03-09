85c95e3614f4b3fde23fd75f9bbc5bb18b503cce
Peter Seiderer <ps.report@gmx.net>
2020-03-09 13:34:31 +0100

    package/mesa3d: fix vc4 neon related compile failure
    Add mesa3d patch to add a vc4 option to disable
    the optional neon support and set it dependent
    on BR2_ARM_CPU_HAS_NEON.
    
    Fixes:
    
      http://autobuild.buildroot.net/results/6387b0a99e1a0922811919623d9a10b0943988df
    
      [1086/1254] Compiling C object 'src/gallium/drivers/vc4/691f666@@vc4_neon@sta/vc4_tiling_lt_neon.c.o'.
      {standard input}: Assembler messages:
      {standard input}:334: Error: selected processor does not support `vldm r4,{q0,q1,q2,q3}' in ARM mode
      {standard input}:335: Error: selected processor does not support `vst1.8 d0,[r3],r2' in ARM mode
      {standard input}:336: Error: selected processor does not support `vst1.8 d1,[r3],r2' in ARM mode
      {standard input}:337: Error: selected processor does not support `vst1.8 d2,[r3],r2' in ARM mode
      {standard input}:338: Error: selected processor does not support `vst1.8 d3,[r3],r2' in ARM mode
      {standard input}:339: Error: selected processor does not support `vst1.8 d4,[r3],r2' in ARM mode
      {standard input}:340: Error: selected processor does not support `vst1.8 d5,[r3],r2' in ARM mode
      {standard input}:341: Error: selected processor does not support `vst1.8 d6,[r3],r2' in ARM mode
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -69,6 +69,12 @@ MESA3D_CONF_OPTS += \
 	-Dgallium-xa=false
 endif
 
+ifeq ($(BR2_ARM_CPU_HAS_NEON),y)
+MESA3D_CONF_OPTS += -Dgallium-vc4-neon=auto
+else
+MESA3D_CONF_OPTS += -Dgallium-vc4-neon=disabled
+endif
+
 # Drivers
 
 #Gallium Drivers
