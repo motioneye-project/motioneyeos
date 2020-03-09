09c15543bce5ce5b3d9a3001888d4738a96a8b37
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-03-09 19:39:30 +0100

    package/kodi-pvr-vdr-vnsi: bump version to 3.6.4-Leia
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-pvr-vdr-vnsi/kodi-pvr-vdr-vnsi.mk
+++ package/kodi-pvr-vdr-vnsi/kodi-pvr-vdr-vnsi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_VDR_VNSI_VERSION = 3.6.3-Leia
+KODI_PVR_VDR_VNSI_VERSION = 3.6.4-Leia
 KODI_PVR_VDR_VNSI_SITE = $(call github,kodi-pvr,pvr.vdr.vnsi,$(KODI_PVR_VDR_VNSI_VERSION))
 KODI_PVR_VDR_VNSI_LICENSE = GPL-2.0+
 KODI_PVR_VDR_VNSI_LICENSE_FILES = debian/copyright
