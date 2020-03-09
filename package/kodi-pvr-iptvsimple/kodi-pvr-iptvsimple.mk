71fa7252cfe30662ecae3f775a5744569d208819
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-03-09 19:39:29 +0100

    package/kodi-pvr-iptvsimple: bump version to 3.9.8-Leia
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-pvr-iptvsimple/kodi-pvr-iptvsimple.mk
+++ package/kodi-pvr-iptvsimple/kodi-pvr-iptvsimple.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_IPTVSIMPLE_VERSION = 3.8.8-Leia
+KODI_PVR_IPTVSIMPLE_VERSION = 3.9.8-Leia
 KODI_PVR_IPTVSIMPLE_SITE = $(call github,kodi-pvr,pvr.iptvsimple,$(KODI_PVR_IPTVSIMPLE_VERSION))
 KODI_PVR_IPTVSIMPLE_LICENSE = GPL-2.0+
 KODI_PVR_IPTVSIMPLE_LICENSE_FILES = debian/copyright
