77d9217083005465926788f15c1597320935dcdb
Louis-Paul Cordier <lpdev@cordier.org>
2020-03-10 10:58:16 +0100

    package/pigz: add host package support
    Signed-off-by: Louis-Paul Cordier <lpdev@cordier.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/pigz/pigz.mk
+++ package/pigz/pigz.mk
@@ -7,6 +7,7 @@
 PIGZ_VERSION = 2.4
 PIGZ_SITE = $(call github,madler,pigz,v$(PIGZ_VERSION))
 PIGZ_DEPENDENCIES = zlib
+HOST_PIGZ_DEPENDENCIES = host-zlib
 PIGZ_LICENSE = Zlib
 PIGZ_LICENSE_FILES = README
 
@@ -14,8 +15,17 @@ define PIGZ_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
 endef
 
+define HOST_PIGZ_BUILD_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) $(HOST_CONFIGURE_OPTS) -C $(@D)
+endef
+
 define PIGZ_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/pigz $(TARGET_DIR)/usr/bin/pigz
 endef
 
+define HOST_PIGZ_INSTALL_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/pigz $(HOST_DIR)/bin/pigz
+endef
+
 $(eval $(generic-package))
+$(eval $(host-generic-package))
