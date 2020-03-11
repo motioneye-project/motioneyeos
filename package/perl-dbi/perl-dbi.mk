5b600f747450ce6006ce865680a2efc63145fac4
Francois Perrad <fperrad@gmail.com>
2020-03-11 08:09:48 +0100

    package/perl-dbi: bump to version 1.643
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/perl-dbi/perl-dbi.mk
+++ package/perl-dbi/perl-dbi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_DBI_VERSION = 1.642
+PERL_DBI_VERSION = 1.643
 PERL_DBI_SOURCE = DBI-$(PERL_DBI_VERSION).tar.gz
 PERL_DBI_SITE = $(BR2_CPAN_MIRROR)/authors/id/T/TI/TIMB
 PERL_DBI_LICENSE = Artistic or GPL-1.0+
