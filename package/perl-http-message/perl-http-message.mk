68ea2a06d8bce1f64fe61cbbdeb6373e1a32cbd4
Francois Perrad <fperrad@gmail.com>
2020-03-11 08:09:50 +0100

    package/perl-http-message: bump to version 6.22
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/perl-http-message/perl-http-message.mk
+++ package/perl-http-message/perl-http-message.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PERL_HTTP_MESSAGE_VERSION = 6.18
+PERL_HTTP_MESSAGE_VERSION = 6.22
 PERL_HTTP_MESSAGE_SOURCE = HTTP-Message-$(PERL_HTTP_MESSAGE_VERSION).tar.gz
 PERL_HTTP_MESSAGE_SITE = $(BR2_CPAN_MIRROR)/authors/id/O/OA/OALDERS
 PERL_HTTP_MESSAGE_LICENSE = Artistic or GPL-1.0+
