179e6766a6db96840d4b7692cd2edfd92e6036f0
Adam Duskett <Aduskett@gmail.com>
2020-03-12 12:47:17 -0700

    package/python-gobject: bump version to 3.36.0
    Other changes:
      - Convert the package to a meson package.
      - Remove 0001-add-PYTHON_INCLUDES-override.patch as it no longer applies.
      - Add gobject-introspection as a dependency.
      - Add the package under myself in the DEVELOPERS file.
    
    Because gobject-introspection is now a dependency of python-gobject, the test
    must be updated at the same time.
    
     - Change TestPythonPy2Gobject to TestPythonPy3Gobject as
       gobject-introspection requires python3.
    
     - Refactor test_python_gobject.py to no longer inherit the
       TestPythonPackageBase class, as this class uses a base config that does not
       support gobject-introspection.
    
     - Update sample_python_gobject to use Glib to find the path of sh.
    
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- support/testing/tests/package/sample_python_gobject.py
+++ support/testing/tests/package/sample_python_gobject.py
@@ -1,1 +1,12 @@
-import gobject  # noqa
+#!/usr/bin/env python
+"""A simple test that uses python-gobject to find the path of sh."""
+from gi.repository import GLib
+
+def main():
+    sh_path = GLib.find_program_in_path('sh')
+    if sh_path == "/bin/sh":
+      return True
+    return False
+
+if __name__ == '__main__':
+    main()
--- support/testing/tests/package/test_python_gobject.py
+++ support/testing/tests/package/test_python_gobject.py
@@ -1,11 +1,33 @@
+import os
 from tests.package.test_python import TestPythonPackageBase
 
-
-class TestPythonPy2Gobject(TestPythonPackageBase):
+class TestPythonPy3Gobject(TestPythonPackageBase):
     __test__ = True
-    config = TestPythonPackageBase.config + \
+    config = \
         """
-        BR2_PACKAGE_PYTHON=y
+        BR2_aarch64=y
+        BR2_TOOLCHAIN_EXTERNAL=y
+        BR2_TARGET_GENERIC_GETTY_PORT="ttyAMA0"
+        BR2_LINUX_KERNEL=y
+        BR2_LINUX_KERNEL_CUSTOM_VERSION=y
+        BR2_LINUX_KERNEL_CUSTOM_VERSION_VALUE="4.16.7"
+        BR2_LINUX_KERNEL_USE_CUSTOM_CONFIG=y
+        BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE="board/qemu/aarch64-virt/linux.config"
+        BR2_LINUX_KERNEL_NEEDS_HOST_OPENSSL=y
+        BR2_TARGET_ROOTFS_CPIO=y
+        BR2_TARGET_ROOTFS_CPIO_GZIP=y
+        BR2_PACKAGE_GOBJECT_INTROSPECTION=y
+        BR2_PACKAGE_PYTHON3=y
         BR2_PACKAGE_PYTHON_GOBJECT=y
         """
+
+    def login(self):
+        img = os.path.join(self.builddir, "images", "rootfs.cpio.gz")
+        kern = os.path.join(self.builddir, "images", "Image")
+        self.emulator.boot(arch="aarch64",
+                           kernel=kern,
+                           kernel_cmdline=["console=ttyAMA0"],
+                           options=["-M", "virt", "-cpu", "cortex-a57", "-m", "512M", "-initrd", img])
+        self.emulator.login()
+
     sample_scripts = ["tests/package/sample_python_gobject.py"]
