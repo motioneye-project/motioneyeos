6581d19ab4e79ffa84f3a4b1a942206c05f4feeb
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-03-13 08:11:51 +0100

    package/kodi-pvr-nextpvr: bump version to 3.3.19-Leia
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-pvr-nextpvr/kodi-pvr-nextpvr.mk
+++ package/kodi-pvr-nextpvr/kodi-pvr-nextpvr.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_NEXTPVR_VERSION = 3.3.18-Leia
+KODI_PVR_NEXTPVR_VERSION = 3.3.19-Leia
 KODI_PVR_NEXTPVR_SITE = $(call github,kodi-pvr,pvr.nextpvr,$(KODI_PVR_NEXTPVR_VERSION))
 KODI_PVR_NEXTPVR_LICENSE = GPL-2.0+
 KODI_PVR_NEXTPVR_LICENSE_FILES = src/client.h
