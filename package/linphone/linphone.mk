50842fc0169d22b6656224b3a40222b32b63bb9c
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-03-13 22:50:15 +0100

    package/linphone: bump to version 4.3.1
    - Update site to get latest release
    - License is GPL-3.0+ since version 4.3.0 and
      https://gitlab.linphone.org/BC/public/liblinphone/commit/2bc924c184e2f00f1be3f82a736bccaeacce106e
    - Switch to cmake-infrastructure
    - Remove most of the existing dependencies (alsa, speex, libupnp, ...)
      as they are now only managed in mediatreamer
    - Add external dependencies: belr, belle-sip, libxml2, mediastreamer,
      sqlite
    - This bump is needed as current linphone does not build with latest
      libeXosip2
    
    Fixes:
     - http://autobuild.buildroot.org/results/ed8bdcbfb7d453ed7d436bada35417ee305e6ac9
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/linphone/linphone.mk
+++ package/linphone/linphone.mk
@@ -4,52 +4,38 @@
 #
 ################################################################################
 
-LINPHONE_VERSION_MAJOR = 3.6
-LINPHONE_VERSION = $(LINPHONE_VERSION_MAJOR).1
-LINPHONE_SITE = http://download-mirror.savannah.gnu.org/releases/linphone/$(LINPHONE_VERSION_MAJOR).x/sources
+LINPHONE_VERSION = 4.3.1
+LINPHONE_SITE = \
+	https://gitlab.linphone.org/BC/public/liblinphone/-/archive/$(LINPHONE_VERSION)
 LINPHONE_CONF_OPTS = \
-	--disable-strict --disable-video
-# configure is out of sync causing deplibs linking issues
-LINPHONE_AUTORECONF = YES
+	-DENABLE_ADVANCED_IM=OFF \
+	-DENABLE_CXX_WRAPPER=OFF \
+	-DENABLE_DB_STORAGE=OFF \
+	-DENABLE_LIME=OFF \
+	-DENABLE_LIME_X3DH=OFF \
+	-DENABLE_STRICT=OFF \
+	-DENABLE_TOOLS=OFF \
+	-DENABLE_TUTORIALS=OFF \
+	-DENABLE_UNIT_TESTS=OFF \
+	-DENABLE_VCARD=OFF \
+	-DENABLE_VIDEO=OFF
 LINPHONE_INSTALL_STAGING = YES
-LINPHONE_DEPENDENCIES = host-pkgconf libeXosip2 speex
-LINPHONE_LICENSE = GPL-2.0+
-LINPHONE_LICENSE_FILES = COPYING
+LINPHONE_DEPENDENCIES = \
+	belle-sip \
+	belr \
+	libxml2 \
+	mediastreamer \
+	sqlite \
+	$(if $(BR2_PACKAGE_ZLIB),zlib)
+LINPHONE_LICENSE = GPL-3.0+
+LINPHONE_LICENSE_FILES = LICENSE.txt
 
-ifeq ($(BR2_arc),y)
-# toolchain __arc__ define conflicts with libosip2 source
-LINPHONE_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -U__arc__"
+ifeq ($(BR2_STATIC_LIBS),y)
+LINPHONE_CONF_OPTS += -DENABLE_STATIC=ON -DENABLE_SHARED=OFF
+else ifeq ($(BR2_SHARED_STATIC_LIBS),y)
+LINPHONE_CONF_OPTS += -DENABLE_STATIC=ON -DENABLE_SHARED=ON
+else ifeq ($(BR2_SHARED_LIBS),y)
+LINPHONE_CONF_OPTS += -DENABLE_STATIC=OFF -DENABLE_SHARED=ON
 endif
 
-ifeq ($(BR2_PACKAGE_LIBGTK2)$(BR2_PACKAGE_XORG7),yy)
-LINPHONE_CONF_OPTS += --enable-gtk_ui
-LINPHONE_DEPENDENCIES += libgtk2
-else
-LINPHONE_CONF_OPTS += --disable-gtk_ui
-endif
-
-# needed for bundled mediastreamer2
-LINPHONE_DEPENDENCIES += host-intltool host-gettext
-
-ifeq ($(BR2_PACKAGE_ALSA_LIB_MIXER)$(BR2_PACKAGE_ALSA_LIB_PCM),yy)
-LINPHONE_CONF_OPTS += --enable-alsa
-LINPHONE_DEPENDENCIES += alsa-lib
-else
-LINPHONE_CONF_OPTS += --disable-alsa
-endif
-
-ifeq ($(BR2_PACKAGE_LIBV4L),y)
-LINPHONE_CONF_OPTS += --enable-libv4l1 --enable-libv4l2
-LINPHONE_DEPENDENCIES += libv4l
-else
-LINPHONE_CONF_OPTS += --disable-libv4l1 --disable-libv4l2
-endif
-
-ifeq ($(BR2_PACKAGE_LIBUPNP),y)
-LINPHONE_DEPENDENCIES += libupnp
-LINPHONE_CONF_OPTS += --enable-upnp
-else
-LINPHONE_CONF_OPTS += --disable-upnp
-endif
-
-$(eval $(autotools-package))
+$(eval $(cmake-package))
