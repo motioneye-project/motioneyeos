4cbe598a9f286a8e1ad54564829eba594ff64327
Adam Duskett <Aduskett@gmail.com>
2020-03-15 13:10:30 -0700

    package/gstreamer1/gst1-plugins-bad: add introspection support
    If gobject-introspection is selected, explicitly set -Dintrospection=enabled in
    the configure options and add a dependency for gobject-introspection.
    
    This feature requires an upstream patch that allows introspection support even
    when cross-compiling.
    
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/gstreamer1/gst1-plugins-bad/gst1-plugins-bad.mk
+++ package/gstreamer1/gst1-plugins-bad/gst1-plugins-bad.mk
@@ -23,7 +23,6 @@ GST1_PLUGINS_BAD_CONF_OPTS = \
 	-Dwinks=disabled \
 	-Dandroidmedia=disabled \
 	-Dapplemedia=disabled \
-	-Dintrospection=disabled \
 	-Dgobject-cast-checks=disabled \
 	-Dglib-asserts=disabled \
 	-Dglib-checks=disabled
@@ -62,6 +61,13 @@ GST1_PLUGINS_BAD_CONF_OPTS += \
 	-Dopencv=disabled
 
 GST1_PLUGINS_BAD_DEPENDENCIES = gst1-plugins-base gstreamer1
+
+ifeq ($(BR2_PACKAGE_GOBJECT_INTROSPECTION),y)
+GST1_PLUGINS_BAD_CONF_OPTS += -Dintrospection=enabled
+GST1_PLUGINS_BAD_DEPENDENCIES += gobject-introspection
+else
+GST1_PLUGINS_BAD_CONF_OPTS += -Dintrospection=disabled
+endif
 
 ifeq ($(BR2_PACKAGE_GST1_PLUGINS_BAD_PLUGIN_WAYLAND),y)
 GST1_PLUGINS_BAD_CONF_OPTS += -Dwayland=enabled
