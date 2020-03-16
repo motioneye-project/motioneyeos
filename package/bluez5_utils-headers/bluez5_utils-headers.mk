3a678c952f4394b119d884ef22910f30860e1c2e
Jörg Krause <joerg.krause@embedded.rocks>
2020-03-16 17:52:50 +0100

    package/{bluez5_utils, bluez5_utils-headers}: bump to version 5.54
    Changes since version 5.52:
    
    5.54:
      Fix issue with HOGP to accept data only from bonded devices.
      Fix issue with A2DP sessions being connected at the same time.
      Fix issue with class UUID matches before connecting profile.
      Add support for handling MTU auto-tuning option for AVDTP.
      Add support for new policy for Just-Works repairing.
      Add support for Enhanced ATT bearer (EATT).
    
    5.53:
      Fix issue with handling unregistration for advertisment.
      Fix issue with A2DP and handling recovering process.
      Fix issue with udpating input device information.
      Add support for loading blocked keys.
    
    Signed-off-by: Jörg Krause <joerg.krause@embedded.rocks>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/bluez5_utils-headers/bluez5_utils-headers.mk
+++ package/bluez5_utils-headers/bluez5_utils-headers.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # Keep the version and patches in sync with bluez5_utils
-BLUEZ5_UTILS_HEADERS_VERSION = 5.52
+BLUEZ5_UTILS_HEADERS_VERSION = 5.54
 BLUEZ5_UTILS_HEADERS_SOURCE = bluez-$(BLUEZ5_UTILS_VERSION).tar.xz
 BLUEZ5_UTILS_HEADERS_SITE = $(BR2_KERNEL_MIRROR)/linux/bluetooth
 BLUEZ5_UTILS_HEADERS_DL_SUBDIR = bluez5_utils
