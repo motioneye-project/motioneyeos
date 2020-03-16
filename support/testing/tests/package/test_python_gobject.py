1101d7cb09050792b9e457682de214d901a2beef
Adam Duskett <Aduskett@gmail.com>
2020-03-16 08:34:59 -0700

    support/testing/: change python-gobject to use prebuilt kernel
    Currently, we build a complete AArch64 system, including a kernel. This
    can take quite some time.
    
    Switch to an armv7 system, which allows us to use one a prebuilt kernel,
    thus significantly reducing the test time.
    
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    [yann.morin.1998@free.fr:
      - use a standalone config; don't inherit from TestPythonPackageBase
      - use the default external toolchain (ARM, not Linaro)
      - rewrite commit log
    ]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- support/testing/tests/package/test_python_gobject.py
+++ support/testing/tests/package/test_python_gobject.py
@@ -5,29 +5,22 @@ class TestPythonPy3Gobject(TestPythonPackageBase):
     __test__ = True
     config = \
         """
-        BR2_aarch64=y
+        BR2_arm=y
+        BR2_ARM_ENABLE_VFP=y
+        BR2_cortex_a9=y
         BR2_TOOLCHAIN_EXTERNAL=y
-        BR2_TARGET_GENERIC_GETTY_PORT="ttyAMA0"
-        BR2_LINUX_KERNEL=y
-        BR2_LINUX_KERNEL_CUSTOM_VERSION=y
-        BR2_LINUX_KERNEL_CUSTOM_VERSION_VALUE="4.16.7"
-        BR2_LINUX_KERNEL_USE_CUSTOM_CONFIG=y
-        BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE="board/qemu/aarch64-virt/linux.config"
-        BR2_LINUX_KERNEL_NEEDS_HOST_OPENSSL=y
-        BR2_TARGET_ROOTFS_CPIO=y
-        BR2_TARGET_ROOTFS_CPIO_GZIP=y
         BR2_PACKAGE_GOBJECT_INTROSPECTION=y
         BR2_PACKAGE_PYTHON3=y
         BR2_PACKAGE_PYTHON_GOBJECT=y
+        BR2_TARGET_ROOTFS_CPIO=y
+        # BR2_TARGET_ROOTFS_TAR is not set
         """
 
     def login(self):
-        img = os.path.join(self.builddir, "images", "rootfs.cpio.gz")
-        kern = os.path.join(self.builddir, "images", "Image")
-        self.emulator.boot(arch="aarch64",
-                           kernel=kern,
-                           kernel_cmdline=["console=ttyAMA0"],
-                           options=["-M", "virt", "-cpu", "cortex-a57", "-m", "512M", "-initrd", img])
+        img = os.path.join(self.builddir, "images", "rootfs.cpio")
+        self.emulator.boot(arch="armv7",
+                           kernel="builtin",
+                           options=["-initrd", img])
         self.emulator.login()
 
     sample_scripts = ["tests/package/sample_python_gobject.py"]
