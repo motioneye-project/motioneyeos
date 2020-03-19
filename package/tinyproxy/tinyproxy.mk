f9b666697895f767857e4bb23f27be0e2622d653
Alexander Mukhin <alexander.i.mukhin@gmail.com>
2020-03-19 17:04:44 +0300

    package/tinyproxy: use the release tarball, not a git clone
    Upstream ships distribution tarballs with pre-built manpages and
    necessary fixes, so that the build doesn't break if asciidoc is not
    available.
    
    Signed-off-by: Alexander Mukhin <alexander.i.mukhin@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/tinyproxy/tinyproxy.mk
+++ package/tinyproxy/tinyproxy.mk
@@ -5,11 +5,9 @@
 ################################################################################
 
 TINYPROXY_VERSION = 1.10.0
-TINYPROXY_SITE = $(call github,tinyproxy,tinyproxy,$(TINYPROXY_VERSION))
+TINYPROXY_SITE = https://github.com/tinyproxy/tinyproxy/releases/download/$(TINYPROXY_VERSION)
+TINYPROXY_SOURCE = tinyproxy-$(TINYPROXY_VERSION).tar.bz2
 TINYPROXY_LICENSE = GPL-2.0+
 TINYPROXY_LICENSE_FILES = COPYING
-
-# building from a git clone and patching Makefile.am
-TINYPROXY_AUTORECONF = YES
 
 $(eval $(autotools-package))
