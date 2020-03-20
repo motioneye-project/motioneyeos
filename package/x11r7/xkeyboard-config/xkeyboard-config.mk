c09061bb5863ea9af9e44d6ab7f379f10d2720f4
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-03-20 07:56:52 +0100

    package/x11r7/xkeyboard-config: bump version to 2.29
    Switch _SITE to https.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xkeyboard-config/xkeyboard-config.mk
+++ package/x11r7/xkeyboard-config/xkeyboard-config.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-XKEYBOARD_CONFIG_VERSION = 2.28
+XKEYBOARD_CONFIG_VERSION = 2.29
 XKEYBOARD_CONFIG_SOURCE = xkeyboard-config-$(XKEYBOARD_CONFIG_VERSION).tar.bz2
-XKEYBOARD_CONFIG_SITE = http://www.x.org/releases/individual/data/xkeyboard-config
+XKEYBOARD_CONFIG_SITE = https://www.x.org/releases/individual/data/xkeyboard-config
 XKEYBOARD_CONFIG_LICENSE = MIT
 XKEYBOARD_CONFIG_LICENSE_FILES = COPYING
 
