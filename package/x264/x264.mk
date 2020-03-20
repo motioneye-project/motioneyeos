2c7122549fdc134ce9aa802253581397a3f0102e
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-03-20 07:50:19 +0100

    package/x264: bump version to 20191217-2245
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x264/x264.mk
+++ package/x264/x264.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-X264_VERSION = 20190623-2245
+X264_VERSION = 20191217-2245
 X264_SITE = http://download.videolan.org/x264/snapshots
 X264_SOURCE = x264-snapshot-$(X264_VERSION).tar.bz2
 X264_LICENSE = GPL-2.0+
