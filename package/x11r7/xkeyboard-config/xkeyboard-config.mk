eb3767422c8726e2d565f3a7838c8f68e2767caf
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-03-21 10:55:33 +0100

    package/x11r7/xkeyboard-config: needs a host python3
    A host python3 is needed to run map-variants.py since version 2.29 and
    https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config/-/commit/57c745a9a373b4c9d272d00ff4f16dbcc6d3c48b
    
    The script (with a backported patch) does not have stringent requirements
    on python, so we may use a system python3 if present.
    
    Fixes:
     - http://autobuild.buildroot.org/results/7c3cdecc2feed62d7e0bad0e790a8bacaaa34cb7
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/x11r7/xkeyboard-config/xkeyboard-config.mk
+++ package/x11r7/xkeyboard-config/xkeyboard-config.mk
@@ -10,10 +10,14 @@ XKEYBOARD_CONFIG_SITE = https://www.x.org/releases/individual/data/xkeyboard-con
 XKEYBOARD_CONFIG_LICENSE = MIT
 XKEYBOARD_CONFIG_LICENSE_FILES = COPYING
 
+XKEYBOARD_CONFIG_DEPENDENCIES = \
+	$(BR2_PYTHON3_HOST_DEPENDENCY) \
+	host-gettext \
+	host-xapp_xkbcomp
+
 # xkeyboard-config.pc
 XKEYBOARD_CONFIG_INSTALL_STAGING = YES
 
 XKEYBOARD_CONFIG_CONF_OPTS = --disable-runtime-deps
-XKEYBOARD_CONFIG_DEPENDENCIES = host-gettext host-xapp_xkbcomp
 
 $(eval $(autotools-package))
