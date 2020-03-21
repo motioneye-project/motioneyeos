cad21f319769c59de22963b58334fe45172caae3
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-03-21 10:35:02 +0100

    package/kodi-*: mass update of license files
    Switch _LICENSE_FILES to debian/copyright instead of using a source
    code file.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-vfs-sftp/kodi-vfs-sftp.mk
+++ package/kodi-vfs-sftp/kodi-vfs-sftp.mk
@@ -7,7 +7,7 @@
 KODI_VFS_SFTP_VERSION = 1.0.5-Leia
 KODI_VFS_SFTP_SITE = $(call github,xbmc,vfs.sftp,$(KODI_VFS_SFTP_VERSION))
 KODI_VFS_SFTP_LICENSE = GPL-2.0+
-KODI_VFS_SFTP_LICENSE_FILES = src/SFTPFile.cpp
+KODI_VFS_SFTP_LICENSE_FILES = debian/copyright
 KODI_VFS_SFTP_DEPENDENCIES = kodi-platform libplatform libssh
 
 $(eval $(cmake-package))
