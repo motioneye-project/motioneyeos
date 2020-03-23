1e4e15578262d7db4981a01e05be68e25b70d50a
Yegor Yefremov <yegorslists@googlemail.com>
2020-03-23 11:49:07 +0100

    package/python3-pyyaml: add special host variant
    Needed to build mongodb.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-pyyaml/python-pyyaml.mk
+++ package/python-pyyaml/python-pyyaml.mk
@@ -4,6 +4,7 @@
 #
 ################################################################################
 
+# Please keep in sync package/python3-pyyaml/python3-pyyaml.mk
 PYTHON_PYYAML_VERSION = 5.3.1
 PYTHON_PYYAML_SOURCE = PyYAML-$(PYTHON_PYYAML_VERSION).tar.gz
 PYTHON_PYYAML_SITE = https://files.pythonhosted.org/packages/64/c2/b80047c7ac2478f9501676c988a5411ed5572f35d1beff9cae07d321512c
