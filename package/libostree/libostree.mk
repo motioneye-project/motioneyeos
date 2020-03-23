f45a81dbb1ecce8f1c598ac009702122cfdc7a15
Marcus Folkesson <marcus.folkesson@gmail.com>
2020-03-23 14:28:09 +0100

    package/libostree: bump to version 2020.3
    Upstream only ever made a dot-relase once, and it is not even available
    as a tag, and it is not even listed as a separate release on their
    github release page. We can assume that dot-release is not in upstream
    habits. Drop the VERSION_MAJOR variable, just use VERSION.
    
    Signed-off-by: Marcus Folkesson <marcus.folkesson@gmail.com>
    [yann.morin.1998@free.fr:
      - drop VERSION_MAJOR
      - two spaces in hash file
    ]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libostree/libostree.mk
+++ package/libostree/libostree.mk
@@ -4,10 +4,9 @@
 #
 ################################################################################
 
-LIBOSTREE_VERSION_MAJOR = 2019.6
-LIBOSTREE_VERSION= $(LIBOSTREE_VERSION_MAJOR)
+LIBOSTREE_VERSION = 2020.3
 LIBOSTREE_SOURCE = libostree-$(LIBOSTREE_VERSION).tar.xz
-LIBOSTREE_SITE = https://github.com/ostreedev/ostree/releases/download/v$(LIBOSTREE_VERSION_MAJOR)
+LIBOSTREE_SITE = https://github.com/ostreedev/ostree/releases/download/v$(LIBOSTREE_VERSION)
 
 LIBOSTREE_LICENSE = LGPL-2.0+
 LIBOSTREE_LICENSE_FILES = COPYING
