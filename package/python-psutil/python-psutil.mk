20fe8d2234035a4a77f5767bf6451852f70fe3a7
Yegor Yefremov <yegorslists@googlemail.com>
2020-03-23 11:49:08 +0100

    package/python3-psutil: add a special host variant
    Needed to build mongodb.
    
    With this host variant there is no need to enforce host
    python in the original package.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-psutil/python-psutil.mk
+++ package/python-psutil/python-psutil.mk
@@ -4,6 +4,7 @@
 #
 ################################################################################
 
+# Please keep in sync with package/python3-psutil/python3-psutil.mk
 PYTHON_PSUTIL_VERSION = 5.7.0
 PYTHON_PSUTIL_SOURCE = psutil-$(PYTHON_PSUTIL_VERSION).tar.gz
 PYTHON_PSUTIL_SITE = https://files.pythonhosted.org/packages/c4/b8/3512f0e93e0db23a71d82485ba256071ebef99b227351f0f5540f744af41
