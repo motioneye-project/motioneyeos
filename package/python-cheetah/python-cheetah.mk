80c09eb511309623ad40e1f940fccba2bb25803b
Yegor Yefremov <yegorslists@googlemail.com>
2020-03-23 11:49:05 +0100

    package/python3-cheetah: add special host variant
    Needed to build mongodb.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-cheetah/python-cheetah.mk
+++ package/python-cheetah/python-cheetah.mk
@@ -4,6 +4,7 @@
 #
 ################################################################################
 
+# Please keep in sync with package/python3-cheetah/python3-cheetah.mk
 PYTHON_CHEETAH_VERSION = 3.2.4
 PYTHON_CHEETAH_SOURCE = Cheetah3-$(PYTHON_CHEETAH_VERSION).tar.gz
 PYTHON_CHEETAH_SITE = https://files.pythonhosted.org/packages/4e/72/e6a7d92279e3551db1b68fd336fd7a6e3d2f2ec742bf486486e6150d77d2
