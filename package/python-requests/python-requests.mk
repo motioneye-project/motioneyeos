97ac0abad212d5bd78acf9f01d56cee2dcb9f1ca
Yegor Yefremov <yegorslists@googlemail.com>
2020-03-23 11:49:06 +0100

    package/python3-requests: add special host variant
    Needed to build mongodb.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-requests/python-requests.mk
+++ package/python-requests/python-requests.mk
@@ -4,6 +4,7 @@
 #
 ################################################################################
 
+# Please keep in sync with package/python3-requests/python3-requests.mk
 PYTHON_REQUESTS_VERSION = 2.23.0
 PYTHON_REQUESTS_SOURCE = requests-$(PYTHON_REQUESTS_VERSION).tar.gz
 PYTHON_REQUESTS_SITE = https://files.pythonhosted.org/packages/f5/4f/280162d4bd4d8aad241a21aecff7a6e46891b905a4341e7ab549ebaf7915
