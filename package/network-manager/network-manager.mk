402ad9fe540752b656e2dd564aad0b241833088c
Lubomir Rintel <lkundrak@v3.sk>
2020-03-24 15:29:49 +0100

    package/network-manager: bump to version 1.22.10
    Add COPYING.LGPL to license files, disable the "cloud configuration"
    tool. It requires libcurl and it is too unlikely to be useful.
    
    The library license is now LGPL-2.1+.
    
    The hash of CONTRIBUTING has changed because it now refers to
    LGPL-2.1+ instead of LGPL-2.0+.
    
    The hash of COPYING is changed due to white-space changes, and the
    removal of an introduction text:
    
        Unless a COPYING file in a subdirectory or file-specific license
        headers specify a different license, the following applies to all
        files in this directory and all subdirectories.
    
        This program is free software; you can redistribute it and/or
        modify it under the terms of the GNU General Public License as
        published by the Free Software Foundation; either version 2 of the
        License, or (at your option) any later version.
    
    Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>
    [Thomas:
     - fix hash of CONTRIBUTING
     - expand commit log
     - drop GFDL license information, since documentation is not installed
       on the target]
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/network-manager/network-manager.mk
+++ package/network-manager/network-manager.mk
@@ -4,18 +4,15 @@
 #
 ################################################################################
 
-NETWORK_MANAGER_VERSION_MAJOR = 1.20
-NETWORK_MANAGER_VERSION = $(NETWORK_MANAGER_VERSION_MAJOR).4
+NETWORK_MANAGER_VERSION_MAJOR = 1.22
+NETWORK_MANAGER_VERSION = $(NETWORK_MANAGER_VERSION_MAJOR).10
 NETWORK_MANAGER_SOURCE = NetworkManager-$(NETWORK_MANAGER_VERSION).tar.xz
 NETWORK_MANAGER_SITE = https://download.gnome.org/sources/NetworkManager/$(NETWORK_MANAGER_VERSION_MAJOR)
 NETWORK_MANAGER_INSTALL_STAGING = YES
 NETWORK_MANAGER_DEPENDENCIES = host-pkgconf udev gnutls libglib2 \
 	libgcrypt wireless_tools util-linux host-intltool readline libndp
-# Even though the COPYING file only contains the GPL-2.0 text, many
-# parts of network-manager are under LGPL-2.0. See the "Legal" section
-# of the CONTRIBUTING file for details.
-NETWORK_MANAGER_LICENSE = GPL-2.0+ (app), LGPL-2.0+ (libnm)
-NETWORK_MANAGER_LICENSE_FILES = COPYING CONTRIBUTING
+NETWORK_MANAGER_LICENSE = GPL-2.0+ (app), LGPL-2.1+ (libnm)
+NETWORK_MANAGER_LICENSE_FILES = COPYING COPYING.LGPL CONTRIBUTING
 
 NETWORK_MANAGER_CONF_ENV = \
 	ac_cv_path_LIBGCRYPT_CONFIG=$(STAGING_DIR)/usr/bin/libgcrypt-config \
@@ -31,7 +28,8 @@ NETWORK_MANAGER_CONF_OPTS = \
 	--disable-more-warnings \
 	--with-crypto=gnutls \
 	--with-iptables=/usr/sbin/iptables \
-	--disable-ifupdown
+	--disable-ifupdown \
+	--without-nm-cloud-setup
 
 ifeq ($(BR2_PACKAGE_OFONO),y)
 NETWORK_MANAGER_DEPENDENCIES += ofono
