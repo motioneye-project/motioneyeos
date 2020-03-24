75371a4a12673ff992ca482b503eed70509154af
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2020-03-24 15:47:25 +0100

    support/testing/tests/package/sample_python_gobject.py: fix flake8 warnings
    Fixes:
    
      support/testing/tests/package/sample_python_gobject.py:5:1: E302 expected 2 blank lines, found 1
      support/testing/tests/package/sample_python_gobject.py:8:7: E111 indentation is not a multiple of four
      support/testing/tests/package/sample_python_gobject.py:11:1: E305 expected 2 blank lines after class or function definition, found 1
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- support/testing/tests/package/sample_python_gobject.py
+++ support/testing/tests/package/sample_python_gobject.py
@@ -2,11 +2,13 @@
 """A simple test that uses python-gobject to find the path of sh."""
 from gi.repository import GLib
 
+
 def main():
     sh_path = GLib.find_program_in_path('sh')
     if sh_path == "/bin/sh":
-      return True
+        return True
     return False
+
 
 if __name__ == '__main__':
     main()
