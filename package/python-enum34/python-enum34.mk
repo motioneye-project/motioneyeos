03d461d10f7461ed7f09b2a6ba2a3e06616d8ee9
James Hilliard <james.hilliard1@gmail.com>
2020-03-26 16:20:18 -0600

    package/python-enum34: bump to version 1.1.10
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-enum34/python-enum34.mk
+++ package/python-enum34/python-enum34.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ENUM34_VERSION = 1.1.6
+PYTHON_ENUM34_VERSION = 1.1.10
 PYTHON_ENUM34_SOURCE = enum34-$(PYTHON_ENUM34_VERSION).tar.gz
-PYTHON_ENUM34_SITE = https://pypi.python.org/packages/bf/3e/31d502c25302814a7c2f1d3959d2a3b3f78e509002ba91aea64993936876
+PYTHON_ENUM34_SITE = https://files.pythonhosted.org/packages/11/c4/2da1f4952ba476677a42f25cd32ab8aaf0e1c0d0e00b89822b835c7e654c
 PYTHON_ENUM34_SETUP_TYPE = setuptools
 PYTHON_ENUM34_LICENSE = BSD-3-Clause
 PYTHON_ENUM34_LICENSE_FILES = enum/LICENSE
