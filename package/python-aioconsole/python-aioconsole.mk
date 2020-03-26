3a33eb87cfc2256c0869e616e21cea1d34db36b2
James Hilliard <james.hilliard1@gmail.com>
2020-03-26 01:59:47 -0600

    package/python-aioconsole: bump to version 0.1.16
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-aioconsole/python-aioconsole.mk
+++ package/python-aioconsole/python-aioconsole.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AIOCONSOLE_VERSION = 0.1.15
+PYTHON_AIOCONSOLE_VERSION = 0.1.16
 PYTHON_AIOCONSOLE_SOURCE = aioconsole-$(PYTHON_AIOCONSOLE_VERSION).tar.gz
-PYTHON_AIOCONSOLE_SITE = https://files.pythonhosted.org/packages/c4/68/56b09bf94aff8e02a6080493b3f85bdf7ddea7e4f264e6a90a158e064898
+PYTHON_AIOCONSOLE_SITE = https://files.pythonhosted.org/packages/70/08/5dbb71777058a4f3b6e61b61ffc5b5021056c4a139e118938773b93e9304
 PYTHON_AIOCONSOLE_SETUP_TYPE = setuptools
 PYTHON_AIOCONSOLE_LICENSE = GPL-3.0
 
