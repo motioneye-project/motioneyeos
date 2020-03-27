4ef8c5d3dd8194059ca6907427dcdad2fab5d81d
Adam Duskett <Aduskett@gmail.com>
2020-03-27 11:29:58 -0700

    package/gupnp: bump version to 1.2.2
    Other changes:
      - Convert the package type to meson as there is no longer autotools support.
    
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    [yann.morin.1998@free.fr: two spaces in hash file]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/gupnp/gupnp.mk
+++ package/gupnp/gupnp.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-GUPNP_VERSION_MAJOR = 1.0
-GUPNP_VERSION = $(GUPNP_VERSION_MAJOR).4
+GUPNP_VERSION_MAJOR = 1.2
+GUPNP_VERSION = $(GUPNP_VERSION_MAJOR).2
 GUPNP_SOURCE = gupnp-$(GUPNP_VERSION).tar.xz
 GUPNP_SITE = http://ftp.gnome.org/pub/gnome/sources/gupnp/$(GUPNP_VERSION_MAJOR)
 GUPNP_LICENSE = LGPL-2.0+
@@ -14,10 +14,10 @@ GUPNP_INSTALL_STAGING = YES
 GUPNP_DEPENDENCIES = host-pkgconf libglib2 libxml2 gssdp util-linux
 
 ifeq ($(BR2_PACKAGE_GOBJECT_INTROSPECTION),y)
-GUPNP_CONF_OPTS += --enable-introspection
+GUPNP_CONF_OPTS += -Dintrospection=true
 GUPNP_DEPENDENCIES += gobject-introspection
 else
-GUPNP_CONF_OPTS += --disable-introspection
+GUPNP_CONF_OPTS += -Dintrospection=false
 endif
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
