3eac848e87a7342b840ef8fe5b2e84e9c4314f11
Gary Bisson <gary.bisson@boundarydevices.com>
2020-03-27 18:12:01 +0100

    board/boundarydevices/common/post-image.sh: use genimage.sh
    - To avoid code duplication and to benefit from support script
    improvements like empty target dir.
    - Note that currently genimage.sh can't be set in defconfig directly for
    i.MX8 targets as the POST_SCRIPT_ARGS is used by
    imx8-bootloader-prepare.sh
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- board/boundarydevices/common/post-image.sh
+++ board/boundarydevices/common/post-image.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-BOARD_DIR="$(dirname $0)"
-GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
-GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
-
-rm -rf "${GENIMAGE_TMP}"
-
-genimage                           \
-	--rootpath "${TARGET_DIR}"     \
-	--tmppath "${GENIMAGE_TMP}"    \
-	--inputpath "${BINARIES_DIR}"  \
-	--outputpath "${BINARIES_DIR}" \
-	--config "${GENIMAGE_CFG}"
+support/scripts/genimage.sh -c $(dirname $0)/genimage.cfg
 
 exit $?
