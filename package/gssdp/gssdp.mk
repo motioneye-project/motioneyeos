7f4d7b759e9282157c8eb60e5ec3e365985c5206
Adam Duskett <Aduskett@gmail.com>
2020-03-27 11:29:57 -0700

    package/gssdp: bump version to 1.2.2
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    [yann.morin.1998@free.fr: two spaces in hash file]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/gssdp/gssdp.mk
+++ package/gssdp/gssdp.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-GSSDP_VERSION_MAJOR = 1.0
-GSSDP_VERSION = $(GSSDP_VERSION_MAJOR).3
+GSSDP_VERSION_MAJOR = 1.2
+GSSDP_VERSION = $(GSSDP_VERSION_MAJOR).2
 GSSDP_SOURCE = gssdp-$(GSSDP_VERSION).tar.xz
 GSSDP_SITE = http://ftp.gnome.org/pub/gnome/sources/gssdp/$(GSSDP_VERSION_MAJOR)
 GSSDP_LICENSE = LGPL-2.0+
