01a82c14012370a3bdff15e6bd761acc10fd79f3
Yann E. MORIN <yann.morin.1998@free.fr>
2020-03-28 19:50:09 +0100

    package/libapparmor: enable python bindings
    Signed-off-by: Angelo Compagnucci <angelo@amarulasolutions.com>
    [yann.morin.1998@free.fr: split off into its own patch]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    Tested-by: Angelo Compagnucci <angelo@amarulasolutions.com>

--- package/libapparmor/libapparmor.mk
+++ package/libapparmor/libapparmor.mk
@@ -15,13 +15,26 @@ LIBAPPARMOR_DEPENDENCIES = host-bison host-flex host-pkgconf
 LIBAPPARMOR_SUBDIR = libraries/libapparmor
 LIBAPPARMOR_INSTALL_STAGING = YES
 
+# Patches 0001 and 0002 touch Makefile.am and an m4 file
+LIBAPPARMOR_AUTORECONF = YES
+
 # Most AppArmor tools will want to link to the static lib.
 # ac_cv_prog_cc_c99 is required for BR2_USE_WCHAR=n because the C99 test
 # provided by autoconf relies on wchar_t.
 LIBAPPARMOR_CONF_OPTS = \
 	ac_cv_prog_cc_c99=-std=gnu99 \
 	--enable-static \
-	--disable-man-pages \
-	--without-python
+	--disable-man-pages
+
+ifeq ($(BR2_PACKAGE_PYTHON3),y)
+LIBAPPARMOR_DEPENDENCIES += host-python3 host-swig python3
+LIBAPPARMOR_CONF_OPTS += \
+	--with-python \
+	PYTHON=$(HOST_DIR)/usr/bin/python3 \
+	PYTHON_CONFIG=$(STAGING_DIR)/usr/bin/python3-config \
+	SWIG=$(SWIG)
+else
+LIBAPPARMOR_CONF_OPTS += --without-python
+endif
 
 $(eval $(autotools-package))
