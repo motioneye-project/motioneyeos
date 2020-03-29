062d0f6913ed6e787123b32d0d8ffe9703efe3ce
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-03-29 18:02:44 +0200

    package/gvfs: fix CVE-2019-12447
    An issue was discovered in GNOME gvfs 1.29.4 through 1.41.2.
    daemon/gvfsbackendadmin.c mishandles file ownership because setfsuid is
    not used.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/gvfs/gvfs.mk
+++ package/gvfs/gvfs.mk
@@ -21,6 +21,10 @@ GVFS_IGNORE_CVES += CVE-2019-3827
 # package/gvfs/0002-admin-Add-query_info_on_read-write-functionality.patch
 GVFS_IGNORE_CVES += CVE-2019-12448
 
+# 0003-admin-Allow-changing-file-owner.patch
+# 0004-admin-Use-fsuid-to-ensure-correct-file-ownership.patch
+GVFS_IGNORE_CVES += CVE-2019-12447
+
 # Export ac_cv_path_LIBGCRYPT_CONFIG unconditionally to prevent
 # build system from searching the host paths.
 GVFS_CONF_ENV = \
