1e45234ef95b7de1406fdf139216f8d41ede07aa
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-03-29 14:22:59 +0200

    package/kodi-vfs-rar: bump version to 2.2.3-Leia
    Updated hash of lib/UnrarXLib/license.txt due to upstream changes:
    https://github.com/xbmc/vfs.rar/commits/Leia/lib/UnrarXLib/license.txt
    
    Changed addon license file to LICENSE.md.
    
    Updated dependencies due to upstream commit:
    https://github.com/xbmc/vfs.rar/commit/6c7a62439eac2b2afeb2bf8c241836cd2ab2b93e
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/kodi-vfs-rar/kodi-vfs-rar.mk
+++ package/kodi-vfs-rar/kodi-vfs-rar.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-KODI_VFS_RAR_VERSION = 60f92ff28ee6c94211b628990696c60518bffcf6
+KODI_VFS_RAR_VERSION = 2.2.3-Leia
 KODI_VFS_RAR_SITE = $(call github,xbmc,vfs.rar,$(KODI_VFS_RAR_VERSION))
 KODI_VFS_RAR_LICENSE = unrar, GPL-2.0+
-KODI_VFS_RAR_LICENSE_FILES = lib/UnrarXLib/license.txt debian/copyright
-KODI_VFS_RAR_DEPENDENCIES = libplatform kodi
+KODI_VFS_RAR_LICENSE_FILES = lib/UnrarXLib/license.txt LICENSE.md
+KODI_VFS_RAR_DEPENDENCIES = kodi tinyxml
 
 $(eval $(cmake-package))
