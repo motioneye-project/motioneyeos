1cb6009dda535786d566611e2ee0c651e3174fd7
James Hilliard <james.hilliard1@gmail.com>
2020-03-31 11:53:25 -0600

    package/cog: bump to version 0.6.0
    Drop patches that are now upstream.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Reviewed-by: Adrian Perez de Castro <aperez@igalia.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/cog/cog.mk
+++ package/cog/cog.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-COG_VERSION = 0.4.0
+COG_VERSION = 0.6.0
 COG_SITE = https://wpewebkit.org/releases
 COG_SOURCE = cog-$(COG_VERSION).tar.xz
 COG_INSTALL_STAGING = YES
@@ -15,6 +15,8 @@ COG_CONF_OPTS = \
 	-DCOG_BUILD_PROGRAMS=ON \
 	-DCOG_PLATFORM_FDO=ON \
 	-DCOG_PLATFORM_DRM=OFF \
+	-DCOG_WESTON_DIRECT_DISPLAY=OFF \
+	-DINSTALL_MAN_PAGES=OFF \
 	-DCOG_HOME_URI='$(call qstrip,$(BR2_PACKAGE_COG_PROGRAMS_HOME_URI))'
 
 $(eval $(cmake-package))
