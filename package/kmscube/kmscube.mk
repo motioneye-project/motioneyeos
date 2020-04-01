396191b1562072de11a702ffb9d61e7de9f40c04
Fabio Estevam <festevam@gmail.com>
2020-04-01 14:05:03 -0300

    kmscube: Change repository to gitlab
    The https://cgit.freedesktop.org/mesa/kmscube repository
    is mirrored from https://gitlab.freedesktop.org/mesa/kmscube, so
    switch to the gitlab one.
    
    The other advantage of using the gitlab repository is that it can handle
    archive downloads, so switch to it.
    
    Suggested-by: Arnout Vandecappelle <arnout@mind.be>
    Signed-off-by: Fabio Estevam <festevam@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/kmscube/kmscube.mk
+++ package/kmscube/kmscube.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 KMSCUBE_VERSION = 6cbd03ab94066dddbba7bedfde87c7c4319c18d5
-KMSCUBE_SITE = https://cgit.freedesktop.org/mesa/kmscube/snapshot
+KMSCUBE_SITE = https://gitlab.freedesktop.org/mesa/kmscube/-/archive/$(KMSCUBE_VERSION)
 KMSCUBE_LICENSE = MIT
 KMSCUBE_DEPENDENCIES = host-pkgconf mesa3d libdrm
 KMSCUBE_AUTORECONF = YES
