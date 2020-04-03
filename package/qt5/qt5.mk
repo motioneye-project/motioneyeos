c527ce8005faf10509a78680c22cd15c7d8ea538
Peter Seiderer <ps.report@gmx.net>
2020-04-03 23:23:56 +0200

    package/qt5: bump version to 5.14.2
    qt5base:
     - rebased 0002-double-conversion-enable-for-microblaze.patch
     - rebased 0004-double-conversion-enable-for-xtensa.patch
    
    qt5declarative:
     - rebased 0001-qsgtexture-fix-debug-build-with-uclibc.patch
    
    qt5enginio:
     - add 0001-Do-not-use-deprecated-QLinkedList.patch
    
    qt5tools:
     - add 0001-Disable-designer-tool-fixes-configure-error.patch
       ToDo: add proper fix/workaround
    
    qt5webengine:
     - update 3rdparty license hash list and chromium-latest.inc file
    
    qt5webkit:
     - add 0006-configure-remove-legacy-reference-to-qtConfig-mircli.patch
    
    Signed-off-by: Peter Seiderer <ps.report@gmx.net>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/qt5/qt5.mk
+++ package/qt5/qt5.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QT5_VERSION_MAJOR = 5.13
+QT5_VERSION_MAJOR = 5.14
 QT5_VERSION = $(QT5_VERSION_MAJOR).2
 QT5_SOURCE_TARBALL_PREFIX = everywhere-src
 QT5_SITE = https://download.qt.io/archive/qt/$(QT5_VERSION_MAJOR)/$(QT5_VERSION)/submodules
