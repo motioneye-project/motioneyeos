cdb127fecaa001b93d2a203bdca69c095713c1d8
Jianhui Zhao <zhaojh329@gmail.com>
2020-04-04 17:50:40 +0800

    package/rtty: bump version to 7.1.3
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/rtty/rtty.mk
+++ package/rtty/rtty.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RTTY_VERSION = 7.1.2
+RTTY_VERSION = 7.1.3
 RTTY_SITE = https://github.com/zhaojh329/rtty/releases/download/v$(RTTY_VERSION)
 RTTY_LICENSE = MIT
 RTTY_LICENSE_FILES = LICENSE
