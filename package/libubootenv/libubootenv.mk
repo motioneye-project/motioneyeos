585c3e652818a7ef4e98eaec01660d69a803c1dd
Pierre-Jean Texier <pjtexier@koncepto.io>
2020-04-07 12:00:05 +0200

    package/libubootenv: bump to version f4b9cde
    This includes the following changes:
    
    f4b9cde Allow negative offsets
    45bf92a Detect sector size if not found in config
    9f59db6 uboot_env: remove unused variables
    65d243e README: libubootenv is now in oe-core
    
    And update hash file formatting (2 spaces).
    
    Signed-off-by: Pierre-Jean Texier <pjtexier@koncepto.io>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libubootenv/libubootenv.mk
+++ package/libubootenv/libubootenv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBUBOOTENV_VERSION = ba952d05ec9ab16029816a06d956bac7fb4e9832
+LIBUBOOTENV_VERSION = f4b9cde3815abe84a98079cedd515283ea08c16b
 LIBUBOOTENV_SITE = $(call github,sbabic,libubootenv,$(LIBUBOOTENV_VERSION))
 LIBUBOOTENV_LICENSE = LGPL-2.1
 LIBUBOOTENV_LICENSE_FILES = Licenses/lgpl-2.1.txt
