a409dc8202adddc612a9d7b32b419923eb19a600
Grzegorz Blach <grzegorz@blach.pl>
2020-04-08 13:48:08 +0200

    package/python-crontab: bump to version 2.4.1
    Signed-off-by: Grzegorz Blach <grzegorz@blach.pl>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-crontab/python-crontab.mk
+++ package/python-crontab/python-crontab.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_CRONTAB_VERSION = 2.4.0
-PYTHON_CRONTAB_SITE = https://files.pythonhosted.org/packages/bc/03/852f9c5f8da7f58abce06fe8ae769cbcf6502f1c60684b16bad72ffcced3
+PYTHON_CRONTAB_VERSION = 2.4.1
+PYTHON_CRONTAB_SITE = https://files.pythonhosted.org/packages/09/b1/31d3ccf2ad3d4f7727b325ad7ea77d042e1939c5cacbf1d7478e391cca51
 PYTHON_CRONTAB_SETUP_TYPE = setuptools
 PYTHON_CRONTAB_LICENSE = LGPL-3.0+
 PYTHON_CRONTAB_LICENSE_FILES = COPYING
