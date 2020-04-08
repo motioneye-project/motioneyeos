5334d9a200f0b3c8df5db7ad7d609aeb32f62ec1
Julien Olivain <juju@cotds.org>
2020-04-08 20:25:03 +0200

    package/fluidsynth: bump to version 2.1.2
    Changes since v2.1.1:
    https://github.com/FluidSynth/fluidsynth/releases/tag/v2.1.2
    - fluidsynth now exits with error when user-provided command-line arguments are out-of-range
    - add verbose error logging to opensles and oboe drivers
    - fix a memory leak in oboe driver
    - fix a NULL dereference in the fluidsynth commandline program
    
    ./utils/test-pkg --package fluidsynth
    6 builds, 1 skipped, 0 build failed, 0 legal-info failed
    
    Signed-off-by: Julien Olivain <juju@cotds.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/fluidsynth/fluidsynth.mk
+++ package/fluidsynth/fluidsynth.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FLUIDSYNTH_VERSION = 2.1.1
+FLUIDSYNTH_VERSION = 2.1.2
 FLUIDSYNTH_SITE = $(call github,FluidSynth,fluidsynth,v$(FLUIDSYNTH_VERSION))
 FLUIDSYNTH_LICENSE = LGPL-2.1+
 FLUIDSYNTH_LICENSE_FILES = LICENSE
