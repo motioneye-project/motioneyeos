afeae17c365f15d810cc974db06da9a1c8eb4bc9
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-04-10 12:39:43 +0200

    package/fmt: bump version to 6.2.0
    Added upstream commit to fix Kodi build error.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/fmt/fmt.mk
+++ package/fmt/fmt.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FMT_VERSION = 6.1.2
+FMT_VERSION = 6.2.0
 FMT_SITE = $(call github,fmtlib,fmt,$(FMT_VERSION))
 FMT_LICENSE = MIT with exception
 FMT_LICENSE_FILES = LICENSE.rst
