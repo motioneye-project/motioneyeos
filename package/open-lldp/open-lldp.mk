27515371fa2be1df0e1074e7dac8a627da3ca6af
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-04-11 19:27:06 +0200

    package/open-lldp: bump version to b71bfb87fefb31c4b1a6a7ae351791c90966c3a8
    - Switch site to github
    - Remove first patch (already in version)
    - Update second patch
    - Update indentation of hash file (two spaces)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/open-lldp/open-lldp.mk
+++ package/open-lldp/open-lldp.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-OPEN_LLDP_VERSION = 036e314bd93602f7388262cc37faf8b626980af1
-OPEN_LLDP_SITE = git://open-lldp.org/open-lldp.git
+OPEN_LLDP_VERSION = b71bfb87fefb31c4b1a6a7ae351791c90966c3a8
+OPEN_LLDP_SITE = $(call github,intel,openlldp,$(OPEN_LLDP_VERSION))
 OPEN_LLDP_DEPENDENCIES = readline libnl libconfig host-pkgconf
 OPEN_LLDP_LICENSE = GPL-2.0
 OPEN_LLDP_LICENSE_FILES = COPYING
