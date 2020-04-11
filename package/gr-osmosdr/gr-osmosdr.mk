e54e960ebd4c536627a9a1db41e1b8fba0a83261
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2020-04-11 11:33:53 +0200

    package/gr-osmosdr: bump to 0.2.0
    gr-osmosdr is now python3 only, so drop everything related to python2.
    python-cheetah is no longer mandatory, so drop this too.
    
    Signed-off-by: Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/gr-osmosdr/gr-osmosdr.mk
+++ package/gr-osmosdr/gr-osmosdr.mk
@@ -4,15 +4,15 @@
 #
 ################################################################################
 
-GR_OSMOSDR_VERSION = 46698a67e7e06419eefaf45c95129c0c12669045
-GR_OSMOSDR_SITE = $(call github,osmocom,gr-osmosdr,$(GR_OSMOSDR_VERSION))
+GR_OSMOSDR_VERSION = 0.2.0
+GR_OSMOSDR_SITE = $(call github,osmocom,gr-osmosdr,v$(GR_OSMOSDR_VERSION))
 GR_OSMOSDR_LICENSE = GPL-3.0+
 GR_OSMOSDR_LICENSE_FILES = COPYING
 
 # gr-osmosdr prevents doing an in-source-tree build
 GR_OSMOSDR_SUPPORTS_IN_SOURCE_BUILD = NO
 
-GR_OSMOSDR_DEPENDENCIES = gnuradio host-python-cheetah
+GR_OSMOSDR_DEPENDENCIES = gnuradio host-python3
 
 GR_OSMOSDR_CONF_OPTS = \
 	-DENABLE_DEFAULT=OFF \
@@ -24,7 +24,7 @@ GR_OSMOSDR_INSTALL_STAGING = YES
 
 ifeq ($(BR2_PACKAGE_GR_OSMOSDR_PYTHON),y)
 GR_OSMOSDR_CONF_OPTS += -DENABLE_PYTHON=ON
-GR_OSMOSDR_DEPENDENCIES += $(if $(BR2_PACKAGE_PYTHON3),python3,python)
+GR_OSMOSDR_DEPENDENCIES += python3
 else
 GR_OSMOSDR_CONF_OPTS += -DENABLE_PYTHON=OFF
 endif
