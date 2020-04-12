44090c97e10541ef1570f3e29222b3c1e6536e09
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-04-12 16:14:39 +0200

    package/mutt: bump version to 1.13.5
    Changelog: http://www.mutt.org/
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mutt/mutt.mk
+++ package/mutt/mutt.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MUTT_VERSION = 1.13.3
+MUTT_VERSION = 1.13.5
 MUTT_SITE = https://bitbucket.org/mutt/mutt/downloads
 MUTT_LICENSE = GPL-2.0+
 MUTT_LICENSE_FILES = GPL
