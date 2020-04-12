b2960a6dc1868bd1fd48d4862aff7bfc31712bc9
Baruch Siach <baruch@tkos.co.il>
2020-04-12 19:40:02 +0300

    package/can-utils: bump to version 2020.02.04
    Cc: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Baruch Siach <baruch@tkos.co.il>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/can-utils/can-utils.mk
+++ package/can-utils/can-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAN_UTILS_VERSION = 2020.02.0
+CAN_UTILS_VERSION = 2020.02.04
 CAN_UTILS_SITE = $(call github,linux-can,can-utils,v$(CAN_UTILS_VERSION))
 CAN_UTILS_LICENSE = BSD-3-Clause or GPL-2.0
 CAN_UTILS_LICENSE_FILES = LICENSES/BSD-3-Clause LICENSES/GPL-2.0-only.txt
