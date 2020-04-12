8961000fe8ae1129bacf44adf219d9c4e4307d77
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-04-12 16:59:35 +0200

    package/openldap: bump version to 2.4.49
    Release notes:
    https://lists.openldap.org/hyperkitty/list/openldap-announce@openldap.org/thread/VMMBUCQHEDF6QA4CDOONP2CDQEOR5YQA/
    
    Switched _SITE to https.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/openldap/openldap.mk
+++ package/openldap/openldap.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-OPENLDAP_VERSION = 2.4.48
+OPENLDAP_VERSION = 2.4.49
 OPENLDAP_SOURCE = openldap-$(OPENLDAP_VERSION).tgz
-OPENLDAP_SITE = http://www.openldap.org/software/download/OpenLDAP/openldap-release
+OPENLDAP_SITE = https://www.openldap.org/software/download/OpenLDAP/openldap-release
 OPENLDAP_LICENSE = OpenLDAP Public License
 OPENLDAP_LICENSE_FILES = LICENSE
 OPENLDAP_INSTALL_STAGING = YES
