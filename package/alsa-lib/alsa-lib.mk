f0cf16ca0a41cce5d1afa9f837053f6756b241e8
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-04-13 13:53:01 +0200

    package/alsa-lib: bump version to 1.2.2
    Changelog: https://www.alsa-project.org/wiki/Changes_v1.2.1.2_v1.2.2
    
    Removed patch applied upstream:
    https://github.com/alsa-project/alsa-lib/commit/8e2c70add782f997f7c269ed3f722888e56ff024
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    [yann.morin.1998@free.fr: two spaces in hash file]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/alsa-lib/alsa-lib.mk
+++ package/alsa-lib/alsa-lib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ALSA_LIB_VERSION = 1.2.1.2
+ALSA_LIB_VERSION = 1.2.2
 ALSA_LIB_SOURCE = alsa-lib-$(ALSA_LIB_VERSION).tar.bz2
 ALSA_LIB_SITE = https://www.alsa-project.org/files/pub/lib
 ALSA_LIB_LICENSE = LGPL-2.1+ (library), GPL-2.0+ (aserver)
