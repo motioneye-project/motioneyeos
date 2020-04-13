7ef2a3cc435f4642a7faa3ca98c63cfa810e7e8d
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-04-13 09:43:15 +0200

    package/tinyxml2: bump version to 8.0.0
    Switched license file to LICENSE.txt due to upstream commit:
    https://github.com/leethomason/tinyxml2/commit/db39dbc0fdf0376674b0fd94a6ff9f16a4d5b174
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/tinyxml2/tinyxml2.mk
+++ package/tinyxml2/tinyxml2.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-TINYXML2_VERSION = 7.1.0
+TINYXML2_VERSION = 8.0.0
 TINYXML2_SITE = $(call github,leethomason,tinyxml2,$(TINYXML2_VERSION))
 TINYXML2_LICENSE = Zlib
-TINYXML2_LICENSE_FILES = readme.md
+TINYXML2_LICENSE_FILES = LICENSE.txt
 TINYXML2_INSTALL_STAGING = YES
 
 ifeq ($(BR2_STATIC_LIBS),y)
