4aa6345bebce882dd4b3b2dc6e5ffe61440cf937
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-04-13 13:43:37 +0200

    package/libglfw: bump version to 3.3.2
    Changelog: https://www.glfw.org/changelog.html
    
    Updated license hash due to upstream commit:
    https://github.com/glfw/glfw/commit/f764836e58ec47b7e105687630c75f8161ffcaa8
    
    xlib_libXi is now a mandatory dependency:
    https://github.com/glfw/glfw/commit/9cc83cf07f47ac8acf46ae12f0f9312fe39cfd6e
    https://github.com/glfw/glfw/commit/b383e70021bc875e6fb5c1a693e5aa85fbc160d5
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    [yann.morin.1998@free.fr: two spaces in hash file]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libglfw/libglfw.mk
+++ package/libglfw/libglfw.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-LIBGLFW_VERSION = 3.3
+LIBGLFW_VERSION = 3.3.2
 LIBGLFW_SITE = $(call github,glfw,glfw,$(LIBGLFW_VERSION))
 LIBGLFW_INSTALL_STAGING = YES
 LIBGLFW_DEPENDENCIES = libgl xlib_libXcursor xlib_libXext \
-	xlib_libXinerama xlib_libXrandr
+	xlib_libXi xlib_libXinerama xlib_libXrandr
 LIBGLFW_LICENSE = Zlib
 LIBGLFW_LICENSE_FILES = LICENSE.md
 
@@ -16,10 +16,6 @@ LIBGLFW_CONF_OPTS += \
 	-DGLFW_BUILD_EXAMPLES=OFF \
 	-DGLFW_BUILD_TESTS=OFF \
 	-DGLFW_BUILD_DOCS=OFF
-
-ifeq ($(BR2_PACKAGE_XLIB_LIBXI),y)
-LIBGLFW_DEPENDENCIES += xlib_libXi
-endif
 
 ifeq ($(BR2_PACKAGE_XLIB_LIBXXF86VM),y)
 LIBGLFW_DEPENDENCIES += xlib_libXxf86vm
