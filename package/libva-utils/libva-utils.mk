1485eb77e186b369b190729cd10a9a6bb5b600ba
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-04-13 15:47:12 +0200

    package/libva-utils: switch back to upstream-provided tarball
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libva-utils/libva-utils.mk
+++ package/libva-utils/libva-utils.mk
@@ -5,11 +5,10 @@
 ################################################################################
 
 LIBVA_UTILS_VERSION = 2.7.1
-LIBVA_UTILS_SITE = $(call github,intel,libva-utils,$(LIBVA_UTILS_VERSION))
+LIBVA_UTILS_SOURCE = libva-utils-$(LIBVA_UTILS_VERSION).tar.bz2
+LIBVA_UTILS_SITE = https://github.com/intel/libva-utils/releases/download/$(LIBVA_UTILS_VERSION)
 LIBVA_UTILS_LICENSE = MIT
 LIBVA_UTILS_LICENSE_FILES = COPYING
-# github tarball does not include configure
-LIBVA_UTILS_AUTORECONF = YES
 LIBVA_UTILS_DEPENDENCIES = host-pkgconf libva
 
 $(eval $(autotools-package))
