22d784d5753ad522328f4fe4be7f993a32fb6a91
James Hilliard <james.hilliard1@gmail.com>
2020-04-13 16:55:19 -0600

    package/python-cryptography: bump to version 2.9
    We can remove CFLAGS="$(TARGET_CFLAGS) -pthread" as of upstream commit:
    https://github.com/pyca/cryptography/commit/085382fb17a560dd11e4710077b63a26d5dcf05c
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-cryptography/python-cryptography.mk
+++ package/python-cryptography/python-cryptography.mk
@@ -4,13 +4,12 @@
 #
 ################################################################################
 
-PYTHON_CRYPTOGRAPHY_VERSION = 2.8
+PYTHON_CRYPTOGRAPHY_VERSION = 2.9
 PYTHON_CRYPTOGRAPHY_SOURCE = cryptography-$(PYTHON_CRYPTOGRAPHY_VERSION).tar.gz
-PYTHON_CRYPTOGRAPHY_SITE = https://files.pythonhosted.org/packages/be/60/da377e1bed002716fb2d5d1d1cab720f298cb33ecff7bf7adea72788e4e4
+PYTHON_CRYPTOGRAPHY_SITE = https://files.pythonhosted.org/packages/9d/0a/d7060601834b1a0a84845d6ae2cd59be077aafa2133455062e47c9733024
 PYTHON_CRYPTOGRAPHY_SETUP_TYPE = setuptools
 PYTHON_CRYPTOGRAPHY_LICENSE = Apache-2.0 or BSD-3-Clause
 PYTHON_CRYPTOGRAPHY_LICENSE_FILES = LICENSE LICENSE.APACHE LICENSE.BSD
 PYTHON_CRYPTOGRAPHY_DEPENDENCIES = host-python-cffi openssl
-PYTHON_CRYPTOGRAPHY_ENV = CFLAGS="$(TARGET_CFLAGS) -pthread"
 
 $(eval $(python-package))
