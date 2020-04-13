2306339d1fe8d5ea2d9e17641f78a374f612f45e
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2020-04-13 21:49:13 +0200

    board/udoo/neo: create boot.scr in TARGET_DIR/boot
    There is no reason to create boot.scr in board/udoo/neo and later
    install it in TARGET_DIR/boot, leaving a stale file behind.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- board/udoo/neo/post-build.sh
+++ board/udoo/neo/post-build.sh
@@ -2,7 +2,6 @@
 
 BOARD_DIR="$(dirname $0)"
 
+install -d -m 755 $TARGET_DIR/boot
 $HOST_DIR/bin/mkimage -A arm -O linux -T script -C none  \
--n "boot script" -d $BOARD_DIR/boot.scr.txt $BOARD_DIR/boot.scr
-
-install -m 0644 -D $BOARD_DIR/boot.scr $TARGET_DIR/boot/boot.scr
+	-n "boot script" -d $BOARD_DIR/boot.scr.txt $TARGET_DIR/boot/boot.scr
