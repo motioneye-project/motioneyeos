d19d0e776db48a0569c95c4e9d5b29dfa3291950
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-04-14 18:15:11 +0200

    package/btrfs-progs: bump version to 5.6
    Changelog:
    https://btrfs.wiki.kernel.org/index.php/Changelog#By_version_.28btrfs-progs.29
    
    Removed patches applied upstream:
    https://git.kernel.org/pub/scm/linux/kernel/git/kdave/btrfs-progs.git/commit/?id=9a03f6d41f8e6b3312dc0c87563d8dbd10eafafe
    https://git.kernel.org/pub/scm/linux/kernel/git/kdave/btrfs-progs.git/commit/?id=4b3e22e988c26c6c814f9ad3f50f493cd4d9dd2a
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/btrfs-progs/btrfs-progs.mk
+++ package/btrfs-progs/btrfs-progs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BTRFS_PROGS_VERSION = 5.2.1
+BTRFS_PROGS_VERSION = 5.6
 BTRFS_PROGS_SITE = $(BR2_KERNEL_MIRROR)/linux/kernel/people/kdave/btrfs-progs
 BTRFS_PROGS_SOURCE = btrfs-progs-v$(BTRFS_PROGS_VERSION).tar.xz
 BTRFS_PROGS_DEPENDENCIES = host-pkgconf lzo util-linux zlib
