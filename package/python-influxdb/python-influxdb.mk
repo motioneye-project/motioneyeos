8647b4d01a715fa25a03aa830b554a96b14842e5
James Hilliard <james.hilliard1@gmail.com>
2020-04-13 19:55:49 -0600

    package/python-influxdb: bump to version 5.3.0
    As of upstream commit 3a5e6219a2b62af076caa4f37fab9258165ec266 msgpack
    is now required.
    
    License hash change is due to date update in upstream commit
    cf83d1d429ecebb093f6eaab773bd41c251530ca.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-influxdb/python-influxdb.mk
+++ package/python-influxdb/python-influxdb.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_INFLUXDB_VERSION = 5.2.3
+PYTHON_INFLUXDB_VERSION = 5.3.0
 PYTHON_INFLUXDB_SOURCE = influxdb-$(PYTHON_INFLUXDB_VERSION).tar.gz
-PYTHON_INFLUXDB_SITE = https://files.pythonhosted.org/packages/d2/0d/351a346886ecbe61211cbfcad8ac73f99f5a9bf526916631c5668dbad601
+PYTHON_INFLUXDB_SITE = https://files.pythonhosted.org/packages/be/8d/85ec8f11299a6dfc115244db71fd8f13e9a69f5e9eb77dc3392f4f959e9a
 PYTHON_INFLUXDB_SETUP_TYPE = setuptools
 PYTHON_INFLUXDB_LICENSE = MIT
 PYTHON_INFLUXDB_LICENSE_FILES = LICENSE
