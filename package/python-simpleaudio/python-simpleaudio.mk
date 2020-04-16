19d5d2af6b283cd4d7017375989a4558bf280547
James Hilliard <james.hilliard1@gmail.com>
2020-04-16 05:28:08 -0600

    package/python-simpleaudio: bump to version 1.0.4
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-simpleaudio/python-simpleaudio.mk
+++ package/python-simpleaudio/python-simpleaudio.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SIMPLEAUDIO_VERSION = 1.0.2
+PYTHON_SIMPLEAUDIO_VERSION = 1.0.4
 PYTHON_SIMPLEAUDIO_SOURCE = simpleaudio-$(PYTHON_SIMPLEAUDIO_VERSION).tar.gz
-PYTHON_SIMPLEAUDIO_SITE = https://files.pythonhosted.org/packages/ad/39/ce09ef827887cdfc755427195d7291446c1fb34a16356d1fec920d62269d
+PYTHON_SIMPLEAUDIO_SITE = https://files.pythonhosted.org/packages/94/1b/4dc29653733202b68c09d9c6ca085cf67ac54859ee860647ef21ac1ff3dc
 PYTHON_SIMPLEAUDIO_LICENSE = MIT
 PYTHON_SIMPLEAUDIO_LICENSE_FILES = LICENSE.txt
 PYTHON_SIMPLEAUDIO_SETUP_TYPE = setuptools
