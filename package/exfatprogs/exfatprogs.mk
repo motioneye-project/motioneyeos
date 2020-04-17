e5bc21559b524d70c1ff3b74cabd4262a3fdef1b
James Hilliard <james.hilliard1@gmail.com>
2020-04-17 14:14:05 -0600

    package/exfatprogs: new package
    exFAT filesystem userspace utilities
    
    We need a patch to fix:
    error: unknown type name ‘ssize_t’
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

---
+++ package/exfatprogs/exfatprogs.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# exfatprogs
+#
+################################################################################
+
+EXFATPROGS_VERSION = 1.0.1
+EXFATPROGS_SITE = $(call github,exfatprogs,exfatprogs,$(EXFATPROGS_VERSION))
+EXFATPROGS_LICENSE = GPL-2.0+
+EXFATPROGS_LICENSE_FILES = COPYING
+EXFATPROGS_AUTORECONF = YES
+EXFATPROGS_DEPENDENCIES = host-pkgconf
+HOST_EXFATPROGS_DEPENDENCIES = host-pkgconf
+
+$(eval $(autotools-package))
+$(eval $(host-autotools-package))
