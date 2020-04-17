65d3813d510e548b556824db7bcd9a1b8f8aff81
Adrien Gallouët <adrien@gallouet.fr>
2020-04-17 13:59:40 +0000

    package/glorytun: bump version to 0.3.4
    The hash of the license file has changed due to year update:
    
    -Copyright (c) 2015-2019, Adrien Gallouët <adrien@gallouet.fr>
    +Copyright (c) 2015-2020, Adrien Gallouët <adrien@gallouet.fr>
    
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/glorytun/glorytun.mk
+++ package/glorytun/glorytun.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GLORYTUN_VERSION = 0.2.2
+GLORYTUN_VERSION = 0.3.4
 GLORYTUN_SITE = https://github.com/angt/glorytun/releases/download/v$(GLORYTUN_VERSION)
 GLORYTUN_DEPENDENCIES = libsodium host-pkgconf
 GLORYTUN_LICENSE = BSD-2-clause
