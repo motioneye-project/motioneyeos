b9e20e96963e2d166e761fbb2d3a17677be49c89
Adam Duskett <Aduskett@gmail.com>
2020-04-17 20:15:56 -0700

    package/pkcs11-helper: bump version to 1.26
    Signed-off-by: Adam Duskett <Aduskett@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/pkcs11-helper/pkcs11-helper.mk
+++ package/pkcs11-helper/pkcs11-helper.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PKCS11_HELPER_VERSION = 1.25.1
+PKCS11_HELPER_VERSION = 1.26
 PKCS11_HELPER_SITE = $(call github,OpenSC,pkcs11-helper,pkcs11-helper-$(PKCS11_HELPER_VERSION))
 PKCS11_HELPER_LICENSE = GPL-2.0 or BSD-3-Clause
 PKCS11_HELPER_LICENSE_FILES = COPYING COPYING.BSD COPYING.GPL
