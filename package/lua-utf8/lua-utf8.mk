0f0649140f73645f4cd00c9c0d27c50e9fa1caf9
Francois Perrad <fperrad@gmail.com>
2020-04-20 08:07:15 +0200

    package/lua-utf8: bump to version 0.1.2
    switch of rocks maintainer
    from https://luarocks.org/modules/dannote/utf8
    to https://luarocks.org/modules/xavier-wang/luautf8
    
    the upstream author stays http://github.com/starwing/luautf8
    
    now, the version scheme is aligned on the upstream one,
    and the version 0.1.2 was released on 06 Apr 2020.
    
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/lua-utf8/lua-utf8.mk
+++ package/lua-utf8/lua-utf8.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-LUA_UTF8_VERSION = 1.2-0
-LUA_UTF8_NAME_UPSTREAM = utf8
-LUA_UTF8_SUBDIR = luautf8
+LUA_UTF8_VERSION = 0.1.2-2
+LUA_UTF8_NAME_UPSTREAM = luautf8
 LUA_UTF8_LICENSE = MIT
+LUA_UTF8_LICENSE_FILES = $(LUA_UTF8_SUBDIR)/LICENSE
 
 $(eval $(luarocks-package))
