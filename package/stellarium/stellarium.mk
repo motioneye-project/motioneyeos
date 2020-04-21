e1504b04983d385a069b4c33dd32a12e508f28d1
Bernd Kuhls <bernd.kuhls@t-online.de>
2020-04-21 07:42:11 +0200

    package/stellarium: bump version to 0.20.1
    Removed patch applied upstream, adjust spaces in hash file.
    
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/stellarium/stellarium.mk
+++ package/stellarium/stellarium.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-STELLARIUM_VERSION = 0.20.0
+STELLARIUM_VERSION = 0.20.1
 STELLARIUM_SITE = https://github.com/Stellarium/stellarium/releases/download/v$(STELLARIUM_VERSION)
 STELLARIUM_LICENSE = GPL-2.0+
 STELLARIUM_LICENSE_FILES = COPYING
