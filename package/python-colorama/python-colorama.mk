5d329c4b71f89b75ac9697eddb9f08d2dc2ca87e
Matt Weber <matthew.weber@rockwellcollins.com>
2020-04-22 09:34:22 -0500

    package/python-colorama: bump to 0.4.3
    Signed-off-by: Matthew Weber <matthew.weber@rockwellcollins.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-colorama/python-colorama.mk
+++ package/python-colorama/python-colorama.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_COLORAMA_VERSION = 0.4.1
+PYTHON_COLORAMA_VERSION = 0.4.3
 PYTHON_COLORAMA_SOURCE = colorama-$(PYTHON_COLORAMA_VERSION).tar.gz
-PYTHON_COLORAMA_SITE = https://files.pythonhosted.org/packages/76/53/e785891dce0e2f2b9f4b4ff5bc6062a53332ed28833c7afede841f46a5db
+PYTHON_COLORAMA_SITE = https://files.pythonhosted.org/packages/82/75/f2a4c0c94c85e2693c229142eb448840fba0f9230111faa889d1f541d12d
 PYTHON_COLORAMA_SETUP_TYPE = setuptools
 PYTHON_COLORAMA_LICENSE = BSD-3-Clause
 PYTHON_COLORAMA_LICENSE_FILES = LICENSE.txt
