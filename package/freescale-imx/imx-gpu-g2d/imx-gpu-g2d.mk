cd3dc5b4fa59bdb14c3b5f1b46b9a4466982b03b
Timo Ketola <timo.ketola@exertus.fi>
2020-04-23 07:51:49 +0300

    package/freescale-imx/imx-gpu-g2d: fix the file name for ARM
    NXP apparently changed file naming convention for this particular file.
    
    Fixes: 1d0ea8d433 (package/freescale-imx/imx-gpu-g2d: bump to version 6.4.0.p1.0)
    Signed-off-by: Timo Ketola <timo.ketola@exertus.fi>
    Reviewed-by: Gary Bisson <gary.bisson@boundarydevices.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/freescale-imx/imx-gpu-g2d/imx-gpu-g2d.mk
+++ package/freescale-imx/imx-gpu-g2d/imx-gpu-g2d.mk
@@ -7,7 +7,7 @@
 ifeq ($(BR2_aarch64),y)
 IMX_GPU_G2D_VERSION = 6.4.0.p1.0-aarch64
 else
-IMX_GPU_G2D_VERSION = 6.4.0.p1.0-aarch32
+IMX_GPU_G2D_VERSION = 6.4.0.p1.0-arm
 endif
 IMX_GPU_G2D_SITE = $(FREESCALE_IMX_SITE)
 IMX_GPU_G2D_SOURCE = imx-gpu-g2d-$(IMX_GPU_G2D_VERSION).bin
