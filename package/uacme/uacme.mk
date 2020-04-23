5946c1fe99f90c8f78170e6eed78adb03860e1d6
Nicola Di Lieto <nicola.dilieto@gmail.com>
2020-04-23 07:51:04 +0200

    package/uacme: bump version to 1.2.3
    This version fixes https://github.com/ndilieto/uacme/issues/22
    
    Signed-off-by: Nicola Di Lieto <nicola.dilieto@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/uacme/uacme.mk
+++ package/uacme/uacme.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UACME_VERSION = 1.2.2
+UACME_VERSION = 1.2.3
 # Released versions are on branch upstream/latest, tagged as
 # upstream/X.Y.Z Do not use vX.Y.Z tags from master, as they do not
 # include .tarball-version
