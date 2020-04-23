2af08373668be47e858d233962dd3c55b69de465
Francois Perrad <fperrad@gmail.com>
2020-04-23 08:33:19 +0200

    package/luafilesystem: bump to version 1.8.0
    Signed-off-by: Francois Perrad <francois.perrad@gadz.org>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/luafilesystem/luafilesystem.mk
+++ package/luafilesystem/luafilesystem.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LUAFILESYSTEM_VERSION = 1.7.0-2
+LUAFILESYSTEM_VERSION = 1.8.0-1
 LUAFILESYSTEM_SUBDIR = luafilesystem
 LUAFILESYSTEM_LICENSE = MIT
 LUAFILESYSTEM_LICENSE_FILES = $(LUAFILESYSTEM_SUBDIR)/LICENSE
