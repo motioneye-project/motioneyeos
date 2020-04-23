d5503c5cd37ea319d97aaaf22fc318d298ded0e8
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-04-23 12:44:41 +0200

    package/linknx: bump to version 0.0.1.38
    - Drop first and second patches (already in version)
    - Retrieve third patch from upstream
    - Retrieve an additionnal patch from upstream
    - host-gettext is needed for AM_ICONV since
      https://github.com/linknx/linknx/commit/0512f6d0a07d50df33bfdca22d48360d164922a8
    - Update indentation of hash file (two spaces)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/linknx/linknx.mk
+++ package/linknx/linknx.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LINKNX_VERSION = 0.0.1.37
+LINKNX_VERSION = 0.0.1.38
 LINKNX_SITE = $(call github,linknx,linknx,$(LINKNX_VERSION))
 LINKNX_LICENSE = GPL-2.0+
 LINKNX_LICENSE_FILES = LICENSE
@@ -17,7 +17,9 @@ LINKNX_CONF_OPTS = \
 	--with-pth=$(STAGING_DIR)/usr \
 	--disable-smtp
 
+# add host-gettext for AM_ICONV macro
 LINKNX_DEPENDENCIES = \
+	host-gettext \
 	host-pkgconf \
 	libpthsem \
 	$(if $(BR2_PACKAGE_ARGP_STANDALONE),argp-standalone) \
