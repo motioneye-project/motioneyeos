ab98c1ffb65b10ede668356014481f63ac9a72e6
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-04-23 12:14:03 +0200

    package/python-jedi: add missing license
    python-jedi bundles its own copy of typeshed since version 0.14.0 and
    https://github.com/davidhalter/jedi/commit/7d2b7bb3c1c4f79f325ccbde9d2b2734aa872312
    
    So add it to the license files (and update indentation of hash file to
    two spaces while at it)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-jedi/python-jedi.mk
+++ package/python-jedi/python-jedi.mk
@@ -8,7 +8,8 @@ PYTHON_JEDI_VERSION = 0.15.1
 PYTHON_JEDI_SOURCE = jedi-$(PYTHON_JEDI_VERSION).tar.gz
 PYTHON_JEDI_SITE = https://files.pythonhosted.org/packages/85/03/cd5a6e44a5753b4d539288d9d1f9645caac889c17dd2950292a8818f86b2
 PYTHON_JEDI_SETUP_TYPE = setuptools
-PYTHON_JEDI_LICENSE = MIT, BSD-3-Clause (flask theme)
-PYTHON_JEDI_LICENSE_FILES = LICENSE.txt docs/_themes/flask/LICENSE
+PYTHON_JEDI_LICENSE = MIT, BSD-3-Clause (flask theme), Apache-2.0 (typeshed)
+PYTHON_JEDI_LICENSE_FILES = \
+	LICENSE.txt docs/_themes/flask/LICENSE jedi/third_party/typeshed/LICENSE
 
 $(eval $(python-package))
