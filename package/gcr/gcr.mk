a905ccf8c09710b846c87695a44297f02e8d5e4c
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-04-24 14:37:15 +0200

    package/gcr: gobject-introspection needs xsltproc
    Build of gcr with gobject-introspection needs host-libxslt otherwise
    build will fail on:
    
      XSLTPROC Gcr-3.gir
    /bin/bash: xsltproc: command not found
    Makefile:6878: recipe for target 'Gcr-3.gir' failed
    
    Fixes:
     - http://autobuild.buildroot.org/results/c08efff174d4773074e6cf3374228b48e057917b
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/gcr/gcr.mk
+++ package/gcr/gcr.mk
@@ -24,7 +24,7 @@ GCR_LICENSE = LGPL-2.1+
 GCR_LICENSE_FILES = COPYING
 
 ifeq ($(BR2_PACKAGE_GOBJECT_INTROSPECTION),y)
-GCR_DEPENDENCIES += gobject-introspection
+GCR_DEPENDENCIES += gobject-introspection host-libxslt
 GCR_CONF_OPTS += --with-introspection
 else
 GCR_CONF_OPTS += --without-introspection
