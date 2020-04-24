9f2035996c0e0a480e57b62c79a73d75dd573b57
James Hilliard <james.hilliard1@gmail.com>
2020-04-24 01:22:29 -0600

    package/exfatprogs: bump to version 1.0.2
    Drop patch that is now upstream.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/exfatprogs/exfatprogs.mk
+++ package/exfatprogs/exfatprogs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EXFATPROGS_VERSION = 1.0.1
+EXFATPROGS_VERSION = 1.0.2
 EXFATPROGS_SITE = $(call github,exfatprogs,exfatprogs,$(EXFATPROGS_VERSION))
 EXFATPROGS_LICENSE = GPL-2.0+
 EXFATPROGS_LICENSE_FILES = COPYING
