e431e04c90dbe6434e26b6f8acd92620a21c71e8
Ryan Barnett <ryanbarnett3@gmail.com>
2020-04-24 19:05:42 -0500

    package/python-tornado: bump version to 6.0.4
    Convert the hash file to using the two space format for hashes. The
    has for the LICENSE file has been updated since version 6.0.4 now
    includes DOS line endings (\r\n).
    
    Signed-off-by: Ryan Barnett <ryanbarnett3@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/python-tornado/python-tornado.mk
+++ package/python-tornado/python-tornado.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TORNADO_VERSION = 6.0.3
+PYTHON_TORNADO_VERSION = 6.0.4
 PYTHON_TORNADO_SOURCE = tornado-$(PYTHON_TORNADO_VERSION).tar.gz
-PYTHON_TORNADO_SITE = https://files.pythonhosted.org/packages/30/78/2d2823598496127b21423baffaa186b668f73cd91887fcef78b6eade136b
+PYTHON_TORNADO_SITE = https://files.pythonhosted.org/packages/95/84/119a46d494f008969bf0c775cb2c6b3579d3c4cc1bb1b41a022aa93ee242
 PYTHON_TORNADO_LICENSE = Apache-2.0
 PYTHON_TORNADO_LICENSE_FILES = LICENSE
 PYTHON_TORNADO_SETUP_TYPE = setuptools
