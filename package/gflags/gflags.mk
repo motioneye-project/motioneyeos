198e42d6ebdc9a55da8378e437f3e4c20678b94b
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-04-25 18:22:44 +0200

    package/gflags: force Release mode
    Force Release otherwise libraries will be suffixed by _debug which will raise
    unexpected build failures with packages that use gflags (e.g. rocksdb)
    
    Fixes:
     - http://autobuild.buildroot.org/results/3545774258babc3584f69e7d523efdf7fff1acb5
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/gflags/gflags.mk
+++ package/gflags/gflags.mk
@@ -10,8 +10,12 @@ GFLAGS_INSTALL_STAGING = YES
 GFLAGS_LICENSE = BSD-3-Clause
 GFLAGS_LICENSE_FILES = COPYING.txt
 
+# Force Release otherwise libraries will be suffixed by _debug which will raise
+# unexpected build failures with packages that use gflags (e.g. rocksdb)
+GFLAGS_CONF_OPTS = -DCMAKE_BUILD_TYPE=Release
+
 ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),)
-GFLAGS_CONF_OPTS = -DBUILD_gflags_LIB=OFF \
+GFLAGS_CONF_OPTS += -DBUILD_gflags_LIB=OFF \
 	-DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) -DNO_THREADS"
 endif
 
