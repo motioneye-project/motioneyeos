7656cae01e2f5a0168b0b758dbcbc71cb50ba049
Alexander Dahl <post@lespocky.de>
2020-04-27 09:11:43 +0200

    package/tzdata: bump to version 2020a
    Signed-off-by: Alexander Dahl <post@lespocky.de>
    [yann.morin.1998@free.fr: two spaces in hash file]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/tzdata/tzdata.mk
+++ package/tzdata/tzdata.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TZDATA_VERSION = 2019c
+TZDATA_VERSION = 2020a
 TZDATA_SOURCE = tzdata$(TZDATA_VERSION).tar.gz
 TZDATA_SITE = https://www.iana.org/time-zones/repository/releases
 TZDATA_STRIP_COMPONENTS = 0
