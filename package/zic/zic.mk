2f976c31b0b6f34fef8c35e8dc5bccbd87e1c369
Alexander Dahl <post@lespocky.de>
2020-04-27 09:11:44 +0200

    package/zic: bump to version 2020a
    Signed-off-by: Alexander Dahl <post@lespocky.de>
    [yann.morin.1998@free.fr: two spaces in hash file]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/zic/zic.mk
+++ package/zic/zic.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ZIC_VERSION = 2019c
+ZIC_VERSION = 2020a
 ZIC_SOURCE = tzcode$(ZIC_VERSION).tar.gz
 ZIC_SITE = https://www.iana.org/time-zones/repository/releases
 ZIC_STRIP_COMPONENTS = 0
