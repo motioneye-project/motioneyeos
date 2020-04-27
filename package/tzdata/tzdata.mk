5c0c4861bea31b5ecbf13e4a0a0a987c15e0d154
Yann E. MORIN <yann.morin.1998@free.fr>
2020-04-27 22:36:08 +0200

    package/tzdata: do extract to get the license file
    Back in 2014, about 6 years ago now, in my infinite wisdom, I
    decided that extracting the tzdata source was not necessary for
    the target variant, because we would be installing the files
    generated by the host variant, in commit 7aad5daa5d (package/tzdata:
    only compile the zoneinfo once).
    
    However, that did not account for the fact that we would eventually
    like to have the licensing information for tzdata, later added in
    2019, in commit 60889ccdf0 (package/tzdata: bump to version 2019b).
    
    However, that last comit only added the license file to the host
    variant, without explanations why that was so. It turns out that the
    reason it was not added to the target variant is, probably, that he
    source code for the target variant is not extracted, and thus saving
    the license file fails.
    
    But we really want the license file for what goes on into the target.
    
    So, do extract the source code for the target variant, even if only to
    get the license file.
    
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    Cc: Richard Braun <rbraun@sceen.net>
    Cc: Martin Bark <martin@barkynet.com>
    Cc: Christopher McCrory <chrismcc@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/tzdata/tzdata.mk
+++ package/tzdata/tzdata.mk
@@ -11,7 +11,7 @@ TZDATA_STRIP_COMPONENTS = 0
 TZDATA_DEPENDENCIES = host-tzdata
 HOST_TZDATA_DEPENDENCIES = host-zic
 TZDATA_LICENSE = Public domain
-HOST_TZDATA_LICENSE_FILES = LICENSE
+TZDATA_LICENSE_FILES = LICENSE
 
 # Take care when re-ordering this list since this might break zone
 # dependencies
@@ -37,9 +37,6 @@ define TZDATA_SET_LOCALTIME
 	echo "$(TZDATA_LOCALTIME)" >$(TARGET_DIR)/etc/timezone
 endef
 endif
-
-# No need to extract for target, we're using the host-installed files
-TZDATA_EXTRACT_CMDS =
 
 define TZDATA_INSTALL_TARGET_CMDS
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/share/zoneinfo
