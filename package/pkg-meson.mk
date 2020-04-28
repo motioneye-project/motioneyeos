5cff3a8bdfba92e9f61d0984df08f1ecd205c072
James Hilliard <james.hilliard1@gmail.com>
2020-04-28 06:22:12 -0600

    package/pkg-meson.mk: set correct pkgconfig for native dependencies
    This sets the correct build.pkg_config_path for all meson packages so
    that dependencies with native: true will properly use the the host
    pkgconfig instead of the target pkgconfig.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/pkg-meson.mk
+++ package/pkg-meson.mk
@@ -87,6 +87,7 @@ define $(2)_CONFIGURE_CMDS
 		--default-library=$(if $(BR2_STATIC_LIBS),static,shared) \
 		--buildtype=$(if $(BR2_ENABLE_DEBUG),debug,release) \
 		--cross-file=$$($$(PKG)_SRCDIR)/build/cross-compilation.conf \
+		-Dbuild.pkg_config_path=$$(HOST_DIR)/lib/pkgconfig \
 		$$($$(PKG)_CONF_OPTS) \
 		$$($$(PKG)_SRCDIR) $$($$(PKG)_SRCDIR)/build
 endef
