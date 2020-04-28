3fe17ae48d65dc76edea7f961494543708ac7f08
Ryan Barnett <ryanbarnett3@gmail.com>
2020-04-28 14:20:28 -0500

    package/cifs-utils: bump to version 6.10
    In the version bump to 6.10 the following changes were:
    
     * Fix hash file to two spaces format
     * Add patch to respect DESTDIR and optionally install man pages for
       mount.smb3 by utilizing CONFIG_MAN.
     * Pass -std=gnu11 to fix compile issues found with the sourcery-arm
       toolchain with C99 style code errors in smbinfo.c and defintion of
       'struct sa' uisng gnu11 for C11 GNU extensions.
    
    Signed-off-by: Ryan Barnett <ryanbarnett3@gmail.com>
    CC: Yann E. MORIN <yann.morin.1998@free.fr>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/cifs-utils/cifs-utils.mk
+++ package/cifs-utils/cifs-utils.mk
@@ -4,18 +4,21 @@
 #
 ################################################################################
 
-CIFS_UTILS_VERSION = 6.9
+CIFS_UTILS_VERSION = 6.10
 CIFS_UTILS_SOURCE = cifs-utils-$(CIFS_UTILS_VERSION).tar.bz2
 CIFS_UTILS_SITE = http://ftp.samba.org/pub/linux-cifs/cifs-utils
 CIFS_UTILS_LICENSE = GPL-3.0+
 CIFS_UTILS_LICENSE_FILES = COPYING
-# Missing install-sh in release tarball
+# Missing install-sh in release tarball and patching Makefile.am
 CIFS_UTILS_AUTORECONF = YES
 CIFS_UTILS_DEPENDENCIES = host-pkgconf
 
 # Let's disable PIE unconditionally. We want PIE to be enabled only by
 # the global BR2_RELRO_FULL option.
 CIFS_UTILS_CONF_OPTS = --disable-pie --disable-man
+
+# uses C11 code in smbinfo.c and mtab.c
+CIFS_UTILS_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -std=gnu11"
 
 ifeq ($(BR2_PACKAGE_KEYUTILS),y)
 CIFS_UTILS_DEPENDENCIES += keyutils
