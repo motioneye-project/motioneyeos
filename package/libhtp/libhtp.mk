b3d51946964546f26a026c4dea337e8084e64e58
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-04-30 21:42:45 +0200

    package/libhtp: security bump to version 0.5.33
    - ChangeLog:
      - compression bomb protection
      - memory handling issue found by Oss-Fuzz
      - improve handling of anomalies in traffic
    - Drop first patch (already in version)
    - Update indentation of hash file (two spaces)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libhtp/libhtp.mk
+++ package/libhtp/libhtp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBHTP_VERSION = 0.5.32
+LIBHTP_VERSION = 0.5.33
 LIBHTP_SITE = $(call github,OISF,libhtp,$(LIBHTP_VERSION))
 LIBHTP_LICENSE = BSD-3-Clause
 LIBHTP_LICENSE_FILES = LICENSE
