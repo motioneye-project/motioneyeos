347d18c8026db61cc06d3c55f72755ebe0b90cc6
Ryan Barnett <ryanbarnett3@gmail.com>
2020-04-29 20:16:55 -0500

    package/gensio: bump to version 2.0.3
    Signed-off-by: Ryan Barnett <ryanbarnett3@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/gensio/gensio.mk
+++ package/gensio/gensio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GENSIO_VERSION = 2.0.1
+GENSIO_VERSION = 2.0.3
 GENSIO_SITE = http://downloads.sourceforge.net/project/ser2net/ser2net
 GENSIO_LICENSE = LGPL-2.1+ (library), GPL-2.0+ (tools)
 GENSIO_LICENSE_FILES = COPYING.LIB COPYING
