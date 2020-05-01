205557a2879cfedf1b499dc834c902e1db52a7a2
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-05-01 15:32:50 +0200

    package/python-msgfy: bump to version 0.1.0
    - python 2 support has been dropped since version 0.1.0 and
      https://github.com/thombashi/msgfy/commit/8ddfd20132b8d4825214252189a65570b562619f
    - Update indentation of hash file (two spaces)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-msgfy/python-msgfy.mk
+++ package/python-msgfy/python-msgfy.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MSGFY_VERSION = 0.0.7
+PYTHON_MSGFY_VERSION = 0.1.0
 PYTHON_MSGFY_SOURCE = msgfy-$(PYTHON_MSGFY_VERSION).tar.gz
-PYTHON_MSGFY_SITE = https://files.pythonhosted.org/packages/23/82/b61a8353c36e60c2c8291c56ad7d00aa37918ef56811727510acada09f7f
+PYTHON_MSGFY_SITE = https://files.pythonhosted.org/packages/24/b5/7cc6ba75b6489245f9b75f11a85202b934fa92f6c99a4fa1c639f08d68e8
 PYTHON_MSGFY_SETUP_TYPE = setuptools
 PYTHON_MSGFY_LICENSE = MIT
 PYTHON_MSGFY_LICENSE_FILES = LICENSE
