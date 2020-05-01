e845f9364c2b81bbb6888f880bf867132ef6413f
Cody Guldner <cody.guldner@rockwellcollins.com>
2020-05-01 10:25:57 -0500

    ne10: bump to v1.2.1
    Signed-off-by: Cody Guldner <cody.guldner@rockwellcollins.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/ne10/ne10.mk
+++ package/ne10/ne10.mk
@@ -4,13 +4,15 @@
 #
 ################################################################################
 
-NE10_VERSION = 1.2.0
+NE10_VERSION = 1.2.1
 NE10_SITE = $(call github,projectNe10,Ne10,v$(NE10_VERSION))
 NE10_LICENSE = BSD-3-Clause or Apache-2.0
 NE10_LICENSE_FILES = doc/LICENSE
 NE10_INSTALL_STAGING = YES
 
-NE10_CONF_OPTS = -DGNULINUX_PLATFORM=ON
+NE10_CONF_OPTS = \
+	-DGNULINUX_PLATFORM=ON \
+	-DNE10_LINUX_TARGET_ARCH=$(if $(BR2_aarch64),aarch64,armv7)
 
 ifeq ($(BR2_STATIC_LIBS),)
 NE10_CONF_OPTS += \
