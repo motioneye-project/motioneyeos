81ac873097ea052c7fe11e88e6cefd6117ea39b9
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-05-01 15:14:02 +0200

    package/proj: bump to version 7.0.1
    - curl and tiff are optional dependencies since version 7.0.0 and
      https://github.com/OSGeo/PROJ/commit/db31b6dfa9c8fe37d5706d95ce81012b8db3c3b9
    - Update indentation of hash file (two spaces)
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/proj/proj.mk
+++ package/proj/proj.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PROJ_VERSION = 6.3.0
+PROJ_VERSION = 7.0.1
 PROJ_SITE = http://download.osgeo.org/proj
 PROJ_LICENSE = MIT
 PROJ_LICENSE_FILES = COPYING
@@ -22,5 +22,19 @@ endif
 PROJ_CONF_ENV = \
 	CFLAGS="$(PROJ_CFLAGS)" \
 	CXXFLAGS="$(PROJ_CXXFLAGS)"
+
+ifeq ($(BR2_PACKAGE_LIBCURL),y)
+PROJ_DEPENDENCIES += libcurl
+PROJ_CONF_OPTS += --with-curl=$(STAGING_DIR)/usr/bin/curl-config
+else
+PROJ_CONF_OPTS += --without-curl
+endif
+
+ifeq ($(BR2_PACKAGE_TIFF),y)
+PROJ_DEPENDENCIES += tiff
+PROJ_CONF_OPTS += --enable-tiff
+else
+PROJ_CONF_OPTS += --disable-tiff
+endif
 
 $(eval $(autotools-package))
