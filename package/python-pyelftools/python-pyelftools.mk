0704168abf839cce688d342d86235ed8e27e76ad
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2020-05-02 23:28:07 +0200

    package/python3-pyelftools: new package
    We will need this Python 3.x variant of the host-python-pyelftools
    package to be able to build some recent versions of U-Boot (>=
    2020.01).
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-pyelftools/python-pyelftools.mk
+++ package/python-pyelftools/python-pyelftools.mk
@@ -4,6 +4,7 @@
 #
 ################################################################################
 
+# Please keep in sync with package/python3-pyelftools/python3-pyelftools.mk
 PYTHON_PYELFTOOLS_VERSION = 0.25
 PYTHON_PYELFTOOLS_SOURCE = pyelftools-$(PYTHON_PYELFTOOLS_VERSION).tar.gz
 PYTHON_PYELFTOOLS_SITE = https://files.pythonhosted.org/packages/fa/9a/0674cb1725196568bdbca98304f2efb17368b57af1a4bb3fc772c026f474
