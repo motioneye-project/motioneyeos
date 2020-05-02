840821850b9c234819f3960cc8c8bd66b6768a03
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-05-02 19:20:30 +0200

    package/python-jedi: bump to version 0.17.0
    Drop flask license as the old flask theme has been replaced with the
    sphinx_rtd_theme since version 0.17.0 and
    https://github.com/davidhalter/jedi/commit/8f96cbdabff671325a2987eb23ba91de90381c15
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/python-jedi/python-jedi.mk
+++ package/python-jedi/python-jedi.mk
@@ -4,12 +4,11 @@
 #
 ################################################################################
 
-PYTHON_JEDI_VERSION = 0.15.1
+PYTHON_JEDI_VERSION = 0.17.0
 PYTHON_JEDI_SOURCE = jedi-$(PYTHON_JEDI_VERSION).tar.gz
-PYTHON_JEDI_SITE = https://files.pythonhosted.org/packages/85/03/cd5a6e44a5753b4d539288d9d1f9645caac889c17dd2950292a8818f86b2
+PYTHON_JEDI_SITE = https://files.pythonhosted.org/packages/e3/5b/65ff9c102d92bf719dfaeff57bc8074d68f26ea480005704a956da995799
 PYTHON_JEDI_SETUP_TYPE = setuptools
-PYTHON_JEDI_LICENSE = MIT, BSD-3-Clause (flask theme), Apache-2.0 (typeshed)
-PYTHON_JEDI_LICENSE_FILES = \
-	LICENSE.txt docs/_themes/flask/LICENSE jedi/third_party/typeshed/LICENSE
+PYTHON_JEDI_LICENSE = MIT, Apache-2.0 (typeshed)
+PYTHON_JEDI_LICENSE_FILES = LICENSE.txt jedi/third_party/typeshed/LICENSE
 
 $(eval $(python-package))
