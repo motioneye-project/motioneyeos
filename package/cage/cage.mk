9a366a15fd4b800926aaaaf67dff4c63f22d1db8
Gilles Talis <gilles.talis@gmail.com>
2020-05-03 16:43:55 +0200

    package/cage: bump to latest upstream version
    cage v0.1.1 can't build with wlroots 0.10.1, as it uses
    a set of wlroots API that are no longer available.
    
    Moving to latest upstream version fixes the issue.
    
    Also:
    - added license file information
    - updated license file hash
    
    Signed-off-by: Gilles Talis <gilles.talis@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/cage/cage.mk
+++ package/cage/cage.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-CAGE_VERSION = 0.1.1
-CAGE_SITE = $(call github,Hjdskes,cage,v$(CAGE_VERSION))
+CAGE_VERSION = 6eb693c05b5b34d4ed5ad8234a9f79a14ac8e07d
+CAGE_SITE = $(call github,Hjdskes,cage,$(CAGE_VERSION))
 CAGE_LICENSE = MIT
+CAGE_LICENSE_FILES = LICENSE
 CAGE_DEPENDENCIES = host-pkgconf wlroots
 
 ifeq ($(BR2_PACKAGE_XORG7),y)
