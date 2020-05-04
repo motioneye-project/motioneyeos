ffd8ffd02f90989dcf2c1f060b5671b3144234fb
Romain Naour <romain.naour@gmail.com>
2020-05-04 16:52:12 +0200

    package/zziplib: bump to version 0.13.71
    Remove upstream patches and CVE tags.
    
    Switch the dependency to python3 added by [1].
    
    Update indentation of hash file (two spaces).
    
    [1] https://github.com/gdraheim/zziplib/commit/a144bec8d06302e7be11f0f46e02947b0becf574
    
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/zziplib/zziplib.mk
+++ package/zziplib/zziplib.mk
@@ -4,23 +4,14 @@
 #
 ################################################################################
 
-ZZIPLIB_VERSION = 0.13.69
+ZZIPLIB_VERSION = 0.13.71
 ZZIPLIB_SITE = $(call github,gdraheim,zziplib,v$(ZZIPLIB_VERSION))
 ZZIPLIB_LICENSE = LGPL-2.0+ or MPL-1.1
 ZZIPLIB_LICENSE_FILES = docs/COPYING.LIB docs/COPYING.MPL docs/copying.htm
 ZZIPLIB_INSTALL_STAGING = YES
 
-# 0001-Avoid-memory-leak-from-__zzip_parse_root_directory.patch
-# 0002-Avoid-memory-leak-from-__zzip_parse_root_directory-2.patch
-# 0003-One-more-free-to-avoid-memory-leak.patch
-ZZIPLIB_IGNORE_CVES += CVE-2018-16548
+ZZIPLIB_DEPENDENCIES = host-pkgconf host-python3 zlib
 
-# 0004-Fix-issue-62-Remove-any-components-from-pathnames-of-extracte.patch
-ZZIPLIB_IGNORE_CVES += CVE-2018-17828
-
-ZZIPLIB_DEPENDENCIES = host-pkgconf host-python zlib
-
-# zziplib is not python3 friendly, so force the python interpreter
-ZZIPLIB_CONF_OPTS = ac_cv_path_PYTHON=$(HOST_DIR)/bin/python2
+ZZIPLIB_CONF_OPTS = ac_cv_path_PYTHON=$(HOST_DIR)/bin/python3
 
 $(eval $(autotools-package))
