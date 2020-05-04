a63f391ea8c759adc793324395ac7c63a09fe794
popoviciri <radoo.popovici@gmail.com>
2020-05-04 22:52:43 +0200

    bring back MOTION_LICENSE; copied this from motioneyeos repo

--- package/motion/motion.mk
+++ package/motion/motion.mk
@@ -6,6 +6,8 @@
 
 MOTION_VERSION = release-4.3.1
 MOTION_SITE = $(call github,Motion-Project,motion,$(MOTION_VERSION))
+MOTION_LICENSE = GPL-2.0
+MOTION_LICENSE_FILES = doc/COPYING
 MOTION_AUTORECONF = YES
 MOTION_DEPENDENCIES = host-pkgconf ffmpeg jpeg libmicrohttpd
 MOTION_CONF_OPTS = --without-pgsql \
