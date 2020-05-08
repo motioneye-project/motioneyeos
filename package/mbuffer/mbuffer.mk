692e63caf25afd242d141862f40fc4d6c91a8c34
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-05-08 16:26:39 +0200

    package/mbuffer: bump to version 20200505
    This bump will fix a build failure with gcc 4.8
    
    Fixes:
     - http://autobuild.buildroot.org/results/1a7fe7ac8cbe9997bc5d1e9ade24bc2ee6ac6abe
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/mbuffer/mbuffer.mk
+++ package/mbuffer/mbuffer.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MBUFFER_VERSION = 20191016
+MBUFFER_VERSION = 20200505
 MBUFFER_SOURCE = mbuffer-$(MBUFFER_VERSION).tgz
 MBUFFER_SITE = http://www.maier-komor.de/software/mbuffer
 MBUFFER_LICENSE = GPL-3.0+
