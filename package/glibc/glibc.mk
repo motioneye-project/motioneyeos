6488684e2beeb22c732d211b58951b6e9eb5fc68
Peter Korsgaard <peter@korsgaard.com>
2020-05-12 10:53:23 +0200

    package/glibc: bump version for additional post-2.30 security fixes
    Fixes the following security vulnerabilities:
    
    CVE-2020-10029: Trigonometric functions on x86 targets suffered from stack
      corruption when they were passed a pseudo-zero argument.  Reported by Guido
      Vranken / ForAllSecure Mayhem.
    
    CVE-2020-1751: A defect in the PowerPC backtrace function could cause an
      out-of-bounds write when executed in a signal frame context.
    
    CVE-2020-1752: A use-after-free vulnerability in the glob function when
      expanding ~user has been fixed.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -17,7 +17,7 @@ else
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
 # When updating the version, please also update localedef
-GLIBC_VERSION = 2.30-20-g50f20fe506abb8853641006a7b90a81af21d7b91
+GLIBC_VERSION = 2.30-67-g4748829f86a458b76642f3e98b1d80f7b868e427
 # Upstream doesn't officially provide an https download link.
 # There is one (https://sourceware.org/git/glibc.git) but it's not reliable,
 # sometimes the connection times out. So use an unofficial github mirror.
