fbad745139125ea0995a436fd81ce7ea233638c0
James Hilliard <james.hilliard1@gmail.com>
2020-05-12 21:28:59 -0600

    package/exfatprogs: bump to version 1.0.3
    Drop patches that are now upstream.
    
    We don't need to autoreconf since we are using a release tarball.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/exfatprogs/exfatprogs.mk
+++ package/exfatprogs/exfatprogs.mk
@@ -4,11 +4,10 @@
 #
 ################################################################################
 
-EXFATPROGS_VERSION = 1.0.2
-EXFATPROGS_SITE = $(call github,exfatprogs,exfatprogs,$(EXFATPROGS_VERSION))
+EXFATPROGS_VERSION = 1.0.3
+EXFATPROGS_SITE = https://github.com/exfatprogs/exfatprogs/releases/download/$(EXFATPROGS_VERSION)
 EXFATPROGS_LICENSE = GPL-2.0+
 EXFATPROGS_LICENSE_FILES = COPYING
-EXFATPROGS_AUTORECONF = YES
 EXFATPROGS_DEPENDENCIES = host-pkgconf
 HOST_EXFATPROGS_DEPENDENCIES = host-pkgconf
 
