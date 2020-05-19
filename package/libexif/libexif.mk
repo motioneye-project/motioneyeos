ca0547ffeaea77b1b59ddcf77a2f3713167f8a7e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-05-19 20:02:16 +0200

    package/libexif: security bump to version 0.6.22
    - Switch site to github
    - Drop patches (already in version)
    - Fix the following CVEs:
      - CVE-2020-13114: Time consumption DoS when parsing canon array
        markers
      - CVE-2020-13113: Potential use of uninitialized memory
      - CVE-2020-13112: Various buffer overread fixes due to integer
        overflows in maker notes
      - CVE-2020-0093: read overflow
      - CVE-2020-12767: fixed division by zero
    
    https://github.com/libexif/libexif/releases/tag/libexif-0_6_22-release
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/libexif/libexif.mk
+++ package/libexif/libexif.mk
@@ -4,21 +4,13 @@
 #
 ################################################################################
 
-LIBEXIF_VERSION = 0.6.21
-LIBEXIF_SOURCE = libexif-$(LIBEXIF_VERSION).tar.bz2
-LIBEXIF_SITE = http://downloads.sourceforge.net/project/libexif/libexif/$(LIBEXIF_VERSION)
+LIBEXIF_VERSION = 0.6.22
+LIBEXIF_SOURCE = libexif-$(LIBEXIF_VERSION).tar.xz
+LIBEXIF_SITE = \
+	https://github.com/libexif/libexif/releases/download/libexif-$(subst .,_,$(LIBEXIF_VERSION))-release
 LIBEXIF_INSTALL_STAGING = YES
 LIBEXIF_DEPENDENCIES = host-pkgconf
 LIBEXIF_LICENSE = LGPL-2.1+
 LIBEXIF_LICENSE_FILES = COPYING
-
-# 0001-fixes-some-not-all-buffer-overreads-during-decoding-.patch
-LIBEXIF_IGNORE_CVES += CVE-2016-6328
-# 0002-On-saving-makernotes-make-sure-the-makernote-contain.patch
-LIBEXIF_IGNORE_CVES += CVE-2017-7544
-# 0004-Improve-deep-recursion-detection-in-exif_data_load_d.patch
-LIBEXIF_IGNORE_CVES += CVE-2018-20030
-# 0005-fix-CVE-2019-9278.patch
-LIBEXIF_IGNORE_CVES += CVE-2019-9278
 
 $(eval $(autotools-package))
