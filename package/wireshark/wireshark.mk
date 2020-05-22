39bfd504102dce2166e9d9e1377744debde64b38
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-05-22 22:10:41 +0200

    package/wireshark: security bump to version 3.2.4
    Fix CVE-2020-13164: In Wireshark 3.2.0 to 3.2.3, 3.0.0 to 3.0.10, and
    2.6.0 to 2.6.16, the NFS dissector could crash. This was addressed in
    epan/dissectors/packet-nfs.c by preventing excessive recursion, such as
    for a cycle in the directory graph on a filesystem.
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/wireshark/wireshark.mk
+++ package/wireshark/wireshark.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WIRESHARK_VERSION = 3.2.3
+WIRESHARK_VERSION = 3.2.4
 WIRESHARK_SOURCE = wireshark-$(WIRESHARK_VERSION).tar.xz
 WIRESHARK_SITE = https://www.wireshark.org/download/src/all-versions
 WIRESHARK_LICENSE = wireshark license
