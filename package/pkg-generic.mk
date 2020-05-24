9ff7b6170561dde1ea01f94cf765abe73140029b
Heiko Thiery <heiko.thiery@gmail.com>
2020-05-24 23:28:29 +0200

    package/pkg-generic.mk: enable hash checks for svn tarbals
    With commit 89f5e989323ace815a32fced27eaefee2f4666de support for
    reproducible archives was added. Thus archives generated from svn do no
    longer needs to be added to BR_NO_CHECK_HASH_FOR.
    
    Cc: Yann E. MORIN <yann.morin.1998@free.fr>
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>

--- package/pkg-generic.mk
+++ package/pkg-generic.mk
@@ -577,7 +577,7 @@ ifndef $(2)_DL_OPTS
  endif
 endif
 
-ifneq ($$(filter bzr cvs hg svn,$$($(2)_SITE_METHOD)),)
+ifneq ($$(filter bzr cvs hg,$$($(2)_SITE_METHOD)),)
 BR_NO_CHECK_HASH_FOR += $$($(2)_SOURCE)
 endif
 
