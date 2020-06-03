6aa30f5ca47afb68ac79457f8ebb16f986b4fd2c
Martin Bark <martin@barkynet.com>
2020-06-03 20:31:50 +0100

    package/nghttp2: security bump version to 1.41.0
    Fix CVE-2020-11080 Denial of service: Overly large SETTINGS frames
    
    Signed-off-by: Martin Bark <martin@barkynet.com>
    [yann.morin.1998@free.fr: two spaces in hash files]
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit e500367ea44adffcfc3132099bedf550d9913313)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/nghttp2/nghttp2.mk
+++ package/nghttp2/nghttp2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NGHTTP2_VERSION = 1.39.2
+NGHTTP2_VERSION = 1.41.0
 NGHTTP2_SITE = https://github.com/nghttp2/nghttp2/releases/download/v$(NGHTTP2_VERSION)
 NGHTTP2_LICENSE = MIT
 NGHTTP2_LICENSE_FILES = COPYING
