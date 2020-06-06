c7123a4956ae16cf0b06e1cabec695d3e34ed18c
Christian Stewart <christian@paral.in>
2020-06-06 13:36:47 -0700

    package/go: bump version to 1.13.12
    go1.13.9 (released 2020/03/19) includes fixes to the go command, tools, the
    runtime, the toolchain, and the crypto/cypher package.
    
    go1.13.10 (released 2020/04/08) includes fixes to the go command, the runtime,
    and the os/exec and time packages.
    
    go1.13.11 (released 2020/05/14) includes fixes to the compiler.
    
    go1.13.12 (released 2020/06/01) includes fixes to the runtime, and the go/types
    and math/big packages.
    
    Release notes: https://golang.org/doc/go1.13
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    (cherry picked from commit 7cbb3dd94ec24929d937a95a57d9a5d54b9d444d)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.13.8
+GO_VERSION = 1.13.12
 GO_SITE = https://storage.googleapis.com/golang
 GO_SOURCE = go$(GO_VERSION).src.tar.gz
 
