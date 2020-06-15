6987a72b5cdd3699216083a5c08401f8775303d7
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-06-15 22:31:50 +0200

    package/tinydtls: security bump to version 0.9-rc1
    - Switch site to github
    - License is now EPL-1.0 or EDL-1.0 as specified in the new LICENSE file
    - Update indentation of hash file (two spaces)
    - Drop first patch (already in version) and second patch (not needed since
      https://github.com/eclipse/tinydtls/commit/f1ff324a4d1cc14dc6e1c3a88ea16f0242e106de)
    - Fix CVE-2017-7243 as specified in
      https://github.com/eclipse/tinydtls/issues/12 as well as other
      security issues:
      https://github.com/eclipse/tinydtls/commit/68a1cdaff9e329e13ea59529f1eb61b05632c297
      https://github.com/eclipse/tinydtls/commit/494a40dfbb174930ca616e560532d52549736b42
      https://github.com/eclipse/tinydtls/commit/2d9f0a82377277af1be8d559d18e30477d63e8ec
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 1d14a3349dd50c245fed4b5a9d2c77b9964571e6)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/tinydtls/tinydtls.mk
+++ package/tinydtls/tinydtls.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-TINYDTLS_REL = r5
-TINYDTLS_VERSION = 0.8.2
-TINYDTLS_SITE = http://downloads.sourceforge.net/project/tinydtls/$(TINYDTLS_REL)
-TINYDTLS_LICENSE = MIT
-TINYDTLS_LICENSE_FILES = tinydtls.h
+TINYDTLS_VERSION = 0.9-rc1
+TINYDTLS_SITE = $(call github,eclipse,tinydtls,v$(TINYDTLS_VERSION))
+TINYDTLS_LICENSE = EPL-1.0 or EDL-1.0
+TINYDTLS_LICENSE_FILES = LICENSE
 TINYDTLS_INSTALL_STAGING = YES
-TINYDTLS_STRIP_COMPONENTS = 2
+# From git
+TINYDTLS_AUTORECONF = YES
 # use inttypes.h data types instead of u_intXX_t for musl compatibility
 TINYDTLS_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -DSHA2_USE_INTTYPES_H"
 
