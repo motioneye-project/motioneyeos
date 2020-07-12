7dc0e94b0c8d17b90064cb4cd4a7bcce128f25e4
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-07-13 00:06:57 +0200

    package/jq: fix license
    Commit c94794175fab58823fde539eac83ec0234768f36 forgot to update hash of
    COPYING
    
    ICU license has been added for decNumber library since
    https://github.com/stedolan/jq/commit/b6be13d5de6dd7d8aad5fd871eb6b0b30fc7d7f6
    
    Fixes:
     - http://autobuild.buildroot.org/results/569aa7ba86a022d3c32a65fb8b58f558aba7ae4c
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 16d0f5bff84a8e0384679d013eae511ef693776b)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/jq/jq.mk
+++ package/jq/jq.mk
@@ -6,7 +6,7 @@
 
 JQ_VERSION = a17dd3248a666d01be75f6b16be37e80e20b0954
 JQ_SITE = $(call github,stedolan,jq,$(JQ_VERSION))
-JQ_LICENSE = MIT (code), CC-BY-3.0 (documentation)
+JQ_LICENSE = MIT (code), ICU (decNumber), CC-BY-3.0 (documentation)
 JQ_LICENSE_FILES = COPYING
 JQ_INSTALL_STAGING = YES
 
