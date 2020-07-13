d546b005d36e4a498142e1871da4a60f498fc69e
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2020-07-13 18:08:19 +0200

    package/gssdp: bump to version 1.2.3
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Signed-off-by: Yann E. MORIN <yann.morin.1998@free.fr>
    (cherry picked from commit 7a2f73e9930d3e46f1530aa865675439adc0e162)
    [Peter: needed by gupnp-1.2.3]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/gssdp/gssdp.mk
+++ package/gssdp/gssdp.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 GSSDP_VERSION_MAJOR = 1.2
-GSSDP_VERSION = $(GSSDP_VERSION_MAJOR).2
+GSSDP_VERSION = $(GSSDP_VERSION_MAJOR).3
 GSSDP_SOURCE = gssdp-$(GSSDP_VERSION).tar.xz
 GSSDP_SITE = http://ftp.gnome.org/pub/gnome/sources/gssdp/$(GSSDP_VERSION_MAJOR)
 GSSDP_LICENSE = LGPL-2.0+
