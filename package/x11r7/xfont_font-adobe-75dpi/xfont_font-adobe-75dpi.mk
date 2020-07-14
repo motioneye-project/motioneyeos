1494a5c77cd11b07a66814d19e974a66b0516b39
Aaron Sierra <asierra@xes-inc.com>
2020-07-13 20:24:59 -0500

    package/x11r7/xfont_font-*: make outputs reproducible
    Prior to gzip 1.10, the compression pipeline used with PCF fonts was
    not reproducible due to the implicit -N/--name injecting a timestamp:
    
      $ cat /path/to/file | gzip > /path/to/file.gz
    
    This updates Portable Compiled Format font packages to have a host-gzip
    dependency, so gzip version 1.10 or newer will reliably be used.
    
    This change does not affect encodings, which use a seemingly
    synonymous compression pipeline, but that happens to be reproducible
    with gzip versions at least as old as version 1.3.13:
    
      $ gzip < /path/to/file > /path/to/file.gz
    
    Reported-by: Jordan Speicher <jspeicher@xes-inc.com>
    Signed-off-by: Aaron Sierra <asierra@xes-inc.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 10082b2e439c95cf285ab462fe0961d4c67d9c91)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/x11r7/xfont_font-adobe-75dpi/xfont_font-adobe-75dpi.mk
+++ package/x11r7/xfont_font-adobe-75dpi/xfont_font-adobe-75dpi.mk
@@ -12,6 +12,11 @@ XFONT_FONT_ADOBE_75DPI_LICENSE_FILES = COPYING
 
 XFONT_FONT_ADOBE_75DPI_INSTALL_STAGING_OPTS = DESTDIR=$(STAGING_DIR) MKFONTSCALE=$(HOST_DIR)/bin/mkfontscale MKFONTDIR=$(HOST_DIR)/bin/mkfontdir install
 XFONT_FONT_ADOBE_75DPI_INSTALL_TARGET_OPTS = DESTDIR=$(TARGET_DIR) MKFONTSCALE=$(HOST_DIR)/bin/mkfontscale MKFONTDIR=$(HOST_DIR)/bin/mkfontdir install-data
-XFONT_FONT_ADOBE_75DPI_DEPENDENCIES = xfont_font-util host-xfont_font-util host-xapp_mkfontscale host-xapp_bdftopcf
+XFONT_FONT_ADOBE_75DPI_DEPENDENCIES = \
+	xfont_font-util \
+	host-xfont_font-util \
+	host-xapp_mkfontscale \
+	host-xapp_bdftopcf \
+	host-gzip
 
 $(eval $(autotools-package))
--- package/x11r7/xfont_font-micro-misc/xfont_font-micro-misc.mk
+++ package/x11r7/xfont_font-micro-misc/xfont_font-micro-misc.mk
@@ -12,6 +12,11 @@ XFONT_FONT_MICRO_MISC_LICENSE_FILES = COPYING
 
 XFONT_FONT_MICRO_MISC_INSTALL_STAGING_OPTS = DESTDIR=$(STAGING_DIR) MKFONTSCALE=$(HOST_DIR)/bin/mkfontscale MKFONTDIR=$(HOST_DIR)/bin/mkfontdir install
 XFONT_FONT_MICRO_MISC_INSTALL_TARGET_OPTS = DESTDIR=$(TARGET_DIR) MKFONTSCALE=$(HOST_DIR)/bin/mkfontscale MKFONTDIR=$(HOST_DIR)/bin/mkfontdir install-data
-XFONT_FONT_MICRO_MISC_DEPENDENCIES = xfont_font-util host-xfont_font-util host-xapp_mkfontscale host-xapp_bdftopcf
+XFONT_FONT_MICRO_MISC_DEPENDENCIES = \
+	xfont_font-util \
+	host-xfont_font-util \
+	host-xapp_mkfontscale \
+	host-xapp_bdftopcf \
+	host-gzip
 
 $(eval $(autotools-package))
