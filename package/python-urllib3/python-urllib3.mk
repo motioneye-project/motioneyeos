a6af42abe24fd2aa90a18cb9864677822c03a210
Matt Weber <matthew.weber@rockwellcollins.com>
2020-07-14 15:02:02 -0500

    package/python-urllib3: security bump to 1.25.9
    Fixes CVE-2020-7212 (1.25.2 - 1.25.7)
    The _encode_invalid_chars function does not remove duplicate percent
    encodings in the _percent_encodings array, which combined with the
    normalization step could take O(N^2) time to compute for a URL of
    length N. This results in a marginally higher CPU consumption
    compared to the potential linear time achieved by deduplicating
    the _percent_encodings array.
    
    CC: Peter Korsgaard <peter@korsgaard.com>
    Signed-off-by: Matthew Weber <matthew.weber@rockwellcollins.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit fc57db8401b806494ceeb20e4c91d2be8a8456eb)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/python-urllib3/python-urllib3.mk
+++ package/python-urllib3/python-urllib3.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_URLLIB3_VERSION = 1.25.6
+PYTHON_URLLIB3_VERSION = 1.25.9
 PYTHON_URLLIB3_SOURCE = urllib3-$(PYTHON_URLLIB3_VERSION).tar.gz
-PYTHON_URLLIB3_SITE = https://files.pythonhosted.org/packages/ff/44/29655168da441dff66de03952880c6e2d17b252836ff1aa4421fba556424
+PYTHON_URLLIB3_SITE = https://files.pythonhosted.org/packages/05/8c/40cd6949373e23081b3ea20d5594ae523e681b6f472e600fbc95ed046a36
 PYTHON_URLLIB3_LICENSE = MIT
 PYTHON_URLLIB3_LICENSE_FILES = LICENSE.txt
 PYTHON_URLLIB3_SETUP_TYPE = setuptools
