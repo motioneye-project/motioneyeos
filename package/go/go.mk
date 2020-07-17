90d8c2459bae9c3325e10cfc1817c0b1f2884d4a
Christian Stewart <christian@paral.in>
2020-07-17 12:37:18 -0700

    package/go: bump version to 1.13.14
    go1.13.14 (released 2020/07/16) includes fixes to the compiler, vet, and
    the database/sql, net/http, and reflect packages.
    
    Signed-off-by: Christian Stewart <christian@paral.in>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    (cherry picked from commit 593254c6f91fc67653ee53d1c392a8bce698889c)
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/go/go.mk
+++ package/go/go.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GO_VERSION = 1.13.13
+GO_VERSION = 1.13.14
 GO_SITE = https://storage.googleapis.com/golang
 GO_SOURCE = go$(GO_VERSION).src.tar.gz
 
