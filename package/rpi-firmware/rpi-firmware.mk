61126882e29456dce4da59995fb5419791f9a691
Calin Crisan <ccrisan@gmail.com>
2020-08-09 22:19:42 +0300

    Raspberry Pi (all): update kernel, firmware & userland

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 5574077183389cd4c65077ba18b59144ed6ccd6d
+RPI_FIRMWARE_VERSION = 7111c89a395159afeb161b205d59c894cfc1fdc1
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_USERLAND_VERSION = 6fb59736b1ae80fc62cddfe3309c800f72e1c07e
+RPI_USERLAND_VERSION = 3e59217bd93b8024fb8fc1c6530b00cbae64bc73
 RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
 RPI_USERLAND_LICENSE = BSD-3-Clause
 RPI_USERLAND_LICENSE_FILES = LICENCE
