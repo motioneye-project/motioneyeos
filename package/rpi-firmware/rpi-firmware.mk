2ccd5f5e06fca39bdeb8907d7c905587711caaa2
Valentino Međimorec <valentino@medimorec.com>
2026-08-14 23:03:12 +0200

    Raspberry Pi 4 Model B - rpi4-rev15-firmware-bump

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 3f20b832b27cd730deb6419b570f31a98167eef6
+RPI_FIRMWARE_VERSION = 191360eaf2e5933eaa0ed76ac0d62722b6f9a58f
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
